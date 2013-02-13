23dd46c03f8add3c8daace840afad35173c8c8d2
Rashid Khan <rashidkpc@gmail.com>
2013-02-13 16:12:57 -0700

    Added timepicker editor

--- panels/timepicker/module.js
+++ panels/timepicker/module.js
@@ -33,6 +33,7 @@ angular.module('kibana.timepicker', [])
     mode          : "relative",
     time_options  : ['5m','15m','1h','6h','12h','24h','2d','7d','30d'],
     timespan      : '15m',
+    timefield     : '@timestamp',
     index         : '"logstash-"yyyy.mm.dd',
     group         : "default",
     refresh       : {
