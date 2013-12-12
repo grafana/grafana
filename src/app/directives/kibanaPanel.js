7580042b29160e10ec8eaed11abbbb71e012b238
Rashid Khan <rashidkpc@gmail.com>
2013-12-12 16:23:42 -0700

    fixed initialization bugs

--- src/app/directives/kibanaPanel.js
+++ src/app/directives/kibanaPanel.js
@@ -61,15 +61,21 @@ function (angular) {
         link: function($scope, elem, attr) {
           // once we have the template, scan it for controllers and
           // load the module.js if we have any
+          var newScope = $scope.$new();
 
           // compile the module and uncloack. We're done
           function loadModule($module) {
             $module.appendTo(elem);
             elem.wrap(container);
             /* jshint indent:false */
-            $compile(elem.contents())($scope);
+            $compile(elem.contents())(newScope);
             elem.removeClass("ng-cloak");
           }
+
+          newScope.$on('$destroy',function(){
+            elem.unbind();
+            elem.remove();
+          });
 
           $scope.$watch(attr.type, function (name) {
             elem.addClass("ng-cloak");
