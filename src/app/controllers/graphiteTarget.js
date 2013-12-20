816cc98658ab390f52a2252f43f699d83e28be4c
Torkel Ödegaard <torkel.odegaard@gmail.com>
2013-12-20 08:37:48 +0100

    working on graphite target editor

--- src/app/controllers/graphiteTarget.js
+++ src/app/controllers/graphiteTarget.js
@@ -14,15 +14,21 @@ function (angular, _) {
         var strSegments = $scope.target.target.split('.');
         _.each(strSegments, function (segment, index) {
           if (segment === '*') {
-            segment = '<i class="icon-asterisk"><i>';
+            $scope.segments[index] = { val: segment, html: '<i class="icon-asterisk"><i>' };
+            return;
           }
 
-          $scope.segments[index] = { val: segment };
+          $scope.segments[index] = { val: segment, html: segment};
         });
       };
 
       $scope.setSegmentStar = function (index) {
-        $scope.segments[index] = {val: '<i class="icon-asterisk"><i>' };
+        $scope.segments[index].val = '*';
+        $scope.segments[index].html = '<i class="icon-asterisk"><i>';
+        $scope.target.target = _.reduce($scope.segments, function(result, segment) {
+            return result ? (result + "." + segment.val) : segment.val;
+          }, null);
+        $scope.targetChanged();
       };
 
       $scope.targetChanged = function() {
