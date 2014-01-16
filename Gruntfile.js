839f0accab42c4d2424764614e4f10434e43397c
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-01-16 19:50:26 +0100

    more wip work

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,31 +4,24 @@ module.exports = function (grunt) {
 
   var config = {
     pkg: grunt.file.readJSON('package.json'),
-    baseDir: '.',
+
+    connect: {
+      dev: {
+        options: {
+          port: 5603,
+          base: '.',
+          keepalive: true
+        }
+      },
+    }
   };
 
-  // load plugins
   require('load-grunt-tasks')(grunt);
-
-  // load task definitions
   grunt.loadTasks('tasks');
 
-  // Utility function to load plugin settings into config
-  function loadConfig(config,path) {
-    require('glob').sync('*', {cwd: path}).forEach(function(option) {
-      var key = option.replace(/\.js$/,'');
-      // If key already exists, extend it. It is your responsibility to avoid naming collisions
-      config[key] = config[key] || {};
-      grunt.util._.extend(config[key], require(path + option)(config,grunt));
-    });
-    // technically not required
-    return config;
-  }
+  grunt.registerTask('server', ['connect:dev']);
 
-  // Merge that object with what with whatever we have here
-  loadConfig(config,'./tasks/options/');
 
-  // pass the config to grunt
   grunt.initConfig(config);
 
 };
