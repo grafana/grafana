250e3546595a3e21457b48d658e54680cdd9aaf8
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-02-01 09:49:57 +0100

    Dashboard search fix, only query against title field (Closes #51,Fixes #24)

--- src/app/services/dashboard.js
+++ src/app/services/dashboard.js
@@ -385,14 +385,12 @@ function (angular, $, kbn, _, config, moment, Modernizr) {
       );
     };
 
-    this.elasticsearch_list = function(query,count) {
+    this.elasticsearch_list = function(query, count) {
       var request = ejs.Request().indices(config.grafana_index).types('dashboard');
-      var q = null;
-      if(config.elasticsearch_all_disabled==true){
-        q = ('title:' + (query || '*'))
-      }else{
-        q = (query || '*')
-      }
+      // if elasticsearch has disabled _all field we need
+      // need to specifiy field here
+      var q = 'title:' + (query || '*');
+
       return request.query(
         ejs.QueryStringQuery(q)
         ).size(count).doSearch(
