1aff1a0000f80ea6a4b8f7019a2bc9c0ff31dec9
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-02-05 15:39:50 +0100

    mini improvement to xasis format (now shows just hours for larger time spans)

--- src/app/directives/grafanaGraph.js
+++ src/app/directives/grafanaGraph.js
@@ -269,11 +269,11 @@ function (angular, $, kbn, moment, _) {
           if(_int >= 10000) {
             return "%Y-%m-%d";
           }
-          if(_int >= 60) {
+          if(_int >= 700) {
             return "%H:%M<br>%m-%d";
           }
 
-          return "%H:%M:%S";
+          return "%H:%M";
         }
 
         var $tooltip = $('<div>');
