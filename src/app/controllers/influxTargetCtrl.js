da62a5653ae767b9868e0b886a4a2679be75d1da
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-03-02 18:24:15 +0100

    added typeahead / autocomplete to influxdb series name, #103

--- src/app/controllers/influxTargetCtrl.js
+++ src/app/controllers/influxTargetCtrl.js
@@ -6,12 +6,38 @@ function (angular) {
 
   var module = angular.module('kibana.controllers');
 
-  module.controller('InfluxTargetCtrl', function($scope) {
+  var seriesList = null;
+
+  module.controller('InfluxTargetCtrl', function($scope, $timeout) {
 
     $scope.init = function() {
       if (!$scope.target.function) {
         $scope.target.function = 'mean';
       }
+
+      if (!seriesList) {
+        seriesList = [];
+        $scope.datasource.listSeries().then(function(series) {
+          seriesList = series;
+        });
+      }
+
+      $scope.oldSeris = $scope.target.series;
+      $scope.$on('typeahead-updated', function(){
+        $timeout($scope.get_data);
+      });
+    };
+
+    // Cannot use typeahead and ng-change on blur at the same time
+    $scope.seriesBlur = function() {
+      if ($scope.oldSeris !== $scope.target.series) {
+        $scope.oldSeris = $scope.target.series;
+        $scope.get_data();
+      }
+    };
+
+    $scope.listSeries = function() {
+      return seriesList;
     };
 
     $scope.duplicate = function() {
