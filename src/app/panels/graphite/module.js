e3b046aecfd23f0abff783b225cb52286c0bc73b
Ian Babrou <ibobrik@gmail.com>
2014-03-31 18:53:43 +0400

    fixed annoying undefined length error

--- src/app/panels/graphite/module.js
+++ src/app/panels/graphite/module.js
@@ -318,9 +318,9 @@ function (angular, app, $, _, kbn, moment, timeSeries) {
         if (last - from < -10000) {
           $scope.datapointsOutside = true;
         }
-      }
 
-      $scope.datapointsCount += datapoints.length;
+        $scope.datapointsCount += datapoints.length;
+      }
 
       return series;
     };
