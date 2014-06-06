6a0d4cdd8c78daebb504ec807fa296155db5ea55
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-06-06 12:31:39 +0200

    asd

--- tasks/default_task.js
+++
@@ -1,5 +0,0 @@
-// Lint and build CSS
-module.exports = function(grunt) {
-  grunt.registerTask('default', ['jshint:source', 'jshint:tests', 'less:src']);
-  grunt.registerTask('test', ['default', 'karma:test']);
-};
