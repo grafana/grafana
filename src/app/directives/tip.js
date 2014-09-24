d60c4bbcab0bb28e31f1766ee1a5194060ab26f7
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-09-24 18:55:55 +0200

    work on share panel view, and better look for checkbox

--- src/app/directives/tip.js
+++ src/app/directives/tip.js
@@ -17,4 +17,18 @@ function (angular, kbn) {
         }
       };
     });
+
+  angular
+    .module('grafana.directives')
+    .directive('tip', function($compile) {
+      return {
+        restrict: 'E',
+        link: function(scope, elem, attrs) {
+          var _t = '<i class="grafana-tip icon-'+(attrs.icon||'question-sign')+'" bs-tooltip="\''+
+            kbn.addslashes(elem.text())+'\'"></i>';
+          elem.replaceWith($compile(angular.element(_t))(scope));
+        }
+      };
+    });
+
 });
