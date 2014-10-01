2e9cc2a74e32d817290a4f83ec0b9d193c387f9a
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-10-01 07:58:31 +0200

    Fix for SharePanelCtrl-specs and travis-ci timezone issue

--- src/test/specs/sharePanelCtrl-specs.js
+++ src/test/specs/sharePanelCtrl-specs.js
@@ -1,8 +1,7 @@
 define([
   './helpers',
-  'moment',
   'controllers/sharePanelCtrl'
-], function(helpers, moment) {
+], function(helpers) {
   'use strict';
 
   describe('SharePanelCtrl', function() {
@@ -12,10 +11,6 @@ define([
 
     beforeEach(ctx.providePhase());
     beforeEach(ctx.createControllerPhase('SharePanelCtrl'));
-
-    function getDate(year, month, day) {
-      return moment.utc(new Date(year, month, day)).toDate();
-    }
 
     describe('shareUrl with current time range and panel', function() {
 
@@ -31,16 +26,16 @@ define([
       it('should generate share url absolute time', function() {
         ctx.$location.path('/test');
         ctx.scope.panel = { id: 22 };
-        ctx.timeSrv.time = { from: getDate(2012,1,1), to: new Date(2014,3,5) };
+        ctx.timeSrv.time = { from: new Date(1362178800000), to: new Date(1396648800000) };
 
         ctx.scope.buildUrl();
-        expect(ctx.scope.shareUrl).to.be('http://server/#/test?from=1328050800000&to=1396648800000&panelId=22&fullscreen');
+        expect(ctx.scope.shareUrl).to.be('http://server/#/test?from=1362178800000&to=1396648800000&panelId=22&fullscreen');
       });
 
       it('should generate share url with time as JSON strings', function() {
         ctx.$location.path('/test');
         ctx.scope.panel = { id: 22 };
-        ctx.timeSrv.time = { from: new Date(2012,1,1).toJSON(), to: new Date(2014,3,5).toJSON() };
+        ctx.timeSrv.time = { from: "2012-01-31T23:00:00.000Z", to: "2014-04-04T22:00:00.000Z" };
 
         ctx.scope.buildUrl();
         expect(ctx.scope.shareUrl).to.be('http://server/#/test?from=1328050800000&to=1396648800000&panelId=22&fullscreen');
