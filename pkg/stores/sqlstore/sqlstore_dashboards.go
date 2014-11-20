00b4d233cc5c81cbdfe42f5c8a4a2d25769f8292
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-11-20 13:02:25 +0100

    Small progress, save search works

--- pkg/stores/sqlstore/sqlstore_dashboards.go
+++ pkg/stores/sqlstore/sqlstore_dashboards.go
@@ -14,7 +14,13 @@ func SaveDashboard(dash *models.Dashboard) error {
 		return err
 	}
 
-	if _, err = sess.Insert(dash); err != nil {
+	if dash.Id == 0 {
+		_, err = sess.Insert(dash)
+	} else {
+		_, err = sess.Update(dash)
+	}
+
+	if err != nil {
 		sess.Rollback()
 		return err
 	} else if err = sess.Commit(); err != nil {
@@ -35,4 +41,14 @@ func GetDashboard(slug string, accountId int64) (*models.Dashboard, error) {
 	}
 
 	return &dashboard, nil
+}
+
+func SearchQuery(query string, accountId int64) ([]*models.SearchResult, error) {
+	sess := x.Limit(100, 0).Where("account_id=?", accountId)
+	sess.Table("Dashboard")
+
+	var results []*models.SearchResult
+	err := sess.Find(&results)
+
+	return results, err
 }
