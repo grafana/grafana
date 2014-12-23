27a8427e69440127a3ab9f0bce9f43007898c685
Mat Schaffer <mat@schaffer.me>
2014-12-23 00:07:14 -0800

    Fix style errors

--- src/app/services/atlas/atlasDatasource.js
+++ src/app/services/atlas/atlasDatasource.js
@@ -1,66 +1,65 @@
 define([
-    'angular',
-    'lodash'
-  ],
-  function (angular, _) {
-    'use strict';
+  'angular',
+  'lodash'
+],
+function (angular, _) {
+  'use strict';
 
-    var module = angular.module('grafana.services');
+  var module = angular.module('grafana.services');
 
-    module.factory('AtlasDatasource', function($q, $http) {
+  module.factory('AtlasDatasource', function($q, $http) {
 
-      function AtlasDatasource(datasource) {
-        this.type = 'atlas';
-        this.url = datasource.url;
-        this.supportMetrics = true;
-      }
+    function AtlasDatasource(datasource) {
+      this.type = 'atlas';
+      this.url = datasource.url;
+      this.supportMetrics = true;
+    }
 
-      AtlasDatasource.prototype.query = function(options) {
-        var deferred = $q.defer();
+    AtlasDatasource.prototype.query = function(options) {
+      var deferred = $q.defer();
 
-        // Atlas can take multiple concatenated stack queries
-        var fullQuery = _.pluck(options.targets, 'query').join(',');
+      // Atlas can take multiple concatenated stack queries
+      var fullQuery = _.pluck(options.targets, 'query').join(',');
 
-        var params = {
-          q: fullQuery,
-          step: options.interval,
-          s: options.range.from,
-          e: options.range.to,
-          format: 'json'
-        };
-
-        var httpOptions = {
-          method: 'GET',
-          url:    this.url + '/api/v1/graph',
-          params: params,
-          inspect: { type: 'atlas' }
-        };
-
-        $http(httpOptions).success(function (data) {
-          deferred.resolve({
-            data: makeTimeSeries(data)
-          });
-        });
-
-        return deferred.promise;
+      var params = {
+        q: fullQuery,
+        step: options.interval,
+        s: options.range.from,
+        e: options.range.to,
+        format: 'json'
       };
 
-      function makeTimeSeries (result) {
-        return _.map(result.legend, function (legend, index) {
-          var series = {target: legend, datapoints: []};
-          var values = _.pluck(result.values, index);
+      var httpOptions = {
+        method: 'GET',
+        url:    this.url + '/api/v1/graph',
+        params: params,
+        inspect: { type: 'atlas' }
+      };
 
-          for (var i = 0; i < values.length; i++) {
-            var value = values[i];
-            var timestamp = result.start + (i * result.step);
-            series.datapoints.push([value, timestamp]);
-          }
-
-          return series;
+      $http(httpOptions).success(function (data) {
+        deferred.resolve({
+          data: makeTimeSeries(data)
         });
-      }
+      });
 
-      return AtlasDatasource;
-    });
+      return deferred.promise;
+    };
 
+    function makeTimeSeries (result) {
+      return _.map(result.legend, function (legend, index) {
+        var series = {target: legend, datapoints: []};
+        var values = _.pluck(result.values, index);
+
+        for (var i = 0; i < values.length; i++) {
+          var value = values[i];
+          var timestamp = result.start + (i * result.step);
+          series.datapoints.push([value, timestamp]);
+        }
+
+        return series;
+      });
+    }
+
+    return AtlasDatasource;
   });
+});
