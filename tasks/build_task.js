599a77c776cf28d4f7578116668f20bcc0fee2b6
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-12-30 11:05:24 +0100

    Added karma test step to build/relase grunt task

--- tasks/build_task.js
+++ tasks/build_task.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
     'jshint:source',
     'jshint:tests',
+    'karma:test',
     'clean:on_start',
     'less:src',
     'concat:cssDark',
