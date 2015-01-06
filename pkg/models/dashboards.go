bcdbec61d79b5e64bef456528277d0f54e5f30a0
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-01-06 18:39:26 +0100

    Dashboard search works better, tag cloud should be done soon

--- pkg/models/dashboards.go
+++ pkg/models/dashboards.go
@@ -27,16 +27,33 @@ type Dashboard struct {
 }
 
 type SearchResult struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
-	Slug  string `json:"slug"`
+	Dashboards []DashboardSearchHit    `json:"dashboards"`
+	Tags       []DashboardTagCloudItem `json:"tags"`
+	TagsOnly   bool                    `json:"tagsOnly"`
+}
+
+type DashboardSearchHit struct {
+	Id    string   `json:"id"`
+	Title string   `json:"title"`
+	Slug  string   `json:"slug"`
+	Tags  []string `json:"tags"`
+}
+
+type DashboardTagCloudItem struct {
+	Term  string `json:"term"`
+	Count int    `json:"count"`
 }
 
 type SearchDashboardsQuery struct {
 	Query     string
 	AccountId int64
 
-	Result []*SearchResult
+	Result []DashboardSearchHit
+}
+
+type GetDashboardTagsQuery struct {
+	AccountId int64
+	Result    []DashboardTagCloudItem
 }
 
 type SaveDashboardCommand struct {
