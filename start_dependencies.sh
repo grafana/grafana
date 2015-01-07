9d629f2780818d7c1e689317a76d6e0e37d0589a
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-01-07 14:05:50 +0100

    Tested with mysql, added simple fig file, need to write a more substantial fig setup

--- start_dependencies.sh
+++
@@ -1,9 +0,0 @@
-docker build -t gf-pro-image .
-
-docker kill gf-pro
-docker rm gf-pro
-
-docker run -d --name="gf-pro" \
-             -p 127.0.0.1:5432:5432 \
-             -v /var/docker/grafana-pro/postgresql:/var/lib/postgres \
-             gf-pro-image
