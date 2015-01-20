b7878f2a7145f242a7ec88a2e2ab54dec062c819
Kyle Mallory <kyle.mallory@experticity.com>
2015-01-20 09:53:33 -0700

    Correcting grunt/lint issues

--- src/app/features/opentsdb/datasource.js
+++ src/app/features/opentsdb/datasource.js
@@ -73,9 +73,7 @@ function (angular, _, kbn) {
     };
 
     OpenTSDBDatasource.prototype.performSuggestQuery = function(query, type, target) {
-
       var that = this;
-      var url = this.url;
       var options = {
         method: 'GET',
         url: this.url + '/api/suggest',
@@ -86,48 +84,51 @@ function (angular, _, kbn) {
 
         }
       };
-      return $http(options).then( function(result) {
+      return $http(options).then(function(result) {
         result.data.sort();
 
-        if ((type == 'metrics' || !target.metric) && _.isEmpty(target.tags))
+        if ((type === 'metrics' || !target.metric) && _.isEmpty(target.tags)) {
           return result.data;
+        }
 
-          return that.performSearchLookup(type, target).then(function(lookupResults) {
-            var output = intersect_safe(that.lastLookupResults, result.data);
-  //        console.log("Lookup Results: " + JSON.stringify(that.lastLookupResults));
-            console.log("Suggest Results: " + JSON.stringify(result.data));
-            console.log("Merged Results: " + JSON.stringify(output));
-            return output;
-          }, function(error) {
-            console.log("Could not performSuggestQuery...");
-            return null;
-          });
+        return that.performSearchLookup(type, target).then(function(lookupResults) {
+          // var output = intersect_safe(that.lastLookupResults, result.data);
+          var output = intersect_safe(lookupResults, result.data);
+          // console.log("Lookup Results: " + JSON.stringify(that.lastLookupResults));
+          // console.log("Suggest Results: " + JSON.stringify(result.data));
+          // console.log("Merged Results: " + JSON.stringify(output));
+          return output;
+        });
       });
     };
-
 
     OpenTSDBDatasource.prototype.performSearchLookup = function(type, target) {
       var that = this;
       var searchTags = [];
       if (!_.isEmpty(target.tags)) {
         _.each(_.pairs(target.tags), function(tag) {
-          searchTags.push(''+ tag[0] + '=' + tag[1]);
+          searchTags.push(tag[0] + '=' + tag[1]);
         });
       }
-      if (type == 'tagv')
-        if (target.currentTagKey)
-          searchTags.push(''+target.currentTagKey+'=*');
-      else if (type == 'tagk')
-        if (target.currentTagValue)
+      if (type === 'tagv') {
+        if (target.currentTagKey) {
+          searchTags.push(target.currentTagKey+'=*');
+        }
+      } else if (type === 'tagk') {
+        if (target.currentTagValue) {
           searchTags.push('*='+target.currentTagValue);
+        }
+      }
 
       var search = '';
-      if (type != 'metrics')
+      if (type !== 'metrics') {
         search += target.metric;
+      }
       search += '{' + searchTags.join(',') + '}';
 
-      if ((type == this.lastLookupType) && (this.lastLookupQuery == search))
+      if ((type === this.lastLookupType) && (this.lastLookupQuery === search)) {
         return this.lastLookupResults;
+      }
       this.lastLookupQuery = search;
       this.lastLookupType = type;
 
@@ -138,28 +139,37 @@ function (angular, _, kbn) {
           m: search
         },
       };
-      console.log(JSON.stringify(options));
+      // console.log(JSON.stringify(options));
 
       return $http(options).then(function(result) {
         // iterate through the results and find all the available/matching tags & values
         var resultSet = new Set();
         _.each(result.data.results, function(lookupResults) {
-          if (type == 'metrics') {
+          if (type === 'metrics') {
             resultSet.add(lookupResults.metric);
           } else {
             _.each(_.pairs(lookupResults.tags), function(tag) {
-              if (type == 'tagk') {
-                if (!target.currentTagValue || (target.currentTagValue == tag[1]))
+              if (type === 'tagk') {
+                if (!target.currentTagValue || (target.currentTagValue === tag[1])) {
                   resultSet.add(tag[0]);
+                }
               } else {
-                if (!target.currentTagKey || (target.currentTagKey == tag[0]))
+                if (!target.currentTagKey || (target.currentTagKey === tag[0])) {
                   resultSet.add(tag[1]);
+                }
               }
-            })
+            });
           }
         });
-        var resultsOut = [v for (v of resultSet)].sort();
-        console.log("Lookup Results: " + JSON.stringify(resultsOut));
+        // Grunt doesn't like this, I guess because its ECMAScript 6?
+        // var resultsOut = [v for (v of resultSet)].sort();
+        var resultsOut = [];
+        resultSet.forEach(function(k,v) {
+          resultsOut.add(v);
+        });
+        resultsOut.sort();
+
+        // console.log("Lookup Results: " + JSON.stringify(resultsOut));
         that.lastLookupResults = resultsOut;
         return resultsOut;
       });
@@ -254,21 +264,21 @@ function (angular, _, kbn) {
       return date.getTime();
     }
 
-    function intersect_safe(a, b)
-    {
+    function intersect_safe(a, b) {
       var ai = 0;
       var bi = 0;
       var result = [];
 
-      while( ai < a.length && bi < b.length ){
-         if      (a[ai] < b[bi] ){ ai++; }
-         else if (a[ai] > b[bi] ){ bi++; }
-         else /* they're equal */
-         {
-           result.push(a[ai]);
-           ai++;
-           bi++;
-         }
+      while(ai < a.length && bi < b.length) {
+        if (a[ai] < b[bi]) {
+          ai++;
+        } else if (a[ai] > b[bi]) {
+          bi++;
+        } else { /* they're equal */
+          result.push(a[ai]);
+          ai++;
+          bi++;
+        }
       }
 
       return result;
