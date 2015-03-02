737ffd88216e0c577c53c93f50654d58689b27e9
Anthony Woods <awoods@raintank.io>
2015-03-02 16:15:44 +0000

    add RaintankAdmin api_key for public collectors.

--- pkg/services/sqlstore/migrations/apikey_mig.go
+++ pkg/services/sqlstore/migrations/apikey_mig.go
@@ -72,4 +72,9 @@ func addApiKeyMigrations(mg *Migrator) {
 	}))
 
 	mg.AddMigration("Drop old table api_key_v1", NewDropTableMigration("api_key_v1"))
+
+	//------ add RaintankAdmin api_key for collectors.-------------
+	mg.AddMigration("insert raintankAdmin key into api_key table", new(RawSqlMigration).
+		Sqlite("INSERT INTO `api_key` VALUES (1,1,'collector','edf918d3643c9f479021639580b07d0b58f962ab281f1e881d57d14802ef1e59c30c936e42a4e9b195bbacacafa3ad22317b','RaintankAdmin',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)").
+		Mysql("INSERT INTO `api_key` VALUES (1,1,'collector','edf918d3643c9f479021639580b07d0b58f962ab281f1e881d57d14802ef1e59c30c936e42a4e9b195bbacacafa3ad22317b','RaintankAdmin',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)"))
 }
