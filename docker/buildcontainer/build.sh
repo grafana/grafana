3a98ce775ab27bb3c6ed689ada3e5dc936c93d93
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-03-07 13:27:02 +0100

    Updated docker build script

--- docker/buildcontainer/build.sh
+++ docker/buildcontainer/build.sh
@@ -3,7 +3,7 @@
 docker kill gfbuild
 docker rm gfbuild
 
-docker build --tag "grafana/buildcontainer" .
+docker build --tag "grafana/buildcontainer" docker/buildcontainer
 
 docker run -i -t \
   -v /home/torkel/dev/go:/go \
