290652956c29e5140902aac48a69769020c23dc0
David Raifaizen <david.raifaizen@lab49.com>
2015-03-19 15:23:36 -0400

    Added unit test for data handler module method

--- src/test/specs/graph-ctrl-specs.js
+++ src/test/specs/graph-ctrl-specs.js
@@ -21,7 +21,9 @@ define([
           data: [
             { target: 'test.cpu1', datapoints: [[1, 10]]},
             { target: 'test.cpu2', datapoints: [[1, 10]]}
-          ]
+          ],
+
+          errors: []
         }));
         ctx.scope.render = sinon.spy();
         ctx.scope.get_data();
@@ -41,6 +43,69 @@ define([
         });
 
       });
+
+    });
+
+    describe('get_data with varying degrees of success', function() {
+      beforeEach(function() {
+        ctx.scope.dataHandlerTotalFailure = sinon.spy();
+        ctx.scope.dataHandlerPartialFailure = sinon.spy();
+      });
+
+
+      it ('should not call error handlers when completely successful', function() {
+        ctx.datasource.query = sinon.stub().returns(ctx.$q.when({
+          data: [
+            { target: 'test.cpu1', datapoints: [[1, 10]]},
+            { target: 'test.cpu2', datapoints: [[1, 10]]}
+          ],
+
+          errors: []
+        }));
+
+        ctx.scope.get_data();
+        ctx.scope.$digest();
+
+        sinon.assert.callCount(ctx.scope.dataHandlerTotalFailure, 0);
+        sinon.assert.callCount(ctx.scope.dataHandlerPartialFailure, 0);
+      });
+
+      it ('should call partial error handler on partial success', function() {
+        ctx.datasource.query = sinon.stub().returns(ctx.$q.when({
+          data: [
+            { target: 'test.cpu1', datapoints: [[1, 10]]}
+          ],
+
+          errors: [
+            { 'target': 'whocares1' }
+          ]
+        }));
+
+        ctx.scope.get_data();
+        ctx.scope.$digest();
+
+        sinon.assert.callCount(ctx.scope.dataHandlerTotalFailure, 0);
+        sinon.assert.callCount(ctx.scope.dataHandlerPartialFailure, 1);
+      });
+
+
+      it ('should call total error handler on total failure', function() {
+        ctx.datasource.query = sinon.stub().returns(ctx.$q.when({
+          data: [
+          ],
+
+          errors: [
+            { 'target': 'whocares1' }
+          ]
+        }));
+
+        ctx.scope.get_data();
+        ctx.scope.$digest();
+
+        sinon.assert.callCount(ctx.scope.dataHandlerTotalFailure, 1);
+      });
+
+
 
     });
 
