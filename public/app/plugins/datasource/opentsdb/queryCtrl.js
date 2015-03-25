b01b121a4b573eff975b33bfe239044459c392db
Mike Kobyakov <mkobyakov@cyngn.com>
2015-03-25 14:32:26 -0700

    Instead of hard-coding the OpenTsdb aggregators list, pull the supported aggregators from the datasource directly.

--- public/app/plugins/datasource/opentsdb/queryCtrl.js
+++ public/app/plugins/datasource/opentsdb/queryCtrl.js
@@ -14,6 +14,12 @@ function (angular, _, kbn) {
       $scope.target.errors = validateTarget($scope.target);
       $scope.aggregators = ['avg', 'sum', 'min', 'max', 'dev', 'zimsum', 'mimmin', 'mimmax'];
 
+      $scope.datasource.performAggregatorsQuery().then(function(result) {
+        if (result) {
+          $scope.aggregators = result;
+        }
+      });
+
       if (!$scope.target.aggregator) {
         $scope.target.aggregator = 'sum';
       }
