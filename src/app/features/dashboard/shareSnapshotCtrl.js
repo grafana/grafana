f4280ca5171a87d16791094ff21a3987ffdff66a
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-03-25 20:36:48 +0100

    More refinements of dashboard snapshot dialog, #1623

--- src/app/features/dashboard/shareSnapshotCtrl.js
+++ src/app/features/dashboard/shareSnapshotCtrl.js
@@ -1,7 +1,8 @@
 define([
   'angular',
+  'lodash',
 ],
-function (angular) {
+function (angular, _) {
   'use strict';
 
   var module = angular.module('grafana.controllers');
@@ -9,8 +10,19 @@ function (angular) {
   module.controller('ShareSnapshotCtrl', function($scope, $rootScope, $location, backendSrv, $timeout, timeSrv) {
 
     $scope.snapshot = {
-      name: $scope.dashboard.title
+      name: $scope.dashboard.title,
+      expire: 0,
+      external: false,
     };
+
+    $scope.step = 1;
+
+    $scope.expireOptions = [
+      {text: '1 Hour', value: 60*60},
+      {text: '1 Day',  value: 60*60*24},
+      {text: '7 Days', value: 60*60*7},
+      {text: 'Never',  value: 0},
+    ];
 
     $scope.createSnapshot = function(external) {
       $scope.dashboard.snapshot = {
@@ -18,14 +30,16 @@ function (angular) {
       };
 
       $scope.loading = true;
+      $scope.snapshot.external = external;
+
       $rootScope.$broadcast('refresh');
 
       $timeout(function() {
-        $scope.saveSnapshot(external);
+        $scope.saveSnapshot();
       }, 3000);
     };
 
-    $scope.saveSnapshot = function(external) {
+    $scope.saveSnapshot = function() {
       var dash = angular.copy($scope.dashboard);
       // change title
       dash.title = $scope.snapshot.name;
@@ -36,6 +50,13 @@ function (angular) {
         panel.targets = [];
         panel.links = [];
       });
+      // remove annotations
+      dash.annotations.list = [];
+      // remove template queries
+      _.each(dash.templating.list, function(variable) {
+        variable.query = "";
+        variable.refresh = false;
+      });
 
       // cleanup snapshotData
       delete $scope.dashboard.snapshot;
@@ -43,15 +64,23 @@ function (angular) {
         delete panel.snapshotData;
       });
 
-      backendSrv.post('/api/snapshots', {dashboard: dash, external: external}).then(function(results) {
+      var cmdData = {
+        dashboard: dash,
+        external: $scope.snapshot.external,
+        expires: $scope.snapshot.expires,
+      };
+
+      backendSrv.post('/api/snapshots', cmdData).then(function(results) {
         $scope.loading = false;
 
-        if (external) {
+        if ($scope.snapshot.external) {
           $scope.snapshotUrl = results.url;
         } else {
           var baseUrl = $location.absUrl().replace($location.url(), "");
           $scope.snapshotUrl = baseUrl + '/dashboard/snapshot/' + results.key;
         }
+
+        $scope.step = 2;
       }, function() {
         $scope.loading = false;
       });
