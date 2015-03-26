1dd59e5854232fe9ce88c8182df687d8382f8ee9
David Raifaizen <david.raifaizen@lab49.com>
2015-03-26 14:03:47 -0400

    Added offline paging for table

--- src/app/panels/table/pagingControl.js
+++ src/app/panels/table/pagingControl.js
@@ -7,16 +7,17 @@ define([
 
     var module = angular.module('grafana.directives');
 
-    module.directive('grafanaTablePager', function($rootScope, timeSrv, $compile) {
+    module.directive('grafanaTablePager', function() {
       return {
         restrict: 'A',
         templateUrl: 'app/panels/table/pagingControl.html',
         link: function(scope, elem) {
           scope.arrowChange = function(change) {
-            scope.curPage += change;
+            // older browsers may not support number input type, so make sure we parse input as text
+            var numericPage = parseInt(scope.curTablePage) || 1;
+            scope.curTablePage = numericPage + change;
           };
         }
-      };
-    });
-
+      }
+   });
   });
