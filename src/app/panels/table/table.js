0ca38db5e3bc507007095cbee9bd35b8d25db409
David Raifaizen <david.raifaizen@lab49.com>
2015-03-26 16:37:05 -0400

    Added ng-repeat to directive to support support on headers

--- src/app/panels/table/table.js
+++ src/app/panels/table/table.js
@@ -8,17 +8,27 @@ define([
 
     var module = angular.module('grafana.directives');
 
-    module.directive('grafanaTable', function($rootScope, timeSrv, $compile) {
+    module.directive('grafanaTable', function($rootScope, $timeout) {
       var data;
       var tableHeight;
+      var headerHeight;
 
       // paging variables
       var dataToSkip;
       var pagedData;
       var numPages = 1;
+      var minPage = 1;
+
+
+      var sortingType = {
+        none: 0,
+        asc: 1,
+        desc: 2
+      };
 
       return {
         restrict: 'A',
+        templateUrl: 'app/panels/table/table.html',
         link: function(scope, elem) {
           scope.height = 280; // set default height for edit mode (prob should be done elsewhere)
           scope.tablePageSize = 20;
@@ -31,7 +41,9 @@ define([
               return;
             }
 
-            numPages = Math.floor(data.datapoints.length / scope.tablePageSize);
+            numPages = Math.ceil(data.datapoints.length / scope.tablePageSize);
+            minPage = numPages > 0 ? 1 : 0;
+
             scope.curTablePage = 1; // set to first page, since new data has come in
             render_panel();
           });
@@ -40,8 +52,8 @@ define([
           scope.$watch('curTablePage', function() {
             scope.curTablePage = parseInt(scope.curTablePage) || 1; // ensure page is numeric
 
-            if (scope.curTablePage < 1) {
-              scope.curTablePage = 1;
+            if (scope.curTablePage < minPage) {
+              scope.curTablePage = minPage;
             }
 
             if (scope.curTablePage > numPages) {
@@ -57,10 +69,6 @@ define([
           });
 
 
-          /**
-           * Rendering will be done without angular bindings in order to avoid performance penalty in the case
-           * that the user wants to view a large number of cells simultaneously.
-           */
           function render_panel() {
             var isHeightSet = setTableHeightVariable();
             if (shouldAbortRender(isHeightSet)) {
@@ -72,14 +80,11 @@ define([
             pagedData = data.datapoints.slice(dataToSkip, scope.tablePageSize + dataToSkip);
 
 
-            // construct table
-            var headers = _.map(data.selectedColumns, function(columnName) {
-              return '<th>' + columnName +  '</th>';
-            }).join('');
+            scope.columnNames = data.selectedColumns;
 
-            headers = '<tr>' + headers + '</tr>';
-
-            var tableData = _.reduce(pagedData, function(prev, cur) {
+            // avoid using angular bindings for table data in order to avoid performance penalty
+            // in case user wants to view a large number of cells simultaneously
+            scope.tableData = _.reduce(pagedData, function(prev, cur) {
               var row = _.map(cur, function(seriesValue) {
                 return '<td>' + seriesValue  + '</td>';
               }).join('');
@@ -90,54 +95,41 @@ define([
             }, '');
 
 
+            $timeout(function() { // after angular is processing, do jquery stuff
+              var thead$el = elem.find('thead');
 
-            var html =
-              '<div class="table-visualization">' +
-                '<div class="table-vis-overflow-container">' +
-                  '<table>' +
+              // we need to hardcode header widths so they do not get lost when the headers become fixed
+              // we base them off the below tds
+              var ths = thead$el.find('th');
+              var tds = elem.find('tbody tr:first-child td');
 
-                    '<thead>' +
-                      headers +
-                    '</thead>' +
-
-                    '<tbody>' +
-                      tableData +
-                    '</tbody>' +
-
-                  '</table>' +
-                '</div>' +
-              '</div>';
-
-
-
-            elem.html(html);
-            $compile(elem.contents())(scope);
-
-            var thead$el = elem.find('thead');
-
-            // we need to hardcode header widths so they do not get lost when the headers become fixed
-            var ths = thead$el.find('th');
-            for (var i = 0; i < ths.length; ++i) {
-              var el = ths.eq(i);
-              var width = el.width();
-              var borderWidth = parseInt(el.css('border-width')) || 0;
-              el.css('width', width + borderWidth);
-            }
-
-            var headerHeight = parseInt(thead$el.css('height')) || 0;
-
-            thead$el
-              .css('position', 'absolute') // fix table head in position
-              .css('top', -headerHeight + 'px'); // create distance from headers and body
+              if (tds.length > 0) {
+                for (var i = 0; i < ths.length; ++i) {
+                  var th = ths.eq(i);
+                  var td = tds.eq(i);
+                  var width = td.width();
+                  var borderWidth = parseFloat(td.css('border-width')) || 0;
+                  th.css('width', width + borderWidth);
+                }
+              }
 
 
-            // margin needed to push the headers above the table body. this has the effect of
-            // essentially increasing the directive's height by the header height amount
-            elem.find('.table-visualization').css('margin-top', headerHeight + 'px');
+              headerHeight = headerHeight || parseFloat(thead$el.css('height')) || 0;
 
-            var heightRemainingFromTotal = tableHeight - headerHeight;
-            elem.find('.table-vis-overflow-container').css('height', heightRemainingFromTotal + 'px');
-            elem.css('height', heightRemainingFromTotal + 'px'); // set physical height of directive
+              thead$el
+                .css('position', 'absolute') // fix table head in position
+                .css('top', -headerHeight + 'px'); // create distance from headers and body
+
+
+              // margin needed to push the headers above the table body. this has the effect of
+              // essentially increasing the directive's height by the header height amount
+              elem.find('.table-visualization').css('margin-top', headerHeight + 'px');
+
+              var heightRemainingFromTotal = tableHeight - headerHeight;
+              elem.find('.table-vis-overflow-container').css('height', heightRemainingFromTotal + 'px');
+              elem.css('height', heightRemainingFromTotal + 'px'); // set physical height of directive
+            }, 0);
+
           }
 
 
@@ -173,7 +165,6 @@ define([
               return false;
             }
           }
-
 
         }
       };
