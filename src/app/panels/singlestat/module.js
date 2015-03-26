f343a46ca48ec42df140ce7b80a76b6b174ecab7
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-03-26 12:05:08 +0100

    Singlestat panel: Fixed error handling, when query fail do not show last value, Fixes #1647

--- src/app/panels/singlestat/module.js
+++ src/app/panels/singlestat/module.js
@@ -81,9 +81,10 @@ function (angular, app, _, TimeSeries, kbn, PanelMeta) {
       panelHelper.updateTimeRange($scope);
 
       return panelHelper.issueMetricQuery($scope, datasource)
-        .then($scope.dataHandler)
-        .then(null, function() {
+        .then($scope.dataHandler, function(err) {
+          $scope.series = [];
           $scope.render();
+          throw err;
         });
     };
 
