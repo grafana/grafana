35d699fea124805345d1c27da538a31d8342ac4c
David Raifaizen <david.raifaizen@lab49.com>
2015-03-27 16:02:07 -0400

    added sort method htat doesn't completely work

--- src/app/panels/table/table.js
+++ src/app/panels/table/table.js
@@ -11,7 +11,7 @@ define([
     module.directive('grafanaTable', function($rootScope, $timeout) {
       var data;
       var tableHeight;
-      var headerHeight;
+      var sortedData;
 
       // paging variables
       var dataToSkip;
@@ -33,6 +33,10 @@ define([
           scope.height = 280; // set default height for edit mode (prob should be done elsewhere)
           scope.tablePageSize = 20;
           scope.sortType = SortType;
+
+          // refers to the order in which the columns were requested to be sorted
+          // for example, we might want to first sort by second column, then by third, then by first, etc.
+          scope.columnSortOrder = [];
 
           scope.$on('render',function(event, renderData) {
             data = renderData || data;
@@ -70,30 +74,8 @@ define([
 
           scope.headerClicked = function(header) {
             changeSortType(header);
-            // sort
+            render_panel();
           };
-
-
-
-          function changeSortType(header) {
-            var newType = null;
-
-            switch (header.sortType) {
-              case SortType.none:
-                newType = SortType.asc;
-                break;
-
-              case SortType.asc:
-                newType = SortType.desc;
-                break;
-
-              case SortType.desc:
-                newType = SortType.none;
-                break;
-            }
-
-            header.sortType = newType;
-          }
 
 
           function render_panel() {
@@ -102,15 +84,58 @@ define([
               return;
             }
 
-            // handle paging
-            dataToSkip = scope.tablePageSize * (scope.curTablePage - 1);
-            pagedData = data.datapoints.slice(dataToSkip, scope.tablePageSize + dataToSkip);
+            setHeaders();
+            handleSorting();
+            handlePaging();
+            setTableData();
+
+            elem.find('.table-vis-overflow-container').height(tableHeight);
+            elem.height(tableHeight); // set physical height of directive
+
+            $timeout(function() { // after angular is processing, do jquery stuff
+              performHeaderPositioning();
+            }, 0);
+          }
+
+          // on resize, the absolutely positioned headers will be shifted, during shift we reposition them
+          $(window).resize(function() {
+            performHeaderPositioning();
+          });
 
 
-            scope.headers = _.map(data.selectedColumns, function(columnName) {
+          function setHeaders() {
+            // only set headers if there has been a change, since we do not want to lose sorting options
+
+            var curHeaders = scope.headers;
+
+            var newHeaders = _.map(data.selectedColumns, function(columnName) {
               return { columnName: columnName, sortType: SortType.none };
             });
 
+            if (!curHeaders) {
+              scope.headers = newHeaders;
+              scope.columnSortOrder = [];
+              return;
+            }
+
+
+            var headersChanged = curHeaders.length !== newHeaders.length;
+            if (!headersChanged) { // check further to see if they did change
+              var curNames = _.pluck(curHeaders, 'columnName');
+              var newNames = _.pluck(curHeaders, 'columnName');
+
+              headersChanged = _.difference(curNames, newNames).length > 0;
+            }
+
+
+            if (headersChanged)
+            {
+              scope.headers = newHeaders;
+              scope.columnSortOrder = [];
+            }
+          }
+
+          function setTableData() {
             // avoid using angular bindings for table data in order to avoid performance penalty
             // in case user wants to view a large number of cells simultaneously
             scope.tableData = _.reduce(pagedData, function(prev, cur) {
@@ -122,20 +147,7 @@ define([
 
               return prev += row;
             }, '');
-
-            elem.find('.table-vis-overflow-container').height(tableHeight);
-            elem.height(tableHeight); // set physical height of directive
-
-            $timeout(function() { // after angular is processing, do jquery stuff
-              performHeaderPositioning();
-            }, 0);
           }
-
-          // on resize, the absolutely positioned headers will be shifted, during shift we should hide them
-          $(window).resize(function() {
-            performHeaderPositioning();
-          });
-
 
           function performHeaderPositioning() {
             var realHeaders = elem.find('.real-table-header');
@@ -154,6 +166,69 @@ define([
 
             fixedHeaders.show();
           }
+
+          function handlePaging() {
+            dataToSkip = scope.tablePageSize * (scope.curTablePage - 1);
+            pagedData = sortedData.slice(dataToSkip, scope.tablePageSize + dataToSkip);
+          }
+
+          function handleSorting() {
+            sortedData = [].concat(data.datapoints);
+            sortedData = sortedData.slice(0, 5);
+
+            var columnToSort = 0;
+            var ascSort = true; // if true, will sort on asc, if not, by desc
+
+            for (var i = 0; i < scope.columnSortOrder.length; ++i) {
+              var header = scope.columnSortOrder[i];
+              columnToSort = _.findIndex(scope.headers, header);
+              if (columnToSort === -1) continue;
+
+              if (header.sortType === SortType.asc) {
+                ascSort = true;
+              }
+              else if (header.sortType === SortType.desc) {
+                ascSort = false;
+              }
+
+              sortedData.sort(sortFunction);
+            }
+
+
+            function sortFunction(a, b) {
+              if (a[columnToSort] === b[columnToSort]) {
+                return 0;
+              }
+              else {
+                var isConditionMet = ascSort ? a[columnToSort] < b[columnToSort] : a[columnToSort] > b[columnToSort];
+                return isConditionMet ? -1 : 1;
+              }
+            }
+          }
+
+          function changeSortType(header) {
+            var newType = null;
+
+            switch (header.sortType) {
+              case SortType.none:
+                newType = SortType.asc;
+                scope.columnSortOrder.push(header); // we are beginning to sort by header column, so add to array
+                break;
+
+              case SortType.asc:
+                newType = SortType.desc;
+                break;
+
+              case SortType.desc:
+                newType = SortType.none;
+                // since we are no longer sorting, remove from sort order array
+                scope.columnSortOrder = _.filter(scope.columnSortOrder, function(sortedHeader) { return header !== sortedHeader;  } );
+                break;
+            }
+
+            header.sortType = newType;
+          }
+
 
 
           function shouldAbortRender(isHeightSet) {
