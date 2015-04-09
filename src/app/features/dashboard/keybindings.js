60e2168149d897e5437fe661ea9ec555eb2a839a
Matt Toback <matt@raintank.io>
2015-04-09 18:39:33 -0400

    Created new endpoint modal styles and html pages

--- src/app/features/dashboard/keybindings.js
+++ src/app/features/dashboard/keybindings.js
@@ -21,7 +21,7 @@ function(angular, $) {
 
         helpModalScope = $rootScope.$new();
         var helpModal = $modal({
-          template: './app/partials/help_modal.html',
+          template: './plugins/raintank/features/partials/endpoint_modal_new.html',
           persist: false,
           show: false,
           scope: helpModalScope,
