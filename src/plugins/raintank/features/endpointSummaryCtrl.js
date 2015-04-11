4eb82ac4546927bbb1f5ca8c9660bc4105490f61
Anthony Woods <awoods@raintank.io>
2015-04-11 10:55:27 +0800

    wire up check status on endpoint summary page

--- src/plugins/raintank/features/endpointSummaryCtrl.js
+++ src/plugins/raintank/features/endpointSummaryCtrl.js
@@ -11,7 +11,10 @@ function (angular, _) {
     $scope.init = function() {
       $scope.endpoints = [];
       $scope.monitors = {};
+      $scope.monitor_types = {};
+      $scope.monitor_types_by_name = {};
       $scope.endpoint = null;
+      $scope.getMonitorTypes();
       var promise = $scope.getEndpoints();
       promise.then(function() {
         $scope.getEndpoint($routeParams.id);
@@ -29,6 +32,17 @@ function (angular, _) {
     $scope.tagsUpdated = function(newVal) {
       backendSrv.post("/api/endpoints", $scope.endpoint);
     }
+
+    $scope.getMonitorTypes = function() {
+      backendSrv.get('/api/monitor_types').then(function(types) {
+        _.forEach(types, function(type) {
+          $scope.monitor_types[type.id] = type;
+          $scope.monitor_types_by_name[type.name] = type;
+        });
+        console.log("monitor_types ready");
+      });
+    };
+
     $scope.getEndpoint = function(id) {
       _.forEach($scope.endpoints, function(endpoint) {
         if (endpoint.id == id) {
@@ -42,6 +56,49 @@ function (angular, _) {
         }
       });
     };
+
+    $scope.getMonitorByTypeName = function(name) {
+      if (name in $scope.monitor_types_by_name) {
+        var type = $scope.monitor_types_by_name[name];
+        return $scope.monitors[type.id];
+      }
+      return undefined;
+    }
+
+    $scope.monitorStateTxt = function(mon) {
+      if (typeof(mon) != "object") {
+        return "nodata";
+      }
+      if (mon.state < 0 || mon.state > 2) {
+        return 'nodata';
+      }
+      var states = ["online", "warn", "critical"];
+      return states[mon.state];
+    }
+
+    $scope.stateChangeStr = function(mon) {
+      if (typeof(mon) != "object") {
+        return "";
+      }
+      var duration = new Date().getTime() - new Date(mon.state_change).getTime();
+      if (duration < 10000) {
+        return "a few seconds ago";
+      }
+      if (duration < 60000) {
+        var secs = Math.floor(duration/1000);
+        return secs + " seconds ago";
+      }
+      if (duration < 3600000) {
+        var mins = Math.floor(duration/1000/60);
+        return mins + " minutes ago";
+      }
+      if (duration < 86400000) {
+        var hours = Math.floor(duration/1000/60/60);
+        return hours + " hours ago";
+      }
+      var days = Math.floor(duration/1000/60/60/24);
+      return days + " days ago";
+    }
 
     $scope.setEndpoint = function(id) {
       $location.path('/endpoints/summary/'+id);
