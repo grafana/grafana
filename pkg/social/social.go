4fd2622e55d7dd859e4c278e75be26886c65e9c0
William Wei <william.wei@demonware.net>
2015-04-15 14:18:10 +0800

    grafana/grafana#1781 add oauth api url as config
    read github api from config file instead of using public github:
    http://api.github.com/user
    
    this would be useful when you are using github enterprise edition

--- pkg/social/social.go
+++ pkg/social/social.go
@@ -73,7 +73,7 @@ func NewOAuthService() {
 		// GitHub.
 		if name == "github" {
 			setting.OAuthService.GitHub = true
-			SocialMap["github"] = &SocialGithub{Config: &config, allowedDomains: info.AllowedDomains}
+			SocialMap["github"] = &SocialGithub{Config: &config, allowedDomains: info.AllowedDomains, APIUrl: info.APIUrl}
 		}
 
 		// Google.
@@ -101,6 +101,7 @@ func isEmailAllowed(email string, allowedDomains []string) bool {
 type SocialGithub struct {
 	*oauth2.Config
 	allowedDomains []string
+	APIUrl []string
 }
 
 func (s *SocialGithub) Type() int {
@@ -120,7 +121,7 @@ func (s *SocialGithub) UserInfo(token *oauth2.Token) (*BasicUserInfo, error) {
 
 	var err error
 	client := s.Client(oauth2.NoContext, token)
-	r, err := client.Get("https://api.github.com/user")
+	r, err := client.Get(s.APIUrl)
 	if err != nil {
 		return nil, err
 	}
