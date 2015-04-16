2aa103526f8a1871f53dabb152cbacba742f2e60
Marco Vito Moscaritolo <mavimo@gmail.com>
2015-04-17 01:15:46 +0200

    Update dependencies.

---
+++ Godeps/_workspace/src/github.com/macaron-contrib/bindata/README.md
@@ -0,0 +1,17 @@
+bindata [![Build Status](https://drone.io/github.com/macaron-contrib/bindata/status.png)](https://drone.io/github.com/macaron-contrib/bindata/latest)
+=======
+
+Package bindata is a helper module that allows to use in-memory static and template files for Macaron via [go-bindata](https://github.com/jteeuwen/go-bindata).
+
+### Installation
+
+	go get github.com/macaron-contrib/bindata
+
+## Getting Help
+
+- [API Reference](https://gowalker.org/github.com/macaron-contrib/bindata)
+- [Documentation](http://macaron.gogs.io/docs/middlewares/bindata)
+
+## License
+
+This project is under Apache v2 License. See the [LICENSE](LICENSE) file for the full license text.
---
+++ Godeps/_workspace/src/github.com/macaron-contrib/bindata/testdata/bindata.go
@@ -0,0 +1,244 @@
+package testdata
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"strings"
+	"os"
+	"time"
+	"io/ioutil"
+	"path"
+	"path/filepath"
+)
+
+func bindata_read(data []byte, name string) ([]byte, error) {
+	gz, err := gzip.NewReader(bytes.NewBuffer(data))
+	if err != nil {
+		return nil, fmt.Errorf("Read %q: %v", name, err)
+	}
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, gz)
+	gz.Close()
+
+	if err != nil {
+		return nil, fmt.Errorf("Read %q: %v", name, err)
+	}
+
+	return buf.Bytes(), nil
+}
+
+type asset struct {
+	bytes []byte
+	info  os.FileInfo
+}
+
+type bindata_file_info struct {
+	name string
+	size int64
+	mode os.FileMode
+	modTime time.Time
+}
+
+func (fi bindata_file_info) Name() string {
+	return fi.name
+}
+func (fi bindata_file_info) Size() int64 {
+	return fi.size
+}
+func (fi bindata_file_info) Mode() os.FileMode {
+	return fi.mode
+}
+func (fi bindata_file_info) ModTime() time.Time {
+	return fi.modTime
+}
+func (fi bindata_file_info) IsDir() bool {
+	return false
+}
+func (fi bindata_file_info) Sys() interface{} {
+	return nil
+}
+
+var _static_hello_css = []byte("\x1f\x8b\x08\x00\x00\x09\x6e\x88\x00\xff\xd2\xcb\x48\xcd\xc9\xc9\xd7\x2b\xcf\x2f\xca\x49\x51\xa8\xe6\xe2\x4c\xcb\xcf\x2b\xd1\x2d\xce\xac\x4a\xb5\x52\x30\x2c\xa8\xb0\xe6\xaa\x05\x04\x00\x00\xff\xff\x7f\xd8\xb3\x03\x21\x00\x00\x00")
+
+func static_hello_css_bytes() ([]byte, error) {
+	return bindata_read(
+		_static_hello_css,
+		"static/hello.css",
+	)
+}
+
+func static_hello_css() (*asset, error) {
+	bytes, err := static_hello_css_bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindata_file_info{name: "static/hello.css", size: 33, mode: os.FileMode(436), modTime: time.Unix(1424499681, 0)}
+	a := &asset{bytes: bytes, info:  info}
+	return a, nil
+}
+
+var _templates_hello_tmpl = []byte("\x1f\x8b\x08\x00\x00\x09\x6e\x88\x00\xff\xf2\x48\xcd\xc9\xc9\xd7\x51\xa8\xae\xd6\xab\xad\x55\x04\x04\x00\x00\xff\xff\x9a\xe7\x7b\x0e\x0d\x00\x00\x00")
+
+func templates_hello_tmpl_bytes() ([]byte, error) {
+	return bindata_read(
+		_templates_hello_tmpl,
+		"templates/hello.tmpl",
+	)
+}
+
+func templates_hello_tmpl() (*asset, error) {
+	bytes, err := templates_hello_tmpl_bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindata_file_info{name: "templates/hello.tmpl", size: 13, mode: os.FileMode(436), modTime: time.Unix(1424500783, 0)}
+	a := &asset{bytes: bytes, info:  info}
+	return a, nil
+}
+
+// Asset loads and returns the asset for the given name.
+// It returns an error if the asset could not be found or
+// could not be loaded.
+func Asset(name string) ([]byte, error) {
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[cannonicalName]; ok {
+		a, err := f()
+		if err != nil {
+			return nil, fmt.Errorf("Asset %s can't read by error: %v", name, err)
+		}
+		return a.bytes, nil
+	}
+	return nil, fmt.Errorf("Asset %s not found", name)
+}
+
+// AssetInfo loads and returns the asset info for the given name.
+// It returns an error if the asset could not be found or
+// could not be loaded.
+func AssetInfo(name string) (os.FileInfo, error) {
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[cannonicalName]; ok {
+		a, err := f()
+		if err != nil {
+			return nil, fmt.Errorf("AssetInfo %s can't read by error: %v", name, err)
+		}
+		return a.info, nil
+	}
+	return nil, fmt.Errorf("AssetInfo %s not found", name)
+}
+
+// AssetNames returns the names of the assets.
+func AssetNames() []string {
+	names := make([]string, 0, len(_bindata))
+	for name := range _bindata {
+		names = append(names, name)
+	}
+	return names
+}
+
+// _bindata is a table, holding each asset generator, mapped to its name.
+var _bindata = map[string]func() (*asset, error){
+	"static/hello.css": static_hello_css,
+	"templates/hello.tmpl": templates_hello_tmpl,
+}
+
+// AssetDir returns the file names below a certain
+// directory embedded in the file by go-bindata.
+// For example if you run go-bindata on data/... and data contains the
+// following hierarchy:
+//     data/
+//       foo.txt
+//       img/
+//         a.png
+//         b.png
+// then AssetDir("data") would return []string{"foo.txt", "img"}
+// AssetDir("data/img") would return []string{"a.png", "b.png"}
+// AssetDir("foo.txt") and AssetDir("notexist") would return an error
+// AssetDir("") will return []string{"data"}.
+func AssetDir(name string) ([]string, error) {
+	node := _bintree
+	if len(name) != 0 {
+		cannonicalName := strings.Replace(name, "\\", "/", -1)
+		pathList := strings.Split(cannonicalName, "/")
+		for _, p := range pathList {
+			node = node.Children[p]
+			if node == nil {
+				return nil, fmt.Errorf("Asset %s not found", name)
+			}
+		}
+	}
+	if node.Func != nil {
+		return nil, fmt.Errorf("Asset %s not found", name)
+	}
+	rv := make([]string, 0, len(node.Children))
+	for name := range node.Children {
+		rv = append(rv, name)
+	}
+	return rv, nil
+}
+
+type _bintree_t struct {
+	Func func() (*asset, error)
+	Children map[string]*_bintree_t
+}
+var _bintree = &_bintree_t{nil, map[string]*_bintree_t{
+	"static": &_bintree_t{nil, map[string]*_bintree_t{
+		"hello.css": &_bintree_t{static_hello_css, map[string]*_bintree_t{
+		}},
+	}},
+	"templates": &_bintree_t{nil, map[string]*_bintree_t{
+		"hello.tmpl": &_bintree_t{templates_hello_tmpl, map[string]*_bintree_t{
+		}},
+	}},
+}}
+
+// Restore an asset under the given directory
+func RestoreAsset(dir, name string) error {
+        data, err := Asset(name)
+        if err != nil {
+                return err
+        }
+        info, err := AssetInfo(name)
+        if err != nil {
+                return err
+        }
+        err = os.MkdirAll(_filePath(dir, path.Dir(name)), os.FileMode(0755))
+        if err != nil {
+                return err
+        }
+        err = ioutil.WriteFile(_filePath(dir, name), data, info.Mode())
+        if err != nil {
+                return err
+        }
+        err = os.Chtimes(_filePath(dir, name), info.ModTime(), info.ModTime())
+        if err != nil {
+                return err
+        }
+        return nil
+}
+
+// Restore assets under the given directory recursively
+func RestoreAssets(dir, name string) error {
+        children, err := AssetDir(name)
+        if err != nil { // File
+                return RestoreAsset(dir, name)
+        } else { // Dir
+                for _, child := range children {
+                        err = RestoreAssets(dir, path.Join(name, child))
+                        if err != nil {
+                                return err
+                        }
+                }
+        }
+        return nil
+}
+
+func _filePath(dir, name string) string {
+        cannonicalName := strings.Replace(name, "\\", "/", -1)
+        return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
+}
+
