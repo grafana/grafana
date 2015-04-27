c57c9f942d35aa895c1b38f56f5958aa995df957
David Raifaizen <david.raifaizen@lab49.com>
2015-04-27 13:44:27 -0400

    Removed intellij comments

--- public/app/panels/table/module.js
+++ public/app/panels/table/module.js
@@ -1,7 +1,1 @@
-/**
- * Created with IntelliJ IDEA.
- * User: david.raifaizen
- * Date: 4/27/15
- * Time: 1:24 PM
- * To change this template use File | Settings | File Templates.
- */
+// blank module file needed for requirejs build task
