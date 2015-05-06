1f48c0739556bc0b443d28c63a09d22c044d54ea
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-05-06 10:57:32 +0200

    more work on dash links

--- public/app/features/dashlinks/module.js
+++ public/app/features/dashlinks/module.js
@@ -18,7 +18,7 @@ function (angular, _) {
         link: function() {
         }
       };
-    }).directive('dashLink', function(linkSrv) {
+    }).directive('dashLink', function(linkSrv, $rootScope) {
       return {
         scope: {
           link: "="
@@ -27,34 +27,61 @@ function (angular, _) {
         controller: 'DashLinkCtrl',
         templateUrl: 'app/features/dashlinks/module.html',
         link: function(scope, elem) {
+          var linkInfo;
+          var anchor = elem.find('a');
+          var icon = elem.find('i');
+          var span = elem.find('span');
 
           function update() {
-            var linkInfo = linkSrv.getPanelLinkAnchorInfo(scope.link);
-            elem.find("span").text(linkInfo.title);
-            elem.find("a").attr("href", linkInfo.href);
+            linkInfo = linkSrv.getPanelLinkAnchorInfo(scope.link);
+            span.text(linkInfo.title);
+            anchor.attr("href", linkInfo.href);
 
-            if (scope.link.type === 'dashboard') {
-              scope.tooltip = 'Go to dashboard';
+            if (scope.link.type === 'absolute') {
+              icon.attr('class', 'fa fw fa-external-link');
+              anchor.attr('target', '_blank');
+            } else {
+              icon.attr('class', 'fa fw fa-th-large');
+              anchor.attr('target', '');
             }
           }
 
+          // tooltip
+          elem.find('a').tooltip({
+            title: function () {
+              if (scope.link.tooltip) {
+                return scope.link.tooltip;
+              }
+              else if (scope.link.type === 'dashboard') {
+                return 'Open dashboard';
+              } else if (scope.link.type === 'absolute') {
+                return 'Open external page';
+              }
+            },
+            html: true,
+            container: 'body', // Grafana change
+          });
+
           update();
           scope.$on('refresh', update);
+          $rootScope.onAppEvent('dash-links-updated', update);
         }
       };
     })
-    .controller("DashLinkCtrl", function($scope) {
-
+    .controller("DashLinkCtrl", function() {
     })
-    .controller('DashLinkEditorCtrl', function($scope, backendSrv) {
+    .controller('DashLinkEditorCtrl', function($scope, backendSrv, $rootScope) {
 
       $scope.dashboard.links = $scope.dashboard.links || [];
-
       $scope.addLink = function() {
         $scope.dashboard.links.push({
           type: 'dashboard',
           name: 'Related dashboard'
         });
+      };
+
+      $scope.updated = function() {
+        $rootScope.appEvent('dash-links-updated');
       };
 
       $scope.searchDashboards = function(queryStr, callback) {
