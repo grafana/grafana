ff3843bc7fe894178c21953447dae6a16985bf20
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-06-01 17:01:04 +0200

    Roles: New user role  that replaces the old  role behavior, Closes #2088

--- pkg/api/dashboard.go
+++ pkg/api/dashboard.go
@@ -55,7 +55,7 @@ func GetDashboard(c *middleware.Context) {
 			Type:      m.DashTypeDB,
 			CanStar:   c.IsSignedIn,
 			CanSave:   c.OrgRole == m.ROLE_ADMIN || c.OrgRole == m.ROLE_EDITOR,
-			CanEdit:   c.OrgRole == m.ROLE_ADMIN || c.OrgRole == m.ROLE_EDITOR,
+			CanEdit:   c.OrgRole == m.ROLE_ADMIN || c.OrgRole == m.ROLE_EDITOR || c.OrgRole == m.ROLE_READ_ONLY_EDITOR,
 		},
 	}
 
