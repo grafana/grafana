4a11196f8b86f1c1b3a0c17d875d4bf00a326804
woodsaj <anthony@monkey.id.au>
2015-06-02 21:58:57 +0800

    allow elasticsearch port to be specified in the configfile

--- pkg/services/elasticstore/elasticstore.go
+++ pkg/services/elasticstore/elasticstore.go
@@ -1,6 +1,7 @@
 package elasticstore
 
 import (
+	"strconv"
 	"github.com/grafana/grafana/pkg/setting"
 	elastigo "github.com/mattbaird/elastigo/lib"
 )
@@ -11,6 +12,8 @@ var (
 
 func Init() {
 	host := setting.Cfg.Section("elasticsearch").Key("host").MustString("localhost")
+	port := setting.Cfg.Section("elasticsearch").Key("port").MustInt64(9200)
 	es = elastigo.NewConn()
 	es.Domain = host
+	es.Port = strconv.FormatInt(port, 10)
 }
