45fab06f5bc8078b41738e733569cb223f49fe96
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-06-04 17:23:46 +0200

    More work on email and notification infra #1456

---
+++ pkg/notifications/notifications.go
@@ -0,0 +1,11 @@
+package notifications
+
+import "github.com/grafana/grafana/pkg/bus"
+
+func Init() {
+	bus.AddHandler("email", sendResetPasswordEmail)
+}
+
+func sendResetPasswordEmail(cmd *SendResetPasswordEmailCommand) error {
+	email := NewMailMessage("")
+}
