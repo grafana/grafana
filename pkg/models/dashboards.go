f430d533e54ecc0b54312111e2f4be08c7daa775
Dieter Plaetinck <dieter@raintank.io>
2015-07-07 12:51:38 -0700

    clearer errors
    "Not found" should only be for http path/method not found (404)
    if it's about specific resources, we should be explicit for clarity

--- pkg/models/dashboards.go
+++ pkg/models/dashboards.go
@@ -11,6 +11,7 @@ import (
 // Typed errors
 var (
 	ErrDashboardNotFound           = errors.New("Dashboard not found")
+	ErrDashboardSnapshotNotFound   = errors.New("Dashboard snapshot not found")
 	ErrDashboardWithSameNameExists = errors.New("A dashboard with the same name already exists")
 	ErrDashboardVersionMismatch    = errors.New("The dashboard has been changed by someone else")
 )
