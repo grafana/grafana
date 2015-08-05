91e5fcf8f13eed3577f068376ac228e0df4f0974
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-08-05 14:18:47 +0200

    fix(templating): fixed issue with repeat row, was pretty buggy due to very recent change, fixes #2443

--- public/app/features/dashboard/dynamicDashboardSrv.js
+++ public/app/features/dashboard/dynamicDashboardSrv.js
@@ -53,10 +53,6 @@ function (angular, _) {
             row.panels = _.without(row.panels, panel);
             j = j - 1;
           }
-          // clean up left over scoped vars
-          else if (panel.scopedVars && panel.repeatIteration !== this.iteration) {
-            delete panel.scopedVars;
-          }
         }
       }
     };
@@ -120,7 +116,6 @@ function (angular, _) {
           panel = copy.panels[i];
           panel.scopedVars = {};
           panel.scopedVars[variable.name] = option;
-          panel.repeatIteration = self.iteration;
         }
       });
     };
