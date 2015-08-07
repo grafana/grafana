63cd3fa1d02047bce8fd0abee69c76bf82a92e44
Don Hsieh <dontwn@gmail.com>
2015-08-07 16:17:06 +0800

    [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template

--- open-falcon/server.js
+++ open-falcon/server.js
@@ -165,7 +165,7 @@ function queryMetric(req, res, targets)
  * @related issues:	OWL-123, OWL-063, OWL-032, OWL-029, OWL-017
 =======
  * @description:	This route returns list of hosts (endpoints)
- *					 if query[0] == '*'; returns list of metrics (counters) 
+ *					 if query[0] == '*'; returns list of metrics (counters)
  *					 otherwise.
  * @related issues:	OWL-029, OWL-017
 >>>>>>> OWL-29 autocomplete for hostname request
@@ -222,7 +222,11 @@ app.get('/', function(req, res) {
 				});
 				res.send(results);
 			} else {
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
 				res.send(results);
+=======
+				res.send([]);
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
 			}
 		});
 	} else {	// Query metrics (counters) of a specific host.
@@ -230,6 +234,7 @@ app.get('/', function(req, res) {
 		if (query[query.length-1].indexOf('{') > -1) {
 			query.pop();
 		} else {}
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
 		host = query.shift();
 		if (host === 'chart') {
 			results.push({text: 'bar', expandable: false});
@@ -247,9 +252,34 @@ app.get('/', function(req, res) {
 				if (arr[i].indexOf('select') > -1) {
 					arr.splice(i, 1);
 				} else {}
+=======
+		var host = query.shift();
+		var depth = query.length;
+		host = '["' + host + '"]';
+		var arr = [];
+		// metric_q is the metric that sent to Open-Falcon for query
+		var metric_q = query.join('.').replace('.*', '').replace('*', '');
+		arr = metric_q.split('.');
+		for (var i in arr) {
+			if (arr[i].indexOf('select') > -1) {
+				arr.splice(i, 1);
+			} else {}
+		}
+		metric_q = arr.join('.');
+
+		var options = {
+			uri: queryUrl + '/api/counters',
+			method: 'POST',
+			form: {
+				"endpoints": host,
+				"q": metric_q,	// both '' and null are ok
+				"limit": '',
+				"_r": Math.random()
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
 			}
 			metric_q = arr.join('.');
 
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
 			var options = {
 				uri: urlDashboard + '/api/counters',
 				method: 'POST',
@@ -307,6 +337,55 @@ app.get('/', function(req, res) {
 				}
 			});
 		}
+=======
+		request(options, function (error, response, body) {
+			if (!error && response.statusCode === 200) {
+				body = JSON.parse(body);
+				var metric = '';
+				var metrics = [];
+				var metricRoot = '';
+				var obj = {};
+				var results = [];
+				if (!metric_q) {	// First metric query of a specific host
+					var metricRoots = [];
+					for (var i in body.data) {
+						arr = body.data[i];
+						metric = arr[0];
+						metricRoot = metric.split('.')[0];
+						if (metricRoots.indexOf(metricRoot) < 0) {
+							metricRoots.push(metricRoot);
+							obj = {};
+							obj.text = metricRoot;
+							if (metric.indexOf('.') > -1) {
+								obj.expandable = true;
+							} else {
+								obj.expandable = false;
+							}
+							results.push(obj);
+						}
+					}
+				} else {	// Query next level of metric
+					for (var j in body.data) {
+						arr = body.data[j];
+						metric = arr[0];
+						metricRoot = metric.split('.')[depth];
+						if (metric.indexOf(metric_q + '.') === 0 && metrics.indexOf(metricRoot) < 0) {
+							metrics.push(metricRoot);
+							obj = {};
+							obj.text = metricRoot;
+							if (metricRoot === metric.split('.').pop()) {
+								obj.expandable = false;
+							} else {
+								obj.expandable = true;
+							}
+							results.push(obj);
+						}
+					}
+				}
+				res.send(results);
+			}
+		});
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
 	}
 });
 
@@ -314,18 +393,27 @@ app.get('/', function(req, res) {
  * @function:		app.post('/', function(req, res))
  * @description:	This route returns list of datapoints [timestamp, value]
  *					 for Grafana to draw diagram.
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
  * @related issues:	OWL-062, OWL-063, OWL-034, OWL-017
+=======
+ * @related issues:	OWL-034, OWL-017
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
  * @param:			object req
  * @param:			object res
  * @return:			array results
  * @author:			Don Hsieh
  * @since:			07/25/2015
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
  * @last modified: 	08/28/2015
+=======
+ * @last modified: 	08/07/2015
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
  * @called by:		POST http://localhost:4001
  *					func ProxyDataSourceRequest(c *middleware.Context)
  *					 in pkg/api/dataproxy.go
  */
 app.post('/', function (req, res) {
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
 	if ('target' in req.body) {
 		var targets = req.body.target;
 		if (targets.indexOf('chart') === 0) {
@@ -342,6 +430,67 @@ app.post('/', function (req, res) {
 				res.send(results);
 			} else {
 				res.send([]);
+=======
+	var queryUrl = req.query['target'].split('//')[1].split(':')[0] + ':9966/graph/history';
+	queryUrl = req.query['target'].split('//')[0] + '//' + queryUrl;
+	console.log('queryUrl =', queryUrl);
+
+	if ('target' in req.body) {
+		var now = Math.floor(new Date() / 1000);
+		var from = req.body.from;
+		var unit = 0;
+		if (from.indexOf('m') > 0) {
+			unit = 60;
+		}
+		if (from.indexOf('h') > 0) {
+			unit = 3600;
+		}
+		if (from.indexOf('d') > 0) {
+			unit = 86400;
+		}
+		if (from.indexOf('h') > 0) {
+			from = parseInt(from) * unit;
+		}
+		var metrics = [];
+		var target = '';
+		var targets = req.body.target;
+		if (typeof(targets) === typeof('')) {
+			targets = [targets];
+		}
+
+		/*
+		 *	MODIFIED FOR TEMPLATING
+		 */
+		console.log('targets =', targets);
+		var i = 0;
+		while (i < targets.length) {	// targets.length changes dynamically
+			target = targets[i];
+			i = i + 1;
+			if (target.indexOf('.select') < 0) {
+				var query = target.split('.');
+				if (query[0].indexOf('{') > -1) {		// check if hostname is requested by templating
+														// Could change to looping through query to see if there is any segment using templating.
+					var template_split = query[0].replace('{', '').replace('}', '').split(',');		// split the elements we get from templating
+					query.shift();		// remove the templating segment
+					var toAppend = query.join('.');		// the rest of the query used to append later
+					for(var idx in template_split){		// formatting a new target
+						var tmp = [];					// formatting a new target
+						tmp.push(template_split[idx]);
+						tmp.push(toAppend);				// append
+						var tmp_join = tmp.join('.');
+						targets.push(tmp_join);			// push to targets[]
+					}
+					continue;	// goto next target
+				} else {}
+				var host = query.shift();
+				var metric = query.join('.');
+				if (host && metric) {
+					metrics.push({
+						"Endpoint": host,
+						"Counter": metric
+					});
+				} else {}
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
 			}
 		} else {
 			if (typeof(targets) === typeof('')) {
@@ -349,6 +498,35 @@ app.post('/', function (req, res) {
 			}
 			queryMetric(req, res, targets);
 		}
+<<<<<<< 124f395b6c30f3d71bbe95537f01a0b612dbc93d
+=======
+
+		if (metrics.length) {
+			var options = {
+				uri: queryUrl,
+				method: 'POST',
+				json: {
+					"endpoint_counters": metrics,
+					"cf": "AVERAGE",
+					"start": now + from,
+					"end": now
+				}
+			};
+			request(options, function (error, response, body) {
+				if (!error && response.statusCode === 200) {
+					var results = [];
+					for (var i in body) {
+						if (body[i].Values) {	// if body[i] has Values
+							results.push(body[i]);
+						}
+					}
+					res.send(results);
+				}
+			});
+		} else {
+			res.send([]);
+		}
+>>>>>>> [OWL-34] Set vm.variable.multi = true; to enable hostname selection in Grafana template
 	} else {
 		res.send([]);
 	}
