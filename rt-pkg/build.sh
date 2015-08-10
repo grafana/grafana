6dba0cecc717bb9543d2a2c0107e31445360e235
James W. Brinkerhoff <jwb@paravolve.net>
2015-08-10 18:32:48 -0400

    determine checkout directory more intelligently, set GOBIN

--- rt-pkg/build.sh
+++ rt-pkg/build.sh
@@ -5,11 +5,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 : ${GOPATH:="${HOME}/.go_workspace"}
+: ${GOBIN:="${DIR}/build"}
 : ${ORG_PATH:="github.com/grafana"}
 : ${REPO_PATH:="${ORG_PATH}/grafana"}
 
 export PATH=$GOPATH/bin:$PATH
-export GOPATH
+export GOPATH GOBIN
 
 cd ${GOPATH}/src/github.com/grafana/grafana
 
--- rt-pkg/depends.sh
+++ rt-pkg/depends.sh
@@ -8,14 +8,20 @@ cd ${DIR}
 : ${ORG_PATH:="github.com/grafana"}
 : ${REPO_PATH:="${ORG_PATH}/grafana"}
 
+if [ ! -z ${CIRCLECI} ] ; then
+  : ${CHECKOUT:="/home/ubuntu/${CIRCLE_PROJECT_REPONAME}"}
+else
+  : ${CHECKOUT:="${DIR}/.."}
+fi
+
 export GOPATH
 
 mkdir -p artifacts
 bundle install
 
-echo "Linking ${GOPATH}/src/${REPO_PATH} to ${DIR}/../"
+echo "Linking ${GOPATH}/src/${REPO_PATH} to ${CHECKOUT}"
 rm -fr ${GOPATH}/src/${REPO_PATH}
 mkdir -p ${GOPATH}/src/${ORG_PATH}
-ln -s ${DIR}/../ ${GOPATH}/src/${REPO_PATH}
+ln -s ${CHECKOUT} ${GOPATH}/src/${REPO_PATH}
 
 go get github.com/tools/godep
