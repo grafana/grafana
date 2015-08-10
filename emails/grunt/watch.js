4439545428a4afb96a7902b334eba57b68678580
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-08-10 21:24:47 +0200

    feat(invite): lots of work completing the new email template system and css inlineing, converted new_user_invite.html to new system, #2353

--- emails/grunt/watch.js
+++ emails/grunt/watch.js
@@ -3,8 +3,10 @@ module.exports = {
         files: [
             //what are the files that we want to watch
             'assets/css/*.css',
-            '*.html'
+            'templates/**/*.html',
+            'grunt/*.js',
         ],
+        tasks: ['default'],
         options: {
             nospawn: true,
             livereload: true,
