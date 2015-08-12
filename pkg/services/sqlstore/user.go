e6d09b3266d32e418b9e6a05cc5ca6e07b09a934
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-08-12 09:23:46 +0200

    fix(db): remove stars and tags when removing user or dashboard, fixes #2016

--- pkg/services/sqlstore/user.go
+++ pkg/services/sqlstore/user.go
@@ -1,7 +1,6 @@
 package sqlstore
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
@@ -309,9 +308,19 @@ func SearchUsers(query *m.SearchUsersQuery) error {
 
 func DeleteUser(cmd *m.DeleteUserCommand) error {
 	return inTransaction(func(sess *xorm.Session) error {
-		var rawSql = fmt.Sprintf("DELETE FROM %s WHERE id=?", x.Dialect().Quote("user"))
-		_, err := sess.Exec(rawSql, cmd.UserId)
-		return err
+		deletes := []string{
+			"DELETE FROM star WHERE user_id = ?",
+			"DELETE FROM user WHERE id = ?",
+		}
+
+		for _, sql := range deletes {
+			_, err := sess.Exec(sql, cmd.UserId)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
 	})
 }
 
