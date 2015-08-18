c4346e8fadedd98a1e4928fce89686a9608214d6
woodsaj <anthony@monkey.id.au>
2015-08-18 17:45:28 +0800

    update metric schema to use string for both key and value of tag.

--- pkg/models/metric.go
+++ pkg/models/metric.go
@@ -3,13 +3,13 @@ package models
 // IndvMetric holds the information from an individual metric item coming in
 // from rabbitmq.
 type MetricDefinition struct {
-	OrgId      int64                  `json:"org_id"`
-	Name       string                 `json:"name"`
-	Metric     string                 `json:"metric"`
-	Interval   int64                  `json:"interval"`
-	Value      float64                `json:"value"`
-	Unit       string                 `json:"unit"`
-	Time       int64                  `json:"time"`
-	TargetType string                 `json:"target_type"`
-	Tags       map[string]interface{} `json:"tags"`
+	OrgId      int64             `json:"org_id"`
+	Name       string            `json:"name"`
+	Metric     string            `json:"metric"`
+	Interval   int64             `json:"interval"`
+	Value      float64           `json:"value"`
+	Unit       string            `json:"unit"`
+	Time       int64             `json:"time"`
+	TargetType string            `json:"target_type"`
+	Tags       map[string]string `json:"tags"`
 }
