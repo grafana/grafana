d8a352a1517638873ab47a3d7d476eb790b57b36
Dieter Plaetinck <dieter@raintank.io>
2015-08-19 22:27:39 +0200

    add metrics for metricpublisher

--- pkg/services/metricpublisher/metricpublisher.go
+++ pkg/services/metricpublisher/metricpublisher.go
@@ -22,10 +22,13 @@ const (
 const maxMpubSize = 5 * 1024 * 1024 // nsq errors if more. not sure if can be changed
 const maxMetricPerMsg = 1000        // emperically found through benchmarks (should result in 64~128k messages)
 var (
-	globalProducer         *nsq.Producer
-	topic                  string
-	metricPublisherMetrics met.Count
-	metricPublisherMsgs    met.Count
+	globalProducer    *nsq.Producer
+	topic             string
+	metricsPublished  met.Count
+	messagesPublished met.Count
+	messagesSize      met.Meter
+	metricsPerMessage met.Meter
+	publishDuration   met.Timer
 )
 
 func Init(metrics met.Backend) {
@@ -44,8 +47,11 @@ func Init(metrics met.Backend) {
 	if err != nil {
 		log.Fatal(0, "failed to initialize nsq producer.", err)
 	}
-	metricPublisherMetrics = metrics.NewCount("metricpublisher.metrics-published")
-	metricPublisherMsgs = metrics.NewCount("metricpublisher.messages-published")
+	metricsPublished = metrics.NewCount("metricpublisher.metrics-published")
+	messagesPublished = metrics.NewCount("metricpublisher.messages-published")
+	messagesSize = metrics.NewMeter("metricpublisher.message_size", 0)
+	metricsPerMessage = metrics.NewMeter("metricpublisher.metrics_per_message", 0)
+	publishDuration = metrics.NewTimer("metricpublisher.publish_duration", 0)
 	//go stresser() // enable this to send a "stress load" to test the metrics pipeline
 }
 
@@ -142,9 +148,13 @@ func Publish(metrics []*m.MetricDefinition) error {
 		if err != nil {
 			log.Fatal(0, "buf.Write failed: %s", err.Error())
 		}
-		metricPublisherMetrics.Inc(int64(len(subslice)))
-		metricPublisherMsgs.Inc(1)
+		metricsPublished.Inc(int64(len(subslice)))
+		messagesPublished.Inc(1)
+		messagesSize.Value(int64(buf.Len()))
+		metricsPerMessage.Value(int64(len(subslice)))
+		pre := time.Now()
 		err = globalProducer.Publish(topic, buf.Bytes())
+		publishDuration.Value(time.Since(pre))
 		if err != nil {
 			panic(fmt.Errorf("can't publish to nsqd: %s", err))
 		}
