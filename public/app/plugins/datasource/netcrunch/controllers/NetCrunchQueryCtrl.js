284085a36d7ac70287aaee9500d7e7beb33ab3f3
Bogusław Gorczyca <boguslaw@gorczyca.name>
2015-09-04 14:01:44 +0200

    NetCrunch datasource query editor was added.

---
+++ public/app/plugins/datasource/netcrunch/controllers/NetCrunchQueryCtrl.js
@@ -0,0 +1,16 @@
+/*****************************************************************
+ *
+ * Author   : Bogusław Gorczyca
+ * Created  : 2015-08-21 13:37
+ *
+ * 2015 Copyright AdRem Software, all rights reserved
+ *
+ *****************************************************************/
+
+/* global angular, console */
+
+(function () {
+
+    'use strict';
+
+})();
