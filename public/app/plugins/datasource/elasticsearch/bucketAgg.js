756ec8ccd7451503643f7a2fcc9a911ddd8bbb50
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-09-05 10:48:11 +0200

    feat(elasticsearch): close to getting group by term options ui working

--- public/app/plugins/datasource/elasticsearch/bucketAgg.js
+++ public/app/plugins/datasource/elasticsearch/bucketAgg.js
@@ -11,11 +11,25 @@ function (angular, _, $) {
     module.controller('ElasticBucketAggCtrl', function($scope, uiSegmentSrv, $q) {
       var bucketAggs = $scope.target.bucketAggs;
 
-      $scope.agg = bucketAggs[$scope.index];
-
       $scope.bucketAggTypes = [
         {text: "Terms",           value: 'terms' },
         {text: "Date Histogram",  value: 'date_histogram' },
+      ];
+
+      $scope.orderOptions = [
+        {text: "Top",     value: 'desc' },
+        {text: "Bottom",  value: 'asc' },
+      ];
+
+      $scope.sizeOptions = [
+        {text: "No limit", value: '0' },
+        {text: "1", value: '1' },
+        {text: "2", value: '2' },
+        {text: "3", value: '4' },
+        {text: "5", value: '5' },
+        {text: "10", value: '10' },
+        {text: "15", value: '15' },
+        {text: "20", value: '20' },
       ];
 
       $scope.$watch("index", function() {
@@ -23,12 +37,36 @@ function (angular, _, $) {
         $scope.isLast = $scope.index === bucketAggs.length - 1;
       });
 
-      if ($scope.agg.type === "terms") {
-        $scope.aggOptionsString = "Top 5, Order by: sum @value";
-      }
+      $scope.init = function() {
+        $scope.agg = bucketAggs[$scope.index];
+        $scope.modelIsValid();
+      };
+
+      $scope.onChangeInternal = function() {
+        if ($scope.modelIsValid()) {
+          $scope.onChange();
+        }
+      };
+
+      $scope.modelIsValid = function() {
+        if ($scope.agg.type === "terms") {
+          $scope.aggOptionsString = "Top 5, Order by: sum @value";
+
+          $scope.agg.order = $scope.agg.order || "desc";
+          $scope.agg.size = $scope.agg.size || "0";
+          $scope.agg.orderBy = $scope.agg.orderBy || "_count";
+        }
+        return true;
+      };
 
       $scope.toggleOptions = function() {
         $scope.showOptions = !$scope.showOptions;
+
+        $scope.orderByOptions = [
+          {text: "Doc Count", value: '_count' },
+          {text: "Term name", value: '_term' },
+          {text: "Average of @value", value: '0' },
+        ];
       }
 
       $scope.addBucketAgg = function() {
@@ -47,6 +85,8 @@ function (angular, _, $) {
         bucketAggs.splice($scope.index, 1);
         $scope.onChange();
       };
+
+      $scope.init();
 
     });
 
