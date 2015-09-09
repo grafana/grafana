005e1e002b7aa476e51bb12cb641b50c05a60ec3
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-09-09 17:40:52 +0200

    more playing around with typescript

---
+++ public/app/controllers/fileSearcher.ts
@@ -0,0 +1,18 @@
+///<reference path="../headers/require.d.ts" />
+
+class FileSearcher {
+
+    constructor() {
+    }
+
+    public getFiles() : string[] {
+      return ["asd"];
+    }
+
+    public getBullshit() : any {
+      return "asd";
+    }
+
+}
+
+export = FileSearcher;
