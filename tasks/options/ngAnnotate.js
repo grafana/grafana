e7b4414b562710144b6cfa2d17947648e6c00901
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-09-14 09:33:26 +0200

    fix(build): fixed issue with ngAnnotate not including files in core/*, fixes #2733

--- tasks/options/ngAnnotate.js
+++ tasks/options/ngAnnotate.js
@@ -4,16 +4,7 @@ module.exports = function(config) {
       expand: true,
       cwd:'<%= genDir %>',
       src: [
-        'app/controllers/**/*.js',
-        'app/plugins/**/*.js',
-        'app/directives/**/*.js',
-        'app/services/**/*.js',
-        'app/filters/**/*.js',
-        'app/features/**/*.js',
-        'app/panels/**/*.js',
-        'app/routes/**/*.js',
-        'app/app.js',
-        'vendor/angular/**/*.js',
+        'app/**/*.js',
       ],
       dest: '<%= genDir %>'
     }
