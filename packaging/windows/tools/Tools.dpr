b718a94acce73dd1a932aceb12b236fadc2bda66
Bogusław Gorczyca <boguslaw@gorczyca.name>
2015-10-02 08:47:33 +0200

    Tools.dll Delphi project was added

---
+++ packaging/windows/tools/Tools.dpr
@@ -0,0 +1,21 @@
+library Tools;
+
+{ Important note about DLL memory management: ShareMem must be the
+  first unit in your library's USES clause AND your project's (select
+  Project-View Source) USES clause if your DLL exports any procedures or
+  functions that pass strings as parameters or function results. This
+  applies to all strings passed to and from your DLL--even those that
+  are nested in records and classes. ShareMem is the interface unit to
+  the BORLNDMM.DLL shared memory manager, which must be deployed along
+  with your DLL. To avoid using BORLNDMM.DLL, pass string information
+  using PChar or ShortString parameters. }
+
+uses
+  System.ShareMem,
+  System.SysUtils,
+  System.Classes;
+
+{$R *.res}
+
+begin
+end.
