b8dfd9053b1aa5ba63c02e25679b0c536c389c85
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-10-10 11:46:00 -0400

    fix(influxdb): influxdb data source did not use right http abstraction for metric queries, fixes #2919

--- public/app/plugins/datasource/influxdb/datasource.js
+++ public/app/plugins/datasource/influxdb/datasource.js
@@ -12,7 +12,7 @@ function (angular, _, dateMath, InfluxSeries, InfluxQueryBuilder) {
 
   var module = angular.module('grafana.services');
 
-  module.factory('InfluxDatasource', function($q, $http, templateSrv) {
+  module.factory('InfluxDatasource', function($q, backendSrv, templateSrv) {
 
     function InfluxDatasource(datasource) {
       this.type = 'influxdb';
@@ -161,7 +161,7 @@ function (angular, _, dateMath, InfluxSeries, InfluxQueryBuilder) {
         options.headers.Authorization = self.basicAuth;
       }
 
-      return $http(options).then(function(result) {
+      return backendSrv.datasourceRequest(options).then(function(result) {
         return result.data;
       }, function(err) {
         if (err.status !== 0 || err.status >= 300) {
