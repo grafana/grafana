bd03d504c0d123fdf266dfa9786da55b96e70e5c
Anthony Woods <awoods@raintank.io>
2015-10-15 21:31:43 +0800

    set default check frequency to 60seconds for all checks except Ping. closes #491

--- public/plugins/raintank/features/endpointConfCtrl.js
+++ public/plugins/raintank/features/endpointConfCtrl.js
@@ -368,7 +368,7 @@ function (angular, _) {
         collector_tags: $scope.global_collectors.collector_tags,
         settings: [],
         enabled: true,
-        frequency: 10,
+        frequency: 60,
         health_settings: {
           steps: 3,
           num_collectors: 3,
@@ -381,8 +381,8 @@ function (angular, _) {
       _.forEach(payload, function(suggestion) {
         _.defaults(suggestion, defaults);
         var type = $scope.monitor_types[suggestion.monitor_type_id];
-        if (type.name.indexOf("HTTP") === 0) {
-          suggestion.frequency = 60;
+        if (type.name.indexOf("Ping") === 0) {
+          suggestion.frequency = 10;
         }
         $scope.monitors[type.name.toLowerCase()] = suggestion;
       });
