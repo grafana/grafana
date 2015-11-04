ec61bee45b057925e7fd794dc44c5bcd8c020a86
utkarshcmu <utkarsh.cmu@gmail.com>
2015-11-03 16:01:21 -0800

    Fixed failing tests, added more tests

--- public/test/specs/dashboardSrv-specs.js
+++ public/test/specs/dashboardSrv-specs.js
@@ -249,12 +249,14 @@ define([
       });
     });
 
-    describe('Formatting epoch timestamp', function() {
+    describe('Formatting epoch timestamp when timezone is set as in browser', function() {
 
       var dashboard;
 
       beforeEach(function() {
-        dashboard = _dashboardSrv.create({});
+        dashboard = _dashboardSrv.create({
+          timezone: 'browser',
+        });
       });
 
       it('Should format timestamp with second resolution by default', function() {
@@ -263,6 +265,25 @@ define([
 
       it('Should format timestamp with millisecond resolution if format is passed as parameter', function() {
         expect(dashboard.formatDate(1234567890007,'YYYY-MM-DD HH:mm:ss.SSS')).to.be('2009-02-13 15:31:30.007');
+      });
+    });
+
+    describe('Formatting epoch timestamp when timezone is set as utc', function() {
+
+      var dashboard;
+
+      beforeEach(function() {
+        dashboard = _dashboardSrv.create({
+          timezone: 'utc',
+        });
+      });
+
+      it('Should format timestamp with second resolution by default', function() {
+        expect(dashboard.formatDate(1234567890000)).to.be('2009-02-13 23:31:30');
+      });
+
+      it('Should format timestamp with millisecond resolution if format is passed as parameter', function() {
+        expect(dashboard.formatDate(1234567890007,'YYYY-MM-DD HH:mm:ss.SSS')).to.be('2009-02-13 23:31:30.007');
       });
     });
 
