90cca9395110ff84b48b5b5a6287248c31f256c2
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-11-05 13:13:13 +0100

    feat(tablepanel): lots of work on table panel

--- public/app/panels/table/specs/transformers_specs.ts
+++ public/app/panels/table/specs/transformers_specs.ts
@@ -67,6 +67,41 @@ describe('when transforming time series table', () => {
         expect(table.rows[1][2]).to.be(undefined);
       });
     });
+
+    describe('JSON Data', () => {
+      var panel = {
+        transform: 'json',
+        fields: [{name: 'timestamp'}, {name: 'message'}]
+      };
+      var rawData = [
+        {
+          type: 'docs',
+          datapoints: [
+            {
+              timestamp: 'time',
+              message: 'message'
+            }
+          ]
+        }
+      ];
+
+      beforeEach(() => {
+        table = TableModel.transform(rawData, panel);
+      });
+
+      it ('should return 2 columns', () => {
+        expect(table.columns.length).to.be(2);
+        expect(table.columns[0].text).to.be('timestamp');
+        expect(table.columns[1].text).to.be('message');
+      });
+
+      it ('should return 2 rows', () => {
+        expect(table.rows.length).to.be(2);
+        expect(table.rows[0][0]).to.be('time');
+        expect(table.rows[0][1]).to.be('message');
+      });
+
+    });
   });
 });
 
