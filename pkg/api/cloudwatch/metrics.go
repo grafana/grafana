f55e24cd1264713aeb77c530332da1f13176cc19
utkarshcmu <utkarsh.cmu@gmail.com>
2015-11-09 00:28:16 -0800

    Added missing AWS Regions

--- pkg/api/cloudwatch/metrics.go
+++ pkg/api/cloudwatch/metrics.go
@@ -67,8 +67,8 @@ func init() {
 
 func handleGetRegions(req *cwRequest, c *middleware.Context) {
 	regions := []string{
-		"us-west-2", "us-west-1", "eu-west-1", "eu-central-1", "ap-southeast-1",
-		"ap-southeast-2", "ap-northeast-1", "sa-east-1",
+		"us-east-1", "us-west-2", "us-west-1", "eu-west-1", "eu-central-1", "ap-southeast-1",
+		"ap-southeast-2", "ap-northeast-1", "sa-east-1", "cn-north-1",
 	}
 
 	result := []interface{}{}
