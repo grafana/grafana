7e518e13c6f0bf0fca15bcd1887edc7d2cc7d695
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2015-11-11 14:06:47 +0900

    sort regions by alphabetical order

--- pkg/api/cloudwatch/metrics.go
+++ pkg/api/cloudwatch/metrics.go
@@ -69,8 +69,8 @@ func init() {
 // Please update the region list in public/app/plugins/datasource/cloudwatch/partials/config.html
 func handleGetRegions(req *cwRequest, c *middleware.Context) {
 	regions := []string{
-		"us-east-1", "us-west-2", "us-west-1", "eu-west-1", "eu-central-1", "ap-southeast-1",
-		"ap-southeast-2", "ap-northeast-1", "sa-east-1", "cn-north-1",
+		"ap-northeast-1", "ap-southeast-1", "ap-southeast-2", "cn-north-1",
+		"eu-central-1", "eu-west-1", "sa-east-1", "us-east-1", "us-west-1", "us-west-2",
 	}
 
 	result := []interface{}{}
