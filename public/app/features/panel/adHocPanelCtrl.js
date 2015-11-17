4a350a9efee93a4979f6106fadd5bbb73921f18b
Maciek Próchniak <mpr@touk.pl>
2015-11-17 21:57:45 +0100

    small fixes

--- public/app/features/panel/adHocPanelCtrl.js
+++ public/app/features/panel/adHocPanelCtrl.js
@@ -1,20 +1,23 @@
 define([
     'angular',
-    'jquery',
+    'jquery'
   ],
   function (angular, $) {
     "use strict";
 
     var module = angular.module('grafana.routes');
 
-    module.controller('AdHocPanelCtrl', function ($scope, $routeParams, $location, $timeout, panelSrv, dashboardSrv, contextSrv, timeSrv) {
-
+    module.controller('AdHocPanelCtrl', function ($scope, $routeParams, $location, $timeout, panelSrv,
+                                                  dashboardSrv, dashboardViewStateSrv, contextSrv, timeSrv) {
       $scope.row = {
         height: $(window).height() + 'px'
       };
+      var docHeight = $(window).height();
+      $scope.height = Math.floor(docHeight * 0.7);
 
       $scope.$index = 0;
       $scope.panel = {
+        id: -1,
         title: 'Ad hoc',
         error: false,
         span: 12,
@@ -23,18 +26,17 @@ define([
       };
 
       $scope.dashboard = dashboardSrv.create({}, {});
+      $scope.dashboardMeta = {
+        canEdit: true
+      };
       dashboardSrv.setCurrent($scope.dashboard);
       timeSrv.init($scope.dashboard);
 
-      $scope.dashboardViewState = {
-        registerPanel: function () {
-        }, state: {}
-      };
-
-      var docHeight = $(window).height();
+      $scope.dashboardViewState = dashboardViewStateSrv.create($scope);
 
       $scope.editMode = true;
-      $scope.height = Math.floor(docHeight * 0.7);
+      $scope.dashboardViewState.registerPanel($scope);
+      $scope.dashboardViewState.update({fullscreen: true, edit: true, panelId: -1});
 
     });
 
