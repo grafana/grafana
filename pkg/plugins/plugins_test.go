421eb37a2e45a5f995a8afa929be8a4e880c74b6
woodsaj <anthony@monkey.id.au>
2015-11-27 17:17:27 +0800

    really fix unit tests.

--- pkg/plugins/plugins_test.go
+++ pkg/plugins/plugins_test.go
@@ -6,12 +6,14 @@ import (
 
 	"github.com/grafana/grafana/pkg/setting"
 	. "github.com/smartystreets/goconvey/convey"
+	"gopkg.in/ini.v1"
 )
 
 func TestPluginScans(t *testing.T) {
 
 	Convey("When scaning for plugins", t, func() {
-		setting.StaticRootPath = filepath.Abs("../../public/")
+		setting.StaticRootPath, _ = filepath.Abs("../../public/")
+		setting.Cfg = ini.Empty()
 		err := Init()
 
 		So(err, ShouldBeNil)
