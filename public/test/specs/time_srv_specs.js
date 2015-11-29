8cdaa044e1eda21afa34f47caa18503d6a590e0d
utkarshcmu <utkarsh.cmu@gmail.com>
2015-11-29 03:14:26 -0800

    Removed repeating test

--- public/test/specs/time_srv_specs.js
+++ public/test/specs/time_srv_specs.js
@@ -92,13 +92,6 @@ define([
         expect(_dashboard.refresh).to.be('30s');
       });
 
-      it('should restore refresh for absolute time range', function() {
-        _dashboard.refresh = '30s';
-
-        ctx.service.setTime({from: '2011-01-01', to: '2015-01-01' });
-        expect(_dashboard.refresh).to.be('30s');
-      });
-
       it('should restore refresh after relative time range is set', function() {
         _dashboard.refresh = '10s';
         ctx.service.setTime({from: moment([2011,1,1]), to: moment([2015,1,1])});
