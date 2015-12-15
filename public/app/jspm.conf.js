8e6fdf62fb81a398b50071dd03bf8eedbcd6aa6a
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-12-15 19:27:04 +0100

    tech(systemjs): things are starting to work

--- public/app/jspm.conf.js
+++ public/app/jspm.conf.js
@@ -12,7 +12,16 @@ System.config({
     "angular-bindonce": "vendor/angular-bindonce/bindonce.js",
     "spectrum": "vendor/spectrum.js",
     "filesaver": "vendor/filesaver.js",
-    "bootstrap-tagsinput": "vendor/tagsinput/bootstrap-tagsinput.js"
+    "bootstrap-tagsinput": "vendor/tagsinput/bootstrap-tagsinput.js",
+    'jquery.flot':             'vendor/flot/jquery.flot',
+    'jquery.flot.pie':         'vendor/flot/jquery.flot.pie',
+    'jquery.flot.events':      'vendor/flot/jquery.flot.events',
+    'jquery.flot.selection':   'vendor/flot/jquery.flot.selection',
+    'jquery.flot.stack':       'vendor/flot/jquery.flot.stack',
+    'jquery.flot.stackpercent':'vendor/flot/jquery.flot.stackpercent',
+    'jquery.flot.time':        'vendor/flot/jquery.flot.time',
+    'jquery.flot.crosshair':   'vendor/flot/jquery.flot.crosshair',
+    'jquery.flot.fillbelow':   'vendor/flot/jquery.flot.fillbelow',
   },
 
   packages: {
