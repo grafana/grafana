0fbde1f757453d091fab44d5384b9bd723d0eb1f
Bogusław Gorczyca <boguslaw@gorczyca.name>
2016-01-11 10:27:56 +0100

    netCrunchConnectionProviderConsts was added

--- public/app/consts/netCrunchConnectionProviderConsts.js
+++ public/app/consts/netCrunchConnectionProviderConsts.js
@@ -1,16 +1,21 @@
 /*****************************************************************
  *
  * Author   : Bogusław Gorczyca
- * Created  : 2015-08-21 14:57
+ * Created  : 2016-01-11 10:15
  *
  * 2015 Copyright AdRem Software, all rights reserved
  *
  *****************************************************************/
 
 define([
-    './netCrunchCounterConsts',
-    './netCrunchCounterTypes',
-    './netCrunchTrendDataProviderConsts',
-    './netCrunchConnectionProviderConsts'
+    'angular'
   ],
-  function () {});
+
+  function (angular) {
+
+    'use strict';
+
+    angular.module('grafana.const').constant('netCrunchConnectionProviderConsts', {
+      API_NAME: '/ncapi/'
+    });
+});
