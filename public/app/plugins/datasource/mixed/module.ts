b165e1c7057208f67f290e83f3ba5ee533e90baa
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-01-14 16:01:17 +0100

    feat(plugins): upgraded opentsdb and prometheus to new plugin module return format

--- public/app/plugins/datasource/mixed/module.ts
+++ public/app/plugins/datasource/mixed/module.ts
@@ -3,12 +3,5 @@
 import angular from 'angular';
 import {MixedDatasource} from './datasource';
 
-var module = angular.module('grafana.directives');
-
-module.directive('metricQueryEditorMixed', function() {
-  return {templateUrl: 'app/plugins/datasource/mixed/partials/query.editor.html'};
-});
-
-
 export {MixedDatasource, MixedDatasource as Datasource};
 
