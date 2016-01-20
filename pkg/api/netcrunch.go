8f028712f5bae3009f1563dafba34e9c074365fe
Bogusław Gorczyca <boguslaw@gorczyca.name>
2016-01-20 16:21:56 +0100

    Merge proxy for NetCrunch datasource

--- pkg/api/netcrunch.go
+++ pkg/api/netcrunch.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+  "net/url"
   "github.com/grafana/grafana/pkg/middleware"
   "github.com/grafana/grafana/pkg/netcrunch"
   "github.com/grafana/grafana/pkg/setting"
@@ -19,9 +20,10 @@ func ProxyNetCrunchServerRequest(c *middleware.Context) {
   datasource := netcrunch.GetNetCrunchDataSource()
   NetCrunchServerSettings := setting.NetCrunch
   datasource.Url = datasource.Url + "/" + NetCrunchServerSettings.Api + "/"
+  url, _ := url.Parse(datasource.Url)
 
   proxyPath := c.Params("*")
-  proxy := NewReverseProxy(&datasource, proxyPath)
+  proxy := NewReverseProxy(&datasource, proxyPath, url)
   proxy.Transport = dataProxyTransport
   proxy.ServeHTTP(c.RW(), c.Req.Request)
 }
