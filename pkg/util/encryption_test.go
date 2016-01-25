c8c337ceadc1ef97114cce30828800b6cb6bd842
Anthony Woods <awoods@raintank.io>
2016-01-26 05:15:29 +0800

    use PBKDF2 to esnure key is 23bytes.

--- pkg/util/encryption_test.go
+++ pkg/util/encryption_test.go
@@ -10,12 +10,11 @@ func TestEncryption(t *testing.T) {
 
 	Convey("When getting encryption key", t, func() {
 
-		key := encryptionKeyToBytes("secret")
+		key := encryptionKeyToBytes("secret", "salt")
 		So(len(key), ShouldEqual, 32)
 
-		key = encryptionKeyToBytes("a very long secret key that is larger then 32bytes")
+		key = encryptionKeyToBytes("a very long secret key that is larger then 32bytes", "salt")
 		So(len(key), ShouldEqual, 32)
-
 	})
 
 	Convey("When decrypting basic payload", t, func() {
