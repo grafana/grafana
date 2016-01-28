b30dcce4bc6c4557ee8702902b702674d1666c17
utkarshcmu <utkarsh.cmu@gmail.com>
2016-01-28 00:52:52 -0800

    Added dtos and UI for more metadata

--- pkg/api/dtos/models.go
+++ pkg/api/dtos/models.go
@@ -30,19 +30,23 @@ type CurrentUser struct {
 }
 
 type DashboardMeta struct {
-	IsStarred  bool      `json:"isStarred,omitempty"`
-	IsHome     bool      `json:"isHome,omitempty"`
-	IsSnapshot bool      `json:"isSnapshot,omitempty"`
-	Type       string    `json:"type,omitempty"`
-	CanSave    bool      `json:"canSave"`
-	CanEdit    bool      `json:"canEdit"`
-	CanStar    bool      `json:"canStar"`
-	Slug       string    `json:"slug"`
-	Expires    time.Time `json:"expires"`
-	Created    time.Time `json:"created"`
-	Updated    time.Time `json:"updated"`
-	UpdatedBy  string    `json:"updatedBy"`
-	CreatedBy  string    `json:"createdBy"`
+	IsStarred    bool      `json:"isStarred,omitempty"`
+	IsHome       bool      `json:"isHome,omitempty"`
+	IsSnapshot   bool      `json:"isSnapshot,omitempty"`
+	Type         string    `json:"type,omitempty"`
+	CanSave      bool      `json:"canSave"`
+	CanEdit      bool      `json:"canEdit"`
+	CanStar      bool      `json:"canStar"`
+	Slug         string    `json:"slug"`
+	Expires      time.Time `json:"expires"`
+	Created      time.Time `json:"created"`
+	Updated      time.Time `json:"updated"`
+	UpdatedBy    string    `json:"updatedBy"`
+	CreatedBy    string    `json:"createdBy"`
+  TotalRows    int64     `json:"totalRows"`
+  TotalPanels  int64     `json:"totalPanels"`
+  TotalQueries int64     `json:"totalQueries"`
+  Version      int       `json:"version"`
 }
 
 type DashboardFullWithMeta struct {
