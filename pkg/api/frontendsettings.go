64c6ae2fc446a31dffe20f84037b017a969bae1d
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-01-27 19:07:39 -0500

    feat(panels): upgradede singlestat panel

--- pkg/api/frontendsettings.go
+++ pkg/api/frontendsettings.go
@@ -123,6 +123,7 @@ func getFrontendSettingsMap(c *middleware.Context) (map[string]interface{}, erro
 		panels[panel.Id] = map[string]interface{}{
 			"module": panel.Module,
 			"name":   panel.Name,
+			"id":     panel.Id,
 			"info":   panel.Info,
 		}
 	}
