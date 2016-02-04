316e1aac67abb0f4d22303cef0f3556a6ee04d64
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-04 15:12:44 +0100

    fix(build): minor fix

--- public/app/features/panel/query_ctrl.ts
+++ public/app/features/panel/query_ctrl.ts
@@ -55,23 +55,3 @@ export class QueryCtrl {
   }
 }
 
-// var directivesModule = angular.module('grafana.directives');
-//
-// /** @ngInject */
-// function metricsQueryOptions(dynamicDirectiveSrv, datasourceSrv) {
-//   return dynamicDirectiveSrv.create({
-//     watchPath: "ctrl.panel.datasource",
-//     directive: scope => {
-//       return datasourceSrv.get(scope.ctrl.panel.datasource).then(ds => {
-//         return System.import(ds.meta.module).then(dsModule => {
-//           return {
-//             name: 'metrics-query-options-' + ds.meta.id,
-//             fn: dsModule.metricsQueryOptions
-//           };
-//         });
-//       });
-//     }
-//   });
-// }
-//
-// directivesModule.directive('metricsQueryOptions', metricsQueryOptions);
