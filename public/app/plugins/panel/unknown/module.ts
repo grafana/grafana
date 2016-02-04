310f7761afbfe8a10ddab138bfb06dcafe570032
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-04 15:12:44 +0100

    fix(build): minor fix

--- public/app/plugins/panel/unknown/module.ts
+++ public/app/plugins/panel/unknown/module.ts
@@ -5,6 +5,7 @@ import {PanelCtrl} from '../../../features/panel/panel';
 export class UnknownPanelCtrl extends PanelCtrl {
   static templateUrl = 'public/app/plugins/panel/unknown/module.html';
 
+  /** @ngInject */
   constructor($scope, $injector) {
     super($scope, $injector);
   }
