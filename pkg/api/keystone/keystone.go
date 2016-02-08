6d70ab8ab675c48f093dcecf515a54cfa8df5cee
Thomas Graichen <thomas.graichen@sap.com>
2016-02-08 12:43:49 +0000

    first try to add keystone v3 support to the keystone api code provided by ryan bak

--- pkg/api/keystone/keystone.go
+++ pkg/api/keystone/keystone.go
@@ -40,6 +40,47 @@ type v2_token_struct struct {
 	Issued_at string
 }
 
+type v3_auth_post_struct struct {
+    Auth v3_auth_struct  `json:"auth"`
+}
+
+type v3_auth_struct struct {
+    Identity v3_identity_struct  `json:"identity"`
+    Scope v3_scope_struct  `json:"scope"`
+}
+
+type v3_identity_struct struct {
+    Methods []string  `json:"methods"`
+    Password v3_passwordmethod_struct  `json:"password"`
+}
+
+type v3_passwordmethod_struct struct {
+    User v3_user_struct `json:"user"`
+}
+
+type v3_user_struct struct {
+    Name string `json:"name"`
+    Password string `json:"password"`
+    Domain v3_userdomain_struct `json:"domain"`
+}
+
+type v3_userdomain_struct struct {
+    Name string `json:"name"`
+}
+
+type v3_scope_struct struct {
+    Project v3_project_struct `json:"project"`
+}
+
+type v3_project_struct struct {
+    Name string `json:"name"`
+    Domain v3_projectdomain_struct `json:"domain"`
+}
+
+type v3_projectdomain_struct struct {
+    Name string `json:"name"`
+}
+
 func getUserName(c *middleware.Context) (string, error) {
 	userQuery := m.GetUserByIdQuery{Id: c.Session.Get(middleware.SESS_KEY_USERID).(int64)}
 	if err := bus.Dispatch(&userQuery); err != nil {
@@ -101,8 +142,40 @@ func authenticateV2(c *middleware.Context) (string, error) {
 }
 
 func authenticateV3(c *middleware.Context) (string, error) {
-	// TODO implement
-	return "", errors.New("Keystone v3 authentication not implemented")
+	server := setting.KeystoneURL
+
+	var auth_post v3_auth_post_struct
+	auth_post.Auth.Identity.Methods = []string{"password"}
+	if username, err := getUserName(c); err != nil {
+		return "", err
+	} else {
+		auth_post.Auth.Identity.Password.User.Name = username
+	}
+	if tenant, err := getOrgName(c); err != nil {
+		return "", err
+	} else {
+		auth_post.Auth.Scope.Project.Name = tenant
+	}
+	auth_post.Auth.Identity.Password.User.Password = c.Session.Get(middleware.SESS_KEY_PASSWORD).(string)
+	// the user domain name is currently hardcoded via a config setting - this should change to an extra domain field in the login dialog later
+	auth_post.Auth.Identity.Password.User.Domain.Name = setting.KeystoneUserDomainName
+	b, _ := json.Marshal(auth_post)
+
+	request, err := http.NewRequest("POST", server + "/v3/tokens", bytes.NewBuffer(b))
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return "", err
+	} else if resp.StatusCode != 201 {
+		return "", errors.New("Keystone authentication failed: " + resp.Status)
+	}
+
+	// in keystone v3 the token is in the response header
+	return resp.Header.Get("X-Subject-Token"), nil
 }
 
 func GetToken(c *middleware.Context) (string, error) {
