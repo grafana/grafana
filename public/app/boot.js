0106033293aef642dc83ef44eebc11226ca5c19e
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-08 14:35:34 +0100

    refactor(): removed stuff

--- public/app/boot.js
+++ public/app/boot.js
@@ -1,8 +1,8 @@
 (function bootGrafana() {
   'use strict';
 
-  System.import('app/app').then(function(app) {
-    app.init();
+  System.import('app/grafana').then(function(grafana) {
+    grafana.default.init();
   }).catch(function(err) {
     console.log('Loading app module failed: ', err);
   });
