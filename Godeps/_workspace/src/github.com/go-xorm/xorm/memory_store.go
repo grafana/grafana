9dac382fbfb9cc0e92ea9d77504b103f08826b7a
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-10 10:09:24 +0100

    dependency(): updated go lib go-xorm

--- Godeps/_workspace/src/github.com/go-xorm/xorm/memory_store.go
+++ Godeps/_workspace/src/github.com/go-xorm/xorm/memory_store.go
@@ -1,4 +1,7 @@
-// MemoryStore implements CacheStore provide local machine
+// Copyright 2015 The Xorm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package xorm
 
 import (
--- Godeps/_workspace/src/github.com/go-xorm/xorm/syslogger.go
+++ Godeps/_workspace/src/github.com/go-xorm/xorm/syslogger.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Xorm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // +build !windows,!nacl,!plan9
 
 package xorm
