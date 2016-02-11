50ddf7913a6d2affed70dcdc94562cc19942243d
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-11 14:28:04 +0100

    docs(): work on example app

---
+++ examples/nginx-app/src/module.js
@@ -0,0 +1,9 @@
+import {LogsPageCtrl} from './components/logs';
+import {StreamPageCtrl} from './components/stream';
+import {NginxAppConfigCtrl} from './components/config';
+
+export {
+  NginxAppConfigCtrl as ConfigCtrl,
+  StreamPageCtrl,
+  LogsPageCtrl
+};
