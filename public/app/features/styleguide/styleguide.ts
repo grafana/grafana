65f9cc986a1fdd5a69c39e41f5877132d5a59d19
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-18 17:25:11 +0100

    ux(): styleguide with auto generated colors generated from the sass variable files

--- public/app/features/styleguide/styleguide.ts
+++ public/app/features/styleguide/styleguide.ts
@@ -1,11 +1,25 @@
 
 import coreModule from 'app/core/core_module';
 import config from 'app/core/config';
+import _ from 'lodash';
 
 class StyleGuideCtrl {
+  colors: any = [];
+  theme: string;
+
+  /** @ngInject **/
+  constructor($http) {
+    this.theme = config.bootData.user.lightTheme ? 'light': 'dark';
+
+    $http.get('public/sass/styleguide.json').then(res => {
+      this.colors = _.map(res.data[this.theme], (value, key) => {
+        return {name: key, value: value};
+      });
+    });
+  }
 
   switchTheme() {
-    var other = config.bootData.user.lightTheme ? 'dark' : 'light';
+    var other = this.theme === 'dark' ? 'light' : 'dark';
     window.location.href = config.appSubUrl + '/styleguide?theme=' + other;
   }
 
