efca38d7f99f4471f83fa096a15f9b02f5e8f303
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-02-22 10:17:35 +0100

    ux(): worked on new popover

--- public/app/core/components/popover/popover.ts
+++ public/app/core/components/popover/popover.ts
@@ -8,15 +8,44 @@ import Drop from 'tether-drop';
 export function popoverDirective() {
   return {
     restrict: 'E',
-    link: function(scope, elem, attrs) {
+    transclude: true,
+    link: function(scope, elem, attrs, ctrl, transclude) {
       var inputElem = elem.prev();
-      console.log(inputElem);
-      var drop = new Drop({
-        target: inputElem[0],
-        content: 'Welcome to the future!',
-        position: 'right middle',
-        classes: 'drop-theme-arrows-bounce-dark',
-        openOn: 'click'
+      if (inputElem.length === 0) {
+        console.log('Failed to find input element for popover');
+        return;
+      }
+
+
+      transclude(function(clone, newScope) {
+        var content = document.createElement("div");
+        _.each(clone, (node) => {
+          content.appendChild(node);
+        });
+
+        var drop = new Drop({
+          target: inputElem[0],
+          content: content,
+          position: 'right middle',
+          classes: 'drop-popover',
+          openOn: 'click',
+          tetherOptions: {
+            offset: "0 -10px"
+          }
+        });
+
+      // inputElem.on('focus.popover', function() {
+      //   drop.open();
+      // });
+      //
+      // inputElem.on('blur.popover', function() {
+      //   close();
+      // });
+
+        scope.$on('$destroy', function() {
+          drop.destroy();
+        });
+
       });
     }
   };
