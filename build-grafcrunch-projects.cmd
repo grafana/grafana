00d92cb3ab3ce678cf86c5af2fd6cddb3bc92c31
Bogusław Gorczyca <boguslaw@gorczyca.name>
2016-03-18 14:11:14 +0100

    GrafCrunch builder has been modified

---
+++ build-grafcrunch-projects.cmd
@@ -0,0 +1,25 @@
+@ECHO OFF
+SET CurrentDir=%cd%
+
+echo BUILDING GrafCrunch Server
+call go run build.go build
+call :CheckError "COMPILE GRAFCRUNCH SERVER"
+
+echo BUILDING GrafCrunch Client
+call grunt release
+call :CheckError "GRAFCRUNCH CLIENT"
+
+goto EndOfScript
+
+:CheckError
+  if %ERRORLEVEL% NEQ 0 goto BuildError
+goto :eof
+
+:BuildError
+  @SET MESSAGE=%~1
+  ECHO BUILD ERROR: %MESSAGE%
+  call cd %CurrentDir%
+  EXIT 1
+goto :eof
+
+:EndOfScript
