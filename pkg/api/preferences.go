5e431149cd5f69c2ababcc77b64ab4615eb663b2
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-03-20 11:54:24 +0100

    feat(preferences): removed unused api prefernce methods, these need to be abstracted more from an http api perpsective, they are too low level for the http api, #4399

--- pkg/api/preferences.go
+++ pkg/api/preferences.go
@@ -1,43 +1,10 @@
 package api
 
 import (
-	"github.com/grafana/grafana/pkg/api/dtos"
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/middleware"
 	m "github.com/grafana/grafana/pkg/models"
 )
-
-// PUT /api/preferences
-func SavePreferences(c *middleware.Context, cmd m.SavePreferencesCommand) Response {
-
-	cmd.UserId = c.UserId
-	cmd.OrgId = c.OrgId
-
-	if err := bus.Dispatch(&cmd); err != nil {
-		return ApiError(500, "Failed to save preferences", err)
-	}
-
-	return ApiSuccess("Preferences saved")
-
-}
-
-// GET /api/preferences
-func GetPreferences(c *middleware.Context) {
-
-	query := m.GetPreferencesQuery{UserId: c.UserId, OrgId: c.OrgId}
-
-	if err := bus.Dispatch(&query); err != nil {
-		c.JsonApiErr(500, "Failed to get preferences", err)
-	}
-
-	dto := dtos.Preferences{
-		HomeDashboardId: query.Result.HomeDashboardId,
-		Timezone:        query.Result.Timezone,
-		Theme:           query.Result.Theme,
-	}
-
-	c.JSON(200, dto)
-}
 
 // POST /api/preferences/set-home-dash
 func SetHomeDashboard(c *middleware.Context, cmd m.SavePreferencesCommand) Response {
@@ -50,5 +17,4 @@ func SetHomeDashboard(c *middleware.Context, cmd m.SavePreferencesCommand) Respo
 	}
 
 	return ApiSuccess("Home dashboard set")
-
 }
