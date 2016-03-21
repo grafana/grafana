a2a07e232beaf621ac02296de4df0bc5b437d24e
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-03-21 21:24:06 +0100

    ux(): created new gf-form-switch directive clean/refactoring

---
+++ public/app/core/components/switch.ts
@@ -0,0 +1,47 @@
+///<reference path="../../headers/common.d.ts" />
+
+import config from 'app/core/config';
+import _ from 'lodash';
+import $ from 'jquery';
+import coreModule from 'app/core/core_module';
+
+var template = `
+<label for="check-{{$id}}" class="gf-form-label {{ctrl.labelClass}} pointer">{{ctrl.label}}</label>
+<div class="gf-form-switch {{ctrl.switchClass}}" >
+  <input id="check-{{$id}}" type="checkbox" ng-model="ctrl.checked" ng-change="ctrl.internalOnChange()">
+  <label for="check-{{$id}}" data-on="Yes" data-off="No"></label>
+</div>
+`;
+
+export class SwitchCtrl {
+  onChange: any;
+
+  internalOnChange() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.onChange();
+        resolve();
+      });
+    });
+  }
+
+}
+
+export function switchDirective() {
+  return {
+    restrict: 'E',
+    controller: SwitchCtrl,
+    controllerAs: 'ctrl',
+    bindToController: true,
+    scope: {
+      checked: "=",
+      label: "@",
+      labelClass: "@",
+      switchClass: "@",
+      onChange: "&",
+    },
+    template: template,
+  };
+}
+
+coreModule.directive('gfFormSwitch', switchDirective);
