40791985884c55fbd1e4cce1ae0a568ffbac3933
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-03-29 11:01:00 +0200

    ux(): dashboard settings form polish, added on hover tooltip for checkbox directive

--- public/app/core/components/switch.ts
+++ public/app/core/components/switch.ts
@@ -4,6 +4,7 @@ import config from 'app/core/config';
 import _ from 'lodash';
 import $ from 'jquery';
 import coreModule from 'app/core/core_module';
+import Drop from 'tether-drop';
 
 var template = `
 <label for="check-{{$id}}" class="gf-form-label {{ctrl.labelClass}} pointer">{{ctrl.label}}</label>
@@ -44,10 +45,27 @@ export function switchDirective() {
       checked: "=",
       label: "@",
       labelClass: "@",
+      tooltip: "@",
       switchClass: "@",
       onChange: "&",
     },
     template: template,
+    link: (scope, elem) => {
+      if (scope.ctrl.tooltip) {
+        var drop = new Drop({
+          target: elem[0],
+          content: scope.ctrl.tooltip,
+          position: "right middle",
+          classes: 'drop-help',
+          openOn: 'hover',
+          hoverOpenDelay: 400,
+        });
+
+        scope.$on('$destroy', function() {
+          drop.destroy();
+        });
+      }
+    }
   };
 }
 
