892e79a9827e74e784117005929c162062c0d448
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-04-04 13:43:13 -0400

    fix(table): another fix for table panel sorting, fixes #4544

--- public/app/plugins/panel/table/module.ts
+++ public/app/plugins/panel/table/module.ts
@@ -114,9 +114,13 @@ class TablePanelCtrl extends MetricsPanelCtrl {
       }
     }
 
+    this.render();
+  }
+
+  render() {
     this.table = transformDataToTable(this.dataRaw, this.panel);
     this.table.sort(this.panel.sort);
-    this.render(this.table);
+    return super.render(this.table);
   }
 
   toggleColumnSort(col, colIndex) {
@@ -130,10 +134,7 @@ class TablePanelCtrl extends MetricsPanelCtrl {
       this.panel.sort.col = colIndex;
       this.panel.sort.desc = true;
     }
-
-    this.table = transformDataToTable(this.dataRaw, this.panel);
-    this.table.sort(this.panel.sort);
-    this.render(this.table);
+    this.render();
   }
 
   exportCsv() {
