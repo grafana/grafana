67c13962feee9c5c4b5c04ec4443745306187c2f
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-04-09 15:32:41 -0400

    feat(pluginlist panel): updates to pluginlist panel, hooked up plugin type categories

--- public/app/plugins/panel/pluginlist/module.ts
+++ public/app/plugins/panel/pluginlist/module.ts
@@ -12,6 +12,7 @@ class DashListCtrl extends PanelCtrl {
   static templateUrl = 'module.html';
 
   pluginList: any[];
+  viewModel: any;
 
   /** @ngInject */
   constructor($scope, $injector, private backendSrv) {
@@ -20,6 +21,11 @@ class DashListCtrl extends PanelCtrl {
 
     this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
     this.pluginList = [];
+    this.viewModel = [
+      {header: "Installed Apps", list: [], type: 'app'},
+      {header: "Installed Panels", list: [], type: 'panel'},
+      {header: "Installed Datasources", list: [], type: 'datasource'},
+    ];
     this.update();
   }
 
@@ -31,6 +37,9 @@ class DashListCtrl extends PanelCtrl {
   update() {
     this.backendSrv.get('api/plugins', {embedded: 0, core: 0}).then(plugins => {
       this.pluginList = plugins;
+      this.viewModel[0].list = _.filter(plugins, {type: 'app'});
+      this.viewModel[1].list = _.filter(plugins, {type: 'panel'});
+      this.viewModel[2].list = _.filter(plugins, {type: 'datasource'});
 
       for (let plugin of this.pluginList) {
         if (!plugin.enabled) {
