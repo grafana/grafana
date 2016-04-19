8044dd1064aed857cc1237168ce8b8f339d58b6f
bergquist <carl.bergquist@gmail.com>
2016-04-19 14:46:03 +0200

    fix(cli): fixes missplaced %

--- pkg/cmd/grafana-cli/commands/install_command.go
+++ pkg/cmd/grafana-cli/commands/install_command.go
@@ -166,7 +166,7 @@ func downloadFile(pluginName, filePath, url string) (err error) {
 
 			src, err := zf.Open()
 			if err != nil {
-				log.Errorf("%Failed to open zipfile: v", err)
+				log.Errorf("Failed to extract file: %v", err)
 			}
 
 			io.Copy(dst, src)
