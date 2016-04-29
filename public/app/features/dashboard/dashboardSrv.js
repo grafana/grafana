749e419a239f3a64ddbc102d5f55a174a861aa70
utkarshcmu <utkarsh.cmu@gmail.com>
2016-04-29 06:59:05 -0700

    Small improvement

--- public/app/features/dashboard/dashboardSrv.js
+++ public/app/features/dashboard/dashboardSrv.js
@@ -184,7 +184,7 @@ function (angular, $, _, moment) {
     p.formatDate = function(date, format) {
       date = moment.isMoment(date) ? date : moment(date);
       format = format || 'YYYY-MM-DD HH:mm:ss';
-      this.timezone = this.timezone ||  contextSrv.user.timezone;
+      this.timezone = this.getTimezone();
 
       return this.timezone === 'browser' ?
         moment(date).format(format) :
