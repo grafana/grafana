891833923d0adbdc0e360a4bfb76f5f85637f9b4
fg2it <fgpg2it@gmail.com>
2016-05-04 02:32:57 +0000

    fix gofmt

--- pkg/components/renderer/renderer.go
+++ pkg/components/renderer/renderer.go
@@ -73,7 +73,7 @@ func RenderToPng(params *RenderOpts) (string, error) {
 		if err := cmd.Process.Kill(); err != nil {
 			log.Error(4, "failed to kill: %v", err)
 		}
-		return "", fmt.Errorf("PhantomRenderer::renderToPng timeout (>%vs)",timeout)
+		return "", fmt.Errorf("PhantomRenderer::renderToPng timeout (>%vs)", timeout)
 	case <-done:
 	}
 
