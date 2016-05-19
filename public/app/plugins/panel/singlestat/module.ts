046f799d315caa420b5a8d6664321713d323bdb3
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-05-19 13:26:57 +0900

    (singlestat) add support alias expansion

--- public/app/plugins/panel/singlestat/module.ts
+++ public/app/plugins/panel/singlestat/module.ts
@@ -201,10 +201,18 @@ class SingleStatCtrl extends MetricsPanelCtrl {
       }
     }
 
+    var self = this;
+    var replaceAlias = function(map) {
+      var replacedMap = angular.copy(map);
+      replacedMap.text = replacedMap.text.replace(/{{alias}}/, self.series[0].alias);
+      return replacedMap;
+    };
+    var valueMaps = this.panel.valueMaps.map(replaceAlias);
+    var rangeMaps = this.panel.rangeMaps.map(replaceAlias);
     // check value to text mappings if its enabled
     if (this.panel.mappingType === 1) {
-      for (var i = 0; i < this.panel.valueMaps.length; i++) {
-        var map = this.panel.valueMaps[i];
+      for (var i = 0; i < valueMaps.length; i++) {
+        var map = valueMaps[i];
         // special null case
         if (map.value === 'null') {
           if (data.value === null || data.value === void 0) {
@@ -222,8 +230,8 @@ class SingleStatCtrl extends MetricsPanelCtrl {
         }
       }
     } else if (this.panel.mappingType === 2) {
-      for (var i = 0; i < this.panel.rangeMaps.length; i++) {
-        var map = this.panel.rangeMaps[i];
+      for (var i = 0; i < rangeMaps.length; i++) {
+        var map = rangeMaps[i];
         // special null case
         if (map.from === 'null' && map.to === 'null') {
           if (data.value === null || data.value === void 0) {
