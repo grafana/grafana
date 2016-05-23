6a272ca5908fb6eb368009c8089bfc82795c1ad6
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-05-23 18:03:17 +0200

    feat(prometheus): progress on new prometheus query editor, #5117

--- public/app/plugins/datasource/prometheus/specs/query_specs.ts
+++ public/app/plugins/datasource/prometheus/specs/query_specs.ts
@@ -2,7 +2,7 @@ import {describe, beforeEach, it, sinon, expect} from 'test/lib/common';
 
 import {PromQuery} from '../prom_query';
 
-describe.only('PromQuery', function() {
+describe('PromQuery', function() {
   var templateSrv = {replace: val => val};
 
   describe('render series with mesurement only', function() {
@@ -17,6 +17,21 @@ describe.only('PromQuery', function() {
 
       var queryText = query.render();
       expect(queryText).to.be('rate(cpu[5m])');
+    });
+  });
+
+  describe('render series with group by label', function() {
+    it('should generate correct query', function() {
+      var query = new PromQuery({
+        metric: 'cpu',
+        functions: [
+          {type: 'sum', params: []},
+          {type: 'by', params: ['app']},
+        ]
+      }, templateSrv, {});
+
+      var queryText = query.render();
+      expect(queryText).to.be('sum(cpu) by(app)');
     });
   });
 
