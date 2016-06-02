0bea0cc5b9fc7734f20e17d51a7f9a966b454dce
bergquist <carl.bergquist@gmail.com>
2016-06-02 16:34:25 +0200

    feat(alerting): add interface for alert backend

--- pkg/services/alerting/datasources/backends.go
+++ pkg/services/alerting/datasources/backends.go
@@ -2,9 +2,16 @@ package graphite
 
 import (
 	"fmt"
+
 	m "github.com/grafana/grafana/pkg/models"
 )
 
+// AlertDatasource is bacon
+type AlertDatasource interface {
+	GetSeries(job *m.AlertJob) (m.TimeSeriesSlice, error)
+}
+
+// GetSeries returns timeseries data from the datasource
 func GetSeries(job *m.AlertJob) (m.TimeSeriesSlice, error) {
 	if job.Datasource.Type == m.DS_GRAPHITE {
 		return GraphiteClient{}.GetSeries(job)
