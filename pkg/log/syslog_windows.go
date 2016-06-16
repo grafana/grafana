2aeb4d16a4138a458dee756fbbad22146c4bfce1
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-06-16 21:10:30 +0200

    fix(build): another windows build issue

--- pkg/log/syslog_windows.go
+++ pkg/log/syslog_windows.go
@@ -17,3 +17,6 @@ func NewSyslog(sec *ini.Section, format log15.Format) *SysLogHandler {
 func (sw *SysLogHandler) Log(r *log15.Record) error {
 	return nil
 }
+
+func (sw *SysLogHandler) Close() {
+}
