90a152f65b0f3bb07200386ee2db18871682d9fc
slqgsm <brmundt10@gmail.com>
2016-06-30 15:15:35 +0000

    added hex units formatter

--- public/app/core/utils/kbn.js
+++ public/app/core/utils/kbn.js
@@ -368,6 +368,26 @@ function($, _, moment) {
     return kbn.toFixed(100*size, decimals) + '%';
   };
 
+  /* Formats the value to hex. Uses float if specified decimals are not 0.
+   * There are two options, one with 0x, and one without */
+
+  kbn.valueFormats.hex = function(value, decimals) {
+    if (value == null) { return ""; }
+    if (decimals === 0) {
+      return Math.floor(value).toString(16).toUpperCase();
+    }
+    return parseFloat(kbn.toFixed(value, decimals)).toString(16).toUpperCase();
+  };
+
+  kbn.valueFormats.hex0x = function(value, decimals) {
+    if (value == null) { return ""; }
+    var hexString = kbn.valueFormats.hex(value, decimals);
+    if (hexString.chartAt(0) === "-") {
+      return "-0x" + hexString.substring(1, -1);
+    }
+    return "0x" + hexString;
+  };
+
   // Currencies
   kbn.valueFormats.currencyUSD = kbn.formatBuilders.currency('$');
   kbn.valueFormats.currencyGBP = kbn.formatBuilders.currency('£');
@@ -634,6 +654,8 @@ function($, _, moment) {
           {text: 'days (d)',          value: 'd'    },
           {text: 'duration (ms)',     value: 'dtdurationms' },
           {text: 'duration (s)',      value: 'dtdurations' }
+          {text: 'hex (0x)',          value: 'hex0x'      },
+          {text: 'hex',               value: 'hex'        }
         ]
       },
       {
