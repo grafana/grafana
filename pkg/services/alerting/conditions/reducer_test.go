95c1a4a936e5a0672da201731089f0edcaa13121
bergquist <carl.bergquist@gmail.com>
2016-08-10 14:06:17 +0200

    feat(alerting): implement more simple reducers

--- pkg/services/alerting/conditions/reducer_test.go
+++ pkg/services/alerting/conditions/reducer_test.go
@@ -8,11 +8,32 @@ import (
 )
 
 func TestSimpleReducer(t *testing.T) {
-	Convey("Test simple reducer", t, func() {
-		Convey("can calculate avg of time serie", func() {
+	Convey("Test simple reducer by calculating", t, func() {
+		Convey("avg", func() {
 			result := testReducer("avg", 1, 2, 3)
 			So(result, ShouldEqual, float64(2))
 		})
+
+		Convey("sum", func() {
+			result := testReducer("sum", 1, 2, 3)
+			So(result, ShouldEqual, float64(6))
+		})
+
+		Convey("min", func() {
+			result := testReducer("min", 3, 2, 1)
+			So(result, ShouldEqual, float64(1))
+		})
+
+		Convey("max", func() {
+			result := testReducer("max", 1, 2, 3)
+			So(result, ShouldEqual, float64(3))
+		})
+
+		Convey("mean odd numbers", func() {
+			result := testReducer("mean", 1, 2, 3000)
+			So(result, ShouldEqual, float64(2))
+		})
+
 	})
 }
 
