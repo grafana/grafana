b66d94343ed12f713429a44d349322728e4bf29b
bergquist <carl.bergquist@gmail.com>
2016-09-30 07:35:48 +0200

    tech(tsdb): add basic skeleton for alerting

---
+++ pkg/tsdb/opentsdb/opentsdb.go
@@ -0,0 +1,30 @@
+package opentsdb
+
+import (
+	"net/http"
+
+	"github.com/grafana/grafana/pkg/log"
+	"github.com/grafana/grafana/pkg/tsdb"
+)
+
+type OpenTsdbExecutor struct {
+	*tsdb.DataSourceInfo
+}
+
+func NewOpenTsdbExecutorExecutor(dsInfo *tsdb.DataSourceInfo) tsdb.Executor {
+	return &OpenTsdbExecutor{dsInfo}
+}
+
+var (
+	plog       log.Logger
+	HttpClient http.Client
+)
+
+func init() {
+	plog = log.New("tsdb.opentsdb")
+	tsdb.RegisterExecutor("opentsdb", NewOpenTsdbExecutorExecutor)
+}
+
+func (e *OpenTsdbExecutor) Execute(queries tsdb.QuerySlice, queryContext *tsdb.QueryContext) *tsdb.BatchResult {
+	panic("Missing implementation")
+}
