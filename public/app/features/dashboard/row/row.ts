bda5cdcb2fd0cd14b2fc413550ac48f9f9a1b576
Torkel Ödegaard <torkel@grafana.org>
2016-10-29 13:14:37 +0200

    ux(dash edit mode): fix for row height when collapsed

--- public/app/features/dashboard/row/row.ts
+++ public/app/features/dashboard/row/row.ts
@@ -105,7 +105,7 @@ export function rowDirective($rootScope) {
       row: "=",
     },
     link: function(scope, element) {
-      scope.$watchGroup(['ctrl.row.height'], function() {
+      scope.$watchGroup(['ctrl.row.collapse', 'ctrl.row.height'], function() {
         element.css({minHeight: scope.ctrl.row.collapse ? '5px' : scope.ctrl.row.height});
       });
 
