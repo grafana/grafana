2088363bf9979ca55879d6ab99736182d6731ac6
utkarshcmu <utkarsh.cmu@gmail.com>
2016-11-02 00:00:18 -0700

    Added tests for graphite alerting

--- pkg/tsdb/graphite/graphite_test.go
+++ pkg/tsdb/graphite/graphite_test.go
@@ -1,1 +1,47 @@
 package graphite
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestGraphiteFunctions(t *testing.T) {
+	Convey("Testing Graphite Executor", t, func() {
+
+		Convey("formatting time range for now", func() {
+
+			timeRange := formatTimeRange("now")
+			So(timeRange, ShouldEqual, "now")
+
+		})
+
+		Convey("formatting time range for now-1m", func() {
+
+			timeRange := formatTimeRange("now-1m")
+			So(timeRange, ShouldEqual, "now-1min")
+
+		})
+
+		Convey("formatting time range for now-1M", func() {
+
+			timeRange := formatTimeRange("now-1M")
+			So(timeRange, ShouldEqual, "now-1mon")
+
+		})
+
+		Convey("fix interval format in query for 1m", func() {
+
+			timeRange := formatTimeRange("aliasByNode(hitcount(averageSeries(app.grafana.*.dashboards.views.count), '1m'), 4)")
+			So(timeRange, ShouldEqual, "aliasByNode(hitcount(averageSeries(app.grafana.*.dashboards.views.count), '1min'), 4)")
+
+		})
+
+		Convey("fix interval format in query for 1M", func() {
+
+			timeRange := formatTimeRange("aliasByNode(hitcount(averageSeries(app.grafana.*.dashboards.views.count), '1M'), 4)")
+			So(timeRange, ShouldEqual, "aliasByNode(hitcount(averageSeries(app.grafana.*.dashboards.views.count), '1mon'), 4)")
+
+		})
+
+	})
+}
