c8d8c8607ac4f6c882fb056c6705a3a3a3510473
hehaiyuan <hehaiyuan.hhy@gmail.com>
2016-11-23 15:40:53 +0800

    add time shift  for prediction data

--- public/app/features/panel/panelHelper.js
+++ public/app/features/panel/panelHelper.js
@@ -97,6 +97,15 @@ function (angular, dateMath, rangeUtil, _, kbn, $) {
         scope.rangeRaw = scope.range;
       }
 
+      if(scope.panel.timeForward) {
+        var timeShift = '+' + scope.panel.timeForward;
+        scope.panelMeta.timeInfo += ' timeshift ' + timeShift;
+        scope.range.from = dateMath.parseDateMath(timeShift, scope.range.from, false);
+        scope.range.to = dateMath.parseDateMath(timeShift, scope.range.to, true);
+
+        scope.rangeRaw = scope.range;
+      }
+
       if (scope.panel.hideTimeOverride) {
         scope.panelMeta.timeInfo = '';
       }
