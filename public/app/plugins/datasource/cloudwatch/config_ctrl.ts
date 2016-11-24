1695aece0c59f58375079aa13b8157845781b9e8
bergquist <carl.bergquist@gmail.com>
2016-11-24 08:39:36 +0100

    feat(cloudwatch): add authtype dropdown to config page

---
+++ public/app/plugins/datasource/cloudwatch/config_ctrl.ts
@@ -0,0 +1,31 @@
+///<reference path="../../../headers/common.d.ts" />
+
+import angular from 'angular';
+import _ from 'lodash';
+
+export class CloudWatchConfigCtrl {
+  static templateUrl = 'partials/config.html';
+  current: any;
+
+  /** @ngInject */
+  constructor($scope) {
+    this.current.jsonData.timeField = this.current.jsonData.timeField || '@timestamp';
+    this.current.jsonData.authType = this.current.jsonData.authType || 'credentials';
+  }
+
+  authTypes = [
+    {name: 'Access & secret key', value: 'keys'},
+    {name: 'Credentials file', value: 'credentials'},
+    {name: 'ARN', value: 'arn'},
+  ];
+
+  indexPatternTypes = [
+    {name: 'No pattern',  value: undefined},
+    {name: 'Hourly',      value: 'Hourly',  example: '[logstash-]YYYY.MM.DD.HH'},
+    {name: 'Daily',       value: 'Daily',   example: '[logstash-]YYYY.MM.DD'},
+    {name: 'Weekly',      value: 'Weekly',  example: '[logstash-]GGGG.WW'},
+    {name: 'Monthly',     value: 'Monthly', example: '[logstash-]YYYY.MM'},
+    {name: 'Yearly',      value: 'Yearly',  example: '[logstash-]YYYY'},
+  ];
+}
+
