747ea82b5f83101ad6d78fea12efba93cf17e76a
hehaiyuan <hehaiyuan.hhy@gmail.com>
2016-12-13 19:16:10 +0800

    add client_id  and can dynamic chage  metrice server

--- public/app/controllers/installCtrl.js
+++ public/app/controllers/installCtrl.js
@@ -8,7 +8,7 @@ define([
 
     var module = angular.module('grafana.controllers');
 
-    module.controller('InstallCtrl', function ($scope, backendSrv) {
+    module.controller('InstallCtrl', function ($scope, backendSrv, datasourceSrv, contextSrv) {
       $scope.installSelect = {
         system: 0,
         service: ""
@@ -16,6 +16,12 @@ define([
       //would be hard code for a while
       $scope.services = ["Hadoop", "zookeeper", "JMX", "Mysql"];
       $scope.platform = ["Window", "Linux"];
+      $scope.orgId = contextSrv.user.orgId;
+      datasourceSrv.get("opentsdb").then(function (ds) {
+        var url = document.createElement('a');
+        url.href = ds.url;
+        $scope.metricsServer = url.hostname;
+      });
       $scope.changeToken = function () {
         _.each(backendSrv.tokens, function (token) {
           if (token.name == $scope.installSelect.system) {
