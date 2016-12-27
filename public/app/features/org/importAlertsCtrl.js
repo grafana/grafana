f222528fdb8a0b802a39172b25eb8d6c6388f46c
hehaiyuan <hehaiyuan.hhy@gmail.com>
2016-12-27 16:15:38 +0800

    add promise function  wait for all the request return

--- public/app/features/org/importAlertsCtrl.js
+++ public/app/features/org/importAlertsCtrl.js
@@ -5,7 +5,7 @@ define([
   function (angular, _) {
     'use strict';
     var module = angular.module('grafana.controllers');
-    module.controller('ImportAlertsCtrl', function($scope, alertMgrSrv, alertSrv, contextSrv) {
+    module.controller('ImportAlertsCtrl', function($q, $scope, alertMgrSrv, alertSrv, contextSrv) {
       $scope.importJson = function () {
         var files = angular.element("#alertUpload")[0].files;
         for (var i = 0, f; f = files[i]; i++) {
@@ -16,7 +16,8 @@ define([
                 var alertDefs = JSON.parse(e.target.result);
                 if (_.isNull(alertDefs[0]) || _.isNull(alertDefs[0].alertDetails))
                   throw "Wrong json";
-                _.each(alertDefs, function (target) {
+                var promiseArr = [];
+                _.each(alertDefs, function (target, i) {
                   var newAlert = {};
                   var milliseconds = (new Date).getTime();
                   newAlert.org = contextSrv.user.orgId;
@@ -26,21 +27,26 @@ define([
                   newAlert.name = target.name;
                   newAlert.creationTime = milliseconds;
                   newAlert.modificationTime = milliseconds;
-                  alertMgrSrv.save(newAlert).then(function onSuccess() {
-                    //nothing to
+                  var p = alertMgrSrv.save(newAlert).then(function onSuccess() {
+                    return i;
                   }, function onFailed(response) {
                     $scope.appEvent('alert-error', ['ERROR', response.status + " " + (response.data || "Request failed")]);
                     throw "Request failed";
                   });
+                  promiseArr.push(p);
                 });
-                $scope.appEvent('alert-success', ['导入成功', '共导入' + alertDefs.length + '个报警设置']);
+                $q.all(promiseArr).then(function(values){
+                  if(values.length == alertDefs.length){
+                    $scope.init();
+                    $scope.dismiss();
+                    $scope.appEvent('alert-success', ['导入成功', '共导入' + alertDefs.length + '个报警设置']);
+                  }
+                });
               } catch (err) {
                 console.log(err);
                 $scope.appEvent('alert-error', ['导入 失败', 'JSON -> JS Serialization failed: ' + err.message]);
-                return;
-              } finally {
                 $scope.dismiss();
-                $scope.init();
+                return;
               }
             };
           })(f);
