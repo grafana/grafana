0719a66d47062fef8a2f3fe74f9a84a663fbb1e1
bergquist <carl.bergquist@gmail.com>
2017-01-10 15:06:48 +0100

    tech(build): updates circle ci trigger scripts

--- scripts/trigger_grafana_packer.sh
+++ scripts/trigger_grafana_packer.sh
@@ -2,9 +2,31 @@
 
 _circle_token=$1
 
-trigger_build_url=https://circleci.com/api/v1/project/grafana/grafana-packer/tree/master?circle-token=${_circle_token}
+trigger_build_url=https://circleci.com/api/v1/project/grafana/grafana-packer/tree/v4.1.x?circle-token=${_circle_token}
+
+post_data=$(cat <<EOF
+{
+  "build_parameters": {
+    "BRANCH": "v4.1.x"
+  }
+}
+EOF
+)
+
+echo ${post_data}
 
 curl \
 --header "Accept: application/json" \
 --header "Content-Type: application/json" \
+--data "${post_data}" \
 --request POST ${trigger_build_url}
+
+#curl \
+#--header "Accept: application/json" \
+#--header "Content-Type: application/json" \
+#-X POST -d '{ "build_parameters": { "BRANCH": "v4.1.x"} }' \
+#${trigger_build_url}
+
+#--request POST ${trigger_build_url}
+
+
