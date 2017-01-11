bd8899b37db452442ca55d36c014ee3537b78db9
Torkel Ödegaard <torkel@grafana.org>
2017-01-11 15:22:31 +0100

    fix(graph): fixed table legend min-height, fixes #7221

--- public/app/plugins/panel/graph/legend.js
+++ public/app/plugins/panel/graph/legend.js
@@ -198,7 +198,7 @@ function (angular, _, $) {
 
             var topPadding = 6;
             var tbodyElem = $('<tbody></tbody>');
-            tbodyElem.css("height", maxHeight - topPadding);
+            tbodyElem.css("max-height", maxHeight - topPadding);
             tbodyElem.append(tableHeaderElem);
             tbodyElem.append(seriesElements);
             $container.append(tbodyElem);
