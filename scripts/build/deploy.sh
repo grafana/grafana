6b70fb27294f406ac7d861942426dbb1aca68915
bergquist <carl.bergquist@gmail.com>
2017-01-23 18:02:50 +0100

    tech(build): fixes broken yum installer

--- scripts/build/deploy.sh
+++ scripts/build/deploy.sh
@@ -4,5 +4,5 @@ mkdir -p dist
 
 docker run -i -t --name gfbuild \
   -v $(pwd)/dist:/tmp/dist \
-  -e "GRAFANA_BRANCH=${GRAFANA_BRANCH}" \
+  -e "GRAFANA_BRANCH=${CIRCLE_BRANCH}" \
   grafana/buildcontainer
