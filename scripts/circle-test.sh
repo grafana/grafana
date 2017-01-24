3d6698389b5964aa83f8feda4e3a72b4f6ac69dd
bergquist <carl.bergquist@gmail.com>
2017-01-24 13:58:37 +0100

    tech(build): adds s3 bucket for nightly

--- scripts/circle-test.sh
+++ scripts/circle-test.sh
@@ -18,12 +18,8 @@ yarn install --pure-lockfile
 
 exit_if_fail npm test
 
-#test -z "$(gofmt -s -l ./pkg/... | tee /dev/stderr)"
 exit_if_fail test -z "$(gofmt -s -l ./pkg/... | tee /dev/stderr)"
-
-#exit_if_fail go run build.go setup
 exit_if_fail go run build.go build
-
 exit_if_fail go vet ./pkg/...
 exit_if_fail go test -v ./pkg/...
 
