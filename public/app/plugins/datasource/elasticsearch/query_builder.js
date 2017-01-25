4e1d8c9ced423766d9831970c7b08c380f89138c
Naoufel JAMALI <naoufelfalconjamali@gmail.com>
2017-01-25 11:29:41 +0000

    Elasticsearch - Term aggregation limit fix  (should fix #7112) (#7338)
    * Elasticsearch - Term aggregation limit fix  (should fix #7112)
    
    * Elasticsearch - Term aggregation limit fix

--- public/app/plugins/datasource/elasticsearch/query_builder.js
+++ public/app/plugins/datasource/elasticsearch/query_builder.js
@@ -29,7 +29,6 @@ function (queryDef) {
     }
 
     queryNode.terms.size = parseInt(aggDef.settings.size, 10) === 0 ? 500 : parseInt(aggDef.settings.size, 10);
-
     if (aggDef.settings.orderBy !== void 0) {
       queryNode.terms.order = {};
       queryNode.terms.order[aggDef.settings.orderBy] = aggDef.settings.order;
@@ -243,19 +242,21 @@ function (queryDef) {
         }
       });
     }
-
+    var size = 500;
+    if(queryDef.size){
+      size = queryDef.size;
+    }
     query.aggs =  {
       "1": {
         "terms": {
           "field": queryDef.field,
-          "size": 500,
+          "size": size,
           "order": {
             "_term": "asc"
           }
         },
       }
     };
-
     return query;
   };
 
