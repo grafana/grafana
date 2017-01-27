80b727bf20d3e30de9c131b23cf3fc0648796af9
Leandro Piccilli <piccilli@gmail.com>
2017-01-27 20:19:54 +0100

    Set time when testing ES datasource, fixes #7044

--- public/app/plugins/datasource/elasticsearch/datasource.js
+++ public/app/plugins/datasource/elasticsearch/datasource.js
@@ -168,6 +168,7 @@ function (angular, _, moment, kbn, ElasticQueryBuilder, IndexPattern, ElasticRes
     };
 
     this.testDatasource = function() {
+      timeSrv.setTime({ from: 'now-1m', to: 'now' });
       return this._get('/_stats').then(function() {
         return { status: "success", message: "Data source is working", title: "Success" };
       }, function(err) {
