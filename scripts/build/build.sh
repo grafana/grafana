d72f257f2775f61245ff97560c65271da4b1554d
bergquist <carl.bergquist@gmail.com>
2017-01-30 17:27:12 +0100

    tech(build): disable package iteration for release builds

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -10,18 +10,27 @@ REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 mkdir -p /go/src/github.com/grafana
 cd /go/src/github.com/grafana
 
-if [ -n "${CIRCLE_TAG}" ]; then
-  echo "Building from tag ${CIRCLE_TAG}"
-  git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
-  cd $REPO_PATH
-else
-  echo "Building from branch ${CIRCLE_BRANCH}"
+if [ "$CIRCLE_TAG" != "" ]; then
+  echo "Builing from tag $CIRCLE_TAG"
   git clone https://github.com/grafana/grafana.git
   cd $REPO_PATH
   git checkout $CIRCLE_TAG
+else
+  echo "Building from branch $CIRCLE_BRANCH"
+  git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
+  cd $REPO_PATH
 fi
 
-go run build.go build
+echo "current dir: $(pwd)"
+
+if [ "$CIRCLE_TAG" != "" ]; then
+  echo "Building incremental build for master"
+  go run build.go build
+else
+  echo "Building a release"
+  go run build.go -includeBuildNumber=false build
+fi
+
 yarn install --pure-lockfile
 
 source /etc/profile.d/rvm.sh
@@ -29,11 +38,15 @@ rvm use 2.1.9 --default
 
 gem install fpm -v 1.4
 
-#if [ -n "${CIRCLE_TAG}" ]; then
-  #go run build.go -includeBuildNumber=false package latest
-#else
-go run build.go package latest
-#fi
+echo "current dir: $(pwd)"
+
+if [ "$CIRCLE_TAG" != "" ]; then
+  echo "Building incremental build for master"
+  go run build.go package latest
+else
+  echo "Building a release"
+  go run build.go -includeBuildNumber=false package latest
+fi
 
 cp dist/* /tmp/dist/
 
