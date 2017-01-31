436e35c84577d9b3a4ef0a3f2ad27ba2c556c780
Torkel Ödegaard <torkel@grafana.org>
2017-01-31 07:59:14 +0100

    fix(): fixed unit test broken in last PR merge

--- public/test/specs/shareModalCtrl-specs.js
+++ public/test/specs/shareModalCtrl-specs.js
@@ -41,7 +41,7 @@ define([
 
         ctx.scope.init();
         var base = 'http://dashboards.grafana.com/render/dashboard-solo/db/my-dash';
-        var params = '?from=1000&to=2000&panelId=22&width=1000&height=500';
+        var params = '?from=1000&to=2000&panelId=22&width=1000&height=500&tz=UTC%2B01%3A00';
         expect(ctx.scope.imageUrl).to.be(base + params);
       });
 
