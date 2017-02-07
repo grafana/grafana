124b30b3ba182e6acfaa66fa05095701ce6f21e5
James Regovich <jregovich@expedia.com>
2017-02-07 09:32:24 -0600

    Fix to formatting

--- pkg/services/alerting/notifiers/hipchat.go
+++ pkg/services/alerting/notifiers/hipchat.go
@@ -2,12 +2,12 @@ package notifiers
 
 import (
 	"encoding/json"
-	"time"
-	"strconv"
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/log"
 	m "github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/alerting"
+	"strconv"
+	"time"
 )
 
 func init() {
@@ -50,19 +50,19 @@ func NewHipChatNotifier(model *m.AlertNotification) (alerting.Notifier, error) {
 
 	return &HipChatNotifier{
 		NotifierBase: NewNotifierBase(model.Id, model.IsDefault, model.Name, model.Type, model.Settings),
-		Url:			url,
-		ApiKey:		apikey,
-		RoomId:		roomid,
+		Url:          url,
+		ApiKey:       apikey,
+		RoomId:       roomid,
 		log:          log.New("alerting.notifier.hipchat"),
 	}, nil
 }
 
 type HipChatNotifier struct {
 	NotifierBase
-	Url       string
-	ApiKey       string
+	Url    string
+	ApiKey string
 	RoomId string
-	log       log.Logger
+	log    log.Logger
 }
 
 func (this *HipChatNotifier) Notify(evalContext *alerting.EvalContext) error {
@@ -77,13 +77,12 @@ func (this *HipChatNotifier) Notify(evalContext *alerting.EvalContext) error {
 		return err
 	}
 
-
-	message = evalContext.GetNotificationTitle() + " in state " + evalContext.GetStateModel().Text + "<br><a href="+ruleUrl+">Check Dasboard</a>"
+	message = evalContext.GetNotificationTitle() + " in state " + evalContext.GetStateModel().Text + "<br><a href=" + ruleUrl + ">Check Dasboard</a>"
 	fields := make([]map[string]interface{}, 0)
 	fieldLimitCount := 4
 	message += "<br>"
 	for index, evt := range evalContext.EvalMatches {
-		message += evt.Metric + " :: " + strconv.FormatFloat(evt.Value.Float64,'f',-1,64) + "<br>"
+		message += evt.Metric + " :: " + strconv.FormatFloat(evt.Value.Float64, 'f', -1, 64) + "<br>"
 		fields = append(fields, map[string]interface{}{
 			"title": evt.Metric,
 			"value": evt.Value,
@@ -105,41 +104,39 @@ func (this *HipChatNotifier) Notify(evalContext *alerting.EvalContext) error {
 	if evalContext.Rule.State != m.AlertStateOK { //dont add message when going back to alert state ok.
 		message += " " + evalContext.Rule.Message
 	}
-//HipChat has a set list of colors
-	switch evalContext.Rule.State{
-		case m.AlertStateOK:
-			color = "green"
-		case m.AlertStateNoData:
-			color = "grey"
-		case m.AlertStateAlerting:
-			color = "red"
+	//HipChat has a set list of colors
+	switch evalContext.Rule.State {
+	case m.AlertStateOK:
+		color = "green"
+	case m.AlertStateNoData:
+		color = "grey"
+	case m.AlertStateAlerting:
+		color = "red"
 	}
 
-// Add a card with link to the dashboard
+	// Add a card with link to the dashboard
 	card := map[string]interface{}{
-		  "style": "link",
-		  "url": 	ruleUrl,
-		  "id": "1",
-		  "title": evalContext.GetNotificationTitle(),
-		  "description": evalContext.GetNotificationTitle() + " in state " + evalContext.GetStateModel().Text  ,
-		  "icon": map[string]interface{}{
-		    "url": "http://grafana.org/assets/img/fav32.png",
-		  },
-		  "date": time.Now().Unix(),
-		}
+		"style":       "link",
+		"url":         ruleUrl,
+		"id":          "1",
+		"title":       evalContext.GetNotificationTitle(),
+		"description": evalContext.GetNotificationTitle() + " in state " + evalContext.GetStateModel().Text,
+		"icon": map[string]interface{}{
+			"url": "http://grafana.org/assets/img/fav32.png",
+		},
+		"date": time.Now().Unix(),
+	}
 
 	body := map[string]interface{}{
-	"message":message,
-				  "notify": "true",
-					"message_format": "html",
-					"color":       color,
-					"card": card,
-				}
-	hipUrl := this.Url +"v2/room/" +this.RoomId + "/notification?auth_token=" +this.ApiKey
+		"message":        message,
+		"notify":         "true",
+		"message_format": "html",
+		"color":          color,
+		"card":           card,
+	}
+	hipUrl := this.Url + "v2/room/" + this.RoomId + "/notification?auth_token=" + this.ApiKey
 	data, _ := json.Marshal(&body)
-	cmd := &m.SendWebhookSync{Url: hipUrl , Body: string(data)}
-
-	this.log.Info("Using URL: " + hipUrl +"with body "+string(data))
+	cmd := &m.SendWebhookSync{Url: hipUrl, Body: string(data)}
 
 	if err := bus.DispatchCtx(evalContext.Ctx, cmd); err != nil {
 		this.log.Error("Failed to send hipchat notification", "error", err, "webhook", this.Name)
