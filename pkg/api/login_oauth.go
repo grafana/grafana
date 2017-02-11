056c3013f5ae055118560e8e09cc447b920530a9
Martin Palma <m@palma.bz>
2017-02-11 20:01:39 +0100

    Function oauth2.SetParam was renamed to oauth2.SetAuthURLParam in the latest golang/oauth2 package.

--- pkg/api/login_oauth.go
+++ pkg/api/login_oauth.go
@@ -56,7 +56,7 @@ func OAuthLogin(ctx *middleware.Context) {
 		if setting.OAuthService.OAuthInfos[name].HostedDomain == "" {
 			ctx.Redirect(connect.AuthCodeURL(state, oauth2.AccessTypeOnline))
 		} else {
-			ctx.Redirect(connect.AuthCodeURL(state, oauth2.SetParam("hd", setting.OAuthService.OAuthInfos[name].HostedDomain), oauth2.AccessTypeOnline))
+			ctx.Redirect(connect.AuthCodeURL(state, oauth2.SetAuthURLParam("hd", setting.OAuthService.OAuthInfos[name].HostedDomain), oauth2.AccessTypeOnline))
 		}
 		return
 	}
