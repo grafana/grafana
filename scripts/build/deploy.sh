26bb9ad399627ea5fc6fd6f116559a2860e711df
bergquist <carl.bergquist@gmail.com>
2017-02-24 09:22:57 +0100

    build: use initial checkout within container
    speedup and simplify the build within the container

--- scripts/build/deploy.sh
+++ scripts/build/deploy.sh
@@ -5,8 +5,10 @@ mkdir -p dist
 echo "Circle branch: ${CIRCLE_BRANCH}"
 echo "Circle tag: ${CIRCLE_TAG}"
 docker run -i -t --name gfbuild \
-  -v $(pwd)/dist:/tmp/dist \
+  -v $(pwd):/go/src/github.com/grafana/grafana \
   -e "CIRCLE_BRANCH=${CIRCLE_BRANCH}" \
   -e "CIRCLE_TAG=${CIRCLE_TAG}" \
   -e "CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM}" \
   grafana/buildcontainer
+
+sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER dist
