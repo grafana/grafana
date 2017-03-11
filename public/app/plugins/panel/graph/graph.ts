df1b163c4cb81325f7127eab67dcba18aa552800
thuck <denisdoria@gmail.com>
2017-03-11 14:11:37 +0100

    Proposal to feature request #6069
    This PR checks if any data point in a series is above Y-Max, or below
    Y-Min. If any of those are true a icon is displayed to indicate that the
    graph is not showing all the data.

--- public/app/plugins/panel/graph/graph.ts
+++ public/app/plugins/panel/graph/graph.ts
@@ -336,6 +336,7 @@ coreModule.directive('grafanaGraph', function($rootScope, timeSrv) {
         } else {
           callPlot(true);
         }
+        checkBoundaries(panel);
       }
 
       function translateFillOption(fill) {
@@ -553,6 +554,28 @@ coreModule.directive('grafanaGraph', function($rootScope, timeSrv) {
         }
 
         return "%H:%M";
+      }
+
+      function checkBoundaries(panel) {
+          var max, min, series, i, j;
+          panel.upperBoundary = false;
+          panel.lowerBoundary = false;
+
+          for (i = 0; i < panel.yaxes.length; i++) {
+            max = panel.yaxes[i].max;
+            min = panel.yaxes[i].min;
+            for (j = 0; j < data.length; j++) {
+              series = data[j];
+              if (max != null && series.stats.max > max) {
+                panel.upperBoundary = true;
+              }
+
+              if (min != null && series.stats.min < min) {
+                panel.lowerBoundary = true;
+              }
+            }
+          }
+
       }
 
       elem.bind("plotselected", function (event, ranges) {
