9ad37e79f74d39c89dfe0f8f67cd87a8b365264d
liuhui <liuhuiiris@126.com>
2017-03-15 15:26:29 +0800

    query report by orgId

--- public/app/features/report/reportCtrl.js
+++ public/app/features/report/reportCtrl.js
@@ -10,6 +10,10 @@ define([
 
     module.controller('ReportCtrl', function ($scope, backendSrv, contextSrv, datasourceSrv) {
       $scope.init = function () {
+        $scope.hasReport = false;
+        if(contextSrv.user.orgId === 3){
+          $scope.hasReport = true;
+        }
       };
       $scope.init();
     });
