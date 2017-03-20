be5c4f47a6a19c96d43e99bafbba7f2369470654
Torkel Ödegaard <torkel@grafana.org>
2017-03-20 14:10:44 +0100

    s3: support for alternative s3 bucket url syntax, closes #7871

--- pkg/components/imguploader/imguploader_test.go
+++ pkg/components/imguploader/imguploader_test.go
@@ -10,29 +10,66 @@ import (
 
 func TestImageUploaderFactory(t *testing.T) {
 	Convey("Can create image uploader for ", t, func() {
-		Convey("S3ImageUploader", func() {
-			var err error
+		Convey("S3ImageUploader config", func() {
 			setting.NewConfigContext(&setting.CommandLineArgs{
 				HomePath: "../../../",
 			})
 
 			setting.ImageUploadProvider = "s3"
 
-			s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
-			s3sec.NewKey("bucket_url", "https://foo.bar.baz.s3-us-east-2.amazonaws.com")
-			s3sec.NewKey("access_key", "access_key")
-			s3sec.NewKey("secret_key", "secret_key")
+			Convey("with bucket url https://foo.bar.baz.s3-us-east-2.amazonaws.com", func() {
+				s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
+				s3sec.NewKey("bucket_url", "https://foo.bar.baz.s3-us-east-2.amazonaws.com")
+				s3sec.NewKey("access_key", "access_key")
+				s3sec.NewKey("secret_key", "secret_key")
 
-			uploader, err := NewImageUploader()
+				uploader, err := NewImageUploader()
 
-			So(err, ShouldBeNil)
-			original, ok := uploader.(*S3Uploader)
+				So(err, ShouldBeNil)
+				original, ok := uploader.(*S3Uploader)
 
-			So(ok, ShouldBeTrue)
-			So(original.region, ShouldEqual, "us-east-2")
-			So(original.bucket, ShouldEqual, "foo.bar.baz")
-			So(original.accessKey, ShouldEqual, "access_key")
-			So(original.secretKey, ShouldEqual, "secret_key")
+				So(ok, ShouldBeTrue)
+				So(original.region, ShouldEqual, "us-east-2")
+				So(original.bucket, ShouldEqual, "foo.bar.baz")
+				So(original.accessKey, ShouldEqual, "access_key")
+				So(original.secretKey, ShouldEqual, "secret_key")
+			})
+
+			Convey("with bucket url https://s3.amazonaws.com/mybucket", func() {
+				s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
+				s3sec.NewKey("bucket_url", "https://s3.amazonaws.com/my.bucket.com")
+				s3sec.NewKey("access_key", "access_key")
+				s3sec.NewKey("secret_key", "secret_key")
+
+				uploader, err := NewImageUploader()
+
+				So(err, ShouldBeNil)
+				original, ok := uploader.(*S3Uploader)
+
+				So(ok, ShouldBeTrue)
+				So(original.region, ShouldEqual, "us-east-1")
+				So(original.bucket, ShouldEqual, "my.bucket.com")
+				So(original.accessKey, ShouldEqual, "access_key")
+				So(original.secretKey, ShouldEqual, "secret_key")
+			})
+
+			Convey("with bucket url https://s3-us-west-2.amazonaws.com/mybucket", func() {
+				s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
+				s3sec.NewKey("bucket_url", "https://s3-us-west-2.amazonaws.com/my.bucket.com")
+				s3sec.NewKey("access_key", "access_key")
+				s3sec.NewKey("secret_key", "secret_key")
+
+				uploader, err := NewImageUploader()
+
+				So(err, ShouldBeNil)
+				original, ok := uploader.(*S3Uploader)
+
+				So(ok, ShouldBeTrue)
+				So(original.region, ShouldEqual, "us-west-2")
+				So(original.bucket, ShouldEqual, "my.bucket.com")
+				So(original.accessKey, ShouldEqual, "access_key")
+				So(original.secretKey, ShouldEqual, "secret_key")
+			})
 		})
 
 		Convey("Webdav uploader", func() {
