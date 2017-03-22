8aa00f9227c7f40150486bb3c65536b176f47c1a
Torkel Ödegaard <torkel@grafana.com>
2017-03-22 15:07:24 +0100

    build: updated publish script

--- packaging/publish/publish_both.sh
+++ packaging/publish/publish_both.sh
@@ -1,19 +1,18 @@
 #! /usr/bin/env bash
-deb_ver=4.1.2-1486989747
-rpm_ver=4.1.2-1486989747
+version=4.2.0
 
-wget https://grafanarel.s3.amazonaws.com/builds/grafana_${deb_ver}_amd64.deb
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${version}_amd64.deb
 
-package_cloud push grafana/stable/debian/jessie grafana_${deb_ver}_amd64.deb
-package_cloud push grafana/stable/debian/wheezy grafana_${deb_ver}_amd64.deb
+package_cloud push grafana/stable/debian/jessie grafana_${version}_amd64.deb
+package_cloud push grafana/stable/debian/wheezy grafana_${version}_amd64.deb
 
-package_cloud push grafana/testing/debian/jessie grafana_${deb_ver}_amd64.deb
-package_cloud push grafana/testing/debian/wheezy grafana_${deb_ver}_amd64.deb
+package_cloud push grafana/testing/debian/jessie grafana_${version}_amd64.deb
+package_cloud push grafana/testing/debian/wheezy grafana_${version}_amd64.deb
 
-wget https://grafanarel.s3.amazonaws.com/builds/grafana-${rpm_ver}.x86_64.rpm
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-${version}-1.x86_64.rpm
 
-package_cloud push grafana/testing/el/6 grafana-${rpm_ver}.x86_64.rpm
-package_cloud push grafana/testing/el/7 grafana-${rpm_ver}.x86_64.rpm
+package_cloud push grafana/testing/el/6 grafana-${version}-1.x86_64.rpm
+package_cloud push grafana/testing/el/7 grafana-${version}-1.x86_64.rpm
 
-package_cloud push grafana/stable/el/7 grafana-${rpm_ver}.x86_64.rpm
-package_cloud push grafana/stable/el/6 grafana-${rpm_ver}.x86_64.rpm
+package_cloud push grafana/stable/el/7 grafana-${version}-1.x86_64.rpm
+package_cloud push grafana/stable/el/6 grafana-${version}-1.x86_64.rpm
