8eb119b59df987a09b02fd56d54e7397ac4a9e93
Max Kutsevol <max@kutsevol.com>
2017-03-22 21:39:16 +0200

    Allow special syntax to get value from arbitrary column in influxdb

--- public/app/plugins/datasource/influxdb/response_parser.ts
+++ public/app/plugins/datasource/influxdb/response_parser.ts
@@ -14,14 +14,25 @@ export default class ResponseParser {
 
     var influxdb11format = query.toLowerCase().indexOf('show tag values') >= 0;
 
+
+    var arbitraryColumn = 0;
+    if (query.indexOf(';;') >= 0) {
+      var part = query.substr(query.indexOf(';;'));
+      arbitraryColumn = part.split(';').length - 2;
+    }
+
     var res = {};
     _.each(influxResults.series, serie => {
       _.each(serie.values, value => {
         if (_.isArray(value)) {
-          if (influxdb11format) {
-            addUnique(res, value[1] || value[0]);
+          if (arbitraryColumn === 0) {
+            if (influxdb11format) {
+              addUnique(res, value[1] || value[0]);
+            } else {
+              addUnique(res, value[0]);
+            }
           } else {
-            addUnique(res, value[0]);
+            addUnique(res, value[arbitraryColumn]);
           }
         } else {
           addUnique(res, value);
@@ -30,7 +41,7 @@ export default class ResponseParser {
     });
 
     return _.map(res, value => {
-      return { text: value};
+      return { text: value };
     });
   }
 }
