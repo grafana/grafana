7d589b8d6cbb397cef8bbf4a619b7b6aad7f5ff9
fir4 <florian.loyau@s4id.net>
2017-03-23 16:23:46 +0100

    webdav: allow specification of a different public_url than upload url

--- pkg/components/imguploader/webdavuploader_test.go
+++ pkg/components/imguploader/webdavuploader_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestUploadToWebdav(t *testing.T) {
-	webdavUploader, _ := NewWebdavImageUploader("http://localhost:9998/dav/", "username", "password")
+	webdavUploader, _ := NewWebdavImageUploader("http://localhost:9998/dav/", "username", "password", "")
 
 	SkipConvey("[Integration test] for external_image_store.webdav", t, func() {
 		path, err := webdavUploader.Upload("../../../public/img/logo_transparent_400x.png")
