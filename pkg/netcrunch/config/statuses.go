fd15add390b6944212a1719803aff1c092e195c7
Bogusław Gorczyca <boguslaw@gorczyca.name>
2017-04-10 15:58:59 +0200

    Refactor statuses

--- pkg/netcrunch/config/statuses.go
+++ pkg/netcrunch/config/statuses.go
@@ -1,14 +1,15 @@
-package netcrunch
+package config
 
 import (
   "path/filepath"
   "gopkg.in/ini.v1"
   "github.com/grafana/grafana/pkg/log"
   "github.com/grafana/grafana/pkg/setting"
+  "github.com/grafana/grafana/pkg/netcrunch/fileUtils"
 )
 
 func getStatusesFilePath() string {
-  return filepath.Join(getGrafCrunchDataPath(), "statuses")
+  return filepath.Join(fileUtils.GetGrafCrunchDataPath(), "statuses")
 }
 
 func getDefaultStatusesFile() (*ini.File) {
@@ -18,12 +19,12 @@ func getDefaultStatusesFile() (*ini.File) {
   return DefaultStatuses
 }
 
-func createDefaultStatusesFile() (bool) {
+func CreateDefaultStatusesFile() (bool) {
   iLog := log.New("GrafCrunch statuses creator")
   statusesFilePath := getStatusesFilePath()
 
   if (!setting.PathExists(statusesFilePath)) {
-    if (!saveIniFile(getDefaultStatusesFile(), statusesFilePath)) {
+    if (!fileUtils.SaveIniFile(getDefaultStatusesFile(), statusesFilePath)) {
       iLog.Info("Statuses creation error")
       return false;
     }
@@ -35,7 +36,7 @@ func createDefaultStatusesFile() (bool) {
 
 func loadStatusesFile() (*ini.File, error) {
   iLog := log.New("GrafCrunch statuses loader")
-  statusesFile, err := loadIniFile(getStatusesFilePath())
+  statusesFile, err := fileUtils.LoadIniFile(getStatusesFilePath())
 
   if (err != nil) {
     iLog.Info("Failed to load statuses")
@@ -67,7 +68,7 @@ func SetInitializationSuccess() bool {
     statusesFile, err := loadStatusesFile()
     if (err == nil) {
       statusesFile.Section("Initialization").NewKey("success", "true")
-      return saveIniFile(statusesFile, getStatusesFilePath())
+      return fileUtils.SaveIniFile(statusesFile, getStatusesFilePath())
     }
     return false
   }
