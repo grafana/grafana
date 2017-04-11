57243a18e54212f96a2ef4b2b196615a0b87c778
Bogusław Gorczyca <boguslaw@gorczyca.name>
2017-04-11 14:34:16 +0200

    Refactor upgrade to 9.2

--- pkg/netcrunch/upgrader/upgradeTo092.go
+++ pkg/netcrunch/upgrader/upgradeTo092.go
@@ -2,9 +2,15 @@ package upgrader
 
 import (
   "errors"
+  "strings"
   "github.com/grafana/grafana/pkg/log"
+  "github.com/grafana/grafana/pkg/models"
   "github.com/grafana/grafana/pkg/netcrunch/config"
+  "github.com/grafana/grafana/pkg/netcrunch/model"
+  "github.com/grafana/grafana/pkg/components/simplejson"
 )
+
+const DS_NETCRUNCH_90 = "netcrunch"
 
 func upgradeTo092() (bool, error) {
   uLog := log.New("GrafCrunch upgrader")
@@ -30,5 +36,72 @@ func upgradeTo092() (bool, error) {
 }
 
 func updateNetCrunchDatasource() bool {
-  return true
+  netCrunchServerSettings, err := config.ReadNetCrunchServerSettingsFile()
+
+  if (err == nil) {
+    result := true
+    orgs, found := model.GetOrgs()
+    if found {
+      for index := range orgs {
+        if (updateNetCrunchDefaultDatasource(netCrunchServerSettings, orgs[index].Id) == false) {
+          result = false;
+        }
+      }
+    }
+    return result
+  }
+
+  return false
+}
+
+func updateNetCrunchDefaultDatasource(netCrunchSettings config.NetCrunchServerSettings, orgId int64) bool {
+  datasource, found := model.GetDataSourceByName("NetCrunch", orgId)
+
+  if found {
+    datasource.Url = getNetCrunchServerUrl(netCrunchSettings)
+    datasource.Type = DS_NETCRUNCH_90
+    datasource.JsonData = createDatasourceJsonData(netCrunchSettings)
+    return model.UpdateDataSource(datasource, orgId)
+  } else {
+    datasource := getDefaultNetCrunchDataSource(netCrunchSettings)
+    return model.AddDataSource(datasource, orgId)
+  }
+}
+
+func getNetCrunchServerUrl(netCrunchServerSettings config.NetCrunchServerSettings) string {
+  return netCrunchServerSettings.Protocol + "://" + getNetCrunchServerSimpleUrl(netCrunchServerSettings)
+}
+
+func getNetCrunchServerSimpleUrl(netCrunchServerSettings config.NetCrunchServerSettings) string {
+  return netCrunchServerSettings.Host + ":" + netCrunchServerSettings.Port
+}
+
+func createDatasourceJsonData(netCrunchServerSettings config.NetCrunchServerSettings) *simplejson.Json {
+  jsonDataBuffer := simplejson.New()
+
+  jsonDataBuffer.Set("simpleUrl", getNetCrunchServerSimpleUrl(netCrunchServerSettings))
+  jsonDataBuffer.Set("isSSL", (strings.Compare(strings.ToUpper(netCrunchServerSettings.Protocol), "HTTPS") == 0))
+  jsonDataBuffer.Set("user", netCrunchServerSettings.User)
+  jsonDataBuffer.Set("password", netCrunchServerSettings.Password)
+
+  return jsonDataBuffer
+}
+
+func getDefaultNetCrunchDataSource(netCrunchServerSettings config.NetCrunchServerSettings) models.DataSource {
+  jsonDataBuffer := createDatasourceJsonData(netCrunchServerSettings)
+
+  return models.DataSource {
+    Name:               "NetCrunch",
+    Type:               DS_NETCRUNCH_90,
+    Access:             models.DS_ACCESS_PROXY,
+    Url:                getNetCrunchServerUrl(netCrunchServerSettings),
+    User:               "",
+    Password:           "",
+    Database:           "",
+    BasicAuth:          false,
+    BasicAuthUser:      "",
+    BasicAuthPassword:  "",
+    IsDefault:          true,
+    JsonData:           jsonDataBuffer,
+  }
 }
