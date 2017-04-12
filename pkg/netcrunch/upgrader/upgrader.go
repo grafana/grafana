6ee50506b30617991ad78cb950cd9d0d5d447ad2
Bogusław Gorczyca <boguslaw@gorczyca.name>
2017-04-12 12:33:55 +0200

    Refactor upgrader

--- pkg/netcrunch/upgrader/upgrader.go
+++ pkg/netcrunch/upgrader/upgrader.go
@@ -1,26 +1,30 @@
 package upgrader
 
-import "github.com/grafana/grafana/pkg/netcrunch/config"
+import (
+  "github.com/grafana/grafana/pkg/log"
+  "github.com/grafana/grafana/pkg/netcrunch/config"
+)
 
 func Upgrade() {
-  var version string = "9.0.0"
+  var(
+    upgradeSuccess bool = false
+    uLog = log.New("GrafCrunch upgrader")
+    upTo094 bool = false
+    upTo100 bool = false
+  )
 
-  upTo092, err := upgradeTo092()
+  if (config.UpgradeMarkerFileExist()) {
+    upgradeSuccess = upgradeFrom090(uLog)
+  } else if version, err := config.ReadVersionFile(); (err == nil) {
 
-  if (notUpgradeTo092(upTo092, err)) {
-    version, err = config.ReadVersionFile()
-  }
-
-  if (err == nil) {
-    upTo094, err := upgradeTo094(version)
-    upTo100, err := upgradeTo100(version)
-
-    if ((err == nil) && (upTo094 || upTo100)) {
-      config.WriteVersionFile()
+    if upTo094, err = upgradeTo094(version, uLog); (err == nil) {
+      upTo100, err = upgradeTo100(version, uLog)
     }
-  }
-}
 
-func notUpgradeTo092(upTo092 bool, err error) bool {
-  return !(upTo092 && (err == nil))
+    upgradeSuccess = ((err == nil) && (upTo094 || upTo100))
+  }
+
+  if (upgradeSuccess){
+    config.WriteVersionFile()
+  }
 }
