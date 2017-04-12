72f0c7946fab48ab8afd23dfadd326ada540e0f7
Bogusław Gorczyca <boguslaw@gorczyca.name>
2017-04-12 11:53:46 +0200

    Refactor GrafCrunch initialization

--- pkg/netcrunch/netcrunch.go
+++ pkg/netcrunch/netcrunch.go
@@ -1,7 +1,9 @@
 package netcrunch
 
 import (
+  "github.com/grafana/grafana/pkg/log"
   "github.com/grafana/grafana/pkg/netcrunch/config"
+  "github.com/grafana/grafana/pkg/netcrunch/defaultDatasource"
   "github.com/grafana/grafana/pkg/netcrunch/model"
   "github.com/grafana/grafana/pkg/netcrunch/upgrader"
 )
@@ -16,6 +18,25 @@ func SetInitializationSuccess() (bool) {
   return config.SetInitializationSuccess()
 }
 
+func Init() {
+  if (config.CreateDefaultStatusesFile()) {
+
+    if (config.ServerSettingsFileExist() && (!config.VersionFileExist())) {
+      iLog := log.New("GrafCrunch initializer")
+
+      if (defaultDatasource.AddDefaultNetCrunchDatasource() && config.RemoveServerSettingsFile()) {
+        iLog.Info("Default NetCrunch datasource has been initialized")
+      } else {
+        iLog.Info("Initialization error")
+      }
+    }
+
+    upgrader.Upgrade()
+  }
+
+  initNetCrunchPlugin()
+}
+
 func initNetCrunchPlugin() {
   model.SetPluginAsCore(NETCRUNCH_APP_PLUGIN_ID_10)
 
@@ -24,9 +45,3 @@ func initNetCrunchPlugin() {
   }
 }
 
-func Init() {
-  if (config.CreateDefaultStatusesFile()) {
-    upgrader.Upgrade()
-  }
-  initNetCrunchPlugin()
-}
