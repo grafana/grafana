b867921b3b533b847725463891d141278e89c49d
Torkel Ödegaard <torkel@grafana.org>
2017-04-12 15:49:59 +0200

    annotation: cleanup

--- public/app/features/annotations/annotations_srv.ts
+++ public/app/features/annotations/annotations_srv.ts
@@ -134,20 +134,9 @@ export class AnnotationsSrv {
   }
 
   postAnnotation(annotations) {
-    console.log("POST /api/annotations\n", annotations);
-
-    // Not implemented yet
-    let implemented = true;
-    if (implemented) {
-      return Promise.all(_.map(annotations, annotation => {
-        return this.backendSrv.post('/api/annotations', annotation);
-      }))
-      .catch(error => {
-        console.log(error);
-      });
-    } else {
-      return Promise.resolve("Not implemented");
-    }
+    return Promise.all(_.map(annotations, annotation => {
+      return this.backendSrv.post('/api/annotations', annotation);
+    }));
   }
 
   translateQueryResult(annotation, results) {
