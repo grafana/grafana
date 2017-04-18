473006e8cfd3da590c549a3ce1fec52c12060306
Torkel Ödegaard <torkel@grafana.com>
2017-04-18 15:36:38 +0200

    build: updated grunt watch to explain best usage

--- tasks/options/exec.js
+++ tasks/options/exec.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
   'use strict'
   return {
-    tslint : "node ./node_modules/tslint/lib/tslint-cli.js -c tslint.json --project ./tsconfig.json",
+    tslint : "node ./node_modules/tslint/lib/tslint-cli.js -c tslint.json --project ./tsconfig.json <%= tslint.source.files.src %>",
     tscompile: "node ./node_modules/typescript/lib/tsc.js -p tsconfig.json --diagnostics",
     tswatch: "node ./node_modules/typescript/lib/tsc.js -p tsconfig.json --diagnostics --watch",
   };
