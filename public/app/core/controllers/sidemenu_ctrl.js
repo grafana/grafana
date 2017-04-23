088dc193cf120f575958266c444d44a010330f4d
hehaiyuan <hehaiyuan.hhy@gmail.com>
2017-04-23 17:35:00 +0800

    knowledgebase feature : new datasource for knowledge. page for search, and add modal for create knowledge

--- public/app/core/controllers/sidemenu_ctrl.js
+++ public/app/core/controllers/sidemenu_ctrl.js
@@ -178,9 +178,15 @@ function (angular, _, $, coreModule, config) {
       // });
 
       $scope.mainLinks.push({
-        text: "关联分析",
+        text: "关联性分析",
         icon: "fa fa-fw fa-line-chart",
         href: $scope.getUrl("/association")
+      });
+
+      $scope.mainLinks.push({
+        text: "运维知识",
+        icon: "fa fa-fw fa-stack-overflow",
+        href: $scope.getUrl("/knowledgebase")
       });
 
       $scope.mainLinks.push({
@@ -210,6 +216,7 @@ function (angular, _, $, coreModule, config) {
                   || currentPath.indexOf('/analysis') == 0
                   || currentPath.indexOf('/association') == 0
                   || currentPath.indexOf('/logs') == 0
+                  || currentPath.indexOf('/knowledgebase') == 0
                   || currentPath.indexOf('/decompose') == 0
                   || currentPath.indexOf('/health') == 0
                   || currentPath.indexOf('/cluster') == 0
---
+++ public/app/features/analysis/createKnowledgeCtrl.js
@@ -0,0 +1,32 @@
+define([
+    'angular',
+    'lodash'
+  ],
+  function (angular, _) {
+    'use strict';
+
+    var module = angular.module('grafana.controllers');
+
+    module.controller('CreateKnowledgeCtrl', function ($scope, backendSrv, contextSrv) {
+      $scope.init = function () {
+        $scope.knowledge = {};
+        $scope.knowledge.symptom = "";
+        $scope.knowledge.solution = "";
+        $scope.knowledge.service = "";
+      };
+
+      $scope.new = function() {
+        console.log($scope.knowledge);
+        $scope.knowledge.org_id = contextSrv.user.orgId;
+        $scope.knowledge.system_id = contextSrv.system;
+
+        backendSrv.knowledge({
+          method: "PUT",
+          url: "",
+          data: $scope.knowledge
+        });
+        $scope.dismiss();
+      };
+
+    });
+  });
