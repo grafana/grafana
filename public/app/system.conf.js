7655c39b987c0c385445e91d29b013704ef42313
Torkel Ödegaard <torkel@grafana.com>
2017-04-24 14:41:24 +0200

    lib: changed twitter emoij lib to be npm dependency

--- public/app/system.conf.js
+++ public/app/system.conf.js
@@ -32,7 +32,7 @@ System.config({
     "jquery.flot.fillbelow": "vendor/flot/jquery.flot.fillbelow",
     "jquery.flot.gauge": "vendor/flot/jquery.flot.gauge",
     "d3": "vendor/d3/d3.js",
-    "twemoji": "vendor/twemoji/twemoji.js"
+    "twemoji": "vendor/npm/twemoji/2/twemoji.amd.js"
   },
 
   packages: {
