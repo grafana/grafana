30b6c3b54a4e7315982df9f2294eb1b596447056
Torkel Ödegaard <torkel@grafana.com>
2017-05-11 08:52:22 +0200

    build: fixed heatmap test

--- public/app/plugins/panel/heatmap/specs/heatmap_data_converter_specs.ts
+++ public/app/plugins/panel/heatmap/specs/heatmap_data_converter_specs.ts
@@ -223,17 +223,17 @@ describe('ES Histogram converter', () => {
         '1422774000000': {
           x: 1422774000000,
           buckets: {
-            '1': { y: 1, count: 1 },
-            '2': { y: 2, count: 5 },
-            '3': { y: 3, count: 0 }
+            '1': { y: 1, count: 1, values: [], points: [] },
+            '2': { y: 2, count: 5, values: [], points: [] },
+            '3': { y: 3, count: 0, values: [], points: [] }
           }
         },
         '1422774060000': {
           x: 1422774060000,
           buckets: {
-            '1': { y: 1, count: 0 },
-            '2': { y: 2, count: 3 },
-            '3': { y: 3, count: 1 }
+            '1': { y: 1, count: 0, values: [], points: [] },
+            '2': { y: 2, count: 3, values: [], points: [] },
+            '3': { y: 3, count: 1, values: [], points: [] }
           }
         },
       };
