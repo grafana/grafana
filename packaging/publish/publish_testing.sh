fcbe3066286f90b27e66a9d5bd5af75a7f3e4a4a
Daniel Lee <dan.limerick@gmail.com>
2017-05-12 13:36:21 +0200

    release: version bump for package cloud script

--- packaging/publish/publish_testing.sh
+++ packaging/publish/publish_testing.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-deb_ver=4.2.0-beta1
-rpm_ver=4.2.0-beta1
+deb_ver=4.3.0-beta1
+rpm_ver=4.3.0-beta1
 
 wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${deb_ver}_amd64.deb
 
