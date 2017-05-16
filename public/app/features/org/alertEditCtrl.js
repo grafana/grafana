31f1cc0479e457f7718f1dbc69a45417ebc67195
liuhui <liuhuiiris@126.com>
2017-05-16 15:18:13 +0800

    新建报警规则可添加tags

--- public/app/features/org/alertEditCtrl.js
+++ public/app/features/org/alertEditCtrl.js
@@ -81,6 +81,7 @@ function (angular, _) {
       };
 
       $scope.alertDef = alertMgrSrv.get($routeParams.id);
+      $scope.target = {'tags':{}};
       $scope.isNew = !$scope.alertDef;
       if ($scope.isNew) {
         $scope.alertDef = {};
@@ -94,6 +95,7 @@ function (angular, _) {
         $scope.alertDef.alertDetails.hostQuery = {};
         $scope.alertDef.alertDetails.hostQuery.expression = ">";
         $scope.alertDef.alertDetails.hostQuery.metricQueries = [{"aggregator": "AVG","metric":""}];
+        $scope.alertDef.alertDetails.tags = null;
       } else {
         $scope.setTarget(panelMeta,$scope.alertDef);
         $scope.setCritThreshold(panelMeta,$scope.alertDef);
@@ -139,6 +141,11 @@ function (angular, _) {
     $scope.setTarget = function(panel,detail) {
       panel.panels[0].targets[0].aggregator = detail.alertDetails.hostQuery.metricQueries[0].aggregator.toLowerCase();
       panel.panels[0].targets[0].metric = detail.alertDetails.hostQuery.metricQueries[0].metric;
+      var tags = detail.alertDetails.tags;
+      _.each(tags, function(tag) {
+        panel.panels[0].targets[0].tags[tag.name] = tag.value;
+        $scope.target.tags[tag.name] = tag.value;
+      });
     };
 
     $scope.setTimeRange = function () {
@@ -167,6 +174,7 @@ function (angular, _) {
       }
       $scope.alertDef.org = contextSrv.user.orgId;
       $scope.alertDef.service = contextSrv.system;
+      $scope.getTags($scope.target.tags, $scope.alertDef.alertDetails);
 
       alertMgrSrv.save($scope.alertDef).then(function onSuccess() {
         $location.path("alerts");
@@ -184,6 +192,99 @@ function (angular, _) {
         .then($scope.getTextValues)
         .then(callback);
     };
+
+    $scope.targetBlur = function() {
+      $scope.target.errors = validateTarget($scope.target);
+
+      // this does not work so good
+      if (!_.isEqual($scope.oldTarget, $scope.target) && _.isEmpty($scope.target.errors)) {
+        $scope.oldTarget = angular.copy($scope.target);
+        $scope.setTags($scope.dashboard.rows[0].panels[0], $scope.target.tags);
+      }
+    };
+
+    $scope.suggestTagKeys = function(query, callback) {
+      $scope.datasource.metricFindQuery('suggest_tagk(' + query + ')')
+        .then($scope.getTextValues)
+        .then(callback);
+    };
+
+    $scope.suggestTagValues = function(query, callback) {
+      $scope.datasource.metricFindQuery('suggest_tagv(' + query + ')')
+        .then($scope.getTextValues)
+        .then(callback);
+    };
+
+    $scope.addTag = function() {
+      if (!$scope.addTagMode) {
+        $scope.addTagMode = true;
+        return;
+      }
+
+      if (!$scope.target.tags) {
+        $scope.target.tags = {};
+      }
+
+      $scope.target.errors = validateTarget($scope.target);
+
+      if (!$scope.target.errors.tags) {
+        $scope.target.tags[$scope.target.currentTagKey] = $scope.target.currentTagValue;
+        $scope.target.currentTagKey = '';
+        $scope.target.currentTagValue = '';
+        $scope.targetBlur();
+      }
+      $scope.addTagMode = false;
+    };
+
+    $scope.removeTag = function(key) {
+      delete $scope.target.tags[key];
+      $scope.targetBlur();
+    };
+
+    $scope.editTag = function(key, value) {
+      $scope.removeTag(key);
+      $scope.target.currentTagKey = key;
+      $scope.target.currentTagValue = value;
+      $scope.addTag();
+    };
+
+    function validateTarget(target) {
+      var errs = {};
+
+      if (target.shouldDownsample) {
+        try {
+          if (target.downsampleInterval) {
+            kbn.describe_interval(target.downsampleInterval);
+          } else {
+            errs.downsampleInterval = "您必须提供采样的间隔 (e.g. '1m' or '1h').";
+          }
+        } catch(err) {
+          errs.downsampleInterval = err.message;
+        }
+      }
+
+      if (target.tags && _.has(target.tags, target.currentTagKey)) {
+        errs.tags = "重复标签'" + target.currentTagKey + "'.";
+      }
+
+      return errs;
+    }
+
+    $scope.getTags = function(tags, detail) {
+      detail.tags = [];
+      for(var i in tags) {
+        var temp = {
+          'name': i,
+          'value': tags[i]
+        };
+        detail.tags.push(temp);
+      };
+    };
+
+    $scope.setTags = function(panel, tags) {
+      panel.targets[0].tags = tags;
+      $scope.broadcastRefresh();
+    }
 
     $scope.init();
   });
