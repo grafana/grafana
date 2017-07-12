8a11ab6a47813f793e3a891bc8a08515e531b59e
travelling-wxy <travel2wxy@163.com>
2017-07-12 14:00:35 +0800

    log-search-result-improve

--- public/app/features/logs/logsCtrl.js
+++ public/app/features/logs/logsCtrl.js
@@ -57,7 +57,7 @@ define([
               "span": 12,
               "styles": [
                 {
-                  "dateFormat": "YYYY-MM-DD HH:mm:ss",
+                  "dateFormat": "YYYY-MM-DD HH:mm:ss,sss",
                   "pattern": "@timestamp",
                   "type": "date"
                 },
@@ -136,7 +136,7 @@ define([
               "span": 12,
               "styles": [
                 {
-                  "dateFormat": "YYYY-MM-DD HH:mm:ss",
+                  "dateFormat": "YYYY-MM-DD HH:mm:ss,sss",
                   "pattern": "@timestamp",
                   "type": "date"
                 },
@@ -220,7 +220,7 @@ define([
               "span": 12,
               "styles": [
                 {
-                  "dateFormat": "YYYY-MM-DD HH:mm:ss",
+                  "dateFormat": "YYYY-MM-DD HH:mm:ss,sss",
                   "pattern": "Time",
                   "type": "date"
                 }
@@ -356,7 +356,6 @@ define([
                 "short",
                 "short"
               ],
-              "interval": "30m",
               "links": [],
               "helpInfo": {
                 "info": false,
