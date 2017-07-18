9e526a9d899105d3f539a3c473e0d9fa577d9d66
liuhui <liuhuiiris@126.com>
2017-07-18 18:04:45 +0800

    ui fix:anomaly list

--- public/app/features/anomaly/anomalyCtrl.js
+++ public/app/features/anomaly/anomalyCtrl.js
@@ -7,7 +7,7 @@ define([
 
     var module = angular.module('grafana.controllers');
 
-    module.controller('AnomalyCtrl', function ($scope, healthSrv, backendSrv, contextSrv, $controller, $rootScope) {
+    module.controller('AnomalyCtrl', function ($scope, healthSrv, backendSrv, contextSrv, $controller, $rootScope, $location) {
       $scope.init = function () {
         $scope.system = backendSrv.getSystemById(contextSrv.user.systemId);
         healthSrv.load().then(function (data) {
@@ -18,9 +18,12 @@ define([
           $scope.clustersLength = $scope.metricHostClusters.length;
           healthSrv.anomalyMetricsData = $scope.metricHostClusters;
           $scope.excludeMetricsData = healthSrv.floor(data.metricHostExcluded.elements);
-          $scope.excludeMetricLength = _.size($scope.excludeMetricsData);
+          _.remove($scope.metricHostClusters, function(cluster) {
+            return cluster.health > 85;
+          });
           $controller('ClusterCtrl', {$scope: $scope}).init();
         });
+        $scope.selected = 0;
       };
       $scope.reload = function() {
         $scope.init();
@@ -39,6 +42,30 @@ define([
           $scope.metricHostClusters = [healthSrv.anomalyMetricsData[index.seriesIndex]];
         });
       });
+
+      $scope.exclude = function(anomaly) {
+        _.each($scope.metricHostClusters, function(cluster) {
+          _.remove(cluster.elements, function(element) {
+            return _.isEqual(anomaly, element);
+          });
+        });
+        healthSrv.exclude(anomaly.metric, anomaly.host);
+        $scope.excludeMetricsData.push(anomaly);
+      };
+
+      $scope.include = function (anomalyDef) {
+        healthSrv.include(anomalyDef.metric, anomalyDef.host).then(function () {
+          $scope.reload();
+        });
+      };
+
+      $scope.selectCluster = function(index) {
+        if($scope.selected == index) {
+          $scope.selected = -1;
+        } else {
+          $scope.selected = index;
+        }
+      };
 
       $scope.init();
     });
