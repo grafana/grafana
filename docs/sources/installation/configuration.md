fcdf2820901ecbafe91a7034e40419bd868feb06
Konstantin Chukhlomin <chuhlomin@gmail.com>
2017-08-06 11:04:38 -0400

    GCS support via JSON API

--- docs/sources/installation/configuration.md
+++ docs/sources/installation/configuration.md
@@ -645,7 +645,7 @@ Time to live for snapshots.
 These options control how images should be made public so they can be shared on services like slack.
 
 ### provider
-You can choose between (s3, webdav, gcp). If left empty Grafana will ignore the upload action.
+You can choose between (s3, webdav, gcs). If left empty Grafana will ignore the upload action.
 
 ## [external_image_storage.s3]
 
@@ -677,14 +677,13 @@ basic auth username
 ### password
 basic auth password
 
-## [external_image_storage.gcp]
+## [external_image_storage.gcs]
 
 ### key_file
 Path to JSON key file associated with a Google service account to authenticate and authorize.
 Service Account keys can be created and downloaded from https://console.developers.google.com/permissions/serviceaccounts.
 
 ### bucket name
-
 Bucket Name on Google Cloud Storage. 
 
 ## [alerting]
--- pkg/components/imguploader/gcpuploader.go
+++ pkg/components/imguploader/gcpuploader.go
@@ -1,56 +1,96 @@
 package imguploader
 
 import (
-	"cloud.google.com/go/storage"
+	"context"
+	"errors"
 	"fmt"
 	"github.com/grafana/grafana/pkg/log"
 	"github.com/grafana/grafana/pkg/util"
-	"golang.org/x/net/context"
-	"google.golang.org/api/option"
+	"golang.org/x/oauth2/google"
 	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-type GCPUploader struct {
+type GCSUploader struct {
 	keyFile string
 	bucket  string
 	log     log.Logger
 }
 
-func NewGCPUploader(keyFile, bucket string) *GCPUploader {
-	return &GCPUploader{
+func NewGCSUploader(keyFile, bucket string) *GCSUploader {
+	return &GCSUploader{
 		keyFile: keyFile,
 		bucket:  bucket,
-		log:     log.New("gcpuploader"),
+		log:     log.New("gcsuploader"),
 	}
 }
 
-func (u *GCPUploader) Upload(imageDiskPath string) (string, error) {
-	ctx := context.Background()
-
-	client, err := storage.NewClient(ctx, option.WithServiceAccountFile(u.keyFile))
-	if err != nil {
-		return "", err
-	}
-
+func (u *GCSUploader) Upload(imageDiskPath string) (string, error) {
 	key := util.GetRandomString(20) + ".png"
-	log.Debug("Uploading image to GCP bucket = %s key = %s", u.bucket, key)
 
-	file, err := ioutil.ReadFile(imageDiskPath)
+	log.Debug("Opening key file ", u.keyFile)
+
+	ctx := context.Background()
+	data, err := ioutil.ReadFile(u.keyFile)
 	if err != nil {
 		return "", err
 	}
 
-	wc := client.Bucket(u.bucket).Object(key).NewWriter(ctx)
-	wc.ContentType = "image/png"
-	wc.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
+	log.Debug("Creating JWT conf")
 
-	if _, err := wc.Write(file); err != nil {
+	conf, err := google.JWTConfigFromJSON(data, "https://www.googleapis.com/auth/devstorage.full_control")
+	if err != nil {
 		return "", err
 	}
 
-	if err := wc.Close(); err != nil {
+	log.Debug("Creating HTTP client")
+
+	client := conf.Client(ctx)
+
+	err = u.uploadFile(client, imageDiskPath, key)
+	if err != nil {
 		return "", err
 	}
 
 	return fmt.Sprintf("https://storage.googleapis.com/%s/%s", u.bucket, key), nil
+}
+
+func (u *GCSUploader) uploadFile(client *http.Client, imageDiskPath, key string) error {
+	log.Debug("Opening image file ", imageDiskPath)
+
+	fileReader, err := os.Open(imageDiskPath)
+	if err != nil {
+		return err
+	}
+
+	reqUrl := fmt.Sprintf(
+		"https://www.googleapis.com/upload/storage/v1/b/%s/o?uploadType=media&name=%s&predefinedAcl=publicRead",
+		u.bucket,
+		key,
+	)
+
+	log.Debug("Request URL: ", reqUrl)
+
+	req, err := http.NewRequest("POST", reqUrl, fileReader)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "image/png")
+
+	log.Debug("Sending POST request to GCS")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	log.Debug("GCS API response header", resp.Header)
+
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("GCS response status code %d", resp.StatusCode))
+	}
+
+	return nil
 }
