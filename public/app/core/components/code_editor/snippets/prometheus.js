679ddc9696bd534856783467a930041cebf2e02c
Alexander Zobnin <alexanderzobnin@gmail.com>
2017-08-10 16:04:37 +0300

    ace: example snippet for prometheus

--- public/app/core/components/code_editor/snippets/prometheus.js
+++ public/app/core/components/code_editor/snippets/prometheus.js
@@ -3,7 +3,19 @@
 ace.define("ace/snippets/prometheus",["require","exports","module"], function(require, exports, module) {
 "use strict";
 
-exports.snippetText =undefined;
-exports.scope = "prometheus";
+// exports.snippetText = "# rate\n\
+// snippet r\n\
+//   rate(${1:metric}[${2:range}])\n\
+// ";
 
+exports.snippets = [
+  {
+    "content": "rate(${1:metric}[${2:range}])",
+    "name": "rate()",
+    "scope": "prometheus",
+    "tabTrigger": "r"
+  }
+];
+
+exports.scope = "prometheus";
 });
