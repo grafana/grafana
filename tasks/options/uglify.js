2df559050adac6f0435dd73b767e47d084dfaa71
travelling-wxy <travel2wxy@163.com>
2017-08-15 19:52:06 +0800

    fix conflict & uglify

--- tasks/options/uglify.js
+++ tasks/options/uglify.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   return {
     genDir: {
       expand: true,
-      src: ['**/*.js', '!dashboards/*.js', '!vendor/**/*.js'],
+      src: ['app/**/*.js', '!dashboards/*.js', '!vendor/**/*.js'],
       dest: '<%= genDir %>',
       cwd: '<%= genDir %>',
       options: {
