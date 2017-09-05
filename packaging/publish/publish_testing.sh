b333ad94815290002650f82a6f2a85528f19e137
Torkel Ödegaard <torkel@grafana.com>
2017-09-05 16:01:28 +0200

    docs: updated docs with v4.5 beta links

--- packaging/publish/publish_testing.sh
+++ packaging/publish/publish_testing.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
-deb_ver=4.3.0-beta1
-rpm_ver=4.3.0-beta1
+deb_ver=4.5.0-beta1
+rpm_ver=4.5.0-beta1
 
-wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${deb_ver}_amd64.deb
+# wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${deb_ver}_amd64.deb
 
-package_cloud push grafana/testing/debian/jessie grafana_${deb_ver}_amd64.deb
+# package_cloud push grafana/testing/debian/jessie grafana_${deb_ver}_amd64.deb
 package_cloud push grafana/testing/debian/wheezy grafana_${deb_ver}_amd64.deb
 package_cloud push grafana/testing/debian/stretch grafana_${deb_ver}_amd64.deb
 
