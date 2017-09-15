87308fd97fc63bbcc2b5fe64a9f56f5016f19e77
bergquist <carl.bergquist@gmail.com>
2017-09-15 15:46:41 +0200

    fixes broken tests :boom:

--- pkg/components/imguploader/gcsuploader_test.go
+++ pkg/components/imguploader/gcsuploader_test.go
@@ -1,6 +1,7 @@
 package imguploader
 
 import (
+	"context"
 	"testing"
 
 	"github.com/grafana/grafana/pkg/setting"
@@ -15,7 +16,7 @@ func TestUploadToGCS(t *testing.T) {
 
 		gcsUploader, _ := NewImageUploader()
 
-		path, err := gcsUploader.Upload("../../../public/img/logo_transparent_400x.png")
+		path, err := gcsUploader.Upload(context.Background(), "../../../public/img/logo_transparent_400x.png")
 
 		So(err, ShouldBeNil)
 		So(path, ShouldNotEqual, "")
