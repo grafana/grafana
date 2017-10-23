701f83a50528ca3483f0d73d61acf6a4ba93456a
Alexander Zobnin <alexanderzobnin@gmail.com>
2017-10-23 21:50:20 +0300

    tech: restrict d3 to only needed modules

--- public/app/plugins/panel/heatmap/color_scale.ts
+++ public/app/plugins/panel/heatmap/color_scale.ts
@@ -1,4 +1,4 @@
-import * as d3 from 'd3';
+import * as d3Scale from 'd3-scale';
 import * as d3ScaleChromatic from 'd3-scale-chromatic';
 
 export function getColorScale(colorScheme: any, lightTheme: boolean, maxValue: number, minValue = 0): (d: any) => any {
@@ -9,17 +9,17 @@ export function getColorScale(colorScheme: any, lightTheme: boolean, maxValue: n
   let start = colorScaleInverted ? maxValue : minValue;
   let end = colorScaleInverted ? minValue : maxValue;
 
-  return d3.scaleSequential(colorInterpolator).domain([start, end]);
+  return d3Scale.scaleSequential(colorInterpolator).domain([start, end]);
 }
 
 export function getOpacityScale(options, maxValue, minValue = 0) {
   let legendOpacityScale;
   if (options.colorScale === 'linear') {
-    legendOpacityScale = d3.scaleLinear()
+    legendOpacityScale = d3Scale.scaleLinear()
     .domain([minValue, maxValue])
     .range([0, 1]);
   } else if (options.colorScale === 'sqrt') {
-    legendOpacityScale = d3.scalePow().exponent(options.exponent)
+    legendOpacityScale = d3Scale.scalePow().exponent(options.exponent)
     .domain([minValue, maxValue])
     .range([0, 1]);
   }
