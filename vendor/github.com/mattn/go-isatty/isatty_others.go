93ac6680ab52db8917533cd28cfd6588a58df841
stuart nelson <stuartnelson3@gmail.com>
2017-10-23 19:57:19 +0200

    update log15 (#9622)
    * Update log15 and go-isatty
    
    * Update usage in pkg log

--- vendor/github.com/mattn/go-isatty/isatty_others.go
+++ vendor/github.com/mattn/go-isatty/isatty_others.go
@@ -1,12 +1,7 @@
-// +build appengine
+// +build !windows
+// +build !appengine
 
 package isatty
-
-// IsTerminal returns true if the file descriptor is terminal which
-// is always false on on appengine classic which is a sandboxed PaaS.
-func IsTerminal(fd uintptr) bool {
-	return false
-}
 
 // IsCygwinTerminal() return true if the file descriptor is a cygwin or msys2
 // terminal. This is also always false on this environment.
