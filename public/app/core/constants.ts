57cf824279d6a99281c51a0c604d30be06d551eb
Alexander Zobnin <alexanderzobnin@gmail.com>
2017-11-23 10:15:36 +0300

    dashboard migration: refactor

--- public/app/core/constants.ts
+++ public/app/core/constants.ts
@@ -4,4 +4,5 @@ export const GRID_CELL_VMARGIN = 10;
 export const GRID_COLUMN_COUNT = 24;
 export const REPEAT_DIR_VERTICAL = 'v';
 
-
+export const DEFAULT_ROW_HEIGHT = 250;
+export const MIN_PANEL_HEIGHT = GRID_CELL_HEIGHT * 3;
