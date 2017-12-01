d6d64c53cd0e3bf1fdea4f98b4c2ce7fb8533ccf
bergquist <carl.bergquist@gmail.com>
2017-12-01 16:23:55 +0100

    typo :boom:

--- pkg/api/render.go
+++ pkg/api/render.go
@@ -12,7 +12,7 @@ import (
 func RenderToPng(c *middleware.Context) {
 	queryReader, err := util.NewUrlQueryReader(c.Req.URL)
 	if err != nil {
-		c.Handle(400, "Rander parameters error", err)
+		c.Handle(400, "Render parameters error", err)
 		return
 	}
 	queryParams := fmt.Sprintf("?%s", c.Req.URL.RawQuery)
