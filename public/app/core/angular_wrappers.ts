8ea94817f55a92a8c844775e8725ce9210370f83
Torkel Ödegaard <torkel@grafana.com>
2017-12-13 16:56:24 +0100

    build: fixed build issue

--- public/app/core/angular_wrappers.ts
+++ public/app/core/angular_wrappers.ts
@@ -2,11 +2,11 @@ import { react2AngularDirective } from 'app/core/utils/react2angular';
 import { PasswordStrength } from './components/PasswordStrength';
 import PageHeader from './components/PageHeader/PageHeader';
 import EmptyListCTA from './components/EmptyListCTA/EmptyListCTA';
-// import { Login } from './components/Login/Login';
+import LoginBackground from './components/Login/LoginBackground';
 
 export function registerAngularDirectives() {
   react2AngularDirective('passwordStrength', PasswordStrength, ['password']);
   react2AngularDirective('pageHeader', PageHeader, ['model', 'noTabs']);
   react2AngularDirective('emptyListCta', EmptyListCTA, ['model']);
-  // react2AngularDirective('login', Login, []);
+  react2AngularDirective('loginBackground', LoginBackground, []);
 }
