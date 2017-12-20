eef37c4071e72b58a6e7cd95a2680dd3b633161e
Daniel Lee <dan.limerick@gmail.com>
2017-12-20 15:00:13 +0100

    dashfolders: disable save button after save of acl

--- public/app/features/dashboard/acl/acl.ts
+++ public/app/features/dashboard/acl/acl.ts
@@ -89,9 +89,13 @@ export class AclCtrl {
       });
     }
 
-    return this.backendSrv.post(`/api/dashboards/id/${this.dashboard.id}/acl`, {
-      items: updated,
-    });
+    return this.backendSrv
+      .post(`/api/dashboards/id/${this.dashboard.id}/acl`, {
+        items: updated,
+      })
+      .then(() => {
+        this.canUpdate = false;
+      });
   }
 
   typeChanged() {
