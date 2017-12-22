2501b1b519d876efddd236c1a602c85e912308c3
bergquist <carl.bergquist@gmail.com>
2017-12-22 10:54:48 +0100

    pagerduty: fixes invalid default value
    autoResolve incident checkbox was set to disabled by default but
    the backend used enabled as default. This commit makes both use
    disabled by defualt
    
    fixes #10222
    
    (cherry picked from commit 7917efb31a90bcf695b3756f284ca2a0728ebad5)

--- pkg/services/alerting/notifiers/pagerduty_test.go
+++ pkg/services/alerting/notifiers/pagerduty_test.go
@@ -26,6 +26,26 @@ func TestPagerdutyNotifier(t *testing.T) {
 				So(err, ShouldNotBeNil)
 			})
 
+			Convey("auto resolve should default to false", func() {
+				json := `{ "integrationKey": "abcdefgh0123456789" }`
+
+				settingsJSON, _ := simplejson.NewJson([]byte(json))
+				model := &m.AlertNotification{
+					Name:     "pagerduty_testing",
+					Type:     "pagerduty",
+					Settings: settingsJSON,
+				}
+
+				not, err := NewPagerdutyNotifier(model)
+				pagerdutyNotifier := not.(*PagerdutyNotifier)
+
+				So(err, ShouldBeNil)
+				So(pagerdutyNotifier.Name, ShouldEqual, "pagerduty_testing")
+				So(pagerdutyNotifier.Type, ShouldEqual, "pagerduty")
+				So(pagerdutyNotifier.Key, ShouldEqual, "abcdefgh0123456789")
+				So(pagerdutyNotifier.AutoResolve, ShouldBeFalse)
+			})
+
 			Convey("settings should trigger incident", func() {
 				json := `
 				{
