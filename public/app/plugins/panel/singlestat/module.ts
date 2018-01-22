77d60a4478ff4e0c071de5df6000834549c04ea9
Jonathan McCall <jonathan.mccall@hbo.com>
2018-01-22 10:25:32 -0500

    Use function to hide prefix/postfix size when using gauge

--- public/app/plugins/panel/singlestat/module.ts
+++ public/app/plugins/panel/singlestat/module.ts
@@ -198,6 +198,10 @@ class SingleStatCtrl extends MetricsPanelCtrl {
     this.setValueMapping(data);
   }
 
+   disableFontSizes() {
+     return this.panel.gauge.show;
+   }
+
   setColoring(options) {
     if (options.background) {
       this.panel.colorValue = false;
