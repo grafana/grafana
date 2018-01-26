d61d1aac61dd9238b81afff29eec9e43920dbf6c
Jonathan McCall <jonathan.mccall@hbo.com>
2018-01-26 11:30:27 -0500

    Add /pause/by-id/:alertId route for consistency

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -293,6 +293,7 @@ func (hs *HttpServer) registerRoutes() {
 		apiRoute.Group("/alerts", func(alertsRoute RouteRegister) {
 			alertsRoute.Post("/test", bind(dtos.AlertTestCommand{}), wrap(AlertTest))
 			alertsRoute.Post("/:alertId/pause", reqEditorRole, bind(dtos.PauseAlertCommand{}), wrap(PauseAlert))
+			alertsRoute.Post("/pause/by-id/:alertId", reqEditorRole, bind(dtos.PauseAlertCommand{}), wrap(PauseAlert))
 			alertsRoute.Post("/pause/by-dashboard/:dashboardId", reqEditorRole, bind(dtos.PauseDashboardAlertsCommand{}), wrap(PauseDashboardAlerts))
 			alertsRoute.Get("/:alertId", ValidateOrgAlert, wrap(GetAlert))
 			alertsRoute.Get("/", wrap(GetAlerts))
