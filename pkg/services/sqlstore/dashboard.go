c099373f2ddc1c6f5ff3c434c562b0fb88de58c2
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2018-01-29 17:20:18 +0100

    dashboard: generate and include uid in dashboard model. #7883

--- pkg/services/sqlstore/dashboard.go
+++ pkg/services/sqlstore/dashboard.go
@@ -175,6 +175,7 @@ func GetDashboard(query *m.GetDashboardQuery) error {
 	}
 
 	dashboard.Data.Set("id", dashboard.Id)
+	dashboard.Data.Set("uid", dashboard.Uid)
 	query.Result = &dashboard
 	return nil
 }
