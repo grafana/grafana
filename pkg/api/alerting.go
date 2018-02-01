74ca6f6dbfea03f5e12e01f2b82b2453a4b89a8e
bergquist <carl.bergquist@gmail.com>
2018-02-01 13:16:56 +0100

    changes dashboard url in alertlist

--- pkg/api/alerting.go
+++ pkg/api/alerting.go
@@ -105,7 +105,8 @@ func transformToDTOs(alerts []*models.Alert, c *middleware.Context) ([]*dtos.Ale
 	for _, alert := range alertDTOs {
 		for _, dash := range dashboardsQuery.Result {
 			if alert.DashboardId == dash.Id {
-				alert.DashbboardUri = "db/" + dash.Slug
+				alert.DashbboardUri = models.GetDashboardUrl(dash.Uid, dash.Slug)
+				break
 			}
 		}
 	}
