85323e13da4e056d1871f75e0bd071063f5affb2
Mikael Olenfalk <mikael@olenfalk.se>
2018-02-01 13:58:28 +0100

    Render using chrome-headless if configured

--- pkg/components/renderer/renderer.go
+++ pkg/components/renderer/renderer.go
@@ -59,37 +59,14 @@ func appendEnviron(baseEnviron []string, name string, value string) []string {
 	return append(results, fmt.Sprintf("%s=%s", name, value))
 }
 
-func RenderToPng(params *RenderOpts) (string, error) {
-	rendererLog.Info("Rendering", "path", params.Path)
-
+func renderWithPhantom(url, pngPath, domain, renderKey string, timeout int, params *RenderOpts) *exec.Cmd {
 	var executable = "phantomjs"
 	if runtime.GOOS == "windows" {
 		executable = executable + ".exe"
 	}
 
-	localDomain := "localhost"
-	if setting.HttpAddr != setting.DEFAULT_HTTP_ADDR {
-		localDomain = setting.HttpAddr
-	}
-
-	url := fmt.Sprintf("%s://%s:%s/%s", setting.Protocol, localDomain, setting.HttpPort, params.Path)
-
 	binPath, _ := filepath.Abs(filepath.Join(setting.PhantomDir, executable))
 	scriptPath, _ := filepath.Abs(filepath.Join(setting.PhantomDir, "render.js"))
-	pngPath, _ := filepath.Abs(filepath.Join(setting.ImagesDir, util.GetRandomString(20)))
-	pngPath = pngPath + ".png"
-
-	orgRole := params.OrgRole
-	if params.IsAlertContext {
-		orgRole = models.ROLE_ADMIN
-	}
-	renderKey := middleware.AddRenderAuthKey(params.OrgId, params.UserId, orgRole)
-	defer middleware.RemoveRenderAuthKey(renderKey)
-
-	timeout, err := strconv.Atoi(params.Timeout)
-	if err != nil {
-		timeout = 15
-	}
 
 	phantomDebugArg := "--debug=false"
 	if log.GetLogLevelFor("png-renderer") >= log.LvlDebug {
@@ -105,7 +82,7 @@ func RenderToPng(params *RenderOpts) (string, error) {
 		"width=" + params.Width,
 		"height=" + params.Height,
 		"png=" + pngPath,
-		"domain=" + localDomain,
+		"domain=" + domain,
 		"timeout=" + strconv.Itoa(timeout),
 		"renderKey=" + renderKey,
 	}
@@ -114,7 +91,74 @@ func RenderToPng(params *RenderOpts) (string, error) {
 		cmdArgs = append([]string{fmt.Sprintf("--output-encoding=%s", params.Encoding)}, cmdArgs...)
 	}
 
-	cmd := exec.Command(binPath, cmdArgs...)
+	rendererLog.Debug(`rendering using phantomjs:`, binPath, cmdArgs)
+
+	return exec.Command(binPath, cmdArgs...)
+}
+
+func renderWithChrome(url, pngPath, domain, renderKey string, timeout int, params *RenderOpts) *exec.Cmd {
+	cmdArgs := []string{
+		setting.RenderingHeadlessScript,
+		"url=" + url,
+		"width=" + params.Width,
+		"height=" + params.Height,
+		"png=" + pngPath,
+		"domain=" + domain,
+		"timeout=" + strconv.Itoa(timeout),
+		"renderKey=" + renderKey,
+	}
+
+	if !setting.RenderingHeadless {
+		cmdArgs = append(cmdArgs, "headless=false")
+	}
+
+	if setting.RenderingSlowMo > 0 {
+		cmdArgs = append(cmdArgs, fmt.Sprintf("slowmo=%d", setting.RenderingSlowMo))
+	}
+
+	// hehe hardcode node for now
+	return exec.Command("node", cmdArgs...)
+}
+
+func getRendererCommand(url, pngPath, domain, renderKey string, timeout int, params *RenderOpts) *exec.Cmd {
+	if setting.RenderingEngine == "chrome" {
+		return renderWithChrome(url, pngPath, domain, renderKey, timeout, params)
+	}
+
+	if setting.RenderingEngine != "phantom" {
+		rendererLog.Warn("invalid renderer configured, falling back to phantom", setting.RenderingEngine)
+	}
+	return renderWithPhantom(url, pngPath, domain, renderKey, timeout, params)
+}
+
+func RenderToPng(params *RenderOpts) (string, error) {
+	rendererLog.Info("Rendering", "path", params.Path)
+
+	localDomain := "localhost"
+	if setting.RenderingDomain != "" {
+		localDomain = setting.RenderingDomain
+	} else if setting.HttpAddr != setting.DEFAULT_HTTP_ADDR {
+		localDomain = setting.HttpAddr
+	}
+
+	url := fmt.Sprintf("%s://%s:%s/%s", setting.Protocol, localDomain, setting.HttpPort, params.Path)
+
+	pngPath, _ := filepath.Abs(filepath.Join(setting.ImagesDir, util.GetRandomString(20)))
+	pngPath = pngPath + ".png"
+
+	orgRole := params.OrgRole
+	if params.IsAlertContext {
+		orgRole = models.ROLE_ADMIN
+	}
+	renderKey := middleware.AddRenderAuthKey(params.OrgId, params.UserId, orgRole)
+	defer middleware.RemoveRenderAuthKey(renderKey)
+
+	timeout, err := strconv.Atoi(params.Timeout)
+	if err != nil {
+		timeout = 15
+	}
+
+	cmd := getRendererCommand(url, pngPath, localDomain, renderKey, timeout, params)
 	output, err := cmd.StdoutPipe()
 
 	if err != nil {
