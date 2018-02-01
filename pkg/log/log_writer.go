3beaf48de445e203a0074c4ba417af4af53f1bc2
Mikael Olenfalk <mikael@olenfalk.se>
2018-02-01 08:52:37 +0100

    Unexport LogWriterImpl

--- pkg/log/log_writer.go
+++ pkg/log/log_writer.go
@@ -5,21 +5,21 @@ import (
 	"strings"
 )
 
-type LogWriterImpl struct {
+type logWriterImpl struct {
 	log    Logger
 	level  Lvl
 	prefix string
 }
 
 func NewLogWriter(log Logger, level Lvl, prefix string) io.Writer {
-	return &LogWriterImpl{
+	return &logWriterImpl{
 		log:    log,
 		level:  level,
 		prefix: prefix,
 	}
 }
 
-func (l *LogWriterImpl) Write(p []byte) (n int, err error) {
+func (l *logWriterImpl) Write(p []byte) (n int, err error) {
 	message := l.prefix + strings.TrimSpace(string(p))
 
 	switch l.level {
