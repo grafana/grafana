651103bdda26e248c427a16b49a54e8313c28150
bergquist <carl.bergquist@gmail.com>
2018-02-15 14:56:52 +0100

    chore: adds comment for exported function

--- pkg/util/shortid_generator.go
+++ pkg/util/shortid_generator.go
@@ -14,6 +14,12 @@ var validUidPattern = regexp.MustCompile(`^[a-zA-Z0-9\-\_]*$`).MatchString
 var ErrDashboardInvalidUid = errors.New("uid contains illegal characters")
 var ErrDashboardUidToLong = errors.New("uid to long. max 40 characters")
 
+func init() {
+	gen, _ := shortid.New(1, allowedChars, 1)
+	shortid.SetDefault(gen)
+}
+
+// VerifyUid verifies the size and content of the uid
 func VerifyUid(uid string) error {
 	if len(uid) > 40 {
 		return ErrDashboardUidToLong
@@ -24,11 +30,6 @@ func VerifyUid(uid string) error {
 	}
 
 	return nil
-}
-
-func init() {
-	gen, _ := shortid.New(1, allowedChars, 1)
-	shortid.SetDefault(gen)
 }
 
 // GenerateShortUid generates a short unique identifier.
