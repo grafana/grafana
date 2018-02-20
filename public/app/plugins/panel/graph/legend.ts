1293fcadd24b4ece234e3f7c4c8b0d13dae1aca9
Patrick O'Carroll <ocarroll.patrick@gmail.com>
2018-02-20 09:45:30 +0100

    added an if to check for null to sort null as 0 (#10961)
    * added an if to check for null to sort null as 0
    
    * changed 0 to -Infinity

--- public/app/plugins/panel/graph/legend.ts
+++ public/app/plugins/panel/graph/legend.ts
@@ -151,7 +151,11 @@ module.directive('graphLegend', function(popoverSrv, $timeout) {
 
         if (panel.legend.sort) {
           seriesList = _.sortBy(seriesList, function(series) {
-            return series.stats[panel.legend.sort];
+            let sort = series.stats[panel.legend.sort];
+            if (sort === null) {
+              sort = -Infinity;
+            }
+            return sort;
           });
           if (panel.legend.sortDesc) {
             seriesList = seriesList.reverse();
