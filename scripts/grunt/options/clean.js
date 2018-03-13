099889cccf36ef164f8aba6362937ef61ef8e932
Bogusław Gorczyca <boguslaw@gorczyca.name>
2018-03-13 14:59:23 +0100

    Update build cleaning process

--- scripts/grunt/options/clean.js
+++ scripts/grunt/options/clean.js
@@ -2,6 +2,8 @@ module.exports = function(config) {
   'use strict';
 
   return {
+    on_start: ['<%= destDir %>', '<%= tempDir %>', '<%= windowsDestDir %>'],
+    windows: ['<%= windowsDestDir %>'],
     release: ['<%= destDir %>', '<%= tempDir %>', '<%= genDir %>'],
     build: ['<%= srcDir %>/build'],
     temp: ['<%= tempDir %>'],
