4f44376564c904a834d612d6a3dcc49816cf9c5c
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2018-04-13 17:11:52 +0200

    remove comment/unused variable

--- pkg/models/dashboards.go
+++ pkg/models/dashboards.go
@@ -33,8 +33,7 @@ var (
 	ErrDashboardInvalidUid                     = errors.New("uid contains illegal characters")
 	ErrDashboardUidToLong                      = errors.New("uid to long. max 40 characters")
 	ErrDashboardCannotSaveProvisionedDashboard = errors.New("Cannot save provisioned dashboard")
-	//ErrDashboardProvisioningDoesNotExist       = errors.New("Dashboard provisioning does not exist")
-	RootFolderName = "General"
+	RootFolderName                             = "General"
 )
 
 type UpdatePluginDashboardError struct {
