b02a860e66c439c7b717d8be7b4650a8e9eb5891
Mario Trangoni <mjtrangoni@gmail.com>
2018-04-23 20:03:57 +0200

    pkg/components: fix ineffassign issues

--- pkg/components/imguploader/imguploader_test.go
+++ pkg/components/imguploader/imguploader_test.go
@@ -19,6 +19,7 @@ func TestImageUploaderFactory(t *testing.T) {
 
 			Convey("with bucket url https://foo.bar.baz.s3-us-east-2.amazonaws.com", func() {
 				s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
+				So(err, ShouldBeNil)
 				s3sec.NewKey("bucket_url", "https://foo.bar.baz.s3-us-east-2.amazonaws.com")
 				s3sec.NewKey("access_key", "access_key")
 				s3sec.NewKey("secret_key", "secret_key")
@@ -37,6 +38,7 @@ func TestImageUploaderFactory(t *testing.T) {
 
 			Convey("with bucket url https://s3.amazonaws.com/mybucket", func() {
 				s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
+				So(err, ShouldBeNil)
 				s3sec.NewKey("bucket_url", "https://s3.amazonaws.com/my.bucket.com")
 				s3sec.NewKey("access_key", "access_key")
 				s3sec.NewKey("secret_key", "secret_key")
@@ -55,15 +57,15 @@ func TestImageUploaderFactory(t *testing.T) {
 
 			Convey("with bucket url https://s3-us-west-2.amazonaws.com/mybucket", func() {
 				s3sec, err := setting.Cfg.GetSection("external_image_storage.s3")
+				So(err, ShouldBeNil)
 				s3sec.NewKey("bucket_url", "https://s3-us-west-2.amazonaws.com/my.bucket.com")
 				s3sec.NewKey("access_key", "access_key")
 				s3sec.NewKey("secret_key", "secret_key")
 
 				uploader, err := NewImageUploader()
-
 				So(err, ShouldBeNil)
-				original, ok := uploader.(*S3Uploader)
 
+				original, ok := uploader.(*S3Uploader)
 				So(ok, ShouldBeTrue)
 				So(original.region, ShouldEqual, "us-west-2")
 				So(original.bucket, ShouldEqual, "my.bucket.com")
@@ -82,6 +84,7 @@ func TestImageUploaderFactory(t *testing.T) {
 			setting.ImageUploadProvider = "webdav"
 
 			webdavSec, err := setting.Cfg.GetSection("external_image_storage.webdav")
+			So(err, ShouldBeNil)
 			webdavSec.NewKey("url", "webdavUrl")
 			webdavSec.NewKey("username", "username")
 			webdavSec.NewKey("password", "password")
@@ -107,14 +110,14 @@ func TestImageUploaderFactory(t *testing.T) {
 			setting.ImageUploadProvider = "gcs"
 
 			gcpSec, err := setting.Cfg.GetSection("external_image_storage.gcs")
+			So(err, ShouldBeNil)
 			gcpSec.NewKey("key_file", "/etc/secrets/project-79a52befa3f6.json")
 			gcpSec.NewKey("bucket", "project-grafana-east")
 
 			uploader, err := NewImageUploader()
-
 			So(err, ShouldBeNil)
-			original, ok := uploader.(*GCSUploader)
 
+			original, ok := uploader.(*GCSUploader)
 			So(ok, ShouldBeTrue)
 			So(original.keyFile, ShouldEqual, "/etc/secrets/project-79a52befa3f6.json")
 			So(original.bucket, ShouldEqual, "project-grafana-east")
@@ -128,15 +131,15 @@ func TestImageUploaderFactory(t *testing.T) {
 
 			Convey("with container name", func() {
 				azureBlobSec, err := setting.Cfg.GetSection("external_image_storage.azure_blob")
+				So(err, ShouldBeNil)
 				azureBlobSec.NewKey("account_name", "account_name")
 				azureBlobSec.NewKey("account_key", "account_key")
 				azureBlobSec.NewKey("container_name", "container_name")
 
 				uploader, err := NewImageUploader()
-
 				So(err, ShouldBeNil)
-				original, ok := uploader.(*AzureBlobUploader)
 
+				original, ok := uploader.(*AzureBlobUploader)
 				So(ok, ShouldBeTrue)
 				So(original.account_name, ShouldEqual, "account_name")
 				So(original.account_key, ShouldEqual, "account_key")
