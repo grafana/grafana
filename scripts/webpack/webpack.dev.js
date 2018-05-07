249c1e8d3dbbca74e7a060fccc1952c7179d94b4
Torkel Ödegaard <torkel@grafana.com>
2018-05-07 14:02:38 +0200

    fix: loading of css url (images/fonts)

--- scripts/webpack/webpack.dev.js
+++ scripts/webpack/webpack.dev.js
@@ -85,7 +85,7 @@ module.exports = merge(common, {
         ]
       },
       require('./sass.rule.js')({
-        sourceMap: true, minimize: false, preserveUrl: true
+        sourceMap: true, minimize: false, preserveUrl: false
       }, extractSass),
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
