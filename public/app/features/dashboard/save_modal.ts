4df24614713e3c97624bad69fc0c35ec02700aeb
Patrick O'Carroll <ocarroll.patrick@gmail.com>
2018-05-24 09:25:26 +0200

    added two spans to button to switch between, added class to succes-btn, created function that switch span and add class

--- public/app/features/dashboard/save_modal.ts
+++ public/app/features/dashboard/save_modal.ts
@@ -39,7 +39,10 @@ const template = `
     </div>
 
     <div class="gf-form-button-row text-center">
-      <button type="submit" class="btn btn-success" ng-disabled="ctrl.saveForm.$invalid">Save</button>
+      <button type="submit" id="saveButton" class="btn btn-success" ng-disabled="ctrl.saveForm.$invalid">
+        <span ng-if="!ctrl.saving">Save</span>
+        <span ng-if="ctrl.saving === true">Saving...</span>
+      </button>
       <button class="btn btn-inverse" ng-click="ctrl.dismiss();">Cancel</button>
     </div>
   </form>
@@ -50,12 +53,14 @@ export class SaveDashboardModalCtrl {
   message: string;
   max: number;
   saveForm: any;
+  saving: boolean;
   dismiss: () => void;
 
   /** @ngInject */
   constructor(private dashboardSrv) {
     this.message = '';
     this.max = 64;
+    this.saving = false;
   }
 
   save() {
@@ -63,11 +68,19 @@ export class SaveDashboardModalCtrl {
       return;
     }
 
+    this.setSaving();
+
     var dashboard = this.dashboardSrv.getCurrent();
     var saveModel = dashboard.getSaveModelClone();
     var options = { message: this.message };
 
     return this.dashboardSrv.save(saveModel, options).then(this.dismiss);
+  }
+
+  setSaving() {
+    let saveButton = document.querySelector('#saveButton');
+    saveButton.className += ' saving';
+    this.saving = true;
   }
 }
 
