98dd759d31ed445917763391f7d1ddfd6922ea01
Serj <msergg@users.noreply.github.com>
2018-05-29 14:45:13 +0300

    Update bms_ip2sms_test.go
    update tests

--- pkg/services/alerting/notifiers/bms_ip2sms_test.go
+++ pkg/services/alerting/notifiers/bms_ip2sms_test.go
@@ -1,171 +1,52 @@
 package notifiers
 
 import (
-	"mime/multipart"
-	"reflect"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/log"
+	"github.com/grafana/grafana/pkg/components/simplejson"
 	m "github.com/grafana/grafana/pkg/models"
-	"github.com/grafana/grafana/pkg/services/alerting"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestNewBMSNotifier(t *testing.T) {
-	type args struct {
-		model *m.AlertNotification
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    alerting.Notifier
-		wantErr bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewBMSNotifier(tt.args.model)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewBMSNotifier() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewBMSNotifier() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+func TestBMSNotifier(t *testing.T) {
+	Convey("BMS notifier tests", t, func() {
 
-func TestBMSNotifier_buildMessage(t *testing.T) {
-	type fields struct {
-		NotifierBase NotifierBase
-		Alphaname    string
-		Username     string
-		Password     string
-		Msisdn       string
-		log          log.Logger
-	}
-	type args struct {
-		evalContext *alerting.EvalContext
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   *m.SendWebhookSync
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			this := &BMSNotifier{
-				NotifierBase: tt.fields.NotifierBase,
-				Alphaname:    tt.fields.Alphaname,
-				Username:     tt.fields.Username,
-				Password:     tt.fields.Password,
-				Msisdn:       tt.fields.Msisdn,
-				log:          tt.fields.log,
-			}
-			if got := this.buildMessage(tt.args.evalContext); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("BMSNotifier.buildMessage() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+		Convey("Parsing alert notification from settings", func() {
+			Convey("empty settings should return error", func() {
+				json := `{ }`
 
-func TestBMSNotifier_generateBMSCmd(t *testing.T) {
-	type fields struct {
-		NotifierBase NotifierBase
-		Alphaname    string
-		Username     string
-		Password     string
-		Msisdn       string
-		log          log.Logger
-	}
-	type args struct {
-		message_txt  string
-		messageField string
-		apiAction    string
-		extraConf    func(writer *multipart.Writer)
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   *m.SendWebhookSync
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			this := &BMSNotifier{
-				NotifierBase: tt.fields.NotifierBase,
-				Alphaname:    tt.fields.Alphaname,
-				Username:     tt.fields.Username,
-				Password:     tt.fields.Password,
-				Msisdn:       tt.fields.Msisdn,
-				log:          tt.fields.log,
-			}
-			if got := this.generateBMSCmd(tt.args.message_txt, tt.args.messageField, tt.args.apiAction, tt.args.extraConf); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("BMSNotifier.generateBMSCmd() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+				settingsJSON, _ := simplejson.NewJson([]byte(json))
+				model := &m.AlertNotification{
+					Name:     "bms_testing",
+					Type:     "bms",
+					Settings: settingsJSON,
+				}
 
-func Test_generateMetricsMessageBMS(t *testing.T) {
-	type args struct {
-		evalContext *alerting.EvalContext
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := generateMetricsMessageBMS(tt.args.evalContext); got != tt.want {
-				t.Errorf("generateMetricsMessageBMS() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+				_, err := NewBMSNotifier(model)
+				So(err, ShouldNotBeNil)
+			})
 
-func TestBMSNotifier_Notify(t *testing.T) {
-	type fields struct {
-		NotifierBase NotifierBase
-		Alphaname    string
-		Username     string
-		Password     string
-		Msisdn       string
-		log          log.Logger
-	}
-	type args struct {
-		evalContext *alerting.EvalContext
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			this := &BMSNotifier{
-				NotifierBase: tt.fields.NotifierBase,
-				Alphaname:    tt.fields.Alphaname,
-				Username:     tt.fields.Username,
-				Password:     tt.fields.Password,
-				Msisdn:       tt.fields.Msisdn,
-				log:          tt.fields.log,
-			}
-			if err := this.Notify(tt.args.evalContext); (err != nil) != tt.wantErr {
-				t.Errorf("BMSNotifier.Notify() error = %v, wantErr %v", err, tt.wantErr)
-			}
+			Convey("from settings", func() {
+				json := `
+				{
+          "alphaname": "http://TESTAI"
+				}`
+
+				settingsJSON, _ := simplejson.NewJson([]byte(json))
+				model := &m.AlertNotification{
+					Name:     "bms_testing",
+					Type:     "bms",
+					Settings: settingsJSON,
+				}
+
+				not, err := NewBMSNotifier(model)
+				bmsNotifier := not.(*BMSNotifier)
+
+				So(err, ShouldBeNil)
+				So(bmsNotifier.Name, ShouldEqual, "bms_testing")
+				So(bmsNotifier.Type, ShouldEqual, "bms")
+				So(bmsNotifier.URL, ShouldEqual, "TESTAI")
+			})
 		})
-	}
+	})
 }
