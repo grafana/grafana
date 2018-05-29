3ce5810bdd3912d9d1f9a8280f441861ecd4cf3c
Serj <msergg@users.noreply.github.com>
2018-05-29 14:33:26 +0300

    Update bms_ip2sms_test.go
    Simplify test

--- pkg/services/alerting/notifiers/bms_ip2sms_test.go
+++ pkg/services/alerting/notifiers/bms_ip2sms_test.go
@@ -1,3 +1,4 @@
+test.go
 package notifiers
 
 import (
@@ -55,72 +56,6 @@ func TestBMSNotifier(t *testing.T) {
 				So(bmsNotifier.Password, ShouldEqual, "pass")
 				So(bmsNotifier.Msisdn, ShouldEqual, "3801144553")
 			})
-
-			Convey("generateCaption should generate a message with all pertinent details", func() {
-				evalContext := alerting.NewEvalContext(nil, &alerting.Rule{
-					Name:    "This is an alarm",
-					Message: "Some kind of message.",
-					State:   m.AlertStateOK,
-				})
-
-				caption := generateImageCaption(evalContext, "http://grafa.url/abcdef", "")
-				So(len(caption), ShouldBeLessThanOrEqualTo, 200)
-				So(caption, ShouldContainSubstring, "Some kind of message.")
-				So(caption, ShouldContainSubstring, "[OK] This is an alarm")
-				So(caption, ShouldContainSubstring, "http://grafa.url/abcdef")
-			})
-
-			Convey("When generating a message", func() {
-
-				Convey("URL should be skipped if it's too long", func() {
-					evalContext := alerting.NewEvalContext(nil, &alerting.Rule{
-						Name:    "This is an alarm",
-						Message: "Some kind of message.",
-						State:   m.AlertStateOK,
-					})
-
-					caption := generateImageCaption(evalContext,
-						"http://grafa.url/abcdefaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-						"foo bar")
-					So(len(caption), ShouldBeLessThanOrEqualTo, 200)
-					So(caption, ShouldContainSubstring, "Some kind of message.")
-					So(caption, ShouldContainSubstring, "[OK] This is an alarm")
-					So(caption, ShouldContainSubstring, "foo bar")
-					So(caption, ShouldNotContainSubstring, "http")
-				})
-
-				Convey("Message should be trimmed if it's too long", func() {
-					evalContext := alerting.NewEvalContext(nil, &alerting.Rule{
-						Name:    "This is an alarm",
-						Message: "Some kind of message that is too long for appending to our pretty little message, this line is actually exactly 197 chars long and I will get there in the end I promise I will. Yes siree that's it.",
-						State:   m.AlertStateOK,
-					})
-
-					caption := generateImageCaption(evalContext,
-						"http://grafa.url/foo",
-						"")
-					So(len(caption), ShouldBeLessThanOrEqualTo, 200)
-					So(caption, ShouldContainSubstring, "[OK] This is an alarm")
-					So(caption, ShouldNotContainSubstring, "http")
-					So(caption, ShouldContainSubstring, "Some kind of message that is too long for appending to our pretty little message, this line is actually exactly 197 chars long and I will get there in the end I promise ")
-				})
-
-				Convey("Metrics should be skipped if they don't fit", func() {
-					evalContext := alerting.NewEvalContext(nil, &alerting.Rule{
-						Name:    "This is an alarm",
-						Message: "Some kind of message that is too long for appending to our pretty little message, this line is actually exactly 197 chars long and I will get there in the end I ",
-						State:   m.AlertStateOK,
-					})
-
-					caption := generateImageCaption(evalContext,
-						"http://grafa.url/foo",
-						"foo bar long song")
-					So(len(caption), ShouldBeLessThanOrEqualTo, 200)
-					So(caption, ShouldContainSubstring, "[OK] This is an alarm")
-					So(caption, ShouldNotContainSubstring, "http")
-					So(caption, ShouldNotContainSubstring, "foo bar")
-				})
-			})
-		})
-	})
+	}
+}
 }
