516839d7b22b5a1fc75c698d47f87e0d4a59ed44
Anton Sergeyev <sergeyev.anton.s@gmail.com>
2018-06-14 12:35:22 +0500

    #11607 Cleanup time of temporary files is now configurable

---
+++ pkg/services/cleanup/cleanup_test.go
@@ -0,0 +1,42 @@
+package cleanup
+
+import (
+	"testing"
+
+	"github.com/grafana/grafana/pkg/setting"
+	. "github.com/smartystreets/goconvey/convey"
+	"time"
+)
+
+func TestCleanUpTmpFiles(t *testing.T) {
+	Convey("Cleanup service tests", t, func() {
+		cfg := setting.Cfg{}
+		cfg.TempDataLifetime, _ = time.ParseDuration("24h")
+		service := CleanUpService{
+			Cfg: &cfg,
+		}
+		now := time.Now()
+		secondAgo := now.Add(-time.Second)
+		dayAgo := now.Add(-time.Second * 3600 * 24 * 7)
+		weekAgo := now.Add(-time.Second * 3600 * 24 * 7)
+
+		Convey("Should not cleanup recent files", func() {
+			So(service.shouldCleanupTempFile(secondAgo, now), ShouldBeFalse);
+		})
+
+		Convey("Should cleanup older files", func() {
+			So(service.shouldCleanupTempFile(dayAgo, now), ShouldBeTrue);
+		})
+
+		Convey("After increasing temporary files lifetime, older files should be kept", func() {
+			cfg.TempDataLifetime, _ = time.ParseDuration("1000h")
+			So(service.shouldCleanupTempFile(weekAgo, now), ShouldBeFalse);
+		})
+
+		Convey("If lifetime is 0, files should never be cleaned up", func() {
+			cfg.TempDataLifetime = 0
+			So(service.shouldCleanupTempFile(weekAgo, now), ShouldBeFalse);
+		})
+	})
+
+}
