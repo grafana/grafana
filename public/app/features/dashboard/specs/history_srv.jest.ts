6c136c128e5d97c087a9a08e0348191503da0bbf
Tobias Skarhed <tobias.skarhed@gmail.com>
2018-06-19 11:23:57 +0200

    Fix TS errors

--- public/app/features/dashboard/specs/history_srv.jest.ts
+++ public/app/features/dashboard/specs/history_srv.jest.ts
@@ -1,40 +1,46 @@
 import '../history/history_srv';
 import { versions, restore } from './history_mocks';
 import { HistorySrv } from '../history/history_srv';
+import { DashboardModel } from '../dashboard_model';
+jest.mock('app/core/store');
 
 describe('historySrv', function() {
-  var versionsResponse = versions();
-  var restoreResponse = restore;
+  const versionsResponse = versions();
+  const restoreResponse = restore;
 
-  var backendSrv = {
+  let backendSrv = {
     get: jest.fn(() => Promise.resolve({})),
     post: jest.fn(() => Promise.resolve({})),
   };
 
-  var q = {
+  let q = {
     when: jest.fn(() => Promise.resolve([])),
   };
 
-  var historySrv = new HistorySrv(backendSrv, q);
+  let historySrv = new HistorySrv(backendSrv, q);
+
+  const dash = new DashboardModel({ id: 1 });
+  const emptyDash = new DashboardModel({});
+  const historyListOpts = { limit: 10, start: 0 };
 
   describe('getHistoryList', function() {
     it('should return a versions array for the given dashboard id', function() {
       backendSrv.get = jest.fn(() => Promise.resolve(versionsResponse));
       historySrv = new HistorySrv(backendSrv, q);
 
-      return historySrv.getHistoryList({ id: 1 }).then(function(versions) {
+      return historySrv.getHistoryList(dash, historyListOpts).then(function(versions) {
         expect(versions).toEqual(versionsResponse);
       });
     });
 
     it('should return an empty array when not given an id', function() {
-      return historySrv.getHistoryList({}).then(function(versions) {
+      return historySrv.getHistoryList(emptyDash, historyListOpts).then(function(versions) {
         expect(versions).toEqual([]);
       });
     });
 
     it('should return an empty array when not given a dashboard', function() {
-      return historySrv.getHistoryList().then(function(versions) {
+      return historySrv.getHistoryList(null, historyListOpts).then(function(versions) {
         expect(versions).toEqual([]);
       });
     });
@@ -45,7 +51,7 @@ describe('historySrv', function() {
       let version = 6;
       backendSrv.post = jest.fn(() => Promise.resolve(restoreResponse(version)));
       historySrv = new HistorySrv(backendSrv, q);
-      return historySrv.restoreDashboard({ id: 1 }, version).then(function(response) {
+      return historySrv.restoreDashboard(dash, version).then(function(response) {
         expect(response).toEqual(restoreResponse(version));
       });
     });
@@ -53,7 +59,7 @@ describe('historySrv', function() {
     it('should return an empty object when not given an id', function() {
       q.when = jest.fn(() => Promise.resolve({}));
       historySrv = new HistorySrv(backendSrv, q);
-      return historySrv.restoreDashboard({}, 6).then(function(response) {
+      return historySrv.restoreDashboard(emptyDash, 6).then(function(response) {
         expect(response).toEqual({});
       });
     });
