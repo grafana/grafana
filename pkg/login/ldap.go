6af539bbc6f86e44f025746f4887d0548bc79cc7
Dan Cech <dcech@grafana.com>
2018-06-25 12:25:24 -0400

    use strings.ToLower

--- pkg/login/ldap.go
+++ pkg/login/ldap.go
@@ -349,7 +349,7 @@ func (a *ldapAuther) searchForUser(username string) (*LdapUserInfo, error) {
 }
 
 func getLdapAttrN(name string, result *ldap.SearchResult, n int) string {
-	if name == "DN" || name == "dn" {
+	if strings.ToLower(name) == "dn" {
 		return result.Entries[n].DN
 	}
 	for _, attr := range result.Entries[n].Attributes {
