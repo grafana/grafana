79838d96414b06b74885224e1c8a515442890a7c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-07-09 13:30:12 +0000

    Rebased lukaszgryglicki 12174
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- public/app/features/templating/custom_variable.ts
+++ public/app/features/templating/custom_variable.ts
@@ -7,6 +7,7 @@ export class CustomVariable implements Variable {
   includeAll: boolean;
   multi: boolean;
   current: any;
+  skipUrlSync: boolean;
 
   defaults = {
     type: 'custom',
@@ -19,6 +20,7 @@ export class CustomVariable implements Variable {
     includeAll: false,
     multi: false,
     allValue: null,
+    skipUrlSync: false,
   };
 
   /** @ngInject **/
