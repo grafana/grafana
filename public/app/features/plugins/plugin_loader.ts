53b4f9dc2bd52955b2f768f081ba0fb755fe38a1
David Kaltschmidt <david.kaltschmidt@gmail.com>
2018-07-11 12:05:18 +0200

    Fix css loading in plugins
    - allow css loader to be imported again (wasnt prefixed by plugin)

--- public/app/features/plugins/plugin_loader.ts
+++ public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    'plugin*': {
+    '*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',
