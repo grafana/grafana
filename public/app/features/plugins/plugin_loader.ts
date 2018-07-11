b6d2561ecdb367ef41706ec316c186e70ba24843
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-07-11 21:02:23 +0900

    add rxjs libs

--- public/app/features/plugins/plugin_loader.ts
+++ public/app/features/plugins/plugin_loader.ts
@@ -33,7 +33,12 @@ import { Subject } from 'rxjs/Subject';
 // these imports add functions to Observable
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/defer';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/concat';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/combineAll';
 
 // add cache busting
