grafana/grafana

commit 4d2dd22095ba4f0f96a2d524cb43a7d71e771e5f

Fix css loading in plugins (#12573)
- allow css loader to be imported again (wasnt prefixed by plugin)

public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    'plugin*': {
+    '*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',

commit 81e62e105143f9493169d86a20bc2dd0766dab38

Fix freezing browser when loading plugin
- broken since 4d2dd2209
- `*` was previously working as a path matcher, but freezes browser when
  used with new cache-busting plugin loader
- changed matcher to be `/*`

public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    '*': {
+    '/*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',