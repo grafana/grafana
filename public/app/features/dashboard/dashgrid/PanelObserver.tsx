e939f918450e8b51e1c05c1d1cfbcc3814734233
ryan <ryantxu@gmail.com>
2018-07-12 11:46:37 +0200

    adding intersection observer polyfill

--- public/app/features/dashboard/dashgrid/PanelObserver.tsx
+++ public/app/features/dashboard/dashgrid/PanelObserver.tsx
@@ -2,6 +2,8 @@ import { PanelModel } from '../panel_model';
 
 import _ from 'lodash';
 
+import 'intersection-observer';
+
 export const PANEL_VISIBILITY_CHANGED_EVENT = 'panel-visibility-changed';
 
 export interface PanelObserver {
