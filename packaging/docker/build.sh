424aa6e564fc6419c9192a4ee6cf74550f5aad67
Leonard Gram <leo@xlson.com>
2018-07-30 16:35:30 +0200

    build: removes unused args to docker build.

--- packaging/docker/build.sh
+++ packaging/docker/build.sh
@@ -5,18 +5,15 @@ _grafana_tag=$1
 # If the tag starts with v, treat this as a official release
 if echo "$_grafana_tag" | grep -q "^v"; then
 	_grafana_version=$(echo "${_grafana_tag}" | cut -d "v" -f 2)
-	_grafana_url="https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-${_grafana_version}.linux-amd64.tar.gz"
 	_docker_repo=${2:-grafana/grafana}
 else
 	_grafana_version=$_grafana_tag
-	_grafana_url="https://s3-us-west-2.amazonaws.com/grafana-releases/master/grafana-${_grafana_version}.linux-x64.tar.gz"
 	_docker_repo=${2:-grafana/grafana-dev}
 fi
 
-echo "Building ${_docker_repo}:${_grafana_version} from ${_grafana_url}"
+echo "Building ${_docker_repo}:${_grafana_version}"
 
 docker build \
-	--build-arg GRAFANA_URL="${_grafana_url}" \
 	--tag "${_docker_repo}:${_grafana_version}" \
 	--no-cache=true .
 
