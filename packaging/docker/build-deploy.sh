99a9dbb04f161eac59cc7450c0daf5934a70129a
Leonard Gram <leo@xlson.com>
2018-07-30 18:52:49 +0200

    build: complete docker build for master and releases.

--- packaging/docker/build-deploy.sh
+++ packaging/docker/build-deploy.sh
@@ -4,8 +4,7 @@ _grafana_version=$1
 ./build.sh "$_grafana_version"
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-#./push_to_docker_hub.sh "$_grafana_version"
-echo "Would have deployed $_grafana_version"
+./push_to_docker_hub.sh "$_grafana_version"
 
 if echo "$_grafana_version" | grep -q "^master-"; then
   apk add --no-cache curl
