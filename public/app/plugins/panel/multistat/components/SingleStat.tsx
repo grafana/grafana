a50e2ab35e9ccb7bdfcbf6a8701f250b3a214f89
Alexander Zobnin <alexanderzobnin@gmail.com>
2018-08-20 18:51:36 +0300

    multistat: minor refactor

--- public/app/plugins/panel/multistat/components/SingleStat.tsx
+++ public/app/plugins/panel/multistat/components/SingleStat.tsx
@@ -40,65 +40,46 @@ export class SingleStat extends React.PureComponent<SingleStatProps> {
   };
 
   render() {
-    const { width, height, label, value, layout, colorBackground, colorValue, flotpairs, labelToTheLeft } = this.props;
-    const valueColor = this.props.color;
-    const bgColor = getBGColor(valueColor, BACKGROUND_OPACITY);
-
-    let containerStyle: React.CSSProperties = {};
-    const MARGIN_COEF = 0.05;
-    const MAX_MARGIN = 8;
-    let horizontalMargin = 0;
-    if (layout === 'vertical') {
-      containerStyle.height = height;
-      const margin = Math.min(Math.ceil(height * MARGIN_COEF), MAX_MARGIN);
-      containerStyle.marginBottom = margin;
-    } else {
-      containerStyle.width = width;
-      horizontalMargin = Math.min(Math.ceil(width * MARGIN_COEF), MAX_MARGIN);
-      containerStyle.marginLeft = horizontalMargin / 2;
-      containerStyle.marginRight = horizontalMargin / 2;
-    }
+    const {
+      width,
+      height,
+      label,
+      value,
+      layout,
+      colorBackground,
+      colorValue,
+      color,
+      flotpairs,
+      labelToTheLeft,
+    } = this.props;
+    const bgColor = getBGColor(color, BACKGROUND_OPACITY);
+    const containerStyle = getContainerStyle(width, height, layout, colorBackground, bgColor);
 
     const showSparkline = this.props.sparkline && this.props.sparkline.show;
-    const sparklinePadding = Math.ceil(width * 0.1);
-    const sparklineWidth = width - horizontalMargin - sparklinePadding;
-    const sparklineHeight = showSparkline ? Math.ceil(height * SPARKLINE_HEIGHT) : 0;
-    const sparklineSize = { w: sparklineWidth, h: sparklineHeight };
-    const sparklineClass = 'multistat-sparkline';
-    const sparklineStyles = { left: sparklinePadding / 2 };
-
-    if (colorBackground) {
-      containerStyle.background = bgColor;
-    }
+    const horizontalMargin = ((containerStyle.marginLeft as number) || 0) * 2;
+    const sparklineProps = getSparklineProps(width, height, horizontalMargin);
+    const sparklineHeight = showSparkline ? sparklineProps.size.h : 0;
 
     const labelFontSize = Math.floor((this.props.labelFontSize || this.props.fontSize) * LABEL_SIZE_COEF);
-    let valueFontSize = this.props.fontSize;
+    const valueFontSize = this.props.fontSize;
     const labelStyle: React.CSSProperties = {
       fontSize: labelFontSize,
     };
-    const labelContainerStyle: React.CSSProperties = {
-      lineHeight: labelFontSize + 'px',
-    };
-
-    let valueStyle: React.CSSProperties = {
-      fontSize: valueFontSize + 'px',
-    };
-    if (colorValue) {
-      valueStyle.color = valueColor;
-    }
 
     const valueTopOffset = getTopOffset(height, sparklineHeight, labelFontSize, valueFontSize, labelToTheLeft);
-    const valueTopOffsetPx = valueTopOffset + 'px';
     const valueContainerStyle: React.CSSProperties = {
-      top: valueTopOffsetPx,
+      top: valueTopOffset + 'px',
     };
 
-    if (labelToTheLeft) {
-      const labelTopOffset = Math.floor(valueTopOffset + (valueFontSize - labelFontSize) / 2);
-      labelContainerStyle.top = labelTopOffset;
-    } else {
-      labelContainerStyle.marginTop = (height - sparklineHeight) * 0.02 + 'px';
-    }
+    const valueStyle = getValueStyle(valueFontSize, colorValue, color);
+    const labelContainerStyle = getLabelContainerStyle(
+      height,
+      labelFontSize,
+      valueFontSize,
+      valueTopOffset,
+      labelToTheLeft,
+      sparklineHeight
+    );
 
     return (
       <div className={`multistat-single ${labelToTheLeft ? 'label-left' : ''}`} style={containerStyle}>
@@ -112,18 +93,78 @@ export class SingleStat extends React.PureComponent<SingleStatProps> {
             {value}
           </span>
         </div>
-        {showSparkline && (
-          <SparkLine
-            color={valueColor}
-            size={sparklineSize}
-            flotpairs={flotpairs}
-            customClass={sparklineClass}
-            customStyles={sparklineStyles}
-          />
-        )}
+        {showSparkline && <SparkLine color={color} flotpairs={flotpairs} {...sparklineProps} />}
       </div>
     );
   }
+}
+
+function getContainerStyle(
+  width: number,
+  height: number,
+  layout: MultiStatPanel.PanelLayout,
+  colorBackground: boolean,
+  bgColor: string
+): React.CSSProperties {
+  let containerStyle: React.CSSProperties = {};
+  const MARGIN_COEF = 0.05;
+  const MAX_MARGIN = 8;
+  if (layout === 'vertical') {
+    containerStyle.height = height;
+    const margin = Math.min(Math.ceil(height * MARGIN_COEF), MAX_MARGIN);
+    containerStyle.marginBottom = margin;
+  } else {
+    containerStyle.width = width;
+    const margin = Math.min(Math.ceil(width * MARGIN_COEF), MAX_MARGIN);
+    containerStyle.marginLeft = margin / 2;
+    containerStyle.marginRight = margin / 2;
+  }
+
+  if (colorBackground) {
+    containerStyle.background = bgColor;
+  }
+
+  return containerStyle;
+}
+
+function getLabelContainerStyle(
+  height: number,
+  labelFontSize: number,
+  valueFontSize: number,
+  valueTopOffset: number,
+  labelToTheLeft: boolean,
+  sparklineHeight: number
+): React.CSSProperties {
+  let labelContainerStyle: React.CSSProperties = {
+    lineHeight: labelFontSize + 'px',
+  };
+  if (labelToTheLeft) {
+    const labelTopOffset = Math.floor(valueTopOffset + (valueFontSize - labelFontSize) / 2);
+    labelContainerStyle.top = labelTopOffset;
+  } else {
+    labelContainerStyle.marginTop = (height - sparklineHeight) * 0.02 + 'px';
+  }
+  return labelContainerStyle;
+}
+
+function getValueStyle(valueFontSize, colorValue, color): React.CSSProperties {
+  let valueStyle: React.CSSProperties = {
+    fontSize: valueFontSize + 'px',
+  };
+  if (colorValue) {
+    valueStyle.color = color;
+  }
+  return valueStyle;
+}
+
+function getSparklineProps(width: number, height: number, margin: number) {
+  const sparklinePadding = Math.ceil(width * 0.1);
+  const sparklineWidth = width - margin - sparklinePadding;
+  const sparklineHeight = Math.ceil(height * SPARKLINE_HEIGHT);
+  const size = { w: sparklineWidth, h: sparklineHeight };
+  const customClass = 'multistat-sparkline';
+  const customStyles = { left: sparklinePadding / 2 };
+  return { size, customClass, customStyles };
 }
 
 function getTopOffset(height, sparklineHeight, labelFontSize, valueFontSize, labelToTheLeft?) {
