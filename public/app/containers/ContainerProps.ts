d8b3fa01d07429f0af13fdb09818f4111fc10197
Torkel Ödegaard <torkel@grafana.com>
2018-09-04 22:38:18 -0700

    mobx: removed unused SearchStore

--- public/app/containers/ContainerProps.ts
+++ public/app/containers/ContainerProps.ts
@@ -1,11 +1,9 @@
-﻿import { SearchStore } from './../stores/SearchStore/SearchStore';
 import { NavStore } from './../stores/NavStore/NavStore';
 import { PermissionsStore } from './../stores/PermissionsStore/PermissionsStore';
 import { ViewStore } from './../stores/ViewStore/ViewStore';
 import { FolderStore } from './../stores/FolderStore/FolderStore';
 
 interface ContainerProps {
-  search: typeof SearchStore.Type;
   nav: typeof NavStore.Type;
   permissions: typeof PermissionsStore.Type;
   view: typeof ViewStore.Type;
--- public/app/stores/RootStore/RootStore.ts
+++ public/app/stores/RootStore/RootStore.ts
@@ -1,5 +1,4 @@
 import { types } from 'mobx-state-tree';
-import { SearchStore } from './../SearchStore/SearchStore';
 import { NavStore } from './../NavStore/NavStore';
 import { ViewStore } from './../ViewStore/ViewStore';
 import { FolderStore } from './../FolderStore/FolderStore';
@@ -7,9 +6,6 @@ import { PermissionsStore } from './../PermissionsStore/PermissionsStore';
 import { TeamsStore } from './../TeamsStore/TeamsStore';
 
 export const RootStore = types.model({
-  search: types.optional(SearchStore, {
-    sections: [],
-  }),
   nav: types.optional(NavStore, {}),
   permissions: types.optional(PermissionsStore, {
     fetching: false,
