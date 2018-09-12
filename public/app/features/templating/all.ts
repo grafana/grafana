f0e905f3c9993135603b74bb6de3b9b41e31e404
Dan Doyle <daldoyle@globalnoc.iu.edu>
2018-09-12 13:17:15 +0000

    First pass at a text based template var, getting feedback from devs

--- public/app/features/templating/all.ts
+++ public/app/features/templating/all.ts
@@ -9,6 +9,7 @@ import { DatasourceVariable } from './datasource_variable';
 import { CustomVariable } from './custom_variable';
 import { ConstantVariable } from './constant_variable';
 import { AdhocVariable } from './adhoc_variable';
+import { TextVariable } from './text_variable';
 
 coreModule.factory('templateSrv', () => {
   return templateSrv;
@@ -22,4 +23,5 @@ export {
   CustomVariable,
   ConstantVariable,
   AdhocVariable,
+  TextVariable
 };
