3211df7303b922d7899ed7a973039e1a3f430570
Peter Holmberg <peter.hlmbrg@gmail.com>
2018-10-01 13:45:00 +0200

    filter users in selector based on search

--- public/app/features/users/state/selectors.ts
+++ public/app/features/users/state/selectors.ts
@@ -1,2 +1,9 @@
-export const getUsers = state => state.users;
+export const getUsers = state => {
+  const regex = new RegExp(state.searchQuery, 'i');
+
+  return state.users.filter(user => {
+    return regex.test(user.login) || regex.test(user.email);
+  });
+};
+
 export const getUsersSearchQuery = state => state.searchQuery;
