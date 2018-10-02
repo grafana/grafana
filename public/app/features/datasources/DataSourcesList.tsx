8fd1d8a057619576b9b13cc963241b15debb92dc
Torkel Ödegaard <torkel@grafana.com>
2018-10-02 09:27:02 +0200

    changed from RFC to PureComponent

--- public/app/features/datasources/DataSourcesList.tsx
+++ public/app/features/datasources/DataSourcesList.tsx
@@ -1,4 +1,4 @@
-import React, { SFC } from 'react';
+import React, { PureComponent } from 'react';
 import classNames from 'classnames/bind';
 import DataSourcesListItem from './DataSourcesListItem';
 import { DataSource } from 'app/types';
@@ -9,24 +9,26 @@ export interface Props {
   layoutMode: LayoutMode;
 }
 
-const DataSourcesList: SFC<Props> = props => {
-  const { dataSources, layoutMode } = props;
+export class DataSourcesList extends PureComponent<Props> {
+  render() {
+    const { dataSources, layoutMode } = this.props;
 
-  const listStyle = classNames({
-    'card-section': true,
-    'card-list-layout-grid': layoutMode === LayoutModes.Grid,
-    'card-list-layout-list': layoutMode === LayoutModes.List,
-  });
+    const listStyle = classNames({
+      'card-section': true,
+      'card-list-layout-grid': layoutMode === LayoutModes.Grid,
+      'card-list-layout-list': layoutMode === LayoutModes.List,
+    });
 
-  return (
-    <section className={listStyle}>
-      <ol className="card-list">
-        {dataSources.map((dataSource, index) => {
-          return <DataSourcesListItem dataSource={dataSource} key={`${dataSource.id}-${index}`} />;
-        })}
-      </ol>
-    </section>
-  );
-};
+    return (
+      <section className={listStyle}>
+        <ol className="card-list">
+          {dataSources.map((dataSource, index) => {
+            return <DataSourcesListItem dataSource={dataSource} key={`${dataSource.id}-${index}`} />;
+          })}
+        </ol>
+      </section>
+    );
+  }
+}
 
 export default DataSourcesList;
