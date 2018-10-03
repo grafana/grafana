afdec3d1b09b526eadfdd502709925e692a66aa3
Torkel Ödegaard <torkel@grafana.com>
2018-10-03 09:43:10 +0200

    Revert "Revert "Org users to react""

--- public/app/features/plugins/state/actions.ts
+++ public/app/features/plugins/state/actions.ts
@@ -24,7 +24,7 @@ export interface SetLayoutModeAction {
   payload: LayoutMode;
 }
 
-export const setLayoutMode = (mode: LayoutMode): SetLayoutModeAction => ({
+export const setPluginsLayoutMode = (mode: LayoutMode): SetLayoutModeAction => ({
   type: ActionTypes.SetLayoutMode,
   payload: mode,
 });
