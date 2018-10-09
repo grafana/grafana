b74e76298f704d871b5edc1bfb704da6bfd7d860
praveensastry <sastry.praveen@gmail.com>
2018-10-09 13:40:17 +1100

    render drag handle only in edit mode

--- public/app/features/dashboard/dashgrid/DashboardRow.tsx
+++ public/app/features/dashboard/dashgrid/DashboardRow.tsx
@@ -87,6 +87,7 @@ export class DashboardRow extends React.Component<DashboardRowProps, any> {
     const title = templateSrv.replaceWithText(this.props.panel.title, this.props.panel.scopedVars);
     const count = this.props.panel.panels ? this.props.panel.panels.length : 0;
     const panels = count === 1 ? 'panel' : 'panels';
+    const editModeEnabled = this.dashboard.meta.canEdit === true;
 
     return (
       <div className={classes}>
@@ -112,7 +113,7 @@ export class DashboardRow extends React.Component<DashboardRowProps, any> {
             &nbsp;
           </div>
         )}
-        <div className="dashboard-row__drag grid-drag-handle" />
+        {editModeEnabled && <div className="dashboard-row__drag grid-drag-handle" />}
       </div>
     );
   }
