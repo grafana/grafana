19b69a82afcf9586a6da07f1635d355c78a3b2cc
Dan Cech <dcech@grafana.com>
2018-10-11 15:26:06 -0400

    fmt

--- pkg/api/org_users.go
+++ pkg/api/org_users.go
@@ -103,8 +103,8 @@ func updateOrgUserHelper(cmd m.UpdateOrgUserCommand) Response {
 // DELETE /api/org/users/:userId
 func RemoveOrgUserForCurrentOrg(c *m.ReqContext) Response {
 	return removeOrgUserHelper(&m.RemoveOrgUserCommand{
-		UserId: c.ParamsInt64(":userId"),
-		OrgId:  c.OrgId,
+		UserId:                   c.ParamsInt64(":userId"),
+		OrgId:                    c.OrgId,
 		ShouldDeleteOrphanedUser: true,
 	})
 }
