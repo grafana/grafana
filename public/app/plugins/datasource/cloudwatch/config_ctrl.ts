c64a5a6e0ed12fd8714d8a4d60c432f2911ad79c
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-10-17 14:02:02 +0900

    re-add hard coded region list

--- public/app/plugins/datasource/cloudwatch/config_ctrl.ts
+++ public/app/plugins/datasource/cloudwatch/config_ctrl.ts
@@ -41,7 +41,32 @@ export class CloudWatchConfigCtrl {
     { name: 'Yearly', value: 'Yearly', example: '[logstash-]YYYY' },
   ];
 
-  regions = [];
+  regions = [
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-northeast-3',
+    'ap-south-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ca-central-1',
+    'cn-north-1',
+    'cn-northwest-1',
+    'eu-central-1',
+    'eu-north-1',
+    'eu-west-1',
+    'eu-west-2',
+    'eu-west-3',
+    'me-south-1',
+    'sa-east-1',
+    'us-east-1',
+    'us-east-2',
+    'us-gov-east-1',
+    'us-gov-west-1',
+    'us-iso-east-1',
+    'us-isob-east-1',
+    'us-west-1',
+    'us-west-2',
+  ];
 
   getRegions() {
     this.datasourceSrv
