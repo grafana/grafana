24c433cbe27412dca4d63a1324d13010fd704018
Johannes Schill <johannes.schill@polyester.se>
2018-10-24 08:39:29 +0200

    Move the variable regex to constants to make sure we use the same regex everywhere and add tests with the new variable syntax #13600

--- public/app/features/templating/variable.ts
+++ public/app/features/templating/variable.ts
@@ -1,5 +1,5 @@
-import kbn from 'app/core/utils/kbn';
 import { assignModelProperties } from 'app/core/utils/model_utils';
+import { variableRegex, variableRegexExec } from 'app/core/constants';
 
 export interface Variable {
   setValue(option);
@@ -14,15 +14,16 @@ export let variableTypes = {};
 export { assignModelProperties };
 
 export function containsVariable(...args: any[]) {
-  let variableName = args[args.length - 1];
-  let str = args[0] || '';
+  const variableName = args[args.length - 1];
+  const variableString = args.slice(0, -1).join(' ');
+  const matches = variableString.match(variableRegex);
+  const isMatchingVariable =
+    matches !== null
+      ? matches.find(match => {
+          const varMatch = variableRegexExec(match);
+          return varMatch !== null && varMatch.indexOf(variableName) > -1;
+        })
+      : false;
 
-  for (let i = 1; i < args.length - 1; i++) {
-    str += ' ' + args[i] || '';
-  }
-
-  variableName = kbn.regexEscape(variableName);
-  const findVarRegex = new RegExp('\\$(' + variableName + ')(?:\\W|$)|\\[\\[(' + variableName + ')\\]\\]', 'g');
-  const match = findVarRegex.exec(str);
-  return match !== null;
+  return !!isMatchingVariable;
 }
