b7787db34e2b71cdc59aef829ea1a3d69b0a1e3c
Athurg Feng <i@atfeng.com>
2018-11-08 18:44:00 +0800

    Add new option to set where to open the message url

--- pkg/services/alerting/notifiers/dingding.go
+++ pkg/services/alerting/notifiers/dingding.go
@@ -2,6 +2,7 @@ package notifiers
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/components/simplejson"
@@ -15,11 +16,16 @@ const DingdingOptionsTemplate = `
       <h3 class="page-heading">DingDing settings</h3>
       <div class="gf-form">
         <span class="gf-form-label width-10">Url</span>
-        <input type="text" required class="gf-form-input max-width-26" ng-model="ctrl.model.settings.url" placeholder="https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxx"></input>
+        <input type="text" required class="gf-form-input max-width-70" ng-model="ctrl.model.settings.url" placeholder="https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxx"></input>
       </div>
       <div class="gf-form">
         <span class="gf-form-label width-10">MessageType</span>
         <select class="gf-form-input max-width-14" ng-model="ctrl.model.settings.msgType" ng-options="s for s in ['link','actionCard']" ng-init="ctrl.model.settings.msgType=ctrl.model.settings.msgType || '` + DefaultDingdingMsgType + `'"></select>
+      </div>
+      <div class="gf-form">
+        <span class="gf-form-label width-10">OpenInBrowser</span>
+        <gf-form-switch class="gf-form" checked="ctrl.model.settings.openInBrowser"></gf-form-switch>
+        <info-popover mode="right-normal">Open the message url in browser instead of inside of Dingding</info-popover>
       </div>
 `
 
@@ -41,20 +47,23 @@ func NewDingDingNotifier(model *m.AlertNotification) (alerting.Notifier, error) 
 	}
 
 	msgType := model.Settings.Get("msgType").MustString(DefaultDingdingMsgType)
+	openInBrowser := model.Settings.Get("openInBrowser").MustBool(true)
 
 	return &DingDingNotifier{
-		NotifierBase: NewNotifierBase(model),
-		MsgType:      msgType,
-		Url:          url,
-		log:          log.New("alerting.notifier.dingding"),
+		NotifierBase:  NewNotifierBase(model),
+		OpenInBrowser: openInBrowser,
+		MsgType:       msgType,
+		Url:           url,
+		log:           log.New("alerting.notifier.dingding"),
 	}, nil
 }
 
 type DingDingNotifier struct {
 	NotifierBase
-	MsgType string
-	Url     string
-	log     log.Logger
+	MsgType       string
+	OpenInBrowser bool //Set whether the message url will open outside of Dingding
+	Url           string
+	log           log.Logger
 }
 
 func (this *DingDingNotifier) Notify(evalContext *alerting.EvalContext) error {
@@ -65,6 +74,18 @@ func (this *DingDingNotifier) Notify(evalContext *alerting.EvalContext) error {
 		this.log.Error("Failed to get messageUrl", "error", err, "dingding", this.Name)
 		messageUrl = ""
 	}
+
+	if this.OpenInBrowser {
+		q := url.Values{
+			"pc_slide": {"false"},
+			"url":      {messageUrl},
+		}
+
+		// Use special link to auto open the message url outside of Dingding
+		// Refer: https://open-doc.dingtalk.com/docs/doc.htm?treeId=385&articleId=104972&docType=1#s9
+		messageUrl = "dingtalk://dingtalkclient/page/link?" + q.Encode()
+	}
+
 	this.log.Info("messageUrl:" + messageUrl)
 
 	message := evalContext.Rule.Message
