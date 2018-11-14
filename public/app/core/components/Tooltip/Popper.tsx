7d74ca4a8d5f5603ddd6a356c825c33d6529199e
Johannes Schill <johannes.schill@polyester.se>
2018-11-14 21:39:13 +0100

    panel-header: Updates for the new react-popper api and make it possible to hover the tooltip popper without it closing

---
+++ public/app/core/components/Tooltip/Popper.tsx
@@ -0,0 +1,42 @@
+﻿import React, { PureComponent } from 'react';
+import { Manager, Popper as ReactPopper, Reference } from 'react-popper';
+
+interface Props {
+  renderContent: (content: any) => any;
+  show: boolean;
+  placement?: any;
+  content: string | ((props: any) => JSX.Element);
+}
+
+class Popper extends PureComponent<Props> {
+  render() {
+    const { children, renderContent, show, placement } = this.props;
+    const { content } = this.props;
+    const modifiers = {
+      flip: { enabled: false },
+      preventOverflow: { enabled: false },
+      hide: { enabled: false },
+    };
+    return (
+      <Manager>
+        <Reference>{({ ref }) => <div ref={ref}>{children}</div>}</Reference>
+        {show && (
+          <ReactPopper placement={placement} modifiers={modifiers}>
+            {({ ref, style, placement, arrowProps }) => {
+              return (
+                <div ref={ref} style={style} data-placement={placement} className="popper">
+                  <div className="popper__background">
+                    {renderContent(content)}
+                    <div ref={arrowProps.ref} data-placement={placement} className="popper__arrow" />
+                  </div>
+                </div>
+              );
+            }}
+          </ReactPopper>
+        )}
+      </Manager>
+    );
+  }
+}
+
+export default Popper;
