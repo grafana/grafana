34c70ca7ceba4f91a1f070b8135ac26039589814
Torkel Ödegaard <torkel@grafana.com>
2018-11-20 13:46:52 +0100

    fix for issue with error view in production builds

--- scripts/webpack/webpack.prod.js
+++ scripts/webpack/webpack.prod.js
@@ -75,15 +75,15 @@ module.exports = merge(common, {
     }),
     new ngAnnotatePlugin(),
     new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, '../../public/views/error.html'),
+      template: path.resolve(__dirname, '../../public/views/error-template.html'),
+      inject: false,
+    }),
+    new HtmlWebpackPlugin({
       filename: path.resolve(__dirname, '../../public/views/index.html'),
       template: path.resolve(__dirname, '../../public/views/index-template.html'),
       inject: 'body',
       chunks: ['vendor', 'app'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, '../../public/views/error.html'),
-      template: path.resolve(__dirname, '../../public/views/error-template.html'),
-      inject: false,
     }),
     function () {
       this.hooks.done.tap('Done', function (stats) {
