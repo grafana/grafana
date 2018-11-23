7ace7e7d7067d2a768a2b989fa4cfa86e071afa2
atulabhi <atulabhi@gmail.com>
2018-11-23 18:28:21 +0530

    removed data

--- data/plugins/grafana-graph-alternative/build/webpack.dev.conf.js
+++
@@ -1,7 +0,0 @@
-const baseWebpackConfig = require('./webpack.base.conf');
-
-var conf = baseWebpackConfig;
-conf.watch = true;
-conf.devtool = "source-map";
-
-module.exports = conf;
--- data/plugins/grafana-piechart-panel-5f249d5/src/module.js
+++
@@ -1,9 +0,0 @@
-import { PieChartCtrl } from './piechart_ctrl';
-import { loadPluginCss } from 'app/plugins/sdk';
-
-loadPluginCss({
-  dark: 'plugins/grafana-piechart-panel/css/piechart.dark.css',
-  light: 'plugins/grafana-piechart-panel/css/piechart.light.css',
-});
-
-export { PieChartCtrl as PanelCtrl };
