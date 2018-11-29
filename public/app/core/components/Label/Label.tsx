b856ae39573d27626cd08cca0eed927ad4e66baa
Peter Holmberg <peter.hlmbrg@gmail.com>
2018-11-29 14:12:18 +0100

    revert Label change

--- public/app/core/components/Label/Label.tsx
+++ public/app/core/components/Label/Label.tsx
@@ -11,9 +11,7 @@ interface Props {
 
 export const Label: SFC<Props> = props => {
   return (
-    <span
-      className={`gf-form-label width-${props.width ? props.width : '10'} ${props.className ? props.className : ''}`}
-    >
+    <span className={`gf-form-label width-${props.width ? props.width : '10'}`}>
       <span>{props.children}</span>
       {props.tooltip && (
         <Tooltip className="gf-form-help-icon--right-normal" placement="auto" content={props.tooltip}>
