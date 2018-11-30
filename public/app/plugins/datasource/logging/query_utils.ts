11a53763e23ab2bf0afcec377083e2e8bc1ba8ee
David Kaltschmidt <david.kaltschmidt@gmail.com>
2018-11-30 15:49:54 +0100

    Explore: Fix logging query parser for regex with quantifiers
    - ensure that selectors have to be preceded by whitespace or line beginning
    - glacing over the fact that ` {0,1}` could be a valid regexp, but we're using space as query/regexp separators anyway, so I think it's fine for now

--- public/app/plugins/datasource/logging/query_utils.ts
+++ public/app/plugins/datasource/logging/query_utils.ts
@@ -1,11 +1,11 @@
-const selectorRegexp = /{[^{]*}/g;
+const selectorRegexp = /(?:^|\s){[^{]*}/g;
 export function parseQuery(input: string) {
   const match = input.match(selectorRegexp);
   let query = '';
   let regexp = input;
 
   if (match) {
-    query = match[0];
+    query = match[0].trim();
     regexp = input.replace(selectorRegexp, '').trim();
   }
 
