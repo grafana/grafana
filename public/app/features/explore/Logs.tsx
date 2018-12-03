d9d84f45bc103775f0d6133408ea809e7025ad62
Erik Sundell <erik.sundell87@gmail.com>
2018-12-03 16:58:46 +0100

    only make it possible to scan for older logs if there is at least one non failing selector

--- public/app/features/explore/Logs.tsx
+++ public/app/features/explore/Logs.tsx
@@ -140,6 +140,7 @@ interface LogsProps {
   range?: RawTimeRange;
   scanning?: boolean;
   scanRange?: RawTimeRange;
+  logExpressionExists?: boolean;
   onChangeTime?: (range: RawTimeRange) => void;
   onClickLabel?: (label: string, value: string) => void;
   onStartScanning?: () => void;
@@ -239,7 +240,17 @@ export default class Logs extends PureComponent<LogsProps, LogsState> {
   };
 
   render() {
-    const { className = '', data, loading = false, onClickLabel, position, range, scanning, scanRange } = this.props;
+    const {
+      className = '',
+      data,
+      loading = false,
+      onClickLabel,
+      position,
+      range,
+      scanning,
+      scanRange,
+      logExpressionExists,
+    } = this.props;
     const { dedup, deferLogs, hiddenLogLevels, renderAll, showLocalTime, showUtc } = this.state;
     let { showLabels } = this.state;
     const hasData = data && data.rows && data.rows.length > 0;
@@ -357,7 +368,8 @@ export default class Logs extends PureComponent<LogsProps, LogsState> {
             ))}
           {hasData && deferLogs && <span>Rendering {dedupedData.rows.length} rows...</span>}
         </div>
-        {!loading &&
+        {logExpressionExists &&
+          !loading &&
           !hasData &&
           !scanning && (
             <div className="logs-nodata">
