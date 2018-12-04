1283a3292dc720d7ebd64cb0a6e23e839cde15b1
Torkel Ödegaard <torkel@grafana.com>
2018-12-04 10:16:51 +0100

    fixed grabage in markup

--- public/app/plugins/datasource/prometheus/components/PromStart.tsx
+++ public/app/plugins/datasource/prometheus/components/PromStart.tsx
@@ -6,7 +6,7 @@ export default class PromStart extends PureComponent<any> {
   render() {
     return (
       <div className="grafana-info-box">
-        <PromCheatSheet onClickExample={this.props.onClickExample} />} etdiv>
+        <PromCheatSheet onClickExample={this.props.onClickExample} />
       </div>
     );
   }
