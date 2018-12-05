ed034b4288a21399e0be2141820b487d0cdda1a9
Johannes Schill <johannes.schill@polyester.se>
2018-12-05 14:56:29 +0100

    Trigger panel.render on title, description, links change #14333

--- public/app/features/panel/GeneralTabCtrl.ts
+++ public/app/features/panel/GeneralTabCtrl.ts
@@ -1,11 +1,35 @@
 import coreModule from 'app/core/core_module';
 
+const obj2string = obj => {
+  return Object.keys(obj)
+    .reduce((acc, curr) => acc.concat(curr + '=' + obj[curr]), [])
+    .join();
+};
+
 export class GeneralTabCtrl {
   panelCtrl: any;
 
   /** @ngInject */
   constructor($scope) {
     this.panelCtrl = $scope.ctrl;
+
+    const updatePanel = () => {
+      console.log('panel.render()');
+      this.panelCtrl.panel.render();
+    };
+
+    const generateValueFromPanel = scope => {
+      const { panel } = scope.ctrl;
+      const panelPropsToTrack = ['title', 'description', 'transparent', 'repeat', 'repeatDirection', 'minSpan'];
+      const panelPropsString = panelPropsToTrack
+        .map(prop => (panel[prop] && panel[prop].toString ? panel[prop].toString() : panel[prop]))
+        .join();
+      const panelLinks = panel.links;
+      const panelLinksString = panelLinks.map(obj2string).join();
+      return panelPropsString + panelLinksString;
+    };
+
+    $scope.$watch(generateValueFromPanel, updatePanel, true);
   }
 }
 
