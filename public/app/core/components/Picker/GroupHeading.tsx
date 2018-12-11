5370f78ee708f3a6b0a53aa56a9cca8c10ce0a90
Erik Sundell <erik.sundell87@gmail.com>
2018-12-11 20:11:18 +0100

    wip: add basic option header

---
+++ public/app/core/components/Picker/GroupHeading.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import { components } from 'react-select';
+
+export const GroupHeading = props => {
+  return (
+    <label className="description-picker-option__button btn picker-option-group">
+      <components.GroupHeading {...props} />
+    </label>
+  );
+};
+
+export default GroupHeading;
