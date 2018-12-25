98d26354c14db5f1a86e2026a53519daf5d683df
Torkel Ödegaard <torkel@grafana.com>
2018-12-25 11:42:23 +0100

    Added a form component to @grafana/ui

--- packages/grafana-ui/src/index.ts
+++ packages/grafana-ui/src/index.ts
@@ -2,3 +2,4 @@ export * from './components';
 export * from './visualizations';
 export * from './types';
 export * from './utils';
+export * from './forms';
--- public/app/features/users/UsersActionBar.tsx
+++ public/app/features/users/UsersActionBar.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
 import { setUsersSearchQuery } from './state/actions';
 import { getInviteesCount, getUsersSearchQuery } from './state/selectors';
 
