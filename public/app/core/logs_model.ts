a364487b3a14864740214c24f329cd6b154f6079
David Kaltschmidt <david.kaltschmidt@gmail.com>
2018-12-31 14:44:13 +0000

    Logs data model: add more log levels
    - add `eror` used in github.com/inconshreveable/log15 (used by Grafana as well)
    - add levels from SYSLOG

--- public/app/core/logs_model.ts
+++ public/app/core/logs_model.ts
@@ -2,14 +2,23 @@ import _ from 'lodash';
 import { TimeSeries } from 'app/core/core';
 import colors, { getThemeColor } from 'app/core/utils/colors';
 
+/**
+ * Mapping of log level abbreviation to canonical log level.
+ * Supported levels are reduce to limit color variation.
+ */
 export enum LogLevel {
+  emerg = 'critical',
+  alert = 'critical',
   crit = 'critical',
   critical = 'critical',
   warn = 'warning',
   warning = 'warning',
   err = 'error',
+  eror = 'error',
   error = 'error',
   info = 'info',
+  notice = 'info',
+  dbug = 'debug',
   debug = 'debug',
   trace = 'trace',
   unkown = 'unkown',
