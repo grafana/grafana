0c178f86233d005c7695ea36422978c5d6187347
Erik Sundell <erik.sundell87@gmail.com>
2019-01-03 16:21:20 +0100

    cleanup

--- public/app/plugins/datasource/stackdriver/annotations_query_ctrl.ts
+++ public/app/plugins/datasource/stackdriver/annotations_query_ctrl.ts
@@ -1,34 +1,12 @@
-import _ from 'lodash';
-import './query_filter_ctrl';
 import { react2AngularDirective } from 'app/core/utils/react2angular';
 import { AnnotationQueryEditor } from './components/AnnotationQueryEditor';
 
 export class StackdriverAnnotationsQueryCtrl {
   static templateUrl = 'partials/annotations.editor.html';
   annotation: any;
-  datasource: any;
 
-  defaultDropdownValue = 'Select Metric';
-  defaultServiceValue = 'All Services';
-
-  defaults = {
-    project: {
-      id: 'default',
-      name: 'loading project...',
-    },
-    metricType: this.defaultDropdownValue,
-    service: this.defaultServiceValue,
-    metric: '',
-    filters: [],
-    metricKind: '',
-    valueType: '',
-  };
-
-  /** @ngInject */
   constructor() {
     this.annotation.target = this.annotation.target || {};
-    this.annotation.target.refId = 'annotationQuery';
-    _.defaultsDeep(this.annotation.target, this.defaults);
     this.handleQueryChange = this.handleQueryChange.bind(this);
 
     react2AngularDirective('annotationQueryEditor', AnnotationQueryEditor, [
