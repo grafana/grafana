b9c4eb70b13a4ee9a5eae150ee8ef02480ed2ad7
Leonard Gram <leo@xlson.com>
2019-01-08 16:20:26 +0100

    build: publishes beta releases to separate repos.

--- scripts/build/update_repo/unlock-gpg-key.sh
+++ scripts/build/update_repo/unlock-gpg-key.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env expect
 
 set password [lindex $argv 0]
-spawn aptly publish repo grafana filesystem:repo:grafana
+spawn gpg --detach-sign --armor /tmp/sign-this
 expect "Enter passphrase: "
 send -- "$password\r"
 expect eof
