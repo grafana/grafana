06d8243ab590e81ffb3721f788672d995a6c0a3a
Johannes Schill <johannes.schill@polyester.se>
2019-01-14 16:51:43 +0100

    feat: Possibility to change document title on pages using the Page component

--- public/app/core/components/Page/Page.tsx
+++ public/app/core/components/Page/Page.tsx
@@ -7,7 +7,7 @@ import PageContents from './PageContents';
 import { CustomScrollbar } from '@grafana/ui';
 
 interface Props {
-  title: string;
+  title?: string;
   children: JSX.Element[] | JSX.Element;
 }
 
@@ -23,10 +23,22 @@ class Page extends Component<Props> {
   componentDidMount() {
     this.body.classList.add(this.bodyClass);
     this.copyFooter();
+    this.updateTitle();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.title !== this.props.title) {
+      this.updateTitle();
+    }
   }
 
   componentWillUnmount() {
     this.body.classList.remove(this.bodyClass);
+  }
+
+  updateTitle = () => {
+    const { title } = this.props;
+    document.title = title ? title + ' - Grafana' : 'Grafana';
   }
 
   copyFooter = () => {
@@ -37,11 +49,11 @@ class Page extends Component<Props> {
   render() {
     return (
       <div className="page-scrollbar-wrapper">
-          <CustomScrollbar autoHeightMin={'100%'}>
-            <div className="page-scrollbar-content" ref={this.scrollbarElementRef}>
-              {this.props.children}
-            </div>
-          </CustomScrollbar>
+        <CustomScrollbar autoHeightMin={'100%'}>
+          <div className="page-scrollbar-content" ref={this.scrollbarElementRef}>
+            {this.props.children}
+          </div>
+        </CustomScrollbar>
       </div>
     );
   }
