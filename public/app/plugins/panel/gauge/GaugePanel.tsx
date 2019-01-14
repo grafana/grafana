b3b1c21d772929cde9b5f6d5adcc8ee97b5608ec
Dominik Prokop <dominik.prokop@grafana.com>
2019-01-14 09:45:55 +0100

    Fixed Gauge being cropped when resizing panel
    Enabled import of SASS viariables into TS

--- public/app/plugins/panel/gauge/GaugePanel.tsx
+++ public/app/plugins/panel/gauge/GaugePanel.tsx
@@ -3,6 +3,7 @@ import { GaugeOptions, PanelProps, NullValueMode } from '@grafana/ui';
 
 import { getTimeSeriesVMs } from 'app/viz/state/timeSeries';
 import Gauge from 'app/viz/Gauge';
+import variables from 'sass/_variables.scss';
 
 interface Props extends PanelProps<GaugeOptions> {}
 
@@ -15,6 +16,13 @@ export class GaugePanel extends PureComponent<Props> {
       nullValueMode: NullValueMode.Ignore,
     });
 
-    return <Gauge timeSeries={vmSeries} {...this.props.options} width={width} height={height} />;
+    return (
+      <Gauge
+        timeSeries={vmSeries}
+        {...this.props.options}
+        width={width - 2 * variables.panelHorizontalPadding}
+        height={height - variables.panelVerticalPadding}
+      />
+    );
   }
 }
