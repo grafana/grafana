1d85a1b606e6f6bd76f90bd4010a7761a9f3bf2e
James Ravn <james@r-vn.org>
2019-01-18 15:35:13 +0000

    Clean up
    * Combine ARGs/RUN into a single layer so it builds a lot faster.
    * Reduce debug output.

--- packaging/docker/download-lib.sh
+++ packaging/docker/download-lib.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
 
-set -exu
+# ./download-lib.sh acquires prebuilt libraries from the archlinux repository for alpine.
+# It requires binutils and xz packages.
+
+set -eu
+if [ -n "${DEBUG:-}" ]; then set -x; fi
 
 url="$1"
 sha256="$2"
 shift;shift
 files_to_copy="$@"
 
+if [ -z "$files_to_copy" ]; then
+  echo "\$files_to_copy was empty"
+  exit 1
+fi
+
 COPY_DIRS="${COPY_DIRS:-}"
 
-# download and extract library
-# requires binutils and xz packages
+echo -n "Installing ${files_to_copy}..."
+
+# Download library.
 curl -Ls ${url} -o /tmp/libs.tar.xz
-echo "${sha256}  /tmp/libs.tar.xz" | sha256sum -c -
+set +e
+echo "${sha256}  /tmp/libs.tar.xz" | sha256sum -sc -
+if [ $? -ne 0 ]; then
+  echo "checksum failed"
+  echo "wanted ${sha256}  /tmp/libs.tar.xz"
+  echo "got    $(sha256sum /tmp/libs.tar.xz)"
+  exit 1
+fi
+set -e
+
+# Extract and copy.
 mkdir /tmp/libs
 tar xf /tmp/libs.tar.xz -C /tmp/libs
 for file in ${files_to_copy}; do
@@ -20,10 +40,12 @@ for file in ${files_to_copy}; do
   mv /tmp/libs/${file} /usr/glibc-compat/lib
 done
 
-# copy any extra required directories
+# Copy any extra required directories.
 for dir in ${COPY_DIRS}; do
   cp -r /tmp/libs/${dir} /${dir}
 done
 
-# clean up
+# Clean up.
 rm -rf /tmp/libs /tmp/libs.tar.xz
+
+echo "done"
