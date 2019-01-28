07331b4c0753f4fcd305c5d96c09b66231a251f7
Dominik Prokop <dominik.prokop@grafana.com>
2019-01-28 13:40:41 +0100

    Revert "Use the same panel loading indicator in explore as on dashboard's panel"
    This reverts commit 05edb3e5ad81b1572576b4f0414b4732089bf546.

--- public/app/features/explore/Panel.tsx
+++ public/app/features/explore/Panel.tsx
@@ -13,6 +13,7 @@ export default class Panel extends PureComponent<Props> {
   render() {
     const { isOpen, loading } = this.props;
     const iconClass = isOpen ? 'fa fa-caret-up' : 'fa fa-caret-down';
+    const loaderClass = loading ? 'explore-panel__loader explore-panel__loader--active' : 'explore-panel__loader';
     return (
       <div className="explore-panel panel-container">
         <div className="explore-panel__header" onClick={this.onClickToggle}>
@@ -23,11 +24,7 @@ export default class Panel extends PureComponent<Props> {
         </div>
         {isOpen && (
           <div className="explore-panel__body">
-            {loading && (
-              <span className="panel-loading">
-                <i className="fa fa-spinner fa-spin" />
-              </span>
-            )}
+            <div className={loaderClass} />
             {this.props.children}
           </div>
         )}
