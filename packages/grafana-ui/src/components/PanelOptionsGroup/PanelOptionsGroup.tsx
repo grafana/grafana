025d37e9a2f2f886e6b6e2f62dfb93d32379bee6
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-02-01 14:19:53 +0100

    add button in header

--- packages/grafana-ui/src/components/PanelOptionsGroup/PanelOptionsGroup.tsx
+++ packages/grafana-ui/src/components/PanelOptionsGroup/PanelOptionsGroup.tsx
@@ -1,26 +1,36 @@
 // Libraries
-import React, { SFC } from 'react';
+import React, { FunctionComponent } from 'react';
 
 interface Props {
   title?: string;
   onClose?: () => void;
-  children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[] | boolean;
+  onAdd?: () => void;
 }
 
-export const PanelOptionsGroup: SFC<Props> = props => {
+export const PanelOptionsGroup: FunctionComponent<Props> = props => {
   return (
     <div className="panel-options-group">
-      {props.title && (
-        <div className="panel-options-group__header">
-          {props.title}
-          {props.onClose && (
-            <button className="btn btn-link" onClick={props.onClose}>
-              <i className="fa fa-remove" />
-            </button>
-          )}
+      {props.onAdd ? (
+        <div className="panel-options-group__header" style={{ cursor: 'pointer' }} onClick={props.onAdd}>
+          <div className="panel-options-group__add">
+            <i className="fa fa-plus" />
+          </div>
+          <span className="panel-options-group__title">{props.title}</span>
         </div>
+      ) : (
+        props.title && (
+          <div className="panel-options-group__header">
+            {props.title}
+            {props.onClose && (
+              <button className="btn btn-link" onClick={props.onClose}>
+                <i className="fa fa-remove" />
+              </button>
+            )}
+          </div>
+        )
       )}
-      <div className="panel-options-group__body">{props.children}</div>
+      {props.children && <div className="panel-options-group__body">{props.children}</div>}
     </div>
   );
 };
