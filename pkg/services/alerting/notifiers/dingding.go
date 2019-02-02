bba92c0746e3bbbb53833dd222cb4f38cca8a2d5
Athurg Feng <i@atfeng.com>
2019-02-02 13:35:17 +0800

    Remove option used to control within browser

--- pkg/services/alerting/notifiers/dingding.go
+++ pkg/services/alerting/notifiers/dingding.go
@@ -22,11 +22,6 @@ const DingdingOptionsTemplate = `
         <span class="gf-form-label width-10">MessageType</span>
         <select class="gf-form-input max-width-14" ng-model="ctrl.model.settings.msgType" ng-options="s for s in ['link','actionCard']" ng-init="ctrl.model.settings.msgType=ctrl.model.settings.msgType || '` + DefaultDingdingMsgType + `'"></select>
       </div>
-      <div class="gf-form">
-        <span class="gf-form-label width-10">OpenInBrowser</span>
-        <gf-form-switch class="gf-form" checked="ctrl.model.settings.openInBrowser"></gf-form-switch>
-        <info-popover mode="right-normal">Open the message url in browser instead of inside of Dingding</info-popover>
-      </div>
 `
 
 func init() {
@@ -47,23 +42,20 @@ func NewDingDingNotifier(model *m.AlertNotification) (alerting.Notifier, error) 
 	}
 
 	msgType := model.Settings.Get("msgType").MustString(DefaultDingdingMsgType)
-	openInBrowser := model.Settings.Get("openInBrowser").MustBool(true)
 
 	return &DingDingNotifier{
-		NotifierBase:  NewNotifierBase(model),
-		OpenInBrowser: openInBrowser,
-		MsgType:       msgType,
-		Url:           url,
-		log:           log.New("alerting.notifier.dingding"),
+		NotifierBase: NewNotifierBase(model),
+		MsgType:      msgType,
+		Url:          url,
+		log:          log.New("alerting.notifier.dingding"),
 	}, nil
 }
 
 type DingDingNotifier struct {
 	NotifierBase
-	MsgType       string
-	OpenInBrowser bool //Set whether the message url will open outside of Dingding
-	Url           string
-	log           log.Logger
+	MsgType string
+	Url     string
+	log     log.Logger
 }
 
 func (this *DingDingNotifier) Notify(evalContext *alerting.EvalContext) error {
@@ -75,16 +67,14 @@ func (this *DingDingNotifier) Notify(evalContext *alerting.EvalContext) error {
 		messageUrl = ""
 	}
 
-	if this.OpenInBrowser {
-		q := url.Values{
-			"pc_slide": {"false"},
-			"url":      {messageUrl},
-		}
-
-		// Use special link to auto open the message url outside of Dingding
-		// Refer: https://open-doc.dingtalk.com/docs/doc.htm?treeId=385&articleId=104972&docType=1#s9
-		messageUrl = "dingtalk://dingtalkclient/page/link?" + q.Encode()
+	q := url.Values{
+		"pc_slide": {"false"},
+		"url":      {messageUrl},
 	}
+
+	// Use special link to auto open the message url outside of Dingding
+	// Refer: https://open-doc.dingtalk.com/docs/doc.htm?treeId=385&articleId=104972&docType=1#s9
+	messageUrl = "dingtalk://dingtalkclient/page/link?" + q.Encode()
 
 	this.log.Info("messageUrl:" + messageUrl)
 
