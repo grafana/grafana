4d2cff41ff15fb801960f8a5a5cd17a7fc8f5136
Torkel Ödegaard <torkel@grafana.com>
2019-02-05 19:38:51 +0100

    Minor code simplification

--- public/app/features/dashboard/containers/DashboardPage.tsx
+++ public/app/features/dashboard/containers/DashboardPage.tsx
@@ -247,7 +247,7 @@ export class DashboardPage extends PureComponent<Props, State> {
         />
         <div className="scroll-canvas scroll-canvas--dashboard">
           <CustomScrollbar autoHeightMin={'100%'} setScrollTop={this.setScrollTop} scrollTop={scrollTop}>
-            {dashboard && editview && <DashboardSettings dashboard={dashboard} />}
+            {editview && <DashboardSettings dashboard={dashboard} />}
 
             <div className={gridWrapperClasses}>
               {dashboard.meta.submenuEnabled && <SubMenu dashboard={dashboard} />}
