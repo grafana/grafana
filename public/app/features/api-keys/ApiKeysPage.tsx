f195459b01f3ea65b0b7ede13225401d048ccaa1
Torkel Ödegaard <torkel@grafana.com>
2019-02-12 17:08:40 +0100

    Removed some icons in action button Trying to align some title case issues

--- public/app/features/api-keys/ApiKeysPage.tsx
+++ public/app/features/api-keys/ApiKeysPage.tsx
@@ -200,7 +200,7 @@ export class ApiKeysPage extends PureComponent<Props, any> {
 
           <div className="page-action-bar__spacer" />
           <button className="btn btn-primary pull-right" onClick={this.onToggleAdding} disabled={isAdding}>
-            Add API Key
+            Add API key
           </button>
         </div>
 
