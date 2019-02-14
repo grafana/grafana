3c2f6094b2a90676558f4c4c58d2871bedd2b0df
Maxim Neverov <maxim.neverov@springernature.com>
2019-02-14 16:31:15 +0100

    Fix percent_diff calculation when points are nulls

--- pkg/services/alerting/conditions/reducer_test.go
+++ pkg/services/alerting/conditions/reducer_test.go
@@ -157,6 +157,18 @@ func TestSimpleReducer(t *testing.T) {
 			result := testReducer("percent_diff", 30, 40, 40)
 			So(result, ShouldEqual, float64(33.33333333333333))
 		})
+
+		Convey("percent_diff with only nulls", func() {
+			reducer := NewSimpleReducer("percent_diff")
+			series := &tsdb.TimeSeries{
+				Name: "test time serie",
+			}
+
+			series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFromPtr(nil), 1))
+			series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFromPtr(nil), 2))
+
+			So(reducer.Reduce(series).Valid, ShouldEqual, false)
+		})
 	})
 }
 
