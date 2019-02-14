688a8e286df3ab8b3d6262eded5dab79dd469521
Maxim Neverov <neverov.max@gmail.com>
2019-02-14 21:52:59 +0100

    Extracted common code for diff calculation
    (cherry picked from commit 28eaac3a9c7082e8c496005c1cb66b4b70a4f82f)

--- pkg/services/alerting/conditions/reducer_test.go
+++ pkg/services/alerting/conditions/reducer_test.go
@@ -143,6 +143,18 @@ func TestSimpleReducer(t *testing.T) {
 			So(result, ShouldEqual, float64(10))
 		})
 
+		Convey("diff with only nulls", func() {
+			reducer := NewSimpleReducer("diff")
+			series := &tsdb.TimeSeries{
+				Name: "test time serie",
+			}
+
+			series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFromPtr(nil), 1))
+			series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFromPtr(nil), 2))
+
+			So(reducer.Reduce(series).Valid, ShouldEqual, false)
+		})
+
 		Convey("percent_diff one point", func() {
 			result := testReducer("percent_diff", 40)
 			So(result, ShouldEqual, float64(0))
