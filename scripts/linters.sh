2787c0a9f622f5a44c0ff13140b36690f8fb26ab
Mario Trangoni <mjtrangoni@gmail.com>
2019-02-17 18:27:25 +0100

    Switch from gometalinter to golangci-lint
    See https://github.com/golangci/golangci-lint,
    
     - GolangCI-Lint is a linters aggregator.
     - It's fast: on average 5 times faster than gometalinter.
     - It's easy to integrate and use, has nice output and has a
       minimum number of false positives.
     - It supports go modules.

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -12,22 +12,26 @@ function exit_if_fail {
 }
 
 go get -u github.com/alecthomas/gometalinter
+go get -u honnef.co/go/tools/cmd/staticcheck
+
+exit_if_fail gometalinter --enable-gc --vendor --deadline 10m --disable-all \
+  --enable=staticcheck
+
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u github.com/tsenart/deadcode
 go get -u github.com/jgautheron/goconst/cmd/goconst
 go get -u github.com/gordonklaus/ineffassign
 go get -u github.com/opennota/check/cmd/structcheck
 go get -u github.com/mdempsky/unconvert
 go get -u github.com/opennota/check/cmd/varcheck
-go get -u honnef.co/go/tools/cmd/staticcheck
 
-exit_if_fail gometalinter --enable-gc --vendor --deadline 10m --disable-all \
-  --enable=deadcode \
-  --enable=goconst \
-  --enable=gofmt \
-  --enable=ineffassign \
-  --enable=structcheck \
-  --enable=unconvert \
-  --enable=varcheck \
-  --enable=staticcheck
+exit_if_fail golangci-lint run --deadline 10m --disable-all \
+  --enable=goconst\
+  --enable=deadcode\
+  --enable=gofmt\
+  --enable=ineffassign\
+  --enable=structcheck\
+  --enable=unconvert\
+  --enable=varcheck
 
 exit_if_fail go vet ./pkg/...
