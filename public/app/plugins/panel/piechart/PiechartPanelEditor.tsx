0f102001b666d01d45075644f80702680733d850
corpglory-dev <dev@corpglory.com>
2019-02-20 17:27:42 +0300

    Make it build

--- public/app/plugins/panel/piechart/PiechartPanelEditor.tsx
+++ public/app/plugins/panel/piechart/PiechartPanelEditor.tsx
@@ -2,16 +2,22 @@ import React, { PureComponent } from 'react';
 import { PanelEditorProps, PanelOptionsGrid } from '@grafana/ui';
 
 import ValueOptions from './ValueOptions';
-import { PiechartOptions } from './types';
+import { PiechartOptions, PiechartValueOptions } from './types';
 
 export default class PiechartPanelEditor extends PureComponent<PanelEditorProps<PiechartOptions>> {
+  onValueOptionsChanged = (valueOptions: PiechartValueOptions) =>
+    this.props.onChange({
+      ...this.props.options,
+      valueOptions,
+    });
+
   render() {
-    const { onChange, options } = this.props;
+    const { options } = this.props;
 
     return (
       <>
         <PanelOptionsGrid>
-          <ValueOptions onChange={onChange} options={options} />
+          <ValueOptions onChange={this.onValueOptionsChanged} options={options.valueOptions} />
         </PanelOptionsGrid>
       </>
     );
