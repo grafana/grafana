1a48d82133d7fb1eacf549ab5e12909b23aafbfd
Leonard Gram <leo@xlson.com>
2019-02-26 11:41:55 +0100

    service: fix for disabled internal metrics.
    Update of the internal metrics for Grafana was
    disabled by mistake when refactoring the code.
    
    Fixes #15651
    
    (cherry picked from commit 36788183d84e50ecfaddeaf20ed6eec055921d83)

--- pkg/cmd/grafana-server/server.go
+++ pkg/cmd/grafana-server/server.go
@@ -29,6 +29,8 @@ import (
 	// self registering services
 	_ "github.com/grafana/grafana/pkg/extensions"
 	_ "github.com/grafana/grafana/pkg/infra/serverlock"
+
+	_ "github.com/grafana/grafana/pkg/infra/usagestats"
 	_ "github.com/grafana/grafana/pkg/metrics"
 	_ "github.com/grafana/grafana/pkg/plugins"
 	_ "github.com/grafana/grafana/pkg/services/alerting"
