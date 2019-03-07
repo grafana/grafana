dbc1315d6f69bb6ce154e5b907d1077d5301c7f7
bergquist <carl.bergquist@gmail.com>
2019-03-07 17:16:08 +0100

    build steps for cache servers

---
+++ scripts/circle-test-cache-servers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function exit_if_fail {
+    command=$@
+    echo "Executing '$command'"
+    eval $command
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echo "'$command' returned $rc."
+        exit $rc
+    fi
+}
+
+echo "running redis and memcache tests"
+#set -e
+#time for d in $(go list ./pkg/...); do
+time exit_if_fail go test -tags="redis memcached" ./pkg/infra/distcache/...
+#done
