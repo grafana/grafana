3bd26df7dea850dafec16e7f51150a7b27e75840
ryan <ryantxu@gmail.com>
2019-03-08 20:49:45 -0800

    cleanup

--- packages/grafana-ui/src/components/Table/TableInputCSV.story.tsx
+++ packages/grafana-ui/src/components/Table/TableInputCSV.story.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import TableInputCSV, { ParseResults } from './TableInputCSV';
-//import { withFullSizeStory } from '../../utils/storybook/withFullSizeStory';
 
 const TableInputStories = storiesOf('UI/Table/Input', module);
 
 TableInputStories.add('default', () => {
-  //return withFullSizeStory(TableInputCSV, {});
   return (
     <div>
       <TableInputCSV
