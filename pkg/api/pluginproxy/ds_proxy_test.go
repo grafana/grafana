fcc18d8121a754b33a53ab3ccab08eb7a8339851
Sean Lafferty <sixers2329@gmail.com>
2019-03-13 14:55:22 -0400

    Change import path for social in the tests

--- pkg/api/pluginproxy/ds_proxy_test.go
+++ pkg/api/pluginproxy/ds_proxy_test.go
@@ -15,10 +15,10 @@ import (
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/log"
+	"github.com/grafana/grafana/pkg/login/social"
 	m "github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/setting"
-	"github.com/grafana/grafana/pkg/social"
 	"github.com/grafana/grafana/pkg/util"
 	. "github.com/smartystreets/goconvey/convey"
 )
