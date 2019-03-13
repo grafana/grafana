78b297884647274d7c24010c0db3c31dc0b9d6fb
Torkel Ödegaard <torkel@grafana.com>
2019-03-13 10:55:20 +0100

    Only send ci metrics to hosted metrics instance

--- scripts/ci-frontend-metrics.sh
+++ scripts/ci-frontend-metrics.sh
@@ -10,19 +10,6 @@ echo -e "Typescript errors: $ERROR_COUNT"
 echo -e "Directives: $DIRECTIVES"
 echo -e "Controllers: $CONTROLLERS"
 
-curl \
-   -d "{\"metrics\": {
-        \"ci.code.noImplicitAny\": $ERROR_COUNT,
-        \"ci.code.directives\": $DIRECTIVES,
-        \"ci.code.controllers\": $CONTROLLERS
-      }
-    }" \
-   -H "Content-Type: application/json" \
-   -u ci:$CIRCLE_STATS_PWD \
-   -X POST https://stats.grafana.org/metric-receiver
-
-echo -e ""
-
 ./scripts/ci-metrics-publisher.sh \
   grafana.ci-code.noImplicitAny=$ERROR_COUNT \
   grafana.ci-code.directives=$DIRECTIVES \
