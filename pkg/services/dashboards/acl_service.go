8647ebe8eeee4be05022d85d7f8bd7c89ebe92a3
Leonard Gram <leo@xlson.com>
2019-03-14 09:18:06 +0100

    permissions: refactor.

--- pkg/services/dashboards/acl_service.go
+++ pkg/services/dashboards/acl_service.go
@@ -2,23 +2,11 @@ package dashboards
 
 import (
 	"github.com/grafana/grafana/pkg/bus"
-	"github.com/grafana/grafana/pkg/log"
 	"github.com/grafana/grafana/pkg/models"
 	"time"
 )
 
-// NewService factory for creating a new dashboard service
-var NewAclService = func() *AclService {
-	return &AclService{
-		log: log.New("dashboard-acl-service"),
-	}
-}
-
-type AclService struct {
-	log log.Logger
-}
-
-func (as *AclService) MakeUserAdmin(orgId int64, userId int64, dashboardId int64, setViewAndEditPermissions bool) error {
+func MakeUserAdmin(orgId int64, userId int64, dashboardId int64, setViewAndEditPermissions bool) error {
 	rtEditor := models.ROLE_EDITOR
 	rtViewer := models.ROLE_VIEWER
 
