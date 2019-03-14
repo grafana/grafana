62f53f5ced5194872fb7075771082c3779d35780
Leonard Gram <leo@xlson.com>
2019-03-14 09:08:48 +0100

    teams: refactored db code.

--- pkg/services/sqlstore/migrations/team_mig.go
+++ pkg/services/sqlstore/migrations/team_mig.go
@@ -56,8 +56,6 @@ func addTeamMigrations(mg *Migrator) {
 	}))
 
 	mg.AddMigration("Add column permission to team_member table", NewAddColumnMigration(teamMemberV1, &Column{
-		Name:     "permission",
-		Type:     DB_BigInt,
-		Nullable: true,
+		Name: "permission", Type: DB_SmallInt, Nullable: true,
 	}))
 }
