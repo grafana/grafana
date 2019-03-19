0d84a3fbe218edf5a336c4412cdc41c813ac0abf
Andrej Ocenas <mr.ocenas@gmail.com>
2019-03-19 13:22:35 +0100

    Add possibility to pass custom input component to FormField

--- packages/grafana-ui/src/components/FormField/FormField.tsx
+++ packages/grafana-ui/src/components/FormField/FormField.tsx
@@ -1,10 +1,12 @@
 import React, { InputHTMLAttributes, FunctionComponent } from 'react';
+// import React, { InputHTMLAttributes } from 'react';
 import { FormLabel } from '../FormLabel/FormLabel';
 
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   labelWidth?: number;
   inputWidth?: number;
+  inputEl?: React.ReactNode;
 }
 
 const defaultProps = {
@@ -12,14 +14,24 @@ const defaultProps = {
   inputWidth: 12,
 };
 
-const FormField: FunctionComponent<Props> = ({ label, labelWidth, inputWidth, ...inputProps }) => {
+/**
+ * Default form field including label used in Grafana UI. Default input element is simple <input />. You can also pass
+ * custom inputEl if required in which case inputWidth and inputProps are ignored.
+ * @param label
+ * @param labelWidth
+ * @param inputWidth
+ * @param inputEl
+ * @param inputProps
+ * @constructor
+ */
+export const FormField: FunctionComponent<Props> = ({ label, labelWidth, inputWidth, inputEl, ...inputProps }) => {
   return (
     <div className="form-field">
       <FormLabel width={labelWidth}>{label}</FormLabel>
-      <input type="text" className={`gf-form-input width-${inputWidth}`} {...inputProps} />
+      {inputEl || <input type="text" className={`gf-form-input width-${inputWidth}`} {...inputProps} />}
     </div>
   );
 };
 
+FormField.displayName = 'FormField';
 FormField.defaultProps = defaultProps;
-export { FormField };
