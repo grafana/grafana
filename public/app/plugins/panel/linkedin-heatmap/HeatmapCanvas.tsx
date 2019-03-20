5ee61efe110399a18459a0ed670c2ad9c1a13de0
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-03-20 13:50:02 +0300

    linkedin: show values on mouse hover and use thresholds

--- public/app/plugins/panel/linkedin-heatmap/HeatmapCanvas.tsx
+++ public/app/plugins/panel/linkedin-heatmap/HeatmapCanvas.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-export class HeatmapCanvas extends React.Component<any> {
+interface HeatmapCanvasProps {
+  width: number;
+  height: number;
+  data: number[][];
+  labels: string[];
+  valueToColor?: (value: number) => string;
+  onPointHover?: (value: number, labels: string[]) => void;
+  onPointClick?: (labels: string[]) => void;
+}
+
+export class HeatmapCanvas extends React.PureComponent<HeatmapCanvasProps> {
   canvas: HTMLCanvasElement | null = null;
+  ctx: CanvasRenderingContext2D;
+  pointSize: number;
 
   componentDidUpdate() {
     this.draw();
@@ -9,6 +21,57 @@ export class HeatmapCanvas extends React.Component<any> {
 
   componentDidMount() {
     this.draw();
+    this.canvas.addEventListener('mousemove', this.handleMouseMove);
+    this.canvas.addEventListener('click', this.handleMouseClick);
+  }
+
+  componentWillUnmount() {
+    this.canvas.removeEventListener('mousemove', this.handleMouseMove);
+    this.canvas.removeEventListener('click', this.handleMouseClick);
+  }
+
+  handleMouseClick = event => {
+    if (!this.ctx) {
+      return;
+    }
+    const x = event.layerX;
+    const y = event.layerY;
+    const pointLabels = this.getPointLabels(x, y);
+    if (this.props.onPointHover) {
+      this.props.onPointClick(pointLabels);
+    }
+  };
+
+  handleMouseMove = event => {
+    if (!this.ctx) {
+      return;
+    }
+    const x = event.layerX;
+    const y = event.layerY;
+    // const pixel = this.ctx.getImageData(x, y, 1, 1);
+    // const pixelData = pixel.data;
+    const pointLabels = this.getPointLabels(x, y);
+    const pointValue = this.getPointValue(x, y);
+    // console.log(pointLabels, pointValue);
+
+    if (this.props.onPointHover && pointValue) {
+      this.props.onPointHover(pointValue, pointLabels);
+    }
+  };
+
+  getPointLabels(x: number, y: number): string[] {
+    const xIndex = Math.floor(x / this.pointSize);
+    const yIndex = Math.floor(y / this.pointSize);
+    return [this.props.labels[xIndex], this.props.labels[yIndex]];
+  }
+
+  getPointValue(x: number, y: number): number {
+    const xIndex = Math.floor(x / this.pointSize);
+    const yIndex = Math.floor(y / this.pointSize);
+    if (xIndex >= this.props.data.length || yIndex >= this.props.data.length) {
+      return null;
+    }
+    return this.props.data[yIndex][xIndex];
   }
 
   draw() {
@@ -22,29 +85,31 @@ export class HeatmapCanvas extends React.Component<any> {
       return;
     }
 
-    const ctx = this.canvas.getContext('2d');
+    console.log('drawing canvas');
     console.log(this.props);
-    // for (let i = 0; i < width; i++) {
-    //   for (let j = 0; j < height; j++) {
-    //     const colorJitter = Math.round(Math.random() * 100);
-    //     ctx.fillStyle = `rgb(200,${colorJitter},0)`;
-    //     ctx.fillRect(i, j, 1, 1);
-    //   }
-    // }
-    const pointWidth = Math.floor(width / data.length);
-    const pointHeight = Math.floor(height / data.length);
-    console.log(pointWidth, pointHeight);
+    const ctx = this.canvas.getContext('2d');
+    this.ctx = ctx;
+    const pointWidth = Math.max(Math.floor(width / data.length), 1);
+    const pointHeight = Math.max(Math.floor(height / data.length), 1);
+    const pointSize = Math.min(pointWidth, pointHeight);
+    this.pointSize = pointSize;
+    // console.log(pointWidth, pointHeight);
     for (let i = 0; i < data.length; i++) {
       const row = data[i];
       for (let j = 0; j < row.length; j++) {
         const value = row[j];
-        ctx.fillStyle = `rgb(200,${value},0)`;
-        ctx.fillRect(i * pointWidth, j * pointHeight, pointWidth, pointHeight);
+        if (this.props.valueToColor) {
+          ctx.fillStyle = this.props.valueToColor(value);
+        } else {
+          ctx.fillStyle = `rgb(200,${value},0)`;
+        }
+        ctx.fillRect(i * pointSize, j * pointSize, pointSize, pointSize);
       }
     }
   }
 
   render() {
+    console.log('rendering canvas component');
     const { width, height } = this.props;
     return <canvas className="linkedin-heatmap-canvas" ref={e => (this.canvas = e)} width={width} height={height} />;
   }
--- public/app/plugins/panel/linkedin-heatmap/LHeatmapPanel.tsx
+++ public/app/plugins/panel/linkedin-heatmap/LHeatmapPanel.tsx
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react';
 
 // Services & Utils
 import { config } from 'app/core/config';
+import { getColorFromHexRgbOrName } from '@grafana/ui/src/utils';
 
 // Components
 import { Gauge } from '@grafana/ui';
@@ -12,7 +13,43 @@ import { HeatmapCanvas } from './HeatmapCanvas';
 import { LHeatmapOptions } from './types';
 import { DisplayValue, PanelProps } from '@grafana/ui';
 
-export class LHeatmapPanel extends PureComponent<PanelProps<LHeatmapOptions>> {
+interface LHeatmapPanelState {
+  pointValue: number;
+  pointLabels: string[];
+}
+
+export class LHeatmapPanel extends PureComponent<PanelProps<LHeatmapOptions>, LHeatmapPanelState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pointValue: null,
+      pointLabels: [],
+    };
+  }
+
+  handlePointHover = (value: number, labels: string[]) => {
+    // console.log(labels, value);
+    this.setState({
+      pointValue: value,
+      pointLabels: labels,
+    });
+  };
+
+  handlePointClick = (labels: string[]) => {
+    console.log('click on', labels);
+  };
+
+  valueToColor = (value: number) => {
+    const thresholds = this.props.options.thresholds;
+    let color = getColorFromHexRgbOrName(thresholds[0].color);
+    for (const threshold of thresholds) {
+      if (value > threshold.value) {
+        color = getColorFromHexRgbOrName(threshold.color);
+      }
+    }
+    return color;
+  };
+
   renderValue = (value: DisplayValue, width: number, height: number): JSX.Element => {
     const { options } = this.props;
 
@@ -36,13 +73,26 @@ export class LHeatmapPanel extends PureComponent<PanelProps<LHeatmapOptions>> {
   };
 
   render() {
-    console.log(this.props);
     const { height, width, panelData } = this.props;
     const heatmapData = panelData.timeSeries[0][0];
     const labels = panelData.timeSeries[0][1];
     return (
       <div className="linkedin-heatmap-poc">
-        <HeatmapCanvas data={heatmapData} labels={labels} width={width} height={height} />
+        <HeatmapCanvas
+          width={width}
+          height={height - 40}
+          data={heatmapData}
+          labels={labels}
+          valueToColor={this.valueToColor}
+          onPointHover={this.handlePointHover}
+          onPointClick={this.handlePointClick}
+        />
+        <div className="linkedin-heatmap-value-container" style={{ padding: '8px' }}>
+          <span>
+            {this.state.pointLabels[0]} - {this.state.pointLabels[1]}
+          </span>
+          <span style={{ marginLeft: '8px' }}>{this.state.pointValue}</span>
+        </div>
       </div>
       // <Graph
       //   timeSeries={vmSeries}
