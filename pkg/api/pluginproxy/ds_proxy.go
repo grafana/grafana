06133843313ddcba2c28de655f47ce02f30268b1
wilson.cheung <wilsoncheung@cryptoblk.io>
2019-03-21 12:23:29 +0800

    reformatted using gofmt

--- pkg/api/pluginproxy/ds_proxy.go
+++ pkg/api/pluginproxy/ds_proxy.go
@@ -252,7 +252,7 @@ func (proxy *DataSourceProxy) validateRequest() error {
 			return errors.New("Posts not allowed on proxied Elasticsearch datasource except on /_msearch")
 		}
 	}
-	
+
 	if String(proxy.ctx.SignedInUser.OrgId) != proxy.ctx.Req.Request.Header.Get("X-Grafana-Org-Id") {
 		return errors.New("Org-id in header not match with logged in user")
 	}
