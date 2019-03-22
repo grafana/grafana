60a97d716355af670a6e5e77832224220f5aafa6
shilpa <shilpa@poshmark.com>
2019-03-22 04:50:31 -0700

    moving average without nested

--- pkg/tsdb/metaqueries/metaqueries.go
+++ pkg/tsdb/metaqueries/metaqueries.go
@@ -40,16 +40,18 @@ func (e *MetaqueriesExecutor) Query(ctx context.Context, dsInfo *models.DataSour
 	fmt.Println("test fmt,", tsdbQuery.Queries[0].Model)
 	fmt.Println("test fmt,", reflect.TypeOf(tsdbQuery.Queries[0].Model))
 
-	//&{map[datasource:MetaQueries metric:ct outputMetricName:Day 7 periods:-7 query:A queryType:TimeShift refId:B]}
 	modelJson := tsdbQuery.Queries[0].Model
 	queryType := modelJson.Get("queryType").MustString()
 	if queryType == "TimeShift" {
 		response, err = e.timeShift(ctx, dsInfo, modelJson, tsdbQuery)
+	} else if queryType == "MovingAverage" {
+		response, err = e.movingAverage(ctx, dsInfo, modelJson, tsdbQuery)
 	}
-
 	println(response)
 	return response, err
 }
+
+var actualFrom int64
 
 func (e *MetaqueriesExecutor) timeShift(ctx context.Context, dsInfo *models.DataSource, modelJson *simplejson.Json, tsdbQuery *tsdb.TsdbQuery) (*tsdb.Response, error) {
 
@@ -68,14 +70,13 @@ func (e *MetaqueriesExecutor) timeShift(ctx context.Context, dsInfo *models.Data
 	}
 
 	from := time.Unix(int64(FromEpochMs), 0).AddDate(0, 0, periodsToShift)
-	to := time.Unix(int64(ToEpochMs), 0)
-	shiftedTo := to.AddDate(0, 0, periodsToShift)
+	to := time.Unix(int64(ToEpochMs), 0).AddDate(0, 0, periodsToShift)
 
 	fmt.Println("from ", from.UTC().String())
-	fmt.Println("to ", shiftedTo.UTC().String())
+	fmt.Println("to ", to.UTC().String())
 
 	tsdbQuery.TimeRange.From = strconv.Itoa(int(from.Unix()))
-	tsdbQuery.TimeRange.To = strconv.Itoa(int(shiftedTo.Unix()))
+	tsdbQuery.TimeRange.To = strconv.Itoa(int(to.Unix()))
 
 	fmt.Println("tsdbQuery.TimeRange.From ", tsdbQuery.TimeRange.From)
 	fmt.Println("tsdbQuery.TimeRange.To ", tsdbQuery.TimeRange.To)
@@ -118,6 +119,112 @@ func (e *MetaqueriesExecutor) timeShift(ctx context.Context, dsInfo *models.Data
 			}
 		}
 	}
+	fmt.Println("test response", err)
+	fmt.Println("test response ", len(response.Results))
+	return response, err
+}
+func (e *MetaqueriesExecutor) movingAverage(ctx context.Context, dsInfo *models.DataSource, modelJson *simplejson.Json, tsdbQuery *tsdb.TsdbQuery) (*tsdb.Response, error) {
+
+	//&{map[outputMetricName:ma periods:2 query:A queryType:MovingAverage refId:B datasource:MetaQueries metric:ct]}
+
+	periodsToShift, err1 := strconv.Atoi(modelJson.Get("periods").MustString())
+	if err1 != nil {
+		// handle error
+	}
+	query := modelJson.Get("query").MustString()
+
+	FromEpochMs, error1 := strconv.ParseInt(strconv.FormatInt(tsdbQuery.TimeRange.GetFromAsMsEpoch(), 10)[0:10], 10, 64)
+	ToEpochMs, error2 := strconv.ParseInt(strconv.FormatInt(tsdbQuery.TimeRange.GetToAsMsEpoch(), 10)[0:10], 10, 64)
+	if error1 != nil {
+	}
+	if error2 != nil {
+	}
+
+	if actualFrom == 0 {
+		actualFrom = FromEpochMs
+	}
+
+	//from := time.Unix(int64(FromEpochMs), 0)
+	shiftedFrom := time.Unix(int64(FromEpochMs), 0).AddDate(0, 0, -(periodsToShift - 1))
+	to := time.Unix(int64(ToEpochMs), 0)
+
+	fmt.Println("from ", shiftedFrom.UTC().String())
+	fmt.Println("to ", to.UTC().String())
+
+	tsdbQuery.TimeRange.From = strconv.Itoa(int(shiftedFrom.Unix()))
+	tsdbQuery.TimeRange.To = strconv.Itoa(int(to.Unix()))
+
+	fmt.Println("tsdbQuery.TimeRange.From ", tsdbQuery.TimeRange.From)
+	fmt.Println("tsdbQuery.TimeRange.To ", tsdbQuery.TimeRange.To)
+
+	var response *tsdb.Response
+	var err error
+
+	for i := 0; i < len(tsdbQuery.Queries); i++ {
+
+		targetRefId := tsdbQuery.Queries[i].Model.Get("refId").MustString()
+
+		if targetRefId == query {
+
+			targetQueryType := tsdbQuery.Queries[i].Model.Get("queryType").MustString()
+			targetDataSource := tsdbQuery.Queries[i].Model.Get("datasource").MustString()
+
+			if targetDataSource == "MetaQueries" && targetQueryType == "MovingAverage" {
+				tsdbQuery.Queries[0] = tsdbQuery.Queries[i]
+				response, err = e.movingAverage(ctx, tsdbQuery.Queries[i].DataSource, tsdbQuery.Queries[i].Model, tsdbQuery)
+
+			} else if tsdbQuery.Queries[i].Model.Get("druidDS") != nil {
+
+				//fmt.Println("actualFrom ",actualFrom)
+				fmt.Println("FromEpochMs ", FromEpochMs)
+				fmt.Println("ToEpochMs ", ToEpochMs)
+
+				druidQuery := tsdbQuery.Queries[i]
+				var druid *tsdb.TsdbQuery
+				druid = tsdbQuery
+
+				for i := 0; i < len(druid.Queries); i++ {
+					druid.Queries = append(druid.Queries[:i], druid.Queries[i+1:]...)
+				}
+				druid.Queries[len(druid.Queries)-1] = nil
+				druid.Queries = []*tsdb.Query{}
+
+				druid.Queries = append(druid.Queries, druidQuery)
+
+				response, err = tsdb.HandleRequest(ctx, druid.Queries[0].DataSource, druid)
+
+				dataPoints := response.Results[""].Series[0].Points
+				points := make([]tsdb.TimePoint, 0)
+				datapointByTime := make(map[int64]float64)
+
+				for i := 0; i < len(dataPoints); i++ {
+
+					datapointByTime[int64(dataPoints[i][1].Float64)] = dataPoints[i][0].Float64
+					var metricSum float64
+
+					if int64(dataPoints[i][1].Float64) >= actualFrom && int64(dataPoints[i][1].Float64) <= ToEpochMs {
+
+						for count := 0; count < periodsToShift; count++ {
+							targetDate := time.Unix(int64(dataPoints[i][1].Float64), 0).AddDate(0, 0, -count).Unix()
+							metricSum += datapointByTime[int64(targetDate)]
+
+						}
+						dataPoints[i][0].Float64 = metricSum / float64(periodsToShift)
+						points = append(points, tsdb.NewTimePoint(dataPoints[i][0], dataPoints[i][1].Float64))
+
+					}
+				}
+
+				response.Results[""].Series[0].Points = dataPoints
+				response.Results[""].Series[0].Points = points
+				fmt.Println("error message ", err)
+				fmt.Println("response message ", len(response.Results))
+				fmt.Println(reflect.TypeOf(response))
+				return response, err
+			}
+		}
+	}
+
 	fmt.Println("test response", err)
 	fmt.Println("test response ", len(response.Results))
 	return response, err
