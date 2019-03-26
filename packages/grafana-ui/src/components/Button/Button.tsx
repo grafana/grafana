bcf951ad4a04621280aa1d3e1e65d850cee92c9f
Dominik Prokop <dominik.prokop@grafana.com>
2019-03-26 17:52:12 +0100

    Export size variants of buttons

--- packages/grafana-ui/src/components/Button/Button.tsx
+++ packages/grafana-ui/src/components/Button/Button.tsx
@@ -1,6 +1,30 @@
 import React, { useContext } from 'react';
-import { AbstractButton, ButtonProps } from './AbstractButton';
+import { AbstractButton, ButtonProps, ButtonSize } from './AbstractButton';
 import { ThemeContext } from '../../themes';
+
+const getSizeNameComponentSegment = (size: ButtonSize) => {
+  switch (size) {
+    case ButtonSize.ExtraSmall:
+      return 'ExtraSmall';
+    case ButtonSize.Small:
+      return 'Small';
+    case ButtonSize.Large:
+      return 'Large';
+    case ButtonSize.ExtraLarge:
+      return 'ExtraLarge';
+    default:
+      return 'Medium';
+  }
+};
+const buttonFactory = (renderAs: string, size: ButtonSize, displayName: string) => {
+  const ButtonComponent: React.FunctionComponent<ButtonProps<HTMLButtonElement>> = props => {
+    const theme = useContext(ThemeContext);
+    return <AbstractButton {...props} size={size} renderAs={renderAs} theme={theme} />;
+  };
+  ButtonComponent.displayName = displayName;
+
+  return ButtonComponent;
+};
 
 export const Button: React.FunctionComponent<ButtonProps<HTMLButtonElement>> = props => {
   const theme = useContext(ThemeContext);
@@ -13,3 +37,45 @@ export const LinkButton: React.FunctionComponent<ButtonProps<HTMLAnchorElement>>
   return <AbstractButton {...props} renderAs="a" theme={theme} />;
 };
 LinkButton.displayName = 'LinkButton';
+
+export const ExtraSmallButton = buttonFactory(
+  'button',
+  ButtonSize.ExtraSmall,
+  `${getSizeNameComponentSegment(ButtonSize.ExtraSmall)}Button`
+);
+export const SmallButton = buttonFactory(
+  'button',
+  ButtonSize.Small,
+  `${getSizeNameComponentSegment(ButtonSize.Small)}Button`
+);
+export const LargeButton = buttonFactory(
+  'button',
+  ButtonSize.Large,
+  `${getSizeNameComponentSegment(ButtonSize.Large)}Button`
+);
+export const ExtraLargeButton = buttonFactory(
+  'button',
+  ButtonSize.ExtraLarge,
+  `${getSizeNameComponentSegment(ButtonSize.ExtraLarge)}Button`
+);
+
+export const ExtraSmallLinkButton = buttonFactory(
+  'a',
+  ButtonSize.ExtraSmall,
+  `${getSizeNameComponentSegment(ButtonSize.ExtraSmall)}LinkButton`
+);
+export const SmallLinkButton = buttonFactory(
+  'a',
+  ButtonSize.Small,
+  `${getSizeNameComponentSegment(ButtonSize.Small)}LinkButton`
+);
+export const LargeLinkButton = buttonFactory(
+  'a',
+  ButtonSize.Large,
+  `${getSizeNameComponentSegment(ButtonSize.Large)}LinkButton`
+);
+export const ExtraLargeLinkButton = buttonFactory(
+  'a',
+  ButtonSize.ExtraLarge,
+  `${getSizeNameComponentSegment(ButtonSize.ExtraLarge)}LinkButton`
+);
