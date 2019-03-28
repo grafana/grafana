0bc314a47bb68acadd1e06fce7840740e9135dd7
Steven Sheehy <ssheehy@firescope.com>
2019-03-28 09:58:19 -0500

    Feature: Case insensitive Loki search (#15948)
    * Case insensitive Loki search
    * Make Loki case insensitivity work with highlighting
    
    Signed-off-by: Steven Sheehy <ssheehy@firescope.com>

--- public/app/plugins/datasource/loki/query_utils.ts
+++ public/app/plugins/datasource/loki/query_utils.ts
@@ -1,4 +1,5 @@
 const selectorRegexp = /(?:^|\s){[^{]*}/g;
+const caseInsensitive = '(?i)'; // Golang mode modifier for Loki, doesn't work in JavaScript
 export function parseQuery(input: string) {
   input = input || '';
   const match = input.match(selectorRegexp);
@@ -10,6 +11,9 @@ export function parseQuery(input: string) {
     regexp = input.replace(selectorRegexp, '').trim();
   }
 
+  if (regexp) {
+    regexp = caseInsensitive + regexp;
+  }
   return { query, regexp };
 }
 
