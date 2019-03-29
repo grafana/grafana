83685762496ff12251398a4b2f453848ed4059e1
Hugo Häggmark <hugo.haggmark@gmail.com>
2019-03-29 15:32:58 +0100

    Chore: breaks build if certain FrontEnd limits are exceeded (#16301)
    Build: breaks build if certain FrontEnd limits are exceeded

--- scripts/circle-test-frontend.sh
+++ scripts/circle-test-frontend.sh
@@ -19,8 +19,9 @@ exit_if_fail npm run test
 end=$(date +%s)
 seconds=$((end - start))
 
+exit_if_fail ./scripts/ci-frontend-metrics.sh
+
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-	exit_if_fail ./scripts/ci-frontend-metrics.sh
 	exit_if_fail ./scripts/ci-metrics-publisher.sh grafana.ci-performance.frontend-tests=$seconds
 fi
 
