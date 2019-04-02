7a3646a188008cea1c33427b2b27d30e996b76f5
Leonard Gram <leo@xlson.com>
2019-04-02 16:14:34 +0200

    build: checks latest version of rpm in repos.

--- scripts/verify-repo-update/verify-repos.sh
+++ scripts/verify-repo-update/verify-repos.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
 docker_build () {
-	repo_file=$1
-	package=$2
-	tag=$3
+	dockerfile=$1
+	repo_file=$2
+	package=$3
+	tag=$4
 
-  docker build -f Dockerfile.deb               \
+  docker build -f $dockerfile            \
     --build-arg "REPO_CONFIG=$repo_file" \
-    --build-arg "PACKAGE=$package"              \
-    --tag $tag                 \
+    --build-arg "PACKAGE=$package"       \
+    --tag $tag                           \
+    --no-cache                           \
     .
 }
 
-docker_build "grafana.list.oss" "grafana" "gf-oss-deb-repo-test"
+
+# Testing deb repos
+docker_build "Dockerfile.deb" "grafana.list.oss" "grafana" "gf-oss-deb-repo-test"
 _oss_deb_v=$(docker run --rm gf-oss-deb-repo-test cat /usr/share/grafana/VERSION)
 
-docker_build "grafana.list.ee" "grafana-enterprise" "gf-ee-deb-repo-test"
+docker_build "Dockerfile.deb" "grafana.list.ee" "grafana-enterprise" "gf-ee-deb-repo-test"
 _ee_deb_v=$(docker run --rm gf-ee-deb-repo-test cat /usr/share/grafana/VERSION)
+
+# Testing rpm repos
+docker_build "Dockerfile.rpm" "grafana.repo.oss" "grafana" "gf-oss-rpm-repo-test"
+_oss_rpm_v=$(docker run --rm gf-oss-rpm-repo-test cat /usr/share/grafana/VERSION)
+
+docker_build "Dockerfile.rpm" "grafana.repo.ee" "grafana-enterprise" "gf-ee-rpm-repo-test"
+_ee_rpm_v=$(docker run --rm gf-ee-rpm-repo-test cat /usr/share/grafana/VERSION)
 
 echo Versions:
 echo OSS deb = ${_oss_deb_v}
 echo EE  deb = ${_ee_deb_v}
+echo OSS rpm = ${_oss_rpm_v}
+echo EE  rpm = ${_ee_rpm_v}
