7a7fbac02ae8f2966babfef15cffd9229f393567
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-04-02 11:01:35 +0200

    Refactor: Made TypeaheadItem a FunctionComponent using emotion

--- public/app/features/explore/TypeaheadItem.tsx
+++ public/app/features/explore/TypeaheadItem.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 // @ts-ignore
 import Highlighter from 'react-highlight-words';
+import { css, cx } from 'emotion';
+import { GrafanaTheme, ThemeContext, selectThemeVariant } from '@grafana/ui';
 
 import { CompletionItem } from 'app/types/explore';
 
@@ -12,20 +14,51 @@ interface Props {
   style: any;
 }
 
-export class TypeaheadItem extends React.PureComponent<Props> {
-  onClick = () => {
-    this.props.onClickItem(this.props.item);
-  };
+const getStyles = (theme: GrafanaTheme) => ({
+  typeaheadItem: css`
+    height: auto;
+    font-family: ${theme.typography.fontFamily.monospace};
+    padding: ${theme.spacing.sm} ${theme.spacing.sm} ${theme.spacing.sm} ${theme.spacing.md};
+    font-size: ${theme.typography.size.sm};
+    text-overflow: ellipsis;
+    overflow: hidden;
+    z-index: 1;
+    display: block;
+    white-space: nowrap;
+    cursor: pointer;
+    transition: color 0.3s cubic-bezier(0.645, 0.045, 0.355, 1), border-color 0.3s cubic-bezier(0.645, 0.045, 0.355, 1),
+      background 0.3s cubic-bezier(0.645, 0.045, 0.355, 1), padding 0.15s cubic-bezier(0.645, 0.045, 0.355, 1);
+  `,
+  typeaheadItemSelected: css`
+    background-color: ${selectThemeVariant({ light: theme.colors.gray6, dark: theme.colors.dark9 }, theme.type)};
+  `,
+  typeaheadMatch: css`
+    color: ${theme.colors.yellow};
+    border-bottom: 1px solid ${theme.colors.yellow};
+    // Undoing mark styling
+    padding: inherit;
+    background: inherit;
+  `,
+  typeaheadItemHint: css`
+    font-size: ${theme.typography.fontFamily.monospace};
+    color: ${theme.colors.text};
+    white-space: normal;
+  `,
+});
 
-  render() {
-    const { isSelected, item, prefix, style } = this.props;
-    const className = isSelected ? 'typeahead-item typeahead-item__selected' : 'typeahead-item';
-    const label = item.label || '';
-    return (
-      <li className={className} onClick={this.onClick} style={style}>
-        <Highlighter textToHighlight={label} searchWords={[prefix]} highlightClassName="typeahead-match" />
-        {item.documentation && isSelected ? <div className="typeahead-item-hint">{item.documentation}</div> : null}
-      </li>
-    );
-  }
-}
+export const TypeaheadItem: FunctionComponent<Props> = (props: Props) => {
+  const css = getStyles(useContext(ThemeContext));
+  const { isSelected, item, prefix, style, onClickItem } = props;
+  const onClick = () => onClickItem(item);
+  const className = isSelected ? cx([css.typeaheadItem, css.typeaheadItemSelected]) : cx([css.typeaheadItem]);
+  const highlightClassName = cx([css.typeaheadMatch]);
+  const itemHintClassName = cx([css.typeaheadItemHint]);
+  const label = item.label || '';
+
+  return (
+    <li className={className} onClick={onClick} style={style}>
+      <Highlighter textToHighlight={label} searchWords={[prefix]} highlightClassName={highlightClassName} />
+      {item.documentation && isSelected ? <div className={itemHintClassName}>{item.documentation}</div> : null}
+    </li>
+  );
+};
