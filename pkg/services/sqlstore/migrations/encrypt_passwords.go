1ebd79742f57e74e3056f79abfebc18b27d817b2
Andrej Ocenas <mr.ocenas@gmail.com>
2019-04-04 13:08:28 +0200

    Remove copied encryption code from migration

--- pkg/services/sqlstore/migrations/encrypt_passwords.go
+++ pkg/services/sqlstore/migrations/encrypt_passwords.go
@@ -1,19 +1,12 @@
 package migrations
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"github.com/go-xorm/xorm"
 	. "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/util"
-	"hash"
-	"io"
 )
 
 type AddEncryptPasswordsMigration struct {
@@ -97,7 +90,7 @@ func updateRows(session *xorm.Session, rows []map[string]string, passwordFieldNa
 }
 
 func getUpdatedSecureJsonData(row map[string]string, passwordFieldName string) (map[string]interface{}, error) {
-	encryptedPassword, err := encrypt([]byte(row[passwordFieldName]), setting.SecretKey)
+	encryptedPassword, err := util.Encrypt([]byte(row[passwordFieldName]), setting.SecretKey)
 	if err != nil {
 		return nil, err
 	}
@@ -111,98 +104,4 @@ func getUpdatedSecureJsonData(row map[string]string, passwordFieldName string) (
 	jsonFieldName := util.ToCamelCase(passwordFieldName)
 	secureJsonData[jsonFieldName] = encryptedPassword
 	return secureJsonData, nil
-}
-
-//
-// All functions below are copy pasted from encryption.go and encoding.go to keep this migration as stable as possible.
-// So if the encryption changes this will still encrypt passwords the way the migration was created (though if some
-// of the imports change this could also change the encryption, bit I think they should be reasonably stable)
-//
-
-const saltLength = 8
-
-// GetRandomString generate random string by specify chars.
-// source: https://github.com/gogits/gogs/blob/9ee80e3e5426821f03a4e99fad34418f5c736413/modules/base/tool.go#L58
-func GetRandomString(n int, alphabets ...byte) string {
-	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	var bytes = make([]byte, n)
-	rand.Read(bytes)
-	for i, b := range bytes {
-		if len(alphabets) == 0 {
-			bytes[i] = alphanum[b%byte(len(alphanum))]
-		} else {
-			bytes[i] = alphabets[b%byte(len(alphabets))]
-		}
-	}
-	return string(bytes)
-}
-
-// PBKDF2 implements Password-Based Key Derivation Function 2), aimed to reduce
-// the vulnerability of encrypted keys to brute force attacks.
-// http://code.google.com/p/go/source/browse/pbkdf2/pbkdf2.go?repo=crypto
-func PBKDF2(password, salt []byte, iter, keyLen int, h func() hash.Hash) []byte {
-	prf := hmac.New(h, password)
-	hashLen := prf.Size()
-	numBlocks := (keyLen + hashLen - 1) / hashLen
-
-	var buf [4]byte
-	dk := make([]byte, 0, numBlocks*hashLen)
-	U := make([]byte, hashLen)
-	for block := 1; block <= numBlocks; block++ {
-		// N.B.: || means concatenation, ^ means XOR
-		// for each block T_i = U_1 ^ U_2 ^ ... ^ U_iter
-		// U_1 = PRF(password, salt || uint(i))
-		prf.Reset()
-		prf.Write(salt)
-		buf[0] = byte(block >> 24)
-		buf[1] = byte(block >> 16)
-		buf[2] = byte(block >> 8)
-		buf[3] = byte(block)
-		prf.Write(buf[:4])
-		dk = prf.Sum(dk)
-		T := dk[len(dk)-hashLen:]
-		copy(U, T)
-
-		// U_n = PRF(password, U_(n-1))
-		for n := 2; n <= iter; n++ {
-			prf.Reset()
-			prf.Write(U)
-			U = U[:0]
-			U = prf.Sum(U)
-			for x := range U {
-				T[x] ^= U[x]
-			}
-		}
-	}
-	return dk[:keyLen]
-}
-
-// Encrypt encrypts a payload with a given secret.
-func encrypt(payload []byte, secret string) ([]byte, error) {
-	salt := GetRandomString(saltLength)
-
-	key := encryptionKeyToBytes(secret, salt)
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	ciphertext := make([]byte, saltLength+aes.BlockSize+len(payload))
-	copy(ciphertext[:saltLength], []byte(salt))
-	iv := ciphertext[saltLength : saltLength+aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[saltLength+aes.BlockSize:], payload)
-
-	return ciphertext, nil
-}
-
-// Key needs to be 32bytes
-func encryptionKeyToBytes(secret, salt string) []byte {
-	return PBKDF2([]byte(secret), []byte(salt), 10000, 32, sha256.New)
 }
