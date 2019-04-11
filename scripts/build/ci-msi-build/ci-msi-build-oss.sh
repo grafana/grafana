e805bdc752c28254e7145edc7def4d78b3cf3456
Brian Gann <bkgann@gmail.com>
2019-04-11 00:52:23 -0500

    allow script to run in place

--- scripts/build/ci-msi-build/ci-msi-build-oss.sh
+++ scripts/build/ci-msi-build/ci-msi-build-oss.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 WORKING_DIRECTORY=`pwd`
-# copy oss files to /master
+# copy zip file to /tmp/dist
 mkdir -p /tmp/dist
 cp ./dist/*.zip /tmp/dist
 echo "Contents of /tmp/dist"
 ls -al /tmp/dist
-mkdir -p /master
-cp -r ./scripts/build/ci-msi-build/oss /master
-echo "Contents of /master"
-ls -al /master
 
 # nssm download has been unreliable, use a cached copy of it
 echo "Caching NSSM"
 mkdir -p /tmp/cache
-cp /master/cache/nssm-2.24.zip /tmp/cache
+cp ./scripts/build/ci-msi-build/oss/cache/nssm-2.24.zip /tmp/cache
 # a build can be specified, which will be pulled down
 #python3 generator/build.py --build 5.4.3
 echo "LIGHT config"
 ls -al /home/xclient/wix/light.exe.config
 cat /home/xclient/wix/light.exe.config
-cp /master/light.exe.config /home/xclient/wix/light.exe.config
+cp ./scripts/build/ci-msi-build/oss/light.exe.config /home/xclient/wix/light.exe.config
 cat /home/xclient/wix/light.exe.config
-cd /master
+cd ./scripts/build/ci-msi-build/oss
 echo "Building MSI"
 python3 generator/build.py "$@"
 chmod a+x /tmp/a/*.msi
