1cf203bea8e248c71fdd8d5add6dd64a228bc8a4
ryan <ryantxu@gmail.com>
2019-04-11 01:27:50 -0700

    app pages

--- packages/grafana-ui/src/types/plugin.ts
+++ packages/grafana-ui/src/types/plugin.ts
@@ -50,18 +50,3 @@ export interface PluginMetaInfo {
   updated: string;
   version: string;
 }
-
-export class AppPlugin {
-  components: {
-    ConfigCtrl?: any;
-  };
-
-  pages: { [str: string]: any };
-
-  constructor(ConfigCtrl: any) {
-    this.components = {
-      ConfigCtrl: ConfigCtrl,
-    };
-    this.pages = {};
-  }
-}
