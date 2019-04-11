22d9606cd62759456ad81c96071da254675e7a3d
Johannes Schill <johannes.schill@polyester.se>
2019-04-11 08:49:42 +0200

    feat: Add util for removing keys with empty values

--- packages/grafana-ui/src/components/RefreshPicker/RefreshPicker.tsx
+++ packages/grafana-ui/src/components/RefreshPicker/RefreshPicker.tsx
@@ -10,7 +10,7 @@ export const defaultIntervals = ['5s', '10s', '30s', '1m', '5m', '15m', '30m', '
 export interface Props {
   initialValue: string;
   intervals?: string[];
-  onRefresh: () => void;
+  onRefresh: () => any; // Promise<any> | ThunkAction<Promise<any>>
   onIntervalChanged: (item: SelectOptionItem) => void;
   value?: SelectOptionItem;
 }
