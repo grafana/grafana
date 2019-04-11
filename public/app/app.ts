d6887bf77f7849735acbfbdfcd209fdb2b416ed9
Ryan McKinley <ryantxu@gmail.com>
2019-04-10 23:02:22 -0700

    Plugins: Optionally preload some plugins during frontend app boot (#15266)
    * auto load
    
    * update comments
    
    * gofmt
    
    * use preload from json
    
    * fix formatting
    
    * change general plugin loader to app
    
    * Refactoring: Plugin preloading #15266

--- public/app/app.ts
+++ public/app/app.ts
@@ -21,6 +21,7 @@ import config from 'app/core/config';
 import _ from 'lodash';
 import moment from 'moment';
 import { addClassIfNoOverlayScrollbar } from 'app/core/utils/scrollbar';
+import { importPluginModule } from 'app/features/plugins/plugin_loader';
 
 // add move to lodash for backward compatabiltiy
 _.move = (array: [], fromIndex: number, toIndex: number) => {
@@ -145,6 +146,11 @@ export class GrafanaApp {
 
       this.preBootModules = null;
     });
+
+    // Preload selected app plugins
+    for (const modulePath of config.pluginsToPreload) {
+      importPluginModule(modulePath);
+    }
   }
 }
 
