c27f3f7170c21cbe8bf565a04b2d2e7e052126e1
Torkel Ödegaard <torkel@grafana.com>
2019-04-12 12:30:45 +0200

    Refactoring and moving settings to plugins section

--- pkg/api/frontendsettings.go
+++ pkg/api/frontendsettings.go
@@ -145,7 +145,7 @@ func (hs *HTTPServer) getFrontendSettingsMap(c *m.ReqContext) (map[string]interf
 
 	panels := map[string]interface{}{}
 	for _, panel := range enabledPlugins.Panels {
-		if panel.State == plugins.PluginStateAlpha && !hs.Cfg.EnableAlphaPlugins {
+		if panel.State == plugins.PluginStateAlpha && !hs.Cfg.PluginsEnableAlpha {
 			continue
 		}
 
