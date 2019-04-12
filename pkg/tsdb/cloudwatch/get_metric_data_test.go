ae8bec94a80a290f21d05ce391a6affe2a905077
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2019-04-13 03:41:19 +0900

    add test for GetMetricData parse response

--- pkg/tsdb/cloudwatch/get_metric_data_test.go
+++ pkg/tsdb/cloudwatch/get_metric_data_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/grafana/grafana/pkg/components/null"
 	"github.com/grafana/grafana/pkg/tsdb"
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -13,7 +14,7 @@ import (
 func TestCloudWatchGetMetricData(t *testing.T) {
 	Convey("CloudWatchGetMetricData", t, func() {
 
-		Convey("can parse cloudwatch json model", func() {
+		Convey("can parse cloudwatch GetMetricData query", func() {
 			queries := map[string]*CloudWatchQuery{
 				"id1": {
 					RefId:      "A",
@@ -57,6 +58,56 @@ func TestCloudWatchGetMetricData(t *testing.T) {
 			So(*res.MetricDataQueries[1].Id, ShouldEqual, "id2")
 			So(*res.MetricDataQueries[1].Expression, ShouldEqual, "id1 * 2")
 			So(*res.MetricDataQueries[1].ReturnData, ShouldEqual, true)
+		})
+
+		Convey("can parse cloudwatch response", func() {
+			timestamp := time.Unix(0, 0)
+			resp := map[string]*cloudwatch.MetricDataResult{
+				"label": &cloudwatch.MetricDataResult{
+					Id:    aws.String("id1"),
+					Label: aws.String("label"),
+					Timestamps: []*time.Time{
+						aws.Time(timestamp),
+						aws.Time(timestamp.Add(60 * time.Second)),
+						aws.Time(timestamp.Add(180 * time.Second)),
+					},
+					Values: []*float64{
+						aws.Float64(10),
+						aws.Float64(20),
+						aws.Float64(30),
+					},
+					StatusCode: aws.String("Complete"),
+				},
+			}
+			query := &CloudWatchQuery{
+				RefId:      "refId1",
+				Region:     "us-east-1",
+				Namespace:  "AWS/ApplicationELB",
+				MetricName: "TargetResponseTime",
+				Dimensions: []*cloudwatch.Dimension{
+					{
+						Name:  aws.String("LoadBalancer"),
+						Value: aws.String("lb"),
+					},
+					{
+						Name:  aws.String("TargetGroup"),
+						Value: aws.String("tg"),
+					},
+				},
+				Statistics: []*string{aws.String("Average")},
+				Period:     60,
+				Alias:      "{{namespace}}_{{metric}}_{{stat}}",
+			}
+			queryRes, err := parseGetMetricDataResponse(resp, query)
+			So(err, ShouldBeNil)
+			So(queryRes.RefId, ShouldEqual, "refId1")
+			So(queryRes.Series[0].Name, ShouldEqual, "AWS/ApplicationELB_TargetResponseTime_Average")
+			So(queryRes.Series[0].Tags["LoadBalancer"], ShouldEqual, "lb")
+			So(queryRes.Series[0].Tags["TargetGroup"], ShouldEqual, "tg")
+			So(queryRes.Series[0].Points[0][0].String(), ShouldEqual, null.FloatFrom(10.0).String())
+			So(queryRes.Series[0].Points[1][0].String(), ShouldEqual, null.FloatFrom(20.0).String())
+			So(queryRes.Series[0].Points[2][0].String(), ShouldEqual, null.FloatFromPtr(nil).String())
+			So(queryRes.Series[0].Points[3][0].String(), ShouldEqual, null.FloatFrom(30.0).String())
 		})
 	})
 }
