4cfcc3aff1c437f1679947cf762fa23d88e1f1d9
Andrej Ocenas <mr.ocenas@gmail.com>
2019-04-15 12:15:35 +0200

    Add explicit typing

--- public/app/features/datasources/utils/passwordHandlers.test.ts
+++ public/app/features/datasources/utils/passwordHandlers.test.ts
@@ -2,7 +2,7 @@ import { createResetHandler, PasswordFieldEnum, Ctrl } from './passwordHandlers'
 
 describe('createResetHandler', () => {
   Object.keys(PasswordFieldEnum).forEach(fieldKey => {
-    const field = PasswordFieldEnum[fieldKey];
+    const field: any = PasswordFieldEnum[fieldKey as any];
 
     it(`should reset existing ${field} field`, () => {
       const event: any = {
