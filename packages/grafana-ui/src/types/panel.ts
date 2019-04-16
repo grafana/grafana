514818f16d3790debf0a95b1ed3e549d28714c35
Ryan McKinley <ryantxu@gmail.com>
2019-04-16 13:23:34 -0700

    Panel Plugins: pass query request/response to react panel plugins (#16577)
    * pass query request/response to panel plugins
    
    * rename finishTime to endTime
    
    * move QueryResponseData to a sub variable
    
    * rename to PanelData
    
    * make data not optional
    
    * make data not optional
    
    * missing optional

--- packages/grafana-ui/src/types/panel.ts
+++ packages/grafana-ui/src/types/panel.ts
@@ -1,14 +1,22 @@
 import { ComponentClass } from 'react';
 import { LoadingState, SeriesData } from './data';
 import { TimeRange } from './time';
-import { ScopedVars } from './datasource';
+import { ScopedVars, DataRequestInfo, DataQueryError } from './datasource';
 
 export type InterpolateFunction = (value: string, scopedVars?: ScopedVars, format?: string | Function) => string;
 
+export interface PanelData {
+  state: LoadingState;
+  series: SeriesData[];
+  request?: DataRequestInfo;
+  error?: DataQueryError;
+}
+
 export interface PanelProps<T = any> {
-  data?: SeriesData[];
+  data: PanelData;
+  // TODO: annotation?: PanelData;
+
   timeRange: TimeRange;
-  loading: LoadingState;
   options: T;
   renderCounter: number;
   width: number;
