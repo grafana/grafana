c316519dd217faa43fcb9990a7eba11156c10bb4
ryan <ryantxu@gmail.com>
2019-04-18 15:31:02 -0700

    cleanup

--- public/app/plugins/datasource/mixed/MixedDatasource.ts
+++ public/app/plugins/datasource/mixed/MixedDatasource.ts
@@ -1,4 +1,3 @@
-import groupBy from 'lodash/groupBy';
 import cloneDeep from 'lodash/cloneDeep';
 
 import {
@@ -19,50 +18,28 @@ export const MIXED_DATASOURCE_NAME = '-- Mixed --';
 export class MixedDatasource implements DataSourceApi<DataQuery> {
   async query(request: DataQueryRequest<DataQuery>, stream?: DataSourceStream): Promise<DataQueryResponse> {
     const datasourceSrv = getDatasourceSrv();
-    let needsDefatulDS = false;
 
-    // Remove any hidden and invalid queries
-    let queries = request.targets.filter(t => {
-      if (!t.datasource) {
-        needsDefatulDS = true;
-      }
+    // Remove any hidden or invalid queries
+    const queries = request.targets.filter(t => {
       return !t.hide || t.datasource !== MIXED_DATASOURCE_NAME;
     });
 
-    if (needsDefatulDS) {
-      const defaultDS = await datasourceSrv.get();
-      queries = queries.map(q => {
-        if (!q.datasource) {
-          q.datasource = defaultDS.name;
-        }
-        return q;
-      });
-    }
-
-    const sets = groupBy(queries, 'datasource');
-    const sources = Object.keys(sets);
-    if (!sources.length) {
+    if (!queries.length) {
       return Promise.resolve({ data: [] }); // nothing
-    }
-
-    // For a single datasource, just delegate
-    if (sources.length === 1) {
-      const ds = await datasourceSrv.get(sources[0]);
-      return ds.query(request, stream);
     }
 
     // Update the sub request list
     request.subRequests = [];
 
     let finished = 0;
-    const everything: QueryResultBase[] = [];
-
-    const promises = sources.map((name, index) => {
-      return datasourceSrv.get(name).then(ds => {
+    const results: QueryResultBase[] = [];
+    const all = queries.map((query, index) => {
+      return datasourceSrv.get(query.datasource).then(ds => {
         const sub = cloneDeep(request);
         sub.requestId = request.requestId + '_' + index;
-        sub.targets = sets[name];
+        sub.targets = [query];
         sub.startTime = Date.now();
+        request.subRequests.push(sub);
         const data: PanelData = {
           state: LoadingState.Loading,
           request: sub,
@@ -79,35 +56,36 @@ export class MixedDatasource implements DataSourceApi<DataQuery> {
             finished++;
 
             // Attach the requestId to the returned results
-            const results = res.data.map((r: QueryResultBase) => {
+            const withMeta = res.data.map((r: QueryResultBase) => {
               if (!r.meta) {
                 r.meta = { requestId: sub.requestId };
               } else {
                 r.meta.requestId = sub.requestId;
               }
-              everything.push(r);
+              results.push(r);
               return;
             });
 
-            if (stream && finished < promises.length) {
-              data.series = getProcessedSeriesData(results);
+            if (stream && finished < all.length) {
+              data.series = getProcessedSeriesData(withMeta);
               stream.onStreamProgress(data);
             }
-            return { data: results };
+            return { data: withMeta };
           })
           .catch(err => {
             sub.endTime = Date.now();
             data.state = LoadingState.Error;
             data.error = toDataQueryError(err);
             finished++;
-            if (stream && finished < promises.length) {
+            if (stream && finished < all.length) {
               stream.onStreamProgress(data);
             }
           });
       });
     });
-    return Promise.all(promises).then(results => {
-      return { data: everything };
+
+    return Promise.all(all).then(results => {
+      return { data: results };
     });
   }
 
