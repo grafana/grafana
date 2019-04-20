38524b81ebc249ad5cbe5b87eeff8b07098b81c3
hagen1778 <hagen1778@gmail.com>
2019-04-20 22:09:51 +0100

    Add meta-variable for better templating experience
    Meta-variable allows to reference already declared variables
    which gives additional flexibility in queries. Such as declaring
    common filters, creating dynamic queries or using CTE extensions
    in databases like VictoriaMetrics.

---
+++ public/app/features/templating/meta_variable.ts
@@ -0,0 +1,60 @@
+import { Variable, assignModelProperties, variableTypes, containsVariable } from './variable';
+
+export class MetaVariable implements Variable {
+  query: string;
+  current: any;
+  options: any[];
+  skipUrlSync: boolean;
+
+  defaults = {
+    type: 'meta',
+    name: '',
+    hide: 2,
+    label: '',
+    query: '',
+    current: {},
+    options: [],
+    skipUrlSync: false,
+  };
+
+  /** @ngInject */
+  constructor(private model, private variableSrv, private templateSrv) {
+    assignModelProperties(this, model, this.defaults);
+  }
+
+  getSaveModel() {
+    assignModelProperties(this.model, this, this.defaults);
+    return this.model;
+  }
+
+  setValue(option) {
+    this.variableSrv.setOptionAsCurrent(this, option);
+  }
+
+  updateOptions() {
+    const text = this.query.trim(),
+          value = this.templateSrv.replace(text, this.templateSrv.variables);
+    this.options = [{ text: text, value: value }];
+    this.current = this.options[0];
+    return Promise.resolve();
+  }
+
+  dependsOn(variable) {
+    return containsVariable(this.query, variable.name);
+  }
+
+  setValueFromUrl(urlValue) {
+    this.query = urlValue;
+    return this.variableSrv.setOptionFromUrl(this, urlValue);
+  }
+
+  getValueForUrl() {
+    return this.current.text;
+  }
+}
+
+variableTypes['meta'] = {
+  name: 'Meta',
+  ctor: MetaVariable,
+  description: 'Define a hidden meta variable, where users can use other variables',
+};
