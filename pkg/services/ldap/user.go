4e440b36f00536ae44f7b152b11889fd13b98d21
Oleg Gaidarenko <markelog@gmail.com>
2019-04-21 08:46:57 +0300

    Chore: duplicate ldap module as a service

---
+++ pkg/services/ldap/user.go
@@ -0,0 +1,27 @@
+package ldap
+
+import (
+	"strings"
+)
+
+type LdapUserInfo struct {
+	DN        string
+	FirstName string
+	LastName  string
+	Username  string
+	Email     string
+	MemberOf  []string
+}
+
+func (u *LdapUserInfo) isMemberOf(group string) bool {
+	if group == "*" {
+		return true
+	}
+
+	for _, member := range u.MemberOf {
+		if strings.EqualFold(member, group) {
+			return true
+		}
+	}
+	return false
+}
