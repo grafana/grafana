2aa02b59f6a9f00c296716afd3ef8380317e0771
Dominik Prokop <dominik.prokop@grafana.com>
2019-04-23 16:32:34 +0200

    Allow grid columns config for PanelOptionsGrid

--- packages/grafana-ui/src/components/PanelOptionsGrid/PanelOptionsGrid.tsx
+++ packages/grafana-ui/src/components/PanelOptionsGrid/PanelOptionsGrid.tsx
@@ -1,10 +1,23 @@
-import React, { SFC } from 'react';
+import React, { SFC, useContext } from 'react';
+import { ThemeContext } from '../../themes/ThemeContext';
+import { css } from 'emotion';
 
 interface Props {
   cols?: number;
   children: JSX.Element[] | JSX.Element;
 }
 
-export const PanelOptionsGrid: SFC<Props> = ({ children }) => {
-  return <div className="panel-options-grid">{children}</div>;
+const getStyles = (columns = 3, breakpoint: string) => css`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-row-gap: 10px;
+  grid-column-gap: 10px;
+  @media screen and (min-width: ${breakpoint}) {
+    grid-template-columns: repeat(${columns}, 1fr);
+  }
+`;
+
+export const PanelOptionsGrid: SFC<Props> = ({ cols, children }) => {
+  const theme = useContext(ThemeContext);
+  return <div className={getStyles(cols, theme.breakpoints.lg)}>{children}</div>;
 };
