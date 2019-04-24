5cc8811c3f969da0ee17d587ac498fc0a58836e6
Torkel Ödegaard <torkel@grafana.com>
2019-04-24 22:01:35 +0200

    PanelChrome: delay state update while other panel is in fullscreen/edit

--- public/app/features/dashboard/dashgrid/PanelChrome.tsx
+++ public/app/features/dashboard/dashgrid/PanelChrome.tsx
@@ -48,6 +48,7 @@ export interface State {
 export class PanelChrome extends PureComponent<Props, State> {
   timeSrv: TimeSrv = getTimeSrv();
   querySubscription: Unsubscribable;
+  delayedStateUpdate: Partial<State>;
 
   constructor(props: Props) {
     super(props);
@@ -118,7 +119,15 @@ export class PanelChrome extends PureComponent<Props, State> {
         }
       }
 
-      this.setState({ isFirstLoad, errorMessage, data });
+      const stateUpdate = { isFirstLoad, errorMessage, data };
+
+      if (this.isVisible) {
+        this.setState(stateUpdate);
+      } else {
+        // if we are getting data while another panel is in fullscreen / edit mode
+        // we need to store the data but not update state yet
+        this.delayedStateUpdate = stateUpdate;
+      }
     },
   };
 
@@ -162,9 +171,15 @@ export class PanelChrome extends PureComponent<Props, State> {
   };
 
   onRender = () => {
-    this.setState({
-      renderCounter: this.state.renderCounter + 1,
-    });
+    const stateUpdate = { renderCounter: this.state.renderCounter + 1 };
+
+    // If we have received a data update while hidden copy over that state as well
+    if (this.delayedStateUpdate) {
+      Object.assign(stateUpdate, this.delayedStateUpdate);
+      this.delayedStateUpdate = null;
+    }
+
+    this.setState(stateUpdate);
   };
 
   onOptionsChange = (options: any) => {
