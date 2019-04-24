dd2769e30b62db27a8e14e3f0d4a71ecf207af52
Dominik Prokop <dominik.prokop@grafana.com>
2019-04-24 16:50:26 +0200

    Cleanup

--- public/app/plugins/datasource/testdata/module.tsx
+++ public/app/plugins/datasource/testdata/module.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { DataSourcePlugin, DataSourcePluginOptionsEditorProps, Input, DataSourceSettings } from '@grafana/ui';
+import { DataSourcePlugin } from '@grafana/ui';
 import { TestDataDatasource } from './datasource';
 import { TestDataQueryCtrl } from './query_ctrl';
 
@@ -9,25 +8,6 @@ class TestDataAnnotationsQueryCtrl {
   static template = '<h2>Annotation scenario</h2>';
 }
 
-interface TestDataOptions extends DataSourceSettings {
-  secondaryNameTest: string;
-}
-
-const TestDataOptionsEditor: React.FunctionComponent<DataSourcePluginOptionsEditorProps<TestDataOptions>> = ({
-  options,
-  onOptionsChange,
-}) => {
-  return (
-    <Input
-      value={options.name}
-      onChange={event => {
-        onOptionsChange({ ...options, name: event.currentTarget.value });
-      }}
-    />
-  );
-};
-
-export const plugin = new DataSourcePlugin<TestDataOptions>(TestDataDatasource)
+export const plugin = new DataSourcePlugin(TestDataDatasource)
   .setQueryCtrl(TestDataQueryCtrl)
-  .setAnnotationQueryCtrl(TestDataAnnotationsQueryCtrl)
-  .setConfigEditor(TestDataOptionsEditor);
+  .setAnnotationQueryCtrl(TestDataAnnotationsQueryCtrl);
