78cd9058a3d756b5b0e1a4c31943220382c1f013
Oleg Gaidarenko <markelog@gmail.com>
2019-04-25 17:12:56 +0300

    Feature: add cron setting for the ldap settings (#16673)
    * Feature: add cron setting for the ldap settings
    
    * Move ldap configuration read to special function
    
    * Introduce cron setting (no docs for it yet, pending approval)
    
    * Chore: address code review comments

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -166,6 +166,7 @@ var (
 	// LDAP
 	LdapEnabled     bool
 	LdapConfigFile  string
+	LdapSyncCron    string
 	LdapAllowSignup = true
 
 	// QUOTA
@@ -877,14 +878,6 @@ func (cfg *Cfg) Load(args *CommandLineArgs) error {
 		return err
 	}
 
-	ldapSec := iniFile.Section("auth.ldap")
-	LdapEnabled = ldapSec.Key("enabled").MustBool(false)
-	LdapConfigFile, err = valueAsString(ldapSec, "config_file", "")
-	if err != nil {
-		return err
-	}
-	LdapAllowSignup = ldapSec.Key("allow_sign_up").MustBool(true)
-
 	alerting := iniFile.Section("alerting")
 	AlertingEnabled = alerting.Key("enabled").MustBool(true)
 	ExecuteAlerts = alerting.Key("execute_alerts").MustBool(true)
@@ -917,6 +910,7 @@ func (cfg *Cfg) Load(args *CommandLineArgs) error {
 		cfg.PluginsEnableAlpha = true
 	}
 
+	cfg.readLDAPConfig()
 	cfg.readSessionConfig()
 	cfg.readSmtpSettings()
 	cfg.readQuotaSettings()
@@ -979,6 +973,14 @@ func valueAsString(section *ini.Section, keyName string, defaultValue string) (v
 type RemoteCacheOptions struct {
 	Name    string
 	ConnStr string
+}
+
+func (cfg *Cfg) readLDAPConfig() {
+	ldapSec := cfg.Raw.Section("auth.ldap")
+	LdapEnabled = ldapSec.Key("enabled").MustBool(false)
+	LdapConfigFile = ldapSec.Key("config_file").String()
+	LdapAllowSignup = ldapSec.Key("allow_sign_up").MustBool(true)
+	LdapSyncCron = ldapSec.Key("sync_cron").String()
 }
 
 func (cfg *Cfg) readSessionConfig() {
