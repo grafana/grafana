20db229fce8277ee06f6bd07f4e86f476bdadb71
Brian Gann <bkgann@gmail.com>
2019-04-26 01:06:35 -0500

    convert to functions and add args for different build types

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
-
 #
 #   This script is executed from within the container.
 #
-
 set -e
 
-BUILD_FAST=0
-if [ "$1" != "" ]; then
-  if [ "$1" = "--fast" ]; then
-    BUILD_FAST=1
-    echo "Fast build enabled"
-    shift
-  fi
-else
-  echo "Fast build disabled"
-fi
-
-EXTRA_OPTS="$@"
-
+##########
 CCARMV6=/opt/rpi-tools/arm-bcm2708/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc
 CCARMV7=arm-linux-gnueabihf-gcc
 CCARM64=aarch64-linux-gnu-gcc
 CCX64=/tmp/x86_64-centos6-linux-gnu/bin/x86_64-centos6-linux-gnu-gcc
-
+##########
 GOPATH=/go
 REPO_PATH=$GOPATH/src/github.com/grafana/grafana
+##########
+
+BUILD_FAST=0
+BUILD_BACKEND=1
+BUILD_FRONTEND=1
+
+while [ "$1" != "" ]; do
+  case "$1" in
+    "--fast")
+      BUILD_FAST=1
+      echo "Fast build enabled"
+      shift
+      ;;
+    "--backend-only")
+      BUILD_FRONTEND=0
+      echo "Building only backend"
+      shift
+      ;;
+    "--frontend-only")
+      BUILD_BACKEND=0
+      echo "Building only frontend"
+      shift
+      ;;
+    * )
+      # unknown param causes args to be passed through to $@
+      break
+      ;;
+  esac
+done
+
+EXTRA_OPTS="$@"
+
 
 cd /go/src/github.com/grafana/grafana
 echo "current dir: $(pwd)"
@@ -42,34 +60,56 @@ fi
 echo "Build arguments: $OPT"
 echo "current dir: $(pwd)"
 
-if [ $BUILD_FAST = "0"]; then
+function build_backend_linux_amd64() {
+  CC=${CCX64} go run build.go ${OPT} build
+}
+
+function build_backend() {
   go run build.go -goarch armv6 -cc ${CCARMV6} ${OPT} build
   go run build.go -goarch armv7 -cc ${CCARMV7} ${OPT} build
   go run build.go -goarch arm64 -cc ${CCARM64} ${OPT} build
-fi
-CC=${CCX64} go run build.go ${OPT} build
+  build_backend_linux_amd64
+}
 
-yarn install --pure-lockfile --no-progress
+function build_frontend() {
+  yarn install --pure-lockfile --no-progress
+  echo "Building frontend"
+  go run build.go ${OPT} build-frontend
+}
 
-echo "Building frontend"
-go run build.go ${OPT} build-frontend
+function package_linux_amd64() {
+  echo "Packaging Linux AMD64"
+  go run build.go -goos linux -pkg-arch amd64 ${OPT} package-only
+}
 
-if [ -d "dist" ]; then
-  rm -rf dist
-fi
-
-mkdir dist
-go run build.go -gen-version ${OPT} > dist/grafana.version
-
-# Load ruby, needed for packing with fpm
-source /etc/profile.d/rvm.sh
-
-echo "Packaging"
-go run build.go -goos linux -pkg-arch amd64 ${OPT} package-only
-if [ $BUILD_FAST = "0"]; then
+function package_all() {
+  echo "Packaging Linux ARM"
   go run build.go -goos linux -pkg-arch armv6 ${OPT} -skipRpm package-only
   go run build.go -goos linux -pkg-arch armv7 ${OPT} package-only
   go run build.go -goos linux -pkg-arch arm64 ${OPT} package-only
+  package_linux_amd64
+}
+
+function package_setup() {
+  echo "Packaging: Setup environment"
+  if [ -d "dist" ]; then
+    rm -rf dist
+  fi
+  mkdir dist
+  go run build.go -gen-version ${OPT} > dist/grafana.version
+  # Load ruby, needed for packing with fpm
+  source /etc/profile.d/rvm.sh
+}
+
+if [ $BUILD_FAST = "0"]; then
+  build_backend
+  package_setup
+  package_all
+else
+  build_backend_linux_amd64
+  package_setup
+  package_linux_amd64
 fi
 
+# last step
 go run build.go latest
