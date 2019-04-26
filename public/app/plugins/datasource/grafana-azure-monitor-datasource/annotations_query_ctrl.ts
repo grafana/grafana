7be416f683ccca4e2480ec7f6325ce7692f155e5
Daniel Lee <dan.limerick@gmail.com>
2019-04-26 14:02:33 +0200

    fix: AzureMonitor adds back editor for annotation queries
    This must have been broken for a month or more. Now possible to
    edit annotation queries again.

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/annotations_query_ctrl.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/annotations_query_ctrl.ts
@@ -8,7 +8,7 @@ export class AzureMonitorAnnotationsQueryCtrl {
     '<your table>\n| where $__timeFilter() \n| project TimeGenerated, Text=YourTitleColumn, Tags="tag1,tag2"';
 
   /** @ngInject */
-  constructor() {
+  constructor(private templateSrv) {
     this.annotation.queryType = this.annotation.queryType || 'Azure Log Analytics';
     this.annotation.rawQuery = this.annotation.rawQuery || this.defaultQuery;
     this.getWorkspaces();
@@ -29,5 +29,21 @@ export class AzureMonitorAnnotationsQueryCtrl {
         return this.workspaces;
       })
       .catch(() => {});
+  }
+
+  getAzureLogAnalyticsSchema = () => {
+    return this.getWorkspaces()
+      .then(() => {
+        return this.datasource.azureLogAnalyticsDatasource.getSchema(this.annotation.workspace);
+      })
+      .catch(() => {});
+  };
+
+  onLogAnalyticsQueryChange = (nextQuery: string) => {
+    this.annotation.rawQuery = nextQuery;
+  };
+
+  get templateVariables() {
+    return this.templateSrv.variables.map(t => '$' + t.name);
   }
 }
