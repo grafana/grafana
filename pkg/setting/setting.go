22458caab2b51a26c4474e63fd1859aa88f89605
Andrej Ocenas <mr.ocenas@gmail.com>
2019-04-26 13:19:11 +0200

    Fix parsing of the config

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -891,8 +891,10 @@ func (cfg *Cfg) Load(args *CommandLineArgs) error {
 		return err
 	}
 
-	AlertingEvaluationTimeout = alerting.Key("evaluation_timeout_seconds").MustDuration(time.Second * 30)
-	AlertingNotificationTimeout = alerting.Key("notification_timeout_seconds").MustDuration(time.Second * 30)
+	evaluationTimeoutSeconds := alerting.Key("evaluation_timeout_seconds").MustInt64(30)
+	AlertingEvaluationTimeout = time.Duration(time.Second * time.Duration(evaluationTimeoutSeconds))
+	notificationTimeoutSeconds := alerting.Key("notification_timeout_seconds").MustInt64(30)
+	AlertingNotificationTimeout = time.Duration(time.Second * time.Duration(notificationTimeoutSeconds))
 	AlertingMaxAttempts = alerting.Key("max_attempts").MustInt(3)
 
 	explore := iniFile.Section("explore")
