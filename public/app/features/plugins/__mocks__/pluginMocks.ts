c5097e2ef36331c7f43df98d752072521a614109
Ryan McKinley <ryantxu@gmail.com>
2019-04-29 09:14:39 -0700

    Plugins: move app/feature/plugin properties into PluginMeta (#16809)

--- public/app/features/plugins/__mocks__/pluginMocks.ts
+++ public/app/features/plugins/__mocks__/pluginMocks.ts
@@ -1,7 +1,6 @@
-import { Plugin } from 'app/types';
-import { PanelPluginMeta, PluginType, PanelDataFormat } from '@grafana/ui';
+import { PanelPluginMeta, PluginMeta, PluginType, PanelDataFormat } from '@grafana/ui';
 
-export const getMockPlugins = (amount: number): Plugin[] => {
+export const getMockPlugins = (amount: number): PluginMeta[] => {
   const plugins = [];
 
   for (let i = 0; i <= amount; i++) {
@@ -87,5 +86,5 @@ export const getMockPlugin = () => {
     pinned: false,
     type: PluginType.panel,
     module: 'path/to/module',
-  } as Plugin;
+  } as PluginMeta;
 };
