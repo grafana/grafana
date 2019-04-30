331e177aa5423e17c7834016de3a9aba04c21d84
ryan <ryantxu@gmail.com>
2019-04-30 07:49:33 -0700

    fix test

--- public/app/plugins/datasource/mixed/MixedDatasource.test.ts
+++ public/app/plugins/datasource/mixed/MixedDatasource.test.ts
@@ -17,25 +17,46 @@ jest.mock('app/features/plugins/datasource_srv', () => ({
   },
 }));
 
-const dummyStream: DataStreamObserver = (event: DataStreamState) => {
-  console.log('DUMMY');
-  return true;
-};
-
 describe('MixedDatasource', () => {
   const request = getQueryOptions({
-    targets: [{ refId: 'QA', datasource: 'A' }, { refId: 'QB', datasource: 'B' }, { refId: 'QC', datasource: 'C' }],
+    targets: [
+      { refId: 'QA', datasource: 'A' }, // 1
+      { refId: 'QB', datasource: 'B' }, // 2
+      { refId: 'QC', datasource: 'C' }, // 3
+    ],
   });
 
   it('direct query should return results', async () => {
+    let counter = 0;
+    let lastEvent: DataStreamState;
+    const dummyStream: DataStreamObserver = (event: DataStreamState) => {
+      lastEvent = event;
+      counter++;
+    };
+
     const ds = await getDatasourceSrv().get('A');
     const res = await ds.query(request, dummyStream);
     expect(res.data[0]).toEqual('AAAA');
+    expect(counter).toBe(0);
+    expect(lastEvent).toBeUndefined();
   });
 
   it('mixed query is async', async () => {
+    let resolver: any;
+    const waiter = new Promise((resolve, reject) => {
+      resolver = resolve;
+    });
+    let counter = 0;
+    const dummyStream: DataStreamObserver = (event: DataStreamState) => {
+      if (++counter === request.targets.length) {
+        resolver(event);
+      }
+    };
+
     const ds = new MixedDatasource();
     const res = await ds.query(request, dummyStream);
-    expect(res.data.length).toBeTruthy();
+    expect(res.data.length).toBeFalsy();
+    await waiter;
+    expect(counter).toBe(3);
   });
 });
