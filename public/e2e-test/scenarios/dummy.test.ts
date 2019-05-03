a6f55386a552d34bc33a068226827623c29e4c20
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-05-03 12:07:07 +0200

    Build: Adds failing test

--- public/e2e-test/scenarios/dummy.test.ts
+++ public/e2e-test/scenarios/dummy.test.ts
@@ -8,3 +8,11 @@ e2eScenario('E2E dummy test', 'should have title Grafana', async browser => {
   expect(response.ok()).toBe(true);
   expect(title).toBe('Grafana');
 });
+
+e2eScenario('Failing test', 'should have title Grafanaaaaaa', async browser => {
+  const page = await browser.newPage();
+  const response = await page.goto(config.baseUrl);
+  const title = await page.title();
+  expect(response.ok()).toBe(true);
+  expect(title).toBe('Grafanaaaaaa');
+});
