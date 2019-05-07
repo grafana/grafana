9d877d670ed520cdf01322f847d9d2229a29bf9f
Andrej Ocenas <mr.ocenas@gmail.com>
2019-05-07 16:08:46 +0200

    release 6.2.0-beta1

--- scripts/build/publish.sh
+++ scripts/build/publish.sh
@@ -6,8 +6,8 @@ EXTRA_OPTS="$@"
 
 # Right now we hack this in into the publish script.
 # Eventually we might want to keep a list of all previous releases somewhere.
-_releaseNoteUrl="https://community.grafana.com/t/release-notes-v6-0-x/14010"
-_whatsNewUrl="http://docs.grafana.org/guides/whats-new-in-v6-0/"
+_releaseNoteUrl="https://community.grafana.com/t/release-notes-v6-2-x/17037"
+_whatsNewUrl="https://grafana.com/docs/guides/whats-new-in-v6-2/"
 
 ./scripts/build/release_publisher/release_publisher \
     --wn ${_whatsNewUrl} \
