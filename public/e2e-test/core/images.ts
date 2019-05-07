c211da9a4926589b6161a8f57c8d927a2a2fcca8
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-05-07 14:06:43 +0200

    Refactor: Creates output dir if missing

--- public/e2e-test/core/images.ts
+++ public/e2e-test/core/images.ts
@@ -6,6 +6,10 @@ import pixelmatch from 'pixelmatch';
 import { constants } from './constants';
 
 export const takeScreenShot = async (page: Page, fileName: string) => {
+  const outputFolderExists = fs.existsSync(constants.screenShotsOutputDir);
+  if (!outputFolderExists) {
+    fs.mkdirSync(constants.screenShotsOutputDir);
+  }
   const path = `${constants.screenShotsOutputDir}/${fileName}.png`;
   await page.screenshot({ path, type: 'png', fullPage: false });
 };
