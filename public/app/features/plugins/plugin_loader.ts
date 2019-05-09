grafana/grafana

commit d881976c9ddf88822c1a71a90a79cf919477ab6f

Refactor: move datemath to grafana/ui (#16890)
* move datemath to grafana/ui

* don't reference @grafana/ui from its own component

public/app/features/plugins/plugin_loader.ts
@@ -19,7 +19,7 @@ import TimeSeries from 'app/core/time_series2';
 import TableModel from 'app/core/table_model';
 import { coreModule, appEvents, contextSrv } from 'app/core/core';
 import { DataSourcePlugin, AppPlugin, PanelPlugin, PluginMeta, DataSourcePluginMeta } from '@grafana/ui/src/types';
-import * as datemath from 'app/core/utils/datemath';
+import * as datemath from '@grafana/ui/src/utils/datemath';
 import * as fileExport from 'app/core/utils/file_export';
 import * as flatten from 'app/core/utils/flatten';
 import * as ticks from 'app/core/utils/ticks';
@@ -105,7 +105,7 @@ exposeToPlugin('app/core/services/backend_srv', {
 });
 
 exposeToPlugin('app/plugins/sdk', sdk);
-exposeToPlugin('app/core/utils/datemath', datemath);
+exposeToPlugin('@grafana/ui/src/utils/datemath', datemath);
 exposeToPlugin('app/core/utils/file_export', fileExport);
 exposeToPlugin('app/core/utils/flatten', flatten);
 exposeToPlugin('app/core/utils/kbn', kbn);

packages/grafana-ui/src/components/TimePicker/TimePicker.tsx
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
 import moment from 'moment';
 import { TimeRange, TimeOptions, TimeOption, SelectOptionItem } from '@grafana/ui';
-import { ButtonSelect } from '@grafana/ui/src/components/Select/ButtonSelect';
+import { ButtonSelect } from '../Select/ButtonSelect';
 import { mapTimeOptionToTimeRange, mapTimeRangeToRangeString } from './time';
 import { Props as TimePickerPopoverProps } from './TimePickerPopover';
 import { TimePickerOptionGroup } from './TimePickerOptionGroup';
-import { PopperContent } from '@grafana/ui/src/components/Tooltip/PopperController';
-import { Timezone } from '../../../../../public/app/core/utils/datemath';
+import { PopperContent } from '../Tooltip/PopperController';
+import { Timezone } from '../../utils/datemath';
 
 export interface Props {
   value: TimeRange;

packages/grafana-ui/src/components/TimePicker/TimePickerCalendar.tsx
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react';
 import Calendar from 'react-calendar/dist/entry.nostyle';
 import moment, { Moment } from 'moment';
-import { TimeFragment } from '@grafana/ui';
-import { Timezone } from '../../../../../public/app/core/utils/datemath';
+import { TimeFragment } from '../../types/time';
+import { Timezone } from '../../utils/datemath';
 
 import { stringToMoment } from './time';
 

packages/grafana-ui/src/components/TimePicker/TimePickerInput.tsx
@@ -1,8 +1,9 @@
 import React, { PureComponent, ChangeEvent } from 'react';
 import moment from 'moment';
-import { TimeFragment, TIME_FORMAT, Input } from '@grafana/ui';
+import { TimeFragment, TIME_FORMAT } from '../../types/time';
 
 import { stringToMoment, isValidTimeString } from './time';
+import { Input } from '../Input/Input';
 
 export interface Props {
   value: TimeFragment;

packages/grafana-ui/src/components/TimePicker/TimePickerPopover.tsx
@@ -1,11 +1,11 @@
 import React, { Component, SyntheticEvent } from 'react';
-import { TimeRange, TimeOptions, TimeOption } from '@grafana/ui';
+import { TimeRange, TimeOptions, TimeOption } from '../../types/time';
 import { Moment } from 'moment';
 
 import { TimePickerCalendar } from './TimePickerCalendar';
 import { TimePickerInput } from './TimePickerInput';
 import { mapTimeOptionToTimeRange } from './time';
-import { Timezone } from '../../../../../public/app/core/utils/datemath';
+import { Timezone } from '../../utils/datemath';
 
 export interface Props {
   value: TimeRange;

packages/grafana-ui/src/components/TimePicker/time.ts
@@ -1,8 +1,8 @@
 import moment, { Moment } from 'moment';
 import { TimeOption, TimeRange, TIME_FORMAT } from '@grafana/ui';
 
-import * as dateMath from '../../../../../public/app/core/utils/datemath';
-import { describeTimeRange } from '../../../../../public/app/core/utils/rangeutil';
+import * as dateMath from '@grafana/ui/src/utils/datemath';
+import { describeTimeRange } from '@grafana/ui/src/utils/rangeutil';
 
 export const mapTimeOptionToTimeRange = (
   timeOption: TimeOption,

packages/grafana-ui/src/utils/datemath.test.ts
@@ -1,15 +1,15 @@
-import sinon from 'sinon';
+import sinon, { SinonFakeTimers } from 'sinon';
 
-import * as dateMath from 'app/core/utils/datemath';
-import moment from 'moment';
-import _ from 'lodash';
+import * as dateMath from './datemath';
+import moment, { Moment, unitOfTime } from 'moment';
+import each from 'lodash/each';
 
 describe('DateMath', () => {
-  const spans = ['s', 'm', 'h', 'd', 'w', 'M', 'y'];
+  const spans: unitOfTime.Base[] = ['s', 'm', 'h', 'd', 'w', 'M', 'y'];
   const anchor = '2014-01-01T06:06:06.666Z';
   const unix = moment(anchor).valueOf();
   const format = 'YYYY-MM-DDTHH:mm:ss.SSSZ';
-  let clock;
+  let clock: SinonFakeTimers;
 
   describe('errors', () => {
     it('should return undefined if passed empty string', () => {
@@ -42,7 +42,7 @@ describe('DateMath', () => {
     expected.setSeconds(0);
     expected.setMilliseconds(0);
 
-    const startOfDay = dateMath.parse('now/d', false).valueOf();
+    const startOfDay = dateMath.parse('now/d', false)!.valueOf();
     expect(startOfDay).toBe(expected.getTime());
   });
 
@@ -50,13 +50,13 @@ describe('DateMath', () => {
     const today = new Date();
     const expected = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(), 0, 0, 0, 0));
 
-    const startOfDay = dateMath.parse('now/d', false, 'utc').valueOf();
+    const startOfDay = dateMath.parse('now/d', false, 'utc')!.valueOf();
     expect(startOfDay).toBe(expected.getTime());
   });
 
   describe('subtraction', () => {
-    let now;
-    let anchored;
+    let now: Moment;
+    let anchored: Moment;
 
     beforeEach(() => {
       clock = sinon.useFakeTimers(unix);
@@ -64,16 +64,16 @@ describe('DateMath', () => {
       anchored = moment(anchor);
     });
 
-    _.each(spans, span => {
+    each(spans, span => {
       const nowEx = 'now-5' + span;
       const thenEx = anchor + '||-5' + span;
 
       it('should return 5' + span + ' ago', () => {
-        expect(dateMath.parse(nowEx).format(format)).toEqual(now.subtract(5, span).format(format));
+        expect(dateMath.parse(nowEx)!.format(format)).toEqual(now.subtract(5, span).format(format));
       });
 
       it('should return 5' + span + ' before ' + anchor, () => {
-        expect(dateMath.parse(thenEx).format(format)).toEqual(anchored.subtract(5, span).format(format));
+        expect(dateMath.parse(thenEx)!.format(format)).toEqual(anchored.subtract(5, span).format(format));
       });
     });
 
@@ -83,20 +83,20 @@ describe('DateMath', () => {
   });
 
   describe('rounding', () => {
-    let now;
+    let now: Moment;
 
     beforeEach(() => {
       clock = sinon.useFakeTimers(unix);
       now = moment();
     });
 
-    _.each(spans, span => {
+    each(spans, span => {
       it('should round now to the beginning of the ' + span, () => {
-        expect(dateMath.parse('now/' + span).format(format)).toEqual(now.startOf(span).format(format));
+        expect(dateMath.parse('now/' + span)!.format(format)).toEqual(now.startOf(span).format(format));
       });
 
       it('should round now to the end of the ' + span, () => {
-        expect(dateMath.parse('now/' + span, true).format(format)).toEqual(now.endOf(span).format(format));
+        expect(dateMath.parse('now/' + span, true)!.format(format)).toEqual(now.endOf(span).format(format));
       });
     });
 
@@ -117,12 +117,12 @@ describe('DateMath', () => {
   describe('relative time to date parsing', () => {
     it('should handle negative time', () => {
       const date = dateMath.parseDateMath('-2d', moment([2014, 1, 5]));
-      expect(date.valueOf()).toEqual(moment([2014, 1, 3]).valueOf());
+      expect(date!.valueOf()).toEqual(moment([2014, 1, 3]).valueOf());
     });
 
     it('should handle multiple math expressions', () => {
       const date = dateMath.parseDateMath('-2d-6h', moment([2014, 1, 5]));
-      expect(date.valueOf()).toEqual(moment([2014, 1, 2, 18]).valueOf());
+      expect(date!.valueOf()).toEqual(moment([2014, 1, 2, 18]).valueOf());
     });
 
     it('should return false when invalid expression', () => {

packages/grafana-ui/src/utils/datemath.ts
@@ -1,8 +1,8 @@
-// @ts-ignore
-import _ from 'lodash';
-import moment from 'moment';
+import includes from 'lodash/includes';
+import isDate from 'lodash/isDate';
+import moment, { unitOfTime } from 'moment';
 
-const units = ['y', 'M', 'w', 'd', 'h', 'm', 's'];
+const units: unitOfTime.Base[] = ['y', 'M', 'w', 'd', 'h', 'm', 's'];
 
 export type Timezone = 'utc';
 
@@ -26,7 +26,7 @@ export function parse(
     if (moment.isMoment(text)) {
       return text;
     }
-    if (_.isDate(text)) {
+    if (isDate(text)) {
       return moment(text);
     }
     // We got some non string which is not a moment nor Date. TS should be able to check for that but not always.
@@ -134,7 +134,7 @@ export function parseDateMath(mathString: string, time: any, roundUp?: boolean):
     }
     unit = mathString.charAt(i++);
 
-    if (!_.includes(units, unit)) {
+    if (!includes(units, unit)) {
       return undefined;
     } else {
       if (type === 0) {

commit 0c1530c7a89ba3321a24567a28a511cd13a6c407

plugins: fix how datemath utils are exposed to plugins (#16976)
Fixes a regression accidentally introduced by #16890 so that datemath 
utils are exposed to plugins in a backward-compatible way.

Fixes #16962

public/app/features/plugins/plugin_loader.ts
@@ -106,7 +106,7 @@ exposeToPlugin('app/core/services/backend_srv', {
 });
 
 exposeToPlugin('app/plugins/sdk', sdk);
-exposeToPlugin('@grafana/ui/src/utils/datemath', datemath);
+exposeToPlugin('app/core/utils/datemath', datemath);
 exposeToPlugin('app/core/utils/file_export', fileExport);
 exposeToPlugin('app/core/utils/flatten', flatten);
 exposeToPlugin('app/core/utils/kbn', kbn);