fc52ae434ef1f6b1f967cea4a28ab47a2030e415
David Kaltschmidt <david.kaltschmidt@gmail.com>
2019-05-10 22:08:56 +0200

    Dont wrap single regexp in new filter syntax

--- public/app/plugins/datasource/loki/query_utils.ts
+++ public/app/plugins/datasource/loki/query_utils.ts
@@ -1,7 +1,6 @@
 import { LokiExpression } from './types';
 
 const selectorRegexp = /(?:^|\s){[^{]*}/g;
-const caseInsensitive = '(?i)'; // Golang mode modifier for Loki, doesn't work in JavaScript
 export function parseQuery(input: string): LokiExpression {
   input = input || '';
   const match = input.match(selectorRegexp);
@@ -12,9 +11,6 @@ export function parseQuery(input: string): LokiExpression {
     // Selector
     query = match[0].trim();
     regexp = input.replace(selectorRegexp, '').trim();
-    if (regexp && regexp.search(/\|=|\|~|!=|!~/) === -1) {
-      regexp = `|~ "${caseInsensitive}${regexp}"`;
-    }
   }
 
   return { regexp, query };
