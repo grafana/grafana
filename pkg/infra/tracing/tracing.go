f4acafd63da08e2a96585eac412cf17a0cfa82c5
Jonathan Rockway <jrockway@users.noreply.github.com>
2019-05-13 14:25:03 -0400

    tracing: jaeger: avoid conditional when parsing the shared_zipkin_spans field we will ignore it when actually creating the tracer if it's not relevant.

--- pkg/infra/tracing/tracing.go
+++ pkg/infra/tracing/tracing.go
@@ -59,9 +59,7 @@ func (ts *TracingService) parseSettings() {
 	ts.samplerType = section.Key("sampler_type").MustString("")
 	ts.samplerParam = section.Key("sampler_param").MustFloat64(1)
 	ts.useZipkinPropagation = section.Key("zipkin_propagation").MustBool(false)
-	if ts.useZipkinPropagation {
-		ts.useSharedZipkinSpans = section.Key("shared_zipkin_spans").MustBool(true)
-	}
+	ts.useSharedZipkinSpans = section.Key("shared_zipkin_spans").MustBool(true)
 }
 
 func (ts *TracingService) initGlobalTracer() error {
