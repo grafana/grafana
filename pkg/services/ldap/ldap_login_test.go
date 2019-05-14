c6dd990d83736de0777d7217556edb79660c0139
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-05-14 14:57:31 +0300

    Tests for disabling user during the LDAP login

--- pkg/services/ldap/ldap_login_test.go
+++ pkg/services/ldap/ldap_login_test.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/ldap.v3"
 
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/models"
 )
 
 func TestLdapLogin(t *testing.T) {
@@ -80,6 +81,99 @@ func TestLdapLogin(t *testing.T) {
 
 			Convey("it should get user", func() {
 				So(scenario.loginUserQuery.User.Login, ShouldEqual, "markelog")
+			})
+		})
+
+		AuthScenario("When user not found in LDAP, but exist in Grafana", func(scenario *scenarioContext) {
+			conn := &mockLdapConn{}
+			result := ldap.SearchResult{Entries: []*ldap.Entry{}}
+			conn.setSearchResult(&result)
+
+			externalUser := &models.ExternalUserInfo{UserId: 42, IsDisabled: false}
+			scenario.getExternalUserInfoByLoginQueryReturns(externalUser)
+
+			conn.bindProvider = func(username, password string) error {
+				return nil
+			}
+			auth := &Auth{
+				server: &ServerConfig{
+					SearchBaseDNs: []string{"BaseDNHere"},
+				},
+				conn: conn,
+				log:  log.New("test-logger"),
+			}
+
+			err := auth.Login(scenario.loginUserQuery)
+
+			Convey("it should disable user", func() {
+				So(scenario.disableExternalUserCalled, ShouldBeTrue)
+				So(scenario.disableUserCmd.IsDisabled, ShouldBeTrue)
+				So(scenario.disableUserCmd.UserId, ShouldEqual, 42)
+			})
+
+			Convey("it should return invalid credentials error", func() {
+				So(err, ShouldEqual, ErrInvalidCredentials)
+			})
+		})
+
+		AuthScenario("When user not found in LDAP, and disabled in Grafana already", func(scenario *scenarioContext) {
+			conn := &mockLdapConn{}
+			result := ldap.SearchResult{Entries: []*ldap.Entry{}}
+			conn.setSearchResult(&result)
+
+			externalUser := &models.ExternalUserInfo{UserId: 42, IsDisabled: true}
+			scenario.getExternalUserInfoByLoginQueryReturns(externalUser)
+
+			conn.bindProvider = func(username, password string) error {
+				return nil
+			}
+			auth := &Auth{
+				server: &ServerConfig{
+					SearchBaseDNs: []string{"BaseDNHere"},
+				},
+				conn: conn,
+				log:  log.New("test-logger"),
+			}
+
+			err := auth.Login(scenario.loginUserQuery)
+
+			Convey("it should't call disable function", func() {
+				So(scenario.disableExternalUserCalled, ShouldBeFalse)
+			})
+
+			Convey("it should return invalid credentials error", func() {
+				So(err, ShouldEqual, ErrInvalidCredentials)
+			})
+		})
+
+		AuthScenario("When user found in LDAP, and disabled in Grafana", func(scenario *scenarioContext) {
+			conn := &mockLdapConn{}
+			entry := ldap.Entry{}
+			result := ldap.SearchResult{Entries: []*ldap.Entry{&entry}}
+			conn.setSearchResult(&result)
+			scenario.userQueryReturns(&models.User{Id: 42, IsDisabled: true})
+
+			conn.bindProvider = func(username, password string) error {
+				return nil
+			}
+			auth := &Auth{
+				server: &ServerConfig{
+					SearchBaseDNs: []string{"BaseDNHere"},
+				},
+				conn: conn,
+				log:  log.New("test-logger"),
+			}
+
+			err := auth.Login(scenario.loginUserQuery)
+
+			Convey("it should re-enable user", func() {
+				So(scenario.disableExternalUserCalled, ShouldBeTrue)
+				So(scenario.disableUserCmd.IsDisabled, ShouldBeFalse)
+				So(scenario.disableUserCmd.UserId, ShouldEqual, 42)
+			})
+
+			Convey("it should not return error", func() {
+				So(err, ShouldBeNil)
 			})
 		})
 	})
--- pkg/services/ldap/test.go
+++ pkg/services/ldap/test.go
@@ -100,6 +100,18 @@ func AuthScenario(desc string, fn scenarioFunc) {
 			return nil
 		})
 
+		bus.AddHandler("test", func(cmd *models.GetExternalUserInfoByLoginQuery) error {
+			sc.getExternalUserInfoByLoginQuery = cmd
+			sc.getExternalUserInfoByLoginQuery.Result = &models.ExternalUserInfo{UserId: 42, IsDisabled: false}
+			return nil
+		})
+
+		bus.AddHandler("test", func(cmd *models.DisableUserCommand) error {
+			sc.disableExternalUserCalled = true
+			sc.disableUserCmd = cmd
+			return nil
+		})
+
 		bus.AddHandler("test", func(cmd *models.AddOrgUserCommand) error {
 			sc.addOrgUserCmd = cmd
 			return nil
@@ -130,16 +142,19 @@ func AuthScenario(desc string, fn scenarioFunc) {
 }
 
 type scenarioContext struct {
-	loginUserQuery           *models.LoginUserQuery
-	getUserByAuthInfoQuery   *models.GetUserByAuthInfoQuery
-	getUserOrgListQuery      *models.GetUserOrgListQuery
-	createUserCmd            *models.CreateUserCommand
-	addOrgUserCmd            *models.AddOrgUserCommand
-	updateOrgUserCmd         *models.UpdateOrgUserCommand
-	removeOrgUserCmd         *models.RemoveOrgUserCommand
-	updateUserCmd            *models.UpdateUserCommand
-	setUsingOrgCmd           *models.SetUsingOrgCommand
-	updateUserPermissionsCmd *models.UpdateUserPermissionsCommand
+	loginUserQuery                  *models.LoginUserQuery
+	getUserByAuthInfoQuery          *models.GetUserByAuthInfoQuery
+	getExternalUserInfoByLoginQuery *models.GetExternalUserInfoByLoginQuery
+	getUserOrgListQuery             *models.GetUserOrgListQuery
+	createUserCmd                   *models.CreateUserCommand
+	disableUserCmd                  *models.DisableUserCommand
+	addOrgUserCmd                   *models.AddOrgUserCommand
+	updateOrgUserCmd                *models.UpdateOrgUserCommand
+	removeOrgUserCmd                *models.RemoveOrgUserCommand
+	updateUserCmd                   *models.UpdateUserCommand
+	setUsingOrgCmd                  *models.SetUsingOrgCommand
+	updateUserPermissionsCmd        *models.UpdateUserPermissionsCommand
+	disableExternalUserCalled       bool
 }
 
 func (sc *scenarioContext) userQueryReturns(user *models.User) {
@@ -158,6 +173,17 @@ func (sc *scenarioContext) userQueryReturns(user *models.User) {
 func (sc *scenarioContext) userOrgsQueryReturns(orgs []*models.UserOrgDTO) {
 	bus.AddHandler("test", func(query *models.GetUserOrgListQuery) error {
 		query.Result = orgs
+		return nil
+	})
+}
+
+func (sc *scenarioContext) getExternalUserInfoByLoginQueryReturns(externalUser *models.ExternalUserInfo) {
+	bus.AddHandler("test", func(cmd *models.GetExternalUserInfoByLoginQuery) error {
+		sc.getExternalUserInfoByLoginQuery = cmd
+		sc.getExternalUserInfoByLoginQuery.Result = &models.ExternalUserInfo{
+			UserId:     externalUser.UserId,
+			IsDisabled: externalUser.IsDisabled,
+		}
 		return nil
 	})
 }
