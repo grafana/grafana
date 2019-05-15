71441b1d8dcecd79fada52c09de471a93909d0b2
Torkel Ödegaard <torkel@grafana.com>
2019-05-15 13:42:46 +0200

    Minor tweak to formatting

--- scripts/cli/tasks/cherrypick.ts
+++ scripts/cli/tasks/cherrypick.ts
@@ -23,7 +23,9 @@ const cherryPickRunner: TaskRunner<CherryPickOptions> = async () => {
 
   let commands = '';
 
+  console.log('--------------------------------------------------------------------');
   console.log('Printing PRs with cherry-pick-needed, in ASC merge date order');
+  console.log('--------------------------------------------------------------------');
 
   for (const item of res.data) {
     if (!item.milestone) {
@@ -32,15 +34,13 @@ const cherryPickRunner: TaskRunner<CherryPickOptions> = async () => {
     }
 
     const issueDetails = await client.get(item.pull_request.url);
-    console.log(
-      `* ${item.title}, (#${item.number}), closed_at ${item.closed_at}, merge-sha: ${
-        issueDetails.data.merge_commit_sha
-      }`
-    );
+    console.log(`* ${item.title}, (#${item.number}), merge-sha: ${issueDetails.data.merge_commit_sha}`);
     commands += `git cherry-pick -x ${issueDetails.data.merge_commit_sha}\n`;
   }
 
+  console.log('--------------------------------------------------------------------');
   console.log('Commands (in order of how they should be executed)');
+  console.log('--------------------------------------------------------------------');
   console.log(commands);
 };
 
