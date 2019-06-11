e82dab672ed2b6f354cd0986ec211958a192ea72
Kyle Brandt <kyle@grafana.com>
2019-06-11 09:33:35 -0400

    remove stray typo

--- pkg/services/sqlstore/user.go
+++ pkg/services/sqlstore/user.go
@@ -533,7 +533,7 @@ func deleteUserInTransaction(sess *DBSession, cmd *m.DeleteUserCommand) error {
 	}
 
 	for _, sql := range deletes {
-		_, err := sess.Exec(sql, cmgitd.UserId)
+		_, err := sess.Exec(sql, cmd.UserId)
 		if err != nil {
 			return err
 		}
