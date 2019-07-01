163cf60e81b35110d5e4f1c055f65d2c6b57a210
Shavonn Brown <shavonn.brown@grafana.com>
2019-07-01 12:36:31 -0400

    should be OR

--- public/app/features/profile/ChangePasswordForm.tsx
+++ public/app/features/profile/ChangePasswordForm.tsx
@@ -47,41 +47,41 @@ export class ChangePasswordForm extends PureComponent<Props, State> {
       return <LoadingPlaceholder text="Saving..." />;
     }
 
-    if (ldapEnabled && authProxyEnabled) {
+    if (ldapEnabled || authProxyEnabled) {
       return <p>You cannot change password when ldap or auth proxy authentication is enabled.</p>;
-    } else {
-      return (
-        <form name="userForm" className="gf-form-group">
-          <div className="gf-form max-width-30">
-            <PasswordInput label="Old Password" onChange={this.onOldPasswordChange.bind(this)} value={oldPassword} />
-          </div>
-          <div className="gf-form max-width-30">
-            <PasswordInput label="New Password" onChange={this.onNewPasswordChange.bind(this)} value={newPassword} />
-          </div>
-          <div className="gf-form max-width-30">
-            <PasswordInput
-              label="Confirm Password"
-              onChange={this.onConfirmPasswordChange.bind(this)}
-              value={confirmNew}
-            />
-          </div>
-          <div className="gf-form-button-row">
-            <Button
-              variant="primary"
-              onClick={(event: MouseEvent<HTMLInputElement>) => {
-                event.preventDefault();
-                onChangePassword({ oldPassword, newPassword, confirmNew });
-              }}
-            >
-              Change Password
-            </Button>
-            <LinkButton variant="transparent" href={`${config.appSubUrl}/profile`}>
-              Cancel
-            </LinkButton>
-          </div>
-        </form>
-      );
     }
+
+    return (
+      <form name="userForm" className="gf-form-group">
+        <div className="gf-form max-width-30">
+          <PasswordInput label="Old Password" onChange={this.onOldPasswordChange.bind(this)} value={oldPassword} />
+        </div>
+        <div className="gf-form max-width-30">
+          <PasswordInput label="New Password" onChange={this.onNewPasswordChange.bind(this)} value={newPassword} />
+        </div>
+        <div className="gf-form max-width-30">
+          <PasswordInput
+            label="Confirm Password"
+            onChange={this.onConfirmPasswordChange.bind(this)}
+            value={confirmNew}
+          />
+        </div>
+        <div className="gf-form-button-row">
+          <Button
+            variant="primary"
+            onClick={(event: MouseEvent<HTMLInputElement>) => {
+              event.preventDefault();
+              onChangePassword({ oldPassword, newPassword, confirmNew });
+            }}
+          >
+            Change Password
+          </Button>
+          <LinkButton variant="transparent" href={`${config.appSubUrl}/profile`}>
+            Cancel
+          </LinkButton>
+        </div>
+      </form>
+    );
   }
 }
 
