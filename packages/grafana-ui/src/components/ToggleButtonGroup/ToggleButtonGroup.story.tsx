b2760aa1ae2f5e87f046123ee59175f59beb5054
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-07-02 16:46:33 +0200

    Add some functionality

--- packages/grafana-ui/src/components/ToggleButtonGroup/ToggleButtonGroup.story.tsx
+++ packages/grafana-ui/src/components/ToggleButtonGroup/ToggleButtonGroup.story.tsx
@@ -5,9 +5,12 @@ import { storiesOf } from '@storybook/react';
 storiesOf('UI/ToggleButtonGroup', module).add('basic toggle button group', () => {
   return (
     <ToggleButtonGroup>
-      <ToggleButton value={true}>First option</ToggleButton>
-      <ToggleButton value={false}>Second option</ToggleButton>
-      <ToggleButton value={false}>Second option</ToggleButton>
+      <ToggleButton selected={true}>First option</ToggleButton>
+      <ToggleButton selected={false}>Second option</ToggleButton>
+      <ToggleButton selected={true} tooltip="This is important">
+        Third option
+      </ToggleButton>
+      <ToggleButton selected={false}>Fourth option</ToggleButton>
     </ToggleButtonGroup>
   );
 });
--- packages/grafana-ui/src/components/ToggleButtonGroup/ToggleButtonGroup.tsx
+++ packages/grafana-ui/src/components/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, PureComponent, useContext, FC } from 'react';
+import React, { ReactNode, PureComponent, useContext, FC, useState } from 'react';
 import { Tooltip } from '../Tooltip/Tooltip';
 import { ThemeContext } from '../../themes/index';
 import { css, cx } from 'emotion';
@@ -45,49 +45,66 @@ export class ToggleButtonGroup extends PureComponent<ToggleButtonGroupProps, Tog
   }
 }
 
-interface ToggleButtonProps {
-  onChange?: (value: any) => void;
-  selected?: boolean;
-  value: any;
-  className?: string;
-  children: ReactNode;
-  tooltip?: string;
-  key?: any;
-}
-
 const getStyles = (theme: GrafanaTheme) => {
   const borderRadius = 3;
 
   return {
     button: css`
-      border: solid 2px #3274d9;
+      border: solid 1px #c7d0d9;
       color: ${theme.colors.text};
       padding: 0 16px;
       background-color: ${theme.background.toggleButton.default};
       border-radius: ${borderRadius}px;
       height: 36px;
       font-size: 14px;
-      font-weight: regular;
+      font-weight: normal;
+      /*Make borders overlap*/
+      margin-left: -1px;
+
+      /*Position to enable z-index*/
+      position: relative;
+      z-index: 899;
       &:hover {
         color: ${theme.colors.activeColor};
+        font-weight: 500;
+      }
+      &:focus {
+        outline: none;
+        z-index: 900;
+        font-weight: 500;
+        color: ${theme.colors.activeColor};
+        border-color: ${theme.colors.blueBase};
       }
     `,
     active: css`
-      font-weight: medium;
+      font-weight: 500;
+      background-color: ${theme.background.toggleButton.active};
+      border-color: ${theme.colors.blueBase};
+      /*To priorotize blue overlapping border*/
+      z-index: 900;
     `,
     middle: css`
       border-radius: 0;
-      border-left: none;
     `,
     first: css`
       border-radius: ${borderRadius}px 0 0 ${borderRadius}px;
+      margin-left: 0;
     `,
     last: css`
       border-radius: 0 ${borderRadius}px ${borderRadius}px 0;
-      border-left: none;
     `,
   };
 };
+
+interface ToggleButtonProps {
+  onChange?: (value: any) => void;
+  selected: boolean;
+  value?: any;
+  className?: string;
+  children: ReactNode;
+  tooltip?: string;
+  key?: any;
+}
 
 export const ToggleButton: FC<ToggleButtonProps> = ({
   children,
@@ -97,23 +114,21 @@ export const ToggleButton: FC<ToggleButtonProps> = ({
   tooltip,
   onChange,
 }) => {
-  // const activeStyles: any = {
-  //   fontWeight: 'medium',
-  // };
-
   const theme = useContext(ThemeContext);
+  const styles = getStyles(theme);
+  const [select, setSelect] = useState(selected);
+  const activeStyle = select ? styles.active : null;
 
   const onClick = (event: React.SyntheticEvent) => {
     event.stopPropagation();
-    if (!selected && onChange) {
+    setSelect(!select);
+    if (onChange) {
       onChange(value);
     }
   };
 
-  // const btnClassName = `btn ${className} ${selected ? 'active' : ''}`;
-
   const button = (
-    <button className={cx([getStyles(theme).button, className])} onClick={onClick}>
+    <button className={cx([styles.button, activeStyle, className])} onClick={onClick}>
       <span>{children}</span>
     </button>
   );
