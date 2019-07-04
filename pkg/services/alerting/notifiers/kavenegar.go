10a4cd45cd3b8caa1b7892ed53a1da5c80fd5c77
Hamed Momeni <hamed.ma7@gmail.com>
2019-07-04 20:44:59 +0430

    Notifiers: finished Kavenegar notificatin channel and added tests
    added message template with replaceable placeholders

--- pkg/services/alerting/notifiers/kavenegar.go
+++ pkg/services/alerting/notifiers/kavenegar.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	apiUrl = "https://api.kavenegar.com/v1/%s/sms/send.json "
+	apiURL = "https://api.kavenegar.com/v1/%s/sms/send.json"
 )
 
 func init() {
@@ -26,20 +26,25 @@ func init() {
 			<h3 class="page-heading">Kavenegar notifications settings</h3>
 			<div class="gf-form">
 				<span class="gf-form-label width-14">API Key</span>
-				<input type="text" required class="gf-form-input max-width-22" ng-model="ctrl.model.settings.apikey" placeholder="Kavenegar API key"></input>
+				<input type="text" required class="gf-form-input max-width-26" ng-model="ctrl.model.settings.apikey" placeholder="Kavenegar API key"></input>
 			</div>
 			<div class="gf-form">
-				<span class="gf-form-label width-14">Sender</span>
-				<input type="text" required class="gf-form-input max-width-22" ng-model="ctrl.model.settings.sender" placeholder="Sender number"></input>
+				<span class="gf-form-label width-14">Sender number</span>
+				<input type="text" class="gf-form-input max-width-26" ng-model="ctrl.model.settings.sender" placeholder="Optional"></input>
 			</div>
 			<div class="gf-form">
-				<textarea rows="7" class="gf-form-input width-27" required ng-model="ctrl.model.settings.recipients"></textarea>
+				<span class="gf-form-label width-14">Recipients</span>
+				<input type="text" required class="gf-form-input max-width-40" ng-model="ctrl.model.settings.recipients" placeholder="0912...;0935..."></input>
 			</div>
 			<div class="gf-form">
 				<span>You can enter multiple phones using a ";" separator</span>
 			</div>
 			<div class="gf-form">
+				<span class="gf-form-label width-6">Content</span>
 				<textarea rows="7" class="gf-form-input width-27" required ng-model="ctrl.model.settings.template"></textarea>
+			</div>
+			<div class="gf-form">
+				<span>You can use the following placeholders: {RULE_NAME}, {RULE_MESSAGE}, {RULE_STATE}</span>
 			</div>
 		</div>
     `,
@@ -47,8 +52,8 @@ func init() {
 
 }
 
-// NewWebHookNotifier is the constructor for
-// the WebHook notifier.
+// NewKavenegarNotifier is the constructor for
+// the Kavenegar notifier.
 func NewKavenegarNotifier(model *models.AlertNotification) (alerting.Notifier, error) {
 	apiKey := model.Settings.Get("apikey").MustString()
 	if apiKey == "" {
@@ -65,37 +70,42 @@ func NewKavenegarNotifier(model *models.AlertNotification) (alerting.Notifier, e
 		return nil, alerting.ValidationError{Reason: "Could not find recipients property in settings"}
 	}
 
-	sender := model.Settings.Get("sender").MustString()
-	if sender == "" {
-		return nil, alerting.ValidationError{Reason: "Could not find sender property in settings"}
-	}
+	sender, _ := model.Settings.Get("sender").String()
 
 	return &KavenegarNotifier{
 		NotifierBase: NewNotifierBase(model),
-		ApiKey:       apiKey,
+		APIKey:       apiKey,
 		Body:         template,
-		Recipients:   strings.Split(";", recips),
+		Recipients:   strings.Split(recips, ";"),
 		Sender:       sender,
 		log:          log.New("alerting.notifier.kavenegar"),
 	}, nil
 }
 
+// KavenegarNotifier is the struct to hold the data for Kavenegar
 type KavenegarNotifier struct {
 	NotifierBase
-	ApiKey     string
+	APIKey     string
 	Recipients []string
 	Body       string
 	Sender     string
 	log        log.Logger
 }
 
+// Notify is the method to dispatch the Kavenegar API call
 func (sn *KavenegarNotifier) Notify(evalContext *alerting.EvalContext) error {
-	sn.log.Info("Initiating Kavenegar notification")
+	sn.log.Info("Initiating Kavenegar notification for")
 
 	form := url.Values{}
 
 	form.Add("receptor", strings.Join(sn.Recipients, ","))
-	form.Add("message", sn.Body)
+
+	template := sn.Body
+	template = strings.Replace(template, "{RULE_NAME}", evalContext.Rule.Name, 1)
+	template = strings.Replace(template, "{RULE_MESSAGE}", evalContext.Rule.Message, 1)
+	template = strings.Replace(template, "{RULE_STATE}", string(evalContext.Rule.State), 1)
+
+	form.Add("message", template)
 
 	if sn.Sender != "" {
 		form.Add("sender", sn.Sender)
@@ -104,9 +114,9 @@ func (sn *KavenegarNotifier) Notify(evalContext *alerting.EvalContext) error {
 	formBody := form.Encode()
 
 	cmd := &models.SendWebhookSync{
-		Url:        fmt.Sprintf(apiUrl, sn.ApiKey),
-		HttpMethod: "POST",
-		Body:       formBody,
+		Url:        fmt.Sprintf("%s?%s", fmt.Sprintf(apiURL, sn.APIKey), formBody),
+		HttpMethod: "GET",
+		Body:       "",
 	}
 
 	if err := bus.DispatchCtx(evalContext.Ctx, cmd); err != nil {
