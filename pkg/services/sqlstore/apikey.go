a4a674ea83a9288701611f203f2a75531fb8a131
Anthony Templeton <twantempleton11@gmail.com>
2019-07-08 15:29:55 -0400

    17447: make changes requested during review
    - use dialect.IsUniqueContraintViolation
    - change if statement to match others
    - return error properly

--- pkg/services/sqlstore/apikey.go
+++ pkg/services/sqlstore/apikey.go
@@ -56,6 +56,9 @@ func AddApiKey(cmd *models.AddApiKeyCommand) error {
 		}
 
 		if _, err := sess.Insert(&t); err != nil {
+			if dialect.IsUniqueConstraintViolation((err)) {
+				return models.ErrDuplicateApiKey
+			}
 			return err
 		}
 		cmd.Result = &t
