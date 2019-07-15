ab109eb0653e0e12aec8e20102c515024c71254f
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-07-15 10:50:29 +0200

    Remove media query and extarct LoginServices

--- public/app/core/components/Login/LoginForm.tsx
+++ public/app/core/components/Login/LoginForm.tsx
@@ -1,63 +1,9 @@
 import React, { PureComponent } from 'react';
 import config from 'app/core/config';
 import { UserSignup } from './UserSignup';
-
-const loginServices: LoginServices = {
-  saml: {
-    enabled: config.samlEnabled,
-    name: 'SAML',
-    className: 'github',
-    icon: 'key',
-  },
-  google: {
-    enabled: config.oauth.google,
-    name: 'Google',
-  },
-  github: {
-    enabled: config.oauth.github,
-    name: 'GitHub',
-  },
-  gitlab: {
-    enabled: config.oauth.gitlab,
-    name: 'GitLab',
-  },
-  grafanacom: {
-    enabled: config.oauth.grafana_com,
-    name: 'Grafana.com',
-    hrefName: 'grafana_com',
-    icon: 'grafana_com',
-  },
-  oauth: {
-    enabled: config.oauth.generic_oauth,
-    name: 'OAuth',
-    icon: 'sign-in',
-    hrefName: 'generic_oauth',
-  },
-};
+import { LoginServiceButtons } from './LoginServiceButtons';
 
 const oauthEnabled = Object.keys(config.oauth).length > 0;
-
-export interface LoginService {
-  enabled: boolean;
-  name: string;
-  hrefName?: string;
-  icon?: string;
-  className?: string;
-}
-
-export interface LoginServices {
-  [key: string]: LoginService;
-}
-
-export interface EnabledLoginServices {
-  saml: boolean;
-  google: boolean;
-  github: boolean;
-  gitlab: boolean;
-  grafanacom: boolean;
-  oauth: boolean;
-}
-
 export interface Props {
   disabledLoginForm: boolean;
   passwordHint: string;
@@ -66,7 +12,6 @@ export interface Props {
   oauthEnabled: boolean;
   ldapEnabled: boolean;
   authProxyEnabled: boolean;
-  loginServices: EnabledLoginServices;
   onSubmit: Function;
 }
 
@@ -146,24 +91,6 @@ export class LoginForm extends PureComponent<Props, State> {
     ) : null;
   }
 
-  renderLoginServices(loginServices: LoginServices) {
-    const keyNames = Object.keys(loginServices);
-    return keyNames.map((key, i: number) => {
-      const service: LoginService = loginServices[key];
-      return service.enabled ? (
-        <a
-          key={key}
-          className={`btn btn-medium btn-service btn-service--${service.className || key} login-btn`}
-          href={`login/${service.hrefName ? service.hrefName : key}`}
-          target="_self"
-        >
-          <i className={`btn-service-icon fa fa-${service.icon ? service.icon : key}`} />
-          Sign in with {service.name}
-        </a>
-      ) : null;
-    });
-  }
-
   render() {
     return (
       <div className="login-out-box">
@@ -183,7 +110,8 @@ export class LoginForm extends PureComponent<Props, State> {
                 </div>
               </div>
               <div className="clearfix" />
-              <div className="login-oauth text-center">{this.renderLoginServices(loginServices)}</div>
+
+              <LoginServiceButtons />
             </>
           ) : null}
 
