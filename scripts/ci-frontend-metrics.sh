fd7c38c62f40a369f626c726bdca42111baf84af
Oleg Gaidarenko <markelog@gmail.com>
2019-07-23 13:12:33 +0300

    Build: Introduce shellcheck (#18081)
    * Build: introduce shellcheck
    
    Fixes #16198
    
    (cherry picked from commit 4b16cd6cc8e8cdc66245ae32119076819411f585)

--- scripts/ci-frontend-metrics.sh
+++ scripts/ci-frontend-metrics.sh
@@ -11,19 +11,19 @@ ERROR_COUNT="$(./node_modules/.bin/tsc --project tsconfig.json --noEmit --noImpl
 DIRECTIVES="$(grep -r -o  directive public/app/**/*  | wc -l)"
 CONTROLLERS="$(grep -r -oP 'class .*Ctrl' public/app/**/*  | wc -l)"
 
-if [ $ERROR_COUNT -gt $ERROR_COUNT_LIMIT ]; then
+if [ "$ERROR_COUNT" -gt $ERROR_COUNT_LIMIT ]; then
   echo -e "Typescript errors $ERROR_COUNT exceeded $ERROR_COUNT_LIMIT so failing build"
-	exit -1
+	exit 1
 fi
 
-if [ $DIRECTIVES -gt $DIRECTIVES_LIMIT ]; then
+if [ "$DIRECTIVES" -gt $DIRECTIVES_LIMIT ]; then
   echo -e "Directive count $DIRECTIVES exceeded $DIRECTIVES_LIMIT so failing build"
-	exit -1
+	exit 1
 fi
 
-if [ $CONTROLLERS -gt $CONTROLLERS_LIMIT ]; then
+if [ "$CONTROLLERS" -gt $CONTROLLERS_LIMIT ]; then
   echo -e "Controllers count $CONTROLLERS exceeded $CONTROLLERS_LIMIT so failing build"
-	exit -1
+	exit 1
 fi
 
 echo -e "Typescript errors: $ERROR_COUNT"
@@ -32,7 +32,7 @@ echo -e "Controllers: $CONTROLLERS"
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
   ./scripts/ci-metrics-publisher.sh \
-    grafana.ci-code.noImplicitAny=$ERROR_COUNT \
-    grafana.ci-code.directives=$DIRECTIVES \
-    grafana.ci-code.controllers=$CONTROLLERS
+    grafana.ci-code.noImplicitAny="$ERROR_COUNT" \
+    grafana.ci-code.directives="$DIRECTIVES" \
+    grafana.ci-code.controllers="$CONTROLLERS"
 fi
