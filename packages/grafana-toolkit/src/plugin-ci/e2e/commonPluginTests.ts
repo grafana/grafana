05538b43f98eaf763fbae12c59f4ea2173bd3b4b
ryan <ryantxu@gmail.com>
2019-07-23 17:18:47 -0700

    add sleep command

--- packages/grafana-toolkit/src/plugin-ci/e2e/commonPluginTests.ts
+++ packages/grafana-toolkit/src/plugin-ci/e2e/commonPluginTests.ts
@@ -2,11 +2,17 @@ import { Browser, Page } from 'puppeteer-core';
 
 import { e2eScenario, takeScreenShot, pluginCi, pages } from '@grafana/toolkit';
 
+const sleep = (milliseconds: number) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
 e2eScenario('Common Plugin Test', 'should pass', async (browser: Browser, page: Page) => {
   const settings = pluginCi.getEndToEndSettings();
   const pluginPage = pages.getPluginPage(settings.plugin.id);
   await pluginPage.init(page);
   await pluginPage.navigateTo();
+  // TODO: find a better way to avoid the 'loading' page
+  await sleep(500);
 
   const fileName = 'plugin-page';
   await takeScreenShot(page, fileName);
