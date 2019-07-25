b0d321a297a423f229e0b2dee92101fce23902aa
ryan <ryantxu@gmail.com>
2019-07-25 16:43:18 -0700

    adding missing files

---
+++ packages/grafana-toolkit/src/plugins/types.ts
@@ -0,0 +1,110 @@
+import { PluginMeta, PluginBuildInfo } from '@grafana/ui';
+import { DataFrame } from '@grafana/data';
+
+export interface PluginPackageDetails {
+  plugin: ZipFileInfo;
+  docs?: ZipFileInfo;
+}
+
+export interface PluginBuildReport {
+  plugin: PluginMeta;
+  packages: PluginPackageDetails;
+  workflow: WorkflowInfo;
+  coverage: CoverageInfo[];
+  tests: TestResultsInfo[];
+  pullRequest?: number;
+  artifactsBaseURL?: string;
+}
+
+export interface JobInfo {
+  job?: string;
+  startTime: number;
+  endTime: number;
+  elapsed: number;
+  status?: string;
+  buildNumber?: number;
+}
+
+export interface WorkflowInfo extends JobInfo {
+  workflowId?: string;
+  jobs: JobInfo[];
+  user?: string;
+  repo?: string;
+}
+
+export interface CoverageDetails {
+  total: number;
+  covered: number;
+  skipped: number;
+  pct: number;
+}
+
+export interface CoverageInfo {
+  job: string;
+  summary: { [key: string]: CoverageDetails };
+  report?: string; // path to report
+}
+
+export interface TestResultsInfo {
+  job: string;
+  grafana?: any;
+  error?: string;
+  passed: number;
+  failed: number;
+  screenshots: string[];
+}
+
+// Saved at the folder level
+export interface PluginHistory {
+  last: {
+    info: PluginDevInfo;
+    report: PluginBuildReport;
+  };
+  size: DataFrame[]; // New frame for each package
+  coverage: DataFrame[]; // New frame for each job
+  timing: DataFrame[]; // New frame for each job/workflow
+}
+
+export interface PluginDevInfo {
+  pluginId: string;
+  name: string;
+  logo?: string; // usually logo.svg or logo.png
+  build: PluginBuildInfo;
+  version: string;
+}
+
+export interface DevSummary {
+  [key: string]: PluginDevInfo;
+}
+
+export interface PluginDevSummary {
+  branch: DevSummary;
+  pr: DevSummary;
+}
+
+export const defaultPluginHistory: PluginHistory = {
+  last: {
+    info: {} as PluginDevInfo,
+    report: {} as PluginBuildReport,
+  },
+  size: [],
+  coverage: [],
+  timing: [],
+};
+
+export interface CountAndSize {
+  count: number;
+  bytes: number;
+}
+
+export interface ExtensionSize {
+  [key: string]: CountAndSize;
+}
+
+export interface ZipFileInfo {
+  name: string;
+  size: number;
+  contents: ExtensionSize;
+  sha1?: string;
+  md5?: string;
+}
