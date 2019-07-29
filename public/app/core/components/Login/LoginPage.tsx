d2b8216babf4d992a1830094e000bb584f8db655
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-07-29 16:34:31 +0200

    LoginPage to FC

--- public/app/core/components/Login/LoginPage.tsx
+++ public/app/core/components/Login/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { FC } from 'react';
 import config from 'app/core/config';
 import { UserSignup } from './UserSignup';
 import { LoginServiceButtons } from './LoginServiceButtons';
@@ -7,93 +7,59 @@ import { LoginForm } from './LoginForm';
 import { ChangePassword } from './ChangePassword';
 
 const isOauthEnabled = () => Object.keys(config.oauth).length > 0;
-export interface Props {
-  disabledLoginForm: boolean;
-  passwordHint: string;
-  loginHint: string;
-  disableUserSignup: boolean;
-  isOauthEnabled: boolean;
-  ldapEnabled: boolean;
-  authProxyEnabled: boolean;
-  onSubmit: Function;
-}
 
-export interface State {
-  loggingIn: boolean;
-  user: string;
-  password: string;
-  email: string;
-  valid: boolean;
-}
-
-export class LoginPage extends PureComponent<Props, State> {
-  submit: Function;
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      user: '',
-      password: '',
-      email: '',
-      loggingIn: false,
-      valid: false,
-    };
-  }
-
-  render() {
-    return (
-      <div className="login container">
-        <div className="login-content">
-          <div className="login-branding">
-            <img className="logo-icon" src="public/img/grafana_icon.svg" alt="Grafana" />
-            <div className="logo-wordmark" />
-          </div>
-          <LoginCtrl>
-            {({ login, isLoggingIn, changePassword, toGrafana, isChangingPassword }) =>
-              isChangingPassword ? (
-                <ChangePassword onSubmit={changePassword} onSkip={toGrafana} />
-              ) : (
-                <div className="login-out-box">
-                  <div className="login-inner-box" id="login-view">
-                    <LoginForm
-                      displayLoginFields={!config.disableLoginForm}
-                      displayForgotPassword={!(config.ldapEnabled || config.authProxyEnabled)}
-                      onSubmit={login}
-                      loginHint={config.loginHint}
-                      passwordHint={config.passwordHint}
-                      isLoggingIn={isLoggingIn}
-                    />
-
-                    {isOauthEnabled() ? (
-                      <>
-                        <div className="text-center login-divider">
-                          <div>
-                            <div className="login-divider-line" />
-                          </div>
-                          <div>
-                            <span className="login-divider-text">
-                              {config.disableLoginForm ? null : <span>or</span>}
-                            </span>
-                          </div>
-                          <div>
-                            <div className="login-divider-line" />
-                          </div>
-                        </div>
-                        <div className="clearfix" />
-
-                        <LoginServiceButtons />
-                      </>
-                    ) : null}
-
-                    <UserSignup />
-                  </div>
-                </div>
-              )
-            }
-          </LoginCtrl>
-
-          <div className="clearfix" />
+export const LoginPage: FC = () => {
+  return (
+    <div className="login container">
+      <div className="login-content">
+        <div className="login-branding">
+          <img className="logo-icon" src="public/img/grafana_icon.svg" alt="Grafana" />
+          <div className="logo-wordmark" />
         </div>
+        <LoginCtrl>
+          {({ login, isLoggingIn, changePassword, toGrafana, isChangingPassword }) =>
+            isChangingPassword ? (
+              <ChangePassword onSubmit={changePassword} onSkip={toGrafana} />
+            ) : (
+              <div className="login-out-box">
+                <div className="login-inner-box" id="login-view">
+                  <LoginForm
+                    displayLoginFields={!config.disableLoginForm}
+                    displayForgotPassword={!(config.ldapEnabled || config.authProxyEnabled)}
+                    onSubmit={login}
+                    loginHint={config.loginHint}
+                    passwordHint={config.passwordHint}
+                    isLoggingIn={isLoggingIn}
+                  />
+
+                  {isOauthEnabled() ? (
+                    <>
+                      <div className="text-center login-divider">
+                        <div>
+                          <div className="login-divider-line" />
+                        </div>
+                        <div>
+                          <span className="login-divider-text">{config.disableLoginForm ? null : <span>or</span>}</span>
+                        </div>
+                        <div>
+                          <div className="login-divider-line" />
+                        </div>
+                      </div>
+                      <div className="clearfix" />
+
+                      <LoginServiceButtons />
+                    </>
+                  ) : null}
+
+                  <UserSignup />
+                </div>
+              </div>
+            )
+          }
+        </LoginCtrl>
+
+        <div className="clearfix" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
