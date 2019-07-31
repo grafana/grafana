f8f1f506edc50de528888060da1422049fecb1f3
gotjosh <josue@grafana.com>
2019-07-31 11:23:00 +0100

    Auth Proxy: Include additional headers as part of the cache key (#18298)
    * Auth Proxy: Include additional headers as part of the cache key
    
    Auth proxy has support to send additional user attributes as part of the
    authentication flow. These attributes (e.g. Groups) need to be monitored
    as part of the process in case of change.
    
    This commit changes the way we compute the cache key to include all of the
    attributes sent as part of the authentication request. That way, if we
    change any user attributes we'll upsert the user information.
    
    (cherry picked from commit ed8aeb29993b3566cdb2686ad4ab512f05fdb154)

--- pkg/middleware/middleware_test.go
+++ pkg/middleware/middleware_test.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"encoding/base32"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -377,7 +378,9 @@ func TestMiddlewareContext(t *testing.T) {
 			setting.LDAPEnabled = true
 			setting.AuthProxyHeaderName = "X-WEBAUTH-USER"
 			setting.AuthProxyHeaderProperty = "username"
+			setting.AuthProxyHeaders = map[string]string{"Groups": "X-WEBAUTH-GROUPS"}
 			name := "markelog"
+			group := "grafana-core-team"
 
 			middlewareScenario(t, "should not sync the user if it's in the cache", func(sc *scenarioContext) {
 				bus.AddHandler("test", func(query *models.GetSignedInUserQuery) error {
@@ -385,11 +388,12 @@ func TestMiddlewareContext(t *testing.T) {
 					return nil
 				})
 
-				key := fmt.Sprintf(cachePrefix, name)
+				key := fmt.Sprintf(cachePrefix, base32.StdEncoding.EncodeToString([]byte(name+"-"+group)))
 				sc.remoteCacheService.Set(key, int64(33), 0)
 				sc.fakeReq("GET", "/")
 
 				sc.req.Header.Add(setting.AuthProxyHeaderName, name)
+				sc.req.Header.Add("X-WEBAUTH-GROUPS", group)
 				sc.exec()
 
 				Convey("Should init user via cache", func() {
