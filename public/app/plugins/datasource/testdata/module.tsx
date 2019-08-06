5839c8ced7206eb0fd6551188d0dc264685a6602
Torkel Ödegaard <torkel@grafana.com>
2019-08-06 14:59:54 +0200

    TestData: added support for nested data source variable queries, and test dashboard

--- public/app/plugins/datasource/testdata/module.tsx
+++ public/app/plugins/datasource/testdata/module.tsx
@@ -1,5 +1,5 @@
 import { DataSourcePlugin } from '@grafana/ui';
-import { TestDataDatasource } from './datasource';
+import { TestDataDataSource } from './datasource';
 import { TestDataQueryCtrl } from './query_ctrl';
 import { TestInfoTab } from './TestInfoTab';
 import { ConfigEditor } from './ConfigEditor';
@@ -10,7 +10,7 @@ class TestDataAnnotationsQueryCtrl {
   static template = '<h2>Annotation scenario</h2>';
 }
 
-export const plugin = new DataSourcePlugin(TestDataDatasource)
+export const plugin = new DataSourcePlugin(TestDataDataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryCtrl(TestDataQueryCtrl)
   .setAnnotationQueryCtrl(TestDataAnnotationsQueryCtrl)
