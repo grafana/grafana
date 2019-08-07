1c5698a85bbd02910b4b6a302dc17f635e4613ac
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-08-07 10:50:20 +0200

    Handle undefined/null strings

---
+++ packages/grafana-data/src/utils/markdown.test.ts
@@ -0,0 +1,8 @@
+import { renderMarkdown } from './markdown';
+
+describe('Markdown wrapper', () => {
+  it('should be able to handle undefined value', () => {
+    const str = renderMarkdown(undefined);
+    expect(str).toBe('');
+  });
+});
