9c56c815aafa85545f5764894fe47fdf3ebe590b
Shavonn Brown <shavonn.brown@grafana.com>
2019-08-19 15:23:07 -0400

    convert teams section of user profile to react

--- public/app/features/profile/ReactProfileWrapper.tsx
+++ public/app/features/profile/ReactProfileWrapper.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { UserProvider } from 'app/core/utils/UserProvider';
 import { UserProfileEditForm } from './UserProfileEditForm';
 import { SharedPreferences } from 'app/core/components/SharedPreferences/SharedPreferences';
+import { UserTeams } from './UserTeams';
 import { config } from '@grafana/runtime';
 
 export const ReactProfileWrapper = () => (
   <UserProvider userId={config.bootData.user.id}>
-    {(api, states, user) => {
+    {(api, states, teams, user) => {
       return (
         <>
           {!states.loadUser && (
@@ -17,6 +18,7 @@ export const ReactProfileWrapper = () => (
             />
           )}
           <SharedPreferences resourceUri="user" />
+          {!states.loadTeams && <UserTeams loadTeams={api.loadTeams} teams={teams} />}
         </>
       );
     }}
---
+++ public/app/features/profile/UserTeams.tsx
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react';
+import { Team } from 'app/types';
+
+export interface Props {
+  teams: Team[];
+  loadTeams: () => void;
+}
+
+export class UserTeams extends PureComponent<Props> {
+  componentDidMount() {
+    this.props.loadTeams();
+  }
+
+  render() {
+    const { teams } = this.props;
+
+    return (
+      <>
+        {teams.length > 0 && (
+          <>
+            <h3 className="page-sub-heading">Teams</h3>
+            <div className="gf-form-group">
+              <table className="filter-table form-inline">
+                <thead>
+                  <tr>
+                    <th />
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Members</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {teams.map((team: Team, index) => {
+                    return (
+                      <tr key={index}>
+                        <td className="width-4 text-center">
+                          <img className="filter-table__avatar" src={team.avatarUrl} />
+                        </td>
+                        <td>{team.name}</td>
+                        <td>{team.email}</td>
+                        <td>{team.memberCount}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
+      </>
+    );
+  }
+}
+
+export default UserTeams;
