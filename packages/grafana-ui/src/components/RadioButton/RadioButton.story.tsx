2f84816d0ee4a8717dbbce5dfa1d4ae587ef0913
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-08-20 10:31:52 +0200

    Add initial RadioButton and story

--- packages/grafana-ui/src/components/RadioButton/RadioButton.story.tsx
+++ packages/grafana-ui/src/components/RadioButton/RadioButton.story.tsx
@@ -1,8 +1,10 @@
 import { storiesOf } from '@storybook/react';
 import { renderComponentWithTheme } from '../../utils/storybook/withTheme';
 import { RadioButton } from './RadioButton';
+import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 
 const RadioButtonStories = storiesOf('UI/RadioButton', module);
+RadioButtonStories.addDecorator(withCenteredStory);
 
 RadioButtonStories.add('RadioButtonGroup', () => {
   return renderComponentWithTheme(RadioButton, {});
--- packages/grafana-ui/src/components/RadioButton/RadioButton.tsx
+++ packages/grafana-ui/src/components/RadioButton/RadioButton.tsx
@@ -1,4 +1,49 @@
 import React from 'react';
+import { css } from 'emotion';
+
+const toggleLabel = css`
+  position: relative;
+  $font-size: 14px;
+  $box-height: 32px;
+  $box-padding: ($box-height - $font-size)/2;
+  font-family: 'Roboto';
+  color: #8e8e8e;
+  font-weight: 500;
+  font-size: $font-size;
+  line-height: 1;
+
+  margin-left: -1px;
+  padding: $box-padding 16px $box-padding 16px;
+  border: 1px solid #555555;
+  background-color: #161719;
+  cursor: pointer;
+  z-index: 1;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  &:hover {
+    color: #5794f2;
+  }
+`;
+
+const toggle = css`
+  .toggle {
+    position: absolute;
+    top: 0;
+    left: -100vw;
+    opacity: 0;
+    z-index: -1000;
+
+    &:checked + ${toggleLabel} {
+      border-color: #5794f2;
+      color: #5794f2;
+      z-index: 2;
+    }
+  }
+`;
 
 interface Props {
   name: string;
@@ -7,10 +52,10 @@ interface Props {
   children: JSX.Element;
 }
 
-export const RadioButton = ({ name, id, className, children }: Props) => (
+export const RadioButton = ({ name, id, children }: Props) => (
   <>
-    <input type="radio" name={name} id={id} className={className} />
-    <label className="radio-label" htmlFor={id}>
+    <input type="radio" name={name} id={id} className={toggle} />
+    <label className={toggleLabel} htmlFor={id}>
       {children}
     </label>
   </>
