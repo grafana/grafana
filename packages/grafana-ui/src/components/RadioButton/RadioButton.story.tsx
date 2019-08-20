fb361a4b91fec54da4d94762662ba4785b025f84
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-08-20 13:14:19 +0200

    Add basic styles for radio button and group

--- packages/grafana-ui/src/components/RadioButton/RadioButton.story.tsx
+++ packages/grafana-ui/src/components/RadioButton/RadioButton.story.tsx
@@ -1,11 +1,20 @@
+import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { renderComponentWithTheme } from '../../utils/storybook/withTheme';
+// import { renderComponentWithTheme } from '../../utils/storybook/withTheme';
 import { RadioButton } from './RadioButton';
+import { RadioButtonGroup } from './RadioButtonGroup';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 
 const RadioButtonStories = storiesOf('UI/RadioButton', module);
 RadioButtonStories.addDecorator(withCenteredStory);
 
 RadioButtonStories.add('RadioButtonGroup', () => {
-  return renderComponentWithTheme(RadioButton, {});
+  return (
+    <RadioButtonGroup name="options">
+      <RadioButton id="label1">Label 1</RadioButton>
+      <RadioButton id="label2">Label 2</RadioButton>
+      <RadioButton id="label3">Label 3</RadioButton>
+      <RadioButton id="label4">Label 4</RadioButton>
+    </RadioButtonGroup>
+  );
 });
---
+++ packages/grafana-ui/src/components/RadioButton/RadioButtonGroup.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { css } from 'emotion';
+import { toggleLabel } from './RadioButton';
+
+const groupStyles = css`
+  display: flex;
+  flex-wrap: wrap;
+  .${toggleLabel}:first-of-type {
+    border-radius: 2px 0 0 2px;
+  }
+  .${toggleLabel}:last-of-type {
+    border-radius: 2px 0 0 2px;
+  }
+`;
+
+interface Props {
+  children: JSX.Element[];
+  name?: string;
+}
+
+export const RadioButtonGroup = ({ children, name }: Props) => {
+  let renderedChildren;
+  if (name) {
+    renderedChildren = children.map(child => React.cloneElement(child, { name }));
+  } else {
+    renderedChildren = children;
+  }
+  return <div className={groupStyles}>{renderedChildren}</div>;
+};
