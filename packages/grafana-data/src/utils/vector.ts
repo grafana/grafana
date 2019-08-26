6bdd98d81e4e8f529ca519a80643d4492630e4c7
ryan <ryantxu@gmail.com>
2019-08-25 22:13:13 -0700

    add to the front or the back

--- packages/grafana-data/src/utils/vector.ts
+++ packages/grafana-data/src/utils/vector.ts
@@ -76,39 +76,6 @@ export class ScaledVector implements Vector<number> {
   }
 }
 
-export class CircularVector<T = any> implements Vector<T> {
-  buffer: T[];
-  index: number;
-  length: number;
-
-  constructor(buffer: T[]) {
-    this.length = buffer.length;
-    this.buffer = buffer;
-    this.index = 0;
-  }
-
-  append(value: T) {
-    let idx = this.index - 1;
-    if (idx < 0) {
-      idx = this.length - 1;
-    }
-    this.buffer[idx] = value;
-    this.index = idx;
-  }
-
-  get(index: number): T {
-    return this.buffer[(index + this.index) % this.length];
-  }
-
-  toArray(): T[] {
-    return vectorToArray(this);
-  }
-
-  toJSON(): T[] {
-    return vectorToArray(this);
-  }
-}
-
 /**
  * Values are returned in the order defined by the input parameter
  */
@@ -121,6 +88,112 @@ export class SortedVector<T = any> implements Vector<T> {
 
   get(index: number): T {
     return this.source.get(this.order[index]);
+  }
+
+  toArray(): T[] {
+    return vectorToArray(this);
+  }
+
+  toJSON(): T[] {
+    return vectorToArray(this);
+  }
+}
+
+interface CircularOptions<T> {
+  buffer?: T[];
+  append?: 'head' | 'tail';
+  capacity?: number;
+}
+
+/**
+ * Circular vector uses a single buffer to capture a stream of values
+ * overwriting the oldest value on add.
+ *
+ * This supports addting to the 'head' or 'tail' and will grow the buffer
+ * to match a configured capacity.
+ */
+export class CircularVector<T = any> implements Vector<T> {
+  private buffer: T[];
+  private index: number;
+  private capacity: number;
+  private tail: boolean;
+
+  constructor(options: CircularOptions<T>) {
+    this.buffer = options.buffer || [];
+    this.capacity = this.buffer.length;
+    this.tail = 'head' !== options.append;
+    this.index = 0;
+
+    this.add = this.getAddFunction();
+    if (options.capacity) {
+      this.setCapacity(options.capacity);
+    }
+  }
+
+  // Pick an add function that either writes forwards or backwards
+  private getAddFunction() {
+    return this.tail
+      ? (value: T) => {
+          this.buffer[this.index] = value;
+          this.index = (this.index + 1) % this.buffer.length;
+        }
+      : (value: T) => {
+          let idx = this.index - 1;
+          if (idx < 0) {
+            idx = this.buffer.length - 1;
+          }
+          this.buffer[idx] = value;
+          this.index = idx;
+        };
+  }
+
+  setCapacity(v: number) {
+    if (this.capacity === v) {
+      return;
+    }
+    // Make a copy so it is in order and new additions can be at the head or tail
+    const copy = this.toArray();
+    if (v > this.length) {
+      this.buffer = copy;
+
+      // Change the 'add' function so it actually appends
+      this.add = this.tail
+        ? (value: T) => {
+            this.buffer.push(value);
+            if (this.buffer.length >= this.capacity) {
+              this.add = this.getAddFunction();
+            }
+          }
+        : (value: T) => {
+            this.buffer.unshift(value);
+            if (this.buffer.length >= this.capacity) {
+              this.add = this.getAddFunction();
+            }
+          };
+    } else if (v < this.capacity) {
+      // Shrink the buffer
+      const delta = this.length - v;
+      if (this.tail) {
+        this.buffer = copy.slice(delta, copy.length); // Keep last items
+      } else {
+        this.buffer = copy.slice(0, copy.length - delta); // Keep first items
+      }
+    }
+    this.capacity = v;
+    this.index = 0;
+  }
+
+  /**
+   * Add the value to the buffer
+   */
+  add: (value: T) => void;
+
+  get(index: number) {
+    return this.buffer[(index + this.index) % this.buffer.length];
+  }
+
+  get length() {
+    return this.buffer.length;
   }
 
   toArray(): T[] {
