3baa07f44b6e4baccee07fe10d7d83c2a562d93b
Torkel Ödegaard <torkel@grafana.com>
2019-08-29 16:19:27 +0200

    Explore: Fixed query status issue, fixes #18778

--- public/app/features/explore/QueryStatus.tsx
+++ public/app/features/explore/QueryStatus.tsx
@@ -39,9 +39,14 @@ interface QueryStatusProps {
 export default class QueryStatus extends PureComponent<QueryStatusProps> {
   render() {
     const { queryResponse, latency } = this.props;
+
+    if (queryResponse.state === LoadingState.NotStarted) {
+      return null;
+    }
+
     return (
       <div className="query-transactions">
-        {queryResponse && <QueryStatusItem queryResponse={queryResponse} latency={latency} />}
+        <QueryStatusItem queryResponse={queryResponse} latency={latency} />
       </div>
     );
   }
