3aa3a453721bfa0ced12f8b94c4802a5657f7034
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2019-08-29 11:02:48 +0100

    Prometheus: Changes brace-insertion behavior to be less annoying (#18698)
    * Changes brace-insertion behavior to be less annoying
    
    * Removes use of braces plugin
    
    * Revert "Removes use of braces plugin"
    
    This reverts commit 4cf4a6073bb77ec7ed569c6917db9bbf2b3fd176.

--- public/app/features/explore/slate-plugins/braces.ts
+++ public/app/features/explore/slate-plugins/braces.ts
@@ -1,45 +1,42 @@
+// @ts-ignore
+import { Change } from 'slate';
+
 const BRACES: any = {
   '[': ']',
   '{': '}',
   '(': ')',
 };
 
-const NON_SELECTOR_SPACE_REGEXP = / (?![^}]+})/;
-
 export default function BracesPlugin() {
   return {
-    onKeyDown(event: any, change: { value?: any; insertText?: any; deleteBackward?: any }) {
+    onKeyDown(event: KeyboardEvent, change: Change) {
       const { value } = change;
-      if (!value.isCollapsed) {
-        return undefined;
-      }
 
       switch (event.key) {
+        case '(':
         case '{':
         case '[': {
           event.preventDefault();
-          // Insert matching braces
-          change
-            .insertText(`${event.key}${BRACES[event.key]}`)
-            .move(-1)
-            .focus();
-          return true;
-        }
 
-        case '(': {
-          event.preventDefault();
-          const text = value.anchorText.text;
-          const offset = value.anchorOffset;
-          const delimiterIndex = text.slice(offset).search(NON_SELECTOR_SPACE_REGEXP);
-          const length = delimiterIndex > -1 ? delimiterIndex + offset : text.length;
-          const forward = length - offset;
-          // Insert matching braces
-          change
-            .insertText(event.key)
-            .move(forward)
-            .insertText(BRACES[event.key])
-            .move(-1 - forward)
-            .focus();
+          const { startOffset, startKey, endOffset, endKey, focusOffset } = value.selection;
+          const text: string = value.focusText.text;
+
+          // If text is selected, wrap selected text in parens
+          if (value.isExpanded) {
+            change
+              .insertTextByKey(startKey, startOffset, event.key)
+              .insertTextByKey(endKey, endOffset + 1, BRACES[event.key])
+              .moveEnd(-1);
+          } else if (
+            focusOffset === text.length ||
+            text[focusOffset] === ' ' ||
+            Object.values(BRACES).includes(text[focusOffset])
+          ) {
+            change.insertText(`${event.key}${BRACES[event.key]}`).move(-1);
+          } else {
+            change.insertText(event.key);
+          }
+
           return true;
         }
 
