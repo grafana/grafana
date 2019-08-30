51e640939ffd90b7452982e25e604a6c01bd182b
Josue Abreu <josue@grafana.com>
2019-08-30 15:04:02 +0100

    LDAP: Adds the /api/admin/ldap/:username endpoint
    This endpoint returns an user found within the configured LDAP server(s). Moreso, it provides the mapping information for the user to help administrators understand how the users would be created within Grafana based on the current configuration.
    
    No changes are executed or saved to the database, this is all an in-memory representation of how the final result would look like.

--- pkg/api/ldap_debug.go
+++ pkg/api/ldap_debug.go
@@ -1,9 +1,50 @@
 package api
 
 import (
+	"net/http"
+
+	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/ldap"
+	"github.com/grafana/grafana/pkg/services/multildap"
 )
 
+var (
+	getLDAPConfig = multildap.GetConfig
+	newLDAP       = multildap.New
+)
+
+// LDAPAttribute is a serializer for user attributes mapped from LDAP. Is meant to display both the serialized value and the LDAP key we received it from.
+type LDAPAttribute struct {
+	ConfigAttributeValue string `json:"cfgAttrValue"`
+	LDAPAttributeValue   string `json:"ldapValue"`
+}
+
+// RoleDTO is a serializer for mapped roles from LDAP
+type RoleDTO struct {
+	OrgId   int64           `json:"orgId"`
+	OrgRole models.RoleType `json:"orgRole"`
+	GroupDN string          `json:"groupDN"`
+}
+
+// TeamDTO is a serializer for mapped Teams from LDAP
+type TeamDTO struct {
+	GroupDN string `json:"groupDN"`
+	TeamId  int64  `json:"teamId"`
+}
+
+// LDAPUserDTO is a serializer for users mapped from LDAP
+type LDAPUserDTO struct {
+	Name           *LDAPAttribute `json:"name"`
+	Surname        *LDAPAttribute `json:"surname"`
+	Email          *LDAPAttribute `json:"email"`
+	Username       *LDAPAttribute `json:"login"`
+	IsGrafanaAdmin *bool          `json:"isGrafanaAdmin"`
+	IsDisabled     bool           `json:"isDisabled"`
+	OrgRoles       *[]RoleDTO     `json:"roles"`
+	Teams          *[]TeamDTO     `json:"teams"`
+}
+
+// ReloadLDAPCfg reloads the LDAP configuration
 func (server *HTTPServer) ReloadLDAPCfg() Response {
 	if !ldap.IsEnabled() {
 		return Error(400, "LDAP is not enabled", nil)
@@ -14,4 +55,65 @@ func (server *HTTPServer) ReloadLDAPCfg() Response {
 		return Error(500, "Failed to reload ldap config.", err)
 	}
 	return Success("LDAP config reloaded")
+}
+
+// GetUserFromLDAP finds an user based on a username in LDAP. This helps illustrate how would the particular user be mapped in Grafana when synced.
+func (server *HTTPServer) GetUserFromLDAP(c *models.ReqContext) Response {
+	ldapConfig, err := getLDAPConfig()
+
+	if err != nil {
+		return Error(400, "Failed to obtain the LDAP configuration. Please ", err)
+	}
+
+	ldap := newLDAP(ldapConfig.Servers)
+
+	username := c.Params(":username")
+
+	if len(username) == 0 {
+		return Error(http.StatusBadRequest, "Validation error. You must specify an username", nil)
+	}
+
+	user, serverConfig, err := ldap.User(username)
+
+	if user == nil {
+		return Error(
+			404, "No user was found on the LDAP server(s)", err)
+	}
+
+	u := &LDAPUserDTO{
+		Name:           &LDAPAttribute{serverConfig.Attr.Name, user.Name},
+		Surname:        &LDAPAttribute{serverConfig.Attr.Surname, user.Name},
+		Email:          &LDAPAttribute{serverConfig.Attr.Email, user.Email},
+		Username:       &LDAPAttribute{serverConfig.Attr.Username, user.Login},
+		IsGrafanaAdmin: user.IsGrafanaAdmin,
+		IsDisabled:     user.IsDisabled,
+	}
+
+	orgRoles := []RoleDTO{}
+
+	for _, g := range serverConfig.Groups {
+		role := &RoleDTO{}
+
+		if isMatchToLDAPGroup(user, g) {
+			role.OrgId = g.OrgID
+			role.OrgRole = user.OrgRoles[g.OrgID]
+			role.GroupDN = g.GroupDN
+
+			orgRoles = append(orgRoles, *role)
+		} else {
+			role.GroupDN = g.GroupDN
+
+			orgRoles = append(orgRoles, *role)
+		}
+	}
+
+	u.OrgRoles = &orgRoles
+
+	return JSON(200, u)
+}
+
+// isMatchToLDAPGroup determines if we were able to match an LDAP group to an organization+role.
+// Since we allow one role per organization. If it's set, we were able to match it.
+func isMatchToLDAPGroup(user *models.ExternalUserInfo, groupConfig *ldap.GroupToOrgRole) bool {
+	return user.OrgRoles[groupConfig.OrgID] == groupConfig.OrgRole
 }
---
+++ pkg/api/ldap_debug_test.go
@@ -0,0 +1,146 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/ldap"
+	"github.com/grafana/grafana/pkg/services/multildap"
+	"github.com/grafana/grafana/pkg/setting"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type LDAPMock struct {
+	Results []*models.ExternalUserInfo
+}
+
+var userSearchResult *models.ExternalUserInfo
+var userSearchConfig *ldap.ServerConfig
+
+func (m *LDAPMock) Login(query *models.LoginUserQuery) (*models.ExternalUserInfo, error) {
+	return &models.ExternalUserInfo{}, nil
+}
+
+func (m *LDAPMock) Users(logins []string) ([]*models.ExternalUserInfo, error) {
+	s := []*models.ExternalUserInfo{}
+	return s, nil
+}
+
+func (m *LDAPMock) User(login string) (*models.ExternalUserInfo, *ldap.ServerConfig, error) {
+	return userSearchResult, userSearchConfig, nil
+}
+
+func getUserFromLDAPContext(t *testing.T, requestURL string) *scenarioContext {
+	t.Helper()
+
+	sc := setupScenarioContext(requestURL)
+
+	hs := &HTTPServer{Cfg: setting.NewCfg()}
+
+	sc.defaultHandler = Wrap(func(c *models.ReqContext) Response {
+		sc.context = c
+		return hs.GetUserFromLDAP(c)
+	})
+
+	sc.m.Get("/api/admin/ldap/:username", sc.defaultHandler)
+
+	sc.resp = httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, requestURL, nil)
+	sc.req = req
+	sc.exec()
+
+	return sc
+}
+
+func TestGetUserFromLDAPApiEndpoint_UserNotFound(t *testing.T) {
+	getLDAPConfig = func() (*ldap.Config, error) {
+		return &ldap.Config{}, nil
+	}
+
+	newLDAP = func(_ []*ldap.ServerConfig) multildap.IMultiLDAP {
+		return &LDAPMock{}
+	}
+
+	userSearchResult = nil
+
+	sc := getUserFromLDAPContext(t, "/api/admin/ldap/user-that-does-not-exist")
+
+	require.Equal(t, sc.resp.Code, http.StatusNotFound)
+	responseString, err := getBody(sc.resp)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "{\"message\":\"No user was found on the LDAP server(s)\"}", responseString)
+}
+
+func TestGetUserFromLDAPApiEndpoint(t *testing.T) {
+	isAdmin := true
+	userSearchResult = &models.ExternalUserInfo{
+		Name:           "John Doe",
+		Email:          "john.doe@example.com",
+		Login:          "johndoe",
+		OrgRoles:       map[int64]models.RoleType{1: models.ROLE_ADMIN},
+		IsGrafanaAdmin: &isAdmin,
+	}
+
+	userSearchConfig = &ldap.ServerConfig{
+		Attr: ldap.AttributeMap{
+			Name:     "ldap-name",
+			Surname:  "ldap-surname",
+			Email:    "ldap-email",
+			Username: "ldap-username",
+		},
+		Groups: []*ldap.GroupToOrgRole{
+			{
+				GroupDN: "cn=admins,ou=groups,dc=grafana,dc=org",
+				OrgID:   1,
+				OrgRole: models.ROLE_ADMIN,
+			},
+		},
+	}
+
+	getLDAPConfig = func() (*ldap.Config, error) {
+		return &ldap.Config{}, nil
+	}
+
+	newLDAP = func(_ []*ldap.ServerConfig) multildap.IMultiLDAP {
+		return &LDAPMock{}
+	}
+
+	sc := getUserFromLDAPContext(t, "/api/admin/ldap/johndoe")
+
+	require.Equal(t, sc.resp.Code, http.StatusOK)
+
+	jsonResponse, err := getJSONbody(sc.resp)
+	assert.Nil(t, err)
+
+	expected := `
+		{
+		  "name": {
+				"cfgAttrValue": "ldap-name", "ldapValue": "John Doe"
+			},
+			"surname": {
+				"cfgAttrValue": "ldap-surname", "ldapValue": "John Doe"
+			},
+			"email": {
+				"cfgAttrValue": "ldap-email", "ldapValue": "john.doe@example.com"
+			},
+			"login": {
+				"cfgAttrValue": "ldap-username", "ldapValue": "johndoe"
+			},
+			"isGrafanaAdmin": true,
+			"isDisabled": false,
+			"roles": [
+				{ "orgId": 1, "orgRole": "Admin", "groupDN": "cn=admins,ou=groups,dc=grafana,dc=org" }
+			],
+			"teams": null
+		}
+	`
+	var expectedJSON interface{}
+	_ = json.Unmarshal([]byte(expected), &expectedJSON)
+
+	assert.Equal(t, jsonResponse, expectedJSON)
+}
