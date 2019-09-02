c931cdcaba861a6db3c4cf36cd92ff6ad43b9116
javadkh2 <khalilian.javad@gmail.com>
2019-09-02 10:18:52 +0430

    bugfix on create lowerCase version of dictionary

--- public/app/locale/translator.ts
+++ public/app/locale/translator.ts
@@ -1,11 +1,20 @@
 import { vocabs } from "./fa-IR";
 
-export const Translator = (vocabularies: { [key: string]: string }) => {
-    const dictionary = Object.entries(vocabularies).map(([key, value]) => ({ [key.toLowerCase()]: value }));
-    return (key = "") => {
+interface Vocabs {
+    [key: string]: string;
+}
+
+export function Translator(vocabs: Vocabs) {
+
+    const dictionary = Object.entries(vocabs).reduce((dic, [key, value]) => ({
+        ...dic,
+        [key.toLowerCase()]: value,
+    }), {} as Vocabs);
+
+    return function translate(key = "") {
         const term = key.toLowerCase();
         return dictionary[term] || term;
     };
-};
+}
 
 export const translate = Translator(vocabs);
