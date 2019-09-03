09032cd766cca75f5809695b111475f6f480600b
Torkel Ödegaard <torkel@grafana.com>
2019-09-03 18:20:10 +0200

    PanelQueryState: restore comment

--- public/app/features/dashboard/state/PanelQueryState.ts
+++ public/app/features/dashboard/state/PanelQueryState.ts
@@ -274,6 +274,8 @@ export class PanelQueryState {
 
     return {
       state: done ? LoadingState.Done : LoadingState.Streaming,
+      // This should not be needed but unfortunately Prometheus datasource sends non DataFrame here bypassing the
+      // typings
       series: this.sendFrames ? getProcessedDataFrames(series) : [],
       legacy: this.sendLegacy ? translateToLegacyData(series) : undefined,
       request: {
