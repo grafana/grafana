cf33874e6f92765807d145efa64f4c0650211a9e
Andrej Ocenas <mr.ocenas@gmail.com>
2019-09-04 16:57:42 +0200

    Fix labels loading for LokiQueryEditor

--- public/app/plugins/datasource/loki/components/LokiQueryEditor.tsx
+++ public/app/plugins/datasource/loki/components/LokiQueryEditor.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
 // Types
 import { AbsoluteTimeRange } from '@grafana/data';
@@ -7,63 +7,60 @@ import { QueryEditorProps, Switch, DataSourceStatus } from '@grafana/ui';
 import { LokiDatasource } from '../datasource';
 import { LokiQuery } from '../types';
 import { LokiQueryField } from './LokiQueryField';
+import { useLokiSyntax } from './useLokiSyntax';
 
 type Props = QueryEditorProps<LokiDatasource, LokiQuery>;
 
-export class LokiQueryEditor extends PureComponent<Props> {
-  onToggleLive = () => {
-    const { query, onChange } = this.props;
-    onChange({
-      ...query,
-      live: !query.live,
-    });
-  };
+export const LokiQueryEditor = memo(function LokiQueryEditor(props: Props) {
+  const { query, panelData, datasource, onChange, onRunQuery } = props;
 
-  render() {
-    const { query, panelData, datasource, onChange, onRunQuery } = this.props;
+  let absolute: AbsoluteTimeRange;
+  if (panelData && panelData.request) {
+    const { range } = panelData.request;
+    absolute = {
+      from: range.from.valueOf(),
+      to: range.to.valueOf(),
+    };
+  } else {
+    absolute = {
+      from: Date.now() - 10000,
+      to: Date.now(),
+    };
+  }
 
-    let absolute: AbsoluteTimeRange;
-    if (panelData && panelData.request) {
-      const { range } = panelData.request;
-      absolute = {
-        from: range.from.valueOf(),
-        to: range.to.valueOf(),
-      };
-    } else {
-      absolute = {
-        from: Date.now() - 10000,
-        to: Date.now(),
-      };
-    }
+  const { isSyntaxReady, setActiveOption, refreshLabels, ...syntaxProps } = useLokiSyntax(
+    datasource.languageProvider,
+    // TODO maybe use real status
+    DataSourceStatus.Connected,
+    absolute
+  );
 
-    return (
-      <div>
-        <LokiQueryField
-          datasource={datasource}
-          query={query}
-          onChange={onChange}
-          onRunQuery={onRunQuery}
-          history={[]}
-          panelData={panelData}
-          // ??? What do we need here?
-          syntaxLoaded={false}
-          onLoadOptions={() => {}}
-          logLabelOptions={[]}
-          syntax={[]}
-          datasourceStatus={DataSourceStatus.Connected}
-          absoluteRange={absolute}
-        />
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <Switch label="Live" checked={!!query.live} onChange={this.onToggleLive} />
-          </div>
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
+  return (
+    <div>
+      <LokiQueryField
+        datasource={datasource}
+        datasourceStatus={DataSourceStatus.Connected}
+        query={query}
+        onChange={onChange}
+        onRunQuery={onRunQuery}
+        history={[]}
+        panelData={panelData}
+        onLoadOptions={setActiveOption}
+        onLabelsRefresh={refreshLabels}
+        syntaxLoaded={isSyntaxReady}
+        absoluteRange={absolute}
+        {...syntaxProps}
+      />
+      <div className="gf-form-inline">
+        <div className="gf-form">
+          <Switch label="Live" checked={!!query.live} onChange={() => onChange({ ...query, live: !query.live })} />
+        </div>
+        <div className="gf-form gf-form--grow">
+          <div className="gf-form-label gf-form-label--grow" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+});
 
 export default LokiQueryEditor;
