91dc036ab4208d4fb9fec7f8b6438974339d2fe5
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-09-06 13:42:42 +0300

    LDAP debug: add bulk-sync button stub

--- public/app/features/admin/LdapSyncInfo.tsx
+++ public/app/features/admin/LdapSyncInfo.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { PureComponent } from 'react';
 
 interface Props {
   ldapSyncInfo: {
@@ -11,30 +11,53 @@ interface Props {
   tableStyle: string;
 }
 
-export const LdapSyncInfo: FC<Props> = ({ headingStyle, ldapSyncInfo, tableStyle }) => {
-  return (
-    <>
-      <h4 className={headingStyle}>LDAP Synchronisation</h4>
-      <table className={`${tableStyle} filter-table form-inline`}>
-        <tbody>
-          <tr>
-            <td>Active synchronisation</td>
-            <td>{ldapSyncInfo.enabled ? 'Enabled' : 'Disabled'}</td>
-          </tr>
-          <tr>
-            <td>Scheduled</td>
-            <td>{ldapSyncInfo.scheduled}</td>
-          </tr>
-          <tr>
-            <td>Next scheduled synchronisation</td>
-            <td>{ldapSyncInfo.nextScheduled}</td>
-          </tr>
-          <tr>
-            <td>Last synchronisation</td>
-            <td>{ldapSyncInfo.lastSync}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
-  );
-};
+interface State {
+  isSyncing: boolean;
+}
+
+export class LdapSyncInfo extends PureComponent<Props, State> {
+  state = {
+    isSyncing: false,
+  };
+
+  handleSyncClick = () => {
+    console.log('Bulk-sync now');
+    this.setState({ isSyncing: !this.state.isSyncing });
+  };
+
+  render() {
+    const { headingStyle, ldapSyncInfo, tableStyle } = this.props;
+    const { isSyncing } = this.state;
+    return (
+      <>
+        <h4 className={headingStyle}>
+          LDAP Synchronisation
+          <button className={`btn btn-secondary pull-right`} onClick={this.handleSyncClick}>
+            <span className="btn-title">Bulk-sync now</span>
+            {isSyncing && <i className="fa fa-spinner fa-fw fa-spin run-icon" />}
+          </button>
+        </h4>
+        <table className={`${tableStyle} filter-table form-inline`}>
+          <tbody>
+            <tr>
+              <td>Active synchronisation</td>
+              <td>{ldapSyncInfo.enabled ? 'Enabled' : 'Disabled'}</td>
+            </tr>
+            <tr>
+              <td>Scheduled</td>
+              <td>{ldapSyncInfo.scheduled}</td>
+            </tr>
+            <tr>
+              <td>Next scheduled synchronisation</td>
+              <td>{ldapSyncInfo.nextScheduled}</td>
+            </tr>
+            <tr>
+              <td>Last synchronisation</td>
+              <td>{ldapSyncInfo.lastSync}</td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
