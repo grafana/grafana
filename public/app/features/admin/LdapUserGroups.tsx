b2bfba5dbd869f903a193810964c67e1ce3a2b08
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-09-06 13:13:20 +0300

    LDAP debug: role mapping view tweaks

--- public/app/features/admin/LdapUserGroups.tsx
+++ public/app/features/admin/LdapUserGroups.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { LdapRole } from '../../types';
+import { Tooltip } from '@grafana/ui';
 
 interface Props {
   groups: LdapRole[];
@@ -23,7 +24,18 @@ export const LdapUserGroups: FC<Props> = ({ className, groups }) => {
               <td className="width-16">{group.orgName}</td>
               <td className="width-14">{group.orgRole}</td>
               <td>{group.groupDN}</td>
-              <td>{!group.orgRole && <span>No match</span>}</td>
+              <td>
+                {!group.orgRole && (
+                  <span className="text-warning pull-right">
+                    No match
+                    <Tooltip placement="top" content="No matching groups found" theme={'info'}>
+                      <div className="gf-form-help-icon gf-form-help-icon--right-normal">
+                        <i className="fa fa-info-circle" />
+                      </div>
+                    </Tooltip>
+                  </span>
+                )}
+              </td>
             </tr>
           );
         })}
