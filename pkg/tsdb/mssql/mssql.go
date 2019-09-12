d89a2ecb238b95d896df6bc6642a8e55f66b164d
Brian Gann <bkgann@gmail.com>
2019-09-12 09:57:00 -0500

    fix for 19046

--- pkg/tsdb/mssql/mssql.go
+++ pkg/tsdb/mssql/mssql.go
@@ -69,7 +69,8 @@ func (t *mssqlRowTransformer) Transform(columnTypes []*sql.ColumnType, rows *cor
 	valuePtrs := make([]interface{}, len(columnTypes))
 
 	for i, stype := range columnTypes {
-		t.log.Debug("type", "type", stype)
+		// debug output on large tables causes high memory utilization/leak
+		// t.log.Debug("type", "type", stype)
 		valuePtrs[i] = &values[i]
 	}
 
