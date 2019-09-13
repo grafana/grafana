06158a89fa6b92813ab25f597d45100a09f0b840
Erik Sundell <erik.sundell87@gmail.com>
2019-09-13 14:23:21 +0200

    Improve migration

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/migrations.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/migrations.ts
@@ -1,7 +1,7 @@
 import { AzureMonitorQueryCtrl } from './query_ctrl';
 
 export function migrateTargetSchema(target: any) {
-  if (!target.azureMonitor.data) {
+  if (target.azureMonitor && !target.azureMonitor.data) {
     const temp = { ...target.azureMonitor };
     target.azureMonitor = {
       data: {
