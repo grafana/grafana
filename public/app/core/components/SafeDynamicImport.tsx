ceea61323e5224032c5f0fb3be83dc4c85015799
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-09-16 14:45:45 +0200

    Refactor: Changes after PR review

--- public/app/core/components/SafeDynamicImport.tsx
+++ public/app/core/components/SafeDynamicImport.tsx
@@ -1,20 +1,48 @@
 import React, { lazy, Suspense, FunctionComponent } from 'react';
-import { cx } from 'emotion';
-import { ErrorBoundaryAlert, LoadingPlaceholder } from '@grafana/ui';
+import { cx, css } from 'emotion';
+import { LoadingPlaceholder, ErrorBoundary, Button } from '@grafana/ui';
 
 export const LoadingChunkPlaceHolder: FunctionComponent = () => (
   <div className={cx('preloader')}>
-    <LoadingPlaceholder text={'Loading chunk...'} />
+    <LoadingPlaceholder text={'Loading...'} />
   </div>
 );
+
+function getAlertPageStyle() {
+  return css`
+    width: 500px;
+    margin: 64px auto;
+  `;
+}
 
 export const SafeDynamicImport = (importStatement: Promise<any>) => ({ ...props }) => {
   const LazyComponent = lazy(() => importStatement);
   return (
-    <ErrorBoundaryAlert style={'page'} title={"We did't manage to load a chunk, try refreshing the page."}>
-      <Suspense fallback={<LoadingChunkPlaceHolder />}>
-        <LazyComponent {...props} />
-      </Suspense>
-    </ErrorBoundaryAlert>
+    <ErrorBoundary>
+      {({ error, errorInfo }) => {
+        if (!errorInfo) {
+          return (
+            <Suspense fallback={<LoadingChunkPlaceHolder />}>
+              <LazyComponent {...props} />
+            </Suspense>
+          );
+        }
+
+        return (
+          <div className={getAlertPageStyle()}>
+            <h2>Grafana has been updated. Please refresh your browser.</h2>
+            <br />
+            <Button size={'lg'} variant={'secondary'} icon="fa fa-repeat" onClick={() => window.location.reload()}>
+              Refresh
+            </Button>
+            <details style={{ whiteSpace: 'pre-wrap' }}>
+              {error && error.toString()}
+              <br />
+              {errorInfo.componentStack}
+            </details>
+          </div>
+        );
+      }}
+    </ErrorBoundary>
   );
 };
