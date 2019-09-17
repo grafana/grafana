10d47ab095ee8f1a61286e601685ab08d535df24
Dominik Prokop <dominik.prokop@grafana.com>
2019-09-17 13:29:14 +0200

    release 6.4.0-beta.1

--- scripts/build/publish.sh
+++ scripts/build/publish.sh
@@ -8,8 +8,8 @@ EXTRA_OPTS="$@"
 
 # Right now we hack this in into the publish script.
 # Eventually we might want to keep a list of all previous releases somewhere.
-_releaseNoteUrl="https://community.grafana.com/t/release-notes-v6-0-x/14010"
-_whatsNewUrl="http://docs.grafana.org/guides/whats-new-in-v6-0/"
+_releaseNoteUrl="https://community.grafana.com/t/release-notes-v-6-4-x/21088"
+_whatsNewUrl="https://grafana.com/docs/guides/whats-new-in-v6-4/"
 
 ./scripts/build/release_publisher/release_publisher \
     --wn "${_whatsNewUrl}" \
