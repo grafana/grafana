0f524fc9478767bd968290b81e85384d00fdb97a
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-09-18 19:30:35 +0200

    MSSQL: Revert usage of new connectionstring format (#19203)
    This reverts commit 2514209 from #18384. Reason is that it doesn't 
    work due to xorm 0.7.1 which doesn't support this new connectionstring 
    format.
    
    Fixes #19189
    Ref #18384
    Ref #17665

--- pkg/tsdb/mssql/mssql.go
+++ pkg/tsdb/mssql/mssql.go
@@ -3,7 +3,6 @@ package mssql
 import (
 	"database/sql"
 	"fmt"
-	"net/url"
 	"strconv"
 
 	"github.com/grafana/grafana/pkg/setting"
@@ -24,7 +23,10 @@ func init() {
 func newMssqlQueryEndpoint(datasource *models.DataSource) (tsdb.TsdbQueryEndpoint, error) {
 	logger := log.New("tsdb.mssql")
 
-	cnnstr := generateConnectionString(datasource)
+	cnnstr, err := generateConnectionString(datasource)
+	if err != nil {
+		return nil, err
+	}
 	if setting.Env == setting.DEV {
 		logger.Debug("getEngine", "connection", cnnstr)
 	}
@@ -43,21 +45,21 @@ func newMssqlQueryEndpoint(datasource *models.DataSource) (tsdb.TsdbQueryEndpoin
 	return sqleng.NewSqlQueryEndpoint(&config, &rowTransformer, newMssqlMacroEngine(), logger)
 }
 
-func generateConnectionString(datasource *models.DataSource) string {
+func generateConnectionString(datasource *models.DataSource) (string, error) {
 	server, port := util.SplitHostPortDefault(datasource.Url, "localhost", "1433")
+
 	encrypt := datasource.JsonData.Get("encrypt").MustString("false")
-
-	query := url.Values{}
-	query.Add("database", datasource.Database)
-	query.Add("encrypt", encrypt)
-
-	u := &url.URL{
-		Scheme:   "sqlserver",
-		User:     url.UserPassword(datasource.User, datasource.DecryptedPassword()),
-		Host:     fmt.Sprintf("%s:%s", server, port),
-		RawQuery: query.Encode(),
+	connStr := fmt.Sprintf("server=%s;port=%s;database=%s;user id=%s;password=%s;",
+		server,
+		port,
+		datasource.Database,
+		datasource.User,
+		datasource.DecryptedPassword(),
+	)
+	if encrypt != "false" {
+		connStr += fmt.Sprintf("encrypt=%s;", encrypt)
 	}
-	return u.String()
+	return connStr, nil
 }
 
 type mssqlRowTransformer struct {
