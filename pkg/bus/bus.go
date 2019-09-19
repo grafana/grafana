2240609d7e4b06573653db4aabbc8d1f3334888a
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-09-19 21:28:48 +0200

    remove non-context bus methods

--- pkg/bus/bus.go
+++ pkg/bus/bus.go
@@ -25,9 +25,8 @@ type TransactionManager interface {
 
 // Bus type defines the bus interface structure
 type Bus interface {
-	Dispatch(msg Msg) error
 	DispatchCtx(ctx context.Context, msg Msg) error
-	Publish(msg Msg) error
+	Publish(ctx context.Context, msg Msg) error
 
 	// InTransaction starts a transaction and store it in the context.
 	// The caller can then pass a function with multiple DispatchCtx calls that
@@ -35,7 +34,6 @@ type Bus interface {
 	// callback returns an error.
 	InTransaction(ctx context.Context, fn func(ctx context.Context) error) error
 
-	AddHandler(handler HandlerFunc)
 	AddHandlerCtx(handler HandlerFunc)
 	AddEventListener(handler HandlerFunc)
 	AddWildcardListener(handler HandlerFunc)
@@ -53,7 +51,6 @@ func (b *InProcBus) InTransaction(ctx context.Context, fn func(ctx context.Conte
 
 // InProcBus defines the bus structure
 type InProcBus struct {
-	handlers          map[string]HandlerFunc
 	handlersWithCtx   map[string]HandlerFunc
 	listeners         map[string][]HandlerFunc
 	wildcardListeners []HandlerFunc
@@ -66,7 +63,6 @@ var globalBus = New()
 // New initialize the bus
 func New() Bus {
 	bus := &InProcBus{}
-	bus.handlers = make(map[string]HandlerFunc)
 	bus.handlersWithCtx = make(map[string]HandlerFunc)
 	bus.listeners = make(map[string][]HandlerFunc)
 	bus.wildcardListeners = make([]HandlerFunc, 0)
@@ -106,43 +102,14 @@ func (b *InProcBus) DispatchCtx(ctx context.Context, msg Msg) error {
 	return err.(error)
 }
 
-// Dispatch function dispatch a message to the bus.
-func (b *InProcBus) Dispatch(msg Msg) error {
-	var msgName = reflect.TypeOf(msg).Elem().Name()
-
-	var handler = b.handlersWithCtx[msgName]
-	withCtx := true
-
-	if handler == nil {
-		withCtx = false
-		handler = b.handlers[msgName]
-	}
-
-	if handler == nil {
-		return ErrHandlerNotFound
-	}
-
-	var params = []reflect.Value{}
-	if withCtx {
-		params = append(params, reflect.ValueOf(context.Background()))
-	}
-	params = append(params, reflect.ValueOf(msg))
-
-	ret := reflect.ValueOf(handler).Call(params)
-	err := ret[0].Interface()
-	if err == nil {
-		return nil
-	}
-	return err.(error)
-}
-
 // Publish function publish a message to the bus listener.
-func (b *InProcBus) Publish(msg Msg) error {
+func (b *InProcBus) Publish(ctx context.Context, msg Msg) error {
 	var msgName = reflect.TypeOf(msg).Elem().Name()
 	var listeners = b.listeners[msgName]
 
-	var params = make([]reflect.Value, 1)
-	params[0] = reflect.ValueOf(msg)
+	var params = []reflect.Value{}
+	params = append(params, reflect.ValueOf(ctx))
+	params = append(params, reflect.ValueOf(msg))
 
 	for _, listenerHandler := range listeners {
 		ret := reflect.ValueOf(listenerHandler).Call(params)
@@ -167,12 +134,6 @@ func (b *InProcBus) AddWildcardListener(handler HandlerFunc) {
 	b.wildcardListeners = append(b.wildcardListeners, handler)
 }
 
-func (b *InProcBus) AddHandler(handler HandlerFunc) {
-	handlerType := reflect.TypeOf(handler)
-	queryTypeName := handlerType.In(0).Elem().Name()
-	b.handlers[queryTypeName] = handler
-}
-
 func (b *InProcBus) AddHandlerCtx(handler HandlerFunc) {
 	handlerType := reflect.TypeOf(handler)
 	queryTypeName := handlerType.In(1).Elem().Name()
@@ -181,18 +142,12 @@ func (b *InProcBus) AddHandlerCtx(handler HandlerFunc) {
 
 func (b *InProcBus) AddEventListener(handler HandlerFunc) {
 	handlerType := reflect.TypeOf(handler)
-	eventName := handlerType.In(0).Elem().Name()
+	eventName := handlerType.In(1).Elem().Name()
 	_, exists := b.listeners[eventName]
 	if !exists {
 		b.listeners[eventName] = make([]HandlerFunc, 0)
 	}
 	b.listeners[eventName] = append(b.listeners[eventName], handler)
-}
-
-// AddHandler attach a handler function to the global bus
-// Package level function
-func AddHandler(implName string, handler HandlerFunc) {
-	globalBus.AddHandler(handler)
 }
 
 // AddHandlerCtx attach a handler function to the global bus context
@@ -212,16 +167,12 @@ func AddWildcardListener(handler HandlerFunc) {
 	globalBus.AddWildcardListener(handler)
 }
 
-func Dispatch(msg Msg) error {
-	return globalBus.Dispatch(msg)
-}
-
 func DispatchCtx(ctx context.Context, msg Msg) error {
 	return globalBus.DispatchCtx(ctx, msg)
 }
 
-func Publish(msg Msg) error {
-	return globalBus.Publish(msg)
+func Publish(ctx context.Context, msg Msg) error {
+	return globalBus.Publish(ctx, msg)
 }
 
 // InTransaction starts a transaction and store it in the context.
--- pkg/bus/bus_test.go
+++ pkg/bus/bus_test.go
@@ -12,56 +12,23 @@ type testQuery struct {
 	Resp string
 }
 
-func TestDispatchCtxCanUseNormalHandlers(t *testing.T) {
+func TestDispatchCtx(t *testing.T) {
 	bus := New()
-
-	handlerWithCtxCallCount := 0
-	handlerCallCount := 0
-
-	handlerWithCtx := func(ctx context.Context, query *testQuery) error {
-		handlerWithCtxCallCount++
-		return nil
-	}
-
-	handler := func(query *testQuery) error {
-		handlerCallCount++
-		return nil
-	}
 
 	err := bus.DispatchCtx(context.Background(), &testQuery{})
 	if err != ErrHandlerNotFound {
 		t.Errorf("expected bus to return HandlerNotFound is no handler is registered")
 	}
-
-	bus.AddHandler(handler)
-
-	t.Run("when a normal handler is registered", func(t *testing.T) {
-		bus.Dispatch(&testQuery{})
-
-		if handlerCallCount != 1 {
-			t.Errorf("Expected normal handler to be called 1 time. was called %d", handlerCallCount)
-		}
-
-		t.Run("when a ctx handler is registered", func(t *testing.T) {
-			bus.AddHandlerCtx(handlerWithCtx)
-			bus.Dispatch(&testQuery{})
-
-			if handlerWithCtxCallCount != 1 {
-				t.Errorf("Expected ctx handler to be called 1 time. was called %d", handlerWithCtxCallCount)
-			}
-		})
-	})
-
 }
 
 func TestQueryHandlerReturnsError(t *testing.T) {
 	bus := New()
 
-	bus.AddHandler(func(query *testQuery) error {
+	bus.AddHandlerCtx(func(ctx context.Context, query *testQuery) error {
 		return errors.New("handler error")
 	})
 
-	err := bus.Dispatch(&testQuery{})
+	err := bus.DispatchCtx(context.Background(), &testQuery{})
 
 	if err == nil {
 		t.Fatal("Send query failed")
@@ -73,13 +40,13 @@ func TestQueryHandlerReturnsError(t *testing.T) {
 func TestQueryHandlerReturn(t *testing.T) {
 	bus := New()
 
-	bus.AddHandler(func(q *testQuery) error {
+	bus.AddHandlerCtx(func(ctx context.Context, q *testQuery) error {
 		q.Resp = "hello from handler"
 		return nil
 	})
 
 	query := &testQuery{}
-	err := bus.Dispatch(query)
+	err := bus.DispatchCtx(context.Background(), query)
 
 	if err != nil {
 		t.Fatal("Send query failed " + err.Error())
@@ -92,17 +59,17 @@ func TestEventListeners(t *testing.T) {
 	bus := New()
 	count := 0
 
-	bus.AddEventListener(func(query *testQuery) error {
+	bus.AddEventListener(func(ctx context.Context, query *testQuery) error {
 		count++
 		return nil
 	})
 
-	bus.AddEventListener(func(query *testQuery) error {
+	bus.AddEventListener(func(ctx context.Context, query *testQuery) error {
 		count += 10
 		return nil
 	})
 
-	err := bus.Publish(&testQuery{})
+	err := bus.Publish(context.Background(), &testQuery{})
 
 	if err != nil {
 		t.Fatal("Publish event failed " + err.Error())
