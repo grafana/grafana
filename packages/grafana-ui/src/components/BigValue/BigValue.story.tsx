3aceac22ac911f0817cb7a16ba1bb1fb16351e6b
Torkel Ödegaard <torkel@grafana.com>
2019-09-21 21:53:55 +0200

    minor progress

--- packages/grafana-ui/src/components/BigValue/BigValue.story.tsx
+++ packages/grafana-ui/src/components/BigValue/BigValue.story.tsx
@@ -30,12 +30,31 @@ BigValueStories.add('Classic singlestat', () => {
   });
 });
 
-BigValueStories.add('New singlestat', () => {
+BigValueStories.add('New Singlestat', () => {
   const { value, title } = getKnobs();
 
   return renderComponentWithTheme(BigValue2, {
     width: 400,
     height: 250,
+    value: {
+      text: value,
+      numeric: 5022,
+      title,
+    },
+    sparkline: {
+      minX: 0,
+      maxX: 5,
+      data: [[0, 10], [1, 20], [2, 15], [3, 25], [4, 5], [5, 10]],
+    },
+  });
+});
+
+BigValueStories.add('New Singlestat wide', () => {
+  const { value, title } = getKnobs();
+
+  return renderComponentWithTheme(BigValue2, {
+    width: 500,
+    height: 200,
     value: {
       text: value,
       numeric: 5022,
--- packages/grafana-ui/src/components/BigValue/BigValue2.tsx
+++ packages/grafana-ui/src/components/BigValue/BigValue2.tsx
@@ -30,22 +30,25 @@ export class BigValue2 extends PureComponent<Props> {
     const { height, width, value, onClick, className, theme, sparkline } = this.props;
 
     const baseColor = getColorFromHexRgbOrName(value.color || 'green', theme.type);
-    const sizes = getSizeCalculations(width, height);
-    const panelStyles = getPanelStyles(width, height, baseColor);
-    const valueStyles = getValueStyles(sizes);
-    const titleStyles = getTitleStyles(sizes);
+    const layout = calculateLayout(width, height);
+    const panelStyles = getPanelStyles(layout, baseColor);
+    const valueAndTitleContainerStyles = getValueAndTitleContainerStyles(layout);
+    const valueStyles = getValueStyles(layout);
+    const titleStyles = getTitleStyles(layout);
 
     return (
       <div className={className} style={panelStyles} onClick={onClick}>
-        {value.title && <div style={titleStyles}>{value.title}</div>}
-        <div style={valueStyles}>{value.text}</div>
-        {sparkline && this.renderChartElement(sizes)}
+        <div style={valueAndTitleContainerStyles}>
+          {value.title && <div style={titleStyles}>{value.title}</div>}
+          <div style={valueStyles}>{value.text}</div>
+        </div>
+        {sparkline && this.renderChartElement(layout)}
       </div>
     );
   }
 
-  renderChartElement(sizes: SizeCalculations) {
-    const { width, sparkline } = this.props;
+  renderChartElement(layout: LayoutResult) {
+    const { sparkline } = this.props;
 
     if (!sparkline) {
       return null;
@@ -56,30 +59,40 @@ export class BigValue2 extends PureComponent<Props> {
     });
 
     const lineStyle: any = {
-      stroke: '#EEE',
+      stroke: '#CCC',
       lineWidth: 2,
       shadowBlur: 7,
       shadowColor: '#333',
       shadowOffsetY: 7,
     };
 
-    const chartWidth = width - PANEL_PADDING * 2;
-    const chartHeight = sizes.chartHeight;
     const scales = {
       time: {
         type: 'time',
       },
     };
 
+    const chartStyles: CSSProperties = {
+      marginTop: `${CHART_TOP_MARGIN}`,
+    };
+
+    if (layout.type === LayoutType.ChartRight) {
+      chartStyles.position = 'absolute';
+      chartStyles.bottom = 0;
+      chartStyles.left = 0;
+      chartStyles.width = `${layout.chartHeight}px`;
+      chartStyles.height = `${layout.chartHeight}px`;
+    }
+
     return (
       <Chart
-        height={chartHeight}
-        width={chartWidth}
+        height={layout.chartHeight}
+        width={layout.chartWidth}
         data={data}
         animate={false}
         padding={[0, 0, 0, 0]}
         scale={scales}
-        style={{ marginTop: `${CHART_TOP_MARGIN}px` }}
+        style={chartStyles}
       >
         <Geom type="line" position="time*value" size={2} color="white" style={lineStyle} shape="smooth" />
       </Chart>
@@ -96,56 +109,89 @@ const LINE_HEIGHT = 1.2;
 const PANEL_PADDING = 16;
 const CHART_TOP_MARGIN = 8;
 
-interface SizeCalculations {
+interface LayoutResult {
   titleFontSize: number;
   valueFontSize: number;
   chartHeight: number;
+  chartWidth: number;
+  type: LayoutType;
+  width: number;
+  height: number;
 }
 
-export function getSizeCalculations(width: number, height: number): SizeCalculations {
+enum LayoutType {
+  ChartBelow,
+  ChartRight,
+}
+
+export function calculateLayout(width: number, height: number): LayoutResult {
+  let type = LayoutType.ChartBelow;
+
   const valueFontSize = Math.min(Math.max(height * VALUE_HEIGHT_RATIO, MIN_VALUE_FONT_SIZE), MAX_VALUE_FONT_SIZE);
   const titleFontSize = Math.max(valueFontSize * TITLE_VALUE_RATIO, MIN_TITLE_FONT_SIZE);
-  const chartHeight =
+  let chartHeight =
     height - valueFontSize * LINE_HEIGHT - titleFontSize * LINE_HEIGHT - PANEL_PADDING * 2 - CHART_TOP_MARGIN;
+  let chartWidth = width - PANEL_PADDING * 2;
+
+  if (width / height > 1.8) {
+    type = LayoutType.ChartRight;
+    chartHeight = height / 2;
+    chartWidth = width;
+  }
 
   return {
     valueFontSize,
     titleFontSize,
     chartHeight,
+    chartWidth,
+    type,
+    width,
+    height,
   };
 }
 
-export function getValueFontSize(width: number, height: number): number {
-  const byWidth = width * 0.2;
-
-  return Math.min(Math.max(byWidth, MIN_VALUE_FONT_SIZE), MAX_VALUE_FONT_SIZE);
-}
-
-export function getTitleStyles(sizes: SizeCalculations) {
-  const titleStyles: CSSProperties = {
-    fontSize: `${sizes.titleFontSize}px`,
+export function getTitleStyles(layout: LayoutResult) {
+  const styles: CSSProperties = {
+    fontSize: `${layout.titleFontSize}px`,
     textShadow: '#333 1px 1px 5px',
     color: '#EEE',
   };
 
-  return titleStyles;
+  return styles;
 }
 
-export function getValueStyles(sizes: SizeCalculations) {
-  const valueStyles: CSSProperties = {
-    fontSize: `${sizes.valueFontSize}px`,
-    color: 'white',
+export function getValueStyles(layout: LayoutResult) {
+  const styles: CSSProperties = {
+    fontSize: `${layout.valueFontSize}px`,
+    color: '#EEE',
     textShadow: '#333 1px 1px 5px',
     lineHeight: LINE_HEIGHT,
   };
 
-  return valueStyles;
+  return styles;
 }
 
-export function getPanelStyles(width: number, height: number, baseColor: string) {
+export function getValueAndTitleContainerStyles(layout: LayoutResult) {
+  const styles: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    justifyContent: 'center',
+  };
+
+  if (layout.type === LayoutType.ChartRight) {
+    styles.flexDirection = 'row';
+    styles.justifyContent = 'space-between';
+    styles.alignItems = 'center';
+  }
+
+  return styles;
+}
+
+export function getPanelStyles(layout: LayoutResult, baseColor: string) {
   const bgColor2 = tinycolor(baseColor)
     .darken(15)
-    .spin(10)
+    .spin(8)
     .toRgbString();
   const bgColor3 = tinycolor(baseColor)
     .darken(5)
@@ -153,12 +199,18 @@ export function getPanelStyles(width: number, height: number, baseColor: string)
     .toRgbString();
 
   const panelStyles: CSSProperties = {
-    width: `${width}px`,
-    height: `${height}px`,
+    width: `${layout.width}px`,
+    height: `${layout.height}px`,
     padding: `${PANEL_PADDING}px`,
     borderRadius: '3px',
     background: `linear-gradient(120deg, ${bgColor2}, ${bgColor3})`,
+    position: 'relative',
   };
+
+  if (layout.type === LayoutType.ChartRight) {
+    panelStyles.display = 'flex';
+    panelStyles.alignItems = 'center';
+  }
 
   return panelStyles;
 }
