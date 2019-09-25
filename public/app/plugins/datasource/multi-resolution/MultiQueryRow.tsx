802ff1b0d309286373803e8163fee9bc4fe2e076
ryan <ryantxu@gmail.com>
2019-09-25 09:07:33 -0700

    multi-resolution building

---
+++ public/app/plugins/datasource/multi-resolution/MultiQueryRow.tsx
@@ -0,0 +1,90 @@
+// Libraries
+import React, { PureComponent } from 'react';
+
+// Types
+import { PanelData, DataQuery } from '@grafana/ui';
+import { QueriesForResolution } from './types';
+import { PanelModel, DashboardModel } from 'app/features/dashboard/state';
+import { QueryEditorRows } from 'app/features/dashboard/panel_editor/QueryEditorRows';
+
+interface Props {
+  panel: PanelModel;
+  dashboard: DashboardModel;
+  data: PanelData;
+  value: QueriesForResolution;
+  onScrollBottom: () => void;
+  onChange: (value: QueriesForResolution) => void;
+}
+
+type State = {
+  isCollapsed: boolean;
+};
+
+export class MultiQueryRow extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isCollapsed: false,
+    };
+  }
+
+  toggleCollapse = () => {
+    this.setState({
+      isCollapsed: !this.state.isCollapsed,
+    });
+  };
+
+  render() {
+    const { value, data, dashboard, panel, onScrollBottom } = this.props;
+    const isCollapsed = false;
+
+    return (
+      <div className="query-editor-row">
+        <div className="query-editor-row__header">
+          <div onClick={this.toggleCollapse}>
+            {isCollapsed && <i className="fa fa-caret-right" />}
+            {!isCollapsed && <i className="fa fa-caret-down" />}
+            <span>&nbsp;</span>
+          </div>
+          <div>
+            <input type="text" className="gf-form query-editor-row__ref-id" value="XXX" />
+          </div>
+          <div
+            className="query-editor-row__collapsed-text"
+            onClick={() => {
+              console.log('toggle');
+            }}
+          >
+            {isCollapsed && <div>XXXXX</div>}
+          </div>
+          <div className="query-editor-row__actions">
+            <button
+              className="query-editor-row__action"
+              onClick={() => {
+                console.log('remove');
+              }}
+              title="Remove"
+            >
+              <i className="fa fa-fw fa-trash" />
+            </button>
+          </div>
+        </div>
+        {!isCollapsed && (
+          <div className="query-editor-row__body gf-form-query">
+            <QueryEditorRows
+              queries={value.targets}
+              datasource={null}
+              onChangeQueries={(queries: DataQuery[]) => {
+                console.log('CHANGE', queries);
+              }}
+              onScrollBottom={onScrollBottom}
+              panel={panel}
+              dashboard={dashboard}
+              data={data}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
