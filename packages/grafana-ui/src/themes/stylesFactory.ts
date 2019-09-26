1cb7a522d3049cfec57e15adce74bac2dbd002d1
Dominik Prokop <dominik.prokop@grafana.com>
2019-09-26 11:22:20 +0200

    Simplify styles creator signature

--- packages/grafana-ui/src/themes/stylesFactory.ts
+++ packages/grafana-ui/src/themes/stylesFactory.ts
@@ -3,11 +3,10 @@ import { KeyValue } from '@grafana/data';
 
 /**
  *  Creates memoized version of styles creator
- * @param stylesCreator function accepting component's props and state and returning object with class names
+ * @param stylesCreator function accepting dependencies based on which styles are created
  */
-
-export function stylesFactory<TProps, TState, TResult extends KeyValue<string>>(
-  stylesCreator: (props: TProps, state?: TState) => TResult
-): (props: TProps, state?: TState) => TResult {
+export function stylesFactory<TDeps, TResult extends KeyValue<string>>(
+  stylesCreator: (deps: TDeps) => TResult
+): (deps: TDeps) => TResult {
   return memoizeOne(stylesCreator);
 }
