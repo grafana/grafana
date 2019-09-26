f4aacbd91aa72dafd4311e9af8f992dee33ee4b0
Dominik Prokop <dominik.prokop@grafana.com>
2019-09-26 23:57:13 +0200

    update trigger

--- .github/actions/deploy-storybook/index.js
+++ .github/actions/deploy-storybook/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  console.log(github.context.payload);
+  console.log(github.context.payload.pull_request);
 } catch (error) {
   core.setFailed(error.message);
 }
