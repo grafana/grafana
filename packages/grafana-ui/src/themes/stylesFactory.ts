212212e60612f346e262ccfaefa86bdd52ae2714
Dominik Prokop <dominik.prokop@grafana.com>
2019-09-26 12:23:20 +0200

    Update typing

--- packages/grafana-ui/src/themes/stylesFactory.ts
+++ packages/grafana-ui/src/themes/stylesFactory.ts
@@ -1,12 +1,12 @@
 import memoizeOne from 'memoize-one';
-import { KeyValue } from '@grafana/data';
+// import { KeyValue } from '@grafana/data';
 
 /**
  *  Creates memoized version of styles creator
  * @param stylesCreator function accepting dependencies based on which styles are created
  */
-export function stylesFactory<TDeps, TResult extends KeyValue<string>>(
-  stylesCreator: (deps?: TDeps) => TResult
-): (deps?: TDeps) => TResult {
+export function stylesFactory<ResultFn extends (this: any, ...newArgs: any[]) => ReturnType<ResultFn>>(
+  stylesCreator: ResultFn
+) {
   return memoizeOne(stylesCreator);
 }
