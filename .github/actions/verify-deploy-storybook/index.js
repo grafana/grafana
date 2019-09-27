5bd4416172506c1e3eaec59f80135db238276d87
Dominik Prokop <dominik.prokop@grafana.com>
2019-09-27 11:26:16 +0200

    see how workspace dir loks like

--- .github/actions/verify-deploy-storybook/index.js
+++ .github/actions/verify-deploy-storybook/index.js
@@ -6,10 +6,8 @@ const DEPLOY_STORYBOOK_LABEL = 'deploy-storybook';
 try {
   const pullRequest = github.context.payload.pull_request;
   let shouldDeploy = false;
-  console.log(pullRequest);
-  console.log(pullRequest.labels);
   if(pullRequest && pullRequest.labels) {
-     shouldDeploy = !!(pullRequest.labels.find(l => l === DEPLOY_STORYBOOK_LABEL))
+     shouldDeploy = !!(pullRequest.labels.find(l => l.name === DEPLOY_STORYBOOK_LABEL))
   }
   core.setOutput('shouldDeploy', shouldDeploy.toString());
 } catch (error) {
