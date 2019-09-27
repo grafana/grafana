1b350ed3e674a411f7c1482e384b6712dcc08b24
Dominik Prokop <dominik.prokop@grafana.com>
2019-09-27 10:47:30 +0200

    contitionaly build storybook

--- .github/actions/verify-deploy-storybook/index.js
+++ .github/actions/verify-deploy-storybook/index.js
@@ -4,10 +4,12 @@ const github = require('@actions/github');
 const DEPLOY_STORYBOOK_LABEL = 'deploy-storybook';
 
 try {
-
   const pullRequest = github.context.payload.pull_request;
-  if(o)
-  console.log(github.context.payload.pull_request);
+  let shouldDeploy = false;
+  if(pullRequest && pullRequest.labels) {
+     shouldDeploy = !!(pullRequest.labels.find(l => l === DEPLOY_STORYBOOK_LABEL))
+  }
+  core.setOutput('shouldDeploy', shouldDeploy.toString());
 } catch (error) {
   core.setFailed(error.message);
 }
--- .github/actions/verify-storybook-deploy/index.js
+++ .github/actions/verify-storybook-deploy/index.js
@@ -1,12 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const DEPLOY_STORYBOOK_LABEL = 'deploy-storybook';
-
 try {
-
-  const pullRequest = github.context.payload.pull_request;
-  if(o)
+  core.setOutput
   console.log(github.context.payload.pull_request);
 } catch (error) {
   core.setFailed(error.message);
