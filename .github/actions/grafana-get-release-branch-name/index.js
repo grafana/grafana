8870ec588f9d6c555d2df772dc4666c77061b4a3
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-10-02 13:11:27 +0200

    WIP: Adds first milestone parser action

---
+++ .github/actions/grafana-get-release-branch-name/index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const milestone = github.context.payload.pull_request.milestone;
+
+  if (!milestone) {
+    core.setFailed('This pull request has no milestone assigned! Please assign an open milestone.');
+  }
+
+  if (milestone.closed) {
+    core.setFailed('Milestone ' + milestone.title + ' is closed! Please assign an open milestone.');
+  }
+
+  const versionPattern = /^(\d+)(?:\.(\d+))?(?:\.(\d+))?(?:-([0-9A-Za-z\.]+))?/;
+  const match = versionPattern.exec(milestone.title);
+  if (!match) {
+    core.setFailed('Could not parse Milestone title ' + milestone.title);
+  }
+
+  const major = Number(match[1]);
+  const minor = Number(match[2] || 0);
+  const patch = Number(match[3] || 0);
+  const meta = match[4];
+
+  core.setOutput('major', major.toString());
+  core.setOutput('minor', minor.toString());
+  core.setOutput('patch', patch.toString());
+  core.setOutput('meta', meta.toString());
+} catch (error) {
+  core.setFailed(error.message);
+}
