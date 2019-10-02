ec8661e73c669ada415f41cf1b9447594e989813
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-10-02 16:41:12 +0200

    TagsInput component

---
+++ packages/grafana-ui/src/components/TagsInput/TagItem.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import { css, cx } from 'emotion';
+import { getTagColorsFromName } from '../../../../../public/app/core/utils/tags';
+
+interface Props {
+  name: string;
+
+  onRemove: (tag: string) => void;
+}
+
+export const TagItem: FC<Props> = ({ name, onRemove }) => {
+  const { color, borderColor } = getTagColorsFromName(name);
+
+  const itemStyle = css`
+    background-color: ${color};
+    border: 1px solid ${borderColor};
+    border-radius: 3px;
+    padding: 3px 6px;
+    margin: 3px;
+    white-space: nowrap;
+    text-shadow: none;
+    font-weight: 500;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+  `;
+
+  const nameStyle = css`
+    margin-right: 3px;
+  `;
+
+  const removeStyle = cx([
+    'fa fa-times',
+    css`
+      cursor: pointer;
+    `,
+  ]);
+
+  return (
+    <div className={itemStyle}>
+      <span className={nameStyle}>{name}</span>
+      <i className={removeStyle} onClick={() => onRemove(name)} />
+    </div>
+  );
+};
---
+++ packages/grafana-ui/src/components/TagsInput/TagsInput.tsx
@@ -0,0 +1,104 @@
+import React, { ChangeEvent, KeyboardEvent, PureComponent } from 'react';
+import { css, cx } from 'emotion';
+import { Button, Input } from '..';
+import { TagItem } from './TagItem';
+
+interface Props {
+  tags?: string[];
+  width?: number;
+
+  onChange: (tags: string[]) => void;
+}
+
+interface State {
+  newTag: string;
+  tags: string[];
+}
+
+export class TagsInput extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      newTag: '',
+      tags: this.props.tags || [],
+    };
+  }
+
+  onNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTag: event.target.value,
+    });
+  };
+
+  onRemove = (tagToRemove: string) => {
+    this.setState(
+      (prevState: State) => ({
+        ...prevState,
+        tags: prevState.tags.filter(tag => tagToRemove !== tag),
+      }),
+      () => this.onChange()
+    );
+  };
+
+  onAdd = () => {
+    if (this.state.newTag === '') {
+      return;
+    }
+
+    this.setState(
+      (prevState: State) => ({
+        ...prevState,
+        tags: [...prevState.tags, prevState.newTag],
+        newTag: '',
+      }),
+      () => this.onChange()
+    );
+  };
+
+  onKeyboardAdd = (event: KeyboardEvent) => {
+    if (event.key === 'Enter' && this.state.newTag !== '') {
+      this.setState(
+        (prevState: State) => ({
+          ...prevState,
+          tags: [...prevState.tags, prevState.newTag],
+          newTag: '',
+        }),
+        () => this.onChange()
+      );
+    }
+  };
+
+  onChange = () => {
+    this.props.onChange(this.state.tags);
+  };
+
+  render() {
+    const { tags, newTag } = this.state;
+
+    const containerStyle = cx(['width-20']);
+    const tagsCloudStyle = css`
+      display: flex;
+      justify-content: flex-start;
+      margin-bottom: 8px;
+      flex-wrap: wrap;
+    `;
+
+    return (
+      <div className={containerStyle}>
+        <div className="gf-form-inline">
+          <Input placeholder="Add name" onChange={this.onNameChange} value={newTag} onKeyUp={this.onKeyboardAdd} />
+          <Button onClick={this.onAdd} variant="primary" size="md">
+            Add
+          </Button>
+        </div>
+        <div className={tagsCloudStyle}>
+          {tags &&
+            tags.map((tag: string, index: number) => {
+              return <TagItem key={`${tag}-${index}`} name={tag} onRemove={this.onRemove} />;
+            })}
+        </div>
+      </div>
+    );
+  }
+}
