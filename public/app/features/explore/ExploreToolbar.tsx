33f8b36b44e43c37a1c745dd09177510230facea
Łukasz Siatka <lukasz.siatka@grafana.com>
2019-10-07 23:28:16 +0200

    updated toolbar - added media query for tail buttons

--- public/app/features/explore/ExploreToolbar.tsx
+++ public/app/features/explore/ExploreToolbar.tsx
@@ -44,6 +44,9 @@ const getStyles = memoizeOne(() => {
   return {
     liveTailButtons: css`
       margin-left: 10px;
+      @media (max-width: 1110px) {
+        margin-left: 4px;
+      }
     `,
   };
 });
