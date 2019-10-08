ce5115050865958c72c399bf780a5a016495cbc3
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-10-08 14:43:12 +0200

    new file for react directive for testing

--- public/app/features/datasources/settings/HttpSettingsCtrl.ts
+++ public/app/features/datasources/settings/HttpSettingsCtrl.ts
@@ -8,7 +8,7 @@ coreModule.directive('datasourceHttpSettings', () => {
       suggestUrl: '@',
       noDirectAccess: '@',
     },
-    templateUrl: 'public/app/features/datasources/partials/http_settings.html',
+    templateUrl: 'public/app/features/datasources/partials/http_settings_next.html',
     link: {
       pre: ($scope: any, elem, attrs) => {
         // do not show access option if direct access is disabled
