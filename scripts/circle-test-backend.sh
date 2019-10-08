d303029644d2a6a18a6656de3d611abf7e6a24e7
Kyle Brandt <kyle@grafana.com>
2019-10-08 11:41:08 -0400

    circleci: run tests on sdk

--- scripts/circle-test-backend.sh
+++ scripts/circle-test-backend.sh
@@ -3,11 +3,20 @@
 # shellcheck source=./scripts/helpers/exit-if-fail.sh
 source "$(dirname "$0")/helpers/exit-if-fail.sh"
 
+# use vendor folder for packages
+export GOFLAGS=-mod=vendor
+
 echo "building backend with install to cache pkgs"
 exit_if_fail time go install ./pkg/cmd/grafana-server
 
-echo "running go test"
+echo "running go test on grafana module"
 set -e
 time for d in $(go list ./pkg/...); do
+  exit_if_fail go test -tags=integration -covermode=atomic "$d"
+done
+
+echo "running go test on sdk module"
+cd pkg/plugins/sdk
+time for d in $(go list ./...); do
   exit_if_fail go test -tags=integration -covermode=atomic "$d"
 done
