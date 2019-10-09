4eb2689f0cdeab761cb237476006087d4c6d189a
Erik Sundell <erik.sundell87@gmail.com>
2019-10-09 17:12:21 +0200

    Fix merge conflict

--- pkg/tsdb/cloudwatch/types.go
+++ pkg/tsdb/cloudwatch/types.go
@@ -1,20 +1,22 @@
 package cloudwatch
 
+import (
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+)
+
 type CloudWatchQuery struct {
 	RefId              string
 	Region             string
-	Id                 string
 	Namespace          string
 	MetricName         string
+	Dimensions         []*cloudwatch.Dimension
 	Statistics         []*string
-	QueryType          string
-	Expression         string
-	ReturnData         bool
-	Dimensions         map[string][]string
 	ExtendedStatistics []*string
 	Period             int
 	Alias              string
+	Id                 string
 	Identifier         string
+	Expression         string
+	ReturnData         bool
 	HighResolution     bool
-	SearchExpressions  []string
 }
