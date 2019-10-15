ab06502b71a274b7dd705eb9dd763b2ca264ef45
Ivana Huckova <ivana.huckova@gmail.com>
2019-10-15 16:29:30 +0200

    Test: Add renders properly test

--- public/app/features/explore/TimeSyncButton.test.tsx
+++ public/app/features/explore/TimeSyncButton.test.tsx
@@ -2,24 +2,22 @@ import React from 'react';
 import { TimeSyncButton } from './TimeSyncButton';
 import { mount } from 'enzyme';
 
-const setup = (isSynced: boolean) => {
+const setup = (isSynced: boolean = true) => {
   const onClick = () => {};
   return mount(<TimeSyncButton onClick={onClick} isSynced={isSynced} />);
 };
 
 describe('TimeSyncButton', () => {
+  it('should render component', () => {
+    const wrapper = setup(true);
+    expect(wrapper).toMatchSnapshot();
+  });
   it('should change style when synced', () => {
-    expect(
-      setup(true)
-        .find('button')
-        .hasClass('css-14r9fpj-timePickerSynced')
-    ).toEqual(true);
+    const wrapper = setup(true);
+    expect(wrapper.find('button').hasClass('css-14r9fpj-timePickerSynced')).toEqual(true);
   });
   it('should not change style when not synced', () => {
-    expect(
-      setup(false)
-        .find('button')
-        .hasClass('css-14r9fpj-timePickerSynced')
-    ).toEqual(false);
+    const wrapper = setup(false);
+    expect(wrapper.find('button').hasClass('css-14r9fpj-timePickerSynced')).toEqual(false);
   });
 });
