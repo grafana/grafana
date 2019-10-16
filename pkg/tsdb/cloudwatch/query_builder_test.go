b12490b723359fee61e0cf74470978288c75b0c7
Erik Sundell <erik.sundell87@gmail.com>
2019-10-16 19:00:16 +0200

    Refactoring. Add more tests

--- pkg/tsdb/cloudwatch/query_builder_test.go
+++ pkg/tsdb/cloudwatch/query_builder_test.go
@@ -18,7 +18,7 @@ func TestCloudWatchQueryBuilder(t *testing.T) {
 			},
 		}
 
-		Convey("building GetMetricDataQueries", func() {
+		Convey("buildMetricDataQueries", func() {
 			Convey("and one GetMetricDataInput is generated for each query statistic", func() {
 				dimensions := make(map[string][]string)
 				dimensions["InstanceId"] = []string{"i-12345678"}
@@ -81,7 +81,7 @@ func TestCloudWatchQueryBuilder(t *testing.T) {
 					So(res[0].MetricStat, ShouldNotBeNil)
 				})
 
-				Convey("will be not used when expression is set in the client", func() {
+				Convey("will not used when expression is set in the client", func() {
 					dimensions := make(map[string][]string)
 					dimensions["InstanceId"] = []string{"i-12345678"}
 					query := &CloudWatchQuery{
@@ -102,68 +102,133 @@ func TestCloudWatchQueryBuilder(t *testing.T) {
 					So(res[0].MetricStat, ShouldBeNil)
 				})
 
+				Convey("will be not used when a dimension key has more than one value", func() {
+					dimensions := make(map[string][]string)
+					dimensions["InstanceId"] = []string{"i-12345678", "i-34562312"}
+					query := &CloudWatchQuery{
+						RefId:      "A",
+						Region:     "us-east-1",
+						Expression: "",
+						Statistics: []*string{aws.String("Average")},
+						Period:     300,
+						Id:         "id1",
+						Identifier: "id1",
+						Dimensions: dimensions,
+					}
+
+					res, err := e.buildMetricDataQueries(query)
+					So(err, ShouldBeNil)
+					So(len(res), ShouldEqual, 1)
+					So(res[0].Expression, ShouldNotBeNil)
+					So(res[0].MetricStat, ShouldBeNil)
+				})
+
+				Convey("will be not used when a dimension key has a value that is equals to *", func() {
+					dimensions := make(map[string][]string)
+					dimensions["InstanceId"] = []string{"i-12345678", "*"}
+					query := &CloudWatchQuery{
+						RefId:      "A",
+						Region:     "us-east-1",
+						Expression: "",
+						Statistics: []*string{aws.String("Average")},
+						Period:     300,
+						Id:         "id1",
+						Identifier: "id1",
+						Dimensions: dimensions,
+					}
+
+					res, err := e.buildMetricDataQueries(query)
+					So(err, ShouldBeNil)
+					So(len(res), ShouldEqual, 1)
+					So(res[0].Expression, ShouldNotBeNil)
+					So(res[0].MetricStat, ShouldBeNil)
+				})
 			})
 
-			Convey("and query expression is being generated server side", func() {
-				Convey("and query has three dimension values for a given dimension key", func() {
-					dimensions := make(map[string][]string)
-					dimensions["LoadBalancer"] = []string{"lb1", "lb2", "lb3"}
-					query := &CloudWatchQuery{
-						Namespace:  "AWS/EC2",
-						MetricName: "CPUUtilization",
-						Dimensions: dimensions,
-						Statistics: []*string{aws.String("Average")},
-						Period:     300,
-						Identifier: "id1",
-						Expression: "",
-					}
+		})
 
-					res, err := e.buildMetricDataQueries(query)
-					So(err, ShouldBeNil)
-					So(len(res), ShouldEqual, 1)
-					So(len(res), ShouldEqual, 1)
-					So(*res[0].Expression, ShouldEqual, "SEARCH('{AWS/EC2,LoadBalancer} MetricName=\"CPUUtilization\" LoadBalancer=(\"lb1\" OR \"lb2\" OR \"lb3\")', 'Average', 300)")
-				})
+		Convey("buildSearchExpression", func() {
+			Convey("and query has three dimension values for a given dimension key", func() {
+				dimensions := make(map[string][]string)
+				dimensions["LoadBalancer"] = []string{"lb1", "lb2", "lb3"}
+				query := &CloudWatchQuery{
+					Namespace:  "AWS/EC2",
+					MetricName: "CPUUtilization",
+					Dimensions: dimensions,
+					Period:     300,
+					Identifier: "id1",
+					Expression: "",
+				}
 
-				Convey("and query has three dimension values for two given dimension keys", func() {
-					dimensions := make(map[string][]string)
-					dimensions["LoadBalancer"] = []string{"lb1", "lb2", "lb3"}
-					dimensions["InstanceId"] = []string{"i-123", "i-456", "i-789"}
-					query := &CloudWatchQuery{
-						Namespace:  "AWS/EC2",
-						MetricName: "CPUUtilization",
-						Dimensions: dimensions,
-						Statistics: []*string{aws.String("Average")},
-						Period:     300,
-						Identifier: "id1",
-						Expression: "",
-					}
+				res := buildSearchExpression(query, "Average")
+				So(res, ShouldEqual, "SEARCH('{AWS/EC2,LoadBalancer} MetricName=\"CPUUtilization\" LoadBalancer=(\"lb1\" OR \"lb2\" OR \"lb3\")', 'Average', 300)")
+			})
 
-					res, err := e.buildMetricDataQueries(query)
-					So(err, ShouldBeNil)
-					So(len(res), ShouldEqual, 1)
-					So(*res[0].Expression, ShouldEqual, "SEARCH('{AWS/EC2,LoadBalancer,InstanceId} MetricName=\"CPUUtilization\" LoadBalancer=(\"lb1\" OR \"lb2\" OR \"lb3\") AND InstanceId=(\"i-123\" OR \"i-456\" OR \"i-789\")', 'Average', 300)")
-				})
+			Convey("and query has three dimension values for two given dimension keys", func() {
+				dimensions := make(map[string][]string)
+				dimensions["LoadBalancer"] = []string{"lb1", "lb2", "lb3"}
+				dimensions["InstanceId"] = []string{"i-123", "i-456", "i-789"}
+				query := &CloudWatchQuery{
+					Namespace:  "AWS/EC2",
+					MetricName: "CPUUtilization",
+					Dimensions: dimensions,
+					Period:     300,
+					Identifier: "id1",
+					Expression: "",
+				}
 
-				Convey("and no AND/OR operators were added if a star was used for dimension value", func() {
-					dimensions := make(map[string][]string)
-					dimensions["LoadBalancer"] = []string{"*"}
-					query := &CloudWatchQuery{
-						Namespace:  "AWS/EC2",
-						MetricName: "CPUUtilization",
-						Dimensions: dimensions,
-						Statistics: []*string{aws.String("Average")},
-						Period:     300,
-						Identifier: "id1",
-						Expression: "",
-					}
+				res := buildSearchExpression(query, "Average")
+				So(res, ShouldEqual, "SEARCH('{AWS/EC2,LoadBalancer,InstanceId} MetricName=\"CPUUtilization\" LoadBalancer=(\"lb1\" OR \"lb2\" OR \"lb3\") AND InstanceId=(\"i-123\" OR \"i-456\" OR \"i-789\")', 'Average', 300)")
+			})
 
-					res, err := e.buildMetricDataQueries(query)
-					So(err, ShouldBeNil)
-					So(len(res), ShouldEqual, 1)
-					So(*res[0].Expression, ShouldNotContainSubstring, "AND")
-					So(*res[0].Expression, ShouldNotContainSubstring, "OR")
-				})
+			Convey("and no AND/OR operators were added if a star was used for dimension value", func() {
+				dimensions := make(map[string][]string)
+				dimensions["LoadBalancer"] = []string{"*"}
+				query := &CloudWatchQuery{
+					Namespace:  "AWS/EC2",
+					MetricName: "CPUUtilization",
+					Dimensions: dimensions,
+					Period:     300,
+					Identifier: "id1",
+					Expression: "",
+				}
+
+				res := buildSearchExpression(query, "Average")
+				So(res, ShouldNotContainSubstring, "AND")
+				So(res, ShouldNotContainSubstring, "OR")
+			})
+
+			Convey("and query has one dimension key with a * value", func() {
+				dimensions := make(map[string][]string)
+				dimensions["LoadBalancer"] = []string{"*"}
+				query := &CloudWatchQuery{
+					Namespace:  "AWS/EC2",
+					MetricName: "CPUUtilization",
+					Dimensions: dimensions,
+					Period:     300,
+					Identifier: "id1",
+					Expression: "",
+				}
+
+				res := buildSearchExpression(query, "Average")
+				So(res, ShouldEqual, "SEARCH('{AWS/EC2,LoadBalancer} MetricName=\"CPUUtilization\" ', 'Average', 300)")
+			})
+
+			Convey("and query has three dimension values for two given dimension keys, and one value is a star", func() {
+				dimensions := make(map[string][]string)
+				dimensions["LoadBalancer"] = []string{"lb1", "lb2", "lb3"}
+				dimensions["InstanceId"] = []string{"i-123", "*", "i-789"}
+				query := &CloudWatchQuery{
+					Namespace:  "AWS/EC2",
+					MetricName: "CPUUtilization",
+					Dimensions: dimensions,
+					Period:     300,
+					Identifier: "id1",
+					Expression: "",
+				}
+
+				res := buildSearchExpression(query, "Average")
+				So(res, ShouldEqual, "SEARCH('{AWS/EC2,LoadBalancer,InstanceId} MetricName=\"CPUUtilization\" LoadBalancer=(\"lb1\" OR \"lb2\" OR \"lb3\")', 'Average', 300)")
 			})
 		})
 	})
