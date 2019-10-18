caae74a09894f1698c25ee920a830fada8e0bfd6
Leonard Gram <leo@xlson.com>
2019-10-18 15:59:40 +0200

    Licensing: fixes broken tests due to new Licensing service dependency

--- pkg/extensions/main.go
+++ pkg/extensions/main.go
@@ -6,6 +6,7 @@ import (
 
 	_ "github.com/crewjam/saml"
 	_ "github.com/gobwas/glob"
+	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/registry"
 	_ "github.com/jung-kurt/gofpdf"
 	_ "github.com/robfig/cron"
@@ -14,18 +15,8 @@ import (
 	_ "gopkg.in/square/go-jose.v2"
 )
 
-type ossLicensingService struct{}
-
-func (ossLicensingService) Init() error {
-	return nil
-}
-
-func (ossLicensingService) HasValidLicense() bool {
-	return false
-}
-
 func init() {
-	registry.RegisterService(&ossLicensingService{})
+	registry.RegisterService(&models.OSSLicensingService{})
 }
 
 var IsEnterprise bool = false
--- pkg/models/licensing.go
+++ pkg/models/licensing.go
@@ -3,3 +3,13 @@ package models
 type Licensing interface {
 	HasValidLicense() bool
 }
+
+type OSSLicensingService struct{}
+
+func (OSSLicensingService) Init() error {
+	return nil
+}
+
+func (OSSLicensingService) HasValidLicense() bool {
+	return false
+}
