b8475c1d2de9b49156de390baa2d02eda7c039f8
Dominik Prokop <dominik.prokop@grafana.com>
2019-10-18 13:49:14 +0200

    Expose Legend class name via getFormStyles

--- packages/grafana-ui/src/components/Forms/getFormStyles.ts
+++ packages/grafana-ui/src/components/Forms/getFormStyles.ts
@@ -1,9 +1,11 @@
 import { stylesFactory } from '../../themes';
 import { GrafanaTheme } from '../../types';
 import { getLabelStyles } from './Label';
+import { getLegendStyles } from './Legend';
 
 export const getFormStyles = stylesFactory((theme: GrafanaTheme) => {
   return {
     ...getLabelStyles(theme),
+    ...getLegendStyles(theme),
   };
 });
