3d5786e48548a0ef1bdfa29a3688588e95f61183
Andrej Ocenas <mr.ocenas@gmail.com>
2019-10-23 21:41:19 +0200

    Add test for Loki config

--- public/app/plugins/datasource/elasticsearch/configuration/mocks.ts
+++ public/app/plugins/datasource/elasticsearch/configuration/mocks.ts
@@ -1,32 +1,15 @@
 import { DataSourceSettings } from '@grafana/ui';
 import { ElasticsearchOptions } from '../types';
+import { createDatasourceSettings } from '../../../../features/datasources/mocks';
 
 export function createDefaultConfigOptions(): DataSourceSettings<ElasticsearchOptions> {
-  return {
-    id: 0,
-    orgId: 0,
-    name: 'elastic-test',
-    typeLogoUrl: '',
-    type: 'datasource',
-    access: 'server',
-    url: 'http://localhost',
-    password: '',
-    user: '',
-    database: '',
-    basicAuth: false,
-    basicAuthPassword: '',
-    basicAuthUser: '',
-    isDefault: false,
-    jsonData: {
-      timeField: '@time',
-      esVersion: 70,
-      interval: 'Hourly',
-      timeInterval: '10s',
-      maxConcurrentShardRequests: 300,
-      logMessageField: 'test.message',
-      logLevelField: 'test.level',
-    },
-    readOnly: false,
-    withCredentials: false,
-  };
+  return createDatasourceSettings<ElasticsearchOptions>({
+    timeField: '@time',
+    esVersion: 70,
+    interval: 'Hourly',
+    timeInterval: '10s',
+    maxConcurrentShardRequests: 300,
+    logMessageField: 'test.message',
+    logLevelField: 'test.level',
+  });
 }
