81bb9465b75dd703e1256b2e7a6f94dc52414125
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-10-24 16:18:20 +0200

    remove not used export

--- packages/grafana-ui/src/types/index.ts
+++ packages/grafana-ui/src/types/index.ts
@@ -4,7 +4,6 @@ export * from './app';
 export * from './datasource';
 export * from './theme';
 export * from './input';
-export * from '../components/Button/types';
 
 import * as PanelEvents from './events';
 export { PanelEvents };
