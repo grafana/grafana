f77478bbfe2da3122d23970aee188a779f40bb87
Arve Knudsen <arve.knudsen@gmail.com>
2019-10-28 08:49:01 +0100

    Fix linting errors
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/plugins/datasource_plugin_test.go
+++ pkg/plugins/datasource_plugin_test.go
@@ -6,20 +6,23 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLoadDatasourceVersion(t *testing.T) {
 	t.Run("If plugin version is not set, it should be treated as plugin version one", func(t *testing.T) {
 		pluginJSON, _ := json.Marshal(DataSourcePlugin{})
 		datasourcePlugin := DataSourcePlugin{}
-		(&datasourcePlugin).Load(json.NewDecoder(bytes.NewReader(pluginJSON)), "/tmp")
+		err := (&datasourcePlugin).Load(json.NewDecoder(bytes.NewReader(pluginJSON)), "/tmp")
+		require.NoError(t, err)
 		assert.True(t, datasourcePlugin.isVersionOne())
 	})
 
 	t.Run("If plugin version is set to one, it should be treated as plugin version one", func(t *testing.T) {
 		pluginJSON, _ := json.Marshal(DataSourcePlugin{SDK: false})
 		datasourcePlugin := DataSourcePlugin{}
-		(&datasourcePlugin).Load(json.NewDecoder(bytes.NewReader(pluginJSON)), "/tmp")
+		err := (&datasourcePlugin).Load(json.NewDecoder(bytes.NewReader(pluginJSON)), "/tmp")
+		require.NoError(t, err)
 		assert.True(t, datasourcePlugin.isVersionOne())
 		assert.False(t, datasourcePlugin.SDK)
 	})
@@ -27,7 +30,8 @@ func TestLoadDatasourceVersion(t *testing.T) {
 	t.Run("If plugin version is set to two, it should not be treated as plugin version one", func(t *testing.T) {
 		pluginJSON, _ := json.Marshal(DataSourcePlugin{SDK: true})
 		datasourcePlugin := DataSourcePlugin{}
-		(&datasourcePlugin).Load(json.NewDecoder(bytes.NewReader(pluginJSON)), "/tmp")
+		err := (&datasourcePlugin).Load(json.NewDecoder(bytes.NewReader(pluginJSON)), "/tmp")
+		require.NoError(t, err)
 		assert.False(t, datasourcePlugin.isVersionOne())
 		assert.True(t, datasourcePlugin.SDK)
 	})
