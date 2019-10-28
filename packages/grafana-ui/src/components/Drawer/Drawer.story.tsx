b3d430cadebaf9e4668074dd183810db4558b5b4
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-10-28 15:26:27 +0100

    styling and inline drawer

--- packages/grafana-ui/src/components/Drawer/Drawer.story.tsx
+++ packages/grafana-ui/src/components/Drawer/Drawer.story.tsx
@@ -37,3 +37,39 @@ export const global = () => {
     </UseState>
   );
 };
+
+export const inLine = () => {
+  let parentElement: HTMLDivElement | null;
+  return (
+    <UseState initialState={{ isOpen: false }}>
+      {(state, updateValue) => {
+        return (
+          <>
+            <div
+              style={{ height: '300px', width: '500px', border: '1px solid white' }}
+              ref={element => (parentElement = element)}
+            >
+              <div
+                style={{ border: '1px solid gray', borderRadius: '4px', padding: '10px', cursor: 'pointer' }}
+                onClick={() => updateValue({ isOpen: !state.isOpen })}
+              >
+                Open drawer
+              </div>
+            </div>
+            {state.isOpen && (
+              <Drawer
+                parentContainer={parentElement as HTMLElement}
+                title="storybook"
+                onClose={() => {
+                  updateValue({ isOpen: !state.isOpen });
+                }}
+              >
+                this is a drawer
+              </Drawer>
+            )}
+          </>
+        );
+      }}
+    </UseState>
+  );
+};
--- packages/grafana-ui/src/components/Drawer/Drawer.tsx
+++ packages/grafana-ui/src/components/Drawer/Drawer.tsx
@@ -1,17 +1,45 @@
 import React, { FC, ReactNode } from 'react';
 import RcDrawer from 'rc-drawer';
+import { css, cx } from 'emotion';
+import { getTheme, stylesFactory } from '../../themes';
+import { GrafanaTheme } from '../../types';
 
 interface Props {
   title: string;
   children: ReactNode;
+  parentContainer?: HTMLElement | string;
 
   onClose: () => void;
 }
 
-export const Drawer: FC<Props> = ({ children, onClose, title }) => {
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  titleContainer: css`
+    font-size: ${theme.typography.size.lg};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  close: css`
+    cursor: pointer;
+  `,
+}));
+
+export const Drawer: FC<Props> = ({ children, parentContainer = 'body', onClose, title }) => {
+  const theme = getTheme();
+  const drawerStyles = getStyles(theme);
   return (
-    <RcDrawer onClose={onClose} handler={false} open={true} maskClosable={true}>
-      <div>{title}</div>
+    <RcDrawer
+      onClose={onClose}
+      handler={false}
+      open={true}
+      maskClosable={true}
+      placement="right"
+      width="30%"
+      getContainer={parentContainer}
+    >
+      <div className={drawerStyles.titleContainer}>
+        {title} <i className={cx('fa fa-close', drawerStyles.close)} onClick={onClose} />
+      </div>
       {children}
     </RcDrawer>
   );
