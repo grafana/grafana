cd0336f866aa0a0a254dc8799f2af5960403ae12
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-10-30 16:06:39 +0100

    Add label and formvalidation

--- packages/grafana-ui/src/components/Forms/Input.story.tsx
+++ packages/grafana-ui/src/components/Forms/Input.story.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { boolean, text } from '@storybook/addon-knobs';
 import { Input } from './Input';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import mdx from './Input.mdx';
@@ -15,5 +16,12 @@ export default {
 };
 
 export const simple = () => {
-  return <Input />;
+  const disabled = boolean('Disabled', false);
+  return <Input disabled={disabled} />;
+};
+
+export const withLabel = () => {
+  const label = text('Label', 'This is a Label');
+  const description = text('Description', '');
+  return <Input description={description} label={label} />;
 };
--- packages/grafana-ui/src/components/Forms/Input.tsx
+++ packages/grafana-ui/src/components/Forms/Input.tsx
@@ -1,39 +1,60 @@
 import React, { FC, HTMLProps } from 'react';
 import { css } from 'emotion';
-import { getTheme, selectThemeVariant, stylesFactory } from '../../themes';
+import { selectThemeVariant, stylesFactory, useTheme } from '../../themes';
 import { GrafanaTheme } from '../../types';
+import { Label } from './Label';
+import { InputStyles } from './types';
+import { FieldValidationMessage } from './FieldValidationMessage';
 
-interface Props extends HTMLProps<HTMLInputElement> {}
+interface Props extends HTMLProps<HTMLInputElement> {
+  label?: string;
+  description?: string;
+  invalid?: boolean;
+  invalidMessage?: string;
+}
 
-const getInputStyle = stylesFactory((theme: GrafanaTheme) => {
-  const colors = theme.colors;
+const getInputStyle = stylesFactory(
+  (theme: GrafanaTheme): InputStyles => {
+    const colors = theme.colors;
 
-  return {
-    input: css`
-      background-color: ${selectThemeVariant({ light: colors.white, dark: colors.gray15 }, theme.type)};
-      border: ${theme.border.width.sm} solid
-        ${selectThemeVariant({ light: colors.gray4, dark: colors.gray25 }, theme.type)};
-      border-radius: ${theme.border.radius.sm};
-      height: ${theme.spacing.formInputHeight};
-      padding: 0 ${theme.spacing.formInputPaddingHorizontal};
-      margin-top: ${theme.spacing.formSpacingBase / 2}px;
-      margin-bottom: ${theme.spacing.formSpacingBase * 2}px;
+    return {
+      input: css`
+        background-color: ${selectThemeVariant({ light: colors.white, dark: colors.gray15 }, theme.type)};
+        border: ${theme.border.width.sm} solid
+          ${selectThemeVariant({ light: colors.gray4, dark: colors.gray25 }, theme.type)};
+        border-radius: ${theme.border.radius.sm};
+        height: ${theme.spacing.formInputHeight};
+        padding: 0 ${theme.spacing.formInputPaddingHorizontal};
+        margin-bottom: ${theme.spacing.formSpacingBase * 2}px;
 
-      line-height: ${theme.typography.lineHeight.lg};
-      font-size: ${theme.typography.size.md};
-      color: ${selectThemeVariant({ light: colors.gray25, dark: colors.gray85 }, theme.type)}
+        line-height: ${theme.typography.lineHeight.lg};
+        font-size: ${theme.typography.size.md};
+        color: ${selectThemeVariant({ light: colors.gray25, dark: colors.gray85 }, theme.type)};
 
-      &[hover],
-      &:hover {
-        border-color: ${colors.gray33};
-      }
-    `,
-  };
-});
+        &:hover {
+          border-color: ${selectThemeVariant({ light: colors.gray70, dark: colors.gray33 }, theme.type)};
+        }
+
+        &[disabled],
+        &:disabled {
+          background-color: ${selectThemeVariant({ light: colors.gray6, dark: colors.gray10 }, theme.type)};
+        }
+      `,
+    };
+  }
+);
 
 export const Input: FC<Props> = props => {
-  const theme = getTheme();
+  const { description, invalid, invalidMessage, label } = props;
+
+  const theme = useTheme();
   const styles = getInputStyle(theme);
 
-  return <input type={props.type} className={styles.input} />;
+  return (
+    <div>
+      {!!label && <Label description={description}>{label}</Label>}
+      <input className={styles.input} {...props} />
+      {invalid && invalidMessage && <FieldValidationMessage>{invalidMessage}</FieldValidationMessage>}
+    </div>
+  );
 };
