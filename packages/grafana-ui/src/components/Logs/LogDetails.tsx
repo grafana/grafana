904da5fcf6fad3c7cb4a5d61cee5de22a560bfb5
Ivana Huckova <ivana.huckova@gmail.com>
2019-10-31 16:34:39 +0100

    Explore: Memoize parseMessage

--- packages/grafana-ui/src/components/Logs/LogDetails.tsx
+++ packages/grafana-ui/src/components/Logs/LogDetails.tsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
-import { LogsParser, getParser, LogRowModel } from '@grafana/data';
+import memoizeOne from 'memoize-one';
+import { getParser, LogRowModel } from '@grafana/data';
 
 import { Themeable } from '../../types/theme';
 import { withTheme } from '../../themes/index';
@@ -8,53 +9,39 @@ import { getLogRowStyles } from './getLogRowStyles';
 //Components
 import { LogDetailsRow } from './LogDetailsRow';
 
-interface Props extends Themeable {
+export interface Props extends Themeable {
   row: LogRowModel;
   getRows: () => LogRowModel[];
   onClickFilterLabel?: (key: string, value: string) => void;
   onClickFilterOutLabel?: (key: string, value: string) => void;
 }
 
-interface State {
-  parsed: boolean;
-  parsedFields: string[];
-  parser?: LogsParser;
-}
-
-class UnThemedLogDetails extends PureComponent<Props, State> {
-  state: State = {
-    parsed: false,
-    parser: undefined,
-    parsedFields: [],
-  };
-
-  parseMessage() {
-    const { row } = this.props;
-    const parser = getParser(row.entry);
-    if (parser) {
-      // Use parser to highlight detected fields
-      const parsedFields = parser.getFields(row.entry);
-      this.setState({ parsedFields, parsed: true, parser });
+class UnThemedLogDetails extends PureComponent<Props> {
+  parseMessage = memoizeOne(rowEntry => {
+    const parser = getParser(rowEntry);
+    if (!parser) {
+      return {};
     }
-  }
-
-  componentDidMount() {
-    this.parseMessage();
-  }
+    // Use parser to highlight detected fields
+    const parsedFields: string[] = parser.getFields(rowEntry);
+    return { parsedFields, parser };
+  });
 
   render() {
     const { row, theme, onClickFilterOutLabel, onClickFilterLabel, getRows } = this.props;
-    const { parsedFields, parser } = this.state;
+    const { parsedFields, parser } = this.parseMessage(row.entry);
     const style = getLogRowStyles(theme, row.logLevel);
     const labels = row.labels ? row.labels : {};
     const labelsAvailable = Object.keys(labels).length > 0;
-    const parsedFieldsAvailable = parsedFields.length > 0;
+    const parsedFieldsAvailable = parsedFields && parsedFields.length > 0;
 
     return (
       <div className={style.logsRowDetailsTable}>
         {labelsAvailable && (
           <div className={style.logsRowDetailsSectionTable}>
-            <div className={style.logsRowDetailsHeading}>Log Labels:</div>
+            <div className={style.logsRowDetailsHeading} aria-label="Log labels">
+              Log Labels:
+            </div>
             {Object.keys(labels).map(key => {
               const value = labels[key];
               const field = `${key}=${value}`;
@@ -78,26 +65,29 @@ class UnThemedLogDetails extends PureComponent<Props, State> {
 
         {parsedFieldsAvailable && (
           <div className={style.logsRowDetailsSectionTable}>
-            <div className={style.logsRowDetailsHeading}>Parsed fields:</div>
-            {parsedFields.map(field => {
-              const key = parser!.getLabelFromField(field);
-              const value = parser!.getValueFromField(field);
-              return (
-                <LogDetailsRow
-                  key={`${key}=${value}`}
-                  parsedKey={key}
-                  parsedValue={value}
-                  field={field}
-                  row={row}
-                  isLabel={false}
-                  getRows={getRows}
-                  parser={parser}
-                />
-              );
-            })}
+            <div className={style.logsRowDetailsHeading} aria-label="Parsed fields">
+              Parsed fields:
+            </div>
+            {parsedFields &&
+              parsedFields.map(field => {
+                const key = parser!.getLabelFromField(field);
+                const value = parser!.getValueFromField(field);
+                return (
+                  <LogDetailsRow
+                    key={`${key}=${value}`}
+                    parsedKey={key}
+                    parsedValue={value}
+                    field={field}
+                    row={row}
+                    isLabel={false}
+                    getRows={getRows}
+                    parser={parser}
+                  />
+                );
+              })}
           </div>
         )}
-        {!parsedFieldsAvailable && !labelsAvailable && <div>No details available</div>}
+        {!parsedFieldsAvailable && !labelsAvailable && <div aria-label="No details">No details available</div>}
       </div>
     );
   }
