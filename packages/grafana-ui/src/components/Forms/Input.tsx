50294552b26dfa55e29ee978a6937349dbbbf537
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-11-01 16:36:17 +0100

    added support for icon in input

--- packages/grafana-ui/src/components/Forms/Input.tsx
+++ packages/grafana-ui/src/components/Forms/Input.tsx
@@ -16,17 +16,29 @@ interface Props extends HTMLProps<HTMLInputElement> {
 }
 
 const getInputStyle = stylesFactory(
-  (theme: GrafanaTheme, invalid = false): InputStyles => {
+  (theme: GrafanaTheme, invalid = false, icon = false): InputStyles => {
     const colors = theme.colors;
+    const backgroundColor = selectThemeVariant({ light: colors.white, dark: colors.gray15 }, theme.type);
+    const borderColor = selectThemeVariant({ light: colors.gray4, dark: colors.gray25 }, theme.type);
+    const height = theme.spacing.formInputHeight;
+    const borders = cx(
+      css`
+        border: ${theme.border.width.sm} solid ${invalid ? colors.redBase : borderColor};
+        border-radius: ${theme.border.radius.sm};
+      `,
+      icon &&
+        css`
+          border-left: 0;
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        `
+    );
 
     return {
       input: cx(
         css`
-          background-color: ${selectThemeVariant({ light: colors.white, dark: colors.gray15 }, theme.type)};
-          border: ${theme.border.width.sm} solid
-            ${invalid ? colors.redBase : selectThemeVariant({ light: colors.gray4, dark: colors.gray25 }, theme.type)};
-          border-radius: ${theme.border.radius.sm};
-          height: ${theme.spacing.formInputHeight};
+          background-color: ${backgroundColor};
+          height: ${height};
           padding: 0 ${theme.spacing.formInputPaddingHorizontal};
           margin-bottom: ${invalid ? theme.spacing.formSpacingBase / 2 : theme.spacing.formSpacingBase * 2}px;
 
@@ -43,8 +55,34 @@ const getInputStyle = stylesFactory(
             color: ${selectThemeVariant({ light: colors.gray33, dark: colors.gray70 }, theme.type)};
           }
         `,
-        getFocusStyle(theme)
+        getFocusStyle(theme),
+        borders
       ),
+      addon: css`
+        border: 1px solid ${borderColor};
+        border-radius: ${theme.border.radius.sm};
+        height: ${height};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: ${backgroundColor};
+
+        &:first-child {
+          border-right: 0;
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+          width: 30px;
+        }
+
+        &:last-child {
+          border-left: 0;
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+      `,
+      inputWithAddonsWrapper: css`
+        display: flex;
+      `,
     };
   }
 );
@@ -53,12 +91,22 @@ export const Input: FC<Props> = props => {
   const { description, icon, invalid, invalidMessage, label, ...restProps } = props;
 
   const theme = useTheme();
-  const styles = getInputStyle(theme, invalid);
+  const styles = getInputStyle(theme, invalid, !!icon);
+
+  const input = <input className={styles.input} {...restProps} />;
+  const inputWithAddons = (
+    <div className={styles.inputWithAddonsWrapper}>
+      <div className={styles.addon}>
+        <i className={icon} />
+      </div>
+      {input}
+    </div>
+  );
 
   return (
     <div>
       {!!label && <Label description={description}>{label}</Label>}
-      <input className={styles.input} {...restProps} />
+      {!!icon ? inputWithAddons : input}
       {invalid && invalidMessage && <FieldValidationMessage>{invalidMessage}</FieldValidationMessage>}
     </div>
   );
