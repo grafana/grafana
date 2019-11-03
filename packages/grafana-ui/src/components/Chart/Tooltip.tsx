311cdb9b63803c664cb70972d1a587942d9085bb
Dominik Prokop <dominik.prokop@grafana.com>
2019-11-03 18:53:52 +0100

    Add tests for chart tooltip positioning

--- packages/grafana-ui/src/components/Chart/Tooltip.tsx
+++ packages/grafana-ui/src/components/Chart/Tooltip.tsx
@@ -1,75 +1,11 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React from 'react';
 import { css } from 'emotion';
-import useMeasure from 'react-use/lib/useMeasure';
-import useWindowSize from 'react-use/lib/useWindowSize';
 import { Portal } from '../Portal/Portal';
-import { TimeZone, Dimensions, GrafanaTheme } from '@grafana/data';
+import { TimeZone, Dimensions } from '@grafana/data';
 import { FlotPosition } from '../Graph/types';
-import { stylesFactory, selectThemeVariant, useTheme } from '../../themes';
+import { TooltipContainer } from './TooltipContainer';
 
 export type TooltipMode = 'single' | 'multi';
-interface TooltipContainerProps {
-  position: { x: number; y: number };
-  offset: { x: number; y: number };
-  children?: JSX.Element;
-}
-
-const getTooltipContainerStyles = stylesFactory((theme: GrafanaTheme) => {
-  const bgColor = selectThemeVariant({ light: theme.colors.gray5, dark: theme.colors.dark1 }, theme.type);
-  return {
-    wrapper: css`
-      overflow: hidden;
-      background: ${bgColor};
-      padding: ${theme.spacing.sm};
-      border-radius: ${theme.border.radius.sm};
-    `,
-  };
-});
-
-const TooltipContainer: React.FC<TooltipContainerProps> = ({ position, offset, children }) => {
-  const theme = useTheme();
-  const [tooltipRef, measurement] = useMeasure();
-  const { width, height } = useWindowSize();
-  const [placement, setPlacement] = useState({
-    x: position.x + offset.x,
-    y: position.y + offset.y,
-  });
-  const styles = getTooltipContainerStyles(theme);
-
-  // Make sure tooltip does not overflow window
-  useLayoutEffect(() => {
-    const xOverflow = width - (position.x + measurement.width);
-    const yOverflow = height - (position.y + measurement.height);
-    let xO = 0,
-      yO = 0;
-    if (xOverflow < 0) {
-      xO = measurement.width + offset.x;
-    }
-    if (yOverflow < 0) {
-      yO = measurement.height + offset.y;
-    }
-
-    setPlacement({
-      x: position.x - xO,
-      y: position.y - yO,
-    });
-  }, [measurement, position]);
-
-  return (
-    <div
-      ref={tooltipRef}
-      style={{
-        position: 'fixed',
-        left: 0,
-        top: 0,
-        transform: `translate3d(${placement.x}px, ${placement.y}px,0)`,
-      }}
-      className={styles.wrapper}
-    >
-      {children}
-    </div>
-  );
-};
 
 // Describes active dimensions user interacts with
 // It's a key-value pair where:
