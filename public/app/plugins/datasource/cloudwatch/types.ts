ed75303bbe4cf50d44c643526cfdbe63eea60509
Shavonn Brown <shavonn.brown@grafana.com>
2019-11-05 15:29:53 -0500

    remove CloudWatchOptions

--- public/app/plugins/datasource/cloudwatch/types.ts
+++ public/app/plugins/datasource/cloudwatch/types.ts
@@ -24,23 +24,3 @@ export interface CloudWatchSecureJsonData {
   accessKey: string;
   secretKey: string;
 }
-
-export interface CloudWatchOptions extends DataSourceJsonData {
-  name: string;
-  jsonData: {
-    authType: string;
-    defaultRegion: string;
-    timeField: string;
-    assumeRoleArn: string;
-    database: string;
-    customMetricsNamespaces: string;
-  };
-  secureJsonData?: {
-    accessKey: string;
-    secretKey: string;
-  };
-  secureJsonFields: {
-    accessKey: boolean;
-    secretKey: boolean;
-  };
-}
