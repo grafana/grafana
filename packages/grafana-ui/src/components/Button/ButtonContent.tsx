0f709cffbc41aac33e4974cb67892a693ab22cca
Andrej Ocenas <mr.ocenas@gmail.com>
2019-11-06 17:28:41 +0100

    Fix when only icon is present (#20208)

--- packages/grafana-ui/src/components/Button/ButtonContent.tsx
+++ packages/grafana-ui/src/components/Button/ButtonContent.tsx
@@ -18,13 +18,22 @@ type Props = {
 export function ButtonContent(props: Props) {
   const { icon, className, iconClassName, children } = props;
   const styles = getStyles();
-  return icon ? (
-    <span className={cx(styles.content, className)}>
-      <i className={cx([icon, iconClassName])} />
-      &nbsp; &nbsp;
-      <span>{children}</span>
-    </span>
-  ) : (
-    <span className={styles.content}>{children}</span>
-  );
+  if (icon && children) {
+    return (
+      <span className={cx(styles.content, className)}>
+        <i className={cx([icon, iconClassName])} />
+        &nbsp; &nbsp;
+        <span>{children}</span>
+      </span>
+    );
+  }
+  if (icon) {
+    return (
+      <span className={cx(styles.content, className)}>
+        <i className={cx([icon, iconClassName])} />
+      </span>
+    );
+  }
+
+  return <span className={styles.content}>{children}</span>;
 }
