e487c68156ff5cc88c7c77c2d81bb64fd80f22c0
Arve Knudsen <arve.knudsen@gmail.com>
2019-11-07 16:36:40 +0100

    Add Dockerfiles for Ubuntu (#20196)
    * Build: Add /Dockerfile.ubuntu
    * Build: Add packaging/docker/Dockerfile.ubuntu
    * Build: Add grafana-docker-ubuntu-pr CircleCI job
    * Build: Add CircleCI jobs for building master/release Ubuntu based Docker images

--- packaging/docker/push_to_docker_hub.sh
+++ packaging/docker/push_to_docker_hub.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 set -e
 
+UBUNTU_BASE=0
+
+while [ "$1" != "" ]; do
+  case "$1" in
+    "--ubuntu")
+      UBUNTU_BASE=1
+      echo "Ubuntu base image enabled"
+      shift
+      ;;
+    * )
+      # unknown param causes args to be passed through to $@
+      break
+      ;;
+  esac
+done
+
 _grafana_tag=${1:-}
 _docker_repo=${2:-grafana/grafana}
 
-# If the tag starts with v, treat this as a official release
+# If the tag starts with v, treat this as an official release
 if echo "$_grafana_tag" | grep -q "^v"; then
 	_grafana_version=$(echo "${_grafana_tag}" | cut -d "v" -f 2)
 else
@@ -13,25 +29,33 @@ fi
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-echo "pushing ${_docker_repo}:${_grafana_version}"
+if [ $UBUNTU_BASE = "0" ]; then
+  echo "pushing ${_docker_repo}:${_grafana_version}"
+else
+  echo "pushing ${_docker_repo}:${_grafana_version}-ubuntu"
+fi
 
 
 docker_push_all () {
 	repo=$1
 	tag=$2
 
-	# Push each image individually
-	docker push "${repo}:${tag}"
-	docker push "${repo}-arm32v7-linux:${tag}"
-	docker push "${repo}-arm64v8-linux:${tag}"
+  if [ $UBUNTU_BASE = "0" ]; then
+    # Push each image individually
+    docker push "${repo}:${tag}"
+    docker push "${repo}-arm32v7-linux:${tag}"
+    docker push "${repo}-arm64v8-linux:${tag}"
 
-	# Create and push a multi-arch manifest
-	docker manifest create "${repo}:${tag}" \
-		"${repo}:${tag}" \
-  	"${repo}-arm32v7-linux:${tag}" \
-		"${repo}-arm64v8-linux:${tag}"
+    # Create and push a multi-arch manifest
+    docker manifest create "${repo}:${tag}" \
+      "${repo}:${tag}" \
+      "${repo}-arm32v7-linux:${tag}" \
+      "${repo}-arm64v8-linux:${tag}"
 
-	docker manifest push "${repo}:${tag}"
+    docker manifest push "${repo}:${tag}"
+  else
+    docker push "${repo}:${tag}-ubuntu"
+  fi
 }
 
 if echo "$_grafana_tag" | grep -q "^v" && echo "$_grafana_tag" | grep -vq "beta"; then
@@ -40,13 +64,25 @@ if echo "$_grafana_tag" | grep -q "^v" && echo "$_grafana_tag" | grep -vq "beta"
 	docker_push_all "${_docker_repo}" "${_grafana_version}"
 	# Push to the grafana-dev repository with the expected tag
 	# for running the end to end tests successfully
-	docker push "grafana/grafana-dev:${_grafana_tag}"
+  if [ ${UBUNTU_BASE} = "0" ]; then
+	  docker push "grafana/grafana-dev:${_grafana_tag}"
+  else
+	  docker push "grafana/grafana-dev:${_grafana_tag}-ubuntu"
+  fi
 elif echo "$_grafana_tag" | grep -q "^v" && echo "$_grafana_tag" | grep -q "beta"; then
 	docker_push_all "${_docker_repo}" "${_grafana_version}"
 	# Push to the grafana-dev repository with the expected tag
 	# for running the end to end tests successfully
-	docker push "grafana/grafana-dev:${_grafana_tag}"
+  if [ ${UBUNTU_BASE} = "0" ]; then
+	  docker push "grafana/grafana-dev:${_grafana_tag}"
+  else
+	  docker push "grafana/grafana-dev:${_grafana_tag}-ubuntu"
+  fi
 elif echo "$_grafana_tag" | grep -q "master"; then
 	docker_push_all "${_docker_repo}" "master"
-	docker push "grafana/grafana-dev:${_grafana_version}"
+  if [ ${UBUNTU_BASE} = "0" ]; then
+    docker push "grafana/grafana-dev:${_grafana_version}"
+  else
+    docker push "grafana/grafana-dev:${_grafana_version}-ubuntu"
+  fi
 fi
