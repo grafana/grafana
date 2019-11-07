dafa81aff110af796e0c8140def1f3f25667c899
Dominik Prokop <dominik.prokop@grafana.com>
2019-11-07 10:53:18 +0100

    Enable tooltip in explore graphs

--- packages/grafana-data/src/dataframe/processDataFrame.ts
+++ packages/grafana-data/src/dataframe/processDataFrame.ts
@@ -63,6 +63,7 @@ function convertTimeSeriesToDataFrame(timeSeries: TimeSeries): DataFrame {
       type: FieldType.number,
       config: {
         unit: timeSeries.unit,
+        color: timeSeries.color,
       },
       values: new ArrayVector<TimeSeriesValue>(),
     },
