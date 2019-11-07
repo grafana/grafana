f4bc34f8c874bd2f9a400c19812394c0c608b452
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-11-07 06:29:35 +0100

    e2eTests: Adds cleanup of created datasource and dashboard

--- public/e2e-test/pages/datasources/editDataSourcePage.ts
+++ public/e2e-test/pages/datasources/editDataSourcePage.ts
@@ -1,13 +1,18 @@
 import {
-  TestPage,
-  ClickablePageObjectType,
-  PageObjectType,
   ClickablePageObject,
+  ClickablePageObjectType,
+  InputPageObject,
+  InputPageObjectType,
   PageObject,
+  PageObjectType,
   Selector,
+  TestPage,
 } from '@grafana/toolkit/src/e2e';
 
 export interface EditDataSourcePage {
+  name: InputPageObjectType;
+  default: ClickablePageObjectType;
+  delete: ClickablePageObjectType;
   saveAndTest: ClickablePageObjectType;
   alert: PageObjectType;
   alertMessage: PageObjectType;
@@ -15,6 +20,11 @@ export interface EditDataSourcePage {
 
 export const editDataSourcePage = new TestPage<EditDataSourcePage>({
   pageObjects: {
+    name: new InputPageObject(Selector.fromAriaLabel('Datasource settings page name input field')),
+    default: new ClickablePageObject(
+      Selector.fromSelector('[aria-label="Datasource settings page basic settings"] .gf-form-switch')
+    ),
+    delete: new ClickablePageObject(Selector.fromAriaLabel('Delete button')),
     saveAndTest: new ClickablePageObject(Selector.fromAriaLabel('Save and Test button')),
     alert: new PageObject(Selector.fromAriaLabel('Datasource settings page Alert')),
     alertMessage: new PageObject(Selector.fromAriaLabel('Datasource settings page Alert message')),
