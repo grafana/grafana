7332f4ee1a3f8600e00161694651455cfd71669b
kay delaney <kay@grafana.com>
2019-11-08 09:43:44 +0000

    Removes more unneeded props

--- packages/grafana-ui/src/components/Cascader/Cascader.tsx
+++ packages/grafana-ui/src/components/Cascader/Cascader.tsx
@@ -3,23 +3,6 @@ import React from 'react';
 // @ts-ignore
 import RCCascader from 'rc-cascader';
 
-export interface AlignType {
-  points?: string[];
-  offset?: number[];
-  targetOffset?: number[];
-  overflow?: {
-    adjustX?: boolean | number;
-    adjustY?: boolean | number;
-  };
-  useCssRight?: boolean;
-  useCssBottom?: boolean;
-  useCssTransform?: boolean;
-}
-
-export interface BuiltInPlacements {
-  [placement: string]: AlignType;
-}
-
 export interface CascaderOption {
   label: string;
   value: string;
@@ -31,22 +14,12 @@ export interface CascaderOption {
 export interface CascaderProps {
   options: CascaderOption[];
   buttonText: string;
-
-  builtinPlacements?: BuiltInPlacements;
-  changeOnSelect?: boolean;
-  defaultValue?: string[];
   disabled?: boolean;
-  dropdownMenuColumnStyle?: React.CSSProperties;
   expandIcon?: React.ReactNode;
-  expandTrigger?: string;
-  popupPlacement?: string;
-  popupVisible?: boolean;
   value?: string[];
 
-  getPopupContainer?: (trigger: React.ReactNode) => React.ReactNode;
   loadData?: (selectedOptions: CascaderOption[]) => void;
   onChange?: (value: string[], selectedOptions: CascaderOption[]) => void;
-  onKeyDown?: () => void;
   onPopupVisibleChange?: (visible: boolean) => void;
 }
 
