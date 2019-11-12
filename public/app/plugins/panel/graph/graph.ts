7e901d5ba069aa8749f32b29f58c2c769f2b8e48
clamber <lamberty@droxit.de>
2019-11-12 14:57:06 +0100

    Graph-Panel: Removed option based bar chart centering. Added centered bar charts as default.

--- public/app/plugins/panel/graph/graph.ts
+++ public/app/plugins/panel/graph/graph.ts
@@ -461,9 +461,7 @@ class GraphElement {
       }
       default: {
         options.series.bars.barWidth = this.getMinTimeStepOfSeries(this.data) / 1.5;
-        if (panel.centered) {
-          options.series.bars.align = 'center';
-        }
+        options.series.bars.align = 'center';
         this.addTimeAxis(options);
         break;
       }
