a1fa6781a048a420e9a593ff75933868704a6342
Dominik Prokop <dominik.prokop@grafana.com>
2019-11-13 11:04:02 +0100

    Simplify Inputs styling (POC)

--- packages/grafana-ui/src/components/Forms/Input/Input.tsx
+++ packages/grafana-ui/src/components/Forms/Input/Input.tsx
@@ -1,15 +1,16 @@
-import React, { cloneElement, FC, HTMLProps, isValidElement, ReactNode } from 'react';
+import React, { FC, HTMLProps, ReactNode } from 'react';
 import { GrafanaTheme } from '@grafana/data';
 import { css, cx } from 'emotion';
-import { Spinner } from '../..';
 import { getFocusStyle } from '../commonStyles';
 import { selectThemeVariant, stylesFactory, useTheme } from '../../../themes';
+import { Icon } from '../../Icon/Icon';
+import { useClientRect } from '../../../utils/useClientRect';
 
-export interface Props extends HTMLProps<HTMLInputElement> {
+export interface Props extends Omit<HTMLProps<HTMLInputElement>, 'prefix'> {
   /** Show an invalid state around the input */
   invalid?: boolean;
   /** Show an icon as a prefix in the input */
-  icon?: string;
+  prefix?: JSX.Element | string | null;
   /** Show a loading indicator as a suffix in the input */
   loading?: boolean;
   /** Add a component as an addon before the input  */
@@ -18,196 +19,223 @@ export interface Props extends HTMLProps<HTMLInputElement> {
   addonAfter?: ReactNode;
 }
 
-const getInputStyle = stylesFactory(
-  (
-    theme: GrafanaTheme,
-    invalid = false,
-    disabled = false,
-    prefix = false,
-    suffix = false,
-    addonBefore = false,
-    addonAfter = false
-  ) => {
-    const colors = theme.colors;
-    const backgroundColor = selectThemeVariant({ light: colors.white, dark: colors.gray15 }, theme.type);
-    const borderColor = selectThemeVariant({ light: colors.gray4, dark: colors.gray25 }, theme.type);
-    const inputBorderColor = invalid ? colors.redBase : borderColor;
-    const borderRadius = theme.border.radius.sm;
-    const height = theme.spacing.formInputHeight;
-    const disabledBackground = selectThemeVariant({ light: colors.gray6, dark: colors.gray10 }, theme.type);
-    const disabledColor = selectThemeVariant({ light: colors.gray33, dark: colors.gray70 }, theme.type);
-    const inputBorders = cx(
+const getInputStyle = stylesFactory((theme: GrafanaTheme, invalid = false) => {
+  const colors = theme.colors;
+  const backgroundColor = selectThemeVariant({ light: colors.white, dark: colors.gray15 }, theme.type);
+  const borderColor = selectThemeVariant({ light: colors.gray4, dark: colors.gray25 }, theme.type);
+  const inputBorderColor = invalid ? colors.redBase : borderColor;
+  const borderRadius = theme.border.radius.sm;
+  const height = theme.spacing.formInputHeight;
+  const disabledBackground = selectThemeVariant({ light: colors.gray6, dark: colors.gray10 }, theme.type);
+  const disabledColor = selectThemeVariant({ light: colors.gray33, dark: colors.gray70 }, theme.type);
+  const inputTextColor = selectThemeVariant({ light: colors.gray25, dark: colors.gray85 }, theme.type);
+
+  const prefixSuffixStaticWidth = '28px';
+  const prefixSuffix = css`
+    position: absolute;
+    top: 0;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-grow: 0;
+    flex-shrink: 0;
+    font-size: ${theme.typography.size.md};
+    height: 100%;
+    /* Min width specified for prefix/suffix classes used outside React component*/
+    min-width: ${prefixSuffixStaticWidth};
+  `;
+
+  return {
+    // Wraps inputWraper and addons
+    wrapper: cx(
       css`
-        border: 1px solid ${inputBorderColor};
+        label: input-wrapper;
+        display: flex;
+        width: 100%;
+        height: ${height};
         border-radius: ${borderRadius};
-      `,
-      prefix &&
-        css`
-          border-left: none;
-        `,
-      (prefix || (!prefix && addonBefore)) &&
-        css`
-          border-top-left-radius: 0;
-          border-bottom-left-radius: 0;
-        `,
-      suffix &&
-        css`
-          border-right: none;
-        `,
-      (suffix || (!suffix && addonAfter)) &&
-        css`
-          border-top-right-radius: 0;
-          border-bottom-right-radius: 0;
-        `
-    );
-
-    const prefixSuffix = css`
-      background-color: ${disabled ? disabledBackground : backgroundColor};
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-grow: 0;
-      flex-shrink: 0;
-      width: 24px;
-      z-index: 0;
-      border: 1px solid ${inputBorderColor};
-    `;
-
-    return {
-      inputWrapper: cx(
-        css`
-          width: 100%;
-          display: flex;
-          height: ${height};
-          border-radius: ${borderRadius};
-        `,
-        getFocusStyle(theme)
-      ),
-      prefix: cx(
-        prefixSuffix,
-        css`
-          border-right: none;
-          border-top-right-radius: 0;
-          border-bottom-right-radius: 0;
-          border-top-left-radius: ${addonBefore ? 0 : borderRadius};
-          border-bottom-left-radius: ${addonBefore ? 0 : borderRadius};
-        `
-      ),
-      input: cx(
-        css`
-          background-color: ${backgroundColor};
-          height: 100%;
-          width: 100%;
-          flex-grow: 1;
-          padding: 0 ${suffix ? theme.spacing.xs : theme.spacing.sm} 0 ${prefix ? theme.spacing.xs : theme.spacing.sm};
-          margin-bottom: ${invalid ? theme.spacing.formSpacingBase / 2 : theme.spacing.formSpacingBase * 2}px;
-          position: relative;
-          z-index: 1;
-
-          line-height: ${theme.typography.lineHeight.lg};
-          font-size: ${theme.typography.size.md};
-          color: ${selectThemeVariant({ light: colors.gray25, dark: colors.gray85 }, theme.type)};
-
-          &:hover {
+        margin-bottom: ${invalid ? theme.spacing.formSpacingBase / 2 : theme.spacing.formSpacingBase * 2}px;
+        &:hover {
+          > .prefix,
+          .suffix,
+          .input {
             border-color: ${invalid
               ? colors.redBase
               : selectThemeVariant({ light: colors.gray70, dark: colors.gray33 }, theme.type)};
           }
+        }
+      `
+    ),
+    // Wraps input and prefix/suffiz
+    inputWrapper: css`
+      label: input-inputWrapper;
+      position: relative;
+      flex-grow: 1;
+      /* we want input to be above addons, especially for focused state */
+      z-index: 1;
 
-          &:focus {
-            outline: none;
-          }
+      /* when input rendered with addon before only*/
+      &:not(:first-child):last-child {
+        > input {
+          border-left: none;
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+      }
 
-          &:disabled {
-            background-color: ${disabledBackground};
-            color: ${disabledColor};
-          }
-        `,
-        inputBorders
-      ),
-      addon: css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: ${backgroundColor};
-        flex-grow: 0;
-        flex-shrink: 0;
-        z-index: 0;
-        position: relative;
-        border: 1px solid ${borderColor};
-        border-radius: ${borderRadius};
-
-        &:first-child {
+      /* when input rendered with addon after only*/
+      &:first-child:not(:last-child) {
+        > input {
           border-right: none;
           border-top-right-radius: 0;
           border-bottom-right-radius: 0;
         }
+      }
+
+      /* when rendered with addon before and after */
+      &:not(:first-child):not(:last-child) {
+        > input {
+          border-right: none;
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+      }
+
+      input {
+        /* paddings specified for classes used outside React component */
+        &:not(:first-child) {
+          padding-left: ${prefixSuffixStaticWidth};
+        }
+        &:not(:last-child) {
+          padding-right: ${prefixSuffixStaticWidth};
+        }
+      }
+    `,
+
+    input: cx(
+      getFocusStyle(theme),
+      css`
+        label: input-input;
+        position: relative;
+        z-index: 0;
+        flex-grow: 1;
+        color: ${inputTextColor};
+        background-color: ${backgroundColor};
+        border: 1px solid ${inputBorderColor};
+        border-radius: ${borderRadius};
+        height: 100%;
+        width: 100%;
+        padding: 0 ${theme.spacing.sm} 0 ${theme.spacing.sm};
+        font-size: ${theme.typography.size.md};
+        &:disabled {
+          background-color: ${disabledBackground};
+          color: ${disabledColor};
+        }
+      `
+    ),
+    addon: css`
+        label: input-addon;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-grow: 0;
+        flex-shrink: 0;
+        position: relative;
+
+        &:first-child {
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+          > :last-child {
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+          }
+        }
 
         &:last-child {
-          border-left: none;
           border-top-left-radius: 0;
           border-bottom-left-radius: 0;
+          > :first-child {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+          }
+        }
+        > *:focus {
+          /* we want anything that has focus and is an addon to be above input */
+          z-index: 2;
+        }
         }
       `,
-      addonElement: css`
-        label: addon-override;
-        border: 0;
-        width: 100%;
-        height: 100%;
-
-        &:focus {
-          box-shadow: none;
-        }
-      `,
-      suffix: cx(
-        prefixSuffix,
-        css`
-          border-left: none;
-          border-top-left-radius: 0;
-          border-bottom-left-radius: 0;
-          border-top-right-radius: ${addonAfter ? 0 : borderRadius};
-          border-bottom-right-radius: ${addonAfter ? 0 : borderRadius};
-        `
-      ),
-    };
-  }
-);
+    prefix: cx(
+      prefixSuffix,
+      css`
+        label: input-prefix;
+        padding-left: ${theme.spacing.sm};
+        padding-right: ${theme.spacing.xs};
+        border-right: none;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        left: 0;
+      `
+    ),
+    suffix: cx(
+      prefixSuffix,
+      css`
+        label: input-suffix;
+        padding-right: ${theme.spacing.sm};
+        padding-left: ${theme.spacing.xs};
+        border-left: none;
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        right: 0;
+      `
+    ),
+  };
+});
 
 export const Input: FC<Props> = props => {
-  const { addonAfter, addonBefore, icon, invalid, loading, ...restProps } = props;
+  const { addonAfter, addonBefore, prefix, invalid, loading, ...restProps } = props;
+  /**
+   * Prefix & suffix are positioned absolutely within inputWrapper. We use client rects beow to apply correct padding
+   * when prefix/suffix is larger than default (28px = 16px(icon) + 12px(left/right paddings))
+   */
+  const [prefixRect, prefixRef] = useClientRect<HTMLDivElement>();
+  const [suffixRect, suffixRef] = useClientRect<HTMLDivElement>();
 
   const theme = useTheme();
-  const styles = getInputStyle(theme, invalid, restProps.disabled, !!icon, !!loading, !!addonBefore, !!addonAfter);
+
+  const styles = getInputStyle(theme, invalid);
 
   return (
-    <div className={styles.inputWrapper} tabIndex={0}>
-      {!!addonBefore && (
-        <div className={styles.addon}>
-          {isValidElement(addonBefore) &&
-            cloneElement(addonBefore, {
-              className: styles.addonElement,
-            })}
-        </div>
-      )}
-      {icon && (
-        <div className={styles.prefix}>
-          <i className={icon} />
-        </div>
-      )}
-      <input className={styles.input} {...restProps} />
-      {loading && (
-        <div className={styles.suffix}>
-          <Spinner />
-        </div>
-      )}
-      {!!addonAfter && (
-        <div className={styles.addon}>
-          {isValidElement(addonAfter) &&
-            cloneElement(addonAfter, {
-              className: styles.addonElement,
-            })}
-        </div>
-      )}
+    <div className={styles.wrapper}>
+      {!!addonBefore && <div className={styles.addon}>{addonBefore}</div>}
+
+      <div className={styles.inputWrapper}>
+        {prefix && (
+          <div className={styles.prefix} ref={prefixRef}>
+            {prefix}
+          </div>
+        )}
+
+        <input
+          className={styles.input}
+          {...restProps}
+          style={{
+            paddingLeft: prefixRect ? prefixRect.width : undefined,
+            paddingRight: suffixRect ? suffixRect.width : undefined,
+          }}
+        />
+
+        {loading && (
+          <div className={styles.suffix} ref={suffixRef}>
+            <Icon name="spinner" className="fa-spin" />
+          </div>
+        )}
+      </div>
+
+      {!!addonAfter && <div className={styles.addon}>{addonAfter}</div>}
     </div>
   );
 };
