4eb01cf2d91ae305ac44932685fe3c2ed5247514
Emil Tullstedt <emil.tullstedt@grafana.com>
2019-11-14 14:45:50 +0100

    NavLinks: Make ordering in navigation configurable

--- pkg/api/dtos/index.go
+++ pkg/api/dtos/index.go
@@ -22,17 +22,39 @@ type PluginCss struct {
 	Dark  string `json:"dark"`
 }
 
+type NavLinkWeight int64
+
+// A sufficiently large negative offset to make it unlikely for
+// plugins to end up placed above either of the default navlinks.
+const defaultNavLinkOffset = -64
+
+const (
+	// These weights may be used by an extension to reliably place
+	// itself below a particular item in the menu.
+
+	WeightCreate = iota + defaultNavLinkOffset
+	WeightDashboard
+	WeightExplore
+	WeightProfile
+	WeightAlerting
+	WeightPlugin
+	WeightConfig
+	WeightAdmin
+	WeightHelp
+)
+
 type NavLink struct {
-	Id           string     `json:"id,omitempty"`
-	Text         string     `json:"text,omitempty"`
-	Description  string     `json:"description,omitempty"`
-	SubTitle     string     `json:"subTitle,omitempty"`
-	Icon         string     `json:"icon,omitempty"`
-	Img          string     `json:"img,omitempty"`
-	Url          string     `json:"url,omitempty"`
-	Target       string     `json:"target,omitempty"`
-	Divider      bool       `json:"divider,omitempty"`
-	HideFromMenu bool       `json:"hideFromMenu,omitempty"`
-	HideFromTabs bool       `json:"hideFromTabs,omitempty"`
-	Children     []*NavLink `json:"children,omitempty"`
+	Id           string        `json:"id,omitempty"`
+	Text         string        `json:"text,omitempty"`
+	Description  string        `json:"description,omitempty"`
+	SubTitle     string        `json:"subTitle,omitempty"`
+	Icon         string        `json:"icon,omitempty"`
+	Img          string        `json:"img,omitempty"`
+	Url          string        `json:"url,omitempty"`
+	Target       string        `json:"target,omitempty"`
+	SortWeight   NavLinkWeight `json:"sortWeight,omitempty"`
+	Divider      bool          `json:"divider,omitempty"`
+	HideFromMenu bool          `json:"hideFromMenu,omitempty"`
+	HideFromTabs bool          `json:"hideFromTabs,omitempty"`
+	Children     []*NavLink    `json:"children,omitempty"`
 }
