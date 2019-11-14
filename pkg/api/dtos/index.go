337f83ccbf3da0ab3cc429fa286a333e0125c623
Emil Tullstedt <emil.tullstedt@grafana.com>
2019-11-14 16:36:05 +0100

    remove unused type declaration

--- pkg/api/dtos/index.go
+++ pkg/api/dtos/index.go
@@ -22,8 +22,6 @@ type PluginCss struct {
 	Dark  string `json:"dark"`
 }
 
-type NavLinkWeight int64
-
 const (
 	// These weights may be used by an extension to reliably place
 	// itself in relation to a particular item in the menu. The weights
@@ -42,17 +40,17 @@ const (
 )
 
 type NavLink struct {
-	Id           string        `json:"id,omitempty"`
-	Text         string        `json:"text,omitempty"`
-	Description  string        `json:"description,omitempty"`
-	SubTitle     string        `json:"subTitle,omitempty"`
-	Icon         string        `json:"icon,omitempty"`
-	Img          string        `json:"img,omitempty"`
-	Url          string        `json:"url,omitempty"`
-	Target       string        `json:"target,omitempty"`
-	SortWeight   NavLinkWeight `json:"sortWeight,omitempty"`
-	Divider      bool          `json:"divider,omitempty"`
-	HideFromMenu bool          `json:"hideFromMenu,omitempty"`
-	HideFromTabs bool          `json:"hideFromTabs,omitempty"`
-	Children     []*NavLink    `json:"children,omitempty"`
+	Id           string     `json:"id,omitempty"`
+	Text         string     `json:"text,omitempty"`
+	Description  string     `json:"description,omitempty"`
+	SubTitle     string     `json:"subTitle,omitempty"`
+	Icon         string     `json:"icon,omitempty"`
+	Img          string     `json:"img,omitempty"`
+	Url          string     `json:"url,omitempty"`
+	Target       string     `json:"target,omitempty"`
+	SortWeight   int64      `json:"sortWeight,omitempty"`
+	Divider      bool       `json:"divider,omitempty"`
+	HideFromMenu bool       `json:"hideFromMenu,omitempty"`
+	HideFromTabs bool       `json:"hideFromTabs,omitempty"`
+	Children     []*NavLink `json:"children,omitempty"`
 }
