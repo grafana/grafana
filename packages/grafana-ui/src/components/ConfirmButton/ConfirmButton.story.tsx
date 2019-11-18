4068324289ceb04953bd2896b136da853c5e0508
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-11-18 19:17:42 +0300

    ConfirmButton: make styled component

--- packages/grafana-ui/src/components/ConfirmButton/ConfirmButton.story.tsx
+++ packages/grafana-ui/src/components/ConfirmButton/ConfirmButton.story.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text, boolean } from '@storybook/addon-knobs';
 import { ConfirmButton } from './ConfirmButton';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { action } from '@storybook/addon-actions';
 
+const getKnobs = () => {
+  return {
+    buttonText: text('Button text', 'Edit'),
+    confirmText: text('Confirm text', 'Save'),
+    disabled: boolean('Disabled', false),
+  };
+};
+
 storiesOf('UI/ConfirmButton', module)
   .addDecorator(withCenteredStory)
   .add('default', () => {
+    const { buttonText, confirmText, disabled } = getKnobs();
     return (
-      <ConfirmButton
-        buttonText="Edit"
-        confirmText="Save"
-        onConfirm={() => {
-          action('Saved')('save!');
-        }}
-      />
+      <>
+        <div className="gf-form-group">
+          <div className="gf-form">
+            <ConfirmButton
+              buttonText={buttonText}
+              confirmText={confirmText}
+              disabled={disabled}
+              onConfirm={() => {
+                action('Saved')('save!');
+              }}
+            />
+          </div>
+        </div>
+      </>
     );
   });
--- packages/grafana-ui/src/components/ConfirmButton/ConfirmButton.tsx
+++ packages/grafana-ui/src/components/ConfirmButton/ConfirmButton.tsx
@@ -1,22 +1,80 @@
 import React, { PureComponent, SyntheticEvent } from 'react';
+import { cx, css } from 'emotion';
+import { stylesFactory, withTheme } from '../../themes';
+import { GrafanaTheme } from '@grafana/data';
+import { Themeable } from '../../types';
 
-interface Props {
+const getStyles = stylesFactory((theme: GrafanaTheme) => {
+  return {
+    buttonContainer: css`
+      width: 24px;
+      direction: rtl;
+      display: flex;
+      align-items: center;
+    `,
+    button: css`
+      position: absolute;
+      text-decoration: underline;
+      color: ${theme.colors.linkExternal};
+    `,
+    buttonDisabled: css`
+      text-decoration: none;
+      color: ${theme.colors.text};
+    `,
+    buttonShow: css`
+      opacity: 1;
+      transition: opacity 0.1s ease;
+      z-index: 2;
+    `,
+    buttonHide: css`
+      opacity: 0;
+      transition: opacity 0.1s ease;
+      z-index: 0;
+    `,
+    confirmButtonContainer: css`
+      overflow: hidden;
+      width: 145px;
+      position: absolute;
+      z-index: 1;
+    `,
+    confirmButton: css`
+      display: flex;
+      align-items: flex-start;
+    `,
+    confirmButtonShow: css`
+      opacity: 1;
+      transition: opacity 0.08s ease-out, transform 0.1s ease-out;
+      transform: translateX(0);
+    `,
+    confirmButtonHide: css`
+      opacity: 0;
+      transition: opacity 0.12s ease-in, transform 0.14s ease-in;
+      transform: translateX(100px);
+    `,
+  };
+});
+
+interface Props extends Themeable {
+  className?: string;
+  style?: React.CSSProperties;
+  buttonText?: string;
+  confirmText?: string;
+  disabled?: boolean;
+
   onConfirm(): void;
   onClick?(): void;
   onCancel?(): void;
-  buttonText: string;
-  confirmText?: string;
-  disabled?: boolean;
 }
 
 interface State {
   showConfirm: boolean;
 }
 
-export class ConfirmButton extends PureComponent<Props, State> {
+class UnThemedConfirmButton extends PureComponent<Props, State> {
   static defaultProps: Partial<Props> = {
-    disabled: false,
+    buttonText: 'Edit',
     confirmText: 'Confirm Delete',
+    disabled: false,
   };
 
   state: State = {
@@ -50,19 +108,28 @@ export class ConfirmButton extends PureComponent<Props, State> {
   };
 
   render() {
-    const { onConfirm, buttonText, disabled, confirmText } = this.props;
-    const showConfirmClass = this.state.showConfirm ? 'show' : 'hide';
-    const showDeleteButtonClass = this.state.showConfirm ? 'hide' : 'show';
-    const disabledClass = disabled ? 'disabled btn-inverse' : '';
+    const { onConfirm, buttonText, disabled, confirmText, className, style, theme } = this.props;
+    const styles = getStyles(theme);
+    const disabledClass = disabled ? cx('disabled btn-inverse', styles.buttonDisabled) : '';
+    const buttonClass = cx(
+      styles.button,
+      className,
+      disabledClass,
+      this.state.showConfirm ? styles.buttonHide : styles.buttonShow
+    );
+    const confirmButtonClass = cx(
+      styles.confirmButton,
+      this.state.showConfirm ? styles.confirmButtonShow : styles.confirmButtonHide
+    );
     const onClick = disabled ? () => {} : this.onClickButton;
 
     return (
-      <span className="confirm-button-container">
-        <a className={`confirm-button btn ${showDeleteButtonClass} ${disabledClass}`} onClick={onClick}>
+      <span className={cx(styles.buttonContainer)}>
+        <a className={buttonClass} style={style} onClick={onClick}>
           {buttonText}
         </a>
-        <span className="confirm-delete-container">
-          <span className={`confirm-delete ${showConfirmClass}`}>
+        <span className={cx(styles.confirmButtonContainer)}>
+          <span className={confirmButtonClass}>
             <a className="btn btn-small" onClick={this.onClickCancel}>
               Cancel
             </a>
@@ -75,3 +142,6 @@ export class ConfirmButton extends PureComponent<Props, State> {
     );
   }
 }
+
+export const ConfirmButton = withTheme(UnThemedConfirmButton);
+ConfirmButton.displayName = 'ConfirmButton';
