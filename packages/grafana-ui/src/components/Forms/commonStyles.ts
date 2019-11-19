7035d959e98507471d4b2d53750df763285a6529
Dominik Prokop <dominik.prokop@grafana.com>
2019-11-19 16:59:39 +0100

    Experiment with different focus style

--- packages/grafana-ui/src/components/Forms/commonStyles.ts
+++ packages/grafana-ui/src/components/Forms/commonStyles.ts
@@ -5,7 +5,6 @@ export const getFocusStyle = (theme: GrafanaTheme) => css`
   &:focus {
     outline: 2px dotted transparent;
     outline-offset: 2px;
-    box-shadow: 0 0 0 2px ${theme.colors.pageBg}, 0 0 0px 4px ${theme.colors.formFocusOutline};
-    transition: all 0.2s cubic-bezier(0.19, 1, 0.22, 1);
+    box-shadow: 0 0 0 2px ${theme.colors.pageBg}, 0 0 0px 4px ${theme.colors.blueLight};
   }
 `;
