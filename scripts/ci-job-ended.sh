67497a60eacecbb00c7dd37a5ebe41753ea25de1
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-11-19 11:36:01 +0100

    WIP: adds stats echo

--- scripts/ci-job-ended.sh
+++ scripts/ci-job-ended.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# shellcheck source=./scripts/helpers/exit-if-fail.sh
+source "$(dirname "$0")/../helpers/exit-if-fail.sh"
 
 echo -e "Echo job start date"
 
 echo "$GF_JOB_START"
+echo "$CIRCLE_JOB"
+
+ exit_if_fail echo "./scripts/ci-metrics-publisher.sh grafana.ci-buildtimes.$CIRCLE_JOB=$GF_JOB_START"
+ 
