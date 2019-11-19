50cc651ea3d3d69d59758164c202047b2ef175f9
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-11-19 14:35:10 +0100

    Refactor: Run stats on master only

--- scripts/ci-job-failed.sh
+++ scripts/ci-job-failed.sh
@@ -8,6 +8,9 @@ echo -e "Report build times and build outcome"
 start=$GF_JOB_START
 runtime=$((($(date +%s%N) - start)/1000000))
 
+echo -e "The job $CIRCLE_JOB took $runtime (ms)"
+
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
 exit_if_fail ./scripts/ci-metrics-publisher.sh "grafana.ci-buildtimes.$CIRCLE_JOB=$runtime"
 exit_if_fail ./scripts/ci-metrics-publisher.sh "grafana.ci-buildoutcome.$CIRCLE_JOB=0"
-
+fi
