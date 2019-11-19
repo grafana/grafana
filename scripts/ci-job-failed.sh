370013e3fb5c408c17e7656d7f5a5a60f85dada4
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-11-19 12:00:53 +0100

    WIP: Adds reporting

---
+++ scripts/ci-job-failed.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck source=./scripts/helpers/exit-if-fail.sh
+source "$(dirname "$0")/../helpers/exit-if-fail.sh"
+
+echo -e "Report build times and build outcome"
+
+echo "$GF_JOB_START"
+echo "$CIRCLE_JOB"
+
+exit_if_fail ./scripts/ci-metrics-publisher.sh "grafana.ci-buildtimes.$CIRCLE_JOB=$GF_JOB_START"
+exit_if_fail ./scripts/ci-metrics-publisher.sh "grafana.ci-buildoutcome.$CIRCLE_JOB=0"
+
