9883b25814e56a6ff6ea3bb34caea7011a67b6ad
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-11-20 13:16:43 +0100

    backend plugins: hclog wrapper args formatting

---
+++ pkg/plugins/backendplugin/log_wrapper_test.go
@@ -0,0 +1,34 @@
+package backendplugin
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogWrapper(t *testing.T) {
+	tcs := []struct {
+		args           []interface{}
+		expectedResult []interface{}
+	}{
+		{args: nil, expectedResult: nil},
+		{args: []interface{}{}, expectedResult: []interface{}{}},
+		{args: []interface{}{"1", "2", "3"}, expectedResult: []interface{}{"1", "2", "3"}},
+		{args: []interface{}{"1", "2"}, expectedResult: []interface{}{"1", "2"}},
+		{args: []interface{}{"1", "2", "timestamp", time.Now()}, expectedResult: []interface{}{"1", "2"}},
+		{args: []interface{}{"1", "2", "timestamp", time.Now(), "3", "4"}, expectedResult: []interface{}{"1", "2", "3", "4"}},
+	}
+
+	for i, tc := range tcs {
+		t.Run(fmt.Sprintf("formatArgs testcase %d", i), func(t *testing.T) {
+			res := formatArgs(tc.args...)
+			if tc.expectedResult == nil {
+				assert.Nil(t, res)
+			} else {
+				assert.Exactly(t, tc.expectedResult, res)
+			}
+		})
+	}
+}
