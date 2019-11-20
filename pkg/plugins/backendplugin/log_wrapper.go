0994386bc62c6d78142a90838ce4c47b4809658f
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-11-20 14:33:32 +0100

    format fix

--- pkg/plugins/backendplugin/log_wrapper.go
+++ pkg/plugins/backendplugin/log_wrapper.go
@@ -20,7 +20,7 @@ func formatArgs(args ...interface{}) []interface{} {
 
 	res := []interface{}{}
 
-	for n := 0; n < len(args); n = n + 2 {
+	for n := 0; n < len(args); n += 2 {
 		key := args[n]
 
 		if stringKey, ok := key.(string); ok && stringKey == "timestamp" {
