9b4ab26e87ef6fdd1065e337b1cec69d4fe23ed7
Dominik Prokop <dominik.prokop@grafana.com>
2019-11-22 16:27:08 +0100

    Update on/off swtch colors

--- packages/grafana-data/src/types/theme.ts
+++ packages/grafana-data/src/types/theme.ts
@@ -231,6 +231,8 @@ export interface GrafanaTheme extends GrafanaThemeCommons {
     formValidationMessageText: string;
     formValidationMessageBg: string;
     formSwitchBg: string;
+    formSwitchBgActive: string;
+    formSwitchBgActiveHover: string;
     formSwitchBgHover: string;
     formSwitchBgDisabled: string;
     formSwitchDot: string;
