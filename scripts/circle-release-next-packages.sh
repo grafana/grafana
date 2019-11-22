b6197d5de096e72185e7c3426396e76e6e0ee1fa
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-11-22 14:05:09 +0100

    fix path

--- scripts/circle-release-next-packages.sh
+++ scripts/circle-release-next-packages.sh
@@ -58,7 +58,7 @@ else
     yarn workspace @grafana/$PACKAGE run build
     runtime=$((($(date +%s%N) - start)/1000000))
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
-    exit_if_fail ./ci-metrics-publisher.sh "grafana.ci-buildtimes.$CIRCLE_JOB.$PACKAGE=$runtime"
+    exit_if_fail ./scripts/ci-metrics-publisher.sh "grafana.ci-buildtimes.$CIRCLE_JOB.$PACKAGE=$runtime"
     fi
   done
 
