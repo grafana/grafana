48709bbc7d0548e929987db8b131d623411718dd
Ivana <ivana.huckova@gmail.com>
2019-11-27 11:58:11 +0100

    Loki: Create metric_find_query

---
+++ public/app/plugins/datasource/loki/metric_find_query.ts
@@ -0,0 +1,48 @@
+import _ from 'lodash';
+import { TimeRange } from '@grafana/data';
+import { LokiDatasource } from './datasource';
+
+export default class LokiMetricFindQuery {
+  range: TimeRange;
+
+  constructor(private datasource: LokiDatasource, private query: string) {
+    this.datasource = datasource;
+    this.query = query;
+  }
+
+  process() {
+    const labelNamesRegex = /^label_names\(\)\s*$/;
+    const labelValuesRegex = /^label_values\((?:(.+),\s*)?([a-zA-Z_][a-zA-Z0-9_]*)\)\s*$/;
+
+    const labelNamesQuery = this.query.match(labelNamesRegex);
+    if (labelNamesQuery) {
+      return this.labelNamesQuery();
+    }
+
+    const labelValuesQuery = this.query.match(labelValuesRegex);
+    if (labelValuesQuery) {
+      return this.labelValuesQuery(labelValuesQuery[2]);
+    }
+
+    return Promise.resolve([]);
+  }
+
+  labelNamesQuery() {
+    const url = '/loki/api/v1/label';
+    return this.datasource.metadataRequest(url).then((result: any) => {
+      return _.map(result.data.data, value => {
+        return { text: value };
+      });
+    });
+  }
+
+  labelValuesQuery(label: string) {
+    let url: string;
+    url = '/loki/api/v1/label/' + label + '/values';
+    return this.datasource.metadataRequest(url).then((result: any) => {
+      return _.map(result.data.data, value => {
+        return { text: value };
+      });
+    });
+  }
+}
