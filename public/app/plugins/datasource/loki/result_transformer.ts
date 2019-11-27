1f9e3cd5c2b8ad2e10d48b6f4a1899f59fb50887
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2019-11-27 09:09:55 +0000

    Update public/app/plugins/datasource/loki/result_transformer.ts

--- public/app/plugins/datasource/loki/result_transformer.ts
+++ public/app/plugins/datasource/loki/result_transformer.ts
@@ -311,7 +311,7 @@ export function lokiStreamsToDataframes(
     const dataFrame = lokiStreamResultToDataFrame(stream, reverse);
     enhanceDataFrame(dataFrame, config);
     return {
-      ...dataFrame(stream, reverse),
+      ...dataFrame,
       refId: target.refId,
       meta: {
         searchWords: getHighlighterExpressionsFromQuery(formatQuery(target.expr, target.regexp)),
