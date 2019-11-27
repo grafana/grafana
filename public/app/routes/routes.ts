37ef48ab67b83a454cd8e2a68d18a33cfb511c14
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-11-27 14:06:28 +0300

    admin user: remove LdapUserPage import from routes

--- public/app/routes/routes.ts
+++ public/app/routes/routes.ts
@@ -6,7 +6,6 @@ import CreateFolderCtrl from 'app/features/folders/CreateFolderCtrl';
 import FolderDashboardsCtrl from 'app/features/folders/FolderDashboardsCtrl';
 import DashboardImportCtrl from 'app/features/manage-dashboards/DashboardImportCtrl';
 import LdapPage from 'app/features/admin/ldap/LdapPage';
-import LdapUserPage from 'app/features/admin/ldap/LdapUserPage';
 import UserAdminPage from 'app/features/admin/UserAdminPage';
 import config from 'app/core/config';
 import { ILocationProvider, route } from 'angular';
