14e45530bbff33cf8d5e240b3d4bdc33af4a0539
MrMaik <github@crayonownage.eu>
2019-11-28 19:35:20 +0100

    add the symbol for ADA to the currency list

--- packages/grafana-data/src/valueFormats/categories.ts
+++ packages/grafana-data/src/valueFormats/categories.ts
@@ -115,6 +115,7 @@ export const getCategories = (): ValueFormatCategory[] => [
       { name: 'Swiss franc (CHF)', id: 'currencyCHF', fn: currency('CHF') },
       { name: 'Polish Złoty (PLN)', id: 'currencyPLN', fn: currency('PLN') },
       { name: 'Bitcoin (฿)', id: 'currencyBTC', fn: currency('฿') },
+      { name: 'ADA (₳)', id: 'currencyADA', fn: currency('₳') },
       { name: 'South African Rand (R)', id: 'currencyZAR', fn: currency('R') },
       { name: 'Indian Rupee (₹)', id: 'currencyINR', fn: currency('₹') },
       { name: 'South Korean Won (₩)', id: 'currencyKRW', fn: currency('₩') },
