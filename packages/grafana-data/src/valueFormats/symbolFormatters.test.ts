94d38fa545eaeacdf565536166c03496072eed5c
MrMaik <github@crayonownage.eu>
2019-11-28 19:37:43 +0100

    add ADA to the tests

--- packages/grafana-data/src/valueFormats/symbolFormatters.test.ts
+++ packages/grafana-data/src/valueFormats/symbolFormatters.test.ts
@@ -7,4 +7,7 @@ describe('Currency', () => {
   it('should format as krw', () => {
     expect(currency('₩')(1532.82, 1, -1)).toEqual('₩1.53K');
   });
+  it('should format as ADA', () => {
+    expect(currency('₳')(1532.82, 1, -1)).toEqual('₳1.53K');
+  });
 });
