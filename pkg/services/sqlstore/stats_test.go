b26b9b220a244142ea41826b2b33bc841259b151
Sofia Papagiannaki <sofia@grafana.com>
2019-11-28 16:14:22 +0200

    Test admins/editors/viewers stats validity

--- pkg/services/sqlstore/stats_test.go
+++ pkg/services/sqlstore/stats_test.go
@@ -2,6 +2,7 @@ package sqlstore
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/grafana/grafana/pkg/models"
@@ -13,9 +14,15 @@ func TestStatsDataAccess(t *testing.T) {
 		InitTestDB(t)
 
 		Convey("Get system stats should not results in error", func() {
+			populateDB()
+
 			query := models.GetSystemStatsQuery{}
 			err := GetSystemStats(&query)
 			So(err, ShouldBeNil)
+			So(query.Result.Users, ShouldEqual, 3)
+			So(query.Result.Editors, ShouldEqual, 1)
+			So(query.Result.Viewers, ShouldEqual, 1)
+			So(query.Result.Admins, ShouldEqual, 3)
 		})
 
 		Convey("Get system user count stats should not results in error", func() {
@@ -48,4 +55,41 @@ func TestStatsDataAccess(t *testing.T) {
 			So(err, ShouldBeNil)
 		})
 	})
+}
+
+func populateDB() {
+	fmt.Println(">>> populateDB")
+	users := make([]models.User, 3)
+	for i := range users {
+		cmd := &models.CreateUserCommand{
+			Email:   fmt.Sprintf("usertest%v@test.com", i),
+			Name:    fmt.Sprintf("user name %v", i),
+			Login:   fmt.Sprintf("user_test_%v_login", i),
+			OrgName: fmt.Sprintf("Org #%v", i),
+		}
+		err := CreateUser(context.Background(), cmd)
+		So(err, ShouldBeNil)
+		users[i] = cmd.Result
+	}
+
+	getOrgByIdQuery := &models.GetOrgByIdQuery{Id: users[0].OrgId}
+	err := GetOrgById(getOrgByIdQuery)
+	So(err, ShouldBeNil)
+	org := getOrgByIdQuery.Result
+
+	cmd := &models.AddOrgUserCommand{
+		OrgId:  org.Id,
+		UserId: users[1].Id,
+		Role:   models.ROLE_EDITOR,
+	}
+	err = AddOrgUser(cmd)
+	So(err, ShouldBeNil)
+
+	cmd = &models.AddOrgUserCommand{
+		OrgId:  org.Id,
+		UserId: users[2].Id,
+		Role:   models.ROLE_VIEWER,
+	}
+	err = AddOrgUser(cmd)
+	So(err, ShouldBeNil)
 }
