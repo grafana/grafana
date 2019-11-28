0f4f5ad3e905cc56a1776512fe2a09af6872b44a
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-11-28 07:57:02 +0100

    Refactor: Adds BASE_URL support

--- packages/grafana-e2e/src/support/url.ts
+++ packages/grafana-e2e/src/support/url.ts
@@ -6,7 +6,7 @@ export const Url: UrlApi = {
   fromBaseUrl: (url: string) => {
     url = url || '';
     const strippedUrl = url.replace('^/', '');
-    const baseUrl = Cypress.config().baseUrl || 'http://localhost:3000';
+    const baseUrl = Cypress.env('BASE_URL') || Cypress.config().baseUrl || 'http://localhost:3000';
     return `${baseUrl}${strippedUrl}`;
   },
 };
