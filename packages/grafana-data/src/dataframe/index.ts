3289ee8b7747693ed7382db7f12b6d71fda38ed1
Ryan McKinley <ryantxu@gmail.com>
2019-12-03 23:17:48 -0800

    @grafana/data: don't export ArrowDataFrame (#20855)

--- packages/grafana-data/src/dataframe/index.ts
+++ packages/grafana-data/src/dataframe/index.ts
@@ -4,4 +4,10 @@ export * from './CircularDataFrame';
 export * from './MutableDataFrame';
 export * from './processDataFrame';
 export * from './dimensions';
-export * from './arrow/ArrowDataFrame';
+
+// NOTE: We can not export arrow in the global scope because it will crash phantomjs
+// In core, this is loaded async.  In plugins you can import using:
+//
+// import { resultsToDataFrames } from '@grafana/data/dataframe/arrow/ArrowDataFrame'
+//
+// export * from './arrow/ArrowDataFrame';
