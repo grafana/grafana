d23e4ac343c729570db31e66383dd7c64c18f016
kay delaney <kay@grafana.com>
2019-12-05 14:45:37 +0000

    Fixes behavior and adds tests

--- public/app/features/explore/state/reducers.ts
+++ public/app/features/explore/state/reducers.ts
@@ -269,7 +269,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
   .addMapper({
     filter: updateDatasourceInstanceAction,
     mapper: (state, action): ExploreItemState => {
-      const { datasourceInstance, version } = action.payload;
+      const { datasourceInstance, version, mode } = action.payload;
 
       // Custom components
       stopQueryState(state.querySubscription);
@@ -291,7 +291,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
       }
 
       const updatedDatasourceInstance = Object.assign(datasourceInstance, { meta: newMetadata });
-      const [supportedModes, mode] = getModesForDatasource(updatedDatasourceInstance, state.mode);
+      const [supportedModes, newMode] = getModesForDatasource(updatedDatasourceInstance, state.mode);
 
       return {
         ...state,
@@ -304,7 +304,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
         loading: false,
         queryKeys: [],
         supportedModes,
-        mode,
+        mode: mode ?? newMode,
         originPanelId: state.urlState && state.urlState.originPanelId,
       };
     },
@@ -657,7 +657,7 @@ const getModesForDatasource = (dataSource: DataSourceApi, currentMode: ExploreMo
 
   // HACK: Used to set Loki's default explore mode to Logs mode.
   // A better solution would be to introduce a "default" or "preferred" mode to the datasource config
-  if (dataSource.meta.name === 'Loki') {
+  if (dataSource.meta.name === 'Loki' && !currentMode) {
     mode = ExploreMode.Logs;
   }
 
