fc344aa35b4be2e12d724b2dee1bead7a16e5593
Hugo Häggmark <hugo.haggmark@grafana.com>
2019-12-10 10:47:31 +0100

    Refactor: Adds params to visit

---
+++ public/e2e-tests/integration/queryVariableCrud.spec.ts
@@ -0,0 +1,14 @@
+import { e2e } from '@grafana/e2e';
+import { ScenarioContext } from '@grafana/e2e/src/support';
+
+e2e.scenario({
+  describeName: 'Smoke tests',
+  itName: 'Login scenario, create test data source, dashboard, panel, and export scenario',
+  addScenarioDataSource: true,
+  addScenarioDashBoard: true,
+  skipScenario: false,
+  scenario: ({ dataSourceName, dashboardTitle, dashboardUid }: ScenarioContext) => {
+    e2e.flows.openDashboard(dashboardUid);
+    e2e.pages.Dashboard.toolbarItems('Dashboard settings').click();
+  },
+});
