0e7456469d222c59fdeaa9068b60d15d7c92191f
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-12-10 12:24:33 +0300

    UI: add link button variant

--- packages/grafana-ui/src/components/Button/styles.ts
+++ packages/grafana-ui/src/components/Button/styles.ts
@@ -94,6 +94,14 @@ export const getButtonStyles = stylesFactory(({ theme, size, variant }: StyleDep
         background: transparent;
       `;
       break;
+
+    case 'link':
+      background = css`
+        ${buttonVariantStyles('', '', theme.colors.linkExternal, 'rgba(0, 0, 0, 0.1)', true)};
+        background: transparent;
+        text-decoration: underline;
+      `;
+      break;
   }
 
   return {
