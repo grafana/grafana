000ca2a548d6d02b42dbfbbae6c658e9634cbfa5
David Kaltschmidt <david.kaltschmidt@gmail.com>
2019-12-11 15:42:52 +0100

    removed comment

--- packages/grafana-ui/src/components/Logs/LogRow.tsx
+++ packages/grafana-ui/src/components/Logs/LogRow.tsx
@@ -140,7 +140,6 @@ class UnThemedLogRow extends PureComponent<Props, State> {
             )}
             {showLabels && row.uniqueLabels && (
               <div className={style.logsRowLabels}>
-                // Only show labels that are unique to this row
                 <LogLabels labels={row.uniqueLabels} />
               </div>
             )}
