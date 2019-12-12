0becebe43ca83250ae8cafa15911f295aafa4f5f
Ryan McKinley <ryantxu@gmail.com>
2019-12-12 14:55:30 -0800

    FieldConfig: support overrides model (#20986)

--- packages/grafana-ui/src/components/SingleStatShared/SingleStatBaseOptions.test.ts
+++ packages/grafana-ui/src/components/SingleStatShared/SingleStatBaseOptions.test.ts
@@ -36,4 +36,30 @@ describe('sharedSingleStatMigrationHandler', () => {
 
     expect(sharedSingleStatMigrationHandler(panel as any)).toMatchSnapshot();
   });
+
+  it('Remove unused `overrides` option', () => {
+    const panel = {
+      options: {
+        fieldOptions: {
+          unit: 'watt',
+          stat: 'last',
+          decimals: 5,
+          defaults: {
+            min: 0,
+            max: 100,
+            mappings: [],
+          },
+          override: {
+            min: 0,
+            max: 100,
+            mappings: [],
+          },
+        },
+      },
+      title: 'Usage',
+      type: 'bargauge',
+    };
+
+    expect(sharedSingleStatMigrationHandler(panel as any)).toMatchSnapshot();
+  });
 });
--- public/app/plugins/panel/stat/types.ts
+++ public/app/plugins/panel/stat/types.ts
@@ -35,7 +35,7 @@ export const standardFieldDisplayOptions: FieldDisplayOptions = {
     ],
     mappings: [],
   },
-  override: {},
+  overrides: [],
 };
 
 export const defaults: StatPanelOptions = {
