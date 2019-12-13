741b74ad73b4a318c7f689e3931c47976496dd49
Dominik Prokop <dominik.prokop@grafana.com>
2019-12-13 17:07:25 +0100

    Export select in Forms namespace

--- packages/grafana-ui/src/components/Forms/index.ts
+++ packages/grafana-ui/src/components/Forms/index.ts
@@ -1,11 +1,13 @@
 import { getFormStyles } from './getFormStyles';
 import { Label } from './Label';
 import { Input } from './Input/Input';
+import { Select } from './Select/Select';
 
 const Forms = {
   getFormStyles,
   Label: Label,
   Input: Input,
+  Select: Select,
 };
 
 export default Forms;
