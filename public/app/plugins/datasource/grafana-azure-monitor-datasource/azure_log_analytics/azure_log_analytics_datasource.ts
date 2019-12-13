db188120a0f44733f95a6cc45253096eb1d515f1
Shavonn Brown <shavonn.brown@grafana.com>
2019-12-13 11:03:08 -0500

    Azure Monitor: Use default from datasource if not saved on dashboard/query (#20899)
    * use default from datasource if not saved on dash/query
    
    * find datasource default workspace in returned workspace list
    
    * don't need the find
    
    * fix dropdown

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_log_analytics/azure_log_analytics_datasource.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_log_analytics/azure_log_analytics_datasource.ts
@@ -87,7 +87,11 @@ export default class AzureLogAnalyticsDatasource {
       );
       const generated = querystringBuilder.generate();
 
-      const workspace = this.templateSrv.replace(item.workspace, options.scopedVars);
+      let workspace = this.templateSrv.replace(item.workspace, options.scopedVars);
+
+      if (!workspace && this.defaultOrFirstWorkspace) {
+        workspace = this.defaultOrFirstWorkspace;
+      }
 
       const url = `${this.baseUrl}/${workspace}/query?${generated.uriString}`;
 
