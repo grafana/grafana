ca81077e4a5e489dcd85a9fcd989d2d8ff203be2
Ryan McKinley <ryantxu@gmail.com>
2019-12-12 17:31:24 -0800

    auto min max

--- packages/grafana-data/src/field/fieldDisplay.ts
+++ packages/grafana-data/src/field/fieldDisplay.ts
@@ -91,7 +91,7 @@ export const getFieldDisplayValues = (options: GetFieldDisplayValuesOptions): Fi
   const values: FieldDisplay[] = [];
 
   if (options.data) {
-    const data = applyFieldOverrides(options.data, fieldOptions, replaceVariables, options.theme);
+    const data = applyFieldOverrides(options);
 
     let hitLimit = false;
     const limit = fieldOptions.limit ? fieldOptions.limit : DEFAULT_FIELD_DISPLAY_VALUES_LIMIT;
