e93aa07c25fb4296b40cb186274d3ee0d5dd8f16
Dominik Prokop <dominik.prokop@grafana.com>
2019-12-17 12:35:23 +0100

    Enable config mocking

--- public/app/core/config.ts
+++ public/app/core/config.ts
@@ -1,5 +1,18 @@
 import { config, GrafanaBootConfig } from '@grafana/runtime';
-
 // Legacy binding paths
 export { config, GrafanaBootConfig as Settings };
+
+let configMock: Partial<GrafanaBootConfig> | null = null;
+
 export default config;
+
+export const getConfig = () => {
+  return configMock || config;
+};
+
+export const mockConfig = (mock: Partial<GrafanaBootConfig>) => {
+  configMock = mock as GrafanaBootConfig;
+  return () => {
+    configMock = null;
+  };
+};
