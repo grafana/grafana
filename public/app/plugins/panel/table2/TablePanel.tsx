7a3a183d012b90f75e335ac3c988efb5403ff2d8
Peter Holmberg <peterholmberg@users.noreply.github.com>
2019-12-18 08:38:50 +0100

    grafana/ui: New table component (#20991)
    * first working example
    
    * Support sorting, adding types while waiting for official ones
    
    * using react-window for windowing
    
    * styles via emotion
    
    * sizing
    
    * set an offset for the table
    
    * change table export
    
    * fixing table cell widths
    
    * Explore: Use new table component in explore (#21031)
    
    * Explore: Use new table component in explore
    
    * enable oncellclick
    
    * only let filterable columns be clickable, refactor renderrow
    
    * remove explore table
    
    * Keep using old merge tables logic
    
    * prettier
    
    * remove unused typings file
    
    * fixing tests
    
    * Fixed explore table issue
    
    * NewTable: Updated styles
    
    * Fixed unit test
    
    * Updated TableModel
    
    * Minor update to explore table height
    
    * typing

--- public/app/plugins/panel/table2/TablePanel.tsx
+++ public/app/plugins/panel/table2/TablePanel.tsx
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 
 // Types
-import { ThemeContext } from '@grafana/ui';
+import { Table } from '@grafana/ui';
 import { PanelProps } from '@grafana/data';
 import { Options } from './types';
-import Table from '@grafana/ui/src/components/Table/Table';
 
 interface Props extends PanelProps<Options> {}
+
+// So that the table does not go all the way to the edge of the panel chrome
+const paddingBottom = 35;
 
 export class TablePanel extends Component<Props> {
   constructor(props: Props) {
@@ -15,16 +17,12 @@ export class TablePanel extends Component<Props> {
   }
 
   render() {
-    const { data, options } = this.props;
+    const { data, height, width } = this.props;
 
     if (data.series.length < 1) {
       return <div>No Table Data...</div>;
     }
 
-    return (
-      <ThemeContext.Consumer>
-        {theme => <Table {...this.props} {...options} theme={theme} data={data.series[0]} />}
-      </ThemeContext.Consumer>
-    );
+    return <Table height={height - paddingBottom} width={width} data={data.series[0]} />;
   }
 }
