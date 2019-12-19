533f3a0ece2107bb6a05ef2ee10e0cfeb45343f0
Kevin Labesse <kevin@labesse.me>
2019-12-19 08:11:05 +0100

    remove await

--- public/app/plugins/datasource/stackdriver/datasource.ts
+++ public/app/plugins/datasource/stackdriver/datasource.ts
@@ -200,7 +200,7 @@ export default class StackdriverDatasource extends DataSourceApi<StackdriverQuer
     let status, message;
     const defaultErrorMessage = 'Cannot connect to Stackdriver API';
     try {
-      const projectName = await this.projectName;
+      const projectName = this.projectName;
       const path = `v3/projects/${projectName}/metricDescriptors`;
       const response = await this.doRequest(`${this.baseUrl}${path}`);
       if (response.status === 200) {
