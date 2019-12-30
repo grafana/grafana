d80342d9fd5e16b198c863bd79ce60bdef42fc04
Ryan McKinley <ryantxu@gmail.com>
2019-12-29 20:10:01 -0800

    support count and currency

--- packages/grafana-data/src/valueFormats/valueFormats.ts
+++ packages/grafana-data/src/valueFormats/valueFormats.ts
@@ -1,7 +1,7 @@
 import { getCategories } from './categories';
 import { DecimalCount } from '../types/displayValue';
 import { toDateTimeValueFormatter } from './dateTimeFormatters';
-import { getOffsetFromSIPrefix, decimalSIPrefix } from './symbolFormatters';
+import { getOffsetFromSIPrefix, decimalSIPrefix, currency } from './symbolFormatters';
 
 export interface FormattedValue {
   text: string;
@@ -196,6 +196,12 @@ export function getValueFormat(id: string): ValueFormatter {
         const offset = getOffsetFromSIPrefix(sub.charAt(0));
         const unit = offset === 0 ? sub : sub.substring(1);
         return decimalSIPrefix(unit, offset);
+      }
+      if (key === 'count') {
+        return simpleCountUnit(sub);
+      }
+      if (key === 'currency') {
+        return currency(sub);
       }
     }
     return toFixedUnit(id);
