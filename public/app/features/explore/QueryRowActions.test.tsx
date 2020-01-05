93e9fa9c6571dcbc70e8daaee763e097307d3285
Ivana <ivana.huckova@gmail.com>
2020-01-05 19:04:24 +0100

    Fix imports, tests

--- public/app/features/explore/QueryRowActions.test.tsx
+++ public/app/features/explore/QueryRowActions.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryRowActions, Props } from './QueryRowActions';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 const setup = (propOverrides?: object) => {
   const props: Props = {
@@ -15,25 +15,26 @@ const setup = (propOverrides?: object) => {
 
   Object.assign(props, propOverrides);
 
-  const wrapper = shallow(<QueryRowActions {...props} />);
-  return wrapper;
+  const mountWrapper = mount(<QueryRowActions {...props} />);
+  const shallowWrapper = shallow(<QueryRowActions {...props} />);
+  return { mountWrapper, shallowWrapper };
 };
 
 describe('QueryRowActions', () => {
   it('should render component', () => {
-    const wrapper = setup();
+    const wrapper = setup().shallowWrapper;
     expect(wrapper).toMatchSnapshot();
   });
   it('should render component without editor mode', () => {
-    const wrapper = setup({ canToggleEditorModes: false });
+    const wrapper = setup({ canToggleEditorModes: false }).shallowWrapper;
     expect(wrapper.find({ 'aria-label': 'Edit mode button' })).toHaveLength(0);
   });
   it('should change icon to fa-eye-slash when query row result is hidden', () => {
-    const wrapper = setup({ isDisabled: true });
-    expect(wrapper.find('i.fa-eye-slash')).toHaveLength(1);
+    const wrapper = setup({ isDisabled: true }).mountWrapper;
+    expect(wrapper.find('i.uil-eye-slash')).toHaveLength(1);
   });
   it('should change icon to fa-eye when query row result is not hidden', () => {
-    const wrapper = setup({ isDisabled: false });
-    expect(wrapper.find('i.fa-eye')).toHaveLength(1);
+    const wrapper = setup({ isDisabled: false }).mountWrapper;
+    expect(wrapper.find('i.uil-eye')).toHaveLength(1);
   });
 });
