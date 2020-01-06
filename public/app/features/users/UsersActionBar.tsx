4b77bb87100d8af116a8cab7cb4553ba23256e97
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-06 05:38:51 +0100

    Explore: updates UserActionBar - refactors it to function component and adds styles

--- public/app/features/users/UsersActionBar.tsx
+++ public/app/features/users/UsersActionBar.tsx
@@ -1,9 +1,22 @@
-import React, { PureComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
+import { css, cx } from 'emotion';
+import { GrafanaTheme } from '@grafana/data';
+import { stylesFactory, ThemeContext } from '@grafana/ui';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { setUsersSearchQuery } from './state/reducers';
 import { getInviteesCount, getUsersSearchQuery } from './state/selectors';
 import { FilterInput } from 'app/core/components/FilterInput/FilterInput';
+
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  btn: css`
+    background: ${theme.colors.primary};
+
+    &:hover {
+      background: ${theme.colors.primaryHover};
+    }
+  `,
+}));
 
 export interface Props {
   searchQuery: string;
@@ -16,67 +29,72 @@ export interface Props {
   externalUserMngLinkName: string;
 }
 
-export class UsersActionBar extends PureComponent<Props> {
-  render() {
-    const {
-      canInvite,
-      externalUserMngLinkName,
-      externalUserMngLinkUrl,
-      searchQuery,
-      pendingInvitesCount,
-      setUsersSearchQuery,
-      onShowInvites,
-      showInvites,
-    } = this.props;
+export const UsersActionBar: FunctionComponent<Props> = (props: Props) => {
+  const {
+    canInvite,
+    externalUserMngLinkName,
+    externalUserMngLinkUrl,
+    searchQuery,
+    pendingInvitesCount,
+    setUsersSearchQuery,
+    onShowInvites,
+    showInvites,
+  } = props;
+  const theme = useContext(ThemeContext);
+  const style = getStyles(theme);
 
-    const pendingInvitesButtonStyle = classNames({
-      btn: true,
-      'toggle-btn': true,
-      active: showInvites,
-    });
+  const pendingInvitesButtonStyle = classNames({
+    btn: true,
+    'toggle-btn': true,
+    active: showInvites,
+  });
 
-    const usersButtonStyle = classNames({
-      btn: true,
-      'toggle-btn': true,
-      active: !showInvites,
-    });
+  const usersButtonStyle = classNames({
+    btn: true,
+    'toggle-btn': true,
+    active: !showInvites,
+  });
 
-    return (
-      <div className="page-action-bar">
-        <div className="gf-form gf-form--grow">
-          <FilterInput
-            labelClassName="gf-form--has-input-icon"
-            inputClassName="gf-form-input width-20"
-            value={searchQuery}
-            onChange={setUsersSearchQuery}
-            placeholder="Filter by email, login or name"
-          />
-          {pendingInvitesCount > 0 && (
-            <div style={{ marginLeft: '1rem' }}>
-              <button className={usersButtonStyle} key="users" onClick={onShowInvites}>
-                Users
-              </button>
-              <button className={pendingInvitesButtonStyle} onClick={onShowInvites} key="pending-invites">
-                Pending Invites ({pendingInvitesCount})
-              </button>
-            </div>
-          )}
-          <div className="page-action-bar__spacer" />
-          {canInvite && (
-            <a className="btn btn-primary" href="org/users/invite">
-              <span>Invite</span>
-            </a>
-          )}
-          {externalUserMngLinkUrl && (
-            <a className="btn btn-primary" href={externalUserMngLinkUrl} target="_blank" rel="noopener">
-              {externalUserMngLinkName}
-            </a>
-          )}
-        </div>
+  return (
+    <div className="page-action-bar">
+      <div className="gf-form gf-form--grow">
+        <FilterInput
+          labelClassName="gf-form--has-input-icon"
+          inputClassName="gf-form-input width-20"
+          value={searchQuery}
+          onChange={setUsersSearchQuery}
+          placeholder="Filter by email, login or name"
+        />
+        {pendingInvitesCount > 0 && (
+          <div style={{ marginLeft: '1rem' }}>
+            <button className={usersButtonStyle} key="users" onClick={onShowInvites}>
+              Users
+            </button>
+            <button className={pendingInvitesButtonStyle} onClick={onShowInvites} key="pending-invites">
+              Pending Invites ({pendingInvitesCount})
+            </button>
+          </div>
+        )}
+        <div className="page-action-bar__spacer" />
+        {canInvite && (
+          <a className={cx('btn', 'btn-primary', style.btn)} href="org/users/invite">
+            <span>Invite</span>
+          </a>
+        )}
+        {externalUserMngLinkUrl && (
+          <a
+            className={cx('btn', 'btn-primary', style.btn)}
+            href={externalUserMngLinkUrl}
+            target="_blank"
+            rel="noopener"
+          >
+            {externalUserMngLinkName}
+          </a>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 function mapStateToProps(state: any) {
   return {
