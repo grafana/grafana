e5aacfab32bb57af8140818be3916e8e216f7d2a
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-06 05:32:38 +0100

    Explore: updates org action bar - refactor from class into function component and adds btn styles

--- public/app/core/components/OrgActionBar/OrgActionBar.tsx
+++ public/app/core/components/OrgActionBar/OrgActionBar.tsx
@@ -1,6 +1,19 @@
-import React, { PureComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
+import { css, cx } from 'emotion';
+import { GrafanaTheme } from '@grafana/data';
+import { stylesFactory, ThemeContext } from '@grafana/ui';
 import LayoutSelector, { LayoutMode } from '../LayoutSelector/LayoutSelector';
 import { FilterInput } from '../FilterInput/FilterInput';
+
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  btn: css`
+    background: ${theme.colors.primary};
+
+    &:hover {
+      background: ${theme.colors.primaryHover};
+    }
+  `,
+}));
 
 export interface Props {
   searchQuery: string;
@@ -11,32 +24,42 @@ export interface Props {
   target?: string;
 }
 
-export default class OrgActionBar extends PureComponent<Props> {
-  render() {
-    const { searchQuery, layoutMode, onSetLayoutMode, linkButton, setSearchQuery, target } = this.props;
-    const linkProps = { href: linkButton.href };
+const OrgActionBar: FunctionComponent<Props> = ({
+  searchQuery,
+  layoutMode,
+  onSetLayoutMode,
+  linkButton,
+  setSearchQuery,
+  target,
+}) => {
+  const linkProps = { href: linkButton.href };
+  const theme = useContext(ThemeContext);
+  const style = getStyles(theme);
 
-    if (target) {
-      (linkProps as any).target = target;
-    }
-
-    return (
-      <div className="page-action-bar">
-        <div className="gf-form gf-form--grow">
-          <FilterInput
-            labelClassName="gf-form--has-input-icon"
-            inputClassName="gf-form-input width-20"
-            value={searchQuery}
-            onChange={setSearchQuery}
-            placeholder={'Filter by name or type'}
-          />
-          <LayoutSelector mode={layoutMode} onLayoutModeChanged={(mode: LayoutMode) => onSetLayoutMode(mode)} />
-        </div>
-        <div className="page-action-bar__spacer" />
-        <a className="btn btn-primary" {...linkProps}>
-          {linkButton.title}
-        </a>
-      </div>
-    );
+  if (target) {
+    (linkProps as any).target = target;
   }
-}
+
+  return (
+    <div className="page-action-bar">
+      <div className="gf-form gf-form--grow">
+        <FilterInput
+          labelClassName="gf-form--has-input-icon"
+          inputClassName={`gf-form-input width-${layoutMode ? 20 : 24}`}
+          value={searchQuery}
+          onChange={setSearchQuery}
+          placeholder={'Filter by name or type'}
+        />
+        {layoutMode ? (
+          <LayoutSelector mode={layoutMode} onLayoutModeChanged={(mode: LayoutMode) => onSetLayoutMode(mode)} />
+        ) : null}
+      </div>
+      <div className="page-action-bar__spacer" />
+      <a className={cx('btn', 'btn-primary', style.btn)} {...linkProps}>
+        {linkButton.title}
+      </a>
+    </div>
+  );
+};
+
+export default OrgActionBar;
