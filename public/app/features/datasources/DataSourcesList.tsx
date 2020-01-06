8e078339d0374b86b565e6176f75ec5672e8c84b
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-06 05:40:17 +0100

    Explore: updates DataSourcesList component - rewrites flexbox list to table

--- public/app/features/datasources/DataSourcesList.tsx
+++ public/app/features/datasources/DataSourcesList.tsx
@@ -1,6 +1,5 @@
 // Libraries
 import React, { FunctionComponent, useContext } from 'react';
-import classNames from 'classnames';
 import { css } from 'emotion';
 import { GrafanaTheme } from '@grafana/data';
 import { stylesFactory, ThemeContext } from '@grafana/ui';
@@ -10,82 +9,57 @@ import DataSourcesListItem from './DataSourcesListItem';
 
 // Types
 import { DataSourceSettings } from '@grafana/data';
-import { List } from '@grafana/ui';
-import { LayoutMode, LayoutModes } from '../../core/components/LayoutSelector/LayoutSelector';
 
 const getStyles = stylesFactory((theme: GrafanaTheme) => ({
-  listHeaders: css`
-    display: flex;
-    flex-direction: row;
-    padding: 0 ${theme.spacing.md};
-
-    @media (max-width: ${theme.breakpoints.md}) {
-      display: none;
-    }
+  tableStyle: css`
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 4px;
   `,
-  listHeadersColumn: css`
-    &:first-child {
-      margin-right: ${theme.spacing.lg};
-      margin-left: ${theme.spacing.sm};
-      width: 55px;
-      min-width: 55px;
-      flex-basis: 10%;
-    }
-
-    &:last-child {
-      flex-basis: 90%;
-      display: flex;
-      font-weight: 700;
-    }
+  tableHeaderLogo: css`
+    min-width: 55px;
+    width: 15%;
+    margin-left: ${theme.spacing.sm};
+    margin-right: ${theme.spacing.lg};
   `,
-  leftHeaderColumn: css`
-    flex-basis: 35%;
-    flex-grow: 1;
+  tableHeaderName: css`
+    width: 30%;
+    font-weight: 700;
   `,
-  centerHeaderColumn: css`
-    flex-basis: 30%;
-    flex-grow: 1;
+  tableHeaderURL: css`
+    width: 30%;
+    font-weight: 700;
   `,
-  rightHeaderColumn: css`
-    flex-basis: 30%;
-    padding-left: 16px;
+  tableHeaderActions: css`
+    width: 25%;
   `,
 }));
 
 export interface Props {
   dataSources: DataSourceSettings[];
-  layoutMode: LayoutMode;
   deleteDataSource: (id: number) => void;
 }
 
-const DataSourcesList: FunctionComponent<Props> = ({ dataSources, layoutMode, deleteDataSource }) => {
+const DataSourcesList: FunctionComponent<Props> = ({ dataSources, deleteDataSource }) => {
   const theme = useContext(ThemeContext);
   const style = getStyles(theme);
 
-  const listStyle = classNames({
-    'card-section': true,
-    'card-list-layout-grid': layoutMode === LayoutModes.Grid,
-    'card-list-layout-list': layoutMode === LayoutModes.List,
-  });
-
   return (
-    <section className={listStyle}>
-      <div className={style.listHeaders}>
-        <div className={style.listHeadersColumn} />
-        <div className={style.listHeadersColumn}>
-          <div className={style.leftHeaderColumn}>Name</div>
-          <div className={style.centerHeaderColumn}>URL</div>
-          <div className={style.rightHeaderColumn} />
-        </div>
-      </div>
-      <List
-        items={dataSources}
-        getItemKey={item => item.id.toString()}
-        renderItem={item => (
+    <table className={style.tableStyle}>
+      <thead>
+        <tr>
+          <th className={style.tableHeaderLogo}>&nbsp;</th>
+          <th className={style.tableHeaderName}>Name</th>
+          <th className={style.tableHeaderURL}>URL</th>
+          <th className={style.tableHeaderActions} />
+        </tr>
+      </thead>
+      <tbody>
+        {dataSources.map(item => (
           <DataSourcesListItem dataSource={item} key={item.id.toString()} deleteDataSource={deleteDataSource} />
-        )}
-      />
-    </section>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
