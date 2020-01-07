0fe5568ea812fd18d570c8dd142e7a1cedfd6eb3
Torkel Ödegaard <torkel@grafana.com>
2020-01-07 18:29:25 +0100

    Footer: Single footer implementation for both react & angular pages

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -1,61 +1,68 @@
 import React, { FC } from 'react';
-import { Tooltip } from '@grafana/ui';
+import config from 'app/core/config';
 
-interface Props {
-  appName: string;
-  buildVersion: string;
-  buildCommit: string;
-  newGrafanaVersionExists: boolean;
-  newGrafanaVersion: string;
+interface FooterLink {
+  text: string;
+  icon?: string;
+  url?: string;
 }
 
-export const Footer: FC<Props> = React.memo(
-  ({ appName, buildVersion, buildCommit, newGrafanaVersionExists, newGrafanaVersion }) => {
-    return (
-      <footer className="footer">
-        <div className="text-center">
-          <ul>
-            <li>
-              <a href="http://docs.grafana.org" target="_blank" rel="noopener">
-                <i className="fa fa-file-code-o" /> Docs
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://grafana.com/products/enterprise/?utm_source=grafana_footer"
-                target="_blank"
-                rel="noopener"
-              >
-                <i className="fa fa-support" /> Support & Enterprise
-              </a>
-            </li>
-            <li>
-              <a href="https://community.grafana.com/" target="_blank" rel="noopener">
-                <i className="fa fa-comments-o" /> Community
-              </a>
-            </li>
-            <li>
-              <a href="https://grafana.com" target="_blank" rel="noopener">
-                {appName}
-              </a>{' '}
-              <span>
-                v{buildVersion} (commit: {buildCommit})
-              </span>
-            </li>
-            {newGrafanaVersionExists && (
-              <li>
-                <Tooltip placement="auto" content={newGrafanaVersion}>
-                  <a href="https://grafana.com/get" target="_blank" rel="noopener">
-                    New version available!
-                  </a>
-                </Tooltip>
-              </li>
-            )}
-          </ul>
-        </div>
-      </footer>
-    );
-  }
-);
+let getFooterLinks = (): FooterLink[] => {
+  const { buildInfo } = config;
 
-export default Footer;
+  const links = [
+    {
+      text: 'Docs',
+      icon: 'fa fa-file-code-o',
+      url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
+    },
+    {
+      text: 'Support & Enterprise',
+      icon: 'fa fa-support',
+      url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
+    },
+    {
+      text: 'Community',
+      icon: 'fa fa-comments-o',
+      url: 'https://community.grafana.com/?utm_source=grafana_footer',
+    },
+    {
+      text: `Grafana v${buildInfo.version} (commit: ${buildInfo.commit})`,
+      icon: 'fa fa-comments-o',
+    },
+  ];
+
+  if (buildInfo.hasUpdate) {
+    links.push({
+      text: `New version available`,
+      icon: 'fa fa-download',
+      url: 'https://grafana.com/grafana/download?utm_source=grafana_footer',
+    });
+  }
+
+  return links;
+};
+
+export function setFooterLinksFn(fn: typeof getFooterLinks) {
+  getFooterLinks = fn;
+}
+
+export const Footer: FC = React.memo(() => {
+  const links = getFooterLinks();
+
+  return (
+    <footer className="footer">
+      <div className="text-center">
+        <ul>
+          {links.map(link => (
+            <li>
+              <a href={link.url} target="_blank" rel="noopener">
+                <i className={link.icon} /> {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+});
