db419099890b1f47d29d362e46fc306cc44de762
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-08 04:15:34 +0100

    Explore: updates result processor get logs result method to avoid counting invalid/negative line limits

--- public/app/features/explore/utils/ResultProcessor.ts
+++ public/app/features/explore/utils/ResultProcessor.ts
@@ -102,7 +102,8 @@ export class ResultProcessor {
       return null;
     }
 
-    const maxLinesLimit = this.state?.queries?.reduce((acc, query) => acc + query.maxLines, 0) || 1000;
+    const maxLinesLimit =
+      this.state?.queries?.reduce((acc, query) => acc + (query.maxLines > 0 ? query.maxLines : 0), 0) || 1000;
     const newResults = dataFrameToLogsModel(this.dataFrames, this.intervalMs, this.timeZone, maxLinesLimit);
     const sortOrder = refreshIntervalToSortOrder(this.state.refreshInterval);
     const sortedNewResults = sortLogsResult(newResults, sortOrder);
