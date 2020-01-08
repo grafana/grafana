426a37f91c68e66a5965b38eb9a93cbf58212272
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-08 05:49:03 +0100

    Explore: adds a method for query limit preprocessing/mapping

--- public/app/plugins/datasource/loki/components/LokiExploreQueryEditor.tsx
+++ public/app/plugins/datasource/loki/components/LokiExploreQueryEditor.tsx
@@ -40,12 +40,26 @@ export const LokiQueryEditor = memo(function LokiQueryEditor(props: Props) {
   function onChangeQueryLimit(value: string, override?: boolean) {
     const { query, onChange, onRunQuery } = props;
     if (onChange) {
-      const nextQuery = { ...query, maxLines: +value };
+      const nextQuery = { ...query, maxLines: preprocessMaxLines(value) };
       onChange(nextQuery);
 
       if (override && onRunQuery) {
         onRunQuery();
       }
+    }
+  }
+
+  function preprocessMaxLines(value: string): number {
+    if (value.length === 0) {
+      // empty input - falls back to dataSource.maxLines limit
+      return NaN;
+    } else if (value.length > 0 && (isNaN(+value) || +value < 0)) {
+      // input with at least 1 character and that is either incorrect (value in the input field is not a number) or negative
+      // falls back to the limit of 0 lines
+      return 0;
+    } else {
+      // default case - correct input
+      return +value;
     }
   }
 
