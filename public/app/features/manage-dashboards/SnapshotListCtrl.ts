8e57510b7cd35fc0d203a3e80be33e961f172faa
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-01-10 15:09:01 +0100

    Fix: Fixes digest issues with Snapshots

--- public/app/features/manage-dashboards/SnapshotListCtrl.ts
+++ public/app/features/manage-dashboards/SnapshotListCtrl.ts
@@ -1,38 +1,49 @@
 import _ from 'lodash';
+import { ILocationService, IScope } from 'angular';
+import { getBackendSrv } from '@grafana/runtime';
+
 import { NavModelSrv } from 'app/core/core';
-import { ILocationService } from 'angular';
 import { GrafanaRootScope } from 'app/routes/GrafanaCtrl';
 import { CoreEvents } from 'app/types';
-import { getBackendSrv } from '@grafana/runtime';
+import { promiseToDigest } from '../../core/utils/promiseToDigest';
 
 export class SnapshotListCtrl {
   navModel: any;
   snapshots: any;
 
   /** @ngInject */
-  constructor(private $rootScope: GrafanaRootScope, navModelSrv: NavModelSrv, private $location: ILocationService) {
+  constructor(
+    private $rootScope: GrafanaRootScope,
+    navModelSrv: NavModelSrv,
+    private $location: ILocationService,
+    private $scope: IScope
+  ) {
     this.navModel = navModelSrv.getNav('dashboards', 'snapshots', 0);
-    getBackendSrv()
-      .get('/api/dashboard/snapshots')
-      .then((result: any) => {
-        const baseUrl = this.$location.absUrl().replace($location.url(), '');
-        this.snapshots = result.map((snapshot: any) => ({
-          ...snapshot,
-          url: snapshot.externalUrl || `${baseUrl}/dashboard/snapshot/${snapshot.key}`,
-        }));
-      });
+    promiseToDigest(this.$scope)(
+      getBackendSrv()
+        .get('/api/dashboard/snapshots')
+        .then((result: any) => {
+          const baseUrl = this.$location.absUrl().replace($location.url(), '');
+          this.snapshots = result.map((snapshot: any) => ({
+            ...snapshot,
+            url: snapshot.externalUrl || `${baseUrl}/dashboard/snapshot/${snapshot.key}`,
+          }));
+        })
+    );
   }
 
   removeSnapshotConfirmed(snapshot: any) {
     _.remove(this.snapshots, { key: snapshot.key });
-    getBackendSrv()
-      .delete('/api/snapshots/' + snapshot.key)
-      .then(
-        () => {},
-        () => {
-          this.snapshots.push(snapshot);
-        }
-      );
+    promiseToDigest(this.$scope)(
+      getBackendSrv()
+        .delete('/api/snapshots/' + snapshot.key)
+        .then(
+          () => {},
+          () => {
+            this.snapshots.push(snapshot);
+          }
+        )
+    );
   }
 
   removeSnapshot(snapshot: any) {
