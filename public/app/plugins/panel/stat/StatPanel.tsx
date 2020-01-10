223d1c7537477cfbcb6bbdf40e2a87dcd7eb4116
Torkel Ödegaard <torkel@grafana.com>
2020-01-10 15:44:41 +0100

    Fixed auto min max

--- public/app/plugins/panel/stat/StatPanel.tsx
+++ public/app/plugins/panel/stat/StatPanel.tsx
@@ -77,6 +77,7 @@ export class StatPanel extends PureComponent<PanelProps<StatPanelOptions>> {
       theme: config.theme,
       data: data.series,
       sparkline: options.graphMode !== BigValueGraphMode.None,
+      autoMinMax: true,
     });
   };
 
