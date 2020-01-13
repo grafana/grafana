49ded55fd08cb13b0943830511c039cc0c506057
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-01-13 13:28:31 +0100

    Fix: Fixes digest issue with CreateFolderCtrl and FolderDashboardsCtrl

--- public/app/features/folders/FolderDashboardsCtrl.ts
+++ public/app/features/folders/FolderDashboardsCtrl.ts
@@ -1,7 +1,9 @@
+import { ILocationService, IScope } from 'angular';
+
 import { FolderPageLoader } from './services/FolderPageLoader';
 import locationUtil from 'app/core/utils/location_util';
 import { NavModelSrv } from 'app/core/core';
-import { ILocationService } from 'angular';
+import { promiseToDigest } from '../../core/utils/promiseToDigest';
 
 export default class FolderDashboardsCtrl {
   navModel: any;
@@ -9,19 +11,26 @@ export default class FolderDashboardsCtrl {
   uid: string;
 
   /** @ngInject */
-  constructor(navModelSrv: NavModelSrv, private $routeParams: any, $location: ILocationService) {
+  constructor(
+    navModelSrv: NavModelSrv,
+    private $routeParams: any,
+    $location: ILocationService,
+    private $scope: IScope
+  ) {
     if (this.$routeParams.uid) {
       this.uid = $routeParams.uid;
 
       const loader = new FolderPageLoader();
 
-      loader.load(this, this.uid, 'manage-folder-dashboards').then((folder: any) => {
-        const url = locationUtil.stripBaseFromUrl(folder.url);
+      promiseToDigest(this.$scope)(
+        loader.load(this, this.uid, 'manage-folder-dashboards').then((folder: any) => {
+          const url = locationUtil.stripBaseFromUrl(folder.url);
 
-        if (url !== $location.path()) {
-          $location.path(url).replace();
-        }
-      });
+          if (url !== $location.path()) {
+            $location.path(url).replace();
+          }
+        })
+      );
     }
   }
 }
