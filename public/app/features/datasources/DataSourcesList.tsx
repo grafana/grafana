88dd164b9b1335e7ef865c72642b892cf1f66180
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-14 11:37:06 +0100

    Datasources: removes unnecessary styles and table headers

--- public/app/features/datasources/DataSourcesList.tsx
+++ public/app/features/datasources/DataSourcesList.tsx
@@ -1,8 +1,5 @@
 // Libraries
-import React, { FunctionComponent, useContext } from 'react';
-import { css } from 'emotion';
-import { GrafanaTheme } from '@grafana/data';
-import { stylesFactory, ThemeContext } from '@grafana/ui';
+import React, { FunctionComponent } from 'react';
 
 // Components
 import DataSourcesListItem from './DataSourcesListItem';
@@ -10,56 +7,18 @@ import DataSourcesListItem from './DataSourcesListItem';
 // Types
 import { DataSourceSettings } from '@grafana/data';
 
-const getStyles = stylesFactory((theme: GrafanaTheme) => ({
-  tableStyle: css`
-    width: 100%;
-    border-collapse: separate;
-    border-spacing: 0 4px;
-  `,
-  tableHeaderLogo: css`
-    min-width: 55px;
-    width: 15%;
-    margin-left: ${theme.spacing.sm};
-    margin-right: ${theme.spacing.lg};
-  `,
-  tableHeaderName: css`
-    width: 30%;
-    font-weight: 700;
-  `,
-  tableHeaderURL: css`
-    width: 30%;
-    font-weight: 700;
-  `,
-  tableHeaderActions: css`
-    width: 25%;
-  `,
-}));
-
 export interface Props {
   dataSources: DataSourceSettings[];
   deleteDataSource: (id: number) => void;
 }
 
 const DataSourcesList: FunctionComponent<Props> = ({ dataSources, deleteDataSource }) => {
-  const theme = useContext(ThemeContext);
-  const style = getStyles(theme);
-
   return (
-    <table className={style.tableStyle}>
-      <thead>
-        <tr>
-          <th className={style.tableHeaderLogo}>&nbsp;</th>
-          <th className={style.tableHeaderName}>Name</th>
-          <th className={style.tableHeaderURL}>URL</th>
-          <th className={style.tableHeaderActions} />
-        </tr>
-      </thead>
-      <tbody>
-        {dataSources.map(item => (
-          <DataSourcesListItem dataSource={item} key={item.id.toString()} deleteDataSource={deleteDataSource} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {dataSources.map(item => (
+        <DataSourcesListItem dataSource={item} key={item.id.toString()} deleteDataSource={deleteDataSource} />
+      ))}
+    </>
   );
 };
 
