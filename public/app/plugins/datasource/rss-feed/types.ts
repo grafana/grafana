67d5be281c56cf52382e4da87543836814944ea1
Dominik Prokop <dominik.prokop@grafana.com>
2020-01-16 14:55:29 +0100

    Update public/app/plugins/datasource/rss-feed/types.ts

--- public/app/plugins/datasource/rss-feed/types.ts
+++ public/app/plugins/datasource/rss-feed/types.ts
@@ -3,7 +3,6 @@ import { DataQuery, DataSourceJsonData } from '@grafana/data';
 export interface RSSFeedQuery extends DataQuery {}
 
 export interface RSSFeedOptions extends DataSourceJsonData {
-  // Saved in the datasource and download with bootData
   feedUrl?: string;
   proxyUrl?: string;
 }
