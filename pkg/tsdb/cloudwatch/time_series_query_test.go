8d6609fd72b3026e6a642266939a212e58777a94
Erik Sundell <erik.sundell87@gmail.com>
2020-01-16 08:45:02 +0100

    Fix broken tests

--- pkg/tsdb/cloudwatch/time_series_query_test.go
+++ pkg/tsdb/cloudwatch/time_series_query_test.go
@@ -1,6 +1,7 @@
 package cloudwatch
 
 import (
+	"context"
 	"testing"
 
 	"github.com/grafana/grafana/pkg/tsdb"
@@ -14,12 +15,12 @@ func TestTimeSeriesQuery(t *testing.T) {
 
 		Convey("Time range is valid", func() {
 			Convey("End time before start time should result in error", func() {
-				_, err := executor.executeTimeSeriesQuery(nil, &tsdb.TsdbQuery{TimeRange: tsdb.NewTimeRange("now-1h", "now-2h")})
+				_, err := executor.executeTimeSeriesQuery(context.TODO(), &tsdb.TsdbQuery{TimeRange: tsdb.NewTimeRange("now-1h", "now-2h")})
 				So(err.Error(), ShouldEqual, "Invalid time range: Start time must be before end time")
 			})
 
 			Convey("End time equals start time should result in error", func() {
-				_, err := executor.executeTimeSeriesQuery(nil, &tsdb.TsdbQuery{TimeRange: tsdb.NewTimeRange("now-1h", "now-1h")})
+				_, err := executor.executeTimeSeriesQuery(context.TODO(), &tsdb.TsdbQuery{TimeRange: tsdb.NewTimeRange("now-1h", "now-1h")})
 				So(err.Error(), ShouldEqual, "Invalid time range: Start time must be before end time")
 			})
 		})
