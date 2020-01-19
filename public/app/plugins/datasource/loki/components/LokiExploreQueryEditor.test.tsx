cf0492f1b8ef2a4e20a8169eaf456c3504690bba
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-19 23:56:03 +0100

    Explore: adds render test of LokiExploreQueryEditor

---
+++ public/app/plugins/datasource/loki/components/LokiExploreQueryEditor.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LokiExploreQueryEditor from './LokiExploreQueryEditor';
+import { LokiDatasource } from '../datasource';
+import { LokiQuery } from '../types';
+import { ExploreMode, PanelData, LoadingState, dateTime } from '@grafana/data';
+
+const setup = (propOverrides?: object) => {
+  const datasourceMock: unknown = {};
+  const datasource: LokiDatasource = datasourceMock as LokiDatasource;
+  const onRunQuery = jest.fn();
+  const onChange = jest.fn();
+  const query: LokiQuery = { expr: '', refId: 'A', maxLines: 0 };
+  const data: PanelData = {
+    state: LoadingState.NotStarted,
+    series: [],
+    request: {
+      requestId: '1',
+      dashboardId: 1,
+      interval: '1s',
+      panelId: 1,
+      range: {
+        from: dateTime('2020-01-01', 'YYYY-MM-DD'),
+        to: dateTime('2020-01-02', 'YYYY-MM-DD'),
+        raw: {
+          from: dateTime('2020-01-01', 'YYYY-MM-DD'),
+          to: dateTime('2020-01-02', 'YYYY-MM-DD'),
+        },
+      },
+      scopedVars: {},
+      targets: [],
+      timezone: 'GMT',
+      app: 'Grafana',
+      startTime: 0,
+    },
+    timeRange: {
+      from: dateTime('2020-01-01', 'YYYY-MM-DD'),
+      to: dateTime('2020-01-02', 'YYYY-MM-DD'),
+      raw: {
+        from: dateTime('2020-01-01', 'YYYY-MM-DD'),
+        to: dateTime('2020-01-02', 'YYYY-MM-DD'),
+      },
+    },
+  };
+  const history: any[] = [];
+  const exploreMode: ExploreMode = ExploreMode.Logs;
+
+  const props: any = {
+    query,
+    data,
+    datasource,
+    exploreMode,
+    history,
+    onChange,
+    onRunQuery,
+  };
+
+  Object.assign(props, propOverrides);
+
+  return shallow(<LokiExploreQueryEditor {...props} />);
+};
+
+describe('LokiExploreExtraField', () => {
+  it('should render component', () => {
+    const wrapper = setup();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
