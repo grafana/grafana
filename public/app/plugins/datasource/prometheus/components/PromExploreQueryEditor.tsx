e8c3c0ca25027485ba1d8ebc864ad1b498686ca6
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-20 05:48:02 +0100

    Datasource: updates PromExploreQueryEditor - rewrite to functional component

--- public/app/plugins/datasource/prometheus/components/PromExploreQueryEditor.tsx
+++ public/app/plugins/datasource/prometheus/components/PromExploreQueryEditor.tsx
@@ -1,85 +1,65 @@
-import React, { PureComponent } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 
 // Types
 import { ExploreQueryFieldProps } from '@grafana/data';
-import { FormLabel } from '@grafana/ui';
 
 import { PrometheusDatasource } from '../datasource';
 import { PromQuery, PromOptions } from '../types';
 
 import PromQueryField from './PromQueryField';
+import { PromExploreExtraField } from './PromExploreExtraField';
+
 export type Props = ExploreQueryFieldProps<PrometheusDatasource, PromQuery, PromOptions>;
 
-interface State {
-  interval: string;
-}
+export const PromExploreQueryEditor = memo(function PromExploreQueryEditor(props: Props) {
+  const { query, data, datasource, history, onChange, onRunQuery } = props;
+  const [step, setStep] = useState(query.interval);
 
-export class PromExploreQueryEditor extends PureComponent<Props, State> {
-  // Query target to be modified and used for queries
-  query: PromQuery;
-
-  constructor(props: Props) {
-    super(props);
-    const { query } = props;
-    this.query = query;
-    // Query target properties that are fully controlled inputs
-    this.state = {
-      // Fully controlled text inputs
-      interval: query.interval,
-    };
-  }
-
-  onFieldChange = (query: PromQuery, override?: any) => {
-    this.query.expr = query.expr;
-  };
-
-  onIntervalChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const interval = e.currentTarget.value;
-    this.query.interval = interval;
-    this.setState({ interval });
-  };
-
-  onRunQuery = () => {
-    const { query } = this;
-    this.props.onChange(query);
-    this.props.onRunQuery();
-  };
-
-  onReturnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      this.onRunQuery();
+  function onChangeQueryStep(value: string, override?: boolean) {
+    const { query, onChange, onRunQuery } = props;
+    if (onChange) {
+      const nextQuery = { ...query, interval: step };
+      onChange(nextQuery);
+      if (override && onRunQuery) {
+        onRunQuery();
+      }
     }
-  };
-
-  render() {
-    const { datasource, query, data, history } = this.props;
-    const { interval } = this.state;
-
-    return (
-      <div className="gf-form-inline">
-        <PromQueryField
-          datasource={datasource}
-          query={query}
-          onRunQuery={this.onRunQuery}
-          onChange={this.onFieldChange}
-          history={history}
-          data={data}
-        >
-          <div className="gf-form-inline explore-input--ml">
-            <div className="gf-form">
-              <FormLabel width={4}>Step</FormLabel>
-              <input
-                type="text"
-                className="gf-form-input width-6"
-                placeholder={'auto'}
-                onChange={this.onIntervalChange}
-                onKeyDown={this.onReturnKeyDown}
-                value={interval}
-              />
-            </div>
-          </div>
-        </PromQueryField>
-      </div>
-    );
   }
-}
+
+  function onStepChange(e: React.SyntheticEvent<HTMLInputElement>) {
+    setStep(e.currentTarget.value);
+  }
+
+  function onReturnKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      onRunQuery();
+    }
+  }
+
+  useEffect(() => {
+    onChangeQueryStep(step);
+  }, [step]);
+
+  return (
+    <div className="gf-form-inline">
+      <PromQueryField
+        datasource={datasource}
+        query={query}
+        onRunQuery={onRunQuery}
+        onChange={onChange}
+        history={history}
+        data={data}
+        ExtraFieldElement={
+          <PromExploreExtraField
+            label={'Step'}
+            onChangeFunc={onStepChange}
+            onKeyDownFunc={onReturnKeyDown}
+            value={step}
+          />
+        }
+      />
+    </div>
+  );
+});
+
+export default PromExploreQueryEditor;
