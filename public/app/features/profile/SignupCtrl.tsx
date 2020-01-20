3cbff9879af9a98ab82f5d894a23e8ab0857529f
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-01-20 14:09:02 +0100

    Add backend call

--- public/app/features/profile/SignupCtrl.tsx
+++ public/app/features/profile/SignupCtrl.tsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { getBackendSrv } from '@grafana/runtime';
 
 interface SignupProps {
   children(arg0: ChildProps): JSX.Element;
@@ -9,7 +10,20 @@ interface ChildProps {
 }
 
 export class SignupCtrl extends PureComponent<SignupProps> {
-  onSubmit = () => {};
+  constructor(props: SignupProps) {
+    super(props);
+  }
+  onSubmit = async () => {
+    const backendSrv = getBackendSrv();
+
+    const response = await backendSrv.post('/api/user/signup/step2');
+    if (response.code === 'redirect-to-select-org') {
+      //select location
+      console.log('Redirecting...');
+    }
+
+    // redirect to /
+  };
 
   render() {
     const { children } = this.props;
