cc2586d250d7420ede652b6e8900e3d8e3febd03
Leonard Gram <leo@xlson.com>
2020-01-20 08:35:45 +0100

    CI: MSI for Enterprise (#21569)
    * CI: ee msi build
    
    * shellcheck
    
    * Fake it so that we get a ee msi build on pull request runs
    
    * fix error in prep job
    
    * save msi for testing
    
    * get enterprise zip
    
    * Fixes to version and hash detection.
    
    * Removes ci test code

--- scripts/build/ci-msi-build/ci-msi-build-ee.sh
+++ scripts/build/ci-msi-build/ci-msi-build-ee.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 WORKING_DIRECTORY=$(pwd)
+DIST_DIRECTORY="$WORKING_DIRECTORY/enterprise-dist"
 # copy zip file to /tmp/dist
 mkdir -p /tmp/dist
-cp ./dist/*.zip /tmp/dist
+cp ./enterprise-dist/*.zip /tmp/dist
 echo "Contents of /tmp/dist"
 ls -al /tmp/dist
 
@@ -11,24 +12,18 @@ ls -al /tmp/dist
 echo "Caching NSSM"
 mkdir -p /tmp/cache
 cp ./scripts/build/ci-msi-build/msigenerator/cache/nssm-2.24.zip /tmp/cache
-# a build can be specified, which will be pulled down
-#python3 generator/build.py --build 5.4.3
-#echo "LIGHT config"
-#ls -al /home/xclient/wix/light.exe.config
-#cat /home/xclient/wix/light.exe.config
-#cp ./scripts/build/ci-msi-build/oss/light.exe.config /home/xclient/wix/light.exe.config
-#cat /home/xclient/wix/light.exe.config
+
 cd ./scripts/build/ci-msi-build/msigenerator
 echo "Building MSI"
 python3 generator/build.py "$@"
 chmod a+x /tmp/scratch/*.msi
-echo "MSI: Copy to $WORKING_DIRECTORY/dist"
-cp /tmp/scratch/*.msi "$WORKING_DIRECTORY/dist"
+echo "MSI: Copy to $DIST_DIRECTORY"
+cp /tmp/scratch/*.msi "$DIST_DIRECTORY"
 echo "MSI: Generate SHA256"
-MSI_FILE=$(ls "$WORKING_DIRECTORY"/dist/*.msi)
+MSI_FILE=$(ls "${DIST_DIRECTORY}"/*.msi)
 SHA256SUM=$(sha256sum "$MSI_FILE" | cut -f1 -d' ')
 echo "$SHA256SUM" > "$MSI_FILE.sha256"
 echo "MSI: SHA256 file content:"
 cat "$MSI_FILE.sha256"
-echo "MSI: contents of $WORKING_DIRECTORY/dist"
-ls -al "$WORKING_DIRECTORY/dist"
+echo "MSI: contents of $DIST_DIRECTORY"
+ls -al "$DIST_DIRECTORY"
