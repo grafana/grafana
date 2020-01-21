b5eda776681bffaf4668d64d8da3873b182cd608
Dominik Prokop <dominik.prokop@grafana.com>
2020-01-21 11:39:50 +0100

    Temporarily fix story (before 21635 is merged)

--- packages/grafana-ui/src/components/Forms/Form.story.tsx
+++ packages/grafana-ui/src/components/Forms/Form.story.tsx
@@ -27,76 +27,88 @@ export const users = () => {
 
   return (
     <>
-      <Form>
-        <Legend>Edit user</Legend>
-        <Field label="Name">
-          <Input
-            id="name"
-            placeholder="Roger Waters"
-            value={name}
-            onChange={e => setName(e.currentTarget.value)}
-            size="md"
-          />
-        </Field>
-        <Field label="Email">
-          <Input
-            id="email"
-            type="email"
-            placeholder="roger.waters@grafana.com"
-            value={email}
-            onChange={e => setEmail(e.currentTarget.value)}
-            size="md"
-          />
-        </Field>
-        <Field label="Username">
-          <Input
-            id="username"
-            placeholder="mr.waters"
-            value={username}
-            onChange={e => setUsername(e.currentTarget.value)}
-            size="md"
-          />
-        </Field>
-        <Field label="Disable" description="Added for testing purposes">
-          <Switch checked={disabledUser} onChange={(_e, checked) => setDisabledUser(checked)} />
-        </Field>
-        <Field>
-          <Checkbox
-            label="Skip SLL cert validation"
-            description="Set to true if you want to skip sll cert validation"
-            value={checked}
-            onChange={setChecked}
-          />
-        </Field>
-        <Button>Update</Button>
+      <Form onSubmit={() => {}}>
+        {() => (
+          <>
+            <Legend>Edit user</Legend>
+            <Field label="Name">
+              <Input
+                id="name"
+                placeholder="Roger Waters"
+                value={name}
+                onChange={e => setName(e.currentTarget.value)}
+                size="md"
+              />
+            </Field>
+            <Field label="Email">
+              <Input
+                id="email"
+                type="email"
+                placeholder="roger.waters@grafana.com"
+                value={email}
+                onChange={e => setEmail(e.currentTarget.value)}
+                size="md"
+              />
+            </Field>
+            <Field label="Username">
+              <Input
+                id="username"
+                placeholder="mr.waters"
+                value={username}
+                onChange={e => setUsername(e.currentTarget.value)}
+                size="md"
+              />
+            </Field>
+            <Field label="Disable" description="Added for testing purposes">
+              <Switch checked={disabledUser} onChange={(_e, checked) => setDisabledUser(checked)} />
+            </Field>
+            <Field>
+              <Checkbox
+                label="Skip SLL cert validation"
+                description="Set to true if you want to skip sll cert validation"
+                value={checked}
+                onChange={setChecked}
+              />
+            </Field>
+            <Button>Update</Button>
+          </>
+        )}
       </Form>
-      <Form>
-        <Legend>Change password</Legend>
-        <Field label="Password">
-          <Input
-            id="password>"
-            type="password"
-            placeholder="Be safe..."
-            value={password}
-            onChange={e => setPassword(e.currentTarget.value)}
-            size="md"
-            prefix={<Icon name="lock" />}
-          />
-        </Field>
-        <Button>Update</Button>
+      <Form onSubmit={() => {}}>
+        {() => (
+          <>
+            <Legend>Change password</Legend>
+            <Field label="Password">
+              <Input
+                id="password>"
+                type="password"
+                placeholder="Be safe..."
+                value={password}
+                onChange={e => setPassword(e.currentTarget.value)}
+                size="md"
+                prefix={<Icon name="lock" />}
+              />
+            </Field>
+            <Button>Update</Button>
+          </>
+        )}
       </Form>
 
-      <Form>
-        <fieldset>
-          <Legend>CERT validation</Legend>
-          <Field
-            label="Path to client cert"
-            description="Authentication against LDAP servers requiring client certificates if not required leave empty "
-          >
-            <TextArea id="clientCert" value={''} size="lg" />
-          </Field>
-        </fieldset>
-        <Button>Update</Button>
+      <Form onSubmit={() => {}}>
+        {() => (
+          <>
+            <fieldset>
+              <Legend>CERT validation</Legend>
+              <Field
+                label="Path to client cert"
+                description="Authentication against LDAP servers requiring client certificates if not required leave empty "
+              >
+                <TextArea id="clientCert" value={''} size="lg" />
+              </Field>
+            </fieldset>
+            <Button>Update</Button>
+          </>
+        )}
       </Form>
     </>
   );
