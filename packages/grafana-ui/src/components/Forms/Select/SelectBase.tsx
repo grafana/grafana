479070d52209387103d1aafde09182f8861084d2
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-01-22 15:45:51 +0100

    Fix imports

--- packages/grafana-ui/src/components/Forms/Select/SelectBase.tsx
+++ packages/grafana-ui/src/components/Forms/Select/SelectBase.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { SelectableValue, deprecationWarning } from '@grafana/data';
 // @ts-ignore
-import { default as ReactSelect, Creatable, AsyncSelect as ReactAsyncSelect } from '@torkelo/react-select';
+import { default as ReactSelect } from '@torkelo/react-select';
 // @ts-ignore
-// import Creatable from '@torkelo/react-select/creatable';
+import Creatable from '@torkelo/react-select/creatable';
 // @ts-ignore
-// import { default as ReactAsyncSelect } from '@torkelo/react-select/async';
+import { default as ReactAsyncSelect } from '@torkelo/react-select/async';
 
 import { Icon } from '../../Icon/Icon';
 import { css, cx } from 'emotion';
