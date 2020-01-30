d7ea31d2a63cbd3dc8039857370d8a1c8d6fe63d
Lukas Siatka <lukasz.siatka@grafana.com>
2020-01-30 16:58:53 +0000

    Explore: updates LokiExploreQueryEditor tests and test setup

--- public/app/plugins/datasource/loki/components/LokiExploreQueryEditor.test.tsx
+++ public/app/plugins/datasource/loki/components/LokiExploreQueryEditor.test.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import LokiExploreQueryEditor from './LokiExploreQueryEditor';
-import { LokiExploreExtraField } from './LokiExploreExtraField';
 import { LokiDatasource } from '../datasource';
 import { LokiQuery } from '../types';
 import { ExploreMode, PanelData, LoadingState, dateTime } from '@grafana/data';
 
-const setup = (propOverrides?: object) => {
+const setup = (renderMethod: any, propOverrides?: object) => {
   const datasourceMock: unknown = {};
   const datasource: LokiDatasource = datasourceMock as LokiDatasource;
   const onRunQuery = jest.fn();
@@ -56,77 +55,23 @@ const setup = (propOverrides?: object) => {
     onRunQuery,
   };
 
-  Object.assign(props, propOverrides);
-
-  return shallow(<LokiExploreQueryEditor {...props} />);
+  Object.assign(props, { ...props, ...propOverrides });
+  return renderMethod(<LokiExploreQueryEditor {...props} />);
 };
 
 describe('LokiExploreQueryEditor', () => {
   it('should render component', () => {
-    const wrapper = setup();
+    const wrapper = setup(shallow);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render LokiQueryField with ExtraFieldElement when ExploreMode is set to Logs', () => {
-    const wrapper = setup();
-    const onMaxLinesChange = (e: React.SyntheticEvent<HTMLInputElement>) => jest.fn();
-    const onReturnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => jest.fn();
-
-    // workaround to "Received: serializes to the same string"
-    expect(JSON.stringify(wrapper.prop('ExtraFieldElement'))).toEqual(
-      JSON.stringify(
-        <LokiExploreExtraField
-          label={'Line limit'}
-          onChangeFunc={onMaxLinesChange}
-          onKeyDownFunc={onReturnKeyDown}
-          value={'0'}
-          type={'number'}
-          min={0}
-        />
-      )
-    );
+    const wrapper = setup(render);
+    expect(wrapper.find('.explore-input--ml')).toHaveLength(1);
   });
 
   it('should render LokiQueryField with no ExtraFieldElement when ExploreMode is not Logs', () => {
-    const wrapper = setup({
-      query: { expr: '', refId: 'A', maxLines: 0 },
-      data: {
-        state: LoadingState.NotStarted,
-        series: [],
-        request: {
-          requestId: '1',
-          dashboardId: 1,
-          interval: '1s',
-          panelId: 1,
-          range: {
-            from: dateTime('2020-01-01', 'YYYY-MM-DD'),
-            to: dateTime('2020-01-02', 'YYYY-MM-DD'),
-            raw: {
-              from: dateTime('2020-01-01', 'YYYY-MM-DD'),
-              to: dateTime('2020-01-02', 'YYYY-MM-DD'),
-            },
-          },
-          scopedVars: {},
-          targets: [],
-          timezone: 'GMT',
-          app: 'Grafana',
-          startTime: 0,
-        },
-        timeRange: {
-          from: dateTime('2020-01-01', 'YYYY-MM-DD'),
-          to: dateTime('2020-01-02', 'YYYY-MM-DD'),
-          raw: {
-            from: dateTime('2020-01-01', 'YYYY-MM-DD'),
-            to: dateTime('2020-01-02', 'YYYY-MM-DD'),
-          },
-        },
-      },
-      datasource: {} as LokiDatasource,
-      exploreMode: ExploreMode.Metrics,
-      history: [],
-      onChange: jest.fn(),
-      onRunQuery: jest.fn(),
-    });
-    expect(wrapper.prop('ExtraFieldElement')).toBe(null);
+    const wrapper = setup(render, { exploreMode: ExploreMode.Metrics });
+    expect(wrapper.find('.explore-input--ml')).toHaveLength(0);
   });
 });
