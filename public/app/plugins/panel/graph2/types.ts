a4d9c5e16c391e776f653a074c91bbe3b724fc4c
Ryan McKinley <ryantxu@gmail.com>
2020-01-30 00:15:48 -0800

    try uPlot

--- public/app/plugins/panel/graph2/types.ts
+++ public/app/plugins/panel/graph2/types.ts
@@ -14,7 +14,7 @@ export interface GraphOptions {
   showBars: boolean;
   showLines: boolean;
   showPoints: boolean;
-  useMicroPlot: boolean;
+  useMicroPlot?: boolean;
 }
 
 export interface Options {
