fda05f0d7ac4f55671353b85bc39d9d8ec3eca4c
Clarity-89 <homes89@ukr.net>
2020-02-05 18:11:17 +0200

    Grafana-UI: Add caret to the picker

--- packages/grafana-ui/src/components/TimePicker/TimeOfDayPicker.tsx
+++ packages/grafana-ui/src/components/TimePicker/TimeOfDayPicker.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import RcTimePicker from 'rc-time-picker';
+import { css } from 'emotion';
 import { dateTime, DateTime, dateTimeAsMoment } from '@grafana/data';
+import { ThemeContext } from '../../index';
 
 interface Props {
   onChange: (value: DateTime) => void;
@@ -20,6 +22,37 @@ export const TimeOfDayPicker: FC<Props> = ({ minuteStep = 1, showHour = true, on
         value={dateTimeAsMoment(value)}
         showHour={showHour}
         minuteStep={minuteStep}
+        inputIcon={<Caret />}
+      />
+    </div>
+  );
+};
+
+const Caret: FC = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <div
+      className={css`
+        position: absolute;
+        height: 100%;
+        right: 8px;
+        display: inline-block;
+        text-align: right;
+        z-index: 1071;
+      `}
+    >
+      <span
+        className={css`
+          border-color: ${theme.colors.white} transparent transparent;
+          border-style: solid;
+          border-width: 4px 4px 2.5px;
+          display: inline-block;
+          position: absolute;
+          top: 50%;
+          right: 2px;
+          margin-top: -2px;
+        `}
       />
     </div>
   );
