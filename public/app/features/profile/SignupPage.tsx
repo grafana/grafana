758e8f79b5950fff3ac1d33dd59f3a6c3e8a88af
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-02-05 14:27:10 +0100

    Make function component

--- public/app/features/profile/SignupPage.tsx
+++ public/app/features/profile/SignupPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import SignupForm from './SignupForm';
 import Page from 'app/core/components/Page/Page';
 
@@ -13,19 +13,17 @@ const navModel = {
     text: '',
   },
 };
-export class SignupPage extends React.PureComponent {
-  render() {
-    return (
-      <Page navModel={navModel}>
-        <Page.Contents>
-          <h3 className="p-b-1">You're almost there.</h3>
-          <div className="p-b-1">
-            We just need a couple of more bits of
-            <br /> information to finish creating your account.
-          </div>
-          <SignupForm />
-        </Page.Contents>
-      </Page>
-    );
-  }
-}
+export const SignupPage: FC = () => {
+  return (
+    <Page navModel={navModel}>
+      <Page.Contents>
+        <h3 className="p-b-1">You're almost there.</h3>
+        <div className="p-b-1">
+          We just need a couple of more bits of
+          <br /> information to finish creating your account.
+        </div>
+        <SignupForm />
+      </Page.Contents>
+    </Page>
+  );
+};
