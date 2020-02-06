aae8394b6e539f8a16ee22c0ec8b39d10f2e4436
Arve Knudsen <arve.knudsen@gmail.com>
2020-02-06 12:12:41 +0100

    Perform insertions in transactions
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/services/sqlstore/annotations_cleanup_test.go
+++ pkg/services/sqlstore/annotations_cleanup_test.go
@@ -10,61 +10,61 @@ import (
 	m "github.com/grafana/grafana/pkg/models"
 )
 
-func addTestAnnotation(dashboard *m.Dashboard, created int64) error {
-	return inTransaction(func(sess *DBSession) error {
-		created *= 1000
-		tags := m.ParseTagPairs([]string{"outage", "error", "type:outage", "server:server-1"})
+func addTestAnnotation(dashboard *m.Dashboard, created int64, sess *DBSession) error {
+	created *= 1000
+	tags := m.ParseTagPairs([]string{"outage", "error", "type:outage", "server:server-1"})
 
-		item := annotations.Item{
-			Tags:        m.JoinTagPairs(tags),
-			Text:        "hello",
-			Type:        "alert",
-			UserId:      1,
-			OrgId:       dashboard.OrgId,
-			DashboardId: dashboard.Id,
-			Created:     created,
-			Updated:     created,
-			Epoch:       created,
-			EpochEnd:    created,
-		}
-		if _, err := sess.Table("annotation").Insert(&item); err != nil {
+	item := annotations.Item{
+		Tags:        m.JoinTagPairs(tags),
+		Text:        "hello",
+		Type:        "alert",
+		UserId:      1,
+		OrgId:       dashboard.OrgId,
+		DashboardId: dashboard.Id,
+		Created:     created,
+		Updated:     created,
+		Epoch:       created,
+		EpochEnd:    created,
+	}
+	if _, err := sess.Table("annotation").Insert(&item); err != nil {
+		return err
+	}
+
+	// Get tag IDs
+	tags, err := EnsureTagsExist(sess, tags)
+	if err != nil {
+		return err
+	}
+	for _, tag := range tags {
+		if _, err := sess.Exec("INSERT INTO annotation_tag (annotation_id, tag_id) VALUES(?,?)", item.Id, tag.Id); err != nil {
 			return err
 		}
+	}
 
-		// Get tag IDs
-		tags, err := EnsureTagsExist(sess, tags)
-		if err != nil {
-			return err
-		}
-		for _, tag := range tags {
-			if _, err := sess.Exec("INSERT INTO annotation_tag (annotation_id, tag_id) VALUES(?,?)", item.Id, tag.Id); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
+	return nil
 }
 
 func TestDeleteExpiredAnnotations(t *testing.T) {
-	Convey("Testing annotations history clean up", t, func() {
+	Convey("Deletion of expired annotations", t, func() {
 		InitTestDB(t)
 		repo := SqlAnnotationRepo{}
 		daysToKeepAnnotations := 5
 		annotationsToWrite := 10
 
 		expiredCreated := (time.Now().Unix() - int64(daysToKeepAnnotations*2*86400))
-		// TODO: Insert all annotations in one transaction, for speed * 1000
 		// Add expired annotations
 		savedDash := insertTestDashboard("test dash 111", 1, 0, false, "this-is-fun")
-		for i := 0; i < annotationsToWrite-1; i++ {
-			err := addTestAnnotation(savedDash, expiredCreated)
-			So(err, ShouldBeNil)
-		}
+		err := inTransaction(func(sess *DBSession) error {
+			for i := 0; i < annotationsToWrite-1; i++ {
+				if err := addTestAnnotation(savedDash, expiredCreated, sess); err != nil {
+					return err
+				}
+			}
 
-		// add one recent
-		newCreated := (time.Now().Unix() - int64(2*86400))
-		err := addTestAnnotation(savedDash, newCreated)
+			// Add one recent
+			newCreated := (time.Now().Unix() - int64(2*86400))
+			return addTestAnnotation(savedDash, newCreated, sess)
+		})
 		So(err, ShouldBeNil)
 
 		Convey("Clean up expired annotations", func() {
@@ -98,19 +98,25 @@ func TestDeleteExpiredAnnotations(t *testing.T) {
 
 		Convey("Don't delete more than MAX_VERSIONS_TO_DELETE per iteration", func() {
 			annotationsToWriteBigNumber := MAX_HISTORY_ENTRIES_TO_DELETE + annotationsToWrite
-			for i := 0; i < annotationsToWriteBigNumber-annotationsToWrite; i++ {
+			err := inTransaction(func(sess *DBSession) error {
 				created := (time.Now().Unix() - int64(daysToKeepAnnotations*2*86400))
-				err := addTestAnnotation(savedDash, created)
-				So(err, ShouldBeNil)
-			}
-
-			err := deleteExpiredAnnotations(&m.DeleteExpiredAnnotationsCommand{})
+				for i := 0; i < annotationsToWriteBigNumber-annotationsToWrite; i++ {
+					if err := addTestAnnotation(savedDash, created, sess); err != nil {
+						return err
+					}
+				}
+				return nil
+			})
 			So(err, ShouldBeNil)
 
-			items, _ := repo.Find(&annotations.ItemQuery{
+			err = deleteExpiredAnnotations(&m.DeleteExpiredAnnotationsCommand{})
+			So(err, ShouldBeNil)
+
+			items, err := repo.Find(&annotations.ItemQuery{
 				OrgId:       1,
 				DashboardId: 1,
 			})
+			So(err, ShouldBeNil)
 
 			// Ensure we have at least daysToKeepAnnotations versions
 			So(len(items), ShouldBeGreaterThanOrEqualTo, daysToKeepAnnotations)
