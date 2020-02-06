85689b05798c14ac07d9d86d9bf2aa94cd537412
Clarity-89 <homes89@ukr.net>
2020-02-06 17:53:13 +0200

    Grafana-UI: Switch width to size for TimeOfDayPicker

--- packages/grafana-ui/src/components/TimePicker/TimeOfDayPicker.tsx
+++ packages/grafana-ui/src/components/TimePicker/TimeOfDayPicker.tsx
@@ -1,21 +1,22 @@
 import React, { FC } from 'react';
 import RcTimePicker from 'rc-time-picker';
-import classNames from 'classnames';
 import { dateTime, DateTime, dateTimeAsMoment } from '@grafana/data';
+import { inputSizes } from '../Forms/commonStyles';
+import { FormInputSize } from '../Forms/types';
 
 interface Props {
   onChange: (value: DateTime) => void;
   value: DateTime;
   showHour?: boolean;
   minuteStep?: number;
-  width?: number;
+  size?: FormInputSize;
 }
 
-export const TimeOfDayPicker: FC<Props> = ({ minuteStep = 1, showHour = true, onChange, value, width }) => {
+export const TimeOfDayPicker: FC<Props> = ({ minuteStep = 1, showHour = true, onChange, value, size = 'auto' }) => {
   return (
     <div>
       <RcTimePicker
-        className={classNames({ [`width-${width}`]: width })}
+        className={inputSizes()[size]}
         defaultValue={dateTimeAsMoment()}
         onChange={(value: any) => onChange(dateTime(value))}
         allowEmpty={false}
