ac7e190ae13007d117f1da5587eefad5eff1348b
Arve Knudsen <arve.knudsen@gmail.com>
2020-02-06 11:42:38 +0100

    Clean up tests
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/services/sqlstore/annotations_cleanup_test.go
+++ pkg/services/sqlstore/annotations_cleanup_test.go
@@ -10,40 +10,38 @@ import (
 	m "github.com/grafana/grafana/pkg/models"
 )
 
-func (r *SqlAnnotationRepo) addTestAnnotation(dashboard *m.Dashboard, repo SqlAnnotationRepo, created int64, item *annotations.Item) error {
-	created *= 1000
-
+func addTestAnnotation(dashboard *m.Dashboard, created int64) error {
 	return inTransaction(func(sess *DBSession) error {
+		created *= 1000
 		tags := m.ParseTagPairs([]string{"outage", "error", "type:outage", "server:server-1"})
-		item.Tags = m.JoinTagPairs(tags)
-		item.Text = "hello"
-		item.Type = "alert"
-		item.UserId = 1
-		item.OrgId = dashboard.OrgId
-		item.DashboardId = dashboard.Id
-		item.Created = created
-		item.Updated = created
-		if item.Epoch == 0 {
-			item.Epoch = created
-			item.EpochEnd = created
-		}
 
-		_, err := sess.Table("annotation").Insert(item)
-		if err != nil {
+		item := annotations.Item{
+			Tags:        m.JoinTagPairs(tags),
+			Text:        "hello",
+			Type:        "alert",
+			UserId:      1,
+			OrgId:       dashboard.OrgId,
+			DashboardId: dashboard.Id,
+			Created:     created,
+			Updated:     created,
+			Epoch:       created,
+			EpochEnd:    created,
+		}
+		if _, err := sess.Table("annotation").Insert(&item); err != nil {
 			return err
 		}
 
-		if len(item.Tags) > 0 {
-			tags, err := EnsureTagsExist(sess, tags)
-			if err != nil {
+		// Get tag IDs
+		tags, err := EnsureTagsExist(sess, tags)
+		if err != nil {
+			return err
+		}
+		for _, tag := range tags {
+			if _, err := sess.Exec("INSERT INTO annotation_tag (annotation_id, tag_id) VALUES(?,?)", item.Id, tag.Id); err != nil {
 				return err
 			}
-			for _, tag := range tags {
-				if _, err := sess.Exec("INSERT INTO annotation_tag (annotation_id, tag_id) VALUES(?,?)", item.Id, tag.Id); err != nil {
-					return err
-				}
-			}
 		}
+
 		return nil
 	})
 }
@@ -60,13 +58,13 @@ func TestDeleteExpiredAnnotations(t *testing.T) {
 		// Add expired annotations
 		savedDash := insertTestDashboard("test dash 111", 1, 0, false, "this-is-fun")
 		for i := 0; i < annotationsToWrite-1; i++ {
-			err := repo.addTestAnnotation(savedDash, repo, expiredCreated, &annotations.Item{OrgId: 1})
+			err := addTestAnnotation(savedDash, expiredCreated)
 			So(err, ShouldBeNil)
 		}
 
 		// add one recent
 		newCreated := (time.Now().Unix() - int64(2*86400))
-		err := repo.addTestAnnotation(savedDash, repo, newCreated, &annotations.Item{OrgId: 1})
+		err := addTestAnnotation(savedDash, newCreated)
 		So(err, ShouldBeNil)
 
 		Convey("Clean up expired annotations", func() {
@@ -102,7 +100,7 @@ func TestDeleteExpiredAnnotations(t *testing.T) {
 			annotationsToWriteBigNumber := MAX_HISTORY_ENTRIES_TO_DELETE + annotationsToWrite
 			for i := 0; i < annotationsToWriteBigNumber-annotationsToWrite; i++ {
 				created := (time.Now().Unix() - int64(daysToKeepAnnotations*2*86400))
-				err := repo.addTestAnnotation(savedDash, repo, created, &annotations.Item{OrgId: 1})
+				err := addTestAnnotation(savedDash, created)
 				So(err, ShouldBeNil)
 			}
 
