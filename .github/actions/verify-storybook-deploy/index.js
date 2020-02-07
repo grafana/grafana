932602db20250a8f4a99744f87633a3ed58c3f2b
Dominik Prokop <dominik.prokop@grafana.com>
2020-02-07 15:05:11 +0100

    Building storybook and moving to correct dir

--- .github/actions/verify-storybook-deploy/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-  core.setOutput
-  console.log(github.context.payload.pull_request);
-} catch (error) {
-  core.setFailed(error.message);
-}
