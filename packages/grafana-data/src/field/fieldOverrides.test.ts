44a9d9a621ea49c0248295d96debc0d19381347b
Ryan McKinley <ryantxu@gmail.com>
2020-02-09 17:12:24 +0100

    test apply

---
+++ packages/grafana-data/src/field/fieldOverrides.test.ts
@@ -0,0 +1,16 @@
+import { findNumericFieldMinMax } from './fieldOverrides';
+import { MutableDataFrame } from '../dataframe';
+
+describe('Global MinMax', () => {
+  it('find global min max', () => {
+    const f0 = new MutableDataFrame();
+    f0.add({ title: 'AAA', value: 100, value2: 1234 }, true);
+    f0.add({ title: 'BBB', value: -20 }, true);
+    f0.add({ title: 'CCC', value: 200, value2: 1000 }, true);
+    expect(f0.length).toEqual(3);
+
+    const minmax = findNumericFieldMinMax([f0]);
+    expect(minmax.min).toEqual(-20);
+    expect(minmax.max).toEqual(1234);
+  });
+});
