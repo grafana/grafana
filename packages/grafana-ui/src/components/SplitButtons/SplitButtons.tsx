716ddadb75f8a6195821c26d4be8957ef9ba0364
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-02-09 19:07:54 +0100

    Set border according to order

--- packages/grafana-ui/src/components/SplitButtons/SplitButtons.tsx
+++ packages/grafana-ui/src/components/SplitButtons/SplitButtons.tsx
@@ -1,12 +1,35 @@
 import React, { FC } from 'react';
-// import { Button, LinkButton } from '../Forms/Button';
-
+import { css } from 'emotion';
+import { stylesFactory } from '../../themes';
 export interface SplitButtonsProps {
   children: any;
 }
 
+const getStyles = stylesFactory((children: number, current: number) => {
+  //First
+  if (current === 0) {
+    return css`
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    `;
+  }
+  //Last
+  if (children - 1 === current) {
+    return css`
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    `;
+  }
+
+  return css`
+    border-radius: 0;
+  `;
+});
+
 export const SplitButtons: FC<SplitButtonsProps> = ({ children }) => {
-  const newChildren = React.cloneElement(children, {});
+  const newChildren = React.Children.map(children, (child, i) =>
+    React.cloneElement(child, { className: getStyles(children.length, i) })
+  );
 
   return <div>{newChildren}</div>;
 };
