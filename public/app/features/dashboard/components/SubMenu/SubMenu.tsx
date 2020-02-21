c0fb78acc32892ddf44ac37c5e3f8b343ed6a84a
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-02-21 14:48:07 +0100

    Refactor: Adds picker to SubMenu

--- public/app/features/dashboard/components/SubMenu/SubMenu.tsx
+++ public/app/features/dashboard/components/SubMenu/SubMenu.tsx
@@ -1,14 +1,27 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { StoreState } from '../../../../types';
+import { VariableState } from '../../../templating/state/types';
+import { VariablePicker } from '../../../templating/picker/VariablePicker';
 
-class UnConnectedSubMenu extends PureComponent {
+export interface Props {
+  variables: Record<string, VariableState>;
+}
+
+class UnConnectedSubMenu extends PureComponent<Props> {
   render() {
-    return <div>New SubMenu</div>;
+    const variables = Object.values(this.props.variables);
+    return (
+      <>
+        {variables.map(state => (
+          <VariablePicker picker={state.picker} editor={state.editor} variable={state.variable} />
+        ))}
+      </>
+    );
   }
 }
 
-const mapStateToProps = (state: StoreState) => state.templating.variables;
+const mapStateToProps = (state: StoreState) => ({ variables: state.templating.variables });
 
 export const SubMenu = connect(mapStateToProps)(UnConnectedSubMenu);
 SubMenu.displayName = 'SubMenu';
