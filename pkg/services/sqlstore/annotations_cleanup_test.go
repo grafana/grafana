e36786e4fad0afde87d37f40569e8b9b17c139bc
Sergey Rustamov <rustamov@gmail.com>
2020-02-21 10:32:59 -0500

    Using testify for testing

--- pkg/services/sqlstore/annotations_cleanup_test.go
+++ pkg/services/sqlstore/annotations_cleanup_test.go
@@ -1,21 +1,21 @@
 package sqlstore
 
 import (
+	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/services/annotations"
 	"testing"
 	"time"
 
-	. "github.com/smartystreets/goconvey/convey"
-
-	m "github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/models"
+	"github.com/stretchr/testify/assert"
 )
 
-func addTestAnnotation(dashboard *m.Dashboard, created int64, sess *DBSession) error {
+func addTestAnnotation(dashboard *models.Dashboard, created int64, sess *DBSession) error {
 	created *= 1000
-	tags := m.ParseTagPairs([]string{"outage", "error", "type:outage", "server:server-1"})
+	tags := models.ParseTagPairs([]string{"outage", "error", "type:outage", "server:server-1"})
 
 	item := annotations.Item{
-		Tags:        m.JoinTagPairs(tags),
+		Tags:        models.JoinTagPairs(tags),
 		Text:        "hello",
 		Type:        "alert",
 		UserId:      1,
@@ -48,79 +48,102 @@ func TestDeleteExpiredAnnotations(t *testing.T) {
 	daysToKeepAnnotations := 5
 	repo := SqlAnnotationRepo{}
 
-	Convey("Deletion of expired annotations", t, func() {
-		annotationsToWrite := 10
+	annotationsToWrite := 10
 
-		InitTestDB(t)
-		savedDash := insertTestDashboard("test dash 111", 1, 0, false, "this-is-fun")
+	InitTestDB(t)
+	savedDash := insertNewTestDashboard(t,"test dash 111", 1, 0, false, "this-is-fun")
 
-		expiredCreated := (time.Now().Unix() - int64(daysToKeepAnnotations*86400))
-		err := inTransaction(func(sess *DBSession) error {
-			for i := 0; i < annotationsToWrite-1; i++ {
-				if err := addTestAnnotation(savedDash, expiredCreated, sess); err != nil {
-					return err
-				}
+	expiredCreated := (time.Now().Unix() - int64(daysToKeepAnnotations*86400))
+	err := inTransaction(func(sess *DBSession) error {
+		for i := 0; i < annotationsToWrite-1; i++ {
+			if err := addTestAnnotation(savedDash, expiredCreated, sess); err != nil {
+				return err
 			}
+		}
 
-			// Add a non-expired one
-			newCreated := (time.Now().Unix() - int64(2*86400))
-			return addTestAnnotation(savedDash, newCreated, sess)
-		})
-		So(err, ShouldBeNil)
-
-		Convey("Expired annotations should be deleted", func() {
-			err := deleteExpiredAnnotations(&m.DeleteExpiredAnnotationsCommand{DaysToKeep: daysToKeepAnnotations})
-			So(err, ShouldBeNil)
-
-			items, err := repo.Find(&annotations.ItemQuery{
-				OrgId:       savedDash.OrgId,
-				DashboardId: savedDash.Id,
-			})
-			So(err, ShouldBeNil)
-
-			So(len(items), ShouldEqual, 1)
-			So(items[0].Tags, ShouldResemble, []string{"outage", "error", "type:outage", "server:server-1"})
-		})
-
-		Convey("Don't delete anything if there're no expired versions", func() {
-			err := deleteExpiredAnnotations(&m.DeleteExpiredAnnotationsCommand{DaysToKeep: daysToKeepAnnotations})
-			So(err, ShouldBeNil)
-
-			items, err := repo.Find(&annotations.ItemQuery{
-				OrgId:       savedDash.OrgId,
-				DashboardId: savedDash.Id,
-			})
-			So(err, ShouldBeNil)
-
-			So(len(items), ShouldEqual, 1)
-		})
+		// Add a non-expired one
+		newCreated := (time.Now().Unix() - int64(2*86400))
+		return addTestAnnotation(savedDash, newCreated, sess)
 	})
+	assert.Nil(t, err)
 
-	Convey("No more than MAX_VERSIONS_TO_DELETE annotations should be deleted per iteration", t, func() {
-		InitTestDB(t)
-		savedDash := insertTestDashboard("test dash 111", 1, 0, false, "this-is-fun")
 
-		numAnnotations := MAX_EXPIRED_ANNOTATIONS_TO_DELETE + 10
-		err := inTransaction(func(sess *DBSession) error {
-			created := (time.Now().Unix() - int64(daysToKeepAnnotations*86400))
-			for i := 0; i < numAnnotations; i++ {
-				if err := addTestAnnotation(savedDash, created, sess); err != nil {
-					return err
-				}
+
+	err = deleteExpiredAnnotations(&models.DeleteExpiredAnnotationsCommand{DaysToKeep: daysToKeepAnnotations})
+	assert.Nil(t, err)
+
+	items, err := repo.Find(&annotations.ItemQuery{
+		OrgId:       savedDash.OrgId,
+		DashboardId: savedDash.Id,
+	})
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(items), 1)
+	assert.ElementsMatch(t, items[0].Tags, []string{"outage", "error", "type:outage", "server:server-1"}, "Can read tags")
+
+
+
+	err = deleteExpiredAnnotations(&models.DeleteExpiredAnnotationsCommand{DaysToKeep: daysToKeepAnnotations})
+	assert.Nil(t, err)
+
+	items, err = repo.Find(&annotations.ItemQuery{
+		OrgId:       savedDash.OrgId,
+		DashboardId: savedDash.Id,
+	})
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(items), 1)
+
+}
+
+func TestDeleteExpiredAnnotationsMax(t *testing.T) {
+	daysToKeepAnnotations := 5
+	repo := SqlAnnotationRepo{}
+
+	InitTestDB(t)
+	savedDash := insertNewTestDashboard(t,"test dash 111", 1, 0, false, "this-is-fun")
+
+	numAnnotations := MAX_EXPIRED_ANNOTATIONS_TO_DELETE + 10
+	err := inTransaction(func(sess *DBSession) error {
+		created := (time.Now().Unix() - int64(daysToKeepAnnotations*86400))
+		for i := 0; i < numAnnotations; i++ {
+			if err := addTestAnnotation(savedDash, created, sess); err != nil {
+				return err
 			}
-			return nil
-		})
-		So(err, ShouldBeNil)
-
-		err = deleteExpiredAnnotations(&m.DeleteExpiredAnnotationsCommand{})
-		So(err, ShouldBeNil)
-
-		items, err := repo.Find(&annotations.ItemQuery{
-			OrgId:       savedDash.OrgId,
-			DashboardId: savedDash.Id,
-		})
-		So(err, ShouldBeNil)
-
-		So(len(items), ShouldEqual, numAnnotations-MAX_EXPIRED_ANNOTATIONS_TO_DELETE)
+		}
+		return nil
 	})
+	assert.Nil(t, err)
+
+	err = deleteExpiredAnnotations(&models.DeleteExpiredAnnotationsCommand{})
+	assert.Nil(t, err)
+
+	items, err := repo.Find(&annotations.ItemQuery{
+		OrgId:       savedDash.OrgId,
+		DashboardId: savedDash.Id,
+	})
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(items), numAnnotations-MAX_EXPIRED_ANNOTATIONS_TO_DELETE)
+}
+
+func insertNewTestDashboard(t *testing.T,title string, orgId int64, folderId int64, isFolder bool, tags ...interface{}) *models.Dashboard {
+	cmd := models.SaveDashboardCommand{
+		OrgId:    orgId,
+		FolderId: folderId,
+		IsFolder: isFolder,
+		Dashboard: simplejson.NewFromAny(map[string]interface{}{
+			"id":    nil,
+			"title": title,
+			"tags":  tags,
+		}),
+	}
+
+	err := SaveDashboard(&cmd)
+	assert.Nil(t, err)
+
+	cmd.Result.Data.Set("id", cmd.Result.Id)
+	cmd.Result.Data.Set("uid", cmd.Result.Uid)
+
+	return cmd.Result
 }
