f3e17d733196e04360e9869b2ce4687b042f1fe3
Ivana <ivana.huckova@gmail.com>
2020-02-24 09:53:40 +0100

    Testing Global component to inject global styles

--- packages/grafana-ui/src/components/Slider/Slider.tsx
+++ packages/grafana-ui/src/components/Slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { Range, createSliderWithTooltip } from 'rc-slider';
-import { css } from 'emotion';
+import { cx, css } from 'emotion';
+import { Global, css as cssCore } from '@emotion/core';
 import { stylesFactory } from '../../themes';
 import { GrafanaTheme } from '@grafana/data';
 import { useTheme } from '../../themes/ThemeContext';
@@ -18,6 +19,7 @@ export interface Props {
 }
 
 const getStyles = stylesFactory((theme: GrafanaTheme, isHorizontal: boolean) => {
+  const trackColor = theme.isLight ? theme.colors.gray5 : theme.colors.dark6;
   const container = isHorizontal
     ? css`
         width: 100%;
@@ -27,8 +29,65 @@ const getStyles = stylesFactory((theme: GrafanaTheme, isHorizontal: boolean) => 
         height: 100%;
         margin: ${theme.spacing.sm} ${theme.spacing.lg} ${theme.spacing.sm} ${theme.spacing.sm};
       `;
+
   return {
     container,
+    slider: css`
+      .rc-slider-vertical .rc-slider-handle {
+        margin-top: -10px;
+      }
+      .rc-slider-handle {
+        border: solid 2px ${theme.colors.blue77};
+        background-color: ${theme.colors.blue77};
+      }
+      .rc-slider-handle:hover {
+        border-color: ${theme.colors.blue77};
+      }
+      .rc-slider-handle:focus {
+        border-color: ${theme.colors.blue77};
+        box-shadow: none;
+      }
+      .rc-slider-handle:active {
+        border-color: ${theme.colors.blue77};
+        box-shadow: none;
+      }
+      .rc-slider-handle-click-focused:focus {
+        border-color: ${theme.colors.blue77};
+      }
+      .rc-slider-dot-active {
+        border-color: ${theme.colors.blue77};
+      }
+      .rc-slider-track {
+        background-color: ${theme.colors.blue77};
+      }
+      .rc-slider-rail {
+        background-color: ${trackColor};
+        border: 1px solid ${trackColor};
+      }
+    `,
+    tooltip: cssCore`
+    body {
+      .rc-slider-tooltip {
+        cursor: grab;
+        user-select: none;
+      }
+
+      .rc-slider-tooltip-inner {
+        color: ${theme.colors.text};
+        background-color: transparent !important;
+        border-radius: 0;
+        box-shadow: none;
+      }
+
+      .rc-slider-tooltip-placement-top .rc-slider-tooltip-arrow {
+        display: none;
+      }
+
+      .rc-slider-tooltip-placement-top {
+        padding: 0;
+      }
+    }
+  `,
   };
 });
 
@@ -46,7 +105,8 @@ export const Slider: FunctionComponent<Props> = ({
   const styles = getStyles(theme, isHorizontal);
   const RangeWithTooltip = createSliderWithTooltip(Range);
   return (
-    <div className={styles.container}>
+    <div className={cx(styles.container, styles.slider)}>
+      <Global styles={styles.tooltip} />
       <RangeWithTooltip
         tipProps={{ visible: true, placement: isHorizontal ? 'top' : 'right' }}
         min={min}
