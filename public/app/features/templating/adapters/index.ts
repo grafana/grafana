dc9f6c6c17f2d23abd76020f9f34cfcf5fccd207
Marcus Andersson <marcus.andersson@grafana.com>
2020-02-26 22:03:30 +0100

    wip: starting to add custom variable type.

--- public/app/features/templating/adapters/index.ts
+++ public/app/features/templating/adapters/index.ts
@@ -4,6 +4,7 @@ import { UrlQueryValue } from '@grafana/runtime';
 
 import { VariableModel, VariableOption, VariableType } from '../variable';
 import { createQueryVariableAdapter } from '../query/adapter';
+import { createCustomVariableAdapter } from '../custom/adapter';
 import { VariableEditorProps, VariablePickerProps, VariableState } from '../state/types';
 import { TemplatingState } from '../state';
 
@@ -23,11 +24,11 @@ export interface VariableAdapter<Model extends VariableModel> {
 }
 
 const allVariableAdapters: Record<VariableType, VariableAdapter<any> | null> = {
-  query: null,
+  query: createQueryVariableAdapter(),
   textbox: null,
   constant: null,
   datasource: null,
-  custom: null,
+  custom: createCustomVariableAdapter(),
   interval: null,
   adhoc: null,
 };
@@ -35,7 +36,6 @@ const allVariableAdapters: Record<VariableType, VariableAdapter<any> | null> = {
 export interface VariableAdapters {
   contains: (type: VariableType) => boolean;
   get: (type: VariableType) => VariableAdapter<any>;
-  set: (type: VariableType, adapter: VariableAdapter<any>) => void;
   registeredTypes: () => Array<{ type: VariableType; label: string }>;
 }
 
@@ -52,14 +52,9 @@ export const variableAdapters: VariableAdapters = {
 
     throw new Error(`There is no adapter for type:${type}`);
   },
-  set: (type: VariableType, adapter: VariableAdapter<any>): void => {
-    allVariableAdapters[type] = adapter;
-  },
   registeredTypes: (): Array<{ type: VariableType; label: string }> => {
     return Object.keys(allVariableAdapters)
       .filter((key: VariableType) => allVariableAdapters[key] !== null)
       .map((key: VariableType) => ({ type: key, label: allVariableAdapters[key].label }));
   },
 };
-
-variableAdapters.set('query', createQueryVariableAdapter());
