baa356e26d4f7cfe7025bc7d518184ce7ccf3cd8
Dominik Prokop <dominik.prokop@grafana.com>
2020-03-03 08:22:26 +0100

    Migration: Save dashboard modals (#22395)
    * Add mechanism for imperatively showing modals
    
    * Migration work in progress
    
    * Reorganise save modal components
    
    * use app events emmiter instead of root scope one
    
    * Add center alignment to layoout component
    
    * Make save buttons wotk
    
    * Prettier
    
    * Remove save dashboard logic  from dashboard srv
    
    * Remove unused code
    
    * Dont show error notifications
    
    * Save modal when dashboard is overwritten
    
    * For tweaks
    
    * Folder picker tweaks
    
    * Save dashboard tweaks
    
    * Copy provisioned dashboard to clipboard
    
    * Enable saving dashboard json to file
    
    * Use SaveDashboardAsButton
    
    * Review
    
    * Align buttons in dashboard settings
    
    * Migrate SaveDashboardAs tests
    
    * TS fixes
    
    * SaveDashboardForm tests migrated
    
    * Fixe some failing tests
    
    * Fix folder picker tests
    
    * Fix HistoryListCtrl tests
    
    * Remove old import
    
    * Enable fixed positioning for folder picker select menu
    
    * Modal: show react modals with appEvents
    
    * Open react modals using event
    
    * Move save dashboard modals to dashboard feature
    
    * Make e2e pass
    
    * Update public/app/features/dashboard/components/SaveDashboard/SaveDashboardButton.tsx
    
    * Hacking old vs new buttons to make all the things look like it's old good Grafana ;)
    
    Co-authored-by: Alexander Zobnin <alexanderzobnin@gmail.com>

---
+++ public/app/features/dashboard/components/SaveDashboard/forms/SaveDashboardForm.tsx
@@ -0,0 +1,67 @@
+import React, { useMemo } from 'react';
+import { Forms, Button, HorizontalGroup } from '@grafana/ui';
+import { e2e } from '@grafana/e2e';
+import { SaveDashboardFormProps } from '../types';
+
+interface SaveDashboardFormDTO {
+  message: string;
+  saveVariables: boolean;
+  saveTimerange: boolean;
+}
+
+export const SaveDashboardForm: React.FC<SaveDashboardFormProps> = ({ dashboard, onCancel, onSuccess, onSubmit }) => {
+  const hasTimeChanged = useMemo(() => dashboard.hasTimeChanged(), [dashboard]);
+  const hasVariableChanged = useMemo(() => dashboard.hasVariableValuesChanged(), [dashboard]);
+
+  return (
+    <Forms.Form
+      onSubmit={async (data: SaveDashboardFormDTO) => {
+        const result = await onSubmit(dashboard.getSaveModelClone(data), data, dashboard);
+        if (result.status === 'success') {
+          if (data.saveVariables) {
+            dashboard.resetOriginalVariables();
+          }
+          if (data.saveTimerange) {
+            dashboard.resetOriginalTime();
+          }
+          onSuccess();
+        }
+      }}
+    >
+      {({ register, errors }) => (
+        <>
+          <Forms.Field label="Changes description">
+            <Forms.TextArea name="message" ref={register} placeholder="Add a note to describe your changes..." />
+          </Forms.Field>
+          {hasTimeChanged && (
+            <Forms.Field label="Save current time range" description="Dashboard time range has changed">
+              <Forms.Switch
+                name="saveTimerange"
+                ref={register}
+                aria-label={e2e.pages.SaveDashboardModal.selectors.saveTimerange}
+              />
+            </Forms.Field>
+          )}
+          {hasVariableChanged && (
+            <Forms.Field label="Save current variables" description="Dashboard variables have changed">
+              <Forms.Switch
+                name="saveVariables"
+                ref={register}
+                aria-label={e2e.pages.SaveDashboardModal.selectors.saveVariables}
+              />
+            </Forms.Field>
+          )}
+
+          <HorizontalGroup>
+            <Button type="submit" aria-label={e2e.pages.SaveDashboardModal.selectors.save}>
+              Save
+            </Button>
+            <Forms.Button variant="secondary" onClick={onCancel}>
+              Cancel
+            </Forms.Button>
+          </HorizontalGroup>
+        </>
+      )}
+    </Forms.Form>
+  );
+};
