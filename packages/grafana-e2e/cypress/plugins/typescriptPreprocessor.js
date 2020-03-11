f26d14eef1c1c1fb2d10a9bad505e3b14bb9acf5
Marcus Andersson <marcus.andersson@grafana.com>
2020-03-11 11:26:52 +0100

    reverting the changes that failed the e2e tests. (#22714)

--- packages/grafana-e2e/cypress/plugins/typescriptPreprocessor.js
+++ packages/grafana-e2e/cypress/plugins/typescriptPreprocessor.js
@@ -1,23 +1,13 @@
-const { resolve } = require('path');
 const wp = require('@cypress/webpack-preprocessor');
-
-const packageRoot = resolve(`${__dirname}/../../`);
-const packageModules = `${packageRoot}/node_modules`;
 
 const webpackOptions = {
   module: {
     rules: [
       {
-        include: modulePath => {
-          return modulePath.startsWith(packageRoot) && !modulePath.startsWith(packageModules);
-        },
         test: /\.ts$/,
         use: [
           {
             loader: 'ts-loader',
-            options: {
-              configFile: `${__dirname}/../tsconfig.json`,
-            },
           },
         ],
       },
