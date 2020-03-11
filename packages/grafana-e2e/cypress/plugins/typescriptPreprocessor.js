ca73566c4a5d1fef438622bb6f352593bc838b33
Steven Vachon <contact@svachon.com>
2020-03-11 13:20:48 -0400

    @grafana/e2e: correctly exclude dependencies from TypeScript builds
    * maintains support for Grafana core e2e tests
    * avoids producing declaration files in plugin source directories

--- packages/grafana-e2e/cypress/plugins/typescriptPreprocessor.js
+++ packages/grafana-e2e/cypress/plugins/typescriptPreprocessor.js
@@ -1,9 +1,23 @@
+const { resolve } = require('path');
 const wp = require('@cypress/webpack-preprocessor');
+
+const anyNodeModules = /node_modules/;
+const packageRoot = resolve(`${__dirname}/../../`);
+const packageModules = `${packageRoot}/node_modules`;
 
 const webpackOptions = {
   module: {
     rules: [
       {
+        include: modulePath => {
+          if (!anyNodeModules.test(modulePath)) {
+            // Is a file within the project
+            return true;
+          } else {
+            // Is a file within this package
+            return modulePath.startsWith(packageRoot) && !modulePath.startsWith(packageModules);
+          }
+        },
         test: /\.ts$/,
         use: [
           {
