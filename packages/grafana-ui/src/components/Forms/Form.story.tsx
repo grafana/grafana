7108dc0005de7356e9096fc087c539b699428ec4
Clarity-89 <homes89@ukr.net>
2020-03-13 12:03:55 +0200

    Grafana-UI: Add invalid state to Forms.Textarea

--- packages/grafana-ui/src/components/Forms/Form.story.tsx
+++ packages/grafana-ui/src/components/Forms/Form.story.tsx
@@ -14,6 +14,7 @@ import { RadioButtonGroup } from './RadioButtonGroup/RadioButtonGroup';
 import { Select } from './Select/Select';
 import Forms from './index';
 import mdx from './Form.mdx';
+import { TextArea } from './TextArea/TextArea';
 
 export default {
   title: 'Forms/Test forms',
@@ -48,6 +49,7 @@ interface FormDTO {
   switch: boolean;
   radio: string;
   select: string;
+  text: string;
   nested: {
     path: string;
   };
@@ -84,6 +86,10 @@ const renderForm = (defaultValues?: Partial<FormDTO>) => (
           </Field>
           <Field label="Nested object">
             <Input name="nested.path" placeholder="Nested path" size="md" ref={register} />
+          </Field>
+
+          <Field label="Textarea" invalid={!!errors.text} error="Text is required">
+            <TextArea name="text" placeholder="Long text" size="md" ref={register({ required: true })} />
           </Field>
 
           <Field label="Checkbox" invalid={!!errors.checkbox} error="We need your consent">
