7223beb93a448b4d33e69a0512b1c636f5028dc5
Torkel Ödegaard <torkel@grafana.com>
2020-03-15 16:17:08 +0100

    Updated

--- e2e/suite1/specs/bar-gauge.spec.ts
+++ e2e/suite1/specs/bar-gauge.spec.ts
@@ -14,18 +14,8 @@ e2e.scenario({
       .get('#panel-6')
       .screenshot('bar-gauge-gradient');
 
-    // const theOutputImage = `${e2e.config().screenshotsFolder}/received/smoke-test-scenario.png`;
     // const theTruthImage = `${e2e.config().screenshotsFolder}/expected/smoke-test-scenario.png`;
-    //
-    //   e2e().wrap(
-    //     e2e.imgSrcToBlob(url).then((blob: any) => {
-    //       e2e.blobToBase64String(blob).then((base64String: string) => {
-    //         const data = base64String.replace(/^data:image\/\w+;base64,/, '');
-    //         e2e().writeFile(theOutputImage, data, 'base64');
-    //       });
-    //     })
-    //   );
-    //   e2e().wait(1000); // give the io a chance to flush image to disk
-    // e2e().compareSnapshot({ pathToFileA: theOutputImage, pathToFileB: theTruthImage });
+    // const theOutputImage = `${e2e.config().screenshotsFolde/smoke-test-scenario.png`;
+    e2e().compareSnapshot({ pathToFileA: theOutputImage, pathToFileB: theTruthImage });
   },
 });
