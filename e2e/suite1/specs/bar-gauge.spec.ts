8d422c19ebf38a7610930876ece1ca410257b88f
Torkel Ödegaard <torkel@grafana.com>
2020-03-16 10:48:55 +0100

    Updated test

--- e2e/suite1/specs/bar-gauge.spec.ts
+++ e2e/suite1/specs/bar-gauge.spec.ts
@@ -11,7 +11,8 @@ e2e.scenario({
     e2e.flows.openDashboard('O6f11TZWk');
 
     e2e()
-      .get('#panel-6')
-      .screenshot('bar-gauge-gradient');
+      .get('#panel-6 .bar-gauge__value')
+      .should('have.css', 'color', 'rgb(242, 73, 92)')
+      .contains('100');
   },
 });
