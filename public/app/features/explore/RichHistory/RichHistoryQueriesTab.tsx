c1cc9098541a6f21d489534d5267bb4446279b5f
Ivana <ivana.huckova@gmail.com>
2020-03-17 16:55:34 +0100

    Revert "Explore: Add test coverage for Rich history (#22722)"
    This reverts commit 40a8aea3137a9dd5c19adfa53a12e5b0d388af9b.

--- public/app/features/explore/RichHistory/RichHistoryQueriesTab.tsx
+++ public/app/features/explore/RichHistory/RichHistoryQueriesTab.tsx
@@ -23,7 +23,7 @@ import RichHistoryCard from './RichHistoryCard';
 import { sortOrderOptions } from './RichHistory';
 import { Select, Slider } from '@grafana/ui';
 
-export interface Props {
+interface Props {
   queries: RichHistoryQuery[];
   sortOrder: SortOrder;
   activeDatasourceOnly: boolean;
@@ -190,7 +190,7 @@ export function RichHistoryQueriesTab(props: Props) {
       <div className={styles.containerContent}>
         <div className={styles.selectors}>
           {!activeDatasourceOnly && (
-            <div className={styles.multiselect} aria-label="Filter datasources">
+            <div className={styles.multiselect}>
               <Select
                 isMulti={true}
                 options={datasources}
@@ -200,7 +200,7 @@ export function RichHistoryQueriesTab(props: Props) {
               />
             </div>
           )}
-          <div className={styles.sort} aria-label="Sort queries">
+          <div className={styles.sort}>
             <Select
               value={sortOrderOptions.filter(order => order.value === sortOrder)}
               options={sortOrderOptions}
