5ad0ab299227b9bd2af5254fc0f795b161d54425
Torkel Ödegaard <torkel@grafana.com>
2020-03-18 08:46:49 +0100

    Fixed unit test

--- public/app/plugins/datasource/graphite/specs/query_ctrl.test.ts
+++ public/app/plugins/datasource/graphite/specs/query_ctrl.test.ts
@@ -3,6 +3,12 @@ import gfunc from '../gfunc';
 import { GraphiteQueryCtrl } from '../query_ctrl';
 import { TemplateSrvStub } from 'test/specs/helpers';
 
+jest.mock('app/core/utils/promiseToDigest', () => ({
+  promiseToDigest: (scope: any) => {
+    return p => p;
+  },
+}));
+
 describe('GraphiteQueryCtrl', () => {
   const ctx = {
     datasource: {
