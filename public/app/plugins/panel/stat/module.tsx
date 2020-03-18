823ac53b4bfe8f7464b6baa159261ba15a9fc348
Dominik Prokop <dominik.prokop@grafana.com>
2020-03-18 12:42:48 +0100

    Use standard field config for gauge, bar gauge and stat panels

--- public/app/plugins/panel/stat/module.tsx
+++ public/app/plugins/panel/stat/module.tsx
@@ -1,24 +1,12 @@
 import { sharedSingleStatMigrationHandler, sharedSingleStatPanelChangedHandler } from '@grafana/ui';
-import { PanelPlugin, ThresholdsMode } from '@grafana/data';
-import { StatPanelOptions, defaults } from './types';
+import { PanelPlugin } from '@grafana/data';
+import { StatPanelOptions, defaults, standardFieldConfig } from './types';
 import { StatPanel } from './StatPanel';
 import { StatPanelEditor } from './StatPanelEditor';
 
 export const plugin = new PanelPlugin<StatPanelOptions>(StatPanel)
   .setDefaults(defaults)
-  .setFieldConfigDefaults({
-    defaults: {
-      thresholds: {
-        mode: ThresholdsMode.Absolute,
-        steps: [
-          { value: -Infinity, color: 'green' },
-          { value: 80, color: 'red' }, // 80%
-        ],
-      },
-      mappings: [],
-      unit: 'mpg',
-    },
-  })
+  .setFieldConfigDefaults(standardFieldConfig)
   .setEditor(StatPanelEditor)
   .setNoPadding()
   .setPanelChangeHandler(sharedSingleStatPanelChangedHandler)
