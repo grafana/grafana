3bd8a1849dc097772aafa519914083f0e55c0a46
Marcus Andersson <marcus.andersson@grafana.com>
2020-03-19 16:47:40 +0100

    added more tests.

--- public/app/features/variables/adhoc/actions.test.ts
+++ public/app/features/variables/adhoc/actions.test.ts
@@ -5,9 +5,16 @@ import { TemplatingState } from 'app/features/variables/state/reducers';
 import { getRootReducer } from '../state/helpers';
 import { toVariablePayload, toVariableIdentifier } from '../state/types';
 import * as variableBuilder from '../testing/variableBuilder';
-import { applyFilterFromTable, AdHocTableOptions } from './actions';
+import {
+  applyFilterFromTable,
+  AdHocTableOptions,
+  changeFilter,
+  addFilter,
+  removeFilter,
+  setFiltersFromUrl,
+} from './actions';
 import { omit } from 'lodash';
-import { filterAdded } from './reducer';
+import { filterAdded, filterUpdated, filterRemoved, filtersRestored } from './reducer';
 import { addVariable } from '../state/sharedReducer';
 import { updateLocation } from 'app/core/actions';
 import { DashboardState } from 'app/types';
@@ -190,6 +197,232 @@ describe('adhoc actions', () => {
 
         expect(addVariableAction).toEqual(createAddVariableAction(variable, 1));
         expect(addFilterAction).toEqual(filterAdded(toVariablePayload(variable, filter)));
+        expect(updateLocationAction).toEqual(updateLocation({ query }));
+
+        return actions.length === expectedNumberOfActions;
+      });
+    });
+  });
+
+  describe('when changeFilter is dispatched', () => {
+    it('then correct actions are dispatched', async () => {
+      const existing = {
+        key: 'key',
+        value: 'value',
+        operator: '=',
+        condition: '',
+      };
+
+      const updated = {
+        ...existing,
+        operator: '!=',
+      };
+
+      const variable = variableBuilder
+        .adHocVariable()
+        .withUUID(uuid)
+        .withFilters([existing])
+        .withName('elastic-filter')
+        .withDatasource('elasticsearch')
+        .build();
+
+      const update = { index: 0, filter: updated };
+
+      const tester = await reduxTester<{ templating: TemplatingState; dashboard: DashboardState }>()
+        .givenRootReducer(getRootReducer())
+        .whenActionIsDispatched(createAddVariableAction(variable))
+        .whenAsyncActionIsDispatched(changeFilter(uuid, update), true);
+
+      tester.thenDispatchedActionsPredicateShouldEqual(actions => {
+        const [filterUpdatedAction, updateLocationAction] = actions;
+        const expectedNumberOfActions = 2;
+
+        const query = { [`var-${variable.name}`]: toUrl([updated]) };
+
+        expect(filterUpdatedAction).toEqual(filterUpdated(toVariablePayload(variable, update)));
+        expect(updateLocationAction).toEqual(updateLocation({ query }));
+
+        return actions.length === expectedNumberOfActions;
+      });
+    });
+  });
+
+  describe('when addFilter is dispatched on variable with existing filter', () => {
+    it('then correct actions are dispatched', async () => {
+      const existing = {
+        key: 'key',
+        value: 'value',
+        operator: '=',
+        condition: '',
+      };
+
+      const adding = {
+        ...existing,
+        operator: '!=',
+      };
+
+      const variable = variableBuilder
+        .adHocVariable()
+        .withUUID(uuid)
+        .withFilters([existing])
+        .withName('elastic-filter')
+        .withDatasource('elasticsearch')
+        .build();
+
+      const tester = await reduxTester<{ templating: TemplatingState; dashboard: DashboardState }>()
+        .givenRootReducer(getRootReducer())
+        .whenActionIsDispatched(createAddVariableAction(variable))
+        .whenAsyncActionIsDispatched(addFilter(uuid, adding), true);
+
+      tester.thenDispatchedActionsPredicateShouldEqual(actions => {
+        const [filterAddAction, updateLocationAction] = actions;
+        const expectedNumberOfActions = 2;
+
+        const query = { [`var-${variable.name}`]: toUrl([existing, adding]) };
+
+        expect(filterAddAction).toEqual(filterAdded(toVariablePayload(variable, adding)));
+        expect(updateLocationAction).toEqual(updateLocation({ query }));
+
+        return actions.length === expectedNumberOfActions;
+      });
+    });
+  });
+
+  describe('when addFilter is dispatched on variable with no existing filter', () => {
+    it('then correct actions are dispatched', async () => {
+      const adding = {
+        key: 'key',
+        value: 'value',
+        operator: '=',
+        condition: '',
+      };
+
+      const variable = variableBuilder
+        .adHocVariable()
+        .withUUID(uuid)
+        .withFilters([])
+        .withName('elastic-filter')
+        .withDatasource('elasticsearch')
+        .build();
+
+      const tester = await reduxTester<{ templating: TemplatingState; dashboard: DashboardState }>()
+        .givenRootReducer(getRootReducer())
+        .whenActionIsDispatched(createAddVariableAction(variable))
+        .whenAsyncActionIsDispatched(addFilter(uuid, adding), true);
+
+      tester.thenDispatchedActionsPredicateShouldEqual(actions => {
+        const [filterAddAction, updateLocationAction] = actions;
+        const expectedNumberOfActions = 2;
+
+        const query = { [`var-${variable.name}`]: toUrl([adding]) };
+
+        expect(filterAddAction).toEqual(filterAdded(toVariablePayload(variable, adding)));
+        expect(updateLocationAction).toEqual(updateLocation({ query }));
+
+        return actions.length === expectedNumberOfActions;
+      });
+    });
+  });
+
+  describe('when removeFilter is dispatched on variable with no existing filter', () => {
+    it('then correct actions are dispatched', async () => {
+      const variable = variableBuilder
+        .adHocVariable()
+        .withUUID(uuid)
+        .withFilters([])
+        .withName('elastic-filter')
+        .withDatasource('elasticsearch')
+        .build();
+
+      const tester = await reduxTester<{ templating: TemplatingState; dashboard: DashboardState }>()
+        .givenRootReducer(getRootReducer())
+        .whenActionIsDispatched(createAddVariableAction(variable))
+        .whenAsyncActionIsDispatched(removeFilter(uuid, 0), true);
+
+      tester.thenDispatchedActionsPredicateShouldEqual(actions => {
+        const [filterRemoveAction, updateLocationAction] = actions;
+        const expectedNumberOfActions = 2;
+
+        const query = { [`var-${variable.name}`]: toUrl([]) };
+
+        expect(filterRemoveAction).toEqual(filterRemoved(toVariablePayload(variable, 0)));
+        expect(updateLocationAction).toEqual(updateLocation({ query }));
+
+        return actions.length === expectedNumberOfActions;
+      });
+    });
+  });
+
+  describe('when removeFilter is dispatched on variable with existing filter', () => {
+    it('then correct actions are dispatched', async () => {
+      const filter = {
+        key: 'key',
+        value: 'value',
+        operator: '=',
+        condition: '',
+      };
+
+      const variable = variableBuilder
+        .adHocVariable()
+        .withUUID(uuid)
+        .withFilters([filter])
+        .withName('elastic-filter')
+        .withDatasource('elasticsearch')
+        .build();
+
+      const tester = await reduxTester<{ templating: TemplatingState; dashboard: DashboardState }>()
+        .givenRootReducer(getRootReducer())
+        .whenActionIsDispatched(createAddVariableAction(variable))
+        .whenAsyncActionIsDispatched(removeFilter(uuid, 0), true);
+
+      tester.thenDispatchedActionsPredicateShouldEqual(actions => {
+        const [filterRemoveAction, updateLocationAction] = actions;
+        const expectedNumberOfActions = 2;
+
+        const query = { [`var-${variable.name}`]: toUrl([]) };
+
+        expect(filterRemoveAction).toEqual(filterRemoved(toVariablePayload(variable, 0)));
+        expect(updateLocationAction).toEqual(updateLocation({ query }));
+
+        return actions.length === expectedNumberOfActions;
+      });
+    });
+  });
+
+  describe('when setFiltersFromUrl is dispatched', () => {
+    it('then correct actions are dispatched', async () => {
+      const existing = {
+        key: 'key',
+        value: 'value',
+        operator: '=',
+        condition: '',
+      };
+
+      const variable = variableBuilder
+        .adHocVariable()
+        .withUUID(uuid)
+        .withFilters([existing])
+        .withName('elastic-filter')
+        .withDatasource('elasticsearch')
+        .build();
+
+      const fromUrl = [
+        { ...existing, condition: '>' },
+        { ...existing, name: 'value-2' },
+      ];
+
+      const tester = await reduxTester<{ templating: TemplatingState; dashboard: DashboardState }>()
+        .givenRootReducer(getRootReducer())
+        .whenActionIsDispatched(createAddVariableAction(variable))
+        .whenAsyncActionIsDispatched(setFiltersFromUrl(uuid, fromUrl), true);
+
+      tester.thenDispatchedActionsPredicateShouldEqual(actions => {
+        const [filterRestoredAction, updateLocationAction] = actions;
+        const expectedNumberOfActions = 2;
+
+        const query = { [`var-${variable.name}`]: toUrl(fromUrl) };
+
+        expect(filterRestoredAction).toEqual(filtersRestored(toVariablePayload(variable, fromUrl)));
         expect(updateLocationAction).toEqual(updateLocation({ query }));
 
         return actions.length === expectedNumberOfActions;
