fcadf46e2f05a2b5820d51db96ba8861f43a724d
Clarity-89 <homes89@ukr.net>
2020-03-20 14:12:00 +0200

    Search: Style ResultItem

--- public/app/features/search/components/SearchResults.tsx
+++ public/app/features/search/components/SearchResults.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
 import { css, cx } from 'emotion';
 import { e2e } from '@grafana/e2e';
-import { Forms, Icon } from '@grafana/ui';
+import { Forms, Icon, selectThemeVariant as stv, useTheme } from '@grafana/ui';
+import { GrafanaTheme } from '@grafana/data';
 import { IconType } from '@grafana/ui/src/components/Icon/types';
 import appEvents from 'app/core/app_events';
 import { CoreEvents } from 'app/types';
@@ -96,7 +97,18 @@ interface CheckboxProps {
 const DashboardCheckbox: FC<CheckboxProps> = ({ checked, onClick, editable = false }) => {
   return (
     editable && (
-      <div onClick={onClick} className="center-vh">
+      <div
+        onClick={onClick}
+        className={cx(
+          'center-vh',
+          css`
+            height: 19px;
+            & > label {
+              height: 100%;
+            }
+          `
+        )}
+      >
         <Forms.Checkbox value={checked} />
       </div>
     )
@@ -110,7 +122,66 @@ interface ResultsItemProps {
   onTagSelected: any;
 }
 
+const getResultsItemStyles = (theme: GrafanaTheme, selected: boolean) => ({
+  wrapper: cx(
+    css`
+      display: flex;
+      align-items: center;
+      margin: ${theme.spacing.xxs};
+      padding: 0 ${theme.spacing.sm};
+      background: ${stv(
+        {
+          light: theme.colors.white,
+          dark: theme.colors.dark4,
+        },
+        theme.type
+      )};
+      box-shadow: -1px -1px 0 0 hsla(0, 0%, 100%, 0.1),
+        1px 1px 0 0 ${stv({ light: 'rgba(0, 0, 0, 0.1)', dark: 'rgba(0, 0, 0, 0.3)' }, theme.type)};
+      color: ${theme.colors.text};
+      border-radius: ${theme.border.radius.md};
+      min-height: 37px;
+      &:hover,
+      &.selected {
+        background: ${stv(
+          { dark: `linear-gradient(135deg, ${theme.colors.dark9}, ${theme.colors.dark6})`, light: theme.colors.gray6 },
+          theme.type
+        )};
+      }
+    `,
+    'pointer',
+    { selected }
+  ),
+  title: css`
+    color: ${selected ? theme.colors.textStrong : theme.colors.text};
+  `,
+  body: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1 1 auto;
+    overflow: hidden;
+    padding: 0 10px;
+  `,
+  folderTitle: css`
+    color: ${theme.colors.textWeak};
+    font-size: ${theme.typography.size.xs};
+    line-height: ${theme.typography.lineHeight.xs};
+    position: relative;
+    top: -1px;
+  `,
+  icon: css`
+    font-size: ${theme.typography.size.lg};
+    width: auto;
+    height: auto;
+    padding: 1px 2px 0 10px;
+  `,
+});
+
 const ResultsItem: FC<ResultsItemProps> = ({ item, editable, onToggleSelection, onTagSelected }) => {
+  const theme = useTheme();
+  const styles = getResultsItemStyles(theme, item.checked);
+
   const onItemClick = (item: DashboardSectionItem) => {
     //Check if one string can be found in the other
     if (window.location.pathname.includes(item.url) || item.url.includes(window.location.pathname)) {
@@ -123,22 +194,16 @@ const ResultsItem: FC<ResultsItemProps> = ({ item, editable, onToggleSelection, 
   };
 
   return (
-    <div
-      aria-label={selectors.dashboards(item.title)}
-      className={cx('search-item search-item--indent pointer', { selected: item.checked })}
-      onClick={() => navigate(item)}
-    >
+    <div aria-label={selectors.dashboards(item.title)} className={styles.wrapper} onClick={() => navigate(item)}>
       <DashboardCheckbox
         editable={editable}
         checked={item.checked}
         onClick={(e: MouseEvent) => onToggleSelection(item, e)}
       />
-      <span className="search-item__icon">
-        <Icon name="th-large" />
-      </span>
-      <span className="search-item__body" onClick={() => onItemClick(item)}>
-        <div className="search-item__body-title">{item.title}</div>
-        <span className="search-item__body-folder-title">{item.folderTitle}</span>
+      <Icon name="th-large" className={styles.icon} />
+      <span className={styles.body} onClick={() => onItemClick(item)}>
+        <div className={styles.title}>{item.title}</div>
+        <span className={styles.folderTitle}>{item.folderTitle}</span>
       </span>
       <span className="search-item__tags">
         {item.tags.map(tag => (
