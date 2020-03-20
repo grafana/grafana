de331b9f41d9086ab26c8327bd2301280cbfb780
flopp999 <21694965+flopp999@users.noreply.github.com>
2020-03-20 16:25:58 +0100

    Text change
    made it more understandable that this is a search field

--- public/app/core/components/search/SearchField.tsx
+++ public/app/core/components/search/SearchField.tsx
@@ -79,7 +79,7 @@ export const SearchField: React.FunctionComponent<SearchFieldProps> = ({ query, 
 
         <input
           type="text"
-          placeholder="Find dashboards by name"
+          placeholder="Search here - Find dashboards by name"
           value={query.query}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
             onChange(event.currentTarget.value);
