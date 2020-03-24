5e230f4be50d3a317ebdde5bea1bdae947681a0a
Torkel Ödegaard <torkel@grafana.com>
2020-03-24 11:19:32 +0100

    Added all repeat options

--- public/app/features/dashboard/components/PanelEditor/GeneralPanelOptions.tsx
+++ public/app/features/dashboard/components/PanelEditor/GeneralPanelOptions.tsx
@@ -13,6 +13,12 @@ export const GeneralPanelOptions: FC<{
   const linkVariablesSuggestions = useMemo(() => getPanelLinksVariableSuggestions(), []);
 
   const variableOptions = getVariableOptions();
+  const directionOptions = [
+    { label: 'Horizontal', value: 'h' },
+    { label: 'Vertical', value: 'v' },
+  ];
+
+  const maxPerRowOptions = [2, 3, 4, 6, 8, 12].map(value => ({ label: value.toString(), value }));
 
   return (
     <div>
@@ -20,13 +26,13 @@ export const GeneralPanelOptions: FC<{
         <Forms.Field label="Panel title">
           <Forms.Input defaultValue={panel.title} onBlur={e => onPanelConfigChange('title', e.currentTarget.value)} />
         </Forms.Field>
-        <Forms.Field label="Description" description="Panel description supports markdown and links">
+        <Forms.Field label="Description" description="Panel description supports markdown and links.">
           <Forms.TextArea
             defaultValue={panel.description}
             onBlur={e => onPanelConfigChange('description', e.currentTarget.value)}
           />
         </Forms.Field>
-        <Forms.Field label="Transparent" description="Display panel without background">
+        <Forms.Field label="Transparent" description="Display panel without background.">
           <Forms.Switch
             value={panel.transparent}
             onChange={e => onPanelConfigChange('transparent', e.currentTarget.checked)}
@@ -42,13 +48,37 @@ export const GeneralPanelOptions: FC<{
         />
       </OptionsGroup>
       <OptionsGroup title="Panel repeats">
-        <Forms.Field label="Repeat by variable" description="Repeat this panel for each value in the selected variable">
+        <Forms.Field
+          label="Repeat by variable"
+          description="Repeat this panel for each value in the selected variable.
+          This is not visible while in edit mode. You need to go back to dashboard and then update the variable or
+          reload the dashboard."
+        >
           <Forms.Select
             value={panel.repeat}
             onChange={value => onPanelConfigChange('repeat', value.value)}
             options={variableOptions}
           />
         </Forms.Field>
+        {panel.repeat && (
+          <Forms.Field label="Repeat direction">
+            <Forms.RadioButtonGroup
+              options={directionOptions}
+              value={panel.repeatDirection || 'h'}
+              onChange={value => onPanelConfigChange('repeatDirection', value)}
+            />
+          </Forms.Field>
+        )}
+
+        {panel.repeat && panel.repeatDirection === 'h' && (
+          <Forms.Field label="Max per row">
+            <Forms.Select
+              options={maxPerRowOptions}
+              value={panel.maxPerRow}
+              onChange={value => onPanelConfigChange('maxPerRow', value.value)}
+            />
+          </Forms.Field>
+        )}
       </OptionsGroup>
     </div>
   );
@@ -65,6 +95,11 @@ function getVariableOptions(): Array<SelectableValue<string>> {
       value: null,
     });
   }
+
+  options.unshift({
+    label: 'Disable repeating',
+    value: null,
+  });
 
   return options;
 }
