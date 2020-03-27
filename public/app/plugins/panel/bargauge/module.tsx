cfcb84a23a61d30b3efc2381dd4041ce9dc9586d
Torkel Ödegaard <torkel@grafana.com>
2020-03-27 10:03:22 +0100

    progress

--- public/app/plugins/panel/bargauge/module.tsx
+++ public/app/plugins/panel/bargauge/module.tsx
@@ -3,12 +3,16 @@ import { PanelPlugin } from '@grafana/data';
 import { BarGaugePanel } from './BarGaugePanel';
 import { BarGaugeOptions, defaults } from './types';
 import { standardFieldConfig } from '../stat/types';
+import { BarGaugePanelEditor } from './BarGaugePanelEditor';
 import { barGaugePanelMigrationHandler } from './BarGaugeMigrations';
 
 export const plugin = new PanelPlugin<BarGaugeOptions>(BarGaugePanel)
   .setDefaults(defaults)
+  .setEditor(BarGaugePanelEditor)
   .setFieldConfigDefaults(standardFieldConfig)
   .setPanelOptions(builder => {
+    /* addStandardSingleValueOptions(builder); */
+
     builder.addRadio({
       id: 'orientation',
       name: 'Orientation',
