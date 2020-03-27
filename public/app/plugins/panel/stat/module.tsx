a30b0217de17c51b4837c8679d55daf91974f54b
Torkel Ödegaard <torkel@grafana.com>
2020-03-27 21:45:39 +0100

    Updated

--- public/app/plugins/panel/stat/module.tsx
+++ public/app/plugins/panel/stat/module.tsx
@@ -11,18 +11,52 @@ export const plugin = new PanelPlugin<StatPanelOptions>(StatPanel)
   .setPanelOptions(builder => {
     addStandardSingleValueOptions(builder);
 
-    builder.addRadio({
-      id: 'orientation',
-      name: 'Orientation',
-      description: 'Stacking direction for multiple bars',
-      settings: {
-        options: [
-          { value: 'auto', label: 'Auto' },
-          { value: 'horizontal', label: 'Horizontal' },
-          { value: 'vertical', label: 'Vertical' },
-        ],
-      },
-    });
+    builder
+      .addRadio({
+        id: 'orientation',
+        name: 'Orientation',
+        description: 'Stacking direction for multiple bars',
+        settings: {
+          options: [
+            { value: 'auto', label: 'Auto' },
+            { value: 'horizontal', label: 'Horizontal' },
+            { value: 'vertical', label: 'Vertical' },
+          ],
+        },
+      })
+      .addRadio({
+        id: 'colorMode',
+        name: 'Color mode',
+        description: 'Color either the value or the background',
+        settings: {
+          options: [
+            { value: 'value', label: 'Value' },
+            { value: 'background', label: 'Background' },
+          ],
+        },
+      })
+      .addRadio({
+        id: 'graphMode',
+        name: 'Graph mode',
+        description: 'Stat panel graph / sparkline mode',
+        settings: {
+          options: [
+            { value: 'none', label: 'None' },
+            { value: 'area', label: 'Area' },
+          ],
+        },
+      })
+      .addRadio({
+        id: 'justifyMode',
+        name: 'Justify mode',
+        description: 'Value & title posititioning',
+        settings: {
+          options: [
+            { value: 'auto', label: 'Auto' },
+            { value: 'center', label: 'Center' },
+          ],
+        },
+      });
   })
   .setNoPadding()
   .setPanelChangeHandler(sharedSingleStatPanelChangedHandler)
