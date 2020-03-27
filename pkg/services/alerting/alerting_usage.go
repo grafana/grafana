598107ae272f08219ed7b11063c5ef9456c573cd
bergquist <carl.bergquist@gmail.com>
2020-03-27 22:04:06 +0100

    bytes didnt return the raw bytes of the object

--- pkg/services/alerting/alerting_usage.go
+++ pkg/services/alerting/alerting_usage.go
@@ -83,7 +83,7 @@ func (ae *AlertEngine) parseAlertRuleModel(settings *simplejson.Json) ([]int64, 
 		return datasourceIDs, nil
 	}
 
-	bytes, err := settings.Bytes()
+	bytes, err := settings.MarshalJSON()
 	if err != nil {
 		return datasourceIDs, err
 	}
