cfe699d5859c4a9a5c61571251867cfb402d2f0b
Ivana <ivana.huckova@gmail.com>
2020-03-30 16:56:18 +0200

    Fix storybook errors

--- packages/grafana-ui/src/types/icon.ts
+++ packages/grafana-ui/src/types/icon.ts
@@ -8,6 +8,7 @@ export type IconName =
   | 'angle-left'
   | 'angle-right'
   | 'pen'
+  | 'power'
   | 'trash-alt'
   | 'exclamation-triangle'
   | 'times'
@@ -18,6 +19,7 @@ export type IconName =
   | 'check'
   | 'add-panel'
   | 'copy'
+  | 'lock'
   | 'panel-add'
   | 'arrow-random'
   | 'arrow-from-right'
@@ -58,3 +60,65 @@ export type IconName =
   | 'eye'
   | 'monitor'
   | 'favorite';
+
+export const getAvailableIcons = (): IconName[] => [
+  'question-circle',
+  'plus',
+  'angle-up',
+  'angle-down',
+  'angle-left',
+  'angle-right',
+  'pen',
+  'power',
+  'trash-alt',
+  'exclamation-triangle',
+  'times',
+  'step-backward',
+  'square-shape',
+  'share-alt',
+  'forward',
+  'check',
+  'add-panel',
+  'copy',
+  'lock',
+  'panel-add',
+  'arrow-random',
+  'arrow-from-right',
+  'keyboard',
+  'search',
+  'chart-line',
+  'search-minus',
+  'clock-nine',
+  'sync',
+  'signin',
+  'cog',
+  'bars',
+  'save',
+  'apps',
+  'folder-plus',
+  'link',
+  'upload',
+  'home-alt',
+  'compass',
+  'sliders-v-alt',
+  'bell',
+  'database',
+  'user',
+  'plug',
+  'shield',
+  'key-skeleton-alt',
+  'users-alt',
+  'graph-bar',
+  'book',
+  'bolt',
+  'comments-alt',
+  'document-info',
+  'info-circle',
+  'bug',
+  'cube',
+  'star',
+  'edit',
+  'eye',
+  'monitor',
+  'favorite',
+];
