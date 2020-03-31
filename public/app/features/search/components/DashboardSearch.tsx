8778ab9326f7793e40b50aa0717cb50d61727f48
Clarity-89 <homes89@ukr.net>
2020-03-31 15:49:04 +0300

    Search: enable search

--- public/app/features/search/components/DashboardSearch.tsx
+++ public/app/features/search/components/DashboardSearch.tsx
@@ -1,5 +1,48 @@
 import React, { FC, useState, useEffect } from 'react';
+import { SearchSrv } from 'app/core/services/search_srv';
+import { SearchField } from './SearchField';
+import { SearchResults } from './SearchResults';
+
+const searchSrv = new SearchSrv();
 
 export const DashboardSearch: FC = () => {
-  return <div className="search-container">Search</div>;
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState({ query: '', parsedQuery: { text: '' }, tags: [], starred: false });
+
+  useEffect(() => {
+    search();
+  }, []);
+
+  const search = () => {
+    searchSrv.search(query).then(results => {
+      console.log('res', results);
+      setResults(results);
+    });
+  };
+
+  return (
+    <div className="search-container">
+      <SearchField query={query} onChange={query => setQuery(q => ({ ...q, query }))} />
+      <div className="search-dropdown">
+        <div className="search-dropdown__col_1">
+          <div className="search-results-scroller">
+            <div className="search-results-container" grafana-scrollbar>
+              <h6 ng-show="!ctrl.isLoading && ctrl.results.length === 0">
+                No dashboards matching your query were found.
+              </h6>
+
+              <SearchResults
+                results={results}
+                onSelectionChanged={() => {}}
+                onTagSelected={() => {}}
+                onFolderExpanding={() => {}}
+                onToggleSelection={() => {}}
+                editable={false}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
