61a5f8f7d2cf22c4217607e4a148b136869e99cb
Ivana <ivana.huckova@gmail.com>
2020-04-01 19:07:10 +0200

    Remove color prop from icon, remove color implemetation in mono icons

--- packages/grafana-ui/src/components/Icon/Icon.tsx
+++ packages/grafana-ui/src/components/Icon/Icon.tsx
@@ -12,39 +12,37 @@ import * as MonoIcon from './assets';
 
 interface IconProps extends React.HTMLAttributes<HTMLDivElement> {
   name: IconName;
-  size?: ComponentSize | 'xl' | 'logo';
+  size?: ComponentSize | 'xl' | 'xxl';
   type?: IconType;
-  color?: string;
 }
 export interface SvgProps extends React.HTMLAttributes<SVGElement> {
   size: number;
-  color: string;
   secondaryColor?: string;
   className?: string;
 }
 
-const getIconStyles = stylesFactory((theme: GrafanaTheme) => {
+const getIconStyles = stylesFactory((theme: GrafanaTheme, type: IconType) => {
+  const defaultIconColor = type === 'default' ? 'currentColor' : theme.colors.orange;
   return {
+    container: css`
+      display: inline-block;
+    `,
     icon: css`
       vertical-align: middle;
       display: inline-block;
       margin-bottom: ${theme.spacing.xxs};
       cursor: pointer;
-    `,
-    currentFontColor: css`
-      svg {
-        fill: currentColor;
+      * {
+        fill: ${defaultIconColor};
       }
     `,
   };
 });
 
 export const Icon = React.forwardRef<HTMLDivElement, IconProps>(
-  ({ size = 'md', type = 'default', color, title, name, className, ...divElementProps }, ref) => {
+  ({ size = 'md', type = 'default', title, name, className, style, ...divElementProps }, ref) => {
     const theme = useTheme();
-    const styles = getIconStyles(theme);
-    const mainColor = color || theme.colors.orange;
-    const secondaryColor = `${mainColor}99`;
+    const styles = getIconStyles(theme, type);
     const svgSize = getSvgSize(size, theme);
     const iconName = type === 'default' ? `Uil${pascalCase(name)}` : pascalCase(name);
 
@@ -57,17 +55,10 @@ export const Icon = React.forwardRef<HTMLDivElement, IconProps>(
     }
 
     return (
-      <span className={cx({ [styles.currentFontColor]: !color && type === 'default' })} {...divElementProps} ref={ref}>
-        {type === 'default' && <Component color={mainColor} size={svgSize} className={cx(styles.icon, className)} />}
-        {type === 'mono' && (
-          <Component
-            color={mainColor}
-            secondaryColor={secondaryColor}
-            size={svgSize}
-            className={cx(styles.icon, className)}
-          />
-        )}
-      </span>
+      <div className={styles.container} {...divElementProps} ref={ref}>
+        {type === 'default' && <Component size={svgSize} className={cx(styles.icon, className)} style={style} />}
+        {type === 'mono' && <Component size={svgSize} className={cx(styles.icon, className)} style={style} />}
+      </div>
     );
   }
 );
@@ -80,11 +71,11 @@ const pascalCase = (string: string) => {
 };
 
 /* Transform string with px to number and add 2 pxs as path in svg is 2px smaller */
-const getSvgSize = (size: ComponentSize | 'xl' | 'logo', theme: GrafanaTheme) => {
+const getSvgSize = (size: ComponentSize | 'xl' | 'xxl', theme: GrafanaTheme) => {
   let svgSize;
   if (size === 'xl') {
     svgSize = Number(theme.typography.heading.h1.slice(0, -2));
-  } else if (size === 'logo') {
+  } else if (size === 'xxl') {
     svgSize = Number(theme.height.lg.slice(0, -2));
   } else {
     svgSize = Number(theme.typography.size[size].slice(0, -2)) + 2;
