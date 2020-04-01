d175eefadc528a625073048538b5a4113993759d
srclosson <srclosson@gmail.com>
2020-04-01 11:00:31 -0600

    Moved from binary to native typescript 1. Moved to a native typescrpt github publish using the existing github client. 2. Change dist.js to detect running in a linked environment. Todo: Optimize docker image for build size.

--- packages/grafana-toolkit/src/cli/index.ts
+++ packages/grafana-toolkit/src/cli/index.ts
@@ -200,14 +200,14 @@ export const run = (includeInternalScripts = false) => {
     .option('--dryrun', 'Do a dry run only', false)
     .option('--verbose', 'Print verbose', false)
     .option('--commitHash <hashKey>', 'Specify the commit hash')
-    .option('--recreate', 'Recreate the release if already present')
-    .description('Publish to github ... etc etc etc')
+    .option('--dev', 'Create a development release')
+    .description('Publish to github')
     .action(async cmd => {
       await execTask(githubPublishTask)({
         dryrun: cmd.dryrun,
         verbose: cmd.verbose,
         commitHash: cmd.commitHash,
-        recreate: cmd.recreate,
+        dev: cmd.dev,
       });
     });
 
