ba9699e75bd0ed8c749b13d5f3428dfb02573959
Clarity-89 <homes89@ukr.net>
2020-04-01 16:53:35 +0300

    Search: add tag filter

--- public/app/features/search/components/DashboardSearch.tsx
+++ public/app/features/search/components/DashboardSearch.tsx
@@ -1,9 +1,12 @@
 import React, { FC, useState, useReducer } from 'react';
 import { useDebounce } from 'react-use';
 import { parse, SearchParserResult } from 'search-query-parser';
+import { Icon } from '@grafana/ui';
 import { SearchSrv } from 'app/core/services/search_srv';
 import { backendSrv } from 'app/core/services/backend_srv';
 import { SearchQuery } from 'app/core/components/search/search';
+import { TagFilter } from 'app/core/components/TagFilter/TagFilter';
+import { contextSrv } from 'app/core/services/context_srv';
 import { SearchField } from './SearchField';
 import { SearchResults } from './SearchResults';
 import { DashboardSection, SearchAction } from '../types';
@@ -81,6 +84,8 @@ const searchReducer = (state: any, action: SearchAction) => {
 };
 
 const defaultQuery: SearchQuery = { query: '', parsedQuery: { text: '' }, tags: [], starred: false };
+const { isEditor, hasEditPermissionInFolders } = contextSrv;
+const canEdit = isEditor || hasEditPermissionInFolders;
 
 export const DashboardSearch: FC = () => {
   const [query, setQuery] = useState(defaultQuery);
@@ -125,6 +130,17 @@ export const DashboardSearch: FC = () => {
 
   const onKeyDown = () => {};
 
+  const onTagFiltersChanged = (tags: string[]) => {
+    setQuery(q => ({ ...q, tags }));
+  };
+
+  const clearSearchFilter = () => {
+    // TODO check if resetting the query text is necessary
+    setQuery(q => ({ ...q, tags: [] }));
+  };
+
+  const onFilterBoxClick = () => {};
+
   return (
     <div className="search-container">
       <SearchField query={query} onChange={onQueryChange} onKeyDown={onKeyDown} />
@@ -141,6 +157,46 @@ export const DashboardSearch: FC = () => {
               />
             </div>
           </div>
+        </div>
+        <div className="search-dropdown__col_2">
+          <div className="search-filter-box" onClick={onFilterBoxClick}>
+            <div className="search-filter-box__header">
+              <Icon name="filter" />
+              Filter by:
+              {query.tags.length > 0 && (
+                <a className="pointer pull-right small" onClick={clearSearchFilter}>
+                  <Icon name="remove" /> Clear
+                </a>
+              )}
+            </div>
+
+            <TagFilter tags={query.tags} tagOptions={searchSrv.getDashboardTags} onChange={onTagFiltersChanged} />
+          </div>
+
+          {canEdit && (
+            <div className="search-filter-box">
+              <a href="dashboard/new" className="search-filter-box-link">
+                <i className="gicon gicon-dashboard-new"></i> New dashboard
+              </a>
+              {isEditor && (
+                <a href="dashboards/folder/new" className="search-filter-box-link">
+                  <i className="gicon gicon-folder-new"></i> New folder
+                </a>
+              )}
+              {canEdit && (
+                <a href="dashboard/import" className="search-filter-box-link">
+                  <i className="gicon gicon-dashboard-import"></i> Import dashboard
+                </a>
+              )}
+              <a
+                className="search-filter-box-link"
+                target="_blank"
+                href="https://grafana.com/dashboards?utm_source=grafana_search"
+              >
+                <img src="public/img/icn-dashboard-tiny.svg" width="20" /> Find dashboards on Grafana.com
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
