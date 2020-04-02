e62b052fdff0221f56340cedab6e605276ab0eb5
Arve Knudsen <arve.knudsen@gmail.com>
2020-04-02 17:27:45 +0200

    components/gtime: Fix test
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/components/gtime/gtime_test.go
+++ pkg/components/gtime/gtime_test.go
@@ -1,10 +1,11 @@
 package gtime
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestParseInterval(t *testing.T) {
@@ -13,7 +14,7 @@ func TestParseInterval(t *testing.T) {
 	tcs := []struct {
 		interval string
 		duration time.Duration
-		err      error
+		err      string
 	}{
 		{interval: "1d", duration: now.Sub(now.AddDate(0, 0, -1))},
 		{interval: "1w", duration: now.Sub(now.AddDate(0, 0, -7))},
@@ -21,17 +22,17 @@ func TestParseInterval(t *testing.T) {
 		{interval: "1M", duration: now.Sub(now.AddDate(0, -1, 0))},
 		{interval: "1y", duration: now.Sub(now.AddDate(-1, 0, 0))},
 		{interval: "5y", duration: now.Sub(now.AddDate(-5, 0, 0))},
-		{interval: "invalid-duration", err: errors.New("time: invalid duration invalid-duration")},
+		{interval: "invalid-duration", err: "time: invalid duration invalid-duration"},
 	}
 
 	for i, tc := range tcs {
 		t.Run(fmt.Sprintf("testcase %d", i), func(t *testing.T) {
 			res, err := ParseInterval(tc.interval)
-			if err != nil && (tc.err == nil || err.Error() != tc.err.Error()) {
-				t.Fatalf("expected '%v' got '%v'", tc.err, err)
-			}
-			if res != tc.duration {
-				t.Errorf("expected %v got %v", tc.duration, res)
+			if tc.err == "" {
+				require.NoError(t, err, "interval %q", tc.interval)
+				require.Equal(t, tc.duration, res, "interval %q", tc.interval)
+			} else {
+				require.EqualError(t, err, tc.err, "interval %q", tc.interval)
 			}
 		})
 	}
