db58f0ec6b64377bbc87b8ae868382753cd367ec
Agnès Toulet <agnes.toulet@gmail.com>
2020-04-02 17:33:20 +0200

    Core: clean gtime tests

--- pkg/components/gtime/gtime_test.go
+++ pkg/components/gtime/gtime_test.go
@@ -15,19 +15,19 @@ func TestParseInterval(t *testing.T) {
 		duration time.Duration
 		err      error
 	}{
-		{interval: "1d", duration: now.Sub(now.AddDate(0, 0, -1)), err: errors.New("")},
-		{interval: "1w", duration: now.Sub(now.AddDate(0, 0, -7)), err: errors.New("")},
-		{interval: "2w", duration: now.Sub(now.AddDate(0, 0, -14)), err: errors.New("")},
-		{interval: "1M", duration: now.Sub(now.AddDate(0, -1, 0)), err: errors.New("")},
-		{interval: "1y", duration: now.Sub(now.AddDate(-1, 0, 0)), err: errors.New("")},
-		{interval: "5y", duration: now.Sub(now.AddDate(-5, 0, 0)), err: errors.New("")},
+		{interval: "1d", duration: now.Sub(now.AddDate(0, 0, -1))},
+		{interval: "1w", duration: now.Sub(now.AddDate(0, 0, -7))},
+		{interval: "2w", duration: now.Sub(now.AddDate(0, 0, -14))},
+		{interval: "1M", duration: now.Sub(now.AddDate(0, -1, 0))},
+		{interval: "1y", duration: now.Sub(now.AddDate(-1, 0, 0))},
+		{interval: "5y", duration: now.Sub(now.AddDate(-5, 0, 0))},
 		{interval: "invalid-duration", err: errors.New("time: invalid duration invalid-duration")},
 	}
 
 	for i, tc := range tcs {
 		t.Run(fmt.Sprintf("testcase %d", i), func(t *testing.T) {
 			res, err := ParseInterval(tc.interval)
-			if err != nil && err.Error() != tc.err.Error() {
+			if err != nil && (tc.err == nil || err.Error() != tc.err.Error()) {
 				t.Fatalf("expected '%v' got '%v'", tc.err, err)
 			}
 			if res != tc.duration {
