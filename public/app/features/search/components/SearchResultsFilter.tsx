4d2f98e55c46f6d35aeab4f0b55096b0f422d00c
Clarity-89 <homes89@ukr.net>
2020-04-02 13:26:06 +0300

    Search: Use HorizontalGroup

--- public/app/features/search/components/SearchResultsFilter.tsx
+++ public/app/features/search/components/SearchResultsFilter.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { css } from 'emotion';
-import { Button, Forms, stylesFactory, useTheme } from '@grafana/ui';
+import { Button, Forms, stylesFactory, useTheme, HorizontalGroup } from '@grafana/ui';
 import { GrafanaTheme, SelectableValue } from '@grafana/data';
 
 type onSelectChange = (value: SelectableValue) => void;
@@ -44,36 +44,34 @@ export const SearchResultsFilter: FC<Props> = ({
   return (
     <div className={styles.wrapper}>
       <Forms.Checkbox value={allChecked} onChange={onSelectAllChanged} />
-      <div className={styles.row}>
-        {showActions ? (
-          <div className={styles.row}>
-            <Button disabled={!canMove} onClick={moveTo} icon="fa fa-exchange" variant="secondary">
-              Move
-            </Button>
-            <Button disabled={!canDelete} onClick={deleteItem} icon="fa fa-trash" variant="destructive">
-              Delete
-            </Button>
-          </div>
-        ) : (
-          <div className={styles.row}>
-            <Forms.Select
-              size="sm"
-              placeholder="Filter by starred"
-              key={selectedStarredFilter}
-              options={starredFilterOptions}
-              onChange={onStarredFilterChange}
-            />
+      {showActions ? (
+        <HorizontalGroup spacing="md">
+          <Button disabled={!canMove} onClick={moveTo} icon="fa fa-exchange" variant="secondary">
+            Move
+          </Button>
+          <Button disabled={!canDelete} onClick={deleteItem} icon="fa fa-trash" variant="destructive">
+            Delete
+          </Button>
+        </HorizontalGroup>
+      ) : (
+        <HorizontalGroup spacing="md">
+          <Forms.Select
+            size="sm"
+            placeholder="Filter by starred"
+            key={selectedStarredFilter}
+            options={starredFilterOptions}
+            onChange={onStarredFilterChange}
+          />
 
-            <Forms.Select
-              size="sm"
-              placeholder="Filter by tag"
-              key={selectedTagFilter}
-              options={tagFilterOptions}
-              onChange={onTagFilterChange}
-            />
-          </div>
-        )}
-      </div>
+          <Forms.Select
+            size="sm"
+            placeholder="Filter by tag"
+            key={selectedTagFilter}
+            options={tagFilterOptions}
+            onChange={onTagFilterChange}
+          />
+        </HorizontalGroup>
+      )}
     </div>
   );
 };
@@ -89,13 +87,6 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
       label {
         height: 20px;
         margin-left: 4px;
-      }
-    `,
-    row: css`
-      display: flex;
-
-      & > button:first-child {
-        margin-right: ${theme.spacing.md};
       }
     `,
   };
