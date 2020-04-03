4f7ac268e02f0ddfb00f081f92ea96770b305387
srclosson <srclosson@gmail.com>
2020-04-03 09:03:55 -0600

    template for testing

---
+++ packages/grafana-toolkit/src/cli/tasks/plugin.utils.test.ts
@@ -0,0 +1,5 @@
+describe('A github publish plugin', () => {
+  it('Should do something', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ packages/grafana-toolkit/src/cli/utils/githubRelease.test.ts
@@ -0,0 +1,5 @@
+describe('A github release tool', () => {
+  it('Should do a release', () => {
+    expect(true).toBeTruthy();
+  });
+});
