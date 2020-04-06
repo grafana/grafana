2c8e2b3b627073940a342ca09f9fe4db17d04837
srclosson <srclosson@gmail.com>
2020-04-06 08:32:29 -0600

    Wrote a basic test.

--- packages/grafana-toolkit/bin/grafana-toolkit.dist.js
+++ packages/grafana-toolkit/bin/grafana-toolkit.dist.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 
 entrypoint = () => {
   const defaultEntryPoint = '../src/cli/index.js';
-  // We are running in dev mode. Don't use compiled binaries, rather use the dev entrypoint.
-  if (fs.existsSync(`${process.env['HOME']}/.config/yarn/link/@grafana/toolkit`)) {
-    console.log('Running in linked mode');
-    return `${__dirname}/grafana-toolkit.js`;
+  const toolkitDirectory = `${process.env['PWD']}/node_modules/@grafana/toolkit`;
+
+  // IF we have a toolkit directory AND linked grafana toolkit AND the toolkit dir is a symbolic lik
+  // THEN run everything in linked mode
+  if (fs.existsSync(toolkitDirectory)) {
+    const tkStat = fs.lstatSync(toolkitDirectory);
+    if (fs.existsSync(`${process.env['HOME']}/.config/yarn/link/@grafana/toolkit` && tkStat.isSymbolicLink())) {
+      console.log('Running in linked mode');
+      return `${__dirname}/grafana-toolkit.js`;
+    }
   }
 
   // We are using npx, and a relative path does not find index.js
--- packages/grafana-toolkit/src/cli/tasks/plugin.utils.test.ts
+++
@@ -1,5 +0,0 @@
-describe('A github publish plugin', () => {
-  it('Should do something', () => {
-    expect(true).toBeTruthy();
-  });
-});
