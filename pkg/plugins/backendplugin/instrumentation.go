8e4ca5bf3957bb769636a0b7295687a2ef8747f0
bergquist <carl.bergquist@gmail.com>
2020-04-06 12:32:47 +0200

    renames metric label

--- pkg/plugins/backendplugin/instrumentation.go
+++ pkg/plugins/backendplugin/instrumentation.go
@@ -11,29 +11,29 @@ func init() {
 	pluginRequestCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "plugin_request_total",
 		Help: "The total amount of plugin requests",
-	}, []string{"name", "endpoint", "status"})
+	}, []string{"plugin_id", "endpoint", "status"})
 
 	pluginRequestLatency = prometheus.NewSummaryVec(prometheus.SummaryOpts{
 		Name:       "plugin_request_latency",
 		Help:       "Plugin request latency",
 		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
-	}, []string{"name", "endpoint"})
+	}, []string{"plugin_id", "endpoint"})
 
 	prometheus.MustRegister(pluginRequestCounter, pluginRequestLatency)
 }
 
 // InstrumentPluginRequest instruments success rate and latency of `fn`
-func InstrumentPluginRequest(pluginType string, endpoint string, fn func() error) error {
+func InstrumentPluginRequest(pluginID string, endpoint string, fn func() error) error {
 	status := "ok"
 
-	t := prometheus.NewTimer(pluginRequestLatency.WithLabelValues(pluginType, endpoint))
+	t := prometheus.NewTimer(pluginRequestLatency.WithLabelValues(pluginID, endpoint))
 
 	err := fn()
 	if err != nil {
 		status = "error"
 	}
 
-	pluginRequestCounter.WithLabelValues(pluginType, endpoint, status).Inc()
+	pluginRequestCounter.WithLabelValues(pluginID, endpoint, status).Inc()
 	t.ObserveDuration()
 
 	return err
