1f717f514a6585ec13e35d82586f8f39d8439f75
Marcus Andersson <marcus.andersson@grafana.com>
2020-04-07 06:54:09 +0200

    TabelPanel: add support for organizing fields/columns. (#23135)
    * Added draft on transformers to sort and hide fields.
    
    * added structure for the UI.
    
    * draft on sorting/filtering UI.
    
    * simplified the datastructure a bit.
    
    * added draft on drag and drop support.
    
    * added some super simple styling. Nothing final still waiting for a proper design on this.
    
    * updated lockfile after merge.
    
    * changed so we use the new path for button.
    
    * added one more test.
    
    * Ignore feature toggle
    
    * Moved editor to app
    
    * Added top description
    
    * Minor update
    
    * Did some renaming and simplified the code a bit.
    
    * fixed so we dont use capital naming on the transformer.
    
    * changed to an vertical drag and drop design.
    
    * added support to rename fields.
    
    Co-authored-by: Torkel Ödegaard <torkel@grafana.com>

--- packages/grafana-ui/src/components/index.ts
+++ packages/grafana-ui/src/components/index.ts
@@ -103,8 +103,6 @@ export { DataLinkInput } from './DataLinks/DataLinkInput';
 export { DataLinksContextMenu } from './DataLinks/DataLinksContextMenu';
 export { SeriesIcon } from './Legend/SeriesIcon';
 export { transformersUIRegistry } from './TransformersUI/transformers';
-export { TransformationRow } from './TransformersUI/TransformationRow';
-export { TransformationsEditor } from './TransformersUI/TransformationsEditor';
 export { JSONFormatter } from './JSONFormatter/JSONFormatter';
 export { JsonExplorer } from './JSONFormatter/json_explorer/json_explorer';
 export { ErrorBoundary, ErrorBoundaryAlert } from './ErrorBoundary/ErrorBoundary';
