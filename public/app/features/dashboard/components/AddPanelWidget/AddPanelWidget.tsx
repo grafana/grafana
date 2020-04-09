2b9d95b03c30fc063a068af349b9d4f54f0dd9a6
Torkel Ödegaard <torkel@grafana.com>
2020-04-09 11:01:04 +0200

    Fixed add panel issue

--- public/app/features/dashboard/components/AddPanelWidget/AddPanelWidget.tsx
+++ public/app/features/dashboard/components/AddPanelWidget/AddPanelWidget.tsx
@@ -86,16 +86,13 @@ export class AddPanelWidgetUnconnected extends React.Component<Props, State> {
 
     const location: LocationUpdate = {
       query: {
-        panelId: newPanel.id,
-        edit: true,
-        fullscreen: true,
+        editPanel: newPanel.id,
       },
       partial: true,
     };
 
-    if (tab === 'visualization') {
-      location.query.tab = 'visualization';
-      location.query.openVizPicker = true;
+    if (tab === 'visualize') {
+      location.query.tab = 'visualize';
     }
 
     reduxStore.dispatch(updateLocation(location));
@@ -174,9 +171,7 @@ export class AddPanelWidgetUnconnected extends React.Component<Props, State> {
           <div className="add-panel-widget__btn-container">
             <div className="add-panel-widget__create">
               {this.renderOptionLink('database', 'Add Query', this.onCreateNewPanel)}
-              {this.renderOptionLink('chart-line', 'Choose Visualization', () =>
-                this.onCreateNewPanel('visualization')
-              )}
+              {this.renderOptionLink('chart-line', 'Choose Visualization', () => this.onCreateNewPanel('visualize'))}
             </div>
             <div className="add-panel-widget__actions">
               <button className="btn btn-inverse add-panel-widget__action" onClick={this.onCreateNewRow}>
