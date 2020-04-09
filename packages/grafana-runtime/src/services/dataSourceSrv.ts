0dc1955fb7841efae80a034fe87723d2411fe481
Marcus Andersson <marcus.andersson@grafana.com>
2020-04-09 10:55:59 +0200

    docs for datasource_srv.

--- packages/grafana-runtime/src/services/dataSourceSrv.ts
+++ packages/grafana-runtime/src/services/dataSourceSrv.ts
@@ -1,15 +1,31 @@
 import { ScopedVars, DataSourceApi } from '@grafana/data';
 
+/**
+ * Used to retrieve the {@link DataSourceSrv} that can be used to
+ *
+ * @public
+ */
 export interface DataSourceSrv {
   get(name?: string, scopedVars?: ScopedVars): Promise<DataSourceApi>;
 }
 
 let singletonInstance: DataSourceSrv;
 
+/**
+ * Used during startup by Grafana to set the DataSourceSrv so it is available
+ * via the the {@link getDataSourceSrv()} to the rest of the application.
+ *
+ * @internal
+ */
 export function setDataSourceSrv(instance: DataSourceSrv) {
   singletonInstance = instance;
 }
 
+/**
+ * Used to retrieve the {@link DataSourceSrv} that can be used to
+ *
+ * @public
+ */
 export function getDataSourceSrv(): DataSourceSrv {
   return singletonInstance;
 }
