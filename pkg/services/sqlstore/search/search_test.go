b9a82eb542f5eee7ab1fec27740c8aaf71087b38
Emil Tullstedt <emil.tullstedt@grafana.com>
2020-04-09 17:11:56 +0200

    Search: More tests

--- pkg/services/sqlstore/search/search_test.go
+++ pkg/services/sqlstore/search/search_test.go
@@ -3,6 +3,7 @@ package search_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/sqlstore"
@@ -16,19 +17,28 @@ import (
 
 var dialect = &migrator.Sqlite3{}
 
-func TestEqualResults(t *testing.T) {
+const (
+	limit int64 = 15
+	page  int64 = 1
+)
+
+func TestBuilder_EqualResults_Basic(t *testing.T) {
 	user := &models.SignedInUser{
 		UserId:  1,
 		OrgId:   1,
 		OrgRole: models.ROLE_EDITOR,
 	}
-	limit := int64(15)
-	page := int64(1)
-	permission := models.PERMISSION_EDIT
 
 	db := setupTestEnvironment(t)
+	err := createDashboards(0, 1, user.OrgId)
+	require.NoError(t, err)
 
-	new := &search.Builder{
+	// create one dashboard in another organization that shouldn't
+	// be listed in the results.
+	err = createDashboards(1, 2, 2)
+	require.NoError(t, err)
+
+	builder := &search.Builder{
 		Filters: []interface{}{
 			search.OrgFilter{OrgId: user.OrgId},
 			search.TitleSorter{},
@@ -36,37 +46,19 @@ func TestEqualResults(t *testing.T) {
 		Dialect: dialect,
 	}
 
-	old := sqlstore.NewSearchBuilder(user, limit, page, permission)
-	old.WithDialect(dialect)
-
-	dashboard, err := simplejson.NewJson([]byte(`{
-    	"id": null,
-    	"uid": null,
-    	"title": "Production Overview",
-    	"tags": [ "templated" ],
-    	"timezone": "browser",
-    	"schemaVersion": 16,
-    	"version": 0
-	}`))
-	require.NoError(t, err)
-	err = sqlstore.SaveDashboard(&models.SaveDashboardCommand{
-		Dashboard: dashboard,
-		UserId:    1,
-		OrgId:     1,
-		UpdatedAt: time.Now(),
-	})
-	require.NoError(t, err)
+	prevBuilder := sqlstore.NewSearchBuilder(user, limit, page, models.PERMISSION_EDIT)
+	prevBuilder.WithDialect(dialect)
 
 	newRes := []sqlstore.DashboardSearchProjection{}
 	err = db.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
-		sql, params := new.ToSql(limit, page)
+		sql, params := builder.ToSql(limit, page)
 		return sess.SQL(sql, params...).Find(&newRes)
 	})
 	require.NoError(t, err)
 
 	oldRes := []sqlstore.DashboardSearchProjection{}
 	err = db.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
-		sql, params := old.ToSql()
+		sql, params := prevBuilder.ToSql()
 		return sess.SQL(sql, params...).Find(&oldRes)
 	})
 	require.NoError(t, err)
@@ -75,8 +67,140 @@ func TestEqualResults(t *testing.T) {
 	assert.EqualValues(t, oldRes, newRes)
 }
 
+func TestBuilder_Pagination(t *testing.T) {
+	user := &models.SignedInUser{
+		UserId:  1,
+		OrgId:   1,
+		OrgRole: models.ROLE_VIEWER,
+	}
+
+	db := setupTestEnvironment(t)
+	err := createDashboards(0, 25, user.OrgId)
+	require.NoError(t, err)
+
+	builder := &search.Builder{
+		Filters: []interface{}{
+			search.OrgFilter{OrgId: user.OrgId},
+			search.TitleSorter{},
+		},
+		Dialect: dialect,
+	}
+
+	resPg1 := []sqlstore.DashboardSearchProjection{}
+	resPg2 := []sqlstore.DashboardSearchProjection{}
+	resPg3 := []sqlstore.DashboardSearchProjection{}
+	err = db.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
+		sql, params := builder.ToSql(15, 1)
+		err := sess.SQL(sql, params...).Find(&resPg1)
+		if err != nil {
+			return err
+		}
+		sql, params = builder.ToSql(15, 2)
+		err = sess.SQL(sql, params...).Find(&resPg2)
+		if err != nil {
+			return err
+		}
+
+		sql, params = builder.ToSql(15, 3)
+		return sess.SQL(sql, params...).Find(&resPg3)
+	})
+	require.NoError(t, err)
+
+	assert.Len(t, resPg1, 15)
+	assert.Len(t, resPg2, 10)
+	assert.Len(t, resPg3, 0, "sanity check: pages after last should be empty")
+
+	assert.Equal(t, "A", resPg1[0].Title, "page 1 should start with the first dashboard")
+	assert.Equal(t, "P", resPg2[0].Title, "page 2 should start with the 16th dashboard")
+}
+
+func TestBuilder_Permissions(t *testing.T) {
+	t.Skip("Permissions are not implemented in the new search builder yet!")
+
+	user := &models.SignedInUser{
+		UserId:  1,
+		OrgId:   1,
+		OrgRole: models.ROLE_VIEWER,
+	}
+
+	db := setupTestEnvironment(t)
+	err := createDashboards(0, 1, user.OrgId)
+	require.NoError(t, err)
+
+	builder := &search.Builder{
+		Filters: []interface{}{
+			search.OrgFilter{OrgId: user.OrgId},
+			search.TitleSorter{},
+		},
+		Dialect: dialect,
+	}
+
+	prevBuilder := sqlstore.NewSearchBuilder(user, limit, page, models.PERMISSION_EDIT)
+	prevBuilder.WithDialect(dialect)
+
+	newRes := []sqlstore.DashboardSearchProjection{}
+	err = db.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
+		sql, params := builder.ToSql(limit, page)
+		return sess.SQL(sql, params...).Find(&newRes)
+	})
+	require.NoError(t, err)
+
+	oldRes := []sqlstore.DashboardSearchProjection{}
+	err = db.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
+		sql, params := prevBuilder.ToSql()
+		return sess.SQL(sql, params...).Find(&oldRes)
+	})
+	require.NoError(t, err)
+
+	assert.Len(t, newRes, 0)
+	assert.EqualValues(t, oldRes, newRes)
+}
+
 func setupTestEnvironment(t *testing.T) *sqlstore.SqlStore {
 	t.Helper()
 
 	return sqlstore.InitTestDB(t)
+}
+
+func createDashboards(startID, endID int, orgID int64) error {
+	if endID < startID {
+		return fmt.Errorf("startID must be smaller than endID")
+	}
+
+	for i := startID; i < endID; i++ {
+		dashboard, err := simplejson.NewJson([]byte(`{
+			"id": null,
+			"uid": null,
+			"title": "` + lexiCounter(i) + `",
+			"tags": [ "templated" ],
+			"timezone": "browser",
+			"schemaVersion": 16,
+			"version": 0
+		}`))
+		if err != nil {
+			return err
+		}
+		err = sqlstore.SaveDashboard(&models.SaveDashboardCommand{
+			Dashboard: dashboard,
+			UserId:    1,
+			OrgId:     orgID,
+			UpdatedAt: time.Now(),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// lexiCounter counts in a lexiographically sortable order.
+func lexiCounter(n int) string {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	value := string(alphabet[n%26])
+
+	if n >= 26 {
+		value = lexiCounter(n/26-1) + value
+	}
+
+	return value
 }
