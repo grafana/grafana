51a885430d72a2d7ea3bfc8a30b438e76208974f
srclosson <srclosson@gmail.com>
2020-04-13 09:49:46 -0600

    added built vm to test tool

--- packages/grafana-toolkit/docker/grafana-plugin-ci-alpine/test/start.sh
+++ packages/grafana-toolkit/docker/grafana-plugin-ci-alpine/test/start.sh
@@ -7,4 +7,8 @@ function finish {
 trap finish EXIT
 
 # Enter the docker container
-docker-compose run citest sh -c "cd /home/circleci; exec sh --login -i"
+if [ "$1" = "built" ]; then
+  docker-compose run cibuilt sh -c "cd /home/circleci; exec sh --login -i"
+else 
+  docker-compose run citest sh -c "cd /home/circleci; exec sh --login -i"
+fi
