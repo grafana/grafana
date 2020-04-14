875edd341820fbb0d46f808d0d5b848a4782fed6
srclosson <srclosson@gmail.com>
2020-04-14 12:49:11 -0600

    Reverted change used only for testing

--- packages/grafana-toolkit/bin/grafana-toolkit.js
+++ packages/grafana-toolkit/bin/grafana-toolkit.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-let includeInternalScripts = true;
+let includeInternalScripts = false;
 
 const isLinkedMode = () => {
   let pwd = process.env['PWD'];
