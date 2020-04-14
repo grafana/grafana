e389a53c004404687967a3f65945e479065a7f65
Ryan McKinley <ryantxu@gmail.com>
2020-04-13 21:59:52 -0700

    add comments

--- public/app/plugins/panel/table/migrations.ts
+++ public/app/plugins/panel/table/migrations.ts
@@ -2,10 +2,15 @@ import { PanelModel } from '@grafana/data';
 import { Options } from './types';
 import { ChangePanelTypeError } from 'app/features/dashboard/state/ChangePanelTypeError';
 
-// This is called when the panel first loads with the version
+/**
+ * At 7.0, the `table` panel was swapped from an angular implementation to a react one.
+ * The models do not match, so this process will delegate to the old implementation when
+ * a saved table configuration exists.
+ */
 export const tableMigrationHandler = (panel: PanelModel<Options>): Partial<Options> => {
   // Table was saved as an angular table, lets just swap to the 'table-old' panel
   if (!panel.pluginVersion && (panel as any).columns) {
+    // TODO: depending on the configuration, we be able to automatically migrate
     throw new ChangePanelTypeError('table-old');
   }
 
@@ -13,7 +18,9 @@ export const tableMigrationHandler = (panel: PanelModel<Options>): Partial<Optio
   return panel.options;
 };
 
-// This is called when the panel changes from another panel
+/**
+ * This is called when the panel changes from another panel
+ */
 export const tablePanelChangedHandler = (
   panel: PanelModel<Partial<Options>> | any,
   prevPluginId: string,
