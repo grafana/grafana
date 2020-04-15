e413bff69581e27a5bdc469bb6f732153f1deb7d
andrej ocenas <mr.ocenas@gmail.com>
2020-04-15 17:11:34 +0200

    Update tests

--- public/app/features/explore/Explore.test.tsx
+++ public/app/features/explore/Explore.test.tsx
@@ -20,6 +20,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from 'app/store/configureStore';
 import { SecondaryActions } from './SecondaryActions';
 import { TraceView } from './TraceView/TraceView';
+import { getTheme } from '@grafana/ui';
 
 const dummyProps: ExploreProps = {
   changeSize: jest.fn(),
@@ -119,20 +120,7 @@ const dummyProps: ExploreProps = {
   },
   originPanelId: 1,
   addQueryRow: jest.fn(),
-};
-
-const setup = (renderMethod: any, propOverrides?: Partial<ExploreProps>) => {
-  const store = configureStore();
-  return renderMethod(
-    <Provider store={store}>
-      <Explore
-        {...{
-          ...dummyProps,
-          ...propOverrides,
-        }}
-      />
-    </Provider>
-  );
+  theme: getTheme(),
 };
 
 const setupErrors = (hasRefId?: boolean) => {
@@ -148,7 +136,7 @@ const setupErrors = (hasRefId?: boolean) => {
 
 describe('Explore', () => {
   it('should render component', () => {
-    const wrapper = setup(shallow);
+    const wrapper = shallow(<Explore {...dummyProps} />);
     expect(wrapper).toMatchSnapshot();
   });
 
