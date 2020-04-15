8f0c0897aad26c020f917a8cf6d42b4b2720cf8c
Torkel Ödegaard <torkel@grafana.com>
2020-04-15 15:57:42 +0200

    fixed ts issue

--- packages/grafana-ui/src/components/TextArea/TextArea.story.tsx
+++ packages/grafana-ui/src/components/TextArea/TextArea.story.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextArea } from './TextArea';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
-import { boolean, number, select, text } from '@storybook/addon-knobs';
+import { boolean, number, text } from '@storybook/addon-knobs';
 import mdx from './TextArea.mdx';
 
 export default {
