2de79a4d8c326cd728ef7ec841ffd62e836ae059
Stephanie Closson <srclosson@gmail.com>
2020-04-16 05:38:56 -0600

    Because alpine uses musl instead of libc, the e2e/cypress was not compatible (#23602)
    So:
    - Created new VM based on debian-slim
    - Could also be used as a build VM
    
    Fixes:
    - ginstall issue with merge somewhere.
    - Trimmed down the alpine VM since we don't need the extra libraries for cypress

--- packages/grafana-toolkit/docker/grafana-plugin-ci-e2e/scripts/deploy.sh
+++ packages/grafana-toolkit/docker/grafana-plugin-ci-e2e/scripts/deploy.sh
@@ -1,25 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+source "/etc/profile"
+source "./deploy-slim.sh"
 source "./deploy-common.sh"
 
-# Make libgcc compatible
-mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+NODEVER="v12.16.2-linux-x64"
+# Install Node
+wget -O - "https://nodejs.org/dist/v12.16.2/node-${NODEVER}.tar.xz" | tar Jvxf - -C "/tmp"
 
-# Replace cp with something that mocks the one that ci-package needs
-rm /bin/cp
-mv /usr/local/bin/cp /bin/cp
+# Move node to /usr/local so it's in the path
+pushd /tmp/node-${NODEVER}
+/bin/rm -f CHANGELOG.md README.md LICENSE
+/bin/cp -r * /usr/local
+/bin/cp -r .??* /usr/local
+popd
+/bin/rm -rf /tmp/node-${NODEVER}
 
-sed -i -e 's/v[[:digit:]]\..*\//edge\//g' /etc/apk/repositories
-apk add nodejs npm yarn build-base openssh 
-
-#
-# Only relevant for testing, but cypress does not work with musl/alpine.
-#
-# apk add xvfb glib nss nspr gdk-pixbuf "gtk+3.0" pango atk cairo dbus-libs libxcomposite libxrender libxi libxtst libxrandr libxscrnsaver alsa-lib at-spi2-atk at-spi2-core cups-libs gcompat libc6-compat
+# Resource the profile so we know our path is being honoured
+source "/etc/profile"
+# Install Yarn. Not in the path yet so fully qualified
+npm i -g yarn
 
 # Install Go
 filename="go1.14.linux-amd64.tar.gz"
 get_file "https://dl.google.com/go/$filename" "/tmp/$filename" "08df79b46b0adf498ea9f320a0f23d6ec59e9003660b4c9c1ce8e5e2c6f823ca"
 untar_file "/tmp/$filename"
+
 
 # Install golangci-lint
 filename="golangci-lint-1.23.7-linux-amd64.tar.gz"
@@ -32,22 +37,20 @@ ln -s /usr/local/golangci-lint-1.23.7-linux-amd64/golangci-lint /usr/local/bin/g
 ln -s /usr/local/go/bin/go /usr/local/bin/go
 ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
 
-# Install dependencies
-apk add fontconfig zip jq
-
 # Install code climate
 get_file "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64" \
     "/usr/local/bin/cc-test-reporter" \
     "38f2442892027f61a07f52c845818750261b2ba58bffb043a582495339d37c05"
-chmod +x /usr/local/bin/cc-test-reporter
+chmod 755 /usr/local/bin/cc-test-reporter
 
-apk add git
 # Install Mage
 mkdir -pv /tmp/mage $HOME/go/bin
 git clone https://github.com/magefile/mage.git /tmp/mage
-cd /tmp/mage && go run bootstrap.go
+pushd /tmp/mage && go run bootstrap.go && popd
 mv $HOME/go/bin/mage /usr/local/bin
 # Cleanup after yourself
 /bin/rm -rf /tmp/mage 
 /bin/rm -rf $HOME/go
-/bin/rm -rf /var/cache/apk/*
+
+# Get the size down
+/bin/rm -rf /var/lib/apt/lists
