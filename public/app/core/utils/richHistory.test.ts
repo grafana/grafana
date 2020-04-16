8a1a6f65952abf73dbad81672b4c76015730f788
Ivana <ivana.huckova@gmail.com>
2020-04-16 16:50:28 +0200

    Update test

--- public/app/core/utils/richHistory.test.ts
+++ public/app/core/utils/richHistory.test.ts
@@ -28,7 +28,7 @@ const mock: any = {
   comment: '',
   datasourceId: 'datasourceId',
   datasourceName: 'datasourceName',
-  queries: ['query3'],
+  queries: ['{"expr": "query3"}'],
   sessionName: '',
   starred: false,
 };
@@ -179,6 +179,7 @@ describe('createQueryHeading', () => {
 describe('createDataQuery', () => {
   it('should correctly create data query from rich history query', () => {
     const dataQuery = createDataQuery(mock.history[0], mock.queries[0], 0);
+    console.log(dataQuery);
     expect(dataQuery).toEqual({ datasource: 'datasource history name', expr: 'query3', refId: 'A' });
   });
 });
