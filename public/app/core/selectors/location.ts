8709c9a8a51d4f95b8abadf11d8e2aa17ecd8c7a
Alex Khomenko <Clarity-89@users.noreply.github.com>
2020-04-20 18:04:51 +0300

    Search: Toggle Search based on search query (#23648)
    * Search: Toggle Search based on search query
    
    * Search: Fix types and closed search param
    
    * Search: Remove appEvents from SearchWrapper
    
    * Search: Reset folder on close
    
    Co-Authored-By: Alexander Zobnin <alexanderzobnin@gmail.com>
    
    * Search: Disable reloadOnSearch for manage dashboards urls
    
    Co-authored-by: Alexander Zobnin <alexanderzobnin@gmail.com>

--- public/app/core/selectors/location.ts
+++ public/app/core/selectors/location.ts
@@ -3,3 +3,4 @@ import { LocationState } from 'app/types';
 export const getRouteParamsId = (state: LocationState) => state.routeParams.id;
 export const getRouteParamsPage = (state: LocationState) => state.routeParams.page;
 export const getRouteParams = (state: LocationState) => state.routeParams;
+export const getLocationQuery = (state: LocationState) => state.query;
