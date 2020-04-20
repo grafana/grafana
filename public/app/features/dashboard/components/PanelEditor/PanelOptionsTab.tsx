fd58b9b9ab7e9a6f59b32c38764e2260ec758662
Dominik Prokop <dominik.prokop@grafana.com>
2020-04-20 12:11:50 +0200

    Move visualisation picker to the sidebar pane

--- public/app/features/dashboard/components/PanelEditor/PanelOptionsTab.tsx
+++ public/app/features/dashboard/components/PanelEditor/PanelOptionsTab.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useRef } from 'react';
 import { DashboardModel, PanelModel } from '../../state';
 import { FieldConfigSource, PanelData, PanelPlugin, SelectableValue } from '@grafana/data';
 import { Counter, DataLinksInlineEditor, Field, Input, RadioButtonGroup, Select, Switch, TextArea } from '@grafana/ui';
@@ -7,6 +7,7 @@ import { getPanelLinksVariableSuggestions } from '../../../panel/panellinks/link
 import { getVariables } from '../../../variables/state/selectors';
 import { PanelOptionsEditor } from './PanelOptionsEditor';
 import { AngularPanelOptions } from './AngularPanelOptions';
+import { VisualizationTab } from './VisualizationTab';
 
 interface Props {
   panel: PanelModel;
@@ -27,8 +28,9 @@ export const PanelOptionsTab: FC<Props> = ({
   onPanelOptionsChanged,
   onFieldConfigsChange,
 }) => {
-  const elements: JSX.Element[] = [];
+  const visTabInputRef = useRef<HTMLInputElement>();
   const linkVariablesSuggestions = useMemo(() => getPanelLinksVariableSuggestions(), []);
+  const elements: JSX.Element[] = [];
   const panelLinksCount = panel && panel.links ? panel.links.length : 0;
 
   const variableOptions = getVariableOptions();
@@ -39,9 +41,14 @@ export const PanelOptionsTab: FC<Props> = ({
 
   const maxPerRowOptions = [2, 3, 4, 6, 8, 12].map(value => ({ label: value.toString(), value }));
 
+  const focusVisPickerInput = (isExpanded: boolean) => {
+    if (isExpanded && visTabInputRef.current) {
+      visTabInputRef.current.focus();
+    }
+  };
   // Fist common panel settings Title, description
   elements.push(
-    <OptionsGroup title="Basic" key="basic settings">
+    <OptionsGroup title="Panel settings" key="Panel settings">
       <Field label="Panel title">
         <Input defaultValue={panel.title} onBlur={e => onPanelConfigChange('title', e.currentTarget.value)} />
       </Field>
@@ -54,6 +61,12 @@ export const PanelOptionsTab: FC<Props> = ({
       <Field label="Transparent" description="Display panel without a background.">
         <Switch value={panel.transparent} onChange={e => onPanelConfigChange('transparent', e.currentTarget.checked)} />
       </Field>
+    </OptionsGroup>
+  );
+
+  elements.push(
+    <OptionsGroup title="Visualisation type" key="Visualisation type" defaultToClosed onToggle={focusVisPickerInput}>
+      <VisualizationTab panel={panel} ref={visTabInputRef} />
     </OptionsGroup>
   );
 
