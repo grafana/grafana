cadb5e6cb16a01262b14ec5c68d689d8c58f7fca
Torkel Ödegaard <torkel@grafana.com>
2020-04-21 09:44:44 +0200

    Fixed prettier config in toolkit to make it consistent with grafana core prettier config

--- plugins-bundled/internal/input-datasource/src/InputConfigEditor.tsx
+++ plugins-bundled/internal/input-datasource/src/InputConfigEditor.tsx
@@ -56,8 +56,9 @@ export class InputConfigEditor extends PureComponent<Props, State> {
         </div>
 
         <div className="grafana-info-box">
-          This data is stored in the datasource json and is returned to every user in the initial request for any datasource. This is an appropriate
-          place to enter a few values. Large datasets will perform better in other datasources.
+          This data is stored in the datasource json and is returned to every user in the initial request for any
+          datasource. This is an appropriate place to enter a few values. Large datasets will perform better in other
+          datasources.
           <br />
           <br />
           <b>NOTE:</b> Changes to this data will only be reflected after a browser refresh.
