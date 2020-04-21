15f81a8eb7c2762258fc9f289b13ec37d6035509
andrej ocenas <mr.ocenas@gmail.com>
2020-04-21 22:51:07 +0200

    Move link creation to utils and add tests

--- public/app/features/explore/LogsContainer.tsx
+++ public/app/features/explore/LogsContainer.tsx
@@ -6,11 +6,8 @@ import { Collapse } from '@grafana/ui';
 import {
   AbsoluteTimeRange,
   DataSourceApi,
-  DateTimeDuration,
-  ExploreMode,
   Field,
   GraphSeriesXY,
-  locationUtil,
   LogLevel,
   LogRowModel,
   LogsDedupStrategy,
@@ -31,9 +28,7 @@ import { LiveLogsWithTheme } from './LiveLogs';
 import { Logs } from './Logs';
 import { LogsCrossFadeTransition } from './utils/LogsCrossFadeTransition';
 import { LiveTailControls } from './useLiveTailControls';
-import { getLinksFromLogsField } from '../panel/panellinks/linkSuppliers';
-import { serializeStateToUrlParam } from '../../core/utils/explore';
-import { getDataSourceSrv } from '@grafana/runtime';
+import { getFieldLinksForExplore } from './utils/links';
 
 interface LogsContainerProps {
   datasourceInstance?: DataSourceApi;
@@ -94,61 +89,8 @@ export class LogsContainer extends PureComponent<LogsContainerProps> {
     return [];
   };
 
-  /**
-   * Get links from the filed of a dataframe that was given to as and in addition check if there is associated
-   * metadata with datasource in which case we will add onClick to open the link in new split window. This assumes
-   * that we just supply datasource name and field value and Explore split window will know how to render that
-   * appropriately. This is for example used for transition from log with traceId to trace datasource to show that
-   * trace.
-   * @param field
-   * @param rowIndex
-   */
   getFieldLinks = (field: Field, rowIndex: number) => {
-    const data = getLinksFromLogsField(field, rowIndex);
-    return data.map(d => {
-      if (d.link.meta?.datasourceUid) {
-        return {
-          ...d.linkModel,
-          onClick: () => {
-            this.props.splitOpen({
-              dataSourceUid: d.link.meta.datasourceUid,
-              // TODO: fix the ambiguity here
-              // This looks weird but in case meta.datasourceUid is set we save the query in url which will get
-              // interpolated into href
-              query: d.linkModel.href,
-            });
-          },
-          // We need to create real href here as the linkModel.href actually contains query. As in this case this is
-          // meant to be internal link (opens split view by default) the href will also points to explore but this
-          // way you can open it in new tab.
-          href: this.generateInternalHref(d.link.meta.datasourceUid, d.linkModel.href, this.props.range),
-        };
-      }
-      return d.linkModel;
-    });
-  };
-
-  /**
-   * Generates href for internal derived field link.
-   */
-  generateInternalHref = (datasourceUid: string, query: string, range: TimeRange) => {
-    return locationUtil.assureBaseUrl(
-      `/explore?left=${serializeStateToUrlParam({
-        range: range.raw,
-        datasource: getDataSourceSrv().getDataSourceSettingsByUid(datasourceUid).name,
-        // Again hardcoded for Jaeger query structure
-        // TODO: fix
-        queries: [{ query }],
-        // This should get overwritten if datasource does not support that mode and we do not know what mode is
-        // preferred anyway.
-        mode: ExploreMode.Metrics,
-        ui: {
-          showingGraph: true,
-          showingTable: true,
-          showingLogs: true,
-        },
-      })}`
-    );
+    return getFieldLinksForExplore(field, rowIndex, this.props.splitOpen, this.props.range);
   };
 
   render() {
