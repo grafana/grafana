6f2635bcd2e9dcdbd7ce6e3f8a3d066750f9cfcb
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2020-04-22 12:59:06 +0100

    Datasource/Loki: Remove code dealing with legacy Loki endpoints (#23437)

--- public/app/plugins/datasource/loki/result_transformer.test.ts
+++ public/app/plugins/datasource/loki/result_transformer.test.ts
@@ -1,28 +1,7 @@
 import { CircularDataFrame, FieldCache, FieldType, MutableDataFrame } from '@grafana/data';
-import { LokiLegacyStreamResult, LokiStreamResult, LokiTailResponse } from './types';
+import { LokiStreamResult, LokiTailResponse } from './types';
 import * as ResultTransformer from './result_transformer';
 import { enhanceDataFrame } from './result_transformer';
-
-const legacyStreamResult: LokiLegacyStreamResult[] = [
-  {
-    labels: '{foo="bar"}',
-    entries: [
-      {
-        line: "foo: [32m'bar'[39m",
-        ts: '1970-01-01T00:00:00Z',
-      },
-    ],
-  },
-  {
-    labels: '{bar="foo"}',
-    entries: [
-      {
-        line: "bar: 'foo'",
-        ts: '1970-01-01T00:00:00Z',
-      },
-    ],
-  },
-];
 
 const streamResult: LokiStreamResult[] = [
   {
@@ -46,48 +25,6 @@ describe('loki result transformer', () => {
 
   afterEach(() => {
     jest.clearAllMocks();
-  });
-
-  describe('legacyLogStreamToDataFrame', () => {
-    it('converts streams to series', () => {
-      const data = legacyStreamResult.map(stream => ResultTransformer.legacyLogStreamToDataFrame(stream));
-
-      expect(data.length).toBe(2);
-      expect(data[0].fields[1].labels!['foo']).toEqual('bar');
-      expect(data[0].fields[0].values.get(0)).toEqual(legacyStreamResult[0].entries[0].ts);
-      expect(data[0].fields[1].values.get(0)).toEqual(legacyStreamResult[0].entries[0].line);
-      expect(data[0].fields[2].values.get(0)).toEqual('2764544e18dbc3fcbeee21a573e8cd1b');
-      expect(data[1].fields[0].values.get(0)).toEqual(legacyStreamResult[1].entries[0].ts);
-      expect(data[1].fields[1].values.get(0)).toEqual(legacyStreamResult[1].entries[0].line);
-      expect(data[1].fields[2].values.get(0)).toEqual('55b7a68547c4c1c88827f13f3cb680ed');
-    });
-  });
-
-  describe('lokiLegacyStreamsToDataframes', () => {
-    it('should enhance data frames', () => {
-      jest.spyOn(ResultTransformer, 'enhanceDataFrame');
-      const dataFrames = ResultTransformer.lokiLegacyStreamsToDataframes(
-        { streams: legacyStreamResult },
-        { refId: 'A' },
-        500,
-        {
-          derivedFields: [
-            {
-              matcherRegex: 'tracer=(w+)',
-              name: 'test',
-              url: 'example.com',
-            },
-          ],
-        }
-      );
-
-      expect(ResultTransformer.enhanceDataFrame).toBeCalled();
-      dataFrames.forEach(frame => {
-        expect(
-          frame.fields.filter(field => field.name === 'test' && field.type === 'string').length
-        ).toBeGreaterThanOrEqual(1);
-      });
-    });
   });
 
   describe('lokiStreamResultToDataFrame', () => {
@@ -128,22 +65,6 @@ describe('loki result transformer', () => {
   });
 
   describe('appendResponseToBufferedData', () => {
-    it('should append response', () => {
-      const data = new MutableDataFrame();
-      data.addField({ name: 'ts', type: FieldType.time, config: { title: 'Time' } });
-      data.addField({ name: 'line', type: FieldType.string });
-      data.addField({ name: 'labels', type: FieldType.other });
-      data.addField({ name: 'id', type: FieldType.string });
-
-      ResultTransformer.appendLegacyResponseToBufferedData({ streams: legacyStreamResult }, data);
-      expect(data.get(0)).toEqual({
-        ts: '1970-01-01T00:00:00Z',
-        line: "foo: [32m'bar'[39m",
-        labels: { foo: 'bar' },
-        id: '2764544e18dbc3fcbeee21a573e8cd1b',
-      });
-    });
-
     it('should return a dataframe with ts in iso format', () => {
       const tailResponse: LokiTailResponse = {
         streams: [
