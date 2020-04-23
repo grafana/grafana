syunkitada/grafana-7517

Variables: fixes error when setting adhoc variables values (#23580)

public/app/features/templating/variable_srv.ts
@@ -3,7 +3,7 @@ import angular, { auto, ILocationService, IPromise, IQService } from 'angular';
 import _ from 'lodash';
 // Utils & Services
 import coreModule from 'app/core/core_module';
-import { variableTypes } from './types';
+import { VariableActions, variableTypes } from './types';
 import { Graph } from 'app/core/utils/dag';
 import { TemplateSrv } from 'app/features/templating/template_srv';
 import { TimeSrv } from 'app/features/dashboard/services/TimeSrv';
@@ -344,9 +344,9 @@ export class VariableSrv {
     }
   }
 
-  isVariableUrlValueDifferentFromCurrent(variable: any, urlValue: any) {
+  isVariableUrlValueDifferentFromCurrent(variable: VariableActions, urlValue: any) {
     // lodash _.isEqual handles array of value equality checks as well
-    return !_.isEqual(variable.current.value, urlValue);
+    return !_.isEqual(variable.getValueForUrl(), urlValue);
   }
 
   updateUrlParamsWithCurrentVariables() {

grafana/grafana

(cherry picked from commit 0091885b135b18256573d2688b029609c0324b6b)

public/app/features/templating/variable_srv.ts
@@ -3,7 +3,7 @@ import angular, { auto, ILocationService, IPromise, IQService } from 'angular';
 import _ from 'lodash';
 // Utils & Services
 import coreModule from 'app/core/core_module';
-import { variableTypes } from './variable';
+import { VariableActions, variableTypes } from './variable';
 import { Graph } from 'app/core/utils/dag';
 import { TemplateSrv } from 'app/features/templating/template_srv';
 import { TimeSrv } from 'app/features/dashboard/services/TimeSrv';
@@ -344,9 +344,9 @@ export class VariableSrv {
     }
   }
 
-  isVariableUrlValueDifferentFromCurrent(variable: any, urlValue: any) {
+  isVariableUrlValueDifferentFromCurrent(variable: VariableActions, urlValue: any) {
     // lodash _.isEqual handles array of value equality checks as well
-    return !_.isEqual(variable.current.value, urlValue);
+    return !_.isEqual(variable.getValueForUrl(), urlValue);
   }
 
   updateUrlParamsWithCurrentVariables() {