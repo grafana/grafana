7daf4cb312fe87a461e933ff7783f163eb38918b
Peter Holmberg <peter.hlmbrg@gmail.com>
2020-04-24 13:51:24 +0200

    fix gradients

--- public/app/plugins/panel/gettingstarted/components/sharedStyles.ts
+++ public/app/plugins/panel/gettingstarted/components/sharedStyles.ts
@@ -1,10 +1,13 @@
 import { GrafanaTheme } from '@grafana/data';
 import { css } from 'emotion';
+import { stylesFactory } from '@grafana/ui';
 
-export const cardStyle = (theme: GrafanaTheme, complete: boolean) => {
-  const borderGradient = complete
-    ? 'linear-gradient(to right, #5182CC 0%, #245BAF 100%)'
-    : 'linear-gradient(to right, #f05a28 0%, #fbca0a 100%)';
+export const cardStyle = stylesFactory((theme: GrafanaTheme, complete: boolean) => {
+  const completeGradient = 'linear-gradient(to right, #5182CC 0%, #245BAF 100%)';
+  const darkThemeGradients = complete ? completeGradient : 'linear-gradient(to right, #f05a28 0%, #fbca0a 100%)';
+  const lightThemeGradients = complete ? completeGradient : 'linear-gradient(to right, #FBCA0A 0%, #F05A28 100%)';
+
+  const borderGradient = theme.isDark ? darkThemeGradients : lightThemeGradients;
 
   return `
       background-color: ${theme.colors.bg1};
@@ -25,11 +28,13 @@ export const cardStyle = (theme: GrafanaTheme, complete: boolean) => {
         background-image: ${borderGradient};
       }
 `;
-};
+});
 
-export const iconStyle = (theme: GrafanaTheme, complete: boolean) => css`
-  color: ${complete ? theme.palette.blue95 : theme.colors.textWeak};
-`;
+export const iconStyle = stylesFactory(
+  (theme: GrafanaTheme, complete: boolean) => css`
+    color: ${complete ? theme.palette.blue95 : theme.colors.textWeak};
+  `
+);
 
 export const cardContent = css`
   padding: 24px 16px;
