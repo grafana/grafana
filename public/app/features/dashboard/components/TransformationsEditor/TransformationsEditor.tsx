7d0ea45cc9a7553762d6f3ea0edc4771f9838d3f
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-04-24 12:51:38 +0200

    e2e: adds e2e for panel edit (#23849)
    * Explore: Create basic E2E test
    
    * Feature: adds e2e tests for panel inspector
    
    * Refactor: adds ts-ignore because of type checking errors
    
    * Refactor: changes after PR comments and updates snapshot
    
    * Refactor: adds typings back for IScope
    
    * e2e: adds e2e for panel edit
    
    Co-authored-by: Andreas Opferkuch <andreas.opferkuch@gmail.com>

--- public/app/features/dashboard/components/TransformationsEditor/TransformationsEditor.tsx
+++ public/app/features/dashboard/components/TransformationsEditor/TransformationsEditor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, CustomScrollbar, ValuePicker, Button, useTheme, VerticalGroup, stylesFactory } from '@grafana/ui';
+import { Button, Container, CustomScrollbar, stylesFactory, useTheme, ValuePicker, VerticalGroup } from '@grafana/ui';
 import {
   DataFrame,
   DataTransformerConfig,
@@ -11,6 +11,7 @@ import {
 import { TransformationOperationRow } from './TransformationOperationRow';
 import { Card, CardProps } from '../../../../core/components/Card/Card';
 import { css } from 'emotion';
+import { e2e } from '@grafana/e2e';
 
 interface Props {
   onChange: (transformations: DataTransformerConfig[]) => void;
@@ -143,9 +144,11 @@ export class TransformationsEditor extends React.PureComponent<Props> {
     return (
       <CustomScrollbar autoHeightMin="100%">
         <Container padding="md">
-          {!hasTransformationsConfigured && this.renderNoAddedTransformsState()}
-          {hasTransformationsConfigured && this.renderTransformationEditors()}
-          {hasTransformationsConfigured && this.renderTransformationSelector()}
+          <div aria-label={e2e.components.TransformTab.selectors.content}>
+            {!hasTransformationsConfigured && this.renderNoAddedTransformsState()}
+            {hasTransformationsConfigured && this.renderTransformationEditors()}
+            {hasTransformationsConfigured && this.renderTransformationSelector()}
+          </div>
         </Container>
       </CustomScrollbar>
     );
