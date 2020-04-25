f890ff2d90e2ceeca79bd1b94cd08bac420263ac
Dominik Prokop <dominik.prokop@grafana.com>
2020-04-25 20:45:29 +0200

    Add deprecated/alpha badge to panels in viz picker, fix long title display

--- public/app/features/dashboard/panel_editor/VizTypePickerPlugin.tsx
+++ public/app/features/dashboard/panel_editor/VizTypePickerPlugin.tsx
@@ -3,6 +3,7 @@ import { GrafanaTheme, PanelPluginMeta } from '@grafana/data';
 import { stylesFactory, useTheme } from '@grafana/ui';
 import { css, cx } from 'emotion';
 import { e2e } from '@grafana/e2e';
+import { PanelPluginBadge } from '../../plugins/PluginSignatureBadge';
 
 interface Props {
   isCurrent: boolean;
@@ -21,20 +22,30 @@ const VizTypePickerPlugin: React.FC<Props> = ({ isCurrent, plugin, onClick, disa
   });
 
   return (
-    <div
-      className={cssClass}
-      onClick={disabled ? () => {} : onClick}
-      title={plugin.name}
-      aria-label={e2e.components.PluginVisualization.selectors.item(plugin.name)}
-    >
-      <div className={styles.name}>{plugin.name}</div>
-      <img className={styles.img} src={plugin.info.logos.small} />
+    <div className={styles.wrapper}>
+      <div
+        className={cssClass}
+        onClick={disabled ? () => {} : onClick}
+        title={plugin.name}
+        aria-label={e2e.components.PluginVisualization.selectors.item(plugin.name)}
+      >
+        <div className={styles.name} title={plugin.name}>
+          {plugin.name}
+        </div>
+        <img className={styles.img} src={plugin.info.logos.small} />
+      </div>
+      <div className={styles.badge}>
+        <PanelPluginBadge plugin={plugin} />
+      </div>
     </div>
   );
 };
 
 const getStyles = stylesFactory((theme: GrafanaTheme) => {
   return {
+    wrapper: css`
+      position: relative;
+    `,
     item: css`
       background: ${theme.colors.bg2};
       border: 1px solid ${theme.colors.border2};
@@ -76,14 +87,19 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
       overflow: hidden;
       white-space: nowrap;
       font-size: ${theme.typography.size.sm};
-      display: flex;
-      flex-direction: column;
-      align-self: center;
+      text-align: center;
       height: 23px;
       font-weight: ${theme.typography.weight.semibold};
+      padding: 0 10px;
+      width: 100%;
     `,
     img: css`
       height: 55px;
+    `,
+    badge: css`
+      position: absolute;
+      bottom: ${theme.spacing.xs};
+      right: ${theme.spacing.xs};
     `,
   };
 });
