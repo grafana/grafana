d45e0842c58ded1d4123db50dd08fac5f0b11645
Torkel Ödegaard <torkel@grafana.com>
2020-04-26 20:17:26 +0200

    fixed e2e

--- e2e/suite1/specs/inspect-drawer.spec.ts
+++ e2e/suite1/specs/inspect-drawer.spec.ts
@@ -27,7 +27,9 @@ e2e.scenario({
 
     e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Edit, PANEL_UNDER_TEST);
 
-    e2e.components.QueryTab.queryInspectorButton.should('be.visible').click();
+    e2e.components.QueryTab.queryInspectorButton()
+      .should('be.visible')
+      .click();
 
     e2e.components.Drawer.General.title(PANEL_UNDER_TEST)
       .should('be.visible')
