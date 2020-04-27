9771b1c74529ee5fc197ea7ff8e72d564c569a32
Alex Khomenko <Clarity-89@users.noreply.github.com>
2020-04-27 15:08:48 +0300

    Search/fix folder sort (#23893)
    * Search: Move layout to query reducer/hook
    
    * Search: Refactor search_srv
    
    * Search: Fix types
    
    * Search: Move extra layout/sort logic to reducer
    
    * Search: Fix Select min-width
    
    * Search: Fix filter by starred
    
    * Search: Update tests
    
    * Search: Simplify query return
    
    * Search: Set width to auto on HorizontalGroup
    
    * Search: Fix tests

--- public/app/features/search/components/ActionRow.tsx
+++ public/app/features/search/components/ActionRow.tsx
@@ -1,21 +1,24 @@
-import React, { Dispatch, FC, SetStateAction } from 'react';
+import React, { Dispatch, FC, FormEvent, SetStateAction } from 'react';
 import { css } from 'emotion';
 import { HorizontalGroup, RadioButtonGroup, stylesFactory, useTheme, Checkbox } from '@grafana/ui';
 import { GrafanaTheme, SelectableValue } from '@grafana/data';
 import { SortPicker } from 'app/core/components/Select/SortPicker';
 import { TagFilter } from 'app/core/components/TagFilter/TagFilter';
 import { SearchSrv } from 'app/core/services/search_srv';
-import { layoutOptions } from '../hooks/useSearchLayout';
-import { DashboardQuery } from '../types';
+import { DashboardQuery, SearchLayout } from '../types';
+
+export const layoutOptions = [
+  { label: 'Folders', value: SearchLayout.Folders, icon: 'folder' },
+  { label: 'List', value: SearchLayout.List, icon: 'list-ul' },
+];
 
 const searchSrv = new SearchSrv();
 
 type onSelectChange = (value: SelectableValue) => void;
 interface Props {
-  layout: string;
   onLayoutChange: Dispatch<SetStateAction<string>>;
   onSortChange: onSelectChange;
-  onStarredFilterChange?: onSelectChange;
+  onStarredFilterChange?: (event: FormEvent<HTMLInputElement>) => void;
   onTagFilterChange: onSelectChange;
   query: DashboardQuery;
   showStarredFilter?: boolean;
@@ -23,7 +26,6 @@ interface Props {
 }
 
 export const ActionRow: FC<Props> = ({
-  layout,
   onLayoutChange,
   onSortChange,
   onStarredFilterChange = () => {},
@@ -37,8 +39,10 @@ export const ActionRow: FC<Props> = ({
 
   return (
     <div className={styles.actionRow}>
-      <HorizontalGroup spacing="md">
-        {!hideLayout ? <RadioButtonGroup options={layoutOptions} onChange={onLayoutChange} value={layout} /> : null}
+      <HorizontalGroup spacing="md" width="auto">
+        {!hideLayout ? (
+          <RadioButtonGroup options={layoutOptions} onChange={onLayoutChange} value={query.layout} />
+        ) : null}
         <SortPicker onChange={onSortChange} value={query.sort} />
       </HorizontalGroup>
       <HorizontalGroup spacing="md" width="auto">
