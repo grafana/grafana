053e6545649852f0f9d4296f3b3769ee5abde9a0
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-04-28 18:26:46 +0200

    Start Forms migration

--- public/app/core/components/Login/ChangePassword.tsx
+++ public/app/core/components/Login/ChangePassword.tsx
@@ -1,8 +1,5 @@
-import React, { ChangeEvent, PureComponent, SyntheticEvent } from 'react';
-import { Tooltip } from '@grafana/ui';
-import { AppEvents } from '@grafana/data';
-
-import appEvents from 'app/core/app_events';
+import React, { FC } from 'react';
+import { Tooltip, Form, Field, Input, HorizontalGroup, Button } from '@grafana/ui';
 import { selectors } from '@grafana/e2e-selectors';
 
 interface Props {
@@ -11,128 +8,173 @@ interface Props {
   focus?: boolean;
 }
 
-interface State {
+interface PasswordDTO {
   newPassword: string;
   confirmNew: string;
-  valid: boolean;
 }
 
-export class ChangePassword extends PureComponent<Props, State> {
-  private userInput: HTMLInputElement;
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      newPassword: '',
-      confirmNew: '',
-      valid: false,
-    };
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (!prevProps.focus && this.props.focus) {
-      this.focus();
-    }
-  }
-
-  focus() {
-    this.userInput.focus();
-  }
-
-  onSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
-
-    const { newPassword, valid } = this.state;
-    if (valid) {
-      this.props.onSubmit(newPassword);
-    } else {
-      appEvents.emit(AppEvents.alertWarning, ['New passwords do not match']);
-    }
+export const ChangePassword: FC<Props> = ({ onSubmit, onSkip }) => {
+  const submit = (passwords: PasswordDTO) => {
+    onSubmit(passwords.newPassword);
   };
-
-  onNewPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      newPassword: e.target.value,
-      valid: this.validate('newPassword', e.target.value),
-    });
-  };
-
-  onConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      confirmNew: e.target.value,
-      valid: this.validate('confirmNew', e.target.value),
-    });
-  };
-
-  onSkip = (e: SyntheticEvent) => {
-    this.props.onSkip();
-  };
-
-  validate(changed: string, pw: string) {
-    if (changed === 'newPassword') {
-      return this.state.confirmNew === pw;
-    } else if (changed === 'confirmNew') {
-      return this.state.newPassword === pw;
-    }
-    return false;
-  }
-
-  render() {
-    return (
-      <div className="login-inner-box" id="change-password-view">
-        <div className="text-left login-change-password-info">
-          <h5>Change Password</h5>
-          Before you can get started with awesome dashboards we need you to make your account more secure by changing
-          your password.
-          <br />
-          You can change your password again later.
-        </div>
-        <form className="login-form-group gf-form-group">
-          <div className="login-form">
-            <input
+  return (
+    <Form onSubmit={submit}>
+      {({ errors, register, getValues }) => (
+        <>
+          <Field label="New password">
+            <Input
               type="password"
-              id="newPassword"
               name="newPassword"
-              className="gf-form-input login-form-input"
-              required
-              placeholder="New password"
-              onChange={this.onNewPasswordChange}
-              ref={input => {
-                this.userInput = input;
-              }}
+              invalid={!!errors.newPassword}
+              ref={register({
+                required: true,
+                validate: v => v === getValues().confirmNew || 'Passwords must match!',
+              })}
             />
-          </div>
-          <div className="login-form">
-            <input
+          </Field>
+          <Field label="Confirmn new password" invalid={!!errors.confirmNew}>
+            <Input
               type="password"
               name="confirmNew"
-              className="gf-form-input login-form-input"
-              required
-              ng-model="command.confirmNew"
-              placeholder="Confirm new password"
-              onChange={this.onConfirmPasswordChange}
+              ref={register({
+                required: 'New password is required',
+                validate: v => v === getValues().newPassword || 'Passwords must match!',
+              })}
             />
-          </div>
-          <div className="login-button-group login-button-group--right text-right">
+          </Field>
+          <HorizontalGroup>
             <Tooltip
-              placement="bottom"
               content="If you skip you will be prompted to change password next time you login."
+              placement="bottom"
             >
-              <a className="btn btn-link" onClick={this.onSkip} aria-label={selectors.pages.Login.skip}>
+              <Button variant="link" aria-label={selectors.pages.Login.skip}>
                 Skip
-              </a>
+              </Button>
             </Tooltip>
+            <Button type="submit">Submit</Button>
+          </HorizontalGroup>
+        </>
+      )}
+    </Form>
+  );
+};
 
-            <button
-              type="submit"
-              className={`btn btn-large p-x-2 ${this.state.valid ? 'btn-primary' : 'btn-inverse'}`}
-              onClick={this.onSubmit}
-              disabled={!this.state.valid}
-            >
-              Save
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+// export class ChangePassword extends PureComponent<Props, State> {
+//   private userInput: HTMLInputElement;
+//   constructor(props: Props) {
+//     super(props);
+//     this.state = {
+//       newPassword: '',
+//       confirmNew: '',
+//       valid: false,
+//     };
+//   }
+
+//   componentDidUpdate(prevProps: Props) {
+//     if (!prevProps.focus && this.props.focus) {
+//       this.focus();
+//     }
+//   }
+
+//   focus() {
+//     this.userInput.focus();
+//   }
+
+//   onSubmit = (e: SyntheticEvent) => {
+//     e.preventDefault();
+
+//     const { newPassword, valid } = this.state;
+//     if (valid) {
+//       this.props.onSubmit(newPassword);
+//     } else {
+//       appEvents.emit(AppEvents.alertWarning, ['New passwords do not match']);
+//     }
+//   };
+
+//   onNewPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     this.setState({
+//       newPassword: e.target.value,
+//       valid: this.validate('newPassword', e.target.value),
+//     });
+//   };
+
+//   onConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     this.setState({
+//       confirmNew: e.target.value,
+//       valid: this.validate('confirmNew', e.target.value),
+//     });
+//   };
+
+//   onSkip = (e: SyntheticEvent) => {
+//     this.props.onSkip();
+//   };
+
+//   validate(changed: string, pw: string) {
+//     if (changed === 'newPassword') {
+//       return this.state.confirmNew === pw;
+//     } else if (changed === 'confirmNew') {
+//       return this.state.newPassword === pw;
+//     }
+//     return false;
+//   }
+
+//   render() {
+//     return (
+//       <div className="login-inner-box" id="change-password-view">
+//         <div className="text-left login-change-password-info">
+//           <h5>Change Password</h5>
+//           Before you can get started with awesome dashboards we need you to make your account more secure by changing
+//           your password.
+//           <br />
+//           You can change your password again later.
+//         </div>
+//         <form className="login-form-group gf-form-group">
+//           <div className="login-form">
+//             <input
+//               type="password"
+//               id="newPassword"
+//               name="newPassword"
+//               className="gf-form-input login-form-input"
+//               required
+//               placeholder="New password"
+//               onChange={this.onNewPasswordChange}
+//               ref={input => {
+//                 this.userInput = input;
+//               }}
+//             />
+//           </div>
+//           <div className="login-form">
+//             <input
+//               type="password"
+//               name="confirmNew"
+//               className="gf-form-input login-form-input"
+//               required
+//               ng-model="command.confirmNew"
+//               placeholder="Confirm new password"
+//               onChange={this.onConfirmPasswordChange}
+//             />
+//           </div>
+//           <div className="login-button-group login-button-group--right text-right">
+//             <Tooltip
+//               placement="bottom"
+//               content="If you skip you will be prompted to change password next time you login."
+//             >
+//               <a className="btn btn-link" onClick={this.onSkip} aria-label={selectors.pages.Login.skip}>
+//                 Skip
+//               </a>
+//             </Tooltip>
+
+//             <button
+//               type="submit"
+//               className={`btn btn-large p-x-2 ${this.state.valid ? 'btn-primary' : 'btn-inverse'}`}
+//               onClick={this.onSubmit}
+//               disabled={!this.state.valid}
+//             >
+//               Save
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
--- public/app/core/components/Login/LoginForm.tsx
+++ public/app/core/components/Login/LoginForm.tsx
@@ -1,12 +1,12 @@
-import React, { ChangeEvent, PureComponent, SyntheticEvent } from 'react';
+import React, { ChangeEvent, PureComponent, SyntheticEvent, FC, useState } from 'react';
 import { selectors } from '@grafana/e2e-selectors';
 
 import { FormModel } from './LoginCtrl';
-import { Button } from '@grafana/ui';
+import { Button, Form, Input, Field } from '@grafana/ui';
+import { css } from 'emotion';
 
 interface Props {
   displayForgotPassword: boolean;
-  onChange?: (valid: boolean) => void;
   onSubmit: (data: FormModel) => void;
   isLoggingIn: boolean;
   passwordHint: string;
@@ -20,7 +20,64 @@ interface State {
   valid: boolean;
 }
 
-export class LoginForm extends PureComponent<Props, State> {
+const forgottenPasswordStyles = css`
+  display: inline-block;
+  margin-top: 20px;
+`;
+
+const wrapperStyles = css`
+  width: 100%;
+  padding-bottom: 15px;
+`;
+
+export const LoginForm: FC<Props> = ({ displayForgotPassword, onSubmit, isLoggingIn, passwordHint, loginHint }) => {
+  const [hasBeenTouched, setHasBeenTouched] = useState(false);
+  return (
+    <div className={wrapperStyles}>
+      <Form onSubmit={onSubmit} validateOn="onChange">
+        {({ register, errors }) => (
+          <>
+            <Field label="Email or username" invalid={!!errors.user} error={errors.user?.message}>
+              <Input
+                onChange={() => setHasBeenTouched(true)}
+                autoFocus
+                name="user"
+                ref={register({ required: 'Email or username is required' })}
+                placeholder={loginHint}
+                aria-label={selectors.pages.Login.username}
+              />
+            </Field>
+            <Field label="Password" invalid={!!errors.password} error={errors.password?.message}>
+              <Input
+                onChange={() => setHasBeenTouched(true)}
+                name="password"
+                type="password"
+                placeholder={passwordHint}
+                ref={register({ required: 'Password is requireed' })}
+                aria-label={selectors.pages.Login.password}
+              />
+            </Field>
+            <Button
+              className={css`
+                justify-content: center;
+                width: 100%;
+              `}
+              disabled={!hasBeenTouched || !!(errors.password || errors.user) || isLoggingIn}
+            >
+              {isLoggingIn ? 'Logging in...' : 'Log in'}
+            </Button>
+            {displayForgotPassword && (
+              <a className={forgottenPasswordStyles} href="user/password/send-reset-email">
+                Forgot your password?
+              </a>
+            )}
+          </>
+        )}
+      </Form>
+    </div>
+  );
+};
+export class OldLoginForm extends PureComponent<Props, State> {
   private userInput: HTMLInputElement;
   constructor(props: Props) {
     super(props);
@@ -62,63 +119,63 @@ export class LoginForm extends PureComponent<Props, State> {
     return user.length > 0 && password.length > 0;
   }
 
-  render() {
-    return (
-      <form name="loginForm" className="login-form-group gf-form-group">
-        <div className="login-form">
-          <input
-            ref={input => {
-              this.userInput = input;
-            }}
-            type="text"
-            name="user"
-            className="gf-form-input login-form-input"
-            required
-            placeholder={this.props.loginHint}
-            aria-label={selectors.pages.Login.username}
-            onChange={this.onChangeUsername}
-          />
-        </div>
-        <div className="login-form">
-          <input
-            type="password"
-            name="password"
-            className="gf-form-input login-form-input"
-            required
-            ng-model="formModel.password"
-            id="inputPassword"
-            placeholder={this.props.passwordHint}
-            aria-label={selectors.pages.Login.password}
-            onChange={this.onChangePassword}
-          />
-        </div>
-        <div className="login-button-group">
-          {!this.props.isLoggingIn ? (
-            <Button
-              size="lg"
-              variant="primary"
-              type="submit"
-              aria-label={selectors.pages.Login.submit}
-              onClick={this.onSubmit}
-              disabled={!this.state.valid}
-            >
-              Log in
-            </Button>
-          ) : (
-            <button type="submit" disabled className="btn btn-large p-x-2 btn-inverse btn-loading">
-              Logging In<span>.</span>
-              <span>.</span>
-              <span>.</span>
-            </button>
-          )}
+  // render() {
+  //   return (
+  //     <form name="loginForm" className="login-form-group gf-form-group">
+  //       <div className="login-form">
+  //         <input
+  //           ref={input => {
+  //             this.userInput = input;
+  //           }}
+  //           type="text"
+  //           name="user"
+  //           className="gf-form-input login-form-input"
+  //           required
+  //           placeholder={this.props.loginHint}
+  //           aria-label={selectors.pages.Login.username}
+  //           onChange={this.onChangeUsername}
+  //         />
+  //       </div>
+  //       <div className="login-form">
+  //         <input
+  //           type="password"
+  //           name="password"
+  //           className="gf-form-input login-form-input"
+  //           required
+  //           ng-model="formModel.password"
+  //           id="inputPassword"
+  //           placeholder={this.props.passwordHint}
+  //           aria-label={selectors.pages.Login.password}
+  //           onChange={this.onChangePassword}
+  //         />
+  //       </div>
+  //       <div className="login-button-group">
+  //         {!this.props.isLoggingIn ? (
+  //           <Button
+  //             size="lg"
+  //             variant="primary"
+  //             type="submit"
+  //             aria-label={selectors.pages.Login.submit}
+  //             onClick={this.onSubmit}
+  //             disabled={!this.state.valid}
+  //           >
+  //             Log in
+  //           </Button>
+  //         ) : (
+  //           <button type="submit" disabled className="btn btn-large p-x-2 btn-inverse btn-loading">
+  //             Logging In<span>.</span>
+  //             <span>.</span>
+  //             <span>.</span>
+  //           </button>
+  //         )}
 
-          {this.props.displayForgotPassword ? (
-            <div className="small login-button-forgot-password">
-              <a href="user/password/send-reset-email">Forgot your password?</a>
-            </div>
-          ) : null}
-        </div>
-      </form>
-    );
-  }
+  //         {this.props.displayForgotPassword ? (
+  //           <div className="small login-button-forgot-password">
+  //             <a href="user/password/send-reset-email">Forgot your password?</a>
+  //           </div>
+  //         ) : null}
+  //       </div>
+  //     </form>
+  //   );
+  // }
 }
