aaede4311f2ad54acd1783f3a7548c78a1518d78
Clarity-89 <homes89@ukr.net>
2020-04-30 13:56:05 +0300

    Storybook: Add mdx for Modal

--- packages/grafana-ui/src/components/Modal/Modal.story.tsx
+++ packages/grafana-ui/src/components/Modal/Modal.story.tsx
@@ -7,6 +7,7 @@ import { Modal } from './Modal';
 import { Icon } from '../Icon/Icon';
 import { ModalTabsHeader } from './ModalTabsHeader';
 import { TabContent } from '../Tabs/TabContent';
+import mdx from './Modal.mdx';
 
 const getKnobs = () => {
   return {
@@ -29,7 +30,12 @@ amet.`
 export default {
   title: 'Overlays/Modal',
   component: Modal,
-  dectorators: [withCenteredStory],
+  decorators: [withCenteredStory],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
 export const basic = () => {
