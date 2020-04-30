4bd028d689f6638f5300913132683b4b2aa2d389
Josh Soref <jsoref@users.noreply.github.com>
2020-04-30 01:03:26 -0400

    spelling: committing

--- scripts/circle-release-next-packages.sh
+++ scripts/circle-release-next-packages.sh
@@ -11,7 +11,7 @@ function parse_git_hash() {
 }
 
 function prepare_version_commit () {
-  echo $'\nCommiting version changes. This commit will not be checked-in!'
+  echo $'\nCommitting version changes. This commit will not be checked-in!'
   git config --global user.email "circleci@grafana.com"
   git config --global user.name "CirceCI"
   git commit -am "Version commit"
