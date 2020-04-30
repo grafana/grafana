d04ccb94d8a6b77e698e6c2b00b60a1fb61b0b07
Jessica Müller <jessica.muller@grafana.com>
2020-04-30 12:40:33 +0200

    Removed unused import from ButtonSelect story

--- packages/grafana-ui/src/components/Forms/Legacy/Select/ButtonSelect.story.internal.tsx
+++ packages/grafana-ui/src/components/Forms/Legacy/Select/ButtonSelect.story.internal.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, object, text } from '@storybook/addon-knobs';
 import { withCenteredStory } from '../../../../utils/storybook/withCenteredStory';
