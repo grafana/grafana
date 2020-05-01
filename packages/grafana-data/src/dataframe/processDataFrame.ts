297a4b0d028395d8e5aa3bacf8bc745c9343cc68
Torkel Ödegaard <torkel@grafana.com>
2020-05-01 12:38:27 +0200

    revert revert going in circles

--- packages/grafana-data/src/dataframe/processDataFrame.ts
+++ packages/grafana-data/src/dataframe/processDataFrame.ts
@@ -75,7 +75,7 @@ function convertTimeSeriesToDataFrame(timeSeries: TimeSeries): DataFrame {
       values: new ArrayVector<number>(times),
     },
     {
-      name: timeSeries.target || TIME_SERIES_FIELD_NAME,
+      name: TIME_SERIES_FIELD_NAME,
       type: FieldType.number,
       config: {
         unit: timeSeries.unit,
@@ -90,6 +90,7 @@ function convertTimeSeriesToDataFrame(timeSeries: TimeSeries): DataFrame {
   }
 
   return {
+    name: timeSeries.target,
     refId: timeSeries.refId,
     meta: timeSeries.meta,
     fields,
