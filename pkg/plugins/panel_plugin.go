0aaf47891d1641e6a5cc0279efc0f310edd830e3
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-05-04 19:53:01 +0200

    POC: Do we want to allow backend component for panel plugins as well?

--- pkg/plugins/panel_plugin.go
+++ pkg/plugins/panel_plugin.go
@@ -2,13 +2,16 @@ package plugins
 
 import (
 	"encoding/json"
+	"path"
 
 	"github.com/grafana/grafana/pkg/plugins/backendplugin"
+	"github.com/grafana/grafana/pkg/util/errutil"
 )
 
 type PanelPlugin struct {
 	FrontendPluginBase
-	SkipDataQuery bool `json:"skipDataQuery"`
+	SkipDataQuery bool   `json:"skipDataQuery"`
+	Executable    string `json:"executable,omitempty"`
 }
 
 func (p *PanelPlugin) Load(decoder *json.Decoder, pluginDir string, backendPluginManager backendplugin.Manager) error {
@@ -18,6 +21,15 @@ func (p *PanelPlugin) Load(decoder *json.Decoder, pluginDir string, backendPlugi
 
 	if err := p.registerPlugin(pluginDir); err != nil {
 		return err
+	}
+
+	if p.Backend {
+		cmd := ComposePluginStartCommmand(p.Executable)
+		fullpath := path.Join(p.PluginDir, cmd)
+		descriptor := backendplugin.NewBackendPluginDescriptor(p.Id, fullpath, backendplugin.PluginStartFuncs{})
+		if err := backendPluginManager.Register(descriptor); err != nil {
+			return errutil.Wrapf(err, "Failed to register backend plugin")
+		}
 	}
 
 	Panels[p.Id] = p
