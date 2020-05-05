aa1d49ea482003937c84e7132bbae262a2f4e9c4
Torkel Ödegaard <torkel@grafana.com>
2020-05-05 14:36:23 +0200

    Fixed the tricky problem of multiple time field when not used in join

--- packages/grafana-data/src/field/fieldState.ts
+++ packages/grafana-data/src/field/fieldState.ts
@@ -45,62 +45,77 @@ function calculateFieldState(field: Field, frame?: DataFrame, allFrames?: DataFr
 
   let title = hasConfigTitle ? field.config!.title! : field.name;
 
-  if (!hasConfigTitle) {
-    let parts: string[] = [];
-    let frameNamesDiffer = false;
+  if (hasConfigTitle) {
+    return {
+      ...field.state,
+      title,
+    };
+  }
 
-    if (allFrames && allFrames.length > 1) {
-      for (let i = 1; i < allFrames.length; i++) {
-        const frame = allFrames[i];
-        if (frame.name !== allFrames[i - 1].name) {
-          frameNamesDiffer = true;
-          break;
-        }
+  // This is an ugly exception for time field
+  // For time series we should normally treat time field with same name
+  // But in case it has a join source we should handle it as normal field
+  if (field.type === FieldType.time && !field.labels) {
+    return {
+      ...field.state,
+      title: title ?? 'Time',
+    };
+  }
+
+  let parts: string[] = [];
+  let frameNamesDiffer = false;
+
+  if (allFrames && allFrames.length > 1) {
+    for (let i = 1; i < allFrames.length; i++) {
+      const frame = allFrames[i];
+      if (frame.name !== allFrames[i - 1].name) {
+        frameNamesDiffer = true;
+        break;
       }
     }
+  }
 
-    let frameNameAdded = false;
-    let labelsAdded = false;
+  let frameNameAdded = false;
+  let labelsAdded = false;
 
-    if (frameNamesDiffer && frame?.name) {
+  if (frameNamesDiffer && frame?.name) {
+    parts.push(frame.name);
+    frameNameAdded = true;
+  }
+
+  if (field.name && field.name !== TIME_SERIES_FIELD_NAME) {
+    parts.push(field.name);
+  }
+
+  if (field.labels && frame) {
+    let singleLabelName = getSingleLabelName(allFrames ?? [frame]);
+
+    if (!singleLabelName) {
+      let allLabels = formatLabels(field.labels);
+      if (allLabels) {
+        parts.push(allLabels);
+        labelsAdded = true;
+      }
+    } else if (field.labels[singleLabelName]) {
+      parts.push(field.labels[singleLabelName]);
+      labelsAdded = true;
+    }
+  }
+
+  // if we have not added frame name and no labels, and field name = Value, we should add frame name
+  if (frame && !frameNameAdded && !labelsAdded && field.name === TIME_SERIES_FIELD_NAME) {
+    if (frame.name && frame.name.length > 0) {
       parts.push(frame.name);
       frameNameAdded = true;
     }
+  }
 
-    if (field.name && field.name !== TIME_SERIES_FIELD_NAME) {
-      parts.push(field.name);
-    }
-
-    if (field.labels && frame) {
-      let singleLabelName = getSingleLabelName(allFrames ?? [frame]);
-
-      if (!singleLabelName) {
-        let allLabels = formatLabels(field.labels);
-        if (allLabels) {
-          parts.push(allLabels);
-          labelsAdded = true;
-        }
-      } else if (field.labels[singleLabelName]) {
-        parts.push(field.labels[singleLabelName]);
-        labelsAdded = true;
-      }
-    }
-
-    // if we have not added frame name and no labels, and field name = Value, we should add frame name
-    if (frame && !frameNameAdded && !labelsAdded && field.name === TIME_SERIES_FIELD_NAME) {
-      if (frame.name && frame.name.length > 0) {
-        parts.push(frame.name);
-        frameNameAdded = true;
-      }
-    }
-
-    if (parts.length) {
-      title = parts.join(' ');
-    } else if (field.name) {
-      title = field.name;
-    } else {
-      title = TIME_SERIES_FIELD_NAME;
-    }
+  if (parts.length) {
+    title = parts.join(' ');
+  } else if (field.name) {
+    title = field.name;
+  } else {
+    title = TIME_SERIES_FIELD_NAME;
   }
 
   return {
