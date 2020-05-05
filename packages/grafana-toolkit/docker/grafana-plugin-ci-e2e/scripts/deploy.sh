41a9d597ee36295ed3d9d5c2dddeacb9c43c24f9
Stephanie Closson <srclosson@gmail.com>
2020-05-05 13:57:08 -0600

    Toolkit: Docker images/Add grabpl tool for deploys (#24309)
    * Add grabpl tool for deploys
    
    * Rest of the files

--- packages/grafana-toolkit/docker/grafana-plugin-ci-e2e/scripts/deploy.sh
+++ packages/grafana-toolkit/docker/grafana-plugin-ci-e2e/scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eo pipefail
+
 source "/etc/profile"
 source "./deploy-slim.sh"
 source "./deploy-common.sh"
@@ -11,7 +13,6 @@ wget -O - "https://nodejs.org/dist/v12.16.2/node-${NODEVER}.tar.xz" | tar Jvxf -
 pushd /tmp/node-${NODEVER}
 /bin/rm -f CHANGELOG.md README.md LICENSE
 /bin/cp -r * /usr/local
-/bin/cp -r .??* /usr/local
 popd
 /bin/rm -rf /tmp/node-${NODEVER}
 
@@ -32,16 +33,19 @@ get_file "https://github.com/golangci/golangci-lint/releases/download/v1.23.7/$f
     "/tmp/$filename" \
     "34df1794a2ea8e168b3c98eed3cc0f3e13ed4cba735e4e40ef141df5c41bc086"
 untar_file "/tmp/$filename"
-chmod 755 /usr/local/bin/golangci-lint
 ln -s /usr/local/golangci-lint-1.23.7-linux-amd64/golangci-lint /usr/local/bin/golangci-lint
 ln -s /usr/local/go/bin/go /usr/local/bin/go
 ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
+chmod 755 /usr/local/bin/golangci-lint
 
 # Install code climate
 get_file "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64" \
     "/usr/local/bin/cc-test-reporter" \
-    "38f2442892027f61a07f52c845818750261b2ba58bffb043a582495339d37c05"
+    "b4138199aa755ebfe171b57cc46910b13258ace5fbc4eaa099c42607cd0bff32"
 chmod 755 /usr/local/bin/cc-test-reporter
+
+wget -O /usr/local/bin/grabpl "https://grafana-downloads.storage.googleapis.com/grafana-build-pipeline/v0.4.4/grabpl"
+chmod +x /usr/local/bin/grabpl
 
 # Install Mage
 mkdir -pv /tmp/mage $HOME/go/bin
