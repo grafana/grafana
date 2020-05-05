b258af7ab04cec175dd3c3946d26e49945a703cc
Peter Holmberg <peter.hlmbrg@gmail.com>
2020-05-05 15:05:11 +0200

    use login background

--- public/app/plugins/panel/welcome/Welcome.tsx
+++ public/app/plugins/panel/welcome/Welcome.tsx
@@ -15,16 +15,18 @@ export const WelcomeBanner = () => {
 
   return (
     <div className={styles.container}>
-      <h1>Welcome to Grafana</h1>
+      <h1 className={styles.title}>Welcome to Grafana</h1>
       <div className={styles.help}>
-        <h3>Need help?</h3>
-        <ButtonSelect
-          defaultValue={helpOptions[0]}
-          variant="secondary"
-          className={styles.smallScreenHelp}
-          onChange={onHelpLinkClick}
-          options={helpOptions}
-        />
+        <h3 className={styles.helpText}>Need help?</h3>
+        <div className={styles.smallScreenHelp}>
+          <ButtonSelect
+            defaultValue={helpOptions[0]}
+            variant="secondary"
+            size="sm"
+            onChange={onHelpLinkClick}
+            options={helpOptions}
+          />
+        </div>
         <div className={styles.helpLinks}>
           {helpOptions.map((option, index) => {
             return (
@@ -44,11 +46,12 @@ const onHelpLinkClick = (option: { label: string; href: string }) => {
 };
 
 const getStyles = stylesFactory((theme: GrafanaTheme) => {
-  const backgrundImage = theme.isDark ? 'public/img/onboarding_art_dark.svg' : 'public/img/onboarding_art_light.svg';
+  const backgroundImage = theme.isDark ? 'public/img/login_background_dark.svg' : 'public/img/onboarding_art_light.svg';
+
   return {
     container: css`
       display: flex;
-      background: url(${backgrundImage}) no-repeat;
+      background: url(${backgroundImage}) no-repeat;
       background-size: cover;
       height: 100%;
       align-items: center;
@@ -60,12 +63,33 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
       }
       @media only screen and (max-width: ${theme.breakpoints.sm}) {
         flex-direction: column;
-        padding: 8px;
+        justify-content: flex-start;
+        align-items: flex-start;
+        padding: ${theme.spacing.md} ${theme.spacing.md} ${theme.spacing.md} 48px;
+      }
+    `,
+    title: css`
+      @media only screen and (max-width: ${theme.breakpoints.md}) {
+        font-size: ${theme.typography.heading.h2};
+      }
+      @media only screen and (max-width: ${theme.breakpoints.sm}) {
+        font-size: ${theme.typography.heading.h3};
       }
     `,
     help: css`
       display: flex;
       align-items: baseline;
+    `,
+    helpText: css`
+      margin-right: ${theme.spacing.md};
+
+      @media only screen and (max-width: ${theme.breakpoints.md}) {
+        font-size: ${theme.typography.heading.h4};
+      }
+
+      @media only screen and (max-width: ${theme.breakpoints.sm}) {
+        display: none;
+      }
     `,
     helpLinks: css`
       margin-left: 24px;
