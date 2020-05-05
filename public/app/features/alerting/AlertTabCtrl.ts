8598d31158b08f2ab534c9eb85131415d43eccec
Josh Soref <jsoref@users.noreply.github.com>
2020-05-05 07:13:15 -0400

    spelling: referred

--- public/app/features/alerting/AlertTabCtrl.ts
+++ public/app/features/alerting/AlertTabCtrl.ts
@@ -166,7 +166,7 @@ export class AlertTabCtrl {
   }
 
   removeNotification(an: any) {
-    // remove notifiers refeered to by id and uid to support notifiers added
+    // remove notifiers referred to by id and uid to support notifiers added
     // before and after we added support for uid
     _.remove(this.alert.notifications, (n: any) => n.uid === an.uid);
     _.remove(this.alertNotifications, (n: any) => n.uid === an.uid);
