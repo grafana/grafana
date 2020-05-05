e2a0255e82d8351e6c4de3c022699f53facd0f20
Shavonn Brown <shavonn.brown@grafana.com>
2020-05-05 15:48:18 -0400

    reactify user sessions

--- public/app/features/profile/ReactProfileWrapper.tsx
+++ public/app/features/profile/ReactProfileWrapper.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { UserProvider } from 'app/core/utils/UserProvider';
+import { UserProvider, UserAPI, LoadingStates } from 'app/core/utils/UserProvider';
 import { UserProfileEditForm } from './UserProfileEditForm';
 import { SharedPreferences } from 'app/core/components/SharedPreferences/SharedPreferences';
 import { UserTeams } from './UserTeams';
+import { UserSessions } from './UserSessions';
 import { UserOrganizations } from './UserOrganizations';
+import { User, Team, UserOrg, UserSession } from 'app/types';
 import { config } from '@grafana/runtime';
 import { LoadingPlaceholder } from '@grafana/ui';
 
 export const ReactProfileWrapper = () => (
   <UserProvider userId={config.bootData.user.id}>
-    {(api, states, teams, orgs, user) => {
+    {(api: UserAPI, states: LoadingStates, teams: Team[], orgs: UserOrg[], sessions: UserSession[], user: User) => {
       return (
         <>
           {states.loadUser ? (
@@ -24,13 +26,22 @@ export const ReactProfileWrapper = () => (
           <SharedPreferences resourceUri="user" />
           <UserTeams isLoading={states.loadTeams} loadTeams={api.loadTeams} teams={teams} />
           {!states.loadUser && (
-            <UserOrganizations
-              isLoading={states.loadOrgs}
-              setUserOrg={api.setUserOrg}
-              loadOrgs={api.loadOrgs}
-              orgs={orgs}
-              user={user}
-            />
+            <>
+              <UserOrganizations
+                isLoading={states.loadOrgs}
+                setUserOrg={api.setUserOrg}
+                loadOrgs={api.loadOrgs}
+                orgs={orgs}
+                user={user}
+              />
+              <UserSessions
+                isLoading={states.loadSessions}
+                loadSessions={api.loadSessions}
+                revokeUserSession={api.revokeUserSession}
+                sessions={sessions}
+                user={user}
+              />
+            </>
           )}
         </>
       );
