1d30d81389712975a7ed27f336b9dde449fe7403
Alex Khomenko <Clarity-89@users.noreply.github.com>
2020-05-06 12:53:28 +0300

    Search: folder view improvements (#24324)
    * Search: Do not set items if no results returned
    
    * Search: Simplify canSave logic
    
    * Search: Add initialLoading state
    
    * Search: Add itemsFetching state to folder
    
    * Search: Fix props and tests
    
    * Search: Fix strict null check

--- public/app/features/search/hooks/useManageDashboards.ts
+++ public/app/features/search/hooks/useManageDashboards.ts
@@ -1,43 +1,26 @@
 import { useMemo, useReducer } from 'react';
-import { backendSrv } from 'app/core/services/backend_srv';
+import { FolderDTO } from 'app/types';
+import { contextSrv } from 'app/core/services/context_srv';
 import { DashboardQuery, DashboardSection, OnDeleteItems, OnMoveItems, OnToggleChecked } from '../types';
-import {
-  DELETE_ITEMS,
-  MOVE_ITEMS,
-  TOGGLE_ALL_CHECKED,
-  TOGGLE_CHECKED,
-  TOGGLE_CAN_SAVE,
-  TOGGLE_EDIT_PERMISSIONS,
-} from '../reducers/actionTypes';
+import { DELETE_ITEMS, MOVE_ITEMS, TOGGLE_ALL_CHECKED, TOGGLE_CHECKED } from '../reducers/actionTypes';
 import { manageDashboardsReducer, manageDashboardsState, ManageDashboardsState } from '../reducers/manageDashboards';
 import { useSearch } from './useSearch';
 
 export const useManageDashboards = (
   query: DashboardQuery,
   state: Partial<ManageDashboardsState> = {},
-  folderUid?: string
+  folder?: FolderDTO
 ) => {
   const reducer = useReducer(manageDashboardsReducer, {
     ...manageDashboardsState,
     ...state,
   });
 
-  const searchCallback = (folderUid: string | undefined) => {
-    if (folderUid) {
-      backendSrv.getFolderByUid(folderUid).then(folder => {
-        dispatch({ type: TOGGLE_CAN_SAVE, payload: folder.canSave });
-        if (!folder.canSave) {
-          dispatch({ type: TOGGLE_EDIT_PERMISSIONS, payload: false });
-        }
-      });
-    }
-  };
-
   const {
-    state: { results, loading, canSave, allChecked, hasEditPermissionInFolders },
+    state: { results, loading, initialLoading, allChecked },
     onToggleSection,
     dispatch,
-  } = useSearch<ManageDashboardsState>(query, reducer, { folderUid, searchCallback });
+  } = useSearch<ManageDashboardsState>(query, reducer, {});
 
   const onToggleChecked: OnToggleChecked = item => {
     dispatch({ type: TOGGLE_CHECKED, payload: item });
@@ -64,9 +47,14 @@ export const useManageDashboards = (
     results,
   ]);
 
+  const canSave = folder?.canSave;
+  const hasEditPermissionInFolders = canSave ? contextSrv.hasEditPermissionInFolders : false;
+  const noFolders = canSave && folder?.id && results.length === 0 && !loading && !initialLoading;
+
   return {
     results,
     loading,
+    initialLoading,
     canSave,
     allChecked,
     hasEditPermissionInFolders,
@@ -77,5 +65,6 @@ export const useManageDashboards = (
     onToggleAllChecked,
     onDeleteItems,
     onMoveItems,
+    noFolders,
   };
 };
