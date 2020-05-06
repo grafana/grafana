de3c3e9e850d442851c7081a014122b5faeddbe5
Josh Soref <jsoref@users.noreply.github.com>
2020-05-06 17:34:13 -0400

    spelling: with

--- packages/grafana-ui/src/components/Forms/Legacy/Select/Select.tsx
+++ packages/grafana-ui/src/components/Forms/Legacy/Select/Select.tsx
@@ -2,7 +2,7 @@
 import classNames from 'classnames';
 import React, { PureComponent } from 'react';
 
-// Ignoring because I couldn't get @types/react-select work wih Torkel's fork
+// Ignoring because I couldn't get @types/react-select work with Torkel's fork
 // @ts-ignore
 import { default as ReactSelect } from '@torkelo/react-select';
 // @ts-ignore
