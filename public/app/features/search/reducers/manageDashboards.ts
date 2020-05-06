3a2c844a8abe363de2fb9889b771caf1ad529f00
Alex Khomenko <Clarity-89@users.noreply.github.com>
2020-05-06 12:53:28 +0300

    Search: folder view improvements (#24324)
    * Search: Do not set items if no results returned
    
    * Search: Simplify canSave logic
    
    * Search: Add initialLoading state
    
    * Search: Add itemsFetching state to folder
    
    * Search: Fix props and tests
    
    * Search: Fix strict null check

--- public/app/features/search/reducers/manageDashboards.ts
+++ public/app/features/search/reducers/manageDashboards.ts
@@ -1,34 +1,19 @@
 import { DashboardSectionItem, SearchAction } from '../types';
-import {
-  TOGGLE_CAN_SAVE,
-  TOGGLE_EDIT_PERMISSIONS,
-  TOGGLE_ALL_CHECKED,
-  TOGGLE_CHECKED,
-  MOVE_ITEMS,
-  DELETE_ITEMS,
-} from './actionTypes';
+import { TOGGLE_ALL_CHECKED, TOGGLE_CHECKED, MOVE_ITEMS, DELETE_ITEMS } from './actionTypes';
 import { dashboardsSearchState, DashboardsSearchState, searchReducer } from './dashboardSearch';
 import { mergeReducers } from '../utils';
 
 export interface ManageDashboardsState extends DashboardsSearchState {
-  canSave: boolean;
   allChecked: boolean;
-  hasEditPermissionInFolders: boolean;
 }
 
 export const manageDashboardsState: ManageDashboardsState = {
   ...dashboardsSearchState,
-  canSave: false,
   allChecked: false,
-  hasEditPermissionInFolders: false,
 };
 
 const reducer = (state: ManageDashboardsState, action: SearchAction) => {
   switch (action.type) {
-    case TOGGLE_CAN_SAVE:
-      return { ...state, canSave: action.payload };
-    case TOGGLE_EDIT_PERMISSIONS:
-      return { ...state, hasEditPermissionInFolders: action.payload };
     case TOGGLE_ALL_CHECKED:
       const newAllChecked = !state.allChecked;
       return {
