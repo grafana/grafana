de37d1c3d7196110847744a44002a578a584888b
Diana Payton <52059945+oddlittlebird@users.noreply.github.com>
2020-05-06 12:54:10 -0700

    Update packages/grafana-data/src/datetime/parser.ts

--- packages/grafana-data/src/datetime/parser.ts
+++ packages/grafana-data/src/datetime/parser.ts
@@ -12,7 +12,7 @@ import { lowerCase } from 'lodash';
 export interface DateTimeOptionsWhenParsing extends DateTimeOptions {
   /**
    * If the input is a Grafana quick date, e.g. now-6h, then you can specify this to control
-   * if the last part of the date and time value should be included or excluded.
+   * whether the last part of the date and time value is included or excluded.
    *
    * As an example now-6h and the current time is 12:20:00 if roundUp is set to true
    * the returned DateTime value will be 06:00:00.
