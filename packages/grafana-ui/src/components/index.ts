3fdbf852d8bb052a07beba0efbc8bfb00277b753
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-05-07 10:07:29 +0200

    Remove index and export

--- packages/grafana-ui/src/components/index.ts
+++ packages/grafana-ui/src/components/index.ts
@@ -122,7 +122,6 @@ export { SelectValueEditor } from './OptionsUI/select';
 export { FieldConfigItemHeaderTitle } from './FieldConfigs/FieldConfigItemHeaderTitle';
 
 // Next-gen forms
-export { default as Forms } from './Forms';
 export { Form } from './Forms/Form';
 export { InputControl } from './InputControl';
 export * from './Button';
