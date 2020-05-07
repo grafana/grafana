e5f064d16e0805bba8d9fb7dd42d784a73118173
Ivana <ivana.huckova@gmail.com>
2020-05-07 19:05:24 +0200

    Update comments

--- public/app/plugins/datasource/elasticsearch/query_builder.ts
+++ public/app/plugins/datasource/elasticsearch/query_builder.ts
@@ -205,8 +205,7 @@ export class ElasticQueryBuilder {
 
     this.addAdhocFilters(query, adhocFilters);
 
-    // Handle document query:
-    // 1. If target doesn't have bucketAggs and type is not raw_document, it is invalid query.
+    // If target doesn't have bucketAggs and type is not raw_document, it is invalid query.
     if (target.bucketAggs.length === 0) {
       metric = target.metrics[0];
       if (!metric || metric.type !== 'raw_document') {
@@ -214,8 +213,10 @@ export class ElasticQueryBuilder {
       }
     }
 
-    // 2. Check if metric type is raw_document. If metric doesn't have size (or size is 0), update size to 500.
-    // Otherwise it will not be a valid query and error will be thrown.
+    /* Handle document query:
+     * Check if metric type is raw_document. If metric doesn't have size (or size is 0), update size to 500.
+     * Otherwise it will not be a valid query and error will be thrown.
+     */
     if (target.metrics[0].type === 'raw_document') {
       metric = target.metrics[0];
       const size = (metric.settings && metric.settings.size !== 0 && metric.settings.size) || 500;
