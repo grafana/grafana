b32bd56fb016c290dde587383f74fa0fb5f347a2
Dominik Prokop <dominik.prokop@grafana.com>
2020-05-10 17:43:17 +0200

    Fix cross dependencies versions for canary

--- scripts/circle-release-next-packages.sh
+++ scripts/circle-release-next-packages.sh
@@ -54,7 +54,7 @@ if [ -z "$count" ]; then
 else
   echo "Changes detected in ${count} packages"
   echo "Releasing packages under ${PACKAGE_VERSION}-${GIT_SHA}"
-  npx lerna version "${PACKAGE_VERSION}-${GIT_SHA}" --no-git-tag-version --no-push --force-publish -y
+  npx lerna version "${PACKAGE_VERSION}-${GIT_SHA}" --exact --no-git-tag-version --no-push --force-publish -y
   echo $'\nGit status:'
   git status -s
 
