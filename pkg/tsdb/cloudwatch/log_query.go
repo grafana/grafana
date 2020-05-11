62ef1df44029504b9fb616359a96a334781e06e1
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2020-05-11 14:44:13 +0100

    Datasource/CloudWatch: Usability improvements (#24447)
    * Datasource/CloudWatch: Improve handling of long-running queries
    
    * Datasource/CloudWatch: Make order of dataframe fields consistent

--- pkg/tsdb/cloudwatch/log_query.go
+++ pkg/tsdb/cloudwatch/log_query.go
@@ -10,6 +10,11 @@ import (
 func logsResultsToDataframes(response *cloudwatchlogs.GetQueryResultsOutput) (*data.Frame, error) {
 	rowCount := len(response.Results)
 	fieldValues := make(map[string]interface{})
+
+	// Maintaining a list of field names in the order returned from CloudWatch
+	// as just iterating over fieldValues would not give a consistent order
+	fieldNames := make([]*string, 0)
+
 	for i, row := range response.Results {
 		for _, resultField := range row {
 			// Strip @ptr field from results as it's not needed
@@ -17,48 +22,37 @@ func logsResultsToDataframes(response *cloudwatchlogs.GetQueryResultsOutput) (*d
 				continue
 			}
 
-			if *resultField.Field == "@timestamp" {
-				if _, exists := fieldValues[*resultField.Field]; !exists {
-					fieldValues[*resultField.Field] = make([]*time.Time, rowCount)
-				}
+			if _, exists := fieldValues[*resultField.Field]; !exists {
+				fieldNames = append(fieldNames, resultField.Field)
 
+				// Check if field is time field
+				if _, err := time.Parse(CLOUDWATCH_TS_FORMAT, *resultField.Value); err == nil {
+					fieldValues[*resultField.Field] = make([]*time.Time, rowCount)
+				} else {
+					fieldValues[*resultField.Field] = make([]*string, rowCount)
+				}
+			}
+
+			if timeField, ok := fieldValues[*resultField.Field].([]*time.Time); ok {
 				parsedTime, err := time.Parse(CLOUDWATCH_TS_FORMAT, *resultField.Value)
 				if err != nil {
 					return nil, err
 				}
 
-				fieldValues[*resultField.Field].([]*time.Time)[i] = &parsedTime
+				timeField[i] = &parsedTime
 			} else {
-				if _, exists := fieldValues[*resultField.Field]; !exists {
-					// Check if field is time field
-					if _, err := time.Parse(CLOUDWATCH_TS_FORMAT, *resultField.Value); err == nil {
-						fieldValues[*resultField.Field] = make([]*time.Time, rowCount)
-					} else {
-						fieldValues[*resultField.Field] = make([]*string, rowCount)
-					}
-				}
-
-				if timeField, ok := fieldValues[*resultField.Field].([]*time.Time); ok {
-					parsedTime, err := time.Parse(CLOUDWATCH_TS_FORMAT, *resultField.Value)
-					if err != nil {
-						return nil, err
-					}
-
-					timeField[i] = &parsedTime
-				} else {
-					fieldValues[*resultField.Field].([]*string)[i] = resultField.Value
-				}
+				fieldValues[*resultField.Field].([]*string)[i] = resultField.Value
 			}
 		}
 	}
 
 	newFields := make([]*data.Field, 0)
-	for fieldName, vals := range fieldValues {
-		newFields = append(newFields, data.NewField(fieldName, nil, vals))
+	for _, fieldName := range fieldNames {
+		newFields = append(newFields, data.NewField(*fieldName, nil, fieldValues[*fieldName]))
 
-		if fieldName == "@timestamp" {
+		if *fieldName == "@timestamp" {
 			newFields[len(newFields)-1].SetConfig(&data.FieldConfig{Title: "Time"})
-		} else if fieldName == "@logStream" || fieldName == "@log" {
+		} else if *fieldName == "@logStream" || *fieldName == "@log" {
 			newFields[len(newFields)-1].SetConfig(
 				&data.FieldConfig{
 					Custom: map[string]interface{}{
