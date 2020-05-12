b3d5dd93117d4c82949e111216907bd21c0a2aff
srclosson <srclosson@gmail.com>
2020-05-12 11:12:48 -0600

    Various fixes and requests for toolkit: 1) Added toolkit to the alpine docker image, so we can get latest fixes without having to re-release 2) Removed cp from e2d docker image 3) Change github client to allow to specify an owner so it's not "grafana" only 4) Toolkit refers to grafana-ui and grafana-data "next", rather than canary

--- packages/grafana-toolkit/docker/grafana-plugin-ci-alpine/build.sh
+++ packages/grafana-toolkit/docker/grafana-plugin-ci-alpine/build.sh
@@ -3,6 +3,13 @@ set -eo pipefail
 
 source ./common.sh
 
+/bin/rm -rfv install/grafana-toolkit
+mkdir -pv install/grafana-toolkit
+cp -rv ../../bin install/grafana-toolkit
+cp -rv ../../src install/grafana-toolkit
+cp -v ../../package.json install/grafana-toolkit
+cp -v ../../tsconfig.json install/grafana-toolkit
+
 output=$(docker build . | tee /dev/tty)
 hash=$(echo "$output" | tail -1 | sed -ne "s/^Successfully built \(.*\)/\1/p")
 if [ ${#hash} -gt 0 ]; then
