5039c25ae3a46a9bd62655da4a89fce0ba63d3af
Dominik Prokop <dominik.prokop@grafana.com>
2020-05-13 13:01:10 +0200

    Transformations: Make transform dropdowns not cropped (#24615)
    (cherry picked from commit 9e24c0944f9dcd8a41e4d17900329b3c25bb2374)

--- public/app/core/components/TransformersUI/CalculateFieldTransformerEditor.tsx
+++ public/app/core/components/TransformersUI/CalculateFieldTransformerEditor.tsx
@@ -201,6 +201,7 @@ export class CalculateFieldTransformerEditor extends React.PureComponent<
               stats={[options.reducer]}
               onChange={this.onStatsChange}
               defaultStat={ReducerID.sum}
+              menuPlacement="bottom"
             />
           </div>
         </div>
@@ -279,12 +280,14 @@ export class CalculateFieldTransformerEditor extends React.PureComponent<
             className="min-width-18 gf-form-spacing"
             value={options?.left}
             onChange={this.onBinaryLeftChanged}
+            menuPlacement="bottom"
           />
           <Select
             className="width-8 gf-form-spacing"
             options={ops}
             value={options.operator ?? ops[0].value}
             onChange={this.onBinaryOperationChanged}
+            menuPlacement="bottom"
           />
           <Select
             allowCustomValue
@@ -293,6 +296,7 @@ export class CalculateFieldTransformerEditor extends React.PureComponent<
             options={rightNames}
             value={options?.right}
             onChange={this.onBinaryRightChanged}
+            menuPlacement="bottom"
           />
         </div>
       </div>
@@ -318,6 +322,7 @@ export class CalculateFieldTransformerEditor extends React.PureComponent<
               options={calculationModes}
               value={calculationModes.find(v => v.value === mode)}
               onChange={this.onModeChanged}
+              menuPlacement="bottom"
             />
           </div>
         </div>
