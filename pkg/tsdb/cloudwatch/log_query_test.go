8b7945d271eb212f79a8475f08f9304e9f33ba42
Andrej Ocenas <mr.ocenas@gmail.com>
2020-05-14 13:39:12 +0200

    Handle empty rows in response

--- pkg/tsdb/cloudwatch/log_query_test.go
+++ pkg/tsdb/cloudwatch/log_query_test.go
@@ -78,6 +78,8 @@ func TestLogsResultsToDataframes(t *testing.T) {
 					Value: aws.String("fakelog"),
 				},
 			},
+			// Sometimes cloudwatch returns empty row
+			{},
 			{
 				&cloudwatchlogs.ResultField{
 					Field: aws.String("@ptr"),
