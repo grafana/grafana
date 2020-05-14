6a31d029235497fe39b304c59ff1474e1a049b55
Marcus Andersson <marcus.andersson@grafana.com>
2020-05-14 16:19:08 +0200

    added tests for the name matcher.

--- packages/grafana-data/src/transformations/matchers/nameMatcher.test.ts
+++ packages/grafana-data/src/transformations/matchers/nameMatcher.test.ts
@@ -2,7 +2,7 @@ import { getFieldMatcher } from '../matchers';
 import { FieldMatcherID } from './ids';
 import { toDataFrame } from '../../dataframe/processDataFrame';
 
-describe('Field Name Matcher', () => {
+describe('Field Name by Regexp Matcher', () => {
   it('Match all with wildcard regex', () => {
     const seriesWithNames = toDataFrame({
       fields: [{ name: 'A hello world' }, { name: 'AAA' }, { name: 'C' }],
@@ -51,6 +51,258 @@ describe('Field Name Matcher', () => {
         resultCount++;
       }
       expect(resultCount).toBe(1);
+    }
+  });
+});
+
+describe('Field Name Matcher', () => {
+  it('Match only exact name', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'A hello world' }, { name: 'AAA' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byName,
+      options: 'C',
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      const didMatch = matcher(field, seriesWithNames, [seriesWithNames]);
+      expect(didMatch).toBe(field.name === 'C');
+    }
+  });
+
+  it('Match should respect letter case', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: '12' }, { name: '112' }, { name: '13' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byName,
+      options: 'c',
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(false);
+    }
+  });
+
+  it('Match none of the field names', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'some.instance.path' }, { name: '112' }, { name: '13' }],
+    });
+    const config = {
+      id: FieldMatcherID.byName,
+      options: '',
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(false);
+    }
+  });
+});
+
+describe('Field Multiple Names Matcher', () => {
+  it('Match only exact name', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'A hello world' }, { name: 'AAA' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byNames,
+      options: ['C'],
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      const didMatch = matcher(field, seriesWithNames, [seriesWithNames]);
+      expect(didMatch).toBe(field.name === 'C');
+    }
+  });
+
+  it('Match should respect letter case', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: '12' }, { name: '112' }, { name: '13' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byNames,
+      options: ['c'],
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(false);
+    }
+  });
+
+  it('Match none of the field names', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'some.instance.path' }, { name: '112' }, { name: '13' }],
+    });
+    const config = {
+      id: FieldMatcherID.byNames,
+      options: [],
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(false);
+    }
+  });
+
+  it('Match all of the field names', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'some.instance.path' }, { name: '112' }, { name: '13' }],
+    });
+    const config = {
+      id: FieldMatcherID.byNames,
+      options: ['some.instance.path', '112', '13'],
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(true);
+    }
+  });
+});
+
+describe('Field Regexp or Names Matcher', () => {
+  it('Match only exact name by name', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'A hello world' }, { name: 'AAA' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        names: ['C'],
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      const didMatch = matcher(field, seriesWithNames, [seriesWithNames]);
+      expect(didMatch).toBe(field.name === 'C');
+    }
+  });
+
+  it('Match all starting with AA', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'A hello world' }, { name: 'AAA' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        pattern: '/^AA/',
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      const didMatch = matcher(field, seriesWithNames, [seriesWithNames]);
+      expect(didMatch).toBe(field.name === 'AAA');
+    }
+  });
+
+  it('Match all starting with AA and C', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'A hello world' }, { name: 'AAA' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        pattern: '/^AA/',
+        names: ['C'],
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      const didMatch = matcher(field, seriesWithNames, [seriesWithNames]);
+      expect(didMatch).toBe(field.name === 'AAA' || field.name === 'C');
+    }
+  });
+
+  it('Match should respect letter case by name if not igored in pattern', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: '12' }, { name: '112' }, { name: '13' }, { name: 'C' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        names: ['c'],
+        pattern: '/c/i',
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      const didMatch = matcher(field, seriesWithNames, [seriesWithNames]);
+      expect(didMatch).toBe(field.name === 'C');
+    }
+  });
+
+  it('Match none of the field names by name', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'some.instance.path' }, { name: '112' }, { name: '13' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        names: [],
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(false);
+    }
+  });
+
+  it('Match all of the field names by name', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'some.instance.path' }, { name: '112' }, { name: '13' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        names: ['some.instance.path', '112', '13'],
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(true);
+    }
+  });
+
+  it('Match all of the field names by regexp', () => {
+    const seriesWithNames = toDataFrame({
+      fields: [{ name: 'some.instance.path' }, { name: '112' }, { name: '13' }],
+    });
+    const config = {
+      id: FieldMatcherID.byRegexpOrNames,
+      options: {
+        pattern: '/.*/',
+      },
+    };
+
+    const matcher = getFieldMatcher(config);
+
+    for (const field of seriesWithNames.fields) {
+      expect(matcher(field, seriesWithNames, [seriesWithNames])).toBe(true);
     }
   });
 });
