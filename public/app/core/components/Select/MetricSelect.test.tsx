ee8dd8713d1e67d5fd654d6b32951637cadb214e
Marcus Andersson <marcus.andersson@grafana.com>
2020-05-15 14:29:15 +0200

    skipping testing of internal component logic.

--- public/app/core/components/Select/MetricSelect.test.tsx
+++ public/app/core/components/Select/MetricSelect.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MetricSelect, compareFn, buildOptions, getSelectedOption } from './MetricSelect';
-import { Select } from '@grafana/ui';
+import { MetricSelect } from './MetricSelect';
+import { LegacyForms } from '@grafana/ui';
+const { Select } = LegacyForms;
 
 describe('MetricSelect', () => {
   describe('When receive props', () => {
@@ -42,47 +43,6 @@ describe('MetricSelect', () => {
           .noOptionsMessage()
       ).toEqual('No options found');
       expect(spyOnChange).toHaveBeenCalledWith('foo');
-    });
-    it('should compareFunc returns true if next and prev valus are same', () => {
-      const prevProps: any = { value: 'foo' };
-      const nextProps: any = { value: 'foo' };
-      expect(compareFn(nextProps, prevProps)).toEqual(true);
-    });
-    it('should compareFunc returns true if prev and next build options are not equal', () => {
-      const prevProps: any = { value: 'fooa', variables: [{ name: 'asd' }], options: [{ prop: 'val' }] };
-      const nextProps: any = { value: 'foo', variables: [{ name: 'qwer' }], options: [{ prop: 'val' }] };
-      expect(compareFn(nextProps, prevProps)).toEqual(true);
-    });
-  });
-  describe('buildOptions', () => {
-    it('should build options correctly', () => {
-      const props: any = {
-        variables: [{ name: 'foo' }, { name: 'bar' }],
-        options: [{ bar: 'bar', baz: 'baz' }],
-      };
-      expect(buildOptions(props)).toEqual([
-        {
-          label: 'Template Variables',
-          options: [
-            { label: '$foo', value: '$foo' },
-            { label: '$bar', value: '$bar' },
-          ],
-        },
-        ...props.options,
-      ]);
-    });
-    it('should return options when variables are empty', () => {
-      const props: any = {
-        variables: [],
-        options: [{ bar: 'bar', baz: 'baz' }],
-      };
-      expect(buildOptions(props)).toEqual([{ bar: 'bar', baz: 'baz' }]);
-    });
-  });
-  describe('getSelectedOption', () => {
-    it('should return the option which value is matching the passed value', () => {
-      const options = [{ value: 'baz' }, { value: 'foo' }, { value: 'bar' }];
-      expect(getSelectedOption(options, 'foo')).toEqual(options[1]);
     });
   });
 });
