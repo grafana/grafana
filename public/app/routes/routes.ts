f2cc239b7bc12f57f97616f1bf7143cf5e46f196
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-05-15 16:35:38 +0200

    Remove old code

--- public/app/routes/routes.ts
+++ public/app/routes/routes.ts
@@ -318,9 +318,6 @@ export function setupAngularRoutes($routeProvider: route.IRouteProvider, $locati
       },
     })
     .when('/profile/select-org', {
-      // templateUrl: 'public/app/features/org/partials/select_org.html',
-      // controller: 'SelectOrgCtrl',
-      // reloadOnSearch: false,
       template: '<react-container/>',
       reloadOnSearch: false,
       resolve: {
