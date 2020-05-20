fe890a0708100d92d7a20e8fa61fb50541c3484a
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-05-20 13:56:28 +0200

    Panel: shows correct panel menu items in view mode (#24912)
    (cherry picked from commit 460847f3edb255ad8662f3f96b776769f165a496)

--- public/app/features/dashboard/utils/getPanelMenu.test.ts
+++ public/app/features/dashboard/utils/getPanelMenu.test.ts
@@ -1,5 +1,6 @@
-import { PanelModel, DashboardModel } from '../state';
+import { DashboardModel, PanelModel } from '../state';
 import { getPanelMenu } from './getPanelMenu';
+import { describe } from '../../../../test/lib/common';
 
 describe('getPanelMenu', () => {
   it('should return the correct panel menu items', () => {
@@ -68,5 +69,76 @@ describe('getPanelMenu', () => {
         },
       ]
     `);
+  });
+
+  describe('when panel is in view mode', () => {
+    it('should return the correct panel menu items', () => {
+      const getExtendedMenu = () => [{ text: 'Toggle legend', shortcut: 'p l', click: jest.fn() }];
+      const ctrl: any = { getExtendedMenu };
+      const scope: any = { $$childHead: { ctrl } };
+      const angularComponent: any = { getScope: () => scope };
+      const panel = new PanelModel({ isViewing: true });
+      const dashboard = new DashboardModel({});
+
+      const menuItems = getPanelMenu(dashboard, panel, angularComponent);
+      expect(menuItems).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "iconClassName": "eye",
+          "onClick": [Function],
+          "shortcut": "v",
+          "text": "View",
+        },
+        Object {
+          "iconClassName": "edit",
+          "onClick": [Function],
+          "shortcut": "e",
+          "text": "Edit",
+        },
+        Object {
+          "iconClassName": "share-alt",
+          "onClick": [Function],
+          "shortcut": "p s",
+          "text": "Share",
+        },
+        Object {
+          "iconClassName": "info-circle",
+          "onClick": [Function],
+          "shortcut": "i",
+          "subMenu": Array [
+            Object {
+              "onClick": [Function],
+              "text": "Panel JSON",
+            },
+          ],
+          "text": "Inspect",
+          "type": "submenu",
+        },
+        Object {
+          "iconClassName": "cube",
+          "onClick": [Function],
+          "subMenu": Array [
+            Object {
+              "href": undefined,
+              "onClick": [Function],
+              "shortcut": "p l",
+              "text": "Toggle legend",
+            },
+          ],
+          "text": "More...",
+          "type": "submenu",
+        },
+        Object {
+          "type": "divider",
+        },
+        Object {
+          "iconClassName": "trash-alt",
+          "onClick": [Function],
+          "shortcut": "p r",
+          "text": "Remove",
+        },
+      ]
+    `);
+    });
   });
 });
