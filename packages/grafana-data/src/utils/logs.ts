2f7d68d8015e699b012a6dbc2b18df04d4776d77
Josh Soref <jsoref@users.noreply.github.com>
2020-05-22 14:06:39 -0400

    Chore: spelling - grafana-data (#24436)

--- packages/grafana-data/src/utils/logs.ts
+++ packages/grafana-data/src/utils/logs.ts
@@ -7,7 +7,7 @@ import { ArrayVector } from '../vector/ArrayVector';
 // This matches:
 // first a label from start of the string or first white space, then any word chars until "="
 // second either an empty quotes, or anything that starts with quote and ends with unescaped quote,
-// or any non whitespace chars that do not start with qoute
+// or any non whitespace chars that do not start with quote
 const LOGFMT_REGEXP = /(?:^|\s)([\w\(\)\[\]\{\}]+)=(""|(?:".*?[^\\]"|[^"\s]\S*))/;
 
 /**
