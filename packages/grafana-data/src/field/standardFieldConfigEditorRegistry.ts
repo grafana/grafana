3224f7587a525ae2a4f00aec063e13275fd62504
Ryan McKinley <ryantxu@gmail.com>
2020-05-29 12:36:15 -0700

    Stats: include all fields (#24829)
    (cherry picked from commit d526647005d8fcba85a02b4de70a10c689726d16)

--- packages/grafana-data/src/field/standardFieldConfigEditorRegistry.ts
+++ packages/grafana-data/src/field/standardFieldConfigEditorRegistry.ts
@@ -1,11 +1,19 @@
 import { Registry, RegistryItem } from '../utils/Registry';
 import { ComponentType } from 'react';
 import { FieldConfigOptionsRegistry } from './FieldConfigOptionsRegistry';
+import { DataFrame, InterpolateFunction, VariableSuggestionsScope, VariableSuggestion } from '../types';
+
+export interface StandardEditorContext {
+  data?: DataFrame[]; // All results
+  replaceVariables?: InterpolateFunction;
+  getSuggestions?: (scope?: VariableSuggestionsScope) => VariableSuggestion[];
+}
 
 export interface StandardEditorProps<TValue = any, TSettings = any> {
   value: TValue;
   onChange: (value?: TValue) => void;
   item: StandardEditorsRegistryItem<TValue, TSettings>;
+  context: StandardEditorContext;
 }
 export interface StandardEditorsRegistryItem<TValue = any, TSettings = any> extends RegistryItem {
   editor: ComponentType<StandardEditorProps<TValue, TSettings>>;
