0e9bfecadd35575c1ed4bc6dbe16c682c50ac931
Ryan McKinley <ryantxu@gmail.com>
2020-05-29 13:03:45 -0700

    OptionsUI: support string list (#25134)

--- packages/grafana-ui/src/utils/standardEditors.tsx
+++ packages/grafana-ui/src/utils/standardEditors.tsx
@@ -20,7 +20,13 @@ import {
 } from '@grafana/data';
 
 import { Switch } from '../components/Switch/Switch';
-import { NumberValueEditor, RadioButtonGroup, StringValueEditor, SelectValueEditor } from '../components';
+import {
+  NumberValueEditor,
+  RadioButtonGroup,
+  StringValueEditor,
+  StringArrayEditor,
+  SelectValueEditor,
+} from '../components';
 import { ValueMappingsValueEditor } from '../components/OptionsUI/mappings';
 import { ThresholdsValueEditor } from '../components/OptionsUI/thresholds';
 import { UnitValueEditor } from '../components/OptionsUI/units';
@@ -227,6 +233,13 @@ export const getStandardOptionEditors = () => {
     editor: StringValueEditor as any,
   };
 
+  const strings: StandardEditorsRegistryItem<string[]> = {
+    id: 'strings',
+    name: 'String array',
+    description: 'An array of strings',
+    editor: StringArrayEditor as any,
+  };
+
   const boolean: StandardEditorsRegistryItem<boolean> = {
     id: 'boolean',
     name: 'Boolean',
@@ -290,5 +303,5 @@ export const getStandardOptionEditors = () => {
     description: '',
   };
 
-  return [text, number, boolean, radio, select, unit, mappings, thresholds, links, color, statsPicker];
+  return [text, number, boolean, radio, select, unit, mappings, thresholds, links, color, statsPicker, strings];
 };
