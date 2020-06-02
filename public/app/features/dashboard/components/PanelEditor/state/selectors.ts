ae354e7e6fc1c757ccfa4528b97b4307b55c4669
Dominik Prokop <dominik.prokop@grafana.com>
2020-06-02 12:38:18 +0200

    Add tests

--- public/app/features/dashboard/components/PanelEditor/state/selectors.ts
+++ public/app/features/dashboard/components/PanelEditor/state/selectors.ts
@@ -2,7 +2,7 @@ import memoizeOne from 'memoize-one';
 import { LocationState } from 'app/types';
 import { PanelPlugin } from '@grafana/data';
 import { PanelEditorTab, PanelEditorTabId } from '../types';
-import { config } from '@grafana/runtime';
+import { getConfig } from 'app/core/config';
 
 export const getPanelEditorTabs = memoizeOne((location: LocationState, plugin?: PanelPlugin) => {
   const tabs: PanelEditorTab[] = [];
@@ -35,7 +35,7 @@ export const getPanelEditorTabs = memoizeOne((location: LocationState, plugin?: 
     });
   }
 
-  if (config.alertingEnabled && plugin.meta.id === 'graph') {
+  if (getConfig().alertingEnabled && plugin.meta.id === 'graph') {
     tabs.push({
       id: PanelEditorTabId.Alert,
       text: 'Alert',
