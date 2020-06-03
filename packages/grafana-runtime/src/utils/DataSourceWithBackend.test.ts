222feffb4eafa8cfb6699357213e9cd27349cd1a
Ryan McKinley <ryantxu@gmail.com>
2020-06-03 14:15:05 -0700

    better tests

--- packages/grafana-runtime/src/utils/DataSourceWithBackend.test.ts
+++ packages/grafana-runtime/src/utils/DataSourceWithBackend.test.ts
@@ -8,12 +8,28 @@ class MyDataSource extends DataSourceWithBackend<DataQuery, DataSourceJsonData> 
   }
 }
 
+const mockDatasourceRequest = jest.fn();
+
 const backendSrv = ({
-  datasourceRequest: jest.fn(),
+  datasourceRequest: mockDatasourceRequest,
 } as unknown) as BackendSrv;
 
 jest.mock('../services', () => ({
   getBackendSrv: () => backendSrv,
+}));
+jest.mock('..', () => ({
+  config: {
+    bootData: {
+      user: {
+        orgId: 77,
+      },
+    },
+    datasources: {
+      sample: {
+        id: 8674,
+      },
+    },
+  },
 }));
 
 describe('DataSourceWithBackend', () => {
@@ -24,12 +40,44 @@ describe('DataSourceWithBackend', () => {
       jsonData: {},
     } as DataSourceInstanceSettings<DataSourceJsonData>;
 
+    mockDatasourceRequest.mockReset();
+    mockDatasourceRequest.mockReturnValue(Promise.resolve({}));
     const ds = new MyDataSource(settings);
     ds.query({
-      targets: [{ refId: 'A' }, { refId: 'B' }],
+      maxDataPoints: 10,
+      intervalMs: 5000,
+      targets: [{ refId: 'A' }, { refId: 'B', datasource: 'sample' }],
     } as DataQueryRequest);
 
-    const mock = (backendSrv.datasourceRequest as any).mock;
+    const mock = mockDatasourceRequest.mock;
     expect(mock.calls.length).toBe(1);
+
+    const args = mock.calls[0][0];
+    expect(args).toMatchInlineSnapshot(`
+      Object {
+        "data": Object {
+          "queries": Array [
+            Object {
+              "datasourceId": 1234,
+              "intervalMs": 5000,
+              "maxDataPoints": 10,
+              "orgId": 77,
+              "refId": "A",
+            },
+            Object {
+              "datasource": "sample",
+              "datasourceId": 8674,
+              "intervalMs": 5000,
+              "maxDataPoints": 10,
+              "orgId": 77,
+              "refId": "B",
+            },
+          ],
+        },
+        "method": "POST",
+        "requestId": undefined,
+        "url": "/api/ds/query",
+      }
+    `);
   });
 });
