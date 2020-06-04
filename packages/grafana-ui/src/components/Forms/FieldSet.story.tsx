990b87fbd9ad3637488db4b3e22554b4492ff9c3
Clarity-89 <homes89@ukr.net>
2020-06-04 10:11:48 +0300

    Grafana UI: Add story

--- packages/grafana-ui/src/components/Forms/FieldSet.story.tsx
+++ packages/grafana-ui/src/components/Forms/FieldSet.story.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { FieldSet } from './FieldSet';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
+import { FieldSet } from './FieldSet';
+import { Form } from './Form';
+import { Field } from './Field';
+import { Input } from '../Input/Input';
 import mdx from './FieldSet.mdx';
+import { Button } from '../Button';
 
 export default {
   title: 'Forms/FieldSet',
@@ -15,5 +19,30 @@ export default {
 };
 
 export const single = () => {
-  return <FieldSet />;
+  return (
+    <Form onSubmit={() => console.log('Submit')}>
+      {() => (
+        <>
+          <FieldSet label="Details">
+            <Field label="Name">
+              <Input name="name" />
+            </Field>
+            <Field label="Email">
+              <Input name="email" />
+            </Field>
+          </FieldSet>
+
+          <FieldSet label="Preferences">
+            <Field label="Color">
+              <Input name="color" />
+            </Field>
+            <Field label="Font size">
+              <Input name="fontsize" />
+            </Field>
+          </FieldSet>
+          <Button variant="primary">Save</Button>
+        </>
+      )}
+    </Form>
+  );
 };
