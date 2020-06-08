3a4526342d99bd31f5adb380d2add02b6153ea26
Steven Vachon <contact@svachon.com>
2020-06-08 09:52:34 -0400

    @grafana/e2e: improvements (#25342)
    * Minor changes
    
    * Remove console.* logger plugin
    
    ... as it doesn't work in Electron
    
    * Only open/close panel editor options and groups when state is inverted
    
    ... meaning, only open when closed and only close when open. This avoids unpredictable states, causing inconsistent results.
    
    * Support for adding multiple datasources and dashboards
    
    ... and having them all auto-removed when tests are completed
    
    * Avoid page errors when removing dashboards and datasources [keep?]
    
    * Wait for chart data before saving panel
    
    ... so that everything is ready when returning to the dashboard

--- packages/grafana-e2e/cypress/plugins/index.js
+++ packages/grafana-e2e/cypress/plugins/index.js
@@ -2,20 +2,16 @@ const compareScreenshots = require('./compareScreenshots');
 const extendConfig = require('./extendConfig');
 const readProvisions = require('./readProvisions');
 const typescriptPreprocessor = require('./typescriptPreprocessor');
-const { install: installConsoleLogger } = require('cypress-log-to-output');
 
 module.exports = (on, config) => {
   on('file:preprocessor', typescriptPreprocessor);
   on('task', { compareScreenshots, readProvisions });
   on('task', {
-    // @todo remove
     log({ message, optional }) {
       optional ? console.log(message, optional) : console.log(message);
       return null;
     },
   });
-
-  installConsoleLogger(on);
 
   // Always extend with this library's config and return for diffing
   // @todo remove this when possible: https://github.com/cypress-io/cypress/issues/5674
--- packages/grafana-e2e/src/flows/deleteDataSource.ts
+++ packages/grafana-e2e/src/flows/deleteDataSource.ts
@@ -1,13 +1,21 @@
 import { e2e } from '../index';
 import { fromBaseUrl } from '../support/url';
 
-export const deleteDataSource = (dataSourceName: string) => {
-  e2e().logToConsole('Deleting data source with name:', dataSourceName);
-  e2e().request('DELETE', fromBaseUrl(`/api/datasources/name/${dataSourceName}`));
+export interface DeleteDataSourceConfig {
+  id: string;
+  name: string;
+}
+
+export const deleteDataSource = ({ id, name }: DeleteDataSourceConfig) => {
+  e2e().logToConsole('Deleting data source with name:', name);
+
+  // Avoid datasources page errors
+  e2e.pages.Home.visit();
+  e2e().request('DELETE', fromBaseUrl(`/api/datasources/name/${name}`));
 
   /* https://github.com/cypress-io/cypress/issues/2831
   Pages.DataSources.visit();
-  Pages.DataSources.dataSources(dataSourceName).click();
+  Pages.DataSources.dataSources(name).click();
 
   Pages.DataSource.delete().click();
 
@@ -16,9 +24,17 @@ export const deleteDataSource = (dataSourceName: string) => {
   Pages.DataSources.visit();
   Pages.DataSources.dataSources().each(item => {
     const text = item.text();
-    if (text && text.indexOf(dataSourceName) !== -1) {
-      expect(false).equals(true, `Data source ${dataSourceName} was found although it was deleted.`);
+    if (text && text.indexOf(name) !== -1) {
+      expect(false).equals(true, `Data source ${name} was found although it was deleted.`);
     }
   });
- */
+  */
+
+  e2e.getScenarioContext().then(({ addedDataSources }: any) => {
+    e2e.setScenarioContext({
+      addedDataSources: addedDataSources.filter((dataSource: DeleteDataSourceConfig) => {
+        return dataSource.id !== id && dataSource.name !== name;
+      }),
+    });
+  });
 };
