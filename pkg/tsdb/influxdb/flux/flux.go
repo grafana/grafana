f2bbf347f2ff98486922b2a65ba72ffdaef94c00
kyle <kyle@grafana.com>
2020-06-08 14:15:26 -0400

    a query works, not all macros will

--- pkg/tsdb/influxdb/flux/flux.go
+++ pkg/tsdb/influxdb/flux/flux.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/tsdb"
@@ -20,24 +20,36 @@ func init() {
 }
 
 func Query(ctx context.Context, dsInfo *models.DataSource, tsdbQuery *tsdb.TsdbQuery) (*tsdb.Response, error) {
-	_, _ = RunnerFromDataSource(dsInfo)
-	frames := data.Frames{data.NewFrame("", data.NewField("test", nil, []string{"Hello"}))}
-	encoded, err := frames.MarshalArrow()
+	tRes := &tsdb.Response{
+		Results: make(map[string]*tsdb.QueryResult),
+	}
+	runner, err := RunnerFromDataSource(dsInfo)
 	if err != nil {
 		return nil, err
 	}
-	return &tsdb.Response{
-		Results: map[string]*tsdb.QueryResult{
-			"A": {
-				Dataframes: encoded,
-			},
-		},
-	}, nil
+
+	for _, query := range tsdbQuery.Queries {
+
+		qm, err := GetQueryModelTSDB(query, tsdbQuery.TimeRange)
+		if err != nil {
+			tRes.Results[query.RefId] = &tsdb.QueryResult{Error: err}
+			continue
+		}
+
+		res := ExecuteQuery(context.Background(), *qm, *runner, 10)
+
+		tRes.Results[query.RefId] = backendDataResponseToTSDBResponse(&res, query.RefId)
+	}
+	return tRes, nil
 }
 
 type Runner struct {
 	client influxdb2.Client
 	org    string
+}
+
+func (r *Runner) runQuery(ctx context.Context, q string) (*influxdb2.QueryTableResult, error) {
+	return r.client.QueryApi(r.org).Query(ctx, q)
 }
 
 func RunnerFromDataSource(dsInfo *models.DataSource) (*Runner, error) {
@@ -60,4 +72,18 @@ func RunnerFromDataSource(dsInfo *models.DataSource) (*Runner, error) {
 		org:    org,
 	}, nil
 
+}
+
+func backendDataResponseToTSDBResponse(dr *backend.DataResponse, refID string) *tsdb.QueryResult {
+	qr := &tsdb.QueryResult{RefId: refID}
+
+	if dr.Error != nil {
+		qr.Error = dr.Error
+		return qr
+	}
+
+	if dr.Frames != nil {
+		qr.Dataframes, qr.Error = dr.Frames.MarshalArrow()
+	}
+	return qr
 }
