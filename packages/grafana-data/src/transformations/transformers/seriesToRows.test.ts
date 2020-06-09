d82dc77de59b3247b8ace3b5708b1ff4b9c119ae
Marcus Andersson <marcus.andersson@grafana.com>
2020-06-09 20:43:12 +0200

    wip: added some refactorings of the seriesToRows transformer.

--- packages/grafana-data/src/transformations/transformers/seriesToRows.test.ts
+++ packages/grafana-data/src/transformations/transformers/seriesToRows.test.ts
@@ -40,7 +40,7 @@ describe('Series to Rows', () => {
       createField('value', FieldType.number, [1, -1]),
     ];
 
-    expect(result[0].fields).toEqual(expected);
+    expect(result[0].fields).toMatchObject(expected);
   });
 
   it('combine three classic time series into one', () => {
@@ -80,8 +80,177 @@ describe('Series to Rows', () => {
       createField('value', FieldType.number, [2, 1, -1]),
     ];
 
-    prettyPrint(result);
-    expect(result[0].fields).toEqual(expected);
+    expect(result[0].fields).toMatchObject(expected);
+  });
+
+  it('combine three time series with multiple values into one', () => {
+    const cfg: DataTransformerConfig<SeriesToRowsOptions> = {
+      id: DataTransformerID.seriesToRows,
+      options: {},
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [1000] },
+        { name: 'temp', type: FieldType.number, values: [1] },
+        { name: 'humidity', type: FieldType.number, values: [10] },
+      ],
+    });
+
+    const seriesB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [1000] },
+        { name: 'temp', type: FieldType.number, values: [-1] },
+      ],
+    });
+
+    const seriesC = toDataFrame({
+      name: 'C',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [500] },
+        { name: 'temp', type: FieldType.number, values: [2] },
+        { name: 'humidity', type: FieldType.number, values: [5] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA, seriesB, seriesC]);
+    const expected: Field[] = [
+      createField('time', FieldType.time, [500, 1000, 1000]),
+      createField('metric', FieldType.string, ['C-series', 'A-series', 'B-series']),
+      createField('temp', FieldType.number, [2, 1, -1]),
+      createField('humidity', FieldType.number, [5, 10, null]),
+    ];
+
+    expect(result[0].fields).toMatchObject(expected);
+  });
+
+  it('combine three time series with multiple values and ISO dates into one', () => {
+    const cfg: DataTransformerConfig<SeriesToRowsOptions> = {
+      id: DataTransformerID.seriesToRows,
+      options: {},
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'time', type: FieldType.time, values: ['2019-10-01T11:10:23Z'] },
+        { name: 'temp', type: FieldType.number, values: [1] },
+        { name: 'humidity', type: FieldType.number, values: [10] },
+      ],
+    });
+
+    const seriesB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'time', type: FieldType.time, values: ['2019-09-01T11:10:23Z'] },
+        { name: 'temp', type: FieldType.number, values: [-1] },
+      ],
+    });
+
+    const seriesC = toDataFrame({
+      name: 'C',
+      fields: [
+        { name: 'time', type: FieldType.time, values: ['2019-11-01T11:10:23Z'] },
+        { name: 'temp', type: FieldType.number, values: [2] },
+        { name: 'humidity', type: FieldType.number, values: [5] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA, seriesB, seriesC]);
+    const expected: Field[] = [
+      createField('time', FieldType.time, ['2019-09-01T11:10:23Z', '2019-10-01T11:10:23Z', '2019-11-01T11:10:23Z']),
+      createField('metric', FieldType.string, ['B-series', 'A-series', 'C-series']),
+      createField('temp', FieldType.number, [-1, 1, 2]),
+      createField('humidity', FieldType.number, [null, 10, 5]),
+    ];
+
+    expect(result[0].fields).toMatchObject(expected);
+  });
+
+  it('combine two classic series with multiple values into one', () => {
+    const cfg: DataTransformerConfig<SeriesToRowsOptions> = {
+      id: DataTransformerID.seriesToRows,
+      options: {},
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [100, 150, 200] },
+        { name: 'temp', type: FieldType.number, values: [1, 4, 5] },
+      ],
+    });
+
+    const seriesB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [100, 125, 126] },
+        { name: 'temp', type: FieldType.number, values: [-1, 2, 3] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA, seriesB]);
+    const expected: Field[] = [
+      createField('time', FieldType.time, [100, 100, 125, 126, 150, 200]),
+      createField('metric', FieldType.string, ['A-series', 'B-series', 'B-series', 'B-series', 'A-series', 'A-series']),
+      createField('value', FieldType.number, [1, -1, 2, 3, 4, 5]),
+    ];
+
+    expect(result[0].fields).toMatchObject(expected);
+  });
+
+  it('combine three series with multiple values into one', () => {
+    const cfg: DataTransformerConfig<SeriesToRowsOptions> = {
+      id: DataTransformerID.seriesToRows,
+      options: {},
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [100, 150, 200] },
+        { name: 'temp', type: FieldType.number, values: [1, 4, 5] },
+        { name: 'humidity', type: FieldType.number, values: [10, 14, 55] },
+      ],
+    });
+
+    const seriesB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [100, 125, 126] },
+        { name: 'temp', type: FieldType.number, values: [-1, 2, 3] },
+      ],
+    });
+
+    const seriesC = toDataFrame({
+      name: 'C',
+      fields: [
+        { name: 'time', type: FieldType.time, values: [100, 124, 149] },
+        { name: 'humidity', type: FieldType.number, values: [22, 25, 30] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA, seriesB, seriesC]);
+    const expected: Field[] = [
+      createField('time', FieldType.time, [100, 100, 100, 124, 125, 126, 149, 150, 200]),
+      createField('metric', FieldType.string, [
+        'A-series',
+        'B-series',
+        'C-series',
+        'C-series',
+        'B-series',
+        'B-series',
+        'C-series',
+        'A-series',
+        'A-series',
+      ]),
+      createField('temp', FieldType.number, [1, -1, null, null, 2, 3, null, 4, 5]),
+      createField('humidity', FieldType.number, [10, null, 22, 25, null, null, 30, 14, 55]),
+    ];
+
+    expect(result[0].fields).toMatchObject(expected);
   });
 });
 
--- packages/grafana-data/src/transformations/transformers/seriesToRows.ts
+++ packages/grafana-data/src/transformations/transformers/seriesToRows.ts
@@ -1,8 +1,10 @@
 import { DataTransformerID } from './ids';
 import { DataTransformerInfo, MatcherConfig } from '../../types/transformations';
-import { DataFrame, Field, FieldType } from '../../types/dataFrame';
+import { DataFrame, FieldType, Field, FieldDTO } from '../../types/dataFrame';
 import { getTimeField, MutableDataFrame, sortDataFrame } from '../../dataframe';
 import { isNumber, omit } from 'lodash';
+import { timeComparer } from '../../field/fieldComparers';
+import { getFieldDisplayName, getFrameDisplayName } from '../../field';
 
 export interface SeriesToRowsOptions {
   fields?: MatcherConfig; // Assume all fields
@@ -19,136 +21,253 @@ export const seriesToRowsTransformer: DataTransformerInfo<SeriesToRowsOptions> =
         return data;
       }
 
-      const timeFieldIndexByFrameIndex = createTimeFieldIndex(data);
+      const timeFields = new TimeFieldsByFrame();
+      const framesStack = new DataFramesStackedByTime(timeFields);
+      const dataFrameBuilder = new DataFrameBuilder(timeFields);
 
-      if (timeFieldNotFoundForAllFrames(timeFieldIndexByFrameIndex, data)) {
+      for (const frame of data) {
+        const frameIndex = framesStack.push(frame);
+        timeFields.add(frameIndex, frame);
+        dataFrameBuilder.addFields(frameIndex, frame);
+      }
+
+      if (data.length !== timeFields.getLength()) {
         return data;
       }
 
-      const frames = data.map((frame, index) => sortDataFrame(frame, timeFieldIndexByFrameIndex[index]));
-      const valueIndexByFrameIndex = createValueFieldIndex(data);
-
-      const result = createMutableDataFrame(timeFieldIndexByFrameIndex, data);
-      const numberOfRows = data.reduce((total, frame) => total + frame.length, 0);
-
-      for (let index = 0; index < numberOfRows; index++) {
-        const frameIndex = frames.reduce((winner, frame, index) => {
-          const winnerTimeField = frames[winner].fields[timeFieldIndexByFrameIndex[winner]];
-          const frameTimeField = frame.fields[timeFieldIndexByFrameIndex[index]];
-
-          const winnerTime = winnerTimeField.values.get(valueIndexByFrameIndex[winner]);
-          const frameTime = frameTimeField.values.get(valueIndexByFrameIndex[index]);
-
-          if (!isNumber(frameTime) || winnerTime <= frameTime) {
-            return winner;
-          }
-          return index;
-        }, 0);
-
-        const frame = frames[frameIndex];
-        const valueIndex = valueIndexByFrameIndex[frameIndex];
-        const timeIndex = timeFieldIndexByFrameIndex[frameIndex];
-        const values = createValuesFromFrame(timeIndex, valueIndex, frame);
-
-        valueIndexByFrameIndex[frameIndex] = valueIndex + 1;
-        result.add(values, true);
+      for (let index = 0; index < framesStack.getLength(); index++) {
+        const { frame, valueIndex, frameIndex } = framesStack.pop();
+        dataFrameBuilder.addValuesAsRow(frame, valueIndex, frameIndex);
       }
 
-      return [result];
+      return [dataFrameBuilder.build()];
     };
   },
 };
+class DataFrameBuilder {
+  private isTimeSeries: boolean;
+  private valueFields: Record<string, FieldDTO>;
+  private timeField: FieldDTO | null;
+  private dataFrames: DataFrame[];
+  private dataFrame: MutableDataFrame | null;
 
-const createTimeFieldIndex = (data: DataFrame[]): Record<number, number> => {
-  return data.reduce((cache: Record<number, number>, frame, index) => {
-    const timeDescription = getTimeField(frame);
-
-    if (isNumber(timeDescription?.timeIndex)) {
-      cache[index] = timeDescription.timeIndex;
-    }
-
-    return cache;
-  }, {});
-};
-
-const createValueFieldIndex = (data: DataFrame[]): Record<number, number> => {
-  return data.reduce((cache: Record<number, number>, frame, index) => {
-    cache[index] = 0;
-    return cache;
-  }, {});
-};
-
-function createValuesFromFrame(timeFieldIndex: number, valueFieldIndex: number, frame: DataFrame): Record<string, any> {
-  const values: Record<string, any> = {
-    metric: `${frame.name}-series`,
-  };
-  const isTimeAndValueFrame = frame.fields.length === 2;
-
-  for (let index = 0; index < frame.fields.length; index++) {
-    const field = frame.fields[index];
-    const value = field.values.get(valueFieldIndex);
-
-    if (index === timeFieldIndex) {
-      values['time'] = value;
-      continue;
-    }
-
-    if (isTimeAndValueFrame) {
-      values['value'] = value;
-      continue;
-    }
-
-    values[field.name] = value;
+  constructor(private timeFields: TimeFieldsByFrame) {
+    this.isTimeSeries = true;
+    this.valueFields = {};
+    this.timeField = null;
+    this.dataFrames = [];
+    this.dataFrame = null;
   }
 
-  return values;
-}
+  addFields(frameIndex: number, frame: DataFrame): void {
+    const timeIndex = this.timeFields.getFieldIndex(frameIndex);
 
-const timeFieldNotFoundForAllFrames = (timeFieldIndex: Record<number, number>, data: DataFrame[]): boolean => {
-  return Object.keys(timeFieldIndex).length !== data.length;
-};
+    if (frame.fields.length > 2) {
+      this.isTimeSeries = false;
+    }
 
-const createMutableDataFrame = (timeFieldByFrame: Record<number, number>, data: DataFrame[]): MutableDataFrame => {
-  const singleValueField = !data.find(frame => frame.fields.length > 2);
-  const dataFrame = new MutableDataFrame();
-  const timeFieldIndex = timeFieldByFrame[0];
-  const timeField = data[0].fields[timeFieldIndex];
+    this.dataFrames.push(frame);
 
-  dataFrame.addField({
-    ...omit(timeField, ['values', 'name']),
-    name: 'time',
-  });
+    for (let index = 0; index < frame.fields.length; index++) {
+      const field = frame.fields[index];
 
-  dataFrame.addField({
-    name: 'metric',
-    type: FieldType.string,
-    config: {},
-  });
+      if (index === timeIndex) {
+        if (!this.timeField) {
+          this.timeField = this.copyStructure(field, 'time');
+        }
+        continue;
+      }
 
-  if (singleValueField) {
-    const valueField = data[0].fields.find((field, index) => index !== timeFieldIndex);
+      const fieldName = field.name ?? getFieldDisplayName(field, frame, this.dataFrames);
 
-    dataFrame.addField({
-      ...omit(valueField, ['values', 'name']),
-      name: 'value',
-    });
+      if (this.valueFields[fieldName]) {
+        continue;
+      }
+
+      this.valueFields[fieldName] = this.copyStructure(field, fieldName);
+    }
+  }
+
+  addValuesAsRow(frame: DataFrame, valueIndex: number, frameIndex: number) {
+    if (!this.dataFrame) {
+      this.dataFrame = this.createDataFrame();
+    }
+
+    const timeIndex = this.timeFields.getFieldIndex(frameIndex);
+
+    const values = frame.fields.reduce((values: Record<string, any>, field, index) => {
+      const value = field.values.get(valueIndex);
+
+      if (index === timeIndex) {
+        values['time'] = value;
+        values['metric'] = `${frame.name}-series`;
+        return values;
+      }
+
+      if (this.isTimeSeries) {
+        values['value'] = value;
+        return values;
+      }
+
+      values[field.name] = value;
+      return values;
+    }, {});
+
+    this.dataFrame.add(values);
+  }
+
+  build(): MutableDataFrame {
+    if (!this.dataFrame) {
+      return new MutableDataFrame();
+    }
+    return this.dataFrame;
+  }
+
+  private createDataFrame(): MutableDataFrame {
+    const dataFrame = new MutableDataFrame();
+
+    if (this.timeField) {
+      dataFrame.addField(this.timeField);
+
+      dataFrame.addField({
+        name: 'metric',
+        type: FieldType.string,
+      });
+    }
+
+    const valueFields = Object.values(this.valueFields);
+
+    if (this.isTimeSeries) {
+      if (valueFields.length > 0) {
+        dataFrame.addField({
+          ...valueFields[0],
+          name: 'value',
+        });
+      }
+
+      return dataFrame;
+    }
+
+    for (const field of valueFields) {
+      dataFrame.addField(field);
+    }
 
     return dataFrame;
   }
 
-  return data.reduce((mutableFrame: MutableDataFrame, frame, frameIndex) => {
-    const timeIndex = timeFieldByFrame[frameIndex];
+  private copyStructure(field: Field, name: string): FieldDTO {
+    return {
+      ...omit(field, ['values', 'name']),
+      name,
+    };
+  }
+}
 
-    return frame.fields.reduce((mutableFrame: MutableDataFrame, field, fieldIndex) => {
-      if (fieldIndex === timeIndex) {
-        return mutableFrame;
-      }
+interface DataFrameStackValue {
+  valueIndex: number;
+  frameIndex: number;
+  frame: DataFrame;
+}
+class DataFramesStackedByTime {
+  private valuesPointerByFrame: Record<number, number>;
+  private dataFrames: DataFrame[];
+  private isSorted: boolean;
 
-      mutableFrame.addField({
-        ...omit(field, ['values']),
-      });
+  constructor(private timeFields: TimeFieldsByFrame) {
+    this.valuesPointerByFrame = {};
+    this.dataFrames = [];
+    this.isSorted = false;
+  }
 
-      return mutableFrame;
-    }, mutableFrame);
-  }, dataFrame);
-};
+  push(frame: DataFrame): number {
+    const index = this.dataFrames.length;
+    this.valuesPointerByFrame[index] = 0;
+    this.dataFrames.push(frame);
+    return index;
+  }
+
+  pop(): DataFrameStackValue {
+    if (!this.isSorted) {
+      this.sortByTime();
+      this.isSorted = true;
+    }
+
+    const frameIndex = this.dataFrames.reduce((champion, frame, index) => {
+      const championTime = this.peekTimeValueForFrame(champion);
+      const contenderTime = this.peekTimeValueForFrame(index);
+      return timeComparer(contenderTime, championTime) >= 0 ? champion : index;
+    }, 0);
+
+    const previousPointer = this.movePointerForward(frameIndex);
+
+    return {
+      frame: this.dataFrames[frameIndex],
+      valueIndex: previousPointer,
+      frameIndex: frameIndex,
+    };
+  }
+
+  getLength(): number {
+    const frames = Object.values(this.dataFrames);
+    return frames.reduce((length: number, frame) => (length += frame.length), 0);
+  }
+
+  private peekTimeValueForFrame(frameIndex: number): any {
+    const timeField = this.timeFields.getField(frameIndex);
+    const valuePointer = this.valuesPointerByFrame[frameIndex];
+    return timeField.values.get(valuePointer);
+  }
+
+  private movePointerForward(frameIndex: number): number {
+    const currentPointer = this.valuesPointerByFrame[frameIndex];
+    this.valuesPointerByFrame[frameIndex] = currentPointer + 1;
+
+    return currentPointer;
+  }
+
+  private sortByTime() {
+    this.dataFrames = this.dataFrames.map((frame, index) => {
+      const timeFieldIndex = this.timeFields.getFieldIndex(index);
+      return sortDataFrame(frame, timeFieldIndex);
+    });
+  }
+}
+class TimeFieldsByFrame {
+  private timeIndexByFrameIndex: Record<number, number>;
+  private timeFieldByFrameIndex: Record<number, Field>;
+
+  constructor() {
+    this.timeIndexByFrameIndex = {};
+    this.timeFieldByFrameIndex = {};
+  }
+
+  add(frameIndex: number, frame: DataFrame): void {
+    const fieldDescription = getTimeField(frame);
+    const timeIndex = fieldDescription?.timeIndex;
+    const timeField = fieldDescription?.timeField;
+
+    if (isNumber(timeIndex)) {
+      this.timeIndexByFrameIndex[frameIndex] = timeIndex;
+    }
+
+    if (timeField) {
+      this.timeFieldByFrameIndex[frameIndex] = timeField;
+    }
+  }
+
+  getField(frameIndex: number): Field {
+    return this.timeFieldByFrameIndex[frameIndex];
+  }
+
+  getFieldIndex(frameIndex: number): number {
+    return this.timeIndexByFrameIndex[frameIndex];
+  }
+
+  has(frameIndex: number): boolean {
+    return typeof this.timeFieldByFrameIndex[frameIndex] === 'number';
+  }
+
+  getLength() {
+    return Object.keys(this.timeIndexByFrameIndex).length;
+  }
+}
