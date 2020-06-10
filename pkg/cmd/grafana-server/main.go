39228940dafd15937b7d1f0f73c75f3d695bf740
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-06-10 18:28:03 +0200

    Remove testdatasourcev2 added temporary for now

--- pkg/cmd/grafana-server/main.go
+++ pkg/cmd/grafana-server/main.go
@@ -30,7 +30,6 @@ import (
 	_ "github.com/grafana/grafana/pkg/tsdb/prometheus"
 	_ "github.com/grafana/grafana/pkg/tsdb/stackdriver"
 	_ "github.com/grafana/grafana/pkg/tsdb/testdatasource"
-	_ "github.com/grafana/grafana/pkg/tsdb/testdatasourcev2"
 )
 
 var version = "5.0.0"
