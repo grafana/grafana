2c96d8ddb89913fd0227ec947c7c469435c543fa
Marcus Andersson <marcus.andersson@grafana.com>
2020-06-11 12:35:46 +0200

    fixed so we don't keep labels or config values from.

--- packages/grafana-data/src/transformations/transformers/merge/DataFrameBuilder.ts
+++ packages/grafana-data/src/transformations/transformers/merge/DataFrameBuilder.ts
@@ -1,5 +1,11 @@
 import { MutableDataFrame } from '../../../dataframe';
-import { DataFrame, FieldType, Field } from '../../../types/dataFrame';
+import {
+  DataFrame,
+  FieldType,
+  Field,
+  TIME_SERIES_TIME_FIELD_NAME,
+  TIME_SERIES_VALUE_FIELD_NAME,
+} from '../../../types/dataFrame';
 import { ArrayVector } from '../../../vector';
 import { omit } from 'lodash';
 import { getFrameDisplayName } from '../../../field';
@@ -11,11 +17,7 @@ interface DataFrameBuilderResult {
 
 type ValueMapper = (frame: DataFrame, valueIndex: number, timeIndex: number) => Record<string, any>;
 
-const fieldNames = {
-  metric: 'Metric',
-  time: 'Time',
-  value: 'Value',
-};
+const TIME_SERIES_METRIC_FIELD_NAME = 'Metric';
 
 export class DataFrameBuilder {
   private isOnlyTimeSeries: boolean;
@@ -44,7 +46,7 @@ export class DataFrameBuilder {
 
       if (index === timeIndex) {
         if (!this.timeField) {
-          this.timeField = this.copyStructure(field, fieldNames.time);
+          this.timeField = this.copyStructure(field, TIME_SERIES_TIME_FIELD_NAME);
         }
         continue;
       }
@@ -68,16 +70,16 @@ export class DataFrameBuilder {
         const value = field.values.get(valueIndex);
 
         if (index === timeIndex) {
-          values[fieldNames.time] = value;
+          values[TIME_SERIES_TIME_FIELD_NAME] = value;
 
           if (this.displayMetricField) {
-            values[fieldNames.metric] = getFrameDisplayName(frame);
+            values[TIME_SERIES_METRIC_FIELD_NAME] = getFrameDisplayName(frame);
           }
           return values;
         }
 
         if (this.isOnlyTimeSeries) {
-          values[fieldNames.value] = value;
+          values[TIME_SERIES_VALUE_FIELD_NAME] = value;
           return values;
         }
 
@@ -95,7 +97,7 @@ export class DataFrameBuilder {
 
       if (this.displayMetricField) {
         dataFrame.addField({
-          name: fieldNames.metric,
+          name: TIME_SERIES_METRIC_FIELD_NAME,
           type: FieldType.string,
         });
       }
@@ -107,10 +109,9 @@ export class DataFrameBuilder {
       if (valueFields.length > 0) {
         dataFrame.addField({
           ...valueFields[0],
-          name: fieldNames.value,
+          name: TIME_SERIES_VALUE_FIELD_NAME,
         });
       }
-
       return dataFrame;
     }
 
@@ -123,9 +124,12 @@ export class DataFrameBuilder {
 
   private copyStructure(field: Field, name: string): Field {
     return {
-      ...omit(field, ['values', 'name']),
+      ...omit(field, ['values', 'name', 'state', 'labels', 'config']),
       name,
       values: new ArrayVector(),
+      config: {
+        ...omit(field.config, 'displayName'),
+      },
     };
   }
 }
--- packages/grafana-data/src/transformations/transformers/merge/merge.test.ts
+++ packages/grafana-data/src/transformations/transformers/merge/merge.test.ts
@@ -1,5 +1,5 @@
 import { mockTransformationsRegistry } from '../../../utils/tests/mockTransformationsRegistry';
-import { DataTransformerConfig, Field, FieldType } from '../../../types';
+import { DataTransformerConfig, Field, FieldType, DataFrame } from '../../../types';
 import { DataTransformerID } from '../ids';
 import { toDataFrame } from '../../../dataframe';
 import { transformDataFrame } from '../../transformDataFrame';
@@ -244,8 +244,107 @@ describe('Merge multipe to single', () => {
 
     expect(result[0].fields).toMatchObject(expected);
   });
+
+  it('combine two time series, where first serie fields has displayName, into one', () => {
+    const cfg: DataTransformerConfig<MergeTransformerOptions> = {
+      id: DataTransformerID.merge,
+      options: {},
+    };
+
+    const serieA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [100, 150, 200], config: { displayName: 'Random time' } },
+        { name: 'Temp', type: FieldType.number, values: [1, 4, 5], config: { displayName: 'Temp' } },
+      ],
+    });
+
+    const serieB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [100, 125, 126] },
+        { name: 'Temp', type: FieldType.number, values: [-1, 2, 3] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [serieA, serieB]);
+    const expected: Field[] = [
+      createField('Time', FieldType.time, [100, 100, 125, 126, 150, 200]),
+      createField('Metric', FieldType.string, ['A', 'B', 'B', 'B', 'A', 'A']),
+      createField('Value', FieldType.number, [1, -1, 2, 3, 4, 5]),
+    ];
+
+    expect(result[0].fields[2].config).toEqual({});
+    expect(result[0].fields).toMatchObject(expected);
+  });
+
+  it('combine two time series, where first serie fields has units, into one', () => {
+    const cfg: DataTransformerConfig<MergeTransformerOptions> = {
+      id: DataTransformerID.merge,
+      options: {},
+    };
+
+    const serieA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [100, 150, 200] },
+        { name: 'Temp', type: FieldType.number, values: [1, 4, 5], config: { units: 'celsius' } },
+      ],
+    });
+
+    const serieB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [100, 125, 126] },
+        { name: 'Temp', type: FieldType.number, values: [-1, 2, 3] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [serieA, serieB]);
+    const expected: Field[] = [
+      createField('Time', FieldType.time, [100, 100, 125, 126, 150, 200]),
+      createField('Metric', FieldType.string, ['A', 'B', 'B', 'B', 'A', 'A']),
+      createField('Value', FieldType.number, [1, -1, 2, 3, 4, 5], { units: 'celsius' }),
+    ];
+
+    expect(result[0].fields[2].config).toEqual({ units: 'celsius' });
+    expect(result[0].fields).toMatchObject(expected);
+  });
+
+  it('combine two time series, where second serie fields has units, into one', () => {
+    const cfg: DataTransformerConfig<MergeTransformerOptions> = {
+      id: DataTransformerID.merge,
+      options: {},
+    };
+
+    const serieA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [100, 150, 200] },
+        { name: 'Temp', type: FieldType.number, values: [1, 4, 5] },
+      ],
+    });
+
+    const serieB = toDataFrame({
+      name: 'B',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [100, 125, 126] },
+        { name: 'Temp', type: FieldType.number, values: [-1, 2, 3], config: { units: 'celsius' } },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [serieA, serieB]);
+    const expected: Field[] = [
+      createField('Time', FieldType.time, [100, 100, 125, 126, 150, 200]),
+      createField('Metric', FieldType.string, ['A', 'B', 'B', 'B', 'A', 'A']),
+      createField('Value', FieldType.number, [1, -1, 2, 3, 4, 5]),
+    ];
+
+    expect(result[0].fields[2].config).toEqual({});
+    expect(result[0].fields).toMatchObject(expected);
+  });
 });
 
-const createField = (name: string, type: FieldType, values: any[]): Field => {
-  return { name, type, values: new ArrayVector(values), config: {}, labels: undefined };
+const createField = (name: string, type: FieldType, values: any[], config = {}): Field => {
+  return { name, type, values: new ArrayVector(values), config, labels: undefined };
 };
