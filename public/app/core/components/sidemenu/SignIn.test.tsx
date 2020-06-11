cbb8733d77e102672f1a002f4e69930d271d672e
Sofia Papagiannaki <papagian@gmail.com>
2020-06-11 19:23:16 +0300

    Modify SignIn page

--- public/app/core/components/sidemenu/SignIn.test.tsx
+++ public/app/core/components/sidemenu/SignIn.test.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { SignIn } from './SignIn';
+import { SignIn, SignInPageProps } from './SignIn';
+import { updateLocation } from 'app/core/actions';
+import { mockToolkitActionCreator } from 'test/core/redux/mocks';
+
+const setup = (propOverrides?: object) => {
+  const props: SignInPageProps = {
+    url: '/',
+    updateLocation: mockToolkitActionCreator(updateLocation),
+  };
+
+  Object.assign(props, propOverrides);
+
+  const wrapper = shallow(<SignIn {...props} />);
+
+  return {
+    wrapper,
+  };
+};
 
 describe('Render', () => {
   it('should render component', () => {
-    const wrapper = shallow(<SignIn url="/" />);
+    const { wrapper } = setup();
 
     expect(wrapper).toMatchSnapshot();
   });
--- public/app/core/components/sidemenu/SignIn.tsx
+++ public/app/core/components/sidemenu/SignIn.tsx
@@ -1,18 +1,37 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+import { MapDispatchToProps } from 'react-redux';
 import { connectWithStore } from 'app/core/utils/connectWithReduxStore';
 import { StoreState } from 'app/types';
 import { Icon } from '@grafana/ui';
+import { updateLocation } from 'app/core/actions';
 
-export const SignIn: FC<any> = ({ url }) => {
-  const loginUrl = `login?redirect=${encodeURIComponent(url)}`;
+interface DispatchProps {
+  updateLocation: typeof updateLocation;
+}
+
+export interface SignInPageProps {
+  url: any;
+  updateLocation: typeof updateLocation;
+}
+
+export type Props = SignInPageProps & DispatchProps;
+
+export const SignIn: FC<SignInPageProps> = ({ url, updateLocation }) => {
+  const onClick = useCallback(() => {
+    updateLocation({
+      path: url,
+      query: { forceLogin: 'true' },
+    });
+  }, []);
+
   return (
     <div className="sidemenu-item">
-      <a href={loginUrl} className="sidemenu-link" target="_self">
+      <a onClick={onClick} className="sidemenu-link" target="_self">
         <span className="icon-circle sidemenu-icon">
           <Icon name="sign-in-alt" size="xl" />
         </span>
       </a>
-      <a href={loginUrl} target="_self">
+      <a onClick={onClick} target="_self">
         <ul className="dropdown-menu dropdown-menu--sidemenu" role="menu">
           <li className="side-menu-header">
             <span className="sidemenu-item-text">Sign In</span>
@@ -27,4 +46,8 @@ const mapStateToProps = (state: StoreState) => ({
   url: state.location.url,
 });
 
-export default connectWithStore(SignIn, mapStateToProps);
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, SignInPageProps> = {
+  updateLocation,
+};
+
+export default connectWithStore(SignIn, mapStateToProps, mapDispatchToProps);
