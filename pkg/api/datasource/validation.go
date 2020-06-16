28b0033ae99d167e044cacfe7d8a475bb5995cd1
Arve Knudsen <arve.knudsen@gmail.com>
2020-06-16 13:24:29 +0200

    Move MSSQL URL validation into mssql package
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/api/datasource/validation.go
+++ pkg/api/datasource/validation.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/tsdb/mssql"
 )
 
 var logger = log.New("datasource")
@@ -37,44 +38,26 @@ var reURL = regexp.MustCompile("^[^:]*://")
 // The data source's type and URL must be provided. If successful, the valid URL object is returned, otherwise an
 // error is returned.
 func ValidateURL(typeName, urlStr string) (*url.URL, error) {
+	var u *url.URL
+	var err error
 	switch strings.ToLower(typeName) {
 	case "mssql":
-		return validateMSSQLURL(urlStr)
+		u, err = mssql.ParseURL(urlStr)
 	default:
 		logger.Debug("Applying default URL parsing for this data source type", "type", typeName, "url", urlStr)
-	}
 
-	// Make sure the URL starts with a protocol specifier, so parsing is unambiguous
-	if !reURL.MatchString(urlStr) {
-		logger.Debug(
-			"Data source URL doesn't specify protocol, so prepending it with http:// in order to make it unambiguous",
-			"type", typeName, "url", urlStr)
-		urlStr = fmt.Sprintf("http://%s", urlStr)
+		// Make sure the URL starts with a protocol specifier, so parsing is unambiguous
+		if !reURL.MatchString(urlStr) {
+			logger.Debug(
+				"Data source URL doesn't specify protocol, so prepending it with http:// in order to make it unambiguous",
+				"type", typeName, "url", urlStr)
+			urlStr = fmt.Sprintf("http://%s", urlStr)
+		}
+		u, err = url.Parse(urlStr)
 	}
-	u, err := url.Parse(urlStr)
 	if err != nil {
 		return nil, URLValidationError{error: err, url: urlStr}
 	}
 
 	return u, nil
-}
-
-func validateMSSQLURL(u string) (*url.URL, error) {
-	logger.Debug("Validating MSSQL URL", "url", u)
-
-	// Recognize ODBC connection strings like host\instance:1234
-	reODBC := regexp.MustCompile(`^[^\\]+(:?\\[^:]+)?(:?:\d+)?$`)
-	var host string
-	switch {
-	case reODBC.MatchString(u):
-		logger.Debug("Recognized as ODBC URL format", "url", u)
-		host = u
-	default:
-		logger.Debug("Couldn't recognize as valid MSSQL URL", "url", u)
-		return nil, fmt.Errorf("unrecognized MSSQL URL format: %q", u)
-	}
-	return &url.URL{
-		Scheme: "sqlserver",
-		Host:   host,
-	}, nil
 }
