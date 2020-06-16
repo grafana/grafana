3547230e282b62f22e615dd19cc9f87c9482732b
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-06-16 07:20:21 +0200

    Chore: styles the code editor box a bit

--- public/app/plugins/panel/text/TextPanelEditor.tsx
+++ public/app/plugins/panel/text/TextPanelEditor.tsx
@@ -1,9 +1,26 @@
 import React, { FC, useMemo } from 'react';
-import { CodeEditor } from '@grafana/ui';
-import { StandardEditorProps } from '@grafana/data';
+import { css, cx } from 'emotion';
+import { CodeEditor, stylesFactory, useTheme } from '@grafana/ui';
+import { GrafanaTheme, StandardEditorProps } from '@grafana/data';
+
 import { TextOptions } from './types';
 
 export const TextPanelEditor: FC<StandardEditorProps<string, any, TextOptions>> = ({ value, onChange, context }) => {
   const language = useMemo(() => context.options?.mode ?? 'markdown', [context]);
-  return <CodeEditor value={value} onChange={onChange} language={language} width="100%" height="150px" />;
+  const theme = useTheme();
+  const styles = getStyles(theme);
+  return (
+    <div className={cx(styles.editorBox)}>
+      <CodeEditor value={value} onChange={onChange} language={language} width="100%" height="150px" />
+    </div>
+  );
 };
+
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  editorBox: css`
+    label: editorBox;
+    border: ${theme.border.width.sm} solid ${theme.colors.border2};
+    border-radius: ${theme.border.radius.sm};
+    margin: ${theme.spacing.xs} 0;
+  `,
+}));
