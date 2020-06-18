b346c5661f5ac7dc5f7417bee6272e48c1441d24
Dominik Prokop <dominik.prokop@grafana.com>
2020-06-18 13:43:16 +0200

    Update usePanelLatestData hook

--- public/app/features/dashboard/components/PanelEditor/usePanelLatestData.ts
+++ public/app/features/dashboard/components/PanelEditor/usePanelLatestData.ts
@@ -31,7 +31,11 @@ export const usePanelLatestData = (panel: PanelModel, options: GetDataOptions): 
         querySubscription.current.unsubscribe();
       }
     };
-  }, [panel, options]);
+    /**
+     * Adding separate options to dependencies array to avoid additional hook for comparing previous options with current.
+     * Otherwise, passing different references to the same object may cause troubles.
+     */
+  }, [panel, options.withFieldConfig, options.withTransforms]);
 
   return {
     data: latestData,
