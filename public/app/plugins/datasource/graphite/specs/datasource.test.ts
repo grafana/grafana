dfb6cff146983aef1f5ac476cb08fa477c12aeae
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-06-18 11:35:11 +0200

    Chore: Upgrades jQuery and @types/jQuery (#25651)
    * Chore: upgrades jQuery and @types/jQuery
    
    * Chore: reduce strict null errors
    
    * Fixed issue in graphite func editor
    
    Co-authored-by: Torkel Ödegaard <torkel@grafana.com>

--- public/app/plugins/datasource/graphite/specs/datasource.test.ts
+++ public/app/plugins/datasource/graphite/specs/datasource.test.ts
@@ -6,7 +6,7 @@ import { dateTime, getFrameDisplayName } from '@grafana/data';
 import { backendSrv } from 'app/core/services/backend_srv'; // will use the version in __mocks__
 
 jest.mock('@grafana/runtime', () => ({
-  ...jest.requireActual('@grafana/runtime'),
+  ...((jest.requireActual('@grafana/runtime') as unknown) as object),
   getBackendSrv: () => backendSrv,
 }));
 
