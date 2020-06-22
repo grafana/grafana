c16890c22df98695aa209424cb4e22d8d257cd52
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-06-22 16:15:29 +0200

    Chore: use fork-ts-checker plugin for eslint check (#25631)
    * Use fork-ts-checker for eslint check
    
    * Fix lint issue
    
    * Add packages to lint
    
    * Add comments to webpack configs

--- scripts/webpack/dependencies.js
+++ scripts/webpack/dependencies.js
@@ -6,7 +6,7 @@ const pull = require('lodash/pull');
 let dependencies = Object.keys(pkg.dependencies);
 // remove jquery so we can add it first
 // remove rxjs so we can only depend on parts of it in code
-pull(dependencies, 'jquery', 'rxjs')
+pull(dependencies, 'jquery', 'rxjs');
 
 // add jquery first
 dependencies.unshift('jquery');
