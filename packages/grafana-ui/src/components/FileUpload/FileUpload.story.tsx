11f30579f22609b27090d08ec5a83dd170acbf65
Clarity-89 <homes89@ukr.net>
2020-06-25 13:45:30 +0300

    Grafana UI: Setup component

---
+++ packages/grafana-ui/src/components/FileUpload/FileUpload.story.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FileUpload } from './FileUpload';
+import { withCenteredStory } from '@grafana/ui/src/utils/storybook/withCenteredStory';
+import mdx from './FileUpload.mdx';
+
+export default {
+  title: 'Forms/FileUpload',
+  component: FileUpload,
+  decorators: [withCenteredStory],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+export const single = () => {
+  return <FileUpload />;
+};
---
+++ packages/grafana-ui/src/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+export interface Props {}
+
+export const FileUpload: FC<Props> = props => {
+  return <div>Hello world!</div>;
+};
