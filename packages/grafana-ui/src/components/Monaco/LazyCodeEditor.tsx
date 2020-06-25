69ea9c6aa161d712f84098402c6788619e78ad42
Ryan McKinley <ryantxu@gmail.com>
2020-06-25 10:55:30 -0700

    all async

---
+++ packages/grafana-ui/src/components/Monaco/LazyCodeEditor.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+
+import { CodeEditorProps } from './CodeEditor';
+import * as monaco from 'monaco-editor';
+
+// @ts-ignore
+self.MonacoEnvironment = {
+  getWorkerUrl: (moduleId: string, label: string) => {
+    console.log('MonacoEnvironment GET WORKER URL', moduleId, label);
+    if (label === 'json') {
+      return './monaco.json.worker.bundle.js';
+    }
+    if (label === 'css') {
+      return './monaco.css.worker.bundle.js';
+    }
+    if (label === 'html') {
+      return './monaco.html.worker.bundle.js';
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return './monaco.ts.worker.bundle.js';
+    }
+    return './monaco.editor.worker.bundle.js';
+  },
+};
+
+export default class LazyCodeEditor extends React.PureComponent<CodeEditorProps> {
+  constructor(props: CodeEditorProps) {
+    super(props);
+  }
+
+  getEditorValue = () => '';
+
+  onBlur = () => {
+    const { onBlur } = this.props;
+    if (onBlur) {
+      onBlur(this.getEditorValue());
+    }
+  };
+
+  editorDidMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
+    const { onSave, onEditorDidMount } = this.props;
+
+    this.getEditorValue = () => editor.getValue();
+
+    if (onSave) {
+      editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, () => {
+        onSave(this.getEditorValue());
+      });
+    }
+
+    if (onEditorDidMount) {
+      onEditorDidMount(editor);
+    }
+  };
+
+  render() {
+    const { theme, language, width, height, showMiniMap, readOnly } = this.props;
+    const value = this.props.value ?? '';
+    const longText = value.length > 100;
+
+    return (
+      <div onBlur={this.onBlur}>
+        <MonacoEditor
+          width={width}
+          height={height}
+          language={language}
+          theme={theme.isDark ? 'vs-dark' : 'vs-light'}
+          value={value}
+          options={{
+            wordWrap: 'off',
+            codeLens: false, // not included in the bundle
+            minimap: {
+              enabled: longText && showMiniMap,
+              renderCharacters: false,
+            },
+            readOnly,
+            lineNumbersMinChars: 4,
+            lineDecorationsWidth: 0,
+            overviewRulerBorder: false,
+            automaticLayout: true,
+          }}
+          editorDidMount={this.editorDidMount}
+        />
+      </div>
+    );
+  }
+}
