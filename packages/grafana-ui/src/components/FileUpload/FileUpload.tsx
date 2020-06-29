a5d904989217e3a26a6adcacdcd37f8cbdee1b5a
Clarity-89 <homes89@ukr.net>
2020-06-29 10:03:23 +0300

    Grafana UI: Show uploaded file name

--- packages/grafana-ui/src/components/FileUpload/FileUpload.tsx
+++ packages/grafana-ui/src/components/FileUpload/FileUpload.tsx
@@ -1,4 +1,4 @@
-import React, { FC, FormEvent } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
 import { GrafanaTheme } from '@grafana/data';
 import { css, cx } from 'emotion';
 import { getFormStyles, Icon } from '../index';
@@ -13,23 +13,48 @@ export interface Props {
   className?: string;
 }
 
+function trimFileName(fileName: string) {
+  const nameLength = 16;
+  const delimiter = fileName.lastIndexOf('.');
+  const extension = fileName.substring(delimiter);
+  const file = fileName.substring(0, delimiter);
+
+  if (file.length < nameLength) {
+    return file;
+  }
+
+  return file.substring(0, nameLength) + '...' + extension;
+}
+
 export const FileUpload: FC<Props> = ({ onFileUpload, className, label = 'Upload file', accept = '*' }) => {
   const theme = useTheme();
   const style = getStyles(theme);
+  const [fileName, setFileName] = useState('');
+
+  const onChange = (event: FormEvent<HTMLInputElement>) => {
+    const file = event.currentTarget?.files?.[0];
+    if (file) {
+      setFileName(file.name ?? '');
+    }
+    onFileUpload(event);
+  };
 
   return (
-    <label className={cx(style.button, className)}>
-      <Icon name="upload" className={style.icon} />
-      {label}
-      <input
-        type="file"
-        id="fileUpload"
-        className={style.fileUpload}
-        onChange={onFileUpload}
-        multiple={false}
-        accept={accept}
-      />
-    </label>
+    <>
+      <label className={cx(style.button, className)}>
+        <Icon name="upload" className={style.icon} />
+        {label}
+        <input
+          type="file"
+          id="fileUpload"
+          className={style.fileUpload}
+          onChange={onChange}
+          multiple={false}
+          accept={accept}
+        />
+      </label>
+      <span className={style.fileName}>{fileName && trimFileName(fileName)}</span>
+    </>
   );
 };
 
@@ -44,6 +69,9 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
     `,
     icon: css`
       margin-right: ${theme.spacing.xs};
+    `,
+    fileName: css`
+      margin-left: ${theme.spacing.xs};
     `,
   };
 });
