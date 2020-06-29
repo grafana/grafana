1a711e7df034c4744a55d927618ba2451d0430e3
Ryan McKinley <ryantxu@gmail.com>
2020-06-29 10:58:47 -0700

    Panel Inspect: use monaco for json display (#25251)

--- packages/grafana-ui/rollup.config.ts
+++ packages/grafana-ui/rollup.config.ts
@@ -25,7 +25,16 @@ const buildCjsPackage = ({ env }) => {
         },
       },
     ],
-    external: ['react', 'react-dom', '@grafana/data', 'moment', '@grafana/e2e-selectors'],
+    external: [
+      'react',
+      'react-dom',
+      '@grafana/data',
+      '@grafana/e2e-selectors',
+      'moment',
+      'monaco-editor', // Monaco should not be used directly
+      'monaco-editor/esm/vs/editor/editor.api', // Monaco should not be used directly
+      'react-monaco-editor',
+    ],
     plugins: [
       commonjs({
         include: /node_modules/,
--- public/app/features/dashboard/components/Inspector/InspectJSONTab.tsx
+++ public/app/features/dashboard/components/Inspector/InspectJSONTab.tsx
@@ -1,9 +1,9 @@
 import React, { PureComponent } from 'react';
 import { chain } from 'lodash';
 import { AppEvents, PanelData, SelectableValue } from '@grafana/data';
-import { Button, ClipboardButton, Field, JSONFormatter, Select, TextArea } from '@grafana/ui';
+import { Button, CodeEditor, Field, Select } from '@grafana/ui';
+import AutoSizer from 'react-virtualized-auto-sizer';
 import { selectors } from '@grafana/e2e-selectors';
-
 import { appEvents } from 'app/core/core';
 import { DashboardModel, PanelModel } from '../../state';
 import { getPanelInspectorStyles } from './styles';
@@ -49,20 +49,18 @@ export class InspectJSONTab extends PureComponent<Props, State> {
     super(props);
     this.state = {
       show: ShowContent.PanelJSON,
-      text: getSaveModelJSON(props.panel),
+      text: getPrettyJSON(props.panel.getSaveModel()),
     };
   }
 
   onSelectChanged = (item: SelectableValue<ShowContent>) => {
-    let text = '';
-    if (item.value === ShowContent.PanelJSON) {
-      text = getSaveModelJSON(this.props.panel);
-    }
+    const show = this.getJSONObject(item.value);
+    const text = getPrettyJSON(show);
     this.setState({ text, show: item.value });
   };
 
-  onTextChanged = (e: React.FormEvent<HTMLTextAreaElement>) => {
-    const text = e.currentTarget.value;
+  // Called onBlur
+  onTextChanged = (text: string) => {
     this.setState({ text });
   };
 
@@ -93,17 +91,7 @@ export class InspectJSONTab extends PureComponent<Props, State> {
       return this.props.panel.getSaveModel();
     }
 
-    return { note: 'Unknown Object', show };
-  };
-
-  getClipboardText = () => {
-    const { show } = this.state;
-    const obj = this.getJSONObject(show);
-    return JSON.stringify(obj, null, 2);
-  };
-
-  onClipboardCopied = () => {
-    appEvents.emit(AppEvents.alertSuccess, ['Content copied to clipboard']);
+    return { note: `Unknown Object: ${show}` };
   };
 
   onApplyPanelModel = () => {
@@ -126,12 +114,6 @@ export class InspectJSONTab extends PureComponent<Props, State> {
     onClose();
   };
 
-  renderPanelJSON(styles: any) {
-    return (
-      <TextArea spellCheck={false} value={this.state.text} onChange={this.onTextChanged} className={styles.editor} />
-    );
-  }
-
   render() {
     const { dashboard } = this.props;
     const { show } = this.state;
@@ -146,14 +128,6 @@ export class InspectJSONTab extends PureComponent<Props, State> {
           <Field label="Select source" className="flex-grow-1">
             <Select options={options} value={selected} onChange={this.onSelectChanged} />
           </Field>
-          <ClipboardButton
-            variant="secondary"
-            className={styles.toolbarItem}
-            getText={this.getClipboardText}
-            onClipboardCopy={this.onClipboardCopied}
-          >
-            Copy to clipboard
-          </ClipboardButton>
           {isPanelJSON && canEdit && (
             <Button className={styles.toolbarItem} onClick={this.onApplyPanelModel}>
               Apply
@@ -161,19 +135,24 @@ export class InspectJSONTab extends PureComponent<Props, State> {
           )}
         </div>
         <div className={styles.content}>
-          {isPanelJSON ? (
-            this.renderPanelJSON(styles)
-          ) : (
-            <div className={styles.viewer}>
-              <JSONFormatter json={this.getJSONObject(show)} />
-            </div>
-          )}
+          <AutoSizer disableWidth>
+            {({ height }) => (
+              <CodeEditor
+                width="100%"
+                height={height}
+                language="json"
+                value={this.state.text}
+                readOnly={!isPanelJSON}
+                onBlur={this.onTextChanged}
+              />
+            )}
+          </AutoSizer>
         </div>
       </>
     );
   }
 }
 
-function getSaveModelJSON(panel: PanelModel): string {
-  return JSON.stringify(panel.getSaveModel(), null, 2);
+function getPrettyJSON(obj: any): string {
+  return JSON.stringify(obj, null, 2);
 }
