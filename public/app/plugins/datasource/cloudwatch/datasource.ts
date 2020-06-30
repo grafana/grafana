2a0d01927a098341f769e898e72ae0f59498ffce
kay delaney <kay@grafana.com>
2020-06-30 12:35:17 +0100

    Add visualisation hints to elasticsearch and cloudwatch

--- public/app/plugins/datasource/cloudwatch/datasource.ts
+++ public/app/plugins/datasource/cloudwatch/datasource.ts
@@ -79,7 +79,7 @@ export class CloudWatchDatasource extends DataSourceApi<CloudWatchQuery, CloudWa
   datasourceName: string;
   debouncedAlert: (datasourceName: string, region: string) => void;
   debouncedCustomAlert: (title: string, message: string) => void;
-  logQueries: Record<string, { id: string; region: string }>;
+  logQueries: Record<string, { id: string; region: string; statsQuery: boolean }>;
   languageProvider: CloudWatchLanguageProvider;
 
   /** @ngInject */
@@ -228,7 +228,11 @@ export class CloudWatchDatasource extends DataSourceApi<CloudWatchQuery, CloudWa
   ): Observable<DataQueryResponse> {
     this.logQueries = {};
     queryParams.forEach(param => {
-      this.logQueries[param.refId] = { id: param.queryId, region: param.region };
+      this.logQueries[param.refId] = {
+        id: param.queryId,
+        region: param.region,
+        statsQuery: param.statsGroups?.length > 0 ?? false,
+      };
     });
     let prevRecordsMatched: Record<string, number> = {};
 
@@ -249,6 +253,10 @@ export class CloudWatchDatasource extends DataSourceApi<CloudWatchQuery, CloudWa
                       moreRecordsMatched = true;
                     }
                     prevRecordsMatched[frame.refId!] = recordsMatched;
+
+                    if (!this.logQueries[frame.refId].statsQuery) {
+                      _.set(frame, 'meta.preferredVisualisationType', 'logs');
+                    }
                   }
                   const noProgressMade = i >= MAX_ATTEMPTS - 2 && !moreRecordsMatched;
                   if (noProgressMade) {
