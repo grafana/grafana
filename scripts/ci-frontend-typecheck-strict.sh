5390216b0395dc9b6e5d31393c9aa39892e46116
Dominik Prokop <dominik.prokop@grafana.com>
2020-07-01 12:54:13 +0200

    Run strict typecheck on PRs

---
+++ scripts/ci-frontend-typecheck-strict.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "Typechecking with strict null checks enabled"
+
+ERROR_COUNT_LIMIT=700
+
+ERROR_COUNT="$(./node_modules/.bin/tsc --project tsconfig.json --noEmit --strictNullChecks true | grep -oP 'Found \K(\d+)')"
+
+if [ "$ERROR_COUNT" -gt $ERROR_COUNT_LIMIT ]; then
+  echo -e "Typescript strict errors $ERROR_COUNT exceeded $ERROR_COUNT_LIMIT so failing build"
+	exit 1
+fi
