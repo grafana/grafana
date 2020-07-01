a86e61515b5c44cd9da71817467feb3bb882d115
Dominik Prokop <dominik.prokop@grafana.com>
2020-07-01 13:33:34 +0200

    Fail fast with basic issues

--- scripts/circle-test-frontend.sh
+++ scripts/circle-test-frontend.sh
@@ -3,6 +3,8 @@
 # shellcheck source=./scripts/helpers/exit-if-fail.sh
 source "$(dirname "$0")/helpers/exit-if-fail.sh"
 
+exit_if_fail ./scripts/ci-frontend-metrics.sh
+
 start=$(date +%s)
 
 exit_if_fail yarn run prettier:check
@@ -13,7 +15,7 @@ exit_if_fail yarn run test
 end=$(date +%s)
 seconds=$((end - start))
 
-exit_if_fail ./scripts/ci-frontend-metrics.sh
+
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
 	exit_if_fail ./scripts/ci-metrics-publisher.sh grafana.ci-performance.frontend-tests=$seconds
