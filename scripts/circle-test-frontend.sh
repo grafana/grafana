8bbd62de48f7c40fcfda6dc83607702ff03303b8
Dominik Prokop <dominik.prokop@grafana.com>
2020-07-01 13:42:59 +0200

    Run metrics check as a first step of frontend test job to fail fastC

--- scripts/circle-test-frontend.sh
+++ scripts/circle-test-frontend.sh
@@ -3,10 +3,10 @@
 # shellcheck source=./scripts/helpers/exit-if-fail.sh
 source "$(dirname "$0")/helpers/exit-if-fail.sh"
 
-exit_if_fail ./scripts/ci-frontend-metrics.sh
 
 start=$(date +%s)
 
+exit_if_fail ./scripts/ci-frontend-metrics.sh
 exit_if_fail yarn run prettier:check
 exit_if_fail yarn run packages:typecheck
 exit_if_fail yarn run typecheck
@@ -14,8 +14,6 @@ exit_if_fail yarn run test
 
 end=$(date +%s)
 seconds=$((end - start))
-
-
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
 	exit_if_fail ./scripts/ci-metrics-publisher.sh grafana.ci-performance.frontend-tests=$seconds
