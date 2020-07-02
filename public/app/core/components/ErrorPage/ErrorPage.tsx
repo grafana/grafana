35cf0416bcd24bda3e640654a754c6f8b505121b
Peter Holmberg <peter.hlmbrg@gmail.com>
2020-07-02 11:23:28 +0200

    simple migration and remove angular part

--- public/app/core/components/ErrorPage/ErrorPage.tsx
+++ public/app/core/components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
 import { connect, MapStateToProps } from 'react-redux';
 import { NavModel } from '@grafana/data';
+import { config } from '@grafana/runtime';
 import { Icon } from '@grafana/ui';
 import Page from '../Page/Page';
-import { getNavModel } from '../../selectors/navModel';
-import { StoreState } from '../../../types';
+import { getNavModel } from 'app/core/selectors/navModel';
+import { StoreState } from 'app/types';
 
 interface ConnectedProps {
   navModel: NavModel;
@@ -33,10 +34,41 @@ export class ErrorPage extends PureComponent<Props> {
                     <p>0%</p>
                   </div>
                   <div className="error-column image-box">
-                    <img src="public/img/graph404.svg" width="100%" />
+                    <img src="public/img/graph404.svg" width="100%" alt="graph" />
                     <div className="error-row error-space-between">
                       <p className="graph-text">Then</p>
                       <p className="graph-text">Now</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="error-column info-box">
+                <div className="error-row current-box">
+                  <p className="current-text">current</p>
+                </div>
+                <div className="error-row" style={{ flex: 1 }}>
+                  <Icon name="minus-circle" className="error-minus" />
+                  <div className="error-column error-space-between error-full-width">
+                    <div className="error-row error-space-between">
+                      <p>Chances you are on the page you are looking for.</p>
+                      <p className="left-margin">0%</p>
+                    </div>
+                    <div>
+                      <h3>Sorry for the inconvenience</h3>
+                      <p>
+                        Please go back to your{' '}
+                        <a href={config.appSubUrl} className="error-link">
+                          home dashboard
+                        </a>{' '}
+                        and try again.
+                      </p>
+                      <p>
+                        If the error persists, seek help on the{' '}
+                        <a href="https://community.grafana.com" target="_blank" className="error-link">
+                          community site
+                        </a>
+                        .
+                      </p>
                     </div>
                   </div>
                 </div>
