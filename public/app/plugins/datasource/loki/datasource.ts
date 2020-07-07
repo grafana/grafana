52f12d10c5df97873c2fb09afb0cc571ff4b2cd3
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2020-07-07 17:42:49 +0200

    Explore/Loki: Escape \ in labels for show context queries (#26116)
    * Hot fix, keep the file
    
    * Add comment
    
    * Update public/app/plugins/datasource/loki/datasource.ts
    
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * Remove script.go file
    
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>

--- public/app/plugins/datasource/loki/datasource.ts
+++ public/app/plugins/datasource/loki/datasource.ts
@@ -409,7 +409,7 @@ export class LokiDatasource extends DataSourceApi<LokiQuery, LokiOptions> {
 
   prepareLogRowContextQueryTarget = (row: LogRowModel, limit: number, direction: 'BACKWARD' | 'FORWARD') => {
     const query = Object.keys(row.labels)
-      .map(label => `${label}="${row.labels[label]}"`)
+      .map(label => `${label}="${row.labels[label].replace(/\\/g, '\\\\')}"`) // escape backslashes in label as users can't escape them by themselves
       .join(',');
 
     const contextTimeBuffer = 2 * 60 * 60 * 1000; // 2h buffer
