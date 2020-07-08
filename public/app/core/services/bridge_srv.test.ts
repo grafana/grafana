81ec60c608339917204199286174c5d7089fb4da
Torkel Ödegaard <torkel@grafana.com>
2020-07-08 13:22:09 +0200

    Templating: Fixed recursive queries triggered when switching dashboard settings view (#26137)
    * Templating: Fixed recursive queries triggered when going into dashboard settings
    
    * Fixed unused import
    
    * use locationUtil
    
    (cherry picked from commit cace879c96dd5bdc75132de090618da9db565ffb)

--- public/app/core/services/bridge_srv.test.ts
+++ public/app/core/services/bridge_srv.test.ts
@@ -12,4 +12,42 @@ describe('when checking template variables', () => {
     expect(findTemplateVarChanges(b, a)).toEqual({ 'var-xyz': 'hello' });
     expect(findTemplateVarChanges(a, b)).toEqual({ 'var-xyz': '' });
   });
+
+  it('then should ignore equal values', () => {
+    const a: UrlQueryMap = {
+      'var-xyz': 'hello',
+      bbb: 'ignore me',
+    };
+    const b: UrlQueryMap = {
+      'var-xyz': 'hello',
+      aaa: 'ignore me',
+    };
+
+    expect(findTemplateVarChanges(b, a)).toBeUndefined();
+    expect(findTemplateVarChanges(a, b)).toBeUndefined();
+  });
+
+  it('then should ignore equal values with empty values', () => {
+    const a: UrlQueryMap = {
+      'var-xyz': '',
+      bbb: 'ignore me',
+    };
+    const b: UrlQueryMap = {
+      'var-xyz': '',
+      aaa: 'ignore me',
+    };
+
+    expect(findTemplateVarChanges(b, a)).toBeUndefined();
+    expect(findTemplateVarChanges(a, b)).toBeUndefined();
+  });
+
+  it('then should ignore empty array values', () => {
+    const a: UrlQueryMap = {
+      'var-adhoc': [],
+    };
+    const b: UrlQueryMap = {};
+
+    expect(findTemplateVarChanges(b, a)).toBeUndefined();
+    expect(findTemplateVarChanges(a, b)).toBeUndefined();
+  });
 });
