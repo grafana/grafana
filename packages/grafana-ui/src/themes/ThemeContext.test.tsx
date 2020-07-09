c8d441fa8b00a1888ef3dd3c58a131ebb2a20930
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2020-07-09 14:25:19 +0200

    Test that a theme change triggers an update

--- packages/grafana-ui/src/themes/ThemeContext.test.tsx
+++ packages/grafana-ui/src/themes/ThemeContext.test.tsx
@@ -3,14 +3,14 @@ import { renderHook } from '@testing-library/react-hooks';
 import { css } from 'emotion';
 import { mount } from 'enzyme';
 import React from 'react';
-import { useStyles } from './ThemeContext';
+import { mockThemeContext, useStyles } from './ThemeContext';
 
 describe('useStyles', () => {
-  it('memoizes the passed in function', () => {
+  it('memoizes the passed in function correctly', () => {
     const stylesCreator = () => ({});
-
     const { rerender, result } = renderHook(() => useStyles(stylesCreator));
     const storedReference = result.current;
+
     rerender();
     expect(storedReference).toBe(result.current);
   });
@@ -20,6 +20,17 @@ describe('useStyles', () => {
     const storedReference = result.current;
     rerender();
     expect(storedReference).not.toBe(result.current);
+  });
+
+  it('updates the memoized function when the theme changes', () => {
+    const stylesCreator = () => ({});
+    const { rerender, result } = renderHook(() => useStyles(stylesCreator));
+    const storedReference = result.current;
+
+    const restoreThemeContext = mockThemeContext({});
+    rerender();
+    expect(storedReference).not.toBe(result.current);
+    restoreThemeContext();
   });
 
   it('passes in theme and returns style object', done => {
