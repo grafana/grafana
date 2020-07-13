7d1e207807bb93962ce394d74f775b8975e85bfc
Sofia Papagiannaki <papagian@users.noreply.github.com>
2020-07-13 11:13:08 +0300

    Fix SignIn test (#26266)

--- public/app/core/components/sidemenu/SignIn.test.tsx
+++ public/app/core/components/sidemenu/SignIn.test.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { SignIn } from './SignIn';
 
-jest.mock('../../config', () => ({
-  appUrl: 'http://localhost:3000/',
-}));
+jest.mock('../../config', () => {
+  const original = jest.requireActual('../../config');
+  const config = original.getConfig();
+  return {
+    getConfig: () => ({
+      ...config,
+      appUrl: 'http://localhost:3000/grafana',
+      appSubUrl: '/grafana',
+    }),
+  };
+});
 
 describe('Render', () => {
   it('should render component', () => {
-    const wrapper = shallow(<SignIn url="/" />);
+    const wrapper = shallow(<SignIn url="/whatever" />);
 
     expect(wrapper).toMatchSnapshot();
   });
