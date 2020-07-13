4385d1211ed2817eeab416920a078aaa1be53ef9
Ryan McKinley <ryantxu@gmail.com>
2020-07-13 10:48:49 -0700

    use fixed sizes

---
+++ packages/grafana-ui/src/components/SingleStatShared/AlignmentFactorsEditor.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import { css, cx } from 'emotion';
+import { stylesFactory, useTheme } from '@grafana/ui';
+import { GrafanaTheme, StandardEditorProps, DisplayValueAlignmentFactors, FieldDisplay } from '@grafana/data';
+
+export interface AlignmentFactorsEditorOptions {
+  getValues: () => FieldDisplay[];
+}
+
+export const AlignmentFactorsEditor: FC<StandardEditorProps<DisplayValueAlignmentFactors, any>> = ({
+  value,
+  onChange,
+  context,
+}) => {
+  const theme = useTheme();
+  const styles = getStyles(theme);
+
+  return (
+    <div>
+      DisplayValueAlignmentFactors!!!
+      <br />
+      <div className={cx(styles.editorBox)}>HELLO</div>
+    </div>
+  );
+};
+
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  editorBox: css`
+    label: editorBox;
+    border: ${theme.border.width.sm} solid ${theme.colors.border2};
+    border-radius: ${theme.border.radius.sm};
+    margin: ${theme.spacing.xs} 0;
+    width: 100%;
+  `,
+}));
