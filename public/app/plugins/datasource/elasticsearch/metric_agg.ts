1acfd3a8fc3f1388a17686d3a4c5809f5ed1af24
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2020-07-15 15:20:39 +0200

    Elasticsearch: Create Raw Doc metric to render raw JSON docs in columns in the new table panel (#26233)
    * test
    
    * WIP: Create v2 version
    
    * Update tests, remove conosole logs, refactor
    
    * Remove incorrect types
    
    * Update type
    
    * Rename legacy and new metrics
    
    * Update
    
    * Run request when Raw Data tto Raw Document switch
    
    * Fix size updating
    
    * Remove _source field from table results as we are showing each source field as column
    
    * Remove _source just for metrics, not logs
    
    * Revert "Remove _source just for metrics, not logs"
    
    This reverts commit 611b6922f762afa0e76fd8679a9b8160bca74e6a.
    
    * Revert "Remove _source field from table results as we are showing each source field as column"
    
    This reverts commit 31a9d5f81b79b91a12a7e0b74f172ff8afc4bdd3.
    
    * Add vis preference for logs
    
    * Update visualisation to logs
    
    * Revert "Revert "Remove _source just for metrics""
    
    This reverts commit a102ab2894e7a9ca6eee307aa9b60dfea169c718.
    
    Co-authored-by: Marcus Efraimsson <marcus.efraimsson@gmail.com>
    (cherry picked from commit 3fd810417fc4ba5a2afb016bfd6bcce75754dae1)

--- public/app/plugins/datasource/elasticsearch/metric_agg.ts
+++ public/app/plugins/datasource/elasticsearch/metric_agg.ts
@@ -101,7 +101,8 @@ export class ElasticMetricAggCtrl {
           $scope.updateMovingAvgModelSettings();
           break;
         }
-        case 'raw_document': {
+        case 'raw_document':
+        case 'raw_data': {
           $scope.agg.settings.size = $scope.agg.settings.size || 500;
           $scope.settingsLinkText = 'Size: ' + $scope.agg.settings.size;
           $scope.target.metrics.splice(0, $scope.target.metrics.length, $scope.agg);
@@ -164,7 +165,10 @@ export class ElasticMetricAggCtrl {
       $scope.showOptions = false;
 
       // reset back to metric/group by query
-      if ($scope.target.bucketAggs.length === 0 && $scope.agg.type !== 'raw_document') {
+      if (
+        $scope.target.bucketAggs.length === 0 &&
+        ($scope.agg.type !== 'raw_document' || $scope.agg.type !== 'raw_data')
+      ) {
         $scope.target.bucketAggs = [queryDef.defaultBucketAgg()];
       }
 
