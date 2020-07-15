b193411587e4f5ef2a7c7fa5edd6af0ecf00a7a2
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-07-15 23:08:35 +0200

    Revert "Provisioning: Remove validation of installed app plugin"
    This reverts commit e854cad6eeb6e94908480f2c9edd81d0bdc623d6.

--- pkg/services/provisioning/plugins/config_reader.go
+++ pkg/services/provisioning/plugins/config_reader.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/plugins"
 	"gopkg.in/yaml.v2"
 )
 
@@ -54,6 +55,11 @@ func (cr *configReaderImpl) readConfig(path string) ([]*pluginsAsConfig, error) 
 
 	checkOrgIDAndOrgName(apps)
 
+	err = validatePluginsConfig(apps)
+	if err != nil {
+		return nil, err
+	}
+
 	return apps, nil
 }
 
@@ -91,6 +97,22 @@ func validateRequiredField(apps []*pluginsAsConfig) error {
 
 		if len(errStrings) != 0 {
 			return fmt.Errorf(strings.Join(errStrings, "\n"))
+		}
+	}
+
+	return nil
+}
+
+func validatePluginsConfig(apps []*pluginsAsConfig) error {
+	for i := range apps {
+		if apps[i].Apps == nil {
+			continue
+		}
+
+		for _, app := range apps[i].Apps {
+			if !plugins.IsAppInstalled(app.PluginID) {
+				return fmt.Errorf("app plugin not installed: %s", app.PluginID)
+			}
 		}
 	}
 
