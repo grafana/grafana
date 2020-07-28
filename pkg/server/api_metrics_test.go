7f866d480080af7e5495a2d7f8c8c29b5a50cea1
Arve Knudsen <arve.knudsen@gmail.com>
2020-07-28 08:28:33 +0200

    Move test helper functions
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/server/api_metrics_test.go
+++ pkg/server/api_metrics_test.go
@@ -36,153 +36,13 @@ import (
 	"xorm.io/xorm"
 )
 
-func createGrafDir(t *testing.T) (string, string) {
-	t.Helper()
-
-	tmpDir, err := ioutil.TempDir("", "")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		os.RemoveAll(tmpDir)
-	})
-
-	rootDir := filepath.Join("..", "..")
-
-	cfgDir := filepath.Join(tmpDir, "conf")
-	err = os.MkdirAll(cfgDir, 0755)
-	require.NoError(t, err)
-	dataDir := filepath.Join(tmpDir, "data")
-	err = os.MkdirAll(dataDir, 0755)
-	require.NoError(t, err)
-	logsDir := filepath.Join(tmpDir, "logs")
-	pluginsDir := filepath.Join(tmpDir, "plugins")
-	publicDir := filepath.Join(tmpDir, "public")
-	err = os.MkdirAll(publicDir, 0755)
-	require.NoError(t, err)
-	emailsDir := filepath.Join(publicDir, "emails")
-	err = fs.CopyRecursive(filepath.Join(rootDir, "public", "emails"), emailsDir)
-	require.NoError(t, err)
-	provDir := filepath.Join(cfgDir, "provisioning")
-	provDSDir := filepath.Join(provDir, "datasources")
-	err = os.MkdirAll(provDSDir, 0755)
-	require.NoError(t, err)
-	provNotifiersDir := filepath.Join(provDir, "notifiers")
-	err = os.MkdirAll(provNotifiersDir, 0755)
-	require.NoError(t, err)
-	provPluginsDir := filepath.Join(provDir, "plugins")
-	err = os.MkdirAll(provPluginsDir, 0755)
-	require.NoError(t, err)
-	provDashboardsDir := filepath.Join(provDir, "dashboards")
-	err = os.MkdirAll(provDashboardsDir, 0755)
-	require.NoError(t, err)
-
-	cfg := ini.Empty()
-	dfltSect := cfg.Section("")
-	_, err = dfltSect.NewKey("app_mode", "development")
-	require.NoError(t, err)
-
-	pathsSect, err := cfg.NewSection("paths")
-	require.NoError(t, err)
-	_, err = pathsSect.NewKey("data", dataDir)
-	require.NoError(t, err)
-	_, err = pathsSect.NewKey("logs", logsDir)
-	require.NoError(t, err)
-	_, err = pathsSect.NewKey("plugins", pluginsDir)
-	require.NoError(t, err)
-
-	logSect, err := cfg.NewSection("log")
-	require.NoError(t, err)
-	_, err = logSect.NewKey("level", "debug")
-	require.NoError(t, err)
-
-	serverSect, err := cfg.NewSection("server")
-	require.NoError(t, err)
-	_, err = serverSect.NewKey("port", "0")
-	require.NoError(t, err)
-
-	anonSect, err := cfg.NewSection("auth.anonymous")
-	require.NoError(t, err)
-	_, err = anonSect.NewKey("enabled", "true")
-	require.NoError(t, err)
-
-	cfgPath := filepath.Join(cfgDir, "test.ini")
-	err = cfg.SaveTo(cfgPath)
-	require.NoError(t, err)
-
-	err = fs.CopyFile(filepath.Join(rootDir, "conf", "defaults.ini"), filepath.Join(cfgDir, "defaults.ini"))
-	require.NoError(t, err)
-
-	return tmpDir, cfgPath
-}
-
-func startGrafana(t *testing.T, grafDir, cfgPath string) string {
-	t.Helper()
-
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-	require.NoError(t, err)
-	server, err := New(Config{
-		ConfigFile: cfgPath,
-		HomePath:   grafDir,
-		Listener:   listener,
-	})
-	require.NoError(t, err)
-
-	go func() {
-		if err := server.Run(); err != nil {
-			t.Log("Server exited uncleanly", "error", err)
-		}
-	}()
-	t.Cleanup(func() {
-		server.Shutdown("")
-	})
-
-	// Wait for Grafana to be ready
-	addr := listener.Addr().String()
-	resp, err := http.Get(fmt.Sprintf("http://%s/healthz", addr))
-	require.NoError(t, err)
-	require.NotNil(t, resp)
-	resp.Body.Close()
-	require.Equal(t, 200, resp.StatusCode)
-
-	t.Logf("Grafana is listening on %s", addr)
-
-	return addr
-}
-
-func setUpDatabase(t *testing.T, grafDir string) {
-	t.Helper()
-
-	connStr := fmt.Sprintf("file:%s/data/grafana.db?mode=rwc", grafDir)
-	engine, err := xorm.NewEngine("sqlite3", connStr)
-	require.NoError(t, err)
-	defer engine.Close()
-	migrator := migrator.NewMigrator(engine)
-	migrations.AddMigrations(migrator)
-	for _, descriptor := range registry.GetServices() {
-		sc, ok := descriptor.Instance.(registry.DatabaseMigrator)
-		if ok {
-			sc.AddMigration(migrator)
-		}
-	}
-	err = migrator.Start()
-	require.NoError(t, err)
-
-	_, err = engine.Insert(&models.DataSource{
-		Id:      1,
-		OrgId:   1,
-		Name:    "Test",
-		Type:    "cloudwatch",
-		Created: time.Now(),
-		Updated: time.Now(),
-	})
-	require.NoError(t, err)
-}
-
 // Test CloudWatch querying.
+//
+// XXX: Have to have all tests as sub-tests of a main closure, since Grafana uses a global xorm engine
+// that can lead to race conditions between tests using different Grafana server instances.
 func TestQueryCloudWatch(t *testing.T) {
 	grafDir, cfgPath := createGrafDir(t)
-
 	setUpDatabase(t, grafDir)
-
 	addr := startGrafana(t, grafDir, cfgPath)
 
 	t.Run("Metrics", func(t *testing.T) {
@@ -330,4 +190,145 @@ func makeCWRequest(t *testing.T, req dtos.MetricRequest, addr string) tsdb.Respo
 	require.NoError(t, err)
 
 	return tr
+}
+
+func createGrafDir(t *testing.T) (string, string) {
+	t.Helper()
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		os.RemoveAll(tmpDir)
+	})
+
+	rootDir := filepath.Join("..", "..")
+
+	cfgDir := filepath.Join(tmpDir, "conf")
+	err = os.MkdirAll(cfgDir, 0755)
+	require.NoError(t, err)
+	dataDir := filepath.Join(tmpDir, "data")
+	err = os.MkdirAll(dataDir, 0755)
+	require.NoError(t, err)
+	logsDir := filepath.Join(tmpDir, "logs")
+	pluginsDir := filepath.Join(tmpDir, "plugins")
+	publicDir := filepath.Join(tmpDir, "public")
+	err = os.MkdirAll(publicDir, 0755)
+	require.NoError(t, err)
+	emailsDir := filepath.Join(publicDir, "emails")
+	err = fs.CopyRecursive(filepath.Join(rootDir, "public", "emails"), emailsDir)
+	require.NoError(t, err)
+	provDir := filepath.Join(cfgDir, "provisioning")
+	provDSDir := filepath.Join(provDir, "datasources")
+	err = os.MkdirAll(provDSDir, 0755)
+	require.NoError(t, err)
+	provNotifiersDir := filepath.Join(provDir, "notifiers")
+	err = os.MkdirAll(provNotifiersDir, 0755)
+	require.NoError(t, err)
+	provPluginsDir := filepath.Join(provDir, "plugins")
+	err = os.MkdirAll(provPluginsDir, 0755)
+	require.NoError(t, err)
+	provDashboardsDir := filepath.Join(provDir, "dashboards")
+	err = os.MkdirAll(provDashboardsDir, 0755)
+	require.NoError(t, err)
+
+	cfg := ini.Empty()
+	dfltSect := cfg.Section("")
+	_, err = dfltSect.NewKey("app_mode", "development")
+	require.NoError(t, err)
+
+	pathsSect, err := cfg.NewSection("paths")
+	require.NoError(t, err)
+	_, err = pathsSect.NewKey("data", dataDir)
+	require.NoError(t, err)
+	_, err = pathsSect.NewKey("logs", logsDir)
+	require.NoError(t, err)
+	_, err = pathsSect.NewKey("plugins", pluginsDir)
+	require.NoError(t, err)
+
+	logSect, err := cfg.NewSection("log")
+	require.NoError(t, err)
+	_, err = logSect.NewKey("level", "debug")
+	require.NoError(t, err)
+
+	serverSect, err := cfg.NewSection("server")
+	require.NoError(t, err)
+	_, err = serverSect.NewKey("port", "0")
+	require.NoError(t, err)
+
+	anonSect, err := cfg.NewSection("auth.anonymous")
+	require.NoError(t, err)
+	_, err = anonSect.NewKey("enabled", "true")
+	require.NoError(t, err)
+
+	cfgPath := filepath.Join(cfgDir, "test.ini")
+	err = cfg.SaveTo(cfgPath)
+	require.NoError(t, err)
+
+	err = fs.CopyFile(filepath.Join(rootDir, "conf", "defaults.ini"), filepath.Join(cfgDir, "defaults.ini"))
+	require.NoError(t, err)
+
+	return tmpDir, cfgPath
+}
+
+func startGrafana(t *testing.T, grafDir, cfgPath string) string {
+	t.Helper()
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+	server, err := New(Config{
+		ConfigFile: cfgPath,
+		HomePath:   grafDir,
+		Listener:   listener,
+	})
+	require.NoError(t, err)
+
+	go func() {
+		if err := server.Run(); err != nil {
+			t.Log("Server exited uncleanly", "error", err)
+		}
+	}()
+	t.Cleanup(func() {
+		server.Shutdown("")
+	})
+
+	// Wait for Grafana to be ready
+	addr := listener.Addr().String()
+	resp, err := http.Get(fmt.Sprintf("http://%s/healthz", addr))
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	resp.Body.Close()
+	require.Equal(t, 200, resp.StatusCode)
+
+	t.Logf("Grafana is listening on %s", addr)
+
+	return addr
+}
+
+func setUpDatabase(t *testing.T, grafDir string) {
+	t.Helper()
+
+	connStr := fmt.Sprintf("file:%s/data/grafana.db?mode=rwc", grafDir)
+	engine, err := xorm.NewEngine("sqlite3", connStr)
+	require.NoError(t, err)
+	defer engine.Close()
+	migrator := migrator.NewMigrator(engine)
+	migrations.AddMigrations(migrator)
+	for _, descriptor := range registry.GetServices() {
+		sc, ok := descriptor.Instance.(registry.DatabaseMigrator)
+		if ok {
+			sc.AddMigration(migrator)
+		}
+	}
+	err = migrator.Start()
+	require.NoError(t, err)
+
+	_, err = engine.Insert(&models.DataSource{
+		Id:      1,
+		OrgId:   1,
+		Name:    "Test",
+		Type:    "cloudwatch",
+		Created: time.Now(),
+		Updated: time.Now(),
+	})
+	require.NoError(t, err)
 }
