cccf0a2208264273eca9ff5d03362aecf73c714b
Emil Tullstedt <emil.tullstedt@grafana.com>
2020-07-31 11:46:46 +0200

    Test: Convert utils tests to testify

--- pkg/util/validation_test.go
+++ pkg/util/validation_test.go
@@ -3,19 +3,15 @@ package util
 import (
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIsEmail(t *testing.T) {
-	Convey("When validating a string that is a valid email", t, func() {
-		result := IsEmail("abc@def.com")
-
-		So(result, ShouldEqual, true)
-	})
-
-	Convey("When validating a string that is not a valid email", t, func() {
-		result := IsEmail("abcdef.com")
-
-		So(result, ShouldEqual, false)
-	})
+	emails := map[string]bool{
+		"abc@def.com": true,
+		"abcdef.com":  false,
+	}
+	for input, expected := range emails {
+		assert.Equal(t, expected, IsEmail(input))
+	}
 }
