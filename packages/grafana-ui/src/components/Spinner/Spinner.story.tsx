a50c5ec8c09a0515d6251f09fb5d889072367f84
Karine Valença <valenca.karine@gmail.com>
2020-08-01 18:54:40 -0300

    add docs for spinner component

--- packages/grafana-ui/src/components/Spinner/Spinner.story.tsx
+++ packages/grafana-ui/src/components/Spinner/Spinner.story.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { number, color } from '@storybook/addon-knobs';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { Spinner } from '@grafana/ui';
+import mdx from './Spinner.mdx'
 
 export default {
   title: 'Visualizations/Spinner',
   component: Spinner,
   decorators: [withCenteredStory],
+  parameters: {
+	  docs: {
+		  page: mdx
+	  },
+  },
 };
 
 export const basic = () => {
@@ -15,4 +21,20 @@ export const basic = () => {
       <Spinner />
     </div>
   );
+};
+
+export const withStyle = () => {
+	return (
+    <div>
+      <Spinner
+        className='my-spin-div'
+        style={{ 
+          backgroundColor: color('White', 'white'), 
+          color: color('Red', 'red') 
+        }}
+        iconClassName='my-spinner-classname'
+        size={number("Size", 34)}
+      />
+    </div>
+	);
 };
