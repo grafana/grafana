fc711c6c1c61da72832bff8990525bba5b32b8f7
Karine Valença <valenca.karine@gmail.com>
2020-08-03 07:55:46 -0300

    fix lint

--- packages/grafana-ui/src/components/Spinner/Spinner.story.tsx
+++ packages/grafana-ui/src/components/Spinner/Spinner.story.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { number, color } from '@storybook/addon-knobs';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { Spinner } from '@grafana/ui';
-import mdx from './Spinner.mdx'
+import mdx from './Spinner.mdx';
 
 export default {
   title: 'Visualizations/Spinner',
   component: Spinner,
   decorators: [withCenteredStory],
   parameters: {
-	  docs: {
-		  page: mdx
-	  },
+    docs: {
+      page: mdx,
+    },
   },
 };
 
@@ -24,17 +24,17 @@ export const basic = () => {
 };
 
 export const withStyle = () => {
-	return (
+  return (
     <div>
       <Spinner
-        className='my-spin-div'
-        style={{ 
-          backgroundColor: color('White', 'white'), 
-          color: color('Red', 'red') 
+        className="my-spin-div"
+        style={{
+          backgroundColor: color('White', 'white'),
+          color: color('Red', 'red'),
         }}
-        iconClassName='my-spinner-classname'
-        size={number("Size", 34)}
+        iconClassName="my-spinner-classname"
+        size={number('Size', 34)}
       />
     </div>
-	);
+  );
 };
