cd3b9805558a23e43294037288e935ebb5ed6a18
Totalus <raph.nev@gmail.com>
2020-08-06 23:30:08 -0400

    Connecting options with Editor

--- packages/grafana-data/src/transformations/transformers/filterByValue.ts
+++ packages/grafana-data/src/transformations/transformers/filterByValue.ts
@@ -6,10 +6,14 @@ import { ArrayVector } from '../../vector/ArrayVector';
 import { guessFieldTypeForField } from '../../dataframe/processDataFrame';
 import { ReducerID } from '../fieldReducer';
 
+export interface ValueFilter {
+  type: string;
+  fieldName: string | null; // Corresponding field name
+  filterExpression: string | null; // The filter expression / value
+}
+
 export interface FilterByValueTransformerOptions {
-  byField?: string;
-  reducers: ReducerID[];
-  calculationsByField: Array<[string | null, ReducerID[]]>;
+  valueFilters: [ValueFilter];
 }
 
 export const filterByValueTransformer: DataTransformerInfo<FilterByValueTransformerOptions> = {
@@ -17,7 +21,7 @@ export const filterByValueTransformer: DataTransformerInfo<FilterByValueTransfor
   name: 'Filter by Value',
   description: 'Filter the data points (rows) depending on the value of certain fields',
   defaultOptions: {
-    calculationsByField: [[null, [ReducerID.count]]],
+    valueFilters: [{ type: 'include', fieldName: null, filterExpression: null }],
   },
 
   /**
@@ -26,10 +30,14 @@ export const filterByValueTransformer: DataTransformerInfo<FilterByValueTransfor
    */
   transformer: (options: FilterByValueTransformerOptions) => {
     console.log('options:', options);
-    // const filterByValueFieldName = options.byField || '';
     // const calculationsByField = options.calculationsByField; //.map((val, index) => ({fieldName: val[0], calculations: val[1]}));
 
     return (data: DataFrame[]) => {
+      // For each frame
+      // For each row
+      // For each filter
+      // Include or exclude row ?
+
       return data;
     };
   },
--- public/app/core/components/TransformersUI/FilterByValueTransformerEditor.tsx
+++ public/app/core/components/TransformersUI/FilterByValueTransformerEditor.tsx
@@ -5,50 +5,68 @@ import {
   standardTransformers,
   TransformerRegistyItem,
   TransformerUIProps,
-  ReducerID,
 } from '@grafana/data';
 import { getAllFieldNamesFromDataFrames } from './OrganizeFieldsTransformerEditor';
-import { Select, StatsPicker, Button } from '@grafana/ui';
+import { Select, Button, Input } from '@grafana/ui';
 import { selectors } from '@grafana/e2e-selectors';
 
-import { FilterByValueTransformerOptions } from '@grafana/data/src/transformations/transformers/filterByValue';
+import {
+  FilterByValueTransformerOptions,
+  ValueFilter,
+} from '@grafana/data/src/transformations/transformers/filterByValue';
 
-function FieldCalculationsSelector(props: any) {
+function FilterSelectorRow(props: any) {
   const { fieldNameOptions, onDelete, onConfigChange, config } = props;
 
   return (
     <div className="gf-form-inline">
       <div className="gf-form">
-        <div className="gf-form-label width-8">On field</div>
+        <div className="gf-form-label width-8">Filter Type</div>
+        <Select
+          className="width-8"
+          options={[
+            { label: 'Include', value: 'include' },
+            { label: 'Exclude', value: 'exclude' },
+          ]}
+          value={config.type}
+          onChange={option => {
+            console.log('onChange filterType', option.value);
+            onConfigChange({ ...config, type: option.value });
+          }}
+          menuPlacement="bottom"
+        />
+      </div>
+      <div className="gf-form gf-form--offset-1">
+        <div className="gf-form-label width-8">Filter on Field</div>
         <Select
           className="width-16"
           placeholder="Field Name"
           options={fieldNameOptions}
-          value={config[0]}
+          value={config.fieldName}
           onChange={value => {
-            console.log('onChange', value);
-            console.log(onConfigChange([value.value, config[1]]));
+            console.log('onChange fieldName', value);
+            onConfigChange({ ...config, fieldName: value });
           }}
           isClearable
           menuPlacement="bottom"
         />
       </div>
       <div className="gf-form gf-form--grow gf-form--offset-1">
-        <div className="gf-form-label width-8" aria-label={selectors.components.Transforms.Reduce.calculationsLabel}>
-          Calculate
-        </div>
-        <StatsPicker
+        <div className="gf-form-label width-8">Filter Expression</div>
+        <Input
           className="flex-grow-1"
-          placeholder="Choose Stat"
-          allowMultiple
-          stats={config[1]}
-          onChange={stats => {
-            onConfigChange([config[0], stats as ReducerID[]]);
+          // defaultValue={''}
+          value={config.filterExpression}
+          placeholder={`Regex`}
+          onBlur={event => {
+            console.log('onBlur', event.currentTarget.value);
+            onConfigChange({ ...config, filterExpression: event.currentTarget.value });
           }}
-          menuPlacement="bottom"
         />
       </div>
-      <Button icon="trash-alt" onClick={onDelete} size="sm" variant="secondary" />
+      <div className="gf-form gf-form--offset-1">
+        <Button icon="trash-alt" onClick={onDelete} style={{ margin: 'auto' }} size="sm" variant="secondary" />
+      </div>
     </div>
   );
 }
@@ -61,31 +79,28 @@ export const FilterByValueTransformerEditor: React.FC<TransformerUIProps<FilterB
   const fieldNames = useMemo(() => getAllFieldNamesFromDataFrames(input), [input]);
   const fieldNameOptions = fieldNames.map((item: string) => ({ label: item, value: item }));
 
-  const onSelectField = (value: SelectableValue<string>) => {
-    onChange({
-      ...options,
-      byField: value.value,
+  const onAddFilter = () => {
+    options.valueFilters.push({
+      type: 'include',
+      fieldName: null,
+      filterExpression: null,
     });
-  };
 
-  const onAddFieldCalculations = () => {
-    options.calculationsByField.push([null, []]);
-    onChange({
-      ...options,
-    });
-  };
-
-  const onDeleteFieldCalculations = (index: number) => () => {
-    options.calculationsByField.splice(index, 1);
     onChange({
       ...options,
     });
   };
 
-  const onConfigChange = (index: number) => (config: [string | any, ReducerID[]]) => {
+  const onDeleteFilter = (index: number) => () => {
+    options.valueFilters.splice(index, 1);
+    onChange({
+      ...options,
+    });
+  };
+
+  const onConfigChange = (index: number) => (config: ValueFilter) => {
     console.log('onConfigChange', index, config, options);
-    options.calculationsByField[index] = config;
-    console.log(options);
+    options.valueFilters[index] = config;
     onChange({
       ...options,
     });
@@ -93,33 +108,18 @@ export const FilterByValueTransformerEditor: React.FC<TransformerUIProps<FilterB
 
   return (
     <div>
-      <div className="gf-form-inline">
-        <div className="gf-form">
-          <div className="gf-form-label width-8">Filter by Value</div>
-          <Select
-            className="width-16"
-            options={fieldNameOptions}
-            value={options.byField}
-            onChange={onSelectField}
-            isClearable
-            placeholder="Field Name"
-            menuPlacement="bottom"
-          />
-        </div>
-      </div>
-
-      {options.calculationsByField.map((val, idx) => (
-        <FieldCalculationsSelector
+      {options.valueFilters.map((val, idx) => (
+        <FilterSelectorRow
           onConfigChange={onConfigChange(idx)}
-          onDelete={onDeleteFieldCalculations(idx)}
+          onDelete={onDeleteFilter(idx)}
           fieldNameOptions={fieldNameOptions}
           config={val}
         />
       ))}
 
       <div className="gf-form-inline gf-form--offset-1">
-        <Button icon="plus" onClick={onAddFieldCalculations}>
-          Add Field Calculations
+        <Button icon="plus" onClick={onAddFilter}>
+          Add filter
         </Button>
       </div>
     </div>
