7d912c56015a695623cd002215aa3eb7a68d80f4
kay delaney <kay@grafana.com>
2020-08-11 00:40:20 +0100

    Panel/BarGauge: Prevent overflow in panel with many series Closes #24889

--- public/app/plugins/panel/bargauge/BarGaugePanel.tsx
+++ public/app/plugins/panel/bargauge/BarGaugePanel.tsx
@@ -100,8 +100,10 @@ export class BarGaugePanel extends PureComponent<PanelProps<BarGaugeOptions>> {
         renderCounter={renderCounter}
         width={width}
         height={height}
+        minVizHeight={10}
         itemSpacing={this.getItemSpacing()}
         orientation={options.orientation}
+        repeaterStyle={{ overflow: 'hidden scroll' }}
       />
     );
   }
