064fc2fefcc5f784c7d805b81972e464df3e5e15
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-08-10 23:05:16 +0200

    Add rate interval variable to prometheus data source

--- public/app/plugins/datasource/prometheus/datasource.ts
+++ public/app/plugins/datasource/prometheus/datasource.ts
@@ -335,13 +335,18 @@ export class PrometheusDatasource extends DataSourceApi<PromQuery, PromOptions> 
     const intervalFactor = target.intervalFactor || 1;
     // Adjust the interval to take into account any specified minimum and interval factor plus Prometheus limits
     const adjustedInterval = this.adjustInterval(interval, minInterval, range, intervalFactor);
-    let scopedVars = { ...options.scopedVars, ...this.getRangeScopedVars(options.range) };
+    let scopedVars = {
+      ...options.scopedVars,
+      ...this.getRangeScopedVars(options.range),
+      ...this.getRangeIntervalScopedVariable(interval, minInterval),
+    };
     // If the interval was adjusted, make a shallow copy of scopedVars with updated interval vars
     if (interval !== adjustedInterval) {
       interval = adjustedInterval;
       scopedVars = Object.assign({}, options.scopedVars, {
         __interval: { text: interval + 's', value: interval + 's' },
         __interval_ms: { text: interval * 1000, value: interval * 1000 },
+        ...this.getRangeIntervalScopedVariable(interval, minInterval),
         ...this.getRangeScopedVars(options.range),
       });
     }
@@ -378,6 +383,12 @@ export class PrometheusDatasource extends DataSourceApi<PromQuery, PromOptions> 
     this._addTracingHeaders(query, options);
 
     return query;
+  }
+
+  getRangeIntervalScopedVariable(interval: number, minInterval: number) {
+    const intervalInSeconds = minInterval === interval ? kbn.interval_to_seconds(this.interval) : minInterval;
+    const rateInterval = Math.max(interval + intervalInSeconds, 4 * intervalInSeconds);
+    return { __rate_interval: { text: rateInterval + 's', value: rateInterval + 's' } };
   }
 
   adjustInterval(interval: number, minInterval: number, range: number, intervalFactor: number) {
