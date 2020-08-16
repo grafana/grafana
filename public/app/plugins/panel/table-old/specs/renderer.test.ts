bedfb381d5284c7a4b0cae9582b83ec7bed78902
Berbe <4251220+Berbe@users.noreply.github.com>
2020-08-16 17:07:31 +0200

    Dashboard: Homogeneise rate units

--- public/app/plugins/panel/table-old/specs/renderer.test.ts
+++ public/app/plugins/panel/table-old/specs/renderer.test.ts
@@ -246,7 +246,7 @@ describe('when rendering table', () => {
 
     it('number column with unit specified should ignore style unit', () => {
       const html = renderer.renderCell(5, 0, 1230);
-      expect(html).toBe('<td>1.23 kbps</td>');
+      expect(html).toBe('<td>1.23 kb/s</td>');
     });
 
     it('number column should be formated', () => {
