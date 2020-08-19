e46115479d5c3f6f743c68ab75a3dd2ad4829688
Ryan McKinley <ryantxu@gmail.com>
2020-08-19 13:21:32 -0700

    add docs for by type

--- packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
+++ packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
@@ -2,6 +2,7 @@ import { Registry } from '@grafana/data';
 import { fieldNameMatcherItem } from './FieldNameMatcherEditor';
 import { FieldMatcherUIRegistryItem } from './types';
 import { fieldNameByRegexMatcherItem } from './FieldNameByRegexMatcherEditor';
+import { fieldTypeMatcherItem } from './FieldTypeMatcherEditor';
 
 export const fieldMatchersUI = new Registry<FieldMatcherUIRegistryItem<any>>(() => {
   return [fieldNameMatcherItem, fieldNameByRegexMatcherItem, fieldTypeMatcherItem];
