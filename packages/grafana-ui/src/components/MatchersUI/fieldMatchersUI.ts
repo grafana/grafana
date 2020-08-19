dd8f90ccdb4169774a24d3fc0096fa656d519c86
Ryan McKinley <ryantxu@gmail.com>
2020-08-19 13:10:57 -0700

    Overrides: expose byType matcher UI (#27056)
    (cherry picked from commit cb82cf110fa05bbf92087375cdd21937564c05e7)

--- packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
+++ packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
@@ -1,7 +1,8 @@
 import { Registry } from '@grafana/data';
 import { fieldNameMatcherItem } from './FieldNameMatcherEditor';
 import { FieldMatcherUIRegistryItem } from './types';
+import { fieldTypeMatcherItem } from './FieldTypeMatcherEditor';
 
 export const fieldMatchersUI = new Registry<FieldMatcherUIRegistryItem<any>>(() => {
-  return [fieldNameMatcherItem];
+  return [fieldNameMatcherItem, fieldTypeMatcherItem];
 });
