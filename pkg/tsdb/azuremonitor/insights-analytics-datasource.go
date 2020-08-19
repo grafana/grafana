5481345ecc4ce9c8eca23853dfdb3b38d56f6471
Kyle Brandt <kyle@grafana.com>
2020-08-19 10:42:54 -0400

    AzureAnalytics: Use fillmode Null with format as time series (#27054)
    previously was using fillmode previous, which is not what we want here.
    Also we do not have fill missing with this data source anyways
    Fixes #27013
    
    (cherry picked from commit 660510084cd4ffcbe58a097c4de1dec07632bec0)

--- pkg/tsdb/azuremonitor/insights-analytics-datasource.go
+++ pkg/tsdb/azuremonitor/insights-analytics-datasource.go
@@ -163,7 +163,7 @@ func (e *InsightsAnalyticsDatasource) executeQuery(ctx context.Context, query *I
 	if query.ResultFormat == "time_series" {
 		tsSchema := frame.TimeSeriesSchema()
 		if tsSchema.Type == data.TimeSeriesTypeLong {
-			wideFrame, err := data.LongToWide(frame, &data.FillMissing{})
+			wideFrame, err := data.LongToWide(frame, nil)
 			if err == nil {
 				frame = wideFrame
 			} else {
