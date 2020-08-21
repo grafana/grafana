1005e3c85fc6fa12c426354e7a15ea254e88cf9d
Clarity-89 <homes89@ukr.net>
2020-08-21 15:16:08 +0300

    Grafana-UI: Move Alert styles to emotion

--- packages/grafana-ui/src/components/Alert/Alert.tsx
+++ packages/grafana-ui/src/components/Alert/Alert.tsx
@@ -1,5 +1,7 @@
 import React, { FC, ReactNode } from 'react';
-import classNames from 'classnames';
+import { css } from 'emotion';
+import { GrafanaTheme } from '@grafana/data';
+import { useTheme } from '../../themes';
 import { Icon } from '../Icon/Icon';
 import { IconName } from '../../types/icon';
 
@@ -16,38 +18,35 @@ export interface Props {
 
 function getIconFromSeverity(severity: AlertVariant): string {
   switch (severity) {
-    case 'error': {
+    case 'error':
+    case 'warning':
       return 'exclamation-triangle';
-    }
-    case 'warning': {
-      return 'exclamation-triangle';
-    }
-    case 'info': {
+    case 'info':
       return 'info-circle';
-    }
-    case 'success': {
+    case 'success':
       return 'check';
-    }
     default:
       return '';
   }
 }
 
 export const Alert: FC<Props> = ({ title, buttonText, onButtonClick, onRemove, children, severity = 'error' }) => {
-  const alertClass = classNames('alert', `alert-${severity}`);
+  const theme = useTheme();
+  const styles = getStyles(theme, severity);
+
   return (
-    <div className="alert-container">
-      <div className={alertClass}>
-        <div className="alert-icon">
+    <div className={styles.container}>
+      <div className={styles.alert}>
+        <div className={styles.icon}>
           <Icon size="xl" name={getIconFromSeverity(severity) as IconName} />
         </div>
-        <div className="alert-body">
-          <div className="alert-title">{title}</div>
-          {children && <div className="alert-text">{children}</div>}
+        <div className={styles.body}>
+          <div className={styles.title}>{title}</div>
+          {children && <div>{children}</div>}
         </div>
         {/* If onRemove is specified, giving preference to onRemove */}
         {onRemove && (
-          <button type="button" className="alert-close" onClick={onRemove}>
+          <button type="button" className={styles.close} onClick={onRemove}>
             <Icon name="times" size="lg" />
           </button>
         )}
@@ -59,4 +58,67 @@ export const Alert: FC<Props> = ({ title, buttonText, onButtonClick, onRemove, c
       </div>
     </div>
   );
+};
+
+const getStyles = (theme: GrafanaTheme, severity: AlertVariant) => {
+  const { redBase, redShade, greenBase, greenShade, blue80, blue77, white } = theme.palette;
+  const backgrounds = {
+    error: css`
+      background: linear-gradient(90deg, ${redBase}, ${redShade});
+    `,
+    warning: css`
+      background: linear-gradient(90deg, ${redBase}, ${redShade});
+    `,
+    info: css`
+      background: linear-gradient(100deg, ${blue80}, ${blue77});
+    `,
+    success: css`
+      background: linear-gradient(100deg, ${greenBase}, ${greenShade});
+    `,
+  };
+
+  return {
+    container: css`
+      z-index: ${theme.zIndex.tooltip};
+    `,
+    alert: css`
+      padding: 15px 20px;
+      margin-bottom: ${theme.spacing.xs};
+      position: relative;
+      color: ${white};
+      text-shadow: 0 1px 0 rgba(0, 0, 0, 0.2);
+      border-radius: ${theme.border.radius.md};
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      ${backgrounds[severity]}
+    `,
+    icon: css`
+      padding: 0 ${theme.spacing.md} 0 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 35px;
+    `,
+    title: css`
+      font-weight: ${theme.typography.weight.semibold};
+    `,
+    body: css`
+      .alert-body {
+        flex-grow: 1;
+
+        a {
+          color: ${white};
+          text-decoration: underline;
+        }
+      }
+    `,
+    close: css`
+      padding: 0 0 0 ${theme.spacing.md};
+      border: none;
+      background: none;
+      display: flex;
+      align-items: center;
+    `,
+  };
 };
