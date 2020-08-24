1c163e5ede9d0e42f4b66ceac5e074d36e0c55ed
beorn7 <beorn@grafana.com>
2020-08-25 00:47:18 +0200

    Simplify `getRateIntervalScopedVariable`
    Signed-off-by: beorn7 <beorn@grafana.com>

--- public/app/plugins/datasource/prometheus/datasource.test.ts
+++ public/app/plugins/datasource/prometheus/datasource.test.ts
@@ -1620,36 +1620,16 @@ describe('PrometheusDatasource', () => {
 
   describe('The __rate_interval variable', () => {
     it('should be 4 times the scrape interval if interval + scrape interval is lower', () => {
-      const { __rate_interval } = ds.getRateIntervalScopedVariable({
-        isMinIntervalSet: false,
-        interval: 23,
-        minInterval: 23,
-      });
+      const { __rate_interval } = ds.getRateIntervalScopedVariable(23, 15);
       expect(__rate_interval.value).toBe('60s');
     });
     it('should be interval + scrape interval if 4 times the scrape interval is lower', () => {
-      const { __rate_interval } = ds.getRateIntervalScopedVariable({
-        isMinIntervalSet: false,
-        interval: 56,
-        minInterval: 56,
-      });
+      const { __rate_interval } = ds.getRateIntervalScopedVariable(56, 15);
       expect(__rate_interval.value).toBe('71s');
     });
-    it('should fall back to 60s if interval is 0', () => {
-      const { __rate_interval } = ds.getRateIntervalScopedVariable({
-        isMinIntervalSet: false,
-        interval: 0,
-        minInterval: 0,
-      });
+    it('should fall back to 60s if interval and scrapeInterval is 0', () => {
+      const { __rate_interval } = ds.getRateIntervalScopedVariable(0, 0);
       expect(__rate_interval.value).toBe('60s');
-    });
-    it('should use minInterval if isMinIntervalSet = true', () => {
-      const { __rate_interval } = ds.getRateIntervalScopedVariable({
-        isMinIntervalSet: true,
-        interval: 60,
-        minInterval: 60,
-      });
-      expect(__rate_interval.value).toBe('240s');
     });
   });
 });
