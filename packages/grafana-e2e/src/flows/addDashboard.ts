165d511b2110e28901e072d96ef8d87d000281c0
Steven Vachon <contact@svachon.com>
2020-08-25 15:47:27 -0400

    Select timezone from dashboard toolbar instead of settings
    ... and refactored `setDashboardTimeRange` for reuse on other pages via a now more generalized `setTimeRange`

--- packages/grafana-e2e/src/flows/addDashboard.ts
+++ packages/grafana-e2e/src/flows/addDashboard.ts
@@ -1,13 +1,11 @@
-import { DashboardTimeRangeConfig, setDashboardTimeRange } from './setDashboardTimeRange';
 import { DeleteDashboardConfig } from './deleteDashboard';
 import { e2e } from '../index';
 import { getDashboardUid } from '../support/url';
-import { selectOption } from './selectOption';
+import { setDashboardTimeRange, TimeRangeConfig } from './setDashboardTimeRange';
 import { v4 as uuidv4 } from 'uuid';
 
 export interface AddDashboardConfig {
-  timeRange: DashboardTimeRangeConfig;
-  timezone: string;
+  timeRange: TimeRangeConfig;
   title: string;
   variables: Array<Partial<AddVariableConfig>>;
 }
@@ -27,40 +25,36 @@ export interface AddVariableConfig {
 // @todo this actually returns type `Cypress.Chainable`
 export const addDashboard = (config?: Partial<AddDashboardConfig>): any => {
   const fullConfig = {
-    timeRange: {
-      from: '2020-01-01 00:00:00',
-      to: '2020-01-01 06:00:00',
-    },
-    timezone: 'Coordinated Universal Time',
     title: `e2e-${uuidv4()}`,
     variables: [],
     ...config,
+    timeRange: {
+      from: '2020-01-01 00:00:00',
+      to: '2020-01-01 06:00:00',
+      zone: 'Coordinated Universal Time',
+      ...config?.timeRange,
+    },
   } as AddDashboardConfig;
 
-  const { timeRange, timezone, title, variables } = fullConfig;
+  const { timeRange, title, variables } = fullConfig;
 
   e2e().logToConsole('Adding dashboard with title:', title);
 
   e2e.pages.AddDashboard.visit();
 
-  e2e.pages.Dashboard.Toolbar.toolbarItems('Dashboard settings').click();
-
-  // @todo use the time range picker's time zone control
-  selectOption(e2e.pages.Dashboard.Settings.General.timezone(), timezone);
-
-  addVariables(variables);
-
-  e2e.components.BackButton.backArrow().click();
+  if (variables.length > 0) {
+    e2e.pages.Dashboard.Toolbar.toolbarItems('Dashboard settings').click();
+    addVariables(variables);
+    e2e.components.BackButton.backArrow().click();
+  }
 
   setDashboardTimeRange(timeRange);
 
   e2e.pages.Dashboard.Toolbar.toolbarItems('Save dashboard').click();
-
   e2e.pages.SaveDashboardAsModal.newName()
     .clear()
     .type(title);
   e2e.pages.SaveDashboardAsModal.save().click();
-
   e2e.flows.assertSuccessNotification();
 
   e2e().logToConsole('Added dashboard with title:', title);
@@ -168,9 +162,6 @@ const addVariable = (config: Partial<AddVariableConfig>, isFirst: boolean): any 
 };
 
 const addVariables = (configs: Array<Partial<AddVariableConfig>>): any => {
-  if (configs.length > 0) {
-    e2e.pages.Dashboard.Settings.General.sectionItems('Variables').click();
-  }
-
+  e2e.pages.Dashboard.Settings.General.sectionItems('Variables').click();
   return configs.map((config, i) => addVariable(config, i === 0));
 };
