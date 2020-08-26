5c780cfd21cff5e0e411515b5dc2ed7cf9f7ce65
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-08-26 10:53:02 +0200

    Refactor: some styling and sorting

--- packages/grafana-ui/src/components/Table/FilterPopup.tsx
+++ packages/grafana-ui/src/components/Table/FilterPopup.tsx
@@ -19,31 +19,38 @@ export const FilterPopup: FC<Props> = ({ onHide, column, field }) => {
   const filterValues = useMemo(() => getValues(options, column.filterValue), [options, column.filterValue]);
   const [values, setValues] = useState<SelectableValue[]>(filterValues ?? []);
   const theme = useTheme();
-  const styles = getStyles(theme, column);
+  const styles = getStyles(theme);
 
   return (
     <ClickOutsideWrapper onClick={onHide} useCapture={true}>
       <div className={cx(styles.filterContainer)} onClick={stopPropagation}>
-        <VerticalGroup spacing="md">
-          <Button
-            variant="link"
-            size="sm"
-            onClick={() => {
-              column.setFilter(undefined);
-              onHide();
-            }}
-          >
-            Clear filter
-          </Button>
-          <Label>Filter by values:</Label>
-          <MultiSelect onChange={setValues} value={values} options={options} closeMenuOnSelect={false} />
-          <HorizontalGroup>
-            <Button size="sm" variant="secondary" onClick={() => onHide()}>
-              Cancel
-            </Button>
-            <Button size="sm" onClick={submitChange(column.setFilter, values, onHide)}>
-              Ok
-            </Button>
+        <VerticalGroup spacing="lg">
+          <VerticalGroup spacing="xs">
+            <Label>Filter by values:</Label>
+            <MultiSelect onChange={setValues} value={values} options={options} />
+          </VerticalGroup>
+          <HorizontalGroup spacing="lg">
+            <HorizontalGroup>
+              <Button size="sm" onClick={submitChange(column.setFilter, values, onHide)}>
+                Ok
+              </Button>
+              <Button size="sm" variant="secondary" onClick={() => onHide()}>
+                Cancel
+              </Button>
+            </HorizontalGroup>
+            <HorizontalGroup>
+              <Button
+                variant="link"
+                size="sm"
+                onClick={() => {
+                  column.setFilter(undefined);
+                  onHide();
+                }}
+                disabled={column.filterValue === undefined}
+              >
+                Clear filter
+              </Button>
+            </HorizontalGroup>
           </HorizontalGroup>
         </VerticalGroup>
       </div>
@@ -51,15 +58,16 @@ export const FilterPopup: FC<Props> = ({ onHide, column, field }) => {
   );
 };
 
-const getStyles = stylesFactory((theme: GrafanaTheme, column: any) => ({
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
   filterContainer: css`
     label: filterContainer;
     width: 100%;
-    height: 180px;
+    min-width: 200px;
+    height: 140px;
     background-color: ${theme.colors.bg1};
-    border: ${theme.border.width.sm} solid ${theme.colors.border1};
-    padding: ${theme.spacing.sm};
-    margin: ${theme.spacing.xs} 0px;
+    border: ${theme.border.width.sm} solid ${theme.colors.border2};
+    padding: ${theme.spacing.md};
+    margin: ${theme.spacing.sm} 0px;
   `,
 }));
 
@@ -78,7 +86,6 @@ const calculateUniqueFieldValues = (field?: Field) => {
 
   if (!field.state.calcs.unique) {
     const set: Record<string, any> = {};
-
     for (let index = 0; index < field.values.length; index++) {
       const fieldValue = field.values.get(index);
       const displayValue = field.display ? field.display(fieldValue) : fieldValue;
@@ -93,10 +100,33 @@ const calculateUniqueFieldValues = (field?: Field) => {
 };
 
 const getOptions = (unique: Record<string, any>): Array<SelectableValue<string>> =>
-  Object.keys(unique).reduce(
-    (all, key) => all.concat({ value: unique[key], label: key }),
-    [] as Array<SelectableValue<string>>
-  );
+  Object.keys(unique)
+    .reduce((all, key) => all.concat({ value: unique[key], label: key }), [] as Array<SelectableValue<string>>)
+    .sort(sortOption);
+
+const sortOption = (a: SelectableValue, b: SelectableValue): number => {
+  if (a.label === undefined && b.label === undefined) {
+    return 0;
+  }
+
+  if (a.label === undefined && b.label !== undefined) {
+    return -1;
+  }
+
+  if (a.label !== undefined && b.label === undefined) {
+    return 1;
+  }
+
+  if (a.label! < b.label!) {
+    return -1;
+  }
+
+  if (a.label! > b.label!) {
+    return 1;
+  }
+
+  return 0;
+};
 
 const getValues = (options: SelectableValue[], filterValues?: SelectableValue[]) => {
   if (!filterValues) {
@@ -122,6 +152,5 @@ const submitChange = (setFilter: (...args: any) => void, options: SelectableValu
 };
 
 const stopPropagation = (event: React.MouseEvent) => {
-  console.log('stopPropagation');
   event.stopPropagation();
 };
