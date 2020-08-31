820d66cb339fef1b335e9701872ca5d93740e396
Clarity-89 <homes89@ukr.net>
2020-08-31 10:58:05 +0300

    Grafana-UI: Add comments

--- packages/grafana-ui/src/components/Collapse/Collapse.tsx
+++ packages/grafana-ui/src/components/Collapse/Collapse.tsx
@@ -83,11 +83,15 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => ({
 }));
 
 export interface Props {
+  /** Expand or collapse te content */
   isOpen?: boolean;
+  /** Text for the Collapse header */
   label: string;
+  /** Indicates loading state of the content */
   loading?: boolean;
   /** Toggle collapsed header icon */
   collapsible?: boolean;
+  /** Callback for the toggle functionality */
   onToggle?: (isOpen: boolean) => void;
 }
 
