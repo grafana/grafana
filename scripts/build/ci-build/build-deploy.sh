67931559a6385ce47b227717c80233aec1f48a6d
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-03 12:48:06 +0200

    Make SDK URL a parameter
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- scripts/build/ci-build/build-deploy.sh
+++ scripts/build/ci-build/build-deploy.sh
@@ -7,5 +7,10 @@ _tag="grafana/build-container:${_version}"
 _dpath=$(dirname "${BASH_SOURCE[0]}")
 cd "$_dpath"
 
-docker build -t $_tag .
+if [[ -z "${OSX_SDK_URL}" ]]; then
+  echo You must set OSX_SDK_URL
+  exit 1
+fi
+
+docker build --build-arg OSX_SDK_URL="${OSX_SDK_URL}" -t $_tag .
 docker push $_tag
