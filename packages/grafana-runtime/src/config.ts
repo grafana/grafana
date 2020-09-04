4e452b6a84281c0f6b86532501f3f73ebe7dfd9b
Torkel Ödegaard <torkel@grafana.com>
2020-09-04 11:28:04 +0200

    Ts fix

--- packages/grafana-runtime/src/config.ts
+++ packages/grafana-runtime/src/config.ts
@@ -61,7 +61,7 @@ export class GrafanaBootConfig implements GrafanaConfig {
   licenseInfo: LicenseInfo = {} as LicenseInfo;
   rendererAvailable = false;
   http2Enabled = false;
-  dateFormats: SystemDateFormatSettings;
+  dateFormats: SystemDateFormatSettings = {} as SystemDateFormatSettings;
 
   constructor(options: GrafanaBootConfig) {
     this.theme = options.bootData.user.lightTheme ? getTheme(GrafanaThemeType.Light) : getTheme(GrafanaThemeType.Dark);
