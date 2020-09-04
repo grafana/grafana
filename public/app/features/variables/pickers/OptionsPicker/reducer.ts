6fea440c45cdd16115938b34bbf336a138c6a9db
josteinHellerud <jostein.hellerud@netlight.com>
2020-09-04 11:11:29 +0200

    reverted change

--- public/app/features/variables/pickers/OptionsPicker/reducer.ts
+++ public/app/features/variables/pickers/OptionsPicker/reducer.ts
@@ -61,6 +61,7 @@ const updateOptions = (state: OptionsPickerState): OptionsPickerState => {
     state.options = [];
     return state;
   }
+
   const selectedOptions = optionsToRecord(state.selectedValues);
   state.selectedValues = Object.values(selectedOptions);
 
