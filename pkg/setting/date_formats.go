4cf1e14625466a9edead625eb786fb1e7be8c014
Torkel Ödegaard <torkel@grafana.com>
2020-09-07 11:07:47 +0200

    Fixes

--- pkg/setting/date_formats.go
+++ pkg/setting/date_formats.go
@@ -16,13 +16,13 @@ type DateFormatIntervals struct {
 }
 
 func (cfg *Cfg) readDateFormats() {
-	preferences := cfg.Raw.Section("date_formats")
-	cfg.DateFormats.FullDate, _ = valueAsString(preferences, "full_date", "YYYY-MM-DD HH:mm:ss")
-	cfg.DateFormats.Interval.Second, _ = valueAsString(preferences, "interval_second", "HH:mm:ss")
-	cfg.DateFormats.Interval.Minute, _ = valueAsString(preferences, "interval_minute", "HH:mm")
-	cfg.DateFormats.Interval.Hour, _ = valueAsString(preferences, "interval_hour", "MM-DD HH:mm")
-	cfg.DateFormats.Interval.Day, _ = valueAsString(preferences, "interval_day", "YYYY-MM-DD")
-	cfg.DateFormats.Interval.Month, _ = valueAsString(preferences, "interval_month", "YYYY-MM")
+	dateFormats := cfg.Raw.Section("date_formats")
+	cfg.DateFormats.FullDate, _ = valueAsString(dateFormats, "full_date", "YYYY-MM-DD HH:mm:ss")
+	cfg.DateFormats.Interval.Second, _ = valueAsString(dateFormats, "interval_second", "HH:mm:ss")
+	cfg.DateFormats.Interval.Minute, _ = valueAsString(dateFormats, "interval_minute", "HH:mm")
+	cfg.DateFormats.Interval.Hour, _ = valueAsString(dateFormats, "interval_hour", "MM-DD HH:mm")
+	cfg.DateFormats.Interval.Day, _ = valueAsString(dateFormats, "interval_day", "YYYY-MM-DD")
+	cfg.DateFormats.Interval.Month, _ = valueAsString(dateFormats, "interval_month", "YYYY-MM")
 	cfg.DateFormats.Interval.Year = "YYYY"
-	cfg.DateFormats.UseBrowserLocale = preferences.Key("date_format_use_browser_locale").MustBool(false)
+	cfg.DateFormats.UseBrowserLocale = dateFormats.Key("date_format_use_browser_locale").MustBool(false)
 }
