add8bd1535c256a4ac63caa94cffa8e02f419570
Dominik Prokop <dominik.prokop@grafana.com>
2020-09-09 12:41:36 +0200

    Min selection distance for Zoom

--- packages/grafana-ui/src/components/uPlot/plugins/ZoomPlugin.tsx
+++ packages/grafana-ui/src/components/uPlot/plugins/ZoomPlugin.tsx
@@ -5,12 +5,20 @@ interface ZoomPluginProps {
   onZoom: (range: { from: number; to: number }) => void;
 }
 
+// min px width that triggers zoom
+const MIN_ZOOM_DIST = 5;
+
 export const ZoomPlugin: React.FC<ZoomPluginProps> = ({ onZoom }) => {
   return (
     <SelectionPlugin
       id="Zoom"
       /* very time series oriented for now */
-      onSelect={selection => onZoom({ from: selection.min * 1000, to: selection.max * 1000 })}
+      onSelect={selection => {
+        if (selection.bbox.width < MIN_ZOOM_DIST) {
+          return;
+        }
+        onZoom({ from: selection.min * 1000, to: selection.max * 1000 });
+      }}
     />
   );
 };
