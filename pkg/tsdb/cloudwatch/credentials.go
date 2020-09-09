4282573d83773c88a7977be6206417a67ca918f5
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-09 12:20:59 +0200

    Remove stale code
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/tsdb/cloudwatch/credentials.go
+++ pkg/tsdb/cloudwatch/credentials.go
@@ -1,18 +1,12 @@
 package cloudwatch
 
 import (
-	"fmt"
-	"os"
 	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
-	"github.com/aws/aws-sdk-go/aws/credentials/endpointcreds"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
-	"github.com/aws/aws-sdk-go/aws/defaults"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
@@ -41,27 +35,3 @@ var newSTSCredentials = stscreds.NewCredentials
 // Stubbable by tests.
 //nolint:gocritic
 var newEC2Metadata = ec2metadata.New
-
-func remoteCredProvider(sess *session.Session) credentials.Provider {
-	ecsCredURI := os.Getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
-
-	if len(ecsCredURI) > 0 {
-		return ecsCredProvider(sess, ecsCredURI)
-	}
-	return ec2RoleProvider(sess)
-}
-
-func ecsCredProvider(sess *session.Session, uri string) credentials.Provider {
-	const host = `169.254.170.2`
-
-	d := defaults.Get()
-	return endpointcreds.NewProviderClient(
-		*d.Config,
-		d.Handlers,
-		fmt.Sprintf("http://%s%s", host, uri),
-		func(p *endpointcreds.Provider) { p.ExpiryWindow = 5 * time.Minute })
-}
-
-func ec2RoleProvider(sess client.ConfigProvider) credentials.Provider {
-	return &ec2rolecreds.EC2RoleProvider{Client: newEC2Metadata(sess), ExpiryWindow: 5 * time.Minute}
-}
--- pkg/tsdb/cloudwatch/credentials_test.go
+++ pkg/tsdb/cloudwatch/credentials_test.go
@@ -1,49 +1,17 @@
 package cloudwatch
 
 import (
-	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
-	"github.com/aws/aws-sdk-go/aws/credentials/endpointcreds"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestECSCredProvider(t *testing.T) {
-	os.Setenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI", "/abc/123")
-	t.Cleanup(func() {
-		os.Unsetenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
-	})
-
-	sess, err := session.NewSession()
-	require.NoError(t, err)
-	provider := remoteCredProvider(sess)
-	require.NotNil(t, provider)
-
-	ecsProvider, ok := provider.(*endpointcreds.Provider)
-	require.NotNil(t, ecsProvider)
-	require.True(t, ok)
-
-	assert.Equal(t, "http://169.254.170.2/abc/123", ecsProvider.Client.Endpoint)
-}
-
-func TestDefaultEC2RoleProvider(t *testing.T) {
-	sess, err := session.NewSession()
-	require.NoError(t, err)
-	provider := remoteCredProvider(sess)
-	require.NotNil(t, provider)
-
-	ec2Provider, ok := provider.(*ec2rolecreds.EC2RoleProvider)
-	require.NotNil(t, ec2Provider)
-	require.True(t, ok)
-}
 
 // Test cloudWatchExecutor.newSession with assumption of IAM role.
 func TestNewSession_AssumeRole(t *testing.T) {
