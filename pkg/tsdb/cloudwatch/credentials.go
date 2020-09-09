33261504509dd29b9ae04f225cdc8e4c0b556a9d
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-09 15:08:33 +0200

    Remove stale code
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/tsdb/cloudwatch/credentials.go
+++ pkg/tsdb/cloudwatch/credentials.go
@@ -1,23 +1,11 @@
 package cloudwatch
 
 import (
-	"sync"
-	"time"
-
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
-
-type envelope struct {
-	credentials *credentials.Credentials
-	expiration  *time.Time
-}
-
-var awsCredsCache = map[string]envelope{}
-var credsCacheLock sync.RWMutex
 
 // Session factory.
 // Stubbable by tests.
