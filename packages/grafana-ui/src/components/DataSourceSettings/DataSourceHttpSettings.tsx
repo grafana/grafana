c63cb1809f2028cd2d73c49c81f94ef2b1e373a2
Peter Holmberg <peterholmberg@users.noreply.github.com>
2020-09-11 15:53:55 +0200

    grafana/ui: Documenting DataSourceHttpSettings (#27547)
    * documenting component
    
    * Update packages/grafana-ui/src/components/DataSourceSettings/DataSourceHttpSettings.mdx
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * text updates
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>

--- packages/grafana-ui/src/components/DataSourceSettings/DataSourceHttpSettings.tsx
+++ packages/grafana-ui/src/components/DataSourceSettings/DataSourceHttpSettings.tsx
@@ -1,20 +1,19 @@
 import React, { useState, useCallback } from 'react';
-import { SelectableValue } from '@grafana/data';
 import { css, cx } from 'emotion';
-import { useTheme } from '../../themes';
+import { DataSourceSettings, SelectableValue } from '@grafana/data';
 import { BasicAuthSettings } from './BasicAuthSettings';
 import { HttpProxySettings } from './HttpProxySettings';
 import { TLSAuthSettings } from './TLSAuthSettings';
-import { DataSourceSettings } from '@grafana/data';
-import { HttpSettingsProps } from './types';
 import { CustomHeadersSettings } from './CustomHeadersSettings';
 import { Select } from '../Forms/Legacy/Select/Select';
 import { Input } from '../Forms/Legacy/Input/Input';
+import { Switch } from '../Forms/Legacy/Switch/Switch';
 import { Icon } from '../Icon/Icon';
 import { FormField } from '../FormField/FormField';
 import { FormLabel } from '../FormLabel/FormLabel';
-import { Switch } from '../Forms/Legacy/Switch/Switch';
 import { TagsInput } from '../TagsInput/TagsInput';
+import { useTheme } from '../../themes';
+import { HttpSettingsProps } from './types';
 
 const ACCESS_OPTIONS: Array<SelectableValue<string>> = [
   {
