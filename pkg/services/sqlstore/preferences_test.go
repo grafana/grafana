39eba5065bfdd86686bfe3422a2f84b49f0d32f9
Wouter Smeenk <woutersmeenk@gmail.com>
2020-09-15 15:20:53 +0200

    Dashboard: Support configuring default timezone via config file (#27404)
    Add a default timezone that the administrator can set in the settings. 
    This setting is be used as default for the users timezone preference.
    Can be used when creating Grafana instances without administrator 
    intervention, in order to give user the correct default timezone.
    
    Fixes #25654

--- pkg/services/sqlstore/preferences_test.go
+++ pkg/services/sqlstore/preferences_test.go
@@ -11,14 +11,17 @@ import (
 
 func TestPreferencesDataAccess(t *testing.T) {
 	Convey("Testing preferences data access", t, func() {
-		InitTestDB(t)
+		ss := InitTestDB(t)
 
 		Convey("GetPreferencesWithDefaults with no saved preferences should return defaults", func() {
+			setting.DefaultTheme = "light"
+			ss.Cfg.DateFormats.DefaultTimezone = "UTC"
+
 			query := &models.GetPreferencesWithDefaultsQuery{User: &models.SignedInUser{}}
-			err := GetPreferencesWithDefaults(query)
+			err := ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
-			So(query.Result.Theme, ShouldEqual, setting.DefaultTheme)
-			So(query.Result.Timezone, ShouldEqual, "browser")
+			So(query.Result.Theme, ShouldEqual, "light")
+			So(query.Result.Timezone, ShouldEqual, "UTC")
 			So(query.Result.HomeDashboardId, ShouldEqual, 0)
 		})
 
@@ -29,7 +32,7 @@ func TestPreferencesDataAccess(t *testing.T) {
 			So(err, ShouldBeNil)
 
 			query := &models.GetPreferencesWithDefaultsQuery{User: &models.SignedInUser{OrgId: 1, UserId: 1}}
-			err = GetPreferencesWithDefaults(query)
+			err = ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
 			So(query.Result.HomeDashboardId, ShouldEqual, 4)
 		})
@@ -41,7 +44,7 @@ func TestPreferencesDataAccess(t *testing.T) {
 			So(err, ShouldBeNil)
 
 			query := &models.GetPreferencesWithDefaultsQuery{User: &models.SignedInUser{OrgId: 1, UserId: 2}}
-			err = GetPreferencesWithDefaults(query)
+			err = ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
 			So(query.Result.HomeDashboardId, ShouldEqual, 1)
 		})
@@ -57,7 +60,7 @@ func TestPreferencesDataAccess(t *testing.T) {
 			query := &models.GetPreferencesWithDefaultsQuery{
 				User: &models.SignedInUser{OrgId: 1, Teams: []int64{2, 3}},
 			}
-			err = GetPreferencesWithDefaults(query)
+			err = ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
 			So(query.Result.HomeDashboardId, ShouldEqual, 3)
 		})
@@ -71,7 +74,7 @@ func TestPreferencesDataAccess(t *testing.T) {
 			So(err, ShouldBeNil)
 
 			query := &models.GetPreferencesWithDefaultsQuery{User: &models.SignedInUser{OrgId: 1}}
-			err = GetPreferencesWithDefaults(query)
+			err = ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
 			So(query.Result.HomeDashboardId, ShouldEqual, 1)
 		})
@@ -89,7 +92,7 @@ func TestPreferencesDataAccess(t *testing.T) {
 			query := &models.GetPreferencesWithDefaultsQuery{
 				User: &models.SignedInUser{OrgId: 1, UserId: 1, Teams: []int64{2, 3}},
 			}
-			err = GetPreferencesWithDefaults(query)
+			err = ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
 			So(query.Result.HomeDashboardId, ShouldEqual, 4)
 		})
@@ -107,7 +110,7 @@ func TestPreferencesDataAccess(t *testing.T) {
 			query := &models.GetPreferencesWithDefaultsQuery{
 				User: &models.SignedInUser{OrgId: 1, UserId: 2},
 			}
-			err = GetPreferencesWithDefaults(query)
+			err = ss.GetPreferencesWithDefaults(query)
 			So(err, ShouldBeNil)
 			So(query.Result.HomeDashboardId, ShouldEqual, 1)
 		})
