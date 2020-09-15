13fe3fe354fb982cbc4f1bed6b5327458e4e4c0c
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-09-15 08:06:04 +0200

    Postgres: Support request cancellation properly (Uses new backendSrv.fetch Observable request API) (#27478)
    * Postgres: Replaces dataSourceRequest with fetch
    
    * Postgres: Replaces dataSourceRequest with fetch
    
    * Tests: removes unnecessary import

--- public/app/plugins/datasource/postgres/types.ts
+++ public/app/plugins/datasource/postgres/types.ts
@@ -1,7 +1,13 @@
+import { MetricFindValue } from '@grafana/data';
+
 export interface PostgresQueryForInterpolation {
   alias?: any;
   format?: any;
   rawSql?: any;
   refId?: any;
   hide?: any;
+}
+
+export interface PostgresMetricFindValue extends MetricFindValue {
+  value?: string;
 }
