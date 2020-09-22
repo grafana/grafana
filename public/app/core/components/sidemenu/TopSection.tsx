ea6d790dc7a756b54f0ad17e34d2334653f58d80
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2020-09-22 11:43:12 +0100

    PMM-6573 Switch DBaaS (#6)
    * PMM-6468 add dbaas menu
    
    * PMM-6468 remove unnecessary routes and menu children
    
    * PMM-6468 get rid of /saas folder
    
    * PMM-6468 Remove extra line
    
    * PMM-6468 Add DBaaS menu item
    
    * PMM-6468 Update TopSection snapshots
    
    * PMM-6573 Show DBaaS menu based on settings request
    
    * PMM-6573 Refactor BE call and useState
    
    * PMM-6573 Fix request origin
    
    Co-authored-by: Alex Tymchuk <alexander.tymchuk@percona.com>

--- public/app/core/components/sidemenu/TopSection.tsx
+++ public/app/core/components/sidemenu/TopSection.tsx
@@ -1,20 +1,37 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import _ from 'lodash';
 import TopSectionItem from './TopSectionItem';
 import config from '../../config';
-import { getLocationSrv } from '@grafana/runtime';
+import { getLocationSrv, getBackendSrv } from '@grafana/runtime';
 
 const TopSection: FC<any> = () => {
+  const [showDBaaS, setShowDBaaS] = useState(false);
   const navTree = _.cloneDeep(config.bootData.navTree);
   const mainLinks = _.filter(navTree, (item) => !item.hideFromMenu);
   const searchLink = {
     text: 'Search',
     icon: 'search',
   };
-
+  const dbaasLink = {
+    id: 'dbaas',
+    icon: 'database',
+    text: 'DBaaS',
+    url: '/graph/d/pmm-dbaas/pmm-dbaas',
+  };
   const onOpenSearch = () => {
     getLocationSrv().update({ query: { search: 'open' }, partial: true });
   };
+  const updateDBaaS = async () => {
+    const { settings } = await getBackendSrv().post(`${window.location.origin}/v1/Settings/Get`);
+    setShowDBaaS(settings.dbaas_enabled);
+  };
+
+  // TODO: once DBaaS is enabled by default move it to config file (api/index.go)
+  useEffect(() => {
+    if (config.bootData.user.isSignedIn) {
+      updateDBaaS();
+    }
+  }, []);
 
   return (
     <div className="sidemenu__top">
@@ -22,6 +39,7 @@ const TopSection: FC<any> = () => {
       {mainLinks.map((link, index) => {
         return <TopSectionItem link={link} key={`${link.id}-${index}`} />;
       })}
+      {showDBaaS && <TopSectionItem link={dbaasLink} />}
     </div>
   );
 };
