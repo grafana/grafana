8c8de31c4947766346770096f637cd3038922eeb
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-23 10:55:25 +0200

    Chore: Upgrade golangci-lint (#27719)
    * Chore: Upgrade golangci-lint to latest
    * ci-build: Upgrade Node/Yarn
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- scripts/build/ci-build/build-deploy.sh
+++ scripts/build/ci-build/build-deploy.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-_version="1.2.26"
+_version="1.2.27"
 _tag="grafana/build-container:${_version}"
 
 _dpath=$(dirname "${BASH_SOURCE[0]}")
 cd "$_dpath"
 
-if [[ -z "${OSX_SDK_URL}" ]]; then
-  echo You must set OSX_SDK_URL
-  exit 1
-fi
-
-docker build --build-arg OSX_SDK_URL="${OSX_SDK_URL}" -t $_tag .
+docker build -t $_tag .
 docker push $_tag
