112dd0fe4cff3e6b893578ea7c9492cbeda22302
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-23 10:24:59 +0200

    ci-build: Upgrade Node/Yarn
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- scripts/build/ci-build/build-deploy.sh
+++ scripts/build/ci-build/build-deploy.sh
@@ -7,5 +7,5 @@ _tag="grafana/build-container:${_version}"
 _dpath=$(dirname "${BASH_SOURCE[0]}")
 cd "$_dpath"
 
-docker build --build-arg OSX_SDK_URL="${OSX_SDK_URL}" -t $_tag .
+docker build -t $_tag .
 docker push $_tag
