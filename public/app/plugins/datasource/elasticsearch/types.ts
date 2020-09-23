4c7131425ba048c648435410f2153d4d85f20304
Giordano Ricci <gio.ricci@grafana.com>
2020-09-23 12:24:46 +0100

    Revert "Elasticsearch: add frozen indices search support (#27472)" (#27726)
    This reverts commit 61a3160c34f659be848d67a10bd9e0e5be6e1b58.

--- public/app/plugins/datasource/elasticsearch/types.ts
+++ public/app/plugins/datasource/elasticsearch/types.ts
@@ -9,7 +9,6 @@ export interface ElasticsearchOptions extends DataSourceJsonData {
   logMessageField?: string;
   logLevelField?: string;
   dataLinks?: DataLinkConfig[];
-  includeFrozen?: boolean;
 }
 
 export interface ElasticsearchAggregation {
