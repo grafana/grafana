9030cd9858a26dee6d0a751f28a414cca564eb75
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-23 09:42:04 +0200

    ci-build: Upgrade golangci-lint
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- scripts/build/ci-build/build-deploy.sh
+++ scripts/build/ci-build/build-deploy.sh
@@ -7,10 +7,5 @@ _tag="grafana/build-container:${_version}"
 _dpath=$(dirname "${BASH_SOURCE[0]}")
 cd "$_dpath"
 
-if [[ -z "${OSX_SDK_URL}" ]]; then
-  echo You must set OSX_SDK_URL
-  exit 1
-fi
-
 docker build --build-arg OSX_SDK_URL="${OSX_SDK_URL}" -t $_tag .
 docker push $_tag
