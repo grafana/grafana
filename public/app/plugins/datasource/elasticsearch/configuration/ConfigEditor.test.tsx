d32241721793e70295601958d96e9f39c751b4e4
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2020-09-25 15:48:41 +0100

    Chore: Upgrade typescript and related packages (#27316)
    * Chore: Upgrade typescript and related packages
    Closes #27115
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * fixes some tests
    
    * Fix failing tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Dominik Prokop <dominik.prokop@grafana.com>
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    Co-authored-by: Dominik Prokop <dominik.prokop@grafana.com>

--- public/app/plugins/datasource/elasticsearch/configuration/ConfigEditor.test.tsx
+++ public/app/plugins/datasource/elasticsearch/configuration/ConfigEditor.test.tsx
@@ -20,7 +20,9 @@ describe('ConfigEditor', () => {
 
   it('should set defaults', () => {
     const options = createDefaultConfigOptions();
+    //@ts-ignore
     delete options.jsonData.esVersion;
+    //@ts-ignore
     delete options.jsonData.timeField;
     delete options.jsonData.maxConcurrentShardRequests;
 
