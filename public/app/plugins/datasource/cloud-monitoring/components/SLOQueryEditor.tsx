9459414c9b4acff444ffee5d7188e6e4d99d84f3
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-09-29 21:09:34 +0200

    Update stackdriver query editor not to overwrite project name

--- public/app/plugins/datasource/cloud-monitoring/components/SLOQueryEditor.tsx
+++ public/app/plugins/datasource/cloud-monitoring/components/SLOQueryEditor.tsx
@@ -15,14 +15,14 @@ export interface Props {
   datasource: CloudMonitoringDatasource;
 }
 
-export const defaultQuery: SLOQuery = {
-  projectName: '',
+export const defaultQuery: (dataSource: CloudMonitoringDatasource) => SLOQuery = dataSource => ({
+  projectName: dataSource.getDefaultProject(),
   alignmentPeriod: 'cloud-monitoring-auto',
   aliasBy: '',
   selectorName: 'select_slo_health',
   serviceId: '',
   sloId: '',
-};
+});
 
 export function SLOQueryEditor({
   query,
