d568e889efc1133b68b5b4402e4e439de603e39c
bergquist <carl.bergquist@gmail.com>
2020-09-30 12:18:09 +0200

    fixes rebase errors

--- pkg/infra/metrics/metrics.go
+++ pkg/infra/metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"errors"
+	"fmt"
 	"runtime"
 	"strings"
 
--- pkg/infra/metrics/metrics_test.go
+++ pkg/infra/metrics/metrics_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLabelNameSanitization(t *testing.T) {
