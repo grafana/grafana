6533c43da01a4b2973435f30af5aa0e8ad9042b9
bergquist <carl.bergquist@gmail.com>
2020-09-30 10:45:45 +0200

    removes invalid chars from label names
    skip instrumenting datasources if datasource
    names only contains invalid chars

--- pkg/infra/metrics/metrics.go
+++ pkg/infra/metrics/metrics.go
@@ -1,7 +1,9 @@
 package metrics
 
 import (
+	"errors"
 	"runtime"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 
@@ -588,4 +590,26 @@ func newCounterStartingAtZero(opts prometheus.CounterOpts, labelValues ...string
 	counter.Add(0)
 
 	return counter
+}
+
+// SanitizeLabelName removes all invalid chars from the label name.
+// If the label name is empty or contains only invalid chars, it
+// will return an error.
+func SanitizeLabelName(name string) (string, error) {
+	if len(name) == 0 {
+		return "", errors.New("label name cannot be empty")
+	}
+
+	out := strings.Builder{}
+	for i, b := range name {
+		if (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || b == '_' || (b >= '0' && b <= '9' && i > 0) {
+			out.WriteRune(b)
+		}
+	}
+
+	if out.Len() == 0 {
+		return "", errors.New("label name only contains invalid chars")
+	}
+
+	return out.String(), nil
 }
---
+++ pkg/infra/metrics/metrics_test.go
@@ -0,0 +1,29 @@
+package metrics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLabelNameSanitization(t *testing.T) {
+	testcases := []struct {
+		input    string
+		expected string
+		err      bool
+	}{
+		{input: "job", expected: "job"},
+		{input: "job._loal['", expected: "job_loal"},
+		{input: "", expected: "", err: true},
+		{input: ";;;", expected: "", err: true},
+	}
+
+	for _, tc := range testcases {
+		got, err := SanitizeLabelName(tc.input)
+		if tc.err {
+			assert.Error(t, err)
+		} else {
+			assert.Equal(t, tc.expected, got)
+		}
+	}
+}
