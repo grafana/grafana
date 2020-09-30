1ea0e2ba2e52f854e42b9a4b85d89bced0326695
Victor Cinaglia <victorcinaglia@gmail.com>
2020-09-30 15:47:49 -0400

    remove unused function to interpolate URLs

--- pkg/api/pluginproxy/utils.go
+++ pkg/api/pluginproxy/utils.go
@@ -4,11 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
-	"net/url"
 	"text/template"
 
 	"github.com/grafana/grafana/pkg/models"
-	"github.com/grafana/grafana/pkg/plugins"
 )
 
 // InterpolateString accepts template data and return a string with substitutions
@@ -25,28 +23,6 @@ func InterpolateString(text string, data templateData) (string, error) {
 	}
 
 	return contentBuf.String(), nil
-}
-
-// InterpolateURL accepts template data and return a string with substitutions
-func InterpolateURL(anURL *url.URL, route *plugins.AppPluginRoute, orgID int64, appID string) (*url.URL, error) {
-	query := models.GetPluginSettingByIdQuery{OrgId: orgID, PluginId: appID}
-	result, err := url.Parse(anURL.String())
-	if query.Result != nil {
-		if len(query.Result.JsonData) > 0 {
-			data := templateData{
-				JsonData: query.Result.JsonData,
-			}
-			interpolatedResult, err := InterpolateString(anURL.String(), data)
-			if err == nil {
-				result, err = url.Parse(interpolatedResult)
-				if err != nil {
-					return nil, fmt.Errorf("Error parsing plugin route url %v", err)
-				}
-			}
-		}
-	}
-
-	return result, err
 }
 
 // Set the X-Grafana-User header if needed (and remove if not)
