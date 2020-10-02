e23ef28be95a8ffc6adfd94cc389a3ce0829bdaf
Clarity-89 <homes89@ukr.net>
2020-10-02 13:25:00 +0300

    Convert scenarios to ts

--- public/app/plugins/datasource/testdata/QueryEditor.test.tsx
+++ public/app/plugins/datasource/testdata/QueryEditor.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { defaultQuery } from './constants';
 import { QueryEditor, Props } from './QueryEditor';
-import scenarios from './__mocks__/scenarios';
+import { scenarios } from './__mocks__/scenarios';
 
 beforeEach(() => {
   jest.clearAllMocks();
