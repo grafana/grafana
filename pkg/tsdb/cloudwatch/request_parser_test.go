acf86cf77709b56f52636e948d895425022e2827
Matheus Alcantara <matheusssilv97@gmail.com>
2020-10-02 11:36:04 -0300

    Cloudwatch: Convert go convey tests to standard test library

--- pkg/tsdb/cloudwatch/request_parser_test.go
+++ pkg/tsdb/cloudwatch/request_parser_test.go
@@ -6,211 +6,207 @@ import (
 
 	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/tsdb"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestParser(t *testing.T) {
-	Convey("TestRequestParser", t, func() {
+	timeRange := tsdb.NewTimeRange("now-1h", "now-2h")
+	from, err := timeRange.ParseFrom()
+	require.Nil(t, err)
+	to, err := timeRange.ParseTo()
+	require.Nil(t, err)
+
+	t.Run("New dimensions structure", func(t *testing.T) {
+		query := simplejson.NewFromAny(map[string]interface{}{
+			"refId":      "ref1",
+			"region":     "us-east-1",
+			"namespace":  "ec2",
+			"metricName": "CPUUtilization",
+			"id":         "",
+			"expression": "",
+			"dimensions": map[string]interface{}{
+				"InstanceId":   []interface{}{"test"},
+				"InstanceType": []interface{}{"test2", "test3"},
+			},
+			"statistics": []interface{}{"Average"},
+			"period":     "600",
+			"hide":       false,
+		})
+
+		res, err := parseRequestQuery(query, "ref1", from, to)
+		require.Nil(t, err)
+		assert.Equal(t, res.Region, "us-east-1")
+		assert.Equal(t, res.RefId, "ref1")
+		assert.Equal(t, res.Namespace, "ec2")
+		assert.Equal(t, res.MetricName, "CPUUtilization")
+		assert.Equal(t, res.Id, "")
+		assert.Equal(t, res.Expression, "")
+		assert.Equal(t, res.Period, 600)
+		assert.Equal(t, res.ReturnData, true)
+		assert.Equal(t, len(res.Dimensions), 2)
+		assert.Equal(t, len(res.Dimensions["InstanceId"]), 1)
+		assert.Equal(t, len(res.Dimensions["InstanceType"]), 2)
+		assert.Equal(t, res.Dimensions["InstanceType"][1], "test3")
+		assert.Equal(t, len(res.Statistics), 1)
+		assert.Equal(t, *res.Statistics[0], "Average")
+	})
+
+	t.Run("Old dimensions structure (backwards compatibility)", func(t *testing.T) {
+		query := simplejson.NewFromAny(map[string]interface{}{
+			"refId":      "ref1",
+			"region":     "us-east-1",
+			"namespace":  "ec2",
+			"metricName": "CPUUtilization",
+			"id":         "",
+			"expression": "",
+			"dimensions": map[string]interface{}{
+				"InstanceId":   "test",
+				"InstanceType": "test2",
+			},
+			"statistics": []interface{}{"Average"},
+			"period":     "600",
+			"hide":       false,
+		})
+
+		res, err := parseRequestQuery(query, "ref1", from, to)
+		require.Nil(t, err)
+		assert.Equal(t, res.Region, "us-east-1")
+		assert.Equal(t, res.RefId, "ref1")
+		assert.Equal(t, res.Namespace, "ec2")
+		assert.Equal(t, res.MetricName, "CPUUtilization")
+		assert.Equal(t, res.Id, "")
+		assert.Equal(t, res.Expression, "")
+		assert.Equal(t, res.Period, 600)
+		assert.Equal(t, res.ReturnData, true)
+		assert.Equal(t, len(res.Dimensions), 2)
+		assert.Equal(t, len(res.Dimensions["InstanceId"]), 1)
+		assert.Equal(t, len(res.Dimensions["InstanceType"]), 1)
+		assert.Equal(t, res.Dimensions["InstanceType"][0], "test2")
+		assert.Equal(t, *res.Statistics[0], "Average")
+	})
+
+	t.Run("Period defined in the editor by the user is being used when time range is short", func(t *testing.T) {
+		query := simplejson.NewFromAny(map[string]interface{}{
+			"refId":      "ref1",
+			"region":     "us-east-1",
+			"namespace":  "ec2",
+			"metricName": "CPUUtilization",
+			"id":         "",
+			"expression": "",
+			"dimensions": map[string]interface{}{
+				"InstanceId":   "test",
+				"InstanceType": "test2",
+			},
+			"statistics": []interface{}{"Average"},
+			"hide":       false,
+		})
+		query.Set("period", "900")
 		timeRange := tsdb.NewTimeRange("now-1h", "now-2h")
 		from, err := timeRange.ParseFrom()
-		So(err, ShouldBeNil)
+		require.Nil(t, err)
 		to, err := timeRange.ParseTo()
-		So(err, ShouldBeNil)
-		Convey("when parsing query editor row json", func() {
-			Convey("using new dimensions structure", func() {
-				query := simplejson.NewFromAny(map[string]interface{}{
-					"refId":      "ref1",
-					"region":     "us-east-1",
-					"namespace":  "ec2",
-					"metricName": "CPUUtilization",
-					"id":         "",
-					"expression": "",
-					"dimensions": map[string]interface{}{
-						"InstanceId":   []interface{}{"test"},
-						"InstanceType": []interface{}{"test2", "test3"},
-					},
-					"statistics": []interface{}{"Average"},
-					"period":     "600",
-					"hide":       false,
-				})
+		require.Nil(t, err)
 
-				res, err := parseRequestQuery(query, "ref1", from, to)
-				So(err, ShouldBeNil)
-				So(res.Region, ShouldEqual, "us-east-1")
-				So(res.RefId, ShouldEqual, "ref1")
-				So(res.Namespace, ShouldEqual, "ec2")
-				So(res.MetricName, ShouldEqual, "CPUUtilization")
-				So(res.Id, ShouldEqual, "")
-				So(res.Expression, ShouldEqual, "")
-				So(res.Period, ShouldEqual, 600)
-				So(res.ReturnData, ShouldEqual, true)
-				So(len(res.Dimensions), ShouldEqual, 2)
-				So(len(res.Dimensions["InstanceId"]), ShouldEqual, 1)
-				So(len(res.Dimensions["InstanceType"]), ShouldEqual, 2)
-				So(res.Dimensions["InstanceType"][1], ShouldEqual, "test3")
-				So(len(res.Statistics), ShouldEqual, 1)
-				So(*res.Statistics[0], ShouldEqual, "Average")
-			})
+		res, err := parseRequestQuery(query, "ref1", from, to)
+		require.Nil(t, err)
+		assert.Equal(t, res.Period, 900)
+	})
 
-			Convey("using old dimensions structure (backwards compatibility)", func() {
-				query := simplejson.NewFromAny(map[string]interface{}{
-					"refId":      "ref1",
-					"region":     "us-east-1",
-					"namespace":  "ec2",
-					"metricName": "CPUUtilization",
-					"id":         "",
-					"expression": "",
-					"dimensions": map[string]interface{}{
-						"InstanceId":   "test",
-						"InstanceType": "test2",
-					},
-					"statistics": []interface{}{"Average"},
-					"period":     "600",
-					"hide":       false,
-				})
+	t.Run("Period is parsed correctly if not defined by user", func(t *testing.T) {
+		query := simplejson.NewFromAny(map[string]interface{}{
+			"refId":      "ref1",
+			"region":     "us-east-1",
+			"namespace":  "ec2",
+			"metricName": "CPUUtilization",
+			"id":         "",
+			"expression": "",
+			"dimensions": map[string]interface{}{
+				"InstanceId":   "test",
+				"InstanceType": "test2",
+			},
+			"statistics": []interface{}{"Average"},
+			"hide":       false,
+			"period":     "auto",
+		})
 
-				res, err := parseRequestQuery(query, "ref1", from, to)
-				So(err, ShouldBeNil)
-				So(res.Region, ShouldEqual, "us-east-1")
-				So(res.RefId, ShouldEqual, "ref1")
-				So(res.Namespace, ShouldEqual, "ec2")
-				So(res.MetricName, ShouldEqual, "CPUUtilization")
-				So(res.Id, ShouldEqual, "")
-				So(res.Expression, ShouldEqual, "")
-				So(res.Period, ShouldEqual, 600)
-				So(res.ReturnData, ShouldEqual, true)
-				So(len(res.Dimensions), ShouldEqual, 2)
-				So(len(res.Dimensions["InstanceId"]), ShouldEqual, 1)
-				So(len(res.Dimensions["InstanceType"]), ShouldEqual, 1)
-				So(res.Dimensions["InstanceType"][0], ShouldEqual, "test2")
-				So(*res.Statistics[0], ShouldEqual, "Average")
-			})
+		t.Run("Time range is 5 minutes", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.Local().Add(time.Minute * time.Duration(5))
 
-			Convey("period defined in the editor by the user is being used", func() {
-				query := simplejson.NewFromAny(map[string]interface{}{
-					"refId":      "ref1",
-					"region":     "us-east-1",
-					"namespace":  "ec2",
-					"metricName": "CPUUtilization",
-					"id":         "",
-					"expression": "",
-					"dimensions": map[string]interface{}{
-						"InstanceId":   "test",
-						"InstanceType": "test2",
-					},
-					"statistics": []interface{}{"Average"},
-					"hide":       false,
-				})
-				Convey("when time range is short", func() {
-					query.Set("period", "900")
-					timeRange := tsdb.NewTimeRange("now-1h", "now-2h")
-					from, err := timeRange.ParseFrom()
-					So(err, ShouldBeNil)
-					to, err := timeRange.ParseTo()
-					So(err, ShouldBeNil)
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 60)
+		})
 
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 900)
-				})
-			})
+		t.Run("Time range is 1 day", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(0, 0, -1)
 
-			Convey("period is parsed correctly if not defined by user", func() {
-				query := simplejson.NewFromAny(map[string]interface{}{
-					"refId":      "ref1",
-					"region":     "us-east-1",
-					"namespace":  "ec2",
-					"metricName": "CPUUtilization",
-					"id":         "",
-					"expression": "",
-					"dimensions": map[string]interface{}{
-						"InstanceId":   "test",
-						"InstanceType": "test2",
-					},
-					"statistics": []interface{}{"Average"},
-					"hide":       false,
-					"period":     "auto",
-				})
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 60)
+		})
 
-				Convey("when time range is 5 minutes", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.Local().Add(time.Minute * time.Duration(5))
+		t.Run("Time range is 2 days", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(0, 0, -2)
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 300)
+		})
 
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 60)
-				})
+		t.Run("Time range is 7 days", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(0, 0, -7)
 
-				Convey("when time range is 1 day", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(0, 0, -1)
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 900)
+		})
 
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 60)
-				})
+		t.Run("Time range is 30 days", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(0, 0, -30)
 
-				Convey("when time range is 2 days", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(0, 0, -2)
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 300)
-				})
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 3600)
+		})
 
-				Convey("when time range is 7 days", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(0, 0, -7)
+		t.Run("Time range is 90 days", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(0, 0, -90)
 
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 900)
-				})
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 21600)
+		})
 
-				Convey("when time range is 30 days", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(0, 0, -30)
+		t.Run("Time range is 1 year", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(-1, 0, 0)
 
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 3600)
-				})
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 21600)
+		})
 
-				Convey("when time range is 90 days", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(0, 0, -90)
+		t.Run("Time range is 2 years", func(t *testing.T) {
+			query.Set("period", "auto")
+			to := time.Now()
+			from := to.AddDate(-2, 0, 0)
 
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 21600)
-				})
-
-				Convey("when time range is 1 year", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(-1, 0, 0)
-
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 21600)
-				})
-
-				Convey("when time range is 2 years", func() {
-					query.Set("period", "auto")
-					to := time.Now()
-					from := to.AddDate(-2, 0, 0)
-
-					res, err := parseRequestQuery(query, "ref1", from, to)
-					So(err, ShouldBeNil)
-					So(res.Period, ShouldEqual, 86400)
-				})
-			})
+			res, err := parseRequestQuery(query, "ref1", from, to)
+			require.Nil(t, err)
+			assert.Equal(t, res.Period, 86400)
 		})
 	})
 }
