6d51dacf5c4f18ecc57ee90eb54d0ce61f2bc70e
CirceCI <circleci@grafana.com>
2020-10-06 16:21:19 -0400

    wtf5

--- jest.config.js
+++ jest.config.js
@@ -19,4 +19,5 @@ module.exports = {
     'monaco-editor/esm/vs/editor/editor.api': '<rootDir>/public/test/mocks/monaco.ts',
   },
   watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  preset: 'ts-jest',
 };
