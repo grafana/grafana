28c9152d05c216ffd5bad59a76a3ce4a9cee6f03
Marcus Andersson <marcus.andersson@grafana.com>
2020-10-06 15:30:22 +0200

    increased the limit.

--- scripts/ci-reference-docs-lint.sh
+++ scripts/ci-reference-docs-lint.sh
@@ -30,7 +30,7 @@ if [ ! -d "$REPORT_PATH" ]; then
 fi
 
 WARNINGS_COUNT="$(find "$REPORT_PATH" -type f -name \*.log -print0 | xargs -0 grep -o "\[33mWarning:" | wc -l | xargs)"
-WARNINGS_COUNT_LIMIT=900
+WARNINGS_COUNT_LIMIT=1045
 
 if [ "$WARNINGS_COUNT" -gt $WARNINGS_COUNT_LIMIT ]; then
   echo -e "API Extractor warnings/errors $WARNINGS_COUNT exceeded $WARNINGS_COUNT_LIMIT so failing build.\n"
