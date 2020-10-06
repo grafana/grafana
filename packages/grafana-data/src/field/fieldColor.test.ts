c0461ba5aca0880b24882ae5dc3edaaf313d99ec
Torkel Ödegaard <torkel@grafana.com>
2020-10-06 11:23:03 +0200

    added more tests

---
+++ packages/grafana-data/src/field/fieldColor.test.ts
@@ -0,0 +1,29 @@
+import { Field, GrafanaThemeType, GrafanaTheme, FieldColorMode } from '../types';
+import { fieldColorModeRegistry, FieldValueColorCalculator } from './fieldColor';
+
+describe('fieldColorModeRegistry', () => {
+  interface GetCalcOptions {
+    mode: FieldColorMode;
+    seriesIndex?: number;
+  }
+
+  function getCalculator(options: GetCalcOptions): FieldValueColorCalculator {
+    const mode = fieldColorModeRegistry.get(options.mode);
+    return mode.getCalculator({} as Field, options.seriesIndex ?? 0, { type: GrafanaThemeType.Dark } as GrafanaTheme);
+  }
+
+  it('Schemes should interpolate', () => {
+    const calcFn = getCalculator({ mode: FieldColorMode.SpectrumGrYlRd });
+    expect(calcFn(70, 0.5, undefined)).toEqual('rgb(226, 192, 61)');
+  });
+
+  it('Palette classic with series index 0', () => {
+    const calcFn = getCalculator({ mode: FieldColorMode.PaletteClassic, seriesIndex: 0 });
+    expect(calcFn(70, 0, undefined)).toEqual('#7EB26D');
+  });
+
+  it('Palette classic with series index 1', () => {
+    const calcFn = getCalculator({ mode: FieldColorMode.PaletteClassic, seriesIndex: 1 });
+    expect(calcFn(70, 0, undefined)).toEqual('#EAB839');
+  });
+});
