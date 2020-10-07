75b8e1e770967cb5ce83e34d71f82c1826341f30
Todd Campbell <todd@sensu.io>
2020-10-07 13:03:31 -0400

    Replace convey testing with testify
    Signed-off-by: Todd Campbell <todd@sensu.io>

--- pkg/services/alerting/notifiers/sensugo_test.go
+++ pkg/services/alerting/notifiers/sensugo_test.go
@@ -3,60 +3,54 @@ package notifiers
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/models"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestSensuGoNotifier(t *testing.T) {
-	Convey("Sensu Go notifier tests", t, func() {
-		Convey("Parsing alert notification from settings", func() {
-			Convey("empty settings should return error", func() {
-				json := `{ }`
+	json := `{ }`
 
-				settingsJSON, err := simplejson.NewJson([]byte(json))
-				So(err, ShouldBeNil)
-				model := &models.AlertNotification{
-					Name:     "Sensu Go",
-					Type:     "sensugo",
-					Settings: settingsJSON,
-				}
+	settingsJSON, err := simplejson.NewJson([]byte(json))
+	assert.NoError(t, err)
+	model := &models.AlertNotification{
+		Name:     "Sensu Go",
+		Type:     "sensugo",
+		Settings: settingsJSON,
+	}
 
-				_, err = NewSensuGoNotifier(model)
-				So(err, ShouldNotBeNil)
-			})
+	_, err = NewSensuGoNotifier(model)
+	assert.Error(t, err)
 
-			Convey("from settings", func() {
-				json := `
-				{
-					"url": "http://sensu-api.example.com:8080",
-					"entity": "grafana_instance_01",
-					"check": "grafana_rule_0",
-					"namespace": "default",
-					"handler": "myhandler",
-					"apikey": "abcdef0123456789abcdef"
-				}`
+	json = `
+	{
+		"url": "http://sensu-api.example.com:8080",
+		"entity": "grafana_instance_01",
+		"check": "grafana_rule_0",
+		"namespace": "default",
+		"handler": "myhandler",
+		"apikey": "abcdef0123456789abcdef"
+	}`
 
-				settingsJSON, _ := simplejson.NewJson([]byte(json))
-				model := &models.AlertNotification{
-					Name:     "Sensu Go",
-					Type:     "sensugo",
-					Settings: settingsJSON,
-				}
+	settingsJSON, err = simplejson.NewJson([]byte(json))
+	assert.NoError(t, err)
+	model = &models.AlertNotification{
+		Name:     "Sensu Go",
+		Type:     "sensugo",
+		Settings: settingsJSON,
+	}
 
-				not, err := NewSensuGoNotifier(model)
-				So(err, ShouldBeNil)
-				sensuGoNotifier := not.(*SensuGoNotifier)
+	not, err := NewSensuGoNotifier(model)
+	assert.NoError(t, err)
+	sensuGoNotifier := not.(*SensuGoNotifier)
 
-				So(sensuGoNotifier.Name, ShouldEqual, "Sensu Go")
-				So(sensuGoNotifier.Type, ShouldEqual, "sensugo")
-				So(sensuGoNotifier.URL, ShouldEqual, "http://sensu-api.example.com:8080")
-				So(sensuGoNotifier.Entity, ShouldEqual, "grafana_instance_01")
-				So(sensuGoNotifier.Check, ShouldEqual, "grafana_rule_0")
-				So(sensuGoNotifier.Namespace, ShouldEqual, "default")
-				So(sensuGoNotifier.Handler, ShouldEqual, "myhandler")
-				So(sensuGoNotifier.APIKey, ShouldEqual, "abcdef0123456789abcdef")
-			})
-		})
-	})
+	assert.Equal(t, "Sensu Go", sensuGoNotifier.Name)
+	assert.Equal(t, "sensugo", sensuGoNotifier.Type)
+	assert.Equal(t, "http://sensu-api.example.com:8080", sensuGoNotifier.URL)
+	assert.Equal(t, "grafana_instance_01", sensuGoNotifier.Entity)
+	assert.Equal(t, "grafana_rule_0", sensuGoNotifier.Check)
+	assert.Equal(t, "default", sensuGoNotifier.Namespace)
+	assert.Equal(t, "myhandler", sensuGoNotifier.Handler)
+	assert.Equal(t, "abcdef0123456789abcdef", sensuGoNotifier.APIKey)
 }
