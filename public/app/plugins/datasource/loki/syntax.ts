796afda6df34f9ef23a23bef5c6b67d582ee8d90
Ivana <ivana.huckova@gmail.com>
2020-10-07 11:45:41 +0200

    Update operators list, add regex quotes and move cursor

--- public/app/plugins/datasource/loki/syntax.ts
+++ public/app/plugins/datasource/loki/syntax.ts
@@ -57,8 +57,9 @@ export const PIPE_PARSERS: CompletionItem[] = [
   },
   {
     label: 'regexp',
-    insertText: 'regexp',
+    insertText: 'regexp ""',
     documentation: 'Extracting labels from the log line using regexp parser. Only available in Loki 2.0+.',
+    move: -1,
   },
   {
     label: 'logfmt',
@@ -69,9 +70,10 @@ export const PIPE_PARSERS: CompletionItem[] = [
 
 export const PIPE_OPERATORS: CompletionItem[] = [
   {
-    label: 'label_filter',
-    insertText: 'label_filter',
-    documentation: 'Only available in Loki 2.0+.',
+    label: 'unwrap identifier',
+    insertText: 'unwrap identifier',
+    documentation:
+      'Take labels and use the values as sample data for metric aggregations. Only available in Loki 2.0+.',
   },
   {
     label: 'label_format',
@@ -90,25 +92,37 @@ export const RANGE_VEC_FUNCTIONS = [
     insertText: 'avg_over_time',
     label: 'avg_over_time',
     detail: 'avg_over_time(range-vector)',
-    documentation: 'The average of all values in the specified interval.',
+    documentation: 'The average of all values in the specified interval. Only available in Loki 2.0+.',
   },
   {
     insertText: 'min_over_time',
     label: 'min_over_time',
     detail: 'min_over_time(range-vector)',
-    documentation: 'The minimum of all values in the specified interval.',
+    documentation: 'The minimum of all values in the specified interval. Only available in Loki 2.0+.',
   },
   {
     insertText: 'max_over_time',
     label: 'max_over_time',
     detail: 'max_over_time(range-vector)',
-    documentation: 'The maximum of all values in the specified interval.',
+    documentation: 'The maximum of all values in the specified interval. Only available in Loki 2.0+.',
   },
   {
     insertText: 'sum_over_time',
     label: 'sum_over_time',
     detail: 'sum_over_time(range-vector)',
-    documentation: 'The sum of all values in the specified interval.',
+    documentation: 'The sum of all values in the specified interval. Only available in Loki 2.0+.',
+  },
+  {
+    insertText: 'stdvar_over_time',
+    label: 'stdvar_over_time',
+    detail: 'stdvar_over_time(range-vector)',
+    documentation: 'Only available in Loki 2.0+.',
+  },
+  {
+    insertText: 'stddev_over_time',
+    label: 'stddev_over_time',
+    detail: 'stddev_over_time(range-vector)',
+    documentation: 'Only available in Loki 2.0+.',
   },
   {
     insertText: 'count_over_time',
@@ -122,6 +136,12 @@ export const RANGE_VEC_FUNCTIONS = [
     detail: 'rate(v range-vector)',
     documentation:
       "Calculates the per-second average rate of increase of the time series in the range vector. Breaks in monotonicity (such as counter resets due to target restarts) are automatically adjusted for. Also, the calculation extrapolates to the ends of the time range, allowing for missed scrapes or imperfect alignment of scrape cycles with the range's time period.",
+  },
+  {
+    insertText: 'quantile_over_time',
+    label: 'quantile_over_time',
+    detail: 'quantile_over_time(value between 0 and 1,range vec)',
+    documentation: '',
   },
 ];
 
@@ -163,7 +183,7 @@ const tokenizer: Grammar = {
     },
   },
   'context-pipe': {
-    pattern: /(\s\|[^=~]\s?)\w*/i,
+    pattern: /\s\|[^=~]\s?\w*/i,
     inside: {
       'pipe-operator': {
         pattern: /\|/i,
