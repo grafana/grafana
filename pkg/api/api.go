2567e5202aeac3009b3ba08fdb81c428e7847d37
Ryan McKinley <ryantxu@gmail.com>
2020-10-08 08:42:15 -0700

    Live: remove admin pages, add alpha panel (#28101)

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -56,7 +56,6 @@ func (hs *HTTPServer) registerRoutes() {
 	r.Get("/admin/orgs", reqGrafanaAdmin, hs.Index)
 	r.Get("/admin/orgs/edit/:id", reqGrafanaAdmin, hs.Index)
 	r.Get("/admin/stats", reqGrafanaAdmin, hs.Index)
-	r.Get("/admin/live", reqGrafanaAdmin, hs.Index)
 	r.Get("/admin/ldap", reqGrafanaAdmin, hs.Index)
 
 	r.Get("/styleguide", reqSignedIn, hs.Index)
