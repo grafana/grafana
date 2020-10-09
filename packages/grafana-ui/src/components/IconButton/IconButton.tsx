1cfa5240e456b64cbaed14f8d735f54dffa8862b
Jess <53754871+jessover9000@users.noreply.github.com>
2020-10-09 15:25:27 +0200

    Add docs for IconButton (#28136)
    * Update IconButton.story.tsx
    
    * Create IconButton.mdx
    
    * Add missing row
    
    * Add props table
    
    * Expand docs
    
    Co-authored-by: Clarity-89 <homes89@ukr.net>

--- packages/grafana-ui/src/components/IconButton/IconButton.tsx
+++ packages/grafana-ui/src/components/IconButton/IconButton.tsx
@@ -9,12 +9,17 @@ import { Tooltip } from '../Tooltip/Tooltip';
 import { TooltipPlacement } from '../Tooltip/PopoverController';
 
 export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Name of the icon **/
   name: IconName;
+  /** Icon size */
   size?: IconSize;
   /** Need this to change hover effect based on what surface it is on */
   surface?: SurfaceType;
+  /** Type od the icon - mono or default */
   iconType?: IconType;
+  /** Tooltip content to display on hover */
   tooltip?: string;
+  /** Position of the tooltip */
   tooltipPlacement?: TooltipPlacement;
 }
 
