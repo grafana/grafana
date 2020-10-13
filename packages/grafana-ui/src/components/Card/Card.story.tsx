092e941ab9b5760ab90acc6084750eee75dd75b7
Clarity-89 <homes89@ukr.net>
2020-10-13 10:49:10 +0300

    Add disabled state

--- packages/grafana-ui/src/components/Card/Card.story.tsx
+++ packages/grafana-ui/src/components/Card/Card.story.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { boolean } from '@storybook/addon-knobs';
+import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { Card } from './Card';
-import { withCenteredStory } from '@grafana/ui/src/utils/storybook/withCenteredStory';
 import mdx from './Card.mdx';
 
 export default {
@@ -15,10 +16,12 @@ export default {
 };
 
 export const Basic = () => {
+  const disabled = boolean('Disabled', false, 'Style props');
   return (
     <Card
       title="Filter by name"
       description="Filter data by query. This is useful if you are sharing the results from a different panel that has many queries and you want to only visualize a subset of that in this panel."
+      disabled={disabled}
     />
   );
 };
@@ -29,6 +32,16 @@ export const WithTooltip = () => {
       title="Reduce"
       description="Reduce all rows or data points to a single value using a function like max, min, mean or last."
       tooltip="Click to apply this transformation."
+    />
+  );
+};
+
+export const WithTags = () => {
+  return (
+    <Card
+      title="Elasticsearch – Custom Templated Query"
+      description="Elastic Search"
+      tags={['elasticsearch', 'test', 'testdata']}
     />
   );
 };
--- packages/grafana-ui/src/components/Card/Card.tsx
+++ packages/grafana-ui/src/components/Card/Card.tsx
@@ -3,39 +3,53 @@ import { css } from 'emotion';
 import { GrafanaTheme } from '@grafana/data';
 import { useTheme, styleMixins } from '../../themes';
 import { Tooltip, PopoverContent } from '../Tooltip/Tooltip';
+import { OnTagClick } from '../Tags/Tag';
+import { TagList } from '..';
 
 export interface Props {
   title?: string;
   description?: string;
   /** Content for the card's tooltip */
   tooltip?: PopoverContent;
+  /** List of tags to display in the card */
+  tags?: string[];
+  /** Optional callback for tag onclick event */
+  onTagClick?: OnTagClick;
+  /** Indicates if the card and all its actions can be interacted with */
+  disabled?: boolean;
 }
 
-export const Card: FC<Props> = ({ title, description, tooltip = '' }) => {
+export const Card: FC<Props> = ({ title, description, tags = [], onTagClick, disabled, tooltip = '' }) => {
   const theme = useTheme();
-  const styles = getStyles(theme);
+  const styles = getStyles(theme, disabled);
   return (
     <>
       <Tooltip placement="top" content={tooltip} theme="info" show={!!tooltip}>
         <div tabIndex={0} className={styles.container}>
           <p className={styles.title}>{title}</p>
           {description && <p className={styles.description}>{description}</p>}
+          {!!tags.length && <TagList tags={tags} onClick={onTagClick} />}
+          <div className={styles.overlay} />
         </div>
       </Tooltip>
     </>
   );
 };
 
-const getStyles = (theme: GrafanaTheme) => {
+const getStyles = (theme: GrafanaTheme, disabled = false) => {
   return {
     container: css`
+      width: 100%;
       color: ${theme.colors.textStrong};
       background: ${theme.colors.bg2};
       border-radius: ${theme.border.radius.sm};
       padding: ${theme.spacing.md};
+      position: relative;
+      pointer-events: ${disabled ? 'none' : 'auto'};
+
       &:hover {
         background: ${styleMixins.hoverColor(theme.colors.bg2, theme)};
-        cursor: pointer;
+        cursor: ${disabled ? 'not-allowed' : 'pointer'};
       }
 
       &:focus {
@@ -43,13 +57,26 @@ const getStyles = (theme: GrafanaTheme) => {
       }
     `,
     title: css`
-      margin-bottom: ${theme.spacing.xs};
+      margin-bottom: ${theme.spacing.xxs};
       font-size: ${theme.typography.size.md};
     `,
     description: css`
-      margin-bottom: ${theme.spacing.xs};
+      margin-bottom: ${theme.spacing.sm};
       font-size: ${theme.typography.size.sm};
       color: ${theme.colors.textSemiWeak};
+    `,
+
+    overlay: css`
+      position: absolute;
+      top: 1px;
+      left: 1px;
+      right: 1px;
+      bottom: 1px;
+      background: linear-gradient(180deg, rgba(75, 79, 84, 0.5) 0%, rgba(82, 84, 92, 0.5) 100%);
+      width: calc(100% - 2px);
+      height: calc(100% - 2px);
+      z-index: ${disabled ? 0 : -1};
+      border-radius: ${theme.border.radius.sm};
     `,
   };
 };
