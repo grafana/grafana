0b300fd1f5d7137f43770d180fd205b464deb0eb
Andrej Ocenas <mr.ocenas@gmail.com>
2020-10-13 16:15:43 +0200

    Update public/app/plugins/datasource/tempo/ConfigEditor.tsx
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>

--- public/app/plugins/datasource/tempo/ConfigEditor.tsx
+++ public/app/plugins/datasource/tempo/ConfigEditor.tsx
@@ -7,7 +7,7 @@ export type Props = DataSourcePluginOptionsEditorProps;
 export const ConfigEditor: React.FC<Props> = ({ options, onOptionsChange }) => {
   return (
     <DataSourceHttpSettings
-      defaultUrl={'http://localhost:16686'}
+      defaultUrl="http://localhost:16686"
       dataSourceConfig={options}
       showAccessOptions={false}
       onChange={onOptionsChange}
