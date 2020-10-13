d762db9de5b2745e228cd5155ab04679540353b2
Ryan McKinley <ryantxu@gmail.com>
2020-10-13 11:41:00 -0700

    split into two functions

--- packages/grafana-data/src/transformations/transformers/reduce.ts
+++ packages/grafana-data/src/transformations/transformers/reduce.ts
@@ -10,6 +10,7 @@ import { KeyValue } from '../../types/data';
 import { guessFieldTypeForField } from '../../dataframe/processDataFrame';
 import { getFieldMatcher } from '../matchers';
 import { getFieldDisplayName } from '../../field';
+import { FieldMatcher } from '../../types/transformations';
 
 export enum ReduceTransformerMode {
   SeriesToRows = 'seriesToRows', // default
@@ -37,124 +38,111 @@ export const reduceTransformer: DataTransformerInfo<ReduceTransformerOptions> = 
   operator: options => source =>
     source.pipe(
       map(data => {
+        if (!options?.reducers?.length) {
+          return data; // nothing selected
+        }
+
         const matcher = options.fields
           ? getFieldMatcher(options.fields)
           : options.includeTimeField && options.mode === ReduceTransformerMode.ReduceFields
           ? alwaysFieldMatcher
           : notTimeFieldMatcher;
-        const calculators = options.reducers && options.reducers.length ? fieldReducers.list(options.reducers) : [];
-        const reducers = calculators.map(c => c.id);
 
-        const processed: DataFrame[] = [];
         // Collapse all matching fields into a single row
         if (options.mode === ReduceTransformerMode.ReduceFields) {
-          for (const series of data) {
-            const fields: Field[] = [];
-            for (const field of series.fields) {
-              if (matcher(field, series, data)) {
-                const results = reduceField({
-                  field,
-                  reducers,
-                });
-                for (const reducer of reducers) {
-                  const value = results[reducer];
-                  const copy = {
-                    ...field,
-                    values: new ArrayVector([value]),
-                  };
-                  copy.state = undefined;
-                  if (reducers.length > 1) {
-                    if (!copy.labels) {
-                      copy.labels = {};
-                    }
-                    copy.labels['reducer'] = fieldReducers.get(reducer).name;
-                  }
-                  fields.push(copy);
-                }
-              }
-            }
-            if (fields.length) {
-              processed.push({
-                ...series,
-                fields,
-                length: 1, // always one row
-              });
-            }
-          }
-
-          return processed;
+          return reduceFields(data, matcher, options.reducers);
         }
 
-        for (const series of data) {
-          const values: ArrayVector[] = [];
-          const fields: Field[] = [];
-          const byId: KeyValue<ArrayVector> = {};
-
-          values.push(new ArrayVector()); // The name
-          fields.push({
-            name: 'Field',
-            type: FieldType.string,
-            values: values[0],
-            config: {},
-          });
-
-          for (const info of calculators) {
-            const vals = new ArrayVector();
-            byId[info.id] = vals;
-            values.push(vals);
-
-            fields.push({
-              name: info.name,
-              type: FieldType.other, // UNKNOWN until after we call the functions
-              values: values[values.length - 1],
-              config: {},
-            });
-          }
-
-          for (let i = 0; i < series.fields.length; i++) {
-            const field = series.fields[i];
-
-            if (matcher(field, series, data)) {
-              const results = reduceField({
-                field,
-                reducers,
-              });
-
-              // Update the name list
-              const fieldName = getFieldDisplayName(field, series, data);
-
-              values[0].buffer.push(fieldName);
-
-              for (const info of calculators) {
-                const v = results[info.id];
-                byId[info.id].buffer.push(v);
-              }
-            }
-          }
-
-          for (const f of fields) {
-            const t = guessFieldTypeForField(f);
-
-            if (t) {
-              f.type = t;
-            }
-          }
-
-          processed.push({
-            ...series, // Same properties, different fields
-            fields,
-            length: values[0].length,
-          });
-        }
-
-        return mergeResults(processed);
+        // Add a row for each series
+        const res = reduceSeriesToRows(data, matcher, options.reducers);
+        return res ? [res] : [];
       })
     ),
 };
 
-const mergeResults = (data: DataFrame[]) => {
-  if (data.length <= 1) {
-    return data;
+/**
+ * @internal only exported for testing
+ */
+export function reduceSeriesToRows(
+  data: DataFrame[],
+  matcher: FieldMatcher,
+  reducerId: ReducerID[]
+): DataFrame | undefined {
+  const calculators = fieldReducers.list(reducerId);
+  const reducers = calculators.map(c => c.id);
+  const processed: DataFrame[] = [];
+
+  for (const series of data) {
+    const values: ArrayVector[] = [];
+    const fields: Field[] = [];
+    const byId: KeyValue<ArrayVector> = {};
+
+    values.push(new ArrayVector()); // The name
+    fields.push({
+      name: 'Field',
+      type: FieldType.string,
+      values: values[0],
+      config: {},
+    });
+
+    for (const info of calculators) {
+      const vals = new ArrayVector();
+      byId[info.id] = vals;
+      values.push(vals);
+
+      fields.push({
+        name: info.name,
+        type: FieldType.other, // UNKNOWN until after we call the functions
+        values: values[values.length - 1],
+        config: {},
+      });
+    }
+
+    for (let i = 0; i < series.fields.length; i++) {
+      const field = series.fields[i];
+
+      if (matcher(field, series, data)) {
+        const results = reduceField({
+          field,
+          reducers,
+        });
+
+        // Update the name list
+        const fieldName = getFieldDisplayName(field, series, data);
+
+        values[0].buffer.push(fieldName);
+
+        for (const info of calculators) {
+          const v = results[info.id];
+          byId[info.id].buffer.push(v);
+        }
+      }
+    }
+
+    for (const f of fields) {
+      const t = guessFieldTypeForField(f);
+
+      if (t) {
+        f.type = t;
+      }
+    }
+
+    processed.push({
+      ...series, // Same properties, different fields
+      fields,
+      length: values[0].length,
+    });
+  }
+
+  return mergeResults(processed);
+}
+
+/**
+ * @internal only exported for testing
+ */
+export function mergeResults(data: DataFrame[]): DataFrame | undefined {
+  if (!data?.length) {
+    return undefined;
   }
 
   const baseFrame = data[0];
@@ -177,6 +165,50 @@ const mergeResults = (data: DataFrame[]) => {
 
   baseFrame.name = undefined;
   baseFrame.length = baseFrame.fields[0].values.length;
+  return baseFrame;
+}
 
-  return [baseFrame];
-};
+/**
+ * @internal -- only exported for testing
+ */
+export function reduceFields(data: DataFrame[], matcher: FieldMatcher, reducerId: ReducerID[]): DataFrame[] {
+  const calculators = fieldReducers.list(reducerId);
+  const reducers = calculators.map(c => c.id);
+  const processed: DataFrame[] = [];
+
+  for (const series of data) {
+    const fields: Field[] = [];
+    for (const field of series.fields) {
+      if (matcher(field, series, data)) {
+        const results = reduceField({
+          field,
+          reducers,
+        });
+        for (const reducer of reducers) {
+          const value = results[reducer];
+          const copy = {
+            ...field,
+            values: new ArrayVector([value]),
+          };
+          copy.state = undefined;
+          if (reducers.length > 1) {
+            if (!copy.labels) {
+              copy.labels = {};
+            }
+            copy.labels['reducer'] = fieldReducers.get(reducer).name;
+          }
+          fields.push(copy);
+        }
+      }
+    }
+    if (fields.length) {
+      processed.push({
+        ...series,
+        fields,
+        length: 1, // always one row
+      });
+    }
+  }
+
+  return processed;
+}
