cdec65e909278b8c3e78c5b45ea518448d731fd0
Clarity-89 <homes89@ukr.net>
2020-10-14 16:57:39 +0300

    Use new Card for AlertRuleItem

--- public/app/features/alerting/AlertRuleItem.tsx
+++ public/app/features/alerting/AlertRuleItem.tsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback } from 'react';
 // @ts-ignore
 import Highlighter from 'react-highlight-words';
 import { AlertRule } from '../../types';
-import { Icon, IconName, Button, Tooltip, LinkButton, HorizontalGroup } from '@grafana/ui';
+import { Icon, IconName, Button, Tooltip, LinkButton, Card } from '@grafana/ui';
 
 export interface Props {
   rule: AlertRule;
@@ -10,56 +10,50 @@ export interface Props {
   onTogglePause: () => void;
 }
 
-class AlertRuleItem extends PureComponent<Props> {
-  renderText(text: string) {
-    return (
+const AlertRuleItem = ({ rule, search, onTogglePause }: Props) => {
+  const ruleUrl = `${rule.url}?editPanel=${rule.panelId}&tab=alert`;
+  const renderText = useCallback(
+    text => (
       <Highlighter
+        key={text}
         highlightClassName="highlight-search-match"
         textToHighlight={text}
-        searchWords={[this.props.search]}
+        searchWords={[search]}
       />
-    );
-  }
+    ),
+    [search]
+  );
 
-  render() {
-    const { rule, onTogglePause } = this.props;
-
-    const ruleUrl = `${rule.url}?editPanel=${rule.panelId}&tab=alert`;
-
-    return (
-      <li className="alert-rule-item">
+  return (
+    <Card
+      tag="li"
+      heading={<a href={ruleUrl}>{renderText(rule.name)}</a>}
+      mediaContent={
         <Icon size="xl" name={rule.stateIcon as IconName} className={`alert-rule-item__icon ${rule.stateClass}`} />
-        <div className="alert-rule-item__body">
-          <div className="alert-rule-item__header">
-            <div className="alert-rule-item__name">
-              <a href={ruleUrl}>{this.renderText(rule.name)}</a>
-            </div>
-            <div className="alert-rule-item__text">
-              <span className={`${rule.stateClass}`}>{this.renderText(rule.stateText)}</span>
-              <span className="alert-rule-item__time"> for {rule.stateAge}</span>
-            </div>
-          </div>
-          {rule.info && <div className="small muted alert-rule-item__info">{this.renderText(rule.info)}</div>}
-        </div>
-
-        <div className="alert-rule-item__actions">
-          <HorizontalGroup spacing="sm">
-            <Tooltip placement="bottom" content="Pausing an alert rule prevents it from executing">
-              <Button
-                variant="secondary"
-                size="sm"
-                icon={rule.state === 'paused' ? 'play' : 'pause'}
-                onClick={onTogglePause}
-              />
-            </Tooltip>
-            <Tooltip placement="right" content="Edit alert rule">
-              <LinkButton size="sm" variant="secondary" href={ruleUrl} icon="cog" />
-            </Tooltip>
-          </HorizontalGroup>
-        </div>
-      </li>
-    );
-  }
-}
+      }
+      metaData={[
+        <span key="state">
+          <span key="text" className={`${rule.stateClass}`}>
+            {renderText(rule.stateText)}{' '}
+          </span>
+          for {rule.stateAge}
+        </span>,
+        rule.info ? renderText(rule.info) : null,
+      ]}
+      actions={[
+        <Tooltip key="pause" placement="bottom" content="Pausing an alert rule prevents it from executing">
+          <Button variant="secondary" icon={rule.state === 'paused' ? 'play' : 'pause'} onClick={onTogglePause}>
+            {rule.state === 'paused' ? 'Resume' : 'Pause'}
+          </Button>
+        </Tooltip>,
+        <Tooltip key="settings" placement="right" content="Edit alert rule">
+          <LinkButton variant="secondary" href={ruleUrl} icon="cog">
+            Edit alert
+          </LinkButton>
+        </Tooltip>,
+      ]}
+    />
+  );
+};
 
 export default AlertRuleItem;
