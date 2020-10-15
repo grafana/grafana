8653fd36474e1b3618bff410003df8201cc1dc6d
Ryan McKinley <ryantxu@gmail.com>
2020-10-15 10:54:42 -0700

    update comment formats

--- pkg/models/measurement.go
+++ pkg/models/measurement.go
@@ -2,15 +2,25 @@ package models
 
 import "github.com/grafana/grafana-plugin-sdk-go/data"
 
-// NOTE:  this likely shoud go in the Plugin SDK since it will be useful from plugins
+// NOTE:
+// this likely shoud go in the Plugin SDK since it will be useful from plugins
 
 // Measurement is a single measurement value
 type Measurement struct {
-	Name   string                      `json:"name,omitempty"`
-	Time   int64                       `json:"time,omitempty"`   // units are usually ms, but depend on the channel
-	Values map[string]interface{}      `json:"values,omitempty"` // typically number or string
-	Config map[string]data.FieldConfig `json:"config,omitempty"` // optional list of field configs
-	Labels map[string]string           `json:"labels,omitempty"` // labels are applied to all values
+	// the measurement name
+	Name string `json:"name,omitempty"`
+
+	// units are usually ms, but depend on the channel
+	Time int64 `json:"time,omitempty"`
+
+	// typically number or string
+	Values map[string]interface{} `json:"values,omitempty"`
+
+	// optional list of field configs
+	Config map[string]data.FieldConfig `json:"config,omitempty"`
+
+	// labels are applied to all values
+	Labels map[string]string `json:"labels,omitempty"`
 }
 
 // MeasurementAction defines what should happen when you send a list of measurements
@@ -29,7 +39,12 @@ const (
 
 // MeasurementBatch is a collection of measurments all sent at once
 type MeasurementBatch struct {
-	Action   MeasurementAction `json:"action,omitempty"`   // default action is append
-	Measures []Measurement     `json:"measures,omitempty"` // batch of measurments
-	Capacity int64             `json:"capacity,omitempty"` // The suggested size of the client buffer
+	// default action is append
+	Action MeasurementAction `json:"action,omitempty"`
+
+	// batch of measurments
+	Measures []Measurement `json:"measures,omitempty"`
+
+	// The suggested size of the client buffer
+	Capacity int64 `json:"capacity,omitempty"`
 }
