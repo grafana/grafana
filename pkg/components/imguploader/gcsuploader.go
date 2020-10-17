039b3b8cf1b69fd94ed5418267ab72d8209cb604
gaston.qiu <gaston.qiu@umbocv.com>
2020-10-17 16:59:59 +0800

    Fix: The signed url should not be removed
    - The signed url function had been removed accidentally after rebase
    to master so I add the function back.
    
    https://github.com/grafana/grafana/pull/26493#discussion_r503907199
    
    Signed-off-by: gaston.qiu <gaston.qiu@umbocv.com>

--- pkg/components/imguploader/gcsuploader.go
+++ pkg/components/imguploader/gcsuploader.go
@@ -13,6 +13,7 @@ import (
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/util"
 	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
 	"google.golang.org/api/option"
 )
 
@@ -84,10 +85,44 @@ func (u *GCSUploader) Upload(ctx context.Context, imageDiskPath string) (string,
 	}
 
 	err = u.uploadFile(ctx, client, imageDiskPath, key)
-	if err != nil {
-		return "", err
+
+	if !u.enableSignedUrls {
+		return fmt.Sprintf("https://storage.googleapis.com/%s/%s", u.bucket, key), nil
 	}
-	return fmt.Sprintf("https://storage.googleapis.com/%s/%s", u.bucket, key), nil
+
+	u.log.Debug("Signing GCS URL")
+	var conf *jwt.Config
+	if u.keyFile != "" {
+		jsonKey, err := ioutil.ReadFile(u.keyFile)
+		if err != nil {
+			return "", fmt.Errorf("ioutil.ReadFile: %v", err)
+		}
+		conf, err = google.JWTConfigFromJSON(jsonKey)
+		if err != nil {
+			return "", fmt.Errorf("google.JWTConfigFromJSON: %v", err)
+		}
+	} else {
+		creds, err := google.FindDefaultCredentials(ctx, storage.ScopeReadWrite)
+		if err != nil {
+			return "", fmt.Errorf("google.FindDefaultCredentials: %v", err)
+		}
+		conf, err = google.JWTConfigFromJSON(creds.JSON)
+		if err != nil {
+			return "", fmt.Errorf("google.JWTConfigFromJSON: %v", err)
+		}
+	}
+	opts := &storage.SignedURLOptions{
+		Scheme:         storage.SigningSchemeV4,
+		Method:         "GET",
+		GoogleAccessID: conf.Email,
+		PrivateKey:     conf.PrivateKey,
+		Expires:        time.Now().Add(u.signedUrlExpiration),
+	}
+	signedUrl, err := storage.SignedURL(u.bucket, key, opts)
+	if err != nil {
+		return "", fmt.Errorf("storage.SignedURL: %v", err)
+	}
+	return signedUrl, nil
 }
 
 func (u *GCSUploader) uploadFile(
