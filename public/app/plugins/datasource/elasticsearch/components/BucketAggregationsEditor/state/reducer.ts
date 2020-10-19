864f675c15d55302cdade759bf0e6a5fe6a61b85
Elfo404 <gio.ricci@grafana.com>
2020-10-19 11:49:42 +0100

    Remove/Restore bucket aggregations when selecting/deselecting raw data metric

--- public/app/plugins/datasource/elasticsearch/components/BucketAggregationsEditor/state/reducer.ts
+++ public/app/plugins/datasource/elasticsearch/components/BucketAggregationsEditor/state/reducer.ts
@@ -1,10 +1,12 @@
 import { defaultBucketAgg } from '../../../query_def';
 import { BucketAggregation, ElasticsearchQuery } from '../../../types';
+import { ChangeMetricTypeAction, CHANGE_METRIC_TYPE } from '../../MetricAggregationsEditor/state/types';
+import { metricAggregationConfig } from '../../MetricAggregationsEditor/utils';
 import { ADD_BUCKET_AGG, BucketAggregationAction, REMOVE_BUCKET_AGG } from './types';
 
 export const reducer = (
   state: BucketAggregation[] = [],
-  action: BucketAggregationAction
+  action: BucketAggregationAction | ChangeMetricTypeAction
 ): ElasticsearchQuery['bucketAggs'] => {
   switch (action.type) {
     case ADD_BUCKET_AGG:
@@ -13,6 +15,16 @@ export const reducer = (
 
     case REMOVE_BUCKET_AGG:
       return state.filter(bucketAgg => bucketAgg.id !== action.payload.id);
+
+    case CHANGE_METRIC_TYPE:
+      if (metricAggregationConfig[action.payload.type].isSingleMetric) {
+        return [];
+      } else if (state.length === 0) {
+        // This means we are switching back to a metric that has `isSingleMetric = false`,
+        // therefore we restore a default bucket aggregation
+        return [defaultBucketAgg()];
+      }
+      return state;
 
     default:
       return state;
