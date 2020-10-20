f762d7e3d437fd6cc6acd20481b8c24a3a0daa81
Clarity-89 <homes89@ukr.net>
2020-10-20 15:22:13 +0300

    Update AlertRuleItem

--- public/app/features/alerting/AlertRuleItem.tsx
+++ public/app/features/alerting/AlertRuleItem.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react';
 // @ts-ignore
 import Highlighter from 'react-highlight-words';
-import { AlertRule } from '../../types';
+import { css } from 'emotion';
 import { Icon, IconName, Button, Tooltip, LinkButton, Card } from '@grafana/ui';
+import { AlertRule } from '../../types';
 
 export interface Props {
   rule: AlertRule;
@@ -25,34 +26,39 @@ const AlertRuleItem = ({ rule, search, onTogglePause }: Props) => {
   );
 
   return (
-    <Card
-      tag="li"
-      heading={<a href={ruleUrl}>{renderText(rule.name)}</a>}
-      image={
-        <Icon size="xl" name={rule.stateIcon as IconName} className={`alert-rule-item__icon ${rule.stateClass}`} />
-      }
-      metadata={[
-        <span key="state">
-          <span key="text" className={`${rule.stateClass}`}>
-            {renderText(rule.stateText)}{' '}
-          </span>
-          for {rule.stateAge}
-        </span>,
-        rule.info ? renderText(rule.info) : null,
-      ]}
-      actions={[
-        <Tooltip key="pause" placement="bottom" content="Pausing an alert rule prevents it from executing">
-          <Button variant="secondary" icon={rule.state === 'paused' ? 'play' : 'pause'} onClick={onTogglePause}>
-            {rule.state === 'paused' ? 'Resume' : 'Pause'}
-          </Button>
-        </Tooltip>,
-        <Tooltip key="settings" placement="right" content="Edit alert rule">
-          <LinkButton variant="secondary" href={ruleUrl} icon="cog">
-            Edit alert
-          </LinkButton>
-        </Tooltip>,
-      ]}
-    />
+    <li
+      className={css`
+        width: 100%;
+      `}
+    >
+      <Card
+        heading={<a href={ruleUrl}>{renderText(rule.name)}</a>}
+        image={
+          <Icon size="xl" name={rule.stateIcon as IconName} className={`alert-rule-item__icon ${rule.stateClass}`} />
+        }
+        metadata={[
+          <span key="state">
+            <span key="text" className={`${rule.stateClass}`}>
+              {renderText(rule.stateText)}{' '}
+            </span>
+            for {rule.stateAge}
+          </span>,
+          rule.info ? renderText(rule.info) : null,
+        ]}
+        actions={[
+          <Tooltip key="pause" placement="bottom" content="Pausing an alert rule prevents it from executing">
+            <Button variant="secondary" icon={rule.state === 'paused' ? 'play' : 'pause'} onClick={onTogglePause}>
+              {rule.state === 'paused' ? 'Resume' : 'Pause'}
+            </Button>
+          </Tooltip>,
+          <Tooltip key="settings" placement="right" content="Edit alert rule">
+            <LinkButton variant="secondary" href={ruleUrl} icon="cog">
+              Edit alert
+            </LinkButton>
+          </Tooltip>,
+        ]}
+      />
+    </li>
   );
 };
 
