09830c43354a44866a2f78b0416888bb2967e761
Arve Knudsen <arve.knudsen@gmail.com>
2020-10-29 08:27:24 +0100

    CloudWatch: Improve method name, performance optimization (#28632)
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/tsdb/cloudwatch/response_parser.go
+++ pkg/tsdb/cloudwatch/response_parser.go
@@ -46,7 +46,7 @@ func (e *cloudWatchExecutor) parseResponse(metricDataOutputs []*cloudwatch.GetMe
 		}
 	}
 
-	cloudWatchResponses := make([]*cloudwatchResponse, 0)
+	cloudWatchResponses := make([]*cloudwatchResponse, 0, len(mdrs))
 	for id, lr := range mdrs {
 		query := queries[id]
 		frames, partialData, err := parseMetricResults(lr, labels[id], query)
