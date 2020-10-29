6f385823538de891faec5dcf105141a520b72840
kay delaney <kay@grafana.com>
2020-10-29 14:52:50 +0000

    Improvements based on feedback

--- public/app/plugins/panel/dashlist/DashList.tsx
+++ public/app/plugins/panel/dashlist/DashList.tsx
@@ -1,12 +1,15 @@
-import { PanelProps } from '@grafana/data';
-import React, { useEffect, useState } from 'react';
-import { DashListOptions } from './types';
-import { getBackendSrv } from 'app/core/services/backend_srv';
-import { DashboardSearchHit } from 'app/features/search/types';
-import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
-import { CustomScrollbar, Icon } from '@grafana/ui';
-import impressionSrv from 'app/core/services/impression_srv';
+import React, { useEffect, useMemo, useState } from 'react';
 import take from 'lodash/take';
+
+import { PanelProps } from '@grafana/data';
+import { CustomScrollbar, Icon, useStyles } from '@grafana/ui';
+
+import { getBackendSrv } from 'app/core/services/backend_srv';
+import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
+import impressionSrv from 'app/core/services/impression_srv';
+import { DashboardSearchHit } from 'app/features/search/types';
+import { DashListOptions } from './types';
+import { getStyles } from './styles';
 
 type Dashboard = DashboardSearchHit & { isSearchResult?: boolean; isRecent?: boolean };
 
@@ -16,61 +19,63 @@ interface DashboardGroup {
   dashboards: Dashboard[];
 }
 
+async function fetchDashboards(options: DashListOptions) {
+  let starredDashboards: Promise<Dashboard[]> = Promise.resolve([]);
+  if (options.showStarred) {
+    const params = { limit: options.maxItems, starred: 'true' };
+    starredDashboards = getBackendSrv().search(params);
+  }
+
+  let recentDashboards: Promise<Dashboard[]> = Promise.resolve([]);
+  if (options.showRecentlyViewed) {
+    const dashIds = take(impressionSrv.getDashboardOpened(), options.maxItems);
+    recentDashboards = getBackendSrv().search({ dashboardIds: dashIds, limit: options.maxItems });
+  }
+
+  let searchedDashboards: Promise<Dashboard[]> = Promise.resolve([]);
+  if (options.showSearch) {
+    const params = {
+      limit: options.maxItems,
+      query: options.query,
+      folderIds: options.folderId,
+      tag: options.tags,
+      type: 'dash-db',
+    };
+
+    searchedDashboards = getBackendSrv().search(params);
+  }
+
+  const [starred, searched, recent] = await Promise.all([starredDashboards, searchedDashboards, recentDashboards]);
+  const dashMap = starred.reduce(
+    (acc, dash) => Object.assign(acc, { [dash.id]: dash }),
+    {} as Record<number, Dashboard>
+  );
+
+  searched.forEach(dash => {
+    if (dashMap.hasOwnProperty(dash.id)) {
+      dashMap[dash.id].isSearchResult = true;
+    } else {
+      dashMap[dash.id] = { ...dash, isSearchResult: true };
+    }
+  });
+
+  recent.forEach(dash => {
+    if (dashMap.hasOwnProperty(dash.id)) {
+      dashMap[dash.id].isRecent = true;
+    } else {
+      dashMap[dash.id] = { ...dash, isRecent: true };
+    }
+  });
+
+  return dashMap;
+}
+
 export function DashList(props: PanelProps<DashListOptions>) {
   const [dashboards, setDashboards] = useState<Record<number, Dashboard>>({});
   useEffect(() => {
-    async function fetchDashboards() {
-      let starredDashboards: Promise<Dashboard[]> = Promise.resolve([]);
-      if (props.options.showStarred) {
-        const params = { limit: props.options.maxItems, starred: 'true' };
-        starredDashboards = getBackendSrv().search(params);
-      }
-
-      let recentDashboards: Promise<Dashboard[]> = Promise.resolve([]);
-      if (props.options.showRecentlyViewed) {
-        const dashIds = take(impressionSrv.getDashboardOpened(), props.options.maxItems);
-        recentDashboards = getBackendSrv().search({ dashboardIds: dashIds, limit: props.options.maxItems });
-      }
-
-      let searchedDashboards: Promise<Dashboard[]> = Promise.resolve([]);
-      if (props.options.showSearch) {
-        const params = {
-          limit: props.options.maxItems,
-          query: props.options.query,
-          folderIds: props.options.folderId,
-          tag: props.options.tags,
-          type: 'dash-db',
-        };
-
-        searchedDashboards = getBackendSrv().search(params);
-      }
-
-      const [starred, searched, recent] = await Promise.all([starredDashboards, searchedDashboards, recentDashboards]);
-      const dashMap = starred.reduce(
-        (acc, dash) => Object.assign(acc, { [dash.id]: dash }),
-        {} as Record<number, Dashboard>
-      );
-
-      searched.forEach(dash => {
-        if (dashMap.hasOwnProperty(dash.id)) {
-          dashMap[dash.id].isSearchResult = true;
-        } else {
-          dashMap[dash.id] = { ...dash, isSearchResult: true };
-        }
-      });
-
-      recent.forEach(dash => {
-        if (dashMap.hasOwnProperty(dash.id)) {
-          dashMap[dash.id].isRecent = true;
-        } else {
-          dashMap[dash.id] = { ...dash, isRecent: true };
-        }
-      });
-
-      setDashboards(dashMap);
-    }
-
-    fetchDashboards();
+    fetchDashboards(props.options).then(dashes => {
+      setDashboards(dashes);
+    });
   }, [
     props.options.showSearch,
     props.options.showStarred,
@@ -89,45 +94,54 @@ export function DashList(props: PanelProps<DashListOptions>) {
     setDashboards(Object.assign({}, dashboards, { [dash.id]: { ...dash, isStarred } }));
   };
 
+  const [starredDashboards, recentDashboards, searchedDashboards] = useMemo(() => {
+    const dashboardList = Object.values(dashboards);
+    return [
+      dashboardList.filter(dash => dash.isStarred),
+      dashboardList.filter(dash => dash.isRecent),
+      dashboardList.filter(dash => dash.isSearchResult),
+    ];
+  }, [dashboards]);
+
   const { showStarred, showRecentlyViewed, showHeadings, showSearch } = props.options;
 
-  const dashboardList = Object.values(dashboards);
   const dashboardGroups: DashboardGroup[] = [
     {
       header: 'Starred dashboards',
-      dashboards: dashboardList.filter(dash => dash.isStarred),
+      dashboards: starredDashboards,
       show: showStarred,
     },
     {
       header: 'Recently viewed dashboards',
-      dashboards: dashboardList.filter(dash => dash.isRecent),
+      dashboards: recentDashboards,
       show: showRecentlyViewed,
     },
     {
       header: 'Search',
-      dashboards: dashboardList.filter(dash => dash.isSearchResult),
+      dashboards: searchedDashboards,
       show: showSearch,
     },
   ];
 
+  const css = useStyles(getStyles);
   return (
     <CustomScrollbar autoHeightMin="100%" autoHeightMax="100%">
       {dashboardGroups.map(
         ({ show, header, dashboards }, i) =>
           show && (
-            <div className="dashlist-section" key={`dash-group-${i}`}>
-              {showHeadings && <h6 className="dashlist-section-header">{header}</h6>}
+            <div className={css.dashlistSection} key={`dash-group-${i}`}>
+              {showHeadings && <h6 className={css.dashlistSectionHeader}>{header}</h6>}
               <ul>
                 {dashboards.map(dash => (
-                  <li className="dashlist-item" key={`dash-${dash.id}`}>
-                    <div className={`dashlist-link dashlist-link-${dash.type}`}>
-                      <div className="dashlist-link-body">
-                        <a className="dashlist-title" href={dash.url}>
+                  <li className={css.dashlistItem} key={`dash-${dash.id}`}>
+                    <div className={css.dashlistLink}>
+                      <div className={css.dashlistLinkBody}>
+                        <a className={css.dashlistTitle} href={dash.url}>
                           {dash.title}
                         </a>
-                        {dash.folderTitle && <div className="dashlist-folder">{dash.folderTitle}</div>}
+                        {dash.folderTitle && <div className={css.dashlistFolder}>{dash.folderTitle}</div>}
                       </div>
-                      <span className="dashlist-star" onClick={e => toggleDashboardStar(e, dash)}>
+                      <span className={css.dashlistStar} onClick={e => toggleDashboardStar(e, dash)}>
                         <Icon name={dash.isStarred ? 'favorite' : 'star'} type={dash.isStarred ? 'mono' : 'default'} />
                       </span>
                     </div>
