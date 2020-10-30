6c58c511dcc1c0f7df5592beed31ebe1939f1990
Andrej Ocenas <mr.ocenas@gmail.com>
2020-10-30 10:03:05 +0100

    Prometheus: Fix copy paste behaving as cut and paste (#28622)

--- packages/grafana-data/src/types/datasource.ts
+++ packages/grafana-data/src/types/datasource.ts
@@ -600,6 +600,6 @@ export abstract class LanguageProvider {
    * Returns startTask that resolves with a task list when main syntax is loaded.
    * Task list consists of secondary promises that load more detailed language features.
    */
-  abstract start: () => Promise<any[]>;
+  abstract start: () => Promise<Array<Promise<any>>>;
   startTask?: Promise<any[]>;
 }
