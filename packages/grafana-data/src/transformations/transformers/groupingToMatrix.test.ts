fbd8ec2db897593919233b4b10e5e374389ce04a
Borja Garrido <bgarrido@cern.ch>
2020-11-02 12:14:15 +0100

    Add new transformer grouping to matrix tests

---
+++ packages/grafana-data/src/transformations/transformers/groupingToMatrix.test.ts
@@ -0,0 +1,111 @@
+import { mockTransformationsRegistry } from '../../utils/tests/mockTransformationsRegistry';
+import { DataTransformerConfig, Field, FieldType } from '../../types';
+import { DataTransformerID } from './ids';
+import { toDataFrame } from '../../dataframe';
+import { transformDataFrame } from '../transformDataFrame';
+import { ArrayVector } from '../../vector';
+import { groupingToMatrixTransformer, groupingToMatrixTransformerOptions } from './groupingToMatrix';
+
+describe('Grouping to Matrix', () => {
+  beforeAll(() => {
+    mockTransformationsRegistry([groupingToMatrixTransformer]);
+  });
+
+  it('generate Matrix with default fields', () => {
+    const cfg: DataTransformerConfig<GroupingToMatrixTransformerOptions> = {
+      id: DataTransformerID.groupingToMatrix,
+      options: {},
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [1000, 1001, 1002] },
+        { name: 'Value', type: FieldType.number, values: [1, 2, 3] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA]);
+    const expected: Field[] = [
+      createField('Time\\Time', FieldType.string, [1000, 1001, 1002]),
+      createField('1000', FieldType.number, [1, '', '']),
+      createField('1001', FieldType.number, ['', 2, '']),
+      createField('1002', FieldType.number, ['', '', 3]),
+    ];
+
+    expect(unwrap(result[0].fields)).toEqual(expected);
+  });
+
+  it('generate Matrix with multiple fields', () => {
+    const cfg: DataTransformerConfig<GroupingToMatrixTransformerOptions> = {
+      id: DataTransformerID.groupingToMatrix,
+      options: {
+        columnField: 'Column',
+        rowField: 'Row',
+        valueField: 'Temp',
+      },
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'Column', type: FieldType.string, values: ['C1', 'C1', 'C2'] },
+        { name: 'Row', type: FieldType.string, values: ['R1', 'R2', 'R1'] },
+        { name: 'Temp', type: FieldType.number, values: [1, 4, 5] },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA, seriesB]);
+    const expected: Field[] = [
+      createField('Row\\Column', FieldType.string, []),
+      createField('C1', FieldType.number, [1, 4, '']),
+      createField('C2', FieldType.number, [5, '', '']),
+    ];
+
+    expect(unwrap(result[0].fields)).toEqual(expected);
+  });
+
+  it('generate Matrix with multiple fields and value type', () => {
+    const cfg: DataTransformerConfig<GroupingToMatrixTransformerOptions> = {
+      id: DataTransformerID.groupingToMatrix,
+      options: {
+        columnField: 'Column',
+        rowField: 'Row',
+        valueField: 'Temp',
+      },
+    };
+
+    const seriesA = toDataFrame({
+      name: 'A',
+      fields: [
+        { name: 'Column', type: FieldType.string, values: ['C1', 'C1', 'C2'] },
+        { name: 'Row', type: FieldType.string, values: ['R1', 'R2', 'R1'] },
+        { name: 'Temp', type: FieldType.number, values: [1, 4, 5], config: { units: 'celsius' } },
+      ],
+    });
+
+    const result = transformDataFrame([cfg], [seriesA, seriesB]);
+    const expected: Field[] = [
+      createField('Row\\Column', FieldType.string, []),
+      createField('C1', FieldType.number, [1, 4, ''], { units: 'celsius' }),
+      createField('C2', FieldType.number, [5, '', ''], { units: 'celsius' }),
+    ];
+
+    expect(unwrap(result[0].fields)).toEqual(expected);
+  });
+});
+
+const createField = (name: string, type: FieldType, values: any[], config = {}): Field => {
+  return { name, type, values: new ArrayVector(values), config, labels: undefined };
+};
+
+const unwrap = (fields: Field[]): Field[] => {
+  return fields.map(field =>
+    createField(
+      field.name,
+      field.type,
+      field.values.toArray().map((value: any) => value),
+      field.config
+    )
+  );
+};
