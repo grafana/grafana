b74b5f8a596c8b26700cf581368b89252cf9f437
Borja Garrido <bgarrido@cern.ch>
2020-11-02 14:26:59 +0100

    Update transformer to latest interfaces

--- packages/grafana-data/src/transformations/transformers/groupingToMatrix.test.ts
+++ packages/grafana-data/src/transformations/transformers/groupingToMatrix.test.ts
@@ -5,13 +5,14 @@ import { toDataFrame } from '../../dataframe';
 import { transformDataFrame } from '../transformDataFrame';
 import { ArrayVector } from '../../vector';
 import { groupingToMatrixTransformer, GroupingToMatrixTransformerOptions } from './groupingToMatrix';
+import { observableTester } from '../../utils/tests/observableTester';
 
 describe('Grouping to Matrix', () => {
   beforeAll(() => {
     mockTransformationsRegistry([groupingToMatrixTransformer]);
   });
 
-  it('generate Matrix with default fields', () => {
+  it('generate Matrix with default fields', done => {
     const cfg: DataTransformerConfig<GroupingToMatrixTransformerOptions> = {
       id: DataTransformerID.groupingToMatrix,
       options: {},
@@ -25,18 +26,23 @@ describe('Grouping to Matrix', () => {
       ],
     });
 
-    const result = transformDataFrame([cfg], [seriesA]);
-    const expected: Field[] = [
-      createField('Time\\Time', FieldType.string, [1000, 1001, 1002]),
-      createField('1000', FieldType.number, [1, '', '']),
-      createField('1001', FieldType.number, ['', 2, '']),
-      createField('1002', FieldType.number, ['', '', 3]),
-    ];
+    observableTester().subscribeAndExpectOnNext({
+      observable: transformDataFrame([cfg], [seriesA]),
+      expect: result => {
+        const expected: Field[] = [
+          createField('Time\\Time', FieldType.string, [1000, 1001, 1002]),
+          createField('1000', FieldType.number, [1, '', '']),
+          createField('1001', FieldType.number, ['', 2, '']),
+          createField('1002', FieldType.number, ['', '', 3]),
+        ];
 
-    expect(unwrap(result[0].fields)).toEqual(expected);
+        expect(unwrap(result[0].fields)).toEqual(expected);
+      },
+      done,
+    });
   });
 
-  it('generate Matrix with multiple fields', () => {
+  it('generate Matrix with multiple fields', done => {
     const cfg: DataTransformerConfig<GroupingToMatrixTransformerOptions> = {
       id: DataTransformerID.groupingToMatrix,
       options: {
@@ -55,17 +61,22 @@ describe('Grouping to Matrix', () => {
       ],
     });
 
-    const result = transformDataFrame([cfg], [seriesA]);
-    const expected: Field[] = [
-      createField('Row\\Column', FieldType.string, []),
-      createField('C1', FieldType.number, [1, 4, '']),
-      createField('C2', FieldType.number, [5, '', '']),
-    ];
+    observableTester().subscribeAndExpectOnNext({
+      observable: transformDataFrame([cfg], [seriesA]),
+      expect: result => {
+        const expected: Field[] = [
+          createField('Row\\Column', FieldType.string, ['R1', 'R2']),
+          createField('C1', FieldType.number, [1, 4]),
+          createField('C2', FieldType.number, [5, '']),
+        ];
 
-    expect(unwrap(result[0].fields)).toEqual(expected);
+        expect(unwrap(result[0].fields)).toEqual(expected);
+      },
+      done,
+    });
   });
 
-  it('generate Matrix with multiple fields and value type', () => {
+  it('generate Matrix with multiple fields and value type', done => {
     const cfg: DataTransformerConfig<GroupingToMatrixTransformerOptions> = {
       id: DataTransformerID.groupingToMatrix,
       options: {
@@ -84,14 +95,19 @@ describe('Grouping to Matrix', () => {
       ],
     });
 
-    const result = transformDataFrame([cfg], [seriesA]);
-    const expected: Field[] = [
-      createField('Row\\Column', FieldType.string, []),
-      createField('C1', FieldType.number, [1, 4, ''], { units: 'celsius' }),
-      createField('C2', FieldType.number, [5, '', ''], { units: 'celsius' }),
-    ];
+    observableTester().subscribeAndExpectOnNext({
+      observable: transformDataFrame([cfg], [seriesA]),
+      expect: result => {
+        const expected: Field[] = [
+          createField('Row\\Column', FieldType.string, ['R1', 'R2']),
+          createField('C1', FieldType.number, [1, 4], { units: 'celsius' }),
+          createField('C2', FieldType.number, [5, ''], { units: 'celsius' }),
+        ];
 
-    expect(unwrap(result[0].fields)).toEqual(expected);
+        expect(unwrap(result[0].fields)).toEqual(expected);
+      },
+      done,
+    });
   });
 });
 
