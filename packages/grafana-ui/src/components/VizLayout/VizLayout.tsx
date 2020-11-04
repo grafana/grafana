3c8a875d8ea76370497b9199fd79535e5a940610
Torkel Ödegaard <torkel@grafana.com>
2020-11-04 15:05:55 +0100

    Updates

---
+++ packages/grafana-ui/src/components/VizLayout/VizLayout.tsx
@@ -0,0 +1,91 @@
+import React, { FC, CSSProperties, ComponentType } from 'react';
+import { useMeasure } from 'react-use';
+import CustomScrollbar from '../CustomScrollbar/CustomScrollbar';
+
+export interface Props {
+  width: number;
+  height: number;
+  legend?: React.ReactElement<VizLayoutSlotProps>;
+  children: (width: number, height: number) => React.ReactNode;
+}
+
+export interface VizLayoutType extends FC<Props> {
+  Slot: ComponentType<VizLayoutSlotProps>;
+}
+
+export const VizLayout: VizLayoutType = ({ width, height, legend, children }) => {
+  const containerStyle: CSSProperties = {
+    display: 'flex',
+    width: `${width}px`,
+    height: `${height}px`,
+  };
+
+  if (!legend) {
+    return <div style={containerStyle}>{children(width, height)}</div>;
+  }
+
+  const { position, maxHeight, maxWidth } = legend.props;
+  const [legendRef, legendMeasure] = useMeasure();
+  let size: VizSize | null = null;
+
+  const vizStyle: CSSProperties = {
+    flexGrow: 1,
+  };
+
+  const legendStyle: CSSProperties = {
+    flexGrow: 1,
+  };
+
+  let positionWithAuto = position;
+  if (position === 'auto') {
+    positionWithAuto = width > height ? 'right' : 'bottom';
+  }
+
+  switch (positionWithAuto) {
+    case 'bottom':
+      containerStyle.flexDirection = 'column';
+      legendStyle.maxHeight = maxHeight;
+
+      if (legendMeasure) {
+        size = { width, height: height - legendMeasure.height };
+      }
+      break;
+    case 'right':
+      containerStyle.flexDirection = 'row';
+      legendStyle.maxWidth = maxWidth;
+
+      if (legendMeasure) {
+        size = { width: width - legendMeasure.width, height };
+      }
+      break;
+  }
+
+  return (
+    <div style={containerStyle}>
+      <div style={vizStyle}>{size && children(size.width, size.height)}</div>
+      {legend && (
+        <div style={legendStyle} ref={legendRef}>
+          <CustomScrollbar>{legend}</CustomScrollbar>
+        </div>
+      )}
+    </div>
+  );
+};
+
+interface VizSize {
+  width: number;
+  height: number;
+}
+
+export interface VizLayoutSlotProps {
+  position?: string;
+  maxHeight?: string;
+  maxWidth?: string;
+  children: React.ReactNode;
+}
+
+export const VizLayoutSlot: FC<VizLayoutSlotProps> = ({ children }) => {
+  return <>{children}</>;
+};
+
+VizLayout.Slot = VizLayoutSlot;
