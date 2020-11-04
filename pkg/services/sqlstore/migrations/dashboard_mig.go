15fc2aea65420a5126d3c0d4d72e6d221d9a5d1f
Agnès Toulet <agnes.toulet@gmail.com>
2020-11-04 17:31:58 +0100

    fix migrations order

--- pkg/services/sqlstore/migrations/dashboard_mig.go
+++ pkg/services/sqlstore/migrations/dashboard_mig.go
@@ -222,4 +222,7 @@ func addDashboardMigration(mg *Migrator) {
 
 	mg.AddMigration("delete tags for deleted dashboards", NewRawSqlMigration(
 		"DELETE FROM dashboard_tag WHERE dashboard_id NOT IN (SELECT id FROM dashboard)"))
+
+	mg.AddMigration("delete stars for deleted dashboards", NewRawSqlMigration(
+		"DELETE FROM star WHERE dashboard_id NOT IN (SELECT id FROM dashboard)"))
 }
