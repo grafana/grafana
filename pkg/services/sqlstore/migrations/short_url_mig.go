dadabc26b35ba5ff49e1cdc523dec6c31cc947b9
Arve Knudsen <arve.knudsen@gmail.com>
2020-11-05 11:31:15 +0100

    Chore: Fix issues reported by staticcheck
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/services/sqlstore/migrations/short_url_mig.go
+++ pkg/services/sqlstore/migrations/short_url_mig.go
@@ -2,27 +2,26 @@ package migrations
 
 import (
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
-	. "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 )
 
-func addShortURLMigrations(mg *Migrator) {
-	shortURLV1 := Table{
+func addShortURLMigrations(mg *migrator.Migrator) {
+	shortURLV1 := migrator.Table{
 		Name: "short_url",
-		Columns: []*Column{
-			{Name: "id", Type: DB_BigInt, Nullable: false, IsPrimaryKey: true, IsAutoIncrement: true},
-			{Name: "org_id", Type: DB_BigInt, Nullable: false},
-			{Name: "uid", Type: DB_NVarchar, Length: 40, Nullable: false},
-			{Name: "path", Type: DB_Text, Nullable: false},
-			{Name: "created_by", Type: DB_Int, Nullable: false},
-			{Name: "created_at", Type: DB_Int, Nullable: false},
-			{Name: "last_seen_at", Type: DB_Int, Nullable: true},
+		Columns: []*migrator.Column{
+			{Name: "id", Type: migrator.DB_BigInt, Nullable: false, IsPrimaryKey: true, IsAutoIncrement: true},
+			{Name: "org_id", Type: migrator.DB_BigInt, Nullable: false},
+			{Name: "uid", Type: migrator.DB_NVarchar, Length: 40, Nullable: false},
+			{Name: "path", Type: migrator.DB_Text, Nullable: false},
+			{Name: "created_by", Type: migrator.DB_Int, Nullable: false},
+			{Name: "created_at", Type: migrator.DB_Int, Nullable: false},
+			{Name: "last_seen_at", Type: migrator.DB_Int, Nullable: true},
 		},
 		Indices: []*migrator.Index{
 			{Cols: []string{"org_id", "uid"}, Type: migrator.UniqueIndex},
 		},
 	}
 
-	mg.AddMigration("create short_url table v1", NewAddTableMigration(shortURLV1))
+	mg.AddMigration("create short_url table v1", migrator.NewAddTableMigration(shortURLV1))
 
 	mg.AddMigration("add index short_url.org_id-uid", migrator.NewAddIndexMigration(shortURLV1, shortURLV1.Indices[0]))
 }
--- pkg/services/sqlstore/migrations/user_auth_token_mig.go
+++ pkg/services/sqlstore/migrations/user_auth_token_mig.go
@@ -1,35 +1,35 @@
 package migrations
 
 import (
-	. "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
+	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 )
 
-func addUserAuthTokenMigrations(mg *Migrator) {
-	userAuthTokenV1 := Table{
+func addUserAuthTokenMigrations(mg *migrator.Migrator) {
+	userAuthTokenV1 := migrator.Table{
 		Name: "user_auth_token",
-		Columns: []*Column{
-			{Name: "id", Type: DB_BigInt, IsPrimaryKey: true, IsAutoIncrement: true},
-			{Name: "user_id", Type: DB_BigInt, Nullable: false},
-			{Name: "auth_token", Type: DB_NVarchar, Length: 100, Nullable: false},
-			{Name: "prev_auth_token", Type: DB_NVarchar, Length: 100, Nullable: false},
-			{Name: "user_agent", Type: DB_NVarchar, Length: 255, Nullable: false},
-			{Name: "client_ip", Type: DB_NVarchar, Length: 255, Nullable: false},
-			{Name: "auth_token_seen", Type: DB_Bool, Nullable: false},
-			{Name: "seen_at", Type: DB_Int, Nullable: true},
-			{Name: "rotated_at", Type: DB_Int, Nullable: false},
-			{Name: "created_at", Type: DB_Int, Nullable: false},
-			{Name: "updated_at", Type: DB_Int, Nullable: false},
+		Columns: []*migrator.Column{
+			{Name: "id", Type: migrator.DB_BigInt, IsPrimaryKey: true, IsAutoIncrement: true},
+			{Name: "user_id", Type: migrator.DB_BigInt, Nullable: false},
+			{Name: "auth_token", Type: migrator.DB_NVarchar, Length: 100, Nullable: false},
+			{Name: "prev_auth_token", Type: migrator.DB_NVarchar, Length: 100, Nullable: false},
+			{Name: "user_agent", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
+			{Name: "client_ip", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
+			{Name: "auth_token_seen", Type: migrator.DB_Bool, Nullable: false},
+			{Name: "seen_at", Type: migrator.DB_Int, Nullable: true},
+			{Name: "rotated_at", Type: migrator.DB_Int, Nullable: false},
+			{Name: "created_at", Type: migrator.DB_Int, Nullable: false},
+			{Name: "updated_at", Type: migrator.DB_Int, Nullable: false},
 		},
-		Indices: []*Index{
-			{Cols: []string{"auth_token"}, Type: UniqueIndex},
-			{Cols: []string{"prev_auth_token"}, Type: UniqueIndex},
-			{Cols: []string{"user_id"}, Type: IndexType},
+		Indices: []*migrator.Index{
+			{Cols: []string{"auth_token"}, Type: migrator.UniqueIndex},
+			{Cols: []string{"prev_auth_token"}, Type: migrator.UniqueIndex},
+			{Cols: []string{"user_id"}, Type: migrator.IndexType},
 		},
 	}
 
-	mg.AddMigration("create user auth token table", NewAddTableMigration(userAuthTokenV1))
-	mg.AddMigration("add unique index user_auth_token.auth_token", NewAddIndexMigration(userAuthTokenV1, userAuthTokenV1.Indices[0]))
-	mg.AddMigration("add unique index user_auth_token.prev_auth_token", NewAddIndexMigration(userAuthTokenV1, userAuthTokenV1.Indices[1]))
+	mg.AddMigration("create user auth token table", migrator.NewAddTableMigration(userAuthTokenV1))
+	mg.AddMigration("add unique index user_auth_token.auth_token", migrator.NewAddIndexMigration(userAuthTokenV1, userAuthTokenV1.Indices[0]))
+	mg.AddMigration("add unique index user_auth_token.prev_auth_token", migrator.NewAddIndexMigration(userAuthTokenV1, userAuthTokenV1.Indices[1]))
 
-	mg.AddMigration("add index user_auth_token.user_id", NewAddIndexMigration(userAuthTokenV1, userAuthTokenV1.Indices[2]))
+	mg.AddMigration("add index user_auth_token.user_id", migrator.NewAddIndexMigration(userAuthTokenV1, userAuthTokenV1.Indices[2]))
 }
