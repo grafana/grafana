8cf3416cc6ef4adaf4e19e3173057efd8e7205f1
kay delaney <kay@grafana.com>
2020-11-11 15:19:20 +0000

    Dashboards: Exit to dashboard when deleting panel from panel view / edit view Closes #11941

--- public/app/features/dashboard/utils/panel.ts
+++ public/app/features/dashboard/utils/panel.ts
@@ -1,5 +1,6 @@
 // Store
 import store from 'app/core/store';
+import { store as reduxStore } from 'app/store/store';
 
 // Models
 import { DashboardModel } from 'app/features/dashboard/state/DashboardModel';
@@ -21,6 +22,7 @@ import { LS_PANEL_COPY_KEY, PANEL_BORDER } from 'app/core/constants';
 import { CoreEvents } from 'app/types';
 
 import { ShareModal } from 'app/features/dashboard/components/ShareModal';
+import { updateLocation } from 'app/core/actions';
 
 export const removePanel = (dashboard: DashboardModel, panel: PanelModel, ask: boolean) => {
   // confirm deletion
@@ -39,7 +41,14 @@ export const removePanel = (dashboard: DashboardModel, panel: PanelModel, ask: b
     });
     return;
   }
+
   dashboard.removePanel(panel);
+  reduxStore.dispatch(
+    updateLocation({
+      query: { viewPanel: null, editPanel: null, tab: null },
+      partial: true,
+    })
+  );
 };
 
 export const duplicatePanel = (dashboard: DashboardModel, panel: PanelModel) => {
