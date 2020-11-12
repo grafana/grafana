0500f67fc1bf849f1f4fa256080d3ef5e657a8e1
Jack Westbrook <jack.westbrook@gmail.com>
2020-11-12 14:52:02 +0100

    refactor(storybook): use previous knobs addon registration

--- packages/grafana-ui/.storybook/main.ts
+++ packages/grafana-ui/.storybook/main.ts
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   stories: stories,
   addons: [
-    '@storybook/addon-knobs/register',
+    '@storybook/addon-knobs',
     '@storybook/addon-actions',
     '@storybook/addon-docs',
     'storybook-dark-mode/register',
