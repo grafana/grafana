1cf8de1c34ad2d6ed1fd027f855859ac4dcae88e
Josiah Goodson <josiah.goodson@gmail.com>
2020-11-13 17:16:58 -0500

    Add support for relative URLs to the news panel

--- public/app/plugins/panel/news/rss.ts
+++ public/app/plugins/panel/news/rss.ts
@@ -9,6 +9,10 @@ export async function loadRSSFeed(url: string): Promise<RssFeed> {
     items: [],
   };
 
+  // regex and url for handling feeds with relative paths in <item> <link>
+  const r = new RegExp('^(?:[a-z]+:)?//', 'i');
+  const base_url = new URL(url).protocol + new URL(url).host;
+
   const getProperty = (node: Element, property: string) => {
     const propNode = node.querySelector(property);
     if (propNode) {
@@ -20,7 +24,7 @@ export async function loadRSSFeed(url: string): Promise<RssFeed> {
   doc.querySelectorAll('item').forEach(node => {
     const item: RssItem = {
       title: getProperty(node, 'title'),
-      link: getProperty(node, 'link'),
+      link: r.test(getProperty(node, 'link')) ? getProperty(node, 'link') : base_url + getProperty(node, 'link'),
       content: getProperty(node, 'description'),
       pubDate: getProperty(node, 'pubDate'),
     };
