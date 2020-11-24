35760ee4bef3007de2e5a6b8a69baeb8950418c7
Hugo Häggmark <hugo.haggmark@grafana.com>
2020-11-24 15:44:40 +0100

    Refactor: removes gf-form div

--- public/app/features/variables/editor/LegacyVariableQueryEditor.tsx
+++ public/app/features/variables/editor/LegacyVariableQueryEditor.tsx
@@ -3,10 +3,14 @@ import { selectors } from '@grafana/e2e-selectors';
 
 import { VariableQueryProps } from 'app/types/plugins';
 import { VariableTextAreaField } from './VariableTextAreaField';
+import { useStyles } from '@grafana/ui';
+import { GrafanaTheme } from '@grafana/data';
+import { css } from 'emotion';
 
 export const LEGACY_VARIABLE_QUERY_EDITOR_NAME = 'Grafana-LegacyVariableQueryEditor';
 
 export const LegacyVariableQueryEditor: FC<VariableQueryProps> = ({ onChange, query }) => {
+  const styles = useStyles(getStyles);
   const [value, setValue] = useState(query);
   const onValueChange = useCallback(
     (event: React.FormEvent<HTMLTextAreaElement>) => {
@@ -23,18 +27,28 @@ export const LegacyVariableQueryEditor: FC<VariableQueryProps> = ({ onChange, qu
   );
 
   return (
-    <VariableTextAreaField
-      name="Query"
-      value={value}
-      placeholder="metric name or tags query"
-      width={100}
-      onChange={onValueChange}
-      onBlur={onBlur}
-      required
-      labelWidth={20}
-      ariaLabel={selectors.pages.Dashboard.Settings.Variables.Edit.QueryVariable.queryOptionsQueryInput}
-    />
+    <div className={styles.container}>
+      <VariableTextAreaField
+        name="Query"
+        value={value}
+        placeholder="metric name or tags query"
+        width={100}
+        onChange={onValueChange}
+        onBlur={onBlur}
+        required
+        labelWidth={20}
+        ariaLabel={selectors.pages.Dashboard.Settings.Variables.Edit.QueryVariable.queryOptionsQueryInput}
+      />
+    </div>
   );
 };
+
+function getStyles(theme: GrafanaTheme) {
+  return {
+    container: css`
+      margin-bottom: ${theme.spacing.xs};
+    `,
+  };
+}
 
 LegacyVariableQueryEditor.displayName = LEGACY_VARIABLE_QUERY_EDITOR_NAME;
