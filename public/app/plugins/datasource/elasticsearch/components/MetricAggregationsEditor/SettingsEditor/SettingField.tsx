d8c515ef8cf80750d7acb671e3f733c95d6f460c
Elfo404 <gio.ricci@grafana.com>
2020-11-27 13:45:37 +0000

    Add autogenerated ID to SettingField

--- public/app/plugins/datasource/elasticsearch/components/MetricAggregationsEditor/SettingsEditor/SettingField.tsx
+++ public/app/plugins/datasource/elasticsearch/components/MetricAggregationsEditor/SettingsEditor/SettingField.tsx
@@ -1,10 +1,11 @@
-import React, { ComponentProps } from 'react';
+import React, { ComponentProps, useState } from 'react';
 import { InlineField, Input } from '@grafana/ui';
 import { useDispatch } from '../../../hooks/useStatelessReducer';
 import { changeMetricSetting } from '../state/actions';
 import { ChangeMetricSettingAction } from '../state/types';
 import { SettingKeyOf } from '../../types';
 import { MetricAggregationWithSettings } from '../aggregations';
+import { uniqueId } from 'lodash';
 
 interface Props<T extends MetricAggregationWithSettings, K extends SettingKeyOf<T>> {
   label: string;
@@ -22,11 +23,13 @@ export function SettingField<T extends MetricAggregationWithSettings, K extends 
   tooltip,
 }: Props<T, K>) {
   const dispatch = useDispatch<ChangeMetricSettingAction<T>>();
+  const [id] = useState(uniqueId(`es-field-id-`));
   const settings = metric.settings;
 
   return (
     <InlineField label={label} labelWidth={16} tooltip={tooltip}>
       <Input
+        id={id}
         placeholder={placeholder}
         onBlur={e => dispatch(changeMetricSetting(metric, settingName, e.target.value as any))}
         defaultValue={settings?.[settingName as keyof typeof settings]}
