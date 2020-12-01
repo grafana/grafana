e626a4e8be8b6feb5914a160a87199b6a125c2ca
Arve Knudsen <arve.knudsen@gmail.com>
2020-12-01 11:33:10 +0100

    middleware: Rename auth_proxy directory to authproxy
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/middleware/auth_proxy.go
+++ pkg/middleware/auth_proxy.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/infra/remotecache"
-	authproxy "github.com/grafana/grafana/pkg/middleware/auth_proxy"
+	"github.com/grafana/grafana/pkg/middleware/authproxy"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/setting"
 )
--- pkg/middleware/auth_proxy_test.go
+++ pkg/middleware/auth_proxy_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/infra/remotecache"
-	authproxy "github.com/grafana/grafana/pkg/middleware/auth_proxy"
+	"github.com/grafana/grafana/pkg/middleware/authproxy"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/stretchr/testify/require"
