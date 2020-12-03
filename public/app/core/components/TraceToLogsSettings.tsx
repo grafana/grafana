4579696bb4b2f624a5bc77abd8dc6b029ce46bde
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-12-03 20:27:56 +0100

    Add info to settings

--- public/app/core/components/TraceToLogsSettings.tsx
+++ public/app/core/components/TraceToLogsSettings.tsx
@@ -2,10 +2,12 @@ import {
   DataSourceJsonData,
   DataSourcePluginOptionsEditorProps,
   DataSourceSelectItem,
+  GrafanaTheme,
   updateDatasourcePluginJsonDataOption,
 } from '@grafana/data';
-import { InlineField } from '@grafana/ui';
+import { InlineField, useStyles } from '@grafana/ui';
 import { getDatasourceSrv } from 'app/features/plugins/datasource_srv';
+import { css } from 'emotion';
 import React from 'react';
 import { DataSourcePicker } from './Select/DataSourcePicker';
 
@@ -32,11 +34,18 @@ export function TraceToLogsSettings({ options, onOptionsChange }: Props) {
         } as DataSourceSelectItem)
     );
   let selectedDatasource = datasources.find(d => d.value === options.jsonData.tracesToLogs?.datasourceUid);
+
+  const styles = useStyles(getStyles);
+
   return (
     <>
       <h3 className="page-heading">Trace to logs</h3>
 
-      <InlineField label="Data source">
+      <div className={styles.infoText}>
+        Trace to logs let&apos;s you navigate from a trace span to the selected data source&apos;s log.
+      </div>
+
+      <InlineField label="Data source" tooltip="The data source the trace is going to navigate to">
         <DataSourcePicker
           datasources={datasources}
           current={selectedDatasource}
@@ -50,3 +59,10 @@ export function TraceToLogsSettings({ options, onOptionsChange }: Props) {
     </>
   );
 }
+
+const getStyles = (theme: GrafanaTheme) => ({
+  infoText: css`
+    padding-bottom: ${theme.spacing.md};
+    color: ${theme.colors.textSemiWeak};
+  `,
+});
