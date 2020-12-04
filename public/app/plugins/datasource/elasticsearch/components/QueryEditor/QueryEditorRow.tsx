64bf64c015de0c5cb8d524710d5e7e7a6390fe90
Elfo404 <gio.ricci@grafana.com>
2020-12-04 11:10:16 +0000

    Cleanup QueryEditorRow styles

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/QueryEditorRow.tsx
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/QueryEditorRow.tsx
@@ -1,16 +1,9 @@
 import { GrafanaTheme } from '@grafana/data';
 import { IconButton, stylesFactory, useTheme } from '@grafana/ui';
 import { getInlineLabelStyles } from '@grafana/ui/src/components/Forms/InlineLabel';
-import { css, cx } from 'emotion';
+import { css } from 'emotion';
 import { noop } from 'lodash';
 import React, { FunctionComponent } from 'react';
-
-const flex = css`
-  label: QueryEditorRow;
-  display: flex;
-  align-items: start;
-  flex-direction: row;
-`;
 
 interface Props {
   label: string;
@@ -30,59 +23,45 @@ export const QueryEditorRow: FunctionComponent<Props> = ({
   const styles = getStyles(theme);
 
   return (
-    <fieldset
-      className={css`
-        display: block;
-        margin-bottom: ${theme.spacing.xs};
-      `}
-    >
-      <div className={cx(flex)}>
-        <div className={cx(getInlineLabelStyles(theme, 17).label)}>
-          <legend
-            className={css`
-              font-size: ${theme.typography.size.sm};
-              margin: 0;
-            `}
-          >
-            {label}
-          </legend>
-          <div
-            className={css`
-              display: flex;
-              flex-direction: row;
-              align-items: center;
-            `}
-          >
-            {onHideClick && (
-              <IconButton
-                name={hidden ? 'eye-slash' : 'eye'}
-                onClick={onHideClick}
-                surface="header"
-                size="sm"
-                aria-pressed={hidden}
-                aria-label="hide metric"
-                className={styles.icon}
-              />
-            )}
-            <IconButton
-              name="trash-alt"
-              surface="header"
-              size="sm"
-              className={styles.icon}
-              onClick={onRemoveClick || noop}
-              disabled={!onRemoveClick}
-              aria-label="remove metric"
-            />
-          </div>
-        </div>
-        {children}
+    <fieldset className={styles.root}>
+      <div className={getInlineLabelStyles(theme, 17).label}>
+        <legend className={styles.label}>{label}</legend>
+        {onHideClick && (
+          <IconButton
+            name={hidden ? 'eye-slash' : 'eye'}
+            onClick={onHideClick}
+            surface="header"
+            size="sm"
+            aria-pressed={hidden}
+            aria-label="hide metric"
+            className={styles.icon}
+          />
+        )}
+        <IconButton
+          name="trash-alt"
+          surface="header"
+          size="sm"
+          className={styles.icon}
+          onClick={onRemoveClick || noop}
+          disabled={!onRemoveClick}
+          aria-label="remove metric"
+        />
       </div>
+      {children}
     </fieldset>
   );
 };
 
 const getStyles = stylesFactory((theme: GrafanaTheme) => {
   return {
+    root: css`
+      display: flex;
+      margin-bottom: ${theme.spacing.xs};
+    `,
+    label: css`
+      font-size: ${theme.typography.size.sm};
+      margin: 0;
+    `,
     icon: css`
       color: ${theme.colors.textWeak};
       margin-left: ${theme.spacing.xxs};
