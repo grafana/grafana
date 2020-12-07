0ea84cd122ae6ed0c24a2a239d10513d94446ef5
Leon Sorokin <leeoniya@gmail.com>
2020-12-06 19:26:00 -0600

    GraphNG: fix and optimize spanNulls (#29633)
    * fix and optimize spanNulls
    
    * AsZero implies spanNulls = true, to prevent null-scanning
    
    * move spanNulls toggle below fillOpacity

--- public/app/plugins/panel/graph3/module.tsx
+++ public/app/plugins/panel/graph3/module.tsx
@@ -66,6 +66,18 @@ export const plugin = new PanelPlugin<Options, GraphFieldConfig>(GraphPanel)
           showIf: c => c.drawStyle !== DrawStyle.Points,
         })
         .addRadio({
+          path: 'spanNulls',
+          name: 'Null values',
+          defaultValue: false,
+          settings: {
+            options: [
+              { label: 'Gaps', value: false },
+              { label: 'Connected', value: true },
+            ],
+          },
+          showIf: c => c.drawStyle === DrawStyle.Line,
+        })
+        .addRadio({
           path: 'points',
           name: 'Points',
           defaultValue: graphFieldOptions.points[0].value,
@@ -83,17 +95,6 @@ export const plugin = new PanelPlugin<Options, GraphFieldConfig>(GraphPanel)
             step: 1,
           },
           showIf: c => c.points !== PointMode.Never,
-        })
-        .addRadio({
-          path: 'spanNulls',
-          name: 'Null values',
-          defaultValue: false,
-          settings: {
-            options: [
-              { label: 'Gaps', value: false },
-              { label: 'Connected', value: true },
-            ],
-          },
         })
         .addRadio({
           path: 'axisPlacement',
