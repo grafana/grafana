4e4211965ebd9326d47a17edaaba3d89e526b6de
Ryan McKinley <ryantxu@gmail.com>
2020-12-07 05:48:10 -0800

    GraphNG: time range should match the panel timeRange (#29596)
    * match time range
    
    * less logging
    
    * almost working
    
    * more logging
    
    * more logging
    
    * fix snapshot
    
    * Use layout effect for the time range ref update
    
    Co-authored-by: Dominik Prokop <dominik.prokop@grafana.com>

--- packages/grafana-ui/src/components/GraphNG/GraphNG.tsx
+++ packages/grafana-ui/src/components/GraphNG/GraphNG.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { useCallback, useLayoutEffect, useMemo, useRef } from 'react';
 import {
   compareDataFrameStructures,
   DataFrame,
@@ -8,6 +8,7 @@ import {
   formattedValueToString,
   getFieldColorModeForField,
   getFieldDisplayName,
+  TimeRange,
 } from '@grafana/data';
 import { alignDataFrames } from './utils';
 import { UPlotChart } from '../uPlot/Plot';
@@ -58,10 +59,16 @@ export const GraphNG: React.FC<GraphNGProps> = ({
 
   const compareFrames = useCallback((a?: DataFrame | null, b?: DataFrame | null) => {
     if (a && b) {
-      return compareDataFrameStructures(a, b, ['min', 'max']);
+      return compareDataFrameStructures(a, b);
     }
     return false;
   }, []);
+
+  // reference change will not triger re-render
+  const currentTimeRange = useRef<TimeRange>(timeRange);
+  useLayoutEffect(() => {
+    currentTimeRange.current = timeRange;
+  }, [timeRange]);
 
   const configRev = useRevision(alignedFrame, compareFrames);
 
@@ -78,6 +85,10 @@ export const GraphNG: React.FC<GraphNGProps> = ({
       builder.addScale({
         scaleKey: 'x',
         isTime: true,
+        range: () => {
+          const r = currentTimeRange.current!;
+          return [r.from.valueOf(), r.to.valueOf()];
+        },
       });
       builder.addAxis({
         scaleKey: 'x',
