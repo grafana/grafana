79a0dbbd12d8bc239cc283f058c92efb974e476f
Eunice Kim <kimeuni@amazon.com>
2020-12-06 18:57:11 -0800

    Fix multi-ppl query

--- public/app/plugins/datasource/elasticsearch/datasource.ts
+++ public/app/plugins/datasource/elasticsearch/datasource.ts
@@ -494,15 +494,15 @@ export class ElasticDatasource extends DataSourceApi<ElasticsearchQuery, Elastic
       subQueries.push(
         from(this.post(this.getPPLUrl(), payload)).pipe(
           map((res: any) => {
-            const er = new ElasticResponse(targets, res, ElasticsearchQueryType.PPL);
+            const er = new ElasticResponse(target, res, ElasticsearchQueryType.PPL);
 
-            if (targets.some(target => target.isLogsQuery)) {
+            if (target.isLogsQuery) {
               const response = er.getLogs(this.logMessageField, this.logLevelField);
               for (const dataFrame of response.data) {
                 enhanceDataFrame(dataFrame, this.dataLinks);
               }
               return response;
-            } else if (targets.some(target => target.format === 'table')) {
+            } else if (target.format === 'table') {
               return er.getTable();
             }
             return er.getTimeSeries();
