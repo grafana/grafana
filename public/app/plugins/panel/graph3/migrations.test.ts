cf2ff80f246ddb61ca44cf225624a1023eabcf7d
Ryan McKinley <ryantxu@gmail.com>
2020-12-08 12:50:18 -0800

    GraphNG: fix bars migration and support color and linewidth (#29697)

--- public/app/plugins/panel/graph3/migrations.test.ts
+++ public/app/plugins/panel/graph3/migrations.test.ts
@@ -30,6 +30,15 @@ describe('Graph Migrations', () => {
     panel.options = graphPanelChangedHandler(panel, 'graph', old);
     expect(panel).toMatchSnapshot();
   });
+
+  it('stepped line', () => {
+    const old: any = {
+      angular: stepedColordLine,
+    };
+    const panel = {} as PanelModel;
+    panel.options = graphPanelChangedHandler(panel, 'graph', old);
+    expect(panel).toMatchSnapshot();
+  });
 });
 
 const stairscase = {
@@ -197,6 +206,99 @@ const twoYAxis = {
   stack: false,
   percentage: false,
   steppedLine: false,
+  timeFrom: null,
+  timeShift: null,
+  datasource: null,
+};
+
+const stepedColordLine = {
+  aliasColors: {
+    'A-series': 'red',
+  },
+  dashLength: 10,
+  fieldConfig: {
+    defaults: {
+      custom: {},
+    },
+    overrides: [],
+  },
+  fill: 5,
+  gridPos: {
+    h: 9,
+    w: 12,
+    x: 0,
+    y: 0,
+  },
+  id: 2,
+  legend: {
+    avg: false,
+    current: false,
+    max: false,
+    min: false,
+    show: true,
+    total: false,
+    values: false,
+  },
+  lines: true,
+  linewidth: 5,
+  maxDataPoints: 20,
+  nullPointMode: 'null',
+  options: {
+    alertThreshold: true,
+  },
+  pluginVersion: '7.4.0-pre',
+  pointradius: 2,
+  renderer: 'flot',
+  seriesOverrides: [],
+  spaceLength: 10,
+  steppedLine: true,
+  thresholds: [],
+  timeRegions: [],
+  title: 'Panel Title',
+  tooltip: {
+    shared: true,
+    sort: 0,
+    value_type: 'individual',
+  },
+  type: 'graph',
+  xaxis: {
+    buckets: null,
+    mode: 'time',
+    name: null,
+    show: true,
+    values: [],
+  },
+  yaxes: [
+    {
+      $$hashKey: 'object:38',
+      format: 'short',
+      label: null,
+      logBase: 1,
+      max: null,
+      min: null,
+      show: true,
+    },
+    {
+      $$hashKey: 'object:39',
+      format: 'short',
+      label: null,
+      logBase: 1,
+      max: null,
+      min: null,
+      show: true,
+    },
+  ],
+  yaxis: {
+    align: false,
+    alignLevel: null,
+  },
+  bars: false,
+  dashes: false,
+  fillGradient: 0,
+  hiddenSeries: false,
+  percentage: false,
+  points: false,
+  stack: false,
   timeFrom: null,
   timeShift: null,
   datasource: null,
