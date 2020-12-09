97675281221f1a39568d5f8b8d9066707e476b7e
Dominik Prokop <dominik.prokop@grafana.com>
2020-12-09 18:54:06 +0100

    ContextMenuPlugin WIP

--- public/app/plugins/panel/graph3/GraphPanel.tsx
+++ public/app/plugins/panel/graph3/GraphPanel.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { ContextMenuPlugin, TooltipPlugin, ZoomPlugin, GraphNG } from '@grafana/ui';
+import { TooltipPlugin, ZoomPlugin, GraphNG, MenuItemsGroup } from '@grafana/ui';
 import { PanelProps } from '@grafana/data';
 import { Options } from './types';
 import { AnnotationsPlugin } from './plugins/AnnotationsPlugin';
 import { ExemplarsPlugin } from './plugins/ExemplarsPlugin';
+import { ContextMenuPlugin } from './plugins/ContextMenuPlugin';
 
 interface GraphPanelProps extends PanelProps<Options> {}
 
@@ -16,6 +17,20 @@ export const GraphPanel: React.FC<GraphPanelProps> = ({
   options,
   onChangeTimeRange,
 }) => {
+  const defaultContextMenuLinks: MenuItemsGroup[] = [
+    {
+      items: [
+        {
+          label: 'Add annotation',
+          icon: 'comment-alt',
+          onClick: () => {
+            alert('TODO');
+          },
+        },
+      ],
+    },
+  ];
+
   return (
     <GraphNG
       data={data.series}
@@ -27,7 +42,7 @@ export const GraphPanel: React.FC<GraphPanelProps> = ({
     >
       <TooltipPlugin mode={options.tooltipOptions.mode as any} timeZone={timeZone} />
       <ZoomPlugin onZoom={onChangeTimeRange} />
-      <ContextMenuPlugin />
+      <ContextMenuPlugin timeZone={timeZone} defaultItems={defaultContextMenuLinks} />
       {data.annotations ? <ExemplarsPlugin exemplars={data.annotations} timeZone={timeZone} /> : <></>}
       {data.annotations ? <AnnotationsPlugin annotations={data.annotations} timeZone={timeZone} /> : <></>}
     </GraphNG>
