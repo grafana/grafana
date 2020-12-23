a0d232ed00e1f80d9e74bb8f2aeed7f028e4653a
Jack Westbrook <jack.westbrook@gmail.com>
2020-12-23 13:12:41 +0100

    refactor(dashboard): use angular directive to render version comparison

--- public/app/features/dashboard/components/DashboardSettings/VersionsSettings.tsx
+++ public/app/features/dashboard/components/DashboardSettings/VersionsSettings.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import _ from 'lodash';
-import { Spinner, HorizontalGroup, Checkbox, Button, Tag } from '@grafana/ui';
-import DangerouslySetHtmlContent from 'dangerously-set-html-content';
+import { Spinner, HorizontalGroup, Checkbox, Button, Tag, Icon } from '@grafana/ui';
+import { AngularComponent, getAngularLoader } from '@grafana/runtime';
 import { DashboardModel } from '../../state/DashboardModel';
 import { historySrv, RevisionsModel, CalculateDiffOptions } from '../VersionHistory/HistorySrv';
 
@@ -15,6 +15,9 @@ type State = {
   versions: DecoratedRevisionModel[];
   viewMode: 'list' | 'compare';
   delta: { basic: string; json: string };
+  newInfo?: DecoratedRevisionModel;
+  baseInfo?: DecoratedRevisionModel;
+  isNewLatest: boolean;
 };
 
 type DecoratedRevisionModel = RevisionsModel & {
@@ -32,14 +35,15 @@ export class VersionsSettings extends PureComponent<Props, State> {
     this.limit = 10;
     this.start = 0;
     this.state = {
-      isLoading: true,
-      isAppending: true,
-      versions: [],
-      viewMode: 'list',
       delta: {
         basic: '',
         json: '',
       },
+      isAppending: true,
+      isLoading: true,
+      versions: [],
+      viewMode: 'list',
+      isNewLatest: false,
     };
   }
 
@@ -63,13 +67,18 @@ export class VersionsSettings extends PureComponent<Props, State> {
   };
 
   getDiff = (diff = 'basic') => {
-    this.setState({ viewMode: 'compare', isLoading: true });
-
     const selectedVersions = this.state.versions.filter(version => version.checked);
-    const newInfo = selectedVersions[0];
-    const baseInfo = selectedVersions[1];
-    // TODO: isNewLatest needs workering.
+    const [newInfo, baseInfo] = selectedVersions;
     const isNewLatest = newInfo.version === this.props.dashboard.version;
+
+    this.setState({
+      baseInfo,
+      isLoading: true,
+      isNewLatest,
+      newInfo,
+      viewMode: 'compare',
+    });
+
     const options: CalculateDiffOptions = {
       new: {
         dashboardId: this.props.dashboard.id,
@@ -85,8 +94,8 @@ export class VersionsSettings extends PureComponent<Props, State> {
     return historySrv
       .calculateDiff(options)
       .then((response: any) => {
-        // @ts-ignore
         this.setState({
+          // @ts-ignore
           delta: {
             [diff]: response,
           },
@@ -124,25 +133,44 @@ export class VersionsSettings extends PureComponent<Props, State> {
     });
   };
 
-  setViewMode = () => {
+  reset = () => {
     this.setState({
+      baseInfo: undefined,
+      delta: { basic: '', json: '' },
+      isNewLatest: false,
+      newInfo: undefined,
+      versions: this.state.versions.map(version => ({ ...version, checked: false })),
       viewMode: 'list',
     });
   };
 
   render() {
-    const canCompare = this.state.versions.filter(version => version.checked).length !== 2;
-    const hasVersions = this.state.versions.length > 1;
-    const hasMore = this.state.versions.length >= this.limit;
+    const { versions, viewMode } = this.state;
+    const canCompare = versions.filter(version => version.checked).length !== 2;
+    const hasVersions = versions.length > 1;
+    const hasMore = versions.length >= this.limit;
 
-    if (this.state.viewMode === 'compare') {
+    if (viewMode === 'compare') {
       return (
         <div>
-          <VersionsHeader />
+          <VersionsHeader
+            isComparing
+            onClick={this.reset}
+            baseVersion={this.state.baseInfo?.version}
+            newVersion={this.state.newInfo?.version}
+            isNewLatest={this.state.isNewLatest}
+          />
           {this.state.isLoading ? (
             <VersionsSpinner msg="Fetching changes&hellip;" />
           ) : (
-            <VersionsDiff delta={this.state.delta} />
+            // Loads angular diff
+            <VersionsDiffView
+              dashboard={this.props.dashboard}
+              newInfo={this.state.newInfo}
+              baseInfo={this.state.baseInfo}
+              isNewLatest={this.state.isNewLatest}
+              delta={this.state.delta}
+            />
           )}
         </div>
       );
@@ -154,7 +182,7 @@ export class VersionsSettings extends PureComponent<Props, State> {
         {this.state.isLoading ? (
           <VersionsSpinner msg="Fetching history list&hellip;" />
         ) : (
-          <VersionsTable versions={this.state.versions} handleCheck={this.handleCheck} />
+          <VersionsTable versions={versions} handleCheck={this.handleCheck} />
         )}
         {this.state.isAppending && <VersionsSpinner msg="Fetching more entries&hellip;" />}
         <VersionsButtons
@@ -211,7 +239,25 @@ const VersionsButtons = ({
   </div>
 );
 
-const VersionsHeader = () => <h3 className="dashboard-settings__header">Versions</h3>;
+const VersionsHeader = ({
+  isComparing = false,
+  onClick = () => {},
+  baseVersion = 0,
+  newVersion = 0,
+  isNewLatest = false,
+}) => (
+  <h3 className="dashboard-settings__header">
+    <span onClick={onClick} className={isComparing ? 'pointer' : ''}>
+      Versions
+    </span>
+    {isComparing && (
+      <span>
+        <Icon name="angle-right" /> Comparing {baseVersion} <Icon name="arrows-h" /> {newVersion}{' '}
+        {isNewLatest && <cite className="muted">(Latest)</cite>}
+      </span>
+    )}
+  </h3>
+);
 
 const VersionsTable = ({ versions, handleCheck }: { versions: DecoratedRevisionModel[]; handleCheck: any }) => (
   <table className="filter-table">
@@ -252,12 +298,43 @@ const VersionsTable = ({ versions, handleCheck }: { versions: DecoratedRevisionM
   </table>
 );
 
-const VersionsDiff = ({ delta }) => {
-  return (
-    <div>
-      <div className="delta-basic">
-        <DangerouslySetHtmlContent html={delta.basic} />
-      </div>
-    </div>
-  );
+type DiffViewProps = {
+  dashboard: DashboardModel;
+  isNewLatest: boolean;
+  newInfo?: DecoratedRevisionModel;
+  baseInfo?: DecoratedRevisionModel;
+  delta: { basic: string; json: string };
 };
+
+export class VersionsDiffView extends PureComponent<DiffViewProps> {
+  element?: HTMLElement | null;
+  angularCmp?: AngularComponent;
+
+  constructor(props: DiffViewProps) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const loader = getAngularLoader();
+    const template =
+      '<gf-dashboard-history dashboard="dashboard" newinfo="newinfo" baseinfo="baseinfo" isnewlatest="isnewlatest" delta="delta"/>';
+    const scopeProps = {
+      dashboard: this.props.dashboard,
+      delta: this.props.delta,
+      baseinfo: this.props.baseInfo,
+      newinfo: this.props.newInfo,
+      isnewlatest: this.props.isNewLatest,
+    };
+    this.angularCmp = loader.load(this.element, scopeProps, template);
+  }
+
+  componentWillUnmount() {
+    if (this.angularCmp) {
+      this.angularCmp.destroy();
+    }
+  }
+
+  render() {
+    return <div ref={ref => (this.element = ref)} />;
+  }
+}
