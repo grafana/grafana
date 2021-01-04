28d4333fdd6cf5c0ffbd928eb5a14391ff04e121
Arve Knudsen <arve.knudsen@gmail.com>
2021-01-04 13:44:16 +0100

    Fix tests
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/services/sqlstore/sqlstore_test.go
+++ pkg/services/sqlstore/sqlstore_test.go
@@ -5,7 +5,8 @@ package sqlstore
 import (
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/setting"
 )
@@ -31,13 +32,13 @@ var sqlStoreTestCases = []sqlStoreTest{
 		connStrValues: []string{"host=1.2.3.4", "port=5678"},
 	},
 	{
-		name:          "Postgres IPv4 (Default Port)",
+		name:          "Postgres IPv4 (default port)",
 		dbType:        "postgres",
 		dbHost:        "1.2.3.4",
 		connStrValues: []string{"host=1.2.3.4", "port=5432"},
 	},
 	{
-		name:          "MySQL IPv4 (Default Port)",
+		name:          "MySQL IPv4 (default port)",
 		dbType:        "mysql",
 		dbHost:        "1.2.3.4",
 		connStrValues: []string{"tcp(1.2.3.4)"},
@@ -55,31 +56,31 @@ var sqlStoreTestCases = []sqlStoreTest{
 		connStrValues: []string{"host=fe80::24e8:31b2:91df:b177", "port=1234"},
 	},
 	{
-		name:          "MySQL IPv6 (Default Port)",
+		name:          "MySQL IPv6 (default port)",
 		dbType:        "mysql",
 		dbHost:        "[::1]",
 		connStrValues: []string{"tcp([::1])"},
 	},
 	{
-		name:          "Postgres IPv6 (Default Port)",
+		name:          "Postgres IPv6 (default port)",
 		dbType:        "postgres",
 		dbHost:        "[::1]",
 		connStrValues: []string{"host=::1", "port=5432"},
 	},
 	{
-		name:          "Postgres Unix Socket Escape Colon",
+		name:          "Postgres Unix socket escaped colon",
 		dbType:        "postgres",
-		dbHost:        "/IAmAPath/ThatWorksDuringNewYear/wierd\\:path:1234",
+		dbHost:        "/IAmAPath/ThatWorksDuringNewYear/weird\\:path:1234",
 		connStrValues: []string{"host=/IAmAPath/ThatWorksDuringNewYear/weird:path", "port=1234"},
 	},
 	{
-		name:          "Postgres Unix Socket (Default Port)",
+		name:          "Postgres Unix socket (default port)",
 		dbType:        "postgres",
-		dbHost:        "/IAmAPath/ThatWorksDuringNewYear/wierd:path",
+		dbHost:        "/IAmAPath/ThatWorksDuringNewYear/weird:path",
 		connStrValues: []string{"host=/IAmAPath/ThatWorksDuringNewYear/weird:path", "port=5432"},
 	},
 	{
-		name:          "Postgres Unix Socket Without Escape Colon",
+		name:          "Postgres Unix socket without escaped colon",
 		dbType:        "postgres",
 		dbHost:        "/IAmAPath/ThatWorksDuringNewYear/weird:path:1234",
 		connStrValues: []string{"host=/IAmAPath/ThatWorksDuringNewYear/weird:path", "port=1234"},
@@ -87,41 +88,38 @@ var sqlStoreTestCases = []sqlStoreTest{
 }
 
 func TestSQLConnectionString(t *testing.T) {
-	Convey("Testing SQL Connection Strings", t, func() {
-		t.Helper()
+	for _, testCase := range sqlStoreTestCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			sqlstore := &SQLStore{}
+			sqlstore.Cfg = makeSQLStoreTestConfig(t, testCase.dbType, testCase.dbHost)
+			sqlstore.readConfig()
 
-		for _, testCase := range sqlStoreTestCases {
-			Convey(testCase.name, func() {
-				sqlstore := &SQLStore{}
-				sqlstore.Cfg = makeSQLStoreTestConfig(testCase.dbType, testCase.dbHost)
-				sqlstore.readConfig()
-
-				connStr, err := sqlstore.buildConnectionString()
-
-				So(err, ShouldBeNil)
-				for _, connSubStr := range testCase.connStrValues {
-					So(connStr, ShouldContainSubstring, connSubStr)
-				}
-			})
-		}
-	})
+			connStr, err := sqlstore.buildConnectionString()
+			require.NoError(t, err)
+			for _, connSubStr := range testCase.connStrValues {
+				assert.Contains(t, connStr, connSubStr)
+			}
+		})
+	}
 }
 
-func makeSQLStoreTestConfig(dbType string, host string) *setting.Cfg {
+func makeSQLStoreTestConfig(t *testing.T, dbType string, host string) *setting.Cfg {
+	t.Helper()
+
 	cfg := setting.NewCfg()
 
 	sec, err := cfg.Raw.NewSection("database")
-	So(err, ShouldBeNil)
+	require.NoError(t, err)
 	_, err = sec.NewKey("type", dbType)
-	So(err, ShouldBeNil)
+	require.NoError(t, err)
 	_, err = sec.NewKey("host", host)
-	So(err, ShouldBeNil)
+	require.NoError(t, err)
 	_, err = sec.NewKey("user", "user")
-	So(err, ShouldBeNil)
+	require.NoError(t, err)
 	_, err = sec.NewKey("name", "test_db")
-	So(err, ShouldBeNil)
+	require.NoError(t, err)
 	_, err = sec.NewKey("password", "pass")
-	So(err, ShouldBeNil)
+	require.NoError(t, err)
 
 	return cfg
 }
