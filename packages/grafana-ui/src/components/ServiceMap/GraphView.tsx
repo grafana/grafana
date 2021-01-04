7c901c9cedbc3be238ecfc188780f1e1183905cc
Andrej Ocenas <mr.ocenas@gmail.com>
2021-01-04 10:41:47 +0100

    Add more complex layout

--- packages/grafana-ui/src/components/ServiceMap/GraphView.tsx
+++ packages/grafana-ui/src/components/ServiceMap/GraphView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useMemo, useCallback, MouseEvent, MutableRefObject } from 'react';
-import { forceSimulation, forceLink, forceManyBody, forceCollide } from 'd3-force';
+import { forceSimulation, forceLink, forceManyBody, forceCollide, forceX } from 'd3-force';
 import useMeasure from 'react-use/lib/useMeasure';
 // import { response } from './x-ray-response';
 import { usePanning } from './usePanning';
@@ -12,11 +12,11 @@ import { ContextMenu } from '..';
 import { DataFrame, DataFrameView, LinkModel } from '@grafana/data';
 import { useZoom } from './useZoom';
 
-interface Config extends Record<string, number> {
-  collide: number;
-  linkDistance: number;
-  chargeStrength: number;
-}
+// interface Config extends Record<string, number> {
+//   collide: number;
+//   linkDistance: number;
+//   chargeStrength: number;
+// }
 
 interface Props {
   services: DataFrame;
@@ -26,12 +26,28 @@ interface Props {
 }
 export function GraphView(props: Props) {
   const [measureRef, { width, height }] = useMeasure();
-  const [config, setConfig] = useState<Config>({
-    collide: 120,
-    linkDistance: 70,
-    chargeStrength: -100,
-  });
-  const [useTestData, setUseTestData] = useState(false);
+  const [config, setConfig] = useState<any>([
+    {
+      show: false,
+      linkDistance: 70,
+      chargeStrength: -1000000,
+      forceX: 20000,
+      rootX: -5000,
+    },
+    {
+      show: false,
+      linkDistance: 70,
+      linkStrength: 0.01,
+      tick: 300,
+    },
+    {
+      show: true,
+      linkDistance: 70,
+      linkStrength: 0.3,
+      tick: 300,
+    },
+  ]);
+
   const [nodeHover, setNodeHover] = useState<string | undefined>(undefined);
   // const [linkHover, setLinkHover] = useState<number | undefined>(undefined);
   const clearNodeHover = useCallback(() => setNodeHover(undefined), []);
@@ -40,7 +56,7 @@ export function GraphView(props: Props) {
     props.services,
     props.edges,
   ]);
-  const { nodes, links, bounds } = useLayout(rawNodes, rawLinks, config);
+  const layouts: any = useLayout(rawNodes, rawLinks, config);
 
   const { scale, onStepDown, onStepUp, ref } = useZoom({
     stepDown: s => s / 1.5,
@@ -48,6 +64,10 @@ export function GraphView(props: Props) {
     min: 0.13,
     max: 2.25,
   });
+
+  const resIndex = config.findIndex((c: any) => c.show);
+  const { nodes, edges, bounds } = layouts.length ? layouts[resIndex] : { nodes: [], edges: [], bounds: undefined };
+
   const { state: panningState, ref: panRef } = usePanning<SVGSVGElement>({
     scale,
     bounds,
@@ -85,11 +105,11 @@ export function GraphView(props: Props) {
           }}
         >
           <LinkArrowMarker />
-          {links.map((l, index) => (
+          {edges.map((e, index) => (
             <Link
               key={index}
-              link={l}
-              showStats={(l.source as NodeDatum).id === nodeHover || (l.target as NodeDatum).id === nodeHover}
+              link={e}
+              showStats={(e.source as NodeDatum).id === nodeHover || (e.target as NodeDatum).id === nodeHover}
               onClick={onEdgeOpen}
             />
           ))}
@@ -119,14 +139,7 @@ export function GraphView(props: Props) {
       </svg>
 
       <div style={{ position: 'absolute', left: 10, top: 10 }}>
-        <ViewControls<Config>
-          config={config}
-          onConfigChange={setConfig}
-          onUseTestDataChange={setUseTestData}
-          useTestData={useTestData}
-          onMinus={onStepDown}
-          onPlus={onStepUp}
-        />
+        <ViewControls<any> config={config} onConfigChange={setConfig} onMinus={onStepDown} onPlus={onStepUp} />
       </div>
 
       {openedNode && (
@@ -189,41 +202,113 @@ function GraphContextMenu({
   );
 }
 
-function useLayout(rawNodes: NodeDatum[], rawLinks: LinkDatum[], config: Config) {
-  const [nodes, setNodes] = useState<NodeDatum[]>([]);
-  const [links, setLinks] = useState<LinkDatum[]>([]);
+function useLayout(rawNodes: NodeDatum[], rawEdges: LinkDatum[], config: any[]) {
+  const [nodes, setNodes] = useState<NodeDatum[][]>([]);
+  const [edges, setEdges] = useState<LinkDatum[][]>([]);
 
   // TODO the use effect is probably not needed here right now, but may make sense later if we decide to move the layout
-  // to webworker or just postpone until other things are rendered
+  // to webworker or just postpone until other things are rendered. Also right now it memoizes this for us.
   useEffect(() => {
-    // TODO figure out a good way to put roots in fixed position on the left
-    // const roots = nodes.filter(n => n.incoming === 0);
-    // roots.forEach((n, index) => {
-    //   n.fx = -500;
-    //   n.fy = index * 200;
-    // });
+    const resultNodes: any = [];
+    const resultEdges: any = [];
 
-    const simulation = forceSimulation(rawNodes)
-      .force('collide', forceCollide(config.collide))
+    const rawNodesCopy = rawNodes.map(n => ({ ...n }));
+    const rawEdgesCopy = rawEdges.map(e => ({ ...e }));
+
+    // TODO figure out a good way to put roots in fixed position on the left
+    let roots = rawNodesCopy.filter(n => n.incoming === 0);
+    roots.forEach((n, index) => {
+      n.fx = config[0].rootX;
+      n.fy = index * 200;
+    });
+
+    const simulation = forceSimulation(rawNodesCopy)
       .force(
         'link',
-        forceLink(rawLinks)
+        forceLink(rawEdgesCopy)
           .id((d: any) => d.id)
-          .distance(config.linkDistance)
+          .distance(config[0].linkDistance)
       )
-      .force('charge', forceManyBody().strength(config.chargeStrength));
+      .force('charge', forceManyBody().strength(config[0].chargeStrength))
+      .force('x', forceX(config[0].forceX));
 
     simulation.tick(300);
+    simulation.stop();
+
+    centerNodes(rawNodesCopy);
+    resultNodes.push(rawNodesCopy);
+    resultEdges.push(rawEdgesCopy);
+
+    const rawNodesCopy2 = rawNodesCopy.map(n => ({ ...n }));
+    const rawEdgesCopy2 = rawEdges.map(e => ({ ...e }));
+
+    // roots = rawNodesCopy2.filter(n => n.incoming === 0);
+    // roots.forEach((n, index) => {
+    //   n.x = n.fx as any;
+    //   n.fx = undefined;
+    //   n.y = n.fy as any;
+    //   n.fy = undefined;
+    // });
+
+    const simulation2 = forceSimulation(rawNodesCopy2)
+      .force(
+        'link',
+        forceLink(rawEdgesCopy2)
+          // .strength(config[1].linkStrength)
+          .id((d: any) => d.id)
+          .distance(config[1].linkDistance)
+      )
+      .force('collide', forceCollide(200));
+
+    simulation2.tick(config[1].tick);
+    simulation2.stop();
+
+    centerNodes(rawNodesCopy2);
+    resultNodes.push(rawNodesCopy2);
+    resultEdges.push(rawEdgesCopy2);
+
+    const rawNodesCopy3 = rawNodesCopy2.map(n => ({ ...n }));
+    const rawEdgesCopy3 = rawEdges.map(e => ({ ...e }));
+    roots = rawNodesCopy3.filter(n => n.incoming === 0);
+    roots.forEach((n, index) => {
+      n.x = n.fx as any;
+      n.fx = undefined;
+      n.y = n.fy as any;
+      n.fy = undefined;
+    });
+
+    const simulation3 = forceSimulation(rawNodesCopy3)
+      .force(
+        'link',
+        forceLink(rawEdgesCopy3)
+          .strength((l: LinkDatum) => (roots.includes(l.source as any) ? config[2].linkStrength : 0))
+          .id((d: any) => d.id)
+          .distance(config[2].linkDistance)
+      )
+      .force('collide', forceCollide(200));
+
+    simulation3.tick(config[2].tick);
+    simulation3.stop();
     // could use a center force but that dose not work well if we want to fix some nodes somewhere (not doing that
     // right now though)
-    centerNodes(rawNodes);
-    setNodes(rawNodes);
-    setLinks(rawLinks);
-  }, [config, rawNodes, rawLinks]);
+    centerNodes(rawNodesCopy3);
+    resultNodes.push(rawNodesCopy3);
+    resultEdges.push(rawEdgesCopy3);
 
-  return { nodes, links, bounds: nodeBounds(nodes) };
+    setNodes(resultNodes);
+    setEdges(resultEdges);
+  }, [config, rawNodes, rawEdges]);
+
+  return nodes.map((n, index) => ({
+    nodes: n,
+    edges: edges[index],
+    bounds: nodeBounds(n) /* momeoize? loops over all nodes every time */,
+  }));
 }
 
+/**
+ * Transform nodes and edges dataframes into array of objects that the layout code can then work with.
+ */
 function processServices(services: DataFrame, edges: DataFrame): { nodes: NodeDatum[]; links: LinkDatum[] } {
   const servicesView = new DataFrameView<{ name: string; id: string; data: XrayService }>(services);
   const servicesMap = servicesView.toArray().reduce((acc, s, index) => {
--- packages/grafana-ui/src/components/ServiceMap/ViewControls.tsx
+++ packages/grafana-ui/src/components/ServiceMap/ViewControls.tsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import { Button } from '../Button';
 
-interface Props<Config extends Record<string, number>> {
+interface Props<Config> {
   config: Config;
   onConfigChange: (config: Config) => void;
-  useTestData: boolean;
-  onUseTestDataChange: (use: boolean) => void;
   onPlus: () => void;
   onMinus: () => void;
 }
-export function ViewControls<Config extends Record<string, number>>(props: Props<Config>) {
-  const { config, onConfigChange, useTestData, onUseTestDataChange, onPlus, onMinus } = props;
+export function ViewControls<Config extends Array<Record<string, any>>>(props: Props<Config>) {
+  const { config, onConfigChange, onPlus, onMinus } = props;
   const [showConfig, setShowConfig] = useState(false);
 
   return (
@@ -18,36 +16,45 @@ export function ViewControls<Config extends Record<string, number>>(props: Props
       <Button icon={'plus-circle'} onClick={onPlus} />
       <Button icon={'minus-circle'} onClick={onMinus} />
       <div>Or use ctrl/meta + scroll</div>
+      <Button size={'xs'} variant={'link'} onClick={() => setShowConfig(showConfig => !showConfig)}>
+        {showConfig ? 'Hide config' : 'Show config'}
+      </Button>
 
-      {false && (
-        <Button size={'xs'} variant={'link'} onClick={() => setShowConfig(showConfig => !showConfig)}>
-          {showConfig ? 'Hide config' : 'Show config'}
-        </Button>
-      )}
-
-      {showConfig && (
-        <>
-          <div>
-            Show test data
-            <input
-              type={'checkbox'}
-              checked={useTestData}
-              onChange={e => onUseTestDataChange(e.currentTarget.checked)}
-            />
-          </div>
-          {Object.keys(config).map(k => (
-            <div key={k}>
-              {k}
+      {showConfig &&
+        config.map((c, index) => (
+          <>
+            <div>
+              Show:
               <input
                 style={{ width: 50 }}
-                type={'number'}
-                value={config[k]}
-                onChange={e => onConfigChange({ ...config, [k]: parseFloat(e.target.value) })}
+                type={'checkbox'}
+                checked={c.show}
+                onChange={e => {
+                  const newConfig: any = [...config];
+                  newConfig[index] = { ...c, show: e.target.checked };
+                  onConfigChange(newConfig);
+                }}
               />
             </div>
-          ))}
-        </>
-      )}
+            {Object.keys(c)
+              .filter(k => k !== 'show')
+              .map(k => (
+                <div key={k}>
+                  {k}
+                  <input
+                    style={{ width: 50 }}
+                    type={'number'}
+                    value={c[k]}
+                    onChange={e => {
+                      const newConfig: any = [...config];
+                      newConfig[index] = { ...c, [k]: parseFloat(e.target.value) };
+                      onConfigChange(newConfig);
+                    }}
+                  />
+                </div>
+              ))}
+          </>
+        ))}
     </>
   );
 }
