5e66b86ebfc13430b8f49c478fbbfa2b04bd61de
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2021-01-08 10:59:43 +0000

    PMM-7239 Validate slack channel name (#38)

--- public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/SlackFields/SlackFields.test.tsx
+++ public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/SlackFields/SlackFields.test.tsx
@@ -4,11 +4,22 @@ import React from 'react';
 import { Form } from 'react-final-form';
 
 import { SlackFields } from './SlackFields';
+import { Messages } from '../AddNotificationChannelModal.messages';
 
 describe('SlackFields', () => {
   it('should render correct fields', () => {
     const wrapper = mount(<Form onSubmit={jest.fn()} render={() => <SlackFields />} />);
 
     expect(wrapper.find(dataQa('channel-text-input')).length).toBe(1);
+  });
+
+  it('should show error when channel has #', () => {
+    const wrapper = mount(<Form onSubmit={jest.fn()} render={() => <SlackFields />} />);
+
+    expect(wrapper.find(dataQa('channel-field-error-message')).text()).toEqual('');
+
+    wrapper.find(dataQa('channel-text-input')).simulate('change', { target: { value: '#testchannel' } });
+
+    expect(wrapper.find(dataQa('channel-field-error-message')).text()).toEqual(Messages.invalidChannelName);
   });
 });
