46c8cb4fc9f43878690f1866ebc9304bf2198233
Jack Westbrook <jack.westbrook@gmail.com>
2021-01-08 15:20:49 +0100

    feat(tablepanel): migrate old styles to config overrides

--- public/app/plugins/panel/table/migrations.test.ts
+++ public/app/plugins/panel/table/migrations.test.ts
@@ -54,4 +54,64 @@ describe('Table Migrations', () => {
     tablePanelChangedHandler(aggregationsPanel, 'table-old', toAggregations);
     expect(aggregationsPanel).toMatchSnapshot();
   });
+
+  it('migrates styles to field config overrides', () => {
+    const oldStyles = {
+      angular: {
+        columns: [],
+        styles: [
+          {
+            alias: 'Time',
+            align: 'auto',
+            dateFormat: 'YYYY-MM-DD HH:mm:ss',
+            pattern: 'Time',
+            type: 'date',
+            $$hashKey: 'object:195',
+          },
+          {
+            alias: '',
+            align: 'auto',
+            colorMode: 'cell',
+            colors: ['rgba(245, 54, 54, 0.9)', 'rgba(237, 129, 40, 0.89)', 'rgba(50, 172, 45, 0.97)'],
+            dateFormat: 'YYYY-MM-DD HH:mm:ss',
+            decimals: 2,
+            mappingType: 1,
+            pattern: 'ColorCell',
+            thresholds: ['5', '10'],
+            type: 'number',
+            unit: 'currencyUSD',
+            $$hashKey: 'object:196',
+          },
+          {
+            alias: '',
+            align: 'auto',
+            colorMode: 'value',
+            colors: ['rgba(245, 54, 54, 0.9)', 'rgba(237, 129, 40, 0.89)', 'rgba(50, 172, 45, 0.97)'],
+            dateFormat: 'YYYY-MM-DD HH:mm:ss',
+            decimals: 2,
+            mappingType: 1,
+            pattern: 'ColorValue',
+            thresholds: ['5', '10'],
+            type: 'number',
+            unit: 'Bps',
+            $$hashKey: 'object:197',
+          },
+          {
+            unit: 'short',
+            type: 'number',
+            alias: '',
+            decimals: 2,
+            colors: ['rgba(245, 54, 54, 0.9)', 'rgba(237, 129, 40, 0.89)', 'rgba(50, 172, 45, 0.97)'],
+            colorMode: null,
+            pattern: '/.*/',
+            thresholds: [],
+            align: 'right',
+          },
+        ],
+      },
+    };
+    const panel = {} as PanelModel;
+    tablePanelChangedHandler(panel, 'table-old', oldStyles);
+    expect(panel).toMatchSnapshot();
+  });
 });
--- public/app/plugins/panel/table/migrations.ts
+++ public/app/plugins/panel/table/migrations.ts
@@ -1,4 +1,4 @@
-import { PanelModel } from '@grafana/data';
+import { PanelModel, FieldMatcherID, ConfigOverrideRule } from '@grafana/data';
 import { ReduceTransformerOptions } from '@grafana/data/src/transformations/transformers/reduce';
 import { Options } from './types';
 
@@ -32,6 +32,12 @@ const columnsMap = {
   count: 'count',
 };
 
+const colorModeMap = {
+  cell: 'color-background',
+  row: 'color-background',
+  value: 'color-text',
+};
+
 const migrateTransformations = (panel: PanelModel<Partial<Options>> | any, oldOpts: any) => {
   const transformations = panel.transformations ?? [];
   if (Object.keys(transformsMap).includes(oldOpts.transform)) {
@@ -50,6 +56,67 @@ const migrateTransformations = (panel: PanelModel<Partial<Options>> | any, oldOp
   return transformations;
 };
 
+const migrateTableStyleToOverride = (style: any) => {
+  const fieldMatcherId = /^\/.*\/$/.test(style.pattern) ? FieldMatcherID.byRegexp : FieldMatcherID.byName;
+  const override: ConfigOverrideRule = {
+    matcher: {
+      id: fieldMatcherId,
+      options: style.pattern,
+    },
+    properties: [],
+  };
+
+  if (style.alias) {
+    override.properties.push({
+      id: 'displayName',
+      value: style.alias,
+    });
+  }
+
+  if (style.unit) {
+    override.properties.push({
+      id: 'unit',
+      value: style.unit,
+    });
+  }
+
+  if (style.decimals) {
+    override.properties.push({
+      id: 'decimals',
+      value: style.decimals,
+    });
+  }
+
+  if (style.type === 'date') {
+    override.properties.push({
+      id: 'unit',
+      value: `time: ${style.dateFormat}`,
+    });
+  }
+
+  if (style.colorMode) {
+    override.properties.push({
+      id: 'custom.displayMode',
+      value: colorModeMap[style.colorMode],
+    });
+  }
+
+  if (style.thresholds?.length) {
+    override.properties.push({
+      id: 'thresholds',
+      value: {
+        mode: 'absolute',
+        steps: [-Infinity, ...style.thresholds].map((threshold, idx) => ({
+          color: style.colors[idx],
+          value: parseInt(threshold, 10),
+        })),
+      },
+    });
+  }
+
+  return override;
+};
+
 /**
  * This is called when the panel changes from another panel
  */
@@ -62,8 +129,11 @@ export const tablePanelChangedHandler = (
   if (prevPluginId === 'table-old' && prevOptions.angular) {
     const oldOpts = prevOptions.angular;
     const transformations = migrateTransformations(panel, oldOpts);
-
+    const overrides = oldOpts.styles.filter((style: any) => style.pattern !== '/.*/').map(migrateTableStyleToOverride);
     panel.transformations = transformations;
+    panel.fieldConfig = {
+      overrides,
+    };
   }
 
   return {};
