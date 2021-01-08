8ad67848e0f17f0fb8d2b8369b6e5f9f69a15ab4
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2021-01-08 10:59:43 +0000

    PMM-7239 Validate slack channel name (#38)

--- public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/SlackFields/SlackFields.tsx
+++ public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/SlackFields/SlackFields.tsx
@@ -2,8 +2,13 @@ import React, { FC } from 'react';
 import { TextInputField, validators } from '@percona/platform-core';
 import { Messages } from '../AddNotificationChannelModal.messages';
 
+const channelNameValidator = (value: string) =>
+  value && value.includes('#') ? Messages.invalidChannelName : undefined;
+
 export const SlackFields: FC = () => {
   const { required } = validators;
 
-  return <TextInputField name="channel" label={Messages.fields.channel} validators={[required]} />;
+  return (
+    <TextInputField name="channel" label={Messages.fields.channel} validators={[required, channelNameValidator]} />
+  );
 };
