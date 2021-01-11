524e123e10a724cc6ae5b286f37e89a62a14c461
Ryan McKinley <ryantxu@gmail.com>
2021-01-10 20:19:48 -0800

    remove unused matchers

--- packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
+++ packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
@@ -5,24 +5,11 @@ import { fieldNameByRegexMatcherItem } from './FieldNameByRegexMatcherEditor';
 import { fieldTypeMatcherItem } from './FieldTypeMatcherEditor';
 import { fieldsByFrameRefIdItem } from './FieldsByFrameRefIdMatcher';
 import { fieldNamesMatcherItem } from './FieldNamesMatcherEditor';
-import { firstFieldsMatcherItem } from './FirstFieldMatcherEditor';
-import { numericFieldsMatcherItem } from './NumericFieldMatcherEditor';
 
-const matchers = [
+export const fieldMatchersUI = new Registry<FieldMatcherUIRegistryItem<any>>(() => [
   fieldNameMatcherItem,
   fieldNameByRegexMatcherItem,
   fieldTypeMatcherItem,
   fieldsByFrameRefIdItem,
   fieldNamesMatcherItem,
-];
-
-export const fieldMatchersUI = new Registry<FieldMatcherUIRegistryItem<any>>(() => matchers);
-
-/**
- * Use for dimension mapping
- *
- * @alpha
- */
-export const fieldDimensionMatchersUI = new Registry<FieldMatcherUIRegistryItem<any>>(() => {
-  return [firstFieldsMatcherItem, numericFieldsMatcherItem, ...matchers];
-});
+]);
