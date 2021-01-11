0afe79d5f1c856239a854320d88deeb4761a4142
Joan López de la Franca Beltran <joanjan14@gmail.com>
2021-01-11 18:13:34 +0100

    Add middleware to check for permission on accessing app includes

---
+++ pkg/middleware/plugins.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/plugins"
+	"github.com/grafana/grafana/pkg/setting"
+)
+
+func ReqAppRoles(c *models.ReqContext) {
+	pluginID := c.Params("id")
+
+	plugin, exists := plugins.Plugins[pluginID]
+	if !exists {
+		return
+	}
+
+	if len(plugin.Includes) == 0 {
+		return
+	}
+
+	ok := false
+	for _, include := range plugin.Includes {
+		if !c.OrgRole.Includes(include.Role) {
+			continue
+		}
+
+		if strings.Contains(c.Req.RequestURI, include.Path) {
+			ok = true
+			break
+		}
+	}
+
+	if !ok {
+		c.Redirect(setting.AppSubUrl + "/")
+	}
+}
