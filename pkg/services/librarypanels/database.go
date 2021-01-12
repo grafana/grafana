4b9c27b6036ad2f3f537781ee737573b67d0b3df
Hugo Häggmark <hugo.haggmark@grafana.com>
2021-01-12 09:26:00 +0100

    LibraryPanels: adds connections

--- pkg/services/librarypanels/database.go
+++ pkg/services/librarypanels/database.go
@@ -40,6 +40,32 @@ func (lps *LibraryPanelService) createLibraryPanel(c *models.ReqContext, cmd cre
 	return libraryPanel, err
 }
 
+// connectDashboard adds a connection between a Library Panel and a Dashboard.
+func (lps *LibraryPanelService) connectDashboard(c *models.ReqContext, uid string, dashboardId int64) error {
+	err := lps.SQLStore.WithTransactionalDbSession(context.Background(), func(session *sqlstore.DBSession) error {
+		panel, err := getLibraryPanel(session, uid, c.SignedInUser.OrgId)
+		if err != nil {
+			return err
+		}
+
+		libraryPanelDashboard := LibraryPanelDashboard{
+			DashboardID:    dashboardId,
+			LibraryPanelID: panel.ID,
+			Created:        time.Now(),
+			CreatedBy:      c.SignedInUser.UserId,
+		}
+		if _, err := session.Insert(&libraryPanelDashboard); err != nil {
+			if lps.SQLStore.Dialect.IsUniqueConstraintViolation(err) {
+				return nil
+			}
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
 // deleteLibraryPanel deletes a Library Panel.
 func (lps *LibraryPanelService) deleteLibraryPanel(c *models.ReqContext, uid string) error {
 	orgID := c.SignedInUser.OrgId
@@ -53,6 +79,29 @@ func (lps *LibraryPanelService) deleteLibraryPanel(c *models.ReqContext, uid str
 			return err
 		} else if rowsAffected != 1 {
 			return errLibraryPanelNotFound
+		}
+
+		return nil
+	})
+}
+
+// disconnectDashboard deletes a connection between a Library Panel and a Dashboard.
+func (lps *LibraryPanelService) disconnectDashboard(c *models.ReqContext, uid string, dashboardId int64) error {
+	return lps.SQLStore.WithTransactionalDbSession(context.Background(), func(session *sqlstore.DBSession) error {
+		panel, err := getLibraryPanel(session, uid, c.SignedInUser.OrgId)
+		if err != nil {
+			return err
+		}
+
+		result, err := session.Exec("DELETE FROM library_panel_dashboard WHERE librarypanel_id=? and dashboard_id=?", panel.ID, dashboardId)
+		if err != nil {
+			return err
+		}
+
+		if rowsAffected, err := result.RowsAffected(); err != nil {
+			return err
+		} else if rowsAffected != 1 {
+			return errLibraryPanelDashboardNotFound
 		}
 
 		return nil
@@ -103,6 +152,33 @@ func (lps *LibraryPanelService) getAllLibraryPanels(c *models.ReqContext) ([]Lib
 	})
 
 	return libraryPanels, err
+}
+
+// getConnectedDashboards gets all dashboards connected to a Library Panel.
+func (lps *LibraryPanelService) getConnectedDashboards(c *models.ReqContext, uid string) ([]int64, error) {
+	connectedDashboardIds := make([]int64, 0)
+	err := lps.SQLStore.WithDbSession(context.Background(), func(session *sqlstore.DBSession) error {
+		panel, err := getLibraryPanel(session, uid, c.SignedInUser.OrgId)
+		if err != nil {
+			return err
+		}
+
+		libraryPanelDashboards := make([]LibraryPanelDashboard, 0)
+		session.Table("library_panel_dashboard")
+		session.Where("librarypanel_id=?", panel.ID)
+		err = session.Find(&libraryPanelDashboards)
+		if err != nil {
+			return err
+		}
+
+		for _, lpd := range libraryPanelDashboards {
+			connectedDashboardIds = append(connectedDashboardIds, lpd.DashboardID)
+		}
+
+		return nil
+	})
+
+	return connectedDashboardIds, err
 }
 
 // patchLibraryPanel updates a Library Panel.
