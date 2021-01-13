c0c0a9503a5937388c784569c85cdc0a507ec9c1
Clarity-89 <homes89@ukr.net>
2021-01-13 16:21:30 +0200

    Grafana-UI: Add story/docs for Counter

--- packages/grafana-ui/src/components/Tabs/Tabs.story.tsx
+++ packages/grafana-ui/src/components/Tabs/Tabs.story.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Story } from '@storybook/react';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { UseState } from '../../utils/storybook/UseState';
-import { TabsBar, Tab, TabContent } from '@grafana/ui';
+import { TabsBar, Tab, TabContent, Counter } from '@grafana/ui';
+import mdx from './TabsBar.mdx';
+import { CounterProps } from './Counter';
 
 export default {
   title: 'Layout/Tabs',
   decorators: [withCenteredStory],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
 const tabs = [
@@ -43,4 +51,12 @@ export const Simple = () => {
       }}
     </UseState>
   );
+};
+
+export const TabCounter: Story<CounterProps> = args => {
+  return <Counter {...args} />;
+};
+
+TabCounter.args = {
+  value: 10,
 };
