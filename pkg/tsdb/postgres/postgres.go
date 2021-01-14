70a08e43494e376b11cc1a95aecdbfb7fb6bb171
ying-jeanne <wangyingensea@hotmail.com>
2021-01-14 11:53:51 +0100

    postgres SSL certification

--- pkg/tsdb/postgres/postgres.go
+++ pkg/tsdb/postgres/postgres.go
@@ -3,6 +3,9 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -20,13 +23,18 @@ func init() {
 	tsdb.RegisterTsdbQueryEndpoint("postgres", newPostgresQueryEndpoint)
 }
 
-func newPostgresQueryEndpoint(datasource *models.DataSource) (tsdb.TsdbQueryEndpoint, error) {
-	logger := log.New("tsdb.postgres")
-	logger.Debug("Creating Postgres query endpoint")
+func cleanUpFiles(config sslAuthenticationConfig) {
+	if config.tmpFilesPath != "" {
+		if err := os.RemoveAll(config.tmpFilesPath); err != nil {
+			log.Warnf("failed to delete temporary files %v: %v", config.tmpFilesPath, err)
+		}
+	}
+}
 
-	cnnstr, err := generateConnectionString(datasource, logger)
+func databaseConnection(datasource *models.DataSource, logger log.Logger) (tsdb.TsdbQueryEndpoint, sslAuthenticationConfig, error) {
+	cnnstr, sslCertificationCfg, err := generateConnectionString(datasource, logger)
 	if err != nil {
-		return nil, err
+		return nil, sslCertificationCfg, err
 	}
 
 	if setting.Env == setting.Dev {
@@ -49,10 +57,18 @@ func newPostgresQueryEndpoint(datasource *models.DataSource) (tsdb.TsdbQueryEndp
 	endpoint, err := sqleng.NewSqlQueryEndpoint(&config, &queryResultTransformer, newPostgresMacroEngine(timescaledb), logger)
 	if err != nil {
 		logger.Debug("Failed connecting to Postgres", "err", err)
-		return nil, err
+		return nil, sslCertificationCfg, err
 	}
 
 	logger.Debug("Successfully connected to Postgres")
+	return endpoint, sslCertificationCfg, err
+}
+
+func newPostgresQueryEndpoint(datasource *models.DataSource) (tsdb.TsdbQueryEndpoint, error) {
+	logger := log.New("tsdb.postgres")
+	logger.Debug("Creating Postgres query endpoint")
+	endpoint, sslCertificationCfg, err := databaseConnection(datasource, logger)
+	defer cleanUpFiles(sslCertificationCfg)
 	return endpoint, err
 }
 
@@ -61,8 +77,61 @@ func escape(input string) string {
 	return strings.ReplaceAll(strings.ReplaceAll(input, `\`, `\\`), "'", `\'`)
 }
 
-func generateConnectionString(datasource *models.DataSource, logger log.Logger) (string, error) {
-	sslMode := strings.TrimSpace(strings.ToLower(datasource.JsonData.Get("sslmode").MustString("verify-full")))
+type sslAuthenticationConfig struct {
+	sslMode         string
+	sslRootCertFile string
+	sslCertFile     string
+	sslKeyFile      string
+	tmpFilesPath    string
+}
+
+func writeConnectionFiles(ds *models.DataSource, logger log.Logger) (sslAuthenticationConfig, error) {
+	sslMode := strings.TrimSpace(strings.ToLower(ds.JsonData.Get("sslmode").MustString("verify-full")))
+	config := sslAuthenticationConfig{}
+	config.sslMode = sslMode
+	if sslMode != "disable" {
+		decrypted := ds.SecureJsonData.Decrypt()
+		currentPath, err := os.Getwd()
+		if err != nil {
+			return config, err
+		}
+		currentPath, err = ioutil.TempDir(currentPath, "tmpSSLCerts")
+		if err != nil {
+			return config, err
+		}
+		config.tmpFilesPath = currentPath
+
+		if len(decrypted["tlsClientCert"]) > 0 {
+			clientCertification := filepath.Join(currentPath, "client.crt")
+			if err = ioutil.WriteFile(clientCertification, []byte(decrypted["tlsCACert"]), 0600); err != nil {
+				return config, err
+			}
+			config.sslCertFile = clientCertification
+		}
+		if len(decrypted["tlsClientKey"]) > 0 {
+			clientKey := filepath.Join(currentPath, "client.key")
+			if err = ioutil.WriteFile(clientKey, []byte(decrypted["tlsClientKey"]), 0600); err != nil {
+				return config, err
+			}
+			config.sslKeyFile = clientKey
+		}
+		if len(decrypted["tlsCACert"]) > 0 {
+			caCert := filepath.Join(currentPath, "ca.crt")
+			if err = ioutil.WriteFile(caCert, []byte(decrypted["tlsCACert"]), 0600); err != nil {
+				return config, err
+			}
+			config.sslRootCertFile = caCert
+		}
+	}
+	return config, nil
+}
+
+func generateConnectionString(datasource *models.DataSource, logger log.Logger) (string, sslAuthenticationConfig, error) {
+	sslCertificationCfg, err := writeConnectionFiles(datasource, logger)
+	if err != nil {
+		return "", sslCertificationCfg, err
+	}
+	sslMode := sslCertificationCfg.sslMode
 	isSSLDisabled := sslMode == "disable"
 
 	var host string
@@ -77,7 +146,7 @@ func generateConnectionString(datasource *models.DataSource, logger log.Logger) 
 			var err error
 			port, err = strconv.Atoi(sp[1])
 			if err != nil {
-				return "", errutil.Wrapf(err, "invalid port in host specifier %q", sp[1])
+				return "", sslCertificationCfg, errutil.Wrapf(err, "invalid port in host specifier %q", sp[1])
 			}
 
 			logger.Debug("Generating connection string with network host/port pair", "host", host, "port", port)
@@ -97,25 +166,37 @@ func generateConnectionString(datasource *models.DataSource, logger log.Logger) 
 	} else {
 		logger.Debug("Postgres SSL is enabled", "sslMode", sslMode)
 
+		// Manage the backward compatibility for certification settings
+		sslRootCert := sslCertificationCfg.sslRootCertFile
+		if sslRootCert == "" {
+			sslRootCert = datasource.JsonData.Get("sslRootCertFile").MustString("")
+		}
+		sslCert := sslCertificationCfg.sslCertFile
+		if sslCert == "" {
+			sslCert = datasource.JsonData.Get("sslCertFile").MustString("")
+		}
+		sslKey := sslCertificationCfg.sslKeyFile
+		if sslKey == "" {
+			sslKey = datasource.JsonData.Get("sslKeyFile").MustString("")
+		}
+
 		// Attach root certificate if provided
-		if sslRootCert := datasource.JsonData.Get("sslRootCertFile").MustString(""); sslRootCert != "" {
+		if sslRootCert != "" {
 			logger.Debug("Setting server root certificate", "sslRootCert", sslRootCert)
 			connStr += fmt.Sprintf(" sslrootcert='%s'", sslRootCert)
 		}
 
 		// Attach client certificate and key if both are provided
-		sslCert := datasource.JsonData.Get("sslCertFile").MustString("")
-		sslKey := datasource.JsonData.Get("sslKeyFile").MustString("")
 		if sslCert != "" && sslKey != "" {
 			logger.Debug("Setting SSL client auth", "sslCert", sslCert, "sslKey", sslKey)
 			connStr += fmt.Sprintf(" sslcert='%s' sslkey='%s'", sslCert, sslKey)
 		} else if sslCert != "" || sslKey != "" {
-			return "", fmt.Errorf("SSL client certificate and key must both be specified")
+			return "", sslCertificationCfg, fmt.Errorf("SSL client certificate and key must both be specified")
 		}
 	}
 
 	logger.Debug("Generated Postgres connection string successfully")
-	return connStr, nil
+	return connStr, sslCertificationCfg, nil
 }
 
 type postgresQueryResultTransformer struct {
