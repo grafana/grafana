4554935f4787ca98c39058a359b3f294b648425d
Sriram S <yesoreyeram@gmail.com>
2021-01-15 05:05:39 +0000

    Revert "Different rendering options for table modal added"
    This reverts commit 74bf4ee0c1b387d118a646ddb1f5ec95590d64f1.

--- packages/grafana-ui/src/components/Table/ModalViewCell.tsx
+++ packages/grafana-ui/src/components/Table/ModalViewCell.tsx
@@ -2,65 +2,28 @@ import React, { PureComponent } from 'react';
 import { Modal } from './../Modal/Modal';
 import { TableCellProps } from './types';
 import { Button } from '../Button';
-import { JSONFormatter } from '../JSONFormatter/JSONFormatter';
-
-const getJsonObjectFromString = (input: any): any => {
-  let output: any = JSON.stringify(input || '');
-  try {
-    output = JSON.parse(input);
-  } catch {} // ignore errors
-  return output;
-};
-
-type ModalViewCellDisplayMode = 'html' | 'json' | 'plain_text';
-
-interface ModalViewCellProps extends TableCellProps {
-  displayMode: ModalViewCellDisplayMode;
-  modalTitle: string;
-  modalContent: string;
-}
 
 interface State {
-  showCellModal: boolean;
+  showHelpModal: boolean;
 }
-
-export class ModalViewCell extends PureComponent<ModalViewCellProps, State> {
+export class ModalViewCell extends PureComponent<TableCellProps, State> {
   state = {
-    showCellModal: false,
+    showHelpModal: false,
   };
-  showCellModal(show: boolean) {
-    this.setState({ showCellModal: show });
+  showHelpModal(show: boolean) {
+    this.setState({ showHelpModal: show });
   }
   render() {
     const { cell } = this.props;
-    const displayMode: ModalViewCellDisplayMode = this.props.displayMode || 'plain_text';
-    const modalTitle = this.props.modalTitle || 'Show';
-    const modalContent = this.props.modalContent || cell.value || '';
-    let displayContent: any;
-    switch (displayMode) {
-      case 'html':
-        displayContent = <div dangerouslySetInnerHTML={{ __html: modalContent }}></div>;
-        break;
-      case 'json':
-        displayContent = <JSONFormatter json={getJsonObjectFromString(cell.value)} open={4} />;
-        break;
-      case 'plain_text':
-      default:
-        displayContent = <div>{cell.value}</div>;
-    }
     return (
       <div>
         <div>
-          <Button className="btn-small" variant="secondary" onClick={() => this.showCellModal(true)}>
-            {modalTitle}
+          <Button className="btn-small" variant="secondary" onClick={() => this.showHelpModal(true)}>
+            Show
           </Button>
         </div>
-        <Modal
-          title={modalTitle || 'Details'}
-          isOpen={this.state.showCellModal}
-          onDismiss={() => this.showCellModal(false)}
-        >
-          {displayContent}
+        <Modal title="More details" isOpen={this.state.showHelpModal} onDismiss={() => this.showHelpModal(false)}>
+          <div>{cell.value}</div>
         </Modal>
       </div>
     );
