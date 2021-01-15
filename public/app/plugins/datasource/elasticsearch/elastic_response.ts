d3ef7882af986dc0c50351e6ad4c6061b6739455
Chris Cowan <chris@chriscowan.us>
2021-01-15 11:12:24 -0700

    Adding code comments

--- public/app/plugins/datasource/elasticsearch/elastic_response.ts
+++ public/app/plugins/datasource/elasticsearch/elastic_response.ts
@@ -465,23 +465,30 @@ export class ElasticResponse {
               // log level. We may rewrite some actual data in the level field if they are different.
               doc['level'] = doc[logLevelField];
             }
+            // When highlighting exists, we need to collect all the highlighted phrases and add them to the DataFrame's
+            // meta.searchWords array.
             if (doc.highlight) {
+              // There might be multiple words so we need two versions of the regular expression. One to match gobally,
+              // when used with part.match, it returns and array of matches. The second one is used to capture the
+              // values between the tags.
               const globalHighlightWordRegex = new RegExp(HIGHLIGHT_TAGS_EXP, 'g');
               const highlightWordRegex = new RegExp(HIGHLIGHT_TAGS_EXP);
               const newSearchWords = Object.keys(doc.highlight)
                 .flatMap(key => {
                   return doc.highlight[key].flatMap((line: string) => {
-                    const matchedWords = line.match(globalHighlightWordRegex);
-                    if (!matchedWords) {
+                    const matchedPhrases = line.match(globalHighlightWordRegex);
+                    if (!matchedPhrases) {
                       return [];
                     }
-                    return matchedWords.map(part => {
+                    return matchedPhrases.map(part => {
                       const matches = part.match(highlightWordRegex);
                       return (matches && matches[1]) || null;
                     });
                   });
                 })
                 .filter(_.identity);
+              // If meta and searchWords already exists, add the words and deduplicate
+              // otherwise create a new set of search words.
               const searchWords = series.meta?.searchWords
                 ? _.uniq([...series.meta.searchWords, ...newSearchWords])
                 : [...newSearchWords];
