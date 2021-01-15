1d5dc5ae1e870d5f64035e50129545bedd3063e2
Dominik Prokop <dominik.prokop@grafana.com>
2021-01-15 13:30:47 +0100

    GraphNG: remove y-axis position control from series color picker in the legend

--- packages/grafana-ui/src/components/VizLegend/VizLegendTableItem.tsx
+++ packages/grafana-ui/src/components/VizLegend/VizLegendTableItem.tsx
@@ -35,7 +35,6 @@ export const LegendTableItem: React.FunctionComponent<Props> = ({
                 onSeriesColorChange(item.label, color);
               }
             }}
-            yAxis={item.yAxis}
           />
           <div
             onClick={event => {
