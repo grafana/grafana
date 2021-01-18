bedd662c0727fbfd02578f84852f5afdfeb4ba19
Dominik Prokop <dominik.prokop@grafana.com>
2021-01-18 12:53:22 +0100

    Panel options UI: Allow collapsible categories (#30301)

--- packages/grafana-data/src/types/options.ts
+++ packages/grafana-data/src/types/options.ts
@@ -39,7 +39,7 @@ export interface OptionEditorConfig<TOptions, TSettings = any, TValue = any> {
   /**
    * Array of strings representing category of the option. First element in the array will make option render as collapsible section.
    */
-  category?: string[];
+  category?: Array<string | undefined>;
 
   /**
    * Set this value if undefined
--- public/app/features/dashboard/components/PanelEditor/DefaultFieldConfigEditor.test.tsx
+++ public/app/features/dashboard/components/PanelEditor/DefaultFieldConfigEditor.test.tsx
@@ -86,4 +86,178 @@ describe('DefaultFieldConfigEditor', () => {
     const editors = queryAllByLabelText(selectors.components.PanelEditor.FieldOptions.propertyEditor('Custom'));
     expect(editors).toHaveLength(2);
   });
+
+  describe('categories', () => {
+    it('should render uncategorized options under panel category', () => {
+      const plugin = new PanelPlugin(() => null).useFieldConfig({
+        standardOptions: {},
+        useCustomConfig: b => {
+          b.addBooleanSwitch({
+            name: 'a',
+            path: 'a',
+          } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'c',
+              path: 'c',
+            } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addTextInput({
+              name: 'b',
+              path: 'b',
+            } as FieldConfigEditorConfig<FakeFieldOptions>);
+        },
+      });
+      plugin.meta.name = 'Test plugin';
+
+      const { queryAllByLabelText } = render(
+        <DefaultFieldConfigEditor data={[]} onChange={jest.fn()} plugin={plugin} config={fieldConfigMock} />
+      );
+
+      expect(
+        queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${plugin.meta.name} options/0`))
+      ).toHaveLength(1);
+
+      expect(queryAllByLabelText(selectors.components.OptionsGroup.toggle(), { exact: false })).toHaveLength(1);
+    });
+
+    it('should render categorized options under custom category', () => {
+      const CATEGORY_NAME = 'Cat1';
+      const plugin = new PanelPlugin(() => null).useFieldConfig({
+        standardOptions: {},
+        useCustomConfig: b => {
+          b.addTextInput({
+            name: 'b',
+            path: 'b',
+          } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'a',
+              path: 'a',
+              category: [CATEGORY_NAME],
+            } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'c',
+              path: 'c',
+              category: [CATEGORY_NAME],
+            } as FieldConfigEditorConfig<FakeFieldOptions>);
+        },
+      });
+      plugin.meta.name = 'Test plugin';
+
+      const { queryAllByLabelText } = render(
+        <DefaultFieldConfigEditor data={[]} onChange={jest.fn()} plugin={plugin} config={fieldConfigMock} />
+      );
+
+      expect(
+        queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${plugin.meta.name} options/0`))
+      ).toHaveLength(1);
+
+      expect(queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${CATEGORY_NAME}/1`))).toHaveLength(1);
+
+      expect(queryAllByLabelText(selectors.components.OptionsGroup.toggle(), { exact: false })).toHaveLength(2);
+    });
+
+    it('should allow subcategories in panel category', () => {
+      const SUBCATEGORY_NAME = 'Sub1';
+      const plugin = new PanelPlugin(() => null).useFieldConfig({
+        standardOptions: {},
+        useCustomConfig: b => {
+          b.addTextInput({
+            name: 'b',
+            path: 'b',
+            category: [undefined, SUBCATEGORY_NAME],
+          } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'a',
+              path: 'a',
+            } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'c',
+              path: 'c',
+            } as FieldConfigEditorConfig<FakeFieldOptions>);
+        },
+      });
+      plugin.meta.name = 'Test plugin';
+
+      const { queryAllByLabelText, queryAllByText } = render(
+        <DefaultFieldConfigEditor data={[]} onChange={jest.fn()} plugin={plugin} config={fieldConfigMock} />
+      );
+
+      expect(
+        queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${plugin.meta.name} options/0`))
+      ).toHaveLength(1);
+
+      expect(queryAllByText(SUBCATEGORY_NAME, { exact: false })).toHaveLength(1);
+    });
+
+    it('should allow subcategories in custom category', () => {
+      const CATEGORY_NAME = 'Cat1';
+      const SUBCATEGORY_NAME = 'Sub1';
+      const plugin = new PanelPlugin(() => null).useFieldConfig({
+        standardOptions: {},
+        useCustomConfig: b => {
+          b.addBooleanSwitch({
+            name: 'a',
+            path: 'a',
+          } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'c',
+              path: 'c',
+            } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addTextInput({
+              name: 'b',
+              path: 'b',
+              category: [CATEGORY_NAME, SUBCATEGORY_NAME],
+            } as FieldConfigEditorConfig<FakeFieldOptions>);
+        },
+      });
+      plugin.meta.name = 'Test plugin';
+
+      const { queryAllByLabelText, queryAllByText } = render(
+        <DefaultFieldConfigEditor data={[]} onChange={jest.fn()} plugin={plugin} config={fieldConfigMock} />
+      );
+
+      expect(
+        queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${plugin.meta.name} options/0`))
+      ).toHaveLength(1);
+      expect(queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${CATEGORY_NAME}/1`))).toHaveLength(1);
+
+      expect(queryAllByText(SUBCATEGORY_NAME, { exact: false })).toHaveLength(1);
+    });
+
+    it('should not render categories with hidden fields only', () => {
+      const CATEGORY_NAME = 'Cat1';
+      const SUBCATEGORY_NAME = 'Sub1';
+      const plugin = new PanelPlugin(() => null).useFieldConfig({
+        standardOptions: {},
+        useCustomConfig: b => {
+          b.addBooleanSwitch({
+            name: 'a',
+            path: 'a',
+          } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addBooleanSwitch({
+              name: 'c',
+              path: 'c',
+            } as FieldConfigEditorConfig<FakeFieldOptions>)
+            .addTextInput({
+              name: 'b',
+              path: 'b',
+              hideFromDefaults: true,
+              category: [CATEGORY_NAME, SUBCATEGORY_NAME],
+            } as FieldConfigEditorConfig<FakeFieldOptions>);
+        },
+      });
+      plugin.meta.name = 'Test plugin';
+
+      const { queryAllByLabelText, queryAllByText } = render(
+        <DefaultFieldConfigEditor data={[]} onChange={jest.fn()} plugin={plugin} config={fieldConfigMock} />
+      );
+
+      expect(
+        queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${plugin.meta.name} options/0`))
+      ).toHaveLength(1);
+
+      expect(queryAllByLabelText(selectors.components.OptionsGroup.toggle(`${CATEGORY_NAME}/1`))).toHaveLength(0);
+
+      expect(queryAllByText(SUBCATEGORY_NAME, { exact: false })).toHaveLength(0);
+    });
+  });
 });
