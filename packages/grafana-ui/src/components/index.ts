f004655b7d29940602aee4882980f91b9a55e2c7
Dominik Prokop <dominik.prokop@grafana.com>
2021-01-18 17:49:49 +0100

    Chore: Remove not used PanelOptionsGrid component (#30358)

--- packages/grafana-ui/src/components/index.ts
+++ packages/grafana-ui/src/components/index.ts
@@ -16,7 +16,6 @@ export { ButtonCascader } from './ButtonCascader/ButtonCascader';
 export { LoadingPlaceholder, LoadingPlaceholderProps } from './LoadingPlaceholder/LoadingPlaceholder';
 export { ColorPicker, SeriesColorPicker } from './ColorPicker/ColorPicker';
 export { SeriesColorPickerPopover, SeriesColorPickerPopoverWithTheme } from './ColorPicker/SeriesColorPickerPopover';
-export { PanelOptionsGrid } from './PanelOptionsGrid/PanelOptionsGrid';
 export { EmptySearchResult } from './EmptySearchResult/EmptySearchResult';
 export { PieChart, PieChartType } from './PieChart/PieChart';
 export { UnitPicker } from './UnitPicker/UnitPicker';
