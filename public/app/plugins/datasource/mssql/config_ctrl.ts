a3c8042048e8ebbbdf5d9fcdd3c040894ebd1bd5
Daniel Lee <dan.limerick@gmail.com>
2021-01-18 22:05:47 +0100

    mssql: support for Windows integrated security
    Adds a dropdown to the config page to choose between
    SQL Server auth and Windows auth. If the login/username
    is empty then the go-mssqldb driver will use single sign on
    (Windows integrated security) if on Windows.

--- public/app/plugins/datasource/mssql/config_ctrl.ts
+++ public/app/plugins/datasource/mssql/config_ctrl.ts
@@ -10,11 +10,26 @@ export class MssqlConfigCtrl {
   current: any;
   onPasswordReset: ReturnType<typeof createResetHandler>;
   onPasswordChange: ReturnType<typeof createChangeHandler>;
+  showUserCredentials: boolean;
 
   /** @ngInject */
   constructor($scope: any) {
     this.current.jsonData.encrypt = this.current.jsonData.encrypt || 'false';
+    this.current.jsonData.authenticationType = this.current.jsonData.authenticationType || 'SQL Server Authentication';
     this.onPasswordReset = createResetHandler(this, PasswordFieldEnum.Password);
     this.onPasswordChange = createChangeHandler(this, PasswordFieldEnum.Password);
+    this.showUserCredentials = this.current.jsonData.authenticationType !== 'Windows Authentication';
+  }
+
+  onAuthenticationTypeChange() {
+    // Workaround to reset the user value in the database and xorm ignoring empty strings. Needs to be an empty string for the Windows auth to work.
+    // This is using the fallback in https://github.com/denisenkom/go-mssqldb to use Windows Auth if login/user id is empty.
+    if (this.current.jsonData.authenticationType === 'Windows Authentication') {
+      this.current.user = ' ';
+    } else {
+      this.current.user = '';
+    }
+
+    this.showUserCredentials = this.current.jsonData.authenticationType !== 'Windows Authentication';
   }
 }
