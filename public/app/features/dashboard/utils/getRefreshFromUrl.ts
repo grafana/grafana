166c53b0f5e6e4a4622cd3669cf05a40afdb4816
Hugo Häggmark <hugo.haggmark@gmail.com>
2021-01-22 08:39:40 +0100

    Chore: adds comment

--- public/app/features/dashboard/utils/getRefreshFromUrl.ts
+++ public/app/features/dashboard/utils/getRefreshFromUrl.ts
@@ -8,6 +8,10 @@ interface Args {
   refreshIntervals?: string[];
 }
 
+// getRefreshFromUrl function returns the value from the supplied &refresh= param in url.
+// If the supplied interval is not allowed or does not exist in the refresh intervals for the dashboard then we
+// try to find the first refresh interval that matches the minRefreshInterval (min_refresh_interval in ini)
+// or just take the first interval.
 export function getRefreshFromUrl({
   params,
   currentRefresh,
