c255ae7b63672b04e53d81a24c0d288d9188a0ef
Domas <domasx2@gmail.com>
2021-01-22 18:02:05 +0200

    sourcemap support for error logging

--- pkg/api/frontend_logging.go
+++ pkg/api/frontend_logging.go
@@ -2,16 +2,55 @@ package api
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
 	"strings"
+	"sync"
 
 	"github.com/getsentry/sentry-go"
+	sourcemap "github.com/go-sourcemap/sourcemap"
+
 	"github.com/grafana/grafana/pkg/api/response"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/plugins"
+	"github.com/grafana/grafana/pkg/setting"
 	"github.com/inconshreveable/log15"
 )
 
 var frontendLogger = log.New("frontend")
+
+type sourceLocation struct {
+	file     string
+	function string
+	line     int
+	col      int
+	pluginId string
+}
+
+type sourceMapLocation struct {
+	dir      string
+	path     string
+	pluginId string
+}
+
+type sourceMap struct {
+	consumer *sourcemap.Consumer
+	pluginId string
+}
+
+// source file url to sourcemap consume
+type sourceMapCacheType struct {
+	cache map[string]*sourceMap
+	sync.Mutex
+}
+
+var sourceMapCache = sourceMapCacheType{
+	cache: make(map[string]*sourceMap),
+}
 
 type frontendSentryExceptionValue struct {
 	Value      string            `json:"value,omitempty"`
@@ -28,6 +67,106 @@ type frontendSentryEvent struct {
 	Exception *frontendSentryException `json:"exception,omitempty"`
 }
 
+func guessSourceMapLocation(sourceUrl string) (*sourceMapLocation, error) {
+	u, err := url.Parse(sourceUrl)
+	if err != nil {
+		return nil, err
+	}
+	if strings.HasPrefix(u.Path, "/public/build/") {
+		return &sourceMapLocation{
+			dir:      setting.StaticRootPath,
+			path:     path.Join("build", u.Path[len("/public/build/"):]) + ".map",
+			pluginId: "",
+		}, nil
+	} else if strings.HasPrefix(u.Path, "/public/plugins/") {
+		for _, route := range plugins.StaticRoutes {
+			pluginPrefix := path.Join("/public/plugins/", route.PluginId)
+			if strings.HasPrefix(u.Path, pluginPrefix) {
+				return &sourceMapLocation{
+					dir:      route.Directory,
+					path:     u.Path[len(pluginPrefix):] + ".map",
+					pluginId: route.PluginId,
+				}, nil
+			}
+		}
+	}
+	return nil, nil
+}
+
+func getSourceMap(sourceUrl string) (*sourceMap, error) {
+	sourceMapCache.Lock()
+	defer sourceMapCache.Unlock()
+
+	if smap, ok := sourceMapCache.cache[sourceUrl]; ok {
+		return smap, nil
+	}
+	frontendLogger.Info("getSourcemapConsumer", "url", sourceUrl)
+	sourceMapLocation, err := guessSourceMapLocation(sourceUrl)
+	if err != nil {
+		return nil, err
+	}
+	if sourceMapLocation != nil {
+		dir := http.Dir(sourceMapLocation.dir)
+		f, err := dir.Open(sourceMapLocation.path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				frontendLogger.Error("smap not exist :shrug:", "file", sourceMapLocation.path)
+				sourceMapCache.cache[sourceUrl] = nil
+				return nil, nil
+			}
+			frontendLogger.Error("failed to open sourcemap file", "file", sourceMapLocation.path)
+			return nil, err
+		}
+		defer func() {
+			if err := f.Close(); err != nil {
+				frontendLogger.Error("Failed to close file", err)
+			}
+		}()
+		b, err := ioutil.ReadAll(f)
+		if err != nil {
+			frontendLogger.Error("failed to read sourcemap file", "file", sourceMapLocation.path)
+			return nil, err
+		}
+		consumer, err := sourcemap.Parse(sourceUrl+".map", b)
+		if err != nil {
+			return nil, err
+		}
+		smap := &sourceMap{
+			consumer: consumer,
+			pluginId: sourceMapLocation.pluginId,
+		}
+		sourceMapCache.cache[sourceUrl] = smap
+		return smap, nil
+	} else {
+		sourceMapCache.cache[sourceUrl] = nil
+	}
+	return nil, nil
+}
+
+func resolveSourceLocation(url string, line int, column int) (*sourceLocation, error) {
+	smap, err := getSourceMap(url)
+	if err != nil {
+		return nil, err
+	}
+	if smap != nil {
+		file, function, line, col, ok := smap.consumer.Source(line, column)
+		if ok {
+			frontendLogger.Info("got a hit!", "url", url, "file", file)
+			if len(function) == 0 {
+				function = "?"
+			}
+			return &sourceLocation{
+				file:     file,
+				line:     line,
+				col:      col,
+				function: function,
+				pluginId: smap.pluginId,
+			}, nil
+		}
+	}
+	return nil, nil
+}
+
 func (value *frontendSentryExceptionValue) FmtMessage() string {
 	return fmt.Sprintf("%s: %s", value.Type, value.Value)
 }
@@ -35,7 +174,19 @@ func (value *frontendSentryExceptionValue) FmtMessage() string {
 func (value *frontendSentryExceptionValue) FmtStacktrace() string {
 	var stacktrace = value.FmtMessage()
 	for _, frame := range value.Stacktrace.Frames {
-		stacktrace += fmt.Sprintf("\n  at %s (%s:%v:%v)", frame.Function, frame.Filename, frame.Lineno, frame.Colno)
+		mappedLocation, err := resolveSourceLocation(frame.Filename, frame.Lineno, frame.Colno)
+		if err != nil {
+			frontendLogger.Error("Failed get sourcemap.", err)
+		}
+		if mappedLocation != nil {
+			tag := "core"
+			if len(mappedLocation.pluginId) > 0 {
+				tag = mappedLocation.pluginId
+			}
+			stacktrace += fmt.Sprintf("\n  at %s (%s|%s:%v:%v)", mappedLocation.function, tag, mappedLocation.file, mappedLocation.line, mappedLocation.col)
+		} else {
+			stacktrace += fmt.Sprintf("\n  at %s (%s:%v:%v)", frame.Function, frame.Filename, frame.Lineno, frame.Colno)
+		}
 	}
 	return stacktrace
 }
