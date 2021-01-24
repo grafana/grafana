8b55828cdf8f79c6cbcbbd0c497f223a4fdbc5c7
Torkel Ödegaard <torkel@grafana.com>
2021-01-24 19:27:22 +0100

    Progress

--- packages/grafana-ui/src/components/PageLayout/PageToolbar.story.tsx
+++ packages/grafana-ui/src/components/PageLayout/PageToolbar.story.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ToolbarButton, ToolbarButtonRow, ButtonGroup } from '@grafana/ui';
+import { ToolbarButton, VerticalGroup } from '@grafana/ui';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { PageToolbar } from './PageToolbar';
-import { DashboardStoryCanvas } from '../../utils/storybook/DashboardStoryCanvas';
+import { StoryExample } from '../../utils/storybook/StoryExample';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Layout/PageToolbar',
@@ -13,8 +14,27 @@ export default {
 
 export const Examples = () => {
   return (
-    <DashboardStoryCanvas>
-      <PageToolbar icon="bell" title=""></PageToolbar>
-    </DashboardStoryCanvas>
+    <VerticalGroup>
+      <StoryExample name="With non clickable title">
+        <PageToolbar pageIcon="bell" title="Dashboard">
+          <ToolbarButton icon="panel-add" />
+          <ToolbarButton icon="sync">Sync</ToolbarButton>
+        </PageToolbar>
+      </StoryExample>
+      <StoryExample name="With clickable title and parent">
+        <PageToolbar
+          pageIcon="bell"
+          title="A very long dashboard name"
+          parent="A long folder name"
+          onClickTitle={() => action('Title clicked')}
+          onClickParent={() => action('Parent clicked')}
+        >
+          <ToolbarButton icon="panel-add" />
+          <ToolbarButton icon="share-alt" />
+          <ToolbarButton icon="sync">Sync</ToolbarButton>
+          <ToolbarButton icon="cog">Settings </ToolbarButton>
+        </PageToolbar>
+      </StoryExample>
+    </VerticalGroup>
   );
 };
--- packages/grafana-ui/src/components/PageLayout/PageToolbar.tsx
+++ packages/grafana-ui/src/components/PageLayout/PageToolbar.tsx
@@ -2,50 +2,99 @@ import React, { FC, ReactNode } from 'react';
 import { css } from 'emotion';
 import { GrafanaTheme } from '@grafana/data';
 import { useStyles } from '../../themes/ThemeContext';
-import { HorizontalGroup } from '../Layout/Layout';
+import { IconName } from '../../types';
+import { Icon } from '../Icon/Icon';
 
 export interface Props {
+  pageIcon?: IconName;
   title: string;
-  titlePrefix?: ReactNode;
-  actions?: ReactNode[];
+  parent?: string;
+  onGoBack?: () => void;
+  onClickTitle?: () => void;
+  onClickParent?: () => void;
+  children?: ReactNode;
 }
 
 /** @alpha */
-export const PageToolbar: FC<Props> = ({ actions = [], title, titlePrefix }) => {
+export const PageToolbar: FC<Props> = React.memo((props) => {
+  const { title, parent, pageIcon, children, onClickTitle, onClickParent } = props;
   const styles = useStyles(getStyles);
 
   return (
-    <div className={styles.toolbarWrapper}>
-      <HorizontalGroup justify="space-between" align="center">
-        <div className={styles.toolbarLeft}>
-          <HorizontalGroup spacing="none">
-            {titlePrefix}
-            <span className={styles.toolbarTitle}>{title}</span>
-          </HorizontalGroup>
+    <div className={styles.toolbar}>
+      <div className={styles.toolbarLeft}>
+        <div className={styles.pageIcon}>{pageIcon && <Icon name={pageIcon} size="lg" />}</div>
+        <div className={styles.titleWrapper}>
+          {parent && onClickParent && (
+            <a onClick={props.onClickParent} className={styles.titleLink}>
+              {parent} <span className={styles.parentIcon}>/</span>
+            </a>
+          )}
+          {onClickTitle && (
+            <a onClick={props.onClickTitle} className={styles.titleLink}>
+              {title}
+            </a>
+          )}
+          {!onClickTitle && <div className={styles.titleText}>{title}</div>}
         </div>
-        <HorizontalGroup spacing="sm" align="center">
-          {actions}
-        </HorizontalGroup>
-      </HorizontalGroup>
+      </div>
+      <div className={styles.spacer}></div>
+      {React.Children.toArray(children)
+        .filter(Boolean)
+        .map((child, index) => {
+          return (
+            <div className={styles.actionWrapper} key={index}>
+              {child}
+            </div>
+          );
+        })}
     </div>
   );
-};
+});
+
+PageToolbar.displayName = 'PageToolbar';
 
 const getStyles = (theme: GrafanaTheme) => {
+  const { spacing, typography } = theme;
+
   return {
-    toolbarWrapper: css`
+    toolbar: css`
       display: flex;
-      padding: ${theme.spacing.sm};
-      background: ${theme.colors.panelBg};
-      justify-content: space-between;
-      border-bottom: 1px solid ${theme.colors.panelBorder};
+      background: ${theme.colors.dashboardBg};
+      padding: 0 ${spacing.sm} ${spacing.sm} ${spacing.sm};
+      justify-content: flex-end;
+      flex-wrap: wrap;
     `,
     toolbarLeft: css`
-      padding-left: ${theme.spacing.sm};
+      display: flex;
     `,
-    toolbarTitle: css`
-      font-size: ${theme.typography.size.lg};
-      padding-left: ${theme.spacing.sm};
+    spacer: css`
+      flex-grow: 1;
+    `,
+    pageIcon: css`
+      display: flex;
+      padding-top: ${spacing.sm};
+      align-items: center;
+    `,
+    titleWrapper: css`
+      display: flex;
+      padding-top: ${spacing.sm};
+      align-items: center;
+    `,
+    parentIcon: css`
+      margin-right: 0 4px;
+    `,
+    titleText: css`
+      font-size: ${typography.size.lg};
+      padding-left: ${spacing.sm};
+    `,
+    titleLink: css`
+      font-size: ${typography.size.lg};
+      padding-left: ${spacing.sm};
+    `,
+    actionWrapper: css`
+      padding-left: ${spacing.sm};
+      padding-top: ${spacing.sm};
     `,
   };
 };
