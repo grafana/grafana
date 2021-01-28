b6aee20d107a5882a874faacc263d8fd4ff27b2d
Torkel Ödegaard <torkel@grafana.com>
2021-01-28 08:50:31 +0100

    Minor fix

--- public/app/index.ts
+++ public/app/index.ts
@@ -1,5 +1,11 @@
 declare let __webpack_public_path__: string;
-__webpack_public_path__ = window.public_cdn_path;
+
+/**
+ * Check if we are hosting files on cdn and set webpack public path
+ */
+if ((window as any).public_cdn_path) {
+  __webpack_public_path__ = (window as any).public_cdn_path;
+}
 
 import app from './app';
 app.initEchoSrv();
