9407cdd51cf1926a46b6f6489ccd290b2c01a3e0
Torkel Ödegaard <torkel@grafana.org>
2021-01-29 15:02:01 +0100

    AlertingNG: Update UX to use new PageToolbar component (#30680)
    * AlertingNG: Update UX to use new PageToolbar component
    
    * Removed unused imports

--- public/app/features/alerting/NextGenAlertingPage.tsx
+++ public/app/features/alerting/NextGenAlertingPage.tsx
@@ -3,8 +3,7 @@ import { hot } from 'react-hot-loader';
 import { MapDispatchToProps, MapStateToProps } from 'react-redux';
 import { css } from 'emotion';
 import { GrafanaTheme, SelectableValue } from '@grafana/data';
-import { Button, Icon, stylesFactory } from '@grafana/ui';
-import { PageToolbar } from 'app/core/components/PageToolbar/PageToolbar';
+import { PageToolbar, stylesFactory, ToolbarButton } from '@grafana/ui';
 import { SplitPaneWrapper } from 'app/core/components/SplitPaneWrapper/SplitPaneWrapper';
 import { connectWithCleanUp } from 'app/core/components/connectWithCleanUp';
 import AlertingQueryEditor from './components/AlertingQueryEditor';
@@ -83,15 +82,15 @@ class NextGenAlertingPage extends PureComponent<Props, State> {
 
   renderToolbarActions() {
     return [
-      <Button variant="destructive" key="discard" onClick={this.onDiscard}>
+      <ToolbarButton variant="destructive" key="discard" onClick={this.onDiscard}>
         Discard
-      </Button>,
-      <Button variant="secondary" key="test" onClick={this.onTest}>
+      </ToolbarButton>,
+      <ToolbarButton key="test" onClick={this.onTest}>
         Test
-      </Button>,
-      <Button variant="primary" key="save" onClick={this.onSaveAlert}>
+      </ToolbarButton>,
+      <ToolbarButton variant="primary" key="save" onClick={this.onSaveAlert}>
         Save
-      </Button>,
+      </ToolbarButton>,
     ];
   }
 
@@ -108,12 +107,9 @@ class NextGenAlertingPage extends PureComponent<Props, State> {
 
     return (
       <div className={styles.wrapper}>
-        <PageToolbar
-          title="Alert editor"
-          titlePrefix={<Icon name="bell" size="lg" />}
-          actions={this.renderToolbarActions()}
-          titlePadding="sm"
-        />
+        <PageToolbar title="Alert editor" pageIcon="bell">
+          {this.renderToolbarActions()}
+        </PageToolbar>
         <div className={styles.splitPanesWrapper}>
           <SplitPaneWrapper
             leftPaneComponents={[
