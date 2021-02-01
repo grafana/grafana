8d7c8c2729ea8980c04b4b03ea0d4e652e506602
Sofia Papagiannaki <sofia@grafana.com>
2021-02-01 13:07:46 +0200

    AlertingNG: change API permissions

--- pkg/services/ngalert/middleware.go
+++ pkg/services/ngalert/middleware.go
@@ -6,15 +6,11 @@ import (
 
 func (ng *AlertNG) validateOrgAlertDefinition(c *models.ReqContext) {
 	uid := c.ParamsEscape(":alertDefinitionUID")
+
 	query := getAlertDefinitionByUIDQuery{UID: uid, OrgID: c.SignedInUser.OrgId}
 
 	if err := ng.getAlertDefinitionByUID(&query); err != nil {
 		c.JsonApiErr(404, "Alert definition not found", nil)
-		return
-	}
-
-	if c.OrgId != query.Result.OrgID {
-		c.JsonApiErr(403, "You are not allowed to edit/view alert definition", nil)
 		return
 	}
 }
