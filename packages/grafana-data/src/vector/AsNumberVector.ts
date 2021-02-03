98717b55af2cf2606506d86de16483523c614b11
Ryan McKinley <ryantxu@gmail.com>
2021-02-02 21:51:09 -0800

    Chore: add more docs annotations (#30847)
    (cherry picked from commit 3a343aa547f0c2c27a28e78640eec29839d95c80)

--- packages/grafana-data/src/vector/AsNumberVector.ts
+++ packages/grafana-data/src/vector/AsNumberVector.ts
@@ -1,6 +1,11 @@
 import { Vector } from '../types';
 import { FunctionalVector } from './FunctionalVector';
 
+/**
+ * This will force all values to be numbers
+ *
+ * @public
+ */
 export class AsNumberVector extends FunctionalVector<number> {
   constructor(private field: Vector) {
     super();
