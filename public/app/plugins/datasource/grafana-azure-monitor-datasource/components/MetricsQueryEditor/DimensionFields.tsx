20152950c56b7a7abe49df81ded0e26c6478997a
joshhunt <josh@trtr.co>
2021-02-04 18:03:20 +0000

    added proper fields, and adding and removing for DimensionFields

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/MetricsQueryEditor/DimensionFields.tsx
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/MetricsQueryEditor/DimensionFields.tsx
@@ -1,25 +1,56 @@
-import { Button, InlineField } from '@grafana/ui';
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
+import { Button, Select, Input, HorizontalGroup, VerticalGroup, InlineLabel } from '@grafana/ui';
 
-interface DimensionFieldsProps {}
+import { MultipleFields } from '../Field';
+import { findOption, MetricsQueryEditorFieldProps, Option } from '../common';
 
-const DimensionFields: React.FC<DimensionFieldsProps> = () => {
-  const [filters, setFilters] = useState<number[]>();
+interface DimensionFieldsProps extends MetricsQueryEditorFieldProps {
+  dimensionOptions: Option[];
+}
+
+const DimensionFields: React.FC<DimensionFieldsProps> = ({ query, dimensionOptions, onChange }) => {
+  const addFilter = useCallback(() => {
+    onChange('dimensionFilters', [
+      ...query.azureMonitor.dimensionFilters,
+      {
+        dimension: '',
+        operator: 'eq',
+        filter: '',
+      },
+    ]);
+  }, [query.azureMonitor.dimensionFilters]);
+
+  const removeFilter = useCallback(
+    (index) => {
+      const newFilters = [...query.azureMonitor.dimensionFilters];
+      newFilters.splice(index, 1);
+      onChange('dimensionFilters', newFilters);
+    },
+    [query.azureMonitor.dimensionFilters]
+  );
 
   return (
-    <InlineField label="Dimension" labelWidth={16}>
-      <div>
-        {filters && filters.map((filter, index) => <div key={index}>filter {filter}</div>)}
+    <MultipleFields label="Dimension" labelWidth={16}>
+      <VerticalGroup spacing="xs">
+        {query.azureMonitor.dimensionFilters.map((filter, index) => (
+          <HorizontalGroup key={index} spacing="xs">
+            <Select
+              placeholder="Field"
+              value={findOption(dimensionOptions, filter.dimension)}
+              options={dimensionOptions}
+              onChange={() => {}}
+            />
+            <InlineLabel aria-label="equals">==</InlineLabel>
+            <Input placeholder="" value={filter.filter} />
+            <Button variant="secondary" size="md" icon="trash-alt" aria-label="Remove" onClick={removeFilter}></Button>
+          </HorizontalGroup>
+        ))}
 
-        <Button
-          variant="secondary"
-          size="md"
-          onClick={() => setFilters((v) => (v ? [...v, v[v.length - 1] + 1] : [0]))}
-        >
+        <Button variant="secondary" size="md" onClick={addFilter}>
           Add new dimension
         </Button>
-      </div>
-    </InlineField>
+      </VerticalGroup>
+    </MultipleFields>
   );
 };
 
