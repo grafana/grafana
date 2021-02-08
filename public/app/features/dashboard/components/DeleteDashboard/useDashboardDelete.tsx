09b01890c3d0699b0dade54f3f3a61b82d928a24
Jack Westbrook <jack.westbrook@gmail.com>
2021-02-08 14:04:28 +0100

    refactor(usedashboarddelete): clean up

--- public/app/features/dashboard/components/DeleteDashboard/useDashboardDelete.tsx
+++ public/app/features/dashboard/components/DeleteDashboard/useDashboardDelete.tsx
@@ -1,16 +1,14 @@
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useAsyncFn } from 'react-use';
 import { AppEvents } from '@grafana/data';
 import appEvents from 'app/core/app_events';
 import { updateLocation } from 'app/core/reducers/location';
-import { StoreState } from 'app/types';
 import { deleteDashboard } from 'app/features/manage-dashboards/state/actions';
 
 export const useDashboardDelete = (uid: string) => {
-  const dashboard = useSelector((state: StoreState) => state.dashboard.getModel());
   const dispatch = useDispatch();
-  const [state, onRestoreDashboard] = useAsyncFn(async () => deleteDashboard(uid, false), []);
+  const [state, onRestoreDashboard] = useAsyncFn(() => deleteDashboard(uid, false), []);
   useEffect(() => {
     if (state.value) {
       dispatch(
@@ -20,7 +18,7 @@ export const useDashboardDelete = (uid: string) => {
           query: {},
         })
       );
-      appEvents.emit(AppEvents.alertSuccess, ['Dashboard Deleted', dashboard!.title + ' has been deleted']);
+      appEvents.emit(AppEvents.alertSuccess, ['Dashboard Deleted', state.value.title + ' has been deleted']);
     }
   }, [state]);
   return { state, onRestoreDashboard };
