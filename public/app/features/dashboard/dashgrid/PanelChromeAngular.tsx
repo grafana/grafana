7e7f90ed6cbb57cba5a1905fd4f4c4a8a654d985
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2021-02-09 06:23:22 +0100

    TextPanel: Fixes so panel title is updated when variables change (#30884) (#31005)
    * TextPanel: Fixes so panel title is updated when variables change
    
    * Tests: fixes tests
    
    * Chore: updates after PR comments
    
    (cherry picked from commit f42bb84cbf6d22727df4972170b5f0cec9499bd7)
    
    Co-authored-by: Hugo Häggmark <hugo.haggmark@grafana.com>

--- public/app/features/dashboard/dashgrid/PanelChromeAngular.tsx
+++ public/app/features/dashboard/dashgrid/PanelChromeAngular.tsx
@@ -14,7 +14,6 @@ import config from 'app/core/config';
 import { DashboardModel, PanelModel } from '../state';
 import { StoreState } from 'app/types';
 import { getDefaultTimeRange, LoadingState, PanelData, PanelPlugin } from '@grafana/data';
-import { updateLocation } from 'app/core/actions';
 import { PANEL_BORDER } from 'app/core/constants';
 import { selectors } from '@grafana/e2e-selectors';
 import { RenderEvent } from 'app/types/events';
@@ -36,7 +35,6 @@ interface ConnectedProps {
 
 interface DispatchProps {
   setPanelAngularComponent: typeof setPanelAngularComponent;
-  updateLocation: typeof updateLocation;
 }
 
 export type Props = OwnProps & ConnectedProps & DispatchProps;
@@ -214,7 +212,7 @@ export class PanelChromeAngularUnconnected extends PureComponent<Props, State> {
   }
 
   render() {
-    const { dashboard, panel, isViewing, isEditing, plugin, angularComponent, updateLocation } = this.props;
+    const { dashboard, panel, isViewing, isEditing, plugin } = this.props;
     const { errorMessage, data, alertState } = this.state;
     const { transparent } = panel;
 
@@ -239,13 +237,11 @@ export class PanelChromeAngularUnconnected extends PureComponent<Props, State> {
           dashboard={dashboard}
           title={panel.title}
           description={panel.description}
-          angularComponent={angularComponent}
           links={panel.links}
           error={errorMessage}
           isViewing={isViewing}
           isEditing={isEditing}
           data={data}
-          updateLocation={updateLocation}
           alertState={alertState}
         />
         <div className={panelContentClassNames}>
@@ -262,6 +258,6 @@ const mapStateToProps: MapStateToProps<ConnectedProps, OwnProps, StoreState> = (
   };
 };
 
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, OwnProps> = { setPanelAngularComponent, updateLocation };
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, OwnProps> = { setPanelAngularComponent };
 
 export const PanelChromeAngular = connect(mapStateToProps, mapDispatchToProps)(PanelChromeAngularUnconnected);
--- public/app/features/dashboard/dashgrid/PanelHeader/PanelHeader.tsx
+++ public/app/features/dashboard/dashgrid/PanelHeader/PanelHeader.tsx
@@ -1,234 +1,74 @@
-import React, { PureComponent } from 'react';
-import classNames from 'classnames';
-import { DataLink, LoadingState, PanelData, PanelMenuItem, QueryResultMetaNotice } from '@grafana/data';
-import { AngularComponent, config } from '@grafana/runtime';
-import { ClickOutsideWrapper, Icon, IconName, Tooltip, stylesFactory } from '@grafana/ui';
+import React, { FC } from 'react';
+import { cx } from 'emotion';
+import { DataLink, PanelData } from '@grafana/data';
+import { Icon } from '@grafana/ui';
 import { selectors } from '@grafana/e2e-selectors';
 
 import PanelHeaderCorner from './PanelHeaderCorner';
-import { PanelHeaderMenu } from './PanelHeaderMenu';
-
 import { DashboardModel } from 'app/features/dashboard/state/DashboardModel';
 import { PanelModel } from 'app/features/dashboard/state/PanelModel';
 import { getPanelLinksSupplier } from 'app/features/panel/panellinks/linkSuppliers';
-import { getPanelMenu } from 'app/features/dashboard/utils/getPanelMenu';
-import { updateLocation } from 'app/core/actions';
-import { css } from 'emotion';
+import { PanelHeaderNotices } from './PanelHeaderNotices';
+import { PanelHeaderMenuTrigger } from './PanelHeaderMenuTrigger';
+import { PanelHeaderLoadingIndicator } from './PanelHeaderLoadingIndicator';
+import { PanelHeaderMenuWrapper } from './PanelHeaderMenuWrapper';
 
 export interface Props {
   panel: PanelModel;
   dashboard: DashboardModel;
   title?: string;
   description?: string;
-  angularComponent?: AngularComponent | null;
   links?: DataLink[];
   error?: string;
   alertState?: string;
   isViewing: boolean;
   isEditing: boolean;
   data: PanelData;
-  updateLocation: typeof updateLocation;
 }
 
-interface ClickCoordinates {
-  x: number;
-  y: number;
-}
+export const PanelHeader: FC<Props> = ({ panel, error, isViewing, isEditing, data, alertState, dashboard }) => {
+  const onCancelQuery = () => panel.getQueryRunner().cancelQuery();
+  const title = panel.replaceVariables(panel.title, {}, 'text');
+  const className = cx('panel-header', !(isViewing || isEditing) ? 'grid-drag-handle' : '');
 
-interface State {
-  panelMenuOpen: boolean;
-  menuItems: PanelMenuItem[];
-}
-
-export class PanelHeader extends PureComponent<Props, State> {
-  clickCoordinates: ClickCoordinates = { x: 0, y: 0 };
-
-  state: State = {
-    panelMenuOpen: false,
-    menuItems: [],
-  };
-
-  eventToClickCoordinates = (event: React.MouseEvent<HTMLDivElement>) => {
-    return {
-      x: Math.floor(event.clientX),
-      y: Math.floor(event.clientY),
-    };
-  };
-
-  onMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-    this.clickCoordinates = this.eventToClickCoordinates(event);
-  };
-
-  isClick = (clickCoordinates: ClickCoordinates) => {
-    return clickCoordinates.x === this.clickCoordinates.x && clickCoordinates.y === this.clickCoordinates.y;
-  };
-
-  onMenuToggle = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!this.isClick(this.eventToClickCoordinates(event))) {
-      return;
-    }
-
-    event.stopPropagation();
-
-    const { dashboard, panel, angularComponent } = this.props;
-    const menuItems = getPanelMenu(dashboard, panel, angularComponent);
-
-    this.setState({
-      panelMenuOpen: !this.state.panelMenuOpen,
-      menuItems,
-    });
-  };
-
-  closeMenu = () => {
-    this.setState({
-      panelMenuOpen: false,
-    });
-  };
-
-  onCancelQuery = () => {
-    this.props.panel.getQueryRunner().cancelQuery();
-  };
-
-  renderLoadingState(state: LoadingState): JSX.Element | null {
-    if (state === LoadingState.Loading) {
-      return (
-        <div className="panel-loading" onClick={this.onCancelQuery}>
-          <Tooltip content="Cancel query">
-            <Icon className="panel-loading__spinner spin-clockwise" name="sync" />
-          </Tooltip>
-        </div>
-      );
-    }
-
-    if (state === LoadingState.Streaming) {
-      const styles = getStyles();
-
-      return (
-        <div className="panel-loading" onClick={this.onCancelQuery}>
-          <div title="Streaming (click to stop)" className={styles.streamIndicator} />
-        </div>
-      );
-    }
-
-    return null;
-  }
-
-  openInspect = (e: React.SyntheticEvent, tab: string) => {
-    const { updateLocation, panel } = this.props;
-
-    e.stopPropagation();
-
-    updateLocation({
-      query: { inspect: panel.id, inspectTab: tab },
-      partial: true,
-    });
-  };
-
-  // This will show one icon for each severity
-  renderNotice = (notice: QueryResultMetaNotice) => {
-    let iconName: IconName = 'info-circle';
-    if (notice.severity === 'error' || notice.severity === 'warning') {
-      iconName = 'exclamation-triangle';
-    }
-
-    return (
-      <Tooltip content={notice.text} key={notice.severity}>
-        {notice.inspect ? (
-          <div className="panel-info-notice pointer" onClick={(e) => this.openInspect(e, notice.inspect!)}>
-            <Icon name={iconName} style={{ marginRight: '8px' }} />
-          </div>
-        ) : (
-          <a className="panel-info-notice" href={notice.link} target="_blank" rel="noreferrer">
-            <Icon name={iconName} style={{ marginRight: '8px' }} />
-          </a>
-        )}
-      </Tooltip>
-    );
-  };
-
-  render() {
-    const { panel, error, isViewing, isEditing, data, alertState } = this.props;
-    const { menuItems } = this.state;
-    const title = panel.replaceVariables(panel.title, {}, 'text');
-
-    const panelHeaderClass = classNames({
-      'panel-header': true,
-      'grid-drag-handle': !(isViewing || isEditing),
-    });
-
-    // dedupe on severity
-    const notices: Record<string, QueryResultMetaNotice> = {};
-
-    for (const series of data.series) {
-      if (series.meta && series.meta.notices) {
-        for (const notice of series.meta.notices) {
-          notices[notice.severity] = notice;
-        }
-      }
-    }
-
-    return (
-      <>
-        {this.renderLoadingState(data.state)}
-        <div className={panelHeaderClass}>
-          <PanelHeaderCorner
-            panel={panel}
-            title={panel.title}
-            description={panel.description}
-            scopedVars={panel.scopedVars}
-            links={getPanelLinksSupplier(panel)}
-            error={error}
-          />
-          <div
-            className="panel-title-container"
-            onClick={this.onMenuToggle}
-            onMouseDown={this.onMouseDown}
-            aria-label={selectors.components.Panels.Panel.title(title)}
-          >
-            <div className="panel-title">
-              {Object.values(notices).map(this.renderNotice)}
-              {alertState && (
-                <Icon
-                  name={alertState === 'alerting' ? 'heart-break' : 'heart'}
-                  className="icon-gf panel-alert-icon"
-                  style={{ marginRight: '4px' }}
-                  size="sm"
-                />
-              )}
-              <span className="panel-title-text">{title}</span>
-              <Icon name="angle-down" className="panel-menu-toggle" />
-              {this.state.panelMenuOpen && (
-                <ClickOutsideWrapper onClick={this.closeMenu} parent={document}>
-                  <PanelHeaderMenu items={menuItems} />
-                </ClickOutsideWrapper>
-              )}
-              {data.request && data.request.timeInfo && (
-                <span className="panel-time-info">
-                  <Icon name="clock-nine" size="sm" /> {data.request.timeInfo}
-                </span>
-              )}
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
-}
-
-/*
- * Styles
- */
-export const getStyles = stylesFactory(() => {
-  return {
-    streamIndicator: css`
-      width: 10px;
-      height: 10px;
-      background: ${config.theme.colors.textFaint};
-      box-shadow: 0 0 2px ${config.theme.colors.textFaint};
-      border-radius: 50%;
-      position: relative;
-      top: 6px;
-      right: 1px;
-    `,
-  };
-});
+  return (
+    <>
+      <PanelHeaderLoadingIndicator state={data.state} onClick={onCancelQuery} />
+      <div className={className}>
+        <PanelHeaderCorner
+          panel={panel}
+          title={panel.title}
+          description={panel.description}
+          scopedVars={panel.scopedVars}
+          links={getPanelLinksSupplier(panel)}
+          error={error}
+        />
+        <PanelHeaderMenuTrigger aria-label={selectors.components.Panels.Panel.title(title)}>
+          {({ closeMenu, panelMenuOpen }) => {
+            return (
+              <div className="panel-title">
+                <PanelHeaderNotices frames={data.series} panelId={panel.id} />
+                {alertState ? (
+                  <Icon
+                    name={alertState === 'alerting' ? 'heart-break' : 'heart'}
+                    className="icon-gf panel-alert-icon"
+                    style={{ marginRight: '4px' }}
+                    size="sm"
+                  />
+                ) : null}
+                <span className="panel-title-text">{title}</span>
+                <Icon name="angle-down" className="panel-menu-toggle" />
+                <PanelHeaderMenuWrapper panel={panel} dashboard={dashboard} show={panelMenuOpen} onClose={closeMenu} />
+                {data.request && data.request.timeInfo && (
+                  <span className="panel-time-info">
+                    <Icon name="clock-nine" size="sm" /> {data.request.timeInfo}
+                  </span>
+                )}
+              </div>
+            );
+          }}
+        </PanelHeaderMenuTrigger>
+      </div>
+    </>
+  );
+};
