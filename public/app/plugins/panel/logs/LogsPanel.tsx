1323bdeb04d7d27e815d99c797da6717448f1a4e
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2021-02-09 12:11:36 +0100

    Update public/app/plugins/panel/logs/LogsPanel.tsx
    Co-authored-by: Giordano Ricci <gio.ricci@grafana.com>

--- public/app/plugins/panel/logs/LogsPanel.tsx
+++ public/app/plugins/panel/logs/LogsPanel.tsx
@@ -10,7 +10,6 @@ export const LogsPanel: React.FunctionComponent<LogsPanelProps> = ({
   data,
   timeZone,
   options: { showLabels, showTime, wrapLogMessage, sortOrder, dedupStrategy },
-  width,
 }) => {
   if (!data) {
     return (
