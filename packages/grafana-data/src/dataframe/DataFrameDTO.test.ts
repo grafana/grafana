fea26aaa9431bdd8283993798cdbf7233f2b6a66
Ryan McKinley <ryantxu@gmail.com>
2021-02-08 22:00:24 -0800

    move replacements to field

--- packages/grafana-data/src/dataframe/DataFrameDTO.test.ts
+++ packages/grafana-data/src/dataframe/DataFrameDTO.test.ts
@@ -1,63 +1,18 @@
 import { FieldType } from '../types/dataFrame';
-import { DataFrameDTO, dataFrameFromDTO, DataFrameReplacementValue, toDataFrameDTO } from './DataFrameDTO';
+import { DataFrameDTO, dataFrameFromDTO, toDataFrameDTO } from './DataFrameDTO';
 
 describe('dataFrameView', () => {
   const dto: DataFrameDTO = {
     name: 'hello',
     fields: [
       { name: 'time', type: FieldType.time, values: [100, 200, 300] },
-      { name: 'name', type: FieldType.string, values: ['a', 'b', null] },
-      { name: 'value', type: FieldType.number, values: [1, 2, null] },
-    ],
-    replace: [
-      { field: 1, value: DataFrameReplacementValue.Undefined, index: [2] },
-      { field: 2, value: DataFrameReplacementValue.NaN, index: [2] },
+      { name: 'name', type: FieldType.string, values: ['a', 'b', null], replaced: { NaN: [2] } },
+      { name: 'value', type: FieldType.number, values: [1, 2, null], replaced: { Inf: [2] } },
     ],
   };
   const frame = dataFrameFromDTO(dto);
 
   it('back to DTO', () => {
-    expect(toDataFrameDTO(frame)).toMatchInlineSnapshot(`
-      Object {
-        "fields": Array [
-          Object {
-            "config": Object {},
-            "labels": undefined,
-            "name": "time",
-            "type": "time",
-            "values": Array [
-              100,
-              200,
-              300,
-            ],
-          },
-          Object {
-            "config": Object {},
-            "labels": undefined,
-            "name": "name",
-            "type": "string",
-            "values": Array [
-              "a",
-              "b",
-              undefined,
-            ],
-          },
-          Object {
-            "config": Object {},
-            "labels": undefined,
-            "name": "value",
-            "type": "number",
-            "values": Array [
-              1,
-              2,
-              NaN,
-            ],
-          },
-        ],
-        "meta": undefined,
-        "name": undefined,
-        "refId": undefined,
-      }
-    `);
+    expect(toDataFrameDTO(frame)).toMatchInlineSnapshot();
   });
 });
--- packages/grafana-data/src/dataframe/DataFrameDTO.ts
+++ packages/grafana-data/src/dataframe/DataFrameDTO.ts
@@ -4,37 +4,40 @@ import { vectorToArray } from '../vector/vectorToArray';
 import { guessFieldTypeFromNameAndValue } from './processDataFrame';
 
 /**
- * JSON does not support these value types
+ * Since JSON does not support encoding some numeric types, we need to replace them on load.
+ * While some system solve this by encoding everythign as strings, this approach lets is
+ * efficiently communicate which values needed special encoding.
  *
  * @alpha
  */
-export enum DataFrameReplacementValue {
-  NaN = 'NaN',
-  Undefined = 'Undef',
-  Infinity = 'Inf',
-  NegInf = 'NegInf',
+export interface DataFrameReplacements {
+  NaN?: number[];
+  Undef?: number[]; // Missing because of absense or join
+  Inf?: number[];
+  NegInf?: number[];
 }
 
 /**
+ * Field object passed over JSON
+ *
  * @alpha
- */
-export interface DataFrameReplacement {
-  value: DataFrameReplacementValue;
-  field: number;
-  index: number[];
-}
-
-/**
- * Like a field, but properties are optional and values may be a simple array
  */
 export interface FieldDTO {
   name: string; // The column name
   type?: FieldType;
   config?: FieldConfig;
   labels?: Labels;
-  values: any[]; // Converted to vector in real DataFrame
+  values: any[]; // Converted to a vector
+
+  // These values were replaced with nulls while encoding and need to be converted on load
+  replaced?: DataFrameReplacements;
 }
 
+/**
+ * The JSON transfer object for DataFrames.  Values are stored in simple JSON
+ *
+ * @alpha
+ */
 export interface DataFrameDTO {
   /**
    * Matches the query target refId
@@ -55,57 +58,50 @@ export interface DataFrameDTO {
    * Field definition without any metadata
    */
   fields: FieldDTO[];
-
-  /**
-   * replacement functions for the incoming data (fixes json limitations)
-   */
-  replace?: DataFrameReplacement[];
 }
 
-export function getReplacementValue(v: DataFrameReplacementValue): any {
+export function getReplacementValue(v: keyof DataFrameReplacements): any {
   switch (v) {
-    case DataFrameReplacementValue.Infinity:
+    case 'Inf':
       return Number.POSITIVE_INFINITY;
-    case DataFrameReplacementValue.NegInf:
+    case 'NegInf':
       return Number.NEGATIVE_INFINITY;
-    case DataFrameReplacementValue.Undefined:
+    case 'Undef':
       return undefined;
-    case DataFrameReplacementValue.NaN:
+    case 'NaN':
       return NaN;
   }
   return undefined; // should not be possible
 }
 
-function applyReplacements(msg: DataFrameDTO) {
-  if (!msg.replace?.length) {
+function applyReplacements(field: FieldDTO) {
+  if (!field.replaced) {
     return;
   }
 
-  for (const r of msg.replace) {
-    const val = getReplacementValue(r.value);
-    const col = msg.fields[r.field].values;
-    for (const idx of r.index) {
-      col[idx] = val;
+  for (const key of Object.keys(field.replaced)) {
+    const val = getReplacementValue(key as keyof DataFrameReplacements);
+    for (const idx of field.replaced[key as keyof DataFrameReplacements]!) {
+      field.values[idx] = val;
     }
   }
 }
 
-function guessFieldType(idx: number, dto: DataFrameDTO): FieldType {
-  const name = dto.fields[idx].name;
-  for (const v of dto.fields[idx].values) {
+function guessFieldType(field: FieldDTO): FieldType {
+  for (const v of field.values) {
     if (v != null) {
-      return guessFieldTypeFromNameAndValue(name, v);
+      return guessFieldTypeFromNameAndValue(field.name, v);
     }
   }
-
   return FieldType.other;
 }
 
 /**
- * NOTE: the data array may be mutated applying any replacement funciton defined
+ * NOTE: data in the original array will be replaced with values from the DataFrameReplacements property
+ *
+ * @alpha
  */
 export function dataFrameFromDTO(dto: DataFrameDTO): DataFrame {
-  applyReplacements(dto);
   let length = 0;
   for (const f of dto.fields) {
     let flen = f.values?.length;
@@ -114,12 +110,19 @@ export function dataFrameFromDTO(dto: DataFrameDTO): DataFrame {
     }
   }
 
-  const fields = dto.fields.map((f, index) => ({
-    ...f,
-    type: f.type ?? guessFieldType(index, dto),
-    config: f.config ?? {},
-    values: new ArrayVector(dto.fields[index].values).setLength(length),
-  }));
+  const fields = dto.fields.map((f, index) => {
+    if (f.replaced) {
+      applyReplacements(f);
+    }
+    f.values.length = length; // will pad with undefined
+    return {
+      ...f,
+      replaced: f.replaced ?? {},
+      type: f.type ?? guessFieldType(f),
+      config: f.config ?? {},
+      values: new ArrayVector(f.values),
+    };
+  });
   return {
     refId: dto.refId,
     meta: dto.meta,
