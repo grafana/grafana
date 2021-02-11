58ebdc0e4075ef5f0eec17e044aaff3f7ff96851
Torkel Ödegaard <torkel@grafana.com>
2021-02-11 12:46:15 +0100

    Fixed backend test data issue, and the color mode option

--- public/app/plugins/panel/piechart/module.tsx
+++ public/app/plugins/panel/piechart/module.tsx
@@ -1,38 +1,51 @@
-import { PanelPlugin } from '@grafana/data';
+import { FieldColorModeId, FieldConfigProperty, PanelPlugin } from '@grafana/data';
 import { PieChartPanel } from './PieChartPanel';
 import { PieChartOptions } from './types';
 import { addStandardDataReduceOptions } from '../stat/types';
 import { PieChartType } from '@grafana/ui';
 
-export const plugin = new PanelPlugin<PieChartOptions>(PieChartPanel).useFieldConfig().setPanelOptions((builder) => {
-  addStandardDataReduceOptions(builder, false);
-
-  builder
-    .addRadio({
-      name: 'Piechart type',
-      description: 'How the piechart should be rendered',
-      path: 'pieType',
-      settings: {
-        options: [
-          { value: PieChartType.Pie, label: 'Pie' },
-          { value: PieChartType.Donut, label: 'Donut' },
-        ],
+export const plugin = new PanelPlugin<PieChartOptions>(PieChartPanel)
+  .useFieldConfig({
+    standardOptions: {
+      [FieldConfigProperty.Color]: {
+        settings: {
+          bySeriesSupport: true,
+        },
+        defaultValue: {
+          mode: FieldColorModeId.PaletteClassic,
+        },
       },
-      defaultValue: PieChartType.Pie,
-    })
-    .addBooleanSwitch({
-      name: 'Show name',
-      path: 'labelOptions.showName',
-      defaultValue: true,
-    })
-    .addBooleanSwitch({
-      name: 'Show value',
-      path: 'labelOptions.showValue',
-      defaultValue: false,
-    })
-    .addBooleanSwitch({
-      name: 'Show percent',
-      path: 'labelOptions.showPercent',
-      defaultValue: false,
-    });
-});
+    },
+  })
+  .setPanelOptions((builder) => {
+    addStandardDataReduceOptions(builder, false);
+
+    builder
+      .addRadio({
+        name: 'Piechart type',
+        description: 'How the piechart should be rendered',
+        path: 'pieType',
+        settings: {
+          options: [
+            { value: PieChartType.Pie, label: 'Pie' },
+            { value: PieChartType.Donut, label: 'Donut' },
+          ],
+        },
+        defaultValue: PieChartType.Pie,
+      })
+      .addBooleanSwitch({
+        name: 'Show name',
+        path: 'labelOptions.showName',
+        defaultValue: true,
+      })
+      .addBooleanSwitch({
+        name: 'Show value',
+        path: 'labelOptions.showValue',
+        defaultValue: false,
+      })
+      .addBooleanSwitch({
+        name: 'Show percent',
+        path: 'labelOptions.showPercent',
+        defaultValue: false,
+      });
+  });
