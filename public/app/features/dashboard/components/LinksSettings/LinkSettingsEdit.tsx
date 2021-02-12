ba59abde9abd030ea02bc14de7be803dc31d7619
Torkel Ödegaard <torkel@grafana.com>
2021-02-12 10:48:34 +0100

    Form improvements

--- public/app/features/dashboard/components/LinksSettings/LinkSettingsEdit.tsx
+++ public/app/features/dashboard/components/LinksSettings/LinkSettingsEdit.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { css } from 'emotion';
-import { CollapsableSection, Button, TagsInput, Select, Field, Input, Switch } from '@grafana/ui';
+import { CollapsableSection, Button, TagsInput, Select, Field, Input, Checkbox } from '@grafana/ui';
 import { SelectableValue } from '@grafana/data';
 import { LinkSettingsMode } from '../DashboardSettings/LinksSettings';
 import { DashboardLink, DashboardModel } from '../../state/DashboardModel';
@@ -44,15 +44,19 @@ type LinkSettingsEditProps = {
 
 export const LinkSettingsEdit: React.FC<LinkSettingsEditProps> = ({ mode, editLinkIdx, dashboard, backToList }) => {
   const [linkSettings, setLinkSettings] = useState(editLinkIdx !== null ? dashboard.links[editLinkIdx] : NEW_LINK);
+
   const onTagsChange = (tags: any[]) => {
     setLinkSettings((link) => ({ ...link, tags: tags }));
   };
+
   const onTypeChange = (selectedItem: SelectableValue) => {
     setLinkSettings((link) => ({ ...link, type: selectedItem.value }));
   };
+
   const onIconChange = (selectedItem: SelectableValue) => {
     setLinkSettings((link) => ({ ...link, icon: selectedItem.value }));
   };
+
   const onChange = (ev: React.FocusEvent<HTMLInputElement>) => {
     const target = ev.currentTarget;
     setLinkSettings((link) => ({
@@ -60,11 +64,13 @@ export const LinkSettingsEdit: React.FC<LinkSettingsEditProps> = ({ mode, editLi
       [target.name]: target.type === 'checkbox' ? target.checked : target.value,
     }));
   };
+
   const addLink = () => {
     dashboard.links = [...dashboard.links, linkSettings];
     dashboard.updateSubmenuVisibility();
     backToList();
   };
+
   const updateLink = () => {
     dashboard.links.splice(editLinkIdx!, 1, linkSettings);
     backToList();
@@ -79,20 +85,14 @@ export const LinkSettingsEdit: React.FC<LinkSettingsEditProps> = ({ mode, editLi
       <Field label="Type">
         <Select value={linkSettings.type} options={LINK_TYPE_OPTIONS} onChange={onTypeChange} />
       </Field>
-
+      <Field label="Title">
+        <Input name="title" value={linkSettings.title} onChange={onChange} />
+      </Field>
       {linkSettings.type === 'dashboards' && (
         <>
           <Field label="With tags">
             <TagsInput tags={linkSettings.tags} placeholder="add tags" onChange={onTagsChange} />
           </Field>
-          <Field label="As dropdown">
-            <Switch name="asDropdown" value={linkSettings.asDropdown} onChange={onChange} />
-          </Field>
-          {linkSettings.asDropdown && (
-            <Field label="Title">
-              <Input name="title" value={linkSettings.title} onChange={onChange} />
-            </Field>
-          )}
         </>
       )}
       {linkSettings.type === 'link' && (
@@ -100,33 +100,50 @@ export const LinkSettingsEdit: React.FC<LinkSettingsEditProps> = ({ mode, editLi
           <Field label="Url">
             <Input name="url" value={linkSettings.url} onChange={onChange} />
           </Field>
-
-          <Field label="Title">
-            <Input name="title" value={linkSettings.title} onChange={onChange} />
-          </Field>
-
           <Field label="Tooltip">
             <Input name="tooltip" value={linkSettings.tooltip} onChange={onChange} placeholder="Open dashboard" />
           </Field>
-
           <Field label="Icon">
             <Select value={linkSettings.icon} options={LINK_ICON_OPTIONS} onChange={onIconChange} />
           </Field>
         </>
       )}
-      <CollapsableSection label="Include" isOpen={true}>
-        <Field label="Time range">
-          <Switch name="keepTime" value={linkSettings.keepTime} onChange={onChange} />
+      <CollapsableSection label="Options" isOpen={true}>
+        {linkSettings.type === 'dashboards' && (
+          <Field>
+            <Checkbox label="Show as dropdown" name="asDropdown" value={linkSettings.asDropdown} onChange={onChange} />
+          </Field>
+        )}
+        <Field>
+          <Checkbox
+            label="Include current time range"
+            name="keepTime"
+            value={linkSettings.keepTime}
+            onChange={onChange}
+          />
         </Field>
-        <Field label="Variable values">
-          <Switch name="includeVars" value={linkSettings.includeVars} onChange={onChange} />
+        <Field>
+          <Checkbox
+            label="Include current template variable values"
+            name="includeVars"
+            value={linkSettings.includeVars}
+            onChange={onChange}
+          />
         </Field>
-        <Field label="Open in new tab">
-          <Switch name="targetBlank" value={linkSettings.targetBlank} onChange={onChange} />
+        <Field>
+          <Checkbox
+            label="Open link in new tab"
+            name="targetBlank"
+            value={linkSettings.targetBlank}
+            onChange={onChange}
+          />
         </Field>
       </CollapsableSection>
-      {mode === 'new' && <Button onClick={addLink}>Add</Button>}
-      {mode === 'edit' && <Button onClick={updateLink}>Update</Button>}
+
+      <div className="gf-form-button-row">
+        {mode === 'new' && <Button onClick={addLink}>Add</Button>}
+        {mode === 'edit' && <Button onClick={updateLink}>Update</Button>}
+      </div>
     </div>
   );
 };
