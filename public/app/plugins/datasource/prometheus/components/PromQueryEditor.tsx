18de7aa21976fb9c14be975565e18aced02ec42d
Ivana <ivana.huckova@gmail.com>
2021-02-15 15:45:06 +0100

    Revert to original fix

--- public/app/plugins/datasource/prometheus/components/PromQueryEditor.tsx
+++ public/app/plugins/datasource/prometheus/components/PromQueryEditor.tsx
@@ -92,7 +92,9 @@ export class PromQueryEditor extends PureComponent<Props, State> {
 
   onRunQuery = () => {
     const { query } = this;
-    this.props.onChange({ ...this.props.query, ...query });
+    // Change of query.hide happes outside of this component and is just passed as prop. We have to update it when running queries.
+    const { hide } = this.props.query;
+    this.props.onChange({ ...query, hide });
     this.props.onRunQuery();
   };
 
