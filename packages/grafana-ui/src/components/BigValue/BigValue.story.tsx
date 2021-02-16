5ab13481311661e720ab41e5979fa360fc5281af
thisisobate <obasiuche62@gmail.com>
2021-02-16 14:07:44 +0100

    BigValue: updates story from knobs to controls

--- packages/grafana-ui/src/components/BigValue/BigValue.story.tsx
+++ packages/grafana-ui/src/components/BigValue/BigValue.story.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import { color, number, select, text } from '@storybook/addon-knobs';
+import { Story } from '@storybook/react';
 import { BigValue, BigValueColorMode, BigValueGraphMode, BigValueJustifyMode, BigValueTextMode } from './BigValue';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import mdx from './BigValue.mdx';
 import { useTheme } from '../../themes';
 import { ArrayVector, FieldSparkline, FieldType } from '@grafana/data';
-
-const getKnobs = () => {
-  return {
-    value: text('value', '$5022'),
-    title: text('title', 'Total Earnings'),
-    colorMode: select('Color mode', [BigValueColorMode.Value, BigValueColorMode.Background], BigValueColorMode.Value),
-    graphMode: select('Graph mode', [BigValueGraphMode.Area, BigValueGraphMode.None], BigValueGraphMode.Area),
-    justifyMode: select('Justify', [BigValueJustifyMode.Auto, BigValueJustifyMode.Center], BigValueJustifyMode.Auto),
-    width: number('Width', 400, { range: true, max: 800, min: 200 }),
-    height: number('Height', 300, { range: true, max: 800, min: 200 }),
-    color: color('Value color', 'red'),
-    textMode: select(
-      'Text mode',
-      [BigValueTextMode.Auto, BigValueTextMode.Name, BigValueTextMode.ValueAndName, BigValueTextMode.None],
-      BigValueTextMode.Auto
-    ),
-  };
-};
 
 export default {
   title: 'Visualizations/BigValue',
@@ -32,11 +14,84 @@ export default {
     docs: {
       page: mdx,
     },
+    knobs: {
+      disabled: true,
+    },
+  },
+  argTypes: {
+    width: { control: { type: 'range', min: 200, max: 800 } },
+    height: { control: { type: 'range', min: 200, max: 800 } },
+    colorMode: { control: { type: 'select', options: [BigValueColorMode.Value, BigValueColorMode.Background] } },
+    graphMode: { control: { type: 'select', options: [BigValueGraphMode.Area, BigValueGraphMode.None] } },
+    justifyMode: { control: { type: 'select', options: [BigValueJustifyMode.Auto, BigValueJustifyMode.Center] } },
+    textMode: {
+      control: {
+        type: 'select',
+        options: [BigValueTextMode.Auto, BigValueTextMode.Name, BigValueTextMode.ValueAndName, BigValueTextMode.None],
+      },
+    },
+    color: { control: 'color' },
+    sparkline: {
+      table: {
+        disable: true,
+      },
+    },
+    onClick: {
+      table: {
+        disable: true,
+      },
+    },
+    className: {
+      table: {
+        disable: true,
+      },
+    },
+    alignmentFactors: {
+      table: {
+        disable: true,
+      },
+    },
+    text: {
+      table: {
+        disable: true,
+      },
+    },
+    count: {
+      table: {
+        disable: true,
+      },
+    },
+    theme: {
+      table: {
+        disable: true,
+      },
+    },
   },
 };
 
-export const Basic = () => {
-  const { value, title, colorMode, graphMode, height, width, color, textMode, justifyMode } = getKnobs();
+interface StoryProps {
+  value: string;
+  title: string;
+  colorMode: BigValueColorMode;
+  graphMode: BigValueGraphMode;
+  height: number;
+  width: number;
+  color: string;
+  textMode: BigValueTextMode;
+  justifyMode: BigValueJustifyMode;
+}
+
+export const Basic: Story<StoryProps> = ({
+  value,
+  title,
+  colorMode,
+  graphMode,
+  height,
+  width,
+  color,
+  textMode,
+  justifyMode,
+}) => {
   const theme = useTheme();
   const sparkline: FieldSparkline = {
     y: {
@@ -65,4 +120,16 @@ export const Basic = () => {
       sparkline={graphMode === BigValueGraphMode.None ? undefined : sparkline}
     />
   );
+};
+
+Basic.args = {
+  value: '$5022',
+  title: 'Total Earnings',
+  colorMode: BigValueColorMode.Value,
+  graphMode: BigValueGraphMode.Area,
+  justifyMode: BigValueJustifyMode.Auto,
+  width: 400,
+  height: 300,
+  color: 'red',
+  textMode: BigValueTextMode.Auto,
 };
