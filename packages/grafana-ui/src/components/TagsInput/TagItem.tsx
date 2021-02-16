b3070cfd681aa1faae02626011cf0140af300711
Torkel Ödegaard <torkel@grafana.org>
2021-02-16 09:39:11 +0100

    TagsInput: Design update and component refactor (#31163)
    * TagsInput: Design update and component refactor
    
    * Update packages/grafana-ui/src/components/TagsInput/TagsInput.tsx
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * Update packages/grafana-ui/src/components/TagsInput/TagsInput.tsx
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * Update packages/grafana-ui/src/components/TagsInput/TagsInput.tsx
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * Update packages/grafana-ui/src/components/TagsInput/TagsInput.tsx
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * Updated
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>

--- packages/grafana-ui/src/components/TagsInput/TagItem.tsx
+++ packages/grafana-ui/src/components/TagsInput/TagItem.tsx
@@ -13,21 +13,24 @@ interface Props {
 
 const getStyles = stylesFactory(({ theme, name }: { theme: GrafanaTheme; name: string }) => {
   const { color, borderColor } = getTagColorsFromName(name);
+  const height = theme.spacing.formInputHeight - 8;
 
   return {
     itemStyle: css`
+      display: flex;
+      align-items: center;
+      height: ${height}px;
+      line-height: ${height - 2}px;
       background-color: ${color};
       color: ${theme.palette.white};
       border: 1px solid ${borderColor};
       border-radius: 3px;
-      padding: 3px 6px;
-      margin: 3px;
+      padding: 0 ${theme.spacing.xs};
+      margin-right: 3px;
       white-space: nowrap;
       text-shadow: none;
       font-weight: 500;
       font-size: ${theme.typography.size.sm};
-      display: flex;
-      align-items: center;
     `,
 
     nameStyle: css`
@@ -36,6 +39,10 @@ const getStyles = stylesFactory(({ theme, name }: { theme: GrafanaTheme; name: s
   };
 });
 
+/**
+ * @internal
+ * Only used internally by TagsInput
+ * */
 export const TagItem: FC<Props> = ({ name, onRemove }) => {
   const theme = useTheme();
   const styles = getStyles({ theme, name });
