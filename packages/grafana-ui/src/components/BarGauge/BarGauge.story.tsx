8960e5205f6a04061e58764d0428285886d0533a
thisisobate <obasiuche62@gmail.com>
2021-02-16 11:34:36 +0100

    fixes type errors and disables unused controls

--- packages/grafana-ui/src/components/BarGauge/BarGauge.story.tsx
+++ packages/grafana-ui/src/components/BarGauge/BarGauge.story.tsx
@@ -19,23 +19,106 @@ export default {
       disabled: true,
     },
   },
+  argTypes: {
+    theme: {
+      table: {
+        disable: true,
+      },
+    },
+    height: {
+      table: {
+        disable: true,
+      },
+    },
+    width: {
+      table: {
+        disable: true,
+      },
+    },
+    field: {
+      table: {
+        disable: true,
+      },
+    },
+    display: {
+      table: {
+        disable: true,
+      },
+    },
+    orientation: {
+      table: {
+        disable: true,
+      },
+    },
+    text: {
+      table: {
+        disable: true,
+      },
+    },
+    itemSpacing: {
+      table: {
+        disable: true,
+      },
+    },
+    lcdCellWidth: {
+      table: {
+        disable: true,
+      },
+    },
+    displayMode: {
+      table: {
+        disable: true,
+      },
+    },
+    onClick: {
+      table: {
+        disable: true,
+      },
+    },
+    className: {
+      table: {
+        disable: true,
+      },
+    },
+    showUnfilled: {
+      table: {
+        disable: true,
+      },
+    },
+    alignmentFactors: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 
+interface StoryProps {
+  value: number;
+  title: string;
+  minValue: number;
+  maxValue: number;
+  threshold1Color: string;
+  threshold2Color: string;
+  threshold1Value: number;
+  threshold2Value: number;
+}
+
 function addBarGaugeStory(overrides: Partial<Props>) {
-  return ({ value, title, minValue, maxValue, threshold1Color, threshold2Color, threshold1Value, threshold2Value }) => {
+  return (storyProps: StoryProps) => {
     const theme = useTheme();
 
     const field: Partial<Field> = {
       type: FieldType.number,
       config: {
-        min: minValue,
-        max: maxValue,
+        min: storyProps.minValue,
+        max: storyProps.maxValue,
         thresholds: {
           mode: ThresholdsMode.Absolute,
           steps: [
             { value: -Infinity, color: 'green' },
-            { value: threshold1Value, color: threshold1Color },
-            { value: threshold2Value, color: threshold2Color },
+            { value: storyProps.threshold1Value, color: storyProps.threshold1Color },
+            { value: storyProps.threshold2Value, color: storyProps.threshold2Color },
           ],
         },
       },
@@ -47,9 +130,9 @@ function addBarGaugeStory(overrides: Partial<Props>) {
       width: 300,
       height: 300,
       value: {
-        text: value.toString(),
-        title: title,
-        numeric: value,
+        text: storyProps.value.toString(),
+        title: storyProps.title,
+        numeric: storyProps.value,
       },
       orientation: VizOrientation.Vertical,
       displayMode: BarGaugeDisplayMode.Basic,
@@ -63,48 +146,28 @@ function addBarGaugeStory(overrides: Partial<Props>) {
   };
 }
 
-export const gradientVertical: Story<Props> = addBarGaugeStory({
+export const gradientVertical: Story<StoryProps> = addBarGaugeStory({
   displayMode: BarGaugeDisplayMode.Gradient,
   orientation: VizOrientation.Vertical,
   height: 500,
   width: 100,
 });
 
-export const gradientHorizontal: Story<Props> = addBarGaugeStory({
+export const gradientHorizontal: Story<StoryProps> = addBarGaugeStory({
   displayMode: BarGaugeDisplayMode.Gradient,
   orientation: VizOrientation.Horizontal,
   height: 100,
   width: 500,
 });
 
-export const lcdHorizontal: Story<Props> = addBarGaugeStory({
+export const lcdHorizontal: Story<StoryProps> = addBarGaugeStory({
   displayMode: BarGaugeDisplayMode.Lcd,
   orientation: VizOrientation.Vertical,
   height: 500,
   width: 100,
 });
 
-gradientVertical.args = {
-  value: 70,
-  title: 'Title',
-  minValue: 0,
-  maxValue: 100,
-  threshold1Value: 40,
-  threshold1Color: 'orange',
-  threshold2Value: 60,
-  threshold2Color: 'red',
-};
-gradientHorizontal.args = {
-  value: 70,
-  title: 'Title',
-  minValue: 0,
-  maxValue: 100,
-  threshold1Value: 40,
-  threshold1Color: 'orange',
-  threshold2Value: 60,
-  threshold2Color: 'red',
-};
-lcdHorizontal.args = {
+gradientVertical.args = gradientHorizontal.args = lcdHorizontal.args = {
   value: 70,
   title: 'Title',
   minValue: 0,
