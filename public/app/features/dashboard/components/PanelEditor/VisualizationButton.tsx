2257292819d91e8de6486735957e1c963acbb0f2
Torkel Ödegaard <torkel@grafana.com>
2021-02-16 17:54:21 +0100

    updates

--- public/app/features/dashboard/components/PanelEditor/VisualizationButton.tsx
+++ public/app/features/dashboard/components/PanelEditor/VisualizationButton.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { css } from 'emotion';
 import { GrafanaTheme, PanelPlugin, PanelPluginMeta } from '@grafana/data';
-import { useTheme, stylesFactory, Icon, Input, ToolbarButton, ButtonGroup, LinkButton, Button } from '@grafana/ui';
+import { useTheme, stylesFactory, Icon, Input, ToolbarButton, ButtonGroup, Button } from '@grafana/ui';
 import { changePanelPlugin } from '../../state/actions';
 import { StoreState } from 'app/types';
 import { PanelModel } from '../../state/PanelModel';
@@ -28,110 +28,104 @@ interface DispatchProps {
 
 type Props = OwnProps & ConnectedProps & DispatchProps;
 
-export const VisualizationButtonUnconnected = React.forwardRef<HTMLInputElement, Props>(
-  (
-    {
-      panel,
-      plugin,
-      changePanelPlugin,
-      toggleVizPicker,
-      isPanelOptionsVisible,
-      isVizPickerOpen,
-      setPanelEditorUIState,
-    },
-    ref
-  ) => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const theme = useTheme();
-    const styles = getStyles(theme);
+export const VisualizationButtonUnconnected: FC<Props> = ({
+  panel,
+  plugin,
+  changePanelPlugin,
+  toggleVizPicker,
+  isPanelOptionsVisible,
+  isVizPickerOpen,
+  setPanelEditorUIState,
+}) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const theme = useTheme();
+  const styles = getStyles(theme);
 
-    const onPluginTypeChange = (meta: PanelPluginMeta) => {
-      if (meta.id === plugin!.meta.id) {
-        toggleVizPicker(false);
-      } else {
-        changePanelPlugin(panel, meta.id);
-      }
-    };
-
-    const onToggleOpen = () => {
-      toggleVizPicker(!isVizPickerOpen);
-    };
-
-    const onKeyPress = useCallback(
-      (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-          const query = e.currentTarget.value;
-          const plugins = getAllPanelPluginMeta();
-          const match = filterPluginList(plugins, query, plugin!.meta);
-          if (match && match.length) {
-            onPluginTypeChange(match[0]);
-          }
-        }
-      },
-      [onPluginTypeChange, plugin]
-    );
-
-    const onToggleOptionsPane = () => {
-      setPanelEditorUIState({ isPanelOptionsVisible: !isPanelOptionsVisible });
-    };
-
-    if (!plugin) {
-      return null;
+  const onPluginTypeChange = (meta: PanelPluginMeta) => {
+    if (meta.id === plugin!.meta.id) {
+      toggleVizPicker(false);
+    } else {
+      changePanelPlugin(panel, meta.id);
     }
+  };
 
-    const suffix =
-      searchQuery !== '' ? (
-        <Button icon="times" variant="link" size="sm" onClick={() => setSearchQuery('')}>
-          Clear filter
-        </Button>
-      ) : null;
+  const onToggleOpen = () => {
+    toggleVizPicker(!isVizPickerOpen);
+  };
 
-    return (
-      <div className={styles.wrapper}>
-        <ButtonGroup>
-          <ToolbarButton
-            tooltip="Click to change visualisation"
-            imgSrc={plugin.meta.info.logos.small}
-            isOpen={isVizPickerOpen}
-            onClick={onToggleOpen}
-            fullWidth
-          >
-            {plugin.meta.name}
-          </ToolbarButton>
-          <ToolbarButton
-            tooltip={isPanelOptionsVisible ? 'Close options pane' : 'Show options pane'}
-            icon="sliders-v-alt"
-            onClick={onToggleOptionsPane}
-            isOpen={isPanelOptionsVisible}
-          />
-        </ButtonGroup>
-        {isVizPickerOpen && (
-          <div className={styles.openWrapper}>
-            <Field>
-              <Input
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.currentTarget.value)}
-                onKeyPress={onKeyPress}
-                prefix={<Icon name="filter" className={styles.icon} />}
-                suffix={suffix}
-                autoFocus
-                placeholder="Filter visualizations"
-                ref={ref}
-              />
-            </Field>
+  const onKeyPress = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        const query = e.currentTarget.value;
+        const plugins = getAllPanelPluginMeta();
+        const match = filterPluginList(plugins, query, plugin!.meta);
+        if (match && match.length) {
+          onPluginTypeChange(match[0]);
+        }
+      }
+    },
+    [onPluginTypeChange, plugin]
+  );
 
-            <VizTypePicker
-              current={plugin.meta}
-              onTypeChange={onPluginTypeChange}
-              searchQuery={searchQuery}
-              onClose={() => {}}
-            />
-          </div>
-        )}
-      </div>
-    );
+  const onToggleOptionsPane = () => {
+    setPanelEditorUIState({ isPanelOptionsVisible: !isPanelOptionsVisible });
+  };
+
+  if (!plugin) {
+    return null;
   }
-);
+
+  const suffix =
+    searchQuery !== '' ? (
+      <Button icon="times" variant="link" size="sm" onClick={() => setSearchQuery('')}>
+        Clear filter
+      </Button>
+    ) : null;
+
+  return (
+    <div className={styles.wrapper}>
+      <ButtonGroup>
+        <ToolbarButton
+          tooltip="Click to change visualisation"
+          imgSrc={plugin.meta.info.logos.small}
+          isOpen={isVizPickerOpen}
+          onClick={onToggleOpen}
+          fullWidth
+        >
+          {plugin.meta.name}
+        </ToolbarButton>
+        <ToolbarButton
+          tooltip={isPanelOptionsVisible ? 'Close options pane' : 'Show options pane'}
+          icon="sliders-v-alt"
+          onClick={onToggleOptionsPane}
+          isOpen={isPanelOptionsVisible}
+        />
+      </ButtonGroup>
+      {isVizPickerOpen && (
+        <div className={styles.openWrapper}>
+          <Field>
+            <Input
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.currentTarget.value)}
+              onKeyPress={onKeyPress}
+              prefix={<Icon name="filter" className={styles.icon} />}
+              suffix={suffix}
+              autoFocus
+              placeholder="Filter visualizations"
+            />
+          </Field>
+
+          <VizTypePicker
+            current={plugin.meta}
+            onTypeChange={onPluginTypeChange}
+            searchQuery={searchQuery}
+            onClose={() => {}}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 VisualizationButtonUnconnected.displayName = 'VisualizationTabUnconnected';
 
