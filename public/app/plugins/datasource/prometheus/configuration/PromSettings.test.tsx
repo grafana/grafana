4ca3dccb6db52a428bdef31ad602e3b97ae7ef4f
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2021-02-22 17:35:15 +0100

    Prometheus: Change default httpMethod for new instances to POST (#31292)
    * Implement POST to specific endpoints and change POST as default
    
    * Add tests for PromSettings
    
    * Add tests to dataosurce.ts for new functionality
    
    * Remove /label /series implementation
    
    * Update public/app/plugins/datasource/prometheus/configuration/PromSettings.tsx

--- public/app/plugins/datasource/prometheus/configuration/PromSettings.test.tsx
+++ public/app/plugins/datasource/prometheus/configuration/PromSettings.test.tsx
@@ -1,7 +1,9 @@
-import { getValueFromEventItem, promSettingsValidationEvents } from './PromSettings';
+import React, { SyntheticEvent } from 'react';
+import { render, screen } from '@testing-library/react';
 import { EventsWithValidation } from '@grafana/ui';
-import { SyntheticEvent } from 'react';
 import { SelectableValue } from '@grafana/data';
+import { getValueFromEventItem, promSettingsValidationEvents, PromSettings } from './PromSettings';
+import { createDefaultConfigOptions } from './mocks';
 
 describe('PromSettings', () => {
   describe('getValueFromEventItem', () => {
@@ -82,5 +84,57 @@ describe('PromSettings', () => {
         expect(validationEvents[EventsWithValidation.onBlur][0].rule(value)).toBe(expected);
       }
     );
+  });
+  describe('PromSettings component', () => {
+    const defaultProps = createDefaultConfigOptions();
+
+    it('should show POST httpMethod if no httpMethod and no url', () => {
+      const options = defaultProps;
+      options.url = '';
+      options.jsonData.httpMethod = '';
+
+      render(
+        <div>
+          <PromSettings onOptionsChange={() => {}} options={options} />
+        </div>
+      );
+      expect(screen.getByText('POST')).toBeInTheDocument();
+    });
+    it('should show GET httpMethod if no httpMethod and url', () => {
+      const options = defaultProps;
+      options.url = 'test_url';
+      options.jsonData.httpMethod = '';
+
+      render(
+        <div>
+          <PromSettings onOptionsChange={() => {}} options={options} />
+        </div>
+      );
+      expect(screen.getByText('GET')).toBeInTheDocument();
+    });
+    it('should show POST httpMethod if POST httpMethod is configured', () => {
+      const options = defaultProps;
+      options.url = 'test_url';
+      options.jsonData.httpMethod = 'POST';
+
+      render(
+        <div>
+          <PromSettings onOptionsChange={() => {}} options={options} />
+        </div>
+      );
+      expect(screen.getByText('POST')).toBeInTheDocument();
+    });
+    it('should show GET httpMethod if GET httpMethod is configured', () => {
+      const options = defaultProps;
+      options.url = 'test_url';
+      options.jsonData.httpMethod = 'GET';
+
+      render(
+        <div>
+          <PromSettings onOptionsChange={() => {}} options={options} />
+        </div>
+      );
+      expect(screen.getByText('GET')).toBeInTheDocument();
+    });
   });
 });
