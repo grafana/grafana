a67cc5691ceda3c1658423a9dc1ae2d017307928
Torkel Ödegaard <torkel@grafana.com>
2021-02-23 18:44:45 +0100

    progress

--- pkg/services/docstore/models.go
+++ pkg/services/docstore/models.go
@@ -3,21 +3,36 @@ package docstore
 import "time"
 
 type Entity struct {
+	Meta EntityMeta
+	Doc  interface{}
 }
 
 type EntityMeta struct {
-	OrgId       int64
-	Id          string
-	ExternalId  string
-	Name        string
-	Type        string
+	OrgId int64
+	// Thinking similar/same as uid
+	Id string
+	// For syncing with external systems (maybe repo and file path?)
+	ExternalId string
+	// Entity name
+	Name string
+	// Entity type
+	Type string
+	// Entity description
 	Description string
-	Tags        []string
-	Version     int64
-	CreatedOn   time.Time
-	UpdatedOn   time.Time
-	CreatedBy   EntityUserInfo
-	UpdatedBy   EntityUserInfo
+	// For filtering & categorization, alternative option is a key/value labelset
+	Tags []EntityTag
+	// Relationships
+	Relationships []EntityRelationship
+	// For optimistic concurrency
+	Version int64
+	// Thinking folder here
+	Container EntityMetaContainer
+	// Dates
+	CreatedOn time.Time
+	UpdatedOn time.Time
+	// Created by user
+	CreatedBy EntityUserInfo
+	UpdatedBy EntityUserInfo
 }
 
 type EntityUserInfo struct {
@@ -27,8 +42,29 @@ type EntityUserInfo struct {
 	AvatarUrl string
 }
 
+// Just the meta info for a a container
 type EntityMetaContainer struct {
 	Name        string
 	ContainerId string
 	ExternalId  string
+}
+
+type EntityRelationship struct {
+	Name       string
+	Id         string
+	OrgId      int64
+	EntityType string
+	IsChild    bool
+}
+
+type EntityTag struct {
+	Name  string
+	Id    string
+	OrgId int64
+	Color string
+}
+
+type EntityStore interface {
+	GetById(id string) Entity
+	CreateEntity(entity Entity)
 }
