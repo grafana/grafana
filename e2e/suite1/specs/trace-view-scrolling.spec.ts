0ea509b4ecf9eacc6604d3583dbbfd93c063b67f
Zoltán Bedi <zoltan.bedi@gmail.com>
2021-02-24 12:51:22 +0100

    Revert "Move fixture to e2e"
    This reverts commit fab88cc54ccf47dd5c28d47fec8b7ac4443e507c.

--- e2e/suite1/specs/trace-view-scrolling.spec.ts
+++ e2e/suite1/specs/trace-view-scrolling.spec.ts
@@ -1,15 +1,13 @@
 import { e2e } from '@grafana/e2e';
-import { response } from '../../mocks/long-trace-response';
 
 describe('Trace view', () => {
   it('Can lazy load big traces', () => {
     e2e.flows.login('admin', 'admin');
-    e2e().on('uncaught:exception', (err, runnable) => {
-      // returning false here prevents Cypress from
-      // failing the test
-      return false;
-    });
-    e2e().intercept('/api/traces/long-trace', response).as('longTrace');
+    e2e()
+      .intercept('GET', '/api/traces/long-trace', {
+        fixture: 'long-trace-response.json',
+      })
+      .as('longTrace');
 
     e2e.pages.Explore.visit();
 
@@ -27,22 +25,11 @@ describe('Trace view', () => {
 
     e2e().wait('@longTrace');
 
-    e2e()
-      .get('details')
-      .should('be.visible')
-      .then(function ($input) {
-        $input[0].setAttribute('open', 'true');
-      })
-      .screenshot();
+    e2e.components.TraceViewer.spanBar().should('have.length', 100);
 
-    // e2e()
-    //   .get(selectors.components.TraceViewer.spanBar(), { timeout: 50000 })
-    //   .should('be.visible')
-    //   .and('have.length', 100);
+    e2e.pages.Explore.General.scrollBar().scrollTo('center');
 
-    // e2e.pages.Explore.General.scrollBar().scrollTo('center');
-
-    // // After scrolling we should have 140 spans instead of the first 100
-    // e2e.components.TraceViewer.spanBar().should('have.length', 140);
+    // After scrolling we should have 140 spans instead of the first 100
+    e2e.components.TraceViewer.spanBar().should('have.length', 140);
   });
 });
