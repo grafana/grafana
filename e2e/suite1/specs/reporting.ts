fabd461319694a700939164f1fadbb6e608d12c0
Clarity-89 <homes89@ukr.net>
2021-02-24 09:11:31 +0200

    Add report test

---
+++ e2e/suite1/specs/reporting.ts
@@ -0,0 +1,13 @@
+import { e2e } from '@grafana/e2e';
+
+e2e.scenario({
+  describeName: 'Reporting',
+  itName: 'Create report',
+  addScenarioDataSource: false,
+  addScenarioDashBoard: false,
+  skipScenario: false,
+  scenario: () => {
+    e2e.pages.NewReport.visit();
+    e2e.components.Reporting.pageHeader().should('be.visible');
+  },
+});
