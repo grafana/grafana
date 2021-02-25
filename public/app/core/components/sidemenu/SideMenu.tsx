a234c8f289f31e0e77142413d679436e11fc64d4
Dominik Prokop <dominik.prokop@grafana.com>
2021-02-25 13:35:32 +0100

    Hide side menu in specified locations

--- public/app/core/components/sidemenu/SideMenu.tsx
+++ public/app/core/components/sidemenu/SideMenu.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useCallback } from 'react';
 import appEvents from '../../app_events';
 import TopSection from './TopSection';
 import BottomSection from './BottomSection';
@@ -6,28 +6,37 @@ import config from 'app/core/config';
 import { CoreEvents } from 'app/types';
 import { Branding } from 'app/core/components/Branding/Branding';
 import { Icon } from '@grafana/ui';
+import { useLocation } from 'react-router-dom';
 
 const homeUrl = config.appSubUrl || '/';
+const queryParamsToHideSidemenu = ['viewPanel'];
 
-export class SideMenu extends PureComponent {
-  toggleSideMenuSmallBreakpoint = () => {
+export const SideMenu = () => {
+  const location = useLocation();
+  const toggleSideMenuSmallBreakpoint = useCallback(() => {
     appEvents.emit(CoreEvents.toggleSidemenuMobile);
-  };
+  }, []);
 
-  render() {
-    return [
+  for (let i = 0; i < queryParamsToHideSidemenu.length; i++) {
+    if (location.search.indexOf(queryParamsToHideSidemenu[i]) > -1) {
+      return null;
+    }
+  }
+
+  return (
+    <div className="sidemenu">
       <a href={homeUrl} className="sidemenu__logo" key="logo">
         <Branding.MenuLogo />
-      </a>,
-      <div className="sidemenu__logo_small_breakpoint" onClick={this.toggleSideMenuSmallBreakpoint} key="hamburger">
+      </a>
+      <div className="sidemenu__logo_small_breakpoint" onClick={toggleSideMenuSmallBreakpoint} key="hamburger">
         <Icon name="bars" size="xl" />
         <span className="sidemenu__close">
           <Icon name="times" />
           &nbsp;Close
         </span>
-      </div>,
+      </div>
       <TopSection key="topsection" />,
       <BottomSection key="bottomsection" />,
-    ];
-  }
-}
+    </div>
+  );
+};
