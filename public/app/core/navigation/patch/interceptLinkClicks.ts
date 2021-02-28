20c6535fa90f84cddd8f5a519b2db6fb8fa00e3f
Torkel Ödegaard <torkel@grafana.com>
2021-02-28 19:03:41 +0100

    fix links with urls that have no starting / to work like before

--- public/app/core/navigation/patch/interceptLinkClicks.ts
+++ public/app/core/navigation/patch/interceptLinkClicks.ts
@@ -1,15 +1,26 @@
 import { locationService, navigationLogger } from '@grafana/runtime';
+import { config } from 'app/core/config';
 
 export function interceptLinkClicks(e: MouseEvent) {
   const anchor = getParentAnchor(e.target as HTMLElement);
 
   if (anchor) {
-    const href = anchor.getAttribute('href');
+    let href = anchor.getAttribute('href');
     const target = anchor.getAttribute('target');
 
     if (href && !target) {
       navigationLogger('utils', false, 'intercepting link click', e);
       e.preventDefault();
+
+      // Ensure old angular urls with no starting '/' are handled the same as before
+      // That is they where seen as being absolute from app root
+      if (href[0] !== '/') {
+        href = config.appSubUrl + href;
+        if (href[0] !== '/') {
+          href = '/' + href;
+        }
+      }
+
       locationService.push(href);
     }
   }
