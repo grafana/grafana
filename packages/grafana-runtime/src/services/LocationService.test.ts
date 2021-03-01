90bfe6c14eace23aaa8ba4778e533de1a3c42bc1
Dominik Prokop <dominik.prokop@grafana.com>
2021-03-01 15:37:02 +0100

    Fix some tests weirdness

--- packages/grafana-runtime/src/services/LocationService.test.ts
+++ packages/grafana-runtime/src/services/LocationService.test.ts
@@ -13,16 +13,5 @@ describe('LocationService', () => {
       }
     `);
     });
-    it('returns query string as object1', () => {
-      locationService.push('/explore?orgId=1&left=%5B"now-1h","now","prom-1",%7B%7D%5D');
-
-      expect(locationService.getSearchObject()).toMatchInlineSnapshot(`
-      Object {
-        "query1": false,
-        "query2": "123",
-        "query3": "text",
-      }
-    `);
-    });
   });
 });
