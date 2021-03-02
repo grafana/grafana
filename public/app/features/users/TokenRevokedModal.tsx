03fc04a7b8eb42e45fc7fb4890d8bb575881235d
Joan López de la Franca Beltran <joanjan14@gmail.com>
2021-03-02 19:28:05 +0100

    Feedback: Refactor TokenRevokedModal to FC

--- public/app/features/users/TokenRevokedModal.tsx
+++ public/app/features/users/TokenRevokedModal.tsx
@@ -2,26 +2,18 @@ import React from 'react';
 import { Button, Modal } from '@grafana/ui';
 import config from 'app/core/config';
 
-interface Props {}
-
-interface State {}
-
-export class TokenRevokedModal extends React.Component<Props, State> {
-  redirectToLogin() {
+export const TokenRevokedModal = () => {
+  const redirectToLogin = () => {
     window.location.href = config.appSubUrl + '/';
-  }
+  };
 
-  render() {
-    return (
-      <Modal title="Token revoked" isOpen={true} onDismiss={this.redirectToLogin} onClickBackdrop={() => {}}>
-        <p>Your token has been revoked because you have reached the maximum amount of concurrent sessions.</p>
-        <p>Please, log in again.</p>
-        <div className="gf-form-group">
-          <Button size="md" variant="primary" onClick={this.redirectToLogin}>
-            Go to login
-          </Button>
-        </div>
-      </Modal>
-    );
-  }
-}
+  return (
+    <Modal title="Token revoked" isOpen={true} onDismiss={redirectToLogin} onClickBackdrop={() => {}}>
+      <p>Your token has been revoked because you have reached the maximum amount of concurrent sessions.</p>
+      <p>Please, log in again.</p>
+      <Button size="md" variant="primary" onClick={redirectToLogin}>
+        Go to login
+      </Button>
+    </Modal>
+  );
+};
