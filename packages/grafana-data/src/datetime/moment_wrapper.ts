33855f8026c28348ed00f0ed2ab2bc9db61e9e3a
Elfo404 <me@giordanoricci.com>
2021-03-04 22:24:32 +0000

    Elasticsearch: revert to isoWeek when resolving weekly indices

--- packages/grafana-data/src/datetime/moment_wrapper.ts
+++ packages/grafana-data/src/datetime/moment_wrapper.ts
@@ -1,6 +1,6 @@
 import { TimeZone } from '../types/time';
 /* eslint-disable id-blacklist, no-restricted-imports, @typescript-eslint/ban-types */
-import moment, { Moment, MomentInput, DurationInputArg1 } from 'moment';
+import moment, { Moment, MomentInput, DurationInputArg1, DurationInputArg2 } from 'moment';
 export interface DateTimeBuiltinFormat {
   __momentBuiltinFormatBrand: any;
 }
@@ -17,6 +17,7 @@ export type DurationUnit =
   | 'M'
   | 'week'
   | 'weeks'
+  | 'isoWeek'
   | 'w'
   | 'day'
   | 'days'
@@ -96,7 +97,7 @@ export const toUtc = (input?: DateTimeInput, formatInput?: FormatInput): DateTim
 };
 
 export const toDuration = (input?: DurationInput, unit?: DurationUnit): DateTimeDuration => {
-  return moment.duration(input as DurationInputArg1, unit) as DateTimeDuration;
+  return moment.duration(input as DurationInputArg1, unit as DurationInputArg2) as DateTimeDuration;
 };
 
 export const dateTime = (input?: DateTimeInput, formatInput?: FormatInput): DateTime => {
