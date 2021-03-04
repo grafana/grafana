c15d1f498a24ba6032a92d70565f2be05bf4980d
Zoltán Bedi <zoltan.bedi@gmail.com>
2021-03-04 21:20:26 +0100

    Revert "Tempo: convert to backend data source (#31618)" (#31702)
    This reverts commit 862f09376fd0de2b0e2a7e913c681fa88824b6dc.

--- public/app/plugins/datasource/tempo/datasource.ts
+++ public/app/plugins/datasource/tempo/datasource.ts
@@ -1,63 +1,121 @@
 import {
-  DataFrame,
-  DataQuery,
+  dateMath,
+  DateTime,
+  MutableDataFrame,
+  DataSourceApi,
+  DataSourceInstanceSettings,
   DataQueryRequest,
   DataQueryResponse,
-  DataSourceInstanceSettings,
+  DataQuery,
   FieldType,
-  MutableDataFrame,
 } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
-import { Observable } from 'rxjs';
+import { getBackendSrv, BackendSrvRequest } from '@grafana/runtime';
+import { Observable, from, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { getTimeSrv, TimeSrv } from 'app/features/dashboard/services/TimeSrv';
+import { serializeParams } from 'app/core/utils/fetch';
 
 export type TempoQuery = {
   query: string;
 } & DataQuery;
 
-export class TempoDatasource extends DataSourceWithBackend<TempoQuery> {
-  constructor(instanceSettings: DataSourceInstanceSettings) {
+export class TempoDatasource extends DataSourceApi<TempoQuery> {
+  constructor(private instanceSettings: DataSourceInstanceSettings, private readonly timeSrv: TimeSrv = getTimeSrv()) {
     super(instanceSettings);
   }
 
-  query(options: DataQueryRequest<TempoQuery>): Observable<DataQueryResponse> {
-    return super.query(options).pipe(
-      map((response) => {
-        if (response.error) {
-          return response;
-        }
+  async metadataRequest(url: string, params?: Record<string, any>): Promise<any> {
+    const res = await this._request(url, params, { hideFromInspector: true }).toPromise();
+    return res.data.data;
+  }
 
-        return {
-          data: [
-            new MutableDataFrame({
-              fields: [
-                {
-                  name: 'trace',
-                  type: FieldType.trace,
-                  values: [JSON.parse((response.data as DataFrame[])[0].fields[0].values.get(0))],
+  query(options: DataQueryRequest<TempoQuery>): Observable<DataQueryResponse> {
+    // At this moment we expect only one target. In case we somehow change the UI to be able to show multiple
+    // traces at one we need to change this.
+    const id = options.targets[0]?.query;
+    if (id) {
+      return this._request(`/api/traces/${encodeURIComponent(id)}`).pipe(
+        map((response) => {
+          return {
+            data: [
+              new MutableDataFrame({
+                fields: [
+                  {
+                    name: 'trace',
+                    type: FieldType.trace,
+                    values: response?.data?.data || [],
+                  },
+                ],
+                meta: {
+                  preferredVisualisationType: 'trace',
                 },
-              ],
-              meta: {
-                preferredVisualisationType: 'trace',
+              }),
+            ],
+          };
+        })
+      );
+    } else {
+      return of({
+        data: [
+          new MutableDataFrame({
+            fields: [
+              {
+                name: 'trace',
+                type: FieldType.trace,
+                values: [],
               },
-            }),
-          ],
-        };
-      })
-    );
+            ],
+            meta: {
+              preferredVisualisationType: 'trace',
+            },
+          }),
+        ],
+      });
+    }
   }
 
   async testDatasource(): Promise<any> {
-    const response = await super.query({ targets: [{ query: '', refId: 'A' }] } as any).toPromise();
-
-    if (!response.error?.message?.startsWith('failed to get trace')) {
-      return { status: 'error', message: 'Data source is not working' };
+    try {
+      await this._request(`/api/traces/random`).toPromise();
+    } catch (e) {
+      // If all went well this request will get back with 400 - Bad request
+      if (e?.status !== 400) {
+        throw e;
+      }
     }
-
     return { status: 'success', message: 'Data source is working' };
+  }
+
+  getTimeRange(): { start: number; end: number } {
+    const range = this.timeSrv.timeRange();
+    return {
+      start: getTime(range.from, false),
+      end: getTime(range.to, true),
+    };
   }
 
   getQueryDisplayText(query: TempoQuery) {
     return query.query;
   }
+
+  private _request(apiUrl: string, data?: any, options?: Partial<BackendSrvRequest>): Observable<Record<string, any>> {
+    // Hack for proxying metadata requests
+    const baseUrl = `/api/datasources/proxy/${this.instanceSettings.id}`;
+    const params = data ? serializeParams(data) : '';
+    const url = `${baseUrl}${apiUrl}${params.length ? `?${params}` : ''}`;
+    const req = {
+      ...options,
+      url,
+    };
+
+    return from(getBackendSrv().datasourceRequest(req));
+  }
+}
+
+function getTime(date: string | DateTime, roundUp: boolean) {
+  if (typeof date === 'string') {
+    date = dateMath.parse(date, roundUp)!;
+  }
+  return date.valueOf() * 1000;
 }
