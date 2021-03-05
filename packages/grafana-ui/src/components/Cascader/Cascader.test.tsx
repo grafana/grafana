2395efba053f4a1738924393dde335bc3854aa47
Vicky Lee <36230812+vickyyyyyyy@users.noreply.github.com>
2021-03-05 18:26:31 +0000

    Grafana/ui: display all selected levels for Cascader (#31729)
    * add support for displaying all selected levels for cascader
    
    * add story for Cascader with display all selected levels
    
    * add tests for Cascader with displayAllSelectedLevels prop
    
    * replace enzyme test with RTL for cascader

--- packages/grafana-ui/src/components/Cascader/Cascader.test.tsx
+++ packages/grafana-ui/src/components/Cascader/Cascader.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Cascader } from './Cascader';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const options = [
   {
@@ -27,36 +28,73 @@ const options = [
   },
 ];
 
-const flatOptions = [
-  {
-    singleLabel: 'Second',
-    label: 'First / Second',
-    value: ['1', '2'],
-  },
-  {
-    singleLabel: 'Third',
-    label: 'First / Third',
-    value: ['1', '3'],
-  },
-  {
-    singleLabel: 'Fourth',
-    label: 'First / Fourth',
-    value: ['1', '4'],
-  },
-  {
-    singleLabel: 'FirstFirst',
-    label: 'FirstFirst',
-    value: ['5'],
-  },
-];
-
 describe('Cascader', () => {
-  let cascader: any;
-  beforeEach(() => {
-    cascader = shallow(<Cascader options={options} onSelect={() => {}} />);
+  const placeholder = 'cascader-placeholder';
+
+  it('filters results when searching', () => {
+    render(<Cascader placeholder={placeholder} options={options} onSelect={() => {}} />);
+
+    userEvent.type(screen.getByPlaceholderText(placeholder), 'Third');
+
+    expect(screen.queryByText('Second')).not.toBeInTheDocument();
+    expect(screen.getByText('First / Third')).toBeInTheDocument();
   });
 
-  it('Should convert options to searchable strings', () => {
-    expect(cascader.state('searchableOptions')).toEqual(flatOptions);
+  it('displays all levels selected with default separator when displayAllSelectedLevels is true', () => {
+    render(
+      <Cascader displayAllSelectedLevels={true} placeholder={placeholder} options={options} onSelect={() => {}} />
+    );
+
+    expect(screen.queryByDisplayValue('First/Second')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByPlaceholderText(placeholder));
+    userEvent.click(screen.getByText('First'));
+    userEvent.click(screen.getByText('Second'));
+
+    expect(screen.getByDisplayValue('First/Second')).toBeInTheDocument();
+  });
+
+  it('displays all levels selected with separator passed in when displayAllSelectedLevels is true', () => {
+    const separator = ',';
+
+    render(
+      <Cascader
+        displayAllSelectedLevels={true}
+        separator={separator}
+        placeholder={placeholder}
+        options={options}
+        onSelect={() => {}}
+      />
+    );
+
+    expect(screen.queryByDisplayValue('First/Second')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByPlaceholderText(placeholder));
+    userEvent.click(screen.getByText('First'));
+    userEvent.click(screen.getByText('Second'));
+
+    expect(screen.getByDisplayValue(`First${separator}Second`)).toBeInTheDocument();
+  });
+
+  it('displays last level selected when displayAllSelectedLevels is false', () => {
+    render(
+      <Cascader displayAllSelectedLevels={false} placeholder={placeholder} options={options} onSelect={() => {}} />
+    );
+
+    userEvent.click(screen.getByPlaceholderText(placeholder));
+    userEvent.click(screen.getByText('First'));
+    userEvent.click(screen.getByText('Second'));
+
+    expect(screen.getByDisplayValue('Second')).toBeInTheDocument();
+  });
+
+  it('displays last level selected when displayAllSelectedLevels is not passed in', () => {
+    render(<Cascader placeholder={placeholder} options={options} onSelect={() => {}} />);
+
+    userEvent.click(screen.getByPlaceholderText(placeholder));
+    userEvent.click(screen.getByText('First'));
+    userEvent.click(screen.getByText('Second'));
+
+    expect(screen.getByDisplayValue('Second')).toBeInTheDocument();
   });
 });
