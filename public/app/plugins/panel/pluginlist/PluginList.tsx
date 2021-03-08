d18df30b767fd1f9936057dafcee58a3ad2ba442
kay delaney <kay@grafana.com>
2021-03-08 14:18:58 +0000

    Use contextSrv and use margin-bottom instead of gap

--- public/app/plugins/panel/pluginlist/PluginList.tsx
+++ public/app/plugins/panel/pluginlist/PluginList.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { GrafanaTheme, PanelProps, PluginMeta, PluginType } from '@grafana/data';
-import { config } from '@grafana/runtime';
 import { useAsync } from 'react-use';
-import { getBackendSrv } from 'app/core/services/backend_srv';
-import { UserDTO } from 'app/types';
-import { CustomScrollbar, ModalsController, stylesFactory, Tooltip, useStyles } from '@grafana/ui';
-import { UpdatePluginModal } from './components/UpdatePluginModal';
-import UserProvider from 'app/core/utils/UserProvider';
 import { css, cx } from 'emotion';
+import { GrafanaTheme, PanelProps, PluginMeta, PluginType } from '@grafana/data';
+import { CustomScrollbar, ModalsController, stylesFactory, Tooltip, useStyles } from '@grafana/ui';
+import { contextSrv } from 'app/core/services/context_srv';
+import { getBackendSrv } from 'app/core/services/backend_srv';
+import { UpdatePluginModal } from './components/UpdatePluginModal';
 
 export function PluginList(props: PanelProps) {
   const pluginState = useAsync(async () => {
@@ -24,6 +22,7 @@ export function PluginList(props: PanelProps) {
   }, []);
 
   const styles = useStyles(getStyles);
+  const isAdmin = contextSrv.user.isGrafanaAdmin;
 
   if (pluginState.loading || pluginState.value === undefined) {
     return null;
@@ -40,45 +39,34 @@ export function PluginList(props: PanelProps) {
                 <img src={plugin.info.logos.small} className={styles.image} width="17" height="17" alt="" />
                 <span className={styles.title}>{plugin.name}</span>
                 <span className={styles.version}>v{plugin.info.version}</span>
-                <UserProvider userId={config.bootData.user.id}>
-                  {(api: any, states: any, teams: any, orgs: any, sessions: any, user?: UserDTO) => {
-                    if (!user?.isGrafanaAdmin) {
-                      return <></>;
-                    }
+                {isAdmin &&
+                  (plugin.hasUpdate ? (
+                    <ModalsController>
+                      {({ showModal, hideModal }) => (
+                        <Tooltip content={`New version: ${plugin.latestVersion}`} placement="top">
+                          <span
+                            className={cx(styles.message, styles.messageUpdate)}
+                            onClick={(e) => {
+                              e.preventDefault();
 
-                    return (
-                      <>
-                        {plugin.hasUpdate ? (
-                          <ModalsController>
-                            {({ showModal, hideModal }) => (
-                              <Tooltip content={`New version: ${plugin.latestVersion}`} placement="top">
-                                <span
-                                  className={cx(styles.message, styles.messageUpdate)}
-                                  onClick={(e) => {
-                                    e.preventDefault();
-
-                                    showModal(UpdatePluginModal, {
-                                      pluginID: plugin.id,
-                                      pluginName: plugin.name,
-                                      onDismiss: hideModal,
-                                      isOpen: true,
-                                    });
-                                  }}
-                                >
-                                  Update available!
-                                </span>
-                              </Tooltip>
-                            )}
-                          </ModalsController>
-                        ) : plugin.enabled ? (
-                          <span className={cx(styles.message, styles.messageNoUpdate)}>Up to date</span>
-                        ) : (
-                          <span className={cx(styles.message, styles.messageEnable)}>Enable now</span>
-                        )}
-                      </>
-                    );
-                  }}
-                </UserProvider>
+                              showModal(UpdatePluginModal, {
+                                pluginID: plugin.id,
+                                pluginName: plugin.name,
+                                onDismiss: hideModal,
+                                isOpen: true,
+                              });
+                            }}
+                          >
+                            Update available!
+                          </span>
+                        </Tooltip>
+                      )}
+                    </ModalsController>
+                  ) : plugin.enabled ? (
+                    <span className={cx(styles.message, styles.messageNoUpdate)}>Up to date</span>
+                  ) : (
+                    <span className={cx(styles.message, styles.messageEnable)}>Enable now</span>
+                  ))}
               </a>
             ))}
 
@@ -100,11 +88,13 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => ({
   pluginList: css`
     display: flex;
     flex-direction: column;
-    gap: ${theme.spacing.d};
   `,
   section: css`
     display: flex;
     flex-direction: column;
+    &:not(:last-of-type) {
+      margin-bottom: 16px;
+    }
   `,
   sectionHeader: css`
     color: ${theme.colors.textWeak};
@@ -123,7 +113,7 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => ({
   `,
   item: css`
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
     cursor: pointer;
     margin: ${theme.spacing.xxs};
