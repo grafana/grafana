9d46c9c68fb8982655959314e5b0e02c3cb59e1f
Torkel Ödegaard <torkel@grafana.com>
2021-03-08 12:55:53 +0100

    Fixing ts issues and tests

--- public/app/features/playlist/playlist_srv.ts
+++ public/app/features/playlist/playlist_srv.ts
@@ -5,10 +5,10 @@ import _ from 'lodash';
 import coreModule from '../../core/core_module';
 import appEvents from 'app/core/app_events';
 
-import { store } from 'app/store/store';
 import { CoreEvents } from 'app/types';
 import { getBackendSrv, locationService } from '@grafana/runtime';
 import { locationUtil, urlUtil, rangeUtil } from '@grafana/data';
+import { Location } from 'history';
 
 export const queryParamsToPreserve: { [key: string]: boolean } = {
   kiosk: true,
@@ -23,12 +23,15 @@ export class PlaylistSrv {
   private interval: number;
   private startUrl: string;
   private numberOfLoops = 0;
-  private storeUnsub: () => void;
   private validPlaylistUrl: string;
+  private locationListenerUnsub?: () => void;
+
   isPlaying: boolean;
 
   /** @ngInject */
-  constructor(private $timeout: any) {}
+  constructor(private $timeout: any) {
+    this.locationUpdated = this.locationUpdated.bind(this);
+  }
 
   next() {
     this.$timeout.cancel(this.cancelPromise);
@@ -51,13 +54,11 @@ export class PlaylistSrv {
     const filteredParams = _.pickBy(queryParams, (value: any, key: string) => queryParamsToPreserve[key]);
     const nextDashboardUrl = locationUtil.stripBaseFromUrl(dash.url);
 
-    // this is done inside timeout to make sure digest happens after
-    // as this can be called from react
-    locationService.push(nextDashboardUrl + '?' + urlUtil.toUrlParams(filteredParams));
-
     this.index++;
     this.validPlaylistUrl = nextDashboardUrl;
     this.cancelPromise = this.$timeout(() => this.next(), this.interval);
+
+    locationService.push(nextDashboardUrl + '?' + urlUtil.toUrlParams(filteredParams));
   }
 
   prev() {
@@ -66,10 +67,9 @@ export class PlaylistSrv {
   }
 
   // Detect url changes not caused by playlist srv and stop playlist
-  storeUpdated() {
-    const location = locationService.getLocation();
-
+  locationUpdated(location: Location) {
     if (location.pathname !== this.validPlaylistUrl) {
+      console.log('asd', location.pathname, this.validPlaylistUrl);
       this.stop();
     }
   }
@@ -82,8 +82,7 @@ export class PlaylistSrv {
     this.isPlaying = true;
 
     // setup location tracking
-    this.storeUnsub = store.subscribe(() => this.storeUpdated());
-    this.validPlaylistUrl = locationService.getLocation().pathname;
+    this.locationListenerUnsub = locationService.getHistory().listen(this.locationUpdated);
 
     appEvents.emit(CoreEvents.playlistStarted);
 
@@ -111,8 +110,8 @@ export class PlaylistSrv {
     this.index = 0;
     this.isPlaying = false;
 
-    if (this.storeUnsub) {
-      this.storeUnsub();
+    if (this.locationListenerUnsub) {
+      this.locationListenerUnsub();
     }
 
     if (this.cancelPromise) {
