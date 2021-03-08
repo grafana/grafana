9e7d214d046f139339bde58e28b45fdb63c3e368
Emil Tullstedt <emil.tullstedt@grafana.com>
2021-03-08 16:31:10 +0100

    Move evaluator to separate file

---
+++ pkg/services/rbac/evaluator.go
@@ -0,0 +1,71 @@
+package rbac
+
+import (
+	"context"
+
+	"github.com/gobwas/glob"
+
+	"github.com/grafana/grafana/pkg/models"
+)
+
+func (ac *RBACService) Evaluate(ctx context.Context, user *models.SignedInUser, permission string, scope ...string) (bool, error) {
+	roles := []string{string(user.OrgRole)}
+	for _, role := range user.OrgRole.Children() {
+		roles = append(roles, string(role))
+	}
+	if user.IsGrafanaAdmin {
+		roles = append(roles, "Grafana Admin")
+	}
+
+	res, err := ac.GetUserPermissions(ctx, GetUserPermissionsQuery{
+		OrgId:  user.OrgId,
+		UserId: user.UserId,
+		Roles:  roles,
+	})
+	if err != nil {
+		return false, err
+	}
+
+	ok, dbScopes := extractPermission(res, permission)
+	if !ok {
+		return false, nil
+	}
+
+	for _, s := range scope {
+		var match bool
+		for dbScope := range dbScopes {
+			rule, err := glob.Compile(dbScope, ':', '/')
+			if err != nil {
+				return false, err
+			}
+
+			match = rule.Match(s)
+			if match {
+				break
+			}
+		}
+
+		if !match {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func extractPermission(permissions []*Permission, permission string) (bool, map[string]struct{}) {
+	scopes := map[string]struct{}{}
+	ok := false
+
+	for _, p := range permissions {
+		if p == nil {
+			continue
+		}
+		if p.Permission == permission {
+			ok = true
+			scopes[p.Scope] = struct{}{}
+		}
+	}
+
+	return ok, scopes
+}
--- pkg/services/rbac/rbac.go
+++ pkg/services/rbac/rbac.go
@@ -3,11 +3,8 @@ package rbac
 import (
 	"context"
 
-	"github.com/gobwas/glob"
-
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/registry"
 	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
@@ -55,66 +52,4 @@ func (ac *RBACService) AddMigration(mg *migrator.Migrator) {
 	}
 
 	addRBACMigrations(mg)
-}
-
-func (ac *RBACService) Evaluate(ctx context.Context, user *models.SignedInUser, permission string, scope ...string) (bool, error) {
-	roles := []string{string(user.OrgRole)}
-	for _, role := range user.OrgRole.Children() {
-		roles = append(roles, string(role))
-	}
-	if user.IsGrafanaAdmin {
-		roles = append(roles, "Grafana Admin")
-	}
-
-	res, err := ac.GetUserPermissions(ctx, GetUserPermissionsQuery{
-		OrgId:  user.OrgId,
-		UserId: user.UserId,
-		Roles:  roles,
-	})
-	if err != nil {
-		return false, err
-	}
-
-	ok, dbScopes := extractPermission(res, permission)
-	if !ok {
-		return false, nil
-	}
-
-	for _, s := range scope {
-		var match bool
-		for dbScope := range dbScopes {
-			rule, err := glob.Compile(dbScope, ':', '/')
-			if err != nil {
-				return false, err
-			}
-
-			match = rule.Match(s)
-			if match {
-				break
-			}
-		}
-
-		if !match {
-			return false, nil
-		}
-	}
-
-	return true, nil
-}
-
-func extractPermission(permissions []*Permission, permission string) (bool, map[string]struct{}) {
-	scopes := map[string]struct{}{}
-	ok := false
-
-	for _, p := range permissions {
-		if p == nil {
-			continue
-		}
-		if p.Permission == permission {
-			ok = true
-			scopes[p.Scope] = struct{}{}
-		}
-	}
-
-	return ok, scopes
 }
