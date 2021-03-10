fee0b6683cf0f2427f7b170466e170490bbf84c5
Peter Holmberg <peter.hlmbrg@gmail.com>
2021-03-10 17:06:28 +0100

    moving expressions to components

--- public/app/features/expressions/types.ts
+++ public/app/features/expressions/types.ts
@@ -1,10 +1,39 @@
-import { DataQuery } from '@grafana/data';
+import { DataQuery, ReducerID, SelectableValue } from '@grafana/data';
 
 export enum GELQueryType {
   math = 'math',
   reduce = 'reduce',
   resample = 'resample',
+  classic = 'classic',
 }
+
+export const gelTypes: Array<SelectableValue<GELQueryType>> = [
+  { value: GELQueryType.math, label: 'Math' },
+  { value: GELQueryType.reduce, label: 'Reduce' },
+  { value: GELQueryType.resample, label: 'Resample' },
+  { value: GELQueryType.classic, label: 'Classic condition' },
+];
+
+export const reducerTypes: Array<SelectableValue<string>> = [
+  { value: ReducerID.min, label: 'Min', description: 'Get the minimum value' },
+  { value: ReducerID.max, label: 'Max', description: 'Get the maximum value' },
+  { value: ReducerID.mean, label: 'Mean', description: 'Get the average value' },
+  { value: ReducerID.sum, label: 'Sum', description: 'Get the sum of all values' },
+  { value: ReducerID.count, label: 'Count', description: 'Get the number of values' },
+];
+
+export const downsamplingTypes: Array<SelectableValue<string>> = [
+  { value: ReducerID.min, label: 'Min', description: 'Fill with the minimum value' },
+  { value: ReducerID.max, label: 'Max', description: 'Fill with the maximum value' },
+  { value: ReducerID.mean, label: 'Mean', description: 'Fill with the average value' },
+  { value: ReducerID.sum, label: 'Sum', description: 'Fill with the sum of all values' },
+];
+
+export const upsamplingTypes: Array<SelectableValue<string>> = [
+  { value: 'pad', label: 'pad', description: 'fill with the last known value' },
+  { value: 'backfilling', label: 'backfilling', description: 'fill with the next known value' },
+  { value: 'fillna', label: 'fillna', description: 'Fill with NaNs' },
+];
 
 /**
  * For now this is a single object to cover all the types.... would likely
