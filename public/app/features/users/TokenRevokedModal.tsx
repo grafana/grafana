b3fea459181b8c9d3702b5042950da0e8af6ddd1
Joan López de la Franca Beltran <joanjan14@gmail.com>
2021-03-10 19:19:09 +0100

    Update TokenRevokedModal style

--- public/app/features/users/TokenRevokedModal.tsx
+++ public/app/features/users/TokenRevokedModal.tsx
@@ -1,19 +1,54 @@
 import React from 'react';
-import { Button, Modal } from '@grafana/ui';
-import config from 'app/core/config';
+import { Button, InfoBox, Portal, stylesFactory, useTheme } from '@grafana/ui';
+import { getModalStyles } from '@grafana/ui/src/components/Modal/getModalStyles';
+import { css } from 'emotion';
 
-export const TokenRevokedModal = () => {
+const getStyles = stylesFactory(() => {
+  return {
+    infobox: css`
+      margin-bottom: 0;
+    `,
+  };
+});
+
+interface Props {
+  maxConcurrentSessions?: number;
+}
+
+export const TokenRevokedModal = (props: Props) => {
+  const theme = useTheme();
+
+  const styles = getStyles();
+  const modalStyles = getModalStyles(theme);
+
   const redirectToLogin = () => {
-    window.location.href = config.appSubUrl + '/';
+    window.location.reload();
   };
 
   return (
-    <Modal title="Token revoked" isOpen={true} onDismiss={redirectToLogin} onClickBackdrop={() => {}}>
-      <p>Your token has been revoked because you have reached the maximum amount of concurrent sessions.</p>
-      <p>Please, log in again.</p>
-      <Button size="md" variant="primary" onClick={redirectToLogin}>
-        Go to login
-      </Button>
-    </Modal>
+    <Portal>
+      <div className={modalStyles.modal}>
+        <InfoBox title="You have been automatically logged out" severity="warning" className={styles.infobox}>
+          <p>
+            Your session token was automatically revoked because you have reached
+            <strong>
+              {' '}
+              the maximum number of
+              {props.maxConcurrentSessions && <span> {props.maxConcurrentSessions} </span>}
+              concurrent sessions{' '}
+            </strong>
+            for your account.
+          </p>
+          <p>
+            <strong>You can sign in again to resume your session. </strong>
+            Contact your administrator or visit license page to review your quota if you are logged out repeatedly.
+          </p>
+          <Button size="sm" variant="primary" onClick={redirectToLogin}>
+            Sign in
+          </Button>
+        </InfoBox>
+      </div>
+      <div className={modalStyles.modalBackdrop} />
+    </Portal>
   );
 };
