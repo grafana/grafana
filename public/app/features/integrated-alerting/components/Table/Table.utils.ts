ded73e3c05105354acf13d13b69e703a106b4434
fabio-silva <ffjs1993@gmail.com>
2021-03-17 14:03:57 +0000

    Revert "Fix bad PAGE_SIZES import (#93)" (#94)
    This reverts commit 2a4e292fe89c18fe4bc8875dd2849595009fe324.

--- public/app/features/integrated-alerting/components/Table/Table.utils.ts
+++ public/app/features/integrated-alerting/components/Table/Table.utils.ts
@@ -1,4 +1,4 @@
-import { PAGE_SIZES } from './Pagination';
+import { PAGE_SIZES } from './Table.constants';
 
 export const getProperPageSize = (pageSize: number) =>
   PAGE_SIZES.find(p => p.value === pageSize) ? pageSize : PAGE_SIZES[0].value;
