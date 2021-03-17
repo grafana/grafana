e6db87be5c4255521822e3a14b87d6fd254f7c88
Owen Diehl <ow.diehl@gmail.com>
2021-03-17 13:10:40 -0400

    Data Source Proxy: Migrate proxy to its own service and make more extensible (#31927)
    * datasource proxy extensions
    
    * revert normalresponse extensions
    
    * data proxy service impl
    
    * lint
    
    * api datasource proxy method
    
    * moves datasource proxy tests into correct new pkg

--- pkg/api/dataproxy.go
+++ pkg/api/dataproxy.go
@@ -1,64 +1,7 @@
 package api
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-	"regexp"
+import "github.com/grafana/grafana/pkg/models"
 
-	"github.com/grafana/grafana/pkg/api/datasource"
-	"github.com/grafana/grafana/pkg/api/pluginproxy"
-	"github.com/grafana/grafana/pkg/infra/metrics"
-	"github.com/grafana/grafana/pkg/models"
-)
-
-// ProxyDataSourceRequest proxies datasource requests
 func (hs *HTTPServer) ProxyDataSourceRequest(c *models.ReqContext) {
-	c.TimeRequest(metrics.MDataSourceProxyReqTimer)
-
-	dsID := c.ParamsInt64(":id")
-	ds, err := hs.DatasourceCache.GetDatasource(dsID, c.SignedInUser, c.SkipCache)
-	if err != nil {
-		if errors.Is(err, models.ErrDataSourceAccessDenied) {
-			c.JsonApiErr(http.StatusForbidden, "Access denied to datasource", err)
-			return
-		}
-		c.JsonApiErr(http.StatusInternalServerError, "Unable to load datasource meta data", err)
-		return
-	}
-
-	err = hs.PluginRequestValidator.Validate(ds.Url, c.Req.Request)
-	if err != nil {
-		c.JsonApiErr(http.StatusForbidden, "Access denied", err)
-		return
-	}
-
-	// find plugin
-	plugin := hs.PluginManager.GetDataSource(ds.Type)
-	if plugin == nil {
-		c.JsonApiErr(http.StatusInternalServerError, "Unable to find datasource plugin", err)
-		return
-	}
-
-	proxyPath := getProxyPath(c)
-	proxy, err := pluginproxy.NewDataSourceProxy(ds, plugin, c, proxyPath, hs.Cfg)
-	if err != nil {
-		if errors.Is(err, datasource.URLValidationError{}) {
-			c.JsonApiErr(http.StatusBadRequest, fmt.Sprintf("Invalid data source URL: %q", ds.Url), err)
-		} else {
-			c.JsonApiErr(http.StatusInternalServerError, "Failed creating data source proxy", err)
-		}
-		return
-	}
-	proxy.HandleRequest()
-}
-
-var proxyPathRegexp = regexp.MustCompile(`^\/api\/datasources\/proxy\/[\d]+\/?`)
-
-func extractProxyPath(originalRawPath string) string {
-	return proxyPathRegexp.ReplaceAllString(originalRawPath, "")
-}
-
-func getProxyPath(c *models.ReqContext) string {
-	return extractProxyPath(c.Req.URL.EscapedPath())
+	hs.DataProxy.ProxyDataSourceRequest(c)
 }
