ec26d8df0f02a75d888968fbd8f166c25aa4a0ca
Marcus Andersson <marcus.andersson@grafana.com>
2021-03-17 16:19:52 +0100

    adding examples on loading indicator in storybook.

--- packages/grafana-ui/src/components/PanelChrome/PanelChrome.story.tsx
+++ packages/grafana-ui/src/components/PanelChrome/PanelChrome.story.tsx
@@ -1,10 +1,10 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useState } from 'react';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
-import { PanelChrome, useTheme } from '@grafana/ui';
-import { PanelChromeProps } from './PanelChrome';
+import { PanelChrome, useTheme, PanelChromeProps } from '@grafana/ui';
 import { HorizontalGroup, VerticalGroup } from '../Layout/Layout';
 import { merge } from 'lodash';
 import { GrafanaTheme } from '@grafana/data';
+import { useInterval } from 'react-use';
 
 export default {
   title: 'Visualizations/PanelChrome',
@@ -43,6 +43,9 @@ function renderPanel(name: string, overrides: Partial<PanelChromeProps>, theme: 
 
 export const StandardPanel = () => {
   const theme = useTheme();
+  const [loading, setLoading] = useState(true);
+
+  useInterval(() => setLoading(true), 5000);
 
   return (
     <div style={{ background: theme.colors.dashboardBg, padding: 100 }}>
@@ -56,6 +59,41 @@ export const StandardPanel = () => {
           {renderPanel(
             'Very long title',
             { title: 'Very long title that should get ellipsis when there is no more space' },
+            theme
+          )}
+        </VerticalGroup>
+      </HorizontalGroup>
+      <div style={{ marginTop: theme.spacing.md }} />
+      <HorizontalGroup spacing="md">
+        <VerticalGroup spacing="md">
+          {renderPanel(
+            'No title and loading indicator',
+            {
+              title: '',
+              leftItems: [
+                <PanelChrome.LoadingIndicator
+                  loading={loading}
+                  onCancel={() => setLoading(false)}
+                  key="loading-indicator"
+                />,
+              ],
+            },
+            theme
+          )}
+        </VerticalGroup>
+        <VerticalGroup spacing="md">
+          {renderPanel(
+            'Very long title',
+            {
+              title: 'Very long title that should get ellipsis when there is no more space',
+              leftItems: [
+                <PanelChrome.LoadingIndicator
+                  loading={loading}
+                  onCancel={() => setLoading(false)}
+                  key="loading-indicator"
+                />,
+              ],
+            },
             theme
           )}
         </VerticalGroup>
--- packages/grafana-ui/src/components/PanelChrome/index.ts
+++ packages/grafana-ui/src/components/PanelChrome/index.ts
@@ -1,10 +1,21 @@
 import React from 'react';
 import { LoadingIndicator } from './LoadingIndicator';
-import { PanelChromeProps, PanelChrome as PanelChromeComponent } from './PanelChrome';
+import { PanelChrome as PanelChromeComponent, PanelChromeProps } from './PanelChrome';
 
+/**
+ * @beta
+ */
+export { PanelChromeProps } from './PanelChrome';
+
+/**
+ * @beta
+ */
 export interface PanelChromeType extends React.FC<PanelChromeProps> {
   LoadingIndicator: typeof LoadingIndicator;
 }
 
+/**
+ * @beta
+ */
 export const PanelChrome = PanelChromeComponent as PanelChromeType;
 PanelChrome.LoadingIndicator = LoadingIndicator;
