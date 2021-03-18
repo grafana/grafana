a2eda798e7e121904056c039325497d1f9d3af33
Arve Knudsen <arve.knudsen@gmail.com>
2021-03-18 13:53:01 +0100

    PluginManager: Make remaining plugin state non-global (#32094)
    * PluginDashboards: Use plugin manager interface
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * PluginManager: Make panels non-global
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * PluginManager: Make apps non-global
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * PluginManager: Make static routes non-global
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * PluginManager: Make pluginTypes non-global
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/api/fakes.go
+++ pkg/api/fakes.go
@@ -4,6 +4,8 @@ import "github.com/grafana/grafana/pkg/plugins"
 
 type fakePluginManager struct {
 	plugins.Manager
+
+	staticRoutes []*plugins.PluginStaticRoute
 }
 
 func (pm *fakePluginManager) GetPlugin(id string) *plugins.PluginBase {
@@ -16,4 +18,8 @@ func (pm *fakePluginManager) GetDataSource(id string) *plugins.DataSourcePlugin 
 
 func (pm *fakePluginManager) Renderer() *plugins.RendererPlugin {
 	return nil
+}
+
+func (pm *fakePluginManager) StaticRoutes() []*plugins.PluginStaticRoute {
+	return pm.staticRoutes
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -29,7 +29,7 @@ import (
 	"github.com/grafana/grafana/pkg/login"
 	"github.com/grafana/grafana/pkg/login/social"
 	"github.com/grafana/grafana/pkg/middleware"
-	_ "github.com/grafana/grafana/pkg/plugins"
+	_ "github.com/grafana/grafana/pkg/plugins/manager"
 	"github.com/grafana/grafana/pkg/registry"
 	_ "github.com/grafana/grafana/pkg/services/alerting"
 	_ "github.com/grafana/grafana/pkg/services/auth"
