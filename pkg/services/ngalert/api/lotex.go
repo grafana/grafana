0fdfe15fe0b52746a0835b0fc553c17f44a4fdbe
Owen Diehl <ow.diehl@gmail.com>
2021-03-18 21:05:24 -0400

    working proxy

--- pkg/services/ngalert/api/lotex.go
+++ pkg/services/ngalert/api/lotex.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,7 +11,7 @@ import (
 
 	apimodels "github.com/grafana/alerting-api/pkg/api"
 	"gopkg.in/macaron.v1"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 
 	"github.com/grafana/grafana/pkg/api/response"
 	"github.com/grafana/grafana/pkg/infra/log"
@@ -37,82 +38,138 @@ func (w *safeMacaronWrapper) CloseNotify() <-chan bool {
 	return make(chan bool)
 }
 
-// withBufferedRW is a hack to work around the type signature exposed by the datasource proxy
-// which uses the underlying ResponseWriter vs what we need to expose implementing the API services
-// (which return a response.Response). Therefore, we replace the response writer so that we can return it.
-func withBufferedRW(ctx *models.ReqContext) (*models.ReqContext, response.Response) {
+// replacedResponseWriter overwrites the underlying responsewriter used by a *models.ReqContext.
+// It's ugly because it needs to replace a value behind a few nested pointers.
+func replacedResponseWriter(ctx *models.ReqContext) (*models.ReqContext, *response.NormalResponse) {
 	resp := response.CreateNormalResponse(make(http.Header), nil, 0)
 	cpy := *ctx
-
-	cpy.Resp = macaron.NewResponseWriter(ctx.Req.Method, &safeMacaronWrapper{resp})
+	cpyMCtx := *cpy.Context
+	cpyMCtx.Resp = macaron.NewResponseWriter(ctx.Req.Method, &safeMacaronWrapper{resp})
+	cpy.Context = &cpyMCtx
 	return &cpy, resp
 }
 
 // withReq proxies a different request
-func (r *LotexRuler) withReq(ctx *models.ReqContext, req *http.Request) response.Response {
-	newCtx, resp := withBufferedRW(ctx)
+func (r *LotexRuler) withReq(
+	ctx *models.ReqContext,
+	req *http.Request,
+	extractor func([]byte) (interface{}, error),
+) response.Response {
+	newCtx, resp := replacedResponseWriter(ctx)
 	newCtx.Req.Request = req
 	r.DataProxy.ProxyDatasourceRequestWithID(newCtx, ctx.ParamsInt64("DatasourceId"))
-	return resp
+
+	status := resp.Status()
+	if status >= 400 {
+		return response.Error(status, string(resp.Body()), nil)
+	}
+
+	t, err := extractor(resp.Body())
+	if err != nil {
+		return response.Error(500, err.Error(), nil)
+	}
+
+	b, err := json.Marshal(t)
+	if err != nil {
+		return response.Error(500, err.Error(), nil)
+	}
+
+	return response.JSON(status, b)
+}
+
+func yamlExtractor(v interface{}) func([]byte) (interface{}, error) {
+	return func(b []byte) (interface{}, error) {
+		decoder := yaml.NewDecoder(bytes.NewReader(b))
+		decoder.KnownFields(true)
+
+		err := decoder.Decode(v)
+
+		return v, err
+	}
+}
+
+func messageExtractor(b []byte) (interface{}, error) {
+	return map[string]string{"message": string(b)}, nil
 }
 
 func (r *LotexRuler) RouteDeleteNamespaceRulesConfig(ctx *models.ReqContext) response.Response {
-	return r.withReq(ctx, &http.Request{
-		URL: withPath(
-			*ctx.Req.URL,
-			fmt.Sprintf("/api/prom/rules/%s", ctx.Params("Namespace")),
-		),
-	})
+	return r.withReq(
+		ctx,
+		&http.Request{
+			Method: "DELETE",
+			URL: withPath(
+				*ctx.Req.URL,
+				fmt.Sprintf("/api/prom/rules/%s", ctx.Params("Namespace")),
+			),
+		},
+		messageExtractor,
+	)
 }
 
 func (r *LotexRuler) RouteDeleteRuleGroupConfig(ctx *models.ReqContext) response.Response {
-	return r.withReq(ctx, &http.Request{
-		URL: withPath(
-			*ctx.Req.URL,
-			fmt.Sprintf(
-				"%s/%s/%s",
-				legacyRulerPrefix,
-				ctx.Params("Namespace"),
-				ctx.Params("Groupname"),
+	return r.withReq(
+		ctx,
+		&http.Request{
+			Method: "DELETE",
+			URL: withPath(
+				*ctx.Req.URL,
+				fmt.Sprintf(
+					"%s/%s/%s",
+					legacyRulerPrefix,
+					ctx.Params("Namespace"),
+					ctx.Params("Groupname"),
+				),
 			),
-		),
-	})
+		},
+		messageExtractor,
+	)
 }
 
 func (r *LotexRuler) RouteGetNamespaceRulesConfig(ctx *models.ReqContext) response.Response {
-	return r.withReq(ctx, &http.Request{
-		URL: withPath(
-			*ctx.Req.URL,
-			fmt.Sprintf(
-				"%s/%s",
-				legacyRulerPrefix,
-				ctx.Params("Namespace"),
+	return r.withReq(
+		ctx, &http.Request{
+			URL: withPath(
+				*ctx.Req.URL,
+				fmt.Sprintf(
+					"%s/%s",
+					legacyRulerPrefix,
+					ctx.Params("Namespace"),
+				),
 			),
-		),
-	})
+		},
+		yamlExtractor(apimodels.NamespaceConfigResponse{}),
+	)
 }
 
 func (r *LotexRuler) RouteGetRulegGroupConfig(ctx *models.ReqContext) response.Response {
-	return r.withReq(ctx, &http.Request{
-		URL: withPath(
-			*ctx.Req.URL,
-			fmt.Sprintf(
-				"%s/%s/%s",
-				legacyRulerPrefix,
-				ctx.Params("Namespace"),
-				ctx.Params("Groupname"),
+	return r.withReq(
+		ctx,
+		&http.Request{
+			URL: withPath(
+				*ctx.Req.URL,
+				fmt.Sprintf(
+					"%s/%s/%s",
+					legacyRulerPrefix,
+					ctx.Params("Namespace"),
+					ctx.Params("Groupname"),
+				),
 			),
-		),
-	})
+		},
+		yamlExtractor(apimodels.RuleGroupConfigResponse{}),
+	)
 }
 
 func (r *LotexRuler) RouteGetRulesConfig(ctx *models.ReqContext) response.Response {
-	return r.withReq(ctx, &http.Request{
-		URL: withPath(
-			*ctx.Req.URL,
-			legacyRulerPrefix,
-		),
-	})
+	return r.withReq(
+		ctx,
+		&http.Request{
+			URL: withPath(
+				*ctx.Req.URL,
+				legacyRulerPrefix,
+			),
+		},
+		yamlExtractor(apimodels.NamespaceConfigResponse{}),
+	)
 }
 
 func (r *LotexRuler) RoutePostNameRulesConfig(ctx *models.ReqContext, conf apimodels.RuleGroupConfig) response.Response {
@@ -132,15 +189,12 @@ func (r *LotexRuler) RoutePostNameRulesConfig(ctx *models.ReqContext, conf apimo
 		Body:          body,
 		ContentLength: ln,
 	}
-	return r.withReq(ctx, req)
+	return r.withReq(ctx, req, messageExtractor)
 }
 
 func withPath(u url.URL, newPath string) *url.URL {
+	// TODO: handle path escaping
 	u.Path = newPath
-	if escaped := url.PathEscape(u.Path); escaped != u.Path {
-		u.RawPath = escaped
-	}
-
 	return &u
 }
 
