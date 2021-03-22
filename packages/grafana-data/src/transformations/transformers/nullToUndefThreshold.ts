8a56cf852d43f19a100c0a74df425c8eb0041b4e
Leon Sorokin <leeoniya@gmail.com>
2021-03-22 17:55:40 -0500

    more comment detail
    Co-authored-by: Ryan McKinley <ryantxu@gmail.com>

--- packages/grafana-data/src/transformations/transformers/nullToUndefThreshold.ts
+++ packages/grafana-data/src/transformations/transformers/nullToUndefThreshold.ts
@@ -1,4 +1,5 @@
 // mutates all nulls -> undefineds in the fieldValues array for value-less refValues ranges below maxThreshold
+// refValues is typically a time array and maxThreshold is the allowable distance between in time
 export function nullToUndefThreshold(refValues: number[], fieldValues: any[], maxThreshold: number): any[] {
   let prevRef;
   let nullIdx;
