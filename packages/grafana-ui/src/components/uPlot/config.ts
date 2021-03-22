a2be9e2a837905c26f1e20cf0dcb1b296a2b56ac
Ryan McKinley <ryantxu@gmail.com>
2021-03-22 08:40:00 -0700

    add a comment

--- packages/grafana-ui/src/components/uPlot/config.ts
+++ packages/grafana-ui/src/components/uPlot/config.ts
@@ -93,6 +93,12 @@ export interface LineConfig {
   lineWidth?: number;
   lineInterpolation?: LineInterpolation;
   lineStyle?: LineStyle;
+
+  /**
+   * Indicate if null values should be treated as gaps or connected.
+   * When the value is a number, it represents the maximum delta in the
+   * X axis that should be considered connected.  For timeseries, this is milliseconds
+   */
   spanNulls?: boolean | number;
 }
 
