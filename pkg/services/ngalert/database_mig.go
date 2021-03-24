a6ded642c399dc5aa7af4911c9187257535f3b10
gotjosh <josue@grafana.com>
2021-03-24 14:20:44 +0000

    Alerting: Fetch configuration from the database and run a notification service (#32175)
    * Alerting: Fetch configuration from the database and run a notification
    instance
    
    Co-Authored-By: Ganesh Vernekar <15064823+codesome@users.noreply.github.com>

--- pkg/services/ngalert/database_mig.go
+++ pkg/services/ngalert/database_mig.go
@@ -107,18 +107,3 @@ func alertInstanceMigration(mg *migrator.Migrator) {
 	mg.AddMigration("add index in alert_instance table on def_org_id, def_uid and current_state columns", migrator.NewAddIndexMigration(alertInstance, alertInstance.Indices[0]))
 	mg.AddMigration("add index in alert_instance table on def_org_id, current_state columns", migrator.NewAddIndexMigration(alertInstance, alertInstance.Indices[1]))
 }
-
-func alertmanagerConfigurationMigration(mg *migrator.Migrator) {
-	alertConfiguration := migrator.Table{
-		Name: "alert_configuration",
-		Columns: []*migrator.Column{
-			{Name: "id", Type: migrator.DB_BigInt, IsPrimaryKey: true, IsAutoIncrement: true},
-			{Name: "alertmanager_configuration", Type: migrator.DB_Text, Nullable: false},
-			{Name: "configuration_version", Type: migrator.DB_NVarchar, Length: 3}, // In a format of vXX e.g. v1, v2, v10, etc
-			{Name: "created_at", Type: migrator.DB_Int, Nullable: false},
-			{Name: "updated_at", Type: migrator.DB_Int, Nullable: false},
-		},
-	}
-
-	mg.AddMigration("create_alert_configuration_table", migrator.NewAddTableMigration(alertConfiguration))
-}
