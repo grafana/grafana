9d6c8f8512a1a271688639d7b2968d4d583627cf
Torkel Ödegaard <torkel@grafana.org>
2021-03-25 08:33:13 +0100

    PanelEdit: v8 Panel Edit UX  (#32124)
    * Initial commit
    
    * Progress
    
    * Update
    
    * Progress
    
    * updates
    
    * Minor fix
    
    * fixed ts issue
    
    * fixed e2e tests
    
    * More explorations
    
    * Making progress
    
    * Panel options and field options unified
    
    * With nested categories
    
    * Starting to find something
    
    * fix paddings
    
    * Progress
    
    * Breakthrough ux layout
    
    * Progress
    
    * Updates
    
    * New way of composing options with search
    
    * added regex search
    
    * Refactoring to react note tree
    
    * Show overrides
    
    * Adding overrides radio button support
    
    * Added popular view
    
    * Separate stat/gauge/bargauge options into value options and display options
    
    * Initial work on getting library panels into viz picker flow
    
    * Fixed issues switching to panel library panel
    
    * Move search input put of LibraryPanelsView
    
    * Changing design again to have content inside boxes
    
    * Style updates
    
    * Refactoring to fix scroll issue
    
    * Option category naming
    
    * Fixed FilterInput issue
    
    * Updated snapshots
    
    * Fix padding
    
    * Updated viz picker design
    
    * Unify library panel an viz picker card
    
    * Updated card with delete action
    
    * Major refactoring back to an object model instead of searching and filtering react node tree
    
    * More refactoring
    
    * Show option category in label when searching
    
    * Nice logic for categories rendering when searching or when only child
    
    * Make getSuggestions more lazy for DataLinksEditor
    
    * Add missing repeat options and handle conditional options
    
    * Prepping options category to be more flexibly and control state from outside
    
    * Added option count to search result
    
    * Minor style tweak
    
    * Added button to close viz picker
    
    * Rewrote overrides to enable searching overrides
    
    * New search engine and tests
    
    * Searching overrides works
    
    * Hide radio buttons while searching
    
    * Added angular options back
    
    * Added memoize for all options so they are not rebuilt for every search key stroke
    
    * Added back support for category counters
    
    * Started unit test work
    
    * Refactoring and base popular options list
    
    * Initial update to e2e test, more coming to add e2e test for search features
    
    * Minor fix
    
    * Review updates
    
    * Fixing category open states
    
    * Unit test progress
    
    * Do not show visualization list mode radio button if library panels is not enabled
    
    * Use boolean
    
    * More unit tests
    
    * Increase library panels per page count and give search focus when switching list mode
    
    * field config change test and search test
    
    * Feedback updates
    
    * Minor tweaks
    
    * Minor refactorings
    
    * More minimal override collapse state

---
+++ public/app/features/dashboard/components/VizTypePicker/PanelTypeCard.tsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import { GrafanaTheme, isUnsignedPluginSignature, PanelPluginMeta, PluginState } from '@grafana/data';
+import { Badge, BadgeProps, IconButton, PluginSignatureBadge, styleMixins, useStyles } from '@grafana/ui';
+import { css, cx } from 'emotion';
+import { selectors } from '@grafana/e2e-selectors';
+
+interface Props {
+  isCurrent: boolean;
+  plugin: PanelPluginMeta;
+  title: string;
+  onClick: () => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+  showBadge?: boolean;
+}
+
+export const PanelTypeCard: React.FC<Props> = ({
+  isCurrent,
+  title,
+  plugin,
+  onClick,
+  onDelete,
+  disabled,
+  showBadge,
+}) => {
+  const styles = useStyles(getStyles);
+  const cssClass = cx({
+    [styles.item]: true,
+    [styles.disabled]: disabled || plugin.state === PluginState.deprecated,
+    [styles.current]: isCurrent,
+  });
+
+  return (
+    <div
+      className={cssClass}
+      aria-label={selectors.components.PluginVisualization.item(plugin.name)}
+      onClick={disabled ? undefined : onClick}
+      title={isCurrent ? 'Click again to close this section' : plugin.name}
+    >
+      <img className={styles.img} src={plugin.info.logos.small} />
+
+      <div className={styles.itemContent}>
+        <div className={styles.name}>{title}</div>
+      </div>
+      {showBadge && (
+        <div className={cx(styles.badge, disabled && styles.disabled)}>
+          <PanelPluginBadge plugin={plugin} />
+        </div>
+      )}
+      {onDelete && (
+        <IconButton
+          name="trash-alt"
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+PanelTypeCard.displayName = 'PanelTypeCard';
+
+const getStyles = (theme: GrafanaTheme) => {
+  return {
+    item: css`
+      position: relative;
+      display: flex;
+      flex-shrink: 0;
+      cursor: pointer;
+      background: ${theme.colors.bg2};
+      border: 1px solid ${theme.colors.border2};
+      border-radius: ${theme.border.radius.sm};
+      align-items: center;
+      padding: 8px;
+      width: 100%;
+      position: relative;
+      overflow: hidden;
+      height: 55px;
+
+      &:hover {
+        background: ${styleMixins.hoverColor(theme.colors.bg2, theme)};
+      }
+    `,
+    itemContent: css`
+      position: relative;
+      width: 100%;
+    `,
+    current: css`
+      label: currentVisualizationItem;
+      border-color: ${theme.colors.bgBlue1};
+    `,
+    disabled: css`
+      opacity: 0.2;
+      filter: grayscale(1);
+      cursor: default;
+      pointer-events: none;
+
+      &:hover {
+        border: 1px solid ${theme.colors.border2};
+      }
+    `,
+    name: css`
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+      font-size: ${theme.typography.size.sm};
+      font-weight: ${theme.typography.weight.semibold};
+      padding: 0 10px;
+      width: 100%;
+    `,
+    img: css`
+      max-height: 38px;
+      width: 38px;
+      display: flex;
+      align-items: center;
+    `,
+    badge: css`
+      background: ${theme.colors.bg1};
+    `,
+  };
+};
+
+interface PanelPluginBadgeProps {
+  plugin: PanelPluginMeta;
+}
+
+const PanelPluginBadge: React.FC<PanelPluginBadgeProps> = ({ plugin }) => {
+  const display = getPanelStateBadgeDisplayModel(plugin);
+
+  if (isUnsignedPluginSignature(plugin.signature)) {
+    return <PluginSignatureBadge status={plugin.signature} />;
+  }
+
+  if (!display) {
+    return null;
+  }
+
+  return <Badge color={display.color} text={display.text} icon={display.icon} tooltip={display.tooltip} />;
+};
+
+function getPanelStateBadgeDisplayModel(panel: PanelPluginMeta): BadgeProps | null {
+  switch (panel.state) {
+    case PluginState.deprecated:
+      return {
+        text: 'Deprecated',
+        color: 'red',
+        tooltip: `${panel.name} panel is deprecated`,
+      };
+    case PluginState.alpha:
+      return {
+        text: 'Alpha',
+        color: 'blue',
+        tooltip: `${panel.name} panel is experimental`,
+      };
+    case PluginState.beta:
+      return {
+        text: 'Beta',
+        color: 'blue',
+        tooltip: `${panel.name} panel is in beta`,
+      };
+    default:
+      return null;
+  }
+}
+
+PanelPluginBadge.displayName = 'PanelPluginBadge';
