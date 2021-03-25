9ccd87dd936a45ab5c3fe11bc2448b75b3b59aad
David Parrott <stomp.box.yo@gmail.com>
2021-03-25 12:00:05 -0700

    Add evaluation time and test
    Add evaluation time and test

--- pkg/services/ngalert/state/state_tracker.go
+++ pkg/services/ngalert/state/state_tracker.go
@@ -4,17 +4,23 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/go-openapi/strfmt"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/grafana/pkg/services/ngalert/eval"
-	"github.com/grafana/grafana/pkg/services/ngalert/models"
+	ngModels "github.com/grafana/grafana/pkg/services/ngalert/models"
+	"github.com/grafana/grafana/pkg/services/ngalert/notifier"
+	promModels "github.com/prometheus/alertmanager/api/v2/models"
 )
 
 type AlertState struct {
-	UID     string
-	CacheId string
-	Labels  data.Labels
-	State   eval.State
-	Results []eval.State
+	UID         string
+	CacheId     string
+	Labels      data.Labels
+	State       eval.State
+	Results     []eval.State
+	StartsAt    strfmt.DateTime
+	EndsAt      strfmt.DateTime
+	EvaluatedAt strfmt.DateTime
 }
 
 type cache struct {
@@ -44,57 +50,82 @@ func (c *cache) getOrCreate(uid string, result eval.Result) AlertState {
 		return state
 	}
 	newState := AlertState{
-		UID:     uid,
-		CacheId: idString,
-		Labels:  result.Instance,
-		State:   result.State,
-		Results: []eval.State{result.State},
+		UID:         uid,
+		CacheId:     idString,
+		Labels:      result.Instance,
+		State:       result.State,
+		Results:     []eval.State{},
+		EvaluatedAt: strfmt.DateTime(result.EvaluatedAt),
 	}
 	c.cacheMap[idString] = newState
 	return newState
 }
 
-func (c *cache) update(stateEntry AlertState) {
+func (c *cache) set(stateEntry AlertState) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.cacheMap[stateEntry.CacheId] = stateEntry
 }
 
-func (c *cache) getStateForEntry(stateId string) eval.State {
+func (c *cache) get(stateId string) AlertState {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	return c.cacheMap[stateId].State
+	return c.cacheMap[stateId]
 }
 
-func (st *StateTracker) ProcessEvalResults(uid string, results eval.Results, condition models.Condition) []AlertState {
+func (st *StateTracker) ProcessEvalResults(uid string, results eval.Results, condition ngModels.Condition) []AlertState {
 	var changedStates []AlertState
 	for _, result := range results {
-		currentState := st.stateCache.getOrCreate(uid, result)
-		currentState.Results = append(currentState.Results, result.State)
-		newState := st.getNextState(uid, result)
-		if newState != currentState.State {
-			currentState.State = newState
-			changedStates = append(changedStates, currentState)
+		if s, ok := st.setNextState(uid, result); ok {
+			changedStates = append(changedStates, s)
 		}
-		st.stateCache.update(currentState)
 	}
 	return changedStates
 }
 
-func (st *StateTracker) getNextState(uid string, result eval.Result) eval.State {
+//Set the current state based on evaluation results
+//return the state and a bool indicating whether a state transition occurred
+func (st *StateTracker) setNextState(uid string, result eval.Result) (AlertState, bool) {
 	currentState := st.stateCache.getOrCreate(uid, result)
-	if currentState.State == result.State {
-		return currentState.State
-	}
 
 	switch {
 	case currentState.State == result.State:
-		return currentState.State
+		currentState.EvaluatedAt = strfmt.DateTime(result.EvaluatedAt)
+		currentState.Results = append(currentState.Results, result.State)
+		st.stateCache.set(currentState)
+		return currentState, false
 	case currentState.State == eval.Normal && result.State == eval.Alerting:
-		return eval.Alerting
+		currentState.State = eval.Alerting
+		currentState.EvaluatedAt = strfmt.DateTime(result.EvaluatedAt)
+		currentState.StartsAt = strfmt.DateTime(result.EvaluatedAt)
+		currentState.Results = append(currentState.Results, result.State)
+		st.stateCache.set(currentState)
+		return currentState, true
 	case currentState.State == eval.Alerting && result.State == eval.Normal:
-		return eval.Normal
+		currentState.State = eval.Normal
+		currentState.EvaluatedAt = strfmt.DateTime(result.EvaluatedAt)
+		currentState.EndsAt = strfmt.DateTime(result.EvaluatedAt)
+		currentState.Results = append(currentState.Results, result.State)
+		st.stateCache.set(currentState)
+		return currentState, true
 	default:
-		return eval.Alerting
+		return currentState, false
 	}
+}
+
+func FromAlertStateToPostableAlerts(firingStates []AlertState) []*notifier.PostableAlert {
+	alerts := make([]*notifier.PostableAlert, 0, len(firingStates))
+	for _, state := range firingStates {
+		alerts = append(alerts, &notifier.PostableAlert{
+			PostableAlert: promModels.PostableAlert{
+				Annotations: promModels.LabelSet{},
+				StartsAt:    state.StartsAt,
+				EndsAt:      state.EndsAt,
+				Alert: promModels.Alert{
+					Labels: promModels.LabelSet(state.Labels),
+				},
+			},
+		})
+	}
+	return alerts
 }
