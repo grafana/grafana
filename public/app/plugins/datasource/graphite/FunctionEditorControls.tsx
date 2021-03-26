4c7a53056683d9d659c558938329fe7abbb6e4e0
Piotr Jamróz <pm.jamroz@gmail.com>
2021-03-26 08:54:40 +0100

    Remove redundant console.log
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>

--- public/app/plugins/datasource/graphite/FunctionEditorControls.tsx
+++ public/app/plugins/datasource/graphite/FunctionEditorControls.tsx
@@ -25,7 +25,6 @@ const FunctionDescription = React.lazy(async () => {
   const { default: rst2html } = await import(/* webpackChunkName: "rst2html" */ 'rst2html');
   return {
     default(props: { description?: string }) {
-      console.log(rst2html(props.description ?? ''));
 
       return <div dangerouslySetInnerHTML={{ __html: rst2html(props.description ?? '') }} />;
     },
