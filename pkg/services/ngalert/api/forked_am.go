d9371475842e0269063ebe9c695543d5368dab55
Sofia Papagiannaki <papagian@users.noreply.github.com>
2021-03-29 18:18:25 +0300

    [Alerting] Forking alert manager API (#32300)
    * Alertmanager lotex ruler
    
    * Apply suggestions from code review

---
+++ pkg/services/ngalert/api/forked_am.go
@@ -0,0 +1,129 @@
+package api
+
+import (
+	"fmt"
+
+	apimodels "github.com/grafana/alerting-api/pkg/api"
+	"github.com/grafana/grafana/pkg/api/response"
+	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/datasources"
+)
+
+type ForkedAMSvc struct {
+	AMSvc, GrafanaSvc AlertmanagerApiService
+	DatasourceCache   datasources.CacheService
+}
+
+func NewForkedAM(datasourceCache datasources.CacheService, proxy, grafana AlertmanagerApiService) *ForkedAMSvc {
+	return &ForkedAMSvc{
+		AMSvc:           proxy,
+		GrafanaSvc:      grafana,
+		DatasourceCache: datasourceCache,
+	}
+}
+
+func (am *ForkedAMSvc) getService(ctx *models.ReqContext) (AlertmanagerApiService, error) {
+	t, err := backendType(ctx, am.DatasourceCache)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t {
+	case apimodels.GrafanaBackend:
+		return am.GrafanaSvc, nil
+	case apimodels.AlertmanagerBackend:
+		return am.AMSvc, nil
+	default:
+		return nil, fmt.Errorf("unexpected backend type (%v)", t)
+	}
+}
+
+func (am *ForkedAMSvc) RouteCreateSilence(ctx *models.ReqContext, body apimodels.SilenceBody) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteCreateSilence(ctx, body)
+}
+
+func (am *ForkedAMSvc) RouteDeleteAlertingConfig(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteDeleteAlertingConfig(ctx)
+}
+
+func (am *ForkedAMSvc) RouteDeleteSilence(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteDeleteSilence(ctx)
+}
+
+func (am *ForkedAMSvc) RouteGetAlertingConfig(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteGetAlertingConfig(ctx)
+}
+
+func (am *ForkedAMSvc) RouteGetAMAlertGroups(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteGetAMAlertGroups(ctx)
+}
+
+func (am *ForkedAMSvc) RouteGetAMAlerts(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteGetAMAlerts(ctx)
+}
+
+func (am *ForkedAMSvc) RouteGetSilence(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteGetSilence(ctx)
+}
+
+func (am *ForkedAMSvc) RouteGetSilences(ctx *models.ReqContext) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteGetSilences(ctx)
+}
+
+func (am *ForkedAMSvc) RoutePostAlertingConfig(ctx *models.ReqContext, body apimodels.PostableUserConfig) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RoutePostAlertingConfig(ctx, body)
+}
+
+func (am *ForkedAMSvc) RoutePostAMAlerts(ctx *models.ReqContext, body apimodels.PostableAlerts) response.Response {
+	s, err := am.getService(ctx)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RoutePostAMAlerts(ctx, body)
+}
