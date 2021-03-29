3a594834ed5492e79a7aebaa0e600f4a0ca7074b
David Parrott <stomp.box.yo@gmail.com>
2021-03-29 15:58:38 -0700

    pr feedback

--- pkg/services/ngalert/schedule/compat.go
+++ pkg/services/ngalert/schedule/compat.go
@@ -11,13 +11,9 @@ func FromAlertStateToPostableAlerts(firingStates []state.AlertState) []*notifier
 	for _, state := range firingStates {
 		alerts = append(alerts, &notifier.PostableAlert{
 			PostableAlert: models.PostableAlert{
-				Annotations: models.LabelSet{},
+				Annotations: models.LabelSet{}, //TODO: add annotations to evaluation results, add them to the state struct, and then set them before sending to the notifier
 				StartsAt:    state.StartsAt,
-				//TODO: When calculating if an alert should not be firing anymore, we should take three things into account:
-				// 1. The re-send the delay if any, we don't want to send every firing alert every time, we should have a fixed delay across all alerts to avoid saturating the notification system
-				// 2. The evaluation interval defined for this particular alert - we don't support that yet but will eventually allow you to define how often do you want this alert to be evaluted
-				// 3. The base interval defined by the scheduler - in the case where #2 is not yet an option we can use the base interval at which every alert runs.
-				EndsAt: state.EndsAt,
+				EndsAt:      state.EndsAt,
 				Alert: models.Alert{
 					Labels: models.LabelSet(state.Labels),
 				},
