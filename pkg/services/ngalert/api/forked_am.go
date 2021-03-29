d748e8659cd865eb6b4109784406253564d2cd9b
Sofia Papagiannaki <sofia@grafana.com>
2021-03-29 12:58:59 +0300

    Apply suggestions from code review

--- pkg/services/ngalert/api/forked_am.go
+++ pkg/services/ngalert/api/forked_am.go
@@ -10,174 +10,120 @@ import (
 )
 
 type ForkedAMSvc struct {
-	AmSvc, GrafanaSvc AlertmanagerApiService
+	AMSvc, GrafanaSvc AlertmanagerApiService
 	DatasourceCache   datasources.CacheService
 }
 
 func NewForkedAM(datasourceCache datasources.CacheService, proxy, grafana AlertmanagerApiService) *ForkedAMSvc {
 	return &ForkedAMSvc{
-		AmSvc:           proxy,
+		AMSvc:           proxy,
 		GrafanaSvc:      grafana,
 		DatasourceCache: datasourceCache,
 	}
 }
 
-func (am *ForkedAMSvc) RouteCreateSilence(ctx *models.ReqContext, body apimodels.SilenceBody) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+func (am *ForkedAMSvc) getService(ctx *models.ReqContext, datasourceCache datasources.CacheService) (AlertmanagerApiService, error) {
+	t, err := backendType(ctx, datasourceCache)
 	if err != nil {
-		return response.Error(400, err.Error(), nil)
+		return nil, err
 	}
 
 	switch t {
 	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteCreateSilence(ctx, body)
+		return am.GrafanaSvc, nil
 	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteCreateSilence(ctx, body)
+		return am.AMSvc, nil
 	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
+		return nil, fmt.Errorf("unexpected backend type (%v)", t)
 	}
+}
+
+func (am *ForkedAMSvc) RouteCreateSilence(ctx *models.ReqContext, body apimodels.SilenceBody) response.Response {
+	s, err := am.getService(ctx, am.DatasourceCache)
+	if err != nil {
+		return response.Error(400, err.Error(), nil)
+	}
+
+	return s.RouteCreateSilence(ctx, body)
 }
 
 func (am *ForkedAMSvc) RouteDeleteAlertingConfig(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteDeleteAlertingConfig(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteDeleteAlertingConfig(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteDeleteAlertingConfig(ctx)
 }
 
 func (am *ForkedAMSvc) RouteDeleteSilence(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteDeleteSilence(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteDeleteSilence(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteDeleteSilence(ctx)
 }
 
 func (am *ForkedAMSvc) RouteGetAlertingConfig(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteGetAlertingConfig(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteGetAlertingConfig(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteGetAlertingConfig(ctx)
 }
 
 func (am *ForkedAMSvc) RouteGetAmAlertGroups(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteGetAmAlertGroups(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteGetAmAlertGroups(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteGetAmAlertGroups(ctx)
 }
 
 func (am *ForkedAMSvc) RouteGetAmAlerts(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteGetAmAlerts(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteGetAmAlerts(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteGetAmAlerts(ctx)
 }
 
 func (am *ForkedAMSvc) RouteGetSilence(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteGetSilence(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteGetSilence(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteGetSilence(ctx)
 }
 
 func (am *ForkedAMSvc) RouteGetSilences(ctx *models.ReqContext) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RouteGetSilences(ctx)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RouteGetSilences(ctx)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RouteGetSilences(ctx)
 }
 
 func (am *ForkedAMSvc) RoutePostAlertingConfig(ctx *models.ReqContext, body apimodels.PostableUserConfig) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RoutePostAlertingConfig(ctx, body)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RoutePostAlertingConfig(ctx, body)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RoutePostAlertingConfig(ctx, body)
 }
 
 func (am *ForkedAMSvc) RoutePostAmAlerts(ctx *models.ReqContext, body apimodels.PostableAlerts) response.Response {
-	t, err := backendType(ctx, am.DatasourceCache)
+	s, err := am.getService(ctx, am.DatasourceCache)
 	if err != nil {
 		return response.Error(400, err.Error(), nil)
 	}
 
-	switch t {
-	case apimodels.GrafanaBackend:
-		return am.GrafanaSvc.RoutePostAmAlerts(ctx, body)
-	case apimodels.AlertmanagerBackend:
-		return am.AmSvc.RoutePostAmAlerts(ctx, body)
-	default:
-		return response.Error(400, fmt.Sprintf("unexpected backend type (%v)", t), nil)
-	}
+	return s.RoutePostAmAlerts(ctx, body)
 }
