486412d457e48a80a41d848410aa1cfa61afff8d
Torkel Ödegaard <torkel@grafana.com>
2021-03-31 14:35:21 +0200

    Updated plugins

---
+++ scripts/webpack/babel.config.js
@@ -0,0 +1,37 @@
+module.exports = function getBabelConfig() {
+  return {
+    cacheDirectory: true,
+    babelrc: false,
+    // Note: order is top-to-bottom and/or left-to-right
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-syntax-dynamic-import', // needed for `() => import()` in routes.ts
+      'angularjs-annotate',
+    ],
+    // Note: order is bottom-to-top and/or right-to-left
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers: 'last 3 versions',
+          },
+          useBuiltIns: 'entry',
+          corejs: 3,
+          modules: false,
+        },
+      ],
+      [
+        '@babel/preset-typescript',
+        {
+          allowNamespaces: true,
+        },
+      ],
+      '@babel/preset-react',
+    ],
+  };
+};
