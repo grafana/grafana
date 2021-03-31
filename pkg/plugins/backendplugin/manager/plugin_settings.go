8f3fd1ef2870f24faacfaae704bbdc93003be4f4
dsotirakis <sotirakis.dim@gmail.com>
2021-03-31 16:59:44 +0300

    Add ability to override config variables with env variables

--- pkg/plugins/backendplugin/manager/plugin_settings.go
+++ pkg/plugins/backendplugin/manager/plugin_settings.go
@@ -2,6 +2,7 @@ package manager
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/grafana/grafana/pkg/setting"
@@ -10,14 +11,24 @@ import (
 type pluginSettings map[string]string
 
 func (ps pluginSettings) ToEnv(prefix string, hostEnv []string) []string {
-	env := []string{}
+	var env []string
 	for k, v := range ps {
+		key := fmt.Sprintf("%s_%s", prefix, strings.ToUpper(k))
+		v = checkForOverrides(key, v)
+
 		env = append(env, fmt.Sprintf("%s_%s=%s", prefix, strings.ToUpper(k), v))
 	}
 
 	env = append(env, hostEnv...)
 
 	return env
+}
+
+func checkForOverrides(key string, v string) string {
+	if value := os.Getenv(key); value != "" {
+		v = value
+	}
+	return v
 }
 
 func getPluginSettings(plugID string, cfg *setting.Cfg) pluginSettings {
