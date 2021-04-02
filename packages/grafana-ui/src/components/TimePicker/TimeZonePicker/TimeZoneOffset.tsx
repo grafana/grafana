16efc4c1b95f27bce5e16634fd17f6bd8a5d7235
Torkel Ödegaard <torkel@grafana.org>
2021-04-02 11:11:46 +0200

    ReactSelect: Upgrades react-select from v3 to v4 and switches from fork to main npm package (#32618)
    * ReactSelect: Upgrades react-select from v3 to v4 and switches from fork to main npm package
    
    * Updated snapshot
    
    * Removed hidden time in timepicker

--- packages/grafana-ui/src/components/TimePicker/TimeZonePicker/TimeZoneOffset.tsx
+++ packages/grafana-ui/src/components/TimePicker/TimeZonePicker/TimeZoneOffset.tsx
@@ -21,7 +21,6 @@ export const TimeZoneOffset: React.FC<PropsWithChildren<Props>> = (props) => {
 
   return (
     <>
-      <span className={styles.localTime}>{formatLocalTime(timestamp, timeZone)}</span>
       <span className={cx(styles.offset, className)}>{formatUtcOffset(timestamp, timeZone)}</span>
     </>
   );
@@ -39,13 +38,6 @@ export const formatUtcOffset = (timestamp: number, timeZone: TimeZone): string =
   return `UTC${offset}`;
 };
 
-const formatLocalTime = (timestamp: number, timeZone: TimeZone): string => {
-  return dateTimeFormat(timestamp, {
-    timeZone,
-    format: 'HH:mm',
-  });
-};
-
 const getStyles = stylesFactory((theme: GrafanaTheme) => {
   const textBase = css`
     font-weight: normal;
@@ -55,11 +47,6 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
   `;
 
   return {
-    localTime: css`
-      display: none;
-      ${textBase};
-      color: ${theme.colors.text};
-    `,
     offset: css`
       ${textBase};
       color: ${theme.colors.text};
