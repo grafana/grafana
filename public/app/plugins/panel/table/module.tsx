4395b9575c093ee4327ddb5d9777e7ba0a94f5d8
Dominik Prokop <dominik.prokop@grafana.com>
2021-04-02 13:49:12 +0200

    TablePanel: Add solid  background cell display mode (#32446)
    * Add solid color background cell display mode
    
    * Update docs/sources/panels/visualizations/table/table-field-options.md
    
    Co-authored-by: achatterjee-grafana <70489351+achatterjee-grafana@users.noreply.github.com>
    
    * Update docs/sources/panels/visualizations/table/table-field-options.md
    
    Co-authored-by: achatterjee-grafana <70489351+achatterjee-grafana@users.noreply.github.com>

--- public/app/plugins/panel/table/module.tsx
+++ public/app/plugins/panel/table/module.tsx
@@ -42,7 +42,8 @@ export const plugin = new PanelPlugin<Options, CustomFieldConfig>(TablePanel)
             options: [
               { value: TableCellDisplayMode.Auto, label: 'Auto' },
               { value: TableCellDisplayMode.ColorText, label: 'Color text' },
-              { value: TableCellDisplayMode.ColorBackground, label: 'Color background' },
+              { value: TableCellDisplayMode.ColorBackground, label: 'Color background (gradient)' },
+              { value: TableCellDisplayMode.ColorBackgroundSolid, label: 'Color background (solid)' },
               { value: TableCellDisplayMode.GradientGauge, label: 'Gradient gauge' },
               { value: TableCellDisplayMode.LcdGauge, label: 'LCD gauge' },
               { value: TableCellDisplayMode.BasicGauge, label: 'Basic gauge' },
