a583e9502691a67a4294d82316d162191c037d34
Torkel Ödegaard <torkel@grafana.com>
2021-04-03 18:39:51 +0200

    Plugins: Support es5 plugins extending es6 core classes

--- public/app/plugins/sdk.ts
+++ public/app/plugins/sdk.ts
@@ -1,7 +1,18 @@
 import { PanelCtrl } from 'app/features/panel/panel_ctrl';
-import { MetricsPanelCtrl } from 'app/features/panel/metrics_panel_ctrl';
+import { MetricsPanelCtrl as MetricsPanelCtrlES6 } from 'app/features/panel/metrics_panel_ctrl';
 import { QueryCtrl } from 'app/features/panel/query_ctrl';
 import { alertTab } from 'app/features/alerting/AlertTabCtrl';
 import { loadPluginCss } from '@grafana/runtime';
+
+const MetricsPanelCtrl = new Proxy(MetricsPanelCtrlES6, {
+  // ES5 code will call it like a function using super
+  apply(target, self, argumentsList) {
+    if (typeof Reflect === 'undefined' || !Reflect.construct) {
+      alert('Browser is too old');
+    }
+
+    return Reflect.construct(target, argumentsList, self.constructor);
+  },
+});
 
 export { PanelCtrl, MetricsPanelCtrl, QueryCtrl, alertTab, loadPluginCss };
