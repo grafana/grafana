b34ead6e8ae8a51ad317b3970e1f194554e7d42e
Tom Pickles <tom@pckls.io>
2021-04-04 01:50:45 +0800

    Tests for building MetricNamespaces URL (#28350)

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_monitor/url_builder.test.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_monitor/url_builder.test.ts
@@ -1,6 +1,40 @@
 import UrlBuilder from './url_builder';
 
 describe('AzureMonitorUrlBuilder', () => {
+  describe('when metric definition is Microsoft.Sql/servers/databases', () => {
+    it('should build the getMetricNamespaces url in the longer format', () => {
+      const url = UrlBuilder.buildAzureMonitorGetMetricNamespacesUrl(
+        '',
+        'sub1',
+        'rg',
+        'Microsoft.Sql/servers/databases',
+        'rn1/rn2',
+        '2017-05-01-preview'
+      );
+      expect(url).toBe(
+        '/sub1/resourceGroups/rg/providers/Microsoft.Sql/servers/rn1/databases/rn2/' +
+          'providers/microsoft.insights/metricNamespaces?api-version=2017-05-01-preview'
+      );
+    });
+  });
+
+  describe('when metric definition is Microsoft.Sql/servers', () => {
+    it('should build the getMetricNamespaces url in the shorter format', () => {
+      const url = UrlBuilder.buildAzureMonitorGetMetricNamespacesUrl(
+        '',
+        'sub1',
+        'rg',
+        'Microsoft.Sql/servers',
+        'rn',
+        '2017-05-01-preview'
+      );
+      expect(url).toBe(
+        '/sub1/resourceGroups/rg/providers/Microsoft.Sql/servers/rn/' +
+          'providers/microsoft.insights/metricNamespaces?api-version=2017-05-01-preview'
+      );
+    });
+  });
+
   describe('when metric definition is Microsoft.Sql/servers/databases and the metricNamespace is default', () => {
     it('should build the getMetricNames url in the longer format', () => {
       const url = UrlBuilder.buildAzureMonitorGetMetricNamesUrl(
