830a9bb7f12f58b9acabaa0e582c04c9867ca1b1
kyle <kyle@grafana.com>
2021-04-05 12:01:19 -0400

    AlertingNG: (wip) play with translating old to ruleGroup

--- pkg/services/ngalert/api/api_trans_dev.go
+++ pkg/services/ngalert/api/api_trans_dev.go
@@ -2,14 +2,19 @@ package api
 
 import (
 	"fmt"
+	"time"
 
+	apimodels "github.com/grafana/alerting-api/pkg/api"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/grafana/pkg/api/response"
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/expr/translate"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/eval"
+	ngmodels "github.com/grafana/grafana/pkg/services/ngalert/models"
+	"github.com/grafana/grafana/pkg/services/ngalert/store"
 	"github.com/grafana/grafana/pkg/util"
+	"github.com/prometheus/common/model"
 )
 
 // conditionEvalEndpoint handles POST /api/alert-definitions/evalOld.
@@ -98,7 +103,7 @@ func (api *API) conditionEvalOldEndpointByID(c *models.ReqContext) response.Resp
 	})
 }
 
-// conditionEvalEndpoint handles POST /api/alert-definitions/evalOld.
+// conditionEvalEndpoint handles POST /api/alert-definitions/oldByID.
 func (api *API) conditionOldEndpointByID(c *models.ReqContext) response.Response {
 	id := c.ParamsInt64("id")
 	if id == 0 {
@@ -130,4 +135,94 @@ func (api *API) conditionOldEndpointByID(c *models.ReqContext) response.Response
 	}
 
 	return response.JSON(200, evalCond)
+}
+
+// conditionEvalEndpoint handles POST /api/alert-definitions/ruleGroupByOldID.
+func (api *API) ruleGroupByOldID(c *models.ReqContext) response.Response {
+	id := c.ParamsInt64("id")
+	if id == 0 {
+		return response.Error(400, "missing id", nil)
+	}
+
+	getAlert := &models.GetAlertByIdQuery{
+		Id: id,
+	}
+	if err := bus.Dispatch(getAlert); err != nil {
+		return response.Error(400, fmt.Sprintf("could find alert with id %v", id), err)
+	}
+
+	if getAlert.Result.OrgId != c.SignedInUser.OrgId {
+		return response.Error(403, "alert does not match organization of user", nil)
+	}
+
+	oldAlert := getAlert.Result
+
+	sb, err := oldAlert.Settings.ToDB()
+	if err != nil {
+		return response.Error(400, "failed to marshal alert settings", err)
+	}
+
+	evalCond, err := translate.DashboardAlertConditions(sb, c.OrgId)
+	if err != nil {
+		return response.Error(400, "Failed to translate alert conditions", err)
+	}
+
+	getDash := &models.GetDashboardQuery{
+		Id:    getAlert.Result.DashboardId,
+		OrgId: getAlert.Result.OrgId,
+	}
+	if err := bus.Dispatch(getDash); err != nil {
+		return response.Error(400, fmt.Sprintf("could find dashboard %v for alert with id %v", getDash.Id, id), err)
+	}
+
+	isGeneralFolder := getDash.Result.FolderId == 0 && getDash.Result.IsFolder == false
+
+	var namespaceUID string
+
+	if isGeneralFolder {
+		namespaceUID = "General"
+	} else {
+		getFolder := &models.GetDashboardQuery{
+			Id:    getDash.Result.FolderId,
+			OrgId: getDash.Result.OrgId,
+		}
+		if err := bus.Dispatch(getFolder); err != nil {
+			return response.Error(400, fmt.Sprintf("could find folder %v for alert with id %v", getFolder.Id, id), err)
+		}
+
+		namespaceUID = getFolder.Result.Uid
+	}
+	rule := ngmodels.AlertRule{
+		Title:     oldAlert.Name,
+		Data:      evalCond.Data,
+		Condition: evalCond.Condition,
+		// Should Alert rule have interval seconds, or is the group Interval?
+	}
+
+	rgc := apimodels.PostableRuleGroupConfig{
+		// TODO? Generate new name on conflict?
+		Name:     oldAlert.Name,
+		Interval: model.Duration(oldAlert.Frequency * int64(time.Second)),
+		Rules: []apimodels.PostableExtendedRuleNode{
+			toPostableExtendedRuleNode(rule),
+		},
+	}
+
+	cmd := store.UpdateRuleGroupCmd{
+		OrgID:           oldAlert.OrgId,
+		NamespaceUID:    namespaceUID,
+		RuleGroupConfig: rgc,
+	}
+
+	err = api.RuleStore.UpdateRuleGroup(cmd)
+
+	if err != nil {
+		return response.JSON(400, util.DynMap{
+			"message:": "failed to save alert rule",
+			"error":    err.Error(),
+			"cmd":      cmd,
+		})
+	}
+
+	return response.JSON(200, cmd)
 }
