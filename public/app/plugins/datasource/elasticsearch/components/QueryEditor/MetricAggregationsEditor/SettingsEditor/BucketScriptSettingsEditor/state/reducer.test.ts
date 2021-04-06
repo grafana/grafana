1a667465e6bea4e1c48caf99f3950d94fb7768da
Elfo404 <me@giordanoricci.com>
2021-04-06 12:19:54 +0100

    Generate names based on previous value & fix list key

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/MetricAggregationsEditor/SettingsEditor/BucketScriptSettingsEditor/state/reducer.test.ts
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/MetricAggregationsEditor/SettingsEditor/BucketScriptSettingsEditor/state/reducer.test.ts
@@ -9,23 +9,32 @@ import {
 import { reducer } from './reducer';
 
 describe('BucketScript Settings Reducer', () => {
-  it('Should correctly add new pipeline variable', () => {
-    const firstVar: PipelineVariable = {
+  it('Should correctly add new pipeline variables', () => {
+    const var1: PipelineVariable = {
       name: 'var1',
       pipelineAgg: '',
     };
 
-    const secondVar: PipelineVariable = {
+    const var2: PipelineVariable = {
       name: 'var2',
+      pipelineAgg: '',
+    };
+
+    const var3: PipelineVariable = {
+      name: 'var3',
       pipelineAgg: '',
     };
 
     reducerTester<PipelineVariable[]>()
       .givenReducer(reducer, [])
       .whenActionIsDispatched(addPipelineVariable())
-      .thenStateShouldEqual([firstVar])
+      .thenStateShouldEqual([var1])
       .whenActionIsDispatched(addPipelineVariable())
-      .thenStateShouldEqual([firstVar, secondVar]);
+      .thenStateShouldEqual([var1, var2])
+      .whenActionIsDispatched(removePipelineVariable(0))
+      .thenStateShouldEqual([var2])
+      .whenActionIsDispatched(addPipelineVariable())
+      .thenStateShouldEqual([var2, var3]);
   });
 
   it('Should correctly remove pipeline variables', () => {
