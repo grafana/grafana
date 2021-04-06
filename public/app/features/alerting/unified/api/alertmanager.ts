88a2c9f4a22b6b8ae820a222265d2c59575b78aa
Domas <domasx2@gmail.com>
2021-04-06 10:14:26 +0300

    adjust frontend types

--- public/app/features/alerting/unified/api/alertmanager.ts
+++ public/app/features/alerting/unified/api/alertmanager.ts
@@ -1,32 +1,39 @@
 import { getBackendSrv } from '@grafana/runtime';
-import { AlertManagerCortexConfig, AlertManagerCortexConfigDTO } from 'app/plugins/datasource/alertmanager/types';
-import { getDatasourceAPIId } from '../utils/datasource';
+import { AlertManagerCortexConfig } from 'app/plugins/datasource/alertmanager/types';
+import { getDatasourceAPIId, GRAFANA_RULES_SOURCE_NAME } from '../utils/datasource';
 
 // "grafana" for grafana-managed, otherwise a datasource name
 export async function fetchAlertManagerConfig(alertmanagerSourceName: string): Promise<AlertManagerCortexConfig> {
-  const conf = await getBackendSrv()
-    .fetch<AlertManagerCortexConfigDTO>({
-      url: `/alertmanager/${getDatasourceAPIId(alertmanagerSourceName)}/config/api/v1/alerts`,
-      showErrorAlert: false,
-      showSuccessAlert: false,
-    })
-    .toPromise();
-  return {
-    template_files: conf.data.template_files,
-    alertmanager_config: JSON.parse(conf.data.alertmanager_config),
-  };
+  try {
+    const result = await getBackendSrv()
+      .fetch<AlertManagerCortexConfig>({
+        url: `/alertmanager/${getDatasourceAPIId(alertmanagerSourceName)}/config/api/v1/alerts`,
+        showErrorAlert: false,
+        showSuccessAlert: false,
+      })
+      .toPromise();
+    return result.data;
+  } catch (e) {
+    // if no config has been uploaded to grafana, it returns error instead of latest config
+    if (
+      alertmanagerSourceName === GRAFANA_RULES_SOURCE_NAME &&
+      e.data?.message?.includes('failed to get latest configuration')
+    ) {
+      return {
+        template_files: {},
+        alertmanager_config: {},
+      };
+    }
+    throw e;
+  }
 }
 
 export async function updateAlertmanagerConfig(
   alertmanagerSourceName: string,
   config: AlertManagerCortexConfig
 ): Promise<void> {
-  const payload: AlertManagerCortexConfigDTO = {
-    template_files: config.template_files,
-    alertmanager_config: JSON.stringify(config.alertmanager_config),
-  };
   await getBackendSrv().post(
     `/alertmanager/${getDatasourceAPIId(alertmanagerSourceName)}/config/api/v1/alerts`,
-    payload
+    config
   );
 }
