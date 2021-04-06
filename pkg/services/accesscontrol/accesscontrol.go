6ac1b9171d0ee220cfcd81d4076f717a9cc2195f
Alexander Zobnin <alexanderzobnin@gmail.com>
2021-04-06 13:53:40 +0300

    Simplify AccessControl type

--- pkg/services/accesscontrol/accesscontrol.go
+++ pkg/services/accesscontrol/accesscontrol.go
@@ -8,12 +8,12 @@ import (
 )
 
 type AccessControl interface {
-	registry.CanBeDisabled
-	Evaluator
-	GetUserPermissions(ctx context.Context, user *models.SignedInUser, roles []string) ([]*Permission, error)
-}
-
-type Evaluator interface {
-	// Evaluate evaluates access to the given resource
+	// Evaluate evaluates access to the given resource.
 	Evaluate(ctx context.Context, user *models.SignedInUser, permission string, scope ...string) (bool, error)
+
+	// GetUserPermissions returns user permissions.
+	GetUserPermissions(ctx context.Context, user *models.SignedInUser, roles []string) ([]*Permission, error)
+
+	// Middleware checks if service disabled or not to switch to fallback authorization.
+	registry.CanBeDisabled
 }
