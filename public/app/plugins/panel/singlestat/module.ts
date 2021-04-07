2c8728a2a4f09971c5e79d271fe08f9be33d95fa
Torkel Ödegaard <torkel@grafana.org>
2021-04-07 09:34:30 +0200

    AngularPanels: Fixes issue with angular panels caused by reason babel upgrade (#32738)

--- public/app/plugins/panel/singlestat/module.ts
+++ public/app/plugins/panel/singlestat/module.ts
@@ -51,13 +51,10 @@ class SingleStatCtrl extends MetricsPanelCtrl {
   static templateUrl = 'module.html';
 
   data: Partial<ShowData> = {};
-
   fontSizes: any[] = [];
   fieldNames: string[] = [];
-
   invalidGaugeRange = false;
-  panel: any;
-  events: any;
+
   valueNameOptions: any[] = [
     { value: 'min', text: 'Min' },
     { value: 'max', text: 'Max' },
