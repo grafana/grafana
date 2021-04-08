b7d8d3e966d8fbfc849d2c0f5a6d425afdcd2943
Dominik Prokop <dominik.prokop@grafana.com>
2021-04-08 17:38:56 +0200

    DataLinks: Ensure window origin to be stripped from url when using absolute urls (#32634)
    * Ensure window origin to be stripped from url when using absolute urls
    
    * Update test
    
    * Make sure link interception works for external links
    
    * Apply suggestions from code review
    
    * Review

--- packages/grafana-data/src/utils/location.test.ts
+++ packages/grafana-data/src/utils/location.test.ts
@@ -1,18 +1,65 @@
 import { locationUtil } from './location';
 
 describe('locationUtil', () => {
+  const { location } = window;
+
   beforeAll(() => {
-    locationUtil.initialize({
-      config: { appSubUrl: '/subUrl' } as any,
-      getVariablesUrlParams: (() => {}) as any,
-      getTimeRangeForUrl: (() => {}) as any,
+    // @ts-ignore
+    delete window.location;
+
+    window.location = {
+      ...location,
+      hash: '#hash',
+      host: 'www.domain.com:9877',
+      hostname: 'www.domain.com',
+      href: 'http://www.domain.com:9877/path/b?search=a&b=c&d#hash',
+      origin: 'http://www.domain.com:9877',
+      pathname: '/path/b',
+      port: '9877',
+      protocol: 'http:',
+      search: '?search=a&b=c&d',
+    };
+  });
+
+  afterAll(() => {
+    window.location = location;
+  });
+
+  describe('strip base when appSubUrl configured', () => {
+    beforeEach(() => {
+      locationUtil.initialize({
+        config: { appSubUrl: '/subUrl' } as any,
+        getVariablesUrlParams: (() => {}) as any,
+        getTimeRangeForUrl: (() => {}) as any,
+      });
+    });
+    test('relative url', () => {
+      const urlWithoutMaster = locationUtil.stripBaseFromUrl('/subUrl/grafana/');
+      expect(urlWithoutMaster).toBe('/grafana/');
+    });
+    test('absolute url url', () => {
+      const urlWithoutMaster = locationUtil.stripBaseFromUrl('http://www.domain.com:9877/subUrl/grafana/');
+      expect(urlWithoutMaster).toBe('/grafana/');
     });
   });
 
-  describe('With /subUrl as appSubUrl', () => {
-    it('/subUrl should be stripped', () => {
+  describe('strip base when appSubUrl not configured', () => {
+    beforeEach(() => {
+      locationUtil.initialize({
+        config: {} as any,
+        getVariablesUrlParams: (() => {}) as any,
+        getTimeRangeForUrl: (() => {}) as any,
+      });
+    });
+
+    test('relative url', () => {
       const urlWithoutMaster = locationUtil.stripBaseFromUrl('/subUrl/grafana/');
-      expect(urlWithoutMaster).toBe('/grafana/');
+      expect(urlWithoutMaster).toBe('/subUrl/grafana/');
+    });
+
+    test('absolute url', () => {
+      const urlWithoutMaster = locationUtil.stripBaseFromUrl('http://www.domain.com:9877/subUrl/grafana/');
+      expect(urlWithoutMaster).toBe('/subUrl/grafana/');
     });
   });
 });
