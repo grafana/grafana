cd69dd7f2825e155e02ff727104e413c28a0fef8
gotjosh <josue@grafana.com>
2021-04-09 10:55:41 +0100

    Alerting: Cleanup and move legacy to a legacy file (#32803)
    * Alerting: Cleanup and move legacy to a legacy file
    
    A quick cleanup of the ngalert/api directory, optimising for an easy
    removal of what is will be considered legacy at some point. A quick
    summary of what's done is:
    
    - Add a prefix `generated` prefix to files that are auto-generated by
      our swagger definitions.
    - Create a legacy file to place all the legacy API routes implementation
      and helpers. Deleting files that where no longer needed after this
    move.
    - Rename the `lotex` file to `lotex_ruler`
    - Adding a couple of comments here and there.
    
    With this, I hope to organise our code in this directory a bit better
    given there's a lot going on.

--- pkg/services/ngalert/api/swagger-codegen/move-and-rename.py
+++ pkg/services/ngalert/api/swagger-codegen/move-and-rename.py
@@ -4,4 +4,4 @@ files = os.listdir(path)
 dest_dir = "../"
 
 for index, file in enumerate(files):
-	os.rename(os.path.join(path, file), os.path.join(dest_dir, ''.join([file.split('.')[0], '_base.go'])))
+	os.rename(os.path.join(path, file), os.path.join(dest_dir, ''.join(['generated_base_',file.split('.')[0], '.go'])))
