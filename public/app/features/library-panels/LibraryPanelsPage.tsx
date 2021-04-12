05648d989b85aa0120c732c462477c64989bb702
Hugo Häggmark <hugo.haggmark@gmail.com>
2021-04-12 08:13:20 +0200

    LibraryPanels: Adds initial section and Page to Dashboard submenu

---
+++ public/app/features/library-panels/LibraryPanelsPage.tsx
@@ -0,0 +1,48 @@
+import React, { FC, useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+
+import { GrafanaRouteComponentProps } from '../../core/navigation/types';
+import { StoreState } from '../../types';
+import { getNavModel } from '../../core/selectors/navModel';
+import Page from '../../core/components/Page/Page';
+import { LibraryPanelsView } from './components/LibraryPanelsView/LibraryPanelsView';
+import { useAsync } from 'react-use';
+import { getLibraryPanels } from './state/api';
+import OrgActionBar from '../../core/components/OrgActionBar/OrgActionBar';
+
+const mapStateToProps = (state: StoreState) => ({
+  navModel: getNavModel(state.navIndex, 'library-panels'),
+});
+
+const connector = connect(mapStateToProps, undefined);
+
+interface OwnProps extends GrafanaRouteComponentProps {}
+
+type Props = OwnProps & ConnectedProps<typeof connector>;
+
+export const LibraryPanelsPage: FC<Props> = ({ navModel }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const { value: searchResult, loading } = useAsync(async () => {
+    return getLibraryPanels();
+  });
+  const hasLibraryPanels = Boolean(searchResult?.libraryPanels.length);
+
+  return (
+    <Page navModel={navModel}>
+      <Page.Contents isLoading={loading}>
+        {hasLibraryPanels && (
+          <OrgActionBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} placeholder={'Search by name'} />
+        )}
+        <LibraryPanelsView
+          className=""
+          onClickCard={() => undefined}
+          searchString={searchQuery}
+          currentPanelId={undefined}
+          showSecondaryActions={true}
+        />
+      </Page.Contents>
+    </Page>
+  );
+};
+
+export default connect(mapStateToProps)(LibraryPanelsPage);
