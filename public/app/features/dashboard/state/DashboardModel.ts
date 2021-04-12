69bcaeaa3e7ad771f95058445b806853006d3107
dessen-xu <1805680163@qq.com>
2021-04-12 16:26:18 +0800

    fix_variable_select_bug

--- public/app/features/dashboard/state/DashboardModel.ts
+++ public/app/features/dashboard/state/DashboardModel.ts
@@ -295,6 +295,10 @@ export class DashboardModel {
         } else {
           copy.templating.list[i].current = original.current;
         }
+
+        current.options.forEach((option: any) => {
+          option.selected = option.value === current.current.value;
+        });
       }
     }
   }
