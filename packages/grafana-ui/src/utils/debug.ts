c996507ace152f2a194f61d6f515a7146a37f271
Dominik Prokop <dominik.prokop@grafana.com>
2021-04-13 17:02:29 +0200

    Global debugger utils

---
+++ packages/grafana-ui/src/utils/debug.ts
@@ -0,0 +1,30 @@
+import { Logger } from './logger';
+
+/**
+ * Allows debug helpers attachement to the window object
+ * @internal
+ */
+export function attachDebbuger(key: string, thebugger?: any, logger?: Logger) {
+  if (process.env.NODE_ENV !== 'production') {
+    let completeDebugger = thebugger ? { ...thebugger } : {};
+
+    if (logger !== undefined) {
+      completeDebugger = { ...completeDebugger, enable: () => logger.enable(), disable: () => logger.disable() };
+    }
+
+    // @ts-ignore
+    let debugGlobal = window['_debug'];
+    if (debugGlobal) {
+      debugGlobal = {
+        ...debugGlobal,
+        [key]: completeDebugger,
+      };
+    } else {
+      debugGlobal = {
+        [key]: completeDebugger,
+      };
+    }
+    // @ts-ignore
+    window['_debug'] = debugGlobal;
+  }
+}
