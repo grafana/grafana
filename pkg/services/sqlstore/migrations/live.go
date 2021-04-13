7d63195df5baf7d263571fc7fcc73862fc19e845
Alexander Emelin <frvzmb@gmail.com>
2021-04-13 15:38:01 +0300

    fix db type

--- pkg/services/sqlstore/migrations/live.go
+++ pkg/services/sqlstore/migrations/live.go
@@ -13,7 +13,7 @@ func addLiveMigrations(mg *Migrator) {
 			{Name: "channel", Type: DB_NVarchar, Length: 255, Nullable: false},
 			{Name: "data", Type: DB_Text, Nullable: false},
 			{Name: "created_by", Type: DB_Int, Nullable: false},
-			{Name: "created", Type: DB_Int, Nullable: false},
+			{Name: "created", Type: DB_DateTime, Nullable: false},
 		},
 		Indices: []*Index{
 			{Cols: []string{"org_id", "channel"}, Type: UniqueIndex},
