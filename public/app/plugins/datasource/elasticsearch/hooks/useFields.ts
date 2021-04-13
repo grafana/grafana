426673d4bcfcf109896c502e6128da118c2a7ea4
Elfo404 <me@giordanoricci.com>
2021-04-13 13:40:50 +0100

    Add tests for useFields hook

--- public/app/plugins/datasource/elasticsearch/hooks/useFields.ts
+++ public/app/plugins/datasource/elasticsearch/hooks/useFields.ts
@@ -10,12 +10,15 @@ type AggregationType = BucketAggregationType | MetricAggregationType;
 
 const getFilter = (aggregationType: AggregationType) => {
   // For all metric types we want only numbers, except for cardinality
+  // TODO: To have a more configuration-driven editor, it would be nice to move this logic in
+  // metricAggregationConfig and bucketAggregationConfig so that each aggregation type can specify on
+  // which kind of data it operates.
   if (isMetricAggregationType(aggregationType)) {
     if (aggregationType !== 'cardinality') {
       return 'number';
     }
 
-    return null;
+    return void 0;
   }
 
   switch (aggregationType) {
@@ -24,22 +27,27 @@ const getFilter = (aggregationType: AggregationType) => {
     case 'geohash_grid':
       return 'geo_point';
     default:
-      return null;
+      return void 0;
   }
 };
 
-const toSelectableValue = ({ value, text }: MetricFindValue): SelectableValue<string> => ({
+const toSelectableValue = ({ text }: MetricFindValue): SelectableValue<string> => ({
   label: text,
-  value: `${value || text}`,
+  value: text,
 });
 
+/**
+ * Returns a function to query the configured datasource for autocomplete values for the specified aggregation type.
+ * Each aggregation can be run on different types, for example avg only operates on numeric fields, geohash_grid only on geo_point fields.
+ * @param aggregationType the type of aggregation to get fields for
+ */
 export const useFields = (aggregationType: AggregationType) => {
   const datasource = useDatasource();
   const range = useRange();
   const filter = getFilter(aggregationType);
 
   return async () => {
-    const rawFields = await datasource.getFields(filter as string, range).toPromise();
+    const rawFields = await datasource.getFields(filter, range).toPromise();
     return rawFields.map(toSelectableValue);
   };
 };
