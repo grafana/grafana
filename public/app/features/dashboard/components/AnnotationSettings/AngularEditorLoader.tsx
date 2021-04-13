515b1d6819537279cc405e24722d2028eb966315
Torkel Ödegaard <torkel@grafana.com>
2021-04-13 07:45:40 +0200

    Fix annotation angular loading issues

--- public/app/features/dashboard/components/AnnotationSettings/AngularEditorLoader.tsx
+++ public/app/features/dashboard/components/AnnotationSettings/AngularEditorLoader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { AnnotationQuery, DataSourceApi } from '@grafana/data';
 import { AngularComponent, getAngularLoader } from '@grafana/runtime';
 
@@ -8,43 +8,52 @@ export interface Props {
   onChange: (annotation: AnnotationQuery) => void;
 }
 
-export const AngularEditorLoader: React.FC<Props> = React.memo(({ annotation, datasource, onChange }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [angularComponent, setAngularComponent] = useState<AngularComponent | null>(null);
+export class AngularEditorLoader extends React.PureComponent<Props> {
+  ref: HTMLDivElement | null = null;
+  angularComponent: AngularComponent;
 
-  useEffect(() => {
-    return () => {
-      if (angularComponent) {
-        angularComponent.destroy();
-      }
-    };
-  }, [angularComponent]);
-
-  useEffect(() => {
-    if (ref.current) {
-      const loader = getAngularLoader();
-      const template = `<plugin-component ng-if="!ctrl.currentDatasource.annotations" type="annotations-query-ctrl"> </plugin-component>`;
-      const scopeProps = {
-        ctrl: {
-          currentDatasource: datasource,
-          currentAnnotation: annotation,
-        },
-      };
-
-      const component = loader.load(ref.current, scopeProps, template);
-      component.digest();
-      component.getScope().$watch(() => {
-        onChange({
-          ...annotation,
-        });
-      });
-
-      setAngularComponent(component);
+  componentWillUnmount() {
+    if (this.angularComponent) {
+      this.angularComponent.destroy();
     }
-    // specifying annotation or onChange causes angular error `Cannot read property '$$nextSibling' of null`
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref, datasource]);
+  }
 
-  return <div ref={ref} />;
-});
-AngularEditorLoader.displayName = 'AngularEditorLoader';
+  componentDidMount() {
+    if (this.ref) {
+      this.loadAngular();
+    }
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.datasource !== this.props.datasource) {
+      this.loadAngular();
+    }
+  }
+
+  loadAngular() {
+    if (this.angularComponent) {
+      this.angularComponent.destroy();
+    }
+
+    const loader = getAngularLoader();
+    const template = `<plugin-component ng-if="!ctrl.currentDatasource.annotations" type="annotations-query-ctrl"> </plugin-component>`;
+    const scopeProps = {
+      ctrl: {
+        currentDatasource: this.props.datasource,
+        currentAnnotation: this.props.annotation,
+      },
+    };
+
+    this.angularComponent = loader.load(this.ref, scopeProps, template);
+    this.angularComponent.digest();
+    this.angularComponent.getScope().$watch(() => {
+      this.props.onChange({
+        ...this.props.annotation,
+      });
+    });
+  }
+
+  render() {
+    return <div ref={(element) => (this.ref = element)} />;
+  }
+}
