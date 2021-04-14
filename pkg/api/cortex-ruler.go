8bf1cc2f66f15a263526fac0cdb7b37c37e8b968
Sofia Papagiannaki <papagian@gmail.com>
2021-04-14 10:52:40 +0300

    Add namespace id in GettableGrafanaRule

--- pkg/api/cortex-ruler.go
+++ pkg/api/cortex-ruler.go
@@ -363,6 +363,7 @@ type GettableGrafanaRule struct {
 	Version         int64               `json:"version" yaml:"version"`
 	UID             string              `json:"uid" yaml:"uid"`
 	NamespaceUID    string              `json:"namespace_uid" yaml:"namespace_uid"`
+	NamespaceID     int64               `json:"namespace_id" yaml:"namespace_id"`
 	RuleGroup       string              `json:"rule_group" yaml:"rule_group"`
 	NoDataState     NoDataState         `json:"no_data_state" yaml:"no_data_state"`
 	ExecErrState    ExecutionErrorState `json:"exec_err_state" yaml:"exec_err_state"`
