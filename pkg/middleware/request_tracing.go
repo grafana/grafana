4f0b803f8c844ed9c8d59dda8a32bb5411eed103
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2021-04-15 15:35:00 +0200

    Chore: Improve request distributed tracing middleware

--- pkg/middleware/request_tracing.go
+++ pkg/middleware/request_tracing.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
@@ -10,13 +11,26 @@ import (
 	"gopkg.in/macaron.v1"
 )
 
-func RequestTracing(handler string) macaron.Handler {
+const routeOperationNameKey = "route.operation.name"
+
+func ProvideRouteOperationName(handler string) macaron.Handler {
 	return func(res http.ResponseWriter, req *http.Request, c *macaron.Context) {
+		c.Data[routeOperationNameKey] = handler
+	}
+}
+
+func RequestTracing() macaron.Handler {
+	return func(res http.ResponseWriter, req *http.Request, c *macaron.Context) {
+		if !strings.HasPrefix(c.Req.URL.Path, "/api/") {
+			c.Next()
+			return
+		}
+
 		rw := res.(macaron.ResponseWriter)
 
 		tracer := opentracing.GlobalTracer()
 		wireContext, _ := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(req.Header))
-		span := tracer.StartSpan(fmt.Sprintf("HTTP %s", handler), ext.RPCServerOption(wireContext))
+		span := tracer.StartSpan(fmt.Sprintf("HTTP %s %s", req.Method, req.URL.Path), ext.RPCServerOption(wireContext))
 		defer span.Finish()
 
 		ctx := opentracing.ContextWithSpan(req.Context(), span)
@@ -31,6 +45,10 @@ func RequestTracing(handler string) macaron.Handler {
 		ext.HTTPMethod.Set(span, req.Method)
 		if status >= 400 {
 			ext.Error.Set(span, true)
+		}
+
+		if routeOperation, exists := c.Data[routeOperationNameKey]; exists {
+			span.SetOperationName(fmt.Sprintf("HTTP %s", routeOperation.(string)))
 		}
 	}
 }
