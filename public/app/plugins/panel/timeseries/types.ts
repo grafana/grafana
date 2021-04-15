0cc620aea7417c3d594aab29d4d86c115d5fad36
Dominik Prokop <dominik.prokop@grafana.com>
2021-04-15 13:00:01 +0200

    GraphNG: stacking (#30749)
    * First iteration
    
    * Dev dash
    
    * Re-use StackingMode type
    
    * Fix ts and api issues
    
    * Stacking work resurected
    
    * Fix overrides
    
    * Correct values in tooltip and updated test dashboard
    
    * Update dev dashboard
    
    * Apply correct bands for stacking
    
    * Merge fix
    
    * Update snapshot
    
    * Revert go.sum
    
    * Handle null values correctyl and make filleBelowTo and stacking mutual exclusive
    
    * Snapshots update
    
    * Graph->Time series stacking migration
    
    * Review comments
    
    * Indicate overrides in StandardEditorContext
    
    * Change stacking UI editor, migrate stacking to object option
    
    * Small refactor, fix for hiding series and dev dashboard

--- public/app/plugins/panel/timeseries/types.ts
+++ public/app/plugins/panel/timeseries/types.ts
@@ -1,14 +1,9 @@
 import { VizLegendOptions, GraphTooltipOptions } from '@grafana/ui';
 
-export interface GraphOptions {
-  // nothing for now
-}
-
 export interface OptionsWithLegend {
   legend: VizLegendOptions;
 }
 
 export interface Options extends OptionsWithLegend {
-  graph: GraphOptions;
   tooltipOptions: GraphTooltipOptions;
 }
