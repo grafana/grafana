66485b3e7055ce455060f22264d511266920c893
Torkel Ödegaard <torkel@grafana.org>
2021-04-16 09:48:52 +0200

    Theme: V8 Theme updates (#33050)
    * GraphNG: Use new theme props
    
    * Minor fix to letterspacing in button
    
    * Minor tweaks
    
    * Updated
    
    * Revert to roboto
    
    * Added concept of a dark or white base
    
    * Style updates
    
    * Sass
    
    * Updated light text blue
    
    * updates
    
    * reverting button group design
    
    * Fixed tests
    
    * updates
    
    * Updated tests

--- packages/grafana-ui/src/utils/storybook/withPaddedStory.tsx
+++ packages/grafana-ui/src/utils/storybook/withPaddedStory.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { GlobalStyles } from '../../themes';
+import { GlobalStyles, useTheme } from '../../themes';
 import { RenderFunction } from '../../types';
 
 const PaddedStory: React.FunctionComponent<{}> = ({ children }) => {
+  const theme = useTheme();
+
   return (
     <div
       style={{
         width: '100%',
         padding: '20px',
         display: 'flex',
+        minHeight: '80vh',
+        background: `${theme.v2.palette.layer1}`,
       }}
     >
       <GlobalStyles />
