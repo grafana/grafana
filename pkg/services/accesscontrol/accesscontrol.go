ea57423317cb632af46ed0d8198642cc7707e875
Alexander Zobnin <alexanderzobnin@gmail.com>
2021-04-16 15:19:46 +0300

    Chore: remove unused fallbacks

--- pkg/services/accesscontrol/accesscontrol.go
+++ pkg/services/accesscontrol/accesscontrol.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/grafana/grafana/pkg/models"
-	"github.com/grafana/grafana/pkg/setting"
 )
 
 type AccessControl interface {
@@ -36,22 +35,4 @@ func HasAccess(ac AccessControl, c *models.ReqContext) func(fallback func(*model
 
 var ReqGrafanaAdmin = func(c *models.ReqContext) bool {
 	return c.IsGrafanaAdmin
-}
-
-var ReqOrgAdmin = func(c *models.ReqContext) bool {
-	return c.OrgRole == models.ROLE_ADMIN
-}
-
-var ReqOrgAdminOrEditor = func(c *models.ReqContext) bool {
-	return c.OrgRole == models.ROLE_ADMIN || c.OrgRole == models.ROLE_EDITOR
-}
-
-var ReqOrgAdminOrEditorsCanAdmin = func(cfg *setting.Cfg) func(*models.ReqContext) bool {
-	return func(c *models.ReqContext) bool {
-		return c.OrgRole == models.ROLE_ADMIN || (cfg.EditorsCanAdmin && c.OrgRole == models.ROLE_EDITOR)
-	}
-}
-
-var ReqSignedIn = func(c *models.ReqContext) bool {
-	return c.IsSignedIn
 }
