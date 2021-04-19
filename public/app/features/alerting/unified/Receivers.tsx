d64cbd1547c4f94032e855f375c194a4cdd1d5a7
Domas <domasx2@gmail.com>
2021-04-19 13:02:58 +0300

    Alerting: receivers page + template list (#33112)

--- public/app/features/alerting/unified/Receivers.tsx
+++ public/app/features/alerting/unified/Receivers.tsx
@@ -3,39 +3,38 @@ import React, { FC, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { AlertingPageWrapper } from './components/AlertingPageWrapper';
 import { AlertManagerPicker } from './components/AlertManagerPicker';
+import { TemplatesTable } from './components/receivers/TemplatesTable';
 import { useAlertManagerSourceName } from './hooks/useAlertManagerSourceName';
 import { useUnifiedAlertingSelector } from './hooks/useUnifiedAlertingSelector';
 import { fetchAlertManagerConfigAction } from './state/actions';
 import { initialAsyncRequestState } from './utils/redux';
 
-const AmRoutes: FC = () => {
+const Receivers: FC = () => {
   const [alertManagerSourceName, setAlertManagerSourceName] = useAlertManagerSourceName();
   const dispatch = useDispatch();
 
-  const amConfigs = useUnifiedAlertingSelector((state) => state.amConfigs);
+  const config = useUnifiedAlertingSelector((state) => state.amConfigs);
 
   useEffect(() => {
     dispatch(fetchAlertManagerConfigAction(alertManagerSourceName));
   }, [alertManagerSourceName, dispatch]);
 
-  const { result, loading, error } = amConfigs[alertManagerSourceName] || initialAsyncRequestState;
+  const { result, loading, error } = config[alertManagerSourceName] || initialAsyncRequestState;
 
   return (
-    <AlertingPageWrapper pageId="am-routes">
+    <AlertingPageWrapper pageId="receivers">
       <Field label="Choose alert manager">
         <AlertManagerPicker current={alertManagerSourceName} onChange={setAlertManagerSourceName} />
       </Field>
-      <br />
-      <br />
       {error && !loading && (
         <InfoBox severity="error" title={<h4>Error loading alert manager config</h4>}>
           {error.message || 'Unknown error.'}
         </InfoBox>
       )}
-      {loading && <LoadingPlaceholder text="loading alert manager config..." />}
-      {result && !loading && !error && <pre>{JSON.stringify(result, null, 2)}</pre>}
+      {loading && <LoadingPlaceholder text="loading receivers..." />}
+      {result && !loading && !error && <TemplatesTable config={result} />}
     </AlertingPageWrapper>
   );
 };
 
-export default AmRoutes;
+export default Receivers;
