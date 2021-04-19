8fd97efe48b29adbbb4336db23718e90a4635a8b
Ivana <ivana.huckova@gmail.com>
2021-04-19 11:39:18 +0200

    Remove redundant variables, makees it more clear

--- public/app/core/logs_model.ts
+++ public/app/core/logs_model.ts
@@ -257,11 +257,9 @@ export function getSeriesProperties(
   // Clamp time range to visible logs otherwise big parts of the graph might look empty
   if (absoluteRange) {
     const earliestTsLogs = sortedRows[0].timeEpochMs;
-    const earliestTsAbsolute = absoluteRange.from;
-    const latestTsAbsolute = absoluteRange.to;
 
-    requestedRangeMs = latestTsAbsolute - earliestTsAbsolute;
-    visibleRangeMs = latestTsAbsolute - earliestTsLogs;
+    requestedRangeMs = absoluteRange.to - absoluteRange.from;
+    visibleRangeMs = absoluteRange.to - earliestTsLogs;
 
     if (visibleRangeMs > 0) {
       // Adjust interval bucket size for potentially shorter visible range
@@ -271,7 +269,7 @@ export function getSeriesProperties(
       bucketSize = Math.max(Math.ceil(resolutionIntervalMs * pxPerBar), minimumBucketSize);
       // makeSeriesForLogs() aligns dataspoints with time buckets, so we do the same here to not cut off data
       const adjustedEarliest = Math.floor(earliestTsLogs / bucketSize) * bucketSize;
-      visibleRange = { from: adjustedEarliest, to: latestTsAbsolute };
+      visibleRange = { from: adjustedEarliest, to: absoluteRange.to };
     } else {
       // We use visibleRangeMs to calculate range coverage of received logs. However, some data sources are rounding up range in requests. This means that received logs
       // can (in edge cases) be outside of the requested range and visibleRangeMs < 0. In that case, we want to change visibleRangeMs to be 1 so we can calculate coverage.
