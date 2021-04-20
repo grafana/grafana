6d84e7ebca4bbecf898578c3cb1b1de9067f0ca5
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2021-04-20 13:28:07 +0200

    Fix lint issue

--- pkg/tsdb/postgres/macros.go
+++ pkg/tsdb/postgres/macros.go
@@ -111,9 +111,8 @@ func (m *postgresMacroEngine) evaluateMacro(name string, args []string) (string,
 		if m.timescaledb {
 			if interval.Seconds() < 1.0 {
 				return fmt.Sprintf("time_bucket('%.1fs',%s)", interval.Seconds(), args[0]), nil
-			} else {
-				return fmt.Sprintf("time_bucket('%.0fs',%s)", interval.Seconds(), args[0]), nil
 			}
+			return fmt.Sprintf("time_bucket('%.0fs',%s)", interval.Seconds(), args[0]), nil
 		}
 
 		return fmt.Sprintf(
