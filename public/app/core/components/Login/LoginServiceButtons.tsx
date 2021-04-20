0bca21f5528ac1ac9ef2afb5dda970c7e5fff802
Jack Westbrook <jack.westbrook@gmail.com>
2021-04-20 15:19:54 +0200

    refactor(loginpage): migrate custom button styles to use Button component

--- public/app/core/components/Login/LoginServiceButtons.tsx
+++ public/app/core/components/Login/LoginServiceButtons.tsx
@@ -1,89 +1,108 @@
 import React from 'react';
 import config from 'app/core/config';
 import { css, cx } from '@emotion/css';
-import { useStyles } from '@grafana/ui';
+import { Icon, IconName, LinkButton, useStyles, VerticalGroup } from '@grafana/ui';
 import { GrafanaTheme } from '@grafana/data';
-
-const loginServices: () => LoginServices = () => {
-  const oauthEnabled = !!config.oauth;
-
-  return {
-    saml: {
-      enabled: config.samlEnabled,
-      name: 'SAML',
-      className: 'github',
-      icon: 'key',
-    },
-    google: {
-      enabled: oauthEnabled && config.oauth.google,
-      name: 'Google',
-    },
-    azuread: {
-      enabled: oauthEnabled && config.oauth.azuread,
-      name: 'Microsoft',
-    },
-    github: {
-      enabled: oauthEnabled && config.oauth.github,
-      name: 'GitHub',
-    },
-    gitlab: {
-      enabled: oauthEnabled && config.oauth.gitlab,
-      name: 'GitLab',
-    },
-    grafanacom: {
-      enabled: oauthEnabled && config.oauth.grafana_com,
-      name: 'Grafana.com',
-      hrefName: 'grafana_com',
-      icon: 'grafana_com',
-    },
-    okta: {
-      enabled: oauthEnabled && config.oauth.okta,
-      name: 'Okta',
-    },
-    oauth: {
-      enabled: oauthEnabled && config.oauth.generic_oauth,
-      name: oauthEnabled && config.oauth.generic_oauth ? config.oauth.generic_oauth.name : 'OAuth',
-      icon: 'sign-in',
-      hrefName: 'generic_oauth',
-    },
-  };
-};
+import { pickBy } from 'lodash';
 
 export interface LoginService {
+  bgColor: string;
+  borderColor: string;
   enabled: boolean;
   name: string;
   hrefName?: string;
-  icon?: string;
-  className?: string;
+  icon: IconName;
 }
 
 export interface LoginServices {
   [key: string]: LoginService;
 }
 
+const loginServices: () => LoginServices = () => {
+  const oauthEnabled = !!config.oauth;
+
+  return {
+    saml: {
+      bgColor: '#464646',
+      borderColor: '#393939',
+      enabled: config.samlEnabled,
+      name: 'SAML',
+      icon: 'key-skeleton-alt',
+    },
+    google: {
+      bgColor: '#e84d3c',
+      borderColor: '#b83e31',
+      enabled: oauthEnabled && config.oauth.google,
+      name: 'Google',
+      icon: 'google',
+    },
+    azuread: {
+      bgColor: '#2f2f2f',
+      borderColor: '#2f2f2f',
+      enabled: oauthEnabled && config.oauth.azuread,
+      name: 'Microsoft',
+      icon: 'microsoft',
+    },
+    github: {
+      bgColor: '#464646',
+      borderColor: '#393939',
+      enabled: oauthEnabled && config.oauth.github,
+      name: 'GitHub',
+      icon: 'github',
+    },
+    gitlab: {
+      bgColor: '#fc6d26',
+      borderColor: '#e24329',
+      enabled: oauthEnabled && config.oauth.gitlab,
+      name: 'GitLab',
+      icon: 'gitlab',
+    },
+    grafanacom: {
+      bgColor: '#262628',
+      borderColor: '#393939',
+      enabled: oauthEnabled && config.oauth.grafana_com,
+      name: 'Grafana.com',
+      hrefName: 'grafana_com',
+      icon: 'grafana',
+    },
+    okta: {
+      bgColor: '#2f2f2f',
+      borderColor: '#393939',
+      enabled: oauthEnabled && config.oauth.okta,
+      name: 'Okta',
+      icon: 'okta',
+    },
+    oauth: {
+      bgColor: '#262628',
+      borderColor: '#393939',
+      enabled: oauthEnabled && config.oauth.generic_oauth,
+      name: oauthEnabled && config.oauth.generic_oauth ? config.oauth.generic_oauth.name : 'OAuth',
+      icon: 'signin',
+      hrefName: 'generic_oauth',
+    },
+  };
+};
+
 const getServiceStyles = (theme: GrafanaTheme) => {
   return {
-    container: css`
-      width: 100%;
-      text-align: center;
-    `,
     button: css`
       color: #d8d9da;
-      margin: 0 0 ${theme.spacing.md};
-      width: 100%;
-      &:hover {
-        color: #fff;
-      }
+      position: relative;
+    `,
+    buttonIcon: css`
+      position: absolute;
+      left: ${theme.spacing.sm};
+      top: 50%;
+      transform: translateY(-50%);
     `,
     divider: {
       base: css`
-        float: left;
-        width: 100%;
-        margin: 0 25% ${theme.spacing.md} 25%;
+        color: ${theme.colors.text};
         display: flex;
+        margin-bottom: ${theme.spacing.sm};
         justify-content: space-between;
         text-align: center;
-        color: ${theme.colors.text};
+        width: 100%;
       `,
       line: css`
         width: 100px;
@@ -115,37 +134,46 @@ const LoginDivider = () => {
 };
 
 export const LoginServiceButtons = () => {
+  const enabledServices = pickBy(loginServices(), (service) => service.enabled);
+  const hasServices = Object.keys(enabledServices).length > 0;
   const styles = useStyles(getServiceStyles);
-  const keyNames = Object.keys(loginServices());
-  const serviceElementsEnabled = keyNames.filter((key) => {
-    const service: LoginService = loginServices()[key];
-    return service.enabled;
-  });
 
-  if (serviceElementsEnabled.length === 0) {
-    return null;
+  if (hasServices) {
+    return (
+      <VerticalGroup>
+        <LoginDivider />
+        {Object.entries(enabledServices).map(([key, service]) => (
+          <LinkButton
+            key={key}
+            className={cx(
+              styles.button,
+              css`
+                background-color: ${service.bgColor};
+                border-color: ${service.borderColor};
+                &:hover {
+                  background-color: ${service.bgColor};
+                }
+              `
+            )}
+            href={`login/${service.hrefName ? service.hrefName : key}`}
+            target="_self"
+            fullWidth
+          >
+            <Icon
+              className={cx(
+                styles.buttonIcon,
+                css`
+                  border-color: ${service.borderColor};
+                `
+              )}
+              name={service.icon}
+            />
+            Sign in with {service.name}
+          </LinkButton>
+        ))}
+      </VerticalGroup>
+    );
   }
 
-  const serviceElements = serviceElementsEnabled.map((key) => {
-    const service: LoginService = loginServices()[key];
-    return (
-      <a
-        key={key}
-        className={cx(`btn btn-medium btn-service btn-service--${service.className || key}`, styles.button)}
-        href={`login/${service.hrefName ? service.hrefName : key}`}
-        target="_self"
-      >
-        <i className={`btn-service-icon fa fa-${service.icon ? service.icon : key}`} />
-        Sign in with {service.name}
-      </a>
-    );
-  });
-
-  const divider = LoginDivider();
-  return (
-    <>
-      {divider}
-      <div className={styles.container}>{serviceElements}</div>
-    </>
-  );
+  return null;
 };
