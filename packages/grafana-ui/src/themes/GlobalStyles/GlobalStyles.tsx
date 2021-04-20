2d0cb18f438fb3e44afff15f4b5e06f81fef34e3
thisisobate <obasiuche62@gmail.com>
2021-04-20 17:59:51 +0100

    Card: migrates styles from sass to emotion

--- packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
+++ packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import { useTheme } from '..';
 import { getElementStyles } from './elements';
+import { getCardStyles } from './card';
 
 /** @internal */
 export function GlobalStyles() {
   const theme = useTheme();
   const types = getElementStyles(theme.v2);
+  const cards = getCardStyles(theme.v2);
 
-  return <Global styles={[types]} />;
+  return <Global styles={[types, cards]} />;
 }
