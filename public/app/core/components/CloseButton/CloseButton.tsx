c40f6f11ce7407f350f67363e3f5d3dd71e63f69
Jack Westbrook <jack.westbrook@gmail.com>
2021-04-21 15:32:55 +0200

    chore(closebutton): post master merge use v2 theme

--- public/app/core/components/CloseButton/CloseButton.tsx
+++ public/app/core/components/CloseButton/CloseButton.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { css } from '@emotion/css';
-import { IconButton, useStyles } from '@grafana/ui';
-import { GrafanaTheme } from '@grafana/data';
+import { IconButton, useStyles2 } from '@grafana/ui';
+import { GrafanaThemeV2 } from '@grafana/data';
 
 type Props = {
   onClick: () => void;
 };
 
 export const CloseButton: React.FC<Props> = ({ onClick }) => {
-  const styles = useStyles(getStyles);
+  const styles = useStyles2(getStyles);
   return <IconButton className={styles} name="times" onClick={onClick} />;
 };
 
-const getStyles = (theme: GrafanaTheme) =>
+const getStyles = (theme: GrafanaThemeV2) =>
   css`
     position: absolute;
-    right: ${theme.v2.spacing(0.5)};
-    top: ${theme.v2.spacing(1)};
+    right: ${theme.spacing(0.5)};
+    top: ${theme.spacing(1)};
   `;
