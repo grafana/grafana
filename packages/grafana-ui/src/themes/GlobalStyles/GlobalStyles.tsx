438b403acc3e4a374872104a2ef1a510abdd5075
Uchechukwu Obasi <obasiuche62@gmail.com>
2021-04-21 15:39:56 +0100

    Migrates old Card sass file to GlobalStyles (#33183)
    * Card: migrates styles from sass to emotion
    
    * removed LayoutSelector component since it's not used in the codebase
    
    * updates card styles with new color name

--- packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
+++ packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import { useTheme2 } from '..';
 import { getElementStyles } from './elements';
+import { getCardStyles } from './card';
 
 /** @internal */
 export function GlobalStyles() {
   const theme = useTheme2();
   const types = getElementStyles(theme);
+  const cards = getCardStyles(theme);
 
-  return <Global styles={[types]} />;
+  return <Global styles={[types, cards]} />;
 }
