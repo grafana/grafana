ba5cd249d1ff7514931a4a3c50e7113f61b584ef
Uchechukwu Obasi <obasiuche62@gmail.com>
2021-04-22 14:45:35 +0100

    creates Themeable v2 type (#33268)

--- packages/grafana-ui/src/types/theme.ts
+++ packages/grafana-ui/src/types/theme.ts
@@ -1,5 +1,9 @@
-import { GrafanaTheme } from '@grafana/data';
+import { GrafanaTheme, GrafanaThemeV2 } from '@grafana/data';
 
 export interface Themeable {
   theme: GrafanaTheme;
+}
+
+export interface Themeable2 {
+  theme: GrafanaThemeV2;
 }
