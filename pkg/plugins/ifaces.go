51727adad56b5adf52ddae144bf3d10de1c994a0
Will Browne <wbrowne@users.noreply.github.com>
2021-04-22 17:24:06 +0200

    remove space

--- pkg/plugins/ifaces.go
+++ pkg/plugins/ifaces.go
@@ -74,7 +74,7 @@ type DataRequestHandler interface {
 
 type PluginInstaller interface {
 	// Install finds the plugin given the provided information
-	// and installs in the provided  plugins directory.
+	// and installs in the provided plugins directory.
 	Install(pluginID, version, pluginsDirectory, pluginZipURL, pluginRepoURL string) error
 	// Uninstall removes the specified plugin from the provided plugins directory.
 	Uninstall(pluginID, pluginPath string) error
