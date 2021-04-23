0d174437b4d39e814e89aaa369f06a3522e7bf86
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2021-04-23 10:08:23 +0200

    Loki: Refactor query editor so query field can be used independently (#33276)
    * Refactor Loki so LokiQueryField can be used independently
    
    * Refactor PromQueryEditor

--- public/app/plugins/datasource/loki/components/AnnotationsQueryEditor.tsx
+++ public/app/plugins/datasource/loki/components/AnnotationsQueryEditor.tsx
@@ -1,9 +1,9 @@
 // Libraries
 import React, { memo } from 'react';
-
 // Types
 import { LokiQuery } from '../types';
-import { LokiQueryFieldForm } from './LokiQueryFieldForm';
+import { LokiQueryField } from './LokiQueryField';
+import { LokiOptionFields } from './LokiOptionFields';
 import LokiDatasource from '../datasource';
 
 interface Props {
@@ -18,7 +18,7 @@ export const LokiAnnotationsQueryEditor = memo(function LokiAnnotationQueryEdito
   const { expr, maxLines, instant, datasource, onChange } = props;
 
   // Timerange to get existing labels from. Hard coding like this seems to be good enough right now.
-  const absolute = {
+  const absoluteRange = {
     from: Date.now() - 10000,
     to: Date.now(),
   };
@@ -31,13 +31,23 @@ export const LokiAnnotationsQueryEditor = memo(function LokiAnnotationQueryEdito
   };
   return (
     <div className="gf-form-group">
-      <LokiQueryFieldForm
+      <LokiQueryField
         datasource={datasource}
         query={queryWithRefId}
         onChange={onChange}
         onRunQuery={() => {}}
+        onBlur={() => {}}
         history={[]}
-        absoluteRange={absolute}
+        absoluteRange={absoluteRange}
+        ExtraFieldElement={
+          <LokiOptionFields
+            queryType={queryWithRefId.instant ? 'instant' : 'range'}
+            lineLimitValue={queryWithRefId?.maxLines?.toString() || ''}
+            query={queryWithRefId}
+            onRunQuery={() => {}}
+            onChange={onChange}
+          />
+        }
       />
     </div>
   );
