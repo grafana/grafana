1dd9d54fbca878082a1209777789c3c378fe9f2a
Elfo404 <me@giordanoricci.com>
2021-04-23 14:38:51 +0100

    Throttle onChange from SpectrumPalette

--- packages/grafana-ui/src/components/ColorPicker/SpectrumPalette.tsx
+++ packages/grafana-ui/src/components/ColorPicker/SpectrumPalette.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { RgbaStringColorPicker } from 'react-colorful';
 import tinycolor from 'tinycolor2';
@@ -6,6 +6,7 @@ import ColorInput from './ColorInput';
 import { GrafanaTheme, getColorForTheme } from '@grafana/data';
 import { css, cx } from '@emotion/css';
 import { useStyles, useTheme } from '../../themes';
+import { useThrottleFn } from 'react-use';
 
 export interface SpectrumPaletteProps {
   color: string;
@@ -13,18 +14,23 @@ export interface SpectrumPaletteProps {
 }
 
 const SpectrumPalette: React.FunctionComponent<SpectrumPaletteProps> = ({ color, onChange }) => {
+  const [currentColor, setColor] = useState(color);
+  useThrottleFn(onChange, 500, [currentColor]);
+
   const theme = useTheme();
   const styles = useStyles(getStyles);
 
   const rgbaString = useMemo(() => {
-    return color.startsWith('rgba') ? color : tinycolor(getColorForTheme(color, theme)).toRgbString();
-  }, [color, theme]);
+    return currentColor.startsWith('rgba')
+      ? currentColor
+      : tinycolor(getColorForTheme(currentColor, theme)).toRgbString();
+  }, [currentColor, theme]);
 
   return (
     <>
-      <RgbaStringColorPicker className={cx(styles.root)} color={rgbaString} onChange={onChange} />
+      <RgbaStringColorPicker className={cx(styles.root)} color={rgbaString} onChange={setColor} />
 
-      <ColorInput theme={theme} color={color} onChange={onChange} className={styles.colorInput} />
+      <ColorInput theme={theme} color={currentColor} onChange={setColor} className={styles.colorInput} />
     </>
   );
 };
