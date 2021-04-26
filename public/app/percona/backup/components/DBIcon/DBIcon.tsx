da3373afaaf29a6ca4175865de51e45c8863e60a
fabio-silva <ffjs1993@gmail.com>
2021-04-26 10:13:23 +0100

    PMM-7953: disabled backup actions (#125)
    * PMM-7953 add disabled style to DBIcon
    
    * PMM-7953 add disabled style to restore action

--- public/app/percona/backup/components/DBIcon/DBIcon.tsx
+++ public/app/percona/backup/components/DBIcon/DBIcon.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
-import { Tooltip } from '@grafana/ui';
+import { Tooltip, useStyles } from '@grafana/ui';
+import { cx } from 'emotion';
 import { DBIconProps, DBIconMap } from './DBIcon.types';
+import { getStyles } from './DBIcon.styles';
 import { Edit, Delete, See, Restore, Backup } from './assets';
 
 const Icons: DBIconMap = {
@@ -11,18 +13,23 @@ const Icons: DBIconMap = {
   backup: Backup,
 };
 
-export const DBIcon: FC<DBIconProps> = ({ type, size, tooltipText, ...rest }) => {
+export const DBIcon: FC<DBIconProps> = ({ type, size, tooltipText, disabled, ...rest }) => {
   if (!Icons[type]) {
     return null;
   }
+  const styles = useStyles(getStyles);
   const Icon = Icons[type];
-  const IconEl = <Icon size={size} {...rest} />;
+  const IconEl = (
+    <span className={cx({ [styles.disabled]: disabled })}>
+      <Icon size={size} {...rest} />
+    </span>
+  );
 
   return tooltipText ? (
     <Tooltip placement="top" content={tooltipText}>
-      <span>{IconEl}</span>
+      {IconEl}
     </Tooltip>
   ) : (
-    IconEl
+    <>{IconEl}</>
   );
 };
