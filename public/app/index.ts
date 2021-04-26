56104eb8a3a481d547b8fb9cbf715f8ec8e11018
Arve Knudsen <arve.knudsen@gmail.com>
2021-04-26 11:38:24 +0200

    CSP: Set nonce attribute on Webpack injected bundles (#33298)
    * CSP: Set __webpack_nonce__
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- public/app/index.ts
+++ public/app/index.ts
@@ -1,4 +1,5 @@
 declare let __webpack_public_path__: string;
+declare let __webpack_nonce__: string;
 
 /**
  * Check if we are hosting files on cdn and set webpack public path
@@ -10,6 +11,10 @@ if ((window as any).public_cdn_path) {
 // This is a path to the public folder without '/build'
 (window as any).__grafana_public_path__ =
   __webpack_public_path__.substring(0, __webpack_public_path__.lastIndexOf('build/')) || __webpack_public_path__;
+
+if ((window as any).nonce) {
+  __webpack_nonce__ = (window as any).nonce;
+}
 
 import app from './app';
 app.init();
