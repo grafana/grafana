759a501564be89e0820c72fdb64e925fda606214
Torkel Ödegaard <torkel@grafana.org>
2021-04-27 10:09:37 +0200

    Singlstat: Fixes all the singlestat migration issues by moving it to DashboardMigration (#33349)
    * Singlstat: Fixes all the singlestat migration issues by doing during DashboardMigration
    
    * fixes
    
    * Updated snapshot

--- public/app/core/constants.ts
+++ public/app/core/constants.ts
@@ -1,6 +1,3 @@
-import { PanelModel } from '../features/dashboard/state';
-import { config } from '@grafana/runtime';
-
 export const GRID_CELL_HEIGHT = 30;
 export const GRID_CELL_VMARGIN = 8;
 export const GRID_COLUMN_COUNT = 24;
@@ -17,18 +14,3 @@ export const PANEL_BORDER = 2;
 export const EDIT_PANEL_ID = 23763571993;
 
 export const DEFAULT_PER_PAGE_PAGINATION = 8;
-
-export const DEPRECATED_PANELS: Record<string, (panel: PanelModel) => string> = {
-  singlestat: (panel: PanelModel) => {
-    // If 'grafana-singlestat-panel' exists, move to that
-    if (config.panels['grafana-singlestat-panel']) {
-      return 'grafana-singlestat-panel';
-    }
-
-    // Otheriwse use gauge or stat panel
-    if ((panel as any).gauge && (panel as any).gauge.show) {
-      return 'gauge';
-    }
-    return 'stat';
-  },
-};
