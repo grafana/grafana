bd0b61ab9d48c0275eac6153c98fd4e6fc7d34a3
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2021-04-27 19:19:14 +0200

    User-Agent middleware that sets Grafana specific user agent

--- pkg/infra/httpclientprovider/http_client_provider.go
+++ pkg/infra/httpclientprovider/http_client_provider.go
@@ -1,14 +1,18 @@
 package httpclientprovider
 
 import (
+	"fmt"
+
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 // New creates a new HTTP client provider with pre-configured middlewares.
 func New(cfg *setting.Cfg) httpclient.Provider {
+	userAgent := fmt.Sprintf("Grafana/%s", cfg.BuildVersion)
 	middlewares := []httpclient.Middleware{
 		DataSourceMetricsMiddleware(),
+		SetUserAgentMiddleware(userAgent),
 		httpclient.BasicAuthenticationMiddleware(),
 		httpclient.CustomHeadersMiddleware(),
 	}
