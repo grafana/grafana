3d928bcf226f2663653fdef6c4a79dcb4daf7f4e
Ryan McKinley <ryantxu@gmail.com>
2021-04-27 21:56:36 -0700

    sync sync sync

--- packages/grafana-ui/src/components/GraphNG/events.ts
+++ packages/grafana-ui/src/components/GraphNG/events.ts
@@ -20,11 +20,6 @@ class UPlotGlobalEvents {
   readonly localPrefix = 'local__';
 
   filter = (type: string, src: uPlot, x: number, y: number, w: number, h: number, dataIdx: number) => {
-    // emit to own or some other sync group
-    let syncKey = src.cursor.sync!.key;
-
-    // console.log(syncKey, type, Object.keys(src.scales));
-
     if (this.events.observers.length) {
       this.events.next({
         type,
@@ -37,6 +32,8 @@ class UPlotGlobalEvents {
       });
     }
 
+    // emit to own or some other sync group
+    const syncKey = src.cursor.sync!.key;
     if (syncKey.startsWith(this.localPrefix)) {
       return false; // don't process events we don't care about
     }
@@ -48,10 +45,23 @@ class UPlotGlobalEvents {
   // Broadcast the values to all global listeners
   publish = (values: KeyValue<number>) => {
     const sync = uPlot.sync(this.globalKey);
-    if (!sync) {
+    if (!sync || !sync.key) {
       return;
     }
-    console.log('TODO sync uplot>>', Object.keys(values));
+
+    // Find all global clients and broadcast
+    for (const client of sync.plots) {
+      const scales = client.cursor.sync?.scales;
+      if (scales?.length === 2) {
+        const x = values[scales[0]];
+        const y = values[scales[1]];
+
+        const px = x == null ? -10 : client.valToPos(x, scales[0]);
+        const py = y == null ? -10 : client.valToPos(y, scales[1]);
+
+        client.setCursor({ left: px, top: py });
+      }
+    }
   };
 }
 
