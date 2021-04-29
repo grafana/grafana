40c1d5925b71de8d18f93e78c0b2437c0566e9cf
Gábor Farkas <gabor.farkas@gmail.com>
2021-04-29 14:55:42 +0200

    removed debug code

--- public/app/plugins/datasource/influxdb/influxQLMetadataQuery.ts
+++ public/app/plugins/datasource/influxdb/influxQLMetadataQuery.ts
@@ -2,33 +2,6 @@ import { InfluxQueryTag } from './types';
 import InfluxDatasource from './datasource';
 import { InfluxQueryBuilder } from './query_builder';
 
-class InvalidMetadataError extends Error {
-  constructor(obj: unknown) {
-    console.error('invalid data', obj);
-    super('invalid data');
-  }
-}
-
-// to make sure the data returned from the server is the correct form.
-// TODO: remove this when we are finished (probably)
-function assertMetadataResponse(data: unknown): Array<{ text: string }> {
-  if (!Array.isArray(data)) {
-    throw new InvalidMetadataError(data);
-  }
-
-  return data.map((item) => {
-    if (item == null || typeof item !== 'object') {
-      throw new InvalidMetadataError(data);
-    }
-
-    const { text } = item;
-    if (typeof text !== 'string') {
-      throw new InvalidMetadataError(data);
-    }
-    return item;
-  });
-}
-
 const runExploreQuery = async (
   type: string,
   withKey: string | undefined,
@@ -37,7 +10,7 @@ const runExploreQuery = async (
 ): Promise<Array<{ text: string }>> => {
   const builder = new InfluxQueryBuilder(target, datasource.database);
   const q = builder.buildExploreQuery(type, withKey);
-  return assertMetadataResponse(await datasource.metricFindQuery(q));
+  return await datasource.metricFindQuery(q);
 };
 
 export async function getAllPolicies(datasource: InfluxDatasource): Promise<string[]> {
