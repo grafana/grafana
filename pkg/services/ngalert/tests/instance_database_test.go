0b7409cc1f9a78ca93d053561c42514922c3b54d
kyle <kyle@grafana.com>
2021-04-29 13:14:00 -0400

    Alerting: (wip) Load annotations from rule into State cache for https://github.com/grafana/alerting-squad/issues/127

--- pkg/services/ngalert/tests/instance_database_test.go
+++ pkg/services/ngalert/tests/instance_database_test.go
@@ -45,8 +45,8 @@ func TestAlertInstanceOperations(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, saveCmd.Labels, getCmd.Result.Labels)
-		require.Equal(t, alertDefinition1.OrgID, getCmd.Result.DefinitionOrgID)
-		require.Equal(t, alertDefinition1.UID, getCmd.Result.DefinitionUID)
+		require.Equal(t, alertDefinition1.OrgID, getCmd.Result.RuleOrgID)
+		require.Equal(t, alertDefinition1.UID, getCmd.Result.RuleUID)
 	})
 
 	t.Run("can save and read new alert instance with no labels", func(t *testing.T) {
@@ -67,8 +67,8 @@ func TestAlertInstanceOperations(t *testing.T) {
 		err = dbstore.GetAlertInstance(getCmd)
 		require.NoError(t, err)
 
-		require.Equal(t, alertDefinition2.OrgID, getCmd.Result.DefinitionOrgID)
-		require.Equal(t, alertDefinition2.UID, getCmd.Result.DefinitionUID)
+		require.Equal(t, alertDefinition2.OrgID, getCmd.Result.RuleOrgID)
+		require.Equal(t, alertDefinition2.UID, getCmd.Result.RuleUID)
 		require.Equal(t, saveCmd.Labels, getCmd.Result.Labels)
 	})
 
@@ -156,8 +156,8 @@ func TestAlertInstanceOperations(t *testing.T) {
 
 		require.Len(t, listQuery.Result, 1)
 
-		require.Equal(t, saveCmdTwo.DefinitionOrgID, listQuery.Result[0].DefinitionOrgID)
-		require.Equal(t, saveCmdTwo.DefinitionUID, listQuery.Result[0].DefinitionUID)
+		require.Equal(t, saveCmdTwo.DefinitionOrgID, listQuery.Result[0].RuleOrgID)
+		require.Equal(t, saveCmdTwo.DefinitionUID, listQuery.Result[0].RuleDefinitionUID)
 		require.Equal(t, saveCmdTwo.Labels, listQuery.Result[0].Labels)
 		require.Equal(t, saveCmdTwo.State, listQuery.Result[0].CurrentState)
 		require.NotEmpty(t, listQuery.Result[0].DefinitionTitle)
