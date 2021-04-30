f22e627aa9073289ca515a4a6fc216028364f7e4
Ryan McKinley <ryantxu@gmail.com>
2021-04-29 20:12:37 -0700

    Models: add cue spec for text, news, and dashlist panels (#32777)

--- public/app/plugins/panel/news/module.tsx
+++ public/app/plugins/panel/news/module.tsx
@@ -1,10 +1,10 @@
 import { isString } from 'lodash';
 import { PanelPlugin } from '@grafana/data';
 import { NewsPanel } from './NewsPanel';
-import { NewsOptions } from './types';
+import { PanelOptions, defaultPanelOptions } from './models.gen';
 import { DEFAULT_FEED_URL, PROXY_PREFIX } from './constants';
 
-export const plugin = new PanelPlugin<NewsOptions>(NewsPanel).setPanelOptions((builder) => {
+export const plugin = new PanelPlugin<PanelOptions>(NewsPanel).setPanelOptions((builder) => {
   builder
     .addTextInput({
       path: 'feedUrl',
@@ -13,13 +13,15 @@ export const plugin = new PanelPlugin<NewsOptions>(NewsPanel).setPanelOptions((b
       settings: {
         placeholder: DEFAULT_FEED_URL,
       },
+      defaultValue: defaultPanelOptions.feedUrl,
     })
     .addBooleanSwitch({
       path: 'useProxy',
       name: 'Use Proxy',
       description: 'If the feed is unable to connect, consider a CORS proxy',
-      showIf: (currentConfig: NewsOptions) => {
+      showIf: (currentConfig: PanelOptions) => {
         return isString(currentConfig.feedUrl) && !currentConfig.feedUrl.startsWith(PROXY_PREFIX);
       },
+      defaultValue: defaultPanelOptions.useProxy,
     });
 });
