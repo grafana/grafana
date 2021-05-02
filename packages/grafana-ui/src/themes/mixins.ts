76603e6bb125da6a1e967a4327ce415804098bef
Torkel Ödegaard <torkel@grafana.org>
2021-05-02 17:45:24 +0200

    Theme: Fix for Graph tooltip background (#33601)

--- packages/grafana-ui/src/themes/mixins.ts
+++ packages/grafana-ui/src/themes/mixins.ts
@@ -65,7 +65,8 @@ export function getFocusStyles(theme: GrafanaThemeV2): CSSObject {
 // max-width is set up based on .grafana-tooltip class that's used in dashboard
 export const getTooltipContainerStyles = (theme: GrafanaThemeV2) => `
   overflow: hidden;
-  background: ${theme.components.tooltip.background};
+  background: ${theme.colors.background.secondary};
+  box-shadow: ${theme.shadows.z2};
   max-width: 800px;
   padding: ${theme.spacing(1)};
   border-radius: ${theme.shape.borderRadius()};
