2c951ec7dad8c453264cd3c54c87e2f19baab95a
Torkel Ödegaard <torkel@grafana.org>
2021-05-03 09:33:27 +0200

    Update DashboardLinks.tsx

--- public/app/features/dashboard/components/SubMenu/DashboardLinks.tsx
+++ public/app/features/dashboard/components/SubMenu/DashboardLinks.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useReducer } from 'react';
 import { Icon, IconName, Tooltip } from '@grafana/ui';
-import { sanitize, sanitizeUrl } from '@grafana/data/src/text/sanitize';
+import { sanitizeUrl } from '@grafana/data/src/text/sanitize';
 import { DashboardLinksDashboard } from './DashboardLinksDashboard';
 import { getLinkSrv } from '../../../panel/panellinks/link_srv';
 
