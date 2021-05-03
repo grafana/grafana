a6782e15dc1710bb1aa7cb224d37243ef596ac40
fabio-silva <ffjs1993@gmail.com>
2021-05-03 11:29:39 +0100

    PMM-8008 remove "test results" field (#133)

--- public/app/percona/backup/components/BackupInventory/BackupInventoryDetails/BackupInventoryDetails.tsx
+++ public/app/percona/backup/components/BackupInventory/BackupInventoryDetails/BackupInventoryDetails.tsx
@@ -3,8 +3,6 @@ import React, { FC } from 'react';
 import { useStyles } from '@grafana/ui';
 import { formatDataModel } from 'app/percona/backup/Backup.utils';
 
-import { Status } from '../../Status';
-
 import { Messages } from './BackupInventoryDetails.messages';
 import { getStyles } from './BackupInventoryDetails.styles';
 import { BackupInventoryDetailsProps } from './BackupInventoryDetails.types';
@@ -17,9 +15,6 @@ export const BackupInventoryDetails: FC<BackupInventoryDetailsProps> = ({ name, 
     <div className={styles.detailsWrapper} data-qa="backup-artifact-details-wrapper">
       <span data-qa="backup-artifact-details-name">
         <span className={styles.detailLabel}>{Messages.backupName}</span> <span>{name}</span>
-      </span>
-      <span data-qa="backup-artifact-details-status">
-        <span className={styles.detailLabel}>{Messages.testResuts}</span> <Status status={status} />
       </span>
       <span data-qa="backup-artifact-details-data-model">
         <span className={styles.detailLabel}>{Messages.dataModel}</span> <span>{dataModelMsg}</span>
