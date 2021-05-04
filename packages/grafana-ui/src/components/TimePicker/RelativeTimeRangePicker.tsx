9d910e4cbcad86bd17f74c30f08c3e34e3bf9306
Marcus Andersson <marcus.andersson@grafana.com>
2021-05-04 14:04:18 +0200

    added basic structure to handle open/close of time range picker.

--- packages/grafana-ui/src/components/TimePicker/RelativeTimeRangePicker.tsx
+++ packages/grafana-ui/src/components/TimePicker/RelativeTimeRangePicker.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, { ReactElement, useState } from 'react';
+import React, { FormEvent, ReactElement, useCallback, useState } from 'react';
 import { css } from '@emotion/css';
 
 // Components
@@ -10,7 +10,8 @@ import { useStyles2 } from '../../themes/ThemeContext';
 
 // Types
 import { RelativeTimeRange, GrafanaThemeV2 } from '@grafana/data';
-import { ToolbarButton } from '../Button';
+import { ButtonGroup, ToolbarButton } from '../Button';
+import { ClickOutsideWrapper } from '../ClickOutsideWrapper/ClickOutsideWrapper';
 
 export interface RelativeTimeRangePickerProps {
   timeRange: RelativeTimeRange;
@@ -19,26 +20,37 @@ export interface RelativeTimeRangePickerProps {
 
 export function RelativeTimeRangePicker(props: RelativeTimeRangePickerProps): ReactElement | null {
   const styles = useStyles2(getStyles);
-  const [isOpen, setIsOpen] = useState(false);
 
-  if (!isOpen) {
-    return (
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = useCallback(() => setIsOpen(false), []);
+
+  const onOpen = useCallback(
+    (event: FormEvent<HTMLButtonElement>) => {
+      event.stopPropagation();
+      event.preventDefault();
+      setIsOpen(!isOpen);
+    },
+    [isOpen]
+  );
+
+  return (
+    <ButtonGroup className={styles.container}>
       <Tooltip content={'TBA'} placement="bottom">
-        <ToolbarButton
-          aria-label="TimePicker Open Button"
-          onClick={() => setIsOpen(true)}
-          icon="clock-nine"
-          isOpen={isOpen}
-        >
+        <ToolbarButton aria-label="TimePicker Open Button" onClick={onOpen} icon="clock-nine" isOpen={isOpen}>
           <span className={styles.container}>
             <span>Add human readable version of timeRange</span>
           </span>
         </ToolbarButton>
       </Tooltip>
-    );
-  }
-
-  return null;
+      {isOpen && (
+        <ClickOutsideWrapper includeButtonPress={false} onClick={onClose}>
+          <div className={styles.content}>
+            <span>Content should go here</span>
+          </div>
+        </ClickOutsideWrapper>
+      )}
+    </ButtonGroup>
+  );
 }
 
 const getStyles = (theme: GrafanaThemeV2) => {
@@ -47,6 +59,17 @@ const getStyles = (theme: GrafanaThemeV2) => {
       position: relative;
       display: flex;
       vertical-align: middle;
+    `,
+    content: css`
+      background: ${theme.colors.background.primary};
+      box-shadow: ${theme.shadows.z3};
+      position: absolute;
+      z-index: ${theme.zIndex.dropdown};
+      width: 546px;
+      top: 116%;
+      border-radius: 2px;
+      border: 1px solid ${theme.colors.border.weak};
+      right: 0;
     `,
   };
 };
