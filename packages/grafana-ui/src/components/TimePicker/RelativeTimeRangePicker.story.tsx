9ade8626aa32054829808997e6bd7a2ac976bd55
Marcus Andersson <marcus.andersson@grafana.com>
2021-05-04 13:47:01 +0200

    adding placeholder for relative time range.

---
+++ packages/grafana-ui/src/components/TimePicker/RelativeTimeRangePicker.story.tsx
@@ -0,0 +1,11 @@
+import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
+import { RelativeTimeRangePicker } from './RelativeTimeRangePicker';
+
+export default {
+  title: 'Pickers and Editors/TimePickers/RelativeTimeRangePicker',
+  component: RelativeTimeRangePicker,
+  decorators: [withCenteredStory],
+  parameters: {
+    docs: {},
+  },
+};
---
+++ packages/grafana-ui/src/components/TimePicker/RelativeTimeRangePicker.tsx
@@ -0,0 +1,53 @@
+// Libraries
+import React, { ReactElement, useState } from 'react';
+import { css } from '@emotion/css';
+
+// Components
+import { Tooltip } from '../Tooltip/Tooltip';
+
+// Utils & Services
+import { useStyles2 } from '../../themes/ThemeContext';
+
+// Types
+import { RelativeTimeRange, GrafanaThemeV2 } from '@grafana/data';
+import { Themeable } from '../../types';
+import { ToolbarButton } from '../Button';
+
+export interface RelativeTimeRangePickerProps extends Themeable {
+  timeRange: RelativeTimeRange;
+  onChange: (timeRange: RelativeTimeRange) => void;
+}
+
+export function RelativeTimeRangePicker(props: RelativeTimeRangePickerProps): ReactElement | null {
+  const styles = useStyles2(getStyles);
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (!isOpen) {
+    return (
+      <Tooltip content={'TBA'} placement="bottom">
+        <ToolbarButton
+          aria-label="TimePicker Open Button"
+          onClick={() => setIsOpen(true)}
+          icon="clock-nine"
+          isOpen={isOpen}
+        >
+          <span className={styles.container}>
+            <span>Add human readable version of timeRange</span>
+          </span>
+        </ToolbarButton>
+      </Tooltip>
+    );
+  }
+
+  return null;
+}
+
+const getStyles = (theme: GrafanaThemeV2) => {
+  return {
+    container: css`
+      position: relative;
+      display: flex;
+      vertical-align: middle;
+    `,
+  };
+};
