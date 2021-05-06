ff7f9825974e782c04e030a639475803143c1a5f
Josh Hunt <joshhunt@users.noreply.github.com>
2021-05-06 20:11:27 +0100

    AzureMonitor: Create read-only React view for Insights Analytics (#33719)
    * AzureMonitor: Create read-only React view for Insights Analytics
    
    * Fix spacing

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/QueryEditor/QueryEditor.tsx
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/QueryEditor/QueryEditor.tsx
@@ -1,4 +1,4 @@
-import { Alert, VerticalGroup } from '@grafana/ui';
+import { Alert } from '@grafana/ui';
 import React from 'react';
 import Datasource from '../../datasource';
 import { AzureMonitorQuery, AzureQueryType, AzureMonitorOption, AzureMonitorErrorish } from '../../types';
@@ -7,6 +7,8 @@ import QueryTypeField from './QueryTypeField';
 import useLastError from '../../utils/useLastError';
 import LogsQueryEditor from '../LogsQueryEditor';
 import ApplicationInsightsEditor from '../ApplicationInsightsEditor';
+import InsightsAnalyticsEditor from '../InsightsAnalyticsEditor';
+import { Space } from '../Space';
 
 interface BaseQueryEditorProps {
   query: AzureMonitorQuery;
@@ -27,22 +29,23 @@ const QueryEditor: React.FC<BaseQueryEditorProps> = ({ query, datasource, onChan
     <div data-testid="azure-monitor-query-editor">
       <QueryTypeField query={query} onQueryChange={onChange} />
 
-      <VerticalGroup>
-        <EditorForQueryType
-          subscriptionId={subscriptionId}
-          query={query}
-          datasource={datasource}
-          onChange={onChange}
-          variableOptionGroup={variableOptionGroup}
-          setError={setError}
-        />
+      <EditorForQueryType
+        subscriptionId={subscriptionId}
+        query={query}
+        datasource={datasource}
+        onChange={onChange}
+        variableOptionGroup={variableOptionGroup}
+        setError={setError}
+      />
 
-        {errorMessage && (
+      {errorMessage && (
+        <>
+          <Space v={2} />
           <Alert severity="error" title="An error occurred while requesting metadata from Azure Monitor">
             {errorMessage}
           </Alert>
-        )}
-      </VerticalGroup>
+        </>
+      )}
     </div>
   );
 };
@@ -87,6 +90,9 @@ const EditorForQueryType: React.FC<EditorForQueryTypeProps> = ({
 
     case AzureQueryType.ApplicationInsights:
       return <ApplicationInsightsEditor query={query} />;
+
+    case AzureQueryType.InsightsAnalytics:
+      return <InsightsAnalyticsEditor query={query} />;
   }
 
   return null;
