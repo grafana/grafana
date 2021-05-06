3a4cc1bcfe79cc444346ff0bf7f5c5f0bcbf6652
Marcus Andersson <marcus.andersson@grafana.com>
2021-05-06 15:38:27 +0200

    refactored the tests.

--- packages/grafana-ui/src/components/TimePicker/RelativeTimeRangePicker/RelativeTimeRangePicker.test.tsx
+++ packages/grafana-ui/src/components/TimePicker/RelativeTimeRangePicker/RelativeTimeRangePicker.test.tsx
@@ -1,50 +1,38 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import React, { useState } from 'react';
+import { render, fireEvent, RenderResult } from '@testing-library/react';
 import { RelativeTimeRangePicker } from './RelativeTimeRangePicker';
+import { RelativeTimeRange } from '@grafana/data';
+
+function setup(initial: RelativeTimeRange = { from: 900, to: 0 }): RenderResult {
+  const StatefulPicker: React.FC<{}> = () => {
+    const [value, setValue] = useState<RelativeTimeRange>(initial);
+    return <RelativeTimeRangePicker timeRange={value} onChange={setValue} />;
+  };
+
+  return render(<StatefulPicker />);
+}
 
 describe('RelativeTimePicker', () => {
   it('should render the picker button with an user friendly text', () => {
-    const { getByText } = render(
-      <RelativeTimeRangePicker
-        onChange={(value) => {}}
-        timeRange={{
-          from: 900,
-          to: 0,
-        }}
-      />
-    );
-
+    const { getByText } = setup({ from: 900, to: 0 });
     expect(getByText('Last 900 seconds')).toBeInTheDocument();
   });
 
   it('should open the picker when clicking the button', () => {
-    const { getByText } = render(
-      <RelativeTimeRangePicker
-        onChange={(value) => {}}
-        timeRange={{
-          from: 900,
-          to: 0,
-        }}
-      />
-    );
-
+    const { getByText } = setup({ from: 900, to: 0 });
     fireEvent.click(getByText('Last 900 seconds'));
-
     expect(getByText('Specify time range')).toBeInTheDocument();
     expect(getByText('Example time ranges')).toBeInTheDocument();
   });
 
   it('should not have open picker without clicking the button', () => {
-    const { queryByText } = render(
-      <RelativeTimeRangePicker
-        onChange={(value) => {}}
-        timeRange={{
-          from: 900,
-          to: 0,
-        }}
-      />
-    );
+    const { queryByText } = setup({ from: 900, to: 0 });
+    expect(queryByText('Specify time range')).toBeNull();
+    expect(queryByText('Example time ranges')).toBeNull();
+  });
 
+  it('should update time range when applying new form values', () => {
+    const { queryByText } = setup({ from: 900, to: 0 });
     expect(queryByText('Specify time range')).toBeNull();
     expect(queryByText('Example time ranges')).toBeNull();
   });
