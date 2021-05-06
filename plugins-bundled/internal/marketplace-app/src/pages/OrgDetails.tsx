116ed0c15212015f7e8d437b8c7a025dc1e58bfb
Jack Westbrook <jack.westbrook@gmail.com>
2021-05-06 13:53:01 +0200

    refactor(marketplace): clean up org details component

--- plugins-bundled/internal/marketplace-app/src/pages/OrgDetails.tsx
+++ plugins-bundled/internal/marketplace-app/src/pages/OrgDetails.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { css } from '@emotion/css';
 
-import { AppRootProps, GrafanaTheme } from '@grafana/data';
+import { AppRootProps, GrafanaTheme2 } from '@grafana/data';
 
 import { PluginList } from '../components/PluginList';
 import { usePlugins } from '../hooks/usePlugins';
 import { useOrg } from '../hooks/useOrg';
 import { MarketplaceAppSettings } from '../types';
 
-import { stylesFactory, useTheme } from '@grafana/ui';
+import { useStyles2 } from '@grafana/ui';
 import { Page } from 'components/Page';
 
 export const OrgDetails = ({ query, meta }: AppRootProps) => {
@@ -17,40 +17,37 @@ export const OrgDetails = ({ query, meta }: AppRootProps) => {
 
   const orgData = useOrg(orgSlug);
   const pluginsData = usePlugins({ includeUnsigned });
-  const theme = useTheme();
-  const styles = getStyles(theme);
+  const styles = useStyles2(getStyles);
 
   const plugins = pluginsData.items.filter((plugin) => plugin.orgSlug === orgSlug);
 
   return (
     <Page>
-      <div
-        className={css`
-          display: flex;
-          margin-bottom: 64px;
-          align-items: center;
-        `}
-      >
-        <img
-          src={orgData.org?.avatarUrl}
-          className={css`
-            object-fit: cover;
-            width: 100%;
-            height: 64px;
-            max-width: 64px;
-          `}
-        />
-        <h1 className={styles.header}>{orgData.org?.name}</h1>
+      <div className={styles.header}>
+        <img src={orgData.org?.avatarUrl} className={styles.img} />
+        <h1 className={styles.orgName}>{orgData.org?.name}</h1>
       </div>
       <PluginList plugins={plugins} />
     </Page>
   );
 };
 
-const getStyles = stylesFactory((theme: GrafanaTheme) => {
+const getStyles = (theme: GrafanaTheme2) => {
   return {
     header: css`
-      margin-left: ${theme.spacing.lg};
+      align-items: center;
+      display: flex;
+      margin-bottom: ${theme.spacing(3)};
+      margin-top: ${theme.spacing(3)};
+    `,
+    img: css`
+      height: 64px;
+      max-width: 64px;
+      object-fit: cover;
+      width: 100%;
+    `,
+    orgName: css`
+      margin-left: ${theme.spacing(3)};
     `,
   };
-});
+};
