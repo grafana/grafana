13b988fc80d2d2b7a5606a8abd0e72157581b4a5
Sergey Kostrukov <sergey@kostrukov.com>
2021-05-06 19:06:50 -0700

    Configuration for Azure token provider

--- pkg/api/pluginproxy/token_provider_azure.go
+++ pkg/api/pluginproxy/token_provider_azure.go
@@ -1,24 +1,31 @@
 package pluginproxy
 
 import (
+	"context"
+
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/plugins"
+	"github.com/grafana/grafana/pkg/setting"
 )
 
 type azureAccessTokenProvider struct {
 	datasourceId             int64
 	datasourceVersion        int
+	ctx                      context.Context
+	cfg                      *setting.Cfg
 	route                    *plugins.AppPluginRoute
 	authParams               *plugins.JwtTokenAuth
 	appIdentityTokenProvider accessTokenProvider
 }
 
-func newAzureAccessTokenProvider(ds *models.DataSource, pluginRoute *plugins.AppPluginRoute,
-	authParams *plugins.JwtTokenAuth) *azureAccessTokenProvider {
+func newAzureAccessTokenProvider(ctx context.Context, cfg *setting.Cfg, ds *models.DataSource,
+	pluginRoute *plugins.AppPluginRoute, authParams *plugins.JwtTokenAuth) *azureAccessTokenProvider {
 	appIdentityTokenProvider := newGenericAccessTokenProvider(ds, pluginRoute, authParams)
 	return &azureAccessTokenProvider{
 		datasourceId:             ds.Id,
 		datasourceVersion:        ds.Version,
+		ctx:                      ctx,
+		cfg:                      cfg,
 		route:                    pluginRoute,
 		authParams:               authParams,
 		appIdentityTokenProvider: appIdentityTokenProvider,
