48b0735146274af81e0ee1f4286680b55231e1e2
Erik Sundell <erik.sundell@grafana.com>
2021-05-10 14:51:19 +0200

    CloudMonitoring: Migrate config editor from angular to react (#33645)
    * fix broken config ctrl
    
    * replace angular config with react config editor
    
    * remove not used code
    
    * add extra linebreak
    
    * add noopener to link
    
    * only test jwt props that we actually need

--- public/app/plugins/datasource/cloud-monitoring/module.ts
+++ public/app/plugins/datasource/cloud-monitoring/module.ts
@@ -1,13 +1,14 @@
 import { DataSourcePlugin } from '@grafana/data';
 import CloudMonitoringDatasource from './datasource';
 import { QueryEditor } from './components/QueryEditor';
-import { CloudMonitoringConfigCtrl } from './config_ctrl';
+import { ConfigEditor } from './components/ConfigEditor/ConfigEditor';
+
 import { CloudMonitoringAnnotationsQueryCtrl } from './annotations_query_ctrl';
 import { CloudMonitoringVariableQueryEditor } from './components/VariableQueryEditor';
 import { CloudMonitoringQuery } from './types';
 
 export const plugin = new DataSourcePlugin<CloudMonitoringDatasource, CloudMonitoringQuery>(CloudMonitoringDatasource)
   .setQueryEditor(QueryEditor)
-  .setConfigCtrl(CloudMonitoringConfigCtrl)
+  .setConfigEditor(ConfigEditor)
   .setAnnotationQueryCtrl(CloudMonitoringAnnotationsQueryCtrl)
   .setVariableQueryEditor(CloudMonitoringVariableQueryEditor);
