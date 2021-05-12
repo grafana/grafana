09da6ca428f3c91efffca41a806c53286ea7d37d
Torkel Ödegaard <torkel@grafana.com>
2021-05-12 11:16:55 +0200

    Use old named color names and colors to begin with

--- packages/grafana-data/src/themes/createVizColors.test.ts
+++ packages/grafana-data/src/themes/createVizColors.test.ts
@@ -3,19 +3,30 @@ import { createVizColors } from './createVizColors';
 
 describe('createVizColors', () => {
   const darkThemeColors = createColors({});
+  const vizColors = createVizColors(darkThemeColors);
 
   it('Can map named colors to real color', () => {
-    const vizColors = createVizColors(darkThemeColors);
-    expect(vizColors.byName('green3')).toBe('#2DC88F');
+    expect(vizColors.byName('green')).toBe('#73BF69');
   });
 
   it('Can map named colors using old aliases to real color', () => {
-    const vizColors = createVizColors(darkThemeColors);
-    expect(vizColors.byName('dark-green')).toBe('#1B855E');
+    expect(vizColors.byName('dark-green')).toBe('#37872D');
   });
 
   it('Can get color by index', () => {
-    const vizColors = createVizColors(darkThemeColors);
-    expect(vizColors.byIndex(133)).toBe('#ECBB09');
+    expect(vizColors.byIndex(133)).toBe('#FADE2A');
+  });
+
+  it('returns color if specified as hex or rgb/a', () => {
+    expect(vizColors.byName('#ff0000')).toBe('#ff0000');
+    expect(vizColors.byName('#ff0000')).toBe('#ff0000');
+    expect(vizColors.byName('#FF0000')).toBe('#FF0000');
+    expect(vizColors.byName('#CCC')).toBe('#CCC');
+    expect(vizColors.byName('rgb(0,0,0)')).toBe('rgb(0,0,0)');
+    expect(vizColors.byName('rgba(0,0,0,1)')).toBe('rgba(0,0,0,1)');
+  });
+
+  it('returns hex for named color that is not a part of named colors palette', () => {
+    expect(vizColors.byName('lime')).toBe('#00ff00');
   });
 });
