ca1e4b690dc1bfa0b1ae29b49b7ff28bef3a3cc3
Ganesh Vernekar <ganeshvern@gmail.com>
2021-05-12 20:13:06 +0530

    Fix the failing tests
    Signed-off-by: Ganesh Vernekar <ganeshvern@gmail.com>

--- pkg/tests/api/alerting/api_notification_channel_test.go
+++ pkg/tests/api/alerting/api_notification_channel_test.go
@@ -18,13 +18,253 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/bus"
-	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/models"
 	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 	ngmodels "github.com/grafana/grafana/pkg/services/ngalert/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/notifier/channels"
 	"github.com/grafana/grafana/pkg/tests/testinfra"
 )
+
+func TestNotificationChannels(t *testing.T) {
+	dir, path := testinfra.CreateGrafDir(t, testinfra.GrafanaOpts{
+		EnableFeatureToggles: []string{"ngalert"},
+		DisableAnonymous:     true,
+	})
+
+	store := testinfra.SetUpDatabase(t, dir)
+	store.Bus = bus.GetBus()
+	grafanaListedAddr := testinfra.StartGrafana(t, dir, path, store)
+
+	mockChannel := newMockNotificationChannel(t, grafanaListedAddr)
+	amConfig := getAlertmanagerConfig(mockChannel.server.Addr)
+	rulesConfig := getRulesConfig(t)
+
+	// Overriding some URLs to send to the mock channel.
+	channels.SlackAPIEndpoint = fmt.Sprintf("http://%s/slack_recvX/slack_testX", mockChannel.server.Addr)
+	channels.PagerdutyEventAPIURL = fmt.Sprintf("http://%s/pagerduty_recvX/pagerduty_testX", mockChannel.server.Addr)
+	channels.TelegramAPIURL = fmt.Sprintf("http://%s/telegram_recv/bot%%s", mockChannel.server.Addr)
+
+	// Create a user to make authenticated requests
+	require.NoError(t, createUser(t, store, models.ROLE_EDITOR, "grafana", "password"))
+
+	{
+		// There are no notification channel config initially.
+		alertsURL := fmt.Sprintf("http://grafana:password@%s/api/alertmanager/grafana/config/api/v1/alerts", grafanaListedAddr)
+		_ = getRequest(t, alertsURL, http.StatusNotFound) // nolint
+	}
+
+	{
+		// Create the namespace we'll save our alerts to.
+		require.NoError(t, createFolder(t, store, 0, "default"))
+
+		// Post the alertmanager config.
+		u := fmt.Sprintf("http://grafana:password@%s/api/alertmanager/grafana/config/api/v1/alerts", grafanaListedAddr)
+		postRequest(t, u, amConfig, http.StatusAccepted)
+
+		// Verifying that all the receivers and routes have been registered.
+		alertsURL := fmt.Sprintf("http://grafana:password@%s/api/alertmanager/grafana/config/api/v1/alerts", grafanaListedAddr)
+		resp := getRequest(t, alertsURL, http.StatusOK) // nolint
+		b := getBody(t, resp.Body)
+		require.JSONEq(t, getExpAlertmanagerConfigFromAPI(mockChannel.server.Addr), b)
+	}
+
+	{
+		// Create rules that will fire as quickly as possible
+		u := fmt.Sprintf("http://grafana:password@%s/api/ruler/grafana/api/v1/rules/default", grafanaListedAddr)
+		postRequest(t, u, rulesConfig, http.StatusAccepted)
+	}
+
+	// Eventually, we'll get all the desired alerts.
+	// nolint:gosec
+	require.Eventually(t, func() bool {
+		return mockChannel.totalNotifications() == len(alertNames) &&
+			mockChannel.matchesExpNotifications(expNotifications)
+	}, 25*time.Second, 1*time.Second)
+
+	require.NoError(t, mockChannel.Close())
+}
+
+func getAlertmanagerConfig(channelAddr string) string {
+	return strings.ReplaceAll(alertmanagerConfig, "CHANNEL_ADDR", channelAddr)
+}
+
+func getExpAlertmanagerConfigFromAPI(channelAddr string) string {
+	return strings.ReplaceAll(expAlertmanagerConfigFromAPI, "CHANNEL_ADDR", channelAddr)
+}
+
+// alertNames are name of alerts to be sent. This should be in sync with
+// the routes that we define in Alertmanager config.
+// EmailAlert and TelegramAlert are missing because they don't
+// send a JSON. Email and POST body are yet to be supported in the tests.
+var alertNames = []string{"DingDingAlert", "SlackAlert1", "SlackAlert2", "PagerdutyAlert", "TeamsAlert", "WebhookAlert"}
+
+func getRulesConfig(t *testing.T) string {
+	interval, err := model.ParseDuration("10s")
+	require.NoError(t, err)
+	rules := apimodels.PostableRuleGroupConfig{
+		Name:     "arulegroup",
+		Interval: interval,
+	}
+
+	// Create rules that will fire as quickly as possible for all the routes.
+	for _, alertName := range alertNames {
+		rules.Rules = append(rules.Rules, apimodels.PostableExtendedRuleNode{
+			GrafanaManagedAlert: &apimodels.PostableGrafanaRule{
+				Title:     alertName,
+				Condition: "A",
+				Data: []ngmodels.AlertQuery{
+					{
+						RefID: "A",
+						RelativeTimeRange: ngmodels.RelativeTimeRange{
+							From: ngmodels.Duration(time.Duration(5) * time.Hour),
+							To:   ngmodels.Duration(time.Duration(3) * time.Hour),
+						},
+						DatasourceUID: "-100",
+						Model: json.RawMessage(`{
+							"type": "math",
+							"expression": "2 + 3 > 1"
+						}`),
+					},
+				},
+			},
+		})
+	}
+
+	b, err := json.Marshal(rules)
+	require.NoError(t, err)
+
+	return string(b)
+}
+
+func getRequest(t *testing.T, url string, expStatusCode int) *http.Response {
+	// nolint:gosec
+	resp, err := http.Get(url)
+	t.Cleanup(func() {
+		require.NoError(t, resp.Body.Close())
+	})
+	require.NoError(t, err)
+	require.Equal(t, expStatusCode, resp.StatusCode)
+	return resp
+}
+
+func postRequest(t *testing.T, url string, body string, expStatusCode int) {
+	buf := bytes.NewReader([]byte(body))
+	// nolint:gosec
+	resp, err := http.Post(url, "application/json", buf)
+	t.Cleanup(func() {
+		require.NoError(t, resp.Body.Close())
+	})
+	require.NoError(t, err)
+	require.Equal(t, expStatusCode, resp.StatusCode)
+}
+
+type mockNotificationChannel struct {
+	t      *testing.T
+	server *http.Server
+
+	receivedNotifications    map[string][]string
+	receivedNotificationsMtx sync.Mutex
+}
+
+func newMockNotificationChannel(t *testing.T, grafanaListedAddr string) *mockNotificationChannel {
+	lastDigit := grafanaListedAddr[len(grafanaListedAddr)-1] - 48
+	lastDigit = (lastDigit + 1) % 10
+	newAddr := fmt.Sprintf("%s%01d", grafanaListedAddr[:len(grafanaListedAddr)-1], lastDigit)
+
+	nc := &mockNotificationChannel{
+		server: &http.Server{
+			Addr: newAddr,
+		},
+		receivedNotifications: make(map[string][]string),
+		t:                     t,
+	}
+
+	nc.server.Handler = nc
+	go func() {
+		require.Equal(t, http.ErrServerClosed, nc.server.ListenAndServe())
+	}()
+
+	return nc
+}
+
+func (nc *mockNotificationChannel) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	nc.receivedNotificationsMtx.Lock()
+	defer nc.receivedNotificationsMtx.Unlock()
+
+	urlParts := strings.Split(req.URL.String(), "/")
+	key := fmt.Sprintf("%s/%s", urlParts[len(urlParts)-2], urlParts[len(urlParts)-1])
+	body := getBody(nc.t, req.Body)
+
+	nc.receivedNotifications[key] = append(nc.receivedNotifications[key], body)
+	res.WriteHeader(http.StatusOK)
+}
+
+func getBody(t *testing.T, body io.ReadCloser) string {
+	b, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+	return string(b)
+}
+
+func (nc *mockNotificationChannel) totalNotifications() int {
+	total := 0
+	nc.receivedNotificationsMtx.Lock()
+	defer nc.receivedNotificationsMtx.Unlock()
+	for _, v := range nc.receivedNotifications {
+		total += len(v)
+	}
+	return total
+}
+
+func (nc *mockNotificationChannel) matchesExpNotifications(exp map[string][]string) bool {
+	nc.receivedNotificationsMtx.Lock()
+	defer nc.receivedNotificationsMtx.Unlock()
+
+	if len(nc.receivedNotifications) != len(exp) {
+		return false
+	}
+
+	for expKey, expVals := range exp {
+		actVals, ok := nc.receivedNotifications[expKey]
+		if !ok || len(actVals) != len(expVals) {
+			return false
+		}
+		for i := range expVals {
+			expVal := expVals[i]
+			var r *regexp.Regexp
+			switch expKey {
+			case "webhook_recv/webhook_test":
+				// It has a time component "startsAt".
+				r = regexp.MustCompile(`.*"startsAt"\s*:\s*"([^"]+)"`)
+			case "slack_recvX/slack_testX":
+				fallthrough
+			case "slack_recv1/slack_test_without_token":
+				// It has a time component "ts".
+				r = regexp.MustCompile(`.*"ts"\s*:\s*([0-9]{10})`)
+			case "pagerduty_recvX/pagerduty_testX":
+				// It has a changing "source".
+				r = regexp.MustCompile(`.*"source"\s*:\s*"([^"]+)"`)
+			}
+			if r != nil {
+				parts := r.FindStringSubmatch(actVals[i])
+				require.Equal(nc.t, 2, len(parts))
+				expVal = fmt.Sprintf(expVal, parts[1])
+			}
+
+			var expJson, actJson interface{}
+			require.NoError(nc.t, json.Unmarshal([]byte(expVal), &expJson))
+			require.NoError(nc.t, json.Unmarshal([]byte(actVals[i]), &actJson))
+			if !assert.ObjectsAreEqual(expJson, actJson) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func (nc *mockNotificationChannel) Close() error {
+	return nc.server.Close()
+}
 
 // alertmanagerConfig has the config for all the notification channels
 // that we want to test. It is recommended to use different URL for each
@@ -572,7 +812,8 @@ var expNotifications = map[string][]string{
 			  "fallback": "Integration Test [FIRING:1]  (SlackAlert1)",
 			  "footer": "Grafana v",
 			  "footer_icon": "https://grafana.com/assets/img/fav32.png",
-			  "color": "#D63232"
+			  "color": "#D63232",
+			  "ts": %s
 			}
 		  ],
 		  "blocks": [
@@ -598,7 +839,8 @@ var expNotifications = map[string][]string{
 			  "fallback": "[FIRING:1]  (SlackAlert2)",
 			  "footer": "Grafana v",
 			  "footer_icon": "https://grafana.com/assets/img/fav32.png",
-			  "color": "#D63232"
+			  "color": "#D63232",
+			  "ts": %s
 			}
 		  ],
 		  "blocks": [
@@ -620,7 +862,7 @@ var expNotifications = map[string][]string{
 		  "event_action": "trigger",
 		  "payload": {
 			"summary": "Integration Test [FIRING:1]  (PagerdutyAlert)",
-			"source": "ganesh",
+			"source": "%s",
 			"severity": "warning",
 			"class": "testclass",
 			"component": "Integration Test",
@@ -711,251 +953,4 @@ var expNotifications = map[string][]string{
 		  "message": "\n**Firing**\nLabels:\n - alertname = WebhookAlert\nAnnotations:\nSource: \n\n\n\n\n"
 		}`,
 	},
-}
-
-func getAlertmanagerConfig(channelAddr string) string {
-	return strings.ReplaceAll(alertmanagerConfig, "CHANNEL_ADDR", channelAddr)
-}
-
-func getExpAlertmanagerConfigFromAPI(channelAddr string) string {
-	return strings.ReplaceAll(expAlertmanagerConfigFromAPI, "CHANNEL_ADDR", channelAddr)
-}
-
-// alertNames are name of alerts to be sent. This should be in sync with
-// the routes that we define in Alertmanager config.
-// EmailAlert and TelegramAlert are missing because they don't
-// send a JSON. Email and POST body are yet to be supported in the tests.
-var alertNames = []string{"DingDingAlert", "SlackAlert1", "SlackAlert2", "PagerdutyAlert", "TeamsAlert", "WebhookAlert"}
-
-func getRulesConfig(t *testing.T) string {
-	interval, err := model.ParseDuration("10s")
-	require.NoError(t, err)
-	rules := apimodels.PostableRuleGroupConfig{
-		Name:     "arulegroup",
-		Interval: interval,
-	}
-
-	// Create rules that will fire as quickly as possible for all the routes.
-	for _, alertName := range alertNames {
-		rules.Rules = append(rules.Rules, apimodels.PostableExtendedRuleNode{
-			GrafanaManagedAlert: &apimodels.PostableGrafanaRule{
-				Title:     alertName,
-				Condition: "A",
-				Data: []ngmodels.AlertQuery{
-					{
-						RefID: "A",
-						RelativeTimeRange: ngmodels.RelativeTimeRange{
-							From: ngmodels.Duration(time.Duration(5) * time.Hour),
-							To:   ngmodels.Duration(time.Duration(3) * time.Hour),
-						},
-						DatasourceUID: "-100",
-						Model: json.RawMessage(`{
-							"type": "math",
-							"expression": "2 + 3 > 1"
-						}`),
-					},
-				},
-			},
-		})
-	}
-
-	b, err := json.Marshal(rules)
-	require.NoError(t, err)
-
-	return string(b)
-}
-
-func TestNotificationChannels(t *testing.T) {
-	dir, path := testinfra.CreateGrafDir(t, testinfra.GrafanaOpts{
-		EnableFeatureToggles: []string{"ngalert"},
-		DisableAnonymous:     true,
-	})
-
-	store := testinfra.SetUpDatabase(t, dir)
-	store.Bus = bus.GetBus()
-	grafanaListedAddr := testinfra.StartGrafana(t, dir, path, store)
-
-	mockChannel := newMockNotificationChannel(t, grafanaListedAddr)
-	amConfig := getAlertmanagerConfig(mockChannel.server.Addr)
-	rulesConfig := getRulesConfig(t)
-
-	// Overriding some URLs to send to the mock channel.
-	channels.SlackAPIEndpoint = fmt.Sprintf("http://%s/slack_recvX/slack_testX", mockChannel.server.Addr)
-	channels.PagerdutyEventAPIURL = fmt.Sprintf("http://%s/pagerduty_recvX/pagerduty_testX", mockChannel.server.Addr)
-	channels.TelegramAPIURL = fmt.Sprintf("http://%s/telegram_recv/bot%%s", mockChannel.server.Addr)
-
-	// Create a user to make authenticated requests
-	require.NoError(t, createUser(t, store, models.ROLE_EDITOR, "grafana", "password"))
-
-	{
-		// There are no notification channel config initially.
-		alertsURL := fmt.Sprintf("http://grafana:password@%s/api/alertmanager/grafana/config/api/v1/alerts", grafanaListedAddr)
-		_ = getRequest(t, alertsURL, http.StatusNotFound) // nolint
-	}
-
-	{
-		// Create the namespace we'll save our alerts to.
-		require.NoError(t, createFolder(t, store, 0, "default"))
-
-		// Post the alertmanager config.
-		u := fmt.Sprintf("http://grafana:password@%s/api/alertmanager/grafana/config/api/v1/alerts", grafanaListedAddr)
-		postRequest(t, u, amConfig, http.StatusAccepted)
-
-		// Verifying that all the receivers and routes have been registered.
-		alertsURL := fmt.Sprintf("http://grafana:password@%s/api/alertmanager/grafana/config/api/v1/alerts", grafanaListedAddr)
-		resp := getRequest(t, alertsURL, http.StatusOK) // nolint
-		b := getBody(t, resp.Body)
-		require.JSONEq(t, getExpAlertmanagerConfigFromAPI(mockChannel.server.Addr), b)
-	}
-
-	{
-		// Create rules that will fire as quickly as possible
-		u := fmt.Sprintf("http://grafana:password@%s/api/ruler/grafana/api/v1/rules/default", grafanaListedAddr)
-		postRequest(t, u, rulesConfig, http.StatusAccepted)
-	}
-
-	// Eventually, we'll get all the desired alerts.
-	// nolint:gosec
-	require.Eventually(t, func() bool {
-		return mockChannel.totalNotifications() == len(alertNames) &&
-			mockChannel.matchesExpNotifications(expNotifications)
-	}, 25*time.Second, 1*time.Second)
-
-	require.NoError(t, mockChannel.Close())
-}
-
-func getRequest(t *testing.T, url string, expStatusCode int) *http.Response {
-	// nolint:gosec
-	resp, err := http.Get(url)
-	t.Cleanup(func() {
-		require.NoError(t, resp.Body.Close())
-	})
-	require.NoError(t, err)
-	require.Equal(t, expStatusCode, resp.StatusCode)
-	return resp
-}
-
-func postRequest(t *testing.T, url string, body string, expStatusCode int) {
-	buf := bytes.NewReader([]byte(body))
-	// nolint:gosec
-	resp, err := http.Post(url, "application/json", buf)
-	t.Cleanup(func() {
-		require.NoError(t, resp.Body.Close())
-	})
-	require.NoError(t, err)
-	require.Equal(t, expStatusCode, resp.StatusCode)
-}
-
-type mockNotificationChannel struct {
-	t      *testing.T
-	server *http.Server
-
-	receivedNotifications    map[string][]string
-	receivedNotificationsMtx sync.Mutex
-}
-
-func newMockNotificationChannel(t *testing.T, grafanaListedAddr string) *mockNotificationChannel {
-	lastDigit := grafanaListedAddr[len(grafanaListedAddr)-1] - 48
-	lastDigit = (lastDigit + 1) % 10
-	newAddr := fmt.Sprintf("%s%01d", grafanaListedAddr[:len(grafanaListedAddr)-1], lastDigit)
-
-	nc := &mockNotificationChannel{
-		server: &http.Server{
-			Addr: newAddr,
-		},
-		receivedNotifications: make(map[string][]string),
-		t:                     t,
-	}
-
-	nc.server.Handler = nc
-	go func() {
-		require.Equal(t, http.ErrServerClosed, nc.server.ListenAndServe())
-	}()
-
-	return nc
-}
-
-func (nc *mockNotificationChannel) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	nc.receivedNotificationsMtx.Lock()
-	defer nc.receivedNotificationsMtx.Unlock()
-
-	urlParts := strings.Split(req.URL.String(), "/")
-	key := fmt.Sprintf("%s/%s", urlParts[len(urlParts)-2], urlParts[len(urlParts)-1])
-	body := getBody(nc.t, req.Body)
-
-	// Special handling for non-static data, like timestamp.
-	if strings.Contains(key, "slack") {
-		// Remove the timestamp from slack.
-		j, err := simplejson.NewJson([]byte(body))
-		require.NoError(nc.t, err)
-
-		attachments, err := j.Get("attachments").Array()
-		require.NoError(nc.t, err)
-
-		aj := simplejson.NewFromAny(attachments[0])
-		aj.Del("ts")
-		j.Set("attachments", []interface{}{aj.Interface()})
-
-		b, err := j.Encode()
-		require.NoError(nc.t, err)
-		body = string(b)
-	}
-
-	nc.receivedNotifications[key] = append(nc.receivedNotifications[key], body)
-	res.WriteHeader(http.StatusOK)
-}
-
-func getBody(t *testing.T, body io.ReadCloser) string {
-	b, err := ioutil.ReadAll(body)
-	require.NoError(t, err)
-	return string(b)
-}
-
-func (nc *mockNotificationChannel) totalNotifications() int {
-	total := 0
-	nc.receivedNotificationsMtx.Lock()
-	defer nc.receivedNotificationsMtx.Unlock()
-	for _, v := range nc.receivedNotifications {
-		total += len(v)
-	}
-	return total
-}
-
-func (nc *mockNotificationChannel) matchesExpNotifications(exp map[string][]string) bool {
-	nc.receivedNotificationsMtx.Lock()
-	defer nc.receivedNotificationsMtx.Unlock()
-
-	if len(nc.receivedNotifications) != len(exp) {
-		return false
-	}
-
-	for expKey, expVals := range exp {
-		actVals, ok := nc.receivedNotifications[expKey]
-		if !ok || len(actVals) != len(expVals) {
-			return false
-		}
-		for i := range expVals {
-			expVal := expVals[i]
-			if expKey == "webhook_recv/webhook_test" {
-				// Special case since it has a time component "startsAt".
-				r := regexp.MustCompile(`.*"startsAt"\s*:\s*"([^"]+)"`)
-				parts := r.FindStringSubmatch(actVals[i])
-				require.Equal(nc.t, 2, len(parts))
-				expVal = fmt.Sprintf(expVal, parts[1])
-			}
-
-			var expJson, actJson interface{}
-			require.NoError(nc.t, json.Unmarshal([]byte(expVal), &expJson))
-			require.NoError(nc.t, json.Unmarshal([]byte(actVals[i]), &actJson))
-			if !assert.ObjectsAreEqual(expJson, actJson) {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func (nc *mockNotificationChannel) Close() error {
-	return nc.server.Close()
 }
