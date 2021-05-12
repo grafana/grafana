2a7af7bc756a472e2c3a0efdd59da90c15f3e838
Torkel Ödegaard <torkel@grafana.com>
2021-05-12 14:56:45 +0200

    Using some named colors

--- packages/grafana-data/src/themes/createVizColors.ts
+++ packages/grafana-data/src/themes/createVizColors.ts
@@ -231,13 +231,13 @@ function getClassicPalette() {
   // Todo replace these with named colors (as many as possible)
 
   return [
-    '#7EB26D', // 0: pale green
-    '#EAB839', // 1: mustard
-    '#6ED0E0', // 2: light blue
-    '#EF843C', // 3: orange
-    '#E24D42', // 4: red
-    '#1F78C1', // 5: ocean
-    '#BA43A9', // 6: purple
+    'green', // '#7EB26D', // 0: pale green
+    'semi-dark-yellow', // '#EAB839', // 1: mustard
+    'light-dark', // #6ED0E0', // 2: light blue
+    'semi-dark-orange', // '#EF843C', // 3: orange
+    'red', // '#E24D42', // 4: red
+    'blue', // #1F78C1', // 5: ocean
+    'purple', // '#BA43A9', // 6: purple
     '#705DA0', // 7: violet
     '#508642', // 8: dark green
     '#CCA300', // 9: dark sand
