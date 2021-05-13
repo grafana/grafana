a718bb6682f8499d055811e4467437c3203c5c52
Alexander Emelin <frvzmb@gmail.com>
2021-05-13 12:01:20 +0300

    Live: labels column as default frame format (#33984)
    * labels columns as default frame format
    
    * update sdk
    
    Co-authored-by: Ryan McKinley <ryantxu@gmail.com>

--- pkg/services/live/pushurl/values.go
+++ pkg/services/live/pushurl/values.go
@@ -5,17 +5,21 @@ import (
 	"strings"
 )
 
+const (
+	unstableSchemaParam = "gf_live_unstable_schema"
+	frameFormatParam    = "gf_live_frame_format"
+)
+
 // UnstableSchemaFromValues extracts unstable schema tip from url values.
 func UnstableSchemaFromValues(values url.Values) bool {
-	key := "gf_live_unstable_schema"
-	return strings.ToLower(values.Get(key)) == "true" || values.Get(key) == "1"
+	return strings.ToLower(values.Get(unstableSchemaParam)) == "true" || values.Get(unstableSchemaParam) == "1"
 }
 
 // FrameFormatFromValues extracts frame format tip from url values.
 func FrameFormatFromValues(values url.Values) string {
-	frameFormat := strings.ToLower(values.Get("gf_live_frame_format"))
+	frameFormat := strings.ToLower(values.Get(frameFormatParam))
 	if frameFormat == "" {
-		frameFormat = "wide"
+		frameFormat = "labels_column"
 	}
 	return frameFormat
 }
