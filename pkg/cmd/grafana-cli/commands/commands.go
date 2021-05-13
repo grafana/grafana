cacbe24e37bd8b7948d93586f7657e802650fa9a
Dominik Prokop <dominik.prokop@grafana.com>
2021-05-13 14:33:57 +0200

    Remove log about the need for Grafana restart

--- pkg/cmd/grafana-cli/commands/commands.go
+++ pkg/cmd/grafana-cli/commands/commands.go
@@ -59,7 +59,6 @@ func runPluginCommand(command func(commandLine utils.CommandLine) error) func(co
 			return err
 		}
 
-		logger.Info(color.GreenString("Please restart Grafana after installing plugins. Refer to Grafana documentation for instructions if necessary.\n\n"))
 		return nil
 	}
 }
