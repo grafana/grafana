c32b7735c2cf3a996fa2538cc8c684d96346b668
Ryan McKinley <ryantxu@gmail.com>
2021-05-15 10:21:23 -0700

    more wording

--- plugins-bundled/internal/catalog-app/src/constants.ts
+++ plugins-bundled/internal/catalog-app/src/constants.ts
@@ -1,3 +1,4 @@
 export const API_ROOT = '/api/plugins';
-export const PLUGIN_ROOT = '/a/grafana-marketplace-app';
+export const PLUGIN_ID = 'grafana-catalog-app';
+export const PLUGIN_ROOT = '/a/' + PLUGIN_ID;
 export const GRAFANA_API_ROOT = '/api/gnet';
