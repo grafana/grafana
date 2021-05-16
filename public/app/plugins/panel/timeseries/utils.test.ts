02338a1fe29671cf0d4102456bfc4f7a2a012013
Ryan McKinley <ryantxu@gmail.com>
2021-05-16 13:36:28 -0700

    support boolean values out-of-the-box

---
+++ public/app/plugins/panel/timeseries/utils.test.ts
@@ -0,0 +1,46 @@
+import { FieldType, toDataFrame } from '@grafana/data';
+import { prepareGraphableFields } from './utils';
+
+describe('prepare timeseries graph', () => {
+  it('errors with no time fields', () => {
+    const frames = [
+      toDataFrame({
+        fields: [
+          { name: 'a', values: [1, 2, 3] },
+          { name: 'b', values: ['a', 'b', 'c'] },
+        ],
+      }),
+    ];
+    const info = prepareGraphableFields(frames);
+    expect(info.warn).toEqual('Data does not have a time field');
+  });
+
+  it('requires a number or boolean value', () => {
+    const frames = [
+      toDataFrame({
+        fields: [
+          { name: 'a', type: FieldType.time, values: [1, 2, 3] },
+          { name: 'b', values: ['a', 'b', 'c'] },
+        ],
+      }),
+    ];
+    const info = prepareGraphableFields(frames);
+    expect(info.warn).toEqual('No graphable fields');
+  });
+
+  it('will graph numbers and boolean values', () => {
+    const frames = [
+      toDataFrame({
+        fields: [
+          { name: 'a', type: FieldType.time, values: [1, 2, 3] },
+          { name: 'b', values: ['a', 'b', 'c'] },
+          { name: 'c', values: [true, false, true] },
+          { name: 'd', values: [100, 200, 300] },
+        ],
+      }),
+    ];
+    const info = prepareGraphableFields(frames);
+    expect(info.warn).toBeUndefined();
+    expect(info.frames![0].fields.map((f) => f.name)).toEqual(['a', 'c', 'd']);
+  });
+});
---
+++ public/app/plugins/panel/timeseries/utils.ts
@@ -0,0 +1,67 @@
+import { ArrayVector, DataFrame, Field, FieldType } from '@grafana/data';
+
+// This will return a set of frames with only graphable values included
+export function prepareGraphableFields(series?: DataFrame[]): { frames?: DataFrame[]; warn?: string } {
+  if (!series?.length) {
+    return { warn: 'No data in response' };
+  }
+  let hasTimeseries = false;
+  const frames: DataFrame[] = [];
+  for (let frame of series) {
+    let isTimeseries = false;
+    let changed = false;
+    const fields: Field[] = [];
+    for (const field of frame.fields) {
+      switch (field.type) {
+        case FieldType.time:
+          isTimeseries = true;
+          hasTimeseries = true;
+          fields.push(field);
+          break;
+        case FieldType.number:
+          fields.push(field);
+          break; // ok
+        case FieldType.boolean:
+          changed = true;
+          fields.push({
+            ...field,
+            config: {
+              ...field.config,
+              unit: 'boolean', // TODO
+            },
+            type: FieldType.number,
+            values: new ArrayVector(
+              field.values.toArray().map((v) => {
+                if (v == null) {
+                  return v;
+                }
+                return Boolean(v) ? 1 : 0;
+              })
+            ),
+          });
+          break;
+        default:
+          changed = true;
+      }
+    }
+    if (isTimeseries && fields.length > 1) {
+      hasTimeseries = true;
+      if (changed) {
+        frames.push({
+          ...frame,
+          fields,
+        });
+      } else {
+        frames.push(frame);
+      }
+    }
+  }
+
+  if (!hasTimeseries) {
+    return { warn: 'Data does not have a time field' };
+  }
+  if (!frames.length) {
+    return { warn: 'No graphable fields' };
+  }
+  return { frames };
+}
