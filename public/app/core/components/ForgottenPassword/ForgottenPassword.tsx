3a7273860909006b453db600f3ec907ab1baa888
Hugo Häggmark <hugo.haggmark@grafana.com>
2021-05-17 09:15:41 +0200

    ForgottenPassword: Adds better error handling (#34174)
    * ForgottenPassword: Adds better error handling
    
    * Chore: updates after PR comments

--- public/app/core/components/ForgottenPassword/ForgottenPassword.tsx
+++ public/app/core/components/ForgottenPassword/ForgottenPassword.tsx
@@ -1,32 +1,68 @@
-import React, { FC, useState } from 'react';
-import { Form, Field, Input, Button, Legend, Container, useStyles, HorizontalGroup, LinkButton } from '@grafana/ui';
-import { getBackendSrv } from '@grafana/runtime';
+import React, { useState } from 'react';
+import { Unsubscribable } from 'rxjs';
 import { css } from '@emotion/css';
-import { GrafanaTheme } from '@grafana/data';
-import config from 'app/core/config';
+import {
+  Alert,
+  Button,
+  Container,
+  FadeTransition,
+  Field,
+  Form,
+  HorizontalGroup,
+  Input,
+  Legend,
+  LinkButton,
+  useStyles2,
+} from '@grafana/ui';
+import { getBackendSrv, toDataQueryError } from '@grafana/runtime';
+import { GrafanaTheme2 } from '@grafana/data';
+
+import { getConfig } from 'app/core/config';
 
 interface EmailDTO {
   userOrEmail: string;
 }
 
-const paragraphStyles = (theme: GrafanaTheme) => css`
-  color: ${theme.colors.formDescription};
-  font-size: ${theme.typography.size.sm};
-  font-weight: ${theme.typography.weight.regular};
-  margin-top: ${theme.spacing.sm};
-  display: block;
-`;
+export interface SendResetEmailDTO {
+  message: string;
+  error?: string;
+}
 
-export const ForgottenPassword: FC = () => {
+export function ForgottenPassword(): JSX.Element {
   const [emailSent, setEmailSent] = useState(false);
-  const styles = useStyles(paragraphStyles);
-  const loginHref = `${config.appSubUrl}/login`;
+  const [error, setError] = useState<string | null>(null);
+  const styles = useStyles2(getStyles);
+  const loginHref = `${getConfig().appSubUrl}/login`;
 
-  const sendEmail = async (formModel: EmailDTO) => {
-    const res = await getBackendSrv().post('/api/user/password/send-reset-email', formModel);
-    if (res) {
-      setEmailSent(true);
-    }
+  const sendEmail = (formModel: EmailDTO) => {
+    setError(null);
+    // using fetch here so we can disable showSuccessAlert, showErrorAlert and handle those
+    const subscription: Unsubscribable = getBackendSrv()
+      .fetch<SendResetEmailDTO>({
+        url: '/api/user/password/send-reset-email',
+        method: 'POST',
+        data: formModel,
+        showSuccessAlert: false,
+        showErrorAlert: false,
+      })
+      .subscribe({
+        next: (response) => {
+          const { error, message } = response.data; // the backend api can respond with 200 Ok but still have an error set
+
+          if (error) {
+            setError(error);
+          } else if (message) {
+            setEmailSent(true);
+          }
+
+          subscription.unsubscribe(); // unsubscribe as soon as a value is received
+        },
+        error: (err) => {
+          const dataError = toDataQueryError(err);
+          setError(dataError.message ?? 'Unknown error');
+          subscription.unsubscribe(); // unsubscribe as soon as an error is received
+        },
+      });
   };
 
   if (emailSent) {
@@ -40,29 +76,54 @@ export const ForgottenPassword: FC = () => {
       </div>
     );
   }
-  return (
-    <Form onSubmit={sendEmail}>
-      {({ register, errors }) => (
-        <>
-          <Legend>Reset password</Legend>
-          <Field
-            label="User"
-            description="Enter your information to get a reset link sent to you"
-            invalid={!!errors.userOrEmail}
-            error={errors?.userOrEmail?.message}
-          >
-            <Input placeholder="Email or username" {...register('userOrEmail', { required: true })} />
-          </Field>
-          <HorizontalGroup>
-            <Button>Send reset email</Button>
-            <LinkButton fill="text" href={loginHref}>
-              Back to login
-            </LinkButton>
-          </HorizontalGroup>
 
-          <p className={styles}>Did you forget your username or email? Contact your Grafana administrator.</p>
-        </>
-      )}
-    </Form>
+  return (
+    <>
+      <Form onSubmit={sendEmail}>
+        {({ register, errors }) => (
+          <>
+            <Legend>Reset password</Legend>
+            <Field
+              label="User"
+              description="Enter your information to get a reset link sent to you"
+              invalid={!!errors.userOrEmail}
+              error={errors?.userOrEmail?.message}
+            >
+              <Input
+                id={'userOrEmail'}
+                placeholder="Email or username"
+                {...register('userOrEmail', { required: true })}
+              />
+            </Field>
+            <HorizontalGroup>
+              <Button>Send reset email</Button>
+              <LinkButton fill="text" href={loginHref}>
+                Back to login
+              </LinkButton>
+            </HorizontalGroup>
+
+            <p className={styles}>Did you forget your username or email? Contact your Grafana administrator.</p>
+          </>
+        )}
+      </Form>
+      <FadeTransition duration={150} visible={Boolean(error)}>
+        <Alert title="Couldn't send reset link to the email address" severity={'error'}>
+          <span>
+            <strong>Reason:&nbsp;</strong>
+            {error}
+          </span>
+        </Alert>
+      </FadeTransition>
+    </>
   );
-};
+}
+
+function getStyles(theme: GrafanaTheme2) {
+  return css`
+    color: ${theme.colors.text.secondary};
+    font-size: ${theme.typography.size.sm};
+    font-weight: ${theme.typography.fontWeightRegular};
+    margin-top: ${theme.spacing(1)};
+    display: block;
+  `;
+}
