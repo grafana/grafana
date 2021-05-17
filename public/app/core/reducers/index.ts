0f00ed1e53455162be81c4d2fec120ca3016f443
Zoltán Bedi <zoltan.bedi@gmail.com>
2021-05-17 16:43:40 +0200

    Chore: remove redux-logger (#34220)

--- public/app/core/reducers/index.ts
+++ public/app/core/reducers/index.ts
@@ -1,9 +1,7 @@
 import { navIndexReducer as navIndex } from './navModel';
 import { appNotificationsReducer as appNotifications } from './appNotification';
-import { applicationReducer as application } from './application';
 
 export default {
   navIndex,
   appNotifications,
-  application,
 };
