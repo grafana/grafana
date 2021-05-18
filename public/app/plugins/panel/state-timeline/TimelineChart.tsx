25dd4cd6e3579d6d09585afbcd10982343a26b91
Leon Sorokin <leeoniya@gmail.com>
2021-05-18 17:30:12 -0500

    Timeline: legend

--- public/app/plugins/panel/state-timeline/TimelineChart.tsx
+++ public/app/plugins/panel/state-timeline/TimelineChart.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { PanelContext, PanelContextRoot, GraphNG, GraphNGProps, BarValueVisibility } from '@grafana/ui';
+import {
+  PanelContext,
+  PanelContextRoot,
+  GraphNG,
+  GraphNGProps,
+  BarValueVisibility,
+  LegendDisplayMode,
+  PlotLegend,
+  UPlotConfigBuilder,
+} from '@grafana/ui';
 import { DataFrame, FieldType, TimeRange } from '@grafana/data';
 import { preparePlotConfigBuilder } from './utils';
 import { TimelineMode, TimelineValueAlignment } from './types';
@@ -33,7 +42,44 @@ export class TimelineChart extends React.Component<TimelineProps> {
     });
   };
 
-  renderLegend = () => null;
+  // renderLegend = (config: UPlotConfigBuilder) => {
+  //   const { legend, frames } = this.props;
+
+  //   if (!config || (legend && legend.displayMode === LegendDisplayMode.Hidden)) {
+  //     return null;
+  //   }
+
+  //   return <PlotLegend data={frames} config={config} maxHeight="35%" maxWidth="60%" {...legend} />;
+  // };
+
+  //renderLegend = () => <div>Legend</div>;
+
+  renderLegend = (config: UPlotConfigBuilder) => {
+    const { legend, frames } = this.props;
+
+    if (!config || (legend && legend.displayMode === LegendDisplayMode.Hidden)) {
+      return null;
+    }
+
+    let stateColors = {};
+
+    frames.forEach((frame) => {
+      frame.fields.forEach((field) => {
+        if (field.type !== FieldType.time) {
+          field.values.toArray().forEach((v) => {
+            let state = field.display!(v);
+            stateColors[state.text] = state.color;
+          });
+        }
+      });
+    });
+
+    console.log(stateColors);
+
+    //return <PlotLegend data={frames} config={config} maxHeight="35%" maxWidth="60%" {...legend} />;
+
+    return <div>Legend</div>;
+  };
 
   render() {
     return (
