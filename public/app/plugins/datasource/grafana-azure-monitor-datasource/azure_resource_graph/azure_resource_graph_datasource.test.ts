3fce3d9f2cf288ef7d302848423e66536d32fe4a
Erik Sundell <erik.sundell87@gmail.com>
2021-05-18 17:41:03 +0200

    improve variable tests

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_resource_graph/azure_resource_graph_datasource.test.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_resource_graph/azure_resource_graph_datasource.test.ts
@@ -1,8 +1,43 @@
 import { TemplateSrv } from 'app/features/templating/template_srv';
 import { backendSrv } from 'app/core/services/backend_srv';
 import AzureResourceGraphDatasource from './azure_resource_graph_datasource';
+import { CustomVariableModel, initialVariableModelState, VariableHide } from 'app/features/variables/types';
 
 const templateSrv = new TemplateSrv();
+
+const single: CustomVariableModel = {
+  ...initialVariableModelState,
+  id: 'var1',
+  name: 'var1',
+  index: 0,
+  current: { value: 'var1-foo', text: 'var1-foo', selected: true },
+  options: [{ value: 'var1-foo', text: 'var1-foo', selected: true }],
+  multi: false,
+  includeAll: false,
+  query: '',
+  hide: VariableHide.dontHide,
+  type: 'custom',
+};
+
+const multi: CustomVariableModel = {
+  ...initialVariableModelState,
+  id: 'var3',
+  name: 'var3',
+  index: 2,
+  current: { value: ['var3-foo', 'var3-baz'], text: 'var3-foo + var3-baz', selected: true },
+  options: [
+    { selected: true, value: 'var3-foo', text: 'var3-foo' },
+    { selected: false, value: 'var3-bar', text: 'var3-bar' },
+    { selected: true, value: 'var3-baz', text: 'var3-baz' },
+  ],
+  multi: true,
+  includeAll: false,
+  query: '',
+  hide: VariableHide.dontHide,
+  type: 'custom',
+};
+
+templateSrv.init([single, multi]);
 
 jest.mock('app/core/services/backend_srv');
 jest.mock('@grafana/runtime', () => ({
@@ -30,16 +65,34 @@ describe('AzureResourceGraphDatasource', () => {
   });
 
   describe('When applying template variables', () => {
-    const target = {
-      azureResourceGraph: {
-        query: 'Resources | project name',
-        resultFormat: '',
-      },
-    };
-
-    it('should return Azure Resource Graph Query', () => {
+    it('should expand single value template variable', () => {
+      const target = {
+        azureResourceGraph: {
+          query: 'Resources | $var1',
+          resultFormat: '',
+        },
+      };
       expect(ctx.ds.applyTemplateVariables(target)).toStrictEqual({
-        azureResourceGraph: { query: 'Resources | project name', resultFormat: 'table' },
+        azureResourceGraph: { query: 'Resources | var1-foo', resultFormat: 'table' },
+        format: undefined,
+        queryType: 'Azure Resource Graph',
+        refId: undefined,
+        subscriptions: undefined,
+      });
+    });
+
+    it('should expand multi value template variable', () => {
+      const target = {
+        azureResourceGraph: {
+          query: 'resources | where $__contains(name, $var3)',
+          resultFormat: '',
+        },
+      };
+      expect(ctx.ds.applyTemplateVariables(target)).toStrictEqual({
+        azureResourceGraph: {
+          query: `resources | where $__contains(name, 'var3-foo','var3-baz')`,
+          resultFormat: 'table',
+        },
         format: undefined,
         queryType: 'Azure Resource Graph',
         refId: undefined,
