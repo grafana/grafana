9574380f185cd7a5fb76ff1541f8e34044d652e6
Dimitris Sotirakis <dimitrios.sotirakis@grafana.com>
2021-05-18 10:30:13 +0300

    Scuemata: Add grafana-cli command to validate resources against scuemata (#33852)
    * Add validate-resource cli command
    
    * Fixes according to reviewer's comments

--- pkg/cmd/grafana-cli/commands/scuemata_validation_command_test.go
+++ pkg/cmd/grafana-cli/commands/scuemata_validation_command_test.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"io/fs"
 	"os"
+	"path/filepath"
 	"testing"
 	"testing/fstest"
 
@@ -19,10 +21,10 @@ func TestValidateScuemataBasics(t *testing.T) {
 			DistPluginCueFS: defaultBaseLoadPaths.DistPluginCueFS,
 		}
 
-		err := validate(baseLoadPaths, load.BaseDashboardFamily)
+		err := validateScuemata(baseLoadPaths, load.BaseDashboardFamily)
 		require.NoError(t, err, "error while loading base dashboard scuemata")
 
-		err = validate(baseLoadPaths, load.DistDashboardFamily)
+		err = validateScuemata(baseLoadPaths, load.DistDashboardFamily)
 		require.NoError(t, err, "error while loading dist dashboard scuemata")
 	})
 
@@ -30,17 +32,17 @@ func TestValidateScuemataBasics(t *testing.T) {
 		genCue, err := os.ReadFile("testdata/missing_family_gen.cue")
 		require.NoError(t, err)
 
-		fs := fstest.MapFS{
+		filesystem := fstest.MapFS{
 			"cue/data/gen.cue": &fstest.MapFile{Data: genCue},
 		}
-		mergedFS := Merge(fs, defaultBaseLoadPaths.BaseCueFS)
+		mergedFS := Merge(filesystem, defaultBaseLoadPaths.BaseCueFS)
 
 		var baseLoadPaths = load.BaseLoadPaths{
 			BaseCueFS:       mergedFS,
 			DistPluginCueFS: defaultBaseLoadPaths.DistPluginCueFS,
 		}
 
-		err = validate(baseLoadPaths, load.BaseDashboardFamily)
+		err = validateScuemata(baseLoadPaths, load.BaseDashboardFamily)
 		assert.EqualError(t, err, "error while loading dashboard scuemata, err: dashboard schema family did not exist at expected path in expected file")
 	})
 
@@ -48,20 +50,71 @@ func TestValidateScuemataBasics(t *testing.T) {
 		genCue, err := os.ReadFile("testdata/missing_panel_gen.cue")
 		require.NoError(t, err)
 
-		fs := fstest.MapFS{
+		filesystem := fstest.MapFS{
 			"cue/data/gen.cue": &fstest.MapFile{Data: genCue},
 		}
-		mergedFS := Merge(fs, defaultBaseLoadPaths.BaseCueFS)
+		mergedFS := Merge(filesystem, defaultBaseLoadPaths.BaseCueFS)
 
 		var baseLoadPaths = load.BaseLoadPaths{
 			BaseCueFS:       mergedFS,
 			DistPluginCueFS: defaultBaseLoadPaths.DistPluginCueFS,
 		}
 
-		err = validate(baseLoadPaths, load.BaseDashboardFamily)
+		err = validateScuemata(baseLoadPaths, load.BaseDashboardFamily)
 		require.NoError(t, err, "error while loading base dashboard scuemata")
 
-		err = validate(baseLoadPaths, load.DistDashboardFamily)
+		err = validateScuemata(baseLoadPaths, load.DistDashboardFamily)
 		assert.EqualError(t, err, "all schema should be valid with respect to basic CUE rules, Family.lineages.0.0: field #Panel not allowed")
+	})
+
+	t.Run("Testing validateResources against scuemata and resource inputs", func(t *testing.T) {
+		validPanel, err := os.ReadFile("testdata/panels/valid_resource_panel.json")
+		require.NoError(t, err)
+
+		invalidPanel, err := os.ReadFile("testdata/panels/invalid_resource_panel.json")
+		require.NoError(t, err)
+
+		filesystem := fstest.MapFS{
+			"valid.json":   &fstest.MapFile{Data: validPanel},
+			"invalid.json": &fstest.MapFile{Data: invalidPanel},
+		}
+		mergedFS := Merge(filesystem, defaultBaseLoadPaths.BaseCueFS)
+
+		var baseLoadPaths = load.BaseLoadPaths{
+			BaseCueFS:       mergedFS,
+			DistPluginCueFS: defaultBaseLoadPaths.DistPluginCueFS,
+		}
+
+		require.NoError(t, fs.WalkDir(mergedFS, ".", func(path string, d fs.DirEntry, err error) error {
+			require.NoError(t, err)
+
+			if d.IsDir() || filepath.Ext(d.Name()) != ".json" {
+				return nil
+			}
+
+			if d.Name() == "valid.json" {
+				t.Run(path, func(t *testing.T) {
+					b, err := mergedFS.Open(path)
+					require.NoError(t, err, "failed to open dashboard file")
+
+					err = validateResources(b, baseLoadPaths, load.BaseDashboardFamily)
+					require.NoError(t, err, "error while loading base dashboard scuemata")
+
+					err = validateResources(b, baseLoadPaths, load.DistDashboardFamily)
+					require.NoError(t, err, "error while loading base dashboard scuemata")
+				})
+			}
+			if d.Name() == "invalid.json" {
+				t.Run(path, func(t *testing.T) {
+					b, err := mergedFS.Open(path)
+					require.NoError(t, err, "failed to open dashboard file")
+
+					err = validateResources(b, baseLoadPaths, load.BaseDashboardFamily)
+					assert.EqualError(t, err, "failed validation: Family.lineages.0.0.panels.0.fieldConfig.defaults: field mappings not allowed")
+				})
+			}
+
+			return nil
+		}))
 	})
 }
