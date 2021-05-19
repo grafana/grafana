434375ac0a6933cd02e86dba564e088529383059
Domas <domasx2@gmail.com>
2021-05-19 18:10:12 +0300

    update front part

--- public/app/features/alerting/unified/utils/matchers.ts
+++ public/app/features/alerting/unified/utils/matchers.ts
@@ -1,35 +1,7 @@
-import { SilenceMatcher } from 'app/plugins/datasource/alertmanager/types';
+import { Matcher } from 'app/plugins/datasource/alertmanager/types';
+import { parseMatcher } from './alertmanager';
 
 // parses comma separated matchers like "foo=bar,baz=~bad*" into SilenceMatcher[]
-export function parseQueryParamMatchers(paramValue: string): SilenceMatcher[] {
-  const matchers: SilenceMatcher[] = [];
-  paramValue
-    .split(',')
-    .map((x) => x.trim())
-    .forEach((item) => {
-      // not supported
-      if (item.includes('!=') || item.includes('!~')) {
-        return;
-      }
-      if (item.includes('=~')) {
-        const [name, value] = item.split('=~');
-        if (name && value) {
-          matchers.push({
-            name,
-            value,
-            isRegex: true,
-          });
-        }
-      } else if (item.includes('=')) {
-        const [name, value] = item.split('=');
-        if (name && value) {
-          matchers.push({
-            name,
-            value,
-            isRegex: false,
-          });
-        }
-      }
-    });
-  return matchers;
+export function parseQueryParamMatchers(paramValue: string): Matcher[] {
+  return paramValue.split(',').map((x) => parseMatcher(x.trim()));
 }
