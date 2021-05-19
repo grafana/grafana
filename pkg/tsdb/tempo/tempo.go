348e76fc8e176d61adf3ea1edddf4b40e9fbfbe4
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2021-05-19 23:53:41 +0200

    Datasource: Shared HTTP client provider for core backend data sources and any data source using the data source proxy (#33439)
    Uses new httpclient package from grafana-plugin-sdk-go introduced 
    via grafana/grafana-plugin-sdk-go#328. 
    Replaces the GetHTTPClient, GetTransport, GetTLSConfig methods defined 
    on DataSource model.
    Longer-term the goal is to migrate core HTTP backend data sources to use the 
    SDK contracts and using httpclient.Provider for creating HTTP clients and such.
    
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/tsdb/tempo/tempo.go
+++ pkg/tsdb/tempo/tempo.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/grafana/grafana/pkg/infra/httpclient"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/plugins"
@@ -18,17 +19,20 @@ type tempoExecutor struct {
 	httpClient *http.Client
 }
 
-// NewExecutor returns a tempoExecutor.DataQueryResult
+// NewExecutor returns a tempoExecutor.
 //nolint: staticcheck // plugins.DataPlugin deprecated
-func NewExecutor(dsInfo *models.DataSource) (plugins.DataPlugin, error) {
-	httpClient, err := dsInfo.GetHttpClient()
-	if err != nil {
-		return nil, err
-	}
+func New(httpClientProvider httpclient.Provider) func(*models.DataSource) (plugins.DataPlugin, error) {
+	//nolint: staticcheck // plugins.DataPlugin deprecated
+	return func(dsInfo *models.DataSource) (plugins.DataPlugin, error) {
+		httpClient, err := dsInfo.GetHTTPClient(httpClientProvider)
+		if err != nil {
+			return nil, err
+		}
 
-	return &tempoExecutor{
-		httpClient: httpClient,
-	}, nil
+		return &tempoExecutor{
+			httpClient: httpClient,
+		}, nil
+	}
 }
 
 var (
