b49dfc4132f9441ffe3fd87fc032ae36014f2c1b
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2021-05-19 12:23:05 +0100

    AzureMonitor: Add Azure Resource Graph (#33293) (#34360)
    * Add Azure Resource Graph in Azure Plugin
    
    * fix lodash import
    
    * Fix mock queries
    
    * use "backend" sdk
    
    * Address comments
    
    * add converter for object type
    
    * Query error should cause 400 & apply template var
    
    * fix backend test & add documentation
    
    * update image
    
    * Address comments
    
    * marshal body from map
    
    * use interpolated query instead of raw query
    
    * fix test
    
    * filter out empty queries
    
    * fix go linting problem
    
    * use new query field language name
    
    * improve variable tests
    
    * add better tests for interpolate variable
    
    Co-authored-by: joshhunt <josh@trtr.co>
    Co-authored-by: Erik Sundell <erik.sundell87@gmail.com>
    (cherry picked from commit 71fd0981ca2f32bec75feb9662357060a82e7ea9)
    
    Co-authored-by: shuotli <63325084+shuotli@users.noreply.github.com>

---
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/ArgQueryEditor/QueryField.tsx
@@ -0,0 +1,32 @@
+import { CodeEditor } from '@grafana/ui';
+import React, { useCallback } from 'react';
+import { AzureQueryEditorFieldProps } from '../../types';
+
+const QueryField: React.FC<AzureQueryEditorFieldProps> = ({ query, onQueryChange }) => {
+  const onChange = useCallback(
+    (newQuery: string) => {
+      onQueryChange({
+        ...query,
+        azureResourceGraph: {
+          ...query.azureResourceGraph,
+          query: newQuery,
+        },
+      });
+    },
+    [onQueryChange, query]
+  );
+
+  return (
+    <CodeEditor
+      value={query.azureResourceGraph.query}
+      language="kusto"
+      height={200}
+      width={1000}
+      showMiniMap={false}
+      onBlur={onChange}
+      onSave={onChange}
+    />
+  );
+};
+
+export default QueryField;
