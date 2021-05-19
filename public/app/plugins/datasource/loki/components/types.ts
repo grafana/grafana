8ddf6de6e7b95ad2e2c5614b66adc31f441de6af
Marcus Andersson <marcus.andersson@grafana.com>
2021-05-19 16:24:27 +0200

    Alerting: app specific query editors for Loki and Prometheus (#34365)
    * Adding simplified version of query editor based on app flag.
    
    * cleaned up the absolute time range.
    
    * changing placeholder text.
    
    * updated snapshot.
    
    * added some tests.
    
    * adding loki query editor tests.
    
    * updating snapshots.

---
+++ public/app/plugins/datasource/loki/components/types.ts
@@ -0,0 +1,5 @@
+import { QueryEditorProps } from '@grafana/data';
+import LokiDatasource from '../datasource';
+import { LokiOptions, LokiQuery } from '../types';
+
+export type LokiQueryEditorProps = QueryEditorProps<LokiDatasource, LokiQuery, LokiOptions>;
