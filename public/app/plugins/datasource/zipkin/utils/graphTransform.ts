615de9bf3432a48f7e4db67c3670520222a61bf4
Andrej Ocenas <mr.ocenas@gmail.com>
2021-05-20 10:01:28 +0200

    Zipkin: Add node graph view to trace response (#34414)
    * Add graph transform
    
    * Add tests
    
    * Refactor code
    
    * Update test
    
    * Fix zipkin block
    
    Co-authored-by: David Kaltschmidt <david.kaltschmidt@gmail.com>

--- public/app/plugins/datasource/zipkin/utils/graphTransform.ts
+++ public/app/plugins/datasource/zipkin/utils/graphTransform.ts
@@ -1,6 +1,6 @@
 import { DataFrame, NodeGraphDataFrameFieldNames as Fields } from '@grafana/data';
-import { Span, TraceResponse } from './types';
-import { getNonOverlappingDuration, getStats, makeFrames, makeSpanMap } from '../../../core/utils/tracing';
+import { ZipkinSpan } from '../types';
+import { getNonOverlappingDuration, getStats, makeFrames, makeSpanMap } from '../../../../core/utils/tracing';
 
 interface Node {
   [Fields.id]: string;
@@ -17,7 +17,7 @@ interface Edge {
   [Fields.source]: string;
 }
 
-export function createGraphFrames(data: TraceResponse): DataFrame[] {
+export function createGraphFrames(data: ZipkinSpan[]): DataFrame[] {
   const { nodes, edges } = convertTraceToGraph(data);
   const [nodesFrame, edgesFrame] = makeFrames();
 
@@ -32,51 +32,45 @@ export function createGraphFrames(data: TraceResponse): DataFrame[] {
   return [nodesFrame, edgesFrame];
 }
 
-function convertTraceToGraph(data: TraceResponse): { nodes: Node[]; edges: Edge[] } {
+function convertTraceToGraph(spans: ZipkinSpan[]): { nodes: Node[]; edges: Edge[] } {
   const nodes: Node[] = [];
   const edges: Edge[] = [];
 
-  const traceDuration = findTraceDuration(data.spans);
-
+  const traceDuration = findTraceDuration(spans);
   const spanMap = makeSpanMap((index) => {
-    if (index >= data.spans.length) {
+    if (index >= spans.length) {
       return undefined;
     }
-    const span = data.spans[index];
     return {
-      span,
-      id: span.spanID,
-      parentIds: span.references?.filter((r) => r.refType === 'CHILD_OF').map((r) => r.spanID) || [],
+      span: spans[index],
+      id: spans[index].id,
+      parentIds: spans[index].parentId ? [spans[index].parentId!] : [],
     };
   });
 
-  for (const span of data.spans) {
-    const process = data.processes[span.processID];
-
-    const ranges: Array<[number, number]> = spanMap[span.spanID].children.map((c) => {
+  for (const span of spans) {
+    const ranges: Array<[number, number]> = spanMap[span.id].children.map((c) => {
       const span = spanMap[c].span;
-      return [span.startTime, span.startTime + span.duration];
+      return [span.timestamp, span.timestamp + span.duration];
     });
     const childrenDuration = getNonOverlappingDuration(ranges);
     const selfDuration = span.duration - childrenDuration;
     const stats = getStats(span.duration / 1000, traceDuration / 1000, selfDuration / 1000);
 
     nodes.push({
-      [Fields.id]: span.spanID,
-      [Fields.title]: process?.serviceName ?? '',
-      [Fields.subTitle]: span.operationName,
+      [Fields.id]: span.id,
+      [Fields.title]: span.localEndpoint?.serviceName || span.remoteEndpoint?.serviceName || 'unknown',
+      [Fields.subTitle]: span.name,
       [Fields.mainStat]: stats.main,
       [Fields.secondaryStat]: stats.secondary,
       [Fields.color]: selfDuration / traceDuration,
     });
 
-    const parentSpanID = span.references?.find((r) => r.refType === 'CHILD_OF')?.spanID;
-    // Sometimes some span can be missing. Don't add edges for those.
-    if (parentSpanID && spanMap[parentSpanID].span) {
+    if (span.parentId && spanMap[span.parentId].span) {
       edges.push({
-        [Fields.id]: parentSpanID + '--' + span.spanID,
-        [Fields.target]: span.spanID,
-        [Fields.source]: parentSpanID,
+        [Fields.id]: span.parentId + '--' + span.id,
+        [Fields.target]: span.id,
+        [Fields.source]: span.parentId,
       });
     }
   }
@@ -88,19 +82,18 @@ function convertTraceToGraph(data: TraceResponse): { nodes: Node[]; edges: Edge[
  * Get the duration of the whole trace as it isn't a part of the response data.
  * Note: Seems like this should be the same as just longest span, but this is probably safer.
  */
-function findTraceDuration(spans: Span[]): number {
+function findTraceDuration(spans: ZipkinSpan[]): number {
   let traceEndTime = 0;
   let traceStartTime = Infinity;
 
   for (const span of spans) {
-    if (span.startTime < traceStartTime) {
-      traceStartTime = span.startTime;
+    if (span.timestamp < traceStartTime) {
+      traceStartTime = span.timestamp;
     }
 
-    if (span.startTime + span.duration > traceEndTime) {
-      traceEndTime = span.startTime + span.duration;
+    if (span.timestamp + span.duration > traceEndTime) {
+      traceEndTime = span.timestamp + span.duration;
     }
   }
-
   return traceEndTime - traceStartTime;
 }
