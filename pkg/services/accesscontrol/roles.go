c56597b3a53efc9a0246a5742fdb3f5b354db1ed
Emil Tullstedt <emil.tullstedt@grafana.com>
2021-05-20 10:50:34 +0200

    Update free text references to predefined roles

--- pkg/services/accesscontrol/roles.go
+++ pkg/services/accesscontrol/roles.go
@@ -135,13 +135,13 @@ var provisioningAdminRole = RoleDTO{
 	},
 }
 
-// PredefinedRoles provides a map of permission sets/roles which can be
+// FixedRoles provides a map of permission sets/roles which can be
 // assigned to a set of users. When adding a new resource protected by
 // Grafana access control the default permissions should be added to a
-// new predefined role in this set so that users can access the new
-// resource. PredefinedRoleGrants lists which organization roles are
-// assigned which predefined roles in this list.
-var PredefinedRoles = map[string]RoleDTO{
+// new fixed role in this set so that users can access the new
+// resource. FixedRoleGrants lists which built-in roles are
+// assigned which fixed roles in this list.
+var FixedRoles = map[string]RoleDTO{
 	usersAdminRead: usersAdminReadRole,
 	usersAdminEdit: usersAdminEditRole,
 
@@ -167,9 +167,9 @@ const (
 	provisioningAdmin = "fixed:provisioning:admin"
 )
 
-// PredefinedRoleGrants specifies which organization roles are assigned
-// to which set of PredefinedRoles by default. Alphabetically sorted.
-var PredefinedRoleGrants = map[string][]string{
+// FixedRoleGrants specifies which built-in roles are assigned
+// to which set of FixedRoles by default. Alphabetically sorted.
+var FixedRoleGrants = map[string][]string{
 	RoleGrafanaAdmin: {
 		ldapAdminEdit,
 		ldapAdminRead,
