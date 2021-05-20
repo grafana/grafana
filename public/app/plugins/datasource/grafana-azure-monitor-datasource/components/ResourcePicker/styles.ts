319453bd15ae9369fcfe3fc3588a98e7cff8dea0
Sarah Zinger <sarahzinger@users.noreply.github.com>
2021-05-20 04:20:01 -0400

    Azure Monitor: Fix Resource Picker UI overflow (#34442)
    * Truncate text
    
    * Replace HorizontalGroup.
    
    * Add loading spinner
    
    * increase modal width
    
    Co-authored-by: joshhunt <josh@trtr.co>

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/ResourcePicker/styles.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/ResourcePicker/styles.ts
@@ -4,6 +4,7 @@ import { GrafanaTheme2 } from '@grafana/data';
 const getStyles = (theme: GrafanaTheme2) => ({
   table: css({
     width: '100%',
+    tableLayout: 'fixed',
   }),
 
   tableScroller: css({
@@ -30,7 +31,8 @@ const getStyles = (theme: GrafanaTheme2) => ({
   cell: css({
     padding: theme.spacing(1, 0),
     width: '25%',
-
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
     '&:first-of-type': {
       width: '50%',
       padding: theme.spacing(1, 0, 1, 2),
@@ -41,6 +43,25 @@ const getStyles = (theme: GrafanaTheme2) => ({
 
   loadingCell: css({
     textAlign: 'center',
+  }),
+
+  spinner: css({
+    marginBottom: 0,
+  }),
+
+  nestedEntry: css({
+    display: 'flex',
+  }),
+
+  entryContentItem: css({
+    margin: theme.spacing(0, 1, 0, 0),
+  }),
+
+  truncated: css({
+    minWidth: 0,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
   }),
 });
 
