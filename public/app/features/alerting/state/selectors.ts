f3a894b5f4f5ce99e7d304f92e312fcb7283c0c6
Peter Holmberg <peter.hlmbrg@gmail.com>
2021-05-20 16:35:12 +0200

    remove old ngalerting code

--- public/app/features/alerting/state/selectors.ts
+++ public/app/features/alerting/state/selectors.ts
@@ -1,27 +1,13 @@
-import { AlertDefinition, AlertRule, AlertRulesState, NotificationChannelState, StoreState } from 'app/types';
-import { config } from '@grafana/runtime';
+import { AlertRule, AlertRulesState, NotificationChannelState, StoreState } from 'app/types';
 
 export const getSearchQuery = (state: AlertRulesState) => state.searchQuery;
 
-export const getAlertRuleItems = (state: StoreState) => {
+export const getAlertRuleItems = (state: StoreState): AlertRule[] => {
   const regex = new RegExp(state.alertRules.searchQuery, 'i');
-  const result: Array<AlertRule | AlertDefinition> = [];
 
-  result.push(
-    ...state.alertRules.items.filter((item) => {
-      return regex.test(item.name) || regex.test(item.stateText) || regex.test(item.info!);
-    })
-  );
-
-  if (config.featureToggles.ngalert) {
-    result.push(
-      ...state.alertDefinition.alertDefinitions.filter((item) => {
-        return regex.test(item.title);
-      })
-    );
-  }
-
-  return result;
+  return state.alertRules.items.filter((item) => {
+    return regex.test(item.name) || regex.test(item.stateText) || regex.test(item.info!);
+  });
 };
 
 export const getNotificationChannel = (state: NotificationChannelState, channelId: number) => {
