fd022276cfe53e18dd0b6b7ee2fdd24e69e4d407
bergquist <carl.bergquist@gmail.com>
2021-05-24 12:18:18 +0200

    instrumentation: start tracing database requests
    ref https://github.com/grafana/grafana/issues/29489
    
    Signed-off-by: bergquist <carl.bergquist@gmail.com>

--- pkg/services/sqlstore/database_wrapper.go
+++ pkg/services/sqlstore/database_wrapper.go
@@ -94,7 +94,7 @@ func (h *databaseQueryWrapper) instrument(ctx context.Context, status string, qu
 		histogram.Observe(elapsed.Seconds())
 	}
 
-	span, _ := opentracing.StartSpanFromContext(ctx, "database query")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "database query")
 	defer span.Finish()
 
 	span.LogFields(
