04195f5a01718a9f8e9d920636109fd5329ec173
thisisobate <obasiuche62@gmail.com>
2021-05-25 10:25:51 +0100

    Table: datalink to have text underline and support for image datalink

--- packages/grafana-ui/src/components/Table/ImageCell.tsx
+++ packages/grafana-ui/src/components/Table/ImageCell.tsx
@@ -1,14 +1,39 @@
-import React, { FC } from 'react';
+import { LinkModel } from '@grafana/data';
+import React, { FC, MouseEventHandler } from 'react';
 import { TableCellProps } from './types';
 
 export const ImageCell: FC<TableCellProps> = (props) => {
-  const { field, cell, tableStyles, cellProps } = props;
+  const { field, cell, tableStyles, row, cellProps } = props;
 
   const displayValue = field.display!(cell.value);
 
+  let link: LinkModel<any> | undefined;
+  let onClick: MouseEventHandler<HTMLAnchorElement> | undefined;
+
+  if (field.getLinks) {
+    link = field.getLinks({
+      valueRowIndex: row.index,
+    })[0];
+  }
+
+  if (link && link.onClick) {
+    onClick = (event) => {
+      // Allow opening in new tab
+      if (!(event.ctrlKey || event.metaKey || event.shiftKey) && link!.onClick) {
+        event.preventDefault();
+        link!.onClick(event);
+      }
+    };
+  }
+
   return (
     <div {...cellProps} className={tableStyles.cellContainer}>
-      <img src={displayValue.text} className={tableStyles.imageCell} />
+      {!link && <img src={displayValue.text} className={tableStyles.imageCell} />}
+      {link && (
+        <a href={link.href} onClick={onClick} target={link.target} title={link.title} className={tableStyles.cellLink}>
+          <img src={displayValue.text} className={tableStyles.imageCell} />
+        </a>
+      )}
     </div>
   );
 };
