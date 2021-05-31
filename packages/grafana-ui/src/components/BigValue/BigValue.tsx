293d3e817c3422daff2c01215918fcc57e3200e0
Torkel Ödegaard <torkel@grafana.org>
2021-05-31 09:01:47 +0200

    StatPanel: Fixes data link tooltip not showing for single value (#34934)

--- packages/grafana-ui/src/components/BigValue/BigValue.tsx
+++ packages/grafana-ui/src/components/BigValue/BigValue.tsx
@@ -59,6 +59,8 @@ export interface Props extends Themeable2 {
   text?: TextDisplayOptions;
   /** Specify which text should be visible in the BigValue */
   textMode?: BigValueTextMode;
+  /** If true disables the tooltip */
+  hasLinks?: boolean;
 
   /**
    * If part of a series of stat panes, this is the total number.
@@ -73,7 +75,7 @@ export class BigValue extends PureComponent<Props> {
   };
 
   render() {
-    const { onClick, className } = this.props;
+    const { onClick, className, hasLinks } = this.props;
     const layout = buildLayout(this.props);
     const panelStyles = layout.getPanelStyles();
     const valueAndTitleContainerStyles = layout.getValueAndTitleContainerStyles();
@@ -81,8 +83,11 @@ export class BigValue extends PureComponent<Props> {
     const titleStyles = layout.getTitleStyles();
     const textValues = layout.textValues;
 
+    // When there is an outer data link this tooltip will override the outer native tooltip
+    const tooltip = hasLinks ? undefined : textValues.tooltip;
+
     return (
-      <div className={className} style={panelStyles} onClick={onClick} title={textValues.tooltip}>
+      <div className={className} style={panelStyles} onClick={onClick} title={tooltip}>
         <div style={valueAndTitleContainerStyles}>
           {textValues.title && <div style={titleStyles}>{textValues.title}</div>}
           <FormattedValueDisplay value={textValues} style={valueStyles} />
