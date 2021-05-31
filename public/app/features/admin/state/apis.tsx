859d0069c0088ae700c5766bb148fa77da1446b1
Clarity-89 <homes89@ukr.net>
2021-05-31 14:16:15 +0300

    Hide per-role counts for licensed users

--- public/app/features/admin/state/apis.tsx
+++ public/app/features/admin/state/apis.tsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import { getBackendSrv } from '@grafana/runtime';
+import { PopoverContent } from '@grafana/ui';
+import { config } from 'app/core/config';
 
 export interface ServerStat {
   name: string;
   value: number;
+  tooltip?: PopoverContent;
 }
+
+const { hasLicense } = config.licenseInfo;
 
 export const getServerStats = async (): Promise<ServerStat[]> => {
   try {
@@ -13,10 +19,24 @@ export const getServerStats = async (): Promise<ServerStat[]> => {
       { name: 'Total admins', value: res.admins },
       { name: 'Total editors', value: res.editors },
       { name: 'Total viewers', value: res.viewers },
-      { name: 'Active users (seen last 30 days)', value: res.activeUsers },
-      { name: 'Active admins (seen last 30 days)', value: res.activeAdmins },
-      { name: 'Active editors (seen last 30 days)', value: res.activeEditors },
-      { name: 'Active viewers (seen last 30 days)', value: res.activeViewers },
+      {
+        name: 'Active users (seen last 30 days)',
+        value: res.activeUsers,
+        tooltip: hasLicense
+          ? () => (
+              <>
+                For active user count by role, see the <a href="/admin/licensing">Licensing page</a>.
+              </>
+            )
+          : '',
+      },
+      ...(!hasLicense
+        ? [
+            { name: 'Active admins (seen last 30 days)', value: res.activeAdmins },
+            { name: 'Active editors (seen last 30 days)', value: res.activeEditors },
+            { name: 'Active viewers (seen last 30 days)', value: res.activeViewers },
+          ]
+        : []),
       { name: 'Active sessions', value: res.activeSessions },
       { name: 'Total dashboards', value: res.dashboards },
       { name: 'Total orgs', value: res.orgs },
