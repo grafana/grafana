f68670045f734e2dc702ca3ef4d33c66f2645001
Alexandra Vargas <alexa1866@gmail.com>
2021-05-31 17:36:14 +0200

    Dashboard: Fix Table view when editing causes the panel data to not update
    - Add event subscription on PanelEditorTableView
    - Extract runAllPanelQueries into Panel Model in order to use it on PanelChrome and PanelEditorTableView
    - Add simple unit test for runAllQueryPaneQueries

--- public/app/features/dashboard/components/PanelEditor/PanelEditorTableView.tsx
+++ public/app/features/dashboard/components/PanelEditor/PanelEditorTableView.tsx
@@ -1,22 +1,42 @@
 import { PanelChrome } from '@grafana/ui';
 import { PanelRenderer } from 'app/features/panel/PanelRenderer';
-import React, { useState } from 'react';
-import { PanelModel } from '../../state';
+import React, { useCallback, useEffect, useState } from 'react';
+import { PanelModel, DashboardModel } from '../../state';
 import { usePanelLatestData } from './usePanelLatestData';
 import { PanelOptions } from 'app/plugins/panel/table/models.gen';
-
+import { RefreshEvent } from 'app/types/events';
+import { Subscription } from 'rxjs';
+import { applyPanelTimeOverrides } from 'app/features/dashboard/utils/panel';
+import { getTimeSrv, TimeSrv } from '../../services/TimeSrv';
 interface Props {
   width: number;
   height: number;
   panel: PanelModel;
+  dashboard: DashboardModel;
 }
 
-export function PanelEditorTableView({ width, height, panel }: Props) {
-  const { data } = usePanelLatestData(panel, { withTransforms: true, withFieldConfig: false }, true);
+export function PanelEditorTableView({ width, height, panel, dashboard }: Props) {
+  const { data } = usePanelLatestData(panel, { withTransforms: true, withFieldConfig: false }, false);
   const [options, setOptions] = useState<PanelOptions>({
     frameIndex: 0,
     showHeader: true,
   });
+
+  const timeSrv: TimeSrv = getTimeSrv();
+  const timeData = applyPanelTimeOverrides(panel, timeSrv.timeRange());
+
+  const onRefresh = useCallback(() => {
+    panel.runAllPanelQueries(dashboard.id, dashboard.getTimezone(), timeData, width);
+  }, [dashboard, panel, timeData, width]);
+
+  // Subscribe to panel events
+  useEffect(() => {
+    const subs = new Subscription();
+    subs.add(panel.events.subscribe(RefreshEvent, onRefresh));
+    return () => {
+      subs.unsubscribe();
+    };
+  }, [onRefresh, panel]);
 
   if (!data) {
     return null;
