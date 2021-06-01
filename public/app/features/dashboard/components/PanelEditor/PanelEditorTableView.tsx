ff03b456f1d4181a7b1949cd2fb956e1ca1246d6
Maria Alexandra <239999+axelavargas@users.noreply.github.com>
2021-06-01 13:52:08 +0200

    Dashboard: Fix Table view when editing causes the panel data to not update (#34998)
    * Dashboard: Fix Table view when editing causes the panel data to not update
    
    - Add event subscription on PanelEditorTableView
    - Extract runAllPanelQueries into Panel Model in order to use it on PanelChrome and PanelEditorTableView
    - Add simple unit test for runAllQueryPaneQueries

--- public/app/features/dashboard/components/PanelEditor/PanelEditorTableView.tsx
+++ public/app/features/dashboard/components/PanelEditor/PanelEditorTableView.tsx
@@ -1,22 +1,38 @@
 import { PanelChrome } from '@grafana/ui';
 import { PanelRenderer } from 'app/features/panel/PanelRenderer';
-import React, { useState } from 'react';
-import { PanelModel } from '../../state';
+import React, { useEffect, useState } from 'react';
+import { PanelModel, DashboardModel } from '../../state';
 import { usePanelLatestData } from './usePanelLatestData';
 import { PanelOptions } from 'app/plugins/panel/table/models.gen';
-
+import { RefreshEvent } from 'app/types/events';
+import { applyPanelTimeOverrides } from 'app/features/dashboard/utils/panel';
+import { getTimeSrv, TimeSrv } from '../../services/TimeSrv';
 interface Props {
   width: number;
   height: number;
   panel: PanelModel;
+  dashboard: DashboardModel;
 }
 
-export function PanelEditorTableView({ width, height, panel }: Props) {
-  const { data } = usePanelLatestData(panel, { withTransforms: true, withFieldConfig: false }, true);
+export function PanelEditorTableView({ width, height, panel, dashboard }: Props) {
+  const { data } = usePanelLatestData(panel, { withTransforms: true, withFieldConfig: false }, false);
   const [options, setOptions] = useState<PanelOptions>({
     frameIndex: 0,
     showHeader: true,
   });
+
+  // Subscribe to panel event
+  useEffect(() => {
+    const timeSrv: TimeSrv = getTimeSrv();
+    const timeData = applyPanelTimeOverrides(panel, timeSrv.timeRange());
+
+    const sub = panel.events.subscribe(RefreshEvent, () => {
+      panel.runAllPanelQueries(dashboard.id, dashboard.getTimezone(), timeData, width);
+    });
+    return () => {
+      sub.unsubscribe();
+    };
+  }, [panel, dashboard, width]);
 
   if (!data) {
     return null;
