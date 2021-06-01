4470a4dcd5b328fc8b41579eb4bcd189b16ce4ba
joshhunt <josh@trtr.co>
2021-06-01 16:31:50 +0100

    AzureMonitor: Use display names for resource types and locations

---
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/azureMetadata/resourceTypes.ts
@@ -0,0 +1,566 @@
+export const resourceTypeMetadata = [
+  {
+    resourceType: 'microsoft.analysisservices/servers',
+    displayName: 'Analysis Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.synapse/workspaces/bigdatapools',
+    displayName: 'Apache Spark pools',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.apimanagement/service',
+    displayName: 'API Management services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.appconfiguration/configurationstores',
+    displayName: 'App Configuration',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.web/sites/slots',
+    displayName: 'App Service (Slots)',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.web/hostingenvironments',
+    displayName: 'App Service Environments',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.web/serverfarms',
+    displayName: 'App Service plans',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.web/sites',
+    displayName: 'App Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/applicationgateways',
+    displayName: 'Application gateways',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.insights/components',
+    displayName: 'Application Insights',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.automation/automationaccounts',
+    displayName: 'Automation Accounts',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.insights/autoscalesettings',
+    displayName: 'Autoscale Settings',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.aadiam/azureadmetrics',
+    displayName: 'Azure AD Metrics',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.cache/redis',
+    displayName: 'Azure Cache for Redis',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.documentdb/databaseaccounts',
+    displayName: 'Azure Cosmos DB accounts',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.kusto/clusters',
+    displayName: 'Azure Data Explorer Clusters',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.dbformariadb/servers',
+    displayName: 'Azure Database for MariaDB servers',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.dbformysql/servers',
+    displayName: 'Azure Database for MySQL servers',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.dbforpostgresql/flexibleservers',
+    displayName: 'Azure Database for PostgreSQL flexible servers',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.dbforpostgresql/servergroupsv2',
+    displayName: 'Azure Database for PostgreSQL server groups',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.dbforpostgresql/servers',
+    displayName: 'Azure Database for PostgreSQL servers',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.dbforpostgresql/serversv2',
+    displayName: 'Azure Database for PostgreSQL servers v2',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.resources/subscriptions',
+    displayName: 'Azure Resource Manager',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.appplatform/spring',
+    displayName: 'Azure Spring Cloud',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.databoxedge/databoxedgedevices',
+    displayName: 'Azure Stack Edge / Data Box Gateway',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.azurestackresourcemonitor/storageaccountmonitor',
+    displayName: 'Azure Stack Resource Monitor',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.synapse/workspaces',
+    displayName: 'Azure Synapse Analytics',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/bastionhosts',
+    displayName: 'Bastions',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.batch/batchaccounts',
+    displayName: 'Batch accounts',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.botservice/botservices',
+    displayName: 'Bot Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.netapp/netappaccounts/capacitypools',
+    displayName: 'Capacity pools',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.classiccompute/domainnames',
+    displayName: 'Cloud services (classic)',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.vmwarecloudsimple/virtualmachines',
+    displayName: 'CloudSimple Virtual Machines',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.cognitiveservices/accounts',
+    displayName: 'Cognitive Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/networkwatchers/connectionmonitors',
+    displayName: 'Connection Monitors',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/connections',
+    displayName: 'Connections',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.containerinstance/containergroups',
+    displayName: 'Container instances',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.containerregistry/registries',
+    displayName: 'Container registries',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.insights/qos',
+    displayName: 'Custom Metric Usage',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.customerinsights/hubs',
+    displayName: 'CustomerInsights',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.datafactory/datafactories',
+    displayName: 'Data factories',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.datafactory/factories',
+    displayName: 'Data factories (V2)',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.datalakeanalytics/accounts',
+    displayName: 'Data Lake Analytics',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.datalakestore/accounts',
+    displayName: 'Data Lake Storage Gen1',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.datashare/accounts',
+    displayName: 'Data Shares',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.synapse/workspaces/sqlpools',
+    displayName: 'Dedicated SQL pools',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.devices/provisioningservices',
+    displayName: 'Device Provisioning Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.compute/disks',
+    displayName: 'Disks',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/dnszones',
+    displayName: 'DNS zones',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.eventgrid/domains',
+    displayName: 'Event Grid Domains',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.eventgrid/systemtopics',
+    displayName: 'Event Grid System Topics',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.eventgrid/topics',
+    displayName: 'Event Grid Topics',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.eventhub/clusters',
+    displayName: 'Event Hubs Clusters',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.eventhub/namespaces',
+    displayName: 'Event Hubs Namespaces',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/expressroutecircuits',
+    displayName: 'ExpressRoute circuits',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/expressrouteports',
+    displayName: 'ExpressRoute Direct',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/expressroutegateways',
+    displayName: 'ExpressRoute Gateways',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.fabric.admin/fabriclocations',
+    displayName: 'Fabric Locations',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/azurefirewalls',
+    displayName: 'Firewalls',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/frontdoors',
+    displayName: 'Front Doors',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.hdinsight/clusters',
+    displayName: 'HDInsight clusters',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.storagecache/caches',
+    displayName: 'HPC caches',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.logic/integrationserviceenvironments',
+    displayName: 'Integration Service Environments',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.iotcentral/iotapps',
+    displayName: 'IoT Central Applications',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.devices/iothubs',
+    displayName: 'IoT Hub',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.keyvault/vaults',
+    displayName: 'Key vaults',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.kubernetes/connectedclusters',
+    displayName: 'Kubernetes - Azure Arc',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.containerservice/managedclusters',
+    displayName: 'Kubernetes services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.media/mediaservices/liveevents',
+    displayName: 'Live events',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/loadbalancers',
+    displayName: 'Load balancers',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.operationalinsights/workspaces',
+    displayName: 'Log Analytics workspaces',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.logic/workflows',
+    displayName: 'Logic apps',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.machinelearningservices/workspaces',
+    displayName: 'Machine learning',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.media/mediaservices',
+    displayName: 'Media Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/natgateways',
+    displayName: 'NAT gateways',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/networkinterfaces',
+    displayName: 'Network interfaces',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/networkvirtualappliances',
+    displayName: 'Network Virtual Appliances',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/networkwatchers',
+    displayName: 'Network Watchers',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.notificationhubs/namespaces/notificationhubs',
+    displayName: 'Notification Hubs',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/p2svpngateways',
+    displayName: 'P2S VPN Gateways',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.peering/peeringservices',
+    displayName: 'Peering Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.powerbidedicated/capacities',
+    displayName: 'Power BI Embedded',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/privateendpoints',
+    displayName: 'Private endpoints',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/privatelinkservices',
+    displayName: 'Private link services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/publicipaddresses',
+    displayName: 'Public IP addresses',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.cache/redisenterprise',
+    displayName: 'Redis Enterprise',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.relay/namespaces',
+    displayName: 'Relays',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.synapse/workspaces/scopepools',
+    displayName: 'Scope pools',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.search/searchservices',
+    displayName: 'Search services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.servicebus/namespaces',
+    displayName: 'Service Bus Namespaces',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.signalrservice/signalr',
+    displayName: 'SignalR',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.operationsmanagement/solutions',
+    displayName: 'Solutions',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.sql/servers/databases',
+    displayName: 'SQL databases',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.sql/servers/elasticpools',
+    displayName: 'SQL elastic pools',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.sql/managedinstances',
+    displayName: 'SQL managed instances',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.storage/storageaccounts',
+    displayName: 'Storage accounts',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.classicstorage/storageaccounts',
+    displayName: 'Storage accounts (classic)',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.storagesync/storagesyncservices',
+    displayName: 'Storage Sync Services',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.streamanalytics/streamingjobs',
+    displayName: 'Stream Analytics jobs',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.media/mediaservices/streamingendpoints',
+    displayName: 'Streaming Endpoints',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.timeseriesinsights/environments',
+    displayName: 'Time Series Insights environments',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/trafficmanagerprofiles',
+    displayName: 'Traffic Manager profiles',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.compute/virtualmachinescalesets',
+    displayName: 'Virtual machine scale sets',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.compute/virtualmachines',
+    displayName: 'Virtual machines',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.classiccompute/virtualmachines',
+    displayName: 'Virtual machines (classic)',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/virtualnetworkgateways',
+    displayName: 'Virtual network gateways',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.netapp/netappaccounts/capacitypools/volumes',
+    displayName: 'Volumes',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.network/vpngateways',
+    displayName: 'VPN Gateways',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.cdn/cdnwebapplicationfirewallpolicies',
+    displayName: 'Web application firewall policies (WAF)',
+    supportsLogs: true,
+  },
+  {
+    resourceType: 'microsoft.web/hostingenvironments/workerpools',
+    displayName: 'WorkerPools',
+    supportsLogs: true,
+  },
+];
+
+export const logsSupportedResourceTypesKusto = resourceTypeMetadata
+  .filter((v) => v.supportsLogs)
+  .map((v) => `"${v.resourceType}"`)
+  .join(',');
+
+// Object, keyed by resourceType ID
+export const resourceTypeDisplayNames: Record<string, string> = resourceTypeMetadata.reduce(
+  (acc, resourceType) => ({
+    ...acc,
+    [resourceType.resourceType]: resourceType.displayName,
+  }),
+  {}
+);
