224fac7a87671f9b25bdab6641ba542aad864480
Tiago Mota Santos <tiago.mota@percona.com>
2021-06-02 10:05:23 +0100

    Add percona icons to IconName

--- packages/grafana-ui/src/types/icon.ts
+++ packages/grafana-ui/src/types/icon.ts
@@ -134,7 +134,26 @@ export type IconName =
   | 'user'
   | 'users-alt'
   | 'wrap-text'
-  | 'x';
+  | 'x'
+  | 'percona-database'
+  | 'percona-analytics'
+  | 'percona-cluster'
+  | 'percona-cluster-network'
+  | 'percona-cluster-computing'
+  | 'percona-dashboard'
+  | 'percona-kubernetes'
+  | 'percona-summary'
+  | 'percona-inventory'
+  | 'percona-add'
+  | 'percona-alert'
+  | 'percona-disk'
+  | 'percona-memory'
+  | 'percona-temperature'
+  | 'percona-cpu'
+  | 'percona-surface'
+  | 'percona-network'
+  | 'percona-process'
+  | 'percona-setting';
 
 export const getAvailableIcons = (): IconName[] => [
   'angle-double-down',
