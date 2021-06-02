ab5cf5cb11ce476eedcf15a8ab1f7b5ba3d97209
dsotirakis <sotirakis.dim@gmail.com>
2021-06-02 18:23:36 +0300

    Remove unnecessary funcs

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -29,7 +29,6 @@ func (hs *HTTPServer) registerRoutes() {
 	reqOrgAdmin := middleware.ReqOrgAdmin
 	reqCanAccessTeams := middleware.AdminOrFeatureEnabled(hs.Cfg.EditorsCanAdmin)
 	reqSnapshotPublicModeOrSignedIn := middleware.SnapshotPublicModeOrSignedIn(hs.Cfg)
-	redirectFromLegacyDashboardSoloURL := middleware.RedirectFromLegacyDashboardSoloURL(hs.Cfg)
 	redirectFromLegacyPanelEditURL := middleware.RedirectFromLegacyPanelEditURL(hs.Cfg)
 	authorize := acmiddleware.Middleware(hs.AccessControl)
 	quota := middleware.Quota(hs.QuotaService)
@@ -88,7 +87,6 @@ func (hs *HTTPServer) registerRoutes() {
 	r.Get("/dashboard-solo/snapshot/*", hs.Index)
 	r.Get("/d-solo/:uid/:slug", reqSignedIn, hs.Index)
 	r.Get("/d-solo/:uid", reqSignedIn, hs.Index)
-	r.Get("/dashboard-solo/db/:slug", reqSignedIn, redirectFromLegacyDashboardSoloURL, hs.Index)
 	r.Get("/dashboard-solo/script/*", reqSignedIn, hs.Index)
 	r.Get("/import/dashboard", reqSignedIn, hs.Index)
 	r.Get("/dashboards/", reqSignedIn, hs.Index)
--- pkg/middleware/dashboard_redirect.go
+++ pkg/middleware/dashboard_redirect.go
@@ -4,30 +4,9 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/setting"
 )
-
-func getDashboardURLBySlug(orgID int64) (string, error) {
-	// TODO: Drop bus call
-	query := models.GetDashboardQuery{OrgId: orgID}
-	if err := bus.Dispatch(&query); err != nil {
-		return "", models.ErrDashboardNotFound
-	}
-
-	return models.GetDashboardUrl(query.Result.Uid, query.Result.Slug), nil
-}
-
-func RedirectFromLegacyDashboardURL() func(c *models.ReqContext) {
-	return func(c *models.ReqContext) {
-		if url, err := getDashboardURLBySlug(c.OrgId); err == nil {
-			url = fmt.Sprintf("%s?%s", url, c.Req.URL.RawQuery)
-			c.Redirect(url, 301)
-			return
-		}
-	}
-}
 
 // In Grafana v7.0 we changed panel edit & view query parameters.
 // This middleware tries to detect those old url parameters and direct to the new url query params
@@ -53,24 +32,5 @@ func RedirectFromLegacyPanelEditURL(cfg *setting.Cfg) func(c *models.ReqContext)
 			newURL := fmt.Sprintf("%s%s?%s", cfg.AppURL, strings.TrimPrefix(c.Req.URL.Path, "/"), queryParams.Encode())
 			c.Redirect(newURL, 301)
 		}
-	}
-}
-
-func RedirectFromLegacyDashboardSoloURL(cfg *setting.Cfg) func(c *models.ReqContext) {
-	return func(c *models.ReqContext) {
-		renderRequest := c.QueryBool("render")
-
-		url, err := getDashboardURLBySlug(c.OrgId)
-		if err != nil {
-			return
-		}
-
-		if renderRequest && strings.Contains(url, cfg.AppSubURL) {
-			url = strings.Replace(url, cfg.AppSubURL, "", 1)
-		}
-
-		url = strings.Replace(url, "/d/", "/d-solo/", 1)
-		url = fmt.Sprintf("%s?%s", url, c.Req.URL.RawQuery)
-		c.Redirect(url, 301)
 	}
 }
