7359ba44d0d7e3001aba9dc31b97d228585fbac1
Dominik Prokop <dominik.prokop@grafana.com>
2021-06-03 04:43:47 +0200

    Timeline/Status grid panel: Add tooltip support (#35005)
    * Timeline/Status grid tooltip support first pass
    
    * Tooltips workin
    
    * Use getValueFormat to get the duration
    
    * Separate boxes highlight from tooltip interpolation
    
    * Separate state timeline tooltip component, rely on field display color to retrieve color of series
    
    * create an onHover/onLeave API and optimize implementation
    
    Co-authored-by: Leon Sorokin <leeoniya@gmail.com>

--- public/app/plugins/panel/state-timeline/TimelineChart.tsx
+++ public/app/plugins/panel/state-timeline/TimelineChart.tsx
@@ -13,12 +13,14 @@ import {
 } from '@grafana/ui';
 import { DataFrame, FieldType, TimeRange } from '@grafana/data';
 import { preparePlotConfigBuilder } from './utils';
-import { TimelineMode, TimelineValueAlignment } from './types';
+import { TimelineMode, TimelineOptions, TimelineValueAlignment } from './types';
 
 /**
  * @alpha
  */
-export interface TimelineProps extends Omit<GraphNGProps, 'prepConfig' | 'propsToDiff' | 'renderLegend'> {
+export interface TimelineProps
+  extends TimelineOptions,
+    Omit<GraphNGProps, 'prepConfig' | 'propsToDiff' | 'renderLegend'> {
   mode: TimelineMode;
   rowHeight: number;
   showValue: BarValueVisibility;
