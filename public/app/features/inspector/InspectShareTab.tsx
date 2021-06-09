eaca89447d36ddbbd989f3515c4c2cc03edad54d
Hugo Häggmark <hugo.haggmark@gmail.com>
2021-06-09 11:38:34 +0200

    Chore: fixes some styles and text

--- public/app/features/inspector/InspectShareTab.tsx
+++ public/app/features/inspector/InspectShareTab.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { Alert, Button, FeatureBadge, Field, HorizontalGroup, Switch, VerticalGroup } from '@grafana/ui';
+import { useAsync } from 'react-use';
+import { css } from '@emotion/css';
+import { AppEvents, FeatureState, GrafanaTheme2 } from '@grafana/data';
+import { Alert, Button, FeatureBadge, Field, HorizontalGroup, Switch, useStyles2, VerticalGroup } from '@grafana/ui';
 
 import {
   getCollectorSanitizers,
@@ -10,9 +13,7 @@ import { inspectDownloader, inspectPackager } from '../dashboard/components/Insp
 import { DashboardModel, PanelModel } from '../dashboard/state';
 import { CopyToClipboard } from '../../core/components/CopyToClipboard/CopyToClipboard';
 import appEvents from '../../core/app_events';
-import { AppEvents, FeatureState } from '@grafana/data';
 import { CollectorType, Sanitizer } from '../dashboard/components/Inspector/types';
-import { useAsync } from 'react-use';
 
 interface Props {
   dashboard: DashboardModel;
@@ -32,6 +33,7 @@ export function InspectShareTab({ dashboard, panel }: Props): JSX.Element {
 
     return inspectPackager().package(items);
   }, [dashboard, panel, sanitizers]);
+  const styles = useStyles2(getStyles);
 
   const onDownloadClick = () => {
     if (value) {
@@ -50,26 +52,37 @@ export function InspectShareTab({ dashboard, panel }: Props): JSX.Element {
   return (
     <VerticalGroup>
       <Alert title="Sharing data" severity="info">
+        <p>Even though we work hard to minimize errors in Grafana they sometimes appear.</p>
         <p>
-          Even though we work hard to minimize errors in Grafana sometimes they appear. Making an error disappear
-          involves reporting the error
-          <a href="https://github.com/grafana/grafana/issues/new/choose">&nbsp;by creating an issue first&nbsp;</a>
+          Making an error disappear involves
+          <a href="https://github.com/grafana/grafana/issues/new/choose" target="_blank" rel="noopener noreferrer">
+            &nbsp;creating an issue on GitHub&nbsp;
+          </a>
           and then adding enough information and reproducible steps to make the investigation and fixing the error a
           breeze.
-          <br />
-          <br />
-          We think this feature will make it easier for you to attach the essential information needed for us to fix an
-          issue.
-          <br />
-          <br />
+        </p>
+        <p>
+          This feature will collect information about your <em>OS</em>, <em>Browser</em>, <em>Grafana</em>,&nbsp;
+          <em>Dashboard</em> & <em>Panel</em> and package it in such a way that it will make it easier for you to attach
+          the essential information needed when reporting an issue. The collected data is only available on this page
+          and is not shared or stored in any way.
+        </p>
+        <p>
           <em>
-            Make sure you sanitize any sensitive data before you attach it to any issue as it will become public to
+            Make sure you remove any sensitive data before you attach it to any issue as it will become public to
             everyone.
           </em>
         </p>
       </Alert>
-      <HorizontalGroup>
-        <Field label="Sanitize data" description="Replaces all sensitive information in the collected data with ******">
+      <div className={styles.sanitizeContainer}>
+        <Field
+          label="Sanitize data"
+          description={
+            <span>
+              Replaces sensitive information in the collected data with <em>******</em>
+            </span>
+          }
+        >
           <Switch
             value={Boolean(sanitizers.length)}
             onChange={(event) => {
@@ -81,20 +94,34 @@ export function InspectShareTab({ dashboard, panel }: Props): JSX.Element {
             }}
           />
         </Field>
-        <FeatureBadge featureState={FeatureState.alpha} />
-      </HorizontalGroup>
+        <div className={styles.featureBadge}>
+          <FeatureBadge featureState={FeatureState.alpha} />
+        </div>
+      </div>
       <HorizontalGroup>
-        <Button type="button" icon="download-alt" onClick={onDownloadClick}>
+        <Button type="button" icon="download-alt" onClick={onDownloadClick} variant="secondary">
           Download shared data
         </Button>
         <CopyToClipboard text={() => value ?? ''} onSuccess={onClipboardSuccess} elType="div">
-          <Button type="button" icon="clipboard-alt" variant="secondary">
+          <Button type="button" icon="clipboard-alt">
             Copy shared data to clipboard
           </Button>
         </CopyToClipboard>
       </HorizontalGroup>
     </VerticalGroup>
   );
+}
+
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    sanitizeContainer: css`
+      display: flex;
+    `,
+    featureBadge: css`
+      align-self: start;
+      padding: ${theme.spacing(0, 1)};
+    `,
+  };
 }
 
 function LoadingIndicator(): JSX.Element {
