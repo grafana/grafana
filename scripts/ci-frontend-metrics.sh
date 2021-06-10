19b57deeef42cdeffb1dabc8ec5afd0995bea4c4
Jack Westbrook <jack.westbrook@gmail.com>
2021-06-10 15:30:58 +0200

    ci(frontend-metrics): introduce outdated and vulnerable dependency commands

--- scripts/ci-frontend-metrics.sh
+++ scripts/ci-frontend-metrics.sh
@@ -11,6 +11,12 @@ LEGACY_FORMS="$(grep -r -oP 'LegacyForms;' public/app | wc -l)"
 STRICT_LINT_RESULTS="$(yarn run eslint --rule '@typescript-eslint/no-explicit-any: ["error"]' --format unix ./public/ || true)"
 STRICT_LINT_EXPLICIT_ANY="$(echo "${STRICT_LINT_RESULTS}" | grep -o "no-explicit-any" | wc -l)"
 
+OUTDATED_DEPENDENCIES="$(yarn outdated | wc -l | xargs)"
+VULNERABILITY_AUDIT="$(yarn audit | grep 'Severity:' | grep -Eo '[0-9]{1,4}')"
+LOW_VULNERABILITIES="$(echo $VULNERABILITY_AUDIT | cut -d' ' -f1)"
+MED_VULNERABILITIES="$(echo $VULNERABILITY_AUDIT | cut -d' ' -f2)"
+HIGH_VULNERABILITIES="$(echo $VULNERABILITY_AUDIT | cut -d' ' -f3)"
+
 echo -e "Typescript errors: $ERROR_COUNT"
 echo -e "Directives: $DIRECTIVES"
 echo -e "Controllers: $CONTROLLERS"
@@ -18,6 +24,10 @@ echo -e "Stories: $STORIES_COUNT"
 echo -e "Documented stories: $MDX_COUNT"
 echo -e "Legacy forms: $LEGACY_FORMS"
 echo -e "TS Explicit any: $STRICT_LINT_EXPLICIT_ANY"
+echo -e "low vulnerabilities: $LOW_VULNERABILITIES"
+echo -e "med vulnerabilities: $MED_VULNERABILITIES"
+echo -e "high vulnerabilities: $HIGH_VULNERABILITIES"
+echo -e "total outdated depdendencies: $OUTDATED_DEPENDENCIES"
 
 echo "Metrics: {
   \"grafana.ci-code.strictErrors\": \"${ERROR_COUNT}\",
@@ -27,4 +37,8 @@ echo "Metrics: {
   \"grafana.ci-code.grafana-ui.mdx\": \"${MDX_COUNT}\",
   \"grafana.ci-code.legacyForms\": \"${LEGACY_FORMS}\",
   \"grafana.ci-code.strictLint.noExplicitAny\": \"${STRICT_LINT_EXPLICIT_ANY}\"
+  \"grafana.ci-code.dependencies.outdated\": \"${OUTDATED_DEPENDENCIES}\",
+  \"grafana.ci-code.dependencies.lowVulnerabilities\": \"${LOW_VULNERABILITIES}\",
+  \"grafana.ci-code.dependencies.mediumVulnerabilities\": \"${MED_VULNERABILITIES}\",
+  \"grafana.ci-code.dependencies.highVulnerabilities\": \"${HIGH_VULNERABILITIES}\",
 }"
