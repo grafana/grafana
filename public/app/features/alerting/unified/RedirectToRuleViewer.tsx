0bd98ce84ba36582b73ef45d8ec8485dac4236e9
Marcus Andersson <marcus.andersson@grafana.com>
2021-06-10 10:17:52 +0200

    listing rules with same name.

--- public/app/features/alerting/unified/RedirectToRuleViewer.tsx
+++ public/app/features/alerting/unified/RedirectToRuleViewer.tsx
@@ -1,46 +1,91 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import { css } from '@emotion/css';
+import { GrafanaTheme2 } from '@grafana/data';
+import { Card, Icon, LoadingPlaceholder, useStyles2, withErrorBoundary } from '@grafana/ui';
 import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
-import { withErrorBoundary } from '@grafana/ui';
 import { useCombinedRulesMatching } from './hooks/useCombinedRule';
 import { createViewLink } from './utils/misc';
 import { getRulesSourceByName } from './utils/datasource';
-import { RuleViewerLayoutContent } from './components/rule-viewer/RuleViewerLayout';
+import { RuleViewerLayout } from './components/rule-viewer/RuleViewerLayout';
+import { AlertLabels } from './components/AlertLabels';
 
 type RedirectToRuleViewerProps = GrafanaRouteComponentProps<{ name?: string; sourceName?: string }>;
 
 export function RedirectToRuleViewer(props: RedirectToRuleViewerProps): JSX.Element | null {
   const { name, sourceName } = props.match.params;
+  const styles = useStyles2(getStyles);
   const { error, loading, result: rules, dispatched } = useCombinedRulesMatching(name, sourceName);
 
   if (error) {
     return <div>error</div>;
   }
 
-  if (loading || !dispatched) {
-    return <div>loading</div>;
+  if (loading || !dispatched || !Array.isArray(rules)) {
+    return (
+      <RuleViewerLayout>
+        <LoadingPlaceholder text="Loading rule..." />
+      </RuleViewerLayout>
+    );
   }
 
   if (!name || !sourceName) {
     return <Redirect to="" />;
   }
 
-  if (Array.isArray(rules) && rules.length === 1) {
+  const rulesSource = getRulesSourceByName(sourceName);
+
+  if (!rulesSource) {
+    return null;
+  }
+
+  if (rules.length === 1) {
     const [rule] = rules;
-    const rulesSource = getRulesSourceByName(sourceName);
-
-    if (!rulesSource) {
-      return <div>error</div>;
-    }
-
     return <Redirect to={createViewLink(rulesSource, rule, '/alerting/list')} />;
   }
 
   return (
-    <RuleViewerLayoutContent>
-      found: {name} in {sourceName}
-    </RuleViewerLayoutContent>
+    <RuleViewerLayout>
+      <div>
+        Several rules in <span className={styles.param}>{sourceName}</span> matched the name{' '}
+        <span className={styles.param}>{name}</span>, please select the rule you want to view.
+      </div>
+      <div className={styles.rules}>
+        {rules.map((rule, index) => {
+          return (
+            <Card
+              key={`${rule.name}-${index}`}
+              heading={rule.name}
+              href={createViewLink(rulesSource, rule, '/alerting/list')}
+            >
+              <Card.Meta separator={''}>
+                <Icon name="folder" />
+                <span className={styles.namespace}>{`${rule.namespace.name} / ${rule.group.name}`}</span>
+              </Card.Meta>
+              <Card.Tags>
+                <AlertLabels labels={rule.labels} />
+              </Card.Tags>
+            </Card>
+          );
+        })}
+      </div>
+    </RuleViewerLayout>
   );
+}
+
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    param: css`
+      font-style: italic;
+      color: ${theme.colors.text.secondary};
+    `,
+    rules: css`
+      margin-top: ${theme.spacing(2)};
+    `,
+    namespace: css`
+      margin-left: ${theme.spacing(1)};
+    `,
+  };
 }
 
 export default withErrorBoundary(RedirectToRuleViewer, { style: 'page' });
