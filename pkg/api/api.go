9c84377ba68a513ad5e42db5c5a456011f2f6b37
Jeremy Price <Jeremy.price@grafana.com>
2021-06-11 17:31:14 +0200

    Update pkg/api/api.go
    Co-authored-by: Karl Persson <kalle.persson@grafana.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -248,7 +248,7 @@ func (hs *HTTPServer) registerRoutes() {
 		// auth api keys
 		apiRoute.Group("/auth/keys", func(keysRoute routing.RouteRegister) {
 			keysRoute.Get("/", authorize(reqOrgAdmin, accesscontrol.ActionApiKeyList, accesscontrol.ScopeUsersAll), routing.Wrap(GetAPIKeys))
-			keysRoute.Post("/", authorize(reqOrgAdmin, accesscontrol.ActionApiKeyAdd, accesscontrol.ScopeUsersAll), quota("api_key"), bind(models.AddApiKeyCommand{}), routing.Wrap(hs.AddAPIKey))
+			keysRoute.Post("/", authorize(reqOrgAdmin, accesscontrol.ActionApiKeyAdd), quota("api_key"), bind(models.AddApiKeyCommand{}), routing.Wrap(hs.AddAPIKey))
 			keysRoute.Delete("/:id", authorize(reqOrgAdmin, accesscontrol.ActionApiKeyDelete, accesscontrol.ScopeUsersAll), routing.Wrap(DeleteAPIKey))
 		})
 
