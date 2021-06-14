56a0b34fa317514e8493fb3f373792fd50dc0a30
Piotr Jamróz <pm.jamroz@gmail.com>
2021-06-14 16:31:36 +0200

    Add tests for data frame conversion

--- pkg/tsdb/graphite/graphite_test.go
+++ pkg/tsdb/graphite/graphite_test.go
@@ -1,7 +1,15 @@
 package graphite
 
 import (
+	"encoding/json"
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -58,4 +66,36 @@ func TestFixIntervalFormat(t *testing.T) {
 			assert.Equal(t, tc.expected, tr)
 		})
 	}
+
+	executor := &GraphiteExecutor{}
+
+	t.Run("Converts response to data frames", func(*testing.T) {
+		body := `
+		[
+			{
+				"target": "target",
+				"tags": { "fooTag": "fooValue", "barTag": "barValue" },
+				"datapoints": [[50, 1], [null, 2], [50, 3]]
+			}
+		]`
+		a := 50.0
+		expectedFrame := data.NewFrame("target",
+			data.NewField("time", nil, []time.Time{time.Unix(1, 0).UTC(), time.Unix(2, 0).UTC(), time.Unix(3, 0).UTC()}),
+			data.NewField("value", data.Labels{
+				"fooTag": "fooValue",
+				"barTag": "barValue",
+			}, []*float64{&a, nil, &a}).SetConfig(&data.FieldConfig{DisplayNameFromDS: "target"}),
+		)
+		expectedFrames := data.Frames{expectedFrame}
+
+		httpResponse := &http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(body))}
+		dataFrames, err := executor.toDataFrames(httpResponse)
+
+		require.NoError(t, err)
+		if !reflect.DeepEqual(expectedFrames, dataFrames) {
+			expectedFramesJSON, _ := json.Marshal(expectedFrames)
+			dataFramesJSON, _ := json.Marshal(dataFrames)
+			t.Errorf("Data frames should have been equal but was, expected:\n%s\nactual:\n%s", expectedFramesJSON, dataFramesJSON)
+		}
+	})
 }
