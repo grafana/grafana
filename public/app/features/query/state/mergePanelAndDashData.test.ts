b0d857bb44bddcd257674e8e5edc2844eaacdc8b
Hugo Häggmark <hugo.haggmark@gmail.com>
2021-06-14 14:50:44 +0200

    Chore: Refactors flaky Frontend test

--- public/app/features/query/state/mergePanelAndDashData.test.ts
+++ public/app/features/query/state/mergePanelAndDashData.test.ts
@@ -3,7 +3,7 @@ import { AlertState, getDefaultTimeRange, LoadingState, PanelData, toDataFrame }
 
 import { DashboardQueryRunnerResult } from './DashboardQueryRunner/types';
 import { mergePanelAndDashData } from './mergePanelAndDashData';
-import { delay } from 'rxjs/operators';
+import { TestScheduler } from 'rxjs/testing';
 
 function getTestContext() {
   const timeRange = getDefaultTimeRange();
@@ -20,100 +20,133 @@ function getTestContext() {
   const panelObservable: Observable<PanelData> = scheduled(of(panelData), asyncScheduler);
   const dashObservable: Observable<DashboardQueryRunnerResult> = scheduled(of(dashData), asyncScheduler);
 
-  return { timeRange, panelObservable, dashObservable };
+  return { timeRange, panelObservable, dashObservable, panelData, dashData };
+}
+
+// https://rxjs-dev.firebaseapp.com/guide/testing/marble-testing
+function runMarbleTest(args: {
+  panelMarble: string;
+  dashMarble: string;
+  panelValues: { [marble: string]: any };
+  dashValues: { [marble: string]: any };
+  expectedValues: { [marble: string]: any };
+  expectedMarble: string;
+}) {
+  const { expectedValues, expectedMarble, panelMarble, panelValues, dashMarble, dashValues } = args;
+  const scheduler: TestScheduler = new TestScheduler((actual, expected) => {
+    expect(actual).toEqual(expected);
+  });
+
+  scheduler.run(({ cold, expectObservable }) => {
+    const panelObservable = cold(panelMarble, panelValues);
+    const dashObservable = cold(dashMarble, dashValues);
+    const result = mergePanelAndDashData(panelObservable, dashObservable);
+    expectObservable(result).toBe(expectedMarble, expectedValues);
+  });
+  scheduler.flush();
 }
 
 describe('mergePanelAndDashboardData', () => {
   describe('when both results are fast', () => {
-    it('then just combine the results', async () => {
-      const { dashObservable, panelObservable, timeRange } = getTestContext();
+    it('then just combine the results', () => {
+      const { panelData, dashData, timeRange } = getTestContext();
 
-      await expect(mergePanelAndDashData(panelObservable, dashObservable)).toEmitValuesWith((received) => {
-        expect(received).toHaveLength(1);
-        const results = received[0];
-        expect(results).toEqual({
-          state: LoadingState.Done,
-          series: [],
-          annotations: [toDataFrame([{ id: 'panelData' }]), toDataFrame([{ id: 'dashData' }])],
-          alertState: { id: 1, state: AlertState.OK, dashboardId: 1, panelId: 1, newStateDate: '' },
-          timeRange,
-        });
+      runMarbleTest({
+        panelMarble: '10ms a',
+        panelValues: { a: panelData },
+        dashMarble: '10ms a',
+        dashValues: { a: dashData },
+        expectedMarble: '10ms a',
+        expectedValues: {
+          a: {
+            state: LoadingState.Done,
+            series: [],
+            annotations: [toDataFrame([{ id: 'panelData' }]), toDataFrame([{ id: 'dashData' }])],
+            alertState: { id: 1, state: AlertState.OK, dashboardId: 1, panelId: 1, newStateDate: '' },
+            timeRange,
+          },
+        },
       });
     });
   });
 
   describe('when dashboard results are slow', () => {
-    it('then flush panel data first', async () => {
-      const { dashObservable, panelObservable, timeRange } = getTestContext();
+    it('then flush panel data first', () => {
+      const { panelData, dashData, timeRange } = getTestContext();
 
-      await expect(mergePanelAndDashData(panelObservable, dashObservable.pipe(delay(250)))).toEmitValuesWith(
-        (received) => {
-          expect(received).toHaveLength(2);
-          const fastResults = received[0];
-          const slowResults = received[1];
-          expect(fastResults).toEqual({
+      runMarbleTest({
+        panelMarble: '10ms a',
+        panelValues: { a: panelData },
+        dashMarble: '210ms a',
+        dashValues: { a: dashData },
+        expectedMarble: '200ms a 9ms b',
+        expectedValues: {
+          a: {
             state: LoadingState.Done,
             series: [],
             annotations: [toDataFrame([{ id: 'panelData' }])],
-            alertState: undefined,
             timeRange,
-          });
-          expect(slowResults).toEqual({
+          },
+          b: {
             state: LoadingState.Done,
             series: [],
             annotations: [toDataFrame([{ id: 'panelData' }]), toDataFrame([{ id: 'dashData' }])],
             alertState: { id: 1, state: AlertState.OK, dashboardId: 1, panelId: 1, newStateDate: '' },
             timeRange,
-          });
-        }
-      );
+          },
+        },
+      });
     });
   });
 
   describe('when panel results are slow', () => {
-    it('then just combine the results', async () => {
-      const { dashObservable, panelObservable, timeRange } = getTestContext();
+    it('then just combine the results', () => {
+      const { panelData, dashData, timeRange } = getTestContext();
 
-      await expect(mergePanelAndDashData(panelObservable.pipe(delay(250)), dashObservable)).toEmitValuesWith(
-        (received) => {
-          expect(received).toHaveLength(1);
-          const results = received[0];
-          expect(results).toEqual({
+      runMarbleTest({
+        panelMarble: '210ms a',
+        panelValues: { a: panelData },
+        dashMarble: '10ms a',
+        dashValues: { a: dashData },
+        expectedMarble: '210ms a',
+        expectedValues: {
+          a: {
             state: LoadingState.Done,
             series: [],
             annotations: [toDataFrame([{ id: 'panelData' }]), toDataFrame([{ id: 'dashData' }])],
             alertState: { id: 1, state: AlertState.OK, dashboardId: 1, panelId: 1, newStateDate: '' },
             timeRange,
-          });
-        }
-      );
+          },
+        },
+      });
     });
   });
 
   describe('when both results are slow', () => {
-    it('then flush panel data first', async () => {
-      const { dashObservable, panelObservable, timeRange } = getTestContext();
+    it('then flush panel data first', () => {
+      const { panelData, dashData, timeRange } = getTestContext();
 
-      await expect(
-        mergePanelAndDashData(panelObservable.pipe(delay(250)), dashObservable.pipe(delay(250)))
-      ).toEmitValuesWith((received) => {
-        expect(received).toHaveLength(2);
-        const fastResults = received[0];
-        const slowResults = received[1];
-        expect(fastResults).toEqual({
-          state: LoadingState.Done,
-          series: [],
-          annotations: [toDataFrame([{ id: 'panelData' }])],
-          alertState: undefined,
-          timeRange,
-        });
-        expect(slowResults).toEqual({
-          state: LoadingState.Done,
-          series: [],
-          annotations: [toDataFrame([{ id: 'panelData' }]), toDataFrame([{ id: 'dashData' }])],
-          alertState: { id: 1, state: AlertState.OK, dashboardId: 1, panelId: 1, newStateDate: '' },
-          timeRange,
-        });
+      runMarbleTest({
+        panelMarble: '210ms a',
+        panelValues: { a: panelData },
+        dashMarble: '210ms a',
+        dashValues: { a: dashData },
+        expectedMarble: '210ms (ab)',
+        expectedValues: {
+          a: {
+            state: LoadingState.Done,
+            series: [],
+            annotations: [toDataFrame([{ id: 'panelData' }])],
+            timeRange,
+          },
+          b: {
+            state: LoadingState.Done,
+            series: [],
+            annotations: [toDataFrame([{ id: 'panelData' }]), toDataFrame([{ id: 'dashData' }])],
+            alertState: { id: 1, state: AlertState.OK, dashboardId: 1, panelId: 1, newStateDate: '' },
+            timeRange,
+          },
+        },
       });
     });
   });
