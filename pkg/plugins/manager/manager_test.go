4a0f4e99fd11ba9caaab60e6452fd7e0d65c0a10
Will Browne <will.browne@grafana.com>
2021-06-14 10:51:22 +0200

    remove test since case already covered

--- pkg/plugins/manager/manager_test.go
+++ pkg/plugins/manager/manager_test.go
@@ -371,34 +371,7 @@ func TestPluginManager_Init(t *testing.T) {
 		assert.Nil(t, pm.plugins[("test")])
 	})
 
-	t.Run("With back-end plugin with a lib dir that has symbolic links", func(t *testing.T) {
-		origAppURL := setting.AppUrl
-		t.Cleanup(func() {
-			setting.AppUrl = origAppURL
-		})
-		setting.AppUrl = defaultAppURL
-
-		pm := createManager(t, func(pm *PluginManager) {
-			pm.Cfg.PluginsPath = "testdata/symbolic-file-links"
-		})
-		err := pm.Init()
-		require.NoError(t, err)
-		// This plugin should be properly registered, even though it has a symbolicly linked file in it.
-		require.Empty(t, pm.scanningErrors)
-		const pluginID = "test"
-		assert.NotNil(t, pm.plugins[pluginID])
-		assert.Equal(t, "datasource", pm.plugins[pluginID].Type)
-		assert.Equal(t, "Test", pm.plugins[pluginID].Name)
-		assert.Equal(t, pluginID, pm.plugins[pluginID].Id)
-		assert.Equal(t, "1.0.0", pm.plugins[pluginID].Info.Version)
-		assert.Equal(t, plugins.PluginSignatureValid, pm.plugins[pluginID].Signature)
-		assert.Equal(t, plugins.GrafanaType, pm.plugins[pluginID].SignatureType)
-		assert.Equal(t, "Grafana Labs", pm.plugins[pluginID].SignatureOrg)
-		assert.False(t, pm.plugins[pluginID].IsCorePlugin)
-		assert.NotNil(t, pm.plugins[("test")])
-	})
-
-	t.Run("With plugin that contains symlink directory", func(t *testing.T) {
+	t.Run("With plugin that contains symlink file + directory", func(t *testing.T) {
 		origAppURL := setting.AppUrl
 		t.Cleanup(func() {
 			setting.AppUrl = origAppURL
