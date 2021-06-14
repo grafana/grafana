25e53149edb064371484c8e262c49044da63f72b
Torkel Ödegaard <torkel@grafana.com>
2021-06-14 10:33:07 +0200

    FilterPill: Wip updated design & storybook

--- packages/grafana-ui/src/components/FilterPill/FilterPill.tsx
+++ packages/grafana-ui/src/components/FilterPill/FilterPill.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { stylesFactory, useTheme2 } from '../../themes';
+import { useStyles2 } from '../../themes';
 import { GrafanaTheme2 } from '@grafana/data';
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import { IconButton } from '../IconButton/IconButton';
 import { IconName } from '../../types';
 
@@ -13,48 +13,46 @@ export interface FilterPillProps {
 }
 
 export const FilterPill: React.FC<FilterPillProps> = ({ label, selected, onClick, icon = 'check' }) => {
-  const theme = useTheme2();
-  const styles = getFilterPillStyles(theme, selected);
+  const styles = useStyles2(getStyles);
   return (
-    <div className={styles.wrapper} onClick={onClick}>
-      <IconButton
-        name={icon}
-        onClick={(e) => {
-          e.stopPropagation();
-          onClick(e);
-        }}
-        className={styles.icon}
-        surface="header"
-      />
-      <span className={styles.label}>{label}</span>
+    <div className={cx(styles.wrapper, selected && styles.selected)} onClick={onClick}>
+      <span>{label}</span>
+      {selected && (
+        <IconButton
+          name={icon}
+          onClick={(e) => {
+            e.stopPropagation();
+            onClick(e);
+          }}
+          className={styles.icon}
+          surface="header"
+        />
+      )}
     </div>
   );
 };
 
-const getFilterPillStyles = stylesFactory((theme: GrafanaTheme2, isSelected: boolean) => {
-  const labelColor = isSelected ? theme.colors.text.primary : theme.colors.text.secondary;
-
+const getStyles = (theme: GrafanaTheme2) => {
   return {
     wrapper: css`
       padding: ${theme.spacing(0.25)} ${theme.spacing(1)};
       background: ${theme.colors.background.secondary};
-      border-radius: ${theme.shape.borderRadius()};
-      padding: ${theme.spacing(0, 2, 0, 0.5)};
+      border-radius: ${theme.shape.borderRadius(8)};
+      padding: ${theme.spacing(0, 2)};
       font-weight: ${theme.typography.fontWeightMedium};
       font-size: ${theme.typography.size.sm};
-      color: ${theme.colors.text.primary};
+      color: ${theme.colors.text.secondary};
       display: flex;
       align-items: center;
       height: 32px;
       cursor: pointer;
     `,
-    icon: css`
-      margin-right: ${theme.spacing(1)};
-      margin-left: ${theme.spacing(0.5)};
-      color: ${labelColor};
+    selected: css`
+      color: ${theme.colors.text.primary};
+      background: ${theme.colors.action.selected};
     `,
-    label: css`
-      color: ${labelColor};
+    icon: css`
+      margin: ${theme.spacing(0, 0, 0, 1)};
     `,
   };
-});
+};
