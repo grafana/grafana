aceea777dba3d060e57b722d92e1573e559619ce
Clarity-89 <homes89@ukr.net>
2021-06-15 14:56:10 +0300

    Fix date formatting

--- packages/grafana-ui/src/components/DatePickerWithInput/DatePickerWithInput.tsx
+++ packages/grafana-ui/src/components/DatePickerWithInput/DatePickerWithInput.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { css } from '@emotion/css';
+import { dateTimeFormat } from '@grafana/data';
 import { DatePicker } from '../DatePicker/DatePicker';
 import { Props as InputProps, Input } from '../Input/Input';
 import { useStyles } from '../../themes';
 
-export const formatDate = (date: Date) => date.toISOString().split('T')[0];
+export const formatDate = (date: Date) => dateTimeFormat(date, { format: 'YYYY-MM-DD' });
 
 export interface DatePickerWithInputProps extends Omit<InputProps, 'ref' | 'value' | 'onChange'> {
   value?: Date;
