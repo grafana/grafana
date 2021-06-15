e5c030861eeab534bca0f82262849503d8680780
Uchechukwu Obasi <obasiuche62@gmail.com>
2021-06-15 15:37:36 +0100

    FileUpload: refactor story from knobs to control (#35760)

--- packages/grafana-ui/src/components/FileUpload/FileUpload.story.tsx
+++ packages/grafana-ui/src/components/FileUpload/FileUpload.story.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { FileUpload } from '@grafana/ui';
 import mdx from './FileUpload.mdx';
-import { useSize } from '../../utils/storybook/useSize';
-import { ComponentSize } from '../../types/size';
+import { Meta, Story } from '@storybook/react';
 
 export default {
   title: 'Forms/FileUpload',
@@ -13,15 +12,28 @@ export default {
     docs: {
       page: mdx,
     },
+    controls: {
+      exclude: ['className', 'onFileUpload'],
+    },
   },
-};
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['xs', 'sm', 'md', 'lg'],
+    },
+  },
+} as Meta;
 
-export const Single = () => {
-  const size = useSize();
+export const Basic: Story = (args) => {
   return (
     <FileUpload
-      size={size as ComponentSize}
+      size={args.size}
       onFileUpload={({ currentTarget }) => console.log('file', currentTarget?.files && currentTarget.files[0])}
     />
   );
+};
+Basic.args = {
+  size: 'md',
 };
