4e7413402ee07f30bb4592323ee10e7cd9aa5261
Jack Westbrook <jack.westbrook@gmail.com>
2021-06-15 18:13:19 +0200

    feat(dashboard): introduce selector with fallback for unknown plugin

--- public/app/features/dashboard/state/selectors.ts
+++ public/app/features/dashboard/state/selectors.ts
@@ -1,4 +1,6 @@
-import { DashboardState, PanelState } from 'app/types';
+import { DashboardState, PanelState, StoreState } from 'app/types';
+import { PanelPlugin } from '@grafana/data';
+import { getPanelPluginNotFound } from '../dashgrid/PanelPluginError';
 
 export function getPanelStateById(state: DashboardState, panelId: number): PanelState {
   if (!panelId) {
@@ -6,4 +8,10 @@ export function getPanelStateById(state: DashboardState, panelId: number): Panel
   }
 
   return state.panels[panelId] ?? ({} as PanelState);
+}
+
+export function getPanelPluginWithFallback(state: StoreState, panelType: string): PanelPlugin {
+  const plugin = state.plugins.panels[panelType];
+
+  return plugin || getPanelPluginNotFound(`Panel plugin not found (${panelType})`, true);
 }
