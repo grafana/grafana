4e503b2726496b199962ecdd7c0f491ffd13685d
Erik Sundell <erik.sundell87@gmail.com>
2021-06-18 08:23:17 +0200

    add default ref id for ds variable queries

--- public/app/features/variables/query/queryRunners.test.ts
+++ public/app/features/variables/query/queryRunners.test.ts
@@ -1,4 +1,4 @@
-import { QueryRunners } from './queryRunners';
+import { QueryRunners, variableDummyRefId } from './queryRunners';
 import { getDefaultTimeRange, VariableSupportType } from '@grafana/data';
 import { VariableRefresh } from '../types';
 import { of } from 'rxjs';
@@ -290,8 +290,17 @@ describe('QueryRunners', () => {
     describe('and calling getTarget', () => {
       it('then it should return correct target', () => {
         const { runner, datasource, variable } = getDatasourceTestContext();
+
         const target = runner.getTarget({ datasource, variable });
         expect(target).toEqual({ refId: 'A', query: 'A query' });
+      });
+      describe('and ref id is missing', () => {
+        it('then it should return correct target', () => {
+          const { runner, datasource, variable } = getDatasourceTestContext();
+          delete variable.query.refId;
+          const target = runner.getTarget({ datasource, variable });
+          expect(target).toEqual({ refId: variableDummyRefId, query: 'A query' });
+        });
       });
     });
 
