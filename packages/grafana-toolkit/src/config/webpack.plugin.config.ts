cbb2625fc510cb21d52201842b4f6dfca3895f6c
Josh Hunt <joshhunt@users.noreply.github.com>
2021-06-22 11:52:08 +0100

    Grafana-UI: Remove references to old monaco libraries (#35557)

--- packages/grafana-toolkit/src/config/webpack.plugin.config.ts
+++ packages/grafana-toolkit/src/config/webpack.plugin.config.ts
@@ -191,8 +191,6 @@ const getBaseWebpackConfig: WebpackConfigurationGetter = async (options) => {
       '@grafana/ui',
       '@grafana/runtime',
       '@grafana/data',
-      'monaco-editor',
-      'react-monaco-editor',
       // @ts-ignore
       (context, request, callback) => {
         const prefix = 'grafana/';
