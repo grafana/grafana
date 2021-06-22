4e02dd04f4534639635df8ca02aa4781a7523049
Karl Persson <kalle.persson92@gmail.com>
2021-06-22 16:24:16 +0200

    Update pkg/api/admin.go
    Co-authored-by: Gabriel MABILLE <gamab@users.noreply.github.com>

--- pkg/api/admin.go
+++ pkg/api/admin.go
@@ -51,7 +51,7 @@ func (hs *HTTPServer) getAuthorizedSettings(ctx context.Context, user *models.Si
 	authorizedBag := make(setting.SettingsBag)
 
 	for section, keys := range bag {
-		ok, err := evaluate(getSettingsScope(section, "*"))
+		ok, err := evaluate(getSettingsScope(section, eval.ScopeAll))
 		if err != nil {
 			return nil, err
 		}
