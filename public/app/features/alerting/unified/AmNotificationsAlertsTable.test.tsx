b3daa6d8c838bbe1f4abf956a5bf8557edcc64f7
Nathan Rodman <nathanrodman@gmail.com>
2021-06-22 20:04:44 -0700

    Add basic unit test for alert notificaitons

---
+++ public/app/features/alerting/unified/AmNotificationsAlertsTable.test.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { locationService, setDataSourceSrv } from '@grafana/runtime';
+import { render, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { fetchAlertGroups } from './api/alertmanager';
+import { byTestId } from 'testing-library-selector';
+import { configureStore } from 'app/store/configureStore';
+import { typeAsJestMock } from 'test/helpers/typeAsJestMock';
+import AlertManagerNotifications from './AlertManagerNotifications';
+import { mockAlertGroup, mockAlertmanagerAlert, mockDataSource, MockDataSourceSrv } from './mocks';
+import { DataSourceType } from './utils/datasource';
+
+jest.mock('./api/alertmanager');
+
+const mocks = {
+  api: {
+    fetchAlertGroups: typeAsJestMock(fetchAlertGroups),
+  },
+};
+
+const renderAmNotifications = () => {
+  const store = configureStore();
+
+  return render(
+    <Provider store={store}>
+      <Router history={locationService.getHistory()}>
+        <AlertManagerNotifications />
+      </Router>
+    </Provider>
+  );
+};
+
+const dataSources = {
+  am: mockDataSource({
+    name: 'Alert Manager',
+    type: DataSourceType.Alertmanager,
+  }),
+};
+
+const ui = {
+  group: byTestId('notifications-group'),
+  groupCollapseToggle: byTestId('notifications-group-collapse-toggle'),
+  notificationsTable: byTestId('notifications-table'),
+};
+
+describe('AmNotifications', () => {
+  beforeAll(() => {
+    mocks.api.fetchAlertGroups.mockImplementation(() => {
+      return Promise.resolve([
+        mockAlertGroup({ labels: {}, alerts: [mockAlertmanagerAlert({ labels: { foo: 'bar' } })] }),
+        mockAlertGroup(),
+      ]);
+    });
+  });
+
+  beforeEach(() => {
+    setDataSourceSrv(new MockDataSourceSrv(dataSources));
+  });
+
+  it('loads and shows groups', async () => {
+    await renderAmNotifications();
+
+    await waitFor(() => expect(mocks.api.fetchAlertGroups).toHaveBeenCalled());
+
+    const groups = await ui.group.getAll();
+
+    expect(groups).toHaveLength(2);
+    expect(groups[0]).toHaveTextContent('No grouping');
+    expect(groups[1]).toHaveTextContent('severity=warningregion=US-Central');
+  });
+});
