0b0aa505f3615230fefe203c7d3efd34d800c9a0
Nathan Rodman <nathanrodman@gmail.com>
2021-06-22 20:06:39 -0700

    Rename Notificaitons component file

--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -428,9 +428,7 @@ export function getAppRoutes(): RouteDescriptor[] {
       path: '/alerting/alertmanager/',
       component: SafeDynamicImport(
         () =>
-          import(
-            /* webpackChunkName: "AlertManagerNotifications" */ 'app/features/alerting/unified/AlertManagerNotifications'
-          )
+          import(/* webpackChunkName: "AlertManagerNotifications" */ 'app/features/alerting/unified/AmNotifications')
       ),
     },
     {
