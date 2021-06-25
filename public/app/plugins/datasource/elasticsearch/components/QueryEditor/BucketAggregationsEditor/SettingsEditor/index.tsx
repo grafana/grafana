85074ae834467448ff7544cae0e38c6b48ab9f87
Elfo404 <me@giordanoricci.com>
2021-06-25 14:30:02 +0100

    Elasticsearch: Allow case sensitive custom options in date_histogram interval

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/BucketAggregationsEditor/SettingsEditor/index.tsx
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/BucketAggregationsEditor/SettingsEditor/index.tsx
@@ -4,34 +4,13 @@ import { useDispatch } from '../../../../hooks/useStatelessReducer';
 import { SettingsEditorContainer } from '../../SettingsEditorContainer';
 import { changeBucketAggregationSetting } from '../state/actions';
 import { BucketAggregation } from '../aggregations';
-import {
-  bucketAggregationConfig,
-  createOrderByOptionsFromMetrics,
-  intervalOptions,
-  orderOptions,
-  sizeOptions,
-} from '../utils';
+import { bucketAggregationConfig, createOrderByOptionsFromMetrics, orderOptions, sizeOptions } from '../utils';
 import { FiltersSettingsEditor } from './FiltersSettingsEditor';
 import { useDescription } from './useDescription';
 import { useQuery } from '../../ElasticsearchQueryContext';
-import { SelectableValue } from '@grafana/data';
+import { DateHistogramSettingsEditor } from './DateHistogramSettingsEditor';
 
-const isValidNewOption: ComponentProps<typeof Select>['isValidNewOption'] = (
-  inputValue,
-  _,
-  options: Array<SelectableValue<string>>
-) => {
-  // TODO: would be extremely nice here to allow only template variables and values that are
-  // valid date histogram's Interval options
-  const valueExists = options.some(({ value }) => value === inputValue);
-  // we also don't want users to create "empty" values
-  return !valueExists && inputValue.trim().length > 0;
-};
-
-const optionStartsWithValue: ComponentProps<typeof Select>['filterOption'] = (option: SelectableValue<string>, value) =>
-  option.value?.startsWith(value) || false;
-
-const inlineFieldProps: Partial<ComponentProps<typeof InlineField>> = {
+export const inlineFieldProps: Partial<ComponentProps<typeof InlineField>> = {
   labelWidth: 16,
 };
 
@@ -41,6 +20,7 @@ interface Props {
 
 export const SettingsEditor = ({ bucketAgg }: Props) => {
   const dispatch = useDispatch();
+
   const { metrics } = useQuery();
   const settingsDescription = useDescription(bucketAgg);
   const orderBy = createOrderByOptionsFromMetrics(metrics);
@@ -106,52 +86,7 @@ export const SettingsEditor = ({ bucketAgg }: Props) => {
         </InlineField>
       )}
 
-      {bucketAgg.type === 'date_histogram' && (
-        <>
-          <InlineField label="Interval" {...inlineFieldProps}>
-            <Select
-              onChange={(e) => dispatch(changeBucketAggregationSetting(bucketAgg, 'interval', e.value!))}
-              options={intervalOptions}
-              value={bucketAgg.settings?.interval || bucketAggregationConfig[bucketAgg.type].defaultSettings?.interval}
-              allowCustomValue
-              isValidNewOption={isValidNewOption}
-              filterOption={optionStartsWithValue}
-            />
-          </InlineField>
-
-          <InlineField label="Min Doc Count" {...inlineFieldProps}>
-            <Input
-              onBlur={(e) => dispatch(changeBucketAggregationSetting(bucketAgg, 'min_doc_count', e.target.value!))}
-              defaultValue={
-                bucketAgg.settings?.min_doc_count ||
-                bucketAggregationConfig[bucketAgg.type].defaultSettings?.min_doc_count
-              }
-            />
-          </InlineField>
-
-          <InlineField label="Trim Edges" {...inlineFieldProps} tooltip="Trim the edges on the timeseries datapoints">
-            <Input
-              onBlur={(e) => dispatch(changeBucketAggregationSetting(bucketAgg, 'trimEdges', e.target.value!))}
-              defaultValue={
-                bucketAgg.settings?.trimEdges || bucketAggregationConfig[bucketAgg.type].defaultSettings?.trimEdges
-              }
-            />
-          </InlineField>
-
-          <InlineField
-            label="Offset"
-            {...inlineFieldProps}
-            tooltip="Change the start value of each bucket by the specified positive (+) or negative offset (-) duration, such as 1h for an hour, or 1d for a day"
-          >
-            <Input
-              onBlur={(e) => dispatch(changeBucketAggregationSetting(bucketAgg, 'offset', e.target.value!))}
-              defaultValue={
-                bucketAgg.settings?.offset || bucketAggregationConfig[bucketAgg.type].defaultSettings?.offset
-              }
-            />
-          </InlineField>
-        </>
-      )}
+      {bucketAgg.type === 'date_histogram' && <DateHistogramSettingsEditor bucketAgg={bucketAgg} />}
 
       {bucketAgg.type === 'histogram' && (
         <>
