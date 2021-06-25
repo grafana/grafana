1a4949b62eb22a9e12a1461f96b6791c970b9e0b
Domas <domas.lapinskas@grafana.com>
2021-06-25 23:07:55 +0300

    Alerting: make alertmanager notifications view responsive (#36067)
    * refac DynamicTableWithGuidelines
    
    * more responsiveness fixes

--- public/app/features/alerting/unified/components/amnotifications/AmNotificationsAlertsTable.tsx
+++ public/app/features/alerting/unified/components/amnotifications/AmNotificationsAlertsTable.tsx
@@ -1,63 +1,91 @@
 import { AlertmanagerAlert } from 'app/plugins/datasource/alertmanager/types';
-import React from 'react';
-
-import { AmNotificationsAlertsTableRow } from './AmNotificationsAlertsTableRow';
-import { getAlertTableStyles } from '../../styles/table';
+import React, { useMemo } from 'react';
 import { useStyles2 } from '@grafana/ui';
-import { GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme2, intervalToAbbreviatedDurationString } from '@grafana/data';
 import { css } from '@emotion/css';
+import { DynamicTableColumnProps, DynamicTableItemProps } from '../DynamicTable';
+import { AmAlertStateTag } from '../silences/AmAlertStateTag';
+import { AlertLabels } from '../AlertLabels';
+import { DynamicTableWithGuidelines } from '../DynamicTableWithGuidelines';
+import { AmNotificationsAlertDetails } from './AmNotificationsAlertDetails';
 
 interface Props {
   alerts: AlertmanagerAlert[];
   alertManagerSourceName: string;
 }
 
+type AmNotificationsAlertsTableColumnProps = DynamicTableColumnProps<AlertmanagerAlert>;
+type AmNotificationsAlertsTableItemProps = DynamicTableItemProps<AlertmanagerAlert>;
+
 export const AmNotificationsAlertsTable = ({ alerts, alertManagerSourceName }: Props) => {
-  const tableStyles = useStyles2(getAlertTableStyles);
   const styles = useStyles2(getStyles);
+
+  const columns = useMemo(
+    (): AmNotificationsAlertsTableColumnProps[] => [
+      {
+        id: 'state',
+        label: 'State',
+        // eslint-disable-next-line react/display-name
+        renderCell: ({ data: alert }) => (
+          <>
+            <AmAlertStateTag state={alert.status.state} />
+            <span className={styles.duration}>
+              for{' '}
+              {intervalToAbbreviatedDurationString({
+                start: new Date(alert.startsAt),
+                end: new Date(alert.endsAt),
+              })}
+            </span>
+          </>
+        ),
+        size: '190px',
+      },
+      {
+        id: 'labels',
+        label: 'Labels',
+        // eslint-disable-next-line react/display-name
+        renderCell: ({ data: { labels } }) => <AlertLabels className={styles.labels} labels={labels} />,
+        size: 1,
+      },
+    ],
+    [styles]
+  );
+
+  const items = useMemo(
+    (): AmNotificationsAlertsTableItemProps[] =>
+      alerts.map((alert) => ({
+        id: alert.fingerprint,
+        data: alert,
+      })),
+    [alerts]
+  );
+
   return (
-    <div className={styles.tableWrapper}>
-      <table className={tableStyles.table} data-testid="notifications-table">
-        <colgroup>
-          <col className={tableStyles.colExpand} />
-          <col className={styles.stateCol} />
-          <col />
-          <col className={styles.buffer} />
-        </colgroup>
-        <thead>
-          <th></th>
-          <th>State</th>
-          <th>Labels</th>
-          <th />
-        </thead>
-        <tbody>
-          {alerts.map((alert, index) => {
-            return (
-              <AmNotificationsAlertsTableRow
-                className={index % 2 === 0 ? tableStyles.evenRow : undefined}
-                alert={alert}
-                alertManagerSourceName={alertManagerSourceName}
-                key={alert.fingerprint}
-              />
-            );
-          })}
-        </tbody>
-      </table>
+    <div className={styles.tableWrapper} data-testid="notifications-table">
+      <DynamicTableWithGuidelines
+        cols={columns}
+        items={items}
+        isExpandable={true}
+        renderExpandedContent={({ data: alert }) => (
+          <AmNotificationsAlertDetails alert={alert} alertManagerSourceName={alertManagerSourceName} />
+        )}
+      />
     </div>
   );
 };
 
 const getStyles = (theme: GrafanaTheme2) => ({
   tableWrapper: css`
-    margin: ${theme.spacing(2, 0, 0, 4.5)};
+    margin-top: ${theme.spacing(3)};
+    ${theme.breakpoints.up('md')} {
+      margin-left: ${theme.spacing(4.5)};
+    }
   `,
-  stateCol: css`
-    width: 228px;
+  duration: css`
+    margin-left: ${theme.spacing(1)};
+    font-size: ${theme.typography.bodySmall.fontSize};
   `,
-  labelsCol: css`
-    width: 60%;
-  `,
-  buffer: css`
-    width: ${theme.spacing(4.5)};
+  labels: css`
+    padding-bottom: 0;
   `,
 });
