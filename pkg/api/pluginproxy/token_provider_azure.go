8219bb482b49bc002f914a903a08358c85b7febc
Sergey Kostrukov <sekost@microsoft.com>
2021-06-28 12:29:08 -0700

    Renames

--- pkg/api/pluginproxy/token_provider_azure.go
+++ pkg/api/pluginproxy/token_provider_azure.go
@@ -9,17 +9,17 @@ import (
 )
 
 type azureAccessTokenProvider struct {
-	ctx  context.Context
-	impl aztokenprovider.AzureTokenProvider
+	ctx           context.Context
+	tokenProvider aztokenprovider.AzureTokenProvider
 }
 
 func newAzureAccessTokenProvider(ctx context.Context, cfg *setting.Cfg, authParams *plugins.JwtTokenAuth) *azureAccessTokenProvider {
 	return &azureAccessTokenProvider{
-		ctx:  ctx,
-		impl: aztokenprovider.NewAzureAccessTokenProvider(cfg, authParams),
+		ctx:           ctx,
+		tokenProvider: aztokenprovider.NewAzureAccessTokenProvider(cfg, authParams),
 	}
 }
 
 func (provider *azureAccessTokenProvider) GetAccessToken() (string, error) {
-	return provider.impl.GetAccessToken(provider.ctx)
+	return provider.tokenProvider.GetAccessToken(provider.ctx)
 }
