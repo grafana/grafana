f748fb01478016de8bd6b9e01d245eca2b09d8f0
Elfo404 <me@giordanoricci.com>
2021-06-29 12:47:30 +0200

    Add tests

--- public/app/plugins/datasource/elasticsearch/components/hooks/useCreatableSelectPersistedBehaviour.ts
+++ public/app/plugins/datasource/elasticsearch/components/hooks/useCreatableSelectPersistedBehaviour.ts
@@ -5,24 +5,25 @@ import { ComponentProps, useState } from 'react';
 const hasValue = <T extends SelectableValue>(searchValue: T['value']) => ({ value }: T) => value === searchValue;
 
 const getInitialState = <T extends SelectableValue>(initialOptions: T[], initialValue?: T['value']): T[] => {
-  return initialOptions.concat(
-    initialOptions.some(hasValue(initialValue))
-      ? []
-      : ({
-          value: initialValue,
-          label: initialValue,
-        } as T)
-  );
+  if (initialValue === undefined || initialOptions.some(hasValue(initialValue))) {
+    return initialOptions;
+  }
+
+  return initialOptions.concat({
+    value: initialValue,
+    label: initialValue,
+  } as T);
 };
 
 interface Params<T extends SelectableValue> {
   options: T[];
   value?: T['value'];
-  onChange?: (value: T['value']) => void;
+  onChange: (value: T['value']) => void;
 }
 
 /**
- *
+ * Creates the Props needed by Select to handle custom values and handles custom value creation
+ * and the initial value when it is not present in the option array.
  */
 export const useCreatableSelectPersistedBehaviour = <T extends SelectableValue>({
   options: initialOptions,
@@ -39,10 +40,10 @@ export const useCreatableSelectPersistedBehaviour = <T extends SelectableValue>(
   return {
     onCreateOption: (value) => {
       addOption(value);
-      onChange?.(value);
+      onChange(value);
     },
     onChange: (e) => {
-      onChange?.(e.value);
+      onChange(e.value);
     },
     allowCustomValue: true,
     options,
