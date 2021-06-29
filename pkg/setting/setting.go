a346a24375c8d67f2f88f4ba77e73a221f9601a9
Sofia Papagiannaki <sofia@grafana.com>
2021-06-29 14:36:43 +0300

    Fix failing test

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -1353,6 +1353,7 @@ func (cfg *Cfg) readAlertingSettings(iniFile *ini.File) error {
 	notificationTimeoutSeconds := alerting.Key("notification_timeout_seconds").MustInt64(30)
 	AlertingNotificationTimeout = time.Second * time.Duration(notificationTimeoutSeconds)
 	AlertingMaxAttempts = alerting.Key("max_attempts").MustInt(3)
+	cfg.AlertingMaxAttempts = AlertingMaxAttempts
 	AlertingMinInterval = alerting.Key("min_interval_seconds").MustInt64(int64(1))
 	if cfg.IsNgAlertEnabled() && AlertingMinInterval < 10 {
 		AlertingMinInterval = 10
