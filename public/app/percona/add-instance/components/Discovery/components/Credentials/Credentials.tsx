66c619344e8270867d48f97ae6850fc7ffee8746
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2021-07-01 16:22:11 +0300

    PMM-7690 iam roles (#147)
    Co-authored-by: Tiago Mota Santos <tiago.a.mota@hotmail.com>
    Co-authored-by: JiriCtvrtka <62988319+JiriCtvrtka@users.noreply.github.com>

--- public/app/percona/add-instance/components/Discovery/components/Credentials/Credentials.tsx
+++ public/app/percona/add-instance/components/Discovery/components/Credentials/Credentials.tsx
@@ -1,6 +1,6 @@
 import { Form as FormFinal } from 'react-final-form';
 import React, { FC, useCallback } from 'react';
-import { TextInputField, validators, PasswordInputField } from '@percona/platform-core';
+import { PasswordInputField, TextInputField } from '@percona/platform-core';
 import { Button, useTheme } from '@grafana/ui';
 import { getStyles } from './Credentials.styles';
 import { SECURITY_CREDENTIALS_DOC_LINK } from './Credentials.constants';
@@ -25,14 +25,12 @@ const Credentials: FC<CredentialsProps> = ({ onSetCredentials, selectInstance })
               name={Messages.form.fields.awsAccessKey.name}
               placeholder={Messages.form.fields.awsAccessKey.placeholder}
               label={Messages.form.fields.awsAccessKey.label}
-              validators={[validators.required]}
               fieldClassName={styles.credentialsField}
             />
             <PasswordInputField
               name={Messages.form.fields.awsSecretKey.name}
               placeholder={Messages.form.fields.awsSecretKey.placeholder}
               label={Messages.form.fields.awsSecretKey.label}
-              validators={[validators.required]}
               fieldClassName={styles.credentialsField}
             />
           </div>
