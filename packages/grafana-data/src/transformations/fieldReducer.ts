98053d2409b28b1c326a96ac9e74d265f6d85023
Torkel Ödegaard <torkel@grafana.org>
2021-07-01 09:43:40 +0200

    Legend: Updates display name for Last (not null) to just Last* (#35633)
    * Legend: Updates display name for Last (not null) to just Last*
    
    * added description
    
    * Removed the asterix
    
    * Added back * suffix

--- packages/grafana-data/src/transformations/fieldReducer.ts
+++ packages/grafana-data/src/transformations/fieldReducer.ts
@@ -129,7 +129,7 @@ export function reduceField(options: ReduceFieldOptions): FieldCalcs {
 export const fieldReducers = new Registry<FieldReducerInfo>(() => [
   {
     id: ReducerID.lastNotNull,
-    name: 'Last (not null)',
+    name: 'Last *',
     description: 'Last non-null value',
     standard: true,
     aliasIds: ['current'],
@@ -138,14 +138,14 @@ export const fieldReducers = new Registry<FieldReducerInfo>(() => [
   {
     id: ReducerID.last,
     name: 'Last',
-    description: 'Last Value',
+    description: 'Last value',
     standard: true,
     reduce: calculateLast,
   },
   { id: ReducerID.first, name: 'First', description: 'First Value', standard: true, reduce: calculateFirst },
   {
     id: ReducerID.firstNotNull,
-    name: 'First (not null)',
+    name: 'First',
     description: 'First non-null value',
     standard: true,
     reduce: calculateFirstNotNull,
