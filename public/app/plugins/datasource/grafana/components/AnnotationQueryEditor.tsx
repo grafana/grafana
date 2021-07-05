cf760873bc6109e29a97437b8e7866b98e568dd0
Torkel Ödegaard <torkel@grafana.com>
2021-07-05 18:04:10 +0200

    Change to new form styles

--- public/app/plugins/datasource/grafana/components/AnnotationQueryEditor.tsx
+++ public/app/plugins/datasource/grafana/components/AnnotationQueryEditor.tsx
@@ -1,42 +1,30 @@
 import React from 'react';
 import { SelectableValue } from '@grafana/data';
-import { FieldSet, InlineField, InlineFieldRow, InlineSwitch, Select } from '@grafana/ui';
+import { Field, FieldSet, Select, Switch } from '@grafana/ui';
 import { css } from '@emotion/css';
 
 import { TagFilter } from 'app/core/components/TagFilter/TagFilter';
 import { GrafanaAnnotationQuery, GrafanaAnnotationType, GrafanaQuery } from '../types';
 import { getAnnotationTags } from 'app/features/annotations/api';
 
-const filterTooltipContent = (
-  <ul>
-    <li>
-      Dashboard: This will fetch annotation and alert state changes for whole dashboard and show them only on the event
-      {"'"}s originating panel.
-    </li>
-    <li>Tags: This will fetch any annotation events that match the tags filter.</li>
-  </ul>
-);
-
-const matchTooltipContent =
-  'By default Grafana only shows annotations that match all tags in the query. Enabling this returns annotations that match any of the tags in the query.';
+const matchTooltipContent = 'Enabling this returns annotations that match any of the tags specified below';
 
 const tagsTooltipContent = (
-  <div>
-    A tag entered here as {"'"}foo{"'"} will match
-    <ul>
-      <li>
-        annotation tags {"'"}foo{"'"}
-      </li>
-      <li>
-        annotation key-value tags formatted as {"'"}foo:bar{"'"}
-      </li>
-    </ul>
-  </div>
+  <div>Specify a list of tags to match. To specify a key and value tag use `key:value` syntax.</div>
 );
 
 const annotationTypes = [
-  { label: 'Dashboard', value: GrafanaAnnotationType.Dashboard },
-  { label: 'Tags', value: GrafanaAnnotationType.Tags },
+  {
+    label: 'Dashboard',
+    value: GrafanaAnnotationType.Dashboard,
+    description:
+      'This will fetch annotation and alert state changes for whole dashboard and show them only on the events originating panel',
+  },
+  {
+    label: 'Tags',
+    value: GrafanaAnnotationType.Tags,
+    description: 'This will fetch any annotation events that match the tags filter',
+  },
 ];
 
 const limitOptions = [10, 50, 100, 200, 300, 500, 1000, 2000].map((limit) => ({
@@ -79,46 +67,27 @@ export default function AnnotationQueryEditor({ query, onChange }: Props) {
     });
 
   return (
-    <FieldSet>
-      <InlineFieldRow>
-        <InlineField label="Filter by" labelWidth={18} tooltip={filterTooltipContent}>
-          <Select
-            inputId="grafana-annotations__filter-by"
-            width={16}
-            options={annotationTypes}
-            value={type}
-            onChange={onFilterByChange}
-          />
-        </InlineField>
-        <InlineField label="Max limit">
-          <Select
-            inputId="grafana-annotations__limit"
-            width={16}
-            options={limitOptions}
-            value={limit}
-            onChange={onMaxLimitChange}
-          />
-        </InlineField>
-      </InlineFieldRow>
+    <FieldSet className={styles.container}>
+      <Field label="Filter by">
+        <Select options={annotationTypes} value={type} onChange={onFilterByChange} />
+      </Field>
+      <Field label="Max limit">
+        <Select width={16} options={limitOptions} value={limit} onChange={onMaxLimitChange} />
+      </Field>
       {type === GrafanaAnnotationType.Tags && tags && (
-        <InlineFieldRow>
-          <InlineField label="Match any" labelWidth={18} tooltip={matchTooltipContent}>
-            <InlineSwitch id="grafana-annotations__match-any" value={matchAny} onChange={onMatchAnyChange} />
-          </InlineField>
-          <InlineField
-            className={styles.tagFilterContainer}
-            label="Tags"
-            labelWidth="auto"
-            tooltip={tagsTooltipContent}
-          >
+        <>
+          <Field label="Match any" description={matchTooltipContent}>
+            <Switch id="grafana-annotations__match-any" value={matchAny} onChange={onMatchAnyChange} />
+          </Field>
+          <Field label="Tags" description={tagsTooltipContent}>
             <TagFilter
               inputId="grafana-annotations__tags"
               onChange={onTagsChange}
               tagOptions={getAnnotationTags}
               tags={tags}
             />
-          </InlineField>
-        </InlineFieldRow>
+          </Field>
+        </>
       )}
     </FieldSet>
   );
@@ -126,8 +95,8 @@ export default function AnnotationQueryEditor({ query, onChange }: Props) {
 
 const getStyles = () => {
   return {
-    tagFilterContainer: css`
-      max-width: 100%;
+    container: css`
+      max-width: 600px;
     `,
   };
 };
