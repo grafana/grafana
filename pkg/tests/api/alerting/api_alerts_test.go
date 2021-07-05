b041d3b715797f933282019496776a0c5b9de758
Arve Knudsen <arve.knudsen@gmail.com>
2021-07-05 15:46:06 +0200

    Add PauseAlert tests
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/tests/api/alerting/api_alerts_test.go
+++ pkg/tests/api/alerting/api_alerts_test.go
@@ -30,7 +30,7 @@ type httpClient struct {
 }
 
 func (c *httpClient) Do(req *http.Request) (*http.Response, error) {
-	c.t.Logf("Performing request: %q", req.URL)
+	c.t.Logf("Performing request, URL: %q, method: %s", req.URL, req.Method)
 	return c.Client.Do(req)
 }
 
@@ -49,10 +49,14 @@ func TestGetAlert(t *testing.T) {
 			func(stack *middleware.Stack) error {
 				if err := stack.Build.Add(&buildRequest{
 					addr: addr,
+					op:   getAlertOp,
 				}, middleware.After); err != nil {
 					return err
 				}
-				return stack.Deserialize.Add(&deserializeResponse{t: t}, middleware.After)
+				return stack.Deserialize.Add(&deserializeResponse{
+					t:  t,
+					op: getAlertOp,
+				}, middleware.After)
 			},
 		},
 	})
@@ -63,7 +67,7 @@ func TestGetAlert(t *testing.T) {
 			Id: &alertID,
 		})
 		require.EqualError(t, err,
-			"operation error grafana#Grafana: GetAlert, HTTP request failed with status code 404: Alert not found")
+			"operation error grafana#Grafana: PauseAlert, HTTP request failed with status code 404: Alert not found")
 	})
 
 	t.Run("Existing alert", func(t *testing.T) {
@@ -107,8 +111,80 @@ func TestGetAlert(t *testing.T) {
 	})
 }
 
+func TestPauseAlert(t *testing.T) {
+	grafDir, cfgPath := testinfra.CreateGrafDir(t)
+	sqlStore := setUpDatabase(t, grafDir)
+	addr := testinfra.StartGrafana(t, grafDir, cfgPath, sqlStore)
+	t.Logf("Server running at %s", addr)
+
+	ctx := context.Background()
+
+	client := grafana.New(grafana.Options{
+		HTTPClient: &httpClient{
+			t: t,
+		},
+		APIOptions: []func(*middleware.Stack) error{
+			func(stack *middleware.Stack) error {
+				if err := stack.Build.Add(&buildRequest{
+					addr: addr,
+					op:   pauseAlertOp,
+				}, middleware.After); err != nil {
+					return err
+				}
+				return stack.Deserialize.Add(&deserializeResponse{
+					t:  t,
+					op: pauseAlertOp,
+				}, middleware.After)
+			},
+		},
+	})
+
+	t.Run("Without permission", func(t *testing.T) {
+		t.Run("Non-existent alert", func(t *testing.T) {
+			alertID := "1234"
+			_, err := client.PauseAlert(ctx, &grafana.PauseAlertInput{
+				Id:     &alertID,
+				Paused: true,
+			})
+			require.EqualError(t, err,
+				"operation error grafana#Grafana: PauseAlert, HTTP request failed with status code 403: Permission denied")
+		})
+
+		t.Run("Existing alert", func(t *testing.T) {
+			now := time.Now().UTC()
+			alert := models.Alert{
+				Updated:      now,
+				Created:      now,
+				State:        models.AlertStateUnknown,
+				NewStateDate: now,
+				// Create with the same org ID as we'll be querying with
+				OrgId: 2,
+			}
+			sqlStore.WithTransactionalDbSession(ctx, func(sess *sqlstore.DBSession) error {
+				_, err := sess.Insert(&alert)
+				return err
+			})
+			alertID := strconv.FormatInt(alert.Id, 10)
+			_, err := client.PauseAlert(ctx, &grafana.PauseAlertInput{
+				Id:     &alertID,
+				Paused: true,
+			})
+			require.EqualError(t, err,
+				"operation error grafana#Grafana: PauseAlert, HTTP request failed with status code 403: Permission denied")
+		})
+	})
+}
+
+type apiOp int
+
+const (
+	getAlertOp apiOp = iota
+	pauseAlertOp
+)
+
 type buildRequest struct {
 	addr string
+	op   apiOp
 }
 
 func (br *buildRequest) ID() string {
@@ -118,16 +194,29 @@ func (br *buildRequest) ID() string {
 func (br *buildRequest) HandleBuild(ctx context.Context, in middleware.BuildInput,
 	next middleware.BuildHandler) (middleware.BuildOutput, middleware.Metadata, error) {
 	req := in.Request.(*smithyhttp.Request)
+	var path string
+	method := "GET"
+	switch br.op {
+	case getAlertOp:
+		path = "/alerts/1"
+	case pauseAlertOp:
+		path = "/alerts/1/pause"
+		method = "POST"
+	default:
+		panic(fmt.Sprintf("Unrecognized op %d", br.op))
+	}
 	var err error
-	req.URL, err = url.Parse(fmt.Sprintf("http://%s/api/alerts/1", br.addr))
+	req.URL, err = url.Parse(fmt.Sprintf("http://%s/api%s", br.addr, path))
 	if err != nil {
 		return middleware.BuildOutput{}, middleware.Metadata{}, err
 	}
+	req.Method = method
 	return next.HandleBuild(ctx, in)
 }
 
 type deserializeResponse struct {
-	t *testing.T
+	t  *testing.T
+	op apiOp
 }
 
 func (dr *deserializeResponse) ID() string {
