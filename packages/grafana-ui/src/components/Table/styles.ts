dd99761535dd08d1dfa21c366a231f1de4f80c7a
Tharun <rajendrantharun@live.com>
2021-07-07 02:08:41 +0530

    inherit color from parent to anchor tag

--- packages/grafana-ui/src/components/Table/styles.ts
+++ packages/grafana-ui/src/components/Table/styles.ts
@@ -44,7 +44,7 @@ export const getTableStyles = (theme: GrafanaTheme2) => {
         }
       }
       a {
-        ${color ? `color: ${color};` : ''};
+        color: inherit;
       }
     `;
   };
