2289d7e9ee34d16bf856819aa0b3c3277f967656
Alex Shpak <alex-shpak@users.noreply.github.com>
2021-07-06 14:09:19 +0200

    Respect hour12 settings when formatting local date (#33679)
    * Respect hour12 settings when formatting local date

--- packages/grafana-data/src/datetime/formats.test.ts
+++ packages/grafana-data/src/datetime/formats.test.ts
@@ -14,6 +14,25 @@ describe('Date Formats', () => {
       ''
     );
 
-    expect(format).toBe('MM/DD/YYYY, HH:mm:ss A');
+    expect(format).toBe('MM/DD/YYYY, hh:mm:ss A');
+  });
+});
+
+describe('Date Formats without hour12', () => {
+  it('localTimeFormat', () => {
+    const format = localTimeFormat(
+      {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      },
+      ''
+    );
+
+    expect(format).toBe('MM/DD/YYYY, HH:mm:ss');
   });
 });
