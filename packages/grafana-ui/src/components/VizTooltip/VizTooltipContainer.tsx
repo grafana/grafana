49cdc8c703e2a132bf25d4811760e02495267236
Ashley Harrison <ashley.harrison@grafana.com>
2021-07-06 16:22:47 +0100

    VizTooltip: Use react-popper, extract positioning calculation into util function + add unit tests

--- packages/grafana-ui/src/components/VizTooltip/VizTooltipContainer.tsx
+++ packages/grafana-ui/src/components/VizTooltip/VizTooltipContainer.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useLayoutEffect, useRef, HTMLAttributes, useMemo } from 'react';
+import React, { useState, useLayoutEffect, HTMLAttributes, useMemo, useRef } from 'react';
 import { css, cx } from '@emotion/css';
 import { useStyles2 } from '../../themes';
 import { getTooltipContainerStyles } from '../../themes/mixins';
 import useWindowSize from 'react-use/lib/useWindowSize';
 import { Dimensions2D, GrafanaTheme2 } from '@grafana/data';
+import { usePopper } from 'react-popper';
+import { calculateTooltipPosition } from './utils';
 
 /**
  * @public
@@ -24,13 +26,22 @@ export const VizTooltipContainer: React.FC<VizTooltipContainerProps> = ({
   className,
   ...otherProps
 }) => {
-  const tooltipRef = useRef<HTMLDivElement>(null);
+  const popperRef = useRef<HTMLDivElement>(null);
   const [tooltipMeasurement, setTooltipMeasurement] = useState<Dimensions2D>({ width: 0, height: 0 });
-  const { width, height } = useWindowSize();
-  const [placement, setPlacement] = useState({
-    x: positionX + offsetX,
-    y: positionY + offsetY,
+  const [virtualElement, setVirtualElement] = useState({
+    getBoundingClientRect() {
+      return {
+        top: positionY,
+        left: positionX,
+        bottom: positionY,
+        right: positionX,
+        width: 0,
+        height: 0,
+      };
+    },
   });
+  const { width, height } = useWindowSize();
+  const { styles: popperStyles, attributes } = usePopper(virtualElement, popperRef.current);
 
   const resizeObserver = useMemo(
     () =>
@@ -53,66 +64,64 @@ export const VizTooltipContainer: React.FC<VizTooltipContainerProps> = ({
   );
 
   useLayoutEffect(() => {
-    if (tooltipRef.current) {
-      resizeObserver.observe(tooltipRef.current);
+    if (popperRef.current) {
+      resizeObserver.observe(popperRef.current);
     }
 
     return () => {
       resizeObserver.disconnect();
     };
-  }, [resizeObserver]);
+  }, [resizeObserver, popperRef]);
 
   // Make sure tooltip does not overflow window
   useLayoutEffect(() => {
-    let x = 0;
-    let y = 0;
-
-    if (tooltipRef && tooltipRef.current) {
-      const overflowRight = Math.max(positionX + offsetX + tooltipMeasurement.width - width, 0);
-      const overflowLeft = Math.abs(Math.min(positionX - offsetX - tooltipMeasurement.width, 0));
-      const wouldOverflowRight = overflowRight > 0;
-      const wouldOverflowLeft = overflowLeft > 0;
-
-      const overflowBelow = Math.max(positionY + offsetY + tooltipMeasurement.height - height, 0);
-      const overflowAbove = Math.abs(Math.min(positionY - offsetY - tooltipMeasurement.height, 0));
-      const wouldOverflowBelow = overflowBelow > 0;
-      const wouldOverflowAbove = overflowAbove > 0;
-
-      if (wouldOverflowRight && wouldOverflowLeft) {
-        x = overflowRight > overflowLeft ? offsetX : width - offsetX - tooltipMeasurement.width;
-      } else if (wouldOverflowRight) {
-        x = positionX - offsetX - tooltipMeasurement.width;
-      } else {
-        x = positionX + offsetX;
-      }
-
-      if (wouldOverflowBelow && wouldOverflowAbove) {
-        y = overflowBelow > overflowAbove ? offsetY : height - offsetY - tooltipMeasurement.height;
-      } else if (wouldOverflowBelow) {
-        y = positionY - offsetY - tooltipMeasurement.height;
-      } else {
-        y = positionY + offsetY;
-      }
+    if (popperRef.current) {
+      const { x, y } = calculateTooltipPosition(
+        positionX,
+        positionY,
+        tooltipMeasurement.width,
+        tooltipMeasurement.height,
+        offsetX,
+        offsetY,
+        width,
+        height
+      );
+      setVirtualElement({
+        getBoundingClientRect() {
+          return {
+            top: y,
+            left: x,
+            bottom: y,
+            right: x,
+            width: 0,
+            height: 0,
+          };
+        },
+      });
     }
-
-    setPlacement({
-      x,
-      y,
-    });
-  }, [width, height, positionX, offsetX, positionY, offsetY, tooltipMeasurement.width, tooltipMeasurement.height]);
+  }, [
+    width,
+    height,
+    positionX,
+    offsetX,
+    positionY,
+    offsetY,
+    tooltipMeasurement.width,
+    tooltipMeasurement.height,
+    popperRef,
+  ]);
 
   const styles = useStyles2(getStyles);
 
   return (
     <div
-      ref={tooltipRef}
+      ref={popperRef}
       style={{
-        position: 'fixed',
-        left: 0,
-        top: 0,
-        transform: `translate3d(${placement.x}px, ${placement.y}px, 0)`,
         transition: 'all ease-out 0.1s',
+        ...popperStyles.popper,
+        display: popperStyles.popper?.transform ? 'block' : 'none',
       }}
+      {...attributes.popper}
       {...otherProps}
       className={cx(styles.wrapper, className)}
     >
