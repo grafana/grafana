f3250c7a1fa124eb1cb49a19ec7f820956cb7ac5
Dominik Prokop <dominik.prokop@grafana.com>
2021-07-07 17:19:08 +0200

    Use popper only for VizTooltip positioning

--- packages/grafana-ui/src/components/VizTooltip/VizTooltipContainer.tsx
+++ packages/grafana-ui/src/components/VizTooltip/VizTooltipContainer.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useLayoutEffect, HTMLAttributes, useMemo, useRef } from 'react';
+import React, { useState, HTMLAttributes, useMemo } from 'react';
 import { css, cx } from '@emotion/css';
 import { useStyles2 } from '../../themes';
 import { getTooltipContainerStyles } from '../../themes/mixins';
-import useWindowSize from 'react-use/lib/useWindowSize';
-import { Dimensions2D, GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme2 } from '@grafana/data';
 import { usePopper } from 'react-popper';
-import { calculateTooltipPosition } from './utils';
 
 /**
  * @public
@@ -26,100 +24,44 @@ export const VizTooltipContainer: React.FC<VizTooltipContainerProps> = ({
   className,
   ...otherProps
 }) => {
-  const tooltipRef = useRef<HTMLDivElement>(null);
-  const [tooltipMeasurement, setTooltipMeasurement] = useState<Dimensions2D>({ width: 0, height: 0 });
-  const [virtualElement, setVirtualElement] = useState({
-    getBoundingClientRect() {
-      return {
-        top: positionY,
-        left: positionX,
-        bottom: positionY,
-        right: positionX,
-        width: 0,
-        height: 0,
-      };
-    },
-  });
-  const { width, height } = useWindowSize();
-  const { styles: popperStyles, attributes } = usePopper(virtualElement, tooltipRef.current);
-
-  const resizeObserver = useMemo(
-    () =>
-      // TS has hard time playing games with @types/resize-observer-browser, hence the ignore
-      // @ts-ignore
-      new ResizeObserver((entries) => {
-        for (let entry of entries) {
-          const tW = Math.floor(entry.contentRect.width + 2 * 8); //  adding padding until Safari supports borderBoxSize
-          const tH = Math.floor(entry.contentRect.height + 2 * 8);
-
-          if (tooltipMeasurement.width !== tW || tooltipMeasurement.height !== tH) {
-            setTooltipMeasurement({
-              width: tW,
-              height: tH,
-            });
-          }
-        }
-      }),
-    [tooltipMeasurement.height, tooltipMeasurement.width]
+  const [tooltipRef, setTooltipRef] = useState<HTMLDivElement | null>(null);
+  const virtualElement = useMemo(
+    () => ({
+      getBoundingClientRect() {
+        return { top: positionY, left: positionX, bottom: positionY, right: positionX, width: 0, height: 0 };
+      },
+    }),
+    [positionY, positionX]
   );
-
-  useLayoutEffect(() => {
-    if (tooltipRef.current) {
-      resizeObserver.observe(tooltipRef.current);
-    }
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [resizeObserver, tooltipRef]);
-
-  // Make sure tooltip does not overflow window
-  useLayoutEffect(() => {
-    if (tooltipRef.current) {
-      const { x, y } = calculateTooltipPosition(
-        positionX,
-        positionY,
-        tooltipMeasurement.width,
-        tooltipMeasurement.height,
-        offsetX,
-        offsetY,
-        width,
-        height
-      );
-      setVirtualElement({
-        getBoundingClientRect() {
-          return {
-            top: y,
-            left: x,
-            bottom: y,
-            right: x,
-            width: 0,
-            height: 0,
-          };
+  const { styles: popperStyles, attributes } = usePopper(virtualElement, tooltipRef, {
+    placement: 'bottom-start',
+    modifiers: [
+      { name: 'arrow', enabled: false },
+      {
+        name: 'preventOverflow',
+        enabled: true,
+        options: {
+          rootBoundary: 'viewport',
         },
-      });
-    }
-  }, [
-    width,
-    height,
-    positionX,
-    offsetX,
-    positionY,
-    offsetY,
-    tooltipMeasurement.width,
-    tooltipMeasurement.height,
-    tooltipRef,
-  ]);
+      },
+      {
+        name: 'offset',
+        options: {
+          offset: [offsetX, offsetY],
+        },
+      },
+    ],
+  });
 
   const styles = useStyles2(getStyles);
 
   return (
     <div
-      ref={tooltipRef}
+      ref={setTooltipRef}
       style={{
-        transition: 'all ease-out 0.1s',
         ...popperStyles.popper,
         display: popperStyles.popper?.transform ? 'block' : 'none',
+        transition: 'all ease-out 0.5s',
       }}
       {...attributes.popper}
       {...otherProps}
