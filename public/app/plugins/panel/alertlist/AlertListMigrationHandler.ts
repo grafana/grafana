ee49b7e257125e6962633b50f7a182ddffbd923b
Domas <domasx2@gmail.com>
2021-07-08 11:40:54 +0300

    add defaults to alert list migration handler

--- public/app/plugins/panel/alertlist/AlertListMigrationHandler.ts
+++ public/app/plugins/panel/alertlist/AlertListMigrationHandler.ts
@@ -1,21 +1,22 @@
 import { PanelModel } from '@grafana/data';
-import { AlertListOptions } from './types';
+import { AlertListOptions, ShowOption, SortOrder } from './types';
 
 export const alertListPanelMigrationHandler = (
   panel: PanelModel<AlertListOptions> & Record<string, any>
 ): Partial<AlertListOptions> => {
   const newOptions: AlertListOptions = {
-    showOptions: panel.options.showOptions ?? panel.show,
-    maxItems: panel.options.maxItems ?? panel.limit,
-    sortOrder: panel.options.sortOrder ?? panel.sortOrder,
-    dashboardAlerts: panel.options.dashboardAlerts ?? panel.onlyAlertsOnDashboard,
-    alertName: panel.options.alertName ?? panel.nameFilter,
-    dashboardTitle: panel.options.dashboardTitle ?? panel.dashboardFilter,
+    showOptions: panel.options.showOptions ?? panel.show ?? ShowOption.Current,
+    maxItems: panel.options.maxItems ?? panel.limit ?? 10,
+    sortOrder: panel.options.sortOrder ?? panel.sortOrder ?? SortOrder.AlphaAsc,
+    dashboardAlerts: panel.options.dashboardAlerts ?? panel.onlyAlertsOnDashboard ?? false,
+    alertName: panel.options.alertName ?? panel.nameFilter ?? '',
+    dashboardTitle: panel.options.dashboardTitle ?? panel.dashboardFilter ?? '',
     folderId: panel.options.folderId ?? panel.folderId,
-    tags: panel.options.tags ?? panel.dashboardTags,
+    tags: panel.options.tags ?? panel.dashboardTags ?? [],
     stateFilter:
       panel.options.stateFilter ??
-      panel.stateFilter.reduce((filterObj: any, curFilter: any) => ({ ...filterObj, [curFilter]: true }), {}),
+      panel.stateFilter?.reduce((filterObj: any, curFilter: any) => ({ ...filterObj, [curFilter]: true }), {}) ??
+      {},
   };
 
   const previousVersion = parseFloat(panel.pluginVersion || '7.4');
