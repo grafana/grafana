35a40bf153e366e9ed19a0908f931f9ca8a71e0c
Ivana <ivana.huckova@gmail.com>
2021-07-09 12:04:16 +0200

    Fix imports in tests

--- public/app/features/explore/QueryRow.test.tsx
+++ public/app/features/explore/QueryRow.test.tsx
@@ -21,6 +21,7 @@ const setup = (propOverrides?: object) => {
     absoluteRange: {} as AbsoluteTimeRange,
     removeQueryRowAction: jest.fn() as any,
     runQueries: jest.fn(),
+    clearCache: jest.fn(),
     queryResponse: {} as PanelData,
     latency: 1,
   };
