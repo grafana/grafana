3c74d5f56bd4ddb7648c265776538e9ac16d473c
Ashley Harrison <ashley.harrison@grafana.com>
2021-07-13 14:58:07 +0100

    Select: Fix remaining E2E tests

--- packages/grafana-e2e/src/flows/setDashboardTimeRange.ts
+++ packages/grafana-e2e/src/flows/setDashboardTimeRange.ts
@@ -1,7 +1,5 @@
-import { e2e } from '../index';
 import { setTimeRange, TimeRangeConfig } from './setTimeRange';
 
 export { TimeRangeConfig };
 
-export const setDashboardTimeRange = (config: TimeRangeConfig) =>
-  e2e.components.PageToolbar.container().within(() => setTimeRange(config));
+export const setDashboardTimeRange = (config: TimeRangeConfig) => setTimeRange(config);
