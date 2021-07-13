176d3505e7eba6247294a8b7364d41247f7148fb
Marcus Andersson <marcus.andersson@grafana.com>
2021-07-13 15:40:55 +0200

    adding transformer that will stretch a data frame from wide to long.

---
+++ packages/grafana-data/src/transformations/transformers/stretch.test.ts
@@ -0,0 +1,198 @@
+import { toDataFrame } from '../../dataframe/processDataFrame';
+import { mockTransformationsRegistry } from '../../utils/tests/mockTransformationsRegistry';
+import { transformDataFrame } from '../transformDataFrame';
+import { DataFrame, FieldType } from '../../types';
+import { DataTransformerID } from './ids';
+import { DataTransformerConfig } from '@grafana/data';
+import { stretchTransformer, StretchTransformerOptions } from './stretch';
+import { ArrayVector } from '../../vector';
+
+describe('Stretch transformer', () => {
+  beforeAll(() => {
+    mockTransformationsRegistry([stretchTransformer]);
+  });
+
+  it('should stretch wide to long', async () => {
+    const source = [
+      toDataFrame({
+        name: 'wide',
+        refId: 'A',
+        fields: [
+          { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          { name: 'count', type: FieldType.string, values: [1, 2, 3, 4, 5, 6] },
+        ],
+      }),
+    ];
+
+    const config: DataTransformerConfig<StretchTransformerOptions> = {
+      id: DataTransformerID.stretch,
+      options: {},
+    };
+
+    await expect(transformDataFrame([config], source)).toEmitValuesWith((received) => {
+      const [transformed] = received;
+
+      expect(transformed).toEqual([
+        toEquableDataFrame({
+          name: 'wide0',
+          refId: 'A0',
+          fields: [
+            { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+            { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          ],
+          length: 6,
+        }),
+        toEquableDataFrame({
+          name: 'wide1',
+          refId: 'A1',
+          fields: [
+            { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+            { name: 'count', type: FieldType.string, values: [1, 2, 3, 4, 5, 6] },
+          ],
+          length: 6,
+        }),
+      ]);
+    });
+  });
+
+  it('should stretch all wide to long when mixed', async () => {
+    const source = [
+      toDataFrame({
+        name: 'wide',
+        refId: 'A',
+        fields: [
+          { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          { name: 'count', type: FieldType.string, values: [1, 2, 3, 4, 5, 6] },
+        ],
+      }),
+      toDataFrame({
+        name: 'long',
+        refId: 'B',
+        fields: [
+          { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+        ],
+      }),
+    ];
+
+    const config: DataTransformerConfig<StretchTransformerOptions> = {
+      id: DataTransformerID.stretch,
+      options: {},
+    };
+
+    await expect(transformDataFrame([config], source)).toEmitValuesWith((received) => {
+      const [transformed] = received;
+
+      expect(transformed).toEqual([
+        toEquableDataFrame({
+          name: 'wide0',
+          refId: 'A0',
+          fields: [
+            { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+            { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          ],
+          length: 6,
+        }),
+        toEquableDataFrame({
+          name: 'wide1',
+          refId: 'A1',
+          fields: [
+            { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+            { name: 'count', type: FieldType.string, values: [1, 2, 3, 4, 5, 6] },
+          ],
+          length: 6,
+        }),
+        toEquableDataFrame({
+          name: 'long',
+          refId: 'B',
+          fields: [
+            { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+            { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          ],
+        }),
+      ]);
+    });
+  });
+
+  it('should stretch none when source only has long frames', async () => {
+    const source = [
+      toDataFrame({
+        name: 'long',
+        refId: 'A',
+        fields: [
+          { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+        ],
+      }),
+      toDataFrame({
+        name: 'long',
+        refId: 'B',
+        fields: [
+          { name: 'time', type: FieldType.time, values: [10, 9, 8, 7, 6, 5] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+        ],
+      }),
+    ];
+
+    const config: DataTransformerConfig<StretchTransformerOptions> = {
+      id: DataTransformerID.stretch,
+      options: {},
+    };
+
+    await expect(transformDataFrame([config], source)).toEmitValuesWith((received) => {
+      const [transformed] = received;
+      expect(transformed).toEqual(source);
+    });
+  });
+
+  it('should stretch none when source only has wide frames without time field', async () => {
+    const source = [
+      toDataFrame({
+        name: 'wide',
+        refId: 'A',
+        fields: [
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+        ],
+      }),
+      toDataFrame({
+        name: 'wide',
+        refId: 'B',
+        fields: [
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+          { name: 'text', type: FieldType.string, values: ['a', 'z', 'b', 'x', 'c', 'b'] },
+        ],
+      }),
+    ];
+
+    const config: DataTransformerConfig<StretchTransformerOptions> = {
+      id: DataTransformerID.stretch,
+      options: {},
+    };
+
+    await expect(transformDataFrame([config], source)).toEmitValuesWith((received) => {
+      const [transformed] = received;
+      expect(transformed).toEqual(source);
+    });
+  });
+});
+
+function toEquableDataFrame(source: any): DataFrame {
+  return toDataFrame({
+    ...source,
+    fields: source.fields.map((field: any) => {
+      return {
+        ...field,
+        values: new ArrayVector(field.values),
+        config: {},
+      };
+    }),
+    meta: {
+      transformations: [DataTransformerID.stretch],
+    },
+  });
+}
---
+++ packages/grafana-data/src/transformations/transformers/stretch.ts
@@ -0,0 +1,59 @@
+import { map } from 'rxjs/operators';
+import { DataTransformerID } from './ids';
+import { DataTransformerInfo } from '../../types/transformations';
+import { DataFrame, FieldType } from '../../types/dataFrame';
+
+export type StretchTransformerOptions = {};
+
+export const stretchTransformer: DataTransformerInfo<StretchTransformerOptions> = {
+  id: DataTransformerID.stretch,
+  name: 'Stretch',
+  description: 'Stretch a set of data frames from wide to long',
+  defaultOptions: {},
+
+  /**
+   * Return a modified copy of the series.  If the transform is not or should not
+   * be applied, just return the input series
+   */
+  operator: (options) => (source) =>
+    source.pipe(
+      map((data) => {
+        if (!Array.isArray(data) || data.length === 0) {
+          return data;
+        }
+
+        const result: DataFrame[] = [];
+
+        for (const frame of data) {
+          const timeField = frame.fields.find((field) => {
+            return field.type === FieldType.time;
+          });
+
+          if (!timeField || frame.fields.length === 2) {
+            result.push(frame);
+            continue;
+          }
+
+          for (const field of frame.fields) {
+            if (field === timeField) {
+              continue;
+            }
+
+            result.push({
+              name: withIndex(frame.name, result.length),
+              refId: withIndex(frame.refId, result.length),
+              fields: [timeField, field],
+              length: frame.length,
+              meta: frame.meta,
+            });
+          }
+        }
+
+        return result;
+      })
+    ),
+};
+
+function withIndex(identifier: string | undefined, length: number): string | undefined {
+  return identifier ? `${identifier}${length}` : undefined;
+}
