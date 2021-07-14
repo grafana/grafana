66cd5e63d4a78e653777ffd31c016cd29c6dfb31
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2021-07-14 07:44:23 -0400

    Loki: Add autocomplete option for $__range variable (#36717)
    * Add autocomplete option for Loki & Promtheus rates
    
    * Update test

--- public/app/plugins/datasource/prometheus/promql.ts
+++ public/app/plugins/datasource/prometheus/promql.ts
@@ -6,6 +6,7 @@ import { CompletionItem } from '@grafana/ui';
 export const RATE_RANGES: CompletionItem[] = [
   { label: '$__interval', sortValue: '$__interval' },
   { label: '$__rate_interval', sortValue: '$__rate_interval' },
+  { label: '$__range', sortValue: '$__range' },
   { label: '1m', sortValue: '00:01:00' },
   { label: '5m', sortValue: '00:05:00' },
   { label: '10m', sortValue: '00:10:00' },
