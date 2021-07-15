0ea88520d1a7b7cce9e9985b645e75fd81be22d8
sam boyer <sdboyer@grafana.com>
2021-07-15 18:22:47 -0400

    Remove test for validateResources

--- pkg/cmd/grafana-cli/commands/scuemata_validation_command_test.go
+++ pkg/cmd/grafana-cli/commands/scuemata_validation_command_test.go
@@ -1,13 +1,10 @@
 package commands
 
 import (
-	"io/fs"
 	"os"
-	"path/filepath"
 	"testing"
 	"testing/fstest"
 
-	"github.com/grafana/grafana/pkg/schema"
 	"github.com/grafana/grafana/pkg/schema/load"
 	"github.com/laher/mergefs"
 	"github.com/stretchr/testify/assert"
@@ -67,58 +64,5 @@ func TestValidateScuemataBasics(t *testing.T) {
 
 		err = validateScuemata(baseLoadPaths, load.DistDashboardFamily)
 		assert.EqualError(t, err, "all schema should be valid with respect to basic CUE rules, Family.lineages.0.0: field #Panel not allowed")
-	})
-
-	t.Run("Testing validateResources against scuemata and resource inputs", func(t *testing.T) {
-		validPanel, err := os.ReadFile("testdata/panels/valid_resource_panel.json")
-		require.NoError(t, err)
-
-		invalidPanel, err := os.ReadFile("testdata/panels/invalid_resource_panel.json")
-		require.NoError(t, err)
-
-		filesystem := fstest.MapFS{
-			"valid.json":   &fstest.MapFile{Data: validPanel},
-			"invalid.json": &fstest.MapFile{Data: invalidPanel},
-		}
-		mergedFS := mergefs.Merge(filesystem, defaultBaseLoadPaths.BaseCueFS)
-
-		var baseLoadPaths = load.BaseLoadPaths{
-			BaseCueFS:       mergedFS,
-			DistPluginCueFS: defaultBaseLoadPaths.DistPluginCueFS,
-		}
-
-		require.NoError(t, fs.WalkDir(mergedFS, ".", func(path string, d fs.DirEntry, err error) error {
-			require.NoError(t, err)
-
-			if d.IsDir() || filepath.Ext(d.Name()) != ".json" {
-				return nil
-			}
-
-			if d.Name() == "valid.json" {
-				t.Run(path, func(t *testing.T) {
-					b, err := mergedFS.Open(path)
-					require.NoError(t, err, "failed to open dashboard file")
-					res := schema.Resource{Value: b, Name: path}
-
-					err = validateResources(res, baseLoadPaths, load.BaseDashboardFamily)
-					require.NoError(t, err, "error while loading base dashboard scuemata")
-
-					err = validateResources(res, baseLoadPaths, load.DistDashboardFamily)
-					require.NoError(t, err, "error while loading dist dashboard scuemata")
-				})
-			}
-			if d.Name() == "invalid.json" {
-				t.Run(path, func(t *testing.T) {
-					b, err := mergedFS.Open(path)
-					require.NoError(t, err, "failed to open dashboard file")
-					res := schema.Resource{Value: b, Name: path}
-
-					err = validateResources(res, baseLoadPaths, load.BaseDashboardFamily)
-					assert.EqualError(t, err, "failed validation: Family.lineages.0.0.panels.0.type: incomplete value !=\"\"")
-				})
-			}
-
-			return nil
-		}))
 	})
 }
