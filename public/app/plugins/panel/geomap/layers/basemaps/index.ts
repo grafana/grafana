13ae065352568e613d2b0ef19e782df5fa78296b
eunice98k <kimeuni@amazon.com>
2021-07-16 10:15:29 -0700

    Add provision base layer

--- public/app/plugins/panel/geomap/layers/basemaps/index.ts
+++ public/app/plugins/panel/geomap/layers/basemaps/index.ts
@@ -2,21 +2,32 @@ import { cartoLayers, carto } from './carto';
 import { esriLayers } from './esri';
 import { genericLayers } from './generic';
 import { osmLayers } from './osm';
+import { provision } from './provision';
+
+const settings = (window as any).grafanaBootData.settings;
 
 // For now just use carto
-export const defaultGrafanaThemedMap = {
-  ...carto,
-  id: 'default',
-  name: 'Default base layer',
-};
+export const defaultGrafanaThemedMap = settings.tileServerURL
+  ? {
+      ...provision,
+      id: 'default',
+      name: 'Default base layer',
+    }
+  : {
+      ...carto,
+      id: 'default',
+      name: 'Default base layer',
+    };
 
 /**
  * Registry for layer handlers
  */
-export const basemapLayers = [
-  defaultGrafanaThemedMap,
-  ...osmLayers,
-  ...cartoLayers,
-  ...esriLayers, // keep formatting
-  ...genericLayers,
-];
+export const basemapLayers = settings.BaseLayerDisabled
+  ? [defaultGrafanaThemedMap]
+  : [
+      defaultGrafanaThemedMap,
+      ...osmLayers,
+      ...cartoLayers,
+      ...esriLayers, // keep formatting
+      ...genericLayers,
+    ];
---
+++ public/app/plugins/panel/geomap/layers/basemaps/provision.ts
@@ -0,0 +1,36 @@
+import { MapLayerRegistryItem, MapLayerOptions, GrafanaTheme2 } from '@grafana/data';
+import Map from 'ol/Map';
+import XYZ from 'ol/source/XYZ';
+import TileLayer from 'ol/layer/Tile';
+
+export interface XYZProvisioned {
+  url: string;
+  attribution: string;
+}
+
+const settings = (window as any).grafanaBootData.settings;
+const url = settings.tileServerURL;
+const attribution = settings.tileServerAttribtuion;
+export const defaultXYZProvisioned: XYZProvisioned = {
+  url: url,
+  attribution: `Tiles © <a href="${url}">${attribution}</a>`,
+};
+
+export const provision: MapLayerRegistryItem<XYZProvisioned> = {
+  id: 'config',
+  name: 'Configured Tile layer',
+  isBaseMap: true,
+
+  create: (map: Map, options: MapLayerOptions<XYZProvisioned>, theme: GrafanaTheme2) => ({
+    init: () => {
+      return new TileLayer({
+        source: new XYZ({
+          url: defaultXYZProvisioned.url,
+          attributions: defaultXYZProvisioned.attribution,
+        }),
+      });
+    },
+  }),
+};
+
+export const provisionLayer = [provision];
