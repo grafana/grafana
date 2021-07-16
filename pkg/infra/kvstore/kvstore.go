398f83ca3eb44eca3a3b3047f4a7a8691e26261f
Dan Cech <dcech@grafana.com>
2021-07-16 17:58:54 -0400

    add key/value store service

---
+++ pkg/infra/kvstore/kvstore.go
@@ -0,0 +1,46 @@
+package kvstore
+
+import (
+	"context"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
+)
+
+func ProvideService(sqlStore *sqlstore.SQLStore) KVStore {
+	s := &KVStoreSQL{
+		sqlStore: sqlStore,
+		log:      log.New("infra.kvstore.sql"),
+	}
+
+	return s
+}
+
+// Interface for kvstore
+type KVStore interface {
+	Get(ctx context.Context, orgId int64, namespace string, key string) (string, error)
+	Set(ctx context.Context, orgId int64, namespace string, key string, value string) error
+}
+
+// Returns a kvstore wrapper with fixed orgId and namespace
+func WithNamespace(kv KVStore, orgId int64, namespace string) *NamespacedKVStore {
+	return &NamespacedKVStore{
+		kvStore:   kv,
+		orgId:     orgId,
+		namespace: namespace,
+	}
+}
+
+type NamespacedKVStore struct {
+	kvStore   KVStore
+	orgId     int64
+	namespace string
+}
+
+func (kv *NamespacedKVStore) Get(ctx context.Context, key string) (string, error) {
+	return kv.kvStore.Get(ctx, kv.orgId, kv.namespace, key)
+}
+
+func (kv *NamespacedKVStore) Set(ctx context.Context, key string, value string) error {
+	return kv.kvStore.Set(ctx, kv.orgId, kv.namespace, key, value)
+}
