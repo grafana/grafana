ec1176f851dd66f9ad44271b078e2c34ba2e0120
Jack Westbrook <jack.westbrook@gmail.com>
2021-07-16 15:13:07 +0200

    feat(catalog): add alert for plugin load error. move hook, content and styles out

--- public/app/features/plugins/admin/pages/PluginDetails.tsx
+++ public/app/features/plugins/admin/pages/PluginDetails.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/css';
-import { AppPlugin, GrafanaTheme2, PluginType, PluginIncludeType } from '@grafana/data';
-import { useStyles2, TabsBar, TabContent, Tab, Icon } from '@grafana/ui';
-import { useAsync } from 'react-use';
-import { contextSrv } from '../../../../core/services/context_srv';
-import { VersionList } from '../components/VersionList';
+import { GrafanaTheme2 } from '@grafana/data';
+import { useStyles2, TabsBar, TabContent, Tab, Icon, Alert } from '@grafana/ui';
+import { AppNotificationSeverity } from 'app/types';
 import { InstallControls } from '../components/InstallControls';
 import { usePlugin } from '../hooks/usePlugins';
 import { Page as PluginPage } from '../components/Page';
@@ -12,73 +10,21 @@ import { Loader } from '../components/Loader';
 import { Page } from 'app/core/components/Page/Page';
 import { PluginLogo } from '../components/PluginLogo';
 import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
-import { loadPlugin } from '../../PluginPage';
-import { AppConfigCtrlWrapper } from '../../wrappers/AppConfigWrapper';
-import { PluginDashboards } from '../../PluginDashboards';
+import { useLoadPluginAndNav } from '../hooks/useLoadPluginAndNav';
+import { PluginDetailsBody } from '../components/PluginDetailsBody';
 
 type PluginDetailsProps = GrafanaRouteComponentProps<{ pluginId?: string }>;
-
-const defaultTabs = [{ label: 'Overview' }, { label: 'Version history' }];
-
-const useLoadPlugin = (pluginId: string) => {
-  const { loading, value: plugin, error } = useAsync(async () => {
-    const plugin = await loadPlugin(pluginId);
-    return plugin;
-  }, [pluginId]);
-
-  const tabs = useMemo(() => {
-    const isAdmin = contextSrv.hasRole('Admin');
-    const tabs: Array<{ label: string }> = [...defaultTabs];
-
-    if (!plugin) {
-      return tabs;
-    }
-
-    if (isAdmin) {
-      if (plugin.meta.type === PluginType.app) {
-        if (plugin.angularConfigCtrl) {
-          tabs.push({
-            label: 'Config',
-          });
-        }
-
-        if (plugin.configPages) {
-          for (const page of plugin.configPages) {
-            tabs.push({
-              label: page.title,
-            });
-          }
-        }
-
-        if (plugin.meta.includes?.find((include) => include.type === PluginIncludeType.dashboard)) {
-          tabs.push({
-            label: 'Dashboards',
-          });
-        }
-      }
-    }
-
-    return tabs;
-  }, [plugin]);
-
-  return {
-    loading,
-    plugin,
-    tabs,
-    error,
-  };
-};
 
 export default function PluginDetails({ match }: PluginDetailsProps): JSX.Element | null {
   const { pluginId } = match.params;
   const [activeTab, setActiveTab] = useState(0);
   const { isLoading, local, remote, remoteVersions } = usePlugin(pluginId!);
-  const { loading, error, plugin, tabs } = useLoadPlugin(pluginId!);
+  const { loading, error, plugin, tabs } = useLoadPluginAndNav(pluginId!, Boolean(local));
   const tab = tabs[activeTab];
   const styles = useStyles2(getStyles);
 
   const description = remote?.description ?? local?.info?.description;
-  const readme = remote?.readme;
+  const readme = remote?.readme || 'No plugin help or readme markdown file was found';
   const version = local?.info?.version || remote?.version;
   const links = (local?.info?.links || remote?.json?.info?.links) ?? [];
   const downloads = remote?.downloads;
@@ -90,8 +36,6 @@ export default function PluginDetails({ match }: PluginDetailsProps): JSX.Elemen
       </Page>
     );
   }
-
-  // if error whadda we do????
 
   return (
     <Page>
@@ -136,100 +80,53 @@ export default function PluginDetails({ match }: PluginDetailsProps): JSX.Elemen
           ))}
         </TabsBar>
         <TabContent>
-          {tab?.label === 'Overview' && (
-            <div
-              className={styles.readme}
-              dangerouslySetInnerHTML={{ __html: readme ?? 'No plugin help or readme markdown file was found' }}
-            />
-          )}
-          {tab?.label === 'Version history' && <VersionList versions={remoteVersions ?? []} />}
-
-          {tab?.label === 'Dashboards' && plugin && (
-            <div
-              className={css`
-                padding: 40px;
-              `}
-            >
-              <PluginDashboards plugin={plugin.meta} />
-            </div>
+          {error && (
+            <Alert severity={AppNotificationSeverity.Error} title="Error Loading Plugin">
+              <>
+                Check the server startup logs for more information. <br />
+                If this plugin was loaded from git, make sure it was compiled.
+              </>
+            </Alert>
           )}
 
-          {tab?.label === 'Config' && plugin?.angularConfigCtrl && (
-            <div
-              className={css`
-                padding: 40px;
-              `}
-            >
-              <AppConfigCtrlWrapper app={plugin as AppPlugin} />
-            </div>
-          )}
+          <PluginDetailsBody tab={tab} plugin={plugin} remoteVersions={remoteVersions ?? []} readme={readme} />
         </TabContent>
       </PluginPage>
     </Page>
   );
 }
 
-export const getStyles = (theme: GrafanaTheme2) => {
-  return {
-    headerContainer: css`
-      display: flex;
-      margin-bottom: 24px;
-      margin-top: 24px;
-      min-height: 120px;
-    `,
-    headerWrapper: css`
-      margin-left: ${theme.spacing(3)};
-    `,
-    headerLinks: css`
-      display: flex;
-      align-items: center;
-      margin-top: ${theme.spacing()};
-      margin-bottom: ${theme.spacing(3)};
+export const getStyles = (theme: GrafanaTheme2) => ({
+  headerContainer: css`
+    display: flex;
+    margin-bottom: 24px;
+    margin-top: 24px;
+    min-height: 120px;
+  `,
+  headerWrapper: css`
+    margin-left: ${theme.spacing(3)};
+  `,
+  headerLinks: css`
+    display: flex;
+    align-items: center;
+    margin-top: ${theme.spacing()};
+    margin-bottom: ${theme.spacing(3)};
 
-      & > * {
-        &::after {
-          content: '|';
-          padding: 0 ${theme.spacing()};
-        }
+    & > * {
+      &::after {
+        content: '|';
+        padding: 0 ${theme.spacing()};
       }
-      & > *:last-child {
-        &::after {
-          content: '';
-          padding-right: 0;
-        }
+    }
+    & > *:last-child {
+      &::after {
+        content: '';
+        padding-right: 0;
       }
-      font-size: ${theme.typography.h4.fontSize};
-    `,
-    headerOrgName: css`
-      font-size: ${theme.typography.h4.fontSize};
-    `,
-    message: css`
-      color: ${theme.colors.text.secondary};
-    `,
-    readme: css`
-      padding: ${theme.spacing(3, 4)};
-
-      & img {
-        max-width: 100%;
-      }
-
-      h1,
-      h2,
-      h3 {
-        margin-top: ${theme.spacing(3)};
-        margin-bottom: ${theme.spacing(2)};
-      }
-
-      *:first-child {
-        margin-top: 0;
-      }
-
-      li {
-        margin-left: ${theme.spacing(2)};
-        & > p {
-          margin: ${theme.spacing()} 0;
-        }
-      }
-    `,
-  };
-};
+    }
+    font-size: ${theme.typography.h4.fontSize};
+  `,
+  headerOrgName: css`
+    font-size: ${theme.typography.h4.fontSize};
+  `,
+});
