61f18dc4b9a58e4b68c8eb51091ac1190e573bd9
Ryan McKinley <ryantxu@gmail.com>
2021-07-18 23:07:02 -0700

    make the geomap panel beta

--- public/app/plugins/panel/geomap/editor/DataLayersEditor.tsx
+++ public/app/plugins/panel/geomap/editor/DataLayersEditor.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from 'react';
-import { StandardEditorProps, MapLayerOptions } from '@grafana/data';
+import { StandardEditorProps, MapLayerOptions, PluginState, MapLayerRegistryItem } from '@grafana/data';
 import { GeomapPanelOptions } from '../types';
 import { LayerEditor } from './LayerEditor';
+import { hasAlphaPanels } from 'app/core/config';
+
+function dataLayerFilter(layer: MapLayerRegistryItem): boolean {
+  if (layer.isBaseMap) {
+    return false;
+  }
+  if (layer.state === PluginState.alpha) {
+    return hasAlphaPanels;
+  }
+  return true;
+}
 
 // For now this supports a *single* data layer -- eventually we should support more than one
 export const DataLayersEditor: FC<StandardEditorProps<MapLayerOptions[], any, GeomapPanelOptions>> = ({
@@ -17,7 +28,7 @@ export const DataLayersEditor: FC<StandardEditorProps<MapLayerOptions[], any, Ge
         console.log('Change overlays:', cfg);
         onChange([cfg]);
       }}
-      filter={(v) => !v.isBaseMap}
+      filter={dataLayerFilter}
     />
   );
 };
