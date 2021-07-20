fa1f2ddd614591ffa29feff7766e6cc531b28324
Ryan McKinley <ryantxu@gmail.com>
2021-07-20 08:57:32 -0700

    add update configuration options

--- public/app/plugins/panel/annolist/module.tsx
+++ public/app/plugins/panel/annolist/module.tsx
@@ -8,67 +8,73 @@ export const plugin = new PanelPlugin<AnnoOptions>(AnnoListPanel)
   .setPanelOptions((builder) => {
     builder
       .addBooleanSwitch({
-        path: 'showUser',
-        name: 'Show user',
-        description: '',
-        defaultValue: true,
-      })
-      .addBooleanSwitch({
-        path: 'showTime',
-        name: 'Show time',
-        description: '',
-        defaultValue: true,
-      })
-      .addBooleanSwitch({
-        path: 'showTags',
-        name: 'Show tags',
-        description: '',
-        defaultValue: true,
-      })
-      .addTextInput({
-        path: 'navigateBefore',
-        name: 'Before',
-        defaultValue: '10m',
-        description: '',
-      })
-      .addTextInput({
-        path: 'navigateAfter',
-        name: 'After',
-        defaultValue: '10m',
-        description: '',
-      })
-      .addBooleanSwitch({
-        path: 'navigateToPanel',
-        name: 'To panel',
-        description: '',
-        defaultValue: true,
-      })
-      .addBooleanSwitch({
+        category: ['Annotation query'],
         path: 'onlyFromThisDashboard',
-        name: 'Only this dashboard',
-        description: '',
+        name: 'This dashboard',
+        description: 'Only return annotations on this dashboard',
         defaultValue: false,
       })
       .addBooleanSwitch({
+        category: ['Annotation query'],
         path: 'onlyInTimeRange',
         name: 'Within Time Range',
-        description: '',
+        description: 'Show annotations that match the dashboard time range',
         defaultValue: false,
       })
       .addCustomEditor({
+        category: ['Annotation query'],
         id: 'tags',
         path: 'tags',
         name: 'Tags',
-        description: '',
+        description: 'Match annotation tags',
         editor(props) {
           return <TagsInput tags={props.value} onChange={props.onChange} />;
         },
       })
       .addNumberInput({
+        category: ['Annotation query'],
         path: 'limit',
         name: 'Limit',
-        description: '',
         defaultValue: 10,
+      })
+      .addBooleanSwitch({
+        category: ['Display'],
+        path: 'showUser',
+        name: 'Show user',
+        defaultValue: true,
+      })
+      .addBooleanSwitch({
+        category: ['Display'],
+        path: 'showTime',
+        name: 'Show time',
+        defaultValue: true,
+      })
+      .addBooleanSwitch({
+        category: ['Display'],
+        path: 'showTags',
+        name: 'Show tags',
+        defaultValue: true,
+      })
+      .addTextInput({
+        category: ['Link behavior'],
+        path: 'navigateBefore',
+        name: 'Time before',
+        defaultValue: '10m',
+        description: '',
+      })
+      .addTextInput({
+        category: ['Link behavior'],
+        path: 'navigateAfter',
+        name: 'Time after',
+        defaultValue: '10m',
+        description: '',
+      })
+      .addBooleanSwitch({
+        category: ['Link behavior'],
+        path: 'navigateToPanel',
+        name: 'Link to panel',
+        description: '',
+        defaultValue: true,
       });
   })
   // TODO, we should support this directly in the plugin infrastructure
