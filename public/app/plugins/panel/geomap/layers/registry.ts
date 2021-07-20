266fc2cffbc062c04c760059deaa818757bc3aa8
Ryan McKinley <ryantxu@gmail.com>
2021-07-20 13:40:52 -0700

    cleanup

--- public/app/plugins/panel/geomap/layers/registry.ts
+++ public/app/plugins/panel/geomap/layers/registry.ts
@@ -1,10 +1,7 @@
-import { MapLayerOptions, MapLayerRegistryItem, Registry } from '@grafana/data';
-import { Map } from 'ol';
-import { get as lodashGet, set as lodashSet } from 'lodash';
+import { MapLayerRegistryItem, Registry } from '@grafana/data';
 
 import { basemapLayers } from './basemaps';
 import { dataLayers } from './data';
-import { config } from '@grafana/runtime';
 
 /**
  * Registry for layer handlers
@@ -13,19 +10,3 @@ export const geomapLayerRegistry = new Registry<MapLayerRegistryItem<any>>(() =>
   ...basemapLayers, // simple basemaps
   ...dataLayers, // Layers with update functions
 ]);
-
-export function createLayerHandler(map: Map, options: MapLayerOptions) {
-  const item = geomapLayerRegistry.getIfExists(options.type);
-  if (!item) {
-    return undefined;
-  }
-
-  if (item.defaultOptions) {
-    options.config = {
-      ...item.defaultOptions,
-      ...options.config,
-    };
-  }
-
-  return item.create(map, options, config.theme2);
-}
