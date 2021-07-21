c123597ea8fb2221c3bd32c658dc99ad26a89a5c
bryanuribe <buribe@hmc.edu>
2021-07-21 13:09:24 -0700

    Test for getPointsFromLookup

--- public/app/plugins/panel/geomap/utils/location.test.ts
+++ public/app/plugins/panel/geomap/utils/location.test.ts
@@ -1,6 +1,17 @@
 import { toDataFrame, FieldType, FrameGeometrySourceMode } from '@grafana/data';
+import { getBackendSrv } from '@grafana/runtime';
 import { toLonLat } from 'ol/proj';
-import { dataFrameToPoints, getLocationFields, getLocationMatchers, LocationInfo } from './location';
+import {
+  convertLocationJson,
+  dataFrameToPoints,
+  getLocationFields,
+  getLocationMatchers,
+  getPointsFromLookup,
+} from './location';
+
+jest.mock('@grafana/runtime', () => ({
+  getBackendSrv: () => ({ get: jest.fn().mockResolvedValue([[2]]) }),
+}));
 
 const longitude = [0, -74.1];
 const latitude = [0, 40.7];
@@ -9,7 +20,7 @@ const lookup = ['US', 'AO'];
 const names = ['A', 'B'];
 
 describe('handle location parsing', () => {
-  it('auto should find geohash field', () => {
+  it('auto should find geohash field', async () => {
     const frame = toDataFrame({
       name: 'simple',
       fields: [
@@ -24,24 +35,22 @@ describe('handle location parsing', () => {
     expect(fields.geohash).toBeDefined();
     expect(fields.geohash?.name).toEqual('geohash');
 
-    const promiseInfo = dataFrameToPoints(frame, matchers);
-    promiseInfo.then((info: LocationInfo) => {
-      expect(info.points.map((p) => toLonLat(p.getCoordinates()))).toMatchInlineSnapshot(`
+    const info = await dataFrameToPoints(frame, matchers);
+    expect(info.points.map((p) => toLonLat(p.getCoordinates()))).toMatchInlineSnapshot(`
+      Array [
         Array [
-          Array [
-            -122.01416015625001,
-            36.979980468750014,
-          ],
-          Array [
-            -73.98193359375,
-            40.71533203125,
-          ],
-        ]
-      `);
-    });
+          -122.01416015625001,
+          36.979980468750014,
+        ],
+        Array [
+          -73.98193359375,
+          40.71533203125,
+        ],
+      ]
+    `);
   });
 
-  it('auto should find coordinate fields', () => {
+  it('auto should find coordinate fields', async () => {
     const frame = toDataFrame({
       name: 'simple',
       fields: [
@@ -52,47 +61,46 @@ describe('handle location parsing', () => {
     });
 
     const matchers = getLocationMatchers();
-    const promiseInfo = dataFrameToPoints(frame, matchers);
-    promiseInfo.then((info: LocationInfo) => {
-      expect(info.points.map((p) => toLonLat(p.getCoordinates()))).toMatchInlineSnapshot(`
+    const info = await dataFrameToPoints(frame, matchers);
+    expect(info.points.map((p) => toLonLat(p.getCoordinates()))).toMatchInlineSnapshot(`
+      Array [
         Array [
-          Array [
-            0,
-            0,
-          ],
-          Array [
-            -74.1,
-            40.69999999999999,
-          ],
-        ]
-      `);
-    });
+          0,
+          0,
+        ],
+        Array [
+          -74.1,
+          40.69999999999999,
+        ],
+      ]
+    `);
   });
 
-  it('auto should find lookup fields', () => {
+  it('map targets to locations and make points', () => {
     const frame = toDataFrame({
       name: 'simple',
       fields: [
         { name: 'name', type: FieldType.string, values: names },
-        { name: 'lookup', type: FieldType.number, values: lookup },
+        { name: 'lookup', type: FieldType.string, values: lookup },
       ],
     });
 
-    const matchers = getLocationMatchers();
-    const promiseInfo = dataFrameToPoints(frame, matchers);
-    promiseInfo.then((info: LocationInfo) => {
-      expect(info.points.map((p) => toLonLat(p.getCoordinates()))).toMatchInlineSnapshot(`
-          Array [
-            Array [
-              -95.712891,
-              37.09023999999998,
-            ],
-            Array [
-              17.873887,
-              -11.202691999999999,
-            ],
-          ]
-      `);
-    });
+    const countriesField = frame.fields.find((field) => field.name === 'lookup');
+    const countriesJson = require('./keyMapping/countries.json');
+    const countriesMap = convertLocationJson(countriesJson);
+
+    const points = getPointsFromLookup(countriesField!, countriesMap);
+    expect(points.map((p) => toLonLat(p.getCoordinates()))).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          -95.712891,
+          37.09023999999998,
+        ],
+        Array [
+          17.873887,
+          -11.202691999999999,
+        ],
+      ]
+    `);
   });
 });
