35853fab77f63d84e6c07fad07e133509d2791f2
gamab <gabi.mabs@gmail.com>
2021-07-21 08:18:41 +0200

    Moved RegistrationList def to roles.go

--- pkg/services/accesscontrol/roles.go
+++ pkg/services/accesscontrol/roles.go
@@ -3,6 +3,7 @@ package accesscontrol
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/grafana/grafana/pkg/models"
 )
@@ -255,4 +256,25 @@ func ValidateBuiltInRoles(builtInRoles []string) error {
 		}
 	}
 	return nil
+}
+
+type RegistrationList struct {
+	mx            sync.RWMutex
+	registrations []RoleRegistration
+}
+
+func (m *RegistrationList) Append(regs ...RoleRegistration) {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	m.registrations = append(m.registrations, regs...)
+}
+
+func (m *RegistrationList) Range(f func(registration RoleRegistration) bool) {
+	m.mx.RLock()
+	defer m.mx.RUnlock()
+	for _, registration := range m.registrations {
+		if ok := f(registration); !ok {
+			return
+		}
+	}
 }
