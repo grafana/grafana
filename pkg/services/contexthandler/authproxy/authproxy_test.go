b3096b88bad4ebb2ec44a5ffb76ccfd938c9df39
yuwaMSFT2 <yuwa@microsoft.com>
2021-07-22 06:49:58 -0700

    Auth: Pass user role to Grafana using auth proxy (#36729)
    * Pass role to Grafana using auth proxy
    
    By default, the role will be applied to the default org of the user.
    If the request uses the standard header "X-Grafana-Org-Id", the role will be applied to the specified org
    
    Tested in both unit test and manually E2E
    
    * Address comment: only allow the user role to be applied to the default org
    
    Co-authored-by: Leonard Gram <leo@xlson.com>

--- pkg/services/contexthandler/authproxy/authproxy_test.go
+++ pkg/services/contexthandler/authproxy/authproxy_test.go
@@ -107,8 +107,9 @@ func TestMiddlewareContext(t *testing.T) {
 	t.Run("When the cache key contains additional headers", func(t *testing.T) {
 		const id int64 = 33
 		const group = "grafana-core-team"
+		const role = "Admin"
 
-		h, err := HashCacheKey(hdrName + "-" + group)
+		h, err := HashCacheKey(hdrName + "-" + group + "-" + role)
 		require.NoError(t, err)
 		key := fmt.Sprintf(CachePrefix, h)
 		err = cache.Set(key, id, 0)
@@ -116,9 +117,10 @@ func TestMiddlewareContext(t *testing.T) {
 
 		auth := prepareMiddleware(t, cache, func(req *http.Request, cfg *setting.Cfg) {
 			req.Header.Set("X-WEBAUTH-GROUPS", group)
-			cfg.AuthProxyHeaders = map[string]string{"Groups": "X-WEBAUTH-GROUPS"}
+			req.Header.Set("X-WEBAUTH-ROLE", role)
+			cfg.AuthProxyHeaders = map[string]string{"Groups": "X-WEBAUTH-GROUPS", "Role": "X-WEBAUTH-ROLE"}
 		})
-		assert.Equal(t, "auth-proxy-sync-ttl:14f69b7023baa0ac98c96b31cec07bc0", key)
+		assert.Equal(t, "auth-proxy-sync-ttl:f5acfffd56daac98d502ef8c8b8c5d56", key)
 
 		gotID, err := auth.Login(logger, false)
 		require.NoError(t, err)
