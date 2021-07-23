885b9dd2cd5af015b69d6b2804b43a500d4eca29
Ashley Harrison <ashley.harrison@grafana.com>
2021-07-23 14:35:28 +0100

    Chore: Update slate types to fix more strict TypeScript errors

--- public/app/features/users/UsersListPage.tsx
+++ public/app/features/users/UsersListPage.tsx
@@ -1,34 +1,43 @@
 import React, { PureComponent } from 'react';
 import { hot } from 'react-hot-loader';
-import { connect } from 'react-redux';
-import { NavModel, renderMarkdown } from '@grafana/data';
+import { connect, ConnectedProps } from 'react-redux';
+import { renderMarkdown } from '@grafana/data';
 import { HorizontalGroup, Pagination, VerticalGroup } from '@grafana/ui';
 
 import Page from 'app/core/components/Page/Page';
 import UsersActionBar from './UsersActionBar';
 import UsersTable from './UsersTable';
 import InviteesTable from './InviteesTable';
-import { Invitee, OrgUser, OrgRole } from 'app/types';
+import { OrgUser, OrgRole, StoreState } from 'app/types';
 import { loadInvitees, loadUsers, removeUser, updateUser } from './state/actions';
 import { getNavModel } from 'app/core/selectors/navModel';
 import { getInvitees, getUsers, getUsersSearchQuery, getUsersSearchPage } from './state/selectors';
 import { setUsersSearchQuery, setUsersSearchPage } from './state/reducers';
 
-export interface Props {
-  navModel: NavModel;
-  invitees: Invitee[];
-  users: OrgUser[];
-  searchQuery: string;
-  searchPage: number;
-  externalUserMngInfo: string;
-  hasFetched: boolean;
-  loadUsers: typeof loadUsers;
-  loadInvitees: typeof loadInvitees;
-  setUsersSearchQuery: typeof setUsersSearchQuery;
-  setUsersSearchPage: typeof setUsersSearchPage;
-  updateUser: typeof updateUser;
-  removeUser: typeof removeUser;
+function mapStateToProps(state: StoreState) {
+  return {
+    navModel: getNavModel(state.navIndex, 'users'),
+    users: getUsers(state.users),
+    searchQuery: getUsersSearchQuery(state.users),
+    searchPage: getUsersSearchPage(state.users),
+    invitees: getInvitees(state.users),
+    externalUserMngInfo: state.users.externalUserMngInfo,
+    hasFetched: state.users.hasFetched,
+  };
 }
+
+const mapDispatchToProps = {
+  loadUsers,
+  loadInvitees,
+  setUsersSearchQuery,
+  setUsersSearchPage,
+  updateUser,
+  removeUser,
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+export type Props = ConnectedProps<typeof connector>;
 
 export interface State {
   showInvites: boolean;
@@ -129,25 +138,4 @@ export class UsersListPage extends PureComponent<Props, State> {
   }
 }
 
-function mapStateToProps(state: any) {
-  return {
-    navModel: getNavModel(state.navIndex, 'users'),
-    users: getUsers(state.users),
-    searchQuery: getUsersSearchQuery(state.users),
-    searchPage: getUsersSearchPage(state.users),
-    invitees: getInvitees(state.users),
-    externalUserMngInfo: state.users.externalUserMngInfo,
-    hasFetched: state.users.hasFetched,
-  };
-}
-
-const mapDispatchToProps = {
-  loadUsers,
-  loadInvitees,
-  setUsersSearchQuery,
-  setUsersSearchPage,
-  updateUser,
-  removeUser,
-};
-
-export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(UsersListPage));
+export default hot(module)(connector(UsersListPage));
