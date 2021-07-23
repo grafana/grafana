3631eb7d7478ec03454cfad282ec088780d7a484
Zoltán Bedi <zoltan.bedi@gmail.com>
2021-07-23 18:02:07 +0200

    Review comments addressed

--- packages/grafana-ui/src/components/FileDropzone/FileListItem.test.tsx
+++ packages/grafana-ui/src/components/FileDropzone/FileListItem.test.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { FileListItem } from './FileListItem';
 
-const file: any = {
-  name: 'someFile.jpg',
-  size: 1680206,
-  lastModified: 1604849095696,
-  type: 'image/jpeg',
-};
+const file = ({
+  fileBits = 'prettyPicture',
+  fileName = 'someFile.jpg',
+  options = { lastModified: 1604849095696, type: 'image/jpeg' },
+}) => new File([fileBits], fileName, options);
 
 describe('The FileListItem component', () => {
   it('should show an error message when error prop is not null', () => {
-    render(<FileListItem file={{ file, id: '1', error: new DOMException('error') }} />);
+    render(<FileListItem file={{ file: file({}), id: '1', error: new DOMException('error') }} />);
 
     expect(screen.getByText('error')).toBeInTheDocument();
     expect(screen.queryByLabelText('Retry')).not.toBeInTheDocument();
@@ -19,7 +18,7 @@ describe('The FileListItem component', () => {
 
   it('should show a retry icon when error is not null and retryUpload prop is passed', () => {
     const retryUpload = jest.fn();
-    render(<FileListItem file={{ file, id: '1', error: new DOMException('error'), retryUpload }} />);
+    render(<FileListItem file={{ file: file({}), id: '1', error: new DOMException('error'), retryUpload }} />);
 
     fireEvent.click(screen.getByLabelText('Retry'));
 
@@ -29,34 +28,34 @@ describe('The FileListItem component', () => {
   });
 
   it('should show a progressbar when the progress prop has a value', () => {
-    render(<FileListItem file={{ file, id: '1', error: null, progress: 800000 }} />);
+    render(<FileListItem file={{ file: file({}), id: '1', error: null, progress: 6 }} />);
 
     expect(screen.queryByText('Cancel')).not.toBeInTheDocument();
-    expect(screen.getByText('48%')).toBeInTheDocument();
+    expect(screen.getByText('46%')).toBeInTheDocument();
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
   });
 
   it('should not show a progressbar when progress is equal to the size', () => {
-    render(<FileListItem file={{ file, id: '1', error: null, progress: 1680206 }} />);
+    render(<FileListItem file={{ file: file({}), id: '1', error: null, progress: 13 }} />);
 
     expect(screen.queryByRole('progressbar')).not.toBeInTheDocument();
   });
 
   it('should show a Cancel button when abortUpload prop is passed', () => {
     const abortUpload = jest.fn();
-    render(<FileListItem file={{ file, id: '1', error: null, progress: 800000, abortUpload }} />);
+    render(<FileListItem file={{ file: file({}), id: '1', error: null, progress: 6, abortUpload }} />);
 
-    fireEvent.click(screen.getByText('Cancel'));
+    fireEvent.click(screen.getByRole('button', { name: 'Cancel' }));
 
     expect(abortUpload).toBeCalledTimes(1);
   });
 
   it('should show a Remove icon when removeFile prop is passed', () => {
     const removeFile = jest.fn();
-    const customFile = { file, id: '1', error: null };
+    const customFile = { file: file({}), id: '1', error: null };
     render(<FileListItem file={customFile} removeFile={removeFile} />);
 
-    fireEvent.click(screen.getByLabelText('Remove'));
+    fireEvent.click(screen.getByRole('button', { name: 'Remove file' }));
 
     expect(removeFile).toBeCalledWith(customFile);
   });
