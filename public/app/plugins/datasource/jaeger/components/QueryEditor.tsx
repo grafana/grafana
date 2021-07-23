463df56af87a6cafadeba64b1b9a52528c8bf12f
Zoltán Bedi <zoltan.bedi@gmail.com>
2021-07-23 13:43:59 +0200

    styling

--- public/app/plugins/datasource/jaeger/components/QueryEditor.tsx
+++ public/app/plugins/datasource/jaeger/components/QueryEditor.tsx
@@ -1,14 +1,7 @@
 import { css } from '@emotion/css';
 import { QueryEditorProps } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import {
-  FileDropzone,
-  FileDropzoneDefaultChildren,
-  InlineField,
-  InlineFieldRow,
-  Input,
-  RadioButtonGroup,
-} from '@grafana/ui';
+import { FileDropzone, InlineField, InlineFieldRow, Input, RadioButtonGroup, useTheme2 } from '@grafana/ui';
 import React from 'react';
 import { JaegerDatasource } from '../datasource';
 import { JaegerQuery, JaegerQueryType } from '../types';
@@ -17,64 +10,71 @@ import { SearchForm } from './SearchForm';
 type Props = QueryEditorProps<JaegerDatasource, JaegerQuery>;
 
 export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) {
+  const theme = useTheme2();
+
   const renderEditorBody = () => {
-    switch (query.queryType) {
-      case 'search':
-        return <SearchForm datasource={datasource} query={query} onChange={onChange} />;
-      case 'upload':
-        return (
+    if (query.queryType === 'search') {
+      return <SearchForm datasource={datasource} query={query} onChange={onChange} />;
+    }
+
+    if (query.queryType !== 'upload') {
+      return (
+        <InlineFieldRow>
+          <InlineField label="Trace ID" labelWidth={21} grow>
+            <Input
+              aria-label={selectors.components.DataSource.Jaeger.traceIDInput}
+              placeholder="Eg. 4050b8060d659e52"
+              value={query.query || ''}
+              onChange={(v) =>
+                onChange({
+                  ...query,
+                  query: v.currentTarget.value,
+                })
+              }
+            />
+          </InlineField>
+        </InlineFieldRow>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <>
+      <div className={css({ width: '50%' })}>
+        <InlineFieldRow>
+          <InlineField label="Query type">
+            <RadioButtonGroup<JaegerQueryType>
+              options={[
+                { value: 'search', label: 'Search' },
+                { value: undefined, label: 'TraceID' },
+                { value: 'upload', label: 'JSON File' },
+              ]}
+              value={query.queryType}
+              onChange={(v) =>
+                onChange({
+                  ...query,
+                  queryType: v,
+                })
+              }
+              size="md"
+            />
+          </InlineField>
+        </InlineFieldRow>
+        {renderEditorBody()}
+      </div>
+      {query.queryType === 'upload' ? (
+        <div className={css({ padding: theme.spacing(2) })}>
           <FileDropzone
             options={{ accept: '.json', multiple: false }}
             onLoad={(result: string) => {
               datasource.uploadedJson = result;
               onRunQuery();
             }}
-          >
-            <FileDropzoneDefaultChildren primaryText="Upload JSON file" />
-          </FileDropzone>
-        );
-      default:
-        return (
-          <InlineFieldRow>
-            <InlineField label="Trace ID" labelWidth={21} grow>
-              <Input
-                aria-label={selectors.components.DataSource.Jaeger.traceIDInput}
-                placeholder="Eg. 4050b8060d659e52"
-                value={query.query || ''}
-                onChange={(v) =>
-                  onChange({
-                    ...query,
-                    query: v.currentTarget.value,
-                  })
-                }
-              />
-            </InlineField>
-          </InlineFieldRow>
-        );
-    }
-  };
-  return (
-    <div className={css({ width: '50%' })}>
-      <InlineFieldRow>
-        <InlineField label="Query type">
-          <RadioButtonGroup<JaegerQueryType>
-            options={[
-              { value: 'search', label: 'Search' },
-              { value: undefined, label: 'TraceID' },
-              { value: 'upload', label: 'JSON File' },
-            ]}
-            value={query.queryType}
-            onChange={(v) =>
-              onChange({
-                ...query,
-                queryType: v,
-              })
-            }
-            size="md"
           />
-        </InlineField>
-      </InlineFieldRow>
-      {renderEditorBody()}
-    </div>
+        </div>
+      ) : null}
+    </>
   );
 }
