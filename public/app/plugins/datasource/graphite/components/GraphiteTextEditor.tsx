4528f7782234c0787b788ed4283d82c0aba6d861
Piotr Jamróz <pm.jamroz@gmail.com>
2021-07-26 12:37:37 +0200

    Update current query when props change

--- public/app/plugins/datasource/graphite/components/GraphiteTextEditor.tsx
+++ public/app/plugins/datasource/graphite/components/GraphiteTextEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { QueryField } from '@grafana/ui';
 import { actions } from '../state/actions';
 import { Dispatch } from 'redux';
@@ -15,10 +15,20 @@ export function GraphiteTextEditor({ rawQuery, dispatch }: Props) {
     dispatch(actions.updateQuery({ query: currentQuery }));
   }, [dispatch, currentQuery]);
 
+  // Needed for migration to React. State in QueryCtrl is initialized asynchronously (using init action) meaning
+  // the angular js partial is rendered first with rawQuery=undefined and only once the state is initialized the
+  // rawQuery is updated with the current query. At the same time in GraphiteTextEditor we need to keep track of
+  // the query with managed state because onRunQuery callback doesn't provide the current value. This means that
+  // the managed state is not updated when props change after the init action finishes and we need to use effect
+  // to update it.
+  useEffect(() => {
+    updateCurrentQuery(rawQuery);
+  }, [rawQuery]);
+
   return (
     <>
       <QueryField
-        query={rawQuery}
+        query={currentQuery}
         onChange={updateCurrentQuery}
         onBlur={applyChanges}
         onRunQuery={applyChanges}
