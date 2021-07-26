1df51f5fe09800c67a88cb9e2d69788c3a9bf727
Piotr Jamróz <pm.jamroz@gmail.com>
2021-07-26 13:01:48 +0200

    Remove managed state in GraphiteTextEditor

--- public/app/plugins/datasource/graphite/components/GraphiteTextEditor.tsx
+++ public/app/plugins/datasource/graphite/components/GraphiteTextEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import { QueryField } from '@grafana/ui';
 import { actions } from '../state/actions';
 import { Dispatch } from 'redux';
@@ -9,29 +9,24 @@ type Props = {
 };
 
 export function GraphiteTextEditor({ rawQuery, dispatch }: Props) {
-  const [currentQuery, updateCurrentQuery] = useState<string>(rawQuery);
+  const updateQuery = useCallback(
+    (query: string) => {
+      dispatch(actions.updateQuery({ query }));
+    },
+    [dispatch]
+  );
 
-  const applyChanges = useCallback(() => {
-    dispatch(actions.updateQuery({ query: currentQuery }));
-  }, [dispatch, currentQuery]);
-
-  // Needed for migration to React. State in QueryCtrl is initialized asynchronously (using init action) meaning
-  // the angular js partial is rendered first with rawQuery=undefined and only once the state is initialized the
-  // rawQuery is updated with the current query. At the same time in GraphiteTextEditor we need to keep track of
-  // the query with managed state because onRunQuery callback doesn't provide the current value. This means that
-  // the managed state is not updated when props change after the init action finishes and we need to use effect
-  // to update it.
-  useEffect(() => {
-    updateCurrentQuery(rawQuery);
-  }, [rawQuery]);
+  const runQuery = useCallback(() => {
+    dispatch(actions.runQuery());
+  }, [dispatch]);
 
   return (
     <>
       <QueryField
-        query={currentQuery}
-        onChange={updateCurrentQuery}
-        onBlur={applyChanges}
-        onRunQuery={applyChanges}
+        query={rawQuery}
+        onChange={updateQuery}
+        onBlur={runQuery}
+        onRunQuery={runQuery}
         placeholder={'Enter a Graphite query (run with Shift+Enter)'}
         portalOrigin="graphite"
       />
