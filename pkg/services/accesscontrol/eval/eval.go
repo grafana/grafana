457ded239eea3bf5270bb2c88c56d5036b9c17b4
Karl Persson <kalle.persson92@gmail.com>
2021-07-26 09:20:25 +0200

    Update pkg/services/accesscontrol/eval/eval.go
    Co-authored-by: Gabriel MABILLE <gamab@users.noreply.github.com>

--- pkg/services/accesscontrol/eval/eval.go
+++ pkg/services/accesscontrol/eval/eval.go
@@ -3,6 +3,6 @@ package eval
 type Evaluator interface {
 	// Evaluate permissions that are grouped by action
 	Evaluate(permissions map[string]map[string]struct{}) (bool, error)
-	// Inject params into templated scopes. Eg. "settings:" + eval.Parameters(":id") and returns a new Evaluator
+	// Inject params into the evaluator's templated scopes. Eg. "settings:" + eval.Parameters(":id") and returns a new Evaluator
 	Inject(params map[string]string) (Evaluator, error)
 }
