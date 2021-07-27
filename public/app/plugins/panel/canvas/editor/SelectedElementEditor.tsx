1f9243b91fab481f39f2480a5ae12b1d63cac69e
Ryan McKinley <ryantxu@gmail.com>
2021-07-27 12:48:57 -0700

    add element editor

---
+++ public/app/plugins/panel/canvas/editor/SelectedElementEditor.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useMemo } from 'react';
+import { StandardEditorProps } from '@grafana/data';
+import { CanvasLayerOptions } from '../base';
+import { PanelOptions } from '../models.gen';
+import { CanvasElementEditor } from './ElementEditor';
+import { DEFAULT_ELEMENT_CONFIG } from '../elements/registry';
+
+export const SelectedElementEditor: FC<StandardEditorProps<CanvasLayerOptions, any, PanelOptions>> = ({
+  value,
+  onChange,
+  context,
+}) => {
+  const selectedElement = useMemo(() => {
+    return value.elements[0] ?? { ...DEFAULT_ELEMENT_CONFIG };
+  }, [value]);
+
+  return (
+    <CanvasElementEditor
+      options={selectedElement}
+      data={context.data}
+      onChange={(cfg) => {
+        console.log('Change element:', cfg);
+        onChange({ ...value, elements: [cfg] });
+      }}
+    />
+  );
+};
