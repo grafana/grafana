f056fbfd92c3f9fdb50c5637e7d84a13cd9e0746
Jack Westbrook <jack.westbrook@gmail.com>
2021-07-27 17:05:44 +0200

    feat(catalog): update Browse page to use new search, filter features

--- public/app/features/plugins/admin/pages/Browse.tsx
+++ public/app/features/plugins/admin/pages/Browse.tsx
@@ -1,13 +1,12 @@
 import React, { ReactElement } from 'react';
 import { css } from '@emotion/css';
 import { SelectableValue, dateTimeParse } from '@grafana/data';
-import { Field, LoadingPlaceholder, Select } from '@grafana/ui';
+import { LoadingPlaceholder, Select, HorizontalGroup, RadioButtonGroup } from '@grafana/ui';
 import { useLocation } from 'react-router-dom';
 import { locationSearchToObject } from '@grafana/runtime';
 
 import { PluginList } from '../components/PluginList';
 import { SearchField } from '../components/SearchField';
-import { HorizontalGroup } from '../components/HorizontalGroup';
 import { useHistory } from '../hooks/useHistory';
 import { CatalogPlugin } from '../types';
 import { Page as PluginPage } from '../components/Page';
@@ -24,9 +23,10 @@ export default function Browse(): ReactElement | null {
 
   const q = query.q as string;
   const filterBy = (query.filterBy as string) ?? 'installed';
+  const filterByType = (query.filterByType as string) ?? 'all';
   const sortBy = (query.sortBy as string) ?? 'name';
 
-  const { plugins, isLoading, error } = usePluginsByFilter(q, filterBy);
+  const { plugins, isLoading, error } = usePluginsByFilter({ searchBy: q, filterBy, filterByType });
   const sortedPlugins = plugins.sort(sorters[sortBy]);
   const history = useHistory();
 
@@ -34,12 +34,16 @@ export default function Browse(): ReactElement | null {
     history.push({ query: { sortBy: value.value } });
   };
 
-  const onFilterByChange = (value: SelectableValue<string>) => {
-    history.push({ query: { filterBy: value.value } });
+  const onFilterByChange = (value: string) => {
+    history.push({ query: { filterBy: value } });
+  };
+
+  const onFilterByTypeChange = (value: string) => {
+    history.push({ query: { filterByType: value } });
   };
 
   const onSearch = (q: any) => {
-    history.push({ query: { filterBy: 'all', q } });
+    history.push({ query: { filterBy: 'all', filterByType: 'all', q } });
   };
 
   // How should we handle errors?
@@ -52,23 +56,20 @@ export default function Browse(): ReactElement | null {
     <Page navModel={navModel}>
       <Page.Contents>
         <PluginPage>
-          <SearchField value={q} onSearch={onSearch} />
-          <HorizontalGroup>
-            <div>
-              {isLoading ? (
-                <LoadingPlaceholder
-                  className={css`
-                    margin-bottom: 0;
-                  `}
-                  text="Loading results"
-                />
-              ) : (
-                `${sortedPlugins.length} ${sortedPlugins.length > 1 ? 'results' : 'result'}`
-              )}
-            </div>
-            <Field label="Show">
-              <Select
-                width={15}
+          <HorizontalGroup justify="space-between">
+            <SearchField value={q} onSearch={onSearch} />
+            <HorizontalGroup>
+              <RadioButtonGroup
+                value={filterByType}
+                onChange={onFilterByTypeChange}
+                options={[
+                  { value: 'all', label: 'All' },
+                  { value: 'datasource', label: 'Data sources' },
+                  { value: 'app', label: 'Applications' },
+                  { value: 'panel', label: 'Panels' },
+                ]}
+              />
+              <RadioButtonGroup
                 value={filterBy}
                 onChange={onFilterByChange}
                 options={[
@@ -76,24 +77,35 @@ export default function Browse(): ReactElement | null {
                   { value: 'installed', label: 'Installed' },
                 ]}
               />
-            </Field>
-            <Field label="Sort by">
               <Select
-                width={20}
+                width={24}
                 value={sortBy}
                 onChange={onSortByChange}
                 options={[
-                  { value: 'name', label: 'Name' },
-                  { value: 'popularity', label: 'Popularity' },
-                  { value: 'updated', label: 'Updated date' },
-                  { value: 'published', label: 'Published date' },
-                  { value: 'downloads', label: 'Downloads' },
+                  { value: 'name', label: 'Sort by name (A-Z)' },
+                  { value: 'updated', label: 'Sort by updated date' },
+                  { value: 'published', label: 'Sort by published date' },
+                  { value: 'downloads', label: 'Sort by downloads' },
                 ]}
               />
-            </Field>
+            </HorizontalGroup>
           </HorizontalGroup>
-
-          {!isLoading && <PluginList plugins={sortedPlugins} />}
+          <div
+            className={css`
+              margin-top: 24px;
+            `}
+          >
+            {isLoading ? (
+              <LoadingPlaceholder
+                className={css`
+                  margin-bottom: 0;
+                `}
+                text="Loading results"
+              />
+            ) : (
+              <PluginList plugins={sortedPlugins} />
+            )}
+          </div>
         </PluginPage>
       </Page.Contents>
     </Page>
@@ -107,5 +119,4 @@ const sorters: { [name: string]: (a: CatalogPlugin, b: CatalogPlugin) => number 
   published: (a: CatalogPlugin, b: CatalogPlugin) =>
     dateTimeParse(b.publishedAt).valueOf() - dateTimeParse(a.publishedAt).valueOf(),
   downloads: (a: CatalogPlugin, b: CatalogPlugin) => b.downloads - a.downloads,
-  popularity: (a: CatalogPlugin, b: CatalogPlugin) => b.popularity - a.popularity,
 };
