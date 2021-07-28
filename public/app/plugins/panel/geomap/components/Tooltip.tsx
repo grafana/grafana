7cc445d7c9f334e4ebeb6072af58c72911c2a996
Ryan McKinley <ryantxu@gmail.com>
2021-07-28 12:46:22 -0700

    basic tooltip behavior

--- public/app/plugins/panel/geomap/components/Tooltip.tsx
+++ public/app/plugins/panel/geomap/components/Tooltip.tsx
@@ -1,12 +1,10 @@
 import React, { PureComponent } from 'react';
-import { stylesFactory } from '@grafana/ui';
-import { Map } from 'ol';
+import { Portal, stylesFactory, VizTooltipContainer } from '@grafana/ui';
+import { Map, MapBrowserEvent } from 'ol';
 import { GrafanaTheme } from '@grafana/data';
 import { css } from '@emotion/css';
 import { config } from 'app/core/config';
 import tinycolor from 'tinycolor2';
-import { FeatureLike } from 'ol/Feature';
-import { Pixel } from 'ol/pixel';
 import { toLonLat } from 'ol/proj';
 
 interface Props {
@@ -15,8 +13,9 @@ interface Props {
 
 interface State {
   visible: boolean;
-  coordinates?: number[];
-  position?: number[];
+  lonLat?: number[];
+  pageX: number;
+  pageY: number;
 }
 
 export class Tooltip extends PureComponent<Props, State> {
@@ -24,59 +23,50 @@ export class Tooltip extends PureComponent<Props, State> {
 
   constructor(props: Props) {
     super(props);
-    this.state = { visible: false };
+    this.state = { visible: false, pageX: 0, pageY: 0 };
   }
-
-  updateViewState = (features: FeatureLike[], pixel: Pixel) => {
-    if (features.length > 0) {
-      const firstFeature = features[0];
-      this.setState({
-        visible: true,
-        coordinates: firstFeature.getProperties().geometry.flatCoordinates,
-      });
-    } else {
-      this.setState({
-        visible: false,
-      });
-    }
-  };
 
   componentDidMount() {
     const { map } = this.props;
 
-    map.on('pointermove', (evt: any) => {
-      const features: FeatureLike[] = [];
-      const pixel = map.getEventPixel(evt.originalEvent);
-      map.forEachFeatureAtPixel(pixel, function (feature) {
-        features.push(feature);
+    map.on('pointermove', (evt: MapBrowserEvent) => {
+      console.log('pointermove', evt);
+      const mouse = evt.originalEvent as any;
+      const update: State = { visible: false, pageX: mouse.pageX, pageY: mouse.pageY };
+      const pixel = map.getEventPixel(mouse);
+      map.forEachFeatureAtPixel(pixel, (feature, layer, geo) => {
+        if (!update.visible) {
+          update.lonLat = toLonLat(geo.getFlatCoordinates());
+          update.visible = true;
+        }
       });
-      this.updateViewState(features, pixel);
+      this.setState(update);
     });
   }
 
   render() {
-    const { visible, coordinates } = this.state;
+    const { visible, pageX, pageY, lonLat } = this.state;
 
-    if (visible && coordinates) {
-      const lonLat = toLonLat(coordinates, 'EPSG:3857');
-
+    if (visible && lonLat) {
       return (
-        <div className={this.style.infoWrap}>
-          {visible && (
-            <table>
-              <tbody>
-                <tr>
-                  <th>Latitude:</th>
-                  <td>{lonLat[1].toFixed(3)}</td>
-                </tr>
-                <tr>
-                  <th>Longitude:&nbsp;</th>
-                  <td> {lonLat[0].toFixed(3)} </td>
-                </tr>
-              </tbody>
-            </table>
-          )}
-        </div>
+        <Portal>
+          <VizTooltipContainer position={{ x: pageX, y: pageY }} offset={{ x: 10, y: 10 }}>
+            <div className={this.style.infoWrap}>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>Latitude:</th>
+                    <td>{lonLat[1].toFixed(3)}</td>
+                  </tr>
+                  <tr>
+                    <th>Longitude:&nbsp;</th>
+                    <td> {lonLat[0].toFixed(3)} </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </VizTooltipContainer>
+        </Portal>
       );
     } else {
       return <></>;
