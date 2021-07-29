18417f60d10cdf212de22124043b17580f161808
Kyle Brandt <kyle@grafana.com>
2021-07-29 14:29:17 -0400

    NGAlert: Send resolve signal to alertmanager on alerting -> Normal (#37363)

--- pkg/services/ngalert/state/manager.go
+++ pkg/services/ngalert/state/manager.go
@@ -175,6 +175,10 @@ func (st *Manager) setNextState(alertRule *ngModels.AlertRule, result eval.Resul
 	case eval.Pending: // we do not emit results with this state
 	}
 
+	// Set Resolved property so the scheduler knows to send a postable alert
+	// to Alertmanager.
+	currentState.Resolved = oldState == eval.Alerting && currentState.State == eval.Normal
+
 	st.set(currentState)
 	if oldState != currentState.State {
 		go st.createAlertAnnotation(currentState.State, alertRule, result)
