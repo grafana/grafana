d51b2630c7a423be51882030e7a13d18b0eb6fe4
Leonard Gram <leo@xlson.com>
2021-07-29 10:18:42 +0200

    Auth: creates a hook in the user mapping flow (#37190)
    * wip
    
    * Auth Info: refactored out into it's own service
    
    * Auth: adds extension point where users are being mapped
    
    * Update pkg/services/login/authinfoservice/service.go
    
    Co-authored-by: Joan López de la Franca Beltran <joanjan14@gmail.com>
    
    * Update pkg/services/login/authinfoservice/service.go
    
    Co-authored-by: Joan López de la Franca Beltran <joanjan14@gmail.com>
    
    * Auth: simplified code
    
    * moved most authinfo stuff to its own package
    
    * added back code
    
    * linter
    
    * simplified
    
    Co-authored-by: Joan López de la Franca Beltran <joanjan14@gmail.com>

--- pkg/services/login/authinfoservice/database.go
+++ pkg/services/login/authinfoservice/database.go
@@ -1,9 +1,11 @@
-package sqlstore
+package authinfoservice
 
 import (
+	"context"
 	"encoding/base64"
-	"errors"
 	"time"
+
+	"github.com/grafana/grafana/pkg/services/sqlstore"
 
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/models"
@@ -13,123 +15,7 @@ import (
 
 var getTime = time.Now
 
-const genericOAuthModule = "oauth_generic_oauth"
-
-func init() {
-	bus.AddHandler("sql", GetUserByAuthInfo)
-	bus.AddHandler("sql", GetExternalUserInfoByLogin)
-	bus.AddHandler("sql", GetAuthInfo)
-	bus.AddHandler("sql", SetAuthInfo)
-	bus.AddHandler("sql", UpdateAuthInfo)
-	bus.AddHandler("sql", DeleteAuthInfo)
-}
-
-func GetUserByAuthInfo(query *models.GetUserByAuthInfoQuery) error {
-	user := &models.User{}
-	has := false
-	var err error
-	authQuery := &models.GetAuthInfoQuery{}
-
-	// Try to find the user by auth module and id first
-	if query.AuthModule != "" && query.AuthId != "" {
-		authQuery.AuthModule = query.AuthModule
-		authQuery.AuthId = query.AuthId
-
-		err = GetAuthInfo(authQuery)
-		if !errors.Is(err, models.ErrUserNotFound) {
-			if err != nil {
-				return err
-			}
-
-			// if user id was specified and doesn't match the user_auth entry, remove it
-			if query.UserId != 0 && query.UserId != authQuery.Result.UserId {
-				err = DeleteAuthInfo(&models.DeleteAuthInfoCommand{
-					UserAuth: authQuery.Result,
-				})
-				if err != nil {
-					sqlog.Error("Error removing user_auth entry", "error", err)
-				}
-
-				authQuery.Result = nil
-			} else {
-				has, err = x.Id(authQuery.Result.UserId).Get(user)
-				if err != nil {
-					return err
-				}
-
-				if !has {
-					// if the user has been deleted then remove the entry
-					err = DeleteAuthInfo(&models.DeleteAuthInfoCommand{
-						UserAuth: authQuery.Result,
-					})
-					if err != nil {
-						sqlog.Error("Error removing user_auth entry", "error", err)
-					}
-
-					authQuery.Result = nil
-				}
-			}
-		}
-	}
-
-	// If not found, try to find the user by id
-	if !has && query.UserId != 0 {
-		has, err = x.Id(query.UserId).Get(user)
-		if err != nil {
-			return err
-		}
-	}
-
-	// If not found, try to find the user by email address
-	if !has && query.Email != "" {
-		user = &models.User{Email: query.Email}
-		has, err = x.Get(user)
-		if err != nil {
-			return err
-		}
-	}
-
-	// If not found, try to find the user by login
-	if !has && query.Login != "" {
-		user = &models.User{Login: query.Login}
-		has, err = x.Get(user)
-		if err != nil {
-			return err
-		}
-	}
-
-	// No user found
-	if !has {
-		return models.ErrUserNotFound
-	}
-
-	// Special case for generic oauth duplicates
-	if query.AuthModule == genericOAuthModule && user.Id != 0 {
-		authQuery.UserId = user.Id
-		authQuery.AuthModule = query.AuthModule
-		err = GetAuthInfo(authQuery)
-		if !errors.Is(err, models.ErrUserNotFound) {
-			if err != nil {
-				return err
-			}
-		}
-	}
-	if authQuery.Result == nil && query.AuthModule != "" {
-		cmd2 := &models.SetAuthInfoCommand{
-			UserId:     user.Id,
-			AuthModule: query.AuthModule,
-			AuthId:     query.AuthId,
-		}
-		if err := SetAuthInfo(cmd2); err != nil {
-			return err
-		}
-	}
-
-	query.Result = user
-	return nil
-}
-
-func GetExternalUserInfoByLogin(query *models.GetExternalUserInfoByLoginQuery) error {
+func (s *Implementation) GetExternalUserInfoByLogin(query *models.GetExternalUserInfoByLoginQuery) error {
 	userQuery := models.GetUserByLoginQuery{LoginOrEmail: query.LoginOrEmail}
 	err := bus.Dispatch(&userQuery)
 	if err != nil {
@@ -153,13 +39,20 @@ func GetExternalUserInfoByLogin(query *models.GetExternalUserInfoByLoginQuery) e
 	return nil
 }
 
-func GetAuthInfo(query *models.GetAuthInfoQuery) error {
+func (s *Implementation) GetAuthInfo(query *models.GetAuthInfoQuery) error {
 	userAuth := &models.UserAuth{
 		UserId:     query.UserId,
 		AuthModule: query.AuthModule,
 		AuthId:     query.AuthId,
 	}
-	has, err := x.Desc("created").Get(userAuth)
+
+	var has bool
+	var err error
+
+	err = s.SQLStore.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
+		has, err = sess.Desc("created").Get(userAuth)
+		return err
+	})
 	if err != nil {
 		return err
 	}
@@ -188,8 +81,8 @@ func GetAuthInfo(query *models.GetAuthInfoQuery) error {
 	return nil
 }
 
-func SetAuthInfo(cmd *models.SetAuthInfoCommand) error {
-	return inTransaction(func(sess *DBSession) error {
+func (s *Implementation) SetAuthInfo(cmd *models.SetAuthInfoCommand) error {
+	return s.SQLStore.WithTransactionalDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
 		authUser := &models.UserAuth{
 			UserId:     cmd.UserId,
 			AuthModule: cmd.AuthModule,
@@ -222,8 +115,8 @@ func SetAuthInfo(cmd *models.SetAuthInfoCommand) error {
 	})
 }
 
-func UpdateAuthInfo(cmd *models.UpdateAuthInfoCommand) error {
-	return inTransaction(func(sess *DBSession) error {
+func (s *Implementation) UpdateAuthInfo(cmd *models.UpdateAuthInfoCommand) error {
+	return s.SQLStore.WithTransactionalDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
 		authUser := &models.UserAuth{
 			UserId:     cmd.UserId,
 			AuthModule: cmd.AuthModule,
@@ -256,13 +149,13 @@ func UpdateAuthInfo(cmd *models.UpdateAuthInfoCommand) error {
 			AuthModule: cmd.AuthModule,
 		}
 		upd, err := sess.Update(authUser, cond)
-		sqlog.Debug("Updated user_auth", "user_id", cmd.UserId, "auth_module", cmd.AuthModule, "rows", upd)
+		s.logger.Debug("Updated user_auth", "user_id", cmd.UserId, "auth_module", cmd.AuthModule, "rows", upd)
 		return err
 	})
 }
 
-func DeleteAuthInfo(cmd *models.DeleteAuthInfoCommand) error {
-	return inTransaction(func(sess *DBSession) error {
+func (s *Implementation) DeleteAuthInfo(cmd *models.DeleteAuthInfoCommand) error {
+	return s.SQLStore.WithTransactionalDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
 		_, err := sess.Delete(cmd.UserAuth)
 		return err
 	})
