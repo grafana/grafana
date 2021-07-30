3e4ba99c362a4bf28ede579ce9ac452654497f09
Connor Lindsey <cblindsey3@gmail.com>
2021-07-30 08:35:54 -0600

    Improve getTraceName performance

--- packages/jaeger-ui-components/src/model/trace-viewer.tsx
+++ packages/jaeger-ui-components/src/model/trace-viewer.tsx
@@ -12,28 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { memoize } from 'lodash';
+
 import { TraceSpan } from '../types/trace';
 
-type spansDict = { [index: string]: TraceSpan };
+export function _getTraceNameImpl(spans: TraceSpan[]) {
+  // Use a span with no references to another span in given array
+  // prefering the span with the fewest references
+  // using start time as a tie breaker
+  let candidateSpan: TraceSpan | undefined;
+  const allIDs: Set<string> = new Set(spans.map(({ spanID }) => spanID));
 
-export function getTraceName(spans: TraceSpan[]) {
-  const allTraceSpans: spansDict = spans.reduce((dict, span) => ({ ...dict, [span.spanID]: span }), {});
-  const rootSpan = spans
-    .filter((sp) => {
-      if (!sp.references || !sp.references.length) {
-        return true;
-      }
-      const parentIDs = sp.references.filter((r) => r.traceID === sp.traceID).map((r) => r.spanID);
+  for (let i = 0; i < spans.length; i++) {
+    const hasInternalRef =
+      spans[i].references &&
+      spans[i].references.some(({ traceID, spanID }) => traceID === spans[i].traceID && allIDs.has(spanID));
+    if (hasInternalRef) {
+      continue;
+    }
 
-      // returns true if no parent from this trace found
-      return !parentIDs.some((pID) => Boolean(allTraceSpans[pID]));
-    })
-    .sort((sp1, sp2) => {
-      const sp1ParentsNum = sp1.references ? sp1.references.length : 0;
-      const sp2ParentsNum = sp2.references ? sp2.references.length : 0;
+    if (!candidateSpan) {
+      candidateSpan = spans[i];
+      continue;
+    }
 
-      return sp1ParentsNum - sp2ParentsNum || sp1.startTime - sp2.startTime;
-    })[0];
+    const thisRefLength = (spans[i].references && spans[i].references.length) || 0;
+    const candidateRefLength = (candidateSpan.references && candidateSpan.references.length) || 0;
 
-  return rootSpan ? `${rootSpan.process.serviceName}: ${rootSpan.operationName}` : '';
+    if (
+      thisRefLength < candidateRefLength ||
+      (thisRefLength === candidateRefLength && spans[i].startTime < candidateSpan.startTime)
+    ) {
+      candidateSpan = spans[i];
+    }
+  }
+  return candidateSpan ? `${candidateSpan.process.serviceName}: ${candidateSpan.operationName}` : '';
 }
+
+export const getTraceName = memoize(_getTraceNameImpl, (spans: TraceSpan[]) => {
+  if (!spans.length) {
+    return 0;
+  }
+  return spans[0].traceID;
+});
