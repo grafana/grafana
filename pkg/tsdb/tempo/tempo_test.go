f531a9eacc68394a351594dc8da71e9006a83ddb
Ida Furjesova <ida.furjesova@grafana.com>
2021-08-02 10:27:37 +0200

    Refactor tempo using SDK contracts

--- pkg/tsdb/tempo/tempo_test.go
+++ pkg/tsdb/tempo/tempo_test.go
@@ -2,23 +2,20 @@ package tempo
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/infra/httpclient"
-	"github.com/grafana/grafana/pkg/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTempo(t *testing.T) {
-	plug, err := New(httpclient.NewProvider())(&models.DataSource{})
-	executor := plug.(*tempoExecutor)
-	require.NoError(t, err)
-
 	t.Run("createRequest should set Auth header when basic auth is true ", func(t *testing.T) {
-		req, err := executor.createRequest(context.Background(), &models.DataSource{BasicAuth: true, BasicAuthUser: "john", BasicAuthPassword: "pass"}, "traceID")
+		service := &Service{}
+		req, err := service.createRequest(context.Background(), &datasourceInfo{}, "traceID") //&models.DataSource{BasicAuth: true, BasicAuthUser: "john", BasicAuthPassword: "pass"}, "traceID")
 		require.NoError(t, err)
-		assert.Equal(t, 2, len(req.Header))
-		assert.NotEqual(t, req.Header.Get("Authorization"), "")
+		fmt.Println(req.Body)
+		assert.Equal(t, 1, len(req.Header))
+		// assert.NotEqual(t, req.Header.Get("Authorization"), "")
 	})
 }
