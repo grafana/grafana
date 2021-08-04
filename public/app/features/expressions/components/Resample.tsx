8aa3845f70c5e3c47997ec5b7305c102fe717437
Ashley Harrison <ashley.harrison@grafana.com>
2021-08-04 15:47:53 +0100

    Select: Make portalling the menu opt-in, but opt-in *everywhere* (#37501)
    * Select: Don't portal by default
    
    * Select: Portal all the Selects
    
    * Fix indendentation in this comment
    
    * Select: Remove @example docs until formatting is correct
    
    * Docs: Add some documentation for the Select changes
    
    * Update docs/sources/whatsnew/whats-new-in-v8-1.md
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Update docs/sources/whatsnew/whats-new-in-v8-1.md
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Update packages/grafana-ui/src/components/Select/types.ts
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Update public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Update public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Update public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Update public/app/core/components/TransformersUI/prepareTimeSeries/PrepareTimeSeriesEditor.tsx
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    * Docs: Variants instead of varients
    
    * Update public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>
    
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>

--- public/app/features/expressions/components/Resample.tsx
+++ public/app/features/expressions/components/Resample.tsx
@@ -34,7 +34,7 @@ export const Resample: FC<Props> = ({ labelWidth, onChange, refIds, query }) => 
     <>
       <InlineFieldRow>
         <InlineField label="Input" labelWidth={labelWidth}>
-          <Select onChange={onRefIdChange} options={refIds} value={query.expression} width={20} />
+          <Select menuShouldPortal onChange={onRefIdChange} options={refIds} value={query.expression} width={20} />
         </InlineField>
       </InlineFieldRow>
       <InlineFieldRow>
@@ -42,10 +42,22 @@ export const Resample: FC<Props> = ({ labelWidth, onChange, refIds, query }) => 
           <Input onChange={onWindowChange} value={query.window} width={15} />
         </InlineField>
         <InlineField label="Downsample">
-          <Select options={downsamplingTypes} value={downsampler} onChange={onSelectDownsampler} width={25} />
+          <Select
+            menuShouldPortal
+            options={downsamplingTypes}
+            value={downsampler}
+            onChange={onSelectDownsampler}
+            width={25}
+          />
         </InlineField>
         <InlineField label="Upsample">
-          <Select options={upsamplingTypes} value={upsampler} onChange={onSelectUpsampler} width={25} />
+          <Select
+            menuShouldPortal
+            options={upsamplingTypes}
+            value={upsampler}
+            onChange={onSelectUpsampler}
+            width={25}
+          />
         </InlineField>
       </InlineFieldRow>
     </>
