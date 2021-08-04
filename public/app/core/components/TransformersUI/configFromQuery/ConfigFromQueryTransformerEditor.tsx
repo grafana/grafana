8ae7fa566b78be14e6c300c13856372dd2111adb
Ashley Harrison <ashley.harrison@grafana.com>
2021-08-04 13:21:39 +0100

    Update public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
    Co-authored-by: Ursula Kallio <73951760+osg-grafana@users.noreply.github.com>

--- public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
+++ public/app/core/components/TransformersUI/configFromQuery/ConfigFromQueryTransformerEditor.tsx
@@ -93,7 +93,7 @@ export const configFromQueryTransformRegistryItem: TransformerRegistryItem<Confi
   help: `
 ### Use cases
 
-This transformation allow you select one query and from it extract standard options like
+This transformation allows you select one query and from it extract standard options such as 
 **Min**, **Max**, **Unit** and **Thresholds** and apply it to other query results.
 This enables dynamic query driven visualization configuration.
 
