7d530cb870f46c04267ad0ff7c6d332a6207f988
Olof Bourghardt <ob655088@gmail.com>
2021-08-04 15:08:01 +0200

    Update public/app/features/explore/Explore.tsx
    Co-authored-by: Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>

--- public/app/features/explore/Explore.tsx
+++ public/app/features/explore/Explore.tsx
@@ -219,7 +219,7 @@ export class Explore extends React.PureComponent<Props, ExploreState> {
 
   renderLogsPanel(width: number) {
     const { exploreId, syncedTimes, theme } = this.props;
-    const spacing = parseInt(theme.spacing(1).slice(0, -2), 10);
+    const spacing = parseInt(theme.spacing(2).slice(0, -2), 10);
     return (
       <LogsContainer
         exploreId={exploreId}
