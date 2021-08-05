faaf2ed354d273c28e87b371c574b6caef987839
Piotr Jamróz <pm.jamroz@gmail.com>
2021-08-05 15:34:44 +0200

    Fix API Extractor warnings

--- packages/grafana-ui/src/components/Section/Section.tsx
+++ packages/grafana-ui/src/components/Section/Section.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { SectionFill } from './SectionFill';
 import { SectionLabel } from './SectionLabel';
 
-/**
- * @alpha
- */
-export const Section = ({
-  label,
-  children,
-  fill,
-}: {
+export interface SectionProps {
   label: string;
   children: React.ReactNode;
   inline?: boolean;
   fill?: boolean;
-}) => (
+}
+
+/**
+ * @alpha
+ */
+export const Section = ({ label, children, fill }: SectionProps) => (
   <div className="gf-form-inline">
     <div className="gf-form">
       <SectionLabel name={label} className="width-7" />
--- packages/grafana-ui/src/components/Section/SectionLabel.tsx
+++ packages/grafana-ui/src/components/Section/SectionLabel.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { cx } from '@emotion/css';
 
-type Props = {
+export interface SectionLabelProps {
   name: string;
   className?: string;
-};
+}
 
 /**
  * @alpha
  */
-export const SectionLabel = ({ name, className }: Props) => (
+export const SectionLabel = ({ name, className }: SectionLabelProps) => (
   <label className={cx('gf-form-label query-keyword', className)}>{name}</label>
 );
