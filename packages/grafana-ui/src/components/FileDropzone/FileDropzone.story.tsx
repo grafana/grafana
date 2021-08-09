95bc889b48eee16388104957da2687626c2004bf
Zoltán Bedi <zoltan.bedi@gmail.com>
2021-08-09 11:59:21 +0200

    UI: FileDropzone component to handle file list overwrite

--- packages/grafana-ui/src/components/FileDropzone/FileDropzone.story.tsx
+++ packages/grafana-ui/src/components/FileDropzone/FileDropzone.story.tsx
@@ -21,3 +21,7 @@ export default {
 export const Basic: Story<FileDropzoneProps> = (args) => {
   return <FileDropzone {...args} />;
 };
+
+export const WithCustomFileList: Story<FileDropzoneProps> = () => {
+  return <FileDropzone fileListRenderer={(file) => <div>{file.file.name}</div>} />;
+};
