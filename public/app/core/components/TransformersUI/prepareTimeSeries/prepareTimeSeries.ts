0efa47348d4216f8625afde88875a83d3ecc160d
Ryan McKinley <ryantxu@gmail.com>
2021-08-11 01:25:33 -0700

    Transformations: support a synchronous transformation pattern (#37780)

--- public/app/core/components/TransformersUI/prepareTimeSeries/prepareTimeSeries.ts
+++ public/app/core/components/TransformersUI/prepareTimeSeries/prepareTimeSeries.ts
@@ -1,5 +1,5 @@
 import {
-  DataTransformerInfo,
+  SynchronousDataTransformerInfo,
   DataFrame,
   FieldType,
   DataTransformerID,
@@ -63,31 +63,30 @@ export function toTimeSeriesMany(data: DataFrame[]): DataFrame[] {
   return result;
 }
 
-export function prepareTimeSeries(data: DataFrame[], options: PrepareTimeSeriesOptions): DataFrame[] {
-  const format = options?.format ?? timeSeriesFormat.TimeSeriesWide;
-  if (format === timeSeriesFormat.TimeSeriesMany) {
-    return toTimeSeriesMany(data);
-  }
-
-  // Join by the first frame
-  const frame = outerJoinDataFrames({
-    frames: data,
-    joinBy: fieldMatchers.get(FieldMatcherID.firstTimeField).get({}),
-    enforceSort: true,
-    keepOriginIndices: true,
-  });
-  return frame ? [frame] : [];
-}
-
-export const prepareTimeSeriesTransformer: DataTransformerInfo<PrepareTimeSeriesOptions> = {
+export const prepareTimeSeriesTransformer: SynchronousDataTransformerInfo<PrepareTimeSeriesOptions> = {
   id: DataTransformerID.prepareTimeSeries,
   name: 'Prepare time series',
   description: `Will stretch data frames from the wide format into the long format. This is really helpful to be able to keep backwards compatability for panels not supporting the new wide format.`,
   defaultOptions: {},
 
-  /**
-   * Return a modified copy of the series.  If the transform is not or should not
-   * be applied, just return the input series
-   */
-  operator: (options) => (source) => source.pipe(map((data) => prepareTimeSeries(data, options))),
+  operator: (options) => (source) =>
+    source.pipe(map((data) => prepareTimeSeriesTransformer.transformer(options)(data))),
+
+  transformer: (options: PrepareTimeSeriesOptions) => {
+    const format = options?.format ?? timeSeriesFormat.TimeSeriesWide;
+    if (format === timeSeriesFormat.TimeSeriesMany) {
+      return toTimeSeriesMany;
+    }
+
+    return (data: DataFrame[]) => {
+      // Join by the first frame
+      const frame = outerJoinDataFrames({
+        frames: data,
+        joinBy: fieldMatchers.get(FieldMatcherID.firstTimeField).get({}),
+        enforceSort: true,
+        keepOriginIndices: true,
+      });
+      return frame ? [frame] : [];
+    };
+  },
 };
