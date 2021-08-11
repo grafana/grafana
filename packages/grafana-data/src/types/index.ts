be9d79ef1116b3690297f9826c36991fbcbd3edf
Ryan McKinley <ryantxu@gmail.com>
2021-08-11 00:23:41 -0700

    Chore: define base panel type in @grafana/data (#37766)

--- packages/grafana-data/src/types/index.ts
+++ packages/grafana-data/src/types/index.ts
@@ -2,6 +2,7 @@ export * from './data';
 export * from './dataFrame';
 export * from './dataLink';
 export * from './dashboard';
+export * from './query';
 export * from './annotations';
 export * from './logs';
 export * from './navModel';
