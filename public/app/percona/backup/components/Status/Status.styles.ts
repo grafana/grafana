ab2a3cf37e27c4c19798098c62e9be0d91568017
fabio-silva <ffjs1993@gmail.com>
2021-08-12 11:05:30 +0100

    PMM-8267: backup status loading animation (#208)
    * PMM-8267: add Ellipsis component
    
    * PMM-8267 useMemo to avoid form cleanup
    
    * PMM-8267 decrease poll interval
    
    * PMM-8267 style ellipsis on status column
    
    * PMM-8267 add tests to Status
    
    * PMM-8267 add className to svg
    
    * PMM-8267 change status styling logic

--- public/app/percona/backup/components/Status/Status.styles.ts
+++ public/app/percona/backup/components/Status/Status.styles.ts
@@ -1,26 +1,16 @@
 import { GrafanaTheme } from '@grafana/data';
-import { stylesFactory } from '@grafana/ui';
-import { BackupStatus, RestoreStatus } from '../../Backup.types';
 import { css } from 'emotion';
 
-export const getStyles = stylesFactory((theme: GrafanaTheme, status: BackupStatus | RestoreStatus) => {
-  const successfulStates = [BackupStatus.BACKUP_STATUS_SUCCESS, RestoreStatus.RESTORE_STATUS_SUCCESS];
-  const errorStates = [
-    BackupStatus.BACKUP_STATUS_ERROR,
-    RestoreStatus.RESTORE_STATUS_ERROR,
-    BackupStatus.BACKUP_STATUS_INVALID,
-    RestoreStatus.RESTORE_STATUS_INVALID,
-  ];
-  const isSuccess = successfulStates.includes(status);
-  const isError = errorStates.includes(status);
-
-  if (isSuccess || isError) {
-    return {
-      status: css`
-        color: ${isSuccess ? theme.palette.greenBase : theme.palette.redBase};
-      `,
-    };
-  }
-
-  return {};
+export const getStyles = ({ palette }: GrafanaTheme) => ({
+  ellipsisContainer: css`
+    display: table;
+    width: 20px;
+    margin: 0 auto;
+  `,
+  statusSuccess: css`
+    color: ${palette.greenBase};
+  `,
+  statusError: css`
+    color: ${palette.redBase};
+  `,
 });
---
+++ public/app/percona/shared/components/Elements/Icons/Ellipsis.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import { SvgProps } from './Icons.types';
+import { getStyles } from './Ellipsis.styles';
+
+export const Ellipsis: FC<SvgProps> = ({ className, ...rest }) => {
+  const styles = getStyles();
+
+  return (
+    <svg viewBox="0 0 20 15" xmlns="http://www.w3.org/2000/svg" className={className} {...rest}>
+      <g>
+        <path
+          stroke="null"
+          d="m5.61584,12.03388c0,1.50105 -1.21673,2.71779 -2.71779,2.71779s-2.71779,-1.21673 -2.71779,-2.71779s1.21673,-2.71779 2.71779,-2.71779s2.71779,1.21673 2.71779,2.71779zm0,0"
+          fill="currentColor"
+          id="ellipsis-one"
+          className={styles.ellipsis}
+        />
+        <path
+          stroke="null"
+          d="m12.70445,11.97617c0,1.50105 -1.21674,2.71779 -2.71779,2.71779s-2.71779,-1.21674 -2.71779,-2.71779s1.21673,-2.71779 2.71779,-2.71779s2.71779,1.21673 2.71779,2.71779zm0,0"
+          fill="currentColor"
+          id="ellipsis-two"
+          className={styles.ellipsis}
+        />
+        <path
+          stroke="null"
+          d="m19.79306,12.02001c0,1.50105 -1.21674,2.71779 -2.71779,2.71779s-2.71779,-1.21674 -2.71779,-2.71779s1.21673,-2.71779 2.71779,-2.71779s2.71779,1.21673 2.71779,2.71779zm0,0"
+          fill="currentColor"
+          id="ellipsis-three"
+          className={styles.ellipsis}
+        />
+      </g>
+    </svg>
+  );
+};
