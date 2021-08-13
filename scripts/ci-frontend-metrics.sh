066bd3cd5f1262e65845b2df2b69661e0f78162e
Alexandra Vargas <alexa1866@gmail.com>
2021-08-13 12:55:59 +0200

    Fix ci-frontend-metrichs script to remove useles cat

--- scripts/ci-frontend-metrics.sh
+++ scripts/ci-frontend-metrics.sh
@@ -2,7 +2,7 @@
 set -e
 
 ERROR_COUNT="$(./node_modules/.bin/tsc --project tsconfig.json --noEmit --strict true | grep -oP 'Found \K(\d+)')"
-ACCESSIBILITY_ERRORS="$(cat pa11y-ci-results.json | grep -oP '\"errors\":(\d+),' | grep -oP '\d+')"
+ACCESSIBILITY_ERRORS="$(grep -oP '\"errors\":(\d+),' pa11y-ci-results.json | grep -oP '\d+')"
 DIRECTIVES="$(grep -r -o  directive public/app/ | wc -l)"
 CONTROLLERS="$(grep -r -oP 'class .*Ctrl' public/app/ | wc -l)"
 STORIES_COUNT="$(find ./packages/grafana-ui/src/components -name "*.story.tsx" | wc -l)"
