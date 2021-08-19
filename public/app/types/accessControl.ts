3dea1394c900371e6a3c5a7f2e72ec0cd3e69c5a
gamab <gabi.mabs@gmail.com>
2021-08-19 15:29:42 +0200

    defining actions in front

--- public/app/types/accessControl.ts
+++ public/app/types/accessControl.ts
@@ -33,7 +33,13 @@ export enum AccessControlAction {
   LDAPUsersRead = 'ldap.user:read',
   LDAPUsersSync = 'ldap.user:sync',
   LDAPStatusRead = 'ldap.status:read',
+
   DataSourcesExplore = 'datasources:explore',
+  DataSourcesRead = 'datasources:read',
+  DataSourcesCreate = 'datasources:create',
+  DataSourcesWrite = 'datasources:write',
+  DataSourcesDelete = 'datasources:delete',
+  DataSourcesIDRead = 'datasources:id:read',
 
   ActionServerStatsRead = 'server.stats:read',
 }
