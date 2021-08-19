237580d67b64429fada2d4707efa3fec11588f40
billabongrob <billabongrob@gmail.com>
2021-08-19 04:41:26 -0500

    Admin: Obfuscate azure_blob Account Key
    Obfuscate azure_blob Account Key #37970
    
    (cherry picked from commit 2fe62af6855a05bf81b226f6f394f3251687c8f5)

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -471,6 +471,7 @@ func RedactedValue(key, value string) string {
 		"PRIVATE_KEY",
 		"SECRET_KEY",
 		"CERTIFICATE",
+		"ACCOUNT_KEY",
 	} {
 		if strings.Contains(uppercased, pattern) {
 			return RedactedPassword
