a09ed48b1ef4c27c4f2152359eae5a65ef7f65bf
Ryan McKinley <ryantxu@gmail.com>
2021-08-24 11:26:52 -0700

    export schema from data and schema

---
+++ packages/grafana-data/src/schema.ts
@@ -0,0 +1,8 @@
+// Moved to `@grafana/schema`, in Grafana 9, this will be removed
+//---------------------------------------------------------------
+// grafana/grafana/packages/grafana-schema$ grep export src/schema/*.ts
+
+export {
+  // Styles that changed
+  VizTextDisplayOptions as TextDisplayOptions, // rename
+} from '@grafana/schema';
