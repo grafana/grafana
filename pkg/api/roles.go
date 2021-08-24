b02375720623ec4c063ee29db149b2afbe0408cf
gamab <gabi.mabs@gmail.com>
2021-08-24 11:00:16 +0200

    Keeping scope under control not doing proxy requests yet

--- pkg/api/roles.go
+++ pkg/api/roles.go
@@ -14,7 +14,6 @@ const (
 	ActionDatasourcesWrite  = "datasources:write"
 	ActionDatasourcesDelete = "datasources:delete"
 	ActionDatasourcesIDRead = "datasources:id:read"
-	// ActionDatasourcesCheckHealth = "datasources:checkhealth"
 )
 
 // API related scopes
@@ -35,75 +34,59 @@ const (
 // grants to organization roles ("Viewer", "Editor", "Admin") or "Grafana Admin"
 // that HTTPServer needs
 func (hs *HTTPServer) declareFixedRoles() error {
-	provisioningAdmin := accesscontrol.RoleRegistration{
-		Role: accesscontrol.RoleDTO{
-			Version:     1,
-			Name:        "fixed:provisioning:admin",
-			Description: "Reload provisioning configurations",
-			Permissions: []accesscontrol.Permission{
-				{
-					Action: ActionProvisioningReload,
-					Scope:  ScopeProvisionersAll,
+	registrations := []accesscontrol.RoleRegistration{
+		{
+			Role: accesscontrol.RoleDTO{
+				Version:     1,
+				Name:        "fixed:provisioning:admin",
+				Description: "Reload provisioning configurations",
+				Permissions: []accesscontrol.Permission{
+					{
+						Action: ActionProvisioningReload,
+						Scope:  ScopeProvisionersAll,
+					},
 				},
 			},
+			Grants: []string{accesscontrol.RoleGrafanaAdmin},
 		},
-		Grants: []string{accesscontrol.RoleGrafanaAdmin},
-	}
-
-	datasourcesAdmin := accesscontrol.RoleRegistration{
-		Role: accesscontrol.RoleDTO{
-			Version:     1,
-			Name:        "fixed:datasources:admin",
-			Description: "Gives access to create, read, update, delete datasources",
-			Permissions: []accesscontrol.Permission{
-				{
-					Action: ActionDatasourcesRead,
-					Scope:  ScopeDatasourcesAll,
-				},
-				{
-					Action: ActionDatasourcesWrite,
-					Scope:  ScopeDatasourcesAll,
-				},
-				{Action: ActionDatasourcesCreate},
-				{
-					Action: ActionDatasourcesDelete,
-					Scope:  ScopeDatasourcesAll,
+		{
+			Role: accesscontrol.RoleDTO{
+				Version:     1,
+				Name:        "fixed:datasources:admin",
+				Description: "Gives access to create, read, update, delete datasources",
+				Permissions: []accesscontrol.Permission{
+					{
+						Action: ActionDatasourcesRead,
+						Scope:  ScopeDatasourcesAll,
+					},
+					{
+						Action: ActionDatasourcesWrite,
+						Scope:  ScopeDatasourcesAll,
+					},
+					{Action: ActionDatasourcesCreate},
+					{
+						Action: ActionDatasourcesDelete,
+						Scope:  ScopeDatasourcesAll,
+					},
 				},
 			},
+			Grants: []string{string(models.ROLE_ADMIN)},
 		},
-		Grants: []string{string(models.ROLE_ADMIN)},
-	}
-
-	datasourcesIDViewer := accesscontrol.RoleRegistration{
-		Role: accesscontrol.RoleDTO{
-			Version:     1,
-			Name:        "fixed:datasources:id:viewer",
-			Description: "Gives access to read datasources ID",
-			Permissions: []accesscontrol.Permission{
-				{
-					Action: ActionDatasourcesIDRead,
-					Scope:  ScopeDatasourcesAll,
+		{
+			Role: accesscontrol.RoleDTO{
+				Version:     1,
+				Name:        "fixed:datasources:id:viewer",
+				Description: "Gives access to read datasources ID",
+				Permissions: []accesscontrol.Permission{
+					{
+						Action: ActionDatasourcesIDRead,
+						Scope:  ScopeDatasourcesAll,
+					},
 				},
 			},
+			Grants: []string{string(models.ROLE_VIEWER)},
 		},
-		Grants: []string{string(models.ROLE_VIEWER)},
 	}
 
-	// TODO protect health check
-	// datasourcesHealthChecker := accesscontrol.RoleRegistration{
-	// 	Role: accesscontrol.RoleDTO{
-	// 		Version:     1,
-	// 		Name:        "fixed:datasources:health:checker",
-	// 		Description: "Gives access to check datasources health",
-	// 		Permissions: []accesscontrol.Permission{
-	// 			{
-	// 				Action: ActionDatasourcesCheckHealth,
-	// 				Scope:  ScopeDatasourcesAll,
-	// 			},
-	// 		},
-	// 	},
-	// 	Grants: []string{string(models.ROLE_VIEWER)},
-	// }
-
-	return hs.AccessControl.DeclareFixedRoles(provisioningAdmin, datasourcesAdmin, datasourcesIDViewer)
+	return hs.AccessControl.DeclareFixedRoles(registrations...)
 }
