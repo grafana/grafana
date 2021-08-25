fa1ca3b77ef18b4e5b504706f15c807e046d90d8
Erik Sundell <erik.sundell87@gmail.com>
2021-08-25 14:05:26 +0200

    add backend

---
+++ public/app/core/services/echo/backends/analytics/ApplicationInsightsBackend.ts
@@ -0,0 +1,46 @@
+import $ from 'jquery';
+import { EchoBackend, EchoEventType, isInteractionEvent, isPageviewEvent, PageviewEchoEvent } from '@grafana/runtime';
+
+export interface ApplicationInsightsBackendOptions {
+  instrumentationKey: string;
+}
+
+export class ApplicationInsightsBackend implements EchoBackend<PageviewEchoEvent, ApplicationInsightsBackendOptions> {
+  supportedEvents = [EchoEventType.Pageview];
+
+  constructor(public options: ApplicationInsightsBackendOptions) {
+    $.ajax({
+      url: 'https://js.monitor.azure.com/scripts/b/ai.2.min.js',
+      dataType: 'script',
+      cache: true,
+    }).done(function () {
+      var snippet = {
+        config: {
+          instrumentationKey: options.instrumentationKey,
+        },
+      };
+      var init = new (window as any).Microsoft.ApplicationInsights.ApplicationInsights(snippet);
+      (window as any).applicationInsights = init.loadAppInsights();
+    });
+  }
+
+  addEvent = (e: PageviewEchoEvent) => {
+    if (!(window as any).applicationInsights) {
+      return;
+    }
+
+    if (isPageviewEvent(e)) {
+      (window as any).applicationInsights.trackPageView();
+    }
+
+    if (isInteractionEvent(e)) {
+      (window as any).applicationInsights.trackPageView({
+        name: e.payload.interactionName,
+        properties: e.payload.properties,
+      });
+    }
+  };
+
+  // Not using Echo buffering, addEvent above sends events to Application Insights as soon as they appear
+  flush = () => {};
+}
