53f7f4b2805531d0698145b9b85c2e9c032d2d85
Santiago Hernández <santiagohernandez.1997@gmail.com>
2021-08-25 11:37:02 -0300

    Alerting: (wip) numeric template functions

--- pkg/services/ngalert/state/cache_test.go
+++ pkg/services/ngalert/state/cache_test.go
@@ -109,7 +109,182 @@ func TestExpandTemplate(t *testing.T) {
 			EvaluationString: "invalid",
 		},
 		expectedError: errors.New(`error executing template __alert_test: template: __alert_test:1:79: executing "__alert_test" at <humanize $value>: error calling humanize: strconv.ParseFloat: parsing "invalid": invalid syntax`),
-	}}
+	}, {
+		name: "humanize1024 float64",
+		text: "{{ range $key, $val := $values }}{{ humanize1024 .Value }}:{{ end }}",
+		alertInstance: eval.Result{
+			Values: map[string]eval.NumberValueCapture{
+				"A": {
+					Var:    "A",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(0.0),
+				},
+				"B": {
+					Var:    "B",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(1.0),
+				},
+				"C": {
+					Var:    "C",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(1048576.0),
+				},
+				"D": {
+					Var:    "D",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(.12),
+				},
+			},
+		},
+		expected: "0:1:1Mi:0.12:",
+	}, {
+		name: "humanize1024 string with error",
+		text: "{{ humanize1024 $value }}",
+		alertInstance: eval.Result{
+			EvaluationString: "invalid",
+		},
+		expectedError: errors.New(`error executing template __alert_test: template: __alert_test:1:79: executing "__alert_test" at <humanize1024 $value>: error calling humanize1024: strconv.ParseFloat: parsing "invalid": invalid syntax`),
+	}, {
+		name: "humanizeDuration - seconds - float64",
+		text: "{{ range $key, $val := $values }}{{ humanizeDuration .Value }}:{{ end }}",
+		alertInstance: eval.Result{
+			Values: map[string]eval.NumberValueCapture{
+				"A": {
+					Var:    "A",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(0),
+				},
+				"B": {
+					Var:    "B",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(1),
+				},
+				"C": {
+					Var:    "C",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(60),
+				},
+				"D": {
+					Var:    "D",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(3600),
+				},
+				"E": {
+					Var:    "E",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(86400),
+				},
+				"F": {
+					Var:    "F",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(86400 + 3600),
+				},
+				"G": {
+					Var:    "G",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(-(86400*2 + 3600*3 + 60*4 + 5)),
+				},
+				"H": {
+					Var:    "H",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(899.99),
+				},
+			},
+		},
+		expected: "0s:1s:1m 0s:1h 0m 0s:1d 0h 0m 0s:1d 1h 0m 0s:-2d 3h 4m 5s:14m 59s:",
+	}, {
+		name: "humanizeDuration - string",
+		text: "{{ humanizeDuration $value }}",
+		alertInstance: eval.Result{
+			EvaluationString: "86400",
+		},
+		expected: "1d 0h 0m 0s",
+	}, {
+		name: "humanizeDuration - subsecond and fractional seconds - float64",
+		text: "{{ range $key, $val := $values }}{{ humanizeDuration .Value }}:{{ end }}",
+		alertInstance: eval.Result{
+			Values: map[string]eval.NumberValueCapture{
+				"A": {
+					Var:    "A",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(.1),
+				},
+				"B": {
+					Var:    "B",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(.0001),
+				},
+				"C": {
+					Var:    "C",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(.12345),
+				},
+				"D": {
+					Var:    "D",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(60.1),
+				},
+				"E": {
+					Var:    "E",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(60.5),
+				},
+				"F": {
+					Var:    "F",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(1.2345),
+				},
+				"G": {
+					Var:    "G",
+					Labels: data.Labels{},
+					Value:  ptr.Float64(12.345),
+				},
+			},
+		},
+		expected: "100ms:100us:123.5ms:1m 0s:1m 0s:1.234s:12.35s:",
+	}, {
+		name: "humanizeDuration - subsecond - string",
+		text: "{{ humanizeDuration $value }}",
+		alertInstance: eval.Result{
+			EvaluationString: ".0001",
+		},
+		expected: "100us",
+	}, {
+		name: "humanizeDuration - fractional seconds - string",
+		text: "{{ humanizeDuration $value }}",
+		alertInstance: eval.Result{
+			EvaluationString: "1.2345",
+		},
+		expected: "1.234s",
+	}, {
+		name: "humanizeDuration - string with error",
+		text: `{{ humanizeDuration $value }}`,
+		alertInstance: eval.Result{
+			EvaluationString: "invalid",
+		},
+		expectedError: errors.New(`error executing template __alert_test: template: __alert_test:1:79: executing "__alert_test" at <humanizeDuration $value>: error calling humanizeDuration: strconv.ParseFloat: parsing "invalid": invalid syntax`),
+	}, {
+		name:     "humanizePercentage - float64",
+		text:     "{{ -0.22222 | humanizePercentage }}:{{ 0.0 | humanizePercentage }}:{{ 0.1234567 | humanizePercentage }}:{{ 1.23456 | humanizePercentage }}",
+		expected: "-22.22%:0%:12.35%:123.5%",
+	}, {
+		name:     "humanizePercentage - string",
+		text:     `{{ "-0.22222" | humanizePercentage }}:{{ "0.0" | humanizePercentage }}:{{ "0.1234567" | humanizePercentage }}:{{ "1.23456" | humanizePercentage }}`,
+		expected: "-22.22%:0%:12.35%:123.5%",
+	}, {
+		name:          "humanizePercentage - string with error",
+		text:          `{{ "invalid" | humanizePercentage }}`,
+		expectedError: errors.New(`error executing template __alert_test: template: __alert_test:1:91: executing "__alert_test" at <humanizePercentage>: error calling humanizePercentage: strconv.ParseFloat: parsing "invalid": invalid syntax`),
+	}, {
+		name:     "humanizeTimestamp - float64",
+		text:     "{{ 1435065584.128 | humanizeTimestamp }}",
+		expected: "2015-06-23 13:19:44.128 +0000 UTC",
+	}, {
+		name:     "humanizeTimestamp - string",
+		text:     `{{ "1435065584.128" | humanizeTimestamp }}`,
+		expected: "2015-06-23 13:19:44.128 +0000 UTC",
+	},
+	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
--- pkg/services/ngalert/state/funcmap.go
+++ pkg/services/ngalert/state/funcmap.go
@@ -5,10 +5,15 @@ import (
 	"math"
 	"strconv"
 	text_template "text/template"
+	"time"
 )
 
 var funcMap = text_template.FuncMap{
-	"humanize": humanize,
+	"humanize":           humanize,
+	"humanize1024":       humanize1024,
+	"humanizeDuration":   humanizeDuration,
+	"humanizePercentage": humanizePercentage,
+	"humanizeTimestamp":  humanizeTimestamp,
 }
 
 func humanize(i interface{}) (string, error) {
@@ -41,8 +46,96 @@ func humanize(i interface{}) (string, error) {
 	return fmt.Sprintf("%.4g%s", v, prefix), nil
 }
 
+func humanize1024(i interface{}) (string, error) {
+	v, err := convertToFloat(i)
+	if err != nil {
+		return "", err
+	}
+	if math.Abs(v) <= 1 || math.IsNaN(v) || math.IsInf(v, 0) {
+		return fmt.Sprintf("%.4g", v), nil
+	}
+	prefix := ""
+	for _, p := range []string{"ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi"} {
+		if math.Abs(v) < 1024 {
+			break
+		}
+		prefix = p
+		v /= 1024
+	}
+	return fmt.Sprintf("%.4g%s", v, prefix), nil
+}
+
+func humanizeDuration(i interface{}) (string, error) {
+	v, err := convertToFloat(i)
+	if err != nil {
+		return "", err
+	}
+	if math.IsNaN(v) || math.IsInf(v, 0) {
+		return fmt.Sprintf("%.4g", v), nil
+	}
+	if v == 0 {
+		return fmt.Sprintf("%.4gs", v), nil
+	}
+	if math.Abs(v) >= 1 {
+		sign := ""
+		if v < 0 {
+			sign = "-"
+			v = -v
+		}
+		seconds := int64(v) % 60
+		minutes := (int64(v) / 60) % 60
+		hours := (int64(v) / 60 / 60) % 24
+		days := int64(v) / 60 / 60 / 24
+		// For days to minutes, we display seconds as an integer.
+		if days != 0 {
+			return fmt.Sprintf("%s%dd %dh %dm %ds", sign, days, hours, minutes, seconds), nil
+		}
+		if hours != 0 {
+			return fmt.Sprintf("%s%dh %dm %ds", sign, hours, minutes, seconds), nil
+		}
+		if minutes != 0 {
+			return fmt.Sprintf("%s%dm %ds", sign, minutes, seconds), nil
+		}
+		// For seconds, we display 4 significant digits.
+		return fmt.Sprintf("%s%.4gs", sign, v), nil
+	}
+	prefix := ""
+	for _, p := range []string{"m", "u", "n", "p", "f", "a", "z", "y"} {
+		if math.Abs(v) >= 1 {
+			break
+		}
+		prefix = p
+		v *= 1000
+	}
+	return fmt.Sprintf("%.4g%ss", v, prefix), nil
+}
+
+func humanizePercentage(i interface{}) (string, error) {
+	v, err := convertToFloat(i)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%.4g%%", v*100), nil
+}
+
+func humanizeTimestamp(i interface{}) (string, error) {
+	v, err := convertToFloat(i)
+	if err != nil {
+		return "", err
+	}
+	if math.IsNaN(v) || math.IsInf(v, 0) {
+		return fmt.Sprintf("%.4g", v), nil
+	}
+
+	ms := (int64(v*1000) % 1000) * int64(time.Millisecond)
+	t := time.Unix(int64(v), ms).UTC()
+	return fmt.Sprint(t), nil
+}
+
 func convertToFloat(i interface{}) (float64, error) {
 	switch v := i.(type) {
+	case *float64:
+		return *v, nil
 	case float64:
 		return v, nil
 	case string:
