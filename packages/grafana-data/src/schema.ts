821b94d590394f3c939d54ddb62839f9c8883ba6
Ryan McKinley <ryantxu@gmail.com>
2021-08-25 08:32:48 -0700

    Update packages/grafana-data/src/schema.ts
    Co-authored-by: Marcus Andersson <marcus.andersson@grafana.com>

--- packages/grafana-data/src/schema.ts
+++ packages/grafana-data/src/schema.ts
@@ -1,7 +1,10 @@
 // Moved to `@grafana/schema`, in Grafana 9, this will be removed
 //---------------------------------------------------------------
 // grafana/grafana/packages/grafana-schema$ grep export src/schema/*.ts
-
+/**
+ * @deprecated Moved to `@grafana/schema` package and will be removed in Grafana 9
+ * @public
+ */
 export {
   // Styles that changed
   VizTextDisplayOptions as TextDisplayOptions, // rename
