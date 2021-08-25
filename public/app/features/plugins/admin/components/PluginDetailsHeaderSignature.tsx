eb4c9a26680a13ac9d66c3713abbe976340e836d
Levente Balogh <balogh.levente.hu@gmail.com>
2021-08-25 16:33:23 +0200

    feat: add a component for displaying signature info in details header

---
+++ public/app/features/plugins/admin/components/PluginDetailsHeaderSignature.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { GrafanaPlugin, PluginMeta, PluginSignatureStatus } from '@grafana/data';
+import { PluginSignatureBadge } from '@grafana/ui';
+import { PluginSignatureDetailsBadge } from './PluginSignatureDetailsBadge';
+
+type Props = {
+  className?: string;
+  plugin: GrafanaPlugin<PluginMeta<{}>>;
+};
+
+// Designed to show plugin signature information in the header on the plugin's details page
+export const PluginDetailsHeaderSignature = ({ plugin, className }: Props) => {
+  const isSignatureValid = plugin.meta.signature === PluginSignatureStatus.valid;
+
+  return (
+    <div>
+      <a href="https://grafana.com/docs/grafana/latest/plugins/plugin-signatures/" target="_blank" rel="noreferrer">
+        <PluginSignatureBadge status={plugin.meta.signature} />
+      </a>
+
+      {isSignatureValid && (
+        <PluginSignatureDetailsBadge
+          signatureType={plugin.meta.signatureType}
+          signatureOrg={plugin.meta.signatureOrg}
+        />
+      )}
+    </div>
+  );
+};
