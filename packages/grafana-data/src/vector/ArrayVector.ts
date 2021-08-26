1c39a7078e19c5fffaaa8683004211acf5d49ace
Leon Sorokin <leeoniya@gmail.com>
2021-08-26 12:13:22 -0500

    ArrayVector polyfill

--- packages/grafana-data/src/vector/ArrayVector.ts
+++ packages/grafana-data/src/vector/ArrayVector.ts
@@ -41,3 +41,55 @@ export class ArrayVector<T = any> extends FunctionalVector<T> implements Mutable
     return this.buffer;
   }
 }
+
+/*
+// WIP polyfill for replacing ArrayVector() with a plain array
+// https://jsfiddle.net/7gfwqrmt/1/
+// TODO: typings kung fu
+
+// JS original sin
+Object.assign(Array.prototype, {
+	get(idx) {
+    return this[idx];
+  },
+  set(idx, value) {
+  	this[idx] = value;
+  },
+  add(value) {
+  	this.push(value);
+  },
+  get(idx) {
+  	return this[idx];
+  },
+  toArray() {
+  	return this;
+  },
+  toJSON() {
+  	return this;
+  }
+});
+
+Object.defineProperty(Array.prototype, 'buffer', {
+	get: function() { return this; }
+});
+
+let notified = false;
+let notice = "ArrayVector is deprecated and will be removed in Grafana 9. Please migrate to plain arrays for field.values."
+
+function ArrayVector(arr) {
+	if (!notified) {
+  	console.error(notice);
+  	notified = true;
+  }
+  return arr || [];
+}
+
+
+let arv = new ArrayVector([12,13,14]);
+
+console.log(arv);
+console.log(arv.get(0));
+console.log(arv.buffer);
+console.log(Array.isArray(arv));
+console.log(arv.slice());
+*/
