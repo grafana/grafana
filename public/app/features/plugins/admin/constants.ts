a74f51eb0a133afaad2d276c7050d895465cb0e8
Levente Balogh <balogh.levente.hu@gmail.com>
2021-08-26 12:09:31 +0200

    refactor(Plugins): use an enum instead of an object

--- public/app/features/plugins/admin/constants.ts
+++ public/app/features/plugins/admin/constants.ts
@@ -1,8 +1,8 @@
 export const API_ROOT = '/api/plugins';
 export const GRAFANA_API_ROOT = '/api/gnet';
-export const PLUGIN_TAB_LABELS = {
-  OVERVIEW: 'Overview',
-  VERSIONS: 'Version history',
-  CONFIG: 'Config',
-  DASHBOARDS: 'Dashboards',
-};
+export enum PLUGIN_TAB_LABELS {
+  OVERVIEW = 'Overview',
+  VERSIONS = 'Version history',
+  CONFIG = 'Config',
+  DASHBOARDS = 'Dashboards',
+}
