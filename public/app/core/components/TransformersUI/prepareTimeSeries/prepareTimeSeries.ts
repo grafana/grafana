131280f2ff9dd8adc319dd3cff1b83c1c0078de8
Douglas Thrift <dthrift@flexera.com>
2021-08-26 16:39:47 -0700

    Transformations: Prepare time series Long progress

--- public/app/core/components/TransformersUI/prepareTimeSeries/prepareTimeSeries.ts
+++ public/app/core/components/TransformersUI/prepareTimeSeries/prepareTimeSeries.ts
@@ -7,6 +7,8 @@ import {
   outerJoinDataFrames,
   fieldMatchers,
   FieldMatcherID,
+  Field,
+  MutableDataFrame,
 } from '@grafana/data';
 import { map } from 'rxjs/operators';
 
@@ -67,6 +69,139 @@ export function toTimeSeriesMany(data: DataFrame[]): DataFrame[] {
   return result;
 }
 
+export function toTimeSeriesLong(data: DataFrame[]): DataFrame[] {
+  if (!Array.isArray(data) || data.length === 0) {
+    return data;
+  }
+
+  const result: DataFrame[] = [];
+  for (const frame of data) {
+    let timeField: Field | undefined;
+    const uniqueValueNames: string[] = [];
+    const uniqueValueNamesToType: Record<string, FieldType> = {};
+    const uniqueLabelKeys: Record<string, boolean> = {};
+    const labelKeyToWideIndices: Record<string, number[]> = {};
+    const uniqueFactorNamesToWideIndex: Record<string, number> = {};
+
+    for (let fieldIndex = 0; fieldIndex < frame.fields.length; fieldIndex++) {
+      const field = frame.fields[fieldIndex];
+
+      switch (field.type) {
+        case FieldType.string:
+        case FieldType.boolean:
+          if (field.name in uniqueFactorNamesToWideIndex) {
+            // TODO error?
+          } else {
+            uniqueFactorNamesToWideIndex[field.name] = fieldIndex;
+            uniqueLabelKeys[field.name] = true;
+          }
+          break;
+        case FieldType.time:
+          if (!timeField) {
+            timeField = field;
+            break;
+          }
+        default:
+          if (field.name in uniqueValueNamesToType) {
+            const type = uniqueValueNamesToType[field.name];
+
+            if (field.type !== type) {
+              // TODO error?
+              continue;
+            }
+          } else {
+            uniqueValueNamesToType[field.name] = field.type;
+            uniqueValueNames.push(field.name);
+          }
+
+          const tKey = JSON.stringify(field.labels);
+          const wideIndices = labelKeyToWideIndices[tKey];
+
+          if (wideIndices !== undefined) {
+            wideIndices.push(fieldIndex);
+          } else {
+            labelKeyToWideIndices[tKey] = [fieldIndex];
+          }
+
+          if (field.labels != null) {
+            for (const labelKey in field.labels) {
+              uniqueLabelKeys[labelKey] = true;
+            }
+          }
+      }
+    }
+
+    if (!timeField) {
+      continue;
+    }
+
+    const sortedUniqueLabelKeys: string[] = [];
+    const uniqueFactorNames: string[] = [];
+    const uniqueFactorNamesWithWideIndices: string[] = [];
+
+    for (const labelKeys in labelKeyToWideIndices) {
+      sortedUniqueLabelKeys.push(labelKeys);
+    }
+    for (const labelKey in uniqueLabelKeys) {
+      uniqueFactorNames.push(labelKey);
+    }
+    for (const name in uniqueFactorNamesToWideIndex) {
+      uniqueFactorNamesWithWideIndices.push(name);
+    }
+
+    sortedUniqueLabelKeys.sort();
+    uniqueFactorNames.sort();
+    uniqueValueNames.sort();
+
+    const longFrame = new MutableDataFrame({
+      name: frame.name,
+      meta: { ...frame.meta, type: DataFrameType.TimeSeriesLong },
+      fields: [{ name: timeField.name, type: timeField.type }],
+    });
+
+    for (const name of uniqueValueNames) {
+      longFrame.addField({ name: name, type: uniqueValueNamesToType[name] });
+    }
+
+    for (const name of uniqueFactorNames) {
+      longFrame.addField({ name: name, type: FieldType.string });
+    }
+
+    for (let wideRowIndex = 0; wideRowIndex < frame.length; wideRowIndex++) {
+      const time = timeField.values.get(wideRowIndex);
+
+      for (const labelKeys of sortedUniqueLabelKeys) {
+        const rowValues: Record<string, any> = {};
+
+        for (const name of uniqueFactorNamesWithWideIndices) {
+          rowValues[name] = frame.fields[uniqueFactorNamesToWideIndex[name]].values.get(wideRowIndex);
+        }
+
+        let index = 0;
+
+        for (const wideFieldIndex of labelKeyToWideIndices[labelKeys]) {
+          const wideField = frame.fields[wideFieldIndex];
+
+          if (index++ === 0 && wideField.labels != null) {
+            for (const labelKey in wideField.labels) {
+              rowValues[labelKey] = wideField.labels[labelKey];
+            }
+          }
+
+          rowValues[wideField.name] = wideField.values.get(wideRowIndex);
+        }
+
+        rowValues[timeField.name] = time;
+        longFrame.add(rowValues);
+      }
+    }
+
+    result.push(longFrame);
+  }
+
+  return result;
+}
+
 export const prepareTimeSeriesTransformer: SynchronousDataTransformerInfo<PrepareTimeSeriesOptions> = {
   id: DataTransformerID.prepareTimeSeries,
   name: 'Prepare time series',
@@ -80,6 +215,8 @@ export const prepareTimeSeriesTransformer: SynchronousDataTransformerInfo<Prepar
     const format = options?.format ?? timeSeriesFormat.TimeSeriesWide;
     if (format === timeSeriesFormat.TimeSeriesMany) {
       return toTimeSeriesMany;
+    } else if (format === timeSeriesFormat.TimeSeriesLong) {
+      return toTimeSeriesLong;
     }
 
     return (data: DataFrame[]) => {
