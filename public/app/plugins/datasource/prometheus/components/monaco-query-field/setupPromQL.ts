25328aaa23ee09d892986a8aa8bd09a9eb0e5166
Gábor Farkas <gabor.farkas@gmail.com>
2021-08-26 17:10:22 +0200

    added new trigger character

--- public/app/plugins/datasource/prometheus/components/monaco-query-field/setupPromQL.ts
+++ public/app/plugins/datasource/prometheus/components/monaco-query-field/setupPromQL.ts
@@ -59,7 +59,7 @@ function getMonacoCompletionItemProvider(
   };
 
   return {
-    triggerCharacters: ['{', ',', '[', '('],
+    triggerCharacters: ['{', ',', '[', '(', '='],
     provideCompletionItems,
   };
 }
