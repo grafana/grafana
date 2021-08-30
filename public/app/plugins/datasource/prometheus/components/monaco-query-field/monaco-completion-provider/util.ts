d749fd7fac704a12ba0d55777b367c0b7fd4f5fb
Gábor Farkas <gabor.farkas@gmail.com>
2021-08-30 10:37:31 +0200

    updated comment

--- public/app/plugins/datasource/prometheus/components/monaco-query-field/monaco-completion-provider/util.ts
+++ public/app/plugins/datasource/prometheus/components/monaco-query-field/monaco-completion-provider/util.ts
@@ -1,7 +1,23 @@
-// this class is used in a switch statement below,
-// and this guarantees that all the possible type-values
-// are handled. if we forget a `case` there, the typescript
-// compiler will raise an error.
+// this helper class is used to make typescript warn you when you forget
+// a case-block in a switch statement.
+// example code that triggers the typescript-error:
+//
+// const x:'A'|'B'|'C' = 'A';
+//
+// switch(x) {
+//   case 'A':
+//     // something
+//   case 'B':
+//     // something
+//   default:
+//     throw new NeverCaseError(x);
+// }
+//
+//
+// typescript will show an error in this case,
+// when you add the missing `case 'C'` code,
+// the problem will be fixed.
+
 export class NeverCaseError extends Error {
   constructor(value: never) {
     super('should never happen');
