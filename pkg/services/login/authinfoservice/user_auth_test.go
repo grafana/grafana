c90a65e7923fafa32e180706aef862db0f6de5ef
Joan López de la Franca Beltran <joanjan14@gmail.com>
2021-08-31 10:06:31 +0200

    Refactor service initialization

--- pkg/services/login/authinfoservice/user_auth_test.go
+++ pkg/services/login/authinfoservice/user_auth_test.go
@@ -22,7 +22,7 @@ import (
 //nolint:goconst
 func TestUserAuth(t *testing.T) {
 	sqlStore := sqlstore.InitTestDB(t)
-	srv := ProvideAuthInfoService(bus.New(), sqlStore, &OSSUserProtectionImpl{}, &ossencryption.Service{})
+	srv := ProvideAuthInfoService(bus.New(), sqlStore, &OSSUserProtectionImpl{}, ossencryption.ProvideService())
 
 	t.Run("Given 5 users", func(t *testing.T) {
 		for i := 0; i < 5; i++ {
