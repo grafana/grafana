6a20bfeffaee716e0a6f307637989649d9ead10d
thisisobate <obasiuche62@gmail.com>
2021-09-01 08:12:30 +0100

    removes unecessary async/await construct

--- public/app/features/playlist/api.ts
+++ public/app/features/playlist/api.ts
@@ -6,15 +6,15 @@ import { notifyApp } from '../../core/actions';
 import { createErrorNotification, createSuccessNotification } from '../../core/copy/appNotification';
 
 export async function createPlaylist(playlist: Playlist) {
-  await withErrorHandling(async () => await getBackendSrv().post('/api/playlists', playlist));
+  await withErrorHandling(() => getBackendSrv().post('/api/playlists', playlist));
 }
 
 export async function updatePlaylist(id: number, playlist: Playlist) {
-  await withErrorHandling(async () => await getBackendSrv().put(`/api/playlists/${id}`, playlist));
+  await withErrorHandling(() => getBackendSrv().put(`/api/playlists/${id}`, playlist));
 }
 
 export async function deletePlaylist(id: number) {
-  await withErrorHandling(async () => await getBackendSrv().delete(`/api/playlists/${id}`), 'Playlist deleted');
+  await withErrorHandling(() => getBackendSrv().delete(`/api/playlists/${id}`), 'Playlist deleted');
 }
 
 export async function getPlaylist(id: number): Promise<Playlist> {
