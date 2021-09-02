b1440ca20ea81b4268364c4b5d86a8790ecce7a0
Levente Balogh <balogh.levente.hu@gmail.com>
2021-09-02 11:51:52 +0200

    refactor(Plugins/Admin): add request info to the reducer

--- public/app/features/plugins/admin/state/reducer.ts
+++ public/app/features/plugins/admin/state/reducer.ts
@@ -1,30 +1,64 @@
-import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import { createSlice, createEntityAdapter, EntityState, AnyAction } from '@reduxjs/toolkit';
 import { fetchAll, fetchSingle } from './actions';
-import { CatalogPlugin } from '../types';
+import { CatalogPlugin, RequestInfo, RequestStatus } from '../types';
+import { STATE_PREFIX } from '../constants';
+
+type ReducerState = {
+  items: EntityState<CatalogPlugin>;
+  requests: Record<string, RequestInfo>;
+};
 
 export const pluginsAdapter = createEntityAdapter<CatalogPlugin>();
 
+const isPendingRequest = (action: AnyAction) => new RegExp(`${STATE_PREFIX}\/(.*)\/pending`).test(action.type);
+
+const isFulfilledRequest = (action: AnyAction) => new RegExp(`${STATE_PREFIX}\/(.*)\/fulfilled`).test(action.type);
+
+const isRejectedRequest = (action: AnyAction) => new RegExp(`${STATE_PREFIX}\/(.*)\/rejected`).test(action.type);
+
+// Extract the trailing '/pending', '/rejected', or '/fulfilled'
+const getOriginalActionType = (type: string) => {
+  const separator = type.lastIndexOf('/');
+
+  return type.substring(0, separator);
+};
+
 export const { reducer } = createSlice({
   name: 'plugins',
-  initialState: pluginsAdapter.getInitialState(),
+  initialState: { items: pluginsAdapter.getInitialState(), requests: {} } as ReducerState,
   reducers: {},
   extraReducers: (builder) =>
     builder
       // Fetch All
       .addCase(fetchAll.fulfilled, (state, action) => {
-        pluginsAdapter.upsertMany(state, action.payload);
+        pluginsAdapter.upsertMany(state.items, action.payload);
       })
       // Fetch Single
       .addCase(fetchSingle.fulfilled, (state, action) => {
-        pluginsAdapter.upsertOne(state, action.payload);
+        pluginsAdapter.upsertOne(state.items, action.payload);
+      })
+      // Install
+      // .addCase(install.fulfilled, (state, action) => {
+      //   pluginsAdapter.upsertOne(state, action.payload);
+      // })
+      // Uninstall
+      // .addCase(uninstall.fulfilled, (state, action) => {
+      //   pluginsAdapter.upsertOne(state, action.payload);
+      // }),
+      .addMatcher(isPendingRequest, (state, action) => {
+        state.requests[getOriginalActionType(action.type)] = {
+          status: RequestStatus.Pending,
+        };
+      })
+      .addMatcher(isFulfilledRequest, (state, action) => {
+        state.requests[getOriginalActionType(action.type)] = {
+          status: RequestStatus.Fulfilled,
+        };
+      })
+      .addMatcher(isRejectedRequest, (state, action) => {
+        state.requests[getOriginalActionType(action.type)] = {
+          status: RequestStatus.Rejected,
+          error: action.payload,
+        };
       }),
-  // Install
-  // .addCase(install.fulfilled, (state, action) => {
-  //   pluginsAdapter.upsertOne(state, action.payload);
-  // })
-  // Uninstall
-  // .addCase(uninstall.fulfilled, (state, action) => {
-  //   pluginsAdapter.upsertOne(state, action.payload);
-  // }),
-  // TODO<add error handling>
 });
