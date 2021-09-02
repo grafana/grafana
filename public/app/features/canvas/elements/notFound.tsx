b262aec77de697aca24b5c2b5a16a7426c84c075
Ryan McKinley <ryantxu@gmail.com>
2021-09-02 10:01:08 -0700

    Canvas: add alpha canvas panel and initial interfaces (#37279)

---
+++ public/app/features/canvas/elements/notFound.tsx
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+
+import { CanvasElementItem, CanvasElementProps } from '../element';
+
+interface NotFoundConfig {
+  orig?: any;
+}
+
+class NotFoundDisplay extends PureComponent<CanvasElementProps<NotFoundConfig>> {
+  render() {
+    const { config } = this.props;
+    return (
+      <div>
+        <h3>NOT FOUND:</h3>
+        <pre>{JSON.stringify(config, null, 2)}</pre>
+      </div>
+    );
+  }
+}
+
+export const notFoundItem: CanvasElementItem<NotFoundConfig> = {
+  id: 'not-found',
+  name: 'Not found',
+  description: 'Display when element type is not found in the registry',
+
+  defaultConfig: {},
+
+  display: NotFoundDisplay,
+
+  defaultSize: {
+    width: 100,
+    height: 100,
+  },
+};
