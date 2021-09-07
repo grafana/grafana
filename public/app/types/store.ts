4807f5033f990d227db7751d8eceffc0a6267a28
Hugo Häggmark <hugo.haggmark@grafana.com>
2021-09-07 10:44:47 +0200

    Chore: Bumps Immer, Redux Toolkit, Redux and React Redux (#38872)
    * Chore: Bumps immer and redux/toolkit
    
    * WIP: transpile errors
    
    * Chore: bumps redux and react redux
    
    * Chore: removes unused dispatch
    
    * make alerting compile with updated redux toolkit
    
    * Test: Fixes broken test
    
    * Chore: fixes strict errors
    
    Co-authored-by: Domas <domasx2@gmail.com>

--- public/app/types/store.ts
+++ public/app/types/store.ts
@@ -2,7 +2,6 @@ import { ThunkAction, ThunkDispatch as GenericThunkDispatch } from 'redux-thunk'
 import { Action, PayloadAction } from '@reduxjs/toolkit';
 import { NavIndex } from '@grafana/data';
 import { AlertRulesState, NotificationChannelState } from './alerting';
-import { UnifiedAlertingState } from '../features/alerting/unified/state/reducers';
 import { TeamsState, TeamState } from './teams';
 import { FolderState } from './folders';
 import { DashboardState } from './dashboard';
@@ -18,6 +17,7 @@ import { ApiKeysState } from './apiKeys';
 import { TemplatingState } from '../features/variables/state/reducers';
 import { ImportDashboardState } from '../features/manage-dashboards/state/reducers';
 import { UserState } from 'app/features/profile/state/reducers';
+import { UnifiedAlertingState } from 'app/features/alerting/unified/state/reducers';
 
 export interface StoreState {
   navIndex: NavIndex;
