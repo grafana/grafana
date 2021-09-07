51776e6bd39fb9cb49ade08b23ec37d18e57e7eb
Torkel Ödegaard <torkel@grafana.org>
2021-09-07 18:50:28 +0200

    UsageStats: Add connected users and client (#38811)

--- pkg/services/live/live.go
+++ pkg/services/live/live.go
@@ -47,15 +47,6 @@ var (
 	loggerCF = log.New("live.centrifuge")
 )
 
-func NewGrafanaLive() *GrafanaLive {
-	return &GrafanaLive{
-		channels: make(map[string]models.ChannelHandler),
-		GrafanaScope: CoreGrafanaScope{
-			Features: make(map[string]models.ChannelHandlerFactory),
-		},
-	}
-}
-
 // CoreGrafanaScope list of core features
 type CoreGrafanaScope struct {
 	Features map[string]models.ChannelHandlerFactory
@@ -339,6 +330,11 @@ type GrafanaLive struct {
 	contextGetter    *liveplugin.ContextGetter
 	runStreamManager *runstream.Manager
 	storage          *database.Storage
+}
+
+type UsageStats struct {
+	NumClients int
+	NumUsers   int
 }
 
 func (g *GrafanaLive) getStreamPlugin(pluginID string) (backend.StreamHandler, error) {
@@ -720,6 +716,13 @@ func (g *GrafanaLive) ClientCount(orgID int64, channel string) (int, error) {
 		return 0, err
 	}
 	return len(p.Presence), nil
+}
+
+func (g *GrafanaLive) UsageStats() UsageStats {
+	clients := g.node.Hub().NumClients()
+	users := g.node.Hub().NumUsers()
+
+	return UsageStats{NumClients: clients, NumUsers: users}
 }
 
 func (g *GrafanaLive) HandleHTTPPublish(ctx *models.ReqContext, cmd dtos.LivePublishCmd) response.Response {
