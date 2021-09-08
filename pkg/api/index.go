78a7c021a8cc543327d69b4f09b57c584a723eda
gamab <gabi.mabs@gmail.com>
2021-09-08 11:33:30 +0200

    Commit sakjur suggestion

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -18,6 +18,16 @@ const (
 	darkName  = "dark"
 )
 
+// dataSourcesConfigurationAccessEvaluator is used to protect the "Configure > Data sources" tab access
+var dataSourcesConfigurationAccessEvaluator = ac.EvalAll(
+	ac.EvalPermission(ActionDatasourcesRead, ScopeDatasourcesAll),
+	ac.EvalAny(
+		ac.EvalPermission(ActionDatasourcesCreate),
+		ac.EvalPermission(ActionDatasourcesDelete),
+		ac.EvalPermission(ActionDatasourcesWrite),
+	),
+)
+
 func (hs *HTTPServer) getProfileNode(c *models.ReqContext) *dtos.NavLink {
 	// Only set login if it's different from the name
 	var login string
@@ -253,7 +263,7 @@ func (hs *HTTPServer) getNavTree(c *models.ReqContext, hasEditPerm bool) ([]*dto
 
 	configNodes := []*dtos.NavLink{}
 
-	if hasAccess(ac.ReqOrgAdmin, getDataSourcesConfigurationAccessEvaluator()) {
+	if hasAccess(ac.ReqOrgAdmin, dataSourcesConfigurationAccessEvaluator) {
 		configNodes = append(configNodes, &dtos.NavLink{
 			Text:        "Data sources",
 			Icon:        "database",
@@ -541,18 +551,4 @@ func getAppNameBodyClass(validLicense bool) string {
 	}
 
 	return "app-grafana"
-}
-
-// getDataSourcesConfigurationAccessEvaluator returns an accesscontrol evaluator
-// required to allow access to the "Configure > Data sources" tab
-func getDataSourcesConfigurationAccessEvaluator() ac.Evaluator {
-	// Read and at least one of the modifying action is needed
-	return ac.EvalAll(
-		ac.EvalPermission(ActionDatasourcesRead, ScopeDatasourcesAll),
-		ac.EvalAny(
-			ac.EvalPermission(ActionDatasourcesCreate),
-			ac.EvalPermission(ActionDatasourcesDelete),
-			ac.EvalPermission(ActionDatasourcesWrite),
-		),
-	)
 }
