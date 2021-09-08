4d310eb355a47b7b26bfc2aafc2673912612e02b
Andrew Hackmann <ajhack93@gmail.com>
2021-09-08 09:37:59 -0500

    adding internal tag

--- public/app/features/query/components/CreateActionComponent.ts
+++ public/app/features/query/components/CreateActionComponent.ts
@@ -2,10 +2,15 @@ export type CreateActionComponent = React.ComponentType;
 
 let extraRenderCreateActions: CreateActionComponent[] = [];
 
+/**
+ * @internal and experimental
+ */
 export function addExtraRenderCreateAction(extra: CreateActionComponent) {
   extraRenderCreateActions = extraRenderCreateActions.concat(extra);
 }
-
+/**
+ * @internal and experimental
+ */
 export function getAllExtraRenderCreateAction(): CreateActionComponent[] {
   return extraRenderCreateActions;
 }
--- public/app/features/query/components/QueryActionComponent.ts
+++ public/app/features/query/components/QueryActionComponent.ts
@@ -9,10 +9,16 @@ export type QueryActionComponent = React.ComponentType<QueryActionComponentProps
 
 let extraRenderQueryActions: QueryActionComponent[] = [];
 
+/**
+ * @internal and experimental
+ */
 export function addExtraRenderQueryAction(extra: QueryActionComponent) {
   extraRenderQueryActions = extraRenderQueryActions.concat(extra);
 }
 
+/**
+ * @internal and experimental
+ */
 export function getAllExtraRenderQueryAction(): QueryActionComponent[] {
   return extraRenderQueryActions;
 }
