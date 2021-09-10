8245989bcd45a2a634ab0aa0f48d90eb230ac1cc
Ryan McKinley <ryantxu@gmail.com>
2021-09-10 09:05:03 -0700

    Geomap: include sample geojson files (#39057)

--- public/app/plugins/panel/geomap/event.ts
+++ public/app/plugins/panel/geomap/event.ts
@@ -2,10 +2,11 @@ import { FeatureLike } from 'ol/Feature';
 import { SimpleGeometry } from 'ol/geom';
 import { Layer } from 'ol/layer';
 import { DataHoverPayload } from '@grafana/data';
+import { Source } from 'ol/source';
 
 export interface GeomapHoverFeature {
   feature: FeatureLike;
-  layer: Layer;
+  layer: Layer<Source>;
   geo: SimpleGeometry;
 }
 
