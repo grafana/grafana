703e6e458d00703e2e91cf74d417ec2675a26650
Andrej Ocenas <mr.ocenas@gmail.com>
2021-09-13 17:32:58 +0200

    Add test for trace id link

--- public/app/plugins/datasource/cloudwatch/utils/datalinks.test.ts
+++ public/app/plugins/datasource/cloudwatch/utils/datalinks.test.ts
@@ -1,5 +1,6 @@
 import { DataQueryResponse, dateMath } from '@grafana/data';
 import { addDataLinksToLogsResponse } from './datalinks';
+import {setDataSourceSrv} from "@grafana/runtime";
 
 describe('addDataLinksToLogsResponse', () => {
   it('should add data links to response', async () => {
@@ -8,9 +9,12 @@ describe('addDataLinksToLogsResponse', () => {
         {
           fields: [
             {
-              config: {
-                links: [],
-              },
+              name: '@message',
+              config: {},
+            },
+            {
+              name: '@xrayTraceId',
+              config: {},
             },
           ],
           refId: 'A',
@@ -34,25 +38,50 @@ describe('addDataLinksToLogsResponse', () => {
       to: dateMath.parse('2016-12-31 16:00:00Z', false)!,
     };
 
+    setDataSourceSrv({
+      async get() {
+        return {
+          name: 'Xray',
+        };
+      },
+    } as any);
+
     await addDataLinksToLogsResponse(
       mockResponse,
       mockOptions,
       { ...time, raw: time },
       (s) => s ?? '',
-      (r) => r
+      (r) => r,
+      'xrayUid'
     );
     expect(mockResponse).toMatchObject({
       data: [
         {
           fields: [
             {
+              name: '@message',
               config: {
                 links: [
                   {
                     url:
                       "https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logs-insights:queryDetail=~(end~'2016-12-31T16*3a00*3a00.000Z~start~'2016-12-31T15*3a00*3a00.000Z~timeType~'ABSOLUTE~tz~'UTC~editorString~'stats*20count*28*40message*29*20by*20bin*281h*29~isLiveTail~false~source~(~'fake-log-group-one~'fake-log-group-two))",
                     title: 'View in CloudWatch console',
-                    targetBlank: true,
+                  },
+                ],
+              },
+            },
+            {
+              name: '@xrayTraceId',
+              config: {
+                links: [
+                  {
+                    url: '',
+                    title: 'Xray',
+                    internal: {
+                      query: { query: '${__value.raw}', region: 'us-east-1', queryType: 'getTrace' },
+                      datasourceUid: 'xrayUid',
+                      datasourceName: 'Xray',
+                    },
                   },
                 ],
               },
