a2f4344bf270b22dc897717f2dd84cb63836ba5e
gotjosh <josue@grafana.com>
2021-09-14 12:55:01 +0100

    Alerting: Refactor & fix unified alerting metrics structure (#39151)
    * Alerting: Refactor & fix unified alerting metrics structure
    
    Fixes and refactors the metrics structure we have for the ngalert service. Now, each component has its own metric struct that includes the JUST the metrics it uses. Additionally, I have fixed the configuration metrics and added new metrics to determine if we have discovered and started all the necessary configurations of an instance.
    
    This allows us to alert on `grafana_alerting_discovered_configurations - grafana_alerting_active_configurations != 0` to know whether an alertmanager instance did not start successfully.

--- pkg/services/ngalert/notifier/alertmanager_test.go
+++ pkg/services/ngalert/notifier/alertmanager_test.go
@@ -38,9 +38,9 @@ func setupAMTest(t *testing.T) *Alertmanager {
 		DataPath: dir,
 	}
 
-	m := metrics.NewMetrics(prometheus.NewRegistry())
+	m := metrics.NewAlertmanagerMetrics(prometheus.NewRegistry())
 	sqlStore := sqlstore.InitTestDB(t)
-	store := &store.DBstore{
+	s := &store.DBstore{
 		BaseInterval:           10 * time.Second,
 		DefaultIntervalSeconds: 60,
 		SQLStore:               sqlStore,
@@ -48,7 +48,7 @@ func setupAMTest(t *testing.T) *Alertmanager {
 	}
 
 	kvStore := newFakeKVStore(t)
-	am, err := newAlertmanager(1, cfg, store, kvStore, m)
+	am, err := newAlertmanager(1, cfg, s, kvStore, m)
 	require.NoError(t, err)
 	return am
 }
