ad38f1aa4a33eaaf2fbd956ab5e72496d1289773
Leon Sorokin <leeoniya@gmail.com>
2021-09-14 10:00:07 -0500

    BarChart: fix stale values and axis tick labels

--- public/app/plugins/panel/barchart/BarChart.tsx
+++ public/app/plugins/panel/barchart/BarChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { cloneDeep } from 'lodash';
 import { DataFrame, TimeRange } from '@grafana/data';
 import { GraphNG, GraphNGProps, PlotLegend, UPlotConfigBuilder, usePanelContext, useTheme2 } from '@grafana/ui';
@@ -27,6 +27,9 @@ export const BarChart: React.FC<BarChartProps> = (props) => {
   const theme = useTheme2();
   const { eventBus } = usePanelContext();
 
+  const propsRef = useRef(props);
+  propsRef.current = props;
+
   const renderLegend = (config: UPlotConfigBuilder) => {
     if (!config || props.legend.displayMode === LegendDisplayMode.Hidden) {
       return null;
@@ -34,6 +37,9 @@ export const BarChart: React.FC<BarChartProps> = (props) => {
 
     return <PlotLegend data={props.frames} config={config} maxHeight="35%" maxWidth="60%" {...props.legend} />;
   };
+
+  const rawValue = (seriesIdx: number, valueIdx: number) =>
+    propsRef.current.frames[0].fields[seriesIdx].values.get(valueIdx);
 
   const prepConfig = (alignedFrame: DataFrame, allFrames: DataFrame[], getTimeRange: () => TimeRange) => {
     const { timeZone, orientation, barWidth, showValue, groupWidth, stacking, legend, tooltip, text } = props;
@@ -52,6 +58,7 @@ export const BarChart: React.FC<BarChartProps> = (props) => {
       legend,
       tooltip,
       text,
+      rawValue,
       allFrames: props.frames,
     });
   };
