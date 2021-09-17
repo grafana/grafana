d73242aeaa56d2da8c07b17a83747896d2f71488
Sofia Papagiannaki <sofia@grafana.com>
2021-09-17 13:54:44 +0300

    Remove duplicated code

--- pkg/services/ngalert/ngalert_test.go
+++ pkg/services/ngalert/ngalert_test.go
@@ -10,39 +10,48 @@ import (
 
 func TestGetRuleDefaultIntervalSeconds(t *testing.T) {
 	testCases := []struct {
-		desc                string
-		alertingMinInterval int64
-		expOutput           int64
+		desc                   string
+		alertingMinIntervalCfg int64
+		// the expected default rule interval (applied if a rule interval is missing)
+		expDefaultIntervalSeconds int64
+		// the expected minimum rule interval (enforced if a rule interval is lower than this value; it is used also for computing the default rule interval)
+		expMinIntervalSeconds int64
 	}{
 		{
-			desc:                "negative min rule interval",
-			alertingMinInterval: -1,
-			expOutput:           defaultIntervalSeconds,
+			desc:                      "negative min rule interval",
+			alertingMinIntervalCfg:    -1,
+			expDefaultIntervalSeconds: defaultIntervalSeconds,     // 60
+			expMinIntervalSeconds:     defaultBaseIntervalSeconds, // 10
 		},
 		{
-			desc:                "zero min rule interval",
-			alertingMinInterval: 0,
-			expOutput:           defaultIntervalSeconds,
+			desc:                      "zero min rule interval",
+			alertingMinIntervalCfg:    0,
+			expDefaultIntervalSeconds: defaultIntervalSeconds,     // 60
+			expMinIntervalSeconds:     defaultBaseIntervalSeconds, // 10
 		},
 		{
-			desc:                "invalid min rule interval",
-			alertingMinInterval: 1,
-			expOutput:           defaultIntervalSeconds,
+			desc:                      "min rule interval not divided exactly by the scheduler interval",
+			alertingMinIntervalCfg:    1,
+			expDefaultIntervalSeconds: defaultIntervalSeconds,     // 60
+			expMinIntervalSeconds:     defaultBaseIntervalSeconds, // 10
 		},
 		{
-			desc:                "valid min rule interval equals base scheduler interval",
-			alertingMinInterval: defaultBaseIntervalSeconds,
-			expOutput:           defaultIntervalSeconds,
+			desc:                      "min rule interval equals base scheduler interval",
+			alertingMinIntervalCfg:    defaultBaseIntervalSeconds, // 10
+			expDefaultIntervalSeconds: defaultIntervalSeconds,     // 60
+			expMinIntervalSeconds:     defaultBaseIntervalSeconds, // 10
 		},
 		{
-			desc:                "valid min rule interval less than default",
-			alertingMinInterval: defaultIntervalSeconds - defaultBaseIntervalSeconds,
-			expOutput:           defaultIntervalSeconds,
+			desc:                      "valid min rule interval less than default rule interval",
+			alertingMinIntervalCfg:    defaultIntervalSeconds - defaultBaseIntervalSeconds, // 50
+			expDefaultIntervalSeconds: defaultIntervalSeconds,                              // 60
+			expMinIntervalSeconds:     defaultIntervalSeconds - defaultBaseIntervalSeconds, // 50
 		},
 		{
-			desc:                "valid min rule interval greater than default",
-			alertingMinInterval: defaultIntervalSeconds + defaultBaseIntervalSeconds,
-			expOutput:           defaultIntervalSeconds + defaultBaseIntervalSeconds,
+			desc:                      "valid min rule interval greater than default rule interval",
+			alertingMinIntervalCfg:    defaultIntervalSeconds + defaultBaseIntervalSeconds, // 70
+			expDefaultIntervalSeconds: defaultIntervalSeconds + defaultBaseIntervalSeconds, // 70
+			expMinIntervalSeconds:     defaultIntervalSeconds + defaultBaseIntervalSeconds, // 70
 		},
 	}
 
@@ -51,51 +60,12 @@ func TestGetRuleDefaultIntervalSeconds(t *testing.T) {
 			alertNG := AlertNG{
 				Log: log.New("test"),
 				Cfg: &setting.Cfg{
-					UnifiedAlertingMinInterval: tc.alertingMinInterval,
+					UnifiedAlertingMinInterval: tc.alertingMinIntervalCfg,
 				},
 			}
-			require.Equal(t, tc.expOutput, alertNG.getRuleDefaultIntervalSeconds())
-		})
-	}
-}
+			require.Equal(t, tc.expDefaultIntervalSeconds, alertNG.getRuleDefaultIntervalSeconds())
+			require.Equal(t, tc.expMinIntervalSeconds, alertNG.getRuleMinIntervalSeconds())
 
-func TestGetRuleMinIntervalSeconds(t *testing.T) {
-	testCases := []struct {
-		desc                string
-		alertingMinInterval int64
-		expOutput           int64
-	}{
-		{
-			desc:                "negative min rule interval",
-			alertingMinInterval: -1,
-			expOutput:           defaultBaseIntervalSeconds,
-		},
-		{
-			desc:                "zero min rule interval",
-			alertingMinInterval: 0,
-			expOutput:           defaultBaseIntervalSeconds,
-		},
-		{
-			desc:                "invalid min rule interval",
-			alertingMinInterval: 1,
-			expOutput:           defaultBaseIntervalSeconds,
-		},
-		{
-			desc:                "valid min rule interval ",
-			alertingMinInterval: defaultIntervalSeconds + defaultBaseIntervalSeconds,
-			expOutput:           defaultIntervalSeconds + defaultBaseIntervalSeconds,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			alertNG := AlertNG{
-				Log: log.New("test"),
-				Cfg: &setting.Cfg{
-					UnifiedAlertingMinInterval: tc.alertingMinInterval,
-				},
-			}
-			require.Equal(t, tc.expOutput, alertNG.getRuleMinIntervalSeconds())
 		})
 	}
 }
