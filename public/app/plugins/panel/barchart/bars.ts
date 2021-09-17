474461ba15ae8ce4bf129684085c0d10ccfa0aae
Leon Sorokin <leeoniya@gmail.com>
2021-09-17 12:33:39 -0500

    GraphNG: uPlot 1.6.16 (#39260)

--- public/app/plugins/panel/barchart/bars.ts
+++ public/app/plugins/panel/barchart/bars.ts
@@ -234,6 +234,8 @@ export function getConfig(opts: BarsOptions, theme: GrafanaTheme2) {
       }
     }
 
+    u.ctx.save();
+
     u.ctx.fillStyle = theme.colors.text.primary;
     u.ctx.font = `${fontSize}px ${theme.typography.fontFamily}`;
 
@@ -265,6 +267,8 @@ export function getConfig(opts: BarsOptions, theme: GrafanaTheme2) {
         );
       }
     });
+
+    u.ctx.restore();
   };
 
   // handle hover interaction with quadtree probing
