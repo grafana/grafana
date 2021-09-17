8fda51c2ac0ad98e54d2134ad28ba2d0f0a42b5f
Tania B <yalyna.ts@gmail.com>
2021-09-17 16:24:48 +0300

    Add SetupTestService to secrets

---
+++ pkg/services/secrets/test_helper.go
@@ -0,0 +1,30 @@
+package secrets
+
+import (
+	"testing"
+
+	"github.com/grafana/grafana/pkg/bus"
+	"github.com/grafana/grafana/pkg/services/encryption/ossencryption"
+	"github.com/grafana/grafana/pkg/services/secrets/database"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/grafana/grafana/pkg/setting"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/ini.v1"
+)
+
+func SetupTestService(t *testing.T) SecretsService {
+	t.Helper()
+	raw, err := ini.Load([]byte(`
+		[security]
+		secret_key = SdlklWklckeLS
+	`))
+	require.NoError(t, err)
+	settings := &setting.OSSImpl{Cfg: &setting.Cfg{Raw: raw}}
+
+	return ProvideSecretsService(
+		database.ProvideSecretsStore(sqlstore.InitTestDB(t)),
+		bus.New(),
+		ossencryption.ProvideService(),
+		settings,
+	)
+}
