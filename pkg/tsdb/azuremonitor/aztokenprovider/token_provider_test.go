4ce064bbf5b2323ffdd5b2e986bbe19ad2f5d347
yuwaMSFT2 <yuwa@microsoft.com>
2021-09-19 12:10:55 -0700

    Add additional tests

--- pkg/tsdb/azuremonitor/aztokenprovider/token_provider_test.go
+++ pkg/tsdb/azuremonitor/aztokenprovider/token_provider_test.go
@@ -202,25 +202,24 @@ func TestAzureTokenProvider_GetAccessTokenFromResponse(t *testing.T) {
 		TokenEndpoint: "https://test.io",
 		AuthHeader:    "Bear xxxxx",
 	}
-	value := struct {
-		Token     string      `json:"access_token"`
-		ExpiresIn json.Number `json:"expires_in"`
-		ExpiresOn string      `json:"expires_on"`
-	}{
-		Token:     "testtoken",
-		ExpiresIn: "1000",
-	}
 
-	data, err := json.Marshal(value)
-	assert.Empty(t, err, "Failed to marshal data")
+	t.Run("Sucessful token parse", func(t *testing.T) {
+		value := struct {
+			Token     string      `json:"access_token"`
+			ExpiresIn json.Number `json:"expires_in"`
+			ExpiresOn string      `json:"expires_on"`
+		}{
+			Token:     "testtoken",
+			ExpiresIn: "1000",
+		}
+		data, err := json.Marshal(value)
+		assert.Empty(t, err, "Failed to marshal data")
 
-	body := io.NopCloser(bytes.NewReader(data))
-	var resp = &http.Response{
-		StatusCode: 200,
-		Body:       body,
-	}
-
-	t.Run("return error if there is empty signed in user", func(t *testing.T) {
+		body := io.NopCloser(bytes.NewReader(data))
+		var resp = &http.Response{
+			StatusCode: 200,
+			Body:       body,
+		}
 		tokenRetriver := getUserIdentityTokenRetriever(cfg, credentials).(*userIdentityTokenRetriever)
 		token, err := tokenRetriver.getAccessTokenFromResponse(resp)
 		assert.Empty(t, err, "Failed on getting token")
@@ -231,5 +230,63 @@ func TestAzureTokenProvider_GetAccessTokenFromResponse(t *testing.T) {
 		timeMax := timeMin.Add(time.Second * 100)
 		assert.True(t, token.ExpiresOn.After(timeMin), "token.ExpiresOn is not after timeMin")
 		assert.True(t, token.ExpiresOn.Before(timeMax), "token.ExpiresOn is not before timeMax")
+	})
+
+	t.Run("Fail token parse on bad status code", func(t *testing.T) {
+		value := struct {
+			Token     string      `json:"access_token"`
+			ExpiresIn json.Number `json:"expires_in"`
+			ExpiresOn string      `json:"expires_on"`
+		}{
+			Token:     "testtoken",
+			ExpiresIn: "1000",
+		}
+		data, err := json.Marshal(value)
+		assert.Empty(t, err, "Failed to marshal data")
+
+		body := io.NopCloser(bytes.NewReader(data))
+		var resp = &http.Response{
+			StatusCode: 401,
+			Body:       body,
+		}
+		tokenRetriver := getUserIdentityTokenRetriever(cfg, credentials).(*userIdentityTokenRetriever)
+		_, err = tokenRetriver.getAccessTokenFromResponse(resp)
+		assert.Error(t, err, "Bad statuscode on token request: 401")
+	})
+
+	t.Run("Fail token parse on bad data", func(t *testing.T) {
+		data, err := json.Marshal("bad test")
+		assert.Empty(t, err, "Failed to marshal data")
+
+		body := io.NopCloser(bytes.NewReader(data))
+		var resp = &http.Response{
+			StatusCode: 200,
+			Body:       body,
+		}
+		tokenRetriver := getUserIdentityTokenRetriever(cfg, credentials).(*userIdentityTokenRetriever)
+		_, err = tokenRetriver.getAccessTokenFromResponse(resp)
+		assert.Contains(t, err.Error(), "Failed to deserialize token response")
+	})
+
+	t.Run("Fail token parse on bad expiresIn", func(t *testing.T) {
+		value := struct {
+			Token     string      `json:"access_token"`
+			ExpiresIn json.Number `json:"expires_in"`
+			ExpiresOn string      `json:"expires_on"`
+		}{
+			Token:     "testtoken",
+			ExpiresIn: "99.999",
+		}
+		data, err := json.Marshal(value)
+		assert.Empty(t, err, "Failed to marshal data")
+
+		body := io.NopCloser(bytes.NewReader(data))
+		var resp = &http.Response{
+			StatusCode: 200,
+			Body:       body,
+		}
+		tokenRetriver := getUserIdentityTokenRetriever(cfg, credentials).(*userIdentityTokenRetriever)
+		_, err = tokenRetriver.getAccessTokenFromResponse(resp)
+		assert.Contains(t, err.Error(), "Faile to get ExpiresIn property of the token")
 	})
 }
