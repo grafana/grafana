f631db8077c79cbd0ae3f6c2c6a5fb01940fe0ec
Santiago <santiagohernandez.1997@gmail.com>
2021-09-20 12:25:03 -0300

    Test and documentation added for reReplaceAll template function

--- pkg/services/ngalert/state/template_test.go
+++ pkg/services/ngalert/state/template_test.go
@@ -330,6 +330,10 @@ func TestExpandTemplate(t *testing.T) {
 		text:     `{{ match "a+" "aa" }} {{ match "a+" "b" }}`,
 		expected: "true false",
 	}, {
+		name:     "regex replacement",
+		text:     "{{ reReplaceAll \"(a)b\" \"x$1\" \"ab\" }}",
+		expected: "xa",
+	}, {
 		name:     "pass multiple arguments to templates",
 		text:     `{{define "x"}}{{.arg0}} {{.arg1}}{{end}}{{template "x" (args 1 "2")}}`,
 		expected: "1 2",
