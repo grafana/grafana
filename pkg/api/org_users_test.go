924f258292338cbeb9868a74258c9410c17a15e3
spinillos <selenepinillos@gmail.com>
2021-09-21 10:41:50 +0200

    Fix test

--- pkg/api/org_users_test.go
+++ pkg/api/org_users_test.go
@@ -8,6 +8,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/grafana/grafana/pkg/bus"
+	"github.com/grafana/grafana/pkg/services/searchusers"
+
 	"github.com/grafana/grafana/pkg/api/dtos"
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/components/simplejson"
@@ -136,11 +139,12 @@ func setupOrgUsersAPIcontext(t *testing.T, role models.RoleType) (*scenarioConte
 	db := sqlstore.InitTestDB(t)
 
 	hs := &HTTPServer{
-		Cfg:           cfg,
-		QuotaService:  &quota.QuotaService{Cfg: cfg},
-		RouteRegister: routing.NewRouteRegister(),
-		AccessControl: accesscontrolmock.New().WithDisabled(),
-		SQLStore:      db,
+		Cfg:                cfg,
+		QuotaService:       &quota.QuotaService{Cfg: cfg},
+		RouteRegister:      routing.NewRouteRegister(),
+		AccessControl:      accesscontrolmock.New().WithDisabled(),
+		SQLStore:           db,
+		searchUsersService: searchusers.ProvideUsersService(bus.New()),
 	}
 
 	sc := setupScenarioContext(t, "/api/org/users/lookup")
