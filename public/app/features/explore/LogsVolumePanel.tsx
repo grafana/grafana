16eaa2389fdb8fe4aa643332b2661269ea45c683
Piotr Jamróz <pm.jamroz@gmail.com>
2021-09-21 09:01:12 +0200

    Use LogsVolumeContentWrapper all content types

--- public/app/features/explore/LogsVolumePanel.tsx
+++ public/app/features/explore/LogsVolumePanel.tsx
@@ -1,8 +1,9 @@
 import { AbsoluteTimeRange, DataQueryResponse, LoadingState, SplitOpen, TimeZone } from '@grafana/data';
-import { Button, Collapse, useTheme2 } from '@grafana/ui';
+import { Button, Collapse, useStyles2, useTheme2 } from '@grafana/ui';
 import { ExploreGraph } from './ExploreGraph';
 import React from 'react';
 import { ExploreId } from '../../types';
+import { css } from '@emotion/css';
 
 type Props = {
   exploreId: ExploreId;
@@ -28,37 +29,37 @@ export function LogsVolumePanel(props: Props) {
   } = props;
   const theme = useTheme2();
   const spacing = parseInt(theme.spacing(2).slice(0, -2), 10);
+  const styles = useStyles2(getStyles);
+  const height = 150;
 
   let LogsVolumePanelContent;
 
   if (!logsVolumeData) {
     LogsVolumePanelContent = (
-      <LogsVolumeContentWrapper>
-        <Button
-          onClick={() => {
-            loadLogsVolumeData(exploreId);
-          }}
-        >
-          Load logs volume
-        </Button>
-      </LogsVolumeContentWrapper>
+      <Button
+        onClick={() => {
+          loadLogsVolumeData(exploreId);
+        }}
+      >
+        Load logs volume
+      </Button>
     );
   } else if (logsVolumeData?.error) {
     LogsVolumePanelContent = (
-      <LogsVolumeContentWrapper>
+      <span>
         Failed to load volume logs for this query:{' '}
         {logsVolumeData.error.data?.message || logsVolumeData.error.statusText}
-      </LogsVolumeContentWrapper>
+      </span>
     );
   } else if (logsVolumeData?.state === LoadingState.Loading) {
-    LogsVolumePanelContent = <LogsVolumeContentWrapper>Logs volume is loading...</LogsVolumeContentWrapper>;
+    LogsVolumePanelContent = <span>Logs volume is loading...</span>;
   } else if (logsVolumeData?.data) {
     if (logsVolumeData.data.length > 0) {
       LogsVolumePanelContent = (
         <ExploreGraph
           loadingState={LoadingState.Done}
           data={logsVolumeData.data}
-          height={150}
+          height={height}
           width={width - spacing}
           absoluteRange={absoluteRange}
           onChangeTime={onUpdateTimeRange}
@@ -67,17 +68,25 @@ export function LogsVolumePanel(props: Props) {
         />
       );
     } else {
-      LogsVolumePanelContent = <LogsVolumeContentWrapper>No volume data.</LogsVolumeContentWrapper>;
+      LogsVolumePanelContent = <span>No volume data.</span>;
     }
   }
 
   return (
     <Collapse label="Logs volume" isOpen={true} loading={logsVolumeData?.state === LoadingState.Loading}>
-      {LogsVolumePanelContent}
+      <div style={{ height }} className={styles.logsVolumeContainer}>
+        {LogsVolumePanelContent}
+      </div>
     </Collapse>
   );
 }
 
-function LogsVolumeContentWrapper({ children }: { children: React.ReactNode }) {
-  return <div style={{ height: '150px', textAlign: 'center', paddingTop: '50px' }}>{children}</div>;
+function getStyles() {
+  return {
+    logsVolumeContainer: css({
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }),
+  };
 }
