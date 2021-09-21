0bf70b14fdd285890a6c413325c0c4d38cb743fa
Alexander Emelin <frvzmb@gmail.com>
2021-09-21 21:57:58 +0300

    Live: api to show available pipeline entities (#39469)
    Co-authored-by: Ryan McKinley <ryantxu@gmail.com>

--- pkg/services/live/pipeline/output_managed_stream.go
+++ pkg/services/live/pipeline/output_managed_stream.go
@@ -16,8 +16,14 @@ func NewManagedStreamOutput(managedStream *managedstream.Runner) *ManagedStreamO
 	return &ManagedStreamOutput{managedStream: managedStream}
 }
 
-func (l *ManagedStreamOutput) Output(_ context.Context, vars OutputVars, frame *data.Frame) ([]*ChannelFrame, error) {
-	stream, err := l.managedStream.GetOrCreateStream(vars.OrgID, vars.Scope, vars.Namespace)
+const OutputTypeManagedStream = "managedStream"
+
+func (out *ManagedStreamOutput) Type() string {
+	return OutputTypeManagedStream
+}
+
+func (out *ManagedStreamOutput) Output(_ context.Context, vars OutputVars, frame *data.Frame) ([]*ChannelFrame, error) {
+	stream, err := out.managedStream.GetOrCreateStream(vars.OrgID, vars.Scope, vars.Namespace)
 	if err != nil {
 		logger.Error("Error getting stream", "error", err)
 		return nil, err
