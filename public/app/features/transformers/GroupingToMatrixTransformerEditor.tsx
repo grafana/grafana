a336ab36667fe5439a78fd80354148c5383211e3
Borja Garrido <bgarrido@cern.ch>
2021-09-21 16:37:06 +0200

    Add field selector to form

--- public/app/features/transformers/GroupingToMatrixTransformerEditor.tsx
+++ public/app/features/transformers/GroupingToMatrixTransformerEditor.tsx
@@ -1,62 +1,77 @@
-import React from 'react';
-import { DataTransformerID, standardTransformers, TransformerRegistyItem, TransformerUIProps } from '@grafana/data';
-import { Input } from '@grafana/ui';
+import React, { useCallback } from 'react';
+import {
+  DataTransformerID,
+  SelectableValue,
+  standardTransformers,
+  TransformerRegistryItem,
+  TransformerUIProps,
+} from '@grafana/data';
+import { Select } from '@grafana/ui';
 import { GroupingToMatrixTransformerOptions } from '@grafana/data/src/transformations/transformers/groupingToMatrix';
+import { useAllFieldNamesFromDataFrames } from './utils';
 
 export const GroupingToMatrixTransformerEditor: React.FC<TransformerUIProps<GroupingToMatrixTransformerOptions>> = ({
   input,
   options,
   onChange,
 }) => {
+  const fieldNames = useAllFieldNamesFromDataFrames(input).map((item: string) => ({ label: item, value: item }));
+
+  const onSelectColumn = useCallback(
+    (value: SelectableValue<string>) => {
+      onChange({
+        ...options,
+        columnField: value.value,
+      });
+    },
+    [onChange, options]
+  );
+
+  const onSelectRow = useCallback(
+    (value: SelectableValue<string>) => {
+      onChange({
+        ...options,
+        rowField: value.value,
+      });
+    },
+    [onChange, options]
+  );
+
+  const onSelectValue = useCallback(
+    (value: SelectableValue<string>) => {
+      onChange({
+        ...options,
+        valueField: value.value,
+      });
+    },
+    [onChange, options]
+  );
+
   return (
     <div className="gf-form-inline">
       <div className="gf-form gf-form--grow">
         <div className="gf-form-label width-8">Column</div>
-        <Input
-          className="width-18"
-          placeholder="Choose Column Field"
-          value={options.columnField ?? ''}
-          onChange={input => {
-            onChange({
-              ...options,
-              columnField: input.currentTarget.value,
-            });
-          }}
+        <Select
+          menuShouldPortal
+          options={fieldNames}
+          value={options.columnField}
+          onChange={onSelectColumn}
+          isClearable
         />
       </div>
       <div className="gf-form gf-form--grow">
         <div className="gf-form-label width-8">Row</div>
-        <Input
-          className="width-18"
-          placeholder="Choose Row Field"
-          value={options.rowField ?? ''}
-          onChange={input => {
-            onChange({
-              ...options,
-              rowField: input.currentTarget.value,
-            });
-          }}
-        />
+        <Select menuShouldPortal options={fieldNames} value={options.rowField} onChange={onSelectRow} isClearable />
       </div>
       <div className="gf-form gf-form--grow">
-        <div className="gf-form-label width-8">Value</div>
-        <Input
-          className="width-18"
-          placeholder="Choose Value Field"
-          value={options.valueField ?? ''}
-          onChange={input => {
-            onChange({
-              ...options,
-              valueField: input.currentTarget.value,
-            });
-          }}
-        />
+        <div className="gf-form-label width-8">Cell Value</div>
+        <Select menuShouldPortal options={fieldNames} value={options.valueField} onChange={onSelectValue} isClearable />
       </div>
     </div>
   );
 };
 
-export const groupingToMatrixTransformerRegistryItem: TransformerRegistyItem<GroupingToMatrixTransformerOptions> = {
+export const groupingToMatrixTransformerRegistryItem: TransformerRegistryItem<GroupingToMatrixTransformerOptions> = {
   id: DataTransformerID.groupingToMatrix,
   editor: GroupingToMatrixTransformerEditor,
   transformation: standardTransformers.groupingToMatrixTransformer,
