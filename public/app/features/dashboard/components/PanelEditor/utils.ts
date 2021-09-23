3f9c11d36529c4ba40fc592caaa8fd0667c0b6ac
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2021-09-23 13:58:14 -0400

    Options: support array value paths for panel options (#39499) (#39596)
    (cherry picked from commit 01deae2105aa66eba8e4143a5d3db41c68e22abe)
    
    Co-authored-by: Ryan McKinley <ryantxu@gmail.com>

--- public/app/features/dashboard/components/PanelEditor/utils.ts
+++ public/app/features/dashboard/components/PanelEditor/utils.ts
@@ -68,6 +68,21 @@ export function setOptionImmutably<T extends object>(options: T, path: string | 
   const splat = !Array.isArray(path) ? path.split('.') : path;
 
   const key = splat.shift()!;
+  if (key.endsWith(']')) {
+    const idx = key.lastIndexOf('[');
+    const index = +key.substring(idx + 1, key.length - 1);
+    const propKey = key.substr(0, idx);
+    let current = (options as Record<string, any>)[propKey];
+    const arr = Array.isArray(current) ? [...current] : [];
+    if (splat.length) {
+      if (current == null || typeof current !== 'object') {
+        current = {};
+      }
+      value = setOptionImmutably(current, splat, value);
+    }
+    arr[index] = value;
+    return { ...options, [propKey]: arr };
+  }
 
   if (!splat.length) {
     return { ...options, [key]: value };
