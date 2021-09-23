1e946316e07eca8b27f8badd84f18ea5fc5b1b8f
Maria Alexandra <239999+axelavargas@users.noreply.github.com>
2021-09-23 15:02:30 +0200

    Pipeline: Fix flakiness on Pa11y CI  (#39358)
    * Pipeline: Add wait prop into pa11y configuration for all the pages
    
    * Pa11y: Use e2e instance to generate html report local environment
    
    * Pipeline: use pa11y-ci hash commit
    
    * Pipeline: Update Pa11y thresholds
    
    * Update package.json
    
    * Update yarn.lock
    
    Co-authored-by: Leon Sorokin <leeoniya@gmail.com>

--- .pa11yci-pr.conf.js
+++ .pa11yci-pr.conf.js
@@ -2,6 +2,7 @@ var config = {
   defaults: {
     concurrency: 1,
     runners: ['axe'],
+    useIncognitoBrowserContext: false,
     chromeLaunchConfig: {
       args: ['--no-sandbox'],
     },
@@ -10,79 +11,99 @@ var config = {
   urls: [
     {
       url: '${HOST}/login',
+      wait: 500,
+      rootElement: '.main-view',
+    },
+    {
+      url: '${HOST}/login',
+      wait: 500,
       actions: [
+        "wait for element input[name='user'] to be added",
         "set field input[name='user'] to admin",
         "set field input[name='password'] to admin",
         "click element button[aria-label='Login button']",
         "wait for element [aria-label='Skip change password button'] to be visible",
       ],
-      threshold: 3,
+      threshold: 1,
+      rootElement: '.main-view',
     },
     {
       url: '${HOST}/?orgId=1',
+      wait: 500,
       threshold: 7,
     },
     {
       url: '${HOST}/d/O6f11TZWk/panel-tests-bar-gauge',
-      hideElements: '.sidemenu',
+      wait: 500,
+      rootElement: '.main-view',
       threshold: 2,
     },
     {
       url: '${HOST}/d/O6f11TZWk/panel-tests-bar-gauge?orgId=1&editview=settings',
-      rootElement: '.dashboard-settings',
+      wait: 500,
+      rootElement: '.main-view',
       threshold: 10,
     },
     {
       url: '${HOST}/?orgId=1&search=open',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 15,
+      threshold: 14,
     },
     {
       url: '${HOST}/alerting/list',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 7,
+      threshold: 6,
     },
     {
       url: '${HOST}/datasources',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 36,
+      threshold: 35,
     },
     {
       url: '${HOST}/org/users',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 4,
+      threshold: 3,
     },
     {
       url: '${HOST}/org/teams',
+      wait: 500,
       rootElement: '.main-view',
       threshold: 1,
     },
     {
       url: '${HOST}/plugins',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 41,
+      threshold: 40,
     },
     {
       url: '${HOST}/org',
+      wait: 500,
       rootElement: '.main-view',
       threshold: 2,
     },
     {
       url: '${HOST}/org/apikeys',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 5,
+      threshold: 3,
     },
     {
       url: '${HOST}/dashboards',
+      wait: 500,
       rootElement: '.main-view',
-      threshold: 11,
+      threshold: 7,
     },
   ],
 };
 
 function myPa11yCiConfiguration(urls, defaults) {
   const HOST_SERVER = process.env.HOST || 'localhost';
-  const PORT_SERVER = process.env.PORT || '3000';
+  const PORT_SERVER = process.env.PORT || '3001';
   for (var idx = 0; idx < urls.length; idx++) {
     urls[idx] = { ...urls[idx], url: urls[idx].url.replace('${HOST}', `${HOST_SERVER}:${PORT_SERVER}`) };
   }
