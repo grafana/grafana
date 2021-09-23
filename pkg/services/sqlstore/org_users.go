ea7bf657d174a5bfe6e1ff86f79a9e6822fdb160
Ida Furjesova <ida.furjesova@grafana.com>
2021-09-23 14:10:45 +0200

    Roll back xorm refactor

--- pkg/services/sqlstore/org_users.go
+++ pkg/services/sqlstore/org_users.go
@@ -96,120 +96,116 @@ func (ss *SQLStore) UpdateOrgUser(ctx context.Context, cmd *models.UpdateOrgUser
 }
 
 func (ss *SQLStore) GetOrgUsers(ctx context.Context, query *models.GetOrgUsersQuery) error {
-	return ss.WithDbSession(ctx, func(sess *DBSession) error {
-		query.Result = make([]*models.OrgUserDTO, 0)
+	query.Result = make([]*models.OrgUserDTO, 0)
 
-		session := sess.Table("org_user")
-		session.Join("INNER", x.Dialect().Quote("user"), fmt.Sprintf("org_user.user_id=%s.id", x.Dialect().Quote("user")))
+	sess := x.Table("org_user")
+	sess.Join("INNER", x.Dialect().Quote("user"), fmt.Sprintf("org_user.user_id=%s.id", x.Dialect().Quote("user")))
 
-		whereConditions := make([]string, 0)
-		whereParams := make([]interface{}, 0)
+	whereConditions := make([]string, 0)
+	whereParams := make([]interface{}, 0)
 
-		whereConditions = append(whereConditions, "org_user.org_id = ?")
-		whereParams = append(whereParams, query.OrgId)
+	whereConditions = append(whereConditions, "org_user.org_id = ?")
+	whereParams = append(whereParams, query.OrgId)
 
-		if query.Query != "" {
-			queryWithWildcards := "%" + query.Query + "%"
-			whereConditions = append(whereConditions, "(email "+dialect.LikeStr()+" ? OR name "+dialect.LikeStr()+" ? OR login "+dialect.LikeStr()+" ?)")
-			whereParams = append(whereParams, queryWithWildcards, queryWithWildcards, queryWithWildcards)
-		}
+	if query.Query != "" {
+		queryWithWildcards := "%" + query.Query + "%"
+		whereConditions = append(whereConditions, "(email "+dialect.LikeStr()+" ? OR name "+dialect.LikeStr()+" ? OR login "+dialect.LikeStr()+" ?)")
+		whereParams = append(whereParams, queryWithWildcards, queryWithWildcards, queryWithWildcards)
+	}
 
-		if len(whereConditions) > 0 {
-			session.Where(strings.Join(whereConditions, " AND "), whereParams...)
-		}
+	if len(whereConditions) > 0 {
+		sess.Where(strings.Join(whereConditions, " AND "), whereParams...)
+	}
 
-		if query.Limit > 0 {
-			session.Limit(query.Limit, 0)
-		}
+	if query.Limit > 0 {
+		sess.Limit(query.Limit, 0)
+	}
 
-		session.Cols(
-			"org_user.org_id",
-			"org_user.user_id",
-			"user.email",
-			"user.name",
-			"user.login",
-			"org_user.role",
-			"user.last_seen_at",
-		)
-		session.Asc("user.email", "user.login")
+	sess.Cols(
+		"org_user.org_id",
+		"org_user.user_id",
+		"user.email",
+		"user.name",
+		"user.login",
+		"org_user.role",
+		"user.last_seen_at",
+	)
+	sess.Asc("user.email", "user.login")
 
-		if err := session.Find(&query.Result); err != nil {
-			return err
-		}
+	if err := sess.Find(&query.Result); err != nil {
+		return err
+	}
 
-		for _, user := range query.Result {
-			user.LastSeenAtAge = util.GetAgeString(user.LastSeenAt)
-		}
+	for _, user := range query.Result {
+		user.LastSeenAtAge = util.GetAgeString(user.LastSeenAt)
+	}
 
-		return nil
-	})
+	return nil
 }
 
 func (ss *SQLStore) SearchOrgUsers(ctx context.Context, query *models.SearchOrgUsersQuery) error {
-	return ss.WithDbSession(ctx, func(sess *DBSession) error {
-		query.Result = models.SearchOrgUsersQueryResult{
-			OrgUsers: make([]*models.OrgUserDTO, 0),
-		}
+	query.Result = models.SearchOrgUsersQueryResult{
+		OrgUsers: make([]*models.OrgUserDTO, 0),
+	}
 
-		session := sess.Table("org_user")
-		session.Join("INNER", x.Dialect().Quote("user"), fmt.Sprintf("org_user.user_id=%s.id", x.Dialect().Quote("user")))
+	sess := x.Table("org_user")
+	sess.Join("INNER", x.Dialect().Quote("user"), fmt.Sprintf("org_user.user_id=%s.id", x.Dialect().Quote("user")))
 
-		whereConditions := make([]string, 0)
-		whereParams := make([]interface{}, 0)
+	whereConditions := make([]string, 0)
+	whereParams := make([]interface{}, 0)
 
-		whereConditions = append(whereConditions, "org_user.org_id = ?")
-		whereParams = append(whereParams, query.OrgID)
+	whereConditions = append(whereConditions, "org_user.org_id = ?")
+	whereParams = append(whereParams, query.OrgID)
 
-		if query.Query != "" {
-			queryWithWildcards := "%" + query.Query + "%"
-			whereConditions = append(whereConditions, "(email "+dialect.LikeStr()+" ? OR name "+dialect.LikeStr()+" ? OR login "+dialect.LikeStr()+" ?)")
-			whereParams = append(whereParams, queryWithWildcards, queryWithWildcards, queryWithWildcards)
-		}
+	if query.Query != "" {
+		queryWithWildcards := "%" + query.Query + "%"
+		whereConditions = append(whereConditions, "(email "+dialect.LikeStr()+" ? OR name "+dialect.LikeStr()+" ? OR login "+dialect.LikeStr()+" ?)")
+		whereParams = append(whereParams, queryWithWildcards, queryWithWildcards, queryWithWildcards)
+	}
 
-		if len(whereConditions) > 0 {
-			session.Where(strings.Join(whereConditions, " AND "), whereParams...)
-		}
+	if len(whereConditions) > 0 {
+		sess.Where(strings.Join(whereConditions, " AND "), whereParams...)
+	}
 
-		if query.Limit > 0 {
-			offset := query.Limit * (query.Page - 1)
-			session.Limit(query.Limit, offset)
-		}
+	if query.Limit > 0 {
+		offset := query.Limit * (query.Page - 1)
+		sess.Limit(query.Limit, offset)
+	}
 
-		session.Cols(
-			"org_user.org_id",
-			"org_user.user_id",
-			"user.email",
-			"user.name",
-			"user.login",
-			"org_user.role",
-			"user.last_seen_at",
-		)
-		session.Asc("user.email", "user.login")
+	sess.Cols(
+		"org_user.org_id",
+		"org_user.user_id",
+		"user.email",
+		"user.name",
+		"user.login",
+		"org_user.role",
+		"user.last_seen_at",
+	)
+	sess.Asc("user.email", "user.login")
 
-		if err := session.Find(&query.Result.OrgUsers); err != nil {
-			return err
-		}
+	if err := sess.Find(&query.Result.OrgUsers); err != nil {
+		return err
+	}
 
-		// get total count
-		orgUser := models.OrgUser{}
-		countSess := session.Table("org_user")
+	// get total count
+	orgUser := models.OrgUser{}
+	countSess := x.Table("org_user")
 
-		if len(whereConditions) > 0 {
-			countSess.Where(strings.Join(whereConditions, " AND "), whereParams...)
-		}
+	if len(whereConditions) > 0 {
+		countSess.Where(strings.Join(whereConditions, " AND "), whereParams...)
+	}
 
-		count, err := countSess.Count(&orgUser)
-		if err != nil {
-			return err
-		}
-		query.Result.TotalCount = count
+	count, err := countSess.Count(&orgUser)
+	if err != nil {
+		return err
+	}
+	query.Result.TotalCount = count
 
-		for _, user := range query.Result.OrgUsers {
-			user.LastSeenAtAge = util.GetAgeString(user.LastSeenAt)
-		}
+	for _, user := range query.Result.OrgUsers {
+		user.LastSeenAtAge = util.GetAgeString(user.LastSeenAt)
+	}
 
-		return nil
-	})
+	return nil
 }
 
 func (ss *SQLStore) RemoveOrgUser(ctx context.Context, cmd *models.RemoveOrgUserCommand) error {
