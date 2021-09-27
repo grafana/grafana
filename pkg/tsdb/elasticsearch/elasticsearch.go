c857fb0d49f50475972644bf7d06afd080218703
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2021-09-27 16:25:55 +0200

    Elasticsearch/Prometheus: Set correct service namespace for SigV4 (#39439) (#39664)
    Makes sure to set the SigV4 service namespace for Elasticsearch and Prometheus.
    
    Fixes #38440
    
    (cherry picked from commit 45a844e77ee667265a8f27bfa683d8ea8196336d)

--- pkg/tsdb/elasticsearch/elasticsearch.go
+++ pkg/tsdb/elasticsearch/elasticsearch.go
@@ -88,6 +88,11 @@ func newInstanceSettings() datasource.InstanceFactoryFunc {
 			return nil, fmt.Errorf("error getting http options: %w", err)
 		}
 
+		// Set SigV4 service namespace
+		if httpCliOpts.SigV4 != nil {
+			httpCliOpts.SigV4.Service = "es"
+		}
+
 		version, err := coerceVersion(jsonData["esVersion"])
 
 		if err != nil {
