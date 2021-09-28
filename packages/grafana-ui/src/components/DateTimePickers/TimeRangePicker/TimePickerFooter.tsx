07f9932b6453cab13ee2164eed80e1b26245d4cc
Oscar Kilhed <oscar.kilhed@grafana.com>
2021-09-28 09:52:29 +0200

    Add more months

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimePickerFooter.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimePickerFooter.tsx
@@ -111,6 +111,14 @@ export const TimePickerFooter: FC<Props> = (props) => {
                     { label: 'February', value: 1 },
                     { label: 'March', value: 2 },
                     { label: 'April', value: 3 },
+                    { label: 'May', value: 4 },
+                    { label: 'June', value: 5 },
+                    { label: 'July', value: 6 },
+                    { label: 'August', value: 7 },
+                    { label: 'September', value: 8 },
+                    { label: 'October', value: 9 },
+                    { label: 'November', value: 10 },
+                    { label: 'December', value: 11 },
                   ]}
                   onChange={(value) => {
                     if (onChangeFiscalYearStartMonth) {
