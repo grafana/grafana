0ce68d4b86fd5acaede097cc2c98db4117430278
Dominik Prokop <dominik.prokop@grafana.com>
2021-09-29 09:35:41 +0200

    Table panel: Make filter case insensitive (#39746)
    * Expose FilterInput from grafana/ui
    
    * Make table filter case insensitive
    
    * Update packages/grafana-ui/src/components/Table/FilterList.tsx
    
    Co-authored-by: Hugo Häggmark <hugo.haggmark@grafana.com>
    
    Co-authored-by: Oscar Kilhed <oscar.kilhed@grafana.com>
    Co-authored-by: Hugo Häggmark <hugo.haggmark@grafana.com>
    (cherry picked from commit f887576a276712e20d2dd491feea1ee2918ddebb)

--- public/app/features/library-panels/components/LibraryPanelsSearch/LibraryPanelsSearch.tsx
+++ public/app/features/library-panels/components/LibraryPanelsSearch/LibraryPanelsSearch.tsx
@@ -1,8 +1,7 @@
 import React, { useReducer } from 'react';
-import { HorizontalGroup, useStyles2, VerticalGroup } from '@grafana/ui';
+import { HorizontalGroup, useStyles2, VerticalGroup, FilterInput } from '@grafana/ui';
 import { GrafanaTheme2, PanelPluginMeta, SelectableValue } from '@grafana/data';
 import { css } from '@emotion/css';
-import { FilterInput } from '../../../../core/components/FilterInput/FilterInput';
 import { SortPicker } from '../../../../core/components/Select/SortPicker';
 import { PanelTypeFilter } from '../../../../core/components/PanelTypeFilter/PanelTypeFilter';
 import { LibraryPanelsView } from '../LibraryPanelsView/LibraryPanelsView';
