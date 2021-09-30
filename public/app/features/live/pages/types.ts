ecf9733a66e22a7e47271312aaca16ec7793f979
An <an.le@grafana.com>
2021-09-30 16:37:31 -0400

    add: rule settings editor (#39875)

--- public/app/features/live/pages/types.ts
+++ public/app/features/live/pages/types.ts
@@ -1,21 +1,23 @@
-export interface Converter {
-  type: string;
+import { SelectableValue } from '@grafana/data';
+export interface Converter extends RuleSetting {
   [t: string]: any;
 }
 
-export interface Processor {
-  type: string;
+export interface Processor extends RuleSetting {
   [t: string]: any;
 }
 
-export interface Output {
-  type: string;
+export interface Output extends RuleSetting {
   [t: string]: any;
   multiple?: {
     outputs: Output[];
   };
 }
 
+export interface RuleSetting<T = any> {
+  type: string;
+  [key: string]: any;
+}
 export interface RuleSettings {
   converter?: Converter;
   processor?: Processor;
@@ -34,4 +36,24 @@ export interface Pipeline {
 export interface GrafanaCloudBackend {
   uid: string;
   settings: any;
+}
+
+export type RuleType = 'converter' | 'processor' | 'output';
+
+export interface PipelineListOption {
+  type: string;
+  description: string;
+  example: object;
+}
+export interface EntitiesTypes {
+  converters: PipelineListOption[];
+  processors: PipelineListOption[];
+  outputs: PipelineListOption[];
+}
+
+export interface PipeLineEntitiesInfo {
+  converter: SelectableValue[];
+  processor: SelectableValue[];
+  output: SelectableValue[];
+  getExample: (rule: RuleType, type: string) => object;
 }
