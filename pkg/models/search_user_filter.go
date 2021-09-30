9298d44721c12a02fcc36a9aadbcf70a729a1e35
spinillos <selenepinillos@gmail.com>
2021-09-30 10:49:33 +0200

    Back activeLast30Days filter to OSS

--- pkg/models/search_user_filter.go
+++ pkg/models/search_user_filter.go
@@ -1,6 +1,13 @@
 package models
 
-import "github.com/grafana/grafana/pkg/infra/log"
+import (
+	"strconv"
+	"time"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+)
+
+const activeLast30Days = "activeLast30Days"
 
 type SearchUserFilter interface {
 	GetFilter(filterName string, params []string) Filter
@@ -37,8 +44,10 @@ type OSSSearchUserFilter struct {
 }
 
 func ProvideOSSSearchUserFilter() *OSSSearchUserFilter {
+	filters := make(map[string]FilterHandler)
+	filters[activeLast30Days] = NewActiveLast30DaysFilter
 	return &OSSSearchUserFilter{
-		filters: make(map[string]FilterHandler, 0),
+		filters: filters,
 	}
 }
 
@@ -57,4 +66,39 @@ func (o *OSSSearchUserFilter) GetFilter(filterName string, params []string) Filt
 
 func (o *OSSSearchUserFilter) GetFilterList() map[string]FilterHandler {
 	return o.filters
+}
+
+type ActiveLast30DaysFilter struct {
+	active bool
+}
+
+func NewActiveLast30DaysFilter(params []string) (Filter, error) {
+	active, err := strconv.ParseBool(params[0])
+	if err != nil {
+		return nil, err
+	}
+	return &ActiveLast30DaysFilter{active: active}, nil
+}
+
+func (a *ActiveLast30DaysFilter) WhereCondition() *WhereCondition {
+	if !a.active {
+		return nil
+	}
+	return &WhereCondition{
+		Condition: "last_seen_at > ?",
+		Params:    a.whereParams(),
+	}
+}
+
+func (a *ActiveLast30DaysFilter) JoinCondition() *JoinCondition {
+	return nil
+}
+
+func (a *ActiveLast30DaysFilter) InCondition() *InCondition {
+	return nil
+}
+
+func (a *ActiveLast30DaysFilter) whereParams() interface{} {
+	activeUserTimeLimit := time.Hour * 24 * 30
+	return time.Now().Add(-activeUserTimeLimit)
 }
