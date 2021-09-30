162dab82de6c528244dfe3753ad4ac9099d54cf2
Gábor Farkas <gabor.farkas@gmail.com>
2021-09-30 15:50:02 +0200

    Prometheus: metrics browser: handle label values with special characters (#39713)
    * prometheus: handle label-values with special characters
    
    * added comment
    
    (cherry picked from commit d363c368536d1d0da82392d4aa102953ceb0b25e)

--- public/app/plugins/datasource/prometheus/components/PrometheusMetricsBrowser.tsx
+++ public/app/plugins/datasource/prometheus/components/PrometheusMetricsBrowser.tsx
@@ -10,6 +10,7 @@ import {
   BrowserLabel as PromLabel,
 } from '@grafana/ui';
 import PromQlLanguageProvider from '../language_provider';
+import { escapeLabelValueInExactSelector, escapeLabelValueInRegexSelector } from '../language_utils';
 import { css, cx } from '@emotion/css';
 import store from 'app/core/store';
 import { FixedSizeList } from 'react-window';
@@ -65,12 +66,12 @@ export function buildSelector(labels: SelectableLabel[]): string {
     if ((label.name === METRIC_LABEL || label.selected) && label.values && label.values.length > 0) {
       const selectedValues = label.values.filter((value) => value.selected).map((value) => value.name);
       if (selectedValues.length > 1) {
-        selectedLabels.push(`${label.name}=~"${selectedValues.join('|')}"`);
+        selectedLabels.push(`${label.name}=~"${selectedValues.map(escapeLabelValueInRegexSelector).join('|')}"`);
       } else if (selectedValues.length === 1) {
         if (label.name === METRIC_LABEL) {
           singleMetric = selectedValues[0];
         } else {
-          selectedLabels.push(`${label.name}="${selectedValues[0]}"`);
+          selectedLabels.push(`${label.name}="${escapeLabelValueInExactSelector(selectedValues[0])}"`);
         }
       }
     }
