cfefa1ba55ded1355982d0ce9494d5a6bd394617
Jack Westbrook <jack.westbrook@gmail.com>
2021-10-01 14:22:44 +0200

    feat(toolkit): set testId to match the Icon name prop in react-inlinesvg mock

--- packages/grafana-toolkit/src/config/react-inlinesvg.tsx
+++ packages/grafana-toolkit/src/config/react-inlinesvg.tsx
@@ -14,8 +14,12 @@ export interface StorageItem {
 
 export const cacheStore: { [key: string]: StorageItem } = Object.create(null);
 
+const SVG_FILE_NAME_REGEX = /(.+)\/(.+)\.svg$/;
+
 const InlineSVG = ({ src }: { src: string }) => {
-  return <svg xmlns="http://www.w3.org/2000/svg" data-testid={src} viewBox="0 0 24 24" />;
+  // testId will be the file name without extension (e.g. `public/img/icons/angle-double-down.svg` -> `angle-double-down`)
+  const testId = src.replace(SVG_FILE_NAME_REGEX, '$2');
+  return <svg xmlns="http://www.w3.org/2000/svg" data-testid={testId} viewBox="0 0 24 24" />;
 };
 
 export default InlineSVG;
