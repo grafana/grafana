0b34584ee8874f0b39052e2bfaef843687f64eb3
Tania B <yalyna.ts@gmail.com>
2021-10-01 15:39:57 +0300

    Security: Add secrets service (#39418)
    * Add secrets service
    
    * Revert accidental changes in util encryption
    
    * Make minor changes
    
    Move functional options to models
    
    Revert renaming types to models
    
    * Add context
    
    * Minor change in GetDataKey
    
    * Use CreateDataKeyWithDBSession in CreateDataKey
    
    * Handle empty DEK name in DeleteDataKey
    
    * Rename defaultProvider
    
    * Remove secrets store service

--- pkg/util/encryption_test.go
+++ pkg/util/encryption_test.go
@@ -28,7 +28,7 @@ func TestEncryption(t *testing.T) {
 		assert.Equal(t, []byte("grafana"), decrypted)
 	})
 
-	t.Run("decrypting empty payload should not fail", func(t *testing.T) {
+	t.Run("decrypting empty payload should fail", func(t *testing.T) {
 		_, err := Decrypt([]byte(""), "1234")
 		require.Error(t, err)
 
