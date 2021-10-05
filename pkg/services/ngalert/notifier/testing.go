13eb57b952601836390d0af99d803dd713eb2a2f
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2021-10-05 09:11:36 +0200

    implement missing func

--- pkg/services/ngalert/notifier/testing.go
+++ pkg/services/ngalert/notifier/testing.go
@@ -65,6 +65,10 @@ func (f *FakeOrgStore) GetOrgs(_ context.Context) ([]int64, error) {
 	return f.orgs, nil
 }
 
+func (f *FakeOrgStore) DeleteOrgEntries(_ context.Context, _ int64, _ string) error {
+	return nil
+}
+
 type FakeKVStore struct {
 	mtx   sync.Mutex
 	store map[int64]map[string]map[string]string
