d3245db9e4bb2e283831d15f4a6804bcaac1fd61
Will Browne <will.browne@grafana.com>
2021-10-08 11:52:01 +0100

    fix bad merge

--- public/app/plugins/panel/canvas/editor/elementEditor.tsx
+++ public/app/plugins/panel/canvas/editor/elementEditor.tsx
@@ -1,123 +1,75 @@
-import React, { FC, useMemo } from 'react';
-import { Select } from '@grafana/ui';
-import { DataFrame, PanelOptionsEditorBuilder, StandardEditorContext } from '@grafana/data';
-import { OptionsPaneCategoryDescriptor } from 'app/features/dashboard/components/PanelEditor/OptionsPaneCategoryDescriptor';
+import { cloneDeep, get as lodashGet } from 'lodash';
+import { optionBuilder } from './options';
+import { CanvasElementOptions, canvasElementRegistry, DEFAULT_CANVAS_ELEMENT_CONFIG } from 'app/features/canvas';
+import { NestedPanelOptions, NestedValueAccess } from '@grafana/data/src/utils/OptionsUIBuilders';
 import { setOptionImmutably } from 'app/features/dashboard/components/PanelEditor/utils';
-import { fillOptionsPaneItems } from 'app/features/dashboard/components/PanelEditor/getVizualizationOptions';
-import { cloneDeep } from 'lodash';
-import { addBackgroundOptions, addBorderOptions } from './options';
-import {
-  CanvasElementItem,
-  CanvasElementOptions,
-  canvasElementRegistry,
-  DEFAULT_CANVAS_ELEMENT_CONFIG,
-} from 'app/features/canvas';
+import { ElementState } from 'app/features/canvas/runtime/element';
+import { Scene } from 'app/features/canvas/runtime/scene';
 
-export interface CanvasElementEditorProps<TConfig = any> {
-  options?: CanvasElementOptions<TConfig>;
-  data: DataFrame[]; // All results
-  onChange: (options: CanvasElementOptions<TConfig>) => void;
-  filter?: (item: CanvasElementItem) => boolean;
+export interface CanvasEditorOptions {
+  element: ElementState;
+  scene: Scene;
+  category?: string[];
 }
 
-export const CanvasElementEditor: FC<CanvasElementEditorProps> = ({ options, onChange, data, filter }) => {
-  // all basemaps
-  const layerTypes = useMemo(() => {
-    return canvasElementRegistry.selectOptions(
-      options?.type // the selected value
-        ? [options.type] // as an array
-        : [DEFAULT_CANVAS_ELEMENT_CONFIG.type],
-      filter
-    );
-  }, [options?.type, filter]);
+export function getElementEditor(opts: CanvasEditorOptions): NestedPanelOptions<CanvasElementOptions> {
+  return {
+    category: opts.category,
+    path: '--', // not used!
 
-  // The options change with each layer type
-  const optionsEditorBuilder = useMemo(() => {
-    const layer = canvasElementRegistry.getIfExists(options?.type);
-    if (!layer || !layer.registerOptionsUI) {
-      return null;
-    }
-
-    const builder = new PanelOptionsEditorBuilder<CanvasElementOptions>();
-    if (layer.registerOptionsUI) {
-      layer.registerOptionsUI(builder);
-    }
-
-    addBackgroundOptions(builder);
-    addBorderOptions(builder);
-    return builder;
-  }, [options?.type]);
-
-  // The react componnets
-  const layerOptions = useMemo(() => {
-    const layer = canvasElementRegistry.getIfExists(options?.type);
-    if (!optionsEditorBuilder || !layer) {
-      return null;
-    }
-
-    const category = new OptionsPaneCategoryDescriptor({
-      id: 'CanvasElement config',
-      title: 'CanvasElement config',
-    });
-
-    const context: StandardEditorContext<any> = {
-      data,
-      options: options,
-    };
-
-    const currentOptions = { ...options, type: layer.id, config: { ...layer.defaultConfig, ...options?.config } };
-
-    // Update the panel options if not set
-    if (!options || (layer.defaultConfig && !options.config)) {
-      onChange(currentOptions as any);
-    }
-
-    const reg = optionsEditorBuilder.getRegistry();
-
-    // Load the options into categories
-    fillOptionsPaneItems(
-      reg.list(),
-
-      // Always use the same category
-      (categoryNames) => category,
-
-      // Custom upate function
-      (path: string, value: any) => {
-        onChange(setOptionImmutably(currentOptions, path, value) as any);
+    // Note that canvas editor writes things to the scene!
+    values: (parent: NestedValueAccess) => ({
+      getValue: (path: string) => {
+        return lodashGet(opts.element.options, path);
       },
-      context
-    );
-
-    return (
-      <>
-        <br />
-        {category.items.map((item) => item.render())}
-      </>
-    );
-  }, [optionsEditorBuilder, onChange, data, options]);
-
-  return (
-    <div>
-      <Select
-        menuShouldPortal
-        options={layerTypes.options}
-        value={layerTypes.current}
-        onChange={(v) => {
-          const layer = canvasElementRegistry.getIfExists(v.value);
+      onChange: (path: string, value: any) => {
+        let options = opts.element.options;
+        if (path === 'type' && value) {
+          const layer = canvasElementRegistry.getIfExists(value);
           if (!layer) {
-            console.warn('layer does not exist', v);
+            console.warn('layer does not exist', value);
             return;
           }
-
-          onChange({
+          options = {
             ...options, // keep current options
             type: layer.id,
             config: cloneDeep(layer.defaultConfig ?? {}),
-          });
-        }}
-      />
+          };
+        } else {
+          options = setOptionImmutably(options, path, value);
+        }
+        opts.scene.onChange(opts.element.UID, options);
+      },
+    }),
 
-      {layerOptions}
-    </div>
-  );
-};
+    // Dynamically fill the selected element
+    build: (builder, context) => {
+      const { options } = opts.element;
+      const layerTypes = canvasElementRegistry.selectOptions(
+        options?.type // the selected value
+          ? [options.type] // as an array
+          : [DEFAULT_CANVAS_ELEMENT_CONFIG.type]
+      );
+
+      builder.addSelect({
+        path: 'type',
+        name: undefined as any, // required, but hide space
+        settings: {
+          options: layerTypes.options,
+        },
+      });
+
+      // force clean layer configuration
+      const layer = canvasElementRegistry.getIfExists(options?.type ?? DEFAULT_CANVAS_ELEMENT_CONFIG.type)!;
+      const currentOptions = { ...options, type: layer.id, config: { ...layer.defaultConfig, ...options?.config } };
+      const ctx = { ...context, options: currentOptions };
+
+      if (layer.registerOptionsUI) {
+        layer.registerOptionsUI(builder, ctx);
+      }
+
+      optionBuilder.addBackground(builder, ctx);
+      optionBuilder.addBorder(builder, ctx);
+    },
+  };
+}
