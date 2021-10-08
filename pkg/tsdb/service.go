cca5a79a9be35f82f6b6a18b49aada3596c86cc6
idafurjes <36131195+idafurjes@users.noreply.github.com>
2021-10-08 14:46:35 +0200

    CloudMonitoring: Migrate to use backend plugin SDK contracts (#38650)
    * Use SDK contracts for cloudmonitoring
    
    * Get build running, tests passing and do some refactoring (#38754)
    
    * fix build+tests and refactor
    
    * remove alerting stuff
    
    * remove unused field
    
    * fix plugin fetch
    
    * end to end
    
    * resp rename
    
    * tidy annotations
    
    * reformatting
    
    * update refID
    
    * reformat imports
    
    * fix styling
    
    * clean up unmarshalling
    
    * uncomment + fix tests
    
    * appease linter
    
    * remove spaces
    
    * remove old cruft
    
    * add check for empty queries
    
    * update tests
    
    * remove pm as dep
    
    * adjust proxy route contract
    
    * fix service loading
    
    * use UNIX val
    
    * fix endpoint + resp
    
    * h@ckz for frontend
    
    * fix resp
    
    * fix interval
    
    * always set custom meta
    
    * remove unused param
    
    * fix labels fetch
    
    * fix linter
    
    * fix test + remove unused field
    
    * apply pr feedback
    
    * fix grafana-auto intervals
    
    * fix tests
    
    * resolve conflicts
    
    * fix bad merge
    
    * fix conflicts
    
    * remove bad logger import
    
    Co-authored-by: Will Browne <wbrowne@users.noreply.github.com>
    Co-authored-by: Will Browne <will.browne@grafana.com>

--- pkg/tsdb/service.go
+++ pkg/tsdb/service.go
@@ -2,7 +2,6 @@ package tsdb
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/plugins"
@@ -10,72 +9,35 @@ import (
 	"github.com/grafana/grafana/pkg/services/datasources"
 	"github.com/grafana/grafana/pkg/services/oauthtoken"
 	"github.com/grafana/grafana/pkg/setting"
-	"github.com/grafana/grafana/pkg/tsdb/cloudmonitoring"
 	_ "github.com/grafana/grafana/pkg/tsdb/postgres"
 )
 
 // NewService returns a new Service.
 func NewService(
-	cfg *setting.Cfg,
-	pluginManager plugins.Manager,
-	backendPluginManager backendplugin.Manager,
-	oauthTokenService *oauthtoken.Service,
-	dataSourcesService *datasources.Service,
-	cloudMonitoringService *cloudmonitoring.Service,
-) *Service {
-	s := newService(cfg, pluginManager, backendPluginManager, oauthTokenService, dataSourcesService)
-
-	// register backend data sources using legacy plugin
-	// contracts/non-SDK contracts
-	s.registry["stackdriver"] = cloudMonitoringService.NewExecutor
-
-	return s
+	cfg *setting.Cfg, backendPluginManager backendplugin.Manager,
+	oauthTokenService *oauthtoken.Service, dataSourcesService *datasources.Service) *Service {
+	return newService(cfg, backendPluginManager, oauthTokenService, dataSourcesService)
 }
 
-func newService(cfg *setting.Cfg, manager plugins.Manager, backendPluginManager backendplugin.Manager,
+func newService(cfg *setting.Cfg, backendPluginManager backendplugin.Manager,
 	oauthTokenService oauthtoken.OAuthTokenService, dataSourcesService *datasources.Service) *Service {
 	return &Service{
 		Cfg:                  cfg,
-		PluginManager:        manager,
 		BackendPluginManager: backendPluginManager,
-		// nolint:staticcheck // plugins.DataPlugin deprecated
-		registry:           map[string]func(*models.DataSource) (plugins.DataPlugin, error){},
-		OAuthTokenService:  oauthTokenService,
-		DataSourcesService: dataSourcesService,
+		OAuthTokenService:    oauthTokenService,
+		DataSourcesService:   dataSourcesService,
 	}
 }
 
 // Service handles data requests to data sources.
 type Service struct {
 	Cfg                  *setting.Cfg
-	PluginManager        plugins.Manager
 	BackendPluginManager backendplugin.Manager
 	OAuthTokenService    oauthtoken.OAuthTokenService
 	DataSourcesService   *datasources.Service
-	//nolint: staticcheck // plugins.DataPlugin deprecated
-	registry map[string]func(*models.DataSource) (plugins.DataPlugin, error)
 }
 
 //nolint: staticcheck // plugins.DataPlugin deprecated
 func (s *Service) HandleRequest(ctx context.Context, ds *models.DataSource, query plugins.DataQuery) (plugins.DataResponse, error) {
-	if factory, exists := s.registry[ds.Type]; exists {
-		var err error
-		plugin, err := factory(ds)
-		if err != nil {
-			//nolint: staticcheck // plugins.DataPlugin deprecated
-			return plugins.DataResponse{}, fmt.Errorf("could not instantiate endpoint for data plugin %q: %w",
-				ds.Type, err)
-		}
-
-		return plugin.DataQuery(ctx, ds, query)
-	}
-	return dataPluginQueryAdapter(ds.Type, s.BackendPluginManager, s.OAuthTokenService, s.DataSourcesService).
-		DataQuery(ctx, ds, query)
-}
-
-// RegisterQueryHandler registers a query handler factory.
-// This is only exposed for tests!
-//nolint: staticcheck // plugins.DataPlugin deprecated
-func (s *Service) RegisterQueryHandler(name string, factory func(*models.DataSource) (plugins.DataPlugin, error)) {
-	s.registry[name] = factory
+	return dataPluginQueryAdapter(ds.Type, s.BackendPluginManager, s.OAuthTokenService, s.DataSourcesService).DataQuery(ctx, ds, query)
 }
