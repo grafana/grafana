e05a59d1621c2f06f985fbb5da64277f49008923
Karl Persson <kalle.persson@grafana.com>
2021-10-14 17:52:58 +0200

    remove unused function

--- pkg/services/accesscontrol/database/database.go
+++ pkg/services/accesscontrol/database/database.go
@@ -3,9 +3,6 @@ package database
 import (
 	"strings"
 
-	acmodels "github.com/grafana/grafana/pkg/extensions/accesscontrol"
-	"github.com/grafana/grafana/pkg/services/accesscontrol"
-
 	"github.com/grafana/grafana/pkg/services/sqlstore"
 )
 
@@ -15,23 +12,6 @@ func ProvideService(sqlStore *sqlstore.SQLStore) *AccessControlStore {
 
 type AccessControlStore struct {
 	sql *sqlstore.SQLStore
-}
-
-func getRoleByName(sess *sqlstore.DBSession, name string, orgID int64) (*accesscontrol.Role, error) {
-	if name == "" {
-		return nil, acmodels.ErrRoleNameRequired
-	}
-
-	role := &accesscontrol.Role{OrgID: orgID, Name: name}
-	has, err := sess.Where("org_id = ? AND name = ?", orgID, name).Get(role)
-	if !has {
-		return nil, acmodels.ErrRoleNotFound
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return role, nil
 }
 
 func deletePermissions(sess *sqlstore.DBSession, ids []int64) error {
