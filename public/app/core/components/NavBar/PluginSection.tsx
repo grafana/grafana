1246440178df36548e9c18a7a7cca278cc00c3f8
Ashley Harrison <ashley.harrison@grafana.com>
2021-10-15 15:27:53 +0100

    Navigation: Only render plugin section if there are items

--- public/app/core/components/NavBar/PluginSection.tsx
+++ public/app/core/components/NavBar/PluginSection.tsx
@@ -17,23 +17,27 @@ const TopSection = () => {
   const activeItemId = pluginLinks.find((item) => isLinkActive(location.pathname, item))?.id;
 
   return (
-    <div data-testid="plugin-section-items" className={styles.container}>
-      {pluginLinks.map((link, index) => {
-        return (
-          <NavBarItem
-            key={`${link.id}-${index}`}
-            isActive={!isSearchActive(location) && activeItemId === link.id}
-            label={link.text}
-            menuItems={link.children}
-            target={link.target}
-            url={link.url}
-          >
-            {link.icon && <Icon name={link.icon as IconName} size="xl" />}
-            {link.img && <img src={link.img} alt={`${link.text} logo`} />}
-          </NavBarItem>
-        );
-      })}
-    </div>
+    <>
+      {pluginLinks.length > 0 && (
+        <div data-testid="plugin-section-items" className={styles.container}>
+          {pluginLinks.map((link, index) => {
+            return (
+              <NavBarItem
+                key={`${link.id}-${index}`}
+                isActive={!isSearchActive(location) && activeItemId === link.id}
+                label={link.text}
+                menuItems={link.children}
+                target={link.target}
+                url={link.url}
+              >
+                {link.icon && <Icon name={link.icon as IconName} size="xl" />}
+                {link.img && <img src={link.img} alt={`${link.text} logo`} />}
+              </NavBarItem>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
