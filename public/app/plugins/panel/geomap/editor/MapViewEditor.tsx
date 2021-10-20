4a7abae6ae7fc621f07e5b85e09980c2df28aec7
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2021-10-20 15:36:04 +0100

    Geomap: Avoid exceptions when missing data and values (heatmap and zoom settings) (#40664)
    (cherry picked from commit 43878ff05f29e306d26c4a781f6082d9fb09b23e)
    
    Co-authored-by: nikki-kiga <42276368+nikki-kiga@users.noreply.github.com>

--- public/app/plugins/panel/geomap/editor/MapViewEditor.tsx
+++ public/app/plugins/panel/geomap/editor/MapViewEditor.tsx
@@ -49,9 +49,9 @@ export const MapViewEditor: FC<StandardEditorProps<MapViewConfig, any, GeomapPan
         onChange({
           ...value,
           id: v.id,
-          lat: v.lat ?? value.lat,
-          lon: v.lon ?? value.lon,
-          zoom: v.zoom ?? value.zoom,
+          lat: v.lat ?? value?.lat,
+          lon: v.lon ?? value?.lon,
+          zoom: v.zoom ?? value?.zoom,
         });
       }
     },
@@ -99,7 +99,7 @@ export const MapViewEditor: FC<StandardEditorProps<MapViewConfig, any, GeomapPan
       <InlineFieldRow>
         <InlineField label="Zoom" labelWidth={labelWidth} grow={true}>
           <NumberInput
-            value={value.zoom ?? 1}
+            value={value?.zoom ?? 1}
             min={1}
             max={18}
             step={0.01}
