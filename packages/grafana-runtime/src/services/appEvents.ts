13e2bf11fb00af3b2ef198115b2d1faa0d395271
Ryan McKinley <ryantxu@gmail.com>
2021-10-20 12:03:44 -0700

    add action

--- packages/grafana-runtime/src/services/appEvents.ts
+++ packages/grafana-runtime/src/services/appEvents.ts
@@ -1,17 +1,37 @@
 import { BusEventBase, BusEventWithPayload, EventBus, GrafanaTheme2, PanelModel, TimeRange } from '@grafana/data';
 
+/**
+ * Called when a dashboard is refreshed
+ *
+ * @public
+ */
 export class RefreshEvent extends BusEventBase {
   static type = 'refresh';
 }
 
+/**
+ * Called when the theme settings change
+ *
+ * @public
+ */
 export class ThemeChangedEvent extends BusEventWithPayload<GrafanaTheme2> {
   static type = 'theme-changed';
 }
 
+/**
+ * Called when time range is updated
+ *
+ * @public
+ */
 export class TimeRangeUpdatedEvent extends BusEventWithPayload<TimeRange> {
   static type = 'time-range-updated';
 }
 
+/**
+ * Called to copy a panel JSON into local storage
+ *
+ * @public
+ */
 export class CopyPanelEvent extends BusEventWithPayload<PanelModel> {
   static type = 'copy-panel';
 }
