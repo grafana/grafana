397114387ab18917b0359587ee203c8f1db037cd
Ryan McKinley <ryantxu@gmail.com>
2021-10-20 11:31:55 -0700

    expose appEvents

--- public/app/angular/panel/panel_directive.ts
+++ public/app/angular/panel/panel_directive.ts
@@ -4,8 +4,9 @@ import { PanelEvents } from '@grafana/data';
 import { PanelModel } from '../../features/dashboard/state';
 import { PanelCtrl } from './panel_ctrl';
 import { Subscription } from 'rxjs';
-import { PanelDirectiveReadyEvent, RefreshEvent, RenderEvent } from 'app/types/events';
+import { PanelDirectiveReadyEvent, RenderEvent } from 'app/types/events';
 import { coreModule } from 'app/core/core_module';
+import { RefreshEvent } from '@grafana/runtime';
 
 const panelTemplate = `
   <ng-transclude class="panel-height-helper"></ng-transclude>
