74fb491b6af1e54e69dbfb8c88b476f449b29eed
gotjosh <josue@grafana.com>
2021-10-22 10:11:06 +0100

    Alerting: Validate contact point configuration during migration to Unified Alerting (#40717)
    * Alerting: Validate contact point configuration during the migration
    
    This minimises the chances of generating broken configuration as part of the migration. Originally, we wanted to generate it and not produce a hard stop in Grafana but this strategy has the chance to avoid delivering notifications for our users.
    
    We now think it's better to hard stop the migration and let the user take care of resolving the configuration manually.

--- pkg/services/ngalert/notifier/channels/telegram.go
+++ pkg/services/ngalert/notifier/channels/telegram.go
@@ -9,7 +9,6 @@ import (
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
-	old_notifiers "github.com/grafana/grafana/pkg/services/alerting/notifiers"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/prometheus/alertmanager/template"
 	"github.com/prometheus/alertmanager/types"
@@ -22,7 +21,7 @@ var (
 // TelegramNotifier is responsible for sending
 // alert notifications to Telegram.
 type TelegramNotifier struct {
-	old_notifiers.NotifierBase
+	*Base
 	BotToken string
 	ChatID   string
 	Message  string
@@ -49,7 +48,7 @@ func NewTelegramNotifier(model *NotificationChannelConfig, t *template.Template,
 	}
 
 	return &TelegramNotifier{
-		NotifierBase: old_notifiers.NewNotifierBase(&models.AlertNotification{
+		Base: NewBase(&models.AlertNotification{
 			Uid:                   model.UID,
 			Name:                  model.Name,
 			Type:                  model.Type,
