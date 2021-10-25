c34db60c9b008e95928cc362e9a9d54acf4cd189
Marcus Andersson <marcus.andersson@grafana.com>
2021-10-25 11:01:49 +0200

    moved the permission check to the datasource component.

--- public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithDataSource.tsx
+++ public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithDataSource.tsx
@@ -1,6 +1,8 @@
 import { DataSourcePluginMeta } from '@grafana/data';
 import { Button } from '@grafana/ui';
+import { contextSrv } from 'app/core/services/context_srv';
 import { addDataSource } from 'app/features/datasources/state/actions';
+import { AccessControlAction } from 'app/types';
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { CatalogPlugin } from '../../types';
@@ -9,7 +11,7 @@ type Props = {
   plugin: CatalogPlugin;
 };
 
-export function GetStartedWithDataSource({ plugin }: Props): React.ReactElement {
+export function GetStartedWithDataSource({ plugin }: Props): React.ReactElement | null {
   const dispatch = useDispatch();
   const onAddDataSource = useCallback(() => {
     const meta = {
@@ -20,9 +22,20 @@ export function GetStartedWithDataSource({ plugin }: Props): React.ReactElement 
     dispatch(addDataSource(meta));
   }, [dispatch, plugin]);
 
+  if (!hasPermissionToCreateDataSource()) {
+    return null;
+  }
+
   return (
     <Button variant="primary" onClick={onAddDataSource}>
       Create a {plugin.name} data source
     </Button>
+  );
+}
+
+function hasPermissionToCreateDataSource(): boolean {
+  return (
+    contextSrv.hasPermission(AccessControlAction.DataSourcesCreate) &&
+    contextSrv.hasPermission(AccessControlAction.DataSourcesWrite)
   );
 }
--- public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithPlugin.tsx
+++ public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithPlugin.tsx
@@ -2,8 +2,6 @@ import React, { ReactElement } from 'react';
 import { PluginType } from '@grafana/data';
 import { CatalogPlugin } from '../../types';
 import { GetStartedWithDataSource } from './GetStartedWithDataSource';
-import { AccessControlAction } from 'app/types';
-import { contextSrv } from 'app/core/core';
 
 type Props = {
   plugin: CatalogPlugin;
@@ -14,26 +12,10 @@ export function GetStartedWithPlugin({ plugin }: Props): ReactElement | null {
     return null;
   }
 
-  if (!hasProperPermissions(plugin.type)) {
-    return null;
-  }
-
   switch (plugin.type) {
     case PluginType.datasource:
       return <GetStartedWithDataSource plugin={plugin} />;
     default:
       return null;
-  }
-}
-
-function hasProperPermissions(pluginType: PluginType | undefined): boolean {
-  switch (pluginType) {
-    case PluginType.datasource:
-      return (
-        contextSrv.hasPermission(AccessControlAction.DataSourcesCreate) &&
-        contextSrv.hasPermission(AccessControlAction.DataSourcesWrite)
-      );
-    default:
-      return true;
   }
 }
