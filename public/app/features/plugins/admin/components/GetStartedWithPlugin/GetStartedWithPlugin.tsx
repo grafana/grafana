64fc890ad41a3690b0cfc96573540887452a590d
Marcus Andersson <marcus.andersson@grafana.com>
2021-10-25 10:50:55 +0200

    added missing permissions check

--- public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithPlugin.tsx
+++ public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithPlugin.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from 'react';
 import { PluginType } from '@grafana/data';
 import { CatalogPlugin } from '../../types';
 import { GetStartedWithDataSource } from './GetStartedWithDataSource';
+import { AccessControlAction } from 'app/types';
+import { contextSrv } from 'app/core/core';
 
 type Props = {
   plugin: CatalogPlugin;
@@ -12,10 +14,26 @@ export function GetStartedWithPlugin({ plugin }: Props): ReactElement | null {
     return null;
   }
 
+  if (!hasProperPermissions(plugin.type)) {
+    return null;
+  }
+
   switch (plugin.type) {
     case PluginType.datasource:
       return <GetStartedWithDataSource plugin={plugin} />;
     default:
       return null;
+  }
+}
+
+function hasProperPermissions(pluginType: PluginType | undefined): boolean {
+  switch (pluginType) {
+    case PluginType.datasource:
+      return (
+        contextSrv.hasPermission(AccessControlAction.DataSourcesCreate) &&
+        contextSrv.hasPermission(AccessControlAction.DataSourcesWrite)
+      );
+    default:
+      return true;
   }
 }
