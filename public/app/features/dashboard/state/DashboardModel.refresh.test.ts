53fdbd181894ec79cad6ff01bb1752920f8ccc3e
Hugo Häggmark <hugo.haggmark@gmail.com>
2021-10-26 09:12:22 +0200

    Tests: adds tests for strict panel refresh logic

--- public/app/features/dashboard/state/DashboardModel.refresh.test.ts
+++ public/app/features/dashboard/state/DashboardModel.refresh.test.ts
@@ -1,28 +1,84 @@
 import { DashboardModel } from './DashboardModel';
 import { appEvents } from '../../../core/core';
 import { VariableChanged } from '../../variables/types';
+import { PanelModel } from './PanelModel';
 
-function getTestContext({ panelInEdit, panelInView }: { panelInEdit?: any; panelInView?: any } = {}) {
+function getTestContext({
+  usePanelInEdit,
+  usePanelInView,
+}: { usePanelInEdit?: boolean; usePanelInView?: boolean } = {}) {
+  jest.clearAllMocks();
+
   const dashboard = new DashboardModel({});
-  dashboard.startRefresh = jest.fn();
-  if (panelInEdit) {
+  const startRefreshMock = jest.fn();
+  dashboard.startRefresh = startRefreshMock;
+  const panelInView = new PanelModel({ id: 99 });
+  const panelInEdit = new PanelModel({ id: 100 });
+  const panelIds = [1, 2, 3];
+  if (usePanelInEdit) {
     dashboard.panelInEdit = panelInEdit;
+    panelIds.push(panelInEdit.id);
   }
-  if (panelInView) {
+  if (usePanelInView) {
     dashboard.panelInView = panelInView;
+    panelIds.push(panelInView.id);
   }
-  return { dashboard };
+
+  appEvents.publish(new VariableChanged({ panelIds }));
+
+  return { dashboard, startRefreshMock, panelInEdit, panelInView };
 }
 
 describe('Strict panel refresh', () => {
-  describe('when strict panel is off', () => {
-    it('then all panels should be refreshed', () => {
-      const { dashboard } = getTestContext();
+  describe('when there is no panel in full view or panel in panel edit during variable change', () => {
+    it('then all affected panels should be refreshed', () => {
+      const { startRefreshMock } = getTestContext();
 
-      appEvents.publish(new VariableChanged({ panelIds: [1, 2, 3] }));
+      expect(startRefreshMock).toHaveBeenCalledTimes(1);
+      expect(startRefreshMock).toHaveBeenLastCalledWith([1, 2, 3]);
+    });
+  });
 
-      expect(dashboard.startRefresh).toHaveBeenCalledTimes(1);
-      expect(dashboard.startRefresh).toHaveBeenLastCalledWith([1, 2, 3]);
+  describe('when there is a panel in full view during variable change', () => {
+    it('then all affected panels should be refreshed', () => {
+      const { panelInView, startRefreshMock } = getTestContext({ usePanelInView: true });
+
+      expect(startRefreshMock).toHaveBeenCalledTimes(1);
+      expect(startRefreshMock).toHaveBeenLastCalledWith([1, 2, 3, panelInView.id]);
+    });
+
+    describe('and when exitViewPanel is called', () => {
+      it('then all affected panels except the panel in full view should be refreshed', () => {
+        const { dashboard, panelInView, startRefreshMock } = getTestContext({ usePanelInView: true });
+        startRefreshMock.mockClear();
+
+        dashboard.exitViewPanel(panelInView);
+
+        expect(startRefreshMock).toHaveBeenCalledTimes(1);
+        expect(startRefreshMock).toHaveBeenLastCalledWith([1, 2, 3]);
+        expect(dashboard['panelsAffectedByVariableChange']).toBeNull();
+      });
+    });
+  });
+
+  describe('when there is a panel in panel edit during variable change', () => {
+    it('then all affected panels should be refreshed', () => {
+      const { panelInEdit, startRefreshMock } = getTestContext({ usePanelInEdit: true });
+      expect(startRefreshMock).toHaveBeenCalledTimes(1);
+      expect(startRefreshMock).toHaveBeenLastCalledWith([1, 2, 3, panelInEdit.id]);
+    });
+
+    describe('and when exitViewPanel is called', () => {
+      it('then all affected panels except the panel in panel edit should be refreshed', () => {
+        const { dashboard, startRefreshMock } = getTestContext({ usePanelInEdit: true });
+        startRefreshMock.mockClear();
+
+        dashboard.exitPanelEditor();
+
+        expect(startRefreshMock).toHaveBeenCalledTimes(1);
+        expect(startRefreshMock).toHaveBeenLastCalledWith([1, 2, 3]);
+        expect(dashboard['panelsAffectedByVariableChange']).toBeNull();
+      });
     });
   });
 });
