b52ce8f7e6369ab7934db3fb4ff5ce7c209703a1
Hugo Häggmark <hugo.haggmark@gmail.com>
2021-10-27 08:42:30 +0200

    Chore: updates to radio button

--- public/app/features/variables/editor/VariableStrictPanelRefreshBox.tsx
+++ public/app/features/variables/editor/VariableStrictPanelRefreshBox.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Alert, Checkbox, Field } from '@grafana/ui';
+import { Field, RadioButtonGroup } from '@grafana/ui';
 
 export interface VariableStrictPanelRefreshBoxProps {
   value: boolean;
@@ -9,23 +9,17 @@ export interface VariableStrictPanelRefreshBoxProps {
 export function VariableStrictPanelRefreshBox({ value, onChange }: VariableStrictPanelRefreshBoxProps): ReactElement {
   return (
     <>
-      <Alert title="Strict panel refresh" severity="info">
-        <p>
-          Using strict panel refresh means only panels that are affected by a variable change will be refreshed. The
-          default setting is to refresh all panels when a variable changes.
-        </p>
-        <p>
-          <i>
-            If strict panel refresh setting is used then the data in panels not affected by a variable change might look
-            stale until next refresh.
-          </i>
-        </p>
-      </Alert>
-      <Field label="Strict panel refresh">
-        <Checkbox
-          id="variables-settings-strict-mode"
+      <Field
+        label="Panel refresh"
+        description="Panel refresh determines which panels will be refreshed when any variable changes"
+      >
+        <RadioButtonGroup
           value={value}
-          onChange={(e) => onChange(e.currentTarget.checked)}
+          options={[
+            { label: 'All panels', value: false },
+            { label: 'Panels impacted by variable change', value: true },
+          ]}
+          onChange={onChange}
         />
       </Field>
     </>
