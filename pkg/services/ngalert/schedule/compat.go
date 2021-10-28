ce84bd782232b5671fea2a18a14de9a14b5b9b01
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2021-10-28 14:05:43 -0400

    extract conversion of state to postable alert + tests

--- pkg/services/ngalert/schedule/compat.go
+++ pkg/services/ngalert/schedule/compat.go
@@ -8,12 +8,46 @@ import (
 
 	"github.com/go-openapi/strfmt"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
-	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 	"github.com/prometheus/alertmanager/api/v2/models"
+
+	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 
 	ngModels "github.com/grafana/grafana/pkg/services/ngalert/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/state"
 )
+
+// stateToPostableAlert converts an state to a model that is accepted by Alertmanager. Annotations and Labels are copied from the state.
+// - if state has at least one result, a new label '__value_string__' is added to the label set
+// - the alert's GeneratorURL is constructed to point to the alert edit page
+func stateToPostableAlert(alertState *state.State, appURL *url.URL) *models.PostableAlert {
+	nL := alertState.Labels.Copy()
+	nA := data.Labels(alertState.Annotations).Copy()
+
+	if len(alertState.Results) > 0 {
+		nA["__value_string__"] = alertState.Results[0].EvaluationString
+	}
+
+	var urlStr string
+	if uid := nL[ngModels.RuleUIDLabel]; len(uid) > 0 && appURL != nil {
+		u := *appURL
+		u.Path = path.Join(u.Path, fmt.Sprintf("/alerting/%s/edit", uid))
+		urlStr = u.String()
+	} else if appURL != nil {
+		urlStr = appURL.String()
+	} else {
+		urlStr = ""
+	}
+
+	return &models.PostableAlert{
+		Annotations: models.LabelSet(nA),
+		StartsAt:    strfmt.DateTime(alertState.StartsAt),
+		EndsAt:      strfmt.DateTime(alertState.EndsAt),
+		Alert: models.Alert{
+			Labels:       models.LabelSet(nL),
+			GeneratorURL: strfmt.URI(urlStr),
+		},
+	}
+}
 
 func FromAlertStateToPostableAlerts(firingStates []*state.State, stateManager *state.Manager, appURL *url.URL) apimodels.PostableAlerts {
 	alerts := apimodels.PostableAlerts{PostableAlerts: make([]models.PostableAlert, 0, len(firingStates))}
@@ -24,33 +58,8 @@ func FromAlertStateToPostableAlerts(firingStates []*state.State, stateManager *s
 		if !alertState.NeedsSending(stateManager.ResendDelay) {
 			continue
 		}
-		nL := alertState.Labels.Copy()
-		nA := data.Labels(alertState.Annotations).Copy()
-
-		if len(alertState.Results) > 0 {
-			nA["__value_string__"] = alertState.Results[0].EvaluationString
-		}
-
-		var urlStr string
-		if uid := nL[ngModels.RuleUIDLabel]; len(uid) > 0 && appURL != nil {
-			u := *appURL
-			u.Path = path.Join(u.Path, fmt.Sprintf("/alerting/%s/edit", uid))
-			urlStr = u.String()
-		} else if appURL != nil {
-			urlStr = appURL.String()
-		} else {
-			urlStr = ""
-		}
-
-		alerts.PostableAlerts = append(alerts.PostableAlerts, models.PostableAlert{
-			Annotations: models.LabelSet(nA),
-			StartsAt:    strfmt.DateTime(alertState.StartsAt),
-			EndsAt:      strfmt.DateTime(alertState.EndsAt),
-			Alert: models.Alert{
-				Labels:       models.LabelSet(nL),
-				GeneratorURL: strfmt.URI(urlStr),
-			},
-		})
+		alert := stateToPostableAlert(alertState, appURL)
+		alerts.PostableAlerts = append(alerts.PostableAlerts, *alert)
 		alertState.LastSentAt = ts
 		sentAlerts = append(sentAlerts, alertState)
 	}
