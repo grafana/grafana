9594583b20e81b2ef8e01da653e10a2637162415
Hugo Häggmark <hugo.haggmark@grafana.com>
2021-10-28 10:52:01 +0200

    Dashboard: Fix repeats in snapshots (#41018)
    (cherry picked from commit fbd68c4e96840484accdf229606c5e4383a3136a)

--- public/app/features/dashboard/state/DashboardModel.ts
+++ public/app/features/dashboard/state/DashboardModel.ts
@@ -269,6 +269,9 @@ export class DashboardModel {
   private getPanelSaveModels() {
     return this.panels
       .filter((panel: PanelModel) => {
+        if (this.isSnapshotTruthy()) {
+          return true;
+        }
         if (panel.type === 'add-panel') {
           return false;
         }
@@ -295,6 +298,9 @@ export class DashboardModel {
         return panel.getSaveModel();
       })
       .map((model: any) => {
+        if (this.isSnapshotTruthy()) {
+          return model;
+        }
         // Clear any scopedVars from persisted mode. This cannot be part of getSaveModel as we need to be able to copy
         // panel models with preserved scopedVars, for example when going into edit mode.
         delete model.scopedVars;
