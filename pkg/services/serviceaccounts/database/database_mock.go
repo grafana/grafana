27b84b52c3c0aaf785c9b8d2cdfccfc1d9e11642
eleijonmarck <eric.leijonmarck@gmail.com>
2021-11-01 20:25:19 +0100

    test

--- pkg/services/serviceaccounts/database/database_mock.go
+++ pkg/services/serviceaccounts/database/database_mock.go
@@ -12,5 +12,6 @@ type MockDatasourceDatabase struct {
 
 func (m *MockDatasourceDatabase) RemovePermission(ctx context.Context, orgID int64, dsID int64, permissionID int64) error {
 	mockedArgs := m.Called(ctx, orgID, dsID, permissionID)
+
 	return mockedArgs.Error(0)
 }
