5075f28d22af99d655b8cd41be61e60f25af95c7
Ivana <ivana.huckova@gmail.com>
2021-11-02 17:27:09 +0100

    Fix width

--- public/app/plugins/datasource/prometheus/components/PromExploreExtraField.tsx
+++ public/app/plugins/datasource/prometheus/components/PromExploreExtraField.tsx
@@ -98,7 +98,7 @@ export const PromExploreExtraField: React.FC<PromExploreExtraFieldProps> = memo(
           aria-label="Step field"
         >
           <InlineFormLabel
-            width={5}
+            width={6}
             tooltip={
               'Time units and built in variables can be used here, for example: $__interval, $__rate_interval, 5s, 1m, 3h, 1d, 1y (Default if no unit is specified: s)'
             }
