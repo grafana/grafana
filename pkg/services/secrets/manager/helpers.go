705f9ac3d762d3b5614f6ae518350c0556c80c4a
Tania B <yalyna.ts@gmail.com>
2021-11-03 15:57:15 +0200

    Enable feature in tests

--- pkg/services/secrets/manager/helpers.go
+++ pkg/services/secrets/manager/helpers.go
@@ -3,6 +3,8 @@ package manager
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/services/encryption/ossencryption"
 	"github.com/grafana/grafana/pkg/services/secrets"
@@ -22,7 +24,11 @@ func SetupTestService(tb testing.TB, store secrets.Store) *SecretsService {
 		[security]
 		secret_key = ` + defaultKey))
 	require.NoError(tb, err)
-	settings := &setting.OSSImpl{Cfg: &setting.Cfg{Raw: raw}}
+	cfg := &setting.Cfg{Raw: raw}
+	cfg.FeatureToggles[feature] = true
+
+	settings := &setting.OSSImpl{Cfg: cfg}
+	assert.True(tb, settings.IsFeatureToggleEnabled(feature))
 
 	return ProvideSecretsService(
 		store,
