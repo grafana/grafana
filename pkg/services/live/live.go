4a00404d9a0b073e95b9046020e71ad07e831a72
idafurjes <36131195+idafurjes@users.noreply.github.com>
2021-11-03 11:31:56 +0100

    Chore: Add context to org (#40685)
    * Add context to org
    
    * Rebase
    
    * Fix rebase

--- pkg/services/live/live.go
+++ pkg/services/live/live.go
@@ -197,7 +197,7 @@ func ProvideService(plugCtxProvider *plugincontext.Provider, cfg *setting.Cfg, r
 		// This can be unreasonable to have in production scenario with many
 		// organizations.
 		query := &models.SearchOrgsQuery{}
-		err := sqlstore.SearchOrgs(query)
+		err := sqlstore.SearchOrgs(context.TODO(), query)
 		if err != nil {
 			return nil, fmt.Errorf("can't get org list: %w", err)
 		}
