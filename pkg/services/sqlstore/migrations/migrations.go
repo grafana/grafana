df79640abd96608e068a5771ace3719bcbf4cbf2
Tania B <yalyna.ts@gmail.com>
2021-11-03 15:15:06 +0200

    Put envelope encryption behind feature toggle

--- pkg/services/sqlstore/migrations/migrations.go
+++ pkg/services/sqlstore/migrations/migrations.go
@@ -52,7 +52,9 @@ func (*OSSMigrations) AddMigration(mg *Migrator) {
 		addLiveChannelMigrations(mg)
 	}
 	ualert.RerunDashAlertMigration(mg)
-	addSecretsMigration(mg)
+	if mg.Cfg.FeatureToggles["encryption"] {
+		addSecretsMigration(mg)
+	}
 	addKVStoreMigrations(mg)
 	ualert.AddDashboardUIDPanelIDMigration(mg)
 }
