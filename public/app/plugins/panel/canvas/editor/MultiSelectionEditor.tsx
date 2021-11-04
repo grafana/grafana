36ada1e6f2a859a82015b62db4242a00dc36def3
Nathan Marrs <nathan.marrs@grafana.com>
2021-11-03 21:06:47 -0700

    Create new group (layer), duplicate current selected elements, remove old elements from current layer

--- public/app/plugins/panel/canvas/editor/MultiSelectionEditor.tsx
+++ public/app/plugins/panel/canvas/editor/MultiSelectionEditor.tsx
@@ -1,16 +1,42 @@
 import React, { FC } from 'react';
 import { Button } from '@grafana/ui';
 import { StandardEditorProps } from '@grafana/data';
+
 import { InstanceState } from '../CanvasPanel';
 import { PanelOptions } from '../models.gen';
+import { GroupState } from 'app/features/canvas/runtime/group';
+import { ElementState } from 'app/features/canvas/runtime/element';
+import { LayerActionID } from '../types';
 
 export const MultiSelectionEditor: FC<StandardEditorProps<any, InstanceState, PanelOptions>> = ({ context }) => {
   const createNewLayer = () => {
-    console.log('TODO: create new layer');
-
     const currentSelectedElements = context?.instanceState.selected;
+    const currentLayer = currentSelectedElements[0].parent;
 
-    console.log(currentSelectedElements);
+    // Create new group and attach currentSelected elements to that group
+    const newLayer = new GroupState(
+      {
+        type: 'group',
+        elements: [],
+      },
+      context.instanceState.scene,
+      currentSelectedElements[0].parent
+    );
+
+    // Remove old elements from current layer
+    // This approach is working currently for some reason...
+    // currentLayer.elements.filter((element: ElementState) => !currentSelectedElements.includes(element));
+
+    currentSelectedElements.forEach((element: ElementState) => {
+      newLayer.doAction(LayerActionID.Duplicate, element);
+      currentLayer.doAction(LayerActionID.Delete, element);
+    });
+
+    // add new group as element to current layer
+    currentLayer.elements.push(newLayer);
+
+    context.instanceState.scene.save();
+    currentLayer.reinitializeMoveable();
   };
 
   return (
