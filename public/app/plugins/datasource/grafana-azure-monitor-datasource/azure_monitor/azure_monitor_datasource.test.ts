0a3c4dc3627f13e127218c617e4a89b171ba5b5b
Erik Sundell <erik.sundell87@gmail.com>
2021-11-04 15:56:35 +0100

    check if variable exist in query

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_monitor/azure_monitor_datasource.test.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/azure_monitor/azure_monitor_datasource.test.ts
@@ -2,7 +2,10 @@ import AzureMonitorDatasource from '../datasource';
 
 import { TemplateSrv } from 'app/features/templating/template_srv';
 import { DataSourceInstanceSettings } from '@grafana/data';
-import { AzureDataSourceJsonData, DatasourceValidationResult } from '../types';
+import { AzureDataSourceJsonData, AzureQueryType, DatasourceValidationResult } from '../types';
+import createMockQuery from '../__mocks__/query';
+import { CustomVariableModel, initialVariableModelState, VariableHide } from 'app/features/variables/types';
+import { singleVariable, subscriptionsVariable } from '../__mocks__/variables';
 
 const templateSrv = new TemplateSrv();
 
@@ -483,6 +486,34 @@ describe('AzureMonitorDatasource', () => {
             ]
           `);
         });
+    });
+
+    describe('When performing targetContainsTemplate', () => {
+      const query = createMockQuery();
+      it('should return false when no variable is being used', () => {
+        query.queryType = AzureQueryType.AzureMonitor;
+        expect(ctx.ds.targetContainsTemplate(query)).toEqual(false);
+      });
+
+      it('should return true when subscriptions field is using a variable', () => {
+        const templateSrv = new TemplateSrv();
+        templateSrv.init([subscriptionsVariable]);
+
+        const ds = new AzureMonitorDatasource(ctx.instanceSettings, templateSrv);
+        query.queryType = AzureQueryType.AzureMonitor;
+        query.subscription = '$subs';
+        expect(ds.targetContainsTemplate(query)).toEqual(true);
+      });
+
+      it('should return false when a variable is used in a different part of the query', () => {
+        const templateSrv = new TemplateSrv();
+        templateSrv.init([singleVariable]);
+
+        const ds = new AzureMonitorDatasource(ctx.instanceSettings, templateSrv);
+        query.queryType = AzureQueryType.AzureMonitor;
+        query.azureLogAnalytics = { resource: `$${singleVariable.name}` };
+        expect(ds.targetContainsTemplate(query)).toEqual(false);
+      });
     });
 
     it('should return an empty array for a Metric that does not have dimensions', () => {
