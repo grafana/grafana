23a922ca9dd3a275a3ba2372508e2146001a08a3
Ryan McKinley <ryantxu@gmail.com>
2021-11-05 17:00:55 -0700

    strip null values

---
+++ public/app/core/utils/object.ts
@@ -0,0 +1,20 @@
+﻿import { isArray, isPlainObject } from 'lodash';
+
+/** @returns a deep clone of the object, but with any null value removed */
+export function sortedDeepCloneWithoutNulls<T>(value: T): T {
+  if (isArray(value)) {
+    return (value.map(sortedDeepCloneWithoutNulls) as unknown) as T;
+  }
+  return Object.keys(value)
+    .sort()
+    .reduce((acc: any, key) => {
+      let v = (value as any)[key];
+      if (v != null) {
+        if (isPlainObject(v)) {
+          v = sortedDeepCloneWithoutNulls(v);
+        }
+        acc[key] = v;
+      }
+      return acc;
+    }, {});
+}
