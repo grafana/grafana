929af34baec6b150d8b709276c93cce547943eb5
Nathan Marrs <nathan.marrs@grafana.com>
2021-11-08 12:04:58 -0800

    Refactor LayersEditor to functional component from class based

--- public/app/plugins/panel/geomap/editor/LayersEditor.tsx
+++ public/app/plugins/panel/geomap/editor/LayersEditor.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { cx } from '@emotion/css';
 import { Container, Icon, IconButton, ValuePicker } from '@grafana/ui';
 import { StandardEditorProps } from '@grafana/data';
@@ -13,19 +13,19 @@ import { dataLayerFilter } from './layerEditor';
 
 type Props = StandardEditorProps<any, any, GeomapPanelOptions, GeomapInstanceState>;
 
-export class LayersEditor extends PureComponent<Props> {
-  style = getLayerDragStyles(config.theme);
+export const LayersEditor = (props: Props) => {
+  const style = getLayerDragStyles(config.theme);
 
-  getRowStyle = (sel: boolean) => {
-    return sel ? `${this.style.row} ${this.style.sel}` : this.style.row;
+  const getRowStyle = (sel: boolean) => {
+    return sel ? `${style.row} ${style.sel}` : style.row;
   };
 
-  onDragEnd = (result: DropResult) => {
+  const onDragEnd = (result: DropResult) => {
     if (!result.destination) {
       return;
     }
 
-    const { layers, actions } = this.props.context.instanceState ?? {};
+    const { layers, actions } = props.context.instanceState ?? {};
     if (!layers || !actions) {
       return;
     }
@@ -38,89 +38,86 @@ export class LayersEditor extends PureComponent<Props> {
     actions.reorder(src, dst);
   };
 
-  render() {
-    const { layers, selected, actions } = this.props.context.instanceState ?? {};
-    if (!layers || !actions) {
-      return <div>No layers?</div>;
-    }
-    const baselayer = layers[0];
-
-    const styles = this.style;
-    return (
-      <>
-        <Container>
-          <ValuePicker
-            icon="plus"
-            label="Add layer"
-            variant="secondary"
-            options={geomapLayerRegistry.selectOptions(undefined, dataLayerFilter).options}
-            onChange={(v) => actions.addlayer(v.value!)}
-            isFullWidth={true}
-          />
-        </Container>
-        <br />
-
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided, snapshot) => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                {(() => {
-                  // reverse order
-                  const rows: any = [];
-                  for (let i = layers.length - 1; i > 0; i--) {
-                    const element = layers[i];
-                    rows.push(
-                      <Draggable key={element.UID} draggableId={element.UID} index={rows.length}>
-                        {(provided, snapshot) => (
-                          <div
-                            className={this.getRowStyle(i === selected)}
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            onMouseDown={() => actions!.selectLayer(element.UID)}
-                          >
-                            <span className={styles.typeWrapper}>{element.options.type}</span>
-                            <div className={styles.textWrapper}>&nbsp; ({element.layer.getSourceState() ?? '?'})</div>
-
-                            <IconButton
-                              name="trash-alt"
-                              title={'remove'}
-                              className={cx(styles.actionIcon, styles.dragIcon)}
-                              onClick={() => actions.deleteLayer(element.UID)}
-                              surface="header"
-                            />
-                            {layers.length > 2 && (
-                              <Icon
-                                title="Drag and drop to reorder"
-                                name="draggabledots"
-                                size="lg"
-                                className={styles.dragIcon}
-                              />
-                            )}
-                          </div>
-                        )}
-                      </Draggable>
-                    );
-                  }
-                  return rows;
-                })()}
-
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-
-        {false && baselayer && (
-          <>
-            <label>Base layer</label>
-            <div className={this.getRowStyle(false)}>
-              <span className={styles.typeWrapper}>{baselayer.options.type}</span>
-              <div className={styles.textWrapper}>&nbsp; {baselayer.UID}</div>
-            </div>
-          </>
-        )}
-      </>
-    );
+  const { layers, selected, actions } = props.context.instanceState ?? {};
+  if (!layers || !actions) {
+    return <div>No layers?</div>;
   }
-}
+  const baselayer = layers[0];
+
+  return (
+    <>
+      <Container>
+        <ValuePicker
+          icon="plus"
+          label="Add layer"
+          variant="secondary"
+          options={geomapLayerRegistry.selectOptions(undefined, dataLayerFilter).options}
+          onChange={(v) => actions.addlayer(v.value!)}
+          isFullWidth={true}
+        />
+      </Container>
+      <br />
+
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {(() => {
+                // reverse order
+                const rows: any = [];
+                for (let i = layers.length - 1; i > 0; i--) {
+                  const element = layers[i];
+                  rows.push(
+                    <Draggable key={element.UID} draggableId={element.UID} index={rows.length}>
+                      {(provided, snapshot) => (
+                        <div
+                          className={getRowStyle(i === selected)}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          onMouseDown={() => actions!.selectLayer(element.UID)}
+                        >
+                          <span className={style.typeWrapper}>{element.options.type}</span>
+                          <div className={style.textWrapper}>&nbsp; ({element.layer.getSourceState() ?? '?'})</div>
+
+                          <IconButton
+                            name="trash-alt"
+                            title={'remove'}
+                            className={cx(style.actionIcon, style.dragIcon)}
+                            onClick={() => actions.deleteLayer(element.UID)}
+                            surface="header"
+                          />
+                          {layers.length > 2 && (
+                            <Icon
+                              title="Drag and drop to reorder"
+                              name="draggabledots"
+                              size="lg"
+                              className={style.dragIcon}
+                            />
+                          )}
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                }
+                return rows;
+              })()}
+
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+
+      {false && baselayer && (
+        <>
+          <label>Base layer</label>
+          <div className={getRowStyle(false)}>
+            <span className={style.typeWrapper}>{baselayer.options.type}</span>
+            <div className={style.textWrapper}>&nbsp; {baselayer.UID}</div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
