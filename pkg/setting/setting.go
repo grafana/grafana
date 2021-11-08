f5bf895d16dba74e59d32ffefaa3ddc1d822ce30
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2021-11-08 12:38:45 -0500

    legacy alerting is disabled by default

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -1383,7 +1383,7 @@ func (cfg *Cfg) readFeatureToggles(iniFile *ini.File) error {
 
 func readAlertingSettings(iniFile *ini.File) error {
 	alerting := iniFile.Section("alerting")
-	AlertingEnabled = alerting.Key("enabled").MustBool(true)
+	AlertingEnabled = alerting.Key("enabled").MustBool(false)
 	ExecuteAlerts = alerting.Key("execute_alerts").MustBool(true)
 	AlertingRenderLimit = alerting.Key("concurrent_render_limit").MustInt(5)
 
