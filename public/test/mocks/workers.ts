79307cbb9c01e15996318c5c010eb25b72b7fe61
ArturWierzbicki <artur.wierzbicki@grafana.com>
2021-11-08 19:03:25 +0400

    Fix: move the custom mockWorker rendering layout to `test/mocks`

--- public/test/mocks/workers.ts
+++ public/test/mocks/workers.ts
@@ -1,7 +1,27 @@
-const mockWorker = {
-  createWorker: () => ({
-    postMessage: () => {},
-  }),
+const { layout } = jest.requireActual('../../app/plugins/panel/nodeGraph/layout.worker.js');
+
+class LayoutMockWorker {
+  constructor() {}
+
+  postMessage(data: any) {
+    const { nodes, edges, config } = data;
+    setTimeout(() => {
+      layout(nodes, edges, config);
+      // @ts-ignore
+      this.onmessage({ data: { nodes, edges } });
+    }, 1);
+  }
+}
+
+jest.mock('../../app/plugins/panel/nodeGraph/createLayoutWorker', () => ({
+  createWorker: () => new LayoutMockWorker(),
+}));
+
+class BasicMockWorker {
+  postMessage() {}
+}
+const mockCreateWorker = {
+  createWorker: () => new BasicMockWorker(),
 };
-jest.mock('../../app/plugins/panel/nodeGraph/createLayoutWorker', () => mockWorker);
-jest.mock('../../app/features/live/centrifuge/createCentrifugeServiceWorker', () => mockWorker);
+
+jest.mock('../../app/features/live/centrifuge/createCentrifugeServiceWorker', () => mockCreateWorker);
