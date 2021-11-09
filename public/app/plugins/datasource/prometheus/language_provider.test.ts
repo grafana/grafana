f809953bd79fdb9323f4994ddc09b53c63656712
Piotr Jamróz <pm.jamroz@gmail.com>
2021-11-09 15:47:09 +0100

    Fix unit test

--- public/app/plugins/datasource/prometheus/language_provider.test.ts
+++ public/app/plugins/datasource/prometheus/language_provider.test.ts
@@ -599,7 +599,7 @@ describe('Language completion provider', () => {
     it('returns empty queries', async () => {
       const instance = new LanguageProvider(datasource);
       const result = await instance.importAbstractQuery({ refId: 'bar', labelMatchers: [] });
-      expect(result).toEqual([{ refId: 'bar', expr: '', range: true }]);
+      expect(result).toEqual({ refId: 'bar', expr: '', range: true });
     });
 
     describe('exporting to abstract query', () => {
