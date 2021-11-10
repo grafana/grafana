36ec281fb60ffa23501578556c16acc7af4acaff
Sarah Zinger <sarah.zinger@grafana.com>
2021-11-09 20:21:32 -0500

    Remove snapshot tests, add new react testing lib tests.

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/AzureCredentialsForm.tsx
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/AzureCredentialsForm.tsx
@@ -220,7 +220,7 @@ export const AzureCredentialsForm: FunctionComponent<Props> = (props: Props) => 
               <div className="gf-form-inline">
                 <div className="gf-form">
                   <InlineFormLabel className="width-12">Client Secret</InlineFormLabel>
-                  <Input className="width-25" placeholder="configured" disabled={true} />
+                  <Input data-testid="client-secret" className="width-25" placeholder="configured" disabled={true} />
                 </div>
                 <div className="gf-form">
                   <div className="max-width-30 gf-form-inline">
