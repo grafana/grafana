9212d2b36ee117837c0378c0d5a74e105f5bf79d
Ryan McKinley <ryantxu@gmail.com>
2021-11-11 11:48:50 -0800

    with bbox

---
+++ public/app/plugins/panel/geomap/style/types.ts
@@ -0,0 +1,39 @@
+import {
+  ColorDimensionConfig,
+  ResourceDimensionConfig,
+  ScaleDimensionConfig,
+  TextDimensionConfig,
+} from 'app/features/dimensions';
+import { Image, Style } from 'ol/style';
+
+// StyleConfig is saved in panel json and is used to configure how items get rendered
+export interface StyleConfig {
+  color?: ColorDimensionConfig;
+  opacity?: number; // defaults to 80%
+
+  // For non-points
+  stroke?: number;
+
+  // Used for points
+  size?: ScaleDimensionConfig;
+  symbol?: ResourceDimensionConfig;
+
+  // when labels are shown
+  text?: TextDimensionConfig;
+  offset?: [number, number];
+}
+
+// Applying the config to real data gives the values
+export interface StyleConfigValues {
+  fillColor: string;
+  lineColor?: string;
+  lineWidth?: number;
+  size?: number;
+  symbol?: string; // the point symbol
+  text?: string;
+  rotation?: number;
+  offset?: [number, number];
+}
+
+// Point values use an image as the marker
+export type StyleImageMaker = (values: StyleConfigValues) => Image;
