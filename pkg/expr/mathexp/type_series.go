4ab9a2f04f95ada50c0fd4f19685e04d0218e77b
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2021-11-16 11:20:31 +0100

    Expr: use value column name as series name

--- pkg/expr/mathexp/type_series.go
+++ pkg/expr/mathexp/type_series.go
@@ -142,7 +142,7 @@ func (s Series) GetLabels() data.Labels { return s.Frame.Fields[seriesTypeValIdx
 
 func (s Series) SetLabels(ls data.Labels) { s.Frame.Fields[seriesTypeValIdx].Labels = ls }
 
-func (s Series) GetName() string { return s.Frame.Name }
+func (s Series) GetName() string { return s.Frame.Fields[seriesTypeValIdx].Name }
 
 func (s Series) GetMeta() interface{} {
 	return s.Frame.Meta.Custom
