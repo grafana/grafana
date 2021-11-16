2995dfbb77d80be7d041c1caffbc41846e226963
Oliver Frye <oliver.frye@carta.com>
2021-11-16 21:08:48 +1000

    fix lint error

--- packages/grafana-ui/src/components/Logs/LogRowMessage.tsx
+++ packages/grafana-ui/src/components/Logs/LogRowMessage.tsx
@@ -65,7 +65,7 @@ function renderLogMessage(
     highlights && highlights.length > 0 && highlights[0] && highlights[0].length > 0 && entry.length < MAX_CHARACTERS;
   const searchWords = highlights ?? [];
   if (hasAnsi) {
-    const highlight = needsHighlighter ? {searchWords, highlightClassName} : undefined;
+    const highlight = needsHighlighter ? { searchWords, highlightClassName } : undefined;
     return <LogMessageAnsi value={entry} highlight={highlight} />;
   } else if (needsHighlighter) {
     return (
