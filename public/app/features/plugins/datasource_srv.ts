068eaf7940cd2e23772f6e4450020f1324e1cf3e
André R. de Miranda <andre@galgo.tech>
2021-11-17 22:05:54 -0300

    Repeated filters.alerting

--- public/app/features/plugins/datasource_srv.ts
+++ public/app/features/plugins/datasource_srv.ts
@@ -204,9 +204,6 @@ export class DatasourceSrv implements DataSourceService {
       if (filters.metrics && !x.meta.metrics) {
         return false;
       }
-      if (filters.alerting && !x.meta.alerting) {
-        return false;
-      }
       if (filters.tracing && !x.meta.tracing) {
         return false;
       }
