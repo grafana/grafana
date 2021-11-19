240549dc0664435602aac2ae9c89f5758387bbd0
Stephanie Closson <srclosson@gmail.com>
2021-11-19 14:59:19 -0400

    cleanup files I didn't want to modify

--- .yarn/sdks/eslint/lib/api.js
+++ .yarn/sdks/eslint/lib/api.js
@@ -11,10 +11,10 @@ const absRequire = (createRequire || createRequireFromPath)(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
-    // Setup the environment to be able to require eslint
+    // Setup the environment to be able to require eslint/lib/api.js
     require(absPnpApiPath).setup();
   }
 }
 
-// Defer to the real eslint your application uses
-module.exports = absRequire(`eslint`);
+// Defer to the real eslint/lib/api.js your application uses
+module.exports = absRequire(`eslint/lib/api.js`);
