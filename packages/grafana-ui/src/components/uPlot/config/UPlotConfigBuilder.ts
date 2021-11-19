07a440fd3aee671dddd1b93fe8a725a331f0492e
Leon Sorokin <leeoniya@gmail.com>
2021-11-19 14:42:04 -0600

    TimeSeries: fix fillBelowTo wrongly affecting fills of unrelated series (#41998)

--- packages/grafana-ui/src/components/uPlot/config/UPlotConfigBuilder.ts
+++ packages/grafana-ui/src/components/uPlot/config/UPlotConfigBuilder.ts
@@ -231,13 +231,13 @@ export class UPlotConfigBuilder {
       // When fillBelowTo option enabled, handle series bands fill manually
       if (this.bands?.length) {
         config.bands = this.bands;
-        const keepFill = new Set<number>();
+        const killFill = new Set<number>();
         for (const b of config.bands) {
-          keepFill.add(b.series[0]);
+          killFill.add(b.series[1]);
         }
 
         for (let i = 1; i < config.series.length; i++) {
-          if (!keepFill.has(i)) {
+          if (killFill.has(i)) {
             config.series[i].fill = undefined;
           }
         }
