8b3d337a71845c2c2fff13db5ded0f078f196cc9
Dominik Prokop <dominik.prokop@grafana.com>
2021-11-23 15:41:51 +0100

    grafana/ui: Export types and utils required by grafana/experimental (#42133)
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    (cherry picked from commit d2d38c978fbe94730ac865343b78ed9b02959db9)

--- packages/grafana-ui/src/utils/reactUtils.ts
+++ packages/grafana-ui/src/utils/reactUtils.ts
@@ -17,3 +17,24 @@ export function getChildId(children: ReactElement): string | undefined {
 
   return typeof inputId === 'string' ? inputId : undefined;
 }
+
+/**
+ * Given react node or function returns element accordingly
+ *
+ * @param itemToRender
+ * @param props props to be passed to the function if item provided as such
+ */
+export function renderOrCallToRender<TProps = any>(
+  itemToRender: ((props?: TProps) => React.ReactNode) | React.ReactNode,
+  props?: TProps
+): React.ReactNode {
+  if (React.isValidElement(itemToRender) || typeof itemToRender === 'string' || typeof itemToRender === 'number') {
+    return itemToRender;
+  }
+
+  if (typeof itemToRender === 'function') {
+    return itemToRender(props);
+  }
+
+  throw new Error(`${itemToRender} is not a React element nor a function that returns React element`);
+}
