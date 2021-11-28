5b1728a8f23f4bdc92a3f339240c31d1e36eea0b
Artur Wierzbicki <artur@arturwierzbicki.com>
2021-11-29 00:05:41 +0400

    #41993: refactor: use the new benchmark plugin in e2e tests

---
+++ e2e/benchmarks/live/6-20hz-panels.spec.ts
@@ -0,0 +1,11 @@
+import { e2e } from '@grafana/e2e';
+
+e2e.benchmark({
+  name: 'Live performance benchmarking - 6x20hz panels',
+  benchmarkingOptions: {
+    dashboardFolder: '/dashboards/live',
+    repeat: 5,
+    delayAfterOpeningDashboard: 1000,
+    duration: 3000,
+  },
+});
