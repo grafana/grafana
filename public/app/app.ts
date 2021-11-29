21beccc0f5219ec46c507db151f107acfd6219a1
Piotr Jamróz <pm.jamroz@gmail.com>
2021-11-29 16:07:40 +0100

    Change comment

--- public/app/app.ts
+++ public/app/app.ts
@@ -166,7 +166,7 @@ function initEchoSrv() {
 
   window.addEventListener('load', (e) => {
     const loadMetricName = 'frontend_boot_load_time_seconds';
-    // should match names in public/views/index-template.html
+    // Metrics below are marked in public/views/index-template.html
     const jsLoadMetricName = 'frontend_boot_js_done_time_seconds';
     const cssLoadMetricName = 'frontend_boot_css_time_seconds';
 
