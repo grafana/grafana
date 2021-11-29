edf4702d610f12ba22a3c9de9f9360a85ba321f7
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2021-11-29 10:59:31 -0500

    Encryption: Add usage stats to secrets service (#42437) (#42450)
    * Encryption: Add usage stats to secrets service
    
    * Sort imports
    
    (cherry picked from commit 58978dcf9669e90f6f2fff7554b1d77d53223cb4)
    
    Co-authored-by: Tania B <yalyna.ts@gmail.com>

--- pkg/services/secrets/manager/helpers.go
+++ pkg/services/secrets/manager/helpers.go
@@ -3,6 +3,7 @@ package manager
 import (
 	"testing"
 
+	"github.com/grafana/grafana/pkg/infra/usagestats"
 	"github.com/grafana/grafana/pkg/services/encryption/ossencryption"
 	"github.com/grafana/grafana/pkg/services/kmsproviders/osskmsproviders"
 	"github.com/grafana/grafana/pkg/services/secrets"
@@ -24,7 +25,6 @@ func SetupTestService(tb testing.TB, store secrets.Store) *SecretsService {
 	require.NoError(tb, err)
 	cfg := &setting.Cfg{Raw: raw}
 	cfg.FeatureToggles = map[string]bool{secrets.EnvelopeEncryptionFeatureToggle: true}
-
 	settings := &setting.OSSImpl{Cfg: cfg}
 	assert.True(tb, settings.IsFeatureToggleEnabled(secrets.EnvelopeEncryptionFeatureToggle))
 
@@ -34,6 +34,7 @@ func SetupTestService(tb testing.TB, store secrets.Store) *SecretsService {
 		osskmsproviders.ProvideService(encryption, settings),
 		encryption,
 		settings,
+		&usagestats.UsageStatsMock{T: tb},
 	)
 	require.NoError(tb, err)
 
