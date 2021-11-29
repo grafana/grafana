5a5c6718bdf511495d318b707e33842a923c58ec
thisisobate <obasiuche62@gmail.com>
2021-11-29 12:57:14 +0100

    add focus trapping to colorPickerPopover

--- packages/grafana-ui/src/components/ColorPicker/ColorPickerPopover.tsx
+++ packages/grafana-ui/src/components/ColorPicker/ColorPickerPopover.tsx
@@ -7,6 +7,7 @@ import { warnAboutColorPickerPropsDeprecation } from './warnAboutColorPickerProp
 import { css } from '@emotion/css';
 import { GrafanaTheme2, colorManipulator } from '@grafana/data';
 import { stylesFactory, withTheme2 } from '../../themes';
+import { FocusScope } from '@react-aria/focus';
 
 export type ColorPickerChangeHandler = (color: string) => void;
 
@@ -117,16 +118,18 @@ class UnThemedColorPickerPopover<T extends CustomPickersDescriptor> extends Reac
     const styles = getStyles(theme);
     return (
       <div className={styles.colorPickerPopover}>
-        <div className={styles.colorPickerPopoverTabs}>
-          <div className={this.getTabClassName('palette')} onClick={this.onTabChange('palette')}>
-            Colors
+        <FocusScope contain restoreFocus autoFocus>
+          <div className={styles.colorPickerPopoverTabs}>
+            <div tabIndex={0} className={this.getTabClassName('palette')} onClick={this.onTabChange('palette')}>
+              Colors
+            </div>
+            <div tabIndex={0} className={this.getTabClassName('spectrum')} onClick={this.onTabChange('spectrum')}>
+              Custom
+            </div>
+            {this.renderCustomPickerTabs()}
           </div>
-          <div className={this.getTabClassName('spectrum')} onClick={this.onTabChange('spectrum')}>
-            Custom
-          </div>
-          {this.renderCustomPickerTabs()}
-        </div>
-        <div className={styles.colorPickerPopoverContent}>{this.renderPicker()}</div>
+          <div className={styles.colorPickerPopoverContent}>{this.renderPicker()}</div>
+        </FocusScope>
       </div>
     );
   }
