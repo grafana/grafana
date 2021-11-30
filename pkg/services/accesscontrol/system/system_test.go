f5ce3812dae117aae5cb2ef4c8471a7bda9a5239
Karl Persson <kalle.persson@grafana.com>
2021-11-30 15:45:26 +0100

    add tests for GET description endpoint

---
+++ pkg/services/accesscontrol/system/system_test.go
@@ -0,0 +1,166 @@
+package system
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"path"
+	"testing"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+
+	"github.com/grafana/grafana/pkg/setting"
+
+	"github.com/grafana/grafana/pkg/api/routing"
+	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/accesscontrol/database"
+	accesscontrolmock "github.com/grafana/grafana/pkg/services/accesscontrol/mock"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/grafana/grafana/pkg/web"
+	"github.com/stretchr/testify/require"
+)
+
+type getDescriptionTestCase struct {
+	desc           string
+	options        Options
+	permissions    []*accesscontrol.Permission
+	expected       Description
+	expectedStatus int
+}
+
+func TestSystem_getDescription(t *testing.T) {
+	tests := []getDescriptionTestCase{
+		{
+			desc: "should return description",
+			options: Options{
+				Resource: "dashboards",
+				Assignments: Assignments{
+					Users:        true,
+					Teams:        true,
+					BuiltInRoles: true,
+				},
+				Permissions: []string{"View", "Edit", "Admin"},
+			},
+			permissions: []*accesscontrol.Permission{
+				{Action: "dashboards.permissions:read"},
+			},
+			expected: Description{
+				Assignments: Assignments{
+					Users:        true,
+					Teams:        true,
+					BuiltInRoles: true,
+				},
+				Permissions: []string{"View", "Edit", "Admin"},
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			desc: "should only return user assignment",
+			options: Options{
+				Resource: "dashboards",
+				Assignments: Assignments{
+					Users:        true,
+					Teams:        false,
+					BuiltInRoles: false,
+				},
+				Permissions: []string{"View"},
+			},
+			permissions: []*accesscontrol.Permission{
+				{Action: "dashboards.permissions:read"},
+			},
+			expected: Description{
+				Assignments: Assignments{
+					Users:        true,
+					Teams:        false,
+					BuiltInRoles: false,
+				},
+				Permissions: []string{"View"},
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			desc: "should return 403 when missing read permission",
+			options: Options{
+				Resource: "dashboards",
+				Assignments: Assignments{
+					Users:        true,
+					Teams:        false,
+					BuiltInRoles: false,
+				},
+				Permissions: []string{"View"},
+			},
+			permissions:    []*accesscontrol.Permission{},
+			expected:       Description{},
+			expectedStatus: http.StatusForbidden,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			_, server := setupTestEnvironment(t, &models.SignedInUser{}, tt.permissions, tt.options)
+			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/api/access-control/system/%s/description", tt.options.Resource), nil)
+			require.NoError(t, err)
+			recorder := httptest.NewRecorder()
+			server.ServeHTTP(recorder, req)
+
+			got := Description{}
+			require.NoError(t, json.NewDecoder(recorder.Body).Decode(&got))
+			assert.Equal(t, tt.expected, got)
+			if tt.expectedStatus == http.StatusOK {
+				assert.Equal(t, tt.expectedStatus, recorder.Code)
+			}
+		})
+	}
+}
+
+func TestSystem_getPermissions(t *testing.T) {
+
+}
+func TestSystem_setBuiltinRolePermission(t *testing.T) {
+
+}
+
+func TestSystem_setTeamPermission(t *testing.T) {
+
+}
+
+func TestSystem_setUserPermission(t *testing.T) {
+
+}
+
+func setupTestEnvironment(t *testing.T, user *models.SignedInUser, permissions []*accesscontrol.Permission, ops Options) (*System, *web.Mux) {
+	store := database.ProvideService(sqlstore.InitTestDB(t))
+
+	system, err := NewSystem(ops, routing.NewRouteRegister(), accesscontrolmock.New().WithPermissions(permissions), store)
+	require.NoError(t, err)
+
+	server := web.New()
+	server.UseMiddleware(web.Renderer(path.Join(setting.StaticRootPath, "views"), "[[", "]]"))
+	server.Use(contextProvider(&testContext{user}))
+	system.router.Register(server)
+
+	return system, server
+}
+
+type testContext struct {
+	user *models.SignedInUser
+}
+
+func contextProvider(tc *testContext) web.Handler {
+	return func(c *web.Context) {
+		signedIn := tc.user != nil
+		reqCtx := &models.ReqContext{
+			Context:      c,
+			SignedInUser: tc.user,
+			IsSignedIn:   signedIn,
+			SkipCache:    true,
+			Logger:       log.New("test"),
+		}
+		c.Map(reqCtx)
+	}
+}
