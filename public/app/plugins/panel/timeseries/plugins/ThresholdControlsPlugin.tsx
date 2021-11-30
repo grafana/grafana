d3c75ec6277885423f316ea70114737bae386957
gillesdemey <gilles.de.mey@gmail.com>
2021-11-30 15:17:53 +0100

    chore: cleanup PR

--- public/app/plugins/panel/timeseries/plugins/ThresholdControlsPlugin.tsx
+++ public/app/plugins/panel/timeseries/plugins/ThresholdControlsPlugin.tsx
@@ -41,6 +41,7 @@ export const ThresholdControlsPlugin: React.FC<ThresholdControlsPluginProps> = (
     if (!thresholds) {
       return null;
     }
+
     const scale = fieldConfig.defaults.unit ?? FIXED_UNIT;
     const decimals = fieldConfig.defaults.decimals;
     const handles = [];
