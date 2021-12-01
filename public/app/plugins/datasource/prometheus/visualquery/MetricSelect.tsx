17e4a80fdc4fef8e778ae1a1eb411cf964b637af
Torkel Ödegaard <torkel@grafana.com>
2021-12-01 11:36:29 +0100

    Update

--- public/app/plugins/datasource/prometheus/visualquery/MetricSelect.tsx
+++ public/app/plugins/datasource/prometheus/visualquery/MetricSelect.tsx
@@ -1,19 +1,25 @@
 import { SegmentAsync } from '@grafana/ui';
 import React, { useCallback, useMemo } from 'react';
 import { debounce } from 'lodash';
+import { PromVisualQuery } from './types';
 
-export interface Props {}
+export interface Props {
+  query: PromVisualQuery;
+}
 
-export function MetricSelect(props: Props) {
+export function MetricSelect({ query }: Props) {
   const loadOptions = useCallback((value: string | undefined) => {
-    return Promise.resolve([{ value: 'My metric' }, { value: 'Another metric' }]);
+    return Promise.resolve([
+      { value: 'My metric', label: 'My metric' },
+      { value: 'Another metric', label: 'Another metric' },
+    ]);
   }, []);
 
   const debouncedLoadOptions = useMemo(() => debounce(loadOptions, 200, { leading: true }), [loadOptions]);
 
   return (
     <SegmentAsync<string>
-      value={'Select metric'}
+      value={query.metric ?? 'Select metric'}
       inputMinWidth={150}
       loadOptions={debouncedLoadOptions}
       reloadOptionsOnChange={true}
--- public/app/plugins/datasource/prometheus/visualquery/PromQueryBuilder.tsx
+++ public/app/plugins/datasource/prometheus/visualquery/PromQueryBuilder.tsx
@@ -2,12 +2,22 @@ import { SegmentSection } from '@grafana/ui';
 import React from 'react';
 import { PromQueryEditorProps } from '../components/types';
 import { MetricSelect } from './MetricSelect';
+import { PromVisualQuery } from './types';
 
 export const PromQueryBuilder = React.memo<PromQueryEditorProps>((props) => {
+  const model: PromVisualQuery = {
+    metric: 'cortex_query_scheduler_queue_duration_seconds_bucket',
+    labels: [
+      { label: 'cluster', op: '=~', value: '$cluster' },
+      { label: 'job', op: '=~', value: '($namespace)/query-scheduler.*' },
+    ],
+    operations: [],
+  };
+
   return (
     <div>
       <SegmentSection label="Metric" fill={true}>
-        <MetricSelect />
+        <MetricSelect query={model} />
       </SegmentSection>
       <SegmentSection label="Labels" fill={true}>
         <div />
