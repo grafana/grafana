b1972f8085e57130563f325f00e023469b7438c5
Ryan McKinley <ryantxu@gmail.com>
2021-12-03 13:52:29 -0800

    init runtime

---
+++ public/app/features/runtime/init.ts
@@ -0,0 +1,58 @@
+import { UrlQueryMap } from '@grafana/data';
+import { getLocationSrv } from '@grafana/runtime';
+import { getConfig } from 'app/core/config';
+import { getDashboardSrv } from '../dashboard/services/DashboardSrv';
+import { getTimeSrv } from '../dashboard/services/TimeSrv';
+
+/**
+ * This will setup features that are accessible through the root window location
+ *
+ * This is useful for manipulating the application from external drivers like puppetter/cypress
+ *
+ * @internal and subject to change
+ */
+export function initWindowRuntime() {
+  (window as any).grafanaRuntime = {
+    /** flag for theme type */
+    getThemeType: () => {
+      const theme = getConfig().theme2;
+      if (theme.isDark) {
+        return 'dark';
+      }
+      if (theme.isLight) {
+        return 'light';
+      }
+      return '?';
+    },
+
+    /** Navigate the page within the currently loaded application */
+    load: (path: string, query?: UrlQueryMap) => {
+      if (query?.theme) {
+        throw new Error(`chaning theme requires full page refresh`);
+      }
+      getLocationSrv().update({
+        path,
+        query,
+        replace: true,
+        partial: false,
+      });
+    },
+
+    /** Get info for the current dashboard.  This will include the migrated dashboard JSON */
+    getDashboardInfo: () => {
+      const d = getDashboardSrv().getCurrent();
+      if (!d) {
+        return undefined;
+      }
+      const tr = getTimeSrv().timeRange();
+      return {
+        model: d.getSaveModelClone(),
+        timeRange: {
+          from: tr.from.valueOf(),
+          to: tr.to.valueOf(),
+          raw: tr.raw,
+        },
+      };
+    },
+  };
+}
