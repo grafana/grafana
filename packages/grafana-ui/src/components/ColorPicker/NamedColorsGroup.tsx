ef9f619c5880de153306b2a9a5f916aae9f8efea
thisisobate <obasiuche62@gmail.com>
2021-12-05 05:43:11 +0100

    implements next and previous arrow key navigation

--- packages/grafana-ui/src/components/ColorPicker/NamedColorsGroup.tsx
+++ packages/grafana-ui/src/components/ColorPicker/NamedColorsGroup.tsx
@@ -3,6 +3,7 @@ import { ThemeVizHue } from '@grafana/data';
 import { Property } from 'csstype';
 import { ColorSwatch, ColorSwatchVariant } from './ColorSwatch';
 import { upperFirst } from 'lodash';
+import { useFocusManager } from '@react-aria/focus';
 
 interface NamedColorsGroupProps {
   hue: ThemeVizHue;
@@ -18,6 +19,26 @@ const NamedColorsGroup: FunctionComponent<NamedColorsGroupProps> = ({
   ...otherProps
 }) => {
   const primaryShade = hue.shades.find((shade) => shade.primary)!;
+  const focusManager = useFocusManager();
+
+  const onKeyDownColorSelect = (e: React.KeyboardEvent, colorName: string) => {
+    if (e.key === 'Tab' || e.altKey || e.ctrlKey || e.metaKey) {
+      return;
+    }
+
+    switch (e.key) {
+      case 'Enter':
+        onColorSelect(colorName);
+        break;
+      case 'ArrowRight':
+        focusManager.focusNext({ wrap: true });
+        break;
+      case 'ArrowLeft':
+        focusManager.focusPrevious({ wrap: true });
+        break;
+    }
+    return;
+  };
 
   return (
     <>
@@ -30,6 +51,7 @@ const NamedColorsGroup: FunctionComponent<NamedColorsGroupProps> = ({
             variant={ColorSwatchVariant.Large}
             color={primaryShade.color}
             onClick={() => onColorSelect(primaryShade.name)}
+            onKeyDown={(event) => onKeyDownColorSelect(event, primaryShade.name)}
           />
         )}
         <div
@@ -47,6 +69,7 @@ const NamedColorsGroup: FunctionComponent<NamedColorsGroupProps> = ({
                     isSelected={shade.name === selectedColor}
                     color={shade.color}
                     onClick={() => onColorSelect(shade.name)}
+                    onKeyDown={(event) => onKeyDownColorSelect(event, shade.name)}
                   />
                 </div>
               )
