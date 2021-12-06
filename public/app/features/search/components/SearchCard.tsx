b6886def01c9fb5ccb824a1c9b810fee001d5c06
Ashley Harrison <ashley.harrison@grafana.com>
2021-12-06 13:29:54 +0000

    quick hack to do image retrying

--- public/app/features/search/components/SearchCard.tsx
+++ public/app/features/search/components/SearchCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 import { useTheme2 } from '@grafana/ui';
@@ -12,20 +12,54 @@ export interface Props {
 export function SearchCard({ item, themeId }: Props) {
   const theme = useTheme2();
   const styles = getStyles(theme);
+  const [imageLoaded, setImageLoaded] = useState(true);
+  const imageSrc = `/preview/dash/${item.uid}/square/${themeId}`;
+
+  const retryImage = () => {
+    setImageLoaded(false);
+    const img = new Image();
+    img.onload = () => setImageLoaded(true);
+    img.onerror = retryImage;
+    setTimeout(() => {
+      img.src = imageSrc;
+    }, 5000);
+  };
 
   return (
     <a className={styles.gridItem} key={item.uid} href={item.url}>
-      <img className={styles.image} src={`/preview/dash/${item.uid}/square/${themeId}`} />
+      {imageLoaded && (
+        <img
+          loading="lazy"
+          className={styles.image}
+          src={imageSrc}
+          onLoad={() => setImageLoaded(true)}
+          onError={retryImage}
+        />
+      )}
+      {!imageLoaded && <div className={styles.placeholder}>No preview available</div>}
     </a>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
   gridItem: css`
+    border-radius: 4px;
     height: 200px;
+    overflow: hidden;
+    position: relative;
   `,
   image: css`
+    width: 100%;
+  `,
+  placeholder: css`
+    align-items: center;
+    background: ${theme.colors.background.secondary};
+    color: ${theme.colors.text.secondary};
+    display: flex;
     height: 100%;
+    justify-content: center;
+    position: absolute;
+    top: 0;
     width: 100%;
   `,
 });
