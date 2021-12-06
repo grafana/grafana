e468fcf518f7dd9f56f313b88e9fb1205ad7f040
Maria Alexandra <239999+axelavargas@users.noreply.github.com>
2021-12-06 00:58:17 -0500

    Navigation: Implement Keyboard Navigation (#41618)
    * Navigation: Start creating new NavBarMenu component
    
    * Navigation: Apply new NavBarMenu to NavBarNext
    
    * Navigation: Remove everything to do with .sidemenu-open--xs
    
    * Navigation: Ensure search is passed to NavBarMenu
    
    * Navigation: Standardise NavBarMenuItem
    
    * This extra check isn't needed anymore
    
    * Navigation: Refactor <li> out of NavBarMenu
    
    * Navigation: Combine NavBarMenuItem with DropdownChild
    
    * use spread syntax since performance shouldn't be a concern for such small arrays
    
    * Improve active item logic
    
    * Ensure unique keys
    
    * Remove this duplicate code
    
    * Add unit tests for getActiveItem
    
    * Add tests for NavBarMenu
    
    * Rename mobileMenuOpen -> menuOpen in NavBarNext (since it can be used for mobile menu or megamenu)
    
    * just use index to key the items
    
    * Use exact versions of @react-aria packages
    
    * Navigation: Make the dropdown header a NavBarMenuItem
    
    * Navigation: Stop using dropdown-menu for styles
    
    * Navigation: Add react-aria relevant packages
    
    * Navigation: Refactor NavBarDropdown to support react aria
    
    * Navigation: apply keyboard navigation to NavBar component
    
    * Navigation: UseHover hook for triggering submenu on navbar
    
    * Navigation: rename testMenu component to NavBarItemButton
    
    * WIP
    
    * some hacks
    
    * Refactor: clean up keybinding events
    
    * Navigation:  render subtitle on item menu and disable it
    
    * Navigation: Adds react-aria types (#42113)
    
    * Refactor: refactor out to NavBarItemWithoutMenu
    
    * Refactor: cleaning up stuff
    
    * Refactor: comment out unused code
    
    * Chore: Removes section and uses items only
    
    * Chore: fix NavBarNext
    
    * Chore: adds tests
    
    * Refactor: minimize props api
    
    * Refactor: various refactors
    
    * Refactor: rename enableAllItems
    
    * Refactor: remove unused code
    
    * Refactor: fix clicking on menuitems
    
    * Refactor: use recommended onAction instead
    
    * Navigation: Fix a11y issues on NavBar
    
    * Navigation: Fix a11y navBar Next
    
    * Navigation: Remove unnecessary label prop, use link.text instead
    
    * Apply suggestions from code review
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    Co-authored-by: Ashley Harrison <ashley.harrison@grafana.com>
    
    * Apply unit tests suggestions from code review
    
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>
    
    * Update react-aria/menu package to latest version and apply PR suggestion
    
    Co-authored-by: Ashley Harrison <ashley.harrison@grafana.com>
    Co-authored-by: Hugo Häggmark <hugo.haggmark@grafana.com>
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>

---
+++ public/app/core/components/NavBar/NavBarItemMenuTrigger.tsx
@@ -0,0 +1,218 @@
+import React, { ReactElement, useState } from 'react';
+import { css, cx } from '@emotion/css';
+import { Icon, IconName, Link, useTheme2 } from '@grafana/ui';
+import { GrafanaTheme2, NavModelItem } from '@grafana/data';
+import { MenuTriggerProps } from '@react-types/menu';
+import { useMenuTriggerState } from '@react-stately/menu';
+import { useMenuTrigger } from '@react-aria/menu';
+import { useFocusVisible, useFocusWithin, useHover, useKeyboard } from '@react-aria/interactions';
+import { useButton } from '@react-aria/button';
+import { DismissButton, useOverlay } from '@react-aria/overlays';
+import { FocusScope } from '@react-aria/focus';
+
+import { NavBarItemMenuContext } from './context';
+
+export interface NavBarItemMenuTriggerProps extends MenuTriggerProps {
+  children: ReactElement;
+  item: NavModelItem;
+  isActive?: boolean;
+  label: string;
+}
+
+export function NavBarItemMenuTrigger(props: NavBarItemMenuTriggerProps): ReactElement {
+  const { item, isActive, label, children: menu, ...rest } = props;
+  const [menuHasFocus, setMenuHasFocus] = useState(false);
+  const theme = useTheme2();
+  const styles = getStyles(theme, isActive);
+
+  // Create state based on the incoming props
+  const state = useMenuTriggerState({ ...rest });
+
+  // Get props for the menu trigger and menu elements
+  const ref = React.useRef(null);
+  const { menuTriggerProps, menuProps } = useMenuTrigger({}, state, ref);
+
+  // style to the focused menu item
+  let { isFocusVisible } = useFocusVisible({ isTextInput: false });
+
+  const { hoverProps } = useHover({
+    onHoverChange: (isHovering) => {
+      if (isHovering) {
+        state.open();
+      } else {
+        state.close();
+      }
+    },
+  });
+
+  const { focusWithinProps } = useFocusWithin({
+    onFocusWithinChange: (isFocused) => {
+      if (isFocused && isFocusVisible) {
+        state.open();
+      }
+      if (!isFocused) {
+        state.close();
+        setMenuHasFocus(false);
+      }
+    },
+  });
+
+  const { keyboardProps } = useKeyboard({
+    onKeyDown: (e) => {
+      switch (e.key) {
+        case 'ArrowRight':
+          if (!state.isOpen) {
+            state.open();
+          }
+          setMenuHasFocus(true);
+          break;
+        default:
+          break;
+      }
+    },
+  });
+
+  // Get props for the button based on the trigger props from useMenuTrigger
+  const { buttonProps } = useButton(menuTriggerProps, ref);
+
+  let element = (
+    <button
+      className={styles.element}
+      {...buttonProps}
+      {...keyboardProps}
+      ref={ref}
+      onClick={item?.onClick}
+      aria-label={label}
+    >
+      <span className={styles.icon}>
+        {item?.icon && <Icon name={item.icon as IconName} size="xl" />}
+        {item?.img && <img src={item.img} alt={`${item.text} logo`} />}
+      </span>
+    </button>
+  );
+
+  if (item?.url) {
+    element =
+      !item.target && item.url.startsWith('/') ? (
+        <Link
+          {...buttonProps}
+          {...keyboardProps}
+          ref={ref}
+          href={item.url}
+          target={item.target}
+          onClick={item?.onClick}
+          className={styles.element}
+          aria-label={label}
+        >
+          <span className={styles.icon}>
+            {item?.icon && <Icon name={item.icon as IconName} size="xl" />}
+            {item?.img && <img src={item.img} alt={`${item.text} logo`} />}
+          </span>
+        </Link>
+      ) : (
+        <a
+          href={item.url}
+          target={item.target}
+          onClick={item?.onClick}
+          {...buttonProps}
+          {...keyboardProps}
+          ref={ref}
+          className={styles.element}
+          aria-label={label}
+        >
+          <span className={styles.icon}>
+            {item?.icon && <Icon name={item.icon as IconName} size="xl" />}
+            {item?.img && <img src={item.img} alt={`${item.text} logo`} />}
+          </span>
+        </a>
+      );
+  }
+
+  const overlayRef = React.useRef(null);
+  const { overlayProps } = useOverlay(
+    {
+      onClose: () => state.close(),
+      shouldCloseOnBlur: true,
+      isOpen: state.isOpen,
+      isDismissable: true,
+    },
+    overlayRef
+  );
+
+  return (
+    <div className={cx(styles.element, 'dropdown')} {...focusWithinProps} {...hoverProps}>
+      {element}
+      {state.isOpen && (
+        <NavBarItemMenuContext.Provider value={{ menuProps, menuHasFocus, onClose: () => state.close() }}>
+          <FocusScope restoreFocus>
+            <div {...overlayProps} ref={overlayRef}>
+              <DismissButton onDismiss={() => state.close()} />
+              {menu}
+              <DismissButton onDismiss={() => state.close()} />
+            </div>
+          </FocusScope>
+        </NavBarItemMenuContext.Provider>
+      )}
+    </div>
+  );
+}
+
+const getStyles = (theme: GrafanaTheme2, isActive?: boolean) => ({
+  container: css`
+    position: relative;
+    color: ${isActive ? theme.colors.text.primary : theme.colors.text.secondary};
+    list-style: none;
+
+    &:hover {
+      background-color: ${theme.colors.action.hover};
+      color: ${theme.colors.text.primary};
+
+      // TODO don't use a hardcoded class here, use isVisible in NavBarDropdown
+      .navbar-dropdown {
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+  `,
+  element: css`
+    background-color: transparent;
+    border: none;
+    color: inherit;
+    display: block;
+    line-height: ${theme.components.sidemenu.width}px;
+    padding: 0;
+    text-align: center;
+    width: ${theme.components.sidemenu.width}px;
+
+    &::before {
+      display: ${isActive ? 'block' : 'none'};
+      content: ' ';
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      width: 4px;
+      border-radius: 2px;
+      background-image: ${theme.colors.gradients.brandVertical};
+    }
+
+    &:focus-visible {
+      background-color: ${theme.colors.action.hover};
+      box-shadow: none;
+      color: ${theme.colors.text.primary};
+      outline: 2px solid ${theme.colors.primary.main};
+      outline-offset: -2px;
+      transition: none;
+    }
+  `,
+  icon: css`
+    height: 100%;
+    width: 100%;
+
+    img {
+      border-radius: 50%;
+      height: ${theme.spacing(3)};
+      width: ${theme.spacing(3)};
+    }
+  `,
+});
