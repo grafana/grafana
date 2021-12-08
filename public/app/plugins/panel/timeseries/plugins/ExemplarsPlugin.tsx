429b420f241db0e478ae15f2d1218d2ad1be27aa
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2021-12-08 17:17:38 +0100

    Autobreakdowns: Selected area (#42890)
    * Refactor, clean up a little
    
    * Selected area is visible

--- public/app/plugins/panel/timeseries/plugins/ExemplarsPlugin.tsx
+++ public/app/plugins/panel/timeseries/plugins/ExemplarsPlugin.tsx
@@ -7,8 +7,8 @@ import {
   TIME_SERIES_TIME_FIELD_NAME,
   TIME_SERIES_VALUE_FIELD_NAME,
 } from '@grafana/data';
-import { EventsCanvas, FIXED_UNIT, UPlotConfigBuilder } from '@grafana/ui';
-import React, { useCallback, useLayoutEffect, useRef } from 'react';
+import { EventsCanvas, FIXED_UNIT, UPlotConfigBuilder, PlotSelection } from '@grafana/ui';
+import React, { useCallback, useLayoutEffect, useRef, useState, useEffect } from 'react';
 import { ExemplarMarker } from './ExemplarMarker';
 import uPlot from 'uplot';
 
@@ -17,16 +17,73 @@ interface ExemplarsPluginProps {
   exemplars: DataFrame[];
   timeZone: TimeZone;
   getFieldLinks: (field: Field, rowIndex: number) => Array<LinkModel<Field>>;
+  onSelect?: (range: { from: number; to: number }) => void;
+  autoBreakdownsEnabled?: boolean;
 }
 
-export const ExemplarsPlugin: React.FC<ExemplarsPluginProps> = ({ exemplars, timeZone, getFieldLinks, config }) => {
+export const ExemplarsPlugin: React.FC<ExemplarsPluginProps> = ({
+  exemplars,
+  timeZone,
+  getFieldLinks,
+  config,
+  autoBreakdownsEnabled = false,
+  onSelect,
+}) => {
   const plotInstance = useRef<uPlot>();
+  const [selection, setSelection] = useState<PlotSelection | null>(null);
+
+  useEffect(() => {
+    if (selection && onSelect) {
+      onSelect({ from: selection.min, to: selection.max });
+    }
+  }, [selection, onSelect]);
+
+  useEffect(() => {
+    return () => {
+      setSelection(null);
+      plotInstance.current?.setSelect({ top: 0, left: 0, width: 0, height: 0 });
+    };
+    //@ts-ignore
+  }, []);
 
   useLayoutEffect(() => {
     config.addHook('init', (u) => {
       plotInstance.current = u;
     });
-  }, [config]);
+
+    if (autoBreakdownsEnabled) {
+      config.addHook('setSelect', (u) => {
+        setSelection({
+          min: u.posToVal(u.select.left, 'x'),
+          max: u.posToVal(u.select.left + u.select.width, 'x'),
+          bbox: {
+            left: u.select.left,
+            top: 0,
+            height: u.select.height,
+            width: u.select.width,
+          },
+        });
+      });
+
+      config.addHook('drawAxes', (u) => {
+        if (selection) {
+          let left = u.valToPos(selection.min, 'x', true);
+          let right = u.valToPos(selection.max, 'x', true);
+          u.ctx.save();
+          u.ctx.fillStyle = 'rgba(0,255,0,0.1)';
+          u.ctx.fillRect(left, u.bbox.top, right - left, u.bbox.height);
+          u.ctx.restore();
+        }
+      });
+
+      // config.setCursor({
+      //   drag: {
+      //     x: true,
+      //     y: true,
+      //   },
+      // });
+    }
+  }, [config, selection, autoBreakdownsEnabled]);
 
   const mapExemplarToXYCoords = useCallback((dataFrame: DataFrame, dataFrameFieldIndex: DataFrameFieldIndex) => {
     const time = dataFrame.fields.find((f) => f.name === TIME_SERIES_TIME_FIELD_NAME);
