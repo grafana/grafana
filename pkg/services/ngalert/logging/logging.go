d2d7241b7281086c57bc4a917ad224e9a7a2de24
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2021-12-09 16:47:55 -0500

    Implement go-kit/log.Logger for internal logger.

--- pkg/services/ngalert/logging/logging.go
+++ pkg/services/ngalert/logging/logging.go
@@ -3,6 +3,8 @@ package logging
 import (
 	"strings"
 
+	"github.com/go-kit/log/level"
+
 	glog "github.com/grafana/grafana/pkg/infra/log"
 )
 
@@ -21,4 +23,41 @@ func (w *GoKitWrapper) Write(p []byte) (n int, err error) {
 	withoutNewline := strings.TrimSuffix(string(p), "\n")
 	w.logger.Info(withoutNewline)
 	return len(p), nil
+}
+
+// Log implements interface go-kit/log/Logger. It tries to extract level and message from the context and writes to underlying message.
+// To successfully extract the log level, the first pair of elements should be 'lvl' and log level. Otherwise, it falls back to info.
+// The following pair should be 'msg' and message. Otherwise, it uses the empty string as message.
+func (l *GoKitWrapper) Log(keyvals ...interface{}) error {
+	if len(keyvals) == 0 {
+		return nil
+	}
+
+	f := l.logger.Info
+	startwith := 0
+	if keyvals[0] == level.Key() {
+		startwith = 2
+		switch keyvals[1] {
+		case level.DebugValue():
+			f = l.logger.Debug
+		case level.InfoValue():
+			f = l.logger.Info
+		case level.WarnValue():
+			f = l.logger.Warn
+		case level.ErrorValue():
+			f = l.logger.Error
+		}
+	}
+
+	msg := ""
+	if keyvals[startwith] == "msg" {
+		str, ok := keyvals[startwith+1].(string)
+		if ok {
+			msg = str
+			startwith += 2
+		}
+	}
+
+	f(msg, keyvals[startwith:]...)
+	return nil
 }
