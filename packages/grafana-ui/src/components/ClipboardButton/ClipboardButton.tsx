ba4210e36af457f29104d1cd62369f7448b87ec2
kay delaney <kay@grafana.com>
2021-12-10 18:37:25 +0000

    UI/ClipboardButton: Remove ClipboardJS in favor of native Clipboard API Closes #42365

--- packages/grafana-ui/src/components/ClipboardButton/ClipboardButton.tsx
+++ packages/grafana-ui/src/components/ClipboardButton/ClipboardButton.tsx
@@ -1,51 +1,26 @@
-import React, { PureComponent } from 'react';
-import Clipboard from 'clipboard';
+import React, { useCallback } from 'react';
 import { Button, ButtonProps } from '../Button';
 
 export interface Props extends ButtonProps {
   /** A function that returns text to be copied */
   getText(): string;
   /** Callback when the text has been successfully copied */
-  onClipboardCopy?(e: Clipboard.Event): void;
+  onClipboardCopy?(): void;
   /** Callback when there was an error copying the text */
-  onClipboardError?(e: Clipboard.Event): void;
+  onClipboardError?(e: any): void;
 }
 
-export class ClipboardButton extends PureComponent<Props> {
-  private clipboard!: Clipboard;
-  private elem!: HTMLButtonElement;
-
-  setRef = (elem: HTMLButtonElement) => {
-    this.elem = elem;
-  };
-
-  componentDidMount() {
-    const { getText, onClipboardCopy, onClipboardError } = this.props;
-
-    this.clipboard = new Clipboard(this.elem, {
-      text: () => getText(),
-    });
-
-    this.clipboard.on('success', (e: Clipboard.Event) => {
-      onClipboardCopy && onClipboardCopy(e);
-    });
-
-    this.clipboard.on('error', (e: Clipboard.Event) => {
-      onClipboardError && onClipboardError(e);
-    });
-  }
-
-  componentWillUnmount() {
-    this.clipboard.destroy();
-  }
-
-  render() {
-    const { getText, onClipboardCopy, onClipboardError, children, ...buttonProps } = this.props;
-
-    return (
-      <Button {...buttonProps} ref={this.setRef}>
-        {children}
-      </Button>
+export function ClipboardButton({ onClipboardCopy, onClipboardError, children, getText, ...buttonProps }: Props) {
+  const copyText = useCallback(() => {
+    navigator.clipboard.writeText(getText()).then(
+      () => onClipboardCopy?.(),
+      (e) => onClipboardError?.(e)
     );
-  }
+  }, [getText, onClipboardCopy, onClipboardError]);
+
+  return (
+    <Button onClick={copyText} {...buttonProps}>
+      {children}
+    </Button>
+  );
 }
