8ca1b98207a8219a3411c69f088a531e74bc4b8f
Serge Zaitsev <serge.zaitsev@grafana.com>
2021-12-13 10:32:26 +0100

    clean up leftovers of macaron imports

--- pkg/web/response_writer_test.go
+++ pkg/web/response_writer_test.go
@@ -5,13 +5,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gopkg.in/macaron.v1"
 )
 
 func Test_responseWriter_WriteHeader(t *testing.T) {
 	t.Run("it should set status code as expected", func(t *testing.T) {
 		f := fakeResponseWriter{}
-		rw := macaron.NewResponseWriter("GET", &f)
+		rw := NewResponseWriter("GET", &f)
 		rw.WriteHeader(200)
 		require.Equal(t, 200, rw.Status())
 		require.Equal(t, 200, f.Status)
@@ -19,7 +18,7 @@ func Test_responseWriter_WriteHeader(t *testing.T) {
 
 	t.Run("it should set status code to 500 if WriteHeader is called with invalid HTTP status", func(t *testing.T) {
 		f := fakeResponseWriter{}
-		rw := macaron.NewResponseWriter("GET", &f)
+		rw := NewResponseWriter("GET", &f)
 		rw.WriteHeader(0)
 		require.Equal(t, 500, rw.Status())
 		require.Equal(t, 500, f.Status)
