caa7fdaa04243bb67e4644f7519b2e23e2c5d113
Levente Balogh <balogh.levente.hu@gmail.com>
2021-12-13 09:46:26 +0100

    chore: jump to the correct step

--- scripts/check-breaking-changes.sh
+++ scripts/check-breaking-changes.sh
@@ -34,7 +34,7 @@ while IFS= read -r line; do
     if [ $STATUS -gt 0 ]
     then
         EXIT_CODE=1
-        GITHUB_MESSAGE="${GITHUB_MESSAGE}**\`${PACKAGE_NAME}\`** has possible breaking changes ([more info](${GITHUB_JOB_LINK}#step:6:1))<br />"    
+        GITHUB_MESSAGE="${GITHUB_MESSAGE}**\`${PACKAGE_NAME}\`** has possible breaking changes ([more info](${GITHUB_JOB_LINK}#step:7:1))<br />"    
     fi    
 
 done <<< "$PACKAGES"
