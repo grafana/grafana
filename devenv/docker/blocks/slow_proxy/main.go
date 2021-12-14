f1a869753b4a00778812a42d754b5497b89c0241
Daniel Lee <dan.limerick@gmail.com>
2021-12-14 17:04:38 +0100

    remove newlines from request headers

--- devenv/docker/blocks/slow_proxy/main.go
+++ devenv/docker/blocks/slow_proxy/main.go
@@ -34,11 +34,24 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		safeSleep := strings.Replace(sleep.String(), "\n", "", -1)
 		safeRequestUri := strings.Replace(r.RequestURI, "\n", "", -1)
+		safeHeader := getSafeHeader(r.Header)
 
-		log.Printf("sleeping for %s then proxying request: url '%s', headers: '%v'", safeSleep, safeRequestUri, r.Header)
+		log.Printf("sleeping for %s then proxying request: url '%s', headers: '%v'", safeSleep, safeRequestUri, safeHeader)
 		<-time.After(sleep)
 		proxy.ServeHTTP(w, r)
 	})
 
 	log.Fatal(http.ListenAndServe(":3011", nil))
+}
+
+func getSafeHeader(header http.Header) map[string][]string {
+	safeHeader := make(map[string][]string)
+	for name, values := range header {
+		safeName := strings.Replace(name, "\n", "", -1)
+		for _, value := range values {
+			safeHeader[name] = append(safeHeader[safeName], strings.Replace(value, "\n", "", -1))
+		}
+	}
+
+	return safeHeader
 }
