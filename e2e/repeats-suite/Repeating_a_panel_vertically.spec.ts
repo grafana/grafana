646828271b0d782f5110b41df9ee76366131a22e
kay delaney <kay@grafana.com>
2021-12-15 10:59:51 +0000

    user essentials mob! :trident:

--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -1,5 +1,4 @@
 import { e2e } from '@grafana/e2e';
-
 const PAGE_UNDER_TEST = 'OY8Ghjt7k/repeating-a-panel-vertically';
 
 describe('Repeating a panel vertically', () => {
@@ -13,15 +12,19 @@ describe('Repeating a panel vertically', () => {
     //   e2e.components.Panels.Panel.title('Panel Title 3'),
     // ];
 
+    let prevTop = Number.NEGATIVE_INFINITY;
     e2e()
       .get(`[data-testid^="data-testid Panel header"]`)
       .should('have.length', 3)
-      .each((element) => cy.log(element))
-      .then((element) => {
-        cy.log(`have ${element.length} panels??`);
-        cy.log(element[0].getBoundingClientRect().top);
-      });
+      .each((el, i) => {
+        expect(el).to.have.text(`Panel Title ${i + 1}`);
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        expect(el).to.be.visible;
 
+        const top = el[0].getBoundingClientRect().top;
+        expect(top).to.be.greaterThan(prevTop);
+        prevTop = top;
+      });
     //panelTitles.forEach((panelTitle, i) => {
     //  panelTitle.should('be.visible');
     //  panelTitle.then((element) => {
