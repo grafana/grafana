5a0dd3cca0a4c25e459c4a58c5bb2607bf5cbc54
Ashley Harrison <ashley.harrison@grafana.com>
2021-12-15 15:42:23 +0000

    Use the api to login in cypress by default

--- packages/grafana-e2e/src/flows/login.ts
+++ packages/grafana-e2e/src/flows/login.ts
@@ -1,22 +1,34 @@
 import { e2e } from '../index';
+import { fromBaseUrl } from '../support/url';
 
 const DEFAULT_USERNAME = 'admin';
 const DEFAULT_PASSWORD = 'admin';
 
-export const login = (username = DEFAULT_USERNAME, password = DEFAULT_PASSWORD) => {
-  e2e().logToConsole('Logging in with username:', username);
-  e2e.pages.Login.visit();
-  e2e.pages.Login.username()
-    .should('be.visible') // prevents flakiness
-    .type(username);
-  e2e.pages.Login.password().type(password);
-  e2e.pages.Login.submit().click();
+export const login = (username = DEFAULT_USERNAME, password = DEFAULT_PASSWORD, loginViaApi = true) => {
+  if (loginViaApi) {
+    cy.request({
+      method: 'POST',
+      url: fromBaseUrl('/login'),
+      body: {
+        user: username,
+        password,
+      },
+    });
+  } else {
+    e2e().logToConsole('Logging in with username:', username);
+    e2e.pages.Login.visit();
+    e2e.pages.Login.username()
+      .should('be.visible') // prevents flakiness
+      .type(username);
+    e2e.pages.Login.password().type(password);
+    e2e.pages.Login.submit().click();
 
-  // Local tests will have insecure credentials
-  if (password === DEFAULT_PASSWORD) {
-    e2e.pages.Login.skip().should('be.visible').click();
+    // Local tests will have insecure credentials
+    if (password === DEFAULT_PASSWORD) {
+      e2e.pages.Login.skip().should('be.visible').click();
+    }
+
+    e2e().get('.login-page').should('not.exist');
   }
-
-  e2e().get('.login-page').should('not.exist');
   e2e().logToConsole('Logged in with username:', username);
 };
