904568e02c2230afd34e460de67b4e72d9e80f2f
Alexandra Vargas <alexa1866@gmail.com>
2021-12-15 11:36:36 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -3,7 +3,7 @@ import { e2e } from '@grafana/e2e';
 const PAGE_UNDER_TEST = 'OY8Ghjt7k/repeating-a-panel-vertically';
 
 describe('Repeating a panel vertically', () => {
-  it('should be able to repeat a panel vertically', () => {
+  it('should be able to repeat a panel vertically', async () => {
     e2e.flows.login('admin', 'admin');
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
 
@@ -13,15 +13,19 @@ describe('Repeating a panel vertically', () => {
       e2e.components.Panels.Panel.title('Panel Title 3'),
     ];
 
-    panelTitles.forEach((panelTitle, i) => {
-      panelTitle.should('be.visible');
-      panelTitle.then(element => {
-        cy.log(element.getBoundingClientRect().top);
-      })
-      // expect(
-      //   panelTitles[i].getBoundingClientRect().y > panelTitles?.[i - 1].getBoundingClientRect().y ??
-      //     Number.NEGATIVE_INFINITY
-      // );
+    await e2e.components.Panels.Panel.title('Panel Title 1').then((element) => {
+      cy.log(element[0].getBoundingClientRect().top);
     });
+
+    //panelTitles.forEach((panelTitle, i) => {
+    //  panelTitle.should('be.visible');
+    //  panelTitle.then((element) => {
+    //    cy.log(element[0].getBoundingClientRect().top);
+    //  });
+    // expect(
+    //   panelTitles[i].getBoundingClientRect().y > panelTitles?.[i - 1].getBoundingClientRect().y ??
+    //     Number.NEGATIVE_INFINITY
+    // );
+    //});
   });
 });
