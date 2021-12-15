bb90814a93e923e98554e97dcd1f81940e54d42b
joshhunt <josh@trtr.co>
2021-12-15 10:48:24 +0000

    user essentials mob! :trident:

--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -3,19 +3,24 @@ import { e2e } from '@grafana/e2e';
 const PAGE_UNDER_TEST = 'OY8Ghjt7k/repeating-a-panel-vertically';
 
 describe('Repeating a panel vertically', () => {
-  it('should be able to repeat a panel vertically', async () => {
+  it('should be able to repeat a panel vertically', () => {
     e2e.flows.login('admin', 'admin');
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
 
-    const panelTitles = [
-      e2e.components.Panels.Panel.title('Panel Title 1'),
-      e2e.components.Panels.Panel.title('Panel Title 2'),
-      e2e.components.Panels.Panel.title('Panel Title 3'),
-    ];
+    // const panelTitles = [
+    //   e2e.components.Panels.Panel.title('Panel Title 1'),
+    //   e2e.components.Panels.Panel.title('Panel Title 2'),
+    //   e2e.components.Panels.Panel.title('Panel Title 3'),
+    // ];
 
-    await e2e.components.Panels.Panel.title('Panel Title 1').then((element) => {
-      cy.log(element[0].getBoundingClientRect().top);
-    });
+    e2e()
+      .get(`[data-testid^="data-testid Panel header"]`)
+      .should('have.length', 3)
+      .each((element) => cy.log(element))
+      .then((element) => {
+        cy.log(`have ${element.length} panels??`);
+        cy.log(element[0].getBoundingClientRect().top);
+      });
 
     //panelTitles.forEach((panelTitle, i) => {
     //  panelTitle.should('be.visible');
