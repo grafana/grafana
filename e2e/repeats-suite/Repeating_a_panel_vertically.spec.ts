f989c45ee6458c76cdfb215fc2c367e08dc8916d
joshhunt <josh@trtr.co>
2021-12-15 09:47:55 +0000

    user essentials mob! :trident:

--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -6,8 +6,11 @@ describe('Repeating a panel vertically', () => {
   it('should be able to repeat a panel vertically', () => {
     e2e.flows.login('admin', 'admin');
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
-    e2e.components.Panels.Panel.title('Panel Title 1').should('be.visible');
-    e2e.components.Panels.Panel.title('Panel Title 2').should('be.visible');
-    e2e.components.Panels.Panel.title('Panel Title 3').should('be.visible');
+
+    const panelTitles = [
+      e2e.components.Panels.Panel.title('Panel Title 1'),
+      e2e.components.Panels.Panel.title('Panel Title 2'),
+      e2e.components.Panels.Panel.title('Panel Title 3'),
+    ];
   });
 });
