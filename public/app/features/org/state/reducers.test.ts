d7921d22c1e994cb2810ac29bd567896c8e1b844
thisisobate <obasiuche62@gmail.com>
2021-12-15 17:25:52 +0100

    writes test for userOrganizationsLoaded reducer

--- public/app/features/org/state/reducers.test.ts
+++ public/app/features/org/state/reducers.test.ts
@@ -1,6 +1,12 @@
 import { reducerTester } from '../../../../test/core/redux/reducerTester';
-import { OrganizationState } from '../../../types';
-import { initialState, organizationLoaded, organizationReducer, setOrganizationName } from './reducers';
+import { OrganizationState, OrgRole } from '../../../types';
+import {
+  initialState,
+  organizationLoaded,
+  organizationReducer,
+  userOrganizationsLoaded,
+  setOrganizationName,
+} from './reducers';
 
 describe('organizationReducer', () => {
   describe('when organizationLoaded is dispatched', () => {
@@ -23,6 +29,22 @@ describe('organizationReducer', () => {
         .thenStateShouldEqual({
           organization: { id: 1, name: 'New Name' },
           userOrg: [],
+        });
+    });
+  });
+
+  describe('when userOrganizationsLoaded is dispatched', () => {
+    it('then state should be correct', () => {
+      reducerTester<OrganizationState>()
+        .givenReducer(organizationReducer, {
+          ...initialState,
+          organization: { id: 1, name: 'An org' },
+          userOrg: [],
+        })
+        .whenActionIsDispatched(userOrganizationsLoaded([{ orgId: 1, name: 'New org', role: OrgRole.Editor }]))
+        .thenStateShouldEqual({
+          organization: { id: 1, name: 'An org' },
+          userOrg: [{ orgId: 1, name: 'New org', role: OrgRole.Editor }],
         });
     });
   });
