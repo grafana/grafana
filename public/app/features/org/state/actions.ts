ea71dafbfb3410fabeef2c7c2520f74e48e30fee
Uchechukwu Obasi <obasiuche62@gmail.com>
2021-12-15 09:42:52 +0100

    Orgs: move duplicate API calls to Redux (#43030)
    * WIP: move user/using api call to redux
    
    * WIP: use redux instead in NewOrgPage component
    
    * moved all the createOrg logic into redux
    
    * type the redux state and action creators automatically
    
    * adds test for setUserOrganization thunk
    
    * update selectOrgPage to use Redux instead
    
    * removes unnecessary dispatch call

--- public/app/features/org/state/actions.ts
+++ public/app/features/org/state/actions.ts
@@ -2,6 +2,7 @@ import { ThunkResult } from 'app/types';
 import { getBackendSrv } from '@grafana/runtime';
 import { organizationLoaded } from './reducers';
 import { updateConfigurationSubtitle } from 'app/core/actions';
+import { getConfig } from 'app/core/config';
 
 type OrganizationDependencies = { getBackendSrv: typeof getBackendSrv };
 
@@ -26,5 +27,28 @@ export function updateOrganization(
 
     dispatch(updateConfigurationSubtitle(organization.name));
     dispatch(loadOrganization(dependencies));
+  };
+}
+
+export function setUserOrganization(
+  orgId: number,
+  dependencies: OrganizationDependencies = { getBackendSrv: getBackendSrv }
+): ThunkResult<any> {
+  return async (dispatch) => {
+    const organizationResponse = await dependencies.getBackendSrv().post('/api/user/using/' + orgId);
+
+    dispatch(updateConfigurationSubtitle(organizationResponse.name));
+  };
+}
+
+export function createOrganization(
+  newOrg: { name: string },
+  dependencies: OrganizationDependencies = { getBackendSrv: getBackendSrv }
+): ThunkResult<any> {
+  return async (dispatch) => {
+    const result = await dependencies.getBackendSrv().post('/api/orgs/', newOrg);
+
+    dispatch(setUserOrganization(result.orgId));
+    window.location.href = getConfig().appSubUrl + '/org';
   };
 }
