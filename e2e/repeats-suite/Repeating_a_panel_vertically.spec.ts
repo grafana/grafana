1f1f8e3c3a231b5a87323e166a9dc42465636bcf
thisisobate <obasiuche62@gmail.com>
2021-12-15 12:08:04 +0100

    user essentials mob! :trident:

--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -6,12 +6,6 @@ describe('Repeating a panel vertically', () => {
     e2e.flows.login('admin', 'admin');
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
 
-    // const panelTitles = [
-    //   e2e.components.Panels.Panel.title('Panel Title 1'),
-    //   e2e.components.Panels.Panel.title('Panel Title 2'),
-    //   e2e.components.Panels.Panel.title('Panel Title 3'),
-    // ];
-
     let prevTop = Number.NEGATIVE_INFINITY;
     e2e()
       .get(`[data-testid^="data-testid Panel header"]`)
@@ -25,15 +19,5 @@ describe('Repeating a panel vertically', () => {
         expect(top).to.be.greaterThan(prevTop);
         prevTop = top;
       });
-    //panelTitles.forEach((panelTitle, i) => {
-    //  panelTitle.should('be.visible');
-    //  panelTitle.then((element) => {
-    //    cy.log(element[0].getBoundingClientRect().top);
-    //  });
-    // expect(
-    //   panelTitles[i].getBoundingClientRect().y > panelTitles?.[i - 1].getBoundingClientRect().y ??
-    //     Number.NEGATIVE_INFINITY
-    // );
-    //});
   });
 });
