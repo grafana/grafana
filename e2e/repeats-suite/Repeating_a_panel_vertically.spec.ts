c3b56eb7973428e931a23a04fe3f58434871412c
thisisobate <obasiuche62@gmail.com>
2021-12-15 11:10:25 +0100

    user essentials mob! :trident:

--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -1,4 +1,5 @@
 import { e2e } from '@grafana/e2e';
+import { TypeSelectors } from '@grafana/e2e/src/support';
 
 const PAGE_UNDER_TEST = 'OY8Ghjt7k/repeating-a-panel-vertically';
 
@@ -7,7 +8,7 @@ describe('Repeating a panel vertically', () => {
     e2e.flows.login('admin', 'admin');
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
 
-    const panelTitles = [
+    const panelTitles: TypeSelectors = [
       e2e.components.Panels.Panel.title('Panel Title 1'),
       e2e.components.Panels.Panel.title('Panel Title 2'),
       e2e.components.Panels.Panel.title('Panel Title 3'),
