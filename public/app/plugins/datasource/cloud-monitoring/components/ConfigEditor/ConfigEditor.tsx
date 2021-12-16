5c0cf7fb05c0191daae487165275673fa6791bb6
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2021-12-16 17:41:28 +0100

    Reduce wording and move down tooltip

--- public/app/plugins/datasource/cloud-monitoring/components/ConfigEditor/ConfigEditor.tsx
+++ public/app/plugins/datasource/cloud-monitoring/components/ConfigEditor/ConfigEditor.tsx
@@ -17,69 +17,9 @@ export class ConfigEditor extends PureComponent<Props> {
 
     return (
       <>
-        <div className="gf-form-group">
-          <div className="grafana-info-box">
-            <h4>Google Cloud Monitoring Authentication</h4>
-            <p>
-              There are two ways to authenticate the Google Cloud Monitoring plugin - either by uploading a Service
-              Account key file or by automatically retrieving credentials from the Google metadata server. The latter
-              option is only available when running Grafana on a GCE virtual machine.
-            </p>
-
-            <h5>Uploading a Service Account Key File</h5>
-            <p>
-              To authenticate with the Google Cloud Monitoring API, you need to create a Google Cloud Platform (GCP)
-              Service Account and key for the Project you want to show data for. A Grafana data source integrates with
-              Project. If you want to visualize data from multiple GCP Projects then you need to create one data source
-              per GCP Project.
-            </p>
-            <p>
-              The <strong>Monitoring Viewer</strong> role provides all the permissions that Grafana needs. The following
-              APIs need to be enabled on GCP for the data source to work:{' '}
-              <a
-                className="external-link"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://console.cloud.google.com/apis/library/monitoring.googleapis.com"
-              >
-                Monitoring API
-              </a>
-              {', '}
-              <a
-                className="external-link"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://console.cloud.google.com/apis/library/cloudresourcemanager.googleapis.com"
-              >
-                Cloud Resource Manager API
-              </a>
-            </p>
-
-            <h5>GCE Default Service Account</h5>
-            <p>
-              If Grafana is running on a Google Compute Engine (GCE) virtual machine, it is possible for Grafana to
-              automatically retrieve the default project id and authentication token from the metadata server. In order
-              for this to work, you need to make sure that you have a service account that is set up as the default
-              account for the virtual machine and that the service account has been given read access to the Google
-              Cloud Monitoring Monitoring API.
-            </p>
-
-            <p>
-              Detailed instructions on how to create a Service Account can be found{' '}
-              <a
-                className="external-link"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://grafana.com/docs/grafana/latest/datasources/google-cloud-monitoring/"
-              >
-                in the documentation.
-              </a>
-            </p>
-          </div>
-        </div>
-
+        <h3 className="page-heading">Authentication</h3>
         <FieldSet>
-          <InlineField label="Authentication type" labelWidth={20}>
+          <InlineField label="Type" labelWidth={20}>
             <Select
               menuShouldPortal
               width={40}
@@ -110,6 +50,21 @@ export class ConfigEditor extends PureComponent<Props> {
             ></JWTConfig>
           )}
         </FieldSet>
+        <div className="gf-form-group">
+          <div className="grafana-info-box">
+            <p>
+              Don’t know how to get a service account key file or create a service account? Read more{' '}
+              <a
+                className="external-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://grafana.com/docs/grafana/latest/datasources/google-cloud-monitoring/"
+              >
+                in the documentation.
+              </a>
+            </p>
+          </div>
+        </div>
         {jsonData.authenticationType === AuthType.GCE && (
           <Alert title="" severity="info">
             Verify GCE default service account by clicking Save & Test
