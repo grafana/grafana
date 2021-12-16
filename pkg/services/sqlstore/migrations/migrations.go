6ab154ae709ba23de2602eb6379ab3a05cb17b96
Ryan McKinley <ryantxu@gmail.com>
2021-12-15 18:49:59 -0800

    add migration

--- pkg/services/sqlstore/migrations/migrations.go
+++ pkg/services/sqlstore/migrations/migrations.go
@@ -60,6 +60,7 @@ func (*OSSMigrations) AddMigration(mg *Migrator) {
 		if mg.Cfg.IsLiveConfigEnabled() {
 			addLiveChannelMigrations(mg)
 		}
+
 		if mg.Cfg.IsDashboardPreviesEnabled() {
 			addDashboardThumbsMigrations(mg)
 		}
