c36884e8d1a6f59c8d89a92d272b5e6ee67d9d72
Nathan Marrs <nathanielmarrs@gmail.com>
2021-12-17 14:05:57 -0800

    Canvas: Add button element + basic onClick support (#43252)

--- public/app/features/canvas/elements/button.tsx
+++ public/app/features/canvas/elements/button.tsx
@@ -5,19 +5,26 @@ import { DimensionContext } from 'app/features/dimensions/context';
 import { TextDimensionEditor } from 'app/features/dimensions/editors/TextDimensionEditor';
 import { TextDimensionConfig } from 'app/features/dimensions/types';
 import { CanvasElementItem, CanvasElementProps } from '../element';
+import { APIEditor, APIEditorConfig, callApi } from 'app/plugins/panel/canvas/editor/APIEditor';
 
 interface ButtonData {
   text?: string;
+  api?: APIEditorConfig;
 }
 
 interface ButtonConfig {
   text?: TextDimensionConfig;
+  api?: APIEditorConfig;
 }
 
 class ButtonDisplay extends PureComponent<CanvasElementProps<ButtonConfig, ButtonData>> {
   render() {
     const { data } = this.props;
-    const onClick = () => console.log('button being clicked :)');
+    const onClick = () => {
+      if (data?.api) {
+        callApi(data.api);
+      }
+    };
 
     return <Button onClick={onClick}>{data?.text}</Button>;
   }
@@ -43,6 +50,7 @@ export const buttonItem: CanvasElementItem<ButtonConfig, ButtonData> = {
   prepareData: (ctx: DimensionContext, cfg: ButtonConfig) => {
     const data: ButtonData = {
       text: cfg?.text ? ctx.getText(cfg.text).value() : '',
+      api: cfg?.api ?? undefined,
     };
 
     return data;
@@ -51,12 +59,20 @@ export const buttonItem: CanvasElementItem<ButtonConfig, ButtonData> = {
   // Heatmap overlay options
   registerOptionsUI: (builder) => {
     const category = ['Button'];
-    builder.addCustomEditor({
-      category,
-      id: 'textSelector',
-      path: 'config.text',
-      name: 'Text',
-      editor: TextDimensionEditor,
-    });
+    builder
+      .addCustomEditor({
+        category,
+        id: 'textSelector',
+        path: 'config.text',
+        name: 'Text',
+        editor: TextDimensionEditor,
+      })
+      .addCustomEditor({
+        category,
+        id: 'apiSelector',
+        path: 'config.api',
+        name: 'API',
+        editor: APIEditor,
+      });
   },
 };
