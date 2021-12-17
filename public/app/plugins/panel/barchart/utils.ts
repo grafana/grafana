c61285534d7f6c02cdcd68821885b6cbe61b92ad
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2021-12-17 19:00:28 +0100

    BarChart: refactor x tick label placement logic (#43256) (#43297)
    (cherry picked from commit dd247f26dec84bcaac09d1a8ec40c5333923cf8b)
    
    Co-authored-by: Leon Sorokin <leeoniya@gmail.com>

--- public/app/plugins/panel/barchart/utils.ts
+++ public/app/plugins/panel/barchart/utils.ts
@@ -108,6 +108,7 @@ export const preparePlotConfigBuilder: UPlotConfigPrepFn<BarChartOptions> = ({
   builder.addScale({
     scaleKey: 'x',
     isTime: false,
+    range: config.xRange,
     distribution: ScaleDistribution.Ordinal,
     orientation: vizOrientation.xOri,
     direction: vizOrientation.xDir,
