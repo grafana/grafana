5e863a291d9372e1e8d4006b0f7a5d3678e34ee4
Artur Wierzbicki <artur@arturwierzbicki.com>
2021-12-17 18:39:38 +0400

    #41993: refactor measureDataRenderDelay to work with same-reference-value-arrays

--- public/app/plugins/panel/timeseries/measureDataRenderDelay.ts
+++ public/app/plugins/panel/timeseries/measureDataRenderDelay.ts
@@ -2,28 +2,42 @@ import { DataFrame, FieldType } from '@grafana/data';
 import { LivePerformance, MeasurementName } from 'app/core/services/LivePerformance';
 import { closestIdx } from 'app/features/live/data/StreamingDataFrame';
 
+const lastSeenTimeByTimeValuesRef = new WeakMap();
+
+const timeValuesArray = (frame: DataFrame): number[] | undefined =>
+  frame.fields?.find((f) => f.type === FieldType.time)?.values?.toArray();
+
+const getLastSeenTime = (newTimeValues: number[], prevFrame: DataFrame): number | undefined => {
+  const lastSeenByRef = lastSeenTimeByTimeValuesRef.get(newTimeValues);
+  if (lastSeenByRef) {
+    return lastSeenByRef;
+  }
+
+  const oldValues = timeValuesArray(prevFrame);
+  if (oldValues?.length) {
+    return oldValues[oldValues.length - 1];
+  }
+
+  return undefined;
+};
+
 const measureDataRenderDelayForFrame = (prevFrame: DataFrame, newFrame: DataFrame, now: number) => {
-  if (!prevFrame.length || !newFrame.length) {
+  const newValues = timeValuesArray(newFrame);
+  if (!newValues?.length) {
     return;
   }
 
-  const newFrameTimeField = newFrame.fields?.find((f) => f.type === FieldType.time);
-  const oldFrameTimeField = prevFrame.fields?.find((f) => f.type === FieldType.time);
-  if (!oldFrameTimeField || !newFrameTimeField) {
-    return;
+  const lastSeenTime = getLastSeenTime(newValues, prevFrame);
+  if (lastSeenTime) {
+    const closest = closestIdx(lastSeenTime, newValues);
+
+    const firstBiggerIndex = newValues[closest] > lastSeenTime ? closest : closest + 1;
+    if (newValues[firstBiggerIndex]) {
+      LivePerformance.instance().add(MeasurementName.DataRenderDelay, now - newValues[firstBiggerIndex]);
+    }
   }
 
-  const oldValues = oldFrameTimeField.values.toArray();
-  const newValues = newFrameTimeField.values.toArray();
-
-  const latestTimeInOldFrame = oldValues[oldValues.length - 1];
-
-  const closest = closestIdx(latestTimeInOldFrame, newValues);
-
-  const firstBiggerIndex = newValues[closest] > latestTimeInOldFrame ? closest : closest + 1;
-  if (newValues[firstBiggerIndex]) {
-    LivePerformance.instance()?.add(MeasurementName.DataRenderDelay, now - newValues[firstBiggerIndex]);
-  }
+  lastSeenTimeByTimeValuesRef.set(newValues, newValues[newValues.length - 1]);
 };
 
 export type MeasureDataRenderDelay = (frames: DataFrame[], oldFrames: DataFrame[]) => void;
