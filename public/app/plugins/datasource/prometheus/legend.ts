b96fcbb3d91c3d7fa0eaae95b4672c76eb7ffa4a
kay delaney <kay@grafana.com>
2021-12-20 15:03:04 +0000

    Simplify logic

--- public/app/plugins/datasource/prometheus/legend.ts
+++ public/app/plugins/datasource/prometheus/legend.ts
@@ -2,7 +2,9 @@ import { Labels } from '@grafana/data';
 
 /** replace labels in a string.  Used for loki+prometheus legend formats */
 export function renderLegendFormat(aliasPattern: string, aliasData: Labels): string {
-  // Negative lookbehind is to avoid https://github.com/github/codeql/blob/ddd4ccbb4b39adf3c2427088f4876432202c4eaa/javascript/ql/src/Performance/PolynomialReDoS.ql
-  const aliasRegex = /\{\{\s*(.+?)(?<!\s)\s*\}\}/g;
-  return aliasPattern.replace(aliasRegex, (_, g1) => (aliasData[g1] ? aliasData[g1] : g1));
+  const aliasRegex = /\{\{(.+?)\}\}/g;
+  return aliasPattern.replace(aliasRegex, (_, g1: string) => {
+    const trimmedMatch = g1.trim();
+    return aliasData[trimmedMatch] ?? trimmedMatch;
+  });
 }
