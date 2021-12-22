e88fbbfb8de45d61c784d3d992e168e94b39fd62
Alexandra Vargas <alexa1866@gmail.com>
2021-12-22 10:59:25 +0100

    user essentials mob! :trident:

--- e2e/repeats-suite/Repeating_a_panel_horizontally.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_horizontally.spec.ts
@@ -1,5 +1,5 @@
 import { e2e } from '@grafana/e2e';
-const PAGE_UNDER_TEST = 'OY8Ghjt7k/repeating-a-panel-vertically';
+const PAGE_UNDER_TEST = 'WVpf2jp7z/repeating-a-panel-horizontally';
 
 describe('Repeating a panel horizontally', () => {
   it('should be able to repeat a panel horizontally', () => {
@@ -7,22 +7,21 @@ describe('Repeating a panel horizontally', () => {
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
 
     let prevLeft = Number.NEGATIVE_INFINITY;
-    let prevTop = Number.NEGATIVE_INFINITY;
-    const panelTitles = [
-      'Panel Title 1',
-      'Panel Title 2',
-      'Panel Title 3',
-    ]
+    let prevTop = null;
+    const panelTitles = ['Panel Title 1', 'Panel Title 2', 'Panel Title 3'];
     panelTitles.forEach((title) => {
       e2e.components.Panels.Panel.title(title)
         .should('be.visible')
         .then(($el) => {
           const { left, top } = $el[0].getBoundingClientRect();
           expect(left).to.be.greaterThan(prevLeft);
-          expect(top).to.be.greaterThan(prevTop);
+          if (prevTop !== null) {
+            expect(top).to.be.equal(prevTop);
+          }
+
           prevLeft = left;
           prevTop = top;
         });
-    })
+    });
   });
 });
--- e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
+++ e2e/repeats-suite/Repeating_a_panel_vertically.spec.ts
@@ -7,19 +7,20 @@ describe('Repeating a panel vertically', () => {
     e2e.flows.openDashboard({ uid: PAGE_UNDER_TEST });
 
     let prevTop = Number.NEGATIVE_INFINITY;
-    const panelTitles = [
-      'Panel Title 1',
-      'Panel Title 2',
-      'Panel Title 3',
-    ]
+    let prevLeft = null;
+    const panelTitles = ['Panel Title 1', 'Panel Title 2', 'Panel Title 3'];
     panelTitles.forEach((title) => {
       e2e.components.Panels.Panel.title(title)
         .should('be.visible')
         .then(($el) => {
-          const top = $el[0].getBoundingClientRect().top;
+          const { left, top } = $el[0].getBoundingClientRect();
           expect(top).to.be.greaterThan(prevTop);
+          if (prevLeft !== null) {
+            expect(left).to.be.equal(prevLeft);
+          }
+          prevLeft = left;
           prevTop = top;
         });
-    })
+    });
   });
 });
