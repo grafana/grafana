53f9e9207e0b586de91b86c3970c6db88f61e46b
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2021-12-22 16:50:58 +0100

    Prometheus: Fix interpolation of variables in Annotation queries (#43459)
    (cherry picked from commit f63c2e4bc4a066cb84c8764a3ee99d63bfcdaef7)

--- public/app/plugins/datasource/prometheus/datasource.ts
+++ public/app/plugins/datasource/prometheus/datasource.ts
@@ -674,7 +674,7 @@ export class PrometheusDatasource extends DataSourceWithBackend<PromQuery, PromO
           data: {
             from: (this.getPrometheusTime(options.range.from, false) * 1000).toString(),
             to: (this.getPrometheusTime(options.range.to, true) * 1000).toString(),
-            queries: [queryModel],
+            queries: [this.applyTemplateVariables(queryModel, {})],
           },
           requestId: `prom-query-${annotation.name}`,
         })
