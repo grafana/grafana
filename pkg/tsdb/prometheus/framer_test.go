b33ce6b915d863a8a7805be9cb717808f1b0c271
Todd Treece <todd.treece@grafana.com>
2021-12-23 11:47:25 -0500

    add matrix range golden test

--- pkg/tsdb/prometheus/framer_test.go
+++ pkg/tsdb/prometheus/framer_test.go
@@ -5,12 +5,15 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/grafana/grafana-plugin-sdk-go/experimental"
 	"github.com/grafana/grafana/pkg/tsdb/prometheus"
 	"github.com/prometheus/common/model"
+	"github.com/stretchr/testify/require"
 )
 
-func Benchmark_matrixToDataFrames(b *testing.B) {
+func BenchmarkMatrixToDataFrames(b *testing.B) {
 	query := &prometheus.PrometheusQuery{
 		LegendFormat: "",
 	}
@@ -21,10 +24,28 @@ func Benchmark_matrixToDataFrames(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		frames := make([]*data.Frame, 0)
 		frames = prometheus.MatrixToDataFrames(m, query, frames)
-		if len(frames) != 695 {
-			b.Fatal("wrong frames length", len(frames))
+		if len(frames) != 1 {
+			b.Fatal("wrong frame count", len(frames))
 		}
 	}
+}
+
+func TestMatrixToDataFrames(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		var m model.Matrix
+		err := loadTestData("matrix_range.json", &m)
+		require.NoError(t, err)
+
+		query := &prometheus.PrometheusQuery{
+			LegendFormat: "",
+		}
+		frames := make(data.Frames, 0)
+		frames = prometheus.MatrixToDataFrames(m, query, frames)
+		res := &backend.DataResponse{Frames: frames}
+
+		err = experimental.CheckGoldenDataResponse("./testdata/matrix_range.txt", res, false)
+		require.NoError(t, err)
+	})
 }
 
 func loadTestData(path string, res interface{}) error {
