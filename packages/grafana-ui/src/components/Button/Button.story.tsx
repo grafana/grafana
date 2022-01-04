45728ed48ea278346bd295862cbae811a0f907ec
kay delaney <kay@grafana.com>
2022-01-04 10:23:48 +0000

    Fix Button.story.tsx

--- packages/grafana-ui/src/components/Button/Button.story.tsx
+++ packages/grafana-ui/src/components/Button/Button.story.tsx
@@ -5,7 +5,7 @@ import mdx from './Button.mdx';
 import { HorizontalGroup, VerticalGroup } from '../Layout/Layout';
 import { ButtonGroup } from './ButtonGroup';
 import { ComponentSize } from '../../types/size';
-import { Card } from '../NewCard/NewCard';
+import { Card } from '../Card/Card';
 
 export default {
   title: 'Buttons/Button',
