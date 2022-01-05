3d8cdac6cad860c0c803780db191ea1dc68e81a7
Ryan McKinley <ryantxu@gmail.com>
2022-01-05 14:30:30 -0800

    expose metadata to frontend

---
+++ public/app/features/admin/AdminFeatureInfo.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from 'react';
+import { FeatureFlagInfo, FeatureToggles } from '@grafana/data';
+import { getBackendSrv } from '@grafana/runtime';
+import { useAsync } from 'react-use';
+import Page from 'app/core/components/Page/Page';
+import { Card, TagList } from '@grafana/ui';
+
+interface FeatureSettings {
+  enabled: FeatureToggles;
+  info: FeatureFlagInfo[];
+  messages?: string[];
+}
+
+export const AdminFeatureInfo: FC = () => {
+  const { loading, value: features } = useAsync(
+    () => getBackendSrv().get('/api/admin/settings/features') as Promise<FeatureSettings>,
+    []
+  );
+
+  const enabled = features ? features.info.filter((f) => f.enabled) : [];
+  const avaliable = features ? features.info.filter((f) => !f.enabled) : [];
+
+  return (
+    <Page.Contents isLoading={loading}>
+      <div>
+        {enabled.length && (
+          <div>
+            <h2>Enabled feature flags:</h2>
+            {renderFlags(enabled)}
+            <br />
+          </div>
+        )}
+        {avaliable.length && (
+          <div>
+            <h2>Avaliable feature flags:</h2>
+            {renderFlags(avaliable)}
+          </div>
+        )}
+      </div>
+    </Page.Contents>
+  );
+};
+
+function renderFlags(features: FeatureFlagInfo[]) {
+  return features.map((f) => {
+    const tags: string[] = [];
+    if (f.modifiesDatabase) {
+      tags.push('Modifies database');
+    }
+    if (f.frontend) {
+      tags.push('frontend');
+    }
+    if (f.requiresEnterprise) {
+      tags.push('enterprise');
+    }
+    if (f.requiresDevMode) {
+      tags.push('dev mode');
+    }
+
+    tags.push(f.state!);
+
+    return (
+      <Card key={f.id} heading={`${f.name} (${f.id})`} description={f.description}>
+        <Card.Tags>
+          <TagList tags={tags} />
+        </Card.Tags>
+      </Card>
+    );
+  });
+}
