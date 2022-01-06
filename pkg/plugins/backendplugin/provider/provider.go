e114c2bc3073805e2886c1e6e8c0fae2c4dcfc53
Will Browne <will.browne@grafana.com>
2022-01-06 15:36:56 +0000

    fix

---
+++ pkg/plugins/backendplugin/provider/provider.go
@@ -0,0 +1,62 @@
+package provider
+
+import (
+	"context"
+	"path/filepath"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/plugins"
+	"github.com/grafana/grafana/pkg/plugins/backendplugin"
+	"github.com/grafana/grafana/pkg/plugins/backendplugin/grpcplugin"
+	"github.com/grafana/grafana/pkg/plugins/backendplugin/pluginextensionv2"
+)
+
+type Service struct {
+	coreRegistry *CoreRegistry
+}
+
+func ProvideService(coreRegistry *CoreRegistry) *Service {
+	return &Service{
+		coreRegistry: coreRegistry,
+	}
+}
+
+func (s *Service) BackendFactory(ctx context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+	for _, provider := range []PluginBackendProvider{CorePluginProvider(ctx, s), RendererProvider, DefaultProvider} {
+		if factory := provider(ctx, p); factory != nil {
+			return factory
+		}
+	}
+	return nil
+}
+
+// PluginBackendProvider is a function type for initializing a Plugin backend.
+type PluginBackendProvider func(_ context.Context, _ *plugins.Plugin) backendplugin.PluginFactoryFunc
+
+var RendererProvider PluginBackendProvider = func(_ context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+	if !p.IsRenderer() {
+		return nil
+	}
+	cmd := plugins.ComposeRendererStartCommand()
+	return grpcplugin.NewRendererPlugin(p.ID, filepath.Join(p.PluginDir, cmd),
+		func(pluginID string, renderer pluginextensionv2.RendererPlugin, logger log.Logger) error {
+			p.Renderer = renderer
+			return nil
+		},
+	)
+}
+
+var DefaultProvider PluginBackendProvider = func(_ context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+	cmd := plugins.ComposePluginStartCommand(p.Executable)
+	return grpcplugin.NewBackendPlugin(p.ID, filepath.Join(p.PluginDir, cmd))
+}
+
+var CorePluginProvider = func(ctx context.Context, s *Service) PluginBackendProvider {
+	return func(_ context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+		if !p.IsCorePlugin() {
+			return nil
+		}
+
+		return s.coreRegistry.Get(p.ID)
+	}
+}
