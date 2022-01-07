0e72f8bfe276f2aba44e2397eda57a74cfaecd8d
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2022-01-07 08:59:36 +0100

    chore(pluginSettings): make the tests a bit more readable

--- public/app/features/plugins/pluginSettings.test.ts
+++ public/app/features/plugins/pluginSettings.test.ts
@@ -14,93 +14,73 @@ describe('PluginSettings', () => {
   });
 
   it('should fetch settings when cache is empty', async () => {
-    // assign
-    getBackendSrv().get = jest.fn().mockResolvedValue({
+    // arrange
+    const testPluginResponse = {
       name: 'TestPlugin',
       type: 'datasource',
       id: 'test-plugin',
       enabled: true,
-    });
+    };
+    getBackendSrv().get = jest.fn().mockResolvedValue(testPluginResponse);
     const getRequestSpy = jest.spyOn(getBackendSrv(), 'get');
     // act
     const response = await getPluginSettings('test');
     // assert
-    expect(response).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
+    expect(response).toEqual(testPluginResponse);
     expect(getRequestSpy).toHaveBeenCalledTimes(1);
     expect(getRequestSpy).toHaveBeenCalledWith('/api/plugins/test/settings');
   });
 
   it('should fetch settings from cache when it has a hit', async () => {
-    // assign
-    getBackendSrv().get = jest.fn().mockResolvedValue({
+    // arrange
+    const testPluginResponse = {
       name: 'TestPlugin',
       type: 'datasource',
       id: 'test-plugin',
       enabled: true,
-    });
+    };
+    getBackendSrv().get = jest.fn().mockResolvedValue(testPluginResponse);
     const getRequestSpy = jest.spyOn(getBackendSrv(), 'get');
     // act
     const response1 = await getPluginSettings('test');
     const response2 = await getPluginSettings('test');
 
     // assert
-    expect(response1).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
-    expect(response2).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
+    expect(response1).toEqual(testPluginResponse);
+    expect(response2).toEqual(testPluginResponse);
     expect(getRequestSpy).toHaveBeenCalledTimes(1);
   });
 
   it('should refetch from backend when cache is cleared', async () => {
-    // assign
-    getBackendSrv().get = jest.fn().mockResolvedValue({
+    // arrange
+    const testPluginResponse = {
       name: 'TestPlugin',
       type: 'datasource',
       id: 'test-plugin',
       enabled: true,
-    });
+    };
+    getBackendSrv().get = jest.fn().mockResolvedValue(testPluginResponse);
     const getRequestSpy = jest.spyOn(getBackendSrv(), 'get');
+
     // act
     const response1 = await getPluginSettings('test');
     await clearPluginSettingsCache('test');
     const response2 = await getPluginSettings('test');
 
     // assert
-    expect(response1).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
-    expect(response2).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
+    expect(response1).toEqual(testPluginResponse);
+    expect(response2).toEqual(testPluginResponse);
     expect(getRequestSpy).toHaveBeenCalledTimes(2);
   });
   it('should fetch from cache when it is cleared for another plugin setting', async () => {
-    // assign
-    getBackendSrv().get = jest.fn().mockResolvedValue({
+    // arrange
+    const testPluginResponse = {
       name: 'TestPlugin',
       type: 'datasource',
       id: 'test-plugin',
       enabled: true,
-    });
+    };
+    getBackendSrv().get = jest.fn().mockResolvedValue(testPluginResponse);
     const getRequestSpy = jest.spyOn(getBackendSrv(), 'get');
     // act
     const response1 = await getPluginSettings('test');
@@ -108,47 +88,29 @@ describe('PluginSettings', () => {
     const response2 = await getPluginSettings('test');
 
     // assert
-    expect(response1).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
-    expect(response2).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
+    expect(response1).toEqual(testPluginResponse);
+    expect(response2).toEqual(testPluginResponse);
     expect(getRequestSpy).toHaveBeenCalledTimes(1);
   });
   it('should clear all cache when no plugin id is provided to the clear function', async () => {
-    // assign
-    getBackendSrv().get = jest.fn().mockResolvedValue({
+    // arrange
+    const testPluginResponse = {
       name: 'TestPlugin',
       type: 'datasource',
       id: 'test-plugin',
       enabled: true,
-    });
+    };
+    getBackendSrv().get = jest.fn().mockResolvedValue(testPluginResponse);
     const getRequestSpy = jest.spyOn(getBackendSrv(), 'get');
+
     // act
     const response1 = await getPluginSettings('test');
     await clearPluginSettingsCache();
     const response2 = await getPluginSettings('test');
 
     // assert
-    expect(response1).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
-    expect(response2).toEqual({
-      name: 'TestPlugin',
-      type: 'datasource',
-      id: 'test-plugin',
-      enabled: true,
-    });
+    expect(response1).toEqual(testPluginResponse);
+    expect(response2).toEqual(testPluginResponse);
     expect(getRequestSpy).toHaveBeenCalledTimes(2);
   });
 });
