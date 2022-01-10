fb0847254ad2772f9570af1c9a8f618b41943397
Karl Persson <kalle.persson@grafana.com>
2022-01-10 15:44:21 +0100

    add tests for GetOrgUsers

--- pkg/services/sqlstore/org_users_test.go
+++ pkg/services/sqlstore/org_users_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/models"
@@ -30,12 +31,39 @@ func TestSQLStore_GetOrgUsers(t *testing.T) {
 			},
 			expectedNumUsers: 10,
 		},
+		{
+			desc: "should return no users",
+			query: &models.GetOrgUsersQuery{
+				OrgId: 1,
+				User: &models.SignedInUser{
+					OrgId:       1,
+					Permissions: map[int64]map[string][]string{1: {"org.users:read": {""}}},
+				},
+			},
+			expectedNumUsers: 0,
+		},
+		{
+			desc: "should return some users",
+			query: &models.GetOrgUsersQuery{
+				OrgId: 1,
+				User: &models.SignedInUser{
+					OrgId: 1,
+					Permissions: map[int64]map[string][]string{1: {"org.users:read": {
+						"users:id:1",
+						"users:id:5",
+						"users:id:9",
+					}}},
+				},
+			},
+			expectedNumUsers: 3,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
 			store := InitTestDB(t)
 			store.Cfg.FeatureToggles = map[string]bool{"accesscontrol": true}
+
 			// Seed users
 			for i := 1; i <= 10; i++ {
 				user, err := store.CreateUser(context.Background(), models.CreateUserCommand{
@@ -43,6 +71,7 @@ func TestSQLStore_GetOrgUsers(t *testing.T) {
 					OrgId: 1,
 				})
 				require.NoError(t, err)
+
 				if i != 1 {
 					err = store.AddOrgUser(context.Background(), &models.AddOrgUserCommand{
 						Role:   "Viewer",
@@ -55,9 +84,20 @@ func TestSQLStore_GetOrgUsers(t *testing.T) {
 
 			err := store.GetOrgUsers(context.Background(), tt.query)
 			require.NoError(t, err)
-
-			// Query
 			require.Len(t, tt.query.Result, tt.expectedNumUsers)
+
+			hasWildcard := false
+			for _, scope := range tt.query.User.Permissions[tt.query.User.OrgId]["org.users:read"] {
+				if scope == "*" || scope == "users:*" || scope == "users:id:*" {
+					hasWildcard = true
+				}
+			}
+
+			if !hasWildcard {
+				for _, u := range tt.query.Result {
+					assert.Contains(t, tt.query.User.Permissions[tt.query.User.OrgId]["org.users:read"], fmt.Sprintf("users:id:%d", u.UserId))
+				}
+			}
 		})
 	}
 }
