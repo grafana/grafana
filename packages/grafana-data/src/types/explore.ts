203f31ec830973600ad1c8e81013b707605fba7a
Erin Noe-Payne <erin.noe.payne@gmail.com>
2022-01-13 16:25:58 -0500

    Implements deep linking to spans

--- packages/grafana-data/src/types/explore.ts
+++ packages/grafana-data/src/types/explore.ts
@@ -1,3 +1,4 @@
+import { PreferredVisualisationType } from './data';
 import { DataQuery } from './query';
 import { RawTimeRange, TimeRange } from './time';
 
@@ -10,11 +11,20 @@ export interface ExploreUrlState<T extends DataQuery = AnyQuery> {
   range: RawTimeRange;
   originPanelId?: number;
   context?: string;
+  panelsState?: ExplorePanelsState;
+}
+
+export interface ExplorePanelsState extends Partial<Record<PreferredVisualisationType, {}>> {
+  trace?: ExploreTracePanelState;
+}
+
+export interface ExploreTracePanelState {
+  spanId?: string;
 }
 
 /**
  * SplitOpen type is used in Explore and related components.
  */
 export type SplitOpen = <T extends DataQuery = any>(
-  options?: { datasourceUid: string; query: T; range?: TimeRange } | undefined
+  options?: { datasourceUid: string; query: T; range?: TimeRange; panelsState?: ExplorePanelsState } | undefined
 ) => void;
