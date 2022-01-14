26582b1771ed85c828b8df969462faa2e65c89a7
Fabio Silva <ffjs1993@gmail.com>
2022-01-14 11:52:00 +0000

    PMM-7509: alerts pagination (#141)
    * PMM-7509 getPageSize: return default as number
    
    * PMM-7509 add pagination to Alerts
    
    * PMM-7509 fix alerts tests

--- public/app/percona/integrated-alerting/components/Alerts/Alerts.constants.ts
+++ public/app/percona/integrated-alerting/components/Alerts/Alerts.constants.ts
@@ -1,1 +1,2 @@
 export const GET_ALERTS_CANCEL_TOKEN = 'getAlerts';
+export const ALERT_RULE_TEMPLATES_TABLE_ID = 'alerts';
--- public/app/percona/integrated-alerting/components/Alerts/Alerts.tsx
+++ public/app/percona/integrated-alerting/components/Alerts/Alerts.tsx
@@ -8,13 +8,14 @@ import { Cell, Column } from 'react-table';
 import { cx } from 'emotion';
 import { Table } from '../Table/Table';
 import { Severity } from '../Severity';
+import { useStoredTablePageSize } from '../Table/Pagination';
 import { Messages } from '../../IntegratedAlerting.messages';
 import { getStyles } from './Alerts.styles';
 import { Alert, AlertStatus } from './Alerts.types';
 import { formatAlerts } from './Alerts.utils';
 import { AlertsService } from './Alerts.service';
 import { AlertsActions } from './AlertsActions';
-import { GET_ALERTS_CANCEL_TOKEN } from './Alerts.constants';
+import { ALERT_RULE_TEMPLATES_TABLE_ID, GET_ALERTS_CANCEL_TOKEN } from './Alerts.constants';
 
 const { noData, columns } = Messages.alerts.table;
 const {
@@ -32,13 +33,19 @@ export const Alerts: FC = () => {
   const theme = useTheme();
   const [pendingRequest, setPendingRequest] = useState(true);
   const [data, setData] = useState<Alert[]>([]);
+  const [pageSize, setPageSize] = useStoredTablePageSize(ALERT_RULE_TEMPLATES_TABLE_ID);
+  const [pageIndex, setPageindex] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [generateToken] = useCancelToken();
 
   const getAlerts = useCallback(async () => {
     setPendingRequest(true);
     try {
-      const { alerts } = await AlertsService.list(generateToken(GET_ALERTS_CANCEL_TOKEN));
+      const { alerts, totals } = await AlertsService.list(pageSize, pageIndex, generateToken(GET_ALERTS_CANCEL_TOKEN));
       setData(formatAlerts(alerts));
+      setTotalItems(totals.total_items || 0);
+      setTotalPages(totals.total_pages || 0);
     } catch (e) {
       if (isApiCancelError(e)) {
         return;
@@ -46,7 +53,7 @@ export const Alerts: FC = () => {
       logger.error(e);
     }
     setPendingRequest(false);
-  }, [generateToken]);
+  }, [generateToken, pageSize, pageIndex]);
 
   const columns = React.useMemo(
     () => [
@@ -110,13 +117,26 @@ export const Alerts: FC = () => {
     [style.disabledRow]
   );
 
+  const handlePaginationChanged = useCallback(
+    (pageSize: number, pageIndex: number) => {
+      setPageSize(pageSize);
+      setPageindex(pageIndex);
+    },
+    [setPageSize]
+  );
+
   useEffect(() => {
     getAlerts();
   }, [getAlerts]);
 
   return (
     <Table
-      totalItems={data.length}
+      showPagination
+      totalItems={totalItems}
+      totalPages={totalPages}
+      pageSize={pageSize}
+      pageIndex={pageIndex}
+      onPaginationChanged={handlePaginationChanged}
       data={data}
       columns={columns}
       pendingRequest={pendingRequest}
