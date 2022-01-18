c9841ac5a76f0e562a494526c156e24564b18810
Artur Wierzbicki <artur@arturwierzbicki.com>
2022-01-18 21:54:57 +0400

    review fix: extract renderKey related functions/structs to auth.go

--- pkg/services/rendering/rendering.go
+++ pkg/services/rendering/rendering.go
@@ -27,13 +27,6 @@ func init() {
 }
 
 const ServiceName = "RenderingService"
-const renderKeyPrefix = "render-%s"
-
-type RenderUser struct {
-	OrgID   int64
-	UserID  int64
-	OrgRole string
-}
 
 type RenderingService struct {
 	log             log.Logger
@@ -296,21 +289,6 @@ func (rs *RenderingService) renderCSV(ctx context.Context, opts CSVOpts, renderK
 	return rs.renderCSVAction(ctx, renderKey, opts)
 }
 
-func (rs *RenderingService) GetRenderUser(ctx context.Context, key string) (*RenderUser, bool) {
-	val, err := rs.RemoteCacheService.Get(ctx, fmt.Sprintf(renderKeyPrefix, key))
-	if err != nil {
-		rs.log.Error("Failed to get render key from cache", "error", err)
-	}
-
-	if val != nil {
-		if user, ok := val.(*RenderUser); ok {
-			return user, true
-		}
-	}
-
-	return nil, false
-}
-
 func (rs *RenderingService) getNewFilePath(rt RenderType) (string, error) {
 	rand, err := util.GetRandomString(20)
 	if err != nil {
@@ -354,92 +332,6 @@ func (rs *RenderingService) getURL(path string) string {
 
 	// &render=1 signals to the legacy redirect layer to
 	return fmt.Sprintf("%s://%s:%s%s/%s&render=1", protocol, rs.domain, rs.Cfg.HTTPPort, subPath, path)
-}
-
-func (rs *RenderingService) CreateRenderingSession(ctx context.Context, opts AuthOpts, sessionOpts SessionOpts) (Session, error) {
-	renderKey, err := generateAndSetRenderKey(rs.RemoteCacheService, ctx, opts, sessionOpts.Expiry)
-	if err != nil {
-		return nil, err
-	}
-
-	return &longLivedRenderKeyProvider{
-		log:         rs.log,
-		renderKey:   renderKey,
-		cache:       rs.RemoteCacheService,
-		authOpts:    opts,
-		sessionOpts: sessionOpts,
-	}, nil
-}
-
-func setRenderKey(cache *remotecache.RemoteCache, ctx context.Context, opts AuthOpts, renderKey string, expiry time.Duration) error {
-	err := cache.Set(ctx, fmt.Sprintf(renderKeyPrefix, renderKey), &RenderUser{
-		OrgID:   opts.OrgID,
-		UserID:  opts.UserID,
-		OrgRole: string(opts.OrgRole),
-	}, expiry)
-	return err
-}
-
-func generateAndSetRenderKey(cache *remotecache.RemoteCache, ctx context.Context, opts AuthOpts, expiry time.Duration) (string, error) {
-	key, err := util.GetRandomString(32)
-	if err != nil {
-		return "", err
-	}
-
-	err = setRenderKey(cache, ctx, opts, key, expiry)
-	if err != nil {
-		return "", err
-	}
-
-	return key, err
-}
-
-func deleteRenderKey(cache *remotecache.RemoteCache, log log.Logger, ctx context.Context, renderKey string) {
-	err := cache.Delete(ctx, fmt.Sprintf(renderKeyPrefix, renderKey))
-	if err != nil {
-		log.Error("Failed to delete render key", "error", err)
-	}
-}
-
-type perRequestRenderKeyProvider struct {
-	cache     *remotecache.RemoteCache
-	log       log.Logger
-	keyExpiry time.Duration
-}
-
-func (r *perRequestRenderKeyProvider) get(ctx context.Context, opts AuthOpts) (string, error) {
-	return generateAndSetRenderKey(r.cache, ctx, opts, r.keyExpiry)
-}
-
-func (r *perRequestRenderKeyProvider) afterRequest(ctx context.Context, opts AuthOpts, renderKey string) {
-	deleteRenderKey(r.cache, r.log, ctx, renderKey)
-}
-
-type longLivedRenderKeyProvider struct {
-	cache       *remotecache.RemoteCache
-	log         log.Logger
-	renderKey   string
-	authOpts    AuthOpts
-	sessionOpts SessionOpts
-}
-
-func (r *longLivedRenderKeyProvider) get(ctx context.Context, opts AuthOpts) (string, error) {
-	if r.sessionOpts.RefreshExpiryOnEachRequest {
-		err := setRenderKey(r.cache, ctx, opts, r.renderKey, r.sessionOpts.Expiry)
-		if err != nil {
-			r.log.Error("Failed to refresh render key", "error", err, "renderKey", r.renderKey)
-		}
-	}
-	return r.renderKey, nil
-}
-
-func (r *longLivedRenderKeyProvider) afterRequest(ctx context.Context, opts AuthOpts, renderKey string) {
-	// do nothing - renderKey from longLivedRenderKeyProvider is deleted only after session expires
-	// or someone calls session.Dispose()
-}
-
-func (r *longLivedRenderKeyProvider) Dispose(ctx context.Context) {
-	deleteRenderKey(r.cache, r.log, ctx, r.renderKey)
 }
 
 func isoTimeOffsetToPosixTz(isoOffset string) string {
