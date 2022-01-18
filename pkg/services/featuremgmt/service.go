33d03ddf80dc33eb3cf4f243ce05d3ec9c18290c
Ryan McKinley <ryantxu@gmail.com>
2022-01-17 21:02:00 -0800

    Update pkg/services/featuremgmt/service.go
    Co-authored-by: Carl Bergquist <carl.bergquist@gmail.com>

--- pkg/services/featuremgmt/service.go
+++ pkg/services/featuremgmt/service.go
@@ -52,7 +52,7 @@ func ProvideManagerService(cfg *setting.Cfg, licensing models.Licensing) (*Featu
 	}
 
 	// Load config settings
-	configfile := filepath.Join(cfg.HomePath, "config", "features.yaml")
+	configfile := filepath.Join(cfg.HomePath, "conf", "features.yaml")
 	if _, err := os.Stat(configfile); err == nil {
 		mgmt.config = configfile
 		err = mgmt.readFile()
