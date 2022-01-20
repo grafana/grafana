7fbc7d019a83bb32e4e355f719c9181827a421ce
Will Browne <wbrowne@users.noreply.github.com>
2022-01-20 18:16:22 +0100

    Plugins: Core plugins register via backend factory provider (#43171)
    * refactoring store interface and init flow
    
    * fix import
    
    * fix linter
    
    * refactor resource calling
    
    * load with class
    
    * re-order args
    
    * fix tests
    
    * fix linter
    
    * remove old creator
    
    * add custom config struct
    
    * fix some tests
    
    * cleanup
    
    * fix
    
    * tackle plugins
    
    * fix linter
    
    * refactor and fix test
    
    * add connect failure error
    
    * add fix for azure, cloud monitoring and test data
    
    * restructure
    
    * remove unused err
    
    * add fake tracer for test
    
    * fix grafana ds plugin

--- pkg/plugins/backendplugin/provider/provider.go
+++ pkg/plugins/backendplugin/provider/provider.go
@@ -7,18 +7,23 @@ import (
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/plugins/backendplugin"
+	"github.com/grafana/grafana/pkg/plugins/backendplugin/coreplugin"
 	"github.com/grafana/grafana/pkg/plugins/backendplugin/grpcplugin"
 	"github.com/grafana/grafana/pkg/plugins/backendplugin/pluginextensionv2"
 )
 
-type Service struct{}
-
-func ProvideService() *Service {
-	return &Service{}
+type Service struct {
+	coreRegistry *coreplugin.Registry
 }
 
-func (*Service) BackendFactory(ctx context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
-	for _, provider := range []PluginBackendProvider{RendererProvider, DefaultProvider} {
+func ProvideService(coreRegistry *coreplugin.Registry) *Service {
+	return &Service{
+		coreRegistry: coreRegistry,
+	}
+}
+
+func (s *Service) BackendFactory(ctx context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+	for _, provider := range []PluginBackendProvider{CorePluginProvider(ctx, s.coreRegistry), RendererProvider, DefaultProvider} {
 		if factory := provider(ctx, p); factory != nil {
 			return factory
 		}
@@ -43,6 +48,17 @@ var RendererProvider PluginBackendProvider = func(_ context.Context, p *plugins.
 }
 
 var DefaultProvider PluginBackendProvider = func(_ context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+	// TODO check for executable
 	cmd := plugins.ComposePluginStartCommand(p.Executable)
 	return grpcplugin.NewBackendPlugin(p.ID, filepath.Join(p.PluginDir, cmd))
+}
+
+var CorePluginProvider = func(ctx context.Context, registry *coreplugin.Registry) PluginBackendProvider {
+	return func(_ context.Context, p *plugins.Plugin) backendplugin.PluginFactoryFunc {
+		if !p.IsCorePlugin() {
+			return nil
+		}
+
+		return registry.Get(p.ID)
+	}
 }
