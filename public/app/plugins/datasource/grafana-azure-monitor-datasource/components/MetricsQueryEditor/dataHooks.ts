05fe5f6d8b3580bf57d11618f12b8024ad66ea9d
Andres Martinez Gotor <andres.martinez@grafana.com>
2022-01-21 16:50:13 +0100

    AzureMonitor: Allow template variables in the subscription field (#44312)
    (cherry picked from commit addeba7f0514b715d31dcb01752f073a416eb15c)

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/MetricsQueryEditor/dataHooks.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/MetricsQueryEditor/dataHooks.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+
 import Datasource from '../../datasource';
 import { AzureMonitorErrorish, AzureMonitorOption, AzureMonitorQuery } from '../../types';
 import { hasOption, toOption } from '../../utils/common';
@@ -57,7 +58,10 @@ export const updateSubscriptions = (
   const { subscription } = query;
 
   // Return early if subscriptions havent loaded, or if the query already has a subscription
-  if (!subscriptionOptions.length || (subscription && hasOption(subscriptionOptions, subscription))) {
+  if (
+    !subscriptionOptions.length ||
+    (subscription && (hasOption(subscriptionOptions, subscription) || subscription.includes('$')))
+  ) {
     return;
   }
 
