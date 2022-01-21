c6e853a13ca97b3d0d0af47860f49a99d9b196ab
Nathan Marrs <nathanielmarrs@gmail.com>
2022-01-20 20:36:34 -0800

    Refactor: Move Value Mappings to Grafana public instead of Grafana UI (#44237)

--- public/app/core/components/editors/registry.tsx
+++ public/app/core/components/editors/registry.tsx
@@ -1,6 +1,15 @@
 import { DashboardPicker, DashboardPickerOptions } from './DashboardPicker';
-import { getStandardOptionEditors } from '@grafana/ui';
-import { StandardEditorsRegistryItem } from '@grafana/data';
+import { getStandardFieldConfigs, getStandardOptionEditors } from '@grafana/ui';
+import {
+  FieldConfigPropertyItem,
+  FieldType,
+  standardEditorsRegistry,
+  StandardEditorsRegistryItem,
+  ValueMapping,
+  ValueMappingFieldConfigSettings,
+  valueMappingsOverrideProcessor,
+} from '@grafana/data';
+import { ValueMappingsValueEditor } from 'app/features/dimensions/editors/ValueMappingsEditor/mappings';
 
 /**
  * Returns collection of standard option editors definitions
@@ -12,5 +21,36 @@ export const getAllOptionEditors = () => {
     description: 'Select dashboard',
     editor: DashboardPicker as any,
   };
-  return [...getStandardOptionEditors(), dashboardPicker];
+
+  const mappings: StandardEditorsRegistryItem<ValueMapping[]> = {
+    id: 'mappings',
+    name: 'Mappings',
+    description: 'Allows defining value mappings',
+    editor: ValueMappingsValueEditor as any,
+  };
+
+  return [...getStandardOptionEditors(), dashboardPicker, mappings];
+};
+
+/**
+ * Returns collection of common field config properties definitions
+ */
+export const getAllStandardFieldConfigs = () => {
+  const mappings: FieldConfigPropertyItem<any, ValueMapping[], ValueMappingFieldConfigSettings> = {
+    id: 'mappings',
+    path: 'mappings',
+    name: 'Value mappings',
+    description: 'Modify the display text based on input value',
+
+    editor: standardEditorsRegistry.get('mappings').editor as any,
+    override: standardEditorsRegistry.get('mappings').editor as any,
+    process: valueMappingsOverrideProcessor,
+    settings: {},
+    defaultValue: [],
+    shouldApply: (x) => x.type !== FieldType.time,
+    category: ['Value mappings'],
+    getItemsCount: (value?) => (value ? value.length : 0),
+  };
+
+  return [...getStandardFieldConfigs(), mappings];
 };
