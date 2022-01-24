c2d9df54377b3a0051180592856995fa33918d41
Kevin Minehart <kmineh0151@gmail.com>
2022-01-24 10:06:09 -0600

    resolve conflicts
    (cherry picked from commit bb38cfcba4b4f824060ff385d858c63f50b72d74)

--- pkg/models/team.go
+++ pkg/models/team.go
@@ -55,7 +55,11 @@ type GetTeamByIdQuery struct {
 	SignedInUser *SignedInUser
 	HiddenUsers  map[string]struct{}
 	Result       *TeamDTO
+	UserIdFilter int64
 }
+
+// FilterIgnoreUser is used in a get / search teams query when the caller does not want to filter teams by user ID / membership
+const FilterIgnoreUser int64 = 0
 
 type GetTeamsByUserQuery struct {
 	OrgId  int64
