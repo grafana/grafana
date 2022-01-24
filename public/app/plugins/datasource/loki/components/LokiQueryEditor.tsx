62aa73ef8eeac5e9536567344b44e4f894f77d7e
Jack Baldry <jack.baldry@grafana.com>
2022-01-24 10:37:16 +0100

    style: Fix linting
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- public/app/plugins/datasource/loki/components/LokiQueryEditor.tsx
+++ public/app/plugins/datasource/loki/components/LokiQueryEditor.tsx
@@ -51,7 +51,7 @@ export function LokiQueryEditor(props: LokiQueryEditorProps) {
       ExtraFieldElement={
         <>
           <LokiOptionFields
-            queryDirection={ query.direction ?? datasource.direction ?? 'BACKWARD' }
+            queryDirection={query.direction ?? datasource.direction ?? 'BACKWARD'}
             lineLimitValue={query?.maxLines?.toString() || ''}
             resolution={query?.resolution || 1}
             query={query}
