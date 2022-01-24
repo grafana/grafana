b88811e2afcc3a3b64b35fac62b212e13b1265f6
Ryan McKinley <ryantxu@gmail.com>
2022-01-24 08:27:35 -0800

    DataFrame: deprecate/remove field.parse(any) (#44299)

--- public/app/core/components/TransformersUI/spatial/spatialTransformer.ts
+++ public/app/core/components/TransformersUI/spatial/spatialTransformer.ts
@@ -53,7 +53,6 @@ async function doSetGeometry(frames: DataFrame[], options: SpatialTransformOptio
                 {
                   ...info.field,
                   name,
-                  parse: undefined,
                   type: FieldType.geo,
                   values: new ArrayVector([toLineString(info.field)]),
                 },
