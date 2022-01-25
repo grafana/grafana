62c46f2e0c13cdae8d4fdcc9b34dc3c867a4a66d
Sarah Zinger <sarah.zinger@grafana.com>
2022-01-25 12:16:10 -0500

    Change QueryEditor to auto-resize and look more like the one in Prometheus.

--- public/app/plugins/datasource/cloudwatch/components/MathExpressionQueryField.tsx
+++ public/app/plugins/datasource/cloudwatch/components/MathExpressionQueryField.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { CodeEditor, Monaco } from '@grafana/ui';
 import language from '../metric-math/definition';
 import { registerLanguage } from '../monarch/register';
@@ -19,6 +19,7 @@ export function MathExpressionQueryField({
   onRunQuery,
   datasource,
 }: React.PropsWithChildren<Props>) {
+  const containerRef = useRef<HTMLDivElement>(null);
   const onEditorMount = useCallback(
     (editor: monacoType.editor.IStandaloneCodeEditor, monaco: Monaco) => {
       editor.onDidFocusEditorText(() => editor.trigger(TRIGGER_SUGGEST.id, TRIGGER_SUGGEST.id, {}));
@@ -27,26 +28,57 @@ export function MathExpressionQueryField({
         onChange(text);
         onRunQuery();
       });
+
+      // auto resizes the editor to be the height of the content it holds
+      // this code comes from the Prometheus query editor.
+      // We may wish to consider abstracting it into the grafana/ui repo in the future
+      const updateElementHeight = () => {
+        const containerDiv = containerRef.current;
+        if (containerDiv !== null && editor.getContentHeight() < 200) {
+          const pixelHeight = editor.getContentHeight();
+          containerDiv.style.height = `${pixelHeight}px`;
+          containerDiv.style.width = '100%';
+          const pixelWidth = containerDiv.clientWidth;
+          editor.layout({ width: pixelWidth, height: pixelHeight });
+        }
+      };
+
+      editor.onDidContentSizeChange(updateElementHeight);
+      updateElementHeight();
     },
     [onChange, onRunQuery]
   );
 
   return (
-    <CodeEditor
-      height={'150px'}
-      language={language.id}
-      value={query}
-      onBlur={(value) => {
-        if (value !== query) {
-          onChange(value);
+    <div ref={containerRef}>
+      <CodeEditor
+        monacoOptions={{
+          // without this setting, the auto-resize functionality causes an infinite loop, don't remove it!
+          scrollBeyondLastLine: false,
+
+          // These additional options are style focused and are a subset of those in the query editor in Prometheus
+          fontSize: 14,
+          lineNumbers: 'off',
+          renderLineHighlight: 'none',
+          scrollbar: {
+            vertical: 'hidden',
+            horizontal: 'hidden',
+          },
+          suggestFontSize: 12,
+          wordWrap: 'on',
+        }}
+        language={language.id}
+        value={query}
+        onBlur={(value) => {
+          if (value !== query) {
+            onChange(value);
+          }
+        }}
+        onBeforeEditorMount={(monaco: Monaco) =>
+          registerLanguage(monaco, language, datasource.metricMathCompletionItemProvider)
         }
-      }}
-      showMiniMap={false}
-      showLineNumbers={true}
-      onBeforeEditorMount={(monaco: Monaco) =>
-        registerLanguage(monaco, language, datasource.metricMathCompletionItemProvider)
-      }
-      onEditorDidMount={onEditorMount}
-    />
+        onEditorDidMount={onEditorMount}
+      />
+    </div>
   );
 }
