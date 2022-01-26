55bf21f1794be072d4351d35f9746182e9e4c439
gillesdemey <gilles.de.mey@gmail.com>
2022-01-26 15:29:51 +0100

    refactor instances [skip ci]

--- public/app/plugins/panel/alertlist/AlertInstances.tsx
+++ public/app/plugins/panel/alertlist/AlertInstances.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import pluralize from 'pluralize';
 import { Icon, useStyles2 } from '@grafana/ui';
 import { Alert } from 'app/types/unified-alerting';
@@ -18,6 +18,7 @@ export const AlertInstances = ({ alerts, options }: Props) => {
   // when custom grouping is enabled, we will always uncollapse the list of alert instances
   const defaultShowInstances = options.groupMode === GroupMode.Custom ? true : options.showInstances;
   const [displayInstances, setDisplayInstances] = useState<boolean>(defaultShowInstances);
+  const styles = useStyles2(getStyles);
 
   const toggleDisplayInstances = useCallback(() => {
     setDisplayInstances((display) => !display);
@@ -31,49 +32,18 @@ export const AlertInstances = ({ alerts, options }: Props) => {
   return (
     <div>
       {options.groupMode === GroupMode.Default && (
-        <UngroupedListHeader
-          alertCount={filteredAlerts.length}
-          displayInstances={displayInstances}
-          toggleDisplayInstances={toggleDisplayInstances}
-        />
+        <div className={styles.instance} onClick={() => toggleDisplayInstances()}>
+          <Icon name={displayInstances ? 'angle-down' : 'angle-right'} size={'md'} />
+          <span>{`${filteredAlerts.length} ${pluralize('instance', filteredAlerts.length)}`}</span>
+        </div>
       )}
-      {options.groupMode === GroupMode.Custom && <GroupedListHeader />}
       {displayInstances && <AlertInstancesTable instances={filteredAlerts} />}
     </div>
   );
 };
 
-interface UngroupedListHeaderProps {
-  alertCount: number;
-  displayInstances: boolean;
-  toggleDisplayInstances: () => void;
-}
-
-const UngroupedListHeader: FC<UngroupedListHeaderProps> = ({
-  alertCount,
-  displayInstances,
-  toggleDisplayInstances,
-}) => {
-  const styles = useStyles2(getStyles);
-
-  return (
-    <div className={styles.instance} onClick={() => toggleDisplayInstances()}>
-      <Icon name={displayInstances ? 'angle-down' : 'angle-right'} size={'md'} />
-      <span>{`${alertCount} ${pluralize('instance', alertCount)}`}</span>
-    </div>
-  );
-};
-
-const GroupedListHeader: FC<{}> = ({ children }) => {
-  const styles = useStyles2(getStyles);
-  return <div className={styles.ungroupedHeader}>{children}</div>;
-};
-
-const getStyles = (theme: GrafanaTheme2) => ({
+const getStyles = (_: GrafanaTheme2) => ({
   instance: css`
     cursor: pointer;
-  `,
-  ungroupedHeader: css`
-    margin-bottom: ${theme.spacing(1)};
   `,
 });
