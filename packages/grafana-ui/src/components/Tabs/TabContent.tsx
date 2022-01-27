1f953c77e304627220bb9da7d74069e7e102ac8b
Fabio Silva <ffjs1993@gmail.com>
2022-01-27 11:53:01 +0000

    fix background color

--- packages/grafana-ui/src/components/Tabs/TabContent.tsx
+++ packages/grafana-ui/src/components/Tabs/TabContent.tsx
@@ -1,5 +1,5 @@
 import React, { FC, HTMLAttributes, ReactNode } from 'react';
-import { stylesFactory, useTheme2 } from '../../themes';
+import { useStyles2 } from '../../themes';
 import { css, cx } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 
@@ -7,17 +7,14 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
 
-const getTabContentStyle = stylesFactory((theme: GrafanaTheme2) => {
-  return {
-    tabContent: css`
-      background: ${theme.colors.background.primary};
-    `,
-  };
+const getTabContentStyle = (theme: GrafanaTheme2) => ({
+  tabContent: css`
+    background: ${theme.colors.background.canvas};
+  `,
 });
 
 export const TabContent: FC<Props> = ({ children, className, ...restProps }) => {
-  const theme = useTheme2();
-  const styles = getTabContentStyle(theme);
+  const styles = useStyles2(getTabContentStyle);
 
   return (
     <div {...restProps} className={cx(styles.tabContent, className)}>
