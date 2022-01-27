ee394a81b78bfcb6468a9df8d378d79ebbd581c8
Fabio Silva <ffjs1993@gmail.com>
2022-01-27 22:50:30 +0000

    fix navbar hover styles

--- public/app/core/components/NavBar/NestedSubMenu.tsx
+++ public/app/core/components/NavBar/NestedSubMenu.tsx
@@ -32,7 +32,7 @@ export const NestedSubMenu: FC<Props> = ({ items = [] }) => {
   );
 };
 
-const getStyles = ({ colors, components, spacing }: GrafanaTheme2) => {
+const getStyles = ({ colors, components, spacing, zIndex }: GrafanaTheme2) => {
   return {
     menu: css`
       background-color: ${colors.background.primary};
@@ -44,6 +44,7 @@ const getStyles = ({ colors, components, spacing }: GrafanaTheme2) => {
       list-style: none;
       opacity: 0;
       visibility: hidden;
+      z-index: ${zIndex.sidemenu};
 
       & > li {
         position: relative;
