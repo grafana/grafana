c8931c382623dbcbbf8b4dc22552a1aed57e739c
tharun <rajendrantharun@live.com>
2022-01-28 02:59:26 +0530

    Explore: modify tempo trace start time to human readable format in table output
    Signed-off-by: tharun <rajendrantharun@live.com>

--- public/app/plugins/datasource/tempo/resultTransformer.ts
+++ public/app/plugins/datasource/tempo/resultTransformer.ts
@@ -12,6 +12,8 @@ import {
 } from '@grafana/data';
 import { SpanStatus, SpanStatusCode } from '@opentelemetry/api';
 import { collectorTypes } from '@opentelemetry/exporter-collector';
+import formatDistance from 'date-fns/formatDistance';
+import differenceInHours from 'date-fns/differenceInHours';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { createGraphFrames } from './graphTransform';
 
@@ -558,7 +560,7 @@ export function createTableFrameFromSearch(data: SearchResponse[], instanceSetti
         },
       },
       { name: 'traceName', type: FieldType.string, config: { displayNameFromDS: 'Trace name' } },
-      { name: 'startTime', type: FieldType.time, config: { displayNameFromDS: 'Start time' } },
+      { name: 'startTime', type: FieldType.string, config: { displayNameFromDS: 'Start time' } },
       { name: 'duration', type: FieldType.number, config: { displayNameFromDS: 'Duration', unit: 'ms' } },
     ],
     meta: {
@@ -569,7 +571,9 @@ export function createTableFrameFromSearch(data: SearchResponse[], instanceSetti
     return frame;
   }
   // Show the most recent traces
-  const traceData = data.map(transformToTraceData).sort((a, b) => b?.startTime! - a?.startTime!);
+  const traceData = data
+    .sort((a, b) => parseInt(b?.startTimeUnixNano!, 10) / 1000000 - parseInt(a?.startTimeUnixNano!, 10) / 1000000)
+    .map(transformToTraceData);
 
   for (const trace of traceData) {
     frame.add(trace);
@@ -586,9 +590,21 @@ function transformToTraceData(data: SearchResponse) {
   if (data.rootTraceName) {
     traceName += data.rootTraceName;
   }
+
+  const traceStartTime = parseInt(data.startTimeUnixNano!, 10) / 1000000;
+
+  let startTime = new Date(traceStartTime).toTimeString();
+
+  if (Math.abs(differenceInHours(new Date(traceStartTime), new Date())) <= 1) {
+    startTime = formatDistance(new Date(traceStartTime), new Date(), {
+      addSuffix: true,
+      includeSeconds: true,
+    });
+  }
+
   return {
     traceID: data.traceID,
-    startTime: parseInt(data.startTimeUnixNano, 10) / 1000 / 1000,
+    startTime: startTime,
     duration: data.durationMs,
     traceName,
   };
