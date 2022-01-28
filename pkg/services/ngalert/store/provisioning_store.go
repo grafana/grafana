8fa9c5d605fa63796a72e297d93ee5e8e3887494
Alex Weaver <weaver.alex.d@gmail.com>
2022-01-28 15:23:35 -0600

    Add a few notes in comments

--- pkg/services/ngalert/store/provisioning_store.go
+++ pkg/services/ngalert/store/provisioning_store.go
@@ -22,6 +22,7 @@ func (pr provenanceRecord) TableName() string {
 
 type ProvisioningStore interface {
 	GetProvenance(o interface{}) (models.Provenance, error)
+	// TODO: API to query all provenances for a specific type
 	SetProvenance(o interface{}, p models.Provenance) error
 }
 
@@ -67,11 +68,33 @@ func (st DBstore) SetProvenance(o interface{}, p models.Provenance) error {
 		return nil
 	}
 
+	/*
+	   NOTES:
+	   BEGIN TRANSACTION
+	   SELECT * FROM ..... WHERE id = 'blah'
+	   UPDATE .... WHERE id = 'blah'
+	   UPDATE .... WHERE foreignId = 'blah'
+	   EXECUTE
+
+
+
+
+	   Begin transaction
+	   update....... WHERE token = 'last_known_token'
+	   CREATE new provenance record....
+	   Execute
+
+	   in memory:
+	   >0 records updated! successful transaction
+
+	   reacquire token, reapply, or reject request....*/
+
 	return st.SQLStore.WithTransactionalDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
 		// TODO: this is gonna slap the database. Is in-memory caching of provenance status possible? does this break HA?
 		// TODO: either way, this is a really naive way of doing this.
 		// TODO: this also totally won't slap the database if we use a unit of work pattern. This solves other problems too.
 		// TODO: Need to make sure that writing a record where our concurrency key fails will also fail the whole transaction. That way, this gets rolled back too. can't just check that 0 updates happened inmemory. Check with jp. If not possible, we need our own concurrency key.
+		// TODO: clean up provenance records periodically
 		q := "DELETE FROM provenance_type WHERE record_key = ? AND record_type = ?"
 		_, err := sess.Exec(q, recordKey, recordType)
 		if err != nil {
