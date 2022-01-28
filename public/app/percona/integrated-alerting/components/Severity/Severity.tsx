15d5a4945219dac2e707065a8ab8b0ae43a64ec3
Fabio Silva <ffjs1993@gmail.com>
2022-01-28 13:14:41 +0000

    fix severity colors

--- public/app/percona/integrated-alerting/components/Severity/Severity.tsx
+++ public/app/percona/integrated-alerting/components/Severity/Severity.tsx
@@ -1,13 +1,13 @@
 import { cx } from '@emotion/css';
 import React, { FC } from 'react';
 
-import { useTheme } from '@grafana/ui';
+import { useTheme2 } from '@grafana/ui';
 
 import { getStyles } from './Severity.styles';
 import { SeverityProps } from './Severity.types';
 
 export const Severity: FC<SeverityProps> = ({ severity, className }) => {
-  const theme = useTheme();
+  const theme = useTheme2();
   const styles = getStyles(theme, severity);
 
   return <span className={cx(styles.severity, className)}>{severity}</span>;
