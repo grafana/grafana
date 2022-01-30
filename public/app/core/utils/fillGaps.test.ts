66a90dcab3e33e997e9160aa9908a304a88ba7ee
Todd Treece <todd.treece@grafana.com>
2022-01-30 13:15:48 -0500

    add additional test coverage

--- public/app/core/utils/fillGaps.test.ts
+++ public/app/core/utils/fillGaps.test.ts
@@ -2,7 +2,7 @@ import { FieldType, MutableDataFrame } from '@grafana/data';
 import { fillGaps } from './fillGaps';
 
 describe('fillGaps', () => {
-  test('should fill nulls as expected', () => {
+  test('should fill nulls as expected for time fields', () => {
     const df = new MutableDataFrame({
       refId: 'A',
       fields: [
@@ -15,6 +15,34 @@ describe('fillGaps', () => {
     expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     expect(result.fields[1].values.toArray()).toStrictEqual([4, null, 6, null, null, null, null, null, null, 8]);
     expect(result.fields[2].values.toArray()).toStrictEqual(['a', null, 'b', null, null, null, null, null, null, 'c']);
+  });
+
+  test('should fill nulls as expected for number fields', () => {
+    const df = new MutableDataFrame({
+      refId: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [1, 3, 10] },
+        { name: 'One', type: FieldType.number, config: { interval: 2 }, values: [2, 6, 10] },
+        { name: 'Two', type: FieldType.string, values: ['a', 'b', 'c'] },
+      ],
+    });
+    const result = fillGaps(df);
+    expect(result.fields[0].values.toArray()).toStrictEqual([1, null, 3, null, 10]);
+    expect(result.fields[1].values.toArray()).toStrictEqual([2, 4, 6, 8, 10]);
+    expect(result.fields[2].values.toArray()).toStrictEqual(['a', null, 'b', null, 'c']);
+  });
+
+  test('should not modify frame if interval is on string field', () => {
+    const df = new MutableDataFrame({
+      refId: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.string, config: { interval: 1 }, values: ['a', 'b'] },
+        { name: 'Value', type: FieldType.number, values: [6, 8] },
+      ],
+    });
+    const result = fillGaps(df);
+    expect(result.fields[0].values.toArray()).toStrictEqual(['a', 'b']);
+    expect(result.fields[1].values.toArray()).toStrictEqual([6, 8]);
   });
 
   test('should not insert nulls for out of order timestamps', () => {
@@ -43,17 +71,30 @@ describe('fillGaps', () => {
     expect(result.fields[1].values.toArray()).toStrictEqual([6, 8]);
   });
 
-  test('should handle empty field values with interval', () => {
+  test('should not modify frame if there are less than two values', () => {
     const df = new MutableDataFrame({
       refId: 'A',
       fields: [
-        { name: 'Time', type: FieldType.time, config: { interval: 2 }, values: [] },
-        { name: 'Value', type: FieldType.number, values: [] },
+        { name: 'Time', type: FieldType.string, config: { interval: 1 }, values: ['a'] },
+        { name: 'Value', type: FieldType.number, values: [6] },
       ],
     });
     const result = fillGaps(df);
-    expect(result.fields[0].values.toArray()).toStrictEqual([]);
-    expect(result.fields[1].values.toArray()).toStrictEqual([]);
+    expect(result.fields[0].values.toArray()).toStrictEqual(['a']);
+    expect(result.fields[1].values.toArray()).toStrictEqual([6]);
+  });
+
+  test('should handle invalid interval value', () => {
+    const df = new MutableDataFrame({
+      refId: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, config: { interval: -1 }, values: [1, 3] },
+        { name: 'Value', type: FieldType.number, values: [6, 8] },
+      ],
+    });
+    const result = fillGaps(df);
+    expect(result.fields[0].values.toArray()).toStrictEqual([1, 3]);
+    expect(result.fields[1].values.toArray()).toStrictEqual([6, 8]);
   });
 
   test('should handle timestamp alignment issues', () => {
--- public/app/core/utils/fillGaps.ts
+++ public/app/core/utils/fillGaps.ts
@@ -1,13 +1,20 @@
-import { DataFrame, ArrayVector } from '@grafana/data';
+import { DataFrame, ArrayVector, FieldType } from '@grafana/data';
 
 export const fillGaps = (frame: DataFrame): DataFrame => {
+  if (frame.length < 2) {
+    return frame;
+  }
+
   let intervalIndex = 0;
   const intervalField = frame.fields.find((f, i) => {
-    if ((f.config.interval ?? 0) > 0) {
-      intervalIndex = i;
-      return true;
+    if (f.type !== FieldType.time && f.type !== FieldType.number) {
+      return false;
     }
-    return false;
+    if ((f.config.interval ?? 0) <= 0) {
+      return false;
+    }
+    intervalIndex = i;
+    return true;
   });
 
   if (!intervalField) {
@@ -27,9 +34,12 @@ export const fillGaps = (frame: DataFrame): DataFrame => {
     values.forEach((v, i) => v.push(i === intervalIndex ? current : original[i][idx]));
   }
 
-  for (let i = 0; i < frame.fields.length; i++) {
-    frame.fields[i].values = new ArrayVector(values[i]);
-  }
-
-  return frame;
+  return {
+    ...frame,
+    length: values[0].length,
+    fields: frame.fields.map((field, i) => ({
+      ...field,
+      values: new ArrayVector(values[i]),
+    })),
+  };
 };
