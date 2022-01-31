72583af5028843685ec49dfe40f833ab2a7946c3
Leon Sorokin <leeoniya@gmail.com>
2022-01-31 15:07:36 -0600

    one mode (to rule them all?)

--- packages/grafana-data/src/transformations/transformers/insertValues.test.ts
+++ packages/grafana-data/src/transformations/transformers/insertValues.test.ts
@@ -1,8 +1,86 @@
-import { FieldType, MutableDataFrame } from '@grafana/data';
-import { insertValuesInterval } from './insertValues';
+import { ArrayVector, FieldType, MutableDataFrame } from '@grafana/data';
+import { insertValues } from './insertValues';
 
-describe('insertValues Transformer (mode: interval)', () => {
-  test('should fill nulls as expected', () => {
+function randInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function genFrame() {
+  let fieldCount = 10;
+  let valueCount = 3000;
+  let step = 1000;
+  let skipProb = 0.5;
+  let skipSteps = [1, 5]; // min, max
+
+  let allValues = Array(fieldCount);
+
+  allValues[0] = Array(valueCount);
+
+  for (let i = 0, curStep = Date.now(); i < valueCount; i++) {
+    curStep = allValues[0][i] = curStep + step * (Math.random() < skipProb ? randInt(skipSteps[0], skipSteps[1]) : 1);
+  }
+
+  for (let fi = 1; fi < fieldCount; fi++) {
+    let values = Array(valueCount);
+
+    for (let i = 0; i < valueCount; i++) {
+      values[i] = Math.random() * 100;
+    }
+
+    allValues[fi] = values;
+  }
+
+  return {
+    length: valueCount,
+    fields: allValues.map((values, i) => {
+      return {
+        name: 'A-' + i,
+        type: i === 0 ? FieldType.time : FieldType.number,
+        config: {
+          interval: i === 0 ? step : null,
+        },
+        values: new ArrayVector(values),
+      };
+    }),
+  };
+}
+
+describe('insertValues Transformer', () => {
+  test('should insert nulls at midpoints between adjacent > threshold: 1', () => {
+    const df = new MutableDataFrame({
+      refId: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [1, 3, 10] },
+        { name: 'One', type: FieldType.number, values: [4, 6, 8] },
+        { name: 'Two', type: FieldType.string, values: ['a', 'b', 'c'] },
+      ],
+    });
+
+    const result = insertValues(df, { threshold: 1 });
+
+    expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 3, 6.5, 10]);
+    expect(result.fields[1].values.toArray()).toStrictEqual([4, null, 6, null, 8]);
+    expect(result.fields[2].values.toArray()).toStrictEqual(['a', null, 'b', null, 'c']);
+  });
+
+  test('should insert nulls at midpoints between adjacent > threshold: 2', () => {
+    const df = new MutableDataFrame({
+      refId: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [5, 7, 11] },
+        { name: 'One', type: FieldType.number, values: [4, 6, 8] },
+        { name: 'Two', type: FieldType.string, values: ['a', 'b', 'c'] },
+      ],
+    });
+
+    const result = insertValues(df, { threshold: 2 });
+
+    expect(result.fields[0].values.toArray()).toStrictEqual([5, 7, 9, 11]);
+    expect(result.fields[1].values.toArray()).toStrictEqual([4, 6, null, 8]);
+    expect(result.fields[2].values.toArray()).toStrictEqual(['a', 'b', null, 'c']);
+  });
+
+  test('should insert nulls at midpoints between adjacent > interval', () => {
     const df = new MutableDataFrame({
       refId: 'A',
       fields: [
@@ -11,13 +89,15 @@ describe('insertValues Transformer (mode: interval)', () => {
         { name: 'Two', type: FieldType.string, values: ['a', 'b', 'c'] },
       ],
     });
-    const result = insertValuesInterval(df);
-    expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    expect(result.fields[1].values.toArray()).toStrictEqual([4, null, 6, null, null, null, null, null, null, 8]);
-    expect(result.fields[2].values.toArray()).toStrictEqual(['a', null, 'b', null, null, null, null, null, null, 'c']);
+
+    const result = insertValues(df);
+
+    expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 3, 6.5, 10]);
+    expect(result.fields[1].values.toArray()).toStrictEqual([4, null, 6, null, 8]);
+    expect(result.fields[2].values.toArray()).toStrictEqual(['a', null, 'b', null, 'c']);
   });
 
-  test('should fill nulls as expected', () => {
+  test('should insert nulls at midpoints between adjacent > interval: 2', () => {
     const df = new MutableDataFrame({
       refId: 'A',
       fields: [
@@ -26,36 +106,12 @@ describe('insertValues Transformer (mode: interval)', () => {
         { name: 'Two', type: FieldType.string, values: ['a', 'b', 'c'] },
       ],
     });
-    const result = insertValuesInterval(df);
+
+    const result = insertValues(df, { threshold: 2 });
+
     expect(result.fields[0].values.toArray()).toStrictEqual([5, 7, 9, 11]);
     expect(result.fields[1].values.toArray()).toStrictEqual([4, 6, null, 8]);
     expect(result.fields[2].values.toArray()).toStrictEqual(['a', 'b', null, 'c']);
-  });
-
-  test('should not modify frame when missing interval', () => {
-    const df = new MutableDataFrame({
-      refId: 'A',
-      fields: [
-        { name: 'Time', type: FieldType.time, values: [1, 3] },
-        { name: 'Value', type: FieldType.number, values: [6, 8] },
-      ],
-    });
-    const result = insertValuesInterval(df);
-    expect(result.fields[0].values.toArray()).toStrictEqual([1, 3]);
-    expect(result.fields[1].values.toArray()).toStrictEqual([6, 8]);
-  });
-
-  test('can guess interval from data', () => {
-    const df = new MutableDataFrame({
-      refId: 'A',
-      fields: [
-        { name: 'Time', type: FieldType.time, values: [1, 3, 4] },
-        { name: 'Value', type: FieldType.number, values: [6, 8, 9] },
-      ],
-    });
-    const result = insertValuesInterval(df, null, true);
-    expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 3, 4]);
-    expect(result.fields[1].values.toArray()).toStrictEqual([6, null, 8, 9]);
   });
 
   test('should noop on fewer than two values', () => {
@@ -66,9 +122,26 @@ describe('insertValues Transformer (mode: interval)', () => {
         { name: 'Value', type: FieldType.number, values: [1] },
       ],
     });
-    const result = insertValuesInterval(df);
+
+    const result = insertValues(df);
+
     expect(result.fields[0].values.toArray()).toStrictEqual([1]);
     expect(result.fields[1].values.toArray()).toStrictEqual([1]);
+  });
+
+  test('should noop on invalid threshold', () => {
+    const df = new MutableDataFrame({
+      refId: 'A',
+      fields: [
+        { name: 'Time', type: FieldType.time, values: [1, 2, 4] },
+        { name: 'Value', type: FieldType.number, values: [1, 1, 1] },
+      ],
+    });
+
+    const result = insertValues(df, { threshold: -1 });
+
+    expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 4]);
+    expect(result.fields[1].values.toArray()).toStrictEqual([1, 1, 1]);
   });
 
   test('should noop on invalid interval', () => {
@@ -79,21 +152,21 @@ describe('insertValues Transformer (mode: interval)', () => {
         { name: 'Value', type: FieldType.number, values: [1, 1, 1] },
       ],
     });
-    const result = insertValuesInterval(df);
+
+    const result = insertValues(df);
+
     expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 4]);
     expect(result.fields[1].values.toArray()).toStrictEqual([1, 1, 1]);
   });
 
-  test('should noop on missing interval', () => {
-    const df = new MutableDataFrame({
-      refId: 'A',
-      fields: [
-        { name: 'Time', type: FieldType.time, values: [1, 2, 4] },
-        { name: 'Value', type: FieldType.number, values: [1, 1, 1] },
-      ],
-    });
-    const result = insertValuesInterval(df);
-    expect(result.fields[0].values.toArray()).toStrictEqual([1, 2, 4]);
-    expect(result.fields[1].values.toArray()).toStrictEqual([1, 1, 1]);
+  test('perf stress test should be <= 10ms', () => {
+    // 10 fields x 3,000 values with 50% skip (output = 10 fields x 6,000 values)
+    let bigFrameA = genFrame();
+
+    // eslint-disable-next-line no-console
+    console.time('insertValues-10x3k');
+    insertValues(bigFrameA);
+    // eslint-disable-next-line no-console
+    console.timeEnd('insertValues-10x3k');
   });
 });
--- packages/grafana-data/src/transformations/transformers/insertValues.ts
+++ packages/grafana-data/src/transformations/transformers/insertValues.ts
@@ -1,123 +1,86 @@
-import { DataFrame, FieldType, Field, ArrayVector } from '../..';
+import { DataFrame, FieldType, ArrayVector } from '../..';
 
 import { map } from 'rxjs/operators';
 
 import { DataTransformerID } from './ids';
 import { DataTransformerInfo } from '../../types/transformations';
 
-const enum InsertValuesMode {
-  Interval = 'interval',
-  Threshold = 'threshold',
-}
-
-const IS_TIME_FIELD = (field: Field) => field.type === FieldType.time;
-const THRESHOLD_HOUR = 3600 * 1e3;
-
 export interface InsertValuesTransformerOptions {
-  mode: InsertValuesMode;
   // value to insert
-  value: any;
-  // finds field to use for interval or threshold checking
-  isRefField?: (field: Field) => boolean;
+  value?: any;
+  // field to use for interval or threshold checking
+  field?: string;
   // minimum threshold required between adjacent non-null/non-undefined values
   threshold?: number;
-  // should refField's values be scanned to find minimum interval?
-  guessInterval?: boolean;
 }
 
 export const insertValuesTransformer: DataTransformerInfo<InsertValuesTransformerOptions> = {
   id: DataTransformerID.insertValues,
   name: 'Insert Values',
   description: 'Can be used to insert null values between adjacent points spanning a minimum time threshold',
-  defaultOptions: {
-    mode: InsertValuesMode.Interval,
-    value: null,
-    isRefField: IS_TIME_FIELD,
-    guessInterval: false,
-  },
-  operator: ({ mode, value = null, isRefField = IS_TIME_FIELD, guessInterval = false, threshold = THRESHOLD_HOUR }) => (
-    source
-  ) =>
-    source.pipe(
-      map((dataFrames) => {
-        if (mode === InsertValuesMode.Interval) {
-          return dataFrames.map((f) => insertValuesInterval(f, value ?? null, guessInterval ?? false, isRefField));
-        } else {
-          return dataFrames.map((f) => insertValuesThreshold(f, value ?? null, threshold, isRefField ?? IS_TIME_FIELD));
-        }
-      })
-    ),
+  operator: (options) => (source) => source.pipe(map((dataFrames) => dataFrames.map((f) => insertValues(f, options)))),
 };
 
-// expects ascending number values in refField
-function getInterval(refField: Field, guessFromValues = false) {
-  let interval = Math.max(refField.config?.interval ?? 0, 0);
+export function insertValues(frame: DataFrame, opts?: InsertValuesTransformerOptions): DataFrame {
+  let { field: refFieldName, threshold, value = null } = opts ?? {};
 
-  if (interval === 0 && guessFromValues && refField.values.length > 2) {
-    let refVals = refField.values.toArray();
-
-    interval = Infinity;
-
-    for (let i = 1; i < refVals.length; i++) {
-      interval = Math.min(interval, refVals[i] - refVals[i - 1]);
-    }
-  }
-
-  return Number.isFinite(interval) ? interval : 0;
-}
-
-// assumes some interval steps may be absent, but what remains in the time field:
-// 1. contains only numbers, would pass Number.isFinite(value)
-// 2. conforms to the specified interval
-// 3. is in ascending order
-export function insertValuesInterval(
-  frame: DataFrame,
-  value: any = null,
-  guessFromValues = false,
-  isRefField = IS_TIME_FIELD
-): DataFrame {
-  if (frame.length <= 1) {
+  if (frame.length < 2) {
     return frame;
   }
 
-  let refField = frame.fields.find(isRefField);
+  let refField = frame.fields.find((field) => {
+    // note: getFieldDisplayName() would require full DF[]
+    return refFieldName != null ? field.name === refFieldName : field.type === FieldType.time;
+  });
 
   if (refField == null) {
     return frame;
   }
 
-  let interval = getInterval(refField, guessFromValues);
+  if (threshold == null) {
+    threshold = refField.config.interval ?? 0;
+  }
 
-  if (interval === 0) {
+  if (threshold <= 0) {
     return frame;
   }
 
   let refValues = refField.values.toArray();
   let len = refValues.length;
-  let minValue = refValues[0];
-  let maxValue = refValues[len - 1];
-  let filledLen = (maxValue - minValue) / interval + 1;
 
-  // cheap exit length matches expected length
-  if (len === filledLen) {
-    return frame;
+  let prevValue: number = refValues[0];
+  let refValuesNew: number[] = [prevValue];
+
+  for (let i = 1; i < len; i++) {
+    let curValue = refValues[i];
+
+    if (curValue - prevValue > threshold) {
+      // insert new value at midpoint
+      refValuesNew.push((prevValue + curValue) / 2);
+    }
+
+    refValuesNew.push(curValue);
+
+    prevValue = curValue;
   }
+
+  let filledLen = refValuesNew.length;
 
   let filledFieldValues: any[][] = [];
 
   for (let field of frame.fields) {
-    let filledValues = Array(filledLen);
+    let filledValues;
 
-    if (field === refField) {
-      for (let i = 0; i < filledLen; i++) {
-        filledValues[i] = minValue + i * interval;
-      }
-    } else {
+    if (field !== refField) {
+      filledValues = Array(filledLen);
+
       let fieldValues = field.values.toArray();
 
       for (let i = 0, j = 0; i < filledLen; i++) {
-        filledValues[i] = refValues[j] === minValue + i * interval ? fieldValues[j++] : value;
+        filledValues[i] = refValues[j] === refValuesNew[i] ? fieldValues[j++] : value;
       }
+    } else {
+      filledValues = refValuesNew;
     }
 
     filledFieldValues.push(filledValues);
@@ -133,9 +96,4 @@ export function insertValuesInterval(
   };
 
   return outFrame;
-}
-
-// TODO: implement
-function insertValuesThreshold(frame: DataFrame, value: any = null, threshold: number, isRefField = IS_TIME_FIELD) {
-  return frame;
 }
