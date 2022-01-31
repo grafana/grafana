5b37d14b7f6007a763e6e94adda0afdc87cf09a4
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-01-31 16:22:46 +0100

    Tempo: Display start time in search results as relative time (#44568) (#44661)
    * Explore: modify tempo trace start time to human readable format in table output
    
    Signed-off-by: tharun <rajendrantharun@live.com>
    
    * add unit tests
    
    Signed-off-by: tharun <rajendrantharun@live.com>
    (cherry picked from commit de1661e87721487e38e701a2871f5f571b7c32ef)
    
    Co-authored-by: Tharun Rajendran <rajendrantharun@live.com>

--- public/app/plugins/datasource/tempo/testResponse.ts
+++ public/app/plugins/datasource/tempo/testResponse.ts
@@ -2202,3 +2202,24 @@ export const otlpResponse = {
     },
   ],
 };
+
+export const tempoSearchResponse = {
+  traces: [
+    {
+      traceID: 'e641dcac1c3a0565',
+      rootServiceName: 'c10d7ca4e3a00354',
+      startTimeUnixNano: '1643356828724000000',
+      durationMs: 65,
+    },
+    {
+      traceID: 'c2983496a2b12544',
+      rootServiceName: '<root span not yet received>',
+      startTimeUnixNano: '1643342166678000000',
+      durationMs: 93,
+    },
+  ],
+  metrics: {
+    inspectedTraces: 2,
+    inspectedBytes: '83720',
+  },
+};
