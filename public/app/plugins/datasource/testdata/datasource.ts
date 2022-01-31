428cf08f2c79f97883c9949f2f7bb16d0c076380
Ryan McKinley <ryantxu@gmail.com>
2022-01-31 11:39:38 -0800

    replace refId on result

--- public/app/plugins/datasource/testdata/datasource.ts
+++ public/app/plugins/datasource/testdata/datasource.ts
@@ -13,6 +13,7 @@ import {
   LoadingState,
   TimeRange,
   ScopedVars,
+  toDataFrame,
 } from '@grafana/data';
 import { Scenario, TestDataQuery } from './types';
 import { DataSourceWithBackend, getBackendSrv, getGrafanaLiveSrv, getTemplateSrv, TemplateSrv } from '@grafana/runtime';
@@ -191,8 +192,11 @@ export class TestDataDataSource extends DataSourceWithBackend<TestDataQuery> {
 
   rawFrameQuery(target: TestDataQuery, options: DataQueryRequest<TestDataQuery>): Observable<DataQueryResponse> {
     try {
-      // NOTE: refId in saved data may not match query target
-      const data: DataFrame[] = JSON.parse(target.rawFrameContent || '[]');
+      const data = JSON.parse(target.rawFrameContent || '[]').map((v: any) => {
+        const f = toDataFrame(v);
+        f.refId = target.refId;
+        return f;
+      });
       return of({ data, state: LoadingState.Done }).pipe(delay(100));
     } catch (ex) {
       return of({ data: [], error: ex }).pipe(delay(100));
