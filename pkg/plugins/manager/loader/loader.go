bf8694e709ab993b68e7a0624722430644d2db38
Will Browne <wbrowne@users.noreply.github.com>
2022-01-31 15:24:51 +0100

    Plugins: Amend plugins loader log statements (#44652)
    * adjust log statements
    
    * Update pkg/plugins/manager/loader/loader.go
    
    Co-authored-by: Marcus Efraimsson <marcus.efraimsson@gmail.com>
    
    * add errs to logs
    
    Co-authored-by: Marcus Efraimsson <marcus.efraimsson@gmail.com>

--- pkg/plugins/manager/loader/loader.go
+++ pkg/plugins/manager/loader/loader.go
@@ -75,18 +75,18 @@ func (l *Loader) loadPlugins(ctx context.Context, class plugins.Class, pluginJSO
 	for _, pluginJSONPath := range pluginJSONPaths {
 		plugin, err := l.readPluginJSON(pluginJSONPath)
 		if err != nil {
-			l.log.Warn("Skipping plugin loading as it's plugin.json is invalid", "id", plugin.ID)
+			l.log.Warn("Skipping plugin loading as its plugin.json could not be read", "path", pluginJSONPath, "err", err)
 			continue
 		}
 
 		pluginJSONAbsPath, err := filepath.Abs(pluginJSONPath)
 		if err != nil {
-			l.log.Warn("Skipping plugin loading as full plugin.json path could not be calculated", "id", plugin.ID)
+			l.log.Warn("Skipping plugin loading as absolute plugin.json path could not be calculated", "pluginID", plugin.ID, "err", err)
 			continue
 		}
 
 		if _, dupe := foundPlugins[filepath.Dir(pluginJSONAbsPath)]; dupe {
-			l.log.Warn("Skipping plugin loading as it's a duplicate", "id", plugin.ID)
+			l.log.Warn("Skipping plugin loading as it's a duplicate", "pluginID", plugin.ID)
 			continue
 		}
 		foundPlugins[filepath.Dir(pluginJSONAbsPath)] = plugin
