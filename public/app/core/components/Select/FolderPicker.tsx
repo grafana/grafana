dfa64bde5a3a9f5d43788466f4c7dad84b182e16
gillesdemey <gilles.de.mey@gmail.com>
2022-02-01 10:32:47 +0100

    chore: add custom menulist [skip ci]

--- public/app/core/components/Select/FolderPicker.tsx
+++ public/app/core/components/Select/FolderPicker.tsx
@@ -1,13 +1,19 @@
-import React, { PureComponent } from 'react';
-import { debounce } from 'lodash';
-import { AsyncSelect } from '@grafana/ui';
-import { AppEvents, SelectableValue } from '@grafana/data';
+import React, { FC, useCallback, useState } from 'react';
+import { AppEvents, GrafanaTheme2, SelectableValue } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
+import { AsyncSelect, Button, InlineField, InlineFieldRow, Input, useTheme2 } from '@grafana/ui';
+import { useAsync, useAsyncFn } from 'react-use';
+import debounce from 'debounce-promise';
 
 import appEvents from '../../app_events';
 import { contextSrv } from 'app/core/services/context_srv';
 import { createFolder, getFolderById, searchFolders } from 'app/features/manage-dashboards/state/actions';
 import { PermissionLevelString } from '../../../types';
+import { MenuListProps } from 'react-select';
+import { SelectMenu } from '@grafana/ui/src/components/Select/SelectMenu';
+import { css } from '@emotion/css';
+
+type SelectedFolder = SelectableValue<number>;
 
 export interface Props {
   onChange: ($folder: { title: string; id: number }) => void;
@@ -31,108 +37,62 @@ export interface Props {
   inputId?: string;
 }
 
-interface State {
-  folder: SelectableValue<number> | null;
-}
+const FolderPicker: FC<Props> = ({
+  inputId,
+  dashboardId,
+  enableCreateNew = false,
+  skipInitialLoad,
+  initialFolderId,
+  initialTitle = '',
+  rootName = 'General',
+  enableReset = false,
+  permissionLevel = PermissionLevelString.Edit,
+  allowEmpty = false,
+  showRoot = true,
+  onChange,
+}) => {
+  const [folder, setFolder] = useState<SelectedFolder | null>(null);
 
-export class FolderPicker extends PureComponent<Props, State> {
-  debouncedSearch: any;
+  const onFolderChange = useCallback(
+    (newFolder: SelectedFolder) => {
+      setFolder(newFolder);
+      onChange({ id: newFolder.value ?? 0, title: newFolder.label ?? rootName });
+    },
+    [onChange, rootName]
+  );
 
-  constructor(props: Props) {
-    super(props);
+  const getOptions = useCallback(
+    async (query: string) => {
+      const searchHits = await searchFolders(query, permissionLevel);
 
-    this.state = {
-      folder: null,
-    };
+      const options: SelectedFolder[] = searchHits.map((hit) => ({ label: hit.title, value: hit.id }));
+      if (contextSrv.isEditor && rootName?.toLowerCase().startsWith(query.toLowerCase()) && showRoot) {
+        options.unshift({ label: rootName, value: 0 });
+      }
 
-    this.debouncedSearch = debounce(this.getOptions, 300, {
-      leading: true,
-      trailing: true,
-    });
-  }
+      if (
+        enableReset &&
+        query === '' &&
+        initialTitle !== '' &&
+        !options.find((option) => option.label === initialTitle)
+      ) {
+        options.unshift({ label: initialTitle, value: initialFolderId });
+      }
 
-  static defaultProps: Partial<Props> = {
-    rootName: 'General',
-    enableReset: false,
-    initialTitle: '',
-    enableCreateNew: false,
-    permissionLevel: PermissionLevelString.Edit,
-    allowEmpty: false,
-    showRoot: true,
-  };
+      return options;
+    },
+    [enableReset, initialFolderId, initialTitle, permissionLevel, rootName, showRoot]
+  );
 
-  componentDidMount = async () => {
-    if (this.props.skipInitialLoad) {
-      const folder = await getInitialValues({
-        getFolder: getFolderById,
-        folderId: this.props.initialFolderId,
-        folderName: this.props.initialTitle,
-      });
-      this.setState({ folder });
-      return;
-    }
+  const debouncedSearch = debounce(getOptions, 300, { leading: true });
 
-    await this.loadInitialValue();
-  };
+  const loadInitialValue = useCallback(async () => {
+    const resetFolder: SelectedFolder = { label: initialTitle, value: undefined };
+    const rootFolder: SelectedFolder = { label: rootName, value: 0 };
 
-  getOptions = async (query: string) => {
-    const { rootName, enableReset, initialTitle, permissionLevel, initialFolderId, showRoot } = this.props;
+    const options = await getOptions('');
 
-    const searchHits = await searchFolders(query, permissionLevel);
-
-    const options: Array<SelectableValue<number>> = searchHits.map((hit) => ({ label: hit.title, value: hit.id }));
-    if (contextSrv.isEditor && rootName?.toLowerCase().startsWith(query.toLowerCase()) && showRoot) {
-      options.unshift({ label: rootName, value: 0 });
-    }
-
-    if (
-      enableReset &&
-      query === '' &&
-      initialTitle !== '' &&
-      !options.find((option) => option.label === initialTitle)
-    ) {
-      options.unshift({ label: initialTitle, value: initialFolderId });
-    }
-
-    return options;
-  };
-
-  onFolderChange = (newFolder: SelectableValue<number>) => {
-    if (!newFolder) {
-      newFolder = { value: 0, label: this.props.rootName };
-    }
-
-    this.setState(
-      {
-        folder: newFolder,
-      },
-      () => this.props.onChange({ id: newFolder.value!, title: newFolder.label! })
-    );
-  };
-
-  createNewFolder = async (folderName: string) => {
-    // @ts-ignore
-    const newFolder = await createFolder({ title: folderName });
-    let folder = { value: -1, label: 'Not created' };
-    if (newFolder.id > -1) {
-      appEvents.emit(AppEvents.alertSuccess, ['Folder Created', 'OK']);
-      folder = { value: newFolder.id, label: newFolder.title };
-      await this.onFolderChange(folder);
-    } else {
-      appEvents.emit(AppEvents.alertError, ['Folder could not be created']);
-    }
-
-    return folder;
-  };
-
-  private loadInitialValue = async () => {
-    const { initialTitle, rootName, initialFolderId, enableReset, dashboardId } = this.props;
-    const resetFolder: SelectableValue<number> = { label: initialTitle, value: undefined };
-    const rootFolder: SelectableValue<number> = { label: rootName, value: 0 };
-
-    const options = await this.getOptions('');
-
-    let folder: SelectableValue<number> | null = null;
+    let folder: SelectedFolder | null = null;
 
     if (initialFolderId !== undefined && initialFolderId !== null && initialFolderId > -1) {
       folder = options.find((option) => option.value === initialFolderId) || null;
@@ -142,7 +102,7 @@ export class FolderPicker extends PureComponent<Props, State> {
       folder = options.find((option) => option.id === initialFolderId) || null;
     }
 
-    if (!folder && !this.props.allowEmpty) {
+    if (!folder && !allowEmpty) {
       if (contextSrv.isEditor) {
         folder = rootFolder;
       } else {
@@ -156,42 +116,118 @@ export class FolderPicker extends PureComponent<Props, State> {
       }
     }
 
-    this.setState(
-      {
-        folder,
-      },
-      () => {
-        // if this is not the same as our initial value notify parent
-        if (folder && folder.value !== initialFolderId) {
-          this.props.onChange({ id: folder.value!, title: folder.label! });
-        }
+    setFolder(folder);
+    // if this is not the same as our initial value notify parent
+    if (folder && folder.value !== initialFolderId) {
+      onChange({ id: folder.value ?? 0, title: folder.label ?? rootName });
+    }
+  }, [allowEmpty, dashboardId, enableReset, getOptions, initialFolderId, initialTitle, onChange, rootName]);
+
+  // initial values for dropdown
+  useAsync(async () => {
+    if (skipInitialLoad) {
+      const folder = await getInitialValues({
+        getFolder: getFolderById,
+        folderId: initialFolderId,
+        folderName: initialTitle,
+      });
+      setFolder(folder);
+    }
+
+    await loadInitialValue();
+  }, [skipInitialLoad, initialFolderId, initialTitle]);
+
+  return (
+    <div data-testid={selectors.components.FolderPicker.containerV2}>
+      <AsyncSelect
+        inputId={inputId}
+        aria-label={selectors.components.FolderPicker.input}
+        loadingMessage="Loading folders..."
+        defaultOptions
+        defaultValue={folder}
+        value={folder}
+        loadOptions={debouncedSearch}
+        onChange={onFolderChange}
+        components={enableCreateNew && { MenuList: SelectMenuWithCreate }}
+        menuShouldPortal
+        backspaceRemovesValue={false}
+        isClearable={false}
+        tabSelectsValue={false}
+      />
+    </div>
+  );
+};
+
+export { FolderPicker };
+
+const SelectMenuWithCreate = (props: MenuListProps<SelectedFolder, false>) => {
+  const theme = useTheme2();
+  const styles = getStyles(theme);
+
+  const [editMode, setEditMode] = useState<boolean>(false);
+
+  const toggleEditMode = useCallback(() => {
+    setEditMode((mode) => !mode);
+  }, []);
+
+  const [{ loading }, handleCreateFolder] = useAsyncFn(
+    async (title: string) => {
+      const newFolder = await createFolder({ title });
+      let folder = { value: -1, label: 'Not created' };
+
+      if (newFolder.id > -1) {
+        appEvents.emit(AppEvents.alertSuccess, ['Folder Created', 'OK']);
+        folder = { value: newFolder.id, label: newFolder.title };
+        onFolderChange(folder); // TODO hook up this function
+      } else {
+        appEvents.emit(AppEvents.alertError, ['Folder could not be created']);
       }
-    );
-  };
 
-  render() {
-    const { folder } = this.state;
-    const { enableCreateNew, inputId } = this.props;
+      return folder;
 
-    return (
-      <div data-testid={selectors.components.FolderPicker.containerV2}>
-        <AsyncSelect
-          inputId={inputId}
-          aria-label={selectors.components.FolderPicker.input}
-          loadingMessage="Loading folders..."
-          defaultOptions
-          defaultValue={folder}
-          value={folder}
-          allowCustomValue={enableCreateNew}
-          loadOptions={this.debouncedSearch}
-          onChange={this.onFolderChange}
-          onCreateOption={this.createNewFolder}
-          menuShouldPortal
-        />
+      toggleEditMode();
+    },
+    [toggleEditMode]
+  );
+
+  return (
+    <SelectMenu {...props}>
+      {props.children}
+      <div className={styles.menuFooter}>
+        {!editMode && (
+          <Button icon="folder-plus" type="button" size="sm" onClick={toggleEditMode} className={styles.createButton}>
+            New folder
+          </Button>
+        )}
+        {/* {editMode && (
+          <InlineFieldRow>
+            <InlineField>
+              <Input name="folder-name" placeholder="Folder name" />
+            </InlineField>
+            <InlineField>
+              <Button disabled={loading} size="sm" type="button" onClick={() => handleCreateFolder('test')}>
+                Create folder
+              </Button>
+            </InlineField>
+          </InlineFieldRow>
+        )} */}
       </div>
-    );
-  }
-}
+    </SelectMenu>
+  );
+};
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  menuFooter: css`
+    padding: ${theme.v1.spacing.sm};
+    background: ${theme.v1.colors.bg1};
+    border-top: solid 1px ${theme.v1.colors.border2};
+    display: flex;
+    flex-direction: column;
+  `,
+  createButton: css`
+    align-self: flex-end;
+  `,
+});
 
 interface Args {
   getFolder: typeof getFolderById;
@@ -199,7 +235,7 @@ interface Args {
   folderName?: string;
 }
 
-export async function getInitialValues({ folderName, folderId, getFolder }: Args): Promise<SelectableValue<number>> {
+export async function getInitialValues({ folderName, folderId, getFolder }: Args): Promise<SelectedFolder> {
   if (folderId === null || folderId === undefined || folderId < 0) {
     throw new Error('folderId should to be greater or equal to zero.');
   }
