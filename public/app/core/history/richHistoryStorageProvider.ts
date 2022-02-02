c068ff78fcfc0bc5f7779e575c8797482e9fcdb9
Piotr Jamróz <pm.jamroz@gmail.com>
2022-02-02 15:01:43 +0100

    Remove unsed files

--- public/app/core/history/richHistoryStorageProvider.ts
+++ public/app/core/history/richHistoryStorageProvider.ts
@@ -1,12 +1,7 @@
 import RichHistoryLocalStorage from './RichHistoryLocalStorage';
-import RichHistoryRemoteStorage from './RichHistoryRemoteStorage';
 import RichHistoryStorage from './RichHistoryStorage';
 
 const richHistoryLocalStorage = new RichHistoryLocalStorage();
-
-// To be implemented and switched with a feature flag.
-// @ts-ignore
-const richHistoryRemoteStorage = new RichHistoryRemoteStorage();
 
 export const getRichHistoryStorage = (): RichHistoryStorage => {
   return richHistoryLocalStorage;
