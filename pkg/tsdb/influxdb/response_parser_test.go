05eaefd47cc851b0732e43712d8b3072df7f0830
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-02-02 15:44:35 +0000

    Added string test case

--- pkg/tsdb/influxdb/response_parser_test.go
+++ pkg/tsdb/influxdb/response_parser_test.go
@@ -59,6 +59,7 @@ func TestInfluxdbResponseParser(t *testing.T) {
 		query := &Query{}
 		labels, err := data.LabelsFromString("datacenter=America")
 		require.Nil(t, err)
+
 		floatField := data.NewField("value", labels, []*float64{
 			pointer.Float64(222), pointer.Float64(222), nil,
 		})
@@ -73,10 +74,27 @@ func TestInfluxdbResponseParser(t *testing.T) {
 			floatField,
 		)
 
+		stringField := data.NewField("value", labels, []string{
+			"path val", "path val", "path val",
+		})
+		stringField.Config = &data.FieldConfig{DisplayNameFromDS: "cpu.path { datacenter: America }"}
+		stringFrame := data.NewFrame("cpu.path { datacenter: America }",
+			data.NewField("time", nil,
+				[]time.Time{
+					time.Date(1970, 1, 1, 0, 1, 51, 0, time.UTC),
+					time.Date(1970, 1, 1, 0, 1, 51, 0, time.UTC),
+					time.Date(1970, 1, 1, 0, 1, 51, 0, time.UTC),
+				}),
+			stringField,
+		)
+
 		result := parser.Parse(prepare(response), query)
 
 		frame := result.Responses["A"]
 		if diff := cmp.Diff(floatFrame, frame.Frames[0], data.FrameTestCompareOptions()...); diff != "" {
+			t.Errorf("Result mismatch (-want +got):\n%s", diff)
+		}
+		if diff := cmp.Diff(stringFrame, frame.Frames[1], data.FrameTestCompareOptions()...); diff != "" {
 			t.Errorf("Result mismatch (-want +got):\n%s", diff)
 		}
 	})
