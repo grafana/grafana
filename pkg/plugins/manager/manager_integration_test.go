f5a117c967767b5c6e16fced4b7d5caa2bf5dfee
spinillos <selenepinillos@gmail.com>
2022-02-02 08:48:56 +0100

    Lint again

--- pkg/plugins/manager/manager_integration_test.go
+++ pkg/plugins/manager/manager_integration_test.go
@@ -2,22 +2,19 @@ package manager
 
 import (
 	"context"
-	"github.com/grafana/grafana/pkg/services/dashboards/manager"
 	"net/http"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/infra/tracing"
-	"go.opentelemetry.io/otel/trace"
-
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
-
+	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/plugins/backendplugin/coreplugin"
 	"github.com/grafana/grafana/pkg/plugins/backendplugin/provider"
 	"github.com/grafana/grafana/pkg/plugins/manager/loader"
 	"github.com/grafana/grafana/pkg/plugins/manager/signature"
+	"github.com/grafana/grafana/pkg/services/dashboards/manager"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/licensing"
 	"github.com/grafana/grafana/pkg/setting"
@@ -36,6 +33,7 @@ import (
 	"github.com/grafana/grafana/pkg/tsdb/prometheus"
 	"github.com/grafana/grafana/pkg/tsdb/tempo"
 	"github.com/grafana/grafana/pkg/tsdb/testdatasource"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
