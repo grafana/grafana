de8fe85440f8c8e253da710caf874585556b2d35
jguer <joao.guerreiro@grafana.com>
2022-02-03 18:06:49 +0100

    APIKeys: api keys can still be used even when service accounts are enabled

--- pkg/api/apikey.go
+++ pkg/api/apikey.go
@@ -11,7 +11,6 @@ import (
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/components/apikeygen"
 	"github.com/grafana/grafana/pkg/models"
-	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/web"
 )
 
@@ -83,11 +82,6 @@ func (hs *HTTPServer) AddAPIKey(c *models.ReqContext) response.Response {
 		}
 	}
 	cmd.OrgId = c.OrgId
-	var err error
-	if hs.Features.IsEnabled(featuremgmt.FlagServiceAccounts) {
-		// Api keys should now be created with addadditionalapikey endpoint
-		return response.Error(400, "API keys should now be added via the AdditionalAPIKey endpoint.", err)
-	}
 
 	newKeyInfo, err := apikeygen.New(cmd.OrgId, cmd.Name)
 	if err != nil {
