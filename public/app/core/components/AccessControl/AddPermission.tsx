c0cec8ad92b80a0c0904c248327093a1d8f1b32b
Gabriel MABILLE <gamab@users.noreply.github.com>
2022-02-03 16:28:13 +0100

    Update public/app/core/components/AccessControl/AddPermission.tsx
    Co-authored-by: Ieva <ieva.vasiljeva@grafana.com>

--- public/app/core/components/AccessControl/AddPermission.tsx
+++ public/app/core/components/AccessControl/AddPermission.tsx
@@ -57,7 +57,7 @@ export const AddPermission = ({
   const renderMissingListUserRights = () => {
     return (
       <Alert severity="info" title="Missing permission">
-        You are missing permissions to list users. Please contact your administrator to get this resolved.
+        You are missing the permission to list users (org.users:read). Please contact your administrator to get this resolved.
       </Alert>
     );
   };
