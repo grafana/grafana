de09805fa476e6d2b332fe9cc26887127fa1dc24
Ashley Harrison <ashley.harrison@grafana.com>
2022-02-03 13:45:02 +0000

    Split console messages into log/warn/error

--- .betterer.ts
+++ .betterer.ts
@@ -4,18 +4,33 @@ import { smaller } from '@betterer/constraints';
 import { readFileSync } from 'node:fs';
 
 export default {
-  'number of console messages when running tests should shrink': () =>
+  'number of console logs when running tests should shrink': () =>
     new BettererTest({
-      test: () => getConsoleMessagesInTestOutput(),
+      test: () => getConsoleMessagesInTestOutput('log'),
+      constraint: smaller,
+      goal: 0
+    }),
+  'number of console warnings when running tests should shrink': () =>
+    new BettererTest({
+      test: () => getConsoleMessagesInTestOutput('warn'),
+      constraint: smaller,
+      goal: 0
+    }),
+  'number of console errors when running tests should shrink': () =>
+    new BettererTest({
+      test: () => getConsoleMessagesInTestOutput('error'),
       constraint: smaller,
       goal: 0
     })
 };
 
-function getConsoleMessagesInTestOutput(): number {
+type ConsoleMessageType = 'error' | 'log' | 'warn';
+
+function getConsoleMessagesInTestOutput(type: ConsoleMessageType): number {
   const file = readFileSync('./jestResults.log', { encoding: 'utf8' });
-  let lines = file.split('\n') // get each line
+  const lines = file.split('\n') // get each line
+  const regex = new RegExp(`^\\s*console.${type}$`);
   // filter each line to get the ones that start with console.error/log/warn
-  let linesContainingConsole = lines.filter(line => /^\s*console.(error|log|warn)$/.test(line))
+  let linesContainingConsole = lines.filter(line => regex.test(line))
   return linesContainingConsole.length;
 }
