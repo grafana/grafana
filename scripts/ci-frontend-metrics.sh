931bab6d00f64f24c5fc6cf9c777203029240e7f
Ashley Harrison <ashley.harrison@grafana.com>
2022-02-08 12:07:08 +0000

    update frontend metrics script

--- scripts/ci-frontend-metrics.sh
+++ scripts/ci-frontend-metrics.sh
@@ -8,7 +8,7 @@ CONTROLLERS="$(grep -r -oP 'class .*Ctrl' public/app/ | wc -l)"
 STORIES_COUNT="$(find ./packages/grafana-ui/src/components -name "*.story.tsx" | wc -l)"
 MDX_COUNT="$(find ./packages/grafana-ui/src/components -name "*.mdx" | wc -l)"
 LEGACY_FORMS="$(grep -r -oP 'LegacyForms;' public/app | wc -l)"
-ENZYME_TEST_COUNT="$(grep -l "from 'enzyme'" (packages|public)/**/*.(test|spec).* | wc -l)"
+ENZYME_TEST_COUNT="$(grep -l -R --include="*.test.*" "from 'enzyme'" public packages | wc -l)"
 
 STRICT_LINT_RESULTS="$(yarn run eslint --rule '@typescript-eslint/no-explicit-any: ["error"]' --format unix --ext .ts,.tsx ./public || true)"
 STRICT_LINT_EXPLICIT_ANY="$(echo "${STRICT_LINT_RESULTS}" | grep -o "no-explicit-any" | wc -l)"
