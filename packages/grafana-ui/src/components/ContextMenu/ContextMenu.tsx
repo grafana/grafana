1bdbc3abdf0993b1ee22de954b2db7dcd5845480
Ryan McKinley <ryantxu@gmail.com>
2022-02-09 11:43:08 -0800

    ContextMenu: make focus on open optional (#45170)

--- packages/grafana-ui/src/components/ContextMenu/ContextMenu.tsx
+++ packages/grafana-ui/src/components/ContextMenu/ContextMenu.tsx
@@ -11,6 +11,8 @@ export interface ContextMenuProps {
   y: number;
   /** Callback for closing the menu */
   onClose?: () => void;
+  /** On menu open focus the first element */
+  focusOnOpen?: boolean;
   /** RenderProp function that returns menu items to display */
   renderMenuItems?: () => React.ReactNode;
   /** A function that returns header element */
@@ -18,7 +20,7 @@ export interface ContextMenuProps {
 }
 
 export const ContextMenu: React.FC<ContextMenuProps> = React.memo(
-  ({ x, y, onClose, renderMenuItems, renderHeader }) => {
+  ({ x, y, onClose, focusOnOpen = true, renderMenuItems, renderHeader }) => {
     const menuRef = useRef<HTMLDivElement>(null);
     const [positionStyles, setPositionStyles] = useState({});
 
@@ -46,7 +48,9 @@ export const ContextMenu: React.FC<ContextMenuProps> = React.memo(
     const header = renderHeader?.();
     const menuItems = renderMenuItems?.();
     const onOpen = (setFocusedItem: (a: number) => void) => {
-      setFocusedItem(0);
+      if (focusOnOpen) {
+        setFocusedItem(0);
+      }
     };
     const onKeyDown = (e: React.KeyboardEvent) => {
       if (e.key === 'Escape') {
