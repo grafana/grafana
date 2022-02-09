fe1094c2b93fdba1a391d91849534e0823da962e
Kristina Durivage <kristina.durivage@grafana.com>
2022-02-09 15:27:07 -0600

    Add tests

--- e2e/various-suite/explore.spec.ts
+++ e2e/various-suite/explore.spec.ts
@@ -1,4 +1,5 @@
 import { e2e } from '@grafana/e2e';
+import { urlUtil } from '../../packages/grafana-data/src/utils/url';
 
 e2e.scenario({
   describeName: 'Explore',
@@ -18,6 +19,27 @@ e2e.scenario({
       });
 
     cy.contains('CSV Metric Values').scrollIntoView().should('be.visible').click();
+
+    cy.location().then((loc) => {
+      const parsedUrl = urlUtil.parseKeyValue(loc.search);
+      const leftJSON = JSON.parse(parsedUrl.left);
+      expect(leftJSON.range.to).to.equal('now');
+      expect(leftJSON.range.from).to.equal('now-1h');
+
+      cy.get('body').click();
+      cy.get('body').type('t{leftarrow}');
+
+      cy.location().then((locPostKeypress) => {
+        const parsedUrl = urlUtil.parseKeyValue(locPostKeypress.search);
+        console.log(parsedUrl);
+        const leftJSON = JSON.parse(parsedUrl.left);
+        // be sure the keypress affected the time window
+        expect(leftJSON.range.to).to.not.equal('now');
+        expect(leftJSON.range.from).to.not.equal('now-1h');
+        // be sure the url does not contain dashboard range values
+        expect(parsedUrl).to.not.have.keys('to', 'from');
+      });
+    });
 
     const canvases = e2e().get('canvas');
     canvases.should('have.length', 1);
