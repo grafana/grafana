81f9a68f8af5144fa61350b2af94184c2f59f1ba
Ivana Huckova <ivana.huckova@gmail.com>
2022-02-11 14:34:08 +0100

    Use replace 5m with  in rate queries

--- public/app/plugins/datasource/prometheus/datasource.ts
+++ public/app/plugins/datasource/prometheus/datasource.ts
@@ -904,11 +904,11 @@ export class PrometheusDatasource
         break;
       }
       case 'ADD_HISTOGRAM_QUANTILE': {
-        expression = `histogram_quantile(0.95, sum(rate(${expression}[5m])) by (le))`;
+        expression = `histogram_quantile(0.95, sum(rate(${expression}[$__rate_interval])) by (le))`;
         break;
       }
       case 'ADD_RATE': {
-        expression = `rate(${expression}[5m])`;
+        expression = `rate(${expression}[$__rate_interval])`;
         break;
       }
       case 'ADD_SUM': {
