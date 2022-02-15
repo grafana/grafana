edbccb2beb5a999b20606f30cfda32bbf4bf313b
gamab <gabi.mabs@gmail.com>
2022-02-15 09:31:22 +0100

    AccessControl: Trigger permission reload with team removal

--- public/app/features/teams/TeamList.tsx
+++ public/app/features/teams/TeamList.tsx
@@ -42,10 +42,7 @@ export class TeamList extends PureComponent<Props, State> {
   }
 
   componentDidMount() {
-    // Don't fetch teams if the user cannot see any
-    if (contextSrv.hasAccess(AccessControlAction.ActionTeamsRead, true)) {
-      this.fetchTeams();
-    }
+    this.fetchTeams();
     if (contextSrv.licensedAccessControlEnabled() && contextSrv.hasPermission(AccessControlAction.ActionRolesList)) {
       this.fetchRoleOptions();
     }
@@ -198,10 +195,8 @@ export class TeamList extends PureComponent<Props, State> {
 
   renderList() {
     const { teamsCount, hasFetched } = this.props;
-    // If the user cannot read any team, we didn't fetch them
-    let isLoading = !hasFetched && contextSrv.hasAccess(AccessControlAction.ActionTeamsRead, true);
 
-    if (isLoading) {
+    if (!hasFetched) {
       return null;
     }
 
@@ -214,12 +209,10 @@ export class TeamList extends PureComponent<Props, State> {
 
   render() {
     const { hasFetched, navModel } = this.props;
-    // If the user cannot read any team, we didn't fetch them
-    let isLoading = !hasFetched && contextSrv.hasAccess(AccessControlAction.ActionTeamsRead, true);
 
     return (
       <Page navModel={navModel}>
-        <Page.Contents isLoading={isLoading}>{this.renderList()}</Page.Contents>
+        <Page.Contents isLoading={!hasFetched}>{this.renderList()}</Page.Contents>
       </Page>
     );
   }
