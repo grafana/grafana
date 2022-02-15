9206c983cbc01f10aa6dfc8faf9a315899ca9baa
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-02-15 09:15:06 +0000

    parseFloatSeries

--- pkg/tsdb/influxdb/response_parser.go
+++ pkg/tsdb/influxdb/response_parser.go
@@ -56,16 +56,16 @@ func parseJSON(buf io.ReadCloser) (Response, error) {
 func transformRows(rows []Row, query Query) data.Frames {
 	frames := data.Frames{}
 	for _, row := range rows {
-		for columnIndex, column := range row.Columns {
+		for colIndex, column := range row.Columns {
 			if column == "time" {
 				continue
 			}
 
 			var timeArray []time.Time
-			var valType = typeof(row.Values[0][columnIndex])
-			var floatArray []*float64
+			var valType = typeof(row.Values[0][colIndex])
 			var stringArray []string
 			var boolArray []bool
+			name := formatFrameName(row, column, query)
 
 			for _, valuePair := range row.Values {
 				timestamp, timestampErr := parseTimestamp(valuePair[0])
@@ -73,30 +73,25 @@ func transformRows(rows []Row, query Query) data.Frames {
 				if timestampErr == nil {
 					timeArray = append(timeArray, timestamp)
 					if valType == "string" {
-						value := valuePair[columnIndex].(string)
+						value := valuePair[colIndex].(string)
 						stringArray = append(stringArray, value)
-					} else if valType == "json.Number" {
-						value := parseNumber(valuePair[columnIndex])
-						floatArray = append(floatArray, value)
 					} else if valType == "bool" {
-						value := valuePair[columnIndex].(bool)
+						value := valuePair[colIndex].(bool)
 						boolArray = append(boolArray, value)
 					}
 				}
 			}
-			name := formatFrameName(row, column, query)
-
-			timeField := data.NewField("time", nil, timeArray)
 
 			if valType == "string" {
+				timeField := data.NewField("time", nil, timeArray)
 				valueField := data.NewField("value", row.Tags, stringArray)
 				valueField.SetConfig(&data.FieldConfig{DisplayNameFromDS: name})
 				frames = append(frames, newDataFrame(name, query.RawQuery, timeField, valueField))
 			} else if valType == "json.Number" {
-				valueField := data.NewField("value", row.Tags, floatArray)
-				valueField.SetConfig(&data.FieldConfig{DisplayNameFromDS: name})
-				frames = append(frames, newDataFrame(name, query.RawQuery, timeField, valueField))
+				var fields = parseFloatSeries(row, colIndex, name)
+				frames = append(frames, newDataFrame(name, query.RawQuery, fields[0], fields[1]))
 			} else if valType == "bool" {
+				timeField := data.NewField("time", nil, timeArray)
 				valueField := data.NewField("value", row.Tags, boolArray)
 				valueField.SetConfig(&data.FieldConfig{DisplayNameFromDS: name})
 				frames = append(frames, newDataFrame(name, query.RawQuery, timeField, valueField))
@@ -105,6 +100,21 @@ func transformRows(rows []Row, query Query) data.Frames {
 	}
 
 	return frames
+}
+
+func parseFloatSeries(row Row, colIndex int, name string) []*data.Field {
+	timeArray := make([]time.Time, len(row.Values))
+	floatArray := make([]*float64, len(row.Values))
+	for i, valuePair := range row.Values {
+		if timestamp, err := parseTimestamp(valuePair[0]); err == nil {
+			timeArray[i] = timestamp
+			floatArray[i] = parseNumber(valuePair[colIndex])
+		}
+	}
+	timeField := data.NewField("time", nil, timeArray)
+	valueField := data.NewField("value", row.Tags, floatArray)
+	valueField.SetConfig(&data.FieldConfig{DisplayNameFromDS: name})
+	return []*data.Field{timeField, valueField}
 }
 
 func newDataFrame(name string, queryString string, timeField *data.Field, valueField *data.Field) *data.Frame {
