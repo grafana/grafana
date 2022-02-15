c98f372f5d4eb5e91e56d9d6d8ed88ae4392322d
Elfo404 <me@giordanoricci.com>
2022-02-15 12:23:47 +0000

    pick visualization based on query results

--- public/app/features/explore/hooks/useAddToDashboard.ts
+++ public/app/features/explore/hooks/useAddToDashboard.ts
@@ -6,7 +6,6 @@ import { getBackendSrv } from '@grafana/runtime';
 import { lastValueFrom } from 'rxjs';
 import { useCallback, useMemo } from 'react';
 import { getExploreItemSelector } from '../state/selectors';
-import { first } from 'lodash';
 export interface SaveToNewDashboardDTO {
   dashboardName: string;
   folder: { title: string; id: number };
@@ -33,7 +32,7 @@ const createDashboard = (dashboardName: string, folderId: number, queries: DataQ
   const dashboard = getDashboardSrv().create({ title: dashboardName }, { folderId });
 
   // TODO: type should be based on current visualization in explore
-  dashboard.addPanel({ targets: queries, type: 'table' });
+  dashboard.addPanel({ targets: queries, type: visualization });
 
   return lastValueFrom(createDashboardApiCall(dashboard.getSaveModelClone(), folderId));
 };
@@ -43,18 +42,47 @@ const isActive = (query: DataQuery) => !query.hide;
 export const useAddToDashboard = (exploreId: ExploreId) => {
   const exploreSelector = useMemo(() => getExploreItemSelector(exploreId), [exploreId]);
   const queries = useSelector((state: StoreState) => exploreSelector(state)?.queries || []);
+  const exploreFrames = useSelector((state: StoreState) => {
+    const exploreState = exploreSelector(state);
+    return {
+      graphFrames: exploreState?.queryResponse.graphFrames,
+      logsFrames: exploreState?.queryResponse.logsFrames,
+      traceFrames: exploreState?.queryResponse.traceFrames,
+      nodeGraphFrames: exploreState?.queryResponse.nodeGraphFrames,
+      tableFrames: exploreState?.queryResponse.tableFrames,
+    };
+  });
 
   return useCallback(
     async (data: SaveToNewDashboardDTO | SaveToExistingDashboardDTO) => {
       const activeQueries = queries.filter(isActive);
-      // to decide which panel to show we should pick the first refId from queries, then pick from graphFrames, logsFrame, traceFrames and nodeGraphFrames
+      // to decide which panel to show we should pick the first refId from queries, then pick from graphFrames, logsFrames, traceFrames and nodeGraphFrames
       // in the store and the first one that matches refId
-      const visualization = 'table';
+      let visualization = 'table';
+      // TODO: tidy this up, check for panel names and if they are available in dashboards
+      for (const { refId } of activeQueries) {
+        if (exploreFrames.graphFrames?.some((frame) => frame.refId === refId)) {
+          visualization = 'graph';
+          break;
+        }
+        if (exploreFrames.logsFrames?.some((frame) => frame.refId === refId)) {
+          visualization = 'logs';
+          break;
+        }
+        if (exploreFrames.nodeGraphFrames?.some((frame) => frame.refId === refId)) {
+          visualization = 'node-graph';
+          break;
+        }
+        if (exploreFrames.traceFrames?.some((frame) => frame.refId === refId)) {
+          visualization = 'trace';
+          break;
+        }
+      }
 
       // TODO: if create dashboard...
-      // @ts-expect-error
       return await createDashboard(data.dashboardName, data.folder.id, queries, visualization);
     },
-    [queries]
+    // FIXME: this will recompute on every render
+    [queries, exploreFrames]
   );
 };
