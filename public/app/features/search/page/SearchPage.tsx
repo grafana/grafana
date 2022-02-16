a45a4c36ce0b1d78a40b4b0fe2b09932168d3235
Ryan McKinley <ryantxu@gmail.com>
2022-02-16 10:02:46 -0800

    show dashboard and panel results

--- public/app/features/search/page/SearchPage.tsx
+++ public/app/features/search/page/SearchPage.tsx
@@ -1,4 +1,4 @@
-import { LoadingState, NavModelItem } from '@grafana/data';
+import { LoadingState, NavModelItem, DataFrame } from '@grafana/data';
 import { Input } from '@grafana/ui';
 import React, { useMemo, useState } from 'react';
 import Page from 'app/core/components/Page/Page';
@@ -18,10 +18,17 @@ export default function SearchPage() {
   const data = useAsync(getDashboardData, []);
   const [query, setQuery] = useState('');
   const filtered = useMemo(() => {
-    if (!data.value?.dashboards) {
-      return { length: 0, fields: [] };
+    if (!data.value?.dashboards.length) {
+      const empty: DataFrame = { length: 0, fields: [] };
+      return {
+        dashboards: empty,
+        panels: empty,
+      };
     }
-    return filterDataFrame(query, data.value.dashboards, 'Name', 'Description', 'Tags');
+    return {
+      dashboards: filterDataFrame(query, data.value.dashboards, 'Name', 'Description', 'Tags'),
+      panels: filterDataFrame(query, data.value.panels, 'Name', 'Description', 'Type'),
+    };
   }, [query, data]);
 
   if (!config.featureToggles.panelTitleSearch) {
@@ -32,24 +39,46 @@ export default function SearchPage() {
     <Page navModel={{ node: node, main: node }}>
       <Page.Contents>
         <Input value={query} onChange={(e) => setQuery(e.currentTarget.value)} autoFocus spellCheck={false} />
-
+        <br /> <br />
         {data.loading && <div>Loading....</div>}
         {!data.loading && (
           <div>
-            <AutoSizer style={{ width: '100%', height: '600px' }}>
+            <AutoSizer style={{ width: '100%', height: '800px' }}>
               {({ width, height }) => {
                 return (
                   <div>
-                    <PanelRenderer
-                      pluginId="table"
-                      title="search"
-                      data={{ series: [filtered], state: LoadingState.Done } as any}
-                      options={{}}
-                      width={width - 2} // ?????? otherwise it keeps growing!!!
-                      height={height}
-                      fieldConfig={{ defaults: {}, overrides: [] }}
-                      timeZone="browser"
-                    />
+                    {filtered.dashboards.length > 0 && (
+                      <>
+                        <h1>Dashboards ({filtered.dashboards.length})</h1>
+                        <PanelRenderer
+                          pluginId="table"
+                          title="Dashboards"
+                          data={{ series: [filtered.dashboards], state: LoadingState.Done } as any}
+                          options={{}}
+                          width={width - 2} // ?????? otherwise it keeps growing!!!
+                          height={300}
+                          fieldConfig={{ defaults: {}, overrides: [] }}
+                          timeZone="browser"
+                        />
+                        <br />
+                      </>
+                    )}
+
+                    {filtered.panels.length > 0 && (
+                      <>
+                        <h1>Panels ({filtered.panels.length})</h1>
+                        <PanelRenderer
+                          pluginId="table"
+                          title="Panels"
+                          data={{ series: [filtered.panels], state: LoadingState.Done } as any}
+                          options={{}}
+                          width={width - 2} // ?????? otherwise it keeps growing!!!
+                          height={300}
+                          fieldConfig={{ defaults: {}, overrides: [] }}
+                          timeZone="browser"
+                        />
+                      </>
+                    )}
                   </div>
                 );
               }}
