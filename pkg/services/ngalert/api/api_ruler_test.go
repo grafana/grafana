251f3ba5c2fbf0ca6ab49514f7452fe2661d2868
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2022-02-16 18:02:20 -0500

    more tests for calculateChanges

--- pkg/services/ngalert/api/api_ruler_test.go
+++ pkg/services/ngalert/api/api_ruler_test.go
@@ -2,130 +2,29 @@ package api
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"math/rand"
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 
+	models2 "github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/store"
 	"github.com/grafana/grafana/pkg/util"
 )
 
-func randomDbAlertRule(orgID int64) *models.AlertRule {
-	randNoDataState := func() models.NoDataState {
-		s := [...]models.NoDataState{
-			models.Alerting,
-			models.NoData,
-			models.OK,
-		}
-		return s[rand.Intn(len(s)-1)]
-	}
-
-	randErrState := func() models.ExecutionErrorState {
-		s := [...]models.ExecutionErrorState{
-			models.AlertingErrState,
-			models.ErrorErrState,
-			models.OkErrState,
-		}
-		return s[rand.Intn(len(s)-1)]
-	}
-
-	interval := (rand.Int63n(6) + 1) * 10
-	forInterval := time.Duration(interval*rand.Int63n(6)) * time.Second
-
-	var annotations map[string]string = nil
-	if rand.Int63()%2 == 0 {
-		qty := rand.Intn(5)
-		annotations = make(map[string]string, qty)
-		for i := 0; i < qty; i++ {
-			annotations[util.GenerateShortUID()] = util.GenerateShortUID()
-		}
-	}
-	var labels map[string]string = nil
-	if rand.Int63()%2 == 0 {
-		qty := rand.Intn(5)
-		labels = make(map[string]string, qty)
-		for i := 0; i < qty; i++ {
-			labels[util.GenerateShortUID()] = util.GenerateShortUID()
-		}
-	}
-
-	var dashUID *string = nil
-	var panelID *int64 = nil
-	if rand.Int63()%2 == 0 {
-		d := util.GenerateShortUID()
-		dashUID = &d
-		p := rand.Int63()
-		panelID = &p
-	}
-
-	return &models.AlertRule{
-		ID:        rand.Int63(),
-		OrgID:     orgID,
-		Title:     "TEST-ALERT-" + util.GenerateShortUID(),
-		Condition: "A",
-		Data: []models.AlertQuery{
-			{
-				DatasourceUID: "-100",
-				Model: json.RawMessage(`{
-			"datasourceUid": "-100",
-			"type":"math",
-			"expression":"2 + 1 < 1"
-		}`),
-				RelativeTimeRange: models.RelativeTimeRange{
-					From: models.Duration(5 * time.Hour),
-					To:   models.Duration(3 * time.Hour),
-				},
-				RefID: "A",
-			}},
-		Updated:         time.Now().Add(-time.Duration(rand.Intn(100) + 1)),
-		IntervalSeconds: rand.Int63n(60) + 1,
-		Version:         rand.Int63(),
-		UID:             util.GenerateShortUID(),
-		NamespaceUID:    util.GenerateShortUID(),
-		DashboardUID:    dashUID,
-		PanelID:         panelID,
-		RuleGroup:       "TEST-GROUP-" + util.GenerateShortUID(),
-		NoDataState:     randNoDataState(),
-		ExecErrState:    randErrState(),
-		For:             forInterval,
-		Annotations:     annotations,
-		Labels:          labels,
-	}
-}
-
-func randomSubmittedAlertRule(orgID int64) *models.AlertRule {
-	r := randomDbAlertRule(orgID)
-	r.ID = 0
-	return r
-}
-
-func randomAlertRules(orgID int64, count int, f func(orgID int64) *models.AlertRule) (map[string]*models.AlertRule, []*models.AlertRule) {
-	uIDs := make(map[string]*models.AlertRule, count)
-	result := make([]*models.AlertRule, 0, count)
-	for len(result) < count {
-		rule := f(orgID)
-		if _, ok := uIDs[rule.UID]; ok {
-			continue
-		}
-		result = append(result, rule)
-		uIDs[rule.UID] = rule
-	}
-	return uIDs, result
-}
-
 func TestCalculateChanges(t *testing.T) {
 	orgId := rand.Int63()
 
-	t.Run("should detect alerts that need to be added", func(t *testing.T) {
+	t.Run("detects alerts that need to be added", func(t *testing.T) {
 		fakeStore := store.NewFakeRuleStore(t)
 
 		namespace := randFolder()
 		groupName := util.GenerateShortUID()
-		submittedMap, submitted := randomAlertRules(orgId, rand.Intn(5)+1, randomSubmittedAlertRule)
+		submitted := models.GenerateAlertRules(rand.Intn(5)+1, models.AlertRuleGen(withOrgID(orgId), simulateSubmitted, withoutUID))
 
 		changes, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, submitted)
 		require.NoError(t, err)
@@ -135,14 +34,183 @@ func TestCalculateChanges(t *testing.T) {
 		require.Len(t, changes.Upsert, len(submitted))
 		for _, rule := range changes.Upsert {
 			require.Nil(t, rule.Existing)
-			expected, ok := submittedMap[rule.New.UID]
-			require.Truef(t, ok, "The upsert contained a rule that was not submitted")
-			require.Equal(t, *expected, rule.New)
+		}
+
+		opts := []cmp.Option{
+			cmp.FilterPath(func(path cmp.Path) bool {
+				return path.String() == "Data.modelProps"
+			}, cmp.Ignore()),
+		}
+
+	outerloop:
+		for _, expected := range submitted {
+			for _, rule := range changes.Upsert {
+				if cmp.Equal(*expected, rule.New, opts...) {
+					continue outerloop
+				}
+			}
+			require.Fail(t, "changes did not contain rule that was submitted")
 		}
 	})
 
-	// should add those that do not exist in db
-	// should delete those that exist in db but not in request
-	// should update those that exist in db (same group)
-	// should update those that exist but in different group
+	t.Run("detects alerts that need to be deleted", func(t *testing.T) {
+		namespace := randFolder()
+		groupName := util.GenerateShortUID()
+		inDatabaseMap, inDatabase := models.GenerateUniqueAlertRules(rand.Intn(5)+1, models.AlertRuleGen(withOrgID(orgId), withGroup(groupName), withNamespace(namespace)))
+
+		fakeStore := store.NewFakeRuleStore(t)
+		fakeStore.PutRule(context.Background(), inDatabase...)
+
+		changes, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, make([]*models.AlertRule, 0))
+		require.NoError(t, err)
+
+		require.Equal(t, 0, changes.newRules)
+		require.Len(t, changes.Upsert, 0)
+		require.Len(t, changes.Delete, len(inDatabaseMap))
+		for _, toDelete := range changes.Delete {
+			require.Contains(t, inDatabaseMap, toDelete.UID)
+			db := inDatabaseMap[toDelete.UID]
+			require.Equal(t, db, toDelete)
+		}
+	})
+
+	t.Run("should detect alerts that needs to be updated", func(t *testing.T) {
+		namespace := randFolder()
+		groupName := util.GenerateShortUID()
+		inDatabaseMap, inDatabase := models.GenerateUniqueAlertRules(rand.Intn(5)+1, models.AlertRuleGen(withOrgID(orgId), withGroup(groupName), withNamespace(namespace)))
+		submittedMap, submitted := models.GenerateUniqueAlertRules(len(inDatabase), models.AlertRuleGen(withOrgID(orgId), withGroup(groupName), withNamespace(namespace), withUIDs(inDatabaseMap)))
+
+		fakeStore := store.NewFakeRuleStore(t)
+		fakeStore.PutRule(context.Background(), inDatabase...)
+
+		changes, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, submitted)
+		require.NoError(t, err)
+
+		require.Len(t, changes.Upsert, len(inDatabase))
+		for _, upsert := range changes.Upsert {
+			require.NotNil(t, upsert.Existing)
+			require.Equal(t, upsert.Existing.UID, upsert.New.UID)
+			require.Equal(t, inDatabaseMap[upsert.Existing.UID], upsert.Existing)
+			require.Equal(t, *submittedMap[upsert.Existing.UID], upsert.New)
+		}
+		require.Len(t, changes.Delete, 0)
+		require.Equal(t, 0, changes.newRules)
+	})
+
+	t.Run("should be able to find alerts by UID in other group/namespace", func(t *testing.T) {
+		inDatabaseMap, inDatabase := models.GenerateUniqueAlertRules(rand.Intn(10)+10, models.AlertRuleGen(withOrgID(orgId)))
+
+		fakeStore := store.NewFakeRuleStore(t)
+		fakeStore.PutRule(context.Background(), inDatabase...)
+
+		namespace := randFolder()
+		groupName := util.GenerateShortUID()
+		submittedMap, submitted := models.GenerateUniqueAlertRules(rand.Intn(len(inDatabase)-5)+5, models.AlertRuleGen(withOrgID(orgId), withGroup(groupName), withNamespace(namespace), withUIDs(inDatabaseMap)))
+
+		changes, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, submitted)
+		require.NoError(t, err)
+
+		require.Len(t, changes.Delete, 0)
+		require.Equal(t, 0, changes.newRules)
+		require.Len(t, changes.Upsert, len(submitted))
+		for _, upsert := range changes.Upsert {
+			require.NotNil(t, upsert.Existing)
+			require.Equal(t, upsert.Existing.UID, upsert.New.UID)
+			require.Equal(t, inDatabaseMap[upsert.Existing.UID], upsert.Existing)
+			require.Equal(t, *submittedMap[upsert.Existing.UID], upsert.New)
+		}
+	})
+
+	t.Run("should fail when submitted rule has UID that does not exist in db", func(t *testing.T) {
+		fakeStore := store.NewFakeRuleStore(t)
+
+		namespace := randFolder()
+		groupName := util.GenerateShortUID()
+		submitted := models.AlertRuleGen(withOrgID(orgId), simulateSubmitted)()
+		require.NotEqual(t, "", submitted.UID)
+
+		_, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, []*models.AlertRule{submitted})
+		require.Error(t, err)
+	})
+
+	t.Run("should fail if cannot fetch current rules in the group", func(t *testing.T) {
+		fakeStore := store.NewFakeRuleStore(t)
+		expectedErr := errors.New("TEST ERROR")
+		fakeStore.Hook = func(cmd interface{}) error {
+			switch cmd.(type) {
+			case models.ListRuleGroupAlertRulesQuery:
+				return expectedErr
+			}
+			return nil
+		}
+
+		namespace := randFolder()
+		groupName := util.GenerateShortUID()
+		submitted := models.AlertRuleGen(withOrgID(orgId), simulateSubmitted, withoutUID)()
+
+		_, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, []*models.AlertRule{submitted})
+		require.ErrorIs(t, err, expectedErr)
+	})
+
+	t.Run("should fail if cannot fetch rule by UID", func(t *testing.T) {
+		fakeStore := store.NewFakeRuleStore(t)
+		expectedErr := errors.New("TEST ERROR")
+		fakeStore.Hook = func(cmd interface{}) error {
+			switch cmd.(type) {
+			case models.GetAlertRuleByUIDQuery:
+				return expectedErr
+			}
+			return nil
+		}
+
+		namespace := randFolder()
+		groupName := util.GenerateShortUID()
+		submitted := models.AlertRuleGen(withOrgID(orgId), simulateSubmitted)()
+
+		_, err := calculateChanges(context.Background(), fakeStore, orgId, namespace, groupName, []*models.AlertRule{submitted})
+		require.Error(t, err, expectedErr)
+	})
+}
+
+func withOrgID(orgId int64) func(rule *models.AlertRule) {
+	return func(rule *models.AlertRule) {
+		rule.OrgID = orgId
+	}
+}
+
+func withGroup(groupName string) func(rule *models.AlertRule) {
+	return func(rule *models.AlertRule) {
+		rule.RuleGroup = groupName
+	}
+}
+
+func withNamespace(namespace *models2.Folder) func(rule *models.AlertRule) {
+	return func(rule *models.AlertRule) {
+		rule.NamespaceUID = namespace.Uid
+	}
+}
+
+// simulateSubmitted resets some fields of the structure that are not populated by API model to model conversion
+func simulateSubmitted(rule *models.AlertRule) {
+	rule.ID = 0
+	rule.Version = 0
+	rule.Updated = time.Time{}
+}
+
+func withoutUID(rule *models.AlertRule) {
+	rule.UID = ""
+}
+
+func withUIDs(uids map[string]*models.AlertRule) func(rule *models.AlertRule) {
+	unused := make([]string, 0, len(uids))
+	for s := range uids {
+		unused = append(unused, s)
+	}
+	return func(rule *models.AlertRule) {
+		if len(unused) == 0 {
+			return
+		}
+		rule.UID = unused[0]
+		unused = unused[1:]
+	}
 }
