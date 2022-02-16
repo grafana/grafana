7bbe832a5a0a65067d62d37f92af87f21656b57d
yangkb09 <yangkb09@gmail.com>
2022-02-16 12:13:04 -0500

    Fix: Update refs of HasAdminPermissionInFolders

--- pkg/services/sqlstore/store.go
+++ pkg/services/sqlstore/store.go
@@ -157,4 +157,5 @@ type Store interface {
 	ExpireOldUserInvites(ctx context.Context, cmd *models.ExpireTempUsersCommand) error
 	GetDBHealthQuery(ctx context.Context, query *models.GetDBHealthQuery) error
 	SearchOrgs(ctx context.Context, query *models.SearchOrgsQuery) error
+	HasAdminPermissionInFolders(ctx context.Context, query *models.HasAdminPermissionInFoldersQuery) error
 }
