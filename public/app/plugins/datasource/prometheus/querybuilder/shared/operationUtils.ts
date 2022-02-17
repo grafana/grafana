75d80b02ac065b6beefa894cdaaa5574185a8389
Stephanie Closson <srclosson@gmail.com>
2022-02-17 07:43:08 -0400

    code review comments. Made range renderer common with code

--- public/app/plugins/datasource/prometheus/querybuilder/shared/operationUtils.ts
+++ public/app/plugins/datasource/prometheus/querybuilder/shared/operationUtils.ts
@@ -23,10 +23,11 @@ export function functionRendererRight(model: QueryBuilderOperation, def: QueryBu
   return str + params.join(', ') + ')';
 }
 
-export function rangeRendererRightWithParams(
+function rangeRendererWithParams(
   model: QueryBuilderOperation,
   def: QueryBuilderOperationDef,
-  innerExpr: string
+  innerExpr: string,
+  renderLeft: boolean
 ) {
   if (def.params.length < 2) {
     throw `Cannot render a function with params of length [${def.params.length}]`;
@@ -58,11 +59,19 @@ export function rangeRendererRightWithParams(
 
   // Frame the first paramater as a range vector
   if (innerExpr) {
-    params.unshift(`${innerExpr}[${rangeVector}]`);
+    renderLeft ? params.push(`${innerExpr}[${rangeVector}]`) : params.unshift(`${innerExpr}[${rangeVector}]`);
   }
 
   // stick everything together
   return str + params.join(', ') + ')';
+}
+
+export function rangeRendererRightWithParams(
+  model: QueryBuilderOperation,
+  def: QueryBuilderOperationDef,
+  innerExpr: string
+) {
+  return rangeRendererWithParams(model, def, innerExpr, false);
 }
 
 export function rangeRendererLeftWithParams(
@@ -70,41 +79,7 @@ export function rangeRendererLeftWithParams(
   def: QueryBuilderOperationDef,
   innerExpr: string
 ) {
-  if (def.params.length < 2) {
-    throw `Cannot render a function with params of length [${def.params.length}]`;
-  }
-
-  // First, make sure the first parameter (that is the range vector) is translated if the user selected 'auto'
-  let rangeVector = (model.params ?? [])[0] ?? 'auto';
-
-  if (rangeVector === 'auto') {
-    rangeVector = '$__rate_interval';
-  }
-
-  // Next frame the remaining parameters, but get rid of the first one because it's used to move the
-  // instant vector into a range vector.
-  const params = renderParams(
-    {
-      ...model,
-      params: model.params.slice(1),
-    },
-    {
-      ...def,
-      params: def.params.slice(1),
-      defaultParams: def.defaultParams.slice(1),
-    },
-    innerExpr
-  );
-
-  const str = model.id + '(';
-
-  // Frame the first paramater as a range vector
-  if (innerExpr) {
-    params.push(`${innerExpr}[${rangeVector}]`);
-  }
-
-  // stick everything together
-  return str + params.join(', ') + ')';
+  return rangeRendererWithParams(model, def, innerExpr, true);
 }
 
 function renderParams(model: QueryBuilderOperation, def: QueryBuilderOperationDef, innerExpr: string) {
