126496cd556863fca0d0a94940d5c4ec67204fcd
spinillos <selenepinillos@gmail.com>
2022-02-17 17:06:03 +0100

    Add missing argument

--- pkg/services/provisioning/dashboards/file_reader_linux_test.go
+++ pkg/services/provisioning/dashboards/file_reader_linux_test.go
@@ -25,7 +25,7 @@ func TestProvisionedSymlinkedFolder(t *testing.T) {
 		Options: map[string]interface{}{"path": symlinkedFolder},
 	}
 
-	reader, err := NewDashboardFileReader(cfg, log.New("test-logger"), nil)
+	reader, err := NewDashboardFileReader(cfg, log.New("test-logger"), nil, nil)
 	if err != nil {
 		t.Error("expected err to be nil")
 	}
