0502a84922853d9a5022b10d856dc094212a3f70
Peter Holmberg <peterholmberg@users.noreply.github.com>
2022-02-17 12:47:38 +0100

    Alerting: Add choice to external alertmanagers (#45157)
    * implement alertmanagersChoice
    
    * return empty array and remove non null assertion

--- public/app/features/alerting/unified/components/admin/ExternalAlertmanagers.tsx
+++ public/app/features/alerting/unified/components/admin/ExternalAlertmanagers.tsx
@@ -1,8 +1,18 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { css } from '@emotion/css';
+import { useDispatch, useSelector } from 'react-redux';
+import { css, cx } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
-import { Button, ConfirmModal, HorizontalGroup, Icon, Tooltip, useStyles2 } from '@grafana/ui';
+import {
+  Button,
+  ConfirmModal,
+  Field,
+  HorizontalGroup,
+  Icon,
+  RadioButtonGroup,
+  Tooltip,
+  useStyles2,
+  useTheme2,
+} from '@grafana/ui';
 import EmptyListCTA from 'app/core/components/EmptyListCTA/EmptyListCTA';
 import { AddAlertManagerModal } from './AddAlertManagerModal';
 import {
@@ -11,13 +21,25 @@ import {
   fetchExternalAlertmanagersConfigAction,
 } from '../../state/actions';
 import { useExternalAmSelector } from '../../hooks/useExternalAmSelector';
+import { StoreState } from 'app/types/store';
+
+const alertmanagerChoices = [
+  { value: 'internal', label: 'Only Internal' },
+  { value: 'external', label: 'Only External' },
+  { value: 'all', label: 'Both internal and external' },
+];
 
 export const ExternalAlertmanagers = () => {
   const styles = useStyles2(getStyles);
   const dispatch = useDispatch();
   const [modalState, setModalState] = useState({ open: false, payload: [{ url: '' }] });
   const [deleteModalState, setDeleteModalState] = useState({ open: false, index: 0 });
+
   const externalAlertManagers = useExternalAmSelector();
+  const alertmanagersChoice = useSelector(
+    (state: StoreState) => state.unifiedAlerting.externalAlertmanagers.alertmanagerConfig.result?.alertmanagersChoice
+  );
+  const theme = useTheme2();
 
   useEffect(() => {
     dispatch(fetchExternalAlertmanagersAction());
@@ -37,10 +59,12 @@ export const ExternalAlertmanagers = () => {
         .map((am) => {
           return am.url;
         });
-      dispatch(addExternalAlertmanagersAction(newList));
+      dispatch(
+        addExternalAlertmanagersAction({ alertmanagers: newList, alertmanagersChoice: alertmanagersChoice ?? 'all' })
+      );
       setDeleteModalState({ open: false, index: 0 });
     },
-    [externalAlertManagers, dispatch]
+    [externalAlertManagers, dispatch, alertmanagersChoice]
   );
 
   const onEdit = useCallback(() => {
@@ -70,16 +94,26 @@ export const ExternalAlertmanagers = () => {
     }));
   }, [setModalState]);
 
+  const onChangeAlertmanagerChoice = (alertmanagersChoice: string) => {
+    dispatch(
+      addExternalAlertmanagersAction({ alertmanagers: externalAlertManagers.map((am) => am.url), alertmanagersChoice })
+    );
+  };
+
+  const onChangeAlertmanagers = (alertmanagers: string[]) => {
+    dispatch(addExternalAlertmanagersAction({ alertmanagers, alertmanagersChoice: alertmanagersChoice ?? 'all' }));
+  };
+
   const getStatusColor = (status: string) => {
     switch (status) {
       case 'active':
-        return 'green';
+        return theme.colors.success.main;
 
       case 'pending':
-        return 'yellow';
+        return theme.colors.warning.main;
 
       default:
-        return 'red';
+        return theme.colors.error.main;
     }
   };
 
@@ -107,49 +141,63 @@ export const ExternalAlertmanagers = () => {
           buttonIcon="bell-slash"
         />
       ) : (
-        <table className="filter-table form-inline filter-table--hover">
-          <thead>
-            <tr>
-              <th>Url</th>
-              <th>Status</th>
-              <th style={{ width: '2%' }}>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {externalAlertManagers?.map((am, index) => {
-              return (
-                <tr key={index}>
-                  <td>
-                    <span className={styles.url}>{am.url}</span>
-                    {am.actualUrl ? (
-                      <Tooltip content={`Discovered ${am.actualUrl} from ${am.url}`} theme="info">
-                        <Icon name="info-circle" />
-                      </Tooltip>
-                    ) : null}
-                  </td>
-                  <td>
-                    <Icon name="heart" style={{ color: getStatusColor(am.status) }} title={am.status} />
-                  </td>
-                  <td>
-                    <HorizontalGroup>
-                      <Button variant="secondary" type="button" onClick={onEdit} aria-label="Edit alertmanager">
-                        <Icon name="pen" />
-                      </Button>
-                      <Button
-                        variant="destructive"
-                        aria-label="Remove alertmanager"
-                        type="button"
-                        onClick={() => setDeleteModalState({ open: true, index })}
-                      >
-                        <Icon name="trash-alt" />
-                      </Button>
-                    </HorizontalGroup>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <>
+          <table className={cx('filter-table form-inline filter-table--hover', styles.table)}>
+            <thead>
+              <tr>
+                <th>Url</th>
+                <th>Status</th>
+                <th style={{ width: '2%' }}>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {externalAlertManagers?.map((am, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <span className={styles.url}>{am.url}</span>
+                      {am.actualUrl ? (
+                        <Tooltip content={`Discovered ${am.actualUrl} from ${am.url}`} theme="info">
+                          <Icon name="info-circle" />
+                        </Tooltip>
+                      ) : null}
+                    </td>
+                    <td>
+                      <Icon name="heart" style={{ color: getStatusColor(am.status) }} title={am.status} />
+                    </td>
+                    <td>
+                      <HorizontalGroup>
+                        <Button variant="secondary" type="button" onClick={onEdit} aria-label="Edit alertmanager">
+                          <Icon name="pen" />
+                        </Button>
+                        <Button
+                          variant="destructive"
+                          aria-label="Remove alertmanager"
+                          type="button"
+                          onClick={() => setDeleteModalState({ open: true, index })}
+                        >
+                          <Icon name="trash-alt" />
+                        </Button>
+                      </HorizontalGroup>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div>
+            <Field
+              label="Send alerts to"
+              description="Sets which Alertmanager will handle your alerts. Internal (Grafana built in Alertmanager), External (All Alertmanagers configured above), or both."
+            >
+              <RadioButtonGroup
+                options={alertmanagerChoices}
+                value={alertmanagersChoice}
+                onChange={(value) => onChangeAlertmanagerChoice(value!)}
+              />
+            </Field>
+          </div>
+        </>
       )}
       <ConfirmModal
         isOpen={deleteModalState.open}
@@ -159,7 +207,13 @@ export const ExternalAlertmanagers = () => {
         onConfirm={() => onDelete(deleteModalState.index)}
         onDismiss={() => setDeleteModalState({ open: false, index: 0 })}
       />
-      {modalState.open && <AddAlertManagerModal onClose={onCloseModal} alertmanagers={modalState.payload} />}
+      {modalState.open && (
+        <AddAlertManagerModal
+          onClose={onCloseModal}
+          alertmanagers={modalState.payload}
+          onChangeAlertmanagerConfig={onChangeAlertmanagers}
+        />
+      )}
     </div>
   );
 };
@@ -176,5 +230,7 @@ const getStyles = (theme: GrafanaTheme2) => ({
     display: flex;
     justify-content: flex-end;
   `,
-  table: css``,
+  table: css`
+    margin-bottom: ${theme.spacing(2)};
+  `,
 });
