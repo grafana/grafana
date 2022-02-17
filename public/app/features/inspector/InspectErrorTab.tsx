6edaddaa615dd0dc82d31a054af247ff75b35206
yaellec <yaelle.chaudy@grafana.com>
2022-02-17 11:48:36 +0100

    moved catch to parseErrorMessage

--- public/app/features/inspector/InspectErrorTab.tsx
+++ public/app/features/inspector/InspectErrorTab.tsx
@@ -6,13 +6,17 @@ interface InspectErrorTabProps {
   error?: DataQueryError;
 }
 
-const parseErrorMessage = (message: string): { msg: string; json: any } => {
-  const [msg, json] = message.split(/(\{.+)/);
-  const jsonError = JSON.parse(json);
-  return {
-    msg,
-    json: jsonError,
-  };
+const parseErrorMessage = (message: string): { msg: string; json?: any } => {
+  try {
+    const [msg, json] = message.split(/(\{.+)/);
+    const jsonError = JSON.parse(json);
+    return {
+      msg,
+      json: jsonError,
+    };
+  } catch {
+    return { msg: message };
+  }
 };
 
 export const InspectErrorTab: React.FC<InspectErrorTabProps> = ({ error }) => {
@@ -28,16 +32,16 @@ export const InspectErrorTab: React.FC<InspectErrorTabProps> = ({ error }) => {
     );
   }
   if (error.message) {
-    try {
-      const { msg, json } = parseErrorMessage(error.message);
+    const { msg, json } = parseErrorMessage(error.message);
+    if (!json) {
+      return <div>{msg}</div>;
+    } else {
       return (
         <>
-          {msg && msg !== '' && <h3>{msg}</h3>}
-          {json && json !== '' && <JSONFormatter json={json} open={5} />}
+          {msg !== '' && <h3>{msg}</h3>}
+          <JSONFormatter json={json} open={5} />
         </>
       );
-    } catch {
-      return <div>{error.message}</div>;
     }
   }
   return <JSONFormatter json={error} open={2} />;
