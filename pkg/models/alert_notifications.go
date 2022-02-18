191ad5871ad3701acfd5134ae6bd04bbb2572111
Will Browne <will.browne@grafana.com>
2022-02-18 11:57:24 +0000

    remove unused field

--- pkg/models/alert_notifications.go
+++ pkg/models/alert_notifications.go
@@ -70,8 +70,7 @@ type UpdateAlertNotificationCommand struct {
 	Settings              *simplejson.Json  `json:"settings"  binding:"Required"`
 	SecureSettings        map[string]string `json:"secureSettings"`
 
-	OrgId                   int64             `json:"-"`
-	EncryptedSecureSettings map[string][]byte `json:"-"`
+	OrgId int64 `json:"-"`
 
 	Result *AlertNotification `json:"-"`
 }
