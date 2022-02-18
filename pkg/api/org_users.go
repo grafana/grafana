6042d7450bf3d736fe75def993ea2145185db89f
gamab <gabi.mabs@gmail.com>
2022-02-18 08:52:31 +0100

    add missing files

--- pkg/api/org_users.go
+++ pkg/api/org_users.go
@@ -150,7 +150,7 @@ func (hs *HTTPServer) getOrgUsersHelper(c *models.ReqContext, query *models.GetO
 		filteredUsers = append(filteredUsers, user)
 	}
 
-	accessControlMetadata := hs.getAllAccessControlMetadata(c, "users", userIDs)
+	accessControlMetadata := hs.getMultiAccessControlMetadata(c, "users", userIDs)
 	if len(accessControlMetadata) > 0 {
 		for i := range filteredUsers {
 			filteredUsers[i].AccessControl = accessControlMetadata[fmt.Sprint(filteredUsers[i].UserId)]
