de1ab82bd6afc7d8d440f82f7d19762938d4141f
kay delaney <kay@grafana.com>
2022-02-18 13:24:59 +0000

    add test for revokeInvite action

--- public/app/features/invites/state/reducers.test.ts
+++ public/app/features/invites/state/reducers.test.ts
@@ -1,24 +1,42 @@
+import { keyBy } from 'lodash';
 import { reducerTester } from '../../../../test/core/redux/reducerTester';
 import { initialState, invitesReducer } from './reducers';
-import { fetchInvitees } from './actions';
+import { fetchInvitees, revokeInvite } from './actions';
 import { getMockInvitees } from 'app/features/users/__mocks__/userMocks';
 
 describe('inviteesReducer', () => {
   describe('when fetchInvitees is dispatched', () => {
     it('then state should be correct', () => {
       const invitees = getMockInvitees(1);
-
       reducerTester<typeof initialState>()
         .givenReducer(invitesReducer, { ...initialState })
         .whenActionIsDispatched(fetchInvitees.fulfilled(invitees, ''))
         .thenStateShouldEqual({
-          entities: {
-            ...invitees.reduceRight<typeof initialState.entities>((prev, cur) => {
-              prev[cur.code] = cur;
-              return prev;
-            }, {}),
-          },
+          entities: keyBy(invitees, 'code'),
           ids: invitees.map((i) => i.code),
+          status: 'succeeded',
+        });
+    });
+  });
+
+  describe('when revokeInvite is dispatched', () => {
+    it('then state should be correct', () => {
+      const invitees = getMockInvitees(1);
+
+      const fakeInitialState: typeof initialState = {
+        entities: keyBy(invitees, 'code'),
+        ids: invitees.map((i) => i.code),
+        status: 'succeeded',
+      };
+
+      reducerTester<typeof initialState>()
+        .givenReducer(invitesReducer, fakeInitialState)
+        .whenActionIsDispatched(revokeInvite.fulfilled(invitees[0].code, '', ''))
+        .thenStateShouldEqual({
+          entities: {
+            [invitees[1].code]: invitees[1],
+          },
+          ids: [invitees[1].code],
           status: 'succeeded',
         });
     });
