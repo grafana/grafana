6256a0530b545e269e268f92336545341690847c
Ashley Harrison <ashley.harrison@grafana.com>
2022-02-22 10:41:32 +0000

    user essentials mob! :trident:

--- public/app/features/variables/editor/actions.test.ts
+++ public/app/features/variables/editor/actions.test.ts
@@ -1,5 +1,7 @@
 import { constantBuilder, customBuilder } from '../shared/testing/builders';
-import { getNextAvailableId } from './actions';
+import { getNextAvailableId, switchToNewMode } from './actions';
+import * as selectors from '../state/selectors';
+import { initialKeyedVariablesState, toKeyedAction } from '../state/keyedVariablesReducer';
 
 describe('getNextAvailableId', () => {
   describe('when called with a custom type and there is already 2 variables', () => {
@@ -13,5 +15,18 @@ describe('getNextAvailableId', () => {
 
       expect(result).toEqual('custom2');
     });
+  });
+});
+
+describe('switchToNewMode', () => {
+  it('should dispatch with the correct rootStateKey', () => {
+    jest.spyOn(selectors, 'getVariablesByKey').mockReturnValue([]);
+    jest.spyOn(selectors, 'getNewVariableIndex').mockReturnValue(0);
+    const mockGetState = jest.fn().mockReturnValue({ templating: initialKeyedVariablesState });
+    const mockDispatch = jest.fn();
+    switchToNewMode(null)(mockDispatch, mockGetState, undefined);
+    expect(mockDispatch).toHaveBeenCalledTimes(2);
+    expect(mockDispatch.mock.calls[0]).toEqual(toKeyedAction('null', null));
+    expect(mockDispatch.mock.calls[1]).toEqual(toKeyedAction('null', null));
   });
 });
