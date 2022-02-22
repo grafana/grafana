000e7346c0745076d72434b526dd593aaf31fc32
Connor Lindsey <cblindsey3@gmail.com>
2022-02-22 15:17:45 -0700

    Tracing: Add option to map tag names to log label names in trace to logs settings (#45178)
    * Add mapped tags input to allow renaming tags in trace to logs settings
    
    * Use mappedTags in createSpanLink
    
    * Update traceToLogs docs
    
    * Show 'add kv' button if no tags
    
    * Update docs
    
    * Default mappedTags to tag values
    
    (cherry picked from commit af2d19b02eb22769ca976e333765d9a6bfb94be0)

--- public/app/features/explore/TraceView/TraceView.tsx
+++ public/app/features/explore/TraceView/TraceView.tsx
@@ -20,7 +20,7 @@ import {
   TTraceTimeline,
   UIElementsContext,
 } from '@jaegertracing/jaeger-ui-components';
-import { TraceToLogsData } from 'app/core/components/TraceToLogsSettings';
+import { TraceToLogsData } from 'app/core/components/TraceToLogs/TraceToLogsSettings';
 import { getDatasourceSrv } from 'app/features/plugins/datasource_srv';
 import { getTimeZone } from 'app/features/profile/state/selectors';
 import { StoreState } from 'app/types';
