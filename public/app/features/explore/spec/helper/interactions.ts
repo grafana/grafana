9cc15af00dec9aebee1012648e7fb7e67da483b3
Piotr Jamróz <pm.jamroz@gmail.com>
2022-02-23 17:21:53 +0100

    Remove unused import

--- public/app/features/explore/spec/helper/interactions.ts
+++ public/app/features/explore/spec/helper/interactions.ts
@@ -1,6 +1,6 @@
 import { selectors } from '@grafana/e2e-selectors';
 import userEvent from '@testing-library/user-event';
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 export const changeDatasource = async (name: string) => {
   const datasourcePicker = (await screen.findByLabelText(selectors.components.DataSourcePicker.container)).children[0];
