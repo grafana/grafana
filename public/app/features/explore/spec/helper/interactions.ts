813f72d387779fb2cb56f02fda424e4b09004c1e
Piotr Jamróz <pm.jamroz@gmail.com>
2022-02-23 16:29:18 +0100

    Update public/app/features/explore/spec/helper/interactions.ts
    Co-authored-by: Giordano Ricci <me@giordanoricci.com>

--- public/app/features/explore/spec/helper/interactions.ts
+++ public/app/features/explore/spec/helper/interactions.ts
@@ -22,7 +22,5 @@ export const runQuery = () => {
 export const openQueryHistory = async () => {
   const button = screen.getByRole('button', { name: 'Rich history button' });
   userEvent.click(button);
-  await waitFor(() => {
-    expect(screen.getByText('The history is local to your browser and is not shared with others.')).toBeInTheDocument();
-  });
+  expect(await screen.findByText('The history is local to your browser and is not shared with others.')).toBeInTheDocument();
 };
