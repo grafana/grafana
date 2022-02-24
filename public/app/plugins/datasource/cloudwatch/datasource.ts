dfe16ef42965a2fbbb346bd7688e2372ad074279
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-02-24 12:55:44 +0100

    WIP for testDatasource

--- public/app/plugins/datasource/cloudwatch/datasource.ts
+++ public/app/plugins/datasource/cloudwatch/datasource.ts
@@ -55,7 +55,6 @@ import {
 import { CloudWatchLanguageProvider } from './language_provider';
 import { VariableWithMultiSupport } from 'app/features/variables/types';
 import { increasingInterval } from './utils/rxjs/increasingInterval';
-import { toTestingStatus } from '@grafana/runtime/src/utils/queryResponse';
 import { addDataLinksToLogsResponse } from './utils/datalinks';
 import { runWithRetry } from './utils/logsRetry';
 import { SQLCompletionItemProvider } from './cloudwatch-sql/completion/CompletionItemProvider';
@@ -877,15 +876,52 @@ export class CloudWatchDatasource
     const metricName = 'EstimatedCharges';
     const dimensions = {};
 
+    let queryTypes = ['logs', 'metrics'];
+    const dataQueryResponses: DataQueryResponse[] = [];
     try {
       await this.getDimensionValues(region ?? '', namespace, metricName, 'ServiceName', dimensions);
+    } catch (error) {
+      dataQueryResponses.push(toDataQueryResponse(error));
+      queryTypes.filter((str) => {
+        return str !== 'metrics';
+      });
+    }
+
+    try {
+      await this.makeLogActionRequest('DescribeLogGroups', []);
+    } catch (error) {
+      dataQueryResponses.push(toDataQueryResponse(error));
+      queryTypes.filter((str) => {
+        return str !== 'logs';
+      });
+    }
+
+    if (queryTypes.length) {
+      let workingTypes = '';
+      for (const type of queryTypes) {
+        workingTypes += ' ' + type;
+      }
       return {
         status: 'success',
-        message: 'Data source is working',
+        message: 'Data source is working for ' + workingTypes,
       };
-    } catch (error) {
-      return toTestingStatus(error);
     }
+
+    const errors: any = {};
+    // this is code from "toTestingStatus"
+    for (const resp of dataQueryResponses) {
+      if (resp.error?.data?.message) {
+        errors.message = resp.error.data.message;
+        errors.details = resp.error?.data?.error ? { message: resp.error.data.error } : undefined;
+      } else if (resp.error?.refId && resp.error?.message) {
+        errors.message = resp.error.message;
+      }
+    }
+    return {
+      status: 'error',
+      message: errors.message,
+      details: errors.details,
+    };
   }
 
   awsRequest(url: string, data: MetricRequest, headers: Record<string, any> = {}): Observable<TSDBResponse> {
