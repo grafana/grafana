568616976f5029976ba245e958b8986eaeb74f58
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2022-02-24 18:58:48 -0500

    create copy method for AlertRule in testing

--- pkg/services/ngalert/models/testing.go
+++ pkg/services/ngalert/models/testing.go
@@ -125,3 +125,59 @@ func GenerateAlertRules(count int, f func() *AlertRule) []*AlertRule {
 	}
 	return result
 }
+
+// CopyRule creates a deep copy of AlertRule
+func CopyRule(r *AlertRule) *AlertRule {
+	result := AlertRule{
+		ID:              r.ID,
+		OrgID:           r.OrgID,
+		Title:           r.Title,
+		Condition:       r.Condition,
+		Updated:         r.Updated,
+		IntervalSeconds: r.IntervalSeconds,
+		Version:         r.Version,
+		UID:             r.UID,
+		NamespaceUID:    r.NamespaceUID,
+		RuleGroup:       r.RuleGroup,
+		NoDataState:     r.NoDataState,
+		ExecErrState:    r.ExecErrState,
+		For:             r.For,
+	}
+
+	if r.DashboardUID != nil {
+		dash := *r.DashboardUID
+		result.DashboardUID = &dash
+	}
+	if r.PanelID != nil {
+		p := *r.PanelID
+		result.PanelID = &p
+	}
+
+	for _, d := range r.Data {
+		q := AlertQuery{
+			RefID:             d.RefID,
+			QueryType:         d.QueryType,
+			RelativeTimeRange: d.RelativeTimeRange,
+			DatasourceUID:     d.DatasourceUID,
+		}
+		q.Model = make([]byte, 0, cap(d.Model))
+		q.Model = append(q.Model, d.Model...)
+		result.Data = append(result.Data, q)
+	}
+
+	if r.Annotations != nil {
+		result.Annotations = make(map[string]string, len(r.Annotations))
+		for s, s2 := range r.Annotations {
+			result.Annotations[s] = s2
+		}
+	}
+
+	if r.Labels != nil {
+		result.Labels = make(map[string]string, len(r.Labels))
+		for s, s2 := range r.Labels {
+			result.Labels[s] = s2
+		}
+	}
+
+	return &result
+}
