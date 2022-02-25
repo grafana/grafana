8f4e6a78fa9957b9b7ce523cfe534bd3fc5b7929
Will Browne <will.browne@grafana.com>
2022-02-25 17:43:30 +0000

    tests passing

--- pkg/services/pluginsettings/service.go
+++ pkg/services/pluginsettings/service.go
@@ -22,6 +22,7 @@ type ServiceImpl struct {
 }
 
 type Service interface {
+	GetPluginSettings(ctx context.Context, orgID int64) ([]*models.PluginSettingInfoDTO, error)
 	GetPluginSettingById(ctx context.Context, query *models.GetPluginSettingByIdQuery) error
 	UpdatePluginSetting(ctx context.Context, cmd *models.UpdatePluginSettingCmd) error
 	UpdatePluginSettingVersion(ctx context.Context, cmd *models.UpdatePluginSettingVersionCmd) error
