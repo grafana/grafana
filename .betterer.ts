80130c97653eb16c8028a177a652671a499584c1
Ashley Harrison <ashley.harrison@grafana.com>
2022-02-25 11:02:49 +0000

    Add betterer tests to prevent adding more type assertions and explicit any's into the codebase

--- .betterer.ts
+++ .betterer.ts
@@ -1,5 +1,17 @@
 import { regexp } from '@betterer/regexp';
+import { eslint } from '@betterer/eslint';
 
 export default {
   'no enzyme tests': () => regexp(/from 'enzyme'/g).include('**/*.test.*'),
+  'no type assertions': () => eslint({
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'never',
+      },
+    ],
+  }).include('**/*.ts*'),
+  'no explicit any': () => eslint({
+    '@typescript-eslint/no-explicit-any': 'error'
+  }).include('**/*.ts*'),
 };
