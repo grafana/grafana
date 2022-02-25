5c39d30d1ad93e90e0bc7fbfc2b5ec0f04e4e1f5
Nathan Marrs <nathanielmarrs@gmail.com>
2022-02-25 13:17:23 -0800

    Update public/app/routes/routes.tsx

--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -400,7 +400,7 @@ export function getAppRoutes(): RouteDescriptor[] {
     {
       path: '/search',
       component: SafeDynamicImport(
-        () => import(/* webpackChunkName: "TestStuffPage"*/ 'app/features/search/page/SearchPage')
+        () => import(/* webpackChunkName: "SearchPage"*/ 'app/features/search/page/SearchPage')
       ),
     },
     {
