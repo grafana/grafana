640d7a3dcf28f401a3193c79b2f32d6e44475a96
gillesdemey <gilles.de.mey@gmail.com>
2022-02-28 12:57:45 +0100

    chore: use selectable card from grafana/ui

--- public/app/features/alerting/unified/components/rule-editor/rule-types/RuleType.tsx
+++ public/app/features/alerting/unified/components/rule-editor/rule-types/RuleType.tsx
@@ -1,5 +1,4 @@
-import { css, cx } from '@emotion/css';
-import { GrafanaTheme2 } from '@grafana/data';
+import { css } from '@emotion/css';
 import { Card, useStyles2 } from '@grafana/ui';
 import React, { FC, ReactNode } from 'react';
 
@@ -13,52 +12,24 @@ interface Props {
 
 const RuleType: FC<Props> = (props) => {
   const { name, description, image, selected = false, onClick } = props;
-  const styles = useStyles2((theme) => getStyles(theme, selected));
+  const styles = useStyles2(getStyles);
 
   return (
-    <Card className={cx(styles.wrapper, styles.selected)} onClick={onClick}>
+    <Card className={styles.wrapper} isSelected={selected} onClick={onClick}>
       <Card.Figure>
         <img src={image} />
       </Card.Figure>
-      <Card.Heading className={styles.heading}>
-        <div className={styles.headWrapper}>
-          <div className={styles.name}>{name}</div>
-          <div className={styles.radioButton}>
-            <input type="radio" checked={selected} />
-          </div>
-        </div>
-      </Card.Heading>
+      <Card.Heading>{name}</Card.Heading>
       <Card.Description>{description}</Card.Description>
     </Card>
   );
 };
 
-const getStyles = (theme: GrafanaTheme2, selected: boolean) => ({
+const getStyles = () => ({
   wrapper: css`
     max-width: 360px;
     cursor: pointer;
     user-select: none;
-  `,
-  selected:
-    selected &&
-    css`
-      outline: ${theme.v1.colors.bgBlue1} solid 2px;
-    `,
-  radioButton: css`
-    align-self: flex-end;
-  `,
-  headWrapper: css`
-    flex: 1;
-    display: flex;
-    flex-direction: row;
-  `,
-  heading: css`
-    > button {
-      width: 100%;
-    }
-  `,
-  name: css`
-    flex: 1;
   `,
 });
 
