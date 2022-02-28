901dae7d78960025ce452038b05fec4acec959cc
Kyle Brandt <kyle@grafana.com>
2022-02-28 09:27:39 -0500

    (WIP) separate test func

--- pkg/middleware/csrf_test.go
+++ pkg/middleware/csrf_test.go
@@ -10,24 +10,29 @@ import (
 )
 
 func TestMiddlewareCSRF(t *testing.T) {
+	rr := csrfScenario(t, "foo", "localhost", "notLocalhost")
+	spew.Dump(rr.Body)
+	require.Equal(t, rr.Code, http.StatusForbidden)
+}
+
+func csrfScenario(t *testing.T, cookieName, origin string, host string) *httptest.ResponseRecorder {
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	req.AddCookie(&http.Cookie{
-		Name: "foo",
+		Name: cookieName,
 	})
 
-	req.Header.Add("ORIGIN", "localhost")
-	req.Header.Add("HOST", "notLocalhost")
+	req.Header.Add("ORIGIN", origin)
+	req.Header.Add("HOST", host)
 
 	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
 
 	rr := httptest.NewRecorder()
-	handler := CSRF("foo")(testHandler)
+	handler := CSRF(cookieName)(testHandler)
 	handler.ServeHTTP(rr, req)
-	spew.Dump(rr.Body)
-	require.Equal(t, rr.Code, http.StatusForbidden)
+	return rr
 }
