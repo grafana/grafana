54837cb7ef6f72ecb85baaffa6f1ea83352fed01
Adela Almasan <adela.almasan@grafana.com>
2022-03-01 16:48:01 -0600

    Added onChange event;

--- packages/grafana-ui/src/components/OptionsUI/number.tsx
+++ packages/grafana-ui/src/components/OptionsUI/number.tsx
@@ -1,10 +1,5 @@
-import React, { useCallback, useRef } from 'react';
-import {
-  FieldConfigEditorProps,
-  toIntegerOrUndefined,
-  toFloatOrUndefined,
-  NumberFieldConfigSettings,
-} from '@grafana/data';
+import React, { useCallback, useState } from 'react';
+import { FieldConfigEditorProps, NumberFieldConfigSettings } from '@grafana/data';
 import { Input } from '../Input/Input';
 
 export const NumberValueEditor: React.FC<FieldConfigEditorProps<number, NumberFieldConfigSettings>> = ({
@@ -13,11 +8,38 @@ export const NumberValueEditor: React.FC<FieldConfigEditorProps<number, NumberFi
   item,
 }) => {
   const { settings } = item;
-  const valueRef = useRef<string | undefined>();
-  const onSafeChange = useCallback(
-    (value: string) => {
-      let num = settings?.integer ? toIntegerOrUndefined(value) : toFloatOrUndefined(value);
-      if (num !== undefined) {
+  const [inputValue, setInputValue] = useState(isNaN(value) ? '' : value.toString());
+
+  const onValueChange = useCallback(
+    (e: React.SyntheticEvent) => {
+      let v: number | undefined = +inputValue;
+      if (isNaN(v)) {
+        v = undefined;
+      }
+      if (e.hasOwnProperty('key')) {
+        // handling keyboard event
+        const evt = e as React.KeyboardEvent<HTMLInputElement>;
+        if (evt.key === 'Enter') {
+          onChange(v);
+        }
+      } else {
+        // handling form event
+        onChange(v);
+      }
+    },
+    [onChange, inputValue]
+  );
+
+  const onLocalChange = useCallback(
+    (e: React.SyntheticEvent<HTMLInputElement>) => {
+      let num = e.currentTarget.valueAsNumber;
+
+      if (isNaN(num)) {
+        const v = e.currentTarget.value;
+        if (v === '' || v === '-') {
+          setInputValue(v);
+        }
+      } else {
         if (settings?.min !== undefined && settings.min > num) {
           num = settings.min;
         }
@@ -25,44 +47,26 @@ export const NumberValueEditor: React.FC<FieldConfigEditorProps<number, NumberFi
         if (settings?.max !== undefined && settings.max > num) {
           num = settings.max;
         }
-      }
-
-      onChange(num);
-    },
-    [settings?.integer, settings?.min, settings?.max, onChange]
-  );
-
-  const onValueChange = useCallback(
-    (e: React.SyntheticEvent) => {
-      if (e.hasOwnProperty('key')) {
-        // handling keyboard event
-        const evt = e as React.KeyboardEvent<HTMLInputElement>;
-        valueRef.current = evt.currentTarget.value;
-        if (evt.key === 'Enter') {
-          onSafeChange(evt.currentTarget.value);
+        if (settings?.integer) {
+          num = Math.floor(num);
         }
-      } else {
-        // handling form event
-        const evt = e as React.FormEvent<HTMLInputElement>;
-        valueRef.current = evt.currentTarget.value;
-        onSafeChange(evt.currentTarget.value);
+        setInputValue(num.toString());
       }
     },
-    [onSafeChange]
+    [settings?.max, settings?.min, settings?.integer, setInputValue]
   );
 
-  const defaultValue = value === undefined || value === null || isNaN(value) ? '' : value.toString();
   return (
     <Input
-      defaultValue={defaultValue}
+      value={inputValue}
       min={settings?.min}
       max={settings?.max}
       type="number"
       step={settings?.step}
       placeholder={settings?.placeholder}
+      onChange={onLocalChange}
       onBlur={onValueChange}
       onKeyDown={onValueChange}
-      key={valueRef.current}
     />
   );
 };
