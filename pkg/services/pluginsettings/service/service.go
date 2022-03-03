62cbad5b5555328fcae354b7256de669f9c36b6b
Will Browne <will.browne@grafana.com>
2022-03-03 17:56:54 +0000

    remove bus and direct use of sqlStore

--- pkg/services/pluginsettings/service/service.go
+++ pkg/services/pluginsettings/service/service.go
@@ -5,38 +5,32 @@ import (
 	"sync"
 	"time"
 
-	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/pluginsettings"
 	"github.com/grafana/grafana/pkg/services/secrets"
 	"github.com/grafana/grafana/pkg/services/sqlstore"
 )
 
-func ProvideService(bus bus.Bus, store *sqlstore.SQLStore, secretsService secrets.Service) *Service {
+func ProvideService(store *sqlstore.SQLStore, secretsService secrets.Service) *Service {
 	s := &Service{
-		bus:            bus,
-		sqlStore:       store,
-		secretsService: secretsService,
-		logger:         log.New("pluginsettings"),
-		pluginSettingDecryptionCache: secureJSONDecryptionCache{
+		store: store,
+		decryptionCache: secureJSONDecryptionCache{
 			cache: make(map[int64]cachedDecryptedJSON),
 		},
+		secretsService: secretsService,
+		logger:         log.New("pluginsettings"),
 	}
-
-	s.bus.AddHandler(s.GetPluginSettingById)
-	s.bus.AddHandler(s.UpdatePluginSetting)
-	s.bus.AddHandler(s.UpdatePluginSettingVersion)
 
 	return s
 }
 
 type Service struct {
-	bus            bus.Bus
-	sqlStore       *sqlstore.SQLStore
-	secretsService secrets.Service
+	store           pluginsettings.Service
+	decryptionCache secureJSONDecryptionCache
 
-	logger                       log.Logger
-	pluginSettingDecryptionCache secureJSONDecryptionCache
+	secretsService secrets.Service
+	logger         log.Logger
 }
 
 type cachedDecryptedJSON struct {
@@ -50,11 +44,11 @@ type secureJSONDecryptionCache struct {
 }
 
 func (s *Service) GetPluginSettings(ctx context.Context, orgID int64) ([]*models.PluginSettingInfoDTO, error) {
-	return s.sqlStore.GetPluginSettings(ctx, orgID)
+	return s.store.GetPluginSettings(ctx, orgID)
 }
 
 func (s *Service) GetPluginSettingById(ctx context.Context, query *models.GetPluginSettingByIdQuery) error {
-	return s.sqlStore.GetPluginSettingById(ctx, query)
+	return s.store.GetPluginSettingById(ctx, query)
 }
 
 func (s *Service) UpdatePluginSetting(ctx context.Context, cmd *models.UpdatePluginSettingCmd) error {
@@ -64,18 +58,18 @@ func (s *Service) UpdatePluginSetting(ctx context.Context, cmd *models.UpdatePlu
 		return err
 	}
 
-	return s.sqlStore.UpdatePluginSetting(ctx, cmd)
+	return s.store.UpdatePluginSetting(ctx, cmd)
 }
 
 func (s *Service) UpdatePluginSettingVersion(ctx context.Context, cmd *models.UpdatePluginSettingVersionCmd) error {
-	return s.sqlStore.UpdatePluginSettingVersion(ctx, cmd)
+	return s.store.UpdatePluginSettingVersion(ctx, cmd)
 }
 
 func (s *Service) DecryptedValues(ps *models.PluginSetting) map[string]string {
-	s.pluginSettingDecryptionCache.Lock()
-	defer s.pluginSettingDecryptionCache.Unlock()
+	s.decryptionCache.Lock()
+	defer s.decryptionCache.Unlock()
 
-	if item, present := s.pluginSettingDecryptionCache.cache[ps.Id]; present && ps.Updated.Equal(item.updated) {
+	if item, present := s.decryptionCache.cache[ps.Id]; present && ps.Updated.Equal(item.updated) {
 		return item.json
 	}
 
@@ -85,7 +79,7 @@ func (s *Service) DecryptedValues(ps *models.PluginSetting) map[string]string {
 		return map[string]string{}
 	}
 
-	s.pluginSettingDecryptionCache.cache[ps.Id] = cachedDecryptedJSON{
+	s.decryptionCache.cache[ps.Id] = cachedDecryptedJSON{
 		updated: ps.Updated,
 		json:    json,
 	}
