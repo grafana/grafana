d752edd2b2af3755978bc09cb640e2efa0931fa2
Kyle Brandt <kyle@grafana.com>
2022-03-03 11:48:26 -0500

    Chore: Remove deprecated no-op option PreferServerCipherSuites (#46174)
    * Chore: Remove deprecated no-op option PreferServerCipherSuites
    from the tls configs.
    Per golang docs: "PreferServerCipherSuites is a legacy field and has no effect."

--- pkg/api/http_server.go
+++ pkg/api/http_server.go
@@ -383,8 +383,7 @@ func (hs *HTTPServer) configureHttps() error {
 	}
 
 	tlsCfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		PreferServerCipherSuites: true,
+		MinVersion: tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
@@ -424,8 +423,7 @@ func (hs *HTTPServer) configureHttp2() error {
 	}
 
 	tlsCfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		PreferServerCipherSuites: true,
+		MinVersion: tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_CHACHA20_POLY1305_SHA256,
 			tls.TLS_AES_128_GCM_SHA256,
