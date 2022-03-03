2efba0df43a07cc318ad35aa4155f8619a32bec2
Giordano Ricci <me@giordanoricci.com>
2022-03-03 08:54:06 +0000

    Explore: allow users to save Explore state to a new panel in a new dashboard (#45148)
    * Add Button & Form
    
    * Save to new dashboard
    
    * minor adjustements
    
    * move modal to a separate component
    
    * handle dashboard name related errors
    
    * pick visualization based on query results
    
    * lift state
    
    * fix types
    
    * Add Open & Close tests
    
    * Add submit test
    
    * add navigation tests
    
    * add tests for API errors
    
    * remove console log
    
    * create wrapper component for AddToDashboardButton
    
    * remove unused mapped prop
    
    * add wrapper test
    
    * rename isActive to isVisible
    
    * invert control over save & redirect logic
    
    * remove leftover commented code
    
    * cleanup setup parameters
    
    * reorganize code & improve tests
    
    * Remove option to add to existing dashboard
    
    * UI tweaks
    
    * disable button if no queries
    
    * Fix tests
    
    * better accessible tests
    
    * handle submission errors
    
    * improve addToDashboard types
    
    * use dashboardSrv' saveDashboard
    
    * remove leftover test helper
    
    * fix typo
    
    Co-authored-by: Piotr Jamróz <pm.jamroz@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kristina <kristina.durivage@grafana.com>
    
    Co-authored-by: Piotr Jamróz <pm.jamroz@gmail.com>
    Co-authored-by: Kristina <kristina.durivage@grafana.com>

---
+++ public/app/features/explore/AddToDashboard/AddToDashboardModal.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { DataQuery } from '@grafana/data';
+import { Alert, Button, Field, Input, InputControl, Modal } from '@grafana/ui';
+import { FolderPicker } from 'app/core/components/Select/FolderPicker';
+import { useForm } from 'react-hook-form';
+import { SaveToNewDashboardDTO } from './addToDashboard';
+
+export interface ErrorResponse {
+  status: string;
+  message?: string;
+}
+
+type FormDTO = SaveToNewDashboardDTO;
+
+interface Props {
+  onClose: () => void;
+  queries: DataQuery[];
+  visualization: string;
+  onSave: (data: FormDTO, redirect: boolean) => Promise<void | ErrorResponse>;
+}
+
+function withRedirect<T extends any[]>(fn: (redirect: boolean, ...args: T) => {}, redirect: boolean) {
+  return async (...args: T) => fn(redirect, ...args);
+}
+
+export const AddToDashboardModal = ({ onClose, queries, visualization, onSave }: Props) => {
+  const [submissionError, setSubmissionError] = useState<string>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<FormDTO>({ defaultValues: { queries, visualization } });
+
+  const onSubmit = async (withRedirect: boolean, data: FormDTO) => {
+    setSubmissionError(undefined);
+    const error = await onSave(data, withRedirect);
+
+    if (error) {
+      switch (error.status) {
+        case 'name-exists':
+        case 'empty-name':
+        case 'name-match':
+          // error.message should always be defined here
+          setError('dashboardName', { message: error.message ?? 'This field is invalid' });
+          break;
+        default:
+          setSubmissionError(
+            error.message ?? 'An unknown error occurred while saving the dashboard. Please try again.'
+          );
+      }
+    }
+  };
+
+  return (
+    <Modal title="Add panel to dashboard" onDismiss={onClose} isOpen>
+      <form>
+        <input type="hidden" {...register('queries')} />
+        <input type="hidden" {...register('visualization')} />
+
+        <p>Create a new dashboard and add a panel with explored queries.</p>
+
+        <Field
+          label="Dashboard name"
+          description="Choose the name of the new dashboard"
+          error={errors.dashboardName?.message}
+          invalid={!!errors.dashboardName}
+        >
+          <Input
+            id="dashboard_name"
+            {...register('dashboardName', {
+              shouldUnregister: true,
+              required: { value: true, message: 'This field is required' },
+            })}
+            // we set default value here instead of in useForm because this input will be unregistered when switching
+            // to "Existing Dashboard" and default values are not populated with manually registered
+            // inputs (ie. when switching back to "New Dashboard")
+            defaultValue="New dashboard (Explore)"
+          />
+        </Field>
+
+        <Field
+          label="Folder"
+          description="Select where the dashboard will be created"
+          error={errors.folderId?.message}
+          invalid={!!errors.folderId}
+        >
+          <InputControl
+            render={({ field: { ref, onChange, ...field } }) => (
+              <FolderPicker onChange={(e) => onChange(e.id)} {...field} enableCreateNew inputId="folder" />
+            )}
+            control={control}
+            name="folderId"
+            shouldUnregister
+            rules={{ required: { value: true, message: 'Select a valid folder to save your dashboard in' } }}
+          />
+        </Field>
+
+        {submissionError && (
+          <Alert severity="error" title="Unknown error">
+            {submissionError}
+          </Alert>
+        )}
+
+        <Modal.ButtonRow>
+          <Button type="reset" onClick={onClose} fill="outline" variant="secondary" disabled={isSubmitting}>
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            onClick={handleSubmit(withRedirect(onSubmit, false))}
+            variant="secondary"
+            icon="compass"
+            disabled={isSubmitting}
+          >
+            Save and keep exploring
+          </Button>
+          <Button
+            type="submit"
+            onClick={handleSubmit(withRedirect(onSubmit, true))}
+            variant="primary"
+            icon="plus"
+            disabled={isSubmitting}
+          >
+            Save and go to dashboard
+          </Button>
+        </Modal.ButtonRow>
+      </form>
+    </Modal>
+  );
+};
