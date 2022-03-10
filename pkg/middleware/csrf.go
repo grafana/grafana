3c9097d71a96e0e0aa6a00b6adbca4b685e4eb45
heoelri <heoelri@users.noreply.github.com>
2022-03-10 17:20:12 +0100

    Update csrf.go
    Cleanup

--- pkg/middleware/csrf.go
+++ pkg/middleware/csrf.go
@@ -41,20 +41,18 @@ func CSRF(loginCookieName string, logger log.Logger) func(http.Handler) http.Han
 				logger.Error("error parsing Origin header", "err", err)
 			}
 
-			// X-Forwarded-Host for (reverse) proxy scenarios
-			// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host
-			xfh, err := url.Parse(r.Header.Get("X-Forwarded-Host"))
-			if err != nil {
-				logger.Error("error parsing X-Forwarded-Host header", "err", err)
-			}
-
-			// X-Forwarded-Host is only used and checked when GF_USE_BEHIND_PROXY is set to 'true'
 			if UseBehindProxy := os.Getenv("GF_USE_BEHIND_PROXY"); UseBehindProxy == "true" {
+
+				// X-Forwarded-Host for (reverse) proxy scenarios
+				// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host
+				xfh, err := url.Parse(r.Header.Get("X-Forwarded-Host"))
+				if err != nil {
+					logger.Error("error parsing X-Forwarded-Host header", "err", err)
+				}
 
 				// Checking that the Host header is not empty AND that the the Origin header matches the Host OR X-Forwarded-Host matches the Origin
 				if err != nil || netAddr.Host == "" || ((origin.String() != "" && origin.Hostname() != netAddr.Host) && (xfh.String() != "" && xfh.String() != origin.Hostname())) {
-					errormsg := "XFH " + xfh.String() + " != Origin " + origin.Hostname()
-					http.Error(w, "origin not allowed - "+errormsg, http.StatusForbidden)
+					http.Error(w, "origin not allowed", http.StatusForbidden)
 					return
 				}
 
@@ -62,8 +60,7 @@ func CSRF(loginCookieName string, logger log.Logger) func(http.Handler) http.Han
 
 				// Checking that the Host header is not empty AND that the the Origin header matches the Host
 				if err != nil || netAddr.Host == "" || (origin.String() != "" && origin.Hostname() != netAddr.Host) {
-					errormsg := "Origin " + origin.Hostname() + "!= server host " + netAddr.Host
-					http.Error(w, "origin not allowed - "+errormsg, http.StatusForbidden)
+					http.Error(w, "origin not allowed", http.StatusForbidden)
 					return
 				}
 
