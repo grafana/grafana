6ff23e70eab92467ca25650bb68d8fedfd76236d
heoelri <heoelri@users.noreply.github.com>
2022-03-10 17:28:36 +0100

    Update csrf.go
    Address some linting issues

--- pkg/middleware/csrf.go
+++ pkg/middleware/csrf.go
@@ -35,37 +35,30 @@ func CSRF(loginCookieName string, logger log.Logger) func(http.Handler) http.Han
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-
 			origin, err := url.Parse(r.Header.Get("Origin"))
 			if err != nil {
 				logger.Error("error parsing Origin header", "err", err)
 			}
-
+			// env var GF_USE_BEHIND_PROXY controls if X-Forwarded-Host is used or not
 			if UseBehindProxy := os.Getenv("GF_USE_BEHIND_PROXY"); UseBehindProxy == "true" {
-
 				// X-Forwarded-Host for (reverse) proxy scenarios
 				// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host
 				xfh, err := url.Parse(r.Header.Get("X-Forwarded-Host"))
 				if err != nil {
 					logger.Error("error parsing X-Forwarded-Host header", "err", err)
 				}
-
 				// Checking that the Host header is not empty AND that the the Origin header matches the Host OR X-Forwarded-Host matches the Origin
 				if err != nil || netAddr.Host == "" || ((origin.String() != "" && origin.Hostname() != netAddr.Host) && (xfh.String() != "" && xfh.String() != origin.Hostname())) {
 					http.Error(w, "origin not allowed", http.StatusForbidden)
 					return
 				}
-
 			} else {
-
 				// Checking that the Host header is not empty AND that the the Origin header matches the Host
 				if err != nil || netAddr.Host == "" || (origin.String() != "" && origin.Hostname() != netAddr.Host) {
 					http.Error(w, "origin not allowed", http.StatusForbidden)
 					return
 				}
-
 			}
-
 			next.ServeHTTP(w, r)
 		})
 	}
