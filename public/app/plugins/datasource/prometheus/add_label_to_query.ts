bbbeb9190562194d23bfdba8a1f1d4d252912567
Andrej Ocenas <mr.ocenas@gmail.com>
2022-03-14 18:35:05 +0100

    Add comment

--- public/app/plugins/datasource/prometheus/add_label_to_query.ts
+++ public/app/plugins/datasource/prometheus/add_label_to_query.ts
@@ -6,6 +6,13 @@ import { PromVisualQuery } from './querybuilder/types';
 
 /**
  * Adds label filter to existing query. Useful for query modification for example for ad hoc filters.
+ *
+ * It uses PromQL parser to find instances of metric and labels, alters them and then splices them back into the query.
+ * Ideally we could use the parse -> change -> render is a simple 3 steps but right now building the visual query
+ * object does not support all possible queries.
+ *
+ * So instead this just operates on substrings of the query with labels and operates just on those. This makes this
+ * more robust and can alter even invalid queries, and preserves in general the query structure and whitespace.
  * @param query
  * @param key
  * @param value
