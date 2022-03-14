5d078ba874d3f1a7f80667ead808dac04263b358
Gilles De Mey <gilles.de.mey@gmail.com>
2022-03-14 11:32:17 +0100

    Alerting: adds support for federated rules (#46037) (#46520)
    (cherry picked from commit a9b1a964b0b9a6b64ed535cb3f1ed2836cc8f837)

--- public/app/features/alerting/unified/components/rules/RuleDetailsActionButtons.tsx
+++ public/app/features/alerting/unified/components/rules/RuleDetailsActionButtons.tsx
@@ -17,6 +17,7 @@ import { CombinedRule, RulesSource } from 'app/types/unified-alerting';
 import { getAlertmanagerByUid } from '../../utils/alertmanager';
 import { useStateHistoryModal } from '../../hooks/useStateHistoryModal';
 import { RulerGrafanaRuleDTO, RulerRuleDTO } from 'app/types/unified-alerting-dto';
+import { isFederatedRuleGroup } from '../../utils/rules';
 
 interface Props {
   rule: CombinedRule;
@@ -39,6 +40,7 @@ export const RuleDetailsActionButtons: FC<Props> = ({ rule, rulesSource }) => {
   const leftButtons: JSX.Element[] = [];
   const rightButtons: JSX.Element[] = [];
 
+  const isFederated = isFederatedRuleGroup(group);
   const { isEditable } = useIsRuleEditable(getRulesSourceName(rulesSource), rulerRule);
   const returnTo = location.pathname + location.search;
   const isViewMode = inViewMode(location.pathname);
@@ -67,7 +69,8 @@ export const RuleDetailsActionButtons: FC<Props> = ({ rule, rulesSource }) => {
   };
 
   // explore does not support grafana rule queries atm
-  if (isCloudRulesSource(rulesSource) && contextSrv.isEditor) {
+  // neither do "federated rules"
+  if (isCloudRulesSource(rulesSource) && contextSrv.isEditor && !isFederated) {
     leftButtons.push(
       <LinkButton
         className={style.button}
@@ -173,7 +176,7 @@ export const RuleDetailsActionButtons: FC<Props> = ({ rule, rulesSource }) => {
     );
   }
 
-  if (isEditable && rulerRule) {
+  if (isEditable && rulerRule && !isFederated) {
     const sourceName = getRulesSourceName(rulesSource);
     const identifier = ruleId.fromRulerRule(sourceName, namespace.name, group.name, rulerRule);
 
