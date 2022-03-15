7433d01fc6c70e7531270509e043faef76831d0b
Ryan McKinley <ryantxu@gmail.com>
2022-03-14 22:55:42 -0700

    save buttons

---
+++ public/app/features/dashboard/components/SaveDashboard/forms/SaveDashboardForm2.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+import { Button, Form, Modal, TextArea } from '@grafana/ui';
+import { selectors } from '@grafana/e2e-selectors';
+
+import { DashboardModel } from 'app/features/dashboard/state';
+import { SaveDashboardData, SaveDashboardOptions } from '../types';
+
+interface FormDTO {
+  message: string;
+}
+
+type Props = {
+  dashboard: DashboardModel; // original
+  saveModel: SaveDashboardData; // already cloned
+  onCancel: () => void;
+  onSuccess: () => void;
+  onSubmit?: (clone: any, options: SaveDashboardOptions, dashboard: DashboardModel) => Promise<any>;
+  options: SaveDashboardOptions;
+};
+
+export const SaveDashboardForm2 = ({ dashboard, saveModel, options, onSubmit, onCancel, onSuccess }: Props) => {
+  const [saving, setSaving] = useState(false);
+
+  return (
+    <Form
+      onSubmit={async (data: FormDTO) => {
+        if (!onSubmit) {
+          return;
+        }
+        setSaving(true);
+        const result = await onSubmit(saveModel.clone, options, dashboard);
+        if (result.status === 'success') {
+          if (options.saveVariables) {
+            dashboard.resetOriginalVariables();
+          }
+          if (options.saveTimerange) {
+            dashboard.resetOriginalTime();
+          }
+          onSuccess();
+        }
+        setSaving(false);
+      }}
+    >
+      {({ register, errors }) => (
+        <>
+          <div>
+            <TextArea {...register('message')} placeholder="Add a note to describe your changes." autoFocus rows={5} />
+          </div>
+
+          <Modal.ButtonRow>
+            <Button variant="secondary" onClick={onCancel} fill="outline">
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              disabled={!saveModel.hasChanges}
+              icon={saving ? 'fa fa-spinner' : undefined}
+              aria-label={selectors.pages.SaveDashboardModal.save}
+            >
+              {saving ? '' : 'Save'}
+            </Button>
+          </Modal.ButtonRow>
+
+          {!saveModel.hasChanges && <div>No changes to save</div>}
+        </>
+      )}
+    </Form>
+  );
+};
