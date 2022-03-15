6394d433d2ce1e047e8d636b8afe8c676bd0cb53
Karl Persson <kalle.persson@grafana.com>
2022-03-15 16:18:50 +0100

    Add permission actions and id scope

--- pkg/services/datasources/accesscontrol.go
+++ pkg/services/datasources/accesscontrol.go
@@ -3,17 +3,20 @@ package datasources
 import "github.com/grafana/grafana/pkg/services/accesscontrol"
 
 const (
-	ActionDatasourcesRead   = "datasources:read"
-	ActionDatasourcesQuery  = "datasources:query"
-	ActionDatasourcesCreate = "datasources:create"
-	ActionDatasourcesWrite  = "datasources:write"
-	ActionDatasourcesDelete = "datasources:delete"
-	ActionDatasourcesIDRead = "datasources.id:read"
+	ActionDatasourcesRead             = "datasources:read"
+	ActionDatasourcesQuery            = "datasources:query"
+	ActionDatasourcesCreate           = "datasources:create"
+	ActionDatasourcesWrite            = "datasources:write"
+	ActionDatasourcesDelete           = "datasources:delete"
+	ActionDatasourcesIDRead           = "datasources.id:read"
+	ActionDatasourcesPermissionsRead  = "datasources.permissions:read"
+	ActionDatasourcesPermissionsWrite = "datasources.permissions:write"
 
 	ScopeDatasourcesRoot = "datasources"
 )
 
 var (
+	ScopeDatasourceID        = accesscontrol.Scope("datasources", "id", accesscontrol.Parameter(":datasourceId"))
 	ScopeDatasourcesAll      = accesscontrol.GetResourceAllScope(ScopeDatasourcesRoot)
 	ScopeDatasourcesProvider = accesscontrol.NewScopeProvider(ScopeDatasourcesRoot)
 )
