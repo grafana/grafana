7e837a58f06dd4b2a0dc7bd19c3aa87b9a17caa0
Ryan McKinley <ryantxu@gmail.com>
2022-03-14 20:58:42 -0700

    update the color dimension

--- public/app/features/dimensions/color.ts
+++ public/app/features/dimensions/color.ts
@@ -1,4 +1,4 @@
-import { DataFrame, Field, GrafanaTheme2 } from '@grafana/data';
+import { DataFrame, Field, getDisplayProcessor, getFieldColorModeForField, GrafanaTheme2 } from '@grafana/data';
 import { ColorDimensionConfig, DimensionSupplier } from './types';
 import { findField, getLastNotNullFieldValue } from './utils';
 
@@ -20,7 +20,7 @@ export function getColorDimensionForField(
   theme: GrafanaTheme2
 ): DimensionSupplier<string> {
   if (!field) {
-    const v = theme.visualization.getColorByName(config.fixed) ?? 'grey';
+    const v = theme.visualization.getColorByName(config.fixed ?? 'grey');
     return {
       isAssumed: Boolean(config.field?.length) || !config.fixed,
       fixed: v,
@@ -29,14 +29,27 @@ export function getColorDimensionForField(
     };
   }
 
-  const getColor = (value: any): string => {
-    const disp = field.display!;
-    return disp(value).color ?? '#ccc';
-  };
+  // Use the expensive color calculation by value
+  const mode = getFieldColorModeForField(field);
+  if (mode.isByValue || field.config.mappings?.length) {
+    const disp = getDisplayProcessor({ field, theme });
+    const getColor = (value: any): string => {
+      return disp(value).color ?? '#ccc';
+    };
 
+    return {
+      field,
+      get: (index: number): string => getColor(field.values.get(index)),
+      value: () => getColor(getLastNotNullFieldValue(field)),
+    };
+  }
+
+  // Typically series or fixed color (does not depend on value)
+  const fixed = mode.getCalculator(field, theme)(0, 0);
   return {
+    fixed,
+    value: () => fixed,
+    get: (i) => fixed,
     field,
-    get: (index: number): string => getColor(field.values.get(index)),
-    value: () => getColor(getLastNotNullFieldValue(field)),
   };
 }
