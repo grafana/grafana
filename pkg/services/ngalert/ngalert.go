92716cb60297e11be5a089f38dbc486f960fdd25
Alexander Weaver <weaver.alex.d@gmail.com>
2022-03-15 11:48:42 -0500

    Alerting: Create abstraction for launching transactions and refactor existing transaction management to use it (#46216)
    * Remove InTransaction from RuleStore and make it its own interface
    
    * Ensure that ctx-based is clear from name
    
    * Resolve merge conflicts
    
    * Refactor tests to work in terms of the introduced abstraction rather than concrete dbstore

--- pkg/services/ngalert/ngalert.go
+++ pkg/services/ngalert/ngalert.go
@@ -144,6 +144,7 @@ func (ng *AlertNG) init() error {
 		DataProxy:            ng.DataProxy,
 		QuotaService:         ng.QuotaService,
 		SecretsService:       ng.SecretsService,
+		TransactionManager:   store,
 		InstanceStore:        store,
 		RuleStore:            store,
 		AlertingStore:        store,
