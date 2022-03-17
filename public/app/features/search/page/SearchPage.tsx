d6f8b820fd571db02f2e5c420e421f0dda4dc4e3
Ryan McKinley <ryantxu@gmail.com>
2022-03-17 15:51:47 -0700

    cleanup

--- public/app/features/search/page/SearchPage.tsx
+++ public/app/features/search/page/SearchPage.tsx
@@ -1,15 +1,14 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import { GrafanaTheme2, NavModelItem } from '@grafana/data';
-import { Input, useStyles2 } from '@grafana/ui';
+import { Input, useStyles2, Spinner } from '@grafana/ui';
 import { config } from '@grafana/runtime';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import { css } from '@emotion/css';
 
 import Page from 'app/core/components/Page/Page';
 import { SearchPageDashboards } from './SearchPageDashboards';
-import { loadResults } from './state/actions';
-import { StoreState } from 'app/types';
+import { useAsync } from 'react-use';
+import { getGrafanaSearcher } from '../service';
 
 const node: NavModelItem = {
   id: 'search',
@@ -19,20 +18,12 @@ const node: NavModelItem = {
 };
 
 export default function SearchPage() {
-  const dispatch = useDispatch();
   const styles = useStyles2(getStyles);
-
-  const results = useSelector((state: StoreState) => state.searchPage.data.results);
-
   const [query, setQuery] = useState('');
 
-  const loadDashboardResults = useCallback(async () => {
-    await dispatch(loadResults(query));
-  }, [query, dispatch]);
-
-  useEffect(() => {
-    loadDashboardResults();
-  }, [query, loadDashboardResults]);
+  const results = useAsync(() => {
+    return getGrafanaSearcher().search(query);
+  }, [query]);
 
   if (!config.featureToggles.panelTitleSearch) {
     return <div className={styles.unsupported}>Unsupported</div>;
@@ -43,19 +34,14 @@ export default function SearchPage() {
       <Page.Contents>
         <Input value={query} onChange={(e) => setQuery(e.currentTarget.value)} autoFocus spellCheck={false} />
         <br /> <br />
-        {!results && query && <div>Loading....</div>}
-        <div>
-          FIELDS: {results?.body.fields.length}
-          <br />
-          RESULT: {results?.body.length}
-        </div>
-        {results?.body && (
+        {results.loading && <Spinner />}
+        {results.value?.body && (
           <div>
             <AutoSizer style={{ width: '100%', height: '1000px' }}>
               {({ width }) => {
                 return (
                   <div>
-                    <SearchPageDashboards dashboards={results?.body!} width={width} />
+                    <SearchPageDashboards dashboards={results.value!.body} width={width} />
                   </div>
                 );
               }}
--- public/app/features/search/page/state/actions.ts
+++ public/app/features/search/page/state/actions.ts
@@ -1,27 +1,13 @@
 import { reduceField } from '@grafana/data';
 import { ThunkResult } from 'app/types';
-import { getRawIndexData, getFrontendGrafanaSearcher } from '../../service/frontend';
 import { fetchResults } from './reducers';
 
 export const loadResults = (query: string): ThunkResult<void> => {
   return async (dispatch) => {
-    const data = await getRawIndexData();
-    if (!data.dashboard) {
-      return;
-    }
-
-    const searcher = getFrontendGrafanaSearcher(data);
-    const results = await searcher.search(query);
-
-    // HACK avoid redux error!
-    results.body.fields.forEach((f) => {
-      reduceField({ field: f, reducers: ['min', 'max'] });
-    });
-
     return dispatch(
       fetchResults({
         data: {
-          results,
+          results: {} as any,
         },
       })
     );
