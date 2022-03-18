30c343aac55e04ad36b8dfd73b599bc6cfbb3fa4
Serge Zaitsev <serge.zaitsev@grafana.com>
2022-03-18 10:25:08 +0100

    make linter happy

--- pkg/services/guardian/provider.go
+++ pkg/services/guardian/provider.go
@@ -25,8 +25,8 @@ func InitGuardianWithStore(store sqlstore.Store) {
 	New = func(ctx context.Context, dashId int64, orgId int64, user *models.SignedInUser) DashboardGuardian {
 		return newDashboardGuardian(ctx, dashId, orgId, user, store)
 	}
-
 }
+
 func InitACLGuardianWithStore(store sqlstore.Store, ac accesscontrol.AccessControl, permissionsServices accesscontrol.PermissionsServices) {
 	New = func(ctx context.Context, dashId int64, orgId int64, user *models.SignedInUser) DashboardGuardian {
 		return NewAccessControlDashboardGuardian(ctx, dashId, user, store, ac, permissionsServices)
