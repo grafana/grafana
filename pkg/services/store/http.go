fedd2e8eb3a2510cd01079504e8a91dbd085ff7d
An Le <an.le@grafana.com>
2022-03-22 15:55:52 -0700

    modify response struct in service

--- pkg/services/store/http.go
+++ pkg/services/store/http.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"strings"
+
 	"github.com/grafana/grafana/pkg/api/response"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/web"
@@ -31,27 +33,30 @@ func (s *httpStorage) Upload(c *models.ReqContext) response.Response {
 		grafanaStorageLogger.Error("error in parsing form", err)
 		return response.Error(400, "error parsing form", err)
 	}
-	cmd, err := s.store.Upload(c.Req.Context(), c.SignedInUser, c.Req.MultipartForm)
+	res, err := s.store.Upload(c.Req.Context(), c.SignedInUser, c.Req.MultipartForm)
 
 	if err != nil {
 		return response.Error(400, "cannot call upload", err)
 	}
 
-	return response.JSON(200, map[string]string{
+	return response.JSON(res.statusCode, map[string]string{
 		"action": "upload",
-		"path":   cmd.Path,
+		"path":   res.path,
+		"file":   res.fileName,
 	})
 }
 
 func (s *httpStorage) Read(c *models.ReqContext) response.Response {
-	action := "Read"
-	scope, path := getPathAndScope(c)
-
-	return response.JSON(200, map[string]string{
-		"action": action,
-		"scope":  scope,
-		"path":   path,
-	})
+	// full path is api/storage/read/upload/example.jpg, but we only want the part after read
+	indexOfPath := strings.Index(c.Req.RequestURI, "read") + 4
+	path := c.Req.RequestURI[indexOfPath:]
+	file, err := s.store.Read(c.Req.Context(), c.SignedInUser, path)
+	grafanaStorageLogger.Info("path in read", "file path", path)
+	if err != nil {
+		return response.Error(400, "cannot call read", err)
+	}
+	c.Resp.Header().Set("Content-Type", file.MimeType)
+	return response.Respond(200, file.Contents)
 }
 
 func (s *httpStorage) Delete(c *models.ReqContext) response.Response {
