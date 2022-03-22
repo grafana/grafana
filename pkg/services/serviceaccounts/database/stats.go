2bcda152eadb554a23d2d673d2e77f8bc9e416a2
eleijonmarck <eric.leijonmarck@gmail.com>
2022-03-22 09:12:48 +0000

    fix: usage stats test

--- pkg/services/serviceaccounts/database/stats.go
+++ pkg/services/serviceaccounts/database/stats.go
@@ -15,7 +15,7 @@ func (s *ServiceAccountsStoreImpl) GetUsageMetrics(ctx context.Context) (map[str
 	sb.Write(`(SELECT COUNT(*) FROM ` + dialect.Quote("user") +
 		` WHERE is_service_account = ` + dialect.BooleanStr(true) + `) AS serviceaccounts,`)
 	sb.Write(`(SELECT COUNT(*) FROM ` + dialect.Quote("api_key") +
-		` WHERE service_account_id IS NOT NULL ) AS serviceaccount_tokens`)
+		` WHERE user_id IS NOT NULL ) AS serviceaccount_tokens`)
 
 	type saStats struct {
 		ServiceAccounts int64 `xorm:"serviceaccounts"`
