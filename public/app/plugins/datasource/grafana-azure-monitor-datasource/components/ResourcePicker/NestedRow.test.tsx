7a8193fa39de0dff3a4d8cea516d78622a732d88
Andres Martinez Gotor <andres.mgotor@gmail.com>
2022-03-22 16:01:16 +0100

    cloud-datasources mob! :trident:

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/ResourcePicker/NestedRow.test.tsx
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/ResourcePicker/NestedRow.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+
 import NestedRow from './NestedRow';
 import { ResourceRowType } from './types';
 
@@ -18,7 +19,39 @@ const defaultProps = {
 };
 
 describe('NestedRow', () => {
-  it('should be selectable', () => {
-    render(<NestedRow {...defaultProps} />);
+  it('should not display a checkbox when the type of row is empty', () => {
+    render(
+      <table>
+        <tbody>
+          <NestedRow {...defaultProps} />
+        </tbody>
+      </table>
+    );
+    const box = screen.queryByRole('checkbox');
+    expect(box).not.toBeInTheDocument();
+  });
+
+  it('should display a checkbox when the type of row is in selectableEntryTypes', () => {
+    render(
+      <table>
+        <tbody>
+          <NestedRow {...defaultProps} selectableEntryTypes={[ResourceRowType.Resource]} />
+        </tbody>
+      </table>
+    );
+    const box = screen.queryByRole('checkbox');
+    expect(box).toBeInTheDocument();
+  });
+
+  it('should not display a checkbox when the type of row is not in selectableEntryTypes', () => {
+    render(
+      <table>
+        <tbody>
+          <NestedRow {...defaultProps} selectableEntryTypes={[ResourceRowType.ResourceGroup]} />
+        </tbody>
+      </table>
+    );
+    const box = screen.queryByRole('checkbox');
+    expect(box).not.toBeInTheDocument();
   });
 });
