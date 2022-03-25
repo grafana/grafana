bab1d7a7f3d1c03f42da7b0d629017ef0ad267bd
Ryan McKinley <ryantxu@gmail.com>
2022-03-25 12:08:30 -0700

    add feature flag

---
+++ public/app/features/dashboard/components/SaveDashboard/SaveDashboardProxy.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SaveProvisionedDashboard } from './SaveProvisionedDashboard';
+import { SaveDashboardAsModal } from './SaveDashboardAsModal';
+import { SaveDashboardModalProps } from './types';
+import { SaveDashboardModal } from './SaveDashboardModal';
+import { config } from '@grafana/runtime';
+import { SaveDashboardDrawer } from './SaveDashboardDrawer';
+
+export const SaveDashboardProxy: React.FC<SaveDashboardModalProps> = ({
+  dashboard,
+  onDismiss,
+  onSaveSuccess,
+  isCopy,
+}) => {
+  if (config.featureToggles.saveDashboardDrawer) {
+    return (
+      <SaveDashboardDrawer dashboard={dashboard} onDismiss={onDismiss} onSaveSuccess={onSaveSuccess} isCopy={isCopy} />
+    );
+  }
+
+  const isProvisioned = dashboard.meta.provisioned;
+  const isNew = dashboard.version === 0;
+  const isChanged = dashboard.version > 0;
+
+  const modalProps = {
+    dashboard,
+    onDismiss,
+    onSaveSuccess,
+    isCopy,
+  };
+
+  return (
+    <>
+      {isChanged && !isProvisioned && <SaveDashboardModal {...modalProps} />}
+      {isProvisioned && <SaveProvisionedDashboard {...modalProps} />}
+      {isNew && <SaveDashboardAsModal {...modalProps} isNew />}
+    </>
+  );
+};
