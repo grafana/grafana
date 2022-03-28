4f2777b379f32d8337fb6d0a9097413e1fb0e4fe
Kristina Durivage <kristina.durivage@grafana.com>
2022-03-28 10:32:42 -0500

    WIP

--- public/app/features/commandPalette/CommandPalette.tsx
+++ public/app/features/commandPalette/CommandPalette.tsx
@@ -8,32 +8,36 @@ import {
   KBarSearch,
   useMatches,
   Action,
+  useRegisterActions,
+  useKBar,
 } from 'kbar';
 import getGlobalActions from './global.actions';
+import getDashboardNavActions from './dashboard.nav.actions';
 
 /**
  * Wrap all the components from KBar here.
  * @constructor
  */
+
 export const CommandPalette = () => {
-  /*  useEffect(() => {
-    const useGetAllGlobalActionsHook = async () => {
-      const actions = await getGlobalActions();
-      useRegisterActions(actions);
-    }
-    useGetAllGlobalActionsHook();
-  });*/
-  console.log('render command palette');
-  const [actions, setActions] = useState<Action[]>([]);
+  const [actions, setActions] = useState<Action[]>(getGlobalActions());
+  //const [actions, setActions] = useState<Action[]>([]);
+  const { query } = useKBar();
 
   useEffect(() => {
-    const loadActions = async () => {
-      const actionsResp = await getGlobalActions();
-      setActions(actionsResp);
+    const regDashboardActions = async () => {
+      //const staticActions = getGlobalActions();
+      const dashAct = await getDashboardNavActions();
+      console.log('add dashboard actions', query);
+      //setActions([...staticActions, dashAct]);
+      setActions([...actions, ...dashAct]);
+      return dashAct;
     };
-
-    loadActions();
+    regDashboardActions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useRegisterActions(!query ? [] : actions, [actions, query]);
 
   return (
     <KBarProvider actions={actions}>
--- public/app/features/commandPalette/global.actions.ts
+++ public/app/features/commandPalette/global.actions.ts
@@ -1,4 +1,4 @@
-import { Action, locationService, getBackendSrv } from '@grafana/runtime';
+import { Action, locationService } from '@grafana/runtime';
 
 const globalActions: Action[] = [
   {
@@ -96,29 +96,6 @@ const globalActions: Action[] = [
   },
 ];
 
-async function getDashboardNav(): Promise<Action[]> {
-  const data: Array<{ type: string; title: string; url: string }> = await getBackendSrv().get('/api/search');
-
-  const parentAction: Action = {
-    id: 'dashboards/go-to',
-    name: 'Go to dashboard',
-  };
-
-  const goToDashboardActions: Action[] = data
-    .filter((item) => item.type === 'dash-db')
-    .map((item) => ({
-      parent: parentAction.id,
-      id: `dashboard/go-to/${item.url}`,
-      name: `Go to dashboard ${item.title}`,
-      perform: () => {
-        locationService.push(item.url);
-      },
-    }));
-
-  return [parentAction, ...goToDashboardActions];
-}
-
-export default async () => {
-  const dashboardNav = await getDashboardNav();
-  return [...dashboardNav, ...globalActions];
+export default () => {
+  return globalActions;
 };
