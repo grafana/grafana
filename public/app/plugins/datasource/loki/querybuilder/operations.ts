feaa4a5c647ceca90fa4aafec8d198ccdefdb645
Torkel Ödegaard <torkel@grafana.com>
2022-03-29 18:50:07 +0200

    Loki: Support binary operations and nested queries in new builder (#47012)
    * Binary ops support
    
    * Add support for nested expressions in loki
    
    * Nested queries working
    
    * Fixing tests
    
    * Share more code between loki and prometheus query modellers

--- public/app/plugins/datasource/loki/querybuilder/operations.ts
+++ public/app/plugins/datasource/loki/querybuilder/operations.ts
@@ -7,6 +7,7 @@ import {
   VisualQueryModeller,
 } from '../../prometheus/querybuilder/shared/types';
 import { FUNCTIONS } from '../syntax';
+import { binaryScalarOperations } from './binaryScalarOperations';
 import { LokiOperationId, LokiOperationOrder, LokiVisualQuery, LokiVisualQueryOperationCategory } from './types';
 
 export function getOperationDefintions(): QueryBuilderOperationDef[] {
@@ -180,6 +181,16 @@ export function getOperationDefintions(): QueryBuilderOperationDef[] {
         return `Use the extracted label \`${label}\` as sample values instead of log lines for the subsequent range aggregation.`;
       },
     },
+    ...binaryScalarOperations,
+    {
+      id: LokiOperationId.NestedQuery,
+      name: 'Binary operation with query',
+      params: [],
+      defaultParams: [],
+      category: LokiVisualQueryOperationCategory.BinaryOps,
+      renderer: (model, def, innerExpr) => innerExpr,
+      addOperationHandler: addNestedQueryHandler,
+    },
   ];
 
   return list;
@@ -326,5 +337,18 @@ export function addLokiOperation(
   return {
     ...query,
     operations,
+  };
+}
+
+function addNestedQueryHandler(def: QueryBuilderOperationDef, query: LokiVisualQuery): LokiVisualQuery {
+  return {
+    ...query,
+    binaryQueries: [
+      ...(query.binaryQueries ?? []),
+      {
+        operator: '/',
+        query,
+      },
+    ],
   };
 }
