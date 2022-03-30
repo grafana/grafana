996a72c2ab46135723c6cb09498fc4b3393842c8
Ashley Harrison <ashley.harrison@grafana.com>
2022-03-30 10:52:38 +0100

    update comment
    Co-authored-by: kay delaney <kay@grafana.com>
    Co-authored-by: Alexandra Vargas <alexa1866@gmail.com>

--- public/app/features/variables/utils.ts
+++ public/app/features/variables/utils.ts
@@ -15,7 +15,7 @@ import { KeyedVariableIdentifier, VariableIdentifier, VariablePayload } from './
 /*
  * This regex matches 3 types of variable reference with an optional format specifier
  * \$(\w+)                          $var1
- * \[\[([\s\S]+?)(?::(\w+))?\]\]    [[var2]] or [[var2:fmt2]]
+ * \[\[(\w+?)(?::(\w+))?\]\]        [[var2]] or [[var2:fmt2]]
  * \${(\w+)(?::(\w+))?}             ${var3} or ${var3:fmt3}
  */
 export const variableRegex = /\$(\w+)|\[\[(\w+?)(?::(\w+))?\]\]|\${(\w+)(?:\.([^:^\}]+))?(?::([^\}]+))?}/g;
