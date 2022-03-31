491d83c502c76fa1b00d40408a58c3f82562ed62
Ashley Harrison <ashley.harrison@grafana.com>
2022-03-31 14:07:39 +0100

    Navigation: prevent navbar from showing when `.sidemenu-hidden` is set

--- public/app/core/components/NavBar/Next/NavBarNext.tsx
+++ public/app/core/components/NavBar/Next/NavBarNext.tsx
@@ -141,6 +141,10 @@ const getStyles = (theme: GrafanaTheme2) => ({
   navWrapper: css({
     position: 'relative',
     display: 'flex',
+
+    '.sidemenu-hidden &': {
+      display: 'none',
+    },
   }),
   sidemenu: css({
     label: 'sidemenu',
@@ -158,10 +162,6 @@ const getStyles = (theme: GrafanaTheme2) => ({
       paddingTop: '0px',
       backgroundColor: 'inherit',
       borderRight: 0,
-    },
-
-    '.sidemenu-hidden &': {
-      visibility: 'hidden',
     },
   }),
   mobileSidemenuLogo: css({
