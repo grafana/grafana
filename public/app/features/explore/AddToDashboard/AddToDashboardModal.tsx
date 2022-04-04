eb968cc169274f682e7bd0865f7414d99148b9e0
Elfo404 <me@giordanoricci.com>
2022-04-04 11:01:45 +0100

    Add to dashboard form

--- public/app/features/explore/AddToDashboard/AddToDashboardModal.tsx
+++ public/app/features/explore/AddToDashboard/AddToDashboardModal.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
-import { Button, Field, Modal, RadioButtonGroup } from '@grafana/ui';
+import React from 'react';
+import { Button, Field, InputControl, Modal, RadioButtonGroup, useStyles2 } from '@grafana/ui';
 import { SelectableValue } from '@grafana/data';
-import { addPanelToDashboard, SaveTarget } from './addToDashboard';
+import { addPanelToDashboard } from './addToDashboard';
 import { useSelector } from 'react-redux';
 import { ExploreId, StoreState } from 'app/types';
 import { DashboardPicker } from 'app/core/components/Select/DashboardPicker';
+import { DeepMap, FieldError, useForm } from 'react-hook-form';
+import { css } from '@emotion/css';
+import { config, locationService } from '@grafana/runtime';
+
+enum SaveTarget {
+  NewDashboard,
+  ExistingDashboard,
+}
 
 const SAVE_TARGETS: Array<SelectableValue<SaveTarget>> = [
   {
@@ -17,65 +25,131 @@ const SAVE_TARGETS: Array<SelectableValue<SaveTarget>> = [
   },
 ];
 
+interface SaveTargetDTO {
+  saveTarget: SaveTarget;
+}
+interface SaveToNewDashboardDTO extends SaveTargetDTO {
+  saveTarget: SaveTarget.NewDashboard;
+}
+
+interface SaveToExistingDashboard extends SaveTargetDTO {
+  saveTarget: SaveTarget.ExistingDashboard;
+  dashboardUid: string;
+}
+
+type FormDTO = SaveToNewDashboardDTO | SaveToExistingDashboard;
+
+function assertIsSaveToExistingDashboardError(
+  errors: DeepMap<FormDTO, FieldError>
+): asserts errors is DeepMap<SaveToExistingDashboard, FieldError> {
+  // the shape of the errors object is always compatible with the type above, but we need to
+  // explicitly assert its type so that TS can narrow down FormDTO to SaveToExistingDashboard
+  // when we use it in the form.
+}
+
+function withRedirect<T extends any[]>(fn: (redirect: boolean, ...args: T) => {}, redirect: boolean) {
+  return async (...args: T) => fn(redirect, ...args);
+}
+
+function openDashboard(openInNewTab: boolean, dashboardUid?: string) {
+  const url = dashboardUid ? `d/${dashboardUid}` : 'dashboard/new';
+
+  if (!openInNewTab) {
+    locationService.push(url);
+  } else {
+    window.open(config.appUrl + url, '_blank');
+  }
+}
+
 interface Props {
   onClose: () => void;
   exploreId: ExploreId;
 }
 
 export const AddToDashboardModal = ({ onClose, exploreId }: Props) => {
-  const [targetMode, setTargetMode] = useState<SaveTarget>(SaveTarget.NewDashboard);
-  const [dashboardUid, setDashboardUid] = useState<string | undefined>(undefined);
   const state = useSelector((state: StoreState) => state);
+  const radioGroupStyles = useStyles2(
+    (theme) => css`
+      margin-bottom: ${theme.spacing(2)};
+    `
+  );
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    watch,
+  } = useForm<FormDTO>({
+    defaultValues: { saveTarget: SaveTarget.NewDashboard },
+  });
+  const saveTarget = watch('saveTarget');
 
-  const onOpen = () => {
+  const onSubmit = async (openInNewTab: boolean, data: FormDTO) => {
+    const dashboardUid = data.saveTarget === SaveTarget.ExistingDashboard ? data.dashboardUid : undefined;
     addPanelToDashboard({
-      exploreItem: state.explore[exploreId]!,
-      targetMode,
       dashboardUid,
-    });
-    onClose();
-  };
-
-  const onOpenInNewTab = () => {
-    addPanelToDashboard({
       exploreItem: state.explore[exploreId]!,
-      openInNewTab: true,
-      targetMode,
-      dashboardUid,
     });
-    onClose();
-  };
 
-  // If we are not in new dashboard mode we have to have picked a dashboard
-  const isValid = targetMode === SaveTarget.NewDashboard || dashboardUid !== undefined;
+    onClose();
+    openDashboard(openInNewTab, dashboardUid);
+  };
 
   return (
     <Modal title="Add panel to dashboard" onDismiss={onClose} isOpen>
-      <Field>
-        <RadioButtonGroup options={SAVE_TARGETS} value={targetMode} onChange={setTargetMode} />
-      </Field>
+      <form>
+        <InputControl
+          control={control}
+          render={({ field: { ref, ...field } }) => (
+            <RadioButtonGroup options={SAVE_TARGETS} {...field} className={radioGroupStyles} />
+          )}
+          name="saveTarget"
+        />
 
-      {targetMode === SaveTarget.ExistingDashboard && (
-        <Field label="Dashboard" description="Select in which dashboard the panel will be created.">
-          <DashboardPicker
-            value={dashboardUid}
-            onChange={(value) => setDashboardUid(value?.uid)}
-            inputId="e2d-dashboard-picker"
-          />
-        </Field>
-      )}
+        {saveTarget === SaveTarget.ExistingDashboard &&
+          (() => {
+            assertIsSaveToExistingDashboardError(errors);
+            return (
+              <InputControl
+                render={({ field: { ref, value, onChange, ...field } }) => (
+                  <Field
+                    label="Dashboard"
+                    description="Select in which dashboard the panel will be created."
+                    error={errors.dashboardUid?.message}
+                    invalid={!!errors.dashboardUid}
+                  >
+                    <DashboardPicker
+                      {...field}
+                      inputId="e2d-dashboard-picker"
+                      defaultOptions
+                      onChange={(d) => onChange(d?.uid)}
+                    />
+                  </Field>
+                )}
+                control={control}
+                name="dashboardUid"
+                shouldUnregister
+                rules={{ required: { value: true, message: 'This field is required.' } }}
+              />
+            );
+          })()}
 
-      <Modal.ButtonRow>
-        <Button type="reset" onClick={onClose} fill="outline" variant="secondary">
-          Cancel
-        </Button>
-        <Button onClick={onOpenInNewTab} variant="secondary" disabled={!isValid}>
-          Open in new tab
-        </Button>
-        <Button onClick={onOpen} variant="primary" disabled={!isValid}>
-          Open
-        </Button>
-      </Modal.ButtonRow>
+        <Modal.ButtonRow>
+          <Button type="reset" onClick={onClose} fill="outline" variant="secondary">
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            variant="secondary"
+            onClick={handleSubmit(withRedirect(onSubmit, true))}
+            icon="external-link-alt"
+          >
+            Open in new tab
+          </Button>
+          <Button type="submit" variant="primary" onClick={handleSubmit(withRedirect(onSubmit, false))} icon="apps">
+            Open
+          </Button>
+        </Modal.ButtonRow>
+      </form>
     </Modal>
   );
 };
--- public/app/features/explore/AddToDashboard/addToDashboard.ts
+++ public/app/features/explore/AddToDashboard/addToDashboard.ts
@@ -1,28 +1,21 @@
 import { DataFrame, DataQuery } from '@grafana/data';
-import { config, locationService } from '@grafana/runtime';
 import { backendSrv } from 'app/core/services/backend_srv';
 import {
   getNewDashboardModelData,
   setDashboardToFetchFromLocalStorage,
 } from 'app/features/dashboard/state/initDashboard';
-import { DashboardDTO, ExploreItemState, ExplorePanelData } from 'app/types';
+import { ExploreItemState, ExplorePanelData } from 'app/types';
 
-export enum SaveTarget {
-  NewDashboard,
-  ExistingDashboard,
-}
-
-export interface AddPanelToDashboardOptions {
+interface AddPanelToDashboardOptions {
   exploreItem: ExploreItemState;
-  targetMode: SaveTarget;
-  openInNewTab?: boolean;
   dashboardUid?: string;
 }
 
-export function addPanelToDashboard(options: AddPanelToDashboardOptions) {
+// TODO: rename this to something like "setDashboardInLocalStorage"
+export async function addPanelToDashboard(options: AddPanelToDashboardOptions) {
   const queries = options.exploreItem?.queries || [];
   const datasource = options.exploreItem?.datasourceInstance;
-  const panelType = getPanelTypeFor(queries, options.exploreItem?.queryResponse);
+  const panelType = getPanelType(queries, options.exploreItem?.queryResponse);
   const panel = {
     targets: queries,
     type: panelType,
@@ -31,39 +24,19 @@ export function addPanelToDashboard(options: AddPanelToDashboardOptions) {
     datasource,
   };
 
-  if (options.targetMode === SaveTarget.NewDashboard) {
-    addToNewDashboard(panel, options.openInNewTab);
-  } else {
-    addToExisting(panel, options.dashboardUid!, options.openInNewTab);
-  }
-}
+  const dto = options.dashboardUid
+    ? await backendSrv.getDashboardByUid(options.dashboardUid)
+    : getNewDashboardModelData();
 
-function addToNewDashboard(panel: any, openInNewTab?: boolean) {
-  const dto = getNewDashboardModelData();
-  dto.dashboard.panels = [panel];
-  openDashboard('dashboard/new', dto, openInNewTab);
-}
-
-async function addToExisting(panel: any, uid: string, openInNewTab?: boolean) {
-  const dto = await backendSrv.getDashboardByUid(uid);
   dto.dashboard.panels = [panel, ...(dto.dashboard.panels ?? [])];
-  openDashboard(`d/${uid}`, dto, openInNewTab);
-}
 
-function openDashboard(url: string, dto: DashboardDTO, openInNewTab?: boolean) {
   setDashboardToFetchFromLocalStorage(dto);
-
-  if (!openInNewTab) {
-    locationService.push('dashboard/new');
-  } else {
-    window.open(config.appUrl + url, '_blank');
-  }
 }
 
 const isVisible = (query: DataQuery) => !query.hide;
 const hasRefId = (refId: DataFrame['refId']) => (frame: DataFrame) => frame.refId === refId;
 
-export function getPanelTypeFor(queries: DataQuery[], queryResponse?: ExplorePanelData) {
+export function getPanelType(queries: DataQuery[], queryResponse?: ExplorePanelData) {
   if (!queryResponse) {
     // return table if no response
     return 'table';
