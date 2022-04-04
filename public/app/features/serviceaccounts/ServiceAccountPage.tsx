73af95751b1851db8492bd6e88f9e9b69b521d3d
eleijonmarck <eric.leijonmarck@gmail.com>
2022-04-04 16:56:37 +0100

    unused import

--- public/app/features/serviceaccounts/ServiceAccountPage.tsx
+++ public/app/features/serviceaccounts/ServiceAccountPage.tsx
@@ -16,7 +16,7 @@ import {
 } from './state/actions';
 import { ServiceAccountTokensTable } from './ServiceAccountTokensTable';
 import { getTimeZone, NavModel } from '@grafana/data';
-import { Button, VerticalGroup } from '@grafana/ui';
+import { Button } from '@grafana/ui';
 import { CreateTokenModal } from './CreateTokenModal';
 import { contextSrv } from 'app/core/core';
 
