89d002f36b2c4ab35ef29036bd39361825dbe11d
Josh Hunt <joshhunt@users.noreply.github.com>
2022-04-05 15:54:52 +0100

    Update public/app/core/services/echo/utils.ts
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>

--- public/app/core/services/echo/utils.ts
+++ public/app/core/services/echo/utils.ts
@@ -5,7 +5,7 @@ import { CurrentUserDTO } from '@grafana/data';
 /**
  * Returns an opaque identifier for a user, for reporting purposes.
  * Because this is for use when reporting across multiple Grafana installations
- * It cannot simply by user.id
+ * It cannot simply be user.id
  */
 export function getUserIdentifier(user: CurrentUserDTO) {
   if (user.externalUserId.length) {
