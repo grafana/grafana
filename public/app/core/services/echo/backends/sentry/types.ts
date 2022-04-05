4ad76a5110ab5c9babeb3e10529376aeeeb1e10a
joshhunt <josh@trtr.co>
2022-04-05 13:44:10 +0100

    wip

--- public/app/core/services/echo/backends/sentry/types.ts
+++ public/app/core/services/echo/backends/sentry/types.ts
@@ -12,4 +12,5 @@ export interface User {
   email: string;
   id: number;
   orgId: number;
+  externalUserId: string;
 }
---
+++ public/app/core/services/echo/backends/utils.test.ts
@@ -0,0 +1,5 @@
+describe('echo getUserIdentifier', () => {
+  it('should return the external user ID (gcom ID) if available', () => {
+    const user = {};
+  });
+});
