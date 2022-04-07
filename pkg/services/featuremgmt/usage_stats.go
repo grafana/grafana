652c802d41b6d06fbe498a490dc399b498632e57
Artur Wierzbicki <wierzbicki.artur.94@gmail.com>
2022-04-07 15:46:45 +0200

    #47127: convert GetUsageStats to return `map[string]interface{}`

--- pkg/services/featuremgmt/usage_stats.go
+++ pkg/services/featuremgmt/usage_stats.go
@@ -7,9 +7,9 @@ import (
 	"github.com/grafana/grafana/pkg/services/featuremgmt/strcase"
 )
 
-func (fm *FeatureManager) GetUsageStats(ctx context.Context) map[string]int {
+func (fm *FeatureManager) GetUsageStats(ctx context.Context) map[string]interface{} {
 	enabled := fm.GetEnabled(ctx)
-	stats := make(map[string]int, len(enabled))
+	stats := make(map[string]interface{}, len(enabled))
 	for featureName := range enabled {
 		stats[asMetricName(featureName)] = 1
 	}
