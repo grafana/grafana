93db1fe26a4d8566185c82f9ad5e1bed46df4c1e
Michael Mandrus <michael.mandrus@grafana.com>
2022-04-07 16:48:23 -0400

    Add new error message for signup not allowed errors

--- pkg/services/login/loginservice/loginservice.go
+++ pkg/services/login/loginservice/loginservice.go
@@ -57,7 +57,7 @@ func (ls *Implementation) UpsertUser(ctx context.Context, cmd *models.UpsertUser
 		}
 		if !cmd.SignupAllowed {
 			cmd.ReqContext.Logger.Warn("Not allowing login, user not found in internal user database and allow signup = false", "authmode", extUser.AuthModule)
-			return login.ErrInvalidCredentials
+			return login.ErrSignupNotAllowed
 		}
 
 		limitReached, err := ls.QuotaService.QuotaReached(cmd.ReqContext, "user")
