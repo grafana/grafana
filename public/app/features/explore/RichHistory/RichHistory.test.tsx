69a0f7ea73f187d495563a9873398c904b2cef1a
Piotr Jamróz <pm.jamroz@gmail.com>
2022-04-07 19:22:34 +0200

    Fix unit test

--- public/app/features/explore/RichHistory/RichHistory.test.tsx
+++ public/app/features/explore/RichHistory/RichHistory.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { GrafanaTheme } from '@grafana/data';
 import { ExploreId } from '../../../types/explore';
 import { RichHistory, RichHistoryProps, Tabs } from './RichHistory';
+import { SortOrder } from '../../../core/utils/richHistoryTypes';
 
 jest.mock('../state/selectors', () => ({ getExploreDatasources: jest.fn() }));
 
@@ -16,6 +17,21 @@ const setup = (propOverrides?: Partial<RichHistoryProps>) => {
     firstTab: Tabs.RichHistory,
     deleteRichHistory: jest.fn(),
     onClose: jest.fn(),
+    richHistorySearchFilters: {
+      search: '',
+      sortOrder: SortOrder.Descending,
+      datasourceFilters: [],
+      from: 0,
+      to: 7,
+    },
+    richHistorySettings: {
+      retentionPeriod: 0,
+      starredTabAsFirstTab: false,
+      activeDatasourceOnly: true,
+      lastUsedDatasourceFilters: [],
+    },
+    updateHistorySearchFilters: jest.fn(),
+    updateHistorySettings: jest.fn(),
   };
 
   Object.assign(props, propOverrides);
