d07343dd584f0adc3e245d2d0484631bf4e3c381
Ivana Huckova <ivana.huckova@gmail.com>
2022-04-11 12:24:41 +0200

    Check for createdAt

--- pkg/services/queryhistory/database.go
+++ pkg/services/queryhistory/database.go
@@ -276,6 +276,11 @@ func (s QueryHistoryService) migrateQueries(ctx context.Context, user *models.Si
 			if instertError != nil {
 				break
 			}
+
+			if query.CreatedAt == 0 {
+				query.CreatedAt = time.Now().Unix()
+			}
+
 			// First add query to query history table
 			queryHistory := QueryHistory{
 				OrgID:         user.OrgId,
