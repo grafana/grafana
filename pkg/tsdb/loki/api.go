4b2337da8dfc3af7f7ab5b343dda87864cfb3039
Gábor Farkas <gabor.farkas@gmail.com>
2022-04-12 19:18:52 +0200

    loki: backend: use streaming-json parser

--- pkg/tsdb/loki/api.go
+++ pkg/tsdb/loki/api.go
@@ -10,8 +10,9 @@ import (
 	"net/url"
 	"strconv"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/loki/pkg/loghttp"
+	"github.com/grafana/grafana/pkg/util/converter"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -137,7 +138,7 @@ func makeLokiError(body io.ReadCloser) error {
 	return fmt.Errorf("%v", errorMessage)
 }
 
-func (api *LokiAPI) DataQuery(ctx context.Context, query lokiQuery) (*loghttp.QueryResponse, error) {
+func (api *LokiAPI) DataQuery(ctx context.Context, query lokiQuery) (*backend.DataResponse, error) {
 	req, err := makeDataRequest(ctx, api.url, query, api.oauthToken)
 	if err != nil {
 		return nil, err
@@ -158,13 +159,9 @@ func (api *LokiAPI) DataQuery(ctx context.Context, query lokiQuery) (*loghttp.Qu
 		return nil, makeLokiError(resp.Body)
 	}
 
-	var response loghttp.QueryResponse
-	err = jsoniter.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		return nil, err
-	}
-
-	return &response, nil
+	iter := jsoniter.Parse(jsoniter.ConfigDefault, resp.Body, 1024)
+	res := converter.ReadPrometheusStyleResult(iter)
+	return res, nil
 }
 
 func makeRawRequest(ctx context.Context, lokiDsUrl string, resourceURL string, oauthToken string) (*http.Request, error) {
--- pkg/tsdb/loki/loki.go
+++ pkg/tsdb/loki/loki.go
@@ -209,12 +209,25 @@ func queryData(ctx context.Context, req *backend.QueryDataRequest, dsInfo *datas
 
 // we extracted this part of the functionality to make it easy to unit-test it
 func runQuery(ctx context.Context, api *LokiAPI, query *lokiQuery) (data.Frames, error) {
-	value, err := api.DataQuery(ctx, *query)
+	response, err := api.DataQuery(ctx, *query)
 	if err != nil {
 		return data.Frames{}, err
 	}
 
-	return parseResponse(value, query)
+	if response.Error != nil {
+		return data.Frames{}, err
+	}
+
+	frames := response.Frames
+
+	for _, frame := range frames {
+		err = adjustFrame(frame, query)
+		if err != nil {
+			return data.Frames{}, err
+		}
+	}
+
+	return frames, nil
 }
 
 func (s *Service) getDSInfo(pluginCtx backend.PluginContext) (*datasourceInfo, error) {
