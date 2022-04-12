7181efd1cf214b47c5e5ab41a79328f0bd15dfc3
Torkel Ödegaard <torkel@grafana.com>
2022-04-12 13:26:07 +0200

    Explore: Allow users to save Explore queries to dashboards  (#47083)
    * Select: Expose AsyncSelectProps interface
    
    * DashboardPicker: Add a generic DashboardPicker component
    
    * Dashboard Service: improve types
    
    * Explore: allow saving explore state in a new panel in an existing dashboard
    
    * Handle saving provisioned dashboards error
    
    * Improve test coverage
    
    * simplify test setup
    
    * Strip base path from url when redirecting to a dashboard
    
    * Keep existing variables when saving to an existing dashboard
    
    * group assertions in test
    
    * SearchCard: handle undefined in meta.updated
    
    * Change required error message
    
    * Add to dashboard alternative
    
    * Add to existing is working
    
    * Add to dashboard form
    
    * remove default add-panel when creating a dashboard from explore
    
    * types cleanup
    
    * remove unneeded BE change
    
    * simplify selector
    
    * Add explore2Dashboard feature toggle
    
    * add tests
    
    * Small refactor & add tests
    
    * small DashboardPicker improvements
    
    * use partial from lodash
    
    * Better error handling
    
    * improve tests & disable button when there are no queries
    
    * rename addPanelToDashboard function
    
    * remove localStorage item if opening tab fails
    
    * UI touchups & tracking
    
    * Fix tests & remove close reporting
    
    * remove echologger debug
    
    * fix adding a panel to an existing dashboard
    
    * Enable explore2Dashboard by default and add docs
    
    * Ensure each panel in dashboards has a valid ID
    
    * force CI restart
    
    Co-authored-by: Elfo404 <me@giordanoricci.com>

--- public/app/core/components/editors/DashboardPicker.tsx
+++ public/app/core/components/editors/DashboardPicker.tsx
@@ -1,63 +1,28 @@
-import React, { FC, useCallback, useState } from 'react';
-import debounce from 'debounce-promise';
+import React, { FC, useCallback } from 'react';
 import { SelectableValue, StandardEditorProps } from '@grafana/data';
-import { DashboardSearchHit } from 'app/features/search/types';
-import { backendSrv } from 'app/core/services/backend_srv';
-import { AsyncSelect } from '@grafana/ui';
-import { useAsync } from 'react-use';
+import { DashboardPicker as BasePicker, DashboardPickerDTO } from 'app/core/components/Select/DashboardPicker';
 
 export interface DashboardPickerOptions {
   placeholder?: string;
   isClearable?: boolean;
 }
 
-const getDashboards = (query = '') => {
-  return backendSrv.search({ type: 'dash-db', query, limit: 100 }).then((result: DashboardSearchHit[]) => {
-    return result.map((item: DashboardSearchHit) => ({
-      value: item.uid,
-      label: `${item?.folderTitle ?? 'General'}/${item.title}`,
-    }));
-  });
-};
-
 /** This will return the item UID */
-export const DashboardPicker: FC<StandardEditorProps<string, any, any>> = ({ value, onChange, item }) => {
-  const [current, setCurrent] = useState<SelectableValue<string>>();
-
-  // This is required because the async select does not match the raw uid value
-  // We can not use a simple Select because the dashboard search should not return *everything*
-  useAsync(async () => {
-    if (!value) {
-      setCurrent(undefined);
-      return;
-    }
-    const res = await backendSrv.getDashboardByUid(value);
-    setCurrent({
-      value: res.dashboard.uid,
-      label: `${res.meta?.folderTitle ?? 'General'}/${res.dashboard.title}`,
-    });
-    return undefined;
-  }, [value]);
+export const DashboardPicker: FC<StandardEditorProps<string, DashboardPickerOptions, any>> = ({
+  value,
+  onChange,
+  item,
+}) => {
+  const { placeholder, isClearable } = item?.settings ?? {};
 
   const onPicked = useCallback(
-    (sel: SelectableValue<string>) => {
-      onChange(sel?.value);
+    (sel?: SelectableValue<DashboardPickerDTO>) => {
+      onChange(sel?.value?.uid);
     },
     [onChange]
   );
-  const debouncedSearch = debounce(getDashboards, 300);
-  const { placeholder, isClearable } = item?.settings ?? {};
 
   return (
-    <AsyncSelect
-      menuShouldPortal
-      isClearable={isClearable}
-      defaultOptions={true}
-      loadOptions={debouncedSearch}
-      onChange={onPicked}
-      placeholder={placeholder ?? 'Select dashboard'}
-      noOptionsMessage="No dashboards found"
-      value={current}
-    />
+    <BasePicker isClearable={isClearable} defaultOptions onChange={onPicked} placeholder={placeholder} value={value} />
   );
 };
