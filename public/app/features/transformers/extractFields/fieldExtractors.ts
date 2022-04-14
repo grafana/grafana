9f8d29f787a7b24af88dcf8b57338bcc19e6bd6a
Andreas Christou <aangelisc@gmail.com>
2022-04-14 19:38:00 +0100

    Additional logic to handle quoted values

--- public/app/features/transformers/extractFields/fieldExtractors.ts
+++ public/app/features/transformers/extractFields/fieldExtractors.ts
@@ -25,6 +25,10 @@ const stripDecor = /['"]|^\{|\}$/g;
 const splitLines = /[\s,;&]+/g;
 // splits kv pairs
 const splitPair = /[=:]/g;
+// finds all strings in single/double quotes
+const quotedValues = /['"](.*?)['"]/g;
+// finds the quoted value placeholder
+const prefixRegex = /(\$val\d+\$)/g;
 
 const extLabels: FieldExtractor = {
   id: FieldExtractorID.KeyValues,
@@ -32,12 +36,28 @@ const extLabels: FieldExtractor = {
   description: 'Look for a=b, c: d values in the line',
   parse: (v: string) => {
     const obj: Record<string, any> = {};
-
+    const quotedValuesMap: Map<string, string> = new Map<string, string>();
+    const placeholderPrefix = '$val';
+    let quoteCounter = 0;
     v.trim()
+      .replace(quotedValues, (matched) => {
+        const placeholder = `${placeholderPrefix}${quoteCounter}$`;
+        quotedValuesMap.set(placeholder, matched);
+        quoteCounter++;
+        return placeholder;
+      })
       .replace(stripDecor, '')
       .split(splitLines)
       .forEach((pair) => {
-        let [k, v] = pair.split(splitPair);
+        let [k, v] = pair
+          .replace(prefixRegex, (matched, _index) => {
+            const originalValue = quotedValuesMap.get(matched);
+            if (originalValue) {
+              return originalValue.replace(stripDecor, '');
+            }
+            return matched;
+          })
+          .split(splitPair);
 
         if (k != null) {
           obj[k] = v;
