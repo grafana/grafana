0d4cd1c288707af08d662a9a3f9497dfd6ddfcce
Stephanie Closson <srclosson@gmail.com>
2022-04-15 15:37:50 -0300

    reintroduce exemplars. Comment out conflicted code

--- public/app/plugins/panel/heatmap-new/HeatmapPanel.tsx
+++ public/app/plugins/panel/heatmap-new/HeatmapPanel.tsx
@@ -2,18 +2,18 @@ import React, { useCallback, useMemo, useRef, useState } from 'react';
 // import { css } from '@emotion/css';
 // import { PanelProps, reduceField, ReducerID, TimeRange } from '@grafana/data';
 import {
-  // ArrayVector,
+  ArrayVector,
   DataFrame,
-  // Field,
-  GrafanaTheme2,
+  Field,
+  // GrafanaTheme2,
   // formattedValueToString,
   PanelProps,
   reduceField,
   ReducerID,
   TimeRange,
-  // ValueLinkConfig,
+  ValueLinkConfig,
 } from '@grafana/data';
-import { Portal, UPlotChart, useTheme2, VizLayout, LegendDisplayMode, VizTooltipContainer, useStyles2 } from '@grafana/ui';
+import { Portal, UPlotChart, useTheme2, VizLayout, LegendDisplayMode, usePanelContext } from '@grafana/ui';
 import { PanelDataErrorView } from '@grafana/runtime';
 
 import { HeatmapData, prepareHeatmapData } from './fields';
@@ -24,19 +24,19 @@ import {
   findDataFramesInPanelData,
   HeatmapHoverEvent,
   prepConfig,
-  // timeFormatter,
-  // translateMatrixIndex,
+  timeFormatter,
+  translateMatrixIndex,
 } from './utils';
 import { HeatmapHoverView } from './HeatmapHoverView';
 import { ColorScale } from './ColorScale';
-// import { getFieldLinksForExplore } from 'app/features/explore/utils/links';
+import { getFieldLinksForExplore } from 'app/features/explore/utils/links';
 import { HeatmapCalculationMode } from 'app/features/transformers/calculateHeatmap/models.gen';
-// import { HeatmapLookup } from './types';
-// import { HeatmapTab } from './hovertabs/HeatmapTab';
-// import { ExemplarTab } from './hovertabs/ExemplarTab';
+import { HeatmapLookup } from './types';
+import { HeatmapTab } from './hovertabs/HeatmapTab';
+import { ExemplarTab } from './hovertabs/ExemplarTab';
 import { ExemplarsPlugin } from './plugins/ExemplarsPlugin';
-import { CloseButton } from 'app/core/components/CloseButton/CloseButton';
-import { css } from '@emotion/css';
+// import { CloseButton } from 'app/core/components/CloseButton/CloseButton';
+// import { css } from '@emotion/css';
 
 interface HeatmapPanelProps extends PanelProps<PanelOptions> {}
 
@@ -53,7 +53,7 @@ export const HeatmapPanel: React.FC<HeatmapPanelProps> = ({
   replaceVariables,
 }) => {
   const theme = useTheme2();
-  const styles = useStyles2(getStyles);
+  // const styles = useStyles2(getStyles);
 
   // ugh
   let timeRangeRef = useRef<TimeRange>(timeRange);
@@ -83,7 +83,7 @@ export const HeatmapPanel: React.FC<HeatmapPanelProps> = ({
     return undefined;
   }, [data, info, options, theme]);
   const facets = useMemo(() => [null, info.heatmap?.fields.map((f) => f.values.toArray())], [info.heatmap]);
-  // const { onSplitOpen } = usePanelContext();
+  const { onSplitOpen } = usePanelContext();
 
   const palette = useMemo(() => quantizeScheme(options.color, theme), [options.color, theme]);
 
@@ -133,62 +133,62 @@ export const HeatmapPanel: React.FC<HeatmapPanelProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [options, data.structureRev]);
 
-  // const getValuesInCell = useCallback(
-  //   (lookupRange: HeatmapLookup): DataFrame[] | undefined => {
-  //     const timeField: Field | undefined = data.annotations?.[0].fields.find((f: Field) => f.type === 'time');
-  //     const valueField: Field | undefined = data.annotations?.[0].fields.find((f: Field) => f.type === 'number');
-  //     if (timeField && valueField) {
-  //       const minIndex: number = timeField.values
-  //         .toArray()
-  //         .findIndex((value: number) => value >= lookupRange.xRange.min!);
-  //       const count: number = timeField.values
-  //         .toArray()
-  //         .slice(minIndex)
-  //         .findIndex((value: number) => value >= lookupRange.xRange.max!);
+  const getValuesInCell = useCallback(
+    (lookupRange: HeatmapLookup): DataFrame[] | undefined => {
+      const timeField: Field | undefined = data.annotations?.[0].fields.find((f: Field) => f.type === 'time');
+      const valueField: Field | undefined = data.annotations?.[0].fields.find((f: Field) => f.type === 'number');
+      if (timeField && valueField) {
+        const minIndex: number = timeField.values
+          .toArray()
+          .findIndex((value: number) => value >= lookupRange.xRange.min!);
+        const count: number = timeField.values
+          .toArray()
+          .slice(minIndex)
+          .findIndex((value: number) => value >= lookupRange.xRange.max!);
 
-  //       // Now find the relevant values in the value field.
-  //       const indicies: number[] = valueField.values
-  //         .toArray()
-  //         .slice(minIndex, minIndex + count)
-  //         .reduce((tally: number[], curr: number, i: number) => {
-  //           if (curr >= lookupRange.yRange?.min! && curr < lookupRange.yRange?.max!) {
-  //             tally.push(i + minIndex);
-  //           }
-  //           return tally;
-  //         }, []);
+        // Now find the relevant values in the value field.
+        const indicies: number[] = valueField.values
+          .toArray()
+          .slice(minIndex, minIndex + count)
+          .reduce((tally: number[], curr: number, i: number) => {
+            if (curr >= lookupRange.yRange?.min! && curr < lookupRange.yRange?.max!) {
+              tally.push(i + minIndex);
+            }
+            return tally;
+          }, []);
 
-  //       return indicies.map((annotationIndex: number, index: number) => {
-  //         return {
-  //           name: `${index + 1}`,
-  //           fields: (data.annotations?.[0].fields || []).map((f: Field, rowIndex: number) => {
-  //             const newField: Field = {
-  //               ...f,
-  //               values: new ArrayVector([f.values.get(annotationIndex)]),
-  //             };
-  //             if (f.config.links?.length) {
-  //               // We have links to configure. Add a getLinks function to the field
-  //               newField.getLinks = (config: ValueLinkConfig) => {
-  //                 return getFieldLinksForExplore({ field: f, rowIndex, splitOpenFn: onSplitOpen, range: timeRange });
-  //               };
-  //             }
-  //             if (f.type === 'time') {
-  //               newField.display = (value: number) => {
-  //                 return {
-  //                   numeric: value,
-  //                   text: timeFormatter(value, timeZone),
-  //                 };
-  //               };
-  //             }
-  //             return newField;
-  //           }),
-  //           length: 1,
-  //         };
-  //       });
-  //     }
-  //     return undefined;
-  //   },
-  //   [data.annotations, onSplitOpen, timeRange, timeZone]
-  // );
+        return indicies.map((annotationIndex: number, index: number) => {
+          return {
+            name: `${index + 1}`,
+            fields: (data.annotations?.[0].fields || []).map((f: Field, rowIndex: number) => {
+              const newField: Field = {
+                ...f,
+                values: new ArrayVector([f.values.get(annotationIndex)]),
+              };
+              if (f.config.links?.length) {
+                // We have links to configure. Add a getLinks function to the field
+                newField.getLinks = (config: ValueLinkConfig) => {
+                  return getFieldLinksForExplore({ field: f, rowIndex, splitOpenFn: onSplitOpen, range: timeRange });
+                };
+              }
+              if (f.type === 'time') {
+                newField.display = (value: number) => {
+                  return {
+                    numeric: value,
+                    text: timeFormatter(value, timeZone),
+                  };
+                };
+              }
+              return newField;
+            }),
+            length: 1,
+          };
+        });
+      }
+      return undefined;
+    },
+    [data.annotations, onSplitOpen, timeRange, timeZone]
+  );
 
   const renderLegend = () => {
     if (options.legend.displayMode === LegendDisplayMode.Hidden || !info.heatmap) {
@@ -233,7 +233,7 @@ export const HeatmapPanel: React.FC<HeatmapPanelProps> = ({
         )}
       </VizLayout>
       <Portal>
-        {/* {hover && (
+        {hover && (
           <HeatmapHoverView
             ttip={{
               layers: [
@@ -253,8 +253,9 @@ export const HeatmapPanel: React.FC<HeatmapPanelProps> = ({
             }}
             isOpen={shouldDisplayCloseButton}
             onClose={onCloseToolTip}
-          /> */}
-        {hover && options.tooltip.show && (
+          />
+        )}
+        {/* {hover && options.tooltip.show && (
           <VizTooltipContainer
             position={{ x: hover.pageX, y: hover.pageY }}
             offset={{ x: 10, y: 10 }}
@@ -268,14 +269,16 @@ export const HeatmapPanel: React.FC<HeatmapPanelProps> = ({
             )}
             <HeatmapHoverView data={info} hover={hover} showHistogram={options.tooltip.yHistogram} />
           </VizTooltipContainer>
-        )}
+        )} */}
       </Portal>
     </>
   );
 };
 
-const getStyles = (theme: GrafanaTheme2) => ({
+{
+  /* const getStyles = (theme: GrafanaTheme2) => ({
   closeButtonSpacer: css`
     margin-bottom: 15px;
   `,
-});
+}); */
+}
