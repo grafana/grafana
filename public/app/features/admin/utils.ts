afca0dd2cb795f0194bbe5649ac2fbe3d948018d
Clarity-89 <homes89@ukr.net>
2022-04-19 17:47:56 +0300

    Fix undefined license settings

--- public/app/features/admin/utils.ts
+++ public/app/features/admin/utils.ts
@@ -2,7 +2,7 @@ import { config } from '@grafana/runtime/src';
 
 export function isTrial() {
   const settings = (config as any).licensing;
-  return settings.isTrial;
+  return settings?.isTrial;
 }
 
 export const highlightTrial = () => isTrial() && config.featureToggles.featureHighlights;
