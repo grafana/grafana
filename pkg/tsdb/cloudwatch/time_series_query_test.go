0a44c8221f4e0cb550c222c9fab9252b210f3984
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-04-19 18:31:26 +0200

    Add isInferredSearchExpression() cases which return labels and documentation for complementary combinations

--- pkg/tsdb/cloudwatch/time_series_query_test.go
+++ pkg/tsdb/cloudwatch/time_series_query_test.go
@@ -3,6 +3,7 @@ package cloudwatch
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
@@ -174,7 +175,6 @@ func Test_QueryData_executeTimeSeriesQuery_formatAlias(t *testing.T) {
 						"namespace": "",
 						"metricName": "",
 						"expression": "SEARCH('{AWS/EC2,InstanceId} MetricName=\"CPUUtilization\"', 'Average', 300)",
-						"dimensions": {},
 						"region": "us-east-2",
 						"id": "a",
 						"alias": "{{period}} {{stat}}",
@@ -234,5 +234,137 @@ func Test_QueryData_executeTimeSeriesQuery_formatAlias(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, "query id", resp.Responses["A"].Frames[0].Name)
 	})
+}
 
+func Test_QueryData_executeTimeSeriesQuery_frame_name_is_label(t *testing.T) {
+	//_when_no_alias_provided_and_isInferredSearchExpression_and_not_isMultiValuedDimensionExpression
+	origNewCWClient := NewCWClient
+	t.Cleanup(func() {
+		NewCWClient = origNewCWClient
+	})
+	var cwClient fakeCWClient
+	NewCWClient = func(sess *session.Session) cloudwatchiface.CloudWatchAPI {
+		return &cwClient
+	}
+
+	cwClient = fakeCWClient{
+		GetMetricDataOutput: cloudwatch.GetMetricDataOutput{
+			MetricDataResults: []*cloudwatch.MetricDataResult{
+				{StatusCode: aws.String("Complete"), Id: aws.String("query id"), Label: aws.String("response label"),
+					Values: []*float64{aws.Float64(1.0)}, Timestamps: []*time.Time{{}}},
+			},
+		},
+	}
+	im := datasource.NewInstanceManager(func(s backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+		return datasourceInfo{}, nil
+	})
+	executor := newExecutor(im, newTestConfig(), &fakeSessionCache{})
+
+	testCases := map[string]struct {
+		dimensions string
+		matchExact bool
+	}{
+		"with specific dimension, matchExact false": {dimensions: `"dimensions": {"InstanceId": ["some-instance"]},`, matchExact: false},
+		"with wildcard dimension, matchExact false": {dimensions: `"dimensions": {"InstanceId": ["*"]},`, matchExact: false},
+		"with wildcard dimension, matchExact true":  {dimensions: `"dimensions": {"InstanceId": ["*"]},`, matchExact: true},
+		"without dimension, matchExact false":       {dimensions: "", matchExact: false},
+		//TODO: add the cases for final condition "if len(query.Alias) == 0 && query.MetricQueryType == MetricQueryTypeQuery"
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			resp, err := executor.QueryData(context.Background(), &backend.QueryDataRequest{
+				PluginContext: backend.PluginContext{DataSourceInstanceSettings: &backend.DataSourceInstanceSettings{}},
+				Queries: []backend.DataQuery{
+					{
+						RefID:     "A",
+						TimeRange: backend.TimeRange{From: time.Now().Add(time.Hour * -2), To: time.Now().Add(time.Hour * -1)},
+						JSON: json.RawMessage(fmt.Sprintf(`{
+						"type":      "timeSeriesQuery",
+						"metricQueryType": 0,
+						"metricEditorMode": 0,
+						"namespace": "",
+						"metricName": "",
+						%s
+						"region": "us-east-2",
+						"id": "query id",
+						"statistic": "Maximum",
+						"period": "1200",
+						"hide": false,
+						"matchExact": %t,
+						"refId": "A"
+					}`, tc.dimensions, tc.matchExact)),
+					},
+				},
+			})
+
+			assert.NoError(t, err)
+			assert.Equal(t, "response label", resp.Responses["A"].Frames[0].Name)
+		})
+	}
+}
+
+func Test_QueryData_executeTimeSeriesQuery_frame_name_is_metricName_stat(t *testing.T) {
+	//_when_no_alias_provided_and_isInferredSearchExpression_and_not_isMultiValuedDimensionExpression
+	origNewCWClient := NewCWClient
+	t.Cleanup(func() {
+		NewCWClient = origNewCWClient
+	})
+	var cwClient fakeCWClient
+	NewCWClient = func(sess *session.Session) cloudwatchiface.CloudWatchAPI {
+		return &cwClient
+	}
+
+	cwClient = fakeCWClient{
+		GetMetricDataOutput: cloudwatch.GetMetricDataOutput{
+			MetricDataResults: []*cloudwatch.MetricDataResult{
+				{StatusCode: aws.String("Complete"), Id: aws.String("query id"), Label: aws.String("response label"),
+					Values: []*float64{aws.Float64(1.0)}, Timestamps: []*time.Time{{}}},
+			},
+		},
+	}
+	im := datasource.NewInstanceManager(func(s backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+		return datasourceInfo{}, nil
+	})
+	executor := newExecutor(im, newTestConfig(), &fakeSessionCache{})
+
+	testCases := map[string]struct {
+		dimensions string
+		matchExact bool
+	}{
+		"with specific dimension, matchExact true": {dimensions: `"dimensions": {"InstanceId": ["some-instance"]},`, matchExact: true},
+		"without dimension, matchExact true":       {dimensions: "", matchExact: true},
+		"multi dimension, matchExact true":         {dimensions: `"dimensions": {"InstanceId": ["some-instance","another-instance"]},`, matchExact: true},
+		"multi dimension, matchExact false":        {dimensions: `"dimensions": {"InstanceId": ["some-instance","another-instance"]},`, matchExact: false},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			resp, err := executor.QueryData(context.Background(), &backend.QueryDataRequest{
+				PluginContext: backend.PluginContext{DataSourceInstanceSettings: &backend.DataSourceInstanceSettings{}},
+				Queries: []backend.DataQuery{
+					{
+						RefID:     "A",
+						TimeRange: backend.TimeRange{From: time.Now().Add(time.Hour * -2), To: time.Now().Add(time.Hour * -1)},
+						JSON: json.RawMessage(fmt.Sprintf(`{
+						"type":      "timeSeriesQuery",
+						"metricQueryType": 0,
+						"metricEditorMode": 0,
+						"namespace": "",
+						"metricName": "CPUUtilization",
+						%s
+						"region": "us-east-2",
+						"id": "query id",
+						"statistic": "Maximum",
+						"period": "1200",
+						"hide": false,
+						"matchExact": %t,
+						"refId": "A"
+					}`, tc.dimensions, tc.matchExact)),
+					},
+				},
+			})
+
+			assert.NoError(t, err)
+			assert.Equal(t, "CPUUtilization_Maximum", resp.Responses["A"].Frames[0].Name)
+		})
+	}
 }
