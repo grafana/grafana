7fa9ae661e66bc08d0585a36afafea5785de2710
Javier López <lopez.pardo@gmail.com>
2022-04-19 19:29:57 +0200

    remove unneeded bundle exceptions

--- packages/grafana-ui/rollup.config.ts
+++ packages/grafana-ui/rollup.config.ts
@@ -35,12 +35,8 @@ const buildCjsPackage = ({ env }) => {
       'moment',
       'jquery', // required to use jquery.plot, which is assigned externally
       'react-inlinesvg', // required to mock Icon svg loading in tests
-      'uplot', // load uplot library dynamically
       '@emotion/react',
       '@emotion/css',
-      '@emotion/cache',
-      '@emotion/babel-plugin',
-      '@emotion/utils',
     ],
     plugins: [
       commonjs({
