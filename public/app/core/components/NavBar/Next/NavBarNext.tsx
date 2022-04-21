abea5a7fd7b7f529207d75280b0b76fa7b662608
Ashley Harrison <ashley.harrison@grafana.com>
2022-04-21 16:24:51 +0100

    initial attempt at a scroll overlay

--- public/app/core/components/NavBar/Next/NavBarNext.tsx
+++ public/app/core/components/NavBar/Next/NavBarNext.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { css, cx } from '@emotion/css';
 import { cloneDeep } from 'lodash';
@@ -66,6 +66,28 @@ export const NavBarNext = React.memo(() => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [menuAnimationInProgress, setMenuAnimationInProgress] = useState(false);
   const [menuIdOpen, setMenuIdOpen] = useState<string | undefined>(undefined);
+  const scrollTopRef = useRef<HTMLDivElement>(null);
+  const scrollBottomRef = useRef<HTMLDivElement>(null);
+  const [showScrollTopIndicator, setShowTopScrollIndicator] = useState(false);
+  const [showScrollBottomIndicator, setShowBottomScrollIndicator] = useState(false);
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.target === scrollTopRef.current) {
+          setShowTopScrollIndicator(!entry.isIntersecting);
+        } else if (entry.target === scrollBottomRef.current) {
+          setShowBottomScrollIndicator(!entry.isIntersecting);
+        }
+      });
+    });
+    [scrollTopRef, scrollBottomRef].forEach((ref) => {
+      if (ref.current) {
+        intersectionObserver.observe(ref.current);
+      }
+    });
+    return () => intersectionObserver.disconnect();
+  }, []);
 
   if (kiosk !== KioskMode.Off) {
     return null;
@@ -103,29 +125,44 @@ export const NavBarNext = React.memo(() => {
               <Icon name="grafana" size="xl" />
             </NavBarItemWithoutMenu>
 
-            <CustomScrollbar hideVerticalTrack hideHorizontalTrack>
-              <NavBarItem className={styles.search} isActive={activeItem === searchItem} link={searchItem}>
-                <Icon name="search" size="xl" />
-              </NavBarItem>
-
-              {coreItems.map((link, index) => (
-                <NavBarItem
-                  key={`${link.id}-${index}`}
-                  isActive={isMatchOrChildMatch(link, activeItem)}
-                  link={{ ...link, subTitle: undefined, onClick: undefined }}
-                >
-                  {link.icon && <Icon name={link.icon as IconName} size="xl" />}
-                  {link.img && <img src={link.img} alt={`${link.text} logo`} />}
+            <CustomScrollbar
+              className={cx(styles.scrollContainer, {
+                [styles.scrollTopVisible]: showScrollTopIndicator,
+                [styles.scrollBottomVisible]: showScrollBottomIndicator,
+              })}
+              hideVerticalTrack
+              hideHorizontalTrack
+            >
+              <div className={styles.scrollContent}>
+                <div className={styles.scrollTopMarker} ref={scrollTopRef}></div>
+                <NavBarItem className={styles.search} isActive={activeItem === searchItem} link={searchItem}>
+                  <Icon name="search" size="xl" />
                 </NavBarItem>
-              ))}
 
-              {pluginItems.length > 0 &&
-                pluginItems.map((link, index) => (
-                  <NavBarItem key={`${link.id}-${index}`} isActive={isMatchOrChildMatch(link, activeItem)} link={link}>
+                {coreItems.map((link, index) => (
+                  <NavBarItem
+                    key={`${link.id}-${index}`}
+                    isActive={isMatchOrChildMatch(link, activeItem)}
+                    link={{ ...link, subTitle: undefined, onClick: undefined }}
+                  >
                     {link.icon && <Icon name={link.icon as IconName} size="xl" />}
                     {link.img && <img src={link.img} alt={`${link.text} logo`} />}
                   </NavBarItem>
                 ))}
+
+                {pluginItems.length > 0 &&
+                  pluginItems.map((link, index) => (
+                    <NavBarItem
+                      key={`${link.id}-${index}`}
+                      isActive={isMatchOrChildMatch(link, activeItem)}
+                      link={link}
+                    >
+                      {link.icon && <Icon name={link.icon as IconName} size="xl" />}
+                      {link.img && <img src={link.img} alt={`${link.text} logo`} />}
+                    </NavBarItem>
+                  ))}
+                <div className={styles.scrollBottomMarker} ref={scrollBottomRef} />
+              </div>
             </CustomScrollbar>
 
             {configItems.map((link, index) => (
@@ -262,5 +299,55 @@ const getStyles = (theme: GrafanaTheme2) => ({
   }),
   menuPortalContainer: css({
     zIndex: theme.zIndex.sidemenu,
+  }),
+  'scrollTopMarker, scrollBottomMarker': css({
+    height: theme.spacing(1),
+    left: 0,
+    position: 'absolute',
+    pointerEvents: 'none',
+    right: 0,
+  }),
+  scrollTopMarker: css({
+    top: 0,
+  }),
+  scrollBottomMarker: css({
+    bottom: 0,
+  }),
+  scrollContent: css({
+    position: 'relative',
+  }),
+  scrollContainer: css({
+    '&:before, &:after': {
+      content: "''",
+      color: theme.colors.text.primary,
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      height: theme.spacing(6),
+      opacity: 0,
+      pointerEvents: 'none',
+      transition: 'opacity 0.2s ease-in-out',
+      zIndex: theme.zIndex.sidemenu - 1,
+    },
+    '&:before': {
+      borderTop: `1px solid ${theme.colors.border.medium}`,
+      top: 0,
+      background: `linear-gradient(0deg, transparent, ${theme.colors.background.canvas})`,
+    },
+    '&:after': {
+      borderBottom: `1px solid ${theme.colors.border.medium}`,
+      bottom: 0,
+      background: `linear-gradient(0deg, ${theme.colors.background.canvas}, transparent)`,
+    },
+  }),
+  scrollTopVisible: css({
+    '&:before': {
+      opacity: 1,
+    },
+  }),
+  scrollBottomVisible: css({
+    '&:after': {
+      opacity: 1,
+    },
   }),
 });
