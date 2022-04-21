ad35db86360221163187f2d831990962095b31c2
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-04-21 05:53:32 -0400

    Service accounts: rename feature toggle (#48037) (#48040)
    * refactor: renaming service-accounts to serviceAccounts
    
    * refactor: renaming service-accounts to serviceAccounts in docs
    
    * tests
    
    (cherry picked from commit 8677552dda7b63f5d44df6fe51ca8a28726c5a5c)
    
    Co-authored-by: Eric Leijonmarck <eric.leijonmarck@gmail.com>

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -19,7 +19,7 @@ export interface FeatureToggles {
   trimDefaults?: boolean;
   envelopeEncryption?: boolean;
   httpclientprovider_azure_auth?: boolean;
-  ['service-accounts']?: boolean;
+  serviceAccounts?: boolean;
   database_metrics?: boolean;
   dashboardPreviews?: boolean;
   dashboardPreviewsScheduler?: boolean;
