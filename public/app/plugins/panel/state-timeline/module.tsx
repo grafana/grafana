7c1c5da6f38ca3c2b1ea5a694d837059b8be30a0
Kyle Cunningham <kyle@codeincarnate.com>
2022-04-22 17:13:57 -0500

    Note support for annotations in module file

--- public/app/plugins/panel/state-timeline/module.tsx
+++ public/app/plugins/panel/state-timeline/module.tsx
@@ -105,4 +105,5 @@ export const plugin = new PanelPlugin<TimelineOptions, TimelineFieldConfig>(Stat
     commonOptionsBuilder.addLegendOptions(builder, false);
     commonOptionsBuilder.addTooltipOptions(builder, true);
   })
-  .setSuggestionsSupplier(new StatTimelineSuggestionsSupplier());
+  .setSuggestionsSupplier(new StatTimelineSuggestionsSupplier())
+  .setDataSupport({ annotations: true });
