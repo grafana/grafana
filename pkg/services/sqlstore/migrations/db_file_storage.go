28f359ff9702f42620d5d4637985b065357c7d3e
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-04-22 11:59:39 +0400

    #45498: refactor db_filestorage FS API backend - use path_hash in DB schema

--- pkg/services/sqlstore/migrations/db_file_storage.go
+++ pkg/services/sqlstore/migrations/db_file_storage.go
@@ -9,7 +9,8 @@ func addDbFileStorageMigration(mg *migrator.Migrator) {
 		Name: "file",
 		Columns: []*migrator.Column{
 			{Name: "path", Type: migrator.DB_NVarchar, Length: 1024, Nullable: false},
-			{Name: "parent_folder_path", Type: migrator.DB_NVarchar, Length: 1024, Nullable: false},
+			{Name: "path_hash", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
+			{Name: "parent_folder_path_hash", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
 			{Name: "contents", Type: migrator.DB_Blob, Nullable: false},
 			{Name: "updated", Type: migrator.DB_DateTime, Nullable: false},
 			{Name: "created", Type: migrator.DB_DateTime, Nullable: false},
@@ -17,22 +18,24 @@ func addDbFileStorageMigration(mg *migrator.Migrator) {
 			{Name: "mime_type", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
 		},
 		Indices: []*migrator.Index{
-			{Cols: []string{"path"}, Type: migrator.UniqueIndex},
+			{Cols: []string{"path_hash"}, Type: migrator.UniqueIndex},
+			{Cols: []string{"parent_folder_path_hash"}},
 		},
 	}
 
 	mg.AddMigration("create file table", migrator.NewAddTableMigration(filesTable))
 	mg.AddMigration("file table idx: path natural pk", migrator.NewAddIndexMigration(filesTable, filesTable.Indices[0]))
+	mg.AddMigration("file table idx: parent_folder_path_hash fast folder retrieval", migrator.NewAddIndexMigration(filesTable, filesTable.Indices[1]))
 
 	fileMetaTable := migrator.Table{
 		Name: "file_meta",
 		Columns: []*migrator.Column{
-			{Name: "path", Type: migrator.DB_NVarchar, Length: 1024, Nullable: false},
-			{Name: "key", Type: migrator.DB_NVarchar, Length: 1024, Nullable: false},
+			{Name: "path_hash", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
+			{Name: "key", Type: migrator.DB_NVarchar, Length: 255, Nullable: false},
 			{Name: "value", Type: migrator.DB_NVarchar, Length: 1024, Nullable: false},
 		},
 		Indices: []*migrator.Index{
-			{Cols: []string{"path", "key"}, Type: migrator.UniqueIndex},
+			{Cols: []string{"path_hash", "key"}, Type: migrator.UniqueIndex},
 		},
 	}
 
