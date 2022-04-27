09842dcc0a17977649ca809f5bfcbc02d24d1157
Owen Smallwood <owen.smallwood@grafana.com>
2022-04-27 11:43:35 -0600

    generates mocks. Adds tests for public dashboard feature flag

--- pkg/api/dashboard_share_test.go
+++ pkg/api/dashboard_share_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/query"
 	"github.com/grafana/grafana/pkg/services/secrets/fakes"
@@ -41,7 +42,39 @@ func TestReturns404WhenFeatureNotEnabled(t *testing.T) {
 			strings.NewReader("{ isPublic: true }"),
 			t,
 		)
+		assert.Equal(t, http.StatusNotFound, response.Code)
+	})
+}
+
+func TestReturnsSuccessWhenFeatureEnabledAndSetsPublicFlagOnDashboard(t *testing.T) {
+	sc := setupHTTPServerWithMockDb(t, false, false)
+
+	setInitCtxSignedInViewer(sc.initCtx)
+	sc.hs.queryDataService = query.ProvideService(
+		nil,
+		nil,
+		nil,
+		&fakePluginRequestValidator{},
+		fakes.NewFakeSecretsService(),
+		&dashboardFakePluginClient{},
+		&fakeOAuthTokenService{},
+	)
+
+	sc.hs.Features = featuremgmt.WithFeatures(featuremgmt.FlagPublicDashboards, true)
+	dashboardUid := "1"
+
+	t.Run("get 200 when feature flag on and public flag set on dashboard", func(t *testing.T) {
+		response := callAPI(
+			sc.server,
+			http.MethodPost,
+			fmt.Sprintf("/api/dashboards/uid/%s/sharing", dashboardUid),
+			strings.NewReader("{ isPublic: true }"),
+			t,
+		)
 		assert.Equal(t, http.StatusOK, response.Code)
+		fmt.Println(response.Body)
+		respJSON, _ := simplejson.NewJson(response.Body.Bytes())
+		assert.Equal(t, "true", respJSON.Get("isPublic"))
 	})
 }
 
