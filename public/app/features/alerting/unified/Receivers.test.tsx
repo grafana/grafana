da78166c9d84d94366a99165a3d4da11ce1cd471
Konrad Lalik <konrad.lalik@grafana.com>
2022-04-27 14:48:53 +0200

    Bring back the test

--- public/app/features/alerting/unified/Receivers.test.tsx
+++ public/app/features/alerting/unified/Receivers.test.tsx
@@ -308,18 +308,18 @@ describe('Receivers', () => {
     });
   });
 
-  // it('Hides create contact point button for users without permission', () => {
-  //   mocks.api.fetchConfig.mockResolvedValue(someGrafanaAlertManagerConfig);
-  //   mocks.api.updateConfig.mockResolvedValue();
-  //   mocks.contextSrv.hasAccess.mockImplementation((action) =>
-  //     [AccessControlAction.AlertingNotificationsRead, AccessControlAction.AlertingNotificationsExternalRead].some(
-  //       (a) => a === action
-  //     )
-  //   );
-  //   renderReceivers();
+  it('Hides create contact point button for users without permission', () => {
+    mocks.api.fetchConfig.mockResolvedValue(someGrafanaAlertManagerConfig);
+    mocks.api.updateConfig.mockResolvedValue();
+    mocks.contextSrv.hasAccess.mockImplementation((action) =>
+      [AccessControlAction.AlertingNotificationsRead, AccessControlAction.AlertingNotificationsExternalRead].some(
+        (a) => a === action
+      )
+    );
+    renderReceivers();
 
-  //   expect(ui.newContactPointButton.query()).not.toBeInTheDocument();
-  // });
+    expect(ui.newContactPointButton.query()).not.toBeInTheDocument();
+  });
 
   it('Cloud alertmanager receiver can be edited', async () => {
     mocks.api.fetchConfig.mockResolvedValue(someCloudAlertManagerConfig);
