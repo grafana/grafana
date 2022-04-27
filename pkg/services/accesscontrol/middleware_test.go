0379548a6fc533ea8cb41b3e156542124e1559fe
Karl Persson <kalle.persson@grafana.com>
2022-04-27 10:48:47 +0200

    fix test

--- pkg/services/accesscontrol/middleware_test.go
+++ pkg/services/accesscontrol/middleware_test.go
@@ -1,24 +1,25 @@
-package accesscontrol
+package accesscontrol_test
 
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/web"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/mock"
+	"github.com/grafana/grafana/pkg/web"
 )
 
 type middlewareTestCase struct {
 	desc           string
 	expectFallback bool
 	expectEndpoint bool
-	evaluator      Evaluator
-	ac             AccessControl
+	evaluator      accesscontrol.Evaluator
+	ac             accesscontrol.AccessControl
 }
 
 func TestMiddleware(t *testing.T) {
@@ -32,18 +33,18 @@ func TestMiddleware(t *testing.T) {
 		{
 			desc: "should pass middleware for correct permissions",
 			ac: mock.New().WithPermissions(
-				[]*Permission{{Action: "users:read", Scope: "users:*"}},
+				[]*accesscontrol.Permission{{Action: "users:read", Scope: "users:*"}},
 			),
-			evaluator:      EvalPermission("users:read", "users:*"),
+			evaluator:      accesscontrol.EvalPermission("users:read", "users:*"),
 			expectFallback: false,
 			expectEndpoint: true,
 		},
 		{
 			desc: "should not reach endpoint when missing permissions",
 			ac: mock.New().WithPermissions(
-				[]*Permission{{Action: "users:read", Scope: "users:1"}},
+				[]*accesscontrol.Permission{{Action: "users:read", Scope: "users:1"}},
 			),
-			evaluator:      EvalPermission("users:read", "users:*"),
+			evaluator:      accesscontrol.EvalPermission("users:read", "users:*"),
 			expectFallback: false,
 			expectEndpoint: false,
 		},
@@ -60,7 +61,7 @@ func TestMiddleware(t *testing.T) {
 			server.UseMiddleware(web.Renderer("../../public/views", "[[", "]]"))
 
 			server.Use(contextProvider())
-			server.Use(Middleware(test.ac)(fallback, test.evaluator))
+			server.Use(accesscontrol.Middleware(test.ac)(fallback, test.evaluator))
 
 			endpointCalled := false
 			server.Get("/", func(c *models.ReqContext) {
