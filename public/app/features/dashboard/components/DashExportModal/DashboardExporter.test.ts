72d4b1647e35b440bddab6811601bdcf40da2fcd
Torkel Ödegaard <torkel@grafana.com>
2022-04-28 12:24:19 +0200

    DashboardExport: Fixes issues sharing dashboards where query data source was set wrong

--- public/app/features/dashboard/components/DashExportModal/DashboardExporter.test.ts
+++ public/app/features/dashboard/components/DashExportModal/DashboardExporter.test.ts
@@ -46,116 +46,6 @@ variableAdapters.register(createDataSourceVariableAdapter());
 
 it('handles a default datasource in a template variable', async () => {
   const dashboard: any = {
-    annotations: {
-      list: [
-        {
-          builtIn: 1,
-          datasource: '-- Grafana --',
-          enable: true,
-          hide: true,
-          iconColor: 'rgba(0, 211, 255, 1)',
-          name: 'Annotations & Alerts',
-          target: {
-            limit: 100,
-            matchAny: false,
-            tags: [],
-            type: 'dashboard',
-          },
-          type: 'dashboard',
-        },
-      ],
-    },
-    editable: true,
-    fiscalYearStartMonth: 0,
-    graphTooltip: 0,
-    id: 331,
-    iteration: 1642157860116,
-    links: [],
-    liveNow: false,
-    panels: [
-      {
-        fieldConfig: {
-          defaults: {
-            color: {
-              mode: 'palette-classic',
-            },
-            custom: {
-              axisLabel: '',
-              axisPlacement: 'auto',
-              barAlignment: 0,
-              drawStyle: 'line',
-              fillOpacity: 0,
-              gradientMode: 'none',
-              hideFrom: {
-                legend: false,
-                tooltip: false,
-                viz: false,
-              },
-              lineInterpolation: 'linear',
-              lineWidth: 1,
-              pointSize: 5,
-              scaleDistribution: {
-                type: 'linear',
-              },
-              showPoints: 'auto',
-              spanNulls: false,
-              stacking: {
-                group: 'A',
-                mode: 'none',
-              },
-              thresholdsStyle: {
-                mode: 'off',
-              },
-            },
-            mappings: [],
-            thresholds: {
-              mode: 'absolute',
-              steps: [
-                {
-                  color: 'green',
-                  value: null,
-                },
-                {
-                  color: 'red',
-                  value: 80,
-                },
-              ],
-            },
-          },
-          overrides: [],
-        },
-        gridPos: {
-          h: 9,
-          w: 12,
-          x: 0,
-          y: 0,
-        },
-        id: 2,
-        options: {
-          legend: {
-            calcs: [],
-            displayMode: 'list',
-            placement: 'bottom',
-          },
-          tooltip: {
-            mode: 'single',
-            sort: 'none',
-          },
-        },
-        targets: [
-          {
-            datasource: {
-              type: 'testdata',
-              uid: 'PD8C576611E62080A',
-            },
-            expr: '{filename="/var/log/system.log"}',
-            refId: 'A',
-          },
-        ],
-        title: 'Panel Title',
-        type: 'timeseries',
-      },
-    ],
     templating: {
       list: [
         {
@@ -184,6 +74,27 @@ it('handles a default datasource in a template variable', async () => {
   const exporter = new DashboardExporter();
   const exported: any = await exporter.makeExportable(dashboardModel);
   expect(exported.templating.list[0].datasource.uid).toBe('${DS_GFDB}');
+});
+
+it('If a panel queries has no datasource prop ignore it', async () => {
+  const dashboard: any = {
+    panels: [
+      {
+        id: 1,
+        type: 'graph',
+        datasource: {
+          uid: 'other',
+          type: 'other',
+        },
+        targets: [{ refId: 'A', a: 'A' }],
+      },
+    ],
+  };
+  const dashboardModel = new DashboardModel(dashboard, {}, () => []);
+  const exporter = new DashboardExporter();
+  const exported: any = await exporter.makeExportable(dashboardModel);
+  expect(exported.panels[0].datasource).toEqual({ uid: '${DS_OTHER}', type: 'other' });
+  expect(exported.panels[0].targets[0].datasource).toEqual({ uid: '${DS_OTHER}', type: 'other' });
 });
 
 describe('given dashboard with repeated panels', () => {
@@ -323,7 +234,7 @@ describe('given dashboard with repeated panels', () => {
     const panel = exported.panels[7];
     expect(exported.__inputs.some((ds: Record<string, string>) => ds.name === 'DS_GFDB')).toBeTruthy();
     expect(panel.datasource.uid).toBe('${DS_GFDB}');
-    expect(panel.targets[0].datasource).toBe('${DS_GFDB}');
+    expect(panel.targets[0].datasource).toEqual({ type: 'testdb', uid: '${DS_GFDB}' });
   });
 
   it('should not include default datasource in __inputs unnecessarily', async () => {
