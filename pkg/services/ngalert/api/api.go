dc25c710a51e1054827078e63d4246ecb879e733
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2022-04-28 20:18:22 +0200

    Alerting: indicate whether an alertrule is provisioned

--- pkg/services/ngalert/api/api.go
+++ pkg/services/ngalert/api/api.go
@@ -67,6 +67,7 @@ type API struct {
 	QuotaService         *quota.QuotaService
 	Schedule             schedule.ScheduleService
 	TransactionManager   provisioning.TransactionManager
+	ProvenanceStore      provisioning.ProvisioningStore
 	RuleStore            store.RuleStore
 	InstanceStore        store.InstanceStore
 	AlertingStore        AlertingStore
@@ -108,6 +109,7 @@ func (api *API) RegisterAPIEndpoints(m *metrics.API) {
 			QuotaService:    api.QuotaService,
 			scheduleService: api.Schedule,
 			store:           api.RuleStore,
+			provenanceStore: api.ProvenanceStore,
 			xactManager:     api.TransactionManager,
 			log:             logger,
 			cfg:             &api.Cfg.UnifiedAlerting,
--- pkg/services/ngalert/ngalert.go
+++ pkg/services/ngalert/ngalert.go
@@ -155,6 +155,7 @@ func (ng *AlertNG) init() error {
 		RuleStore:            store,
 		AlertingStore:        store,
 		AdminConfigStore:     store,
+		ProvenanceStore:      store,
 		MultiOrgAlertmanager: ng.MultiOrgAlertmanager,
 		StateManager:         ng.stateManager,
 		AccessControl:        ng.accesscontrol,
