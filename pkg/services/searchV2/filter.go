28dd6be960f57b0895582da21b1845ef98bd72a4
Ryan McKinley <ryantxu@gmail.com>
2022-04-29 16:04:08 -0700

    add explain

---
+++ pkg/services/searchV2/filter.go
@@ -0,0 +1,67 @@
+package searchV2
+
+import (
+	"fmt"
+
+	"github.com/blugelabs/bluge/search"
+	"github.com/blugelabs/bluge/search/searcher"
+	"github.com/blugelabs/bluge/search/similarity"
+)
+
+type PermissionFilter struct {
+	field string
+	who   string
+	what  string
+}
+
+// who and what are part of the query request
+func newPermissionFilter(who string, what string) *PermissionFilter {
+	return &PermissionFilter{
+		who:  who,
+		what: what,
+	}
+}
+
+// Location returns the location being queried
+func (q *PermissionFilter) Who() string {
+	return q.who
+}
+
+func (q *PermissionFilter) SetField(f string) *PermissionFilter {
+	q.field = f
+	return q
+}
+
+func (q *PermissionFilter) Field() string {
+	return q.field
+}
+
+func (q *PermissionFilter) Searcher(i search.Reader, options search.SearcherOptions) (search.Searcher, error) {
+	field := q.field
+	if q.field == "" {
+		field = options.DefaultSearchField
+	}
+
+	fmt.Printf("open reader: %s\n", field)
+	dvReader, err := i.DocumentValueReader([]string{field})
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := searcher.NewMatchAllSearcher(i, 1, similarity.ConstantScorer(1), options)
+	return searcher.NewFilteringSearcher(s, func(d *search.DocumentMatch) bool {
+		var rule string
+		err := dvReader.VisitDocumentValues(d.Number, func(field string, term []byte) {
+			rule = string(term)
+		})
+		if err != nil {
+			return false
+		}
+		fmt.Printf("check permissions: [%d] %s|%s // %s\n", d.Number, q.who, q.what, rule)
+		return true
+	}), err
+}
+
+func (q *PermissionFilter) Validate() error {
+	return nil
+}
