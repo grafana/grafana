6c2f62856e71da45d373dc3243e7b8a9bd7030f0
Jesse Weaver <jesse.weaver@grafana.com>
2022-04-29 16:22:09 -0600

    Extract basic queries with no error checking

--- pkg/models/dashboard_queries.go
+++ pkg/models/dashboard_queries.go
@@ -1,13 +1,22 @@
 package models
 
-import "github.com/grafana/grafana/pkg/components/simplejson"
+import (
+	"github.com/grafana/grafana/pkg/components/simplejson"
+)
 
 func GetQueriesFromDashboard(dashboard *simplejson.Json) map[int64][]*simplejson.Json {
 	result := make(map[int64][]*simplejson.Json)
 
 	for _, panelObj := range dashboard.Get("panels").MustArray() {
 		panel := simplejson.NewFromAny(panelObj)
-		result[panel.Get("id").MustInt64()] = nil
+
+		var panelQueries []*simplejson.Json
+
+		for _, queryObj := range panel.Get("targets").MustArray() {
+			panelQueries = append(panelQueries, simplejson.NewFromAny(queryObj))
+		}
+
+		result[panel.Get("id").MustInt64()] = panelQueries
 	}
 
 	return result
--- pkg/models/dashboard_queries_test.go
+++ pkg/models/dashboard_queries_test.go
@@ -8,90 +8,217 @@ import (
 )
 
 const (
-	simpleDashboard = `{
-	  "panels": [
-	    {
-	      "fieldConfig": {
-	        "defaults": {
-	          "color": {
-	            "mode": "palette-classic"
-	          },
-	          "custom": {
-	            "axisLabel": "",
-	            "axisPlacement": "auto",
-	            "barAlignment": 0,
-	            "drawStyle": "line",
-	            "fillOpacity": 0,
-	            "gradientMode": "none",
-	            "hideFrom": {
-	              "legend": false,
-	              "tooltip": false,
-	              "viz": false
-	            },
-	            "lineInterpolation": "linear",
-	            "lineWidth": 1,
-	            "pointSize": 5,
-	            "scaleDistribution": {
-	              "type": "linear"
-	            },
-	            "showPoints": "auto",
-	            "spanNulls": false,
-	            "stacking": {
-	              "group": "A",
-	              "mode": "none"
-	            },
-	            "thresholdsStyle": {
-	              "mode": "off"
-	            }
-	          },
-	          "mappings": [],
-	          "thresholds": {
-	            "mode": "absolute",
-	            "steps": [
-	              {
-	                "color": "green",
-	                "value": null
-	              },
-	              {
-	                "color": "red",
-	                "value": 80
-	              }
-	            ]
-	          }
-	        },
-	        "overrides": []
-	      },
-	      "gridPos": {
-	        "h": 9,
-	        "w": 12,
-	        "x": 0,
-	        "y": 0
-	      },
-	      "id": 2,
-	      "options": {
-	        "legend": {
-	          "calcs": [],
-	          "displayMode": "list",
-	          "placement": "bottom"
-	        },
-	        "tooltip": {
-	          "mode": "single",
-	          "sort": "none"
-	        }
-	      },
-	      "title": "Panel Title",
-	      "type": "timeseries"
-	    }
-	  ],
-	  "schemaVersion": 35
-	}`
+	dashboardWithNoQueries = `
+{
+  "panels": [
+    {
+      "fieldConfig": {
+        "defaults": {
+          "color": {
+            "mode": "palette-classic"
+          },
+          "custom": {
+            "axisLabel": "",
+            "axisPlacement": "auto",
+            "barAlignment": 0,
+            "drawStyle": "line",
+            "fillOpacity": 0,
+            "gradientMode": "none",
+            "hideFrom": {
+              "legend": false,
+              "tooltip": false,
+              "viz": false
+            },
+            "lineInterpolation": "linear",
+            "lineWidth": 1,
+            "pointSize": 5,
+            "scaleDistribution": {
+              "type": "linear"
+            },
+            "showPoints": "auto",
+            "spanNulls": false,
+            "stacking": {
+              "group": "A",
+              "mode": "none"
+            },
+            "thresholdsStyle": {
+              "mode": "off"
+            }
+          },
+          "mappings": [],
+          "thresholds": {
+            "mode": "absolute",
+            "steps": [
+              {
+                "color": "green",
+                "value": null
+              },
+              {
+                "color": "red",
+                "value": 80
+              }
+            ]
+          }
+        },
+        "overrides": []
+      },
+      "gridPos": {
+        "h": 9,
+        "w": 12,
+        "x": 0,
+        "y": 0
+      },
+      "id": 2,
+      "options": {
+        "legend": {
+          "calcs": [],
+          "displayMode": "list",
+          "placement": "bottom"
+        },
+        "tooltip": {
+          "mode": "single",
+          "sort": "none"
+        }
+      },
+      "title": "Panel Title",
+      "type": "timeseries"
+    }
+  ],
+  "schemaVersion": 35
+}`
+
+	dashboardWithQueries = `
+{
+  "panels": [
+    {
+      "fieldConfig": {
+        "defaults": {
+          "color": {
+            "mode": "palette-classic"
+          },
+          "custom": {
+            "axisLabel": "",
+            "axisPlacement": "auto",
+            "barAlignment": 0,
+            "drawStyle": "line",
+            "fillOpacity": 0,
+            "gradientMode": "none",
+            "hideFrom": {
+              "legend": false,
+              "tooltip": false,
+              "viz": false
+            },
+            "lineInterpolation": "linear",
+            "lineWidth": 1,
+            "pointSize": 5,
+            "scaleDistribution": {
+              "type": "linear"
+            },
+            "showPoints": "auto",
+            "spanNulls": false,
+            "stacking": {
+              "group": "A",
+              "mode": "none"
+            },
+            "thresholdsStyle": {
+              "mode": "off"
+            }
+          },
+          "mappings": [],
+          "thresholds": {
+            "mode": "absolute",
+            "steps": [
+              {
+                "color": "green",
+                "value": null
+              },
+              {
+                "color": "red",
+                "value": 80
+              }
+            ]
+          }
+        },
+        "overrides": []
+      },
+      "gridPos": {
+        "h": 9,
+        "w": 12,
+        "x": 0,
+        "y": 0
+      },
+      "id": 2,
+      "options": {
+        "legend": {
+          "calcs": [],
+          "displayMode": "list",
+          "placement": "bottom"
+        },
+        "tooltip": {
+          "mode": "single",
+          "sort": "none"
+        }
+      },
+      "targets": [
+        {
+          "datasource": {
+            "type": "prometheus",
+            "uid": "_yxMP8Ynk"
+          },
+          "exemplar": true,
+          "expr": "go_goroutines{job=\"$job\"}",
+          "interval": "",
+          "legendFormat": "",
+          "refId": "A"
+        }
+      ],
+      "title": "Panel Title",
+      "type": "timeseries"
+    }
+  ],
+  "schemaVersion": 35
+}`
 )
 
 func TestGetQueriesFromDashboard(t *testing.T) {
-	json, err := simplejson.NewJson([]byte(simpleDashboard))
-	require.NoError(t, err)
+	t.Run("can extract no queries from empty dashboard", func(t *testing.T) {
+		json, err := simplejson.NewJson([]byte(`{"panels": {}}`))
+		require.NoError(t, err)
 
-	queries := GetQueriesFromDashboard(json)
-	require.Len(t, queries, 1)
-	require.Contains(t, queries, int64(2))
+		queries := GetQueriesFromDashboard(json)
+		require.Len(t, queries, 0)
+	})
+
+	t.Run("can extract no queries from empty panel", func(t *testing.T) {
+		json, err := simplejson.NewJson([]byte(dashboardWithNoQueries))
+		require.NoError(t, err)
+
+		queries := GetQueriesFromDashboard(json)
+		require.Len(t, queries, 1)
+		require.Contains(t, queries, int64(2))
+		require.Len(t, queries[2], 0)
+	})
+
+	t.Run("can extract queries from panels", func(t *testing.T) {
+		json, err := simplejson.NewJson([]byte(dashboardWithQueries))
+		require.NoError(t, err)
+
+		queries := GetQueriesFromDashboard(json)
+		require.Len(t, queries, 1)
+		require.Contains(t, queries, int64(2))
+		require.Len(t, queries[2], 1)
+		query, err := queries[2][0].MarshalJSON()
+		require.NoError(t, err)
+		require.JSONEq(t, `{
+            "datasource": {
+              "type": "prometheus",
+              "uid": "_yxMP8Ynk"
+            },
+            "exemplar": true,
+            "expr": "go_goroutines{job=\"$job\"}",
+            "interval": "",
+            "legendFormat": "",
+            "refId": "A"
+		}`, string(query))
+	})
 }
