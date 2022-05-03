e843c5c83e1ca381e3087197eb6c4f7f66d250a6
Ryan McKinley <ryantxu@gmail.com>
2022-05-02 22:57:39 -0700

    add stars service

--- pkg/api/stars.go
+++ pkg/api/stars.go
@@ -6,8 +6,36 @@ import (
 
 	"github.com/grafana/grafana/pkg/api/response"
 	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/web"
 )
+
+func (hs *HTTPServer) GetStars(c *models.ReqContext) response.Response {
+	// SELECT uid FROM star JOIN dashboard ON star.dashboard_id = dashboard.id
+	// WHERE star.user_id = 1
+	type row struct {
+		Uid string
+	}
+	rows := make([]*row, 0, 1000)
+
+	err := hs.SQLStore.WithDbSession(c.Req.Context(), func(sess *sqlstore.DBSession) error {
+		sess.Table("star").
+			Join("INNER", "dashboard", "star.dashboard_id = dashboard.id").
+			Where("user_id = ?", c.UserId)
+		sess.Cols("uid")
+		return sess.Find(&rows)
+	})
+	if err != nil {
+		return response.Error(500, "error finding stars", err)
+	}
+
+	// Add all UIDs to star response
+	uids := []string{}
+	for _, row := range rows {
+		uids = append(uids, row.Uid)
+	}
+	return response.JSON(200, uids)
+}
 
 func (hs *HTTPServer) StarDashboard(c *models.ReqContext) response.Response {
 	id, err := strconv.ParseInt(web.Params(c.Req)[":id"], 10, 64)
