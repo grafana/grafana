9e20e2b5fe4b78f3be0ea15bf0ca05c3d0cd9cad
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2022-05-03 09:27:12 -0400

    remove answered questions and update index

--- pkg/services/sqlstore/playlist_test.go
+++ pkg/services/sqlstore/playlist_test.go
@@ -1,14 +1,13 @@
-//go:build integration
-// +build integration
-
 package sqlstore
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/models"
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/grafana/pkg/models"
 )
 
 func TestIntegrationPlaylistDataAccess(t *testing.T) {
@@ -23,30 +22,34 @@ func TestIntegrationPlaylistDataAccess(t *testing.T) {
 		err := ss.CreatePlaylist(context.Background(), &cmd)
 		require.NoError(t, err)
 
+		uid := cmd.Result.Uid
+		id := cmd.Result.Id
+		fmt.Printf("uid: %s, id: %d\n", uid, id)
+
 		t.Run("Can update playlist", func(t *testing.T) {
 			items := []models.PlaylistItemDTO{
 				{Title: "influxdb", Value: "influxdb", Type: "dashboard_by_tag"},
 				{Title: "Backend response times", Value: "2", Type: "dashboard_by_id"},
 			}
-			query := models.UpdatePlaylistCommand{Name: "NYC office ", OrgId: 1, Id: 1, Interval: "10s", Items: items}
+			query := models.UpdatePlaylistCommand{Name: "NYC office ", OrgId: 1, Uid: uid, Interval: "10s", Items: items}
 			err = ss.UpdatePlaylist(context.Background(), &query)
 			require.NoError(t, err)
 		})
 
 		t.Run("Can remove playlist", func(t *testing.T) {
-			deleteQuery := models.DeletePlaylistCommand{Id: 1, OrgId: 1}
+			deleteQuery := models.DeletePlaylistCommand{Uid: uid, OrgId: 1}
 			err = ss.DeletePlaylist(context.Background(), &deleteQuery)
 			require.NoError(t, err)
 
-			getQuery := models.GetPlaylistByIdQuery{Id: 1}
+			getQuery := models.GetPlaylistByUidQuery{Uid: uid}
 			err = ss.GetPlaylist(context.Background(), &getQuery)
 			require.NoError(t, err)
-			require.Equal(t, int64(0), getQuery.Result.Id, "playlist should've been removed")
+			require.Equal(t, uid, getQuery.Result.Uid, "playlist should've been removed")
 		})
 	})
 
 	t.Run("Delete playlist that doesn't exist", func(t *testing.T) {
-		deleteQuery := models.DeletePlaylistCommand{Id: 1, OrgId: 1}
+		deleteQuery := models.DeletePlaylistCommand{Uid: "654312", OrgId: 1}
 		err := ss.DeletePlaylist(context.Background(), &deleteQuery)
 		require.NoError(t, err)
 	})
@@ -57,8 +60,8 @@ func TestIntegrationPlaylistDataAccess(t *testing.T) {
 			cmd  models.DeletePlaylistCommand
 		}{
 			{desc: "none", cmd: models.DeletePlaylistCommand{}},
-			{desc: "no OrgId", cmd: models.DeletePlaylistCommand{Id: 1}},
-			{desc: "no Id", cmd: models.DeletePlaylistCommand{OrgId: 1}},
+			{desc: "no OrgId", cmd: models.DeletePlaylistCommand{Uid: "1"}},
+			{desc: "no Uid", cmd: models.DeletePlaylistCommand{OrgId: 1}},
 		}
 
 		for _, tc := range testCases {
