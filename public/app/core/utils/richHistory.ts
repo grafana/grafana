8da4eca4e2904100c407202c8c482e8c4cee6ebb
Piotr Jamróz <pm.jamroz@gmail.com>
2022-05-04 15:20:08 +0200

    Revert error logging test

--- public/app/core/utils/richHistory.ts
+++ public/app/core/utils/richHistory.ts
@@ -2,7 +2,7 @@ import { omit } from 'lodash';
 
 import { DataQuery, DataSourceApi, dateTimeFormat, ExploreUrlState, urlUtil } from '@grafana/data';
 import { serializeStateToUrlParam } from '@grafana/data/src/utils/url';
-import { getDataSourceSrv, logError } from '@grafana/runtime';
+import { getDataSourceSrv } from '@grafana/runtime';
 import { notifyApp } from 'app/core/actions';
 import { createErrorNotification, createWarningNotification } from 'app/core/copy/appNotification';
 import { dispatch } from 'app/store/store';
@@ -77,11 +77,6 @@ export async function addToRichHistory(
 }
 
 export async function getRichHistory(filters: RichHistorySearchFilters): Promise<RichHistoryQuery[]> {
-  try {
-    throw new Error('test');
-  } catch (error) {
-    logError(error);
-  }
   return await getRichHistoryStorage().getRichHistory(filters);
 }
 
