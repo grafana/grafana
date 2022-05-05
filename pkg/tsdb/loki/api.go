46b40b6e82417661078be5f1c4a9d802e440d9ec
Gábor Farkas <gabor.farkas@gmail.com>
2022-05-05 08:47:40 +0200

    Loki: backend: use streaming JSON parser (#47656)
    * converter: remove __name__ customization because Loki does not do that
    
    Loki does not handle __name__ in a special way.
    for Prometheus, the caller can implement the formatting
    by themselves
    
    * converter: change labels-formatting
    
    the labels.String() method does not handle strange values well
    
    * loki: backend: use streaming-json parser
    
    * more idiomatic code
    
    Co-authored-by: Todd Treece <360020+toddtreece@users.noreply.github.com>
    
    * simpler row-length check
    
    * simpler code
    
    Co-authored-by: Todd Treece <360020+toddtreece@users.noreply.github.com>

--- pkg/tsdb/loki/api.go
+++ pkg/tsdb/loki/api.go
@@ -10,8 +10,9 @@ import (
 	"net/url"
 	"strconv"
 
+	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/loki/pkg/loghttp"
+	"github.com/grafana/grafana/pkg/util/converter"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -135,7 +136,7 @@ func makeLokiError(body io.ReadCloser) error {
 	return fmt.Errorf("%v", errorMessage)
 }
 
-func (api *LokiAPI) DataQuery(ctx context.Context, query lokiQuery) (*loghttp.QueryResponse, error) {
+func (api *LokiAPI) DataQuery(ctx context.Context, query lokiQuery) (data.Frames, error) {
 	req, err := makeDataRequest(ctx, api.url, query)
 	if err != nil {
 		return nil, err
@@ -156,13 +157,14 @@ func (api *LokiAPI) DataQuery(ctx context.Context, query lokiQuery) (*loghttp.Qu
 		return nil, makeLokiError(resp.Body)
 	}
 
-	var response loghttp.QueryResponse
-	err = jsoniter.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		return nil, err
+	iter := jsoniter.Parse(jsoniter.ConfigDefault, resp.Body, 1024)
+	res := converter.ReadPrometheusStyleResult(iter)
+
+	if res.Error != nil {
+		return nil, res.Error
 	}
 
-	return &response, nil
+	return res.Frames, nil
 }
 
 func makeRawRequest(ctx context.Context, lokiDsUrl string, resourceURL string) (*http.Request, error) {
