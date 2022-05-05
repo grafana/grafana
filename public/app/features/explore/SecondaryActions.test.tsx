89c7458f5730e75d3189560d65071a2ec3fc0f42
Laura <laura.benz@grafana.com>
2022-05-05 10:08:28 +0200

    Chore: transfer three tests to testing-library

--- public/app/features/explore/SecondaryActions.test.tsx
+++ public/app/features/explore/SecondaryActions.test.tsx
@@ -1,28 +1,29 @@
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { noop } from 'lodash';
 import React from 'react';
 
 import { SecondaryActions } from './SecondaryActions';
 
-const addQueryRowButtonSelector = '[aria-label="Add row button"]';
-const richHistoryButtonSelector = '[aria-label="Rich history button"]';
+// TODO: remove!
 const queryInspectorButtonSelector = '[aria-label="Query inspector button"]';
 
 describe('SecondaryActions', () => {
-  it('should render component two buttons', () => {
-    const wrapper = shallow(
+  it('should render component with three buttons', () => {
+    render(
       <SecondaryActions
         onClickAddQueryRowButton={noop}
         onClickRichHistoryButton={noop}
         onClickQueryInspectorButton={noop}
       />
     );
-    expect(wrapper.find(addQueryRowButtonSelector)).toHaveLength(1);
-    expect(wrapper.find(richHistoryButtonSelector)).toHaveLength(1);
+
+    expect(screen.getByRole('button', { name: /Add row button/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Rich history button/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Query inspector button/i })).toBeInTheDocument();
   });
 
   it('should not render add row button if addQueryRowButtonHidden=true', () => {
-    const wrapper = shallow(
+    render(
       <SecondaryActions
         addQueryRowButtonHidden={true}
         onClickAddQueryRowButton={noop}
@@ -30,41 +31,46 @@ describe('SecondaryActions', () => {
         onClickQueryInspectorButton={noop}
       />
     );
-    expect(wrapper.find(addQueryRowButtonSelector)).toHaveLength(0);
-    expect(wrapper.find(richHistoryButtonSelector)).toHaveLength(1);
+
+    expect(screen.queryByRole('button', { name: /Add row button/i })).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Rich history button/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Query inspector button/i })).toBeInTheDocument();
   });
 
   it('should disable add row button if addQueryRowButtonDisabled=true', () => {
-    const wrapper = shallow(
+    render(
       <SecondaryActions
-        addQueryRowButtonDisabled={true}
+        addQueryRowButtonHidden={true}
         onClickAddQueryRowButton={noop}
         onClickRichHistoryButton={noop}
         onClickQueryInspectorButton={noop}
       />
     );
-    expect(wrapper.find(addQueryRowButtonSelector).props().disabled).toBe(true);
+    // TODO: Continue here
+    // expect(screen.queryByRole('button', { name: /Add row button/i })).toHaveAttribute('disabled');
+
+    // expect(wrapper.find(addQueryRowButtonSelector).props().disabled).toBe(true);
   });
 
-  it('should map click handlers correctly', () => {
-    const onClickAddRow = jest.fn();
-    const onClickHistory = jest.fn();
-    const onClickQueryInspector = jest.fn();
-    const wrapper = shallow(
-      <SecondaryActions
-        onClickAddQueryRowButton={onClickAddRow}
-        onClickRichHistoryButton={onClickHistory}
-        onClickQueryInspectorButton={onClickQueryInspector}
-      />
-    );
-
-    wrapper.find(addQueryRowButtonSelector).simulate('click');
-    expect(onClickAddRow).toBeCalled();
-
-    wrapper.find(richHistoryButtonSelector).simulate('click');
-    expect(onClickHistory).toBeCalled();
-
-    wrapper.find(queryInspectorButtonSelector).simulate('click');
-    expect(onClickQueryInspector).toBeCalled();
-  });
+  // it('should map click handlers correctly', () => {
+  //   const onClickAddRow = jest.fn();
+  //   const onClickHistory = jest.fn();
+  //   const onClickQueryInspector = jest.fn();
+  //   const wrapper = shallow(
+  //     <SecondaryActions
+  //       onClickAddQueryRowButton={onClickAddRow}
+  //       onClickRichHistoryButton={onClickHistory}
+  //       onClickQueryInspectorButton={onClickQueryInspector}
+  //     />
+  //   );
+  //
+  //   wrapper.find(addQueryRowButtonSelector).simulate('click');
+  //   expect(onClickAddRow).toBeCalled();
+  //
+  //   wrapper.find(richHistoryButtonSelector).simulate('click');
+  //   expect(onClickHistory).toBeCalled();
+  //
+  //   wrapper.find(queryInspectorButtonSelector).simulate('click');
+  //   expect(onClickQueryInspector).toBeCalled();
+  // });
 });
