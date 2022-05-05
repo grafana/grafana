d3f7fad9fff903eb8714e996e75e7de1f0210789
gillesdemey <gilles.de.mey@gmail.com>
2022-05-05 19:32:41 +0200

    adds inline documentation to the Math expression

--- public/app/features/expressions/components/Math.tsx
+++ public/app/features/expressions/components/Math.tsx
@@ -1,7 +1,9 @@
 import { css } from '@emotion/css';
-import React, { ChangeEvent, FC } from 'react';
+import React, { ChangeEvent, FC, useCallback, useState } from 'react';
 
-import { InlineField, TextArea } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data';
+import { Stack } from '@grafana/experimental';
+import { Button, Icon, InlineField, TextArea, useStyles2 } from '@grafana/ui';
 
 import { ExpressionQuery } from '../types';
 
@@ -12,24 +14,143 @@ interface Props {
 }
 
 const mathPlaceholder =
-  'Math operations on one more queries, you reference the query by ${refId} ie. $A, $B, $C etc\n' +
-  'Example: $A + $B\n' +
-  'Available functions: abs(), log(), is_number(), round(), ceil(), floor(), is_inf(), is_nan(), is_null(), ';
+  'Math operations on one more queries, you reference the query by ${refId} ie. $A, $B, $C etc\n' + 'Example: $A + $B';
 
 export const Math: FC<Props> = ({ labelWidth, onChange, query }) => {
+  const [showHelp, setShowHelp] = useState<boolean>(true);
+
+  const toggleShowHelp = useCallback(() => {
+    setShowHelp((show) => !show);
+  }, []);
+
   const onExpressionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     onChange({ ...query, expression: event.target.value });
   };
 
+  const styles = useStyles2((theme) => getStyles(theme, showHelp));
+
   return (
-    <InlineField
-      label="Expression"
-      labelWidth={labelWidth}
-      className={css`
-        align-items: baseline;
-      `}
-    >
-      <TextArea value={query.expression} onChange={onExpressionChange} rows={4} placeholder={mathPlaceholder} />
-    </InlineField>
+    <Stack direction="row">
+      <InlineField
+        label="Expression"
+        labelWidth={labelWidth}
+        grow={true}
+        shrink={true}
+        className={css`
+          align-items: flex-start;
+          flex: 0.7;
+        `}
+      >
+        <>
+          <TextArea value={query.expression} onChange={onExpressionChange} rows={5} placeholder={mathPlaceholder} />
+          <Button color="primary" size="sm" onClick={() => toggleShowHelp()}>
+            {showHelp === false ? 'Show' : 'Hide'} help
+          </Button>
+        </>
+      </InlineField>
+      <div className={styles.documentationContainer}>
+        <header className={styles.documentationHeader}>
+          <Icon name="book-open" /> Math operator
+        </header>
+        <div>
+          Run Math operations on one more queries, you reference the query by {'${refId}'} ie. $A, $B, $C etc.
+          <br />
+          Example: <code>$A + $B</code>
+        </div>
+        <header className={styles.documentationHeader}>Available Math functions</header>
+        <div className={styles.documentationFunctions}>
+          <DocumentedFunction
+            name="abs"
+            description="returns the absolute value of its argument which can be a number or a series"
+          />
+          <DocumentedFunction
+            name="is_inf"
+            description="takes a number or a series and returns 1 for Inf values (negative or positive) and 0 for other values"
+          />
+          <DocumentedFunction
+            name="is_nan"
+            description="takes a number or a series and returns 1 for NaN values and 0 for other values"
+          />
+          <DocumentedFunction
+            name="is_null"
+            description="takes a number or a series and returns 1 for null values and 0 for other values"
+          />
+          <DocumentedFunction
+            name="is_number"
+            description="takes a number or a series and returns 1 for all real number values and 0 for non-number"
+          />
+          <DocumentedFunction
+            name="log"
+            description="returns the natural logarithm of of its argument which can be a number or a series"
+          />
+          <DocumentedFunction
+            name="inf, infn, nan, and null"
+            description="The inf, infn, nan, and null functions all return a single value of the name"
+          />
+          <DocumentedFunction name="round" description="returns a rounded integer value" />
+          <DocumentedFunction name="ceil" description="rounds the number up to the nearest integer value" />
+          <DocumentedFunction name="floor" description="rounds the number down to the nearest integer value" />
+        </div>
+        <div>
+          See our additional documentation on{' '}
+          <a
+            className={styles.documentationLink}
+            target="_blank"
+            href="https://grafana.com/docs/grafana/latest/panels/query-a-data-source/use-expressions-to-manipulate-data/about-expressions/#math"
+            rel="noreferrer"
+          >
+            <Icon size="xs" name="external-link-alt" /> Math expressions
+          </a>
+          .
+        </div>
+      </div>
+    </Stack>
   );
 };
+
+interface DocumentedFunctionProps {
+  name: string;
+  description: React.ReactNode;
+}
+const DocumentedFunction = ({ name, description }: DocumentedFunctionProps) => {
+  const styles = useStyles2(getDocumentedFunctionStyles);
+
+  return (
+    <>
+      <span className={styles.name}>{name}</span>
+      <span className={styles.description}>{description}</span>
+    </>
+  );
+};
+
+const getStyles = (theme: GrafanaTheme2, showHelp?: boolean) => ({
+  documentationHeader: css`
+    font-size: ${theme.typography.h5.fontSize};
+    font-weight: ${theme.typography.h5.fontWeight};
+  `,
+  documentationLink: css`
+    color: ${theme.v1.colors.linkExternal};
+    text-decoration: ${theme.v1.typography.link.decoration};
+  `,
+  documentationContainer: css`
+    display: ${showHelp ? 'flex' : 'none'};
+    flex: 1;
+    flex-direction: column;
+    gap: ${theme.v1.spacing.md};
+  `,
+  documentationFunctions: css`
+    display: grid;
+    grid-template-columns: max-content auto;
+    column-gap: ${theme.v1.spacing.md};
+  `,
+});
+
+const getDocumentedFunctionStyles = (theme: GrafanaTheme2) => ({
+  name: css`
+    font-weight: ${theme.v1.typography.weight.bold};
+  `,
+  description: css`
+    font-size: ${theme.v1.typography.size.sm};
+    color: ${theme.v1.colors.textFaint};
+  `,
+});
