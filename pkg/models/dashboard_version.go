0f357b820b77f39cd75be06b2d2922f7b1823085
Kat Yang <69819079+yangkb09@users.noreply.github.com>
2022-05-06 09:49:24 -0400

    Update pkg/models/dashboard_version.go
    Co-authored-by: Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>

--- pkg/models/dashboard_version.go
+++ pkg/models/dashboard_version.go
@@ -17,7 +17,6 @@ var (
 type DashboardVersion struct {
 	Id            int64  `json:"id"`
 	DashboardId   int64  `json:"dashboardId"`
-	UID           string `json:"uid"`
 	ParentVersion int    `json:"parentVersion"`
 	RestoredFrom  int    `json:"restoredFrom"`
 	Version       int    `json:"version"`
