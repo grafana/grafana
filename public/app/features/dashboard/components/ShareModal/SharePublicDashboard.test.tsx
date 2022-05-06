5f8a7e6b41b4817b032cbc28de15cad713a8ffdd
Jeff Levin <jeff@levinology.com>
2022-05-05 17:53:51 -0800

    add test for dashboardCanBePublic

--- public/app/features/dashboard/components/ShareModal/SharePublicDashboard.test.tsx
+++ public/app/features/dashboard/components/ShareModal/SharePublicDashboard.test.tsx
@@ -2,8 +2,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 
 import config from 'app/core/config';
-
-import { DashboardModel, PanelModel } from '../../state';
+import { DashboardModel, PanelModel } from 'app/features/dashboard/state';
 
 import { ShareModal } from './ShareModal';
 
