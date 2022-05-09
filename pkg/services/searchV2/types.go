d6d358ef267dfe4d0ba6e8b88d9f8597a677443e
Ryan McKinley <ryantxu@gmail.com>
2022-05-09 01:00:09 -0700

    Search: create bluge based index  (#48606)

--- pkg/services/searchV2/types.go
+++ pkg/services/searchV2/types.go
@@ -8,8 +8,27 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 )
 
+type FacetField struct {
+	Field string `json:"field"`
+	Limit int    `json:"limit,omitempty"` // explicit page size
+}
+
 type DashboardQuery struct {
-	Query string
+	Query        string       `json:"query"`
+	Location     string       `json:"location,omitempty"` // parent folder ID
+	Sort         string       `json:"sort,omitempty"`     // field ASC/DESC
+	Datasource   string       `json:"ds_uid,omitempty"`   // "datasource" collides with the JSON value at the same leel :()
+	Tags         []string     `json:"tags,omitempty"`
+	Kind         []string     `json:"kind,omitempty"`
+	UIDs         []string     `json:"uid,omitempty"`
+	IDs          []int64      `json:"id,omitempty"`      // deprecated -- but will convert internal ID to UIDs
+	Explain      bool         `json:"explain,omitempty"` // adds details on why document matched
+	Facet        []FacetField `json:"facet,omitempty"`
+	SkipLocation bool         `json:"skipLocation,omitempty"`
+	AccessInfo   bool         `json:"accessInfo,omitempty"` // adds field for access control
+	HasPreview   string       `json:"hasPreview,omitempty"` // the light|dark theme
+	Limit        int          `json:"limit,omitempty"`      // explicit page size
+	From         int          `json:"from,omitempty"`       // for paging
 }
 
 type SearchService interface {
