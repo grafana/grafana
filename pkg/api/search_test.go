1206719f0e124660b44f0a9e2d2a0e1d04c46b34
Karl Persson <kalle.persson@grafana.com>
2022-05-10 17:57:08 +0200

    Add tests

--- pkg/api/search_test.go
+++ pkg/api/search_test.go
@@ -2,37 +2,65 @@ package api
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
-	"github.com/grafana/grafana/pkg/services/search"
-	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/grafana/grafana/pkg/services/dashboards"
 )
 
 func TestHTTPServer_Search(t *testing.T) {
 	sc := setupHTTPServer(t, true, true)
 	sc.initCtx.IsSignedIn = true
 	sc.initCtx.SignedInUser = &models.SignedInUser{}
-	sc.hs.SearchService = search.ProvideService(sc.cfg, sc.db.(*sqlstore.SQLStore))
+
+	sc.hs.SearchService = &mockSearchService{
+		ExpectedResult: models.HitList{
+			{ID: 1, UID: "folder1", Title: "folder1", Type: models.DashHitFolder},
+			{ID: 2, UID: "folder2", Title: "folder2", Type: models.DashHitFolder},
+		},
+	}
+
 	sc.acmock.GetUserPermissionsFunc = func(ctx context.Context, user *models.SignedInUser, options accesscontrol.Options) ([]*accesscontrol.Permission, error) {
-		return []*accesscontrol.Permission{
-			{Action: "folders:read", Scope: "folders:*"},
-			{Action: "dashboards:read", Scope: "dashboards:*"},
-		}, nil
+		return []*accesscontrol.Permission{{Action: "folders:read", Scope: "folders:*"}, {Action: "folders:write", Scope: "folders:uid:folder2"}}, nil
+	}
+
+	type withMeta struct {
+		models.Hit
+		AccessControl accesscontrol.Metadata `json:"accessControl,omitempty"`
 	}
 
 	t.Run("should attach access control metadata to response", func(t *testing.T) {
 		recorder := callAPI(sc.server, http.MethodGet, "/api/search?accesscontrol=true", nil, t)
 		assert.Equal(t, http.StatusOK, recorder.Code)
-		fmt.Println(recorder.Body.String())
+		var result []withMeta
+		require.NoError(t, json.Unmarshal(recorder.Body.Bytes(), &result))
+
+		for _, r := range result {
+			if r.ID == 1 {
+				assert.Len(t, r.AccessControl, 1)
+				assert.True(t, r.AccessControl[dashboards.ActionFoldersRead])
+			} else {
+				assert.Len(t, r.AccessControl, 2)
+				assert.True(t, r.AccessControl[dashboards.ActionFoldersRead])
+				assert.True(t, r.AccessControl[dashboards.ActionFoldersWrite])
+			}
+		}
 	})
 
 	t.Run("should not attach access control metadata to response", func(t *testing.T) {
+		recorder := callAPI(sc.server, http.MethodGet, "/api/search", nil, t)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		var result []withMeta
+		require.NoError(t, json.Unmarshal(recorder.Body.Bytes(), &result))
 
+		for _, r := range result {
+			assert.Len(t, r.AccessControl, 0)
+		}
 	})
 }
