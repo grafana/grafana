d49013abcf4f20a1fc539d54439110013a6df2d7
Alexander Zobnin <alexanderzobnin@gmail.com>
2022-05-13 13:49:22 +0300

    ServiceAccounts: refactor ServiceAccountRoleRow

--- public/app/features/serviceaccounts/ServiceAccountRoleRow.tsx
+++ public/app/features/serviceaccounts/ServiceAccountRoleRow.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from '@emotion/css';
-import React, { PureComponent } from 'react';
+import React, { FC } from 'react';
 
 import { UserRolePicker } from 'app/core/components/RolePicker/UserRolePicker';
 import { contextSrv } from 'app/core/core';
@@ -15,46 +15,48 @@ interface Props {
   builtInRoles: Record<string, Role[]>;
 }
 
-export class ServiceAccountRoleRow extends PureComponent<Props> {
-  render() {
-    const { label, serviceAccount, roleOptions, builtInRoles, onRoleChange } = this.props;
-    const canUpdateRole = contextSrv.hasPermissionInMetadata(AccessControlAction.ServiceAccountsWrite, serviceAccount);
-    const rolePickerDisabled = !canUpdateRole;
-    const labelClass = cx(
-      'width-16',
-      css`
-        font-weight: 500;
-      `
-    );
+export const ServiceAccountRoleRow: FC<Props> = ({
+  label,
+  serviceAccount,
+  roleOptions,
+  builtInRoles,
+  onRoleChange,
+}) => {
+  const canUpdateRole = contextSrv.hasPermissionInMetadata(AccessControlAction.ServiceAccountsWrite, serviceAccount);
+  const rolePickerDisabled = !canUpdateRole;
+  const labelClass = cx(
+    'width-16',
+    css`
+      font-weight: 500;
+    `
+  );
+  const inputId = `${label}-input`;
 
-    const inputId = `${label}-input`;
-    return (
-      <tr>
-        <td className={labelClass}>
-          <label htmlFor={inputId}>{label}</label>
-        </td>
-        <td className="width-25" colSpan={2}>
-          {contextSrv.licensedAccessControlEnabled() ? (
-            <UserRolePicker
-              userId={serviceAccount.id}
-              orgId={serviceAccount.orgId}
-              builtInRole={serviceAccount.role}
-              onBuiltinRoleChange={onRoleChange}
-              roleOptions={roleOptions}
-              builtInRoles={builtInRoles}
-              disabled={rolePickerDisabled}
-            />
-          ) : (
-            <OrgRolePicker
-              aria-label="Role"
-              value={serviceAccount.role}
-              disabled={!canUpdateRole}
-              onChange={onRoleChange}
-            />
-          )}
-        </td>
-        <td></td>
-      </tr>
-    );
-  }
-}
+  return (
+    <tr>
+      <td className={labelClass}>
+        <label htmlFor={inputId}>{label}</label>
+      </td>
+      <td className="width-25" colSpan={2}>
+        {contextSrv.licensedAccessControlEnabled() ? (
+          <UserRolePicker
+            userId={serviceAccount.id}
+            orgId={serviceAccount.orgId}
+            builtInRole={serviceAccount.role}
+            onBuiltinRoleChange={onRoleChange}
+            roleOptions={roleOptions}
+            builtInRoles={builtInRoles}
+            disabled={rolePickerDisabled}
+          />
+        ) : (
+          <OrgRolePicker
+            aria-label="Role"
+            value={serviceAccount.role}
+            disabled={!canUpdateRole}
+            onChange={onRoleChange}
+          />
+        )}
+      </td>
+    </tr>
+  );
+};
