5dd778d70e609ac5cd4f2ff0130d1853a9c692f4
Jeff Levin <jeff@levinology.com>
2022-05-16 12:06:47 -0800

    remove unused includes

--- pkg/api/metrics_test.go
+++ pkg/api/metrics_test.go
@@ -2,37 +2,25 @@ package api
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 	"testing"
 
-	acmock "github.com/grafana/grafana/pkg/services/accesscontrol/mock"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
-	"github.com/grafana/grafana/pkg/services/sqlstore/mockstore"
 	"github.com/grafana/grafana/pkg/web/webtest"
 
 	"golang.org/x/oauth2"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/models"
-	"github.com/grafana/grafana/pkg/plugins"
 	fakeDatasources "github.com/grafana/grafana/pkg/services/datasources/fakes"
-	datasources "github.com/grafana/grafana/pkg/services/datasources/service"
 	"github.com/grafana/grafana/pkg/services/query"
-	"github.com/grafana/grafana/pkg/services/secrets/fakes"
-	"github.com/grafana/grafana/pkg/services/secrets/kvstore"
-	secretsManager "github.com/grafana/grafana/pkg/services/secrets/manager"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	queryDatasourceInput = `{
-		"from": "",
+var queryDatasourceInput = `{
+"from": "",
 		"to": "",
 		"queries": [
 			{
@@ -45,123 +33,6 @@ var (
 			}
 		]
 	}`
-
-	getDashboardByIdOutput = `{
-		"annotations": {
-			"list": [
-			{
-				"builtIn": 1,
-				"datasource": "-- Grafana --",
-				"enable": true,
-				"hide": true,
-				"iconColor": "rgba(0, 211, 255, 1)",
-				"name": "Annotations & Alerts",
-				"target": {
-					"limit": 100,
-					"matchAny": false,
-					"tags": [],
-					"type": "dashboard"
-				},
-				"type": "dashboard"
-			}
-			]
-		},
-		"editable": true,
-		"fiscalYearStartMonth": 0,
-		"graphTooltip": 0,
-		"links": [],
-		"liveNow": false,
-		"panels": [
-		{
-			"fieldConfig": {
-				"defaults": {
-					"color": {
-						"mode": "palette-classic"
-					},
-					"custom": {
-						"axisLabel": "",
-						"axisPlacement": "auto",
-						"barAlignment": 0,
-						"drawStyle": "line",
-						"fillOpacity": 0,
-						"gradientMode": "none",
-						"hideFrom": {
-							"legend": false,
-							"tooltip": false,
-							"viz": false
-						},
-						"lineInterpolation": "linear",
-						"lineWidth": 1,
-						"pointSize": 5,
-						"scaleDistribution": {
-							"type": "linear"
-						},
-						"showPoints": "auto",
-						"spanNulls": false,
-						"stacking": {
-							"group": "A",
-							"mode": "none"
-						},
-						"thresholdsStyle": {
-							"mode": "off"
-						}
-					},
-					"mappings": [],
-					"thresholds": {
-						"mode": "absolute",
-						"steps": [
-						{
-							"color": "green",
-							"value": null
-						},
-						{
-							"color": "red",
-							"value": 80
-						}
-						]
-					}
-				},
-				"overrides": []
-			},
-			"gridPos": {
-				"h": 9,
-				"w": 12,
-				"x": 0,
-				"y": 0
-			},
-			"id": 2,
-			"options": {
-				"legend": {
-					"calcs": [],
-					"displayMode": "list",
-					"placement": "bottom"
-				},
-				"tooltip": {
-					"mode": "single",
-					"sort": "none"
-				}
-			},
-			"title": "Panel Title",
-			"type": "timeseries"
-		}
-		],
-		"schemaVersion": 35,
-		"style": "dark",
-		"tags": [],
-		"templating": {
-			"list": []
-		},
-		"time": {
-			"from": "now-6h",
-			"to": "now"
-		},
-		"timepicker": {},
-		"timezone": "",
-		"title": "New dashboard",
-		"version": 0,
-		"weekStart": ""
-	}`
-)
 
 type fakePluginRequestValidator struct {
 	err error
@@ -183,19 +54,6 @@ func (ts *fakeOAuthTokenService) GetCurrentOAuthToken(context.Context, *models.S
 func (ts *fakeOAuthTokenService) IsOAuthPassThruEnabled(*models.DataSource) bool {
 	return ts.passThruEnabled
 }
-
-func (c *dashboardFakePluginClient) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	c.req = req
-	resp := backend.Responses{}
-	return &backend.QueryDataResponse{Responses: resp}, nil
-}
-
-type dashboardFakePluginClient struct {
-	plugins.Client
-
-	req *backend.QueryDataRequest
-}
-
 
 // `/ds/query` endpoint test
 func TestAPIEndpoint_Metrics_QueryMetricsV2(t *testing.T) {
