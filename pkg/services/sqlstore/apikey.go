e033750489c3ebfb969d12d80c2409a6beac592e
jguer <joao.guerreiro@grafana.com>
2022-05-16 16:18:23 +0200

    add get by hash sqlstore test

--- pkg/services/sqlstore/apikey.go
+++ pkg/services/sqlstore/apikey.go
@@ -146,9 +146,9 @@ func (ss *SQLStore) GetApiKeyByName(ctx context.Context, query *models.GetApiKey
 }
 
 func (ss *SQLStore) GetAPIKeyByHash(ctx context.Context, hash string) (*models.ApiKey, error) {
-	var apikey *models.ApiKey
+	var apikey models.ApiKey
 	err := ss.WithDbSession(ctx, func(sess *DBSession) error {
-		has, err := sess.Where("key=?", hash).Get(apikey)
+		has, err := sess.Where("key=?", hash).Get(&apikey)
 		if err != nil {
 			return err
 		} else if !has {
@@ -158,5 +158,5 @@ func (ss *SQLStore) GetAPIKeyByHash(ctx context.Context, hash string) (*models.A
 		return nil
 	})
 
-	return apikey, err
+	return &apikey, err
 }
