9c0d120da64568a34533b11264498aaa2395c79e
Sofia Papagiannaki <sofia@grafana.com>
2022-05-17 13:25:12 +0300

    Revert "Fix ngalert spec"
    This reverts commit 0c6a68693e5e364ecf2f1a3ea055764cf8a4a194.

--- pkg/services/ngalert/api/tooling/definitions/provisioning_templates.go
+++ pkg/services/ngalert/api/tooling/definitions/provisioning_templates.go
@@ -5,7 +5,7 @@ package definitions
 // Get all message templates.
 //
 //     Responses:
-//       200: MessageTemplateResponse
+//       200: []MessageTemplate
 //       400: ValidationError
 
 // swagger:route GET /api/provisioning/templates/{ID} provisioning RouteGetTemplate
@@ -13,26 +13,12 @@ package definitions
 // Get a message template.
 //
 //     Responses:
-//       200: MessageTemplateResponse
-//       404: NotFoundResponse
-
-// swagger:parameters RouteGetTemplate
-type RouteGetTemplateParam struct {
-	// Template ID
-	// in:path
-	ID string
-}
+//       200: MessageTemplate
+//       404: NotFound
 
 type MessageTemplate struct {
 	Name     string
 	Template string
 }
 
-//swagger:response MessageTemplateResponse
-type MessageTemplateResponse struct {
-	// in:body
-	Body []MessageTemplate `json:"body"`
-}
-
-//swagger:response NotFoundResponse
-type NotFoundResponse struct{}
+type NotFound struct{}
