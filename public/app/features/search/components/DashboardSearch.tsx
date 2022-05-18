ff608d78c74a7835c5b95bd65111ad5c79036b39
Ryan McKinley <ryantxu@gmail.com>
2022-05-17 20:28:27 -0700

    add if false

--- public/app/features/search/components/DashboardSearch.tsx
+++ public/app/features/search/components/DashboardSearch.tsx
@@ -20,7 +20,7 @@ export interface Props {
 }
 
 export default function DashboardSearch({ onCloseSearch }: Props) {
-  if (config.featureToggles.panelTitleSearch) {
+  if (false && config.featureToggles.panelTitleSearch) {
     // TODO: "folder:current" ????
     return <DashbaordSearchNEW onCloseSearch={onCloseSearch} />;
   }
