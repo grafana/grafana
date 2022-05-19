3122a8b521a0dff483382e5f6a18b806d045b6c7
Jean-Philippe Quemener <jeanphilippe.quemener@gmail.com>
2022-05-19 12:54:51 +0200

    make changes 8.4 compatible

--- pkg/tsdb/legacydata/service/service.go
+++ pkg/tsdb/legacydata/service/service.go
@@ -41,10 +41,7 @@ func ProvideService(pluginsClient plugins.Client, oAuthTokenService oauthtoken.O
 
 //nolint: staticcheck // legacydata.DataResponse deprecated
 func (h *Service) HandleRequest(ctx context.Context, ds *models.DataSource, query legacydata.DataQuery) (legacydata.DataResponse, error) {
-	decryptedJsonData, err := h.dataSourcesService.DecryptedValues(ctx, ds)
-	if err != nil {
-		return legacydata.DataResponse{}, err
-	}
+	decryptedJsonData := h.dataSourcesService.DecryptedValues(ds)
 
 	req, err := generateRequest(ctx, ds, decryptedJsonData, query)
 	if err != nil {
