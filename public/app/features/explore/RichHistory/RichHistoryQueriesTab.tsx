9707ccef115f8599e3adde1a3adc9261d43dcd5d
Piotr Jamróz <pm.jamroz@gmail.com>
2022-05-19 13:37:28 +0200

    Fix linting errors

--- public/app/features/explore/RichHistory/RichHistoryQueriesTab.tsx
+++ public/app/features/explore/RichHistory/RichHistoryQueriesTab.tsx
@@ -2,8 +2,8 @@ import { css } from '@emotion/css';
 import React, { useEffect } from 'react';
 
 import { GrafanaTheme, SelectableValue } from '@grafana/data';
-import { Button, FilterInput, MultiSelect, RangeSlider, Select, stylesFactory, useTheme } from '@grafana/ui';
 import { config } from '@grafana/runtime';
+import { Button, FilterInput, MultiSelect, RangeSlider, Select, stylesFactory, useTheme } from '@grafana/ui';
 import {
   createDatasourcesList,
   mapNumbertoTimeInSlider,
