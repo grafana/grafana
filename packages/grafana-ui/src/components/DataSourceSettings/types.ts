bd320ee0b36b674e2cd75c18899d46df66d99479
Erik Sundell <erik.sundell87@gmail.com>
2022-05-20 16:04:20 +0200

    Auth: Remove grafana ui dependency to the aws sdk (#43559)
    * remove grafana ui dependency to the aws sdk
    
    * wip
    
    * cleanup
    
    * add tests
    
    * point to real version of aws-sdk

--- packages/grafana-ui/src/components/DataSourceSettings/types.ts
+++ packages/grafana-ui/src/components/DataSourceSettings/types.ts
@@ -37,4 +37,6 @@ export interface HttpSettingsProps extends HttpSettingsBaseProps {
   sigV4AuthToggleEnabled?: boolean;
   /** Azure authentication settings **/
   azureAuthSettings?: AzureAuthSettings;
+  /** If SIGV4 is enabled, provide an editor for SIGV4 connection config  **/
+  renderSigV4Editor?: React.ReactNode;
 }
