193f3f3c7deb0adbdb4b837f70ce31ceca5a63a7
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2022-05-20 18:02:18 -0400

    add test to for stale results

--- pkg/services/ngalert/state/manager_test.go
+++ pkg/services/ngalert/state/manager_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"sort"
 	"testing"
 	"time"
 
+	"github.com/benbjohnson/clock"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
@@ -2023,4 +2025,105 @@ func TestStaleResultsHandler(t *testing.T) {
 		// The expected number of state entries remains after results are processed
 		assert.Equal(t, tc.finalStateCount, len(existingStatesForRule))
 	}
+}
+
+func TestStaleResults(t *testing.T) {
+	getCacheID := func(t *testing.T, rule *models.AlertRule, result eval.Result) string {
+		t.Helper()
+		labels := data.Labels{}
+		labels[models.NamespaceUIDLabel] = rule.NamespaceUID
+		labels[models.RuleUIDLabel] = rule.UID
+		labels["alertname"] = rule.Title
+		for key, value := range rule.Labels {
+			labels[key] = value
+		}
+		for key, value := range result.Instance {
+			labels[key] = value
+		}
+		lbls := models.InstanceLabels(labels)
+		key, err := lbls.StringKey()
+		require.NoError(t, err)
+		return key
+	}
+
+	checkExpectedStates := func(t *testing.T, actual []*state.State, expected map[string]struct{}) {
+		t.Helper()
+		require.Len(t, actual, len(expected))
+		for _, currentState := range actual {
+			_, ok := expected[currentState.CacheId]
+			require.Truef(t, ok, "State %s is not expected. States: %v", currentState.CacheId, expected)
+		}
+	}
+
+	t.Run("should mark missing states as stale", func(t *testing.T) {
+		// init
+		ctx := context.Background()
+		_, dbstore := tests.SetupTestEnv(t, 1)
+		clk := clock.NewMock()
+		clk.Set(time.Now())
+
+		st := state.NewManager(log.New("test_stale_results_handler"), testMetrics.GetStateMetrics(), nil, dbstore, dbstore, &dashboards.FakeDashboardService{}, &image.NoopImageService{})
+
+		orgID := rand.Int63()
+		rule := tests.CreateTestAlertRule(t, ctx, dbstore, 10, orgID)
+
+		initResults := eval.Results{
+			eval.Result{
+				Instance:    data.Labels{"test1": "testValue1"},
+				State:       eval.Alerting,
+				EvaluatedAt: clk.Now(),
+			},
+			eval.Result{
+				Instance:    data.Labels{"test1": "testValue2"},
+				State:       eval.Alerting,
+				EvaluatedAt: clk.Now(),
+			},
+			eval.Result{
+				Instance:    data.Labels{"test1": "testValue3"},
+				State:       eval.Normal,
+				EvaluatedAt: clk.Now(),
+			},
+		}
+
+		initStates := map[string]struct{}{
+			getCacheID(t, rule, initResults[0]): {},
+			getCacheID(t, rule, initResults[1]): {},
+			getCacheID(t, rule, initResults[2]): {},
+		}
+
+		// Init
+		processed := st.ProcessEvalResults(ctx, clk.Now(), rule, initResults)
+		checkExpectedStates(t, processed, initStates)
+		currentStates := st.GetStatesForRuleUID(orgID, rule.UID)
+		checkExpectedStates(t, currentStates, initStates)
+
+		staleDuration := 2 * time.Duration(rule.IntervalSeconds) * time.Second
+		clk.Add(staleDuration)
+		results := eval.Results{
+			eval.Result{
+				Instance:    data.Labels{"test1": "testValue1"},
+				State:       eval.Alerting,
+				EvaluatedAt: clk.Now(),
+			},
+		}
+		clk.Add(time.Nanosecond) // we use time now when calculate stale states. Evaluation tick and real time are not the same. usually, difference is way greater than nanosecond.
+		expectedStaleReturned := getCacheID(t, rule, initResults[1])
+		processed = st.ProcessEvalResults(ctx, clk.Now(), rule, results)
+		checkExpectedStates(t, processed, map[string]struct{}{
+			getCacheID(t, rule, results[0]): {},
+			expectedStaleReturned:           {},
+		})
+		for _, s := range processed {
+			if s.CacheId == expectedStaleReturned {
+				assert.Truef(t, s.Resolved, "Returned stale state should have Resolved set to true")
+				assert.Equal(t, eval.Normal, s.State)
+				assert.Equal(t, models.StateReasonMissingSeries, s.StateReason)
+				break
+			}
+		}
+		currentStates = st.GetStatesForRuleUID(orgID, rule.UID)
+		checkExpectedStates(t, currentStates, map[string]struct{}{
+			getCacheID(t, rule, results[0]): {},
+		})
+	})
 }
