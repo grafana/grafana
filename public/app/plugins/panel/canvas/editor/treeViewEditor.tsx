3cd2f9cf536406b79d8729c6385feb46c29cb5ff
Adela Almasan <adela.almasan@grafana.com>
2022-05-20 16:04:49 -0500

    tree view navigation

---
+++ public/app/plugins/panel/canvas/editor/treeViewEditor.tsx
@@ -0,0 +1,48 @@
+import { NestedPanelOptions } from '@grafana/data/src/utils/OptionsUIBuilders';
+import { ElementState } from 'app/features/canvas/runtime/element';
+import { FrameState } from 'app/features/canvas/runtime/frame';
+import { Scene } from 'app/features/canvas/runtime/scene';
+
+import { InstanceState } from '../CanvasPanel';
+
+import { TreeNavigationEditor } from './TreeNavigationEditor';
+
+export interface TreeViewEditorProps {
+  scene: Scene;
+  layer: FrameState;
+  selected: ElementState[];
+}
+
+export function getTreeViewEditor(opts: InstanceState): NestedPanelOptions<TreeViewEditorProps> {
+  const { selected, scene } = opts;
+
+  if (selected) {
+    console.log('yes select');
+    for (const element of selected) {
+      if (element instanceof FrameState) {
+        scene.currentLayer = element;
+        break;
+      }
+
+      if (element.parent) {
+        scene.currentLayer = element.parent;
+        break;
+      }
+    }
+  }
+
+  return {
+    category: ['Tree Navigation'],
+    path: '--', // not used!
+    build: (builder, context) => {
+      builder.addCustomEditor({
+        category: [],
+        id: 'navigation',
+        path: '__', // not used
+        name: '',
+        editor: TreeNavigationEditor,
+        settings: { scene, layer: scene.currentLayer, selected },
+      });
+    },
+  };
+}
