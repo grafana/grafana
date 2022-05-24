acbab24454bfbd446b05ac165a48271b22f81be7
Owen Smallwood <owen.smallwood@grafana.com>
2022-05-24 12:18:19 -0600

    Determines path using location locationUtils. This covers the case when grafana is being hosted on a subpath. Updates tests.

--- public/app/features/dashboard/utils/publicDashboards.test.ts
+++ public/app/features/dashboard/utils/publicDashboards.test.ts
@@ -1,17 +1,61 @@
-import { describe } from '../../../../test/lib/common';
+import { locationUtil } from '@grafana/data/src';
+
+import { beforeEach, describe } from '../../../../test/lib/common';
 import { updateConfig } from '../../../core/config';
 
 import { isPublicDashboardView } from './publicDashboards';
 
+beforeEach(() => {
+  locationUtil.initialize({
+    config: { appSubUrl: '' } as any,
+    getVariablesUrlParams: (() => {}) as any,
+    getTimeRangeForUrl: (() => {}) as any,
+  });
+});
+
 describe('public dashboard utils', () => {
-  updateConfig({ featureToggles: { publicDashboards: true } });
+  updateConfig({
+    featureToggles: { publicDashboards: true },
+  });
 
   it('returns true for public url', () => {
-    window.history.pushState({}, 'Test Title', '/p/abc/123');
+    // @ts-ignore
+    delete window.location;
+    window.location = {
+      ...window.location,
+      origin: 'https://example.com',
+      pathname: 'https://example.com/p/abc',
+    };
+
     expect(isPublicDashboardView()).toBeTruthy();
   });
+
+  it('returns true for public url when grafana is served on a subpath', () => {
+    locationUtil.initialize({
+      config: { appSubUrl: '/subpath' } as any,
+      getVariablesUrlParams: (() => {}) as any,
+      getTimeRangeForUrl: (() => {}) as any,
+    });
+    // @ts-ignore
+    delete window.location;
+    window.location = {
+      ...window.location,
+      origin: 'https://example.com',
+      pathname: 'https://example.com/subpath/p/abc',
+    };
+
+    expect(isPublicDashboardView()).toBeTruthy();
+  });
+
   it('returns false for dashboard url', () => {
-    window.history.pushState({}, 'Test Title', '/d/abc/123');
+    // @ts-ignore
+    delete window.location;
+    window.location = {
+      ...window.location,
+      origin: 'https://example.com',
+      pathname: 'https://example.com/d/abc/123',
+    };
+
     expect(isPublicDashboardView()).toBeFalsy();
   });
 });
--- public/app/features/dashboard/utils/publicDashboards.ts
+++ public/app/features/dashboard/utils/publicDashboards.ts
@@ -1,5 +1,10 @@
+import { locationUtil } from '@grafana/data/src';
+
 import { getConfig } from '../../../core/config';
 
 export const isPublicDashboardView = () => {
-  return getConfig().featureToggles.publicDashboards && window.location.pathname.split('/')[1] === 'p';
+  return (
+    getConfig().featureToggles.publicDashboards &&
+    locationUtil.stripBaseFromUrl(window.location.pathname).split('/')[1] === 'p'
+  );
 };
