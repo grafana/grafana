cb4ee91359d45b0a18820e12c9c2889691de75cf
Michael Mandrus <michael.mandrus@grafana.com>
2022-05-25 14:41:39 -0400

    log which secrets manager is being used

--- pkg/services/secrets/kvstore/kvstore.go
+++ pkg/services/secrets/kvstore/kvstore.go
@@ -16,12 +16,14 @@ const (
 func ProvideService(sqlStore sqlstore.Store, secretsService secrets.Service, remoteCheck UseRemoteSecretsPluginCheck) SecretsKVStore {
 	logger := log.New("secrets.kvstore")
 	if remoteCheck.ShouldUseRemoteSecretsPlugin() {
+		logger.Info("secrets kvstore is using a remote plugin for secrets management")
 		return &secretsKVStorePlugin{
 			secretsPlugin:  remoteCheck.GetManager().SecretsManager().SecretsManager,
 			secretsService: secretsService,
 			log:            logger,
 		}
 	}
+	logger.Info("secrets kvstore is using the default (SQL) implementation for secrets management")
 	return &secretsKVStoreSQL{
 		sqlStore:       sqlStore,
 		secretsService: secretsService,
