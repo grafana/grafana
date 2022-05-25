f2407faf540704d647fbd3829956a1d2e393f3e0
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-05-25 12:41:05 -0700

    add tests

---
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/datasource.test.ts
@@ -0,0 +1,35 @@
+import { createMockInstanceSetttings } from './__mocks__/instanceSettings';
+import createMockQuery from './__mocks__/query';
+import Datasource from './datasource';
+
+describe('Azure Monitor Datasource', () => {
+  describe('interpolateVariablesInQueries()', () => {
+    it('should interpolate variables in the queries', () => {
+      const ds = new Datasource(createMockInstanceSetttings());
+      const queries = [createMockQuery({ azureMonitor: { resourceGroup: '$resourceGroup' } })];
+
+      const interpolatedQueries = ds.interpolateVariablesInQueries(queries, {
+        resourceGroup: { text: 'the-resource-group', value: 'the-resource-group' },
+      });
+
+      expect(interpolatedQueries).toContainEqual(
+        expect.objectContaining({
+          azureMonitor: expect.objectContaining({ resourceGroup: 'the-resource-group' }),
+        })
+      );
+    });
+
+    it('should include a datasource ref when interpolating queries', () => {
+      const ds = new Datasource(createMockInstanceSetttings());
+      const queries = [createMockQuery()];
+
+      const interpolatedQueries = ds.interpolateVariablesInQueries(queries, {});
+
+      expect(interpolatedQueries).toContainEqual(
+        expect.objectContaining({
+          datasource: expect.objectContaining({ type: 'azuremonitor', uid: 'abc' }),
+        })
+      );
+    });
+  });
+});
