2bfa479018ea78d50fb9400e3ddd2f917af8a374
Laura <laura.benz@grafana.com>
2022-05-25 17:30:05 +0200

    feat: create a component and export it

---
+++ packages/grafana-ui/src/components/PanelContainer/PanelContainer.tsx
@@ -0,0 +1,23 @@
+import { css, cx } from '@emotion/css';
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+import { GrafanaTheme2 } from '@grafana/data';
+
+import { useStyles2 } from '../../themes';
+
+type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+const getStyles = (theme: GrafanaTheme2) =>
+  css`
+    background-color: ${theme.components.panel.background};
+    border: ${theme.components.panel.borderColor};
+    border-radius: 3px;
+  `;
+export const PanelContainer = ({ children, className, ...props }: Props) => {
+  const css = useStyles2(getStyles);
+  return (
+    <div className={cx([css, className])} {...props}>
+      {children}
+    </div>
+  );
+};
