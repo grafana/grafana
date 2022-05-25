e3a51a4aa82357a39f81a242a05e9aa7d8b154e9
Peter Holmberg <peterholmberg@users.noreply.github.com>
2022-05-25 06:30:37 -0700

    Alerting: Add legacy indicator to navbar (#49511)
    * update id and add translation key
    
    * remove logs

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -526,7 +526,7 @@ func (hs *HTTPServer) buildLegacyAlertNavLinks() []*dtos.NavLink {
 		{
 			Text:       "Alerting",
 			SubTitle:   "Alert rules and notifications",
-			Id:         "alerting",
+			Id:         "alerting-legacy",
 			Icon:       "bell",
 			Url:        hs.Cfg.AppSubURL + "/alerting/list",
 			Children:   alertChildNavs,
