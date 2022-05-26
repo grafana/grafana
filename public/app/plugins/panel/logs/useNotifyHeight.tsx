ffc7aa1ce447f275eb6d69b5b623dda5214d0901
Martin Fuster <mfuster@litebox.ai>
2022-05-26 17:04:17 -0300

    stop self notifying height

--- public/app/plugins/panel/logs/useNotifyHeight.tsx
+++ public/app/plugins/panel/logs/useNotifyHeight.tsx
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 const useNotifyHeight = () => {
   useEffect(() => {
     const interval = setInterval(() => {
-      const scrollbar = document.querySelector('.scrollbar-view');
-      const panelTitle = document.querySelector('.panel-title');
-      if (scrollbar && panelTitle) {
-        const height = `${scrollbar?.scrollHeight + panelTitle?.clientHeight}px`;
-        window.parent.postMessage(height, '*');
+      if (window.top !== window.self) {
+        const scrollbar = document.querySelector('.scrollbar-view');
+        const panelTitle = document.querySelector('.panel-title');
+        if (scrollbar && panelTitle) {
+          const height = `${scrollbar?.scrollHeight + panelTitle?.clientHeight}px`;
+          window.parent.postMessage(height, '*');
+        }
       }
     }, 0);
 
