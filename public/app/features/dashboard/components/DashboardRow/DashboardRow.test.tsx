ec41b2d0aaf1804fe7d8fb7fd3ec85cb481c473d
Joao Silva <joao.silva@grafana.com>
2022-05-26 17:08:39 +0100

    Chore: Converts DashboardRow tests to RTL

--- public/app/features/dashboard/components/DashboardRow/DashboardRow.test.tsx
+++ public/app/features/dashboard/components/DashboardRow/DashboardRow.test.tsx
@@ -1,4 +1,4 @@
-import { mount } from 'enzyme';
+import { screen, render, fireEvent, cleanup } from '@testing-library/react';
 import React from 'react';
 
 import { PanelModel } from '../../state/PanelModel';
@@ -6,7 +6,7 @@ import { PanelModel } from '../../state/PanelModel';
 import { DashboardRow } from './DashboardRow';
 
 describe('DashboardRow', () => {
-  let wrapper: any, panel: PanelModel, dashboardMock: any;
+  let panel: PanelModel, dashboardMock: any;
 
   beforeEach(() => {
     dashboardMock = {
@@ -19,18 +19,21 @@ describe('DashboardRow', () => {
     };
 
     panel = new PanelModel({ collapsed: false });
-    wrapper = mount(<DashboardRow panel={panel} dashboard={dashboardMock} />);
+    render(<DashboardRow panel={panel} dashboard={dashboardMock} />);
   });
 
   it('Should not have collapsed class when collaped is false', () => {
-    expect(wrapper.find('.dashboard-row')).toHaveLength(1);
-    expect(wrapper.find('.dashboard-row--collapsed')).toHaveLength(0);
+    const row = screen.getByTestId('dashboard-row-container');
+    expect(row).toBeInTheDocument();
+    expect(row).not.toHaveClass('dashboard-row--collapsed');
   });
 
   it('Should collapse after clicking title', () => {
-    wrapper.find('.dashboard-row__title').simulate('click');
+    fireEvent.click(screen.getByTestId('data-testid dashboard-row-title-'));
+    //screen.find('.dashboard-row__title').simulate('click');
 
-    expect(wrapper.find('.dashboard-row--collapsed')).toHaveLength(1);
+    const row = screen.getByTestId('dashboard-row-container');
+    expect(row).toHaveClass('dashboard-row--collapsed');
     expect(dashboardMock.toggleRow.mock.calls).toHaveLength(1);
   });
 
@@ -39,19 +42,21 @@ describe('DashboardRow', () => {
   });
 
   it('should have two actions as admin', () => {
-    expect(wrapper.find('.dashboard-row__actions .pointer')).toHaveLength(2);
+    expect(screen.getByTestId('dashboard-row-actions').children).toHaveLength(2);
   });
 
   it('should not show row drag handle when cannot edit', () => {
+    cleanup();
     dashboardMock.meta.canEdit = false;
-    wrapper = mount(<DashboardRow panel={panel} dashboard={dashboardMock} />);
-    expect(wrapper.find('.dashboard-row__drag')).toHaveLength(0);
+    render(<DashboardRow panel={panel} dashboard={dashboardMock} />);
+    expect(screen.queryByTestId('dashboard-row-drag')).not.toBeInTheDocument();
   });
 
   it('should have zero actions when cannot edit', () => {
+    cleanup();
     dashboardMock.meta.canEdit = false;
     panel = new PanelModel({ collapsed: false });
-    wrapper = mount(<DashboardRow panel={panel} dashboard={dashboardMock} />);
-    expect(wrapper.find('.dashboard-row__actions .pointer')).toHaveLength(0);
+    render(<DashboardRow panel={panel} dashboard={dashboardMock} />);
+    expect(screen.queryByTestId('dashboard-row-actions')).not.toBeInTheDocument();
   });
 });
--- public/app/features/dashboard/components/DashboardRow/DashboardRow.tsx
+++ public/app/features/dashboard/components/DashboardRow/DashboardRow.tsx
@@ -86,7 +86,7 @@ export class DashboardRow extends React.Component<DashboardRowProps, any> {
     const canEdit = this.props.dashboard.meta.canEdit === true;
 
     return (
-      <div className={classes}>
+      <div className={classes} data-testid="dashboard-row-container">
         <a
           className="dashboard-row__title pointer"
           data-testid={selectors.components.DashboardRow.title(title)}
@@ -99,7 +99,7 @@ export class DashboardRow extends React.Component<DashboardRowProps, any> {
           </span>
         </a>
         {canEdit && (
-          <div className="dashboard-row__actions">
+          <div className="dashboard-row__actions" data-testid="dashboard-row-actions">
             <RowOptionsButton
               title={this.props.panel.title}
               repeat={this.props.panel.repeat}
@@ -115,7 +115,7 @@ export class DashboardRow extends React.Component<DashboardRowProps, any> {
             &nbsp;
           </div>
         )}
-        {canEdit && <div className="dashboard-row__drag grid-drag-handle" />}
+        {canEdit && <div data-testid="dashboard-row-drag" className="dashboard-row__drag grid-drag-handle" />}
       </div>
     );
   }
