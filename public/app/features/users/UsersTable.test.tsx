73900407a2e085f14a0ba59acd4e987e0ebf9694
polinaboneva <polina.boneva@grafana.com>
2022-05-30 19:00:53 +0300

    Fixes on UsersTable test; no additions

--- public/app/features/users/UsersTable.test.tsx
+++ public/app/features/users/UsersTable.test.tsx
@@ -15,45 +15,41 @@ jest.mock('app/core/core', () => ({
   },
 }));
 
-function getProps(propOverrides?: object) {
+const setup = (propOverrides?: object) => {
   const props: Props = {
     users: [] as OrgUser[],
     onRoleChange: jest.fn(),
     onRemoveUser: jest.fn(),
   };
 
-  return Object.assign(props, propOverrides);
-}
+  Object.assign(props, propOverrides);
+
+  render(<UsersTable {...props} />);
+};
 
 describe('Render', () => {
   it('should render component', () => {
-    render(<UsersTable {...getProps()} />);
+    expect(() => setup()).not.toThrow();
   });
 
-  it('should render users table', () => {
-    render(
-      <UsersTable
-        {...getProps({
-          users: getMockUsers(5),
-        })}
-      />
-    );
+  it('should render users in table', () => {
+    const usersData = getMockUsers(5);
+    setup({ users: usersData });
+
+    usersData.forEach((user) => {
+      expect(screen.getByText(user.name)).toBeInTheDocument();
+    });
   });
 });
 
 describe('Remove modal', () => {
   it('should render confirm check on delete', async () => {
-    render(
-      <UsersTable
-        {...getProps({
-          users: getMockUsers(3),
-        })}
-      />
-    );
+    const usersData = getMockUsers(3);
+    setup({ users: usersData });
     const user = userEvent.setup();
 
     await user.click(screen.getAllByRole('button', { name: /delete/i })[0]);
 
-    expect(screen.getAllByText(/sure/i).length).toEqual(1);
+    expect(screen.getByText(/are you sure/i)).toBeInTheDocument();
   });
 });
