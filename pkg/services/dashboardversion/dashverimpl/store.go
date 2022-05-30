fd2d48d3dd48ae267e40a8fe6a268c83438d8988
Ida Furjesova <ida.furjesova@grafana.com>
2022-05-30 11:04:29 +0200

    Fix lint 2

--- pkg/services/dashboardversion/dashverimpl/store.go
+++ pkg/services/dashboardversion/dashverimpl/store.go
@@ -19,9 +19,9 @@ type sqlStore struct {
 	db db.DB
 }
 
-func (s *sqlStore) Get(ctx context.Context, query *dashver.GetDashboardVersionQuery) (*dashver.DashboardVersion, error) {
+func (ss *sqlStore) Get(ctx context.Context, query *dashver.GetDashboardVersionQuery) (*dashver.DashboardVersion, error) {
 	var version dashver.DashboardVersion
-	err := s.db.WithDbSession(ctx, func(sess *sqlstore.DBSession) error {
+	err := ss.db.WithDbSession(ctx, func(sess *sqlstore.DBSession) error {
 		has, err := sess.Where("dashboard_version.dashboard_id=? AND dashboard_version.version=? AND dashboard.org_id=?", query.DashboardID, query.Version, query.OrgID).
 			Join("LEFT", "dashboard", `dashboard.id = dashboard_version.dashboard_id`).
 			Get(&version)
