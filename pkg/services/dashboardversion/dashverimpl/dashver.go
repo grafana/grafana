c719a8631a732dbf39cab27289954ab5263105d8
Ida Furjesova <ida.furjesova@grafana.com>
2022-05-31 14:58:30 +0200

    Split GetDashboarVersions method

--- pkg/services/dashboardversion/dashverimpl/dashver.go
+++ pkg/services/dashboardversion/dashverimpl/dashver.go
@@ -63,3 +63,11 @@ func (s *Service) DeleteExpired(ctx context.Context, cmd *dashver.DeleteExpiredV
 	}
 	return nil
 }
+
+// List all dashboard versions for the given dashboard ID.
+func (s *Service) List(ctx context.Context, query *dashver.ListDashboardVersionsQuery) ([]*dashver.DashboardVersionDTO, error) {
+	if query.Limit == 0 {
+		query.Limit = 1000
+	}
+	return s.store.List(ctx, query)
+}
