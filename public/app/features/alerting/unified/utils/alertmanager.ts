grafana/grafana

commit 651bb773dbf45148127ae404bc46676b9976dcc6

Alerting: do not unescape external AM label values (#45334)

public/app/features/alerting/unified/utils/alertmanager.ts
@@ -118,14 +118,6 @@ const matcherOperators = [
   MatcherOperator.equal,
 ];
 
-function unescapeMatcherValue(value: string) {
-  let trimmed = value.trim().replace(/\\"/g, '"');
-  if (trimmed.startsWith('"') && trimmed.endsWith('"') && !trimmed.endsWith('\\"')) {
-    trimmed = trimmed.substr(1, trimmed.length - 2);
-  }
-  return trimmed.replace(/\\"/g, '"');
-}
-
 export function parseMatcher(matcher: string): Matcher {
   const trimmed = matcher.trim();
   if (trimmed.startsWith('{') && trimmed.endsWith('}')) {
@@ -141,7 +133,7 @@ export function parseMatcher(matcher: string): Matcher {
   }
   const [operator, idx] = operatorsFound[0];
   const name = trimmed.substr(0, idx).trim();
-  const value = unescapeMatcherValue(trimmed.substr(idx + operator.length).trim());
+  const value = trimmed.substr(idx + operator.length).trim();
   if (!name) {
     throw new Error(`Invalid matcher: ${trimmed}`);
   }

public/app/features/alerting/unified/utils/alertmanager.test.ts
@@ -31,16 +31,23 @@ describe('Alertmanager utils', () => {
       });
     });
 
-    it('should parse escaped values correctly', () => {
-      expect(parseMatcher('foo=~"bar\\"baz\\""')).toEqual<Matcher>({
+    // Alertmanager has some strict requirements for label values;
+    // we should not automatically encode or decode any values sent
+    // and instead let AM return any errors like (matcher value contains unescaped double quote: bar"baz")
+    // and allow the user to update the values to the correct format
+    //
+    // see https://github.com/prometheus/alertmanager/blob/4030e3670b359b8814aa8340ea1144f32b1f5ab3/pkg/labels/parse.go#L55-L99
+    // and https://github.com/prometheus/alertmanager/blob/4030e3670b359b8814aa8340ea1144f32b1f5ab3/pkg/labels/parse.go#L101-L178
+    it('should not parse escaped values', () => {
+      expect(parseMatcher('foo="^[a-z0-9-]{1}[a-z0-9-]{0,30}$"')).toEqual<Matcher>({
         name: 'foo',
-        value: 'bar"baz"',
-        isRegex: true,
+        value: '"^[a-z0-9-]{1}[a-z0-9-]{0,30}$"',
+        isRegex: false,
         isEqual: true,
       });
       expect(parseMatcher('foo=~bar\\"baz\\"')).toEqual<Matcher>({
         name: 'foo',
-        value: 'bar"baz"',
+        value: 'bar\\"baz\\"',
         isRegex: true,
         isEqual: true,
       });

commit 021dd3ef9f94177c1fbb125fc160ebf652c6d055

Alerting: unescape label matcher values when migrating matchers
When alert rules are migrated, the "matchers" field is used. The value of each label matcher is also wrapped in double quotes.

When editing the notification policy afterwards we use the "object_matchers" property but do not remove the double quotes around the value.

A regression was introduced in https://github.com/grafana/grafana/pull/45334/files in the attempt to fix another bug related to label matcher values.

This commit re-introduces the "unescapeMatcherValue" function that is called when updating Grafana managed alert rules with existing "matchers" instead of "object_matchers".

In the event that the migrated alert rule had label matchers

public/app/features/alerting/unified/utils/alertmanager.ts
@@ -117,6 +117,16 @@ export function matchersToString(matchers: Matcher[]) {
   return `{${combinedMatchers}}`;
 }
 
+// this function is used to unescape label values that are recorded in the "matchers" property as opposed to "object_matchers"
+// when we edit a Grafana managed rule that was migrated
+function unescapeMatcherValue(value: string) {
+  let trimmed = value.trim().replace(/\\"/g, '"');
+  if (trimmed.startsWith('"') && trimmed.endsWith('"') && !trimmed.endsWith('\\"')) {
+    trimmed = trimmed.slice(1, trimmed.length - 1);
+  }
+  return trimmed.replace(/\\"/g, '"');
+}
+
 export const matcherFieldOptions: SelectableValue[] = [
   { label: MatcherOperator.equal, description: 'Equals', value: MatcherOperator.equal },
   { label: MatcherOperator.notEqual, description: 'Does not equal', value: MatcherOperator.notEqual },
@@ -146,7 +156,7 @@ export function parseMatcher(matcher: string): Matcher {
   }
   const [operator, idx] = operatorsFound[0];
   const name = trimmed.slice(0, idx).trim();
-  const value = trimmed.slice(idx + operator.length).trim();
+  const value = unescapeMatcherValue(trimmed.slice(idx + operator.length).trim());
   if (!name) {
     throw new Error(`Invalid matcher: ${trimmed}`);
   }

public/app/features/alerting/unified/utils/alertmanager.test.ts
@@ -38,23 +38,27 @@ describe('Alertmanager utils', () => {
       });
     });
 
-    // Alertmanager has some strict requirements for label values;
-    // we should not automatically encode or decode any values sent
-    // and instead let AM return any errors like (matcher value contains unescaped double quote: bar"baz")
-    // and allow the user to update the values to the correct format
+    // For cloud rules we use the "matchers" property where the entire matcher is encoded in a single string and values might need to be wrapped in escaped double quotes.
+    // Grafana managed rules instead use a "object_matchers" proprietary field where values do not need to be escaped.
     //
     // see https://github.com/prometheus/alertmanager/blob/4030e3670b359b8814aa8340ea1144f32b1f5ab3/pkg/labels/parse.go#L55-L99
     // and https://github.com/prometheus/alertmanager/blob/4030e3670b359b8814aa8340ea1144f32b1f5ab3/pkg/labels/parse.go#L101-L178
-    it('should not parse escaped values', () => {
-      expect(parseMatcher('foo="^[a-z0-9-]{1}[a-z0-9-]{0,30}$"')).toEqual<Matcher>({
+    it('should parse escaped values', () => {
+      expect(parseMatcher('foo="bar"')).toEqual<Matcher>({
         name: 'foo',
-        value: '"^[a-z0-9-]{1}[a-z0-9-]{0,30}$"',
+        value: 'bar',
         isRegex: false,
+        isEqual: true,
+      });
+      expect(parseMatcher('foo=~"^[a-z0-9-]{1}[a-z0-9-]{0,30}$"')).toEqual<Matcher>({
+        name: 'foo',
+        value: '^[a-z0-9-]{1}[a-z0-9-]{0,30}$',
+        isRegex: true,
         isEqual: true,
       });
       expect(parseMatcher('foo=~bar\\"baz\\"')).toEqual<Matcher>({
         name: 'foo',
-        value: 'bar\\"baz\\"',
+        value: 'bar"baz"',
         isRegex: true,
         isEqual: true,
       });