070ad6eace63a59346d702149980ee027c620857
Jeff Levin <jeff@levinology.com>
2022-05-31 14:21:15 -0800

    fix migration

--- pkg/services/sqlstore/migrations/dashboard_public_config_mig.go
+++ pkg/services/sqlstore/migrations/dashboard_public_config_mig.go
@@ -12,13 +12,21 @@ func addPublicDashboardMigration(mg *Migrator) {
 			{Name: "dashboard_uid", Type: DB_NVarchar, Length: 40, Nullable: false},
 			{Name: "org_id", Type: DB_BigInt, Nullable: false},
 			{Name: "time_variables", Type: DB_Text, Nullable: false},
+			{Name: "refresh_rate", Type: DB_Int, Nullable: false, Default: "30"},
+			{Name: "template_variables", Type: DB_MediumText, Nullable: true},
 		},
 		Indices: []*Index{
 			{Cols: []string{"uid"}, Type: UniqueIndex},
 			{Cols: []string{"org_id", "dashboard_uid"}},
 		},
 	}
-
 	mg.AddMigration("create dashboard public config v1", NewAddTableMigration(dashboardPublicCfgV1))
+
+	// table has no dependencies. Drop then recreate with correct values
+	addDropAllIndicesMigrations(mg, "v1", dashboardPublicCfgV1)
+	mg.AddMigration("Drop old dashboard public config table", NewDropTableMigration("dashboard_public_config"))
+
+	// recreate table with proper primary key type
+	mg.AddMigration("recreate dashboard public config v1", NewAddTableMigration(dashboardPublicCfgV1))
 	addTableIndicesMigrations(mg, "v1", dashboardPublicCfgV1)
 }
