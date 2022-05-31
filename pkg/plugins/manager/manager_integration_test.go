9bd24fb4a919e1bf4baaf2d3f750b87880ec1dd8
Will Browne <will.browne@grafana.com>
2022-05-31 16:49:14 +0100

    adjust ctx in test

--- pkg/plugins/manager/manager_integration_test.go
+++ pkg/plugins/manager/manager_integration_test.go
@@ -97,12 +97,13 @@ func TestPluginManager_int_init(t *testing.T) {
 		provider.ProvideService(coreRegistry)))
 	require.NoError(t, err)
 
-	verifyCorePluginCatalogue(t, pm)
-	verifyBundledPlugins(t, pm)
-	verifyPluginStaticRoutes(t, pm)
+	ctx := context.Background()
+	verifyCorePluginCatalogue(t, ctx, pm)
+	verifyBundledPlugins(t, ctx, pm)
+	verifyPluginStaticRoutes(t, ctx, pm)
 }
 
-func verifyCorePluginCatalogue(t *testing.T, pm *PluginManager) {
+func verifyCorePluginCatalogue(t *testing.T, ctx context.Context, pm *PluginManager) {
 	t.Helper()
 
 	expPanels := map[string]struct{}{
@@ -168,44 +169,44 @@ func verifyCorePluginCatalogue(t *testing.T, pm *PluginManager) {
 		"test-app": {},
 	}
 
-	panels := pm.Plugins(context.Background(), plugins.Panel)
+	panels := pm.Plugins(ctx, plugins.Panel)
 	assert.Equal(t, len(expPanels), len(panels))
 	for _, p := range panels {
-		p, exists := pm.Plugin(context.Background(), p.ID)
+		p, exists := pm.Plugin(ctx, p.ID)
 		require.NotEqual(t, plugins.PluginDTO{}, p)
 		assert.True(t, exists)
 		assert.Contains(t, expPanels, p.ID)
-		assert.Contains(t, pm.registeredPlugins(context.Background()), p.ID)
+		assert.Contains(t, pm.registeredPlugins(ctx), p.ID)
 	}
 
-	dataSources := pm.Plugins(context.Background(), plugins.DataSource)
+	dataSources := pm.Plugins(ctx, plugins.DataSource)
 	assert.Equal(t, len(expDataSources), len(dataSources))
 	for _, ds := range dataSources {
-		p, exists := pm.Plugin(context.Background(), ds.ID)
+		p, exists := pm.Plugin(ctx, ds.ID)
 		require.NotEqual(t, plugins.PluginDTO{}, p)
 		assert.True(t, exists)
 		assert.Contains(t, expDataSources, ds.ID)
-		assert.Contains(t, pm.registeredPlugins(context.Background()), ds.ID)
+		assert.Contains(t, pm.registeredPlugins(ctx), ds.ID)
 	}
 
-	apps := pm.Plugins(context.Background(), plugins.App)
+	apps := pm.Plugins(ctx, plugins.App)
 	assert.Equal(t, len(expApps), len(apps))
 	for _, app := range apps {
-		p, exists := pm.Plugin(context.Background(), app.ID)
+		p, exists := pm.Plugin(ctx, app.ID)
 		require.NotEqual(t, plugins.PluginDTO{}, p)
 		assert.True(t, exists)
 		assert.Contains(t, expApps, app.ID)
-		assert.Contains(t, pm.registeredPlugins(context.Background()), app.ID)
+		assert.Contains(t, pm.registeredPlugins(ctx), app.ID)
 	}
 
-	assert.Equal(t, len(expPanels)+len(expDataSources)+len(expApps), len(pm.Plugins(context.Background())))
+	assert.Equal(t, len(expPanels)+len(expDataSources)+len(expApps), len(pm.Plugins(ctx)))
 }
 
-func verifyBundledPlugins(t *testing.T, pm *PluginManager) {
+func verifyBundledPlugins(t *testing.T, ctx context.Context, pm *PluginManager) {
 	t.Helper()
 
 	dsPlugins := make(map[string]struct{})
-	for _, p := range pm.Plugins(context.Background(), plugins.DataSource) {
+	for _, p := range pm.Plugins(ctx, plugins.DataSource) {
 		dsPlugins[p.ID] = struct{}{}
 	}
 
@@ -214,7 +215,7 @@ func verifyBundledPlugins(t *testing.T, pm *PluginManager) {
 		pluginRoutes[r.PluginID] = r
 	}
 
-	inputPlugin, exists := pm.Plugin(context.Background(), "input")
+	inputPlugin, exists := pm.Plugin(ctx, "input")
 	require.NotEqual(t, plugins.PluginDTO{}, inputPlugin)
 	assert.True(t, exists)
 	assert.NotNil(t, dsPlugins["input"])
@@ -225,7 +226,7 @@ func verifyBundledPlugins(t *testing.T, pm *PluginManager) {
 	}
 }
 
-func verifyPluginStaticRoutes(t *testing.T, pm *PluginManager) {
+func verifyPluginStaticRoutes(t *testing.T, ctx context.Context, pm *PluginManager) {
 	routes := make(map[string]*plugins.StaticRoute)
 	for _, route := range pm.Routes() {
 		routes[route.PluginID] = route
@@ -233,11 +234,11 @@ func verifyPluginStaticRoutes(t *testing.T, pm *PluginManager) {
 
 	assert.Len(t, routes, 2)
 
-	inputPlugin, _ := pm.Plugin(context.Background(), "input")
+	inputPlugin, _ := pm.Plugin(ctx, "input")
 	assert.NotNil(t, routes["input"])
 	assert.Equal(t, routes["input"].Directory, inputPlugin.PluginDir)
 
-	testAppPlugin, _ := pm.Plugin(context.Background(), "test-app")
+	testAppPlugin, _ := pm.Plugin(ctx, "test-app")
 	assert.Contains(t, routes, "test-app")
 	assert.Equal(t, routes["test-app"].Directory, testAppPlugin.PluginDir)
 }
