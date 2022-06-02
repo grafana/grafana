77edb353d62cbcd1fbb4d442cacf9407a1722e2b
Konrad Lalik <konrad.lalik@grafana.com>
2022-06-02 15:59:36 +0200

    Fix lint

--- public/app/core/components/Select/FolderPicker.test.tsx
+++ public/app/core/components/Select/FolderPicker.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import selectEvent from 'react-select-event';
 
