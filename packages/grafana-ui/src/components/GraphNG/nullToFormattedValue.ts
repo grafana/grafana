8cef5e925c3496ed0ff032a6ce06333f3b7b5b68
Kyle Cunningham <kyle@codeincarnate.com>
2022-06-01 21:55:12 -0500

    Comment out unused vars

--- packages/grafana-ui/src/components/GraphNG/nullToFormattedValue.ts
+++ packages/grafana-ui/src/components/GraphNG/nullToFormattedValue.ts
@@ -1,11 +1,15 @@
-import { getValueFormat, DataFrame, ThresholdsMode } from '@grafana/data';
+import {
+  // getValueFormat,
+  DataFrame,
+  // ThresholdsMode
+} from '@grafana/data';
 
 export function nullToFormattedValue(frame: DataFrame) {
   frame.fields.forEach((f) => {
     const noValue = +f.config?.noValue!;
-    const disp = getValueFormat(
-      f.config.thresholds?.mode === ThresholdsMode.Percentage ? 'percent' : f.config.unit ?? ''
-    );
+    // const disp = getValueFormat(
+    //   f.config.thresholds?.mode === ThresholdsMode.Percentage ? 'percent' : f.config.unit ?? ''
+    // );
 
     if (!Number.isNaN(noValue)) {
       const values = f.values.toArray();
