d41b0f0a21b18b468296f1349b1aa60cd5136abe
Piotr Jamróz <pm.jamroz@gmail.com>
2022-06-02 15:48:04 +0200

    Test commenting query history (#49802)

--- public/app/features/explore/spec/queryHistory.test.tsx
+++ public/app/features/explore/spec/queryHistory.test.tsx
@@ -11,12 +11,14 @@ import {
   assertDataSourceFilterVisibility,
   assertLoadMoreQueryHistoryNotVisible,
   assertQueryHistory,
+  assertQueryHistoryComment,
   assertQueryHistoryElementsShown,
   assertQueryHistoryExists,
   assertQueryHistoryIsStarred,
   assertQueryHistoryTabIsSelected,
 } from './helper/assert';
 import {
+  commentQueryHistory,
   closeQueryHistory,
   deleteQueryHistory,
   inputQuery,
@@ -180,6 +182,25 @@ describe('Explore: Query History', () => {
     expect(reportInteractionMock).toBeCalledWith('grafana_explore_query_history_deleted', {
       queryHistoryEnabled: false,
     });
+  });
+
+  it('add comments to query history', async () => {
+    const urlParams = {
+      left: serializeStateToUrlParam({
+        datasource: 'loki',
+        queries: [{ refId: 'A', expr: 'query #1' }],
+        range: { from: 'now-1h', to: 'now' },
+      }),
+    };
+
+    const { datasources } = setupExplore({ urlParams });
+    (datasources.loki.query as jest.Mock).mockReturnValueOnce(makeLogsQueryResponse());
+    await waitForExplore();
+    await openQueryHistory();
+    await assertQueryHistory(['{"expr":"query #1"}'], ExploreId.left);
+
+    await commentQueryHistory(0, 'test comment');
+    await assertQueryHistoryComment(['test comment'], ExploreId.left);
   });
 
   it('updates query history settings', async () => {
