76d3dc7c1ab2ad45cd6a67e8a1327791bef143fb
Todd Treece <todd.treece@grafana.com>
2022-06-03 22:14:42 -0400

    re-enable streaming feature flag by default

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -220,6 +220,7 @@ var (
 			Name:        "prometheusStreamingJSONParser",
 			Description: "Enable streaming JSON parser for Prometheus datasource",
 			State:       FeatureStateBeta,
+			Expression:  "true", // Enabled by default
 		},
 		{
 			Name:            "validateDashboardsOnSave",
