e9478130c6f840130fe89b27e784c1b59f9b62f8
Agnès Toulet <agnes.toulet@gmail.com>
2022-06-07 15:00:24 +0200

    rename cleanUp function

--- public/app/features/dashboard/components/DeleteDashboard/useDashboardDelete.tsx
+++ public/app/features/dashboard/components/DeleteDashboard/useDashboardDelete.tsx
@@ -5,17 +5,17 @@ import { locationService } from '@grafana/runtime';
 import { useAppNotification } from 'app/core/copy/appNotification';
 import { deleteDashboard } from 'app/features/manage-dashboards/state/actions';
 
-export const useDashboardDelete = (uid: string, cleanDashboard: () => void) => {
+export const useDashboardDelete = (uid: string, cleanUpDashboardAndVariables: () => void) => {
   const [state, onDeleteDashboard] = useAsyncFn(() => deleteDashboard(uid, false), []);
   const notifyApp = useAppNotification();
 
   useEffect(() => {
     if (state.value) {
-      cleanDashboard();
+      cleanUpDashboardAndVariables();
       locationService.replace('/');
       notifyApp.success('Dashboard Deleted', `${state.value.title} has been deleted`);
     }
-  }, [state, notifyApp, cleanDashboard]);
+  }, [state, notifyApp, cleanUpDashboardAndVariables]);
 
   return { state, onDeleteDashboard };
 };
