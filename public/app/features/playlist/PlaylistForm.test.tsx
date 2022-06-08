b8db5625c34e27410fbfa03a53fc91b8d4602447
Ashley Harrison <ashley.harrison@grafana.com>
2022-06-08 10:26:08 +0100

    fix linting errors

--- public/app/features/playlist/PlaylistForm.test.tsx
+++ public/app/features/playlist/PlaylistForm.test.tsx
@@ -1,5 +1,5 @@
 import { within } from '@testing-library/dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
