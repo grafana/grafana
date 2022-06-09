9c1944aba4b5146aa69b67e0a08ef5d37c44e847
Levente Balogh <balogh.levente.hu@gmail.com>
2022-06-09 12:50:55 +0200

    feat(Data Connections): load the cloud-onboarding app under the "Cloud onboarding" tab

--- public/app/features/data-connections/constants.ts
+++ public/app/features/data-connections/constants.ts
@@ -1,3 +1,9 @@
+// The ID of the app plugin that we render under that "Cloud Integrations" tab
+export const CLOUD_ONBOARDING_APP_ID = 'grafana-easystart-app';
+
+// The ID of the main nav-tree item (the main item in the NavIndex)
+export const ROUTE_BASE_ID = 'data-connections';
+
 export enum ROUTES {
   DataSources = '/data-connections/data-sources',
   Plugins = '/data-connections/plugins',
