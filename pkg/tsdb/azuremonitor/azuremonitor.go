b759c4f692dddd3b5ad3227076c3b4a03bdc62cf
Andreas Christou <andreas.christou@grafana.com>
2022-06-09 17:36:41 +0100

    Improve errors and responses from health endpoint

--- pkg/tsdb/azuremonitor/azuremonitor.go
+++ pkg/tsdb/azuremonitor/azuremonitor.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
@@ -178,83 +179,79 @@ func (s *Service) getDSInfo(pluginCtx backend.PluginContext) (types.DatasourceIn
 	return instance, nil
 }
 
-func checkAzureMonitorMetricsHealth(dsInfo types.DatasourceInfo) error {
-	// Azure Monitor health check
+func checkAzureMonitorMetricsHealth(dsInfo types.DatasourceInfo) (*http.Response, error) {
 	url := fmt.Sprintf("%v/subscriptions?api-version=2019-03-01", dsInfo.Routes["Azure Monitor"].URL)
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	_, err = dsInfo.Services["Azure Monitor"].HTTPClient.Do(request)
+	res, err := dsInfo.Services["Azure Monitor"].HTTPClient.Do(request)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return res, nil
 }
 
-func checkAzureLogAnalyticsHealth(dsInfo types.DatasourceInfo) error {
+func checkAzureLogAnalyticsHealth(dsInfo types.DatasourceInfo) (*http.Response, error) {
 	defaultWorkspaceId := dsInfo.Settings.LogAnalyticsDefaultWorkspace
 
 	if defaultWorkspaceId == "" {
 		workspacesUrl := fmt.Sprintf("%v/subscriptions/%v/providers/Microsoft.OperationalInsights/workspaces?api-version=2017-04-26-preview", dsInfo.Routes["Azure Monitor"].URL, dsInfo.Settings.SubscriptionId)
 		workspacesReq, err := http.NewRequest(http.MethodGet, workspacesUrl, nil)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		res, err := dsInfo.Services["Azure Monitor"].HTTPClient.Do(workspacesReq)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		var target struct {
 			Value []types.LogAnalyticsWorkspaceResponse
 		}
 		json.NewDecoder(res.Body).Decode(&target)
 		if len(target.Value) == 0 {
-			return errors.New("no default workspace found")
+			return nil, errors.New("no default workspace found")
 		}
 		defaultWorkspaceId = target.Value[0].Properties.CustomerId
 	}
 
 	workspaceUrl := fmt.Sprintf("%v/v1/workspaces/%v/metadata", dsInfo.Routes["Azure Log Analytics"].URL, defaultWorkspaceId)
-	fmt.Println(defaultWorkspaceId, workspaceUrl)
 	workspaceReq, err := http.NewRequest(http.MethodGet, workspaceUrl, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	_, err = dsInfo.Services["Azure Log Analytics"].HTTPClient.Do(workspaceReq)
+	res, err := dsInfo.Services["Azure Log Analytics"].HTTPClient.Do(workspaceReq)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return res, nil
 }
 
-func checkAzureMonitorGraphHealth(dsInfo types.DatasourceInfo) error {
-	// Azure Resource Graph health check
+func checkAzureMonitorGraphHealth(dsInfo types.DatasourceInfo) (*http.Response, error) {
 	body, err := json.Marshal(map[string]interface{}{
 		"query":         "Resources | project id | limit 1",
 		"subscriptions": []string{dsInfo.Settings.SubscriptionId},
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 	url := fmt.Sprintf("%v/providers/Microsoft.ResourceGraph/resources?api-version=2021-06-01-preview", dsInfo.Routes["Azure Resource Graph"].URL)
 	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
 	request.Header.Set("Content-Type", "application/json")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	_, err = dsInfo.Services["Azure Resource Graph"].HTTPClient.Do(request)
+	res, err := dsInfo.Services["Azure Resource Graph"].HTTPClient.Do(request)
 	if err != nil {
-		fmt.Println(err)
-		return err
+		return nil, err
 	}
 
-	return nil
+	return res, nil
 }
 
 func (s *Service) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
@@ -263,35 +260,62 @@ func (s *Service) CheckHealth(ctx context.Context, req *backend.CheckHealthReque
 		return nil, err
 	}
 
-	err = checkAzureMonitorMetricsHealth(dsInfo)
-	if err != nil {
-		backend.Logger.Error(err.Error())
-		return &backend.CheckHealthResult{
-			Status:  backend.HealthStatusError,
-			Message: fmt.Sprintf("Error querying Azure Monitor endpoint: %v", err.Error()),
-		}, nil
+	status := backend.HealthStatusOk
+	message := ""
+
+	metricsRes, err := checkAzureMonitorMetricsHealth(dsInfo)
+	if err != nil || metricsRes.StatusCode != 200 {
+		if err != nil {
+			return nil, err
+		} else {
+			body, err := io.ReadAll(metricsRes.Body)
+			if err != nil {
+				return nil, err
+			}
+			backend.Logger.Error(string(body))
+		}
+		status = backend.HealthStatusError
+		message = "1. Error connecting to Azure Monitor endpoint."
+	} else {
+		message = "1. Successfully connected to Azure Monitor endpoint."
 	}
 
-	err = checkAzureLogAnalyticsHealth(dsInfo)
-	if err != nil {
-		backend.Logger.Error(err.Error())
-		return &backend.CheckHealthResult{
-			Status:  backend.HealthStatusError,
-			Message: fmt.Sprintf("Error querying Azure Log Analytics endpoint: %v", err.Error()),
-		}, nil
+	logsRes, err := checkAzureLogAnalyticsHealth(dsInfo)
+	if err != nil || logsRes.StatusCode != 200 {
+		if err != nil {
+			return nil, err
+		} else {
+			body, err := io.ReadAll(logsRes.Body)
+			if err != nil {
+				return nil, err
+			}
+			backend.Logger.Error(string(body))
+		}
+		status = backend.HealthStatusError
+		message = fmt.Sprintf("%v\n 2. Error connecting to Azure Log Analytics endpoint.", message)
+	} else {
+		message = fmt.Sprintf("%v\n 2. Successfully connected to Azure Log Analytics endpoint.", message)
 	}
 
-	err = checkAzureMonitorGraphHealth(dsInfo)
-	if err != nil {
-		backend.Logger.Error(err.Error())
-		return &backend.CheckHealthResult{
-			Status:  backend.HealthStatusError,
-			Message: fmt.Sprintf("Error querying Azure Resource Graph endpoint: %v", err.Error()),
-		}, nil
+	resourceGraphRes, err := checkAzureMonitorGraphHealth(dsInfo)
+	if err != nil || resourceGraphRes.StatusCode != 200 {
+		if err != nil {
+			return nil, err
+		} else {
+			body, err := io.ReadAll(resourceGraphRes.Body)
+			if err != nil {
+				return nil, err
+			}
+			backend.Logger.Error(string(body))
+		}
+		status = backend.HealthStatusError
+		message = fmt.Sprintf("%v\n 3. Error connecting to Azure Resource Graph endpoint.", message)
+	} else {
+		message = fmt.Sprintf("%v\n 3. Successfully connected to Azure Resource Graph endpoint.", message)
 	}
 
 	return &backend.CheckHealthResult{
-		Status:  backend.HealthStatusOk,
-		Message: "Successfully queried all Azure Monitor services.",
+		Status:  status,
+		Message: message,
 	}, nil
 }
