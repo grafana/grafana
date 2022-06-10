dd32db90ef49e41e13f0a04f7f9f7595a7946137
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-06-10 11:50:40 -0700

    make overrides optional

--- public/app/plugins/datasource/cloud-monitoring/__mocks__/cloudMonitoringMetricDescriptor.ts
+++ public/app/plugins/datasource/cloud-monitoring/__mocks__/cloudMonitoringMetricDescriptor.ts
@@ -1,6 +1,6 @@
 import { MetricDescriptor, MetricKind, ValueTypes } from '../types';
 
-export const createMockMetricDescriptor = (overrides: Partial<MetricDescriptor>): MetricDescriptor => {
+export const createMockMetricDescriptor = (overrides?: Partial<MetricDescriptor>): MetricDescriptor => {
   return {
     metricKind: MetricKind.CUMULATIVE,
     valueType: ValueTypes.DOUBLE,
