a180158580ed39cf24edaed06d60072af9ede8f1
Ryan McKinley <ryantxu@gmail.com>
2022-06-10 12:24:04 -0700

    add storage page

--- public/app/features/storage/StoragePage.tsx
+++ public/app/features/storage/StoragePage.tsx
@@ -1,30 +1,50 @@
 import { css } from '@emotion/css';
 import React, { useState } from 'react';
+import { useAsync } from 'react-use';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { FilterInput, useStyles2 } from '@grafana/ui';
+import { dataFrameToJSON, GrafanaTheme2 } from '@grafana/data';
+import { FilterInput, Spinner, useStyles2 } from '@grafana/ui';
 import Page from 'app/core/components/Page/Page';
 import { useNavModel } from 'app/core/hooks/useNavModel';
 
-import { FileBrowser } from './FileBrowser';
+import { getGrafanaStorage } from './helper';
 
 export default function StoragePage() {
   const styles = useStyles2(getStyles);
   const navModel = useNavModel('storage');
   const [searchQuery, setSearchQuery] = useState('');
-  const [path, setPath] = useState('');
+  const [path] = useState('upload'); // from URL?
+
+  const folder = useAsync(async () => {
+    return getGrafanaStorage().list(path);
+  }, [path]);
+
+  const renderFolder = () => {
+    if (folder.value) {
+      if (!folder.value.length) {
+        return <div>nothing... TODO, big upload form</div>;
+      }
+
+      return (
+        <div>
+          <pre>{JSON.stringify(dataFrameToJSON(folder.value), null, 2)}</pre>
+        </div>
+      );
+    }
+    if (folder.loading) {
+      return <Spinner />;
+    }
+    return <div>??</div>; // should not be possible
+  };
 
   return (
     <Page navModel={navModel}>
-      <Page.Contents>
+      <Page.Contents isLoading={folder.loading}>
         <div className={styles.toolbar}>
           <FilterInput value={searchQuery} onChange={setSearchQuery} placeholder="Search by name or type" width={50} />
         </div>
 
-        <div className={styles.border}>
-          <FileBrowser path={path} onPathChange={setPath} />
-          TODO: upload to this folder....
-        </div>
+        {renderFolder()}
       </Page.Contents>
     </Page>
   );
