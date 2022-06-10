27e568c4e2a4055f3e673fb5f97e3810d3db57c2
Ryan McKinley <ryantxu@gmail.com>
2022-06-10 12:24:21 -0700

    add storage page

---
+++ public/app/features/storage/helper.ts
@@ -0,0 +1,32 @@
+import { DataFrame, dataFrameFromJSON, DataFrameJSON } from '@grafana/data';
+import { getBackendSrv } from '@grafana/runtime';
+
+// Likely should be built into the search interface!
+export interface GrafanaStorage {
+  list: (path: string) => Promise<DataFrame | undefined>;
+}
+
+class SimpleStorage implements GrafanaStorage {
+  constructor() {}
+
+  async list(path: string): Promise<DataFrame | undefined> {
+    let url = 'api/storage/list/';
+    if (path) {
+      url += path + '/';
+    }
+    const rsp = (await getBackendSrv().get(url)) as DataFrameJSON;
+    if (rsp && rsp.data) {
+      return dataFrameFromJSON(rsp);
+    }
+    return undefined;
+  }
+}
+
+let storage: GrafanaStorage | undefined;
+
+export function getGrafanaStorage() {
+  if (!storage) {
+    storage = new SimpleStorage();
+  }
+  return storage;
+}
