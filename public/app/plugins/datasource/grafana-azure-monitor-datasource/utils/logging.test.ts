0626f6b3bff0bdcec6bef62788c8d07ab7d7eed9
yaellec <yaelle.chaudy@grafana.com>
2022-06-13 18:02:33 +0200

    added logging tests

---
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/utils/logging.test.ts
@@ -0,0 +1,56 @@
+import { AzureQueryType } from '../types';
+
+import { getAzureMonitorEvent } from './logging';
+
+describe('AzureMonitor: logging', () => {
+  it('return a properly formed event for Azure Monitor metrics', () => {
+    const query = {
+      refId: 'A',
+      queryType: AzureQueryType.AzureMonitor,
+      azureMonitor: {
+        dimensionFilters: [],
+        alias: 'new name for it',
+        top: '10',
+      },
+    };
+    expect(getAzureMonitorEvent(query)).toEqual({
+      query_type: AzureQueryType.AzureMonitor,
+      alias: true,
+      top: true,
+      dimensions: 0,
+    });
+  });
+  it('return a properly formed event for Logs Analytics', () => {
+    const query = {
+      refId: 'B',
+      queryType: AzureQueryType.LogAnalytics,
+      azureLogAnalytics: {
+        query: `Perf
+        | where $__timeFilter(TimeGenerated)
+        | where CounterName == "% Processor Time"
+        | summarize avg(CounterValue) by bin(TimeGenerated, 5m), Computer
+        | order by TimeGenerated asc`,
+        resultFormat: 'table',
+      },
+    };
+    expect(getAzureMonitorEvent(query)).toEqual({
+      query_type: AzureQueryType.LogAnalytics,
+      complexity: 4,
+      format: 'table',
+    });
+  });
+  it('return a properly formed event for Azure resource graph', () => {
+    const query = {
+      refId: 'C',
+      queryType: AzureQueryType.AzureResourceGraph,
+      azureResourceGraph: {
+        query: 'resources | count ',
+        resultFormat: 'table',
+      },
+    };
+    expect(getAzureMonitorEvent(query)).toEqual({
+      query_type: AzureQueryType.AzureResourceGraph,
+      complexity: 1,
+    });
+  });
+});
