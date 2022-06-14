10b432ec5f36d9c1e04e2ae3f40a4a159880e931
Isabella Siu <isabella.siu@grafana.com>
2022-06-14 15:42:20 -0400

    CloudWatch: Factor LogGroupSelector into a separate component

--- public/app/plugins/datasource/cloudwatch/components/LogsQueryEditor.tsx
+++ public/app/plugins/datasource/cloudwatch/components/LogsQueryEditor.tsx
@@ -14,6 +14,7 @@ import { CloudWatchLogsQueryField } from './LogsQueryField';
 
 type Props = QueryEditorProps<CloudWatchDatasource, CloudWatchQuery, CloudWatchJsonData> & {
   allowCustomValue?: boolean;
+  query: CloudWatchLogsQuery;
 };
 
 const labelClass = css`
