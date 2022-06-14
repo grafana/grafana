a33a02362963d5f6ee78a4bbcae4252d9f88937b
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2022-06-14 15:32:52 -0400

    backend: add PlaylistUIDs to Playlist; remove playlist IDs from API (#49609)
    * backend/api: refactor PlaylistId to PlaylistUid
    * Add org_id to Get and Update playlist functions
    Fix migration - no longer pad the uid; fix mysql syntax
    
    The relevant tests are passing using postgres, mysql and the default sqllite backends, but there are a number of other failing tests when using postgres and myself so I'm not entirely confident with those results.
    
    * fix bad query in GetPlaylistItem and add a test that would have caught the mistake in the first place. Reverted the playlist_uid column addition in playlist_item; it became unnecessary after this PR.
    
    Added default value to the new UID column based on PR feedback.
    
    * break this PRs migration into its own function
    
    * Playlists: Update UI to use the updated API
    
    Co-authored-by: Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>

--- public/app/features/playlist/PlaylistEditPage.tsx
+++ public/app/features/playlist/PlaylistEditPage.tsx
@@ -21,16 +21,16 @@ interface ConnectedProps {
 }
 
 export interface RouteParams {
-  id: number;
+  uid: string;
 }
 
 interface Props extends ConnectedProps, GrafanaRouteComponentProps<RouteParams> {}
 
 export const PlaylistEditPage: FC<Props> = ({ navModel, match }) => {
   const styles = useStyles2(getPlaylistStyles);
-  const { playlist, loading } = usePlaylist(match.params.id);
+  const { playlist, loading } = usePlaylist(match.params.uid);
   const onSubmit = async (playlist: Playlist) => {
-    await updatePlaylist(match.params.id, playlist);
+    await updatePlaylist(match.params.uid, playlist);
     locationService.push('/playlists');
   };
 
