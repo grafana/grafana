9e4abb209c0364b98459e3352118f2c2b3c5eede
Alexander Zobnin <alexanderzobnin@gmail.com>
2022-06-15 16:47:12 +0300

    Service accounts: Migrate expired API keys

--- pkg/services/sqlstore/apikey.go
+++ pkg/services/sqlstore/apikey.go
@@ -46,8 +46,7 @@ func (ss *SQLStore) GetAPIKeys(ctx context.Context, query *models.GetApiKeysQuer
 func (ss *SQLStore) GetAllAPIKeys(ctx context.Context, orgID int64) []*models.ApiKey {
 	result := make([]*models.ApiKey, 0)
 	err := ss.WithDbSession(ctx, func(dbSession *DBSession) error {
-		sess := dbSession.
-			Where("(expires IS NULL OR expires >= ?) AND service_account_id IS NULL", timeNow().Unix()).Asc("name")
+		sess := dbSession.Where("service_account_id IS NULL").Asc("name")
 		if orgID != -1 {
 			sess = sess.Where("org_id=?", orgID)
 		}
