a03f40a81d69ea613ecd55e34b0a422f67b785f4
drew08t <drew08@gmail.com>
2022-06-15 09:50:24 -0700

    Add NumberInput to core slider

--- public/app/core/components/OptionsUI/slider.tsx
+++ public/app/core/components/OptionsUI/slider.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { cx } from '@emotion/css';
+import { Global } from '@emotion/react';
+import SliderComponent from 'rc-slider';
+import React, { useCallback, useState } from 'react';
 
 import { FieldConfigEditorProps, SliderFieldConfigSettings } from '@grafana/data';
-import { Slider } from '@grafana/ui';
+import { useTheme2 } from '@grafana/ui';
+
+import { getStyles } from '../../../../../packages/grafana-ui/src/components/Slider/styles';
+
+import { NumberInput } from './NumberInput';
 
 export const SliderValueEditor: React.FC<FieldConfigEditorProps<number, SliderFieldConfigSettings>> = ({
   value,
@@ -9,18 +16,70 @@ export const SliderValueEditor: React.FC<FieldConfigEditorProps<number, SliderFi
   item,
 }) => {
   const { settings } = item;
-  const initialValue = typeof value === 'number' ? value : typeof value === 'string' ? +value : 0;
+  const min = settings?.min || 0;
+  const max = settings?.max || 100;
+  const step = settings?.step;
+  const marks = settings?.marks;
+  const included = settings?.included;
+  const ariaLabelForHandle = settings?.ariaLabelForHandle;
+
+  const isHorizontal = true;
+  const theme = useTheme2();
+  const styles = getStyles(theme, isHorizontal, Boolean(marks));
+  const SliderWithTooltip = SliderComponent;
+  const [sliderValue, setSliderValue] = useState<number>(value ?? min);
+
+  const onSliderChange = useCallback(
+    (v: number) => {
+      setSliderValue(v);
+
+      if (onChange) {
+        onChange(v);
+      }
+    },
+    [setSliderValue, onChange]
+  );
+
+  const onSliderInputChange = useCallback(
+    (value?: number) => {
+      let v = value;
+
+      if (Number.isNaN(v) || !v) {
+        v = 0;
+      }
+
+      setSliderValue(v);
+
+      if (onChange) {
+        onChange(v);
+      }
+    },
+    [onChange]
+  );
+
+  const sliderInputClassNames = !isHorizontal ? [styles.sliderInputVertical] : [];
 
   return (
-    <Slider
-      value={initialValue}
-      min={settings?.min || 0}
-      max={settings?.max || 100}
-      step={settings?.step}
-      marks={settings?.marks}
-      included={settings?.included}
-      onChange={onChange}
-      ariaLabelForHandle={settings?.ariaLabelForHandle}
-    />
+    <div className={cx(styles.container, styles.slider)}>
+      {/** Slider tooltip's parent component is body and therefore we need Global component to do css overrides for it. */}
+      <Global styles={styles.tooltip} />
+      <label className={cx(styles.sliderInput, ...sliderInputClassNames)}>
+        <SliderWithTooltip
+          min={min}
+          max={max}
+          step={step}
+          defaultValue={value}
+          value={sliderValue}
+          onChange={onSliderChange}
+          vertical={!isHorizontal}
+          reverse={false}
+          ariaLabelForHandle={ariaLabelForHandle}
+          marks={marks}
+          included={included}
+        />
+        {/* Uses text input so that the number spinners are not shown */}
+        <NumberInput value={sliderValue} onChange={onSliderInputChange} max={settings?.max} min={settings?.min} />
+      </label>
+    </div>
   );
 };
