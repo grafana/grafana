e2de39a6117c5a65279c9a836277a2fcbf6deabc
Ashley Harrison <ashley.harrison@grafana.com>
2022-06-16 10:50:24 +0100

    more test experimentation

--- public/app/features/search/page/components/FolderSection.test.tsx
+++ public/app/features/search/page/components/FolderSection.test.tsx
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-import { getGrafanaSearcher } from '../../service';
+import { DataFrameView } from '@grafana/data';
+
+import { getGrafanaSearcher, QueryResponse, SearchQuery } from '../../service';
 
 import { FolderSection } from './FolderSection';
 
@@ -11,14 +13,24 @@ import { FolderSection } from './FolderSection';
 // }))
 
 describe('FolderSection', () => {
+  let searchSpy: jest.SpyInstance<Promise<QueryResponse>, [query: SearchQuery]>;
+
   beforeEach(() => {
-    jest.spyOn(getGrafanaSearcher(), 'search').mockImplementation(() => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([]);
-        }, 10000);
-      });
-    });
+    searchSpy = jest.spyOn(getGrafanaSearcher(), 'search').mockResolvedValue({
+      isItemLoaded: jest.fn(),
+      loadMoreItems: jest.fn(),
+      totalRows: 0,
+      view: new DataFrameView({
+        fields: [],
+        length: 0,
+      }),
+    }); // .mockImplementation(() => {
+    //   return new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //       resolve([]);
+    //     }, 10000);
+    //   });
+    // });
   });
 
   const mockOnTagSelected = jest.fn();
@@ -34,21 +46,26 @@ describe('FolderSection', () => {
     expect(await screen.findByText(mockSection.title)).toBeInTheDocument();
   });
 
+  // TODO add children to wait for
+  it.skip('does not render the section header when renderStandaloneBody is set', async () => {
+    render(<FolderSection renderStandaloneBody section={mockSection} onTagSelected={mockOnTagSelected} />);
+    console.log(screen.debug());
+    expect(screen.queryByText(mockSection.title)).not.toBeInTheDocument();
+  });
+
   it('clicking the section header retrieves the folder contents and shows a spinner', async () => {
+    // Promise that never resolves
     render(<FolderSection section={mockSection} onTagSelected={mockOnTagSelected} />);
 
     await userEvent.click(await screen.findByRole('button', { name: mockSection.title }));
     expect(getGrafanaSearcher().search).toHaveBeenCalled();
-    expect(await screen.findByTestId('loading-spinner')).toBeInTheDocument();
   });
 
-  it('renders a loading spinner whilst the data is being retrieved', async () => {
+  it('shows a message if there are no results', async () => {
     render(<FolderSection section={mockSection} onTagSelected={mockOnTagSelected} />);
-    console.log(screen.debug());
-    const spinner = await screen.findByTestId('loading-spinner');
-    console.log(screen.debug());
-    expect(screen.getByText('title')).toBeInTheDocument();
-    expect(spinner).toBeInTheDocument();
-    console.log(screen.debug());
+
+    await userEvent.click(await screen.findByRole('button', { name: mockSection.title }));
+    expect(getGrafanaSearcher().search).toHaveBeenCalled();
+    expect(await screen.findByText('No results found')).toBeInTheDocument();
   });
 });
