072fb05d85b6708346e2404851b2cc8d133c3985
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-06-15 17:38:49 -0700

    add test

--- public/app/plugins/datasource/cloud-monitoring/components/Experimental/GroupBy.tsx
+++ public/app/plugins/datasource/cloud-monitoring/components/Experimental/GroupBy.tsx
@@ -38,7 +38,6 @@ export const GroupBy: FunctionComponent<Props> = ({
         <EditorField
           label="Group by"
           tooltip="You can reduce the amount of data returned for a metric by combining different time series. To combine multiple time series, you can specify a grouping and a function. Grouping is done on the basis of labels. The grouping function is used to combine the time series in the group into a single time series."
-          htmlFor={`${refId}-group-by`}
         >
           <MultiSelect
             inputId={`${refId}-group-by`}
