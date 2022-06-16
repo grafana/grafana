8f9bab5a6b1b39db2c0b2806d7009558b347a086
Karl Persson <kalle.persson@grafana.com>
2022-06-16 16:56:28 +0200

    cache token in memory and strip it from url

--- public/app/core/utils/urlToken.ts
+++ public/app/core/utils/urlToken.ts
@@ -1,15 +1,14 @@
-import store from 'app/core/store';
+let cachedToken = '';
 
 export const loadUrlToken = (): string | null => {
-  if (store.get('auth_token')) {
-    return store.get('auth_token');
+  if (cachedToken !== '') {
+    return cachedToken;
   }
 
   const params = new URLSearchParams(window.location.search);
   const token = params.get('auth_token');
   if (token !== null && token !== '') {
-    // store token in local storage
-    store.set('auth_token', token);
+    cachedToken = token;
     // strip auth_token from url
     params.delete('auth_token');
     window.history.replaceState(null, '', `${window.origin}/${params.toString()}`);
