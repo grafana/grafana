a05af3fc411043d083164258686e658068918d49
Torkel Ödegaard <torkel@grafana.com>
2022-06-17 15:00:09 +0200

    Fixing test

--- public/app/features/scenes/models/Scene.test.tsx
+++ public/app/features/scenes/models/Scene.test.tsx
@@ -1,5 +1,15 @@
-// import { Scene } from './Scene';
-// import { SceneFlexLayout } from './SceneFlexLayout';
-// import { VizPanel } from './VizPanel';
+import { Scene } from './Scene';
+import { SceneFlexLayout } from './SceneFlexLayout';
 
-describe('Scene', () => {});
+describe('Scene', () => {
+  it('Simple scene', () => {
+    const scene = new Scene({
+      title: 'Hello',
+      layout: new SceneFlexLayout({
+        children: [],
+      }),
+    });
+
+    expect(scene.state.title).toBe('Hello');
+  });
+});
