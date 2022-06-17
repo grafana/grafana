e3610c5730a0d97bba27b114120cb221dde3b0d8
Ryan McKinley <ryantxu@gmail.com>
2022-06-17 13:49:30 -0700

    flexbox FTW

--- public/app/core/components/OptionsUI/units.tsx
+++ public/app/core/components/OptionsUI/units.tsx
@@ -1,18 +1,35 @@
+import { css } from '@emotion/css';
 import React from 'react';
 
-import { FieldConfigEditorProps, UnitFieldConfigSettings } from '@grafana/data';
-import { IconButton, UnitPicker } from '@grafana/ui';
+import { FieldConfigEditorProps, GrafanaTheme2, UnitFieldConfigSettings } from '@grafana/data';
+import { IconButton, UnitPicker, useStyles2 } from '@grafana/ui';
 
 type Props = FieldConfigEditorProps<string, UnitFieldConfigSettings>;
 
 export function UnitValueEditor({ value, onChange, item }: Props) {
+  const styles = useStyles2(getStyles);
   if (item?.settings?.isClearable && value != null) {
     return (
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <UnitPicker value={value} onChange={onChange} />
-        <IconButton name="times" onClick={() => onChange(undefined)} style={{ marginLeft: '8px' }} />
+      <div className={styles.wrapper}>
+        <span className={styles.first}>
+          <UnitPicker value={value} onChange={onChange} />
+        </span>
+        <IconButton name="times" onClick={() => onChange(undefined)} />
       </div>
     );
   }
   return <UnitPicker value={value} onChange={onChange} />;
 }
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  wrapper: css`
+    width: 100%;
+    display: flex;
+    flex-direction: rows;
+    align-items: center;
+  `,
+  first: css`
+    margin-right: 8px;
+    flex-grow: 2;
+  `,
+});
