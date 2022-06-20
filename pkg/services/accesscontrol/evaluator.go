002220f22ef97277d7c291ba4a1f80921cab8a04
gamab <gabi.mabs@gmail.com>
2022-06-20 13:05:03 +0200

    Adding unmarshalling for evaluators

--- pkg/services/accesscontrol/evaluator.go
+++ pkg/services/accesscontrol/evaluator.go
@@ -2,6 +2,7 @@ package accesscontrol
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -201,4 +202,115 @@ func (a anyEvaluator) GoString() string {
 	}
 
 	return fmt.Sprintf("any(%s)", strings.Join(permissions, " "))
+}
+
+type EvaluatorDTO struct {
+	Ev Evaluator
+}
+
+// convertFromRaw has a collection of methods for converting raw json unmarshalling to our Evaluator structs.
+// This is only used internally to unmarshal json data to an Evaluator
+type convertFromRaw struct{}
+
+func fromRaw() *convertFromRaw {
+	return &convertFromRaw{}
+}
+
+func (f *convertFromRaw) EvaluatorList(raw interface{}) ([]Evaluator, error) {
+	var evs []Evaluator
+
+	// First check if it's a list of elements
+	itfList, ok := raw.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("expected a list of evaluators from '%v'", itfList)
+	}
+	for _, itf := range itfList {
+		// Now check each element could be the json representation of an evaluator
+		evRaw, ok := itf.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("expected a key value map from '%v'", evRaw)
+		}
+		ev, err := f.Evaluator(evRaw)
+		if err != nil {
+			return nil, err
+		}
+		if ev == nil {
+			continue
+		}
+		evs = append(evs, ev)
+	}
+	return evs, nil
+}
+
+func (f *convertFromRaw) PermissionEvaluator(raw map[string]interface{}) (Evaluator, error) {
+	if _, ok := raw["action"]; !ok {
+		return nil, fmt.Errorf("perm evaluator should have an action")
+	}
+	action, ok := raw["action"].(string)
+	if !ok {
+		return nil, fmt.Errorf("invalid action found '%v'", raw["action"])
+	}
+	var scopes []string
+	if _, ok := raw["scopes"]; ok {
+		scopesItf, ok := raw["scopes"].([]interface{})
+		if !ok {
+			return nil, fmt.Errorf("invalid scopes found '%v'", raw["scopes"])
+		}
+		for _, scopeItf := range scopesItf {
+			scope, ok := scopeItf.(string)
+			if !ok {
+				return nil, fmt.Errorf("invalid scope found '%v'", scopeItf)
+			}
+			scopes = append(scopes, scope)
+		}
+	}
+	return EvalPermission(action, scopes...), nil
+}
+
+func (f *convertFromRaw) Evaluator(raw map[string]interface{}) (Evaluator, error) {
+	if len(raw) == 0 {
+		return nil, nil
+	}
+
+	// Case Permission
+	if _, ok := raw["action"]; ok {
+		return f.PermissionEvaluator(raw)
+	}
+	// Case Any
+	anyItf, ok := raw["any"]
+	if ok {
+		anyOf, errParse := f.EvaluatorList(anyItf)
+		if errParse != nil {
+			return nil, errParse
+		}
+		return EvalAny(anyOf...), nil
+	}
+	// Case All
+	allItf, ok := raw["all"]
+	if ok {
+		allOf, errParse := f.EvaluatorList(allItf)
+		if errParse != nil {
+			return nil, errParse
+		}
+		return EvalAll(allOf...), nil
+	}
+
+	// Default
+	return nil, fmt.Errorf("expected 'any', 'all' or 'perm' evaluator")
+}
+
+func (ev *EvaluatorDTO) UnmarshalJSON(data []byte) error {
+	raw := map[string]interface{}{}
+	err := json.Unmarshal(data, &raw)
+	if err != nil {
+		return fmt.Errorf("unmarshalling expected 'any', 'all' or 'perm'")
+	}
+
+	parsed, errParse := fromRaw().Evaluator(raw)
+	if errParse != nil {
+		return fmt.Errorf("error parsing evaluator: %w", errParse)
+	}
+
+	ev.Ev = parsed
+	return nil
 }
--- pkg/services/accesscontrol/evaluator_test.go
+++ pkg/services/accesscontrol/evaluator_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type evaluateTestCase struct {
@@ -407,6 +408,136 @@ func TestEval(t *testing.T) {
 		t.Run(test.desc, func(t *testing.T) {
 			ok := test.evaluator.Evaluate(test.permissions)
 			assert.Equal(t, test.expected, ok)
+		})
+	}
+}
+
+func TestEvaluatorDTO_UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    []byte
+		want    EvaluatorDTO
+		wantErr bool
+	}{
+		{
+			name:    "nil",
+			data:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "empty",
+			data:    []byte{},
+			wantErr: true,
+		},
+		{
+			name: "perm eval",
+			data: []byte(`{"action": "users:read", "scopes": ["users:*", "org.users:*"]}`),
+			want: EvaluatorDTO{
+				Ev: permissionEvaluator{Action: "users:read", Scopes: []string{"users:*", "org.users:*"}},
+			},
+			wantErr: false,
+		},
+		{
+			name:    "perm eval without scopes",
+			data:    []byte(`{"action": "users:read"}`),
+			want:    EvaluatorDTO{Ev: permissionEvaluator{Action: "users:read"}},
+			wantErr: false,
+		},
+		{
+			name:    "no action in perm eval",
+			data:    []byte(`{"act": "users:read", "scopes": ["users:*", "org.users:*"]}`),
+			wantErr: true,
+		},
+		{
+			name:    "action is not a string in perm eval",
+			data:    []byte(`{"action": ["users:read"], "scopes": ["users:*", "org.users:*"]}`),
+			wantErr: true,
+		},
+		{
+			name:    "scopes is not a list in perm eval",
+			data:    []byte(`{"action": "users:read", "scopes": "users:*"}`),
+			wantErr: true,
+		},
+		{
+			name:    "scope is not a string in perm eval",
+			data:    []byte(`{"action": "users:read", "scopes": ["users:*", ["org.users:*"]]}`),
+			wantErr: true,
+		},
+		{
+			name: "any eval",
+			data: []byte(`{"any": [
+				{"action": "users:read", "scopes": ["users:*", "org.users:*"]},
+				{"action": "teams:read", "scopes": ["teams:*"]}
+			]}`),
+			want: EvaluatorDTO{
+				Ev: anyEvaluator{anyOf: []Evaluator{
+					permissionEvaluator{Action: "users:read", Scopes: []string{"users:*", "org.users:*"}},
+					permissionEvaluator{Action: "teams:read", Scopes: []string{"teams:*"}},
+				}},
+			},
+			wantErr: false,
+		},
+		{
+			name:    "no list in any eval",
+			data:    []byte(`{"any": {"action": "users:read", "scopes": ["users:*", "org.users:*"]}}`),
+			wantErr: true,
+		},
+		{
+			name: "incorrect type in any eval",
+			data: []byte(`{"any": [
+				{"action": "users:read", "scopes": ["users:*", "org.users:*"]},
+				"action"
+			]}`),
+			wantErr: true,
+		},
+		{
+			name: "all eval",
+			data: []byte(`{"all": [
+				{"action": "users:read", "scopes": ["users:*", "org.users:*"]},
+				{"action": "teams:read", "scopes": ["teams:*"]}
+			]}`),
+			want: EvaluatorDTO{
+				Ev: allEvaluator{allOf: []Evaluator{
+					permissionEvaluator{Action: "users:read", Scopes: []string{"users:*", "org.users:*"}},
+					permissionEvaluator{Action: "teams:read", Scopes: []string{"teams:*"}},
+				}},
+			},
+			wantErr: false,
+		},
+		{
+			name: "complex eval",
+			data: []byte(`{"any": [
+				{"all": [
+					{"action": "users:read", "scopes": ["users:id:1", "org.users:id:1"]},
+					{"action": "teams:read", "scopes": ["teams:id:2"]}
+				]},
+				{"action": "users:read", "scopes": ["users:*", "org.users:*"]},
+				{"action": "teams:read", "scopes": ["teams:*"]}
+			]}`),
+			want: EvaluatorDTO{
+				Ev: anyEvaluator{anyOf: []Evaluator{
+					allEvaluator{allOf: []Evaluator{
+						permissionEvaluator{Action: "users:read", Scopes: []string{"users:id:1", "org.users:id:1"}},
+						permissionEvaluator{Action: "teams:read", Scopes: []string{"teams:id:2"}},
+					}},
+					permissionEvaluator{Action: "users:read", Scopes: []string{"users:*", "org.users:*"}},
+					permissionEvaluator{Action: "teams:read", Scopes: []string{"teams:*"}},
+				}},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ev := &EvaluatorDTO{}
+			err := ev.UnmarshalJSON(tt.data)
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+
+			require.EqualValues(t, tt.want, *ev)
 		})
 	}
 }
