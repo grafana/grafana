c91cab58cdb7e20c8af66a1af36c948d3b6363ce
Guilherme Caulada <guilherme.caulada@grafana.com>
2022-06-20 15:04:35 -0300

    Try using transaction to fix db lock during tests

--- pkg/services/secrets/kvstore/migration/datasources.go
+++ pkg/services/secrets/kvstore/migration/datasources.go
@@ -9,6 +9,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/datasources"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/secrets/kvstore"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
@@ -17,6 +18,7 @@ const (
 )
 
 type DataSourceSecretMigrationService struct {
+	sqlStore           *sqlstore.SQLStore
 	dataSourcesService datasources.DataSourceService
 	secretsStore       kvstore.SecretsKVStore
 	features           featuremgmt.FeatureToggles
@@ -26,8 +28,10 @@ type DataSourceSecretMigrationService struct {
 func ProvideDataSourceMigrationService(
 	cfg *setting.Cfg, dataSourcesService datasources.DataSourceService,
 	secretsStore kvstore.SecretsKVStore, features featuremgmt.FeatureToggles,
+	sqlStore *sqlstore.SQLStore,
 ) kvstore.SecretMigrationService {
 	return &DataSourceSecretMigrationService{
+		sqlStore:           sqlStore,
 		dataSourcesService: dataSourcesService,
 		secretsStore:       secretsStore,
 		features:           features,
@@ -36,45 +40,47 @@ func ProvideDataSourceMigrationService(
 }
 
 func (s *DataSourceSecretMigrationService) Run(ctx context.Context) error {
-	query := &models.GetDataSourcesQuery{}
-	err := s.dataSourcesService.GetDataSources(ctx, query)
-	if err != nil {
-		return err
-	}
-
-	s.log.Debug("starting data source secret migration")
-	for _, ds := range query.Result {
-		hasMigration, _ := ds.JsonData.Get("secretMigrationComplete").Bool()
-		if !hasMigration {
-			secureJsonData, err := s.dataSourcesService.DecryptLegacySecrets(ctx, ds)
-			if err != nil {
-				return err
-			}
-
-			jsonData, err := json.Marshal(secureJsonData)
-			if err != nil {
-				return err
-			}
-
-			err = s.secretsStore.Set(ctx, ds.OrgId, ds.Name, dataSourceSecretType, string(jsonData))
-			if err != nil {
-				return err
-			}
-
-			ds.JsonData.Set("secretMigrationComplete", true)
-			err = s.dataSourcesService.UpdateDataSource(ctx, &models.UpdateDataSourceCommand{Id: ds.Id, OrgId: ds.OrgId, Uid: ds.Uid, JsonData: ds.JsonData})
-			if err != nil {
-				return err
-			}
+	return s.sqlStore.InTransaction(ctx, func(ctx context.Context) error {
+		query := &models.GetDataSourcesQuery{}
+		err := s.dataSourcesService.GetDataSources(ctx, query)
+		if err != nil {
+			return err
 		}
 
-		if s.features.IsEnabled(featuremgmt.FlagDisableSecretsCompatibility) && len(ds.SecureJsonData) > 0 {
-			err := s.dataSourcesService.DeleteDataSourceSecrets(ctx, &models.DeleteDataSourceSecretsCommand{UID: ds.Uid, OrgID: ds.OrgId, ID: ds.Id})
-			if err != nil {
-				return err
+		s.log.Debug("starting data source secret migration")
+		for _, ds := range query.Result {
+			hasMigration, _ := ds.JsonData.Get("secretMigrationComplete").Bool()
+			if !hasMigration {
+				secureJsonData, err := s.dataSourcesService.DecryptLegacySecrets(ctx, ds)
+				if err != nil {
+					return err
+				}
+
+				jsonData, err := json.Marshal(secureJsonData)
+				if err != nil {
+					return err
+				}
+
+				err = s.secretsStore.Set(ctx, ds.OrgId, ds.Name, dataSourceSecretType, string(jsonData))
+				if err != nil {
+					return err
+				}
+
+				ds.JsonData.Set("secretMigrationComplete", true)
+				err = s.dataSourcesService.UpdateDataSource(ctx, &models.UpdateDataSourceCommand{Id: ds.Id, OrgId: ds.OrgId, Uid: ds.Uid, JsonData: ds.JsonData})
+				if err != nil {
+					return err
+				}
+			}
+
+			if s.features.IsEnabled(featuremgmt.FlagDisableSecretsCompatibility) && len(ds.SecureJsonData) > 0 {
+				err := s.dataSourcesService.DeleteDataSourceSecrets(ctx, &models.DeleteDataSourceSecretsCommand{UID: ds.Uid, OrgID: ds.OrgId, ID: ds.Id})
+				if err != nil {
+					return err
+				}
 			}
 		}
-	}
-	s.log.Debug("data source secret migration complete")
-	return nil
+		s.log.Debug("data source secret migration complete")
+		return nil
+	})
 }
