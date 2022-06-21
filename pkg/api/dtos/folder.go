fdb9f0c19a301a2d4995130decfd0b4509d727a3
Karl Persson <kalle.persson@grafana.com>
2022-06-21 12:02:31 +0200

    RBAC: Add access control metadata to Folder dto

--- pkg/api/dtos/folder.go
+++ pkg/api/dtos/folder.go
@@ -1,22 +1,27 @@
 package dtos
 
-import "time"
+import (
+	"time"
+
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+)
 
 type Folder struct {
-	Id        int64     `json:"id"`
-	Uid       string    `json:"uid"`
-	Title     string    `json:"title"`
-	Url       string    `json:"url"`
-	HasAcl    bool      `json:"hasAcl"`
-	CanSave   bool      `json:"canSave"`
-	CanEdit   bool      `json:"canEdit"`
-	CanAdmin  bool      `json:"canAdmin"`
-	CanDelete bool      `json:"canDelete"`
-	CreatedBy string    `json:"createdBy"`
-	Created   time.Time `json:"created"`
-	UpdatedBy string    `json:"updatedBy"`
-	Updated   time.Time `json:"updated"`
-	Version   int       `json:"version"`
+	Id            int64                  `json:"id"`
+	Uid           string                 `json:"uid"`
+	Title         string                 `json:"title"`
+	Url           string                 `json:"url"`
+	HasAcl        bool                   `json:"hasAcl"`
+	CanSave       bool                   `json:"canSave"`
+	CanEdit       bool                   `json:"canEdit"`
+	CanAdmin      bool                   `json:"canAdmin"`
+	CanDelete     bool                   `json:"canDelete"`
+	CreatedBy     string                 `json:"createdBy"`
+	Created       time.Time              `json:"created"`
+	UpdatedBy     string                 `json:"updatedBy"`
+	Updated       time.Time              `json:"updated"`
+	Version       int                    `json:"version"`
+	AccessControl accesscontrol.Metadata `json:"accessControl,omitempty"`
 }
 
 type FolderSearchHit struct {
