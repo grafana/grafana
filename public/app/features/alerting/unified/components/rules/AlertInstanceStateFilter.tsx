47c252dddaacfb6a6cc0de5bd14448e57fa3d3c6
Konrad Lalik <konrad.lalik@grafana.com>
2022-06-21 14:05:24 +0200

    Improve counter tags

--- public/app/features/alerting/unified/components/rules/AlertInstanceStateFilter.tsx
+++ public/app/features/alerting/unified/components/rules/AlertInstanceStateFilter.tsx
@@ -1,7 +1,9 @@
+import { css } from '@emotion/css';
 import { capitalize } from 'lodash';
 import React from 'react';
 
-import { Label, RadioButtonGroup, Tag } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data/src';
+import { Label, RadioButtonGroup, Tag, useStyles2 } from '@grafana/ui';
 import { GrafanaAlertState, PromAlertingRuleState } from 'app/types/unified-alerting-dto';
 
 export type InstanceStateFilter = GrafanaAlertState | PromAlertingRuleState.Pending | PromAlertingRuleState.Firing;
@@ -21,25 +23,27 @@ export const AlertInstanceStateFilter = ({
   filterType,
   itemPerStateStats,
 }: Props) => {
-  const grafanaOptions = Object.values(GrafanaAlertState).map((value) => ({
-    label: value,
-    value,
-    component: () => {
-      return itemPerStateStats && itemPerStateStats[value] ? (
-        <Tag name={itemPerStateStats[value].toFixed(0)} colorIndex={9} />
+  const styles = useStyles2(getStyles);
+
+  const getOptionComponent = (state: InstanceStateFilter) => {
+    return function InstanceStateCounter() {
+      return itemPerStateStats && itemPerStateStats[state] ? (
+        <Tag name={itemPerStateStats[state].toFixed(0)} colorIndex={9} className={styles.tag} />
       ) : null;
-    },
+    };
+  };
+
+  const grafanaOptions = Object.values(GrafanaAlertState).map((state) => ({
+    label: state,
+    value: state,
+    component: getOptionComponent(state),
   }));
 
   const promOptionValues = [PromAlertingRuleState.Firing, PromAlertingRuleState.Pending] as const;
   const promOptions = promOptionValues.map((state) => ({
     label: capitalize(state),
     value: state,
-    component: () => {
-      return itemPerStateStats && itemPerStateStats[state] ? (
-        <Tag name={itemPerStateStats[state].toFixed(0)} colorIndex={9} />
-      ) : null;
-    },
+    component: getOptionComponent(state),
   }));
 
   const stateOptions = filterType === 'grafana' ? grafanaOptions : promOptions;
@@ -60,3 +64,15 @@ export const AlertInstanceStateFilter = ({
     </div>
   );
 };
+
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    tag: css`
+      font-size: 11px;
+      font-weight: normal;
+      padding: ${theme.spacing(0.25, 0.5)};
+      vertical-align: middle;
+      margin-left: ${theme.spacing(0.5)};
+    `,
+  };
+}
