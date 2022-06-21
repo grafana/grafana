94a19ce6e0a1d7e3d9caa71e4b4786d98af7e157
Fabio Silva <ffjs1993@gmail.com>
2022-06-21 11:16:43 +0100

    PMM-5868 add pmm branding to v7

--- public/app/core/components/Login/LoginLayout.tsx
+++ public/app/core/components/Login/LoginLayout.tsx
@@ -5,7 +5,7 @@ import { GrafanaTheme2 } from '@grafana/data';
 import { useStyles2, styleMixins } from '@grafana/ui';
 
 import { Branding } from '../Branding/Branding';
-import { Footer } from '../Footer/Footer';
+import LoginFooter from '../Footer/LoginFooter';
 
 interface InnerBoxProps {
   enterAnimation?: boolean;
@@ -17,7 +17,6 @@ export const InnerBox: FC<InnerBoxProps> = ({ children, enterAnimation = true })
 
 export const LoginLayout: FC = ({ children }) => {
   const loginStyles = useStyles2(getLoginStyles);
-  const subTitle = Branding.GetLoginSubTitle();
   const [startAnim, setStartAnim] = useState(false);
 
   useEffect(() => setStartAnim(true), []);
@@ -29,12 +28,11 @@ export const LoginLayout: FC = ({ children }) => {
           <Branding.LoginLogo className={loginStyles.loginLogo} />
           <div className={loginStyles.titleWrapper}>
             <h1 className={loginStyles.mainTitle}>{Branding.LoginTitle}</h1>
-            {subTitle && <h3 className={loginStyles.subTitle}>{Branding.GetLoginSubTitle()}</h3>}
           </div>
         </div>
         <div className={loginStyles.loginOuterBox}>{children}</div>
       </div>
-      <Footer />
+      <LoginFooter />
     </Branding.LoginBackground>
   );
 };
