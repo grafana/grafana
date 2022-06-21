f49bc689685275596728f0bc353e322d6b4fc5d3
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-06-21 16:02:22 -0700

    add additonal tests

--- public/app/plugins/datasource/cloud-monitoring/components/Experimental/Metrics.test.tsx
+++ public/app/plugins/datasource/cloud-monitoring/components/Experimental/Metrics.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import React from 'react';
 import { openMenu, select } from 'react-select-event';
 
@@ -77,5 +77,112 @@ describe('Metrics', () => {
     await openMenu(metricName);
     await select(metricName, 'metricName', { container: document.body });
     expect(onChange).toBeCalledWith(expect.objectContaining({ type: 'type' }));
+  });
+
+  it('should render available metric options according to the selected service', async () => {
+    const onChange = jest.fn();
+    const datasource = createMockDatasource({
+      getMetricTypes: jest.fn().mockResolvedValue([
+        createMockMetricDescriptor({
+          service: 'service_a',
+          serviceShortName: 'srv_a',
+          type: 'metric1',
+          description: 'description_metric1',
+          displayName: 'displayName_metric1',
+        }),
+        createMockMetricDescriptor({
+          service: 'service_b',
+          serviceShortName: 'srv_b',
+          type: 'metric2',
+          description: 'description_metric2',
+          displayName: 'displayName_metric2',
+        }),
+        createMockMetricDescriptor({
+          service: 'service_b',
+          serviceShortName: 'srv_b',
+          type: 'metric3',
+          description: 'description_metric3',
+          displayName: 'displayName_metric3',
+        }),
+      ]),
+    });
+
+    render(
+      <Metrics
+        refId="refId"
+        metricType="metric1"
+        projectName="projectName"
+        templateVariableOptions={[]}
+        datasource={datasource}
+        onChange={onChange}
+      >
+        {() => <div />}
+      </Metrics>
+    );
+
+    const metricName = await screen.findByLabelText('Metric name');
+    await openMenu(metricName);
+
+    const metricNameOptions = screen.getByLabelText('Select options menu');
+    expect(within(metricNameOptions).getByText('description_metric1')).toBeInTheDocument();
+    expect(within(metricNameOptions).getByText('displayName_metric1')).toBeInTheDocument();
+    expect(within(metricNameOptions).queryByText('displayName_metric2')).not.toBeInTheDocument();
+    expect(within(metricNameOptions).queryByText('description_metric2')).not.toBeInTheDocument();
+    expect(within(metricNameOptions).queryByText('displayName_metric3')).not.toBeInTheDocument();
+    expect(within(metricNameOptions).queryByText('description_metric3')).not.toBeInTheDocument();
+
+    await select(screen.getByLabelText('Service'), 'Srv B', { container: document.body });
+    expect(within(metricNameOptions).queryByText('displayName_metric1')).not.toBeInTheDocument();
+    expect(within(metricNameOptions).queryByText('description_metric1')).not.toBeInTheDocument();
+    expect(within(metricNameOptions).getByText('displayName_metric2')).toBeInTheDocument();
+    expect(within(metricNameOptions).getByText('description_metric2')).toBeInTheDocument();
+    expect(within(metricNameOptions).getByText('displayName_metric3')).toBeInTheDocument();
+    expect(within(metricNameOptions).getByText('description_metric3')).toBeInTheDocument();
+  });
+
+  it('should have a distinct list of services', async () => {
+    const onChange = jest.fn();
+    const datasource = createMockDatasource({
+      getMetricTypes: jest.fn().mockResolvedValue([
+        createMockMetricDescriptor({
+          service: 'service_a',
+          serviceShortName: 'srv_a',
+          type: 'metric1',
+          description: 'description_metric1',
+          displayName: 'displayName_metric1',
+        }),
+        createMockMetricDescriptor({
+          service: 'service_b',
+          serviceShortName: 'srv_b',
+          type: 'metric2',
+          description: 'description_metric2',
+          displayName: 'displayName_metric2',
+        }),
+        createMockMetricDescriptor({
+          service: 'service_b',
+          serviceShortName: 'srv_b',
+          type: 'metric3',
+          description: 'description_metric3',
+          displayName: 'displayName_metric3',
+        }),
+      ]),
+    });
+
+    render(
+      <Metrics
+        refId="refId"
+        metricType="metric1"
+        projectName="projectName"
+        templateVariableOptions={[]}
+        datasource={datasource}
+        onChange={onChange}
+      >
+        {() => <div />}
+      </Metrics>
+    );
+
+    const service = await screen.findByLabelText('Service');
+    await openMenu(service);
+    expect(screen.getAllByLabelText('Select option').length).toEqual(2);
   });
 });
