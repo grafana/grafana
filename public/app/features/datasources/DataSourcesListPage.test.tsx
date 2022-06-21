17d1e73a903c22c560019fafd71efc5c12fe53d2
Alex <alexchelseaalex@gmail.com>
2022-06-21 01:35:36 +0100

    Convert DataSourceListPage test to RTL

--- public/app/features/datasources/DataSourcesListPage.test.tsx
+++ public/app/features/datasources/DataSourcesListPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { DataSourceSettings, NavModel, LayoutModes } from '@grafana/data';
