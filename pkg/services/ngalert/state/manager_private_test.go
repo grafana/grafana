661b320d4e07c6ff3e8becb6024a68c9edd5263c
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2022-06-21 22:04:22 -0400

    fixup

--- pkg/services/ngalert/state/manager_private_test.go
+++ pkg/services/ngalert/state/manager_private_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/benbjohnson/clock"
-	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/services/dashboards"
