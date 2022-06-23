214162e7b24a3751fc4d51a873002e27fa55b2fc
Sofia Papagiannaki <sofia@grafana.com>
2022-06-23 12:45:18 +0300

    Fix text log for unsupported types

---
+++ pkg/infra/log/text/text_logger.go
@@ -0,0 +1,40 @@
+package text
+
+import (
+	"encoding"
+	"fmt"
+	"io"
+	"reflect"
+
+	gokitlog "github.com/go-kit/log"
+)
+
+type textLogger struct {
+	w io.Writer
+}
+
+// NewTextLogger similar to gokitlog.NewLogfmtLogger
+// but converts unsupported types to string
+func NewTextLogger(w io.Writer) gokitlog.Logger {
+	return &textLogger{w}
+}
+
+func (l textLogger) Log(keyvals ...interface{}) error {
+	for i, val := range keyvals {
+		switch val.(type) {
+		case nil, string, []byte, encoding.TextMarshaler, error, fmt.Stringer:
+		default:
+			rvalue := reflect.ValueOf(val)
+			switch rvalue.Kind() {
+			case reflect.Array, reflect.Chan, reflect.Func, reflect.Map, reflect.Slice, reflect.Struct:
+				keyvals[i] = fmt.Sprintf("%+v", val)
+			default:
+			}
+		}
+	}
+	ll := gokitlog.NewLogfmtLogger(l.w)
+	if err := ll.Log(keyvals...); err != nil {
+		return err
+	}
+	return nil
+}
