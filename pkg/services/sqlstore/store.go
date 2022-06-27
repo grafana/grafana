9c6f1693ad042a87ede8df4f051e5a024c81b17d
Ida Furjesova <ida.furjesova@grafana.com>
2022-06-27 12:28:14 +0200

    Fix wire generation

--- pkg/services/sqlstore/store.go
+++ pkg/services/sqlstore/store.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
+	"github.com/grafana/grafana/pkg/services/user"
 )
 
 type Store interface {
@@ -25,7 +26,7 @@ type Store interface {
 	CreateLoginAttempt(ctx context.Context, cmd *models.CreateLoginAttemptCommand) error
 	GetUserLoginAttemptCount(ctx context.Context, query *models.GetUserLoginAttemptCountQuery) error
 	DeleteOldLoginAttempts(ctx context.Context, cmd *models.DeleteOldLoginAttemptsCommand) error
-	CreateUser(ctx context.Context, cmd models.CreateUserCommand) (*models.User, error)
+	CreateUser(ctx context.Context, cmd user.CreateUserCommand) (*user.User, error)
 	GetUserById(ctx context.Context, query *models.GetUserByIdQuery) error
 	GetUserByLogin(ctx context.Context, query *models.GetUserByLoginQuery) error
 	GetUserByEmail(ctx context.Context, query *models.GetUserByEmailQuery) error
