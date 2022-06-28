grafana/grafana

commit f4f31b40fd9003ae87896eb328795d40ec6b1a9c

HeatmapNG: consolidate frame types & fix color ranging (#51089)
* rename yZeroDisplay -> yMinDisplay
* remove heatmap-cells-sparse frame type
* parse x bucket size to millis
* take into account hideLE & hideGE filters to auto-range color scale
* extract cell value range scanning to heatmapData

public/app/features/transformers/calculateHeatmap/heatmap.ts
@@ -13,6 +13,8 @@ import {
   Field,
   getValueFormat,
   formattedValueToString,
+  durationToMilliseconds,
+  parseDuration,
 } from '@grafana/data';
 import { ScaleDistribution } from '@grafana/schema';
 
@@ -56,12 +58,32 @@ export interface HeatmapRowsCustomMeta {
   yOrdinalDisplay: string[];
   yOrdinalLabel?: string[];
   yMatchWithLabel?: string;
-  yZeroDisplay?: string;
+  yMinDisplay?: string;
 }
 
 /** simple utility to get heatmap metadata from a frame */
 export function readHeatmapRowsCustomMeta(frame?: DataFrame): HeatmapRowsCustomMeta {
   return (frame?.meta?.custom ?? {}) as HeatmapRowsCustomMeta;
+}
+
+export function isHeatmapCellsDense(frame: DataFrame) {
+  let foundY = false;
+
+  for (let field of frame.fields) {
+    // dense heatmap frames can only have one of these fields
+    switch (field.name) {
+      case 'y':
+      case 'yMin':
+      case 'yMax':
+        if (foundY) {
+          return false;
+        }
+
+        foundY = true;
+    }
+  }
+
+  return foundY;
 }
 
 export interface RowsHeatmapOptions {
@@ -129,7 +151,7 @@ export function rowsToCellsHeatmap(opts: RowsHeatmapOptions): DataFrame {
   if (custom.yMatchWithLabel) {
     custom.yOrdinalLabel = yFields.map((f) => f.labels?.[custom.yMatchWithLabel!] ?? '');
     if (custom.yMatchWithLabel === 'le') {
-      custom.yZeroDisplay = '0.0';
+      custom.yMinDisplay = '0.0';
     }
   }
 
@@ -137,8 +159,8 @@ export function rowsToCellsHeatmap(opts: RowsHeatmapOptions): DataFrame {
   // TODO: this leaves the internally prepended '0.0' without this formatting treatment
   if (opts.unit?.length || opts.decimals != null) {
     const fmt = getValueFormat(opts.unit ?? 'short');
-    if (custom.yZeroDisplay) {
-      custom.yZeroDisplay = formattedValueToString(fmt(0, opts.decimals));
+    if (custom.yMinDisplay) {
+      custom.yMinDisplay = formattedValueToString(fmt(0, opts.decimals));
     }
     custom.yOrdinalDisplay = custom.yOrdinalDisplay.map((name) => {
       let num = +name;
@@ -274,11 +296,12 @@ export function calculateHeatmapFromData(frames: DataFrame[], options: HeatmapCa
   const scaleDistribution = options.yBuckets?.scale ?? {
     type: ScaleDistribution.Linear,
   };
+
   const heat2d = heatmap(xs, ys, {
     xSorted: true,
     xTime: xField.type === FieldType.time,
     xMode: xBucketsCfg.mode,
-    xSize: xBucketsCfg.value ? +xBucketsCfg.value : undefined,
+    xSize: durationToMilliseconds(parseDuration(xBucketsCfg.value ?? '')),
     yMode: yBucketsCfg.mode,
     ySize: yBucketsCfg.value ? +yBucketsCfg.value : undefined,
     yLog: scaleDistribution?.type === ScaleDistribution.Log ? (scaleDistribution?.log as any) : undefined,

packages/grafana-data/src/types/dataFrameTypes.ts
@@ -26,11 +26,4 @@ export enum DataFrameType {
    * If the y value is actually ordinal, use `meta.custom` to specify the bucket lookup values
    */
   HeatmapCells = 'heatmap-cells',
-
-  /**
-   *  WIP sparse heatmap support
-   *
-   *  @private
-   */
-  HeatmapSparse = 'heatmap-cells-sparse',
 }

pkg/util/converter/prom.go
@@ -429,7 +429,7 @@ func readMatrixOrVectorWide(iter *jsoniter.Iterator, resultType string) *backend
 			histogram.yMin.Labels = valueField.Labels
 			frame := data.NewFrame(valueField.Name, histogram.time, histogram.yMin, histogram.yMax, histogram.count, histogram.yLayout)
 			frame.Meta = &data.FrameMeta{
-				Type: "heatmap-cells-sparse",
+				Type: "heatmap-cells",
 			}
 			if frame.Name == data.TimeSeriesValueFieldName {
 				frame.Name = "" // only set the name if useful
@@ -535,7 +535,7 @@ func readMatrixOrVectorMulti(iter *jsoniter.Iterator, resultType string) *backen
 			histogram.yMin.Labels = valueField.Labels
 			frame := data.NewFrame(valueField.Name, histogram.time, histogram.yMin, histogram.yMax, histogram.count, histogram.yLayout)
 			frame.Meta = &data.FrameMeta{
-				Type: "heatmap-cells-sparse",
+				Type: "heatmap-cells",
 			}
 			if frame.Name == data.TimeSeriesValueFieldName {
 				frame.Name = "" // only set the name if useful

pkg/util/converter/testdata/prom-matrix-histogram-no-labels-frame.jsonc
@@ -1,9 +1,9 @@
 //  🌟 This was machine generated.  Do not edit. 🌟
-//  
+//
 //  Frame[0] {
-//      "type": "heatmap-cells-sparse"
+//      "type": "heatmap-cells"
 //  }
-//  Name: 
+//  Name:
 //  Dimensions: 5 Fields by 932 Rows
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
 //  | Name: xMax                    | Name: yMin             | Name: yMax             | Name: count        | Name: yLayout |
@@ -21,15 +21,15 @@
 //  | 2022-04-14 19:08:20 +0000 UTC | 9.07293025972535e-06   | 9.894100606163098e-06  | 3.712286008031362  | 0             |
 //  | ...                           | ...                    | ...                    | ...                | ...           |
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
-//  
-//  
+//
+//
 //  🌟 This was machine generated.  Do not edit. 🌟
 {
   "frames": [
     {
       "schema": {
         "meta": {
-          "type": "heatmap-cells-sparse"
+          "type": "heatmap-cells"
         },
         "fields": [
           {

pkg/util/converter/testdata/prom-matrix-histogram-no-labels-wide-frame.jsonc
@@ -1,9 +1,9 @@
 //  🌟 This was machine generated.  Do not edit. 🌟
-//  
+//
 //  Frame[0] {
-//      "type": "heatmap-cells-sparse"
+//      "type": "heatmap-cells"
 //  }
-//  Name: 
+//  Name:
 //  Dimensions: 5 Fields by 932 Rows
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
 //  | Name: xMax                    | Name: yMin             | Name: yMax             | Name: count        | Name: yLayout |
@@ -21,15 +21,15 @@
 //  | 2022-04-14 19:08:20 +0000 UTC | 9.07293025972535e-06   | 9.894100606163098e-06  | 3.712286008031362  | 0             |
 //  | ...                           | ...                    | ...                    | ...                | ...           |
 //  +-------------------------------+------------------------+------------------------+--------------------+---------------+
-//  
-//  
+//
+//
 //  🌟 This was machine generated.  Do not edit. 🌟
 {
   "frames": [
     {
       "schema": {
         "meta": {
-          "type": "heatmap-cells-sparse"
+          "type": "heatmap-cells"
         },
         "fields": [
           {

pkg/util/converter/testdata/prom-vector-histogram-no-labels-frame.jsonc
@@ -1,9 +1,9 @@
 //  🌟 This was machine generated.  Do not edit. 🌟
-//  
+//
 //  Frame[0] {
-//      "type": "heatmap-cells-sparse"
+//      "type": "heatmap-cells"
 //  }
-//  Name: 
+//  Name:
 //  Dimensions: 5 Fields by 134 Rows
 //  +-----------------------------------+------------------------+------------------------+---------------------+---------------+
 //  | Name: xMax                        | Name: yMin             | Name: yMax             | Name: count         | Name: yLayout |
@@ -21,15 +21,15 @@
 //  | 2022-04-14 20:21:08.042 +0000 UTC | 9.07293025972535e-06   | 9.894100606163098e-06  | 3.392982456140351   | 0             |
 //  | ...                               | ...                    | ...                    | ...                 | ...           |
 //  +-----------------------------------+------------------------+------------------------+---------------------+---------------+
-//  
-//  
+//
+//
 //  🌟 This was machine generated.  Do not edit. 🌟
 {
   "frames": [
     {
       "schema": {
         "meta": {
-          "type": "heatmap-cells-sparse"
+          "type": "heatmap-cells"
         },
         "fields": [
           {

commit 0996db6c0969f5a5f721f76262db4d79b186bd87

fix x bucket count setting for calculate (regressed in #51089)

.betterer.results
@@ -7074,10 +7074,10 @@ exports[`better eslint`] = {
     "public/app/features/transformers/calculateHeatmap/editor/helper.ts:2898656130": [
       [10, 37, 3, "Unexpected any. Specify a different type.", "193409811"]
     ],
-    "public/app/features/transformers/calculateHeatmap/heatmap.ts:992367420": [
+    "public/app/features/transformers/calculateHeatmap/heatmap.ts:3354697941": [
       [65, 9, 52, "Do not use any type assertions.", "3480700880"],
-      [321, 63, 29, "Do not use any type assertions.", "255738422"],
-      [321, 89, 3, "Unexpected any. Specify a different type.", "193409811"]
+      [326, 63, 29, "Do not use any type assertions.", "255738422"],
+      [326, 89, 3, "Unexpected any. Specify a different type.", "193409811"]
     ],
     "public/app/features/transformers/configFromQuery/ConfigFromQueryTransformerEditor.test.tsx:1060905279": [
       [44, 29, 21, "Do not use any type assertions.", "1548027068"]

public/app/features/transformers/calculateHeatmap/heatmap.ts
@@ -316,7 +316,12 @@ export function calculateHeatmapFromData(frames: DataFrame[], options: HeatmapCa
     xSorted: true,
     xTime: xField.type === FieldType.time,
     xMode: xBucketsCfg.mode,
-    xSize: durationToMilliseconds(parseDuration(xBucketsCfg.value ?? '')),
+    xSize:
+      xBucketsCfg.mode === HeatmapCalculationMode.Size
+        ? durationToMilliseconds(parseDuration(xBucketsCfg.value ?? ''))
+        : xBucketsCfg.value
+        ? +xBucketsCfg.value
+        : undefined,
     yMode: yBucketsCfg.mode,
     ySize: yBucketsCfg.value ? +yBucketsCfg.value : undefined,
     yLog: scaleDistribution?.type === ScaleDistribution.Log ? (scaleDistribution?.log as any) : undefined,