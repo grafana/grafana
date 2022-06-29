bff1d6bf0b2fe639273e123a05d130430c2778ac
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2022-06-29 17:37:25 +0200

    validate that the mute time intervals exist when updating routing tree

--- pkg/services/ngalert/provisioning/notification_policies.go
+++ pkg/services/ngalert/provisioning/notification_policies.go
@@ -76,6 +76,15 @@ func (nps *NotificationPolicyService) UpdatePolicyTree(ctx context.Context, orgI
 		return fmt.Errorf("%w: %s", ErrValidation, err.Error())
 	}
 
+	muteTimes := map[string]struct{}{}
+	for _, mt := range revision.cfg.AlertmanagerConfig.MuteTimeIntervals {
+		muteTimes[mt.Name] = struct{}{}
+	}
+	err = tree.ValidateMuteTimes(muteTimes)
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrValidation, err.Error())
+	}
+	
 	revision.cfg.AlertmanagerConfig.Config.Route = &tree
 
 	serialized, err := serializeAlertmanagerConfig(*revision.cfg)
