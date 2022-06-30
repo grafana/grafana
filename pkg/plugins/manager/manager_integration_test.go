5d3dde7cfe79fb3a3c8c1a9f869ddb9a707bc038
Will Browne <will.browne@grafana.com>
2022-06-30 14:03:35 +0100

    revert utils

--- pkg/plugins/manager/manager_integration_test.go
+++ pkg/plugins/manager/manager_integration_test.go
@@ -1,30 +1,100 @@
-package manager_test
+package manager
 
 import (
 	"context"
+	"net/http"
 	"path/filepath"
 	"strings"
 	"testing"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
+	"github.com/grafana/grafana/pkg/infra/tracing"
+	"github.com/grafana/grafana/pkg/plugins"
+	"github.com/grafana/grafana/pkg/plugins/backendplugin/coreplugin"
+	"github.com/grafana/grafana/pkg/plugins/backendplugin/provider"
+	"github.com/grafana/grafana/pkg/plugins/manager/loader"
+	"github.com/grafana/grafana/pkg/plugins/manager/registry"
+	"github.com/grafana/grafana/pkg/plugins/manager/signature"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
+	"github.com/grafana/grafana/pkg/services/licensing"
+	"github.com/grafana/grafana/pkg/services/searchV2"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/grafana/grafana/pkg/setting"
+	"github.com/grafana/grafana/pkg/tsdb/azuremonitor"
+	"github.com/grafana/grafana/pkg/tsdb/cloudmonitoring"
+	"github.com/grafana/grafana/pkg/tsdb/cloudwatch"
+	"github.com/grafana/grafana/pkg/tsdb/elasticsearch"
+	"github.com/grafana/grafana/pkg/tsdb/grafanads"
+	"github.com/grafana/grafana/pkg/tsdb/graphite"
+	"github.com/grafana/grafana/pkg/tsdb/influxdb"
+	"github.com/grafana/grafana/pkg/tsdb/loki"
+	"github.com/grafana/grafana/pkg/tsdb/mssql"
+	"github.com/grafana/grafana/pkg/tsdb/mysql"
+	"github.com/grafana/grafana/pkg/tsdb/opentsdb"
+	"github.com/grafana/grafana/pkg/tsdb/postgres"
+	"github.com/grafana/grafana/pkg/tsdb/prometheus"
+	"github.com/grafana/grafana/pkg/tsdb/tempo"
+	"github.com/grafana/grafana/pkg/tsdb/testdatasource"
+	"go.opentelemetry.io/otel/trace"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/grafana/grafana/pkg/plugins"
-	"github.com/grafana/grafana/pkg/plugins/manager"
-	"github.com/grafana/grafana/pkg/plugins/managertest"
-	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"gopkg.in/ini.v1"
 )
 
 func TestPluginManager_int_init(t *testing.T) {
 	t.Helper()
 
-	grafanaRootPath, err := filepath.Abs("../../../")
+	staticRootPath, err := filepath.Abs("../../../public/")
 	require.NoError(t, err)
 
-	pm, err := managertest.ProvidePluginManager(t, managertest.PluginManagerOpts{
-		SQLStore:        sqlstore.InitTestDB(t),
-		GrafanaRootPath: grafanaRootPath,
-	})
+	bundledPluginsPath, err := filepath.Abs("../../../plugins-bundled/internal")
+	require.NoError(t, err)
+
+	features := featuremgmt.WithFeatures()
+	cfg := &setting.Cfg{
+		Raw:                    ini.Empty(),
+		Env:                    setting.Prod,
+		StaticRootPath:         staticRootPath,
+		BundledPluginsPath:     bundledPluginsPath,
+		IsFeatureToggleEnabled: features.IsEnabled,
+		PluginSettings: map[string]map[string]string{
+			"plugin.datasource-id": {
+				"path": "testdata/test-app",
+			},
+		},
+	}
+
+	tracer := &fakeTracer{}
+
+	license := &licensing.OSSLicensingService{
+		Cfg: cfg,
+	}
+
+	hcp := httpclient.NewProvider()
+	am := azuremonitor.ProvideService(cfg, hcp, tracer)
+	cw := cloudwatch.ProvideService(cfg, hcp, features)
+	cm := cloudmonitoring.ProvideService(hcp, tracer)
+	es := elasticsearch.ProvideService(hcp)
+	grap := graphite.ProvideService(hcp, tracer)
+	idb := influxdb.ProvideService(hcp)
+	lk := loki.ProvideService(hcp, features, tracer)
+	otsdb := opentsdb.ProvideService(hcp)
+	pr := prometheus.ProvideService(hcp, cfg, features, tracer)
+	tmpo := tempo.ProvideService(hcp)
+	td := testdatasource.ProvideService(cfg, features)
+	pg := postgres.ProvideService(cfg)
+	my := mysql.ProvideService(cfg, hcp)
+	ms := mssql.ProvideService(cfg)
+	sv2 := searchV2.ProvideService(cfg, sqlstore.InitTestDB(t), nil, nil)
+	graf := grafanads.ProvideService(cfg, sv2, nil)
+
+	coreRegistry := coreplugin.ProvideCoreRegistry(am, cw, cm, es, grap, idb, lk, otsdb, pr, tmpo, td, pg, my, ms, graf)
+
+	pmCfg := plugins.FromGrafanaCfg(cfg)
+	pm, err := ProvideService(cfg, registry.NewInMemory(), loader.New(pmCfg, license, signature.NewUnsignedAuthorizer(pmCfg),
+		provider.ProvideService(coreRegistry)))
 	require.NoError(t, err)
 
 	ctx := context.Background()
@@ -33,7 +103,7 @@ func TestPluginManager_int_init(t *testing.T) {
 	verifyPluginStaticRoutes(t, ctx, pm)
 }
 
-func verifyCorePluginCatalogue(t *testing.T, ctx context.Context, pm *manager.PluginManager) {
+func verifyCorePluginCatalogue(t *testing.T, ctx context.Context, pm *PluginManager) {
 	t.Helper()
 
 	expPanels := map[string]struct{}{
@@ -106,6 +176,7 @@ func verifyCorePluginCatalogue(t *testing.T, ctx context.Context, pm *manager.Pl
 		require.NotEqual(t, plugins.PluginDTO{}, p)
 		assert.True(t, exists)
 		assert.Contains(t, expPanels, p.ID)
+		assert.Contains(t, pm.registeredPlugins(ctx), p.ID)
 	}
 
 	dataSources := pm.Plugins(ctx, plugins.DataSource)
@@ -115,6 +186,7 @@ func verifyCorePluginCatalogue(t *testing.T, ctx context.Context, pm *manager.Pl
 		require.NotEqual(t, plugins.PluginDTO{}, p)
 		assert.True(t, exists)
 		assert.Contains(t, expDataSources, ds.ID)
+		assert.Contains(t, pm.registeredPlugins(ctx), p.ID)
 	}
 
 	apps := pm.Plugins(ctx, plugins.App)
@@ -124,12 +196,13 @@ func verifyCorePluginCatalogue(t *testing.T, ctx context.Context, pm *manager.Pl
 		require.NotEqual(t, plugins.PluginDTO{}, p)
 		assert.True(t, exists)
 		assert.Contains(t, expApps, app.ID)
+		assert.Contains(t, pm.registeredPlugins(ctx), p.ID)
 	}
 
 	assert.Equal(t, len(expPanels)+len(expDataSources)+len(expApps), len(pm.Plugins(ctx)))
 }
 
-func verifyBundledPlugins(t *testing.T, ctx context.Context, pm *manager.PluginManager) {
+func verifyBundledPlugins(t *testing.T, ctx context.Context, pm *PluginManager) {
 	t.Helper()
 
 	dsPlugins := make(map[string]struct{})
@@ -153,7 +226,7 @@ func verifyBundledPlugins(t *testing.T, ctx context.Context, pm *manager.PluginM
 	}
 }
 
-func verifyPluginStaticRoutes(t *testing.T, ctx context.Context, pm *manager.PluginManager) {
+func verifyPluginStaticRoutes(t *testing.T, ctx context.Context, pm *PluginManager) {
 	routes := make(map[string]*plugins.StaticRoute)
 	for _, route := range pm.Routes() {
 		routes[route.PluginID] = route
@@ -168,4 +241,20 @@ func verifyPluginStaticRoutes(t *testing.T, ctx context.Context, pm *manager.Plu
 	testAppPlugin, _ := pm.Plugin(ctx, "test-app")
 	assert.Contains(t, routes, "test-app")
 	assert.Equal(t, routes["test-app"].Directory, testAppPlugin.PluginDir)
+}
+
+type fakeTracer struct {
+	tracing.Tracer
+}
+
+func (ft *fakeTracer) Run(context.Context) error {
+	return nil
+}
+
+func (ft *fakeTracer) Start(ctx context.Context, _ string, _ ...trace.SpanStartOption) (context.Context, tracing.Span) {
+	return ctx, nil
+}
+
+func (ft *fakeTracer) Inject(context.Context, http.Header, tracing.Span) {
+
 }
