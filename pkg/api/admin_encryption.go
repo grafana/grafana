e99b9de74f35f6b21c81a3fa2fc34680722e85cc
Joan López de la Franca Beltran <joanjan14@gmail.com>
2022-07-04 13:25:27 +0200

    Encryption: Expose secrets migrations through HTTP API

--- pkg/api/admin_encryption.go
+++ pkg/api/admin_encryption.go
@@ -9,7 +9,31 @@ import (
 
 func (hs *HTTPServer) AdminRotateDataEncryptionKeys(c *models.ReqContext) response.Response {
 	if err := hs.SecretsService.RotateDataKeys(c.Req.Context()); err != nil {
-		return response.Error(http.StatusInternalServerError, "Failed to rotate data key", err)
+		return response.Error(http.StatusInternalServerError, "Failed to rotate data keys", err)
+	}
+
+	return response.Respond(http.StatusNoContent, "")
+}
+
+func (hs *HTTPServer) AdminReEncryptEncryptionKeys(c *models.ReqContext) response.Response {
+	if err := hs.SecretsService.ReEncryptDataKeys(c.Req.Context()); err != nil {
+		return response.Error(http.StatusInternalServerError, "Failed to re-encrypt data keys", err)
+	}
+
+	return response.Respond(http.StatusNoContent, "")
+}
+
+func (hs *HTTPServer) AdminReEncryptSecrets(c *models.ReqContext) response.Response {
+	if err := hs.secretsMigrator.ReEncryptSecrets(c.Req.Context()); err != nil {
+		return response.Error(http.StatusInternalServerError, "Failed to re-encrypt secrets", err)
+	}
+
+	return response.Respond(http.StatusNoContent, "")
+}
+
+func (hs *HTTPServer) AdminRollbackSecrets(c *models.ReqContext) response.Response {
+	if err := hs.secretsMigrator.RollBackSecrets(c.Req.Context()); err != nil {
+		return response.Error(http.StatusInternalServerError, "Failed to rollback secrets", err)
 	}
 
 	return response.Respond(http.StatusNoContent, "")
