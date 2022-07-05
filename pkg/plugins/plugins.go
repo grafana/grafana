cc4f494c330e2b13f0dae95040040f385103aa99
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-07-05 14:32:07 +0400

    remove unneeded prop

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -38,7 +38,6 @@ type Plugin struct {
 	BaseURL string
 
 	Renderer       pluginextensionv2.RendererPlugin
-	Sanitizer      pluginextensionv2.SanitizerClient
 	SecretsManager secretsmanagerplugin.SecretsManagerPlugin
 	client         backendplugin.Plugin
 	log            log.Logger
