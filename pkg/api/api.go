a1f4d885b54e4453afdca2ecb26d0fa3e1527794
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-07-06 10:27:55 +0100

    Only define plugin install endpoints when catalog enabled (#51788) (#51789)
    (cherry picked from commit ba9fc10a745642fe7b0ba6c0a25b0de1a76365b5)
    
    Co-authored-by: Will Browne <wbrowne@users.noreply.github.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -310,10 +310,12 @@ func (hs *HTTPServer) registerRoutes() {
 		apiRoute.Any("/plugins/:pluginId/resources/*", hs.CallResource)
 		apiRoute.Get("/plugins/errors", routing.Wrap(hs.GetPluginErrorsList))
 
-		apiRoute.Group("/plugins", func(pluginRoute routing.RouteRegister) {
-			pluginRoute.Post("/:pluginId/install", routing.Wrap(hs.InstallPlugin))
-			pluginRoute.Post("/:pluginId/uninstall", routing.Wrap(hs.UninstallPlugin))
-		}, reqGrafanaAdmin)
+		if hs.Cfg.PluginAdminEnabled {
+			apiRoute.Group("/plugins", func(pluginRoute routing.RouteRegister) {
+				pluginRoute.Post("/:pluginId/install", routing.Wrap(hs.InstallPlugin))
+				pluginRoute.Post("/:pluginId/uninstall", routing.Wrap(hs.UninstallPlugin))
+			}, reqGrafanaAdmin)
+		}
 
 		apiRoute.Group("/plugins", func(pluginRoute routing.RouteRegister) {
 			pluginRoute.Get("/:pluginId/dashboards/", routing.Wrap(hs.GetPluginDashboards))
