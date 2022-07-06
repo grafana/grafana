e790ed52cf9038fe2c37c874c8198dec3166e726
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-07-06 17:36:39 +0400

    use go:embed

--- pkg/services/store/types_test.go
+++ pkg/services/store/types_test.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	_ "embed"
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
@@ -8,171 +9,11 @@ import (
 )
 
 var (
-	exampleListFrameJSON = `{
-  "schema": {
-    "meta": {
-      "type": "directory-listing",
-      "custom": {
-        "HasMore": false
-      }
-    },
-    "fields": [
-      {
-        "name": "name",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "mediaType",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "size",
-        "type": "number",
-        "typeInfo": {
-          "frame": "int64"
-        },
-        "config": {
-          "unit": "bytes"
-        }
-      }
-    ]
-  },
-  "data": {
-    "values": [
-      [
-        "DL_1.jpg",
-        "Screen Shot 2022-06-23 at 9.05.39 PM.png",
-        "Screen Shot 2022-06-24 at 11.58.32 AM.png",
-        "Screen Shot 2022-06-30 at 3.45.03 PM.png",
-        "Screen Shot 2022-07-05 at 3.24.27 PM.png",
-        "image.png",
-        "rocket_1f680.png",
-        "test-folder",
-        "topcoder12.png"
-      ],
-      [
-        "image/jpeg",
-        "image/png",
-        "image/png",
-        "image/png",
-        "image/png",
-        "image/png",
-        "image/png",
-        "directory",
-        "image/png"
-      ],
-      [
-        943004,
-        684257,
-        256396,
-        8796,
-        388290,
-        182568,
-        29066,
-        0,
-        90563
-      ]
-    ]
-  }
-}
-`
-	exampleRootLevelListJSON = `{
-  "schema": {
-    "meta": {
-      "type": "directory-listing"
-    },
-    "fields": [
-      {
-        "name": "name",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "title",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "description",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "mediaType",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "storageType",
-        "type": "string",
-        "typeInfo": {
-          "frame": "string"
-        }
-      },
-      {
-        "name": "readOnly",
-        "type": "boolean",
-        "typeInfo": {
-          "frame": "bool"
-        }
-      },
-      {
-        "name": "builtIn",
-        "type": "boolean",
-        "typeInfo": {
-          "frame": "bool"
-        }
-      }
-    ]
-  },
-  "data": {
-    "values": [
-      [
-        "public-static",
-        "resources"
-      ],
-      [
-        "Public static files",
-        "Resources"
-      ],
-      [
-        "Access files from the static public files",
-        "Upload custom resource files"
-      ],
-      [
-        "directory",
-        "directory"
-      ],
-      [
-        "disk",
-        "sql"
-      ],
-      [
-        true,
-        false
-      ],
-      [
-        true,
-        true
-      ]
-    ]
-  }
-}
-`
+	//go:embed testdata/example_list_frame.json
+	exampleListFrameJSON string
+
+	//go:embed testdata/example_root_level_list.json
+	exampleRootLevelListJSON string
 )
 
 func TestGetFileNames(t *testing.T) {
