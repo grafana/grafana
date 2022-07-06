254c5585d430ee5e021f1f830d430ae600844c05
Guilherme Caulada <guilherme.caulada@grafana.com>
2022-07-06 11:01:53 -0300

    Use kvstore for datasource secret migration status

--- pkg/services/datasources/service/secrets_mig.go
+++ pkg/services/datasources/service/secrets_mig.go
@@ -3,35 +3,51 @@ package service
 import (
 	"context"
 
+	"github.com/grafana/grafana/pkg/infra/kvstore"
 	"github.com/grafana/grafana/pkg/services/datasources"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
 )
 
 type DataSourceSecretMigrationService struct {
 	dataSourcesService datasources.DataSourceService
+	kvStore            kvstore.KVStore
+	features           featuremgmt.FeatureToggles
 }
 
-func ProvideDataSourceMigrationService(dataSourcesService datasources.DataSourceService) *DataSourceSecretMigrationService {
+func ProvideDataSourceMigrationService(
+	dataSourcesService datasources.DataSourceService,
+	kvStore kvstore.KVStore,
+	features featuremgmt.FeatureToggles,
+) *DataSourceSecretMigrationService {
 	return &DataSourceSecretMigrationService{
 		dataSourcesService: dataSourcesService,
+		kvStore:            kvStore,
+		features:           features,
 	}
 }
 
 func (s *DataSourceSecretMigrationService) Migrate(ctx context.Context) error {
-	query := &datasources.GetAllDataSourcesQuery{}
-	err := s.dataSourcesService.GetAllDataSources(ctx, query)
+	migrationStatus, _, err := s.kvStore.Get(ctx, 0, secretType, "secretMigrationStatus")
 	if err != nil {
 		return err
 	}
 
-	for _, ds := range query.Result {
-		hasMigration := ds.JsonData.Get("secretMigrationComplete").MustBool()
-		if !hasMigration {
+	disableSecretsCompatibility := s.features.IsEnabled("disableSecretsCompatibility")
+	needCompatibility := migrationStatus != "compatible" && !disableSecretsCompatibility
+	needMigration := migrationStatus != "complete" && disableSecretsCompatibility
+
+	if needCompatibility || needMigration {
+		query := &datasources.GetAllDataSourcesQuery{}
+		err := s.dataSourcesService.GetAllDataSources(ctx, query)
+		if err != nil {
+			return err
+		}
+
+		for _, ds := range query.Result {
 			secureJsonData, err := s.dataSourcesService.DecryptedValues(ctx, ds)
 			if err != nil {
 				return err
 			}
-
-			ds.JsonData.Set("secretMigrationComplete", true)
 
 			// Secrets are set by the update data source function if the SecureJsonData is set in the command
 			// Secrets are deleted by the update data source function if the disableSecretsCompatibility flag is enabled
@@ -53,6 +69,12 @@ func (s *DataSourceSecretMigrationService) Migrate(ctx context.Context) error {
 			if err != nil {
 				return err
 			}
+		}
+
+		if disableSecretsCompatibility {
+			s.kvStore.Set(ctx, 0, secretType, "secretMigrationStatus", "complete")
+		} else {
+			s.kvStore.Set(ctx, 0, secretType, "secretMigrationStatus", "compatible")
 		}
 	}
 
