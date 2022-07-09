d299066856a07d840039dc69d766eddcc0e852e4
Ryan McKinley <ryantxu@gmail.com>
2022-07-08 18:16:14 -0700

    fix backend build

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -228,9 +228,6 @@ func (hs *HTTPServer) registerRoutes() {
 				orgRoute.Get("/list/*", routing.Wrap(hs.StorageService.List))
 				orgRoute.Get("/read/*", routing.Wrap(hs.StorageService.Read))
 
-				// hack files into dashboards
-				orgRoute.Get("/dashboard/*", routing.Wrap(hs.StorageService.GetDashboard))
-
 				if hs.Features.IsEnabled(featuremgmt.FlagStorageLocalUpload) {
 					orgRoute.Post("/delete/*", reqGrafanaAdmin, routing.Wrap(hs.StorageService.Delete))
 					orgRoute.Post("/upload", reqGrafanaAdmin, routing.Wrap(hs.StorageService.Upload))
