733f95e23e6ad676770ad9fb4cec33ac03e0a554
drew08t <drew08@gmail.com>
2022-07-11 13:45:12 -0700

    Add vector example to measure overlay

--- public/app/plugins/panel/geomap/components/MeasureOverlay.tsx
+++ public/app/plugins/panel/geomap/components/MeasureOverlay.tsx
@@ -1,5 +1,12 @@
 import { css } from '@emotion/css';
-import { Map } from 'ol';
+// import Feature from 'ol/Feature';
+import Map from 'ol/Map';
+import { Geometry, LineString, Point } from 'ol/geom';
+import { Draw, Modify } from 'ol/interaction';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { getArea, getLength } from 'ol/sphere';
+import { Circle as CircleStyle, Fill, RegularShape, Stroke, Style, Text } from 'ol/style';
 import React, { PureComponent } from 'react';
 import tinycolor from 'tinycolor2';
 
@@ -15,6 +22,246 @@ interface State {
   length?: boolean;
 }
 
+const style = new Style({
+  fill: new Fill({
+    color: 'rgba(255, 255, 255, 0.2)',
+  }),
+  stroke: new Stroke({
+    color: 'rgba(0, 0, 0, 0.5)',
+    lineDash: [10, 10],
+    width: 2,
+  }),
+  image: new CircleStyle({
+    radius: 5,
+    stroke: new Stroke({
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 0.2)',
+    }),
+  }),
+});
+
+const labelStyle = new Style({
+  text: new Text({
+    font: '14px Calibri,sans-serif',
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 1)',
+    }),
+    backgroundFill: new Fill({
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
+    padding: [3, 3, 3, 3],
+    textBaseline: 'bottom',
+    offsetY: -15,
+  }),
+  image: new RegularShape({
+    radius: 8,
+    points: 3,
+    angle: Math.PI,
+    displacement: [0, 10],
+    fill: new Fill({
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
+  }),
+});
+
+const tipStyle = new Style({
+  text: new Text({
+    font: '12px Calibri,sans-serif',
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 1)',
+    }),
+    backgroundFill: new Fill({
+      color: 'rgba(0, 0, 0, 0.4)',
+    }),
+    padding: [2, 2, 2, 2],
+    textAlign: 'left',
+    offsetX: 15,
+  }),
+});
+
+const modifyStyle = new Style({
+  image: new CircleStyle({
+    radius: 5,
+    stroke: new Stroke({
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
+    fill: new Fill({
+      color: 'rgba(0, 0, 0, 0.4)',
+    }),
+  }),
+  text: new Text({
+    text: 'Drag to modify',
+    font: '12px Calibri,sans-serif',
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 1)',
+    }),
+    backgroundFill: new Fill({
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
+    padding: [2, 2, 2, 2],
+    textAlign: 'left',
+    offsetX: 15,
+  }),
+});
+
+const segmentStyle = new Style({
+  text: new Text({
+    font: '12px Calibri,sans-serif',
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 1)',
+    }),
+    backgroundFill: new Fill({
+      color: 'rgba(0, 0, 0, 0.4)',
+    }),
+    padding: [2, 2, 2, 2],
+    textBaseline: 'bottom',
+    offsetY: -12,
+  }),
+  image: new RegularShape({
+    radius: 6,
+    points: 3,
+    angle: Math.PI,
+    displacement: [0, 8],
+    fill: new Fill({
+      color: 'rgba(0, 0, 0, 0.4)',
+    }),
+  }),
+});
+
+const segmentStyles = [segmentStyle];
+const source = new VectorSource();
+const modify = new Modify({ source: source, style: modifyStyle });
+let tipPoint: Geometry;
+let draw: Draw; // global so we can remove it later
+
+const formatLength = function (line: Geometry) {
+  const length = getLength(line);
+  let output;
+  if (length > 100) {
+    output = Math.round((length / 1000) * 100) / 100 + ' km';
+  } else {
+    output = Math.round(length * 100) / 100 + ' m';
+  }
+  return output;
+};
+
+const formatArea = function (polygon: Geometry) {
+  const area = getArea(polygon);
+  let output;
+  if (area > 10000) {
+    output = Math.round((area / 1000000) * 100) / 100 + ' km\xB2';
+  } else {
+    output = Math.round(area * 100) / 100 + ' m\xB2';
+  }
+  return output;
+};
+
+// eslint-disable-next-line
+function styleFunction(feature: any, segments: boolean, drawType?: string, tip?: string) {
+  const styles = [style];
+  const geometry = feature.getGeometry();
+  if (geometry) {
+    const type = geometry.getType();
+    let point, label, line;
+    if (!drawType || drawType === type) {
+      if (type === 'Polygon') {
+        console.log(geometry);
+        point = geometry.getInteriorPoint();
+        label = formatArea(geometry);
+        line = new LineString(geometry.getCoordinates()[0]);
+      } else if (type === 'LineString') {
+        point = new Point(geometry.getLastCoordinate());
+        label = formatLength(geometry);
+        line = geometry;
+      }
+    }
+    if (segments && line) {
+      let count = 0;
+      line.forEachSegment(function (a: number, b: number) {
+        const segment = new LineString([a, b]);
+        const label = formatLength(segment);
+        if (segmentStyles.length - 1 < count) {
+          segmentStyles.push(segmentStyle.clone());
+        }
+        const segmentPoint = new Point(segment.getCoordinateAt(0.5));
+        segmentStyles[count].setGeometry(segmentPoint);
+        segmentStyles[count].getText().setText(label);
+        styles.push(segmentStyles[count]);
+        count++;
+      });
+    }
+    if (label) {
+      labelStyle.setGeometry(point);
+      labelStyle.getText().setText(label);
+      styles.push(labelStyle);
+    }
+    if (tip && type === 'Point' && !modify.getOverlay().getSource().getFeatures().length) {
+      tipPoint = geometry;
+      tipStyle.getText().setText(tip);
+      styles.push(tipStyle);
+    }
+  }
+
+  return styles;
+}
+
+function addInteraction(map: Map, typeSelect: string, showSegments: boolean, clearPrevious: boolean) {
+  const drawType = typeSelect;
+  const activeTip = 'Click to continue ' + (drawType === 'Polygon' ? 'polygon' : 'line');
+  const idleTip = 'Click to start';
+  let tip = idleTip;
+  draw = new Draw({
+    source: source,
+    type: drawType,
+    style: function (feature) {
+      return styleFunction(feature, showSegments, drawType, tip);
+    },
+  });
+  draw.on('drawstart', function () {
+    if (clearPrevious) {
+      source.clear();
+    }
+    modify.setActive(false);
+    tip = activeTip;
+  });
+  draw.on('drawend', function () {
+    modifyStyle.setGeometry(tipPoint);
+    modify.setActive(true);
+    map.once('pointermove', function () {
+      modifyStyle.setGeometry('');
+    });
+    tip = idleTip;
+  });
+  modify.setActive(true);
+  map.addInteraction(draw);
+}
+
+const initMeasure = function (map: Map, typeSelect: string, showSegments: boolean, clearPrevious: boolean) {
+  const vector = new VectorLayer({
+    source: source,
+    style: function (feature) {
+      return styleFunction(feature, showSegments);
+    },
+  });
+
+  map.addLayer(vector);
+  map.addInteraction(modify);
+  addInteraction(map, typeSelect, showSegments, clearPrevious);
+
+  // TODO: add handlers for option change
+  // typeSelect.onchange = function () {
+  //   map.removeInteraction(draw);
+  //   addInteraction();
+  // };
+
+  // showSegments.onchange = function () {
+  //   vector.changed();
+  //   draw.getOverlay().changed();
+  // };
+};
+
 export class MeasureOverlay extends PureComponent<Props, State> {
   style = getStyles(config.theme);
 
@@ -23,14 +270,9 @@ export class MeasureOverlay extends PureComponent<Props, State> {
     this.state = { length: true };
   }
 
-  updateViewState = () => {
-    const view = this.props.map.getView();
-    console.log(view);
-  };
-
   componentDidMount() {
-    this.props.map.on('moveend', this.updateViewState);
-    this.updateViewState();
+    // TODO: base these options on control state
+    initMeasure(this.props.map, 'Polygon', true, false);
   }
 
   render() {
