4d0a2a1db4a785a4bfbed3e9bf676cdee6d2a3d2
sam boyer <sdboyer@grafana.com>
2022-07-13 12:33:53 -0400

    Add generator logic

--- public/cmgen.go
+++ public/cmgen.go
@@ -86,29 +86,46 @@ func main() {
 		panic(err)
 	}
 
-	// ////
-	// b, err := format.Node(fo)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(string(b))
-	// ////
-
 	fv := ctx.BuildFile(fo)
 	iter, err := fv.Fields(cue.Definitions(true))
 	if err != nil {
 		panic(err)
 	}
+
+	// Empty value, to root the path-filling
+	rv := ctx.CompileString(`{}`)
 	for iter.Next() {
 		n := strings.TrimPrefix(iter.Selector().String(), "#")
-		if never[n] || todo[n] || iter.Value().Kind() != cue.StructKind {
-			fmt.Printf("skipping %s...\n", n)
+		nlow := strings.ToLower(n)
+
+		var reason string
+		switch {
+		case never[n]:
+			reason = fmt.Sprintf("skipping %s: on never blacklist", n)
+		case todo[n]:
+			reason = fmt.Sprintf("skipping %s: on todo blacklist", n)
+		case iter.Value().Kind() != cue.StructKind:
+			reason = fmt.Sprintf("skipping %s: not a struct", n)
+		case fieldsContainReference(iter.Value()):
+			reason = fmt.Sprintf("skipping %s: contains a reference", n)
+		}
+		if reason != "" {
+			fmt.Println(reason)
 			continue
 		}
-		nlow := strings.ToLower(n)
-		// Eval to deref any cross-refs
-		// TODO definitely not OK
-		linf, err := ecue.NewLineage(iter.Value().Eval(), nlow, nlow)
+		fmt.Println(iter.Value())
+
+		// Have to fill at a non-ref path in order to keep cue from generating _#defs on
+		// fmting a definition value
+		p := cue.ParsePath(nlow)
+		lrv := rv.FillPath(p, iter.Value())
+
+		// We can skip eval for now because we've already skipped references. Still need proper
+		// answer to the reference issue, though.
+		// val := lrv.LookupPath(p).Eval()
+		val := lrv.LookupPath(p)
+
+		linf, err := ecue.NewLineage(val, nlow, nlow)
 		if err != nil {
 			panic(fmt.Sprintf("%s: %s", n, err))
 		}
@@ -127,4 +144,81 @@ func main() {
 			panic(fmt.Sprintf("%s: %s", n, err))
 		}
 	}
+}
+
+func fieldsContainReference(v cue.Value) bool {
+	if !v.Exists() {
+		return false
+	}
+	if isReference(v) {
+		return true
+	}
+
+	var iter *cue.Iterator
+	var err error
+	switch v.IncompleteKind() {
+	// Simple equality is technically not sufficient b/c Kind is a bitfield, but our
+	// types generally aren't complicated enough to have multi-kinded disjuncts, so good enough
+	case cue.StructKind:
+		if fieldsContainReference(v.LookupPath(cue.MakePath(cue.AnyString))) {
+			return true
+		}
+		iter, err = v.Fields(cue.All())
+	case cue.ListKind:
+		if fieldsContainReference(v.LookupPath(cue.MakePath(cue.AnyIndex))) {
+			return true
+		}
+
+		var liter cue.Iterator
+		liter, err = v.List()
+		iter = &liter
+	default:
+		return containsReference(v)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	for iter.Next() {
+		if fieldsContainReference(iter.Value()) {
+			return true
+		}
+	}
+	return false
+}
+
+// containsReference recursively flattens expressions within a Value to find all
+// its constituent Values, and checks if any of those Values are references.
+//
+// It does NOT walk struct fields - only expression structures, as returned from Expr().
+// Remember that Expr() _always_ drops values in default branches.
+func containsReference(v cue.Value) bool {
+	if isReference(v) {
+		return true
+	}
+	for _, dv := range flatten(v) {
+		if isReference(dv) {
+			return true
+		}
+	}
+	return false
+}
+
+func isReference(v cue.Value) bool {
+	_, path := v.ReferencePath()
+	return len(path.Selectors()) > 0
+}
+
+func flatten(v cue.Value) []cue.Value {
+	all := []cue.Value{v}
+
+	op, dvals := v.Expr()
+	defv, has := v.Default()
+	if !v.Equals(defv) && (op != cue.NoOp || has) {
+		all = append(all, dvals...)
+		for _, dv := range dvals {
+			all = append(all, flatten(dv)...)
+		}
+	}
+	return all
 }
