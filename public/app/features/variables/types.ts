24cfee7c27fd0596c4de0f93a36359ab5b5428fa
joshhunt <josh@trtr.co>
2022-07-13 09:26:36 +0100

    wip

--- public/app/features/variables/types.ts
+++ public/app/features/variables/types.ts
@@ -8,11 +8,111 @@ import {
   LoadingState,
   QueryEditorProps,
   VariableModel as BaseVariableModel,
-  VariableType,
 } from '@grafana/data';
 import { TemplateSrv } from '@grafana/runtime';
 
 import { NEW_VARIABLE_ID } from './constants';
+
+// ---
+// Base variable
+export interface VariableModel extends BaseVariableModel {
+  name: string;
+  label?: string;
+  id: string;
+  rootStateKey: string | null;
+  global: boolean;
+  hide: VariableHide;
+  skipUrlSync: boolean;
+  index: number;
+  state: LoadingState;
+  error: any | null;
+  description: string | null;
+}
+
+// ---
+// Generic variable types
+export interface VariableWithOptions extends VariableModel {
+  current: VariableOption;
+  options: VariableOption[];
+  query: string;
+}
+
+export interface VariableWithMultiSupport extends VariableWithOptions {
+  multi: boolean;
+  includeAll: boolean;
+  allValue?: string | null;
+}
+
+// ---
+// Specific variable types
+export interface QueryVariableModel extends VariableWithMultiSupport {
+  type: 'query';
+  datasource: DataSourceRef | null;
+  definition: string;
+  sort: VariableSort;
+  queryValue?: string;
+  query: any;
+  regex: string;
+  refresh: VariableRefresh;
+}
+
+export interface AdHocVariableModel extends VariableModel {
+  type: 'adhoc';
+  datasource: DataSourceRef | null;
+  filters: AdHocVariableFilter[];
+}
+
+export interface ConstantVariableModel extends VariableWithOptions {
+  type: 'constant';
+}
+
+export interface DataSourceVariableModel extends VariableWithMultiSupport {
+  type: 'datasource';
+  regex: string;
+  refresh: VariableRefresh;
+}
+
+export interface IntervalVariableModel extends VariableWithOptions {
+  type: 'interval';
+  auto: boolean;
+  auto_min: string;
+  auto_count: number;
+  refresh: VariableRefresh;
+}
+
+export interface TextBoxVariableModel extends VariableWithOptions {
+  type: 'textbox';
+  originalQuery: string | null;
+}
+
+export interface CustomVariableModel extends VariableWithMultiSupport {
+  type: 'custom';
+}
+
+export interface SystemVariable<TProps extends { toString: () => string }> extends VariableModel {
+  type: 'system';
+  current: { value: TProps };
+}
+
+export interface UserVariableModel extends SystemVariable<UserProps> {}
+export interface OrgVariableModel extends SystemVariable<OrgProps> {}
+export interface DashboardVariableModel extends SystemVariable<DashboardProps> {}
+
+export type TypedVariableModel =
+  | QueryVariableModel
+  | AdHocVariableModel
+  | ConstantVariableModel
+  | DataSourceVariableModel
+  | IntervalVariableModel
+  | TextBoxVariableModel
+  | CustomVariableModel
+  | UserVariableModel
+  | OrgVariableModel
+  | DashboardVariableModel;
+
+//
+// ----------------
+//
 
 export enum TransactionStatus {
   NotStarted = 'Not started',
@@ -56,66 +156,17 @@ export interface AdHocVariableFilter {
   condition: string;
 }
 
-export interface AdHocVariableModel extends VariableModel {
-  datasource: DataSourceRef | null;
-  filters: AdHocVariableFilter[];
-}
-
-export interface IntervalVariableModel extends VariableWithOptions {
-  auto: boolean;
-  auto_min: string;
-  auto_count: number;
-  refresh: VariableRefresh;
-}
-
-export interface CustomVariableModel extends VariableWithMultiSupport {}
-
-export interface DataSourceVariableModel extends VariableWithMultiSupport {
-  regex: string;
-  refresh: VariableRefresh;
-}
-
-export interface QueryVariableModel extends DataSourceVariableModel {
-  datasource: DataSourceRef | null;
-  definition: string;
-  sort: VariableSort;
-  queryValue?: string;
-  query: any;
-}
-
-export interface TextBoxVariableModel extends VariableWithOptions {
-  originalQuery: string | null;
-}
-
-export interface ConstantVariableModel extends VariableWithOptions {}
-
-export interface VariableWithMultiSupport extends VariableWithOptions {
-  multi: boolean;
-  includeAll: boolean;
-  allValue?: string | null;
-}
-
-export interface VariableWithOptions extends VariableModel {
-  current: VariableOption;
-  options: VariableOption[];
-  query: string;
-}
-
 export interface DashboardProps {
   name: string;
   uid: string;
   toString: () => string;
 }
 
-export interface DashboardVariableModel extends SystemVariable<DashboardProps> {}
-
 export interface OrgProps {
   name: string;
   id: number;
   toString: () => string;
 }
-
-export interface OrgVariableModel extends SystemVariable<OrgProps> {}
 
 export interface UserProps {
   login: string;
@@ -124,29 +175,10 @@ export interface UserProps {
   toString: () => string;
 }
 
-export interface UserVariableModel extends SystemVariable<UserProps> {}
-
-export interface SystemVariable<TProps extends { toString: () => string }> extends VariableModel {
-  current: { value: TProps };
-}
-
-export interface VariableModel extends BaseVariableModel {
-  id: string;
-  rootStateKey: string | null;
-  global: boolean;
-  hide: VariableHide;
-  skipUrlSync: boolean;
-  index: number;
-  state: LoadingState;
-  error: any | null;
-  description: string | null;
-}
-
 export const initialVariableModelState: VariableModel = {
   id: NEW_VARIABLE_ID,
   rootStateKey: null,
   name: '',
-  type: '' as unknown as VariableType,
   global: false,
   index: -1,
   hide: VariableHide.dontHide,
@@ -154,6 +186,7 @@ export const initialVariableModelState: VariableModel = {
   state: LoadingState.NotStarted,
   error: null,
   description: null,
+  type: 'query',
 };
 
 export interface VariableQueryEditorProps {
