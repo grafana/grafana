10ce1ad7a51c4817e228413420e1f700654e7a4d
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2022-07-13 10:17:17 -0400

    remove unused field

--- pkg/services/ngalert/schedule/schedule.go
+++ pkg/services/ngalert/schedule/schedule.go
@@ -79,7 +79,6 @@ type schedule struct {
 
 	ruleStore     store.RuleStore
 	instanceStore store.InstanceStore
-	orgStore      store.OrgStore
 
 	stateManager *state.Manager
 
@@ -111,7 +110,6 @@ type SchedulerCfg struct {
 	StopAppliedFunc func(ngmodels.AlertRuleKey)
 	Evaluator       eval.Evaluator
 	RuleStore       store.RuleStore
-	OrgStore        store.OrgStore
 	InstanceStore   store.InstanceStore
 	Metrics         *metrics.Scheduler
 	AlertSender     AlertsSender
@@ -133,7 +131,6 @@ func NewScheduler(cfg SchedulerCfg, appURL *url.URL, stateManager *state.Manager
 		evaluator:             cfg.Evaluator,
 		ruleStore:             cfg.RuleStore,
 		instanceStore:         cfg.InstanceStore,
-		orgStore:              cfg.OrgStore,
 		metrics:               cfg.Metrics,
 		appURL:                appURL,
 		disableGrafanaFolder:  cfg.Cfg.ReservedLabels.IsReservedLabelDisabled(ngmodels.FolderTitleLabel),
