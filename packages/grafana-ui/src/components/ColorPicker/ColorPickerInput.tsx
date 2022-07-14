eece704870cc307fea3ab84fbaaef1506b215500
Clarity-89 <homes89@ukr.net>
2022-07-14 10:25:15 +0300

    ColorPickerInput: Extend Input props

--- packages/grafana-ui/src/components/ColorPicker/ColorPickerInput.tsx
+++ packages/grafana-ui/src/components/ColorPicker/ColorPickerInput.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import React, { useState } from 'react';
+import React, { useState, forwardRef } from 'react';
 import { RgbaStringColorPicker } from 'react-colorful';
 import { useThrottleFn } from 'react-use';
 
@@ -7,38 +7,51 @@ import { colorManipulator, GrafanaTheme2 } from '@grafana/data';
 
 import { useStyles2, useTheme2 } from '../../themes';
 import { ClickOutsideWrapper } from '../ClickOutsideWrapper/ClickOutsideWrapper';
+import { Props as InputProps } from '../Input/Input';
 
 import ColorInput from './ColorInput';
-export interface ColorPickerInputProps {
+
+export interface ColorPickerInputProps extends Omit<InputProps, 'color' | 'onChange'> {
   value: string;
   onChange: (color: string) => void;
 }
 
-export const ColorPickerInput = ({ value, onChange }: ColorPickerInputProps) => {
-  const [currentColor, setColor] = useState(value);
-  const [isOpen, setIsOpen] = useState(false);
-  const theme = useTheme2();
-  const styles = useStyles2(getStyles);
+export const ColorPickerInput = forwardRef<HTMLInputElement, ColorPickerInputProps>(
+  ({ value, onChange, ...inputProps }, ref) => {
+    const [currentColor, setColor] = useState(value);
+    const [isOpen, setIsOpen] = useState(false);
+    const theme = useTheme2();
+    const styles = useStyles2(getStyles);
 
-  useThrottleFn(
-    (c) => {
-      onChange(colorManipulator.asHexString(theme.visualization.getColorByName(c)));
-    },
-    500,
-    [currentColor]
-  );
+    useThrottleFn(
+      (c) => {
+        onChange(colorManipulator.asHexString(theme.visualization.getColorByName(c)));
+      },
+      500,
+      [currentColor]
+    );
 
-  return (
-    <ClickOutsideWrapper onClick={() => setIsOpen(false)}>
-      <div className={styles.wrapper}>
-        {isOpen && <RgbaStringColorPicker color={currentColor} onChange={setColor} className={styles.picker} />}
-        <div onClick={() => setIsOpen(true)}>
-          <ColorInput theme={theme} color={currentColor} onChange={setColor} className={styles.input} />
+    return (
+      <ClickOutsideWrapper onClick={() => setIsOpen(false)}>
+        <div className={styles.wrapper}>
+          {isOpen && <RgbaStringColorPicker color={currentColor} onChange={setColor} className={styles.picker} />}
+          <div onClick={() => setIsOpen(true)}>
+            <ColorInput
+              {...inputProps}
+              theme={theme}
+              color={currentColor}
+              onChange={setColor}
+              className={styles.input}
+              ref={ref}
+            />
+          </div>
         </div>
-      </div>
-    </ClickOutsideWrapper>
-  );
-};
+      </ClickOutsideWrapper>
+    );
+  }
+);
+
+ColorPickerInput.displayName = 'ColorPickerInput';
 
 const getStyles = (theme: GrafanaTheme2) => {
   return {
