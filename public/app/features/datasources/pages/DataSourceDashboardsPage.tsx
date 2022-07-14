4165997a791668e3dd66a9d05000ce179975c1f6
Levente Balogh <balogh.levente.hu@gmail.com>
2022-07-14 12:10:03 +0200

    refactor: use the new `navId` prop where we can

--- public/app/features/datasources/pages/DataSourceDashboardsPage.tsx
+++ public/app/features/datasources/pages/DataSourceDashboardsPage.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import { Page } from 'app/core/components/Page/Page';
 import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
-import { getNavModel } from 'app/core/selectors/navModel';
-import { StoreState } from 'app/types';
 
 import { DataSourceDashboards } from '../components/DataSourceDashboards';
 
@@ -12,11 +9,9 @@ export interface Props extends GrafanaRouteComponentProps<{ uid: string }> {}
 
 export function DataSourceDashboardsPage(props: Props): React.ReactElement {
   const uid = props.match.params.uid;
-  const navIndex = useSelector((s: StoreState) => s.navIndex);
-  const navModel = getNavModel(navIndex, `datasource-dashboards-${uid}`);
 
   return (
-    <Page navModel={navModel}>
+    <Page navId={`datasource-dashboards-${uid}`}>
       <Page.Contents>
         <DataSourceDashboards uid={uid} />
       </Page.Contents>
--- public/app/features/datasources/pages/DataSourcesListPage.tsx
+++ public/app/features/datasources/pages/DataSourcesListPage.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import { Page } from 'app/core/components/Page/Page';
-import { getNavModel } from 'app/core/selectors/navModel';
-import { StoreState } from 'app/types';
 
 import { DataSourcesList } from '../components/DataSourcesList';
 
 export function DataSourcesListPage(): React.ReactElement {
-  const navModel = useSelector(({ navIndex }: StoreState) => getNavModel(navIndex, 'datasources'));
-
   return (
-    <Page navModel={navModel}>
+    <Page navId="datasources">
       <Page.Contents>
         <DataSourcesList />
       </Page.Contents>
