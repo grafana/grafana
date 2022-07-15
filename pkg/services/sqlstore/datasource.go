4f7c2a96bd9b9c37e60a00a49067baabeec915c3
Elfo404 <me@giordanoricci.com>
2022-07-15 10:52:26 +0200

    Only publish deletion event if datasource was actually deleted

--- pkg/services/sqlstore/datasource.go
+++ pkg/services/sqlstore/datasource.go
@@ -131,21 +131,13 @@ func (ss *SQLStore) DeleteDataSource(ctx context.Context, cmd *datasources.Delet
 		}
 
 		// Publish data source deletion event
-		if ds != nil {
+		if cmd.DeletedDatasourcesCount > 0 {
 			sess.publishAfterCommit(&events.DataSourceDeleted{
 				Timestamp: time.Now(),
 				Name:      ds.Name,
 				ID:        ds.Id,
 				UID:       ds.Uid,
 				OrgID:     ds.OrgId,
-			})
-		} else {
-			sess.publishAfterCommit(&events.DataSourceDeleted{
-				Timestamp: time.Now(),
-				Name:      cmd.Name,
-				ID:        cmd.ID,
-				UID:       cmd.UID,
-				OrgID:     cmd.OrgID,
 			})
 		}
 
