87a25ffa8fd7fa709180be968306c618d0db48c0
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2022-07-15 13:42:45 +0200

    [WIP] Create fake response for the new service to get contact point errors

--- public/app/features/alerting/unified/api/grafana.ts
+++ public/app/features/alerting/unified/api/grafana.ts
@@ -1,6 +1,14 @@
 import { getBackendSrv } from '@grafana/runtime';
-import { NotifierDTO } from 'app/types';
+import { ContactPointStateDTO, NotifierDTO } from 'app/types';
 
 export function fetchNotifiers(): Promise<NotifierDTO[]> {
   return getBackendSrv().get(`/api/alert-notifiers`);
+}
+
+export function fetchContactPointsState(alertManagerSourceName: String): Promise<ContactPointStateDTO[]> {
+  return new Promise<ContactPointStateDTO[]>((resolve) => {
+    setTimeout(() => {
+      resolve([]);
+    }, 1000);
+  });
 }
