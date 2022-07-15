0f34a99f8bd95accf3d8f9091e74a2e21251421f
Leandro Deveikis <leandro.deveikis@gmail.com>
2022-07-15 16:55:16 -0300

    setting services within a slice

--- pkg/services/secrets/kvstore/plugin_mig_test.go
+++ pkg/services/secrets/kvstore/plugin_mig_test.go
@@ -4,14 +4,7 @@ import (
 	"context"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/grafana/pkg/plugins/backendplugin/secretsmanagerplugin"
-	"github.com/grafana/grafana/pkg/services/secrets/fakes"
-	secretsManager "github.com/grafana/grafana/pkg/services/secrets/manager"
-	"github.com/grafana/grafana/pkg/services/sqlstore"
-	"github.com/grafana/grafana/pkg/setting"
 	"github.com/stretchr/testify/require"
-	"gopkg.in/ini.v1"
 )
 
 // This tests will create a mock sql database and an inmemory
@@ -21,7 +14,7 @@ func TestPluginSecretMigrationService_Migrate(t *testing.T) {
 
 	t.Run("migration run ok - 2 secrets migrated", func(t *testing.T) {
 		// --- SETUP
-		migratorService, secretsStore, sqlSecretStore := setupTestMigratorService(t)
+		migratorService, secretsStore, sqlSecretStore := SetupTestMigratorService(t)
 		var orgId int64 = 1
 		namespace1, namespace2 := "namespace-test", "namespace-test2"
 		typ := "type-test"
@@ -60,62 +53,4 @@ func validateSecretWasStoreInPlugin(t *testing.T, secretsStore SecretsKVStore, c
 	resPlugin, err := secretsStore.Keys(ctx, orgId, namespace1, typ)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(resPlugin))
-}
-
-//
-func setupTestMigratorService(t *testing.T) (*PluginSecretMigrationService, SecretsKVStore, *secretsKVStoreSQL) {
-	t.Helper()
-
-	rawCfg := `
-		[secrets]
-		use_plugin = true
-		migrate_to_plugin = true
-		`
-	raw, err := ini.Load([]byte(rawCfg))
-	require.NoError(t, err)
-	cfg := &setting.Cfg{Raw: raw}
-	// this would be the plugin - mocked at the moment
-	secretsStoreForPlugin := NewFakeSecretsKVStore()
-	// Mocked remote plugin check, always return true
-	remoteCheck := provideMockRemotePluginCheck()
-
-	// this is to init the sql secret store inside the migration
-	sqlStore := sqlstore.InitTestDB(t)
-	secretsService := secretsManager.SetupTestService(t, fakes.NewFakeSecretsStore())
-
-	migratorService := ProvidePluginSecretMigrationService(
-		secretsStoreForPlugin,
-		cfg,
-		sqlStore,
-		secretsService,
-		remoteCheck,
-	)
-
-	secretsSql := &secretsKVStoreSQL{
-		sqlStore:       sqlStore,
-		secretsService: secretsService,
-		log:            log.New("test.logger"),
-		decryptionCache: decryptionCache{
-			cache: make(map[int64]cachedDecrypted),
-		},
-	}
-
-	return migratorService, secretsStoreForPlugin, secretsSql
-}
-
-//
-type mockRemoteSecretsPluginCheck struct {
-	UseRemoteSecretsPluginCheck
-}
-
-func provideMockRemotePluginCheck() *mockRemoteSecretsPluginCheck {
-	return &mockRemoteSecretsPluginCheck{}
-}
-
-func (c *mockRemoteSecretsPluginCheck) ShouldUseRemoteSecretsPlugin() bool {
-	return true
-}
-
-func (c *mockRemoteSecretsPluginCheck) GetPlugin() (secretsmanagerplugin.SecretsManagerPlugin, error) {
-	return nil, nil
 }
