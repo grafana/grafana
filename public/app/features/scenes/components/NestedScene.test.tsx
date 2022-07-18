00f08b1be59ace18328d0e3e73516fd6e27c9d1d
Torkel Ödegaard <torkel@grafana.com>
2022-07-18 10:35:12 -0700

    Added test for nested scene

--- public/app/features/scenes/components/NestedScene.test.tsx
+++ public/app/features/scenes/components/NestedScene.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import React from 'react';
 
 import { NestedScene } from './NestedScene';
