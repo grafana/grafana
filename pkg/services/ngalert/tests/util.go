6a830d566855edeb709b9852b7587d2589f71a1a
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2022-07-18 17:03:26 +0200

    add missing arg

--- pkg/services/ngalert/tests/util.go
+++ pkg/services/ngalert/tests/util.go
@@ -63,7 +63,7 @@ func SetupTestEnv(t *testing.T, baseInterval time.Duration) (*ngalert.AlertNG, *
 	)
 
 	ng, err := ngalert.ProvideService(
-		cfg, nil, routing.NewRouteRegister(), sqlStore, nil, nil, nil, nil,
+		cfg, nil, nil, routing.NewRouteRegister(), sqlStore, nil, nil, nil, nil,
 		secretsService, nil, m, folderService, ac, &dashboards.FakeDashboardService{}, nil, bus,
 	)
 	require.NoError(t, err)
