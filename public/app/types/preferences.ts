2911aedc7fdd183ad38df14cf96d1d68adec45f4
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2022-07-19 17:10:31 +0200

    docs: Add clarification to types

--- public/app/types/preferences.ts
+++ public/app/types/preferences.ts
@@ -4,6 +4,7 @@ export interface UserPreferencesDTO {
   timezone: TimeZone;
   weekStart: string;
   locale: string;
+  // It is undefined when there is not dashboard assigned (default)
   homeDashboardUID?: string;
   theme: string;
   queryHistory: {
