b7c941513f818d21c5594c66c6c8550a74fb7c03
Levente Balogh <balogh.levente.hu@gmail.com>
2022-07-19 10:08:00 +0200

    feat(data-connections): set the the custom routes via the context provider

--- public/app/features/data-connections/DataConnectionsPage.tsx
+++ public/app/features/data-connections/DataConnectionsPage.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { Page } from 'app/core/components/Page/Page';
+import { DataSourcesRoutesContext } from 'app/features/datasources/state';
 
 import { ROUTES } from './constants';
 import { useNavModel } from './hooks/useNavModel';
 import { CloudIntegrations } from './tabs/CloudIntegrations';
-import { DataSources } from './tabs/DataSources';
+import { DataSources, DataSourcesNew, DataSourcesEdit } from './tabs/DataSources';
 import { Plugins } from './tabs/Plugins';
 import { RecordedQueries } from './tabs/RecordedQueries';
 
@@ -14,20 +15,29 @@ export default function DataConnectionsPage(): React.ReactElement | null {
   const navModel = useNavModel();
 
   return (
-    <Page navModel={navModel}>
-      <Page.Contents>
-        <Switch>
-          <Route path={ROUTES.DataSources} component={DataSources} />
-          <Route path={ROUTES.DataSourcesNew} component={DataSources} />
-          <Route path={ROUTES.DataSourcesEdit} component={DataSources} />
-          <Route path={ROUTES.Plugins} component={Plugins} />
-          <Route path={ROUTES.CloudIntegrations} component={CloudIntegrations} />
-          <Route path={ROUTES.RecordedQueries} component={RecordedQueries} />
+    <DataSourcesRoutesContext.Provider
+      value={{
+        New: ROUTES.DataSourcesNew,
+        List: ROUTES.DataSources,
+        Edit: ROUTES.DataSourcesEdit,
+        Dashboards: ROUTES.DataSourcesDashboards,
+      }}
+    >
+      <Page navModel={navModel}>
+        <Page.Contents>
+          <Switch>
+            <Route path={ROUTES.DataSourcesNew} component={DataSourcesNew} />
+            <Route path={ROUTES.DataSourcesEdit} component={DataSourcesEdit} />
+            <Route path={ROUTES.DataSources} component={DataSources} />
+            <Route path={ROUTES.Plugins} component={Plugins} />
+            <Route path={ROUTES.CloudIntegrations} component={CloudIntegrations} />
+            <Route path={ROUTES.RecordedQueries} component={RecordedQueries} />
 
-          {/* Default page */}
-          <Route component={DataSources} />
-        </Switch>
-      </Page.Contents>
-    </Page>
+            {/* Default page */}
+            <Route component={DataSources} />
+          </Switch>
+        </Page.Contents>
+      </Page>
+    </DataSourcesRoutesContext.Provider>
   );
 }
