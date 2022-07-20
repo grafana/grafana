7c57e27057f6488c286dc01ba9296e744c81ddc7
Jean-Philippe Quéméner <JohnnyQQQQ@users.noreply.github.com>
2022-07-20 14:55:41 +0200

    Update pkg/services/ngalert/api/api_admin.go
    Co-authored-by: gotjosh <josue.abreu@gmail.com>

--- pkg/services/ngalert/api/api_admin.go
+++ pkg/services/ngalert/api/api_admin.go
@@ -82,7 +82,7 @@ func (srv AdminSrv) RoutePostNGalertConfig(c *models.ReqContext, body apimodels.
 
 	if sendAlertsTo == ngmodels.ExternalAlertmanagers &&
 		len(body.Alertmanagers)+len(externalAlertmanagers) < 1 {
-		return response.Error(400, "At least one Alertmanager must be provided to choose this option", nil)
+		return response.Error(400, "At least one Alertmanager must be provided or configured as a datasource that handles alerts to choose this option", nil)
 	}
 
 	cfg := &ngmodels.AdminConfiguration{
