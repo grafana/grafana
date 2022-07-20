f86d74e69a7816992562f70c0c3249c5948e92e7
Michael Mandrus <michael.mandrus@grafana.com>
2022-07-20 13:34:50 -0400

    add blank file for eventual unit tests

---
+++ pkg/services/secrets/kvstore/plugin_fatal_crash_test.go
@@ -0,0 +1,7 @@
+package kvstore
+
+// TODO make test for plugin start failure + fatal flag set
+// TODO make test for plugin start failure + fatal flag not set
+// TODO make test for secret set + fatal flag not set + backwards compat disabled
+// TODO make test for secret get + fatal flag set + backwards compat enabled
+// TODO make test for migration + fatal flag not set + error deleting unified secrets
