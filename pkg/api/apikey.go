765fd2915101ca35077f7ad17c595c545936d9e5
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-07-21 12:25:23 +0300

    Move API key swagger definittions

--- pkg/api/apikey.go
+++ pkg/api/apikey.go
@@ -13,7 +13,18 @@ import (
 	"github.com/grafana/grafana/pkg/web"
 )
 
-// GetAPIKeys returns a list of API keys
+// swagger:route GET /auth/keys api_keys getAPIkeys
+//
+// Get auth keys.
+//
+// Will return auth keys.
+//
+// Responses:
+// 200: getAPIkeyResponse
+// 401: unauthorisedError
+// 403: forbiddenError
+// 404: notFoundError
+// 500: internalServerError
 func (hs *HTTPServer) GetAPIKeys(c *models.ReqContext) response.Response {
 	query := models.GetApiKeysQuery{OrgId: c.OrgId, User: c.SignedInUser, IncludeExpired: c.QueryBool("includeExpired")}
 
@@ -48,7 +59,16 @@ func (hs *HTTPServer) GetAPIKeys(c *models.ReqContext) response.Response {
 	return response.JSON(http.StatusOK, result)
 }
 
-// DeleteAPIKey deletes an API key
+// swagger:route DELETE /auth/keys/{id} api_keys deleteAPIkey
+//
+// Delete API key.
+//
+// Responses:
+// 200: okResponse
+// 401: unauthorisedError
+// 403: forbiddenError
+// 404: notFoundError
+// 500: internalServerError
 func (hs *HTTPServer) DeleteAPIKey(c *models.ReqContext) response.Response {
 	id, err := strconv.ParseInt(web.Params(c.Req)[":id"], 10, 64)
 	if err != nil {
@@ -70,7 +90,19 @@ func (hs *HTTPServer) DeleteAPIKey(c *models.ReqContext) response.Response {
 	return response.Success("API key deleted")
 }
 
-// AddAPIKey adds an API key
+// swagger:route POST /auth/keys api_keys addAPIkey
+//
+// Creates an API key.
+//
+// Will return details of the created API key
+//
+// Responses:
+// 200: postAPIkeyResponse
+// 400: badRequestError
+// 401: unauthorisedError
+// 403: forbiddenError
+// 409: conflictError
+// 500: internalServerError
 func (hs *HTTPServer) AddAPIKey(c *models.ReqContext) response.Response {
 	cmd := models.AddApiKeyCommand{}
 	if err := web.Bind(c.Req, &cmd); err != nil {
@@ -117,4 +149,41 @@ func (hs *HTTPServer) AddAPIKey(c *models.ReqContext) response.Response {
 	}
 
 	return response.JSON(http.StatusOK, result)
+}
+
+// swagger:parameters getAPIkeys
+type GetAPIkeysParams struct {
+	// Show expired keys
+	// in:query
+	// required:false
+	// default:false
+	IncludeExpired bool `json:"includeExpired"`
+}
+
+// swagger:parameters addAPIkey
+type AddAPIkeyParams struct {
+	// in:body
+	// required:true
+	Body models.AddApiKeyCommand
+}
+
+// swagger:parameters deleteAPIkey
+type DeleteAPIkeyParams struct {
+	// in:path
+	// required:true
+	ID int64 `json:"id"`
+}
+
+// swagger:response getAPIkeyResponse
+type GetAPIkeyResponse struct {
+	// The response message
+	// in: body
+	Body []*dtos.ApiKeyDTO `json:"body"`
+}
+
+// swagger:response postAPIkeyResponse
+type PostAPIkeyResponse struct {
+	// The response message
+	// in: body
+	Body dtos.NewApiKeyResult `json:"body"`
 }
