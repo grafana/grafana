4fbf8df598859b0ef83c08321472391abf13428a
Ryan McKinley <ryantxu@gmail.com>
2022-07-21 17:27:52 -0700

    adding basic http endpoints

---
+++ pkg/services/entity/http.go
@@ -0,0 +1,136 @@
+package entity
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/grafana/grafana-plugin-sdk-go/experimental/entity"
+	"github.com/grafana/grafana/pkg/api/response"
+	"github.com/grafana/grafana/pkg/api/routing"
+	"github.com/grafana/grafana/pkg/cmd/grafana-cli/logger"
+	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/web"
+)
+
+func (s *StandardEntityStoreServer) RegisterEntityRoutes(route routing.RouteRegister) {
+	route.Get("/*", s.doGetEntity)
+	route.Delete("/*", routing.Wrap(s.doDelteEntity))
+	route.Put("/*", routing.Wrap(s.doEntityUpdate))
+
+	route.Post("/save", routing.Wrap(s.doSave))
+	route.Post("/pr", routing.Wrap(s.doPR))
+}
+
+func (s *StandardEntityStoreServer) RegisterKindsRoutes(route routing.RouteRegister) {
+	route.Get("/", routing.Wrap(s.doListKinds))
+	route.Post("/test/:kind", routing.Wrap(s.doTestKind))
+}
+
+func (s *StandardEntityStoreServer) doGetEntity(c *models.ReqContext) {
+	params := web.Params(c.Req)
+	urlParams := c.Req.URL.Query()
+
+	// Get the history
+	if isTrue(urlParams, "history") {
+		rsp, err := s.GetEntityHistory(c.Req.Context(), &entity.GetHistoryRequest{
+			Path:       params["*"],
+			MaxResults: 100,
+			PageToken:  urlParams.Get("pageToken"),
+		})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, map[string]string{"path": params["*"], "error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, rsp)
+		}
+		return
+	}
+
+	// regular get
+	req := &entity.GetEntityRequest{
+		Path:        params["*"],
+		Version:     urlParams.Get("version"), // URL parameter
+		WithPayload: true,
+	}
+	isRaw := isTrue(urlParams, "raw")
+	if !isRaw {
+		req.WithStorageMeta = true
+		req.WithACL = true
+		req.WithPRs = true
+	}
+
+	rsp, err := s.GetEntity(c.Req.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, map[string]string{"path": req.Path, "error": err.Error()})
+		return
+	}
+
+	// Check for ETag updates
+	if rsp.Meta != nil && rsp.Meta.Etag != "" {
+		c.Resp.Header().Set("ETag", rsp.Meta.Etag)
+
+		previousEtag := c.Req.Header.Get("If-None-Match")
+		if previousEtag == rsp.Meta.Etag {
+			c.Resp.WriteHeader(http.StatusNotModified)
+			return
+		}
+	}
+
+	if isRaw {
+		ct := "application/json"
+		k := s.kinds.Get(rsp.Kind)
+		if k != nil {
+			ct = k.Info().ContentType
+		}
+
+		if ct != "" {
+			c.Resp.Header().Set("Content-Type", ct)
+		}
+
+		c.Resp.WriteHeader(http.StatusOK)
+		if _, err := c.Resp.Write(rsp.Payload); err != nil {
+			logger.Warn("Error writing to response", "err", err)
+		}
+		c.Resp.Flush()
+	} else {
+		c.JSON(http.StatusOK, rsp)
+	}
+}
+
+func (s *StandardEntityStoreServer) doDelteEntity(c *models.ReqContext) response.Response {
+	params := web.Params(c.Req)
+	path := params["*"]
+
+	return response.JSON(200, "DELETE: "+path)
+}
+
+func (s *StandardEntityStoreServer) doEntityUpdate(c *models.ReqContext) response.Response {
+	params := web.Params(c.Req)
+	path := params["*"]
+
+	return response.JSON(200, "UPDATE: "+path)
+}
+
+func (s *StandardEntityStoreServer) doSave(c *models.ReqContext) response.Response {
+	return response.JSON(200, "TODO... save")
+}
+
+func (s *StandardEntityStoreServer) doPR(c *models.ReqContext) response.Response {
+	return response.JSON(200, "TODO... create PR")
+}
+
+func (s *StandardEntityStoreServer) doListKinds(c *models.ReqContext) response.Response {
+	rsp, err := s.ListKinds(c.Req.Context(), &entity.ListKindsRequest{})
+	if err != nil {
+		return response.Error(400, "error", err)
+	}
+	return response.JSON(200, rsp)
+}
+
+func (s *StandardEntityStoreServer) doTestKind(c *models.ReqContext) response.Response {
+	return response.JSON(200, "TODO... test kind")
+}
+
+func isTrue(params url.Values, key string) bool {
+	raw, ok := params[key]
+	return ok && (raw[0] == "" || raw[0] == "true")
+}
---
+++ pkg/services/entity/store.go
@@ -0,0 +1,159 @@
+package entity
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gogo/status"
+	"github.com/grafana/grafana-plugin-sdk-go/experimental/entity"
+	"github.com/grafana/grafana/pkg/api/routing"
+	"github.com/grafana/grafana/pkg/services/entity/kind"
+	"google.golang.org/grpc/codes"
+)
+
+type EntityStore interface {
+	entity.EntityStoreServer
+
+	// Expose the kinds
+	GetKinds() entity.KindRegistry
+
+	// Register HTTP paths
+	RegisterEntityRoutes(apiRoute routing.RouteRegister)
+	RegisterKindsRoutes(apiRoute routing.RouteRegister)
+}
+
+type StandardEntityStoreServer struct {
+	entity.EntityStoreServer
+	kinds entity.KindRegistry
+}
+
+func ProvideService() EntityStore {
+	k, err := entity.NewKindRegistry(
+		// Core types
+		//----------------------
+		&kind.DashboardKind{},
+
+		// Images
+		//----------------------
+		entity.NewRawFileKind(
+			&entity.KindInfo{
+				ID:          "png",
+				Description: "image",
+				PathSuffix:  ".png",
+			},
+			nil, // could add a sanitizer here
+		),
+		entity.NewRawFileKind(
+			&entity.KindInfo{
+				ID:          "gif",
+				Description: "image",
+				PathSuffix:  ".gif",
+			},
+			nil, // could add a sanitizer here
+		),
+		entity.NewRawFileKind(
+			&entity.KindInfo{
+				ID:          "webp",
+				Description: "image",
+				PathSuffix:  ".webp",
+			},
+			nil, // could add a sanitizer here
+		),
+		entity.NewRawFileKind(
+			&entity.KindInfo{
+				ID:          "svg",
+				Description: "image",
+				PathSuffix:  ".svg",
+			},
+			nil, // could add a sanitizer here
+		),
+	)
+	if err != nil {
+		// panic?
+		fmt.Printf("ERROR: %+v\n", err)
+	}
+	srv := &StandardEntityStoreServer{
+		kinds: k,
+	}
+	return srv
+}
+
+func (s *StandardEntityStoreServer) GetKinds() entity.KindRegistry {
+	return s.kinds
+}
+
+func (s *StandardEntityStoreServer) GetEntity(_ context.Context, req *entity.GetEntityRequest) (*entity.EntityMessage, error) {
+	kind := s.kinds.GetFromSuffix(req.Path)
+	if kind == nil {
+		kind = entity.FolderKind
+	}
+
+	rsp := &entity.EntityMessage{
+		Path: req.Path,
+		Kind: kind.Info().ID,
+	}
+
+	if req.WithPayload {
+		rsp.Payload = []byte(`{"LOAD": "` + req.Path + `"}`)
+	}
+
+	if req.WithStorageMeta {
+		version := req.Version
+		if version == "" {
+			version = "aaa"
+		}
+
+		rsp.Meta = &entity.StorageMetadata{
+			CreatedAt: 1234,
+			UpdatedBy: "??",
+			Version:   version,
+		}
+	}
+	return rsp, nil
+}
+
+func (s *StandardEntityStoreServer) ListFolder(context.Context, *entity.ListFolderRequest) (*entity.FolderListing, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListFolder not implemented")
+}
+
+func (s *StandardEntityStoreServer) SaveEntity(context.Context, *entity.SaveEntityRequest) (*entity.EntityMessage, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SaveEntity not implemented")
+}
+
+func (s *StandardEntityStoreServer) DeleteEntity(context.Context, *entity.DeleteEntityRequest) (*entity.DeleteResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteEntity not implemented")
+}
+
+func (s *StandardEntityStoreServer) GetEntityHistory(_ context.Context, req *entity.GetHistoryRequest) (*entity.EntityHistoryResponse, error) {
+	rsp := &entity.EntityHistoryResponse{
+		Path:          req.Path,
+		NextPageToken: "dummyNextToken",
+	}
+	for i := 0; i < 10; i++ {
+		rsp.Event = append(rsp.Event, &entity.EntityHistory{
+			When:    int64(10000 + i + 6000),
+			Version: fmt.Sprintf("V%d", i),
+			Who:     "aaa",
+		})
+	}
+	return rsp, nil
+}
+
+func (s *StandardEntityStoreServer) CreatePR(context.Context, *entity.CreatePullRequest) (*entity.EntityMessage, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreatePR not implemented")
+}
+
+func (s *StandardEntityStoreServer) ListKinds(context.Context, *entity.ListKindsRequest) (*entity.ListKindsResponse, error) {
+	kinds := s.kinds.List()
+	rsp := &entity.ListKindsResponse{
+		Kinds: make([]*entity.KindInfo, len(kinds)),
+	}
+	for i, k := range kinds {
+		rsp.Kinds[i] = k.Info()
+	}
+	return rsp, nil
+}
+
+func (s *StandardEntityStoreServer) WatchEntity(*entity.GetEntityRequest, entity.EntityStore_WatchEntityServer) error {
+	return status.Errorf(codes.Unimplemented, "method WatchEntity not implemented")
+}
