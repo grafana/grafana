0e13ee345d28b17a3a93696b2edf0685a29b4028
Kristina <kristina.durivage@grafana.com>
2022-07-25 13:05:57 -0500

    Explore: Change datasourceName usage to datasourceUID or datasourceRef where appropriate (#52479)
    * First pass at using datasource UID when appropriate
    
    * Fix tests
    
    * be more lenient with lookup to accomodate different URLs
    
    * Make test setup get mock work like real datasource get
    
    * Fix the typing issue and remaining tests
    
    * Fix PR feedback

--- public/app/features/explore/spec/helper/setup.tsx
+++ public/app/features/explore/spec/helper/setup.tsx
@@ -58,9 +58,14 @@ export function setupExplore(options?: SetupOptions): {
     getInstanceSettings(ref: DataSourceRef) {
       return dsSettings.map((d) => d.settings).find((x) => x.name === ref || x.uid === ref || x.uid === ref.uid);
     },
-    get(name?: string | null, scopedVars?: ScopedVars): Promise<DataSourceApi> {
+    get(datasource?: string | DataSourceRef | null, scopedVars?: ScopedVars): Promise<DataSourceApi> {
+      const datasourceStr = typeof datasource === 'string';
       return Promise.resolve(
-        (name ? dsSettings.find((d) => d.api.name === name || d.api.uid === name) : dsSettings[0])!.api
+        (datasource
+          ? dsSettings.find((d) =>
+              datasourceStr ? d.api.name === datasource || d.api.uid === datasource : d.api.uid === datasource?.uid
+            )
+          : dsSettings[0])!.api
       );
     },
   } as any);
