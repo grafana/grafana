fff9fed15f6b6c29d036987512e661745664a92a
kitsuned <80879390+kitsuned@users.noreply.github.com>
2022-07-26 02:39:47 +0300

    MegaMenu: Fix broken hamburger toggle

--- public/app/core/components/MegaMenu/NavBarMenu.tsx
+++ public/app/core/components/MegaMenu/NavBarMenu.tsx
@@ -2,12 +2,13 @@ import { css } from '@emotion/css';
 import { useDialog } from '@react-aria/dialog';
 import { FocusScope } from '@react-aria/focus';
 import { OverlayContainer, useOverlay } from '@react-aria/overlays';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import CSSTransition from 'react-transition-group/CSSTransition';
 
 import { GrafanaTheme2, NavModelItem } from '@grafana/data';
 import { reportInteraction } from '@grafana/runtime';
 import { CustomScrollbar, Icon, IconButton, useTheme2 } from '@grafana/ui';
+import { useGrafana } from 'app/core/context/GrafanaContext';
 
 import { TOP_BAR_LEVEL_HEIGHT } from '../AppChrome/types';
 import { NavItem } from '../NavBar/NavBarMenu';
@@ -25,58 +26,76 @@ export interface Props {
 export function NavBarMenu({ activeItem, navItems, searchBarHidden, onClose }: Props) {
   const theme = useTheme2();
   const styles = getStyles(theme, searchBarHidden);
-  const animationSpeed = theme.transitions.duration.shortest;
+  const animationSpeed = theme.transitions.duration.shortest + 300;
   const animStyles = getAnimStyles(theme, animationSpeed);
+  const { chrome } = useGrafana();
+  const state = chrome.useState();
   const ref = useRef(null);
+  const backdropRef = useRef(null);
   const { dialogProps } = useDialog({}, ref);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onMenuClose = () => setIsOpen(false);
 
   const { overlayProps, underlayProps } = useOverlay(
     {
       isDismissable: true,
       isOpen: true,
-      onClose,
+      onClose: onMenuClose,
     },
     ref
   );
 
+  useEffect(() => {
+    if (state.megaMenuOpen) {
+      setIsOpen(true);
+    }
+  }, [state.megaMenuOpen]);
+
   return (
     <OverlayContainer>
-      <FocusScope contain autoFocus>
-        <CSSTransition appear={true} in={true} classNames={animStyles.overlay} timeout={animationSpeed}>
-          <div data-testid="navbarmenu" ref={ref} {...overlayProps} {...dialogProps} className={styles.container}>
-            <div className={styles.mobileHeader}>
-              <Icon name="bars" size="xl" />
-              <IconButton
-                aria-label="Close navigation menu"
-                name="times"
-                onClick={onClose}
-                size="xl"
-                variant="secondary"
-              />
-            </div>
-            <NavBarToggle
-              className={styles.menuCollapseIcon}
-              isExpanded={true}
-              onClick={() => {
-                reportInteraction('grafana_navigation_collapsed');
-                onClose();
-              }}
+      <CSSTransition
+        in={isOpen}
+        unmountOnExit={true}
+        classNames={animStyles.overlay}
+        timeout={animationSpeed}
+        onExited={onClose}
+      >
+        <div data-testid="navbarmenu" ref={ref} {...overlayProps} {...dialogProps} className={styles.container}>
+          <div className={styles.mobileHeader}>
+            <Icon name="bars" size="xl" />
+            <IconButton
+              aria-label="Close navigation menu"
+              name="times"
+              onClick={onMenuClose}
+              size="xl"
+              variant="secondary"
             />
+          </div>
+          <NavBarToggle
+            className={styles.menuCollapseIcon}
+            isExpanded={true}
+            onClick={() => {
+              reportInteraction('grafana_navigation_collapsed');
+              onMenuClose();
+            }}
+          />
+          <FocusScope contain autoFocus>
             <nav className={styles.content}>
               <CustomScrollbar hideHorizontalTrack>
                 <ul className={styles.itemList}>
                   {navItems.map((link) => (
-                    <NavItem link={link} onClose={onClose} activeItem={activeItem} key={link.text} />
+                    <NavItem link={link} onClose={onMenuClose} activeItem={activeItem} key={link.text} />
                   ))}
                 </ul>
               </CustomScrollbar>
             </nav>
-          </div>
-        </CSSTransition>
-        <CSSTransition appear={true} in={true} classNames={animStyles.backdrop} timeout={animationSpeed}>
-          <div className={styles.backdrop} {...underlayProps} />
-        </CSSTransition>
-      </FocusScope>
+          </FocusScope>
+        </div>
+      </CSSTransition>
+      <CSSTransition in={isOpen} unmountOnExit={true} classNames={animStyles.backdrop} timeout={animationSpeed}>
+        <div ref={backdropRef} className={styles.backdrop} {...underlayProps} />
+      </CSSTransition>
     </OverlayContainer>
   );
 }
@@ -109,6 +128,7 @@ const getStyles = (theme: GrafanaTheme2, searchBarHidden?: boolean) => {
       zIndex: theme.zIndex.navbarFixed - 1,
       position: 'fixed',
       top: topPosition,
+      backgroundColor: theme.colors.background.primary,
       boxSizing: 'content-box',
       [theme.breakpoints.up('md')]: {
         borderRight: `1px solid ${theme.colors.border.weak}`,
@@ -154,7 +174,7 @@ const getAnimStyles = (theme: GrafanaTheme2, animationDuration: number) => {
 
   const overlayTransition = {
     ...commonTransition,
-    transitionProperty: 'background-color, box-shadow, width',
+    transitionProperty: 'box-shadow, width',
     // this is needed to prevent a horizontal scrollbar during the animation on firefox
     '.scrollbar-view': {
       overflow: 'hidden !important',
@@ -167,7 +187,6 @@ const getAnimStyles = (theme: GrafanaTheme2, animationDuration: number) => {
   };
 
   const overlayOpen = {
-    backgroundColor: theme.colors.background.primary,
     boxShadow: theme.shadows.z3,
     width: '100%',
     [theme.breakpoints.up('md')]: {
@@ -178,10 +197,6 @@ const getAnimStyles = (theme: GrafanaTheme2, animationDuration: number) => {
   const overlayClosed = {
     boxShadow: 'none',
     width: 0,
-    [theme.breakpoints.up('md')]: {
-      backgroundColor: theme.colors.background.primary,
-      width: theme.spacing(7),
-    },
   };
 
   const backdropOpen = {
@@ -194,16 +209,16 @@ const getAnimStyles = (theme: GrafanaTheme2, animationDuration: number) => {
 
   return {
     backdrop: {
-      appear: css(backdropClosed),
-      appearActive: css(backdropTransition, backdropOpen),
-      appearDone: css(backdropOpen),
+      enter: css(backdropClosed),
+      enterActive: css(backdropTransition, backdropOpen),
+      enterDone: css(backdropOpen),
       exit: css(backdropOpen),
       exitActive: css(backdropTransition, backdropClosed),
     },
     overlay: {
-      appear: css(overlayClosed),
-      appearActive: css(overlayTransition, overlayOpen),
-      appearDone: css(overlayOpen),
+      enter: css(overlayClosed),
+      enterActive: css(overlayTransition, overlayOpen),
+      enterDone: css(overlayOpen),
       exit: css(overlayOpen),
       exitActive: css(overlayTransition, overlayClosed),
     },
