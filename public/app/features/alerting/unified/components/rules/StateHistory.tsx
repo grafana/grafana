650d95cf3d0ad578720b57e2239cbc66a3d76382
Gilles De Mey <gilles.de.mey@gmail.com>
2022-07-25 16:10:05 +0200

    Alerting: group alert state history by set of labels and allow filtering

--- public/app/features/alerting/unified/components/rules/StateHistory.tsx
+++ public/app/features/alerting/unified/components/rules/StateHistory.tsx
@@ -1,10 +1,10 @@
 import { css } from '@emotion/css';
-import { uniqueId } from 'lodash';
-import React, { FC } from 'react';
+import { groupBy } from 'lodash';
+import React, { FC, FormEvent, useCallback, useState } from 'react';
 
-import { AlertState, dateTimeFormat, GrafanaTheme } from '@grafana/data';
-import { Alert, LoadingPlaceholder, useStyles } from '@grafana/ui';
-import { StateHistoryItem, StateHistoryItemData } from 'app/types/unified-alerting';
+import { AlertState, dateTimeFormat, GrafanaTheme2 } from '@grafana/data';
+import { Alert, Field, Icon, Input, Label, LoadingPlaceholder, Stack, Tooltip, useStyles2 } from '@grafana/ui';
+import { StateHistoryItemData } from 'app/types/unified-alerting';
 import { GrafanaAlertStateWithReason, PromAlertingRuleState } from 'app/types/unified-alerting-dto';
 
 import { useManagedAlertStateHistory } from '../../hooks/useManagedAlertStateHistory';
@@ -19,7 +19,10 @@ type StateHistoryRowItem = {
   text?: string;
   data?: StateHistoryItemData;
   timestamp?: number;
+  stringifiedLabels: string;
 };
+
+type StateHistoryMap = Record<string, StateHistoryRowItem[]>;
 
 type StateHistoryRow = DynamicTableItemProps<StateHistoryRowItem>;
 
@@ -28,7 +31,14 @@ interface RuleStateHistoryProps {
 }
 
 const StateHistory: FC<RuleStateHistoryProps> = ({ alertId }) => {
+  const [textFilter, setTextFilter] = useState<string>('');
+  const handleTextFilter = useCallback((event: FormEvent<HTMLInputElement>) => {
+    setTextFilter(event.currentTarget.value);
+  }, []);
+
   const { loading, error, result = [] } = useManagedAlertStateHistory(alertId);
+
+  const styles = useStyles2(getStyles);
 
   if (loading && !error) {
     return <LoadingPlaceholder text={'Loading history...'} />;
@@ -44,30 +54,105 @@ const StateHistory: FC<RuleStateHistoryProps> = ({ alertId }) => {
     { id: 'timestamp', label: 'Time', size: 'max-content', renderCell: renderTimestampCell },
   ];
 
-  const items: StateHistoryRow[] = result
-    .reduce((acc: StateHistoryRowItem[], item, index) => {
-      acc.push({
-        id: String(item.id),
-        state: item.newState,
-        text: item.text,
-        data: item.data,
-        timestamp: item.updated,
-      });
+  const items: StateHistoryRowItem[] = result.reduce((acc: StateHistoryRowItem[], item) => {
+    // let's grab the last matching set of `{<string>}` since the alert name could also contain { or }
+    const LABELS_REGEX = /{.*?}/g;
+    const stringifiedLabels = item.text.match(LABELS_REGEX)?.at(-1) ?? '';
 
-      // if the preceding state is not the same, create a separate state entry – this likely means the state was reset
-      if (!hasMatchingPrecedingState(index, result)) {
-        acc.push({ id: uniqueId(), state: item.prevState });
-      }
+    acc.push({
+      id: String(item.id),
+      state: item.newState,
+      // let's omit the labels for each entry since it's just added noise to each state history item
+      text: item.text.replace(stringifiedLabels, ''),
+      data: item.data,
+      timestamp: item.updated,
+      stringifiedLabels,
+    });
 
-      return acc;
-    }, [])
-    .map((historyItem) => ({
-      id: historyItem.id,
-      data: historyItem,
-    }));
+    return acc;
+  }, []);
 
-  return <DynamicTable cols={columns} items={items} />;
+  // we have to group our state history items by their unique combination of tags since we want to display a DynamicTable for each alert instance
+  // (effectively unique combination of labels)
+  const groupedItems: StateHistoryMap = groupBy(items, (item) => item.stringifiedLabels);
+
+  const tables = Object.entries(groupedItems)
+    // sort and filter each table
+    .sort()
+    .filter(([groupKey]) => matchKey(groupKey, textFilter))
+    .map(([groupKey, items]) => {
+      const tableItems: StateHistoryRow[] = items.map((historyItem) => ({
+        id: historyItem.id,
+        data: historyItem,
+      }));
+
+      return (
+        <div key={groupKey}>
+          <header className={styles.tableGroupKey}>
+            <code>{groupKey}</code>
+          </header>
+          <DynamicTable cols={columns} items={tableItems} />
+        </div>
+      );
+    });
+
+  return (
+    <div>
+      <nav>
+        <Field
+          label={
+            <Label>
+              <Stack gap={0.5}>
+                <span>Filter group</span>
+                <Tooltip
+                  content={
+                    <div>
+                      Filter each state history group either by exact match or a regular expression, ex:{' '}
+                      <code>{`region=eu-west-1`}</code> or <code>{`/region=us-.+/`}</code>
+                    </div>
+                  }
+                >
+                  <Icon name="info-circle" size="sm" />
+                </Tooltip>
+              </Stack>
+            </Label>
+          }
+        >
+          <Input
+            prefix={<Icon name={'search'} />}
+            onChange={handleTextFilter}
+            placeholder="Search"
+            data-testid="search-query-input"
+          />
+        </Field>
+      </nav>
+      {tables}
+    </div>
+  );
 };
+
+// match a string either by exact text match or with regular expression when in the form of "/<regex>/"
+function matchKey(groupKey: string, textFilter: string) {
+  // if the text filter is empty we show all matches
+  if (textFilter === '') {
+    return true;
+  }
+
+  const isRegExp = textFilter.startsWith('/') && textFilter.endsWith('/');
+
+  // not a regular expression, use normal text matching
+  if (!isRegExp) {
+    return groupKey.includes(textFilter);
+  }
+
+  // regular expression, try parsing and applying
+  // when we fail to parse the text as a regular expression, we return no match
+  try {
+    return new RegExp(textFilter.slice(1, -1)).test(groupKey);
+  } catch (err) {
+    return false;
+  }
+}
 
 function renderValueCell(item: StateHistoryRow) {
   const matches = item.data.data?.evalMatches ?? [];
@@ -95,7 +180,7 @@ function renderTimestampCell(item: StateHistoryRow) {
 }
 
 const LabelsWrapper: FC<{}> = ({ children }) => {
-  const { wrapper } = useStyles(getStyles);
+  const { wrapper } = useStyles2(getStyles);
   return <div className={wrapper}>{children}</div>;
 };
 
@@ -105,25 +190,16 @@ const TimestampStyle = css`
   flex-direction: column;
 `;
 
-const getStyles = (theme: GrafanaTheme) => ({
+const getStyles = (theme: GrafanaTheme2) => ({
   wrapper: css`
     & > * {
-      margin-right: ${theme.spacing.xs};
+      margin-right: ${theme.spacing(1)};
     }
   `,
+  tableGroupKey: css`
+    margin-top: ${theme.spacing(2)};
+    margin-bottom: ${theme.spacing(2)};
+  `,
 });
-
-// this function will figure out if a given historyItem has a preceding historyItem where the states match - in other words
-// the newState of the previous historyItem is the same as the prevState of the current historyItem
-function hasMatchingPrecedingState(index: number, items: StateHistoryItem[]): boolean {
-  const currentHistoryItem = items[index];
-  const previousHistoryItem = items[index + 1];
-
-  if (!previousHistoryItem) {
-    return false;
-  }
-
-  return previousHistoryItem.newState === currentHistoryItem.prevState;
-}
 
 export { StateHistory };
