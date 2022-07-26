b7bda960d85a022b8a7ddcf2d484e4bd8c21ac0a
eleijonmarck <eric.leijonmarck@gmail.com>
2022-07-26 08:29:10 +0100

    Revert "added back pkg/services/login/authinfoservice/database/stats.go"
    This reverts commit 2ba6e3c4d602122bda86911c56934407904eb268.

--- pkg/cmd/grafana-cli/commands/conflict_user_command_test.go
+++ pkg/cmd/grafana-cli/commands/conflict_user_command_test.go
@@ -99,7 +99,7 @@ func TestMergeUser(t *testing.T) {
 			t.Logf("testing getting user")
 			query := &models.GetUserByIdQuery{Id: userWithUpperCase.ID}
 			err = sqlStore.GetUserById(context.Background(), query)
-			require.Error(t, user.ErrUserNotFound, err)
+			require.Error(t, models.ErrUserNotFound, err)
 
 			testUser := &models.SignedInUser{
 				OrgId: testOrgID,
