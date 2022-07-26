a52443ac98a99262d62e40956b6a5bfae59e3e20
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-07-26 14:03:59 +0400

    lint

--- pkg/services/thumbs/datasources_lookup_test.go
+++ pkg/services/thumbs/datasources_lookup_test.go
@@ -7,9 +7,6 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
-	ac "github.com/grafana/grafana/pkg/services/accesscontrol"
-	"github.com/grafana/grafana/pkg/services/dashboards"
-	"github.com/grafana/grafana/pkg/services/datasources"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/searchV2"
 	"github.com/stretchr/testify/mock"
@@ -21,63 +18,6 @@ var (
 	exampleListFrameJSON string
 	exampleListFrame     = &data.Frame{}
 	_                    = exampleListFrame.UnmarshalJSON([]byte(exampleListFrameJSON))
-
-	orgId                   = int64(1)
-	permissionsWithScopeAll = map[string][]string{
-		datasources.ActionIDRead:           {datasources.ScopeAll},
-		datasources.ActionDelete:           {datasources.ScopeAll},
-		ac.ActionDatasourcesExplore:        {datasources.ScopeAll},
-		datasources.ActionQuery:            {datasources.ScopeAll},
-		datasources.ActionRead:             {datasources.ScopeAll},
-		datasources.ActionWrite:            {datasources.ScopeAll},
-		datasources.ActionPermissionsRead:  {datasources.ScopeAll},
-		datasources.ActionPermissionsWrite: {datasources.ScopeAll},
-
-		dashboards.ActionFoldersCreate:           {dashboards.ScopeFoldersAll},
-		dashboards.ActionFoldersRead:             {dashboards.ScopeFoldersAll},
-		dashboards.ActionFoldersWrite:            {dashboards.ScopeFoldersAll},
-		dashboards.ActionFoldersDelete:           {dashboards.ScopeFoldersAll},
-		dashboards.ActionFoldersPermissionsRead:  {dashboards.ScopeFoldersAll},
-		dashboards.ActionFoldersPermissionsWrite: {dashboards.ScopeFoldersAll},
-
-		dashboards.ActionDashboardsCreate:           {dashboards.ScopeDashboardsAll},
-		dashboards.ActionDashboardsRead:             {dashboards.ScopeDashboardsAll},
-		dashboards.ActionDashboardsWrite:            {dashboards.ScopeDashboardsAll},
-		dashboards.ActionDashboardsDelete:           {dashboards.ScopeDashboardsAll},
-		dashboards.ActionDashboardsPermissionsRead:  {dashboards.ScopeDashboardsAll},
-		dashboards.ActionDashboardsPermissionsWrite: {dashboards.ScopeDashboardsAll},
-	}
-	permissionsWithUidScopes = map[string][]string{
-		datasources.ActionIDRead:    {},
-		datasources.ActionDelete:    {},
-		ac.ActionDatasourcesExplore: {},
-		datasources.ActionQuery:     {},
-		datasources.ActionRead: {
-			datasources.ScopeProvider.GetResourceScopeUID("datasource-2"),
-			datasources.ScopeProvider.GetResourceScopeUID("datasource-3"),
-		},
-		datasources.ActionWrite:            {},
-		datasources.ActionPermissionsRead:  {},
-		datasources.ActionPermissionsWrite: {},
-
-		dashboards.ActionFoldersCreate: {},
-		dashboards.ActionFoldersRead: {
-			dashboards.ScopeFoldersProvider.GetResourceScopeUID("ujaM1h6nz"),
-		},
-		dashboards.ActionFoldersWrite:            {},
-		dashboards.ActionFoldersDelete:           {},
-		dashboards.ActionFoldersPermissionsRead:  {},
-		dashboards.ActionFoldersPermissionsWrite: {},
-
-		dashboards.ActionDashboardsCreate: {},
-		dashboards.ActionDashboardsRead:   {},
-		dashboards.ActionDashboardsWrite: {
-			dashboards.ScopeDashboardsProvider.GetResourceScopeUID("7MeksYbmk"),
-		},
-		dashboards.ActionDashboardsDelete:           {},
-		dashboards.ActionDashboardsPermissionsRead:  {},
-		dashboards.ActionDashboardsPermissionsWrite: {},
-	}
 )
 
 func TestShouldParseUidFromSearchResponseFrame(t *testing.T) {
