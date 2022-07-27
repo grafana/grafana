7712a75a00521c38fb2d8e4099dc89ce5c147839
Kyle Brandt <kyle@grafana.com>
2022-07-27 13:13:23 -0400

    pass service not nil

--- pkg/services/alerting/test_rule.go
+++ pkg/services/alerting/test_rule.go
@@ -32,7 +32,7 @@ func (e *AlertEngine) AlertTest(orgID int64, dashboard *simplejson.Json, panelID
 
 		handler := NewEvalHandler(e.DataService)
 
-		context := NewEvalContext(context.Background(), rule, fakeRequestValidator{}, e.SQLStore, nil, nil)
+		context := NewEvalContext(context.Background(), rule, fakeRequestValidator{}, e.SQLStore, nil, e.datasourceService)
 		context.IsTestRun = true
 		context.IsDebug = true
 
