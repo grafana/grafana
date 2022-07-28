3fafae0f878f9a88ce712a4fb425fe862b5cc7ec
drew08t <drew08@gmail.com>
2022-07-28 09:36:58 -0700

    Move measure styles to global styles

--- public/app/plugins/panel/geomap/components/MeasureOverlay.tsx
+++ public/app/plugins/panel/geomap/components/MeasureOverlay.tsx
@@ -5,13 +5,14 @@ import { Draw, Modify } from 'ol/interaction';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
 import { getArea, getLength } from 'ol/sphere';
-import { Circle as CircleStyle, Fill, RegularShape, Stroke, Style, Text } from 'ol/style';
 import React, { useState } from 'react';
 import tinycolor from 'tinycolor2';
 
 import { GrafanaTheme } from '@grafana/data';
 import { IconButton, RadioButtonGroup, stylesFactory } from '@grafana/ui';
 import { config } from 'app/core/config';
+
+import { style, labelStyle, tipStyle, modifyStyle, segmentStyle } from '../globalStyles';
 
 type Props = {
   map: Map;
@@ -91,115 +92,6 @@ export const MeasureOverlay = ({ map, menuActiveState }: Props) => {
     </div>
   );
 };
-
-// Open Layer styles
-const style = new Style({
-  fill: new Fill({
-    color: 'rgba(255, 255, 255, 0.2)',
-  }),
-  stroke: new Stroke({
-    color: 'rgba(0, 0, 0, 0.5)',
-    lineDash: [10, 10],
-    width: 2,
-  }),
-  image: new CircleStyle({
-    radius: 5,
-    stroke: new Stroke({
-      color: 'rgba(0, 0, 0, 0.7)',
-    }),
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 0.2)',
-    }),
-  }),
-});
-
-const labelStyle = new Style({
-  text: new Text({
-    font: '14px Calibri,sans-serif',
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
-    }),
-    backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.7)',
-    }),
-    padding: [3, 3, 3, 3],
-    textBaseline: 'bottom',
-    offsetY: -15,
-  }),
-  image: new RegularShape({
-    radius: 8,
-    points: 3,
-    angle: Math.PI,
-    displacement: [0, 10],
-    fill: new Fill({
-      color: 'rgba(0, 0, 0, 0.7)',
-    }),
-  }),
-});
-
-const tipStyle = new Style({
-  text: new Text({
-    font: '12px Calibri,sans-serif',
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
-    }),
-    backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
-    }),
-    padding: [2, 2, 2, 2],
-    textAlign: 'left',
-    offsetX: 15,
-  }),
-});
-
-const modifyStyle = new Style({
-  image: new CircleStyle({
-    radius: 5,
-    stroke: new Stroke({
-      color: 'rgba(0, 0, 0, 0.7)',
-    }),
-    fill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
-    }),
-  }),
-  text: new Text({
-    text: 'Drag to modify',
-    font: '12px Calibri,sans-serif',
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
-    }),
-    backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.7)',
-    }),
-    padding: [2, 2, 2, 2],
-    textAlign: 'left',
-    offsetX: 15,
-  }),
-});
-
-const segmentStyle = new Style({
-  text: new Text({
-    font: '12px Calibri,sans-serif',
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
-    }),
-    backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
-    }),
-    padding: [2, 2, 2, 2],
-    textBaseline: 'bottom',
-    offsetY: -12,
-  }),
-  image: new RegularShape({
-    radius: 6,
-    points: 3,
-    angle: Math.PI,
-    displacement: [0, 8],
-    fill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
-    }),
-  }),
-});
 
 const segmentStyles = [segmentStyle];
 const source = new VectorSource();
