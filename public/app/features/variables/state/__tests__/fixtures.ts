f18e5c36fbfceaaf65a10cba1a44ca8af55cbb81
joshhunt <josh@trtr.co>
2022-07-30 15:24:46 +0100

    Add fixture-factories for all variable types

--- public/app/features/variables/state/__tests__/fixtures.ts
+++ public/app/features/variables/state/__tests__/fixtures.ts
@@ -1,9 +1,16 @@
 import {
+  AdHocVariableModel,
   BaseVariableModel,
   ConstantVariableModel,
   CustomVariableModel,
+  DashboardVariableModel,
+  DataSourceVariableModel,
+  IntervalVariableModel,
   LoadingState,
+  OrgVariableModel,
   QueryVariableModel,
+  TextBoxVariableModel,
+  UserVariableModel,
   VariableHide,
   VariableOption,
   VariableRefresh,
@@ -55,7 +62,20 @@ export function createQueryVariable(input: Partial<QueryVariableModel> = {}): Qu
   };
 }
 
-export function createConstantVariable(input: Partial<ConstantVariableModel>): ConstantVariableModel {
+export function createAdhocVariable(input?: Partial<AdHocVariableModel>): AdHocVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'adhoc',
+    datasource: {
+      uid: 'abc-123',
+      type: 'prometheus',
+    },
+    filters: [],
+    ...input,
+  };
+}
+
+export function createConstantVariable(input: Partial<ConstantVariableModel> = {}): ConstantVariableModel {
   return {
     ...createBaseVariableModel(),
     type: 'constant',
@@ -67,7 +87,92 @@ export function createConstantVariable(input: Partial<ConstantVariableModel>): C
   };
 }
 
-export function createCustomVariable(input: Partial<CustomVariableModel>): CustomVariableModel {
+export function createDatasourceVariable(input: Partial<DataSourceVariableModel> = {}): DataSourceVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'datasource',
+    regex: '',
+    refresh: VariableRefresh.onDashboardLoad,
+    multi: false,
+    includeAll: false,
+    query: '',
+    current: createVariableOption('prom-prod', 'Prometheus (main)', true),
+    options: [createVariableOption('prom-prod', 'Prometheus (main)', true), createVariableOption('prom-dev')],
+    ...input,
+  };
+}
+
+export function createIntervalVariable(input: Partial<IntervalVariableModel> = {}): IntervalVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'interval',
+    auto: false,
+    auto_count: 30,
+    auto_min: '10s',
+    refresh: VariableRefresh.onTimeRangeChanged,
+    query: '1m,10m,30m,1h,6h,12h,1d,7d,14d,30d',
+    options: [],
+    current: createVariableOption('10m'),
+    ...input,
+  };
+}
+
+export function createTextBoxVariable(input: Partial<TextBoxVariableModel> = {}): TextBoxVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'textbox',
+    originalQuery: null,
+    query: '',
+    current: createVariableOption('prom-prod'),
+    options: [],
+    ...input,
+  };
+}
+
+export function createUserVariable(input: Partial<UserVariableModel> = {}): UserVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'system',
+    current: {
+      value: {
+        login: 'biggus-chungus',
+        id: 0,
+        email: 'chungus@example.com',
+      },
+    },
+    ...input,
+  };
+}
+
+export function createOrgVariable(input: Partial<OrgVariableModel> = {}): OrgVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'system',
+    current: {
+      value: {
+        name: 'Big Chungus Corp.',
+        id: 3,
+      },
+    },
+    ...input,
+  };
+}
+
+export function createDashboardVariable(input: Partial<DashboardVariableModel> = {}): DashboardVariableModel {
+  return {
+    ...createBaseVariableModel(),
+    type: 'system',
+    current: {
+      value: {
+        name: 'Chungus Monitoring',
+        uid: 'b1g',
+      },
+    },
+    ...input,
+  };
+}
+
+export function createCustomVariable(input: Partial<CustomVariableModel> = {}): CustomVariableModel {
   return {
     ...createBaseVariableModel(),
     type: 'custom',
