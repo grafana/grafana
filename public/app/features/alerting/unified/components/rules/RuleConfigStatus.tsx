68f86f5671c23f1e3c65d5b8b334d586a41fc3e4
Konrad Lalik <konrad.lalik@grafana.com>
2022-08-01 13:36:08 +0200

    Fix evaluation interval parsing

--- public/app/features/alerting/unified/components/rules/RuleConfigStatus.tsx
+++ public/app/features/alerting/unified/components/rules/RuleConfigStatus.tsx
@@ -14,6 +14,7 @@ interface RuleConfigStatusProps {
 
 export function RuleConfigStatus({ rule }: RuleConfigStatusProps) {
   const styles = useStyles2(getStyles);
+
   const { exceedsLimit } = checkEvaluationIntervalGlobalLimit(rule.group.interval ?? '');
 
   if (!exceedsLimit) {
