48a463d9a2f6b44bd487077754eca4976d831d51
Ashley Harrison <ashley.harrison@grafana.com>
2022-08-01 15:16:19 +0100

    remove UseState + add controls support in TimeRangeInput story

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangeInput.story.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangeInput.story.tsx
@@ -1,97 +1,60 @@
 import { action } from '@storybook/addon-actions';
+import { useArgs } from '@storybook/client-api';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import { dateTime, DefaultTimeZone, TimeRange, TimeZone } from '@grafana/data';
+import { dateTime, DefaultTimeZone } from '@grafana/data';
 import { TimeRangeInput } from '@grafana/ui';
 
-import { UseState } from '../../utils/storybook/UseState';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 
-import { TimeRangeInputProps } from './TimeRangeInput';
 import mdx from './TimeRangeInput.mdx';
+
+const now = dateTime(Date.now());
 
 const meta: ComponentMeta<typeof TimeRangeInput> = {
   title: 'Pickers and Editors/TimePickers/TimeRangeInput',
   component: TimeRangeInput,
   decorators: [withCenteredStory],
   parameters: {
+    controls: {
+      exclude: ['onChange', 'onChangeTimeZone'],
+    },
     docs: {
       page: mdx,
     },
   },
+  args: {
+    value: {
+      from: now.subtract(6, 'h'),
+      to: now,
+      raw: {
+        from: 'now-6h',
+        to: 'now',
+      },
+    },
+    timeZone: DefaultTimeZone,
+  },
 };
 
-interface State {
-  value: TimeRange;
-  timeZone: TimeZone;
-}
-
-const getComponentWithState = (initialState: State, props: TimeRangeInputProps) => (
-  <UseState initialState={initialState}>
-    {(state, updateValue) => {
-      return (
-        <TimeRangeInput
-          {...props}
-          value={state.value}
-          timeZone={state.timeZone}
-          onChange={(value) => {
-            action('onChange fired')(value);
-            updateValue({
-              ...state,
-              value,
-            });
-          }}
-          onChangeTimeZone={(timeZone) => {
-            action('onChangeTimeZone fired')(timeZone);
-            updateValue({
-              ...state,
-              timeZone,
-            });
-          }}
-        />
-      );
-    }}
-  </UseState>
-);
-
-export const Relative: ComponentStory<typeof TimeRangeInput> = (props) => {
-  const to = dateTime();
-  const from = to.subtract(6, 'h');
-
-  return getComponentWithState(
-    {
-      value: {
-        from,
-        to,
-        raw: {
-          from: 'now-6h',
-          to: 'now',
-        },
-      },
-      timeZone: DefaultTimeZone,
-    },
-    props
-  );
-};
-
-export const Absolute: ComponentStory<typeof TimeRangeInput> = (props) => {
-  const to = dateTime();
-  const from = to.subtract(6, 'h');
-
-  return getComponentWithState(
-    {
-      value: {
-        from,
-        to,
-        raw: {
-          from,
-          to,
-        },
-      },
-      timeZone: DefaultTimeZone,
-    },
-    props
+export const Basic: ComponentStory<typeof TimeRangeInput> = (args) => {
+  const [, updateArgs] = useArgs();
+  return (
+    <TimeRangeInput
+      {...args}
+      onChange={(value) => {
+        action('onChange fired')(value);
+        updateArgs({
+          value,
+        });
+      }}
+      onChangeTimeZone={(timeZone) => {
+        action('onChangeTimeZone fired')(timeZone);
+        updateArgs({
+          timeZone,
+        });
+      }}
+    />
   );
 };
 
