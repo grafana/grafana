c21bea49aa13da475e9de0a62140efc371774162
Kevin Minehart <kmineh0151@gmail.com>
2022-08-01 16:25:41 -0500

    move grabpl build-frontend

--- pkg/build/cmd/buildfrontend.go
+++ pkg/build/cmd/buildfrontend.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"strings"
 
-	"github.com/grafana/grafana/pkg/build/config"
-	"github.com/grafana/grafana/pkg/build/gitutil"
+	"github.com/grafana/grafana/pkg/build/errutil"
+	"github.com/grafana/grafana/pkg/build/frontend"
+	"github.com/grafana/grafana/pkg/build/syncutil"
 	"github.com/urfave/cli/v2"
 )
 
@@ -20,42 +16,23 @@ func BuildFrontend(c *cli.Context) error {
 		version = strings.TrimPrefix(c.Args().Get(0), "v")
 	}
 
-	metadata, err := config.GetMetadata(filepath.Join("dist", "version.json"))
+	cfg, mode, err := frontend.GetConfig(c, version)
 	if err != nil {
 		return err
 	}
 
-	edition := config.Edition(c.String("edition"))
-	cfg := config.Config{
-		GitHubToken: c.String("github-token"),
-	}
-	ctx := context.Background()
-	ghClient := gitutil.NewGitHubClient(ctx, cfg)
+	p := syncutil.NewWorkerPool(cfg.NumWorkers)
+	defer p.Close()
 
-	const grafanaDir = "."
-	if version == "" {
-		buildID := c.String("build-id")
-		var err error
-		version, err = config.GetGrafanaVersion(buildID, grafanaDir)
-		if err != nil {
-			return err
-		}
+	g, _ := errutil.GroupWithContext(c.Context)
+	if err := frontend.Build(mode, frontend.GrafanaDir, p, g); err != nil {
+		return err
 	}
-	verMode := metadata.ReleaseMode
-
-	if edition == config.EditionEnterprise || edition == config.EditionEnterprise2 {
-		if err := gitutil.PullEnterpriseBits(ctx, ghClient, cfg, grafanaDir, version, verMode); err != nil {
-			return cli.Exit(err.Error(), 1)
-		}
+	if err := g.Wait(); err != nil {
+		return err
 	}
 
-	log.Printf("Running front-end test suite")
-	cmd := exec.Command("yarn", "run", "ci:test-frontend")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		return cli.Exit(fmt.Sprintf("go test failed: %s", err), 1)
-	}
+	log.Println("Successfully built Grafana front-end!")
 
 	return nil
 }
