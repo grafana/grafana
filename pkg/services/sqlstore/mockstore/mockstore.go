cbba769eaba13a2ef7398421a940d80ae4c572e2
Ida Furjesova <ida.furjesova@grafana.com>
2022-08-03 15:32:04 +0200

    Remove GetUserById from store interface

--- pkg/services/sqlstore/mockstore/mockstore.go
+++ pkg/services/sqlstore/mockstore/mockstore.go
@@ -137,11 +137,6 @@ func (m *SQLStoreMock) CreateUser(ctx context.Context, cmd user.CreateUserComman
 	return nil, m.ExpectedError
 }
 
-func (m *SQLStoreMock) GetUserById(ctx context.Context, query *models.GetUserByIdQuery) error {
-	query.Result = m.ExpectedUser
-	return m.ExpectedError
-}
-
 func (m *SQLStoreMock) GetUserByLogin(ctx context.Context, query *models.GetUserByLoginQuery) error {
 	query.Result = m.ExpectedUser
 	return m.ExpectedError
