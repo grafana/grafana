037f682c8c4eacf220fca4976bd9538a87c44cee
Hamas Shafiq <hamas.shafiq@grafana.com>
2022-08-08 15:59:35 +0100

    Chore: Extract inline CSS into getStyles

--- public/app/plugins/datasource/jaeger/components/QueryEditor.tsx
+++ public/app/plugins/datasource/jaeger/components/QueryEditor.tsx
@@ -1,8 +1,8 @@
 import { css } from '@emotion/css';
 import React from 'react';
 
-import { QueryEditorProps } from '@grafana/data';
-import { FileDropzone, InlineField, InlineFieldRow, QueryField, RadioButtonGroup, useTheme2 } from '@grafana/ui';
+import { GrafanaTheme2, QueryEditorProps } from '@grafana/data';
+import { FileDropzone, InlineField, InlineFieldRow, QueryField, RadioButtonGroup, useStyles2 } from '@grafana/ui';
 
 import { JaegerDatasource } from '../datasource';
 import { JaegerQuery, JaegerQueryType } from '../types';
@@ -12,8 +12,7 @@ import { SearchForm } from './SearchForm';
 type Props = QueryEditorProps<JaegerDatasource, JaegerQuery>;
 
 export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) {
-  const theme = useTheme2();
-  const styles = getStyles();
+  const styles = useStyles2(getStyles);
 
   const onChangeQuery = (value: string) => {
     const nextQuery: JaegerQuery = { ...query, query: value };
@@ -26,7 +25,7 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
         return <SearchForm datasource={datasource} query={query} onChange={onChange} />;
       case 'upload':
         return (
-          <div className={css({ padding: theme.spacing(2) })}>
+          <div className={styles.fileDropzoneContainer}>
             <FileDropzone
               options={{ multiple: false }}
               onLoad={(result) => {
@@ -56,7 +55,7 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
 
   return (
     <>
-      <div className={styles.container}>
+      <div className={styles.fileDropzoneContainer}>
         <InlineFieldRow>
           <InlineField label="Query type">
             <RadioButtonGroup<JaegerQueryType>
@@ -82,8 +81,11 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
   );
 }
 
-const getStyles = () => ({
+const getStyles = (theme: GrafanaTheme2) => ({
   container: css`
     width: 100%;
+  `,
+  fileDropzoneContainer: css`
+    padding: ${theme.spacing(2)};
   `,
 });
