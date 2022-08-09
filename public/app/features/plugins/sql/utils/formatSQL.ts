39dbe3d7bf49f8fbbafffb0d126db68b0b27d3de
Zoltán Bedi <zoltan.bedi@gmail.com>
2022-08-09 18:45:56 +0200

    Revert "sql plugins - angular to react - base sql datasource (#51655)"
    This reverts commit fa560d96b6768596d430f3f1d5ed966b06452237.

--- public/app/features/plugins/sql/utils/formatSQL.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import sqlFormatter from 'sql-formatter-plus';
-
-export function formatSQL(q: string) {
-  return sqlFormatter.format(q).replace(/(\$ \{ .* \})|(\$ __)|(\$ \w+)/g, (m: string) => {
-    return m.replace(/\s/g, '');
-  });
-}
