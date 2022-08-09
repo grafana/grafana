ff808e8989f6e7f8000cfb4dc1eec4f6e6464a99
Guilherme Caulada <guilherme.caulada@grafana.com>
2022-08-09 15:43:16 -0300

    Remove unrelated opentsdb change on grafana-cli wire

--- pkg/cmd/grafana-cli/runner/wire.go
+++ pkg/cmd/grafana-cli/runner/wire.go
@@ -129,7 +129,6 @@ import (
 	"github.com/grafana/grafana/pkg/tsdb/loki"
 	"github.com/grafana/grafana/pkg/tsdb/mssql"
 	"github.com/grafana/grafana/pkg/tsdb/mysql"
-	"github.com/grafana/grafana/pkg/tsdb/opentsdb"
 	"github.com/grafana/grafana/pkg/tsdb/postgres"
 	"github.com/grafana/grafana/pkg/tsdb/prometheus"
 	"github.com/grafana/grafana/pkg/tsdb/tempo"
@@ -319,7 +318,6 @@ var wireSet = wire.NewSet(
 	wire.Bind(new(sqlstore.Store), new(*sqlstore.SQLStore)),
 	wire.Bind(new(db.DB), new(*sqlstore.SQLStore)),
 	prefimpl.ProvideService,
-	opentsdb.ProvideService,
 )
 
 func Initialize(cfg *setting.Cfg) (Runner, error) {
