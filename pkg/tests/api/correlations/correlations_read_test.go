862ba8a7a096590b2e2243c5d74809906c39446a
Elfo404 <me@giordanoricci.com>
2022-08-11 19:43:19 +0100

    Fix build error

--- pkg/tests/api/correlations/correlations_read_test.go
+++ pkg/tests/api/correlations/correlations_read_test.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/correlations"
 	"github.com/grafana/grafana/pkg/services/datasources"
+	"github.com/grafana/grafana/pkg/services/org"
 	"github.com/grafana/grafana/pkg/services/user"
 	"github.com/stretchr/testify/require"
 )
@@ -30,12 +30,12 @@ func TestIntegrationReadCorrelation(t *testing.T) {
 	}
 
 	ctx.createUser(user.CreateUserCommand{
-		DefaultOrgRole: string(models.ROLE_VIEWER),
+		DefaultOrgRole: string(org.RoleViewer),
 		Password:       viewerUser.password,
 		Login:          viewerUser.username,
 	})
 	ctx.createUser(user.CreateUserCommand{
-		DefaultOrgRole: string(models.ROLE_ADMIN),
+		DefaultOrgRole: string(org.RoleAdmin),
 		Password:       adminUser.password,
 		Login:          adminUser.username,
 	})
