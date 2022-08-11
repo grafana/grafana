ae5479e4a2609e83f91b7bc013fef46c3f07ecee
Ashley Harrison <ashley.harrison@grafana.com>
2022-08-11 08:47:15 +0100

    slightly cleaner...

--- packages/grafana-ui/src/components/ToolbarButton/ToolbarButtonRow.tsx
+++ packages/grafana-ui/src/components/ToolbarButton/ToolbarButtonRow.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from '@emotion/css';
-import React, { forwardRef, HTMLAttributes, useState, useRef, useLayoutEffect, useMemo, ReactNode } from 'react';
+import React, { forwardRef, HTMLAttributes, useState, useRef, useLayoutEffect, useMemo } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 
@@ -14,66 +14,56 @@ const gap = 8;
 
 export const ToolbarButtonRow = forwardRef<HTMLDivElement, Props>(({ className, children, ...rest }, ref) => {
   const styles = useStyles2(getStyles);
-  const [showMoreVisible, setShowMoreVisible] = useState(false);
   const visibleRef = useRef<HTMLDivElement>(null);
-  const ref2 = useRef<HTMLDivElement>(null);
-  const [visibleChildren, setVisibleChildren] = useState<ReactNode[]>(React.Children.toArray(children));
-  const [overflowChildren, setOverflowChildren] = useState<ReactNode[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [numVisibleChildren, setNumVisibleChildren] = useState(React.Children.toArray(children).length);
   const [childWidths, setChildWidths] = useState<number[]>([]);
 
   const resizeObserver = useMemo(
     () =>
-      new ResizeObserver((entries) => {
-        for (let entry of entries) {
-          if (visibleRef.current && ref2.current) {
-            if (childWidths.length === 0) {
-              setChildWidths(Array.from(visibleRef.current.children).map((child) => child.clientWidth));
-            }
-            const { clientWidth, scrollWidth, childElementCount } = visibleRef.current;
-            const { clientWidth: clientWidth2 } = ref2.current;
-            let newVisibleChildren = visibleChildren.slice();
-            let newOverflowChildren = overflowChildren.slice();
-            let newWidth = scrollWidth;
+      new ResizeObserver(() => {
+        if (visibleRef.current && containerRef.current) {
+          if (childWidths.length === 0) {
+            setChildWidths(Array.from(visibleRef.current.children).map((child) => child.clientWidth));
+          }
+          const { clientWidth, scrollWidth } = visibleRef.current;
+          const { clientWidth: containerWidth } = containerRef.current;
+          let newWidth = scrollWidth;
+          let newVisibleChildren = numVisibleChildren;
 
-            if (newWidth > clientWidth2) {
-              // move children from visible to overflow until newWidth is less than clientWidth
-              for (let i = childElementCount - 1; i >= 0; i--) {
-                if (newWidth > clientWidth) {
-                  // remove children until we are below the client width
-                  newVisibleChildren = React.Children.toArray(children).slice(0, i);
-                  newOverflowChildren = React.Children.toArray(children).slice(i);
-                  newWidth -= visibleRef.current.children[i].clientWidth + gap;
-                } else {
-                  break;
-                }
-              }
-            } else if (newWidth < clientWidth2 && overflowChildren.length > 0) {
-              // see if we can move children from overflow to visible without exceeding clientWidth
-              for (let i = 0; i < overflowChildren.length; i++) {
-                newWidth += childWidths[visibleChildren.length + i] + gap;
-                if (newWidth <= clientWidth2) {
-                  const childToMove = newOverflowChildren.shift();
-                  newVisibleChildren.push(childToMove);
-                } else {
-                  break;
-                }
+          if (newWidth > containerWidth) {
+            for (let i = numVisibleChildren - 1; i >= 0; i--) {
+              if (newWidth > clientWidth) {
+                // remove children until we are below the client width
+                newWidth -= visibleRef.current.children[i].clientWidth + gap;
+              } else {
+                newVisibleChildren = i + 1;
+                break;
               }
             }
-
-            if (newVisibleChildren.length !== visibleChildren.length) {
-              setVisibleChildren(newVisibleChildren);
-              setOverflowChildren(newOverflowChildren);
-              setShowMoreVisible(newOverflowChildren.length > 0);
+          } else if (newWidth < containerWidth && numVisibleChildren < React.Children.toArray(children).length) {
+            // see if we can move children from overflow to visible without exceeding clientWidth
+            for (let i = numVisibleChildren; i < React.Children.toArray(children).length; i++) {
+              newWidth += childWidths[i] + gap;
+              if (newWidth > containerWidth) {
+                break;
+              } else {
+                newVisibleChildren = i + 1;
+              }
             }
+          }
+
+          if (newVisibleChildren !== numVisibleChildren) {
+            setNumVisibleChildren(newVisibleChildren);
           }
         }
       }),
-    [childWidths, children, overflowChildren, visibleChildren]
+    [childWidths, children, numVisibleChildren]
   );
 
   useLayoutEffect(() => {
-    if (ref2.current) {
-      resizeObserver.observe(ref2.current);
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
     }
 
     return () => {
@@ -82,13 +72,17 @@ export const ToolbarButtonRow = forwardRef<HTMLDivElement, Props>(({ className, 
   }, [resizeObserver]);
 
   return (
-    <div ref={ref2} className={cx(styles.wrapper, className)} {...rest}>
-      <div className={styles.visibleItems} ref={visibleRef}>
-        {visibleChildren}
+    <>
+      <div ref={containerRef} className={cx(styles.wrapper, className)} {...rest}>
+        <div className={styles.visibleItems} ref={visibleRef}>
+          {React.Children.toArray(children).slice(0, numVisibleChildren)}
+        </div>
+        {/* {overflowChildren} */}
       </div>
-      {showMoreVisible && <ToolbarButton icon="ellipsis-v" iconOnly narrow />}
-      {/* {overflowChildren} */}
-    </div>
+      {numVisibleChildren < React.Children.toArray(children).length && (
+        <ToolbarButton icon="ellipsis-v" iconOnly narrow />
+      )}
+    </>
   );
 });
 
