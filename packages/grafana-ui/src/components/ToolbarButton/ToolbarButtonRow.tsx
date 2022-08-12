f670f5c6e1d39b1dfb6c1acc39f8dfb46044e11d
Ashley Harrison <ashley.harrison@grafana.com>
2022-08-12 11:31:23 +0100

    much cleaner implementation with intersectionobserver

--- packages/grafana-ui/src/components/ToolbarButton/ToolbarButtonRow.tsx
+++ packages/grafana-ui/src/components/ToolbarButton/ToolbarButtonRow.tsx
@@ -1,9 +1,9 @@
 import { css, cx } from '@emotion/css';
-import React, { forwardRef, HTMLAttributes, useState, useRef, useLayoutEffect, useMemo } from 'react';
+import React, { forwardRef, HTMLAttributes, useState, useRef, useLayoutEffect } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 
-import { useStyles2 } from '../../themes';
+import { useTheme2 } from '../../themes';
 import { Dropdown } from '../Dropdown/Dropdown';
 
 import { ToolbarButton } from './ToolbarButton';
@@ -11,106 +11,74 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
-const gap = 8;
-const overflowButtonWidth = 28;
-const fudgeFactor = 3;
-
 export const ToolbarButtonRow = forwardRef<HTMLDivElement, Props>(({ className, children, ...rest }, ref) => {
-  const styles = useStyles2(getStyles);
-  const innerRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
-  const [numVisibleChildren, setNumVisibleChildren] = useState(React.Children.toArray(children).length);
-  const [childWidths, setChildWidths] = useState<number[]>([]);
-
-  const resizeObserver = useMemo(
-    () =>
-      new ResizeObserver((entries) => {
-        window.requestAnimationFrame(() => {
-          // prevent an infinite loop when the children change
-          if (!Array.isArray(entries) || !entries.length) {
-            return;
-          }
-          if (innerRef.current && containerRef.current) {
-            if (childWidths.length === 0) {
-              setChildWidths(
-                Array.from(innerRef.current.children).map((child) => {
-                  return child instanceof HTMLElement ? child.offsetWidth : 0;
-                })
-              );
-            }
-            const { offsetWidth, scrollWidth } = innerRef.current;
-            const { clientWidth: containerWidth } = containerRef.current;
-            let newWidth = scrollWidth;
-            let newVisibleChildren = numVisibleChildren;
-
-            if (newWidth > containerWidth + fudgeFactor) {
-              // add the overflow button
-              newWidth += overflowButtonWidth + gap;
-              for (let i = numVisibleChildren - 1; i >= 0; i--) {
-                if (newWidth > offsetWidth) {
-                  // remove children until we are below the client width
-                  newWidth -= childWidths[i] + gap;
-                } else {
-                  newVisibleChildren = i + 1;
-                  break;
-                }
-              }
-            } else if (newWidth < containerWidth && numVisibleChildren < React.Children.toArray(children).length) {
-              // see if we can move children from overflow to visible without exceeding clientWidth
-              for (let i = numVisibleChildren; i < React.Children.toArray(children).length; i++) {
-                newWidth += childWidths[i] + gap;
-                // remove overflow button if we're adding the last child back
-                if (i === React.Children.toArray(children).length - 1) {
-                  newWidth -= overflowButtonWidth + gap;
-                }
-                if (newWidth > containerWidth) {
-                  break;
-                } else {
-                  newVisibleChildren = i + 1;
-                }
-              }
-            }
-
-            if (newVisibleChildren !== numVisibleChildren) {
-              setNumVisibleChildren(newVisibleChildren);
-            }
-          }
-        });
-      }),
-    [childWidths, children, numVisibleChildren]
+  const [childVisibility, setChildVisibility] = useState<boolean[]>(
+    Array(React.Children.toArray(children).length).fill(true)
   );
+  const theme = useTheme2();
+  const styles = getStyles(theme, childVisibility.indexOf(false) - 1);
 
   useLayoutEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.target instanceof HTMLElement && entry.target.parentNode) {
+            const index = Array.prototype.indexOf.call(entry.target.parentNode.children, entry.target);
+            setChildVisibility((prev) => {
+              const newVisibility = [...prev];
+              newVisibility[index] = entry.isIntersecting;
+              return newVisibility;
+            });
+          }
+        });
+      },
+      {
+        threshold: 1,
+        root: containerRef.current,
+      }
+    );
     if (containerRef.current) {
-      resizeObserver.observe(containerRef.current);
+      Array.from(containerRef.current.children).forEach((item) => {
+        intersectionObserver.observe(item);
+      });
     }
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [resizeObserver]);
+    return () => intersectionObserver.disconnect();
+  }, []);
 
   const renderOverflowChildren = () => (
-    <div className={styles.overflowItems}>{React.Children.toArray(children).slice(numVisibleChildren)}</div>
+    <div className={styles.overflowItems}>
+      {React.Children.toArray(children).map((child, index) => !childVisibility[index] && child)}
+    </div>
   );
 
   return (
     <div ref={containerRef} className={cx(styles.wrapper, className)} {...rest}>
-      <div className={styles.visibleItems} ref={innerRef}>
-        {React.Children.toArray(children).slice(0, numVisibleChildren)}
-        {numVisibleChildren < React.Children.toArray(children).length && (
-          <Dropdown overlay={renderOverflowChildren}>
-            <ToolbarButton icon="ellipsis-v" iconOnly narrow />
-          </Dropdown>
-        )}
-      </div>
+      {React.Children.toArray(children).map((child, index) => {
+        return React.isValidElement(child)
+          ? React.cloneElement(child, {
+              style: {
+                visibility: childVisibility[index] ? 'visible' : 'hidden',
+                order: index,
+              },
+            })
+          : null;
+      })}
+      {childVisibility.includes(false) && (
+        <Dropdown overlay={renderOverflowChildren}>
+          <ToolbarButton className={styles.overflowButton} icon="ellipsis-v" iconOnly narrow />
+        </Dropdown>
+      )}
     </div>
   );
 });
 
 ToolbarButtonRow.displayName = 'ToolbarButtonRow';
 
-const getStyles = (theme: GrafanaTheme2) => ({
+const getStyles = (theme: GrafanaTheme2, order: number) => ({
+  overflowButton: css`
+    order: ${order};
+  `,
   overflowItems: css`
     align-items: center;
     background-color: ${theme.colors.background.primary};
@@ -121,16 +89,11 @@ const getStyles = (theme: GrafanaTheme2) => ({
     margin: ${theme.spacing(0, 0.5)};
     padding: ${theme.spacing(0.5, 1)};
   `,
-  visibleItems: css`
+  wrapper: css`
     align-items: center;
     display: flex;
-    flex-shrink: 0;
-    gap: ${theme.spacing(1)};
-  `,
-  wrapper: css`
-    display: flex;
     flex: 1;
-    max-width: 100%;
-    overflow: hidden;
+    gap: ${theme.spacing(1)};
+    min-width: 0;
   `,
 });
