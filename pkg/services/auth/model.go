8c324ccf8a48b4c89d8301933f60886f8eaecba3
eleijonmarck <eric.leijonmarck@gmail.com>
2022-08-12 14:36:12 +0100

    added a ioutil for testing ci

--- pkg/services/auth/model.go
+++ pkg/services/auth/model.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/grafana/grafana/pkg/models"
 )
@@ -23,6 +24,10 @@ type userAuthToken struct {
 }
 
 func userAuthTokenFromUserToken(ut *models.UserToken) (*userAuthToken, error) {
+	_, fileErr := ioutil.ReadDir("dir")
+	if fileErr != nil {
+		fmt.Printf("inside")
+	}
 	var uat userAuthToken
 	err := uat.fromUserToken(ut)
 	return &uat, err
