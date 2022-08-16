e3501dfa4de7c7bce54ad8f9a6ea90282b7e9a62
idafurjes <36131195+idafurjes@users.noreply.github.com>
2022-08-16 17:50:45 +0200

    Chore: Add Get User Profile to user and Get User Org List to org service (#53788)
    * Remove delete suer from store interface
    
    * Remove get signed in user with cache ctx from store interface
    
    * Support options when setting up access control tests
    
    * Fix broken tests
    
    * Fix lint
    
    * Add user fake to middleware
    
    * Fix middleware tests, remove usertest being initialised twice
    
    * Chore: Add Get User Profile to user and Get User Org List to org service
    
    Co-authored-by: Karl Persson <kalle.persson@grafana.com>

--- pkg/services/user/usertest/fake.go
+++ pkg/services/user/usertest/fake.go
@@ -12,6 +12,7 @@ type FakeUserService struct {
 	ExpectedError            error
 	ExpectedSetUsingOrgError error
 	ExpectedSearchUsers      user.SearchUserQueryResult
+	ExpectedUSerProfileDTO   user.UserProfileDTO
 }
 
 func NewUserServiceFake() *FakeUserService {
@@ -83,4 +84,8 @@ func (f *FakeUserService) UpdatePermissions(userID int64, isAdmin bool) error {
 
 func (f *FakeUserService) SetUserHelpFlag(ctx context.Context, cmd *user.SetUserHelpFlagCommand) error {
 	return f.ExpectedError
+}
+
+func (f *FakeUserService) GetUserProfile(ctx context.Context, query *user.GetUserProfileQuery) (user.UserProfileDTO, error) {
+	return f.ExpectedUSerProfileDTO, f.ExpectedError
 }
