db102c45ee6439f764aedf6810695eaf193ad6a7
Adela Almasan <adela.almasan@grafana.com>
2022-08-17 15:59:15 -0500

    Header buttons update

--- public/app/features/query-library/components/QueryEditorDrawerHeader.tsx
+++ public/app/features/query-library/components/QueryEditorDrawerHeader.tsx
@@ -1,7 +1,8 @@
+import { css } from '@emotion/css';
 import React from 'react';
 
-import { DataQuery } from '@grafana/data/src';
-import { Button, HorizontalGroup } from '@grafana/ui';
+import { DataQuery, GrafanaTheme2 } from '@grafana/data/src';
+import { Button, ButtonCascader, HorizontalGroup, useStyles2 } from '@grafana/ui';
 
 import { SavedQuery } from '../api/SavedQueriesApi';
 import { getSavedQuerySrv } from '../api/SavedQueriesSrv';
@@ -12,12 +13,22 @@ type Props = {
 };
 
 export const QueryEditorDrawerHeader = ({ savedQuery, onDismiss }: Props) => {
-  //@TODO add buttons actions
+  const styles = useStyles2(getStyles);
 
-  // @TODO currently fails as it's loading a hardcoded query, revisit
   const deleteQuery = async () => {
-    await getSavedQuerySrv().deleteSavedQuery({ uid: 'system/queries/ds-variables.json' });
+    await getSavedQuerySrv().deleteSavedQuery({ uid: savedQuery.uid });
     onDismiss();
+  };
+
+  const useQueryOptions = () => {
+    return [
+      { label: 'Create dashboard panel', value: 'dashboard-panel', icon: 'apps' },
+      { label: 'Create alert rule in Alert Manager', value: 'alert-rule', icon: 'bell' },
+      { label: 'Use in explore', value: 'explore', icon: 'compass' },
+      { label: 'Create incident in Grafana OnCall', value: 'incident-oncall', icon: 'compass' },
+      { label: 'Create incident in Grafana Incident', value: 'incident-incident', icon: 'compass' },
+      { label: 'Explore logs', value: 'incident-incident', icon: 'compass' },
+    ];
   };
 
   return (
@@ -27,13 +38,32 @@ export const QueryEditorDrawerHeader = ({ savedQuery, onDismiss }: Props) => {
         <Button icon="times" size="sm" variant={'secondary'} onClick={onDismiss}>
           Close
         </Button>
-        <Button icon="share-alt" size="sm" variant={'secondary'} />
-        <Button icon="upload" size="sm" variant={'secondary'} />
-        <Button size="sm" variant={'secondary'}>
+        <ButtonCascader
+          options={useQueryOptions()}
+          variant="secondary"
+          icon={'grafana'}
+          buttonProps={{ className: styles.cascaderButton }}
+        >
+          Use query
+        </ButtonCascader>
+        <Button icon="sync" size="sm" variant={'secondary'}>
+          Run
+        </Button>
+        {/*<Button icon="share-alt" size="sm" variant={'secondary'}>Export</Button>*/}
+        <Button icon="lock" size="sm" variant={'secondary'} />
+        <Button size="sm" variant={'primary'}>
           Save
         </Button>
         <Button icon="trash-alt" size="sm" variant={'destructive'} onClick={deleteQuery} />
       </HorizontalGroup>
     </HorizontalGroup>
   );
+};
+
+export const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    cascaderButton: css`
+      height: 24px;
+    `,
+  };
 };
