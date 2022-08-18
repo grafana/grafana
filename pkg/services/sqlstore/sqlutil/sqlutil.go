82b63688d236a4c5788043822e8a22d003bb8a8c
ying-jeanne <74549700+ying-jeanne@users.noreply.github.com>
2022-08-17 21:07:20 -0500

    Chore: remove xorm from preference (#53803)
    * Chore: remove xorm from preference
    
    * separte feature toggle
    
    * fix comments
    
    * fix comments
    
    * remove the dublicated namedexec

--- pkg/services/sqlstore/sqlutil/sqlutil.go
+++ pkg/services/sqlstore/sqlutil/sqlutil.go
@@ -28,7 +28,7 @@ func MySQLTestDB() TestDB {
 	if port == "" {
 		port = "3306"
 	}
-	conn_str := fmt.Sprintf("grafana:password@tcp(%s:%s)/grafana_tests?collation=utf8mb4_unicode_ci&sql_mode='ANSI_QUOTES'", host, port)
+	conn_str := fmt.Sprintf("grafana:password@tcp(%s:%s)/grafana_tests?collation=utf8mb4_unicode_ci&sql_mode='ANSI_QUOTES'&parseTime=true", host, port)
 	return TestDB{
 		DriverName: "mysql",
 		ConnStr:    conn_str,
