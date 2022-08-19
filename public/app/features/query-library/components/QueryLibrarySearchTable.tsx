d45aa52614902aaf5a6f17f23fd231474a31e43d
Adela Almasan <adela.almasan@grafana.com>
2022-08-19 10:32:56 -0500

    table style

--- public/app/features/query-library/components/QueryLibrarySearchTable.tsx
+++ public/app/features/query-library/components/QueryLibrarySearchTable.tsx
@@ -1,12 +1,14 @@
 import { css, cx } from '@emotion/css';
+import { Global } from '@emotion/react';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useAsync } from 'react-use';
 import AutoSizer from 'react-virtualized-auto-sizer';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { Button, FilterInput, HorizontalGroup, ModalsController, Spinner, useStyles2 } from '@grafana/ui';
+import { Button, FilterInput, HorizontalGroup, ModalsController, Spinner, useStyles2, useTheme2 } from '@grafana/ui';
 
 import { getGrafanaSearcher, SearchQuery } from '../../search/service';
+import { getGlobalStyles } from '../globalStyles';
 import { QueryItem } from '../types';
 
 import { DatasourceTypePicker } from './DatasourceTypePicker';
@@ -20,14 +22,17 @@ const QueryLibrarySearchTable = () => {
   const [searchQueryBy, setSearchByQuery] = useState<string>('');
   const [reload, setReload] = useState(0);
 
+  const theme = useTheme2();
+  const globalCSS = getGlobalStyles(theme);
+
   // @TODO update with real data
   const authors = ['Artur Wierzbicki', 'Drew Slobodnjak', 'Nathan Marrs', 'Raphael Batyrbaev', 'Adela Almasan'];
   const dates = [
-    'August 13, 2022, 4:10pm',
-    'August 13, 2022, 2:32pm',
-    'August 14, 2022, 1:00am',
-    'August 16, 2022, 12:00pm',
-    'August 17, 2022, 2:33pm',
+    'August 17, 2022, 2:32pm',
+    'August 17, 2022, 4:10pm',
+    'August 18, 2022, 1:00am',
+    'August 18, 2022, 12:00pm',
+    'August 19, 2022, 2:33pm',
   ];
 
   const searchQuery = useMemo<SearchQuery>(() => {
@@ -70,103 +75,106 @@ const QueryLibrarySearchTable = () => {
 
   const found = results.value;
   return (
-    <div className={styles.tableWrapper}>
-      <HorizontalGroup width="100%" justify="space-between" spacing={'md'} height={25}>
-        <HorizontalGroup>
-          <FilterInput
-            placeholder="Search queries by name, source, or variable"
-            autoFocus={true}
-            value={searchQueryBy}
-            onChange={setSearchByQuery}
-            width={50}
-            className={styles.searchBy}
-          />
-          Filter by datasource type
-          <DatasourceTypePicker
-            current={datasourceType}
-            onChange={(newDsType) => {
-              setDatasourceType(() => newDsType);
+    <>
+      <Global styles={globalCSS} />
+      <div className={styles.tableWrapper}>
+        <HorizontalGroup width="100%" justify="space-between" spacing={'md'} height={25}>
+          <HorizontalGroup>
+            <FilterInput
+              placeholder="Search queries by name, source, or variable"
+              autoFocus={true}
+              value={searchQueryBy}
+              onChange={setSearchByQuery}
+              width={50}
+              className={styles.searchBy}
+            />
+            Filter by datasource type
+            <DatasourceTypePicker
+              current={datasourceType}
+              onChange={(newDsType) => {
+                setDatasourceType(() => newDsType);
+              }}
+            />
+          </HorizontalGroup>
+          <ModalsController>
+            {({ showModal, hideModal }) => {
+              return (
+                <div className={styles.createQueryButton}>
+                  <Button
+                    icon="plus"
+                    size="md"
+                    onClick={() => {
+                      showModal(QueryCreateDrawer, {
+                        onDismiss: hideModal,
+                        updateComponent: () => {
+                          setReload(reload + 1);
+                        },
+                      });
+                    }}
+                  >
+                    Create query
+                  </Button>
+                </div>
+              );
             }}
-          />
+          </ModalsController>
         </HorizontalGroup>
+
         <ModalsController>
           {({ showModal, hideModal }) => {
             return (
-              <div className={styles.createQueryButton}>
-                <Button
-                  icon="plus"
-                  size="md"
-                  onClick={() => {
-                    showModal(QueryCreateDrawer, {
-                      onDismiss: hideModal,
-                      updateComponent: () => {
-                        setReload(reload + 1);
-                      },
-                    });
-                  }}
-                >
-                  Create query
-                </Button>
-              </div>
+              <AutoSizer className={styles.autosizer} style={{ width: '100%', height: '100%' }}>
+                {({ width, height }) => {
+                  return (
+                    <table className={cx('filter-table form-inline filter-table--hover', styles.table)}>
+                      <thead>
+                        <tr>
+                          <th />
+                          <th>Status</th>
+                          <th>Name and raw query</th>
+                          <th>Data Source</th>
+                          <th>User</th>
+                          <th>Date</th>
+                          <th />
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {found?.length === 0 && (
+                          <tr className={styles.transparentBg}>
+                            <td />
+                            <td />
+                            <td />
+                            <td>
+                              <div className={styles.noData}>No data</div>
+                            </td>
+                            <td />
+                            <td />
+                            <th />
+                          </tr>
+                        )}
+                        {found!.map((item, key) => {
+                          return (
+                            <QueryListItem
+                              query={item}
+                              key={item.uid}
+                              showModal={showModal}
+                              hideModal={hideModal}
+                              updateComponent={() => setReload(reload + 1)}
+                              author={key < authors.length ? authors[key] : authors[key - authors.length]}
+                              date={key < dates.length ? dates[key] : dates[key - dates.length]}
+                            />
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  );
+                }}
+              </AutoSizer>
             );
           }}
         </ModalsController>
-      </HorizontalGroup>
-
-      <ModalsController>
-        {({ showModal, hideModal }) => {
-          return (
-            <AutoSizer className={styles.autosizer} style={{ width: '100%', height: '100%' }}>
-              {({ width, height }) => {
-                return (
-                  <table className={cx('filter-table form-inline filter-table--hover', styles.table)}>
-                    <thead>
-                      <tr>
-                        <th />
-                        <th>Status</th>
-                        <th>Name and raw query</th>
-                        <th>Data Source</th>
-                        <th>User</th>
-                        <th>Date</th>
-                        <th />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {found?.length === 0 && (
-                        <tr className={styles.transparentBg}>
-                          <td />
-                          <td />
-                          <td />
-                          <td>
-                            <div className={styles.noData}>No data</div>
-                          </td>
-                          <td />
-                          <td />
-                          <th />
-                        </tr>
-                      )}
-                      {found!.map((item, key) => {
-                        return (
-                          <QueryListItem
-                            query={item}
-                            key={item.uid}
-                            showModal={showModal}
-                            hideModal={hideModal}
-                            updateComponent={() => setReload(reload + 1)}
-                            author={key < authors.length ? authors[key] : authors[key - authors.length]}
-                            date={key < dates.length ? dates[key] : dates[key - dates.length]}
-                          />
-                        );
-                      })}
-                    </tbody>
-                  </table>
-                );
-              }}
-            </AutoSizer>
-          );
-        }}
-      </ModalsController>
-    </div>
+      </div>
+    </>
   );
 };
 
