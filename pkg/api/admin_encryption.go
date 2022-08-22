8a1cfe8a1500f88dcc78c58eb03005bc28fee89a
Michael Mandrus <michael.mandrus@grafana.com>
2022-08-22 16:35:28 -0400

    change pre-migration checks in admin api

--- pkg/api/admin_encryption.go
+++ pkg/api/admin_encryption.go
@@ -50,6 +50,8 @@ func (hs *HTTPServer) AdminRollbackSecrets(c *models.ReqContext) response.Respon
 	return response.Respond(http.StatusOK, "Secrets rolled back successfully")
 }
 
+// To migrate to the plugin, it must be installed and configured
+// so as not to lose access to migrated secrets
 func (hs *HTTPServer) MigrateSecretsToPlugin(c *models.ReqContext) response.Response {
 	if skv.EvaluateRemoteSecretsPlugin(hs.secretsPluginManager, hs.Cfg) != nil {
 		hs.log.Warn("Received secrets plugin migration request while plugin is not available")
@@ -63,10 +65,12 @@ func (hs *HTTPServer) MigrateSecretsToPlugin(c *models.ReqContext) response.Resp
 	return response.Respond(http.StatusOK, "Secret migration to plugin triggered successfully")
 }
 
+// To migrate from the plugin, it must be installed only
+// as it is possible the user disabled it and then wants to migrate
 func (hs *HTTPServer) MigrateSecretsFromPlugin(c *models.ReqContext) response.Response {
-	if skv.EvaluateRemoteSecretsPlugin(hs.secretsPluginManager, hs.Cfg) != nil {
-		hs.log.Warn("Received secrets plugin migration request while plugin is not available")
-		return response.Respond(http.StatusBadRequest, "Secrets plugin is not available")
+	if hs.secretsPluginManager.SecretsManager() == nil {
+		hs.log.Warn("Received secrets plugin migration request while plugin is not installed")
+		return response.Respond(http.StatusBadRequest, "Secrets plugin is not installed")
 	}
 	err := hs.secretsPluginMigrator.TriggerPluginMigration(c.Req.Context(), false)
 	if err != nil {
