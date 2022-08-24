14f55bc4f4e1303321d6e94c35b251d486a75784
Santiago Hernández <santiagohernandez.1997@gmail.com>
2022-08-24 11:56:42 -0300

    redact password in AM URLs

--- pkg/services/ngalert/sender/router.go
+++ pkg/services/ngalert/sender/router.go
@@ -127,17 +127,30 @@ func (d *AlertsRouter) SyncAndApplyConfigFromDatabase() error {
 			continue
 		}
 
-		amHash := cfg.AsSHA256()
-		d.logger.Debug("alertmanagers found in the configuration", "alertmanagers", amHash)
+		// Avoid logging sensitive data
+		var redactedAMs []string
+		for _, am := range cfg.Alertmanagers {
+			parsedAM, err := url.Parse(am)
+			if err != nil {
+				d.logger.Error("failed to parse alertmanager string",
+					"org", cfg.OrgID,
+					"err", err)
+				continue
+			}
+			redactedAMs = append(redactedAMs, parsedAM.Redacted())
+		}
+
+		d.logger.Debug("alertmanagers found in the configuration", "alertmanagers", redactedAMs)
 
 		// We have a running sender, check if we need to apply a new config.
+		amHash := cfg.AsSHA256()
 		if ok {
 			if d.externalAlertmanagersCfgHash[cfg.OrgID] == amHash {
-				d.logger.Debug("sender configuration is the same as the one running, no-op", "org", cfg.OrgID, "alertmanagers", amHash)
+				d.logger.Debug("sender configuration is the same as the one running, no-op", "org", cfg.OrgID, "alertmanagers", redactedAMs)
 				continue
 			}
 
-			d.logger.Debug("applying new configuration to sender", "org", cfg.OrgID, "alertmanagers", amHash)
+			d.logger.Debug("applying new configuration to sender", "org", cfg.OrgID, "alertmanagers", redactedAMs)
 			err := existing.ApplyConfig(cfg)
 			if err != nil {
 				d.logger.Error("failed to apply configuration", "err", err, "org", cfg.OrgID)
@@ -148,7 +161,7 @@ func (d *AlertsRouter) SyncAndApplyConfigFromDatabase() error {
 		}
 
 		// No sender and have Alertmanager(s) to send to - start a new one.
-		d.logger.Info("creating new sender for the external alertmanagers", "org", cfg.OrgID, "alertmanagers", amHash)
+		d.logger.Info("creating new sender for the external alertmanagers", "org", cfg.OrgID, "alertmanagers", redactedAMs)
 		s, err := NewExternalAlertmanagerSender()
 		if err != nil {
 			d.logger.Error("unable to start the sender", "err", err, "org", cfg.OrgID)
