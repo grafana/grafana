7a21ec7f3acb6183ef93e1f46b90140d121ef1ee
Maryan Plakhtiy <maryan.plakhtiy@fluxninja.com>
2022-08-25 19:46:51 +0200

    Add simple adhoc filter injection into druid query

--- public/app/plugins/datasource/grafadruid-druid-datasource/DruidDataSource.ts
+++ public/app/plugins/datasource/grafadruid-druid-datasource/DruidDataSource.ts
@@ -1,19 +1,39 @@
 import { DataSourceInstanceSettings, MetricFindValue } from '@grafana/data';
-import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import { DataSourceWithBackend } from '@grafana/runtime';
+import { getTemplateSrv, TemplateSrv } from 'app/features/templating/template_srv';
+
 import { DruidSettings, DruidQuery } from './types';
 
 const druidVariableRegex = /\"\[\[(\w+)(?::druid:(\w+))?\]\]\"|\"\${(\w+)(?::druid:(\w+))?}\"/g;
 
+interface AdHocFilterItem {
+  key?: any;
+  operator?: any;
+  value?: any
+}
+
 export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSettings> {
-  constructor(instanceSettings: DataSourceInstanceSettings<DruidSettings>) {
+
+  constructor(
+    instanceSettings: DataSourceInstanceSettings<DruidSettings>,
+    private readonly templateSrv: TemplateSrv = getTemplateSrv(),
+  ) {
     super(instanceSettings);
   }
+
   filterQuery(query: DruidQuery) {
     return !query.hide;
   }
+
   applyTemplateVariables(templatedQuery: DruidQuery) {
-    const templateSrv = getTemplateSrv();
-    let template = JSON.stringify({ ...templatedQuery, expr: undefined }).replace(
+    const shouldEnhanceWithAdHocFilters = templatedQuery.datasource && templatedQuery.datasource.type === "grafadruid-druid-datasource";
+    let template = JSON.stringify({...templatedQuery, expr: undefined});
+
+    if (shouldEnhanceWithAdHocFilters) {
+      template = this.enhanceExprWithAdHocFilters(template);
+    }
+
+    template = template.replace(
       druidVariableRegex,
       (match, variable1, format1, variable2, format2) => {
         if (format1 || format2 === 'json') {
@@ -22,8 +42,54 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
         return match;
       }
     );
-    return { ...JSON.parse(templateSrv.replace(template)), expr: templatedQuery.expr };
+
+    let expr = templatedQuery.expr;
+    if (shouldEnhanceWithAdHocFilters) {
+      expr = this.enhanceExprWithAdHocFilters(expr);
+    }
+
+    return {...JSON.parse(this.templateSrv.replace(template)), expr};
   }
+
+  enhanceExprWithAdHocFilters(expr: string) {
+    const adhocFilters = this.templateSrv.getAdhocFilters(this.name);
+
+    const exprParsed = JSON.parse(expr);
+
+    const filterMap: Record<string, AdHocFilterItem[]> = adhocFilters.reduce((acc: any, filter: AdHocFilterItem) => {
+      const uniqueKey = `${filter.key}-${filter.operator}`;
+      if (!acc[uniqueKey]) {
+        acc[uniqueKey] = [filter];
+      } else {
+        acc[uniqueKey].push(filter);
+      }
+      return acc;
+    }, {} as Record<string, AdHocFilterItem[]>)
+
+    const filterFields = Object.values(filterMap).map((filters: AdHocFilterItem[]) => {
+      if (filters.length === 1) {
+        return {
+          dimension: filters[0].key,
+          type: "selector",
+          value: filters[0].value,
+        }
+      } else {
+        return {
+          dimension: filters[0].key,
+          type: "in",
+          values: filters.map(filter => filter.value),
+        }
+      }
+    })
+    if (exprParsed.builder && exprParsed.builder.filter) {
+      if (exprParsed.builder.filter.type === "and") {
+        exprParsed.builder.filter.fields = exprParsed.builder.filter.fields.concat(filterFields);
+      }
+    }
+
+    return JSON.stringify(exprParsed);
+  }
+
   async metricFindQuery(query: DruidQuery, options?: any): Promise<MetricFindValue[]> {
     return this.postResource('query-variable', this.applyTemplateVariables(query)).then((response) => {
       return response;
@@ -38,7 +104,7 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
         "query": columnNamesSql,
       },
       "settings": {},
-      "expr": "{\"builder\":{\"queryType\":\"sql\",\"query\":\""+columnNamesSql+"\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"${__from:date:iso}/${__to:date:iso}\"]}},\"settings\":{}}"
+      "expr": "{\"builder\":{\"queryType\":\"sql\",\"query\":\"" + columnNamesSql + "\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"${__from:date:iso}/${__to:date:iso}\"]}},\"settings\":{}}"
     }
 
     return this.postResource('query-variable', this.applyTemplateVariables(tagKeysQuery as any));
@@ -52,7 +118,7 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
         "query": columnValuesSql,
       },
       "settings": {},
-      "expr": "{\"builder\":{\"queryType\":\"sql\",\"query\":\""+columnValuesSql+"\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"${__from:date:iso}/${__to:date:iso}\"]}},\"settings\":{}}"
+      "expr": "{\"builder\":{\"queryType\":\"sql\",\"query\":\"" + columnValuesSql + "\",\"intervals\":{\"type\":\"intervals\",\"intervals\":[\"${__from:date:iso}/${__to:date:iso}\"]}},\"settings\":{}}"
     }
     return this.postResource('query-variable', this.applyTemplateVariables(tagValuesQuery as any));
   }
