7835fdebf6dcdab236f3e0d7966b3b202a00d991
Piotr Pomierski <pomierskipiotr@gmail.com>
2022-08-27 21:09:08 +0200

    use single quote marks

--- packages/grafana-ui/src/components/Button/Button.story.tsx
+++ packages/grafana-ui/src/components/Button/Button.story.tsx
@@ -1,21 +1,21 @@
-import { ComponentStory } from "@storybook/react";
-import React from "react";
+import { ComponentStory } from '@storybook/react';
+import React from 'react';
 
-import { ComponentSize } from "../../types/size";
-import { Card } from "../Card/Card";
-import { HorizontalGroup, VerticalGroup } from "../Layout/Layout";
+import { ComponentSize } from '../../types/size';
+import { Card } from '../Card/Card';
+import { HorizontalGroup, VerticalGroup } from '../Layout/Layout';
 
 import {
   allButtonVariants,
   allButtonFills,
   Button,
   ButtonProps,
-} from "./Button";
-import mdx from "./Button.mdx";
-import { ButtonGroup } from "./ButtonGroup";
+} from './Button';
+import mdx from './Button.mdx';
+import { ButtonGroup } from './ButtonGroup';
 
 export default {
-  title: "Buttons/Button",
+  title: 'Buttons/Button',
   component: Button,
   parameters: {
     controls: { expanded: true },
@@ -25,7 +25,7 @@ export default {
   },
   argTypes: {
     size: {
-      options: ["sm", "md", "lg"],
+      options: ['sm', 'md', 'lg'],
     },
     tooltip: {
       table: {
@@ -41,14 +41,14 @@ export default {
 };
 
 export const Examples: ComponentStory<typeof Button> = () => {
-  const sizes: ComponentSize[] = ["lg", "md", "sm"];
+  const sizes: ComponentSize[] = ['lg', 'md', 'sm'];
   return (
     <VerticalGroup>
       {allButtonFills.map((buttonFill) => (
         <VerticalGroup key={buttonFill}>
-          <HorizontalGroup spacing="lg">
+          <HorizontalGroup spacing='lg'>
             {allButtonVariants.map((variant) => (
-              <VerticalGroup spacing="lg" key={`${buttonFill}-${variant}`}>
+              <VerticalGroup spacing='lg' key={`${buttonFill}-${variant}`}>
                 {sizes.map((size) => (
                   <Button
                     variant={variant}
@@ -65,36 +65,36 @@ export const Examples: ComponentStory<typeof Button> = () => {
               </VerticalGroup>
             ))}
           </HorizontalGroup>
-          <div style={{ padding: "20px 0", width: "100%" }} />
+          <div style={{ padding: '20px 0', width: '100%' }} />
         </VerticalGroup>
       ))}
-      <HorizontalGroup spacing="lg">
+      <HorizontalGroup spacing='lg'>
         <div>With icon and text</div>
-        <Button icon="cloud" size="sm">
+        <Button icon='cloud' size='sm'>
           Configure
         </Button>
-        <Button icon="cloud">Configure</Button>
-        <Button icon="cloud" size="lg">
+        <Button icon='cloud'>Configure</Button>
+        <Button icon='cloud' size='lg'>
           Configure
         </Button>
       </HorizontalGroup>
       <div />
-      <HorizontalGroup spacing="lg">
+      <HorizontalGroup spacing='lg'>
         <div>With icon only</div>
-        <Button icon="cloud" size="sm" />
-        <Button icon="cloud" size="md" />
-        <Button icon="cloud" size="lg" />
+        <Button icon='cloud' size='sm' />
+        <Button icon='cloud' size='md' />
+        <Button icon='cloud' size='lg' />
       </HorizontalGroup>
       <div />
-      <Button icon="plus" fullWidth>
+      <Button icon='plus' fullWidth>
         Button with fullWidth
       </Button>
       <div />
-      <HorizontalGroup spacing="lg">
+      <HorizontalGroup spacing='lg'>
         <div>Inside ButtonGroup</div>
         <ButtonGroup>
-          <Button icon="sync">Run query</Button>
-          <Button icon="angle-down" />
+          <Button icon='sync'>Run query</Button>
+          <Button icon='angle-down' />
         </ButtonGroup>
       </HorizontalGroup>
       <Card>
@@ -105,7 +105,7 @@ export const Examples: ComponentStory<typeof Button> = () => {
               {variant}
             </Button>
           ))}
-          <Button variant="primary" disabled>
+          <Button variant='primary' disabled>
             Disabled
           </Button>
         </Card.Actions>
@@ -117,5 +117,5 @@ export const Examples: ComponentStory<typeof Button> = () => {
 export const Default = ({ ...args }: ButtonProps) => <Button {...args} />;
 
 Default.args = {
-  children: "Example button",
+  children: 'Example button',
 };
