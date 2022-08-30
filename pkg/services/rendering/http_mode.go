f62cff4cac1dcc7bcb02a3fe84a0c86f45b2ca51
Joan López de la Franca Beltran <5459617+joanlopez@users.noreply.github.com>
2022-08-30 14:04:55 +0200

    Rendering: Add support for renderer token (#54425) (#54437)
    (cherry picked from commit a4f75cc0438712c90b02d24740416f8615e3a0cb)
    (cherry picked from commit 6ec06f66b9dec2e73ce8734f041a6e266849df9b)

--- pkg/services/rendering/http_mode.go
+++ pkg/services/rendering/http_mode.go
@@ -28,6 +28,8 @@ var netClient = &http.Client{
 	Transport: netTransport,
 }
 
+const authTokenHeader = "X-Auth-Token" //#nosec G101 -- This is a false positive
+
 var (
 	remoteVersionFetchInterval time.Duration = time.Second * 15
 	remoteVersionFetchRetries  uint          = 4
@@ -138,6 +140,7 @@ func (rs *RenderingService) doRequest(ctx context.Context, url *url.URL, headers
 		return nil, err
 	}
 
+	req.Header.Set(authTokenHeader, rs.Cfg.RendererAuthToken)
 	req.Header.Set("User-Agent", fmt.Sprintf("Grafana/%s", rs.Cfg.BuildVersion))
 	for k, v := range headers {
 		req.Header[k] = v
