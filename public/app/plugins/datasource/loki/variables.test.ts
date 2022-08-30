621c74ed90faaddbd02c8eb5c22911fb34e5607a
Matias Chomicki <matyax@gmail.com>
2022-08-30 16:04:23 +0200

    feat(loki-query-editor): replace unnecessary class with class method

--- public/app/plugins/datasource/loki/variables.test.ts
+++ public/app/plugins/datasource/loki/variables.test.ts
@@ -1,11 +1,11 @@
 import { TemplateSrv } from 'app/features/templating/template_srv';
 
-import { LokiMetricFindQuery } from './metricFindQuery';
 import { createLokiDatasource, createMetadataRequest } from './mocks';
 import { LokiVariableQueryType } from './types';
+import { LokiVariableSupport } from './variables';
 
-describe('LokiMetricFindQuery', () => {
-  let lokiMetricFindQuery: LokiMetricFindQuery;
+describe('LokiVariableSupport', () => {
+  let lokiVariableSupport: LokiVariableSupport;
 
   beforeEach(() => {
     const datasource = createLokiDatasource({} as unknown as TemplateSrv);
@@ -18,19 +18,19 @@ describe('LokiMetricFindQuery', () => {
         )
       );
 
-    lokiMetricFindQuery = new LokiMetricFindQuery(datasource);
+    lokiVariableSupport = new LokiVariableSupport(datasource);
   });
 
   it('should return label names for Loki', async () => {
     // label_names()
-    const response = await lokiMetricFindQuery.execute({ refId: 'test', type: LokiVariableQueryType.LabelNames });
+    const response = await lokiVariableSupport.execute({ refId: 'test', type: LokiVariableQueryType.LabelNames });
 
     expect(response).toEqual([{ text: 'label1' }, { text: 'label2' }]);
   });
 
   it('should return label values for Loki when no matcher', async () => {
     // label_values(label1)
-    const response = await lokiMetricFindQuery.execute({
+    const response = await lokiVariableSupport.execute({
       refId: 'test',
       type: LokiVariableQueryType.LabelValues,
       label: 'label1',
@@ -41,7 +41,7 @@ describe('LokiMetricFindQuery', () => {
 
   it('should return label values for Loki with matcher', async () => {
     // label_values({label1="value1", label2="value2"},label5)
-    const response = await lokiMetricFindQuery.execute({
+    const response = await lokiVariableSupport.execute({
       refId: 'test',
       type: LokiVariableQueryType.LabelValues,
       stream: '{label1="value1", label2="value2"}',
--- public/app/plugins/datasource/loki/variables.ts
+++ public/app/plugins/datasource/loki/variables.ts
@@ -1,29 +1,40 @@
 import { from, Observable } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import { CustomVariableSupport, DataQueryRequest, DataQueryResponse } from '@grafana/data';
 
 import { LokiVariableQueryEditor } from './components/VariableQueryEditor';
 import { LokiDatasource } from './datasource';
-import { LokiMetricFindQuery } from './metricFindQuery';
-import { LokiVariableQuery } from './types';
+import { LokiVariableQuery, LokiVariableQueryType } from './types';
 
 export class LokiVariableSupport extends CustomVariableSupport<LokiDatasource, LokiVariableQuery> {
-  private metricFindQuery: LokiMetricFindQuery;
   editor = LokiVariableQueryEditor;
 
-  constructor(datasource: LokiDatasource) {
+  constructor(private datasource: LokiDatasource) {
     super();
-    this.metricFindQuery = new LokiMetricFindQuery(datasource);
     this.query = this.query.bind(this);
   }
 
-  query(request: DataQueryRequest<LokiVariableQuery>): Observable<DataQueryResponse> {
-    const executeObservable = this.metricFindQuery.execute(request.targets[0]);
+  async execute(query: LokiVariableQuery) {
+    if (query.type === LokiVariableQueryType.LabelNames) {
+      return this.datasource.labelNamesQuery();
+    }
 
-    return from(executeObservable).pipe(
-      mergeMap(() => executeObservable),
-      map((data) => ({ data }))
-    );
+    if (!query.label) {
+      return [];
+    }
+
+    // If we have query expr, use /series endpoint
+    if (query.stream) {
+      return this.datasource.labelValuesSeriesQuery(query.stream, query.label);
+    }
+
+    return this.datasource.labelValuesQuery(query.label);
+  }
+
+  query(request: DataQueryRequest<LokiVariableQuery>): Observable<DataQueryResponse> {
+    const result = this.execute(request.targets[0]);
+
+    return from(result).pipe(map((data) => ({ data })));
   }
 }
