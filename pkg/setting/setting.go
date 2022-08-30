b98fb98a7a34323561d045380ac27da60f549945
Joan López de la Franca Beltran <5459617+joanlopez@users.noreply.github.com>
2022-08-30 12:09:38 +0200

    Rendering: Add support for renderer token (#54425)
    (cherry picked from commit a4f75cc0438712c90b02d24740416f8615e3a0cb)
    (cherry picked from commit 6ec06f66b9dec2e73ce8734f041a6e266849df9b)

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -237,6 +237,7 @@ type Cfg struct {
 	CSVsDir                        string
 	RendererUrl                    string
 	RendererCallbackUrl            string
+	RendererAuthToken              string
 	RendererConcurrentRequestLimit int
 
 	// Security
@@ -1391,6 +1392,7 @@ func (cfg *Cfg) readRenderingSettings(iniFile *ini.File) error {
 	renderSec := iniFile.Section("rendering")
 	cfg.RendererUrl = valueAsString(renderSec, "server_url", "")
 	cfg.RendererCallbackUrl = valueAsString(renderSec, "callback_url", "")
+	cfg.RendererAuthToken = valueAsString(renderSec, "renderer_token", "-")
 
 	if cfg.RendererCallbackUrl == "" {
 		cfg.RendererCallbackUrl = AppUrl
