c4b111285a4717de2e4a14e31092af20aec3ad42
Erik Sundell <erik.sundell87@gmail.com>
2022-08-31 09:10:04 +0200

    pr feedback

--- public/app/plugins/datasource/cloudwatch/tracking.ts
+++ public/app/plugins/datasource/cloudwatch/tracking.ts
@@ -16,25 +16,33 @@ interface CloudWatchOnDashboardLoadedTrackingEvent {
   /* The number of CloudWatch metrics queries present in the dashboard*/
   metrics_queries_count: number;
 
-  /* The number of queries using the "Search" mode. Should be measured in relation to metrics_queries_count */
+  /* The number of queries using the "Search" mode. 
+  Should be measured in relation to metrics_queries_count, e.g metrics_search_count + metrics_query_count = metrics_queries_count */
   metrics_search_count: number;
 
-  /* The number of search queries that are using the builder mode. Should be measured in relation to metrics_search_count */
+  /* The number of search queries that are using the builder mode. 
+  Should be measured in relation to metrics_search_count, e.g metrics_search_builder_count + metrics_search_code_count = metrics_search_count */
   metrics_search_builder_count: number;
 
-  /* The number of search queries that are using the code mode. Should be measured in relation to metrics_search_count */
+  /* The number of search queries that are using the code mode. 
+  Should be measured in relation to metrics_search_count, e.g metrics_search_builder_count + metrics_search_code_count = metrics_search_count */
   metrics_search_code_count: number;
 
-  /* The number of search queries that have enabled the `match exact` toggle in the builder mode. Should be measured in relation to metrics_search_builder_count */
+  /* The number of search queries that have enabled the `match exact` toggle in the builder mode. 
+  Should be measured in relation to metrics_search_builder_count. 
+  E.g 'Out of 5 metric seach queries (metrics_search_builder_count), 2 had match exact toggle (metrics_search_match_exact_count) enabled */
   metrics_search_match_exact_count: number;
 
-  /* The number of queries using the "Query" mode (AKA Metric Insights). Should be measured in relation to metrics_queries_count */
+  /* The number of queries using the "Query" mode (AKA Metric Insights). 
+  Should be measured in relation to metrics_queries_count, e.g metrics_search_count + metrics_query_count = metrics_queries_count */
   metrics_query_count: number;
 
-  /* The number of "Insights" queries that are using the builder mode. Should be measured in relation to metrics_query_count */
+  /* The number of "Insights" queries that are using the builder mode. 
+  Should be measured in relation to metrics_query_count, e.g metrics_query_builder_count + metrics_query_code_count = metrics_query_count */
   metrics_query_builder_count: number;
 
-  /* The number of "Insights" queries that are using the code mode. Should be measured in relation to metrics_query_count */
+  /* The number of "Insights" queries that are using the code mode. 
+  Should be measured in relation to metrics_query_count, e.g metrics_query_builder_count + metrics_query_code_count = metrics_query_count */
   metrics_query_code_count: number;
 }
 
