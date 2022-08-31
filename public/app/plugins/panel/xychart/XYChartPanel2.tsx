6d1165a6b982c2a7fc8b0facbbbf75e0e34e881e
Victor Marin <victor.marin@grafana.com>
2022-08-31 18:01:34 +0300

    Rewrite Pure Component to Function Component

--- public/app/plugins/panel/xychart/XYChartPanel2.tsx
+++ public/app/plugins/panel/xychart/XYChartPanel2.tsx
@@ -1,4 +1,5 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { usePrevious } from 'react-use';
 
 import {
   DisplayProcessor,
@@ -24,75 +25,60 @@ import { FacetedData } from '@grafana/ui/src/components/uPlot/types';
 
 import { TooltipView } from './TooltipView';
 import { XYChartOptions } from './models.gen';
-import { prepData, prepScatter } from './scatter';
+import { prepData, prepScatter, ScatterPanelInfo } from './scatter';
 import { ScatterHoverEvent, ScatterSeries } from './types';
 
 type Props = PanelProps<XYChartOptions>;
-type State = {
-  error?: string;
-  series: ScatterSeries[];
-  builder?: UPlotConfigBuilder;
-  facets?: FacetedData;
-  hover?: ScatterHoverEvent;
-};
 
-export class XYChartPanel2 extends PureComponent<Props, State> {
-  state: State = {
-    series: [],
-  };
+export const XYChartPanel2: React.FC<Props> = (props: Props) => {
+  const [error, setError] = useState<string | undefined>();
+  const [series, setSeries] = useState<ScatterSeries[]>([]);
+  const [builder, setBuilder] = useState<UPlotConfigBuilder | undefined>();
+  const [facets, setFacets] = useState<FacetedData | undefined>();
+  const [hover, setHover] = useState<ScatterHoverEvent | undefined>();
+  const oldOptions = usePrevious(props.options);
+  const oldData = usePrevious(props.data);
 
-  componentDidMount() {
-    this.initSeries(); // also data
-  }
+  const initSeries = useCallback(() => {
+    const getData = () => props.data.series;
+    const info: ScatterPanelInfo = prepScatter(props.options, getData, config.theme2, scatterHoverCallback);
 
-  componentDidUpdate(oldProps: Props) {
-    const { options, data } = this.props;
-    const configsChanged = options !== oldProps.options || data.structureRev !== oldProps.data.structureRev;
-
-    if (configsChanged) {
-      this.initSeries();
-    } else if (data !== oldProps.data) {
-      this.initFacets();
+    if (info.series.length && props.data.series) {
+      setBuilder(info.builder);
+      setSeries(info.series);
+      setFacets(() => prepData(info, props.data.series));
+      setError(undefined);
     }
-  }
+  }, [props.data.series, props.options]);
 
-  scatterHoverCallback = (hover?: ScatterHoverEvent) => {
-    this.setState({ hover });
-  };
+  const initFacets = useCallback(() => {
+    setFacets(() => prepData({ error, series }, props.data.series));
+  }, [props.data.series, error, series]);
 
-  getData = () => {
-    return this.props.data.series;
-  };
-
-  initSeries = () => {
-    const { options, data } = this.props;
-    const info: State = prepScatter(options, this.getData, config.theme2, this.scatterHoverCallback);
-    if (info.series.length && data.series) {
-      info.facets = prepData(info, data.series);
-      info.error = undefined;
+  useEffect(() => {
+    if (oldOptions !== props.options || oldData?.structureRev !== props.data.structureRev) {
+      initSeries();
+    } else if (oldData !== props.data) {
+      initFacets();
     }
-    this.setState(info);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props]);
+
+  const scatterHoverCallback = (hover?: ScatterHoverEvent) => {
+    setHover(hover);
   };
 
-  initFacets = () => {
-    this.setState({
-      facets: prepData(this.state, this.props.data.series),
-    });
-  };
-
-  renderLegend = () => {
-    const { data, options } = this.props;
-    const { series } = this.state;
+  const renderLegend = () => {
     const items: VizLegendItem[] = [];
     const defaultFormatter = (v: any) => (v == null ? '-' : v.toFixed(1));
     const theme = config.theme2;
 
     for (const s of series) {
-      const frame = s.frame(data.series);
+      const frame = s.frame(props.data.series);
       if (frame) {
         for (const item of s.legend(frame)) {
           item.getDisplayValues = () => {
-            const calcs = options.legend.calcs;
+            const calcs = props.options.legend.calcs;
 
             if (!calcs?.length) {
               return [];
@@ -158,54 +144,56 @@ export class XYChartPanel2 extends PureComponent<Props, State> {
       }
     }
 
-    if (!options.legend.showLegend) {
+    if (!props.options.legend.showLegend) {
       return null;
     }
 
     return (
-      <VizLayout.Legend placement={options.legend.placement} width={options.legend.width}>
-        <VizLegend placement={options.legend.placement} items={items} displayMode={options.legend.displayMode} />
+      <VizLayout.Legend placement={props.options.legend.placement} width={props.options.legend.width}>
+        <VizLegend
+          placement={props.options.legend.placement}
+          items={items}
+          displayMode={props.options.legend.displayMode}
+        />
       </VizLayout.Legend>
     );
   };
 
-  render() {
-    const { width, height, timeRange, data, options } = this.props;
-    const { error, facets, builder, hover, series } = this.state;
-    if (error || !builder) {
-      return (
-        <div className="panel-empty">
-          <p>{error}</p>
-        </div>
-      );
-    }
-
+  if (error || !builder) {
     return (
-      <>
-        <VizLayout width={width} height={height} legend={this.renderLegend()}>
-          {(vizWidth: number, vizHeight: number) => (
-            // <pre style={{ width: vizWidth, height: vizHeight, border: '1px solid green', margin: '0px' }}>
-            //   {JSON.stringify(scatterData, null, 2)}
-            // </pre>
-            <UPlotChart config={builder} data={facets!} width={vizWidth} height={vizHeight} timeRange={timeRange}>
-              {/*children ? children(config, alignedFrame) : null*/}
-            </UPlotChart>
-          )}
-        </VizLayout>
-        <Portal>
-          {hover && options.tooltip.mode !== TooltipDisplayMode.None && (
-            <VizTooltipContainer position={{ x: hover.pageX, y: hover.pageY }} offset={{ x: 10, y: 10 }}>
-              <TooltipView
-                options={options.tooltip}
-                allSeries={series}
-                rowIndex={hover.xIndex}
-                hoveredPointIndex={hover.scatterIndex}
-                data={data.series}
-              />
-            </VizTooltipContainer>
-          )}
-        </Portal>
-      </>
+      <div className="panel-empty">
+        <p>{error}</p>
+      </div>
     );
   }
-}
+
+  console.log(facets);
+
+  return (
+    <>
+      <VizLayout width={props.width} height={props.height} legend={renderLegend()}>
+        {(vizWidth: number, vizHeight: number) => (
+          // <pre style={{ width: vizWidth, height: vizHeight, border: '1px solid green', margin: '0px' }}>
+          //   {JSON.stringify(scatterData, null, 2)}
+          // </pre>
+          <UPlotChart config={builder} data={facets!} width={vizWidth} height={vizHeight} timeRange={props.timeRange}>
+            {/*children ? children(config, alignedFrame) : null*/}
+          </UPlotChart>
+        )}
+      </VizLayout>
+      <Portal>
+        {hover && props.options.tooltip.mode !== TooltipDisplayMode.None && (
+          <VizTooltipContainer position={{ x: hover.pageX, y: hover.pageY }} offset={{ x: 10, y: 10 }}>
+            <TooltipView
+              options={props.options.tooltip}
+              allSeries={series}
+              rowIndex={hover.xIndex}
+              hoveredPointIndex={hover.scatterIndex}
+              data={props.data.series}
+            />
+          </VizTooltipContainer>
+        )}
+      </Portal>
+    </>
+  );
+};
