03e53f5c6ed2718793f835f5014fbb26dc61bdd6
Galen <galen.kistler@grafana.com>
2022-09-01 12:20:37 -0500

    add unit tests for thanos

--- public/app/features/alerting/unified/api/buildInfo.test.ts
+++ public/app/features/alerting/unified/api/buildInfo.test.ts
@@ -29,83 +29,65 @@ beforeEach(() => {
 });
 
 describe('discoverDataSourceFeatures', () => {
-  describe('When buildinfo returns 200 response', () => {
-    it('Should return Thanos with disabled ruler API', async () => {
-      fetch = jest.fn();
+  describe('When buildinfo returns 404 error', () => {
+    it('Should return cortex with ruler API disabled when prom rules works and ruler api returns not avaiable errors', async () => {
+      fetch.mockReturnValue(
+        throwError(() => ({
+          status: 404,
+        }))
+      );
 
-      mocks.fetchTestRulerRulesGroup.mockRejectedValueOnce({
+      mocks.fetchTestRulerRulesGroup.mockRejectedValue({
+        status: 404,
         data: {
-          message: RULER_NOT_SUPPORTED_MSG,
+          message: 'page not found',
         },
-      } as RulesQueryErrorResponse);
-      mocks.fetchRules.mockRejectedValueOnce(undefined);
-
-      fetch
-        .mockReturnValueOnce(
-          of({
-            data: {
-              status: 'success',
-              data: {
-                version: '0.18.0',
-              },
-            },
-          })
-          // For the second call (to thanos flags api) return flags as this is a mock thanos datasource
-        )
-        .mockReturnValueOnce(
-          of({
-            data: {
-              'grpc-address': 'true',
-            },
-          } as ThanosFlagsResponse)
-        )
-        // Third call to rules API should fail
-        .mockRejectedValueOnce(new Error('404'));
+      });
+      mocks.fetchRules.mockResolvedValue([]);
 
       const response = await discoverDataSourceFeatures({
         url: '/datasource/proxy',
-        name: 'Prometheus',
+        name: 'Cortex',
         type: 'prometheus',
       });
 
-      expect(response.application).toBe(PromApplication.Thanos);
+      expect(response.application).toBe(PromApplication.Lotex);
       expect(response.features.rulerApiEnabled).toBe(false);
-      expect(mocks.fetchRules).not.toHaveBeenCalled();
-      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalled();
-    });
-    // it('Should return Thanos with enabled ruler API', async () => {
-    //   fetch
-    //     .mockReturnValueOnce(
-    //       of({
-    //         data: {
-    //           status: 'success',
-    //           data: {
-    //             version: '0.28.0',
-    //           },
-    //         },
-    //       })
-    //       // For the second call (to thanos flags api) return flags as this is a mock thanos datasource
-    //     )
-    //     .mockReturnValueOnce(of({
-    //       data: {
-    //         'grpc-address': 'true'
-    //       }
-    //     } as ThanosFlagsResponse))
-    //     // Third call to rules API should fail
-    //     .mockRejectedValueOnce(new Error('404'));
-    //
-    //   const response = await discoverDataSourceFeatures({
-    //     url: '/datasource/proxy',
-    //     name: 'Prometheus',
-    //     type: 'prometheus',
-    //   });
-    //
-    //   expect(response.application).toBe(PromApplication.Thanos);
-    //   expect(response.features.rulerApiEnabled).toBe(true);
-    //   expect(mocks.fetchRules).not.toHaveBeenCalled();
-    //   expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalled();
-    // });
 
+      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledTimes(1);
+      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledWith('Cortex');
+
+      expect(mocks.fetchRules).toHaveBeenCalledTimes(1);
+      expect(mocks.fetchRules).toHaveBeenCalledWith('Cortex');
+    });
+
+    it('Should return cortex with ruler API enabled when prom rules works and ruler api returns cortex error', async () => {
+      fetch.mockReturnValue(
+        throwError(() => ({
+          status: 404,
+        }))
+      );
+
+      mocks.fetchTestRulerRulesGroup.mockResolvedValue(null);
+      mocks.fetchRules.mockResolvedValue([]);
+
+      const response = await discoverDataSourceFeatures({
+        url: '/datasource/proxy',
+        name: 'Cortex',
+        type: 'prometheus',
+      });
+
+      expect(response.application).toBe(PromApplication.Lotex);
+      expect(response.features.rulerApiEnabled).toBe(true);
+
+      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledTimes(1);
+      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledWith('Cortex');
+
+      expect(mocks.fetchRules).toHaveBeenCalledTimes(1);
+      expect(mocks.fetchRules).toHaveBeenCalledWith('Cortex');
+    });
+  });
+  describe('When buildinfo returns 200 response', () => {
     it('Should return Prometheus with disabled ruler API when application and features fields are missing', async () => {
       fetch = jest.fn();
       fetch
@@ -170,11 +152,12 @@ describe('discoverDataSourceFeatures', () => {
     );
 
     it('When the data source is Loki should not call the buildinfo endpoint', async () => {
+      mocks.fetchTestRulerRulesGroup.mockResolvedValue(null);
+      mocks.fetchRules.mockResolvedValue([]);
       await discoverDataSourceFeatures({ url: '/datasource/proxy', name: 'Loki', type: 'loki' });
 
       expect(fetch).not.toBeCalled();
     });
-
     it('When the data source is Loki should test Prom and Ruler API endpoints to discover available features', async () => {
       mocks.fetchTestRulerRulesGroup.mockResolvedValue(null);
       mocks.fetchRules.mockResolvedValue([]);
@@ -190,64 +173,82 @@ describe('discoverDataSourceFeatures', () => {
       expect(mocks.fetchRules).toHaveBeenCalledTimes(1);
       expect(mocks.fetchRules).toHaveBeenCalledWith('Loki');
     });
-  });
-
-  describe('When buildinfo returns 404 error', () => {
-    it('Should return cortex with ruler API disabled when prom rules works and ruler api returns not avaiable errors', async () => {
-      fetch.mockReturnValue(
-        throwError(() => ({
-          status: 404,
-        }))
-      );
-
-      mocks.fetchTestRulerRulesGroup.mockRejectedValue({
-        status: 404,
-        data: {
-          message: 'page not found',
-        },
-      });
-      mocks.fetchRules.mockResolvedValue([]);
+    it('Should return Thanos with enabled ruler API', async () => {
+      fetch
+        .mockReturnValueOnce(
+          of({
+            data: {
+              status: 'success',
+              data: {
+                version: '0.28.0',
+              },
+            },
+          })
+          // For the second call (to thanos flags api) return flags as this is a mock thanos datasource
+        )
+        .mockReturnValueOnce(
+          of({
+            data: {
+              'grpc-address': 'true',
+            },
+          } as ThanosFlagsResponse)
+        )
+        // Third call to rules API should fail
+        .mockRejectedValueOnce(new Error('404'));
 
       const response = await discoverDataSourceFeatures({
         url: '/datasource/proxy',
-        name: 'Cortex',
+        name: 'Prometheus',
         type: 'prometheus',
       });
 
-      expect(response.application).toBe(PromApplication.Lotex);
-      expect(response.features.rulerApiEnabled).toBe(false);
-
-      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledTimes(1);
-      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledWith('Cortex');
-
-      expect(mocks.fetchRules).toHaveBeenCalledTimes(1);
-      expect(mocks.fetchRules).toHaveBeenCalledWith('Cortex');
+      expect(response.application).toBe(PromApplication.Thanos);
+      expect(response.features.rulerApiEnabled).toBe(true);
+      expect(mocks.fetchRules).not.toHaveBeenCalled();
+      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalled();
     });
 
-    it('Should return cortex with ruler API enabled when prom rules works and ruler api returns cortex error', async () => {
-      fetch.mockReturnValue(
-        throwError(() => ({
-          status: 404,
-        }))
-      );
+    // Something in this test is breaking subsequent tests, so its got to run last...
+    it('Should return Thanos with disabled ruler API', async () => {
+      fetch = jest.fn();
 
-      mocks.fetchTestRulerRulesGroup.mockResolvedValue(null);
-      mocks.fetchRules.mockResolvedValue([]);
+      mocks.fetchTestRulerRulesGroup.mockRejectedValueOnce({
+        data: {
+          message: RULER_NOT_SUPPORTED_MSG,
+        },
+      } as RulesQueryErrorResponse);
+      mocks.fetchRules.mockRejectedValueOnce(undefined);
+
+      fetch
+        .mockReturnValueOnce(
+          of({
+            data: {
+              status: 'success',
+              data: {
+                version: '0.18.0',
+              },
+            },
+          })
+          // For the second call (to thanos flags api) return flags as this is a mock thanos datasource
+        )
+        .mockReturnValueOnce(
+          of({
+            data: {
+              'grpc-address': 'true',
+            },
+          } as ThanosFlagsResponse)
+        );
 
       const response = await discoverDataSourceFeatures({
         url: '/datasource/proxy',
-        name: 'Cortex',
+        name: 'Prometheus',
         type: 'prometheus',
       });
 
-      expect(response.application).toBe(PromApplication.Lotex);
-      expect(response.features.rulerApiEnabled).toBe(true);
-
-      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledTimes(1);
-      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalledWith('Cortex');
-
-      expect(mocks.fetchRules).toHaveBeenCalledTimes(1);
-      expect(mocks.fetchRules).toHaveBeenCalledWith('Cortex');
+      expect(response.application).toBe(PromApplication.Thanos);
+      expect(response.features.rulerApiEnabled).toBe(false);
+      expect(mocks.fetchRules).not.toHaveBeenCalled();
+      expect(mocks.fetchTestRulerRulesGroup).toHaveBeenCalled();
     });
   });
 });
