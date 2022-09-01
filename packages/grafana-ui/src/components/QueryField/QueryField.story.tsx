ce10906ebf3138408de81f2af63d11b938a8e6d3
Tima Gixe <timagixe@protonmail.com>
2022-09-02 00:19:48 +0300

    Storybook: Add controls to QueryField story

--- packages/grafana-ui/src/components/QueryField/QueryField.story.tsx
+++ packages/grafana-ui/src/components/QueryField/QueryField.story.tsx
@@ -1,18 +1,57 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import { QueryField } from '@grafana/ui';
-
+import { TypeaheadInput } from '../../types';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
+
+import { QueryField, QueryFieldProps } from './QueryField';
+
+const disabledControl = {
+  table: {
+    disable: true,
+  },
+};
 
 const meta: ComponentMeta<typeof QueryField> = {
   title: 'Data Source/QueryField',
   component: QueryField,
   decorators: [withCenteredStory],
+  argTypes: {
+    onTypeahead: disabledControl,
+    onChange: disabledControl,
+    onBlur: disabledControl,
+    onClick: disabledControl,
+    onRunQuery: disabledControl,
+    onRichValueChange: disabledControl,
+    onWillApplySuggestion: disabledControl,
+    portalOrigin: disabledControl,
+    additionalPlugins: disabledControl,
+    cleanText: disabledControl,
+    syntax: disabledControl,
+    syntaxLoaded: disabledControl,
+    placeholder: {
+      control: 'text',
+    },
+    query: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
 };
 
-export const basic: ComponentStory<typeof QueryField> = () => {
-  return <QueryField portalOrigin="mock-origin" query="" />;
+export const Basic: ComponentStory<typeof QueryField> = (args: Omit<QueryFieldProps, 'theme'>) => (
+  <QueryField {...args} />
+);
+
+Basic.args = {
+  onTypeahead: async (_input: TypeaheadInput) => ({
+    suggestions: [],
+  }),
+  query: 'Query text',
+  placeholder: 'Placeholder text',
+  disabled: false,
 };
 
 export default meta;
