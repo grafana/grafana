2f881d11b67ba01eb65c60d831f57e141f253132
Jeff Levin <jeff@levinology.com>
2022-09-01 08:59:03 -0800

    fix formatting

--- public/app/features/manage-dashboards/components/PublicDashboardListTable.tsx
+++ public/app/features/manage-dashboards/components/PublicDashboardListTable.tsx
@@ -1,25 +1,43 @@
+import { css } from '@emotion/css';
 import React, { FC, useState, useEffect } from 'react';
 
-import { LinkButton, Link, Switch, Icon, ClipboardButton } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data';
+import { Link, Field, Switch, ClipboardButton, useStyles2 } from '@grafana/ui';
 import {
   ListPublicDashboardResponse,
   listPublicDashboards,
   generatePublicDashboardUrl,
 } from 'app/features/dashboard/components/ShareModal/SharePublicDashboardUtils';
 
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    link: css`
+      color: ${theme.colors.primary.text};
+      text-decoration: underline;
+      margin-right: ${theme.spacing()};
+    `,
+  };
+}
+
 export const PublicDashboardListTable: FC = () => {
+  const styles = useStyles2(getStyles);
   const [publicDashboards, setPublicDashboards] = useState<ListPublicDashboardResponse[]>([]);
 
   useEffect(() => {
     listPublicDashboards(setPublicDashboards).catch();
   }, []);
 
+  function togglePublicDashboard() {}
+
   function publicDashboardLinks(pd: ListPublicDashboardResponse) {
     if (pd.isEnabled) {
       return (
         <>
-          <Link href={generatePublicDashboardUrl(pd.accessToken)}>/public-dashboards/{pd.accessToken}</Link>
+          <Link className={styles.link} href={generatePublicDashboardUrl(pd.accessToken)}>
+            /public-dashboards/{pd.accessToken}
+          </Link>
           <ClipboardButton
+            size="sm"
             variant="primary"
             icon="copy"
             getText={() => {
@@ -31,18 +49,21 @@ export const PublicDashboardListTable: FC = () => {
         </>
       );
     } else {
-      return <Link href={generatePublicDashboardUrl(pd.accessToken)}>/public-dashboards/{pd.accessToken}</Link>;
+      return (
+        <Link className={styles.link} href={generatePublicDashboardUrl(pd.accessToken)}>
+          /public-dashboards/{pd.accessToken}
+        </Link>
+      );
     }
   }
 
   return (
     <div className="page-action-bar">
-      <table className="filter-table form-inline filter-table--hover">
+      <table className="filter-table">
         <thead>
           <tr>
-            <th>Dashboard Title</th>
-            <th>Dashboard Uid</th>
-            <th>Url</th>
+            <th>Dashboard</th>
+            <th>Public Dashboard Url</th>
             <th>Enabled</th>
           </tr>
         </thead>
@@ -50,15 +71,13 @@ export const PublicDashboardListTable: FC = () => {
           {publicDashboards.map((pd) => (
             <tr key={pd.uid}>
               <td>
-                <LinkButton size="sm" href={`/d/${pd.dashboardUid}`}>
-                  <Icon size="sm" name="arrow-up" />
-                </LinkButton>{' '}
-                {pd.title}
+                <Link className={styles.link} href={`/d/${pd.dashboardUid}`}>
+                  {pd.title}
+                </Link>
               </td>
-              <td>{pd.dashboardUid}</td>
               <td>{publicDashboardLinks(pd)}</td>
-              <td>
-                <Switch value={pd.isEnabled} disabled={false} />
+              <td style={{ lineHeight: '20px' }}>
+                <Switch value={pd.isEnabled} onChange={togglePublicDashboard} />
               </td>
             </tr>
           ))}
