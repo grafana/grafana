e5aeb0f2320f0ea6cdf540b79107822cad8b73c2
Matias Chomicki <matyax@gmail.com>
2022-09-05 10:03:49 +0200

    fix(elasticsearch): make 8 the default and fallback version

--- public/app/plugins/datasource/elasticsearch/configuration/ConfigEditor.test.tsx
+++ public/app/plugins/datasource/elasticsearch/configuration/ConfigEditor.test.tsx
@@ -38,7 +38,7 @@ describe('ConfigEditor', () => {
     expect(mockOnOptionsChange).toHaveBeenCalledWith(
       expect.objectContaining({
         jsonData: expect.objectContaining({
-          esVersion: '7.0.0',
+          esVersion: '8.0.0',
           timeField: '@timestamp',
           maxConcurrentShardRequests: 5,
         }),
