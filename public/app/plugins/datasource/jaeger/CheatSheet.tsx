05c6af74c81d0ecf022d377bbd92ee00c86bcb70
Hamas Shafiq <hamas.shafiq@grafana.com>
2022-09-05 18:03:28 +0100

    Wording

--- public/app/plugins/datasource/jaeger/CheatSheet.tsx
+++ public/app/plugins/datasource/jaeger/CheatSheet.tsx
@@ -4,6 +4,20 @@ export default function CheatSheet() {
   return (
     <>
       <h2 id="jaeger-cheat-sheet">Jaeger Cheat Sheet</h2>
+      <p>
+        Documentation for the Jaeger data source can be found{' '}
+        <a
+          href="https://grafana.com/docs/grafana/latest/datasources/jaeger"
+          target="_blank"
+          rel="noreferrer"
+          style={{ color: 'blue' }}
+        >
+          here,
+        </a>{' '}
+        but here is a quick overview of the query types:
+      </p>
+
+      <hr />
       <ul>
         <li>
           Search - filter traces by service name. Addtionally, you can filter by tags or min/max duration, as well as
@@ -24,17 +38,6 @@ export default function CheatSheet() {
           to see an example of a valid JSON file.
         </li>
       </ul>
-      <p>
-        More information about Jaeger can be found{' '}
-        <a
-          href="https://grafana.com/docs/grafana/latest/datasources/jaeger"
-          target="_blank"
-          rel="noreferrer"
-          style={{ color: 'blue' }}
-        >
-          here.
-        </a>
-      </p>
     </>
   );
 }
