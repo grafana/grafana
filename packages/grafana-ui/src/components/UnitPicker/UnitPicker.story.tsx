738abd8fc80b7253f8c149b6860315b87f9692c3
Tima Gixe <timagixe@protonmail.com>
2022-09-05 11:37:34 +0300

    Storybook: add controls to `UnitPicker`

--- packages/grafana-ui/src/components/UnitPicker/UnitPicker.story.tsx
+++ packages/grafana-ui/src/components/UnitPicker/UnitPicker.story.tsx
@@ -7,15 +7,25 @@ import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 
 import mdx from './UnitPicker.mdx';
 
+type Props = React.ComponentProps<typeof UnitPicker>;
+
 const meta: ComponentMeta<typeof UnitPicker> = {
   title: 'Pickers and Editors/UnitPicker',
   component: UnitPicker,
   decorators: [withCenteredStory],
   parameters: {
+    controls: {
+      exclude: ['onChange', 'value'],
+    },
     docs: mdx,
   },
 };
 
-export const simple: ComponentStory<typeof UnitPicker> = () => <UnitPicker onChange={(val) => console.log(val)} />;
+export const Basic: ComponentStory<typeof UnitPicker> = (args: Props) => <UnitPicker {...args} />;
+
+Basic.args = {
+  onChange: console.log,
+  width: 30,
+};
 
 export default meta;
