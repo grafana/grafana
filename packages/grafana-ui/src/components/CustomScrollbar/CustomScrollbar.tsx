a694c3f4ceac58624665d060e81fc46f92a9e568
Ashley Harrison <ashley.harrison@grafana.com>
2022-09-05 13:44:47 +0100

    Extract ScrollIndicators logic into it's own file

--- packages/grafana-ui/src/components/CustomScrollbar/CustomScrollbar.tsx
+++ packages/grafana-ui/src/components/CustomScrollbar/CustomScrollbar.tsx
@@ -1,12 +1,12 @@
 import { css, cx } from '@emotion/css';
-import classNames from 'classnames';
-import React, { FC, RefCallback, useCallback, useEffect, useRef, useState } from 'react';
+import React, { FC, RefCallback, useCallback, useEffect, useRef } from 'react';
 import Scrollbars, { positionValues } from 'react-custom-scrollbars-2';
 
 import { GrafanaTheme2 } from '@grafana/data';
 
 import { useStyles2 } from '../../themes';
-import { Icon } from '../Icon/Icon';
+
+import { ScrollIndicators } from './ScrollIndicators';
 
 export type ScrollbarPosition = positionValues;
 
@@ -50,10 +50,6 @@ export const CustomScrollbar: FC<Props> = ({
   children,
 }) => {
   const ref = useRef<Scrollbars & { view: HTMLDivElement }>(null);
-  const [showScrollTopIndicator, setShowTopScrollIndicator] = useState(false);
-  const [showScrollBottomIndicator, setShowBottomScrollIndicator] = useState(false);
-  const scrollTopMarker = useRef<HTMLDivElement>(null);
-  const scrollBottomMarker = useRef<HTMLDivElement>(null);
   const styles = useStyles2(getStyles);
 
   useEffect(() => {
@@ -83,28 +79,6 @@ export const CustomScrollbar: FC<Props> = ({
       }
     }, updateAfterMountMs);
   }, [updateAfterMountMs]);
-
-  // Here we observe the top and bottom markers to determine if we should show the scroll indicators
-  useEffect(() => {
-    if (showScrollIndicators) {
-      const intersectionObserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (entry.target === scrollTopMarker.current) {
-            setShowTopScrollIndicator(!entry.isIntersecting);
-          } else if (entry.target === scrollBottomMarker.current) {
-            setShowBottomScrollIndicator(!entry.isIntersecting);
-          }
-        });
-      });
-      [scrollTopMarker, scrollBottomMarker].forEach((ref) => {
-        if (ref.current) {
-          intersectionObserver.observe(ref.current);
-        }
-      });
-      return () => intersectionObserver.disconnect();
-    }
-    return;
-  }, [showScrollIndicators]);
 
   function renderTrack(className: string, hideTrack: boolean | undefined, passedProps: JSX.IntrinsicElements['div']) {
     if (passedProps.style && hideTrack) {
@@ -144,38 +118,12 @@ export const CustomScrollbar: FC<Props> = ({
     ref.current && setScrollTop && setScrollTop(ref.current.getValues());
   }, [setScrollTop]);
 
-  const scrollChildren = showScrollIndicators ? (
-    <>
-      <div
-        className={cx(styles.scrollIndicator, styles.scrollTopIndicator, {
-          [styles.scrollIndicatorVisible]: showScrollTopIndicator,
-        })}
-      >
-        <Icon className={classNames(styles.scrollIcon, styles.scrollTopIcon)} name="angle-up" />
-      </div>
-      <div className={styles.scrollContent}>
-        <div ref={scrollTopMarker} />
-        {children}
-        <div ref={scrollBottomMarker} />
-      </div>
-      <div
-        className={cx(styles.scrollIndicator, styles.scrollBottomIndicator, {
-          [styles.scrollIndicatorVisible]: showScrollBottomIndicator,
-        })}
-      >
-        <Icon className={classNames(styles.scrollIcon, styles.scrollBottomIcon)} name="angle-down" />
-      </div>
-    </>
-  ) : (
-    children
-  );
-
   return (
     <Scrollbars
       data-testid={testId}
       ref={ref}
       className={cx(styles.customScrollbar, className, {
-        [styles.scrollContainer]: showScrollIndicators,
+        [styles.scrollbarWithScrollIndicators]: showScrollIndicators,
       })}
       onScrollStop={onScrollStop}
       autoHeight={true}
@@ -193,7 +141,7 @@ export const CustomScrollbar: FC<Props> = ({
       renderView={renderView}
       onScroll={onScroll}
     >
-      {scrollChildren}
+      {showScrollIndicators ? <ScrollIndicators>{children}</ScrollIndicators> : children}
     </Scrollbars>
   );
 };
@@ -245,50 +193,14 @@ const getStyles = (theme: GrafanaTheme2) => {
         }
       }
     `,
-    scrollContent: css({
-      flex: 1,
-      position: 'relative',
-    }),
     // override the scroll container position so that the scroll indicators
     // are positioned at the top and bottom correctly.
     // react-custom-scrollbars doesn't provide any way for us to hook in nicely,
     // so we have to override with !important. feelsbad.
-    scrollContainer: css`
+    scrollbarWithScrollIndicators: css`
       .scrollbar-view {
         position: static !important;
       }
     `,
-    scrollIndicator: css({
-      height: theme.spacing(6),
-      left: 0,
-      opacity: 0,
-      pointerEvents: 'none',
-      position: 'absolute',
-      right: 0,
-      transition: theme.transitions.create('opacity'),
-      zIndex: theme.zIndex.sidemenu,
-    }),
-    scrollTopIndicator: css({
-      background: `linear-gradient(0deg, transparent, ${theme.colors.background.canvas})`,
-      top: 0,
-    }),
-    scrollBottomIndicator: css({
-      background: `linear-gradient(180deg, transparent, ${theme.colors.background.canvas})`,
-      bottom: 0,
-    }),
-    scrollIndicatorVisible: css({
-      opacity: 1,
-    }),
-    scrollIcon: css({
-      left: '50%',
-      position: 'absolute',
-      transform: 'translateX(-50%)',
-    }),
-    scrollTopIcon: css({
-      top: 0,
-    }),
-    scrollBottomIcon: css({
-      bottom: 0,
-    }),
   };
 };
