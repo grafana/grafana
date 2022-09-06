ce7866b51589802cab5df7b5cd776650d7e01cfb
Torkel Ödegaard <torkel@grafana.com>
2022-09-06 19:00:32 +0200

    Dashboard: Unsaved changes warning fixes (#54706)
    * Quick test of alternative method
    
    * change order of if condition again for better readability
    
    * Think I have something that is working
    
    * Update
    
    (cherry picked from commit e5c32c8cc9ef18041dd25df0d3b5db860128aefd)

--- public/app/features/dashboard/state/PanelModel.ts
+++ public/app/features/dashboard/state/PanelModel.ts
@@ -61,6 +61,7 @@ const notPersistedProperties: { [str: string]: boolean } = {
   queryRunner: true,
   replaceVariables: true,
   configRev: true,
+  hasSavedPanelEditChange: true,
   getDisplayTitle: true,
   dataSupport: true,
   key: true,
@@ -161,7 +162,7 @@ export class PanelModel implements DataConfigSource, IPanelModel {
   links?: DataLink[];
   declare transparent: boolean;
 
-  libraryPanel?: { uid: undefined; name: string } | PanelModelLibraryPanel;
+  libraryPanel?: { uid: undefined; name: string; version?: number } | PanelModelLibraryPanel;
 
   autoMigrateFrom?: string;
 
@@ -170,6 +171,7 @@ export class PanelModel implements DataConfigSource, IPanelModel {
   isEditing = false;
   isInView = false;
   configRev = 0; // increments when configs change
+  hasSavedPanelEditChange?: boolean;
   hasRefreshed?: boolean;
   cacheTimeout?: string | null;
   cachedPluginOptions: Record<string, PanelOptionsCache> = {};
