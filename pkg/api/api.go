79b652e3947f77193e79d0c9e1d3646af018b3ad
gamab <gabi.mabs@gmail.com>
2022-09-07 13:30:45 +0200

    First set of permissions
    Minimize code
    
    Typo
    
    Enforce RBAC on listing plugins
    
    Adding check to GetPluginSettingByID
    
    Adding comment
    
    Now using two scopes
    
    Add Resolver
    
    Co-authored-by: Kalle Persson <kalle.persson@grafana.com>
    
    use variable instead of redefining its value
    
    Shorten ReqCanReadPlugin
    
    Do not cover settings just yet because it conflicts with app plugins access
    
    Co-authored-by: Jguer <joao.guerreiro@grafana.com>
    
    Removing protection of the settings endpoint and increasing protection of the install endpoint
    
    Co-authored-by: Jguer <joao.guerreiro@grafana.com>
    
    Adding read all to PluginsExternalReader
    
    Protecting /plugins frontend endpoints
    
    Co-authored-by: Will Browne <will.browne@grafana.com>
    
    Correct test
    
    Redefine role
    
    Use real ac in test
    
    Register resolver in test
    
    Fixing the expected error
    
    Lower security on front pages access with RBAC
    
    Adding plugins:read to the frontend
    
    Allow Viewers to list non-core plugins
    
    Fix test
    
    WIP on adding meta to gnet plugins
    
    Metadata for remote plugins as well
    
    Remove unused import
    
    Protect settings with read

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -370,8 +370,8 @@ func (hs *HTTPServer) registerRoutes() {
 
 		if hs.Cfg.PluginAdminEnabled && !hs.Cfg.PluginAdminExternalManageEnabled {
 			apiRoute.Group("/plugins", func(pluginRoute routing.RouteRegister) {
-				pluginRoute.Post("/:pluginId/install", authorize(reqGrafanaAdmin, ac.EvalPermission(plugins.ActionInstall)), routing.Wrap(hs.InstallPlugin))
-				pluginRoute.Post("/:pluginId/uninstall", authorize(reqGrafanaAdmin, ac.EvalPermission(plugins.ActionInstall)), routing.Wrap(hs.UninstallPlugin))
+				pluginRoute.Post("/:pluginId/install", authorize(reqGrafanaAdmin, plugins.InstallEvaluator(ac.Parameter(":pluginId"))), routing.Wrap(hs.InstallPlugin))
+				pluginRoute.Post("/:pluginId/uninstall", authorize(reqGrafanaAdmin, plugins.InstallEvaluator(ac.Parameter(":pluginId"))), routing.Wrap(hs.UninstallPlugin))
 			})
 		}
 
--- pkg/api/plugins_test.go
+++ pkg/api/plugins_test.go
@@ -101,8 +101,14 @@ func Test_PluginsInstallAndUninstall(t *testing.T) {
 }
 
 func Test_PluginsInstallAndUninstall_AccessControl(t *testing.T) {
-	canInstall := []ac.Permission{{Action: plugins.ActionInstall}}
-	cannotInstall := []ac.Permission{{Action: "plugins:cannotinstall"}}
+	canInstall := []ac.Permission{
+		{Action: plugins.ActionInstall},
+		{Action: plugins.ActionRead, Scope: plugins.ScopeProvider.GetResourceScope("test")},
+	}
+	cannotInstall := []ac.Permission{
+		{Action: plugins.ActionInstall},
+		{Action: plugins.ActionRead, Scope: plugins.ScopeProvider.GetResourceScope("not-test")},
+	}
 
 	type testCase struct {
 		expectedCode                     int
