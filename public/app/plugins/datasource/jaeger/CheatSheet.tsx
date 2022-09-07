c3d38f3876bab910d1532084897fb3487ac6276f
Hamas Shafiq <hamas.shafiq@grafana.com>
2022-09-07 15:25:00 +0100

    Use paragraphs instead of bullet points

--- public/app/plugins/datasource/jaeger/CheatSheet.tsx
+++ public/app/plugins/datasource/jaeger/CheatSheet.tsx
@@ -10,34 +10,33 @@ export default function CheatSheet() {
           href="https://grafana.com/docs/grafana/latest/datasources/jaeger"
           target="_blank"
           rel="noreferrer"
-          style={{ color: 'blue' }}
+          style={{ color: '#1F62E0' }}
         >
           here,
         </a>{' '}
         but here is a quick overview of the query types:
       </p>
 
-      <hr />
-      <ul>
-        <li>
-          Search - filter traces by service name. Addtionally, you can filter by tags or min/max duration, as well as
-          limit the number of traces that are returned.
-        </li>
-        <li>TraceID - if you have a trace ID, simply enter the trace ID to see the trace.</li>
-        <li>
-          JSON File - you can upload a JSON file that contains a single trace to visualize it. If the file has multiple
-          traces then the first trace is used for visualization. Click{' '}
-          <a
-            href="https://grafana.com/docs/grafana/latest/datasources/jaeger/#upload-json-trace-file"
-            target="_blank"
-            rel="noreferrer"
-            style={{ color: '#1F62E0' }}
-          >
-            here
-          </a>{' '}
-          to see an example of a valid JSON file.
-        </li>
-      </ul>
+      <p>
+        Search - filter traces by service name. Addtionally, you can filter by tags or min/max duration, as well as
+        limit the number of traces that are returned.
+      </p>
+
+      <p>TraceID - if you have a trace ID, simply enter the trace ID to see the trace.</p>
+
+      <p>
+        JSON File - you can upload a JSON file that contains a single trace to visualize it. If the file has multiple
+        traces then the first trace is used for visualization. Click{' '}
+        <a
+          href="https://grafana.com/docs/grafana/latest/datasources/jaeger/#upload-json-trace-file"
+          target="_blank"
+          rel="noreferrer"
+          style={{ color: '#1F62E0' }}
+        >
+          here
+        </a>{' '}
+        to see an example of a valid JSON file.
+      </p>
     </>
   );
 }
