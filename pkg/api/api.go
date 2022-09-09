aef20d953f535311f4b5b69601a52afcb088f53e
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-09-09 18:09:07 +0300

    Chore: fix formating for swagger definitions

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,29 +3,29 @@
 // The Grafana backend exposes an HTTP API, the same API is used by the frontend to do
 // everything from saving dashboards, creating users and updating data sources.
 //
-// Schemes: http, https
-// BasePath: /api
-// Version: 0.0.1
-// License: GNU Affero General Public License v3.0 https://www.gnu.org/licenses/agpl-3.0.en.html
-// Contact: Grafana Labs<hello@grafana.com> https://grafana.com
+//	Schemes: http, https
+//	BasePath: /api
+//	Version: 0.0.1
+//	License: GNU Affero General Public License v3.0 https://www.gnu.org/licenses/agpl-3.0.en.html
+//	Contact: Grafana Labs<hello@grafana.com> https://grafana.com
 //
-// Consumes:
-// - application/json
+//	Consumes:
+//	- application/json
 //
-// Produces:
-// - application/json
+//	Produces:
+//	- application/json
 //
-// Security:
-// - basic:
-// - api_key:
+//	Security:
+//	- basic:
+//	- api_key:
 //
-// SecurityDefinitions:
-// basic:
-//  type: basic
-// api_key:
-//  type: apiKey
-//  name: Authorization
-//  in: header
+//	SecurityDefinitions:
+//	basic:
+//	 type: basic
+//	api_key:
+//	 type: apiKey
+//	 name: Authorization
+//	 in: header
 //
 // swagger:meta
 package api
