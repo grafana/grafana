0aab1d510a95fd8c39e55809a2711cab4fc77f4f
dsotirakis <dimitrios.sotirakis@grafana.com>
2022-09-13 13:14:22 +0300

    Fix lint

--- pkg/build/cmd/exportversion.go
+++ pkg/build/cmd/exportversion.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/urfave/cli/v2"
-	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/urfave/cli/v2"
 )
 
 func ExportVersion(c *cli.Context) error {
@@ -18,12 +18,12 @@ func ExportVersion(c *cli.Context) error {
 	if err := os.RemoveAll(distDir); err != nil {
 		return err
 	}
-	if err := os.Mkdir(distDir, 0775); err != nil {
+	if err := os.Mkdir(distDir, 0750); err != nil {
 		return err
 	}
 
 	// nolint:gosec
-	if err := ioutil.WriteFile(filepath.Join(distDir, "grafana.version"), []byte(version), 0664); err != nil {
+	if err := os.WriteFile(filepath.Join(distDir, "grafana.version"), []byte(version), 0664); err != nil {
 		return err
 	}
 
