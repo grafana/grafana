70112156dc01f69ede0f58457df7f81e7e5da31e
Konrad Lalik <konrad.lalik@grafana.com>
2022-09-13 12:47:38 +0200

    Improve types

--- public/app/features/alerting/unified/components/rule-editor/ExpressionEditor.tsx
+++ public/app/features/alerting/unified/components/rule-editor/ExpressionEditor.tsx
@@ -1,16 +1,24 @@
+import { css } from '@emotion/css';
 import { noop } from 'lodash';
-import React, { FC, useCallback, useMemo, useRef } from 'react';
-import { useAsync, useObservable } from 'react-use';
+import React, { FC, useCallback, useMemo } from 'react';
+import { useAsync, useAsyncFn } from 'react-use';
+import { lastValueFrom } from 'rxjs';
 
-import { CoreApp, DataQuery, DataSourceInstanceSettings } from '@grafana/data';
-import { getDataSourceSrv } from '@grafana/runtime';
-import { Button } from '@grafana/ui/src';
+import {
+  CoreApp,
+  DataFrameJSON,
+  DataQuery,
+  DataSourceInstanceSettings,
+  FieldSchema,
+  GrafanaTheme2,
+} from '@grafana/data';
+import { getBackendSrv, getDataSourceSrv } from '@grafana/runtime';
+import { Alert, Button, Icon, TagList, Tooltip, useStyles2 } from '@grafana/ui';
 import { LokiQuery } from 'app/plugins/datasource/loki/types';
 import { PromQuery } from 'app/plugins/datasource/prometheus/types';
 
-import { AlertingQueryRunner } from '../../state/AlertingQueryRunner';
-import { dataQueryToAlertQuery } from '../../utils/query';
-import { RuleViewerVisualization } from '../rule-viewer/RuleViewerVisualization';
+import { isGrafanaAlertState } from '../../../../../types/unified-alerting-dto';
+import { AlertStateTag } from '../rules/AlertStateTag';
 
 export interface ExpressionEditorProps {
   value?: string;
@@ -18,13 +26,15 @@ export interface ExpressionEditorProps {
   dsSettings: DataSourceInstanceSettings; // will be a prometheus or loki datasource
 }
 
+interface CloudRulePreviewResponse {
+  instances: DataFrameJSON[];
+}
+
 export const ExpressionEditor: FC<ExpressionEditorProps> = ({ value, onChange, dsSettings }) => {
-  const queryRunner = useRef(new AlertingQueryRunner());
-  const queryData = useObservable(queryRunner.current.get());
+  const styles = useStyles2(getStyles);
 
   const { mapToValue, mapToQuery } = useQueryMappers(dsSettings.name);
   const dataQuery = mapToQuery({ refId: 'A', hide: false }, value);
-  const alertQuery = dataQueryToAlertQuery(dataQuery, dsSettings.uid);
 
   const {
     error,
@@ -41,8 +51,20 @@ export const ExpressionEditor: FC<ExpressionEditorProps> = ({ value, onChange, d
     [onChange, mapToValue]
   );
 
+  const [previewState, fetchPreview] = useAsyncFn(() => {
+    return lastValueFrom(
+      getBackendSrv().fetch<CloudRulePreviewResponse>({
+        method: 'POST',
+        url: `/api/v1/rule/test/${dsSettings.uid}`,
+        data: {
+          expr: mapToValue(dataQuery),
+        },
+      })
+    );
+  }, [dsSettings, dataQuery]);
+
   const onRunQueriesClick = async () => {
-    await queryRunner.current.run([dataQueryToAlertQuery(dataQuery, dsSettings.uid)]);
+    await fetchPreview();
   };
 
   if (loading || dataSource?.name !== dsSettings.name) {
@@ -53,6 +75,13 @@ export const ExpressionEditor: FC<ExpressionEditorProps> = ({ value, onChange, d
     const errorMessage = error?.message || 'Data source plugin does not export any Query Editor component';
     return <div>Could not load query editor due to: {errorMessage}</div>;
   }
+
+  const previewLoaded = !!previewState.value?.ok && !previewState.loading && !previewState.error;
+  const previewInstances = previewState.value?.data?.instances ?? [];
+  // The preview API returns arrays with empty elements when there are no firing alerts
+  const previewHasAlerts = previewInstances.every((instance) =>
+    instance?.data?.values.every((value) => value.length > 0)
+  );
 
   const QueryEditor = dataSource?.components?.QueryEditor;
 
@@ -66,14 +95,122 @@ export const ExpressionEditor: FC<ExpressionEditorProps> = ({ value, onChange, d
         onRunQuery={noop}
         datasource={dataSource}
       />
-      <Button icon="sync" type="button" onClick={onRunQueriesClick}>
-        Run queries
-      </Button>
 
-      <RuleViewerVisualization query={alertQuery} data={queryData && queryData[alertQuery.refId]} />
+      <div className={styles.preview}>
+        <Button type="button" onClick={onRunQueriesClick} disabled={previewState.loading}>
+          Preview alerts
+        </Button>
+        {previewLoaded && (
+          <Alert title="Alerts preview" severity="info" className={styles.previewAlert}>
+            {previewHasAlerts ? (
+              <>Preview based on the result of running the query for this moment.</>
+            ) : (
+              <>There are no firing alerts for your query.</>
+            )}
+          </Alert>
+        )}
+        {previewHasAlerts &&
+          previewInstances.map((frame, index) => {
+            const fields: FieldSchema[] = frame.schema?.fields ?? [];
+            const values: string[][] = frame.data?.values ?? [];
+
+            const labelFields = fields.filter((field) => !['State', 'Info'].includes(field.name));
+            const stateFieldIndex = fields.findIndex((field) => field.name === 'State');
+            const infoFieldIndex = fields.findIndex((field) => field.name === 'Info');
+
+            const labelIndexes = labelFields.map((labelField) => fields.indexOf(labelField));
+
+            return (
+              <table key={index} className={styles.table}>
+                <thead>
+                  <tr>
+                    <th>State</th>
+                    <th>Labels</th>
+                    <th>Info</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.from({ length: values[stateFieldIndex].length }, (v, i) => i).map((index) => {
+                    const labelValues = labelIndexes.map((labelIndex) => [
+                      fields[labelIndex].name,
+                      values[labelIndex][index],
+                    ]);
+                    const state = values[stateFieldIndex][index];
+                    const info = values[infoFieldIndex][index];
+
+                    return (
+                      <tr key={index}>
+                        <td>{isGrafanaAlertState(state) && <AlertStateTag state={state} />}</td>
+                        <td>
+                          <TagList tags={labelValues.map(([key, value]) => `${key}=${value}`)} />
+                        </td>
+                        <td>
+                          <Tooltip content={info}>
+                            <Icon name="info-circle" />
+                          </Tooltip>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            );
+          })}
+      </div>
     </>
   );
 };
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  table: css`
+    width: 100%;
+
+    td,
+    th {
+      padding: ${theme.spacing(1, 0)};
+    }
+
+    td + td,
+    th + th {
+      padding-left: ${theme.spacing(1)};
+    }
+
+    thead th:nth-child(1) {
+      width: 80px;
+    }
+
+    thead th:nth-child(2) {
+      width: auto;
+    }
+
+    thead th:nth-child(3) {
+      width: 40px;
+    }
+
+    td:nth-child(3) {
+      text-align: center;
+    }
+
+    tbody tr:nth-child(2n + 1) {
+      background-color: ${theme.colors.background.secondary};
+    }
+  `,
+  preview: css`
+    padding: ${theme.spacing(2, 0)};
+    max-width: ${theme.breakpoints.values.md}px;
+  `,
+  previewAlert: css`
+    margin: ${theme.spacing(1, 0)};
+  `,
+  previewGrid: css`
+    margin: ${theme.spacing(2, 0)};
+    display: grid;
+    gap: ${theme.spacing(2)};
+    justify-content: flex-start;
+    align-items: center;
+    grid-template-columns: min-content 1fr min-content;
+  `,
+});
 
 type QueryMappers<T extends DataQuery = DataQuery> = {
   mapToValue: (query: T) => string;
