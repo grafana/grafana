5bb72d5ef0b5098978951f178b656b8bcd68b025
yaellec <yaelle.chaudy@grafana.com>
2022-09-13 16:52:26 +0200

    revert azure tracking changes

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/tracking.ts
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/tracking.ts
@@ -9,7 +9,7 @@ import { reportInteraction } from '@grafana/runtime';
  * - stats about number of azure dashboards loaded, number of users
  * - stats about the grafana and plugins versions used by our users
  *
- * Dashboard: Ad0pti0N
+ * Dashboard: https://ops.grafana.net/d/Ad0pti0N/data-sources-adoption-tracking?orgId=1
  * Changelog:
  * - v9.1.0 : list of queries logged
  * - v9.1.2 : list removed in favour of stats, user_id removed
@@ -34,27 +34,4 @@ export type AzureMonitorDashboardLoadedProps = {
   azure_log_analytics_queries_hidden: number;
   /** number of hidden queries (not executed) of type Azure Resource Graph if any  */
   azure_resource_graph_queries_hidden: number;
-};
-
-/**
- * Used to track data source creation via either the specific plugin page `/plugins/grafana-azure-monitor-datasource`
- * or the general datasources page `/datasources/new`
- *
- * This event corresponds to the start event of our data source creation funnel.
- * Combined with the end event, it allows answering questions about:
- * - Conversion (percentage of user that successfully set up a data source)
- * - Time spent on the config page
- *
- * Dashboard: Ad0pti0N
- * Changelog:
- * - v9.2.0 : logging datasource_id, grafana version and the source of event
- */
-export const trackAzureMonitorDataSourceCreated = (props: AzureMonitorDataSourceCreatedProps) => {
-  reportInteraction('grafana_ds_azuremonitor_add_datasource_clicked', props);
-};
-
-type AzureMonitorDataSourceCreatedProps = {
-  grafana_version?: string;
-  datasource_id: string;
-  source: 'plugins/azure' | 'datasources/new';
 };
