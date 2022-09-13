761514a4d17f5ea0f6ad5bc2d32034e2de41c9cc
nmarrs <nathanielmarrs@gmail.com>
2022-09-13 13:08:24 -0700

    Initial set up of metric value canvas element

--- public/app/features/canvas/elements/metricValue.tsx
+++ public/app/features/canvas/elements/metricValue.tsx
@@ -1,56 +1,52 @@
 import { css } from '@emotion/css';
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { stylesFactory } from '@grafana/ui';
-import { config } from 'app/core/config';
+import { DataFrame, FieldNamePickerConfigSettings, GrafanaTheme2, StandardEditorsRegistryItem } from '@grafana/data';
+import { usePanelContext, useStyles2 } from '@grafana/ui';
+import { FieldNamePicker } from '@grafana/ui/src/components/MatchersUI/FieldNamePicker';
+import { TextDimensionMode } from 'app/features/dimensions';
 import { DimensionContext } from 'app/features/dimensions/context';
 import { ColorDimensionEditor } from 'app/features/dimensions/editors/ColorDimensionEditor';
 import { TextDimensionEditor } from 'app/features/dimensions/editors/TextDimensionEditor';
-import { ColorDimensionConfig, TextDimensionConfig } from 'app/features/dimensions/types';
 
 import { CanvasElementItem, CanvasElementProps, defaultBgColor, defaultTextColor } from '../element';
 
-export enum Align {
-  Left = 'left',
-  Center = 'center',
-  Right = 'right',
-}
+import { Align, TextBoxConfig, TextBoxData, VAlign } from './textBox';
 
-export enum VAlign {
-  Top = 'top',
-  Middle = 'middle',
-  Bottom = 'bottom',
-}
+const dummyFieldSettings: StandardEditorsRegistryItem<string, FieldNamePickerConfigSettings> = {
+  settings: {},
+} as any;
 
-export interface TextBoxData {
-  text?: string;
-  color?: string;
-  size?: number; // 0 or missing will "auto size"
-  align: Align;
-  valign: VAlign;
-}
+const MetricValueDisplay = (props: CanvasElementProps<TextBoxConfig, TextBoxData>) => {
+  const styles = useStyles2(getStyles(props.data));
 
-export interface TextBoxConfig {
-  text?: TextDimensionConfig;
-  color?: ColorDimensionConfig;
-  size?: number; // 0 or missing will "auto size"
-  align: Align;
-  valign: VAlign;
-}
-
-class TextBoxDisplay extends PureComponent<CanvasElementProps<TextBoxConfig, TextBoxData>> {
-  render() {
-    const { data } = this.props;
-    const styles = getStyles(config.theme2, data);
-    return (
-      <div className={styles.container}>
-        <span className={styles.span}>{data?.text}</span>
-      </div>
-    );
+  if (!props?.data?.text) {
+    return <MetricValueInlineEdit {...props} />;
   }
-}
-const getStyles = stylesFactory((theme: GrafanaTheme2, data) => ({
+  return (
+    <div className={styles.container}>
+      <span className={styles.span}>{props.data?.text}</span>
+    </div>
+  );
+};
+
+const MetricValueInlineEdit = (props: CanvasElementProps<TextBoxConfig, TextBoxData>) => {
+  const context = usePanelContext();
+  const data = context.instanceState?.scene?.data.series as DataFrame[];
+  const styles = useStyles2(getStyles(props.data));
+  return (
+    <div className={styles.container}>
+      <FieldNamePicker
+        context={{ data }}
+        value={''}
+        onChange={(foo: any) => console.log(foo)}
+        item={dummyFieldSettings}
+      />
+    </div>
+  );
+};
+
+const getStyles = (data: TextBoxData | undefined) => (theme: GrafanaTheme2) => ({
   container: css`
     position: absolute;
     height: 100%;
@@ -59,18 +55,19 @@ const getStyles = stylesFactory((theme: GrafanaTheme2, data) => ({
   `,
   span: css`
     display: table-cell;
-    vertical-align: ${data.valign};
-    text-align: ${data.align};
+    vertical-align: ${data?.valign};
+    text-align: ${data?.align};
     font-size: ${data?.size}px;
     color: ${data?.color};
   `,
-}));
-export const textBoxItem: CanvasElementItem<TextBoxConfig, TextBoxData> = {
-  id: 'text-box',
-  name: 'Text',
-  description: 'Text box',
+});
 
-  display: TextBoxDisplay,
+export const metricValueItem: CanvasElementItem<TextBoxConfig, TextBoxData> = {
+  id: 'metric-value',
+  name: 'Metric Value',
+  description: 'Display a field value',
+
+  display: MetricValueDisplay,
 
   defaultSize: {
     width: 240,
@@ -85,6 +82,8 @@ export const textBoxItem: CanvasElementItem<TextBoxConfig, TextBoxData> = {
       color: {
         fixed: defaultTextColor,
       },
+      text: { mode: TextDimensionMode.Field, fixed: '', field: '' },
+      size: 24,
     },
     background: {
       color: {
@@ -93,7 +92,6 @@ export const textBoxItem: CanvasElementItem<TextBoxConfig, TextBoxData> = {
     },
   }),
 
-  // Called when data changes
   prepareData: (ctx: DimensionContext, cfg: TextBoxConfig) => {
     const data: TextBoxData = {
       text: cfg.text ? ctx.getText(cfg.text).value() : '',
@@ -109,9 +107,8 @@ export const textBoxItem: CanvasElementItem<TextBoxConfig, TextBoxData> = {
     return data;
   },
 
-  // Heatmap overlay options
   registerOptionsUI: (builder) => {
-    const category = ['Text box'];
+    const category = ['Metric value'];
     builder
       .addCustomEditor({
         category,
