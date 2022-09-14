c3ec9cc7af2d9caa8535f7c6c6405129a3c53497
Ivana Huckova <ivana.huckova@gmail.com>
2022-09-14 16:28:53 +0200

    Refactor to use QueryPatternsCard component

--- public/app/plugins/datasource/loki/querybuilder/components/QueryPatternsModal.tsx
+++ public/app/plugins/datasource/loki/querybuilder/components/QueryPatternsModal.tsx
@@ -1,17 +1,17 @@
 import { css } from '@emotion/css';
 import { capitalize } from 'lodash';
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { DataQuery, GrafanaTheme2 } from '@grafana/data';
-import { Button, Card, Collapse, Modal, useStyles2 } from '@grafana/ui';
+import { Button, Collapse, Modal, useStyles2 } from '@grafana/ui';
 import { getNextRefIdChar } from 'app/core/utils/query';
-import { RawQuery } from 'app/plugins/datasource/prometheus/querybuilder/shared/RawQuery';
 
-import logqlGrammar from '../../syntax';
 import { LokiQuery } from '../../types';
 import { lokiQueryModeller } from '../LokiQueryModeller';
 import { buildVisualQueryFromString } from '../parsing';
 import { LokiQueryPattern, LokiQueryPatternType } from '../types';
+
+import { QueryPatternsCard } from './QueryPatternsCard';
 
 type Props = {
   isOpen: boolean;
@@ -25,13 +25,14 @@ type Props = {
 export const QueryPatternsModal = (props: Props) => {
   const { isOpen, onClose, onChange, onAddQuery, query, queries } = props;
   const [openTabs, setOpenTabs] = useState<string[]>([]);
-  const [selectedPatternConfirmationIndex, setSelectedPatternConfirmationIndex] = useState<number | null>(null);
-  const indexOfSelectedCard = useRef<number | null>(null);
+  const [selectedPatternName, setSelectedPatternName] = useState<string | null>(null);
 
   const styles = useStyles2(getStyles);
-  const lang = { grammar: logqlGrammar, name: 'logql' };
-  const hasPreviousQuery = buildVisualQueryFromString(query.expr).query.operations.length > 0;
   const hasNewQueryOption = !!onAddQuery;
+  const hasPreviousQuery = useMemo(
+    () => buildVisualQueryFromString(query.expr).query.operations.length > 0,
+    [query.expr]
+  );
 
   const onPatternSelect = (pattern: LokiQueryPattern, selectAsNewQuery = false) => {
     const queryString = selectAsNewQuery ? '' : query.expr;
@@ -50,7 +51,7 @@ export const QueryPatternsModal = (props: Props) => {
         expr: lokiQueryModeller.renderQuery(visualQuery.query),
       });
     }
-    setSelectedPatternConfirmationIndex(null);
+    setSelectedPatternName(null);
     onClose();
   };
 
@@ -77,66 +78,16 @@ export const QueryPatternsModal = (props: Props) => {
               {lokiQueryModeller
                 .getQueryPatterns()
                 .filter((pattern) => pattern.type === patternType)
-                .map((pattern, index) => (
-                  <Card key={pattern.name} className={styles.card}>
-                    <Card.Heading>{pattern.name}</Card.Heading>
-                    <div className={styles.rawQueryContainer}>
-                      <RawQuery
-                        query={lokiQueryModeller.renderQuery({ labels: [], operations: pattern.operations })}
-                        lang={lang}
-                        className={styles.rawQuery}
-                      />
-                    </div>
-                    <Card.Actions>
-                      {selectedPatternConfirmationIndex !== index ? (
-                        <Button
-                          size="sm"
-                          onClick={() => {
-                            if (hasPreviousQuery) {
-                              // If user has previous query, we need to confirm that they want to replace it
-                              indexOfSelectedCard.current = index;
-                              setSelectedPatternConfirmationIndex(index);
-                            } else {
-                              onPatternSelect(pattern);
-                            }
-                          }}
-                        >
-                          Use this query
-                        </Button>
-                      ) : (
-                        <>
-                          <div className={styles.spacing}>
-                            {`If you would like to use this query, ${
-                              hasNewQueryOption
-                                ? 'you can either replace your current query or create a new query'
-                                : 'your current query will be replaced'
-                            }.`}
-                          </div>
-                          <Button size="sm" fill="outline" onClick={() => setSelectedPatternConfirmationIndex(null)}>
-                            Back
-                          </Button>
-                          <Button
-                            size="sm"
-                            onClick={() => {
-                              onPatternSelect(pattern);
-                            }}
-                          >
-                            Replace query
-                          </Button>
-                          {hasNewQueryOption && (
-                            <Button
-                              size="sm"
-                              onClick={() => {
-                                onPatternSelect(pattern, true);
-                              }}
-                            >
-                              Create new query
-                            </Button>
-                          )}
-                        </>
-                      )}
-                    </Card.Actions>
-                  </Card>
+                .map((pattern) => (
+                  <QueryPatternsCard
+                    key={pattern.name}
+                    pattern={pattern}
+                    hasNewQueryOption={hasNewQueryOption}
+                    hasPreviousQuery={hasPreviousQuery}
+                    onPatternSelect={onPatternSelect}
+                    selectedPatternName={selectedPatternName}
+                    setSelectedPatternName={setSelectedPatternName}
+                  />
                 ))}
             </div>
           </Collapse>
@@ -156,19 +107,6 @@ const getStyles = (theme: GrafanaTheme2) => {
       flex-direction: row;
       flex-wrap: wrap;
       justify-content: space-between;
-    `,
-    card: css`
-      width: 49.5%;
-      display: flex;
-      flex-direction: column;
-    `,
-    rawQueryContainer: css`
-      flex-grow: 1;
-    `,
-    rawQuery: css`
-      background-color: ${theme.colors.background.primary};
-      padding: ${theme.spacing(1)};
-      margin-top: ${theme.spacing(1)};
     `,
     spacing: css`
       margin-bottom: ${theme.spacing(1)};
