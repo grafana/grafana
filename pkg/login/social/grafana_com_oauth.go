3b6849da5c8ead184c8988d73e9e17c8efc38861
gamab <gabi.mabs@gmail.com>
2022-09-14 11:47:38 +0200

    Not using token
    Co-authored-by: Jguer <joao.guerreiro@grafana.com>

--- pkg/login/social/grafana_com_oauth.go
+++ pkg/login/social/grafana_com_oauth.go
@@ -45,7 +45,7 @@ func (s *SocialGrafanaCom) IsOrganizationMember(organizations []OrgRecord) bool 
 	return false
 }
 
-func (s *SocialGrafanaCom) UserInfo(client *http.Client, token *oauth2.Token) (*BasicUserInfo, error) {
+func (s *SocialGrafanaCom) UserInfo(client *http.Client, _ *oauth2.Token) (*BasicUserInfo, error) {
 	var data struct {
 		Id    int         `json:"id"`
 		Name  string      `json:"name"`
