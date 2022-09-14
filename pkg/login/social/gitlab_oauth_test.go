ca692ec73f8bfe55b702be311ea749fd214b7636
gamab <gabi.mabs@gmail.com>
2022-09-14 09:37:55 +0200

    Cover more options

--- pkg/login/social/gitlab_oauth_test.go
+++ pkg/login/social/gitlab_oauth_test.go
@@ -20,10 +20,10 @@ const (
 	rootUserRespBody   = `{"id":1,"username":"root","name":"Administrator","state":"active","email":"root@example.org","is_admin":true,"namespace_id":1}`
 	editorUserRespBody = `{"id":3,"username":"gitlab-editor","name":"Gitlab Editor","state":"active","email":"gitlab-editor@example.org","is_admin":false,"namespace_id":1}`
 
-	adminGroup       = `{"id":4,"web_url":"http://grafana-gitlab.local/groups/admins","name":"Admins","path":"admins","project_creation_level":"developer","full_name":"Admins","full_path":"admins","created_at":"2022-09-13T19:38:04.891Z"}`
-	editorGroup      = `{"id":5,"web_url":"http://grafana-gitlab.local/groups/editors","name":"Editors","path":"editors","project_creation_level":"developer","full_name":"Editors","full_path":"editors","created_at":"2022-09-13T19:38:15.074Z"}`
-	viewerGroup      = `{"id":6,"web_url":"http://grafana-gitlab.local/groups/viewers","name":"Viewers","path":"viewers","project_creation_level":"developer","full_name":"Viewers","full_path":"viewers","created_at":"2022-09-13T19:38:25.777Z"}`
-	serverAdminGroup = `{"id":7,"web_url":"http://grafana-gitlab.local/groups/serveradmins","name":"ServerAdmins","path":"serveradmins","project_creation_level":"developer","full_name":"ServerAdmins","full_path":"serveradmins","created_at":"2022-09-13T19:38:36.227Z"}`
+	adminGroup  = `{"id":4,"web_url":"http://grafana-gitlab.local/groups/admins","name":"Admins","path":"admins","project_creation_level":"developer","full_name":"Admins","full_path":"admins","created_at":"2022-09-13T19:38:04.891Z"}`
+	editorGroup = `{"id":5,"web_url":"http://grafana-gitlab.local/groups/editors","name":"Editors","path":"editors","project_creation_level":"developer","full_name":"Editors","full_path":"editors","created_at":"2022-09-13T19:38:15.074Z"}`
+	viewerGroup = `{"id":6,"web_url":"http://grafana-gitlab.local/groups/viewers","name":"Viewers","path":"viewers","project_creation_level":"developer","full_name":"Viewers","full_path":"viewers","created_at":"2022-09-13T19:38:25.777Z"}`
+	// serverAdminGroup = `{"id":7,"web_url":"http://grafana-gitlab.local/groups/serveradmins","name":"ServerAdmins","path":"serveradmins","project_creation_level":"developer","full_name":"ServerAdmins","full_path":"serveradmins","created_at":"2022-09-13T19:38:36.227Z"}`
 )
 
 func TestSocialGitlab_UserInfo(t *testing.T) {
@@ -33,55 +33,73 @@ func TestSocialGitlab_UserInfo(t *testing.T) {
 		},
 	}
 
-	tests := []struct {
-		Name                    string
+	type conf struct {
 		AllowAssignGrafanaAdmin bool
-		UserRespBody            string
-		GroupsRespBody          string
-		RoleAttributePath       string
-		ExpectedLogin           string
-		ExpectedEmail           string
-		ExpectedRole            org.RoleType
-		ExpectedGrafanaAdmin    *bool
+		RoleAttributeStrict     bool
+		DefaultOrgRole          org.RoleType
+	}
+
+	tests := []struct {
+		Name                 string
+		Cfg                  conf
+		UserRespBody         string
+		GroupsRespBody       string
+		RoleAttributePath    string
+		ExpectedLogin        string
+		ExpectedEmail        string
+		ExpectedRole         org.RoleType
+		ExpectedGrafanaAdmin *bool
 	}{
 		{
-			Name:                    "Server Admin Allowed",
-			AllowAssignGrafanaAdmin: true,
-			UserRespBody:            rootUserRespBody,
-			GroupsRespBody:          "[" + strings.Join([]string{adminGroup, editorGroup, viewerGroup, serverAdminGroup}, ",") + "]",
-			RoleAttributePath:       gitlabAttrPath,
-			ExpectedLogin:           "root",
-			ExpectedEmail:           "root@example.org",
-			ExpectedRole:            "Admin",
-			ExpectedGrafanaAdmin:    trueBoolPtr(),
+			Name:                 "Server Admin Allowed",
+			Cfg:                  conf{AllowAssignGrafanaAdmin: true},
+			UserRespBody:         rootUserRespBody,
+			GroupsRespBody:       "[" + strings.Join([]string{adminGroup, editorGroup, viewerGroup}, ",") + "]",
+			RoleAttributePath:    gitlabAttrPath,
+			ExpectedLogin:        "root",
+			ExpectedEmail:        "root@example.org",
+			ExpectedRole:         "Admin",
+			ExpectedGrafanaAdmin: trueBoolPtr(),
 		},
 		{ // Edge case, he is in the Viewer Group, Server Admin is disabled but the attribute path has a condition for it
-			Name:                    "Server Admin Disabled",
-			AllowAssignGrafanaAdmin: false,
-			UserRespBody:            rootUserRespBody,
-			GroupsRespBody:          "[" + strings.Join([]string{viewerGroup}, ",") + "]",
-			RoleAttributePath:       gitlabAttrPath,
-			ExpectedLogin:           "root",
-			ExpectedEmail:           "root@example.org",
-			ExpectedRole:            "Admin",
-			ExpectedGrafanaAdmin:    nil,
+			Name:                 "Server Admin Disabled",
+			Cfg:                  conf{AllowAssignGrafanaAdmin: false},
+			UserRespBody:         rootUserRespBody,
+			GroupsRespBody:       "[" + strings.Join([]string{viewerGroup}, ",") + "]",
+			RoleAttributePath:    gitlabAttrPath,
+			ExpectedLogin:        "root",
+			ExpectedEmail:        "root@example.org",
+			ExpectedRole:         "Admin",
+			ExpectedGrafanaAdmin: nil,
 		},
 		{
-			Name:                    "Editor",
-			AllowAssignGrafanaAdmin: true,
-			UserRespBody:            editorUserRespBody,
-			GroupsRespBody:          "[" + strings.Join([]string{viewerGroup, editorGroup}, ",") + "]",
-			RoleAttributePath:       gitlabAttrPath,
-			ExpectedLogin:           "gitlab-editor",
-			ExpectedEmail:           "gitlab-editor@example.org",
-			ExpectedRole:            "Editor",
-			ExpectedGrafanaAdmin:    falseBoolPtr(),
+			Name:                 "Editor",
+			Cfg:                  conf{AllowAssignGrafanaAdmin: true},
+			UserRespBody:         editorUserRespBody,
+			GroupsRespBody:       "[" + strings.Join([]string{viewerGroup, editorGroup}, ",") + "]",
+			RoleAttributePath:    gitlabAttrPath,
+			ExpectedLogin:        "gitlab-editor",
+			ExpectedEmail:        "gitlab-editor@example.org",
+			ExpectedRole:         "Editor",
+			ExpectedGrafanaAdmin: falseBoolPtr(),
+		},
+		{
+			Name:                 "Fallback to default",
+			Cfg:                  conf{AllowAssignGrafanaAdmin: true, DefaultOrgRole: org.RoleViewer},
+			UserRespBody:         editorUserRespBody,
+			GroupsRespBody:       "[" + strings.Join([]string{}, ",") + "]",
+			RoleAttributePath:    gitlabAttrPath,
+			ExpectedLogin:        "gitlab-editor",
+			ExpectedEmail:        "gitlab-editor@example.org",
+			ExpectedRole:         "Viewer",
+			ExpectedGrafanaAdmin: falseBoolPtr(),
 		},
 	}
 
 	for _, test := range tests {
 		provider.roleAttributePath = test.RoleAttributePath
-		provider.allowAssignGrafanaAdmin = test.AllowAssignGrafanaAdmin
+		provider.allowAssignGrafanaAdmin = test.Cfg.AllowAssignGrafanaAdmin
+		provider.autoAssignOrgRole = string(test.Cfg.DefaultOrgRole)
 
 		t.Run(test.Name, func(t *testing.T) {
 			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
