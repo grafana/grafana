9d2b494d43849af68a91a5e45cefb7f67a695c85
Piotr Jamróz <pm.jamroz@gmail.com>
2022-09-15 15:54:24 +0200

    Ensure top nav is rendered when correlations are disabled
    This is for the case when:
    
    [feature_toggles]
    topnav = true

--- public/app/features/correlations/CorrelationsFeatureToggle.tsx
+++ public/app/features/correlations/CorrelationsFeatureToggle.tsx
@@ -1,20 +1,10 @@
-import { css } from '@emotion/css';
 import React from 'react';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { useStyles2 } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 
 export default function FeatureTogglePage() {
-  const styles = useStyles2(
-    (theme: GrafanaTheme2) =>
-      css`
-        margin-top: ${theme.spacing(2)};
-      `
-  );
-
   return (
-    <Page className={styles}>
+    <Page navId="correlations">
       <Page.Contents>
         <h1>Correlations are disabled</h1>
         To enable Correlations, add it in the Grafana config:
