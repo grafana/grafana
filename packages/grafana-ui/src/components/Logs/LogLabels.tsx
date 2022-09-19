4335f44adb33fac912e54ecf1566fb0dc9d95b3d
Gábor Farkas <gabor.farkas@gmail.com>
2022-09-19 13:00:47 +0200

    improved deprecation-message

--- packages/grafana-ui/src/components/Logs/LogLabels.tsx
+++ packages/grafana-ui/src/components/Logs/LogLabels.tsx
@@ -12,7 +12,7 @@ interface Props {
   labels: Labels;
 }
 
-/** @deprecated */
+/** @deprecated will be removed in the next major version */
 export const LogLabels: FunctionComponent<Props> = ({ labels }) => {
   const styles = useStyles2(getStyles);
   const displayLabels = Object.keys(labels).filter((label) => !label.startsWith('_') && !HIDDEN_LABELS.includes(label));
