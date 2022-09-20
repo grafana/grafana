40cfbe7bcbb514e40f4cb55b2814e1cd5760cada
Konrad Lalik <konrad.lalik@grafana.com>
2022-09-20 10:43:39 +0200

    Alerting: Fix default query's data source when no default datasource specified (#55435)
    (cherry picked from commit 20e6e0d59392e5a03b7ceb54bf7d16134a5f1484)

--- public/app/features/alerting/unified/utils/datasource.ts
+++ public/app/features/alerting/unified/utils/datasource.ts
@@ -176,7 +176,7 @@ export function getDatasourceAPIUid(dataSourceName: string) {
 }
 
 export function getFirstCompatibleDataSource(): DataSourceInstanceSettings<DataSourceJsonData> | undefined {
-  return getRulesDataSources()[0];
+  return getDataSourceSrv().getList({ alerting: true })[0];
 }
 
 export function getDefaultOrFirstCompatibleDataSource(): DataSourceInstanceSettings<DataSourceJsonData> | undefined {
