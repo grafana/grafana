492ad3fef1b95924fcac35daf21e64c5ceabb706
Juan Cabanas <juani_cabanas@hotmail.com>
2022-09-20 10:58:53 -0300

    msw setup modified

--- public/test/jest.setup.ts
+++ public/test/jest.setup.ts
@@ -4,14 +4,11 @@ import { server } from './mocks/server';
 // enable API mocking in test runs using the same request handlers
 // as for the client-side mocking.
 beforeAll(() => {
-  console.log('está andando bien');
-  server.listen({ onUnhandledRequest: 'error' });
+  server.listen({ onUnhandledRequest: 'bypass' });
 });
 afterAll(() => {
-  console.log('after all');
   server.close();
 });
 afterEach(() => {
-  console.log('after each');
   server.resetHandlers();
 });
