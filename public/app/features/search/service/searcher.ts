9ab695d3029e090704b70b8d4959839c1d2f4fd1
Ryan McKinley <ryantxu@gmail.com>
2022-09-20 08:24:03 -0700

    merge main

--- public/app/features/search/service/searcher.ts
+++ public/app/features/search/service/searcher.ts
@@ -7,8 +7,8 @@ import { GrafanaSearcher } from './types';
 let searcher: GrafanaSearcher | undefined = undefined;
 
 export function getGrafanaSearcher(): GrafanaSearcher {
-  const sqlSearcher = new SQLSearcher();
   if (!searcher) {
+    const sqlSearcher = new SQLSearcher();
     const useBluge = config.featureToggles.panelTitleSearch;
     searcher = useBluge ? new BlugeSearcher(sqlSearcher) : sqlSearcher;
   }
