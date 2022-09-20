7f8e101303ebf767a85e2f437cde903ed19fd913
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-09-21 00:31:41 +0200

    Search: rename copy/paste

--- pkg/setting/setting_search.go
+++ pkg/setting/setting_search.go
@@ -15,9 +15,9 @@ type SearchSettings struct {
 func readSearchSettings(iniFile *ini.File) SearchSettings {
 	s := SearchSettings{}
 
-	previewsCrawlerSection := iniFile.Section("search")
-	s.DashboardLoadingBatchSize = previewsCrawlerSection.Key("dashboard_loading_batch_size").MustInt(200)
-	s.FullReindexInterval = previewsCrawlerSection.Key("full_reindex_interval").MustDuration(5 * time.Minute)
-	s.IndexUpdateInterval = previewsCrawlerSection.Key("index_update_interval").MustDuration(10 * time.Second)
+	searchSection := iniFile.Section("search")
+	s.DashboardLoadingBatchSize = searchSection.Key("dashboard_loading_batch_size").MustInt(200)
+	s.FullReindexInterval = searchSection.Key("full_reindex_interval").MustDuration(5 * time.Minute)
+	s.IndexUpdateInterval = searchSection.Key("index_update_interval").MustDuration(10 * time.Second)
 	return s
 }
