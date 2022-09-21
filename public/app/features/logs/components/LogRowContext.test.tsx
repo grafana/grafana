e8c2e4b48a9362ed08e501fbf29a9b5f889f7da8
Sven Grossmann <svennergr@gmail.com>
2022-09-21 15:13:45 +0200

    add tests

--- public/app/features/logs/components/LogRowContext.test.tsx
+++ public/app/features/logs/components/LogRowContext.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
-import { LogRowModel } from '@grafana/data';
+import { LogRowModel, LogsSortOrder } from '@grafana/data';
 
 import { LogGroupPosition, LogRowContextGroup } from './LogRowContext';
 
@@ -22,5 +22,30 @@ describe('LogRowContextGroup component', () => {
       </div>
     );
     expect(screen.getAllByTestId('ansiLogLine')).toHaveLength(2);
+  });
+
+  it.each([
+    [LogGroupPosition.Top, LogsSortOrder.Ascending, 'before'],
+    [LogGroupPosition.Top, LogsSortOrder.Descending, 'after'],
+    [LogGroupPosition.Bottom, LogsSortOrder.Ascending, 'after'],
+    [LogGroupPosition.Bottom, LogsSortOrder.Descending, 'before'],
+  ])(`should when component is %s and sorting is %s display '%s'`, async (groupPosition, logsSortOrder, expected) => {
+    const defaultProps = {
+      rows: ['Log 1', 'Log 2', 'Log 3'],
+      onLoadMoreContext: () => {},
+      canLoadMoreRows: false,
+      row: {} as LogRowModel,
+      className: '',
+      groupPosition,
+      logsSortOrder,
+    };
+
+    render(
+      <div>
+        <LogRowContextGroup {...defaultProps} />
+      </div>
+    );
+
+    expect(await screen.findByText(`Showing 3 lines ${expected} match.`)).toBeInTheDocument();
   });
 });
