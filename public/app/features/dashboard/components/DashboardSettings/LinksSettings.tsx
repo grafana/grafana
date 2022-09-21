4f7ef527aef625714ae63eef0b2a18ed7be7744e
eledobleefe <laura.fernandez@grafana.com>
2022-09-21 13:02:41 +0200

    Fix breadcrumbs and menu link to Link page

--- public/app/features/dashboard/components/DashboardSettings/LinksSettings.tsx
+++ public/app/features/dashboard/components/DashboardSettings/LinksSettings.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
 import { NavModelItem } from '@grafana/data';
+import { locationService } from '@grafana/runtime';
 import { Page } from 'app/core/components/PageNew/Page';
-import { useGrafana } from 'app/core/context/GrafanaContext';
 
 import { LinkSettingsEdit, LinkSettingsList } from '../LinksSettings';
 import { newLink } from '../LinksSettings/LinkSettingsEdit';
@@ -11,50 +11,41 @@ import { SettingsPageProps } from './types';
 
 export type LinkSettingsMode = 'list' | 'new' | 'edit';
 
-export function LinksSettings({ dashboard, sectionNav }: SettingsPageProps) {
-  const [editIdx, setEditIdx] = useState<number | null>(null);
+export function LinksSettings({ dashboard, sectionNav, editIndex }: SettingsPageProps) {
   const [isNew, setIsNew] = useState<boolean>(false);
-  const { chrome } = useGrafana();
-
-  React.useEffect(() => {
-    if (sectionNav) {
-      chrome.update({
-        sectionNav: sectionNav.node,
-      });
-    }
-  }, [sectionNav, chrome]);
 
   const onGoBack = () => {
-    setEditIdx(null);
+    locationService.partial({ editIndex: undefined });
     setIsNew(false);
   };
 
   const onNew = () => {
     dashboard.links = [...dashboard.links, { ...newLink }];
-    setEditIdx(dashboard.links.length - 1);
+    locationService.partial({ editIndex: dashboard.links.length - 1 });
     setIsNew(true);
   };
 
   const onEdit = (idx: number) => {
-    setEditIdx(idx);
+    locationService.partial({ editIndex: idx });
     setIsNew(false);
   };
 
-  const isEditing = editIdx !== null;
+  const isEditing = editIndex !== undefined;
 
   let pageNav: NavModelItem | undefined;
   if (isEditing) {
     const title = isNew ? 'New link' : 'Edit link';
+    const description = isNew ? 'Create a new link on your dashboard' : 'Edit a specific link of your dashboard';
     pageNav = {
       text: title,
-      subTitle: 'Manage a specific link',
+      subTitle: description,
     };
   }
 
   return (
     <Page navModel={sectionNav} pageNav={pageNav}>
       {!isEditing && <LinkSettingsList dashboard={dashboard} onNew={onNew} onEdit={onEdit} />}
-      {isEditing && <LinkSettingsEdit dashboard={dashboard} editLinkIdx={editIdx!} onGoBack={onGoBack} />}
+      {isEditing && <LinkSettingsEdit dashboard={dashboard} editLinkIdx={editIndex!} onGoBack={onGoBack} />}
     </Page>
   );
 }
