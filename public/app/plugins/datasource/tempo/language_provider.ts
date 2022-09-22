1f7a4757b5405a15dcff790213d3ffdc58c0215d
Hamas Shafiq <hamas.shafiq@grafana.com>
2022-09-22 09:42:39 +0100

    Only wrap the value in quotes when selected

--- public/app/plugins/datasource/tempo/language_provider.ts
+++ public/app/plugins/datasource/tempo/language_provider.ts
@@ -75,16 +75,13 @@ export default class TempoLanguageProvider extends LanguageProvider {
     tagName = tagName.split('=')[0];
 
     const response = await this.request(`/api/search/tag/${tagName}/values`, []);
-    // Values that contain a space must be wrapped in quotes for the query to return results.
-    // Wrap all values in quotes as this has no negative impact on values that don't contain a space.
-    const tagValues = response.tagValues.map((value: string) => `"${value}"`);
 
     const suggestions: CompletionItemGroup[] = [];
 
-    if (response && tagValues) {
+    if (response && response.tagValues) {
       suggestions.push({
         label: `Tag Values`,
-        items: tagValues.map((tagValue: string) => ({ label: tagValue })),
+        items: response.tagValues.map((tagValue: string) => ({ label: tagValue, insertText: `"${tagValue}"` })),
       });
     }
     return { suggestions };
