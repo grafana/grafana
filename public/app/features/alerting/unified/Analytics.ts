20c544f8a5578d695d2514c28a3b2c1517b3aa74
Virginia Cepeda <virginia.cepeda@grafana.com>
2022-09-23 16:24:30 -0300

    Add messages for new trackings

--- public/app/features/alerting/unified/Analytics.ts
+++ public/app/features/alerting/unified/Analytics.ts
@@ -2,4 +2,7 @@ export const LogMessages = {
   filterByLabel: 'filtering alert instances by label',
   loadedList: 'loaded Alert Rules list',
   leavingRuleGroupEdit: 'leaving rule group edit without saving',
+  alertRuleFromPanel: 'creating alert rule from panel',
+  alertRuleFromScratch: 'creating alert rule from scratch',
+  clickingAlertStateFilters: 'clicking alert state filters',
 };
