ed2795f24c426d4f1ac086221b6abf04461d1a3b
Torkel Ödegaard <torkel@grafana.com>
2022-09-25 18:18:48 +0200

    Fixing more tests and adding support for weight config

--- pkg/setting/setting_navigation.go
+++ pkg/setting/setting_navigation.go
@@ -1,6 +1,9 @@
 package setting
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 type NavigationAppConfig struct {
 	SectionID  string
@@ -8,7 +11,7 @@ type NavigationAppConfig struct {
 }
 
 func (cfg *Cfg) readNavigationSettings() {
-	sec := cfg.Raw.Section("navigation")
+	sec := cfg.Raw.Section("navigation.apps")
 	cfg.NavigationAppConfig = map[string]NavigationAppConfig{
 		"grafana-k8s-app":                  {SectionID: "monitoring", SortWeight: 1},
 		"grafana-synthetic-monitoring-app": {SectionID: "monitoring", SortWeight: 2},
@@ -23,7 +26,17 @@ func (cfg *Cfg) readNavigationSettings() {
 	for _, key := range sec.Keys() {
 		if strings.HasPrefix(key.Name(), "nav_id_") {
 			pluginId := strings.Replace(key.Name(), "nav_id_", "", 1)
-			cfg.NavigationAppConfig[pluginId] = NavigationAppConfig{SectionID: sec.Key(key.Name()).MustString("")}
+			// Support <id> <weight> value
+			values := strings.Split(sec.Key(key.Name()).MustString(""), " ")
+
+			appCfg := &NavigationAppConfig{SectionID: values[0]}
+			if len(values) > 1 {
+				if weight, err := strconv.ParseInt(values[1], 10, 64); err == nil {
+					appCfg.SortWeight = weight
+				}
+			}
+
+			cfg.NavigationAppConfig[pluginId] = *appCfg
 		}
 	}
 }
--- pkg/setting/setting_navigation_test.go
+++ pkg/setting/setting_navigation_test.go
@@ -6,22 +6,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNavigationSettingDefaults(t *testing.T) {
-	cfg := NewCfg()
-	_, _ = cfg.Raw.NewSection("navigation")
-	cfg.readNavigationSettings()
-
-	require.Equal(t, "alerting", cfg.NavigationAppConfig["grafana-k8s-app"].SectionID)
-}
-
 func TestNavigationSettings(t *testing.T) {
-	cfg := NewCfg()
-	sec, _ := cfg.Raw.NewSection("navigation.apps")
-	_, _ = sec.NewKey("nav_id_grafana-k8s-app", "dashboards")
-	_, _ = sec.NewKey("nav_id_other-app", "admin")
 
-	cfg.readNavigationSettings()
+	t.Run("Should include defaults", func(t *testing.T) {
+		cfg := NewCfg()
+		_, _ = cfg.Raw.NewSection("navigation.apps")
+		cfg.readNavigationSettings()
 
-	require.Equal(t, "dashboards", cfg.NavigationAppConfig["grafana-k8s-app"].SectionID)
-	require.Equal(t, "admin", cfg.NavigationAppConfig["other-app"].SectionID)
+		require.Equal(t, "monitoring", cfg.NavigationAppConfig["grafana-k8s-app"].SectionID)
+	})
+
+	t.Run("Can add additional overrides via ini system", func(t *testing.T) {
+		cfg := NewCfg()
+		sec, _ := cfg.Raw.NewSection("navigation.apps")
+		_, _ = sec.NewKey("nav_id_grafana-k8s-app", "dashboards")
+		_, _ = sec.NewKey("nav_id_other-app", "admin 12")
+
+		cfg.readNavigationSettings()
+
+		require.Equal(t, "dashboards", cfg.NavigationAppConfig["grafana-k8s-app"].SectionID)
+		require.Equal(t, "admin", cfg.NavigationAppConfig["other-app"].SectionID)
+
+		require.Equal(t, int64(0), cfg.NavigationAppConfig["grafana-k8s-app"].SortWeight)
+		require.Equal(t, int64(12), cfg.NavigationAppConfig["other-app"].SortWeight)
+	})
 }
