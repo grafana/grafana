e49071bdc0cd8c4a84c6fa6bb0d15e0f4fb0e6c4
Ryan McKinley <ryantxu@gmail.com>
2022-09-26 14:55:16 -0700

    use object shape from protobuf

--- pkg/services/store/object/object.go
+++ pkg/services/store/object/object.go
@@ -13,31 +13,39 @@ type UserInfo struct {
 	Login string `protobuf:"bytes,2,opt,name=login,proto3" json:"login,omitempty"` // string ID?
 }
 
-// Raw saved object metadata
+// The canonical object/document data -- this represents the raw bytes and storage level metadata
 type RawObject struct {
-	// Unique ID for this object
+	// Unique ID
 	UID string `protobuf:"bytes,1,opt,name=UID,proto3" json:"UID,omitempty"`
 	// Identify the object kind.  This kind will be used to apply a schema to the body and
 	// will trigger additional indexing behavior.
 	Kind string `protobuf:"bytes,2,opt,name=kind,proto3" json:"kind,omitempty"`
-	// Time in millies since epoch that the object was modified
+	// Time in epoch milliseconds that the object was modified
 	Modified int64 `protobuf:"varint,3,opt,name=modified,proto3" json:"modified,omitempty"`
 	// Who modified the object
-	ModifiedBy *UserInfo `protobuf:"bytes,5,opt,name=modified_by,json=modifiedBy,proto3" json:"modified_by,omitempty"`
+	ModifiedBy *UserInfo `protobuf:"bytes,4,opt,name=modified_by,json=modifiedBy,proto3" json:"modified_by,omitempty"`
 	// Content Length
-	Size int64 `protobuf:"varint,7,opt,name=size,proto3" json:"size,omitempty"`
-	// MD5 digest of the Body
-	ETag string `protobuf:"bytes,8,opt,name=ETag,proto3" json:"ETag,omitempty"`
-	// Raw bytes of the storage object.  The kind+schema will ensure
-	Body []byte `protobuf:"bytes,9,opt,name=body,proto3" json:"body,omitempty"`
-	// A small set of properties managed by raw storage layer.  The kind+schema will define val
-	Properties map[string]string `protobuf:"bytes,10,rep,name=properties,proto3" json:"properties,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
-	// object version (might be commit hash, or number)
-	Version string `protobuf:"bytes,11,opt,name=version,proto3" json:"version,omitempty"`
-	// last time the object status was checked with an external system (provisioning/git)
-	SyncTime int64 `protobuf:"varint,12,opt,name=syncTime,proto3" json:"syncTime,omitempty"`
-	// optional "save" or "commit" message from the author
-	Comment string `protobuf:"bytes,13,opt,name=comment,proto3" json:"comment,omitempty"`
+	Size int64 `protobuf:"varint,5,opt,name=size,proto3" json:"size,omitempty"`
+	// MD5 digest of the body
+	ETag string `protobuf:"bytes,6,opt,name=ETag,proto3" json:"ETag,omitempty"`
+	// Raw bytes of the storage object.  The kind will determine what is a valid payload
+	Body []byte `protobuf:"bytes,7,opt,name=body,proto3" json:"body,omitempty"`
+	// The version will change when the object is saved. It is not necessarily sortable
+	//
+	// NOTE: currently managed by the dashboard+dashboard_version tables
+	Version string `protobuf:"bytes,8,opt,name=version,proto3" json:"version,omitempty"`
+	// optional "save" or "commit" message
+	//
+	// NOTE: currently managed by the dashboard_version table, and will be returned from a "history" command
+	Comment string `protobuf:"bytes,9,opt,name=comment,proto3" json:"comment,omitempty"`
+	// Location (path/repo/etc) that defines the canonocal form
+	//
+	// NOTE: currently managed by the dashboard_provisioning table
+	SyncSrc string `protobuf:"bytes,10,opt,name=sync_src,json=syncSrc,proto3" json:"sync_src,omitempty"`
+	// Time in epoch milliseconds that the object was last synced with an external system (provisioning/git)
+	//
+	// NOTE: currently managed by the dashboard_provisioning table
+	SyncTime int64 `protobuf:"varint,11,opt,name=sync_time,json=syncTime,proto3" json:"sync_time,omitempty"`
 }
 
 // Searchable fields extracted from the object
