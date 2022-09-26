cac693c769afe88e4b6c1c18d20314e27ba38cb7
Torkel Ödegaard <torkel@grafana.com>
2022-09-26 07:37:15 +0200

    Remove unused fake

--- pkg/api/fakes.go
+++ pkg/api/fakes.go
@@ -34,34 +34,6 @@ func (pm *fakePluginInstaller) Remove(_ context.Context, pluginID string) error 
 	return nil
 }
 
-type fakePluginStore struct {
-	plugins.Store
-
-	plugins map[string]plugins.PluginDTO
-}
-
-func (pr fakePluginStore) Plugin(_ context.Context, pluginID string) (plugins.PluginDTO, bool) {
-	p, exists := pr.plugins[pluginID]
-
-	return p, exists
-}
-
-func (pr fakePluginStore) Plugins(_ context.Context, pluginTypes ...plugins.Type) []plugins.PluginDTO {
-	var result []plugins.PluginDTO
-	if len(pluginTypes) == 0 {
-		pluginTypes = plugins.PluginTypes
-	}
-	for _, v := range pr.plugins {
-		for _, t := range pluginTypes {
-			if v.Type == t {
-				result = append(result, v)
-			}
-		}
-	}
-
-	return result
-}
-
 type fakeRendererManager struct {
 	plugins.RendererManager
 }
