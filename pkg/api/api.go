372e9cab0bf57886bfa87c808076a79a0dc37950
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-09-26 20:04:24 +0300

    Fix swagger generation faiure

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,24 +3,23 @@
 // The Grafana backend exposes an HTTP API, the same API is used by the frontend to do
 // everything from saving dashboards, creating users and updating data sources.
 //
-// Schemes: http, https
-// BasePath: /api
-// Version: 0.0.1
-// License: GNU Affero General Public License v3.0 https://www.gnu.org/licenses/agpl-3.0.en.html
-// Contact: Grafana Labs<hello@grafana.com> https://grafana.com
+//	Schemes: http, https
+//	BasePath: /api
+//	Version: 0.0.1
+//	License: GNU Affero General Public License v3.0 https://www.gnu.org/licenses/agpl-3.0.en.html
+//	Contact: Grafana Labs<hello@grafana.com> https://grafana.com
 //
-// Consumes:
-// - application/json
+//	Consumes:
+//	- application/json
 //
-// Produces:
-// - application/json
+//	Produces:
+//	- application/json
 //
-// Security:
-// - basic:
-// - api_key:
+//	Security:
+//	- basic:
+//	- api_key:
 //
-// SecurityDefinitions:
-//
+//	SecurityDefinitions:
 //	basic:
 //	 type: basic
 //
