80f198104782cc92d7afc5aa68cda1ae22a0f573
Leodegario Pasakdal <leodegario.pasakdal@grafana.com>
2022-09-26 09:50:15 +0200

    move ruleeditor smaller component to its own file

--- public/app/features/alerting/unified/RuleEditor.tsx
+++ public/app/features/alerting/unified/RuleEditor.tsx
@@ -1,61 +1,17 @@
-import { css } from '@emotion/css';
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { useAsync } from 'react-use';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { Alert, LinkButton, LoadingPlaceholder, useStyles2, withErrorBoundary } from '@grafana/ui';
-import { useCleanup } from 'app/core/hooks/useCleanup';
+import { LoadingPlaceholder, withErrorBoundary } from '@grafana/ui';
 import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
 import { useDispatch } from 'app/types';
-import { RuleIdentifier } from 'app/types/unified-alerting';
 
+import { AlertWarning } from './AlertWarning';
+import { ExistingRuleEditor } from './ExistingRuleEditor';
 import { RuleEditorWrapper } from './RuleEditorWrapper';
 import { AlertRuleForm } from './components/rule-editor/AlertRuleForm';
-import { useIsRuleEditable } from './hooks/useIsRuleEditable';
-import { useUnifiedAlertingSelector } from './hooks/useUnifiedAlertingSelector';
-import { fetchAllPromBuildInfoAction, fetchEditableRuleAction } from './state/actions';
+import { fetchAllPromBuildInfoAction } from './state/actions';
 import { useRulesAccess } from './utils/accessControlHooks';
-import { initialAsyncRequestState } from './utils/redux';
 import * as ruleId from './utils/rule-id';
-
-interface ExistingRuleEditorProps {
-  identifier: RuleIdentifier;
-}
-
-const ExistingRuleEditor: FC<ExistingRuleEditorProps> = ({ identifier }) => {
-  useCleanup((state) => (state.unifiedAlerting.ruleForm.existingRule = initialAsyncRequestState));
-  const { loading, result, error, dispatched } = useUnifiedAlertingSelector((state) => state.ruleForm.existingRule);
-  const dispatch = useDispatch();
-  const { isEditable } = useIsRuleEditable(ruleId.ruleIdentifierToRuleSourceName(identifier), result?.rule);
-
-  useEffect(() => {
-    if (!dispatched) {
-      dispatch(fetchEditableRuleAction(identifier));
-    }
-  }, [dispatched, dispatch, identifier]);
-
-  if (loading || isEditable === undefined) {
-    return <LoadingPlaceholder text="Loading rule..." />;
-  }
-
-  if (error) {
-    return (
-      <Alert severity="error" title="Failed to load rule">
-        {error.message}
-      </Alert>
-    );
-  }
-
-  if (!result) {
-    return <AlertWarning title="Rule not found">Sorry! This rule does not exist.</AlertWarning>;
-  }
-
-  if (isEditable === false) {
-    return <AlertWarning title="Cannot edit rule">Sorry! You do not have permission to edit this rule.</AlertWarning>;
-  }
-
-  return <AlertRuleForm existing={result} />;
-};
 
 type RuleEditorProps = GrafanaRouteComponentProps<{ id?: string }>;
 
@@ -92,18 +48,5 @@ const RuleEditor: FC<RuleEditorProps> = ({ match }) => {
 
   return <RuleEditorWrapper edit={Boolean(identifier)}>{getContent()}</RuleEditorWrapper>;
 };
-
-const AlertWarning: FC<{ title: string }> = ({ title, children }) => (
-  <Alert className={useStyles2(warningStyles).warning} severity="warning" title={title}>
-    <p>{children}</p>
-    <LinkButton href="alerting/list">To rule list</LinkButton>
-  </Alert>
-);
-
-const warningStyles = (theme: GrafanaTheme2) => ({
-  warning: css`
-    margin: ${theme.spacing(4)};
-  `,
-});
 
 export default withErrorBoundary(RuleEditor, { style: 'page' });
