49b5c9aa972f174035f50e2ddb320b269d491b91
sam boyer <sdboyer@grafana.com>
2022-09-26 11:26:18 -0400

    codegen: Introduce TS codegen veneer (#54816)
    * Split all named types out into defs, etc.
    
    * Use latest cuetsy, refactor generators accordingly
    
    * Return AST type from plugin TS generator
    
    * Near-complete checkin of TS veneer code generator
    
    * First full completed pass
    
    * Improve the attribute name
    
    * Defer use of the dashboard veneer type to follow-up
    
    * Remove dummy index, prettier on veneer
    
    * Fix merge errors in gen.go
    
    * Add match field to SpecialValueMap
    
    * Fix backend lint errors

--- public/app/plugins/panel/stat/models.gen.ts
+++ public/app/plugins/panel/stat/models.gen.ts
@@ -7,10 +7,10 @@
 // Run `make gen-cue` from repository root to regenerate.
 
 
+
 import * as ui from '@grafana/schema';
 
 export const PanelModelVersion = Object.freeze([0, 0]);
-
 
 export interface PanelOptions extends ui.SingleStatBaseOptions {
   colorMode: ui.BigValueColorMode;
@@ -25,4 +25,3 @@ export const defaultPanelOptions: Partial<PanelOptions> = {
   justifyMode: ui.BigValueJustifyMode.Auto,
   textMode: ui.BigValueTextMode.Auto,
 };
-
