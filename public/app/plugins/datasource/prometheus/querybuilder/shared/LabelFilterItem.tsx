9fdee2882a6e2f663fb905848c31538c0a13dc24
Ivana Huckova <ivana.huckova@gmail.com>
2022-09-26 15:54:06 +0200

    Loki: Show invalid fields in Label filter

--- public/app/plugins/datasource/prometheus/querybuilder/shared/LabelFilterItem.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/shared/LabelFilterItem.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import { uniqBy } from 'lodash';
 import React, { useState } from 'react';
 
@@ -14,9 +15,20 @@ export interface Props {
   onGetLabelNames: (forLabel: Partial<QueryBuilderLabelFilter>) => Promise<SelectableValue[]>;
   onGetLabelValues: (forLabel: Partial<QueryBuilderLabelFilter>) => Promise<SelectableValue[]>;
   onDelete: () => void;
+  invalidLabel?: boolean;
+  invalidValue?: boolean;
 }
 
-export function LabelFilterItem({ item, defaultOp, onChange, onDelete, onGetLabelNames, onGetLabelValues }: Props) {
+export function LabelFilterItem({
+  item,
+  defaultOp,
+  onChange,
+  onDelete,
+  onGetLabelNames,
+  onGetLabelValues,
+  invalidLabel,
+  invalidValue,
+}: Props) {
   const [state, setState] = useState<{
     labelNames?: SelectableValue[];
     labelValues?: SelectableValue[];
@@ -46,6 +58,13 @@ export function LabelFilterItem({ item, defaultOp, onChange, onDelete, onGetLabe
     return uniqBy([...selectedOptions, ...labelValues], 'value');
   };
 
+  // This is necessary to show invalid state on the value in label input
+  const invalidClassNameOverride = invalidLabel
+    ? css`
+        margin-left: 0 !important;
+      `
+    : '';
+
   return (
     <div data-testid="prometheus-dimensions-filter-item">
       <InputGroup>
@@ -72,6 +91,7 @@ export function LabelFilterItem({ item, defaultOp, onChange, onDelete, onGetLabe
               } as any as QueryBuilderLabelFilter);
             }
           }}
+          invalid={invalidLabel}
         />
 
         <Select
@@ -84,6 +104,7 @@ export function LabelFilterItem({ item, defaultOp, onChange, onDelete, onGetLabe
               onChange({ ...item, op: change.value } as any as QueryBuilderLabelFilter);
             }
           }}
+          className={invalidClassNameOverride}
         />
 
         <Select
@@ -121,6 +142,7 @@ export function LabelFilterItem({ item, defaultOp, onChange, onDelete, onGetLabe
               onChange({ ...item, value: changes, op: item.op ?? defaultOp } as any as QueryBuilderLabelFilter);
             }
           }}
+          invalid={invalidValue}
         />
         <AccessoryButton aria-label="remove" icon="times" variant="secondary" onClick={onDelete} />
       </InputGroup>
