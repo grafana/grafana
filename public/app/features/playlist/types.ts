6b3b00fffec3aa6b886ea4cd1f34af55c56661c6
Ryan McKinley <ryantxu@gmail.com>
2022-09-28 15:31:33 -0700

    add note in frontend

--- public/app/features/playlist/types.ts
+++ public/app/features/playlist/types.ts
@@ -9,6 +9,7 @@ export interface Playlist {
   items?: PlaylistItem[];
 }
 
+// TODO: use Playlist+Playlist item from `@grafana/schema`... but add the dashboards inline
 export interface PlaylistItem {
   type: 'dashboard_by_tag' | 'dashboard_by_uid' | 'dashboard_by_id'; // _by_id is deprecated
   value: string; // tag or uid
