1ac56418e53a6364f726b730aba1eb6fbaa34950
Ying WANG <ying.wang@grafana.com>
2022-09-28 14:09:28 +0200

    some changes

--- pkg/services/temp_user/tempuserimpl/sqlx_store.go
+++ pkg/services/temp_user/tempuserimpl/sqlx_store.go
@@ -19,78 +19,78 @@ func (ss *sqlxStore) UpdateTempUserStatus(ctx context.Context, cmd *models.Updat
 }
 
 func (ss *sqlxStore) CreateTempUser(ctx context.Context, cmd *models.CreateTempUserCommand) error {
-	
-	return ss.db.WithTransactionalDbSession(ctx, func(sess *sqlstore.DBSession) error {
-		// create user
-		user := &models.TempUser{
-			Email:           cmd.Email,
-			Name:            cmd.Name,
-			OrgId:           cmd.OrgId,
-			Code:            cmd.Code,
-			Role:            cmd.Role,
-			Status:          cmd.Status,
-			RemoteAddr:      cmd.RemoteAddr,
-			InvitedByUserId: cmd.InvitedByUserId,
-			EmailSentOn:     time.Now(),
-			Created:         time.Now().Unix(),
-			Updated:         time.Now().Unix(),
-		}
-
-		if _, err := sess.Insert(user); err != nil {
-			return err
-		}
-
-		cmd.Result = user
-
-		return nil
-	})
+	user := &models.TempUser{
+		Email:           cmd.Email,
+		Name:            cmd.Name,
+		OrgId:           cmd.OrgId,
+		Code:            cmd.Code,
+		Role:            cmd.Role,
+		Status:          cmd.Status,
+		RemoteAddr:      cmd.RemoteAddr,
+		InvitedByUserId: cmd.InvitedByUserId,
+		EmailSentOn:     time.Now(),
+		Created:         time.Now().Unix(),
+		Updated:         time.Now().Unix(),
+	}
+	query := `INSERT INTO temp_user (
+		email, 
+		name, 
+		org_id, 
+		code, 
+		role, 
+		status, 
+		remote_addr,
+		invited_by_user_id,
+		email_sent_on,
+		created,
+		updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	var err error
+	user.Id, err = ss.sess.ExecWithReturningId(
+		ctx, query, user.Email, user.Name, user.OrgId, user.Code, user.Role, user.Status, user.RemoteAddr, user.InvitedByUserId, user.EmailSentOn, user.Created, user.Updated)
+	cmd.Result = user
+	return err
 }
 
 func (ss *sqlxStore) UpdateTempUserWithEmailSent(ctx context.Context, cmd *models.UpdateTempUserWithEmailSentCommand) error {
-	return ss.db.WithTransactionalDbSession(ctx, func(sess *sqlstore.DBSession) error {
-		user := &models.TempUser{
-			EmailSent:   true,
-			EmailSentOn: time.Now(),
-		}
-
-		_, err := sess.Where("code = ?", cmd.Code).Cols("email_sent", "email_sent_on").Update(user)
-
-		return err
-	})
+	_, err := ss.sess.Exec(ctx, "UPDATE temp_user SET email_sent=?, email_sent_on=? WHERE code=?", true, time.Now(), cmd.Code)
+	return err
 }
 
 func (ss *sqlxStore) GetTempUsersQuery(ctx context.Context, query *models.GetTempUsersQuery) error {
+	rawSQL := `SELECT
+					tu.id     		  as id,
+					tu.org_id 		  as org_id,
+					tu.email  		  as email,
+					tu.name           as name,
+					tu.role           as role,
+					tu.code           as code,
+					tu.status         as status,
+					tu.email_sent     as email_sent,
+					tu.email_sent_on  as email_sent_on,
+					tu.created		  as created,
+					u.login			  as invited_by_login,
+					u.name			  as invited_by_name,
+					u.email			  as invited_by_email
+			    FROM "temp_user" as tu
+					LEFT OUTER JOIN "user" as u on u.id = tu.invited_by_user_id
+					WHERE tu.status=?`
+	params := []interface{}{string(query.Status)}
+
+	if query.OrgId > 0 {
+		rawSQL += ` AND tu.org_id=?`
+		params = append(params, query.OrgId)
+	}
+
+	if query.Email != "" {
+		rawSQL += ` AND tu.email=?`
+		params = append(params, query.Email)
+	}
+
+	rawSQL += " ORDER BY tu.created desc"
+	
 	return ss.db.WithDbSession(ctx, func(dbSess *sqlstore.DBSession) error {
-		rawSQL := `SELECT
-	                tu.id             as id,
-	                tu.org_id         as org_id,
-	                tu.email          as email,
-									tu.name           as name,
-									tu.role           as role,
-									tu.code           as code,
-									tu.status         as status,
-									tu.email_sent     as email_sent,
-									tu.email_sent_on  as email_sent_on,
-									tu.created				as created,
-									u.login						as invited_by_login,
-									u.name						as invited_by_name,
-									u.email						as invited_by_email
-	                FROM ` + ss.db.GetDialect().Quote("temp_user") + ` as tu
-									LEFT OUTER JOIN ` + ss.db.GetDialect().Quote("user") + ` as u on u.id = tu.invited_by_user_id
-									WHERE tu.status=?`
-		params := []interface{}{string(query.Status)}
 
-		if query.OrgId > 0 {
-			rawSQL += ` AND tu.org_id=?`
-			params = append(params, query.OrgId)
-		}
-
-		if query.Email != "" {
-			rawSQL += ` AND tu.email=?`
-			params = append(params, query.Email)
-		}
-
-		rawSQL += " ORDER BY tu.created desc"
+		
 
 		query.Result = make([]*models.TempUserDTO, 0)
 		sess := dbSess.SQL(rawSQL, params...)
