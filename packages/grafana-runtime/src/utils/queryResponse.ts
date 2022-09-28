b172f37e1287511d0dc0167802646877a192e985
Will Browne <will.browne@grafana.com>
2022-09-28 14:50:54 +0100

    undo fe changes

--- packages/grafana-runtime/src/utils/queryResponse.ts
+++ packages/grafana-runtime/src/utils/queryResponse.ts
@@ -30,18 +30,12 @@ export const cachedResponseNotice: QueryResultMetaNotice = { severity: 'info', t
  */
 export interface DataResponse {
   error?: string;
-  errorDetails?: ErrorDetails;
   refId?: string;
   frames?: DataFrameJSON[];
 
   // Legacy TSDB format...
   series?: TimeSeries[];
   tables?: TableData[];
-}
-
-export interface ErrorDetails {
-  status?: string;
-  message?: string;
 }
 
 /**
@@ -93,24 +87,6 @@ export function toDataQueryResponse(
             message: dr.error,
           };
           rsp.state = LoadingState.Error;
-        }
-      }
-
-      if (dr.errorDetails) {
-        rsp.state = LoadingState.Error;
-
-        if (!rsp.error) {
-          rsp.error = {
-            refId: dr.refId,
-            message: dr.errorDetails.message,
-          };
-        }
-
-        if (!rsp.errorDetails) {
-          rsp.errorDetails = new Map<string, ErrorDetails>();
-        }
-        if (dr.refId) {
-          rsp.errorDetails.set(dr.refId, dr.errorDetails);
         }
       }
 
