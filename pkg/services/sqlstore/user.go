a072ebdcddf87ec63ad174cc10f596182324b6d4
Ida Furjesova <ida.furjesova@grafana.com>
2022-09-30 11:28:55 +0200

    Move tests to user store

--- pkg/services/sqlstore/user.go
+++ pkg/services/sqlstore/user.go
@@ -7,7 +7,6 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/grafana/grafana/pkg/events"
 	"github.com/grafana/grafana/pkg/models"
@@ -201,6 +200,7 @@ func (ss *SQLStore) GetUserById(ctx context.Context, query *models.GetUserByIdQu
 	})
 }
 
+// deprecated method, use only for tests
 func (ss *SQLStore) SetUsingOrg(ctx context.Context, cmd *models.SetUsingOrgCommand) error {
 	getOrgsForUserCmd := &models.GetUserOrgListQuery{UserId: cmd.UserId}
 	if err := ss.GetUserOrgList(ctx, getOrgsForUserCmd); err != nil {
@@ -295,28 +295,6 @@ func (ss *SQLStore) GetUserOrgList(ctx context.Context, query *models.GetUserOrg
 		sort.Sort(byOrgName(query.Result))
 		return err
 	})
-}
-
-func newSignedInUserCacheKey(orgID, userID int64) string {
-	return fmt.Sprintf("signed-in-user-%d-%d", userID, orgID)
-}
-
-func (ss *SQLStore) GetSignedInUserWithCacheCtx(ctx context.Context, query *models.GetSignedInUserQuery) error {
-	cacheKey := newSignedInUserCacheKey(query.OrgId, query.UserId)
-	if cached, found := ss.CacheService.Get(cacheKey); found {
-		cachedUser := cached.(user.SignedInUser)
-		query.Result = &cachedUser
-		return nil
-	}
-
-	err := ss.GetSignedInUser(ctx, query)
-	if err != nil {
-		return err
-	}
-
-	cacheKey = newSignedInUserCacheKey(query.Result.OrgID, query.UserId)
-	ss.CacheService.Set(cacheKey, *query.Result, time.Second*5)
-	return nil
 }
 
 func (ss *SQLStore) GetSignedInUser(ctx context.Context, query *models.GetSignedInUserQuery) error {
