563c1f54f114c45be18250cbd16b8947cfb6ba91
nmarrs <nathanielmarrs@gmail.com>
2022-09-30 10:26:05 -0700

    Update migration to run if plugin version is empty string (default when running first migration for a plugin)

--- public/app/plugins/panel/canvas/migrations.ts
+++ public/app/plugins/panel/canvas/migrations.ts
@@ -5,20 +5,18 @@ import { PanelOptions } from './models.gen';
 export const canvasMigrationHandler = (panel: PanelModel): Partial<PanelOptions> => {
   const pluginVersion = panel?.pluginVersion ?? '';
 
-  // TODO: how to set plugin version?? want this to run for everything < v9.2.0
-  console.log('this is happening', { pluginVersion });
-
   // Rename text-box to rectangle
-  //   if (pluginVersion.startsWith('9.3')) {
-  const root = panel.options?.root;
-  if (root?.elements) {
-    for (const element of root.elements) {
-      if (element.type === 'text-box') {
-        element.type = 'rectangle';
+  // Initial plugin version is empty string for first migration
+  if (pluginVersion === '') {
+    const root = panel.options?.root;
+    if (root?.elements) {
+      for (const element of root.elements) {
+        if (element.type === 'text-box') {
+          element.type = 'rectangle';
+        }
       }
     }
   }
-  //   }
 
   return panel.options;
 };
