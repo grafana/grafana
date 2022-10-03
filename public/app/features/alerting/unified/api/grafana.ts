5cfb3e7848726171dcb052bc6d1f9fd7a0f4f01c
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2022-10-03 18:10:23 +0200

    Return always empty ContactPointsState array when catching an error in the request response

--- public/app/features/alerting/unified/api/grafana.ts
+++ public/app/features/alerting/unified/api/grafana.ts
@@ -1,9 +1,8 @@
 import { lastValueFrom } from 'rxjs';
 
-import { FetchError, getBackendSrv } from '@grafana/runtime';
+import { getBackendSrv } from '@grafana/runtime';
 import { ContactPointsState, NotifierDTO, ReceiversStateDTO } from 'app/types';
 
-import { isFetchError } from '../../../../../../packages/grafana-runtime/src/services/backendSrv';
 import { getDatasourceAPIUid } from '../utils/datasource';
 
 export function fetchNotifiers(): Promise<NotifierDTO[]> {
@@ -38,7 +37,7 @@ export const contactPointsStateDtoToModel = (receiversStateDto: ReceiversStateDT
     //update integrations in response
     cpState.integrations.forEach((integrationStatusDTO) => {
       //update errorcount
-      const hasError = Boolean(integrationStatusDTO?.lastError);
+      const hasError = Boolean(integrationStatusDTO?.lastNotifyAttemptError);
       if (hasError) {
         receiverState.errorCount += 1;
         contactPointsState.errorCount += 1;
@@ -61,10 +60,6 @@ export const contactPointsStateDtoToModel = (receiversStateDto: ReceiversStateDT
 export const getIntegrationType = (integrationName: string): string | undefined =>
   parseIntegrationName(integrationName)?.type;
 
-function isContactPointsStateNotAvailable(error: FetchError) {
-  return error.status === 404;
-}
-
 export async function fetchContactPointsState(alertManagerSourceName: string): Promise<ContactPointsState> {
   try {
     const response = await lastValueFrom(
@@ -76,60 +71,6 @@ export async function fetchContactPointsState(alertManagerSourceName: string): P
     );
     return contactPointsStateDtoToModel(response.data);
   } catch (error) {
-    if (isFetchError(error)) {
-      if (!isContactPointsStateNotAvailable(error)) {
-        return contactPointsStateDtoToModel([]);
-      } else {
-        throw error;
-      }
-    } else {
-      throw error;
-    }
+    return contactPointsStateDtoToModel([]);
   }
-}
-
-// This method will be removed
-export function fetchContactPointsState_(alertManagerSourceName: String): Promise<ContactPointsState> {
-  return new Promise<ContactPointsState>((resolve, reject) => {
-    // Response EXAMPLE
-    const fakeResponse = [
-      {
-        active: true, // Whether the contact point is used or not.
-        integrations: [
-          // Can be multiple of the same type. Are identified by the index.
-          {
-            lastError: 'establish connection to server: dial tcp: lookup smtp.example.org on 8.8.8.8:53: no such host',
-            lastNotify: '2022-09-19T15:34:40.696Z',
-            lastNotifyDuration: '117.2455ms',
-            name: 'email[0]',
-          },
-          {
-            lastError: 'establish connection to server: dial tcp: lookup smtp.example.org on 8.8.8.8:53: no such host',
-            lastNotify: '2022-09-19T15:34:40.696Z',
-            lastNotifyDuration: '117.2455ms',
-            name: 'email[1]',
-          },
-        ],
-        name: 'multiple3',
-      },
-      {
-        active: true, // Whether the contact point is used or not.
-        integrations: [
-          // Can be multiple of the same type. Are identified by the index.
-          {
-            lastNotify: '0001-01-01T00:00:00.000Z',
-            lastNotifyDuration: '117.2455ms',
-            name: 'email[0]',
-          },
-        ],
-        name: 'grafana-default-email',
-      },
-    ];
-
-    const fakeState: ContactPointsState = contactPointsStateDtoToModel(fakeResponse);
-
-    setTimeout(() => {
-      resolve(fakeState);
-    }, 1000);
-  });
 }
