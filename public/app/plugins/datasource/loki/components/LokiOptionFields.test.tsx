ebfe20f35848157b015e3bc7662370ec4ea43924
Gareth Dawson <gwdawson.work@gmail.com>
2022-10-03 13:27:37 +0100

    WIP: Added tests to LokiOptionFields.test.tsx

--- public/app/plugins/datasource/loki/components/LokiOptionFields.test.tsx
+++ public/app/plugins/datasource/loki/components/LokiOptionFields.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import { LokiOptionFieldsProps, LokiOptionFields } from './LokiOptionFields';
@@ -18,17 +19,107 @@ const setup = () => {
     onRunQuery,
   };
 
-  return render(<LokiOptionFields {...props} />);
+  return props;
 };
 
-describe('LokiOptionFields', () => {
-  it('should render step field', () => {
-    setup();
-    expect(screen.getByTestId('lineLimitField')).toBeInTheDocument();
-  });
-
+describe('Query Type Field', () => {
   it('should render query type field', () => {
-    setup();
+    // Arrange
+    const props = setup();
+    render(<LokiOptionFields {...props} />);
+    // Assert
     expect(screen.getByTestId('queryTypeField')).toBeInTheDocument();
   });
+
+  it('should have a default value of "Range"', () => {
+    // Arrange
+    const props = setup();
+    render(<LokiOptionFields {...props} />);
+    // Assert
+    expect(screen.getByLabelText('Range')).toBeChecked();
+    expect(screen.getByLabelText('Instant')).not.toBeChecked();
+  });
+
+  //! Warning: You seem to have overlapping act() calls, this is not supported. Be sure to await previous act() calls before making a new one.
+  it.only('should call onChange when value is changed', async () => {
+    // Arrange
+    const props = setup();
+    render(<LokiOptionFields {...props} />);
+    // Act
+    userEvent.click(screen.getByLabelText('Instant'));
+    // Assert
+    await waitFor(() => expect(props.onChange).toHaveBeenCalledTimes(1));
+  });
+
+  //! FAIL, (expected: "instant to be checked", received: "range to be checked")
+  // it('should change the value to "Instant" on click', async () => {
+  //   // Arrange
+  //   const props = setup();
+  //   render(<LokiOptionFields {...props} />);
+  //   // Act
+  //   userEvent.click(screen.getByLabelText('Instant'));
+  //   // Assert
+  //   await waitFor(() => {
+  //     expect(screen.getByLabelText('Instant')).toBeChecked();
+  //     expect(screen.getByLabelText('Range')).not.toBeChecked();
+  //   });
+  // });
+});
+
+describe('Line Limit Field', () => {
+  it('should render line limit field', () => {
+    // Arrange
+    const props = setup();
+    render(<LokiOptionFields {...props} />);
+    // Assert
+    expect(screen.getByRole('spinbutton')).toBeInTheDocument();
+  });
+
+  it('should have a default value of 1', () => {
+    // Arrange
+    const props = setup();
+    render(<LokiOptionFields {...props} />);
+    // Assert
+    expect(screen.getByRole('spinbutton')).toHaveValue(1);
+  });
+
+  //! FAIL, (EXPECTED: 2, RECEIVED: 1)
+  // it('should have an updated value of 2', async () => {
+  //   // Arrange
+  //   const props = setup();
+  //   render(<LokiOptionFields {...props} />);
+  //   // Act
+  //   userEvent.type(screen.getByRole('spinbutton'), '2');
+  //   // Assert
+  //   await waitFor(() => {
+  //     expect(screen.getByRole('spinbutton')).toHaveValue(2);
+  //   });
+  // });
+});
+
+describe('Resolution Field', () => {
+  it('should render the resolution field', () => {
+    // Arrange
+    const props = setup();
+    render(<LokiOptionFields {...props} />);
+    // Assert
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+  });
+
+  //! FAIL, (EXPECTED: 1, RECEIVED: '')
+  //* Me and Ivana had a quick look at this, we couldn't figure out why the value of the input is ''.
+  // it('should have a default value of 1', () => {
+  //   // Arrange
+  //   const props = setup();
+  //   render(<LokiOptionFields {...props} />);
+  //   // Assert
+  //   // screen.debug();
+  //   expect(screen.getByLabelText('Select resolution')).toHaveValue(1 / 1);
+  // });
+
+  // it('', () => {
+  //   // Arrange
+  //   const props = setup();
+  //   render(<LokiOptionFields {...props} />);
+  // });
 });
