caf757b5bdc82eeb9763a4cc70d801127d87e4b2
Miklós Tolnai <miklos.tolnai@grafana.com>
2022-10-04 11:20:09 +0200

    fix spacer

--- public/app/features/connections/tabs/ConnectData/ConnectData.tsx
+++ public/app/features/connections/tabs/ConnectData/ConnectData.tsx
@@ -1,5 +1,7 @@
+import { css } from '@emotion/css';
 import React, { useState } from 'react';
 
+import { useStyles2 } from '@grafana/ui';
 import { useGetAllWithFilters } from 'app/features/plugins/admin/state/hooks';
 
 import { CardGrid } from './CardGrid';
@@ -7,8 +9,15 @@ import { CategoryHeader } from './CategoryHeader';
 import { NoResults } from './NoResults';
 import { Search } from './Search';
 
+const getStyles = () => ({
+  spacer: css`
+    height: 16px;
+  `,
+});
+
 export function ConnectData() {
   const [searchTerm, setSearchTerm] = useState('');
+  const styles = useStyles2(getStyles);
 
   const handleSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
     setSearchTerm(e.currentTarget.value.toLowerCase());
@@ -20,7 +29,7 @@ export function ConnectData() {
     <>
       <Search onChange={handleSearchChange} />
       {/* We need this extra spacing when there are no filters */}
-      <div margin-bottom="16px" />
+      <div className={styles.spacer} />
       {isLoading ? (
         <div>Loading...</div>
       ) : !!error ? (
