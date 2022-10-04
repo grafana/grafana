d802ed40f8222e3d47d99311e69219e2920400bd
Ryan McKinley <ryantxu@gmail.com>
2022-10-04 16:56:12 -0700

    adding history

--- pkg/services/store/object/sqlstash/sql_storage_server.go
+++ pkg/services/store/object/sqlstash/sql_storage_server.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/grafana/grafana/pkg/infra/log"
@@ -54,9 +55,21 @@ func userFromContext(ctx context.Context) *user.SignedInUser {
 }
 
 func (s sqlObjectServer) Write(ctx context.Context, r *object.WriteObjectRequest) (*object.WriteObjectResponse, error) {
+	// TODO: this needs to be extracted from the body content
 	summary := object.ObjectSummary{
 		Name:        "hello",
 		Description: "description",
+		Labels: map[string]string{
+			"hello": "world",
+			"test":  "",
+		},
+		References: []*object.ExternalReference{
+			{
+				Kind: "ds",
+				Type: "influx",
+				UID:  "xyz",
+			},
+		},
 	}
 
 	etag := createContentsHash(r.Body)
@@ -65,11 +78,19 @@ func (s sqlObjectServer) Write(ctx context.Context, r *object.WriteObjectRequest
 		return nil, err
 	}
 
-	orgId := 123
-	rsp := &object.WriteObjectResponse{}
 	modifier := userFromContext(ctx)
+	key := fmt.Sprintf("%d/%s.%s", modifier.OrgID, r.UID, r.Kind)
+
+	rsp := &object.WriteObjectResponse{}
 
 	err = s.sess.WithTransaction(ctx, func(tx *session.SessionTx) error {
+		rows := make([]*object.ObjectVersionInfo, 0, 1) // TODO? is there a non-magic binding way to do this?
+		err := s.sess.Select(ctx, &rows, `SELECT "etag","version" FROM object WHERE key=?`, key)
+		if err != nil {
+			return err
+		}
+
+		isUpdate := false
 		timestamp := time.Now()
 		versionInfo := &object.ObjectVersionInfo{
 			Version:  "1",
@@ -82,8 +103,62 @@ func (s sqlObjectServer) Write(ctx context.Context, r *object.WriteObjectRequest
 				Login: modifier.Login,
 			},
 		}
-		rsp.Object = versionInfo
 
+		// Check if it existed before
+		if len(rows) > 0 {
+			current := rows[0]
+			if current.ETag == etag {
+				rsp.Object = current // TODO more
+				rsp.Status = object.WriteObjectResponse_UNCHANGED
+				return nil
+			}
+
+			rsp.Status = object.WriteObjectResponse_MODIFIED
+			isUpdate = true
+
+			// Increment the version
+			i, _ := strconv.ParseInt(current.Version, 0, 64)
+			if i < 1 {
+				i = timestamp.UnixMilli()
+			}
+			versionInfo.Version = fmt.Sprintf("%d", i+1)
+
+			// Clear the labels+refs
+			if _, err := tx.Exec(ctx, "DELETE FROM object_labels WHERE object_key = ?", key); err != nil {
+				return err
+			}
+			if _, err := tx.Exec(ctx, "DELETE FROM object_ref WHERE object_key = ?", key); err != nil {
+				return err
+			}
+		}
+
+		// 1. Add the `object_history` values
+		_, err = tx.Exec(ctx, `INSERT INTO object_history (`+
+			`"key", "version", "message", `+
+			`"size", "body", "etag", `+
+			`"updated", "updated_by") `+
+			`VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+			key, versionInfo.Version, versionInfo.Comment,
+			versionInfo.Size, r.Body, versionInfo.ETag,
+			timestamp, modifier.UserID,
+		)
+		if err != nil {
+			return err
+		}
+
+		// 2. Add the labels rows
+
+		// 3. Add the references rows
+
+		// 4. Add/update the main `object` table
+		rsp.Object = versionInfo
+		if isUpdate {
+			// TODO: the update
+			return nil
+		}
+
+		// Insert the new row
+		parent_folder_key := "???"
 		query :=
 			`INSERT INTO object (` +
 				`"key", "parent_folder_key", "kind", "size", "body", "etag", "version",` +
@@ -91,10 +166,7 @@ func (s sqlObjectServer) Write(ctx context.Context, r *object.WriteObjectRequest
 				`"name", "description", "summary") ` +
 				`VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-		key := fmt.Sprintf("%d/%s.%s", orgId, r.UID, r.Kind)
-		parent_folder_key := "???"
-
-		rsp, err := tx.Exec(ctx, query,
+		_, err = tx.Exec(ctx, query,
 			key, parent_folder_key, r.Kind, versionInfo.Size, r.Body, etag, versionInfo.Version,
 			timestamp, modifier.UserID, timestamp, modifier.UserID, // created + modified
 			summary.Name,
@@ -102,12 +174,9 @@ func (s sqlObjectServer) Write(ctx context.Context, r *object.WriteObjectRequest
 			string(summaryjson),
 		)
 		if err != nil {
-			return err
+			rsp.Status = object.WriteObjectResponse_CREATED
 		}
-
-		fmt.Printf("GOT: %v\n", rsp)
-
-		return nil
+		return err
 	})
 
 	return rsp, err
