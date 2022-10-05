111784d6959d51920f0ff736789db002692ca65c
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2022-10-05 15:22:39 +0200

    In GrafanaEvaluationBehaviour component : Split evaluation interval from for duration and add button to edit to allow editing it and warning

--- public/app/features/alerting/unified/components/rule-editor/GrafanaEvaluationBehavior.tsx
+++ public/app/features/alerting/unified/components/rule-editor/GrafanaEvaluationBehavior.tsx
@@ -1,9 +1,9 @@
-import { css } from '@emotion/css';
-import React, { useState } from 'react';
+import { css, cx } from '@emotion/css';
+import React, { FC, useState, useEffect } from 'react';
 import { RegisterOptions, useFormContext } from 'react-hook-form';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { Field, InlineLabel, Input, InputControl, useStyles2 } from '@grafana/ui';
+import { durationToMilliseconds, GrafanaTheme2, parseDuration } from '@grafana/data';
+import { Button, Field, InlineLabel, Input, InputControl, useStyles2 } from '@grafana/ui';
 
 import { RuleFormValues } from '../../types/rule-form';
 import { checkEvaluationIntervalGlobalLimit } from '../../utils/config';
@@ -51,43 +51,127 @@ export const forValidationOptions = (evaluateEvery: string): RegisterOptions => 
   },
 });
 
-export const evaluateEveryValidationOptions: RegisterOptions = {
-  required: {
-    value: true,
-    message: 'Required.',
-  },
-  validate: (value: string) => {
-    try {
-      const duration = parsePrometheusDuration(value);
-
-      if (duration < MIN_TIME_RANGE_STEP_S * 1000) {
-        return `Cannot be less than ${MIN_TIME_RANGE_STEP_S} seconds.`;
-      }
-
-      if (duration % (MIN_TIME_RANGE_STEP_S * 1000) !== 0) {
-        return `Must be a multiple of ${MIN_TIME_RANGE_STEP_S} seconds.`;
-      }
-
-      return true;
-    } catch (error) {
-      return error instanceof Error ? error.message : 'Failed to parse duration';
-    }
-  },
-};
-
-export const GrafanaEvaluationBehavior = () => {
+function EvaluationIntervalInput() {
   const styles = useStyles2(getStyles);
-  const [showErrorHandling, setShowErrorHandling] = useState(false);
+  const [editInterval, setEditInterval] = useState(false);
+  const {
+    setFocus,
+    register,
+    formState: { errors },
+    watch,
+  } = useFormContext<RuleFormValues>();
+
+  const group = watch('group');
+  const evaluateEveryId = 'eval-every-input';
+
+  const onBlur = () => setEditInterval(false);
+  const evaluateEveryValidationOptions: RegisterOptions = {
+    required: {
+      value: true,
+      message: 'Required.',
+    },
+    pattern: positiveDurationValidationPattern,
+    validate: (value: string) => {
+      const duration = parseDuration(value);
+      if (Object.keys(duration).length) {
+        const diff = durationToMilliseconds(duration);
+        if (diff < MIN_TIME_RANGE_STEP_S * 1000) {
+          return `Cannot be less than ${MIN_TIME_RANGE_STEP_S} seconds.`;
+        }
+        if (diff % (MIN_TIME_RANGE_STEP_S * 1000) !== 0) {
+          return `Must be a multiple of ${MIN_TIME_RANGE_STEP_S} seconds.`;
+        }
+      }
+      return true;
+    },
+  };
+
+  useEffect(() => {
+    editInterval && setFocus('evaluateEvery');
+  }, [editInterval, setFocus]);
+
+  return (
+    <div>
+      <div className={styles.flexRow}>
+        <div className={styles.evaluateLabel}>{`Alert rules in '${group}' are evaluated every`}</div>
+        <Field
+          className={styles.inlineField}
+          error={errors.evaluateEvery?.message}
+          invalid={!!errors.evaluateEvery}
+          validationMessageHorizontalOverflow={true}
+        >
+          <Input
+            id={evaluateEveryId}
+            width={8}
+            {...register('evaluateEvery', evaluateEveryValidationOptions)}
+            readOnly={!editInterval}
+            onBlur={onBlur}
+            className={styles.evaluateInput}
+          />
+        </Field>
+        <Button
+          icon={editInterval ? 'exclamation-circle' : 'edit'}
+          type="button"
+          variant="secondary"
+          disabled={editInterval}
+          onClick={() => {
+            if (!editInterval) {
+              setFocus('evaluateEvery');
+              setEditInterval(true);
+            }
+            editInterval && setEditInterval(false);
+          }}
+        >
+          <span className={cx(editInterval && 'text-warning')}>
+            {editInterval ? `You are updating evaluation interval for the group '${group}'` : 'Edit group behaviour'}
+          </span>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function ForInput() {
+  const styles = useStyles2(getStyles);
   const {
     register,
     formState: { errors },
     watch,
   } = useFormContext<RuleFormValues>();
 
-  const { exceedsLimit: exceedsGlobalEvaluationLimit } = checkEvaluationIntervalGlobalLimit(watch('evaluateEvery'));
-
-  const evaluateEveryId = 'eval-every-input';
   const evaluateForId = 'eval-for-input';
+
+  return (
+    <div className={styles.flexRow}>
+      <InlineLabel
+        htmlFor={evaluateForId}
+        width={7}
+        tooltip='Once condition is breached, alert will go into pending state. If it is pending for longer than the "for" value, it will become a firing alert.'
+      >
+        for
+      </InlineLabel>
+      <Field
+        className={styles.inlineField}
+        error={errors.evaluateFor?.message}
+        invalid={!!errors.evaluateFor?.message}
+        validationMessageHorizontalOverflow={true}
+      >
+        <Input
+          id={evaluateForId}
+          width={8}
+          {...register('evaluateFor', forValidationOptions(watch('evaluateEvery')))}
+        />
+      </Field>
+    </div>
+  );
+}
+
+export const GrafanaEvaluationBehavior: FC = () => {
+  const styles = useStyles2(getStyles);
+  const [showErrorHandling, setShowErrorHandling] = useState(false);
+  const { watch } = useFormContext<RuleFormValues>();
+
+  const { exceedsLimit: exceedsGlobalEvaluationLimit } = checkEvaluationIntervalGlobalLimit(watch('evaluateEvery'));
 
   return (
     // TODO remove "and alert condition" for recording rules
@@ -96,42 +180,9 @@ export const GrafanaEvaluationBehavior = () => {
         label="Evaluate"
         description="Evaluation interval applies to every rule within a group. It can overwrite the interval of an existing alert rule."
       >
-        <div className={styles.flexRow}>
-          <InlineLabel
-            htmlFor={evaluateEveryId}
-            width={16}
-            tooltip="How often the alert will be evaluated to see if it fires"
-          >
-            Evaluate every
-          </InlineLabel>
-          <Field
-            className={styles.inlineField}
-            error={errors.evaluateEvery?.message}
-            invalid={!!errors.evaluateEvery}
-            validationMessageHorizontalOverflow={true}
-          >
-            <Input id={evaluateEveryId} width={8} {...register('evaluateEvery', evaluateEveryValidationOptions)} />
-          </Field>
-
-          <InlineLabel
-            htmlFor={evaluateForId}
-            width={7}
-            tooltip='Once condition is breached, alert will go into pending state. If it is pending for longer than the "for" value, it will become a firing alert.'
-          >
-            for
-          </InlineLabel>
-          <Field
-            className={styles.inlineField}
-            error={errors.evaluateFor?.message}
-            invalid={!!errors.evaluateFor?.message}
-            validationMessageHorizontalOverflow={true}
-          >
-            <Input
-              id={evaluateForId}
-              width={8}
-              {...register('evaluateFor', forValidationOptions(watch('evaluateEvery')))}
-            />
-          </Field>
+        <div className={styles.flexColumn}>
+          <EvaluationIntervalInput />
+          <ForInput />
         </div>
       </Field>
       {exceedsGlobalEvaluationLimit && <EvaluationIntervalLimitExceeded />}
@@ -183,16 +234,30 @@ const getStyles = (theme: GrafanaTheme2) => ({
   inlineField: css`
     margin-bottom: 0;
   `,
+  flexColumn: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+  `,
   flexRow: css`
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
     align-items: flex-start;
+    margin-bottom: ${theme.spacing(1)};
   `,
   collapseToggle: css`
     margin: ${theme.spacing(2, 0, 2, -1)};
   `,
   globalLimitValue: css`
     font-weight: ${theme.typography.fontWeightBold};
+  `,
+  evaluateLabel: css`
+    align-self: center;
+    margin-right: ${theme.spacing(1)};
+  `,
+  evaluateInput: css`
+    margin-right: ${theme.spacing(1)};
   `,
 });
