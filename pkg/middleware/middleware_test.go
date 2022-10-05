bc7a383252cc6242a029ab194c62ecb511aaa288
idafurjes <36131195+idafurjes@users.noreply.github.com>
2022-10-05 15:47:56 +0200

    Remove org methods from sqlstore interface (#56358)
    * Remove org methods from sqlstore interface
    
    * Remove some mocks
    
    * Fix some tests

--- pkg/middleware/middleware_test.go
+++ pkg/middleware/middleware_test.go
@@ -342,12 +342,10 @@ func TestMiddlewareContext(t *testing.T) {
 	middlewareScenario(t, "When anonymous access is enabled", func(t *testing.T, sc *scenarioContext) {
 		sc.mockSQLStore.ExpectedOrg = &models.Org{Id: 1, Name: sc.cfg.AnonymousOrgName}
 		sc.orgService.ExpectedOrg = &org.Org{ID: 1, Name: sc.cfg.AnonymousOrgName}
-		orga, err := sc.mockSQLStore.CreateOrgWithMember(sc.cfg.AnonymousOrgName, 1)
-		require.NoError(t, err)
 		sc.fakeReq("GET", "/").exec()
 
 		assert.Equal(t, int64(0), sc.context.UserID)
-		assert.Equal(t, orga.Id, sc.context.OrgID)
+		assert.Equal(t, int64(1), sc.context.OrgID)
 		assert.Equal(t, org.RoleEditor, sc.context.OrgRole)
 		assert.False(t, sc.context.IsSignedIn)
 	}, func(cfg *setting.Cfg) {
