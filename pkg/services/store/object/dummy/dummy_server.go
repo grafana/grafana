ca8160f90cd962b6632142069dec3936486d5199
Ryan McKinley <ryantxu@gmail.com>
2022-10-05 11:28:01 -0700

    update status code

--- pkg/services/store/object/dummy/dummy_server.go
+++ pkg/services/store/object/dummy/dummy_server.go
@@ -181,7 +181,7 @@ func (i dummyObjectServer) update(ctx context.Context, r *object.WriteObjectRequ
 			},
 		}
 		rsp.Object = versionInfo.ObjectVersionInfo
-		rsp.Status = object.WriteObjectResponse_MODIFIED
+		rsp.Status = object.WriteObjectResponse_UPDATED
 
 		// When saving, it must be different than the head version
 		if i.ETag == updated.ETag {
