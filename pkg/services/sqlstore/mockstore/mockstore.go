762add450522e57c8598c64c5502dd6bc52f4460
Ida Furjesova <ida.furjesova@grafana.com>
2022-10-05 12:43:48 +0200

    Remove some mocks

--- pkg/services/sqlstore/mockstore/mockstore.go
+++ pkg/services/sqlstore/mockstore/mockstore.go
@@ -98,13 +98,6 @@ func (m *SQLStoreMock) GetOrgByNameHandler(ctx context.Context, query *models.Ge
 	return m.ExpectedError
 }
 
-func (m *SQLStoreMock) CreateOrgWithMember(name string, userID int64) (models.Org, error) {
-	return *m.ExpectedOrg, nil
-}
-func (m *SQLStoreMock) CreateOrg(ctx context.Context, cmd *models.CreateOrgCommand) error {
-	return m.ExpectedError
-}
-
 func (m *SQLStoreMock) UpdateOrgAddress(ctx context.Context, cmd *models.UpdateOrgAddressCommand) error {
 	return m.ExpectedError
 }
@@ -135,16 +128,7 @@ func (m *SQLStoreMock) CreateUser(ctx context.Context, cmd user.CreateUserComman
 	return nil, m.ExpectedError
 }
 
-func (m *SQLStoreMock) SetUsingOrg(ctx context.Context, cmd *models.SetUsingOrgCommand) error {
-	return m.ExpectedSetUsingOrgError
-}
-
 func (m *SQLStoreMock) GetUserProfile(ctx context.Context, query *models.GetUserProfileQuery) error {
-	return m.ExpectedError
-}
-
-func (m *SQLStoreMock) GetUserOrgList(ctx context.Context, query *models.GetUserOrgListQuery) error {
-	query.Result = m.ExpectedUserOrgList
 	return m.ExpectedError
 }
 
