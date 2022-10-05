e5b9bf3ff337b8bfc240e470393792c5dc3966ec
Levente Balogh <balogh.levente.hu@gmail.com>
2022-10-05 12:05:12 +0200

    feat(Connections)get the active navmodel ID from the path :wq

--- public/app/features/connections/ConnectionsPage.tsx
+++ public/app/features/connections/ConnectionsPage.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { createSelector } from 'reselect';
 
+import { getActiveItem } from 'app/core/components/NavBar/utils';
 import { Page } from 'app/core/components/Page/Page';
 import { DataSourcesList } from 'app/features/datasources/components/DataSourcesList';
 import { NewDataSource } from 'app/features/datasources/components/NewDataSource';
 import { DataSourcesRoutesContext } from 'app/features/datasources/state';
+import { StoreState } from 'app/types';
 
 import { ROUTES } from './constants';
-import { useNavModel } from './hooks/useNavModel';
 import { ConnectData } from './tabs/ConnectData';
 import { DataSourcesEdit } from './tabs/DataSourcesEdit';
 
+// TEMPRORARY
+// Putting it in here until it turns out how the https://github.com/grafana/grafana/pull/56360 goes.
+// --------------------------------------------------------------------
+const selectNavId = createSelector(
+  (state: StoreState) => state.navBarTree,
+  (state: StoreState, pathname: string) => pathname,
+  (navBarTree, pathname) => {
+    // Falling back to use `navId` in case the page explicitly specifies it.
+    // Otherwise just find the active item based on the current path.
+    return getActiveItem(navBarTree, pathname)?.id || '';
+  }
+);
+// --------------------------------------------------------------------
+
 export default function ConnectionsPage() {
-  const navModel = useNavModel();
+  const { pathname } = useLocation();
+  const navId = useSelector((state: StoreState) => selectNavId(state, pathname));
 
   return (
     <DataSourcesRoutesContext.Provider
@@ -23,7 +41,7 @@ export default function ConnectionsPage() {
         Dashboards: ROUTES.DataSourcesDashboards,
       }}
     >
-      <Page navModel={navModel}>
+      <Page navId={navId}>
         <Page.Contents>
           <Switch>
             <Route path={ROUTES.DataSourcesNew} component={NewDataSource} />
