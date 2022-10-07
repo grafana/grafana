8892954ba7eba77e5546052b77df8914f6f6084b
Konrad Lalik <konrad.lalik@grafana.com>
2022-10-07 15:01:47 +0200

    Alerting: Fix evaluation interval validation (#56115)
    Co-authored-by: Gilles De Mey <gilles.de.mey@gmail.com>
    (cherry picked from commit d945091ff14a5d66c6b34fe75e72ef5befa19be8)

--- public/app/features/alerting/unified/components/rule-editor/GrafanaEvaluationBehavior.tsx
+++ public/app/features/alerting/unified/components/rule-editor/GrafanaEvaluationBehavior.tsx
@@ -2,11 +2,11 @@ import { css } from '@emotion/css';
 import React, { FC, useState } from 'react';
 import { useFormContext, RegisterOptions } from 'react-hook-form';
 
-import { parseDuration, durationToMilliseconds, GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme2 } from '@grafana/data';
 import { Field, InlineLabel, Input, InputControl, useStyles2 } from '@grafana/ui';
 
 import { RuleFormValues } from '../../types/rule-form';
-import { positiveDurationValidationPattern, durationValidationPattern } from '../../utils/time';
+import { parsePrometheusDuration } from '../../utils/time';
 import { CollapseToggle } from '../CollapseToggle';
 
 import { GrafanaAlertStatePicker } from './GrafanaAlertStatePicker';
@@ -16,32 +16,62 @@ import { RuleEditorSection } from './RuleEditorSection';
 
 const MIN_TIME_RANGE_STEP_S = 10; // 10 seconds
 
-const forValidationOptions: RegisterOptions = {
+export const forValidationOptions = (evaluateEvery: string): RegisterOptions => ({
   required: {
     value: true,
     message: 'Required.',
   },
-  pattern: durationValidationPattern,
-};
-
-const evaluateEveryValidationOptions: RegisterOptions = {
-  required: {
-    value: true,
-    message: 'Required.',
-  },
-  pattern: positiveDurationValidationPattern,
   validate: (value: string) => {
-    const duration = parseDuration(value);
-    if (Object.keys(duration).length) {
-      const diff = durationToMilliseconds(duration);
-      if (diff < MIN_TIME_RANGE_STEP_S * 1000) {
+    // parsePrometheusDuration does not allow 0 but does allow 0s
+    if (value === '0') {
+      return true;
+    }
+
+    try {
+      const millisFor = parsePrometheusDuration(value);
+
+      // 0 is a special value meaning for equals evaluation interval
+      if (millisFor === 0) {
+        return true;
+      }
+
+      try {
+        const millisEvery = parsePrometheusDuration(evaluateEvery);
+        return millisFor >= millisEvery
+          ? true
+          : 'For duration must be greater than or equal to the evaluation interval.';
+      } catch (err) {
+        // if we fail to parse "every", assume validation is successful, or the error messages
+        // will overlap in the UI
+        return true;
+      }
+    } catch (error) {
+      return error instanceof Error ? error.message : 'Failed to parse duration';
+    }
+  },
+});
+
+export const evaluateEveryValidationOptions: RegisterOptions = {
+  required: {
+    value: true,
+    message: 'Required.',
+  },
+  validate: (value: string) => {
+    try {
+      const duration = parsePrometheusDuration(value);
+
+      if (duration < MIN_TIME_RANGE_STEP_S * 1000) {
         return `Cannot be less than ${MIN_TIME_RANGE_STEP_S} seconds.`;
       }
-      if (diff % (MIN_TIME_RANGE_STEP_S * 1000) !== 0) {
+
+      if (duration % (MIN_TIME_RANGE_STEP_S * 1000) !== 0) {
         return `Must be a multiple of ${MIN_TIME_RANGE_STEP_S} seconds.`;
       }
+
+      return true;
+    } catch (error) {
+      return error instanceof Error ? error.message : 'Failed to parse duration';
     }
-    return true;
   },
 };
 
@@ -51,6 +81,7 @@ export const GrafanaEvaluationBehavior: FC = () => {
   const {
     register,
     formState: { errors },
+    watch,
   } = useFormContext<RuleFormValues>();
 
   const evaluateEveryId = 'eval-every-input';
@@ -71,7 +102,14 @@ export const GrafanaEvaluationBehavior: FC = () => {
           >
             Evaluate every
           </InlineLabel>
-          <Input id={evaluateEveryId} width={8} {...register('evaluateEvery', evaluateEveryValidationOptions)} />
+          <Field
+            className={styles.inlineField}
+            error={errors.evaluateEvery?.message}
+            invalid={!!errors.evaluateEvery?.message}
+            validationMessageHorizontalOverflow={true}
+          >
+            <Input id={evaluateEveryId} width={8} {...register('evaluateEvery', evaluateEveryValidationOptions)} />
+          </Field>
           <InlineLabel
             htmlFor={evaluateForId}
             width={7}
@@ -85,7 +123,11 @@ export const GrafanaEvaluationBehavior: FC = () => {
             invalid={!!errors.evaluateFor?.message}
             validationMessageHorizontalOverflow={true}
           >
-            <Input id={evaluateForId} width={8} {...register('evaluateFor', forValidationOptions)} />
+            <Input
+              id={evaluateForId}
+              width={8}
+              {...register('evaluateFor', forValidationOptions(watch('evaluateEvery')))}
+            />
           </Field>
         </div>
       </Field>
