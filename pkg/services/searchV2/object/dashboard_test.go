592c5472266483fe9cc20b06a5409e8f62e786a3
Ryan McKinley <ryantxu@gmail.com>
2022-10-07 17:56:18 -0700

    remove tests that check all devenv etags

--- pkg/services/searchV2/object/dashboard_test.go
+++ pkg/services/searchV2/object/dashboard_test.go
@@ -2,15 +2,12 @@ package object
 
 import (
 	"bytes"
-	"crypto/md5"
-	"encoding/hex"
 	"encoding/json"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/grafana/grafana-plugin-sdk-go/experimental"
 	"github.com/grafana/grafana/pkg/services/searchV2/dslookup"
 	"github.com/grafana/grafana/pkg/services/store/object"
 	"github.com/stretchr/testify/require"
@@ -33,14 +30,11 @@ func dsLookup() dslookup.DatasourceLookup {
 	})
 }
 
-func TestReadDashboard(t *testing.T) {
-	devdash := "../../../../devenv/dev-dashboards/"
+func TestReadSummaries(t *testing.T) {
+	devdash := "../../../../devenv/dev-dashboards/panel-graph/"
 
 	reader := NewDashboardSummaryBuilder(dsLookup())
 	failed := make([]string, 0, 10)
-	table := newSummaryTable()
-
-	snapshots := false
 
 	err := filepath.Walk(devdash,
 		func(path string, info os.FileInfo, err error) error {
@@ -56,39 +50,31 @@ func TestReadDashboard(t *testing.T) {
 					return err
 				}
 
-				obj := &object.RawObject{
-					UID:     path[len(devdash):],
-					Size:    info.Size(),
-					Updated: info.ModTime().UnixMilli(),
-					Body:    body,
-					ETag:    createContentsHash(body),
-				}
-
-				summary, err := reader(obj)
+				uid := path[len(devdash):]
+				summary, err := reader(&object.RawObject{
+					UID:  uid,
+					Body: body,
+				})
 				if err != nil {
 					return err
 				}
-				table.Add(obj, summary)
 
-				// Check each snapshot
-				if snapshots {
-					out, err := json.MarshalIndent(summary, "", "  ")
+				out, err := json.MarshalIndent(summary, "", "  ")
+				if err != nil {
+					return err
+				}
+
+				gpath := "testdata/gdev-walk-" + strings.ReplaceAll(uid, "/", "-")
+
+				// Ignore gosec warning G304 since it's a test
+				// nolint:gosec
+				golden, _ := os.ReadFile(gpath)
+
+				if !bytes.Equal(out, golden) {
+					failed = append(failed, uid)
+					err = os.WriteFile(gpath, out, 0600)
 					if err != nil {
 						return err
-					}
-
-					gpath := "testdata/gdev-walk-" + strings.ReplaceAll(obj.UID, "/", "-")
-
-					// Ignore gosec warning G304 since it's a test
-					// nolint:gosec
-					golden, _ := os.ReadFile(gpath)
-
-					if !bytes.Equal(out, golden) {
-						failed = append(failed, obj.UID)
-						err = os.WriteFile(gpath, out, 0600)
-						if err != nil {
-							return err
-						}
 					}
 				}
 			}
@@ -96,17 +82,6 @@ func TestReadDashboard(t *testing.T) {
 		})
 	require.NoError(t, err)
 
-	// Check the tabular formts
-	experimental.CheckGoldenJSONFrame(t, "testdata", "dash_raw", table.Raw, true)
-	experimental.CheckGoldenJSONFrame(t, "testdata", "dash_summary", table.Summary, true)
-	experimental.CheckGoldenJSONFrame(t, "testdata", "dash_references", table.References, true)
-	experimental.CheckGoldenJSONFrame(t, "testdata", "dash_labels", table.Labels, true)
-
 	// accumulated in the walk test
 	require.Equal(t, []string{}, failed)
-}
-
-func createContentsHash(contents []byte) string {
-	hash := md5.Sum(contents)
-	return hex.EncodeToString(hash[:])
 }
