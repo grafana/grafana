7e24e192308820aa2632c48091e417eadf64b96f
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-10-11 17:06:36 +0300

    Swagger: Remove license (#56716)

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,7 +6,6 @@
 //	Schemes: http, https
 //	BasePath: /api
 //	Version: 0.0.1
-//	License: GNU Affero General Public License v3.0 https://www.gnu.org/licenses/agpl-3.0.en.html
 //	Contact: Grafana Labs<hello@grafana.com> https://grafana.com
 //
 //	Consumes:
