b668e469ef22a6c065a7ec8737eeaef789aa95d1
Mihaly Gyongyosi <mgyongyosi@users.noreply.github.com>
2022-10-13 18:19:18 +0200

    Invalidate OAuth tokens during logout

--- pkg/api/login.go
+++ pkg/api/login.go
@@ -299,6 +299,12 @@ func (hs *HTTPServer) Logout(c *models.ReqContext) {
 		}
 	}
 
+	if exists, entry := hs.oauthTokenService.HasOAuthEntry(c.Req.Context(), c.SignedInUser); exists {
+		if err := hs.oauthTokenService.InvalidateOAuthTokens(c.Req.Context(), entry); err != nil {
+			hs.log.Warn("failed to invalidate OAuth tokens for User", "userId", c.UserID, "error", err)
+		}
+	}
+
 	err := hs.AuthTokenService.RevokeToken(c.Req.Context(), c.UserToken, false)
 	if err != nil && !errors.Is(err, models.ErrUserTokenNotFound) {
 		hs.log.Error("failed to revoke auth token", "error", err)
