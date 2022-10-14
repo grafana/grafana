166197110f9026d1e016f9adab5ce3c7ed2e9a6e
yaellec <yaelle.chaudy@grafana.com>
2022-10-14 14:31:11 +0200

    Made dropdown menu keyboard accessible

--- public/app/core/components/PageHeader/PageHeader.tsx
+++ public/app/core/components/PageHeader/PageHeader.tsx
@@ -22,10 +22,16 @@ const SelectNav = ({ children, customCss }: { children: NavModelItem[]; customCs
 
   return (
     <div className={`gf-form-select-wrapper width-20 ${customCss}`}>
-      <div className="dropdown">
-        <div className="gf-form-input dropdown-toggle" data-toggle="dropdown">
+      <div role="menu" className="dropdown">
+        <button
+          type="button"
+          role="option"
+          aria-selected
+          className="gf-form-input dropdown-toggle"
+          data-toggle="dropdown"
+        >
           {defaultSelectedItem?.text}
-        </div>
+        </button>
         <ul className="dropdown-menu dropdown-menu--menu">
           {children.map((navItem: NavModelItem) => {
             if (navItem.hideFromTabs) {
