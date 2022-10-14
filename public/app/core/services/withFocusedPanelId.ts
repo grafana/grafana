e31b44d4ba591499563c6bb64d68cc5f6d7bab2d
Ashley Harrison <ashley.harrison@grafana.com>
2022-10-14 17:04:03 +0100

    any/type assertion fixes

--- public/app/core/services/withFocusedPanelId.ts
+++ public/app/core/services/withFocusedPanelId.ts
@@ -3,8 +3,8 @@ export function withFocusedPanel(fn: (panelId: number) => void) {
     const elements = document.querySelectorAll(':hover');
 
     for (let i = elements.length - 1; i > 0; i--) {
-      const element = elements[i] as unknown as HTMLElement;
-      if (element.dataset?.panelid) {
+      const element = elements[i];
+      if (element instanceof HTMLElement && element.dataset?.panelid) {
         fn(parseInt(element.dataset?.panelid, 10));
       }
     }
