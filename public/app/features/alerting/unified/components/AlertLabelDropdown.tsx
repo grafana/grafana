418d46d1a65365c8ad86635e7e4a08cac419b7a2
Virginia Cepeda <virginia.cepeda@grafana.com>
2022-10-17 10:06:13 -0300

    Use ref in component

--- public/app/features/alerting/unified/components/AlertLabelDropdown.tsx
+++ public/app/features/alerting/unified/components/AlertLabelDropdown.tsx
@@ -10,26 +10,27 @@ export interface AlertLabelDropdownProps {
   type: 'key' | 'value';
 }
 
-const AlertLabelDropdown: FC<AlertLabelDropdownProps> = React.forwardRef(function labelPicker(
-  { onChange, options, defaultValue, type },
-  ref
-) {
-  return (
-    <Field disabled={false} data-testid={`alertlabel-${type}-picker`}>
-      <Select
-        placeholder={'Choose key'}
-        width={29}
-        className="ds-picker select-container"
-        backspaceRemovesValue={false}
-        onChange={onChange}
-        options={options}
-        maxMenuHeight={500}
-        noOptionsMessage="No labels found"
-        defaultValue={defaultValue}
-        allowCustomValue
-      />
-    </Field>
-  );
-});
+const AlertLabelDropdown: FC<AlertLabelDropdownProps> = React.forwardRef<HTMLDivElement, AlertLabelDropdownProps>(
+  function labelPicker({ onChange, options, defaultValue, type }, ref) {
+    return (
+      <div ref={ref}>
+        <Field disabled={false} data-testid={`alertlabel-${type}-picker`}>
+          <Select
+            placeholder={'Choose key'}
+            width={29}
+            className="ds-picker select-container"
+            backspaceRemovesValue={false}
+            onChange={onChange}
+            options={options}
+            maxMenuHeight={500}
+            noOptionsMessage="No labels found"
+            defaultValue={defaultValue}
+            allowCustomValue
+          />
+        </Field>
+      </div>
+    );
+  }
+);
 
 export default AlertLabelDropdown;
