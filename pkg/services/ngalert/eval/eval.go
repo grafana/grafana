0ebb2d1618e1b34aa556dd438e5bfa8cabf97ff2
Alex Weaver <weaver.alex.d@gmail.com>
2022-10-17 14:46:25 -0500

    Extract header logic and add rule UID header

--- pkg/services/ngalert/eval/eval.go
+++ pkg/services/ngalert/eval/eval.go
@@ -157,12 +157,31 @@ func (s State) String() string {
 	return [...]string{"Normal", "Alerting", "Pending", "NoData", "Error"}[s]
 }
 
+func buildDatasourceHeaders(ctx EvaluationContext) map[string]string {
+	headers := map[string]string{
+		// Many data sources check this in query method as sometimes alerting needs special considerations.
+		// Several existing systems also compare against the value of this header. Altering this constitutes a breaking change.
+		//
+		// Note: The spelling of this headers is intentionally degenerate from the others for compatibility reasons.
+		// When sent over a network, the key of this header is canonicalized to "Fromalert".
+		"FromAlert": "true",
+
+		"X-Cache-Skip": "true",
+	}
+
+	if ctx.RuleUID != "" {
+		headers["X-Rule-Uid"] = ctx.RuleUID
+	}
+
+	return headers
+}
+
 // getExprRequest validates the condition, gets the datasource information and creates an expr.Request from it.
 func getExprRequest(ctx EvaluationContext, data []models.AlertQuery, dsCacheService datasources.CacheService) (*expr.Request, error) {
 	req := &expr.Request{
 		OrgId: ctx.User.OrgID,
 		Headers: map[string]string{
-			// Some data sources check this in query method as sometimes alerting needs special considerations.
+
 			"FromAlert":    "true",
 			"X-Cache-Skip": "true",
 		},
