7a91afc504996e1ec9e0d9585f16ca846fc4d6a7
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-10-18 14:50:59 +0800

    Search: Sort alphabetically in the folder view, increase the limit of the folder search from 50 to 1000 (#57078) (#57141)
    * search: sort folders
    
    * search: increase the limit for folder search to 1000
    
    * add folder view sort test
    
    * search: getFolderViewSort
    
    * search: revert test
    
    Co-authored-by: Todd Treece <todd.treece@grafana.com>
    (cherry picked from commit c26cf6a51791967e1892c8cc2f177401dc4977af)

--- public/app/features/search/service/types.ts
+++ public/app/features/search/service/types.ts
@@ -71,4 +71,7 @@ export interface GrafanaSearcher {
   starred: (query: SearchQuery) => Promise<QueryResponse>;
   tags: (query: SearchQuery) => Promise<TermCount[]>;
   getSortOptions: () => Promise<SelectableValue[]>;
+
+  /** Gets the default sort used for the Folder view */
+  getFolderViewSort: () => string;
 }
