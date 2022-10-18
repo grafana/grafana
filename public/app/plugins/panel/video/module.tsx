1da2aebd7f13a67aa716160b0bca84d5894951cc
Ryan McKinley <ryantxu@gmail.com>
2022-10-18 10:28:24 -0700

    cleanup

--- public/app/plugins/panel/video/module.tsx
+++ public/app/plugins/panel/video/module.tsx
@@ -1,32 +1,15 @@
-import { FieldColorModeId, FieldConfigProperty, PanelPlugin, SetFieldConfigOptionsArgs } from '@grafana/data';
+import { PanelPlugin } from '@grafana/data';
 import { GraphFieldConfig } from '@grafana/schema';
 
 import { VideoPanel } from './VideoPanel';
-import { PanelOptions, defaultPanelOptions, defaultPanelFieldConfig, PanelFieldConfig } from './models.gen';
+import { PanelOptions, defaultPanelOptions } from './models.gen';
 
-export const plugin = new PanelPlugin<PanelOptions, GraphFieldConfig>(VideoPanel)
-  .useFieldConfig(getVideoFieldConfig(defaultPanelFieldConfig))
-  .setPanelOptions((builder, context) => {
-    const opts = context.options ?? defaultPanelOptions;
-    // TODO
-    console.log('OPTS', opts);
-  });
-
-function getVideoFieldConfig(cfg: PanelFieldConfig): SetFieldConfigOptionsArgs<PanelFieldConfig> {
-  return {
-    standardOptions: {
-      [FieldConfigProperty.Color]: {
-        settings: {
-          byValueSupport: true,
-          bySeriesSupport: true,
-          preferThresholdsMode: false,
-        },
-        defaultValue: {
-          mode: FieldColorModeId.PaletteClassic,
-        },
-      },
-    },
-
-    useCustomConfig: (builder) => {},
-  };
-}
+export const plugin = new PanelPlugin<PanelOptions, GraphFieldConfig>(VideoPanel).setPanelOptions(
+  (builder, context) => {
+    builder.addBooleanSwitch({
+      path: 'autoPlay',
+      name: 'Auto play',
+      defaultValue: defaultPanelOptions.autoPlay,
+    });
+  }
+);
