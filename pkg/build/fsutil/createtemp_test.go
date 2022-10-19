78a3e64e4fb4b65788259fe5c4436e55abec13e0
Dimitris Sotirakis <dimitrios.sotirakis@grafana.com>
2022-10-19 13:58:22 +0300

    CI: Add `CreateTempDir` func and use it in publish packages logic (#57171)
    * Add CreateTempDir func and use it in publish packages logic
    
    * Fill err return in defer func
    
    (cherry picked from commit e829b5014911363be862306b34a41fb0eeb13362)

--- pkg/build/fsutil/createtemp_test.go
+++ pkg/build/fsutil/createtemp_test.go
@@ -26,3 +26,23 @@ func TestCreateTempFile(t *testing.T) {
 		require.Len(t, strings.Split(pathParts[len(pathParts)-1], "-"), 2)
 	})
 }
+
+func TestCreateTempDir(t *testing.T) {
+	t.Run("empty suffix, expects pattern like: /var/folders/abcd/abcdefg/A/1137975807/", func(t *testing.T) {
+		filePath, err := CreateTempFile("")
+		require.NoError(t, err)
+
+		pathParts := strings.Split(filePath, "/")
+		require.Greater(t, len(pathParts), 1)
+		require.Len(t, strings.Split(pathParts[len(pathParts)-1], "-"), 1)
+	})
+
+	t.Run("non-empty suffix, expects /var/folders/abcd/abcdefg/A/1137975807-foobar/", func(t *testing.T) {
+		filePath, err := CreateTempFile("foobar")
+		require.NoError(t, err)
+
+		pathParts := strings.Split(filePath, "/")
+		require.Greater(t, len(pathParts), 1)
+		require.Len(t, strings.Split(pathParts[len(pathParts)-1], "-"), 2)
+	})
+}
