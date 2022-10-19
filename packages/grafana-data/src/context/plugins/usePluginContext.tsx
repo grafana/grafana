49deea458e0534d1c40d8e4fcc7be12749f6dd72
Marcus Andersson <marcus.andersson@grafana.com>
2022-10-19 07:55:35 +0200

    fixed error message when trying to use hook outside of context.

--- packages/grafana-data/src/context/plugins/usePluginContext.tsx
+++ packages/grafana-data/src/context/plugins/usePluginContext.tsx
@@ -5,7 +5,7 @@ import { Context, PluginContextType } from './PluginContext';
 export function usePluginContext(): PluginContextType {
   const context = useContext(Context);
   if (!context) {
-    throw new Error('add a b etter error message');
+    throw new Error('usePluginContext must be used within a PluginContextProvider');
   }
   return context;
 }
