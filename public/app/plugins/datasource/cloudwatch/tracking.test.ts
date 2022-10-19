87d2ce176b7345fcdba70574cccf75bcf6c78b32
Erik Sundell <erik.sundell87@gmail.com>
2022-10-19 08:39:18 +0200

    CloudWatch: Make sure adoption tracking is done on valid, migrated queries (#56872)
    * make sure adoption tracking is done on valid, migrated queries
    
    * ignore hidden queries
    
    * fix test
    
    * remove obsolete test
    
    (cherry picked from commit a30885c523e0ce4a05047c7939df69adc8c2ebbd)

--- public/app/plugins/datasource/cloudwatch/tracking.test.ts
+++ public/app/plugins/datasource/cloudwatch/tracking.test.ts
@@ -26,14 +26,14 @@ describe('onDashboardLoadedHandler', () => {
       grafana_version: 'v9.0.0',
       org_id: 1,
       logs_queries_count: 1,
-      metrics_queries_count: 21,
+      metrics_queries_count: 20,
       metrics_query_builder_count: 3,
       metrics_query_code_count: 4,
       metrics_query_count: 7,
-      metrics_search_builder_count: 9,
+      metrics_search_builder_count: 8,
       metrics_search_code_count: 5,
-      metrics_search_count: 14,
-      metrics_search_match_exact_count: 9,
+      metrics_search_count: 13,
+      metrics_search_match_exact_count: 8,
     });
   });
 });
