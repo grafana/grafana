c34c1d0cb4665ef74c1c935d760c08c300625c2b
Brendan O'Handley <brendan.ohandley@grafana.com>
2022-10-20 15:49:40 -0500

    Graphite: Never escape asPercent function params as string (#56593)
    * handle asPercent func by never esc params as string
    
    * add test and clean up

--- public/app/plugins/datasource/graphite/gfunc.test.ts
+++ public/app/plugins/datasource/graphite/gfunc.test.ts
@@ -1,4 +1,4 @@
-import gfunc from './gfunc';
+import gfunc, { FuncInstance } from './gfunc';
 
 describe('gfunc', () => {
   const INDEX = {
@@ -19,5 +19,36 @@ describe('gfunc', () => {
       defaultParams: [''],
       unknown: true,
     });
+  });
+
+  it('renders the version < .9 asPercent function parameters by not escaping them as a string', () => {
+    // this function is returned from the graphite functions endpoint
+    const asPercentDef = {
+      name: 'asPercent',
+      description: 'Calculates a percentage.',
+      category: 'Combine',
+      params: [
+        {
+          name: 'total',
+          type: 'string',
+          optional: true,
+          multiple: false,
+        },
+        {
+          name: 'nodes',
+          type: 'node_or_tag',
+          optional: true,
+          multiple: true,
+          options: [],
+        },
+      ],
+      defaultParams: [],
+    };
+
+    const asPercent = new FuncInstance(asPercentDef);
+
+    const asPercentRendered = asPercent.render('#A', () => '#A');
+
+    expect(asPercentRendered).toEqual('asPercent(#A)');
   });
 });
