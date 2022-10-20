29c45a7ca0b591687840bc6db2b74c5672e7a937
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-10-20 16:05:47 +0200

    CloudWatch: Extract request parsing grouping by region from ParseQueries

--- pkg/tsdb/cloudwatch/models/request_parser.go
+++ pkg/tsdb/cloudwatch/models/request_parser.go
@@ -25,24 +25,24 @@ type metricsDataQuery struct {
 	Datasource        map[string]string      `json:"datasource,omitempty"`
 	Dimensions        map[string]interface{} `json:"dimensions,omitempty"`
 	Expression        string                 `json:"expression,omitempty"`
-	Id                string                 `json:"id,omitempty"`
-	Label             *string                `json:"label,omitempty"`
-	MatchExact        *bool                  `json:"matchExact,omitempty"`
-	MaxDataPoints     int                    `json:"maxDataPoints,omitempty"`
-	MetricEditorMode  *int                   `json:"metricEditorMode,omitempty"`
-	MetricName        string                 `json:"metricName,omitempty"`
-	MetricQueryType   MetricQueryType        `json:"metricQueryType,omitempty"`
-	Namespace         string                 `json:"namespace,omitempty"`
-	Period            string                 `json:"period,omitempty"`
-	RefId             string                 `json:"refId,omitempty"`
-	Region            string                 `json:"region,omitempty"`
-	SqlExpression     string                 `json:"sqlExpression,omitempty"`
-	Statistic         *string                `json:"statistic,omitempty"`
-	Statistics        []*string              `json:"statistics,omitempty"`
-	TimezoneUTCOffset string                 `json:"timezoneUTCOffset,omitempty"`
-	QueryType         string                 `json:"type,omitempty"`
-	Hide              *bool                  `json:"hide,omitempty"`
-	Alias             string                 `json:"alias,omitempty"`
+	Id                string          `json:"id,omitempty"`
+	Label             *string         `json:"label,omitempty"`
+	MatchExact        *bool           `json:"matchExact,omitempty"`
+	MaxDataPoints     int             `json:"maxDataPoints,omitempty"`
+	MetricEditorMode  *int            `json:"metricEditorMode,omitempty"`
+	MetricName        string          `json:"metricName,omitempty"`
+	MetricQueryType   MetricQueryType `json:"metricQueryType,omitempty"`
+	Namespace         string          `json:"namespace,omitempty"`
+	Period            string          `json:"period,omitempty"`
+	RefId             string          `json:"refId,omitempty"`
+	Region            string          `json:"region,omitempty"`
+	SqlExpression     string          `json:"sqlExpression,omitempty"`
+	Statistic         *string         `json:"statistic,omitempty"`
+	Statistics        []*string       `json:"statistics,omitempty"`
+	TimezoneUTCOffset string          `json:"timezoneUTCOffset,omitempty"`
+	QueryType         string          `json:"type,omitempty"`
+	Hide              *bool           `json:"hide,omitempty"`
+	Alias             string          `json:"alias,omitempty"`
 }
 
 // ParseMetricDataQueries decodes the metric data queries json, validates, sets default values and returns an array of CloudWatchQueries.
@@ -71,7 +71,7 @@ func ParseMetricDataQueries(queries []backend.DataQuery, startTime time.Time, en
 			metricsDataQuery.MatchExact = &trueBooleanValue
 		}
 
-		refID := query.RefID
+		refID := metricsDataQuery.RefId
 		cwQuery, err := parseRequestQuery(metricsDataQuery, refID, startTime, endTime)
 		if err != nil {
 			return nil, &QueryError{Err: err, RefID: refID}
