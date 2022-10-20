431d780f39d75a4d203488fb4e880db224b4250f
Galen <galen.kistler@grafana.com>
2022-10-20 15:31:09 -0500

    simplify parameter assignment

--- public/app/plugins/datasource/prometheus/metric_find_query.ts
+++ public/app/plugins/datasource/prometheus/metric_find_query.ts
@@ -70,17 +70,7 @@ export default class PrometheusMetricFindQuery {
   labelValuesQuery(label: string, metric?: string) {
     const start = this.datasource.getPrometheusTime(this.range.from, false);
     const end = this.datasource.getPrometheusTime(this.range.to, true);
-
-    const params = metric
-      ? {
-          'match[]': metric,
-          start: start.toString(),
-          end: end.toString(),
-        }
-      : {
-          start: start.toString(),
-          end: end.toString(),
-        };
+    const params = { ...(metric && { 'match[]': metric }), start: start.toString(), end: end.toString() };
 
     if (!metric || this.datasource.doesDatasourceSupportLabelsMatchAPI()) {
       const url = `/api/v1/label/${label}/values`;
