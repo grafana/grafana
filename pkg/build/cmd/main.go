4b05ea5e1189d9aa65c7e21c14f3750828be36a6
Guilherme Caulada <guilherme.caulada@grafana.com>
2022-10-20 11:11:53 -0300

    Build: Add github release command to build/cmd (#56349)
    * Add github release command to build/cmd
    
    * Use go-github library and implement dry-run
    
    * Make tag optional and default to metadata
    
    * Fix minor bug with tag default
    
    * Make some refactors to ease testing
    
    * Add tests for publish github command
    
    * Refactor publish github tests
    
    * Refactor test helper function name
    
    * Isolate local test
    
    (cherry picked from commit 96a97f9827a89b96e70e00259d712f85ff6d0880)

--- pkg/build/cmd/main.go
+++ pkg/build/cmd/main.go
@@ -200,8 +200,9 @@ func main() {
 					Flags: []cli.Flag{
 						&dryRunFlag,
 						&cli.StringFlag{
-							Name:  "path",
-							Usage: "Path to the asset to be published",
+							Name:     "path",
+							Required: true,
+							Usage:    "Path to the asset to be published",
 						},
 						&cli.StringFlag{
 							Name:     "repo",
