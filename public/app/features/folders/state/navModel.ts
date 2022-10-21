2a4c889e0ec300e704f5321648c5994ffbe978f2
Sonia Aguilar <33540275+soniaAguilarPeiron@users.noreply.github.com>
2022-10-21 16:17:20 +0200

    fix: add alerting enabled condition FOR adding alerting tab in folder view (#57434) (#57451)
    (cherry picked from commit cd0e0fcc538ce26eca94bd0324f48e72323b5a61)

--- public/app/features/folders/state/navModel.ts
+++ public/app/features/folders/state/navModel.ts
@@ -1,4 +1,5 @@
 import { NavModel, NavModelItem } from '@grafana/data';
+import { config } from '@grafana/runtime';
 import { contextSrv } from 'app/core/services/context_srv';
 import { AccessControlAction, FolderDTO } from 'app/types';
 
@@ -29,8 +30,8 @@ export function buildNavModel(folder: FolderDTO): NavModelItem {
     url: `${folder.url}/library-panels`,
   });
 
-  if (contextSrv.hasPermission(AccessControlAction.AlertingRuleRead)) {
-    model.children.push({
+  if (contextSrv.hasPermission(AccessControlAction.AlertingRuleRead) && config.unifiedAlertingEnabled) {
+    model.children!.push({
       active: false,
       icon: 'bell',
       id: `folder-alerting-${folder.uid}`,
