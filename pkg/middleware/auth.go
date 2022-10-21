40ac6c6016ff5b0d028ef7c001cc8d6185bc5799
Kristina Durivage <kristina.durivage@grafana.com>
2022-10-21 09:02:20 -0500

    Remove redundant code, reverse polarity

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -64,15 +64,13 @@ func writeRedirectCookie(c *models.ReqContext) {
 		redirectTo = setting.AppSubUrl + c.Req.RequestURI
 	}
 
-	if redirectTo != "/" {
-		if setting.AppSubUrl != "" && !strings.HasPrefix(redirectTo, setting.AppSubUrl) {
-			redirectTo = setting.AppSubUrl + c.Req.RequestURI
-		}
-
-		// remove any forceLogin=true params
-		redirectTo = removeForceLoginParams(redirectTo)
-		cookies.WriteCookie(c.Resp, "redirect_to", url.QueryEscape(redirectTo), 0, nil)
+	if redirectTo == "/" {
+		return
 	}
+
+	// remove any forceLogin=true params
+	redirectTo = removeForceLoginParams(redirectTo)
+	cookies.WriteCookie(c.Resp, "redirect_to", url.QueryEscape(redirectTo), 0, nil)
 }
 
 var forceLoginParamsRegexp = regexp.MustCompile(`&?forceLogin=true`)
