87df7bc8f90fddba044817bed1a2130a7e2e4a64
Alex Moreno <alexander.moreno@grafana.com>
2022-10-25 15:36:01 +0200

    Use simplejson

--- pkg/services/ngalert/notifier/channels/pagerduty.go
+++ pkg/services/ngalert/notifier/channels/pagerduty.go
@@ -61,42 +61,9 @@ func PagerdutyFactory(fc FactoryConfig) (NotificationChannel, error) {
 
 // NewPagerdutyNotifier is the constructor for the PagerDuty notifier
 func newPagerdutyNotifier(fc FactoryConfig) (*PagerdutyNotifier, error) {
-	settings := pagerdutySettings{}
-	err := fc.Config.unmarshalSettings(&settings)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal settings: %w", err)
-	}
-
-	settings.Key = fc.DecryptFunc(context.Background(), fc.Config.SecureSettings, "integrationKey", settings.Key)
-	if settings.Key == "" {
+	key := fc.DecryptFunc(context.Background(), fc.Config.SecureSettings, "integrationKey", fc.Config.Settings.Get("integrationKey").MustString())
+	if key == "" {
 		return nil, errors.New("could not find integration key property in settings")
-	}
-
-	if settings.Severity == "" {
-		settings.Severity = "critical"
-	}
-
-	settings.customDetails = map[string]string{
-		"firing":       `{{ template "__text_alert_list" .Alerts.Firing }}`,
-		"resolved":     `{{ template "__text_alert_list" .Alerts.Resolved }}`,
-		"num_firing":   `{{ .Alerts.Firing | len }}`,
-		"num_resolved": `{{ .Alerts.Resolved | len }}`,
-	}
-
-	if settings.Class == "" {
-		settings.Class = "default"
-	}
-
-	if settings.Component == "" {
-		settings.Component = "Grafana"
-	}
-
-	if settings.Group == "" {
-		settings.Group = "default"
-	}
-
-	if settings.Summary == "" {
-		settings.Summary = DefaultMessageTitleEmbed
 	}
 
 	return &PagerdutyNotifier{
@@ -107,11 +74,24 @@ func newPagerdutyNotifier(fc FactoryConfig) (*PagerdutyNotifier, error) {
 			DisableResolveMessage: fc.Config.DisableResolveMessage,
 			Settings:              fc.Config.Settings,
 		}),
-		tmpl:     fc.Template,
-		log:      log.New("alerting.notifier." + fc.Config.Name),
-		ns:       fc.NotificationService,
-		images:   fc.ImageStore,
-		settings: settings,
+		tmpl:   fc.Template,
+		log:    log.New("alerting.notifier." + fc.Config.Name),
+		ns:     fc.NotificationService,
+		images: fc.ImageStore,
+		settings: pagerdutySettings{
+			Key:      key,
+			Severity: fc.Config.Settings.Get("severity").MustString("critical"),
+			customDetails: map[string]string{
+				"firing":       `{{ template "__text_alert_list" .Alerts.Firing }}`,
+				"resolved":     `{{ template "__text_alert_list" .Alerts.Resolved }}`,
+				"num_firing":   `{{ .Alerts.Firing | len }}`,
+				"num_resolved": `{{ .Alerts.Resolved | len }}`,
+			},
+			Class:     fc.Config.Settings.Get("class").MustString("default"),
+			Component: fc.Config.Settings.Get("component").MustString("Grafana"),
+			Group:     fc.Config.Settings.Get("group").MustString("default"),
+			Summary:   fc.Config.Settings.Get("summary").MustString(DefaultMessageTitleEmbed),
+		},
 	}, nil
 }
 
