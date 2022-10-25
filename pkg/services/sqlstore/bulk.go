abeab867191ae041e839ec584fb0cf404348f7c6
Alex Weaver <weaver.alex.d@gmail.com>
2022-10-25 13:29:04 -0500

    Validate batch size

--- pkg/services/sqlstore/bulk.go
+++ pkg/services/sqlstore/bulk.go
@@ -7,6 +7,8 @@ import (
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 )
 
+const DefaultBatchSize = 1000
+
 type BulkOpSettings struct {
 	BatchSize int
 }
@@ -15,6 +17,13 @@ func NativeSettingsForDialect(d migrator.Dialect) BulkOpSettings {
 	return BulkOpSettings{
 		BatchSize: d.BatchSize(),
 	}
+}
+
+func normalizeBulkSettings(s BulkOpSettings) BulkOpSettings {
+	if s.BatchSize < 1 {
+		s.BatchSize = DefaultBatchSize
+	}
+	return s
 }
 
 func (sess *DBSession) BulkInsert(table interface{}, recordsSlice interface{}, opts BulkOpSettings) (int64, error) {
@@ -28,6 +37,7 @@ func (sess *DBSession) BulkInsert(table interface{}, recordsSlice interface{}, o
 }
 
 func InBatches(items interface{}, opts BulkOpSettings, fn func(batch interface{}) error) error {
+	opts = normalizeBulkSettings(opts)
 	slice := reflect.Indirect(reflect.ValueOf(items))
 	if slice.Kind() != reflect.Slice {
 		return fmt.Errorf("need a slice in order to batch records")
