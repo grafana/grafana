fe6964d3475586dc7d233dfe869fe27cb2f8f4a8
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-10-26 14:26:47 +0300

    Nested Folders: Add store tests for Create()

--- pkg/services/folder/folderimpl/store_test.go
+++ pkg/services/folder/folderimpl/store_test.go
@@ -1,10 +1,114 @@
 package folderimpl
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestCreate(t *testing.T) {}
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
+	"github.com/grafana/grafana/pkg/services/folder"
+	"github.com/grafana/grafana/pkg/services/org"
+	"github.com/grafana/grafana/pkg/services/org/orgimpl"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-func TestDelete(t *testing.T) {}
+func TestCreate(t *testing.T) {
+	db := sqlstore.InitTestDB(t)
+	store := ProvideStore(db, db.Cfg, *featuremgmt.WithFeatures())
+
+	orgService := orgimpl.ProvideService(db, db.Cfg)
+	orgID, err := orgService.GetOrCreate(context.Background(), "test-org")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err = orgService.Delete(context.Background(), &org.DeleteOrgCommand{ID: orgID})
+		require.NoError(t, err)
+	})
+
+	t.Run("creating a 1st level folder should succeed", func(t *testing.T) {
+		title := "folder1"
+		f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
+			Title: title,
+			// OrgID: orgID,
+		})
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := store.Delete(context.Background(), f.UID, orgID)
+			require.NoError(t, err)
+		})
+
+		assert.Equal(t, title, f.Title)
+		assert.NotEmpty(t, f.ID)
+		assert.NotEmpty(t, f.UID)
+
+		parents, err := store.GetParents(context.Background(), &folder.GetParentsCommand{
+			UID: f.UID,
+		})
+		require.NoError(t, err)
+		assert.Len(t, parents, 1)
+		assert.Equal(t, accesscontrol.GeneralFolderUID, parents[0].UID)
+	})
+
+	t.Run("creating a folder with unknown parent should fail", func(t *testing.T) {
+		title := "folder1"
+		_, err := store.Create(context.Background(), &folder.CreateFolderCommand{
+			Title: title,
+			// OrgID: orgID,
+			ParentUID: "unknown",
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("creating a folder with known parent should succeed", func(t *testing.T) {
+		title := "folder1"
+		parent, err := store.Create(context.Background(), &folder.CreateFolderCommand{
+			Title: title,
+			// OrgID: orgID,
+		})
+		require.NoError(t, err)
+		require.Equal(t, title, parent.Title)
+		require.NotEmpty(t, parent.ID)
+		assert.NotEmpty(t, parent.UID)
+		t.Cleanup(func() {
+			err := store.Delete(context.Background(), parent.UID, orgID)
+			require.NoError(t, err)
+		})
+		ancestors, err := store.GetParents(context.Background(), &folder.GetParentsCommand{
+			UID: parent.UID,
+		})
+		require.NoError(t, err)
+		assert.Len(t, ancestors, 1)
+		assert.Equal(t, accesscontrol.GeneralFolderUID, ancestors[0])
+
+		title = "folder2"
+		f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
+			Title: title,
+			// OrgID: orgID,
+		})
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := store.Delete(context.Background(), f.UID, orgID)
+			require.NoError(t, err)
+		})
+
+		assert.Equal(t, title, f.Title)
+		assert.NotEmpty(t, f.ID)
+		assert.NotEmpty(t, f.UID)
+
+		ancestors, err = store.GetParents(context.Background(), &folder.GetParentsCommand{
+			UID: f.UID,
+		})
+		require.NoError(t, err)
+		assert.Len(t, ancestors, 2)
+		assert.Equal(t, accesscontrol.GeneralFolderUID, ancestors[0].UID)
+		assert.Equal(t, parent.UID, ancestors[1].UID)
+	})
+}
+
+func TestDelete(t *testing.T) {
+
+}
 
 func TestUpdate(t *testing.T) {}
 
