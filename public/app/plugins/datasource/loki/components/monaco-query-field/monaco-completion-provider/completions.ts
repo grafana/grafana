6f71e86766d882b53f53e09e948a7a096422a215
Matias Chomicki <matyax@gmail.com>
2022-10-26 15:53:43 +0200

    Chore: remove console log

--- public/app/plugins/datasource/loki/components/monaco-query-field/monaco-completion-provider/completions.ts
+++ public/app/plugins/datasource/loki/components/monaco-query-field/monaco-completion-provider/completions.ts
@@ -253,7 +253,6 @@ export async function getCompletions(
   situation: Situation,
   dataProvider: CompletionDataProvider
 ): Promise<Completion[]> {
-  console.log(situation.type);
   switch (situation.type) {
     case 'EMPTY':
     case 'AT_ROOT':
