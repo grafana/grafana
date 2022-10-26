21bfab983cd37907b693343ad3f40075b9a35a06
Ida Furjesova <ida.furjesova@grafana.com>
2022-10-26 12:10:33 +0200

    Make store private

--- pkg/services/folder/folderimpl/store.go
+++ pkg/services/folder/folderimpl/store.go
@@ -10,44 +10,69 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 )
 
-type store struct {
+type store interface {
+	// Create creates a folder and returns the newly-created folder.
+	Create(ctx context.Context, cmd *folder.CreateFolderCommand) (*folder.Folder, error)
+
+	// Delete deletes a folder from the folder store.
+	Delete(ctx context.Context, uid string, orgID int64) error
+
+	// Update updates the given folder's UID, Title, and Description.
+	// Use Move to change a dashboard's parent ID.
+	Update(ctx context.Context, cmd *folder.UpdateFolderCommand) (*folder.Folder, error)
+
+	// Move changes the given folder's parent folder uid and applies any necessary permissions changes.
+	Move(ctx context.Context, cmd *folder.MoveFolderCommand) (*folder.Folder, error)
+
+	// Get returns a folder.
+	Get(ctx context.Context, cmd *folder.GetFolderCommand) (*folder.Folder, error)
+
+	// GetParents returns an ordered list of parent folder of the given folder.
+	GetParents(ctx context.Context, cmd *folder.GetParentsCommand) ([]*folder.Folder, error)
+
+	// GetChildren returns the set of immediate children folders (depth=1) of the
+	// given folder.
+	GetChildren(ctx context.Context, cmd *folder.GetTreeCommand) ([]*folder.Folder, error)
+}
+
+type sqlStore struct {
 	db  db.DB
 	log log.Logger
 	cfg *setting.Cfg
 	fm  featuremgmt.FeatureManager
 }
 
-// store implements the folder.Store interface.
-var _ folder.Store = (*store)(nil)
+// sqlStore implements the store interface.
+var _ store = (*sqlStore)(nil)
 
-func ProvideStore(db db.DB, cfg *setting.Cfg, features featuremgmt.FeatureManager) *store {
-	return &store{db: db, log: log.New("folder-store"), cfg: cfg, fm: features}
+func ProvideStore(db db.DB, cfg *setting.Cfg, features featuremgmt.FeatureManager) *sqlStore {
+	return &sqlStore{db: db, log: log.New("folder-store"), cfg: cfg, fm: features}
 }
 
-func (s *store) Create(ctx context.Context, cmd *folder.CreateFolderCommand) (*folder.Folder, error) {
+func (s *sqlStore) Create(ctx context.Context, cmd *folder.CreateFolderCommand) (*folder.Folder, error) {
 	panic("not implemented")
 }
 
-func (s *store) Delete(ctx context.Context, uid string, orgID int64) error {
+func (ss *sqlStore) Delete(ctx context.Context, uid string, orgID int64) error {
 	panic("not implemented")
 }
 
-func (s *store) Update(ctx context.Context, cmd *folder.UpdateFolderCommand) (*folder.Folder, error) {
+func (ss *sqlStore) Update(ctx context.Context, cmd *folder.UpdateFolderCommand) (*folder.Folder, error) {
 	panic("not implemented")
 }
 
-func (s *store) Move(ctx context.Context, cmd *folder.MoveFolderCommand) (*folder.Folder, error) {
+func (ss *sqlStore) Move(ctx context.Context, cmd *folder.MoveFolderCommand) (*folder.Folder, error) {
 	panic("not implemented")
 }
 
-func (s *store) Get(ctx context.Context, cmd *folder.GetFolderCommand) (*folder.Folder, error) {
+func (ss *sqlStore) Get(ctx context.Context, cmd *folder.GetFolderCommand) (*folder.Folder, error) {
 	panic("not implemented")
 }
 
-func (s *store) GetParents(ctx context.Context, cmd *folder.GetParentsCommand) ([]*folder.Folder, error) {
+func (ss *sqlStore) GetParents(ctx context.Context, cmd *folder.GetParentsCommand) ([]*folder.Folder, error) {
 	panic("not implemented")
 }
 
-func (s *store) GetChildren(ctx context.Context, cmd *folder.GetTreeCommand) ([]*folder.Folder, error) {
+func (ss *sqlStore) GetChildren(ctx context.Context, cmd *folder.GetTreeCommand) ([]*folder.Folder, error) {
 	panic("not implemented")
 }
