0bcf2dfddbeee06b2bba338797a886a462a624fe
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-10-27 15:57:37 +0300

    Add store tests for Move()

--- pkg/services/folder/folderimpl/sqlstore_test.go
+++ pkg/services/folder/folderimpl/sqlstore_test.go
@@ -31,6 +31,7 @@ func TestCreate(t *testing.T) {
 	t.Run("creating a 1st level folder without providing a parent should default to the root folder", func(t *testing.T) {
 		title := "folder1"
 		desc := "folder desc"
+
 		f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
 			Title:       title,
 			Description: desc,
@@ -47,14 +48,9 @@ func TestCreate(t *testing.T) {
 		assert.NotEmpty(t, f.ID)
 		assert.NotEmpty(t, f.UID)
 
-		parents, err := store.GetParents(context.Background(), &folder.GetParentsCommand{
-			UID: f.UID,
-		})
-		require.NoError(t, err)
-		assert.Len(t, parents, 1)
-		assert.Equal(t, folder.GeneralFolderUID, parents[0].UID)
+		assertAncestorUIDs(t, store, f.UID, []string{folder.GeneralFolderUID})
 
-		ff, err := store.Get(context.Background(), &folder.GetFolderCommand{
+		ff, err := store.Get(context.Background(), &folder.GetFolderQuery{
 			UID: &f.UID,
 		})
 		assert.NoError(t, err)
@@ -88,12 +84,7 @@ func TestCreate(t *testing.T) {
 			err := store.Delete(context.Background(), parent.UID, orgID)
 			require.NoError(t, err)
 		})
-		ancestors, err := store.GetParents(context.Background(), &folder.GetParentsCommand{
-			UID: parent.UID,
-		})
-		require.NoError(t, err)
-		assert.Len(t, ancestors, 1)
-		assert.Equal(t, folder.GeneralFolderUID, ancestors[0])
+		assertAncestorUIDs(t, store, parent.UID, []string{folder.GeneralFolderUID})
 
 		title = "folder2"
 		desc := "folder desc"
@@ -114,15 +105,10 @@ func TestCreate(t *testing.T) {
 		assert.NotEmpty(t, f.ID)
 		assert.NotEmpty(t, f.UID)
 
-		ancestors, err = store.GetParents(context.Background(), &folder.GetParentsCommand{
-			UID: f.UID,
-		})
-		require.NoError(t, err)
-		assert.Len(t, ancestors, 2)
-		assert.Equal(t, accesscontrol.GeneralFolderUID, ancestors[0].UID)
-		assert.Equal(t, parent.UID, ancestors[1].UID)
+		assertAncestorUIDs(t, store, f.UID, []string{folder.GeneralFolderUID, parent.UID})
+		assertChildrenUIDs(t, store, parent.UID, []string{f.UID})
 
-		ff, err := store.Get(context.Background(), &folder.GetFolderCommand{
+		ff, err := store.Get(context.Background(), &folder.GetFolderQuery{
 			UID: &f.UID,
 		})
 		assert.NoError(t, err)
@@ -131,37 +117,7 @@ func TestCreate(t *testing.T) {
 	})
 
 	t.Run("creating a nested folder with the maximum nested folder depth should fail", func(t *testing.T) {
-		ancestorUIDs := []string{accesscontrol.GeneralFolderUID}
-		for i := 0; i < folder.MaxNestedFolderDepth; i++ {
-			parentUID := ancestorUIDs[len(ancestorUIDs)-1]
-			title := fmt.Sprintf("folder-%d", i)
-			f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
-				Title: title,
-				// OrgID: orgID,
-				ParentUID: parentUID,
-			})
-			require.NoError(t, err)
-			t.Cleanup(func() {
-				err := store.Delete(context.Background(), f.UID, orgID)
-				require.NoError(t, err)
-			})
-
-			require.Equal(t, title, f.Title)
-			require.NotEmpty(t, f.ID)
-			require.NotEmpty(t, f.UID)
-
-			ancestorUIDs = append(ancestorUIDs, f.UID)
-
-			parents, err := store.GetParents(context.Background(), &folder.GetParentsCommand{
-				UID: f.UID,
-			})
-			require.NoError(t, err)
-			parentUIDs := make([]string, len(ancestorUIDs))
-			for _, p := range parents {
-				parentUIDs = append(parentUIDs, p.UID)
-			}
-			require.Equal(t, ancestorUIDs, parentUIDs)
-		}
+		ancestorUIDs := createSubTree(t, store, orgID, accesscontrol.GeneralFolderUID, folder.MaxNestedFolderDepth, "")
 
 		title := fmt.Sprintf("folder-%d", len(ancestorUIDs))
 		_, err := store.Create(context.Background(), &folder.CreateFolderCommand{
@@ -196,38 +152,7 @@ func TestDelete(t *testing.T) {
 		assert.Error(t, err)
 	})
 
-	ancestorUIDs := []string{folder.GeneralFolderUID}
-	for i := 0; i < folder.MaxNestedFolderDepth; i++ {
-		parentUID := ancestorUIDs[len(ancestorUIDs)-1]
-		title := fmt.Sprintf("folder-%d", i)
-		f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
-			Title: title,
-			// OrgID: orgID,
-			ParentUID: parentUID,
-		})
-		require.NoError(t, err)
-		t.Cleanup(func() {
-			err := store.Delete(context.Background(), f.UID, orgID)
-			require.NoError(t, err)
-		})
-
-		require.Equal(t, title, f.Title)
-		require.NotEmpty(t, f.ID)
-		require.NotEmpty(t, f.UID)
-
-		ancestorUIDs = append(ancestorUIDs, f.UID)
-
-		parents, err := store.GetParents(context.Background(), &folder.GetParentsCommand{
-			UID: f.UID,
-		})
-		require.NoError(t, err)
-		parentUIDs := make([]string, len(ancestorUIDs))
-		for _, p := range parents {
-			parentUIDs = append(parentUIDs, p.UID)
-		}
-		require.Equal(t, ancestorUIDs, parentUIDs)
-	}
-
+	ancestorUIDs := createSubTree(t, store, orgID, accesscontrol.GeneralFolderUID, folder.MaxNestedFolderDepth, "")
 	require.Len(t, ancestorUIDs, folder.MaxNestedFolderDepth)
 
 	t.Run("deleting folder with children should fail", func(t *testing.T) {
@@ -239,7 +164,7 @@ func TestDelete(t *testing.T) {
 		err = store.Delete(context.Background(), ancestorUIDs[len(ancestorUIDs)-1], orgID)
 		require.NoError(t, err)
 
-		children, err := store.GetChildren(context.Background(), &folder.GetTreeCommand{
+		children, err := store.GetChildren(context.Background(), &folder.GetTreeQuery{
 			UID:   ancestorUIDs[len(ancestorUIDs)-2],
 			Depth: folder.MaxNestedFolderDepth,
 		})
@@ -285,7 +210,7 @@ func TestUpdate(t *testing.T) {
 		})
 		require.NoError(t, err)
 
-		ff, err := store.Get(context.Background(), &folder.GetFolderCommand{
+		ff, err := store.Get(context.Background(), &folder.GetFolderQuery{
 			UID: &f.UID,
 		})
 		require.NoError(t, err)
@@ -308,7 +233,7 @@ func TestUpdate(t *testing.T) {
 		assert.Equal(t, newTitle, updated.Title)
 		assert.Equal(t, newDesc, updated.Description)
 
-		updated, err = store.Get(context.Background(), &folder.GetFolderCommand{
+		updated, err = store.Get(context.Background(), &folder.GetFolderQuery{
 			UID: &updated.UID,
 		})
 		require.NoError(t, err)
@@ -326,7 +251,7 @@ func TestUpdate(t *testing.T) {
 
 		assert.Equal(t, newUID, updated.UID)
 
-		updated, err = store.Get(context.Background(), &folder.GetFolderCommand{
+		updated, err = store.Get(context.Background(), &folder.GetFolderQuery{
 			UID: &updated.UID,
 		})
 		require.NoError(t, err)
@@ -335,7 +260,97 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
-func TestMove(t *testing.T) {}
+func TestMove(t *testing.T) {
+	db := sqlstore.InitTestDB(t)
+	store := ProvideStore(db, db.Cfg, *featuremgmt.WithFeatures())
+
+	// create org
+	orgService := orgimpl.ProvideService(db, db.Cfg)
+	orgID, err := orgService.GetOrCreate(context.Background(), "test-org")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err = orgService.Delete(context.Background(), &org.DeleteOrgCommand{ID: orgID})
+		require.NoError(t, err)
+	})
+
+	t.Run("moving unknown folder should fail", func(t *testing.T) {
+		_, err = store.Move(context.Background(), &folder.MoveFolderCommand{
+			UID: "unknown",
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("moving to unknown folder should fail", func(t *testing.T) {
+		title := "folder1"
+		f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
+			Title: title,
+			// OrgID: orgID,
+		})
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := store.Delete(context.Background(), f.UID, orgID)
+			require.NoError(t, err)
+		})
+
+		_, err = store.Move(context.Background(), &folder.MoveFolderCommand{
+			UID:          f.UID,
+			NewParentUID: "unknown",
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("moving to an existing folder under the same organisation should succeed", func(t *testing.T) {
+		subTree1 := createSubTree(t, store, orgID, folder.GeneralFolderUID, 2, "tree1-")
+		f1 := subTree1[1]
+		children := subTree1[2 : len(subTree1)-1]
+		subTree2 := createSubTree(t, store, orgID, folder.GeneralFolderUID, 2, "tree2-")
+		f2 := subTree2[1]
+
+		_, err = store.Move(context.Background(), &folder.MoveFolderCommand{
+			UID:          f1,
+			NewParentUID: f2,
+		})
+		require.NoError(t, err)
+
+		assertAncestorUIDs(t, store, f1, []string{folder.GeneralFolderUID, f2})
+		assertChildrenUIDs(t, store, f1, children)
+	})
+
+	t.Run("moving to an existing folder under the same organisation that would cause the maximum depth to exceed should fail", func(t *testing.T) {
+		subTree1 := createSubTree(t, store, orgID, folder.GeneralFolderUID, 2, "tree1-")
+		f1 := subTree1[1]
+		children := subTree1[2 : len(subTree1)-1]
+		subTree2 := createSubTree(t, store, orgID, folder.GeneralFolderUID, folder.MaxNestedFolderDepth-len(children), "tree2-")
+		f2 := subTree2[1]
+
+		_, err = store.Move(context.Background(), &folder.MoveFolderCommand{
+			UID:          f1,
+			NewParentUID: f2,
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("moving to folder under a different folder should fail", func(t *testing.T) {
+		// create another org
+		otherOrgID, err := orgService.GetOrCreate(context.Background(), "test-org-2")
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err = orgService.Delete(context.Background(), &org.DeleteOrgCommand{ID: otherOrgID})
+			require.NoError(t, err)
+		})
+
+		subTree1 := createSubTree(t, store, orgID, folder.GeneralFolderUID, 1, "")
+		f1 := subTree1[len(subTree1)-1]
+		subTree2 := createSubTree(t, store, otherOrgID, folder.GeneralFolderUID, 1, "")
+		f2 := subTree2[len(subTree2)-1]
+
+		_, err = store.Move(context.Background(), &folder.MoveFolderCommand{
+			UID:          f1,
+			NewParentUID: f2,
+		})
+		require.Error(t, err)
+	})
+}
 
 func TestGet(t *testing.T) {}
 
@@ -346,3 +361,64 @@ func TestGetParents(t *testing.T) {}
 func TestGetChildren(t *testing.T) {}
 
 func TestGetDescendents(t *testing.T) {}
+
+func createSubTree(t *testing.T, store *sqlStore, orgID int64, parentUID string, depth int, prefix string) []string {
+	t.Helper()
+
+	ancestorUIDs := []string{parentUID}
+	for i := 0; i < int(depth); i++ {
+		parentUID := ancestorUIDs[len(ancestorUIDs)-1]
+		title := fmt.Sprintf("%sfolder-%d", prefix, i)
+		f, err := store.Create(context.Background(), &folder.CreateFolderCommand{
+			Title: title,
+			// OrgID: orgID,
+			ParentUID: parentUID,
+		})
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := store.Delete(context.Background(), f.UID, orgID)
+			require.NoError(t, err)
+		})
+
+		require.Equal(t, title, f.Title)
+		require.NotEmpty(t, f.ID)
+		require.NotEmpty(t, f.UID)
+
+		ancestorUIDs = append(ancestorUIDs, f.UID)
+
+		parents, err := store.GetParents(context.Background(), &folder.GetParentsQuery{
+			UID: f.UID,
+		})
+		require.NoError(t, err)
+		parentUIDs := make([]string, len(ancestorUIDs))
+		for _, p := range parents {
+			parentUIDs = append(parentUIDs, p.UID)
+		}
+		require.Equal(t, ancestorUIDs, parentUIDs)
+	}
+	return ancestorUIDs
+}
+
+func assertAncestorUIDs(t *testing.T, store *sqlStore, UID string, expected []string) {
+	ancestors, err := store.GetParents(context.Background(), &folder.GetParentsQuery{
+		UID: UID,
+	})
+	require.NoError(t, err)
+	actualAncestorsUIDs := make([]string, 0)
+	for _, f := range ancestors {
+		actualAncestorsUIDs = append(actualAncestorsUIDs, f.UID)
+	}
+	assert.Equal(t, expected, actualAncestorsUIDs)
+}
+
+func assertChildrenUIDs(t *testing.T, store *sqlStore, UID string, expected []string) {
+	ancestors, err := store.GetChildren(context.Background(), &folder.GetTreeQuery{
+		UID: UID,
+	})
+	require.NoError(t, err)
+	actualChildrenUIDs := make([]string, 0)
+	for _, f := range ancestors {
+		actualChildrenUIDs = append(actualChildrenUIDs, f.UID)
+	}
+	assert.Equal(t, expected, actualChildrenUIDs)
+}
