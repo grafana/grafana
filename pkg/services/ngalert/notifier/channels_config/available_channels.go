1ab0af1eb288fa73ee7307039001807be64ac5f3
Alex Moreno <alexander.moreno@grafana.com>
2022-10-27 16:16:36 +0200

    Alerting: Add custom title to DingDing contact point (#57498)
    * Add custom title to DingDing contact point
    
    * Update pkg/services/ngalert/notifier/channels_config/available_channels.go
    
    Co-authored-by: Santiago <santiagohernandez.1997@gmail.com>
    
    * Update pkg/services/ngalert/notifier/channels/dingding.go
    
    Co-authored-by: Santiago <santiagohernandez.1997@gmail.com>
    
    * Add error checking before URL templating
    
    * Remove comment
    
    * Use simplejson
    
    Co-authored-by: Santiago <santiagohernandez.1997@gmail.com>

--- pkg/services/ngalert/notifier/channels_config/available_channels.go
+++ pkg/services/ngalert/notifier/channels_config/available_channels.go
@@ -132,9 +132,18 @@ func GetAvailableNotifiers() []*NotifierPlugin {
 						},
 					},
 				},
+				{ // New in 9.3.
+					Label:        "Title",
+					Element:      ElementTypeInput,
+					InputType:    InputTypeText,
+					Description:  "Templated title of the message",
+					Placeholder:  channels.DefaultMessageTitleEmbed,
+					PropertyName: "title",
+				},
 				{ // New in 8.0.
 					Label:        "Message",
 					Element:      ElementTypeTextArea,
+					Description:  "Custom DingDing message. You can use template variables.",
 					Placeholder:  channels.DefaultMessageEmbed,
 					PropertyName: "message",
 				},
