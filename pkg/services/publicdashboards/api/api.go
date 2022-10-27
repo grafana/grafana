7340430ad5c5d12a6664c2ebbd058d46cef51cec
Juan Cabanas <juan.cabanas@grafana.com>
2022-10-27 16:27:19 -0300

    comment modified

--- pkg/services/publicdashboards/api/api.go
+++ pkg/services/publicdashboards/api/api.go
@@ -134,7 +134,7 @@ func (api *Api) ListPublicDashboards(c *models.ReqContext) response.Response {
 }
 
 // Delete a public dashboard
-// DELETE /api/dashboards/:uid/public-dashboards/:publicDashboardUid
+// DELETE /api/dashboards/uid/:uid/public-dashboards/:publicDashboardUid
 func (api *Api) DeletePublicDashboard(c *models.ReqContext) response.Response {
 	err := api.PublicDashboardService.Delete(c.Req.Context(), c.SignedInUser.OrgID, web.Params(c.Req)[":uid"], web.Params(c.Req)[":publicDashboardUid"])
 	if err != nil {
