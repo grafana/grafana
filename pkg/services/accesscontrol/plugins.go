a5241e76498886b78f6c740c02f84eee96f71960
gamab <gabi.mabs@gmail.com>
2022-10-27 18:17:54 +0200

    Add missing files
    Co-authored-by: Jguer <joao.guerreiro@grafana.com>

---
+++ pkg/services/accesscontrol/plugins.go
@@ -0,0 +1,36 @@
+package accesscontrol
+
+import (
+	"strings"
+)
+
+type PluginJSON struct {
+	ID    string             `json:"id"`
+	Roles []RoleRegistration `json:"roles"`
+}
+
+// ValidatePluginPermissions errors when a permission does not match expected pattern for plugins
+func ValidatePluginPermissions(pluginID string, permissions []Permission) error {
+	for i := range permissions {
+		if !strings.HasPrefix(permissions[i].Action, AppPluginRolePrefix) &&
+			!strings.HasPrefix(permissions[i].Action, pluginID+":") &&
+			!strings.HasPrefix(permissions[i].Action, pluginID+".") {
+			return &ErrorActionPrefixMissing{Action: permissions[i].Action, Prefixes: []string{AppPluginRolePrefix, pluginID + ":"}}
+		}
+	}
+
+	return nil
+}
+
+// ValidatePluginRole errors when a plugin role does not match expected pattern
+// or doesn't have permissions matching the expected pattern.
+func ValidatePluginRole(pluginID string, role RoleDTO) error {
+	if pluginID == "" {
+		return ErrPluginIDRequired
+	}
+	if !strings.HasPrefix(role.Name, AppPluginRolePrefix+pluginID+":") {
+		return &ErrorRolePrefixMissing{Role: role.Name, Prefixes: []string{AppPluginRolePrefix + pluginID + ":"}}
+	}
+
+	return ValidatePluginPermissions(pluginID, role.Permissions)
+}
