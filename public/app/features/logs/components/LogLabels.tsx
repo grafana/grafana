6fb2899af98209e9ccb58cb460f4e58c9c4171fc
Gareth Dawson <gwdawson.work@gmail.com>
2022-10-28 11:11:09 +0100

    replace hardcoded values in LogLabels.tsx

--- public/app/features/logs/components/LogLabels.tsx
+++ public/app/features/logs/components/LogLabels.tsx
@@ -53,10 +53,10 @@ const getStyles = (theme: GrafanaTheme2) => {
     logsLabel: css`
       label: logs-label;
       display: flex;
-      padding: 0 2px;
+      padding: ${theme.spacing(0, 0.25)};
       background-color: ${theme.colors.background.secondary};
       border-radius: ${theme.shape.borderRadius(1)};
-      margin: 1px 4px 0 0;
+      margin: ${theme.spacing(0.125, 0.5, 0, 0)};
       text-overflow: ellipsis;
       white-space: nowrap;
       overflow: hidden;
