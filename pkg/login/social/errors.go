2bd16474ddec15fa9a503182861d1bb0a410f594
Karl Persson <kalle.persson@grafana.com>
2022-10-28 11:18:31 +0200

    Oauth: change error type to a struct that unwraps into a social error

--- pkg/login/social/errors.go
+++ pkg/login/social/errors.go
@@ -1,9 +1,26 @@
 package social
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
 
 var (
 	ErrIDTokenNotFound  = errors.New("id_token not found")
-	ErrInvalidBasicRole = errors.New("user does not have a valid basic role")
 	ErrEmailNotFound    = errors.New("error getting user info: no email found in access token")
 )
+
+type InvalidBasicRoleError struct {
+	assignedRole string
+}
+
+func (e *InvalidBasicRoleError) Error() string {
+	return fmt.Sprintf("integration requires a valid org role assigned in idP. Assigned role: %s", e.assignedRole)
+}
+
+func (e *InvalidBasicRoleError) Unwrap() error {
+	return Error{cases.Title(language.Und).String(e.Error())}
+}
