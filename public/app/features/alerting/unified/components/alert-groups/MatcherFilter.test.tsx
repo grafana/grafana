48a42ae6de7e145ef82101080cf597fb35b5bd55
Gilles De Mey <gilles.de.mey@gmail.com>
2022-10-31 17:59:25 +0100

    test: fix tests

--- public/app/features/alerting/unified/components/alert-groups/MatcherFilter.test.tsx
+++ public/app/features/alerting/unified/components/alert-groups/MatcherFilter.test.tsx
@@ -12,9 +12,14 @@ import { MatcherFilter } from './MatcherFilter';
 jest.mock('@grafana/runtime');
 
 describe('Analytics', () => {
-  it('Sends log info when filtering alert instances by label', async () => {
-    lodash.debounce = jest.fn().mockImplementation((fn) => fn);
+  beforeEach(() => {
+    lodash.debounce = jest.fn().mockImplementation((fn) => {
+      fn.cancel = () => {};
+      return fn;
+    });
+  });
 
+  it('Sends log info when filtering alert instances by label', async () => {
     render(<MatcherFilter onFilterChange={jest.fn()} />);
 
     const searchInput = screen.getByTestId('search-query-input');
@@ -24,7 +29,6 @@ describe('Analytics', () => {
   });
 
   it('should call onChange handler', async () => {
-    lodash.debounce = jest.fn().mockImplementation((fn) => fn);
     const onFilterMock = jest.fn();
 
     render(<MatcherFilter defaultQueryString="foo" onFilterChange={onFilterMock} />);
