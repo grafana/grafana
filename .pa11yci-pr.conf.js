33c7791bbd22a782494b57219d7430d5e0e0493f
joshhunt <josh@trtr.co>
2022-10-31 11:22:22 +0000

    bump pa11y thresholds

--- .pa11yci-pr.conf.js
+++ .pa11yci-pr.conf.js
@@ -101,7 +101,7 @@ var config = {
       rootElement: '.main-view',
       // the unified alerting promotion alert's content contrast is too low
       // see https://github.com/grafana/grafana/pull/41829
-      threshold: 5,
+      threshold: 6,
     },
     {
       url: '${HOST}/datasources',
