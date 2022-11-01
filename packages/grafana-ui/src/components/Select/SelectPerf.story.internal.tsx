aa97e8ff178e66d1d401db8d3523726b0ffeb566
joshhunt <josh@trtr.co>
2022-11-01 11:35:16 +0000

    user essentials mob! :trident:

--- packages/grafana-ui/src/components/Select/SelectPerf.story.internal.tsx
+++ packages/grafana-ui/src/components/Select/SelectPerf.story.internal.tsx
@@ -1,37 +1,71 @@
 import { Meta } from '@storybook/react';
-import React from 'react';
-import { createFilter } from 'react-select';
+import React, { useMemo } from 'react';
+import ReactSelect, { createFilter } from 'react-select';
 
 import { SelectableValue } from '@grafana/data';
 
 import { withCenteredStory, withHorizontallyCenteredStory } from '../../utils/storybook/withCenteredStory';
+import { Label } from '../Forms/Label';
 
 import { Select, VirtualizedSelect } from './Select';
 
 const meta: Meta = {
   title: 'Forms/Select (Perf)',
   decorators: [withCenteredStory, withHorizontallyCenteredStory],
+  argTypes: {
+    numberOfOptions: {
+      defaultValue: 10_000,
+      control: { type: 'number' },
+    },
+  },
 };
 
 export default meta;
 
-const lotsOfItems: SelectableValue[] = [];
-const ALPHABET = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-
-for (let index = 0; index < 10000; index++) {
-  const letter = ALPHABET[index % ALPHABET.length];
-  lotsOfItems.push({ label: letter + '-' + index, value: letter + '-' + index });
+const _customFilter = createFilter({ ignoreAccents: false });
+function customFilter(opt: SelectableValue, searchQuery: string) {
+  return _customFilter(
+    {
+      label: opt.label ?? '',
+      value: opt.value ?? '',
+      data: {},
+    },
+    searchQuery
+  );
 }
 
-export function SelectThousands() {
+export function PerformanceScenarios({ numberOfOptions }: { numberOfOptions: number }) {
+  const options = useMemo(() => {
+    const opts: SelectableValue[] = [];
+    const ALPHABET = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+
+    for (let index = 0; index < numberOfOptions; index++) {
+      const letter = ALPHABET[index % ALPHABET.length];
+      opts.push({ label: letter + '-' + index, value: letter + '-' + index });
+    }
+
+    return opts;
+  }, [numberOfOptions]);
+
   return (
     <div>
-      Virtual:
-      <VirtualizedSelect options={lotsOfItems} onChange={() => {}} />
-      Virtual with ignoreAccents false:
-      <VirtualizedSelect filterOption={createFilter({ ignoreAccents: false })} options={lotsOfItems} onChange={() => {}} />
-      Normal:
-      <Select options={lotsOfItems} onChange={() => {}} />
+      <Label>Virtual:</Label>
+      <VirtualizedSelect options={options} onChange={() => {}} />
+      <br />
+
+      <Label>Virtual with ignoreAccents false:</Label>
+      <VirtualizedSelect filterOption={customFilter} options={options} onChange={() => {}} />
+      <br />
+
+      <Label>Normal:</Label>
+      <Select options={options} onChange={() => {}} />
+      <br />
+
+      <Label>Standard react-select</Label>
+      <ReactSelect options={options} onChange={() => {}} />
+      <br />
+
+      <p>Rendered with {options.length.toLocaleString()} options</p>
     </div>
   );
 }
