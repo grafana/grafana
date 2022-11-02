4a1728a6c65b6330045481fbe2799e71d4506519
Konrad Lalik <konrad.lalik@grafana.com>
2022-11-02 16:58:44 +0100

    Improve dashboard selection list

--- public/app/features/alerting/unified/components/rule-editor/AnnotationsField.tsx
+++ public/app/features/alerting/unified/components/rule-editor/AnnotationsField.tsx
@@ -1,30 +1,18 @@
 import { css, cx } from '@emotion/css';
-import React, { CSSProperties, useCallback, useState } from 'react';
+import produce from 'immer';
+import React, { useCallback, useState } from 'react';
 import { useFieldArray, useFormContext } from 'react-hook-form';
-import { useDebounce, useToggle } from 'react-use';
-import { FixedSizeList } from 'react-window';
+import { useToggle } from 'react-use';
 
-import { GrafanaTheme, GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme } from '@grafana/data';
 import { Stack } from '@grafana/experimental';
-import {
-  Button,
-  Field,
-  FilterInput,
-  Input,
-  InputControl,
-  Label,
-  LoadingPlaceholder,
-  Modal,
-  TextArea,
-  useStyles,
-  useStyles2,
-} from '@grafana/ui';
+import { Button, Field, Input, InputControl, Label, Modal, TextArea, useStyles } from '@grafana/ui';
 
-import { dashboardApi } from '../../api/alertingApi';
 import { RuleFormValues } from '../../types/rule-form';
 import { Annotation } from '../../utils/constants';
 
 import { AnnotationKeyInput } from './AnnotationKeyInput';
+import { DashboardPicker } from './DashboardPicker';
 
 const AnnotationsField = () => {
   const styles = useStyles(getStyles);
@@ -37,6 +25,7 @@ const AnnotationsField = () => {
     register,
     watch,
     formState: { errors },
+    setValue,
   } = useFormContext<RuleFormValues>();
   const annotations = watch('annotations');
 
@@ -48,32 +37,29 @@ const AnnotationsField = () => {
   const { fields, append, remove } = useFieldArray({ control, name: 'annotations' });
 
   const onPanelChange = () => {
-    // if (!selectedDashboard || !selectedPanel) {
-    //   return;
-    // }
-    //
-    // const dashboardFieldIdx = fields.findIndex((f) => f.key === Annotation.dashboardUID);
-    // const panelFieldIdx = fields.findIndex((f) => f.key === Annotation.panelID);
-    //
-    // remove([dashboardFieldIdx, panelFieldIdx]);
-    // if (dashboardField) {
-    //   dashboardField.value = selectedDashboard;
-    // } else {
-    // if (dashboardFieldIdx === -1 || panelFieldIdx === -1) {
-    // append([
-    //   { key: Annotation.dashboardUID, value: selectedDashboard },
-    //   { key: Annotation.panelID, value: selectedPanel.toString(10) },
-    // ]);
-    // } else {
-    //   insert(dashboardFieldIdx, { key: Annotation.dashboardUID, value: selectedDashboard });
-    //   insert(panelFieldIdx, { key: Annotation.panelID, value: selectedPanel.toString(10) });
-    // }
-    // }
-    // if (panelField) {
-    //   panelField.value = selectedPanel.toString(10);
-    // } else {
-    //   append({ key: Annotation.panelID, value: selectedPanel.toString(10) });
-    // }
+    if (!selectedDashboard || !selectedPanel) {
+      return;
+    }
+
+    const updatedAnnotations = produce(annotations, (draft) => {
+      const dashboardAnn = draft.find((a) => a.key === Annotation.dashboardUID);
+      const panelAnn = draft.find((a) => a.key === Annotation.panelID);
+
+      if (dashboardAnn) {
+        dashboardAnn.value = selectedDashboard;
+      } else {
+        draft.push({ key: Annotation.dashboardUID, value: selectedDashboard });
+      }
+
+      if (panelAnn) {
+        panelAnn.value = selectedPanel.toString(10);
+      } else {
+        draft.push({ key: Annotation.panelID, value: selectedPanel.toString(10) });
+      }
+    });
+
+    setValue('annotations', updatedAnnotations);
+    setShowPanelSelector(false);
   };
 
   return (
@@ -131,20 +117,22 @@ const AnnotationsField = () => {
             </div>
           );
         })}
-        <Button
-          className={styles.addAnnotationsButton}
-          icon="plus-circle"
-          type="button"
-          variant="secondary"
-          onClick={() => {
-            append({ key: '', value: '' });
-          }}
-        >
-          Add info
-        </Button>
-        <Button type="button" variant="secondary" onClick={setShowPanelSelector}>
-          Set panel and dashboard
-        </Button>
+        <Stack direction="row" gap={1}>
+          <Button
+            className={styles.addAnnotationsButton}
+            icon="plus-circle"
+            type="button"
+            variant="secondary"
+            onClick={() => {
+              append({ key: '', value: '' });
+            }}
+          >
+            Add new annotation
+          </Button>
+          <Button type="button" variant="secondary" icon="dashboard" onClick={setShowPanelSelector}>
+            Set panel and dashboard
+          </Button>
+        </Stack>
         <Modal
           title="Select dashboard and panel"
           closeOnEscape
@@ -166,7 +154,7 @@ const AnnotationsField = () => {
             >
               Confirm
             </Button>
-            <Button type="button" variant="secondary">
+            <Button type="button" variant="secondary" onClick={() => setShowPanelSelector(false)}>
               Cancel
             </Button>
           </Modal.ButtonRow>
@@ -175,154 +163,6 @@ const AnnotationsField = () => {
     </>
   );
 };
-
-interface PanelDTO {
-  id: number;
-  title?: string;
-}
-
-function panelSort(a: PanelDTO, b: PanelDTO) {
-  if (a.title && b.title) {
-    return a.title.localeCompare(b.title);
-  }
-  if (a.title && !b.title) {
-    return 1;
-  } else if (!a.title && b.title) {
-    return -1;
-  }
-
-  return 0;
-}
-
-interface DashboardPickerProps {
-  dashboardUid?: string;
-  panelId?: number;
-  onDashboardChange: (uid: string | undefined) => void;
-  onPanelChange: (id: number | undefined) => void;
-}
-
-const DashboardPicker = ({ dashboardUid, panelId, onDashboardChange, onPanelChange }: DashboardPickerProps) => {
-  const styles = useStyles2(getPickerStyles);
-
-  const [dashboardFilter, setDashboardFilter] = useState('');
-  const [debouncedDashboardFilter, setDebouncedDashboardFilter] = useState('');
-
-  const [panelFilter, setPanelFilter] = useState('');
-
-  const { useSearchQuery, useDashboardQuery } = dashboardApi;
-
-  const { currentData: filteredDashboards = [], isFetching: isDashSearchFetching } = useSearchQuery({
-    query: debouncedDashboardFilter,
-  });
-  const { currentData: dashboardResult, isFetching: isDashboardFetching } = useDashboardQuery(
-    { uid: dashboardUid ?? '' },
-    { skip: !dashboardUid }
-  );
-
-  useDebounce(
-    () => {
-      setDebouncedDashboardFilter(dashboardFilter);
-    },
-    500,
-    [dashboardFilter]
-  );
-
-  const handleDashboardChange = (dashboardUid: string) => {
-    onDashboardChange(dashboardUid);
-    onPanelChange(undefined);
-  };
-
-  const filteredPanels =
-    dashboardResult?.dashboard?.panels
-      ?.filter((panel): panel is PanelDTO => panel.title?.includes(panelFilter))
-      .sort(panelSort) ?? [];
-
-  const DashboardRow = ({ index, style }: { index: number; style: CSSProperties }) => {
-    const dashboard = filteredDashboards[index];
-    const isSelected = dashboardUid === dashboard.uid;
-
-    return (
-      <div
-        title={dashboard.title}
-        style={style}
-        className={cx(styles.row, { [styles.rowOdd]: index % 2 === 1, [styles.rowSelected]: isSelected })}
-        onClick={() => handleDashboardChange(dashboard.uid)}
-      >
-        {dashboard.title}
-      </div>
-    );
-  };
-
-  const PanelRow = ({ index, style }: { index: number; style: CSSProperties }) => {
-    const panel = filteredPanels[index];
-    const isSelected = panelId === panel.id;
-
-    return (
-      <div
-        style={style}
-        className={cx(styles.row, { [styles.rowOdd]: index % 2 === 1, [styles.rowSelected]: isSelected })}
-        onClick={() => onPanelChange(panel.id)}
-      >
-        {panel.title || '<No title>'}
-      </div>
-    );
-  };
-
-  return (
-    <Stack direction="row" gap={2}>
-      <div style={{ flex: 1 }}>
-        <FilterInput
-          value={dashboardFilter}
-          onChange={setDashboardFilter}
-          title="Search dashboard"
-          placeholder="Search dashboard"
-          autoFocus
-        />
-        {isDashSearchFetching && (
-          <LoadingPlaceholder text="Loading dashboards..." className={styles.loadingPlaceholder} />
-        )}
-        <FixedSizeList itemSize={32} height={550} itemCount={filteredDashboards.length} width="100%">
-          {DashboardRow}
-        </FixedSizeList>
-      </div>
-      <div style={{ flex: 1 }}>
-        <FilterInput value={panelFilter} onChange={setPanelFilter} title="Search panel" placeholder="Search panel" />
-        {!isDashboardFetching && (
-          <FixedSizeList itemSize={32} height={550} itemCount={filteredPanels.length} width="100%">
-            {PanelRow}
-          </FixedSizeList>
-        )}
-        {!dashboardUid && !isDashboardFetching && <div>Select a dashboard to get a list of available panels</div>}
-        {isDashboardFetching && (
-          <LoadingPlaceholder text="Loading dashboard..." className={styles.loadingPlaceholder} />
-        )}
-      </div>
-    </Stack>
-  );
-};
-
-const getPickerStyles = (theme: GrafanaTheme2) => ({
-  row: css`
-    padding: ${theme.spacing(0.5)};
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    cursor: pointer;
-    border: 2px solid transparent;
-  `,
-  rowSelected: css`
-    border-color: ${theme.colors.border.strong};
-  `,
-  rowOdd: css`
-    background-color: ${theme.colors.background.secondary};
-  `,
-  loadingPlaceholder: css`
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `,
-});
 
 const getStyles = (theme: GrafanaTheme) => ({
   annotationValueInput: css`
---
+++ public/app/features/alerting/unified/components/rule-editor/DashboardPicker.tsx
@@ -0,0 +1,190 @@
+import { css, cx } from '@emotion/css';
+import React, { CSSProperties, useState } from 'react';
+import { useDebounce } from 'react-use';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { FixedSizeList } from 'react-window';
+
+import { GrafanaTheme2 } from '@grafana/data/src';
+import { FilterInput, LoadingPlaceholder, useStyles2, Icon } from '@grafana/ui';
+
+import { dashboardApi } from '../../api/alertingApi';
+
+interface PanelDTO {
+  id: number;
+  title?: string;
+}
+
+function panelSort(a: PanelDTO, b: PanelDTO) {
+  if (a.title && b.title) {
+    return a.title.localeCompare(b.title);
+  }
+  if (a.title && !b.title) {
+    return 1;
+  } else if (!a.title && b.title) {
+    return -1;
+  }
+
+  return 0;
+}
+
+interface DashboardPickerProps {
+  dashboardUid?: string;
+  panelId?: number;
+  onDashboardChange: (uid: string | undefined) => void;
+  onPanelChange: (id: number | undefined) => void;
+}
+
+export const DashboardPicker = ({ dashboardUid, panelId, onDashboardChange, onPanelChange }: DashboardPickerProps) => {
+  const styles = useStyles2(getPickerStyles);
+
+  const [dashboardFilter, setDashboardFilter] = useState('');
+  const [debouncedDashboardFilter, setDebouncedDashboardFilter] = useState('');
+
+  const [panelFilter, setPanelFilter] = useState('');
+
+  const { useSearchQuery, useDashboardQuery } = dashboardApi;
+
+  const { currentData: filteredDashboards = [], isFetching: isDashSearchFetching } = useSearchQuery({
+    query: debouncedDashboardFilter,
+  });
+  const { currentData: dashboardResult, isFetching: isDashboardFetching } = useDashboardQuery(
+    { uid: dashboardUid ?? '' },
+    { skip: !dashboardUid }
+  );
+
+  useDebounce(
+    () => {
+      setDebouncedDashboardFilter(dashboardFilter);
+    },
+    500,
+    [dashboardFilter]
+  );
+
+  const handleDashboardChange = (dashboardUid: string) => {
+    onDashboardChange(dashboardUid);
+    onPanelChange(undefined);
+  };
+
+  const filteredPanels =
+    dashboardResult?.dashboard?.panels
+      ?.filter((panel): panel is PanelDTO => panel.title?.includes(panelFilter))
+      .sort(panelSort) ?? [];
+
+  const DashboardRow = ({ index, style }: { index: number; style: CSSProperties }) => {
+    const dashboard = filteredDashboards[index];
+    const isSelected = dashboardUid === dashboard.uid;
+
+    return (
+      <div
+        title={dashboard.title}
+        style={style}
+        className={cx(styles.row, { [styles.rowOdd]: index % 2 === 1, [styles.rowSelected]: isSelected })}
+        onClick={() => handleDashboardChange(dashboard.uid)}
+      >
+        <div className={styles.dashboardTitle}>{dashboard.title}</div>
+        <div className={styles.dashboardFolder}>
+          <Icon name="folder" /> {dashboard.folderTitle}
+        </div>
+      </div>
+    );
+  };
+
+  const PanelRow = ({ index, style }: { index: number; style: CSSProperties }) => {
+    const panel = filteredPanels[index];
+    const isSelected = panelId === panel.id;
+
+    return (
+      <div
+        style={style}
+        className={cx(styles.row, { [styles.rowOdd]: index % 2 === 1, [styles.rowSelected]: isSelected })}
+        onClick={() => onPanelChange(panel.id)}
+      >
+        {panel.title || '<No title>'}
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <FilterInput
+        value={dashboardFilter}
+        onChange={setDashboardFilter}
+        title="Search dashboard"
+        placeholder="Search dashboard"
+        autoFocus
+      />
+      <FilterInput value={panelFilter} onChange={setPanelFilter} title="Search panel" placeholder="Search panel" />
+
+      {isDashSearchFetching && (
+        <LoadingPlaceholder text="Loading dashboards..." className={styles.loadingPlaceholder} />
+      )}
+      <div style={{ flex: '1 1 auto' }}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <FixedSizeList itemSize={50} height={height} width={width} itemCount={filteredDashboards.length}>
+              {DashboardRow}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
+      </div>
+
+      {!isDashboardFetching && (
+        <div style={{ flex: '1 1 auto' }}>
+          <AutoSizer>
+            {({ width, height }) => (
+              <FixedSizeList itemSize={32} height={height} width={width} itemCount={filteredPanels.length}>
+                {PanelRow}
+              </FixedSizeList>
+            )}
+          </AutoSizer>
+        </div>
+      )}
+      {!dashboardUid && !isDashboardFetching && <div>Select a dashboard to get a list of available panels</div>}
+      {isDashboardFetching && <LoadingPlaceholder text="Loading dashboard..." className={styles.loadingPlaceholder} />}
+    </div>
+  );
+};
+
+const getPickerStyles = (theme: GrafanaTheme2) => ({
+  container: css`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: min-content auto;
+    gap: ${theme.spacing(2)};
+    height: 500px;
+  `,
+  dashboardTitle: css`
+    height: 22px;
+    font-weight: ${theme.typography.fontWeightBold};
+  `,
+  dashboardFolder: css`
+    height: 20px;
+    font-size: ${theme.typography.bodySmall.fontSize};
+    color: ${theme.colors.text.secondary};
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    column-gap: ${theme.spacing(1)};
+    align-items: center;
+  `,
+  row: css`
+    padding: ${theme.spacing(0.5)};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    cursor: pointer;
+    border: 2px solid transparent;
+  `,
+  rowSelected: css`
+    border-color: ${theme.colors.border.strong};
+  `,
+  rowOdd: css`
+    background-color: ${theme.colors.background.secondary};
+  `,
+  loadingPlaceholder: css`
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+});
