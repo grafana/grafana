27e84da8c29a217bb87bf07fd83ee33094e1850b
Gareth Dawson <gwdawson.work@gmail.com>
2022-11-03 16:23:08 +0000

    make query case-insensitive when using line filter

--- public/app/plugins/datasource/loki/querybuilder/operationUtils.ts
+++ public/app/plugins/datasource/loki/querybuilder/operationUtils.ts
@@ -257,6 +257,10 @@ export function addNestedQueryHandler(def: QueryBuilderOperationDef, query: Loki
 
 export function getLineFilterRenderer(operation: string) {
   return function lineFilterRenderer(model: QueryBuilderOperation, def: QueryBuilderOperationDef, innerExpr: string) {
+    console.log('M', model);
+    if (model.id === LokiOperationId.LineContainsInsensitive) {
+      return `${innerExpr} ${operation} \`(?i)${model.params[0]}\``;
+    }
     return `${innerExpr} ${operation} \`${model.params[0]}\``;
   };
 }
