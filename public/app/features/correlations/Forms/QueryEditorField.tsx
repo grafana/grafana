b4d8f27fc157809549ec4f1f1773b3d51a8f5ca0
Laura Benz <laura.benz@grafana.com>
2022-11-03 15:57:30 +0100

    temp: intermediate result

--- public/app/features/correlations/Forms/QueryEditorField.tsx
+++ public/app/features/correlations/Forms/QueryEditorField.tsx
@@ -3,7 +3,7 @@ import React, { useMemo, useState } from 'react';
 import { Controller } from 'react-hook-form';
 import { useAsync } from 'react-use';
 
-import { getDataSourceSrv } from '@grafana/runtime';
+import { createQueryRunner, getDataSourceSrv } from '@grafana/runtime';
 import { Field, LoadingPlaceholder, Alert, Button, HorizontalGroup, Icon, useTheme2 } from '@grafana/ui';
 
 interface Props {
@@ -40,8 +40,23 @@ export const QueryEditorField = ({ dsUid, invalid, error, name }: Props) => {
   }, [dsUid]);
   const QueryEditor = datasource?.components?.QueryEditor;
 
-  const handleValidation = () => {
+  const handleValidation = (value) => {
+    // TODO: TS7006: Parameter 'value' implicitly has an 'any' type.
+    // TODO: value is not the right thing to use as it doesn't provide the right data structure for queries below, see packages/grafana-data/src/types/queryRunner.ts
     // trigger query
+    const runner = createQueryRunner();
+    if (datasource) {
+      runner.run({
+        datasource: datasource,
+        queries: value /*, timezone: TimeZoneUtc, timeRange, maxDataPoints: 100, minInterval: null*/,
+      });
+      // TODO:TS2345: Argument of type '{ datasource: DataSourceApi<DataQuery, DataSourceJsonData, {}>; queries: any; }'
+      //  is not assignable to parameter of type 'QueryRunnerOptions'.
+      //  Type '{ datasource: DataSourceApi<DataQuery, DataSourceJsonData, {}>; queries: any; }' is missing the following
+      //  properties from type 'QueryRunnerOptions': timezone, timeRange, maxDataPoints, minInterval
+    }
+    // TODO: Check whether value works for queries?
+    // runner.get() // maybe this as well => .subscribe()
     // filter result as we only need to know whether it was successful or not
     // if it was successful change state for isValidQuery to true
     setIsValidQuery(true);
@@ -74,26 +89,29 @@ export const QueryEditorField = ({ dsUid, invalid, error, name }: Props) => {
           if (!QueryEditor) {
             return <Alert title="Data source does not export a query editor."></Alert>;
           }
-
+          // TODO: Check whether datasource.type === 'loki' || 'prometheus' is necessary
           return (
             <>
               <QueryEditor
-                onRunQuery={datasource.type === 'loki' || 'prometheus' ? handleValidation : () => {}}
+                onRunQuery={datasource.type === 'loki' || 'prometheus' ? () => handleValidation(value) : () => {}}
                 onChange={onChange}
                 datasource={datasource}
                 query={value}
               />
               <HorizontalGroup justify="flex-end">
-                {isValidQuery ? (
-                  <div className={styles.valid}>
-                    <Icon name="check" /> This query is valid.
-                  </div>
-                ) : (
-                  <div className={styles.error}>
-                    <Icon name="exclamation-triangle" /> This query is not valid.
-                  </div>
-                )}
-                <Button variant="primary" icon={'check'} type="button" onClick={handleValidation}>
+                {
+                  // TODO: No need to show anything until button is clicked. Introduce another state or use something that's already there
+                  isValidQuery ? (
+                    <div className={styles.valid}>
+                      <Icon name="check" /> This query is valid.
+                    </div>
+                  ) : (
+                    <div className={styles.error}>
+                      <Icon name="exclamation-triangle" /> This query is not valid.
+                    </div>
+                  )
+                }
+                <Button variant="primary" icon={'check'} type="button" onClick={() => handleValidation(value)}>
                   Validate query
                 </Button>
               </HorizontalGroup>
