e6b088fbf5ee4de2f9ec454e568bf970d85ed6dd
Giordano Ricci <me@giordanoricci.com>
2022-11-03 09:55:02 +0000

    Explore: Fix shared crosshair for logs, logsvolume and graph panels (#57892)
    * Explore: enable shared corsshair for logs, logsvolume & graph panel
    
    * avoid recreating a scoped bus on every render

--- public/app/features/explore/Explore.test.tsx
+++ public/app/features/explore/Explore.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { AutoSizerProps } from 'react-virtualized-auto-sizer';
 
-import { DataSourceApi, LoadingState, CoreApp, createTheme } from '@grafana/data';
+import { DataSourceApi, LoadingState, CoreApp, createTheme, EventBusSrv } from '@grafana/data';
 import { configureStore } from 'app/store/configureStore';
 import { ExploreId } from 'app/types/explore';
 
@@ -86,6 +86,7 @@ const dummyProps: Props = {
   splitted: false,
   changeGraphStyle: () => {},
   graphStyle: 'lines',
+  eventBus: new EventBusSrv(),
 };
 
 jest.mock('@grafana/runtime/src/services/dataSourceSrv', () => {
