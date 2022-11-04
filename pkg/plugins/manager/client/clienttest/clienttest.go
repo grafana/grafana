f6915cb1690d2edb93d1ec0780cc8b404a4b6d73
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2022-11-04 15:56:06 +0100

    adds decorator tests

--- pkg/plugins/manager/client/clienttest/clienttest.go
+++ pkg/plugins/manager/client/clienttest/clienttest.go
@@ -10,108 +10,108 @@ import (
 
 type TestClient struct {
 	plugins.Client
-	queryDataFunc    backend.QueryDataHandlerFunc
-	callResourceFunc backend.CallResourceHandlerFunc
-	checkHealthFunc  backend.CheckHealthHandlerFunc
-}
-
-func NewClient() *TestClient {
-	return &TestClient{}
+	QueryDataFunc    backend.QueryDataHandlerFunc
+	CallResourceFunc backend.CallResourceHandlerFunc
+	CheckHealthFunc  backend.CheckHealthHandlerFunc
 }
 
 func (c *TestClient) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	if c.queryDataFunc != nil {
-		return c.queryDataFunc(ctx, req)
+	if c.QueryDataFunc != nil {
+		return c.QueryDataFunc(ctx, req)
 	}
 
 	return nil, nil
 }
 
 func (c *TestClient) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	if c.callResourceFunc != nil {
-		return c.callResourceFunc(ctx, req, sender)
+	if c.CallResourceFunc != nil {
+		return c.CallResourceFunc(ctx, req, sender)
 	}
 
 	return nil
 }
 
 func (c *TestClient) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	if c.checkHealthFunc != nil {
-		return c.checkHealthFunc(ctx, req)
+	if c.CheckHealthFunc != nil {
+		return c.CheckHealthFunc(ctx, req)
 	}
 
 	return nil, nil
 }
 
-type TestMiddleware struct {
-	next                     plugins.Client
-	Name                     string
-	queryDataCallChain       []string
-	callResourceCallChain    []string
-	collectMetricsCallChain  []string
-	checkHealthCallChain     []string
-	subscribeStreamCallChain []string
-	publishStreamCallChain   []string
-	runStreamCallChain       []string
+type MiddlewareScenarioContext struct {
+	QueryDataCallChain       []string
+	CallResourceCallChain    []string
+	CollectMetricsCallChain  []string
+	CheckHealthCallChain     []string
+	SubscribeStreamCallChain []string
+	PublishStreamCallChain   []string
+	RunStreamCallChain       []string
 }
 
-func NewMiddleware(name string) plugins.ClientMiddleware {
+func (ctx *MiddlewareScenarioContext) NewMiddleware(name string) plugins.ClientMiddleware {
 	return plugins.ClientMiddlewareFunc(func(next plugins.Client) plugins.Client {
 		return &TestMiddleware{
 			next: next,
 			Name: name,
+			sCtx: ctx,
 		}
 	})
 }
 
+type TestMiddleware struct {
+	next plugins.Client
+	sCtx *MiddlewareScenarioContext
+	Name string
+}
+
 func (m *TestMiddleware) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	m.queryDataCallChain = append(m.queryDataCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.QueryDataCallChain = append(m.sCtx.QueryDataCallChain, fmt.Sprintf("before %s", m.Name))
 	res, err := m.next.QueryData(ctx, req)
-	m.queryDataCallChain = append(m.queryDataCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.QueryDataCallChain = append(m.sCtx.QueryDataCallChain, fmt.Sprintf("after %s", m.Name))
 	return res, err
 }
 
 func (m *TestMiddleware) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	m.callResourceCallChain = append(m.callResourceCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.CallResourceCallChain = append(m.sCtx.CallResourceCallChain, fmt.Sprintf("before %s", m.Name))
 	err := m.next.CallResource(ctx, req, sender)
-	m.callResourceCallChain = append(m.callResourceCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.CallResourceCallChain = append(m.sCtx.CallResourceCallChain, fmt.Sprintf("after %s", m.Name))
 	return err
 }
 
 func (m *TestMiddleware) CollectMetrics(ctx context.Context, req *backend.CollectMetricsRequest) (*backend.CollectMetricsResult, error) {
-	m.collectMetricsCallChain = append(m.collectMetricsCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.CollectMetricsCallChain = append(m.sCtx.CollectMetricsCallChain, fmt.Sprintf("before %s", m.Name))
 	res, err := m.next.CollectMetrics(ctx, req)
-	m.collectMetricsCallChain = append(m.collectMetricsCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.CollectMetricsCallChain = append(m.sCtx.CollectMetricsCallChain, fmt.Sprintf("after %s", m.Name))
 	return res, err
 }
 
 func (m *TestMiddleware) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	m.checkHealthCallChain = append(m.checkHealthCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.CheckHealthCallChain = append(m.sCtx.CheckHealthCallChain, fmt.Sprintf("before %s", m.Name))
 	res, err := m.next.CheckHealth(ctx, req)
-	m.checkHealthCallChain = append(m.checkHealthCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.CheckHealthCallChain = append(m.sCtx.CheckHealthCallChain, fmt.Sprintf("after %s", m.Name))
 	return res, err
 }
 
 func (m *TestMiddleware) SubscribeStream(ctx context.Context, req *backend.SubscribeStreamRequest) (*backend.SubscribeStreamResponse, error) {
-	m.subscribeStreamCallChain = append(m.subscribeStreamCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.SubscribeStreamCallChain = append(m.sCtx.SubscribeStreamCallChain, fmt.Sprintf("before %s", m.Name))
 	res, err := m.next.SubscribeStream(ctx, req)
-	m.subscribeStreamCallChain = append(m.subscribeStreamCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.SubscribeStreamCallChain = append(m.sCtx.SubscribeStreamCallChain, fmt.Sprintf("after %s", m.Name))
 	return res, err
 }
 
 func (m *TestMiddleware) PublishStream(ctx context.Context, req *backend.PublishStreamRequest) (*backend.PublishStreamResponse, error) {
-	m.publishStreamCallChain = append(m.publishStreamCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.PublishStreamCallChain = append(m.sCtx.PublishStreamCallChain, fmt.Sprintf("before %s", m.Name))
 	res, err := m.next.PublishStream(ctx, req)
-	m.publishStreamCallChain = append(m.publishStreamCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.PublishStreamCallChain = append(m.sCtx.PublishStreamCallChain, fmt.Sprintf("after %s", m.Name))
 	return res, err
 }
 
 func (m *TestMiddleware) RunStream(ctx context.Context, req *backend.RunStreamRequest, sender *backend.StreamSender) error {
-	m.runStreamCallChain = append(m.runStreamCallChain, fmt.Sprintf("before %s", m.Name))
+	m.sCtx.RunStreamCallChain = append(m.sCtx.RunStreamCallChain, fmt.Sprintf("before %s", m.Name))
 	err := m.next.RunStream(ctx, req, sender)
-	m.runStreamCallChain = append(m.runStreamCallChain, fmt.Sprintf("after %s", m.Name))
+	m.sCtx.RunStreamCallChain = append(m.sCtx.RunStreamCallChain, fmt.Sprintf("after %s", m.Name))
 	return err
 }
 
 var _ plugins.Client = &TestClient{}
-var _ plugins.ClientMiddleware = NewMiddleware("")
--- pkg/plugins/manager/client/decorator_test.go
+++ pkg/plugins/manager/client/decorator_test.go
@@ -1,17 +1,66 @@
 package client
 
 import (
+	"context"
 	"testing"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/plugins/manager/client/clienttest"
 	"github.com/stretchr/testify/require"
 )
 
+func TestDecorator(t *testing.T) {
+	var queryDataCalled bool
+	var callResourceCalled bool
+	var checkHealthCalled bool
+	c := &clienttest.TestClient{
+		QueryDataFunc: func(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+			queryDataCalled = true
+			return nil, nil
+		},
+		CallResourceFunc: func(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+			callResourceCalled = true
+			return nil
+		},
+		CheckHealthFunc: func(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
+			checkHealthCalled = true
+			return nil, nil
+		},
+	}
+	require.NotNil(t, c)
+
+	ctx := clienttest.MiddlewareScenarioContext{}
+
+	mwOne := ctx.NewMiddleware("mw1")
+	mwTwo := ctx.NewMiddleware("mw2")
+
+	d, err := NewDecorator(c, mwOne, mwTwo)
+	require.NoError(t, err)
+	require.NotNil(t, d)
+
+	_, _ = d.QueryData(context.Background(), nil)
+	require.True(t, queryDataCalled)
+
+	_ = d.CallResource(context.Background(), nil, nil)
+	require.True(t, callResourceCalled)
+
+	_, _ = d.CheckHealth(context.Background(), nil)
+	require.True(t, checkHealthCalled)
+
+	require.Len(t, ctx.QueryDataCallChain, 4)
+	require.EqualValues(t, []string{"before mw1", "before mw2", "after mw2", "after mw1"}, ctx.QueryDataCallChain)
+	require.Len(t, ctx.CallResourceCallChain, 4)
+	require.EqualValues(t, []string{"before mw1", "before mw2", "after mw2", "after mw1"}, ctx.CallResourceCallChain)
+	require.Len(t, ctx.CheckHealthCallChain, 4)
+	require.EqualValues(t, []string{"before mw1", "before mw2", "after mw2", "after mw1"}, ctx.CheckHealthCallChain)
+}
+
 func TestReverseMiddlewares(t *testing.T) {
 	t.Run("Should reverse 1 middleware", func(t *testing.T) {
+		ctx := clienttest.MiddlewareScenarioContext{}
 		middlewares := []plugins.ClientMiddleware{
-			clienttest.NewMiddleware("mw1"),
+			ctx.NewMiddleware("mw1"),
 		}
 		reversed := reverseMiddlewares(middlewares)
 		require.Len(t, reversed, 1)
@@ -19,9 +68,10 @@ func TestReverseMiddlewares(t *testing.T) {
 	})
 
 	t.Run("Should reverse 2 middlewares", func(t *testing.T) {
+		ctx := clienttest.MiddlewareScenarioContext{}
 		middlewares := []plugins.ClientMiddleware{
-			clienttest.NewMiddleware("mw1"),
-			clienttest.NewMiddleware("mw2"),
+			ctx.NewMiddleware("mw1"),
+			ctx.NewMiddleware("mw2"),
 		}
 		reversed := reverseMiddlewares(middlewares)
 		require.Len(t, reversed, 2)
@@ -30,10 +80,11 @@ func TestReverseMiddlewares(t *testing.T) {
 	})
 
 	t.Run("Should reverse 3 middlewares", func(t *testing.T) {
+		ctx := clienttest.MiddlewareScenarioContext{}
 		middlewares := []plugins.ClientMiddleware{
-			clienttest.NewMiddleware("mw1"),
-			clienttest.NewMiddleware("mw2"),
-			clienttest.NewMiddleware("mw3"),
+			ctx.NewMiddleware("mw1"),
+			ctx.NewMiddleware("mw2"),
+			ctx.NewMiddleware("mw3"),
 		}
 		reversed := reverseMiddlewares(middlewares)
 		require.Len(t, reversed, 3)
@@ -43,11 +94,12 @@ func TestReverseMiddlewares(t *testing.T) {
 	})
 
 	t.Run("Should reverse 4 middlewares", func(t *testing.T) {
+		ctx := clienttest.MiddlewareScenarioContext{}
 		middlewares := []plugins.ClientMiddleware{
-			clienttest.NewMiddleware("mw1"),
-			clienttest.NewMiddleware("mw2"),
-			clienttest.NewMiddleware("mw3"),
-			clienttest.NewMiddleware("mw4"),
+			ctx.NewMiddleware("mw1"),
+			ctx.NewMiddleware("mw2"),
+			ctx.NewMiddleware("mw3"),
+			ctx.NewMiddleware("mw4"),
 		}
 		reversed := reverseMiddlewares(middlewares)
 		require.Len(t, reversed, 4)
