4dd4bbfda4c33c1bb7d35efca85b54486eb371f9
Gareth Dawson <gwdawson.work@gmail.com>
2022-11-04 15:33:55 +0000

    add caseInsensitive optional parameter

--- public/app/plugins/datasource/loki/querybuilder/operationUtils.ts
+++ public/app/plugins/datasource/loki/querybuilder/operationUtils.ts
@@ -255,12 +255,10 @@ export function addNestedQueryHandler(def: QueryBuilderOperationDef, query: Loki
   };
 }
 
-export function getLineFilterRenderer(operation: string) {
+export function getLineFilterRenderer(operation: string, caseInsensitive?: boolean) {
   return function lineFilterRenderer(model: QueryBuilderOperation, def: QueryBuilderOperationDef, innerExpr: string) {
-    if (
-      model.id === LokiOperationId.LineContainsInsensitive ||
-      model.id === LokiOperationId.LineContainsNotInsensitive
-    ) {
+    if (caseInsensitive) {
+      console.log('case insensitive', caseInsensitive);
       return `${innerExpr} ${operation} \`(?i)${model.params[0]}\``;
     }
     return `${innerExpr} ${operation} \`${model.params[0]}\``;
