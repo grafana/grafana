13547dae518021182095cbf6355478dee7c04049
Torkel Ödegaard <torkel@grafana.com>
2022-11-07 07:52:32 +0100

    revert

--- packages/grafana-toolkit/src/config/eslint.plugin.js
+++ packages/grafana-toolkit/src/config/eslint.plugin.js
@@ -2,6 +2,5 @@ module.exports = {
   extends: ['@grafana/eslint-config'],
   rules: {
     'react/prop-types': 'off',
-    '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
   },
 };
