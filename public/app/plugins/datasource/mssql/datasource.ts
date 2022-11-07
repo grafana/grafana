dbff6e3e07034340872a17ac790121765a917354
Zoltán Bedi <zoltan.bedi@gmail.com>
2022-11-07 17:04:53 +0100

    MSSql/Postgres: Fix visual query editor filter disappearing (#58248)
    (cherry picked from commit b47230623944f01aa6e2367913e2f7596ff284bf)

--- public/app/plugins/datasource/mssql/datasource.ts
+++ public/app/plugins/datasource/mssql/datasource.ts
@@ -72,6 +72,9 @@ export class MssqlDatasource extends SqlDatasource {
   }
 
   getDB(): DB {
+    if (this.db !== undefined) {
+      return this.db;
+    }
     return {
       init: () => Promise.resolve(true),
       datasets: () => this.fetchDatasets(),
