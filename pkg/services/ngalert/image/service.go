e926da953a0bb6df21ecf2b79cf1df6b669c719e
George Robinson <george.robinson@grafana.com>
2022-11-08 16:49:14 +0000

    Alerting: Use fnv hash in singleflight

--- pkg/services/ngalert/image/service.go
+++ pkg/services/ngalert/image/service.go
@@ -2,6 +2,7 @@ package image
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"time"
@@ -135,7 +136,7 @@ func (s *ScreenshotImageService) NewImage(ctx context.Context, r *models.AlertRu
 		Timeout:      screenshotTimeout,
 	}
 
-	k := fmt.Sprintf("%s-%d-%s", opts.DashboardUID, opts.PanelID, opts.Theme)
+	k := base64.StdEncoding.EncodeToString(opts.Sum())
 	result, err, _ := s.singleflight.Do(k, func() (interface{}, error) {
 		screenshot, err := s.limiter.Do(ctx, opts, s.screenshots.Take)
 		if err != nil {
