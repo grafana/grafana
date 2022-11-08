6dbb05e90524f466ceb5716259502da946e3bae1
polinaboneva <polina.boneva@grafana.com>
2022-11-08 15:50:55 +0200

    add test PanelChrome prop padding

--- packages/grafana-ui/src/components/PanelChrome/PanelChrome.test.tsx
+++ packages/grafana-ui/src/components/PanelChrome/PanelChrome.test.tsx
@@ -1,19 +1,33 @@
 import { screen, render } from '@testing-library/react';
 import React from 'react';
 
-import { PanelChrome } from './PanelChrome';
+import { PanelChrome, PanelChromeProps } from './PanelChrome';
 
-const setup = () => {
-  render(
-    <PanelChrome width={100} height={100}>
-      {(innerWidth, innerHeight) => {
-        return <div style={{ width: innerWidth, height: innerHeight, color: 'pink' }}>Panel&apos;s Content</div>;
-      }}
-    </PanelChrome>
-  );
+const setup = (propOverrides?: Partial<PanelChromeProps>) => {
+  const props: PanelChromeProps = {
+    width: 100,
+    height: 100,
+    children: (innerWidth, innerHeight) => {
+      return <div style={{ width: innerWidth, height: innerHeight, color: 'pink' }}>Panel&apos;s Content</div>;
+    },
+  };
+
+  Object.assign(props, propOverrides);
+  render(<PanelChrome {...props} />);
 };
 
 it('renders an empty panel with required props only', () => {
   setup();
   expect(screen.getByText("Panel's Content")).toBeInTheDocument();
+});
+
+it('renders an empty panel without padding', () => {
+  setup({ padding: 'none' });
+  expect(screen.getByText("Panel's Content").parentElement).toHaveStyle({ padding: '0px' });
+});
+
+it('renders an empty panel with padding', () => {
+  setup({ padding: 'md' });
+  expect(screen.getByText("Panel's Content").offsetHeight).not.toBe(100);
+  expect(screen.getByText("Panel's Content").parentElement).not.toHaveStyle({ padding: '0px' });
 });
