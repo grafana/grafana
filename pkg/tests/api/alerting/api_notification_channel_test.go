9c7a0d826b1b429f3fdd2e2b7f5e7282ada3694c
Alex Moreno <alexander.moreno@grafana.com>
2022-11-08 13:02:39 +0100

    Alerting: Remove duplicate Slack notification title (#58107) (#58413)
    Move mentions to a markdown-formatted pretext field
    to prevent issues mixing blocks and legacy-attachment content.
    
    (cherry picked from commit d581b368bdeeaf8870e495a15dcfdf5aef293be9)
    
    Co-authored-by: Will Jordan <wjordan@users.noreply.github.com>

--- pkg/tests/api/alerting/api_notification_channel_test.go
+++ pkg/tests/api/alerting/api_notification_channel_test.go
@@ -2102,7 +2102,6 @@ var expNonEmailNotifications = map[string][]string{
 	"slack_recv1/slack_test_without_token": {
 		`{
 		  "channel": "#test-channel",
-          "text": "Integration Test [FIRING:1] SlackAlert1 (default)",
 		  "username": "Integration Test",
 		  "icon_emoji": "🚀",
 		  "icon_url": "https://awesomeemoji.com/rocket",
@@ -2115,16 +2114,9 @@ var expNonEmailNotifications = map[string][]string{
 			  "footer": "Grafana v",
 			  "footer_icon": "https://grafana.com/assets/img/fav32.png",
 			  "color": "#D63232",
-			  "ts": %s
-			}
-		  ],
-		  "blocks": [
-			{
-			  "text": {
-				"text": "<!here|here> <!subteam^group1><!subteam^group2> <@user1><@user2>",
-				"type": "mrkdwn"
-			  },
-			  "type": "section"
+			  "ts": %s,
+              "mrkdwn_in": ["pretext"],
+              "pretext": "<!here|here> <!subteam^group1><!subteam^group2> <@user1><@user2>"
 			}
 		  ]
 		}`,
@@ -2132,7 +2124,6 @@ var expNonEmailNotifications = map[string][]string{
 	"slack_recvX/slack_testX": {
 		`{
 		  "channel": "#test-channel",
-          "text": "[FIRING:1] SlackAlert2 (default)",
 		  "username": "Integration Test",
 		  "attachments": [
 			{
@@ -2143,16 +2134,9 @@ var expNonEmailNotifications = map[string][]string{
 			  "footer": "Grafana v",
 			  "footer_icon": "https://grafana.com/assets/img/fav32.png",
 			  "color": "#D63232",
-			  "ts": %s
-			}
-		  ],
-		  "blocks": [
-			{
-			  "text": {
-				"text": "<@user1><@user2>",
-				"type": "mrkdwn"
-			  },
-			  "type": "section"
+			  "ts": %s,
+              "mrkdwn_in": ["pretext"],
+              "pretext": "<@user1><@user2>"
 			}
 		  ]
 		}`,
