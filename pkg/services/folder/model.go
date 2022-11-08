50116620ecd3a917be2947f1f35acd64e2bb8181
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-11-08 12:23:08 +0200

    Apply suggestion from code review
    Fix error msgID

--- pkg/services/folder/model.go
+++ pkg/services/folder/model.go
@@ -10,7 +10,7 @@ var ErrMaximumDepthReached = errutil.NewBase(errutil.StatusBadRequest, "folder.m
 var ErrBadRequest = errutil.NewBase(errutil.StatusBadRequest, "folder.bad-request")
 var ErrDatabaseError = errutil.NewBase(errutil.StatusInternal, "folder.database-error")
 var ErrInternal = errutil.NewBase(errutil.StatusInternal, "folder.internal")
-var ErrFolderTooDeep = errutil.NewBase(errutil.StatusInternal, "folder.tooDeep")
+var ErrFolderTooDeep = errutil.NewBase(errutil.StatusInternal, "folder.too-deep")
 
 const (
 	GeneralFolderUID     = "general"
