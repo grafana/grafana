6095ab9667653cd823b0ae6db78ffbec26e1eada
jguer <joao.guerreiro@grafana.com>
2022-11-09 02:08:01 +0100

    Add sample table

--- public/app/features/admin/SupportBundles.tsx
+++ public/app/features/admin/SupportBundles.tsx
@@ -12,7 +12,36 @@ function SupportBundles() {
           Labs team.
         </div>
 
-        <Button variant="primary">Download Support Bundle</Button>
+        <table className="filter-table form-inline">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Requested by</th>
+              <th>Expires</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>2022-11-10 12h04</th>
+              <th>jguer</th>
+              <th>2022-11-11 12h04</th>
+              <th>
+                <Button variant="secondary">Download</Button>
+              </th>
+            </tr>
+            <tr>
+              <th>2022-12-10 14h21</th>
+              <th>kallee</th>
+              <th>2022-12-11 14h21</th>
+              <th>
+                <Button variant="secondary">Download</Button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+
+        <Button variant="primary">Create New Support Bundle</Button>
       </Page.Contents>
     </Page>
   );
