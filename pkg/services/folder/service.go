bf5a08e03986315c1f04da994e3d15ee88a6bb18
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-11-10 11:41:03 +0200

    API: Support creating a nested folder (#58508)
    * API: Support nested folder creation
    
    * Update swagger
    
    * fixup
    
    * Update pkg/api/dtos/folder.go
    
    Co-authored-by: Serge Zaitsev <serge.zaitsev@grafana.com>
    
    * Fix some tests
    
    * create legacy folder url from title and uid
    
    Co-authored-by: idafurjes <36131195+idafurjes@users.noreply.github.com>
    Co-authored-by: Serge Zaitsev <serge.zaitsev@grafana.com>
    Co-authored-by: Ida Furjesova <ida.furjesova@grafana.com>

--- pkg/services/folder/service.go
+++ pkg/services/folder/service.go
@@ -12,7 +12,7 @@ type Service interface {
 	GetFolderByID(ctx context.Context, user *user.SignedInUser, id int64, orgID int64) (*models.Folder, error)
 	GetFolderByUID(ctx context.Context, user *user.SignedInUser, orgID int64, uid string) (*models.Folder, error)
 	GetFolderByTitle(ctx context.Context, user *user.SignedInUser, orgID int64, title string) (*models.Folder, error)
-	CreateFolder(ctx context.Context, user *user.SignedInUser, orgID int64, title, uid string) (*models.Folder, error)
+	Create(ctx context.Context, cmd *CreateFolderCommand) (*Folder, error)
 	UpdateFolder(ctx context.Context, user *user.SignedInUser, orgID int64, existingUid string, cmd *models.UpdateFolderCommand) error
 	DeleteFolder(ctx context.Context, cmd *DeleteFolderCommand) error
 	MakeUserAdmin(ctx context.Context, orgID int64, userID, folderID int64, setViewAndEditPermissions bool) error
