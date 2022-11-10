ec64e081adeff63b24f2b0fa33ec0d879e33350a
George Robinson <george.robinson@grafana.com>
2022-11-10 09:41:31 +0000

    Alerting: Log when alert rule cannot be screenshot to help debugging (#58537)
    (cherry picked from commit 68600c224b36c4668cec75b7acc30369ac8f4eec)

--- pkg/services/ngalert/image/service.go
+++ pkg/services/ngalert/image/service.go
@@ -124,11 +124,15 @@ func NewScreenshotImageServiceFromCfg(cfg *setting.Cfg, db *store.DBstore, ds da
 // alert rule has a Dashboard UID and the dashboard exists, but does not have a
 // Panel ID in its annotations then an ErrNoPanel error is returned.
 func (s *ScreenshotImageService) NewImage(ctx context.Context, r *models.AlertRule) (*models.Image, error) {
+	logger := s.logger.FromContext(ctx)
+
 	if r.DashboardUID == nil || *r.DashboardUID == "" {
+		logger.Debug("Cannot take screenshot for alert rule as it is not associated with a dashboard")
 		return nil, ErrNoDashboard
 	}
 
 	if r.PanelID == nil || *r.PanelID == 0 {
+		logger.Debug("Cannot take screenshot for alert rule as it is not associated with a panel")
 		return nil, ErrNoPanel
 	}
 
@@ -138,13 +142,11 @@ func (s *ScreenshotImageService) NewImage(ctx context.Context, r *models.AlertRu
 		Timeout:      screenshotTimeout,
 	}
 
+	logger = logger.New("dashboard", opts.DashboardUID, "panel", opts.PanelID)
+
 	// To prevent concurrent screenshots of the same dashboard panel we use singleflight,
 	// deduplicated on a base64 hash of the screenshot options.
 	optsHash := base64.StdEncoding.EncodeToString(opts.Hash())
-
-	logger := s.logger.FromContext(ctx).New(
-		"dashboard", opts.DashboardUID,
-		"panel", opts.PanelID)
 
 	// If there is an image is in the cache return it instead of taking another screenshot
 	if image, ok := s.cache.Get(ctx, optsHash); ok {
