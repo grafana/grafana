b6dfe96ffa5e793180032c2f0ee93221324b5871
Gareth Dawson <gwdawson.work@gmail.com>
2022-11-10 15:14:37 +0000

    render label browser inside of the modal

--- public/app/plugins/datasource/loki/querybuilder/components/LabelBrowserModal.tsx
+++ public/app/plugins/datasource/loki/querybuilder/components/LabelBrowserModal.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
 
 import { Modal } from '@grafana/ui';
+import { LocalStorageValueProvider } from 'app/core/components/LocalStorageValueProvider';
+
+import LanguageProvider from '../../LanguageProvider';
+import { LokiLabelBrowser } from '../../components/LokiLabelBrowser';
+import { LokiQuery } from '../../types';
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  languageProvider: LanguageProvider;
+  onChange: any;
+  onRunQuery: any;
+  query: LokiQuery;
 };
 
 export const LabelBrowserModal = (props: Props) => {
-  const { isOpen, onClose } = props;
+  const { isOpen, onClose, languageProvider } = props;
+
+  const LAST_USED_LABELS_KEY = 'grafana.datasources.loki.browser.labels';
+
+  const changeQuery = (value: string, override?: boolean) => {
+    const { query, onChange, onRunQuery } = props;
+
+    if (onChange) {
+      const nextQuery = { ...query, expr: value };
+      onChange(nextQuery);
+
+      if (override && onRunQuery) {
+        onRunQuery();
+      }
+    }
+  };
+
+  const onChange = (selector: string) => {
+    changeQuery(selector, true);
+    onClose();
+  };
 
   return (
     <Modal isOpen={isOpen} title="Label browser" onDismiss={onClose}>
-      <div>Label browser content will go here...</div>
+      <LocalStorageValueProvider<string[]> storageKey={LAST_USED_LABELS_KEY} defaultValue={[]}>
+        {(lastUsedLabels, onLastUsedLabelsSave, onLastUsedLabelsDelete) => {
+          return (
+            <LokiLabelBrowser
+              languageProvider={languageProvider}
+              onChange={onChange}
+              lastUsedLabels={lastUsedLabels}
+              storeLastUsedLabels={onLastUsedLabelsSave}
+              deleteLastUsedLabels={onLastUsedLabelsDelete}
+            />
+          );
+        }}
+      </LocalStorageValueProvider>
     </Modal>
   );
 };
