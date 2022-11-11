3c2fd45b65bb176117e1b7e0b1a1514c1fd17ed2
bohandley <brendan.ohandley@gmail.com>
2022-11-11 11:39:05 -0500

    remove comments

--- public/app/plugins/datasource/prometheus/metric_find_query.ts
+++ public/app/plugins/datasource/prometheus/metric_find_query.ts
@@ -55,23 +55,6 @@ export default class PrometheusMetricFindQuery {
     return Promise.resolve([]);
   }
 
-  // labelNamesQuery() {
-  //   const start = this.datasource.getPrometheusTime(this.range.from, false);
-  //   const end = this.datasource.getPrometheusTime(this.range.to, true);
-  //   const params = {
-  //     start: start.toString(),
-  //     end: end.toString(),
-  //   };
-
-  //   const url = `/api/v1/labels`;
-
-  //   return this.datasource.metadataRequest(url, params).then((result: any) => {
-  //     return _map(result.data.data, (value) => {
-  //       return { text: value };
-  //     });
-  //   });
-  // }
-
   labelValuesQuery(label: string, metric?: string) {
     const start = this.datasource.getPrometheusTime(this.range.from, false);
     const end = this.datasource.getPrometheusTime(this.range.to, true);
