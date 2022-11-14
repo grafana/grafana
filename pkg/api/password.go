16a74040a0d0099eaf2066e2266672c44b6c515d
Karl Persson <kalle.persson@grafana.com>
2022-11-14 09:42:51 +0100

    [v9.1.x] Omit error from http response (#58450)

--- pkg/api/password.go
+++ pkg/api/password.go
@@ -29,8 +29,8 @@ func (hs *HTTPServer) SendResetPasswordEmail(c *models.ReqContext) response.Resp
 	userQuery := models.GetUserByLoginQuery{LoginOrEmail: form.UserOrEmail}
 
 	if err := hs.SQLStore.GetUserByLogin(c.Req.Context(), &userQuery); err != nil {
-		c.Logger.Info("Requested password reset for user that was not found", "user", userQuery.LoginOrEmail)
-		return response.Error(http.StatusOK, "Email sent", err)
+		c.Logger.Info("Requested password reset for user that was not found", "user", userQuery.LoginOrEmail, "error", err)
+		return response.Error(http.StatusOK, "Email sent", nil)
 	}
 
 	emailCmd := models.SendResetPasswordEmailCommand{User: userQuery.Result}
