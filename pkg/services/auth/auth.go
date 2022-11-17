513f6f5f9a5a06d2acbc9fece98cf5159a30476c
Karl Persson <kalle.persson@grafana.com>
2022-11-17 17:05:25 +0100

    Auth: move quota consts to auth file

--- pkg/services/auth/auth.go
+++ pkg/services/auth/auth.go
@@ -6,6 +6,11 @@ import (
 	"github.com/grafana/grafana/pkg/services/quota"
 )
 
+const (
+	QuotaTargetSrv quota.TargetSrv = "auth"
+	QuotaTarget    quota.Target    = "session"
+)
+
 type ActiveTokenService interface {
 	ActiveTokenCount(ctx context.Context, _ *quota.ScopeParameters) (*quota.Map, error)
 }
