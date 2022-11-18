813f6d072c6a1a079ab84d0705b200d4ec1b482a
Ivan Ortega <ivanortegaalba@gmail.com>
2022-11-18 12:18:11 +0100

    Refactor reporter to not depend on JSON mocha one

--- e2e/loki-reporter.js
+++ e2e/loki-reporter.js
@@ -1,42 +1,84 @@
 'use strict';
 
 const Mocha = require('mocha');
-const { EVENT_RUN_BEGIN, EVENT_RUN_END, EVENT_TEST_FAIL, EVENT_TEST_PASS, EVENT_SUITE_BEGIN, EVENT_SUITE_END } =
-  Mocha.Runner.constants;
-const JsonReporter = Mocha.reporters.JSON;
+const { EVENT_TEST_END, EVENT_RUN_END, EVENT_TEST_FAIL, EVENT_TEST_PASS } = Mocha.Runner.constants;
 
-// this reporter outputs test results, indenting two spaces per suite
-class MyReporter extends Mocha.reporters.JSON {
-  constructor(runner) {
-    super(runner);
+class LogReporter extends Mocha.reporters.Base {
+  constructor(runner, options = {}) {
+    super(runner, options);
+
+    this.tests = [];
+    this.failures = [];
+    this.passes = [];
+
+    runner.on(EVENT_TEST_END, function (test) {
+      this.tests.push(testToJSON(test));
+    });
+
+    runner.on(EVENT_TEST_PASS, function (test) {
+      this.passes.push(testToJSON(test));
+    });
+
+    runner.on(EVENT_TEST_FAIL, function (test) {
+      console.log(this);
+      this.failures.push(testToJSON(test));
+    });
 
     runner.once(EVENT_RUN_END, () => {
-      this.reportStats(runner);
-      this.reportErrors(runner);
+      this.reportStats();
+      this.reportErrors();
     });
   }
 
-  reportStats(runner) {
-    console.log(`CypressStats ${this.strigifyObj(runner.testResults.stats)}`);
+  reportStats() {
+    console.log(`CypressStats ${logObj(this.stats)}`);
+
+    this.tests.map((test) => {
+      console.log(`CypressTestResult ${logObj(test)}`);
+    });
   }
 
-  reportErrors(runner) {
-    runner.testResults.failures.forEach((failure) => {
-      const suite = String(failure.fullTitle).replace(failure.title, '').trim();
+  reportErrors() {
+    this.failures.forEach((failure) => {
+      const suite = failure.suite;
       const test = failure.title;
       const error = failure.err.message;
 
-      console.error(`CypressError ${this.strigifyObj({ suite, test, error })}`);
+      console.error(`CypressError ${logObj({ suite, test, error })}`);
     });
-  }
-
-  strigifyObj(obj) {
-    return Object.entries(obj).map(([key, value]) => `${key}="${this.escapeValue(value)}"`);
-  }
-
-  escapeValue(str) {
-    return String(str).replaceAll('"', '\\"');
   }
 }
 
-module.exports = MyReporter;
+function logObj(obj) {
+  return Object.entries(obj)
+    .map(([key, value]) => `${key}="${escapeQuotes(value)}"`)
+    .join(' ');
+}
+
+function escapeQuotes(str) {
+  return String(str).replaceAll('"', '\\"');
+}
+
+function testToJSON(test) {
+  let err = test.err || {};
+
+  if (err instanceof Error) {
+    err = {
+      name: err.name,
+      stack: err.stack,
+      message: err.message,
+    };
+  }
+
+  return {
+    title: test.title,
+    suite: String(test.fullTitle).replace(test.title, '').trim(),
+    file: test.file,
+    duration: test.duration,
+    currentRetry: test.currentRetry(),
+    speed: test.speed,
+    err,
+  };
+}
+
+module.exports = LogReporter;
