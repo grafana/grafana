19bd8338edc12cbf88c7431dd20b5a5c3ba6a30f
Yuri Tseretyan <tceretian@gmail.com>
2022-11-18 12:39:31 -0500

    revert

--- pkg/services/ngalert/eval/eval.go
+++ pkg/services/ngalert/eval/eval.go
@@ -225,7 +225,7 @@ func buildDatasourceHeaders(ctx EvaluationContext) map[string]string {
 
 	key, ok := models.RuleKeyFromContext(ctx.Ctx)
 	if ok {
-		headers["X-Grafana-Rule-Uid"] = key.UID
+		headers["X-Rule-Uid"] = key.UID
 		headers["X-Grafana-Org-Id"] = strconv.FormatInt(key.OrgID, 10)
 	}
 
