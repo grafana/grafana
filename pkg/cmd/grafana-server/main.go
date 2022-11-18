73ed7cc3f1012a96d50346d394cf82bf33131d5a
Dan Cech <dcech@grafana.com>
2022-11-18 17:01:28 -0500

    windows support

--- pkg/cmd/grafana-server/main.go
+++ pkg/cmd/grafana-server/main.go
@@ -1,41 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
-	"syscall"
+
+	"github.com/grafana/grafana/pkg/util/cmd"
 )
 
 func main() {
-	curr, err := os.Executable()
-	if err != nil {
-		fmt.Println("Error locating executable:", err)
-		os.Exit(1)
-	}
-
-	executable := "grafana"
-	if runtime.GOOS == "windows" {
-		executable += ".exe"
-	}
-
-	binary := filepath.Join(filepath.Dir(filepath.Clean(curr)), executable)
-	if _, err := os.Stat(binary); err != nil {
-		binary, err = exec.LookPath(executable)
-		if err != nil {
-			fmt.Printf("Error locating %s: %s\n", executable, err)
-		}
-	}
-
-	args := append([]string{"grafana", "server"}, os.Args[1:]...)
-
-	// bypassing gosec G204 because we need to build the command programmatically
-	// nolint:gosec
-	execErr := syscall.Exec(binary, args, os.Environ())
-	if execErr != nil {
-		fmt.Printf("Error running %s: %s\n", binary, execErr)
-		os.Exit(1)
-	}
+	os.Exit(cmd.RunGrafanaCmd("server"))
 }
