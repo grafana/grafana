1166c7c94ae8254b0aec5634a142871757030872
Karl Persson <kalle.persson@grafana.com>
2022-11-18 12:59:17 +0100

    LoginAttempts: Add tests

--- pkg/login/brute_force_login_protection_test.go
+++ pkg/login/brute_force_login_protection_test.go
@@ -12,65 +12,6 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 )
 
-func TestValidateLoginAttempts(t *testing.T) {
-	testCases := []struct {
-		name          string
-		loginAttempts int64
-		cfg           *setting.Cfg
-		expected      error
-	}{
-		{
-			name:          "When brute force protection enabled and user login attempt count is less than max",
-			loginAttempts: maxInvalidLoginAttempts - 1,
-			cfg:           cfgWithBruteForceLoginProtectionEnabled(t),
-			expected:      nil,
-		},
-		{
-			name:          "When brute force protection enabled and user login attempt count equals max",
-			loginAttempts: maxInvalidLoginAttempts,
-			cfg:           cfgWithBruteForceLoginProtectionEnabled(t),
-			expected:      ErrTooManyLoginAttempts,
-		},
-		{
-			name:          "When brute force protection enabled and user login attempt count is greater than max",
-			loginAttempts: maxInvalidLoginAttempts + 1,
-			cfg:           cfgWithBruteForceLoginProtectionEnabled(t),
-			expected:      ErrTooManyLoginAttempts,
-		},
-
-		{
-			name:          "When brute force protection disabled and user login attempt count is less than max",
-			loginAttempts: maxInvalidLoginAttempts - 1,
-			cfg:           cfgWithBruteForceLoginProtectionDisabled(t),
-			expected:      nil,
-		},
-		{
-			name:          "When brute force protection disabled and user login attempt count equals max",
-			loginAttempts: maxInvalidLoginAttempts,
-			cfg:           cfgWithBruteForceLoginProtectionDisabled(t),
-			expected:      nil,
-		},
-		{
-			name:          "When brute force protection disabled and user login attempt count is greater than max",
-			loginAttempts: maxInvalidLoginAttempts + 1,
-			cfg:           cfgWithBruteForceLoginProtectionDisabled(t),
-			expected:      nil,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			store := mockstore.NewSQLStoreMock()
-			store.ExpectedLoginAttempts = tc.loginAttempts
-
-			query := &models.LoginUserQuery{Username: "user", Cfg: tc.cfg}
-
-			err := validateLoginAttempts(context.Background(), query, store)
-			require.Equal(t, tc.expected, err)
-		})
-	}
-}
-
 func TestSaveInvalidLoginAttempt(t *testing.T) {
 	t.Run("When brute force protection enabled", func(t *testing.T) {
 		store := mockstore.NewSQLStoreMock()
