f35ab2f79d0b103fb6fddef2510fd6de1deac948
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-11-18 11:15:17 -0500

    [v9.3.x] Nav: Split Admin into three sections for new IA (#58974)
    Nav: Split Admin into three sections for new IA (#58229)
    
    * start to split admin into two sections
    
    * most of new admin nav implemented
    
    * landing pages
    
    * hide admin for non-admins
    
    * update admin redirects if not topnav
    
    * clean up
    
    * updated IA for admin (still WIP)
    
    * move plugin pages into correct admin sections
    
    * fix backend unit test
    
    * move correlations into the correct section
    
    * add translations for admin sections
    
    Co-authored-by: Ashley Harrison <ashley.harrison@grafana.com>
    (cherry picked from commit 5978dc138ea1df9972b148cefd9866a5e7628528)
    
    Co-authored-by: Josh Hunt <joshhunt@users.noreply.github.com>

--- public/app/core/components/NavBar/navBarItem-translations.ts
+++ public/app/core/components/NavBar/navBarItem-translations.ts
@@ -69,6 +69,12 @@ export function getNavTitle(navId: string | undefined) {
       return config.featureToggles.topnav
         ? t('nav.config.title', 'Administration')
         : t('nav.config.titleBeforeTopnav', 'Configuration');
+    case 'admin/general':
+      return t('nav.admin-general.title', 'General');
+    case 'admin/plugins':
+      return t('nav.admin-plugins.title', 'Plugins and data');
+    case 'admin/access':
+      return t('nav.admin-access.title', 'Users and access');
     case 'datasources':
       return t('nav.datasources.title', 'Data sources');
     case 'correlations':
@@ -80,7 +86,9 @@ export function getNavTitle(navId: string | undefined) {
     case 'plugins':
       return t('nav.plugins.title', 'Plugins');
     case 'org-settings':
-      return t('nav.org-settings.title', 'Preferences');
+      return config.featureToggles.topnav
+        ? t('nav.org-settings.title', 'Default preferences')
+        : t('nav.org-settings.titleBeforeTopnav', 'Preferences');
     case 'apikeys':
       return t('nav.api-keys.title', 'API keys');
     case 'serviceaccounts':
@@ -88,7 +96,9 @@ export function getNavTitle(navId: string | undefined) {
     case 'admin':
       return t('nav.admin.title', 'Server admin');
     case 'global-users':
-      return t('nav.global-users.title', 'Users');
+      return config.featureToggles.topnav
+        ? t('nav.global-users.title', 'Users (All orgs)')
+        : t('nav.global-users.titleBeforeTopnav', 'Users');
     case 'global-orgs':
       return t('nav.global-orgs.title', 'Organizations');
     case 'server-settings':
