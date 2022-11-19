4d5b5e0e8e2fa717172e7dae235348b474338f4c
Ivan Ortega <ivanortegaalba@gmail.com>
2022-11-19 11:00:05 +0100

    rename and fix tests

--- e2e/log-reporter.js
+++ e2e/log-reporter.js
@@ -33,23 +33,24 @@ class LogReporter extends Mocha.reporters.Base {
   reportStats() {
     const stats = {
       ...this.stats,
+      // Format time in epoch
       start: this.stats.start.getTime(),
       end: this.stats.end.getTime(),
     };
 
     // Example
-    // suites=1 tests=2 passes=1 pending=0 failures=1 start=1668783563731 end=1668783645198 duration=81467
-    console.log(`CypressStats ${logObj(stats)}`);
+    // CypressStats suites=1 tests=2 passes=1 pending=0 failures=1
+    // start=1668783563731 end=1668783645198 duration=81467
+    console.log(`CypressStats ${objToLogAttributes(stats)}`);
   }
 
   reportResults() {
     this.tests.map((test) => {
       // Example
       // CypressTestResult title="Login scenario, create test data source, dashboard, panel, and export scenario"
-      // suite="Smoke tests > Login scenario, create test data source, dashboard, panel, and export scenario"
-      // file=../../e2e/smoke-tests-suite/1-smoketests.spec.ts duration=68694 currentRetry=0 speed=undefined
-      // err=false
-      console.log(`CypressTestResult ${logObj(test)}`);
+      // suite="Smoke tests" file=../../e2e/smoke-tests-suite/1-smoketests.spec.ts duration=68694
+      // currentRetry=0 speed=undefined err=false
+      console.log(`CypressTestResult ${objToLogAttributes(test)}`);
     });
   }
 
@@ -60,44 +61,74 @@ class LogReporter extends Mocha.reporters.Base {
       const error = failure.err;
 
       // Example
-      // CypressError suite="Smoke tests > Login scenario, create test data source, dashboard,
-      // panel, and export scenario" test="Login scenario, create test data source, dashboard,
+      // CypressError suite="Smoke tests" test="Login scenario, create test data source, dashboard,
       // panel, and export scenario" error=false
-      console.error(`CypressError ${logObj({ suite, test, error })}`);
+      console.error(`CypressError ${objToLogAttributes({ suite, test, error })}`);
     });
   }
 }
 
-function logObj(obj) {
+/**
+ * Stringify object to be log friendly
+ * @param {Object} obj
+ * @returns {String}
+ */
+function objToLogAttributes(obj) {
   return Object.entries(obj)
     .map(([key, value]) => `${key}=${formatValue(value)}`)
     .join(' ');
 }
 
+/**
+ * Escape double quotes
+ * @param {String} str
+ * @returns
+ */
 function escapeQuotes(str) {
   return String(str).replaceAll('"', '\\"');
 }
 
+/**
+ * Wrap the value within double quote if needed
+ * @param {*} value
+ * @returns
+ */
 function formatValue(value) {
   const hasWhiteSpaces = /\s/g.test(value);
 
   return hasWhiteSpaces ? `"${escapeQuotes(value)}"` : value;
 }
 
+/**
+ * Simplify the Mocha test object to get the information we need want to report
+ * @param {Mocha.Test} test
+ * @returns {Object}
+ */
 function cleanTest(test) {
   const err = test.err instanceof Error ? test.err.toString() : false;
+  const testLocation = getTestLocation(test);
+  // Remove the test title
+  const suite = testLocation.slice(0, testLocation.length - 1).join(' > ');
 
   return {
     currentRetry: test.currentRetry(),
     duration: test.duration,
     speed: test.speed,
     file: getTestFile(test),
-    suite: getTestLocation(test).join(' > '),
+    suite,
     title: test.title,
     err,
   };
 }
 
+/**
+ * Get the test path in the suite herarchy
+ * @example
+ * ['Root Suite in a file', 'Nested suite', 'test title']
+ *
+ * @param {Mocha.Test} test
+ * @returns {Array<String>}
+ */
 function getTestLocation(test) {
   let path = test.title ? [test.title] : [];
 
@@ -108,6 +139,11 @@ function getTestLocation(test) {
   return path;
 }
 
+/**
+ * Get the relative path to the executed spec file
+ * @param {Mocha.Test} test
+ * @returns {String | null}
+ */
 function getTestFile(test) {
   if (test?.file) {
     return test?.file;
