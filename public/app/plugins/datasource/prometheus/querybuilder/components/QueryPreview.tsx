84023d5de2bd68e545c2baaff290e44afeb4ea6d
Galen <galen.kistler@grafana.com>
2022-11-21 12:53:22 -0600

    remove raw query text line

--- public/app/plugins/datasource/prometheus/querybuilder/components/QueryPreview.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/components/QueryPreview.tsx
@@ -13,9 +13,7 @@ export function QueryPreview({ query }: Props) {
   return (
     <EditorRow>
       <EditorFieldGroup>
-        <EditorField label="Raw query">
-          <RawQuery query={query} lang={{ grammar: promqlGrammar, name: 'promql' }} />
-        </EditorField>
+        <RawQuery query={query} lang={{ grammar: promqlGrammar, name: 'promql' }} />
       </EditorFieldGroup>
     </EditorRow>
   );
