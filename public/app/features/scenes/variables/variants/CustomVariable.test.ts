0d29ea9ed041056c71c78066a33a018cfbc6a1ac
Ivan Ortega <ivanortegaalba@gmail.com>
2022-11-22 15:08:46 +0100

    port more tests from adapter

--- public/app/features/scenes/variables/variants/CustomVariable.test.ts
+++ public/app/features/scenes/variables/variants/CustomVariable.test.ts
@@ -96,24 +96,113 @@ describe('CustomVariable', () => {
       expect(variable.state.text).toEqual('label,1');
       expect(variable.state.options).toEqual([{ label: 'label,1', value: 'value,1' }]);
     });
-  });
 
-  describe('When no value is provided', () => {
-    it('Should pick first value', async () => {
+    it('Should generate correctly the options for key:value and only values combined', async () => {
       const variable = new CustomVariable({
         name: 'test',
         options: [],
         value: '',
         text: '',
-        query: 'A',
+        query: 'label-1 : value-1, value-2, label\\,3 : value-3,value\\,4',
       });
 
       await lastValueFrom(variable.validateAndUpdate());
 
-      expect(variable.state.value).toBe('A');
-      expect(variable.state.text).toBe('A');
+      expect(variable.state.value).toEqual('value-1');
+      expect(variable.state.text).toEqual('label-1');
+      expect(variable.state.options).toEqual([
+        { label: 'label-1', value: 'value-1' },
+        { label: 'value-2', value: 'value-2' },
+        { label: 'label,3', value: 'value-3' },
+        { label: 'value,4', value: 'value,4' },
+      ]);
     });
 
+    it('Should generate correctly the options for key:value pairs with extra spaces', async () => {
+      const variable = new CustomVariable({
+        name: 'test',
+        options: [],
+        value: '',
+        text: '',
+        query: 'a,  b,   c, d :    e',
+      });
+
+      await lastValueFrom(variable.validateAndUpdate());
+
+      expect(variable.state.value).toEqual('a');
+      expect(variable.state.text).toEqual('a');
+      expect(variable.state.options).toEqual([
+        {
+          label: 'a',
+          value: 'a',
+        },
+        {
+          label: 'b',
+          value: 'b',
+        },
+        {
+          label: 'c',
+          value: 'c',
+        },
+        {
+          label: 'd',
+          value: 'e',
+        },
+      ]);
+    });
+
+    it('Should generate correctly the options for only values as URLs', async () => {
+      const variable = new CustomVariable({
+        name: 'test',
+        options: [],
+        value: '',
+        text: '',
+        query: 'http://www.google.com/, http://www.amazon.com/',
+      });
+
+      await lastValueFrom(variable.validateAndUpdate());
+
+      expect(variable.state.value).toEqual('http://www.google.com/');
+      expect(variable.state.text).toEqual('http://www.google.com/');
+      expect(variable.state.options).toEqual([
+        {
+          label: 'http://www.google.com/',
+          value: 'http://www.google.com/',
+        },
+        {
+          label: 'http://www.amazon.com/',
+          value: 'http://www.amazon.com/',
+        },
+      ]);
+    });
+
+    it('Should generate correctly the options for key/values as URLs', async () => {
+      const variable = new CustomVariable({
+        name: 'test',
+        options: [],
+        value: '',
+        text: '',
+        query: 'google : http://www.google.com/, amazon : http://www.amazon.com/',
+      });
+
+      await lastValueFrom(variable.validateAndUpdate());
+
+      expect(variable.state.value).toEqual('http://www.google.com/');
+      expect(variable.state.text).toEqual('google');
+      expect(variable.state.options).toEqual([
+        {
+          label: 'google',
+          value: 'http://www.google.com/',
+        },
+        {
+          label: 'amazon',
+          value: 'http://www.amazon.com/',
+        },
+      ]);
+    });
+  });
+
+  describe('When value is provided', () => {
     it('Should keep current value if current value is valid', async () => {
       const variable = new CustomVariable({
         name: 'test',
