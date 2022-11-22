823b7cfdc91c2447d5111822f17c53f2da85a34e
Karl Persson <kalle.persson@grafana.com>
2022-11-22 09:53:07 +0100

    LoginAttempt: Add comments to service interface

--- pkg/services/loginattempt/login_attempt.go
+++ pkg/services/loginattempt/login_attempt.go
@@ -5,7 +5,10 @@ import (
 )
 
 type Service interface {
+	// Add adds a new login attempt record for provided username
 	Add(ctx context.Context, username, IPAddress string) error
+	// Validate checks if username has to many login attempts inside a window.
+	// Will return true if provided username do not have too many attempts.
 	Validate(ctx context.Context, username string) (bool, error)
 }
 
