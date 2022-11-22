4c3adeff7c72fdce60a5752ad66a6624d67480d6
Joao Silva <100691367+JoaoSilvaGrafana@users.noreply.github.com>
2022-11-22 17:45:52 +0100

    [v9.3.x] Preferences: Add confirmation modal when saving org preferences (#59119) (#59141)

--- public/app/features/org/OrgDetailsPage.test.tsx
+++ public/app/features/org/OrgDetailsPage.test.tsx
@@ -1,8 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { mockToolkitActionCreator } from 'test/core/redux/mocks';
 
 import { NavModel } from '@grafana/data';
+import { ModalManager } from 'app/core/services/ModalManager';
+import { configureStore } from 'app/store/configureStore';
 
 import { backendSrv } from '../../core/services/backend_srv';
 import { Organization } from '../../types';
@@ -12,6 +16,7 @@ import { setOrganizationName } from './state/reducers';
 
 jest.mock('app/core/core', () => {
   return {
+    ...jest.requireActual('app/core/core'),
     contextSrv: {
       hasPermission: () => true,
     },
@@ -56,7 +61,11 @@ const setup = (propOverrides?: object) => {
   };
   Object.assign(props, propOverrides);
 
-  render(<OrgDetailsPage {...props} />);
+  render(
+    <Provider store={configureStore()}>
+      <OrgDetailsPage {...props} />
+    </Provider>
+  );
 };
 
 describe('Render', () => {
@@ -83,5 +92,25 @@ describe('Render', () => {
         },
       })
     ).not.toThrow();
+  });
+
+  it('should show a modal when submitting', async () => {
+    new ModalManager().init();
+    setup({
+      organization: {
+        name: 'Cool org',
+        id: 1,
+      },
+      preferences: {
+        homeDashboardUID: 'home-dashboard',
+        theme: 'Default',
+        timezone: 'Default',
+        locale: '',
+      },
+    });
+
+    await userEvent.click(screen.getByRole('button', { name: 'Save' }));
+
+    expect(screen.getByText('Confirm preferences update')).toBeInTheDocument();
   });
 });
