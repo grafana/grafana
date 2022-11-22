a80d19dcebf17208b628520edcbef7f0d03e2a83
Clarity-89 <homes89@ukr.net>
2022-11-22 14:48:08 +0200

    Admin: Combine admin and org components

--- public/app/features/admin/UserListPage.tsx
+++ public/app/features/admin/UserListPage.tsx
@@ -1,16 +1,36 @@
+import { css } from '@emotion/css';
 import React, { useState } from 'react';
 
-import UsersListPage from '../users/UsersListPage';
+import { GrafanaTheme2 } from '@grafana/data';
+import { RadioButtonGroup, Field, useStyles2 } from '@grafana/ui';
 
-import UserListAdminPage from './UserListAdminPage';
+import { Page } from '../../core/components/Page/Page';
+import { UsersListPageContent } from '../users/UsersListPage';
 
+import { UserListAdminPageContent } from './UserListAdminPage';
+
+const views = [
+  { value: 'admin', label: 'All organisations' },
+  { value: 'org', label: 'This organisation' },
+];
 export function UserListPage() {
+  const styles = useStyles2(getStyles);
   const [view, setView] = useState('admin');
 
   return (
-    <div>
-      <p>Test</p>
-      {view === 'admin' ? <UserListAdminPage /> : <UsersListPage />}
-    </div>
+    <Page navId={'global-users'}>
+      <Field label={'Display list of users for'} className={styles.container}>
+        <RadioButtonGroup options={views} onChange={setView} value={view} />
+      </Field>
+      {view === 'admin' ? <UserListAdminPageContent /> : <UsersListPageContent />}
+    </Page>
   );
 }
+
+const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    container: css`
+      margin: ${theme.spacing(2, 0)};
+    `,
+  };
+};
