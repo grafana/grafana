e6a7b537038123d00af6e9961b324e5d7d5c729e
Sven Grossmann <sven.grossmann@grafana.com>
2022-11-23 12:52:35 +0100

    [9.2.x] Fix #58598 X-ID-Token header missing on Loki Datasource (#58784) (#59196)
    * Fix #58598 X-ID-Token header missing on Loki Datasource (#58784)
    
    * Fix #58598 X-ID-Token header missing on Loki Datasource
    
    * Remove unecessary continue statements
    
    * Add getAuthHeadersForCallResource unit tests
    
    * Fix test and switch statement issues introduced during merge
    
    (cherry picked from commit f1ef63791ad17d4c3b16cd296148c403515ad37d)
    
    * update test
    
    * missed linting
    
    Co-authored-by: Yann Vigara <yvigara@users.noreply.github.com>

--- pkg/tsdb/loki/loki.go
+++ pkg/tsdb/loki/loki.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/textproto"
 	"regexp"
 	"strings"
 	"sync"
@@ -122,18 +123,22 @@ func (s *Service) CallResource(ctx context.Context, req *backend.CallResourceReq
 func getAuthHeadersForCallResource(headers map[string][]string) map[string]string {
 	data := make(map[string]string)
 
-	if auth := arrayHeaderFirstValue(headers["Authorization"]); auth != "" {
-		data["Authorization"] = auth
-	}
+	for k, values := range headers {
+		k = textproto.CanonicalMIMEHeaderKey(k)
+		firstValue := arrayHeaderFirstValue(values)
 
-	if cookie := arrayHeaderFirstValue(headers["Cookie"]); cookie != "" {
-		data["Cookie"] = cookie
+		if firstValue == "" {
+			continue
+		}
+		switch k {
+		case "Authorization":
+			data["Authorization"] = firstValue
+		case "X-Id-Token":
+			data["X-ID-Token"] = firstValue
+		case "Cookie":
+			data["Cookie"] = firstValue
+		}
 	}
-
-	if idToken := arrayHeaderFirstValue(headers["X-ID-Token"]); idToken != "" {
-		data["X-ID-Token"] = idToken
-	}
-
 	return data
 }
 
