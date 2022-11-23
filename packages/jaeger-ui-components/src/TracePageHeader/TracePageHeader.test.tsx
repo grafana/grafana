b3cbf13cfaf75ac226ea08c3adf3e498f566de90
Hamas Shafiq <hamas.shafiq@grafana.com>
2022-11-23 22:24:32 +0000

    Refactor to TS

--- packages/jaeger-ui-components/src/TracePageHeader/TracePageHeader.test.tsx
+++ packages/jaeger-ui-components/src/TracePageHeader/TracePageHeader.test.tsx
@@ -14,16 +14,22 @@
 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { ViewRangeTimeUpdate } from 'src/TraceTimelineViewer/types';
 
 import traceGenerator from '../demo/trace-generators';
 import { getTraceName } from '../model/trace-viewer';
 import transformTraceData from '../model/transform-trace-data';
 
-import TracePageHeader from './TracePageHeader';
+import TracePageHeader, { TracePageHeaderEmbedProps } from './TracePageHeader';
 
 const trace = transformTraceData(traceGenerator.trace({}));
-const setup = (propOverrides) => {
+const setup = (propOverrides?: TracePageHeaderEmbedProps) => {
   const defaultProps = {
+    canCollapse: false,
+    hideSummary: false,
+    onSlimViewClicked: () => {},
+    onTraceGraphViewClicked: () => {},
+    slimView: false,
     trace,
     hideMap: false,
     showArchiveButton: false,
@@ -31,8 +37,11 @@ const setup = (propOverrides) => {
     showStandaloneLink: false,
     showViewOptions: false,
     textFilter: '',
-    viewRange: { time: { current: [10, 20] } },
+    timeZone: '',
+    viewRange: { time: { current: [10, 20] as [number, number] } },
     updateTextFilter: () => {},
+    updateNextViewRangeTime: (update: ViewRangeTimeUpdate) => {},
+    updateViewRangeTime: (start: number, end: number, trackSrc?: string) => {},
     ...propOverrides,
   };
 
@@ -42,15 +51,12 @@ const setup = (propOverrides) => {
 describe('TracePageHeader test', () => {
   it('should render a header ', () => {
     setup();
-
     expect(screen.getByRole('banner')).toBeInTheDocument();
   });
 
   it('should render nothing if a trace is not present', () => {
-    setup({ trace: null });
-
+    setup({ trace: null } as TracePageHeaderEmbedProps);
     expect(screen.queryByRole('banner')).not.toBeInTheDocument();
-    expect(screen.queryByRole('heading', { traceName: getTraceName(trace.spans) })).not.toBeInTheDocument();
     expect(screen.queryAllByRole('listitem')).toHaveLength(0);
     expect(screen.queryByText(/Reset Selection/)).not.toBeInTheDocument();
   });
@@ -58,7 +64,7 @@ describe('TracePageHeader test', () => {
   it('should render the trace title', () => {
     setup();
 
-    expect(screen.getByRole('heading', { traceName: getTraceName(trace.spans) })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: getTraceName(trace!.spans) })).toBeInTheDocument();
   });
 
   it('should render the header items', () => {
@@ -68,61 +74,53 @@ describe('TracePageHeader test', () => {
 
     expect(headerItems).toHaveLength(5);
     //                                                        Year-month-day hour-minute-second
-    expect(headerItems[0].textContent.match(/Trace Start:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{3}/g)).toBeTruthy();
-    expect(headerItems[1].textContent.match(/Duration:[\d|\.][\.|\d|s][\.|\d|s]?[\d]?/)).toBeTruthy();
-    expect(headerItems[2].textContent.match(/Services:\d\d?/g)).toBeTruthy();
-    expect(headerItems[3].textContent.match(/Depth:\d\d?/)).toBeTruthy();
-    expect(headerItems[4].textContent.match(/Total Spans:\d\d?\d?\d?/)).toBeTruthy();
+    expect(
+      headerItems?.[0].textContent?.match(/Trace Start:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{3}/g)
+    ).toBeTruthy();
+    expect(headerItems?.[1].textContent?.match(/Duration:[\d|\.][\.|\d|s][\.|\d|s]?[\d]?/)).toBeTruthy();
+    expect(headerItems?.[2].textContent?.match(/Services:\d\d?/g)).toBeTruthy();
+    expect(headerItems?.[3].textContent?.match(/Depth:\d\d?/)).toBeTruthy();
+    expect(headerItems?.[4].textContent?.match(/Total Spans:\d\d?\d?\d?/)).toBeTruthy();
   });
 
   it('should render a <SpanGraph>', () => {
     setup();
-
     expect(screen.getByText(/Reset Selection/)).toBeInTheDocument();
   });
 
   describe('observes the visibility toggles for various UX elements', () => {
     it('hides the minimap when hideMap === true', () => {
-      setup({ hideMap: true });
-
+      setup({ hideMap: true } as TracePageHeaderEmbedProps);
       expect(screen.queryByText(/Reset Selection/)).not.toBeInTheDocument();
     });
 
     it('hides the summary when hideSummary === true', () => {
-      const { rerender } = setup({ hideSummary: false });
+      const { rerender } = setup({ hideSummary: false } as TracePageHeaderEmbedProps);
       expect(screen.queryAllByRole('listitem')).toHaveLength(5);
 
-      rerender(<TracePageHeader hideSummary={false} trace={null} />);
+      rerender(<TracePageHeader {...({ hideSummary: false, trace: null } as TracePageHeaderEmbedProps)} />);
       expect(screen.queryAllByRole('listitem')).toHaveLength(0);
 
       rerender(
         <TracePageHeader
-          trace={trace}
-          hideSummary={true}
-          hideMap={false}
-          showArchiveButton={false}
-          showShortcutsHelp={false}
-          showStandaloneLink={false}
-          showViewOptions={false}
-          textFilter=""
-          viewRange={{ time: { current: [10, 20] } }}
-          updateTextFilter={() => {}}
+          {...({
+            trace: trace,
+            hideSummary: true,
+            hideMap: false,
+            viewRange: { time: { current: [10, 20] } },
+          } as TracePageHeaderEmbedProps)}
         />
       );
       expect(screen.queryAllByRole('listitem')).toHaveLength(0);
 
       rerender(
         <TracePageHeader
-          trace={trace}
-          hideSummary={false}
-          hideMap={false}
-          showArchiveButton={false}
-          showShortcutsHelp={false}
-          showStandaloneLink={false}
-          showViewOptions={false}
-          textFilter=""
-          viewRange={{ time: { current: [10, 20] } }}
-          updateTextFilter={() => {}}
+          {...({
+            trace: trace,
+            hideSummary: false,
+            hideMap: false,
+            viewRange: { time: { current: [10, 20] } },
+          } as TracePageHeaderEmbedProps)}
         />
       );
       expect(screen.queryAllByRole('listitem')).toHaveLength(5);
