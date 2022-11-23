c52a65ca2a9ff6ca47c875ce51ad675d619b5d68
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2022-11-23 13:04:46 +0200

    Depend on the interface instead

--- pkg/services/folder/folderimpl/folder.go
+++ pkg/services/folder/folderimpl/folder.go
@@ -34,7 +34,7 @@ type Service struct {
 	dashboardService dashboards.DashboardService
 	dashboardStore   dashboards.Store
 	searchService    *search.SearchService
-	features         *featuremgmt.FeatureManager
+	features         featuremgmt.FeatureToggles
 	permissions      accesscontrol.FolderPermissionsService
 	accessControl    accesscontrol.AccessControl
 
@@ -49,7 +49,7 @@ func ProvideService(
 	dashboardService dashboards.DashboardService,
 	dashboardStore dashboards.Store,
 	db db.DB, // DB for the (new) nested folder store
-	features *featuremgmt.FeatureManager,
+	features featuremgmt.FeatureToggles,
 	folderPermissionsService accesscontrol.FolderPermissionsService,
 	searchService *search.SearchService,
 ) folder.Service {
