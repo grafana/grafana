b9fc5e4be8060fa8e432fa52e986ff19a186f884
Ryan McKinley <ryantxu@gmail.com>
2022-11-29 13:08:23 -0800

    support empty slugs with md5

--- pkg/infra/slugify/slugify.go
+++ pkg/infra/slugify/slugify.go
@@ -7,6 +7,7 @@ package slugify
 
 import (
 	"bytes"
+	"encoding/base64"
 	"strings"
 	"unicode/utf8"
 )
@@ -21,7 +22,18 @@ var (
 
 // Slugify creates the slug for a given value
 func Slugify(value string) string {
-	return simpleSlugger.Slugify(value)
+	s := simpleSlugger.Slugify(value)
+	if s == "" {
+		// If the dashboard name is only characters outside of the
+		// sluggable characters, the slug creation will return an
+		// empty string which will mess up URLs. This failsafe picks
+		// that up and creates the slug as a base64 identifier instead.
+		s = base64.RawURLEncoding.EncodeToString([]byte(value))
+		if len(s) > 64 {
+			s = s[:64]
+		}
+	}
+	return s
 }
 
 func validCharacter(c rune) bool {
--- pkg/infra/slugify/slugify_test.go
+++ pkg/infra/slugify/slugify_test.go
@@ -8,6 +8,7 @@ func TestSlugify(t *testing.T) {
 	results := make(map[string]string)
 	results["hello-playground"] = "Hello, playground"
 	results["hello-it-s-paradise"] = "😢 😣 😤 😥 😦 😧 😨 😩 😪 😫 😬 Hello, it's paradise"
+	results["8J-YoiDwn5ijIPCfmKQg8J-YpSDwn5imIPCfmKcg8J-YqCDwn5ipIPCfmKog8J-Y"] = "😢 😣 😤 😥 😦 😧 😨 😩 😪 😫 😬"
 	results["hi-this-is-a-test"] = "方向盤後面 hi this is a test خلف المقو"
 	results["cong-hoa-xa-hoi-chu-nghia-viet-nam"] = "Cộng hòa xã hội chủ nghĩa Việt Nam"
 	results["noi-nang-canh-canh-ben-long-bieng-khuay"] = "Nỗi nàng canh cánh bên lòng biếng khuây" // This line in a poem called Truyen Kieu
