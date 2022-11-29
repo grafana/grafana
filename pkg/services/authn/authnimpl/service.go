d71efa1bb4a642216cd4016250eb3de0174da64f
Karl Persson <kalle.persson@grafana.com>
2022-11-29 11:17:19 +0100

    Authn: Add tracing

--- pkg/services/authn/authnimpl/service.go
+++ pkg/services/authn/authnimpl/service.go
@@ -3,15 +3,32 @@ package authnimpl
 import (
 	"context"
 
+	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/services/authn"
+	"github.com/grafana/grafana/pkg/setting"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 var _ authn.Service = new(Service)
 
-type Service struct {
-	clients map[string]authn.Client
+func ProvideService(cfg *setting.Cfg, tracer tracing.Tracer) *Service {
+	return &Service{
+		clients: make(map[string]authn.Client),
+		tracer:  tracer,
+	}
 }
 
-func (s Service) Authenticate(ctx context.Context, client string, r *authn.Request) (*authn.Identity, error) {
+type Service struct {
+	clients map[string]authn.Client
+
+	tracer tracing.Tracer
+}
+
+func (s *Service) Authenticate(ctx context.Context, client string, r *authn.Request) (*authn.Identity, error) {
+	ctx, span := s.tracer.Start(ctx, "authn.Authenticate")
+	defer span.End()
+
+	span.SetAttributes("authn.client", client, attribute.Key("authn.client").String(client))
+
 	panic("implement me")
 }
