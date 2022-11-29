c7a53a74de9f27d4d58082ee50311d262c157f96
Karl Persson <kalle.persson@grafana.com>
2022-11-29 13:02:26 +0100

    AuthN: configure anonymous client if enabled in config and add initial implementation of Authenticate function

--- pkg/services/authn/authnimpl/service.go
+++ pkg/services/authn/authnimpl/service.go
@@ -6,32 +6,48 @@ import (
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/services/authn"
+	"github.com/grafana/grafana/pkg/services/authn/clients"
+	"github.com/grafana/grafana/pkg/services/org"
 	"github.com/grafana/grafana/pkg/setting"
 	"go.opentelemetry.io/otel/attribute"
 )
 
 var _ authn.Service = new(Service)
 
-func ProvideService(cfg *setting.Cfg, tracer tracing.Tracer) *Service {
-	return &Service{
+func ProvideService(cfg *setting.Cfg, tracer tracing.Tracer, orgService org.Service) *Service {
+	s := &Service{
 		log:     log.New("authn.service"),
 		clients: make(map[string]authn.Client),
 		tracer:  tracer,
 	}
+
+	if s.cfg.AnonymousEnabled {
+		s.clients[authn.ClientAnonymous] = clients.ProvideAnonymous(cfg, orgService)
+	}
+
+	return s
 }
 
 type Service struct {
 	log     log.Logger
+	cfg     *setting.Cfg
 	clients map[string]authn.Client
 
 	tracer tracing.Tracer
 }
 
-func (s *Service) Authenticate(ctx context.Context, client string, r *authn.Request) (*authn.Identity, error) {
+func (s *Service) Authenticate(ctx context.Context, clientName string, r *authn.Request) (*authn.Identity, error) {
 	ctx, span := s.tracer.Start(ctx, "authn.Authenticate")
 	defer span.End()
 
-	span.SetAttributes("authn.client", client, attribute.Key("authn.client").String(client))
+	span.SetAttributes("authn.client", clientName, attribute.Key("authn.client").String(clientName))
 
-	panic("implement me")
+	client, ok := s.clients[clientName]
+	if !ok {
+		s.log.FromContext(ctx).Warn("auth client not found", "client", clientName)
+		// TODO: handle error
+		return nil, nil
+	}
+
+	return client.Authenticate(ctx, r)
 }
