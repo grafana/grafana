91c2dede6b8ad7ff1aaadf32b3b65212dca8cdd9
Virginia Cepeda <virginia.cepeda@grafana.com>
2022-11-29 17:29:26 -0300

    Display dropdown with valid configs upon error and allow to save them

--- public/app/features/alerting/unified/state/actions.ts
+++ public/app/features/alerting/unified/state/actions.ts
@@ -539,23 +539,31 @@ interface UpdateAlertManagerConfigActionOptions {
   successMessage?: string; // show toast on success
   redirectPath?: string; // where to redirect on success
   refetch?: boolean; // refetch config on success
+  fetchLatestConfig?: boolean; //obtain latest config prior to saving it
 }
 
 export const updateAlertManagerConfigAction = createAsyncThunk<void, UpdateAlertManagerConfigActionOptions, {}>(
   'unifiedalerting/updateAMConfig',
-  ({ alertManagerSourceName, oldConfig, newConfig, successMessage, redirectPath, refetch }, thunkAPI): Promise<void> =>
+  (
+    { alertManagerSourceName, oldConfig, newConfig, successMessage, redirectPath, refetch, fetchLatestConfig = true },
+    thunkAPI
+  ): Promise<void> =>
     withAppEvents(
       withSerializedError(
         (async () => {
-          const latestConfig = await thunkAPI.dispatch(fetchAlertManagerConfigAction(alertManagerSourceName)).unwrap();
+          if (fetchLatestConfig) {
+            const latestConfig = await thunkAPI
+              .dispatch(fetchAlertManagerConfigAction(alertManagerSourceName))
+              .unwrap();
 
-          if (
-            !(isEmpty(latestConfig.alertmanager_config) && isEmpty(latestConfig.template_files)) &&
-            JSON.stringify(latestConfig) !== JSON.stringify(oldConfig)
-          ) {
-            throw new Error(
-              'It seems configuration has been recently updated. Please reload page and try again to make sure that recent changes are not overwritten.'
-            );
+            if (
+              !(isEmpty(latestConfig.alertmanager_config) && isEmpty(latestConfig.template_files)) &&
+              JSON.stringify(latestConfig) !== JSON.stringify(oldConfig)
+            ) {
+              throw new Error(
+                'It seems configuration has been recently updated. Please reload page and try again to make sure that recent changes are not overwritten.'
+              );
+            }
           }
           await updateAlertManagerConfig(alertManagerSourceName, addDefaultsToAlertmanagerConfig(newConfig));
           if (refetch) {
