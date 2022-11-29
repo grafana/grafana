113de2d719870f9cbfae91593a51f7f33bab135a
Karl Persson <kalle.persson@grafana.com>
2022-11-29 14:32:02 +0100

    AuthN: Add FIXME comment

--- pkg/services/authn/authnimpl/service.go
+++ pkg/services/authn/authnimpl/service.go
@@ -45,8 +45,17 @@ func (s *Service) Authenticate(ctx context.Context, clientName string, r *authn.
 	client, ok := s.clients[clientName]
 	if !ok {
 		s.log.FromContext(ctx).Warn("auth client not found", "client", clientName)
+		span.AddEvents([]string{"message"}, []tracing.EventValue{{Str: "auth client is not configured"}})
 		return nil, authn.ErrClientNotFound
 	}
+
+	// FIXME: We want to perform common authentication operations here.
+	// We will add them as we start to implement clients that requires them.
+	// Those operations can be Syncing user, syncing teams, create a session etc.
+	// We would need to check what operations a client support and also if they are requested
+	// because for e.g. basic auth we want to create a session if the call is coming from the
+	// login handler, but if we want to perform basic auth during a request (called from contexthandler) we don't
+	// want a session to be created.
 
 	return client.Authenticate(ctx, r)
 }
