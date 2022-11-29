f07a2a38dbcec47e1ca4821a039e78ec6592a47a
Karl Persson <kalle.persson@grafana.com>
2022-11-29 11:08:34 +0100

    Authn: Add Client interface and Reqeust and Identity structures

--- pkg/services/authn/authn.go
+++ pkg/services/authn/authn.go
@@ -1,4 +1,21 @@
 package authn
 
+import (
+	"context"
+	"net/http"
+)
+
 type Service interface {
+	Authenticate(ctx context.Context, client string, r *Request) (*Identity, error)
+}
+
+type Client interface {
+	Authenticate(ctx context.Context, r *Request) (*Identity, error)
+}
+
+type Request struct {
+	HTTPRequest *http.Request
+}
+
+type Identity struct {
 }
