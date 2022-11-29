6d66a9870ddb5a083b614934951b6963dd37419d
Matias Chomicki <matyax@gmail.com>
2022-11-29 17:20:58 +0100

    feat(loki-query-validation): add support for multi-line queries

--- public/app/plugins/datasource/loki/components/monaco-query-field/monaco-completion-provider/validation.ts
+++ public/app/plugins/datasource/loki/components/monaco-query-field/monaco-completion-provider/validation.ts
@@ -4,8 +4,20 @@ import { parser } from '@grafana/lezer-logql';
 import { monacoTypes } from '@grafana/ui';
 import { ErrorId } from 'app/plugins/datasource/prometheus/querybuilder/shared/parsingUtils';
 
-export function validateQuery(model: monacoTypes.editor.ITextModel) {
+interface ParserErrorBoundary {
+  startLineNumber: number;
+  startColumn: number;
+  endLineNumber: number;
+  endColumn: number;
+}
+
+function isErrorBoundary(boundary: ParserErrorBoundary | null): boundary is ParserErrorBoundary {
+  return boundary !== null;
+}
+
+export function validateQuery(model: monacoTypes.editor.ITextModel): ParserErrorBoundary[] | false {
   const query = model.getValue();
+  const lines = model.getLinesContent();
 
   if (!query) {
     return false;
@@ -26,13 +38,38 @@ export function validateQuery(model: monacoTypes.editor.ITextModel) {
     return false;
   }
 
-  return errorNodes.map((node: SyntaxNode) => {
-    const parent = node.parent || node;
+  return errorNodes.map((node) => findErrorBoundary(query, lines, node)).filter(isErrorBoundary);
+}
+
+function findErrorBoundary(query: string, queryLines: string[], node: SyntaxNode): ParserErrorBoundary | null {
+  if (queryLines.length === 1) {
     return {
       startLineNumber: 1,
-      startColumn: parent.from,
-      endLineNumber: model.getLineCount(),
-      endColumn: parent.to,
+      startColumn: node.from,
+      endLineNumber: 1,
+      endColumn: node.to,
     };
-  });
+  }
+
+  const errorNode = node.from === node.to && node.parent ? node.parent : node;
+
+  let startPos = 0,
+    endPos = 0;
+  for (let line = 0; line < queryLines.length; line++) {
+    endPos = startPos + queryLines[line].length;
+
+    if (errorNode.from > endPos) {
+      startPos += queryLines[line].length + 1;
+      continue;
+    }
+
+    return {
+      startLineNumber: line + 1,
+      startColumn: errorNode.from - startPos + 1,
+      endLineNumber: line + 1,
+      endColumn: errorNode.to - startPos + 1,
+    };
+  }
+
+  return null;
 }
