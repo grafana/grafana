1add194f875e9013e20ad61a8ed80c3b958a7d98
Karl Persson <kalle.persson@grafana.com>
2022-11-29 11:09:56 +0100

    Authn: Implement Authenticate method in service

--- pkg/services/authn/authnimpl/service.go
+++ pkg/services/authn/authnimpl/service.go
@@ -1,8 +1,17 @@
 package authnimpl
 
-import "github.com/grafana/grafana/pkg/services/authn"
+import (
+	"context"
+
+	"github.com/grafana/grafana/pkg/services/authn"
+)
 
 var _ authn.Service = new(Service)
 
 type Service struct {
+	clients map[string]authn.Client
+}
+
+func (s Service) Authenticate(ctx context.Context, client string, r *authn.Request) (*authn.Identity, error) {
+	panic("implement me")
 }
