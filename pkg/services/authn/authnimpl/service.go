7197d05d947f28723610319d0ce31c69cf5edb40
Karl Persson <kalle.persson@grafana.com>
2022-11-29 11:22:27 +0100

    Authn: Add logger

--- pkg/services/authn/authnimpl/service.go
+++ pkg/services/authn/authnimpl/service.go
@@ -3,6 +3,7 @@ package authnimpl
 import (
 	"context"
 
+	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/services/authn"
 	"github.com/grafana/grafana/pkg/setting"
@@ -13,12 +14,14 @@ var _ authn.Service = new(Service)
 
 func ProvideService(cfg *setting.Cfg, tracer tracing.Tracer) *Service {
 	return &Service{
+		log:     log.New("authn.service"),
 		clients: make(map[string]authn.Client),
 		tracer:  tracer,
 	}
 }
 
 type Service struct {
+	log     log.Logger
 	clients map[string]authn.Client
 
 	tracer tracing.Tracer
