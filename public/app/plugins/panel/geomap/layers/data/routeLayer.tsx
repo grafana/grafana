c7a28a51e92bbe69c8068b5cfc2e34ff87be691a
drew08t <drew08@gmail.com>
2022-12-01 12:24:03 -0800

    Add arrow support and simplify color functions

--- public/app/plugins/panel/geomap/layers/data/routeLayer.tsx
+++ public/app/plugins/panel/geomap/layers/data/routeLayer.tsx
@@ -30,10 +30,12 @@ import Feature from 'ol/Feature';
 import { alpha } from '@grafana/data/src/themes/colorManipulator';
 import { LineString, SimpleGeometry } from 'ol/geom';
 import FlowLine from 'ol-ext/style/FlowLine';
+import tinycolor from 'tinycolor2';
 
 // Configuration options for Circle overlays
 export interface RouteConfig {
   style: StyleConfig;
+  arrow?: number;
 }
 
 const defaultOptions: RouteConfig = {
@@ -42,6 +44,7 @@ const defaultOptions: RouteConfig = {
     opacity: 1,
     lineWidth: 2,
   },
+  arrow: 0,
 };
 
 export const ROUTE_LAYER_ID = 'route';
@@ -99,26 +102,32 @@ export const routeLayer: MapLayerRegistryItem<RouteConfig> = {
         const styles = [];
         const geom = feature.getGeometry();
 
-        let opacityString: string;
-        const opacityInt = Math.floor((style.config.opacity ?? 1) * 255);
-        opacityString = componentToHex(opacityInt);
-
         if (geom instanceof SimpleGeometry && dims.color) {
           const coordinates = geom.getCoordinates();
           if (coordinates) {
             for (let i = 0; i < coordinates.length - 1; i++) {
-              const color1 = dims.color.get(i);
-              const color2 = dims.color.get(i + 1);
-              const color1Hex = colorToHex(colorValues(color1));
-              const color2Hex = colorToHex(colorValues(color2));
+              const color1 = tinycolor(theme.visualization.getColorByName(dims.color.get(i)))
+                .setAlpha(style.config.opacity ?? 1)
+                .toString();
+              const color2 = tinycolor(theme.visualization.getColorByName(dims.color.get(i + 1)))
+                .setAlpha(style.config.opacity ?? 1)
+                .toString();
 
               const flowStyle = new FlowLine({
                 visible: true,
                 lineCap: 'round',
-                color: color1Hex + opacityString,
-                color2: color2Hex + opacityString,
+                color: color1,
+                color2: color2,
                 width: style.config.lineWidth,
               });
+              if (config.arrow) {
+                flowStyle.setArrow(config.arrow);
+                if (config.arrow > 0) {
+                  flowStyle.setArrowColor(color2);
+                } else {
+                  flowStyle.setArrowColor(color1);
+                }
+              }
               const LS = new LineString([coordinates[i], coordinates[i + 1]]);
               flowStyle.setGeometry(LS);
               styles.push(flowStyle);
@@ -232,6 +241,18 @@ export const routeLayer: MapLayerRegistryItem<RouteConfig> = {
             },
             defaultValue: defaultOptions.style,
           })
+          .addRadio({
+            path: 'config.arrow',
+            name: 'Arrow',
+            settings: {
+              options: [
+                { label: 'None', value: 0 },
+                { label: 'Forward', value: 1 },
+                { label: 'Reverse', value: -1 },
+              ],
+            },
+            defaultValue: defaultOptions.arrow,
+          })
           .addSliderInput({
             path: 'config.style.lineWidth',
             name: 'Line width',
@@ -279,65 +300,4 @@ function findNearestTimeIndex(timestamps: number[], time: number): number | null
     }
     return lastIdx;
   }
-}
-
-// Return array of [r,g,b,a] from any valid color. If failed, returns transparent
-function colorValues(color: string) {
-  if (color === '') return [0, 0, 0, 0];
-  if (color.toLowerCase() === 'transparent') return [0, 0, 0, 0];
-  // Hex color
-  if (color[0] === '#') {
-    if (color.length < 7) {
-      // convert #RGB and #RGBA to #RRGGBB and #RRGGBBAA
-      color =
-        '#' +
-        color[1] +
-        color[1] +
-        color[2] +
-        color[2] +
-        color[3] +
-        color[3] +
-        (color.length > 4 ? color[4] + color[4] : '');
-    }
-    return [
-      parseInt(color.substr(1, 2), 16),
-      parseInt(color.substr(3, 2), 16),
-      parseInt(color.substr(5, 2), 16),
-      color.length > 7 ? parseInt(color.substr(7, 2), 16) / 255 : 1,
-    ];
-  }
-  // Named colors
-  // TODO find a way to interpret named colors without creating an element
-  if (color.indexOf('rgb') === -1) {
-    const temp_elem = document.body.appendChild(document.createElement('fictum')); // intentionally use unknown tag to lower chances of css rule override with !important
-    const flag = 'rgb(1, 2, 3)';
-    temp_elem.style.color = flag;
-    if (temp_elem.style.color !== flag) return [0, 0, 0, 0];
-    temp_elem.style.color = color;
-    if (temp_elem.style.color === flag || temp_elem.style.color === '') return [0, 0, 0, 0];
-    color = getComputedStyle(temp_elem).color;
-    document.body.removeChild(temp_elem);
-  }
-  // RGB colors
-  if (color.indexOf('rgb') === 0) {
-    if (color.indexOf('rgba') === -1) color += ',1'; // convert 'rgb(R,G,B)' to 'rgb(R,G,B)A'
-    return color.match(/[\.\d]+/g)!.map(function (a) {
-      return +a;
-    });
-  }
-  return [0, 0, 0, 0];
-}
-
-function componentToHex(c: number) {
-  const hex = c.toString(16);
-  return hex.length == 1 ? '0' + hex : hex;
-}
-
-function colorToHex(c: number[] | undefined) {
-  let hex: string = '';
-  if (c && c.length > 2) {
-    hex = '#' + componentToHex(c[0]) + componentToHex(c[1]) + componentToHex(c[2]);
-    hex = hex.toLocaleUpperCase();
-  }
-  return hex;
 }
