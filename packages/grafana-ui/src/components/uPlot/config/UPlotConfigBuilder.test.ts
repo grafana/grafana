5e3442032afc80b71161051346f0b8102d830d85
Leon Sorokin <leeoniya@gmail.com>
2022-12-01 17:01:35 -0600

    TimeSeries: Fix rendering when zooming to time ranges between datapoints (#59444)
    (cherry picked from commit e1e858323a41d3c1f157002ac3c720287006d3a2)

--- packages/grafana-ui/src/components/uPlot/config/UPlotConfigBuilder.test.ts
+++ packages/grafana-ui/src/components/uPlot/config/UPlotConfigBuilder.test.ts
@@ -613,7 +613,6 @@ describe('UPlotConfigBuilder', () => {
           Object {
             "facets": undefined,
             "fill": [Function],
-            "gaps": [Function],
             "paths": [Function],
             "points": Object {
               "fill": "#0000ff",
@@ -741,7 +740,6 @@ describe('UPlotConfigBuilder', () => {
             Object {
               "facets": undefined,
               "fill": [Function],
-              "gaps": [Function],
               "paths": [Function],
               "points": Object {
                 "fill": "#0000ff",
@@ -760,7 +758,6 @@ describe('UPlotConfigBuilder', () => {
             Object {
               "facets": undefined,
               "fill": [Function],
-              "gaps": [Function],
               "paths": [Function],
               "points": Object {
                 "fill": "#00ff00",
@@ -779,7 +776,6 @@ describe('UPlotConfigBuilder', () => {
             Object {
               "facets": undefined,
               "fill": [Function],
-              "gaps": [Function],
               "paths": [Function],
               "points": Object {
                 "fill": "#ff0000",
