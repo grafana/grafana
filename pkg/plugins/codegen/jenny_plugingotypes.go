523971fc93cb8f4146091d5aba57c3b7143a8414
Marcus Andersson <marcus.andersson@grafana.com>
2022-12-01 11:20:54 +0100

    removed the pre-pended header in the gotypes jenny since it is done in the postprocess.

--- pkg/plugins/codegen/jenny_plugingotypes.go
+++ pkg/plugins/codegen/jenny_plugingotypes.go
@@ -1,7 +1,6 @@
 package codegen
 
 import (
-	"bytes"
 	"fmt"
 	"path/filepath"
 	"strings"
@@ -32,24 +31,10 @@ func (j *pgoJenny) Generate(decl *pfs.PluginDecl) (*codejen.File, error) {
 		return nil, nil
 	}
 
-	buf := new(bytes.Buffer)
-
-	if err := tmpls.Lookup("autogen_header.tmpl").Execute(buf, templateVars_autogen_header{
-		GeneratorPath:  "public/app/plugins/gen.go", // FIXME hardcoding is not OK
-		LineagePath:    filepath.ToSlash(filepath.Join(decl.PluginPath, "models.cue")),
-		LineageCUEPath: decl.Slot.Name(),
-		GenLicense:     true,
-	}); err != nil {
-		return nil, fmt.Errorf("error generating file header: %w", err)
-	}
-
 	f, err := j.inner.Generate(decl)
 	if err != nil {
 		return nil, err
 	}
-
-	fmt.Fprint(buf, f.Data)
-	f.Data = buf.Bytes()
 
 	pluginfolder := filepath.Base(decl.PluginPath)
 	slotname := strings.ToLower(decl.Slot.Name())
