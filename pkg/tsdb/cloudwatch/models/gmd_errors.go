96b5a5e8852283151431cf7482251f1bdf211e72
Erik Sundell <erik.sundell87@gmail.com>
2022-12-02 09:36:42 +0100

    move error messages to models

--- pkg/tsdb/cloudwatch/models/gmd_errors.go
+++ pkg/tsdb/cloudwatch/models/gmd_errors.go
@@ -6,3 +6,10 @@ const (
 	MaxQueryResultsExceeded    = "MaxQueryResultsExceeded"
 	MaxMatchingResultsExceeded = "MaxMatchingResultsExceeded"
 )
+
+var ErrorMessages = map[string]string{
+	MaxMetricsExceeded:         "Maximum number of allowed metrics exceeded. Your search may have been limited",
+	MaxQueryTimeRangeExceeded:  "Max time window exceeded for query",
+	MaxQueryResultsExceeded:    "Only the first 500 time series can be returned by a query.",
+	MaxMatchingResultsExceeded: "The query matched more than 10.000 metrics, results might not be accurate.",
+}
