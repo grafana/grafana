2d43978dee43977c766846d4f7911d2f13ea03df
joshhunt <josh@trtr.co>
2022-12-02 15:38:53 +0000

    Add no-aria-label-e2e-selectors to betterer

--- .betterer.ts
+++ .betterer.ts
@@ -46,6 +46,7 @@ function countEslintErrors() {
 
     const baseRules: Partial<Linter.RulesRecord> = {
       '@typescript-eslint/no-explicit-any': 'error',
+      '@grafana/eslint-rules/no-aria-label-selectors': 'error',
     };
 
     const nonTestFilesRules: Partial<Linter.RulesRecord> = {
