f02958fc0956947d0410da9465d67d23dc070de3
joshhunt <josh@trtr.co>
2022-12-05 17:34:34 +0000

    update todo tests:

--- public/app/core/components/TimePicker/TimePickerWithHistory.test.tsx
+++ public/app/core/components/TimePicker/TimePickerWithHistory.test.tsx
@@ -2,4 +2,7 @@ describe('TimePickerWithHistory', () => {
   it.todo('Should load with no history');
   it.todo('Should load with old TimeRange history');
   it.todo('Should load with new TimePickerHistoryItem history');
+
+  it.todo('Should not save duplicates');
+  it.todo('Should show 4 most recently used time ranges');
 });
