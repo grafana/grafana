7242feb9a8b4d1698eb163f1658dadbd7be04621
joshhunt <josh@trtr.co>
2022-12-05 15:29:48 +0000

    Refactor time picker history to store simpler json objects

---
+++ public/app/core/components/TimePicker/TimePickerWithHistory.test.tsx
@@ -0,0 +1,5 @@
+describe('TimePickerWithHistory', () => {
+  it.todo('Should load with no history');
+  it.todo('Should load with old TimeRange history');
+  it.todo('Should load with new TimePickerHistoryItem history');
+});
