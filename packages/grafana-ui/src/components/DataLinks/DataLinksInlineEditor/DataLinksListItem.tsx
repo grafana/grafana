27db9e76dae3d146df73c6f23035b3da88ba1020
Dominik Prokop <dominik.prokop@grafana.com>
2022-12-05 15:33:34 +0100

    Fix data links edit icons being off screen

--- packages/grafana-ui/src/components/DataLinks/DataLinksInlineEditor/DataLinksListItem.tsx
+++ packages/grafana-ui/src/components/DataLinks/DataLinksInlineEditor/DataLinksListItem.tsx
@@ -31,26 +31,24 @@ export const DataLinksListItem: FC<DataLinksListItemProps> = ({ link, onEdit, on
 
   return (
     <div className={styles.wrapper}>
-      <VerticalGroup spacing="xs">
-        <HorizontalGroup justify="space-between" align="flex-start" width="100%">
-          <div className={cx(styles.url, !hasUrl && styles.notConfigured, isCompactExploreUrl && styles.errored)}>
-            {hasTitle ? title : 'Data link title not provided'}
-          </div>
-          <HorizontalGroup>
-            <IconButton name="pen" onClick={onEdit} />
-            <IconButton name="times" onClick={onRemove} />
-          </HorizontalGroup>
-        </HorizontalGroup>
-        <div
-          className={cx(styles.url, !hasUrl && styles.notConfigured, isCompactExploreUrl && styles.errored)}
-          title={url}
-        >
-          {hasUrl ? url : 'Data link url not provided'}
+      <div className={styles.titleWrapper}>
+        <div className={cx(styles.url, !hasUrl && styles.notConfigured, isCompactExploreUrl && styles.errored)}>
+          {hasTitle ? title : 'Data link title not provided'}
         </div>
-        {isCompactExploreUrl && (
-          <FieldValidationMessage>Explore data link may not work in the future. Please edit.</FieldValidationMessage>
-        )}
-      </VerticalGroup>
+        <div className={styles.actionButtons}>
+          <IconButton name="pen" onClick={onEdit} />
+          <IconButton name="times" onClick={onRemove} />
+        </div>
+      </div>
+      <div
+        className={cx(styles.url, !hasUrl && styles.notConfigured, isCompactExploreUrl && styles.errored)}
+        title={url}
+      >
+        {hasUrl ? url : 'Data link url not provided'}
+      </div>
+      {isCompactExploreUrl && (
+        <FieldValidationMessage>Explore data link may not work in the future. Please edit.</FieldValidationMessage>
+      )}
     </div>
   );
 };
@@ -63,6 +61,19 @@ const getDataLinkListItemStyles = stylesFactory((theme: GrafanaTheme2) => {
       &:last-child {
         margin-bottom: 0;
       }
+      display: flex;
+      flex-direction: column;
+    `,
+    titleWrapper: css`
+      label: data-links-list-item-title;
+      justify-content: space-between;
+      display: flex;
+      width: 100%;
+      align-items: center;
+    `,
+    actionButtons: css`
+      margin-left: 8px;
+      display: flex;
     `,
     errored: css`
       color: ${theme.colors.error.text};
