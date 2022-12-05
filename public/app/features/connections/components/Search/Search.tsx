3c934a85c39242e5f7ad07958a49fbe7bb2b3198
Marcus Andersson <marcus.andersson@grafana.com>
2022-12-05 15:14:35 +0100

    adding debounce.

--- public/app/features/connections/components/Search/Search.tsx
+++ public/app/features/connections/components/Search/Search.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/css';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { useDebounce } from 'react-use';
 
 import { Icon, Input, useStyles2 } from '@grafana/ui';
 
@@ -13,10 +14,27 @@ const getStyles = () => ({
 
 export const Search: FC<{ onChange: (e: React.FormEvent<HTMLInputElement>) => void }> = ({ onChange }) => {
   const styles = useStyles2(getStyles);
+  const [inputEvent, setInputEvent] = useState<React.FormEvent<HTMLInputElement> | undefined>();
+
+  useDebounce(
+    () => {
+      if (!inputEvent) {
+        return;
+      }
+      onChange(inputEvent);
+    },
+    500,
+    [inputEvent]
+  );
 
   return (
     <div className={styles.searchContainer}>
-      <Input onChange={onChange} prefix={<Icon name="search" />} placeholder="Search all" aria-label="Search all" />
+      <Input
+        onChange={setInputEvent}
+        prefix={<Icon name="search" />}
+        placeholder="Search all"
+        aria-label="Search all"
+      />
     </div>
   );
 };
