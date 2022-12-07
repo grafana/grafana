14da76fb31e93278b920f45b4531aac6c72d77d3
bergquist <carl.bergquist@gmail.com>
2022-12-07 09:04:49 +0100

    contexthandler: add uname as response header
    Signed-off-by: bergquist <carl.bergquist@gmail.com>

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -10,6 +10,11 @@ var (
 	// Register each toggle here
 	standardFeatureFlags = []FeatureFlag{
 		{
+			Name:        "returnUnameHeader",
+			Description: "Return user login as header for authenticated requests",
+			State:       FeatureStateAlpha,
+		},
+		{
 			Name:        "alertingBigTransactions",
 			Description: "Use big transactions for alerting database writes",
 			State:       FeatureStateAlpha,
