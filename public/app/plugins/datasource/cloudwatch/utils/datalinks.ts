88d69f886ed9fd510d651071b13dd29623cdb53b
Shirley <4163034+fridgepoet@users.noreply.github.com>
2022-12-07 17:18:40 +0100

    Update public/app/plugins/datasource/cloudwatch/utils/datalinks.ts
    Co-authored-by: Erik Sundell <erik.sundell87@gmail.com>

--- public/app/plugins/datasource/cloudwatch/utils/datalinks.ts
+++ public/app/plugins/datasource/cloudwatch/utils/datalinks.ts
@@ -74,7 +74,7 @@ function createAwsConsoleLink(
     .filter((group) => group?.value)
     .map((group) => (group.value ?? '').replace(/:\*$/, '')); // remove `:*` from end of arn
   const logGroupNames = target.logGroupNames;
-  let sources = arns?.length ? arns : logGroupNames;
+  const sources = arns?.length ? arns : logGroupNames;
   const interpolatedExpression = target.expression ? replace(target.expression) : '';
   const interpolatedGroups = sources?.flatMap(getVariableValue) ?? [];
 
