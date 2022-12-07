08f67c18ae4eb437fa53454f8287b550d2ac35c3
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2022-12-07 12:25:49 +0000

    update user conflict resolution tool

--- pkg/cmd/grafana-cli/commands/conflict_user_command.go
+++ pkg/cmd/grafana-cli/commands/conflict_user_command.go
@@ -61,7 +61,7 @@ func initializeConflictResolver(cmd *utils.ContextCommandLine, f Formatter, ctx 
 		return nil, fmt.Errorf("%v: %w", "failed to get users with conflicting logins", err)
 	}
 	quotaService := quotaimpl.ProvideService(s, cfg)
-	userService, err := userimpl.ProvideService(s, nil, cfg, nil, nil, quotaService)
+	userService, err := userimpl.ProvideService(s, nil, cfg, nil, nil, quotaService, nil, nil)
 	if err != nil {
 		return nil, fmt.Errorf("%v: %w", "failed to get user service", err)
 	}
