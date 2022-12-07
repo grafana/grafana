a85864e73190d5e40aafd38acf92bcc8a13b384b
Esteban Beltran <esteban.beltran@elastic.co>
2022-12-07 14:06:47 +0100

    Improved betterer

--- .betterer.ts
+++ .betterer.ts
@@ -1,34 +1,82 @@
 import { regexp } from '@betterer/regexp';
 import { BettererFileTest } from '@betterer/betterer';
 import { ESLint, Linter } from 'eslint';
-import { existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
+import path from 'path';
 
 export default {
-  'no enzyme tests': () => regexp(/from 'enzyme'/g).include('**/*.test.*'),
+  'no enzyme tests': () => countEnzymeTests().include('**/*.test.*'),
   'better eslint': () => countEslintErrors().include('**/*.{ts,tsx}'),
   'no undocumented stories': () => countUndocumentedStories().include('**/*.story.tsx'),
 };
 
-function countUndocumentedStories() {
+function countEnzymeTests() {
+  const cwd = process.cwd();
+  let found = false;
   return new BettererFileTest(async (filePaths, fileTestResult) => {
     filePaths.forEach((filePath) => {
+      if (!isPathRelativeTo(cwd, filePath)) {
+        return;
+      }
+      if (filePath.match(/\.test\.(ts|tsx)$/)) {
+        // get file contents
+        const fileContents = readFileSync(filePath, 'utf8');
+        if (fileContents.match(/from 'enzyme'/)) {
+          const file = fileTestResult.addFile(filePath, '');
+          // Add the issue to the first character of the file:
+          file.addIssue(0, 0, 'No enzyme tests are allowed, please use react-testing-library');
+          found = true;
+        }
+      }
+    });
+    if (!found) {
+      fileTestResult
+        .addFile(path.resolve(cwd, 'keep-empty'), '')
+        .addIssue(0, 0, 'fake error to prevent betterer report from blowing up');
+    }
+  });
+}
+
+function countUndocumentedStories() {
+  const cwd = process.cwd();
+  let found = false;
+  return new BettererFileTest(async (filePaths, fileTestResult) => {
+    filePaths.forEach((filePath) => {
+      if (!isPathRelativeTo(cwd, filePath)) {
+        return;
+      }
       if (!existsSync(filePath.replace(/\.story.tsx$/, '.mdx'))) {
+        found = true;
         // In this case the file contents don't matter:
         const file = fileTestResult.addFile(filePath, '');
         // Add the issue to the first character of the file:
         file.addIssue(0, 0, 'No undocumented stories are allowed, please add an .mdx file with some documentation');
       }
     });
+    if (!found) {
+      fileTestResult
+        .addFile(path.resolve(cwd, 'keep-empty'), '')
+        .addIssue(0, 0, 'fake error to prevent betterer report from blowing up');
+    }
   });
 }
 
 function countEslintErrors() {
   return new BettererFileTest(async (filePaths, fileTestResult, resolver) => {
     const { baseDirectory } = resolver;
+    const cwd = process.cwd();
     const cli = new ESLint({ cwd: baseDirectory });
+    let found = false;
+
+    fileTestResult
+      .addFile(path.resolve(cwd, 'keep-empty'), '')
+      .addIssue(0, 0, 'fake error to prevent betterer report from blowing up');
 
     await Promise.all(
       filePaths.map(async (filePath) => {
+        if (!isPathRelativeTo(cwd, filePath)) {
+          return;
+        }
         const linterOptions = (await cli.calculateConfigForFile(filePath)) as Linter.Config;
 
         const rules: Partial<Linter.RulesRecord> = {
@@ -66,10 +114,21 @@ function countEslintErrors() {
             const { messages } = lintResult;
             const file = fileTestResult.addFile(filePath, '');
             messages.forEach((message, index) => {
+              found = true;
               file.addIssue(0, 0, message.message, `${index}`);
             });
           });
+        if (!found) {
+          fileTestResult
+            .addFile(path.resolve(cwd, 'keep-empty'), '')
+            .addIssue(0, 0, 'fake error to prevent betterer report from blowing up');
+        }
       })
     );
   });
+}
+
+function isPathRelativeTo(parent: string, dir: string) {
+  const relative = path.relative(parent, dir);
+  return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
 }
--- scripts/cli/reportBettererStats.ts
+++ scripts/cli/reportBettererStats.ts
@@ -1,14 +1,38 @@
 import { betterer } from '@betterer/betterer';
+import fs from 'fs';
 import { camelCase } from 'lodash';
+import path from 'path';
 
 function logStat(name: string, value: number) {
   // Note that this output format must match the parsing in ci-frontend-metrics.sh
   // which expects the two values to be separated by a space
-  console.log(`${name} ${value}`);
+  console.log(`  - ${name} ${value}`);
+}
+
+function findConfigFile() {
+  let configPath = path.resolve('.betterer.ts');
+  if (fs.existsSync(configPath)) {
+    return configPath;
+  }
+  configPath = path.resolve(__dirname, '../../', '.betterer.ts');
+  return configPath;
 }
 
 async function main() {
-  const results = await betterer.results();
+  const cwd = process.cwd();
+  let projectName = path.basename(cwd);
+  try {
+    const packageJsonPath = path.resolve(cwd, 'package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    projectName = packageJson.name;
+  } catch (e) {}
+  console.log(`Betterer stats for ${projectName}`);
+  const configFile = findConfigFile();
+  const results = await betterer.results({
+    configPaths: configFile,
+    cwd: cwd,
+    resultsPath: './.betterer.results',
+  });
 
   for (const testResults of results.resultSummaries) {
     const countByMessage = {};
