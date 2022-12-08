e5949e4112e4802392d8e8eafab303e181a0b0ba
polinaboneva <polina.boneva@grafana.com>
2022-12-08 14:03:22 +0200

    notices have their own square space in the size of the panel header

--- public/app/features/dashboard/dashgrid/PanelHeader/PanelHeaderNotice.tsx
+++ public/app/features/dashboard/dashgrid/PanelHeader/PanelHeaderNotice.tsx
@@ -1,28 +1,47 @@
-import React, { FC } from 'react';
+import { css } from '@emotion/css';
+import React from 'react';
 
-import { QueryResultMetaNotice } from '@grafana/data';
-import { Icon, Tooltip } from '@grafana/ui';
+import { GrafanaTheme2, QueryResultMetaNotice } from '@grafana/data';
+import { Icon, Tooltip, useStyles2 } from '@grafana/ui';
 
 interface Props {
   notice: QueryResultMetaNotice;
   onClick: (e: React.SyntheticEvent, tab: string) => void;
 }
 
-export const PanelHeaderNotice: FC<Props> = ({ notice, onClick }) => {
+export function PanelHeaderNotice({ notice, onClick }: Props) {
+  const styles = useStyles2(getStyles);
+
   const iconName =
     notice.severity === 'error' || notice.severity === 'warning' ? 'exclamation-triangle' : 'info-circle';
 
   return (
     <Tooltip content={notice.text} key={notice.severity}>
       {notice.inspect ? (
-        <div className="panel-info-notice pointer" onClick={(e) => onClick(e, notice.inspect!)}>
-          <Icon name={iconName} style={{ marginRight: '8px' }} />
+        <div className={styles.notice} onClick={(e) => onClick(e, notice.inspect!)}>
+          <Icon name={iconName} />
         </div>
       ) : (
-        <a className="panel-info-notice" href={notice.link} target="_blank" rel="noreferrer">
-          <Icon name={iconName} style={{ marginRight: '8px' }} />
+        <a className={styles.notice} href={notice.link} target="_blank" rel="noreferrer">
+          <Icon name={iconName} />
         </a>
       )}
     </Tooltip>
   );
+}
+
+const getStyles = (theme: GrafanaTheme2) => {
+  const { headerHeight } = theme.components.panel;
+
+  return {
+    notice: css({
+      label: 'panel-info-notice',
+      width: theme.spacing(headerHeight),
+      height: theme.spacing(headerHeight),
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      cursor: 'pointer',
+    }),
+  };
 };
