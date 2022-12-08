b151e0bb946e8aa1337956004af4aa8caa39c7ac
Ryan McKinley <ryantxu@gmail.com>
2022-12-08 10:20:15 -0800

    update workflows

--- pkg/services/store/service.go
+++ pkg/services/store/service.go
@@ -592,7 +592,6 @@ func (s *standardStorageService) getWorkflowOptions(ctx context.Context, user *u
 		Workflows: make([]workflowInfo, 0),
 	}
 
-	//scope, _ := splitFirstSegment(path)
 	root, _ := s.tree.getRoot(user.OrgID, path)
 	if root == nil {
 		return options, fmt.Errorf("can not read")
