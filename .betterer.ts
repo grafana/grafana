0475f5e47edeeb09e49169e4babdb0c14db84bb4
Esteban Beltran <esteban.beltran@elastic.co>
2022-12-08 12:49:58 +0100

    Linting: Speed up betterer by sharing eslint instances by groups

--- .betterer.ts
+++ .betterer.ts
@@ -2,7 +2,8 @@ import { regexp } from '@betterer/regexp';
 import { BettererFileTest } from '@betterer/betterer';
 import { ESLint, Linter } from 'eslint';
 import { existsSync } from 'fs';
-
+import { exec } from 'child_process';
+import path from 'path';
 export default {
   'no enzyme tests': () => regexp(/from 'enzyme'/g).include('**/*.test.*'),
   'better eslint': () => countEslintErrors().include('**/*.{ts,tsx}'),
@@ -22,54 +23,91 @@ function countUndocumentedStories() {
   });
 }
 
+async function findEslintConfigFiles(): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    const findEslintFiles = exec("find . -iname '.eslintrc*' -not -path '*/node_modules/*'", (err, stdout) => {
+      if (err) {
+        reject(err);
+      }
+      const files = stdout.split('\n').filter((file) => !!file.trim());
+      resolve(files);
+    });
+  });
+}
+
 function countEslintErrors() {
   return new BettererFileTest(async (filePaths, fileTestResult, resolver) => {
     const { baseDirectory } = resolver;
     const cli = new ESLint({ cwd: baseDirectory });
 
-    await Promise.all(
-      filePaths.map(async (filePath) => {
-        const linterOptions = (await cli.calculateConfigForFile(filePath)) as Linter.Config;
+    const testFiles: string[] = [];
+    const codeFiles: string[] = [];
 
-        const rules: Partial<Linter.RulesRecord> = {
-          '@typescript-eslint/no-explicit-any': 'error',
-        };
+    const eslintConfigFiles = await findEslintConfigFiles();
+    const eslintConfigMainPaths = eslintConfigFiles.map((file) => path.resolve(path.dirname(file)));
 
-        const isTestFile =
-          filePath.endsWith('.test.tsx') ||
-          filePath.endsWith('.test.ts') ||
-          filePath.includes('__mocks__') ||
-          filePath.includes('public/test/');
+    filePaths.forEach((filePath) => {
+      const isTestFile =
+        filePath.endsWith('.test.tsx') ||
+        filePath.endsWith('.test.ts') ||
+        filePath.includes('__mocks__') ||
+        filePath.includes('public/test/');
+      if (isTestFile) {
+        testFiles.push(filePath);
+      } else {
+        codeFiles.push(filePath);
+      }
+    });
 
-        if (!isTestFile) {
-          rules['@typescript-eslint/consistent-type-assertions'] = [
-            'error',
-            {
-              assertionStyle: 'never',
-            },
-          ];
-        }
+    const baseRules: Partial<Linter.RulesRecord> = {
+      '@typescript-eslint/no-explicit-any': 'error',
+    };
 
-        const runner = new ESLint({
-          baseConfig: {
-            ...linterOptions,
-            rules,
-          },
-          useEslintrc: false,
-          cwd: baseDirectory,
-        });
+    const nonTestFilesRules: Partial<Linter.RulesRecord> = {
+      ...baseRules,
+      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+    };
 
-        const lintResults = await runner.lintFiles([filePath]);
-        lintResults
-          .filter((lintResult) => lintResult.source)
-          .forEach((lintResult) => {
-            const { messages } = lintResult;
-            const file = fileTestResult.addFile(filePath, '');
-            messages.forEach((message, index) => {
-              file.addIssue(0, 0, message.message, `${index}`);
-            });
+    const fileGroups: Record<string, string[]> = {};
+
+    for (const filePath of filePaths) {
+      let configPath = eslintConfigMainPaths.find((configPath) => filePath.startsWith(configPath)) ?? '';
+      const isTestFile =
+        filePath.endsWith('.test.tsx') ||
+        filePath.endsWith('.test.ts') ||
+        filePath.includes('__mocks__') ||
+        filePath.includes('public/test/');
+      if (isTestFile) {
+        configPath += '-test';
+      }
+      if (!fileGroups[configPath]) {
+        fileGroups[configPath] = [];
+      }
+      fileGroups[configPath].push(filePath);
+    }
+
+    for (const configPath of Object.keys(fileGroups)) {
+      const rules = configPath.endsWith('-test') ? baseRules : nonTestFilesRules;
+      const linterOptions = (await cli.calculateConfigForFile(fileGroups[configPath][0])) as Linter.Config;
+      const runner = new ESLint({
+        baseConfig: {
+          ...linterOptions,
+          rules: rules,
+        },
+        useEslintrc: false,
+        cwd: baseDirectory,
+      });
+      const lintResults = await runner.lintFiles(fileGroups[configPath]);
+      lintResults
+        .filter((lintResult) => lintResult.source)
+        .forEach((lintResult) => {
+          const { messages } = lintResult;
+          const filePath = lintResult.filePath;
+          const file = fileTestResult.addFile(filePath, '');
+          messages.forEach((message, index) => {
+            file.addIssue(0, 0, message.message, `${index}`);
           });
-      })
-    );
+        });
+    }
   });
 }
