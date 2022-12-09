3c0404bf82f3478d826ee853604ba87cf75c283b
gotjosh <josue.abreu@gmail.com>
2022-12-09 10:57:55 -0400

    turn them all lower case

--- pkg/services/ngalert/store/alert_rule.go
+++ pkg/services/ngalert/store/alert_rule.go
@@ -486,9 +486,9 @@ func (st DBstore) GetAlertRulesForScheduling(ctx context.Context, query *ngmodel
 		query.ResultRules = rules
 
 		if query.PopulateFolders {
-			foldersSql := sess.Table("dashboard").Alias("D").Select("D.uid, D.title").
+			foldersSql := sess.Table("dashboard").Alias("d").Select("d.uid, d.title").
 				Where("is_folder = ?", true).
-				And(`EXISTS (SELECT 1 FROM alert_rule A WHERE "D".uid = A.namespace_uid)`)
+				And(`EXISTS (SELECT 1 FROM alert_rule a WHERE d.uid = a.namespace_uid)`)
 			if len(ids) > 0 {
 				foldersSql = foldersSql.NotIn("org_id", ids)
 			}
