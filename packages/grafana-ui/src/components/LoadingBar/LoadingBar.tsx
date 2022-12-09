879aa3501e548e4dbdc114b0b8705646e015d786
polinaboneva <polina.boneva@grafana.com>
2022-12-09 17:23:38 +0200

    do not use internal comment as it doesn't do anything

--- packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
+++ packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
@@ -5,18 +5,12 @@ import { GrafanaTheme2 } from '@grafana/data';
 
 import { useStyles2 } from '../../themes';
 
-/**
- * @internal
- */
 export interface LoadingBarProps {
   width?: string;
   height?: string;
   ariaLabel?: string;
 }
 
-/**
- * @internal
- */
 export function LoadingBar({ width, height, ariaLabel = 'Loading bar' }: LoadingBarProps) {
   const styles = useStyles2(getStyles(width, height));
 
