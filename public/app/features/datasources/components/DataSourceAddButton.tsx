5ac430146a502c732f89a0fed7cf2e58ff8fd402
Miklós Tolnai <miklos.tolnai@grafana.com>
2022-12-12 16:38:34 +0100

    fix DataSourceAddButton component
    This component implicitly returned `false` when `canCreateDataSource`
    was false, and that way it didn't qualify as a function component.
    We fixed it to explicitly return null in this case.

--- public/app/features/datasources/components/DataSourceAddButton.tsx
+++ public/app/features/datasources/components/DataSourceAddButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import { LinkButton } from '@grafana/ui';
 import { contextSrv } from 'app/core/core';
@@ -6,15 +6,13 @@ import { AccessControlAction } from 'app/types';
 
 import { useDataSourcesRoutes } from '../state';
 
-export function DataSourceAddButton() {
+export const DataSourceAddButton: FC = () => {
   const canCreateDataSource = contextSrv.hasPermission(AccessControlAction.DataSourcesCreate);
   const dataSourcesRoutes = useDataSourcesRoutes();
 
-  return (
-    canCreateDataSource && (
-      <LinkButton icon="plus" href={dataSourcesRoutes.New}>
-        Add new data source
-      </LinkButton>
-    )
-  );
-}
+  return canCreateDataSource ? (
+    <LinkButton icon="plus" href={dataSourcesRoutes.New}>
+      Add new data source
+    </LinkButton>
+  ) : null;
+};
