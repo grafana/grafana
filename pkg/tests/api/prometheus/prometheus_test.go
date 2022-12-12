f13c68c4bf0a29773809da104b96aee1f57eba9e
ismail simsek <ismailsimsek09@gmail.com>
2022-12-12 17:41:20 +0100

    Clean the integration test

--- pkg/tests/api/prometheus/prometheus_test.go
+++ pkg/tests/api/prometheus/prometheus_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestIntegrationPrometheusBuffered(t *testing.T) {
+func TestIntegrationPrometheus(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
@@ -104,54 +104,6 @@ func TestIntegrationPrometheusBuffered(t *testing.T) {
 		require.Equal(t, "basicAuthUser", username)
 		require.Equal(t, "basicAuthPassword", pwd)
 	})
-}
-
-func TestIntegrationPrometheusClient(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping integration test")
-	}
-	dir, path := testinfra.CreateGrafDir(t, testinfra.GrafanaOpts{})
-
-	grafanaListeningAddr, testEnv := testinfra.StartGrafanaEnv(t, dir, path)
-	ctx := context.Background()
-
-	testinfra.CreateUser(t, testEnv.SQLStore, user.CreateUserCommand{
-		DefaultOrgRole: string(org.RoleAdmin),
-		Password:       "admin",
-		Login:          "admin",
-	})
-
-	var outgoingRequest *http.Request
-	outgoingServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		outgoingRequest = r
-		w.WriteHeader(http.StatusUnauthorized)
-	}))
-	t.Cleanup(outgoingServer.Close)
-
-	jsonData := simplejson.NewFromAny(map[string]interface{}{
-		"httpMethod":            "post",
-		"httpHeaderName1":       "X-CUSTOM-HEADER",
-		"customQueryParameters": "q1=1&q2=2",
-	})
-	secureJSONData := map[string]string{
-		"basicAuthPassword": "basicAuthPassword",
-		"httpHeaderValue1":  "custom-header-value",
-	}
-
-	uid := "prometheus"
-	err := testEnv.Server.HTTPServer.DataSourcesService.AddDataSource(ctx, &datasources.AddDataSourceCommand{
-		OrgId:          1,
-		Access:         datasources.DS_ACCESS_PROXY,
-		Name:           "Prometheus",
-		Type:           datasources.DS_PROMETHEUS,
-		Uid:            uid,
-		Url:            outgoingServer.URL,
-		BasicAuth:      true,
-		BasicAuthUser:  "basicAuthUser",
-		JsonData:       jsonData,
-		SecureJsonData: secureJSONData,
-	})
-	require.NoError(t, err)
 
 	t.Run("When calling /api/ds/query should set expected headers on outgoing HTTP request", func(t *testing.T) {
 		query := simplejson.NewFromAny(map[string]interface{}{
@@ -183,28 +135,6 @@ func TestIntegrationPrometheusClient(t *testing.T) {
 		require.NotNil(t, outgoingRequest)
 		require.Equal(t, "/api/v1/query_range", outgoingRequest.URL.Path)
 		require.Contains(t, outgoingRequest.URL.String(), "&q1=1&q2=2")
-		require.Equal(t, "custom-header-value", outgoingRequest.Header.Get("X-CUSTOM-HEADER"))
-		username, pwd, ok := outgoingRequest.BasicAuth()
-		require.True(t, ok)
-		require.Equal(t, "basicAuthUser", username)
-		require.Equal(t, "basicAuthPassword", pwd)
-	})
-
-	t.Run("When calling /api/datasources/uid/{uid}/resources/api/v1/labels should set expected headers on outgoing HTTP request", func(t *testing.T) {
-		u := fmt.Sprintf("http://%s/api/datasources/uid/%s/resources/api/v1/labels", grafanaListeningAddr, uid)
-		// nolint:gosec
-		resp, err := http.Post(u, "application/json", nil)
-		require.NoError(t, err)
-		require.Equal(t, http.StatusUnauthorized, resp.StatusCode)
-		t.Cleanup(func() {
-			err := resp.Body.Close()
-			require.NoError(t, err)
-		})
-		_, err = io.ReadAll(resp.Body)
-		require.NoError(t, err)
-
-		require.NotNil(t, outgoingRequest)
-		require.Equal(t, "/api/v1/labels?q1=1&q2=2", outgoingRequest.URL.String())
 		require.Equal(t, "custom-header-value", outgoingRequest.Header.Get("X-CUSTOM-HEADER"))
 		username, pwd, ok := outgoingRequest.BasicAuth()
 		require.True(t, ok)
