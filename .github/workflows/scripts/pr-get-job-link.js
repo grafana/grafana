b1aae58bab0686c00f7fbc73300ccf17136bc2fc
Marcus Andersson <marcus.andersson@grafana.com>
2022-12-13 12:01:39 +0100

    Fixed so we link to the proper job and step.

--- .github/workflows/scripts/pr-get-job-link.js
+++ .github/workflows/scripts/pr-get-job-link.js
@@ -1,9 +1,13 @@
 
 module.exports = async ({ github, context, core }) => {
+    const name = "Detect breaking changes";
     const { owner, repo } = context.repo;
     const url = `https://api.github.com/repos/${owner}/${repo}/actions/runs/${context.runId}/jobs`
-    const result = await github.request(url)
-    const link = `https://github.com/grafana/grafana/runs/${result.data.jobs[0].id}?check_suite_focus=true`;
-
+    
+    const result = await github.request(url);
+    const job = result.jobs.find(j => j.name === name);
+    const stepIndex = job.steps.findIndex(s => s.name === name);
+    const link = `${job.html_url}#step:${stepIndex + 1}:1`;
+    
     core.setOutput('link', link);
 }
