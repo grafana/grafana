c5aa975fa262bb93df4fbd125aaa08ebfa66d2d8
Erik Sundell <erik.sundell87@gmail.com>
2022-12-13 09:41:56 +0100

    convert to function component

--- public/app/plugins/datasource/cloudwatch/components/CloudWatchLink.tsx
+++ public/app/plugins/datasource/cloudwatch/components/CloudWatchLink.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { usePrevious } from 'react-use';
 
 import { PanelData } from '@grafana/data';
 import { Icon } from '@grafana/ui';
@@ -13,59 +14,39 @@ interface Props {
   datasource: CloudWatchDatasource;
 }
 
-interface State {
-  href: string;
-}
+export function CloudWatchLink({ panelData, query, datasource }: Props) {
+  const [href, setHref] = useState('');
+  const prevPanelData = usePrevious<PanelData | undefined>(panelData);
 
-export default class CloudWatchLink extends Component<Props, State> {
-  state: State = { href: '' };
+  useEffect(() => {
+    if (prevPanelData !== panelData && panelData?.request?.range) {
+      const arns = (query.logGroups ?? [])
+        .filter((group) => group?.value)
+        .map((group) => (group.value ?? '').replace(/:\*$/, '')); // remove `:*` from end of arn
+      const logGroupNames = query.logGroupNames;
+      let sources = arns?.length ? arns : logGroupNames;
 
-  async componentDidUpdate(prevProps: Props) {
-    const { panelData: panelDataNew } = this.props;
-    const { panelData: panelDataOld } = prevProps;
+      const range = panelData?.request?.range;
+      const start = range.from.toISOString();
+      const end = range.to.toISOString();
 
-    if (panelDataOld !== panelDataNew && panelDataNew?.request) {
-      const href = this.getExternalLink();
-      this.setState({ href });
+      const urlProps: AwsUrl = {
+        end,
+        start,
+        timeType: 'ABSOLUTE',
+        tz: 'UTC',
+        editorString: query.expression ?? '',
+        isLiveTail: false,
+        source: sources ?? [],
+      };
+
+      setHref(encodeUrl(urlProps, datasource.api.getActualRegion(query.region)));
     }
-  }
+  }, [panelData, prevPanelData, datasource, query]);
 
-  getExternalLink(): string {
-    const { query, panelData, datasource } = this.props;
-    const arns = (query.logGroups ?? [])
-      .filter((group) => group?.value)
-      .map((group) => (group.value ?? '').replace(/:\*$/, '')); // remove `:*` from end of arn
-    const logGroupNames = query.logGroupNames;
-    let sources = arns?.length ? arns : logGroupNames;
-
-    const range = panelData?.request?.range;
-
-    if (!range) {
-      return '';
-    }
-
-    const start = range.from.toISOString();
-    const end = range.to.toISOString();
-
-    const urlProps: AwsUrl = {
-      end,
-      start,
-      timeType: 'ABSOLUTE',
-      tz: 'UTC',
-      editorString: query.expression ?? '',
-      isLiveTail: false,
-      source: sources ?? [],
-    };
-
-    return encodeUrl(urlProps, datasource.api.getActualRegion(query.region));
-  }
-
-  render() {
-    const { href } = this.state;
-    return (
-      <a href={href} target="_blank" rel="noopener noreferrer">
-        <Icon name="share-alt" /> CloudWatch Logs Insights
-      </a>
-    );
-  }
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <Icon name="share-alt" /> CloudWatch Logs Insights
+    </a>
+  );
 }
