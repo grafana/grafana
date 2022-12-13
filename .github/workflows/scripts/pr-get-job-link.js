b94c3c796782e1c06e81e9456f67e66d3c98bd5c
Marcus Andersson <marcus.andersson@grafana.com>
2022-12-13 13:47:55 +0100

    Fixed according to feedback.

--- .github/workflows/scripts/pr-get-job-link.js
+++ .github/workflows/scripts/pr-get-job-link.js
@@ -1,13 +1,9 @@
 
-module.exports = async ({ github, context, core }) => {
-    const name = "Detect breaking changes";
+module.exports = async ({ name, github, context, core }) => {
     const { owner, repo } = context.repo;
     const url = `https://api.github.com/repos/${owner}/${repo}/actions/runs/${context.runId}/jobs`
-    
     const result = await github.request(url);
     const job = result.jobs.find(j => j.name === name);
-    const stepIndex = job.steps.findIndex(s => s.name === name);
-    const link = `${job.html_url}#step:${stepIndex + 1}:1`;
     
-    core.setOutput('link', link);
+    core.setOutput('link', job.html_url);
 }
