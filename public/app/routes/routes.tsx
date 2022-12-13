6a737488e4b457905a56336a44299078a53de0bd
Ryan McKinley <ryantxu@gmail.com>
2022-12-13 09:29:56 -0800

    make export a real page

--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -367,6 +367,13 @@ export function getAppRoutes(): RouteDescriptor[] {
       ),
     },
     {
+      path: '/admin/storage/export',
+      roles: () => ['Admin'],
+      component: SafeDynamicImport(
+        () => import(/* webpackChunkName: "ExportPage" */ 'app/features/storage/ExportPage')
+      ),
+    },
+    {
       path: '/admin/storage/:path*',
       roles: () => ['Admin'],
       component: SafeDynamicImport(
