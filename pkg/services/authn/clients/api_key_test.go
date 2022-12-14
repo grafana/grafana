bd84b49d61d783654f50cdd0cf0bfad09bab9b29
Karl Persson <kalle.persson@grafana.com>
2022-12-14 16:04:11 +0100

    Add tests

--- pkg/services/authn/clients/api_key_test.go
+++ pkg/services/authn/clients/api_key_test.go
@@ -7,11 +7,130 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/grafana/grafana/pkg/components/apikeygen"
+	apikeygenprefix "github.com/grafana/grafana/pkg/components/apikeygenprefixed"
+	"github.com/grafana/grafana/pkg/services/apikey"
 	"github.com/grafana/grafana/pkg/services/apikey/apikeytest"
 	"github.com/grafana/grafana/pkg/services/authn"
+	"github.com/grafana/grafana/pkg/services/org"
+	"github.com/grafana/grafana/pkg/services/user"
 	"github.com/grafana/grafana/pkg/services/user/usertest"
 	"github.com/stretchr/testify/assert"
 )
+
+var (
+	revoked      = true
+	secret, hash = genApiKey(false)
+)
+
+func TestAPIKey_Authenticate(t *testing.T) {
+	type TestCase struct {
+		desc             string
+		req              *authn.Request
+		expectedKey      *apikey.APIKey
+		expectedUser     *user.SignedInUser
+		expectedErr      error
+		expectedIdentity *authn.Identity
+	}
+
+	tests := []TestCase{
+		{
+			desc: "should success for valid token that is not connected to a service account",
+			req: &authn.Request{HTTPRequest: &http.Request{
+				Header: map[string][]string{
+					"Authorization": {"Bearer " + secret},
+				},
+			}},
+			expectedKey: &apikey.APIKey{
+				Id:    1,
+				OrgId: 1,
+				Key:   hash,
+				Role:  org.RoleAdmin,
+			},
+			expectedIdentity: &authn.Identity{
+				ID:       "api-key:1",
+				OrgID:    1,
+				OrgRoles: map[int64]org.RoleType{1: org.RoleAdmin},
+			},
+		},
+		{
+			desc: "should success for valid token that is connected to service account",
+			req: &authn.Request{HTTPRequest: &http.Request{
+				Header: map[string][]string{
+					"Authorization": {"Bearer " + secret},
+				},
+			}},
+			expectedKey: &apikey.APIKey{
+				Id:               1,
+				OrgId:            1,
+				Key:              hash,
+				ServiceAccountId: intPtr(1),
+			},
+			expectedUser: &user.SignedInUser{
+				UserID:           1,
+				OrgID:            1,
+				IsServiceAccount: true,
+				OrgCount:         1,
+				OrgRole:          org.RoleViewer,
+				Name:             "test",
+			},
+			expectedIdentity: &authn.Identity{
+				ID:       "service-account:1",
+				OrgID:    1,
+				OrgCount: 1,
+				Name:     "test",
+				OrgRoles: map[int64]org.RoleType{1: org.RoleViewer},
+			},
+		},
+		{
+			desc: "should fail for expired api key",
+			req:  &authn.Request{HTTPRequest: &http.Request{Header: map[string][]string{"Authorization": {"Bearer " + secret}}}},
+			expectedKey: &apikey.APIKey{
+				Key:     hash,
+				Expires: intPtr(0),
+			},
+			expectedErr: ErrAPIKeyExpired,
+		},
+		{
+			desc: "should fail for revoked api key",
+			req:  &authn.Request{HTTPRequest: &http.Request{Header: map[string][]string{"Authorization": {"Bearer " + secret}}}},
+			expectedKey: &apikey.APIKey{
+				Key:       hash,
+				IsRevoked: &revoked,
+			},
+			expectedErr: ErrAPIKeyRevoked,
+		},
+		{
+			desc: "should fail if service account is disabled",
+			req:  &authn.Request{HTTPRequest: &http.Request{Header: map[string][]string{"Authorization": {"Bearer " + secret}}}},
+			expectedKey: &apikey.APIKey{
+				Key:              hash,
+				ServiceAccountId: intPtr(1),
+			},
+			expectedUser: &user.SignedInUser{IsDisabled: true},
+			expectedErr:  ErrServiceAccountDisabled,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			c := ProvideAPIKey(&apikeytest.Service{
+				ExpectedAPIKey: tt.expectedKey,
+			}, &usertest.FakeUserService{
+				ExpectedSignedInUser: tt.expectedUser,
+			})
+
+			identity, err := c.Authenticate(context.Background(), tt.req)
+			if tt.expectedErr != nil {
+				assert.Nil(t, identity)
+				assert.ErrorIs(t, err, tt.expectedErr)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, *tt.expectedIdentity, *identity)
+			}
+		})
+	}
+}
 
 func TestAPIKey_Test(t *testing.T) {
 	type TestCase struct {
@@ -68,6 +187,19 @@ func TestAPIKey_Test(t *testing.T) {
 			assert.Equal(t, tt.expected, c.Test(context.Background(), tt.req))
 		})
 	}
+}
+
+func intPtr(n int64) *int64 {
+	return &n
+}
+
+func genApiKey(legacy bool) (string, string) {
+	if legacy {
+		res, _ := apikeygen.New(1, "test")
+		return res.ClientSecret, res.HashedKey
+	}
+	res, _ := apikeygenprefix.New("test")
+	return res.ClientSecret, res.HashedKey
 }
 
 func encodeBasicAuth(username, password string) string {
