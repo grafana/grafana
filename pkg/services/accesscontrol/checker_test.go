f5afb4742ea67fa01664f0c4f7a979b3cfba06c2
Karl Persson <kalle.persson@grafana.com>
2022-12-15 14:59:54 +0100

    Add tests for checker

---
+++ pkg/services/accesscontrol/checker_test.go
@@ -0,0 +1,95 @@
+package accesscontrol
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/grafana/grafana/pkg/services/user"
+	"github.com/stretchr/testify/assert"
+)
+
+type testData struct {
+	uid       string
+	folderUid string
+}
+
+func (d testData) Scopes() []string {
+	return []string{
+		"dashboards:uid:" + d.uid,
+		"folders:uid:" + d.folderUid,
+	}
+}
+
+func generateTestData() []testData {
+	var data []testData
+	for i := 1; i < 100; i++ {
+		data = append(data, testData{
+			uid:       strconv.Itoa(i),
+			folderUid: strconv.Itoa(i + 100),
+		})
+	}
+	return data
+}
+
+func Test_Checker(t *testing.T) {
+	data := generateTestData()
+	type testCase struct {
+		desc        string
+		user        *user.SignedInUser
+		expectedLen int
+	}
+	tests := []testCase{
+		{
+			desc: "should pass for every entity with wildcard scope for action",
+			user: &user.SignedInUser{
+				OrgID:       1,
+				Permissions: map[int64]map[string][]string{1: {"dashboards:read": {"dashboards:*"}}},
+			},
+			expectedLen: len(data),
+		},
+		{
+			desc: "should pass for every entity with wildcard scope for action",
+			user: &user.SignedInUser{
+				OrgID:       1,
+				Permissions: map[int64]map[string][]string{1: {"dashboards:read": {"folders:*"}}},
+			},
+			expectedLen: len(data),
+		},
+		{
+			desc: "should only pass for for 3 scopes",
+			user: &user.SignedInUser{
+				OrgID:       1,
+				Permissions: map[int64]map[string][]string{1: {"dashboards:read": {"dashboards:uid:4", "dashboards:uid:50", "dashboards:uid:99"}}},
+			},
+			expectedLen: 3,
+		},
+		{
+			desc: "should only pass 4 with secondary supported scope",
+			user: &user.SignedInUser{
+				OrgID:       1,
+				Permissions: map[int64]map[string][]string{1: {"dashboards:read": {"folders:uid:104", "folders:uid:150", "folders:uid:154", "folders:uid:199"}}},
+			},
+			expectedLen: 4,
+		},
+		{
+			desc: "should pass none for missing action",
+			user: &user.SignedInUser{
+				OrgID:       1,
+				Permissions: map[int64]map[string][]string{1: {}},
+			},
+			expectedLen: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			check := Checker(tt.user, "dashboards:read")
+			numPasses := 0
+			for _, d := range data {
+				if ok := check(d.Scopes()...); ok {
+					numPasses++
+				}
+			}
+			assert.Equal(t, tt.expectedLen, numPasses)
+		})
+	}
+}
