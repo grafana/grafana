e1ea2367e47d0d6582f2713e7bbae2f9e1e7bfa7
Dominik Prokop <dominik.prokop@grafana.com>
2022-12-15 13:34:53 +0100

    Unsubscribe from existing transformations stream if new data comes in

--- public/app/features/scenes/core/SceneDataTransformer.ts
+++ public/app/features/scenes/core/SceneDataTransformer.ts
@@ -50,6 +50,11 @@ export class SceneDataTransformer extends SceneObjectBase<SceneDataTransformerSt
   }
 
   private transformData(data: Observable<PanelData>) {
+    if (this._transformationsSub) {
+      this._transformationsSub.unsubscribe();
+      this._transformationsSub = undefined;
+    }
+
     this._transformationsSub = data.pipe(getTransformationsStream(this, this.state.transformations)).subscribe({
       next: (data) => {
         this.setState({ data });
