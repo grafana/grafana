d57bfd6a66573dc65ce8c22d6f3a43ce57c91ecf
Owen Smallwood <owen.smallwood@grafana.com>
2022-12-15 12:03:40 -0600

    dont include the old health checks for the time being

--- pkg/services/healthchecks/api/api.go
+++ pkg/services/healthchecks/api/api.go
@@ -3,11 +3,8 @@ package api
 import (
 	"github.com/grafana/grafana/pkg/api/response"
 	"github.com/grafana/grafana/pkg/api/routing"
-	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/healthchecks"
-	"github.com/grafana/grafana/pkg/web"
-	"net/http"
 )
 import "github.com/grafana/grafana/pkg/infra/log"
 
@@ -19,60 +16,25 @@ type Api struct {
 
 func ProvideApi(hcs healthchecks.Service, rr routing.RouteRegister) *Api {
 	api := &Api{
-		HealthCheckService: nil,
-		RouteRegister:      nil,
+		HealthCheckService: hcs,
+		RouteRegister:      rr,
 		Log:                log.New("healthchecks.api"),
 	}
-
-	// is this behind a feature flag?
 
 	return api
 }
 
 func (api *Api) RegisterApiEndpoints() {
-	api.RouteRegister.Get("/healthz", routing.Wrap(api.Heathz))
+	// TODO figure out how to move the old health checks over here. They're awkward.
+	//api.RouteRegister.Get("/healthz/v2", routing.Wrap(api.Healthz))
+	//api.RouteRegister.Get("/api/health/v2", routing.Wrap(api.HealthHandler))
 }
 
-func (api *Api) Heathz(c *models.ReqContext) response.Response {
-	c.Resp.WriteHeader(http.StatusOK)
-	if _, err := c.Resp.Write([]byte("Ok")); err != nil {
-		// Can we use the api logger for this?
-		api.Log.Error("could not write to response", "err", err)
-	}
-
-	return response.Success("healthy?")
+func (api *Api) ListHealthChecks(ctx *models.ReqContext) response.Response {
+	return response.Success("THERE ARE NONE. SORRY!")
 }
 
-func (api *Api) HealthHandler(ctx *web.Context) {
-	//notHeadOrGet := ctx.Req.Method != http.MethodGet && ctx.Req.Method != http.MethodHead
-	//if notHeadOrGet || ctx.Req.URL.Path != "/api/health" {
-	//	return
-	//}
-
-	data := simplejson.New()
-	data.Set("database", "ok")
-	//if !hs.Cfg.AnonymousHideVersion {
-	//	data.Set("version", hs.Cfg.BuildVersion)
-	//	data.Set("commit", hs.Cfg.BuildCommit)
-	//}
-
-	err := api.HealthCheckService.CheckDatabaseHealth(ctx.Req.Context())
-	if err != nil {
-		data.Set("database", "failing")
-		ctx.Resp.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		ctx.Resp.WriteHeader(http.StatusServiceUnavailable)
-	} else {
-		ctx.Resp.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		ctx.Resp.WriteHeader(http.StatusOK)
-	}
-
-	dataBytes, err := data.EncodePretty()
-	if err != nil {
-		api.Log.Error("Failed to encode data", "err", err)
-		return
-	}
-
-	if _, err := ctx.Resp.Write(dataBytes); err != nil {
-		api.Log.Error("Failed to write to response", "err", err)
-	}
+func (api *Api) GetHealthCheck(ctx *models.ReqContext, key string) response.Response {
+	// TODO
+	return response.Success("should get a single health check result")
 }
