3a350e7c3c76ede14ef1c86d7a487f9ec0b1ee26
Laura Benz <laura.benz@grafana.com>
2022-12-15 11:57:25 +0100

    refactor: move testing to related unit test

--- public/app/features/correlations/Forms/QueryEditorField.test.tsx
+++ public/app/features/correlations/Forms/QueryEditorField.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import React, { ReactNode } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { MockDataSourceApi } from 'test/mocks/datasource_srv';
 
+import { LoadingState } from '@grafana/data';
 import { setDataSourceSrv, setQueryRunnerFactory } from '@grafana/runtime';
 import { MockDataSourceSrv } from 'app/features/alerting/unified/mocks';
 
@@ -11,7 +12,7 @@ import { QueryRunner } from '../../query/state/QueryRunner';
 import { QueryEditorField } from './QueryEditorField';
 
 const Wrapper = ({ children }: { children: ReactNode }) => {
-  const methods = useForm();
+  const methods = useForm({ defaultValues: { query: {} } });
   return <FormProvider {...methods}>{children}</FormProvider>;
 };
 
@@ -23,7 +24,7 @@ const defaultGetHandler = async (name: string) => {
   return dsApi;
 };
 
-const renderWithContext = async (
+const renderWithContext = (
   children: ReactNode,
   getHandler: (name: string) => Promise<MockDataSourceApi> = defaultGetHandler
 ) => {
@@ -33,11 +34,9 @@ const renderWithContext = async (
   setDataSourceSrv(dsServer);
 
   render(<Wrapper>{children}</Wrapper>);
-};
 
-beforeAll(() => {
-  setQueryRunnerFactory(() => new QueryRunner());
-});
+  return dsServer.get;
+};
 
 describe('QueryEditorField', () => {
   it('should render the query editor', async () => {
@@ -68,5 +67,87 @@ describe('QueryEditorField', () => {
     expect(
       await screen.findByRole('alert', { name: 'Data source does not export a query editor.' })
     ).toBeInTheDocument();
+  });
+
+  describe('query validation', () => {
+    beforeEach(() => {
+      // Request errors cause the query runner to print to console,
+      // this means the test would fail because of the fail on log rules.
+      // We mock console.error to avoid this.
+      jest.spyOn(console, 'error').mockImplementation();
+    });
+
+    afterAll(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should validate when the query is valid', async () => {
+      const dsApi = new MockDataSourceApi('something');
+      dsApi.components = {
+        QueryEditor: () => <>query editor</>,
+      };
+
+      renderWithContext(<QueryEditorField name="query" dsUid="something" />, async () => {
+        return dsApi;
+      });
+
+      await waitForElementToBeRemoved(() => screen.queryByText(/loading query editor/i));
+
+      // Request errors should result in failed validation
+      dsApi.error = 'Some error';
+      fireEvent.click(screen.getByRole('button', { name: /Validate query$/i }));
+      await waitFor(() => {
+        const alertEl = screen.getByRole('alert');
+        expect(alertEl).toBeInTheDocument();
+        expect(alertEl).toHaveTextContent(/this query is not valid/i);
+      });
+      dsApi.error = null;
+
+      // Results with data should be valid
+      dsApi.result = {
+        data: [
+          {
+            name: 'test',
+            fields: [],
+            length: 0,
+          },
+        ],
+        state: LoadingState.Done,
+      };
+      fireEvent.click(screen.getByRole('button', { name: /Validate query$/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText('This query is valid.')).toBeInTheDocument();
+      });
+
+      // results with error should be invalid, even when they have data
+      dsApi.result = {
+        data: [],
+        state: LoadingState.Error,
+      };
+      fireEvent.click(screen.getByRole('button', { name: /Validate query$/i }));
+      await waitFor(() => {
+        const alertEl = screen.getByRole('alert');
+        expect(alertEl).toBeInTheDocument();
+        expect(alertEl).toHaveTextContent(/this query is not valid/i);
+      });
+
+      dsApi.result = {
+        data: [
+          {
+            name: 'test',
+            fields: [],
+            length: 0,
+          },
+        ],
+        state: LoadingState.Error,
+      };
+      fireEvent.click(screen.getByRole('button', { name: /Validate query$/i }));
+      await waitFor(() => {
+        const alertEl = screen.getByRole('alert');
+        expect(alertEl).toBeInTheDocument();
+        expect(alertEl).toHaveTextContent(/this query is not valid/i);
+      });
+    });
   });
 });
