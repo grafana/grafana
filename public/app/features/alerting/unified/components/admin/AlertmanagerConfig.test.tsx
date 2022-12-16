9764469568d285b2ab653b8f3ec89a461cb423bd
Virginia Cepeda <virginia.cepeda@grafana.com>
2022-12-16 11:36:36 -0300

    Remove invalid mock

--- public/app/features/alerting/unified/components/admin/AlertmanagerConfig.test.tsx
+++ public/app/features/alerting/unified/components/admin/AlertmanagerConfig.test.tsx
@@ -20,7 +20,6 @@ import {
   deleteAlertManagerConfig,
   updateAlertManagerConfig,
   fetchStatus,
-  fetchValidAlertManagerConfig,
 } from '../../api/alertmanager';
 import {
   disableRBAC,
@@ -47,7 +46,6 @@ const mocks = {
     deleteAlertManagerConfig: jest.mocked(deleteAlertManagerConfig),
     updateAlertManagerConfig: jest.mocked(updateAlertManagerConfig),
     fetchStatus: jest.mocked(fetchStatus),
-    fetchValidConfig: jest.mocked(fetchValidAlertManagerConfig),
   },
 };
 
