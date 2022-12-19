83770aeafbdadf5dd7462cea4e90118cfdc6f90e
jguer <joao.guerreiro@grafana.com>
2022-12-19 16:34:43 +0100

    fix DeleteUserPermissions not being called on RemoveOrgUser

--- pkg/services/login/loginservice/loginservice.go
+++ pkg/services/login/loginservice/loginservice.go
@@ -310,10 +310,12 @@ func (ls *Implementation) syncOrgRoles(ctx context.Context, usr *user.User, extU
 				logger.Error(err.Error(), "userId", cmd.UserID, "orgId", cmd.OrgID)
 				continue
 			}
-			if err := ls.accessControl.DeleteUserPermissions(ctx, orgId, cmd.UserID); err != nil {
-				logger.Warn("failed to delete permissions for user", "userID", cmd.UserID, "orgID", orgId)
-			}
 
+			return err
+		}
+
+		if err := ls.accessControl.DeleteUserPermissions(ctx, orgId, cmd.UserID); err != nil {
+			logger.Error("failed to delete permissions for user", "error", err, "userID", cmd.UserID, "orgID", orgId)
 			return err
 		}
 	}
