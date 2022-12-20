439b0cad43c47af5b8349117cc5a212554d27436
Dimitris Sotirakis <dimitrios.sotirakis@grafana.com>
2022-12-20 12:05:25 +0200

    [v9.3.x] CI: Add automation for github assets publishing (#59491) (#60192)
    CI: Add automation for github assets publishing (#59491)
    
    * Add github.star
    
    * Make step depend on fetch images
    
    * artifacts -> path
    
    * Add github token
    
    * Fix custom path
    
    * Add case where path is absent
    
    * Add GH_REGISTRY secret
    
    (cherry picked from commit e9cf8fa7519d73e6029f612437622a2ed4580c8d)

--- pkg/build/cmd/main.go
+++ pkg/build/cmd/main.go
@@ -200,9 +200,8 @@ func main() {
 					Flags: []cli.Flag{
 						&dryRunFlag,
 						&cli.StringFlag{
-							Name:     "path",
-							Required: true,
-							Usage:    "Path to the asset to be published",
+							Name:  "path",
+							Usage: "Path to the asset to be published",
 						},
 						&cli.StringFlag{
 							Name:     "repo",
