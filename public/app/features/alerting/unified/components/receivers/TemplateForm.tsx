fc9fd4609cd33c575f1935985bd8fb1a18f3a125
Armand Grillet <2117580+armandgrillet@users.noreply.github.com>
2022-12-21 07:53:56 +0100

    Use versioned link

--- public/app/features/alerting/unified/components/receivers/TemplateForm.tsx
+++ public/app/features/alerting/unified/components/receivers/TemplateForm.tsx
@@ -183,7 +183,7 @@ function TemplatingGuideline() {
         </div>
         <div>
           <LinkButton
-            href="https://grafana.com/docs/grafana/latest/alerting/manage-notifications/create-message-template"
+            href="https://grafana.com/docs/grafana/v9.3/alerting/manage-notifications/create-message-template"
             target="_blank"
             icon="external-link-alt"
           >
