98670ad04201516b2268d14555d8fafa130814e7
Dominik Prokop <dominik.prokop@grafana.com>
2022-12-28 16:55:07 +0100

    Runtime: Expose plugin import utils

--- packages/grafana-runtime/src/index.ts
+++ packages/grafana-runtime/src/index.ts
@@ -6,7 +6,13 @@
 export * from './services';
 export * from './config';
 export * from './analytics/types';
-export { loadPluginCss, SystemJS, type PluginCssOptions } from './utils/plugin';
+export {
+  loadPluginCss,
+  SystemJS,
+  type PluginCssOptions,
+  setPluginImportUtils,
+  getPluginImportUtils,
+} from './utils/plugin';
 export { reportMetaAnalytics, reportInteraction, reportPageview, reportExperimentView } from './analytics/utils';
 export { featureEnabled } from './utils/licensing';
 export { logInfo, logDebug, logWarning, logError } from './utils/logging';
