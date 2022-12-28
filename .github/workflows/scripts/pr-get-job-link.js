190fc6738e5fa8cc6001e0ecaefcea8cfc0946ae
Noam Levy <noam@groundcover.com>
2022-12-28 15:55:39 +0000

    initial test target

--- .github/workflows/scripts/pr-get-job-link.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = async ({ github, context, core }) => {
-    const { owner, repo } = context.repo;
-    const url = `https://api.github.com/repos/${owner}/${repo}/actions/runs/${context.runId}/jobs`
-    const result = await github.request(url)
-    const link = `https://github.com/grafana/grafana/runs/${result.data.jobs[0].id}?check_suite_focus=true`;
-
-    core.setOutput('link', link);
-}
