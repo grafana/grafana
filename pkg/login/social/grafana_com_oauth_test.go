96734fdba6f17373e75ef3e99a0dbd51f3891322
eleijonmarck <eric.leijonmarck@gmail.com>
2022-12-30 12:38:01 +0100

    added tests

--- pkg/login/social/grafana_com_oauth_test.go
+++ pkg/login/social/grafana_com_oauth_test.go
@@ -5,14 +5,26 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/services/org"
 	"github.com/stretchr/testify/require"
+)
+
+const (
+	UserInfoURL  = "/api/oauth2/user"
+	userResponse = `{
+		"id": 123,
+		"name": "grafana name",
+		"login": "grafanalogin",
+		"username": "grafanalogin",
+		"email": "grafana@grafana.com",
+		"role": "Admin",
+		"orgs": [    {      "login": "grafana",      "role": "Admin"    } ]
+	}`
 )
 
 func TestSocialGrafanaCom_UserInfo(t *testing.T) {
 	provider := SocialGrafanaCom{
 		SocialBase: &SocialBase{
-			log: newLogger("gitlab_oauth_test", "debug"),
+			log: newLogger("grafana_com_oauth_test", "debug"),
 		},
 	}
 
@@ -23,13 +35,33 @@ func TestSocialGrafanaCom_UserInfo(t *testing.T) {
 	tests := []struct {
 		Name          string
 		Cfg           conf
-		ExpectedRole  org.RoleType
+		userInfoResp  string
+		want          *BasicUserInfo
 		ExpectedError error
 	}{
 		{
 			Name:         "should return empty role as userInfo when Skip Org Role Sync Enabled",
+			userInfoResp: userResponse,
 			Cfg:          conf{skipOrgRoleSync: true},
-			ExpectedRole: "",
+			want: &BasicUserInfo{
+				Id:    "1",
+				Name:  "Eric Leijonmarck",
+				Email: "octocat@github.com",
+				Login: "octocat",
+				Role:  "",
+			},
+		},
+		{
+			Name:         "should  role as userInfo when Skip Org Role Sync Enabled",
+			userInfoResp: userResponse,
+			Cfg:          conf{skipOrgRoleSync: false},
+			want: &BasicUserInfo{
+				Id:    "1",
+				Name:  "Eric Leijonmarck",
+				Email: "octocat@github.com",
+				Login: "octocat",
+				Role:  "Admin",
+			},
 		},
 	}
 
@@ -41,16 +73,14 @@ func TestSocialGrafanaCom_UserInfo(t *testing.T) {
 				w.WriteHeader(http.StatusOK)
 				w.Header().Set("Content-Type", "application/json")
 				switch r.RequestURI {
-				case userURI:
-					_, err := w.Write([]byte(test.UserRespBody))
-					require.NoError(t, err)
-				case groupsURI:
-					_, err := w.Write([]byte(test.GroupsRespBody))
+				case UserInfoURL:
+					_, err := w.Write([]byte(test.userInfoResp))
 					require.NoError(t, err)
 				default:
 					w.WriteHeader(http.StatusNotFound)
 				}
 			}))
+			provider.url = ts.URL
 			actualResult, err := provider.UserInfo(ts.Client(), nil)
 			if test.ExpectedError != nil {
 				require.Equal(t, err, test.ExpectedError)
@@ -58,7 +88,7 @@ func TestSocialGrafanaCom_UserInfo(t *testing.T) {
 			}
 
 			require.NoError(t, err)
-			require.Equal(t, test.ExpectedRole, actualResult.Role)
+			require.Equal(t, test.want.Role, actualResult.Role)
 		})
 	}
 }
