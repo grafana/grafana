55a2bcc3db39f35506214c9274aeda5298b12a07
Karl Persson <kalle.persson@grafana.com>
2023-01-02 11:28:20 +0100

    AuthN: add tests

---
+++ pkg/services/authn/clients/basic_test.go
@@ -0,0 +1,55 @@
+package clients
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/grafana/grafana/pkg/services/authn"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasic_Test(t *testing.T) {
+	type TestCase struct {
+		desc     string
+		req      *authn.Request
+		expected bool
+	}
+
+	tests := []TestCase{
+		{
+			desc: "should succeed when authorization header is set with basic prefix",
+			req: &authn.Request{
+				HTTPRequest: &http.Request{
+					Header: map[string][]string{
+						authorizationHeaderName: {encodeBasicAuth("user", "password")},
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			desc: "should fail when no http request is passed",
+			req:  &authn.Request{},
+		},
+		{
+			desc: "should fail when no http authorization header is set in http request",
+			req: &authn.Request{
+				HTTPRequest: &http.Request{Header: map[string][]string{}},
+			},
+		},
+		{
+			desc: "should fail when authorization header is set but without basic prefix",
+			req: &authn.Request{
+				HTTPRequest: &http.Request{Header: map[string][]string{authorizationHeaderName: {"something"}}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			c := ProvideBasic()
+			assert.Equal(t, tt.expected, c.Test(context.Background(), tt.req))
+		})
+	}
+}
