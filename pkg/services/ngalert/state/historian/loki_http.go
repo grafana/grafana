15f922f974e213869845221ee036b6e6f5615cfc
Alex Weaver <weaver.alex.d@gmail.com>
2023-01-06 16:34:54 -0600

    Create loki client type and ping method

---
+++ pkg/services/ngalert/state/historian/loki_http.go
@@ -0,0 +1,41 @@
+package historian
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+)
+
+type httpLokiClient struct {
+	client http.Client
+	url    *url.URL
+	log    log.Logger
+}
+
+func newLokiClient(u *url.URL, logger log.Logger) *httpLokiClient {
+	return &httpLokiClient{
+		client: http.Client{},
+		url:    u,
+		log:    logger.New("backend", "loki"),
+	}
+}
+
+func (c *httpLokiClient) ping() error {
+	uri := c.url.JoinPath("/loki/api/v1/status/buildinfo")
+	req, err := http.NewRequest(http.MethodGet, uri.String(), nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %w", err)
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("request to the loki buildinfo endpoint returned a non-200 status code: %d", res.StatusCode)
+	}
+	return nil
+}
