68b12caae4d3abb4dad61275d80e06a2ab0faad8
Kat Yang <69819079+yangkb09@users.noreply.github.com>
2023-01-10 09:56:44 -0500

    Revert "Chore: Remove star model duplicates (#61191)"
    This reverts commit 9a14a7db03cf242ba7723a03112d87f64ffa9a2d.

--- pkg/services/dashboards/database/database.go
+++ pkg/services/dashboards/database/database.go
@@ -22,7 +22,6 @@ import (
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 	"github.com/grafana/grafana/pkg/services/sqlstore/permissions"
 	"github.com/grafana/grafana/pkg/services/sqlstore/searchstore"
-	"github.com/grafana/grafana/pkg/services/star"
 	"github.com/grafana/grafana/pkg/services/store"
 	"github.com/grafana/grafana/pkg/services/tag"
 	"github.com/grafana/grafana/pkg/setting"
@@ -964,7 +963,7 @@ func (d *DashboardStore) GetDashboardUIDById(ctx context.Context, query *models.
 func (d *DashboardStore) GetDashboards(ctx context.Context, query *models.GetDashboardsQuery) error {
 	return d.store.WithDbSession(ctx, func(sess *db.Session) error {
 		if len(query.DashboardIds) == 0 && len(query.DashboardUIds) == 0 {
-			return star.ErrCommandValidationFailed
+			return models.ErrCommandValidationFailed
 		}
 
 		var dashboards = make([]*models.Dashboard, 0)
