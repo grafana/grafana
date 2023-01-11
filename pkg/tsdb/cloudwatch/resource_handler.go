f28764feaa6077654a1dc7606e405f4f41269bb1
Erik Sundell <erik.sundell87@gmail.com>
2023-01-11 19:05:31 +0100

    remove unused route

--- pkg/tsdb/cloudwatch/resource_handler.go
+++ pkg/tsdb/cloudwatch/resource_handler.go
@@ -19,7 +19,6 @@ func (e *cloudWatchExecutor) newResourceMux() *http.ServeMux {
 	mux.HandleFunc("/ec2-instance-attribute", handleResourceReq(e.handleGetEc2InstanceAttribute))
 	mux.HandleFunc("/resource-arns", handleResourceReq(e.handleGetResourceArns))
 	mux.HandleFunc("/describe-log-groups", routes.ResourceRequestMiddleware(routes.LogGroupsHandler, logger, e.getRequestContext))
-	mux.HandleFunc("/all-log-groups", handleResourceReq(e.handleGetAllLogGroups))
 	mux.HandleFunc("/metrics", routes.ResourceRequestMiddleware(routes.MetricsHandler, logger, e.getRequestContext))
 	mux.HandleFunc("/dimension-values", routes.ResourceRequestMiddleware(routes.DimensionValuesHandler, logger, e.getRequestContext))
 	mux.HandleFunc("/dimension-keys", routes.ResourceRequestMiddleware(routes.DimensionKeysHandler, logger, e.getRequestContext))
