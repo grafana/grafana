38e95cb152058e3249778e4c9a7e30933371475f
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2023-01-12 17:07:38 +0100

    Use usePluginBridge hook instead of getPluginSettings to check if plugin is installed

--- public/app/features/alerting/unified/components/receivers/grafanaAppReceivers/grafanaApp.ts
+++ public/app/features/alerting/unified/components/receivers/grafanaAppReceivers/grafanaApp.ts
@@ -1,29 +1,16 @@
-import { useAsync } from 'react-use';
-
-import { getPluginSettings } from 'app/features/plugins/pluginSettings';
 import { Receiver } from 'app/plugins/datasource/alertmanager/types';
 
 import { useGetOnCallIntegrationsQuery } from '../../../api/onCallApi';
+import { SupportedPlugin, usePluginBridge } from '../../PluginBridge';
 
 import { isOnCallReceiver } from './onCall/onCall';
-import { AmRouteReceiver, GrafanaAppReceiverEnum, GRAFANA_APP_PLUGIN_IDS, ReceiverWithTypes } from './types';
-
-export const useGetAppIsInstalledAndEnabled = (grafanaAppType: GrafanaAppReceiverEnum) => {
-  const {
-    loading,
-    error,
-    value: plugin,
-  } = useAsync(() => getPluginSettings(GRAFANA_APP_PLUGIN_IDS[grafanaAppType], { showErrorAlert: false }));
-  const installed = plugin && !error && !loading;
-  return installed;
-};
+import { AmRouteReceiver, GrafanaAppReceiverEnum, ReceiverWithTypes } from './types';
 
 export const useGetGrafanaReceiverTypeChecker = () => {
-  const isOnCallEnabled = useGetAppIsInstalledAndEnabled(GrafanaAppReceiverEnum.GRAFANA_ONCALL);
+  const { installed: isOnCallEnabled } = usePluginBridge(SupportedPlugin.OnCall);
   const { data } = useGetOnCallIntegrationsQuery(undefined, {
     skip: !isOnCallEnabled,
   });
-
   const getGrafanaReceiverType = (receiver: Receiver): GrafanaAppReceiverEnum | undefined => {
     //CHECK FOR ONCALL PLUGIN
     const onCallIntegrations = data ?? [];
