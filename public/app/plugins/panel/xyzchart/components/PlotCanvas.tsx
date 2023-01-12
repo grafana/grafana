a8011f6b4cb9ac4c184640715aaa3737c21f7e41
Kyle Cunningham <kyle@codeincarnate.com>
2023-01-12 18:11:29 +0700

    Fix various errors

--- public/app/plugins/panel/xyzchart/components/PlotCanvas.tsx
+++ public/app/plugins/panel/xyzchart/components/PlotCanvas.tsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber';
-import React, { createRef, useEffect, useState, RefObject, ReactNode, Suspense } from 'react';
+import React, { createRef, useEffect, useState, RefObject, Suspense } from 'react';
+import { AmbientLight, PointLight } from 'three';
 
 import { DataFrame } from '@grafana/data';
 
@@ -11,14 +12,15 @@ import { getIntervalLabels, prepData } from '../utils';
 import { Camera } from './Camera';
 import { GridVolume } from './GridVolume';
 import { PointCloud } from './PointCloud';
+
 interface Props {
   frames: DataFrame[];
   options: ScatterPlotOptions;
 }
 
 export const PlotCanvas: React.FC<Props> = ({ frames, options }) => {
-  let ambLightRef: RefObject<ReactNode> = createRef();
-  let pntLightRef: RefObject<ReactNode> = createRef();
+  let ambLightRef: RefObject<AmbientLight> = createRef();
+  let pntLightRef: RefObject<PointLight> = createRef();
   const [pointData, setPointData] = useState(prepData(frames, options.pointColor ?? '#ff0000'));
   const [intervalLabels, setIntervalLabels] = useState(getIntervalLabels(frames));
 
@@ -32,14 +34,16 @@ export const PlotCanvas: React.FC<Props> = ({ frames, options }) => {
 
   return (
     <>
-      <Canvas mode="concurrent" raycaster={{ params: { Points: { threshold: 2 } } }} linear flat>
+      <Canvas raycaster={{ params: { Points: { threshold: 2 } } }} linear flat>
         {/* 
           Context does not work outside of Canvas. Seems Canvas is outside parent component in DOM 
           https://github.com/facebook/react/issues/17126
         */}
         <OptionsProvider value={options}>
           <Camera />
+          {/* eslint-disable-next-line */}
           <ambientLight ref={ambLightRef} intensity={0.8} color={WHITE} />
+          {/* eslint-disable-next-line */}
           <pointLight ref={pntLightRef} intensity={1.0} position={[10, 10, 10]} />
           <Suspense fallback={null}>
             <PointCloud frames={frames} points={pointData} lights={[ambLightRef, pntLightRef]} />
