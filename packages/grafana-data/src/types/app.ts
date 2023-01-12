b47dab7553b97425f020a2540d2cb2ecbe9671ab
Piotr Jamróz <pm.jamroz@gmail.com>
2023-01-12 20:43:40 +0100

    Post-merge fix (remove duplicate)

--- packages/grafana-data/src/types/app.ts
+++ packages/grafana-data/src/types/app.ts
@@ -17,7 +17,6 @@ export enum CoreApp {
   Unknown = 'unknown',
   PanelEditor = 'panel-editor',
   PanelViewer = 'panel-viewer',
-  Correlations = 'correlations',
 }
 
 export interface AppRootProps<T extends KeyValue = KeyValue> {
