e34c02296bfef6c0b980cc1c29392875d1a7bbe7
sam boyer <sdboyer@grafana.com>
2023-01-12 14:47:34 -0500

    Move DataQuery into mudball

--- packages/grafana-data/src/types/query.ts
+++ packages/grafana-data/src/types/query.ts
@@ -1,3 +1,6 @@
+import { DataQuery, DataSourceRef } from '@grafana/schema';
+export type { DataQuery, DataSourceRef };
+
 /**
  * Attached to query results (not persisted)
  *
@@ -5,52 +8,6 @@
  */
 export enum DataTopic {
   Annotations = 'annotations',
-}
-
-/**
- * @public
- */
-export interface DataSourceRef {
-  /** The plugin type-id */
-  type?: string;
-
-  /** Specific datasource instance */
-  uid?: string;
-}
-
-/**
- * These are the common properties available to all queries in all datasources
- * Specific implementations will *extend* this interface adding the required properties
- * for the given context
- *
- * @public
- */
-export interface DataQuery {
-  /**
-   * A - Z
-   */
-  refId: string;
-
-  /**
-   * true if query is disabled (ie should not be returned to the dashboard)
-   */
-  hide?: boolean;
-
-  /**
-   * Unique, guid like, string used in explore mode
-   */
-  key?: string;
-
-  /**
-   * Specify the query flavor
-   */
-  queryType?: string;
-
-  /**
-   * For mixed data sources the selected datasource is on the query level.
-   * For non mixed scenarios this is undefined.
-   */
-  datasource?: DataSourceRef | null;
 }
 
 /**
