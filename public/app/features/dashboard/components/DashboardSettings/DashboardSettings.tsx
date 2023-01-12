30aa6144a8c8262fe771a97566b8e59820013685
Joao Silva <joao.silva@grafana.com>
2023-01-12 12:01:38 +0000

    Add topnav check

--- public/app/features/dashboard/components/DashboardSettings/DashboardSettings.tsx
+++ public/app/features/dashboard/components/DashboardSettings/DashboardSettings.tsx
@@ -58,9 +58,11 @@ export function DashboardSettings({ dashboard, editview, pageNav, sectionNav }: 
   const size = config.featureToggles.topnav ? 'sm' : 'md';
 
   const actions = [
-    <Button variant="secondary" key="close" fill="outline" size={size} onClick={onClose}>
-      Close
-    </Button>,
+    config.featureToggles.topnav && (
+      <Button variant="secondary" key="close" fill="outline" size={size} onClick={onClose}>
+        Close
+      </Button>
+    ),
     canSaveAs && (
       <SaveDashboardAsButton
         dashboard={dashboard}
