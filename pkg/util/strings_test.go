fb29511fd2e672327afe47ad40595076334add51
Alexander Zobnin <alexanderzobnin@gmail.com>
2023-01-13 12:09:33 +0100

    add test for invalid string

--- pkg/util/strings_test.go
+++ pkg/util/strings_test.go
@@ -54,6 +54,7 @@ func TestSplitString(t *testing.T) {
 		"test1 , test2 test3": {"test1", "test2", "test3"},
 		"foo, bar baz":        {"foo", "bar", "baz"},
 		`"foo", "bar baz"`:    {"foo", "bar baz"},
+		`"`:                   {},
 	}
 	for input, expected := range tests {
 		assert.EqualValues(t, expected, SplitString(input))
