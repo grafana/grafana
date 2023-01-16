22564edebcb6360c55816d89e6b8551589997461
Alexandra Vargas <alexa1866@gmail.com>
2023-01-16 14:17:41 +0000

    Remove squared outline focus and remove tabIndex in Tooltip component, now is focusable by default

--- packages/grafana-ui/src/components/Tooltip/Tooltip.tsx
+++ packages/grafana-ui/src/components/Tooltip/Tooltip.tsx
@@ -15,71 +15,68 @@ export interface TooltipProps {
   placement?: TooltipPlacement;
   content: PopoverContent;
   children: JSX.Element;
-  tabIndex?: number;
   /**
    * Set to true if you want the tooltip to stay long enough so the user can move mouse over content to select text or click a link
    */
   interactive?: boolean;
 }
 
-export const Tooltip = React.memo(
-  ({ children, theme, interactive, show, placement, content, tabIndex }: TooltipProps) => {
-    const [controlledVisible, setControlledVisible] = React.useState(show);
+export const Tooltip = React.memo(({ children, theme, interactive, show, placement, content }: TooltipProps) => {
+  const [controlledVisible, setControlledVisible] = React.useState(show);
 
-    useEffect(() => {
-      if (controlledVisible !== false) {
-        const handleKeyDown = (enterKey: KeyboardEvent) => {
-          if (enterKey.key === 'Escape') {
-            setControlledVisible(false);
-          }
-        };
-        document.addEventListener('keydown', handleKeyDown);
-        return () => {
-          document.removeEventListener('keydown', handleKeyDown);
-        };
-      } else {
-        return;
-      }
-    }, [controlledVisible]);
+  useEffect(() => {
+    if (controlledVisible !== false) {
+      const handleKeyDown = (enterKey: KeyboardEvent) => {
+        if (enterKey.key === 'Escape') {
+          setControlledVisible(false);
+        }
+      };
+      document.addEventListener('keydown', handleKeyDown);
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+      };
+    } else {
+      return;
+    }
+  }, [controlledVisible]);
 
-    const { getArrowProps, getTooltipProps, setTooltipRef, setTriggerRef, visible, update } = usePopperTooltip({
-      visible: controlledVisible,
-      placement: placement,
-      interactive: interactive,
-      delayHide: interactive ? 100 : 0,
-      delayShow: 150,
-      offset: [0, 8],
-      trigger: ['hover', 'focus'],
-      onVisibleChange: setControlledVisible,
-    });
+  const { getArrowProps, getTooltipProps, setTooltipRef, setTriggerRef, visible, update } = usePopperTooltip({
+    visible: controlledVisible,
+    placement: placement,
+    interactive: interactive,
+    delayHide: interactive ? 100 : 0,
+    delayShow: 150,
+    offset: [0, 8],
+    trigger: ['hover', 'focus'],
+    onVisibleChange: setControlledVisible,
+  });
 
-    const styles = useStyles2(getStyles);
-    const containerStyle = styles[theme ?? 'info'];
+  const styles = useStyles2(getStyles);
+  const containerStyle = styles[theme ?? 'info'];
 
-    return (
-      <>
-        {React.cloneElement(children, {
-          ref: setTriggerRef,
-          tabIndex: tabIndex,
-        })}
-        {visible && (
-          <Portal>
-            <div ref={setTooltipRef} {...getTooltipProps({ className: containerStyle })}>
-              <div {...getArrowProps({ className: 'tooltip-arrow' })} />
-              {typeof content === 'string' && content}
-              {React.isValidElement(content) && React.cloneElement(content)}
-              {typeof content === 'function' &&
-                update &&
-                content({
-                  updatePopperPosition: update,
-                })}
-            </div>
-          </Portal>
-        )}
-      </>
-    );
-  }
-);
+  return (
+    <>
+      {React.cloneElement(children, {
+        ref: setTriggerRef,
+        tabIndex: 0, // tooltip should be keyboard focusable
+      })}
+      {visible && (
+        <Portal>
+          <div ref={setTooltipRef} {...getTooltipProps({ className: containerStyle })}>
+            <div {...getArrowProps({ className: 'tooltip-arrow' })} />
+            {typeof content === 'string' && content}
+            {React.isValidElement(content) && React.cloneElement(content)}
+            {typeof content === 'function' &&
+              update &&
+              content({
+                updatePopperPosition: update,
+              })}
+          </div>
+        </Portal>
+      )}
+    </>
+  );
+});
 
 Tooltip.displayName = 'Tooltip';
 
