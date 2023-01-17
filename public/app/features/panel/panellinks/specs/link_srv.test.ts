c620253431f421e0c9b396189eff4df56e8fecd8
Juan Cabanas <juan.cabanas@grafana.com>
2023-01-17 16:25:09 -0300

    test modified

--- public/app/features/panel/panellinks/specs/link_srv.test.ts
+++ public/app/features/panel/panellinks/specs/link_srv.test.ts
@@ -23,7 +23,6 @@ describe('linkSrv', () => {
   function initLinkSrv() {
     const _dashboard: any = {
       time: { from: 'now-6h', to: 'now' },
-      timepicker: { hidden: false },
       getTimezone: jest.fn(() => 'browser'),
       timeRangeUpdated: () => {},
     };
