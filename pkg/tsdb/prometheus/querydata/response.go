1da33ef5b594037ae34764f01fcdc4cb47dcb565
ismail simsek <ismailsimsek09@gmail.com>
2023-01-18 15:57:55 +0100

    When there is no frame add empty frame to get metadata attached to it

--- pkg/tsdb/prometheus/querydata/response.go
+++ pkg/tsdb/prometheus/querydata/response.go
@@ -30,21 +30,22 @@ func (s *QueryData) parseResponse(ctx context.Context, q *models.Query, res *htt
 		VectorWideSeries: s.enableWideSeries,
 	})
 
-	if r.Error != nil {
-		return r, r.Error
-	}
-
-	if r.Frames == nil {
-		return r, fmt.Errorf("received empty response from prometheus")
-	}
-
 	// The ExecutedQueryString can be viewed in QueryInspector in UI
+	// Add frame to attach metadata to it
+	if len(r.Frames) == 0 {
+		r.Frames = append(r.Frames, data.NewFrame(""))
+	}
+
 	for _, frame := range r.Frames {
 		if s.enableWideSeries {
 			addMetadataToWideFrame(q, frame)
 		} else {
 			addMetadataToMultiFrame(q, frame)
 		}
+	}
+
+	if r.Error != nil {
+		return r, r.Error
 	}
 
 	r = s.processExemplars(q, r)
