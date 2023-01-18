90d9290cd17ebb16ff5a75ae4105c0050cd461b4
Alex Weaver <weaver.alex.d@gmail.com>
2023-01-18 15:52:12 -0600

    Genericize auth/tenant headers and improve logging in error case

--- pkg/services/ngalert/state/historian/loki.go
+++ pkg/services/ngalert/state/historian/loki.go
@@ -11,6 +11,7 @@ import (
 
 type remoteLokiClient interface {
 	ping() error
+	push([]stream) error
 }
 
 type RemoteLokiBackend struct {
@@ -30,9 +31,59 @@ func (h *RemoteLokiBackend) TestConnection() error {
 	return h.client.ping()
 }
 
-func (h *RemoteLokiBackend) RecordStatesAsync(ctx context.Context, _ *models.AlertRule, _ []state.StateTransition) {
+func (h *RemoteLokiBackend) RecordStatesAsync(ctx context.Context, rule *models.AlertRule, states []state.StateTransition) {
 	logger := h.log.FromContext(ctx)
-	logger.Debug("Remote Loki state history backend was called with states")
+	streams := h.buildStreams(rule, states, logger)
+	h.recordStreamsAsync(ctx, streams, logger)
+}
+
+func (h *RemoteLokiBackend) buildStreams(rule *models.AlertRule, states []state.StateTransition, logger log.Logger) []stream {
+	buckets := make(map[string][]row) // label hash -> entries
+	for _, state := range states {
+		if !shouldRecord(state) {
+			continue
+		}
+
+		labels := removePrivateLabels(state.State.Labels)
+		repr := labels.String()
+
+		buckets[repr] = append(buckets[repr], row{
+			At:  state.State.LastEvaluationTime,
+			Val: state.Formatted(),
+		})
+	}
+
+	result := make([]stream, 0, len(buckets))
+	for repr, rows := range buckets {
+		labels, err := data.LabelsFromString(repr)
+		if err != nil {
+			logger.Error("Failed to parse frame labels, skipping state history batch: %w", err)
+			continue
+		}
+		result = append(result, stream{
+			Stream: labels,
+			Values: rows,
+		})
+	}
+
+	return result
+}
+
+func (h *RemoteLokiBackend) recordStreamsAsync(ctx context.Context, streams []stream, logger log.Logger) {
+	go func() {
+		if err := h.recordStreams(ctx, streams, logger); err != nil {
+			logger.Error("Failed to save alert state history batch", "error", err)
+		}
+	}()
+}
+
+func (h *RemoteLokiBackend) recordStreams(ctx context.Context, streams []stream, logger log.Logger) error {
+	logger.Error("pushing streams", "streams", streams)
+	if err := h.client.push(streams); err != nil {
+		return err
+	}
+	logger.Debug("Done saving alert state history batch")
+	return nil
 }
 
 func (h *RemoteLokiBackend) QueryStates(ctx context.Context, query models.HistoryQuery) (*data.Frame, error) {
--- pkg/services/ngalert/state/historian/loki_http.go
+++ pkg/services/ngalert/state/historian/loki_http.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -39,18 +40,10 @@ func newLokiClient(cfg LokiConfig, logger log.Logger) *httpLokiClient {
 func (c *httpLokiClient) ping() error {
 	uri := c.cfg.Url.JoinPath("/loki/api/v1/labels")
 	req, err := http.NewRequest(http.MethodGet, uri.String(), nil)
-
-	if c.cfg.BasicAuthUser != "" || c.cfg.BasicAuthPassword != "" {
-		req.SetBasicAuth(c.cfg.BasicAuthUser, c.cfg.BasicAuthPassword)
-	}
-
-	if c.cfg.TenantID != "" {
-		req.Header.Add("X-Scope-OrgID", c.cfg.TenantID)
-	}
-
 	if err != nil {
 		return fmt.Errorf("error creating request: %w", err)
 	}
+	c.setAuthAndTenantHeaders(req)
 
 	res, err := c.client.Do(req)
 	if res != nil {
@@ -83,7 +76,7 @@ type row struct {
 
 func (r *row) MarshalJSON() ([]byte, error) {
 	return json.Marshal([2]string{
-		fmt.Sprintf("%d", r.At.Unix()), r.Val,
+		fmt.Sprintf("%d", r.At.UnixNano()), r.Val,
 	})
 }
 
@@ -96,22 +89,43 @@ func (c *httpLokiClient) push(s []stream) error {
 		return fmt.Errorf("failed to serialize Loki payload: %w", err)
 	}
 
-	uri := c.url.JoinPath("/loki/api/v1/push")
+	uri := c.cfg.Url.JoinPath("/loki/api/v1/push")
 	req, err := http.NewRequest(http.MethodPost, uri.String(), bytes.NewBuffer(enc))
 	if err != nil {
 		return fmt.Errorf("failed to create Loki request: %w", err)
 	}
 
+	c.setAuthAndTenantHeaders(req)
 	req.Header.Add("content-type", "application/json")
 
 	resp, err := c.client.Do(req)
+	if resp != nil {
+		defer func() {
+			if err := resp.Body.Close(); err != nil {
+				c.log.Warn("Failed to close response body", "err", err)
+			}
+		}()
+	}
 	if err != nil {
 		return fmt.Errorf("failed to send request: %w", err)
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		// TODO: log body if error?
-		return fmt.Errorf("received an error response from loki, status: %d", resp.StatusCode)
+		byt, _ := io.ReadAll(resp.Body)
+		if len(byt) > 0 {
+			c.log.Error("Error response from Loki", "response", string(byt))
+		}
+		return fmt.Errorf("received a non-200 response from loki, status: %d", resp.StatusCode)
 	}
 	return nil
+}
+
+func (c *httpLokiClient) setAuthAndTenantHeaders(req *http.Request) {
+	if c.cfg.BasicAuthUser != "" || c.cfg.BasicAuthPassword != "" {
+		req.SetBasicAuth(c.cfg.BasicAuthUser, c.cfg.BasicAuthPassword)
+	}
+
+	if c.cfg.TenantID != "" {
+		req.Header.Add("X-Scope-OrgID", c.cfg.TenantID)
+	}
 }
