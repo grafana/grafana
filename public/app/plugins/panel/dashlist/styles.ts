98d18c775ea866735593e294c37e6718b87db8d8
Torkel Ödegaard <torkel@grafana.com>
2023-01-19 14:25:21 +0100

    Updated

--- public/app/plugins/panel/dashlist/styles.ts
+++ public/app/plugins/panel/dashlist/styles.ts
@@ -21,6 +21,7 @@ export const getStyles = (theme: GrafanaTheme2) => ({
     &:hover {
       a {
         color: ${theme.colors.text.link};
+        text-decoration: underline;
       }
     }
   `,
