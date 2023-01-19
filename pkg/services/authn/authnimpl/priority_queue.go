e696b7555ae970e0061f31859f07ee49e3a5d6e4
Karl Persson <kalle.persson@grafana.com>
2023-01-19 14:19:29 +0100

    AuthN: update comment

--- pkg/services/authn/authnimpl/priority_queue.go
+++ pkg/services/authn/authnimpl/priority_queue.go
@@ -18,13 +18,13 @@ type queueItem[T any] struct {
 }
 
 func (q *queue[T]) insert(v T, p uint) {
-	// no clients in the queue so we just add it
+	// no items in the queue so we just add it
 	if len(q.items) == 0 {
 		q.items = append(q.items, queueItem[T]{v, p})
 		return
 	}
 
-	// find the position in the queue the client should be placed based on priority
+	// find the position in the queue the item should be placed based on priority
 	for i, item := range q.items {
 		if p < item.p {
 			q.items = append(q.items[:i+1], q.items[i:]...)
@@ -33,6 +33,6 @@ func (q *queue[T]) insert(v T, p uint) {
 		}
 	}
 
-	// client did not have higher priority then what is in the queue currently, so we need to add it to the end
+	// item did not have higher priority then what is in the queue currently, so we need to add it to the end
 	q.items = append(q.items, queueItem[T]{v, p})
 }
