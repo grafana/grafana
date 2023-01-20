c49a34746f2daa9fda1d78b0c1d9095ed52c3c59
Ivana Huckova <ivana.huckova@gmail.com>
2023-01-20 16:13:41 +0100

    Default to true

--- public/app/features/explore/utils/supplementaryQueries.ts
+++ public/app/features/explore/utils/supplementaryQueries.ts
@@ -17,7 +17,6 @@ export const loadSupplementaryQueries = (): SupplementaryQueries => {
   // We default to true for all supp queries
   let supplementaryQueries: SupplementaryQueries = {
     [SupplementaryQueryType.LogsVolume]: { enabled: true },
-    // This is set to false temporarily, until we have UI to display logs sample and a way how to enable/disable it
     [SupplementaryQueryType.LogsSample]: { enabled: true },
   };
 
