876d36671a92847d02558b3cabf32c4d227df40d
Joan López de la Franca Beltran <joanjan14@gmail.com>
2023-01-23 11:19:40 +0100

    Kindsys: Include generated-code links to Kinds report

--- pkg/kindsys/report.go
+++ pkg/kindsys/report.go
@@ -10,6 +10,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path"
+	"reflect"
 	"sort"
 	"strings"
 
@@ -18,9 +20,26 @@ import (
 	"github.com/grafana/grafana/pkg/plugins/pfs/corelist"
 	"github.com/grafana/grafana/pkg/plugins/plugindef"
 	"github.com/grafana/grafana/pkg/registry/corekind"
+	"github.com/grafana/thema"
 )
 
-const reportFileName = "report.json"
+const (
+	// Program's output
+	reportFileName = "report.json"
+
+	// External references
+	repoBaseURL = "https://github.com/grafana/grafana/tree/main"
+	docsBaseURL = "https://grafana.com/docs/grafana/next/developers/kinds"
+
+	// Local references
+	coreTSPath  = "packages/grafana-schema/src/raw/%s/%s/%s_types.gen.ts"
+	coreGoPath  = "pkg/kinds/%s/%s_kind_gen.go"
+	coreCUEPath = "kinds/%s/%s_kind.cue"
+
+	composableTSPath  = "public/app/plugins/%s/%s/models.gen.ts"
+	composableGoPath  = "pkg/tsdb/%s/types_%s_gen.go"
+	composableCUEPath = "public/app/plugins/%s/%s/composable_%s.cue"
+)
 
 func main() {
 	report := buildKindStateReport()
@@ -54,17 +73,53 @@ var plannedCoreKinds = []string{
 	"QueryHistory",
 }
 
-type KindStateReport struct {
-	Kinds      map[string]kindsys.SomeKindProperties `json:"kinds"`
-	Dimensions map[string]Dimension                  `json:"dimensions"`
+type Kind struct {
+	kindsys.SomeKindProperties
+	Category  string
+	SchemaRef string
+	GoRef     string
+	TsRef     string
+	DocsRef   string
 }
 
-func (r *KindStateReport) add(k kindsys.SomeKindProperties, category string) {
+// MarshalJSON is overwritten to marshal
+// kindsys.SomeKindProperties at root level.
+func (k Kind) MarshalJSON() ([]byte, error) {
+	b, err := json.Marshal(k.SomeKindProperties)
+	if err != nil {
+		return nil, err
+	}
+
+	var m map[string]interface{}
+	if err = json.Unmarshal(b, &m); err != nil {
+		return nil, err
+	}
+
+	m["category"] = k.Category
+
+	for _, ref := range []string{"SchemaRef", "GoRef", "TsRef", "DocsRef"} {
+		refVal := reflect.ValueOf(k).FieldByName(ref).String()
+		if len(refVal) > 0 {
+			m[toCamelCase(ref)] = refVal
+		} else {
+			m[toCamelCase(ref)] = "n/a"
+		}
+	}
+
+	return json.Marshal(m)
+}
+
+type KindStateReport struct {
+	Kinds      map[string]Kind      `json:"kinds"`
+	Dimensions map[string]Dimension `json:"dimensions"`
+}
+
+func (r *KindStateReport) add(k Kind) {
 	kName := k.Common().MachineName
 
 	r.Kinds[kName] = k
 	r.Dimensions["maturity"][k.Common().Maturity.String()].add(kName)
-	r.Dimensions["category"][category].add(kName)
+	r.Dimensions["category"][k.Category].add(kName)
 }
 
 type Dimension map[string]*DimensionValue
@@ -85,7 +140,7 @@ func (dv *DimensionValue) add(s string) {
 // the final report.
 func emptyKindStateReport() *KindStateReport {
 	return &KindStateReport{
-		Kinds: make(map[string]kindsys.SomeKindProperties),
+		Kinds: make(map[string]Kind),
 		Dimensions: map[string]Dimension{
 			"maturity": {
 				"planned":      emptyDimensionValue("planned"),
@@ -117,9 +172,19 @@ func buildKindStateReport() *KindStateReport {
 	seen := make(map[string]bool)
 	for _, k := range b.All() {
 		seen[k.Props().Common().Name] = true
+		k.Lineage()
 		switch k.Props().(type) {
 		case kindsys.CoreProperties:
-			r.add(k.Props(), "core")
+			category := "core"
+			commonProps := k.Props().Common()
+			r.add(Kind{
+				SomeKindProperties: k.Props(),
+				Category:           category,
+				GoRef:              buildCoreGoRef(commonProps),
+				DocsRef:            buildDocsRef(category, commonProps),
+				TsRef:              buildCoreTSRef(k.Lineage(), k.Decl()),
+				SchemaRef:          buildCoreSchemaRef(commonProps),
+			})
 		}
 	}
 
@@ -128,25 +193,41 @@ func buildKindStateReport() *KindStateReport {
 			continue
 		}
 
-		r.add(kindsys.CoreProperties{
-			CommonProperties: kindsys.CommonProperties{
-				Name:              kn,
-				PluralName:        kn + "s",
-				MachineName:       machinize(kn),
-				PluralMachineName: machinize(kn) + "s",
-				Maturity:          "planned",
+		r.add(Kind{
+			SomeKindProperties: kindsys.CoreProperties{
+				CommonProperties: kindsys.CommonProperties{
+					Name:              kn,
+					PluralName:        kn + "s",
+					MachineName:       machinize(kn),
+					PluralMachineName: machinize(kn) + "s",
+					Maturity:          "planned",
+				},
 			},
-		}, "core")
+			Category: "core",
+		})
 	}
 
 	all := kindsys.SchemaInterfaces(nil)
 	for _, pp := range corelist.New(nil) {
 		for _, si := range all {
+			category := "composable"
+			var goRef string
 			if ck, has := pp.ComposableKinds[si.Name()]; has {
-				r.add(ck.Props(), "composable")
+				cp := ck.Decl().Properties
+				if pp.Properties.Backend != nil && *pp.Properties.Backend {
+					goRef = buildComposableGoRef(pp.Properties.Type, cp)
+				}
+				r.add(Kind{
+					SomeKindProperties: ck.Props(),
+					Category:           category,
+					GoRef:              goRef,
+					DocsRef:            buildDocsRef(category, ck.Props().Common()),
+					TsRef:              buildComposableTSRef(pp.Properties.Type, cp),
+					SchemaRef:          buildComposableSchemaRef(pp.Properties.Type, cp),
+				})
 			} else if may := si.Should(string(pp.Properties.Type)); may {
 				n := plugindef.DerivePascalName(pp.Properties) + si.Name()
-				props := kindsys.ComposableProperties{
+				ck := kindsys.ComposableProperties{
 					SchemaInterface: si.Name(),
 					CommonProperties: kindsys.CommonProperties{
 						Name:              n,
@@ -157,7 +238,11 @@ func buildKindStateReport() *KindStateReport {
 						Maturity:          "planned",
 					},
 				}
-				r.add(props, "composable")
+				r.add(Kind{
+					SomeKindProperties: ck,
+					Category:           category,
+					GoRef:              goRef,
+				})
 			}
 		}
 	}
@@ -169,6 +254,44 @@ func buildKindStateReport() *KindStateReport {
 	}
 
 	return r
+}
+
+func buildDocsRef(category string, props kindsys.CommonProperties) string {
+	return path.Join(docsBaseURL, category, props.MachineName, "schema-reference")
+}
+
+func buildCoreTSRef(lin thema.Lineage, decl kindsys.Decl[kindsys.CoreProperties]) string {
+	vpath := fmt.Sprintf("v%v", lin.Latest().Version()[0])
+	if decl.Properties.Common().Maturity.Less(kindsys.MaturityStable) {
+		vpath = "x"
+	}
+
+	return path.Join(repoBaseURL, fmt.Sprintf(coreTSPath, decl.Properties.MachineName, vpath, decl.Properties.MachineName))
+}
+
+func buildComposableTSRef(pType plugindef.Type, cp kindsys.ComposableProperties) string {
+	pName := strings.Replace(cp.MachineName, strings.ToLower(cp.SchemaInterface), "", 1)
+	return path.Join(repoBaseURL, fmt.Sprintf(composableTSPath, string(pType), pName))
+}
+
+func buildCoreGoRef(cp kindsys.CommonProperties) string {
+	return path.Join(repoBaseURL, fmt.Sprintf(coreGoPath, cp.MachineName, cp.MachineName))
+}
+
+func buildComposableGoRef(pType plugindef.Type, cp kindsys.ComposableProperties) string {
+	schemaInterface := strings.ToLower(cp.SchemaInterface)
+	pName := strings.Replace(cp.MachineName, schemaInterface, "", 1)
+	return path.Join(repoBaseURL, fmt.Sprintf(composableGoPath, pName, schemaInterface))
+}
+
+func buildCoreSchemaRef(cp kindsys.CommonProperties) string {
+	return path.Join(repoBaseURL, fmt.Sprintf(coreCUEPath, cp.MachineName, cp.MachineName))
+}
+
+func buildComposableSchemaRef(pType plugindef.Type, cp kindsys.ComposableProperties) string {
+	schemaInterface := strings.ToLower(cp.SchemaInterface)
+	pName := strings.Replace(cp.MachineName, schemaInterface, "", 1)
+	return path.Join(repoBaseURL, fmt.Sprintf(composableCUEPath, string(pType), pName, schemaInterface))
 }
 
 func machinize(s string) string {
@@ -188,6 +311,10 @@ func machinize(s string) string {
 			return -1
 		}
 	}, s)
+}
+
+func toCamelCase(s string) string {
+	return strings.ToLower(string(s[0])) + s[1:]
 }
 
 type reportJenny struct{}
