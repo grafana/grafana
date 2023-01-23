0e8a9e5f0dd50619c674d7f697e43b8462fc2e13
Torkel Ödegaard <torkel@grafana.com>
2023-01-23 16:10:38 +0100

    QueryEditorRow: Fixes issue loading query editor when data source variable selected

--- public/app/features/query/components/QueryEditorRow.tsx
+++ public/app/features/query/components/QueryEditorRow.tsx
@@ -241,7 +241,7 @@ export class QueryEditorRow<TQuery extends DataQuery> extends PureComponent<Prop
   waitingForDatasourceToLoad = (): boolean => {
     // if we not yet have loaded the datasource in state the
     // ds in props and the ds in state will have different values.
-    return this.props.dataSource.uid !== this.state.datasource?.uid;
+    return this.props.dataSource.type !== this.state.datasource?.meta.id;
   };
 
   renderPluginEditor = () => {
