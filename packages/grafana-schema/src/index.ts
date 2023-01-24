1d0571d1ddd07847c760191ab53bf2c43170e968
Clarity-89 <homes89@ukr.net>
2023-01-24 17:45:50 +0200

    Remove data frame related types

--- packages/grafana-schema/src/index.ts
+++ packages/grafana-schema/src/index.ts
@@ -5,17 +5,3 @@
  */
 export * from './veneer/common.types';
 export * from './index.gen';
-// TODO fix these duplicates
-export type {
-  ValueMappingResult,
-  ValueMap,
-  ValueMapping,
-  FieldConfig,
-  MappingType,
-  RangeMap,
-  RegexMap,
-  SpecialValueMap,
-  Threshold,
-  ThresholdsConfig,
-} from './index.gen';
-export { defaultFieldConfig, defaultThresholdsConfig, ThresholdsMode } from './index.gen';
