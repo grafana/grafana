6ff0d415b9d76315731a88f959a92ac381a79045
eledobleefe <laura.fernandez@grafana.com>
2023-01-25 12:17:47 +0100

    Get rid of Input changes as they should be in a different PR

--- packages/grafana-ui/src/components/Input/Input.tsx
+++ packages/grafana-ui/src/components/Input/Input.tsx
@@ -61,7 +61,7 @@ export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
           {...restProps}
           style={{
             paddingLeft: prefix ? prefixRect.width + 12 : undefined,
-            paddingRight: suffix || loading ? (suffix ? suffixRect.width + 12 : 0) : undefined,
+            paddingRight: suffix || loading ? suffixRect.width + 12 : undefined,
           }}
         />
 
@@ -171,8 +171,7 @@ export const getInputStyles = stylesFactory(({ theme, invalid = false, width }: 
         &:not(:first-child) {
           padding-left: ${prefixSuffixStaticWidth};
         }
-        &:not(:last-child),
-        &:not(& + div > [data-testid='Spinner']) {
+        &:not(:last-child) {
           padding-right: ${prefixSuffixStaticWidth};
         }
         &[readonly] {
