6bd11e0ebfa388723dd9ae7e83f5fdc6dac2f237
Eric Leijonmarck <eric.leijonmarck@gmail.com>
2023-01-25 15:16:08 +0100

    Auth: Add skip_org_role_sync setting for github (#61673)
    * add: skip_org_role_sync setting for github
    
    * fix: frontend
    
    * rearranged tests
    
    * refactor: assignGrafanaAdmin skip also
    
    * Add: tests for allowGrafanaAdmin
    
    - both for the case when both settings are set and the setting for only
      allowGrafanaAdmin
    
    * Apply suggestions from code review
    
    Co-authored-by: Christopher Moyer <35463610+chri2547@users.noreply.github.com>
    
    * Update docs/sources/setup-grafana/configure-grafana/_index.md
    
    Co-authored-by: Christopher Moyer <35463610+chri2547@users.noreply.github.com>
    
    * Update pkg/login/social/github_oauth.go
    
    Co-authored-by: Ieva <ieva.vasiljeva@grafana.com>
    
    * added vairable inside scope
    
    * Update docs/sources/setup-grafana/configure-security/configure-authentication/github/index.md
    
    * Update docs/sources/setup-grafana/configure-security/configure-authentication/github/index.md
    
    Co-authored-by: Christopher Moyer <35463610+chri2547@users.noreply.github.com>
    Co-authored-by: Ieva <ieva.vasiljeva@grafana.com>

--- pkg/api/frontendsettings.go
+++ pkg/api/frontendsettings.go
@@ -148,6 +148,7 @@ func (hs *HTTPServer) getFrontendSettingsMap(c *models.ReqContext) (map[string]i
 			"OAuthSkipOrgRoleUpdateSync": hs.Cfg.OAuthSkipOrgRoleUpdateSync,
 			"SAMLSkipOrgRoleSync":        hs.Cfg.SectionWithEnvOverrides("auth.saml").Key("skip_org_role_sync").MustBool(false),
 			"LDAPSkipOrgRoleSync":        hs.Cfg.LDAPSkipOrgRoleSync,
+			"GithubSkipOrgRoleSync":      hs.Cfg.GithubSkipOrgRoleSync,
 			"GoogleSkipOrgRoleSync":      hs.Cfg.GoogleSkipOrgRoleSync,
 			"JWTAuthSkipOrgRoleSync":     hs.Cfg.JWTAuthSkipOrgRoleSync,
 			"GrafanaComSkipOrgRoleSync":  hs.Cfg.GrafanaComSkipOrgRoleSync,
