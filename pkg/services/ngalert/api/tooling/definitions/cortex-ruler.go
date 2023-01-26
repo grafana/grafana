7fa2e7b622050cecaeaf8275fe10089307438889
Alex Moreno <alexander.moreno@grafana.com>
2023-01-26 18:44:03 +0100

    Add is_paused attr to the POST alert rule group endpoint

--- pkg/services/ngalert/api/tooling/definitions/cortex-ruler.go
+++ pkg/services/ngalert/api/tooling/definitions/cortex-ruler.go
@@ -374,6 +374,7 @@ type PostableGrafanaRule struct {
 	UID          string              `json:"uid" yaml:"uid"`
 	NoDataState  NoDataState         `json:"no_data_state" yaml:"no_data_state"`
 	ExecErrState ExecutionErrorState `json:"exec_err_state" yaml:"exec_err_state"`
+	IsPaused     bool                `json:"is_paused" yaml:"is_paused"`
 }
 
 // swagger:model
