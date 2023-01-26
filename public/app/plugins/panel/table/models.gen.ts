89b40ed7e4892a3bf6510839b5d7ceca28a1fea9
jev forsberg <jev.forsberg@grafana.com>
2023-01-26 10:35:00 -0700

    baldm0mma/addRowNums/ remove double prop

--- public/app/plugins/panel/table/models.gen.ts
+++ public/app/plugins/panel/table/models.gen.ts
@@ -15,7 +15,6 @@ export const modelVersion = Object.freeze([1, 0]);
 export interface PanelOptions {
   frameIndex: number;
   showHeader: boolean;
-  showRowNumbers?: boolean;
   showTypeIcons?: boolean;
   showRowNumbers?: boolean;
   sortBy?: TableSortByFieldState[];
