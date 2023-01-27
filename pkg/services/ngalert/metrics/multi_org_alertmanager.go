0dd543880aef1f02a99a2acde292470237700ed0
gotjosh <josue.abreu@gmail.com>
2023-01-27 18:06:40 +0000

    Alerting: Refactor metrics/ngalert.go into seperate files

---
+++ pkg/services/ngalert/metrics/multi_org_alertmanager.go
@@ -0,0 +1,32 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+type MultiOrgAlertmanager struct {
+	Registerer               prometheus.Registerer
+	ActiveConfigurations     prometheus.Gauge
+	DiscoveredConfigurations prometheus.Gauge
+	registries               *OrgRegistries
+}
+
+func newMultiOrgAlertmanagerMetrics(r prometheus.Registerer) *MultiOrgAlertmanager {
+	return &MultiOrgAlertmanager{
+		Registerer: r,
+		registries: NewOrgRegistries(),
+		DiscoveredConfigurations: promauto.With(r).NewGauge(prometheus.GaugeOpts{
+			Namespace: Namespace,
+			Subsystem: Subsystem,
+			Name:      "discovered_configurations",
+			Help:      "The number of organizations we've discovered that require an Alertmanager configuration.",
+		}),
+		ActiveConfigurations: promauto.With(r).NewGauge(prometheus.GaugeOpts{
+			Namespace: Namespace,
+			Subsystem: Subsystem,
+			Name:      "active_configurations",
+			Help:      "The number of active Alertmanager configurations.",
+		}),
+	}
+}
