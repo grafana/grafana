f41adb400f8801dfe4e7e188946d6ea824b814ac
Alex Moreno <alexander.moreno@grafana.com>
2023-01-27 12:53:19 +0100

    Add provisioning changes

--- pkg/services/ngalert/api/tooling/definitions/provisioning_alert_rules.go
+++ pkg/services/ngalert/api/tooling/definitions/provisioning_alert_rules.go
@@ -111,6 +111,8 @@ type ProvisionedAlertRule struct {
 	Labels map[string]string `json:"labels,omitempty"`
 	// readonly: true
 	Provenance models.Provenance `json:"provenance,omitempty"`
+	// example: false
+	IsPaused bool `json:"isPaused"`
 }
 
 func (a *ProvisionedAlertRule) UpstreamModel() (models.AlertRule, error) {
@@ -129,6 +131,7 @@ func (a *ProvisionedAlertRule) UpstreamModel() (models.AlertRule, error) {
 		For:          time.Duration(a.For),
 		Annotations:  a.Annotations,
 		Labels:       a.Labels,
+		IsPaused:     &a.IsPaused,
 	}, nil
 }
 
@@ -149,6 +152,7 @@ func NewAlertRule(rule models.AlertRule, provenance models.Provenance) Provision
 		Annotations:  rule.Annotations,
 		Labels:       rule.Labels,
 		Provenance:   provenance,
+		IsPaused:     *rule.IsPaused,
 	}
 }
 
