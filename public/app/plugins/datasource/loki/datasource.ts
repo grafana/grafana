d0e95f8c9509eb6fe681283af63f938b51e78473
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2023-01-27 16:41:40 +0100

    Loki: Add X-Query-Tags header for logs sample and data sample (#62333)

--- public/app/plugins/datasource/loki/datasource.ts
+++ public/app/plugins/datasource/loki/datasource.ts
@@ -86,6 +86,7 @@ import {
   LokiQueryType,
   LokiVariableQuery,
   LokiVariableQueryType,
+  SupportingQueryType,
 } from './types';
 import { LokiVariableSupport } from './variables';
 
@@ -191,7 +192,7 @@ export class LokiDatasource
           ...normalizedQuery,
           refId: `${REF_ID_STARTER_LOG_VOLUME}${normalizedQuery.refId}`,
           instant: false,
-          volumeQuery: true,
+          supportingQueryType: SupportingQueryType.LogsVolume,
           expr: `sum by (level) (count_over_time(${expr}[$__interval]))`,
         };
 
