openai-oss-forks/grafana-experiments

Login: Fix panic when UpsertUser is called without ReqContext (#62539)

pkg/services/login/loginservice/loginservice_test.go
@@ -137,6 +137,28 @@ func Test_teamSync(t *testing.T) {
 	})
 }
 
+func TestUpsertUser_crashOnLog_issue62538(t *testing.T) {
+	authInfoMock := &logintest.AuthInfoServiceFake{}
+	authInfoMock.ExpectedError = user.ErrUserNotFound
+	loginsvc := Implementation{
+		QuotaService:    quotatest.New(false, nil),
+		AuthInfoService: authInfoMock,
+	}
+
+	email := "test_user@example.org"
+	upsertCmd := &login.UpsertUserCommand{
+		ExternalUser:     &login.ExternalUserInfo{Email: email},
+		UserLookupParams: login.UserLookupParams{Email: &email},
+		SignupAllowed:    false,
+	}
+
+	var err error
+	require.NotPanics(t, func() {
+		err = loginsvc.UpsertUser(context.Background(), upsertCmd)
+	})
+	require.ErrorIs(t, err, login.ErrSignupNotAllowed)
+}
+
 func createSimpleUser() user.User {
 	user := user.User{
 		ID: 1,

grafana/grafana

(cherry picked from commit b1151dd118f2a1c305a6f757661e42e59cf145c1)

pkg/services/login/loginservice/loginservice_test.go
@@ -6,14 +6,17 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/grafana/grafana/pkg/services/login"
+
 	"github.com/go-kit/log"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/grafana/grafana/pkg/infra/log/level"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/login/logintest"
 	"github.com/grafana/grafana/pkg/services/quota"
 	"github.com/grafana/grafana/pkg/services/sqlstore/mockstore"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_syncOrgRoles_doesNotBreakWhenTryingToRemoveLastOrgAdmin(t *testing.T) {
@@ -110,6 +113,28 @@ func Test_teamSync(t *testing.T) {
 			require.Error(t, err)
 		})
 	})
+}
+
+func TestUpsertUser_crashOnLog_issue62538(t *testing.T) {
+	authInfoMock := &logintest.AuthInfoServiceFake{}
+	authInfoMock.ExpectedError = models.ErrUserNotFound
+	loginsvc := Implementation{
+		QuotaService:    &quota.QuotaService{},
+		AuthInfoService: authInfoMock,
+	}
+
+	email := "test_user@example.org"
+	upsertCmd := &models.UpsertUserCommand{
+		ExternalUser:     &models.ExternalUserInfo{Email: email},
+		UserLookupParams: models.UserLookupParams{Email: &email},
+		SignupAllowed:    false,
+	}
+
+	var err error
+	require.NotPanics(t, func() {
+		err = loginsvc.UpsertUser(context.Background(), upsertCmd)
+	})
+	require.ErrorIs(t, err, login.ErrSignupNotAllowed)
 }
 
 func createSimpleUser() models.User {