073738464f208675e780bc8bb322ed7c13eb0a6b
Todd Treece <todd.treece@grafana.com>
2023-02-01 16:07:37 -0500

    convert object in controller back to k8ssys.Base[dashboard.Dashboard]

--- pkg/services/dashboards/service/controller.go
+++ pkg/services/dashboards/service/controller.go
@@ -6,7 +6,11 @@ import (
 	"time"
 
 	"github.com/grafana/grafana/pkg/apimachinery/bridge"
+	"github.com/grafana/grafana/pkg/kinds/dashboard"
+	"github.com/grafana/grafana/pkg/kindsys/k8ssys"
 	"github.com/grafana/grafana/pkg/registry/corecrd"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/client-go/tools/cache"
@@ -40,11 +44,29 @@ func (c *DashboardController) Run(ctx context.Context) error {
 
 	dashboardInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			fmt.Printf("dashboard added: %s \n", obj)
+			uObj, ok := obj.(*unstructured.Unstructured)
+			if !ok {
+				fmt.Println("dashboard add: failed to convert to dashboard")
+				return
+			}
+
+			dash := k8ssys.Base[dashboard.Dashboard]{}
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(uObj.UnstructuredContent(), &dash)
+			if err != nil {
+				fmt.Println("dashboard add: failed to convert to dashboard", err)
+				return
+			}
+
+			fmt.Printf("dashboard added: %+v \n", dash)
 			//c.dashboardService.SaveDashboard()
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
-			fmt.Printf("dashboard changed:%s \n %s \n", oldObj, newObj)
+			dash, ok := newObj.(*k8ssys.Base[dashboard.Dashboard])
+			if !ok {
+				fmt.Println("dashboard update: failed to convert to dashboard")
+				return
+			}
+			fmt.Printf("dashboard updated: %+v \n", dash)
 			//c.dashboardService.SaveDashboard()
 		},
 		DeleteFunc: func(obj interface{}) {
