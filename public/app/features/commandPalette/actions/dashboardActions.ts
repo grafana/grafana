9124367a5c20d6ca4963eed441efc94188b31247
Ashley Harrison <ashley.harrison@grafana.com>
2023-02-01 10:08:15 +0000

    only search for dashboards when not logged in if anon access is enabled

--- public/app/features/commandPalette/actions/dashboardActions.ts
+++ public/app/features/commandPalette/actions/dashboardActions.ts
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { locationUtil } from '@grafana/data';
 import { config } from '@grafana/runtime';
 import { t } from 'app/core/internationalization';
+import { contextSrv } from 'app/core/services/context_srv';
 import impressionSrv from 'app/core/services/impression_srv';
 import { getGrafanaSearcher } from 'app/features/search/service';
 
@@ -16,42 +17,42 @@ const MAX_RECENT_DASHBOARDS = 5;
 const debouncedDashboardSearch = debounce(getDashboardSearchResultActions, 200);
 
 export async function getRecentDashboardActions(): Promise<CommandPaletteAction[]> {
-  if (config.bootData.user.isSignedIn) {
-    const recentUids = (await impressionSrv.getDashboardOpened()).slice(0, MAX_RECENT_DASHBOARDS);
-    const resultsDataFrame = await getGrafanaSearcher().search({
-      kind: ['dashboard'],
-      limit: MAX_RECENT_DASHBOARDS,
-      uid: recentUids,
-    });
-
-    // Search results are alphabetical, so reorder them according to recently viewed
-    const recentResults = resultsDataFrame.view.toArray();
-    recentResults.sort((resultA, resultB) => {
-      const orderA = recentUids.indexOf(resultA.uid);
-      const orderB = recentUids.indexOf(resultB.uid);
-      return orderA - orderB;
-    });
-
-    const recentDashboardActions: CommandPaletteAction[] = recentResults.map((item) => {
-      const { url, name } = item; // items are backed by DataFrameView, so must hold the url in a closure
-      return {
-        id: `recent-dashboards${url}`,
-        name: `${name}`,
-        section: t('command-palette.section.recent-dashboards', 'Recent dashboards'),
-        priority: RECENT_DASHBOARDS_PRORITY,
-        url: locationUtil.stripBaseFromUrl(url),
-      };
-    });
-
-    return recentDashboardActions;
-  } else {
+  if (!contextSrv.user.isSignedIn) {
     return [];
   }
+
+  const recentUids = (await impressionSrv.getDashboardOpened()).slice(0, MAX_RECENT_DASHBOARDS);
+  const resultsDataFrame = await getGrafanaSearcher().search({
+    kind: ['dashboard'],
+    limit: MAX_RECENT_DASHBOARDS,
+    uid: recentUids,
+  });
+
+  // Search results are alphabetical, so reorder them according to recently viewed
+  const recentResults = resultsDataFrame.view.toArray();
+  recentResults.sort((resultA, resultB) => {
+    const orderA = recentUids.indexOf(resultA.uid);
+    const orderB = recentUids.indexOf(resultB.uid);
+    return orderA - orderB;
+  });
+
+  const recentDashboardActions: CommandPaletteAction[] = recentResults.map((item) => {
+    const { url, name } = item; // items are backed by DataFrameView, so must hold the url in a closure
+    return {
+      id: `recent-dashboards${url}`,
+      name: `${name}`,
+      section: t('command-palette.section.recent-dashboards', 'Recent dashboards'),
+      priority: RECENT_DASHBOARDS_PRORITY,
+      url: locationUtil.stripBaseFromUrl(url),
+    };
+  });
+
+  return recentDashboardActions;
 }
 
 export async function getDashboardSearchResultActions(searchQuery: string): Promise<CommandPaletteAction[]> {
   // Empty strings should not come through to here
-  if (searchQuery.length === 0) {
+  if (searchQuery.length === 0 || (!contextSrv.user.isSignedIn && !config.bootData.settings.anonymousEnabled)) {
     return [];
   }
 
