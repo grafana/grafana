0d77040ce78612a624f8f551d231ab17649b11df
jev forsberg <jev.forsberg@grafana.com>
2023-02-01 16:24:53 -0700

    baldm0mma/bug-62410/ remove function incorrectly calculating table height

--- packages/grafana-ui/src/components/Table/Table.tsx
+++ packages/grafana-ui/src/components/Table/Table.tsx
@@ -34,6 +34,7 @@ import {
 } from './utils';
 
 const COLUMN_MIN_WIDTH = 150;
+const FOOTER_ROW_HEIGHT = 36;
 
 export const Table = memo((props: Props) => {
   const {
@@ -63,25 +64,12 @@ export const Table = memo((props: Props) => {
   const [footerItems, setFooterItems] = useState<FooterItem[] | undefined>(footerValues);
 
   const footerHeight = useMemo(() => {
-    const EXTENDED_ROW_HEIGHT = headerHeight;
-    let length = 0;
-
-    if (!footerItems) {
+    if (!footerItems?.length) {
       return 0;
     }
 
-    for (const fv of footerItems) {
-      if (Array.isArray(fv) && fv.length > length) {
-        length = fv.length;
-      }
-    }
-
-    if (length > 1) {
-      return EXTENDED_ROW_HEIGHT * length;
-    }
-
-    return EXTENDED_ROW_HEIGHT;
-  }, [footerItems, headerHeight]);
+    return FOOTER_ROW_HEIGHT;
+  }, [footerItems]);
 
   // React table data array. This data acts just like a dummy array to let react-table know how many rows exist
   // The cells use the field to look up values
