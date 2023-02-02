dfa18588a49671644174f63b2c554c1c430beeae
Nicolas Dupeux <nicolas.dupeux@corp.ovh.com>
2023-02-02 11:36:33 +0100

    [opentsdb] Allow custom tag key in OpenTSDB Query editor

--- public/app/plugins/datasource/opentsdb/components/TagSection.tsx
+++ public/app/plugins/datasource/opentsdb/components/TagSection.tsx
@@ -149,6 +149,7 @@ export function TagSection({
               className="gf-form-input"
               value={curTagKey ? toOption('' + curTagKey) : undefined}
               placeholder="key"
+              allowCustomValue
               onOpenMenu={async () => {
                 updKeyIsLoading(true);
                 const tKs = await suggestTagKeys(query);
