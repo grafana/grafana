eee1dd8f230043f7dd4fbc5849e6510aae09a6dc
Jack Westbrook <jack.westbrook@gmail.com>
2023-02-06 11:15:58 +0100

    test(extensions): fix up failing tests

--- public/app/features/plugins/extensions/registry.test.ts
+++ public/app/features/plugins/extensions/registry.test.ts
@@ -1,87 +1,94 @@
-import type { AppPluginConfig, PluginsExtensionConfig } from '@grafana/runtime';
+import { EXPECTED_COLOR } from 'jest-matcher-utils';
+
+import { AppPluginConfig, PluginExtensionTypes, PluginsExtensionLinkConfig } from '@grafana/runtime';
 
 import { createPluginExtensionsRegistry } from './registry';
 
 describe('Plugin registry', () => {
   describe('createPluginExtensionsRegistry function', () => {
     const registry = createPluginExtensionsRegistry({
-      'belugacdn-app': createConfig({
-        links: [
-          {
-            id: 'declare-incident',
-            description: 'Incidents are occurring!',
-            path: '/incidents/declare',
-          },
-        ],
-      }),
-      'strava-app': createConfig({
-        links: [
-          {
-            id: 'declare-incident',
-            description: 'Incidents are occurring!',
-            path: '/incidents/declare',
-          },
-        ],
-      }),
-      'duplicate-links-app': createConfig({
-        links: [
-          {
-            id: 'declare-incident',
-            description: 'Incidents are occurring!',
-            path: '/incidents/declare',
-          },
-          {
-            id: 'declare-incident',
-            description: 'Incidents are occurring!',
-            path: '/incidents/declare2',
-          },
-        ],
-      }),
+      'belugacdn-app': createConfig([
+        {
+          target: 'plugins/belugacdn-app/menu',
+          title: 'The title',
+          type: PluginExtensionTypes.link,
+          description: 'Incidents are occurring!',
+          path: '/incidents/declare',
+        },
+      ]),
+      'strava-app': createConfig([
+        {
+          target: 'plugins/strava-app/menu',
+          title: 'The title',
+          type: PluginExtensionTypes.link,
+          description: 'Incidents are occurring!',
+          path: '/incidents/declare',
+        },
+      ]),
+      'duplicate-links-app': createConfig([
+        {
+          target: 'plugins/duplicate-links-app/menu',
+          title: 'The title',
+          type: PluginExtensionTypes.link,
+          description: 'Incidents are occurring!',
+          path: '/incidents/declare',
+        },
+        {
+          target: 'plugins/duplicate-links-app/menu',
+          title: 'The title',
+          type: PluginExtensionTypes.link,
+          description: 'Incidents are occurring!',
+          path: '/incidents/declare2',
+        },
+      ]),
       'no-extensions-app': createConfig(undefined),
     });
 
     it('should configure a registry link', () => {
-      const link = registry.links['belugacdn-app.declare-incident'];
+      const [link] = registry['plugins/belugacdn-app/menu'];
 
       expect(link).toEqual({
+        title: 'The title',
+        type: 'link',
         description: 'Incidents are occurring!',
         href: '/a/belugacdn-app/incidents/declare',
       });
     });
 
-    it('should configure all registry links', () => {
-      const numberOfLinks = Object.keys(registry.links).length;
+    it('should configure all registry targets', () => {
+      const numberOfTargets = Object.keys(registry).length;
 
-      expect(numberOfLinks).toBe(3);
+      expect(numberOfTargets).toBe(3);
     });
 
-    it('should configure registry links from multiple plugins', () => {
-      const pluginALink = registry.links['belugacdn-app.declare-incident'];
-      const pluginBLink = registry.links['strava-app.declare-incident'];
+    it('should configure registry targets from multiple plugins', () => {
+      const [pluginALink] = registry['plugins/belugacdn-app/menu'];
+      const [pluginBLink] = registry['plugins/strava-app/menu'];
 
       expect(pluginALink).toEqual({
+        title: 'The title',
+        type: 'link',
         description: 'Incidents are occurring!',
         href: '/a/belugacdn-app/incidents/declare',
       });
 
       expect(pluginBLink).toEqual({
+        title: 'The title',
+        type: 'link',
         description: 'Incidents are occurring!',
         href: '/a/strava-app/incidents/declare',
       });
     });
 
-    it('should configure first link when duplicates exist', () => {
-      const link = registry.links['duplicate-links-app.declare-incident'];
+    it('should configure multiple links for a single target', () => {
+      const links = registry['plugins/duplicate-links-app/menu'];
 
-      expect(link).toEqual({
-        description: 'Incidents are occurring!',
-        href: '/a/duplicate-links-app/incidents/declare',
-      });
+      expect(links.length).toBe(2);
     });
   });
 });
 
-function createConfig(extensions?: PluginsExtensionConfig): AppPluginConfig {
+function createConfig(extensions?: PluginsExtensionLinkConfig[]): AppPluginConfig {
   return {
     id: 'myorg-basic-app',
     preload: false,
