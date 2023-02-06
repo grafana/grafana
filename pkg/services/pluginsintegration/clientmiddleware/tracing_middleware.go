b379dabe6ba980165d2ebc811755f005a6d4cf91
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2023-02-06 11:11:20 +0100

    Tracing middleware: handle errors

--- pkg/services/pluginsintegration/clientmiddleware/tracing_middleware.go
+++ pkg/services/pluginsintegration/clientmiddleware/tracing_middleware.go
@@ -3,6 +3,8 @@ package clientmiddleware
 import (
 	"context"
 
+	"go.opentelemetry.io/otel/codes"
+	
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/plugins"
@@ -23,44 +25,62 @@ type TracingMiddleware struct {
 	next   plugins.Client
 }
 
+func (m *TracingMiddleware) traceWrap(ctx context.Context, opName string) (context.Context, func(error)) {
+	ctx, span := m.tracer.Start(ctx, opName)
+	return ctx, func(err error) {
+		span.End()
+		if err != nil {
+			span.SetStatus(codes.Error, opName+" error")
+			span.RecordError(err)
+		}
+	}
+}
+
 func (m *TracingMiddleware) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	ctx, span := m.tracer.Start(ctx, "queryData")
-	defer span.End()
-	return m.next.QueryData(ctx, req)
+	ctx, end := m.traceWrap(ctx, "queryData")
+	resp, err := m.next.QueryData(ctx, req)
+	end(err)
+	return resp, err
 }
 
 func (m *TracingMiddleware) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	ctx, span := m.tracer.Start(ctx, "callResource")
-	defer span.End()
-	return m.next.CallResource(ctx, req, sender)
+	ctx, end := m.traceWrap(ctx, "callResource")
+	err := m.next.CallResource(ctx, req, sender)
+	end(err)
+	return err
 }
 
 func (m *TracingMiddleware) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	ctx, span := m.tracer.Start(ctx, "checkHealth")
-	defer span.End()
-	return m.next.CheckHealth(ctx, req)
+	ctx, end := m.traceWrap(ctx, "checkHealth")
+	resp, err := m.next.CheckHealth(ctx, req)
+	end(err)
+	return resp, err
 }
 
 func (m *TracingMiddleware) CollectMetrics(ctx context.Context, req *backend.CollectMetricsRequest) (*backend.CollectMetricsResult, error) {
-	ctx, span := m.tracer.Start(ctx, "collectMetrics")
-	defer span.End()
-	return m.next.CollectMetrics(ctx, req)
+	ctx, end := m.traceWrap(ctx, "collectMetrics")
+	resp, err := m.next.CollectMetrics(ctx, req)
+	end(err)
+	return resp, err
 }
 
 func (m *TracingMiddleware) SubscribeStream(ctx context.Context, req *backend.SubscribeStreamRequest) (*backend.SubscribeStreamResponse, error) {
-	ctx, span := m.tracer.Start(ctx, "subscribeStream")
-	defer span.End()
-	return m.next.SubscribeStream(ctx, req)
+	ctx, end := m.traceWrap(ctx, "subscribeStream")
+	resp, err := m.next.SubscribeStream(ctx, req)
+	end(err)
+	return resp, err
 }
 
 func (m *TracingMiddleware) PublishStream(ctx context.Context, req *backend.PublishStreamRequest) (*backend.PublishStreamResponse, error) {
-	ctx, span := m.tracer.Start(ctx, "publishStream")
-	defer span.End()
-	return m.next.PublishStream(ctx, req)
+	ctx, end := m.traceWrap(ctx, "publishStream")
+	resp, err := m.next.PublishStream(ctx, req)
+	end(err)
+	return resp, err
 }
 
 func (m *TracingMiddleware) RunStream(ctx context.Context, req *backend.RunStreamRequest, sender *backend.StreamSender) error {
-	ctx, span := m.tracer.Start(ctx, "runStream")
-	defer span.End()
-	return m.next.RunStream(ctx, req, sender)
+	ctx, end := m.traceWrap(ctx, "runStream")
+	err := m.next.RunStream(ctx, req, sender)
+	end(err)
+	return err
 }
