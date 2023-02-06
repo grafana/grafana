bcfabbd167fb05c191b83c5dcb7367d14a204b85
Ashley Harrison <ashley.harrison@grafana.com>
2023-02-06 13:19:29 +0000

    fix some more any/type assertions

--- public/app/plugins/datasource/testdata/components/SimulationQueryEditor.tsx
+++ public/app/plugins/datasource/testdata/components/SimulationQueryEditor.tsx
@@ -31,7 +31,7 @@ export const SimulationQueryEditor = ({ onChange, query, ds }: EditorProps) => {
 
   // This only changes once
   const info = useAsync(async () => {
-    const v = (await ds.getResource('sims')) as SimInfo[];
+    const v: SimInfo[] = await ds.getResource('sims');
     return {
       sims: v,
       options: v.map((s) => ({ label: s.name, value: s.type, description: s.description })),
