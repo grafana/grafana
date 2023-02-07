6c2ab21758423be604fc2c8a77f16afbd6c957c4
Eric Leijonmarck <eric.leijonmarck@gmail.com>
2023-02-07 09:27:08 +0000

    added exhaustive tests

--- pkg/services/login/authinfo_test.go
+++ pkg/services/login/authinfo_test.go
@@ -15,22 +15,195 @@ func TestIsExternallySynced(t *testing.T) {
 		expected bool
 	}{
 		{
+			name:     "AzureAD synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{AzureADSkipOrgRoleSync: false},
+			provider: AzureADLabel,
+			expected: true,
+		},
+		{
+			name:     "AzureAD synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{AzureADSkipOrgRoleSync: true},
+			provider: AzureADLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{AzureADSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: AzureADLabel,
+			expected: false,
+		},
+		{
 			name:     "Google synced user should return that it is externally synced",
 			cfg:      &setting.Cfg{GoogleSkipOrgRoleSync: false},
-			provider: "Google",
+			provider: GoogleLabel,
 			expected: true,
 		},
 		{
 			name:     "Google synced user should return that it is not externally synced when org role sync is set",
 			cfg:      &setting.Cfg{GoogleSkipOrgRoleSync: true},
-			provider: "Google",
+			provider: GoogleLabel,
 			expected: false,
 		},
-		// deprecated setting for skipping org role sync for all external oauth providers
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
 		{
 			name:     "external user should return that it is not externally synced when oauth org role sync is set",
 			cfg:      &setting.Cfg{GoogleSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
-			provider: "Google",
+			provider: GoogleLabel,
+			expected: false,
+		},
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set and google skip org role sync set",
+			cfg:      &setting.Cfg{GoogleSkipOrgRoleSync: true, OAuthSkipOrgRoleUpdateSync: true},
+			provider: GoogleLabel,
+			expected: false,
+		},
+		{
+			name:     "Okta synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{OktaSkipOrgRoleSync: false},
+			provider: OktaLabel,
+			expected: true,
+		},
+		{
+			name: "Okta synced user should return that it is not externally synced when org role sync is set",
+			cfg:  &setting.Cfg{OktaSkipOrgRoleSync: true},
+
+			provider: OktaLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{OktaSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: OktaLabel,
+			expected: false,
+		},
+		{
+			name:     "Github synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{GithubSkipOrgRoleSync: false},
+			provider: GithubLabel,
+			expected: true,
+		},
+		{
+			name:     "Github synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{GithubSkipOrgRoleSync: true},
+			provider: GithubLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{GithubSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: GithubLabel,
+			expected: false,
+		},
+		// gitlab
+		{
+			name:     "Gitlab synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{GitLabSkipOrgRoleSync: false},
+			provider: GitLabLabel,
+			expected: true,
+		},
+		{
+			name:     "Gitlab synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{GitLabSkipOrgRoleSync: true},
+			provider: GitLabLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{GitLabSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: GitLabLabel,
+			expected: false,
+		},
+		// grafana.com
+		{
+			name:     "Grafana.com synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{GrafanaComSkipOrgRoleSync: false},
+			provider: GrafanaComLabel,
+			expected: true,
+		},
+		{
+			name:     "Grafana.com synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{GrafanaComSkipOrgRoleSync: true},
+			provider: GrafanaComLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{GrafanaComSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: GrafanaComLabel,
+			expected: false,
+		},
+		{
+			name:     "Generic OAuth synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{OAuthSkipOrgRoleUpdateSync: false},
+			provider: GenericOAuthLabel,
+			expected: true,
+		},
+		{
+			name:     "Generic OAuth synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{OAuthSkipOrgRoleUpdateSync: true},
+			provider: GenericOAuthLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{GenericOAuthSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: GenericOAuthLabel,
+			expected: false,
+		},
+		{
+			name:     "SAML synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{SAMLSkipOrgRoleSync: false},
+			provider: SAMLLabel,
+			expected: true,
+		},
+		{
+			name:     "SAML synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{SAMLSkipOrgRoleSync: true},
+			provider: SAMLLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{SAMLSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: SAMLLabel,
+			expected: false,
+		},
+		{
+			name:     "LDAP synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{LDAPSkipOrgRoleSync: false},
+			provider: LDAPLabel,
+			expected: true,
+		},
+		{
+			name:     "LDAP synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{LDAPSkipOrgRoleSync: true},
+			provider: LDAPLabel,
+			expected: false,
+		},
+		{
+			name:     "JWT synced user should return that it is externally synced",
+			cfg:      &setting.Cfg{JWTAuthSkipOrgRoleSync: false},
+			provider: JWTLabel,
+			expected: true,
+		},
+		{
+			name:     "JWT synced user should return that it is not externally synced when org role sync is set",
+			cfg:      &setting.Cfg{JWTAuthSkipOrgRoleSync: true},
+			provider: JWTLabel,
+			expected: false,
+		},
+		// FIXME: remove this test as soon as we remove the deprecated setting for skipping org role sync for all external oauth providers
+		{
+			name:     "external user should return that it is not externally synced when oauth org role sync is set",
+			cfg:      &setting.Cfg{JWTAuthSkipOrgRoleSync: false, OAuthSkipOrgRoleUpdateSync: true},
+			provider: JWTLabel,
 			expected: false,
 		},
 	}
