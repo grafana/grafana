65a2096158ed5423ac5d571c5561e4334d1826ff
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2023-02-07 17:00:16 +0100

    Add TestHTTPClientTracing

---
+++ pkg/services/updatechecker/instrumentation/httpclient_test.go
@@ -0,0 +1,32 @@
+package instrumentation
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/codes"
+	
+	"github.com/grafana/grafana/pkg/infra/tracing"
+)
+
+func TestHTTPClientTracing(t *testing.T) {
+	tracer := tracing.NewFakeTracer()
+	cl := NewInstrumentedHTTPClient(http.DefaultClient, tracer)
+	srv := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+
+	}))
+	t.Cleanup(srv.Close)
+	resp, err := cl.Get(context.Background(), srv.URL)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+
+	require.Len(t, tracer.Spans, 1, "tracer must have started 1 span")
+	span := tracer.Spans[0]
+	assert.True(t, span.Ended, "span should hae ended")
+	assert.Equal(t, codes.Unset, span.StatusCode, "span should have unset status code")
+	assert.NoError(t, span.Err, "span should have no error")
+}
