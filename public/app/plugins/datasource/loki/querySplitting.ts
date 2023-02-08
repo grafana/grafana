e633be6028a80298769a6ad205ffcd3ffedb0e2e
Matias Chomicki <matyax@gmail.com>
2023-02-08 14:49:39 +0100

    Query splitting: catch and raise errors

--- public/app/plugins/datasource/loki/querySplitting.ts
+++ public/app/plugins/datasource/loki/querySplitting.ts
@@ -36,6 +36,9 @@ export function runPartitionedQuery(datasource: LokiDatasource, request: DataQue
 
           subscriber.next(response);
         },
+        error: (error) => {
+          subscriber.error(error);
+        },
       });
   };
 
