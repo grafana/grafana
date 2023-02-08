10cdf6adc42fba8618217d6a2148aa509b2c3146
Todd Treece <todd.treece@grafana.com>
2023-02-08 14:55:33 -0500

    fix lint issue

--- pkg/services/k8s/client/clientset.go
+++ pkg/services/k8s/client/clientset.go
@@ -12,7 +12,6 @@ import (
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	k8schema "k8s.io/apimachinery/pkg/runtime/schema"
 	memory "k8s.io/client-go/discovery/cached"
 	"k8s.io/client-go/dynamic"
@@ -156,7 +155,7 @@ func (c *Clientset) GetResourceClient(gcrd k8ssys.Kind, namespace ...string) (dy
 // GetResourceInformer returns a SharedIndexInformer for the given Kind.
 func (c *Clientset) GetResourceInformer(gcrd k8ssys.Kind) cache.SharedIndexInformer {
 	gvk := gcrd.GVK()
-	gvr := schema.GroupVersionResource{
+	gvr := k8schema.GroupVersionResource{
 		Group:    gvk.Group,
 		Version:  gvk.Version,
 		Resource: gcrd.Schema.Spec.Names.Plural,
