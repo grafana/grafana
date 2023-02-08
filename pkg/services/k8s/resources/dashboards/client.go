64e6cee0f9e9864340bda02cea420ddb4d5f84ad
Todd Treece <todd.treece@grafana.com>
2023-02-08 13:44:27 -0500

    add ProvideClientset

--- pkg/services/k8s/resources/dashboards/client.go
+++ pkg/services/k8s/resources/dashboards/client.go
@@ -3,19 +3,22 @@ package dashboards
 import (
 	"github.com/grafana/grafana/pkg/kinds/dashboard"
 	"github.com/grafana/grafana/pkg/kindsys/k8ssys"
+	"github.com/grafana/grafana/pkg/services/k8s/client"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/cache"
 )
 
+var _ client.Resource = (*Resource)(nil)
+
 type Resource struct {
+	cache.SharedIndexInformer
+	dynamic.ResourceInterface
+
 	crd   *k8ssys.Kind
 	kind  *dashboard.Kind
 	model *dashboard.Dashboard
-
-	informer cache.SharedIndexInformer
-	resource dynamic.ResourceInterface
 }
 
-func ProvideResource() {
-
+func ProvideResource(clientset *client.Clientset) *Resource {
+	return &Resource{}
 }
