ff6251d685a0cbd22063a8f925a714b2eaf6665e
Victor Marin <36818606+mdvictor@users.noreply.github.com>
2023-02-09 10:09:52 +0200

    Fix MSSQL queries failing because of bad interpolation (#63167)
    fix failing mssql queries

--- pkg/tsdb/mssql/macros_test.go
+++ pkg/tsdb/mssql/macros_test.go
@@ -349,6 +349,20 @@ func TestMacroEngine(t *testing.T) {
 				require.NotNil(t, err)
 			}
 		})
+
+		t.Run("should return unmodified sql if there are no macros present", func(t *testing.T) {
+			sqls := []string{
+				"select * from table",
+				"select count(val) from table",
+				"select col1, col2,col3, col4 from table where col1 = 'val1' and col2 = 'val2' order by col1 asc",
+			}
+
+			for _, sql := range sqls {
+				actual, err := engine.Interpolate(query, timeRange, sql)
+				require.Nil(t, err)
+				require.Equal(t, sql, actual)
+			}
+		})
 	})
 }
 
