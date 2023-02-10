80d2449ec3bb6889e3c81af76209b1c7c01cf67f
Matias Chomicki <matyax@gmail.com>
2023-02-10 18:10:07 +0100

    Chore: remove unused import

--- public/app/plugins/datasource/loki/queryUtils.ts
+++ public/app/plugins/datasource/loki/queryUtils.ts
@@ -1,7 +1,7 @@
 import { SyntaxNode } from '@lezer/common';
 import { escapeRegExp } from 'lodash';
 
-import { DataQueryRequest, DataQueryResponse, DataQueryResponseData, QueryResultMetaStat } from '@grafana/data';
+import { DataQueryResponse, DataQueryResponseData, QueryResultMetaStat } from '@grafana/data';
 import {
   parser,
   LineFilter,
