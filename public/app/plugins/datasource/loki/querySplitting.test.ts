dd6ad2900e971b2d9a1a6799bca23d54276cc502
Matias Chomicki <matyax@gmail.com>
2023-02-10 12:11:14 +0100

    Range splitting: add unit tests for dynamic limit requesting

--- public/app/plugins/datasource/loki/querySplitting.test.ts
+++ public/app/plugins/datasource/loki/querySplitting.test.ts
@@ -4,22 +4,23 @@ import { getQueryOptions } from 'test/helpers/getQueryOptions';
 import { dateTime } from '@grafana/data';
 
 import { LokiDatasource } from './datasource';
-import { createLokiDatasource } from './mocks';
+import { createLokiDatasource, logFrameA } from './mocks';
 import { runPartitionedQuery } from './querySplitting';
 import { LokiQuery } from './types';
 
 describe('runPartitionedQuery()', () => {
   let datasource: LokiDatasource;
-  const request = getQueryOptions<LokiQuery>({
-    targets: [{ expr: 'count_over_time({a="b"}[1m])', refId: 'A' }],
-    range: {
+  const range = {
+    from: dateTime('2023-02-08T05:00:00.000Z'),
+    to: dateTime('2023-02-10T06:00:00.000Z'),
+    raw: {
       from: dateTime('2023-02-08T05:00:00.000Z'),
       to: dateTime('2023-02-10T06:00:00.000Z'),
-      raw: {
-        from: dateTime('2023-02-08T05:00:00.000Z'),
-        to: dateTime('2023-02-10T06:00:00.000Z'),
-      },
     },
+  };
+  const request = getQueryOptions<LokiQuery>({
+    targets: [{ expr: 'count_over_time({a="b"}[1m])', refId: 'A' }],
+    range,
   });
   beforeEach(() => {
     datasource = createLokiDatasource();
@@ -30,6 +31,29 @@ describe('runPartitionedQuery()', () => {
     await expect(runPartitionedQuery(datasource, request)).toEmitValuesWith(() => {
       // 3 days, 3 chunks, 3 requests.
       expect(datasource.runQuery).toHaveBeenCalledTimes(3);
+    });
+  });
+
+  describe('Dynamic maxLines for logs requests', () => {
+    const request = getQueryOptions<LokiQuery>({
+      targets: [{ expr: '{a="b"}', refId: 'A', maxLines: 4 }],
+      range,
+    });
+    beforeEach(() => {
+      jest.spyOn(datasource, 'runQuery').mockReturnValue(of({ data: [logFrameA], refId: 'A' }));
+    });
+    test('Stops requesting once maxLines of logs have been received', async () => {
+      await expect(runPartitionedQuery(datasource, request)).toEmitValuesWith(() => {
+        // 3 days, 3 chunks, 2 responses of 2 logs, 2 requests
+        expect(datasource.runQuery).toHaveBeenCalledTimes(2);
+      });
+    });
+    test('Performs all the requests if maxLines has not been reached', async () => {
+      request.targets[0].maxLines = 9999;
+      await expect(runPartitionedQuery(datasource, request)).toEmitValuesWith(() => {
+        // 3 days, 3 chunks, 3 responses of 2 logs, 3 requests
+        expect(datasource.runQuery).toHaveBeenCalledTimes(3);
+      });
     });
   });
 });
