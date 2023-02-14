5fad33b2e3026c30f727f4585663be2a0fdc06af
Todd Treece <todd.treece@grafana.com>
2023-02-14 15:38:15 -0500

    handle feature flag in ProvideRESTConfig

--- pkg/services/k8s/client/config.go
+++ pkg/services/k8s/client/config.go
@@ -1,12 +1,16 @@
 package client
 
 import (
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 // ProvideRESTConfig returns a rest.Config for the current environment.
-func ProvideRESTConfig() (*rest.Config, error) {
+func ProvideRESTConfig(toggles featuremgmt.FeatureToggles) (*rest.Config, error) {
+	if !toggles.IsEnabled(featuremgmt.FlagK8s) {
+		return &rest.Config{}, nil
+	}
 	config, err := rest.InClusterConfig()
 	if err == nil {
 		return config, nil
