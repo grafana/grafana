2538d39279e4e5b061bfa9e0ed4525b4ae5b0995
Giuseppe Guerra <giuseppe@guerra.in>
2023-02-21 17:06:19 +0100

    Fix tests

--- pkg/api/plugins_test.go
+++ pkg/api/plugins_test.go
@@ -614,7 +614,9 @@ func Test_PluginsList_AccessControl(t *testing.T) {
 				hs.Cfg = setting.NewCfg()
 				hs.PluginSettings = &pluginSettings
 				hs.pluginStore = pluginStore
-				hs.pluginsUpdateChecker = updatechecker.ProvidePluginsService(hs.Cfg, pluginStore)
+				var err error
+				hs.pluginsUpdateChecker, err = updatechecker.ProvidePluginsService(hs.Cfg, pluginStore, tracing.InitializeTracerForTest())
+				require.NoError(t, err)
 			})
 
 			res, err := server.Send(webtest.RequestWithSignedInUser(server.NewGetRequest("/api/plugins"), userWithPermissions(1, tc.permissions)))
