75334dbc58b9d830b5ec903fafe3727bc987211d
Laura Benz <laura.benz@grafana.com>
2023-02-22 17:18:12 +0100

    refactor: remove linting error

--- public/app/features/explore/spec/query.test.tsx
+++ public/app/features/explore/spec/query.test.tsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import React from 'react';
 
 import { serializeStateToUrlParam } from '@grafana/data';
 import { locationService } from '@grafana/runtime';
