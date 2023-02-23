16b416b88b6227a35f961d8043335809b144e290
Karl Persson <kalle.persson@grafana.com>
2023-02-23 13:06:06 +0100

    AuthN: Extract enable disabled users logic to its own hook (#63628)

--- pkg/services/authn/authnimpl/service.go
+++ pkg/services/authn/authnimpl/service.go
@@ -144,6 +144,7 @@ func ProvideService(
 	userSyncService := sync.ProvideUserSync(userService, userProtectionService, authInfoService, quotaService)
 	orgUserSyncService := sync.ProvideOrgSync(userService, orgService, accessControlService)
 	s.RegisterPostAuthHook(userSyncService.SyncUserHook, 10)
+	s.RegisterPostAuthHook(userSyncService.EnableDisabledUserHook, 20)
 	s.RegisterPostAuthHook(orgUserSyncService.SyncOrgRolesHook, 30)
 	s.RegisterPostAuthHook(userSyncService.SyncLastSeenHook, 40)
 
