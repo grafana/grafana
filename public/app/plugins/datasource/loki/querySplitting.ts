6fc06739e65b8b3f3358ff325e231bfdfea5e933
Matias Chomicki <matyax@gmail.com>
2023-02-23 17:21:44 +0100

    Range splitting: intercalate logs and metric queries when > 1 queries

--- public/app/plugins/datasource/loki/querySplitting.ts
+++ public/app/plugins/datasource/loki/querySplitting.ts
@@ -86,28 +86,20 @@ function adjustTargetsFromResponseState(targets: LokiQuery[], response: DataQuer
     .filter((target) => target.maxLines === undefined || target.maxLines > 0);
 }
 
-export function runPartitionedQuery(datasource: LokiDatasource, request: DataQueryRequest<LokiQuery>) {
+type LokiGroupedRequest = Array<{ request: DataQueryRequest<LokiQuery>; partition: TimeRange[] }>;
+
+export function runGroupedQueries(datasource: LokiDatasource, requests: LokiGroupedRequest) {
   let mergedResponse: DataQueryResponse | null;
-  const query = request.targets[0];
-  const partition = partitionTimeRange(
-    isLogsQuery(query.expr),
-    request.range,
-    request.intervalMs,
-    query.resolution ?? 1
-  );
-  const totalRequests = partition.length;
+  const totalRequests = Math.max(...requests.map(({ partition }) => partition.length));
 
   let shouldStop = false;
   let subquerySubsciption: Subscription | null = null;
-  const runNextRequest = (subscriber: Subscriber<DataQueryResponse>, requestN: number) => {
+  const runNextRequest = (subscriber: Subscriber<DataQueryResponse>, requestN: number, requestIndex: number) => {
+    console.log(requestIndex, requestN);
     if (shouldStop) {
       subscriber.complete();
       return;
     }
-
-    const requestId = `${request.requestId}_${requestN}`;
-    const range = partition[requestN - 1];
-    const targets = adjustTargetsFromResponseState(request.targets, mergedResponse);
 
     const done = (response: DataQueryResponse) => {
       response.state = LoadingState.Done;
@@ -117,38 +109,46 @@ export function runPartitionedQuery(datasource: LokiDatasource, request: DataQue
 
     const nextRequest = () => {
       mergedResponse = mergedResponse || { data: [] };
-      if (requestN > 1) {
+      const { nextRequestN, nextRequestIndex } = getNextRequestPointers(requests, requestIndex, requestN);
+      if (nextRequestN > 0) {
         mergedResponse.state = LoadingState.Streaming;
         subscriber.next(mergedResponse);
-        runNextRequest(subscriber, requestN - 1);
+
+        runNextRequest(subscriber, nextRequestN, nextRequestIndex);
         return;
       }
       done(mergedResponse);
     };
 
+    const requestId = `${requests[requestIndex].request.requestId}_${requestN}`;
+    const range = requests[requestIndex].partition[requestN - 1];
+    const targets = adjustTargetsFromResponseState(requests[requestIndex].request.targets, mergedResponse);
+
     if (!targets.length && mergedResponse) {
-      done(mergedResponse);
+      nextRequest();
       return;
     }
 
-    subquerySubsciption = datasource.runQuery({ ...request, range, requestId, targets }).subscribe({
-      next: (partialResponse) => {
-        if (partialResponse.error) {
-          subscriber.error(partialResponse.error);
-        }
-        mergedResponse = combineResponses(mergedResponse, partialResponse);
-      },
-      complete: () => {
-        nextRequest();
-      },
-      error: (error) => {
-        subscriber.error(error);
-      },
-    });
+    subquerySubsciption = datasource
+      .runQuery({ ...requests[requestIndex].request, range, requestId, targets })
+      .subscribe({
+        next: (partialResponse) => {
+          if (partialResponse.error) {
+            subscriber.error(partialResponse.error);
+          }
+          mergedResponse = combineResponses(mergedResponse, partialResponse);
+        },
+        complete: () => {
+          nextRequest();
+        },
+        error: (error) => {
+          subscriber.error(error);
+        },
+      });
   };
 
   const response = new Observable<DataQueryResponse>((subscriber) => {
-    runNextRequest(subscriber, totalRequests);
+    runNextRequest(subscriber, totalRequests, 0);
     return () => {
       shouldStop = true;
       if (subquerySubsciption != null) {
@@ -160,23 +160,36 @@ export function runPartitionedQuery(datasource: LokiDatasource, request: DataQue
   return response;
 }
 
+function getNextRequestPointers(requests: LokiGroupedRequest, requestIndex: number, requestN: number) {
+  // There's a pending metric request:
+  if (requestIndex === 0 && requests[1].partition[requestN - 1]) {
+    return {
+      nextRequestIndex: 1,
+      nextRequestN: requestN,
+    };
+  }
+  return {
+    nextRequestIndex: 0,
+    nextRequestN: requestN - 1,
+  };
+}
+
 export function runPartitionedQueries(datasource: LokiDatasource, request: DataQueryRequest<LokiQuery>) {
   const logQueries = request.targets.filter((query) => isLogsQuery(query.expr));
   const metricQueries = request.targets.filter((query) => !logQueries.includes(query));
 
-  const queries = [];
+  const requests = [];
   if (logQueries.length) {
-    queries.push({
-      queries: logQueries,
+    requests.push({
+      request: { ...request, targets: logQueries },
       partition: partitionTimeRange(true, request.range, request.intervalMs, logQueries[0].resolution ?? 1),
     });
   }
   if (metricQueries.length) {
-    queries.push({
-      queries: logQueries,
-      partition: partitionTimeRange(false, request.range, request.intervalMs, logQueries[0].resolution ?? 1),
+    requests.push({
+      request: { ...request, targets: metricQueries },
+      partition: partitionTimeRange(false, request.range, request.intervalMs, metricQueries[0].resolution ?? 1),
     });
   }
-  console.log(queries);
-  return runPartitionedQuery(datasource, request);
+  return runGroupedQueries(datasource, requests);
 }
