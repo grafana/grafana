b42f973a7ae27d9739d41b8b6dd21de2a9cec59e
Sonia Aguilar <33540275+soniaAguilarPeiron@users.noreply.github.com>
2023-02-24 13:43:54 +0100

    Alerting: Fix changing datasource and creating new query not  using defaults. (#63092)
    * Set default query when changing data source or adding new alert query
    
    * Set default query when creating new alert rule
    
    * Set fefault query for cloud and recording rules
    
    * Create hook for getting defaults in AlertRuleForm
    
    * fixing tests
    
    * Use conditionals with 'if' for more clarity and rename lazy to async
    
    * Add loading indicator for default queries
    
    * Fix tests
    
    * Make newModel a sync method and fix tests error
    
    * Use useAsync instead of useEffect for an async call

--- public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/reducer.test.tsx
+++ public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/reducer.test.tsx
@@ -1,4 +1,4 @@
-import { getDefaultRelativeTimeRange, RelativeTimeRange } from '@grafana/data';
+import { DataSourceApi, getDefaultRelativeTimeRange, PluginMeta, RelativeTimeRange } from '@grafana/data';
 import { getDataSourceSrv } from '@grafana/runtime/src/services/__mocks__/dataSourceSrv';
 import {
   dataSource as expressionDatasource,
@@ -7,6 +7,9 @@ import {
 import { ExpressionQuery, ExpressionQueryType } from 'app/features/expressions/types';
 import { defaultCondition } from 'app/features/expressions/utils/expressionTypes';
 import { AlertQuery } from 'app/types/unified-alerting-dto';
+
+import { mockDataSource } from '../../../mocks';
+import * as dataSource from '../../../utils/datasource';
 
 import {
   addNewDataQuery,
@@ -48,6 +51,12 @@ const expressionQuery: AlertQuery = {
   refId: 'B',
   queryType: '',
 };
+
+const defaultDataSource = mockDataSource();
+const dataSourceMocked = jest.spyOn(dataSource, 'getDefaultOrFirstCompatibleDataSource');
+beforeAll(() => {
+  return dataSourceMocked.mockReturnValue(defaultDataSource);
+});
 
 describe('Query and expressions reducer', () => {
   it('should return initial state', () => {
@@ -96,7 +105,23 @@ describe('Query and expressions reducer', () => {
       queries: [alertQuery],
     };
 
-    const newState = queriesAndExpressionsReducer(initialState, addNewDataQuery());
+    const newDataSource: DataSourceApi = {
+      meta: { alerting: true } as unknown as PluginMeta,
+      name: 'some name',
+      uid: 'some uid',
+    } as unknown as DataSourceApi;
+
+    const defaultQuery: AlertQuery = {
+      refId: 'A',
+      queryType: '',
+      datasourceUid: '',
+      model: { refId: 'A', expression: 'THIS IS THE DEFAULT EXPRESSION' },
+      relativeTimeRange: getDefaultRelativeTimeRange(),
+    };
+    const newState = queriesAndExpressionsReducer(
+      initialState,
+      addNewDataQuery({ ds: newDataSource, defaultQuery: defaultQuery })
+    );
     expect(newState.queries).toHaveLength(2);
     expect(newState).toMatchSnapshot();
   });
