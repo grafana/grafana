ffab959f9ed02bad732793669c5bafb8f4f39704
Jeff Levin <jeff@levinology.com>
2023-02-23 15:38:21 -0900

    remove duplicate

--- pkg/services/k8s/informer/factory.go
+++ pkg/services/k8s/informer/factory.go
@@ -33,12 +33,6 @@ type Factory struct {
 	informers map[schema.GroupVersionResource]cache.SharedIndexInformer
 }
 
-type ResourceWatcher interface {
-	Add(context.Context, any) error
-	Update(ctx context.Context, old, new any) error
-	Delete(context.Context, any) error
-}
-
 func ProvideFactory(
 	cfg *rest.Config,
 	features featuremgmt.FeatureToggles,
