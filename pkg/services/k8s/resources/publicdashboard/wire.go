c44aa8aca49606bf344ed6d7e6b42f670ef847be
Todd Treece <todd.treece@grafana.com>
2023-02-23 19:48:52 -0500

    wire up public dashboard service

--- pkg/services/k8s/resources/publicdashboard/wire.go
+++ pkg/services/k8s/resources/publicdashboard/wire.go
@@ -2,9 +2,12 @@ package publicdashboard
 
 import (
 	"github.com/google/wire"
+	"github.com/grafana/grafana/pkg/services/publicdashboards"
 )
 
 var WireSet = wire.NewSet(
 	ProvideWatcher,
 	wire.Bind(new(Watcher), new(*watcher)),
+	ProvideService,
+	wire.Bind(new(publicdashboards.Service), new(*ServiceWrapper)),
 )
