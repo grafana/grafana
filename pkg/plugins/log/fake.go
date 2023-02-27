4c27e7f97d3b7808aea7698f71955007e715c6ca
Will Browne <will.browne@grafana.com>
2023-02-27 16:28:25 +0000

    update naming

--- pkg/plugins/log/fake.go
+++ pkg/plugins/log/fake.go
@@ -1,41 +1,41 @@
 package log
 
-var _ Logger = (*FakeLogger)(nil)
+var _ Logger = (*TestLogger)(nil)
 
-type FakeLogger struct {
+type TestLogger struct {
 	DebugLogs Logs
 	InfoLogs  Logs
 	WarnLogs  Logs
 	ErrorLogs Logs
 }
 
-func NewTestLogger() *FakeLogger {
-	return &FakeLogger{}
+func NewTestLogger() *TestLogger {
+	return &TestLogger{}
 }
 
-func (f *FakeLogger) New(_ ...interface{}) Logger {
+func (f *TestLogger) New(_ ...interface{}) Logger {
 	return NewTestLogger()
 }
 
-func (f *FakeLogger) Info(msg string, ctx ...interface{}) {
+func (f *TestLogger) Info(msg string, ctx ...interface{}) {
 	f.InfoLogs.Calls++
 	f.InfoLogs.Message = msg
 	f.InfoLogs.Ctx = ctx
 }
 
-func (f *FakeLogger) Warn(msg string, ctx ...interface{}) {
+func (f *TestLogger) Warn(msg string, ctx ...interface{}) {
 	f.WarnLogs.Calls++
 	f.WarnLogs.Message = msg
 	f.WarnLogs.Ctx = ctx
 }
 
-func (f *FakeLogger) Debug(msg string, ctx ...interface{}) {
+func (f *TestLogger) Debug(msg string, ctx ...interface{}) {
 	f.DebugLogs.Calls++
 	f.DebugLogs.Message = msg
 	f.DebugLogs.Ctx = ctx
 }
 
-func (f *FakeLogger) Error(msg string, ctx ...interface{}) {
+func (f *TestLogger) Error(msg string, ctx ...interface{}) {
 	f.ErrorLogs.Calls++
 	f.ErrorLogs.Message = msg
 	f.ErrorLogs.Ctx = ctx
--- pkg/plugins/log/logger.go
+++ pkg/plugins/log/logger.go
@@ -5,40 +5,40 @@ import (
 )
 
 func New(name string) Logger { //_ ...interface{}
-	return &DefaultLogger{
+	return &grafanaInfraLogWrapper{
 		l: log.New(name),
 	}
 }
 
-type DefaultLogger struct {
+type grafanaInfraLogWrapper struct {
 	l *log.ConcreteLogger
 }
 
-func (d *DefaultLogger) New(ctx ...interface{}) Logger {
+func (d *grafanaInfraLogWrapper) New(ctx ...interface{}) Logger {
 	if len(ctx) == 0 {
-		return &DefaultLogger{
+		return &grafanaInfraLogWrapper{
 			l: d.l.New(),
 		}
 	}
 
 	ctx = append([]interface{}{"logger"}, ctx...)
-	return &DefaultLogger{
+	return &grafanaInfraLogWrapper{
 		l: d.l.New(ctx...),
 	}
 }
 
-func (d *DefaultLogger) Debug(msg string, ctx ...interface{}) {
+func (d *grafanaInfraLogWrapper) Debug(msg string, ctx ...interface{}) {
 	d.l.Debug(msg, ctx...)
 }
 
-func (d *DefaultLogger) Info(msg string, ctx ...interface{}) {
+func (d *grafanaInfraLogWrapper) Info(msg string, ctx ...interface{}) {
 	d.l.Info(msg, ctx...)
 }
 
-func (d *DefaultLogger) Warn(msg string, ctx ...interface{}) {
+func (d *grafanaInfraLogWrapper) Warn(msg string, ctx ...interface{}) {
 	d.l.Warn(msg, ctx...)
 }
 
-func (d *DefaultLogger) Error(msg string, ctx ...interface{}) {
+func (d *grafanaInfraLogWrapper) Error(msg string, ctx ...interface{}) {
 	d.l.Error(msg, ctx...)
 }
