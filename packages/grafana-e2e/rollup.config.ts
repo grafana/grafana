9c0471565e2eb1e091c1bcc7cab7c0800c7da7f8
Jack Westbrook <jack.westbrook@gmail.com>
2023-02-27 13:01:24 +0100

    Revert "Update dependency rollup to v3 (#58252)" (#63790)
    This reverts commit a97e9a1e1778f70d8176217d166ac845183bf43c.

--- packages/grafana-e2e/rollup.config.ts
+++ packages/grafana-e2e/rollup.config.ts
@@ -4,24 +4,17 @@ import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import { externals } from 'rollup-plugin-node-externals';
 
-import pkg from './package.json' assert { type: 'json' };
+const pkg = require('./package.json');
 
 export default [
   {
     input: 'src/index.ts',
-    plugins: [externals({ deps: true, packagePath: './package.json' }), resolve(), esbuild()],
+    plugins: [externals({ deps: true, packagePath: './package.json' }), resolve(), esbuild({ target: 'node16' })],
     output: [
       {
         format: 'cjs',
         sourcemap: true,
         dir: path.dirname(pkg.publishConfig.main),
-      },
-      {
-        format: 'esm',
-        sourcemap: true,
-        dir: path.dirname(pkg.publishConfig.module),
-        preserveModules: true,
-        preserveModulesRoot: path.join(process.env.PROJECT_CWD, 'packages/grafana-e2e-selectors/src'),
       },
     ],
   },
