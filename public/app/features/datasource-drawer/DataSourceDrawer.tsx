aeed084440cd139d63acbaf68bafd097cb56a3af
Oscar Kilhed <oscar.kilhed@grafana.com>
2023-02-28 18:40:25 +0100

    Fix prettier issue

--- public/app/features/datasource-drawer/DataSourceDrawer.tsx
+++ public/app/features/datasource-drawer/DataSourceDrawer.tsx
@@ -108,7 +108,7 @@ function PickerContent(props: PickerContentProps) {
                 ...fileUploadOptions,
                 onDrop: (...args) => {
                   onDismiss();
-                    fileUploadOptions?.onDrop?.(...args);
+                  fileUploadOptions?.onDrop?.(...args);
                 },
               }}
             >
