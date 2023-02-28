3e8484c44f79ca4ca7fff573221c0fd2212a2704
Victor Marin <victor.marin@grafana.com>
2023-02-28 17:00:30 +0200

    refactor types.ts

--- public/app/plugins/panel/heatmap/types.ts
+++ public/app/plugins/panel/heatmap/types.ts
@@ -9,24 +9,13 @@ import {
   PanelOptions,
 } from './panelcfg.gen';
 
-export const defaultPanelOptions: PanelOptions = {
-  ...defaultPanelOptionsGen,
-  calculation: defaultPanelOptionsGen.calculation!,
-  cellValues: defaultPanelOptionsGen.cellValues!,
-  color: {
-    ...defaultPanelOptionsGen.color!,
-    mode: HeatmapColorMode.Scheme,
-    scale: HeatmapColorScale.Exponential,
-  },
-  tooltip: defaultPanelOptionsGen.tooltip!,
-  legend: defaultPanelOptionsGen.legend!,
-  yAxis: {
-    axisPlacement: AxisPlacement.Left,
-  },
-  exemplars: defaultPanelOptionsGen.exemplars!,
-  filterValues: defaultPanelOptionsGen.filterValues!,
-  rowsFrame: {
-    layout: HeatmapCellLayout.auto,
-  },
-  showValue: defaultPanelOptionsGen.showValue!,
+defaultPanelOptionsGen.color!.mode = HeatmapColorMode.Scheme;
+defaultPanelOptionsGen.color!.scale = HeatmapColorScale.Exponential;
+defaultPanelOptionsGen.yAxis = {
+  axisPlacement: AxisPlacement.Left,
 };
+defaultPanelOptionsGen.rowsFrame = {
+  layout: HeatmapCellLayout.auto,
+};
+
+export const defaultPanelOptions = defaultPanelOptionsGen as PanelOptions;
