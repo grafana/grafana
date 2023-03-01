e202bc728f8f2fd92fe96c4227075c47b9e955f2
eledobleefe <laura.fernandez@grafana.com>
2023-03-01 17:26:25 +0100

    Add InlineBanner to Alert story

--- packages/grafana-ui/src/components/Alert/Alert.story.tsx
+++ packages/grafana-ui/src/components/Alert/Alert.story.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 
 import { Alert, AlertVariant, VerticalGroup } from '@grafana/ui';
 
-import { StoryExample } from '../../utils/storybook/StoryExample';
 import { withCenteredStory, withHorizontallyCenteredStory } from '../../utils/storybook/withCenteredStory';
 import mdx from '../Alert/Alert.mdx';
 
@@ -21,15 +20,47 @@ const meta: ComponentMeta<typeof Alert> = {
     knobs: {
       disable: true,
     },
-    controls: {
-      exclude: ['onRemove'],
-    },
+    controls: {},
   },
   argTypes: {
     severity: { control: { type: 'select', options: severities } },
+    onRemove: {
+      description: "To show a button or a close icon, select 'function'. To get rid of them, select 'undefined'",
+      control: { type: 'select', options: [action('Remove button clicked'), undefined] },
+    },
+    buttonContent: {
+      description: "To show an example of button, select 'Close', otherwise, select 'undefined'",
+      control: { type: 'select', options: ['Close', undefined] },
+    },
   },
 };
 
+export const InlineBanner: ComponentStory<typeof Alert> = ({ severity, title, buttonContent, onRemove }) => {
+  return (
+    <Alert
+      title={title}
+      severity={severity}
+      buttonContent={buttonContent ? <span>{buttonContent}</span> : ''}
+      onRemove={onRemove ? onRemove : undefined}
+    >
+      <VerticalGroup>
+        <div>Child content that includes some alert details, like maybe what actually happened.</div>
+      </VerticalGroup>
+    </Alert>
+  );
+};
+
+InlineBanner.args = {
+  severity: 'error',
+  title: 'Title',
+  buttonContent: 'Close',
+  onRemove: action('Remove button clicked'),
+};
+
+InlineBanner.parameters = {
+  controls: { expanded: true },
+};
+/*
 export const Examples: ComponentStory<typeof Alert> = ({ severity, title, buttonContent }) => {
   return (
     <VerticalGroup>
@@ -187,6 +218,6 @@ Examples.args = {
   severity: 'error',
   title: 'Some very important message',
   buttonContent: 'Close',
-};
+};*/
 
 export default meta;
