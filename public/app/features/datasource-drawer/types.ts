dc1600ff14acc3deecc5de02f6584dcf20d3c8b1
Oscar Kilhed <oscar.kilhed@grafana.com>
2023-03-01 11:26:19 +0100

    DataSourcePicker: Add new style of data source picker. (#63736)
    * MVP of a new datasource picker
    
    * Add datasource select history, naming DatasourceSelect -> DataSourceDrawer
    
    * refactor cards
    
    * Cleanup and fixing sort order for recents
    
    * add feature flag
    
    * fix feature flag name and use it
    
    * Highlight selected
    
    * Move new ds picker to core
    
    * Restore original datasource picker
    
    * Remove unused property
    
    * update yarn.lock
    
    * Rename folder, update codeowners
    
    * add test for util functions
    
    * Remove es-lint exception
    
    * Change feature toggle description
    
    * remove unnecessary if
    
    Co-authored-by: Ivan Ortega Alba <ivanortegaalba@gmail.com>
    
    * Make test a bit more clear
    
    Co-authored-by: Ivan Ortega Alba <ivanortegaalba@gmail.com>
    
    * Clean up api, filter once and before maps, minor code cleanup
    
    * Fix prettier issue
    
    ---------
    
    Co-authored-by: Ivan Ortega Alba <ivanortegaalba@gmail.com>

---
+++ public/app/features/datasource-drawer/types.ts
@@ -0,0 +1,52 @@
+import { DropzoneOptions } from 'react-dropzone';
+
+import { DataSourceInstanceSettings } from '@grafana/data';
+import { DataSourceJsonData, DataSourceRef } from '@grafana/schema';
+
+export interface DataSourceDrawerProps {
+  datasources: Array<DataSourceInstanceSettings<DataSourceJsonData>>;
+  onFileDrop?: () => void;
+  onChange: (ds: string) => void;
+  current: DataSourceInstanceSettings<DataSourceJsonData> | string | DataSourceRef | null | undefined;
+  enableFileUpload?: boolean;
+  fileUploadOptions?: DropzoneOptions;
+  recentlyUsed?: string[];
+}
+
+export interface PickerContentProps extends DataSourceDrawerProps {
+  onDismiss: () => void;
+}
+
+export interface DataSourcePickerProps {
+  onChange: (ds: DataSourceInstanceSettings) => void;
+  current: DataSourceRef | string | null; // uid
+  tracing?: boolean;
+  recentlyUsed?: string[];
+  mixed?: boolean;
+  dashboard?: boolean;
+  metrics?: boolean;
+  type?: string | string[];
+  annotations?: boolean;
+  variables?: boolean;
+  alerting?: boolean;
+  pluginId?: string;
+  /** If true,we show only DSs with logs; and if true, pluginId shouldnt be passed in */
+  logs?: boolean;
+  // Does not set the default data source if there is no value.
+  noDefault?: boolean;
+  inputId?: string;
+  filter?: (dataSource: DataSourceInstanceSettings) => boolean;
+  onClear?: () => void;
+  disabled?: boolean;
+  enableFileUpload?: boolean;
+  fileUploadOptions?: DropzoneOptions;
+}
+
+export interface DataSourcePickerWithHistoryProps extends Omit<DataSourcePickerProps, 'recentlyUsed'> {
+  localStorageKey?: string;
+}
+
+export interface DataSourcePickerHistoryItem {
+  lastUse: string;
+  uid: string;
+}
