ca8e1d6fd98106da8e28a4bc8ea41283ecbdf713
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2023-03-02 09:54:09 +0100

    Remove unused variable

--- public/app/features/datasources/components/DataSourceCategories.tsx
+++ public/app/features/datasources/components/DataSourceCategories.tsx
@@ -5,7 +5,6 @@ import { config } from '@grafana/runtime';
 import { LinkButton } from '@grafana/ui';
 import { DataSourcePluginCategory } from 'app/types';
 
-import { DestinationPage } from '../../connections/components/ConnectionsRedirectNotice';
 import { ROUTES } from '../../connections/constants';
 
 import { DataSourceTypeCardList } from './DataSourceTypeCardList';
@@ -19,12 +18,8 @@ export type Props = {
 };
 
 export function DataSourceCategories({ categories, onClickDataSourceType }: Props) {
-  const destinationLinks = {
-    [DestinationPage.dataSources]: ROUTES.DataSources,
-    [DestinationPage.connectData]: ROUTES.ConnectData,
-  };
   const moreDataSourcesLink = config.featureToggles.dataConnectionsConsole
-    ? `${destinationLinks[DestinationPage.connectData]}?cat=data-source`
+    ? `${ROUTES.ConnectData}?cat=data-source`
     : '/plugins?filterBy=all&filterByType=datasource&utm_source=grafana_add_ds';
 
   return (
