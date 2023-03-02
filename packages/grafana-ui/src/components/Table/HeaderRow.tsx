6d80775a46796bd9bc7c88d033b04cc3555e1c04
Ryan McKinley <ryantxu@gmail.com>
2023-03-01 17:55:18 -0800

    revert some changes

--- packages/grafana-ui/src/components/Table/HeaderRow.tsx
+++ packages/grafana-ui/src/components/Table/HeaderRow.tsx
@@ -3,7 +3,7 @@ import { HeaderGroup, Column } from 'react-table';
 
 import { selectors } from '@grafana/e2e-selectors';
 
-import { getIconForFieldType } from '../../types';
+import { getFieldTypeIcon } from '../../types';
 import { Icon } from '../Icon/Icon';
 
 import { Filter } from './Filter';
@@ -58,12 +58,7 @@ function renderHeaderCell(column: any, tableStyles: TableStyles, showTypeIcons?:
         <>
           <button {...column.getSortByToggleProps()} className={tableStyles.headerCellLabel}>
             {showTypeIcons && (
-              <Icon
-                name={getIconForFieldType(field?.type)}
-                title={field?.type}
-                size="sm"
-                className={tableStyles.typeIcon}
-              />
+              <Icon name={getFieldTypeIcon(field)} title={field?.type} size="sm" className={tableStyles.typeIcon} />
             )}
             <div>{column.render('Header')}</div>
             {column.isSorted &&
