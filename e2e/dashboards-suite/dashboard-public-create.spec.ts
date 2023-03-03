26be0603a3e442b1f39b2daec3ceaaabde03776a
Juan Cabanas <juan.cabanas@grafana.com>
2023-03-03 09:39:53 -0300

    test fixed

--- e2e/dashboards-suite/dashboard-public-create.spec.ts
+++ e2e/dashboards-suite/dashboard-public-create.spec.ts
@@ -145,8 +145,6 @@ e2e.scenario({
           .request({ url: getPublicDashboardAPIUrl(String(url)), failOnStatusCode: false })
           .then((resp) => {
             expect(resp.status).to.eq(403);
-            e2e.pages.PublicDashboard.page().should('not.exist');
-            e2e.pages.PublicDashboard.NotAvailable.container().should('be.visible');
           });
       });
   },
