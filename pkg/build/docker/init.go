04eedc0e4ce8d301da0e399952c75e7cea25702f
dsotirakis <dimitrios.sotirakis@grafana.com>
2023-03-03 11:07:06 +0200

    Add specific tag for docker image

--- pkg/build/docker/init.go
+++ pkg/build/docker/init.go
@@ -10,6 +10,9 @@ import (
 // AllArchs is a list of all supported Docker image architectures.
 var AllArchs = []string{"amd64", "armv7", "arm64"}
 
+// emulatorImage is the docker image used as the cross-platform emulator
+var emulatorImage = "tonistiigi/binfmt:qemu-v7.0.0"
+
 // Init initializes the OS for Docker image building.
 func Init() error {
 	// Necessary for cross-platform builds
@@ -20,12 +23,12 @@ func Init() error {
 	// Enable execution of Docker images for other architectures
 	//nolint:gosec
 	cmd := exec.Command("docker", "run", "--privileged", "--rm",
-		"tonistiigi/binfmt", "--install", "all")
+		emulatorImage, "--install", "all")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("failed to enable execution of cross-platform Docker images: %w\n%s", err, output)
 	}
-	log.Println("emulators have been installed successfully!", string(output))
+	log.Println("emulators have been installed successfully!")
 
 	return nil
 }
