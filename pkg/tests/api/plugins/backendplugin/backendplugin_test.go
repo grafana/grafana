5e91e25edfffeb8a056de5a80370145fe59c6971
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-03-03 18:01:23 +0200

    Chore: Remove CreateUserForTests() (#64125)
    * Chore: Remove CreateUserForTests
    
    * Apply suggestion from code review
    
    (cherry picked from commit e6e8351ee922993b8b71be4a0019f270546f4f89)

--- pkg/tests/api/plugins/backendplugin/backendplugin_test.go
+++ pkg/tests/api/plugins/backendplugin/backendplugin_test.go
@@ -262,7 +262,7 @@ func newTestScenario(t *testing.T, name string, opts []testScenarioOption, callb
 	tsCtx.testEnv = testEnv
 	ctx := context.Background()
 
-	testinfra.CreateUser(t, testEnv.SQLStore, user.CreateUserCommand{
+	u := testinfra.CreateUser(t, testEnv.SQLStore, user.CreateUserCommand{
 		DefaultOrgRole: string(org.RoleAdmin),
 		Password:       "admin",
 		Login:          "admin",
@@ -284,7 +284,7 @@ func newTestScenario(t *testing.T, name string, opts []testScenarioOption, callb
 
 	tsCtx.uid = "test-plugin"
 	cmd := &datasources.AddDataSourceCommand{
-		OrgId:          1,
+		OrgId:          u.OrgID,
 		Access:         datasources.DS_ACCESS_PROXY,
 		Name:           "TestPlugin",
 		Type:           tsCtx.testPluginID,
@@ -306,7 +306,7 @@ func newTestScenario(t *testing.T, name string, opts []testScenarioOption, callb
 	require.NoError(t, err)
 
 	getDataSourceQuery := &datasources.GetDataSourceQuery{
-		OrgId: 1,
+		OrgId: u.OrgID,
 		Uid:   tsCtx.uid,
 	}
 	err = testEnv.Server.HTTPServer.DataSourcesService.GetDataSource(ctx, getDataSourceQuery)
