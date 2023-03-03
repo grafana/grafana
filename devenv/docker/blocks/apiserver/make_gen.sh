0c5ffdc3763d622a537c7276e5fac04380033e6d
Jeff Levin <jeff@levinology.com>
2023-03-03 11:20:41 -0900

    working state. need to figure out host name for docker loopback on linux

--- devenv/docker/blocks/apiserver/make_gen.sh
+++ devenv/docker/blocks/apiserver/make_gen.sh
@@ -7,7 +7,7 @@ rm -rf certs final
 cfssl genkey -initca csr.json | cfssljson -bare ca
 mkdir -p certs final
 mv ca* certs/
-cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -hostname=172.17.0.1 csr.json | cfssljson -bare out
+cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -hostname=host.docker.internal csr.json | cfssljson -bare out
 mv out* final/
 
 echo 'PUT THIS IN VALIDATING WEBHOOK CONFIGURATION'
