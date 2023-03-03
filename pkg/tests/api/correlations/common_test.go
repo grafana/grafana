bdf8b7531b5657061eaab11886144e854389185c
Giordano Ricci <me@giordanoricci.com>
2023-03-03 14:06:59 +0000

    [9.4.x] Correlations: Fix flaky integration tests (#64004) (#64139)
    Correlations: Fix flaky integration tests (#64004)
    
    * Correlations: Fix flaky integration tests
    
    * set explore-squad as correlations tests code owners
    
    (cherry picked from commit 4a7fbea7a4075743e1c021d7d4ff75b3ffe1e641)

--- pkg/tests/api/correlations/common_test.go
+++ pkg/tests/api/correlations/common_test.go
@@ -43,7 +43,7 @@ func NewTestEnv(t *testing.T) TestContext {
 }
 
 type User struct {
-	username string
+	User     user.User
 	password string
 }
 
@@ -121,8 +121,8 @@ func (c TestContext) getURL(url string, user User) string {
 	c.t.Helper()
 
 	baseUrl := fmt.Sprintf("http://%s", c.env.Server.HTTPServer.Listener.Addr())
-	if user.username != "" && user.password != "" {
-		baseUrl = fmt.Sprintf("http://%s:%s@%s", user.username, user.password, c.env.Server.HTTPServer.Listener.Addr())
+	if user.User.Login != "" && user.password != "" {
+		baseUrl = fmt.Sprintf("http://%s:%s@%s", user.User.Login, user.password, c.env.Server.HTTPServer.Listener.Addr())
 	}
 
 	return fmt.Sprintf(
@@ -132,7 +132,7 @@ func (c TestContext) getURL(url string, user User) string {
 	)
 }
 
-func (c TestContext) createUser(cmd user.CreateUserCommand) {
+func (c TestContext) createUser(cmd user.CreateUserCommand) User {
 	c.t.Helper()
 	store := c.env.SQLStore
 	store.Cfg.AutoAssignOrg = true
@@ -144,8 +144,13 @@ func (c TestContext) createUser(cmd user.CreateUserCommand) {
 	usrSvc, err := userimpl.ProvideService(store, orgService, store.Cfg, nil, nil, quotaService)
 	require.NoError(c.t, err)
 
-	_, err = usrSvc.CreateUserForTests(context.Background(), &cmd)
+	user, err := usrSvc.CreateUserForTests(context.Background(), &cmd)
 	require.NoError(c.t, err)
+
+	return User{
+		User:     *user,
+		password: cmd.Password,
+	}
 }
 
 func (c TestContext) createDs(cmd *datasources.AddDataSourceCommand) {
