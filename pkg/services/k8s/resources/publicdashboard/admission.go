91b749286b9634112139c09e53572471b6a1d2ac
Charandas Batra <charandas.batra@grafana.com>
2023-03-07 15:00:53 -0800

    Refactor publicdashboard save, use correct orgId

--- pkg/services/k8s/resources/publicdashboard/admission.go
+++ pkg/services/k8s/resources/publicdashboard/admission.go
@@ -3,9 +3,9 @@ package publicdashboard
 import (
 	"context"
 	"fmt"
-
 	"github.com/grafana/grafana/pkg/services/k8s/admission"
 	"github.com/grafana/grafana/pkg/services/publicdashboards"
+	publicdashboardModels "github.com/grafana/grafana/pkg/services/publicdashboards/models"
 	"github.com/grafana/grafana/pkg/services/publicdashboards/validation"
 )
 
@@ -28,25 +28,29 @@ func ProvideValidation(
 
 func (v *pdValidation) Validate(ctx context.Context, request *admission.AdmissionRequest) error {
 	k8Dashboard := request.Object.(*PublicDashboard)
-	dto, err := k8sPublicDashboardToDTO(k8Dashboard)
+	pdModel, err := k8sPublicDashboardToDTO(k8Dashboard)
 	if err != nil {
 		return err
 	}
 
 	// API VALIDATIONS
-	if !validation.IsValidShortUID(dto.DashboardUid) {
-		return fmt.Errorf("invalid dashboard ID: %v", dto.PublicDashboard.DashboardUid)
+	if !validation.IsValidShortUID(pdModel.DashboardUid) {
+		return fmt.Errorf("invalid dashboard ID: %v", pdModel.DashboardUid)
 	}
 
 	// SERVICE VALIDATIONS
 	// NOTE - review this later. maybe shouldn't be checking dependency
 	// ensure dashboard exists
-	dashboard, err := v.publicdashboardsService.FindDashboard(ctx, dto.OrgId, dto.DashboardUid) // TODO: should dto.OrgId be coming from the user?
+	dashboard, err := v.publicdashboardsService.FindDashboard(ctx, pdModel.OrgId, pdModel.DashboardUid) // TODO: should dto.OrgId be coming from the user?
 	if err != nil {
 		return err
 	}
 
 	// validate fields
+	// TODO: make validatePublicDashboard take a PublicDashboard Model
+	dto := &publicdashboardModels.SavePublicDashboardDTO{
+		PublicDashboard: pdModel,
+	}
 	err = validation.ValidatePublicDashboard(dto, dashboard)
 	if err != nil {
 		return err
@@ -54,7 +58,7 @@ func (v *pdValidation) Validate(ctx context.Context, request *admission.Admissio
 
 	// verify public dashboard does not exist and that we didn't get one from the
 	// request
-	existingPubdash, err := v.publicdashboardsStore.Find(ctx, dto.PublicDashboard.Uid)
+	existingPubdash, err := v.publicdashboardsStore.Find(ctx, pdModel.Uid)
 	if err != nil {
 		return fmt.Errorf("Create: failed to find the public dashboard: %w", err)
 	} else if existingPubdash != nil {
