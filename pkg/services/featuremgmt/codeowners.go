ac96d6a740535ccb8e1186ee2317f7b5f346343c
Gábor Farkas <gabor.farkas@gmail.com>
2023-03-08 09:49:18 +0100

    feature flags: assign flags to observability-logs squad

--- pkg/services/featuremgmt/codeowners.go
+++ pkg/services/featuremgmt/codeowners.go
@@ -5,14 +5,15 @@ package featuremgmt
 type codeowner string
 
 const (
-	grafanaAppPlatformSquad     codeowner = "@grafana/grafana-app-platform-squad"
-	grafanaDashboardsSquad      codeowner = "@grafana/dashboards-squad"
-	grafanaExploreSquad         codeowner = "@grafana/explore-squad"
-	grafanaBiSquad              codeowner = "@grafana/grafana-bi-squad"
-	grafanaDatavizSquad         codeowner = "@grafana/dataviz-squad"
-	grafanaUserEssentialsSquad  codeowner = "@grafana/user-essentials"
-	grafanaBackendPlatformSquad codeowner = "@grafana/backend-platform"
-	grafanaPluginsPlatformSquad codeowner = "@grafana/plugins-platform-backend"
-	grafanaAsCodeSquad          codeowner = "@grafana/grafana-as-code"
-	grafanaAuthnzSquad          codeowner = "@grafana/grafana-authnz-team"
+	grafanaAppPlatformSquad       codeowner = "@grafana/grafana-app-platform-squad"
+	grafanaDashboardsSquad        codeowner = "@grafana/dashboards-squad"
+	grafanaExploreSquad           codeowner = "@grafana/explore-squad"
+	grafanaBiSquad                codeowner = "@grafana/grafana-bi-squad"
+	grafanaDatavizSquad           codeowner = "@grafana/dataviz-squad"
+	grafanaUserEssentialsSquad    codeowner = "@grafana/user-essentials"
+	grafanaBackendPlatformSquad   codeowner = "@grafana/backend-platform"
+	grafanaPluginsPlatformSquad   codeowner = "@grafana/plugins-platform-backend"
+	grafanaAsCodeSquad            codeowner = "@grafana/grafana-as-code"
+	grafanaAuthnzSquad            codeowner = "@grafana/grafana-authnz-team"
+	grafanaObservabilityLogsSquad codeowner = "@grafana/observability-logs"
 )
