3eeafd18ca238b8d97eed2ae0d1c0d1ba1b2d509
Kyle Cunningham <kyle@codeincarnate.com>
2023-03-08 14:53:11 +0700

    Remove double import

--- pkg/tests/api/alerting/api_alertmanager_test.go
+++ pkg/tests/api/alerting/api_alertmanager_test.go
@@ -17,8 +17,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/grafana/grafana/pkg/expr"
-
 	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 	ngmodels "github.com/grafana/grafana/pkg/services/ngalert/models"
 	ngstore "github.com/grafana/grafana/pkg/services/ngalert/store"
