5fa97d840eae745d1ba06bd0ba5447c9d36397d0
bohandley <brendan.ohandley@gmail.com>
2023-03-08 11:22:33 -0500

    add o11y-metrics as Owner for metrics feature flags

--- pkg/services/featuremgmt/toggles_gen_test.go
+++ pkg/services/featuremgmt/toggles_gen_test.go
@@ -42,23 +42,21 @@ func TestFeatureToggleFiles(t *testing.T) {
 	})
 
 	ownerlessFeatures := map[string]bool{
-		"database_metrics":                  true,
-		"prometheusAzureOverrideAudience":   true,
-		"tracing":                           true,
-		"cloudWatchDynamicLabels":           true,
-		"prometheusWideSeries":              true,
-		"disableSecretsCompatibility":       true,
-		"logRequestsInstrumentedAsUnknown":  true,
-		"dataConnectionsConsole":            true,
-		"cloudWatchCrossAccountQuerying":    true,
-		"redshiftAsyncQueryDataSupport":     true,
-		"athenaAsyncQueryDataSupport":       true,
-		"newPanelChromeUI":                  true,
-		"showDashboardValidationWarnings":   true,
-		"datasourceOnboarding":              true,
-		"secureSocksDatasourceProxy":        true,
-		"disablePrometheusExemplarSampling": true,
-		"individualCookiePreferences":       true,
+		"database_metrics":                 true,
+		"prometheusAzureOverrideAudience":  true,
+		"tracing":                          true,
+		"cloudWatchDynamicLabels":          true,
+		"disableSecretsCompatibility":      true,
+		"logRequestsInstrumentedAsUnknown": true,
+		"dataConnectionsConsole":           true,
+		"cloudWatchCrossAccountQuerying":   true,
+		"redshiftAsyncQueryDataSupport":    true,
+		"athenaAsyncQueryDataSupport":      true,
+		"newPanelChromeUI":                 true,
+		"showDashboardValidationWarnings":  true,
+		"datasourceOnboarding":             true,
+		"secureSocksDatasourceProxy":       true,
+		"individualCookiePreferences":      true,
 	}
 
 	t.Run("all new features should have an owner", func(t *testing.T) {
