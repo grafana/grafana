a9a55a5d1e6a2975474ce5bfa8b3d61eefc424f1
Piotr Jamróz <pm.jamroz@gmail.com>
2023-03-08 15:33:21 +0100

    Revert code simplifications
    To simplify the PR, this can be added in a separate PR

--- public/app/features/explore/utils/supplementaryQueries.ts
+++ public/app/features/explore/utils/supplementaryQueries.ts
@@ -139,78 +139,83 @@ export const getSupplementaryQueryProvider = (
   request: DataQueryRequest,
   explorePanelData: Observable<ExplorePanelData>
 ): Observable<DataQueryResponse> | undefined => {
-  const queries = request.targets.filter((t) => {
-    return t.datasource?.uid !== MIXED_DATASOURCE_NAME;
-  });
-  // Build groups of queries to run in parallel
-  const sets: { [key: string]: DataQuery[] } = groupBy(queries, 'datasource.uid');
-  const mixed: Array<{ datasource: Promise<DataSourceApi>; targets: DataQuery[] }> = [];
-
-  for (const key in sets) {
-    const targets = sets[key];
-    mixed.push({
-      datasource: !targets[0].datasource
-        ? Promise.resolve(datasourceInstance)
-        : getDataSourceSrv().get(targets[0].datasource, request.scopedVars),
-      targets,
+  if (hasSupplementaryQuerySupport(datasourceInstance, type)) {
+    return datasourceInstance.getDataProvider(type, request);
+  } else if (datasourceInstance.meta?.mixed === true) {
+    const queries = request.targets.filter((t) => {
+      return t.datasource?.uid !== MIXED_DATASOURCE_NAME;
     });
-  }
+    // Build groups of queries to run in parallel
+    const sets: { [key: string]: DataQuery[] } = groupBy(queries, 'datasource.uid');
+    const mixed: Array<{ datasource: Promise<DataSourceApi>; targets: DataQuery[] }> = [];
 
-  return from(mixed).pipe(
-    mergeMap((query, i) => {
-      return from(query.datasource).pipe(
-        mergeMap((ds) => {
-          const dsRequest = cloneDeep(request);
-          dsRequest.requestId = `${dsRequest.requestId || ''}_${i}`;
-          dsRequest.targets = query.targets;
+    for (const key in sets) {
+      const targets = sets[key];
+      mixed.push({
+        datasource: getDataSourceSrv().get(targets[0].datasource, request.scopedVars),
+        targets,
+      });
+    }
 
-          if (hasSupplementaryQuerySupport(ds, type)) {
-            const dsProvider = ds.getDataProvider(type, dsRequest);
-            if (dsProvider) {
-              // 1) It provides data for current request - use the provider
-              return dsProvider;
+    return from(mixed).pipe(
+      mergeMap((query, i) => {
+        return from(query.datasource).pipe(
+          mergeMap((ds) => {
+            const dsRequest = cloneDeep(request);
+            dsRequest.requestId = `mixed-${type}-${i}-${dsRequest.requestId || ''}`;
+            dsRequest.targets = query.targets;
+
+            if (hasSupplementaryQuerySupport(ds, type)) {
+              const dsProvider = ds.getDataProvider(type, dsRequest);
+              if (dsProvider) {
+                // 1) It provides data for current request - use the provider
+                return dsProvider;
+              } else {
+                // 2) It doesn't provide data for current request -> return nothing
+                return of({
+                  data: [],
+                  state: LoadingState.NotStarted,
+                });
+              }
             } else {
-              // 2) It doesn't provide data for current request -> return nothing
-              return of({
-                data: [],
-                state: LoadingState.NotStarted,
-              });
+              // 3) Data source doesn't support the supplementary query -> use fallback
+              // the fallback cannot determine data availability based on request, it
+              // works on the results once they are available so it never uses the cache
+              return getSupplementaryQueryFallback(type, explorePanelData, query.targets, ds.name);
             }
-          } else {
-            // 3) Data source doesn't support the supplementary query -> use fallback
-            // the fallback cannot determine data availability based on request, it
-            // works on the results once they are available so it never uses the cache
-            return getSupplementaryQueryFallback(type, explorePanelData, query.targets, ds.name);
+          })
+        );
+      }),
+      scan<DataQueryResponse, DataQueryResponse>(
+        (acc, next) => {
+          if (acc.error || next.state === LoadingState.NotStarted) {
+            return acc;
           }
-        })
-      );
-    }),
-    scan<DataQueryResponse, DataQueryResponse>(
-      (acc, next) => {
-        if (acc.error || next.state === LoadingState.NotStarted) {
-          return acc;
-        }
 
-        if (next.state === LoadingState.Loading && acc.state === LoadingState.NotStarted) {
+          if (next.state === LoadingState.Loading && acc.state === LoadingState.NotStarted) {
+            return {
+              ...acc,
+              state: LoadingState.Loading,
+            };
+          }
+
+          if (next.state && next.state !== LoadingState.Done) {
+            return acc;
+          }
+
           return {
             ...acc,
-            state: LoadingState.Loading,
+            data: [...acc.data, ...next.data],
+            state: LoadingState.Done,
           };
-        }
-
-        if (next.state && next.state !== LoadingState.Done) {
-          return acc;
-        }
-
-        return {
-          ...acc,
-          data: [...acc.data, ...next.data],
-          state: LoadingState.Done,
-        };
-      },
-      { data: [], state: LoadingState.NotStarted }
-    ),
-    distinct()
-  );
+        },
+        { data: [], state: LoadingState.NotStarted }
+      ),
+      distinct()
+    );
+  } else {
+    // Create a fallback to results based logs volume
+    return getSupplementaryQueryFallback(type, explorePanelData, request.targets, datasourceInstance.name);
+  }
   return undefined;
 };
