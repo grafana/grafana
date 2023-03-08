78128b9ea40f8db70b5b42f2632e221fc4612d53
Karl Persson <kalle.persson@grafana.com>
2023-03-08 09:20:51 +0100

    FeatureToggles: Set authnz squad as owner of our feature toggles

--- pkg/services/featuremgmt/codeowners.go
+++ pkg/services/featuremgmt/codeowners.go
@@ -12,4 +12,5 @@ const (
 	grafanaUserEssentialsSquad  codeowner = "@grafana/user-essentials"
 	grafanaBackendPlatformSquad codeowner = "@grafana/backend-platform"
 	grafanaPluginsPlatformSquad codeowner = "@grafana/plugins-platform-backend"
+	grafanaAuthnzSquad          codeowner = "@grafana/grafana-authnz-team"
 )
