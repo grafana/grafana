e44c4bb51ffbb2d6890240090e87e9eb6af6bef6
Ryan McKinley <ryantxu@gmail.com>
2023-03-08 08:15:04 -0800

    uid munging

--- pkg/services/k8s/resources/dashboard/utils.go
+++ pkg/services/k8s/resources/dashboard/utils.go
@@ -3,6 +3,7 @@ package dashboard
 import (
 	"crypto/sha256"
 	"fmt"
+	"unicode"
 
 	"github.com/grafana/grafana/pkg/components/simplejson"
 	"k8s.io/apimachinery/pkg/util/validation"
@@ -10,7 +11,7 @@ import (
 
 // This makes an consistent mapping between Grafana UIDs and k8s compatible names
 func GrafanaUIDToK8sName(uid string) string {
-	if validation.IsQualifiedName(uid) == nil {
+	if allLowercaseAlphaNum(uid) && validation.IsQualifiedName(uid) == nil {
 		return uid // OK, so just use it directly
 	}
 
@@ -19,6 +20,15 @@ func GrafanaUIDToK8sName(uid string) string {
 	_, _ = h.Write([]byte(uid))
 	bs := h.Sum(nil)
 	return fmt.Sprintf("g%x", bs[:12])
+}
+
+func allLowercaseAlphaNum(s string) bool {
+	for _, r := range s {
+		if !(unicode.IsLower(r) || unicode.IsDigit(r)) {
+			return false
+		}
+	}
+	return true
 }
 
 func stripNulls(j *simplejson.Json) {
