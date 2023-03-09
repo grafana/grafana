5510509bc7b35d3760ab892da7424cedb993c9dc
Charandas Batra <charandas.batra@grafana.com>
2023-03-09 14:15:49 -0800

    expand cert provisioning profiles, a first fake authz implementation specified to kind

--- devenv/docker/blocks/apiserver/make_gen.sh
+++ devenv/docker/blocks/apiserver/make_gen.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 pushd /tmp
 go install github.com/cloudflare/cfssl/cmd/...@latest
 popd
-rm -rf certs final
-cfssl genkey -initca csr.json | cfssljson -bare ca
-mkdir -p certs final
-mv ca* certs/
-cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -hostname=host.docker.internal csr.json | cfssljson -bare out
-mv out* final/
+rm -rf certs webhooks authz
+
+mkdir -p certs webhooks authz
+
+cfssl genkey -initca ca.json | cfssljson -bare ca
+mv ca*.pem certs/
+
+cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem webhooks.json | cfssljson -bare webhooks
+mv webhooks*.pem webhooks/
+
+cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem -profile=client authz-webhook-user.json | cfssljson -bare authz-webhook-user
+mv authz-webhook-user*.pem authz/
+
+rm -rf *.csr
 
 echo 'PUT THIS IN VALIDATING WEBHOOK CONFIGURATION'
 echo 'clientConfig'
 echo '  caBundle: {BASE64}'
-echo 'writing base64 encoded cert to final/base_64_cert.pem'
-cat certs/ca.pem | base64 >> final/base_64_cert.pem
+echo 'writing base64 encoded cert to webhooks/base_64_cert.pem'
+cat certs/ca.pem | base64 >> webhooks/base_64_cert.pem
