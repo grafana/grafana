821b1f8d0902fe98363ed1763397024b27412a85
bohandley <brendan.ohandley@gmail.com>
2023-03-10 14:04:37 -0500

    clean up the alphabet type

--- public/app/plugins/datasource/prometheus/querybuilder/components/MetricEncyclopediaModal.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/components/MetricEncyclopediaModal.tsx
@@ -770,32 +770,3 @@ const alphabet = [
   'Y',
   'Z',
 ];
-
-// type alphabetCheckType = {
-//   a?: number;
-//   b?: number;
-//   c?: number;
-//   d?: number;
-//   e?: number;
-//   f?: number;
-//   g?: number;
-//   h?: number;
-//   i?: number;
-//   j?: number;
-//   k?: number;
-//   l?: number;
-//   m?: number;
-//   n?: number;
-//   o?: number;
-//   p?: number;
-//   q?: number;
-//   r?: number;
-//   s?: number;
-//   t?: number;
-//   u?: number;
-//   v?: number;
-//   w?: number;
-//   x?: number;
-//   y?: number;
-//   z?: number;
-// };
