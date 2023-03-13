8ad55bb16636dcbf55ca5f57f108f592026d15d8
Virginia Cepeda <virginia.cepeda@grafana.com>
2023-03-13 17:57:22 -0300

    Refactor as MultipleDataSourcePicker as FC

--- public/app/features/alerting/unified/components/rules/MultipleDataSourcePicker.tsx
+++ public/app/features/alerting/unified/components/rules/MultipleDataSourcePicker.tsx
@@ -1,7 +1,6 @@
-// Libraries
-import React, { PureComponent } from 'react';
+import React, { FC, useState } from 'react';
+import { PopValueActionMeta, RemoveValueActionMeta } from 'react-select';
 
-// Components
 import {
   DataSourceInstanceSettings,
   getDataSourceUID,
@@ -9,84 +8,23 @@ import {
   SelectableValue,
 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import { getDataSourceSrv, DataSourcePickerState } from '@grafana/runtime';
+import { getDataSourceSrv, DataSourcePickerState, DataSourcePickerProps } from '@grafana/runtime';
 import { ExpressionDatasourceRef } from '@grafana/runtime/src/utils/DataSourceWithBackend';
-import { DataSourceJsonData } from '@grafana/schema';
 import { ActionMeta, HorizontalGroup, PluginSignatureBadge, MultiSelect } from '@grafana/ui';
 
-/**
- * Component props description for the {@link DataSourcePicker}
- *
- * @internal
- */
-export interface DataSourcePickerProps {
+export interface MultipleDataSourcePickerProps extends Omit<DataSourcePickerProps, 'onChange' | 'current'> {
   onChange: (ds: DataSourceInstanceSettings, action: 'add' | 'remove') => void;
   current: string[] | undefined;
-  hideTextValue?: boolean;
-  onBlur?: () => void;
-  autoFocus?: boolean;
-  openMenuOnFocus?: boolean;
-  placeholder?: string;
-  tracing?: boolean;
-  mixed?: boolean;
-  dashboard?: boolean;
-  metrics?: boolean;
-  type?: string | string[];
-  annotations?: boolean;
-  variables?: boolean;
-  alerting?: boolean;
-  pluginId?: string;
-  /** If true,we show only DSs with logs; and if true, pluginId shouldnt be passed in */
-  logs?: boolean;
-  // If set to true and there is no value select will be empty, otherwise it will preselect default data source
-  noDefault?: boolean;
-  width?: number;
-  inputId?: string;
-  filter?: (dataSource: DataSourceInstanceSettings) => boolean;
-  onClear?: () => void;
-  invalid?: boolean;
-  disabled?: boolean;
-  isLoading?: boolean;
 }
 
-interface DataSourcesSettingsInterface {
-  [dataSourceName: string]: DataSourceInstanceSettings<DataSourceJsonData> | undefined;
-}
+export const MultipleDataSourcePicker: FC<MultipleDataSourcePickerProps> = (props) => {
+  const dataSourceSrv = getDataSourceSrv();
 
-/**
- * Component to be able to select a datasource from the list of installed and enabled
- * datasources in the current Grafana instance.
- *
- * @internal
- */
-export class MultipleDataSourcePicker extends PureComponent<DataSourcePickerProps, DataSourcePickerState> {
-  dataSourceSrv = getDataSourceSrv();
+  const [state, setState] = useState<DataSourcePickerState>();
 
-  static defaultProps: Partial<DataSourcePickerProps> = {
-    autoFocus: false,
-    openMenuOnFocus: false,
-    placeholder: 'Select data source',
-  };
-
-  state: DataSourcePickerState = {};
-
-  constructor(props: DataSourcePickerProps) {
-    super(props);
-  }
-
-  componentDidMount() {
-    const { current } = this.props;
-
-    const dataSourcesMap: DataSourcesSettingsInterface = {};
-    current?.forEach((dataSourceName) => {
-      const dsSettings = this.dataSourceSrv.getInstanceSettings(dataSourceName);
-      dataSourcesMap[dataSourceName] = dsSettings;
-    });
-  }
-
-  onChange = (items: Array<SelectableValue<string>>, actionMeta: ActionMeta) => {
-    if (actionMeta.action === 'clear' && this.props.onClear) {
-      this.props.onClear();
+  const onChange = (items: Array<SelectableValue<string>>, actionMeta: ActionMeta) => {
+    if (actionMeta.action === 'clear' && props.onClear) {
+      props.onClear();
       return;
     }
 
@@ -95,30 +33,32 @@ export class MultipleDataSourcePicker extends PureComponent<DataSourcePickerProp
     let dataSourceName, action: 'add' | 'remove';
 
     if (actionMeta.action === 'pop-value' || actionMeta.action === 'remove-value') {
-      //@ts-ignore
-      dataSourceName = actionMeta.removedValue.value;
+      const castedActionMeta:
+        | RemoveValueActionMeta<SelectableValue<string>>
+        | PopValueActionMeta<SelectableValue<string>> = actionMeta;
+      dataSourceName = castedActionMeta.removedValue?.value;
       action = 'remove';
     } else {
       dataSourceName = selectedItem.value;
       action = 'add';
     }
 
-    const dsSettings = this.dataSourceSrv.getInstanceSettings(dataSourceName);
+    const dsSettings = dataSourceSrv.getInstanceSettings(dataSourceName);
 
     if (dsSettings) {
-      this.props.onChange(dsSettings, action);
-      this.setState({ error: undefined });
+      props.onChange(dsSettings, action);
+      setState({ error: undefined });
     }
   };
 
-  private getCurrentValue(): Array<SelectableValue<string>> | undefined {
-    const { current, hideTextValue, noDefault } = this.props;
+  const getCurrentValue = (): Array<SelectableValue<string>> | undefined => {
+    const { current, hideTextValue, noDefault } = props;
     if (!current && noDefault) {
       return;
     }
 
-    return current?.map((dataSourceName) => {
-      const ds = this.dataSourceSrv.getInstanceSettings(dataSourceName);
+    return current?.map((dataSourceName: string) => {
+      const ds = dataSourceSrv.getInstanceSettings(dataSourceName);
       if (ds) {
         return {
           label: ds.name.slice(0, 37),
@@ -142,13 +82,13 @@ export class MultipleDataSourcePicker extends PureComponent<DataSourcePickerProp
         hideText: hideTextValue,
       };
     });
-  }
+  };
 
-  getDataSourceOptions() {
+  const getDataSourceOptions = () => {
     const { alerting, tracing, metrics, mixed, dashboard, variables, annotations, pluginId, type, filter, logs } =
-      this.props;
+      props;
 
-    const options = this.dataSourceSrv
+    const options = dataSourceSrv
       .getList({
         alerting,
         tracing,
@@ -170,58 +110,56 @@ export class MultipleDataSourcePicker extends PureComponent<DataSourcePickerProp
       }));
 
     return options;
-  }
+  };
 
-  render() {
-    const {
-      autoFocus,
-      onBlur,
-      onClear,
-      openMenuOnFocus,
-      placeholder,
-      width,
-      inputId,
-      disabled = false,
-      isLoading = false,
-    } = this.props;
-    const { error } = this.state;
-    const options = this.getDataSourceOptions();
-    const value = this.getCurrentValue();
-    const isClearable = typeof onClear === 'function';
+  const {
+    autoFocus,
+    onBlur,
+    onClear,
+    openMenuOnFocus,
+    placeholder,
+    width,
+    inputId,
+    disabled = false,
+    isLoading = false,
+  } = props;
 
-    return (
-      <div data-testid={selectors.components.DataSourcePicker.container}>
-        <MultiSelect
-          isLoading={isLoading}
-          disabled={disabled}
-          data-testid={selectors.components.DataSourcePicker.inputV2}
-          inputId={inputId || 'data-source-picker'}
-          className="ds-picker select-container"
-          isClearable={isClearable}
-          backspaceRemovesValue={true}
-          onChange={this.onChange}
-          options={options}
-          autoFocus={autoFocus}
-          onBlur={onBlur}
-          width={width}
-          openMenuOnFocus={openMenuOnFocus}
-          maxMenuHeight={500}
-          placeholder={placeholder}
-          noOptionsMessage="No datasources found"
-          value={value ?? []}
-          invalid={Boolean(error) || Boolean(this.props.invalid)}
-          getOptionLabel={(o) => {
-            if (o.meta && isUnsignedPluginSignature(o.meta.signature) && o !== value) {
-              return (
-                <HorizontalGroup align="center" justify="space-between" height="auto">
-                  <span>{o.label}</span> <PluginSignatureBadge status={o.meta.signature} />
-                </HorizontalGroup>
-              );
-            }
-            return o.label || '';
-          }}
-        />
-      </div>
-    );
-  }
-}
+  const options = getDataSourceOptions();
+  const value = getCurrentValue();
+  const isClearable = typeof onClear === 'function';
+
+  return (
+    <div data-testid={selectors.components.DataSourcePicker.container}>
+      <MultiSelect
+        isLoading={isLoading}
+        disabled={disabled}
+        data-testid={selectors.components.DataSourcePicker.inputV2}
+        inputId={inputId || 'data-source-picker'}
+        className="ds-picker select-container"
+        isClearable={isClearable}
+        backspaceRemovesValue={true}
+        onChange={onChange}
+        options={options}
+        autoFocus={autoFocus}
+        onBlur={onBlur}
+        width={width}
+        openMenuOnFocus={openMenuOnFocus}
+        maxMenuHeight={500}
+        placeholder={placeholder}
+        noOptionsMessage="No datasources found"
+        value={value ?? []}
+        invalid={Boolean(state?.error) || Boolean(props.invalid)}
+        getOptionLabel={(o) => {
+          if (o.meta && isUnsignedPluginSignature(o.meta.signature) && o !== value) {
+            return (
+              <HorizontalGroup align="center" justify="space-between" height="auto">
+                <span>{o.label}</span> <PluginSignatureBadge status={o.meta.signature} />
+              </HorizontalGroup>
+            );
+          }
+          return o.label || '';
+        }}
+      />
+    </div>
+  );
+};
