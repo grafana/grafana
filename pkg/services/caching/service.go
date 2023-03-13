c09c99cf0736ed92f54cb8e77e08a35bf723f9d7
Michael Mandrus <michael.mandrus@grafana.com>
2023-03-13 13:53:45 -0400

    update caching service interface

--- pkg/services/caching/service.go
+++ pkg/services/caching/service.go
@@ -3,6 +3,8 @@ package caching
 import (
 	"context"
 	"errors"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 )
 
 var (
@@ -14,13 +16,28 @@ func ProvideCachingService() *OSSCachingService {
 }
 
 type CachingService interface {
-	HandleRequest(context.Context) error
+	CacheQueryResponse(context.Context, *backend.QueryDataResponse) error
+	CacheResourceResponse(context.Context, *backend.QueryDataResponse) error
+	HandleQueryRequest(context.Context) (*backend.QueryDataResponse, error)
+	HandleResourceRequest(context.Context) (*backend.QueryDataResponse, error)
 }
 
 type OSSCachingService struct {
 }
 
-func (s *OSSCachingService) HandleRequest(ctx context.Context) error {
+func (s *OSSCachingService) HandleQueryRequest(ctx context.Context) (*backend.QueryDataResponse, error) {
+	return nil, ErrCachingNotAvailable
+}
+
+func (s *OSSCachingService) HandleResourceRequest(ctx context.Context) (*backend.QueryDataResponse, error) {
+	return nil, ErrCachingNotAvailable
+}
+
+func (s *OSSCachingService) CacheQueryResponse(ctx context.Context, resp *backend.QueryDataResponse) error {
+	return ErrCachingNotAvailable
+}
+
+func (s *OSSCachingService) CacheResourceResponse(ctx context.Context, resp *backend.QueryDataResponse) error {
 	return ErrCachingNotAvailable
 }
 
