1fbb0e90157acd505bf1732dfb3629dfb5949651
Alex Weaver <weaver.alex.d@gmail.com>
2023-03-14 11:17:56 -0500

    Add tests for writes and error propagation

--- pkg/services/ngalert/state/historian/fanout_test.go
+++ pkg/services/ngalert/state/historian/fanout_test.go
@@ -2,6 +2,7 @@ package historian
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
@@ -23,16 +24,55 @@ func TestFanoutBackend(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, "one", resp.Name)
 	})
+
+	t.Run("writes dispatch to all", func(t *testing.T) {
+		one := &fakeBackend{}
+		two := &fakeBackend{}
+		three := &fakeBackend{}
+		fan := NewFanoutBackend(one, two, three)
+		rule := history_model.RuleMeta{}
+		vs := []state.StateTransition{{}}
+
+		err := <-fan.Record(context.Background(), rule, vs)
+
+		require.NoError(t, err)
+		require.NotEmpty(t, one.last)
+		require.NotEmpty(t, two.last)
+		require.NotEmpty(t, three.last)
+	})
+
+	t.Run("writes combine errors", func(t *testing.T) {
+		one := &fakeBackend{err: fmt.Errorf("error one")}
+		two := &fakeBackend{err: fmt.Errorf("error two")}
+		three := &fakeBackend{}
+		fan := NewFanoutBackend(one, two, three)
+		rule := history_model.RuleMeta{}
+		vs := []state.StateTransition{{}}
+
+		err := <-fan.Record(context.Background(), rule, vs)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "error one")
+		require.ErrorContains(t, err, "error two")
+	})
 }
 
 type fakeBackend struct {
 	resp *data.Frame
+	err  error
+	last []state.StateTransition
 }
 
 func (f *fakeBackend) Record(ctx context.Context, rule history_model.RuleMeta, states []state.StateTransition) <-chan error {
-	return nil
+	ch := make(chan error, 1)
+	if f.err != nil {
+		ch <- f.err
+	}
+	f.last = states
+	defer close(ch)
+	return ch
 }
 
 func (f *fakeBackend) Query(ctx context.Context, query ngmodels.HistoryQuery) (*data.Frame, error) {
-	return f.resp, nil
+	return f.resp, f.err
 }
