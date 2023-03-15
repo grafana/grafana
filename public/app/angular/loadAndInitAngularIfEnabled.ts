deb53bab66eee3dc3143c0538c6c26a2180981ae
Ryan McKinley <ryantxu@gmail.com>
2023-03-14 19:00:52 -0700

    more

--- public/app/angular/loadAndInitAngularIfEnabled.ts
+++ public/app/angular/loadAndInitAngularIfEnabled.ts
@@ -1,5 +1,5 @@
 import { deprecationWarning } from '@grafana/data';
-import { config, setAngularLoader, setLegacyAngularInjector } from '@grafana/runtime';
+import { config, setAngularLoader, setLegacyAngularInjector, getDataSourceSrv, getBackendSrv } from '@grafana/runtime';
 import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
 
 export async function loadAndInitAngularIfEnabled() {
@@ -24,10 +24,27 @@ export async function loadAndInitAngularIfEnabled() {
     // Temporary path to allow access to services exposed directly by the angular injector
     setLegacyAngularInjector({
       get: (key: string) => {
+        // See ./registerComponents.ts
         switch (key) {
+          // This does not yet have a better option
           case 'dashboardSrv': {
-            deprecationWarning('getLegacyAngularInjector', 'getDashboardSrv'); // any suggestions?
+            deprecationWarning('getLegacyAngularInjector', 'getDashboardSrv');
+            // deprecated... but not yet clear what we want to actually expose :grimmice:
             return getDashboardSrv();
+          }
+
+          // These have better options in @grafana/runtime
+          case 'datasourceSrv': {
+            deprecationWarning(
+              'getLegacyAngularInjector',
+              'datasourceSrv',
+              'use getDataSourceSrv() in @grafana/runtime'
+            ); // any suggestions?
+            return getDataSourceSrv();
+          }
+          case 'backendSrv': {
+            deprecationWarning('getLegacyAngularInjector', 'backendSrv', 'use getBackendSrv() in @grafana/runtime');
+            return getBackendSrv();
           }
         }
         throw 'Angular is disabled.  Unable to expose: ' + key;
