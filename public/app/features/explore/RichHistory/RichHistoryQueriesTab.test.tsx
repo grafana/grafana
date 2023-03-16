b77335c7f8cf36ecf07f86abec93ac3804e9fcf7
Sven Grossmann <svennergr@gmail.com>
2023-03-16 11:40:44 +0100

    fix import

--- public/app/features/explore/RichHistory/RichHistoryQueriesTab.test.tsx
+++ public/app/features/explore/RichHistory/RichHistoryQueriesTab.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { DataSourceSrv, setDataSourceSrv } from '@grafana/runtime';
