14d598482070454b9fb8f4d98180d185ac34da4c
polinaboneva <polina.boneva@grafana.com>
2023-03-16 18:48:12 +0200

    Empty Dashboard state has its own CTA items and its own separate box to choose a library panel to create

---
+++ public/app/features/dashboard/components/AddLibraryPanelWidget/AddLibraryPanelWidget.tsx
@@ -0,0 +1,114 @@
+import { css, cx, keyframes } from '@emotion/css';
+import React from 'react';
+import { connect, MapDispatchToProps } from 'react-redux';
+import tinycolor from 'tinycolor2';
+
+import { GrafanaTheme2 } from '@grafana/data';
+import { LibraryPanel } from '@grafana/schema';
+import { IconButton, useStyles2 } from '@grafana/ui';
+import { addPanel } from 'app/features/dashboard/state/reducers';
+
+import {
+  LibraryPanelsSearch,
+  LibraryPanelsSearchVariant,
+} from '../../../library-panels/components/LibraryPanelsSearch/LibraryPanelsSearch';
+import { DashboardModel, PanelModel } from '../../state';
+
+export type PanelPluginInfo = { id: number; defaults: { gridPos: { w: number; h: number }; title: string } };
+
+export interface OwnProps {
+  panel: PanelModel;
+  dashboard: DashboardModel;
+}
+
+export interface DispatchProps {
+  addPanel: typeof addPanel;
+}
+
+export type Props = OwnProps & DispatchProps;
+
+export const AddLibraryPanelWidgetUnconnected = ({ panel, dashboard }: Props) => {
+  const onCancelAddPanel = (evt: React.MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    dashboard.removePanel(panel);
+  };
+
+  const onAddLibraryPanel = (panelInfo: LibraryPanel) => {
+    const { gridPos } = panel;
+
+    const newPanel = {
+      ...panelInfo.model,
+      gridPos,
+      libraryPanel: panelInfo,
+    };
+
+    dashboard.addPanel(newPanel);
+    dashboard.removePanel(panel);
+  };
+
+  const styles = useStyles2(getStyles);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={cx('panel-container', styles.callToAction)}>
+        <div className={cx(styles.headerRow, 'grid-drag-handle')}>
+          <span>Add panel from panel library</span>
+          <div className="flex-grow-1" />
+          <IconButton aria-label="Close 'Add Panel' widget" name="times" onClick={onCancelAddPanel} />
+        </div>
+        <LibraryPanelsSearch onClick={onAddLibraryPanel} variant={LibraryPanelsSearchVariant.Tight} showPanelFilter />
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, OwnProps> = { addPanel };
+
+export const AddLibraryPanelWidget = connect(undefined, mapDispatchToProps)(AddLibraryPanelWidgetUnconnected);
+
+const getStyles = (theme: GrafanaTheme2) => {
+  const pulsate = keyframes({
+    '0%': {
+      boxShadow: `0 0 0 2px ${theme.colors.background.canvas}, 0 0 0px 4px ${theme.colors.primary.main}`,
+    },
+    '50%': {
+      boxShadow: `0 0 0 2px ${theme.components.dashboard.background}, 0 0 0px 4px ${tinycolor(theme.colors.primary.main)
+        .darken(20)
+        .toHexString()}`,
+    },
+    '100%': {
+      boxShadow: `0 0 0 2px ${theme.components.dashboard.background}, 0 0 0px 4px  ${theme.colors.primary.main}`,
+    },
+  });
+
+  return {
+    // wrapper is used to make sure box-shadow animation isn't cut off in dashboard page
+    wrapper: css({
+      height: '100%',
+      paddingTop: `${theme.spacing(0.5)}`,
+    }),
+    headerRow: css({
+      display: 'flex',
+      alignItems: 'center',
+      height: '38px',
+      flexShrink: 0,
+      width: '100%',
+      fontSize: theme.typography.fontSize,
+      fontWeight: theme.typography.fontWeightMedium,
+      paddingLeft: `${theme.spacing(1)}`,
+      transition: 'background-color 0.1s ease-in-out',
+      cursor: 'move',
+
+      '&:hover': {
+        background: `${theme.colors.background.secondary}`,
+      },
+    }),
+    callToAction: css({
+      overflow: 'hidden',
+      outline: '2px dotted transparent',
+      outlineOffset: '2px',
+      boxShadow: '0 0 0 2px black, 0 0 0px 4px #1f60c4',
+      animation: `${pulsate} 2s ease infinite`,
+    }),
+  };
+};
