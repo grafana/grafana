eeff74092fd70005ad9991cf844f8d6a08dbc1e3
Ivan Prokushkin <ivnpro@yandex-team.ru>
2023-03-16 17:52:11 +0300

    align __interval to minInterval so it divides it

--- pkg/tsdb/intervalv2/intervalv2_test.go
+++ pkg/tsdb/intervalv2/intervalv2_test.go
@@ -16,24 +16,30 @@ func TestIntervalCalculator_Calculate(t *testing.T) {
 	timeNow := time.Now()
 
 	testCases := []struct {
-		name       string
-		timeRange  backend.TimeRange
-		resolution int64
-		expected   string
+		name        string
+		timeRange   backend.TimeRange
+		resolution  int64
+		expected    string
+		minInterval time.Duration
 	}{
-		{"from 5m to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(5 * time.Minute)}, 0, "200ms"},
-		{"from 5m to now and 500 resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(5 * time.Minute)}, 500, "500ms"},
-		{"from 15m to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(15 * time.Minute)}, 0, "500ms"},
-		{"from 15m to now and 100 resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(15 * time.Minute)}, 100, "10s"},
-		{"from 30m to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(30 * time.Minute)}, 0, "1s"},
-		{"from 30m to now and 3000 resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(30 * time.Minute)}, 3000, "500ms"},
-		{"from 1h to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(time.Hour)}, 0, "2s"},
-		{"from 1h to now and 1000 resoluion", backend.TimeRange{From: timeNow, To: timeNow.Add(time.Hour)}, 1000, "5s"},
+		{"from 5m to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(5 * time.Minute)}, 0, "200ms", time.Millisecond * 1},
+		{"from 5m to now and 500 resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(5 * time.Minute)}, 500, "500ms", time.Millisecond * 1},
+		{"from 15m to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(15 * time.Minute)}, 0, "500ms", time.Millisecond * 1},
+		{"from 15m to now and 100 resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(15 * time.Minute)}, 100, "10s", time.Millisecond * 1},
+		{"from 30m to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(30 * time.Minute)}, 0, "1s", time.Millisecond * 1},
+		{"from 30m to now and 3000 resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(30 * time.Minute)}, 3000, "500ms", time.Millisecond * 1},
+		{"from 1h to now and default resolution", backend.TimeRange{From: timeNow, To: timeNow.Add(time.Hour)}, 0, "2s", time.Millisecond * 1},
+		{"from 1h to now and 1000 resoluion", backend.TimeRange{From: timeNow, To: timeNow.Add(time.Hour)}, 1000, "5s", time.Millisecond * 1},
+
+		{"from 5m to now and default resolution and with zero mininterva", backend.TimeRange{From: timeNow, To: timeNow.Add(5 * time.Minute)}, 0, "200ms", 0},
+		{"from 5m to now and 500 resolution and with mininterval", backend.TimeRange{From: timeNow, To: timeNow.Add(5 * time.Minute)}, 500, "354ms", time.Millisecond * 177},
+		{"from 15m to now and default resolution and with mininterval", backend.TimeRange{From: timeNow, To: timeNow.Add(15 * time.Minute)}, 0, "354ms", time.Millisecond * 177},
+		{"from 15m to now and 100 resolution and with mininterval", backend.TimeRange{From: timeNow, To: timeNow.Add(15 * time.Minute)}, 100, "8s", time.Second * 4},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			interval := calculator.Calculate(tc.timeRange, time.Millisecond*1, tc.resolution)
+			interval := calculator.Calculate(tc.timeRange, tc.minInterval, tc.resolution)
 			assert.Equal(t, tc.expected, interval.Text)
 		})
 	}
