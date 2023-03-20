727022888851b49f31f834611d99f7d13f9c343c
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-03-20 15:13:44 +0100

    Radius for Tab

--- packages/grafana-ui/src/components/Tabs/Tab.tsx
+++ packages/grafana-ui/src/components/Tabs/Tab.tsx
@@ -118,7 +118,7 @@ const getTabStyles = stylesFactory((theme: GrafanaTheme2) => {
         left: 0;
         right: 0;
         height: 4px;
-        border-radius: ${theme.shape.borderRadius()};
+        border-radius: ${theme.shape.radius.default};
         bottom: 0px;
         background-image: ${theme.colors.gradients.brandHorizontal} !important;
       }
