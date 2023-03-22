59cb4e8d92c5274f598343a9c6badc9a77400baa
Domas <domasx2@gmail.com>
2023-03-22 11:58:21 +0200

    check if Faro is defined

--- packages/grafana-ui/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ packages/grafana-ui/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -39,7 +39,7 @@ export class ErrorBoundary extends PureComponent<Props, State> {
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     captureException(error, { contexts: { react: { componentStack: errorInfo.componentStack } } });
-    faro.api.pushError(error);
+    faro?.api.pushError(error);
     this.setState({ error, errorInfo });
 
     if (this.props.onError) {
