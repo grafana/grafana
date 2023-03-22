4820cb466aa72d07010ce19330dd57df3fc124e1
ismail simsek <ismailsimsek09@gmail.com>
2023-03-22 22:34:31 +0100

    Fix import

--- packages/grafana-runtime/src/utils/DataSourceWithBackend.test.ts
+++ packages/grafana-runtime/src/utils/DataSourceWithBackend.test.ts
@@ -11,7 +11,12 @@ import {
   DataSourceRef,
 } from '@grafana/data';
 
-import { DataSourceWithBackend, standardStreamOptionsProvider, toStreamingDataResponse } from './DataSourceWithBackend';
+import {
+  DataSourceWithBackend,
+  standardStreamOptionsProvider,
+  toStreamingDataResponse,
+  isExpressionReference,
+} from './DataSourceWithBackend';
 
 class MyDataSource extends DataSourceWithBackend<DataQuery, DataSourceJsonData> {
   constructor(instanceSettings: DataSourceInstanceSettings<DataSourceJsonData>) {
