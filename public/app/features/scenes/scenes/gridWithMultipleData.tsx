647f693e598e7d9e533e81bf4d9116364219c7a3
Torkel Ödegaard <torkel@grafana.com>
2023-03-23 10:31:25 +0100

    Scenes: Update to latest scenes package and remove editing stuff  (#65222)
    * Update to latest scenes and remove editing stuff
    
    * fixing test

--- public/app/features/scenes/scenes/gridWithMultipleData.tsx
+++ public/app/features/scenes/scenes/gridWithMultipleData.tsx
@@ -2,7 +2,6 @@ import { VizPanel, SceneGridRow, SceneTimePicker, SceneGridLayout, SceneTimeRang
 import { TestDataQueryType } from 'app/plugins/datasource/testdata/dataquery.gen';
 
 import { DashboardScene } from '../dashboard/DashboardScene';
-import { SceneEditManager } from '../editor/SceneEditManager';
 
 import { getQueryRunnerWithRandomWalkQuery } from './queries';
 
@@ -83,7 +82,6 @@ export function getGridWithMultipleData(): DashboardScene {
         }),
       ],
     }),
-    $editor: new SceneEditManager({}),
     $timeRange: new SceneTimeRange(),
     $data: getQueryRunnerWithRandomWalkQuery(),
     actions: [new SceneTimePicker({})],
