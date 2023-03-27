c5d38d0880f946b73e15f9d785d2b3f47a198a92
Levente Balogh <balogh.levente.hu@gmail.com>
2023-03-27 08:10:59 +0200

    Wip

---
+++ packages/grafana-runtime/src/services/pluginExtensions/getPluginExtensions.ts
@@ -0,0 +1,24 @@
+import { PluginExtension } from '@grafana/data';
+
+export type GetPluginExtensions = ({ placement, context }: { placement: string; context?: object }) => {
+  extensions: PluginExtension[];
+};
+
+let singleton: GetPluginExtensions | undefined;
+
+export function setPluginExtensionGetter(instance: GetPluginExtensions): void {
+  // We allow overriding the registry in tests
+  if (singleton && process.env.NODE_ENV !== 'test') {
+    throw new Error('setPluginExtensionGetter() function should only be called once, when Grafana is starting.');
+  }
+  singleton = instance;
+}
+
+function getPluginExtensionGetter(): GetPluginExtensions {
+  if (!singleton) {
+    throw new Error('getPluginExtensionGetter() can only be used after the Grafana instance has started.');
+  }
+  return singleton;
+}
+
+export const getPluginExtensions: GetPluginExtensions = (options) => getPluginExtensionGetter()(options);
