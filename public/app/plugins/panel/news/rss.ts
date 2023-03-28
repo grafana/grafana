75a43fe5e799728a4e3128d1144b06d45016c38e
Kristian Bremberg <kristian.bremberg@grafana.com>
2023-03-28 18:30:24 +0200

    remove console.log

--- public/app/plugins/panel/news/rss.ts
+++ public/app/plugins/panel/news/rss.ts
@@ -6,7 +6,6 @@ import { Feed } from './types';
 export function parseRSSFeed(txt: string): Feed {
   const domParser = new DOMParser();
   const doc = domParser.parseFromString(textUtil.sanitizeTrustedTypes(txt, 'rss') as unknown as string, 'text/xml');
-console.log(doc);
   const feed: Feed = {
     items: Array.from(doc.querySelectorAll('item')).map((node) => ({
       title: getProperty(node, 'title'),
