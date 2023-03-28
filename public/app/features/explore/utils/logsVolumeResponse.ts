4893cf0a40019e804a7a6dbe1791781877bf4e79
Matias Chomicki <matyax@gmail.com>
2023-03-28 12:45:38 +0200

    Add unit test

--- public/app/features/explore/utils/logsVolumeResponse.ts
+++ public/app/features/explore/utils/logsVolumeResponse.ts
@@ -1,4 +1,4 @@
-import { DataQueryResponse } from '@grafana/data';
+import { DataQueryError, DataQueryResponse } from '@grafana/data';
 
 // Currently we can only infer if an error response is a timeout or not.
 // TODO: add backend support for specifying the type of error.
@@ -6,13 +6,13 @@ export function isTimeoutErrorResponse(response: DataQueryResponse | undefined):
   if (!response) {
     return false;
   }
-  if (!response?.error && response?.errors) {
+  if (!response?.error && !response?.errors) {
     return false;
   }
 
   const errors = response?.error ? [response?.error] : response?.errors || [];
 
-  return errors.reduce((isTimeoutError, error) => {
+  return errors.reduce((isTimeoutError: boolean, error: DataQueryError) => {
     const message = `${error.message || error.data?.message}`?.toLowerCase();
     return isTimeoutError || message.includes('timeout');
   }, false);
