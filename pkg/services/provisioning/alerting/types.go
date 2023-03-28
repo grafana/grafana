41b97746c707926577352045524ceee27ffc73dc
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-03-28 16:02:06 -0400

    missing import

--- pkg/services/provisioning/alerting/types.go
+++ pkg/services/provisioning/alerting/types.go
@@ -3,6 +3,7 @@ package alerting
 import (
 	"fmt"
 
+	"github.com/grafana/grafana/pkg/services/ngalert/models"
 	"github.com/grafana/grafana/pkg/services/provisioning/alerting/file"
 	"github.com/grafana/grafana/pkg/services/provisioning/values"
 )
@@ -16,7 +17,7 @@ type OrgID int64
 type AlertingFile struct {
 	configVersion
 	Filename            string
-	Groups              []file.AlertRuleGroupWithFolderTitle
+	Groups              []models.AlertRuleGroupWithFolderTitle
 	DeleteRules         []file.RuleDelete
 	ContactPoints       []ContactPoint
 	DeleteContactPoints []DeleteContactPoint
