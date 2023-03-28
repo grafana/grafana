0e384f7e6f00bcb6e440f22078b75bf19fe1a95f
Leon Sorokin <leeoniya@gmail.com>
2023-03-28 17:16:24 -0500

    HeatMap: Sort y buckets when all bucket names are numeric (#65322)
    (cherry picked from commit 6f4232bee383a7782ebbdcd31624f11a2d2bdfab)

--- public/app/plugins/panel/heatmap/fields.ts
+++ public/app/plugins/panel/heatmap/fields.ts
@@ -19,6 +19,7 @@ import {
   rowsToCellsHeatmap,
 } from 'app/features/transformers/calculateHeatmap/heatmap';
 import { HeatmapCellLayout } from 'app/features/transformers/calculateHeatmap/models.gen';
+import { parseSampleValue, sortSeriesByLabel } from 'app/plugins/datasource/prometheus/result_transformer';
 
 import { CellValues, PanelOptions } from './models.gen';
 import { boundedMinMax } from './utils';
@@ -94,6 +95,14 @@ export function prepareHeatmapData(
   // Everything past here assumes a field for each row in the heatmap (buckets)
   if (!rowsHeatmap) {
     if (frames.length > 1) {
+      let allNamesNumeric = frames.every(
+        (frame) => !Number.isNaN(parseSampleValue(frame.name ?? frame.fields[1].name))
+      );
+
+      if (allNamesNumeric) {
+        frames.sort(sortSeriesByLabel);
+      }
+
       rowsHeatmap = [
         outerJoinDataFrames({
           frames,
