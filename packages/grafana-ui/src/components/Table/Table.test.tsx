427403fb4551cfca3bb665a5daea70c2eea03629
jev forsberg <jev.forsberg@grafana.com>
2023-03-29 09:56:51 -0600

    baldm0mma/revert/ update tests

--- packages/grafana-ui/src/components/Table/Table.test.tsx
+++ packages/grafana-ui/src/components/Table/Table.test.tsx
@@ -447,7 +447,7 @@ describe('Table', () => {
       expect(within(getFooter()).getByRole('columnheader').getElementsByTagName('span')[0].textContent).toEqual(
         'Count'
       );
-      expect(within(getFooter()).getByRole('columnheader').getElementsByTagName('span')[1].textContent).toEqual('4');
+      expect(within(getFooter()).getByRole('columnheader').getElementsByTagName('span')[1].textContent).toEqual('5');
     });
 
     it('should show correct counts when turning `count rows` on and off', async () => {
@@ -473,7 +473,7 @@ describe('Table', () => {
       expect(within(getFooter()).getByRole('columnheader').getElementsByTagName('span')[0].textContent).toEqual(
         'Count'
       );
-      expect(within(getFooter()).getByRole('columnheader').getElementsByTagName('span')[1].textContent).toEqual('4');
+      expect(within(getFooter()).getByRole('columnheader').getElementsByTagName('span')[1].textContent).toEqual('5');
 
       const onSortByChange = jest.fn();
       const onCellFilterAdded = jest.fn();
