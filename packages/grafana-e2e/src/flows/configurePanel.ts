92533066d1780349950afc5b55194757cef03b73
Ashley Harrison <ashley.harrison@grafana.com>
2023-03-30 16:07:23 +0100

    check visibility of add panel button

--- packages/grafana-e2e/src/flows/configurePanel.ts
+++ packages/grafana-e2e/src/flows/configurePanel.ts
@@ -91,6 +91,7 @@ export const configurePanel = (config: PartialAddPanelConfig | PartialEditPanelC
       e2e.components.Panels.Panel.headerItems('Edit').click();
     } else {
       try {
+        e2e.components.PageToolbar.item('Add panel').should('be.visible');
         e2e.components.PageToolbar.item('Add panel').click();
       } catch (e) {
         // Depending on the screen size, the "Add panel" button might be hidden
