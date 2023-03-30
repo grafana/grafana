c65cd5833aff0aa4f666457554e6b1486940a313
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-03-30 11:46:11 +0300

    Nested folders: Fix move (#65526)
    * Nested Folders: Fix move response
    
    Fix JSON tags to be consistent with the GET endpoint
    
    * Add API test for moving to root folder
    
    * Allow move to root folder
    
    * Fix failure message
    
    * Update OpenAPI specification

--- pkg/api/folder.go
+++ pkg/api/folder.go
@@ -201,25 +201,37 @@ func (hs *HTTPServer) setDefaultFolderPermissions(ctx context.Context, orgID int
 	return nil
 }
 
+// swagger:route POST /folders/{folder_uid}/move folders moveFolder
+//
+// Move folder.
+//
+// Responses:
+// 200: folderResponse
+// 401: unauthorisedError
+// 403: forbiddenError
+// 404: notFoundError
+// 500: internalServerError
 func (hs *HTTPServer) MoveFolder(c *contextmodel.ReqContext) response.Response {
 	if hs.Features.IsEnabled(featuremgmt.FlagNestedFolders) {
 		cmd := folder.MoveFolderCommand{}
 		if err := web.Bind(c.Req, &cmd); err != nil {
 			return response.Error(http.StatusBadRequest, "bad request data", err)
 		}
-		var theFolder *folder.Folder
 		var err error
 
-		if cmd.NewParentUID != "" {
-			cmd.OrgID = c.OrgID
-			cmd.UID = web.Params(c.Req)[":uid"]
-			cmd.SignedInUser = c.SignedInUser
-			theFolder, err = hs.folderService.Move(c.Req.Context(), &cmd)
-			if err != nil {
-				return response.Error(http.StatusInternalServerError, "update folder uid failed", err)
-			}
+		cmd.OrgID = c.OrgID
+		cmd.UID = web.Params(c.Req)[":uid"]
+		cmd.SignedInUser = c.SignedInUser
+		theFolder, err := hs.folderService.Move(c.Req.Context(), &cmd)
+		if err != nil {
+			return response.Error(http.StatusInternalServerError, "move folder failed", err)
 		}
-		return response.JSON(http.StatusOK, theFolder)
+
+		g, err := guardian.NewByUID(c.Req.Context(), cmd.UID, c.OrgID, c.SignedInUser)
+		if err != nil {
+			return response.Err(err)
+		}
+		return response.JSON(http.StatusOK, hs.newToFolderDto(c, g, theFolder))
 	}
 	result := map[string]string{}
 	result["message"] = "To use this service, you need to activate nested folder feature."
@@ -406,6 +418,16 @@ type CreateFolderParams struct {
 	// in:body
 	// required:true
 	Body folder.CreateFolderCommand `json:"body"`
+}
+
+// swagger:parameters moveFolder
+type MoveFolderParams struct {
+	// in:path
+	// required:true
+	FolderUID string `json:"folder_uid"`
+	// in:body
+	// required:true
+	Body folder.MoveFolderCommand `json:"body"`
 }
 
 // swagger:parameters deleteFolder
