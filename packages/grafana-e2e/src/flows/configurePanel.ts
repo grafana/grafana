fef6b214d57d26b3a86a900187a0d0bdf4c4cf3b
Ashley Harrison <ashley.harrison@grafana.com>
2023-03-30 16:10:09 +0100

    check visibility of add new panel button before clicking

--- packages/grafana-e2e/src/flows/configurePanel.ts
+++ packages/grafana-e2e/src/flows/configurePanel.ts
@@ -98,6 +98,7 @@ export const configurePanel = (config: PartialAddPanelConfig | PartialEditPanelC
         e2e.components.PageToolbar.item('Show more items').click();
         e2e.components.PageToolbar.item('Add panel').last().click();
       }
+      e2e.pages.AddDashboard.addNewPanel().should('be.visible');
       e2e.pages.AddDashboard.addNewPanel().click();
     }
 
