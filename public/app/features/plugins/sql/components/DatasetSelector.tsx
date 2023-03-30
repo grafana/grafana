21dc37d7f023497d990159f5c003f2def0d5cd0f
jev forsberg <jev.forsberg@grafana.com>
2023-03-30 15:03:20 -0600

    baldm0mma/sql_datasource_update/ clean datasetSelector

--- public/app/features/plugins/sql/components/DatasetSelector.tsx
+++ public/app/features/plugins/sql/components/DatasetSelector.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { useAsync } from 'react-use';
 
 import { SelectableValue } from '@grafana/data';
 import { Select } from '@grafana/ui';
@@ -12,39 +11,13 @@ interface DatasetSelectorProps extends ResourceSelectorProps {
   onChange: (v: SelectableValue) => void;
 }
 
-export const DatasetSelector = ({ defaultDatabase, onChange }: DatasetSelectorProps) => {
-  // const state = useAsync(async () => {
-  //   const datasets = await db.datasets();
-  //   return datasets.map(toOption);
-  // }, []);
-
-  // const useDefaultDataset = useEffect(() => {
-  //   if (!applyDefault) {
-  //     return;
-  //   }
-  //   // Set default dataset when values are fetched
-  //   if (!value) {
-  //     if (state.value && state.value[0]) {
-  //       onChange(state.value[0]);
-  //     }
-  //   } else {
-  //     if (state.value && state.value.find((v) => v.value === value) === undefined) {
-  //       // if value is set and newly fetched values does not contain selected value
-  //       if (state.value.length > 0) {
-  //         onChange(state.value[0]);
-  //       }
-  //     }
-  //   }
-  // }, [state.value, value, applyDefault, onChange]);
-
-  return (
-    <Select
-      aria-label="Dataset selector"
-      value={toOption(defaultDatabase)}
-      disabled={true}
-      onChange={onChange}
-      menuShouldPortal={true}
-      placeholder={defaultDatabase}
-    />
-  );
-};
+export const DatasetSelector = ({ defaultDatabase, onChange }: DatasetSelectorProps) => (
+  <Select
+    aria-label="Dataset selector"
+    value={toOption(defaultDatabase)}
+    disabled={true}
+    onChange={onChange}
+    menuShouldPortal={true}
+    placeholder={defaultDatabase}
+  />
+);
