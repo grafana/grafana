45db44ef8d8eea5fc73c77c1f006410c87883a83
Kristian Bremberg <kristian.bremberg@grafana.com>
2023-04-01 09:29:39 +0200

    Sanitize scriptURL

--- public/app/trustedTypePolicies.ts
+++ public/app/trustedTypePolicies.ts
@@ -1,5 +1,6 @@
 import DOMPurify from 'dompurify';
 
+import { textUtil } from '@grafana/data';
 import { config } from '@grafana/runtime';
 
 if (config.bootData.settings.featureToggles.trustedTypes && window.trustedTypes && window.trustedTypes.createPolicy) {
@@ -7,6 +8,6 @@ if (config.bootData.settings.featureToggles.trustedTypes && window.trustedTypes 
   window.trustedTypes.createPolicy('default', {
     createHTML: (string) => DOMPurify.sanitize(string, { RETURN_TRUSTED_TYPE: true }).toString(),
     createScript: (string) => string,
-    createScriptURL: (string) => (string.startsWith('data:') ? '' : string),
+    createScriptURL: (string) => textUtil.sanitizeUrl(string),
   });
 }
