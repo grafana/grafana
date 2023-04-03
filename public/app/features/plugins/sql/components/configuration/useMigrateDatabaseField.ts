76618467ddcb91e0646c5eb4ceb79b6cbb7123ed
Kyle Cunningham <kyle@codeincarnate.com>
2023-04-03 15:03:41 +0700

    Add migration

--- public/app/features/plugins/sql/components/configuration/useMigrateDatabaseField.ts
+++ public/app/features/plugins/sql/components/configuration/useMigrateDatabaseField.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-import { DataSourceJsonData, DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { logDebug } from '@grafana/runtime';
 
 import { SQLOptions } from '../../types';
@@ -8,11 +8,13 @@ import { SQLOptions } from '../../types';
 /**
  * Moves the database field from the options object to jsonData.database and empties the database field.
  */
-export function useMigrateDatabaseField<T extends DataSourceJsonData = SQLOptions, S = {}>({
+export function useMigrateDatabaseField<T extends SQLOptions, S = {}>({
   onOptionsChange,
   options,
 }: DataSourcePluginOptionsEditorProps<T, S>) {
   useEffect(() => {
+    const jsonData = options.jsonData;
+
     if (options.database) {
       logDebug(`Migrating from options.database with value ${options.database} for ${options.name}`);
       onOptionsChange({
@@ -21,5 +23,26 @@ export function useMigrateDatabaseField<T extends DataSourceJsonData = SQLOption
         jsonData: { ...options.jsonData, database: options.database },
       });
     }
+
+    if (
+      jsonData.maxOpenConns === undefined &&
+      jsonData.maxIdleConns === undefined &&
+      jsonData.maxIdleConnsAuto === undefined
+    ) {
+      // logDebug(`Setting default max open connections to ${} and setting max idle connection to ${}`);
+
+      onOptionsChange({
+        ...options,
+        jsonData: {
+          ...jsonData,
+          maxOpenConns: 100,
+          maxIdleConns: 50,
+          maxIdleConnsAuto: true,
+        },
+      });
+    }
+
+    // const ret = onOptionsChange({...options});
+    // console.log(ret);
   }, [onOptionsChange, options]);
 }
