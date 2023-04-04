35567acb187b46ed17d28ff7674a37ffbd2e0941
Leon Sorokin <leeoniya@gmail.com>
2023-04-04 14:51:24 -0500

    remove unused xField

--- packages/grafana-ui/src/components/GraphNG/GraphNG.tsx
+++ packages/grafana-ui/src/components/GraphNG/GraphNG.tsx
@@ -39,7 +39,6 @@ export const FIXED_UNIT = '__fixed';
 export type PropDiffFn<T extends any = any> = (prev: T, next: T) => boolean;
 
 export interface GraphNGProps extends Themeable2 {
-  xField?: string;
   frames: DataFrame[];
   structureRev?: number; // a number that will change when the frames[] structure changes
   width: number;
