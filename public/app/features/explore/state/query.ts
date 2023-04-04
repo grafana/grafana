c2543352df1b55b463309ede67111c9f14a1be1e
Abdul Haidar <abdulhdr1@gmail.com>
2023-04-04 00:45:25 -0300

    assign `null` to `clearedAtIndex` on first batch of logs live

--- public/app/features/explore/state/query.ts
+++ public/app/features/explore/state/query.ts
@@ -967,6 +967,13 @@ export const queryReducer = (state: ExploreItemState, action: AnyAction): Explor
   }
 
   if (queryStreamUpdatedAction.match(action)) {
+    const isFirstStreaming =
+      action.payload.response.state === LoadingState.Streaming && state.queryResponse.state === LoadingState.Loading;
+
+    if (isFirstStreaming) {
+      return processQueryResponse({ ...state, clearedAtIndex: null }, action);
+    }
+
     return processQueryResponse(state, action);
   }
 
