f3e13ad6fa62de907150e5b3c3283620684049a8
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2023-04-04 19:56:47 +0100

    Row-repeats: Fix disappearing panel when viewed panel is refreshed (#58392)
    * Row-repeats: Fix disappearing panel when viewed panel is refreshed
    
    Close #57159
    
    (cherry picked from commit f932952c68f19b853bb81bd55db03827a90aed67)

--- public/app/features/dashboard/state/DashboardModel.ts
+++ public/app/features/dashboard/state/DashboardModel.ts
@@ -552,7 +552,7 @@ export class DashboardModel implements TimeModel {
   }
 
   processRepeats() {
-    if (this.isSnapshotTruthy() || !this.hasVariables()) {
+    if (this.isSnapshotTruthy() || !this.hasVariables() || this.panelInView) {
       return;
     }
 
