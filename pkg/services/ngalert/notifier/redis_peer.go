1e3e1af326861cbca5351216fc35f3d4b0c7a44a
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2023-04-05 10:06:24 +0200

    fix typo

--- pkg/services/ngalert/notifier/redis_peer.go
+++ pkg/services/ngalert/notifier/redis_peer.go
@@ -496,7 +496,7 @@ func (c *RedisChannel) Broadcast(b []byte) {
 	c.p.messagesSent.WithLabelValues(c.msgType).Inc()
 	c.p.messagesSentSize.WithLabelValues(c.msgType).Add(float64(len(b)))
 	pub := c.p.redis.Publish(context.Background(), c.channel, string(b))
-	// In error here might not be as critical as one might think on first sight.
+	// An error here might not be as critical as one might think on first sight.
 	// The state will eventually be propagted to other members by the full sync.
 	if pub.Err() != nil {
 		c.p.logger.Error("msg", "error publishing message to redis", "err", pub.Err(), "channel", c.channel)
