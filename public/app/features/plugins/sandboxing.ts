ebdfe0a8feb6aa4c693a428e143ffb59e56511aa
Esteban Beltran <esteban@academo.me>
2023-04-05 11:47:49 +0200

    Testing near membrane

--- public/app/features/plugins/sandboxing.ts
+++ public/app/features/plugins/sandboxing.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="ses" />
-import 'ses';
+import createVirtualEnvironment from '@locker/near-membrane-dom';
 import react from 'react';
 
 import * as grafanaData from '@grafana/data';
@@ -45,7 +45,7 @@ export function getSandboxedWebApis({ pluginName, isDevMode }: { pluginName: str
   };
 }
 
-const compartmentDependencies = {
+export const compartmentDependencies = {
   '@grafana/data': grafanaData,
   '@grafana/ui': grafanaUIraw,
   '@grafana/runtime': grafanaRuntime,
@@ -60,7 +60,6 @@ const importSandboxCache = new Map<string, Promise<any>>();
  * For non-sandbox plugins, this is handled by systemjs
  */
 export function importPluginInsideSandbox(path: string): Promise<any> {
-  doLockdown();
   if (importSandboxCache.has(path)) {
     return importSandboxCache.get(path)!;
   }
@@ -70,47 +69,47 @@ export function importPluginInsideSandbox(path: string): Promise<any> {
 }
 
 export async function doImportPluginInsideSandbox(path: string): Promise<any> {
-  const pluginName = path.split('/')[1];
-  console.log('Importing plugin inside sandbox: ', pluginName, ' from path: ', path, '');
-  // load plugin code into a string
-  const response = await fetch('public/' + path + '.js');
-  const pluginCode = await response.text();
+  let resolved = false;
+  return new Promise(async (resolve, reject) => {
+    const pluginName = path.split('/')[1];
+    console.log('Importing plugin inside sandbox: ', pluginName, ' from path: ', path, '');
+    // load plugin code into a string
+    const response = await fetch('public/' + path + '.js');
+    const pluginCode = await response.text();
 
-  let pluginExports = {};
-  const isDevMode = process.env.NODE_ENV === 'development';
+    let pluginExports = {};
+    // const isDevMode = process.enjv.NODE_ENV === 'development';
 
-  //@ts-ignore - Compartment is a global
-  const pluginCompartment = new Compartment({
-    define: function (deps: string[], code: any) {
-      //@ts-ignore - emulate a minimal AMD setup
-      const resolvedDeps = deps.map((dep) => compartmentDependencies[dep]);
-      // execute the module's code with dependencies to get its export
-      pluginExports = code(...resolvedDeps);
-    },
-    ...getSandboxedWebApis({
-      pluginName,
-      isDevMode,
-    }),
-  });
+    const distortionMap = new Map();
 
-  try {
-    pluginCompartment.evaluate(pluginCode, {
-      // TODO: Figure if we can fix webpack's eval sourcecode
-      // __rejectSomeDirectEvalExpressions__: !isDevMode,
+    const env = createVirtualEnvironment(window, {
+      distortionCallback(v) {
+        return distortionMap.get(v) ?? v;
+      },
+      endowments: Object.getOwnPropertyDescriptors({
+        define(deps: string[], code: () => {}) {
+          //@ts-ignore
+          const resolvedDeps = deps.map((dep) => compartmentDependencies[dep]);
+          // execute the module's code with dependencies to get its export
+          //@ts-ignore
+          pluginExports = code.apply(null, resolvedDeps);
+          if (!resolved) {
+            resolved = true;
+            resolve(pluginExports);
+          }
+        },
+        ...getSandboxedWebApis({
+          pluginName,
+          isDevMode: true,
+        }),
+      }),
     });
-    return pluginExports;
-  } catch (e) {
-    console.error(`[sandbox] Error loading plugin ${path}`, e);
-    throw e;
-  }
-}
 
-let isLockedDown = false;
-
-function doLockdown() {
-  if (isLockedDown) {
-    return;
-  }
-  isLockedDown = true;
-  console.log('Locking down');
+    try {
+      env.evaluate(pluginCode);
+    } catch (e) {
+      console.error(`[sandbox] Error loading plugin ${path}`, e);
+      reject(e);
+    }
+  });
 }
