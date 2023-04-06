b646064db28a61566f4e040224d63ac036e82171
Torkel Ödegaard <torkel@grafana.com>
2023-04-06 17:58:47 +0200

    Fix lint error

--- packages/grafana-ui/src/components/MatchersUI/FieldsByFrameRefIdMatcher.test.tsx
+++ packages/grafana-ui/src/components/MatchersUI/FieldsByFrameRefIdMatcher.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { toDataFrame, FieldType } from '@grafana/data';
