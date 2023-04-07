e89f74c3b618c0ca73650b3f734cebc237b9e6be
Gilles De Mey <gilles.de.mey@gmail.com>
2023-04-07 13:40:49 +0200

    add comment on missing datasource

--- public/app/features/alerting/unified/components/rule-viewer/RuleViewerVisualization.tsx
+++ public/app/features/alerting/unified/components/rule-viewer/RuleViewerVisualization.tsx
@@ -142,6 +142,14 @@ function createExploreLink(settings: DataSourceInstanceSettings, model: AlertDat
   const { name } = settings;
   const { refId, ...rest } = model;
 
+  /**
+    In my testing I've found some alerts that don't have a data source embedded inside the model.
+   
+    At this moment in time it is unclear to me why some alert definitions not have a data source embedded in the model.
+    Ideally we'd resolve the datasource name to the proper datasource Ref "{ type: string, uid: string }" and pass that in to the model.
+   
+    I don't think that should happen here, the fact that the datasource ref is sometimes missing here is a symptom of another cause. (Gilles)
+   */
   return urlUtil.renderUrl(`${config.appSubUrl}/explore`, {
     left: JSON.stringify({
       datasource: name,
