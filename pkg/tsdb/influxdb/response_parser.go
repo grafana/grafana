520d653a8ee757ad0ab6563db06dac2f2dc30451
Galen <galen.kistler@grafana.com>
2023-04-07 10:23:57 -0500

    add interval to time field config

--- pkg/tsdb/influxdb/response_parser.go
+++ pkg/tsdb/influxdb/response_parser.go
@@ -103,6 +103,7 @@ func transformRows(rows []Row, query Query) data.Frames {
 		} else {
 			for colIndex, column := range row.Columns {
 				if column == "time" {
+
 					continue
 				}
 
@@ -146,6 +147,9 @@ func transformRows(rows []Row, query Query) data.Frames {
 				name := string(formatFrameName(row, column, query, frameName[:]))
 
 				timeField := data.NewField("time", nil, timeArray)
+
+				timeField.SetConfig(&data.FieldConfig{Interval: float64(query.Interval.Milliseconds())})
+
 				if valType == "string" {
 					valueField := data.NewField("value", row.Tags, stringArray)
 					valueField.SetConfig(&data.FieldConfig{DisplayNameFromDS: name})
