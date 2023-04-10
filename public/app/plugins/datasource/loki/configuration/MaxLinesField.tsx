e226db618d9bf9cccd0d5d906250e252233c320a
Matias Chomicki <matyax@gmail.com>
2023-04-10 16:40:56 +0200

    Loki config: add missing section title to max lines

--- public/app/plugins/datasource/loki/configuration/MaxLinesField.tsx
+++ public/app/plugins/datasource/loki/configuration/MaxLinesField.tsx
@@ -11,27 +11,37 @@ type Props = {
 export const MaxLinesField = (props: Props) => {
   const { value, onChange } = props;
   return (
-    <FormField
-      label="Maximum lines"
-      labelWidth={11}
-      inputWidth={20}
-      inputEl={
-        <input
-          type="number"
-          className="gf-form-input width-8 gf-form-input--has-help-icon"
-          value={value}
-          onChange={(event) => onChange(event.currentTarget.value)}
-          spellCheck={false}
-          placeholder="1000"
-        />
-      }
-      tooltip={
-        <>
-          Loki queries must contain a limit of the maximum number of lines returned (default: 1000). Increase this limit
-          to have a bigger result set for ad-hoc analysis. Decrease this limit if your browser becomes sluggish when
-          displaying the log results.
-        </>
-      }
-    />
+    <>
+      <h3 className="page-heading">Queries</h3>
+
+      <div className="gf-form-group">
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <FormField
+              label="Maximum lines"
+              labelWidth={11}
+              inputWidth={20}
+              inputEl={
+                <input
+                  type="number"
+                  className="gf-form-input width-8 gf-form-input--has-help-icon"
+                  value={value}
+                  onChange={(event) => onChange(event.currentTarget.value)}
+                  spellCheck={false}
+                  placeholder="1000"
+                />
+              }
+              tooltip={
+                <>
+                  Loki queries must contain a limit of the maximum number of lines returned (default: 1000). Increase
+                  this limit to have a bigger result set for ad-hoc analysis. Decrease this limit if your browser
+                  becomes sluggish when displaying the log results.
+                </>
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
