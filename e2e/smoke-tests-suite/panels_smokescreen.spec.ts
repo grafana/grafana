3b2bf287ba1d465f670790d3709c545bc2a4a607
nmarrs <nathanielmarrs@gmail.com>
2023-04-10 14:25:51 -0700

    Update e2e test with less than elegant solution and replicate crash

--- e2e/smoke-tests-suite/panels_smokescreen.spec.ts
+++ e2e/smoke-tests-suite/panels_smokescreen.spec.ts
@@ -7,8 +7,35 @@ e2e.scenario({
   addScenarioDashBoard: false,
   skipScenario: false,
   scenario: () => {
-    e2e.flows.addDashboard({
-      title: 'e2e-panels-smokescreen',
-    });
+    e2e.flows.addDashboard();
+
+    // TODO: Try and use e2e.flows.addPanel() instead of block below
+    try {
+      e2e.components.PageToolbar.itemButton('Add panel button').should('be.visible');
+      e2e.components.PageToolbar.itemButton('Add panel button').click();
+    } catch (e) {
+      // Depending on the screen size, the "Add panel" button might be hidden
+      e2e.components.PageToolbar.item('Show more items').click();
+      e2e.components.PageToolbar.item('Add panel button').last().click();
+    }
+    e2e.pages.AddDashboard.itemButton('Add new visualization menu item').should('be.visible');
+    e2e.pages.AddDashboard.itemButton('Add new visualization menu item').click();
+
+    // Loop through every panel type and ensure no crash
+    e2e.components.PanelEditor.toggleVizPicker().click();
+    e2e.components.PluginVisualization.item('Table').scrollIntoView().should('be.visible').click();
+    e2e.components.PanelEditor.toggleVizPicker().should((e) => expect(e).to.contain('Table'));
+
+    e2e.components.PanelEditor.toggleVizPicker().click();
+    e2e.components.PluginVisualization.item('Stat').scrollIntoView().should('be.visible').click();
+    e2e.components.PanelEditor.toggleVizPicker().should((e) => expect(e).to.contain('Stat'));
+
+    e2e.components.PanelEditor.toggleVizPicker().click();
+    e2e.components.PluginVisualization.item('Gauge').scrollIntoView().should('be.visible').click();
+    e2e.components.PanelEditor.toggleVizPicker().should((e) => expect(e).to.contain('Gauge'));
+
+    e2e.components.PanelEditor.toggleVizPicker().click();
+    e2e.components.PluginVisualization.item('Geomap').scrollIntoView().should('be.visible').click();
+    e2e.components.PanelEditor.toggleVizPicker().should((e) => expect(e).to.contain('Geomap'));
   },
 });
