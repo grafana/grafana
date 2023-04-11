e0030af2049af36352dc3b22c2985df299edb925
Armand Grillet <2117580+armandgrillet@users.noreply.github.com>
2023-04-11 14:03:09 +0200

    Update legacy alerting warning

--- public/app/features/alerting/components/DeprecationNotice.tsx
+++ public/app/features/alerting/components/DeprecationNotice.tsx
@@ -5,23 +5,19 @@ import { Alert } from '@grafana/ui';
 export const LOCAL_STORAGE_KEY = 'grafana.legacyalerting.unifiedalertingpromo';
 
 const DeprecationNotice = () => (
-  <Alert severity="warning" title="Grafana legacy alerting is going away soon">
+  <Alert severity="warning" title="Grafana legacy alerting is deprecated and will be deleted soon">
     <p>
-      You are using Grafana legacy alerting, it has been deprecated and will be removed in the next major version of
-      Grafana.
+      You are using Grafana legacy alerting which is deprecated since Grafana 9.0. Legacy alerting will be removed from
+      Grafana in an upcoming release without further notice.
       <br />
-      We encourage you to upgrade to the new Grafana Alerting experience.
+      We recommend you to upgrade to Grafana Alerting as soon as possible.
     </p>
     <p>
       See{' '}
-      <a href="https://grafana.com/docs/grafana/latest/alerting/unified-alerting/difference-old-new/">
-        What’s New with Grafana Alerting
+      <a href="https://grafana.com/docs/grafana/latest/alerting/migrating-alerts/">
+        how to upgrade to Grafana Alerting
       </a>{' '}
-      to learn more about what&lsquo;s new or learn{' '}
-      <a href="https://grafana.com/docs/grafana/latest/alerting/unified-alerting/opt-in/">
-        how to enable the new Grafana Alerting feature
-      </a>
-      .
+      to learn more.
     </p>
   </Alert>
 );
