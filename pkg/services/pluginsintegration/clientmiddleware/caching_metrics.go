7bd34af64d6e34e6a1e0399fdbca612283334c49
Michael Mandrus <michael.mandrus@grafana.com>
2023-04-11 20:20:58 -0400

    fix a panic

--- pkg/services/pluginsintegration/clientmiddleware/caching_metrics.go
+++ pkg/services/pluginsintegration/clientmiddleware/caching_metrics.go
@@ -25,7 +25,7 @@ var ResourceCachingRequestHistogram = prometheus.NewHistogramVec(prometheus.Hist
 	Name:      "resource_caching_request_duration_seconds",
 	Help:      "histogram of grafana resource endpoint requests in seconds",
 	Buckets:   []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10, 25, 50, 100},
-}, []string{"datasource_type", "cache"})
+}, []string{"plugin_id", "cache"})
 
 func getQueryType(req *contextmodel.ReqContext) string {
 	if req.IsPublicDashboardView {
