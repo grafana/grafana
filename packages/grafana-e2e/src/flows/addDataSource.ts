7de3ee2041f87e38bc14a3cb552d8f1505a2c5ed
Andreas Christou <andreas.christou@grafana.com>
2023-04-12 13:13:33 +0100

    Make health check optional
    (cherry picked from commit 91f66d61e1a5bbc97d79e91f1e7c012dd0561945)

--- packages/grafana-e2e/src/flows/addDataSource.ts
+++ packages/grafana-e2e/src/flows/addDataSource.ts
@@ -14,6 +14,7 @@ export interface AddDataSourceConfig {
   skipTlsVerify: boolean;
   type: string;
   timeout?: number;
+  awaitHealth?: boolean;
 }
 
 // @todo this actually returns type `Cypress.Chainable<AddDaaSourceConfig>`
@@ -40,11 +41,14 @@ export const addDataSource = (config?: Partial<AddDataSourceConfig>) => {
     skipTlsVerify,
     type,
     timeout,
+    awaitHealth,
   } = fullConfig;
 
-  e2e()
-    .intercept(/health/)
-    .as('health');
+  if (awaitHealth) {
+    e2e()
+      .intercept(/health/)
+      .as('health');
+  }
 
   e2e().logToConsole('Adding data source with name:', name);
   e2e.pages.AddDataSource.visit();
@@ -80,7 +84,9 @@ export const addDataSource = (config?: Partial<AddDataSourceConfig>) => {
 
   e2e.pages.DataSource.saveAndTest().click();
 
-  e2e().wait('@health', { timeout: timeout ?? e2e.config().defaultCommandTimeout });
+  if (awaitHealth) {
+    e2e().wait('@health', { timeout: timeout ?? e2e.config().defaultCommandTimeout });
+  }
 
   // use the timeout passed in if it exists, otherwise, continue to use the default
   e2e.pages.DataSource.alert()
