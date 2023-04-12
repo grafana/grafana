bcd0060e93aefee40402b6f12167fd44d9e89ffd
Sven Grossmann <svennergr@gmail.com>
2023-04-12 18:20:03 +0200

    Logs: Add new LogRowContext types to grafana/data

--- public/app/plugins/datasource/loki/datasource.ts
+++ public/app/plugins/datasource/loki/datasource.ts
@@ -31,6 +31,8 @@ import {
   rangeUtil,
   ScopedVars,
   TimeRange,
+  LogRowContextOptions,
+  LogRowContextQueryDirection,
 } from '@grafana/data';
 import { BackendSrvRequest, config, DataSourceWithBackend, FetchError } from '@grafana/runtime';
 import { DataQuery } from '@grafana/schema';
@@ -40,7 +42,6 @@ import { getTimeSrv, TimeSrv } from 'app/features/dashboard/services/TimeSrv';
 import { getTemplateSrv, TemplateSrv } from 'app/features/templating/template_srv';
 
 import { serializeParams } from '../../../core/utils/fetch';
-import { RowContextOptions } from '../../../features/logs/components/log-context/types';
 import { getLogLevelFromKey } from '../../../features/logs/utils';
 import { renderLegendFormat } from '../prometheus/legend';
 import { replaceVariables, returnVariables } from '../prometheus/querybuilder/shared/parsingUtils';
@@ -646,10 +647,10 @@ export class LokiDatasource
 
   getLogRowContext = async (
     row: LogRowModel,
-    options?: RowContextOptions,
+    options?: LogRowContextOptions,
     origQuery?: DataQuery
   ): Promise<{ data: DataFrame[] }> => {
-    const direction = (options && options.direction) || 'BACKWARD';
+    const direction = (options && options.direction) || LogRowContextQueryDirection.Backward;
     const limit = (options && options.limit) || 10;
     const { query, range } = await this.logContextProvider.prepareLogRowContextQueryTarget(
       row,
