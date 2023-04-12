d875b25049b2f58f5ad382824cf68c375582aff7
Matias Chomicki <matyax@gmail.com>
2023-04-12 15:39:43 +0200

    Loki search words: process negative expressions as negative search words

--- public/app/plugins/datasource/loki/queryUtils.ts
+++ public/app/plugins/datasource/loki/queryUtils.ts
@@ -17,6 +17,8 @@ import {
   MetricExpr,
   Matcher,
   Identifier,
+  Nre,
+  Neq,
 } from '@grafana/lezer-logql';
 import { DataQuery } from '@grafana/schema';
 
@@ -31,7 +33,7 @@ export function formatQuery(selector: string | undefined): string {
 
 /**
  * Returns search terms from a LogQL query.
- * E.g., `{} |= foo |=bar != baz` returns `['foo', 'bar']`.
+ * E.g.: `{} |= foo |=bar != baz` returns `['foo', 'bar', '-baz']`.
  */
 export function getHighlighterExpressionsFromQuery(input: string): string[] {
   const results = [];
@@ -49,9 +51,10 @@ export function getHighlighterExpressionsFromQuery(input: string): string[] {
   for (let filter of filters) {
     const pipeExact = filter.getChild(Filter)?.getChild(PipeExact);
     const pipeMatch = filter.getChild(Filter)?.getChild(PipeMatch);
+    const negativeExp = filter.getChild(Filter)?.getChild(Neq) || filter.getChild(Filter)?.getChild(Nre);
     const string = filter.getChild(String);
 
-    if ((!pipeExact && !pipeMatch) || !string) {
+    if ((!pipeExact && !pipeMatch && !negativeExp) || !string) {
       continue;
     }
 
@@ -76,7 +79,7 @@ export function getHighlighterExpressionsFromQuery(input: string): string[] {
     }
 
     if (resultTerm) {
-      results.push(resultTerm);
+      results.push(negativeExp ? `-${resultTerm}` : resultTerm);
     }
   }
   return results;
