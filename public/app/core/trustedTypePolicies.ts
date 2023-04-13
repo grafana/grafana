bf7f66359254673ef441a153ba4c7eb720bbcbc6
Kristian Bremberg <kristian.bremberg@grafana.com>
2023-04-13 14:18:42 +0200

    Encode <

--- public/app/core/trustedTypePolicies.ts
+++ public/app/core/trustedTypePolicies.ts
@@ -4,7 +4,7 @@ import { config } from '@grafana/runtime';
 if (config.bootData.settings.featureToggles.trustedTypes && window.trustedTypes && window.trustedTypes.createPolicy) {
   // check if browser supports Trusted Types
   window.trustedTypes.createPolicy('default', {
-    createHTML: (string) => string.replace(/<script/gi, ''),
+    createHTML: (string) => string.replace(/<script/gi, '&lt;script'),
     createScript: (string) => string,
     createScriptURL: (string) => textUtil.sanitizeUrl(string),
   });
