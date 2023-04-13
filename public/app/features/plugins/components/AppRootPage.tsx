31e80c176cd55ef2f69ade3f33d1070802d14293
Esteban Beltran <esteban@academo.me>
2023-04-13 09:28:20 +0200

    Sandbox App plugin pages

--- public/app/features/plugins/components/AppRootPage.tsx
+++ public/app/features/plugins/components/AppRootPage.tsx
@@ -66,13 +66,15 @@ export function AppRootPage({ pluginId, pluginNavSection }: Props) {
   }
 
   const pluginRoot = plugin.root && (
-    <plugin.root
-      meta={plugin.meta}
-      basename={match.url}
-      onNavChanged={onNavChanged}
-      query={queryParams}
-      path={location.pathname}
-    />
+    <div data-plugin-sandbox="true">
+      <plugin.root
+        meta={plugin.meta}
+        basename={match.url}
+        onNavChanged={onNavChanged}
+        query={queryParams}
+        path={location.pathname}
+      />
+    </div>
   );
 
   if (config.featureToggles.topnav && !pluginNav) {
