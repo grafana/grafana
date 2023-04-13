9a2ee3fc489200ef0f483300aaf5043068abb7bb
Ivan Ortega <ivanortegaalba@gmail.com>
2023-04-13 11:23:54 +0200

    Restore aria label because select doesnt forward it

--- public/app/features/datasource-picker/DataSourcePicker.tsx
+++ public/app/features/datasource-picker/DataSourcePicker.tsx
@@ -129,11 +129,11 @@ export class DataSourcePicker extends PureComponent<DataSourcePickerProps, DataS
     const isClearable = typeof onClear === 'function';
 
     return (
-      <div data-testid={selectors.components.DataSourcePicker.container}>
+      <div aria-label={selectors.components.DataSourcePicker.container}>
         <Select
           isLoading={isLoading}
           disabled={disabled}
-          data-testid={selectors.components.DataSourcePicker.inputV2}
+          aria-label={selectors.components.DataSourcePicker.inputV2}
           inputId={inputId || 'data-source-picker'}
           className="ds-picker select-container"
           isMulti={false}
