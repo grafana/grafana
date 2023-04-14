ee166205cde431f3e673a77456933185256e5587
Ashley Harrison <ashley.harrison@grafana.com>
2023-04-14 11:46:18 +0100

    Navigation: move connections in IA (#66559)
    move connections in IA

--- pkg/services/navtree/models.go
+++ pkg/services/navtree/models.go
@@ -17,11 +17,11 @@ const (
 	WeightDashboard
 	WeightExplore
 	WeightAlerting
-	WeightDataConnections
 	WeightPlugin
 	WeightConfig
 	WeightAlertsAndIncidents
 	WeightMonitoring
+	WeightDataConnections
 	WeightApps
 	WeightAdmin
 	WeightProfile
