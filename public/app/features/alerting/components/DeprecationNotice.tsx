da161c4d3d650f6a510a58d70a82bf2efbb4b89c
Armand Grillet <2117580+armandgrillet@users.noreply.github.com>
2023-04-14 16:30:28 +0200

    Clarify alerting deprecation

--- public/app/features/alerting/components/DeprecationNotice.tsx
+++ public/app/features/alerting/components/DeprecationNotice.tsx
@@ -7,8 +7,8 @@ export const LOCAL_STORAGE_KEY = 'grafana.legacyalerting.unifiedalertingpromo';
 const DeprecationNotice = () => (
   <Alert severity="warning" title="Grafana legacy alerting is deprecated and will be removed in a future release.">
     <p>
-      You are using Grafana legacy alerting, which has been deprecated since Grafana 9.0. It will be removed in a future
-      release without further notice.
+      You are using Grafana legacy alerting, which has been deprecated since Grafana 9.0. The codebase is now staying as
+      is and will be removed in Grafana 11.0.
       <br />
       We recommend upgrading to Grafana Alerting as soon as possible.
     </p>
