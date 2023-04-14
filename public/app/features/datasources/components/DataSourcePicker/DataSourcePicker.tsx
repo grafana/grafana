0f33a61a8d33dd5647c89b0439fa6fef766db6a7
Ivan Ortega <ivanortegaalba@gmail.com>
2023-04-14 12:33:19 +0200

    Wrap DS picker under to switch using toggles

---
+++ public/app/features/datasources/components/DataSourcePicker/DataSourcePicker.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  DataSourcePicker as DeprecatedDataSourcePicker,
+  DataSourcePickerProps as DeprecatedDataSourcePickerProps,
+} from '@grafana/runtime';
+import { config } from 'app/core/config';
+import { DataSourcePickerWithHistory } from 'app/features/datasource-drawer/DataSourcePickerWithHistory';
+import { DataSourcePickerWithHistoryProps } from 'app/features/datasource-drawer/types';
+
+type DataSourcePickerProps = DeprecatedDataSourcePickerProps & DataSourcePickerWithHistoryProps;
+
+/**
+ * DataSourcePicker is a wrapper around the old DataSourcePicker and the new one.
+ * Depending on the feature toggle, it will render the old or the new one.
+ * Feature toggle: advancedDataSourcePicker
+ */
+export function DataSourcePicker(props: DataSourcePickerProps) {
+  return !config.featureToggles.advancedDataSourcePicker ? (
+    <DeprecatedDataSourcePicker {...props} />
+  ) : (
+    <DataSourcePickerWithHistory {...props} />
+  );
+}
