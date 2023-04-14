1d39813668cfb76de7477adf1df0ba07a81062e9
Leon Sorokin <leeoniya@gmail.com>
2023-04-14 03:31:44 -0500

    re-sync

--- packages/grafana-data/src/transformations/transformers/joinDataFrames.test.ts
+++ packages/grafana-data/src/transformations/transformers/joinDataFrames.test.ts
@@ -1,7 +1,6 @@
 import { toDataFrame } from '../../dataframe/processDataFrame';
 import { FieldType } from '../../types/dataFrame';
 import { mockTransformationsRegistry } from '../../utils/tests/mockTransformationsRegistry';
-import { ArrayVector } from '../../vector';
 
 import { calculateFieldTransformer } from './calculateField';
 import { JoinMode } from './joinByField';
@@ -321,42 +320,40 @@ describe('align frames', () => {
 
   describe('check ascending data', () => {
     it('simple ascending', () => {
-      const v = new ArrayVector([1, 2, 3, 4, 5]);
+      const v = [1, 2, 3, 4, 5];
       expect(isLikelyAscendingVector(v)).toBeTruthy();
     });
     it('simple ascending with null', () => {
-      const v = new ArrayVector([null, 2, 3, 4, null]);
+      const v = [null, 2, 3, 4, null];
       expect(isLikelyAscendingVector(v)).toBeTruthy();
     });
     it('single value', () => {
-      const v = new ArrayVector([null, null, null, 4, null]);
+      const v = [null, null, null, 4, null];
       expect(isLikelyAscendingVector(v)).toBeTruthy();
-      expect(isLikelyAscendingVector(new ArrayVector([4]))).toBeTruthy();
-      expect(isLikelyAscendingVector(new ArrayVector([] as any[]))).toBeTruthy();
+      expect(isLikelyAscendingVector([4])).toBeTruthy();
+      expect(isLikelyAscendingVector([])).toBeTruthy();
     });
 
     it('middle values', () => {
-      const v = new ArrayVector([null, null, 5, 4, null]);
+      const v = [null, null, 5, 4, null];
       expect(isLikelyAscendingVector(v)).toBeFalsy();
     });
 
     it('decending', () => {
-      expect(isLikelyAscendingVector(new ArrayVector([7, 6, null]))).toBeFalsy();
-      expect(isLikelyAscendingVector(new ArrayVector([7, 8, 6]))).toBeFalsy();
+      expect(isLikelyAscendingVector([7, 6, null])).toBeFalsy();
+      expect(isLikelyAscendingVector([7, 8, 6])).toBeFalsy();
     });
 
     it('ascending first/last', () => {
-      expect(isLikelyAscendingVector(new ArrayVector([10, 20, 30, 5, 15, 7, 43, 29, 11]), 3)).toBeFalsy();
-      expect(
-        isLikelyAscendingVector(new ArrayVector([null, 10, 20, 30, 5, null, 15, 7, 43, 29, 11, null]), 3)
-      ).toBeFalsy();
+      expect(isLikelyAscendingVector([10, 20, 30, 5, 15, 7, 43, 29, 11], 3)).toBeFalsy();
+      expect(isLikelyAscendingVector([null, 10, 20, 30, 5, null, 15, 7, 43, 29, 11, null], 3)).toBeFalsy();
     });
 
     it('null stuffs', () => {
-      expect(isLikelyAscendingVector(new ArrayVector([null, null, 1]), 3)).toBeTruthy();
-      expect(isLikelyAscendingVector(new ArrayVector([1, null, null]), 3)).toBeTruthy();
-      expect(isLikelyAscendingVector(new ArrayVector([null, null, null]), 3)).toBeTruthy();
-      expect(isLikelyAscendingVector(new ArrayVector([null, 1, null]), 3)).toBeTruthy();
+      expect(isLikelyAscendingVector([null, null, 1], 3)).toBeTruthy();
+      expect(isLikelyAscendingVector([1, null, null], 3)).toBeTruthy();
+      expect(isLikelyAscendingVector([null, null, null], 3)).toBeTruthy();
+      expect(isLikelyAscendingVector([null, 1, null], 3)).toBeTruthy();
     });
   });
 });
