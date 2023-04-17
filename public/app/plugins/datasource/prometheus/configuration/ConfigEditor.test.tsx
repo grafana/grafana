8591a3d90c06d9b02939aa7acf0d2638d7855d76
bohandley <brendan.ohandley@gmail.com>
2023-04-16 21:43:41 -0400

    add tests for config validations

---
+++ public/app/plugins/datasource/prometheus/configuration/ConfigEditor.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { FieldValidationMessage } from '@grafana/ui';
+
+import { validateInput } from './ConfigEditor';
+import { DURATION_REGEX } from './PromSettings';
+
+// replaces promSettingsValidationEvents to display a <FieldValidationMessage> onBlur for duration input errors
+describe('promSettings validateInput', () => {
+  it.each`
+    value    | expected
+    ${'1ms'} | ${true}
+    ${'1M'}  | ${true}
+    ${'1w'}  | ${true}
+    ${'1d'}  | ${true}
+    ${'1h'}  | ${true}
+    ${'1m'}  | ${true}
+    ${'1s'}  | ${true}
+    ${'1y'}  | ${true}
+  `(
+    "when calling the rule with correct formatted value: '$value' then result should be '$expected'",
+    ({ value, expected }) => {
+      expect(validateInput(value, DURATION_REGEX)).toBe(expected);
+    }
+  );
+
+  const error = <FieldValidationMessage>Value is not valid</FieldValidationMessage>;
+  it.each`
+    value     | expected
+    ${'1 ms'} | ${error}
+    ${'1x'}   | ${error}
+    ${' '}    | ${error}
+    ${'w'}    | ${error}
+    ${'1.0s'} | ${error}
+  `(
+    "when calling the rule with incorrect formatted value: '$value' then result should be '$expected'",
+    ({ value, expected }) => {
+      expect(validateInput(value, DURATION_REGEX)).toStrictEqual(expected);
+    }
+  );
+
+  it('should display a custom validation message', () => {
+    const invalidDuration = 'invalid';
+    const customMessage = 'This is invalid';
+    const errorWithCustomMessage = <FieldValidationMessage>{customMessage}</FieldValidationMessage>;
+    expect(validateInput(invalidDuration, DURATION_REGEX, customMessage)).toStrictEqual(errorWithCustomMessage);
+  });
+});
--- public/app/plugins/datasource/prometheus/configuration/PromSettings.test.tsx
+++ public/app/plugins/datasource/prometheus/configuration/PromSettings.test.tsx
@@ -3,11 +3,10 @@ import React, { SyntheticEvent } from 'react';
 import { Provider } from 'react-redux';
 
 import { SelectableValue } from '@grafana/data';
-import { FieldValidationMessage } from '@grafana/ui';
 
 import { configureStore } from '../../../../store/configureStore';
 
-import { DURATION_REGEX, getValueFromEventItem, PromSettings, validateDurationInput } from './PromSettings';
+import { getValueFromEventItem, PromSettings } from './PromSettings';
 import { createDefaultConfigOptions } from './mocks';
 
 describe('PromSettings', () => {
@@ -35,54 +34,6 @@ describe('PromSettings', () => {
         const result = getValueFromEventItem({ value });
         expect(result).toEqual(value);
       });
-    });
-  });
-
-  // replaces promSettingsValidationEvents to display a <FieldValidationMessage> onBlur for duration input errors
-  describe('promSettings validateDurationInput', () => {
-    it.each`
-      value    | expected
-      ${'1ms'} | ${true}
-      ${'1M'}  | ${true}
-      ${'1w'}  | ${true}
-      ${'1d'}  | ${true}
-      ${'1h'}  | ${true}
-      ${'1m'}  | ${true}
-      ${'1s'}  | ${true}
-      ${'1y'}  | ${true}
-    `(
-      "when calling the rule with correct formatted value: '$value' then result should be '$expected'",
-      ({ value, expected }) => {
-        expect(validateDurationInput(value, DURATION_REGEX)).toBe(expected);
-      }
-    );
-
-    const error = (
-      <FieldValidationMessage>
-        Value is not valid, you can use number with time unit specifier: y, M, w, d, h, m, s
-      </FieldValidationMessage>
-    );
-    it.each`
-      value     | expected
-      ${'1 ms'} | ${error}
-      ${'1x'}   | ${error}
-      ${' '}    | ${error}
-      ${'w'}    | ${error}
-      ${'1.0s'} | ${error}
-    `(
-      "when calling the rule with incorrect formatted value: '$value' then result should be '$expected'",
-      ({ value, expected }) => {
-        expect(validateDurationInput(value, DURATION_REGEX)).toStrictEqual(expected);
-      }
-    );
-
-    it('should display a custom validation message', () => {
-      const invalidDuration = 'invalid';
-      const customMessage = 'This is invalid';
-      const errorWithCustomMessage = <FieldValidationMessage>{customMessage}</FieldValidationMessage>;
-      expect(validateDurationInput(invalidDuration, DURATION_REGEX, customMessage)).toStrictEqual(
-        errorWithCustomMessage
-      );
     });
   });
 
