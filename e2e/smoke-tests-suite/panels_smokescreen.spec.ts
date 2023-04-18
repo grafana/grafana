2b73f7202be07a2092ece429426c12b01a7c50f2
nmarrs <nathanielmarrs@gmail.com>
2023-04-18 11:36:40 -0700

    Remove hardcoded panel list in favor of window.grafanaBootData

--- e2e/smoke-tests-suite/panels_smokescreen.spec.ts
+++ e2e/smoke-tests-suite/panels_smokescreen.spec.ts
@@ -1,32 +1,5 @@
 import { e2e } from '@grafana/e2e';
-
-// TODO: Better way to generate this list (ideally including all panels >=beta state) (list is case sensitive)
-const PANELS_TO_CHECK = [
-  'Time series',
-  'Bar chart',
-  'Stat',
-  'Gauge',
-  'Bar gauge',
-  'Table',
-  'Pie chart',
-  'State timeline',
-  'Heatmap',
-  'Status history',
-  'Histogram',
-  'Text',
-  'Alert list',
-  'Dashboard list',
-  'News',
-  'Annotations list',
-  'Candlestick',
-  'Canvas',
-  'Flame Graph',
-  'Geomap',
-  'Logs',
-  'Node Graph',
-  'Traces',
-  'XY Chart',
-];
+import { GrafanaBootConfig } from '@grafana/runtime';
 
 e2e.scenario({
   describeName: 'Panels smokescreen',
@@ -49,18 +22,24 @@ e2e.scenario({
     e2e.pages.AddDashboard.itemButton('Add new visualization menu item').should('be.visible');
     e2e.pages.AddDashboard.itemButton('Add new visualization menu item').click();
 
-    // Loop through every panel type and ensure no crash
-    PANELS_TO_CHECK.forEach((panel) => {
-      e2e.components.PanelEditor.toggleVizPicker().click();
-      e2e.components.PluginVisualization.item(panel).scrollIntoView().should('be.visible').click();
+    e2e()
+      .window()
+      .then((win: Cypress.AUTWindow & { grafanaBootData: GrafanaBootConfig['bootData'] }) => {
+        // Loop through every panel type and ensure no crash
+        Object.entries(win.grafanaBootData.settings.panels).forEach(([_, panel]) => {
+          // TODO: Remove Flame Graph from exclusion list as part of addressing #66803
+          if (!panel.hideFromList && panel.state !== 'deprecated' && panel.name !== 'Flame Graph') {
+            e2e.components.PanelEditor.toggleVizPicker().click();
+            e2e.components.PluginVisualization.item(panel.name).scrollIntoView().should('be.visible').click();
 
-      // Wait for panel to load (TODO: Better way to do this?)
-      cy.wait(500);
+            // Wait for panel to load (TODO: Better way to do this?)
+            cy.wait(500);
 
-      e2e.components.PanelEditor.toggleVizPicker().should((e) => expect(e).to.contain(panel));
-
-      // TODO: Come up with better check / better failure messaging to clearly indicate which panel failed
-      cy.contains('An unexpected error happened').should('not.exist');
-    });
+            e2e.components.PanelEditor.toggleVizPicker().should((e) => expect(e).to.contain(panel.name));
+            // TODO: Come up with better check / better failure messaging to clearly indicate which panel failed
+            cy.contains('An unexpected error happened').should('not.exist');
+          }
+        });
+      });
   },
 });
