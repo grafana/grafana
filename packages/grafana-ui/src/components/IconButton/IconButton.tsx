c151b6b714648d07bd30e7257ce9c2a162a49fed
Laura Benz <laura.benz@grafana.com>
2023-04-18 15:45:24 +0200

    refactor: remove unnecessary styling and adjust to button styling

--- packages/grafana-ui/src/components/IconButton/IconButton.tsx
+++ packages/grafana-ui/src/components/IconButton/IconButton.tsx
@@ -70,8 +70,6 @@ export const IconButton = React.forwardRef<HTMLButtonElement, Props>(
 IconButton.displayName = 'IconButton';
 
 const getStyles = stylesFactory((theme: GrafanaTheme2, size: IconSize, variant: IconButtonVariant) => {
-  const pixelSize = getSvgSize(size);
-  const hoverSize = Math.max(pixelSize / 3, 8);
   let iconColor = theme.colors.text.primary;
 
   if (variant === 'primary') {
@@ -82,76 +80,43 @@ const getStyles = stylesFactory((theme: GrafanaTheme2, size: IconSize, variant: 
 
   return {
     button: css`
-      width: ${pixelSize}px;
-      height: ${pixelSize}px;
-      background: transparent;
-      border: none;
-      color: ${iconColor};
-      padding: 0;
-      margin: 0;
-      outline: none;
-      box-shadow: none;
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      position: relative;
-      border-radius: ${theme.shape.borderRadius()};
       z-index: 0;
-      margin-right: ${theme.spacing(0.5)};
-
-      &[disabled],
-      &:disabled {
-        cursor: not-allowed;
-        color: ${theme.colors.action.disabledText};
-        opacity: 0.65;
-        box-shadow: none;
-      }
+      position: relative;
+      margin: 0 ${theme.spacing(0.5)} 0 0;
+      box-shadow: none;
+      outline: none;
+      border: none;
+      display: inline-flex;
+      background: transparent;
+      justify-content: center;
+      align-items: center;
+      padding: 0;
+      color: ${iconColor};
 
       &:before {
-        content: '';
-        display: block;
-        opacity: 1;
+        z-index: -1;
         position: absolute;
+        box-sizing: border-box;
+        height: 105%;
+        width: 105%;
+        border-radius: ${theme.shape.radius.default};
+        content: '';
+        background: none;
         transition-duration: 0.2s;
         transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-        z-index: -1;
-        bottom: -${hoverSize}px;
-        left: -${hoverSize}px;
-        right: -${hoverSize}px;
-        top: -${hoverSize}px;
-        background: none;
-        border-radius: 50%;
-        box-sizing: border-box;
-        transform: scale(0);
         transition-property: transform, opacity;
       }
 
-      &:focus,
-      &:focus-visible {
-        ${getFocusStyles(theme)}
-      }
-
-      &:focus:not(:focus-visible) {
-        ${getMouseFocusStyles(theme)}
-      }
-
       &:hover {
-        color: ${iconColor};
-
         &:before {
           background-color: ${variant === 'secondary'
             ? theme.colors.action.hover
             : colorManipulator.alpha(iconColor, 0.12)};
-          border: none;
-          box-shadow: none;
-          opacity: 1;
-          transform: scale(0.8);
         }
       }
     `,
     icon: css`
       vertical-align: baseline;
-      display: flex;
     `,
   };
 });
