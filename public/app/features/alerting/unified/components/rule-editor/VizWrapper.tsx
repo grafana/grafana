1a449b89800ff4fd6405a5f09909f91e1542bee4
Gilles De Mey <gilles.de.mey@gmail.com>
2023-04-18 10:34:43 +0200

    update styles

--- public/app/features/alerting/unified/components/rule-editor/VizWrapper.tsx
+++ public/app/features/alerting/unified/components/rule-editor/VizWrapper.tsx
@@ -1,15 +1,23 @@
 import { css } from '@emotion/css';
-import appEvents from 'app/core/app_events';
 import React from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
-import { AbsoluteTimeRange, DataFrame, GrafanaTheme2, isTimeSeriesFrames, PanelData, ThresholdsConfig } from '@grafana/data';
+import {
+  AbsoluteTimeRange,
+  DataFrame,
+  GrafanaTheme2,
+  isTimeSeriesFrames,
+  PanelData,
+  ThresholdsConfig,
+} from '@grafana/data';
 import { GraphTresholdsStyleMode, LoadingState } from '@grafana/schema';
 import { useStyles2 } from '@grafana/ui';
-
+import appEvents from 'app/core/app_events';
 import { GraphContainer } from 'app/features/explore/Graph/GraphContainer';
 import { RawPrometheusContainer } from 'app/features/explore/RawPrometheusContainer';
 import { ExploreId } from 'app/types/explore';
-import { PrepareTimeSeriesOptions, prepareTimeSeriesTransformer, timeSeriesFormat } from 'app/features/transformers/prepareTimeSeries/prepareTimeSeries';
+import { toTimeSeriesMulti } from 'app/features/transformers/prepareTimeSeries/prepareTimeSeries';
+
 
 interface Props {
   data: PanelData;
@@ -19,59 +27,68 @@ interface Props {
 }
 
 /** The VizWrapper is just a simple component that renders either a table or a graph based on the type of data we receive from "PanelData" */
-export const VizWrapper = ({ data, thresholds, thresholdsType = GraphTresholdsStyleMode.Line }: Props) => {
+export const VizWrapper = ({ data, thresholds, thresholdsType }: Props) => {
   const styles = useStyles2(getStyles);
-
-  console.log(data)
-  const isTimeSeriesData = isTimeSeriesFrames(data.series)
+  const isTimeSeriesData = isTimeSeriesFrames(data.series);
+  const thresholdsStyle = thresholdsType ? { mode: thresholdsType } : undefined;
 
   return (
     <div className={styles.wrapper}>
-      {isTimeSeriesData ? (
-        <GraphContainer
-          loading={data.state === LoadingState.Loading}
-          data={data.series}
-          eventBus={appEvents}
-          height={200}
-          width={800}
-          absoluteRange={data.timeRange as unknown as AbsoluteTimeRange}
-          timeZone="browser"
-          onChangeTime={() => { }}
-          splitOpenFn={() => { }}
-          loadingState={data.state}
-          thresholdsConfig={thresholds}
-        />
-      ) : (
-        <RawPrometheusContainer
-          showRawPrometheus={false} // TODO set to true
-          exploreId={ExploreId.left}
-          loading={data.state === LoadingState.Loading}
-          tableResult={prepareTimeSeries(data.series)}
-          width={800}
-          range={data.timeRange}
-          timeZone="browser"
-          splitOpenFn={() => { }}
-        />
-      )}
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <div style={{ width }}>
+            {isTimeSeriesData ? (
+              <GraphContainer
+                loading={data.state === LoadingState.Loading}
+                data={data.series}
+                eventBus={appEvents}
+                height={300}
+                width={width}
+                absoluteRange={data.timeRange as unknown as AbsoluteTimeRange}
+                timeZone="browser"
+                onChangeTime={() => { }}
+                splitOpenFn={() => { }}
+                loadingState={data.state}
+                thresholdsConfig={thresholds}
+                thresholdsStyle={thresholdsStyle}
+              />
+            ) : (
+              <RawPrometheusContainer
+                showRawPrometheus={false} // TODO set to true
+                exploreId={ExploreId.left}
+                loading={data.state === LoadingState.Loading}
+                tableResult={prepareTimeSeries(data.series)}
+                width={width}
+                range={data.timeRange}
+                timeZone="browser"
+                splitOpenFn={() => { }}
+              />
+            )}
+          </div>
+        )}
+      </AutoSizer>
     </div>
   );
 };
 
-/**
- The eval endpoint returns a dataframe for each "alert instance" that would be generated
- This is not very friendly to show in a table so we're converting multiple data frames to
- a single data frame with multiple fields
+/*
+  The eval endpoint returns a dataframe for each "alert instance" that would be generated
+  This is not very friendly to show in a table so we're converting multiple data frames to
+  a single data frame with multiple fields
 
     Fields:
 
     Time | <Label> | <Label> | Value
     []   | []      | []      | []
-
 */
 function prepareTimeSeries(series: DataFrame[]): DataFrame[] {
-  return series
+  console.dir(series)
+  return toTimeSeriesMulti(series)
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  wrapper: css``,
+  wrapper: css`
+    width: 100%;
+    position: relative;
+  `,
 });
