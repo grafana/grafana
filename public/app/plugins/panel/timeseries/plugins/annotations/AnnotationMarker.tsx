4c6f3350d8456c0ed70ddf302c5988bb663d5201
Adela Almasan <adela.almasan@grafana.com>
2023-04-19 11:10:11 -0500

    fix typo

--- public/app/plugins/panel/timeseries/plugins/annotations/AnnotationMarker.tsx
+++ public/app/plugins/panel/timeseries/plugins/annotations/AnnotationMarker.tsx
@@ -128,7 +128,7 @@ export function AnnotationMarker({ annotation, timeZone, width }: Props) {
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
         className={!isRegionAnnotation ? styles.markerWrapper : undefined}
-        data-testId={selectors.pages.SoloPanel.Annotations.marker}
+        data-testid={selectors.pages.SoloPanel.Annotations.marker}
       >
         {marker}
       </div>
