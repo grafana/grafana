c6d675d0e9199803b7f5db8a0ac9845989db5770
Sven Grossmann <svennergr@gmail.com>
2023-04-19 09:16:58 +0200

    add provider to tests

--- public/app/features/logs/components/log-context/LogRowContextModal.test.tsx
+++ public/app/features/logs/components/log-context/LogRowContextModal.test.tsx
@@ -2,6 +2,9 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
+
+import { configureStore } from 'app/store/configureStore';
 
 import { createLogRow } from '../__mocks__/logRow';
 
@@ -25,8 +28,17 @@ describe('LogRowContextModal', () => {
   });
 
   it('should not render modal when it is closed', () => {
+    const store = configureStore();
     render(
-      <LogRowContextModal row={row} open={false} onClose={() => {}} getRowContext={getRowContext} timeZone={timeZone} />
+      <Provider store={store}>
+        <LogRowContextModal
+          row={row}
+          open={false}
+          onClose={() => {}}
+          getRowContext={getRowContext}
+          timeZone={timeZone}
+        />
+      </Provider>
     );
 
     expect(screen.queryByText('Log context')).not.toBeInTheDocument();
@@ -34,14 +46,17 @@ describe('LogRowContextModal', () => {
 
   it('should render modal when it is open', async () => {
     act(() => {
+      const store = configureStore();
       render(
-        <LogRowContextModal
-          row={row}
-          open={true}
-          onClose={() => {}}
-          getRowContext={getRowContext}
-          timeZone={timeZone}
-        />
+        <Provider store={store}>
+          <LogRowContextModal
+            row={row}
+            open={true}
+            onClose={() => {}}
+            getRowContext={getRowContext}
+            timeZone={timeZone}
+          />
+        </Provider>
       );
     });
 
@@ -49,22 +64,34 @@ describe('LogRowContextModal', () => {
   });
 
   it('should call getRowContext on open and change of row', () => {
+    const store = configureStore();
     render(
-      <LogRowContextModal row={row} open={false} onClose={() => {}} getRowContext={getRowContext} timeZone={timeZone} />
+      <Provider store={store}>
+        <LogRowContextModal
+          row={row}
+          open={false}
+          onClose={() => {}}
+          getRowContext={getRowContext}
+          timeZone={timeZone}
+        />
+      </Provider>
     );
 
     expect(getRowContext).not.toHaveBeenCalled();
   });
   it('should call getRowContext on open', async () => {
     act(() => {
+      const store = configureStore();
       render(
-        <LogRowContextModal
-          row={row}
-          open={true}
-          onClose={() => {}}
-          getRowContext={getRowContext}
-          timeZone={timeZone}
-        />
+        <Provider store={store}>
+          <LogRowContextModal
+            row={row}
+            open={true}
+            onClose={() => {}}
+            getRowContext={getRowContext}
+            timeZone={timeZone}
+          />
+        </Provider>
       );
     });
     await waitFor(() => expect(getRowContext).toHaveBeenCalledTimes(2));
@@ -72,14 +99,17 @@ describe('LogRowContextModal', () => {
 
   it('should call getRowContext when limit changes', async () => {
     act(() => {
+      const store = configureStore();
       render(
-        <LogRowContextModal
-          row={row}
-          open={true}
-          onClose={() => {}}
-          getRowContext={getRowContext}
-          timeZone={timeZone}
-        />
+        <Provider store={store}>
+          <LogRowContextModal
+            row={row}
+            open={true}
+            onClose={() => {}}
+            getRowContext={getRowContext}
+            timeZone={timeZone}
+          />
+        </Provider>
       );
     });
     await waitFor(() => expect(getRowContext).toHaveBeenCalledTimes(2));
