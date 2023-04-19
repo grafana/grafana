5b4c3d06a7f892c47f713f6b5f7f811bf9d198df
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2023-04-19 17:55:51 +0200

    fix upgrade of prometheus dep

--- pkg/services/ngalert/sender/notifier.go
+++ pkg/services/ngalert/sender/notifier.go
@@ -41,8 +41,8 @@ import (
 
 	"github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/discovery/targetgroup"
-	"github.com/prometheus/prometheus/pkg/labels"
-	"github.com/prometheus/prometheus/pkg/relabel"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/relabel"
 )
 
 const (
