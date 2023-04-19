6c789f0f91803ac4af7d87f11e3bf9a32aadccb2
eledobleefe <laura.fernandez@grafana.com>
2023-04-19 11:50:44 +0200

    Improve Text story

--- packages/grafana-ui/src/components/Text/Text.story.tsx
+++ packages/grafana-ui/src/components/Text/Text.story.tsx
@@ -5,16 +5,34 @@ import { StoryExample } from '../../utils/storybook/StoryExample';
 import { VerticalGroup } from '../Layout/Layout';
 
 import { Text } from './Text';
+import mdx from './Text.mdx';
 
 const meta: Meta = {
-  title: 'General/Typography',
+  title: 'General/Text',
   component: Text,
   parameters: {
-    docs: {},
+    docs: {
+      page: mdx,
+    },
+  },
+  argTypes: {
+    as: { control: 'select', options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span', 'p', 'legend'] },
+    variant: { control: 'select', options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span', 'p', 'legend'] },
+    weight: {
+      control: 'select',
+      options: ['fontWeightBold', 'fontWeightMedium', 'fontWeightLight', 'fontWeightRegular'],
+    },
+    color: {
+      control: 'select',
+      options: ['color.error.text', 'color.success.text', 'color.warning.text', 'color.primary.text'],
+    },
+    truncate: { control: 'boolean' },
+    textAlign: { control: 'select', options: ['inherit', 'initial', 'left', 'right', 'center', 'justify'] },
+    margin: { control: 'text' },
   },
 };
 
-export const Typopgraphy: Story = () => {
+export const Example: Story = () => {
   return (
     <VerticalGroup>
       <StoryExample name="Native header elements (global styles)">
@@ -27,6 +45,24 @@ export const Typopgraphy: Story = () => {
       </StoryExample>
     </VerticalGroup>
   );
+};
+
+export const Basic: Story = (args) => {
+  return (
+    <Text as={args.as} variant={args.variant} weight={args.weight} textAlignment={args.textAlignment} {...args}>
+      Hola caracola
+    </Text>
+  );
+};
+
+Basic.args = {
+  variant: undefined,
+  as: 'h1',
+  weight: 'fontWeightBold',
+  textAlign: 'center',
+  truncate: false,
+  color: 'color.error.text',
+  children: 'Hola caracola',
 };
 
 export default meta;
