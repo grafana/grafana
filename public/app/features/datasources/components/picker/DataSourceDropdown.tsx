280286cc2bc7bb395f97b0321648108c5a4969b3
Ivan Ortega <ivanortegaalba@gmail.com>
2023-04-20 21:48:22 +0200

    Adjust styles for dropdown and modal

--- public/app/features/datasources/components/picker/DataSourceDropdown.tsx
+++ public/app/features/datasources/components/picker/DataSourceDropdown.tsx
@@ -199,7 +199,7 @@ function getStylesPickerContent(theme: GrafanaTheme2) {
     container: css`
       display: flex;
       flex-direction: column;
-      height: 480px;
+      height: 412px;
       box-shadow: ${theme.shadows.z3};
       width: 480px;
       background: ${theme.colors.background.primary};
@@ -209,15 +209,16 @@ function getStylesPickerContent(theme: GrafanaTheme2) {
       background: ${theme.colors.background.secondary};
     `,
     dataSourceList: css`
-      height: 423px;
-      padding: 0 ${theme.spacing(2)};
+      flex: 1;
+      height: 100%;
     `,
     footer: css`
+      flex: 0;
       display: flex;
       justify-content: space-between;
-      padding: ${theme.spacing(2)};
+      padding: ${theme.spacing(1.5)};
       border-top: 1px solid ${theme.colors.border.weak};
-      height: 57px;
+      background-color: ${theme.colors.background.secondary};
     `,
   };
 }
