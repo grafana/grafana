16fcc5850f1fbd96a68030e8a58093d8dd31d57c
jev forsberg <jev.forsberg@grafana.com>
2023-04-20 11:18:35 -0600

    baldm0mma/sql_datasource_update/ update tests to reflect enabled feature flag

--- public/app/features/plugins/sql/components/SqlComponents.test.tsx
+++ public/app/features/plugins/sql/components/SqlComponents.test.tsx
@@ -7,85 +7,73 @@ import { DatasetSelector } from './DatasetSelector';
 import { SqlQueryEditor } from './QueryEditor';
 import { buildSqlQueryEditorProps, buildMockDataSelectorProps, buildMockDatasource } from './SqlComponents.testHelpers';
 
-// Needed a way to test, but only under the conditions of a truthy feature flag.
-const sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled = !!config.featureToggles.sqlDatasourceDatabaseSelection;
-console.log(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled, 'featureFlag');
-const runTestIf = (condition: boolean) => (condition ? it : it.skip);
+beforeEach(() => {
+  config.featureToggles.sqlDatasourceDatabaseSelection = true;
+});
+
+afterEach(() => {
+  config.featureToggles.sqlDatasourceDatabaseSelection = false;
+});
 
 describe('SqlQueryEditor', () => {
   describe('alerts', () => {
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      'should ONLY render the `database update` alert',
-      async () => {
-        // @ts-ignore
-        // Property 'templateSrv' is a protected type and is not a class derived from 'SqlDatasource'.ts(2322); Oh hush now.
-        render(<SqlQueryEditor {...buildSqlQueryEditorProps({ datasource: buildMockDatasource(true) })} />);
+    it('should ONLY render the `database update` alert', async () => {
+      // @ts-ignore
+      // Property 'templateSrv' is a protected type and is not a class derived from 'SqlDatasource'.ts(2322); Oh hush now.
+      render(<SqlQueryEditor {...buildSqlQueryEditorProps({ datasource: buildMockDatasource(true) })} />);
 
-        await waitFor(() => {
-          const visableAlert = screen.getByRole('alert', { name: 'Default datasource update' });
-          expect(visableAlert).toBeInTheDocument();
+      await waitFor(() => {
+        const visableAlert = screen.getByRole('alert', { name: 'Default datasource update' });
+        expect(visableAlert).toBeInTheDocument();
 
-          // Use `queryBy<queryType>` syntax when attempting to find an element that will not exist on the virtual DOM.
-          // This will return `null` instead of throwing an error.
-          const invisableAlert = screen.queryByRole('alert', { name: 'Default datasource error' });
-          expect(invisableAlert).not.toBeInTheDocument();
-        });
-      }
-    );
+        // Use `queryBy<queryType>` syntax when attempting to find an element that will not exist on the virtual DOM.
+        // This will return `null` instead of throwing an error.
+        const invisableAlert = screen.queryByRole('alert', { name: 'Default datasource error' });
+        expect(invisableAlert).not.toBeInTheDocument();
+      });
+    });
 
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      'should ONLY render the `no postgres database` alert',
-      async () => {
-        render(<SqlQueryEditor {...buildSqlQueryEditorProps({ queryHeaderProps: { isPostgresInstance: true } })} />);
+    it('should ONLY render the `no postgres database` alert', async () => {
+      render(<SqlQueryEditor {...buildSqlQueryEditorProps({ queryHeaderProps: { isPostgresInstance: true } })} />);
 
-        await waitFor(() => {
-          const alert = screen.getByRole('alert', { name: 'Default datasource error' });
-          expect(alert).toBeInTheDocument();
+      await waitFor(() => {
+        const alert = screen.getByRole('alert', { name: 'Default datasource error' });
+        expect(alert).toBeInTheDocument();
 
-          const invisableAlert = screen.queryByRole('alert', { name: 'Default datasource update' });
-          expect(invisableAlert).toBeNull();
-        });
-      }
-    );
+        const invisableAlert = screen.queryByRole('alert', { name: 'Default datasource update' });
+        expect(invisableAlert).toBeNull();
+      });
+    });
   });
 });
 
 describe('DatasetSelector', () => {
   describe('should render with the correct default placeholder values', () => {
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      `should render with 'Select table' since no current dataset is chosen, no dataset has been preconfigured,
-        and the selector has not been disabled via 'isPostgresInstance'`,
-      async () => {
-        render(<DatasetSelector {...buildMockDataSelectorProps()} />);
+    it(`should render with 'Select table' since no current dataset is chosen, no dataset has been preconfigured,
+        and the selector has not been disabled via 'isPostgresInstance'`, async () => {
+      render(<DatasetSelector {...buildMockDataSelectorProps()} />);
 
-        await waitFor(() => {
-          expect(screen.getByText(/Select/i).textContent).toBe('Select table');
-        });
-      }
-    );
+      await waitFor(() => {
+        expect(screen.getByText(/Select/i).textContent).toBe('Select table');
+      });
+    });
 
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      `should render with 'Unconfigured database' since a 'isPostgresInstance' is truthy,
-        which means that the datasource is Postgres, and does not have a default configured database`,
-      async () => {
-        render(<DatasetSelector {...buildMockDataSelectorProps({ isPostgresInstance: true })} />);
+    it(`should render with 'Unconfigured database' since a 'isPostgresInstance' is truthy,
+        which means that the datasource is Postgres, and does not have a default configured database`, async () => {
+      render(<DatasetSelector {...buildMockDataSelectorProps({ isPostgresInstance: true })} />);
 
-        await waitFor(() => {
-          expect(screen.getByText(/unconfig/i).textContent).toBe('Unconfigured database');
-        });
-      }
-    );
+      await waitFor(() => {
+        expect(screen.getByText(/unconfig/i).textContent).toBe('Unconfigured database');
+      });
+    });
 
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      'should render with `database 1` since a preconfigured database exists',
-      async () => {
-        render(<DatasetSelector {...buildMockDataSelectorProps({ preconfiguredDataset: 'database 1' })} />);
+    it('should render with `database 1` since a preconfigured database exists', async () => {
+      render(<DatasetSelector {...buildMockDataSelectorProps({ preconfiguredDataset: 'database 1' })} />);
 
-        await waitFor(() => {
-          expect(screen.getByText(/database/i).textContent).toBe('database 1');
-        });
-      }
-    );
+      await waitFor(() => {
+        expect(screen.getByText(/database/i).textContent).toBe('database 1');
+      });
+    });
   });
 
   describe('should disable the database selector appropriately', () => {
@@ -122,28 +110,22 @@ describe('DatasetSelector', () => {
       });
     });
 
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      'should not query the database if disabled',
-      async () => {
-        const mockProps = buildMockDataSelectorProps({ isPostgresInstance: true });
-        render(<DatasetSelector {...mockProps} />);
+    it('should not query the database if disabled', async () => {
+      const mockProps = buildMockDataSelectorProps({ isPostgresInstance: true });
+      render(<DatasetSelector {...mockProps} />);
 
-        await waitFor(() => {
-          expect(mockProps.db.datasets).not.toHaveBeenCalled();
-        });
-      }
-    );
+      await waitFor(() => {
+        expect(mockProps.db.datasets).not.toHaveBeenCalled();
+      });
+    });
 
-    runTestIf(sqlDatasourceDatabaseSelectionFeatureFlagIsEnabled)(
-      'should not query the database if preconfigured',
-      async () => {
-        const mockProps = buildMockDataSelectorProps({ preconfiguredDataset: 'database 1' });
-        render(<DatasetSelector {...mockProps} />);
+    it('should not query the database if preconfigured', async () => {
+      const mockProps = buildMockDataSelectorProps({ preconfiguredDataset: 'database 1' });
+      render(<DatasetSelector {...mockProps} />);
 
-        await waitFor(() => {
-          expect(mockProps.db.datasets).not.toHaveBeenCalled();
-        });
-      }
-    );
+      await waitFor(() => {
+        expect(mockProps.db.datasets).not.toHaveBeenCalled();
+      });
+    });
   });
 });
