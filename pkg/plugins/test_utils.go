c7f1d4f90cdaaa563aade6d2d6c2221a87827899
Giuseppe Guerra <to@nyo.zz.mu>
2023-04-20 17:52:16 +0200

    Moved inMemoryFS to test_utils.go

---
+++ pkg/plugins/test_utils.go
@@ -0,0 +1,75 @@
+package plugins
+
+import (
+	"bytes"
+	"io/fs"
+	"os"
+	"time"
+)
+
+var (
+	_ FS = &inMemoryFS{}
+)
+
+type inMemoryFS struct {
+	files map[string][]byte
+}
+
+func NewInMemoryFS(files map[string][]byte) FS {
+	return &inMemoryFS{files: files}
+}
+
+func NewFakeFS() FS {
+	return NewInMemoryFS(nil)
+}
+
+func (f inMemoryFS) Base() string {
+	return ""
+}
+
+func (f inMemoryFS) Files() []string {
+	fps := make([]string, 0, len(f.files))
+	for fn := range f.files {
+		fps = append(fps, fn)
+	}
+	return fps
+}
+
+func (f inMemoryFS) Open(fn string) (fs.File, error) {
+	if _, ok := f.files[fn]; !ok {
+		return nil, ErrFileNotExist
+	}
+	return &inMemoryFile{path: fn, reader: bytes.NewReader(f.files[fn])}, nil
+}
+
+type inMemoryFile struct {
+	path   string
+	reader *bytes.Reader
+}
+
+func (f *inMemoryFile) Stat() (fs.FileInfo, error) {
+	return inMemoryFileInfo{
+		name: f.path,
+		size: f.reader.Size(),
+	}, nil
+}
+
+func (f *inMemoryFile) Read(b []byte) (int, error) {
+	return f.reader.Read(b)
+}
+
+func (f *inMemoryFile) Close() error {
+	return nil
+}
+
+type inMemoryFileInfo struct {
+	name string
+	size int64
+}
+
+func (f inMemoryFileInfo) Name() string       { return f.name }
+func (f inMemoryFileInfo) Size() int64        { return f.size }
+func (f inMemoryFileInfo) Mode() os.FileMode  { return 0444 } // Read for all
+func (f inMemoryFileInfo) ModTime() time.Time { return time.Time{} }
+func (f inMemoryFileInfo) IsDir() bool        { return false }
+func (f inMemoryFileInfo) Sys() interface{}   { return nil }
