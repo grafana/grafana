4a1a7c0e4f05bcb824f879efaa95444a0fceec62
joshhunt <josh@trtr.co>
2023-04-21 17:52:55 +0100

    NestedFolders: Basic SearchView

--- public/app/features/browse-dashboards/components/SearchView.tsx
+++ public/app/features/browse-dashboards/components/SearchView.tsx
@@ -4,6 +4,10 @@ import { useKeyNavigationListener } from 'app/features/search/hooks/useSearchKey
 import { SearchResultsProps, SearchResultsTable } from 'app/features/search/page/components/SearchResultsTable';
 import { newSearchSelection, updateSearchSelection } from 'app/features/search/page/selection';
 import { getSearchStateManager } from 'app/features/search/state/SearchStateManager';
+import { DashboardViewItemKind } from 'app/features/search/types';
+import { useDispatch, useSelector } from 'app/types';
+
+import { setItemSelectionState } from '../state';
 
 interface SearchViewProps {
   height: number;
@@ -12,6 +16,8 @@ interface SearchViewProps {
 }
 
 export function SearchView({ folderUID, width, height }: SearchViewProps) {
+  const dispatch = useDispatch();
+  const selectedItems = useSelector((wholeState) => wholeState.browseDashboards.selectedItems);
   const showManage = true; // TODO: bring this in from parent?
 
   const { keyboardEvents } = useKeyNavigationListener();
@@ -25,19 +31,31 @@ export function SearchView({ folderUID, width, height }: SearchViewProps) {
 
   const value = state.result;
 
-  const selection = showManage ? searchSelection.isSelected : undefined;
+  const selectionChecker = useCallback(
+    (kind: string | undefined, uid: string): boolean => {
+      if (!kind || kind === '*') {
+        return false;
+      }
+
+      return selectedItems[assertDashboardViewItemKind(kind)][uid] ?? false;
+    },
+    [selectedItems]
+  );
 
   const clearSelection = useCallback(() => {
     searchSelection.items.clear();
     setSearchSelection({ ...searchSelection });
   }, [searchSelection]);
 
-  const toggleSelection = useCallback(
+  const handleItemSelectionChange = useCallback(
     (kind: string, uid: string) => {
-      const current = searchSelection.isSelected(kind, uid);
-      setSearchSelection(updateSearchSelection(searchSelection, !current, kind, [uid]));
+      const newIsSelected = !selectionChecker(kind, uid);
+
+      dispatch(
+        setItemSelectionState({ item: { kind: assertDashboardViewItemKind(kind), uid }, isSelected: newIsSelected })
+      );
     },
-    [searchSelection]
+    [selectionChecker, dispatch]
   );
 
   if (!value) {
@@ -46,8 +64,8 @@ export function SearchView({ folderUID, width, height }: SearchViewProps) {
 
   const props: SearchResultsProps = {
     response: value,
-    selection,
-    selectionToggle: toggleSelection,
+    selection: selectionChecker,
+    selectionToggle: handleItemSelectionChange,
     clearSelection,
     width: width,
     height: height,
@@ -58,4 +76,17 @@ export function SearchView({ folderUID, width, height }: SearchViewProps) {
   };
 
   return <SearchResultsTable {...props} />;
+}
+
+function assertDashboardViewItemKind(kind: string): DashboardViewItemKind {
+  switch (kind) {
+    case 'folder':
+      return 'folder';
+    case 'dashboard':
+      return 'dashboard';
+    case 'panel':
+      return 'panel';
+  }
+
+  throw new Error('Unsupported kind' + kind);
 }
