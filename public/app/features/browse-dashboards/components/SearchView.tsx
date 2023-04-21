76cc3ec059fe4043f9605c48b0305af979ab92e5
joshhunt <josh@trtr.co>
2023-04-21 10:55:57 +0100

    First stab and reusing SearchView for new Browse Dashboards

--- public/app/features/browse-dashboards/components/SearchView.tsx
+++ public/app/features/browse-dashboards/components/SearchView.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { parseRouteParams } from 'app/features/search/utils';
+import { useKeyNavigationListener } from 'app/features/search/hooks/useSearchKeyboardSelection';
+import { SearchResultsProps, SearchResultsTable } from 'app/features/search/page/components/SearchResultsTable';
+import { newSearchSelection, updateSearchSelection } from 'app/features/search/page/selection';
+import { getSearchStateManager } from 'app/features/search/state/SearchStateManager';
 
 interface SearchViewProps {
-  searchState: ReturnType<typeof parseRouteParams>;
+  height: number;
+  width: number;
+  folderUID: string | undefined;
 }
 
-export function SearchView({ searchState }: SearchViewProps) {
-  return (
-    <div>
-      <p>SearchView</p>
+export function SearchView({ folderUID, width, height }: SearchViewProps) {
+  const showManage = true; // TODO: bring this in from parent?
 
-      <pre>{JSON.stringify(searchState, null, 2)}</pre>
-    </div>
+  const { keyboardEvents } = useKeyNavigationListener();
+
+  const stateManager = getSearchStateManager();
+  useEffect(() => stateManager.initStateFromUrl(folderUID), [folderUID, stateManager]);
+
+  const state = stateManager.useState();
+
+  const [searchSelection, setSearchSelection] = useState(() => newSearchSelection());
+
+  const value = state.result;
+
+  const selection = showManage ? searchSelection.isSelected : undefined;
+
+  const clearSelection = useCallback(() => {
+    searchSelection.items.clear();
+    setSearchSelection({ ...searchSelection });
+  }, [searchSelection]);
+
+  const toggleSelection = useCallback(
+    (kind: string, uid: string) => {
+      const current = searchSelection.isSelected(kind, uid);
+      setSearchSelection(updateSearchSelection(searchSelection, !current, kind, [uid]));
+    },
+    [searchSelection]
   );
+
+  if (!value) {
+    return <div>loading?</div>;
+  }
+
+  const props: SearchResultsProps = {
+    response: value,
+    selection,
+    selectionToggle: toggleSelection,
+    clearSelection,
+    width: width,
+    height: height,
+    onTagSelected: stateManager.onAddTag,
+    keyboardEvents,
+    onDatasourceChange: state.datasource ? stateManager.onDatasourceChange : undefined,
+    onClickItem: stateManager.onSearchItemClicked,
+  };
+
+  return <SearchResultsTable {...props} />;
 }
