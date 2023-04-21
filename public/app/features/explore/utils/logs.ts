6abd75b69cb967b6cfca3351c6a3bcfa3dd94935
Sven Grossmann <sven.grossmann@grafana.com>
2023-04-21 17:21:02 +0200

    Log Context: Add `wrap lines` button to easier wrap lines (#67047)
    * add wrap lines button
    
    * change default to 50 lines
    
    * fix test
    
    * change css classes

--- public/app/features/explore/utils/logs.ts
+++ public/app/features/explore/utils/logs.ts
@@ -4,4 +4,5 @@ export const SETTINGS_KEYS = {
   wrapLogMessage: 'grafana.explore.logs.wrapLogMessage',
   prettifyLogMessage: 'grafana.explore.logs.prettifyLogMessage',
   logsSortOrder: 'grafana.explore.logs.sortOrder',
+  logContextWrapLogMessage: 'grafana.explore.logs.logContext.wrapLogMessage',
 };
