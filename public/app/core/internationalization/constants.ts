9bfd287529d0d5e599add4b5d7a2cc757b6fc571
mchuc <46643095+mchuc@users.noreply.github.com>
2023-04-23 19:44:39 +0200

    polish language

--- public/app/core/internationalization/constants.ts
+++ public/app/core/internationalization/constants.ts
@@ -4,6 +4,7 @@ export const ENGLISH_US = 'en-US';
 export const FRENCH_FRANCE = 'fr-FR';
 export const SPANISH_SPAIN = 'es-ES';
 export const GERMAN_GERMANY = 'de-DE';
+export const POLISH_POLAND = 'pl-PL';
 export const CHINESE_SIMPLIFIED = 'zh-Hans';
 export const PSEUDO_LOCALE = 'pseudo-LOCALE';
 
@@ -43,6 +44,12 @@ export const LANGUAGES: LanguageDefinitions[] = [
     code: GERMAN_GERMANY,
     name: 'Deutsch',
     loader: () => import('../../../locales/de-DE/grafana.json'),
+  },
+  
+  {
+    code: POLISH_POLAND,
+    name: 'polski',
+    loader: () => import('../../../locales/pl-PL/grafana.json'),
   },
 
   {
