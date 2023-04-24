4e492ae725de458887c791c51049cc970c7fe50c
Ashley Harrison <ashley.harrison@grafana.com>
2023-04-24 16:41:32 +0100

    Navigation: Unify `Page` component (#66951)
    * remove old page component
    
    * add test to check initDashboard is only called once (prevent variables loading twice)
    
    * add help node
    
    * update unit tests
    
    * remove last mentions of topnav
    
    * fix unit tests
    
    * remove unused props from ButtonRow interface
    
    * remove prop from test

--- public/app/core/components/Page/PluginPage.tsx
+++ public/app/core/components/Page/PluginPage.tsx
@@ -3,7 +3,7 @@ import React, { useContext } from 'react';
 import { PluginPageProps } from '@grafana/runtime';
 import { PluginPageContext } from 'app/features/plugins/components/PluginPageContext';
 
-import { Page } from '../Page/Page';
+import { Page } from './Page';
 
 export function PluginPage({ actions, children, info, pageNav, layout, renderTitle, subTitle }: PluginPageProps) {
   const context = useContext(PluginPageContext);
