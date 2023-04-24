20a6a5edb8f1f95ae51813f49ef9dc230a4e7128
Kuba Siemiatkowski <jakub.siemiatkowski@grafana.com>
2023-04-24 17:28:13 +0200

    - prettier and betterer fixes

--- public/app/features/datasources/components/DataSourceConfigAlert.tsx
+++ public/app/features/datasources/components/DataSourceConfigAlert.tsx
@@ -99,7 +99,10 @@ export const DataSourceConfigAlert = React.forwardRef<HTMLDivElement, Props>(
               , or &nbsp;
               <Link
                 aria-label={`Explore data`}
-                className={cx(styles.link, { [`${styles.disabled}`]: !canExploreDataSources, 'test-disabled': !canExploreDataSources })}
+                className={cx(styles.link, {
+                  [`${styles.disabled}`]: !canExploreDataSources,
+                  'test-disabled': !canExploreDataSources,
+                })}
                 href={exploreUrl}
               >
                 {exploreDataLinkText}
