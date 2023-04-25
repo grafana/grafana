c7aafea3700343a38729cde98f6a63e1c0866ad0
Esteban Beltran <academo@users.noreply.github.com>
2023-04-25 14:24:16 +0200

    Toolkit: Remove deprecated `plugin:sign` command (#67130)

--- packages/grafana-toolkit/src/cli/index.ts
+++ packages/grafana-toolkit/src/cli/index.ts
@@ -5,7 +5,6 @@ import { nodeVersionCheckerTask } from './tasks/nodeVersionChecker';
 import { buildPackageTask } from './tasks/package.build';
 import { pluginBuildTask } from './tasks/plugin.build';
 import { ciBuildPluginTask, ciPackagePluginTask, ciPluginReportTask } from './tasks/plugin.ci';
-import { pluginSignTask } from './tasks/plugin.sign';
 import { pluginUpdateTask } from './tasks/plugin.update';
 import { getToolkitVersion, githubPublishTask } from './tasks/plugin.utils';
 import { bundleManagedTask } from './tasks/plugin/bundle.managed';
@@ -123,17 +122,12 @@ export const run = (includeInternalScripts = false) => {
       },
       []
     )
-    .description('[Deprecated] Create a plugin signature')
-    .action(async (cmd) => {
+    .description('[removed] Use grafana sign-plugin instead')
+    .action(() => {
       console.log(
-        chalk.yellow('\n⚠️  DEPRECATED. This command is deprecated and will be removed in v10. ⚠️') +
-          '\nPlease migrate to grafana sign-plugin https://github.com/grafana/plugin-tools/tree/main/packages/sign-plugin'
+        'No longer supported. Use grafana sign-plugin https://github.com/grafana/plugin-tools/tree/main/packages/sign-plugin\n'
       );
-      await execTask(pluginSignTask)({
-        signatureType: cmd.signatureType,
-        rootUrls: cmd.rootUrls,
-        silent: true,
-      });
+      process.exit(1);
     });
 
   program
