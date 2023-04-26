8d44e8b6d1a6ed9bd1f63224fa4ff049992efed3
Ryan McKinley <ryantxu@gmail.com>
2023-04-26 14:14:01 -0700

    update file names

--- packages/grafana-schema/src/index.ts
+++ packages/grafana-schema/src/index.ts
@@ -6,7 +6,7 @@
 export * from './veneer/common.types';
 export * from './index.gen';
 
-// TODO index.gen shoudl include these
+// TODO: The following imports should be included in ./index.gen
 export type { Team } from './raw/team/x/team_types.gen';
 export type { LibraryPanel } from './veneer/librarypanel.types';
 export type { Dashboard } from './veneer/dashboard.types';
