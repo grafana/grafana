3002076ebe85b0b169fbbc074809371c09c1d1a9
Zoltán Bedi <zoltan.bedi@gmail.com>
2023-04-28 16:50:47 +0200

    Remove exclamation mark

--- public/app/plugins/panel/gauge/GaugePanel.tsx
+++ public/app/plugins/panel/gauge/GaugePanel.tsx
@@ -62,7 +62,7 @@ export class GaugePanel extends PureComponent<PanelProps<PanelOptions>> {
           const min = field.config.min ?? 0;
           const max = field.config.max ?? field.config.unit === 'percent' ? 100 : 1;
           field.state = field.state ?? {};
-          field.state.range = { min, max, delta: max! - min! };
+          field.state.range = { min, max, delta: max - min };
           field.display = getDisplayProcessor({ field, theme: config.theme2 });
         }
       }
