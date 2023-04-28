166641d66d22c72d93d6d5c4c69b1630d9450e87
Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
2023-04-28 18:00:56 +0200

    NestedFolders: Permission for creating and editing (#67406)
    * Initial permissions
    
    * Show/hide checkboxes
    
    * Fix kinds from main
    
    * CreateNewButton tests
    
    * Update DashboardsTree test
    
    * Mock folder permissions
    
    * Rename showCheckBoxes to canSelect
    
    * Make column ordering look better

--- public/app/features/browse-dashboards/components/DashboardsTree.tsx
+++ public/app/features/browse-dashboards/components/DashboardsTree.tsx
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 import { CellProps, Column, HeaderProps, TableInstance, useTable } from 'react-table';
 import { FixedSizeList as List } from 'react-window';
 
-import { GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme2, isTruthy } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { Checkbox, useStyles2 } from '@grafana/ui';
 import { DashboardViewItem, DashboardViewItemKind } from 'app/features/search/types';
@@ -23,6 +23,7 @@ interface DashboardsTreeProps {
   onFolderClick: (uid: string, newOpenState: boolean) => void;
   onAllSelectionChange: (newState: boolean) => void;
   onItemSelectionChange: (item: DashboardViewItem, newState: boolean) => void;
+  canSelect: boolean;
 }
 
 type DashboardsTreeColumn = Column<DashboardsTreeItem>;
@@ -46,34 +47,37 @@ export function DashboardsTree({
   onFolderClick,
   onAllSelectionChange,
   onItemSelectionChange,
+  canSelect = false,
 }: DashboardsTreeProps) {
   const styles = useStyles2(getStyles);
 
   const tableColumns = useMemo(() => {
-    const checkboxColumn: DashboardsTreeColumn = {
-      id: 'checkbox',
-      width: 0,
-      Header: ({ selectedItems }: DashboardTreeHeaderProps) => {
-        const isAllSelected = selectedItems?.$all ?? false;
-        return <Checkbox value={isAllSelected} onChange={(ev) => onAllSelectionChange(ev.currentTarget.checked)} />;
-      },
-      Cell: ({ row: { original: row }, selectedItems }: DashboardsTreeCellProps) => {
-        const item = row.item;
-        if (item.kind === 'ui-empty-folder' || !selectedItems) {
-          return <></>;
+    const checkboxColumn: DashboardsTreeColumn | null = canSelect
+      ? {
+          id: 'checkbox',
+          width: 0,
+          Header: ({ selectedItems }: DashboardTreeHeaderProps) => {
+            const isAllSelected = selectedItems?.$all ?? false;
+            return <Checkbox value={isAllSelected} onChange={(ev) => onAllSelectionChange(ev.currentTarget.checked)} />;
+          },
+          Cell: ({ row: { original: row }, selectedItems }: DashboardsTreeCellProps) => {
+            const item = row.item;
+            if (item.kind === 'ui-empty-folder' || !selectedItems) {
+              return <></>;
+            }
+
+            const isSelected = selectedItems?.[item.kind][item.uid] ?? false;
+
+            return (
+              <Checkbox
+                data-testid={selectors.pages.BrowseDashbards.table.checkbox(item.uid)}
+                value={isSelected}
+                onChange={(ev) => onItemSelectionChange(item, ev.currentTarget.checked)}
+              />
+            );
+          },
         }
-
-        const isSelected = selectedItems?.[item.kind][item.uid] ?? false;
-
-        return (
-          <Checkbox
-            data-testid={selectors.pages.BrowseDashbards.table.checkbox(item.uid)}
-            value={isSelected}
-            onChange={(ev) => onItemSelectionChange(item, ev.currentTarget.checked)}
-          />
-        );
-      },
-    };
+      : null;
 
     const nameColumn: DashboardsTreeColumn = {
       id: 'name',
@@ -95,9 +99,10 @@ export function DashboardsTree({
       Header: 'Tags',
       Cell: TagsCell,
     };
+    const columns = [canSelect && checkboxColumn, nameColumn, typeColumn, tagsColumns].filter(isTruthy);
 
-    return [checkboxColumn, nameColumn, typeColumn, tagsColumns];
-  }, [onItemSelectionChange, onAllSelectionChange, onFolderClick]);
+    return columns;
+  }, [onItemSelectionChange, onAllSelectionChange, onFolderClick, canSelect]);
 
   const table = useTable({ columns: tableColumns, data: items }, useCustomFlexLayout);
   const { getTableProps, getTableBodyProps, headerGroups } = table;
