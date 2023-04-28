090ef70f95b67ef07b61464379b7c2a846d7c110
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-04-28 13:16:30 -0400

    Alerting: Update alerting module to 20230428154802-ad53acfab07f (#67541)

--- pkg/tests/api/alerting/api_notification_channel_test.go
+++ pkg/tests/api/alerting/api_notification_channel_test.go
@@ -723,8 +723,13 @@ func TestIntegrationNotificationChannels(t *testing.T) {
 	mockEmail := &mockEmailHandler{}
 
 	// Set up responses
-	mockChannel.responses["slack_recv1"] = `{"ok": true}`
-	mockChannel.responses["slack_recvX"] = `{"ok": true}`
+	mockChannel.responses["slack_recv1"] = func(res http.ResponseWriter) {
+		res.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(res, `{"ok": true}`)
+	}
+	mockChannel.responses["slack_recvX"] = func(res http.ResponseWriter) {
+		fmt.Fprint(res, `ok`)
+	}
 
 	// Overriding some URLs to send to the mock channel.
 	os, opa, ot, opu, ogb, ol, oth := alertingSlack.APIURL, alertingPagerduty.APIURL,
@@ -1009,7 +1014,7 @@ type mockNotificationChannel struct {
 	server *http.Server
 
 	receivedNotifications  map[string][]string
-	responses              map[string]string
+	responses              map[string]func(res http.ResponseWriter)
 	notificationErrorCount int
 	notificationsMtx       sync.RWMutex
 }
@@ -1027,7 +1032,7 @@ func newMockNotificationChannel(t *testing.T, grafanaListedAddr string) *mockNot
 			Addr: listener.Addr().String(),
 		},
 		receivedNotifications: make(map[string][]string),
-		responses:             make(map[string]string),
+		responses:             make(map[string]func(res http.ResponseWriter)),
 		t:                     t,
 	}
 
@@ -1056,8 +1061,11 @@ func (nc *mockNotificationChannel) ServeHTTP(res http.ResponseWriter, req *http.
 	body := getBody(nc.t, req.Body)
 
 	nc.receivedNotifications[key] = append(nc.receivedNotifications[key], body)
-	res.WriteHeader(http.StatusOK)
-	fmt.Fprint(res, nc.responses[paths[0]])
+	if f, ok := nc.responses[paths[0]]; ok {
+		f(res)
+	} else {
+		res.WriteHeader(http.StatusOK)
+	}
 }
 
 func (nc *mockNotificationChannel) totalNotifications() int {
