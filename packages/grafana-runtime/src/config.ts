1fbac96bd495894f05956e97f5394bd0147f4a9b
Kyle Cunningham <codeincarnate@users.noreply.github.com>
2023-04-28 13:23:08 +0700

    SQL Datasources: Update behavior of default connection limits (#66687)
    * Update behavior of defaults in connection limits
    
    * Refactor to use config object instead
    
    * Refactor remove unneeded function
    
    ---------
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>

--- packages/grafana-runtime/src/config.ts
+++ packages/grafana-runtime/src/config.ts
@@ -154,6 +154,11 @@ export class GrafanaBootConfig implements GrafanaConfig {
   rudderstackDataPlaneUrl: undefined;
   rudderstackSdkUrl: undefined;
   rudderstackConfigUrl: undefined;
+  sqlConnectionLimits = {
+    maxOpenConns: 100,
+    maxIdleConns: 100,
+    connMaxLifetime: 14400,
+  };
 
   tokenExpirationDayLimit: undefined;
 
