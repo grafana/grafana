9caa9d1cfd09016091adbbe24337259fa16c855a
bohandley <brendan.ohandley@gmail.com>
2023-04-29 21:23:52 -0400

    galen alphabet refactor suggestion

--- public/app/plugins/datasource/prometheus/querybuilder/components/metrics-modal/LetterSearch.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/components/metrics-modal/LetterSearch.tsx
@@ -60,64 +60,12 @@ export function LetterSearch(props: LetterSearchProps) {
   );
 }
 
-function alphabetCheck() {
-  const alphabetDict: { [char: string]: number } = {
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0,
-    E: 0,
-    F: 0,
-    G: 0,
-    H: 0,
-    I: 0,
-    J: 0,
-    K: 0,
-    L: 0,
-    M: 0,
-    N: 0,
-    O: 0,
-    P: 0,
-    Q: 0,
-    R: 0,
-    S: 0,
-    T: 0,
-    U: 0,
-    V: 0,
-    W: 0,
-    X: 0,
-    Y: 0,
-    Z: 0,
-  };
+export const alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
-  return alphabetDict;
+function alphabetCheck(): { [char: string]: number } {
+  const check: { [char: string]: number } = {};
+
+  [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].forEach((char) => (check[char] = 0));
+
+  return check;
 }
-
-export const alphabet = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
