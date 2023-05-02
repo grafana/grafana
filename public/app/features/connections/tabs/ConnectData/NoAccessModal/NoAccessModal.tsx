ab05f8492202097996b8314b08c2d921be9a2928
Torkel Ödegaard <torkel@grafana.com>
2023-05-02 10:51:59 +0200

    Connections: Simplify connections nav (#66813)
    * Connections: Simplify connections nav
    
    * rename Connections pages everywhere
    
    ---------
    
    Co-authored-by: Miklós Tolnai <miklos.tolnai@grafana.com>
    (cherry picked from commit 9614dc2446eac438bedb0783654ccf77acb6dae8)

--- public/app/features/connections/tabs/ConnectData/NoAccessModal/NoAccessModal.tsx
+++ public/app/features/connections/tabs/ConnectData/NoAccessModal/NoAccessModal.tsx
@@ -91,7 +91,7 @@ export function NoAccessModal({ item, isOpen, onDismiss }: NoAccessModalProps) {
           <div>
             <p>
               Editors cannot add new connections. You may check to see if it is already configured in{' '}
-              <a href="/connections/your-connections">Your Connections</a>.
+              <a href="/connections/your-datasources">Your data sources</a>.
             </p>
             <p>To add a new connection, contact your Grafana admin.</p>
           </div>
