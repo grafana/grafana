e54164b5217930a4b76aa93c2faf443345403700
Giuseppe Guerra <giuseppe@guerra.in>
2023-05-02 18:15:17 +0200

    Tests: Fix

--- pkg/cmd/grafana-cli/commands/conflict_user_command.go
+++ pkg/cmd/grafana-cli/commands/conflict_user_command.go
@@ -20,6 +20,7 @@ import (
 	"github.com/grafana/grafana/pkg/cmd/grafana-cli/utils"
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/infra/tracing"
+	"github.com/grafana/grafana/pkg/plugins/manager/loader/hooks"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
@@ -71,7 +72,7 @@ func initializeConflictResolver(cmd *utils.ContextCommandLine, f Formatter, ctx 
 	if err != nil {
 		return nil, fmt.Errorf("%v: %w", "failed to get feature management service", err)
 	}
-	acService, err := acimpl.ProvideService(cfg, s, routing, nil, nil, featMgmt, nil)
+	acService, err := acimpl.ProvideService(cfg, s, routing, nil, nil, featMgmt, hooks.NewService())
 	if err != nil {
 		return nil, fmt.Errorf("%v: %w", "failed to get access control", err)
 	}
