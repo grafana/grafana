53cb379d6fe524dbcc296d2857d81c5daa842a28
Ashley Harrison <ashley.harrison@grafana.com>
2023-05-02 17:11:35 +0100

    only refresh at the end

--- public/app/features/browse-dashboards/components/BrowseActions/BrowseActions.tsx
+++ public/app/features/browse-dashboards/components/BrowseActions/BrowseActions.tsx
@@ -4,17 +4,21 @@ import React from 'react';
 import { GrafanaTheme2 } from '@grafana/data';
 import { Button, useStyles2 } from '@grafana/ui';
 import appEvents from 'app/core/app_events';
-import { useDispatch } from 'app/types';
+import { useDispatch, useSelector } from 'app/types';
 import { ShowModalReactEvent } from 'app/types/events';
 
 import {
+  childrenByParentUIDSelector,
   deleteDashboard,
   deleteFolder,
+  fetchChildren,
   moveDashboard,
   moveFolder,
+  rootItemsSelector,
   setAllSelection,
   useActionSelectionState,
 } from '../../state';
+import { findItem } from '../../state/utils';
 
 import { DeleteModal } from './DeleteModal';
 import { MoveModal } from './MoveModal';
@@ -27,43 +31,65 @@ export function BrowseActions() {
   const dispatch = useDispatch();
   const selectedDashboards = Object.keys(selectedItems.dashboard).filter((uid) => selectedItems.dashboard[uid]);
   const selectedFolders = Object.keys(selectedItems.folder).filter((uid) => selectedItems.folder[uid]);
+  const rootItems = useSelector(rootItemsSelector);
+  const childrenByParentUID = useSelector(childrenByParentUIDSelector);
 
-  const onActionComplete = () => {
+  const onActionComplete = (parentsToRefresh: Set<string | undefined>) => {
     dispatch(
       setAllSelection({
         isSelected: false,
       })
     );
+    for (const parentUID of parentsToRefresh) {
+      dispatch(fetchChildren(parentUID));
+    }
   };
 
   const onDelete = async () => {
+    const parentsToRefresh = new Set<string | undefined>();
+
     // Delete all the folders sequentially
     // TODO error handling here
     for (const folderUID of selectedFolders) {
       await dispatch(deleteFolder(folderUID));
+      // find the parent folder uid and add it to parentsToRefresh
+      const folder = findItem(rootItems ?? [], childrenByParentUID, folderUID);
+      parentsToRefresh.add(folder?.parentUID);
     }
 
-    // Delete all the dashboards sequenetially
+    // Delete all the dashboards sequentially
     // TODO error handling here
     for (const dashboardUID of selectedDashboards) {
       await dispatch(deleteDashboard(dashboardUID));
+      // find the parent folder uid and add it to parentsToRefresh
+      const dashboard = findItem(rootItems ?? [], childrenByParentUID, dashboardUID);
+      parentsToRefresh.add(dashboard?.parentUID);
     }
-    onActionComplete();
+    onActionComplete(parentsToRefresh);
   };
 
   const onMove = async (destinationUID: string) => {
+    const parentsToRefresh = new Set<string | undefined>();
+    parentsToRefresh.add(destinationUID);
+
     // Move all the folders sequentially
     // TODO error handling here
     for (const folderUID of selectedFolders) {
       await dispatch(moveFolder({ folderUID, destinationUID }));
+      // find the parent folder uid and add it to parentsToRefresh
+      const folder = findItem(rootItems ?? [], childrenByParentUID, folderUID);
+      parentsToRefresh.add(folder?.parentUID);
     }
 
     // Move all the dashboards sequentially
     // TODO error handling here
     for (const dashboardUID of selectedDashboards) {
       await dispatch(moveDashboard({ dashboardUID, destinationUID }));
+      // find the parent folder uid and add it to parentsToRefresh
+      const dashboard = findItem(rootItems ?? [], childrenByParentUID, dashboardUID);
+      parentsToRefresh.add(dashboard?.parentUID);
     }
-    onActionComplete();
+    onActionComplete(parentsToRefresh);
   };
 
   const showMoveModal = () => {
--- public/app/features/browse-dashboards/state/actions.ts
+++ public/app/features/browse-dashboards/state/actions.ts
@@ -4,9 +4,6 @@ import { GENERAL_FOLDER_UID } from 'app/features/search/constants';
 import { getFolderChildren } from 'app/features/search/service/folders';
 import { createAsyncThunk, DashboardDTO } from 'app/types';
 
-import { rootItemsSelector, childrenByParentUIDSelector } from './hooks';
-import { findItem } from './utils';
-
 export const fetchChildren = createAsyncThunk(
   'browseDashboards/fetchChildren',
   async (parentUID: string | undefined) => {
@@ -19,38 +16,20 @@ export const fetchChildren = createAsyncThunk(
 export const deleteDashboard = createAsyncThunk(
   'browseDashboards/deleteDashboard',
   async (dashboardUID: string, thunkApi) => {
-    const result = await getBackendSrv().delete<DeleteDashboardResponse>(`/api/dashboards/uid/${dashboardUID}`);
-    const { dispatch, getState } = thunkApi;
-    const state = getState();
-
-    // refetch the parent folder children to update the tree
-    const dashboard = findItem(rootItemsSelector(state) ?? [], childrenByParentUIDSelector(state), dashboardUID);
-    dispatch(fetchChildren(dashboard?.parentUID));
-
-    return result;
+    return getBackendSrv().delete<DeleteDashboardResponse>(`/api/dashboards/uid/${dashboardUID}`);
   }
 );
 
 export const deleteFolder = createAsyncThunk('browseDashboards/deleteFolder', async (folderUID: string, thunkApi) => {
-  const result = await getBackendSrv().delete(`/api/folders/${folderUID}`, undefined, {
+  return getBackendSrv().delete(`/api/folders/${folderUID}`, undefined, {
     params: { forceDeleteRules: true },
   });
-  const { dispatch, getState } = thunkApi;
-  const state = getState();
-
-  // refetch the parent and destination folder children to update the tree
-  const folder = findItem(rootItemsSelector(state) ?? [], childrenByParentUIDSelector(state), folderUID);
-  dispatch(fetchChildren(folder?.parentUID));
-
-  return result;
 });
 
 export const moveDashboard = createAsyncThunk(
   'browseDashboards/moveDashboard',
   async ({ dashboardUID, destinationUID }: { dashboardUID: string; destinationUID: string }, thunkApi) => {
     const fullDash: DashboardDTO = await getBackendSrv().get(`/api/dashboards/uid/${dashboardUID}`);
-    const { dispatch, getState } = thunkApi;
-    const state = getState();
 
     const options = {
       dashboard: fullDash.dashboard,
@@ -58,32 +37,16 @@ export const moveDashboard = createAsyncThunk(
       overwrite: false,
     };
 
-    const result = await getBackendSrv().post('/api/dashboards/db', {
+    return getBackendSrv().post('/api/dashboards/db', {
       message: '',
       ...options,
     });
-
-    // refetch the parent and destination folder children to update the tree
-    const dashboard = findItem(rootItemsSelector(state) ?? [], childrenByParentUIDSelector(state), dashboardUID);
-    dispatch(fetchChildren(dashboard?.parentUID));
-    dispatch(fetchChildren(destinationUID));
-
-    return result;
   }
 );
 
 export const moveFolder = createAsyncThunk(
   'browseDashboards/moveFolder',
   async ({ folderUID, destinationUID }: { folderUID: string; destinationUID: string }, thunkApi) => {
-    const result = await getBackendSrv().post(`/api/folders/${folderUID}/move`, { parentUID: destinationUID });
-    const { dispatch, getState } = thunkApi;
-    const state = getState();
-
-    // refetch the parent and destination folder children to update the tree
-    const folder = findItem(rootItemsSelector(state) ?? [], childrenByParentUIDSelector(state), folderUID);
-    dispatch(fetchChildren(folder?.parentUID));
-    dispatch(fetchChildren(destinationUID));
-
-    return result;
+    return getBackendSrv().post(`/api/folders/${folderUID}/move`, { parentUID: destinationUID });
   }
 );
