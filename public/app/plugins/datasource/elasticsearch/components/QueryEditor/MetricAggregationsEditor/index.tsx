af9176b3a54d5c82b5abf4ee20fa1efa914f5eea
Gábor Farkas <gabor.farkas@gmail.com>
2023-05-05 15:00:39 +0200

    Elasticsearch: Improve query type selection (#63402)
    * elastic: display query types explicitly
    
    * fixed test
    
    * fixed test
    
    * updated test names
    
    * fix react-key-issue

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/MetricAggregationsEditor/index.tsx
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/MetricAggregationsEditor/index.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from '../../../hooks/useStatelessReducer';
 import { IconButton } from '../../IconButton';
 import { useQuery } from '../ElasticsearchQueryContext';
 import { QueryEditorRow } from '../QueryEditorRow';
+import { QueryEditorSpecialMetricRow } from '../QueryEditorSpecialMetricRow';
 
 import { MetricAggregation } from './../../../types';
 import { MetricEditor } from './MetricEditor';
@@ -21,21 +22,39 @@ export const MetricAggregationsEditor = ({ nextId }: Props) => {
 
   return (
     <>
-      {metrics?.map((metric, index) => (
-        <QueryEditorRow
-          key={`${metric.type}-${metric.id}`}
-          label={`Metric (${metric.id})`}
-          hidden={metric.hide}
-          onHideClick={() => dispatch(toggleMetricVisibility(metric.id))}
-          onRemoveClick={totalMetrics > 1 && (() => dispatch(removeMetric(metric.id)))}
-        >
-          <MetricEditor value={metric} />
+      {metrics?.map((metric, index) => {
+        switch (metric.type) {
+          case 'logs':
+            return <QueryEditorSpecialMetricRow key={`${metric.type}-${metric.id}`} name="Logs" metric={metric} />;
+          case 'raw_data':
+            return <QueryEditorSpecialMetricRow key={`${metric.type}-${metric.id}`} name="Raw Data" metric={metric} />;
+          case 'raw_document':
+            return (
+              <QueryEditorSpecialMetricRow
+                key={`${metric.type}-${metric.id}`}
+                name="Raw Document"
+                metric={metric}
+                info="(NOTE: Raw document query type is deprecated)"
+              />
+            );
+          default:
+            return (
+              <QueryEditorRow
+                key={`${metric.type}-${metric.id}`}
+                label={`Metric (${metric.id})`}
+                hidden={metric.hide}
+                onHideClick={() => dispatch(toggleMetricVisibility(metric.id))}
+                onRemoveClick={totalMetrics > 1 && (() => dispatch(removeMetric(metric.id)))}
+              >
+                <MetricEditor value={metric} />
 
-          {!metricAggregationConfig[metric.type].isSingleMetric && index === 0 && (
-            <IconButton iconName="plus" onClick={() => dispatch(addMetric(nextId))} label="add" />
-          )}
-        </QueryEditorRow>
-      ))}
+                {!metricAggregationConfig[metric.type].isSingleMetric && index === 0 && (
+                  <IconButton iconName="plus" onClick={() => dispatch(addMetric(nextId))} label="add" />
+                )}
+              </QueryEditorRow>
+            );
+        }
+      })}
     </>
   );
 };
