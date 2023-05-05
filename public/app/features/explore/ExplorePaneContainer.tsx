3f665fb666fd9aaefa67c574b825749cb9471d89
Giordano Ricci <me@giordanoricci.com>
2023-05-05 13:34:10 +0100

    zombie children workaround

--- public/app/features/explore/ExplorePaneContainer.tsx
+++ public/app/features/explore/ExplorePaneContainer.tsx
@@ -1,9 +1,11 @@
 import { css } from '@emotion/css';
 import React, { useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
 
 import { EventBusSrv, GrafanaTheme2 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { useStyles2 } from '@grafana/ui';
+import { StoreState } from 'app/types';
 import { ExploreId } from 'app/types/explore';
 
 import Explore from './Explore';
@@ -27,7 +29,16 @@ interface Props {
   exploreId: ExploreId;
 }
 
-export function ExplorePaneContainer({ exploreId }: Props) {
+/*
+  Connected components subscribe to the store before function components (using hooks) and can react to store changes. Thus, this connector function is called before the parent component (ExplorePage) is rerendered.
+  This means that child components' mapStateToProps will be executed with a zombie `exploreId` that is not present anymore in the store if the pane gets closed.
+  By connecting this component and returning the pane we workaround the zombie children issue here instead of modifying every children.
+  This is definitely not the ideal solution and we should in the future invest more time in exploring other approaches to better handle this scenario, potentially by refactoring panels to be function components 
+  (therefore immune to this behaviour), or by forbidding them to access the store directly and instead pass them all the data they need via props or context.
+
+  You can read more about this issue here: https://react-redux.js.org/api/hooks#stale-props-and-zombie-children
+*/
+function ExplorePaneContainerUnconnected({ exploreId }: Props) {
   const styles = useStyles2(getStyles);
   const eventBus = useRef(new EventBusSrv());
   const ref = useRef(null);
@@ -43,3 +54,13 @@ export function ExplorePaneContainer({ exploreId }: Props) {
     </div>
   );
 }
+
+function mapStateToProps(state: StoreState, props: Props) {
+  const pane = state.explore.panes[props.exploreId];
+
+  return { pane };
+}
+
+const connector = connect(mapStateToProps);
+
+export const ExplorePaneContainer = connector(ExplorePaneContainerUnconnected);
