a39299e1eed18e3d5911befdc2826fcf57b469ca
Juan Cabanas <juan.cabanas@grafana.com>
2023-05-09 09:29:46 -0300

    NavBar: app chrome state wrongly overwritten when ds modal is opened … (#68041)
    NavBar: app chrome state wrongly overwritten when ds modal is opened (#67952)
    
    (cherry picked from commit 24668137f808fb55c6aa55136be2ff9de8b04a42)

--- public/app/features/dashboard/containers/DashboardPage.tsx
+++ public/app/features/dashboard/containers/DashboardPage.tsx
@@ -64,7 +64,6 @@ export type DashboardPageRouteSearchParams = {
   editPanel?: string;
   viewPanel?: string;
   editview?: string;
-  shareView?: string;
   panelType?: string;
   inspect?: string;
   from?: string;
@@ -429,7 +428,6 @@ export class UnthemedDashboardPage extends PureComponent<Props, State> {
           onAddPanel={this.onAddPanel}
           kioskMode={kioskMode}
           hideTimePicker={dashboard.timepicker.hidden}
-          shareModalActiveTab={this.props.queryParams.shareView}
         />
       </header>
     );
