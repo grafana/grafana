971d2255185271291a69b57f7b0873e5774b4ce6
Torkel Ödegaard <torkel@grafana.com>
2023-05-09 07:46:13 +0200

    update wording

--- public/app/features/dashboard/components/PanelEditor/AngularPanelPluginWarning.tsx
+++ public/app/features/dashboard/components/PanelEditor/AngularPanelPluginWarning.tsx
@@ -14,7 +14,26 @@ export function AngularPanelPluginWarning({ plugin }: Props) {
   return (
     <div className={styles.wrapper}>
       <Alert title="Angular panel plugin" severity="warning">
-        The selected visualization plugin is using deprecated plugin APIs and will stop working in Grafana 11.
+        <div className="markdown-html">
+          <p>The selected visualization plugin is using deprecated plugin APIs.</p>
+          <ul>
+            <li>
+              <a
+                href="https://grafana.com/docs/grafana/latest/developers/angular_deprecation/"
+                className="external-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Read more on angular deprecation
+              </a>
+            </li>
+            <li>
+              <a href={`plugins/${encodeURIComponent(plugin.meta.id)}`} className="external-link">
+                View plugin details
+              </a>
+            </li>
+          </ul>
+        </div>
       </Alert>
     </div>
   );
