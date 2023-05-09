b3592437339d0ae0fc966bb44317cddc335a9c22
Andreas Christou <andreas.christou@grafana.com>
2023-05-09 16:03:15 +0100

    Update tracking
    - Add traces tracking
    - Update other query type tracking

--- public/app/plugins/datasource/azuremonitor/module.test.ts
+++ public/app/plugins/datasource/azuremonitor/module.test.ts
@@ -1,6 +1,8 @@
 import { DashboardLoadedEvent } from '@grafana/data';
 import { reportInteraction } from '@grafana/runtime';
+
 import './module';
+import { AzureMonitorQuery, ResultFormat } from './types';
 
 jest.mock('@grafana/runtime', () => {
   return {
@@ -17,11 +19,36 @@ jest.mock('@grafana/runtime', () => {
             grafanaVersion: 'v9.0.0',
             queries: {
               'grafana-azure-monitor-datasource': [
-                { queryType: 'Azure Monitor', hide: false },
-                { queryType: 'Azure Log Analytics', hide: false },
-                { queryType: 'Azure Resource Graph', hide: true },
-                { queryType: 'Azure Monitor', hide: false },
-              ],
+                {
+                  queryType: 'Azure Monitor',
+                  hide: false,
+                  azureMonitor: { resources: ['test_resource_1', 'test_resource_2'] },
+                },
+                { queryType: 'Azure Monitor', hide: true },
+                {
+                  queryType: 'Azure Log Analytics',
+                  hide: false,
+                  azureLogAnalytics: { resources: ['test_workspace_1', 'test_workspace_2'] },
+                },
+                {
+                  queryType: 'Azure Log Analytics',
+                  hide: true,
+                },
+                { queryType: 'Azure Resource Graph', hide: true, subscriptions: ['sub1', 'sub2'] },
+                { queryType: 'Azure Resource Graph', hide: false },
+                {
+                  queryType: 'Azure Traces',
+                  hide: false,
+                  azureTraces: {
+                    resources: ['test-ai-1', 'test-ai-2'],
+                    operationId: 'test-op-id',
+                    resultFormat: 'table',
+                    traceTypes: ['trace'],
+                    filters: [{ filters: 'test-filter', operation: 'eq', property: 'test-property' }],
+                  },
+                },
+                { queryType: 'Azure Traces', hide: true, azureTraces: { resultFormat: 'trace' } },
+              ] as AzureMonitorQuery[],
             },
           })
         );
@@ -37,12 +64,26 @@ describe('queriesOnInitDashboard', () => {
       dashboard_id: 'dashboard123',
       grafana_version: 'v9.0.0',
       org_id: 1,
-      azure_monitor_queries: 2,
+      azure_monitor_queries: 1,
+      azure_monitor_queries_hidden: 1,
+      azure_monitor_multiple_resource: 1,
+
       azure_log_analytics_queries: 1,
-      azure_resource_graph_queries: 0,
-      azure_monitor_queries_hidden: 0,
-      azure_log_analytics_queries_hidden: 0,
+      azure_log_analytics_queries_hidden: 1,
+      azure_log_multiple_resource: 1,
+
+      azure_resource_graph_queries: 1,
       azure_resource_graph_queries_hidden: 1,
+      azure_resource_graph_multiple_subscription: 1,
+
+      azure_traces_queries: 1,
+      azure_traces_queries_hidden: 1,
+      azure_traces_multiple_resource: 1,
+      azure_traces_table: 1,
+      azure_traces_trace: 1,
+      azure_traces_operation_id_specified: 1,
+      azure_traces_event_type_specified: 1,
+      azure_traces_filters: 1,
     });
   });
 });
--- public/app/plugins/datasource/azuremonitor/module.ts
+++ public/app/plugins/datasource/azuremonitor/module.ts
@@ -6,7 +6,7 @@ import AzureMonitorQueryEditor from './components/QueryEditor';
 import Datasource from './datasource';
 import pluginJson from './plugin.json';
 import { trackAzureMonitorDashboardLoaded } from './tracking';
-import { AzureMonitorQuery, AzureDataSourceJsonData, AzureQueryType } from './types';
+import { AzureMonitorQuery, AzureDataSourceJsonData, AzureQueryType, ResultFormat } from './types';
 
 export const plugin = new DataSourcePlugin<Datasource, AzureMonitorQuery, AzureDataSourceJsonData>(Datasource)
   .setConfigEditor(ConfigEditor)
@@ -21,23 +21,69 @@ getAppEvents().subscribe<DashboardLoadedEvent<AzureMonitorQuery>>(
       [AzureQueryType.AzureMonitor]: {
         hidden: 0,
         visible: 0,
+        multiResource: 0,
       },
       [AzureQueryType.LogAnalytics]: {
         hidden: 0,
         visible: 0,
+        multiResource: 0,
       },
       [AzureQueryType.AzureResourceGraph]: {
         hidden: 0,
         visible: 0,
+        multiSubscription: 0,
+      },
+      [AzureQueryType.AzureTraces]: {
+        hidden: 0,
+        visible: 0,
+        multiResource: 0,
+        table: 0,
+        trace: 0,
+        operationIdSpecified: 0,
+        eventTypesSpecified: 0,
+        filtersSpecified: 0,
       },
     };
     azureQueries.forEach((query) => {
-      if (
-        query.queryType === AzureQueryType.AzureMonitor ||
-        query.queryType === AzureQueryType.LogAnalytics ||
-        query.queryType === AzureQueryType.AzureResourceGraph
-      ) {
-        stats[query.queryType][query.hide ? 'hidden' : 'visible']++;
+      if (query.queryType === AzureQueryType.AzureMonitor) {
+        stats[AzureQueryType.AzureMonitor][query.hide ? 'hidden' : 'visible']++;
+        if (query.azureMonitor?.resources && query.azureMonitor.resources.length > 1) {
+          stats[AzureQueryType.AzureMonitor].multiResource++;
+        }
+      }
+      if (query.queryType === AzureQueryType.LogAnalytics) {
+        stats[AzureQueryType.LogAnalytics][query.hide ? 'hidden' : 'visible']++;
+        if (query.azureLogAnalytics?.resources && query.azureLogAnalytics.resources.length > 1) {
+          stats[AzureQueryType.LogAnalytics].multiResource++;
+        }
+      }
+      if (query.queryType === AzureQueryType.AzureResourceGraph) {
+        stats[AzureQueryType.AzureResourceGraph][query.hide ? 'hidden' : 'visible']++;
+        if (query.subscriptions && query.subscriptions.length > 1) {
+          stats[AzureQueryType.AzureResourceGraph].multiSubscription++;
+        }
+      }
+      if (query.queryType === AzureQueryType.AzureTraces) {
+        stats[AzureQueryType.AzureTraces][query.hide ? 'hidden' : 'visible']++;
+        if (query.azureTraces) {
+          if (query.azureTraces.resultFormat) {
+            stats[AzureQueryType.AzureTraces][
+              query.azureTraces.resultFormat === ResultFormat.Trace ? ResultFormat.Trace : ResultFormat.Table
+            ]++;
+          }
+          if (query.azureTraces.resources && query.azureTraces.resources.length > 1) {
+            stats[AzureQueryType.AzureTraces].multiResource++;
+          }
+          if (query.azureTraces.operationId && query.azureTraces.operationId !== '') {
+            stats[AzureQueryType.AzureTraces].operationIdSpecified++;
+          }
+          if (query.azureTraces.traceTypes && query.azureTraces.traceTypes.length > 0) {
+            stats[AzureQueryType.AzureTraces].eventTypesSpecified++;
+          }
+          if (query.azureTraces.filters && query.azureTraces.filters.length > 0) {
+            stats[AzureQueryType.AzureTraces].filtersSpecified++;
+          }
+        }
       }
     });
 
@@ -46,12 +92,30 @@ getAppEvents().subscribe<DashboardLoadedEvent<AzureMonitorQuery>>(
         grafana_version: grafanaVersion,
         dashboard_id: dashboardId,
         org_id: orgId,
+        // Metrics queries stats
         azure_monitor_queries: stats[AzureQueryType.AzureMonitor].visible,
-        azure_log_analytics_queries: stats[AzureQueryType.LogAnalytics].visible,
-        azure_resource_graph_queries: stats[AzureQueryType.AzureResourceGraph].visible,
         azure_monitor_queries_hidden: stats[AzureQueryType.AzureMonitor].hidden,
+        azure_monitor_multiple_resource: stats[AzureQueryType.AzureMonitor].multiResource,
+
+        // Logs queries stats
+        azure_log_analytics_queries: stats[AzureQueryType.LogAnalytics].visible,
         azure_log_analytics_queries_hidden: stats[AzureQueryType.LogAnalytics].hidden,
+        azure_log_multiple_resource: stats[AzureQueryType.LogAnalytics].multiResource,
+
+        // ARG queries stats
+        azure_resource_graph_queries: stats[AzureQueryType.AzureResourceGraph].visible,
         azure_resource_graph_queries_hidden: stats[AzureQueryType.AzureResourceGraph].hidden,
+        azure_resource_graph_multiple_subscription: stats[AzureQueryType.AzureResourceGraph].multiSubscription,
+
+        // Traces queries stats
+        azure_traces_queries: stats[AzureQueryType.AzureTraces].visible,
+        azure_traces_queries_hidden: stats[AzureQueryType.AzureTraces].hidden,
+        azure_traces_multiple_resource: stats[AzureQueryType.AzureTraces].multiResource,
+        azure_traces_table: stats[AzureQueryType.AzureTraces].table,
+        azure_traces_trace: stats[AzureQueryType.AzureTraces].trace,
+        azure_traces_operation_id_specified: stats[AzureQueryType.AzureTraces].operationIdSpecified,
+        azure_traces_event_type_specified: stats[AzureQueryType.AzureTraces].eventTypesSpecified,
+        azure_traces_filters: stats[AzureQueryType.AzureTraces].filtersSpecified,
       });
     }
   }
