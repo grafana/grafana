148183c868fe5e8cbe8583c181297f7c4d817e46
Jack Westbrook <jack.westbrook@gmail.com>
2023-05-09 10:59:54 +0100

    chore(storybook): mimic broken alphabetical storySort and docs overview ordering

--- packages/grafana-ui/.storybook/preview.ts
+++ packages/grafana-ui/.storybook/preview.ts
@@ -46,9 +46,19 @@ const preview: Preview = {
     },
     layout: 'fullscreen',
     options: {
-      storySort: {
-        method: 'alphabetical',
-        order: ['Docs Overview', ['Intro']],
+      // Sort stories first by Docs Overview, then alphabetically
+      // We should be able to use the builtin alphabetical sort, but is broken in SB 7.0
+      // https://github.com/storybookjs/storybook/issues/22470
+      storySort: (a, b) => {
+        if (a.title.startsWith('Docs Overview')) {
+          if (b.title.startsWith('Docs Overview')) {
+            return 0;
+          }
+          return -1;
+        } else if (b.title.startsWith('Docs Overview')) {
+          return 1;
+        }
+        return a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true });
       },
     },
   },
