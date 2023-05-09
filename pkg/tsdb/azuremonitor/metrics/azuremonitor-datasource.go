126426e15e3891fb0ed3e4566a8ce76ffd5fbd19
Adam Simpson <adam@adamsimpson.net>
2023-05-09 09:06:36 -0400

    AzureMonitor: Correct series name accounting for dimensions (#67050) (#68033)
    Correct displayName accounting for dimensions
    
    Co-authored-by: Andreas Christou <andreas.christou@grafana.com>

--- pkg/tsdb/azuremonitor/metrics/azuremonitor-datasource.go
+++ pkg/tsdb/azuremonitor/metrics/azuremonitor-datasource.go
@@ -317,9 +317,12 @@ func (e *AzureMonitorDatasource) parseResponse(amr types.AzureMonitorResponse, q
 				Unit: toGrafanaUnit(amr.Value[0].Unit),
 			})
 		}
-		resourceID, ok := labels["microsoft.resourceid"]
+
+		resourceIdLabel := "microsoft.resourceid"
+		resourceID, ok := labels[resourceIdLabel]
 		if !ok {
-			resourceID = labels["Microsoft.ResourceId"]
+			resourceIdLabel = "Microsoft.ResourceId"
+			resourceID = labels[resourceIdLabel]
 		}
 		resourceIDSlice := strings.Split(resourceID, "/")
 		resourceName := ""
@@ -331,18 +334,15 @@ func (e *AzureMonitorDatasource) parseResponse(amr types.AzureMonitorResponse, q
 			resourceName = extractResourceNameFromMetricsURL(query.URL)
 			resourceID = extractResourceIDFromMetricsURL(query.URL)
 		}
-		displayName := ""
-		if query.Alias != "" {
-			displayName = formatAzureMonitorLegendKey(query.Alias, resourceName,
-				amr.Value[0].Name.LocalizedValue, "", "", amr.Namespace, amr.Value[0].ID, labels)
-		} else if len(labels) > 0 {
-			// If labels are set, it will be used as the legend so we need to set a more user-friendly name
-			displayName = amr.Value[0].Name.LocalizedValue
-			if resourceName != "" {
-				displayName += " " + resourceName
-			}
+		if _, ok := labels[resourceIdLabel]; ok {
+			delete(labels, resourceIdLabel)
+			labels["resourceName"] = resourceName
 		}
-		if displayName != "" {
+
+		if query.Alias != "" {
+			displayName := formatAzureMonitorLegendKey(query.Alias, resourceName,
+				amr.Value[0].Name.LocalizedValue, "", "", amr.Namespace, amr.Value[0].ID, labels)
+
 			if dataField.Config != nil {
 				dataField.Config.DisplayName = displayName
 			} else {
