7bb00851c95d41cb179138197bcecb42d9112edf
Alexander Zobnin <alexanderzobnin@gmail.com>
2023-05-10 13:30:50 +0300

    Settings: Do not hide sensitive values if it's empty (#68088)
    * Settings: Do not hide sensitive values if it's empty
    
    * Fix implementation
    
    * Add tests for RedactedValue function
    
    (cherry picked from commit 0b6ae0d1197303a7afc8397937ec9c5d6243f187)

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -563,6 +563,10 @@ func ToAbsUrl(relativeUrl string) string {
 }
 
 func RedactedValue(key, value string) string {
+	if value == "" {
+		return ""
+	}
+
 	uppercased := strings.ToUpper(key)
 	// Sensitive information: password, secrets etc
 	for _, pattern := range []string{
