35e292688c1bbd600722245e85018240f3e3845b
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-05-10 11:26:57 +0200

    Add some styles

--- public/app/core/components/NestedFolderPicker/NestedFolderPicker.tsx
+++ public/app/core/components/NestedFolderPicker/NestedFolderPicker.tsx
@@ -3,23 +3,29 @@ import React, { useState } from 'react';
 import { FixedSizeList as List } from 'react-window';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { FilterInput, IconButton, useStyles2 } from '@grafana/ui';
+import { Button, FilterInput, IconButton, useStyles2 } from '@grafana/ui';
 
 const LIST_HEIGHT = 200;
-const ROW_HEIGHT = 35;
+const ROW_HEIGHT = 40;
 
 interface RowProps {
   index: number;
   style: React.CSSProperties;
+  data: Array<{
+    name: string;
+    level: number;
+    expanded: boolean;
+  }>;
 }
 
-function Row({ index, style }: RowProps) {
-  const styles = useStyles2(getStyles);
+function Row({ index, style, data }: RowProps) {
+  const folder = data[index];
+  const styles = useStyles2((theme) => getRowStyles(theme, folder.level));
 
   return (
-    <div style={style} className={styles.row}>
-      <IconButton name="angle-right" />
-      {index}
+    <div style={style} className={styles}>
+      <IconButton name={folder.expanded ? 'angle-down' : 'angle-right'} />
+      {folder.name}
     </div>
   );
 }
@@ -33,24 +39,96 @@ export function NestedFolderPicker() {
       <h4>Select folder</h4>
       <FilterInput placeholder="Search folder" value={search} escapeRegex={false} onChange={(val) => setSearch(val)} />
       <p className={styles.headerRow}>Name</p>
-      <List height={LIST_HEIGHT} width="100%" itemSize={35} itemCount={ROW_HEIGHT}>
+      <List height={LIST_HEIGHT} width="100%" itemData={DATA} itemSize={ROW_HEIGHT} itemCount={DATA.length}>
         {Row}
       </List>
+      <Button>Select</Button>
     </div>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  row: css({
-    display: 'flex',
-    alignItems: 'center',
-    paddingLeft: theme.spacing(1),
-  }),
   headerRow: css({
     backgroundColor: theme.colors.background.secondary,
     height: ROW_HEIGHT,
     lineHeight: ROW_HEIGHT + 'px',
     margin: 0,
-    paddingLeft: theme.spacing(4),
+    marginTop: theme.spacing(2),
+    paddingLeft: theme.spacing(3),
   }),
 });
+
+const getRowStyles = (theme: GrafanaTheme2, level: number) =>
+  css({
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: `solid 2px ${theme.colors.border}`,
+    paddingLeft: theme.spacing(level * 2),
+    ':hover': {
+      backgroundColor: theme.colors.background.secondary,
+      cursor: 'pointer',
+    },
+  });
+
+const DATA = [
+  {
+    name: 'Funky folder',
+    level: 1,
+    expanded: true,
+  },
+  {
+    name: 'Funky folder 2',
+    level: 1,
+    expanded: true,
+  },
+  {
+    name: 'Wasd folder',
+    level: 1,
+    expanded: true,
+  },
+  {
+    name: 'Asdf folder',
+    level: 2,
+    expanded: false,
+  },
+  {
+    name: 'Wasd folder 2',
+    level: 2,
+    expanded: false,
+  },
+  {
+    name: 'Funky folder 2',
+    level: 1,
+    expanded: false,
+  },
+  {
+    name: 'Funky folder',
+    level: 1,
+    expanded: false,
+  },
+  {
+    name: 'Funky folder 2',
+    level: 1,
+    expanded: false,
+  },
+  {
+    name: 'Wasd folder',
+    level: 1,
+    expanded: false,
+  },
+  {
+    name: 'Asdf folder',
+    level: 2,
+    expanded: false,
+  },
+  {
+    name: 'Wasd folder 2',
+    level: 2,
+    expanded: false,
+  },
+  {
+    name: 'Funky folder 2',
+    level: 1,
+    expanded: false,
+  },
+];
