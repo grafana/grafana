5277adc1a4f11fda771c7db3b76e3c35baef9b4c
yangkb09 <yangkb09@gmail.com>
2023-05-10 09:31:32 -0400

    feat: add subcommands: check, owners, modules; chunk out some functions

--- modowners.go
+++ modowners.go
@@ -10,72 +10,110 @@ import (
 )
 
 type Module struct {
-	Name   string
-	Owners []string
+	Name     string
+	Owners   []string
+	Indirect bool
 }
 
-// TODO: figure out how to return the parsed modfile as a []Module
-// func ParseGoMod() ([]Module, error) {
-// 	// Turn go.mod into array of bytes
-// 	data, err := os.ReadFile("dummy/go.mod")
-// 	if err != nil {
-// 		log.Fatalf("failed to read file: %s", err)
-// 	}
-// 	fmt.Printf("Contents of go.mod file:\n%s\n", string(data))
-
-// 	// Parse modfile
-// 	modFile, err := modfile.Parse("", data, nil)
-// 	if err != nil {
-// 		log.Fatalf("failed to parse modfile: %s", err)
-// 	}
-
-// 	// TODO: remove this
-// 	fmt.Println("MODFILE", modFile.Module.Mod.Path)
-
-// 	return modFile, error
-// }
-
-func main() {
-	// Turn go.mod into array of bytes
-	data, err := os.ReadFile("dummy/go.mod")
-	if err != nil {
-		log.Fatalf("failed to read file: %s", err)
-	}
-
-	// Parse modfile
-	modFile, err := modfile.Parse("", data, nil)
-	if err != nil {
-		log.Fatalf("failed to parse modfile: %s", err)
-	}
-
+func parseModule(mod *modfile.Require) Module {
 	// New Module struct
-	m := Module{}
+	m := Module{Name: mod.Mod.String()}
 
-	// Flag to track if dependency is indirect
-	flag := false
-	fmt.Println("flag", flag) // QUESTION: including this line for now because if i comment this out, i get compile warning "flag declared but not used" - how come?
-
-	// Iterate through requires in modfile
-	for _, require := range modFile.Require {
-		// For each require, access the comment
-		for _, comment := range require.Syntax.Comments.Suffix {
-			owners := strings.Fields(comment.Token)
-			fmt.Println("owners!!", owners) // QUESTION: i left the last dependency in the dummy modfile empty - why doesn't this print the empty dependency?
-			// For each comment, determine if it contains an owner(s)
-			for _, owner := range owners {
-				// Break if dependency is indirect
-				if strings.Contains(owner, "indirect") {
-					flag = true // QUESTION: why do we need flag if we're breaking anyway?
-					break
-				}
-				// If an owner, add to owners list
-				if strings.Contains(owner, "@") {
-					if !strings.Contains(strings.Join(m.Owners, " "), owner) { // QUESTION: this `strings.Join(m.Owners, " ")` doesn't change the original m.Owners, right?
-						m.Owners = append(m.Owners, owner)
-					}
-				}
+	// For each require, access the comment
+	for _, comment := range mod.Syntax.Comments.Suffix {
+		owners := strings.Fields(comment.Token)
+		// For each comment, determine if it contains an owner(s)
+		for _, owner := range owners {
+			if strings.Contains(owner, "indirect") {
+				m.Indirect = true
+			}
+			// If an owner, add to owners list
+			if strings.Contains(owner, "@") {
+				m.Owners = append(m.Owners, owner)
 			}
 		}
 	}
-	fmt.Println("owners: ", m.Owners)
+	return m
+}
+
+func parseGoMod(name string) ([]Module, error) {
+	// Turn go.mod into array of bytes
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse modfile
+	modFile, err := modfile.Parse(name, data, nil)
+	if err != nil {
+		return nil, err
+	}
+	modules := []Module{}
+	// Iterate through requires in modfile
+	for _, mod := range modFile.Require {
+		m := parseModule(mod)
+		modules = append(modules, m)
+	}
+	return modules, nil
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("usage: modowners subcommand go.mod...")
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "check":
+		m, err := parseGoMod(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fail := false
+		for _, mod := range m {
+			if mod.Indirect == false && len(mod.Owners) == 0 {
+				fmt.Println(mod.Name)
+				fail = true
+			}
+		}
+		if fail {
+			os.Exit(1)
+		}
+	case "owners": // Print owners for specific dependency(s)
+		// TODO: enhance to take list of modules (specific one, two, or etc)
+		// TODO: have indirect flag that prints indirect
+
+		// TODO: owners and modules may optionally take a list (modules for owners, owners for modules)
+		// TODO: introduce help messages
+		// TODO: test with go test
+		// TODO: move every subcommand into its own func to keep main small
+		m, err := parseGoMod(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		owners := map[string]int{}
+		for _, mod := range m {
+			if mod.Indirect == false {
+				for _, owner := range mod.Owners {
+					owners[owner]++
+				}
+			}
+		}
+		for owner, n := range owners {
+			fmt.Println(owner, n)
+		}
+
+	case "modules": // Print all direct dependencies
+		// TODO: optionally print the count
+		m, err := parseGoMod(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, mod := range m {
+			if mod.Indirect == false {
+				fmt.Println(mod.Name)
+			}
+		}
+	default:
+		os.Exit(1)
+	}
 }
