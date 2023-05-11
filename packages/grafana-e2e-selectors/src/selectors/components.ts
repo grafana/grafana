4279a7671190feace1351a68f19a976442d4a629
khushijain21 <khushij393@gmail.com>
2023-05-11 17:44:40 +0530

    Added data-testid

--- packages/grafana-e2e-selectors/src/selectors/components.ts
+++ packages/grafana-e2e-selectors/src/selectors/components.ts
@@ -141,8 +141,8 @@ export const Components = {
       content: 'Panel editor data pane content',
     },
     applyButton: 'data-testid Apply changes and go back to dashboard',
-    toggleVizPicker: 'toggle-viz-picker',
-    toggleVizOptions: 'toggle-viz-options',
+    toggleVizPicker: 'data-testid toggle-viz-picker',
+    toggleVizOptions: 'data-testid toggle-viz-options',
     toggleTableView: 'toggle-table-view',
 
     // [Geomap] Map controls
