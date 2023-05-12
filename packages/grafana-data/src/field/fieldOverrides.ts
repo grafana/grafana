e357ce9578a3f0055e4f7a4cf871dbaa87fee31a
Domas Lapinskas <domasx2@gmail.com>
2023-05-12 15:05:32 +0300

    bind variable replacer provided to link.onBuildUrl to data frame vars

--- packages/grafana-data/src/field/fieldOverrides.ts
+++ packages/grafana-data/src/field/fieldOverrides.ts
@@ -378,6 +378,9 @@ export const getLinksSupplier =
         __dataContext: dataContext,
       };
 
+      const boundReplaceVariables: InterpolateFunction = (value, scopedVars, format) =>
+        replaceVariables(value, { ...dataLinkScopedVars, ...scopedVars }, format);
+
       // We are not displaying reduction result
       if (config.valueRowIndex !== undefined && !isNaN(config.valueRowIndex)) {
         dataContext.value.rowIndex = config.valueRowIndex;
@@ -394,7 +397,7 @@ export const getLinksSupplier =
             link.onClick!({
               origin: origin ?? field,
               e: evt,
-              replaceVariables: (v) => replaceVariables(v, dataLinkScopedVars),
+              replaceVariables: boundReplaceVariables,
             });
           },
           origin: field,
@@ -409,13 +412,13 @@ export const getLinksSupplier =
           scopedVars: dataLinkScopedVars,
           field,
           range: link.internal.range ?? ({} as any),
-          replaceVariables,
+          replaceVariables: boundReplaceVariables,
         });
       }
       let href = link.onBuildUrl
         ? link.onBuildUrl({
             origin: field,
-            replaceVariables,
+            replaceVariables: boundReplaceVariables,
           })
         : link.url;
 
