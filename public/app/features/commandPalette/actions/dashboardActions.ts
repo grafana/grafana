423d6a81bc81c32ff41fb32bbce77a2ce8e0eab9
Ashley Harrison <ashley.harrison@grafana.com>
2023-05-12 13:44:58 +0100

    only update the state if this is the most recent request

--- public/app/features/commandPalette/actions/dashboardActions.ts
+++ public/app/features/commandPalette/actions/dashboardActions.ts
@@ -1,5 +1,5 @@
 import debounce from 'debounce-promise';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { locationUtil } from '@grafana/data';
 import { config } from '@grafana/runtime';
@@ -83,14 +83,22 @@ export async function getSearchResultActions(searchQuery: string): Promise<Comma
 export function useSearchResults(searchQuery: string, isShowing: boolean) {
   const [searchResults, setSearchResults] = useState<CommandPaletteAction[]>([]);
   const [isFetchingSearchResults, setIsFetchingSearchResults] = useState(false);
+  const lastRequestTimestamp = useRef<number>();
 
   // Hit dashboards API
   useEffect(() => {
     if (isShowing && searchQuery.length > 0) {
       setIsFetchingSearchResults(true);
+      const timestamp = Date.now();
       debouncedSearch(searchQuery).then((resultActions) => {
-        setSearchResults(resultActions);
-        setIsFetchingSearchResults(false);
+        // Only update the state if this is the most recent request
+        // We don't need to worry about clearing the isFetching state either
+        // If there's a later request in progress, this will clear it for us
+        if (!lastRequestTimestamp.current || timestamp > lastRequestTimestamp.current) {
+          setSearchResults(resultActions);
+          setIsFetchingSearchResults(false);
+          lastRequestTimestamp.current = timestamp;
+        }
       });
     } else {
       setSearchResults([]);
