9b7e0e0d5762caf52665e4e7f9cf37e4ee612d99
Ivan Ortega <ivanortegaalba@gmail.com>
2023-05-12 14:45:24 +0200

    Add Panel: Enable new DS picker and onboarding DS prompt

--- pkg/services/featuremgmt/toggles_gen.go
+++ pkg/services/featuremgmt/toggles_gen.go
@@ -308,7 +308,7 @@ const (
 	FlagPluginsAPIManifestKey = "pluginsAPIManifestKey"
 
 	// FlagAdvancedDataSourcePicker
-	// Enable a new data source picker with contextual information, recently used order, CSV upload and advanced mode
+	// Enable a new data source picker with contextual information, recently used order and advanced mode
 	FlagAdvancedDataSourcePicker = "advancedDataSourcePicker"
 
 	// FlagOpensearchDetectVersion
@@ -316,7 +316,7 @@ const (
 	FlagOpensearchDetectVersion = "opensearchDetectVersion"
 
 	// FlagFaroDatasourceSelector
-	// Enable the data source selector within the Frontend Apps section of the Frontend Observability 
+	// Enable the data source selector within the Frontend Apps section of the Frontend Observability
 	FlagFaroDatasourceSelector = "faroDatasourceSelector"
 
 	// FlagEnableDatagridEditing
