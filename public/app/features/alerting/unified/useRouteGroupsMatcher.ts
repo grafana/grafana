a52d172fb98ada5a6422f7d7dfa14b5fdf00b567
Konrad Lalik <konrad.lalik@grafana.com>
2023-05-15 15:38:43 +0200

    Move worker init to the file scope

--- public/app/features/alerting/unified/useRouteGroupsMatcher.ts
+++ public/app/features/alerting/unified/useRouteGroupsMatcher.ts
@@ -1,29 +1,22 @@
 import * as comlink from 'comlink';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback } from 'react';
 
 import { AlertmanagerGroup, RouteWithID } from '../../../plugins/datasource/alertmanager/types';
 
 import { logInfo } from './Analytics';
 import type { RouteGroupsMatcher } from './routeGroupsMatcher.worker';
 
+const worker = new Worker(new URL('./routeGroupsMatcher.worker.ts', import.meta.url), { type: 'module' });
+const routeMatcher = comlink.wrap<RouteGroupsMatcher>(worker);
+
 export function useRouteGroupsMatcher() {
-  const workerRef = useRef(new Worker(new URL('./routeGroupsMatcher.worker.ts', import.meta.url), { type: 'module' }));
-  const routeMatcherRef = useRef(comlink.wrap<RouteGroupsMatcher>(workerRef.current));
-
-  useEffect(() => {
-    const worker = workerRef.current;
-    const routeMatcher = routeMatcherRef.current;
-
-    return () => {
-      routeMatcher[comlink.releaseProxy]();
-      worker.terminate();
-    };
-  }, []);
-
   const getRouteGroupsMap = useCallback(async (rootRoute: RouteWithID, alertGroups: AlertmanagerGroup[]) => {
     const startTime = performance.now();
-    const result = await routeMatcherRef.current.getRouteGroupsMap(rootRoute, alertGroups);
+
+    const result = await routeMatcher.getRouteGroupsMap(rootRoute, alertGroups);
+
     const timeSpent = performance.now() - startTime;
+
     logInfo(`Route Groups Matched in  ${timeSpent} ms`, {
       matchingTime: timeSpent.toString(),
       alertGroupsCount: alertGroups.length.toString(),
