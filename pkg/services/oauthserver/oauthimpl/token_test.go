a63ad675eec6cf97c82b0b4086741ee68dea4b39
gamab <gabi.mabs@gmail.com>
2023-05-15 21:47:14 +0200

    client_credentials grant test

--- pkg/services/oauthserver/oauthimpl/token_test.go
+++ pkg/services/oauthserver/oauthimpl/token_test.go
@@ -2,18 +2,25 @@ package oauthimpl
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/ory/fosite"
-	"github.com/stretchr/testify/require"
-
 	"github.com/grafana/grafana/pkg/models/roletype"
 	ac "github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/oauthserver"
+	"github.com/grafana/grafana/pkg/services/serviceaccounts"
 	"github.com/grafana/grafana/pkg/services/team"
 	"github.com/grafana/grafana/pkg/services/user"
+	"github.com/ory/fosite"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 func TestOAuth2ServiceImpl_handleClientCredentials(t *testing.T) {
@@ -412,6 +419,163 @@ func TestOAuth2ServiceImpl_handleJWTBearer(t *testing.T) {
 			for k, v := range tt.expectedClaims {
 				require.Equal(t, v, sessionData.JWTClaims.Extra[k])
 			}
+		})
+	}
+}
+
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	Scope       string `json:"scope"`
+	TokenType   string `json:"token_type"`
+}
+
+type Claims struct {
+	jwt.Claims
+	ClientID     string              `json:"client_id"`
+	Groups       []string            `json:"groups"`
+	Email        string              `json:"email"`
+	Name         string              `json:"name"`
+	Login        string              `json:"login"`
+	Scopes       []string            `json:"scope"`
+	Entitlements map[string][]string `json:"entitlements"`
+}
+
+func TestOAuth2ServiceImpl_HandleTokenRequest(t *testing.T) {
+	client1Secret := "RANDOMSECRET"
+	hashedSecret, err := bcrypt.GenerateFromPassword([]byte(client1Secret), bcrypt.DefaultCost)
+	require.NoError(t, err)
+	client1 := &oauthserver.Client{
+		ExternalServiceName: "testapp",
+		ClientID:            "RANDOMID",
+		Secret:              string(hashedSecret),
+		GrantTypes:          string(fosite.GrantTypeClientCredentials),
+		ServiceAccountID:    2,
+		ImpersonatePermissions: []ac.Permission{
+			{Action: "folders:read", Scope: "folders:*"},
+			{Action: "dashboards:read", Scope: "folders:*"},
+			{Action: "dashboards:read", Scope: "dashboards:*"},
+		},
+		SelfPermissions: []ac.Permission{
+			{Action: "users:impersonate", Scope: "users:*"},
+		},
+	}
+	sa1 := &serviceaccounts.ServiceAccountProfileDTO{
+		Id:         client1.ServiceAccountID,
+		Name:       "testapp",
+		Login:      "testapp",
+		OrgId:      oauthserver.TmpOrgID,
+		IsDisabled: false,
+		Created:    time.Now(),
+		Updated:    time.Now(),
+		Role:       "Viewer",
+	}
+
+	// user56 := &user.User{
+	// 	ID:      56,
+	// 	Email:   "user56@example.org",
+	// 	Login:   "user56",
+	// 	Name:    "User 56",
+	// 	Updated: now,
+	// }
+	// teams := []*team.TeamDTO{
+	// 	{ID: 1, Name: "Team 1", OrgID: 1},
+	// 	{ID: 2, Name: "Team 2", OrgID: 1},
+	// }
+
+	tests := []struct {
+		name       string
+		initEnv    func(env *TestEnv)
+		urlValues  url.Values
+		wantCode   int
+		wantScope  []string
+		wantClaims *Claims
+	}{
+		{
+			name: "client credentials grant",
+			initEnv: func(env *TestEnv) {
+				env.OAuthStore.On("GetExternalService", mock.Anything, client1.ClientID).Return(client1, nil)
+				env.SAService.On("RetrieveServiceAccount", mock.Anything, oauthserver.TmpOrgID, client1.ServiceAccountID).Return(sa1, nil)
+				env.AcStore.ExpectedUserPermissions = client1.SelfPermissions
+			},
+			urlValues: url.Values{
+				"grant_type":    {"client_credentials"},
+				"client_id":     {client1.ClientID},
+				"client_secret": {client1Secret},
+				"scope":         {"profile email groups entitlements"},
+				"audience":      {"http://localhost:3000"},
+			},
+			wantCode:  http.StatusOK,
+			wantScope: []string{"profile", "email", "groups", "entitlements"},
+			wantClaims: &Claims{
+				Claims: jwt.Claims{
+					Subject:  "user:id:2", // From client1.ServiceAccountID
+					Issuer:   "test",      // From env.S.Config.Issuer
+					Audience: jwt.Audience{"http://localhost:3000"},
+				},
+				ClientID: client1.ClientID,
+				Name:     "testapp",
+				Login:    "testapp",
+				// Scopes:       []string{"profile", "email", "groups", "entitlements"}, // TODO SHOULD WE ADD SCOPE TO THE JWT WITH CLIENT_CREDENTIALS?
+				Entitlements: map[string][]string{
+					"users:impersonate": {"users:*"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := setupTestEnv(t)
+			if tt.initEnv != nil {
+				tt.initEnv(env)
+			}
+
+			req := httptest.NewRequest("POST", "/oauth2/token", strings.NewReader(tt.urlValues.Encode()))
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+			resp := httptest.NewRecorder()
+
+			env.S.HandleTokenRequest(resp, req)
+
+			require.Equal(t, tt.wantCode, resp.Code)
+			if tt.wantCode != http.StatusOK {
+				return
+			}
+
+			body := resp.Body.Bytes()
+			require.NotEmpty(t, body)
+
+			var tokenResp TokenResponse
+			require.NoError(t, json.Unmarshal(body, &tokenResp))
+
+			// Check token response
+			require.NotEmpty(t, tokenResp.Scope)
+			require.ElementsMatch(t, tt.wantScope, strings.Split(tokenResp.Scope, " "))
+			require.Positive(t, tokenResp.ExpiresIn)
+			require.Equal(t, "bearer", tokenResp.TokenType)
+			require.NotEmpty(t, tokenResp.AccessToken)
+
+			// Check access token
+			parsedToken, err := jwt.ParseSigned(tokenResp.AccessToken)
+			require.NoError(t, err)
+			require.Len(t, parsedToken.Headers, 1)
+			typeHeader := parsedToken.Headers[0].ExtraHeaders["typ"]
+			require.Equal(t, "at+jwt", strings.ToLower(typeHeader.(string)))
+			require.Equal(t, "RS256", parsedToken.Headers[0].Algorithm)
+			// Check access token claims
+			var claims Claims
+			require.NoError(t, parsedToken.Claims(pk.Public(), &claims))
+			// Check times and remove them
+			// TODO: Should we validate times?
+			require.Positive(t, claims.IssuedAt.Time())
+			require.Positive(t, claims.Expiry.Time())
+			claims.IssuedAt = jwt.NewNumericDate(time.Time{})
+			claims.Expiry = jwt.NewNumericDate(time.Time{})
+			// Check the ID and remove it
+			require.NotEmpty(t, claims.ID)
+			claims.ID = ""
+			// Compare the rest
+			require.Equal(t, tt.wantClaims, &claims)
 		})
 	}
 }
