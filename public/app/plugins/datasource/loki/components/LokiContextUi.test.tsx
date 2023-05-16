52355657b2737468a9c37565d7c94c10185b2b1c
Ivana Huckova <ivana.huckova@gmail.com>
2023-05-16 18:45:24 +0200

    Remove unused imports

--- public/app/plugins/datasource/loki/components/LokiContextUi.test.tsx
+++ public/app/plugins/datasource/loki/components/LokiContextUi.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { selectOptionInTest } from 'test/helpers/selectOptionInTest';
