2e0e65667e685782b8379f01bd2d676c527310f7
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-05-18 13:06:42 -0400

    add test for the bug

--- pkg/services/ngalert/backtesting/engine_test.go
+++ pkg/services/ngalert/backtesting/engine_test.go
@@ -263,6 +263,36 @@ func TestEvaluatorTest(t *testing.T) {
 		})
 	})
 
+	t.Run("should not fail if 'to-from' is not times of interval", func(t *testing.T) {
+		from := time.Unix(0, 0)
+		to := from.Add(5 * ruleInterval)
+
+		states := []state.StateTransition{
+			{
+				State: &state.State{
+					CacheID:     "state-1",
+					Labels:      models.GenerateAlertLabels(rand.Intn(5)+1, "test-"),
+					State:       eval.Normal,
+					StateReason: util.GenerateShortUID(),
+				},
+			},
+		}
+
+		manager.stateCallback = func(now time.Time) []state.StateTransition {
+			return states
+		}
+
+		frame, err := engine.Test(context.Background(), nil, rule, from, to)
+		require.NoError(t, err)
+		expectedLen := frame.Rows()
+		for i := 0; i < 100; i++ {
+			jitter := time.Duration(rand.Int63n(ruleInterval.Milliseconds())) * time.Millisecond
+			frame, err = engine.Test(context.Background(), nil, rule, from, to.Add(jitter))
+			require.NoError(t, err)
+			require.Equalf(t, expectedLen, frame.Rows(), "jitter %v caused result to be different that base-line", jitter)
+		}
+	})
+
 	t.Run("should backfill field with nulls if a new dimension created in the middle", func(t *testing.T) {
 		from := time.Unix(0, 0)
 
