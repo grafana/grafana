294f0ea1d147bb528c78a791100309a5c9cfd18c
Ivan Ortega <ivanortegaalba@gmail.com>
2023-05-18 12:55:32 +0200

    refactor and fixing types

--- public/app/features/datasources/components/picker/DataSourceDropdown.tsx
+++ public/app/features/datasources/components/picker/DataSourceDropdown.tsx
@@ -18,6 +18,7 @@ import { useDatasource } from '../../hooks';
 import { DataSourceList } from './DataSourceList';
 import { DataSourceLogo, DataSourceLogoPlaceHolder } from './DataSourceLogo';
 import { DataSourceModal } from './DataSourceModal';
+import { applyMaxSize, maxSize } from './popperModifiers';
 import { PickerContentProps, DataSourceDropdownProps } from './types';
 import { dataSourceLabel, matchDataSourceWithSearch } from './utils';
 
@@ -28,42 +29,6 @@ const INTERACTION_ITEM = {
   ADD_FILE: 'add_file',
   OPEN_ADVANCED_DS_PICKER: 'open_advanced_ds_picker',
   CONFIG_NEW_DS_EMPTY_STATE: 'config_new_ds_empty_state',
-};
-
-const MODAL_MARGIN = 20;
-const FLIP_THRESHOLD = 200;
-
-const maxSize = {
-  name: 'maxSize',
-  enabled: true,
-  phase: 'main',
-  requires: ['offset', 'preventOverflow', 'flip'],
-  fn({ state, name, options }: ModifierArguments<{}>) {
-    const overflow = detectOverflow(state, options);
-    const { x, y } = state.modifiersData.preventOverflow || { x: 0, y: 0 };
-    const { width, height } = state.rects.popper;
-    const [basePlacement] = state.placement.split('-');
-
-    const widthProp = basePlacement === 'left' ? 'left' : 'right';
-    const heightProp = basePlacement === 'top' ? 'top' : 'bottom';
-
-    state.modifiersData[name] = {
-      width: width - overflow[widthProp] - x,
-      height: height - overflow[heightProp] - y,
-    };
-  },
-};
-
-const applyMaxSize = {
-  name: 'applyMaxSize',
-  enabled: true,
-  phase: 'beforeWrite',
-  requires: ['maxSize'],
-  fn({ state }: ModifierArguments<{}>) {
-    const { height } = state.modifiersData.maxSize;
-    state.styles.popper.maxHeight = `${height - MODAL_MARGIN}px`;
-    state.styles.popper.minHeight = `${FLIP_THRESHOLD}px`;
-  },
 };
 
 export function DataSourceDropdown(props: DataSourceDropdownProps) {
@@ -110,22 +75,14 @@ export function DataSourceDropdown(props: DataSourceDropdownProps) {
   const popper = usePopper(markerElement, selectorElement, {
     placement: 'bottom-start',
     modifiers: [
-      // @ts-ignore
-      maxSize,
-      // @ts-ignore
-      applyMaxSize,
-      {
-        name: 'flip',
-        options: {
-          fallbackPlacements: ['top-start'],
-        },
-      },
       {
         name: 'offset',
         options: {
           offset: [0, 4],
         },
       },
+      maxSize,
+      applyMaxSize,
     ],
   });
 
