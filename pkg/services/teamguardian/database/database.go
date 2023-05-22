ca17934645efc4a122f1139d00b283dd0ee7938a
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2023-05-22 14:44:01 +0100

    remove CanAdmin method from team guardian

--- pkg/services/teamguardian/database/database.go
+++ pkg/services/teamguardian/database/database.go
@@ -16,15 +16,6 @@ func ProvideTeamGuardianStore(sqlStore db.DB, teamService team.Service) *TeamGua
 	return &TeamGuardianStoreImpl{sqlStore: sqlStore, teamService: teamService}
 }
 
-func (t *TeamGuardianStoreImpl) GetTeamMembers(ctx context.Context, query team.GetTeamMembersQuery) ([]*team.TeamMemberDTO, error) {
-	queryResult, err := t.teamService.GetTeamMembers(ctx, &query)
-	if err != nil {
-		return nil, err
-	}
-
-	return queryResult, nil
-}
-
 func (t *TeamGuardianStoreImpl) DeleteByUser(ctx context.Context, userID int64) error {
 	return t.sqlStore.WithTransactionalDbSession(ctx, func(sess *db.Session) error {
 		var rawSQL = "DELETE FROM team_member WHERE user_id = ?"
