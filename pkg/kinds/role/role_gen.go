765edca2b79d4af832a13cd90f9a090f237196f4
Ryan McKinley <ryantxu@gmail.com>
2023-05-22 16:11:52 -0700

    generate types

--- pkg/kinds/role/role_gen.go
+++ pkg/kinds/role/role_gen.go
@@ -12,6 +12,6 @@ package role
 // Resource is the wire representation of Role. (TODO be better)
 type Resource struct {
 	Metadata Metadata `json:"metadata"`
-	Spec     Spec     `json:"spec"`
-	Status   Status   `json:"status"`
+	Spec Spec `json:"spec"`
+	Status Status `json:"status"`
 }
