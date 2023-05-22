0c0c4209127eed4361580ddb0411de5ac488e36a
Ashley Harrison <ashley.harrison@grafana.com>
2023-05-22 11:20:38 +0100

    use an effect

--- public/app/plugins/datasource/opentsdb/components/OpenTsdbQueryEditor.tsx
+++ public/app/plugins/datasource/opentsdb/components/OpenTsdbQueryEditor.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { GrafanaTheme2, QueryEditorProps, textUtil } from '@grafana/data';
 import { useStyles2 } from '@grafana/ui';
@@ -62,17 +62,21 @@ export function OpenTsdbQueryEditor({
     query.downsampleFillPolicy = 'none';
   }
 
-  datasource.getAggregators().then((aggs: string[]) => {
-    if (aggs.length !== 0 && aggs !== aggregators) {
-      setAggregators(aggs);
-    }
-  });
+  useEffect(() => {
+    datasource.getAggregators().then((aggs: string[]) => {
+      if (aggs.length !== 0) {
+        setAggregators(aggs);
+      }
+    });
+  }, [datasource]);
 
-  datasource.getFilterTypes().then((newFilterTypes: string[]) => {
-    if (newFilterTypes.length !== 0 && newFilterTypes !== filterTypes) {
-      setFilterTypes(newFilterTypes);
-    }
-  });
+  useEffect(() => {
+    datasource.getFilterTypes().then((newFilterTypes: string[]) => {
+      if (newFilterTypes.length !== 0) {
+        setFilterTypes(newFilterTypes);
+      }
+    });
+  }, [datasource]);
 
   async function suggestMetrics(value: string): Promise<Array<{ value: string; description: string }>> {
     return datasource.metricFindQuery(`metrics(${value})`).then(getTextValues);
