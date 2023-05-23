843ef16f036f838199b640bc0d13345898af7df2
yangkb09 <yangkb09@gmail.com>
2023-05-23 12:54:19 -0400

    test: add test for check when all modules have owners

--- dummy/modowners_test.go
+++ dummy/modowners_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"log"
+	"testing"
+	"testing/fstest"
+)
 
 func TestCommonElement(t *testing.T) {
 	for _, test := range []struct {
@@ -16,5 +21,39 @@ func TestCommonElement(t *testing.T) {
 		if hasCommonElement(test.A, test.B) != test.Result {
 			t.Error(test)
 		}
+	}
+}
+
+func TestCheck(t *testing.T) {
+	buf := &bytes.Buffer{}        // NOTE: empty buffer, growing list of bytes
+	logger := log.New(buf, "", 0) // NOTE: uses buffer as writer, appends data to buffer
+	filesystem := fstest.MapFS{"go.txd": &fstest.MapFile{Data: []byte(`
+	require (
+		cloud.google.com/go/storage v1.28.1 // @delivery
+		cuelang.org/go v0.5.0 // @as-code @backend-platform
+		github.com/Azure/azure-sdk-for-go v65.0.0+incompatible // indirect, @delivery
+		github.com/Masterminds/semver v1.5.0 // @delivery @backend-platform
+	)
+	`)}}
+	err := check(filesystem, logger, []string{"go.txd"})
+	if err != nil {
+		t.Error(err, buf.String()) // NOTE: print output of check cmd
+	}
+}
+
+func TestModules(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logger := log.New(buf, "", 0)
+	filesystem := fstest.MapFS{"go.txd": &fstest.MapFile{Data: []byte(`
+	require (
+		cloud.google.com/go/storage v1.28.1
+		cuelang.org/go v0.5.0 // @as-code @backend-platform
+		github.com/Azure/azure-sdk-for-go v65.0.0+incompatible // indirect, @delivery
+		github.com/Masterminds/semver v1.5.0 // @delivery @backend-platform
+	)
+	`)}}
+	err := modules(filesystem, logger, []string{"go.txd", "-i", "-o", "@delivery"}) // NOTE: pass various flags, these are cmd line arguments
+	if err != nil {
+		t.Error(err, buf.String()) // NOTE: print output of check cmd
 	}
 }
