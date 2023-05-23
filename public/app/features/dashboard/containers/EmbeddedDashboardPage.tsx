6ea3cde2fa1c9229fba5fc2fc28cab29113ba9da
Clarity-89 <homes89@ukr.net>
2023-05-23 09:46:33 +0300

    Dashboard embed: Add toolbar

--- public/app/features/dashboard/containers/EmbeddedDashboardPage.tsx
+++ public/app/features/dashboard/containers/EmbeddedDashboardPage.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react';
 
 import { PageLayoutType } from '@grafana/data';
+import { TimeZone } from '@grafana/schema';
+import { PageToolbar } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 import { useGrafana } from 'app/core/context/GrafanaContext';
 import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
 import { useDispatch, useSelector } from 'app/types';
 
+import { updateTimeZoneForSession } from '../../profile/state/reducers';
+import { DashNavTimeControls } from '../components/DashNav/DashNavTimeControls';
 import { DashboardFailed } from '../components/DashboardLoading/DashboardFailed';
 import { DashboardLoading } from '../components/DashboardLoading/DashboardLoading';
 import { DashboardGrid } from '../dashgrid/DashboardGrid';
+import { DashboardModel } from '../state';
 import { initDashboard } from '../state/initDashboard';
 
 interface EmbeddedDashboardPageRouteParams {
@@ -56,6 +61,7 @@ export default function EmbeddedDashboardPage({ match, route }: Props) {
 
   return (
     <Page pageNav={{ text: dashboard.title }} layout={PageLayoutType.Custom}>
+      <Toolbar dashboard={dashboard} />
       {dashboardState.initError && <DashboardFailed initError={dashboardState.initError} />}
       <div className={''}>
         <DashboardGrid dashboard={dashboard} isEditable={false} viewPanel={null} editPanel={null} hidePanelMenus />
@@ -63,3 +69,19 @@ export default function EmbeddedDashboardPage({ match, route }: Props) {
     </Page>
   );
 }
+
+const Toolbar = ({ dashboard }: { dashboard: DashboardModel }) => {
+  const dispatch = useDispatch();
+
+  const onChangeTimeZone = (timeZone: TimeZone) => {
+    dispatch(updateTimeZoneForSession(timeZone));
+  };
+
+  return (
+    <PageToolbar title={dashboard.title} buttonOverflowAlignment="right">
+      {!dashboard.timepicker.hidden && (
+        <DashNavTimeControls dashboard={dashboard} onChangeTimeZone={onChangeTimeZone} />
+      )}
+    </PageToolbar>
+  );
+};
