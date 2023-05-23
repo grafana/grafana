ff83ae36acb8a5521baf45ca604b7c13d38a0345
Clarity-89 <homes89@ukr.net>
2023-05-23 09:26:38 +0300

    Dashboard embed: Separate routes

--- public/app/features/dashboard/routes.ts
+++ public/app/features/dashboard/routes.ts
@@ -4,39 +4,40 @@ import { RouteDescriptor } from '../../core/navigation/types';
 import { DashboardRoutes } from '../../types';
 
 export const getPublicDashboardRoutes = (): RouteDescriptor[] => {
-  const routes: RouteDescriptor[] = [];
   if (config.featureToggles.publicDashboards) {
-    routes.push(
-      ...[
-        {
-          path: '/dashboard/public',
-          pageClass: 'page-dashboard',
-          routeName: DashboardRoutes.Public,
-          component: SafeDynamicImport(
-            () =>
-              import(
-                /* webpackChunkName: "ListPublicDashboardPage" */ '../../features/manage-dashboards/PublicDashboardListPage'
-              )
-          ),
-        },
-        {
-          path: '/public-dashboards/:accessToken',
-          pageClass: 'page-dashboard',
-          routeName: DashboardRoutes.Public,
-          chromeless: true,
-          component: SafeDynamicImport(
-            () =>
-              import(
-                /* webpackChunkName: "PublicDashboardPage" */ '../../features/dashboard/containers/PublicDashboardPage'
-              )
-          ),
-        },
-      ]
-    );
+    return [
+      {
+        path: '/dashboard/public',
+        pageClass: 'page-dashboard',
+        routeName: DashboardRoutes.Public,
+        component: SafeDynamicImport(
+          () =>
+            import(
+              /* webpackChunkName: "ListPublicDashboardPage" */ '../../features/manage-dashboards/PublicDashboardListPage'
+            )
+        ),
+      },
+      {
+        path: '/public-dashboards/:accessToken',
+        pageClass: 'page-dashboard',
+        routeName: DashboardRoutes.Public,
+        chromeless: true,
+        component: SafeDynamicImport(
+          () =>
+            import(
+              /* webpackChunkName: "PublicDashboardPage" */ '../../features/dashboard/containers/PublicDashboardPage'
+            )
+        ),
+      },
+    ];
   }
+  return [];
+};
+
+export const getEmbeddedDashboardRoutes = (): RouteDescriptor[] => {
   // TODO add feature toggle
-  if (true) {
-    routes.push({
+  return [
+    {
       path: '/d-embed/:uid',
       pageClass: 'dashboard-embed',
       routeName: DashboardRoutes.Embed,
@@ -46,8 +47,6 @@ export const getPublicDashboardRoutes = (): RouteDescriptor[] => {
             /* webpackChunkName: "EmbeddedDashboardPage" */ '../../features/dashboard/containers/EmbeddedDashboardPage'
           )
       ),
-    });
-  }
-
-  return routes;
+    },
+  ];
 };
--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -19,7 +19,7 @@ import { AccessControlAction, DashboardRoutes } from 'app/types';
 
 import { SafeDynamicImport } from '../core/components/DynamicImports/SafeDynamicImport';
 import { RouteDescriptor } from '../core/navigation/types';
-import { getPublicDashboardRoutes } from '../features/dashboard/routes';
+import { getEmbeddedDashboardRoutes, getPublicDashboardRoutes } from '../features/dashboard/routes';
 
 export const extraRoutes: RouteDescriptor[] = [];
 
@@ -508,6 +508,7 @@ export function getAppRoutes(): RouteDescriptor[] {
     ...extraRoutes,
     ...getPublicDashboardRoutes(),
     ...getDataConnectionsRoutes(),
+    ...getEmbeddedDashboardRoutes(),
     {
       path: '/*',
       component: PageNotFound,
