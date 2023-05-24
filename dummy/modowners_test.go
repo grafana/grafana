57734d1a537f0877b7bc0b6b4c9c48027fd0f8dd
yangkb09 <yangkb09@gmail.com>
2023-05-24 14:34:08 -0400

    test: add TestModules to test modules func without any flags returns direct dependencies

--- dummy/modowners_test.go
+++ dummy/modowners_test.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/stretchr/testify/assert"
 	"log"
 	"strings"
 	"testing"
@@ -46,18 +44,8 @@ func TestCheck(t *testing.T) {
 }
 
 func TestModules(t *testing.T) {
-	// Test case: no flags, print all direct dependencies
-	// logger := log.New(os.Stderr, "", log.LstdFlags)
-	// buf := &bytes.Buffer{}
-	// mockLogger := log.New(buf, "", log.LstdFlags)
-	// logger = mocks.mockLogger
-
 	buf := &bytes.Buffer{}
 	logger := log.New(buf, "", 0)
-	fmt.Println("LOGGER HERE", logger)
-	logs := strings.Split(buf.String(), "\n")
-	fmt.Println("LOGS HERE", logs)
-
 	filesystem := fstest.MapFS{"go.txd": &fstest.MapFile{Data: []byte(`
 	require (
 		cloud.google.com/go/storage v1.28.1
@@ -66,21 +54,26 @@ func TestModules(t *testing.T) {
 		github.com/Masterminds/semver v1.5.0 // @delivery @backend-platform
 	)
 	`)}}
+
 	err := modules(filesystem, logger, []string{"-m", "go.txd"}) // NOTE: pass various flags, these are cmd line arguments
 	if err != nil {
-		t.Error(err, buf.String()) // NOTE: print output of check cmd
+		t.Error(err, buf.String())
 	}
 
+	logs := buf.String()
+
 	// Expected results
-	expectedResults := []string{
+	expectedModules := []string{
 		"cloud.google.com/go/storage v1.28.1",
 		"cuelang.org/go v0.5.0",
 		"github.com/Azure/azure-sdk-for-go v65.0.0+incompatible",
 		"github.com/Masterminds/semver v1.5.0",
 	}
 
-	// Compare expected results to actual logs
-	for _, module := range expectedResults {
-		assert.Contains(t, logs, module)
+	expectedResults := strings.Join(expectedModules, "\n")
+
+	// Compare logs to expected results
+	if logs != expectedResults {
+		t.Error(err)
 	}
 }
