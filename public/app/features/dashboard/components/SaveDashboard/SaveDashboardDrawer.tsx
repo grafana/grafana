54a50791114906ec98b5bd459354b56c02b5e48c
Torkel Ödegaard <torkel@grafana.com>
2023-05-25 16:41:02 +0200

    Drawer: Fixes closeOnMaskClick false issue

--- public/app/features/dashboard/components/SaveDashboard/SaveDashboardDrawer.tsx
+++ public/app/features/dashboard/components/SaveDashboard/SaveDashboardDrawer.tsx
@@ -132,6 +132,7 @@ export const SaveDashboardDrawer = ({ dashboard, onDismiss, onSaveSuccess, isCop
       title={title}
       onClose={onDismiss}
       subtitle={dashboard.title}
+      closeOnMaskClick={false}
       tabs={
         <TabsBar>
           <Tab label={'Details'} active={!showDiff} onChangeTab={() => setShowDiff(false)} />
