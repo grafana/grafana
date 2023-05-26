2b98cef0bb2f3ee72bbaf7455c324ee5dbebd1f4
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2023-05-26 16:55:14 +0400

    lint

--- pkg/infra/filestorage/fs_integration_test.go
+++ pkg/infra/filestorage/fs_integration_test.go
@@ -170,7 +170,6 @@ func runTests(createCases func() []fsTestCase, t *testing.T) {
 }
 
 func TestIntegrationFsStorage(t *testing.T) {
-
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
