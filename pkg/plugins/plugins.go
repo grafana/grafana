985d7e7b8e225f087e5b7600f2bb0929261c2249
Will Browne <will.browne@grafana.com>
2023-05-26 14:51:03 +0100

    simplify

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -98,10 +98,6 @@ func (p PluginDTO) IsApp() bool {
 	return p.Type == App
 }
 
-func (p PluginDTO) IsDataSource() bool {
-	return p.Type == DataSource
-}
-
 func (p PluginDTO) IsCorePlugin() bool {
 	return p.Class == Core
 }
