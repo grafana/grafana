26417916cf7a32790061fd1f8bc04c9a9bcb9ad5
Ivan Ortega <ivanortegaalba@gmail.com>
2023-05-29 12:10:37 +0200

    Fix typos

--- packages/grafana-schema/src/raw/dashboard/x/dashboard_types.gen.ts
+++ packages/grafana-schema/src/raw/dashboard/x/dashboard_types.gen.ts
@@ -554,7 +554,7 @@ export interface Panel {
    * which causes them to be different than what is selected in
    * the dashboard time picker in the top-right corner of the dashboard. You can use this to show metrics from different
    * time periods or days on the same dashboard.
-   * The value is formated as time operation like: `now-5m` (Last 5 minutes), `now/d` (the day so far),
+   * The value is formatted as time operation like: `now-5m` (Last 5 minutes), `now/d` (the day so far),
    * `now-5d/d`(Last 5 days), `now/w` (This week so far), `now-2y/y` (Last 2 years).
    * Note: Panel time overrides have no effect when the dashboard’s time range is absolute.
    * See: https://grafana.com/docs/grafana/latest/panels-visualizations/query-transform-data/#query-options
