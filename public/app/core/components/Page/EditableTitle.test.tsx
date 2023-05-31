e563d9672ac8fb13ff2c54edb62eb0d3a653fed1
Ashley Harrison <ashley.harrison@grafana.com>
2023-05-31 11:33:22 +0100

    don't use autosavefield

--- public/app/core/components/Page/EditableTitle.test.tsx
+++ public/app/core/components/Page/EditableTitle.test.tsx
@@ -2,6 +2,8 @@ import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
+import { FetchError } from '@grafana/runtime';
+
 import { EditableTitle } from './EditableTitle';
 
 describe('EditableTitle', () => {
@@ -43,7 +45,7 @@ describe('EditableTitle', () => {
     expect(document.activeElement).toBe(screen.getByRole('textbox'));
   });
 
-  it('blurring the input calls the onChange callback and reverts back to text', async () => {
+  it('blurring the input calls the onEdit callback and reverts back to text', async () => {
     render(<EditableTitle value={value} onEdit={mockEdit} />);
 
     const editButton = screen.getByRole('button', { name: 'Edit title' });
@@ -52,14 +54,9 @@ describe('EditableTitle', () => {
     const input = screen.getByRole('textbox');
     await user.clear(input);
     await user.type(input, 'New value');
-    act(() => {
-      jest.runAllTimers();
-    });
-
-    expect(mockEdit).toHaveBeenCalledWith('New value');
-    expect(await screen.findByText('Saved!')).toBeInTheDocument();
 
     await user.click(document.body);
+    expect(mockEdit).toHaveBeenCalledWith('New value');
     act(() => {
       jest.runAllTimers();
     });
@@ -68,5 +65,83 @@ describe('EditableTitle', () => {
       expect(screen.getByRole('heading')).toBeInTheDocument();
       expect(screen.getByRole('button', { name: 'Edit title' })).toBeInTheDocument();
     });
+  });
+
+  it('pressing enter calls the onEdit callback and reverts back to text', async () => {
+    render(<EditableTitle value={value} onEdit={mockEdit} />);
+
+    const editButton = screen.getByRole('button', { name: 'Edit title' });
+    await user.click(editButton);
+
+    const input = screen.getByRole('textbox');
+    await user.clear(input);
+    await user.type(input, 'New value');
+
+    await user.keyboard('{enter}');
+    expect(mockEdit).toHaveBeenCalledWith('New value');
+    act(() => {
+      jest.runAllTimers();
+    });
+    await waitFor(() => {
+      expect(screen.queryByRole('textbox')).not.toBeInTheDocument();
+      expect(screen.getByRole('heading')).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: 'Edit title' })).toBeInTheDocument();
+    });
+  });
+
+  it('displays an error message when attempting to save an empty value', async () => {
+    render(<EditableTitle value={value} onEdit={mockEdit} />);
+
+    const editButton = screen.getByRole('button', { name: 'Edit title' });
+    await user.click(editButton);
+
+    const input = screen.getByRole('textbox');
+    await user.clear(input);
+    await user.keyboard('{enter}');
+
+    expect(screen.getByText('Please enter a title')).toBeInTheDocument();
+  });
+
+  it('displays a regular error message', async () => {
+    const mockEditError = jest.fn().mockImplementation(() => {
+      throw new Error('Uh oh spaghettios');
+    });
+    render(<EditableTitle value={value} onEdit={mockEditError} />);
+
+    const editButton = screen.getByRole('button', { name: 'Edit title' });
+    await user.click(editButton);
+
+    const input = screen.getByRole('textbox');
+    await user.clear(input);
+    await user.type(input, 'New value');
+
+    await user.keyboard('{enter}');
+    expect(screen.getByText('Uh oh spaghettios')).toBeInTheDocument();
+  });
+
+  it('displays a detailed fetch error message', async () => {
+    const mockEditError = jest.fn().mockImplementation(() => {
+      const fetchError: FetchError = {
+        status: 500,
+        config: {
+          url: '',
+        },
+        data: {
+          message: 'Uh oh spaghettios a fetch error',
+        },
+      };
+      throw fetchError;
+    });
+    render(<EditableTitle value={value} onEdit={mockEditError} />);
+
+    const editButton = screen.getByRole('button', { name: 'Edit title' });
+    await user.click(editButton);
+
+    const input = screen.getByRole('textbox');
+    await user.clear(input);
+    await user.type(input, 'New value');
+
+    await user.keyboard('{enter}');
+    expect(screen.getByText('Uh oh spaghettios a fetch error')).toBeInTheDocument();
   });
 });
--- public/app/core/components/Page/EditableTitle.tsx
+++ public/app/core/components/Page/EditableTitle.tsx
@@ -3,7 +3,7 @@ import React, { useCallback, useRef, useState } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { isFetchError } from '@grafana/runtime';
-import { AutoSaveField, IconButton, Input, useStyles2 } from '@grafana/ui';
+import { Field, Icon, IconButton, Input, Spinner, useStyles2 } from '@grafana/ui';
 import { H1 } from '@grafana/ui/src/unstable';
 
 export interface Props {
@@ -14,62 +14,90 @@ export interface Props {
 export const EditableTitle = ({ value, onEdit }: Props) => {
   const styles = useStyles2(getStyles);
   const [isEditing, setIsEditing] = useState(false);
+  const [isEditInProgress, setIsEditInProgress] = useState(false);
+  // we use this to track whether the edit was successful so we can show a checkmark icon
+  // this should get reset to false whenever we exit edit mode
+  const [isEditSuccessful, setIsEditSuccessful] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string>();
-  const [changeInProgress, setChangeInProgress] = useState(false);
   const timeoutID = useRef<number>();
 
-  const onFinishChange = useCallback(
-    async (newValue: string) => {
-      try {
-        await onEdit(newValue);
-      } catch (error) {
-        if (isFetchError(error)) {
-          setErrorMessage(error.data.message);
-        } else if (error instanceof Error) {
-          setErrorMessage(error.message);
+  const exitEditMode = () => {
+    setIsEditing(false);
+    setIsEditSuccessful(false);
+  };
+
+  const onCommitChange = useCallback(
+    async (event: React.FormEvent<HTMLInputElement>) => {
+      const newValue = event.currentTarget.value;
+
+      if (!newValue) {
+        setErrorMessage('Please enter a title');
+      } else if (newValue === value) {
+        // no need to bother saving if the value hasn't changed
+        // just clear any previous error messages and exit edit mode
+        setErrorMessage(undefined);
+        exitEditMode();
+      } else {
+        setIsEditInProgress(true);
+        if (timeoutID.current) {
+          window.clearTimeout(timeoutID.current);
         }
-        throw error;
+        try {
+          await onEdit(newValue);
+          setErrorMessage(undefined);
+          setIsEditSuccessful(true);
+          timeoutID.current = window.setTimeout(() => {
+            exitEditMode();
+          }, 1500);
+        } catch (error) {
+          if (isFetchError(error)) {
+            setErrorMessage(error.data.message);
+          } else if (error instanceof Error) {
+            setErrorMessage(error.message);
+          }
+        }
+        setIsEditInProgress(false);
       }
-      timeoutID.current = window.setTimeout(() => {
-        setChangeInProgress(false);
-      }, 2000);
     },
-    [onEdit]
+    [onEdit, value]
   );
 
-  return !isEditing && !changeInProgress ? (
+  const getIcon = useCallback(() => {
+    if (isEditInProgress) {
+      return <Spinner />;
+    } else if (isEditSuccessful) {
+      return <Icon name="check" />;
+    } else {
+      return null;
+    }
+  }, [isEditInProgress, isEditSuccessful]);
+
+  return !isEditing ? (
     <div className={styles.textContainer}>
-      {!isEditing && !changeInProgress && (
-        <div className={styles.textWrapper}>
-          <H1 truncate>{value}</H1>
-          <IconButton name="pen" size="lg" tooltip="Edit title" onClick={() => setIsEditing(true)} />
-        </div>
-      )}
+      <div className={styles.textWrapper}>
+        <H1 truncate>{value}</H1>
+        <IconButton name="pen" size="lg" tooltip="Edit title" onClick={() => setIsEditing(true)} />
+      </div>
     </div>
   ) : (
     <div className={styles.inputContainer}>
-      <AutoSaveField saveErrorMessage={errorMessage} className={styles.field} onFinishChange={onFinishChange}>
-        {(onChange) => (
-          <Input
-            className={styles.input}
-            defaultValue={value}
-            onChange={(event) => {
-              if (event.currentTarget.value) {
-                setChangeInProgress(true);
-                if (timeoutID.current) {
-                  window.clearTimeout(timeoutID.current);
-                }
-                onChange(event.currentTarget.value);
-              }
-            }}
-            // perfectly reasonable to autofocus here since we've made a conscious choice by clicking the edit button
-            // eslint-disable-next-line jsx-a11y/no-autofocus
-            autoFocus
-            onBlur={() => setIsEditing(false)}
-            onFocus={() => setIsEditing(true)}
-          />
-        )}
-      </AutoSaveField>
+      <Field className={styles.field} invalid={!!errorMessage} error={errorMessage}>
+        <Input
+          className={styles.input}
+          defaultValue={value}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              onCommitChange(event);
+            }
+          }}
+          // perfectly reasonable to autofocus here since we've made a conscious choice by clicking the edit button
+          // eslint-disable-next-line jsx-a11y/no-autofocus
+          autoFocus
+          suffix={getIcon()}
+          onBlur={onCommitChange}
+          onFocus={() => setIsEditing(true)}
+        />
+      </Field>
     </div>
   );
 };
@@ -83,14 +111,15 @@ const getStyles = (theme: GrafanaTheme2) => {
     }),
     field: css({
       flex: 1,
+      // magic number here to ensure the input text lines up exactly with the h1 text
+      // input has a 1px border + theme.spacing(1) padding so we need to offset that
+      left: `calc(-${theme.spacing(1)} - 1px)`,
+      position: 'relative',
       marginBottom: 0,
     }),
     input: css({
       input: {
         ...theme.typography.h1,
-        // magic number here to ensure the input text lines up exactly with the h1 text
-        // input has a 1px border + theme.spacing(1) padding so we need to offset that
-        left: `calc(-${theme.spacing(1)} - 1px)`,
       },
     }),
     inputContainer: css({
