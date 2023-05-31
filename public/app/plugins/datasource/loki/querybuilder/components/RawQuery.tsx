f91323249e52eaf3ca7cc2641918d8134df3fd94
Matias Chomicki <matyax@gmail.com>
2023-05-31 16:12:35 +0200

    Use tooltip to inform query validity

--- public/app/plugins/datasource/loki/querybuilder/components/RawQuery.tsx
+++ public/app/plugins/datasource/loki/querybuilder/components/RawQuery.tsx
@@ -1,14 +1,15 @@
 import { css, cx } from '@emotion/css';
 import Prism from 'prismjs';
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data/src';
-import { useTheme2 } from '@grafana/ui/src';
+import { Alert, Tooltip, useTheme2 } from '@grafana/ui/src';
 
 import {
   highlightErrorsInQuery,
   placeHolderScopedVars,
   processErrorTokens,
+  validateQuery,
 } from '../../components/monaco-query-field/monaco-completion-provider/validation';
 import { LokiDatasource } from '../../datasource';
 import { lokiGrammar } from '../../syntax';
@@ -22,35 +23,43 @@ export interface Props {
 export function RawQuery({ query, className, datasource }: Props) {
   const theme = useTheme2();
   const styles = getStyles(theme);
-  const highlighted = Prism.highlight(
-    highlightErrorsInQuery(datasource.interpolateString(query, placeHolderScopedVars)),
-    lokiGrammar,
-    'lokiql'
+  const interpolatedQuery = useMemo(
+    () => datasource.interpolateString(query, placeHolderScopedVars),
+    [datasource, query]
   );
+  const highlighted = Prism.highlight(highlightErrorsInQuery(query, interpolatedQuery), lokiGrammar, 'lokiql');
+  const hasErrors = useMemo(() => validateQuery(query, interpolatedQuery, [query]), [query, interpolatedQuery]);
 
-  Prism.hooks.add('after-tokenize', function (env) {
-    if (env.language !== 'lokiql') {
-      return;
-    }
+  useEffect(() => {
+    Prism.hooks.add('after-tokenize', function (env) {
+      if (env.language !== 'lokiql') {
+        return;
+      }
 
-    env.tokens = processErrorTokens(env.tokens);
-    console.log(env.tokens);
-  });
-  Prism.hooks.add('wrap', (env) => {
-    if (env.language !== 'lokiql') {
-      return;
-    }
-    if (env.type === 'error') {
-      env.classes.push(styles.queryError);
-    }
-  });
+      env.tokens = processErrorTokens(env.tokens);
+    });
+    Prism.hooks.add('wrap', (env) => {
+      if (env.language !== 'lokiql') {
+        return;
+      }
+      if (env.type === 'error') {
+        env.classes.push(styles.queryError);
+      }
+    });
+  }, [styles.queryError]);
 
   return (
-    <div
-      className={cx(styles.editorField, 'prism-syntax-highlight', className)}
-      aria-label="selector"
-      dangerouslySetInnerHTML={{ __html: highlighted }}
-    />
+    <>
+      <Tooltip
+        content={hasErrors ? 'The query appears to be incorrect and could fail to be executed' : 'The query is valid'}
+      >
+        <div
+          className={cx(styles.editorField, 'prism-syntax-highlight', className)}
+          aria-label="selector"
+          dangerouslySetInnerHTML={{ __html: highlighted }}
+        />
+      </Tooltip>
+    </>
   );
 }
 
