21021502b5577eef6f84617d4f2ddd9c3c98d61a
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2023-06-02 13:26:20 +0200

    Loki: Remove unused datasource prop from LokiQueryBuilderOptions (#69445)
    Loki: Remove unused props from LokiQueryBuilderOptions

--- public/app/plugins/datasource/loki/components/LokiQueryEditor.tsx
+++ public/app/plugins/datasource/loki/components/LokiQueryEditor.tsx
@@ -203,7 +203,6 @@ export const LokiQueryEditor = React.memo<LokiQueryEditorProps>((props) => {
           onRunQuery={onRunQuery}
           app={app}
           maxLines={datasource.maxLines}
-          datasource={datasource}
           queryStats={queryStats}
         />
       </EditorRows>
