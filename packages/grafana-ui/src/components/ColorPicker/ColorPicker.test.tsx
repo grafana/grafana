bc97c3b75549c28100ad243c57094ee5f70d919a
Pierre <pierre@meo.life>
2023-06-02 08:54:33 +0200

    Fix typecheck

--- packages/grafana-ui/src/components/ColorPicker/ColorPicker.test.tsx
+++ packages/grafana-ui/src/components/ColorPicker/ColorPicker.test.tsx
@@ -17,11 +17,11 @@ describe('ColorPicker', () => {
     render(<ColorPicker color="blue" enableNamedColors onChange={() => {}} />);
     const mainButton = screen.getAllByRole('button');
     expect(mainButton.length).toBe(1);
-    mainButton.forEach((button) => expect(button.type).toBe('button'));
+    mainButton.forEach((button) => expect(button).toHaveAttribute('type', 'button'));
     await userEvent.click(mainButton[0]);
     const buttons = screen.getAllByRole('button');
     expect(buttons.length).toBe(35);
-    buttons.forEach((button) => expect(button.type).toBe('button'));
+    buttons.forEach((button) => expect(button).toHaveAttribute('type', 'button'));
   });
 
   it('renders custom trigger when supplied', () => {
