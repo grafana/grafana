9d5661c4f79b57ea0451b36edf786b492d74f870
Todd Treece <todd.treece@grafana.com>
2023-06-07 08:44:05 -0400

    add schema validate

---
+++ pkg/services/k8s/apiserver/admission/schema_validate.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package admission
+
+import (
+	"context"
+	"io"
+
+	"k8s.io/apiserver/pkg/admission"
+)
+
+const PluginNameSchemaValidate = "SchemaValidate"
+
+// Register registers a plugin
+func RegisterSchemaValidte(plugins *admission.Plugins) {
+	plugins.Register(PluginNameSchemaValidate, func(config io.Reader) (admission.Interface, error) {
+		return NewDenyByName(), nil
+	})
+}
+
+type schemaValidate struct{}
+
+var _ admission.ValidationInterface = schemaValidate{}
+
+// Validate makes an admission decision based on the request attributes.  It is NOT allowed to mutate.
+func (schemaValidate) Validate(ctx context.Context, a admission.Attributes, o admission.ObjectInterfaces) (err error) {
+	return nil
+}
+
+// Handles returns true if this admission controller can handle the given operation
+// where operation can be one of CREATE, UPDATE, DELETE, or CONNECT
+func (schemaValidate) Handles(operation admission.Operation) bool {
+	return true
+}
+
+// NewDenyByName creates an always deny admission handler
+func NewSchemaValidate() admission.Interface {
+	return new(schemaValidate)
+}
