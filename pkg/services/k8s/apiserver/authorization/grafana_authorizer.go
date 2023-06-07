21e986c9af60aa9f668c00d203068cdd06e11bd5
Charandas Batra <charandas.batra@grafana.com>
2023-06-07 13:16:26 -0700

    reduce nesting, fail early in unhappy cases

--- pkg/services/k8s/apiserver/authorization/grafana_authorizer.go
+++ pkg/services/k8s/apiserver/authorization/grafana_authorizer.go
@@ -24,45 +24,49 @@ func NewGrafanaAuthorizer() *GrafanaAuthorizer {
 }
 
 func (auth GrafanaAuthorizer) Authorize(ctx context.Context, a authorizer.Attributes) (authorized authorizer.Decision, reason string, err error) {
-	if userInfo, ok := request.UserFrom(ctx); ok {
-		extra := userInfo.GetExtra()
-		groups := userInfo.GetGroups()
+	userInfo, ok := request.UserFrom(ctx);
+	if !ok {
+		return authorizer.DecisionDeny, fmt.Sprintf("could not determine user info from context, denied %s access on requested resource=%s, path=%s", a.GetVerb(), a.GetResource(), a.GetPath()), nil
+	}
+	extra := userInfo.GetExtra()
+	groups := userInfo.GetGroups()
 
-		if orgId := extra["org-id"]; len(orgId) > 0 {
-			// We are being called using a Grafana token
-			if orgRole := extra["org-role"]; len(orgRole) > 0 {
-				switch org.RoleType(orgRole[0]) {
-				case org.RoleAdmin:
-					return authorizer.DecisionAllow, "", nil
-				case org.RoleEditor:
-					switch a.GetVerb() {
-					case "get", "list", "watch", "create", "update", "patch", "delete", "put", "post":
-						return authorizer.DecisionAllow, "", nil
-					default:
-						return authorizer.DecisionDeny, errorMessageForGrafanaOrgRole(orgRole[0], a), nil
-					}
-				case org.RoleViewer:
-					switch a.GetVerb() {
-					case "get", "list", "watch":
-						return authorizer.DecisionAllow, "", nil
-					default:
-						return authorizer.DecisionDeny, errorMessageForGrafanaOrgRole(orgRole[0], a), nil
-					}
-				default:
-					return authorizer.DecisionDeny, errorMessageForGrafanaOrgRole(orgRole[0], a), nil
-				}
+	// TODO: take orgId into account in relation to namespaced resources
+	// if orgId := extra["org-id"]; len(orgId) > 0 {}
+	
+	// We are being called using a Grafana token
+	if orgRole := extra["org-role"]; len(orgRole) > 0 {
+		switch org.RoleType(orgRole[0]) {
+		case org.RoleAdmin:
+			return authorizer.DecisionAllow, "", nil
+		case org.RoleEditor:
+			switch a.GetVerb() {
+			case "get", "list", "watch", "create", "update", "patch", "delete", "put", "post":
+				return authorizer.DecisionAllow, "", nil
+			default:
+				return authorizer.DecisionDeny, errorMessageForGrafanaOrgRole(orgRole[0], a), nil
 			}
-		} else if groups != nil && len(groups) > 0 {
-			for _, gr := range groups {
-				if gr == "system:masters" {
-					// We are being called using LoopbackConfig / internal privileged access
-					return authorizer.DecisionAllow, "", nil
-				}
+		case org.RoleViewer:
+			switch a.GetVerb() {
+			case "get", "list", "watch":
+				return authorizer.DecisionAllow, "", nil
+			default:
+				return authorizer.DecisionDeny, errorMessageForGrafanaOrgRole(orgRole[0], a), nil
 			}
-			return authorizer.DecisionDeny, errorMessageForK8sGroup(groups, a), nil
-		} else {
-			fmt.Printf("Didn't match the K8s or Grafana code paths for groups=%v, extra=%v", groups, extra)
+		default:
+			return authorizer.DecisionDeny, errorMessageForGrafanaOrgRole(orgRole[0], a), nil
 		}
 	}
-	return authorizer.DecisionDeny, fmt.Sprintf("could not determine user info from context, denied %s access on requested resource=%s, path=%s", a.GetVerb(), a.GetResource(), a.GetPath()), nil
+
+	if groups != nil && len(groups) > 0 {
+		for _, gr := range groups {
+			if gr == "system:masters" {
+				// We are being called using LoopbackConfig / internal privileged access
+				return authorizer.DecisionAllow, "", nil
+			}
+		}
+		return authorizer.DecisionDeny, errorMessageForK8sGroup(groups, a), nil
+	}
+
+	return authorizer.DecisionDeny, fmt.Sprintf("could not match either of the authorizer's criteria, denied %s access on requested resource=%s, path=%s", a.GetVerb(), a.GetResource(), a.GetPath()), nil
 }
