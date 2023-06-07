744ca027b6a3d7f25f2db10a05494ec1b080fefd
Shivendra Sisodiya <shivendra@Shivendras-MacBook-Pro.local>
2023-06-07 10:42:59 +0530

    removed header toolbar from public dashboard

--- public/app/features/dashboard/containers/PublicDashboardPage.tsx
+++ public/app/features/dashboard/containers/PublicDashboardPage.tsx
@@ -2,23 +2,20 @@ import { css } from '@emotion/css';
 import React, { useEffect } from 'react';
 import { usePrevious } from 'react-use';
 
-import { GrafanaTheme2, PageLayoutType, TimeZone } from '@grafana/data';
+import { GrafanaTheme2, PageLayoutType } from '@grafana/data';
 import { selectors as e2eSelectors } from '@grafana/e2e-selectors/src';
-import { PageToolbar, useStyles2 } from '@grafana/ui';
+import { useStyles2 } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 import { useGrafana } from 'app/core/context/GrafanaContext';
 import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
-import { updateTimeZoneForSession } from 'app/features/profile/state/reducers';
 import { useSelector, useDispatch } from 'app/types';
 
-import { DashNavTimeControls } from '../components/DashNav/DashNavTimeControls';
 import { DashboardFailed } from '../components/DashboardLoading/DashboardFailed';
 import { DashboardLoading } from '../components/DashboardLoading/DashboardLoading';
 import { PublicDashboardFooter } from '../components/PublicDashboardFooter/PublicDashboardsFooter';
 import { PublicDashboardNotAvailable } from '../components/PublicDashboardNotAvailable/PublicDashboardNotAvailable';
 import { DashboardGrid } from '../dashgrid/DashboardGrid';
 import { getTimeSrv } from '../services/TimeSrv';
-import { DashboardModel } from '../state';
 import { initDashboard } from '../state/initDashboard';
 
 interface PublicDashboardPageRouteParams {
@@ -34,22 +31,6 @@ interface PublicDashboardPageRouteSearchParams {
 export type Props = GrafanaRouteComponentProps<PublicDashboardPageRouteParams, PublicDashboardPageRouteSearchParams>;
 
 const selectors = e2eSelectors.pages.PublicDashboard;
-
-const Toolbar = ({ dashboard }: { dashboard: DashboardModel }) => {
-  const dispatch = useDispatch();
-
-  const onChangeTimeZone = (timeZone: TimeZone) => {
-    dispatch(updateTimeZoneForSession(timeZone));
-  };
-
-  return (
-    <PageToolbar title={dashboard.title} buttonOverflowAlignment="right">
-      {!dashboard.timepicker.hidden && (
-        <DashNavTimeControls dashboard={dashboard} onChangeTimeZone={onChangeTimeZone} />
-      )}
-    </PageToolbar>
-  );
-};
 
 const PublicDashboardPage = (props: Props) => {
   const { match, route, location } = props;
@@ -105,7 +86,6 @@ const PublicDashboardPage = (props: Props) => {
 
   return (
     <Page pageNav={{ text: dashboard.title }} layout={PageLayoutType.Custom} data-testid={selectors.page}>
-      <Toolbar dashboard={dashboard} />
       {dashboardState.initError && <DashboardFailed initError={dashboardState.initError} />}
       <div className={styles.gridContainer}>
         <DashboardGrid dashboard={dashboard} isEditable={false} viewPanel={null} editPanel={null} hidePanelMenus />
