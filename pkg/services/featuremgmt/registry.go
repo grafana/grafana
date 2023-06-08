ccfc17af62173b038538d16ee99355f1d9b06b39
Alexander Zobnin <alexanderzobnin@gmail.com>
2023-06-08 17:38:27 +0200

    Authentication UI: Enable by default

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -490,7 +490,8 @@ var (
 		{
 			Name:        "authenticationConfigUI",
 			Description: "Enables authentication configuration UI",
-			State:       FeatureStateAlpha,
+			State:       FeatureStateStable,
+			Expression:  "true",
 			Owner:       grafanaAuthnzSquad,
 		},
 		{
