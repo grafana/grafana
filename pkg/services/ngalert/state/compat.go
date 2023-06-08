be8b10f5ee295cfa072da9666a8f0c9f6ee6f694
Matthew Jacobson <matthew.jacobson@grafana.com>
2023-06-08 18:59:54 -0400

    Alerting: Repurpose rule testing endpoint to return potential alerts (#69755)
    * Alerting: Repurpose rule testing endpoint to return potential alerts
    
    This feature replaces the existing no-longer in-use grafana ruler testing API endpoint /api/v1/rule/test/grafana. The new endpoint returns a list of potential alerts created by the given alert rule, including built-in + interpolated labels and annotations.
    
    The key priority of this endpoint is that it is intended to be as true as possible to what would be generated by the ruler except that the resulting alerts are not filtered to only Resolved / Firing and ready to be sent.
    
    This means that the endpoint will, among other things:
    
    - Attach static annotations and labels from the rule configuration to the alert instances.
    - Attach dynamic annotations from the datasource to the alert instances.
    - Attach built-in labels and annotations created by the Grafana Ruler (such as alertname and grafana_folder) to the alert instances.
    - Interpolate templated annotations / labels and accept allowed template functions.
    
    (cherry picked from commit ba3994d33877049909819add70cc3d1bf1033cc7)

--- pkg/services/ngalert/state/compat.go
+++ pkg/services/ngalert/state/compat.go
@@ -1,4 +1,4 @@
-package schedule
+package state
 
 import (
 	"encoding/json"
@@ -18,7 +18,6 @@ import (
 	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 	"github.com/grafana/grafana/pkg/services/ngalert/eval"
 	ngModels "github.com/grafana/grafana/pkg/services/ngalert/models"
-	"github.com/grafana/grafana/pkg/services/ngalert/state"
 )
 
 const (
@@ -28,13 +27,13 @@ const (
 	Rulename = "rulename"
 )
 
-// stateToPostableAlert converts a state to a model that is accepted by Alertmanager. Annotations and Labels are copied from the state.
+// StateToPostableAlert converts a state to a model that is accepted by Alertmanager. Annotations and Labels are copied from the state.
 // - if state has at least one result, a new label '__value_string__' is added to the label set
 // - the alert's GeneratorURL is constructed to point to the alert detail view
 // - if evaluation state is either NoData or Error, the resulting set of labels is changed:
 //   - original alert name (label: model.AlertNameLabel) is backed up to OriginalAlertName
 //   - label model.AlertNameLabel is overwritten to either NoDataAlertName or ErrorAlertName
-func stateToPostableAlert(alertState *state.State, appURL *url.URL) *models.PostableAlert {
+func StateToPostableAlert(alertState *State, appURL *url.URL) *models.PostableAlert {
 	nL := alertState.Labels.Copy()
 	nA := data.Labels(alertState.Annotations).Copy()
 
@@ -98,7 +97,7 @@ func stateToPostableAlert(alertState *state.State, appURL *url.URL) *models.Post
 // It effectively replaces the legacy behavior of "Keep Last State" by separating the regular alerting flow from the no data scenario into a separate alerts.
 // The Alert is defined as:
 // {  alertname=DatasourceNoData rulename=original_alertname } + { rule labelset } + { rule annotations }
-func noDataAlert(labels data.Labels, annotations data.Labels, alertState *state.State, urlStr string) *models.PostableAlert {
+func noDataAlert(labels data.Labels, annotations data.Labels, alertState *State, urlStr string) *models.PostableAlert {
 	if name, ok := labels[model.AlertNameLabel]; ok {
 		labels[Rulename] = name
 	}
@@ -117,7 +116,7 @@ func noDataAlert(labels data.Labels, annotations data.Labels, alertState *state.
 
 // errorAlert is a special alert sent when evaluation of an alert rule failed due to an error. Like noDataAlert, it
 // replaces the old behaviour of "Keep Last State" creating a separate alert called DatasourceError.
-func errorAlert(labels, annotations data.Labels, alertState *state.State, urlStr string) *models.PostableAlert {
+func errorAlert(labels, annotations data.Labels, alertState *State, urlStr string) *models.PostableAlert {
 	if name, ok := labels[model.AlertNameLabel]; ok {
 		labels[Rulename] = name
 	}
@@ -134,16 +133,16 @@ func errorAlert(labels, annotations data.Labels, alertState *state.State, urlStr
 	}
 }
 
-func FromStateTransitionToPostableAlerts(firingStates []state.StateTransition, stateManager *state.Manager, appURL *url.URL) apimodels.PostableAlerts {
+func FromStateTransitionToPostableAlerts(firingStates []StateTransition, stateManager *Manager, appURL *url.URL) apimodels.PostableAlerts {
 	alerts := apimodels.PostableAlerts{PostableAlerts: make([]models.PostableAlert, 0, len(firingStates))}
-	var sentAlerts []*state.State
+	var sentAlerts []*State
 	ts := time.Now()
 
 	for _, alertState := range firingStates {
 		if !alertState.NeedsSending(stateManager.ResendDelay) {
 			continue
 		}
-		alert := stateToPostableAlert(alertState.State, appURL)
+		alert := StateToPostableAlert(alertState.State, appURL)
 		alerts.PostableAlerts = append(alerts.PostableAlerts, *alert)
 		if alertState.StateReason == ngModels.StateReasonMissingSeries { // do not put stale state back to state manager
 			continue
@@ -157,14 +156,14 @@ func FromStateTransitionToPostableAlerts(firingStates []state.StateTransition, s
 
 // FromAlertsStateToStoppedAlert selects only transitions from firing states (states eval.Alerting, eval.NoData, eval.Error)
 // and converts them to models.PostableAlert with EndsAt set to time.Now
-func FromAlertsStateToStoppedAlert(firingStates []state.StateTransition, appURL *url.URL, clock clock.Clock) apimodels.PostableAlerts {
+func FromAlertsStateToStoppedAlert(firingStates []StateTransition, appURL *url.URL, clock clock.Clock) apimodels.PostableAlerts {
 	alerts := apimodels.PostableAlerts{PostableAlerts: make([]models.PostableAlert, 0, len(firingStates))}
 	ts := clock.Now()
 	for _, transition := range firingStates {
 		if transition.PreviousState == eval.Normal || transition.PreviousState == eval.Pending {
 			continue
 		}
-		postableAlert := stateToPostableAlert(transition.State, appURL)
+		postableAlert := StateToPostableAlert(transition.State, appURL)
 		postableAlert.EndsAt = strfmt.DateTime(ts)
 		alerts.PostableAlerts = append(alerts.PostableAlerts, *postableAlert)
 	}
