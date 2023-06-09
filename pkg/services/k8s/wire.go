c9284f7519752b73ff94e3fe71f17ccf7fe05429
Todd Treece <todd.treece@grafana.com>
2023-06-09 13:31:38 -0400

    add k8s client

--- pkg/services/k8s/wire.go
+++ pkg/services/k8s/wire.go
@@ -2,7 +2,9 @@ package k8s
 
 import (
 	"github.com/google/wire"
+
 	"github.com/grafana/grafana/pkg/services/k8s/apiserver"
+	"github.com/grafana/grafana/pkg/services/k8s/client"
 )
 
-var WireSet = wire.NewSet(apiserver.WireSet)
+var WireSet = wire.NewSet(apiserver.WireSet, client.WireSet)
