91841e304d8d8ccd0dc220a431a3974fa318f90f
yangkb09 <yangkb09@gmail.com>
2023-06-09 15:07:32 -0400

    clean up comments for readability

--- scripts/modowners/modowners_generation_script.go
+++ scripts/modowners/modowners_generation_script.go
@@ -8,23 +8,9 @@ import (
 	"path/filepath"
 )
 
-/*
-load in the modules
-	for each module, call func that takes in module name and return list of files
-	with list of files, call func that takes in list of files, returns presumed owner (single team name)
-	modify modfile, write it back out (should be straightfwd) modfile.AddComment
-		need to write it back to my filesystem as go.mod.altered, compare to go.mod, raise the PR
-
-create folder called testdata
-test files in the folder
-whatever i want to do with this func, i do it with test data - return to the test
-encourages me to write thing func so it doesnt print to stdoutput and parse from OS, rather send it io.Reader with w/e go.mod file and return either io.Writer or
-call thing()
-*/
-
 // input: module name, output: list of files that import it
 // how??
-func getFiles(modules []Module) ([]string, error) {
+func getFiles(modules []Module) (map[string][]string, error) {
 	fmt.Println("I AM GET FILES")
 
 	// Path to the Grafana repository
@@ -39,8 +25,8 @@ func getFiles(modules []Module) ([]string, error) {
 	fmt.Println("repoPath", repoPath)
 	fmt.Println("repoAbsPath", repoAbsPath)
 
-	// List of importing files
-	importingFiles := make([]string, 0)
+	// // List of importing files
+	// importingFiles := make([]string, 0)
 
 	// Set to store unique imported packages
 	importedPackages := make(map[string]bool)
@@ -49,30 +35,41 @@ func getFiles(modules []Module) ([]string, error) {
 		importedPackages[module.Name] = true
 	}
 	fmt.Println("I AM BEFORE VISIT")
+
+	// Find what files are in specified repo
 	fset := token.NewFileSet()
 	filemap, err := parser.ParseDir(fset, repoAbsPath, nil, parser.ImportsOnly)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Map to store imported files for each module
+	importedFiles := make(map[string][]string)
+
 	// loop over elements of slice
+	// filemap is a map where key is what pkg we're in and value is ask.Package, which has info about a package obtained via parsing it's go source files
 	for p, ast := range filemap {
 
 		// m is a map[string]interface.
 		// loop over keys and values in the map.
 
-		fmt.Println("KEY FROM FILEMAP", p)
+		fmt.Println("KEY FROM FILEMAP, aka what pkg are we in", p)
 		fmt.Println("VALUE FROM FILEMAP", ast)
 		// print out the files in the map: name and their imports
 
 		for filename, fileobj := range ast.Files {
-			// ast.Files bunch of file objects in build directory
+			// ast.Files is a map of file objects in build directory
+			// filename is the absolute path to the repoAbsPath
 			fmt.Println("filename from ast.Files", filename)
 
 			for _, curImport := range fileobj.Imports {
 				// j, _ := json.MarshalIndent(curImport, "", "")
 				// fmt.Println(string(j))
 				fmt.Println("curImport.Path.Value", curImport.Path.Value)
+				// TODO: how to get name of import we're looking for
+				// TODO: how to append the files it's in to the value
+				// i think we're currently getting the imports for a current file. i thought we want the files for a current import (aka, the opposite)
+				// importedFiles[]
 			}
 
 		}
@@ -115,48 +112,7 @@ func getFiles(modules []Module) ([]string, error) {
 			}
 	*/
 
-	// Visit function to check each Go file in the repository
-	// visit := func(path string, info os.DirEntry, err error) error {
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	// Check if the file is a Go file
-	// 	if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
-	// 		// Parse the Go file
-	// 		fset := token.NewFileSet()
-	// 		file, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
-	// 		if err != nil {
-	// 			return err
-	// 		}
-
-	// 		// Check if the file imports any of the modules' names
-	// 		for _, importSpec := range file.Imports {
-	// 			importPath := strings.Trim(importSpec.Path.Value, "\"")
-	// 			if importedPackages[importPath] {
-	// 				importingFiles = append(importingFiles, path)
-	// 				break
-	// 			}
-	// 		}
-	// 	}
-
-	// 	return nil
-	// }
-	fmt.Println("I AM AFTER VISIT")
-
-	// Walk through the repository directory and its subdirectories
-	// err = filepath.WalkDir(repoAbsPath, visit)
-	// fmt.Println("I AM WALKDIR ERR", err)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	fmt.Println("FILES FROM GETFILES", importingFiles)
-
-	return importingFiles, nil
-
-	// for each module, return a list of files that import it
-	// DO NOW: determine how to get list of files that import a module based on module name
+	return importedFiles, nil
 }
 
 func main() {
