d3916875ef51a6ce9de3bd908efef311313b64ba
jguer <me@jguer.space>
2023-06-09 10:32:42 +0200

    draft gitlab openid

--- pkg/login/social/gitlab_oauth.go
+++ pkg/login/social/gitlab_oauth.go
@@ -1,8 +1,12 @@
 package social
 
 import (
+	"bytes"
+	"compress/zlib"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"regexp"
 
@@ -16,6 +20,26 @@ type SocialGitlab struct {
 	allowedGroups   []string
 	apiUrl          string
 	skipOrgRoleSync bool
+}
+
+type apiData struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	State    string `json:"state"`
+	Name     string `json:"name"`
+}
+
+type userData struct {
+	ID     string   `json:"sub"`
+	Login  string   `json:"preferred_username"`
+	Email  string   `json:"email"`
+	Name   string   `json:"name"`
+	Groups []string `json:"groups_direct"`
+
+	EmailVerified  bool              `json:"email_verified"`
+	Role           roletype.RoleType `json:"-"`
+	IsGrafanaAdmin *bool             `json:"-"`
 }
 
 func (s *SocialGitlab) IsGroupMember(groups []string) bool {
@@ -86,26 +110,52 @@ func (s *SocialGitlab) GetGroupsPage(client *http.Client, url string) ([]string,
 	return fullPaths, next
 }
 
-func (s *SocialGitlab) UserInfo(client *http.Client, _ *oauth2.Token) (*BasicUserInfo, error) {
-	var data struct {
-		Id       int
-		Username string
-		Email    string
-		Name     string
-		State    string
+func (s *SocialGitlab) UserInfo(client *http.Client, token *oauth2.Token) (*BasicUserInfo, error) {
+	data := s.extractFromToken(token)
+
+	// fallback to API
+	if data == nil {
+		var errAPI error
+		data, errAPI = s.extractFromAPI(client, token)
+		if errAPI != nil {
+			return nil, errAPI
+		}
 	}
 
+	userInfo := &BasicUserInfo{
+		Id:             data.ID,
+		Name:           data.Name,
+		Login:          data.Login,
+		Email:          data.Email,
+		Groups:         data.Groups,
+		Role:           data.Role,
+		IsGrafanaAdmin: data.IsGrafanaAdmin,
+	}
+
+	if !s.IsGroupMember(data.Groups) {
+		return nil, errMissingGroupMembership
+	}
+
+	if s.allowAssignGrafanaAdmin && s.skipOrgRoleSync {
+		s.log.Debug("allowAssignGrafanaAdmin and skipOrgRoleSync are both set, Grafana Admin role will not be synced, consider setting one or the other")
+	}
+
+	return userInfo, nil
+}
+
+func (s *SocialGitlab) extractFromAPI(client *http.Client, token *oauth2.Token) (*userData, error) {
+	apiResp := &apiData{}
 	response, err := s.httpGet(client, s.apiUrl+"/user")
 	if err != nil {
 		return nil, fmt.Errorf("Error getting user info: %s", err)
 	}
 
-	if err = json.Unmarshal(response.Body, &data); err != nil {
+	if err = json.Unmarshal(response.Body, &apiResp); err != nil {
 		return nil, fmt.Errorf("error getting user info: %s", err)
 	}
 
-	if data.State != "active" {
-		return nil, fmt.Errorf("user %s is inactive", data.Username)
+	if apiResp.State != "active" {
+		return nil, fmt.Errorf("user %s is inactive", apiResp.Username)
 	}
 
 	groups := s.GetGroups(client)
@@ -123,23 +173,91 @@ func (s *SocialGitlab) UserInfo(client *http.Client, _ *oauth2.Token) (*BasicUse
 			isGrafanaAdmin = &grafanaAdmin
 		}
 	}
-	if s.allowAssignGrafanaAdmin && s.skipOrgRoleSync {
-		s.log.Debug("allowAssignGrafanaAdmin and skipOrgRoleSync are both set, Grafana Admin role will not be synced, consider setting one or the other")
-	}
 
-	userInfo := &BasicUserInfo{
-		Id:             fmt.Sprintf("%d", data.Id),
-		Name:           data.Name,
-		Login:          data.Username,
-		Email:          data.Email,
+	return &userData{
+		ID:             fmt.Sprintf("%d", apiResp.ID),
+		Login:          apiResp.Username,
+		Email:          apiResp.Email,
+		Name:           apiResp.Name,
 		Groups:         groups,
 		Role:           role,
 		IsGrafanaAdmin: isGrafanaAdmin,
+	}, nil
+}
+
+func (s *SocialGitlab) extractFromToken(token *oauth2.Token) *userData {
+	s.log.Debug("Extracting user info from OAuth token", fmt.Sprintf("%+v", token))
+
+	idTokenAttribute := "id_token"
+
+	idToken := token.Extra(idTokenAttribute)
+	if idToken == nil {
+		s.log.Debug("No id_token found", "token", token)
+		return nil
 	}
 
-	if !s.IsGroupMember(groups) {
-		return nil, errMissingGroupMembership
+	jwtRegexp := regexp.MustCompile("^([-_a-zA-Z0-9=]+)[.]([-_a-zA-Z0-9=]+)[.]([-_a-zA-Z0-9=]+)$")
+	matched := jwtRegexp.FindStringSubmatch(idToken.(string))
+	if matched == nil {
+		s.log.Debug("id_token is not in JWT format", "id_token", idToken.(string))
+		return nil
 	}
 
-	return userInfo, nil
+	// Check token signature
+
+	rawJSON, err := base64.RawURLEncoding.DecodeString(matched[2])
+	if err != nil {
+		s.log.Error("Error base64 decoding id_token", "raw_payload", matched[2], "error", err)
+		return nil
+	}
+
+	headerBytes, err := base64.RawURLEncoding.DecodeString(matched[1])
+	if err != nil {
+		s.log.Error("Error base64 decoding header", "header", matched[1], "error", err)
+		return nil
+	}
+
+	var header map[string]interface{}
+	if err := json.Unmarshal(headerBytes, &header); err != nil {
+		s.log.Error("Error deserializing header", "error", err)
+		return nil
+	}
+
+	if compressionVal, exists := header["zip"]; exists {
+		compression, ok := compressionVal.(string)
+		if !ok {
+			s.log.Warn("Unknown compression algorithm")
+			return nil
+		}
+
+		if compression != "DEF" {
+			s.log.Warn("Unknown compression algorithm", "algorithm", compression)
+			return nil
+		}
+
+		fr, err := zlib.NewReader(bytes.NewReader(rawJSON))
+		if err != nil {
+			s.log.Error("Error creating zlib reader", "error", err)
+			return nil
+		}
+		defer func() {
+			if err := fr.Close(); err != nil {
+				s.log.Warn("Failed closing zlib reader", "error", err)
+			}
+		}()
+		rawJSON, err = io.ReadAll(fr)
+		if err != nil {
+			s.log.Error("Error decompressing payload", "error", err)
+			return nil
+		}
+	}
+
+	var data userData
+	if err := json.Unmarshal(rawJSON, &data); err != nil {
+		s.log.Error("Error decoding id_token JSON", "raw_json", string(rawJSON), "error", err)
+		return nil
+	}
+
+	s.log.Debug("Received id_token", "raw_json", string(rawJSON), "data", fmt.Sprintf("%+v", data))
+	return &data
 }
