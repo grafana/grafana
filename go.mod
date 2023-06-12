7d736b24b56da3dd3cb6b62e6ec34b0ff3c6bfcc
yangkb09 <yangkb09@gmail.com>
2023-06-12 17:21:53 -0400

    refactor: change getFiles to accept single import name and return list of files that import it

--- go.mod
+++ go.mod
@@ -108,11 +108,11 @@ require (
 	go.opentelemetry.io/otel/trace v1.14.0
 	golang.org/x/crypto v0.7.0
 	golang.org/x/exp v0.0.0-20230307190834-24139beb5833
-	golang.org/x/net v0.9.0
+	golang.org/x/net v0.10.0
 	golang.org/x/oauth2 v0.6.0
-	golang.org/x/sync v0.1.0
+	golang.org/x/sync v0.2.0
 	golang.org/x/time v0.3.0
-	golang.org/x/tools v0.7.0
+	golang.org/x/tools v0.9.3
 	gonum.org/v1/gonum v0.11.0
 	google.golang.org/api v0.111.0
 	google.golang.org/grpc v1.54.0
@@ -217,7 +217,7 @@ require (
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0
 	go.uber.org/goleak v1.2.1 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
@@ -418,7 +418,7 @@ require (
 	github.com/xlab/treeprint v1.1.0
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.14.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
-	golang.org/x/mod v0.9.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
 
--- scripts/modowners/modowners_generation_script.go
+++ scripts/modowners/modowners_generation_script.go
@@ -4,127 +4,62 @@ import (
 	"fmt"
 	"go/parser"
 	"go/token"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
-// input: module name, output: list of files that import it
-// how??
-func getFiles(modules []Module) (map[string][]string, error) {
-	fmt.Println("I AM GET FILES")
-
-	// Path to the Grafana repository
-	repoPath := "github.com/grafana/grafana"
+// input: import name, output: list of files that import it
+func getFiles(importName string) ([]string, error) {
+	var importingFiles []string
 
 	// Get the absolute path to the repository
-	repoAbsPath, err := filepath.Abs("../../pkg/build/npm")
+	repoAbsPath, err := filepath.Abs("")
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println("repoPath", repoPath)
 	fmt.Println("repoAbsPath", repoAbsPath)
-
-	// // List of importing files
-	// importingFiles := make([]string, 0)
-
-	// Map to store imported files for each module
-	importedFiles := make(map[string][]string)
-
-	for _, module := range modules {
-		importedFiles[module.Name] = []string{}
-	}
-	fmt.Println("I AM BEFORE VISIT")
 
 	// Find what files are in specified repo
 	fset := token.NewFileSet()
-	filemap, err := parser.ParseDir(fset, repoAbsPath, nil, parser.ImportsOnly)
+	packages, err := parser.ParseDir(fset, repoAbsPath, nil, parser.ImportsOnly)
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("failed to parse directory: %w", err)
 	}
-
-	for importName, _ := range importedFiles {
-		// loop over elements of slice
-		// filemap is a map where key is what pkg we're in and value is ask.Package, which has info about a package obtained via parsing it's go source files
-		for p, ast := range filemap {
-
-			// m is a map[string]interface.
-			// loop over keys and values in the map.
-
-			fmt.Println("KEY FROM FILEMAP, aka what pkg are we in", p)
-			fmt.Println("VALUE FROM FILEMAP", ast)
-			// print out the files in the map: name and their imports
-
-			for filename, fileobj := range ast.Files {
-				// ast.Files is a map of file objects in build directory
-				// filename is the absolute path to the repoAbsPath
-				fmt.Println("filename from ast.Files", filename)
-
-				for _, curImport := range fileobj.Imports {
-					// j, _ := json.MarshalIndent(curImport, "", "")
-					// fmt.Println(string(j))
-					fmt.Println("curImport.Path.Value", curImport.Path.Value)
-					if curImport.Path.Value == importName {
-						importedFiles[importName] = append(importedFiles[importName], filename) // NOTE: i dont think filename is what we want - it looks like /Users/kat/grafana/grafana/pkg/build/npm/npm.go
-					}
-
+	fmt.Println("packages", packages)
+	// Iterate over parsed packages and their files
+	for _, pkg := range packages {
+		fmt.Println("pkg", pkg)
+		for _, file := range pkg.Files {
+			// Check if the file imports the specified package
+			for _, imp := range file.Imports {
+				importPath, err := strconv.Unquote(imp.Path.Value)
+				if err != nil {
+					log.Printf("Failed to unquote import path %s: %v\n", imp.Path.Value, err)
+					continue
 				}
-
+				if strings.Contains(importPath, importName) {
+					importingFiles = append(importingFiles, fset.Position(file.Pos()).Filename)
+					break
+				}
 			}
 		}
 	}
-	fmt.Println("importedFiles", importedFiles)
-	fmt.Println("FILEMAP", filemap)
 
-	/*
-		see if i can iterate through the file structure
-		think about: what do we ultimately want to reutrn form this func: map
-			map where key is import name and value is []string of files the import is in
+	fmt.Println("importingFiles", importingFiles)
 
-		insantiate an empty map that i can use as a result that i return
-			intead of logging what we just logged, changed the code to add it to that map
-
-
-		only step left is i need to iterate over all the directories
-
-		change the path that we're parsing now to the pkg directory
-		need to be a line of code to find all the subdirectories
-		recursviely do it until i run out of subdirectories
-
-		-- big picture --
-		first find all subdirectories and make a list of paths
-		for each path, call the code that we have now
-
-		keep the result set outside and keep appending to it
-
-		--
-		just parsing one directory, not parsing subfolders
-			going to src/build, grabbing all the .go files, putting the filename as the key, parsing the file and biulding the AST obj and putting tht as value
-
-			directory given as key
-			value:
-
-
-			ask serge!!: bc you're allowed pointers, other languages do concrete data structures.
-			{
-			FileName: asd, Parent Directory: { path: /src, Files: [{FileName: asd}]
-			}
-	*/
-
-	return importedFiles, nil
+	return importingFiles, nil
 }
 
 func main() {
-	// parse go.mod to get list of modules
-	m, _ := parseGoMod(os.DirFS("."), "go.mod")
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	files, _ := getFiles(m)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	fmt.Println("FILES FROM MAIN", files)
+	fmt.Println("os.Args", os.Args[1])
+	files, err := getFiles(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("files from getFiles", files)
 
 }
