fdd80506d711549de9d6c3849eac71e48c5eea59
Ashley Harrison <ashley.harrison@grafana.com>
2023-06-12 09:47:33 +0100

    add analytics for folder creation

--- public/app/features/browse-dashboards/components/CreateNewButton.test.tsx
+++ public/app/features/browse-dashboards/components/CreateNewButton.test.tsx
@@ -3,27 +3,49 @@ import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { TestProvider } from 'test/helpers/TestProvider';
 
+import { FolderDTO } from 'app/types';
+
 import CreateNewButton from './CreateNewButton';
+
+const mockParentFolder: FolderDTO = {
+  canAdmin: true,
+  canDelete: true,
+  canEdit: true,
+  canSave: true,
+  created: '',
+  createdBy: '',
+  hasAcl: true,
+  id: 1,
+  title: 'myFolder',
+  uid: '12345',
+  updated: '',
+  updatedBy: '',
+  url: '',
+  version: 1,
+};
 
 function render(...[ui, options]: Parameters<typeof rtlRender>) {
   rtlRender(<TestProvider>{ui}</TestProvider>, options);
 }
 
-async function renderAndOpen(folderUID?: string) {
-  render(<CreateNewButton canCreateDashboard canCreateFolder parentFolderUid={folderUID} />);
+async function renderAndOpen(folder?: FolderDTO) {
+  render(<CreateNewButton canCreateDashboard canCreateFolder parentFolder={folder} />);
   const newButton = screen.getByText('New');
   await userEvent.click(newButton);
 }
 
 describe('NewActionsButton', () => {
-  it('should display the correct urls with a given folderUID', async () => {
-    await renderAndOpen('123');
+  it('should display the correct urls with a given parent folder', async () => {
+    await renderAndOpen(mockParentFolder);
 
-    expect(screen.getByText('New dashboard')).toHaveAttribute('href', '/dashboard/new?folderUid=123');
-    expect(screen.getByText('Import')).toHaveAttribute('href', '/dashboard/import?folderUid=123');
+    expect(screen.getByText('New dashboard')).toHaveAttribute(
+      'href',
+      `/dashboard/new?folderUid=${mockParentFolder.uid}`
+    );
+    expect(screen.getByText('Import')).toHaveAttribute('href', `/dashboard/import?folderUid=${mockParentFolder.uid}`);
   });
 
-  it('should display urls without params when there is no folderUID', async () => {
+  it('should display urls without params when there is no parent folder', async () => {
     await renderAndOpen();
 
     expect(screen.getByText('New dashboard')).toHaveAttribute('href', '/dashboard/new');
@@ -31,8 +53,7 @@ describe('NewActionsButton', () => {
   });
 
   it('clicking the "New folder" button opens the drawer', async () => {
-    const mockParentFolderTitle = 'mockParentFolderTitle';
-    render(<CreateNewButton canCreateDashboard canCreateFolder parentFolderTitle={mockParentFolderTitle} />);
+    render(<CreateNewButton canCreateDashboard canCreateFolder parentFolder={mockParentFolder} />);
 
     const newButton = screen.getByText('New');
     await userEvent.click(newButton);
@@ -41,7 +62,7 @@ describe('NewActionsButton', () => {
     const drawer = screen.getByRole('dialog', { name: 'Drawer title New folder' });
     expect(drawer).toBeInTheDocument();
     expect(within(drawer).getByRole('heading', { name: 'New folder' })).toBeInTheDocument();
-    expect(within(drawer).getByText(`Location: ${mockParentFolderTitle}`)).toBeInTheDocument();
+    expect(within(drawer).getByText(`Location: ${mockParentFolder.title}`)).toBeInTheDocument();
   });
 
   it('should only render dashboard items when folder creation is disabled', async () => {
--- public/app/features/browse-dashboards/components/CreateNewButton.tsx
+++ public/app/features/browse-dashboards/components/CreateNewButton.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 
+import { reportInteraction } from '@grafana/runtime';
 import { Button, Drawer, Dropdown, Icon, Menu, MenuItem } from '@grafana/ui';
 import { createNewFolder } from 'app/features/folders/state/actions';
 import {
@@ -9,6 +10,7 @@ import {
   getImportPhrase,
   getNewPhrase,
 } from 'app/features/search/tempI18nPhrases';
+import { FolderDTO } from 'app/types';
 
 import { NewFolderForm } from './NewFolderForm';
 
@@ -19,40 +21,35 @@ const mapDispatchToProps = {
 const connector = connect(null, mapDispatchToProps);
 
 interface OwnProps {
-  parentFolderTitle?: string;
-  /**
-   * Pass a folder UID in which the dashboard or folder will be created
-   */
-  parentFolderUid?: string;
+  parentFolder?: FolderDTO;
   canCreateFolder: boolean;
   canCreateDashboard: boolean;
 }
 
 type Props = OwnProps & ConnectedProps<typeof connector>;
 
-function CreateNewButton({
-  parentFolderTitle,
-  parentFolderUid,
-  canCreateDashboard,
-  canCreateFolder,
-  createNewFolder,
-}: Props) {
+function CreateNewButton({ parentFolder, canCreateDashboard, canCreateFolder, createNewFolder }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const [showNewFolderDrawer, setShowNewFolderDrawer] = useState(false);
 
   const onCreateFolder = (folderName: string) => {
-    createNewFolder(folderName, parentFolderUid);
+    createNewFolder(folderName, parentFolder?.uid);
+    const depth = parentFolder?.parents ? parentFolder.parents.length + 1 : 0;
+    reportInteraction('grafana_manage_dashboards_folder_created', {
+      is_subfolder: Boolean(parentFolder?.uid),
+      folder_depth: depth,
+    });
     setShowNewFolderDrawer(false);
   };
 
   const newMenu = (
     <Menu>
       {canCreateDashboard && (
-        <MenuItem url={addFolderUidToUrl('/dashboard/new', parentFolderUid)} label={getNewDashboardPhrase()} />
+        <MenuItem url={addFolderUidToUrl('/dashboard/new', parentFolder?.uid)} label={getNewDashboardPhrase()} />
       )}
       {canCreateFolder && <MenuItem onClick={() => setShowNewFolderDrawer(true)} label={getNewFolderPhrase()} />}
       {canCreateDashboard && (
-        <MenuItem url={addFolderUidToUrl('/dashboard/import', parentFolderUid)} label={getImportPhrase()} />
+        <MenuItem url={addFolderUidToUrl('/dashboard/import', parentFolder?.uid)} label={getImportPhrase()} />
       )}
     </Menu>
   );
@@ -68,7 +65,7 @@ function CreateNewButton({
       {showNewFolderDrawer && (
         <Drawer
           title={getNewFolderPhrase()}
-          subtitle={parentFolderTitle ? `Location: ${parentFolderTitle}` : undefined}
+          subtitle={parentFolder?.title ? `Location: ${parentFolder.title}` : undefined}
           scrollableContent
           onClose={() => setShowNewFolderDrawer(false)}
           size="sm"
