4a4224cce464b9e89457ffb436d30c039a445818
Giuseppe Guerra <giuseppe@guerra.in>
2023-06-13 18:21:46 +0200

    Update tests

---
+++ public/app/features/plugins/admin/components/PluginDetailsPage.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { TestProvider } from 'test/helpers/TestProvider';
+
+import { PluginSignatureStatus } from '@grafana/data';
+
+import { CatalogPlugin } from '../types';
+
+import { PluginDetailsPage } from './PluginDetailsPage';
+
+jest.mock('../state/hooks', () => ({
+  __esModule: true,
+  ...jest.requireActual('../state/hooks'),
+  useGetSingle: jest.fn().mockImplementation((id: string) => {
+    return {
+      description: 'The test plugin',
+      downloads: 5,
+      id: 'test-plugin',
+      info: {
+        logos: { small: '', large: '' },
+      },
+      name: 'Testing Plugin',
+      orgName: 'Test',
+      popularity: 0,
+      signature: PluginSignatureStatus.valid,
+      publishedAt: '2020-09-01',
+      updatedAt: '2021-06-28',
+      hasUpdate: false,
+      isInstalled: false,
+      isCore: false,
+      isDev: false,
+      isEnterprise: false,
+      isDisabled: false,
+      isPublished: true,
+      angularDetected: id === 'angular',
+    };
+  }),
+}));
+
+describe('PluginDetailsAngularDeprecation', () => {
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders the component for angular plugins', () => {
+    // render(<PluginDetailsPage plugin={{ ...plugin, angularDetected: true }} />);
+    render(
+      <TestProvider>
+        <PluginDetailsPage pluginId="angular" />
+      </TestProvider>
+    );
+    expect(screen.getByText(/angular plugin/i)).toBeVisible();
+  });
+
+  it('does not render the component for non-angular plugins', () => {
+    // render(<PluginDetailsPage plugin={{ ...plugin, angularDetected: false }} />);
+    render(
+      <TestProvider>
+        <PluginDetailsPage pluginId="not-angular" />
+      </TestProvider>
+    );
+    expect(screen.queryByText(/angular plugin/i)).toBeNull();
+  });
+});
