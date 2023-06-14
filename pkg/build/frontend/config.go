a6b524fd56ed6f11b17d1850bb5e868eec241743
Dimitris Sotirakis <dimitrios.sotirakis@grafana.com>
2023-06-14 09:56:19 +0300

    NPM packages: Fail `build-frontend-packages` step if `package.json` and input tag differ (#70011)
    * Fail if package.json version is different than the tag
    
    * Add tests
    
    * Update message
    
    * Small refactoring
    
    * Fix lint

--- pkg/build/frontend/config.go
+++ pkg/build/frontend/config.go
@@ -1,31 +1,39 @@
 package frontend
 
 import (
-	"github.com/urfave/cli/v2"
+	"fmt"
 
 	"github.com/grafana/grafana/pkg/build/config"
+	"github.com/urfave/cli/v2"
 )
 
 const GrafanaDir = "."
 
-func GetConfig(c *cli.Context, version string) (config.Config, config.Edition, error) {
+func GetConfig(c *cli.Context, inputTagVersion string) (config.Config, config.Edition, error) {
 	cfg := config.Config{
 		NumWorkers:     c.Int("jobs"),
 		GitHubToken:    c.String("github-token"),
-		PackageVersion: version,
+		PackageVersion: inputTagVersion,
 	}
 
 	mode := config.Edition(c.String("edition"))
+	buildID := c.String("build-id")
+	packageVersion, err := config.GetGrafanaVersion(buildID, GrafanaDir)
+	if err != nil {
+		return config.Config{}, "", cli.Exit(err.Error(), 1)
+	}
 
-	if version == "" {
-		buildID := c.String("build-id")
-		var err error
-		version, err = config.GetGrafanaVersion(buildID, GrafanaDir)
-		cfg.PackageVersion = version
+	if inputTagVersion == "" {
+		cfg.PackageVersion = packageVersion
 		if err != nil {
 			return config.Config{}, config.EditionOSS, cli.Exit(err.Error(), 1)
 		}
+		return cfg, mode, err
+	}
+	if inputTagVersion != packageVersion {
+		return config.Config{}, "", cli.Exit(fmt.Errorf("package.json version and input tag version differ %s != %s.\nPlease update package.json", packageVersion, inputTagVersion), 1)
 	}
 
+	cfg.PackageVersion = inputTagVersion
 	return cfg, mode, nil
 }
