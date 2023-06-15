42c59bfca3019c744b444e745bea8af7d5391250
Jack Westbrook <jack.westbrook@gmail.com>
2023-06-15 18:08:08 +0200

    feat(plugins): add back ability to transform plugin src for cdns

--- public/app/features/plugins/plugin_loader.ts
+++ public/app/features/plugins/plugin_loader.ts
@@ -46,10 +46,10 @@ import builtInPlugins from './built_in_plugins';
 import { PLUGIN_CDN_URL_KEY } from './constants';
 import { sandboxPluginDependencies } from './sandbox/plugin_dependencies';
 import { importPluginModuleInSandbox } from './sandbox/sandbox_plugin_loader';
+import { registerPluginInCache } from './systemjsPlugins/pluginCacheBuster';
 
 // import { locateFromCDN, translateForCDN } from './systemjsPlugins/pluginCDN';
 // import { fetchCSS, locateCSS } from './systemjsPlugins/pluginCSS';
-// import { locateWithCache, registerPluginInCache } from './systemjsPlugins/pluginCacheBuster';
 
 // Help the 6.4 to 6.5 migration
 // The base classes were moved from @grafana/ui to @grafana/data
@@ -95,6 +95,12 @@ const importMap = {
   'app/features/dashboard/impression_store': {
     impressions: impressionSrv,
   },
+  d3: d3,
+  emotion: emotion,
+  jquery: {
+    default: jquery,
+    __useDefault: true,
+  },
   'jquery.flot.crosshair': { fakeDep: 1 },
   'jquery.flot.events': { fakeDep: 1 },
   'jquery.flot.fillbelow': { fakeDep: 1 },
@@ -105,6 +111,13 @@ const importMap = {
   'jquery.flot.stackpercent': { fakeDep: 1 },
   'jquery.flot.time': { fakeDep: 1 },
   'jquery.flot': { fakeDep: 1 },
+  lodash: _,
+  moment: {
+    default: moment,
+    __useDefault: true,
+  },
+  prismjs: prismjs,
+  react: react,
   'react-dom': reactDom,
   'react-redux': reactRedux,
 
@@ -122,25 +135,12 @@ const importMap = {
   // (This means that we are exposing two versions of the same package).
   'react-router-dom': reactRouterDom, // react-router-dom@v5
   'react-router': reactRouterCompat, // react-router-dom@v6, react-router@v6 (included)
-  'rxjs/operators': rxjsOperators,
-  'slate-plain-serializer': slatePlain,
-  'slate-react': slateReact,
-  d3: d3,
-  emotion: emotion,
-  jquery: {
-    default: jquery,
-    __useDefault: true,
-  },
-  lodash: _,
-  moment: {
-    default: moment,
-    __useDefault: true,
-  },
-  prismjs: prismjs,
-  react: react,
   redux: redux,
   rxjs: rxjs,
+  'rxjs/operators': rxjsOperators,
   slate: slate,
+  'slate-plain-serializer': slatePlain,
+  'slate-react': slateReact,
 } as Record<string, System.Module>;
 
 const imports = Object.keys(importMap).reduce((acc, key) => {
@@ -160,7 +160,13 @@ const imports = Object.keys(importMap).reduce((acc, key) => {
   return acc;
 }, {} as Record<string, string>);
 
+// pass the map of module names so systemjs can resolve them
+// to the imports above.
+SystemJS.addImportMap({ imports });
+
 const moduleTypesRegEx = /^[^#?]+\.(css|html|json|wasm)([?#].*)?$/;
+const loadPluginCssRegEx = /^plugins.+\.css$/i;
+const jsContentTypeRegEx = /^text\/javascript(;|$)/;
 
 // Fetch and eval if assest is not JS or loading from CDN
 // otherwise we load via script for performance and security goodness!
@@ -168,59 +174,148 @@ SystemJS.shouldFetch = function (url: string) {
   const isCDN = Boolean(config.pluginsCDNBaseURL) && url.startsWith(config.pluginsCDNBaseURL);
   const isNotJS = moduleTypesRegEx.test(url);
 
-  const shouldFetch = isNotJS || isCDN;
-
-  console.log('SystemJS should fetch:', {
-    url,
-    shouldFetch,
-  });
-
-  return shouldFetch;
+  return isNotJS || isCDN;
 };
 
 const systemJSPrototype = SystemJS.constructor.prototype;
-const fetch = systemJSPrototype.fetch;
+const systemJSFetch = systemJSPrototype.fetch;
 
-// Is this how we tackle plugin lifecycle hooks now?
 systemJSPrototype.fetch = function (url: string, options: Record<string, unknown>) {
-  console.log('SystemJS fetch hook:', { url, options });
-  return fetch(url, options).then((res: any) => {
+  return systemJSFetch(url, options).then(async (res: Response) => {
+    const contentType = res.headers.get('content-type') || '';
+    // if it's a JS file on the CDN we need to transform the asset paths in the source
+    if (jsContentTypeRegEx.test(contentType) && res.url.startsWith(config.pluginsCDNBaseURL)) {
+      const source = await res.text();
+      const splitUrl = res.url.split('/public/plugins/');
+      const baseAddress = splitUrl[0];
+      const pluginId = splitUrl[1].split('/')[0];
+      const transformedSrc = jsPluginCDNTransform(source, baseAddress, pluginId);
+      return new Response(new Blob([transformedSrc], { type: 'text/javascript' }));
+    }
     return res;
   });
 };
 
 const originalResolve = systemJSPrototype.resolve;
-// We can hook resolutions like this...
+// TODO: Need to understand how to handle caching of requests.
 systemJSPrototype.resolve = function (id: string, parentUrl: string) {
-  console.log('SystemJS resolve hook:', { id, parentUrl });
+  // console.log('SystemJS resolve hook:', { id, parentUrl });
   try {
     return originalResolve.apply(this, arguments);
   } catch (err) {
-    console.log(`SystemJS: Failed to resolve: '${id}'`);
-    return monkeyPatchLoadPluginCss(id, parentUrl);
+    if (loadPluginCssRegEx.test(id)) {
+      return monkeyPatchLoadPluginCss(id);
+    }
+    console.log(`SystemJS: failed to resolve '${id}'`);
+    return id;
   }
 };
 
-const loadPluginCssRegEx = /^plugins.+\.css$/i;
+// For backwards compatiblity with older plugins that use `loadPluginCss`
+// we need to translate the path for systemjs 6.x.x to understand
+const monkeyPatchLoadPluginCss = (id: string) => `./public/${id}`;
 
-function monkeyPatchLoadPluginCss(id: string, parentUrl: string) {
-  console.log('SystemJS monkeyPatchLoadPluginCss', { id, test: loadPluginCssRegEx.test(id) });
-  if (loadPluginCssRegEx.test(id)) {
-    return `./public/${id}`;
-  }
-
-  console.log('oh no!!!!');
-  return '/some-url.js';
+// TODO: this should replace translateForCDN from './systemjsPlugins/pluginCDN'
+function jsPluginCDNTransform(source: string, baseAddress: string, pluginId: string) {
+  let transformedSrc = source;
+  transformedSrc = transformedSrc.replace(/(\/?)(public\/plugins)/g, `${baseAddress}/$2`);
+  transformedSrc = transformedSrc.replace(/(["|'])(plugins\/.+?.css)(["|'])/g, `$1${baseAddress}/public/$2$3`);
+  // TODO: SystemJS 6 already does this transform, do we need it for sandbox?
+  transformedSrc = transformedSrc.replace(
+    /(\/\/#\ssourceMappingURL=)(.+)\.map/g,
+    `$1${baseAddress}/public/plugins/${pluginId}/$2.map`
+  );
+  return transformedSrc;
 }
 
-// pass the map of module names so systemjs can resolve them
-// to the imports above.
-SystemJS.addImportMap({ imports });
+export async function importPluginModule({
+  path,
+  version,
+  isAngular,
+  pluginId,
+}: {
+  path: string;
+  pluginId: string;
+  version?: string;
+  isAngular?: boolean;
+}): Promise<any> {
+  if (version) {
+    registerPluginInCache({ path, version });
+  }
 
-// // Let's see what's in the SystemJS entries.
-// for (const [name, moduleExports] of SystemJS.entries()) {
-//   console.log({ name, moduleExports });
-// }
+  const builtIn = builtInPlugins[path];
+  if (builtIn) {
+    // for handling dynamic imports
+    if (typeof builtIn === 'function') {
+      return await builtIn();
+    } else {
+      return builtIn;
+    }
+  }
+
+  // the sandboxing environment code cannot work in nodejs and requires a real browser
+  if (isFrontendSandboxSupported({ isAngular, pluginId })) {
+    return importPluginModuleInSandbox({ pluginId });
+  }
+
+  return SystemJS.import(path);
+}
+
+function isFrontendSandboxSupported({ isAngular, pluginId }: { isAngular?: boolean; pluginId: string }): boolean {
+  // To fast test and debug the sandbox in the browser.
+  const sandboxQueryParam = location.search.includes('nosandbox') && config.buildInfo.env === 'development';
+  const isPluginExcepted = config.disableFrontendSandboxForPlugins.includes(pluginId);
+  return (
+    !isAngular &&
+    Boolean(config.featureToggles.pluginsFrontendSandbox) &&
+    process.env.NODE_ENV !== 'test' &&
+    !isPluginExcepted &&
+    !sandboxQueryParam
+  );
+}
+
+export function importDataSourcePlugin(meta: grafanaData.DataSourcePluginMeta): Promise<GenericDataSourcePlugin> {
+  return importPluginModule({
+    path: meta.module,
+    version: meta.info?.version,
+    isAngular: meta.angularDetected,
+    pluginId: meta.id,
+  }).then((pluginExports) => {
+    if (pluginExports.plugin) {
+      const dsPlugin = pluginExports.plugin as GenericDataSourcePlugin;
+      dsPlugin.meta = meta;
+      return dsPlugin;
+    }
+
+    if (pluginExports.Datasource) {
+      const dsPlugin = new grafanaData.DataSourcePlugin<
+        grafanaData.DataSourceApi<grafanaData.DataQuery, grafanaData.DataSourceJsonData>,
+        grafanaData.DataQuery,
+        grafanaData.DataSourceJsonData
+      >(pluginExports.Datasource);
+      dsPlugin.setComponentsFromLegacyExports(pluginExports);
+      dsPlugin.meta = meta;
+      return dsPlugin;
+    }
+
+    throw new Error('Plugin module is missing DataSourcePlugin or Datasource constructor export');
+  });
+}
+
+export function importAppPlugin(meta: grafanaData.PluginMeta): Promise<grafanaData.AppPlugin> {
+  return importPluginModule({
+    path: meta.module,
+    version: meta.info?.version,
+    isAngular: meta.angularDetected,
+    pluginId: meta.id,
+  }).then((pluginExports) => {
+    const plugin = pluginExports.plugin ? (pluginExports.plugin as grafanaData.AppPlugin) : new grafanaData.AppPlugin();
+    plugin.init(meta);
+    plugin.meta = meta;
+    plugin.setComponentsFromLegacyExports(pluginExports);
+    return plugin;
+  });
+}
 
 // grafanaRuntime.SystemJS.registry.set('css', grafanaRuntime.SystemJS.newModule({ locate: locateCSS, fetch: fetchCSS }));
 // grafanaRuntime.SystemJS.registry.set('plugin-loader', grafanaRuntime.SystemJS.newModule({ locate: locateWithCache }));
@@ -356,92 +451,3 @@ SystemJS.addImportMap({ imports });
 // for (const flotDep of flotDeps) {
 //   exposeToPlugin(flotDep, { fakeDep: 1 });
 // }
-
-export async function importPluginModule({
-  path,
-  version,
-  isAngular,
-  pluginId,
-}: {
-  path: string;
-  pluginId: string;
-  version?: string;
-  isAngular?: boolean;
-}): Promise<any> {
-  // if (version) {
-  //   registerPluginInCache({ path, version });
-  // }
-
-  const builtIn = builtInPlugins[path];
-  if (builtIn) {
-    // for handling dynamic imports
-    if (typeof builtIn === 'function') {
-      return await builtIn();
-    } else {
-      return builtIn;
-    }
-  }
-
-  // the sandboxing environment code cannot work in nodejs and requires a real browser
-  if (isFrontendSandboxSupported({ isAngular, pluginId })) {
-    return importPluginModuleInSandbox({ pluginId });
-  }
-
-  return SystemJS.import(path);
-}
-
-function isFrontendSandboxSupported({ isAngular, pluginId }: { isAngular?: boolean; pluginId: string }): boolean {
-  // To fast test and debug the sandbox in the browser.
-  const sandboxQueryParam = location.search.includes('nosandbox') && config.buildInfo.env === 'development';
-  const isPluginExcepted = config.disableFrontendSandboxForPlugins.includes(pluginId);
-  return (
-    !isAngular &&
-    Boolean(config.featureToggles.pluginsFrontendSandbox) &&
-    process.env.NODE_ENV !== 'test' &&
-    !isPluginExcepted &&
-    !sandboxQueryParam
-  );
-}
-
-export function importDataSourcePlugin(meta: grafanaData.DataSourcePluginMeta): Promise<GenericDataSourcePlugin> {
-  return importPluginModule({
-    path: meta.module,
-    version: meta.info?.version,
-    isAngular: meta.angularDetected,
-    pluginId: meta.id,
-  }).then((pluginExports) => {
-    if (pluginExports.plugin) {
-      const dsPlugin = pluginExports.plugin as GenericDataSourcePlugin;
-      dsPlugin.meta = meta;
-      return dsPlugin;
-    }
-
-    if (pluginExports.Datasource) {
-      const dsPlugin = new grafanaData.DataSourcePlugin<
-        grafanaData.DataSourceApi<grafanaData.DataQuery, grafanaData.DataSourceJsonData>,
-        grafanaData.DataQuery,
-        grafanaData.DataSourceJsonData
-      >(pluginExports.Datasource);
-      dsPlugin.setComponentsFromLegacyExports(pluginExports);
-      dsPlugin.meta = meta;
-      return dsPlugin;
-    }
-
-    throw new Error('Plugin module is missing DataSourcePlugin or Datasource constructor export');
-  });
-}
-
-export function importAppPlugin(meta: grafanaData.PluginMeta): Promise<grafanaData.AppPlugin> {
-  return importPluginModule({
-    path: meta.module,
-    version: meta.info?.version,
-    isAngular: meta.angularDetected,
-    pluginId: meta.id,
-  }).then((pluginExports) => {
-    const plugin = pluginExports.plugin ? (pluginExports.plugin as grafanaData.AppPlugin) : new grafanaData.AppPlugin();
-    plugin.init(meta);
-    plugin.meta = meta;
-    plugin.setComponentsFromLegacyExports(pluginExports);
-    return plugin;
-  });
-}
