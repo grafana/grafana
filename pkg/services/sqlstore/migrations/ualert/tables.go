458ad9199c77b47b7fdff446b4200f0136df5816
Santiago <santiagohernandez.1997@gmail.com>
2023-06-15 15:11:20 -0300

    update integration tests

--- pkg/services/sqlstore/migrations/ualert/tables.go
+++ pkg/services/sqlstore/migrations/ualert/tables.go
@@ -24,10 +24,6 @@ func AddTablesMigrations(mg *migrator.Migrator) {
 	mg.AddMigration("add last_applied column to alert_configuration_history", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_configuration_history"}, &migrator.Column{
 		Name: "last_applied", Type: migrator.DB_Int, Nullable: false, Default: "0",
 	}))
-
-	mg.AddMigration("add index in alert_image on url", migrator.NewAddIndexMigration(migrator.Table{Name: "alert_image"}, &migrator.Index{
-		Cols: []string{"url"}, Type: migrator.IndexType,
-	}))
 	// End of migration log, add new migrations above this line.
 }
 
