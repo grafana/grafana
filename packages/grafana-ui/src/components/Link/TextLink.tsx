e228026caa850a9a69bc5ab5b4f1eb261ef30b4d
eledobleefe <laura.fernandez@grafana.com>
2023-06-16 09:38:49 +0200

    Improve text and icon alignment. Add prop to specify the text alignment when it is an standalone link

--- packages/grafana-ui/src/components/Link/TextLink.tsx
+++ packages/grafana-ui/src/components/Link/TextLink.tsx
@@ -15,16 +15,19 @@ export interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
   /** Color to use for text */
   color?: keyof GrafanaTheme2['colors']['text'] | 'error' | 'success' | 'warning' | 'info';
-  /** If the link will redirect users to a page in or out Grafana */
+  /** Specify if the link will redirect users to a page in or out Grafana */
   external?: boolean;
-  /** Specify the icon name to be used when the link is not an inline element. If this is undefined but the link is external and not inline, the link icon will be shown. */
-  icon?: IconName;
   /** True if the element should be displayed inline with surrounding text, false if it should be displayed as a block */
   inline?: boolean;
   /** What typograpy variant should be used for the component. Only use if default variant for the defined element is not what is needed */
   variant?: keyof ThemeTypographyVariantTypes;
   /** Override the default weight for the used variant */
   weight?: 'light' | 'regular' | 'medium' | 'bold';
+  /** When it is a standalone element, whether to align the text to left, center or right of its block*/
+  textAlignment?: 'left' | 'right' | 'center';
+  /** When the link is inline it won't show an icon.
+   * If it is a standalone element, it will show the icon specified. When this is an external link, the default icon will be 'external-link-alt' */
+  icon?: IconName;
   children: string;
 }
 
@@ -35,6 +38,7 @@ export const TextLink = forwardRef<HTMLAnchorElement, Props>(
     const theme = useTheme2();
     const styles = getLinkStyles(theme, variant, weight, color, inline);
     const externalIcon = icon || 'external-link-alt';
+
     return external ? (
       <a href={validUrl} ref={ref} target="_blank" rel="noreferrer" {...rest} className={styles}>
         {children}
@@ -56,7 +60,8 @@ export const getLinkStyles = (
   variant?: keyof ThemeTypographyVariantTypes,
   weight?: Props['weight'],
   color?: Props['color'],
-  inline = true
+  inline = true,
+  textAlignment: Props['textAlignment'] = 'left'
 ) => {
   const linkColor = color ? customColor(color, theme) : theme.colors.text.link;
 
@@ -69,21 +74,28 @@ export const getLinkStyles = (
     },
     `
       color: ${linkColor};
-      display: block;
+      display: flex;
+      justify-content: ${getFlexAlignment(textAlignment)};
       &:hover {
         text-decoration: underline;
       };
-      & > div > svg {
-        margin-left: ${theme.spacing(1)};
+      & > div {
+        display: flex;
+
+        & > svg {
+          margin-left: ${theme.spacing(1)};
+          align-self: center;
+        }
       }
+    }
       `,
     inline && {
-      display: 'inline',
+      display: 'inline-flex',
     },
   ]);
 };
 
-export function getSvgVariantSize(variant: keyof ThemeTypographyVariantTypes = 'body') {
+const getSvgVariantSize = (variant: keyof ThemeTypographyVariantTypes = 'body') => {
   if (variant === 'h1' || variant === 'h2') {
     return 'xl';
   } else if (variant === 'h3' || variant === 'h4') {
@@ -93,4 +105,14 @@ export function getSvgVariantSize(variant: keyof ThemeTypographyVariantTypes = '
   } else {
     return 'md';
   }
-}
+};
+
+const getFlexAlignment = (textAlignment: Props['textAlignment']) => {
+  if (textAlignment === 'right') {
+    return 'flex-end';
+  } else if (textAlignment === 'center') {
+    return 'center';
+  } else {
+    return 'flex-start';
+  }
+};
