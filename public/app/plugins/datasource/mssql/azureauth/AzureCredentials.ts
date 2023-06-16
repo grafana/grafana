6c8ae742589f3e6d37f852c8c57a9cfe7275b741
jev forsberg <jev.forsberg@grafana.com>
2023-06-16 11:48:37 -0600

    baldm0mma/mssql_fe/ add AzureCredentials.ts

---
+++ public/app/plugins/datasource/mssql/azureauth/AzureCredentials.ts
@@ -0,0 +1,46 @@
+import { SelectableValue } from '@grafana/data';
+
+export enum AzureCloud {
+  Public = 'AzureCloud',
+  China = 'AzureChinaCloud',
+  USGovernment = 'AzureUSGovernment',
+  None = '',
+}
+
+export const KnownAzureClouds: Array<SelectableValue<AzureCloud>> = [
+  { value: AzureCloud.Public, label: 'Azure' },
+  { value: AzureCloud.China, label: 'Azure China' },
+  { value: AzureCloud.USGovernment, label: 'Azure US Government' },
+];
+
+export type AzureAuthType = 'msi' | 'clientsecret';
+
+export type ConcealedSecret = symbol;
+
+interface AzureCredentialsBase {
+  authType: AzureAuthType;
+  defaultSubscriptionId?: string;
+}
+
+export interface AzureManagedIdentityCredentials extends AzureCredentialsBase {
+  authType: 'msi';
+}
+
+export interface AzureClientSecretCredentials extends AzureCredentialsBase {
+  authType: 'clientsecret';
+  azureCloud?: string;
+  tenantId?: string;
+  clientId?: string;
+  clientSecret?: string | ConcealedSecret;
+}
+
+export type AzureCredentials = AzureManagedIdentityCredentials | AzureClientSecretCredentials;
+
+export function isCredentialsComplete(credentials: AzureCredentials): boolean {
+  switch (credentials.authType) {
+    case 'msi':
+      return true;
+    case 'clientsecret':
+      return !!(credentials.azureCloud && credentials.tenantId && credentials.clientId && credentials.clientSecret);
+  }
+}
