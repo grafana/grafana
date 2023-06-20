514307ead19555fb0ead6a45817925d830fcb146
Galen <galen.kistler@grafana.com>
2023-06-20 14:06:54 -0500

    WIP

--- public/app/plugins/datasource/influxdb/components/editor/query/fsql/FSQLEditor.tsx
+++ public/app/plugins/datasource/influxdb/components/editor/query/fsql/FSQLEditor.tsx
@@ -32,7 +32,7 @@
 // };
 
 import { css, cx } from '@emotion/css';
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data/src';
 import { InlineFormLabel, LinkButton, Themeable2, withTheme2 } from '@grafana/ui/src';
@@ -47,72 +47,74 @@ import { FlightSQLDatasource } from './FlightSQLDatasource';
 interface Props extends Themeable2 {
   onChange: (query: InfluxQuery) => void;
   onRunQuery: () => void;
-  query: InfluxQuery;
+  query: SQLQuery;
   datasource: InfluxDatasource;
 }
 
-function UnthemedSQLQueryEditor(props: Props) {
-  console.log('UnthemedSQLQueryEditor init');
-  const { query, theme, datasource, onRunQuery, onChange } = props;
-  const styles = getStyles(theme);
+class UnthemedSQLQueryEditor extends PureComponent<Props> {
+  render() {
+    const { query, theme, datasource, onRunQuery, onChange } = this.props;
+    const styles = getStyles(theme);
 
-  const flightSQLDatasource = new FlightSQLDatasource({
-    url: datasource.urls[0],
-    access: datasource.access,
-    id: datasource.id,
-    // So the datasource wants some connection stuff that doesn't apply to flightSQL? Ignoring for now.
-    //@ts-ignore @todo fix PoC code
-    jsonData: { ...datasource.jsonData, allowCleartextPasswords: false },
-    meta: datasource.meta,
-    name: datasource.name,
-    readOnly: false,
-    type: datasource.type,
-    uid: datasource.uid,
-  });
+    const flightSQLDatasource = new FlightSQLDatasource({
+      url: datasource.urls[0],
+      access: datasource.access,
+      id: datasource.id,
+      // So the datasource wants some connection stuff that doesn't apply to flightSQL? Ignoring for now.
+      //@ts-ignore @todo fix PoC code
+      jsonData: { ...datasource.jsonData, allowCleartextPasswords: false },
+      meta: datasource.meta,
+      name: datasource.name,
+      readOnly: false,
+      type: datasource.type,
+      uid: datasource.uid,
+    });
 
-  const onRunSQLQuery = () => {
-    console.log('RUN');
-    return onRunQuery();
-  };
+    const onRunSQLQuery = () => {
+      console.log('RUN');
+      return onRunQuery();
+    };
 
-  const onSQLChange = (query: SQLQuery) => {
-    // query => rawSql for now
-    props.onChange({ ...query, query: query.rawSql, resultFormat: query.format });
-  };
+    const onSQLChange = (query: SQLQuery) => {
+      // query => rawSql for now
+      console.log('ON CHANGE', query);
+      onChange({ ...query });
+    };
 
-  const helpTooltip = (
-    <div>
-      Type: <i>ctrl+space</i> to show template variable suggestions <br />
-      Many queries can be copied from Chronograf
-    </div>
-  );
-
-  return (
-    <>
-      <SqlQueryEditor
-        datasource={flightSQLDatasource}
-        query={query}
-        onRunQuery={onRunSQLQuery}
-        onChange={onSQLChange}
-      />
-      <div className={cx('gf-form-inline', styles.editorActions)}>
-        <LinkButton
-          icon="external-link-alt"
-          variant="secondary"
-          target="blank"
-          href="https://docs.influxdata.com/influxdb/latest/query-data/get-started/"
-        >
-          SQL language syntax (fix link)
-        </LinkButton>
-        <div className="gf-form gf-form--grow">
-          <div className="gf-form-label gf-form-label--grow"></div>
-        </div>
-        <InlineFormLabel width={5} tooltip={helpTooltip}>
-          Help
-        </InlineFormLabel>
+    const helpTooltip = (
+      <div>
+        Type: <i>ctrl+space</i> to show template variable suggestions <br />
+        Many queries can be copied from Chronograf
       </div>
-    </>
-  );
+    );
+
+    return (
+      <>
+        <SqlQueryEditor
+          datasource={flightSQLDatasource}
+          query={query}
+          onRunQuery={onRunSQLQuery}
+          onChange={onSQLChange}
+        />
+        <div className={cx('gf-form-inline', styles.editorActions)}>
+          <LinkButton
+            icon="external-link-alt"
+            variant="secondary"
+            target="blank"
+            href="https://docs.influxdata.com/influxdb/latest/query-data/get-started/"
+          >
+            SQL language syntax (fix link)
+          </LinkButton>
+          <div className="gf-form gf-form--grow">
+            <div className="gf-form-label gf-form-label--grow"></div>
+          </div>
+          <InlineFormLabel width={5} tooltip={helpTooltip}>
+            Help
+          </InlineFormLabel>
+        </div>
+      </>
+    );
+  }
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
