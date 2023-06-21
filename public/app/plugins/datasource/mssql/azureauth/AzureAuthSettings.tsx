59655a49dc49c789b60da7965e8fd7bfcb803efd
jev forsberg <jev.forsberg@grafana.com>
2023-06-21 16:04:52 -0600

    baldm0mma/mssql_fe/ clean up azureAuthSettings

--- public/app/plugins/datasource/mssql/azureauth/AzureAuthSettings.tsx
+++ public/app/plugins/datasource/mssql/azureauth/AzureAuthSettings.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo } from 'react';
 
 import { config } from '@grafana/runtime';
-// import { InlineField, InlineFieldRow, InlineSwitch, Input } from '@grafana/ui';
 import { HttpSettingsBaseProps } from '@grafana/ui/src/components/DataSourceSettings/types';
 
 import { KnownAzureClouds, AzureCredentials } from './AzureCredentials';
@@ -12,74 +11,20 @@ export const AzureAuthSettings = (props: HttpSettingsBaseProps) => {
   const { dataSourceConfig: dsSettings, onChange } = props;
   const managedIdentityEnabled = config.azure.managedIdentityEnabled;
 
-  // const [overrideAudienceAllowed] = useState<boolean>(
-  //   // JEV: FF this feature/or remove?
-  //   config.featureToggles.prometheusAzureOverrideAudience || !!dsSettings.jsonData.azureEndpointResourceId
-  // );
-
-  // const [overrideAudienceChecked, setOverrideAudienceChecked] = useState<boolean>(
-  //   !!dsSettings.jsonData.azureEndpointResourceId
-  // );
-
   const credentials = useMemo(() => getCredentials(dsSettings, config), [dsSettings]);
 
   const onCredentialsChange = (credentials: AzureCredentials): void => {
     onChange(updateCredentials(dsSettings, config, credentials));
   };
 
-  // const onOverrideAudienceChange = (ev: FormEvent<HTMLInputElement>): void => {
-  //   setOverrideAudienceChecked(ev.currentTarget.checked);
-  //   if (!ev.currentTarget.checked) {
-  //     onChange({
-  //       ...dsSettings,
-  //       jsonData: { ...dsSettings.jsonData, azureEndpointResourceId: undefined },
-  //     });
-  //   }
-  // };
-
-  // const onResourceIdChange = (ev: FormEvent<HTMLInputElement>): void => {
-  //   if (overrideAudienceChecked) {
-  //     onChange({
-  //       ...dsSettings,
-  //       jsonData: { ...dsSettings.jsonData, azureEndpointResourceId: ev.currentTarget.value },
-  //     });
-  //   }
-  // };
-
   return (
-    <>
-      {/* <h6>Azure authentication</h6> */}
-      <AzureCredentialsForm
-        managedIdentityEnabled={managedIdentityEnabled}
-        credentials={credentials}
-        azureCloudOptions={KnownAzureClouds}
-        onCredentialsChange={onCredentialsChange}
-        disabled={dsSettings.readOnly}
-      />
-      {/* {overrideAudienceAllowed && (
-        <>
-          <h6>Azure configuration</h6>
-          <div className="gf-form-group">
-            <InlineFieldRow>
-              <InlineField labelWidth={26} label="Override AAD audience" disabled={dsSettings.readOnly}>
-                <InlineSwitch value={overrideAudienceChecked} onChange={onOverrideAudienceChange} />
-              </InlineField>
-            </InlineFieldRow>
-            {overrideAudienceChecked && (
-              <InlineFieldRow>
-                <InlineField labelWidth={26} label="Resource ID" disabled={dsSettings.readOnly}>
-                  <Input
-                    className="width-30"
-                    value={dsSettings.jsonData.azureEndpointResourceId || ''}
-                    onChange={onResourceIdChange}
-                  />
-                </InlineField>
-              </InlineFieldRow>
-            )}
-          </div>
-        </>
-      )} */}
-    </>
+    <AzureCredentialsForm
+      managedIdentityEnabled={managedIdentityEnabled}
+      credentials={credentials}
+      azureCloudOptions={KnownAzureClouds}
+      onCredentialsChange={onCredentialsChange}
+      disabled={dsSettings.readOnly}
+    />
   );
 };
 
