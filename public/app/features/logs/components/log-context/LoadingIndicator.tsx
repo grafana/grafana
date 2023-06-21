af0a176b28e62fc46cb38184409f9dc6e4142b45
Sven Grossmann <svennergr@gmail.com>
2023-06-21 13:14:53 +0200

    better positioning of loading indicators

---
+++ public/app/features/logs/components/log-context/LoadingIndicator.tsx
@@ -0,0 +1,22 @@
+import { css } from '@emotion/css';
+import React from 'react';
+
+import { Spinner } from '@grafana/ui';
+
+// ideally we'd use `@grafana/ui/LoadingPlaceholder`, but that
+// one has a large margin-bottom.
+export const LoadingIndicator = ({ place }: { place: 'top' | 'bottom' }) => {
+  const text = place === 'top' ? 'Loading newer logs...' : 'Loading older logs...';
+  return (
+    <div className={loadingIndicatorStyles}>
+      <div>
+        {text} <Spinner inline />
+      </div>
+    </div>
+  );
+};
+
+const loadingIndicatorStyles = css`
+  display: flex;
+  justify-content: center;
+`;
