e7235fc4749a8415decd19c70430745bf83fdcab
Jack Westbrook <jack.westbrook@gmail.com>
2023-06-26 11:37:13 +0200

    refactor(plugins): clean up cache buster and tests

--- public/app/features/plugins/systemjsPlugins/pluginCacheBuster.test.ts
+++ public/app/features/plugins/systemjsPlugins/pluginCacheBuster.test.ts
@@ -8,40 +8,40 @@ describe('PluginCacheBuster', () => {
   it('should append plugin version as cache flag if plugin is registered in buster', () => {
     const slug = 'bubble-chart-1';
     const version = 'v1.0.0';
-    const path = resolvePath(slug);
+    const path = `plugins/${slug}/module`;
     const address = `http://localhost:3000/public/${path}.js`;
 
     registerPluginInCache({ path, version });
 
     const url = `${address}?_cache=${encodeURI(version)}`;
-    expect(locateWithCache({ address }, now)).toBe(url);
+    expect(locateWithCache(address, now)).toBe(url);
   });
 
   it('should append Date.now as cache flag if plugin is not registered in buster', () => {
     const slug = 'bubble-chart-2';
-    const address = `http://localhost:3000/public/${resolvePath(slug)}.js`;
+    const address = `http://localhost:3000/public/plugins/${slug}/module.js`;
 
     const url = `${address}?_cache=${encodeURI(String(now))}`;
-    expect(locateWithCache({ address }, now)).toBe(url);
+    expect(locateWithCache(address, now)).toBe(url);
   });
 
   it('should append Date.now as cache flag if plugin is invalidated in buster', () => {
     const slug = 'bubble-chart-3';
     const version = 'v1.0.0';
-    const path = resolvePath(slug);
+    const path = `plugins/${slug}/module`;
     const address = `http://localhost:3000/public/${path}.js`;
 
     registerPluginInCache({ path, version });
     invalidatePluginInCache(slug);
 
     const url = `${address}?_cache=${encodeURI(String(now))}`;
-    expect(locateWithCache({ address }, now)).toBe(url);
+    expect(locateWithCache(address, now)).toBe(url);
   });
 
   it('should also clear plugin settings cache', () => {
     const slug = 'bubble-chart-3';
     const version = 'v1.0.0';
-    const path = resolvePath(slug);
+    const path = `plugins/${slug}/module`;
 
     const clearPluginSettingsCacheSpy = jest.spyOn(pluginSettings, 'clearPluginSettingsCache');
 
@@ -52,7 +52,3 @@ describe('PluginCacheBuster', () => {
     expect(clearPluginSettingsCacheSpy).toBeCalledWith('bubble-chart-3');
   });
 });
-
-function resolvePath(slug: string): string {
-  return `plugins/${slug}/module`;
-}
