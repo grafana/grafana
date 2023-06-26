cf9fde415573659eb48a4944b152af35475f6b70
Giuseppe Guerra <giuseppe@guerra.in>
2023-06-26 17:54:48 +0200

    Register dynamicAngularDetector background service

--- pkg/server/backgroundsvcs/background_services.go
+++ pkg/server/backgroundsvcs/background_services.go
@@ -23,6 +23,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/ngalert"
 	"github.com/grafana/grafana/pkg/services/notifications"
 	plugindashboardsservice "github.com/grafana/grafana/pkg/services/plugindashboards/service"
+	"github.com/grafana/grafana/pkg/services/pluginsintegration/angulardetectorsprovider"
 	"github.com/grafana/grafana/pkg/services/pluginsintegration/keyretriever/dynamic"
 	"github.com/grafana/grafana/pkg/services/provisioning"
 	publicdashboardsmetric "github.com/grafana/grafana/pkg/services/publicdashboards/metric"
@@ -52,7 +53,7 @@ func ProvideBackgroundServiceRegistry(
 	bundleService *supportbundlesimpl.Service,
 	publicDashboardsMetric *publicdashboardsmetric.Service,
 	keyRetriever *dynamic.KeyRetriever,
-	// dynamicAngularDetector *angulardetectorsprovider.Dynamic,
+	dynamicAngularDetector *angulardetectorsprovider.Dynamic,
 	// Need to make sure these are initialized, is there a better place to put them?
 	_ dashboardsnapshots.Service, _ *alerting.AlertNotificationService,
 	_ serviceaccounts.Service, _ *guardian.Provider,
@@ -90,7 +91,7 @@ func ProvideBackgroundServiceRegistry(
 		bundleService,
 		publicDashboardsMetric,
 		keyRetriever,
-		// dynamicAngularDetector,
+		dynamicAngularDetector,
 	)
 }
 
