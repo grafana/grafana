af27525128bafa7aed851fe0bf6374131f8ca7f0
Selene <selenepinillos@gmail.com>
2023-06-27 11:47:14 +0200

    Chore: Generate plugin version and remove lineage version (#69781)
    * Generate plugin version and remove lineage version
    
    * Generate version for each major
    
    * Remove pluginVersion from core plugins
    
    * Remove code version generation
    
    * Generate cue files
    
    * Move grafana version into jenny
    
    * fix go import

--- public/app/plugins/datasource/azuremonitor/dataquery.gen.ts
+++ public/app/plugins/datasource/azuremonitor/dataquery.gen.ts
@@ -10,8 +10,6 @@
 
 import * as common from '@grafana/schema';
 
-export const DataQueryModelVersion = Object.freeze([0, 0]);
-
 export interface AzureMonitorQuery extends common.DataQuery {
   /**
    * Azure Monitor Logs sub-query properties.
