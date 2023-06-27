a3f481d3c3e44ef8aa67e67d673723d3630c05be
Alex Simonok <sashasimonok@gmail.com>
2023-06-27 13:32:03 +0300

    Add all selected variable options for add annotation form

--- public/app/plugins/panel/timeseries/plugins/AnnotationEditorPlugin.tsx
+++ public/app/plugins/panel/timeseries/plugins/AnnotationEditorPlugin.tsx
@@ -3,9 +3,11 @@ import { useMountedState } from 'react-use';
 import uPlot from 'uplot';
 
 import { CartesianCoords2D, DataFrame, TimeZone } from '@grafana/data';
+import { getTemplateSrv } from '@grafana/runtime';
 import { PlotSelection, UPlotConfigBuilder } from '@grafana/ui';
 
 import { AnnotationEditor } from './annotations/AnnotationEditor';
+import { AnnotationsDataFrameViewDTO } from './types';
 
 type StartAnnotatingFn = (props: {
   // pixel coordinates of the clicked point on the uPlot canvas
@@ -19,6 +21,17 @@ interface AnnotationEditorPluginProps {
   children?: (props: { startAnnotating: StartAnnotatingFn }) => React.ReactNode;
 }
 
+const getTagsFromVariables = () => {
+  const variables = getTemplateSrv().getVariables();
+  return variables.reduce((acc: string[], variable) => {
+    if ('options' in variable) {
+      const selectedOptions = variable.options.filter((option) => option.selected);
+      return acc.concat(...selectedOptions.map((option) => option.text || ''));
+    }
+    return acc;
+  }, []);
+};
+
 /**
  * @alpha
  */
@@ -27,6 +40,7 @@ export const AnnotationEditorPlugin: React.FC<AnnotationEditorPluginProps> = ({ 
   const [bbox, setBbox] = useState<DOMRect>();
   const [isAddingAnnotation, setIsAddingAnnotation] = useState(false);
   const [selection, setSelection] = useState<PlotSelection | null>(null);
+  const [annotation, setAnnotation] = useState<Pick<AnnotationsDataFrameViewDTO, 'time' | 'timeEnd' | 'tags'>>();
   const isMounted = useMountedState();
 
   const clearSelection = useCallback(() => {
@@ -36,6 +50,7 @@ export const AnnotationEditorPlugin: React.FC<AnnotationEditorPluginProps> = ({ 
       plotInstance.current.setSelect({ top: 0, left: 0, width: 0, height: 0 });
     }
     setIsAddingAnnotation(false);
+    setAnnotation(undefined);
   }, [setIsAddingAnnotation, setSelection]);
 
   useLayoutEffect(() => {
@@ -70,15 +85,22 @@ export const AnnotationEditorPlugin: React.FC<AnnotationEditorPluginProps> = ({ 
     const setSelect = (u: uPlot) => {
       if (annotating) {
         setIsAddingAnnotation(true);
+        const min = u.posToVal(u.select.left, 'x');
+        const max = u.posToVal(u.select.left + u.select.width, 'x');
         setSelection({
-          min: u.posToVal(u.select.left, 'x'),
-          max: u.posToVal(u.select.left + u.select.width, 'x'),
+          min,
+          max,
           bbox: {
             left: u.select.left,
             top: 0,
             height: u.select.height,
             width: u.select.width,
           },
+        });
+        setAnnotation({
+          time: min,
+          timeEnd: max,
+          tags: getTagsFromVariables(),
         });
         annotating = false;
       }
@@ -129,6 +151,11 @@ export const AnnotationEditorPlugin: React.FC<AnnotationEditorPluginProps> = ({ 
           width: 0,
         },
       });
+      setAnnotation({
+        time: min,
+        timeEnd: min,
+        tags: getTagsFromVariables(),
+      });
       setIsAddingAnnotation(true);
     },
     [bbox]
@@ -138,6 +165,7 @@ export const AnnotationEditorPlugin: React.FC<AnnotationEditorPluginProps> = ({ 
     <>
       {isAddingAnnotation && selection && bbox && (
         <AnnotationEditor
+          annotation={annotation as AnnotationsDataFrameViewDTO}
           selection={selection}
           onDismiss={clearSelection}
           onSave={clearSelection}
