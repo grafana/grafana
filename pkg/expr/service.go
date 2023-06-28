1f38d0cc693ccc97bb214bac4758cd0c8ea8ebeb
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-06-28 16:38:56 -0400

    tests

--- pkg/expr/service.go
+++ pkg/expr/service.go
@@ -15,6 +15,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/datasources"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/pluginsintegration/plugincontext"
+	"github.com/grafana/grafana/pkg/services/user"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
@@ -57,13 +58,18 @@ func NodeTypeFromDatasourceUID(uid string) NodeType {
 type Service struct {
 	cfg          *setting.Cfg
 	dataService  backend.QueryDataHandler
-	pCtxProvider *plugincontext.Provider
+	pCtxProvider pluginContextProvider
 	features     featuremgmt.FeatureToggles
 
 	pluginsClient backend.CallResourceHandler
 
 	tracer  tracing.Tracer
 	metrics *metrics
+}
+
+type pluginContextProvider interface {
+	Get(ctx context.Context, pluginID string, user *user.SignedInUser, orgID int64) (backend.PluginContext, error)
+	GetWithDataSource(ctx context.Context, pluginID string, user *user.SignedInUser, ds *datasources.DataSource) (backend.PluginContext, error)
 }
 
 func ProvideService(cfg *setting.Cfg, pluginClient plugins.Client, pCtxProvider *plugincontext.Provider,
