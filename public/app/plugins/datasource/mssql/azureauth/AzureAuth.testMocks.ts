08de90c8ce7e2b70d39b6d905b1edbc06bb0344b
jev forsberg <jev.forsberg@grafana.com>
2023-06-28 15:37:54 -0600

    baldm0mma/mssql_fe add ignore

--- public/app/plugins/datasource/mssql/azureauth/AzureAuth.testMocks.ts
+++ public/app/plugins/datasource/mssql/azureauth/AzureAuth.testMocks.ts
@@ -5,10 +5,12 @@ import { AzureAuthSecureJSONDataType, AzureAuthJSONDataType } from '../types';
 
 export const configWithManagedIdentityEnabled: GrafanaBootConfig = {
   azure: { managedIdentityEnabled: true, userIdentityEnabled: false },
+  // @ts-ignore
 } as unknown as GrafanaBootConfig;
 
 export const configWithManagedIdentityDisabled: GrafanaBootConfig = {
   azure: { managedIdentityEnabled: false, userIdentityEnabled: false, cloud: 'AzureCloud' },
+  // @ts-ignore
 } as unknown as GrafanaBootConfig;
 
 export const dataSourceSettingsWithMsiCredentials: DataSourceSettings<
@@ -16,13 +18,10 @@ export const dataSourceSettingsWithMsiCredentials: DataSourceSettings<
   AzureAuthSecureJSONDataType
 > = {
   jsonData: { azureCredentials: { authType: 'msi' } },
+  // @ts-ignore
 } as unknown as DataSourceSettings<AzureAuthJSONDataType, AzureAuthSecureJSONDataType>;
 
-// Will return symbol as the secret is concealed
-export const dataSourceSettingsWithClientSecretOnServer: DataSourceSettings<
-  AzureAuthJSONDataType,
-  AzureAuthSecureJSONDataType
-> = {
+const basicJSONData = {
   jsonData: {
     azureCredentials: {
       authType: 'clientsecret',
@@ -30,20 +29,24 @@ export const dataSourceSettingsWithClientSecretOnServer: DataSourceSettings<
       clientId: 'XXXX-client-id-XXXX',
     },
   },
-  secureJsonFields: { azureClientSecret: true },
-} as unknown as DataSourceSettings<AzureAuthJSONDataType, AzureAuthSecureJSONDataType>;
+};
+
+// Will return symbol as the secret is concealed
+export const dataSourceSettingsWithClientSecretOnServer: DataSourceSettings<
+  AzureAuthJSONDataType,
+  AzureAuthSecureJSONDataType
+  // @ts-ignore
+> = { ...basicJSONData, secureJsonFields: { azureClientSecret: true } } as unknown as DataSourceSettings<
+  AzureAuthJSONDataType,
+  AzureAuthSecureJSONDataType
+>;
 
 // Will return the secret as a string from the secureJsonData
 export const dataSourceSettingsWithClientSecretInSecureJSONData: DataSourceSettings<
   AzureAuthJSONDataType,
   AzureAuthSecureJSONDataType
 > = {
-  jsonData: {
-    azureCredentials: {
-      authType: 'clientsecret',
-      tenantId: 'XXXX-tenant-id-XXXX',
-      clientId: 'XXXX-client-id-XXXX',
-    },
-  },
+  ...basicJSONData,
   secureJsonData: { azureClientSecret: 'XXXX-super-secret-secret-XXXX' },
+  // @ts-ignore
 } as unknown as DataSourceSettings<AzureAuthJSONDataType, AzureAuthSecureJSONDataType>;
