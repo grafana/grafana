7e220b1525710ba5ada0e440e4cf1afe36c0dac8
Leon Sorokin <leeoniya@gmail.com>
2023-06-29 08:58:46 -0500

    randomize content length

--- packages/grafana-ui/src/components/uPlot/plugins/utils.ts
+++ packages/grafana-ui/src/components/uPlot/plugins/utils.ts
@@ -1,11 +1,11 @@
 const CONTENT_LINES = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+Lorem ipsum dolor sit amet, consectetur
 sed do eiusmod tempor incididunt ut labore et dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit
-esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia
+aliqua. Ut enim ad minim veniam,
+ullamco laboris nisi ut aliquip ex ea commodo
+Duis aute irure dolor in reprehenderit in
+esse cillum dolore
+occaecat cupidatat non
 deserunt mollit anim id est laborum.
 `
   .trim()
