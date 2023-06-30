61d816b137ab89c959624c3264f4f6d1f14b5fca
Galen <galen.kistler@grafana.com>
2023-06-30 07:18:00 -0500

    WIP

--- pkg/services/sqlstore/migrations/influx_database_mig.go
+++ pkg/services/sqlstore/migrations/influx_database_mig.go
@@ -1,22 +1,36 @@
 package migrations
 
-import "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
+import (
+	. "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
+)
 
-func addInfluxDatabaseMigration(mg *migrator.Migrator) {
-	mg.AddMigration("Add influx database table", migrator.NewRawSQLMigration(`
-UPDATE "data_source" AS ds
+func addInfluxDatabaseMigration(mg *Migrator) {
+	//
+	//mg.AddMigration("Update uid column values in alert_notification", new(RawSQLMigration).
+	//	SQLite("UPDATE alert_notification SET uid=printf('%09d',id) WHERE uid IS NULL;").
+	//	Postgres("UPDATE alert_notification SET uid=lpad('' || id::text,9,'0') WHERE uid IS NULL;").
+	//	Mysql("UPDATE alert_notification SET uid=lpad(id,9,'0') WHERE uid IS NULL;"))
+
+	mg.AddMigration("Add influx database table", new(RawSQLMigration).
+		SQLite(`
+UPDATE data_source
+SET json_data = json_set(json_data, '$.dbName', data_source.database)
+WHERE data_source.type = 'influxdb'
+  AND data_source.database <> ''
+`).
+		Postgres(`
+UPDATE data_source
+SET json_data = jsonb_set(json_data, '{database}', '{"dbName": ds.database}')
+`).
+		Mysql(`
+UPDATE data_source
 SET
-    ds.json_data = JSON_SET(ds.json_data, '$.database', '{"dbName": ds.database}')
+    json_data = JSON_SET(json_data, '$.database', '{"dbName": ds.database}')
 WHERE
-    ds.type = 'influxdb'
+    type = 'influxdb'
 AND
-    ds.database IS NOT NULL;
-
-
+    database IS NOT NULL
+AND id =130
 `))
-}
 
-mg.AddMigration("Update uid column values in alert_notification", new(RawSQLMigration).
-	SQLite("UPDATE alert_notification SET uid=printf('%09d',id) WHERE uid IS NULL;").
-	Postgres("UPDATE alert_notification SET uid=lpad('' || id::text,9,'0') WHERE uid IS NULL;").
-	Mysql("UPDATE alert_notification SET uid=lpad(id,9,'0') WHERE uid IS NULL;"))
+}
