e55ec33f25ae32834893556603a3553f0f4ffa22
Matias Chomicki <matyax@gmail.com>
2023-07-04 15:02:40 +0200

    Derived fields: refactor legacy styles

--- public/app/plugins/datasource/loki/configuration/DerivedFields.tsx
+++ public/app/plugins/datasource/loki/configuration/DerivedFields.tsx
@@ -12,12 +12,17 @@ import { DebugSection } from './DebugSection';
 import { DerivedField } from './DerivedField';
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  infoText: css`
-    padding-bottom: ${theme.spacing(2)};
-    color: ${theme.colors.text.secondary};
+  addButton: css`
+    margin-right: 10px;
   `,
   derivedField: css`
     margin-bottom: ${theme.spacing(1)};
+  `,
+  container: css`
+    margin-bottom: ${theme.spacing(4)};
+  `,
+  debugSection: css`
+    margin-top: ${theme.spacing(4)};
   `,
 });
 
@@ -50,7 +55,7 @@ export const DerivedFields = ({ fields = [], onChange }: Props) => {
         />
       }
     >
-      <div className="gf-form-group">
+      <div className={styles.container}>
         {fields.map((field, index) => {
           return (
             <DerivedField
@@ -82,9 +87,7 @@ export const DerivedFields = ({ fields = [], onChange }: Props) => {
         <div>
           <Button
             variant="secondary"
-            className={css`
-              margin-right: 10px;
-            `}
+            className={styles.addButton}
             icon="plus"
             onClick={(event) => {
               event.preventDefault();
@@ -101,18 +104,18 @@ export const DerivedFields = ({ fields = [], onChange }: Props) => {
             </Button>
           )}
         </div>
-      </div>
 
-      {showDebug && (
-        <div className="gf-form-group">
-          <DebugSection
-            className={css`
-              margin-bottom: 10px;
-            `}
-            derivedFields={fields}
-          />
-        </div>
-      )}
+        {showDebug && (
+          <div className={styles.debugSection}>
+            <DebugSection
+              className={css`
+                margin-bottom: 10px;
+              `}
+              derivedFields={fields}
+            />
+          </div>
+        )}
+      </div>
     </ConfigSubSection>
   );
 };
