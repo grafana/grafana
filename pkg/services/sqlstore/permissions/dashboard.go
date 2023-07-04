a70abc3aac7aef2ef9900a853ee376550b66a653
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-07-04 09:08:49 +0300

    MySQL < 8.0 changes

--- pkg/services/sqlstore/permissions/dashboard.go
+++ pkg/services/sqlstore/permissions/dashboard.go
@@ -210,7 +210,7 @@ func (f *accessControlDashboardPermissionFilter) buildClauses() {
 					f.addRecQry(recQueryName, permSelector.String(), permSelectorArgs)
 					builder.WriteString(fmt.Sprintf("(folder.uid IN (SELECT uid FROM %s", recQueryName))
 				default:
-					nestedFoldersSelectors, nestedFoldersArgs := nestedFoldersSelectors(permSelector.String(), permSelectorArgs, "folder_id", "id")
+					nestedFoldersSelectors, nestedFoldersArgs := nestedFoldersSelectors(permSelector.String(), permSelectorArgs, "folder")
 					builder.WriteRune('(')
 					builder.WriteString(nestedFoldersSelectors)
 					args = append(args, nestedFoldersArgs...)
@@ -260,7 +260,7 @@ func (f *accessControlDashboardPermissionFilter) buildClauses() {
 					builder.WriteString("(dashboard.uid IN ")
 					builder.WriteString(fmt.Sprintf("(SELECT uid FROM %s)", recQueryName))
 				default:
-					nestedFoldersSelectors, nestedFoldersArgs := nestedFoldersSelectors(permSelector.String(), permSelectorArgs, "uid", "uid")
+					nestedFoldersSelectors, nestedFoldersArgs := nestedFoldersSelectors(permSelector.String(), permSelectorArgs, "dashboard")
 					builder.WriteRune('(')
 					builder.WriteString(nestedFoldersSelectors)
 					builder.WriteRune(')')
@@ -340,26 +340,26 @@ func actionsToCheck(actions []string, permissions map[string][]string, wildcards
 	return toCheck
 }
 
-func nestedFoldersSelectors(permSelector string, permSelectorArgs []interface{}, leftTableCol string, rightTableCol string) (string, []interface{}) {
+func nestedFoldersSelectors(permSelector string, permSelectorArgs []interface{}, leftTable string) (string, []interface{}) {
 	wheres := make([]string, 0, folder.MaxNestedFolderDepth+1)
 	args := make([]interface{}, 0, len(permSelectorArgs)*(folder.MaxNestedFolderDepth+1))
 
 	joins := make([]string, 0, folder.MaxNestedFolderDepth+2)
 
-	tmpl := "INNER JOIN folder %s ON %s.%s = %s.uid AND %s.org_id = %s.org_id "
+	tmpl := "INNER JOIN folder %s ON %s.parent_uid = %s.uid AND %s.org_id = %s.org_id"
 
-	prev := "d"
-	onCol := "uid"
-	for i := 1; i <= folder.MaxNestedFolderDepth+2; i++ {
+	wheres = append(wheres, fmt.Sprintf("(%s.uid IN (SELECT substr(scope, 13) FROM permission WHERE scope LIKE '%s' AND %s)", leftTable, "folders:uid:%", permSelector))
+	args = append(args, permSelectorArgs...)
+	prev := "f1"
+	for i := 2; i <= folder.MaxNestedFolderDepth+2; i++ {
 		t := fmt.Sprintf("f%d", i)
-		s := fmt.Sprintf(tmpl, t, prev, onCol, t, prev, t)
+		s := fmt.Sprintf(tmpl, t, prev, t, prev, t)
 		joins = append(joins, s)
 
-		wheres = append(wheres, fmt.Sprintf("(dashboard.%s IN (SELECT d.%s FROM dashboard d %s WHERE %s.uid IN (SELECT substr(scope, 13) FROM permission WHERE scope LIKE '%s' AND %s))", leftTableCol, rightTableCol, strings.Join(joins, " "), t, "folders:uid:%", permSelector))
+		wheres = append(wheres, fmt.Sprintf("(folder.uid IN (SELECT f1.uid FROM folder f1 %s AND %s.uid IN (SELECT substr(scope, 13) FROM permission WHERE scope LIKE '%s' AND %s))", strings.Join(joins, " "), t, "folders:uid:%", permSelector))
 		args = append(args, permSelectorArgs...)
 
 		prev = t
-		onCol = "parent_uid"
 	}
 
 	return strings.Join(wheres, ") OR "), args
