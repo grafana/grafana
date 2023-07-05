281304d6ee473cb3b8f226b941c257607a18f244
Galen <galen.kistler@grafana.com>
2023-07-05 09:13:49 -0500

    update sql query

--- pkg/services/sqlstore/migrations/influx_database_mig.go
+++ pkg/services/sqlstore/migrations/influx_database_mig.go
@@ -12,13 +12,28 @@ SET json_data = json_set(json_data, '$.dbName', data_source.database)
 WHERE data_source.type = 'influxdb'
   AND data_source.database IS NOT NULL
   AND data_source.database <> ''
-  AND JSON_VALID(json_data)
+  AND (
+      json_extract(json_data, '$.dbName') IS NULL
+      OR json_extract(json_data, '$.dbName') = ''
+      )
+  AND JSON_VALID(json_data);
+
+UPDATE data_source
+SET database = ''
+WHERE data_source.type = 'influxdb'
+  AND data_source.database IS NOT NULL
+  AND data_source.database <> ''
+  AND (
+        json_extract(json_data, '$.dbName') IS NOT NULL
+        OR json_extract(json_data, '$.dbName') <> ''
+    )
+  AND JSON_VALID(json_data);
 `).
 		Postgres(`
 UPDATE data_source
 SET json_data = jsonb_set(cast(json_data AS jsonb), '{dbName}', concat('"', data_source.database, '"')::jsonb)
     AND data_source.database IS NOT NULL
-    AND (data_source.database <> '') IS NOT TRUE
+    AND (data_source.database <> '') IS NOT TRUE	
 `).
 		Mysql(`
 UPDATE data_source
