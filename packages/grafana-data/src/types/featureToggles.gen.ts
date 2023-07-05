daf9f9cd199e0bbc110222a3f005dc05dab1681a
Galen Kistler <109082771+gtk-grafana@users.noreply.github.com>
2023-07-05 14:39:49 -0500

    Prometheus: Incremental querying profile data updates (#70701)
    * add prometheusIncrementalQueryInstrumentation feature flag, update instrumentation data

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -108,4 +108,5 @@ export interface FeatureToggles {
   flameGraphV2?: boolean;
   elasticToggleableFilters?: boolean;
   vizAndWidgetSplit?: boolean;
+  prometheusIncrementalQueryInstrumentation?: boolean;
 }
