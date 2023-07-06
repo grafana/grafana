6da6ead3ac73f6c517579175dbad19d198e82bbb
sjd210 <sjd210@cam.ac.uk>
2023-07-06 15:25:26 +0100

    Revert "RemoveFirstLastQuotes"
    This reverts commit e1ef5764b896084d9bdf2564c6f4d1d0fd76db07.

--- public/app/features/plugins/sql/datasource/SqlDatasource.ts
+++ public/app/features/plugins/sql/datasource/SqlDatasource.ts
@@ -89,7 +89,7 @@ export abstract class SqlDatasource extends DataSourceWithBackend<SQLQuery, SQLO
 
     if (Array.isArray(value)) {
       const quotedValues = value.map((v) => this.getQueryModel().quoteLiteral(v));
-      return quotedValues.join(',').slice(1, -1);
+      return quotedValues.join(',');
     }
 
     return value;
