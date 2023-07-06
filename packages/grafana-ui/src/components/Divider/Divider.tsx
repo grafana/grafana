4cb346d083844069a354ed48330f00561348d4cb
Roxana Turc <anamaria-roxana.turc@grafana.com>
2023-07-06 16:50:03 +0100

    Made changes suggested to the component, story and docs

--- packages/grafana-ui/src/components/Divider/Divider.tsx
+++ packages/grafana-ui/src/components/Divider/Divider.tsx
@@ -5,11 +5,11 @@ import { GrafanaTheme2 } from '@grafana/data';
 
 import { useTheme2 } from '../../themes';
 
-interface Props {
+interface DividerProps {
   direction?: 'vertical' | 'horizontal';
 }
 
-export const Divider = ({ direction = 'horizontal' }: Props) => {
+export const Divider = ({ direction = 'horizontal' }: DividerProps) => {
   const theme = useTheme2();
   const styles = getStyles(theme);
 
@@ -25,7 +25,7 @@ Divider.displayName = 'Divider';
 const getStyles = (theme: GrafanaTheme2) => {
   return {
     horizontalDivider: css`
-      border-bottom: 1px solid ${theme.colors.border.weak};
+      border-top: 1px solid ${theme.colors.border.weak};
       margin: ${theme.spacing(2, 0)};
       width: 100%;
     `,
