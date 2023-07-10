dffdd48a6782f2abdb5c3e08472de30c1daa58e8
Matias Chomicki <matyax@gmail.com>
2023-07-10 17:35:16 +0200

    LogRowMessageDisplayedFields: turn into component

--- public/app/features/logs/components/LogRowMessageDisplayedFields.tsx
+++ public/app/features/logs/components/LogRowMessageDisplayedFields.tsx
@@ -1,51 +1,49 @@
 import { css } from '@emotion/css';
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import { LogRowModel, Field, LinkModel, DataFrame } from '@grafana/data';
-import { withTheme2, Themeable2 } from '@grafana/ui';
 
 import { getAllFields } from './logParser';
 
-export interface Props extends Themeable2 {
+export interface Props {
   row: LogRowModel;
   showDetectedFields: string[];
   wrapLogMessage: boolean;
   getFieldLinks?: (field: Field, rowIndex: number, dataFrame: DataFrame) => Array<LinkModel<Field>>;
 }
 
-class UnThemedLogRowMessageDisplayedFields extends PureComponent<Props> {
-  render() {
-    const { row, showDetectedFields, getFieldLinks, wrapLogMessage } = this.props;
-    const fields = getAllFields(row, getFieldLinks);
-    const wrapClassName = wrapLogMessage
-      ? ''
-      : css`
-          white-space: nowrap;
-        `;
-    // only single key/value rows are filterable, so we only need the first field key for filtering
-    const line = showDetectedFields
-      .map((parsedKey) => {
-        const field = fields.find((field) => {
-          const { keys } = field;
-          return keys[0] === parsedKey;
-        });
+export const LogRowMessageDisplayedFields = React.memo((props: Props) => {
+  const { row, showDetectedFields, getFieldLinks, wrapLogMessage } = props;
+  const fields = getAllFields(row, getFieldLinks);
+  const wrapClassName = wrapLogMessage ? '' : styles.noWrap;
+  // only single key/value rows are filterable, so we only need the first field key for filtering
+  const line = showDetectedFields
+    .map((parsedKey) => {
+      const field = fields.find((field) => {
+        const { keys } = field;
+        return keys[0] === parsedKey;
+      });
 
-        if (field !== undefined && field !== null) {
-          return `${parsedKey}=${field.values}`;
-        }
+      if (field !== undefined && field !== null) {
+        return `${parsedKey}=${field.values}`;
+      }
 
-        if (row.labels[parsedKey] !== undefined && row.labels[parsedKey] !== null) {
-          return `${parsedKey}=${row.labels[parsedKey]}`;
-        }
+      if (row.labels[parsedKey] !== undefined && row.labels[parsedKey] !== null) {
+        return `${parsedKey}=${row.labels[parsedKey]}`;
+      }
 
-        return null;
-      })
-      .filter((s) => s !== null)
-      .join(' ');
+      return null;
+    })
+    .filter((s) => s !== null)
+    .join(' ');
 
-    return <td className={wrapClassName}>{line}</td>;
-  }
-}
+  return <td className={wrapClassName}>{line}</td>;
+});
 
-export const LogRowMessageDisplayedFields = withTheme2(UnThemedLogRowMessageDisplayedFields);
+const styles = {
+  noWrap: css`
+    white-space: nowrap;
+  `,
+};
+
 LogRowMessageDisplayedFields.displayName = 'LogRowMessageDisplayedFields';
