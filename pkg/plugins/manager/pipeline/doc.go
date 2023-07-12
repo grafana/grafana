55f79b08463dcfe6a80955e26fc793823816ad43
Will Browne <will.browne@grafana.com>
2023-07-12 12:12:08 +0100

    update doc

--- pkg/plugins/manager/pipeline/doc.go
+++ pkg/plugins/manager/pipeline/doc.go
@@ -2,7 +2,7 @@
 //
 // A pipeline is a sequence of stages that are executed in order. Each stage is made up of a series of steps.
 // A plugin loader pipeline is defined by the following stages:
-// 	 Discovery: Find plugins (e.g. from disk, remote, etc.), filter the results based on some criteria and construct the plugin base.
+// 	 Discovery: Find plugins (e.g. from disk, remote, etc.), and filter the results based on some criteria.
 // 	 Bootstrap: Create the plugins found in the discovery stage and enrich them with metadata.
 // 	 Verification: Verify the plugins based on some criteria (e.g. signature validation, angular detection, etc.)
 // 	 Initialization: Initialize the plugin for use (e.g. register with Grafana, etc.)
