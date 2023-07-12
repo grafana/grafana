170d1f50127150b5c40d7d153217b4c8d4127e79
Karl Persson <kalle.persson@grafana.com>
2023-07-12 11:45:20 +0200

    Migration: Remove unique constraint

--- pkg/services/sqlstore/migrations/accesscontrol/scope_split.go
+++ pkg/services/sqlstore/migrations/accesscontrol/scope_split.go
@@ -13,7 +13,7 @@ func AddScopeSplitMigrations(mg *migrator.Migrator) {
 	mg.AddMigration("permission scope split migration", &scopeSplitMigrator{})
 
 	mg.AddMigration("add permission kind_attribute_identifier index", migrator.NewAddIndexMigration(permissionTable, &migrator.Index{
-		Cols: []string{"kind", "attribute", "identifier"}, Type: migrator.UniqueIndex,
+		Cols: []string{"kind", "attribute", "identifier"},
 	}))
 
 	mg.AddMigration("add permission identifier index", migrator.NewAddIndexMigration(permissionTable, &migrator.Index{
