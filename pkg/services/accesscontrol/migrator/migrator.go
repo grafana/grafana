81343efe65cce9f8586800f90b303a6f5ab5ac68
Karl Persson <kalle.persson@grafana.com>
2023-07-13 15:12:36 +0200

    Implement migrator

--- pkg/services/accesscontrol/migrator/migrator.go
+++ pkg/services/accesscontrol/migrator/migrator.go
@@ -1,33 +1,45 @@
 package migrator
 
 import (
+	"context"
+	"time"
+
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
 )
 
+// TODO: perform updates in a more efficient way
+// TODO: should we feature flag this?
 func MigrateScopeSplit(db db.DB, log log.Logger) error {
-	// TODO: DROP INDEXES IF EXISTS
-	/*
-			var permissions []accesscontrol.Permission
+	t := time.Now()
+	var count = 0
+	err := db.WithTransactionalDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
+		// TODO: DROP INDEXES IF EXISTS?
+		var permissions []accesscontrol.Permission
 
-			sess.SQL("SELECT * FROM permission WHERE scope IS NOT ''").Find(&permissions)
+		sess.SQL("SELECT * FROM permission WHERE NOT scope = '' AND kind = '' AND attribute = '' AND identifier = ''").Find(&permissions)
 
-			for i, p := range permissions {
-				kind, attribute, identifier := p.SplitScope()
+		for i, p := range permissions {
+			count++
+			kind, attribute, identifier := p.SplitScope()
 
-				permissions[i].Kind = kind
-				permissions[i].Attribute = attribute
-				permissions[i].Identifier = identifier
+			permissions[i].Kind = kind
+			permissions[i].Attribute = attribute
+			permissions[i].Identifier = identifier
 
-				// TODO: batch update
-				_, err := sess.Exec("UPDATE permission SET kind = ?, attribute = ?, identifier = ? WHERE id = ?", permissions[i].Kind, permissions[i].Attribute, permissions[i].Identifier, permissions[i].ID)
-				if err != nil {
-					return err
-				}
+			// TODO: batch update
+			_, err := sess.Exec("UPDATE permission SET kind = ?, attribute = ?, identifier = ? WHERE id = ?", permissions[i].Kind, permissions[i].Attribute, permissions[i].Identifier, permissions[i].ID)
+			if err != nil {
+				return err
 			}
+		}
 
-			return nil
+		return nil
+	})
 
-	*/
-	return nil
+	log.Debug("Migrated permissions ", "count", count, "in", time.Since(t))
+
+	return err
 }
