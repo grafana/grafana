bf64ac3c85e0606670372a83ba21fb4b5aebc7f9
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2023-07-13 15:31:09 -0400

    chore: wrap HTTP server in a dskit module
    Much of the logic from this comes from the POC branch, so:
    - credit for this work goes to everyone else
    - mistakes are my own
    
    This is needed to support microservice deployment modes.

--- pkg/modules/dependencies.go
+++ pkg/modules/dependencies.go
@@ -2,13 +2,14 @@ package modules
 
 const (
 	// All includes all modules necessary for Grafana to run as a standalone application.
-	All string = "all"
-	// BackgroundServices includes all Grafana services that run in the background
+	All                string = "all"
 	BackgroundServices string = "background-services"
 	// CertGenerator generates certificates for grafana-apiserver
 	CertGenerator string = "cert-generator"
 	// GrafanaAPIServer is the Kubertenes API server for Grafana Resources
 	GrafanaAPIServer string = "grafana-apiserver"
+	// HTTPServer is the HTTP server for Grafana
+	HTTPServer string = "http-server"
 )
 
 // dependencyMap defines Module Targets => Dependencies
@@ -18,5 +19,5 @@ var dependencyMap = map[string][]string{
 	CertGenerator:    {},
 	GrafanaAPIServer: {CertGenerator},
 
-	All: {BackgroundServices},
+	All: {BackgroundServices, HTTPServer},
 }
