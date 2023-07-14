14e4a8d0bf6185d57758e0f869e5ced9601010fa
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-07-14 14:35:24 -0400

    tests

---
+++ pkg/expr/nodes_bench_test.go
@@ -0,0 +1,74 @@
+package expr
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+)
+
+func BenchmarkWideToMany(b *testing.B) {
+	b.Run("time and *float64", func(b *testing.B) {
+		f := data.NewFrame("",
+			data.NewField("Time", nil, []time.Time{}))
+		for i := 0; i < 10; i++ {
+			lbls := make(data.Labels, 5)
+			for j := 0; j < 5; j++ {
+				lbls[fmt.Sprintf("lbl%d", j)] = fmt.Sprintf("value%d", i)
+			}
+			f.Fields = append(f.Fields, data.NewField(fmt.Sprintf("val-%d", i), lbls, []*float64{}))
+		}
+		for i := 0; i < 100; i++ {
+			row := make([]interface{}, 0, len(f.Fields))
+			row = append(row, time.Now().Add(-time.Duration(i)))
+			for j := 1; j < cap(row); j++ {
+				v := rand.Float64()
+				row = append(row, &v)
+			}
+			f.AppendRow(row...)
+		}
+
+		b.ReportAllocs()
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, err := WideToMany(f)
+			if err != nil {
+				panic(err)
+			}
+		}
+	})
+
+	b.Run("*time and int64", func(b *testing.B) { // worst case
+		f := data.NewFrame("",
+			data.NewField("Time", nil, []*time.Time{}))
+		for i := 0; i < 10; i++ {
+			lbls := make(data.Labels, 5)
+			for j := 0; j < 5; j++ {
+				lbls[fmt.Sprintf("lbl%d", j)] = fmt.Sprintf("value%d", i)
+			}
+			f.Fields = append(f.Fields, data.NewField(fmt.Sprintf("val-%d", i), lbls, []int64{}))
+		}
+		for i := 0; i < 100; i++ {
+			row := make([]interface{}, 0, len(f.Fields))
+			now := time.Now().Add(-time.Duration(i))
+			row = append(row, &now)
+			for j := 1; j < cap(row); j++ {
+				row = append(row, rand.Int63())
+			}
+			f.AppendRow(row...)
+		}
+
+		b.ReportAllocs()
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, err := WideToMany(f)
+			if err != nil {
+				panic(err)
+			}
+		}
+	})
+}
--- pkg/expr/nodes_test.go
+++ pkg/expr/nodes_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 
@@ -271,4 +272,43 @@ func TestConvertDataFramesToResults(t *testing.T) {
 			}
 		})
 	})
+}
+
+func TestWideToMany(t *testing.T) {
+	f := data.NewFrame("Test",
+		data.NewField("Time", nil, []time.Time{}))
+	for i := 0; i < 10; i++ {
+		lbls := make(data.Labels, 5)
+		for j := 0; j < 5; j++ {
+			lbls[fmt.Sprintf("lbl%d", j)] = fmt.Sprintf("value%d", i)
+		}
+		f.Fields = append(f.Fields, data.NewField(fmt.Sprintf("val-%d", i), lbls, []*float64{}))
+	}
+	for i := 0; i < 100; i++ {
+		row := make([]interface{}, 0, len(f.Fields))
+		row = append(row, time.Now().Add(-time.Duration(i)))
+		for j := 1; j < cap(row); j++ {
+			v := rand.Float64()
+			row = append(row, &v)
+		}
+		f.AppendRow(row...)
+	}
+
+	ser, err := WideToMany(f)
+	if err != nil {
+		require.NoError(t, err)
+	}
+	require.Len(t, ser, 10)
+
+	timeField := f.Fields[0]
+	for idx, series := range ser {
+		field := f.Fields[idx+1]
+		require.Equal(t, field.Len(), series.Len())
+		require.EqualValues(t, field.Labels, series.GetLabels())
+		for i := 0; i < field.Len(); i++ {
+			actualTime, actualValue := series.GetPoint(i)
+			require.EqualValues(t, timeField.At(i), actualTime)
+			require.EqualValues(t, field.At(i), actualValue)
+		}
+	}
 }
