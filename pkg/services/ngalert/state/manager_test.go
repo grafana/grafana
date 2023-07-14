3ecbca4c9d6932d3273479bf40fcd1ec0c144b24
Alex Weaver <weaver.alex.d@gmail.com>
2023-07-14 15:55:48 -0500

    Refactor annotation historian to isolate dashboard service dependency

--- pkg/services/ngalert/state/manager_test.go
+++ pkg/services/ngalert/state/manager_test.go
@@ -226,7 +226,8 @@ func TestDashboardAnnotations(t *testing.T) {
 
 	fakeAnnoRepo := annotationstest.NewFakeAnnotationsRepo()
 	metrics := metrics.NewHistorianMetrics(prometheus.NewRegistry())
-	hist := historian.NewAnnotationBackend(fakeAnnoRepo, &dashboards.FakeDashboardService{}, nil, metrics)
+	store := historian.NewAnnotationStore(fakeAnnoRepo, &dashboards.FakeDashboardService{}, metrics)
+	hist := historian.NewAnnotationBackend(store, nil, metrics)
 	cfg := state.ManagerCfg{
 		Metrics:                 testMetrics.GetStateMetrics(),
 		ExternalURL:             nil,
@@ -2256,7 +2257,8 @@ func TestProcessEvalResults(t *testing.T) {
 	for _, tc := range testCases {
 		fakeAnnoRepo := annotationstest.NewFakeAnnotationsRepo()
 		metrics := metrics.NewHistorianMetrics(prometheus.NewRegistry())
-		hist := historian.NewAnnotationBackend(fakeAnnoRepo, &dashboards.FakeDashboardService{}, nil, metrics)
+		store := historian.NewAnnotationStore(fakeAnnoRepo, &dashboards.FakeDashboardService{}, metrics)
+		hist := historian.NewAnnotationBackend(store, nil, metrics)
 		cfg := state.ManagerCfg{
 			Metrics:                 testMetrics.GetStateMetrics(),
 			ExternalURL:             nil,
