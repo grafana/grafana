b6bf30649e654c2e9aa295f3f7f4587843ac9bbb
Matias Chomicki <matyax@gmail.com>
2023-07-17 17:10:55 +0200

    Remove unused imports

--- public/app/features/logs/components/LogRowMessageDisplayedFields.tsx
+++ public/app/features/logs/components/LogRowMessageDisplayedFields.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 
 import { LogRowModel, Field, LinkModel, DataFrame } from '@grafana/data';
 
