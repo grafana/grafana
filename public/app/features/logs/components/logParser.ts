7ef357aa32fb0c6c08b0d1b7f5465537475060d9
Matias Chomicki <matyax@gmail.com>
2023-07-18 18:39:19 +0200

    LogParser: safely stringify field value (#71873)
    * LogParser: safely stringify field value
    
    * LogParser: move null check to the top

--- public/app/features/logs/components/logParser.ts
+++ public/app/features/logs/components/logParser.ts
@@ -1,6 +1,7 @@
 import memoizeOne from 'memoize-one';
 
 import { DataFrame, Field, FieldType, LinkModel, LogRowModel } from '@grafana/data';
+import { safeStringifyValue } from 'app/core/utils/explore';
 import { ExploreFieldLinkModel } from 'app/features/explore/utils/links';
 
 export type FieldDef = {
@@ -69,9 +70,14 @@ export const getDataframeFields = memoizeOne(
       .filter((field, index) => !shouldRemoveField(field, index, row))
       .map((field) => {
         const links = getFieldLinks ? getFieldLinks(field, row.rowIndex, row.dataFrame) : [];
+        const fieldVal = field.values[row.rowIndex];
+        const outputVal =
+          typeof fieldVal === 'string' || typeof fieldVal === 'number'
+            ? fieldVal.toString()
+            : safeStringifyValue(fieldVal);
         return {
           keys: [field.name],
-          values: [field.values[row.rowIndex].toString()],
+          values: [outputVal],
           links: links,
           fieldIndex: field.index,
         };
@@ -80,6 +86,11 @@ export const getDataframeFields = memoizeOne(
 );
 
 function shouldRemoveField(field: Field, index: number, row: LogRowModel) {
+  // field that has empty value (we want to keep 0 or empty string)
+  if (field.values[row.rowIndex] == null) {
+    return true;
+  }
+
   // hidden field, remove
   if (field.config.custom?.hidden) {
     return true;
@@ -88,11 +99,6 @@ function shouldRemoveField(field: Field, index: number, row: LogRowModel) {
   // field with data-links, keep
   if ((field.config.links ?? []).length > 0) {
     return false;
-  }
-
-  // field that has empty value (we want to keep 0 or empty string)
-  if (field.values[row.rowIndex] == null) {
-    return true;
   }
 
   // the remaining checks use knowledge of how we parse logs-dataframes
