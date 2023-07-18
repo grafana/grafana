78728b9e1d57dcf5d20f0b19d2b5ad476fb33dc9
Dora <103416234+doracretu3pillar@users.noreply.github.com>
2023-07-18 19:43:41 +0300

    PMM-12191 Backup: Allow Compatible Services for all vendors type (#672)
    * PMM-12191: Allow Compatible Services for all vendors type
    
    * PMM-12191: Disable radio button only if vendor is not mysql or mongo
    
    ---------
    
    Co-authored-by: “dorac-ext” <dora.cioaba@yahoo.com>
    Co-authored-by: Artem Gavrilov <charlieblackwood7@gmail.com>

--- public/app/percona/backup/components/BackupInventory/RestoreBackupModal/RestoreBackupModal.tsx
+++ public/app/percona/backup/components/BackupInventory/RestoreBackupModal/RestoreBackupModal.tsx
@@ -289,7 +289,10 @@ export const RestoreBackupModal: FC<RestoreBackupModalProps> = ({
                     name="serviceType"
                     label={Messages.serviceSelection}
                     fullWidth
-                    disabled={values.vendor !== DATABASE_LABELS[Databases.mysql]}
+                    disabled={
+                      values.vendor !== DATABASE_LABELS[Databases.mysql] &&
+                      values.vendor !== DATABASE_LABELS[Databases.mongodb]
+                    }
                   />
                   <TextInputField disabled name="vendor" label={Messages.vendor} />
 
