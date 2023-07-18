0b8140487cc9fa286b3241b52a4c5083be095de5
Matias Chomicki <matyax@gmail.com>
2023-07-18 17:03:05 +0200

    ModifyQuery: use Lucene parser to detect key:values in queries

--- public/app/plugins/datasource/elasticsearch/modifyQuery.ts
+++ public/app/plugins/datasource/elasticsearch/modifyQuery.ts
@@ -1,22 +1,41 @@
+import lucene, { AST, NodeTerm } from 'lucene';
+
 import { escapeRegex } from '@grafana/data';
 
 type ModifierType = '' | '-';
+
+// @ts-ignore
+window.lucene = lucene;
+// @ts-ignore
+window.queryHasFilter = queryHasFilter;
 
 /**
  * Checks for the presence of a given label:"value" filter in the query.
  */
 export function queryHasFilter(query: string, key: string, value: string, modifier: ModifierType = ''): boolean {
-  key = escapeFilter(key);
-  const regex = getFilterRegex(key, value);
-  const matches = query.matchAll(regex);
-  for (const match of matches) {
-    if (modifier === '-' && match[0].startsWith(modifier)) {
+  const field = `${modifier}${lucene.term.escape(key)}`;
+  let ast: AST | null = null;
+  try {
+    ast = lucene.parse(normalizeQuery(query));
+  } catch (e) {
+    console.log(query);
+    return false;
+  }
+
+  let node = getNode(ast);
+  while (node) {
+    if (node.field === field && node.term === value) {
       return true;
     }
-    if (modifier === '' && !match[0].startsWith('-')) {
-      return true;
+    if (getNextAST(ast)) {
+      ast = getNextAST(ast);
+      node = getNode(ast);
+    } else {
+      node = getNextNode(ast);
+      ast = null;
     }
   }
+
   return false;
 }
 
@@ -65,4 +84,66 @@ export function removeFilterFromQuery(query: string, key: string, value: string,
  */
 export function escapeFilter(value: string) {
   return value.replace(/:/g, '\\:');
+}
+
+/**
+ * Normalizes the query by removing whitespace around colons, which breaks parsing.
+ */
+function normalizeQuery(query: string) {
+  return query.replace(/\s+:\s+/g, ':');
+}
+
+/**
+ * Given an AST, resolve the NodeTerm:
+ * - if it's a NodeTerm, return it
+ * - if left is defined and is a NodeTerm, return it
+ * - otherwise return null
+ */
+function getNode(ast: AST | null): NodeTerm | null {
+  if (!ast) {
+    return null;
+  }
+  if ('term' in ast) {
+    return ast;
+  }
+  if ('left' in ast && 'term' in ast.left) {
+    return ast.left;
+  }
+  return null;
+}
+
+/**
+ * Given an AST, resolve the next AST:
+ * - if right is defined and is a NodeTerm, return it
+ * - otherwise return null
+ */
+function getNextAST(ast: AST | null): AST | null {
+  if (!ast) {
+    return null;
+  }
+  if (!('right' in ast)) {
+    return null;
+  }
+  if ('left' in ast.right) {
+    return ast.right;
+  }
+  return null;
+}
+
+/**
+ * When the NextAST is null, resolve the NodeTerm from the current AST:
+ * - if right is defined and is a NodeTerm, return it
+ * - otherwise return null
+ */
+function getNextNode(ast: AST | null): NodeTerm | null {
+  if (!ast) {
+    return null;
+  }
+  if (!('right' in ast)) {
+    return null;
+  }
+  if ('term' in ast.right) {
+    return ast.right;
+  }
+  return null;
 }
