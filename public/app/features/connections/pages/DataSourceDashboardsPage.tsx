9a990908b2c1f02f836cbc9d6cd62474518d0830
Miklós Tolnai <miklos.tolnai@grafana.com>
2023-07-18 15:06:13 +0200

    fix datasource dashboard page's nav

--- public/app/features/connections/pages/DataSourceDashboardsPage.tsx
+++ public/app/features/connections/pages/DataSourceDashboardsPage.tsx
@@ -4,16 +4,14 @@ import { useParams } from 'react-router-dom';
 import { Page } from 'app/core/components/Page/Page';
 
 import { DataSourceDashboards } from '../components/DataSourceDashboards';
-import { useDataSourceSettingsNav } from '../state';
+import { useDataSourceSettingsNav } from '../hooks/useDataSourceSettingsNav';
 
 export function DataSourceDashboardsPage() {
   const { uid } = useParams<{ uid: string }>();
-  const params = new URLSearchParams(location.search);
-  const pageId = params.get('page');
-  const nav = useDataSourceSettingsNav(uid, pageId);
+  const { navId, pageNav } = useDataSourceSettingsNav();
 
   return (
-    <Page navId="connections-datasources" pageNav={nav.main}>
+    <Page navId={navId} pageNav={pageNav}>
       <Page.Contents>
         <DataSourceDashboards uid={uid} />
       </Page.Contents>
