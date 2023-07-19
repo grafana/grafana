14b905d198f807762b9f78519af300e7fb99ee84
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2023-07-19 12:13:06 +0100

    remove comments

--- pkg/services/accesscontrol/migrator/migrator.go
+++ pkg/services/accesscontrol/migrator/migrator.go
@@ -10,8 +10,6 @@ import (
 	"github.com/grafana/grafana/pkg/services/sqlstore"
 )
 
-// TODO: perform updates in a more efficient way
-// TODO: should we feature flag this?
 func MigrateScopeSplit(db db.DB, log log.Logger) error {
 	t := time.Now()
 	var count = 0
@@ -31,7 +29,6 @@ func MigrateScopeSplit(db db.DB, log log.Logger) error {
 			permissions[i].Attribute = attribute
 			permissions[i].Identifier = identifier
 
-			// TODO: batch update
 			_, err := sess.Exec("UPDATE permission SET kind = ?, attribute = ?, identifier = ? WHERE id = ?", permissions[i].Kind, permissions[i].Attribute, permissions[i].Identifier, permissions[i].ID)
 			if err != nil {
 				return err
