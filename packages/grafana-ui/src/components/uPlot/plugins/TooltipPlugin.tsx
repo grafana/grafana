263bede8db8f4bca42b7140c41bed30752ffd390
Sol <94075844+sjd210@users.noreply.github.com>
2023-07-19 10:31:35 +0100

    Revert "Tooltip: Set tooltip of active panel to front of zIndex (#70747)"
    This reverts commit b1555c95950f2f36a7cb9debd0af66b69c624504.

--- packages/grafana-ui/src/components/uPlot/plugins/TooltipPlugin.tsx
+++ packages/grafana-ui/src/components/uPlot/plugins/TooltipPlugin.tsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/css';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useMountedState } from 'react-use';
 import uPlot from 'uplot';
@@ -13,12 +12,11 @@ import {
   formattedValueToString,
   getDisplayProcessor,
   getFieldDisplayName,
-  GrafanaTheme2,
   TimeZone,
 } from '@grafana/data';
 import { TooltipDisplayMode, SortOrder } from '@grafana/schema';
 
-import { useStyles2, useTheme2 } from '../../../themes/ThemeContext';
+import { useTheme2 } from '../../../themes/ThemeContext';
 import { Portal } from '../../Portal/Portal';
 import { SeriesTable, SeriesTableRowProps, VizTooltipContainer } from '../../VizTooltip';
 import { UPlotConfigBuilder } from '../config/UPlotConfigBuilder';
@@ -274,10 +272,8 @@ export const TooltipPlugin = ({
     tooltip = renderTooltip(otherProps.data, focusedSeriesIdx, focusedPointIdx);
   }
 
-  const style = useStyles2(getStyles);
-
   return (
-    <Portal className={isActive ? style.tooltipWrapper : undefined}>
+    <Portal>
       {tooltip && coords && (
         <VizTooltipContainer position={{ x: coords.x, y: coords.y }} offset={{ x: TOOLTIP_OFFSET, y: TOOLTIP_OFFSET }}>
           {tooltip}
@@ -325,9 +321,3 @@ export function positionTooltip(u: uPlot, bbox: DOMRect) {
 
   return { x, y };
 }
-
-const getStyles = (theme: GrafanaTheme2) => ({
-  tooltipWrapper: css`
-    z-index: ${theme.zIndex.portal + 1} !important;
-  `,
-});
