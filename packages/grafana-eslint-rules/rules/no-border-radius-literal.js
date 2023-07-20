2ded571487bb3ffe7398ada697cbddb2509d0baf
eledobleefe <laura.fernandez@grafana.com>
2023-07-20 12:22:50 +0200

    Move rule to grafana-eslint-rules/rules folder

---
+++ packages/grafana-eslint-rules/rules/no-border-radius-literal.js
@@ -0,0 +1,69 @@
+// @ts-check
+const { ESLintUtils } = require('@typescript-eslint/utils');
+
+
+const createRule = ESLintUtils.RuleCreator(
+  (name) => `https://github.com/grafana/grafana#${name}`
+);
+
+const borderRadiusRule = createRule({
+  create(context) {
+    return {
+      CallExpression(node) {
+        if (node.callee.parent === 'css') {
+          const borderRadius = node.arguments[0].properties.find((property) => {
+            return property.key?.name === 'borderRadius';
+          });
+          if (borderRadius && borderRadius.value.type === 'Literal') {
+            context.report({
+              node: node.arguments[0],
+              messageId: 'borderRadiusId'
+          });
+          }
+        }
+      },
+    };
+  },
+  name: 'no-border-radius-literal',
+  meta: {
+    docs: {
+      description: 'Check if border-radius theme tokens are used',
+      recommended: false,
+    },
+    messages: {
+      borderRadiusId: 'Custom values are not allowed for borderRadius. Use theme.shape tokens instead.',
+    },
+    schema: [],
+  }
+});
+
+module.exports = borderRadiusRule;
+
+// module.exports = {
+//   name: 'no-border-radius-literal',
+//   meta: {
+//     docs: {
+//       description: 'Check if border-radius theme tokens are used',
+//       category: 'Possible Errors',
+//       recommended: false,
+//     },
+//     schema: [],
+//   },
+//   create(context) {
+//     return {
+//       "CallExpression[callee.name='css']": (node) => {
+//         if (node.arguments[0].type === 'ObjectExpression') {
+//           const borderRadius = node.arguments[0].properties.find((property) => {
+//             return property.key?.name === 'borderRadius';
+//           });
+//           if (borderRadius && borderRadius.value.type === 'Literal') {
+//             context.report(
+//               node.arguments[0],
+//               'Custom values are not allowed for borderRadius. Use theme.shape tokens instead.'
+//             );
+//           }
+//         }
+//       },
+//     };
+//   },
+// };
