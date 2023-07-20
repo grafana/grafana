d259ca74848ddd2b271534d98fb4e91a91960a3f
Matias Chomicki <matyax@gmail.com>
2023-07-20 18:02:33 +0200

    Explore: document current limitation on isFilterLabelActive

--- public/app/features/explore/Explore.tsx
+++ public/app/features/explore/Explore.tsx
@@ -186,6 +186,8 @@ export class Explore extends React.PureComponent<Props, ExploreState> {
 
   /**
    * Used by Logs details.
+   * Returns true if all queries have the filter, otherwise false.
+   * @alpha
    */
   isFilterLabelActive = async (key: string, value: string) => {
     if (!config.featureToggles.toggleLabelsInLogsUI) {
