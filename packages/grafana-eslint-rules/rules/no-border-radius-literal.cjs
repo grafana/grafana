8b05af88d74700f5f709998a3f9c15a49f333a57
eledobleefe <laura.fernandez@grafana.com>
2023-07-21 13:15:07 +0200

    Fix type problems in ESLint rule and delete duplicated file

--- packages/grafana-eslint-rules/rules/no-border-radius-literal.cjs
+++ packages/grafana-eslint-rules/rules/no-border-radius-literal.cjs
@@ -1,24 +1,28 @@
 // @ts-check
 const { ESLintUtils, AST_NODE_TYPES } = require('@typescript-eslint/utils');
 
-
-const createRule = ESLintUtils.RuleCreator(
-  (name) => `https://github.com/grafana/grafana#${name}`
-);
+const createRule = ESLintUtils.RuleCreator((name) => `https://github.com/grafana/grafana#${name}`);
 
 const borderRadiusRule = createRule({
   create(context) {
     return {
       CallExpression(node) {
-        if (node.callee.type === AST_NODE_TYPES.Identifier && node.callee.name === 'css' && node.arguments[0].type === 'ObjectExpression') {
-          const borderRadius = node.arguments[0].properties.find((property) => {
-            return property === 'borderRadius';
-          });
-          if (borderRadius && borderRadius.value.type === 'Literal') {
-            context.report({
-              node: node.arguments[0],
-              messageId: 'borderRadiusId'
-          });
+        if (
+          node.callee.type === AST_NODE_TYPES.Identifier &&
+          node.callee.name === 'css' &&
+          node.arguments[0].type === 'ObjectExpression'
+        ) {
+          for (let property of node.arguments[0].properties) {
+            if (
+              property.type === AST_NODE_TYPES.Property &&
+              property.key.type === AST_NODE_TYPES.Identifier &&
+              property.key.name === 'borderRadius'
+            ) {
+              context.report({
+                node: node.arguments[0],
+                messageId: 'borderRadiusId',
+              });
+            }
           }
         }
       },
@@ -26,6 +30,7 @@ const borderRadiusRule = createRule({
   },
   name: 'no-border-radius-literal',
   meta: {
+    type: 'problem',
     docs: {
       description: 'Check if border-radius theme tokens are used',
       recommended: false,
@@ -34,36 +39,9 @@ const borderRadiusRule = createRule({
       borderRadiusId: 'Custom values are not allowed for borderRadius. Use theme.shape tokens instead.',
     },
     schema: [],
-  }
+  },
+  defaultOptions: [],
 });
 
 module.exports = borderRadiusRule;
 
-// module.exports = {
-//   name: 'no-border-radius-literal',
-//   meta: {
-//     docs: {
-//       description: 'Check if border-radius theme tokens are used',
-//       category: 'Possible Errors',
-//       recommended: false,
-//     },
-//     schema: [],
-//   },
-//   create(context) {
-//     return {
-//       "CallExpression[callee.name='css']": (node) => {
-//         if (node.arguments[0].type === 'ObjectExpression') {
-//           const borderRadius = node.arguments[0].properties.find((property) => {
-//             return property.key?.name === 'borderRadius';
-//           });
-//           if (borderRadius && borderRadius.value.type === 'Literal') {
-//             context.report(
-//               node.arguments[0],
-//               'Custom values are not allowed for borderRadius. Use theme.shape tokens instead.'
-//             );
-//           }
-//         }
-//       },
-//     };
-//   },
-// };
