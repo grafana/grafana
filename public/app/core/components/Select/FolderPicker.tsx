9e8308002331c5a4d731b07be947f07c09bac01d
joshhunt <josh@trtr.co>
2023-07-24 11:48:56 +0100

    Use new NestedFolderPicker in SaveDashboardAsForm

--- public/app/core/components/Select/FolderPicker.tsx
+++ public/app/core/components/Select/FolderPicker.tsx
@@ -6,17 +6,40 @@ import { NestedFolderPicker, NestedFolderPickerProps } from '../NestedFolderPick
 
 import { OldFolderPicker } from './OldFolderPicker';
 
+interface FolderPickerProps extends NestedFolderPickerProps {
+  // These props are only used by the old folder picker, and should be removed when old picker is removed
+
+  /** @deprecated */
+  initialTitle?: string;
+
+  /** @deprecated */
+  dashboardId?: number | string;
+
+  /** @deprecated */
+  enableCreateNew?: boolean;
+}
+
 // Temporary wrapper component to switch between the NestedFolderPicker and the old flat
 // FolderPicker depending on feature flags
-export function FolderPicker(props: NestedFolderPickerProps) {
+export function FolderPicker(props: FolderPickerProps) {
   const nestedEnabled = config.featureToggles.nestedFolders && config.featureToggles.nestedFolderPicker;
-  return nestedEnabled ? <NestedFolderPicker {...props} /> : <OldFolderPickerWrapper {...props} />;
+  const { initialTitle, dashboardId, enableCreateNew, ...newFolderPickerProps } = props;
+
+  return nestedEnabled ? <NestedFolderPicker {...newFolderPickerProps} /> : <OldFolderPickerWrapper {...props} />;
 }
 
 // Converts new NestedFolderPicker props to old non-nested folder picker props
 // Seperate component so the hooks aren't created if not used
-function OldFolderPickerWrapper({ value, showRootFolder, onChange }: NestedFolderPickerProps) {
+function OldFolderPickerWrapper({
+  value,
+  showRootFolder,
+  onChange,
+  initialTitle,
+  dashboardId,
+  enableCreateNew,
+}: FolderPickerProps) {
   const [initialFolderUID] = useState(value);
+
   const handleOnChange = useCallback(
     (newFolder: { title: string; uid: string }) => {
       if (onChange) {
@@ -26,5 +49,14 @@ function OldFolderPickerWrapper({ value, showRootFolder, onChange }: NestedFolde
     [onChange]
   );
 
-  return <OldFolderPicker onChange={handleOnChange} initialFolderUid={initialFolderUID} showRoot={showRootFolder} />;
+  return (
+    <OldFolderPicker
+      onChange={handleOnChange}
+      showRoot={showRootFolder}
+      initialFolderUid={initialFolderUID}
+      initialTitle={initialTitle}
+      dashboardId={dashboardId}
+      enableCreateNew={enableCreateNew}
+    />
+  );
 }
