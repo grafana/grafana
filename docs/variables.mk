e48a7cce14726f4344ae292e430ac5a5a25450dd
Jack Baldry <jack.baldry@grafana.com>
2023-07-24 17:45:03 +0100

    Simplify workflow for parsing
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -1,11 +1,17 @@
+# Project target version to build by default.
+TARGET := next
+
 # List of projects to provide to the make-docs script.
-PROJECTS := grafana
+PROJECTS := grafana:$(TARGET)
+
+# Path to workflow containing doc-validator job.
+WORKFLOW := "$(shell git rev-parse --show-toplevel)/.github/workflows/build-technical-documentation.yml"
 
 # Use the doc-validator image defined in CI by default.
-export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: "(grafana/doc-validator[^"]+)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/doc-validator.yml")
+export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: (grafana/doc-validator.+),\1,p' $(WORKFLOW))
 
 # Skip some doc-validator checks.
-export DOC_VALIDATOR_SKIP_CHECKS := $(shell sed -En "s, *'--skip-checks=(.+)',\1,p" "$(shell git rev-parse --show-toplevel)/.github/workflows/doc-validator.yml")
+export DOC_VALIDATOR_SKIP_CHECKS := $(shell sed -En 's,^ *DOC_VALIDATOR_SKIP_CHECKS: (.+),\1,p' $(WORKFLOW))
 
 # Only run on sections that have been enabled in CI.
-export DOC_VALIDATOR_INCLUDE := $(shell sed -En "s, *'--include=\\^docs/sources/(.+)',/hugo/content/docs/grafana/latest/\1,p" "$(shell git rev-parse --show-toplevel)/.github/workflows/doc-validator.yml")
+export DOC_VALIDATOR_INCLUDE := $(shell sed -En 's,^ *DOC_VALIDATOR_INCLUDE: (.+),/hugo/content/docs/grafana/$(TARGET)/\1,p' $(WORKFLOW))
