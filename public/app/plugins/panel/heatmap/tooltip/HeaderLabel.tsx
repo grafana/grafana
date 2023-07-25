90bfcc9a759606cb51e1a8ca5450c7eb438edfac
Adela Almasan <adela.almasan@grafana.com>
2023-07-25 10:34:13 -0500

    header label overflow styling

--- public/app/plugins/panel/heatmap/tooltip/HeaderLabel.tsx
+++ public/app/plugins/panel/heatmap/tooltip/HeaderLabel.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/css';
 import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { HorizontalGroup, useStyles2 } from '@grafana/ui';
+import { HorizontalGroup, Tooltip, useStyles2 } from '@grafana/ui';
 
 import { LabelValue } from './tooltipUtils';
 
@@ -14,10 +14,13 @@ export const HeaderLabel = ({ headerLabel }: HeaderLabelProps) => {
   const styles = useStyles2(getStyles);
 
   return (
-    <HorizontalGroup justify-content="space-between" spacing="lg">
-      <div>
+    <HorizontalGroup justify-content="space-between" spacing="lg" wrap maxLength={260}>
+      <div className={styles.wrapper}>
         <span className={styles.label}>{headerLabel.label}</span>
-        <span className={styles.value}>{headerLabel.value}</span>
+        <Tooltip content={headerLabel.value}>
+          <span className={styles.value}>{headerLabel.value}</span>
+        </Tooltip>
+        <span className={styles.fadedMask}></span>
       </div>
     </HorizontalGroup>
   );
@@ -31,5 +34,33 @@ const getStyles = (theme: GrafanaTheme2) => ({
   value: css`
     font-weight: 500;
     line-height: 18px;
+    align-self: center;
+    max-width: 220px;
+  `,
+  wrapper: css`
+    display: flex;
+    flex-direction: row;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    //-webkit-mask-image: linear-gradient(90deg, #000 60%, transparent);
+  `,
+  fadedMask: css`
+    display: block;
+    position: absolute;
+    width: 200px;
+    height: 12px;
+    right: 25px;
+    top: 12px;
+    pointer-events: none;
+    background: linear-gradient(to right, transparent 30%, ${theme.colors.background.secondary} 100%);
+    background: -webkit-gradient(
+      linear,
+      left top,
+      right top,
+      color-stop(0%, transparent),
+      color-stop(100%, ${theme.colors.background.secondary})
+    );
+    background: -webkit-linear-gradient(left, transparent 30%, ${theme.colors.background.secondary} 100%);
   `,
 });
