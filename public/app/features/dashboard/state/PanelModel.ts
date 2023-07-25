b158f119e269cccc7f27c85467181d0f43bffedb
Giuseppe Guerra <giuseppe@guerra.in>
2023-07-25 10:14:17 +0200

    Fix tests

--- public/app/features/dashboard/state/PanelModel.ts
+++ public/app/features/dashboard/state/PanelModel.ts
@@ -620,7 +620,7 @@ export class PanelModel implements DataConfigSource, IPanelModel {
   }
 
   isAngularPlugin(): boolean {
-    return this.plugin?.meta.angularDetected ?? false;
+    return (this.plugin && this.plugin.angularPanelCtrl) !== undefined || (this.plugin?.meta?.angularDetected ?? false);
   }
 
   destroy() {
