5d7e24915ec336be03046ed3156623108ea5b51c
bohandley <brendan.ohandley@gmail.com>
2023-07-25 08:46:03 -0400

    update e2e tests for new connection component

--- packages/grafana-e2e-selectors/src/selectors/components.ts
+++ packages/grafana-e2e-selectors/src/selectors/components.ts
@@ -45,7 +45,7 @@ export const Components = {
         drop: 'TestData drop values',
       },
     },
-    DataSourceHttpSettings: {
+    ConnectionSettings: {
       urlInput: 'Datasource HTTP settings url',
     },
     Jaeger: {
