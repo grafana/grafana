eb9ef4533a76481e1457052adf63e281c8236bf4
Gabriel MABILLE <gabriel.mabille@grafana.com>
2023-07-25 15:26:37 +0200

    Revert to deleting/inserting but remove concurrency for SQLite3

--- pkg/services/accesscontrol/migrator/migrator.go
+++ pkg/services/accesscontrol/migrator/migrator.go
@@ -9,14 +9,13 @@ import (
 	"github.com/grafana/grafana/pkg/infra/log"
 	ac "github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/grafana/grafana/pkg/services/sqlstore/session"
 )
 
 func MigrateScopeSplit(db db.DB, log log.Logger) error {
 	t := time.Now()
 	ctx := context.Background()
-
-	var muCnt sync.Mutex
-	var cnt = 0
+	cnt := 0
 
 	// Search for the permissions to update
 	var permissions []ac.Permission
@@ -32,59 +31,53 @@ func MigrateScopeSplit(db db.DB, log log.Logger) error {
 		return nil
 	}
 
-	// Use multiple workers to update the permissions new fields by batch
-	errConcurrentUpdate := ac.ConcurrentBatch(ac.Concurrency, len(permissions), ac.BatchSize, func(start, end int) error {
-		n := end - start
-
-		var save [][]interface{}
-		if db.GetDialect().DriverName() == "mysql" {
-			save = saveQuerySQLite(start, end, permissions)
-		} else {
-			save = saveQuery(start, end, permissions)
-		}
-
-		// Batch update the permissions
-		if errBatchUpdate := db.WithTransactionalDbSession(ctx, func(sess *sqlstore.DBSession) error {
-			for i := range save {
-				if _, errSave := sess.Exec(save[i]...); errSave != nil {
-					log.Error("error saving permissions", "migration", "scopeSplit", "error", errSave)
-					return errSave
-				}
+	// SQLite does not seem to handle concurrency well
+	if db.GetDialect().DriverName() == "sqlite3" {
+		if errBatch := batch(len(permissions), ac.BatchSize, func(start, end int) error {
+			if err := batchScopeSplitScope(ctx, log, db, permissions, t, end, start); err != nil {
+				return err
 			}
+
+			cnt += end - start
 			return nil
-		}); errBatchUpdate != nil {
-			log.Error("error updating permission batch", "migration", "scopeSplit", "start", start, "end", end)
-			return errBatchUpdate
+		}); errBatch != nil {
+			log.Error("could not migrate permissions", "migration", "scopeSplit", "total", len(permissions), "succeeded", cnt, "left", len(permissions)-cnt, "error", errBatch)
+			return errBatch
 		}
+	} else {
+		var muCnt sync.Mutex
 
-		// Update count
-		muCnt.Lock()
-		cnt += n
-		muCnt.Unlock()
+		// Use multiple workers to update the permissions new fields by batch
+		errConcurrentUpdate := ac.ConcurrentBatch(ac.Concurrency, len(permissions), ac.BatchSize, func(start, end int) error {
+			if err := batchScopeSplitScope(ctx, log, db, permissions, t, end, start); err != nil {
+				return err
+			}
 
-		return nil
-	})
-	if errConcurrentUpdate != nil {
-		log.Error("could not migrate permissions", "migration", "scopeSplit", "total", len(permissions), "succeeded", cnt, "left", len(permissions)-cnt, "error", errConcurrentUpdate)
-		return errConcurrentUpdate
+			muCnt.Lock()
+			cnt += end - start
+			muCnt.Unlock()
+			return nil
+		})
+		if errConcurrentUpdate != nil {
+			log.Error("could not migrate permissions", "migration", "scopeSplit", "total", len(permissions), "succeeded", cnt, "left", len(permissions)-cnt, "error", errConcurrentUpdate)
+			return errConcurrentUpdate
+		}
 	}
 
 	log.Debug("migrated permissions", "migration", "scopeSplit", "total", len(permissions), "succeeded", cnt, "in", time.Since(t))
 	return nil
 }
 
-// This works on Postgres and MySQL but fails on SQLite (table lock)
-func saveQuery(start int, end int, permissions []ac.Permission) [][]interface{} {
-	t := time.Now()
+func batchScopeSplitScope(ctx context.Context, log log.Logger, db db.DB, permissions []ac.Permission, t time.Time, end int, start int) error {
 	n := end - start
 
 	// IDs to remove
 	delQuery := "DELETE FROM permission WHERE id IN ("
-	delArgs := make([]interface{}, 1, n+1)
+	delArgs := make([]interface{}, 0, n)
 
 	// Query to insert the updated permissions
 	insertQuery := "INSERT INTO permission (id, role_id, action, scope, kind, attribute, identifier, created, updated) VALUES "
-	insertArgs := make([]interface{}, 1, 9*n+1)
+	insertArgs := make([]interface{}, 0, 9*n)
 
 	// Prepare batch of updated permissions
 	for i := start; i < end; i++ {
@@ -102,35 +95,42 @@ func saveQuery(start int, end int, permissions []ac.Permission) [][]interface{} 
 	}
 	// Remove trailing ','
 	insertQuery = insertQuery[:len(insertQuery)-1]
-	insertArgs[0] = insertQuery
 
 	// Remove trailing ',' and close brackets
 	delQuery = delQuery[:len(delQuery)-1] + ")"
-	delArgs[0] = delQuery
 
-	return [][]interface{}{delArgs, insertArgs}
-}
-
-// This works on SQLite but results in a deadlock on MySQL (due to gap locking)
-func saveQuerySQLite(start int, end int, permissions []ac.Permission) [][]interface{} {
-	t := time.Now()
-	n := end - start
-
-	insertQuery := "REPLACE INTO permission (id, role_id, action, scope, kind, attribute, identifier, created, updated) VALUES "
-	insertArgs := make([]interface{}, 1, 9*n+1)
-
-	for i := start; i < end; i++ {
-		kind, attribute, identifier := permissions[i].SplitScope()
-
-		insertQuery += "(?, ?, ?, ?, ?, ?, ?, ?, ?),"
-		insertArgs = append(insertArgs, permissions[i].ID, permissions[i].RoleID,
-			permissions[i].Action, permissions[i].Scope,
-			kind, attribute, identifier,
-			permissions[i].Created, t,
-		)
+	// Batch update the permissions
+	if errBatchUpdate := db.GetSqlxSession().WithTransaction(ctx, func(tx *session.SessionTx) error {
+		if _, errDel := tx.Exec(ctx, delQuery, delArgs...); errDel != nil {
+			log.Error("error saving permissions", "migration", "scopeSplit", "error", errDel)
+			return errDel
+		}
+		if _, errInsert := tx.Exec(ctx, insertQuery, insertArgs...); errInsert != nil {
+			log.Error("error saving permissions", "migration", "scopeSplit", "error", errInsert)
+			return errInsert
+		}
+		return nil
+	}); errBatchUpdate != nil {
+		log.Error("error updating permission batch", "migration", "scopeSplit", "start", start, "end", end)
+		return errBatchUpdate
 	}
 
-	insertQuery = insertQuery[:len(insertQuery)-1]
-	insertArgs[0] = insertQuery
-	return [][]interface{}{insertArgs}
+	return nil
+}
+
+func batch(count, batchSize int, eachFn func(start, end int) error) error {
+	for i := 0; i < count; {
+		end := i + batchSize
+		if end > count {
+			end = count
+		}
+
+		if err := eachFn(i, end); err != nil {
+			return err
+		}
+
+		i = end
+	}
+
+	return nil
 }
