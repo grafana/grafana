e8226be3b37893bc516e5e95e6669f5bd01f66e5
Giuseppe Guerra <giuseppe@guerra.in>
2023-07-25 09:45:05 +0200

    lint

--- public/app/features/query/components/QueryGroup.test.tsx
+++ public/app/features/query/components/QueryGroup.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
