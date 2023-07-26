99f1f39a4d6ca527b3a90f19fde2b7f1105e71ac
Kyle Cunningham <kyle@codeincarnate.com>
2023-07-26 16:35:35 -0500

    Fix tests

--- packages/grafana-data/src/transformations/transformers/formatTime.test.ts
+++ packages/grafana-data/src/transformations/transformers/formatTime.test.ts
@@ -13,9 +13,10 @@ describe('Format Time Transformer', () => {
     const options = {
       timeField: 'time',
       outputFormat: 'YYYY-MM',
+      useTimezone: false,
     };
 
-    const formatter = createTimeFormatter(options.timeField, options.outputFormat);
+    const formatter = createTimeFormatter(options.timeField, options.outputFormat, options.useTimezone);
     const frame = toDataFrame({
       fields: [
         {
@@ -26,7 +27,7 @@ describe('Format Time Transformer', () => {
       ],
     });
 
-    const newFrame = formatter(frame.fields, [frame], frame);
+    const newFrame = formatter(frame.fields);
     expect(newFrame[0].values).toEqual(['2021-02', '2023-07', '2023-04', '2023-07', '2023-08']);
   });
 
@@ -34,9 +35,10 @@ describe('Format Time Transformer', () => {
     const options = {
       timeField: 'time',
       outputFormat: 'YYYY-MM h:mm:ss a',
+      useTimezone: false,
     };
 
-    const formatter = createTimeFormatter(options.timeField, options.outputFormat);
+    const formatter = createTimeFormatter(options.timeField, options.outputFormat, options.useTimezone);
     const frame = toDataFrame({
       fields: [
         {
@@ -47,7 +49,7 @@ describe('Format Time Transformer', () => {
       ],
     });
 
-    const newFrame = formatter(frame.fields, [frame], frame);
+    const newFrame = formatter(frame.fields);
     expect(newFrame[0].values).toEqual([
       '2021-02 1:46:40 am',
       '2023-07 2:00:00 pm',
@@ -61,9 +63,10 @@ describe('Format Time Transformer', () => {
     const options = {
       timeField: 'time',
       outputFormat: 'YYYY-MM h:mm:ss a',
+      useTimezone: false,
     };
 
-    const formatter = createTimeFormatter(options.timeField, options.outputFormat);
+    const formatter = createTimeFormatter(options.timeField, options.outputFormat, options.useTimezone);
     const frame = toDataFrame({
       fields: [
         {
@@ -74,175 +77,13 @@ describe('Format Time Transformer', () => {
       ],
     });
 
-    const newFrame = formatter(frame.fields, [frame], frame);
+    const newFrame = formatter(frame.fields);
     expect(newFrame[0].values).toEqual([
       '2021-02 1:46:40 am',
       '2023-07 2:00:00 pm',
       '2023-04 3:20:00 pm',
       '2023-07 5:34:49 pm',
       'Invalid date',
-    ]);
-  });
-});
-
-describe('field convert types transformer', () => {
-  it('can convert multiple fields', () => {
-    const options = {
-      conversions: [
-        { targetField: 'stringy nums', destinationType: FieldType.number },
-        { targetField: 'proper dates', destinationType: FieldType.time },
-      ],
-    };
-
-    const stringyNumbers = toDataFrame({
-      fields: [
-        { name: 'A', type: FieldType.number, values: [1, 2, 3, 4, 5] },
-        {
-          name: 'proper dates',
-          type: FieldType.string,
-          values: [
-            '2021-07-19 00:00:00.000',
-            '2021-07-23 00:00:00.000',
-            '2021-07-25 00:00:00.000',
-            '2021-08-01 00:00:00.000',
-            '2021-08-02 00:00:00.000',
-          ],
-        },
-        { name: 'stringy nums', type: FieldType.string, values: ['10', '12', '30', '14', '10'] },
-      ],
-    });
-
-    const numbers = convertFieldTypes(options, [stringyNumbers]);
-    expect(
-      numbers[0].fields.map((f) => ({
-        type: f.type,
-        values: f.values,
-      }))
-    ).toEqual([
-      { type: FieldType.number, values: [1, 2, 3, 4, 5] },
-      {
-        type: FieldType.time,
-        values: [1626674400000, 1627020000000, 1627192800000, 1627797600000, 1627884000000],
-      },
-      {
-        type: FieldType.number,
-        values: [10, 12, 30, 14, 10],
-      },
-    ]);
-  });
-
-  it('will convert field to complex objects', () => {
-    const options = {
-      conversions: [
-        { targetField: 'numbers', destinationType: FieldType.other },
-        { targetField: 'objects', destinationType: FieldType.other },
-        { targetField: 'arrays', destinationType: FieldType.other },
-        { targetField: 'invalids', destinationType: FieldType.other },
-        { targetField: 'mixed', destinationType: FieldType.other },
-      ],
-    };
-
-    const comboTypes = toDataFrame({
-      fields: [
-        {
-          name: 'numbers',
-          type: FieldType.number,
-          values: [-1, 1, null],
-        },
-        {
-          name: 'objects',
-          type: FieldType.string,
-          values: [
-            '{ "neg": -100, "zero": 0, "pos": 1, "null": null, "array": [0, 1, 2], "nested": { "number": 1 } }',
-            '{ "string": "abcd" }',
-            '{}',
-          ],
-        },
-        {
-          name: 'arrays',
-          type: FieldType.string,
-          values: ['[true]', '[99]', '["2021-08-02 00:00:00.000"]'],
-        },
-        {
-          name: 'invalids',
-          type: FieldType.string,
-          values: ['abcd', '{ invalidJson }', '[unclosed array'],
-        },
-        {
-          name: 'mixed',
-          type: FieldType.string,
-          values: [
-            '{ "neg": -100, "zero": 0, "pos": 1, "null": null, "array": [0, 1, 2], "nested": { "number": 1 } }',
-            '["a string", 1234, {"a complex": "object"}]',
-            '["this is invalid JSON]',
-          ],
-        },
-      ],
-    });
-
-    const complex = convertFieldTypes(options, [comboTypes]);
-    expect(
-      complex[0].fields.map((f) => ({
-        type: f.type,
-        values: f.values,
-      }))
-    ).toEqual([
-      {
-        type: FieldType.other,
-        values: [-1, 1, null],
-      },
-      {
-        type: FieldType.other,
-        values: [
-          { neg: -100, zero: 0, pos: 1, null: null, array: [0, 1, 2], nested: { number: 1 } },
-          { string: 'abcd' },
-          {},
-        ],
-      },
-      { type: FieldType.other, values: [[true], [99], ['2021-08-02 00:00:00.000']] },
-      { type: FieldType.other, values: [null, null, null] },
-      {
-        type: FieldType.other,
-        values: [
-          { neg: -100, zero: 0, pos: 1, null: null, array: [0, 1, 2], nested: { number: 1 } },
-          ['a string', 1234, { 'a complex': 'object' }],
-          null,
-        ],
-      },
-    ]);
-  });
-
-  it('will convert field to strings', () => {
-    const options = {
-      conversions: [{ targetField: 'numbers', destinationType: FieldType.string }],
-    };
-
-    const comboTypes = toDataFrame({
-      fields: [
-        { name: 'numbers', type: FieldType.number, values: [-100, 0, 1, null, NaN] },
-        {
-          name: 'strings',
-          type: FieldType.string,
-          values: ['true', 'false', '0', '99', '2021-08-02 00:00:00.000'],
-        },
-      ],
-    });
-
-    const stringified = convertFieldTypes(options, [comboTypes]);
-    expect(
-      stringified[0].fields.map((f) => ({
-        type: f.type,
-        values: f.values,
-      }))
-    ).toEqual([
-      {
-        type: FieldType.string,
-        values: ['-100', '0', '1', 'null', 'NaN'],
-      },
-      {
-        type: FieldType.string,
-        values: ['true', 'false', '0', '99', '2021-08-02 00:00:00.000'],
-      },
     ]);
   });
 });
