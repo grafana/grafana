272dae78f0c14bbe9663c35e462ac888aadd2d3b
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2023-07-27 13:04:54 -0300

    PublicDashboards: Remove query expressions from dashboard model

--- pkg/services/publicdashboards/api/query.go
+++ pkg/services/publicdashboards/api/query.go
@@ -4,10 +4,8 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/grafana/grafana/pkg/api/dtos"
 	"github.com/grafana/grafana/pkg/api/response"
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
-	"github.com/grafana/grafana/pkg/services/dashboards"
 	. "github.com/grafana/grafana/pkg/services/publicdashboards/models"
 	"github.com/grafana/grafana/pkg/services/publicdashboards/validation"
 	"github.com/grafana/grafana/pkg/web"
@@ -21,33 +19,10 @@ func (api *Api) ViewPublicDashboard(c *contextmodel.ReqContext) response.Respons
 		return response.Err(ErrInvalidAccessToken.Errorf("ViewPublicDashboard: invalid access token"))
 	}
 
-	pubdash, dash, err := api.PublicDashboardService.FindEnabledPublicDashboardAndDashboardByAccessToken(
-		c.Req.Context(),
-		accessToken,
-	)
+	dto, err := api.PublicDashboardService.GetPublicDashboardForView(c.Req.Context(), accessToken)
 	if err != nil {
 		return response.Err(err)
 	}
-
-	meta := dtos.DashboardMeta{
-		Slug:                       dash.Slug,
-		Type:                       dashboards.DashTypeDB,
-		CanStar:                    false,
-		CanSave:                    false,
-		CanEdit:                    false,
-		CanAdmin:                   false,
-		CanDelete:                  false,
-		Created:                    dash.Created,
-		Updated:                    dash.Updated,
-		Version:                    dash.Version,
-		IsFolder:                   false,
-		FolderId:                   dash.FolderID,
-		PublicDashboardAccessToken: pubdash.AccessToken,
-		PublicDashboardEnabled:     pubdash.IsEnabled,
-	}
-	dash.Data.Get("timepicker").Set("hidden", !pubdash.TimeSelectionEnabled)
-
-	dto := dtos.DashboardFullWithMeta{Meta: meta, Dashboard: dash.Data}
 
 	return response.JSON(http.StatusOK, dto)
 }
