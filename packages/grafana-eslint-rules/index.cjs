3556f35fdc7286fb75fcaa3819f6192cd21d8659
Ashley Harrison <ashley.harrison@grafana.com>
2023-07-27 14:53:59 +0100

    create rule to find instances of theme variables

--- packages/grafana-eslint-rules/index.cjs
+++ packages/grafana-eslint-rules/index.cjs
@@ -1,7 +1,9 @@
 const noAriaLabelSelectors = require('./rules/no-aria-label-e2e-selectors.cjs');
+const themeImports = require('./rules/theme-imports.cjs');
 
 module.exports = {
   rules: {
     'no-aria-label-selectors': noAriaLabelSelectors,
+    'theme-imports': themeImports,
   },
 };
---
+++ packages/grafana-eslint-rules/rules/theme-imports.cjs
@@ -0,0 +1,51 @@
+// @ts-check
+const { ESLintUtils, AST_NODE_TYPES } = require('@typescript-eslint/utils');
+
+const createRule = ESLintUtils.RuleCreator((name) => `https://github.com/grafana/grafana#${name}`);
+
+const borderRadiusRule = createRule({
+  create(context) {
+    return {
+      Identifier: function (node) {
+        if (node.name === 'theme') {
+          const ancestors = context.getAncestors().reverse();
+          const paths = [];
+          let lastAncestor = null;
+          for (const ancestor of ancestors) {
+            if (ancestor.type === AST_NODE_TYPES.MemberExpression && ancestor.property.type === AST_NODE_TYPES.Identifier) {
+              paths.push(ancestor.property.name)
+              lastAncestor = ancestor;
+            } else {
+              break;
+            }
+          }
+          if (paths.length > 0 && lastAncestor) {
+            paths.unshift('theme');
+            context.report({
+              node: lastAncestor,
+              messageId: "themeImport",
+              data: {
+                identifier: paths.join('.')
+              }
+            })
+          }
+        }
+      }
+    };
+  },
+  name: 'theme-imports',
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Check for theme token imports',
+      recommended: false,
+    },
+    messages: {
+      themeImport: 'Theme token import: {{ identifier }}',
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+});
+
+module.exports = borderRadiusRule;
