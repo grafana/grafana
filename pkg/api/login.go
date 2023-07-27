36ca9c9ec8349153ae532a2cf92c1ffad2effe7b
maksym.revutskyi@gmail.com <mrevutskyi@linkedin.com>
2023-07-27 13:41:48 -0700

    Auth: fix auth.proxy when oauth is used with auto_login
    Before attemping an auto-login for oauth, verifies if current context has already been
    authenticated via auth-proxy
    
    Fixes: #72476

--- pkg/api/login.go
+++ pkg/api/login.go
@@ -115,7 +115,8 @@ func (hs *HTTPServer) LoginView(c *contextmodel.ReqContext) {
 		return
 	}
 
-	if hs.tryAutoLogin(c) {
+	// If user is not already signed in via auth-proxy, try auto-login
+	if !c.IsSignedIn && hs.tryAutoLogin(c) {
 		return
 	}
 
