8995de8e35f949e1e09dcf00294151eceea488ee
Gilles De Mey <gilles.de.mey@gmail.com>
2023-07-31 17:31:35 +0200

    use small icons for small text

--- public/app/features/alerting/unified/components/MetaText.tsx
+++ public/app/features/alerting/unified/components/MetaText.tsx
@@ -24,7 +24,7 @@ const MetaText = ({ children, icon, color = 'secondary', ...rest }: Props) => {
     >
       <Text variant="bodySmall" color={color}>
         <Stack direction="row" alignItems="center" gap={0.5}>
-          {icon && <Icon name={icon} />}
+          {icon && <Icon size="sm" name={icon} />}
           {children}
         </Stack>
       </Text>
