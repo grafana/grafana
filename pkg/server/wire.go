82cadb9dfdacc9f5c09afd3ad15aca11f72878d7
Todd Treece <todd.treece@grafana.com>
2023-07-31 10:27:26 -0400

    Revert "Modules: Add registry (#70859)"
    This reverts commit 8f975cfdb8d0841791725236351edf562f782fda.

--- pkg/server/wire.go
+++ pkg/server/wire.go
@@ -31,7 +31,6 @@ import (
 	"github.com/grafana/grafana/pkg/middleware/csrf"
 	"github.com/grafana/grafana/pkg/middleware/loggermw"
 	"github.com/grafana/grafana/pkg/modules"
-	moduleRegistry "github.com/grafana/grafana/pkg/modules/registry"
 	pluginDashboards "github.com/grafana/grafana/pkg/plugins/manager/dashboards"
 	"github.com/grafana/grafana/pkg/registry/backgroundsvcs"
 	"github.com/grafana/grafana/pkg/registry/corekind"
@@ -363,7 +362,6 @@ var wireBasicSet = wire.NewSet(
 	wire.Bind(new(oauthserver.OAuth2Server), new(*oasimpl.OAuth2ServiceImpl)),
 	loggermw.Provide,
 	modules.WireSet,
-	moduleRegistry.WireSet,
 	signingkeysimpl.ProvideEmbeddedSigningKeysService,
 	wire.Bind(new(signingkeys.Service), new(*signingkeysimpl.Service)),
 )
