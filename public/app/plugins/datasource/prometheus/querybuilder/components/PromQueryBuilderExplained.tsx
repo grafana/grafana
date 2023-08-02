4fa00f57067fabc91bba05a2037cbbaf18bea498
Ben Sully <ben.sully@grafana.com>
2023-08-02 11:49:00 +0100

    Add example of using LLMs via @grafana/experimental and LLM app plugin
    This PR is meant to serve as an example of adding a small LLM-powered feature
    to Grafana's frontend using the APIs available in @grafana/experimental and
    the OpenAI proxy provided by the `grafana-llm-app` app plugin.
    
    It's not meant to be merged, but I'm hoping it will help people who would
    like to use LLMs in our upcoming hackathon.
    
    To test it out, you'll need to start Grafana's backend with the following
    environment variables set:
    
    - `OPENAI_API_KEY` should be set to a valid OpenAI API key
    - `GF_INSTALL_PLUGINS=https://storage.googleapis.com/grafana-llm-app/grafana-llm-app-0.1.0.zip; grafana-llm-app`
    
    Then `make run` will install the LLM plugin when starting Grafana's backend.
    
    You should now be able to see a new section when clicking 'Explain' on a
    Prometheus code editor query, which contains a streamed explanation of
    the query from an LLM. (It's pretty terrible UX, but it's only an example!)
    
    ---
    
    The PR itself does three main things:
    
    1. bump the version of @grafana/experimental to a pre-release, git version
       that includes the LLM APIs (this will be released more officially soon,
       so I'll keep this PR updated)
    2. adds an example provisioning file for the `grafana-llm-app` plugin,
       which configures the OpenAI API key
    3. adds some logic to the `PromQueryBuilderExplained` component to call the LLM
       and display the output if the LLM plugin is enabled and configured.

--- public/app/plugins/datasource/prometheus/querybuilder/components/PromQueryBuilderExplained.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/components/PromQueryBuilderExplained.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAsync } from 'react-use';
 
-import { Stack } from '@grafana/experimental';
+import { Stack, llms } from '@grafana/experimental';
+import { Spinner } from '@grafana/ui';
 
 import promqlGrammar from '../../promql';
 import { promQueryModeller } from '../PromQueryModeller';
@@ -20,8 +22,44 @@ export const PromQueryBuilderExplained = React.memo<Props>(({ query }) => {
   const visQuery = buildVisualQueryFromString(query || '').query;
   const lang = { grammar: promqlGrammar, name: 'promql' };
 
+  const [llmReply, setLLMReply] = useState('');
+
+  const { value } = useAsync(async () => {
+    // Check if the LLM plugin is enabled and configured.
+    // If not, we won't be able to make requests, so return early.
+    const enabled = await llms.openai.enabled();
+    if (!enabled) {
+      return { enabled };
+    }
+
+    llms.openai
+      .streamChatCompletions({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'You are an expert in the Prometheus query language PromQL. Explain the purpose of the following Prometheus query.',
+          },
+          { role: 'user', content: query },
+        ],
+      })
+      .pipe(
+        // Accumulate the stream content into a stream of strings, where each
+        // element contains the accumulated message so far.
+        llms.openai.accumulateContent()
+      )
+      .subscribe(setLLMReply);
+    return { enabled };
+  }, [query]);
+
   return (
     <Stack gap={0.5} direction="column">
+      {value?.enabled && (
+        <OperationExplainedBox title="Query summary">
+          {llmReply === '' ? <Spinner /> : <pre>{llmReply}</pre>}
+        </OperationExplainedBox>
+      )}
       <OperationExplainedBox
         stepNumber={1}
         title={<RawQuery query={`${visQuery.metric} ${promQueryModeller.renderLabels(visQuery.labels)}`} lang={lang} />}
