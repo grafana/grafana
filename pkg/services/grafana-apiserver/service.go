f122117061eb2652e52fb858e75d8a7bb9cabfd9
sam boyer <sdboyer@grafana.com>
2023-08-03 11:20:39 -0400

    Re-enable validating admission controller

--- pkg/services/grafana-apiserver/service.go
+++ pkg/services/grafana-apiserver/service.go
@@ -35,6 +35,7 @@ import (
 	"github.com/grafana/grafana/pkg/infra/appcontext"
 	"github.com/grafana/grafana/pkg/middleware"
 	"github.com/grafana/grafana/pkg/modules"
+	"github.com/grafana/grafana/pkg/registry/corekind"
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
 	grafanaAdmission "github.com/grafana/grafana/pkg/services/k8s/apiserver/admission"
 	"github.com/grafana/grafana/pkg/services/k8s/apiserver/authorization"
@@ -64,6 +65,7 @@ type service struct {
 
 	restConfig *rest.Config
 	rr         routing.RouteRegister
+	corereg    *corekind.Base
 
 	restOptionsGetter func(runtime.Codec) genericregistry.RESTOptionsGetter
 
@@ -73,11 +75,12 @@ type service struct {
 	stoppedCh chan error
 }
 
-func ProvideService(cfg *setting.Cfg, rr routing.RouteRegister, restOptionsGetter func(runtime.Codec) genericregistry.RESTOptionsGetter) (*service, error) {
+func ProvideService(cfg *setting.Cfg, rr routing.RouteRegister, restOptionsGetter func(runtime.Codec) genericregistry.RESTOptionsGetter, corereg *corekind.Base) (*service, error) {
 	s := &service{
 		rr:       rr,
 		dataPath: path.Join(cfg.DataPath, "k8s"),
 		stopCh:   make(chan struct{}),
+		corereg:  corereg,
 
 		restOptionsGetter: restOptionsGetter,
 	}
@@ -129,10 +132,11 @@ func (s *service) start(ctx context.Context) error {
 	// plugins that depend on the Core V1 APIs and informers.
 	o.RecommendedOptions.Admission.Plugins = admission.NewPlugins()
 	grafanaAdmission.RegisterDenyByName(o.RecommendedOptions.Admission.Plugins)
+	grafanaAdmission.RegisterSchemaValidate(o.RecommendedOptions.Admission.Plugins, s.corereg)
 	grafanaAdmission.RegisterAddDefaultFields(o.RecommendedOptions.Admission.Plugins)
-	o.RecommendedOptions.Admission.RecommendedPluginOrder = []string{grafanaAdmission.PluginNameDenyByName, grafanaAdmission.PluginNameAddDefaultFields}
+	o.RecommendedOptions.Admission.RecommendedPluginOrder = []string{grafanaAdmission.PluginNameDenyByName, grafanaAdmission.PluginNameSchemaValidate, grafanaAdmission.PluginNameAddDefaultFields}
 	o.RecommendedOptions.Admission.DisablePlugins = append([]string{}, o.RecommendedOptions.Admission.EnablePlugins...)
-	o.RecommendedOptions.Admission.EnablePlugins = []string{grafanaAdmission.PluginNameDenyByName, grafanaAdmission.PluginNameAddDefaultFields}
+	o.RecommendedOptions.Admission.EnablePlugins = []string{grafanaAdmission.PluginNameDenyByName, grafanaAdmission.PluginNameSchemaValidate, grafanaAdmission.PluginNameAddDefaultFields}
 
 	// Get the util to get the paths to pre-generated certs
 	certUtil := certgenerator.CertUtil{
--- pkg/services/k8s/apiserver/admission/schema_validate.go
+++ pkg/services/k8s/apiserver/admission/schema_validate.go
@@ -5,12 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"sort"
 
+	"github.com/grafana/kindsys"
 	"github.com/grafana/kindsys/encoding"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apiserver/pkg/admission"
 
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/plugins/pfs/corelist"
 	"github.com/grafana/grafana/pkg/registry/corekind"
 )
 
@@ -25,7 +28,7 @@ func RegisterSchemaValidate(plugins *admission.Plugins, reg *corekind.Base) {
 
 type schemaValidate struct {
 	log log.Logger
-	reg *corekind.Base
+	reg *distreg
 }
 
 var _ admission.ValidationInterface = schemaValidate{}
@@ -47,6 +50,9 @@ func (sv schemaValidate) Validate(ctx context.Context, a admission.Attributes, o
 
 	switch a.GetOperation() { //nolint:exhaustive
 	case admission.Create, admission.Update:
+		// Add in all composable kinds
+		// FIXME re-composing on every admission control call is absurd. This is a plugin management responsibility. Fix after https://github.com/grafana/grafana/pull/71184 lands
+
 		// This logic will accept any known version of the kind, not just the
 		// current/latest one that is known to the server. Errors may occur when
 		// translating to the current/latest version, but that's not this admission
@@ -75,6 +81,61 @@ func (schemaValidate) Handles(operation admission.Operation) bool {
 func NewSchemaValidate(reg *corekind.Base) admission.Interface {
 	return schemaValidate{
 		log: log.New("admission.schema-validate"),
-		reg: reg,
+		reg: buildDistRegistry(reg),
 	}
+}
+
+type distreg struct {
+	all []kindsys.Core
+}
+
+// All returns a slice of [kindsys.Core] containing all core Grafana kinds.
+//
+// The returned slice is sorted lexicographically by kind machine name.
+func (b *distreg) All() []kindsys.Core {
+	ret := make([]kindsys.Core, len(b.all))
+	copy(ret, b.all)
+	return ret
+}
+
+// ByName looks up a kind in the registry by name. If no kind exists for the
+// given name, nil is returned.
+func (b *distreg) ByName(name string) kindsys.Core {
+	i := sort.Search(len(b.all), func(i int) bool {
+		return b.all[i].Name() >= name
+	})
+
+	if b.all[i].Name() == name {
+		return b.all[i]
+	}
+	return nil
+}
+
+func buildDistRegistry(reg *corekind.Base) *distreg {
+	dr := &distreg{
+		all: make([]kindsys.Core, 0, len(reg.All())),
+	}
+
+	coreplugins := corelist.New(nil)
+	for _, corek := range reg.All() {
+		for _, slot := range corek.Props().(kindsys.CoreProperties).Slots {
+			var toCompose []kindsys.Composable
+			for _, pp := range coreplugins {
+				for _, compok := range pp.ComposableKinds {
+					if compok.Implements().Name() == slot.SchemaInterface {
+						toCompose = append(toCompose, compok)
+					}
+				}
+			}
+			var err error
+			corek, err = corek.Compose(slot, toCompose...)
+			if err != nil {
+				// should be unreachable, panic for now, err once this is separated out
+				panic(err)
+			}
+		}
+		dr.all = append(dr.all, corek)
+	}
+
+	return dr
 }
