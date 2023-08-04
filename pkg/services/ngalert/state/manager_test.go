7c6d4d446186dd54b7a7c77c51f2c68a87d61d70
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-08-04 09:54:02 -0400

    use clearer timestamp

--- pkg/services/ngalert/state/manager_test.go
+++ pkg/services/ngalert/state/manager_test.go
@@ -280,12 +280,10 @@ func TestDashboardAnnotations(t *testing.T) {
 }
 
 func TestProcessEvalResults(t *testing.T) {
-	t1, err := time.Parse("2006-01-02", "2021-03-25")
-	if err != nil {
-		t.Fatalf("error parsing date format: %s", err.Error())
-	}
 	evaluationDuration := 10 * time.Millisecond
 	evaluationInterval := 10 * time.Second
+
+	t1 := time.Time{}.Add(evaluationInterval)
 
 	tn := func(n int) time.Time {
 		return t1.Add(time.Duration(n-1) * evaluationInterval)
