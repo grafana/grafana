cd3c0493e063d0fcda0b0aa30ca7a74d2bcb15df
Giordano Ricci <me@giordanoricci.com>
2023-08-07 15:42:24 +0100

    Alerting: use DS ref when generating explore links (#72986)
    Co-authored-by: Gilles De Mey <gilles.de.mey@gmail.com>

--- public/app/features/alerting/unified/utils/misc.test.ts
+++ public/app/features/alerting/unified/utils/misc.test.ts
@@ -89,9 +89,9 @@ describe('Unified Altering misc', () => {
 
 describe('createExploreLink', () => {
   it('should create a correct explore link', () => {
-    const url = createExploreLink('dsName', 'cpu_utilization > 0.5');
+    const url = createExploreLink({ uid: 'uid', type: 'type' }, 'cpu_utilization > 0.5');
     expect(url).toBe(
-      '/explore?left=%7B%22datasource%22%3A%22dsName%22%2C%22queries%22%3A%5B%7B%22refId%22%3A%22A%22%2C%22expr%22%3A%22cpu_utilization+%3E+0.5%22%7D%5D%2C%22range%22%3A%7B%22from%22%3A%22now-1h%22%2C%22to%22%3A%22now%22%7D%7D'
+      '/explore?left=%7B%22datasource%22%3A%22uid%22%2C%22queries%22%3A%5B%7B%22refId%22%3A%22A%22%2C%22datasource%22%3A%7B%22uid%22%3A%22uid%22%2C%22type%22%3A%22type%22%7D%2C%22expr%22%3A%22cpu_utilization+%3E+0.5%22%7D%5D%2C%22range%22%3A%7B%22from%22%3A%22now-1h%22%2C%22to%22%3A%22now%22%7D%7D'
     );
   });
 });
