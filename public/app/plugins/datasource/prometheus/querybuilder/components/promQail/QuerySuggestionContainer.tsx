407ef85737b12f6a88ec68132b1f3e59a5ba79c2
bohandley <brendan.ohandley@gmail.com>
2023-08-09 17:42:11 -0400

    add buttons to add or refine prompt

--- public/app/plugins/datasource/prometheus/querybuilder/components/promQail/QuerySuggestionContainer.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/components/promQail/QuerySuggestionContainer.tsx
@@ -10,23 +10,26 @@ import { QuerySuggestion, SuggestionType } from './types';
 export type Props = {
   querySuggestions: QuerySuggestion[];
   suggestionType: SuggestionType;
+  closeDrawer: () => void;
 };
 
 export function QuerySuggestionContainer(props: Props) {
-  const { suggestionType, querySuggestions } = props;
+  const { suggestionType, querySuggestions, closeDrawer } = props;
 
   const theme = useTheme2();
   const styles = getStyles(theme);
 
-  let text, secondaryText;
+  let text, secondaryText, refineText;
 
   if (suggestionType === SuggestionType.Historical) {
     text = 'Here is the metric you have selected:';
     secondaryText = 'These queries based off of historical data (top used queries) for your metric.';
+    refineText = 'I want to write a prompt';
   } else if (suggestionType === SuggestionType.AI) {
     text = 'Here are 5 query suggestions:';
     secondaryText =
       'These queries were based off of natural language descriptions of the most commonly used PromQL queries.';
+    refineText = 'Refine prompt';
   }
 
   return (
@@ -37,6 +40,18 @@ export function QuerySuggestionContainer(props: Props) {
         {querySuggestions.map((qs: QuerySuggestion, idx: number) => {
           return <QuerySuggestionItem querySuggestion={qs} key={idx} order={idx + 1} />;
         })}
+      </div>
+      <div>
+        <div className={cx(styles.afterButtons, styles.textPadding)}>
+          <Button fill="outline" variant="secondary" size="sm">
+            {refineText}
+          </Button>
+        </div>
+        <div className={cx(styles.textPadding, styles.floatRight)}>
+          <Button fill="outline" variant="secondary" size="sm" onClick={closeDrawer}>
+            Cancel
+          </Button>
+        </div>
       </div>
     </>
   );
