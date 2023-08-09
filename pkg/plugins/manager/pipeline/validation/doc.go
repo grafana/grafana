95a14947bfd3a538bb9934d665df98f9fcaf79f1
Will Browne <will.browne@grafana.com>
2023-08-09 14:23:43 +0100

    adjust doc

--- pkg/plugins/manager/pipeline/validation/doc.go
+++ pkg/plugins/manager/pipeline/validation/doc.go
@@ -1,6 +1,5 @@
 // Package validation defines the Validation stage of the plugin loader pipeline.
 //
 // The Validation stage must implement the Validator interface.
-// - Validate(ctx context.Context, ps []*plugins.Plugin) ([]*plugins.Plugin, error)
 
 package validation
