adb21de692361097db76dae3f30a9eec08fa5e1c
Jean-Philippe Quéméner <JohnnyQQQQ@users.noreply.github.com>
2023-08-09 19:00:51 +0200

    Alerting: optimize rules with multiple loki range queries (#73103)

--- pkg/services/ngalert/store/loki_range_to_instant.go
+++ pkg/services/ngalert/store/loki_range_to_instant.go
@@ -19,57 +19,74 @@ func (t dsType) isLoki() bool {
 	return t.DS.Type == datasources.DS_LOKI
 }
 
-func canBeInstant(r *models.AlertRule) bool {
+// canBeInstant checks if any of the query nodes that are loki range queries can be migrated to instant queries.
+// If any are migratable, those indices are returned.
+func canBeInstant(r *models.AlertRule) ([]int, bool) {
 	if len(r.Data) < 2 {
-		return false
+		return nil, false
 	}
-	// First query part should be range query.
-	if r.Data[0].QueryType != "range" {
-		return false
-	}
+	var (
+		optimizableIndices []int
+		canBeOptimized     = false
+	)
+	// Loop over query nodes to find all Loki range queries.
+	for i := range r.Data {
+		if r.Data[i].QueryType != "range" {
+			continue
+		}
+		var t dsType
+		// We can ignore the error here, the query just won't be optimized.
+		_ = json.Unmarshal(r.Data[i].Model, &t)
 
-	var t dsType
-	// We can ignore the error here, the query just won't be optimized.
-	_ = json.Unmarshal(r.Data[0].Model, &t)
-
-	if !t.isLoki() {
-		return false
+		if !t.isLoki() {
+			continue
+		}
+		var validReducers bool
+		// Loop over all query nodes to find the reduce node.
+		for ii := range r.Data {
+			// Second query part should be and expression.
+			if !expr.IsDataSource(r.Data[ii].DatasourceUID) {
+				continue
+			}
+			exprRaw := make(map[string]interface{})
+			if err := json.Unmarshal(r.Data[ii].Model, &exprRaw); err != nil {
+				continue
+			}
+			// Second query part should use first query part as expression.
+			if ref, ok := exprRaw["expression"].(string); !ok || ref != r.Data[i].RefID {
+				continue
+			}
+			// Second query part should be "last()"
+			if val, ok := exprRaw["reducer"].(string); !ok || val != "last" {
+				validReducers = false
+				break
+			}
+			validReducers = true
+		}
+		// If we found a reduce node that uses last, we can add the loki query to the optimizations.
+		if validReducers {
+			canBeOptimized = true
+			optimizableIndices = append(optimizableIndices, i)
+		}
 	}
-
-	exprRaw := make(map[string]interface{})
-	if err := json.Unmarshal(r.Data[1].Model, &exprRaw); err != nil {
-		return false
-	}
-
-	// Second query part should be and expression.
-	if !expr.IsDataSource(r.Data[1].DatasourceUID) {
-		return false
-	}
-
-	// Second query part should be "last()"
-	if val, ok := exprRaw["reducer"].(string); !ok || val != "last" {
-		return false
-	}
-	// Second query part should use first query part as expression.
-	if ref, ok := exprRaw["expression"].(string); !ok || ref != r.Data[0].RefID {
-		return false
-	}
-	return true
+	return optimizableIndices, canBeOptimized
 }
 
-// migrateToInstant will move a range-query to an instant query. This should only
+// migrateToInstant will move the provided indices from a range-query to an instant query. This should only
 // be used for loki.
-func migrateToInstant(r *models.AlertRule) error {
-	modelRaw := make(map[string]interface{})
-	if err := json.Unmarshal(r.Data[0].Model, &modelRaw); err != nil {
-		return err
+func migrateToInstant(r *models.AlertRule, optimizableIndices []int) error {
+	for _, lokiQueryIndex := range optimizableIndices {
+		modelRaw := make(map[string]interface{})
+		if err := json.Unmarshal(r.Data[lokiQueryIndex].Model, &modelRaw); err != nil {
+			return err
+		}
+		modelRaw["queryType"] = "instant"
+		model, err := json.Marshal(modelRaw)
+		if err != nil {
+			return err
+		}
+		r.Data[lokiQueryIndex].Model = model
+		r.Data[lokiQueryIndex].QueryType = "instant"
 	}
-	modelRaw["queryType"] = "instant"
-	model, err := json.Marshal(modelRaw)
-	if err != nil {
-		return err
-	}
-	r.Data[0].Model = model
-	r.Data[0].QueryType = "instant"
 	return nil
 }
