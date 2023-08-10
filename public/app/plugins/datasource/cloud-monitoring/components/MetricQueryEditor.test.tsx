76fb6364b9a88074a5ee0d31623fa92f00d413cf
alyssabull <alyssabull@gmail.com>
2023-08-09 22:35:06 -0600

    Fix typo

--- public/app/plugins/datasource/cloud-monitoring/components/MetricQueryEditor.test.tsx
+++ public/app/plugins/datasource/cloud-monitoring/components/MetricQueryEditor.test.tsx
@@ -1,7 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-// import { act } from 'react-dom/test-utils';
-import TestRenderer from 'react-test-renderer';
 
 import { createMockDatasource } from '../__mocks__/cloudMonitoringDatasource';
 import { createMockQuery } from '../__mocks__/cloudMonitoringQuery';
