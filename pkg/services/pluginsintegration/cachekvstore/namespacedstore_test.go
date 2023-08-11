2161feb82cae1ad56f9a6ad8e83affa16f0ea32b
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2023-08-11 12:51:15 +0200

    Sort imports

--- pkg/services/pluginsintegration/cachekvstore/namespacedstore_test.go
+++ pkg/services/pluginsintegration/cachekvstore/namespacedstore_test.go
@@ -8,8 +8,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/grafana/grafana/pkg/infra/kvstore"
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/grafana/pkg/infra/kvstore"
 )
 
 func TestDefaultStoreKeyGetter(t *testing.T) {
--- pkg/services/pluginsintegration/cachekvstore/singlekeystore_test.go
+++ pkg/services/pluginsintegration/cachekvstore/singlekeystore_test.go
@@ -5,8 +5,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/grafana/grafana/pkg/infra/kvstore"
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/grafana/pkg/infra/kvstore"
 )
 
 func TestSingleKeyNamespacedStore(t *testing.T) {
