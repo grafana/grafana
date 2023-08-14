1f7c381ede87f8a450c3e8831b07274754f5ba00
Sarah Zinger <sarah.zinger@grafana.com>
2023-08-14 12:18:58 -0400

    Cloudwatch: reorg files in components (#73176)

--- public/app/plugins/datasource/cloudwatch/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Dimensions } from './Dimensions/Dimensions';
-export { QueryInlineField, QueryField } from './Forms';
-export { QueryEditor as PanelQueryEditor } from './QueryEditor';
-export { CloudWatchLogsQueryEditor } from './LogsQueryEditor';
-export { MetricStatEditor } from './MetricStatEditor';
-export { SQLBuilderEditor } from './SQLBuilderEditor';
-export { MathExpressionQueryField } from './MathExpressionQueryField';
