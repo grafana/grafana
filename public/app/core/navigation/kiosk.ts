b970a29995b3cab816c356048cf4f796e53a881a
Clarity-89 <homes89@ukr.net>
2023-08-15 09:11:42 +0300

    Dashboards: Add kiosk embed mode

--- public/app/core/navigation/kiosk.ts
+++ public/app/core/navigation/kiosk.ts
@@ -11,6 +11,8 @@ export function getKioskMode(queryParams: UrlQueryMap): KioskMode | null {
     case '1':
     case true:
       return KioskMode.Full;
+    case 'embed':
+      return KioskMode.Embed;
     default:
       return null;
   }
--- public/app/types/dashboard.ts
+++ public/app/types/dashboard.ts
@@ -99,6 +99,7 @@ export interface DashboardInitError {
 export enum KioskMode {
   TV = 'tv',
   Full = 'full',
+  Embed = 'embed',
 }
 
 export type GetMutableDashboardModelFn = () => DashboardModel | null;
