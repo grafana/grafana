93a6bf56ac6500064a78ce67bd9a67c71b0e4816
Miklós Tolnai <miklos.tolnai@grafana.com>
2023-08-17 08:45:35 +0200

    move useDataSourceSettingsNav from datasources to connections
    without any modification in the code yet
    
    The reason for this is that its only usage is in this file, so we are
    going to merge it with the other useDataSourceSettingsNav hook in the
    next commit.

--- public/app/features/connections/hooks/useDataSourceSettingsNav.ts
+++ public/app/features/connections/hooks/useDataSourceSettingsNav.ts
@@ -1,10 +1,71 @@
 import { useLocation, useParams } from 'react-router-dom';
 
-import {
-  useDataSource,
-  useDataSourceSettingsNav as useDataSourceSettingsNavOriginal,
-} from 'app/features/datasources/state/hooks';
+import { NavModel, NavModelItem } from '@grafana/data';
+import { getDataSourceSrv } from '@grafana/runtime';
+import { getNavModel } from 'app/core/selectors/navModel';
+import { useDataSource, useDataSourceSettings } from 'app/features/datasources/state/hooks';
+import { getDataSourceLoadingNav, buildNavModel, getDataSourceNav } from 'app/features/datasources/state/navModel';
 import { useGetSingle } from 'app/features/plugins/admin/state/hooks';
+import { useSelector } from 'app/types';
+
+export const useDataSourceSettingsNavOriginal = (dataSourceId: string, pageId: string | null) => {
+  const { plugin, loadError, loading } = useDataSourceSettings();
+  const dataSource = useDataSource(dataSourceId);
+  const dsi = getDataSourceSrv()?.getInstanceSettings(dataSourceId);
+  const hasAlertingEnabled = Boolean(dsi?.meta?.alerting ?? false);
+  const isAlertManagerDatasource = dsi?.type === 'alertmanager';
+  const alertingSupported = hasAlertingEnabled || isAlertManagerDatasource;
+
+  const datasourcePlugin = useGetSingle(dataSource.type);
+  const navIndex = useSelector((state) => state.navIndex);
+  const navIndexId = pageId ? `datasource-${pageId}-${dataSourceId}` : `datasource-settings-${dataSourceId}`;
+  let pageNav: NavModel = {
+    node: {
+      text: 'Data Source Nav Node',
+    },
+    main: {
+      text: 'Data Source Nav Node',
+    },
+  };
+
+  if (loadError) {
+    const node: NavModelItem = {
+      text: loadError,
+      subTitle: 'Data Source Error',
+      icon: 'exclamation-triangle',
+    };
+
+    pageNav = {
+      node: node,
+      main: node,
+    };
+  }
+
+  if (loading || !plugin) {
+    pageNav = getNavModel(navIndex, navIndexId, getDataSourceLoadingNav('settings'));
+  }
+
+  if (plugin) {
+    pageNav = getNavModel(
+      navIndex,
+      navIndexId,
+      getDataSourceNav(buildNavModel(dataSource, plugin), pageId || 'settings')
+    );
+  }
+
+  return {
+    node: pageNav.node,
+    main: {
+      ...pageNav.main,
+      text: dataSource.name,
+      dataSourcePluginName: datasourcePlugin?.name || plugin?.meta.name || '',
+      active: true,
+    },
+    dataSourceHeader: {
+      alertingSupported,
+    },
+  };
+};
 
 // We are extending the original useDataSourceSettingsNav in the following ways:
 // - changing the URL of the nav items to point to Connections
