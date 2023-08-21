f24ab14e9d29e1cdabd8d7d9368fea10ceeb40ca
Dominik Prokop <dominik.prokop@grafana.com>
2023-08-21 17:43:48 +0200

    DashboardLoaders: Add support for loading library panels (#73406)
    * DashboardLoaders: Add support for loading library panels
    
    * Bump scenes
    
    * Scene object for loading library panels
    
    * Remove unused function
    
    * Remove console.log
    
    * Bump scenes
    
    * Review

--- public/app/features/scenes/dashboard/DashboardsLoader.ts
+++ public/app/features/scenes/dashboard/DashboardsLoader.ts
@@ -5,14 +5,12 @@ import {
   QueryVariableModel,
   VariableModel,
 } from '@grafana/data';
-import { config } from '@grafana/runtime';
 import {
   VizPanel,
   SceneTimePicker,
   SceneGridLayout,
   SceneGridRow,
   SceneTimeRange,
-  SceneQueryRunner,
   SceneVariableSet,
   VariableValueSelectors,
   SceneVariable,
@@ -21,9 +19,7 @@ import {
   QueryVariable,
   ConstantVariable,
   SceneRefreshPicker,
-  SceneDataTransformer,
   SceneGridItem,
-  SceneDataProvider,
   SceneObject,
   SceneControlsSpacer,
   VizPanelMenu,
@@ -32,12 +28,12 @@ import {
 import { StateManagerBase } from 'app/core/services/StateManagerBase';
 import { dashboardLoaderSrv } from 'app/features/dashboard/services/DashboardLoaderSrv';
 import { DashboardModel, PanelModel } from 'app/features/dashboard/state';
-import { SHARED_DASHBOARD_QUERY } from 'app/plugins/datasource/dashboard/types';
 
 import { DashboardScene } from './DashboardScene';
+import { LibraryVizPanel } from './LibraryVizPanel';
 import { panelMenuBehavior } from './PanelMenuBehavior';
-import { ShareQueryDataProvider } from './ShareQueryDataProvider';
 import { getVizPanelKeyForPanelId } from './utils';
+import { createPanelDataProvider } from './utils/createPanelDataProvider';
 
 export interface DashboardLoaderState {
   dashboard?: DashboardScene;
@@ -129,6 +125,18 @@ export function createSceneObjectsForPanels(oldPanels: PanelModel[]): Array<Scen
           currentRowPanels = [];
         }
       }
+    } else if (panel.libraryPanel?.uid && !('model' in panel.libraryPanel)) {
+      const gridItem = new SceneGridItem({
+        body: new LibraryVizPanel({
+          title: panel.title,
+          uid: panel.libraryPanel.uid,
+        }),
+        y: panel.gridPos.y,
+        x: panel.gridPos.x,
+        width: panel.gridPos.w,
+        height: panel.gridPos.h,
+      });
+      panels.push(gridItem);
     } else {
       const panelObject = createVizPanelFromPanelModel(panel);
 
@@ -191,13 +199,13 @@ export function createDashboardSceneFromDashboardModel(oldModel: DashboardModel)
     title: oldModel.title,
     uid: oldModel.uid,
     body: new SceneGridLayout({
+      isLazy: true,
       children: createSceneObjectsForPanels(oldModel.panels),
     }),
     $timeRange: new SceneTimeRange(oldModel.time),
     $variables: variables,
     $behaviors: [
       new behaviors.CursorSync({
-        key: 'dashboard',
         sync: oldModel.graphTooltip,
       }),
     ],
@@ -292,39 +300,6 @@ export function createVizPanelFromPanelModel(panel: PanelModel) {
       }),
     }),
   });
-}
-
-export function createPanelDataProvider(panel: PanelModel): SceneDataProvider | undefined {
-  // Skip setting query runner for panels without queries
-  if (!panel.targets?.length) {
-    return undefined;
-  }
-
-  // Skip setting query runner for panel plugins with skipDataQuery
-  if (config.panels[panel.type]?.skipDataQuery) {
-    return undefined;
-  }
-
-  let dataProvider: SceneDataProvider | undefined = undefined;
-
-  if (panel.datasource?.uid === SHARED_DASHBOARD_QUERY) {
-    dataProvider = new ShareQueryDataProvider({ query: panel.targets[0] });
-  } else {
-    dataProvider = new SceneQueryRunner({
-      queries: panel.targets,
-      maxDataPoints: panel.maxDataPoints ?? undefined,
-    });
-  }
-
-  // Wrap inner data provider in a data transformer
-  if (panel.transformations?.length) {
-    dataProvider = new SceneDataTransformer({
-      $data: dataProvider,
-      transformations: panel.transformations,
-    });
-  }
-
-  return dataProvider;
 }
 
 let loader: DashboardLoader | null = null;
