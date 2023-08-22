6fdf1fd46422fa8b9944250665198476282a981b
joshhunt <josh@trtr.co>
2023-08-22 13:12:49 +0100

    user essentials mob! :trident:

--- e2e/dashboards-suite/dashboard-timepicker.spec.ts
+++ e2e/dashboards-suite/dashboard-timepicker.spec.ts
@@ -7,22 +7,34 @@ e2e.scenario({
   addScenarioDashBoard: false,
   skipScenario: false,
   scenario: () => {
-    //Make sure that the browser time zone is set to UTC
+    //Make sure that the browser time zone is set to UTC - it's set to Pacific/Honolulu UTC-12
     //Open preferences page and set timezone to Tokyo
     e2e.pages.ProfilePage.visit();
-    
+
+    e2e.flows.selectOption({
+      container: e2e.components.TimeZonePicker.containerV2(),
+      optionText: 'Asia/Tokyo',
+    });
+
+    // This refreshes the page
+    e2e.components.UserProfile.preferencesSaveButton().click();
+
     //Open dashboard with time range from 2022-06-08 00:00:00 to 2022-06-10 23:59:59
+    // e2e().visit('/dashboard/new');
     e2e.flows.openDashboard({
       uid: '5SdHCasdf',
       timeRange: {
+        zone: 'Default',
         from: '2022-06-08 00:00:00',
         to: '2022-06-10 23:59:59',
       },
     });
+
     //Open timepicker
+    e2e.components.TimePicker.openButton().click();
     //Open calendar
-    //Assert that the calendar shows 08, 09 and 10 as selected days
+    e2e.components.TimePicker.calendar.openButton().first().click();
+    //Assert that the calendar shows 08 and 09 and 10 as selected days
+    e2e().get('.react-calendar__tile--active, .react-calendar__tile--hasActive').should('have.length', 3);
   },
 });
-
-
