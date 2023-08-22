adf449ab85ef6ce27bb7c6cd8ea5a402a2c0fd43
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-08-22 09:19:32 +0200

    Add MDX and story

--- packages/grafana-ui/src/components/Stack/Stack.internal.story.tsx
+++ packages/grafana-ui/src/components/Stack/Stack.internal.story.tsx
@@ -4,11 +4,17 @@ import React, { ReactNode } from 'react';
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 
 import { Stack } from './Stack';
+import mdx from './Stack.mdx';
 
 const meta: Meta<typeof Stack> = {
   title: 'General/Stack',
   component: Stack,
   decorators: [withCenteredStory],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
 const Item = ({ children }: { children: ReactNode }) => <div style={{ backgroundColor: 'lightgrey' }}>{children}</div>;
@@ -19,6 +25,23 @@ export const Basic: StoryFn<typeof Stack> = (props) => {
       <Item>Item 1</Item>
       <Item>Item 2</Item>
       <Item>Item 3</Item>
+    </Stack>
+  );
+};
+
+export const FlexGrowExample: StoryFn<typeof Stack> = () => {
+  return (
+    <Stack>
+      <Stack direction="column">
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+      <Stack flexGrow={3}>
+        <Item>This Stack</Item>
+        <Item>uses</Item>
+        <Item>flexGrow={3}</Item>
+      </Stack>
     </Stack>
   );
 };
