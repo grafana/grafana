f8f7bec5cac51228e3d475acca571b6bafc44c93
Esteban Beltran <esteban@academo.me>
2023-08-24 11:55:38 +0200

    Move panel wrapper to different file

--- packages/grafana-runtime/src/services/dashboard-srv/DashboardSrvSingleton.ts
+++ packages/grafana-runtime/src/services/dashboard-srv/DashboardSrvSingleton.ts
@@ -1,53 +1,5 @@
-import { GridPos } from '@grafana/schema';
-
-import { PluginsAPIDashboardModel, PluginsAPIDashboardSrv, PluginsAPIPanelModel } from './types';
-
-class PluginsAPIPanelModelWrapper implements PluginsAPIPanelModel {
-  #panel: PluginsAPIPanelModel;
-  constructor(panel: PluginsAPIPanelModel) {
-    this.#panel = panel;
-  }
-
-  get id() {
-    return this.#panel.id;
-  }
-  get title() {
-    return this.#panel.title || '';
-  }
-  get type() {
-    return this.#panel.type;
-  }
-  get gridPos() {
-    return this.#panel.gridPos;
-  }
-  get options() {
-    return this.#panel.options;
-  }
-
-  set id(id: number) {
-    this.#panel.id = id;
-  }
-
-  set title(title: string) {
-    this.#panel.title = title;
-  }
-
-  set type(_) {
-    throw new Error('Cannot set type on a panel');
-  }
-
-  set gridPos(gridPos: GridPos) {
-    this.#panel.gridPos = gridPos;
-  }
-
-  set options(options: unknown) {
-    this.#panel.options = options;
-  }
-
-  refresh() {
-    this.#panel.refresh();
-  }
-}
+import { PluginsAPIPanelModelWrapper } from './PanelWrapper';
+import { PluginsAPIDashboardModel, PluginsAPIDashboardSrv } from './types';
 
 export class PluginsAPIDashboardModelWrapper implements PluginsAPIDashboardModel {
   #dashboard: PluginsAPIDashboardModel | undefined;
