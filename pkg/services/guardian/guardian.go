13039ea9bde8a04b8d79c4edb7016e76c25732a1
Karl Persson <kalle.persson@grafana.com>
2023-08-24 15:37:54 +0200

    authz: Clean up acl endpoints and dashboard guardian (#73746)
    * RBAC: remove unnessisary guardian construction and update tests
    
    * RBAC: remove usage of guardian in UpdateFolderPermissions and refactor test
    
    * RBAC: remove usage of guardian in update and get permissions for dashboards

--- pkg/services/guardian/guardian.go
+++ pkg/services/guardian/guardian.go
@@ -32,16 +32,6 @@ type DashboardGuardian interface {
 	CanAdmin() (bool, error)
 	CanDelete() (bool, error)
 	CanCreate(folderID int64, isFolder bool) (bool, error)
-	CheckPermissionBeforeUpdate(permission dashboards.PermissionType, updatePermissions []*dashboards.DashboardACL) (bool, error)
-
-	// GetACL returns ACL.
-	GetACL() ([]*dashboards.DashboardACLInfoDTO, error)
-
-	// GetACLWithoutDuplicates returns ACL and strips any permission
-	// that already has an inherited permission with higher or equal
-	// permission.
-	GetACLWithoutDuplicates() ([]*dashboards.DashboardACLInfoDTO, error)
-	GetHiddenACL(*setting.Cfg) ([]*dashboards.DashboardACL, error)
 }
 
 type dashboardGuardianImpl struct {
@@ -468,22 +458,6 @@ func (g *FakeDashboardGuardian) CanCreate(_ int64, _ bool) (bool, error) {
 
 func (g *FakeDashboardGuardian) HasPermission(permission dashboards.PermissionType) (bool, error) {
 	return g.HasPermissionValue, nil
-}
-
-func (g *FakeDashboardGuardian) CheckPermissionBeforeUpdate(permission dashboards.PermissionType, updatePermissions []*dashboards.DashboardACL) (bool, error) {
-	return g.CheckPermissionBeforeUpdateValue, g.CheckPermissionBeforeUpdateError
-}
-
-func (g *FakeDashboardGuardian) GetACL() ([]*dashboards.DashboardACLInfoDTO, error) {
-	return g.GetACLValue, nil
-}
-
-func (g *FakeDashboardGuardian) GetACLWithoutDuplicates() ([]*dashboards.DashboardACLInfoDTO, error) {
-	return g.GetACL()
-}
-
-func (g *FakeDashboardGuardian) GetHiddenACL(cfg *setting.Cfg) ([]*dashboards.DashboardACL, error) {
-	return g.GetHiddenACLValue, nil
 }
 
 // nolint:unused
