c15011a95b5e0dd1c6ea5ad47092946ca4c76ac6
Horst Gutmann <horst.gutmann@grafana.com>
2023-08-24 07:59:02 +0200

    Update version to 10.1.1 in gen.ts files

--- packages/grafana-schema/src/raw/composable/bargauge/panelcfg/x/BarGaugePanelCfg_types.gen.ts
+++ packages/grafana-schema/src/raw/composable/bargauge/panelcfg/x/BarGaugePanelCfg_types.gen.ts
@@ -11,7 +11,7 @@
 
 import * as common from '@grafana/schema';
 
-export const pluginVersion = "10.1.0-pre";
+export const pluginVersion = "10.1.1";
 
 export interface Options extends common.SingleStatBaseOptions {
   displayMode: common.BarGaugeDisplayMode;
