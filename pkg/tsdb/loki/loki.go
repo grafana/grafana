39f09717781b3f32e5e49e9137dbad86b2122952
Sven Grossmann <svennergr@gmail.com>
2023-08-25 11:50:47 +0200

    improve loki `queryData` instrumentation

--- pkg/tsdb/loki/loki.go
+++ pkg/tsdb/loki/loki.go
@@ -14,6 +14,7 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 
 	"github.com/grafana/grafana/pkg/infra/httpclient"
 	"github.com/grafana/grafana/pkg/infra/log"
@@ -122,7 +123,7 @@ func callResource(ctx context.Context, req *backend.CallResourceRequest, sender 
 	span.SetAttributes("url", lokiURL, attribute.Key("url").String(lokiURL))
 	defer span.End()
 
-	api := newLokiAPI(dsInfo.HTTPClient, dsInfo.URL, plog)
+	api := newLokiAPI(dsInfo.HTTPClient, dsInfo.URL, plog, tracer)
 	rawLokiResponse, err := api.RawQuery(ctx, lokiURL)
 
 	if err != nil {
@@ -145,6 +146,8 @@ func callResource(ctx context.Context, req *backend.CallResourceRequest, sender 
 func (s *Service) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
 	dsInfo, err := s.getDSInfo(ctx, req.PluginContext)
 	if err != nil {
+		logger := logger.FromContext(ctx)
+		logger.Error("failed to get data source info", "err", err)
 		result := backend.NewQueryDataResponse()
 		return result, err
 	}
@@ -160,15 +163,17 @@ func (s *Service) QueryData(ctx context.Context, req *backend.QueryDataRequest) 
 func queryData(ctx context.Context, req *backend.QueryDataRequest, dsInfo *datasourceInfo, responseOpts ResponseOpts, tracer tracing.Tracer) (*backend.QueryDataResponse, error) {
 	result := backend.NewQueryDataResponse()
 
-	api := newLokiAPI(dsInfo.HTTPClient, dsInfo.URL, logger.FromContext(ctx))
+	logger := logger.FromContext(ctx)
+	api := newLokiAPI(dsInfo.HTTPClient, dsInfo.URL, logger, tracer)
 
 	queries, err := parseQuery(req)
 	if err != nil {
+		logger.Error("failed to parse queries", "err", err)
 		return result, err
 	}
 
 	for _, query := range queries {
-		ctx, span := tracer.Start(ctx, "datasource.loki")
+		ctx, span := tracer.Start(ctx, "datasource.loki.queryData.runQuery")
 		span.SetAttributes("expr", query.Expr, attribute.Key("expr").String(query.Expr))
 		span.SetAttributes("start_unixnano", query.Start, attribute.Key("start_unixnano").Int64(query.Start.UnixNano()))
 		span.SetAttributes("stop_unixnano", query.End, attribute.Key("stop_unixnano").Int64(query.End.UnixNano()))
@@ -177,21 +182,20 @@ func queryData(ctx context.Context, req *backend.QueryDataRequest, dsInfo *datas
 			span.SetAttributes("query_group_id", req.GetHTTPHeader("X-Query-Group-Id"), attribute.Key("query_group_id").String(req.GetHTTPHeader("X-Query-Group-Id")))
 		}
 
-		logger := logger.FromContext(ctx) // get logger with trace-id and other contextual info
-		logger.Debug("Sending query", "start", query.Start, "end", query.End, "step", query.Step, "query", query.Expr)
-
 		frames, err := runQuery(ctx, api, query, responseOpts)
 
-		span.End()
 		queryRes := backend.DataResponse{}
 
 		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			queryRes.Error = err
 		} else {
 			queryRes.Frames = frames
 		}
 
 		result.Responses[query.RefID] = queryRes
+		span.End()
 	}
 	return result, nil
 }
@@ -200,14 +204,15 @@ func queryData(ctx context.Context, req *backend.QueryDataRequest, dsInfo *datas
 func runQuery(ctx context.Context, api *LokiAPI, query *lokiQuery, responseOpts ResponseOpts) (data.Frames, error) {
 	frames, err := api.DataQuery(ctx, *query, responseOpts)
 	if err != nil {
+		logger.Error("error querying loki", "err", err)
 		return data.Frames{}, err
 	}
 
 	for _, frame := range frames {
-		if err = adjustFrame(frame, query, !responseOpts.metricDataplane, responseOpts.logsDataplane); err != nil {
-			return data.Frames{}, err
-		}
+		err = adjustFrame(frame, query, !responseOpts.metricDataplane, responseOpts.logsDataplane)
+
 		if err != nil {
+			logger.Error("error adjusting frame", "err", err)
 			return data.Frames{}, err
 		}
 	}
@@ -223,7 +228,7 @@ func (s *Service) getDSInfo(ctx context.Context, pluginCtx backend.PluginContext
 
 	instance, ok := i.(*datasourceInfo)
 	if !ok {
-		return nil, fmt.Errorf("failed to cast datsource info")
+		return nil, fmt.Errorf("failed to cast data source info")
 	}
 
 	return instance, nil
