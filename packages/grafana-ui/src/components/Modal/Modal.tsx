cdad31fcc7ae65e00be8c92ff0135093d505384f
dennnnny <dcno017@gmail.com>
2023-08-25 18:07:13 +0800

    fix: change translation modal to grafana-ui.modal

--- packages/grafana-ui/src/components/Modal/Modal.tsx
+++ packages/grafana-ui/src/components/Modal/Modal.tsx
@@ -85,7 +85,12 @@ export function Modal(props: PropsWithChildren<Props>) {
               typeof title !== 'string' && title
             }
             <div className={styles.modalHeaderClose}>
-              <IconButton name="times" size="xl" onClick={onDismiss} tooltip={t('modal.close-tooltip', 'Close')} />
+              <IconButton
+                name="times"
+                size="xl"
+                onClick={onDismiss}
+                tooltip={t('grafana-ui.modal.close-tooltip', 'Close')}
+              />
             </div>
           </div>
           <div className={cx(styles.modalContent, contentClassName)}>{children}</div>
