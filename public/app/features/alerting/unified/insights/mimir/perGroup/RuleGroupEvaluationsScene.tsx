f9674ad2cd394bc152d56c637537b0639dae50cb
Virginia Cepeda <virginia.cepeda@grafana.com>
2023-08-29 14:51:52 -0300

    Fix lint

--- public/app/features/alerting/unified/insights/mimir/perGroup/RuleGroupEvaluationsScene.tsx
+++ public/app/features/alerting/unified/insights/mimir/perGroup/RuleGroupEvaluationsScene.tsx
@@ -1,16 +1,10 @@
-import {
-  PanelBuilders,
-  SceneFlexItem,
-  SceneQueryRunner,
-  SceneTimeRange,
-} from '@grafana/scenes';
+import { PanelBuilders, SceneFlexItem, SceneQueryRunner, SceneTimeRange } from '@grafana/scenes';
 import { DataSourceRef, GraphDrawStyle } from '@grafana/schema';
 
 const QUERY_A = `grafanacloud_instance_rule_evaluations_total:rate5m{rule_group="$rule_group"} - grafanacloud_instance_rule_evaluation_failures_total:rate5m{rule_group=~"$rule_group"}`;
 const QUERY_B = `grafanacloud_instance_rule_evaluation_failures_total:rate5m{rule_group=~"$rule_group"}`;
 
 export function getRuleGroupEvaluationsScene(timeRange: SceneTimeRange, datasource: DataSourceRef, panelTitle: string) {
-  
   const query = new SceneQueryRunner({
     datasource,
     queries: [
