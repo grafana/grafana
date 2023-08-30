63c4a2bcd00544191feedb37e48836e650838b4f
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2023-08-30 11:42:22 -0400

    spelling

--- pkg/server/doc.go
+++ pkg/server/doc.go
@@ -8,12 +8,12 @@ package server
 // wire.go contains wire sets used by both OSS and Enterprise builds. These are
 // generally base wiresets imported by the OSS- or Enterprise-specific sets.
 //
-// wireexts_oss.go contains the "extentions" wiresets, used only by OSS builds.
+// wireexts_oss.go contains the "extensions" wiresets, used only by OSS builds.
 // wireexts_enterprise.go contains wiresets used only by Enterprise builds. This
 // file is located in the grafana-enterprise repo.
 //
-// NOTE WELL: The extentions sets can import wiresets from wire.go, but sets in
-// wire.go cannot include a build-specific wireset. The extention set must be built in wire.go.
+// NOTE WELL: The extensions sets can import wiresets from wire.go, but sets in
+// wire.go cannot include a build-specific wireset. The extension set must be built in wire.go.
 //
 // We use go build tags during build to configure which wiresets are used in a
 // given build. We do not commit generated wire sets (wire_gen.go) into the
