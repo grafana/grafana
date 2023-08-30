f2ad9c65f646927f94db962044f830109c7b2e90
Andres Martinez <andres.martinez@grafana.com>
2023-08-30 11:50:27 +0200

    Fix test execution

--- public/plugins/testdata/jest-setup.js
+++ public/plugins/testdata/jest-setup.js
@@ -1,2 +1,6 @@
 // Jest setup provided by Grafana scaffolding
 import './.config/jest-setup';
+
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
