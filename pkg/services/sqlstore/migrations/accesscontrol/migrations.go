62a6e7ba9a0e6eb8c9b82fc60ff76b857a5904ae
Karl Persson <kalle.persson@grafana.com>
2023-08-30 16:29:38 +0200

    remove index

--- pkg/services/sqlstore/migrations/accesscontrol/migrations.go
+++ pkg/services/sqlstore/migrations/accesscontrol/migrations.go
@@ -186,8 +186,4 @@ func AddMigration(mg *migrator.Migrator) {
 	mg.AddMigration("add permission identifier index", migrator.NewAddIndexMigration(permissionV1, &migrator.Index{
 		Cols: []string{"identifier"},
 	}))
-
-	mg.AddMigration("add role action kind attribute and role index", migrator.NewAddIndexMigration(permissionV1, &migrator.Index{
-		Cols: []string{"role_id", "action", "kind", "attribute"},
-	}))
 }
