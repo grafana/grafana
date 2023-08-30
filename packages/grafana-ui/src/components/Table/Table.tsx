4e0be70508365040a3d92f59a98eaed769a9fe54
sjd210 <sjd210@cam.ac.uk>
2023-08-30 16:36:53 +0100

    Prettier

--- packages/grafana-ui/src/components/Table/Table.tsx
+++ packages/grafana-ui/src/components/Table/Table.tsx
@@ -222,7 +222,8 @@ export const Table = memo((props: Props) => {
 
       const isFieldPopulated = (f: Field) => f.values.length > 0;
       const isDataFramePopulated = (df: DataFrame) => df.fields.some((f: Field) => isFieldPopulated(f));
-      const isNestedDataFieldPopulated = (dfs: DataFrame[] | undefined) => dfs ? dfs.some((df: DataFrame) => isDataFramePopulated(df)) : false;
+      const isNestedDataFieldPopulated = (dfs: DataFrame[] | undefined) =>
+        dfs ? dfs.some((df: DataFrame) => isDataFramePopulated(df)) : false;
       row.canExpand = isNestedDataFieldPopulated(nestedDataField?.values[rowIndex]);
 
       const expandedRowStyle = state.expanded[row.index] ? css({ '&:hover': { background: 'inherit' } }) : {};
