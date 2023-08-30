e1fe5672873409a20c5d86c98cb9633af03138a1
sjd210 <sjd210@cam.ac.uk>
2023-08-30 15:37:47 +0100

    Explicit results for undefined NestedDataField

--- packages/grafana-ui/src/components/Table/Table.tsx
+++ packages/grafana-ui/src/components/Table/Table.tsx
@@ -222,7 +222,7 @@ export const Table = memo((props: Props) => {
 
       const isFieldPopulated = (f: Field) => f.values.length > 0;
       const isDataFramePopulated = (df: DataFrame) => df.fields.some((f: Field) => isFieldPopulated(f));
-      const isNestedDataFieldPopulated = (dfs: DataFrame[]) => dfs.some((df: DataFrame) => isDataFramePopulated(df));
+      const isNestedDataFieldPopulated = (dfs: DataFrame[] | undefined) => dfs ? dfs.some((df: DataFrame) => isDataFramePopulated(df)) : false;
       row.canExpand = isNestedDataFieldPopulated(nestedDataField?.values[rowIndex]);
 
       const expandedRowStyle = state.expanded[row.index] ? css({ '&:hover': { background: 'inherit' } }) : {};
