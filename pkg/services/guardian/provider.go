39641707af68225d7dd50880ad03921e79d76f70
Karl Persson <kalle.persson@grafana.com>
2023-08-30 10:14:17 +0200

    Guardian: Remove legacy dashboard gurdian (#74022)
    Gurdian: Remove legacy dashboard gurdian

--- pkg/services/guardian/provider.go
+++ pkg/services/guardian/provider.go
@@ -3,7 +3,6 @@ package guardian
 import (
 	"context"
 
-	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/dashboards"
 	"github.com/grafana/grafana/pkg/services/folder"
@@ -15,34 +14,12 @@ import (
 type Provider struct{}
 
 func ProvideService(
-	cfg *setting.Cfg, store db.DB, ac accesscontrol.AccessControl,
+	cfg *setting.Cfg, ac accesscontrol.AccessControl,
 	dashboardService dashboards.DashboardService, teamService team.Service,
 ) *Provider {
-	if !ac.IsDisabled() {
-		// TODO: Fix this hack, see https://github.com/grafana/grafana-enterprise/issues/2935
-		InitAccessControlGuardian(cfg, ac, dashboardService)
-	} else {
-		InitLegacyGuardian(cfg, store, dashboardService, teamService)
-	}
+	// TODO: Fix this hack, see https://github.com/grafana/grafana-enterprise/issues/2935
+	InitAccessControlGuardian(cfg, ac, dashboardService)
 	return &Provider{}
-}
-
-func InitLegacyGuardian(cfg *setting.Cfg, store db.DB, dashSvc dashboards.DashboardService, teamSvc team.Service) {
-	New = func(ctx context.Context, dashId int64, orgId int64, user *user.SignedInUser) (DashboardGuardian, error) {
-		return newDashboardGuardian(ctx, cfg, dashId, orgId, user, store, dashSvc, teamSvc)
-	}
-
-	NewByUID = func(ctx context.Context, dashUID string, orgId int64, user *user.SignedInUser) (DashboardGuardian, error) {
-		return newDashboardGuardianByUID(ctx, cfg, dashUID, orgId, user, store, dashSvc, teamSvc)
-	}
-
-	NewByDashboard = func(ctx context.Context, dash *dashboards.Dashboard, orgId int64, user *user.SignedInUser) (DashboardGuardian, error) {
-		return newDashboardGuardianByDashboard(ctx, cfg, dash, orgId, user, store, dashSvc, teamSvc)
-	}
-
-	NewByFolder = func(ctx context.Context, f *folder.Folder, orgId int64, user *user.SignedInUser) (DashboardGuardian, error) {
-		return newDashboardGuardianByFolder(ctx, cfg, f, orgId, user, store, dashSvc, teamSvc)
-	}
 }
 
 func InitAccessControlGuardian(
