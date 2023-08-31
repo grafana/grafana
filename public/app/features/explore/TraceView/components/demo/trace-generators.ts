c954686071666ba56e0f7bfd3cfefbe8e3e68dcb
Fabrizio <fabrizio.casati@grafana.com>
2023-08-31 12:24:21 +0200

    Fix flaky test

--- public/app/features/explore/TraceView/components/demo/trace-generators.ts
+++ public/app/features/explore/TraceView/components/demo/trace-generators.ts
@@ -140,14 +140,16 @@ function attachReferences(spans: TraceSpanData[], depth: number, spansPerLevel: 
 
 export default chance.mixin({
   trace({
-    // long trace
-    // very short trace
-    // average case
-    numberOfSpans = chance.pickone([
-      Math.ceil(chance.normal({ mean: 200, dev: 10 })) + 1,
-      Math.ceil(chance.integer({ min: 3, max: 10 })),
-      Math.ceil(chance.normal({ mean: 45, dev: 15 })) + 1,
-    ]),
+    numberOfSpans = Math.max(
+      Math.ceil(
+        chance.pickone([
+          chance.normal({ mean: 200, dev: 10 }), // long trace
+          chance.integer({ min: 3, max: 10 }), // very short trace
+          chance.normal({ mean: 45, dev: 15 }), // average case
+        ])
+      ),
+      1 // `pickone` might pick a negative number (or zero) from one of the normal distributions, but we want to have at least one span
+    ),
     numberOfProcesses = chance.integer({ min: 1, max: 10 }),
     maxDepth = chance.integer({ min: 1, max: 10 }),
     spansPerLevel = null,
@@ -192,6 +194,8 @@ export default chance.mixin({
     traceEndTime = 0,
     operations = OPERATIONS_LIST,
   }: ChanceSpanOptions) {
+    console.log('span');
+
     // Set default values for trace start/end time.
     traceStartTime = traceStartTime || chance.timestamp() * 1000 * 1000;
     traceEndTime = traceEndTime || traceStartTime + 100000;
