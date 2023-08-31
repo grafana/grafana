1530c4db86cc9af3459607910744c537ec6bfedf
eledobleefe <laura.fernandez@grafana.com>
2023-08-31 13:47:56 +0200

    Apply Grid component to a real page

--- public/app/core/components/NavLandingPage/NavLandingPage.tsx
+++ public/app/core/components/NavLandingPage/NavLandingPage.tsx
@@ -2,7 +2,8 @@ import { css } from '@emotion/css';
 import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { useStyles2 } from '@grafana/ui';
+import { useStyles2, useTheme2 } from '@grafana/ui';
+import { Grid } from '@grafana/ui/src/unstable';
 import { Page } from 'app/core/components/Page/Page';
 import { useNavModel } from 'app/core/hooks/useNavModel';
 
@@ -16,24 +17,25 @@ interface Props {
 export function NavLandingPage({ navId, header }: Props) {
   const { node } = useNavModel(navId);
   const styles = useStyles2(getStyles);
+  const theme: GrafanaTheme2 = useTheme2();
   const children = node.children?.filter((child) => !child.hideFromTabs);
-
+  const gridChildren = children?.map((child) => (
+    <NavLandingPageCard key={child.id} description={child.subTitle} text={child.text} url={child.url ?? ''} />
+  ));
   return (
     <Page navId={node.id}>
       <Page.Contents>
         <div className={styles.content}>
           {header}
-          {children && children.length > 0 && (
-            <section className={styles.grid}>
-              {children?.map((child) => (
-                <NavLandingPageCard
-                  key={child.id}
-                  description={child.subTitle}
-                  text={child.text}
-                  url={child.url ?? ''}
-                />
-              ))}
-            </section>
+          {gridChildren && gridChildren.length > 0 && (
+            <Grid
+              display="grid"
+              gap={theme.spacing(3)}
+              templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+              autoRows="138px"
+            >
+              {gridChildren}
+            </Grid>
           )}
         </div>
       </Page.Contents>
@@ -48,10 +50,6 @@ const getStyles = (theme: GrafanaTheme2) => ({
     gap: theme.spacing(2),
   }),
   grid: css({
-    display: 'grid',
-    gap: theme.spacing(3),
-    gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-    gridAutoRows: '138px',
     padding: theme.spacing(2, 0),
   }),
 });
