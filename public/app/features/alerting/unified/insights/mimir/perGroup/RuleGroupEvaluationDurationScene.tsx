6755be66f98c9a76c05bbd9babe6b478d8375f8d
Virginia Cepeda <virginia.cepeda@grafana.com>
2023-08-31 17:01:47 -0300

    Alerting: Add Insights tab (#72407)
    * Add tabs for Insights/Getting Started in home page
    
    * Display panels using the scenes framework
    
    * Hide Insights page under a feature flag
    
    * Add a new panel and improve ASH queries
    
    * Improve panels layout
    
    * Add more panels
    
    * Change datasource UID
    
    * Rename most fired alerts table title
    
    * Show navigation cards on top
    
    * Improve panels
    
    * Add transformation to display link to view alert rule
    
    * Restructure panels
    
    * Fix panels layout
    
    * Fix grid layout in landing tab
    
    * Add transformation to most fired rules table
    
    * Move grafana-specific scenes under dedicated folder
    
    * Add scene with custom datasource that queries an API
    
    * Add scenes panels for more datasources
    
    Not just ASH but including grafanacloud-usage and grafanacloud-prom metrics
    
    * Changes to grafana panels
    
    * Add per rule group scenes with query variables
    
    Also improve existing panels legends/formatting
    
    * Fix lint
    
    * Fix legends for some panels
    
    * Fix lint
    
    * Move files under new alerting home directory
    
    * Refactor transformation in MostFiredInstancesTable scene
    
    * fix lint
    
    * Display panels in collapsable sections
    
    * Improvements to data displayed in several panels

---
+++ public/app/features/alerting/unified/insights/mimir/perGroup/RuleGroupEvaluationDurationScene.tsx
@@ -0,0 +1,34 @@
+import { PanelBuilders, SceneFlexItem, SceneQueryRunner, SceneTimeRange } from '@grafana/scenes';
+import { DataSourceRef, GraphDrawStyle } from '@grafana/schema';
+
+const QUERY_A = `grafanacloud_instance_rule_group_last_duration_seconds{rule_group="$rule_group"}`;
+
+export function getRuleGroupEvaluationDurationScene(
+  timeRange: SceneTimeRange,
+  datasource: DataSourceRef,
+  panelTitle: string
+) {
+  const query = new SceneQueryRunner({
+    datasource,
+    queries: [
+      {
+        refId: 'A',
+        expr: QUERY_A,
+        range: true,
+        legendFormat: '{{rule_group}}',
+      },
+    ],
+    $timeRange: timeRange,
+  });
+
+  return new SceneFlexItem({
+    width: 'calc(50% - 4px)',
+    height: 300,
+    body: PanelBuilders.timeseries()
+      .setTitle(panelTitle)
+      .setData(query)
+      .setCustomFieldConfig('drawStyle', GraphDrawStyle.Line)
+      .setUnit('s')
+      .build(),
+  });
+}
