ffa03e4e940eb685432f5a266496c8d73afbe7d8
Charandas Batra <charandas.batra@grafana.com>
2023-09-01 10:31:08 -0700

    Revert debug change

--- pkg/modules/modules.go
+++ pkg/modules/modules.go
@@ -3,7 +3,6 @@ package modules
 import (
 	"context"
 	"errors"
-	"time"
 
 	"github.com/grafana/dskit/modules"
 	"github.com/grafana/dskit/services"
@@ -52,7 +51,6 @@ func New(
 func (m *service) Run(ctx context.Context) error {
 	var err error
 
-	time.Sleep(10 * time.Second)
 	for mod, targets := range dependencyMap {
 		if !m.moduleManager.IsModuleRegistered(mod) {
 			continue
