719658bcbda8dd157820a99bc1295334a4acb845
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2023-09-01 16:13:05 +0200

    PR review: Moved marshal into namespacedstore.go

--- pkg/services/pluginsintegration/cachekvstore/namespacedstore.go
+++ pkg/services/pluginsintegration/cachekvstore/namespacedstore.go
@@ -2,6 +2,7 @@ package cachekvstore
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -138,4 +139,24 @@ func (s *NamespacedStore) ListKeys(ctx context.Context) ([]string, error) {
 		res = append(res, key.Key)
 	}
 	return res, nil
+}
+
+// marshal marshals the provided value to a string to store it in the kv store.
+// The provided value can be of a type implementing fmt.Stringer, a string or []byte.
+// If the value is none of those, it is marshaled to JSON.
+func marshal(value any) (string, error) {
+	switch value := value.(type) {
+	case fmt.Stringer:
+		return value.String(), nil
+	case string:
+		return value, nil
+	case []byte:
+		return string(value), nil
+	default:
+		b, err := json.Marshal(value)
+		if err != nil {
+			return "", fmt.Errorf("json marshal: %w", err)
+		}
+		return string(b), nil
+	}
 }
--- pkg/services/pluginsintegration/cachekvstore/namespacedstore_test.go
+++ pkg/services/pluginsintegration/cachekvstore/namespacedstore_test.go
@@ -2,6 +2,7 @@ package cachekvstore
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"sort"
@@ -190,6 +191,47 @@ func TestNamespacedStore(t *testing.T) {
 			})
 		})
 	})
+}
+
+func TestMarshal(t *testing.T) {
+	t.Run("json", func(t *testing.T) {
+		// Other type (rather than string, []byte or fmt.Stringer) marshals to JSON.
+		var value struct {
+			A string `json:"a"`
+			B string `json:"b"`
+		}
+		expV, err := json.Marshal(value)
+		require.NoError(t, err)
+
+		v, err := marshal(value)
+		require.NoError(t, err)
+		require.Equal(t, string(expV), v)
+	})
+
+	t.Run("string", func(t *testing.T) {
+		v, err := marshal("value")
+		require.NoError(t, err)
+		require.Equal(t, "value", v)
+	})
+
+	t.Run("stringer", func(t *testing.T) {
+		var s stringer
+		v, err := marshal(s)
+		require.NoError(t, err)
+		require.Equal(t, s.String(), v)
+	})
+
+	t.Run("byte slice", func(t *testing.T) {
+		v, err := marshal([]byte("value"))
+		require.NoError(t, err)
+		require.Equal(t, "value", v)
+	})
+}
+
+type stringer struct{}
+
+func (s stringer) String() string {
+	return "aaaa"
 }
 
 // mockStoreKeyGetter is a mock implementation of StoreKeyGetter.
