327ae272a7bb7cfc03bbe408975da789e7f2405c
Jack Westbrook <jack.westbrook@gmail.com>
2023-09-04 16:26:35 +0200

    refactor(plugins): migrate PluginsErrorsInfo component to use Alert component

--- public/app/features/plugins/components/PluginsErrorsInfo.tsx
+++ public/app/features/plugins/components/PluginsErrorsInfo.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/css';
+import { style } from 'd3';
 import React from 'react';
 
 import { GrafanaTheme2, PluginErrorCode, PluginSignatureStatus, PluginType } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import { HorizontalGroup, InfoBox, List, PluginSignatureBadge, useStyles2 } from '@grafana/ui';
+import { Alert, HorizontalGroup, Icon, List, PluginSignatureBadge, useStyles2 } from '@grafana/ui';
 
 import { useGetErrors, useFetchStatus } from '../admin/state/hooks';
 
@@ -29,35 +30,40 @@ export function PluginsErrorsInfo({ filterByPluginType }: PluginsErrorInfoProps)
   }
 
   return (
-    <InfoBox
+    <Alert
+      title="Read more about plugin signing"
       aria-label={selectors.pages.PluginsList.signatureErrorNotice}
       severity="warning"
-      urlTitle="Read more about plugin signing"
-      url="https://grafana.com/docs/grafana/latest/plugins/plugin-signatures/"
     >
-      <div>
-        <p>
-          Unsigned plugins were found during plugin initialization. Grafana Labs cannot guarantee the integrity of these
-          plugins. We recommend only using signed plugins.
-        </p>
-        The following plugins are disabled and not shown in the list below:
-        <List
-          items={errors}
-          className={styles.list}
-          renderItem={(error) => (
-            <div className={styles.wrapper}>
-              <HorizontalGroup spacing="sm" justify="flex-start" align="center">
-                <strong>{error.pluginId}</strong>
-                <PluginSignatureBadge
-                  status={mapPluginErrorCodeToSignatureStatus(error.errorCode)}
-                  className={styles.badge}
-                />
-              </HorizontalGroup>
-            </div>
-          )}
-        />
-      </div>
-    </InfoBox>
+      <p>
+        Unsigned plugins were found during plugin initialization. Grafana Labs cannot guarantee the integrity of these
+        plugins. We recommend only using signed plugins.
+      </p>
+      The following plugins are disabled and not shown in the list below:
+      <List
+        items={errors}
+        className={styles.list}
+        renderItem={(error) => (
+          <div className={styles.wrapper}>
+            <HorizontalGroup spacing="sm" justify="flex-start" align="center">
+              <strong>{error.pluginId}</strong>
+              <PluginSignatureBadge
+                status={mapPluginErrorCodeToSignatureStatus(error.errorCode)}
+                className={styles.badge}
+              />
+            </HorizontalGroup>
+          </div>
+        )}
+      />
+      <a
+        href="https://grafana.com/docs/grafana/latest/plugins/plugin-signatures/"
+        className={styles.docsLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Icon name="book" /> Read more about plugin signing
+      </a>
+    </Alert>
   );
 }
 
@@ -84,6 +90,11 @@ function getStyles(theme: GrafanaTheme2) {
     }),
     badge: css({
       marginTop: 0,
+    }),
+    docsLink: css({
+      display: 'inline-block',
+      color: theme.colors.text.link,
+      marginTop: theme.spacing(2),
     }),
   };
 }
