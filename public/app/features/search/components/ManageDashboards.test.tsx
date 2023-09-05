024f5b7883b50cee11876ae29767bd0e9d00d93f
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2023-09-05 14:10:39 +0100

    don't use legacy AC fallback for frontend access control checks

--- public/app/features/search/components/ManageDashboards.test.tsx
+++ public/app/features/search/components/ManageDashboards.test.tsx
@@ -31,17 +31,17 @@ jest.spyOn(console, 'error').mockImplementation();
 
 describe('ManageDashboards', () => {
   beforeEach(() => {
-    (contextSrv.hasAccess as jest.Mock).mockClear();
+    (contextSrv.hasPermission as jest.Mock).mockClear();
   });
 
   it("should hide and show dashboard actions based on user's permissions", async () => {
-    (contextSrv.hasAccess as jest.Mock).mockReturnValue(false);
+    (contextSrv.hasPermission as jest.Mock).mockReturnValue(false);
 
     const { rerender } = await setup();
 
     expect(screen.queryByRole('button', { name: /new/i })).not.toBeInTheDocument();
 
-    (contextSrv.hasAccess as jest.Mock).mockReturnValue(true);
+    (contextSrv.hasPermission as jest.Mock).mockReturnValue(true);
     await waitFor(() => rerender(<ManageDashboardsNew folder={{ canEdit: true } as FolderDTO} />));
 
     expect(screen.getByRole('button', { name: /new/i })).toBeInTheDocument();
