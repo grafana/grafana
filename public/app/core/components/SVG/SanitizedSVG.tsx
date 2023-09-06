0826491ec96a97e37598860318d1ea784391c106
Adela Almasan <adela.almasan@grafana.com>
2023-09-05 23:22:29 -0500

    add id to svg style classes

--- public/app/core/components/SVG/SanitizedSVG.tsx
+++ public/app/core/components/SVG/SanitizedSVG.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import SVG, { Props } from 'react-inlinesvg';
+import { v4 as uuidv4 } from 'uuid';
 
 import { textUtil } from '@grafana/data';
 
-export const SanitizedSVG = (props: Props) => {
-  return <SVG {...props} cacheRequests={true} preProcessor={getCleanSVG} />;
+type SanitizedSVGProps = Props & { cleanStyle?: boolean };
+let shouldCleanSvgStyle = false;
+
+export const SanitizedSVG = (props: SanitizedSVGProps) => {
+  const { cleanStyle, ...inlineSvgProps } = props;
+  shouldCleanSvgStyle = cleanStyle ?? false;
+
+  return <SVG {...inlineSvgProps} cacheRequests={true} preProcessor={getCleanSVG} />;
 };
 
 let cache = new Map<string, string>();
@@ -13,7 +20,31 @@ function getCleanSVG(code: string): string {
   let clean = cache.get(code);
   if (!clean) {
     clean = textUtil.sanitizeSVGContent(code);
+
+    if (shouldCleanSvgStyle && clean.indexOf('<style type="text/css">') > -1) {
+      clean = svgStyleCleanup(clean);
+    }
+
     cache.set(code, clean);
   }
+
   return clean;
+}
+
+function svgStyleCleanup(elementCode: string) {
+  let svgId = elementCode.match(new RegExp('<svg.*id\\s*=\\s*([\'"])(.*?)\\1'))?.[2];
+  if (!svgId) {
+    const pos = elementCode.indexOf('<svg') + 5;
+    svgId = `x${uuidv4()}`;
+    elementCode = elementCode.substring(0, pos) + `id="${svgId}" ` + elementCode.substring(pos);
+  }
+
+  const matchStyle = elementCode.match(new RegExp('<style type="text\\/css"[^>]*>([^<]*)<\\/style>'));
+  if (matchStyle) {
+    let svgStyle = matchStyle[0];
+    let replacedId = svgStyle.replace(/(#(.*?))?\./g, `#${svgId} .`);
+    elementCode = elementCode.replace(svgStyle, replacedId);
+  }
+
+  return elementCode;
 }
