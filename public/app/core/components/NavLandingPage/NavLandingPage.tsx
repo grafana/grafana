e8f38a2409e38cdfb1f5db6686510c9469568770
eledobleefe <laura.fernandez@grafana.com>
2023-09-07 09:33:13 +0200

    Add Flex to NavLandingPage

--- public/app/core/components/NavLandingPage/NavLandingPage.tsx
+++ public/app/core/components/NavLandingPage/NavLandingPage.tsx
@@ -2,8 +2,8 @@ import { css } from '@emotion/css';
 import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { useStyles2, useTheme2 } from '@grafana/ui';
-import { Grid } from '@grafana/ui/src/unstable';
+import { useStyles2 } from '@grafana/ui';
+import { Flex, Grid } from '@grafana/ui/src/unstable';
 import { Page } from 'app/core/components/Page/Page';
 import { useNavModel } from 'app/core/hooks/useNavModel';
 
@@ -17,38 +17,34 @@ interface Props {
 export function NavLandingPage({ navId, header }: Props) {
   const { node } = useNavModel(navId);
   const styles = useStyles2(getStyles);
-  const theme: GrafanaTheme2 = useTheme2();
   const children = node.children?.filter((child) => !child.hideFromTabs);
-  const gridChildren = children?.map((child) => (
-    <NavLandingPageCard key={child.id} description={child.subTitle} text={child.text} url={child.url ?? ''} />
-  ));
+  const gridChildren = () => {
+    const list: React.JSX.Element[] = [];
+    children?.forEach((child) => {
+      list.push(
+        <NavLandingPageCard key={child.id} description={child.subTitle} text={child.text} url={child.url ?? ''} />
+      );
+    });
+    return list;
+  };
   return (
     <Page navId={node.id}>
       <Page.Contents>
-        <div className={styles.content}>
+        <Flex direction="column" gap={2}>
           {header}
-          {gridChildren && gridChildren.length > 0 && (
-            <Grid
-              display="grid"
-              gap={theme.spacing(3)}
-              templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-              autoRows="138px"
-            >
-              {gridChildren}
+          {children && children.length > 0 && (
+            <Grid display="grid" gap={3} templateColumns="repeat(auto-fill, minmax(300px,1fr))" autoRows="138px">
+              {gridChildren()}
             </Grid>
           )}
-        </div>
+        </Flex>
       </Page.Contents>
     </Page>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  content: css({
-    display: 'flex',
-    flexDirection: 'column',
-    gap: theme.spacing(2),
-  }),
+  //TODO is this needed?
   grid: css({
     padding: theme.spacing(2, 0),
   }),
