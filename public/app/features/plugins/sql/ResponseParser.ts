c0fbf60d3b8d20e9619c996433e510660832dbc4
Gábor Farkas <gabor.farkas@gmail.com>
2023-09-08 09:34:14 +0200

    nicer import
    Co-authored-by: Adam Yeats <16296989+adamyeats@users.noreply.github.com>

--- public/app/features/plugins/sql/ResponseParser.ts
+++ public/app/features/plugins/sql/ResponseParser.ts
@@ -2,7 +2,7 @@ import { uniqBy } from 'lodash';
 
 import { DataFrame, MetricFindValue } from '@grafana/data';
 
-import { ResponseParser as ResponseParserType } from './types';
+import type { ResponseParser as ResponseParserType } from './types';
 
 export class ResponseParser implements ResponseParserType {
   transformMetricFindResponse(frame: DataFrame): MetricFindValue[] {
