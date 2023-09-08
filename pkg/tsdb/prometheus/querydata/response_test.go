80a1dace1ff53c3df3adfd1d234b97d8700a3f14
ismail simsek <ismailsimsek09@gmail.com>
2023-09-08 17:16:15 +0200

    More unit tests to cover edge cases

--- pkg/tsdb/prometheus/querydata/response_test.go
+++ pkg/tsdb/prometheus/querydata/response_test.go
@@ -15,21 +15,44 @@ import (
 
 func TestQueryData_parseResponse(t *testing.T) {
 	qd := QueryData{exemplarSampler: exemplar.NewStandardDeviationSampler}
-	gr := "{\"data\":{\"resultType\":\"vector\", \"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}]},\"status\":\"success\"}"
-	br := "{\"data\":{\"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}],\"resultType\":\"vector\"},\"status\":\"success\"}"
 
 	t.Run("resultType is before result the field must parsed normally", func(t *testing.T) {
-
-		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(gr))}
+		resBody := "{\"data\":{\"resultType\":\"vector\", \"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}]},\"status\":\"success\"}"
+		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(resBody))}
 		result := qd.parseResponse(context.Background(), &models.Query{}, res)
 		assert.Nil(t, result.Error)
 		assert.Len(t, result.Frames, 1)
 	})
 
 	t.Run("resultType is after the result field must parsed normally", func(t *testing.T) {
-		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(br))}
+		resBody := "{\"data\":{\"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}],\"resultType\":\"vector\"},\"status\":\"success\"}"
+		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(resBody))}
 		result := qd.parseResponse(context.Background(), &models.Query{}, res)
 		assert.Nil(t, result.Error)
 		assert.Len(t, result.Frames, 1)
+	})
+
+	t.Run("no resultType is existed in the data", func(t *testing.T) {
+		resBody := "{\"data\":{\"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}]},\"status\":\"success\"}"
+		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(resBody))}
+		result := qd.parseResponse(context.Background(), &models.Query{}, res)
+		assert.Error(t, result.Error)
+		assert.Equal(t, result.Error.Error(), "unknown result type: ")
+	})
+
+	t.Run("resultType is set as empty string before result", func(t *testing.T) {
+		resBody := "{\"data\":{\"resultType\":\"\", \"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}]},\"status\":\"success\"}"
+		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(resBody))}
+		result := qd.parseResponse(context.Background(), &models.Query{}, res)
+		assert.Error(t, result.Error)
+		assert.Equal(t, result.Error.Error(), "unknown result type: ")
+	})
+
+	t.Run("resultType is set as empty string after result", func(t *testing.T) {
+		resBody := "{\"data\":{\"result\":[{\"metric\":{\"__name__\":\"some_name\",\"environment\":\"some_env\",\"id\":\"some_id\",\"instance\":\"some_instance:1234\",\"job\":\"some_job\",\"name\":\"another_name\",\"region\":\"some_region\"},\"value\":[1.1,\"2\"]}],\"resultType\":\"\"},\"status\":\"success\"}"
+		res := &http.Response{Body: io.NopCloser(bytes.NewBufferString(resBody))}
+		result := qd.parseResponse(context.Background(), &models.Query{}, res)
+		assert.Error(t, result.Error)
+		assert.Equal(t, result.Error.Error(), "unknown result type: ")
 	})
 }
