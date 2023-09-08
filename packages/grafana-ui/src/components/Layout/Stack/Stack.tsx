38a16518f7bb752d4f55d3e5ce06e7cf1e4968bc
Joao Silva <joao.silva@grafana.com>
2023-09-08 14:51:51 +0100

    Add example to responsive docs

--- packages/grafana-ui/src/components/Layout/Stack/Stack.tsx
+++ packages/grafana-ui/src/components/Layout/Stack/Stack.tsx
@@ -5,8 +5,6 @@ import { ThemeSpacingTokens } from '@grafana/data';
 import { Direction, Flex } from '../Flex/Flex';
 import { ResponsiveProp } from '../utils/responsiveness';
 
-const themeTokenControl = { control: 'select', options: [0, 0.25, 0.5, 1, 1.5, 2, 3, 4, 5, 6, 8, 10] };
-
 interface StackProps {
   direction?: ResponsiveProp<Direction>;
   gap?: ResponsiveProp<ThemeSpacingTokens>;
--- packages/grafana-ui/src/components/Layout/utils/responsiveness.tsx
+++ packages/grafana-ui/src/components/Layout/utils/responsiveness.tsx
@@ -2,6 +2,11 @@ import { CSSInterpolation } from '@emotion/css';
 
 import { GrafanaTheme2, ThemeBreakpointsKey } from '@grafana/data';
 
+/**
+ * Type that represents a prop that can be responsive.
+ *
+ * @example To turn a prop like `margin: number` responsive, change it to `margin: ResponsiveProp<number>`.
+ */
 export type ResponsiveProp<T> =
   | T
   | {
@@ -21,6 +26,8 @@ export type ResponsiveProp<T> =
  * @param prop Prop as it is passed to the component
  * @param getCSS Function that returns the css block for the prop
  * @returns The CSS block repeated for each breakpoint
+ *
+ * @example To get the responsive css equivalent of `margin && { margin }`, you can write `getResponsiveStyle(theme, margin, (val) => { margin: val })`
  */
 export function getResponsiveStyle<T>(
   theme: GrafanaTheme2,
