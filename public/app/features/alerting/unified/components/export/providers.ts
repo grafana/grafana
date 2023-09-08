9c0ac4c57841a61402a0264b3fd4670f5f55f602
Konrad Lalik <konrad.lalik@grafana.com>
2023-09-08 16:26:54 +0200

    Alerting: Add Grafana-managed groups and rules export (#74522)

---
+++ public/app/features/alerting/unified/components/export/providers.ts
@@ -0,0 +1,36 @@
+interface RuleExportProvider<TFormat> {
+  name: string;
+  exportFormat: TFormat;
+  formatter?: (raw: string) => string;
+}
+
+const JsonRuleExportProvider: RuleExportProvider<'json'> = {
+  name: 'JSON',
+  exportFormat: 'json',
+  formatter: (raw: string) => {
+    try {
+      return JSON.stringify(JSON.parse(raw), null, 4);
+    } catch (e) {
+      return raw;
+    }
+  },
+};
+
+const YamlRuleExportProvider: RuleExportProvider<'yaml'> = {
+  name: 'YAML',
+  exportFormat: 'yaml',
+};
+
+// TODO Waiting for BE changes
+// const HclRuleExportProvider: RuleExportProvider<'hcl'> = {
+//   name: 'HCL',
+//   exportFormat: 'hcl',
+// };
+
+export const grafanaRuleExportProviders = {
+  [JsonRuleExportProvider.exportFormat]: JsonRuleExportProvider,
+  [YamlRuleExportProvider.exportFormat]: YamlRuleExportProvider,
+  // [HclRuleExportProvider.exportFormat]: HclRuleExportProvider,
+} as const;
+
+export type RuleExportFormats = keyof typeof grafanaRuleExportProviders;
