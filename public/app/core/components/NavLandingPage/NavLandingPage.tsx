784663445bf88dfc6fd753c5b98a19b052c56c68
eledobleefe <laura.fernandez@grafana.com>
2023-09-11 12:31:40 +0200

    Modified to work with GridItems

--- public/app/core/components/NavLandingPage/NavLandingPage.tsx
+++ public/app/core/components/NavLandingPage/NavLandingPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Flex, Grid } from '@grafana/ui/src/unstable';
+import { Flex, Grid, GridItem } from '@grafana/ui/src/unstable';
 import { Page } from 'app/core/components/Page/Page';
 import { useNavModel } from 'app/core/hooks/useNavModel';
 
@@ -15,12 +15,15 @@ export function NavLandingPage({ navId, header }: Props) {
   const { node } = useNavModel(navId);
   const children = node.children?.filter((child) => !child.hideFromTabs);
   const gridChildren = () => {
-    const list: React.JSX.Element[] = [];
-    children?.forEach((child) => {
-      list.push(
-        <NavLandingPageCard key={child.id} description={child.subTitle} text={child.text} url={child.url ?? ''} />
-      );
-    });
+    if (!children) {
+      return [];
+    }
+    //@ts-ignore
+    const list = children.map((child, i) => (
+      <GridItem key={i} columnEnd={i===3 ? 'span 3' : undefined}>
+        <NavLandingPageCard description={child.subTitle} text={child.text} url={child.url ?? ''} />
+      </GridItem>
+    ));
     return list;
   };
   return (
@@ -28,11 +31,9 @@ export function NavLandingPage({ navId, header }: Props) {
       <Page.Contents>
         <Flex direction="column" gap={2}>
           {header}
-          {children && children.length > 0 && (
-            <Grid display="grid" gap={3} templateColumns="repeat(auto-fill, minmax(300px,1fr))" autoRows="138px">
-              {gridChildren()}
-            </Grid>
-          )}
+          <Grid display="grid" gap={3} templateColumns="repeat(auto-fill, minmax(300px,1fr))" autoRows="138px">
+            {gridChildren()}
+          </Grid>
         </Flex>
       </Page.Contents>
     </Page>
