b8de68cb43198deb95db58bd1b5255558b9dc40c
Andrej Ocenas <mr.ocenas@gmail.com>
2023-09-11 18:14:01 +0200

    Remove backendType reference

--- public/app/plugins/datasource/grafana-pyroscope-datasource/VariableQueryEditor.tsx
+++ public/app/plugins/datasource/grafana-pyroscope-datasource/VariableQueryEditor.tsx
@@ -133,16 +133,15 @@ function ProfileTypeRow(props: {
   initialValue?: string;
 }) {
   const profileTypes = useProfileTypes(props.datasource);
-  const label = props.datasource.backendType === 'phlare' ? 'Profile type' : 'Application';
   return (
     <InlineFieldRow>
       <InlineField
-        label={label}
-        aria-label={label}
+        label={'Profile type'}
+        aria-label={'Profile type'}
         labelWidth={20}
         tooltip={
           <div>
-            Select {props.datasource.backendType === 'phlare' ? 'profile type' : 'application'} for which to retrieve
+            Select profile type for which to retrieve
             available labels
           </div>
         }
