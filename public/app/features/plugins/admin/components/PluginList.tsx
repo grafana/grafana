c11873f70b55e0ad21c307af43744786ae558f64
eledobleefe <laura.fernandez@grafana.com>
2023-09-11 13:08:33 +0200

    Use the GridItem in the PluginList

--- public/app/features/plugins/admin/components/PluginList.tsx
+++ public/app/features/plugins/admin/components/PluginList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { config } from '@grafana/runtime';
-import { Grid } from '@grafana/ui/src/unstable';
+import { Grid, GridItem } from '@grafana/ui/src/unstable';
 
 import { CatalogPlugin, PluginListDisplayMode } from '../types';
 
@@ -17,16 +17,13 @@ export const PluginList = ({ plugins, displayMode }: Props) => {
   const isList = displayMode === PluginListDisplayMode.List;
   const { pathname } = useLocation();
   const pathName = config.appSubUrl + (pathname.endsWith('/') ? pathname.slice(0, -1) : pathname);
-  const gridItemsList = () => {
-    const list: React.JSX.Element[] = [];
-    plugins.map((plugin) => {
-      list.push(<PluginListItem key={plugin.id} plugin={plugin} pathName={pathName} displayMode={displayMode} />)
-    });
-    return list;
-  };
   return (
     <Grid display='grid' gap={3} templateColumns={isList ? '1fr' : 'repeat(auto-fill, minmax(288px, 1fr))'} data-testid="plugin-list">
-      {gridItemsList()}
+      {plugins.map((plugin) => (
+        <GridItem key={plugin.id} displayContents>
+          <PluginListItem key={plugin.id} plugin={plugin} pathName={pathName} displayMode={displayMode} />
+        </GridItem>
+      ))}
     </Grid>
   );
 };
