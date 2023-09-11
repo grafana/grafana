78982c71f21a23a6591548b310d41c7cf4536829
eledobleefe <laura.fernandez@grafana.com>
2023-09-11 13:14:16 +0200

    Fix gridColumnEnd and change code due to GridItem usage

--- public/app/core/components/NavLandingPage/NavLandingPage.tsx
+++ public/app/core/components/NavLandingPage/NavLandingPage.tsx
@@ -14,26 +14,20 @@ interface Props {
 export function NavLandingPage({ navId, header }: Props) {
   const { node } = useNavModel(navId);
   const children = node.children?.filter((child) => !child.hideFromTabs);
-  const gridChildren = () => {
-    if (!children) {
-      return [];
-    }
-    //@ts-ignore
-    const list = children.map((child, i) => (
-      <GridItem key={i} columnEnd={i===3 ? 'span 3' : undefined}>
-        <NavLandingPageCard description={child.subTitle} text={child.text} url={child.url ?? ''} />
-      </GridItem>
-    ));
-    return list;
-  };
   return (
     <Page navId={node.id}>
       <Page.Contents>
         <Flex direction="column" gap={2}>
           {header}
-          <Grid display="grid" gap={3} templateColumns="repeat(auto-fill, minmax(300px,1fr))" autoRows="138px">
-            {gridChildren()}
-          </Grid>
+          {children && children.length > 0 && (
+            <Grid display="grid" gap={3} templateColumns="repeat(auto-fill, minmax(300px,1fr))" autoRows="138px">
+              {children?.map((child) => (
+                <GridItem key={child.id} displayContents>
+                  <NavLandingPageCard description={child.subTitle} text={child.text} url={child.url ?? ''} />
+                </GridItem>
+              ))}
+            </Grid>
+          )}
         </Flex>
       </Page.Contents>
     </Page>
