e4ce9d4f4515901fb0d752cb19b0223d910faf77
Matias Chomicki <matyax@gmail.com>
2023-09-11 17:15:48 +0200

    Remove unsused import

--- public/app/features/logs/components/LogDetailsRow.test.tsx
+++ public/app/features/logs/components/LogDetailsRow.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React, { ComponentProps } from 'react';
 
 import { LogRowModel } from '@grafana/data';
