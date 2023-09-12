bc41cd5ddefa1a7978d936b5487d7ed9e881856e
Andrej Ocenas <mr.ocenas@gmail.com>
2023-09-12 12:28:48 +0200

    Flamegraph: Move to package (#73113)

---
+++ packages/grafana-flamegraph/src/types.ts
@@ -0,0 +1,57 @@
+import { LevelItem } from './FlameGraph/dataTransform';
+
+export type ClickedItemData = {
+  posX: number;
+  posY: number;
+  label: string;
+  item: LevelItem;
+  level: number;
+};
+
+export enum SampleUnit {
+  Bytes = 'bytes',
+  Short = 'short',
+  Nanoseconds = 'ns',
+}
+
+export enum ColumnTypes {
+  Symbol = 'Symbol',
+  Self = 'Self',
+  Total = 'Total',
+}
+
+export enum SelectedView {
+  TopTable = 'topTable',
+  FlameGraph = 'flameGraph',
+  Both = 'both',
+}
+
+export interface TableData {
+  self: number;
+  total: number;
+  // For diff view
+  totalRight: number;
+}
+
+export interface TopTableData {
+  symbol: string;
+  self: TopTableValue;
+  total: TopTableValue;
+}
+
+export type TopTableValue = {
+  value: number;
+  unitValue: string;
+};
+
+export enum ColorScheme {
+  ValueBased = 'valueBased',
+  PackageBased = 'packageBased',
+}
+
+export enum ColorSchemeDiff {
+  Default = 'default',
+  DiffColorBlind = 'diffColorBlind',
+}
+
+export type TextAlign = 'left' | 'right';
