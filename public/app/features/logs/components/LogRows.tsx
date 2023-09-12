181ea8cd80e8f71ef5f29fceb870380473f3834d
Galen Kistler <109082771+gtk-grafana@users.noreply.github.com>
2023-09-12 09:57:14 -0500

    Logs Panel: Performance issue while scrolling within panel in safari (#74694)
    * adding contain:strict to let browsers know the layout of logs panel can be rendered independently
    
    * add new prop to allow LogRow component to contain content
    
    (cherry picked from commit 4f5728233c8994b47cd7904b2c66927c803f2a91)

--- public/app/features/logs/components/LogRows.tsx
+++ public/app/features/logs/components/LogRows.tsx
@@ -1,3 +1,4 @@
+import { cx } from '@emotion/css';
 import memoizeOne from 'memoize-one';
 import React, { PureComponent } from 'react';
 
@@ -53,6 +54,11 @@ export interface Props extends Themeable2 {
   isFilterLabelActive?: (key: string, value: string) => Promise<boolean>;
   pinnedRowId?: string;
   containerRendered?: boolean;
+  /**
+   * If false or undefined, the `contain:strict` css property will be added to the wrapping `<table>` for performance reasons.
+   * Any overflowing content will be clipped at the table boundary.
+   */
+  overflowingContent?: boolean;
 }
 
 interface State {
@@ -128,7 +134,7 @@ class UnThemedLogRows extends PureComponent<Props, State> {
     const keyMaker = new UniqueKeyMaker();
 
     return (
-      <table className={styles.logsRowsTable}>
+      <table className={cx(styles.logsRowsTable, this.props.overflowingContent ? '' : styles.logsRowsTableContain)}>
         <tbody>
           {hasData &&
             firstRows.map((row) => (
