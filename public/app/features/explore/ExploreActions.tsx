9c8b9b0f839fbddd15705ca113d367fecce82cbb
Kristina Durivage <kristina.durivage@grafana.com>
2023-09-12 07:28:54 -0500

    fix import

--- public/app/features/explore/ExploreActions.tsx
+++ public/app/features/explore/ExploreActions.tsx
@@ -2,7 +2,7 @@ import { useRegisterActions, useKBar, Action, Priority } from 'kbar';
 import { useEffect, useState } from 'react';
 
 import { config } from '@grafana/runtime';
-import { contextSrv } from 'app/core/core';
+import { contextSrv } from 'app/core/services/context_srv';
 import { MIXED_DATASOURCE_NAME } from 'app/plugins/datasource/mixed/MixedDataSource';
 import { AccessControlAction, ExploreItemState, useDispatch, useSelector } from 'app/types';
 
