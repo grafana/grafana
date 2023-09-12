5ef17cc93d80cd97daf77ac44f9d26fa00b08dcf
jev forsberg <jev.forsberg@grafana.com>
2023-09-12 09:53:40 -0600

    baldm0mma/transformation_info_modal/ update ExtractFieldsHelper

--- public/app/features/transformers/helpers/ExtractFieldsHelper.tsx
+++ public/app/features/transformers/helpers/ExtractFieldsHelper.tsx
@@ -1,108 +1,42 @@
-import React from 'react';
-
-// JEV: esc `'` with `&apos;`?
-
 export const ExtractFieldsHelper = () => {
-  return (
-    <div>
-      <p>
-        Use this transformation to select one source of data and extract content from it in different formats. Set the
-        following fields:
-      </p>
-      <ul style={{ paddingLeft: 30 }}>
-        <li>
-          <strong>Source</strong> - Select the field for the source of data.
-        </li>
-        <li>
-          <strong>Format</strong> - Select one of the following:
-          <ul style={{ paddingLeft: 30 }}>
-            <li>
-              <strong>JSON</strong> - To parse JSON content from the source.
-            </li>
-            <li>
-              <strong>Key+value parse</strong> - To parse content in the format <code>a=b</code> or <code>c:d</code>{' '}
-              from the source.
-            </li>
-            <li>
-              <strong>Auto</strong> - To discover fields automatically.
-            </li>
-          </ul>
-        </li>
-        <li>
-          <strong>Replace all fields</strong> - Optional: Select this option if you want to hide all other fields and
-          display only your calculated field in the visualization.
-        </li>
-        <li>
-          <strong>Keep time</strong> - Optional: Only available if <strong>Replace all fields</strong> is true. Keep the
-          time field in the output.
-        </li>
-      </ul>
-      <br />
-      <p>Consider the following data set:</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Timestamp</th>
-            <th style={{ paddingLeft: 30 }}>json_data</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1636678740000000000</td>
-            <td style={{ paddingLeft: 30 }}>{JSON.stringify({ value: 1 })}</td>
-          </tr>
-          <tr>
-            <td>1636678680000000000</td>
-            <td style={{ paddingLeft: 30 }}>{JSON.stringify({ value: 5 })}</td>
-          </tr>
-          <tr>
-            <td>1636678620000000000</td>
-            <td style={{ paddingLeft: 30 }}>{JSON.stringify({ value: 12 })}</td>
-          </tr>
-        </tbody>
-      </table>
-      <br />
-      <p>
-        You could prepare the data to be used by a{' '}
-        <a href="/docs/grafana/latest/panels-visualizations/visualizations/time-series/">Time series panel</a> with this
-        configuration:
-      </p>
-      <ul style={{ paddingLeft: 30 }}>
-        <li>Source: json_data</li>
-        <li>
-          Format: JSON
-          <ul style={{ paddingLeft: 30 }}>
-            <li>Field: value</li>
-            <li>alias: my_value</li>
-          </ul>
-        </li>
-        <li>Replace all fields: true</li>
-        <li>Keep time: true</li>
-      </ul>
-      <br />
-      <p>This will generate the following output:</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Timestamp</th>
-            <th style={{ paddingLeft: 30 }}>my_value</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1636678740000000000</td>
-            <td style={{ paddingLeft: 30 }}>1</td>
-          </tr>
-          <tr>
-            <td>1636678680000000000</td>
-            <td style={{ paddingLeft: 30 }}>5</td>
-          </tr>
-          <tr>
-            <td>1636678620000000000</td>
-            <td style={{ paddingLeft: 30 }}>12</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+  return `
+  Use this transformation to select one source of data and extract content from it in different formats. Set the following fields:
+
+  - **Source** - Select the field for the source of data.
+  - **Format** - Select one of the following:
+    - **JSON** - To parse JSON content from the source.
+    - **Key+value parse** - To parse content in the format 'a=b' or 'c:d' from the source.
+    - **Auto** - To discover fields automatically.
+  - **Replace all fields** - Optional: Select this option if you want to hide all other fields and display only your calculated field in the visualization.
+  - **Keep time** - Optional: Only available if **Replace all fields** is true. Keep the time field in the output.
+
+  Consider the following data set:
+
+  ## Data Set Example
+
+  | Timestamp         | json_data |
+  |-------------------|-----------|
+  | 1636678740000000000 | {"value": 1} |
+  | 1636678680000000000 | {"value": 5} |
+  | 1636678620000000000 | {"value": 12} |
+
+  You could prepare the data to be used by a [Time series panel](https://grafana.com/docs/grafana/latest/panels-visualizations/visualizations/time-series/) with this configuration:
+
+  - Source: json_data
+  - Format: JSON
+    - Field: value
+    - Alias: my_value
+  - Replace all fields: true
+  - Keep time: true
+
+  This will generate the following output:
+
+  ## Transformed Data
+
+  | Timestamp         | my_value |
+  |-------------------|----------|
+  | 1636678740000000000 | 1        |
+  | 1636678680000000000 | 5        |
+  | 1636678620000000000 | 12       |
+  `;
 };
