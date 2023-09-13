bfba7426694efbb918b12b61c79140cb67d06374
Santiago <santiagohernandez.1997@gmail.com>
2023-09-13 18:32:20 +0200

    Alerting: Add empty remote alertmanager struct

---
+++ pkg/services/ngalert/notifier/external_alertmanager.go
@@ -0,0 +1,159 @@
+package notifier
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/url"
+
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/grafana/grafana/pkg/infra/log"
+	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
+	"github.com/grafana/grafana/pkg/services/ngalert/models"
+	amclient "github.com/prometheus/alertmanager/api/v2/client"
+)
+
+type externalAlertmanager struct {
+	log           log.Logger
+	url           string
+	tenantID      string
+	orgID         int64
+	amClient      *amclient.AlertmanagerAPI
+	httpClient    *http.Client
+	defaultConfig string
+}
+
+type externalAlertmanagerConfig struct {
+	URL               string
+	TenantID          string
+	BasicAuthPassword string
+	DefaultConfig     string
+}
+
+func newExternalAlertmanager(cfg externalAlertmanagerConfig, orgID int64) (*externalAlertmanager, error) {
+	client := http.Client{
+		Transport: &roundTripper{
+			tenantID:          cfg.TenantID,
+			basicAuthPassword: cfg.BasicAuthPassword,
+			next:              http.DefaultTransport,
+		},
+	}
+
+	if cfg.URL == "" {
+		return nil, errors.New("empty URL")
+	}
+
+	u, err := url.Parse(cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	transport := httptransport.NewWithClient(u.Host, amclient.DefaultBasePath, []string{u.Scheme}, &client)
+
+	_, err = Load([]byte(cfg.DefaultConfig))
+	if err != nil {
+		return nil, err
+	}
+
+	return &externalAlertmanager{
+		amClient:      amclient.New(transport, nil),
+		httpClient:    &client,
+		log:           log.New("ngalert.notifier.external.alertmanager"),
+		url:           cfg.URL,
+		tenantID:      cfg.TenantID,
+		orgID:         orgID,
+		defaultConfig: cfg.DefaultConfig,
+	}, nil
+}
+
+func (am *externalAlertmanager) SaveAndApplyConfig(ctx context.Context, cfg *apimodels.PostableUserConfig) error {
+	return nil
+}
+
+func (am *externalAlertmanager) SaveAndApplyDefaultConfig(ctx context.Context) error {
+	return nil
+}
+
+func (am *externalAlertmanager) GetStatus() (apimodels.GettableStatus, error) {
+	return apimodels.GettableStatus{}, nil
+}
+
+func (am *externalAlertmanager) CreateSilence(*apimodels.PostableSilence) (string, error) {
+	return "", nil
+}
+
+func (am *externalAlertmanager) DeleteSilence(string) error {
+	return nil
+}
+
+func (am *externalAlertmanager) GetSilence(silenceID string) (apimodels.GettableSilence, error) {
+	return apimodels.GettableSilence{}, nil
+}
+
+func (am *externalAlertmanager) ListSilences([]string) (apimodels.GettableSilences, error) {
+	return apimodels.GettableSilences{}, nil
+}
+
+func (am *externalAlertmanager) GetAlerts(active, silenced, inhibited bool, filter []string, receiver string) (apimodels.GettableAlerts, error) {
+	return apimodels.GettableAlerts{}, nil
+}
+
+func (am *externalAlertmanager) GetAlertGroups(active, silenced, inhibited bool, filter []string, receiver string) (apimodels.AlertGroups, error) {
+	return apimodels.AlertGroups{}, nil
+}
+
+func (am *externalAlertmanager) PutAlerts(postableAlerts apimodels.PostableAlerts) error {
+	return nil
+}
+
+func (am *externalAlertmanager) GetReceivers(ctx context.Context) ([]apimodels.Receiver, error) {
+	return []apimodels.Receiver{}, nil
+}
+
+func (am *externalAlertmanager) ApplyConfig(ctx context.Context, config *models.AlertConfiguration) error {
+	return nil
+}
+
+func (am *externalAlertmanager) TestReceivers(ctx context.Context, c apimodels.TestReceiversConfigBodyParams) (*TestReceiversResult, error) {
+	return &TestReceiversResult{}, nil
+}
+
+func (am *externalAlertmanager) TestTemplate(ctx context.Context, c apimodels.TestTemplatesConfigBodyParams) (*TestTemplatesResults, error) {
+	return &TestTemplatesResults{}, nil
+}
+
+func (am *externalAlertmanager) StopAndWait() {
+}
+
+func (am *externalAlertmanager) Ready() bool {
+	return false
+}
+
+func (am *externalAlertmanager) FileStore() *FileStore {
+	return &FileStore{}
+}
+
+func (am *externalAlertmanager) OrgID() int64 {
+	return am.orgID
+}
+
+func (am *externalAlertmanager) ConfigHash() [16]byte {
+	return [16]byte{}
+}
+
+type roundTripper struct {
+	tenantID          string
+	basicAuthPassword string
+	next              http.RoundTripper
+}
+
+// RoundTrip implements the http.RoundTripper interface
+// while adding the `X-Scope-OrgID` header and basic auth credentials.
+func (r *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("X-Scope-OrgID", r.tenantID)
+	if r.tenantID != "" && r.basicAuthPassword != "" {
+		req.SetBasicAuth(r.tenantID, r.basicAuthPassword)
+	}
+
+	return r.next.RoundTrip(req)
+}
