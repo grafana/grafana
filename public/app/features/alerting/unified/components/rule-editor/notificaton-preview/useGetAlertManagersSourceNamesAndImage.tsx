b96687a603670153a7e1b84841ed5a9be9456e0e
Virginia Cepeda <virginia.cepeda@grafana.com>
2023-09-13 18:49:22 -0300

    Change way of obtaining external datasources info

--- public/app/features/alerting/unified/components/rule-editor/notificaton-preview/useGetAlertManagersSourceNamesAndImage.tsx
+++ public/app/features/alerting/unified/components/rule-editor/notificaton-preview/useGetAlertManagersSourceNamesAndImage.tsx
@@ -1,7 +1,7 @@
 import { AlertmanagerChoice } from '../../../../../../plugins/datasource/alertmanager/types';
 import { alertmanagerApi } from '../../../api/alertmanagerApi';
 import { useExternalDataSourceAlertmanagers } from '../../../hooks/useExternalAmSelector';
-import { GRAFANA_RULES_SOURCE_NAME } from '../../../utils/datasource';
+import { getAlertManagerDataSources, GRAFANA_RULES_SOURCE_NAME } from '../../../utils/datasource';
 
 export interface AlertManagerNameWithImage {
   name: string;
@@ -12,10 +12,13 @@ export const useGetAlertManagersSourceNamesAndImage = () => {
   //get current alerting config
   const { currentData: amConfigStatus } = alertmanagerApi.useGetAlertmanagerChoiceStatusQuery(undefined);
 
-  const externalDsAlertManagers: AlertManagerNameWithImage[] = useExternalDataSourceAlertmanagers().map((ds) => ({
-    name: ds.dataSource.name,
-    img: ds.dataSource.meta.info.logos.small,
-  }));
+  const externalDsAlertManagers = getAlertManagerDataSources()
+    .filter((ds) => ds.jsonData.handleGrafanaManagedAlerts)
+    .map((ds) => ({
+      name: ds.name,
+      img: ds.meta.info.logos.small,
+    }));
+
   const alertmanagerChoice = amConfigStatus?.alertmanagersChoice;
   const alertManagerSourceNamesWithImage: AlertManagerNameWithImage[] =
     alertmanagerChoice === AlertmanagerChoice.Internal
