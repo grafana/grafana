4a57ea29a79402dde9636c7babd0af7321979f04
Karl Persson <kalle.persson@grafana.com>
2023-09-14 16:58:04 +0200

    AuthN: log error and move on if id token failed to be signed

--- pkg/services/authn/authnimpl/sync/id_token_sync.go
+++ pkg/services/authn/authnimpl/sync/id_token_sync.go
@@ -3,18 +3,20 @@ package sync
 import (
 	"context"
 
+	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/services/auth"
 	"github.com/grafana/grafana/pkg/services/authn"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 )
 
 type IDTokenSync struct {
+	log      log.Logger
 	signer   auth.IDSignerService
 	features featuremgmt.FeatureToggles
 }
 
 func ProvideIDTokenSync(signer auth.IDSignerService, features featuremgmt.FeatureToggles) *IDTokenSync {
-	return &IDTokenSync{signer: signer, features: features}
+	return &IDTokenSync{log: log.New("id-token.sync"), signer: signer, features: features}
 }
 
 func (s *IDTokenSync) SyncIDTokenHook(ctx context.Context, identity *authn.Identity, req *authn.Request) error {
@@ -25,7 +27,10 @@ func (s *IDTokenSync) SyncIDTokenHook(ctx context.Context, identity *authn.Ident
 	// HACK: implement identity.Requester for authn.Identity
 	token, err := s.signer.SignIdentity(ctx, identity.SignedInUser(), req.HTTPRequest)
 	if err != nil {
-		return err
+		namespace, id := identity.NamespacedID()
+		s.log.Error("Failed to sign id token", "err", err, "namespace", namespace, "id", id)
+		// for now don't return error so we don't break authentication from this hook
+		return nil
 	}
 
 	identity.IDToken = token
