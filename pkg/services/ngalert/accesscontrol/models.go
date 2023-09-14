73cd63260eb99d17ea5553d12a94aa7959ed4ef0
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-09-14 13:56:04 -0400

    convert to service

--- pkg/services/ngalert/accesscontrol/models.go
+++ pkg/services/ngalert/accesscontrol/models.go
@@ -1,8 +1,16 @@
 package accesscontrol
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	ErrAuthorization = errors.New("user is not authorized")
 )
 
+type ActionsProvider struct {
+	Create string
+	Read   string
+	Update string
+	Delete string
+}
--- pkg/services/ngalert/accesscontrol/rules.go
+++ pkg/services/ngalert/accesscontrol/rules.go
@@ -3,22 +3,49 @@ package accesscontrol
 import (
 	"fmt"
 
+	"golang.org/x/net/context"
+
 	"github.com/grafana/grafana/pkg/expr"
+	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/auth/identity"
 	"github.com/grafana/grafana/pkg/services/dashboards"
 	"github.com/grafana/grafana/pkg/services/datasources"
 	"github.com/grafana/grafana/pkg/services/ngalert/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/store"
 )
 
+var logger = log.New("ngalert.accesscontrol")
+
+type RuleService struct {
+	ac      accesscontrol.AccessControl
+	actions ActionsProvider
+}
+
+func NewRuleService(ac accesscontrol.AccessControl, actions ActionsProvider) *RuleService {
+	return &RuleService{
+		ac:      ac,
+		actions: actions,
+	}
+}
+
+func (r *RuleService) hasAccess(ctx context.Context, user identity.Requester, evaluator accesscontrol.Evaluator) bool {
+	result, err := r.ac.Evaluate(ctx, user, evaluator)
+	if err != nil { // this is how accesscontrol.HasAccess works. //TODO change when AuthorizeDatasourceAccessForRule can return errors
+		logger.FromContext(ctx).Error("Failed to evaluate access control", "error", err)
+		return false
+	}
+	return result
+}
+
 // AuthorizeDatasourceAccessForRule checks that user has access to all data sources declared by the rule
-func AuthorizeDatasourceAccessForRule(rule *models.AlertRule, evaluator func(evaluator accesscontrol.Evaluator) bool) bool {
+func (r *RuleService) AuthorizeDatasourceAccessForRule(ctx context.Context, user identity.Requester, rule *models.AlertRule) bool {
 	for _, query := range rule.Data {
 		if query.QueryType == expr.DatasourceType || query.DatasourceUID == expr.DatasourceUID || query.
 			DatasourceUID == expr.OldDatasourceUID {
 			continue
 		}
-		if !evaluator(accesscontrol.EvalPermission(datasources.ActionQuery, datasources.ScopeProvider.GetResourceScopeUID(query.DatasourceUID))) {
+		if !r.hasAccess(ctx, user, accesscontrol.EvalPermission(datasources.ActionQuery, datasources.ScopeProvider.GetResourceScopeUID(query.DatasourceUID))) {
 			return false
 		}
 	}
@@ -26,9 +53,9 @@ func AuthorizeDatasourceAccessForRule(rule *models.AlertRule, evaluator func(eva
 }
 
 // AuthorizeAccessToRuleGroup checks all rules against AuthorizeDatasourceAccessForRule and exits on the first negative result
-func AuthorizeAccessToRuleGroup(rules []*models.AlertRule, evaluator func(evaluator accesscontrol.Evaluator) bool) bool {
+func (r *RuleService) AuthorizeAccessToRuleGroup(ctx context.Context, user identity.Requester, rules models.RulesGroup) bool {
 	for _, rule := range rules {
-		if !AuthorizeDatasourceAccessForRule(rule, evaluator) {
+		if !r.AuthorizeDatasourceAccessForRule(ctx, user, rule) {
 			return false
 		}
 	}
@@ -39,12 +66,12 @@ func AuthorizeAccessToRuleGroup(rules []*models.AlertRule, evaluator func(evalua
 // NOTE: if there are rules for deletion, and the user does not have access to data sources that a rule uses, the rule is removed from the list.
 // If the user is not authorized to perform the changes the function returns ErrAuthorization with a description of what action is not authorized.
 // Return changes that the user is authorized to perform or ErrAuthorization
-func AuthorizeRuleChanges(change *store.GroupDelta, evaluator func(evaluator accesscontrol.Evaluator) bool) error {
+func (r *RuleService) AuthorizeRuleChanges(ctx context.Context, user identity.Requester, change *store.GroupDelta) error {
 	namespaceScope := dashboards.ScopeFoldersProvider.GetResourceScopeUID(change.GroupKey.NamespaceUID)
 
 	rules, ok := change.AffectedGroups[change.GroupKey]
 	if ok { // not ok can be when user creates a new rule group or moves existing alerts to a new group
-		if !AuthorizeAccessToRuleGroup(rules, evaluator) { // if user is not authorized to do operation in the group that is being changed
+		if !r.AuthorizeAccessToRuleGroup(ctx, user, rules) { // if user is not authorized to do operation in the group that is being changed
 			return fmt.Errorf("%w to change group %s because it does not have access to one or many rules in this group", ErrAuthorization, change.GroupKey.RuleGroup)
 		}
 	} else if len(change.Delete) > 0 {
@@ -53,12 +80,12 @@ func AuthorizeRuleChanges(change *store.GroupDelta, evaluator func(evaluator acc
 	}
 
 	if len(change.Delete) > 0 {
-		allowed := evaluator(accesscontrol.EvalPermission(accesscontrol.ActionAlertingRuleDelete, namespaceScope))
+		allowed := r.hasAccess(ctx, user, accesscontrol.EvalPermission(r.actions.Delete, namespaceScope))
 		if !allowed {
 			return fmt.Errorf("%w to delete alert rules that belong to folder %s", ErrAuthorization, change.GroupKey.NamespaceUID)
 		}
 		for _, rule := range change.Delete {
-			if !AuthorizeDatasourceAccessForRule(rule, evaluator) {
+			if !r.AuthorizeDatasourceAccessForRule(ctx, user, rule) {
 				return fmt.Errorf("%w to delete an alert rule '%s' because the user does not have read permissions for one or many datasources the rule uses", ErrAuthorization, rule.UID)
 			}
 		}
@@ -67,39 +94,37 @@ func AuthorizeRuleChanges(change *store.GroupDelta, evaluator func(evaluator acc
 	var addAuthorized, updateAuthorized bool
 
 	if len(change.New) > 0 {
-		addAuthorized = evaluator(accesscontrol.EvalPermission(accesscontrol.ActionAlertingRuleCreate, namespaceScope))
+		addAuthorized = r.hasAccess(ctx, user, accesscontrol.EvalPermission(r.actions.Create, namespaceScope))
 		if !addAuthorized {
 			return fmt.Errorf("%w to create alert rules in the folder %s", ErrAuthorization, change.GroupKey.NamespaceUID)
 		}
 		for _, rule := range change.New {
-			dsAllowed := AuthorizeDatasourceAccessForRule(rule, evaluator)
-			if !dsAllowed {
+			if !r.AuthorizeDatasourceAccessForRule(ctx, user, rule) {
 				return fmt.Errorf("%w to create a new alert rule '%s' because the user does not have read permissions for one or many datasources the rule uses", ErrAuthorization, rule.Title)
 			}
 		}
 	}
 
 	for _, rule := range change.Update {
-		dsAllowed := AuthorizeDatasourceAccessForRule(rule.New, evaluator)
-		if !dsAllowed {
+		if !r.AuthorizeDatasourceAccessForRule(ctx, user, rule.New) {
 			return fmt.Errorf("%w to update alert rule '%s' (UID: %s) because the user does not have read permissions for one or many datasources the rule uses", ErrAuthorization, rule.Existing.Title, rule.Existing.UID)
 		}
 
 		// Check if the rule is moved from one folder to the current. If yes, then the user must have the authorization to delete rules from the source folder and add rules to the target folder.
 		if rule.Existing.NamespaceUID != rule.New.NamespaceUID {
-			allowed := evaluator(accesscontrol.EvalAll(accesscontrol.EvalPermission(accesscontrol.ActionAlertingRuleDelete, dashboards.ScopeFoldersProvider.GetResourceScopeUID(rule.Existing.NamespaceUID))))
+			allowed := r.hasAccess(ctx, user, accesscontrol.EvalPermission(r.actions.Delete, dashboards.ScopeFoldersProvider.GetResourceScopeUID(rule.Existing.NamespaceUID)))
 			if !allowed {
 				return fmt.Errorf("%w to delete alert rules from folder UID %s", ErrAuthorization, rule.Existing.NamespaceUID)
 			}
 
 			if !addAuthorized {
-				addAuthorized = evaluator(accesscontrol.EvalPermission(accesscontrol.ActionAlertingRuleCreate, namespaceScope))
+				addAuthorized = r.hasAccess(ctx, user, accesscontrol.EvalPermission(r.actions.Create, namespaceScope))
 				if !addAuthorized {
 					return fmt.Errorf("%w to create alert rules in the folder '%s'", ErrAuthorization, change.GroupKey.NamespaceUID)
 				}
 			}
 		} else if !updateAuthorized { // if it is false then the authorization was not checked. If it is true then the user is authorized to update rules
-			updateAuthorized = evaluator(accesscontrol.EvalPermission(accesscontrol.ActionAlertingRuleUpdate, namespaceScope))
+			updateAuthorized = r.hasAccess(ctx, user, accesscontrol.EvalPermission(r.actions.Update, namespaceScope))
 			if !updateAuthorized {
 				return fmt.Errorf("%w to update alert rules that belong to folder %s", ErrAuthorization, change.GroupKey.NamespaceUID)
 			}
@@ -112,7 +137,7 @@ func AuthorizeRuleChanges(change *store.GroupDelta, evaluator func(evaluator acc
 				// add a safeguard in the case of inconsistency. If user hit this then there is a bug in the calculating of changes struct
 				return fmt.Errorf("failed to authorize moving an alert rule %s between groups because unable to check access to group %s from which the rule is moved", rule.Existing.UID, rule.Existing.RuleGroup)
 			}
-			if !AuthorizeAccessToRuleGroup(rules, evaluator) {
+			if !r.AuthorizeAccessToRuleGroup(ctx, user, rules) {
 				return fmt.Errorf("%w to move rule %s between two different groups because user does not have access to the source group %s", ErrAuthorization, rule.Existing.UID, rule.Existing.RuleGroup)
 			}
 		}
