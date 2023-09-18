5fa6d13b3b753400a5aaf240831c95c7e75a8052
Virginia Cepeda <virginia.cepeda@grafana.com>
2023-09-18 16:03:29 -0300

    Add Grafana Alertmanager panels

---
+++ public/app/features/alerting/unified/insights/grafana/alertmanager/SilencesByStateScene.tsx
@@ -0,0 +1,32 @@
+import { PanelBuilders, SceneFlexItem, SceneQueryRunner, SceneTimeRange } from '@grafana/scenes';
+import { DataSourceRef, GraphDrawStyle } from '@grafana/schema';
+
+const QUERY_A = 'sum by (state) (grafanacloud_grafana_instance_alerting_silences)';
+
+export function getGrafanaAlertmanagerSilencesScene(
+  timeRange: SceneTimeRange,
+  datasource: DataSourceRef,
+  panelTitle: string
+) {
+  const query = new SceneQueryRunner({
+    datasource,
+    queries: [
+      {
+        refId: 'A',
+        expr: QUERY_A,
+        range: true,
+        legendFormat: '{{state}}',
+      },
+    ],
+    $timeRange: timeRange,
+  });
+
+  return new SceneFlexItem({
+    minHeight: 300,
+    body: PanelBuilders.timeseries()
+      .setTitle(panelTitle)
+      .setData(query)
+      .setCustomFieldConfig('drawStyle', GraphDrawStyle.Line)
+      .build(),
+  });
+}
