cc965a3ad423ef93eafe568dee1b1334790f96b3
Galen <galen.kistler@grafana.com>
2023-09-18 15:14:26 -0500

    add milliseconds to log panel txt file download

--- public/app/features/inspector/utils/download.ts
+++ public/app/features/inspector/utils/download.ts
@@ -31,7 +31,7 @@ export function downloadLogsModelAsTxt(logsModel: Pick<LogsModel, 'meta' | 'rows
   textToDownload = textToDownload + '\n\n';
 
   logsModel.rows.forEach((row) => {
-    const newRow = dateTimeFormatISO(row.timeEpochMs) + '\t' + row.entry + '\n';
+    const newRow = dateTimeFormat(row.timeEpochMs, { defaultWithMS: true }) + '\t' + row.entry + '\n';
     textToDownload = textToDownload + newRow;
   });
 
