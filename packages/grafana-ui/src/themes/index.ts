734892185edd70ad882f1eb3df138a20a7d42027
Marcus Andersson <marcus.andersson@grafana.com>
2023-09-19 16:48:15 +0200

    moved themecontext to data

--- packages/grafana-ui/src/themes/index.ts
+++ packages/grafana-ui/src/themes/index.ts
@@ -1,13 +1,4 @@
-export {
-  ThemeContext,
-  withTheme,
-  withTheme2,
-  useTheme,
-  useTheme2,
-  useStyles,
-  useStyles2,
-  mockThemeContext,
-} from './ThemeContext';
+export { withTheme, withTheme2, useTheme, useTheme2, useStyles, useStyles2, mockThemeContext } from './ThemeContext';
 export { getTheme, mockTheme } from './getTheme';
 export { stylesFactory } from './stylesFactory';
 export { GlobalStyles } from './GlobalStyles/GlobalStyles';
