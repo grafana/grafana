78a4248e24e20ba162f1166e3f0f57fd2018955f
Andrej Ocenas <mr.ocenas@gmail.com>
2023-09-19 15:53:50 +0200

    Remove unused code

--- public/app/plugins/panel/flamegraph/suggestions.ts
+++ public/app/plugins/panel/flamegraph/suggestions.ts
@@ -1,7 +1,7 @@
 import { VisualizationSuggestionsBuilder } from '@grafana/data';
+import { checkFields } from '@grafana/flamegraph';
 import { SuggestionName } from 'app/types/suggestions';
 
-import { checkFields } from './components/FlameGraph/dataTransform';
 
 export class FlameGraphSuggestionsSupplier {
   getListWithDefaults(builder: VisualizationSuggestionsBuilder) {
