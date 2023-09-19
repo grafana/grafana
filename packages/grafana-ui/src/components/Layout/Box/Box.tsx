9d2e5ebe2848010d4787eafe0d07ca6a9e19680d
Joao Silva <100691367+JoaoSilvaGrafana@users.noreply.github.com>
2023-09-19 16:03:30 +0100

    Update packages/grafana-ui/src/components/Layout/Box/Box.tsx
    Co-authored-by: Alex Khomenko <Clarity-89@users.noreply.github.com>

--- packages/grafana-ui/src/components/Layout/Box/Box.tsx
+++ packages/grafana-ui/src/components/Layout/Box/Box.tsx
@@ -77,7 +77,7 @@ export const Box = ({ children, ...props }: React.PropsWithChildren<BoxProps>) =
 
 Box.displayName = 'Box';
 
-const customBorderColor = (color: BorderColor, theme: GrafanaTheme2): string | undefined => {
+const customBorderColor = (color: BorderColor, theme: GrafanaTheme2) => {
   switch (color) {
     case 'error':
     case 'success':
