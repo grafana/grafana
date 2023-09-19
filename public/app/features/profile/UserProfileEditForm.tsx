d56f3a84c11cd245b367206977556ddd7fe8e841
khushijain21 <khushij393@gmail.com>
2023-09-19 09:26:11 +0530

    removed gf-form-button-row

--- public/app/features/profile/UserProfileEditForm.tsx
+++ public/app/features/profile/UserProfileEditForm.tsx
@@ -31,47 +31,48 @@ export const UserProfileEditForm = ({ user, isSavingUser, updateProfile }: Props
     <Form onSubmit={onSubmitProfileUpdate} validateOn="onBlur">
       {({ register, errors }) => {
         return (
-          <FieldSet>
-            <Field
-              label={t('user-profile.fields.name-label', 'Name') + lockMessage}
-              invalid={!!errors.name}
-              error={<Trans i18nKey="user-profile.fields.name-error">Name is required</Trans>}
-              disabled={disabledEdit}
-            >
-              <Input
-                {...register('name', { required: true })}
-                id="edit-user-profile-name"
-                placeholder={t('user-profile.fields.name-label', 'Name')}
-                defaultValue={user?.name ?? ''}
-                suffix={<InputSuffix />}
-              />
-            </Field>
+          <>
+            <FieldSet>
+              <Field
+                label={t('user-profile.fields.name-label', 'Name') + lockMessage}
+                invalid={!!errors.name}
+                error={<Trans i18nKey="user-profile.fields.name-error">Name is required</Trans>}
+                disabled={disabledEdit}
+              >
+                <Input
+                  {...register('name', { required: true })}
+                  id="edit-user-profile-name"
+                  placeholder={t('user-profile.fields.name-label', 'Name')}
+                  defaultValue={user?.name ?? ''}
+                  suffix={<InputSuffix />}
+                />
+              </Field>
 
-            <Field
-              label={t('user-profile.fields.email-label', 'Email') + lockMessage}
-              invalid={!!errors.email}
-              error={<Trans i18nKey="user-profile.fields.email-error">Email is required</Trans>}
-              disabled={disabledEdit}
-            >
-              <Input
-                {...register('email', { required: true })}
-                id="edit-user-profile-email"
-                placeholder={t('user-profile.fields.email-label', 'Email')}
-                defaultValue={user?.email ?? ''}
-                suffix={<InputSuffix />}
-              />
-            </Field>
+              <Field
+                label={t('user-profile.fields.email-label', 'Email') + lockMessage}
+                invalid={!!errors.email}
+                error={<Trans i18nKey="user-profile.fields.email-error">Email is required</Trans>}
+                disabled={disabledEdit}
+              >
+                <Input
+                  {...register('email', { required: true })}
+                  id="edit-user-profile-email"
+                  placeholder={t('user-profile.fields.email-label', 'Email')}
+                  defaultValue={user?.email ?? ''}
+                  suffix={<InputSuffix />}
+                />
+              </Field>
 
-            <Field label={t('user-profile.fields.username-label', 'Username') + lockMessage} disabled={disabledEdit}>
-              <Input
-                {...register('login')}
-                id="edit-user-profile-username"
-                defaultValue={user?.login ?? ''}
-                placeholder={t('user-profile.fields.username-label', 'Username') + lockMessage}
-                suffix={<InputSuffix />}
-              />
-            </Field>
-
+              <Field label={t('user-profile.fields.username-label', 'Username') + lockMessage} disabled={disabledEdit}>
+                <Input
+                  {...register('login')}
+                  id="edit-user-profile-username"
+                  defaultValue={user?.login ?? ''}
+                  placeholder={t('user-profile.fields.username-label', 'Username') + lockMessage}
+                  suffix={<InputSuffix />}
+                />
+              </Field>
+            </FieldSet>
             <Button
               variant="primary"
               disabled={isSavingUser || disabledEdit}
@@ -80,7 +81,7 @@ export const UserProfileEditForm = ({ user, isSavingUser, updateProfile }: Props
             >
               <Trans i18nKey="common.save">Save</Trans>
             </Button>
-          </FieldSet>
+          </>
         );
       }}
     </Form>
