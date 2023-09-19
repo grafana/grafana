e11821f1f4485ff299c72c801b46daa1c5fa3b91
Ken Ng <ngken@0995.com>
2023-09-19 10:29:42 -0400

    add internationalization to flamegraph context menu

--- public/app/plugins/panel/flamegraph/components/FlameGraph/FlameGraphContextMenu.tsx
+++ public/app/plugins/panel/flamegraph/components/FlameGraph/FlameGraphContextMenu.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { MenuItem, ContextMenu } from '@grafana/ui';
 
 import { ClickedItemData } from '../types';
+import { t } from 'app/core/internationalization';
 
 type Props = {
   itemData: ClickedItemData;
@@ -16,7 +17,7 @@ const FlameGraphContextMenu = ({ itemData, onMenuItemClick, onItemFocus, onSandw
     return (
       <>
         <MenuItem
-          label="Focus block"
+          label={t('flame-graph-context-menu.focus-label','Focus block')}
           icon={'eye'}
           onClick={() => {
             onItemFocus();
@@ -24,7 +25,7 @@ const FlameGraphContextMenu = ({ itemData, onMenuItemClick, onItemFocus, onSandw
           }}
         />
         <MenuItem
-          label="Copy function name"
+          label={t('flame-graph-context-menu.copy-label','Copy function name')}
           icon={'copy'}
           onClick={() => {
             navigator.clipboard.writeText(itemData.label).then(() => {
@@ -33,7 +34,7 @@ const FlameGraphContextMenu = ({ itemData, onMenuItemClick, onItemFocus, onSandw
           }}
         />
         <MenuItem
-          label="Sandwich view"
+          label={t('flame-graph-context-menu.sandwich-label','Sandwich view')}
           icon={'gf-show-context'}
           onClick={() => {
             onSandwich();
