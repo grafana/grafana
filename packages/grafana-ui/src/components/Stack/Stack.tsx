5d4bff76df4a8583b7a1ee94a99fc34067561ea1
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-09-20 11:40:07 +0200

    Add forwardRef

--- packages/grafana-ui/src/components/Stack/Stack.tsx
+++ packages/grafana-ui/src/components/Stack/Stack.tsx
@@ -9,12 +9,16 @@ interface StackProps {
   gap?: ThemeSpacingTokens;
 }
 
-export const Stack = ({ gap = 1, direction = 'vertical', children }: React.PropsWithChildren<StackProps>) => {
-  return (
-    <Flex gap={gap} direction={direction === 'vertical' ? 'column' : 'row'} wrap="wrap">
-      {React.Children.map(children, (child) => (
-        <div>{child}</div>
-      ))}
-    </Flex>
-  );
-};
+export const Stack = React.forwardRef<HTMLDivElement, React.PropsWithChildren<StackProps>>(
+  ({ gap = 1, direction = 'vertical', children }, ref) => {
+    return (
+      <Flex ref={ref} gap={gap} direction={direction === 'vertical' ? 'column' : 'row'} wrap="wrap">
+        {React.Children.map(children, (child) => (
+          <div>{child}</div>
+        ))}
+      </Flex>
+    );
+  }
+);
+
+Stack.displayName = 'Stack';
