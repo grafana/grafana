3ed5b66dd907d5680aac59acf2a5ab39bfe38f9a
eledobleefe <laura.fernandez@grafana.com>
2023-09-22 16:56:32 +0200

    Add explanation of how the minWidthColumn works

--- packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
+++ packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
@@ -14,7 +14,9 @@ interface GridProps extends Omit<HTMLAttributes<HTMLDivElement>, 'className'> {
   /** Number of columns */
   columns?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
-  /** For a responsive layout, fit as many columns while maintaining this minimum column width */
+  /** For a responsive layout, fit as many columns while maintaining this minimum column width.
+   *  The real width will be calculated based on the theme spacing tokens: `theme.spacing(minColumnWidth)`
+   */
   minColumnWidth?: 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 44 | 55 | 72 | 89 | 144;
 }
 
