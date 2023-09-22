ed0ec78a4927fc70260fa68e1166b7b2011f30b8
suntala <arati.rana@grafana.com>
2023-09-22 14:05:02 +0200

    Change panel titles type to NVarchar temporarily

--- pkg/services/sqlstore/migrations/panel_mig.go
+++ pkg/services/sqlstore/migrations/panel_mig.go
@@ -6,7 +6,7 @@ import (
 
 func addDashboardPanelMigrations(mg *Migrator) {
 	mg.AddMigration("Add column panel_titles in dashboard", NewAddColumnMigration(DashboardV2, &Column{
-		Name: "panel_titles", Type: DB_LongText, Length: 255, Nullable: true,
+		Name: "panel_titles", Type: DB_NVarchar, Length: 255, Nullable: true,
 	}))
 
 	mg.AddMigration("Add index for dashboard_panel_titles", NewAddIndexMigration(DashboardV2, &Index{
