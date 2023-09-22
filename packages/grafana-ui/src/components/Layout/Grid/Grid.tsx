9ff2a95b089ce363621d35d8ac4e455ba7573002
eledobleefe <laura.fernandez@grafana.com>
2023-09-22 11:42:43 +0200

    Set columns and minColumnWidth props and work with them to create the grid

--- packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
+++ packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
@@ -5,27 +5,50 @@ import { GrafanaTheme2, ThemeSpacingTokens } from '@grafana/data';
 
 import { useStyles2 } from '../../../themes';
 
-export interface GridProps {
+type GridProps = GridPropsBase & (GridColumnsProps | GridMinColumnWidthProps);
+
+interface GridColumnsProps {
+  columns: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+}
+
+interface GridMinColumnWidthProps {
+  minColumnWidth: 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 55 | 89 | 144;
+}
+
+export interface GridPropsBase {
   children: NonNullable<React.ReactNode>;
-  /** Defines whether the element is a block-level or an inline-level grid */
-  display?: 'grid' | 'inline-grid';
   /** Specifies the gutters between columns and rows. It is overwritten when a column or row gap has a value */
   gap?: ThemeSpacingTokens;
 }
 
-export const Grid = ({ children, display = 'grid', gap }: GridProps) => {
-  const styles = useStyles2(getGridStyles, display, gap);
+export const Grid = (props: GridProps) => {
+  const columns = 'columns' in props ? props.columns : undefined;
+  const minColumnWidth = 'minColumnWidth' in props ? props.minColumnWidth : undefined;
+  const styles = useStyles2(getGridStyles, props.gap, columns, minColumnWidth);
 
-  return <div className={styles.grid}>{children}</div>;
+  return <div className={styles.grid}>{props.children}</div>;
 };
 
 Grid.displayName = 'Grid';
 
-const getGridStyles = (theme: GrafanaTheme2, display: 'grid' | 'inline-grid', gap?: ThemeSpacingTokens) => {
+const getGridStyles = (
+  theme: GrafanaTheme2,
+  gap?: ThemeSpacingTokens,
+  columns?: GridColumnsProps['columns'],
+  minColumnWidth?: GridMinColumnWidthProps['minColumnWidth']
+) => {
   return {
-    grid: css({
-      display,
-      gap: gap ? theme.spacing(gap) : undefined,
-    }),
+    grid: css([
+      {
+        display: 'grid',
+        gap: gap ? theme.spacing(gap) : undefined,
+      },
+      minColumnWidth && {
+        gridTemplateColumns: `repeat(auto-fill, minmax(${theme.spacing(minColumnWidth)}, 1fr))`,
+      },
+      columns && {
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+      },
+    ]),
   };
 };
