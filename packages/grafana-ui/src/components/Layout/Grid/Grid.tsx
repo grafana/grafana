b64daa7fb2cad312502118cad65c3c088bde04c0
joshhunt <josh@trtr.co>
2023-09-22 11:37:33 +0100

    examples

--- packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
+++ packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
@@ -1,41 +1,41 @@
 import { css } from '@emotion/css';
-import React from 'react';
+import React, { forwardRef, HTMLAttributes } from 'react';
 
 import { GrafanaTheme2, ThemeSpacingTokens } from '@grafana/data';
 
 import { useStyles2 } from '../../../themes';
 
-type GridProps = GridPropsBase & (GridColumnsProps | GridMinColumnWidthProps);
-
-interface GridColumnsProps {
-  columns: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-}
-
-interface GridMinColumnWidthProps {
-  minColumnWidth: 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 55 | 89 | 144;
-}
-
-export interface GridPropsBase {
+interface GridProps extends Omit<HTMLAttributes<HTMLDivElement>, 'className'> {
   children: NonNullable<React.ReactNode>;
+
   /** Specifies the gutters between columns and rows. It is overwritten when a column or row gap has a value */
   gap?: ThemeSpacingTokens;
+
+  /** Number of columns */
+  columns?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+  /** For a responsive layout, fit as many columns while maintaining this minimum column width */
+  minColumnWidth?: 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 44 | 55 | 72 | 89 | 144;
 }
 
-export const Grid = (props: GridProps) => {
-  const columns = 'columns' in props ? props.columns : undefined;
-  const minColumnWidth = 'minColumnWidth' in props ? props.minColumnWidth : undefined;
-  const styles = useStyles2(getGridStyles, props.gap, columns, minColumnWidth);
+export const Grid = forwardRef<HTMLDivElement, GridProps>((props, ref) => {
+  const { children, gap, columns, minColumnWidth, ...rest } = props;
+  const styles = useStyles2(getGridStyles, gap, columns, minColumnWidth);
 
-  return <div className={styles.grid}>{props.children}</div>;
-};
+  return (
+    <div ref={ref} {...rest} className={styles.grid}>
+      {children}
+    </div>
+  );
+});
 
 Grid.displayName = 'Grid';
 
 const getGridStyles = (
   theme: GrafanaTheme2,
-  gap?: ThemeSpacingTokens,
-  columns?: GridColumnsProps['columns'],
-  minColumnWidth?: GridMinColumnWidthProps['minColumnWidth']
+  gap: GridProps['gap'],
+  columns: GridProps['columns'],
+  minColumnWidth: GridProps['minColumnWidth']
 ) => {
   return {
     grid: css([
