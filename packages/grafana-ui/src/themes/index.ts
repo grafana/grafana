0dadde56fbeb426f84b20f48234477babf30b918
Jack Westbrook <jack.westbrook@gmail.com>
2023-09-25 09:54:21 +0200

    chore(grafana-ui): re-export ThemeContext from grafana/data for backwards compatibility

--- packages/grafana-ui/src/themes/index.ts
+++ packages/grafana-ui/src/themes/index.ts
@@ -1,3 +1,6 @@
+// @deprecated use ThemeContext from @grafana/data instead
+export { ThemeContext } from '@grafana/data';
+
 export { withTheme, withTheme2, useTheme, useTheme2, useStyles, useStyles2, mockThemeContext } from './ThemeContext';
 export { getTheme, mockTheme } from './getTheme';
 export { stylesFactory } from './stylesFactory';
