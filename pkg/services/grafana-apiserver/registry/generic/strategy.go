193c4fefb508def8831ce3423434c50cdb0fb06a
Todd Treece <todd.treece@grafana.com>
2023-09-25 18:45:00 -0400

    K8s: Add SQL and unified storage dual writer

---
+++ pkg/services/grafana-apiserver/registry/generic/strategy.go
@@ -0,0 +1,90 @@
+package generic
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/apiserver/pkg/storage"
+	"k8s.io/apiserver/pkg/storage/names"
+)
+
+type genericStrategy struct {
+	runtime.ObjectTyper
+	names.NameGenerator
+}
+
+// NewStrategy creates and returns a genericStrategy instance
+func NewStrategy(typer runtime.ObjectTyper) genericStrategy {
+	return genericStrategy{typer, names.SimpleNameGenerator}
+}
+
+func (genericStrategy) NamespaceScoped() bool {
+	return false
+}
+
+func (genericStrategy) PrepareForCreate(ctx context.Context, obj runtime.Object) {
+}
+
+func (genericStrategy) PrepareForUpdate(ctx context.Context, obj, old runtime.Object) {
+}
+
+func (genericStrategy) Validate(ctx context.Context, obj runtime.Object) field.ErrorList {
+	return field.ErrorList{}
+}
+
+// WarningsOnCreate returns warnings for the creation of the given object.
+func (genericStrategy) WarningsOnCreate(ctx context.Context, obj runtime.Object) []string { return nil }
+
+func (genericStrategy) AllowCreateOnUpdate() bool {
+	return false
+}
+
+func (genericStrategy) AllowUnconditionalUpdate() bool {
+	return false
+}
+
+func (genericStrategy) Canonicalize(obj runtime.Object) {
+}
+
+func (genericStrategy) ValidateUpdate(ctx context.Context, obj, old runtime.Object) field.ErrorList {
+	return field.ErrorList{}
+}
+
+// WarningsOnUpdate returns warnings for the given update.
+func (genericStrategy) WarningsOnUpdate(ctx context.Context, obj, old runtime.Object) []string {
+	return nil
+}
+
+func GetAttrs(obj runtime.Object) (labels.Set, fields.Set, error) {
+	accessor, err := meta.Accessor(obj)
+	if err != nil {
+		return nil, nil, err
+	}
+	return labels.Set(accessor.GetLabels()), objectMetaFieldsSet(accessor, false), nil
+}
+
+// objectMetaFieldsSet returns a fields that represent the ObjectMeta.
+func objectMetaFieldsSet(objectMeta metav1.Object, namespaceScoped bool) fields.Set {
+	if namespaceScoped {
+		return fields.Set{
+			"metadata.name":      objectMeta.GetName(),
+			"metadata.namespace": objectMeta.GetNamespace(),
+		}
+	}
+	return fields.Set{
+		"metadata.name": objectMeta.GetName(),
+	}
+}
+
+func matcher(label labels.Selector, field fields.Selector) storage.SelectionPredicate {
+	return storage.SelectionPredicate{
+		Label:    label,
+		Field:    field,
+		GetAttrs: GetAttrs,
+	}
+}
---
+++ pkg/services/grafana-apiserver/storage/dualwriter.go
@@ -0,0 +1,103 @@
+package storage
+
+import (
+	"context"
+
+	metainternalversion "k8s.io/apimachinery/pkg/apis/meta/internalversion"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/rest"
+)
+
+var _ rest.Storage = (*DualWriter)(nil)
+var _ rest.Scoper = (*DualWriter)(nil)
+var _ rest.TableConvertor = (*DualWriter)(nil)
+var _ rest.CreaterUpdater = (*DualWriter)(nil)
+var _ rest.CollectionDeleter = (*DualWriter)(nil)
+var _ rest.GracefulDeleter = (*DualWriter)(nil)
+
+type UnifiedStorage interface {
+	rest.Storage
+	rest.StandardStorage
+	rest.Scoper
+	rest.TableConvertor
+}
+
+type SQLStorage interface {
+	rest.CreaterUpdater
+	rest.CollectionDeleter
+	rest.GracefulDeleter
+}
+
+// DualWriter is a storage implementation that writes to both SQLStorage and UnifiedStorage
+type DualWriter struct {
+	UnifiedStorage
+	sql SQLStorage
+}
+
+func NewDualWriter(sql SQLStorage, unified UnifiedStorage) *DualWriter {
+	return &DualWriter{
+		UnifiedStorage: unified,
+		sql:            sql,
+	}
+}
+
+// Create overrides the default behavior of the UnifiedStorage and writes to both the SQL and UnifiedStorage
+func (d *DualWriter) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
+	_, err := d.sql.Create(ctx, obj, createValidation, options)
+	if err != nil {
+		return nil, err
+	}
+
+	obj, err = d.UnifiedStorage.Create(ctx, obj, createValidation, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
+
+// Update overrides the default behavior of the UnifiedStorage and writes to both the SQL and UnifiedStorage
+func (d *DualWriter) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc, forceAllowCreate bool, options *metav1.UpdateOptions) (runtime.Object, bool, error) {
+	_, _, err := d.sql.Update(ctx, name, objInfo, createValidation, updateValidation, forceAllowCreate, options)
+	if err != nil {
+		return nil, false, err
+	}
+
+	obj, created, err := d.UnifiedStorage.Update(ctx, name, objInfo, createValidation, updateValidation, forceAllowCreate, options)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return obj, created, nil
+}
+
+// Delete overrides the default behavior of the UnifiedStorage and writes to both the SQL and UnifiedStorage
+func (d *DualWriter) Delete(ctx context.Context, name string, deleteValidation rest.ValidateObjectFunc, options *metav1.DeleteOptions) (runtime.Object, bool, error) {
+	_, _, err := d.sql.Delete(ctx, name, deleteValidation, options)
+	if err != nil {
+		return nil, false, err
+	}
+
+	obj, deleted, err := d.UnifiedStorage.Delete(ctx, name, deleteValidation, options)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return obj, deleted, nil
+}
+
+// DeleteCollection overrides the default behavior of the UnifiedStorage and writes to both the SQL and UnifiedStorage
+func (d *DualWriter) DeleteCollection(ctx context.Context, deleteValidation rest.ValidateObjectFunc, options *metav1.DeleteOptions, listOptions *metainternalversion.ListOptions) (runtime.Object, error) {
+	_, err := d.sql.DeleteCollection(ctx, deleteValidation, options, listOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	obj, err := d.UnifiedStorage.DeleteCollection(ctx, deleteValidation, options, listOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
