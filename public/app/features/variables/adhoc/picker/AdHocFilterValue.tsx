8a9bf3a96aee39e7b94f534496ce0892c8edbd77
Isabella Siu <Isabella.siu@grafana.com>
2023-09-25 09:57:39 -0400

    OpenSearch: Add timeRange to parameters passed to getTagValues (#74952)

--- public/app/features/variables/adhoc/picker/AdHocFilterValue.tsx
+++ public/app/features/variables/adhoc/picker/AdHocFilterValue.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 import { DataSourceRef, MetricFindValue, SelectableValue } from '@grafana/data';
 import { SegmentAsync } from '@grafana/ui';
+import { getTimeSrv } from 'app/features/dashboard/services/TimeSrv';
 
 import { getDatasourceSrv } from '../../../plugins/datasource_srv';
 
@@ -45,6 +46,7 @@ const fetchFilterValues = async (datasource: DataSourceRef, key: string): Promis
     return [];
   }
 
-  const metrics = await ds.getTagValues({ key });
+  const timeRange = getTimeSrv().timeRange();
+  const metrics = await ds.getTagValues({ key, timeRange });
   return metrics.map((m: MetricFindValue) => ({ label: m.text, value: m.text }));
 };
