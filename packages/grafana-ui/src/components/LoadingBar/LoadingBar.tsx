4f5f80d15a7bc560bb946b6930760afaffe8384c
Gilles De Mey <gilles.de.mey@gmail.com>
2023-09-26 15:26:54 +0200

    adds overflow wrapper to loading bar

--- packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
+++ packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
@@ -1,29 +1,37 @@
-import { css, keyframes } from '@emotion/css';
-import React, { CSSProperties } from 'react';
+import { css, cx, keyframes } from '@emotion/css';
+import React from 'react';
 
 import { useStyles2 } from '../../themes';
 
 export interface LoadingBarProps {
   width: number;
+  delay?: number;
   ariaLabel?: string;
 }
 
+const BAR_WIDTH = 28;
 const MILLISECONDS_PER_PIXEL = 2.4;
 const MIN_DURATION_MS = 500;
 const MAX_DURATION_MS = 4000;
+const DEFAULT_ANIMATION_DELAY = 300;
+const MAX_TRANSLATE_X = (100 / BAR_WIDTH) * 100;
 
-export function LoadingBar({ width, ariaLabel = 'Loading bar' }: LoadingBarProps) {
+export function LoadingBar({ width, delay = DEFAULT_ANIMATION_DELAY, ariaLabel = 'Loading bar' }: LoadingBarProps) {
   const styles = useStyles2(getStyles);
   const durationMs = Math.min(Math.max(Math.round(width * MILLISECONDS_PER_PIXEL), MIN_DURATION_MS), MAX_DURATION_MS);
-  const containerStyles: CSSProperties = {
-    width: '100%',
-    animation: `${styles.animation} ${durationMs}ms infinite linear`,
+  const animationStyles = {
+    animationName: styles.animation,
+    // an initial delay to prevent the loader from showing if the response is faster than the delay
+    animationDelay: `${delay}ms`,
+    animationDuration: `${durationMs}ms`,
+    animationTimingFunction: 'linear',
+    animationIterationCount: 'infinite',
     willChange: 'transform',
   };
 
   return (
-    <div style={containerStyles}>
-      <div aria-label={ariaLabel} className={styles.bar} />
+    <div style={{ overflow: 'hidden' }}>
+      <div aria-label={ariaLabel} className={cx(styles.bar, css(animationStyles))} />
     </div>
   );
 }
@@ -32,16 +40,18 @@ const getStyles = () => {
   return {
     animation: keyframes({
       '0%': {
-        transform: 'translateX(-50%)',
+        transform: 'translateX(-100%)',
       },
-      '100%': {
-        transform: `translateX(100%)`,
+      // this gives us a delay between iterations
+      '85%, 100%': {
+        transform: `translateX(${MAX_TRANSLATE_X}%)`,
       },
     }),
     bar: css({
-      width: '28%',
+      width: BAR_WIDTH + '%',
       height: 1,
       background: 'linear-gradient(90deg, rgba(110, 159, 255, 0) 0%, #6E9FFF 80.75%, rgba(110, 159, 255, 0) 100%)',
+      transform: 'translateX(-100%)',
     }),
   };
 };
