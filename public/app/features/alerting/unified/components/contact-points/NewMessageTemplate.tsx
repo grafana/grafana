5a1580c65966ca3f8e28483c1e197b4594ed5855
Gilles De Mey <gilles.de.mey@gmail.com>
2023-09-26 10:44:18 +0200

    Alerting: Contact points v2 part 3 (#72444)

---
+++ public/app/features/alerting/unified/components/contact-points/NewMessageTemplate.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Alert } from '@grafana/ui';
+
+import { useAlertmanagerConfig } from '../../hooks/useAlertmanagerConfig';
+import { useAlertmanager } from '../../state/AlertmanagerContext';
+import { NewTemplateView } from '../receivers/NewTemplateView';
+
+const NewMessageTemplate = () => {
+  const { selectedAlertmanager } = useAlertmanager();
+  const { data, isLoading, error } = useAlertmanagerConfig(selectedAlertmanager);
+
+  if (isLoading && !data) {
+    return 'loading...';
+  }
+
+  if (error) {
+    return (
+      <Alert severity="error" title="Failed to fetch message template">
+        {String(error)}
+      </Alert>
+    );
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return <NewTemplateView alertManagerSourceName={selectedAlertmanager!} config={data} />;
+};
+
+export default NewMessageTemplate;
