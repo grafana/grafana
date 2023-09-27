af8a46adbbcccf0a9a119db9d7dba592f1b6bbe4
Andres Martinez Gotor <andres.martinez@grafana.com>
2023-09-27 09:44:43 +0200

    Bug Fix: Restore health check error (#75508)

--- pkg/plugins/errors.go
+++ pkg/plugins/errors.go
@@ -18,6 +18,12 @@ var (
 	// ErrMethodNotImplemented error returned when a plugin method is not implemented.
 	ErrMethodNotImplemented = errMethodNotImplementedBase.Errorf("method not implemented")
 
+	// ErrPluginHealthCheck error returned when a plugin fails its health check.
+	// Exposed as a base error to wrap it with plugin error.
+	ErrPluginHealthCheck = errutil.Internal("plugin.healthCheck",
+		errutil.WithPublicMessage("Plugin health check failed"),
+		errutil.WithDownstream())
+
 	// ErrPluginDownstreamError error returned when a plugin request fails.
 	// Exposed as a base error to wrap it with plugin downstream errors.
 	ErrPluginDownstreamErrorBase = errutil.Internal("plugin.downstreamError",
