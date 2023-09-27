476d581e09ba5ac53166b9be2fff976f69f65b14
Joey Tawadrous <joey.tawadrous@grafana.com>
2023-09-27 08:58:37 +0100

    Fix issue with flame graph

--- packages/grafana-flamegraph/src/FlameGraph/dataTransform.ts
+++ packages/grafana-flamegraph/src/FlameGraph/dataTransform.ts
@@ -67,7 +67,9 @@ export function nestedSetToLevels(container: FlameGraphDataContainer): [LevelIte
       children: [],
     };
 
-    if (uniqueLabels[container.getLabel(i)]) {
+    // if the label value is something like 'toString' then checking its position in the array will return 'ƒ toString() { [native code] }'
+    // which results in a TypeError when we try to push to it. So we also have to check if the array already has at least one value
+    if (uniqueLabels[container.getLabel(i)] && uniqueLabels[container.getLabel(i)].length > 0) {
       uniqueLabels[container.getLabel(i)].push(newItem);
     } else {
       uniqueLabels[container.getLabel(i)] = [newItem];
