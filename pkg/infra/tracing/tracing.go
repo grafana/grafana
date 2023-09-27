da3ee948ce78b72083ec760a123331374f777965
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2023-09-27 14:05:52 +0200

    deps

--- pkg/infra/tracing/tracing.go
+++ pkg/infra/tracing/tracing.go
@@ -21,7 +21,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	trace "go.opentelemetry.io/otel/trace"
 
 	"github.com/go-kit/log/level"
