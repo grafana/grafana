ca30c81bc8e77f056184fafee86ef63bcfee4916
Aaron Sanders <aaron.sanders@grafana.com>
2023-09-27 10:59:00 -0500

    Fix formatting to pass prettier check

--- public/app/features/dashboard/components/GenAI/GenAIDashDescriptionButton.tsx
+++ public/app/features/dashboard/components/GenAI/GenAIDashDescriptionButton.tsx
@@ -12,13 +12,12 @@ interface GenAIDashDescriptionButtonProps {
 }
 
 const DESCRIPTION_GENERATION_STANDARD_PROMPT =
-'You are an expert in creating Grafana Dashboards.\n' +
-'Your goal is to write a descriptive and concise dashboard description.\n' +
-'You will be given the title and description of the dashboard\'s panels.\n' +
-'The dashboard description is meant to explain the purpose of the dashboard and what it shows.\n' +
-'There should be no numbers in the description except where they are important.\n' +
-'The description should be, at most, 140 characters.';
-
+  'You are an expert in creating Grafana Dashboards.\n' +
+  'Your goal is to write a descriptive and concise dashboard description.\n' +
+  "You will be given the title and description of the dashboard's panels.\n" +
+  'The dashboard description is meant to explain the purpose of the dashboard and what it shows.\n' +
+  'There should be no numbers in the description except where they are important.\n' +
+  'The description should be, at most, 140 characters.';
 
 export const GenAIDashDescriptionButton = ({ onGenerate, dashboard }: GenAIDashDescriptionButtonProps) => {
   const messages = React.useMemo(() => getMessages(dashboard), [dashboard]);
@@ -36,14 +35,14 @@ function getMessages(dashboard: DashboardModel): Message[] {
       - Title: ${panel.title}\n
       ${panel.description ? `- Description: ${panel.description}` : ''}
       `
-  )
+  );
   let panelString: string;
   if (panels.length <= 10) {
     panelString = `The the panels in the dashboard are:\n${panels.join('\n')}`;
   } else {
     panelString = `There are ${panels.length} panels.\n
     Due to space constraints, only the information for the first ten are how is presented.\n
-    The the panels in the dashboard are:\n${panels.slice(10).join('\n')}`
+    The the panels in the dashboard are:\n${panels.slice(10).join('\n')}`;
   } // This truncation should prevent exceeding the allowed size for GPT calls.
   // Additionally, context windows that are too long degrade performance,
   // So it is possibly that if we can condense it further it would be better
