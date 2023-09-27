fb3baeacb51dd03da32b99961c127c318bd6976e
Aaron Sanders <aaron.sanders@grafana.com>
2023-09-27 14:10:36 -0500

    Further changes to prompts for dash title/description Factor prompt construction for panel information into utils

--- public/app/features/dashboard/components/GenAI/GenAIDashDescriptionButton.tsx
+++ public/app/features/dashboard/components/GenAI/GenAIDashDescriptionButton.tsx
@@ -4,7 +4,7 @@ import { DashboardModel } from '../../state';
 
 import { GenAIButton } from './GenAIButton';
 import { EventSource, reportGenerateAIButtonClicked } from './tracking';
-import { Message, Role } from './utils';
+import { getDashboardPanelPrompt, Message, Role } from './utils';
 
 interface GenAIDashDescriptionButtonProps {
   onGenerate: (description: string) => void;
@@ -15,9 +15,12 @@ const DESCRIPTION_GENERATION_STANDARD_PROMPT =
   'You are an expert in creating Grafana Dashboards.\n' +
   'Your goal is to write a descriptive and concise dashboard description.\n' +
   "You will be given the title and description of the dashboard's panels.\n" +
-  'The dashboard description is meant to explain the purpose of the dashboard and what it shows.\n' +
+  'The dashboard description is meant to explain the purpose of the dashboard and what its panels show.\n' +
+  'If the dashboard has no panels, the description should be "Empty dashboard"\n' +
   'There should be no numbers in the description except where they are important.\n' +
-  'The description should be, at most, 140 characters.';
+  'The dashboard description should not have the dashboard title or any quotation marks in it.\n' +
+  'The description should be, at most, 140 characters.\n' +
+  'Respond with only the description of the dashboard.';
 
 export const GenAIDashDescriptionButton = ({ onGenerate, dashboard }: GenAIDashDescriptionButtonProps) => {
   const messages = React.useMemo(() => getMessages(dashboard), [dashboard]);
@@ -29,23 +32,7 @@ export const GenAIDashDescriptionButton = ({ onGenerate, dashboard }: GenAIDashD
 };
 
 function getMessages(dashboard: DashboardModel): Message[] {
-  const panels: string[] = dashboard.panels.map(
-    (panel, idx) => `
-      - Panel ${idx}\n
-      - Title: ${panel.title}\n
-      ${panel.description ? `- Description: ${panel.description}` : ''}
-      `
-  );
-  let panelString: string;
-  if (panels.length <= 10) {
-    panelString = `The the panels in the dashboard are:\n${panels.join('\n')}`;
-  } else {
-    panelString = `There are ${panels.length} panels.\n
-    Due to space constraints, only the information for the first ten are how is presented.\n
-    The the panels in the dashboard are:\n${panels.slice(10).join('\n')}`;
-  } // This truncation should prevent exceeding the allowed size for GPT calls.
-  // Additionally, context windows that are too long degrade performance,
-  // So it is possibly that if we can condense it further it would be better
+  const panelPrompt = getDashboardPanelPrompt(dashboard);
 
   return [
     {
@@ -53,8 +40,7 @@ function getMessages(dashboard: DashboardModel): Message[] {
       role: Role.system,
     },
     {
-      content: `The title of the dashboard is "${dashboard.title}"\n
-      ${panelString}`,
+      content: `The title of the dashboard is "${dashboard.title}"\n` + `${panelPrompt}`,
       role: Role.system,
     },
   ];
--- public/app/features/dashboard/components/GenAI/utils.ts
+++ public/app/features/dashboard/components/GenAI/utils.ts
@@ -1,4 +1,4 @@
-import { DashboardModel } from '../../state';
+import { DashboardModel, PanelModel } from '../../state';
 import { Diffs, jsonDiff } from '../VersionHistory/utils';
 
 import { openai } from './llms';
@@ -50,4 +50,46 @@ export async function isLLMPluginEnabled() {
   // Check if the LLM plugin is enabled and configured.
   // If not, we won't be able to make requests, so return early.
   return await openai.enabled();
+}
+
+/**
+ *
+ * @param dashboard Dashboard to generate a title or description for
+ * @returns String for inclusion in prompts stating what the dashboard's panels are
+ */
+export function getDashboardPanelPrompt(dashboard: DashboardModel): string {
+  const getPanelString = (panel: PanelModel, idx: number) => `
+  - Panel ${idx}\n
+  - Title: ${panel.title}\n
+  ${panel.description ? `- Description: ${panel.description}` : ''}
+  `;
+
+  const panelStrings: string[] = dashboard.panels.map(getPanelString);
+  let panelPrompt: string;
+
+  if (panelStrings.length <= 10) {
+    panelPrompt = `The panels in the dashboard are:\n${panelStrings.join('\n')}`;
+  } else {
+    const withDescriptions = dashboard.panels.filter((panel) => panel.description);
+    const withoutDescriptions = dashboard.panels.filter((panel) => !panel.description);
+    let concatenatedPanelStrings;
+    if (withDescriptions.length >= 10) {
+      concatenatedPanelStrings = withDescriptions.slice(10).map(getPanelString).join('\n');
+    } else {
+      concatenatedPanelStrings = withDescriptions.map(getPanelString).join('\n');
+      concatenatedPanelStrings += '\n';
+      concatenatedPanelStrings += withoutDescriptions
+        .slice(10 - withDescriptions.length)
+        .map(getPanelString)
+        .join('n');
+    }
+    panelPrompt =
+      `There are ${panelStrings.length} panels.\n` +
+      'Due to space constraints, only the information for ten of them is presented.\n' +
+      'These ten are not necessarily the first ten, but prioritized to those with descriptions.\n' +
+      `The panels in the dashboard are:\n${concatenatedPanelStrings}`;
+  } // This truncation should prevent exceeding the allowed size for GPT calls.
+  // Additionally, context windows that are too long degrade performance,
+  // So it is possibly that if we can condense it further it would be better
+  return panelPrompt;
 }
