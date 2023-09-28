77b3e589c619c71c335864cea67b134d0a40dd24
Todd Treece <todd.treece@grafana.com>
2023-09-28 18:36:18 -0400

    add access control to list of authorizers

--- pkg/services/grafana-apiserver/auth/authorizer/provider.go
+++ pkg/services/grafana-apiserver/auth/authorizer/provider.go
@@ -6,15 +6,18 @@ import (
 	"k8s.io/apiserver/pkg/authorization/authorizerfactory"
 	"k8s.io/apiserver/pkg/authorization/union"
 
+	"github.com/grafana/grafana/pkg/services/grafana-apiserver/auth/authorizer/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/grafana-apiserver/auth/authorizer/org"
 )
 
 func ProvideAuthorizer(
 	orgIDAuthorizer *org.OrgIDAuthorizer,
 	orgRoleAuthorizer *org.OrgRoleAuthorizer,
+	acAuthorizr *accesscontrol.AccessControlAuthorizer,
 ) authorizer.Authorizer {
 	authorizers := []authorizer.Authorizer{
 		authorizerfactory.NewPrivilegedGroups(user.SystemPrivilegedGroup),
+		acAuthorizr,
 		orgIDAuthorizer,
 		orgRoleAuthorizer,
 	}
--- pkg/services/grafana-apiserver/auth/authorizer/wireset.go
+++ pkg/services/grafana-apiserver/auth/authorizer/wireset.go
@@ -3,10 +3,12 @@ package authorizer
 import (
 	"github.com/google/wire"
 
+	"github.com/grafana/grafana/pkg/services/grafana-apiserver/auth/authorizer/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/grafana-apiserver/auth/authorizer/org"
 )
 
 var WireSet = wire.NewSet(
 	org.WireSet,
+	accesscontrol.WireSet,
 	ProvideAuthorizer,
 )
