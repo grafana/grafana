815962ac4de951cc044f9dc905cf1c34e2d90f6c
Gabriel MABILLE <gabriel.mabille@grafana.com>
2023-09-28 15:22:31 +0200

    Add test to double check the filters or correctly OrderBy

--- pkg/services/team/sortopts/sortopts_test.go
+++ pkg/services/team/sortopts/sortopts_test.go
@@ -6,6 +6,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestSorter_Filters(t *testing.T) {
+	require.Equal(t, SortOptionsByQueryParam["name-asc"].Filter[0].OrderBy(), "LOWER(team.name) ASC")
+	require.Equal(t, SortOptionsByQueryParam["name-desc"].Filter[0].OrderBy(), "LOWER(team.name) DESC")
+	require.Equal(t, SortOptionsByQueryParam["email-asc"].Filter[0].OrderBy(), "team.email ASC")
+	require.Equal(t, SortOptionsByQueryParam["email-desc"].Filter[0].OrderBy(), "team.email DESC")
+	require.Equal(t, SortOptionsByQueryParam["memberCount-asc"].Filter[0].OrderBy(), "member_count ASC")
+	require.Equal(t, SortOptionsByQueryParam["memberCount-desc"].Filter[0].OrderBy(), "member_count DESC")
+}
+
 func TestSorter_OrderBy(t *testing.T) {
 	type fields struct {
 		Field         string
