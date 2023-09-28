acf3b4fac2792f7a2fb688b808bffdb6e399b974
Ivana Huckova <ivana.huckova@gmail.com>
2023-09-28 11:44:33 +0200

    Rename and add exemplars

--- pkg/tsdb/loki/instrumentation/instrumentation.go
+++ pkg/tsdb/loki/instrumentation/instrumentation.go
@@ -1,18 +1,20 @@
 package instrumentation
 
 import (
+	"context"
 	"time"
 
+	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 var (
-	pluginResponseParsingDurationSeconds = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	pluginParsingResponseDurationSeconds = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: "grafana",
-		Name:      "loki_plugin_response_parse_duration_seconds",
+		Name:      "loki_plugin_parse_response_duration_seconds",
 		Help:      "Duration of Loki parsing the response in seconds",
-		Buckets:   []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10, 25},
+		Buckets:   []float64{.001, 0.0025, .005, .0075, .01, .02, .03, .04, .05, .075, .1, .25, .5, 1, 5, 10, 25},
 	}, []string{"status", "endpoint"})
 )
 
@@ -20,6 +22,13 @@ const (
 	EndpointQueryData = "queryData"
 )
 
-func UpdatePluginResponseParsingDurationSeconds(duration time.Duration, status string) {
-	pluginResponseParsingDurationSeconds.WithLabelValues(status, EndpointQueryData).Observe(duration.Seconds())
+func UpdatePluginParsingResponseDurationSeconds(ctx context.Context, duration time.Duration, status string) {
+	histogram := pluginParsingResponseDurationSeconds.WithLabelValues(status, EndpointQueryData)
+
+	if traceID := tracing.TraceIDFromContext(ctx, true); traceID != "" {
+		histogram.(prometheus.ExemplarObserver).ObserveWithExemplar(duration.Seconds(), prometheus.Labels{"traceID": traceID})
+	} else {
+		histogram.Observe(duration.Seconds())
+	}
+
 }
