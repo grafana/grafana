4b07404aaf87be029e13af08d57573f56ed4ae04
Ashley Harrison <ashley.harrison@grafana.com>
2023-09-28 15:30:34 +0100

    use Trans instead of Text

--- public/app/features/dashboard/components/DashboardSettings/JsonEditorSettings.tsx
+++ public/app/features/dashboard/components/DashboardSettings/JsonEditorSettings.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { config } from '@grafana/runtime';
-import { Button, CodeEditor, useStyles2, Text } from '@grafana/ui';
+import { Button, CodeEditor, useStyles2 } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 import { Trans } from 'app/core/internationalization';
 import { dashboardWatcher } from 'app/features/live/dashboard/dashboardWatcher';
@@ -26,12 +26,10 @@ export function JsonEditorSettings({ dashboard, sectionNav }: SettingsPageProps)
   return (
     <Page navModel={sectionNav} pageNav={pageNav}>
       <div className={styles.wrapper}>
-        <Text>
-          <Trans i18nKey="dashboard-settings.json-editor.subtitle">
-            The JSON model below is the data structure that defines the dashboard. This includes dashboard settings,
-            panel settings, layout, queries, and so on.
-          </Trans>
-        </Text>
+        <Trans i18nKey="dashboard-settings.json-editor.subtitle">
+          The JSON model below is the data structure that defines the dashboard. This includes dashboard settings, panel
+          settings, layout, queries, and so on.
+        </Trans>
         <CodeEditor
           value={dashboardJson}
           language="json"
