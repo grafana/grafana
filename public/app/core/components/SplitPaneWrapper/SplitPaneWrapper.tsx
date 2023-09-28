9f31be9071bf1243072b318d661dd4bc006599c4
Leon Sorokin <leeoniya@gmail.com>
2023-09-28 11:15:53 -0500

    different approach

--- public/app/core/components/SplitPaneWrapper/SplitPaneWrapper.tsx
+++ public/app/core/components/SplitPaneWrapper/SplitPaneWrapper.tsx
@@ -87,6 +87,7 @@ export class SplitPaneWrapper extends PureComponent<React.PropsWithChildren<Prop
 
     return (
       <SplitPane
+        className={styles.splitPane}
         split={splitOrientation}
         minSize={minSize}
         maxSize={maxSize}
@@ -141,6 +142,9 @@ const getStyles = (theme: GrafanaTheme2, hasSplit: boolean) => {
   `;
 
   return {
+    splitPane: css`
+      overflow: visible !important;
+    `,
     resizerV: cx(
       resizer,
       css`
