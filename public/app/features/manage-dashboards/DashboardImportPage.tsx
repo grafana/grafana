e33bd09773056e6658a21a738d7af5eed1ef5f3b
otilor <gabrielfemi799@gmail.com>
2023-09-29 16:18:18 +0100

    Update public/app/features/manage-dashboards/DashboardImportPage.tsx
    Co-authored-by: Ashley Harrison <ashharrison90@gmail.com>

--- public/app/features/manage-dashboards/DashboardImportPage.tsx
+++ public/app/features/manage-dashboards/DashboardImportPage.tsx
@@ -197,7 +197,7 @@ class UnthemedDashboardImport extends PureComponent<Props> {
                     <Trans i18nKey="dashboard-import.form-actions.load">Load</Trans>
                   </Button>
                   <LinkButton variant="secondary" href={`${config.appSubUrl}/dashboards`}>
-                    <Trans i18nKey="dashboard-import-page.cancel">Cancel</Trans>
+                    <Trans i18nKey="dashboard-import.form-actions.cancel">Cancel</Trans>
                   </LinkButton>
                 </HorizontalGroup>
               </>
