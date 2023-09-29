7906c1c17d4ebf2ead4fed979e33cec447f6bcea
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-09-29 09:32:05 +0200

    PMM-12251 Service acc.

---
+++ pkg/api/dtos/service_token.go
@@ -0,0 +1,59 @@
+package dtos
+
+import (
+	"time"
+
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/org"
+)
+
+// swagger:model
+type NewServiceTokenResult struct {
+	// example: 1
+	ID int64 `json:"id"`
+	// example: grafana
+	Name string `json:"name"`
+	// example: glsa_yscW25imSKJIuav8zF37RZmnbiDvB05G_fcaaf58a
+	Key string `json:"key"`
+}
+
+type ServiceTokenDTO struct {
+	Id            int64                  `json:"id"`
+	Name          string                 `json:"name"`
+	Role          org.RoleType           `json:"role"`
+	Expiration    *time.Time             `json:"expiration,omitempty"`
+	AccessControl accesscontrol.Metadata `json:"accessControl,omitempty"`
+}
+
+// @PERCONA
+type ServiceToken struct {
+	Id               int64
+	OrgId            int64
+	Name             string
+	Key              string
+	Role             org.RoleType
+	Created          time.Time
+	Updated          time.Time
+	LastUsedAt       *time.Time `xorm:"last_used_at"`
+	Expires          *int64
+	ServiceAccountId *int64
+}
+
+type GetServiceTokenByIdQuery struct {
+	ServiceTokenId int64
+	Result         *ServiceToken
+}
+
+type ServiceTokenDetailsDTO struct {
+	Id         int64        `json:"id"`
+	OrgId      int64        `json:"orgId,omitempty"`
+	Name       string       `json:"name"`
+	Role       org.RoleType `json:"role"`
+	Expiration *time.Time   `json:"expiration,omitempty"`
+}
+
+type GetServiceTokenByNameQuery struct {
+	KeyName string
+	OrgId   int64
+	Result  *ServiceToken
+}
