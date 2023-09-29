72b2103d5605dd95c5702971bf37b66f4b9bd49b
harisrozajac <haris.rozajac12@gmail.com>
2023-09-28 22:08:34 -0600

    Fix vertical and horizontal scrolling

--- public/app/features/explore/ContentOutline/ContentOutlineItem.tsx
+++ public/app/features/explore/ContentOutline/ContentOutlineItem.tsx
@@ -1,3 +1,4 @@
+import { cx, css } from '@emotion/css';
 import React, { useEffect, useRef, ReactNode } from 'react';
 
 import { useContentOutlineContext } from './ContentOutlineContext';
@@ -12,9 +13,21 @@ interface ContentOutlineItemProps extends ContentOutlineItemBaseProps {
   className?: string;
 }
 
+const baseStyle = css({
+  // acounts for the height of the sticky ExploreToolbar
+  scrollMarginTop: '60px',
+  /** when the pane is narrow enough to have horizontal scroll, this prevents
+   * the pane from scrolling horizontally and pushing the contentoutline off screen
+   * */
+  scrollMarginLeft: '70px',
+});
+
 function ContentOutlineItem({ title, icon, children, className }: ContentOutlineItemProps) {
   const { register, unregister } = useContentOutlineContext();
   const ref = useRef(null);
+  const styles = cx(baseStyle, className);
+
+  // const styles = cx(baseStyle, className);
 
   useEffect(() => {
     // When the component mounts, register it and get its unique ID.
@@ -25,7 +38,7 @@ function ContentOutlineItem({ title, icon, children, className }: ContentOutline
   }, [title, icon, register, unregister]);
 
   return (
-    <div className={className} ref={ref}>
+    <div className={styles} ref={ref}>
       {children}
     </div>
   );
