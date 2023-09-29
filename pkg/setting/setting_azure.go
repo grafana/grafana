c2a9a5f8359c60837ec8948b45e5bf10b2bb13bc
Andreas Christou <andreas.christou@grafana.com>
2023-09-29 10:06:03 +0100

    [v9.5.x] Azure: Settings for Azure AD Workload Identity (#75690)
    * Backport workload identity changes
    
    * Tidy go.mod
    
    * Add missing flag
    
    (cherry picked from commit b52b829ece0d99baebcdf2e67f177894d77334b3)

--- pkg/setting/setting_azure.go
+++ pkg/setting/setting_azure.go
@@ -15,5 +15,23 @@ func (cfg *Cfg) readAzureSettings() {
 	azureSettings.ManagedIdentityEnabled = azureSection.Key("managed_identity_enabled").MustBool(false)
 	azureSettings.ManagedIdentityClientId = azureSection.Key("managed_identity_client_id").String()
 
+	// Workload Identity authentication
+	if azureSection.Key("workload_identity_enabled").MustBool(false) {
+		azureSettings.WorkloadIdentityEnabled = true
+		workloadIdentitySettings := &azsettings.WorkloadIdentitySettings{}
+
+		if val := azureSection.Key("workload_identity_tenant_id").String(); val != "" {
+			workloadIdentitySettings.TenantId = val
+		}
+		if val := azureSection.Key("workload_identity_client_id").String(); val != "" {
+			workloadIdentitySettings.ClientId = val
+		}
+		if val := azureSection.Key("workload_identity_token_file").String(); val != "" {
+			workloadIdentitySettings.TokenFile = val
+		}
+
+		azureSettings.WorkloadIdentitySettings = workloadIdentitySettings
+	}
+
 	cfg.Azure = azureSettings
 }
