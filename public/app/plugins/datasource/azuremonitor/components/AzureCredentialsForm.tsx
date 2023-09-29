161ecb36e3988bec71aed672fb3f68b57d9722c5
Andreas Christou <andreas.christou@grafana.com>
2023-09-29 11:50:06 +0100

    [v9.5.x] Azure: Add support for Workload Identity authentication (#75730)
    * Azure: Add support for Workload Identity authentication (#75681)
    
    * Update Azure Monitor
    
    * Update Prometheus
    
    * Update README
    
    * Update docs/sources/datasources/azure-monitor/_index.md
    
    Co-authored-by: Andrew Hackmann <5140848+bossinc@users.noreply.github.com>
    
    * Update docs/sources/datasources/azure-monitor/_index.md
    
    Co-authored-by: Beverly <131809838+BeverlyJaneJ@users.noreply.github.com>
    
    * Update docs/sources/datasources/azure-monitor/_index.md
    
    Co-authored-by: Beverly <131809838+BeverlyJaneJ@users.noreply.github.com>
    
    * Update docs/sources/datasources/azure-monitor/_index.md
    
    Co-authored-by: Beverly <131809838+BeverlyJaneJ@users.noreply.github.com>
    
    * README updates
    
    * Fix prettier
    
    * memoize options
    
    ---------
    
    Co-authored-by: Andrew Hackmann <5140848+bossinc@users.noreply.github.com>
    Co-authored-by: Beverly <131809838+BeverlyJaneJ@users.noreply.github.com>
    (cherry picked from commit 5796836662198ef09375c87f83ebf298f1c595b8)
    
    # Conflicts:
    #	public/app/plugins/datasource/azuremonitor/components/AzureCredentialsForm.tsx
    #	public/app/plugins/datasource/prometheus/configuration/AzureCredentialsForm.tsx
    
    * Fix types

--- public/app/plugins/datasource/azuremonitor/components/AzureCredentialsForm.tsx
+++ public/app/plugins/datasource/azuremonitor/components/AzureCredentialsForm.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useMemo } from 'react';
 
 import { SelectableValue } from '@grafana/data';
 import { LegacyForms, Button, Select, InlineField } from '@grafana/ui';
@@ -10,41 +10,66 @@ const { Input } = LegacyForms;
 
 export interface Props {
   managedIdentityEnabled: boolean;
+  workloadIdentityEnabled: boolean;
   credentials: AzureCredentials;
   azureCloudOptions?: SelectableValue[];
-  onCredentialsChange?: (updatedCredentials: AzureCredentials) => void;
+  onCredentialsChange: (updatedCredentials: AzureCredentials) => void;
   disabled?: boolean;
   children?: JSX.Element;
 }
 
-const authTypeOptions: Array<SelectableValue<AzureAuthType>> = [
-  {
-    value: 'msi',
-    label: 'Managed Identity',
-  },
-  {
-    value: 'clientsecret',
-    label: 'App Registration',
-  },
-];
-
 const LABEL_WIDTH = 18;
 
 export const AzureCredentialsForm = (props: Props) => {
-  const { credentials, azureCloudOptions, onCredentialsChange, disabled, managedIdentityEnabled } = props;
+  const {
+    credentials,
+    azureCloudOptions,
+    onCredentialsChange,
+    disabled,
+    managedIdentityEnabled,
+    workloadIdentityEnabled,
+  } = props;
+
+  const authTypeOptions = useMemo(() => {
+    let opts: Array<SelectableValue<AzureAuthType>> = [
+      {
+        value: 'clientsecret',
+        label: 'App Registration',
+      },
+    ];
+
+    if (managedIdentityEnabled) {
+      opts.push({
+        value: 'msi',
+        label: 'Managed Identity',
+      });
+    }
+
+    if (workloadIdentityEnabled) {
+      opts.push({
+        value: 'workloadidentity',
+        label: 'Workload Identity',
+      });
+    }
+
+    return opts;
+  }, [managedIdentityEnabled, workloadIdentityEnabled]);
 
   const onAuthTypeChange = (selected: SelectableValue<AzureAuthType>) => {
-    if (onCredentialsChange) {
-      const updated: AzureCredentials = {
-        ...credentials,
-        authType: selected.value || 'msi',
-      };
-      onCredentialsChange(updated);
-    }
+    const defaultAuthType = managedIdentityEnabled
+      ? 'msi'
+      : workloadIdentityEnabled
+      ? 'workloadidentity'
+      : 'clientsecret';
+    const updated: AzureCredentials = {
+      ...credentials,
+      authType: selected.value || defaultAuthType,
+    };
+    onCredentialsChange(updated);
   };
 
   const onAzureCloudChange = (selected: SelectableValue<string>) => {
-    if (onCredentialsChange && credentials.authType === 'clientsecret') {
+    if (credentials.authType === 'clientsecret') {
       const updated: AzureCredentials = {
         ...credentials,
         azureCloud: selected.value,
@@ -54,7 +79,7 @@ export const AzureCredentialsForm = (props: Props) => {
   };
 
   const onTenantIdChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (onCredentialsChange && credentials.authType === 'clientsecret') {
+    if (credentials.authType === 'clientsecret') {
       const updated: AzureCredentials = {
         ...credentials,
         tenantId: event.target.value,
@@ -64,7 +89,7 @@ export const AzureCredentialsForm = (props: Props) => {
   };
 
   const onClientIdChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (onCredentialsChange && credentials.authType === 'clientsecret') {
+    if (credentials.authType === 'clientsecret') {
       const updated: AzureCredentials = {
         ...credentials,
         clientId: event.target.value,
@@ -74,7 +99,7 @@ export const AzureCredentialsForm = (props: Props) => {
   };
 
   const onClientSecretChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (onCredentialsChange && credentials.authType === 'clientsecret') {
+    if (credentials.authType === 'clientsecret') {
       const updated: AzureCredentials = {
         ...credentials,
         clientSecret: event.target.value,
@@ -84,7 +109,7 @@ export const AzureCredentialsForm = (props: Props) => {
   };
 
   const onClientSecretReset = () => {
-    if (onCredentialsChange && credentials.authType === 'clientsecret') {
+    if (credentials.authType === 'clientsecret') {
       const updated: AzureCredentials = {
         ...credentials,
         clientSecret: '',
@@ -95,7 +120,7 @@ export const AzureCredentialsForm = (props: Props) => {
 
   return (
     <div className="gf-form-group">
-      {managedIdentityEnabled && (
+      {authTypeOptions.length > 1 && (
         <InlineField
           label="Authentication"
           labelWidth={LABEL_WIDTH}
