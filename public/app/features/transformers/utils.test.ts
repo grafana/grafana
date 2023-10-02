a61acb4ca15e44e123ffbec805dc235bc897c2de
Oscar Kilhed <oscar.kilhed@grafana.com>
2023-10-02 10:12:50 +0200

    Add more tests for numberOrVariableValidator

--- public/app/features/transformers/utils.test.ts
+++ public/app/features/transformers/utils.test.ts
@@ -3,16 +3,44 @@ import { config } from '@grafana/runtime';
 import { numberOrVariableValidator } from './utils';
 
 describe('validator', () => {
-  it('validates a number', () => {
+  it('validates a positive number', () => {
     expect(numberOrVariableValidator(1)).toBe(true);
   });
 
-  it('validates a string that is an integer', () => {
+  it('validates a negative number', () => {
+    expect(numberOrVariableValidator(-1)).toBe(true);
+  });
+
+  it('validates zero', () => {
+    expect(numberOrVariableValidator(0)).toBe(true);
+  });
+
+  it('validates a float', () => {
+    expect(numberOrVariableValidator(1.2)).toBe(true);
+  });
+
+  it('validates a negative float', () => {
+    expect(numberOrVariableValidator(1.2)).toBe(true);
+  });
+
+  it('validates a string that is a positive integer', () => {
     expect(numberOrVariableValidator('1')).toBe(true);
   });
 
-  it('validats a string that is not a float', () => {
+  it('validats a string that is a negative integer', () => {
+    expect(numberOrVariableValidator('-1')).toBe(true);
+  });
+
+  it('validats a string that is zero', () => {
+    expect(numberOrVariableValidator('0')).toBe(true);
+  });
+
+  it('validats a string that is a float', () => {
     expect(numberOrVariableValidator('1.2')).toBe(true);
+  });
+
+  it('validats a string that is a negative float', () => {
+    expect(numberOrVariableValidator('-1.2')).toBe(true);
   });
 
   it('fails a string that is not a number', () => {
@@ -28,5 +56,10 @@ describe('validator', () => {
     config.featureToggles.transformationsVariableSupport = false;
     expect(numberOrVariableValidator('$foo')).toBe(false);
     config.featureToggles.transformationsVariableSupport = true;
+  });
+  it('fails a string that has multiple variables', () => {
+    config.featureToggles.transformationsVariableSupport = true;
+    expect(numberOrVariableValidator('$foo$asd')).toBe(false);
+    config.featureToggles.transformationsVariableSupport = false;
   });
 });
