f00bbe41944e8a6ad18629ec599cf88d9d8e2d94
otilor <gabrielfemi799@gmail.com>
2023-10-03 11:38:30 +0100

    Update public/app/features/dashboard/components/DeleteDashboard/DeleteDashboardButton.tsx
    Co-authored-by: Ashley Harrison <ashharrison90@gmail.com>

--- public/app/features/dashboard/components/DeleteDashboard/DeleteDashboardButton.tsx
+++ public/app/features/dashboard/components/DeleteDashboard/DeleteDashboardButton.tsx
@@ -24,7 +24,7 @@ export const DeleteDashboardButton = ({ dashboard }: Props) => (
         }}
         aria-label="Dashboard settings page delete dashboard button"
       >
-        <Trans i18nKey="dashboard-delete-button">Delete Dashboard</Trans>
+        <Trans i18nKey="dashboard-settings.dashboard-delete-button">Delete Dashboard</Trans>
       </Button>
     )}
   </ModalsController>
