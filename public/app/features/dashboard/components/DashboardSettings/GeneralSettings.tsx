784453dbd9533f9b1fcd08775d2744a524b9b962
otilor <gabrielfemi799@gmail.com>
2023-10-03 11:41:59 +0100

    Update public/app/features/dashboard/components/DashboardSettings/GeneralSettings.tsx
    Co-authored-by: Ashley Harrison <ashharrison90@gmail.com>

--- public/app/features/dashboard/components/DashboardSettings/GeneralSettings.tsx
+++ public/app/features/dashboard/components/DashboardSettings/GeneralSettings.tsx
@@ -207,7 +207,7 @@ export function GeneralSettingsUnconnected({
 
         {/* @todo: Update "Graph tooltip" description to remove prompt about reloading when resolving #46581 */}
         <CollapsableSection
-          label={t('dashboard-settings.general-settings.panel-options.label', 'Panel options')}
+          label={t('dashboard-settings.general.panel-options-label', 'Panel options')}
           isOpen={true}
         >
           <Field
