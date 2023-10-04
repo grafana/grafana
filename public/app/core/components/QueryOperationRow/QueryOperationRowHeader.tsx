5ae81b78d2d3e09742bdcdcaef0e13d6352a8f30
Kyle Cunningham <kyle@codeincarnate.com>
2023-10-04 15:01:03 -0500

    Update row title in transformations

--- public/app/core/components/QueryOperationRow/QueryOperationRowHeader.tsx
+++ public/app/core/components/QueryOperationRow/QueryOperationRowHeader.tsx
@@ -18,6 +18,12 @@ export interface QueryOperationRowHeaderProps {
   reportDragMousePosition: MouseEventHandler<HTMLDivElement>;
   title?: string;
   id: string;
+  expanderMessages?: ExpanderMessages;
+}
+
+export interface ExpanderMessages {
+  open: string;
+  close: string;
 }
 
 export const QueryOperationRowHeader = ({
@@ -32,8 +38,17 @@ export const QueryOperationRowHeader = ({
   reportDragMousePosition,
   title,
   id,
+  expanderMessages
 }: QueryOperationRowHeaderProps) => {
   const styles = useStyles2(getStyles);
+
+  let tooltipMessage = isContentVisible ? 'Collapse query row' : 'Expand query row';
+  if (expanderMessages !== undefined && isContentVisible) {
+    tooltipMessage = expanderMessages.close;
+  }
+  else if (expanderMessages !== undefined) {
+    tooltipMessage = expanderMessages?.open;
+  }
 
   return (
     <div className={styles.header}>
@@ -41,7 +56,7 @@ export const QueryOperationRowHeader = ({
         {collapsable && (
           <IconButton
             name={isContentVisible ? 'angle-down' : 'angle-right'}
-            tooltip={isContentVisible ? 'Collapse query row' : 'Expand query row'}
+            tooltip={tooltipMessage}
             className={styles.collapseIcon}
             onClick={onRowToggle}
             aria-expanded={isContentVisible}
