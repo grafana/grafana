51a8067d6e6a726d384c0927d01e0eb732c260f9
Kyle Cunningham <kyle@codeincarnate.com>
2023-10-04 10:44:10 -0500

    Sentence casing

--- public/app/features/dashboard/components/PanelEditor/state/selectors.ts
+++ public/app/features/dashboard/components/PanelEditor/state/selectors.ts
@@ -33,7 +33,7 @@ export const getPanelEditorTabs = memoizeOne((tab?: string, plugin?: PanelPlugin
 
     tabs.push({
       id: PanelEditorTabId.Transform,
-      text: 'Transform Data',
+      text: 'Transform data',
       icon: 'process',
       active: false,
     });
