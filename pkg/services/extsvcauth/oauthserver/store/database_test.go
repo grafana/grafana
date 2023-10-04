193ec8de2b5fe594e31c569c70d5968314f8fa55
Gabriel MABILLE <gamab@users.noreply.github.com>
2023-10-04 16:53:17 +0200

    AuthN: Move oauthserver to extsvcauth (#75972)
    * AuthN: Move oauthserver to extsvcauth
    
    * Codeowners

--- pkg/services/extsvcauth/oauthserver/store/database_test.go
+++ pkg/services/extsvcauth/oauthserver/store/database_test.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/extsvcauth/oauthserver"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
-	"github.com/grafana/grafana/pkg/services/oauthserver"
 )
 
 func TestStore_RegisterAndGetClient(t *testing.T) {
