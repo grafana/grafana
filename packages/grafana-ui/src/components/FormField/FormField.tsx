52f4093a85cdad3dae483edf1ffe1112629bb799
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-10-05 17:02:35 +0200

    Fix whitespace

--- packages/grafana-ui/src/components/FormField/FormField.tsx
+++ packages/grafana-ui/src/components/FormField/FormField.tsx
@@ -21,7 +21,6 @@ const defaultProps = {
 };
 
 /**
- *
  * Default form field including label used in Grafana UI. Default input element is simple <input />. You can also pass
  * custom inputEl if required in which case inputWidth and inputProps are ignored.
  * @deprecated
