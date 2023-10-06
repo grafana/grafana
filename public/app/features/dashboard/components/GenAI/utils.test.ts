4db6a2644c6eb4b9fc5b63061f201add231a9d66
Ivan Ortega <ivanortegaalba@gmail.com>
2023-10-06 14:31:23 +0200

    Adapt test to the new generated string

--- public/app/features/dashboard/components/GenAI/utils.test.ts
+++ public/app/features/dashboard/components/GenAI/utils.test.ts
@@ -35,31 +35,63 @@ describe('getDashboardChanges', () => {
 
     // Call the function to test
     const result = getDashboardChanges(dashboard);
-
     // Assertions
-    expect(result.userChanges).toEqual({
-      panels: [
-        {
-          op: 'replace',
-          originalValue: 'Panel 1',
-          value: 'New title',
-          startLineNumber: expect.any(Number),
-          path: ['panels', '0', 'title'],
-        },
-      ],
-    });
+    expect(result.migrationChanges).toEqual(
+      '===================================================================\n' +
+        '--- Before migration changes\t\n' +
+        '+++ After migration changes\t\n' +
+        '@@ -1,25 +1,21 @@\n' +
+        ' {\n' +
+        '   "editable": true,\n' +
+        '   "graphTooltip": 0,\n' +
+        '-  "schemaVersion": 37,\n' +
+        '+  "schemaVersion": 38,\n' +
+        '   "timezone": "",\n' +
+        '   "panels": [\n' +
+        '     {\n' +
+        '-      "fieldConfig": {\n' +
+        '-        "defaults": {},\n' +
+        '-        "overrides": []\n' +
+        '-      },\n' +
+        '+      "id": 1,\n' +
+        '       "options": {\n' +
+        '         "legend": {\n' +
+        '           "displayMode": "hidden",\n' +
+        '           "showLegend": false\n' +
+        '         }\n' +
+        '       },\n' +
+        '       "repeatDirection": "h",\n' +
+        '+      "title": "Panel 1",\n' +
+        '       "transformations": [],\n' +
+        '-      "transparent": false,\n' +
+        '-      "type": "timeseries",\n' +
+        '-      "title": "Panel 1"\n' +
+        '+      "type": "timeseries"\n' +
+        '     }\n' +
+        '   ]\n' +
+        ' }\n' +
+        '\\ No newline at end of file\n'
+    );
+    expect(result.userChanges).toEqual(
+      '===================================================================\n' +
+        '--- Before user changes\t\n' +
+        '+++ After user changes\t\n' +
+        '@@ -11,11 +11,11 @@\n' +
+        '           "displayMode": "hidden",\n' +
+        '           "showLegend": false\n' +
+        '         }\n' +
+        '       },\n' +
+        '       "repeatDirection": "h",\n' +
+        '-      "title": "Panel 1",\n' +
+        '+      "title": "New title",\n' +
+        '       "transformations": [],\n' +
+        '       "type": "timeseries"\n' +
+        '     }\n' +
+        '   ]\n' +
+        ' }\n' +
+        '\\ No newline at end of file\n'
+    );
     expect(result.migrationChanges).toBeDefined();
-    expect(result.userChanges).not.toContain({
-      panels: [
-        {
-          op: 'replace',
-          originalValue: 'Panel 1',
-          value: 'New title',
-          startLineNumber: expect.any(Number),
-          path: ['panels', '0', 'title'],
-        },
-      ],
-    });
   });
 });
 
