28b8a70b713999a3715570301f3d4aa1580b033f
Adela Almasan <adela.almasan@grafana.com>
2023-10-06 09:14:36 -0500

    move PopoverModal to core

--- public/app/core/components/PopoverModal.tsx
+++ public/app/core/components/PopoverModal.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import { Toggletip } from '@grafana/ui';
 import { ToggletipProps } from '@grafana/ui/src/components/Toggletip';
 
+/**
+ * Temporary wrapper component for Toggletip to support the "popover" theme.
+ *
+ */
+
 interface PopoverModalProps extends Omit<ToggletipProps, 'theme'> {}
 
 export function PopoverModal({ children, ...otherProps }: PopoverModalProps) {
