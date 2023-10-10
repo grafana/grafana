8bb229a17580c8cc601da6a463b3b20db10b6ec3
Andres Martinez <andres.martinez@grafana.com>
2023-10-10 17:20:02 +0200

    more fixes

--- packages/plugins/utils.ts
+++ packages/plugins/utils.ts
@@ -14,21 +14,13 @@ export function getPluginJson() {
 // Support bundling nested plugins by finding all plugin.json files in src directory
 // then checking for a sibling module.[jt]sx? file.
 export async function getEntries(): Promise<Record<string, string>> {
-  const pluginsJson = await glob(path.resolve('**/plugin.json'), { absolute: true });
-
-  const plugins = await Promise.all(
-    pluginsJson.map((pluginJson) => {
-      const folder = path.dirname(pluginJson);
-      return glob(`${folder}/module.{ts,tsx,js,jsx}`, { absolute: true });
-    })
-  );
-
-  return plugins.reduce((result, modules) => {
-    return modules.reduce((result: { [s: string]: string }, module) => {
-      result['module'] = module;
-      return result;
-    }, result);
-  }, {});
+  const pluginsJson = await glob(path.resolve(process.cwd(), `module.{ts,tsx}`));
+  if (pluginsJson.length > 0) {
+    return {
+      module: pluginsJson[0],
+    };
+  }
+  throw new Error('Could not find module.ts or module.tsx file');
 }
 
 export function hasLicense() {
