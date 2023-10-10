15240edc0fe7af2977989bd087a2d351eda35b80
Matias Chomicki <matyax@gmail.com>
2023-10-10 11:47:36 +0200

    Query: remove unused var

--- public/app/features/explore/state/query.ts
+++ public/app/features/explore/state/query.ts
@@ -731,7 +731,6 @@ export const runLoadMoreQueries = createAsyncThunk<void, RunLoadMoreQueriesOptio
     const interpolateCorrelationHelperVars =
       isCorrelationEditorMode && !isLeftPane && correlationEditorHelperData !== undefined;
     let newQuerySource: Observable<ExplorePanelData>;
-    let newQuerySubscription: SubscriptionLike;
 
     // Filter queries by those explicitly requested by refId
     const queries = exploreQueries.map((query) => ({
@@ -793,7 +792,7 @@ export const runLoadMoreQueries = createAsyncThunk<void, RunLoadMoreQueriesOptio
       )
     );
 
-    newQuerySubscription = newQuerySource.subscribe({
+    newQuerySource.subscribe({
       next(data) {
         if (data.logsResult !== null && data.state === LoadingState.Done) {
           reportInteraction('grafana_explore_logs_result_displayed', {
