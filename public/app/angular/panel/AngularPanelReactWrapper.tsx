ce31f8073fcd88d19195fe852836517967e3c756
Torkel Ödegaard <torkel@grafana.com>
2023-10-10 14:57:16 +0200

    Make options work

--- public/app/angular/panel/AngularPanelReactWrapper.tsx
+++ public/app/angular/panel/AngularPanelReactWrapper.tsx
@@ -1,8 +1,16 @@
 import React, { ComponentType, useEffect, useRef } from 'react';
 import { Observable, ReplaySubject } from 'rxjs';
 
-import { dateTimeFormat, DateTimeInput, EventBusSrv, PanelData, PanelPlugin, PanelProps } from '@grafana/data';
-import { AngularComponent, getAngularLoader } from '@grafana/runtime';
+import {
+  dateTimeFormat,
+  DateTimeInput,
+  EventBusSrv,
+  PanelData,
+  PanelPlugin,
+  PanelProps,
+  FieldConfigSource,
+} from '@grafana/data';
+import { AngularComponent, getAngularLoader, RefreshEvent } from '@grafana/runtime';
 import { sceneGraph } from '@grafana/scenes';
 import { PanelModel, DashboardModel } from 'app/features/dashboard/state';
 import { GetDataOptions } from 'app/features/query/state/PanelQueryRunner';
@@ -32,14 +40,7 @@ export function getAngularPanelReactWrapper(plugin: PanelPlugin): ComponentType<
       const loader = getAngularLoader();
       const template = '<plugin-component type="panel" class="panel-height-helper"></plugin-component>';
       const queryRunner = new FakeQueryRunner();
-
-      const fakePanel = {
-        events: new EventBusSrv(),
-        type: plugin.meta.id,
-        id: props.id,
-        fieldConfig: props.fieldConfig,
-        getQueryRunner: () => queryRunner,
-      };
+      const fakePanel = new FakePanel(plugin, props, queryRunner);
 
       angularState.current = {
         // @ts-ignore
@@ -77,6 +78,46 @@ export function getAngularPanelReactWrapper(plugin: PanelPlugin): ComponentType<
 
     return <div ref={divRef} className="panel-height-helper" />;
   };
+}
+
+class FakePanel {
+  id: number;
+  type: string;
+  title: string;
+  plugin: PanelPlugin;
+  events: EventBusSrv;
+  queryRunner: FakeQueryRunner;
+  fieldConfig: FieldConfigSource;
+  options: Record<string, unknown>;
+
+  constructor(plugin: PanelPlugin, props: PanelProps, queryRunner: FakeQueryRunner) {
+    // Assign legacy "root" level options
+    if (props.options.angularOptions) {
+      Object.assign(this, props.options.angularOptions);
+    }
+
+    this.id = props.id;
+    this.type = plugin.meta.id;
+    this.title = props.title;
+    this.fieldConfig = props.fieldConfig;
+    this.options = props.options;
+
+    this.plugin = plugin;
+    this.events = new EventBusSrv();
+    this.queryRunner = queryRunner;
+  }
+
+  refresh() {
+    this.events.publish(new RefreshEvent());
+  }
+
+  render() {
+    this.events.publish(new RenderEvent());
+  }
+
+  getQueryRunner() {
+    return this.queryRunner;
+  }
 }
 
 class FakeQueryRunner {
