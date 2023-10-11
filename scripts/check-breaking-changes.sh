e9492aebaad7faba764344bd0072b2da63e0cd24
Andres Martinez <andres.martinez@grafana.com>
2023-10-11 11:20:24 +0200

    rename grafana-plugin-configs folder

--- scripts/check-breaking-changes.sh
+++ scripts/check-breaking-changes.sh
@@ -4,7 +4,7 @@
 PACKAGES=$(ls -d ./packages/*/)
 EXIT_CODE=0
 GITHUB_MESSAGE=""
-SKIP_PACKAGES=("grafana-eslint-rules" "plugins")
+SKIP_PACKAGES=("grafana-eslint-rules" "grafana-plugin-configs")
 
 # Loop through the packages
 while IFS=" " read -r -a package; do
