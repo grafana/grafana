67e3e576d0c0999933435f8d50517cd0061617a7
Brendan O'Handley <brendan.ohandley@grafana.com>
2023-10-12 12:46:49 -0400

    Prometheus: Fix config bug for sigv4 auth (#76390)
    fix config bug for selecting sigv4

--- public/app/plugins/datasource/prometheus/types.ts
+++ public/app/plugins/datasource/prometheus/types.ts
@@ -46,6 +46,7 @@ export interface PromOptions extends DataSourceJsonData {
   incrementalQueryOverlapWindow?: string;
   disableRecordingRules?: boolean;
   sigV4Auth?: boolean;
+  oauthPassThru?: boolean;
 }
 
 export type ExemplarTraceIdDestination = {
