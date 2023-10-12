293f5fb98a2c48d724bb980de9dbb37493452c9f
nmarrs <nathanielmarrs@gmail.com>
2023-10-11 20:40:59 -0700

    after wrestling with prompt for hours, this seems to be about as stable as I can manage for time being 😬

--- public/app/features/dashboard/components/GenAI/GenAIDashboardChangesButton.tsx
+++ public/app/features/dashboard/components/GenAI/GenAIDashboardChangesButton.tsx
@@ -17,14 +17,13 @@ const CHANGES_GENERATION_STANDARD_PROMPT = [
   'Your goal is to write a description of the changes for a dashboard',
   'When referring to panel changes, use the panel title',
   'When using panel title, wrap it with double quotes',
-  'When the panel changes the position, just mention the panel title has changed position',
+  'When the panel changes the position, just mention the panel has changed position and not the specific change',
   'When an entire panel is added or removed, use the panel title and only say it was added or removed and disregard the rest of the changes for that panel',
   'Group changes when all panels are affected',
   'Do not mention line number',
   'Refer to templating elements as variables',
   'Ignore and never mention changes about plugin version',
-  'Try to make it as short as possible.',
-  '',
+  'Try to make the response as short as possible',
 ].join('.\n');
 
 export const GenAIDashboardChangesButton = ({ dashboard, onGenerate, disabled }: GenAIDashboardChangesButtonProps) => {
@@ -58,11 +57,23 @@ function getMessages(dashboard: DashboardModel): Message[] {
       role: Role.system,
     },
     {
-      content: `Group the following diff under "User changes" as a bullet list:\n${userChanges}`,
+      content: `Summarize the following user changes diff under a basic "User changes" heading as a bulleted list:\n${userChanges}`,
       role: Role.system,
     },
     {
-      content: `Group the following diff under "Migration changes" as a bullet list:\n${migrationChanges}`,
+      content: `Be sure to only include substantial user changes, such as adding or removing panels, changing panel titles or descriptions, etc. Ignore other changes and do not include them in the summary. Do not include "User Changes" section if there are no actual user changes to report.`,
+      role: Role.system,
+    },
+    {
+      content: `Threshold step changes of value being removed and replaced with color "green" should always be ignored`,
+      role: Role.system,
+    },
+    {
+      content: `If the following migrations diff has substantial changes add the following sentence on a new separate line "Dashboard migration changes". Ignore any threshold step changes and any diff less than 10 lines long.\n This is the migrations diff:\n${migrationChanges}`,
+      role: Role.system,
+    },
+    {
+      content: `As a fallback, if there are no detected user changes, respond only with "Dashboard migration changes" and nothing else.`,
       role: Role.system,
     },
   ];
