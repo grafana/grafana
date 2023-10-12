2a6a82362d62e61b91f50ac03e4359742408e1a0
Fabrizio <fabrizio.casati@grafana.com>
2023-10-12 13:34:31 +0200

    Improve logs

--- pkg/tsdb/grafana-pyroscope-datasource/service.go
+++ pkg/tsdb/grafana-pyroscope-datasource/service.go
@@ -34,16 +34,16 @@ type Service struct {
 }
 
 func (s *Service) getInstance(ctx context.Context, pluginCtx backend.PluginContext) (*PyroscopeDatasource, error) {
-	logger.Debug("Get intance")
+	logger.Debug("getInstance called")
 
 	i, err := s.im.Get(ctx, pluginCtx)
 	if err != nil {
-		logger.Debug("Error in getting intance", "error", err)
+		logger.Debug("getInstance errored", "error", err)
 		return nil, err
 	}
 
 	in := i.(*PyroscopeDatasource)
-	logger.Debug("Successfully got instance")
+	logger.Debug("getInstance succeded")
 	return in, nil
 }
 
@@ -60,71 +60,86 @@ func newInstanceSettings(httpClientProvider httpclient.Provider, ac accesscontro
 }
 
 func (s *Service) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	logger.Debug("Received query request", "queries", req.Queries)
+	logger.Debug("QueryData called", "queries", req.Queries)
 
 	i, err := s.getInstance(ctx, req.PluginContext)
 	if err != nil {
-		logger.Debug("Failure in processing query request", "error", err)
+		logger.Debug("QueryData errored", "error", err)
 		return nil, err
 	}
 
 	response, err := i.QueryData(ctx, req)
-	logger.Debug("Successfully processed query request")
+	logger.Debug("QueryData succeeded")
 	return response, err
 }
 
 func (s *Service) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	logger.Debug("Successfully processed query request")
+	logger.Debug("CallResource called")
+
 	i, err := s.getInstance(ctx, req.PluginContext)
 	if err != nil {
-		logger.Debug("Successfully processed query request")
+		logger.Debug("CallResource errored", "error", err)
 		return err
 	}
-	logger.Debug("Successfully processed query request")
-	return i.CallResource(ctx, req, sender)
+
+	call := i.CallResource(ctx, req, sender)
+	logger.Debug("CallResource succeeded")
+	return call
 }
 
 func (s *Service) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	logger.Debug("Successfully processed query request")
+	logger.Debug("CheckHealth called")
+
 	i, err := s.getInstance(ctx, req.PluginContext)
 	if err != nil {
-		logger.Debug("Successfully processed query request")
+		logger.Debug("CheckHealth errorer", "error", err)
 		return nil, err
 	}
-	logger.Debug("Successfully processed query request")
-	return i.CheckHealth(ctx, req)
+
+	check, err := i.CheckHealth(ctx, req)
+	logger.Debug("CheckHealth succeded")
+	return check, err
 }
 
 func (s *Service) SubscribeStream(ctx context.Context, req *backend.SubscribeStreamRequest) (*backend.SubscribeStreamResponse, error) {
-	logger.Debug("Successfully processed query request")
+	logger.Debug("SubscribeStream called")
+
 	i, err := s.getInstance(ctx, req.PluginContext)
 	if err != nil {
-		logger.Debug("Successfully processed query request")
+		logger.Debug("SubscribeStream errored", "error", err)
 		return nil, err
 	}
-	logger.Debug("Successfully processed query request")
-	return i.SubscribeStream(ctx, req)
+
+	stream, err := i.SubscribeStream(ctx, req)
+	logger.Debug("SubscribeStream succeeded")
+	return stream, err
 }
 
 func (s *Service) RunStream(ctx context.Context, req *backend.RunStreamRequest, sender *backend.StreamSender) error {
-	logger.Debug("Successfully processed query request")
+	logger.Debug("RunStream called")
+
 	i, err := s.getInstance(ctx, req.PluginContext)
 	if err != nil {
-		logger.Debug("Successfully processed query request")
+		logger.Debug("RunStream errored", "error", err)
 		return err
 	}
-	logger.Debug("Successfully processed query request")
-	return i.RunStream(ctx, req, sender)
+
+	stream := i.RunStream(ctx, req, sender)
+	logger.Debug("RunStream succeeded")
+	return stream
 }
 
 // PublishStream is called when a client sends a message to the stream.
 func (s *Service) PublishStream(ctx context.Context, req *backend.PublishStreamRequest) (*backend.PublishStreamResponse, error) {
-	logger.Debug("Successfully processed query request")
+	logger.Debug("PublishStream called")
+
 	i, err := s.getInstance(ctx, req.PluginContext)
 	if err != nil {
-		logger.Debug("Successfully processed query request")
+		logger.Debug("PublishStream errored", "error", err)
 		return nil, err
 	}
-	logger.Debug("Successfully processed query request")
-	return i.PublishStream(ctx, req)
+
+	stream, err := i.PublishStream(ctx, req)
+	logger.Debug("PublishStream succeeded")
+	return stream, err
 }
