d27f5ea89de30ab943849f8d1304d56c265dd2e6
Serge Zaitsev <serge.zaitsev@grafana.com>
2023-10-13 16:43:54 +0200

    change integer to boolean

--- pkg/services/folder/folderimpl/folder.go
+++ pkg/services/folder/folderimpl/folder.go
@@ -99,7 +99,7 @@ func (s *Service) DBMigration(db db.DB) {
 		}
 		_, err = sess.Exec(`
 			DELETE FROM folder WHERE NOT EXISTS
-				(SELECT 1 FROM dashboard WHERE dashboard.uid = folder.uid AND dashboard.org_id = folder.org_id AND dashboard.is_folder = 1)
+				(SELECT 1 FROM dashboard WHERE dashboard.uid = folder.uid AND dashboard.org_id = folder.org_id AND dashboard.is_folder = true)
 		`)
 		return err
 	})
