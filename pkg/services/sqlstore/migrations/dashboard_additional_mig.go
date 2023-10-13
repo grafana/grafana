294e945edc517516af9a86688453c8112a07f945
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-10-13 10:00:42 +0300

    Introduce dashboard.folder_uid column

---
+++ pkg/services/sqlstore/migrations/dashboard_additional_mig.go
@@ -0,0 +1,9 @@
+package migrations
+
+import "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
+
+func addDashboardFolderMigrations(mg *migrator.Migrator) {
+	mg.AddMigration("Add folder_uid for dashboard", migrator.NewAddColumnMigration(migrator.Table{Name: "dashboard"}, &migrator.Column{
+		Name: "folder_uid", Type: migrator.DB_NVarchar, Length: 40, Nullable: true,
+	}))
+}
