51e0efee572bc795a21d9bbcb892b7a9f2171f60
aditipatelpro <aditipatelpro@gmail.com>
2023-10-13 12:57:56 -0500

    chore: removed markup from cheatsheet file

--- public/app/plugins/datasource/postgres/CheatSheet.tsx
+++ public/app/plugins/datasource/postgres/CheatSheet.tsx
@@ -3,76 +3,58 @@ import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { useStyles2 } from '@grafana/ui';
-import { t, Trans } from 'app/core/internationalization';
 
 export function CheatSheet() {
   const styles = useStyles2(getStyles);
 
   return (
     <div>
-      <h2>
-        <Trans i18nKey="cheat-sheet.title">PostgreSQL cheat sheet</Trans>
-      </h2>
-      {t('cheat-sheet.time-series.title', 'Time series:')}
+      <h2>PostgreSQL cheat sheet</h2>
+      Time series:
       <ul className={styles.ulPadding}>
-        <Trans i18nKey="cheat-sheet.time-series.description">
-          <li>
-            return column named <i>time</i> (UTC in seconds or timestamp)
-          </li>
-          <li>return column(s) with numeric datatype as values</li>
-        </Trans>
+        <li>
+          return column named <i>time</i> (UTC in seconds or timestamp)
+        </li>
+        <li>return column(s) with numeric datatype as values</li>
       </ul>
-      {t('cheat-sheet.optional.title', 'Optional:')}
+      Optional:
       <ul className={styles.ulPadding}>
-        <Trans i18nKey="cheat-sheet.optional.description">
-          <li>
-            return column named <i>metric</i> to represent the series name.
-          </li>
-          <li>If multiple value columns are returned the metric column is used as prefix.</li>
-          <li>If no column named metric is found the column name of the value column is used as series name</li>
-        </Trans>
+        <li>
+          return column named <i>metric</i> to represent the series name.
+        </li>
+        <li>If multiple value columns are returned the metric column is used as prefix.</li>
+        <li>If no column named metric is found the column name of the value column is used as series name</li>
       </ul>
-      <p>
-        <Trans i18nKey="cheat-sheet.time-series.note">
-          Resultsets of time series queries need to be sorted by time.
-        </Trans>
-      </p>
-      {t('cheat-sheet.table.title', 'Table:')}
+      <p>Resultsets of time series queries need to be sorted by time.</p>
+      Table:
       <ul className={styles.ulPadding}>
-        <Trans i18nKey="cheat-sheet.table.description">
-          <li>return any set of columns</li>
-        </Trans>
+        <li>return any set of columns</li>
       </ul>
-      {t('cheat-sheet.macros.title', 'Macros:')}
+      Macros:
       <ul className={styles.ulPadding}>
-        <Trans i18nKey="cheat-sheet.macros.description">
-          <li>$__time(column) -&gt; column as &quot;time&quot;</li>
-          <li>$__timeEpoch -&gt; extract(epoch from column) as &quot;time&quot;</li>
-          <li>
-            $__timeFilter(column) -&gt; column BETWEEN &apos;2017-04-21T05:01:17Z&apos; AND
-            &apos;2017-04-21T05:01:17Z&apos;
-          </li>
-          <li>$__unixEpochFilter(column) -&gt; column &gt;= 1492750877 AND column &lt;= 1492750877</li>
-          <li>
-            $__unixEpochNanoFilter(column) -&gt; column &gt;= 1494410783152415214 AND column &lt;= 1494497183142514872
-          </li>
-          <li>
-            $__timeGroup(column,&apos;5m&apos;[, fillvalue]) -&gt; (extract(epoch from column)/300)::bigint*300 by
-            setting fillvalue grafana will fill in missing values according to the interval fillvalue can be either a
-            literal value, NULL or previous; previous will fill in the previous seen value or NULL if none has been seen
-            yet
-          </li>
-          <li>
-            $__timeGroupAlias(column,&apos;5m&apos;) -&gt; (extract(epoch from column)/300)::bigint*300 AS
-            &quot;time&quot;
-          </li>
-          <li>$__unixEpochGroup(column,&apos;5m&apos;) -&gt; floor(column/300)*300</li>
-          <li>$__unixEpochGroupAlias(column,&apos;5m&apos;) -&gt; floor(column/300)*300 AS &quot;time&quot;</li>
-        </Trans>
+        <li>$__time(column) -&gt; column as &quot;time&quot;</li>
+        <li>$__timeEpoch -&gt; extract(epoch from column) as &quot;time&quot;</li>
+        <li>
+          $__timeFilter(column) -&gt; column BETWEEN &apos;2017-04-21T05:01:17Z&apos; AND
+          &apos;2017-04-21T05:01:17Z&apos;
+        </li>
+        <li>$__unixEpochFilter(column) -&gt; column &gt;= 1492750877 AND column &lt;= 1492750877</li>
+        <li>
+          $__unixEpochNanoFilter(column) -&gt; column &gt;= 1494410783152415214 AND column &lt;= 1494497183142514872
+        </li>
+        <li>
+          $__timeGroup(column,&apos;5m&apos;[, fillvalue]) -&gt; (extract(epoch from column)/300)::bigint*300 by setting
+          fillvalue grafana will fill in missing values according to the interval fillvalue can be either a literal
+          value, NULL or previous; previous will fill in the previous seen value or NULL if none has been seen yet
+        </li>
+        <li>
+          $__timeGroupAlias(column,&apos;5m&apos;) -&gt; (extract(epoch from column)/300)::bigint*300 AS
+          &quot;time&quot;
+        </li>
+        <li>$__unixEpochGroup(column,&apos;5m&apos;) -&gt; floor(column/300)*300</li>
+        <li>$__unixEpochGroupAlias(column,&apos;5m&apos;) -&gt; floor(column/300)*300 AS &quot;time&quot;</li>
       </ul>
-      <p>
-        <Trans i18nKey="cheat-sheet.macros.example-note">Example of group by and order by with $__timeGroup:</Trans>
-      </p>
+      <p>Example of group by and order by with $__timeGroup:</p>
       <pre>
         <code>
           SELECT $__timeGroup(date_time_col, &apos;1h&apos;), sum(value) as value <br />
@@ -84,10 +66,7 @@ export function CheatSheet() {
           <br />
         </code>
       </pre>
-      {t(
-        'cheat-sheet.macros.conditionals-note',
-        'Or build your own conditionals using these macros which just return the values:'
-      )}
+      Or build your own conditionals using these macros which just return the values:
       <ul className={styles.ulPadding}>
         <li>$__timeFrom() -&gt; &apos;2017-04-21T05:01:17Z&apos;</li>
         <li>$__timeTo() -&gt; &apos;2017-04-21T05:01:17Z&apos;</li>
