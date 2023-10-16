af5c709e335a9c738a090da2e55f9d4eeb9b24c3
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-10-16 11:38:57 +0300

    Add index

--- pkg/services/sqlstore/migrations/dashboard_additional_mig.go
+++ pkg/services/sqlstore/migrations/dashboard_additional_mig.go
@@ -6,4 +6,8 @@ func addDashboardFolderMigrations(mg *migrator.Migrator) {
 	mg.AddMigration("Add folder_uid for dashboard", migrator.NewAddColumnMigration(migrator.Table{Name: "dashboard"}, &migrator.Column{
 		Name: "folder_uid", Type: migrator.DB_NVarchar, Length: 40, Nullable: false, Default: "0",
 	}))
+
+	mg.AddMigration("Add unique index for dashboard_org_id_folder_uid_title", migrator.NewAddIndexMigration(migrator.Table{Name: "dashboard"}, &migrator.Index{
+		Cols: []string{"org_id", "folder_uid", "title"}, Type: migrator.UniqueIndex,
+	}))
 }
