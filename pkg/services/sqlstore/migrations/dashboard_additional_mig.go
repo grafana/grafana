62a8a9803d6d57c09d4ea18867e95fa978ffb252
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-10-16 11:38:18 +0300

    Change folder_uid to not nullable

--- pkg/services/sqlstore/migrations/dashboard_additional_mig.go
+++ pkg/services/sqlstore/migrations/dashboard_additional_mig.go
@@ -4,6 +4,6 @@ import "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 
 func addDashboardFolderMigrations(mg *migrator.Migrator) {
 	mg.AddMigration("Add folder_uid for dashboard", migrator.NewAddColumnMigration(migrator.Table{Name: "dashboard"}, &migrator.Column{
-		Name: "folder_uid", Type: migrator.DB_NVarchar, Length: 40, Nullable: true,
+		Name: "folder_uid", Type: migrator.DB_NVarchar, Length: 40, Nullable: false, Default: "0",
 	}))
 }
