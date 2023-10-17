e131579a708553c1b03042f761e026e20c87ac59
Ihor Yeromin <yeryomin.igor@gmail.com>
2023-10-17 16:41:12 +0200

    feat(state-timeline-tooltip): clean-up

--- public/app/plugins/panel/status-history/StatusHistoryTooltip2.tsx
+++ public/app/plugins/panel/status-history/StatusHistoryTooltip2.tsx
@@ -16,17 +16,13 @@ import { useStyles2 } from '@grafana/ui';
 import { VizTooltipContent } from '@grafana/ui/src/components/VizTooltip/VizTooltipContent';
 import { VizTooltipFooter } from '@grafana/ui/src/components/VizTooltip/VizTooltipFooter';
 import { VizTooltipHeader } from '@grafana/ui/src/components/VizTooltip/VizTooltipHeader';
-import { ColorIndicator, LabelValue } from '@grafana/ui/src/components/VizTooltip/types';
-import { getTitleFromHref } from 'app/features/explore/utils/links';
-
-import { Options } from './panelcfg.gen';
+import { LabelValue } from '@grafana/ui/src/components/VizTooltip/types';
 
 interface StatusHistoryTooltipProps {
   data: DataFrame[];
   dataIdxs: Array<number | null>;
   alignedData: DataFrame;
   seriesIdx: number | null | undefined;
-  // datapointIdx: number;
   timeZone: TimeZone;
   isPinned: boolean;
 }
@@ -44,22 +40,20 @@ export const StatusHistoryTooltip2 = ({
   dataIdxs,
   alignedData,
   seriesIdx,
-  // datapointIdx,
   timeZone,
   isPinned,
 }: StatusHistoryTooltipProps) => {
   const styles = useStyles2(getStyles);
 
-  // check other dataIdx, it can be undefined or null in array
+  // @todo: check other dataIdx, it can be undefined or null in array
   const datapointIdx = dataIdxs.find((idx) => idx !== undefined);
-  // @todo: remove -1 when uPlot v2 arrive
-  // context: first value in dataIdxs always null and represent X serie
   const seriesIndex = dataIdxs.findIndex((idx) => idx != null);
 
   if (!data || datapointIdx == null) {
     return null;
   }
 
+  const xField = alignedData.fields[0];
   const field = alignedData.fields[seriesIndex!];
 
   const links: Array<LinkModel<Field>> = [];
@@ -77,31 +71,13 @@ export const StatusHistoryTooltip2 = ({
     });
   }
 
-  const xField = alignedData.fields[0];
-  // const xFieldFmt = xField.display || getDisplayProcessor({ field: xField, timeZone, theme });
-  // const xFieldFmt = xField.display;
-  // const xFieldFmt = xField.display || getDisplayProcessor();
-
-  // const dataFrameFieldIndex = field.state?.origin;
-  // const fieldFmt = field.display || getDisplayProcessor({ field, timeZone, theme });
   const fieldFmt = field.display || getDisplayProcessor();
-  // const fieldFmt = field.display;
   const value = field.values[datapointIdx!];
   const display = fieldFmt(value);
 
-  // const fieldDisplayName = dataFrameFieldIndex
-  //   ? getFieldDisplayName(
-  //       data[dataFrameFieldIndex.frameIndex].fields[dataFrameFieldIndex.fieldIndex],
-  //       data[dataFrameFieldIndex.frameIndex],
-  //       data
-  //     )
-  //   : '';
-
   const getHeaderLabel = (): LabelValue => {
     return {
-      // label: fieldDisplayName,
       label: getFieldDisplayName(field),
-      // value: display.text,
       value: fmt(field, field.values[datapointIdx]),
       color: display.color || (FALLBACK_COLOR as string),
     };
@@ -110,13 +86,8 @@ export const StatusHistoryTooltip2 = ({
   const getContentLabelValue = (): LabelValue[] => {
     return [
       {
-        // label: getFieldDisplayName(xField, frame),
-        // label: xField.name,
         label: 'Time',
         value: fmt(xField, xField.values[datapointIdx]),
-        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-        // color: series.pointColor(frame) as string,
-        // color: display.color || (FALLBACK_COLOR as string),
       },
     ];
   };
