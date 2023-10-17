de1ed216f4bbf6f341aa22b144fa66d583a63981
Adam Bannach <113929542+abannachGrafana@users.noreply.github.com>
2023-10-17 11:15:51 -0500

    Feat: Add cloud plugin cost management to admin section (#76547)
    * feat: add cost management to admin and put adaptive metrics and log volume under it
    
    * test: fix applinks test
    
    * chore: fix lint error
    
    * remove "new" from feature toggle description
    
    ---------
    
    Co-authored-by: Ashley Harrison <ashley.harrison@grafana.com>

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -897,5 +897,12 @@ var (
 			Stage:        FeatureStageExperimental,
 			Owner:        grafanaPluginsPlatformSquad,
 		},
+		{
+			Name:         "costManagementUi",
+			Description:  "Toggles the display of the cost management ui plugin",
+			Stage:        FeatureStageExperimental,
+			FrontendOnly: false,
+			Owner:        grafanaDatabasesFrontend,
+		},
 	}
 )
