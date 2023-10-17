f5076d1868caa14ce44a70e812315541b4199d9f
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2023-10-17 10:27:45 +0200

    Plugins: Add status_source label to plugin request metrics (#76236)
    * Plugins: Chore: Renamed instrumentation middleware to metrics middleware
    
    * Removed repeated logger attributes in middleware and contextual logger
    
    * renamed loggerParams to logParams
    
    * PR review suggestion
    
    * Add pluginsInstrumentationStatusSource feature toggle
    
    * Plugin error source prometheus metrics
    
    * Add error_source to logs
    
    * re-generate feature toggles
    
    * fix compilation issues
    
    * remove unwanted changes
    
    * Removed logger middleware changes, implement error source using context
    
    * Renamed pluginmeta to pluginrequestmeta, changed some method names
    
    * Fix comment
    
    * pluginrequestmeta.go -> plugin_request_meta.go
    
    * Replaced plugin request meta with status source
    
    * Add tests for pluginrequestmeta status source
    
    * Fix potential nil pointer dereference in instrmentation middleware
    
    * Add metrics middleware tests
    
    * Sort imports in clienttest.go
    
    * Add StatusSourceFromContext test
    
    * Add error_source label to plugin_request_duration_seconds
    
    * Re-generate feature flags
    
    * lint
    
    * Use StatusSourcePlugin by default
    
    * re-generate feature flags

--- pkg/services/pluginsintegration/pluginsintegration.go
+++ pkg/services/pluginsintegration/pluginsintegration.go
@@ -156,7 +156,7 @@ func CreateMiddlewares(cfg *setting.Cfg, oAuthTokenService oauthtoken.OAuthToken
 	skipCookiesNames := []string{cfg.LoginCookieName}
 	middlewares := []plugins.ClientMiddleware{
 		clientmiddleware.NewTracingMiddleware(tracer),
-		clientmiddleware.NewMetricsMiddleware(promRegisterer, registry),
+		clientmiddleware.NewMetricsMiddleware(promRegisterer, registry, features),
 		clientmiddleware.NewContextualLoggerMiddleware(),
 		clientmiddleware.NewLoggerMiddleware(cfg, log.New("plugin.instrumentation")),
 		clientmiddleware.NewTracingHeaderMiddleware(),
