fb6064b018728438ca077199f44177d35fa026fb
Matias Chomicki <matyax@gmail.com>
2023-10-18 11:40:13 +0200

    Remove unused import

--- public/app/features/logs/components/PopoverMenu.tsx
+++ public/app/features/logs/components/PopoverMenu.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { GrafanaTheme2, LogRowModel } from '@grafana/data';
 import { Menu, useStyles2 } from '@grafana/ui';
