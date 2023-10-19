a21df4939ca69c1e1c3a2f43ecae703a836962f0
linoman <2051016+linoman@users.noreply.github.com>
2023-10-19 15:16:30 +0200

    Add proxy service template

---
+++ pkg/services/serviceaccounts/proxy/service.go
@@ -0,0 +1,55 @@
+package proxy
+
+import (
+	"context"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/services/apikey"
+	"github.com/grafana/grafana/pkg/services/serviceaccounts"
+	"github.com/grafana/grafana/pkg/services/serviceaccounts/manager"
+)
+
+// ServiceAccountsProxy is a proxy for the serviceaccounts.Service interface
+// that is used to add validations to service accounts and protects external
+// service accounts from being modified by users.
+
+type ServiceAccountsProxy struct {
+	log            log.Logger
+	proxiedService *manager.ServiceAccountsService
+}
+
+func ProvideServiceAccountsProxy(
+	proxiedService *manager.ServiceAccountsService,
+) (*ServiceAccountsProxy, error) {
+	s := &ServiceAccountsProxy{
+		log:            log.New("serviceaccounts.proxy"),
+		proxiedService: proxiedService,
+	}
+	return s, nil
+}
+
+var _ serviceaccounts.Service = (*ServiceAccountsProxy)(nil)
+
+func (s *ServiceAccountsProxy) CreateServiceAccount(ctx context.Context, orgID int64, saForm *serviceaccounts.CreateServiceAccountForm) (*serviceaccounts.ServiceAccountDTO, error) {
+	return s.proxiedService.CreateServiceAccount(ctx, orgID, saForm)
+}
+
+func (s *ServiceAccountsProxy) DeleteServiceAccount(ctx context.Context, orgID, serviceAccountID int64) error {
+	return s.proxiedService.DeleteServiceAccount(ctx, orgID, serviceAccountID)
+}
+
+func (s *ServiceAccountsProxy) RetrieveServiceAccount(ctx context.Context, orgID, serviceAccountID int64) (*serviceaccounts.ServiceAccountProfileDTO, error) {
+	return s.proxiedService.RetrieveServiceAccount(ctx, orgID, serviceAccountID)
+}
+
+func (s *ServiceAccountsProxy) RetrieveServiceAccountIdByName(ctx context.Context, orgID int64, name string) (int64, error) {
+	return s.proxiedService.RetrieveServiceAccountIdByName(ctx, orgID, name)
+}
+
+func (s *ServiceAccountsProxy) UpdateServiceAccount(ctx context.Context, orgID, serviceAccountID int64, saForm *serviceaccounts.UpdateServiceAccountForm) (*serviceaccounts.ServiceAccountProfileDTO, error) {
+	return s.proxiedService.UpdateServiceAccount(ctx, orgID, serviceAccountID, saForm)
+}
+
+func (s *ServiceAccountsProxy) AddServiceAccountToken(ctx context.Context, serviceAccountID int64, cmd *serviceaccounts.AddServiceAccountTokenCommand) (*apikey.APIKey, error) {
+	return s.proxiedService.AddServiceAccountToken(ctx, serviceAccountID, cmd)
+}
