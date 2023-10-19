c0be1139b45ef265224de6ee478d7a4cfcee3146
Andres Martinez <andres.martinez@grafana.com>
2023-10-19 10:35:33 +0200

    eslint cache path

--- packages/grafana-plugin-configs/webpack.config.ts
+++ packages/grafana-plugin-configs/webpack.config.ts
@@ -29,6 +29,12 @@ const config = async (env: any): Promise<Configuration> => {
         config: [__filename],
       },
       cacheDirectory: path.resolve(__dirname, '../../.yarn/.cache/webpack', path.basename(process.cwd())),
+      cacheLocation: path.resolve(
+        __dirname,
+        '../../.yarn/.cache/eslint-webpack-plugin',
+        path.basename(process.cwd()),
+        '.eslintcache'
+      ),
     },
 
     context: process.cwd(),
