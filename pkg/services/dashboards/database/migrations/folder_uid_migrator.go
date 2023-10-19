d860761d16ba8c45926d34fc66321e669ad38bdc
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-10-19 15:26:42 +0300

    FIx for MySQL 5.7

--- pkg/services/dashboards/database/migrations/folder_uid_migrator.go
+++ pkg/services/dashboards/database/migrations/folder_uid_migrator.go
@@ -14,33 +14,57 @@ func (m *FolderUIDMigration) SQL(dialect migrator.Dialect) string {
 	return "code migration"
 }
 
-func (m *FolderUIDMigration) Exec(sess *xorm.Session, migrator *migrator.Migrator) error {
-	// for dashboards the source of truth is the dashboard table
-	q := `UPDATE dashboard SET folder_uid = (SELECT derived.uid FROM (SELECT d.uid FROM dashboard AS d WHERE d.id = dashboard.folder_id) AS derived) WHERE is_folder = ?`
-	r, err := sess.Exec(q, migrator.Dialect.BooleanStr(false))
+func (m *FolderUIDMigration) Exec(sess *xorm.Session, mgrtr *migrator.Migrator) error {
+	// for dashboards the source of truth is the  dashboard table
+	q := `UPDATE dashboard
+	SET folder_uid = folder.uid
+	FROM dashboard folder
+	WHERE dashboard.folder_id = folder.id
+	  AND dashboard.is_folder = ?`
+	if mgrtr.Dialect.DriverName() == migrator.MySQL {
+		q = `UPDATE dashboard AS d
+		LEFT JOIN dashboard AS folder ON d.folder_id = folder.id
+		SET d.folder_uid = folder.uid
+		WHERE d.is_folder = ?`
+	}
+
+	r, err := sess.Exec(q, mgrtr.Dialect.BooleanStr(false))
 	if err != nil {
-		migrator.Logger.Error("Failed to migrate dashboard folder_uid for dashboards", "error", err)
+		mgrtr.Logger.Error("Failed to migrate dashboard folder_uid for dashboards", "error", err)
 		return err
 	}
 	dashboardRowsAffected, dashboardRowsAffectedErr := r.RowsAffected()
 	if dashboardRowsAffectedErr != nil {
-		migrator.Logger.Error("Failed to get dashboard rows affected", "error", dashboardRowsAffectedErr)
+		mgrtr.Logger.Error("Failed to get dashboard rows affected", "error", dashboardRowsAffectedErr)
 	}
 
 	// for folders the source of truth is the folder table
-	q = `UPDATE dashboard SET folder_uid = (SELECT f.parent_uid FROM folder f WHERE f.org_id = dashboard.org_id AND f.uid = dashboard.uid) WHERE is_folder = ?`
-	r, err = sess.Exec(q, migrator.Dialect.BooleanStr(true))
+	q = `UPDATE dashboard
+	SET folder_uid = folder.parent_uid
+	FROM folder
+	WHERE dashboard.uid = folder.uid AND dashboard.org_id = folder.org_id
+	AND dashboard.is_folder = ?`
+	if mgrtr.Dialect.DriverName() == migrator.MySQL {
+		q = `UPDATE dashboard
+		SET folder_uid = (
+		    SELECT folder.parent_uid
+		    FROM folder
+		    WHERE dashboard.uid = folder.uid AND dashboard.org_id = folder.org_id
+		)
+		WHERE is_folder = ?`
+	}
+	r, err = sess.Exec(q, mgrtr.Dialect.BooleanStr(true))
 	if err != nil {
-		migrator.Logger.Error("Failed to migrate dashboard folder_uid for folders", "error", err)
+		mgrtr.Logger.Error("Failed to migrate dashboard folder_uid for folders", "error", err)
 		return err
 	}
 
 	folderRowsAffected, folderRowsAffectedErr := r.RowsAffected()
 	if folderRowsAffectedErr != nil {
-		migrator.Logger.Error("Failed to get folder rows affected", "error", folderRowsAffectedErr)
+		mgrtr.Logger.Error("Failed to get folder rows affected", "error", folderRowsAffectedErr)
 	}
 
-	migrator.Logger.Debug("Migrating dashboard data", "dashboards rows", dashboardRowsAffected, "folder rows", folderRowsAffected)
+	mgrtr.Logger.Debug("Migrating dashboard data", "dashboards rows", dashboardRowsAffected, "folder rows", folderRowsAffected)
 	return nil
 }
 
