f504f9b368748e4c88e8d73597aa69643f56c069
Ashley Harrison <ashley.harrison@grafana.com>
2023-10-19 16:49:33 +0100

    fix FormField test

--- public/test/mocks/react-inlinesvg.tsx
+++ public/test/mocks/react-inlinesvg.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function ReactInlineSVG({ src, innerRef, cacheRequests, preProcessor, ...rest }) {
-  return <svg id={src} {...rest} />;
+  return <svg id={src} ref={innerRef} {...rest} />;
 }
 
 export const cacheStore = {};
