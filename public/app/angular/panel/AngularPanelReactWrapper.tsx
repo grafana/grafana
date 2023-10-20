d5b40e20cddeb04af1d6f9387f45b20279eb8990
Dominik Prokop <dominik.prokop@grafana.com>
2023-10-20 15:48:10 +0200

    Fix scenes runtime error for legacy angular panels

--- public/app/angular/panel/AngularPanelReactWrapper.tsx
+++ public/app/angular/panel/AngularPanelReactWrapper.tsx
@@ -3,6 +3,7 @@ import { Observable, ReplaySubject } from 'rxjs';
 
 import { EventBusSrv, PanelData, PanelPlugin, PanelProps, FieldConfigSource } from '@grafana/data';
 import { AngularComponent, getAngularLoader, RefreshEvent } from '@grafana/runtime';
+import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
 import { DashboardModelCompatibilityWrapper } from 'app/features/dashboard-scene/utils/DashboardModelCompatibilityWrapper';
 import { GetDataOptions } from 'app/features/query/state/PanelQueryRunner';
 import { RenderEvent } from 'app/types/events';
