47dcace2d19f231cbae65e4ee557fddd045bd2e2
jev forsberg <jev.forsberg@grafana.com>
2023-10-23 15:58:39 -0600

    baldm0mma/doc_builder/ update joinByLabels

--- public/app/features/transformers/docs/content.ts
+++ public/app/features/transformers/docs/content.ts
@@ -669,60 +669,62 @@ export const transformationDocsContent: Record<string, TransformationInfo> = {
 
   ${buildImageContent('/static/img/docs/transformations/join-fields-before-7-0.png', imageRenderType, this.name + 1)}
 
-I applied a transformation to join the query results using the time field. Now I can run calculations, combine, and organize the results in this new table.
+  I applied a transformation to join the query results using the time field. Now I can run calculations, combine, and organize the results in this new table.
 
   ${buildImageContent('/static/img/docs/transformations/join-fields-after-7-0.png', imageRenderType, this.name + 2)}
   `;
     },
   },
-  // joinByLabels: {
-  //   name: 'Join by labels',
-  //   getHelperDocs: `
-  // Use this transformation to join multiple results into a single table. This is especially useful for converting multiple
-  // time series results into a single wide table with a shared **Label** field.
+  joinByLabels: {
+    name: 'Join by labels',
+    getHelperDocs: function () {
+      return `
+  Use this transformation to join multiple results into a single table. This is especially useful for converting multiple
+  time series results into a single wide table with a shared **Label** field.
 
-  // - **Join** - Select the label to join by between the labels available or common across all time series.
-  // - **Value** - The name for the output result.
+  - **Join** - Select the label to join by between the labels available or common across all time series.
+  - **Value** - The name for the output result.
 
-  // #### Example
+  #### Example
 
-  // ##### Input
+  ##### Input
 
-  // series1{what="Temp", cluster="A", job="J1"}
+  series1{what="Temp", cluster="A", job="J1"}
 
-  // | Time | Value |
-  // | ---- | ----- |
-  // | 1    | 10    |
-  // | 2    | 200   |
+  | Time | Value |
+  | ---- | ----- |
+  | 1    | 10    |
+  | 2    | 200   |
 
-  // series2{what="Temp", cluster="B", job="J1"}
+  series2{what="Temp", cluster="B", job="J1"}
 
-  // | Time | Value |
-  // | ---- | ----- |
-  // | 1    | 10    |
-  // | 2    | 200   |
+  | Time | Value |
+  | ---- | ----- |
+  | 1    | 10    |
+  | 2    | 200   |
 
-  // series3{what="Speed", cluster="B", job="J1"}
+  series3{what="Speed", cluster="B", job="J1"}
 
-  // | Time | Value |
-  // | ---- | ----- |
-  // | 22   | 22    |
-  // | 28   | 77    |
+  | Time | Value |
+  | ---- | ----- |
+  | 22   | 22    |
+  | 28   | 77    |
 
-  // ##### Config
+  ##### Config
 
-  // value: "what"
+  value: "what"
 
-  // ##### Output
+  ##### Output
 
-  // | cluster | job | Temp | Speed |
-  // | ------- | --- | ---- | ----- |
-  // | A       | J1  | 10   |       |
-  // | A       | J1  | 200  |       |
-  // | B       | J1  | 10   | 22    |
-  // | B       | J1  | 200  | 77    |
-  // `,
-  // },
+  | cluster | job | Temp | Speed |
+  | ------- | --- | ---- | ----- |
+  | A       | J1  | 10   |       |
+  | A       | J1  | 200  |       |
+  | B       | J1  | 10   | 22    |
+  | B       | J1  | 200  | 77    |
+  `;
+    },
+  },
   // labelsToFields: {
   //   name: 'Labels to fields',
   //   getHelperDocs: `
