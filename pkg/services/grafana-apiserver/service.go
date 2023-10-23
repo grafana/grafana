6813d49a21754d47ebbce932ec6cddda3a5b0081
Todd Treece <toddtreece@gmail.com>
2023-10-23 17:05:20 -0400

    K8s: Add tracer provider to config

--- pkg/services/grafana-apiserver/service.go
+++ pkg/services/grafana-apiserver/service.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/infra/appcontext"
+	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/middleware"
 	"github.com/grafana/grafana/pkg/modules"
 	"github.com/grafana/grafana/pkg/registry"
@@ -90,6 +91,8 @@ type service struct {
 	handler  web.Handler
 	builders []APIGroupBuilder
 
+	tracing *tracing.TracingService
+
 	authorizer authorizer.Authorizer
 }
 
@@ -97,12 +100,14 @@ func ProvideService(
 	cfg *setting.Cfg,
 	rr routing.RouteRegister,
 	authz authorizer.Authorizer,
+	tracing *tracing.TracingService,
 ) (*service, error) {
 	s := &service{
 		config:     newConfig(cfg),
 		rr:         rr,
 		stopCh:     make(chan struct{}),
 		builders:   []APIGroupBuilder{},
+		tracing:    tracing,
 		authorizer: authz,
 	}
 
@@ -253,6 +258,8 @@ func (s *service) start(ctx context.Context) error {
 		}
 		return genericapiserver.DefaultBuildHandlerChain(requestHandler, c)
 	}
+
+	serverConfig.TracerProvider = s.tracing.GetTracerProvider()
 
 	// Create the server
 	server, err := serverConfig.Complete().New("grafana-apiserver", genericapiserver.NewEmptyDelegate())
