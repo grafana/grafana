18c24cf41ceeac766a58806032a2c79ac4513e92
Shabeeb Khalid <shabeeb.khalid@meetiqm.com>
2023-10-23 10:24:25 +0300

    Use context in aws ListMetricsPages
    Use ListMetricsPagesWithContext and pass context in related sub calls

--- pkg/tsdb/cloudwatch/mocks/list_metrics_service.go
+++ pkg/tsdb/cloudwatch/mocks/list_metrics_service.go
@@ -1,6 +1,8 @@
 package mocks
 
 import (
+	"context"
+
 	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/models/resources"
 	"github.com/stretchr/testify/mock"
 )
@@ -9,19 +11,19 @@ type ListMetricsServiceMock struct {
 	mock.Mock
 }
 
-func (a *ListMetricsServiceMock) GetDimensionKeysByDimensionFilter(r resources.DimensionKeysRequest) ([]resources.ResourceResponse[string], error) {
+func (a *ListMetricsServiceMock) GetDimensionKeysByDimensionFilter(ctx context.Context, r resources.DimensionKeysRequest) ([]resources.ResourceResponse[string], error) {
 	args := a.Called(r)
 
 	return args.Get(0).([]resources.ResourceResponse[string]), args.Error(1)
 }
 
-func (a *ListMetricsServiceMock) GetDimensionValuesByDimensionFilter(r resources.DimensionValuesRequest) ([]resources.ResourceResponse[string], error) {
+func (a *ListMetricsServiceMock) GetDimensionValuesByDimensionFilter(ctx context.Context, r resources.DimensionValuesRequest) ([]resources.ResourceResponse[string], error) {
 	args := a.Called(r)
 
 	return args.Get(0).([]resources.ResourceResponse[string]), args.Error(1)
 }
 
-func (a *ListMetricsServiceMock) GetMetricsByNamespace(r resources.MetricsRequest) ([]resources.ResourceResponse[resources.Metric], error) {
+func (a *ListMetricsServiceMock) GetMetricsByNamespace(ctx context.Context, r resources.MetricsRequest) ([]resources.ResourceResponse[resources.Metric], error) {
 	args := a.Called(r)
 
 	return args.Get(0).([]resources.ResourceResponse[resources.Metric]), args.Error(1)
