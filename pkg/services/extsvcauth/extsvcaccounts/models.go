a3eb534ad3666373bf483ef359ef8db31a5e02f5
linoman <2051016+linoman@users.noreply.github.com>
2023-10-23 14:09:42 +0200

    auth: add serviceaccount proxy (#76815)
    * Add proxy service template
    
    * Replace SA srv with proxy for external SA srv
    
    * Move service account prefix to a constant
    
    * Prevent deletion from external service account
    
    * Make SA validation a resusable function
    
    * Add protection for creating service accounts
    
    * Add protection when updating service accounts
    
    * Add IsExternal field for service account
    
    * Protect ext service account token generation
    
    * Add verbose errors for form name or sa name
    
    * add tests
    
    * Add logs
    
    * Adjusts tests
    
    ---------
    
    Co-authored-by: Misi <mgyongyosi@users.noreply.github.com>
    Co-authored-by: Gabriel MABILLE <gamab@users.noreply.github.com>

--- pkg/services/extsvcauth/extsvcaccounts/models.go
+++ pkg/services/extsvcauth/extsvcaccounts/models.go
@@ -3,13 +3,21 @@ package extsvcaccounts
 import (
 	"github.com/grafana/grafana/pkg/models/roletype"
 	ac "github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/util/errutil"
 )
 
 const (
-	extsvcPrefix = "extsvc-"
+	ExtSvcPrefix = "extsvc-"
 	kvStoreType  = "extsvc-token"
 	// #nosec G101 - this is not a hardcoded secret
 	tokenNamePrefix = "extsvc-token"
+)
+
+var (
+	ErrCannotBeDeleted   = errutil.BadRequest("extsvcaccounts.ErrCannotBeDeleted", errutil.WithPublicMessage("external service account cannot be deleted"))
+	ErrInvalidName       = errutil.BadRequest("extsvcaccounts.ErrInvalidName", errutil.WithPublicMessage("only external service account names can be prefixed with 'extsvc-'"))
+	ErrCannotBeUpdated   = errutil.BadRequest("extsvcaccounts.ErrCannotBeUpdated", errutil.WithPublicMessage("external service account cannot be updated"))
+	ErrCannotCreateToken = errutil.BadRequest("extsvcaccounts.ErrCannotCreateToken", errutil.WithPublicMessage("cannot add external service account token"))
 )
 
 // Credentials represents the credentials associated to an external service
