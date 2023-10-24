88156166f102336853fb34a1104af858ad1bb128
joshhunt <josh@trtr.co>
2023-10-24 16:17:22 +0100

    make existing tests narrower so menu doesn't default to docked. add test for auto docking at large viewport

--- e2e/various-suite/navigation.spec.ts
+++ e2e/various-suite/navigation.spec.ts
@@ -3,6 +3,7 @@ import { fromBaseUrl } from '../utils/support/url';
 
 describe('Docked Navigation', () => {
   beforeEach(() => {
+    cy.viewport(1280, 800);
     e2e.flows.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'));
 
     cy.visit(fromBaseUrl('/'), {
@@ -13,7 +14,7 @@ describe('Docked Navigation', () => {
   });
 
   it('should remain docked when reloading the page', () => {
-    // First, expand the mega menu
+    // Expand, then dock the mega menu
     cy.get('[aria-label="Toggle menu"]').click();
     cy.get('[aria-label="Dock menu"]').click();
 
@@ -24,7 +25,7 @@ describe('Docked Navigation', () => {
   });
 
   it('should remain docked when navigating to another page', () => {
-    // First, expand the mega menu
+    // Expand, then dock the mega menu
     cy.get('[aria-label="Toggle menu"]').click();
     cy.get('[aria-label="Dock menu"]').click();
 
@@ -32,6 +33,15 @@ describe('Docked Navigation', () => {
     e2e.components.NavMenu.Menu().should('be.visible');
 
     cy.contains('a', 'Users').click();
+    e2e.components.NavMenu.Menu().should('be.visible');
+  });
+
+  it('should become docked at larger viewport sizes', () => {
+    e2e.components.NavMenu.Menu().should('not.exist');
+
+    cy.viewport(1920, 1080);
+    cy.reload();
+
     e2e.components.NavMenu.Menu().should('be.visible');
   });
 });
