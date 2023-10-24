e0400cac3f17fc79bab69a0df97d18260255944a
Gabriel MABILLE <gamab@users.noreply.github.com>
2023-10-24 15:54:14 +0200

    AuthN: Add metrics to external service accounts management (#76789)
    * AuthN: Add metrics to external service accounts management
    
    * Add a new metric to count stored external service accounts
    
    * Update variable names
    
    Co-authored-by: linoman <2051016+linoman@users.noreply.github.com>
    
    * Add test to SearchOrgServiceAccounts
    
    * Add feature flags checks before registering and using the metrics
    
    ---------
    
    Co-authored-by: linoman <2051016+linoman@users.noreply.github.com>

--- pkg/services/serviceaccounts/serviceaccounts.go
+++ pkg/services/serviceaccounts/serviceaccounts.go
@@ -13,14 +13,13 @@ Service accounts are used to authenticate API requests. They are not users and
 do not have a password.
 */
 type Service interface {
+	AddServiceAccountToken(ctx context.Context, serviceAccountID int64, cmd *AddServiceAccountTokenCommand) (*apikey.APIKey, error)
 	CreateServiceAccount(ctx context.Context, orgID int64, saForm *CreateServiceAccountForm) (*ServiceAccountDTO, error)
 	DeleteServiceAccount(ctx context.Context, orgID, serviceAccountID int64) error
 	RetrieveServiceAccount(ctx context.Context, orgID, serviceAccountID int64) (*ServiceAccountProfileDTO, error)
 	RetrieveServiceAccountIdByName(ctx context.Context, orgID int64, name string) (int64, error)
-	UpdateServiceAccount(ctx context.Context, orgID, serviceAccountID int64,
-		saForm *UpdateServiceAccountForm) (*ServiceAccountProfileDTO, error)
-	AddServiceAccountToken(ctx context.Context, serviceAccountID int64,
-		cmd *AddServiceAccountTokenCommand) (*apikey.APIKey, error)
+	SearchOrgServiceAccounts(ctx context.Context, query *SearchOrgServiceAccountsQuery) (*SearchOrgServiceAccountsResult, error)
+	UpdateServiceAccount(ctx context.Context, orgID, serviceAccountID int64, saForm *UpdateServiceAccountForm) (*ServiceAccountProfileDTO, error)
 }
 
 //go:generate mockery --name ExtSvcAccountsService --structname MockExtSvcAccountsService --output tests --outpkg tests --filename extsvcaccmock.go
