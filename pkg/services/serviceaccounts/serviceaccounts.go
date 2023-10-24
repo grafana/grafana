34042b8e3bee45bd0eeef30d66295ff361fdbff5
linoman <2051016+linoman@users.noreply.github.com>
2023-10-24 14:03:52 +0200

    expand serviceaccount service interface

--- pkg/services/serviceaccounts/serviceaccounts.go
+++ pkg/services/serviceaccounts/serviceaccounts.go
@@ -13,12 +13,22 @@ Service accounts are used to authenticate API requests. They are not users and
 do not have a password.
 */
 type Service interface {
+	AddServiceAccountToken(ctx context.Context, serviceAccountID int64,
+		cmd *AddServiceAccountTokenCommand) (*apikey.APIKey, error)
 	CreateServiceAccount(ctx context.Context, orgID int64, saForm *CreateServiceAccountForm) (*ServiceAccountDTO, error)
 	DeleteServiceAccount(ctx context.Context, orgID, serviceAccountID int64) error
 	RetrieveServiceAccount(ctx context.Context, orgID, serviceAccountID int64) (*ServiceAccountProfileDTO, error)
 	RetrieveServiceAccountIdByName(ctx context.Context, orgID int64, name string) (int64, error)
 	UpdateServiceAccount(ctx context.Context, orgID, serviceAccountID int64,
 		saForm *UpdateServiceAccountForm) (*ServiceAccountProfileDTO, error)
-	AddServiceAccountToken(ctx context.Context, serviceAccountID int64,
-		cmd *AddServiceAccountTokenCommand) (*apikey.APIKey, error)
+
+	// functions needed for serviceaccounts/api/api.go
+
+	ListTokens(ctx context.Context, query *GetSATokensQuery) ([]apikey.APIKey, error)
+	MigrateApiKey(ctx context.Context, orgID int64, keyId int64) error
+	MigrateApiKeysToServiceAccounts(ctx context.Context, orgID int64) (*MigrationResult, error)
+	SearchOrgServiceAccounts(ctx context.Context, query *SearchOrgServiceAccountsQuery) (*SearchOrgServiceAccountsResult, error)
+
+	// Service account tokens
+	DeleteServiceAccountToken(ctx context.Context, orgID, serviceAccountID, tokenID int64) error
 }
