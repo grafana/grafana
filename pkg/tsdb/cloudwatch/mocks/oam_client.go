a154dd3e7bbca295ffa6d92e0f377f1156da01c3
Shabeeb Khalid <shabeeb.khalid@meetiqm.com>
2023-10-25 18:26:12 +0300

    Fix context

--- pkg/tsdb/cloudwatch/mocks/oam_client.go
+++ pkg/tsdb/cloudwatch/mocks/oam_client.go
@@ -1,7 +1,8 @@
 package mocks
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
+	"context"
+
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/oam"
 	"github.com/stretchr/testify/mock"
@@ -11,12 +12,12 @@ type FakeOAMClient struct {
 	mock.Mock
 }
 
-func (o *FakeOAMClient) ListSinksWithContext(ctx aws.Context, input *oam.ListSinksInput, opts ...request.Option) (*oam.ListSinksOutput, error) {
+func (o *FakeOAMClient) ListSinksWithContext(ctx context.Context, input *oam.ListSinksInput, opts ...request.Option) (*oam.ListSinksOutput, error) {
 	args := o.Called(input)
 	return args.Get(0).(*oam.ListSinksOutput), args.Error(1)
 }
 
-func (o *FakeOAMClient) ListAttachedLinksWithContext(ctx aws.Context, input *oam.ListAttachedLinksInput, opts ...request.Option) (*oam.ListAttachedLinksOutput, error) {
+func (o *FakeOAMClient) ListAttachedLinksWithContext(ctx context.Context, input *oam.ListAttachedLinksInput, opts ...request.Option) (*oam.ListAttachedLinksOutput, error) {
 	args := o.Called(input)
 	return args.Get(0).(*oam.ListAttachedLinksOutput), args.Error(1)
 }
