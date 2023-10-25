1c7b055a240f169c4bbe84e496fb0f067c5b26b4
Ryan McKinley <ryantxu@gmail.com>
2023-10-25 16:56:32 -0700

    rename helper

--- pkg/tests/apis/playlist/playlist_crud_test.go
+++ pkg/tests/apis/playlist/playlist_crud_test.go
@@ -14,7 +14,7 @@ func TestPlaylistCRUD(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
-	ctx := apis.NewK8sTestContext(t)
+	helper := apis.NewK8sTestHelper(t)
 
 	t.Run("Check API Setup", func(t *testing.T) {
 		gvr := schema.GroupVersionResource{
@@ -22,36 +22,36 @@ func TestPlaylistCRUD(t *testing.T) {
 			Version:  "v0alpha1",
 			Resource: "playlists",
 		}
-		g, ok := ctx.Groups[gvr.Group]
+		g, ok := helper.Groups[gvr.Group]
 		require.True(t, ok)
 		require.Equal(t, gvr.Version, g.PreferredVersion.Version)
 
 		// Check view permissions
-		rsp := ctx.List(ctx.Org1.Viewer, gvr, "default")
+		rsp := helper.List(helper.Org1.Viewer, gvr, "default")
 		require.Equal(t, 200, rsp.Response.StatusCode)
 		require.NotNil(t, rsp.Result)
 		require.Empty(t, rsp.Result.Items)
 		require.Nil(t, rsp.Status)
 
 		// Check view permissions
-		rsp = ctx.List(ctx.Org2.Viewer, gvr, "default")
+		rsp = helper.List(helper.Org2.Viewer, gvr, "default")
 		require.Equal(t, 403, rsp.Response.StatusCode) // Org2 can not see default namespace
 		require.Nil(t, rsp.Result)
 		require.Equal(t, metav1.StatusReasonForbidden, rsp.Status.Reason)
 
 		// Check view permissions
-		rsp = ctx.List(ctx.Org2.Viewer, gvr, "org-22")
+		rsp = helper.List(helper.Org2.Viewer, gvr, "org-22")
 		require.Equal(t, 403, rsp.Response.StatusCode) // Unknown/not a member
 		require.Nil(t, rsp.Result)
 		require.Equal(t, metav1.StatusReasonForbidden, rsp.Status.Reason)
 	})
 
 	t.Run("Do simple CRUD via k8s", func(t *testing.T) {
-		v := ctx.LoadAnyResource("testdata/playlist-generate.yaml")
+		v := helper.LoadAnyResource("testdata/playlist-generate.yaml")
 		require.Equal(t, "playlist.grafana.app/v0alpha1", v.APIVersion)
 
 		// Create with auto generated name
-		rsp := ctx.PostResource(ctx.Org1.Editor, "playlists", v)
+		rsp := helper.PostResource(helper.Org1.Editor, "playlists", v)
 		require.Equal(t, 201, rsp.Response.StatusCode) // created!
 		require.NotEmpty(t, rsp.Result.Name)
 		require.Equal(t, "Playlist with auto generated UID", v.Spec["title"])
@@ -60,14 +60,14 @@ func TestPlaylistCRUD(t *testing.T) {
 		// Now Update the title
 		update := rsp.Result
 		update.Spec["title"] = "Change the title"
-		rsp = ctx.PutResource(ctx.Org1.Editor, "playlists", *update)
+		rsp = helper.PutResource(helper.Org1.Editor, "playlists", *update)
 		require.Equal(t, 200, rsp.Response.StatusCode) // OK
 		require.Equal(t, "Change the title", rsp.Result.Spec["title"])
 		require.NotEqual(t, update.ResourceVersion, rsp.Result.ResourceVersion) // should be bigger!
 
 		// Viewer can not update!
 		update.Spec["interval"] = "1m"
-		rsp = ctx.PutResource(ctx.Org1.Viewer, "playlists", *update)
+		rsp = helper.PutResource(helper.Org1.Viewer, "playlists", *update)
 		require.Equal(t, 403, rsp.Response.StatusCode)
 	})
 }
