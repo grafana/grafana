13ca632fa42f97ea53c5d674627d2c4598871652
joshhunt <josh@trtr.co>
2023-10-25 12:29:13 +0100

    fix test

--- public/app/features/alerting/unified/components/admin/AlertmanagerConfig.test.tsx
+++ public/app/features/alerting/unified/components/admin/AlertmanagerConfig.test.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { TestProvider } from 'test/helpers/TestProvider';
 import { byLabelText, byRole, byTestId } from 'testing-library-selector';
 
+import { selectors } from '@grafana/e2e-selectors';
 import { locationService, setDataSourceSrv } from '@grafana/runtime';
 import { contextSrv } from 'app/core/services/context_srv';
 import store from 'app/core/store';
@@ -82,7 +83,7 @@ const ui = {
   confirmButton: byRole('button', { name: /Yes, reset configuration/ }),
   resetButton: byRole('button', { name: /Reset configuration/ }),
   saveButton: byRole('button', { name: /Save/ }),
-  configInput: byLabelText(/Code editor container/),
+  configInput: byTestId(selectors.components.CodeEditor.container),
   readOnlyConfig: byTestId('readonly-config'),
 };
 
