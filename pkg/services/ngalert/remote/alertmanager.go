435d5364731cb7f7742b5c51ab7135f9f84d8d66
Santiago <santiagohernandez.1997@gmail.com>
2023-10-25 10:59:45 +0200

    add debug log line when sending alerts to the remote alertmanager

--- pkg/services/ngalert/remote/alertmanager.go
+++ pkg/services/ngalert/remote/alertmanager.go
@@ -206,6 +206,7 @@ func (am *Alertmanager) GetAlertGroups(ctx context.Context, active, silenced, in
 }
 
 func (am *Alertmanager) PutAlerts(ctx context.Context, alerts apimodels.PostableAlerts) error {
+	am.log.Debug("Sending alerts to a remote alertmanager", "url", am.url, "alerts", len(alerts.PostableAlerts))
 	am.sender.SendAlerts(alerts)
 	return nil
 }
