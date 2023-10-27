13b49860f9165e8a5dd7bab0a800c5732b9e02e5
Matias Chomicki <matyax@gmail.com>
2023-10-27 17:53:29 +0200

    Prettier

--- public/app/features/explore/Logs/LogsContainer.tsx
+++ public/app/features/explore/Logs/LogsContainer.tsx
@@ -101,15 +101,21 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
     }
 
     // Mixed mode.
-    const dsPromises: Array<Promise<{ ds: DataSourceApi, refId: string}>> = [];
+    const dsPromises: Array<Promise<{ ds: DataSourceApi; refId: string }>> = [];
     for (const query of logsQueries) {
-      const mustCheck = !newState.logContextSupport[query.refId] || newState.logContextSupport[query.refId].uid !== query.datasource?.uid;
+      const mustCheck =
+        !newState.logContextSupport[query.refId] ||
+        newState.logContextSupport[query.refId].uid !== query.datasource?.uid;
       if (mustCheck) {
-        dsPromises.push(new Promise((resolve) => {
-          getDataSourceSrv().get(query.datasource).then(ds => {
-            resolve({ ds, refId: query.refId });
+        dsPromises.push(
+          new Promise((resolve) => {
+            getDataSourceSrv()
+              .get(query.datasource)
+              .then((ds) => {
+                resolve({ ds, refId: query.refId });
+              });
           })
-        }));
+        );
       }
     }
 
@@ -119,7 +125,8 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
 
     Promise.all(dsPromises).then((dsInstances) => {
       dsInstances.forEach(({ ds, refId }) => {
-        newState.logDetailsFilterAvailable = newState.logDetailsFilterAvailable || Boolean(ds.modifyQuery) || hasToggleableQueryFiltersSupport(ds);
+        newState.logDetailsFilterAvailable =
+          newState.logDetailsFilterAvailable || Boolean(ds.modifyQuery) || hasToggleableQueryFiltersSupport(ds);
         if (hasLogsContextSupport(ds)) {
           newState.logContextSupport[refId] = ds;
         } else {
