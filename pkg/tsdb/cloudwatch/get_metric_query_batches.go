1161f939a2bbc4771d0729ba43da4f7294f513e7
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2023-10-27 12:00:04 +0200

    used more sets and pass tests

--- pkg/tsdb/cloudwatch/get_metric_query_batches.go
+++ pkg/tsdb/cloudwatch/get_metric_query_batches.go
@@ -13,14 +13,20 @@ var nonWordRegex = regexp.MustCompile(`\W+`)
 // getMetricQueryBatches separates queries into batches if necessary. Metric Insight queries cannot run together, and math expressions must be run
 // with all the queries they reference.
 func getMetricQueryBatches(queries []*models.CloudWatchQuery, logger log.Logger) [][]*models.CloudWatchQuery {
+	// let's deal with IDs and pass them around, then use this following map to convert them back to queries at the end
+	queriesMap := make(map[string]*models.CloudWatchQuery) // maybe don't need pointers everywhere
+	for _, q := range queries {
+		queriesMap[q.Id] = q
+	}
+
 	metricInsightIndices := []int{}
-	mathIndices := []int{}
+	mathQueries := make(map[string]bool)
 	for i, query := range queries {
 		switch query.GetGetMetricDataAPIMode() {
 		case models.GMDApiModeSQLExpression:
 			metricInsightIndices = append(metricInsightIndices, i)
 		case models.GMDApiModeMathExpression:
-			mathIndices = append(mathIndices, i)
+			mathQueries[query.Id] = true
 		default:
 		}
 	}
@@ -30,54 +36,48 @@ func getMetricQueryBatches(queries []*models.CloudWatchQuery, logger log.Logger)
 	}
 
 	logger.Debug("Separating queries into batches")
-	// Map ids to their queries
-	idToIndex := map[string]int{}
-	for i, query := range queries {
-		if query.Id != "" {
-			idToIndex[query.Id] = i
-		}
-	}
 
 	// Find and track which queries are referenced by math queries
-	queryReferences := make([][]int, len(queries))
-	isReferenced := make(map[*models.CloudWatchQuery]bool)
-	for _, idx := range mathIndices {
-		tokens := nonWordRegex.Split(queries[idx].Expression, -1)
-		references := []int{}
+	groupOfReferencedQueries := make(map[string][]string)
+	isReferenced := make(map[string]bool)
+	for mathQueryId := range mathQueries {
+		tokens := nonWordRegex.Split(queriesMap[mathQueryId].Expression, -1)
+		references := []string{}
 		for _, token := range tokens {
-			ref, found := idToIndex[token]
+			_, found := queriesMap[token]
 			if found {
-				references = append(references, ref)
-				isReferenced[queries[ref]] = true
+				references = append(references, token)
+				isReferenced[token] = true
 			}
 		}
-		queryReferences[idx] = references
+		groupOfReferencedQueries[mathQueryId] = references
 	}
 
 	// root query
 	// Create a new batch for every query not used in another query
 	batches := [][]*models.CloudWatchQuery{}
-	for i, query := range queries {
-		if _, ok := isReferenced[query]; !ok {
-			batches = append(batches, getReferencedQueries(queries, queryReferences, i))
+	for _, query := range queries {
+		if _, ok := isReferenced[query.Id]; !ok {
+			batches = append(batches, getReferencedQueries(queriesMap, groupOfReferencedQueries, query.Id))
 		}
 	}
 	return batches
 }
 
 // getReferencedQueries gets all the queries referenced by startQuery and its referenced queries
-func getReferencedQueries(queries []*models.CloudWatchQuery, queryReferences [][]int, rootQueryIndex int) []*models.CloudWatchQuery {
-	usedQueries := make([]bool, len(queries))
+func getReferencedQueries(queries map[string]*models.CloudWatchQuery, queryReferences map[string][]string, rootQueryId string) []*models.CloudWatchQuery {
+	usedQueries := make(map[string]bool)
 	batch := []*models.CloudWatchQuery{}
 
-	queriesToAdd := []int{rootQueryIndex}
-	usedQueries[rootQueryIndex] = true
+	queriesToAdd := []string{rootQueryId}
+	usedQueries[rootQueryId] = true
 	for i := 0; i < len(queriesToAdd); i++ {
-		batch = append(batch, queries[queriesToAdd[i]])
-		for _, queryIdx := range queryReferences[queriesToAdd[i]] {
-			if !usedQueries[queryIdx] {
-				usedQueries[queryIdx] = true
-				queriesToAdd = append(queriesToAdd, queryIdx)
+		currentQueryId := queriesToAdd[i]
+		batch = append(batch, queries[currentQueryId])
+		for _, referencedQueryId := range queryReferences[currentQueryId] {
+			if !usedQueries[referencedQueryId] {
+				usedQueries[referencedQueryId] = true
+				queriesToAdd = append(queriesToAdd, referencedQueryId)
 			}
 		}
 	}
