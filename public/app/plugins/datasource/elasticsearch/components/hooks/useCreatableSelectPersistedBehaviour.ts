c483b8249e2277d60f9086e1e5d7eeeb549cda3f
Matias Chomicki <matyax@gmail.com>
2023-10-30 15:19:16 +0100

    useCreatableSelectPersistedBehaviour: revert change

--- public/app/plugins/datasource/elasticsearch/components/hooks/useCreatableSelectPersistedBehaviour.ts
+++ public/app/plugins/datasource/elasticsearch/components/hooks/useCreatableSelectPersistedBehaviour.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { SelectableValue } from '@grafana/data';
 
@@ -32,11 +32,7 @@ interface Params {
  * and the initial value when it is not present in the option array.
  */
 export const useCreatableSelectPersistedBehaviour = ({ options: initialOptions, value, onChange }: Params) => {
-  const [options, setOptions] = useState<Array<SelectableValue<string>>>([]);
-
-  useEffect(() => {
-    setOptions(getInitialState(initialOptions, value));
-  }, [initialOptions, value]);
+  const [options, setOptions] = useState(getInitialState(initialOptions, value));
 
   const addOption = (newValue: string) => setOptions([...options, { value: newValue, label: newValue }]);
 
