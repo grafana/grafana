4fa2dd303e177ab5dcd0881a27cad144a60f8193
suntala <arati.rana@grafana.com>
2023-11-01 15:13:04 +0100

    Fix mysql fts

--- pkg/api/folder_bench_test.go
+++ pkg/api/folder_bench_test.go
@@ -436,18 +436,24 @@ func setupDBPanelTitle(b testing.TB) benchScenario {
 	for j := 0; j < dashNum; j++ {
 		str := fmt.Sprintf("dashboard_%d", j)
 		dashID := generateID(IDs)
-		dashs = append(dashs, &dashboards.Dashboard{
-			ID:       dashID,
-			OrgID:    signedInUser.OrgID,
-			IsFolder: false,
-			UID:      str,
-			Slug:     str,
-			Title:    str,
-			Data:     simplejson.New(),
-			Created:  now,
-			Updated:  now,
-		})
 
+		panelTitles := fmt.Sprintf("%s panel_%d ", lorem, j)
+		if j == 99999 {
+			panelTitles += "apple "
+		}
+
+		dashs = append(dashs, &dashboards.Dashboard{
+			ID:          dashID,
+			OrgID:       signedInUser.OrgID,
+			IsFolder:    false,
+			UID:         str,
+			Slug:        str,
+			Title:       str,
+			Data:        simplejson.New(),
+			Created:     now,
+			Updated:     now,
+			PanelTitles: panelTitles,
+		})
 	}
 
 	err = db.WithDbSession(context.Background(), func(sess *sqlstore.DBSession) error {
@@ -600,4 +606,21 @@ func generateID(reserved map[int64]struct{}) int64 {
 	return n
 }
 
-const lorem = `Lorem Ipsum is simply dummy text of the printing `
+const lorem = `
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
+and scrambled it to make a type specimen book. It has survived not only five centuries, but also the 
+leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with 
+the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a 
+long established fact that a reader will be distracted by the readable content of a page when looking at 
+its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+as opposed to using Content here, content here, making it look like readable English. Many desktop 
+publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search 
+for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over 
+the years, sometimes by accident, sometimes on purpose (injected humour and the like). Where does it 
+come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of 
+classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor
+ at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a 
+ Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the 
+ undoubtable source. `
--- pkg/services/sqlstore/migrations/panel_mig.go
+++ pkg/services/sqlstore/migrations/panel_mig.go
@@ -8,5 +8,8 @@ func addDashboardPanelMigrations(mg *migrator.Migrator) {
 	mg.AddMigration("Add column panel_titles in dashboard", migrator.NewRawSQLMigration("").
 		SQLite("ALTER TABLE dashboard ADD panel_titles TEXT;").
 		Postgres(`ALTER TABLE dashboard ADD COLUMN panel_titles tsvector;`).
-		Mysql("ALTER TABLE dashboard ADD FULLTEXT(panel_titles);"))
+		Mysql("ALTER TABLE dashboard ADD panel_titles TEXT;"))
+
+	mg.AddMigration("Add full text column panel_titles in dashboard", migrator.NewRawSQLMigration("").
+		Mysql(`ALTER TABLE dashboard ADD FULLTEXT(panel_titles);`))
 }
