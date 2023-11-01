3e42018bb214e18a93dc07367d6f21ab9d5105bc
Ryan McKinley <ryantxu@gmail.com>
2023-11-01 12:46:12 -0700

    move used utils into graph panel

--- packages/grafana-ui/src/components/Graph/types.ts
+++ packages/grafana-ui/src/components/Graph/types.ts
@@ -1,14 +1,4 @@
 /** @deprecated */
-export interface FlotPosition {
-  pageX: number;
-  pageY: number;
-  x: number;
-  x1: number;
-  y: number;
-  y1: number;
-}
-
-/** @deprecated */
 export interface FlotItem<T> {
   datapoint: [number, number];
   dataIndex: number;
