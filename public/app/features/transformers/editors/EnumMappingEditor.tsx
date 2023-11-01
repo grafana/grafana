270d353f1bc09ffdcf51271b30d13ac0485a2560
nmarrs <nathanielmarrs@gmail.com>
2023-10-31 17:05:22 -0700

    refactor enum mapping row as its own component (cleans up a lot, including removing state for tracking current editing index)

--- public/app/features/transformers/editors/EnumMappingEditor.tsx
+++ public/app/features/transformers/editors/EnumMappingEditor.tsx
@@ -1,21 +1,13 @@
 import { css } from '@emotion/css';
 import { isEqual } from 'lodash';
 import React, { useEffect, useState } from 'react';
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 
 import { DataFrame, EnumFieldConfig, GrafanaTheme2 } from '@grafana/data';
 import { ConvertFieldTypeTransformerOptions } from '@grafana/data/src/transformations/transformers/convertFieldType';
-import {
-  Button,
-  FieldValidationMessage,
-  HorizontalGroup,
-  Icon,
-  IconButton,
-  InlineFieldRow,
-  Input,
-  useStyles2,
-  VerticalGroup,
-} from '@grafana/ui';
+import { Button, HorizontalGroup, InlineFieldRow, useStyles2, VerticalGroup } from '@grafana/ui';
+
+import EnumMappingRow from './EnumMappingRow';
 
 type EnumMappingEditorProps = {
   input: DataFrame[];
@@ -30,8 +22,6 @@ export const EnumMappingEditor = ({ input, options, convertFieldTransformIndex, 
   const [enumRows, updateEnumRows] = useState<string[]>(
     options.conversions[convertFieldTransformIndex].enumConfig?.text ?? []
   );
-
-  const [validationError, setValidationError] = useState<string | null>(null);
 
   const onDragEnd = (result: DropResult) => {
     if (!result.destination) {
@@ -51,6 +41,7 @@ export const EnumMappingEditor = ({ input, options, convertFieldTransformIndex, 
 
   useEffect(() => {
     // Generate enum values from scratch when none exist in save model
+    // TODO: consider case when changing target field
     if (!options.conversions[convertFieldTransformIndex].enumConfig?.text?.length && input.length) {
       generateEnumValues();
     }
@@ -107,15 +98,6 @@ export const EnumMappingEditor = ({ input, options, convertFieldTransformIndex, 
     onChangeEnumMapping(index, value);
   };
 
-  // This current approach leads to sticky input (not dismissed when clicking outside input, and only through tabbing)
-  // This can be addressed via adding a outside click handler and keeping track of inputRefs of each row
-  const [editingIndex, setEditingIndex] = useState<number | null>(null);
-  const [editingValue, setEditingValue] = useState<string>('');
-  const setEditRow = (index: number, value: string) => {
-    setEditingIndex(index);
-    setEditingValue(value);
-  };
-
   useEffect(() => {
     const applyEnumConfig = () => {
       const textValues = enumRows.map((value) => value);
@@ -132,6 +114,10 @@ export const EnumMappingEditor = ({ input, options, convertFieldTransformIndex, 
     applyEnumConfig();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [convertFieldTransformIndex, enumRows]);
+
+  const checkIsEnumUniqueValue = (value: string) => {
+    return enumRows.includes(value);
+  };
 
   return (
     <InlineFieldRow>
@@ -154,62 +140,16 @@ export const EnumMappingEditor = ({ input, options, convertFieldTransformIndex, 
                     // Reverse the order of the enum values to match the order of the enum values in the table to the order in the visualization
                     const mappedIndex = enumRows.length - index - 1;
                     return (
-                      <Draggable
+                      <EnumMappingRow
                         key={`${convertFieldTransformIndex}/${value}`}
-                        draggableId={`${convertFieldTransformIndex}/${value}`}
+                        convertFieldTransformIndex={convertFieldTransformIndex}
+                        value={value}
                         index={index}
-                      >
-                        {(provided) => (
-                          <tr key={index} ref={provided.innerRef} {...provided.draggableProps}>
-                            <td>
-                              <div className={styles.dragHandle} {...provided.dragHandleProps}>
-                                <Icon name="draggabledots" size="lg" />
-                              </div>
-                            </td>
-                            {editingIndex === mappedIndex ? (
-                              <td>
-                                <Input
-                                  type="text"
-                                  value={editingValue}
-                                  onChange={(event) => {
-                                    if (
-                                      event.currentTarget.value !== '' &&
-                                      enumRows.includes(event.currentTarget.value)
-                                    ) {
-                                      setValidationError('Enum value already exists');
-                                    } else {
-                                      setValidationError(null);
-                                    }
-
-                                    setEditingValue(event.currentTarget.value);
-                                  }}
-                                  onBlur={() => {
-                                    setEditingIndex(null);
-                                    setValidationError(null);
-                                    onChangeEnumValue(mappedIndex, editingValue);
-                                  }}
-                                />
-                                {validationError && <FieldValidationMessage>{validationError}</FieldValidationMessage>}
-                              </td>
-                            ) : (
-                              <td onClick={() => setEditRow(mappedIndex, value)} className={styles.clickableTableCell}>
-                                {value && value !== '' ? value : 'Click to edit'}
-                              </td>
-                            )}
-                            <td className={styles.textAlignCenter}>
-                              <HorizontalGroup spacing="sm">
-                                <IconButton
-                                  name="trash-alt"
-                                  onClick={() => onRemoveEnumRow(mappedIndex)}
-                                  data-testid="remove-enum-row"
-                                  aria-label="Delete enum row"
-                                  tooltip="Delete"
-                                />
-                              </HorizontalGroup>
-                            </td>
-                          </tr>
-                        )}
-                      </Draggable>
+                        mappedIndex={mappedIndex}
+                        onChangeEnumValue={onChangeEnumValue}
+                        onRemoveEnumRow={onRemoveEnumRow}
+                        checkIsEnumUniqueValue={checkIsEnumUniqueValue}
+                      />
                     );
                   })}
                   {provided.placeholder}
@@ -231,28 +171,7 @@ const getStyles = (theme: GrafanaTheme2) => ({
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(2),
   }),
-  dragHandle: css({
-    cursor: 'grab',
-    // create focus ring around the whole row when the drag handle is tab-focused
-    // needs position: relative on the drag row to work correctly
-    '&:focus-visible&:after': {
-      bottom: 0,
-      content: '""',
-      left: 0,
-      position: 'absolute',
-      right: 0,
-      top: 0,
-      outline: `2px solid ${theme.colors.primary.main}`,
-      outlineOffset: '-2px',
-    },
-  }),
   button: css({
     marginTop: theme.spacing(1),
-  }),
-  textAlignCenter: css({
-    textAlign: 'center',
-  }),
-  clickableTableCell: css({
-    cursor: 'pointer',
   }),
 });
---
+++ public/app/features/transformers/editors/EnumMappingRow.tsx
@@ -0,0 +1,128 @@
+import { css } from '@emotion/css';
+import React, { FormEvent, useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+
+import { GrafanaTheme2 } from '@grafana/data';
+import { Icon, Input, IconButton, HorizontalGroup, FieldValidationMessage, useStyles2 } from '@grafana/ui';
+
+type EnumMappingRowProps = {
+  convertFieldTransformIndex: number;
+  value: string;
+  index: number;
+  mappedIndex: number;
+  onChangeEnumValue: (index: number, value: string) => void;
+  onRemoveEnumRow: (index: number) => void;
+  checkIsEnumUniqueValue: (value: string) => boolean;
+};
+
+const EnumMappingRow = ({
+  convertFieldTransformIndex,
+  value,
+  index,
+  mappedIndex,
+  onChangeEnumValue,
+  onRemoveEnumRow,
+  checkIsEnumUniqueValue,
+}: EnumMappingRowProps) => {
+  const styles = useStyles2(getStyles);
+
+  // This current approach leads to sticky input (not dismissed when clicking outside input, and only through tabbing)
+  // This can be addressed via adding a outside click handler and keeping track of inputRefs of each rows
+  const [enumValue, setEnumValue] = useState<string>(value);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const [validationError, setValidationError] = useState<string | null>(null);
+
+  const onEnumInputChange = (event: FormEvent<HTMLInputElement>) => {
+    if (
+      event.currentTarget.value !== '' &&
+      checkIsEnumUniqueValue(event.currentTarget.value) &&
+      event.currentTarget.value !== value
+    ) {
+      setValidationError('Enum value already exists');
+    } else {
+      setValidationError(null);
+    }
+
+    setEnumValue(event.currentTarget.value);
+  };
+
+  const onEnumInputBlur = () => {
+    setIsEditing(false);
+    setValidationError(null);
+    onChangeEnumValue(mappedIndex, enumValue);
+  };
+
+  const onEnumValueClick = () => {
+    setIsEditing(true);
+  };
+
+  const onRemoveButtonClick = () => {
+    onRemoveEnumRow(mappedIndex);
+  };
+
+  return (
+    <Draggable
+      key={`${convertFieldTransformIndex}/${value}`}
+      draggableId={`${convertFieldTransformIndex}/${value}`}
+      index={index}
+    >
+      {(provided) => (
+        <tr key={index} ref={provided.innerRef} {...provided.draggableProps}>
+          <td>
+            <div className={styles.dragHandle} {...provided.dragHandleProps}>
+              <Icon name="draggabledots" size="lg" />
+            </div>
+          </td>
+          {isEditing ? (
+            <td>
+              <Input type="text" value={enumValue} onChange={onEnumInputChange} onBlur={onEnumInputBlur} />
+              {validationError && <FieldValidationMessage>{validationError}</FieldValidationMessage>}
+            </td>
+          ) : (
+            <td onClick={onEnumValueClick} className={styles.clickableTableCell}>
+              {value && value !== '' ? value : 'Click to edit'}
+            </td>
+          )}
+          <td className={styles.textAlignCenter}>
+            <HorizontalGroup spacing="sm">
+              <IconButton
+                name="trash-alt"
+                onClick={onRemoveButtonClick}
+                data-testid="remove-enum-row"
+                aria-label="Delete enum row"
+                tooltip="Delete"
+              />
+            </HorizontalGroup>
+          </td>
+        </tr>
+      )}
+    </Draggable>
+  );
+};
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  dragHandle: css({
+    cursor: 'grab',
+    // create focus ring around the whole row when the drag handle is tab-focused
+    // needs position: relative on the drag row to work correctly
+    '&:focus-visible&:after': {
+      bottom: 0,
+      content: '""',
+      left: 0,
+      position: 'absolute',
+      right: 0,
+      top: 0,
+      outline: `2px solid ${theme.colors.primary.main}`,
+      outlineOffset: '-2px',
+    },
+  }),
+  textAlignCenter: css({
+    textAlign: 'center',
+  }),
+  clickableTableCell: css({
+    cursor: 'pointer',
+  }),
+});
+
+export default EnumMappingRow;
