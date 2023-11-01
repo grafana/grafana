384f5ccdc6be681cb8298d2a1690f052031db64b
Todd Treece <360020+toddtreece@users.noreply.github.com>
2023-11-01 09:44:04 -0400

    Playlist: Add internal API version (#77318)

--- pkg/apis/playlist/register.go
+++ pkg/apis/playlist/register.go
@@ -1,23 +1,29 @@
-package v0alpha1
+package playlist
 
 import (
+	"fmt"
+	"time"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apiserver/pkg/registry/generic"
+	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	common "k8s.io/kube-openapi/pkg/common"
 
 	grafanaapiserver "github.com/grafana/grafana/pkg/services/grafana-apiserver"
 	"github.com/grafana/grafana/pkg/services/grafana-apiserver/endpoints/request"
+	grafanarest "github.com/grafana/grafana/pkg/services/grafana-apiserver/rest"
+	"github.com/grafana/grafana/pkg/services/grafana-apiserver/utils"
 	"github.com/grafana/grafana/pkg/services/playlist"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 // GroupName is the group name for this API.
 const GroupName = "playlist.grafana.app"
-const VersionID = "v0alpha1"
+const VersionID = runtime.APIVersionInternal
 
 var _ grafanaapiserver.APIGroupBuilder = (*PlaylistAPIBuilder)(nil)
 
@@ -50,11 +56,7 @@ func (b *PlaylistAPIBuilder) InstallSchema(scheme *runtime.Scheme) error {
 		&Playlist{},
 		&PlaylistList{},
 	)
-	if err := RegisterConversions(scheme); err != nil {
-		return err
-	}
-	metav1.AddToGroupVersion(scheme, b.gv)
-	return scheme.SetVersionPriority(b.gv)
+	return nil
 }
 
 func (b *PlaylistAPIBuilder) GetAPIGroupInfo(
@@ -62,11 +64,53 @@ func (b *PlaylistAPIBuilder) GetAPIGroupInfo(
 	codecs serializer.CodecFactory, // pointer?
 	optsGetter generic.RESTOptionsGetter,
 ) (*genericapiserver.APIGroupInfo, error) {
-	return nil, nil
+	apiGroupInfo := genericapiserver.NewDefaultAPIGroupInfo(GroupName, scheme, metav1.ParameterCodec, codecs)
+	storage := map[string]rest.Storage{}
+
+	legacyStore := &legacyStorage{
+		service:                   b.service,
+		namespacer:                b.namespacer,
+		DefaultQualifiedResource:  b.gv.WithResource("playlists").GroupResource(),
+		SingularQualifiedResource: b.gv.WithResource("playlist").GroupResource(),
+	}
+	legacyStore.tableConverter = utils.NewTableConverter(
+		legacyStore.DefaultQualifiedResource,
+		[]metav1.TableColumnDefinition{
+			{Name: "Name", Type: "string", Format: "name"},
+			{Name: "Title", Type: "string", Format: "string", Description: "The playlist name"},
+			{Name: "Interval", Type: "string", Format: "string", Description: "How often the playlist will update"},
+			{Name: "Created At", Type: "date"},
+		},
+		func(obj runtime.Object) ([]interface{}, error) {
+			m, ok := obj.(*Playlist)
+			if !ok {
+				return nil, fmt.Errorf("expected playlist")
+			}
+			return []interface{}{
+				m.Name,
+				m.Spec.Title,
+				m.Spec.Interval,
+				m.CreationTimestamp.UTC().Format(time.RFC3339),
+			}, nil
+		},
+	)
+	storage["playlists"] = legacyStore
+
+	// enable dual writes if a RESTOptionsGetter is provided
+	if optsGetter != nil {
+		store, err := newStorage(scheme, optsGetter, legacyStore)
+		if err != nil {
+			return nil, err
+		}
+		storage["playlists"] = grafanarest.NewDualWriter(legacyStore, store)
+	}
+
+	apiGroupInfo.VersionedResourcesStorageMap["v0alpha1"] = storage
+	return &apiGroupInfo, nil
 }
 
 func (b *PlaylistAPIBuilder) GetOpenAPIDefinitions() common.GetOpenAPIDefinitions {
-	return getOpenAPIDefinitions
+	return nil // no custom OpenAPI definitions
 }
 
 func (b *PlaylistAPIBuilder) GetAPIRoutes() *grafanaapiserver.APIRoutes {
