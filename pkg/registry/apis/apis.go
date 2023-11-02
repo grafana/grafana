536b7d15b76c1733855712e963c09214ced129e0
Ryan McKinley <ryantxu@gmail.com>
2023-11-02 11:53:49 -0700

    remove the duplicate internal version

--- pkg/registry/apis/apis.go
+++ pkg/registry/apis/apis.go
@@ -6,7 +6,6 @@ import (
 	"github.com/grafana/grafana/pkg/registry"
 	"github.com/grafana/grafana/pkg/registry/apis/example"
 	"github.com/grafana/grafana/pkg/registry/apis/playlist"
-	playlistV0 "github.com/grafana/grafana/pkg/registry/apis/playlist/v0alpha1"
 )
 
 var (
@@ -18,7 +17,7 @@ type Service struct{}
 // ProvideRegistryServiceSink is an entry point for each service that will force initialization
 // and give each builder the chance to register itself with the main server
 func ProvideRegistryServiceSink(
-	_ *playlistV0.PlaylistAPIBuilder,
+	//_ *playlistV0.PlaylistAPIBuilder,
 	_ *playlist.PlaylistAPIBuilder,
 	_ *example.TestingAPIBuilder,
 ) *Service {
