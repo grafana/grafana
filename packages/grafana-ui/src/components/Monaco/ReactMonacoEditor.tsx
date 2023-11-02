7e20c9e1011ceb9f1608bfed41f7904acc9722ee
Jack Westbrook <jack.westbrook@gmail.com>
2023-11-02 17:38:28 +0100

    feat(codeeditor): use esm to load monaco editor

--- packages/grafana-ui/src/components/Monaco/ReactMonacoEditor.tsx
+++ packages/grafana-ui/src/components/Monaco/ReactMonacoEditor.tsx
@@ -1,4 +1,5 @@
-import MonacoEditor, { loader as monacoEditorLoader, Monaco } from '@monaco-editor/react';
+import Editor, { loader as monacoEditorLoader, Monaco } from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
 import React, { useCallback } from 'react';
 
 import { useTheme2 } from '../../themes';
@@ -6,11 +7,39 @@ import { useTheme2 } from '../../themes';
 import defineThemes from './theme';
 import type { ReactMonacoEditorProps } from './types';
 
-monacoEditorLoader.config({
-  paths: {
-    vs: (window.__grafana_public_path__ ?? 'public/') + 'lib/monaco/min/vs',
+monacoEditorLoader.config({ monaco });
+
+self.MonacoEnvironment = {
+  getWorker(_moduleId, label) {
+    if (label === 'json') {
+      return new Worker(
+        /* webpackChunkName: "json-worker" */ new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url)
+      );
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new Worker(
+        /* webpackChunkName: "css-worker" */ new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url)
+      );
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new Worker(
+        /* webpackChunkName: "html-worker" */ new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url)
+      );
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new Worker(
+        /* webpackChunkName: "typescript-worker" */ new URL(
+          'monaco-editor/esm/vs/language/typescript/ts.worker',
+          import.meta.url
+        )
+      );
+    }
+
+    return new Worker(
+      /* webpackChunkName: "editor-worker" */ new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url)
+    );
   },
-});
+};
 
 export const ReactMonacoEditor = (props: ReactMonacoEditorProps) => {
   const { beforeMount } = props;
@@ -25,10 +54,6 @@ export const ReactMonacoEditor = (props: ReactMonacoEditorProps) => {
   );
 
   return (
-    <MonacoEditor
-      {...props}
-      theme={theme.isDark ? 'grafana-dark' : 'grafana-light'}
-      beforeMount={onMonacoBeforeMount}
-    />
+    <Editor {...props} theme={theme.isDark ? 'grafana-dark' : 'grafana-light'} beforeMount={onMonacoBeforeMount} />
   );
 };
