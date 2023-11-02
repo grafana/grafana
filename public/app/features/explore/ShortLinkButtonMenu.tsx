a2d1c3dcc4d71a87e4893a2c58992d5a1b8b548b
Kristina Durivage <kristina.durivage@grafana.com>
2023-11-02 18:21:09 -0500

    Perist last clicked, add translations

--- public/app/features/explore/ShortLinkButtonMenu.tsx
+++ public/app/features/explore/ShortLinkButtonMenu.tsx
@@ -1,11 +1,11 @@
 import { css } from '@emotion/css';
 import React, { useState } from 'react';
 
-import { GrafanaTheme2, urlUtil } from '@grafana/data';
-import { Stack } from '@grafana/experimental';
+import { GrafanaTheme2, IconName, urlUtil } from '@grafana/data';
 import { reportInteraction } from '@grafana/runtime';
-import { useStyles2, ToolbarButton, Dropdown, Menu } from '@grafana/ui';
+import { useStyles2, ToolbarButton, Dropdown, Menu, Stack, ToolbarButtonRow } from '@grafana/ui';
 import { t } from 'app/core/internationalization';
+import store from 'app/core/store';
 import { createAndCopyShortLink } from 'app/core/utils/shortLinks';
 import { useSelector } from 'app/types';
 
@@ -16,79 +16,123 @@ export function ShortLinkButtonMenu() {
   const styles = useStyles2(getStyles);
   const panes = useSelector(selectPanes);
   const [isOpen, setIsOpen] = useState(false);
-  const onCopyShortLink = (url?: string) => {
+  const onCopyShortLink = (persistType?: string, url?: string) => {
+    if (persistType !== undefined) {
+      store.set('grafana.explore.shortLinkShareType', persistType);
+    }
     createAndCopyShortLink(url || global.location.href);
     reportInteraction('grafana_explore_shortened_link_clicked');
   };
 
+  interface ShortLinkMenuItemData {
+    key: string;
+    label: string;
+    icon: IconName;
+    getUrl: Function;
+    description?: string;
+  }
+
+  const menuOptions: ShortLinkMenuItemData[] = [
+    {
+      key: 'copy-link',
+      label: t('explore.toolbar.copy-shortened-link', 'Copy shortened link'),
+      icon: 'link',
+      getUrl: () => undefined,
+    },
+    {
+      key: 'copy-link-abs-time',
+      label: t('explore.toolbar.copy-shortened-link-abs-time', 'Copy shortened link with absolute time'),
+      icon: 'clock-nine',
+      description: t(
+        'explore.toolbar.copy-shortened-link-abs-time-desc',
+        'Locks the current time for consistent viewing'
+      ),
+      getUrl: () => {
+        const urlStates = Object.entries(panes)
+          .map(([exploreId, pane]) => {
+            if (!pane) {
+              return;
+            }
+            const urlState = getUrlStateFromPaneState(pane);
+            urlState.range = {
+              to: pane.range.to.valueOf().toString(),
+              from: pane.range.from.valueOf().toString(),
+            };
+            return JSON.stringify({ [exploreId]: { ...urlState } });
+          })
+          .filter((urlState): urlState is string => urlState !== undefined);
+        return urlUtil.renderUrl('/explore', { panes: urlStates, schemaVersion: 1 });
+      },
+    },
+  ];
+
   const MenuActions = () => {
     return (
       <Menu>
-        <Menu.Item
-          key="copy-link"
-          label="Copy shortened URL"
-          icon="link"
-          onClick={() => onCopyShortLink()}
-          className={styles.menuItem}
-        />
-        <Menu.Item
-          key="copy-link-abs-time"
-          label="Copy absolute time URL"
-          icon="clock-nine"
-          description="Locks the current time for consistent viewing"
-          onClick={() => {
-            const exploreIds = Object.keys(panes);
-            const urlStates = Object.values(panes)
-              .map((pane, i) => {
-                if (pane) {
-                  const urlState = getUrlStateFromPaneState(pane);
-                  urlState.range = {
-                    to: pane.range.to.valueOf().toString(),
-                    from: pane.range.from.valueOf().toString(),
-                  };
-                  return JSON.stringify({ [exploreIds[i]]: { ...urlState } });
-                } else {
-                  return undefined;
-                }
-              })
-              .filter((urlState): urlState is string => urlState !== undefined);
-            onCopyShortLink(urlUtil.renderUrl('/explore', { panes: urlStates, schemaVersion: 1 }));
-          }}
-          className={styles.menuItem}
-        />
+        {menuOptions.map((option) => {
+          return (
+            <Menu.Item
+              key={option.key}
+              label={option.label}
+              icon={option.icon}
+              onClick={() => {
+                const url = option.getUrl();
+                onCopyShortLink(option.key, url);
+              }}
+              description={option.description}
+              className={styles.menuItem}
+            />
+          );
+        })}
       </Menu>
     );
   };
 
+  const buttonMode = (() => {
+    const defaultMode: ShortLinkMenuItemData = {
+      key: 'copy-link',
+      label: '',
+      icon: 'share-alt',
+      getUrl: () => undefined,
+    };
+    const savedModeKey = store.get('grafana.explore.shortLinkShareType');
+    if (savedModeKey !== undefined) {
+      const foundMode = menuOptions.find((option) => option.key === savedModeKey);
+      return foundMode || defaultMode;
+    } else {
+      return defaultMode;
+    }
+  })();
+
   // we need the Toolbar button click to be an action separate from opening/closing the menu
   return (
-    <Stack gap={0} direction="row" alignItems="baseline" wrap={false}>
-      <ToolbarButton
-        className={styles.buttonContainer}
-        tooltip={t('explore.toolbar.copy-shortened-link', 'Copy shortened link')}
-        icon="share-alt"
-        iconOnly={true}
-        narrow={true}
-        onClick={() => onCopyShortLink()}
-        aria-label={t('explore.toolbar.copy-shortened-link', 'Copy shortened link')}
-      />
-      <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={setIsOpen}>
+    <ToolbarButtonRow>
+      <Stack gap={0} direction="row" alignItems="center" wrap="nowrap">
         <ToolbarButton
-          className={styles.buttonContainer}
+          tooltip={t('explore.toolbar.copy-shortened-link', 'Copy shortened link')}
+          icon={buttonMode.icon}
+          iconOnly={true}
           narrow={true}
-          isOpen={isOpen}
+          onClick={() => {
+            const url = buttonMode.getUrl();
+            onCopyShortLink(buttonMode.key, url);
+          }}
           aria-label={t('explore.toolbar.copy-shortened-link', 'Copy shortened link')}
         />
-      </Dropdown>
-    </Stack>
+        <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={setIsOpen}>
+          <ToolbarButton
+            narrow={true}
+            isOpen={isOpen}
+            aria-label={t('explore.toolbar.copy-shortened-link-menu', 'Open copy link options')}
+          />
+        </Dropdown>
+      </Stack>
+    </ToolbarButtonRow>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => {
   return {
-    buttonContainer: css({
-      backgroundColor: theme.colors.background.canvas,
-    }),
     menuItem: css({
       alignItems: 'start',
       '>:nth-child(2)': {
