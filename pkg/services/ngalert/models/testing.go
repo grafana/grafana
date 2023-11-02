0d4639667c713b15ccc1f5096c6eecfb53a3c02e
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-11-02 15:33:26 -0400

    tests

--- pkg/services/ngalert/models/testing.go
+++ pkg/services/ngalert/models/testing.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"math/rand"
 	"sync"
+	"testing"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/expr"
 	"github.com/grafana/grafana/pkg/services/folder"
@@ -425,6 +427,46 @@ func CreateClassicConditionExpression(refID string, inputRefID string, reducer s
             ]
 		}`, refID, inputRefID, operation, threshold, reducer, expr.DatasourceUID, expr.DatasourceType)),
 	}
+}
+
+func CreateHysteresisExpression(t *testing.T, refID string, inputRefID string, threshold int, recoveryThreshold int) AlertQuery {
+	t.Helper()
+	q := AlertQuery{
+		RefID:         refID,
+		QueryType:     expr.DatasourceType,
+		DatasourceUID: expr.DatasourceUID,
+		Model: json.RawMessage(fmt.Sprintf(`
+		{
+			"refId": "%[1]s",
+            "type": "threshold",
+            "datasource": {
+                "uid": "%[5]s",
+                "type": "%[6]s"
+            },
+			"expression": "%[2]s",
+            "conditions": [
+                {
+                    "type": "query",
+                    "evaluator": {
+                        "params": [
+                            %[3]d
+                        ],
+                        "type": "gt"
+                    },
+					"unloadEvaluator": {
+                        "params": [
+                            %[4]d
+                        ],
+                        "type": "lt"
+					}
+                }
+            ]
+		}`, refID, inputRefID, threshold, recoveryThreshold, expr.DatasourceUID, expr.DatasourceType)),
+	}
+	h, err := q.IsHysteresisExpression()
+	require.NoError(t, err)
+	require.Truef(t, h, "test model is expected to be a hysteresis expression")
+	return q
 }
 
 type AlertInstanceMutator func(*AlertInstance)
