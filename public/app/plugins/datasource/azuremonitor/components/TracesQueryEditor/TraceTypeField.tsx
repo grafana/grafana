4dc0ad33df0a03617488ffaf1198be7dd38aab2a
alyssabull <alyssabull@gmail.com>
2023-11-02 20:15:31 -0600

    Lint

--- public/app/plugins/datasource/azuremonitor/components/TracesQueryEditor/TraceTypeField.tsx
+++ public/app/plugins/datasource/azuremonitor/components/TracesQueryEditor/TraceTypeField.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { SelectableValue } from '@grafana/data';
 import { MultiSelect } from '@grafana/ui';
