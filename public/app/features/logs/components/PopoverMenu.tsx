26ec3ce2b3c8e548da3ef1c9e4e586f4a7b75a5e
Matias Chomicki <matyax@gmail.com>
2023-11-03 13:31:32 +0100

    Popover menu: track interactions

--- public/app/features/logs/components/PopoverMenu.tsx
+++ public/app/features/logs/components/PopoverMenu.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/css';
 import React, { useEffect, useRef } from 'react';
 
 import { GrafanaTheme2, LogRowModel } from '@grafana/data';
+import { reportInteraction } from '@grafana/runtime';
 import { Menu, useStyles2 } from '@grafana/ui';
 
 import { copyText } from '../utils';
@@ -55,6 +56,7 @@ export const PopoverMenu = ({
           onClick={() => {
             copyText(selection, containerRef);
             close();
+            track('copy_selection_clicked', selection.length);
           }}
         />
         {onClickFilterValue && (
@@ -63,6 +65,7 @@ export const PopoverMenu = ({
             onClick={() => {
               onClickFilterValue(selection, row.dataFrame.refId);
               close();
+              track('line_filter_clicked', selection.length);
             }}
           />
         )}
@@ -72,6 +75,7 @@ export const PopoverMenu = ({
             onClick={() => {
               onClickFilterOutValue(selection, row.dataFrame.refId);
               close();
+              track('line_filter_out_clicked', selection.length);
             }}
           />
         )}
@@ -79,6 +83,12 @@ export const PopoverMenu = ({
     </div>
   );
 };
+
+function track(event: string, selectionLength: number) {
+  reportInteraction(`grafana_explore_logs_${event}`, {
+    selectionLength,
+  });
+}
 
 const getStyles = (theme: GrafanaTheme2) => ({
   menu: css({
