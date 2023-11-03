f3d5683d6f4963c0622df4b6de86a4517620b270
João Calisto <joao.calisto@grafana.com>
2023-11-03 15:42:20 +0000

    Update pkg/services/featuremgmt/models.go
    Co-authored-by: Michael Mandrus <41969079+mmandrus@users.noreply.github.com>

--- pkg/services/featuremgmt/models.go
+++ pkg/services/featuremgmt/models.go
@@ -115,7 +115,7 @@ type FeatureFlag struct {
 	FrontendOnly      bool  `json:"frontend,omitempty"`          // change is only seen in the frontend
 	HideFromDocs      bool  `json:"hideFromDocs,omitempty"`      // don't add the values to docs
 	HideFromAdminPage bool  `json:"hideFromAdminPage,omitempty"` // don't display the feature in the admin page - add a comment with the reasoning
-	AllowSelfServe    *bool `json:"allowSelfServe,omitempty"`    // allow admin users to toggle the feature state from the admin page
+	AllowSelfServe    *bool `json:"allowSelfServe,omitempty"`    // allow admin users to toggle the feature state from the admin page; this is required for GA toggles only
 
 	// This field is only for the feature management API. To enable your feature toggle by default, use `Expression`.
 	Enabled bool `json:"enabled,omitempty"`
