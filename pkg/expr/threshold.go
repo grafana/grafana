c090d3a5964b8b4633b764e747b1296e4fccc625
GitHamburg <jsonsea@foxmail.com>
2023-11-06 10:20:07 +0800

    Alerting: fix alarm condition functions's conflicting files

--- pkg/expr/threshold.go
+++ pkg/expr/threshold.go
@@ -133,31 +133,37 @@ func (tc *ThresholdCommand) Execute(ctx context.Context, now time.Time, vars mat
 }
 
 // createMathExpression converts all the info we have about a "threshold" expression in to a Math expression
-func createMathExpression(referenceVar string, thresholdFunc string, args []float64) (string, error) {
+func createMathExpression(referenceVar string, thresholdFunc string, args []float64, invert bool) (string, error) {
+	var exp string
 	switch thresholdFunc {
 	case ThresholdIsAbove:
-		return fmt.Sprintf("${%s} > %f", referenceVar, args[0]), nil
+		exp = fmt.Sprintf("${%s} > %f", referenceVar, args[0])
 	case ThresholdIsBelow:
-		return fmt.Sprintf("${%s} < %f", referenceVar, args[0]), nil
+		exp = fmt.Sprintf("${%s} < %f", referenceVar, args[0])
 	case ThresholdIsEqual:
-		return fmt.Sprintf("${%s} == %f", referenceVar, args[0]), nil
+		exp = fmt.Sprintf("${%s} == %f", referenceVar, args[0])
 	case ThresholdIsNotEqual:
-		return fmt.Sprintf("${%s} != %f", referenceVar, args[0]), nil
+		exp = fmt.Sprintf("${%s} != %f", referenceVar, args[0])
 	case ThresholdIsGe:
-		return fmt.Sprintf("${%s} >= %f", referenceVar, args[0]), nil
+		exp = fmt.Sprintf("${%s} >= %f", referenceVar, args[0])
 	case ThresholdIsLe:
-		return fmt.Sprintf("${%s} <= %f", referenceVar, args[0]), nil
+		exp = fmt.Sprintf("${%s} <= %f", referenceVar, args[0])
 	case ThresholdIsWithinRange:
-		return fmt.Sprintf("${%s} > %f && ${%s} < %f", referenceVar, args[0], referenceVar, args[1]), nil
+		exp = fmt.Sprintf("${%s} > %f && ${%s} < %f", referenceVar, args[0], referenceVar, args[1])
 	case ThresholdIsWithinRangeIncluded:
-		return fmt.Sprintf("${%s} >= %f && ${%s} <= %f", referenceVar, args[0], referenceVar, args[1]), nil
+		exp = fmt.Sprintf("${%s} >= %f && ${%s} <= %f", referenceVar, args[0], referenceVar, args[1])
 	case ThresholdIsOutsideRange:
-		return fmt.Sprintf("${%s} < %f || ${%s} > %f", referenceVar, args[0], referenceVar, args[1]), nil
+		exp = fmt.Sprintf("${%s} < %f || ${%s} > %f", referenceVar, args[0], referenceVar, args[1])
 	case ThresholdIsOutsideRangeIncluded:
-		return fmt.Sprintf("${%s} <= %f || ${%s} >= %f", referenceVar, args[0], referenceVar, args[1]), nil
+		exp = fmt.Sprintf("${%s} <= %f || ${%s} >= %f", referenceVar, args[0], referenceVar, args[1])
 	default:
 		return "", fmt.Errorf("failed to evaluate threshold expression: no such threshold function %s", thresholdFunc)
 	}
+
+	if invert {
+		return fmt.Sprintf("!(%s)", exp), nil
+	}
+	return exp, nil
 }
 
 func IsSupportedThresholdFunc(name string) bool {
