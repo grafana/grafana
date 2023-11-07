9e54012407142ab5b76db9a38231672f1b1615ff
Galen Kistler <109082771+gtk-grafana@users.noreply.github.com>
2023-11-07 08:00:06 -0600

    Logs Panel: Table UI - Add time and body fields to column selection in logs table visualization (#77468)
    * add the ability to set visibility of time and body fields, set default of time and body fields when no other fields are selected

--- public/app/features/explore/Logs/LogsTableWrap.tsx
+++ public/app/features/explore/Logs/LogsTableWrap.tsx
@@ -34,7 +34,11 @@ interface Props extends Themeable2 {
   onClickFilterOutLabel?: (key: string, value: string, refId?: string) => void;
 }
 
-export type fieldNameMeta = { percentOfLinesWithLabel: number; active: boolean | undefined };
+export type fieldNameMeta = {
+  percentOfLinesWithLabel: number;
+  active: boolean | undefined;
+  type?: 'BODY_FIELD' | 'TIME_FIELD';
+};
 type fieldName = string;
 type fieldNameMetaStore = Record<fieldName, fieldNameMeta>;
 
@@ -96,13 +100,27 @@ export function LogsTableWrap(props: Props) {
    *
    */
   useEffect(() => {
+    // If the data frame is empty, there's nothing to viz, it could mean the user has unselected all columns
+    if (!dataFrame.length) {
+      return;
+    }
     const numberOfLogLines = dataFrame ? dataFrame.length : 0;
     const logsFrame = parseLogsFrame(dataFrame);
     const labels = logsFrame?.getLogFrameLabelsAsLabels();
 
-    const otherFields = logsFrame ? logsFrame.extraFields.filter((field) => !field?.config?.custom?.hidden) : [];
+    const otherFields = [];
+
+    if (logsFrame) {
+      otherFields.push(...logsFrame.extraFields.filter((field) => !field?.config?.custom?.hidden));
+    }
     if (logsFrame?.severityField) {
       otherFields.push(logsFrame?.severityField);
+    }
+    if (logsFrame?.bodyField) {
+      otherFields.push(logsFrame?.bodyField);
+    }
+    if (logsFrame?.timeField) {
+      otherFields.push(logsFrame?.timeField);
     }
 
     // Use a map to dedupe labels and count their occurrences in the logs
@@ -158,6 +176,24 @@ export function LogsTableWrap(props: Props) {
     });
 
     pendingLabelState = getColumnsFromProps(pendingLabelState);
+
+    // Get all active columns
+    const active = Object.keys(pendingLabelState).filter((key) => pendingLabelState[key].active);
+
+    // If nothing is selected, then select the default columns
+    if (active.length === 0) {
+      if (logsFrame?.bodyField?.name) {
+        pendingLabelState[logsFrame.bodyField.name].active = true;
+      }
+      if (logsFrame?.timeField?.name) {
+        pendingLabelState[logsFrame.timeField.name].active = true;
+      }
+    }
+
+    if (logsFrame?.bodyField?.name && logsFrame?.timeField?.name) {
+      pendingLabelState[logsFrame.bodyField.name].type = 'BODY_FIELD';
+      pendingLabelState[logsFrame.timeField.name].type = 'TIME_FIELD';
+    }
 
     setColumnsWithMeta(pendingLabelState);
 
