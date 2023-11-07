7b9403b195cee66961731e9e8f05e2939bbb807c
Kyle Cunningham <kyle@codeincarnate.com>
2023-11-07 17:52:56 -0600

    Improve time series detection

--- packages/grafana-data/src/dataframe/utils.ts
+++ packages/grafana-data/src/dataframe/utils.ts
@@ -12,6 +12,31 @@ export function isTimeSeriesFrame(frame: DataFrame) {
   // and at least one number field
   const timeField = frame.fields.find((field) => field.type === FieldType.time);
   const numberField = frame.fields.find((field) => field.type === FieldType.number);
+
+  
+  // There are certain query types in which we will
+  // get times but they will be the same or not be
+  // in increasing order. To have a time-series the
+  // times need to be ordered from past to present
+  if (timeField !== undefined) {
+    let greatestTime: number | null = null;
+
+
+    for (let i = 0; i < timeField.values.length; i++) {
+      const time = timeField.values[i];
+
+      // Check to see if the current time is greater than
+      // the great time. If we get to the end then we 
+      // have a time series otherwise we return false
+      if (greatestTime === null || time > greatestTime) {
+        greatestTime = time;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+
   return timeField !== undefined && numberField !== undefined;
 }
 
