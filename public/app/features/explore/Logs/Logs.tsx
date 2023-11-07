097f52022f47254f26e8fb12dce834aa44d654c3
Matias Chomicki <matyax@gmail.com>
2023-11-07 18:32:36 +0100

    Load more: remove refIds from signature
    We can resolve them inside Explore state

--- public/app/features/explore/Logs/Logs.tsx
+++ public/app/features/explore/Logs/Logs.tsx
@@ -105,7 +105,7 @@ interface Props extends Themeable2 {
   range: TimeRange;
   onClickFilterValue?: (value: string, refId?: string) => void;
   onClickFilterOutValue?: (value: string, refId?: string) => void;
-  loadMore?(range: AbsoluteTimeRange, refIds: string[]): void;
+  loadMore?(range: AbsoluteTimeRange): void;
 }
 
 export type LogsVisualisationType = 'table' | 'logs';
@@ -836,9 +836,7 @@ class UnthemedLogs extends PureComponent<Props, State> {
               scrollToTopLogs={this.scrollToTopLogs}
               addResultsToCache={addResultsToCache}
               clearCache={clearCache}
-              loadMore={(range: AbsoluteTimeRange) => {
-                loadMore?.(range, logsQueries?.map((q) => q.refId) ?? []);
-              }}
+              loadMore={loadMore}
             />
           </div>
         </PanelChrome>
