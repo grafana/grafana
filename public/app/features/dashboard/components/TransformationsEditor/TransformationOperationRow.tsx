d67bb291acfec5fe454106d20af8f678f407e2f7
Leon Sorokin <leeoniya@gmail.com>
2023-11-07 21:26:14 -0600

    unused import

--- public/app/features/dashboard/components/TransformationsEditor/TransformationOperationRow.tsx
+++ public/app/features/dashboard/components/TransformationsEditor/TransformationOperationRow.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { useToggle } from 'react-use';
 
 import { DataTransformerConfig, TransformerRegistryItem, FrameMatcherID, SelectableValue } from '@grafana/data';
