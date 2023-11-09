1aa9cd2d88c07b1b28f80cdba580d64446078cbd
Kyle Cunningham <codeincarnate@users.noreply.github.com>
2023-11-08 23:59:42 -0600

    Update packages/grafana-data/src/dataframe/utils.ts
    Co-authored-by: Jev Forsberg <46619047+baldm0mma@users.noreply.github.com>

--- packages/grafana-data/src/dataframe/utils.ts
+++ packages/grafana-data/src/dataframe/utils.ts
@@ -24,7 +24,7 @@ export function isTimeSeriesFrame(frame: DataFrame) {
       const time = timeField.values[i];
 
       // Check to see if the current time is greater than
-      // the great time. If we get to the end then we
+      // the last time. If we get to the end then we
       // have a time series otherwise we return false
       if (greatestTime === null || time > greatestTime) {
         greatestTime = time;
