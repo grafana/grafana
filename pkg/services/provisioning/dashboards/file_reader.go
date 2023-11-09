1c758ab5f2465489a3c21fe23b344c77502c6f1a
Kat Yang <69819079+yangkb09@users.noreply.github.com>
2023-11-09 10:57:33 -0500

    Chore: Deprecate folderID from dashboardIdentity (#77650)

--- pkg/services/provisioning/dashboards/file_reader.go
+++ pkg/services/provisioning/dashboards/file_reader.go
@@ -243,6 +243,7 @@ func (fr *FileReader) saveDashboard(ctx context.Context, path string, folderID i
 	// keeps track of which UIDs and titles we have already provisioned
 	dash := jsonFile.dashboard
 	provisioningMetadata.uid = dash.Dashboard.UID
+	// nolint:staticcheck
 	provisioningMetadata.identity = dashboardIdentity{title: dash.Dashboard.Title, folderID: dash.Dashboard.FolderID}
 
 	// fix empty folder_uid from already provisioned dashboards
@@ -480,6 +481,7 @@ type provisioningMetadata struct {
 }
 
 type dashboardIdentity struct {
+	// Deprecated: use FolderUID instead
 	folderID int64
 	title    string
 }
