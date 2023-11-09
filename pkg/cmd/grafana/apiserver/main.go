de5bfe05d30b127afe39c29c4d7ca4a381ff5ff3
Ryan McKinley <ryantxu@gmail.com>
2023-11-08 17:00:52 -0800

    use same executable

---
+++ pkg/cmd/grafana/apiserver/main.go
@@ -0,0 +1,50 @@
+package apiserver
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	genericapiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/component-base/cli"
+)
+
+func newCommandStartExampleAPIServer(defaults *ExampleServerOptions, stopCh <-chan struct{}) *cobra.Command {
+	o := *defaults
+	cmd := &cobra.Command{
+		Short: "Launch the example API server",
+		Long:  "Launch the example API server",
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := o.Complete(); err != nil {
+				return err
+			}
+
+			config, err := o.Config()
+			if err != nil {
+				return err
+			}
+
+			if err := o.RunExampleServer(config, stopCh); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	o.RecommendedOptions.AddFlags(flags)
+
+	return cmd
+}
+
+func RunMain() {
+	stopCh := genericapiserver.SetupSignalHandler()
+	options, err := NewExampleServerOptions(os.Stdout, os.Stderr)
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := newCommandStartExampleAPIServer(options, stopCh)
+
+	code := cli.Run(cmd)
+	os.Exit(code)
+}
