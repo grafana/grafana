2027377caba3cc87bb4764f22df8f05148380919
Ryan McKinley <ryantxu@gmail.com>
2023-11-12 13:55:57 -0800

    remove commented out blocks

--- pkg/registry/apis/example/register.go
+++ pkg/registry/apis/example/register.go
@@ -200,29 +200,3 @@ func (b *TestingAPIBuilder) GetAPIRoutes() *grafanaapiserver.APIRoutes {
 		},
 	}
 }
-
-// // SchemeGroupVersion is group version used to register these objects
-// var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: VersionID}
-
-// // Resource takes an unqualified resource and returns a Group qualified GroupResource
-// func Resource(resource string) schema.GroupResource {
-// 	return SchemeGroupVersion.WithResource(resource).GroupResource()
-// }
-
-// var (
-// 	// SchemeBuilder points to a list of functions added to Scheme.
-// 	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
-// 	localSchemeBuilder = &SchemeBuilder
-// 	// AddToScheme is a common registration function for mapping packaged scoped group & version keys to a scheme.
-// 	AddToScheme = localSchemeBuilder.AddToScheme
-// )
-
-// // Adds the list of known types to the given scheme.
-// func addKnownTypes(scheme *runtime.Scheme) error {
-// 	scheme.AddKnownTypes(SchemeGroupVersion,
-// 		&example.RuntimeInfo{},
-// 	)
-
-// 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
-// 	return scheme.SetVersionPriority(SchemeGroupVersion)
-// }
