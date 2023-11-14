e5e4b53019855708bd733c5588d0b3bc08cae376
jev forsberg <jev.forsberg@grafana.com>
2023-11-14 12:24:35 -0700

    baldm0mma/docs_expansion/ update Partition by values

--- public/app/features/transformers/docs/content.ts
+++ public/app/features/transformers/docs/content.ts
@@ -1036,7 +1036,9 @@ export const transformationDocsContent: TransformationDocsContentType = {
     name: 'Partition by values',
     getHelperDocs: function () {
       return `
-  Use this transformation to eliminate the need for multiple queries to the same data source with different 'WHERE' clauses when graphing multiple series. Consider a metrics SQL table with the following data:
+  Use this transformation to streamline the process of graphing multiple series without the need for multiple queries with different 'WHERE' clauses.
+  
+  This is particularly useful when dealing with a metrics SQL table, as illustrated below:
 
   | Time                | Region | Value |
   | ------------------- | ------ | ----- |
@@ -1047,12 +1049,12 @@ export const transformationDocsContent: TransformationDocsContentType = {
 
   Prior to v9.3, if you wanted to plot a red trendline for US and a blue one for EU in the same TimeSeries panel, you would likely have to split this into two queries:
 
-  'SELECT Time, Value FROM metrics WHERE Time > "2022-10-20" AND Region="US"'<br>
+  'SELECT Time, Value FROM metrics WHERE Time > "2022-10-20" AND Region="US"'
   'SELECT Time, Value FROM metrics WHERE Time > "2022-10-20" AND Region="EU"'
 
-  This also requires you to know ahead of time which regions actually exist in the metrics table.
+  This approach also required you to know in advance which regions actually exist in the metrics table.
 
-  With the _Partition by values_ transformer, you can now issue a single query and split the results by unique values in one or more columns ('fields') of your choosing. The following example uses 'Region'.
+  With the "Partition by values" transformation, you can issue a single query and split the results by unique values in one or more columns ('fields') of your choosing. The following example uses 'Region':
 
   'SELECT Time, Region, Value FROM metrics WHERE Time > "2022-10-20"'
 
@@ -1065,6 +1067,8 @@ export const transformationDocsContent: TransformationDocsContentType = {
   | ------------------- | ------ | ----- |
   | 2022-10-20 12:00:00 | EU     | 2936  |
   | 2022-10-20 01:00:00 | EU     | 912   |
+
+  This transformation simplifies the process and enhances the flexibility of visualizing multiple series within the same TimeSeries panel.
   `;
     },
   },
