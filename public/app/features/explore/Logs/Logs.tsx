47fd1f1a5f80c2fea0b33f30eb6c62ae367c633a
Matias Chomicki <matyax@gmail.com>
2023-11-15 17:52:06 +0100

    Infinite scroll: support loading oldest logs in ascending and descending order

--- public/app/features/explore/Logs/Logs.tsx
+++ public/app/features/explore/Logs/Logs.tsx
@@ -775,7 +775,7 @@ class UnthemedLogs extends PureComponent<Props, State> {
                 <InfiniteScroll
                   loading={loading}
                   loadMoreLogs={loadMoreLogs}
-                  range={absoluteRange}
+                  range={this.props.range}
                   rows={logRows}
                   scrollElement={this.state.logsContainer}
                   sortOrder={logsSortOrder}
