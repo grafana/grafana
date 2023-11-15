b3d6cb871fd4de21487aec04715538c92323de94
Clarity-89 <homes89@ukr.net>
2023-11-15 18:35:08 +0200

    Make 'columns' and 'minColumnWidth' mutually exclusive

--- packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
+++ packages/grafana-ui/src/components/Layout/Grid/Grid.tsx
@@ -6,7 +6,7 @@ import { GrafanaTheme2, ThemeSpacingTokens } from '@grafana/data';
 import { useStyles2 } from '../../../themes';
 import { getResponsiveStyle, ResponsiveProp } from '../utils/responsiveness';
 
-interface GridProps extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | 'style'> {
+interface GridPropsBase extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | 'style'> {
   children: NonNullable<React.ReactNode>;
   /** Specifies the gutters between columns and rows. It is overwritten when a column or row gap has a value. */
   gap?: ResponsiveProp<ThemeSpacingTokens>;
@@ -17,6 +17,23 @@ interface GridProps extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | '
    */
   minColumnWidth?: ResponsiveProp<1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 44 | 55 | 72 | 89 | 144>;
 }
+
+interface PropsWithColumns extends GridPropsBase {
+  /** Number of columns */
+  columns?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  minColumnWidth?: never;
+}
+
+interface PropsWithMinColumnWidth extends GridPropsBase {
+  columns?: never;
+  /** For a responsive layout, fit as many columns while maintaining this minimum column width.
+   *  The real width will be calculated based on the theme spacing tokens: `theme.spacing(minColumnWidth)`
+   */
+  minColumnWidth?: 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 44 | 55 | 72 | 89 | 144;
+}
+
+/** 'columns' and 'minColumnWidth' are mutually exclusive */
+type GridProps = PropsWithColumns | PropsWithMinColumnWidth;
 
 export const Grid = forwardRef<HTMLDivElement, GridProps>((props, ref) => {
   const { children, gap, columns, minColumnWidth, ...rest } = props;
