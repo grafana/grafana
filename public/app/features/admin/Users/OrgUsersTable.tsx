12d569f8fd016a6b34661b1d24441a68a4610357
Jo <joao.guerreiro@grafana.com>
2023-11-16 13:04:15 +0100

    [v10.2.x] RolePicker: Optimise rendering inside lists of items (#78260)
    * Role picker: Fix flickering at service accounts page (#77049)
    
    * Role picker: Fix flickering at service accounts page
    
    * Set role picker fixed width
    
    * Fix betterer
    
    * Fix styles
    
    (cherry picked from commit aa7a6da98522f3af18e037aaf7fdef9b813290cb)
    
    * RolePicker: Optimise rendering inside lists of items (#77297)
    
    * Role picker: Load users roles in batch
    
    * Use orgId in request
    
    * Add roles to OrgUser type
    
    * Improve loading logic
    
    * Improve loading indicator
    
    * Fix org page
    
    * Update service accounts page
    
    * Use bulk roles query for teams
    
    * Use POST requests for search
    
    * Use post request for teams
    
    * Update betterer results
    
    * Review suggestions
    
    * AdminEditOrgPage: move API calls to separate file
    
    (cherry picked from commit cf7a2ea73354603b5b61a051f86ba31f546350d8)
    
    ---------
    
    Co-authored-by: Alexander Zobnin <alexanderzobnin@gmail.com>

--- public/app/features/admin/Users/OrgUsersTable.tsx
+++ public/app/features/admin/Users/OrgUsersTable.tsx
@@ -57,6 +57,7 @@ export interface Props {
   changePage: (page: number) => void;
   page: number;
   totalPages: number;
+  rolesLoading?: boolean;
 }
 
 export const OrgUsersTable = ({
@@ -68,6 +69,7 @@ export const OrgUsersTable = ({
   changePage,
   page,
   totalPages,
+  rolesLoading,
 }: Props) => {
   const [userToRemove, setUserToRemove] = useState<OrgUser | null>(null);
   const [roleOptions, setRoleOptions] = useState<Role[]>([]);
@@ -128,12 +130,15 @@ export const OrgUsersTable = ({
           return contextSrv.licensedAccessControlEnabled() ? (
             <UserRolePicker
               userId={original.userId}
+              roles={original.roles || []}
+              isLoading={rolesLoading}
               orgId={orgId}
               roleOptions={roleOptions}
               basicRole={value}
               onBasicRoleChange={(newRole) => onRoleChange(newRole, original)}
               basicRoleDisabled={basicRoleDisabled}
               basicRoleDisabledMessage={disabledRoleMessage}
+              width={40}
             />
           ) : (
             <OrgRolePicker
@@ -182,7 +187,7 @@ export const OrgUsersTable = ({
         },
       },
     ],
-    [orgId, roleOptions, onRoleChange]
+    [rolesLoading, orgId, roleOptions, onRoleChange]
   );
 
   return (
