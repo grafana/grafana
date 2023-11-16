f4270cc87bbf8edd6f7de203f1eba5857f1792ff
Piotr Jamróz <pm.jamroz@gmail.com>
2023-11-16 10:19:36 +0100

    Branch benchmark

--- public/app/features/explore/spec/queryHistory.test.tsx
+++ public/app/features/explore/spec/queryHistory.test.tsx
@@ -192,6 +192,8 @@ describe('Explore: Query History', () => {
   });
 
   it('add comments to query history', async () => {
+    // eslint-disable-next-line no-console
+    console.time('*** BENCHMARK ***');
     const urlParams = {
       left: serializeStateToUrlParam({
         datasource: 'loki',
@@ -207,6 +209,8 @@ describe('Explore: Query History', () => {
     await assertQueryHistory(['{"expr":"query #1"}'], 'left');
     await commentQueryHistory(0, 'test comment');
     await assertQueryHistoryComment(['test comment'], 'left');
+    // eslint-disable-next-line no-console
+    console.timeEnd('*** BENCHMARK ***');
   });
 
   it('removes the query item from the history panel when user deletes a regular query', async () => {
