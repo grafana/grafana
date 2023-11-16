d6b478b45ca6122943ca8f9f5ebfcb64a01742f6
Matias Chomicki <matyax@gmail.com>
2023-11-16 17:18:33 +0100

    Loki: implement DataSourceWithQueryModificationSupportSupport

--- public/app/plugins/datasource/loki/datasource.ts
+++ public/app/plugins/datasource/loki/datasource.ts
@@ -37,6 +37,7 @@ import {
   LegacyMetricFindQueryOptions,
   AdHocVariableFilter,
   urlUtil,
+  DataSourceWithQueryModificationSupportSupport,
 } from '@grafana/data';
 import { Duration } from '@grafana/lezer-logql';
 import { BackendSrvRequest, config, DataSourceWithBackend, getTemplateSrv, TemplateSrv } from '@grafana/runtime';
@@ -136,7 +137,8 @@ export class LokiDatasource
     DataSourceWithSupplementaryQueriesSupport<LokiQuery>,
     DataSourceWithQueryImportSupport<LokiQuery>,
     DataSourceWithQueryExportSupport<LokiQuery>,
-    DataSourceWithToggleableQueryFiltersSupport<LokiQuery>
+    DataSourceWithToggleableQueryFiltersSupport<LokiQuery>,
+    DataSourceWithQueryModificationSupportSupport<LokiQuery>
 {
   private streams = new LiveStreams();
   private logContextProvider: LogContextProvider;
@@ -864,7 +866,7 @@ export class LokiDatasource
   }
 
   /**
-   * Implemented as part of `DataSourceApi`. Used to modify a query based on the provided action.
+   * Implemented as part of `DataSourceWithQueryModificationSupportSupport`. Used to modify a query based on the provided action.
    * It is used, for example, in the Query Builder to apply hints such as parsers, operations, etc.
    * @returns A new LokiQuery with the specified modification applied.
    */
@@ -948,6 +950,26 @@ export class LokiDatasource
         break;
     }
     return { ...query, expr: expression };
+  }
+
+  /**
+   * Implemented as part of `DataSourceWithQueryModificationSupportSupport`. Returns a list of operation
+   * types that are supported by `modifyQuery()`.
+   */
+  getSupportedQueryModifications()
+  {
+    return [
+      'ADD_FILTER',
+      'ADD_FILTER_OUT',
+      'ADD_LOGFMT_PARSER',
+      'ADD_JSON_PARSER',
+      'ADD_UNPACK_PARSER',
+      'ADD_NO_PIPELINE_ERROR',
+      'ADD_LEVEL_LABEL_FORMAT',
+      'ADD_LABEL_FILTER',
+      'ADD_STRING_FILTER',
+      'ADD_STRING_FILTER_OUT'
+    ];
   }
 
   /**
