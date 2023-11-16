e04a4f63b9b2c536ac8b556e9ae84f954017a8b2
jev forsberg <jev.forsberg@grafana.com>
2023-11-16 15:56:30 -0700

    baldm0mma/docs_expansion/ update Partition by values

--- public/app/features/transformers/docs/content.ts
+++ public/app/features/transformers/docs/content.ts
@@ -1042,14 +1042,7 @@ export const transformationDocsContent: TransformationDocsContentType = {
   | 2022-10-20 12:00:00 | EU     | 2936  |
   | 2022-10-20 01:00:00 | US     | 1327  |
   | 2022-10-20 01:00:00 | EU     | 912   |
-
-  Prior to v9.3, if you wanted to plot a red trendline for US and a blue one for EU in the same TimeSeries panel, you would likely have to split this into two queries:
-
-  'SELECT Time, Value FROM metrics WHERE Time > "2022-10-20" AND Region="US"'
-  'SELECT Time, Value FROM metrics WHERE Time > "2022-10-20" AND Region="EU"'
-
-  This approach also required you to know in advance which regions actually exist in the metrics table.
-
+  
   With the "Partition by values" transformation, you can issue a single query and split the results by unique values in one or more columns ('fields') of your choosing. The following example uses 'Region':
 
   'SELECT Time, Region, Value FROM metrics WHERE Time > "2022-10-20"'
