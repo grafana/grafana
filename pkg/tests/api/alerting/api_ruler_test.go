d9273a9b2d0d394ece70f56272f8bc0e01930f3d
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-11-17 18:10:26 +0200

    make lint-go

--- pkg/tests/api/alerting/api_ruler_test.go
+++ pkg/tests/api/alerting/api_ruler_test.go
@@ -136,11 +136,11 @@ func TestIntegrationAlertRulePermissions(t *testing.T) {
 
 			for _, rule := range allRules["folder1"][0].Rules {
 				assert.Equal(t, "folder1", rule.GrafanaManagedAlert.NamespaceUID)
-				assert.Equal(t, int64(1), rule.GrafanaManagedAlert.NamespaceID)
+				assert.Equal(t, int64(1), rule.GrafanaManagedAlert.NamespaceID) //nolint: staticcheck
 			}
 			for _, rule := range allRules["folder2"][0].Rules {
 				assert.Equal(t, "folder2", rule.GrafanaManagedAlert.NamespaceUID)
-				assert.Equal(t, int64(2), rule.GrafanaManagedAlert.NamespaceID)
+				assert.Equal(t, int64(2), rule.GrafanaManagedAlert.NamespaceID) //nolint: staticcheck
 			}
 		})
 
