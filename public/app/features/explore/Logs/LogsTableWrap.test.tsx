081671679c538277dfdd771cae97dfe7427a4ab8
Galen <galen.kistler@grafana.com>
2023-11-21 13:12:10 -0600

    remove unused import

--- public/app/features/explore/Logs/LogsTableWrap.test.tsx
+++ public/app/features/explore/Logs/LogsTableWrap.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React, { ComponentProps } from 'react';
 
 import {
