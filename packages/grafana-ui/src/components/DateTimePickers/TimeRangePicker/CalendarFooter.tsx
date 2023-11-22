9e43d14d7a33e3764596ab03f1c08fa09404c9d2
joshhunt <josh@trtr.co>
2023-11-22 18:28:19 +0000

    swap buttons:

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/CalendarFooter.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/CalendarFooter.tsx
@@ -8,13 +8,13 @@ import { TimePickerCalendarProps } from './TimePickerCalendar';
 
 export function Footer({ onClose, onApply }: TimePickerCalendarProps) {
   return (
-    <Stack justifyContent="space-between">
-      <Button onClick={onApply}>
-        <Trans i18nKey="time-picker.calendar.apply-button">Apply time range</Trans>
-      </Button>
-
+    <Stack gap={2} justifyContent="space-between">
       <Button variant="secondary" onClick={onClose}>
         <Trans i18nKey="time-picker.calendar.cancel-button">Cancel</Trans>
+      </Button>
+
+      <Button onClick={onApply}>
+        <Trans i18nKey="time-picker.calendar.apply-button">Apply time range</Trans>
       </Button>
     </Stack>
   );
