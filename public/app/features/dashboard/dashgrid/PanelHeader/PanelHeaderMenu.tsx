caf39763fc61438d736415ea8abcba7476162c2e
eledobleefe <laura.fernandez@grafana.com>
2023-11-27 14:27:07 +0100

    Add info for setReturnToPrevious in the Explore menu item

--- public/app/features/dashboard/dashgrid/PanelHeader/PanelHeaderMenu.tsx
+++ public/app/features/dashboard/dashgrid/PanelHeader/PanelHeaderMenu.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { PanelMenuItem } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { Menu } from '@grafana/ui';
+import { useGrafana } from 'app/core/context/GrafanaContext';
 
 export interface Props {
   items: PanelMenuItem[];
@@ -12,6 +14,9 @@ export interface Props {
 }
 
 export function PanelHeaderMenu({ items }: Props) {
+  const location = useLocation();
+  const { chrome } = useGrafana();
+
   const renderItems = (items: PanelMenuItem[]) => {
     return items.map((item) => {
       switch (item.type) {
@@ -31,7 +36,15 @@ export function PanelHeaderMenu({ items }: Props) {
               icon={item.iconClassName}
               childItems={item.subMenu ? renderItems(item.subMenu) : undefined}
               url={item.href}
-              onClick={item.onClick}
+              onClick={(e: React.MouseEvent) => {
+                if (item && item.onClick) {
+                  if (item.text === 'Explore') {
+                    chrome.setReturnToPrevious({ show: true, href: location.pathname, title: 'Dashboard' });
+                    console.log('location', location);
+                  }
+                  item.onClick(e);
+                }
+              }}
               shortcut={item.shortcut}
               testId={selectors.components.Panels.Panel.menuItems(item.text)}
             />
