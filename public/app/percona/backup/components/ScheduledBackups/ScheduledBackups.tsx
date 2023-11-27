06264acbfd919bed21bed3a65d1c567869e09e3f
Dora <103416234+doracretu3pillar@users.noreply.github.com>
2023-11-27 19:29:20 +0200

    PMM-12443: filter for backups (#685)
    * Added Filter for All Backups table
    
    * Added filter for Restore, ScheduledBackups and StorageLocations
    
    * Filters for StorageLocation, RestoreHistory and BackupInventory
    
    * Fixed UI tests
    
    * Fixed lint
    
    * Fixed test

--- public/app/percona/backup/components/ScheduledBackups/ScheduledBackups.tsx
+++ public/app/percona/backup/components/ScheduledBackups/ScheduledBackups.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/display-name */
 import cronstrue from 'cronstrue';
 import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
-import { Cell, Column, Row } from 'react-table';
+import { Cell, Row } from 'react-table';
 
 import { AppEvents, urlUtil } from '@grafana/data';
 import { locationService } from '@grafana/runtime';
@@ -10,7 +10,7 @@ import { appEvents } from 'app/core/app_events';
 import { Page } from 'app/core/components/Page/Page';
 import { DeleteModal } from 'app/percona/shared/components/Elements/DeleteModal';
 import { FeatureLoader } from 'app/percona/shared/components/Elements/FeatureLoader';
-import { Table } from 'app/percona/shared/components/Elements/Table';
+import { ExtendedColumn, FilterFieldTypes, Table } from 'app/percona/shared/components/Elements/Table';
 import { useCancelToken } from 'app/percona/shared/components/hooks/cancelToken.hook';
 import { DATABASE_LABELS } from 'app/percona/shared/core';
 import { fetchStorageLocations } from 'app/percona/shared/core/reducers/backups/backupLocations';
@@ -44,7 +44,6 @@ export const ScheduledBackups: FC = () => {
   const styles = useStyles(getStyles);
   const { result: locations = [] } = useSelector(getBackupLocations);
   const locationsByLocationId = useMemo(() => formatLocationsToMap(locations), [locations]);
-
   const retentionValue = useCallback((n: number) => {
     if (n < 0) {
       return '';
@@ -131,31 +130,40 @@ export const ScheduledBackups: FC = () => {
   );
 
   const columns = useMemo(
-    (): Array<Column<ScheduledBackup>> => [
+    (): Array<ExtendedColumn<ScheduledBackup>> => [
       {
         Header: Messages.scheduledBackups.table.columns.name,
         accessor: 'name',
         id: 'name',
+        type: FilterFieldTypes.TEXT,
       },
       {
         Header: Messages.scheduledBackups.table.columns.vendor,
         accessor: 'vendor',
-        Cell: ({ value }) => <>{DATABASE_LABELS[value]}</>,
+        Cell: ({ value }) => DATABASE_LABELS[value],
+        type: FilterFieldTypes.DROPDOWN,
+        options: Object.values(DATABASE_LABELS).map((item: string) => ({
+          label: item,
+          value: item,
+        })),
       },
       {
         Header: Messages.scheduledBackups.table.columns.frequency,
         accessor: 'cronExpression',
-        Cell: ({ value }) => <>{cronstrue.toString(value, { use24HourTimeFormat: true })}</>,
+        Cell: ({ value }) => cronstrue.toString(value, { use24HourTimeFormat: true }),
+        type: FilterFieldTypes.TEXT,
       },
       {
         Header: Messages.scheduledBackups.table.columns.retention,
         accessor: 'retention',
-        Cell: ({ value }) => <>{retentionValue(value)}</>,
+        Cell: ({ value }) => retentionValue(value),
+        type: FilterFieldTypes.TEXT,
       },
       {
         Header: Messages.scheduledBackups.table.columns.type,
         accessor: 'mode',
-        Cell: ({ value }) => <>{formatBackupMode(value)}</>,
+        Cell: ({ value }) => formatBackupMode(value),
+        type: FilterFieldTypes.TEXT,
       },
       {
         Header: Messages.scheduledBackups.table.columns.location,
@@ -165,17 +173,24 @@ export const ScheduledBackups: FC = () => {
             {value} ({locationsByLocationId[row.original.locationId]?.type})
           </span>
         ),
+        type: FilterFieldTypes.DROPDOWN,
+        options: locations.map((item) => ({
+          label: item.name,
+          value: item.name,
+        })),
       },
       {
         Header: Messages.scheduledBackups.table.columns.lastBackup,
         accessor: 'lastBackup',
         Cell: ({ value }) => <>{value ? <DetailedDate date={value} /> : ''}</>,
         width: '200px',
+        type: FilterFieldTypes.TEXT,
       },
       {
         Header: Messages.scheduledBackups.table.columns.actions,
         accessor: 'id',
         width: '150px',
+        type: FilterFieldTypes.TEXT,
         Cell: ({ row }) => (
           <ScheduledBackupsActions
             row={row}
@@ -189,7 +204,7 @@ export const ScheduledBackups: FC = () => {
         ),
       },
     ],
-    [actionPending, handleCopy, handleToggle, locationsByLocationId, retentionValue]
+    [actionPending, handleCopy, handleToggle, locationsByLocationId, retentionValue, locations]
   );
 
   const renderSelectedSubRow = React.useCallback(
@@ -270,6 +285,7 @@ export const ScheduledBackups: FC = () => {
             renderExpandedRow={renderSelectedSubRow}
             getCellProps={getCellProps}
             getRowId={useCallback((row: ScheduledBackup) => row.id, [])}
+            showFilter
           />
           <DeleteModal
             title={Messages.scheduledBackups.deleteModalTitle}
