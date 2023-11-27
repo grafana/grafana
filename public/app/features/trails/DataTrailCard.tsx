e04c2a6ce5b5b8c6092a3fe89cbc9804b083617e
André Pereira <adrapereira@gmail.com>
2023-11-27 15:11:05 +0000

    Added button to remove bookmark. Added trail to home card

--- public/app/features/trails/DataTrailCard.tsx
+++ public/app/features/trails/DataTrailCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { AdHocFiltersVariable, sceneGraph } from '@grafana/scenes';
-import { useStyles2, Stack } from '@grafana/ui';
+import { useStyles2, Stack, Icon, Tooltip } from '@grafana/ui';
 
 import { DataTrail } from './DataTrail';
 import { LOGS_METRIC, VAR_DATASOURCE_EXPR, VAR_FILTERS } from './shared';
@@ -14,7 +14,7 @@ export interface Props {
   onDelete?: () => void;
 }
 
-export function DataTrailCard({ trail, onSelect }: Props) {
+export function DataTrailCard({ trail, onSelect, onDelete }: Props) {
   const styles = useStyles2(getStyles);
 
   const filtersVariable = sceneGraph.lookupVariable(VAR_FILTERS, trail)!;
@@ -26,23 +26,33 @@ export function DataTrailCard({ trail, onSelect }: Props) {
   const dsValue = getDataSource(trail);
 
   return (
-    <button className={styles.container} onClick={() => onSelect(trail)}>
-      <div className={styles.heading}>{getMetricName(trail.state.metric)}</div>
-      <Stack gap={1.5}>
-        {dsValue && (
-          <Stack direction="column" gap={0.5}>
-            <div className={styles.label}>Datasource</div>
-            <div className={styles.value}>{getDataSource(trail)}</div>
-          </Stack>
-        )}
-        {filters.map((filter, index) => (
-          <Stack key={index} direction="column" gap={0.5}>
-            <div className={styles.label}>{filter.key}</div>
-            <div className={styles.value}>{filter.value}</div>
-          </Stack>
-        ))}
-      </Stack>
-    </button>
+    <div className={styles.wrapper}>
+      <button className={styles.container} onClick={() => onSelect(trail)}>
+        <div className={styles.heading}>{getMetricName(trail.state.metric)}</div>
+        <trail.state.history.Component model={trail.state.history} />
+        <Stack gap={1.5}>
+          {dsValue && (
+            <Stack direction="column" gap={0.5}>
+              <div className={styles.label}>Datasource</div>
+              <div className={styles.value}>{getDataSource(trail)}</div>
+            </Stack>
+          )}
+          {filters.map((filter, index) => (
+            <Stack key={index} direction="column" gap={0.5}>
+              <div className={styles.label}>{filter.key}</div>
+              <div className={styles.value}>{filter.value}</div>
+            </Stack>
+          ))}
+        </Stack>
+      </button>
+      {onDelete && (
+        <Tooltip content={'Remove bookmark'}>
+          <button className={styles.delete} onClick={onDelete}>
+            <Icon className={styles.deleteIcon} name={'times-circle'} size={'md'} />
+          </button>
+        </Tooltip>
+      )}
+    </div>
   );
 }
 
@@ -70,6 +80,7 @@ function getStyles(theme: GrafanaTheme2) {
       display: 'flex',
       flexDirection: 'column',
       gap: theme.spacing(2),
+      width: '100%',
       border: `1px solid ${theme.colors.border.weak}`,
       borderRadius: theme.shape.radius.default,
       cursor: 'pointer',
@@ -91,9 +102,32 @@ function getStyles(theme: GrafanaTheme2) {
       padding: theme.spacing(0),
       display: 'flex',
       fontWeight: theme.typography.fontWeightMedium,
+      width: '100%',
+      overflowX: 'hidden',
     }),
     body: css({
       padding: theme.spacing(0),
+    }),
+    delete: css({
+      border: 'none',
+      background: 'transparent',
+      padding: 0,
+      margin: 0,
+      cursor: 'pointer',
+      position: 'absolute',
+      top: '-12px',
+      right: '-7px',
+      color: theme.colors.error.main,
+      '&:hover': {
+        color: theme.colors.error.shade,
+      },
+    }),
+    wrapper: css({
+      position: 'relative',
+      display: 'flex',
+    }),
+    deleteIcon: css({
+      background: theme.colors.background.primary,
     }),
   };
 }
