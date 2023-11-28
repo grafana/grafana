0876c9e3c728a9f2559956e82fdf20aa339f50ce
Gilles De Mey <gilles.de.mey@gmail.com>
2023-11-28 13:49:35 +0100

    add aria-label for unused contact points

--- public/app/features/alerting/unified/components/contact-points/components/UnusedBadge.tsx
+++ public/app/features/alerting/unified/components/contact-points/components/UnusedBadge.tsx
@@ -5,6 +5,7 @@ import { Badge } from '@grafana/ui';
 export const UnusedContactPointBadge = () => (
   <Badge
     text="Unused"
+    aria-label="unused"
     color="orange"
     icon="exclamation-triangle"
     tooltip="This contact point is not used in any notification policy and it will not receive any alerts"
