71464bfc4ba1a24106afecaa034612c21c1a1b8b
Gabriel MABILLE <gabriel.mabille@grafana.com>
2023-11-28 17:04:30 +0100

    Syntax sugar

--- pkg/services/authn/clients/api_key_test.go
+++ pkg/services/authn/clients/api_key_test.go
@@ -135,10 +135,10 @@ func TestAPIKey_Authenticate(t *testing.T) {
 				assert.Nil(t, identity)
 				assert.ErrorIs(t, err, tt.expectedErr)
 				return
-			} else {
-				assert.NoError(t, err)
-				assert.EqualValues(t, *tt.expectedIdentity, *identity)
 			}
+
+			assert.NoError(t, err)
+			assert.EqualValues(t, *tt.expectedIdentity, *identity)
 			assert.Equal(t, tt.req.OrgID, tt.expectedIdentity.OrgID, "the request organization should match the identity's one")
 		})
 	}
