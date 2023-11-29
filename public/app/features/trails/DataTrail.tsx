c0510dc478e2c545094ef4b420cae36c7bb57fa8
Darren Janeczek <darren.janeczek@grafana.com>
2023-11-28 23:38:11 -0500

    fix: ability to (de)serialize parents and current index
    - refactored so only history stores parent relations and current index
    
    - rounded indirect parent links

--- public/app/features/trails/DataTrail.tsx
+++ public/app/features/trails/DataTrail.tsx
@@ -43,10 +43,6 @@ export interface DataTrailState extends SceneObjectState {
 
   // Synced with url
   metric?: string;
-
-  // Indicates which step in the data trail this is
-  stepIndex: number;
-  parentIndex: number; // If there is no parent, this will be -1
 }
 
 export class DataTrail extends SceneObjectBase<DataTrailState> {
@@ -64,8 +60,6 @@ export class DataTrail extends SceneObjectBase<DataTrailState> {
       ],
       history: state.history ?? new DataTrailHistory({}),
       settings: state.settings ?? new DataTrailSettings({}),
-      stepIndex: state.stepIndex ?? 0,
-      parentIndex: state.parentIndex ?? -1,
       ...state,
     });
 
@@ -80,6 +74,29 @@ export class DataTrail extends SceneObjectBase<DataTrailState> {
     // Some scene elements publish this
     this.subscribeToEvent(MetricSelectedEvent, this._handleMetricSelectedEvent.bind(this));
 
+    // Pay attention to changes in history (i.e., changing the step)
+    this.state.history.subscribeToState((newState, oldState) => {
+      const oldNumberOfSteps = oldState.steps.length;
+      const newNumberOfSteps = newState.steps.length;
+
+      const newStepWasAppended = newNumberOfSteps > oldNumberOfSteps;
+
+      if (newStepWasAppended) {
+        // Do nothing because the state is already up to date -- it created a new step!
+        return;
+      }
+
+      if (oldState.currentStep === newState.currentStep) {
+        // The same step was clicked on -- no need to change anything.
+        return;
+      }
+
+      // History changed because a different node was selected
+      const step = newState.steps[newState.currentStep];
+
+      this.goBackToStep(step);
+    });
+
     return () => {
       if (!this.state.embedded) {
         getUrlSyncManager().cleanUp(this);
@@ -88,7 +105,7 @@ export class DataTrail extends SceneObjectBase<DataTrailState> {
     };
   }
 
-  public goBackToStep(step: DataTrailHistoryStep) {
+  private goBackToStep(step: DataTrailHistoryStep) {
     if (!this.state.embedded) {
       getUrlSyncManager().cleanUp(this);
     }
