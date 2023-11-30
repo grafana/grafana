5a80962de9596c8555bc0beb34341e48afcb6989
Matthew Jacobson <matthew.jacobson@grafana.com>
2023-11-30 11:01:11 -0500

    Alerting: Add clean_upgrade config and deprecate force_migration (#78324)
    * Alerting: Add clean_upgrade config and deprecate force_migration
    
    Upgrading to UA and rolling back will no longer delete any data by default. 
    Instead, each set of tables will remain unchanged when switching between 
    legacy and UA. As such, the force_migration config has been deprecated 
    and no extra configuration is required to roll back to legacy anymore.
    
    If clean_upgrade is set to true when upgrading from legacy alerting to Unified
    Alerting, grafana will first delete all existing Unified Alerting resources,
    thus re-upgrading all organizations from scratch. If false or unset,
    organizations that have previously upgraded will not lose their existing Unified
     Alerting data when switching between legacy and Unified Alerting.
    
     Similar to force_migration, it should be kept false when not needed as it may
     cause unintended data-loss if left enabled.
    
    ---------
    
    Co-authored-by: Christopher Moyer <35463610+chri2547@users.noreply.github.com>

--- pkg/services/ngalert/migration/service.go
+++ pkg/services/ngalert/migration/service.go
@@ -16,9 +16,6 @@ import (
 // actionName is the unique row-level lock name for serverlock.ServerLockService.
 const actionName = "alerting migration"
 
-//nolint:stylecheck
-var ForceMigrationError = fmt.Errorf("Grafana has already been migrated to Unified Alerting. Any alert rules created while using Unified Alerting will be deleted by rolling back. Set force_migration=true in your grafana.ini and restart Grafana to roll back and delete Unified Alerting configuration data.")
-
 type UpgradeService interface {
 	Run(ctx context.Context) error
 }
@@ -84,6 +81,7 @@ func newTransition(currentType migrationStore.AlertingType, cfg *setting.Cfg) tr
 		CurrentType:      currentType,
 		DesiredType:      desiredType,
 		CleanOnDowngrade: cfg.ForceMigration,
+		CleanOnUpgrade:   cfg.UnifiedAlerting.Upgrade.CleanUpgrade,
 	}
 }
 
@@ -92,6 +90,7 @@ type transition struct {
 	CurrentType      migrationStore.AlertingType
 	DesiredType      migrationStore.AlertingType
 	CleanOnDowngrade bool
+	CleanOnUpgrade   bool
 }
 
 // isNoChange returns true if the migration is a no-op.
@@ -111,28 +110,25 @@ func (t transition) isDowngrading() bool {
 
 // shouldClean returns true if the migration should delete all unified alerting data.
 func (t transition) shouldClean() bool {
-	return t.isDowngrading() && t.CleanOnDowngrade
+	return t.isDowngrading() && t.CleanOnDowngrade || t.isUpgrading() && t.CleanOnUpgrade
 }
 
 // applyTransition applies the transition to the database.
 // If the transition is a no-op, nothing will be done.
+// If the transition is a downgrade and CleanOnDowngrade is false, nothing will be done.
 // If the transition is a downgrade and CleanOnDowngrade is true, all unified alerting data will be deleted.
-// If the transition is a downgrade and CleanOnDowngrade is false, an error will be returned.
-// If the transition is an upgrade, all orgs will be migrated.
+// If the transition is an upgrade and CleanOnUpgrade is false, all orgs will be migrated.
+// If the transition is an upgrade and CleanOnUpgrade is true, all unified alerting data will be deleted and then all orgs will be migrated.
 func (ms *migrationService) applyTransition(ctx context.Context, t transition) error {
 	l := ms.log.New(
 		"CurrentType", t.CurrentType,
 		"DesiredType", t.DesiredType,
 		"CleanOnDowngrade", t.CleanOnDowngrade,
+		"CleanOnUpgrade", t.CleanOnUpgrade,
 	)
 	if t.isNoChange() {
 		l.Info("Migration already complete")
 		return nil
-	}
-
-	// Safeguard to prevent accidental data loss when reverting from UA to LA.
-	if t.isDowngrading() && !ms.cfg.ForceMigration {
-		return ForceMigrationError
 	}
 
 	if t.shouldClean() {
