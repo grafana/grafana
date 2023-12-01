609f517eb0fc47931df93dfea5b3d4cfeb6533ac
Ryan McKinley <ryantxu@gmail.com>
2023-12-01 09:51:32 +0100

    fix error templates

--- pkg/tests/testinfra/testinfra.go
+++ pkg/tests/testinfra/testinfra.go
@@ -157,6 +157,14 @@ func CreateGrafDir(t *testing.T, opts ...GrafanaOpts) (string, string) {
 	viewsDir := filepath.Join(publicDir, "views")
 	err = fs.CopyRecursive(filepath.Join(rootDir, "public", "views"), viewsDir)
 	require.NoError(t, err)
+	// Copy index template to index.html, since Grafana will try to use the latter
+	err = fs.CopyFile(filepath.Join(rootDir, "public", "views", "index.html"),
+		filepath.Join(viewsDir, "index.html"))
+	require.NoError(t, err)
+	// Copy error template to error.html, since Grafana will try to use the latter
+	err = fs.CopyFile(filepath.Join(rootDir, "public", "views", "error.html"),
+		filepath.Join(viewsDir, "error.html"))
+	require.NoError(t, err)
 	emailsDir := filepath.Join(publicDir, "emails")
 	err = fs.CopyRecursive(filepath.Join(rootDir, "public", "emails"), emailsDir)
 	require.NoError(t, err)
