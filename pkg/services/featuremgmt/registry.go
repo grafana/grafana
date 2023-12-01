e24c8fc2ade183bdc54656c925736d724c588f43
Matt Jacobson <matthew.jacobson@grafana.com>
2023-11-30 19:11:26 -0500

    Alerting: Create feature flag for alert query optimization
    Currently enabled by default to parity existing functionality, will be disabled
    by default in a future PR.

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1315,5 +1315,14 @@ var (
 			RequiresRestart: true,
 			Created:         time.Date(2024, time.January, 10, 12, 0, 0, 0, time.UTC),
 		},
+		{
+			Name:           "alertingQueryOptimization",
+			Description:    "Optimizes eligible queries in order to reduce load on datasources",
+			Stage:          FeatureStageGeneralAvailability,
+			Expression:     "true",
+			Owner:          grafanaAlertingSquad,
+			AllowSelfServe: false,
+			Created:        time.Date(2024, time.January, 10, 12, 0, 0, 0, time.UTC),
+		},
 	}
 )
