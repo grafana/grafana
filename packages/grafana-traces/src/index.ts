f0e68d56129eadc639c2433accb3c4a7219a3eef
Fabrizio <fabrizio.casati@grafana.com>
2023-12-04 14:36:03 +0100

    Update imports

--- packages/grafana-traces/src/index.ts
+++ packages/grafana-traces/src/index.ts
@@ -4,6 +4,25 @@
  * @packageDocumentation
  */
 
+import { IntervalInput } from './IntervalInput/IntervalInput';
+import { TagMappingInput } from './TraceToLogs/TagMappingInput';
+import * as TraceToLogsSettings from './TraceToLogs/TraceToLogsSettings';
+import * as TraceToMetricsSettings from './TraceToMetrics/TraceToMetricsSettings';
+import * as TraceToProfilesSettings from './TraceToProfiles/TraceToProfilesSettings';
+import { getNonOverlappingDuration, getStats, makeFrames, makeSpanMap } from './utils';
+
+export {
+  IntervalInput,
+  TagMappingInput,
+  TraceToLogsSettings,
+  TraceToMetricsSettings,
+  TraceToProfilesSettings,
+  getNonOverlappingDuration,
+  getStats,
+  makeFrames,
+  makeSpanMap,
+};
+
 // dummy type
 export type Props = {
   value?: string;
