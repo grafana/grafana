7b7d34b5f2e5aac7dea4cfa8fcab0d27f6a378b7
Gilles De Mey <gilles.de.mey@gmail.com>
2023-12-04 18:10:53 +0100

    allow plugin component to be lazy loaded

--- public/app/features/plugins/extensions/validators.ts
+++ public/app/features/plugins/extensions/validators.ts
@@ -139,6 +139,9 @@ export function isReactComponent(component: unknown): component is React.Compone
   const isReactLazyComponent = (obj: unknown): obj is { $$typeof: Symbol } =>
     hasReactTypeProp(obj) && obj.$$typeof === Symbol.for('react.lazy');
 
+  const isReactLazyComponent = (obj: unknown): boolean =>
+    hasReactTypeProp(obj) && obj.$$typeof === Symbol.for('react.lazy');
+
   // We currently don't have any strict runtime-checking for this.
   // (The main reason is that we don't want to start depending on React implementation details.)
   return typeof component === 'function' || isReactMemoObject(component) || isReactLazyComponent(component);
