1bbc29ae09f3ac8d91bde7248aef7f882a78b15d
Ashley Harrison <ashley.harrison@grafana.com>
2023-12-04 12:59:19 +0000

    refactor out DataSourcesListCard

---
+++ public/app/features/datasources/components/DataSourcesListCard.tsx
@@ -0,0 +1,89 @@
+import { css } from '@emotion/css';
+import React from 'react';
+
+import { DataSourceSettings, GrafanaTheme2 } from '@grafana/data';
+import { config } from '@grafana/runtime';
+import { Card, LinkButton, Tag, useStyles2 } from '@grafana/ui';
+
+import { useDataSourcesRoutes } from '../state';
+import { trackCreateDashboardClicked, trackExploreClicked } from '../tracking';
+import { constructDataSourceExploreUrl } from '../utils';
+
+export interface Props {
+  dataSource: DataSourceSettings;
+  hasWriteRights: boolean;
+  hasExploreRights: boolean;
+}
+
+export function DataSourcesListCard({ dataSource, hasWriteRights, hasExploreRights }: Props) {
+  const dataSourcesRoutes = useDataSourcesRoutes();
+  const dsLink = config.appSubUrl + dataSourcesRoutes.Edit.replace(/:uid/gi, dataSource.uid);
+  const styles = useStyles2(getStyles);
+
+  return (
+    <Card href={hasWriteRights ? dsLink : undefined}>
+      <Card.Heading>{dataSource.name}</Card.Heading>
+      <Card.Figure>
+        <img src={dataSource.typeLogoUrl} alt="" height="40px" width="40px" className={styles.logo} />
+      </Card.Figure>
+      <Card.Meta>
+        {[
+          dataSource.typeName,
+          dataSource.url,
+          dataSource.isDefault && <Tag key="default-tag" name={'default'} colorIndex={1} />,
+        ]}
+      </Card.Meta>
+      <Card.Tags>
+        {/* Build Dashboard */}
+        <LinkButton
+          icon="apps"
+          fill="outline"
+          variant="secondary"
+          href={`dashboard/new-with-ds/${dataSource.uid}`}
+          onClick={() => {
+            trackCreateDashboardClicked({
+              grafana_version: config.buildInfo.version,
+              datasource_uid: dataSource.uid,
+              plugin_name: dataSource.typeName,
+              path: location.pathname,
+            });
+          }}
+        >
+          Build a dashboard
+        </LinkButton>
+
+        {/* Explore */}
+        {hasExploreRights && (
+          <LinkButton
+            icon="compass"
+            fill="outline"
+            variant="secondary"
+            className={styles.button}
+            href={constructDataSourceExploreUrl(dataSource)}
+            onClick={() => {
+              trackExploreClicked({
+                grafana_version: config.buildInfo.version,
+                datasource_uid: dataSource.uid,
+                plugin_name: dataSource.typeName,
+                path: location.pathname,
+              });
+            }}
+          >
+            Explore
+          </LinkButton>
+        )}
+      </Card.Tags>
+    </Card>
+  );
+}
+
+const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    logo: css({
+      objectFit: 'contain',
+    }),
+    button: css({
+      marginLeft: theme.spacing(2),
+    }),
+  };
+};
