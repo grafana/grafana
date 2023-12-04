90e6eac20092d7d6d7ec68d588f1d69a3a393e72
nmarrs <nathanielmarrs@gmail.com>
2023-12-04 14:33:35 -0800

    add stubbed AchievementService + achievements / types

---
+++ public/app/features/achievements/AchievementsService.ts
@@ -0,0 +1,11 @@
+// mix of util / helper functions to save achievement data, and to check if an achievement has been completed
+
+// function to use across Grafana to register an achievement as completed
+// needs to look at current user data, see if achievement is already completed, and if not, save it as completed on user object
+// also needs to check if the achievement is a level up, and if so, save the new level on the user object
+
+// function to check if a user has completed an achievement
+
+// function to provide all achievements for a user (with completion status)
+
+// function to retrieve user level
---
+++ public/app/features/achievements/types.ts
@@ -0,0 +1,44 @@
+export enum AchievementLevel {
+  Novice = 1,
+  Beginner = 2,
+  Experienced = 3,
+  Expert = 4,
+  Wizard = 5,
+}
+
+export enum AchievementId {
+  NavigateToDashboard = 'NavigateToDashboard',
+  NavigateToExplore = 'NavigateToExplore',
+  WatchIntroToGrafanaVideo = 'WatchIntroToGrafanaVideo',
+  ConnectYourFirstDatasource = 'ConnectYourFirstDatasource',
+  UseExploreToMakeAQuery = 'UseExploreToMakeAQuery',
+  AddExplorePanelToADashboard = 'AddExplorePanelToADashboard',
+  AddATitleAndDescriptionToAPanelInADashboard = 'AddATitleAndDescriptionToAPanelInADashboard',
+  ChangeTheTheme = 'ChangeTheTheme',
+  ExploreKeyboardShortcuts = 'ExploreKeyboardShortcuts',
+  ChangePanelSettings = 'ChangePanelSettings',
+  ImplementDataLink = 'ImplementDataLink',
+  AddTemplateVariable = 'AddTemplateVariable',
+  AddDataTransformation = 'AddDataTransformation',
+  AddCanvasVisualization = 'AddCanvasVisualization',
+  AddMetricValueElement = 'AddMetricValueElement',
+  EnableCrosshairSharing = 'EnableCrosshairSharing',
+  LegendChangeSeriesColor = 'LegendChangeSeriesColor',
+  LegendShowSeries = 'LegendShowSeries',
+  LegendHideSeries = 'LegendHideSeries',
+  AddCustomThresholds = 'AddCustomThresholds',
+  AddValueMapping = 'AddValueMapping',
+  AddAdvancedDataLink = 'AddAdvancedDataLink',
+  UseOuterJoinTransformation = 'UseOuterJoinTransformation',
+  MakePublicDashboard = 'MakePublicDashboard',
+  StreamDataToGrafana = 'StreamDataToGrafana',
+}
+
+export type Achievement = {
+  id: AchievementId;
+  title: string;
+  description?: string;
+  level: AchievementLevel;
+  link?: string;
+  completed?: boolean;
+};
