e1d1e52ca4569a8c971a57f64744b18cf7036c50
Ashley Harrison <ashley.harrison@grafana.com>
2023-12-04 15:57:23 +0000

    use bouncing logo instead of loading spinner for page loader

--- public/app/core/navigation/GrafanaRouteLoading.tsx
+++ public/app/core/navigation/GrafanaRouteLoading.tsx
@@ -1,24 +1,40 @@
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import React from 'react';
 
-import { LoadingPlaceholder, useStyles2 } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data';
+import { config } from '@grafana/runtime';
+import { useStyles2 } from '@grafana/ui';
 
 export function GrafanaRouteLoading() {
   const styles = useStyles2(getStyles);
 
   return (
-    <div className={styles.loadingPage}>
-      <LoadingPlaceholder text={'Loading...'} />
+    <div
+      className={cx('preloader', styles.loadingPage, {
+        [styles.loadingPageDockedNav]: config.featureToggles.dockedMegaMenu,
+      })}
+      aria-live="polite"
+      role="status"
+      aria-label="Loading"
+    >
+      <div className="preloader__enter">
+        <div className="preloader__bounce">
+          <div className="preloader__logo"></div>
+        </div>
+      </div>
     </div>
   );
 }
 
-const getStyles = () => ({
+const getStyles = (theme: GrafanaTheme2) => ({
   loadingPage: css({
     height: '100%',
     flexDrection: 'column',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  }),
+  loadingPageDockedNav: css({
+    backgroundColor: theme.colors.background.primary,
   }),
 });
