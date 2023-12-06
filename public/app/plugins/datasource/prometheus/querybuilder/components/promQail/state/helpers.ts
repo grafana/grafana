e998499311006fe360559dc246febb3c5077b30c
Yasir Ekinci <yas.ekinci@grafana.com>
2023-12-06 13:08:01 +0100

    PromQail: update prompt and model
    - Split up system and user prompt
    - Use latest gpt-3.5-turbo-1106 model
    - Add metric type to user prompt
    - Simpler formatting for templates

--- public/app/plugins/datasource/prometheus/querybuilder/components/promQail/state/helpers.ts
+++ public/app/plugins/datasource/prometheus/querybuilder/components/promQail/state/helpers.ts
@@ -11,15 +11,16 @@ import { PromVisualQuery } from '../../../types';
 import {
   ExplainSystemPrompt,
   GetExplainUserPrompt,
-  GetSuggestSystemPrompt,
-  SuggestSystemPromptParams,
+  SuggestSystemPrompt,
+  GetSuggestUserPrompt,
+  SuggestUserPromptParams,
 } from '../prompts';
 import { Interaction, QuerySuggestion, SuggestionType } from '../types';
 
 import { createInteraction, stateSlice } from './state';
 import { getTemplateSuggestions } from './templates';
 
-const OPENAI_MODEL_NAME = 'gpt-3.5-turbo';
+const OPENAI_MODEL_NAME = 'gpt-3.5-turbo-1106';
 const promQLTemplatesCollection = 'grafana.promql.templates';
 // actions to update the state
 const { updateInteraction } = stateSlice.actions;
@@ -77,8 +78,17 @@ export function getExplainMessage(
   ];
 }
 
-function getSuggestMessage({ promql, question, labels, templates }: SuggestSystemPromptParams): llms.openai.Message[] {
-  return [{ role: 'system', content: GetSuggestSystemPrompt({ promql, question, labels, templates }) }];
+function getSuggestMessages({
+  promql,
+  question,
+  metricType,
+  labels,
+  templates,
+}: SuggestUserPromptParams): llms.openai.Message[] {
+  return [
+    { role: 'system', content: SuggestSystemPrompt },
+    { role: 'user', content: GetSuggestUserPrompt({ promql, question, metricType, labels, templates }) },
+  ];
 }
 
 /**
@@ -372,13 +382,14 @@ export async function promQailSuggest(
 
     const resultsString = results
       .map((r) => {
-        return `PromQL: ${r.payload.promql}\nDescription: ${r.payload.description}`;
+        return `${r.payload.promql} | ${r.payload.description} (score=${(r.score * 100).toFixed(1)})`;
       })
       .join('\n');
 
-    const promptMessages = getSuggestMessage({
+    const promptMessages = getSuggestMessages({
       promql: query.metric,
       question: interaction ? interaction.prompt : '',
+      metricType: metricType,
       labels: labelNames.join(', '),
       templates: resultsString,
     });
@@ -387,7 +398,7 @@ export async function promQailSuggest(
       .streamChatCompletions({
         model: OPENAI_MODEL_NAME,
         messages: promptMessages,
-        temperature: 0,
+        temperature: 0.5,
       })
       .pipe(llms.openai.accumulateContent())
       .subscribe((response) => {
