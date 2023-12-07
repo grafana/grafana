7a8dd0f92d8b3308a4ac9c437fe65de4a331d153
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-12-07 11:13:39 -0500

    update silences API

--- pkg/services/ngalert/api/api_ruler.go
+++ pkg/services/ngalert/api/api_ruler.go
@@ -50,7 +50,7 @@ var (
 
 // RouteDeleteAlertRules deletes all alert rules the user is authorized to access in the given namespace
 // or, if non-empty, a specific group of rules in the namespace.
-// Returns http.StatusUnauthorized if user does not have access to any of the rules that match the filter.
+// Returns http.StatusForbidden if user does not have access to any of the rules that match the filter.
 // Returns http.StatusBadRequest if all rules that match the filter and the user is authorized to delete are provisioned.
 func (srv RulerSrv) RouteDeleteAlertRules(c *contextmodel.ReqContext, namespaceTitle string, group string) response.Response {
 	namespace, err := srv.store.GetNamespaceByTitle(c.Req.Context(), namespaceTitle, c.SignedInUser.GetOrgID(), c.SignedInUser)
