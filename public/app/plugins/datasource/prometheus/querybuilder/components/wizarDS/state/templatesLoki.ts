65556ffbfdc9c3508e40b434e3b8b77f1640c0a8
Matias Chomicki <matyax@gmail.com>
2023-12-07 11:46:56 +0100

    Loki templates: update with content

--- public/app/plugins/datasource/prometheus/querybuilder/components/wizarDS/state/templatesLoki.ts
+++ public/app/plugins/datasource/prometheus/querybuilder/components/wizarDS/state/templatesLoki.ts
@@ -2,21 +2,58 @@ import { Suggestion } from '../types';
 
 export const componentTemplates: Suggestion[] = [
   {
+    route: `/explore`,
+    target: `[aria-label="Query patterns"]`,
+    title: `Kickstart your query`,
+    content: `This is the 'Kick start your query' button. It will help you get started with your first query!`,
     component: 'Kickstart your query',
     explanation: `Click to see a list of operation patterns that help you quickly get started adding multiple operations to your query. These include:
       \n
       \n
-      Rate query starters
+      Log query starters
       \n
-      Histogram query starters
-      \n
-      Binary query starters`,
-    testid: 'wizard-prometheus-kickstart-your-query',
+      Metric query starters`,
+    testid: 'wizard-loki-kickstart-your-query',
     order: 1,
-    link: 'https://grafana.com/docs/grafana/latest/datasources/prometheus/query-editor/#toolbar-elements',
-    route: '',
-    target: '',
-    content: '',
-    title: '',
+    link: 'https://grafana.com/docs/grafana/latest/datasources/loki/query-editor/#toolbar-elements',
+  },
+  {
+    route: `/explore`,
+    target: `[aria-label="Label browser"]`,
+    title: `Label browser`,
+    content: `Use the Loki label browser to navigate through your labels and values, and build queries.This is the 'Kick start your query' button. It will help you get started with your first query!`,
+    component: 'Label browser',
+    explanation: `To navigate Loki and build a query:\n
+    \n
+    1. Choose labels to locate.
+    2. Search for the values of your selected labels.\n
+    3. The search field supports fuzzy search, and the label browser also supports faceting to list only possible label combinations.\n
+    4. Select the Show logs button to display log lines based on the selected labels, or select the Show logs rate button to show the rate based on metrics such as requests per second. Additionally, you can validate the selector by clicking the Validate selector button. Click Clear to start from the beginning.,\n`,
+    testid: 'wizard-loki-label-browser',
+    order: 1,
+    link: 'https://grafana.com/docs/grafana/latest/datasources/loki/query-editor/#toolbar-elements',
+  },
+  {
+    route: `/explore`,
+    target: `[data-testid="QueryEditorModeToggle"]`,
+    title: `Query builder mode`,
+    content: `Use the query builder mode to build queries using a zero-code visual interface.`,
+    component: 'Query mode: builder',
+    explanation: `Builder mode helps you build queries using a visual interface without needing to manually enter LogQL. This option is best for users who have limited or no previous experience working with Loki and LogQL.`,
+    testid: 'wizard-loki-label-browser',
+    order: 1,
+    link: 'https://grafana.com/docs/grafana/latest/datasources/loki/query-editor/#builder-mode',
+  },
+  {
+    route: `/explore`,
+    target: `[data-testid="QueryEditorModeToggle"]`,
+    title: `Code editor mode`,
+    content: `Use the query editor mode to build queries using a code editor with query validation and autocomplete support.`,
+    component: 'Query mode: code',
+    explanation: `In Code mode, you can write complex queries using a text editor with autocompletion feature, syntax highlighting, and query validation. It also contains a label browser to further help you write queries.\n
+      For more information about Loki’s query language, refer to the Loki documentation.`,
+    testid: 'wizard-loki-label-browser',
+    order: 1,
+    link: 'https://grafana.com/docs/grafana/latest/datasources/loki/query-editor/#code-mode',
   },
 ];
