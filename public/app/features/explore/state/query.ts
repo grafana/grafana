12f3cdb902b5cdcf51d5ea911b7c20419f9206ff
Matias Chomicki <matyax@gmail.com>
2023-12-11 15:13:23 +0100

    Remove comment

--- public/app/features/explore/state/query.ts
+++ public/app/features/explore/state/query.ts
@@ -703,7 +703,6 @@ export const runLoadMoreLogsQueries = createAsyncThunk<void, RunLoadMoreLogsQuer
 
     let newQuerySource: Observable<ExplorePanelData>;
 
-    // Filter queries by those explicitly requested by refId
     const logQueries = queryResponse.logsResult?.queries || [];
     const queries = logQueries.map((query: DataQuery) => ({
       ...query,
