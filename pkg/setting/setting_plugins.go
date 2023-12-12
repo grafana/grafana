702a42ba005c359f90b4cd67005bfe0bac9149bc
Giuseppe Guerra <giuseppe@guerra.in>
2023-12-12 17:39:09 +0100

    lint

--- pkg/setting/setting_plugins.go
+++ pkg/setting/setting_plugins.go
@@ -32,7 +32,7 @@ func (cfg *Cfg) readPluginSettings(iniFile *ini.File) error {
 	cfg.PluginSettings = extractPluginSettings(iniFile.Sections())
 	cfg.PluginSkipPublicKeyDownload = pluginsSection.Key("public_key_retrieval_disabled").MustBool(false)
 	cfg.PluginForcePublicKeyDownload = pluginsSection.Key("public_key_retrieval_on_startup").MustBool(false)
-	
+
 	cfg.PluginsAllowUnsigned = readPluginIDsList(pluginsSection.Key("allow_loading_unsigned_plugins").MustString(""))
 	cfg.DisablePlugins = readPluginIDsList(pluginsSection.Key("disable_plugins").MustString(""))
 	cfg.HideAngularDeprecation = readPluginIDsList(pluginsSection.Key("hide_angular_deprecation").MustString(""))
