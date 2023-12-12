d8fab9cf795d71d61eb21df9f20e836020dd35ba
nmarrs <nathanielmarrs@gmail.com>
2023-12-12 11:54:02 -0800

    Add tracking for deprecated panels

--- public/app/features/dashboard/utils/tracking.ts
+++ public/app/features/dashboard/utils/tracking.ts
@@ -11,6 +11,17 @@ export function trackDashboardLoaded(dashboard: DashboardModel, versionBeforeMig
       return r;
     }, {});
 
+  // Track number and type of deprecated panels still in use
+  const deprecatedGraphPanelCount = dashboard.panels.filter((panel) => panel.type === 'graph').length;
+  const deprecatedTablePanelCount = dashboard.panels.filter((panel) => panel.type === 'table-old').length;
+  const deprecatedStatPanelCount = dashboard.panels.filter((panel) => panel.type === 'grafana-singlestat-panel').length;
+  const deprecatedPieChartPanelCount = dashboard.panels.filter(
+    (panel) => panel.type === 'grafana-piechart-panel'
+  ).length;
+  const deprecatedWorldmapPanelCount = dashboard.panels.filter(
+    (panel) => panel.type === 'grafana-worldmap-panel'
+  ).length;
+
   DashboardInteractions.dashboardInitialized({
     uid: dashboard.uid,
     title: dashboard.title,
@@ -18,6 +29,11 @@ export function trackDashboardLoaded(dashboard: DashboardModel, versionBeforeMig
     schemaVersion: dashboard.schemaVersion,
     version_before_migration: versionBeforeMigration,
     panels_count: dashboard.panels.length,
+    deprecated_graph_panel_count: deprecatedGraphPanelCount,
+    deprecated_table_panel_count: deprecatedTablePanelCount,
+    deprecated_stat_panel_count: deprecatedStatPanelCount,
+    deprecated_piechart_panel_count: deprecatedPieChartPanelCount,
+    deprecated_worldmap_panel_count: deprecatedWorldmapPanelCount,
     ...variables,
   });
 }
