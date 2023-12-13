150f8b2fc610387bcb4b3ab3f97f355c3169a3f1
Clarity-89 <homes89@ukr.net>
2023-12-13 16:28:50 +0200

    Fix labels rules

--- public/app/features/alerting/unified/components/rule-editor/LabelsField.tsx
+++ public/app/features/alerting/unified/components/rule-editor/LabelsField.tsx
@@ -159,7 +159,7 @@ const LabelsWithSuggestions: FC<{ dataSourceName: string }> = ({ dataSourceName 
                     <InputControl
                       name={`labels.${index}.key`}
                       control={control}
-                      rules={{ required: index > 0 ? false : 'Required.' }}
+                      rules={{ required: Boolean(labels[index]?.value) ? 'Required.' : false }}
                       render={({ field: { onChange, ref, ...rest } }) => {
                         return (
                           <AlertLabelDropdown
@@ -186,7 +186,7 @@ const LabelsWithSuggestions: FC<{ dataSourceName: string }> = ({ dataSourceName 
                     <InputControl
                       control={control}
                       name={`labels.${index}.value`}
-                      rules={{ required: index > 0 ? false : 'Required.' }}
+                      rules={{ required: Boolean(labels[index]?.value) ? 'Required.' : false }}
                       render={({ field: { onChange, ref, ...rest } }) => {
                         return (
                           <AlertLabelDropdown
