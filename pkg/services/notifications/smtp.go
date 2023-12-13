15c6d923086e3ad8115e11f790e6b1d32f3b0a1a
Owen Smallwood <owen.smallwood@grafana.com>
2023-12-13 14:15:35 -0600

    add static headers first

--- pkg/services/notifications/smtp.go
+++ pkg/services/notifications/smtp.go
@@ -62,13 +62,13 @@ func (sc *SmtpClient) Send(messages ...*Message) (int, error) {
 // buildEmail converts the Message DTO to a gomail message.
 func (sc *SmtpClient) buildEmail(msg *Message) *gomail.Message {
 	m := gomail.NewMessage()
-	m.SetHeader("From", msg.From)
-	m.SetHeader("To", msg.To...)
-	m.SetHeader("Subject", msg.Subject)
 	// add all static headers to the email message
 	for h, val := range sc.cfg.StaticHeaders {
 		m.SetHeader(h, val)
 	}
+	m.SetHeader("From", msg.From)
+	m.SetHeader("To", msg.To...)
+	m.SetHeader("Subject", msg.Subject)
 	sc.setFiles(m, msg)
 	for _, replyTo := range msg.ReplyTo {
 		m.SetAddressHeader("Reply-To", replyTo, "")
