db7bd0caa99dfc9bf8be66ea4a0e43c695094772
eledobleefe <laura.fernandez@grafana.com>
2023-12-14 12:04:14 +0100

    Delete store code, shorten button text and add tooltip

--- public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.tsx
+++ public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { UrlQueryValue } from '@grafana/data';
-import { Button } from '@grafana/ui';
-// import { useGrafana } from 'app/core/context/GrafanaContext';
+import { Button, Tooltip } from '@grafana/ui';
 
 export interface ReturnToPreviousProps {
   href: UrlQueryValue;
@@ -12,26 +11,32 @@ export interface ReturnToPreviousProps {
 }
 
 export const ReturnToPrevious = ({ href, title, children }: ReturnToPreviousProps) => {
-  // const { chrome } = useGrafana();
   const history = useHistory();
 
   const handleOnClick = () => {
-    //   chrome.setReturnToPrevious({ show: false, href: '', title: '' });
     href && history.push(href.toString());
   };
 
-  return (
-    <Button
-      icon="angle-left"
-      size="sm"
-      variant="secondary"
-      onClick={handleOnClick}
-      title={title?.toString()}
-      className="return-to-previous"
-    >
-      Back to {children?.toString()}
-    </Button>
-  );
+  const titleLength = 15;
+  const shortenTitle =
+    children && children.toString().length > titleLength
+      ? children.toString().slice(0, titleLength).concat('...')
+      : children;
+  const button = () => {
+    return (
+      <Button
+        icon="angle-left"
+        size="sm"
+        variant="secondary"
+        onClick={handleOnClick}
+        title={title?.toString()}
+        className="return-to-previous"
+      >
+        Back to {children?.toString().slice(0, 15).concat('...')}
+      </Button>
+    );
+  };
+  return shortenTitle && children ? <Tooltip content={`Back to ${children.toString()}`}>{button()}</Tooltip> : button();
 };
 
 ReturnToPrevious.displayName = 'ReturnToPrevious';
