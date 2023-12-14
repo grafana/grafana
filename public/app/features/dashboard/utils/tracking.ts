a17548d71115c767c2c902079e3509250fb37057
nmarrs <nathanielmarrs@gmail.com>
2023-12-14 13:31:00 -0800

    Refactor dashboard tracking code

--- public/app/features/dashboard/utils/tracking.ts
+++ public/app/features/dashboard/utils/tracking.ts
@@ -6,21 +6,18 @@ export function trackDashboardLoaded(dashboard: DashboardModel, versionBeforeMig
   // Count the different types of variables
   const variables = dashboard.templating.list
     .map((v) => v.type)
-    .reduce((r, k) => {
+    .reduce((r: Record<string, number>, k) => {
       r[variableName(k)] = 1 + r[variableName(k)] || 1;
       return r;
     }, {});
 
-  // Track number and type of deprecated panels still in use
-  const deprecatedGraphPanelCount = dashboard.panels.filter((panel) => panel.type === 'graph').length;
-  const deprecatedTablePanelCount = dashboard.panels.filter((panel) => panel.type === 'table-old').length;
-  const deprecatedStatPanelCount = dashboard.panels.filter((panel) => panel.type === 'grafana-singlestat-panel').length;
-  const deprecatedPieChartPanelCount = dashboard.panels.filter(
-    (panel) => panel.type === 'grafana-piechart-panel'
-  ).length;
-  const deprecatedWorldmapPanelCount = dashboard.panels.filter(
-    (panel) => panel.type === 'grafana-worldmap-panel'
-  ).length;
+  // Track number and type of panels in use
+  const panels = dashboard.panels
+    .map((p) => p.type)
+    .reduce((r: Record<string, number>, p) => {
+      r[panelName(p)] = 1 + r[panelName(p)] || 1;
+      return r;
+    }, {});
 
   DashboardInteractions.dashboardInitialized({
     uid: dashboard.uid,
@@ -29,13 +26,10 @@ export function trackDashboardLoaded(dashboard: DashboardModel, versionBeforeMig
     schemaVersion: dashboard.schemaVersion,
     version_before_migration: versionBeforeMigration,
     panels_count: dashboard.panels.length,
-    deprecated_graph_panel_count: deprecatedGraphPanelCount,
-    deprecated_table_panel_count: deprecatedTablePanelCount,
-    deprecated_stat_panel_count: deprecatedStatPanelCount,
-    deprecated_piechart_panel_count: deprecatedPieChartPanelCount,
-    deprecated_worldmap_panel_count: deprecatedWorldmapPanelCount,
+    ...panels,
     ...variables,
   });
 }
 
 const variableName = (type: string) => `variable_type_${type}_count`;
+const panelName = (type: string) => `panel_type_${type}_count`;
