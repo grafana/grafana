8c114ccdd122f95f12215b184dda80f6653ba5cb
khushijain21 <khushij393@gmail.com>
2023-12-14 15:53:36 +0530

    usersession

--- public/app/features/admin/UserSessions.tsx
+++ public/app/features/admin/UserSessions.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/css';
 import React, { PureComponent } from 'react';
 
-import { ConfirmButton, ConfirmModal, Button } from '@grafana/ui';
+import { ConfirmButton, ConfirmModal, Button, Stack } from '@grafana/ui';
 import { contextSrv } from 'app/core/core';
 import { i18nDate } from 'app/core/internationalization';
 import { AccessControlAction, UserSession } from 'app/types';
@@ -48,8 +48,8 @@ class BaseUserSessions extends PureComponent<Props, State> {
     const { sessions } = this.props;
     const { showLogoutModal } = this.state;
 
-    const logoutFromAllDevicesClass = css`
-      margin-top: 0.8rem;
+    const marginBottomClass = css`
+      margin-bottom: 4rem;
     `;
 
     const canLogout = contextSrv.hasPermission(AccessControlAction.UsersLogout);
@@ -57,59 +57,62 @@ class BaseUserSessions extends PureComponent<Props, State> {
     return (
       <>
         <h3 className="page-heading">Sessions</h3>
-        <div className="gf-form-group">
-          <div className="gf-form">
-            <table className="filter-table form-inline">
-              <thead>
-                <tr>
-                  <th>Last seen</th>
-                  <th>Logged on</th>
-                  <th>IP address</th>
-                  <th colSpan={2}>Browser and OS</th>
-                </tr>
-              </thead>
-              <tbody>
-                {sessions &&
-                  sessions.map((session, index) => (
-                    <tr key={`${session.id}-${index}`}>
-                      <td>{session.isActive ? 'Now' : session.seenAt}</td>
-                      <td>{i18nDate(session.createdAt, { dateStyle: 'long' })}</td>
-                      <td>{session.clientIp}</td>
-                      <td>{`${session.browser} on ${session.os} ${session.osVersion}`}</td>
-                      <td>
-                        <div className="pull-right">
-                          {canLogout && (
-                            <ConfirmButton
-                              confirmText="Confirm logout"
-                              confirmVariant="destructive"
-                              onConfirm={this.onSessionRevoke(session.id)}
-                              autoFocus
-                            >
-                              Force logout
-                            </ConfirmButton>
-                          )}
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-          <div className={logoutFromAllDevicesClass}>
-            {canLogout && sessions.length > 0 && (
-              <Button variant="secondary" onClick={this.showLogoutConfirmationModal} ref={this.forceAllLogoutButton}>
-                Force logout from all devices
-              </Button>
-            )}
-            <ConfirmModal
-              isOpen={showLogoutModal}
-              title="Force logout from all devices"
-              body="Are you sure you want to force logout from all devices?"
-              confirmText="Force logout"
-              onConfirm={this.onAllSessionsRevoke}
-              onDismiss={this.dismissLogoutConfirmationModal}
-            />
-          </div>
+        <div className={marginBottomClass}>
+          <Stack direction="column" gap={1.5}>
+            <div>
+              <table className="filter-table form-inline">
+                <thead>
+                  <tr>
+                    <th>Last seen</th>
+                    <th>Logged on</th>
+                    <th>IP address</th>
+                    <th colSpan={2}>Browser and OS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sessions &&
+                    sessions.map((session, index) => (
+                      <tr key={`${session.id}-${index}`}>
+                        <td>{session.isActive ? 'Now' : session.seenAt}</td>
+                        <td>{i18nDate(session.createdAt, { dateStyle: 'long' })}</td>
+                        <td>{session.clientIp}</td>
+                        <td>{`${session.browser} on ${session.os} ${session.osVersion}`}</td>
+                        <td>
+                          <div className="pull-right">
+                            {canLogout && (
+                              <ConfirmButton
+                                confirmText="Confirm logout"
+                                confirmVariant="destructive"
+                                onConfirm={this.onSessionRevoke(session.id)}
+                                autoFocus
+                              >
+                                Force logout
+                              </ConfirmButton>
+                            )}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div>
+              {canLogout && sessions.length > 0 && (
+                <Button variant="secondary" onClick={this.showLogoutConfirmationModal} ref={this.forceAllLogoutButton}>
+                  Force logout from all devices
+                </Button>
+              )}
+              <ConfirmModal
+                isOpen={showLogoutModal}
+                title="Force logout from all devices"
+                body="Are you sure you want to force logout from all devices?"
+                confirmText="Force logout"
+                onConfirm={this.onAllSessionsRevoke}
+                onDismiss={this.dismissLogoutConfirmationModal}
+              />
+            </div>
+          </Stack>
         </div>
       </>
     );
