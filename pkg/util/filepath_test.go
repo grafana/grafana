e1fcd12e2c318c9861ecca66f3a0517cf6c908c1
Will Browne <will.browne@grafana.com>
2023-12-15 16:11:35 +0000

    fix tests

--- pkg/util/filepath_test.go
+++ pkg/util/filepath_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,16 +17,16 @@ func TestCleanRelativePath(t *testing.T) {
 			expectedPath: ".",
 		},
 		{
-			input:        "/test/test.txt",
-			expectedPath: "test/test.txt",
+			input:        filepath.Join(string(filepath.Separator), "test", "test.txt"),
+			expectedPath: filepath.Join("test", "test.txt"),
 		},
 		{
-			input:        "../../test/test.txt",
-			expectedPath: "test/test.txt",
+			input:        filepath.Join("..", string(filepath.Separator), "..", string(filepath.Separator), "test", "test.txt"),
+			expectedPath: filepath.Join("test", "test.txt"),
 		},
 		{
-			input:        "./../test/test.txt",
-			expectedPath: "test/test.txt",
+			input:        filepath.Join(".", string(filepath.Separator), "..", string(filepath.Separator), "test", "test.txt"),
+			expectedPath: filepath.Join("test", "test.txt"),
 		},
 	}
 
