cacae6a43b73f340fb21af376bffbc2d04e6c028
Oscar Kilhed <oscar.kilhed@grafana.com>
2023-12-15 11:27:01 +0100

    Transformations: Move formatString transformation to private preview (#79498)
    * Move moving average and cumulative sum to private preview
    
    * update docs
    
    * move formatString to private preview
    
    * rebuild docs
    
    * undo changes that don't belong to this commit
    
    * undo cumulative/window featureflag
    
    * fix case

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -982,7 +982,7 @@ var (
 		{
 			Name:         "formatString",
 			Description:  "Enable format string transformer",
-			Stage:        FeatureStageExperimental,
+			Stage:        FeatureStagePrivatePreview,
 			FrontendOnly: true,
 			Owner:        grafanaBiSquad,
 			Created:      time.Date(2023, time.October, 13, 12, 0, 0, 0, time.UTC),
