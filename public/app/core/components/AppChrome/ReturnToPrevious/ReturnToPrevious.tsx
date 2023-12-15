f02a3d6b15871c1322efc250a613225a3801e1b0
eledobleefe <laura.fernandez@grafana.com>
2023-12-15 13:05:17 +0100

    Use button group

--- public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.tsx
+++ public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { UrlQueryValue } from '@grafana/data';
-import { Button, Tooltip, useStyles2 } from '@grafana/ui';
+import { Button, ButtonGroup, IconButton, Tooltip, useStyles2 } from '@grafana/ui';
 
 export interface ReturnToPreviousProps {
   href: UrlQueryValue;
@@ -22,16 +22,19 @@ export const ReturnToPrevious = ({ href, title, children }: ReturnToPreviousProp
   const shortenTitle = children && children.toString().length > titleLength ? true : false;
   const button = () => {
     return (
-      <Button
-        icon="angle-left"
-        size="sm"
-        variant="secondary"
-        onClick={handleOnClick}
-        title={title?.toString()}
-        className={styles.returnToPrevious}
-      >
-        Back to {children?.toString()}
-      </Button>
+      <ButtonGroup>
+        <Button
+          icon="angle-left"
+          size="sm"
+          variant="secondary"
+          onClick={handleOnClick}
+          title={title?.toString()}
+          className={styles.returnToPrevious}
+        >
+          Back to {children?.toString()}
+        </Button>
+        <IconButton name="times" aria-label="Close" variant="secondary" />
+      </ButtonGroup>
     );
   };
   return shortenTitle && children ? <Tooltip content={`Back to ${children.toString()}`}>{button()}</Tooltip> : button();
