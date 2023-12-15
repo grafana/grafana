d05f7ef294129c68441b07397855b8999e86de7d
Ivan Ortega <ivanortegaalba@gmail.com>
2023-12-15 10:14:54 +0100

    Compare properly the options after discarding

--- public/app/features/dashboard-scene/scene/DashboardScene.test.tsx
+++ public/app/features/dashboard-scene/scene/DashboardScene.test.tsx
@@ -76,38 +76,35 @@ describe('DashboardScene', () => {
 
       it('A change to refresh picker interval settings should set isDirty true', () => {
         const refreshPicker = dashboardSceneGraph.getRefreshPicker(scene)!;
-        // const prevState = [...refreshPicker.state.intervals!];
+        const prevState = [...refreshPicker.state.intervals!];
         refreshPicker.setState({ intervals: ['10s'] });
 
         expect(scene.state.isDirty).toBe(true);
 
-        // TODO: Make the dashboard to restore the defaults on discard
-        // scene.onDiscard();
-        // expect(refreshPicker.state.intervals).toEqual(prevState);
+        scene.onDiscard();
+        expect(dashboardSceneGraph.getRefreshPicker(scene)!.state.intervals).toEqual(prevState);
       });
 
       it('A change to time picker visibility settings should set isDirty true', () => {
         const dashboardControls = dashboardSceneGraph.getDashboardControls(scene)!;
-        // const prevState = dashboardControls.state.hideTimeControls;
+        const prevState = dashboardControls.state.hideTimeControls;
         dashboardControls.setState({ hideTimeControls: true });
 
         expect(scene.state.isDirty).toBe(true);
 
-        // TODO: Make the dashboard to restore the defaults on discard
-        // scene.onDiscard();
-        // expect(dashboardControls.state.hideTimeControls).toEqual(prevState);
+        scene.onDiscard();
+        expect(dashboardSceneGraph.getDashboardControls(scene)!.state.hideTimeControls).toEqual(prevState);
       });
 
       it('A change to time zone should set isDirty true', () => {
-        const timeRange = scene.state.$timeRange!;
-        // const prevState = timeRange.state.timeZone;
+        const timeRange = sceneGraph.getTimeRange(scene)!;
+        const prevState = timeRange.state.timeZone;
         timeRange.setState({ timeZone: 'UTC' });
 
         expect(scene.state.isDirty).toBe(true);
 
-        // TODO: Make the dashboard to restore the defaults on discard
-        // scene.onDiscard();
-        // expect(timeRange.state.timeZone).toBe(prevState);
+        scene.onDiscard();
+        expect(sceneGraph.getTimeRange(scene)!.state.timeZone).toBe(prevState);
       });
     });
   });
