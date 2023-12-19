7db0742d0002f1b2db67b2d47b754d3b5619edd1
Mihai Doarna <mihai.doarna@grafana.com>
2023-12-19 16:39:19 +0200

    add basic validation for sso settings

--- pkg/services/ssosettings/errors.go
+++ pkg/services/ssosettings/errors.go
@@ -1,7 +1,15 @@
 package ssosettings
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/grafana/grafana/pkg/util/errutil"
+)
 
 var (
 	ErrNotFound = errors.New("not found")
+
+	ErrInvalidSettings   = errutil.ValidationFailed("sso.settings", errutil.WithPublicMessage("settings field is invalid"))
+	ErrEmptyClientId     = errutil.ValidationFailed("sso.emptyClientId", errutil.WithPublicMessage("settings.clientId cannot be empty"))
+	ErrEmptyClientSecret = errutil.ValidationFailed("sso.emptyClientSecret", errutil.WithPublicMessage("settings.clientSecret cannot be empty"))
 )
