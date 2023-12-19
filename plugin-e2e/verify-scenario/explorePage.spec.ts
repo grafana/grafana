bb40a771623ad990312181888aa90a403f736e35
Erik Sundell <erik.sundell87@gmail.com>
2023-12-19 09:30:24 +0100

    add explore page tests

---
+++ plugin-e2e/verify-scenario/explorePage.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@grafana/plugin-e2e';
+
+import { prometheusLabels } from './mocks/resources';
+
+test('query data response should be OK when query is valid', async ({ explorePage, page }) => {
+  await explorePage.datasource.set('gdev-testdata');
+  await expect(explorePage.runQuery()).toBeOK();
+});
+
+test('query data response should not be OK when query is invalid', async ({ explorePage }) => {
+  await explorePage.datasource.set('gdev-testdata');
+  const queryEditorRow = await explorePage.getQueryEditorRow('A');
+  await queryEditorRow.getByLabel('Labels').fill('invalid-label-format');
+  await expect(explorePage.runQuery()).not.toBeOK();
+});
