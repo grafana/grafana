280c2c5ab786dc012d46e826b3471f20e2f067c7
Darren Janeczek <darren.janeczek@grafana.com>
2023-12-22 12:44:24 -0500

    fix: prettier

--- public/app/features/trails/AutomaticMetricQueries/graph-builders/percentiles.ts
+++ public/app/features/trails/AutomaticMetricQueries/graph-builders/percentiles.ts
@@ -3,8 +3,5 @@ import { PanelBuilders } from '@grafana/scenes';
 import { AutoQueryDef } from '../types';
 
 export function percentilesGraphBuilder(def: AutoQueryDef) {
-  return PanelBuilders.timeseries()
-    .setTitle(def.title)
-    .setUnit(def.unit)
-    .setCustomFieldConfig('fillOpacity', 9);
+  return PanelBuilders.timeseries().setTitle(def.title).setUnit(def.unit).setCustomFieldConfig('fillOpacity', 9);
 }
