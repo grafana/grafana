d90e259746065e26a5d122dfb456ce4d0a1afaa5
Ryan McKinley <ryantxu@gmail.com>
2024-01-01 16:01:40 -0800

    use emotion css

--- public/app/features/dashboard/components/TransformationsEditor/TransformationFilter.tsx
+++ public/app/features/dashboard/components/TransformationsEditor/TransformationFilter.tsx
@@ -4,7 +4,6 @@ import React, { useMemo } from 'react';
 import {
   DataTransformerConfig,
   GrafanaTheme2,
-  SelectableValue,
   StandardEditorContext,
   StandardEditorsRegistryItem,
 } from '@grafana/data';
@@ -21,50 +20,53 @@ interface TransformationFilterProps {
   onChange: (index: number, config: DataTransformerConfig) => void;
 }
 
-const dataFramesSourceOpts: Array<SelectableValue<DataTopic>> = [
-  { value: DataTopic.Series, label: 'Series' },
-  { value: DataTopic.Annotations, label: 'Annotations' },
-];
-
 export const TransformationFilter = ({ index, data, config, onChange }: TransformationFilterProps) => {
   const styles = useStyles2(getStyles);
-  const context = useMemo(() => {
-    // eslint-disable-next-line
-    return { data: data.series } as StandardEditorContext<unknown>;
-  }, [data]);
+
+  const opts = useMemo(() => {
+    return {
+      // eslint-disable-next-line
+      context: { data: data.series } as StandardEditorContext<unknown>,
+      showTopic: true || data.annotations?.length || config.topic?.length,
+      showFilter: config.topic !== DataTopic.Annotations,
+      source: [
+        { value: DataTopic.Series, label: `Query results` },
+        { value: DataTopic.Annotations, label: `Annotation data` },
+      ],
+    };
+  }, [data, config.topic]);
 
   return (
-    <>
-      <div>
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <div className="gf-form-label width-8">Topic</div>
+    <div className={styles.wrapper}>
+      <Field label="Apply transformation to">
+        <>
+          {opts.showTopic && (
             <Select
-              className="width-18"
-              options={dataFramesSourceOpts}
-              value={dataFramesSourceOpts.find((v) => v.value === config.topic)}
+              isClearable={true}
+              options={opts.source}
+              value={opts.source.find((v) => v.value === config.topic)}
+              placeholder={opts.source[0].label}
+              className={styles.padded}
               onChange={(option) => {
                 onChange(index, {
                   ...config,
-                  topic: option.value,
+                  topic: option?.value,
                 });
               }}
             />
-          </div>
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <Field label="Apply transformation to">
-          <FrameSelectionEditor
-            value={config.filter!}
-            context={context}
-            // eslint-disable-next-line
-            item={{} as StandardEditorsRegistryItem}
-            onChange={(filter) => onChange(index, { ...config, filter })}
-          />
-        </Field>
-      </div>
-    </>
+          )}
+          {opts.showFilter && (
+            <FrameSelectionEditor
+              value={config.filter!}
+              context={opts.context}
+              // eslint-disable-next-line
+              item={{} as StandardEditorsRegistryItem}
+              onChange={(filter) => onChange(index, { ...config, filter })}
+            />
+          )}
+        </>
+      </Field>
+    </div>
   );
 };
 
@@ -72,13 +74,16 @@ const getStyles = (theme: GrafanaTheme2) => {
   const borderRadius = theme.shape.radius.default;
 
   return {
-    wrapper: css`
-      padding: ${theme.spacing(2)};
-      border: 2px solid ${theme.colors.background.secondary};
-      border-top: none;
-      border-radius: 0 0 ${borderRadius} ${borderRadius};
-      position: relative;
-      top: -4px;
-    `,
+    wrapper: css({
+      padding: theme.spacing(2),
+      border: `2px solid ${theme.colors.background.secondary}`,
+      borderTop: `none`,
+      borderRadius: `0 0 ${borderRadius} ${borderRadius}`,
+      position: `relative`,
+      top: `-4px`,
+    }),
+    padded: css({
+      marginBottom: theme.spacing(1),
+    }),
   };
 };
