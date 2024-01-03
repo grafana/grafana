44012b5cd6c587afe717d85b591e0443e384fa3f
Dominik Prokop <dominik.prokop@grafana.com>
2024-01-03 11:34:00 +0100

    DashboardScene: Enable scene tracking information alt

--- packages/grafana-runtime/src/analytics/utils.ts
+++ packages/grafana-runtime/src/analytics/utils.ts
@@ -43,14 +43,17 @@ export const reportPageview = () => {
  *
  * @public
  */
-export const reportInteraction = (interactionName: string, properties?: Record<string, unknown>) => {
-  getEchoSrv().addEvent<InteractionEchoEvent>({
-    type: EchoEventType.Interaction,
-    payload: {
-      interactionName,
-      properties,
+export const reportInteraction = (interactionName: string, properties?: Record<string, unknown>, meta?: {}) => {
+  getEchoSrv().addEvent<InteractionEchoEvent>(
+    {
+      type: EchoEventType.Interaction,
+      payload: {
+        interactionName,
+        properties,
+      },
     },
-  });
+    meta
+  );
 };
 
 /**
