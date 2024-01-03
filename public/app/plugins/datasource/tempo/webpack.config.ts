ee8d0822be384a987cf18d84d397895f053c3352
Fabrizio <fabrizio.casati@grafana.com>
2024-01-03 17:39:22 +0100

    Remove temporary hacks

--- public/app/plugins/datasource/tempo/webpack.config.ts
+++ public/app/plugins/datasource/tempo/webpack.config.ts
@@ -1,17 +1,3 @@
 import config from '@grafana/plugin-configs/webpack.config';
 
-const configWithFallback = async (env: Record<string, unknown>) => {
-  const response = await config(env);
-  if (response !== undefined && response.resolve !== undefined) {
-    response.resolve.fallback = {
-      ...response.resolve.fallback,
-      stream: require.resolve('stream-browserify'),
-      events: require.resolve('events/'),
-      string_decoder: require.resolve('string_decoder/'),
-      buffer: require.resolve('buffer/'),
-    };
-  }
-  return response;
-};
-
-export default configWithFallback;
+export default config;
