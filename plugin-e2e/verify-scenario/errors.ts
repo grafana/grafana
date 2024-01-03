5e08cc5757861671ad6b6e3cfb30f3a8dbdc3a86
Erik Sundell <erik.sundell87@gmail.com>
2024-01-03 14:35:59 +0100

    update message frame

--- plugin-e2e/verify-scenario/errors.ts
+++ plugin-e2e/verify-scenario/errors.ts
@@ -1,4 +1,4 @@
 export const formatExpectError = (message: string) => {
-  return `@grafana/plugin-e2e error: ${message}. 
+  return `Error while verifying @grafana/plugin-e2e scenarios: ${message}. 
     See https://github.com/grafana/grafana/blob/main/plugin-e2e/verify-scenario/README.md for more information.`;
 };
