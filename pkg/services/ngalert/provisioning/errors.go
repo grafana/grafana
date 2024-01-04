d4bed18055b3cf11114091332fd323ba6970d46e
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-01-04 15:55:51 -0500

    Update pkg/services/ngalert/provisioning/errors.go
    Co-authored-by: Alexander Weaver <weaver.alex.d@gmail.com>

--- pkg/services/ngalert/provisioning/errors.go
+++ pkg/services/ngalert/provisioning/errors.go
@@ -13,7 +13,7 @@ var ErrPermissionDenied = errors.New("permission denied")
 
 var (
 	ErrNoAlertmanagerConfiguration  = errutil.Internal("alerting.notification.configMissing", errutil.WithPublicMessage("No alertmanager configuration present in this organization"))
-	ErrBadAlertmanagerConfiguration = errutil.Internal("alerting.notification.configCorrupted").MustTemplate("Failed to unmarshall the Alertmanager configuration", errutil.WithPublic("Current Alertmanager configuration in the storage is corrupted. Reset the configuration or rollback to the recent valid one."))
+	ErrBadAlertmanagerConfiguration = errutil.Internal("alerting.notification.configCorrupted").MustTemplate("Failed to unmarshal the Alertmanager configuration", errutil.WithPublic("Current Alertmanager configuration in the storage is corrupted. Reset the configuration or rollback to a recent valid one."))
 )
 
 func makeErrBadAlertmanagerConfiguration(err error) error {
