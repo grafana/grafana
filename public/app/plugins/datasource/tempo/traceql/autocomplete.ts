d14533042f588d7285301dd1d64fdcb5f923eece
harrymauray05 <harrymaurya05@gmail.com>
2024-01-07 23:33:24 +0530

    traceql : adding } when { is inserted automatically

--- public/app/plugins/datasource/tempo/traceql/autocomplete.ts
+++ public/app/plugins/datasource/tempo/traceql/autocomplete.ts
@@ -305,8 +305,8 @@ export class CompletionProvider implements monacoTypes.languages.CompletionItemP
         return [];
       }
       case 'EMPTY': {
-        return this.getScopesCompletions('{ ')
-          .concat(this.getIntrinsicsCompletions('{ '))
+        return this.getScopesCompletions('{ ','$0 }')
+          .concat(this.getIntrinsicsCompletions('{ ','$0 }'))
           .concat(this.getTagsCompletions('{ .'));
       }
       case 'SPANSET_EMPTY':
@@ -500,7 +500,7 @@ function getRangeAndOffset(monaco: Monaco, model: monacoTypes.editor.ITextModel,
     column: position.column,
     lineNumber: position.lineNumber,
   };
-
+  
   const offset = model.getOffsetAt(positionClone);
   return { offset, range };
 }
