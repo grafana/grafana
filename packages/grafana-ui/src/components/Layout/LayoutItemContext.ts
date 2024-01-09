8511719b3939ef748a991120cf505ae26e998fea
Leon Sorokin <leeoniya@gmail.com>
2024-01-08 22:33:35 -0600

    add note

--- packages/grafana-ui/src/components/Layout/LayoutItemContext.ts
+++ packages/grafana-ui/src/components/Layout/LayoutItemContext.ts
@@ -4,6 +4,18 @@ export interface LayoutItemContextProps {
   setAnchoredCount: (nextCount: ((prevCount: number) => number) | number) => void;
 }
 
+/**
+ * Provides an API for downstream components (e.g. within panels) to inform the layout
+ * how many anchored tooltips or context menus could overflow the panel bounds. The layout
+ * system can then boost the z-index of items with any anchored contents to prevent the overflown
+ * content from rendering underneath adjacent layout items (e.g. other panels) that naturally
+ * render later/higher in the stacking order
+ *
+ * This is used by VizTooltips and Annotations, which anchor to data points or time range within
+ * the viz drawing area
+ *
+ * @internal
+ */
 export const LayoutItemContext = createContext<LayoutItemContextProps>({
   setAnchoredCount: () => {},
 });
