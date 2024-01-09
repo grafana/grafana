05d1da1d205011ff86085f22478dec6efeca9ac4
eledobleefe <laura.fernandez@grafana.com>
2024-01-09 13:23:03 +0100

    Create ReturnToPrevious component

---
+++ public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.tsx
@@ -0,0 +1,64 @@
+import { css } from '@emotion/css';
+import React from 'react';
+
+import { GrafanaTheme2, UrlQueryValue } from '@grafana/data';
+import { locationService } from '@grafana/runtime';
+import { Button, ButtonGroup, useStyles2 } from '@grafana/ui';
+import { useQueryParams } from 'app/core/hooks/useQueryParams';
+
+export interface ReturnToPreviousProps {
+  href: UrlQueryValue;
+  title: UrlQueryValue;
+  children: UrlQueryValue;
+}
+
+export const ReturnToPrevious = ({ href, title, children }: ReturnToPreviousProps) => {
+  const styles = useStyles2(getStyles);
+  const handleOnClick = () => {
+    href && locationService.push(href.toString());
+  };
+  const [, setParams] = useQueryParams();
+  const closeButton = () => {
+    setParams({ __returnToTitle: null, __returnToUrl: null });
+    const currentLocation = locationService.getLocation();
+    locationService.push(currentLocation);
+  };
+
+  const shortenTitle = (title: UrlQueryValue) => {
+    const titleLength = 37;
+    if (title && title.toString().length > titleLength) {
+      return title.toString().substring(0, titleLength) + '...';
+    } else {
+      return title;
+    }
+  };
+  return (
+    <ButtonGroup className={styles.wrapper}>
+      <Button
+        icon="angle-left"
+        size="sm"
+        variant="primary"
+        fill="outline"
+        onClick={handleOnClick}
+        title={title?.toString()}
+        className={styles.returnToPrevious}
+      >
+        Back to {shortenTitle(children)}
+      </Button>
+      <Button icon="times" aria-label="Close" variant="primary" fill="outline" size="sm" onClick={closeButton} />
+    </ButtonGroup>
+  );
+};
+const getStyles = (theme: GrafanaTheme2) => ({
+  returnToPrevious: css({
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  }),
+  wrapper: css({
+    backgroundColor: theme.colors.background.secondary,
+    display: 'flex',
+    justifyContent: 'space-between',
+  }),
+});
+
+ReturnToPrevious.displayName = 'ReturnToPrevious';
