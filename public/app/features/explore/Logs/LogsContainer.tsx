5811bc8c42e797f10ce7042bdacb7128f4244a0a
Matias Chomicki <matyax@gmail.com>
2024-01-10 17:23:34 +0100

    Logs container: prevent unnecessary rerenders from arrow functions

--- public/app/features/explore/Logs/LogsContainer.tsx
+++ public/app/features/explore/Logs/LogsContainer.tsx
@@ -244,6 +244,14 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
     );
   };
 
+  addResultsToCache = () => {
+    this.props.addResultsToCache(this.props.exploreId);
+  }
+
+  clearCache = () => {
+    this.props.clearCache(this.props.exploreId);
+  }
+
   render() {
     const {
       loading,
@@ -267,7 +275,6 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
       splitOpenFn,
       isLive,
       exploreId,
-      addResultsToCache,
       clearCache,
       logsVolume,
       scrollElement,
@@ -330,8 +337,8 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
             getRowContextQuery={this.getLogRowContextQuery}
             getLogRowContextUi={this.getLogRowContextUi}
             getFieldLinks={this.getFieldLinks}
-            addResultsToCache={() => addResultsToCache(exploreId)}
-            clearCache={() => clearCache(exploreId)}
+            addResultsToCache={this.addResultsToCache}
+            clearCache={this.clearCache}
             eventBus={this.props.eventBus}
             panelState={this.props.panelState}
             logsFrames={this.props.logsFrames}
