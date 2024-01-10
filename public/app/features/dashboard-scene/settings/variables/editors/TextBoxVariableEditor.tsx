18bddd0d33930d81df79f8b0a834e654ad255e33
Alexandra Vargas <alexa1866@gmail.com>
2024-01-10 15:02:57 +0100

    Implement TextBoxVariable editor

--- public/app/features/dashboard-scene/settings/variables/editors/TextBoxVariableEditor.tsx
+++ public/app/features/dashboard-scene/settings/variables/editors/TextBoxVariableEditor.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import { TextBoxVariable } from '@grafana/scenes';
 
+import { TextBoxVariableForm } from '../components/TextBoxVariableForm';
+
 interface TextBoxVariableEditorProps {
   variable: TextBoxVariable;
   onChange: (variable: TextBoxVariable) => void;
 }
 
-export function TextBoxVariableEditor(props: TextBoxVariableEditorProps) {
-  return <div>TextBoxVariableEditor</div>;
+export function TextBoxVariableEditor({ variable }: TextBoxVariableEditorProps) {
+  const { value } = variable.useState();
+  const onQueryChange = (event: React.FormEvent<HTMLInputElement>) => {
+    variable.setState({ value: event.currentTarget.value });
+  };
+
+  const onBlur = (event: React.FormEvent<HTMLInputElement>) => {
+    variable.setState({ value: event.currentTarget.value });
+  };
+
+  return <TextBoxVariableForm value={value} onChange={onQueryChange} onBlur={onBlur} />;
 }
