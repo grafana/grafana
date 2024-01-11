a73179657c0b715023bf14f0fffb722e006415e1
Ryan McKinley <ryantxu@gmail.com>
2024-01-11 11:01:41 -0800

    stub

--- pkg/registry/apis/query/cache.go
+++ pkg/registry/apis/query/cache.go
@@ -1,9 +1,22 @@
 package query
 
 import (
+	"sync"
+
 	"github.com/grafana/grafana/pkg/apis/query/v0alpha1"
 )
 
-type pluginsCache struct {
-	cache *v0alpha1.DataSourceList
+type registry struct {
+	plugins        *v0alpha1.DataSourcePluginList
+	pluginsMu      sync.RWMutex
+	pluginsChecked int64
+
+	datasources   map[string]dsCache
+	datasourcesMu sync.RWMutex
+}
+
+type dsCache struct {
+	cache   *v0alpha1.DataSourceList
+	mu      sync.RWMutex
+	checked int64
 }
