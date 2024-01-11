f47f9e61a07ec48f13b9169e1753b5cd00d955f3
Charandas Batra <charandas.batra@grafana.com>
2024-01-11 15:41:56 -0800

    Add minio port-forward and make logs reading possible

--- devenv/velero/setup-with-minio.sh
+++ devenv/velero/setup-with-minio.sh
@@ -6,6 +6,8 @@ set -o pipefail
 
 cd $(dirname $0)
 
+MINIO_LOCALHOST_PORT=9000
+
 brew install velero
 
 DATA_DIR="../../data"
@@ -27,3 +29,8 @@ velero install \
     --secret-file $DATA_DIR/devenv-velero/credentials-velero \
     --use-volume-snapshots=false \
     --backup-location-config region=minio,s3ForcePathStyle="true",s3Url=http://minio.velero.svc:9000
+
+
+MINIO_POD=$(kubectl get pods -n velero -l component=minio -o jsonpath='{.items[0].metadata.name}')
+kubectl port-forward $MINIO_POD -n velero $MINIO_LOCALHOST_PORT:9000 &
+kubectl patch -n velero backupstoragelocation default --type merge -p "{\"spec\":{\"config\":{\"publicUrl\":\"http://localhost:$MINIO_LOCALHOST_PORT\"}}}"
