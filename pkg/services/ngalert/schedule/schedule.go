89e51a4bdcd9dc0513cb748f1a69bb0f29310e2b
Alex Weaver <weaver.alex.d@gmail.com>
2024-01-11 13:26:20 -0600

    Add log just for test purposes

--- pkg/services/ngalert/schedule/schedule.go
+++ pkg/services/ngalert/schedule/schedule.go
@@ -307,6 +307,7 @@ func (sch *schedule) processTick(ctx context.Context, dispatcherGroup *errgroup.
 		}
 
 		if isReadyToRun {
+			sch.log.Error("\n\nRule is ready to run", "uid", item.UID, "tick", tickNum, "frequency", itemFrequency, "offset", offset) // TODO: Remove me!
 			readyToRun = append(readyToRun, readyToRunItem{ruleInfo: ruleInfo, evaluation: evaluation{
 				scheduledAt: tick,
 				rule:        item,
