1bba8667fbc690f53f425cee45292175e2cfdc00
Armand Grillet <2117580+armandgrillet@users.noreply.github.com>
2024-01-12 18:35:31 +0100

    Alerting: Rename min interval field to interval in alert query editor (#80453)

--- public/app/features/alerting/unified/components/rule-editor/QueryWrapper.tsx
+++ public/app/features/alerting/unified/components/rule-editor/QueryWrapper.tsx
@@ -239,12 +239,12 @@ export function MinIntervalOption({
 
   return (
     <InlineField
-      label="Min interval"
+      label="Interval"
       labelWidth={24}
       tooltip={
         <>
-          A lower limit for the interval. Recommended to be set to write frequency, for example <code>1m</code> if your
-          data is written every minute.
+          Interval sent to the data source. Recommended to be set to write frequency, for example <code>1m</code> if
+          your data is written every minute.
         </>
       }
     >
