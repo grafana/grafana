3fe8ea5a4d807cfd7ad38a9e60b9adb88ad1dc62
Ivan Ortega <ivanortegaalba@gmail.com>
2024-01-12 10:50:12 +0100

    typo

--- public/app/features/dashboard-scene/settings/variables/components/VariableValuesPreview.tsx
+++ public/app/features/dashboard-scene/settings/variables/components/VariableValuesPreview.tsx
@@ -33,7 +33,7 @@ export const VariableValuesPreview = ({ options }: VariableValuesPreviewProps) =
       <InlineFieldRow>
         {previewOptions.map((o, index) => (
           <InlineFieldRow key={`${o.value}-${index}`} className={styles.optionContainer}>
-            <InlineLabel data-test-id={selectors.pages.Dashboard.Settings.Variables.Edit.General.previewOfValuesOption}>
+            <InlineLabel data-testid={selectors.pages.Dashboard.Settings.Variables.Edit.General.previewOfValuesOption}>
               <div className={styles.label}>{o.label}</div>
             </InlineLabel>
           </InlineFieldRow>
