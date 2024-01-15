0770112dc21ce69de7d6276697c78c9b04b0ae94
yangkb09 <yangkb09@gmail.com>
2024-01-15 17:01:50 -0500

    chore: revert non-test changes

--- pkg/services/dashboards/service/dashboard_service.go
+++ pkg/services/dashboards/service/dashboard_service.go
@@ -729,7 +729,7 @@ func (dr DashboardServiceImpl) CountInFolder(ctx context.Context, orgID int64, f
 		return 0, err
 	}
 
-	return dr.dashboardStore.CountDashboardsInFolder(ctx, &dashboards.CountDashboardsInFolderRequest{FolderUID: folder.UID, OrgID: orgID})
+	return dr.dashboardStore.CountDashboardsInFolder(ctx, &dashboards.CountDashboardsInFolderRequest{FolderID: folder.ID, OrgID: orgID})
 }
 
 func (dr *DashboardServiceImpl) DeleteInFolder(ctx context.Context, orgID int64, folderUID string, u identity.Requester) error {
