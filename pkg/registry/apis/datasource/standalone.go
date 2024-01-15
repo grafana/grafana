03c46a49e37e7ee8fca0e4c3da2497e720b2895c
Will Browne <wbrowne@users.noreply.github.com>
2024-01-15 14:43:41 +0100

    Purge some dependencies (#80541)
    * purge some dependencies
    
    * fix linter

--- pkg/registry/apis/datasource/standalone.go
+++ pkg/registry/apis/datasource/standalone.go
@@ -3,91 +3,49 @@ package datasource
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/bus"
-	"github.com/grafana/grafana/pkg/infra/httpclient/httpclientprovider"
 	"github.com/grafana/grafana/pkg/infra/kvstore"
 	"github.com/grafana/grafana/pkg/infra/localcache"
-	"github.com/grafana/grafana/pkg/infra/metrics"
-	"github.com/grafana/grafana/pkg/infra/remotecache"
-	"github.com/grafana/grafana/pkg/infra/serverlock"
 	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/infra/usagestats/service"
-	"github.com/grafana/grafana/pkg/plugins/backendplugin/coreplugin"
-	provider2 "github.com/grafana/grafana/pkg/plugins/backendplugin/provider"
-	"github.com/grafana/grafana/pkg/plugins/manager/loader/assetpath"
-	"github.com/grafana/grafana/pkg/plugins/manager/loader/finder"
-	"github.com/grafana/grafana/pkg/plugins/manager/process"
+	"github.com/grafana/grafana/pkg/plugins"
+	pCfg "github.com/grafana/grafana/pkg/plugins/config"
+	"github.com/grafana/grafana/pkg/plugins/manager/loader"
+	"github.com/grafana/grafana/pkg/plugins/manager/pipeline/bootstrap"
+	"github.com/grafana/grafana/pkg/plugins/manager/pipeline/discovery"
+	"github.com/grafana/grafana/pkg/plugins/manager/pipeline/initialization"
+	"github.com/grafana/grafana/pkg/plugins/manager/pipeline/termination"
+	"github.com/grafana/grafana/pkg/plugins/manager/pipeline/validation"
 	"github.com/grafana/grafana/pkg/plugins/manager/registry"
 	"github.com/grafana/grafana/pkg/plugins/manager/signature"
 	"github.com/grafana/grafana/pkg/plugins/manager/sources"
-	"github.com/grafana/grafana/pkg/plugins/pluginscdn"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/ossaccesscontrol"
-	"github.com/grafana/grafana/pkg/services/apikey/apikeyimpl"
-	database2 "github.com/grafana/grafana/pkg/services/dashboards/database"
 	"github.com/grafana/grafana/pkg/services/datasources/guardian"
 	service4 "github.com/grafana/grafana/pkg/services/datasources/service"
 	"github.com/grafana/grafana/pkg/services/encryption/provider"
 	service2 "github.com/grafana/grafana/pkg/services/encryption/service"
-	"github.com/grafana/grafana/pkg/services/extsvcauth/oauthserver/oasimpl"
-	registry2 "github.com/grafana/grafana/pkg/services/extsvcauth/registry"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
-	"github.com/grafana/grafana/pkg/services/folder/folderimpl"
 	"github.com/grafana/grafana/pkg/services/hooks"
 	"github.com/grafana/grafana/pkg/services/kmsproviders/osskmsproviders"
 	"github.com/grafana/grafana/pkg/services/licensing"
-	"github.com/grafana/grafana/pkg/services/org/orgimpl"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/angulardetectorsprovider"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/angularinspector"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/angularpatternsstore"
 	"github.com/grafana/grafana/pkg/services/pluginsintegration/config"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/keyretriever"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/keyretriever/dynamic"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/keystore"
-	licensing2 "github.com/grafana/grafana/pkg/services/pluginsintegration/licensing"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/loader"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/pipeline"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/pluginerrs"
-	service3 "github.com/grafana/grafana/pkg/services/pluginsintegration/pluginsettings/service"
 	"github.com/grafana/grafana/pkg/services/pluginsintegration/pluginstore"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/serviceregistration"
 	"github.com/grafana/grafana/pkg/services/quota/quotaimpl"
-	"github.com/grafana/grafana/pkg/services/searchV2"
 	"github.com/grafana/grafana/pkg/services/secrets/database"
 	kvstore2 "github.com/grafana/grafana/pkg/services/secrets/kvstore"
 	"github.com/grafana/grafana/pkg/services/secrets/manager"
-	"github.com/grafana/grafana/pkg/services/serviceaccounts/extsvcaccounts"
-	manager2 "github.com/grafana/grafana/pkg/services/serviceaccounts/manager"
-	"github.com/grafana/grafana/pkg/services/signingkeys/signingkeysimpl"
 	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrations"
-	"github.com/grafana/grafana/pkg/services/store"
 	"github.com/grafana/grafana/pkg/services/supportbundles/bundleregistry"
-	"github.com/grafana/grafana/pkg/services/tag/tagimpl"
-	"github.com/grafana/grafana/pkg/services/team/teamimpl"
-	"github.com/grafana/grafana/pkg/services/user/userimpl"
-	"github.com/grafana/grafana/pkg/services/validations"
 	"github.com/grafana/grafana/pkg/setting"
-	"github.com/grafana/grafana/pkg/tsdb/azuremonitor"
-	cloudmonitoring "github.com/grafana/grafana/pkg/tsdb/cloud-monitoring"
-	"github.com/grafana/grafana/pkg/tsdb/cloudwatch"
-	"github.com/grafana/grafana/pkg/tsdb/elasticsearch"
-	postgres "github.com/grafana/grafana/pkg/tsdb/grafana-postgresql-datasource"
-	pyroscope "github.com/grafana/grafana/pkg/tsdb/grafana-pyroscope-datasource"
 	testdatasource "github.com/grafana/grafana/pkg/tsdb/grafana-testdata-datasource"
-	"github.com/grafana/grafana/pkg/tsdb/grafanads"
-	"github.com/grafana/grafana/pkg/tsdb/graphite"
-	"github.com/grafana/grafana/pkg/tsdb/influxdb"
-	"github.com/grafana/grafana/pkg/tsdb/loki"
-	"github.com/grafana/grafana/pkg/tsdb/mssql"
-	"github.com/grafana/grafana/pkg/tsdb/mysql"
-	"github.com/grafana/grafana/pkg/tsdb/opentsdb"
-	"github.com/grafana/grafana/pkg/tsdb/parca"
-	"github.com/grafana/grafana/pkg/tsdb/prometheus"
-	"github.com/grafana/grafana/pkg/tsdb/tempo"
 )
+
+const testDataPluginId = "grafana-testdata-datasource"
 
 // This is a helper function to create a new datasource API server for a group
 // This currently has no dependencies and only works for testdata.  In future iterations
@@ -97,7 +55,7 @@ func NewStandaloneDatasource(group string) (*DataSourceAPIBuilder, error) {
 	if group != "testdata.datasource.grafana.app" {
 		return nil, fmt.Errorf("only testadata is currently supported")
 	}
-	pluginId := "grafana-testdata-datasource"
+	pluginId := testDataPluginId
 
 	cfg, err := setting.NewCfgFromArgs(setting.CommandLineArgs{
 		// TODO: Add support for args?
@@ -147,115 +105,18 @@ func NewStandaloneDatasource(group string) (*DataSourceAPIBuilder, error) {
 	if err != nil {
 		return nil, err
 	}
-	remoteCache, err := remotecache.ProvideService(cfg, sqlStore, usageStats, secretsService)
-	if err != nil {
-		return nil, err
-	}
 	ossImpl := setting.ProvideProvider(cfg)
 	configCfg, err := config.ProvideConfig(ossImpl, cfg, featureToggles)
 	if err != nil {
 		return nil, err
 	}
 	inMemory := registry.ProvideService()
-	licensingService := licensing2.ProvideLicensing(cfg, ossLicensingService)
-	ossPluginRequestValidator := validations.ProvideValidator()
-	registerer := metrics.ProvideRegisterer(cfg)
-	sourcesService := sources.ProvideService(cfg)
-	local := finder.ProvideLocalFinder(configCfg)
-	discovery := pipeline.ProvideDiscoveryStage(configCfg, local, inMemory)
-	keystoreService := keystore.ProvideService(kvStore)
-	keyRetriever := dynamic.ProvideService(cfg, keystoreService)
-	keyretrieverService := keyretriever.ProvideService(keyRetriever)
-	signatureSignature := signature.ProvideService(configCfg, keyretrieverService)
-	pluginscdnService := pluginscdn.ProvideService(configCfg)
-	assetpathService := assetpath.ProvideService(configCfg, pluginscdnService)
-	bootstrap := pipeline.ProvideBootstrapStage(configCfg, signatureSignature, assetpathService)
-	unsignedPluginAuthorizer := signature.ProvideOSSAuthorizer(configCfg)
-	validation := signature.ProvideValidatorService(unsignedPluginAuthorizer)
-	angularpatternsstoreService := angularpatternsstore.ProvideService(kvStore)
-	angulardetectorsproviderDynamic, err := angulardetectorsprovider.ProvideDynamic(configCfg, angularpatternsstoreService, featureToggles)
-	if err != nil {
-		return nil, err
-	}
-	angularinspectorService, err := angularinspector.ProvideService(configCfg, angulardetectorsproviderDynamic)
-	if err != nil {
-		return nil, err
-	}
-	signatureErrorRegistry := pluginerrs.ProvideSignatureErrorTracker()
-	validate := pipeline.ProvideValidationStage(configCfg, validation, angularinspectorService, signatureErrorRegistry)
-	httpclientProvider := httpclientprovider.New(cfg, ossPluginRequestValidator, tracingService)
-	azuremonitorService := azuremonitor.ProvideService(httpclientProvider)
-	cloudWatchService := cloudwatch.ProvideService(cfg, httpclientProvider, featureToggles)
-	cloudmonitoringService := cloudmonitoring.ProvideService(httpclientProvider, tracingService)
-	elasticsearchService := elasticsearch.ProvideService(httpclientProvider, tracingService)
-	graphiteService := graphite.ProvideService(httpclientProvider, tracingService)
-	influxdbService := influxdb.ProvideService(httpclientProvider, featureToggles)
-	lokiService := loki.ProvideService(httpclientProvider, featureToggles, tracingService)
-	opentsdbService := opentsdb.ProvideService(httpclientProvider)
-	prometheusService := prometheus.ProvideService(httpclientProvider, cfg, featureToggles)
-	tempoService := tempo.ProvideService(httpclientProvider)
-	testdatasourceService := testdatasource.ProvideService()
-	postgresService := postgres.ProvideService(cfg)
-	mysqlService := mysql.ProvideService(cfg, httpclientProvider)
-	mssqlService := mssql.ProvideService(cfg)
-	entityEventsService := store.ProvideEntityEventsService(cfg, sqlStore, featureToggles)
 	quotaService := quotaimpl.ProvideService(sqlStore, cfg)
-	orgService, err := orgimpl.ProvideService(sqlStore, cfg, quotaService)
+	loaderLoader, err := createLoader(configCfg, inMemory)
 	if err != nil {
 		return nil, err
 	}
-	teamService := teamimpl.ProvideService(sqlStore, cfg)
-	userService, err := userimpl.ProvideService(sqlStore, orgService, cfg, teamService, cacheService, quotaService, bundleregistryService)
-	if err != nil {
-		return nil, err
-	}
-	tagimplService := tagimpl.ProvideService(sqlStore)
-	dashboardsStore, err := database2.ProvideDashboardStore(sqlStore, cfg, featureToggles, tagimplService, quotaService)
-	if err != nil {
-		return nil, err
-	}
-	dashboardFolderStoreImpl := folderimpl.ProvideDashboardFolderStore(sqlStore)
-	folderService := folderimpl.ProvideService(accessControl, inProcBus, cfg, dashboardsStore, dashboardFolderStoreImpl, sqlStore, featureToggles, registerer)
-	searchService := searchV2.ProvideService(cfg, sqlStore, entityEventsService, acimplService, tracingService, featureToggles, orgService, userService, folderService)
-	systemUsers := store.ProvideSystemUsersService()
-	storageService, err := store.ProvideService(sqlStore, featureToggles, cfg, quotaService, systemUsers)
-	if err != nil {
-		return nil, err
-	}
-	grafanadsService := grafanads.ProvideService(searchService, storageService)
-	pyroscopeService := pyroscope.ProvideService(httpclientProvider)
-	parcaService := parca.ProvideService(httpclientProvider)
-	corepluginRegistry := coreplugin.ProvideCoreRegistry(tracingService, azuremonitorService, cloudWatchService, cloudmonitoringService, elasticsearchService, graphiteService, influxdbService, lokiService, opentsdbService, prometheusService, tempoService, testdatasourceService, postgresService, mysqlService, mssqlService, grafanadsService, pyroscopeService, parcaService)
-	providerService := provider2.ProvideService(corepluginRegistry)
-	processService := process.ProvideService()
-	apikeyService, err := apikeyimpl.ProvideService(sqlStore, cfg, quotaService)
-	if err != nil {
-		return nil, err
-	}
-	serviceAccountsService, err := manager2.ProvideServiceAccountsService(cfg, usageStats, sqlStore, apikeyService, kvStore, userService, orgService, acimplService)
-	if err != nil {
-		return nil, err
-	}
-	extSvcAccountsService := extsvcaccounts.ProvideExtSvcAccountsService(acimplService, inProcBus, sqlStore, featureManager, registerer, serviceAccountsService, secretsService, tracingService)
-	signingkeysimplService, err := signingkeysimpl.ProvideEmbeddedSigningKeysService(sqlStore, secretsService, remoteCache, routeRegisterImpl)
-	if err != nil {
-		return nil, err
-	}
-	oAuth2ServiceImpl, err := oasimpl.ProvideService(routeRegisterImpl, inProcBus, sqlStore, cfg, extSvcAccountsService, accessControl, acimplService, userService, teamService, signingkeysimplService, featureManager)
-	if err != nil {
-		return nil, err
-	}
-	serverLockService := serverlock.ProvideService(sqlStore, tracingService)
-	registryRegistry := registry2.ProvideExtSvcRegistry(oAuth2ServiceImpl, extSvcAccountsService, serverLockService, featureToggles)
-	service12 := service3.ProvideService(sqlStore, secretsService)
-	serviceregistrationService := serviceregistration.ProvideService(configCfg, registryRegistry, service12)
-	initialize := pipeline.ProvideInitializationStage(configCfg, inMemory, licensingService, providerService, processService, serviceregistrationService, acimplService)
-	terminate, err := pipeline.ProvideTerminationStage(configCfg, inMemory, processService)
-	if err != nil {
-		return nil, err
-	}
-	loaderLoader := loader.ProvideService(discovery, bootstrap, validate, initialize, terminate)
-	pluginstoreService, err := pluginstore.ProvideService(inMemory, sourcesService, loaderLoader)
+	pluginstoreService, err := pluginstore.ProvideService(inMemory, newTestDataPluginSource(cfg), loaderLoader)
 	if err != nil {
 		return nil, err
 	}
@@ -283,4 +144,54 @@ func NewStandaloneDatasource(group string) (*DataSourceAPIBuilder, error) {
 		cacheServiceImpl,
 		accessControl,
 	)
+}
+
+var _ sources.Registry = (*testDataPluginSource)(nil)
+
+type testDataPluginSource struct {
+	cfg *setting.Cfg
+}
+
+func newTestDataPluginSource(cfg *setting.Cfg) *testDataPluginSource {
+	return &testDataPluginSource{
+		cfg: cfg,
+	}
+}
+
+func (t *testDataPluginSource) List(_ context.Context) []plugins.PluginSource {
+	p := filepath.Join(t.cfg.StaticRootPath, "app/plugins/datasource", testDataPluginId)
+	return []plugins.PluginSource{sources.NewLocalSource(plugins.ClassCore, []string{p})}
+}
+
+func createLoader(cfg *pCfg.Cfg, pr registry.Service) (loader.Service, error) {
+	d := discovery.New(cfg, discovery.Opts{
+		FindFilterFuncs: []discovery.FindFilterFunc{
+			func(ctx context.Context, _ plugins.Class, b []*plugins.FoundBundle) ([]*plugins.FoundBundle, error) {
+				return discovery.NewDuplicatePluginFilterStep(pr).Filter(ctx, b)
+			},
+		},
+	})
+	b := bootstrap.New(cfg, bootstrap.Opts{
+		DecorateFuncs: []bootstrap.DecorateFunc{}, // no decoration required
+	})
+	v := validation.New(cfg, validation.Opts{
+		ValidateFuncs: []validation.ValidateFunc{
+			validation.SignatureValidationStep(signature.NewValidator(signature.NewUnsignedAuthorizer(cfg))),
+		},
+	})
+	i := initialization.New(cfg, initialization.Opts{
+		InitializeFuncs: []initialization.InitializeFunc{
+			initialization.PluginRegistrationStep(pr),
+		},
+	})
+	t, err := termination.New(cfg, termination.Opts{
+		TerminateFuncs: []termination.TerminateFunc{
+			termination.DeregisterStep(pr),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return loader.New(d, b, v, i, t), nil
 }
