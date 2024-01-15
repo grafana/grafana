3fc38e3ce09964ca551fafbd21d6ec8020d8f7e9
kay delaney <kay@grafana.com>
2024-01-15 09:13:54 +0000

    Update styles to better reflect current panel edit styles

--- public/app/features/dashboard-scene/panel-edit/PanelOptionsPane.tsx
+++ public/app/features/dashboard-scene/panel-edit/PanelOptionsPane.tsx
@@ -4,29 +4,12 @@ import React, { useMemo, useState } from 'react';
 import { GrafanaTheme2 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { SceneComponentProps, SceneObjectBase, SceneObjectState } from '@grafana/scenes';
-import {
-  ButtonGroup,
-  Field,
-  FilterInput,
-  Input,
-  RadioButtonGroup,
-  Select,
-  Switch,
-  TextArea,
-  ToolbarButton,
-  useStyles2,
-} from '@grafana/ui';
+import { ButtonGroup, FilterInput, RadioButtonGroup, ToolbarButton, useStyles2 } from '@grafana/ui';
 import { OptionsPaneCategory } from 'app/features/dashboard/components/PanelEditor/OptionsPaneCategory';
 import { getAllPanelPluginMeta } from 'app/features/panel/state/util';
 
 import { PanelVizTypePicker } from './PanelVizTypePicker';
 import { VizPanelManager } from './VizPanelManager';
-
-const directionOptions = [
-  { label: 'Horizontal', value: 'h' },
-  { label: 'Vertical', value: 'v' },
-];
-const maxPerRowOptions = [2, 3, 4, 6, 8, 12].map((value) => ({ label: value.toString(), value }));
 
 export interface PanelOptionsPaneState extends SceneObjectState {}
 
@@ -42,7 +25,7 @@ export class PanelOptionsPane extends SceneObjectBase<PanelOptionsPaneState> {
   static Component = ({ model }: SceneComponentProps<PanelOptionsPane>) => {
     const { panelManager } = model;
     const { panel } = panelManager.state;
-    const { title, description, displayMode, pluginId } = panel.useState();
+    const { pluginId } = panel.useState();
     const styles = useStyles2(getStyles);
     const [isVizPickerOpen, setVizPickerOpen] = useState(true);
 
@@ -120,13 +103,14 @@ export const VisualizationButton = ({ pluginId, onClick }: { pluginId: string; o
   //   return null;
   // }
 
+  const styles = useStyles2(getVizButtonStyles);
   const pluginMeta = useMemo(() => getAllPanelPluginMeta().filter((p) => p.id === pluginId)[0], [pluginId]);
 
   return (
-    <div className={vizButtonStyles.wrapper}>
+    <div className={styles.wrapper}>
       <ButtonGroup>
         <ToolbarButton
-          className={vizButtonStyles.vizButton}
+          className={styles.vizButton}
           tooltip="Click to change visualization"
           imgSrc={pluginMeta.info.logos.small}
           // isOpen={isVizPickerOpen}
@@ -151,12 +135,15 @@ export const VisualizationButton = ({ pluginId, onClick }: { pluginId: string; o
   );
 };
 
-const vizButtonStyles = {
-  wrapper: css({
-    display: 'flex',
-    flexDirection: 'column',
-  }),
-  vizButton: css({
-    textAlign: 'left',
-  }),
-};
+function getVizButtonStyles(theme: GrafanaTheme2) {
+  return {
+    wrapper: css({
+      display: 'flex',
+      flexDirection: 'column',
+      paddingRight: theme.spacing(2),
+    }),
+    vizButton: css({
+      textAlign: 'left',
+    }),
+  };
+}
