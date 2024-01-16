81c421c72a4671d08ad6b0d9158d697b35b5acce
Alexandra Vargas <alexa1866@gmail.com>
2024-01-16 10:03:41 +0100

    Apply PR suggestions

--- public/app/features/dashboard-scene/settings/variables/editors/TextBoxVariableEditor.tsx
+++ public/app/features/dashboard-scene/settings/variables/editors/TextBoxVariableEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { TextBoxVariable } from '@grafana/scenes';
 
@@ -11,13 +11,15 @@ interface TextBoxVariableEditorProps {
 
 export function TextBoxVariableEditor({ variable }: TextBoxVariableEditorProps) {
   const { value } = variable.useState();
-  const onQueryChange = (event: React.FormEvent<HTMLInputElement>) => {
-    variable.setState({ value: event.currentTarget.value });
+  const [textValue, setTextValue] = useState(value);
+
+  const onTextValueChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setTextValue(event.currentTarget.value);
   };
 
-  const onBlur = (event: React.FormEvent<HTMLInputElement>) => {
-    variable.setState({ value: event.currentTarget.value });
+  const onBlur = () => {
+    variable.setState({ value: textValue });
   };
 
-  return <TextBoxVariableForm value={value} onChange={onQueryChange} onBlur={onBlur} />;
+  return <TextBoxVariableForm value={textValue} onChange={onTextValueChange} onBlur={onBlur} />;
 }
