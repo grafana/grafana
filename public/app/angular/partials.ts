ca490c9ba17363125296b8b3eaa547e9bc376ea4
Jack Westbrook <jack.westbrook@gmail.com>
2024-01-16 17:54:56 +0100

    build(vite): attempt to get angular partials loading with vite plugin

--- public/app/angular/partials.ts
+++ public/app/angular/partials.ts
@@ -1,4 +1,34 @@
-let templates = (require as any).context('../', true, /\.html$/);
-templates.keys().forEach((key: string) => {
-  templates(key);
-});
+// TODO: Vite has no require.context support yet. Attempt at a workaround below.
+// let templates = (require as any).context('../', true, /\.html$/);
+// templates.keys().forEach((key: string) => {
+//   templates(key);
+// });
+
+// See vite.config.ts angularHtmlImport function for the code that _should_ assist with these templates.
+
+import 'app/angular/panel/partials/query_editor_row.html';
+import 'app/angular/partials/http_settings_next.html';
+import 'app/angular/partials/tls_auth_settings.html';
+import 'app/features/admin/partials/admin_home.html';
+import 'app/features/admin/partials/edit_org.html';
+import 'app/features/admin/partials/stats.html';
+import 'app/features/admin/partials/styleguide.html';
+import 'app/features/alerting/partials/alert_tab.html';
+import 'app/features/annotations/partials/event_editor.html';
+import 'app/partials/confirm_modal.html';
+import 'app/partials/modal.html';
+import 'app/partials/reset_password.html';
+import 'app/partials/signup_invited.html';
+import 'app/plugins/panel/graph/axes_editor.html';
+import 'app/plugins/panel/graph/tab_display.html';
+import 'app/plugins/panel/graph/tab_legend.html';
+import 'app/plugins/panel/graph/tab_series_overrides.html';
+import 'app/plugins/panel/graph/tab_thresholds.html';
+import 'app/plugins/panel/graph/tab_time_regions.html';
+import 'app/plugins/panel/graph/thresholds_form.html';
+import 'app/plugins/panel/graph/time_regions_form.html';
+import 'app/plugins/panel/heatmap/partials/axes_editor.html';
+import 'app/plugins/panel/heatmap/partials/display_editor.html';
+import 'app/plugins/panel/table-old/column_options.html';
+import 'app/plugins/panel/table-old/editor.html';
+import 'app/plugins/panel/table-old/module.html';
