45078523417efa48f094f5bed9fa34d34d0ebaf9
eledobleefe <laura.fernandez@grafana.com>
2024-01-16 07:29:19 +0100

    Add a comment

--- packages/grafana-ui/src/components/Modal/Modal.tsx
+++ packages/grafana-ui/src/components/Modal/Modal.tsx
@@ -66,6 +66,7 @@ export function Modal(props: PropsWithChildren<Props>) {
 
   const headerClass = cx(styles.modalHeader, typeof title !== 'string' && styles.modalHeaderWithTabs);
   console.log(divContent.current);
+  // https://www.w3.org/WAI/standards-guidelines/act/rules/0ssw9k/proposed/
   const height = divContent.current?.clientHeight;
   const childHeight = divContent.current?.lastElementChild?.clientHeight;
 
