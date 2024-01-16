14c82c2725f75d5926f5eea526291fefe552c035
Oscar Kilhed <oscar.kilhed@grafana.com>
2024-01-16 18:59:32 +0100

    Scenes: Show transformations when editing scene dashboard (#80372)
    * Make dashboard data source query actually use DashboardDataSource
    
    * remove commented out bit
    
    * Always wrap SceneQueryRunner with SceneDataTransformer
    
    * Update Dashboard model compat wrapper tests
    
    * DashboardQueryEditor test
    
    * VizPanelManager tests update
    
    * transform save model to scene tests update
    
    * Betterer
    
    * PanelMenuBehavior test update
    
    * Few more bits
    
    * Prettier
    
    * Show transformations when editing scene dashboard
    
    * remove and edit transformations works
    
    * add add and remove buttons
    
    * Change styles to object to fix betterer issue
    
    * Revert "Change styles to object to fix betterer issue"
    
    This reverts commit 8627b9162c2d8496317440731cac0f531d55782e.
    
    * Fix the correct file...
    
    * Some refactoring
    
    * remove unneessary if statement
    
    * panel data not present on first render
    
    * move transformation tabs out of folder
    
    * fix tests
    
    * add lint exception
    
    * refactor tab component
    
    * Fix merge issue
    
    * reorder components
    
    ---------
    
    Co-authored-by: Torkel Ödegaard <torkel@grafana.com>
    Co-authored-by: Dominik Prokop <dominik.prokop@grafana.com>

---
+++ public/app/features/dashboard-scene/panel-edit/PanelDataPane/EmptyTransformationsMessage.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { selectors } from '@grafana/e2e-selectors';
+import { Box, Button, Stack, Text } from '@grafana/ui';
+import { Trans } from 'app/core/internationalization';
+
+interface EmptyTransformationsProps {
+  onShowPicker: () => void;
+}
+export function EmptyTransformationsMessage(props: EmptyTransformationsProps) {
+  return (
+    <Box alignItems="center" padding={4}>
+      <Stack direction="column" alignItems="center" gap={2}>
+        <Text element="h3" textAlignment="center">
+          <Trans key="transformations.empty.add-transformation-header">Start transforming data</Trans>
+        </Text>
+        <Text element="p" textAlignment="center" data-testid={selectors.components.Transforms.noTransformationsMessage}>
+          <Trans key="transformations.empty.add-transformation-body">
+            Transformations allow data to be changed in various ways before your visualization is shown.
+            <br />
+            This includes joining data together, renaming fields, making calculations, formatting data for display, and
+            more.
+          </Trans>
+        </Text>
+        <Button
+          icon="plus"
+          variant="primary"
+          size="md"
+          onClick={props.onShowPicker}
+          data-testid={selectors.components.Transforms.addTransformationButton}
+        >
+          Add transformation
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
