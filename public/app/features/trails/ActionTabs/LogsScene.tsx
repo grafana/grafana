7cee3e7ccd9c1ebbeb5263291b66182aca701e0b
André Pereira <adrapereira@gmail.com>
2024-01-17 15:26:13 +0000

    wip

--- public/app/features/trails/ActionTabs/LogsScene.tsx
+++ public/app/features/trails/ActionTabs/LogsScene.tsx
@@ -1,22 +1,82 @@
-import { PanelBuilders, SceneFlexItem, SceneQueryRunner } from '@grafana/scenes';
+import React from 'react';
+
+import {
+  DataSourceVariable,
+  PanelBuilders,
+  SceneComponentProps,
+  SceneFlexItem,
+  SceneObject,
+  SceneObjectBase,
+  SceneObjectState,
+  SceneQueryRunner,
+  SceneVariableSet,
+  VariableValueSelectors,
+} from '@grafana/scenes';
+import { Stack } from '@grafana/ui';
 
 import { SelectMetricAction } from '../SelectMetricAction';
-import { LOGS_METRIC } from '../shared';
+import { LOGS_METRIC, VAR_LOGS_DATASOURCE, VAR_LOGS_DATASOURCE_EXPR } from '../shared';
+
+interface LogsSceneState extends SceneObjectState {
+  initialDS?: string;
+  controls: SceneObject[];
+}
+
+export class LogsScene extends SceneObjectBase<LogsSceneState> {
+  public constructor(state: Partial<LogsSceneState>) {
+    super({
+      $variables: state.$variables ?? getVariableSet(state.initialDS),
+      controls: state.controls ?? [new VariableValueSelectors({ layout: 'vertical' })],
+      $data: new SceneQueryRunner({
+        queries: [
+          {
+            refId: 'A',
+            datasource: { uid: VAR_LOGS_DATASOURCE_EXPR },
+            expr: '{${filters}} | logfmt',
+          },
+        ],
+      }),
+      ...state,
+    });
+  }
+
+  static Component = ({ model }: SceneComponentProps<LogsScene>) => {
+    const { controls } = model.useState();
+    const panel = PanelBuilders.logs()
+      .setTitle('Logs')
+      .setHeaderActions(new SelectMetricAction({ metric: LOGS_METRIC, title: 'Open' }))
+      .build();
+
+    return (
+      <Stack gap={1} direction={'column'}>
+        {controls && (
+          <Stack gap={1}>
+            {controls.map((control) => (
+              <control.Component key={control.state.key} model={control} />
+            ))}
+          </Stack>
+        )}
+        <panel.Component model={panel} />
+      </Stack>
+    );
+  };
+}
+
+function getVariableSet(initialDS?: string) {
+  return new SceneVariableSet({
+    variables: [
+      new DataSourceVariable({
+        name: VAR_LOGS_DATASOURCE,
+        label: 'Logs data source',
+        value: initialDS,
+        pluginId: 'loki',
+      }),
+    ],
+  });
+}
 
 export function buildLogsScene() {
   return new SceneFlexItem({
-    $data: new SceneQueryRunner({
-      queries: [
-        {
-          refId: 'A',
-          datasource: { uid: 'gdev-loki' },
-          expr: '{${filters}} | logfmt',
-        },
-      ],
-    }),
-    body: PanelBuilders.logs()
-      .setTitle('Logs')
-      .setHeaderActions(new SelectMetricAction({ metric: LOGS_METRIC, title: 'Open' }))
-      .build(),
+    body: new LogsScene({}),
   });
 }
