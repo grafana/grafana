2ad1720a624b6349b7cb485e39189e5846c20b3a
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-01-17 11:54:41 +0100

    Remove unused code

--- public/app/features/admin/ldap/LdapUserGroups.tsx
+++ public/app/features/admin/ldap/LdapUserGroups.tsx
@@ -47,45 +47,4 @@ export const LdapUserGroups = ({ groups, showAttributeMapping }: Props) => {
       getRowId={(row) => row.orgId + row.orgRole}
     />
   );
-
-  return (
-    <div className="gf-form-group">
-      <div className="gf-form">
-        <table className="filter-table form-inline">
-          <thead>
-            <tr>
-              {showAttributeMapping && <th>LDAP Group</th>}
-              <th>
-                Organization
-                <Tooltip placement="top" content="Only the first match for an Organization will be used" theme={'info'}>
-                  <Icon name="info-circle" />
-                </Tooltip>
-              </th>
-              <th>Role</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((group, index) => {
-              return (
-                <tr key={`${group.orgId}-${index}`}>
-                  {showAttributeMapping && <td>{group.groupDN}</td>}
-                  {group.orgName && group.orgRole ? <td>{group.orgName}</td> : <td />}
-                  {group.orgRole ? (
-                    <td>{group.orgRole}</td>
-                  ) : (
-                    <td>
-                      <span className="text-warning">No match</span>
-                      <Tooltip placement="top" content="No matching groups found" theme={'info'}>
-                        <Icon name="info-circle" />
-                      </Tooltip>
-                    </td>
-                  )}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
 };
