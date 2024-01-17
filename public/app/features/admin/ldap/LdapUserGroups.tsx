c74e56c7d5c057ca27265399e7586fd899732c5e
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-01-17 11:54:09 +0100

    InteractiveTable in LdapUserGroups

--- public/app/features/admin/ldap/LdapUserGroups.tsx
+++ public/app/features/admin/ldap/LdapUserGroups.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Tooltip, Icon } from '@grafana/ui';
+import { Tooltip, Icon, InteractiveTable, type CellProps } from '@grafana/ui';
 import { LdapRole } from 'app/types';
 
 interface Props {
@@ -10,6 +10,43 @@ interface Props {
 
 export const LdapUserGroups = ({ groups, showAttributeMapping }: Props) => {
   const items = showAttributeMapping ? groups : groups.filter((item) => item.orgRole);
+  const columns = [
+    {
+      id: 'groupDN',
+      header: 'LDAP Group',
+      visible: () => showAttributeMapping || false,
+    },
+    {
+      id: 'orgName',
+      header: 'Organization',
+      cell: (props: CellProps<LdapRole, string | undefined>) =>
+        props.value && props.row.original.orgRole ? props.value : '',
+    },
+    {
+      id: 'orgRole',
+      header: 'Role',
+      cell: (props: CellProps<LdapRole, string | undefined>) =>
+        props.value || (
+          <>
+            No match{' '}
+            <Tooltip content="No matching organizations found">
+              <Icon name="info-circle" />
+            </Tooltip>
+          </>
+        ),
+    },
+  ];
+
+  return (
+    <InteractiveTable
+      headerTooltips={{
+        orgName: { content: 'Only the first match for an Organization will be used', iconName: 'info-circle' },
+      }}
+      columns={columns}
+      data={items}
+      getRowId={(row) => row.orgId + row.orgRole}
+    />
+  );
 
   return (
     <div className="gf-form-group">
