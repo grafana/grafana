fd32279df694d0a9f09c1c1ba9198ba4d9222227
Todd Treece <toddtreece@gmail.com>
2024-01-17 15:47:09 -0500

    add playlist tests

--- test-api-raw.js
+++ test-api-raw.js
@@ -1,8 +1,10 @@
 import http from 'k6/http';
 
 export const options = {
-  vus: 50,
-  duration: '30s',
+  stages: [
+    { duration: '2m', target: 2000 }, // fast ramp-up to a high point
+    { duration: '1m', target: 0 }, // quick ramp-down to 0 users
+  ],
 };
 
 export default function () {
---
+++ test-playlist-legacy.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+
+export const options = {
+  vus: 50,
+  duration: '30s',
+};
+
+export default function () {
+  const headers = { 'Content-Type': 'application/json', Authentication: `Bearer ${__ENV.TOKEN}` };
+  http.get(`http://localhost:3000/api/playlists/${__ENV.PLAYLIST_UID}`, { headers });
+}
