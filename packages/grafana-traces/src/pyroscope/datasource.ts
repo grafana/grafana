90e71082992315f39097a8d69d3a017659a73e10
Fabrizio <fabrizio.casati@grafana.com>
2024-01-17 12:01:08 +0100

    Remove unused exports

--- packages/grafana-traces/src/pyroscope/datasource.ts
+++ packages/grafana-traces/src/pyroscope/datasource.ts
@@ -1,20 +1,13 @@
 import { Observable } from 'rxjs';
 
-import { AbstractQuery, CoreApp, DataQueryRequest, DataQueryResponse, ScopedVars } from '@grafana/data';
+import { CoreApp, DataQueryRequest, DataQueryResponse, ScopedVars } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
 
 import { PyroscopeDataSourceOptions, ProfileTypeMessage, Query } from './types';
 
 export abstract class PyroscopeDataSource extends DataSourceWithBackend<Query, PyroscopeDataSourceOptions> {
   abstract applyTemplateVariables(query: Query, scopedVars: ScopedVars): Query;
-  abstract exportToAbstractQueries(queries: Query[]): Promise<AbstractQuery[]>;
-  abstract exportToAbstractQuery(query: Query): AbstractQuery;
-  abstract getAllProfileTypes(): Promise<ProfileTypeMessage[]>;
   abstract getDefaultQuery(app: CoreApp): Partial<Query>;
-  abstract getLabelNames(query: string, start: number, end: number): Promise<string[]>;
-  abstract getLabelValues(query: string, label: string, start: number, end: number): Promise<string[]>;
   abstract getProfileTypes(): Promise<ProfileTypeMessage[]>;
-  abstract importFromAbstractQueries(abstractQueries: AbstractQuery[]): Promise<Query[]>;
-  abstract importFromAbstractQuery(labelBasedQuery: AbstractQuery): Query;
   abstract query(request: DataQueryRequest<Query>): Observable<DataQueryResponse>;
 }
