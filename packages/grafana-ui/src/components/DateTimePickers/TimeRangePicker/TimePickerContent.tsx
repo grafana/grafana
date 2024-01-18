f285eb6717de7d40a07dd091387238866ad08096
Haris Rozajac <58232930+harisrozajac@users.noreply.github.com>
2024-01-18 14:06:27 -0700

    Time Range: Copy-paste Time Range (#80107)
    * Working copy-paste functionality with validation
    
    * WIP; uses 't c' shortcut to copy time range
    
    * shortcuts working for explore and dashboards
    
    * cleanup
    
    * Don't update url when pasting in explore
    
    * Error handling, sync pane functionality, add to help modal
    
    * cleanup
    
    * add tests
    
    * fix i18n
    
    * Diferrentiate between explore and dashboard paste events; make on error prop generic
    
    * Fix
    
    * extract getting the copied time range logic into a function
    
    * Remove comments
    
    * Make error handling generic; markup for translations
    
    * Additional translation markup
    
    * markup for aria-label
    
    * Fix test
    
    * Replace fireEvent with userEvent
    
    * fix translations to match the standard pattern
    
    * Refactor keybindingSrv and TimeSrv to remove PasteTimeContext
    
    * Fix test
    
    * Remove unneccessary aria labels; update icons; buttons inline

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimePickerContent.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimePickerContent.tsx
@@ -22,6 +22,7 @@ interface Props {
   onChange: (timeRange: TimeRange) => void;
   onChangeTimeZone: (timeZone: TimeZone) => void;
   onChangeFiscalYearStartMonth?: (month: number) => void;
+  onError?: (error?: string) => void;
   timeZone?: TimeZone;
   fiscalYearStartMonth?: number;
   quickOptions?: TimeOption[];
@@ -122,7 +123,7 @@ export const TimePickerContent = (props: Props) => {
 };
 
 const NarrowScreenForm = (props: FormProps) => {
-  const { value, hideQuickRanges, onChange, timeZone, historyOptions = [], showHistory } = props;
+  const { value, hideQuickRanges, onChange, timeZone, historyOptions = [], showHistory, onError } = props;
   const styles = useStyles2(getNarrowScreenStyles);
   const isAbsolute = isDateTime(value.raw.from) || isDateTime(value.raw.to);
   const [collapsedFlag, setCollapsedFlag] = useState(!isAbsolute);
@@ -156,7 +157,13 @@ const NarrowScreenForm = (props: FormProps) => {
       {!collapsed && (
         <div className={styles.body} id="expanded-timerange">
           <div className={styles.form}>
-            <TimeRangeContent value={value} onApply={onChange} timeZone={timeZone} isFullscreen={false} />
+            <TimeRangeContent
+              value={value}
+              onApply={onChange}
+              timeZone={timeZone}
+              isFullscreen={false}
+              onError={onError}
+            />
           </div>
           {showHistory && (
             <TimeRangeList
@@ -173,7 +180,7 @@ const NarrowScreenForm = (props: FormProps) => {
 };
 
 const FullScreenForm = (props: FormProps) => {
-  const { onChange, value, timeZone, fiscalYearStartMonth, isReversed, historyOptions } = props;
+  const { onChange, value, timeZone, fiscalYearStartMonth, isReversed, historyOptions, onError } = props;
   const styles = useStyles2(getFullScreenStyles, props.hideQuickRanges);
   const onChangeTimeOption = (timeOption: TimeOption) => {
     return onChange(mapOptionToTimeRange(timeOption, timeZone));
@@ -194,6 +201,7 @@ const FullScreenForm = (props: FormProps) => {
           onApply={onChange}
           isFullscreen={true}
           isReversed={isReversed}
+          onError={onError}
         />
       </div>
       {props.showHistory && (
