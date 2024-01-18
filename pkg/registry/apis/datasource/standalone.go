5549a6d22e1a3dcdfc44db0da857aa4b61e3f434
Will Browne <will.browne@grafana.com>
2024-01-18 11:43:57 +0000

    use standalone svcs

--- pkg/registry/apis/datasource/standalone.go
+++ pkg/registry/apis/datasource/standalone.go
@@ -3,18 +3,12 @@ package datasource
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
-	"time"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 
 	common "github.com/grafana/grafana/pkg/apis/common/v0alpha1"
-	"github.com/grafana/grafana/pkg/apis/datasource/v0alpha1"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
-	"github.com/grafana/grafana/pkg/services/datasources"
-	"github.com/grafana/grafana/pkg/services/grafana-apiserver/endpoints/request"
 	"github.com/grafana/grafana/pkg/setting"
 	testdatasource "github.com/grafana/grafana/pkg/tsdb/grafana-testdata-datasource"
 )
@@ -37,26 +31,22 @@ func NewStandaloneDatasource(group string) (*DataSourceAPIBuilder, error) {
 		return nil, err
 	}
 
-	f, err := os.Open(filepath.Join(cfg.StaticRootPath, "app/plugins/datasource", pluginID, "plugin.json"))
+	_, pluginStore, dsService, dsCache, err := apiBuilderServices(cfg, pluginID)
 	if err != nil {
 		return nil, err
 	}
 
-	pluginJSON, err := plugins.ReadPluginJSON(f)
-	if err != nil {
-		return nil, err
-	}
-	err = f.Close()
-	if err != nil {
-		return nil, err
+	td, exists := pluginStore.Plugin(context.Background(), pluginID)
+	if !exists {
+		return nil, fmt.Errorf("plugin %s not found", pluginID)
 	}
 
 	var testsDataQuerierFactory QuerierFactoryFunc = func(ctx context.Context, ri common.ResourceInfo, pj plugins.JSONData) (Querier, error) {
-		return NewTestDataQuerier(pluginJSON, testdatasource.ProvideService())
+		return NewDefaultQuerier(ri, td.JSONData, testdatasource.ProvideService(), dsService, dsCache), nil
 	}
 
 	return NewDataSourceAPIBuilder(
-		pluginJSON,
+		td.JSONData,
 		NewQuerierProvider(testsDataQuerierFactory),
 		&TestDataPluginContextProvider{},
 		acimpl.ProvideAccessControl(cfg),
@@ -67,82 +57,4 @@ type TestDataPluginContextProvider struct{}
 
 func (p *TestDataPluginContextProvider) PluginContextForDataSource(_ context.Context, _, _ string) (backend.PluginContext, error) {
 	return backend.PluginContext{}, nil
-}
-
-type TestDataQuerier struct {
-	ri         common.ResourceInfo
-	pluginJSON plugins.JSONData
-	testdata   *testdatasource.Service
-}
-
-func NewTestDataQuerier(pluginJSON plugins.JSONData, testdata *testdatasource.Service) (*TestDataQuerier, error) {
-	ri, err := resourceFromPluginID(pluginJSON.ID)
-	if err != nil {
-		return nil, err
-	}
-
-	return &TestDataQuerier{
-		ri:         ri,
-		pluginJSON: pluginJSON,
-		testdata:   testdata,
-	}, nil
-}
-
-func (q *TestDataQuerier) Query(ctx context.Context, query *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	return q.testdata.QueryData(ctx, query)
-}
-
-func (q *TestDataQuerier) Resource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	return q.testdata.CallResource(ctx, req, sender)
-}
-
-func (q *TestDataQuerier) Health(ctx context.Context, query *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	return q.testdata.CheckHealth(ctx, query)
-}
-
-func (q *TestDataQuerier) Datasource(ctx context.Context, name string) (*v0alpha1.DataSourceConnection, error) {
-	info, err := request.NamespaceInfoFrom(ctx, true)
-	if err != nil {
-		return nil, err
-	}
-
-	dss := staticDataSources(q.pluginJSON.ID)
-	for _, ds := range dss {
-		if ds.UID == name {
-			return asConnection(q.ri.TypeMeta(), ds, info.Value)
-		}
-	}
-	return nil, fmt.Errorf("testdata datasource %s not found", name)
-}
-
-func (q *TestDataQuerier) Datasources(ctx context.Context) (*v0alpha1.DataSourceConnectionList, error) {
-	info, err := request.NamespaceInfoFrom(ctx, true)
-	if err != nil {
-		return nil, err
-	}
-
-	dss := staticDataSources(q.pluginJSON.ID)
-	return asConnectionList(q.ri.TypeMeta(), dss, info.Value)
-}
-
-func staticDataSources(pluginID string) []*datasources.DataSource {
-	now := time.Now()
-	return []*datasources.DataSource{
-		{
-			OrgID:   int64(1), // default -- used in the list command
-			Type:    pluginID,
-			UID:     "builtin", // fake for now
-			Created: now,
-			Updated: now,
-			Name:    "Testdata (builtin)",
-		},
-		{
-			OrgID:   int64(1), // default -- used in the list command
-			Type:    pluginID,
-			UID:     "PD8C576611E62080A", // match the gdev version
-			Created: now,
-			Updated: now,
-			Name:    "gdev-testdata",
-		},
-	}
 }
