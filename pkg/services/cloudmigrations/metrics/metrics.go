f95a4005a010d1db0b855347a5838a06a125224d
Michael Mandrus <michael.mandrus@grafana.com>
2024-01-21 23:48:58 -0500

    fix sample metric

--- pkg/services/cloudmigrations/metrics/metrics.go
+++ pkg/services/cloudmigrations/metrics/metrics.go
@@ -10,7 +10,7 @@ const (
 )
 
 var promMetrics = []prometheus.Collector{
-	prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
 		Name:      "datasources_migrated",
