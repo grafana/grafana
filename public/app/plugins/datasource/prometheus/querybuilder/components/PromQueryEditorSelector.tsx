639bf3036d04a6eb9cdc261b77cf8767ba3bf22d
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-01-22 08:42:24 -0600

    Prometheus: Add e2e tests for decoupling (#80333)
    * update selectors for prom
    
    * add selector to switch component, needs id instead of testid
    
    * add testid and ids to Prom settings
    
    * add e2e tests for prom config
    
    * add config to editor test
    
    * export select function
    
    * start query editor spec
    
    * clean up describe
    
    * add selectors for general query editor
    
    * add selectors to components in options in best locations
    
    * wrap header switch in id because component doesn't accept testid nor id
    
    * add id to wrap legend components in one selector
    
    * update selector in shared folder component, note to change in shared library
    
    * update selector in shared folder component, note to change in shared library
    
    * add notes for selectors in shared folder
    
    * add tests and file for query editor
    
    * add selectors for metrics browser in code editor
    
    * add selector to component to open metrics browser
    
    * add selectors to components within the metrics browser
    
    * add tests for metrics browser and stub resource calls
    
    * add selectors to query builder components
    
    * add e2e tests for query builder
    
    * generic query builder test with hints
    
    * add selectors for more code editor parts
    
    * add test for code and update selector
    
    * fix tests with selector
    
    * remove shared folder changes and use data-testid where possible
    
    * remove unused import
    
    * share getResources
    
    * create variable query editor selectors
    
    * add selectors to the variable query editor
    
    * add e2e tests for the Prometheus variable query editor
    
    * fix test function
    
    * refactor add data source method
    
    * add annotation selectors
    
    * add selectors to annotation components
    
    * add annotation e2e tests
    
    * commit for yarn i18n:extract error in drone

--- public/app/plugins/datasource/prometheus/querybuilder/components/PromQueryEditorSelector.tsx
+++ public/app/plugins/datasource/prometheus/querybuilder/components/PromQueryEditorSelector.tsx
@@ -123,7 +123,9 @@ export const PromQueryEditorSelector = React.memo<Props>((props) => {
         >
           Kick start your query
         </Button>
-        <QueryHeaderSwitch label="Explain" value={explain} onChange={onShowExplainChange} />
+        <div data-testid={selectors.components.DataSource.Prometheus.queryEditor.explain}>
+          <QueryHeaderSwitch label="Explain" value={explain} onChange={onShowExplainChange} />
+        </div>
         <FlexItem grow={1} />
         {app !== CoreApp.Explore && app !== CoreApp.Correlations && (
           <Button
@@ -136,7 +138,9 @@ export const PromQueryEditorSelector = React.memo<Props>((props) => {
             Run queries
           </Button>
         )}
-        <QueryEditorModeToggle mode={editorMode} onChange={onEditorModeChange} />
+        <div data-testid={selectors.components.DataSource.Prometheus.queryEditor.editorToggle}>
+          <QueryEditorModeToggle mode={editorMode} onChange={onEditorModeChange} />
+        </div>
       </EditorHeader>
       <Space v={0.5} />
       <EditorRows>
