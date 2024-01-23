0bbcaa2a344b50f09c2305edb3ff10e1e2ff634e
André Pereira <adrapereira@gmail.com>
2024-01-23 15:03:06 +0000

    Fix conflicts

--- public/app/features/trails/DataTrailCard.tsx
+++ public/app/features/trails/DataTrailCard.tsx
@@ -2,13 +2,12 @@ import { css } from '@emotion/css';
 import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { getDataSourceSrv } from '@grafana/runtime';
 import { AdHocFiltersVariable, sceneGraph } from '@grafana/scenes';
 import { useStyles2, Stack, Tooltip, Button } from '@grafana/ui';
 
 import { DataTrail } from './DataTrail';
 import { LOGS_METRIC, VAR_FILTERS } from './shared';
-import { getDataSource } from './utils';
+import { getDataSource, getDataSourceName } from './utils';
 
 export interface Props {
   trail: DataTrail;
