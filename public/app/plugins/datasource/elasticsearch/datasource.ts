9ab5a906b1e87574db4bd0eaed43c9b1382909e8
Ivana Huckova <ivana.huckova@gmail.com>
2024-01-23 14:23:42 +0100

    Loki: Fix throwing error when no labels received

--- public/app/plugins/datasource/elasticsearch/datasource.ts
+++ public/app/plugins/datasource/elasticsearch/datasource.ts
@@ -217,7 +217,8 @@ export class ElasticDatasource
     }).pipe(
       mergeMap((index) => {
         // catch all errors and emit an object with an err property to simplify checks later in the pipeline
-        const path = indexUrlList[listLen - index - 1];
+        const path = encodeURI(indexUrlList[listLen - index - 1]).replace(/:/g, '%3A');
+        console.log('requesting', path);
         const requestObservable = config.featureToggles.enableElasticsearchBackendQuerying
           ? from(this.getResource(path))
           : this.legacyQueryRunner.request('GET', path);
