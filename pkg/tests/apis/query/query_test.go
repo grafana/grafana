b83fa26a90d150aff6358e8132ed58992ce30485
Ryan McKinley <ryantxu@gmail.com>
2024-01-23 10:11:54 -0800

    fix test

--- pkg/tests/apis/query/query_test.go
+++ pkg/tests/apis/query/query_test.go
@@ -26,7 +26,6 @@ func TestSimpleQuery(t *testing.T) {
 		AppModeProduction: false, // dev mode required for datasource connections
 		DisableAnonymous:  true,
 		EnableFeatureToggles: []string{
-			featuremgmt.FlagGrafanaAPIServer,
 			featuremgmt.FlagGrafanaAPIServerWithExperimentalAPIs, // Required to start the example service
 		},
 	})
