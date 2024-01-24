7f2a0e38780cf97f37173e7dcf217efa50da6a66
Oscar Kilhed <oscar.kilhed@grafana.com>
2024-01-24 14:14:48 +0100

    Scenes: Add transformation flow for panel edit (#80738)
    * Adding transformations works
    
    * Use source data as data input for transformation settings, add search box suffix
    
    * remove useCallback that are probably not needed, fix tests
    
    * remove unused import
    
    * add tests for adding and removing transformations
    
    * use view all constant
    
    * Add reordering functionality
    
    * Fix removing one transformation removes all consecutive transformations
    
    * use closeDrawer function
    
    * Add tests for changing transformations
    
    * Remove any
    
    ---------
    
    Co-authored-by: Dominik Prokop <dominik.prokop@grafana.com>

--- public/app/features/dashboard-scene/panel-edit/VizPanelManager.tsx
+++ public/app/features/dashboard-scene/panel-edit/VizPanelManager.tsx
@@ -20,8 +20,9 @@ import {
   SceneQueryRunner,
   sceneGraph,
   SceneDataProvider,
+  SceneDataTransformer,
 } from '@grafana/scenes';
-import { DataQuery } from '@grafana/schema';
+import { DataQuery, DataTransformerConfig } from '@grafana/schema';
 import { getPluginVersion } from 'app/features/dashboard/state/PanelModel';
 import { storeLastUsedDataSourceInLocalStorage } from 'app/features/datasources/components/picker/utils';
 import { updateQueries } from 'app/features/query/state/updateQueries';
@@ -217,6 +218,12 @@ export class VizPanelManager extends SceneObjectBase<VizPanelManagerState> {
     runner.setState({ queries });
   }
 
+  public changeTransformations(transformations: DataTransformerConfig[]) {
+    const dataprovider = this.dataTransformer;
+    dataprovider.setState({ transformations });
+    dataprovider.reprocessTransformations();
+  }
+
   public inspectPanel() {
     const panel = this.state.panel;
     const panelId = getPanelIdForVizPanel(panel);
@@ -235,6 +242,14 @@ export class VizPanelManager extends SceneObjectBase<VizPanelManagerState> {
       throw new Error('Query runner not found');
     }
     return runner;
+  }
+
+  get dataTransformer(): SceneDataTransformer {
+    const provider = this.state.panel.state.$data;
+    if (!provider || !(provider instanceof SceneDataTransformer)) {
+      throw new Error('Could not find SceneDataTransformer for panel');
+    }
+    return provider;
   }
 
   get panelData(): SceneDataProvider {
