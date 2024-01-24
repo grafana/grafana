aa5e47013a4897935bbedf4a065d07148756eda3
Clarity-89 <homes89@ukr.net>
2024-01-24 09:52:40 +0200

    Add responsive styles and prop docs

--- packages/grafana-ui/src/components/Layout/Space.tsx
+++ packages/grafana-ui/src/components/Layout/Space.tsx
@@ -1,19 +1,30 @@
 import { css, cx } from '@emotion/css';
 import React from 'react';
 
-import { GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme2, ThemeSpacingTokens } from '@grafana/data';
 
 import { useStyles2 } from '../../themes';
 
+import { getResponsiveStyle, ResponsiveProp } from './utils/responsiveness';
+
 export interface SpaceProps {
-  v?: number;
-  h?: number;
+  /**
+   * The amount of vertical space to use.
+   */
+  v?: ResponsiveProp<ThemeSpacingTokens>;
+  /**
+   * The amount of horizontal space to use.
+   */
+  h?: ResponsiveProp<ThemeSpacingTokens>;
+  /**
+   * The layout of the space. If set `inline`, the space will behave like an inline-block element,
+   * otherwise it will behave like a block element.
+   */
   layout?: 'block' | 'inline';
 }
 
 export const Space = (props: SpaceProps) => {
   const styles = useStyles2(getStyles, props);
-
   return <span className={cx(styles.wrapper)} />;
 };
 
@@ -25,10 +36,12 @@ Space.defaultProps = {
 
 const getStyles = (theme: GrafanaTheme2, props: SpaceProps) => ({
   wrapper: css([
-    {
-      paddingRight: theme.spacing(props.h ?? 0),
-      paddingBottom: theme.spacing(props.v ?? 0),
-    },
+    getResponsiveStyle(theme, props.h, (val) => ({
+      paddingRight: theme.spacing(val),
+    })),
+    getResponsiveStyle(theme, props.v, (val) => ({
+      paddingBottom: theme.spacing(val),
+    })),
     props.layout === 'inline' && {
       display: 'inline-block',
     },
