207221e1821338669e6700817d662d130cd1f8a5
Ivan Ortega <ivanortegaalba@gmail.com>
2024-01-25 18:43:04 +0100

    Extract components to be reused

--- public/app/features/dashboard-scene/settings/variables/editors/AdHocFiltersVariableEditor.tsx
+++ public/app/features/dashboard-scene/settings/variables/editors/AdHocFiltersVariableEditor.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useAsync } from 'react-use';
 
+import { DataSourceInstanceSettings } from '@grafana/data';
+import { getDataSourceSrv } from '@grafana/runtime';
 import { AdHocFiltersVariable } from '@grafana/scenes';
+import { DataSourceRef } from '@grafana/schema';
+
+import { AdHocVariableForm } from '../components/AdHocVariableForm';
 
 interface AdHocFiltersVariableEditorProps {
   variable: AdHocFiltersVariable;
-  onChange: (variable: AdHocFiltersVariable) => void;
+  onRunQuery: (variable: AdHocFiltersVariable) => void;
 }
 
 export function AdHocFiltersVariableEditor(props: AdHocFiltersVariableEditorProps) {
-  return <div>AdHocFiltersVariableEditor</div>;
+  const { variable } = props;
+  const { set: filterSet } = variable.useState();
+  const { datasource: datasourceRef } = filterSet.useState();
+
+  const { value: datasource } = useAsync(async () => {
+    return await getDataSourceSrv().get(datasourceRef);
+  }, [variable.state]);
+
+  const message = datasource?.getTagKeys
+    ? 'Ad hoc filters are applied automatically to all queries that target this data source'
+    : 'This data source does not support ad hoc filters yet.';
+
+  const onDataSourceChange = (ds: DataSourceInstanceSettings) => {
+    const dsRef: DataSourceRef = {
+      uid: ds.uid,
+      type: ds.type,
+    };
+    filterSet.setState({ datasource: dsRef });
+  };
+
+  return <AdHocVariableForm datasource={datasource} infoText={message} onDataSourceChange={onDataSourceChange} />;
 }
