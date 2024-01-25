7e5544ab21da88ec2bf987ee6583fcf9bbb944bf
idafurjes <36131195+idafurjes@users.noreply.github.com>
2024-01-25 11:10:35 +0100

    Add MFolderIDsServiceCount to count folderIDs in services pkg (#81237)

--- pkg/services/folder/folderimpl/sqlstore.go
+++ pkg/services/folder/folderimpl/sqlstore.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/infra/metrics"
 	"github.com/grafana/grafana/pkg/services/dashboards"
 	"github.com/grafana/grafana/pkg/services/folder"
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
@@ -70,6 +71,7 @@ func (ss *sqlStore) Create(ctx context.Context, cmd folder.CreateFolderCommand) 
 			return err
 		}
 
+		metrics.MFolderIDsServiceCount.WithLabelValues(metrics.Folder).Inc()
 		foldr, err = ss.Get(ctx, folder.GetFolderQuery{
 			ID: &lastInsertedID, // nolint:staticcheck
 		})
@@ -170,6 +172,7 @@ func (ss *sqlStore) Get(ctx context.Context, q folder.GetFolderQuery) (*folder.F
 			exists, err = sess.SQL("SELECT * FROM folder WHERE uid = ? AND org_id = ?", q.UID, q.OrgID).Get(foldr)
 		// nolint:staticcheck
 		case q.ID != nil:
+			metrics.MFolderIDsServiceCount.WithLabelValues(metrics.Folder).Inc()
 			exists, err = sess.SQL("SELECT * FROM folder WHERE id = ?", q.ID).Get(foldr)
 		case q.Title != nil:
 			s := strings.Builder{}
