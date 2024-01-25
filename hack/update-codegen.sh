ba77bf28a18e251c13694b8fef28f42c94705f3b
Charandas Batra <charandas.batra@grafana.com>
2024-01-24 16:52:38 -0800

    K8s: update codegen to 0.29 - midway

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,27 +19,54 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo $GOPATH/pkg/mod/k8s.io/code-generator@v0.27.1)}
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo $GOPATH/pkg/mod/k8s.io/code-generator@v0.29.1)}
 
 OUTDIR="${HOME}/go/src"
 
-echo $OUTDIR
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
-CLIENTSET_NAME_VERSIONED=clientset \
-CLIENTSET_PKG_NAME=clientset \
-"${CODEGEN_PKG}/generate-groups.sh" "all" \
-  github.com/grafana/grafana/pkg/generated \
-  github.com/grafana/grafana/pkg/apis \
-  "datasource:v0alpha1" \
-  --output-base "${OUTDIR}" \
-  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
 
-CLIENTSET_NAME_VERSIONED=clientset \
-CLIENTSET_PKG_NAME=clientset \
-"${CODEGEN_PKG}/generate-internal-groups.sh" "deepcopy,defaulter,conversion,openapi"  \
-  github.com/grafana/grafana/pkg/generated \
-  github.com/grafana/grafana/pkg/apis \
-  github.com/grafana/grafana/pkg/apis \
-  "datasource:v0alpha1" \
-  --output-base "${OUTDIR}" \
-  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+kube::codegen::gen_helpers \
+    --input-pkg-root github.com/grafana/grafana/pkg/apis \
+    --output-base "${OUTDIR}" \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+
+
+if [[ -n "${API_KNOWN_VIOLATIONS_DIR:-}" ]]; then
+    report_filename="${API_KNOWN_VIOLATIONS_DIR}/grafana_grafana_violation_exceptions.list"
+    if [[ "${UPDATE_API_KNOWN_VIOLATIONS:-}" == "true" ]]; then
+        update_report="--update-report"
+    fi
+fi
+
+for api_pkg in $(ls ./pkg/apis); do
+  for pkg_version in $(ls ./pkg/apis/${api_pkg}); do
+    ## NOTE: we name the packages after the pkg_version, so that they match the expectations on being moved
+    ## In the intermediate directory, it looks like a nested path - omitted/paths/${pkg_version}/${pkg_version}
+    ## This is a necessary albeit unintuitive path but we can live with it, since its in a temporary dir structure
+    echo "Generating openapi package for ${api_pkg}, version=${pkg_version} ..."
+    kube::codegen::gen_openapi \
+      --input-pkg-root github.com/grafana/grafana/pkg/apis/${api_pkg}/${pkg_version} \
+      --output-pkg-root github.com/grafana/grafana/pkg/generated/${api_pkg}/${pkg_version} \
+      --openapi-name ${pkg_version} \
+      --output-base "${OUTDIR}" \
+      --report-filename "${report_filename:-"/dev/null"}" \
+      ${update_report:+"${update_report}"} \
+      --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+    mv pkg/generated/${api_pkg}/${pkg_version}/${pkg_version}/zz_generated.openapi.go  pkg/apis/${api_pkg}/${pkg_version}/
+  done
+done
+
+exit 1
+
+kube::codegen::gen_client \
+    --with-watch \
+    --with-applyconfig \
+    --input-pkg-root github.com/grafana/grafana/pkg/apis \
+    --output-pkg-root github.com/grafana/grafana/pkg/generated \
+    --output-base "${OUTDIR}" \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
