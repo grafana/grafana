95df9a788e77967d36b4ca5e619f77f135625bf2
Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
2024-01-25 17:01:22 +0100

    LDAP: Use InteractiveTable and remove gf-form usage (#80291)
    * Use InteractiveTable
    
    * Remove unused return
    
    * Fix icon alignment
    
    * InteractiveTable in LdapUserMappingInfo
    
    * Update no teams text
    
    * InteractiveTable in LdapUserGroups
    
    * Remove unused code
    
    * Cleanup
    
    * LdapSyncInfo to InteractiveTable
    
    * Update more tables
    
    * Memoize
    
    * Fix connection status
    
    * Update lockfile
    
    * Refactor LdapSyncInfo
    
    * Fix lockfile
    
    * Remove showAttributeMapping as it is always true

--- public/app/features/admin/ldap/LdapUserMappingInfo.tsx
+++ public/app/features/admin/ldap/LdapUserMappingInfo.tsx
@@ -1,47 +1,56 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
+import { InteractiveTable } from '@grafana/ui';
 import { LdapUserInfo } from 'app/types';
 
 interface Props {
   info: LdapUserInfo;
-  showAttributeMapping?: boolean;
 }
 
-export const LdapUserMappingInfo = ({ info, showAttributeMapping }: Props) => {
-  return (
-    <div className="gf-form-group">
-      <div className="gf-form">
-        <table className="filter-table form-inline">
-          <thead>
-            <tr>
-              <th colSpan={2}>User information</th>
-              {showAttributeMapping && <th>LDAP attribute</th>}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="width-16">First name</td>
-              <td>{info.name.ldapValue}</td>
-              {showAttributeMapping && <td>{info.name.cfgAttrValue}</td>}
-            </tr>
-            <tr>
-              <td className="width-16">Surname</td>
-              <td>{info.surname.ldapValue}</td>
-              {showAttributeMapping && <td>{info.surname.cfgAttrValue}</td>}
-            </tr>
-            <tr>
-              <td className="width-16">Username</td>
-              <td>{info.login.ldapValue}</td>
-              {showAttributeMapping && <td>{info.login.cfgAttrValue}</td>}
-            </tr>
-            <tr>
-              <td className="width-16">Email</td>
-              <td>{info.email.ldapValue}</td>
-              {showAttributeMapping && <td>{info.email.cfgAttrValue}</td>}
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+export const LdapUserMappingInfo = ({ info }: Props) => {
+  const columns = useMemo(
+    () => [
+      {
+        id: 'userInfo',
+        header: 'User Information',
+        disableGrow: true,
+      },
+      {
+        id: 'ldapValue',
+      },
+      {
+        id: 'cfgAttrValue',
+        header: 'LDAP attribute',
+      },
+    ],
+    []
   );
+
+  const rows = useMemo(
+    () => [
+      {
+        userInfo: 'First name',
+        ldapValue: info.name.ldapValue,
+        cfgAttrValue: info.name.cfgAttrValue,
+      },
+      {
+        userInfo: 'Surname',
+        ldapValue: info.surname.ldapValue,
+        cfgAttrValue: info.surname.cfgAttrValue,
+      },
+      {
+        userInfo: 'Username',
+        ldapValue: info.login.ldapValue,
+        cfgAttrValue: info.login.cfgAttrValue,
+      },
+      {
+        userInfo: 'Email',
+        ldapValue: info.email.ldapValue,
+        cfgAttrValue: info.email.cfgAttrValue,
+      },
+    ],
+    [info]
+  );
+
+  return <InteractiveTable columns={columns} data={rows} getRowId={(row) => row.userInfo} />;
 };
