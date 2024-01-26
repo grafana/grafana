7f546855b37c1c57ea416453831098323f256ea2
suntala <arati.rana@grafana.com>
2024-01-26 12:58:25 +0100

    Refactor structure of TestCreate

--- pkg/services/store/entity/sqlstash/sql_storage_server_test.go
+++ pkg/services/store/entity/sqlstash/sql_storage_server_test.go
@@ -13,99 +13,108 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 )
 
-// #TODO: convert to table test where we provide expected response entity and error don't read from the table if we expect non nil error
 func TestCreate(t *testing.T) {
 	s := setUpTestServer(t)
 
-	t.Run("entity with only Key and CreatedBy", func(t *testing.T) {
-		key := "/playlist.grafana.app/playlists/default/set-minimum-uid"
-		createdBy := "set-minimum-creator"
-		req := entity.CreateEntityRequest{
-			Entity: &entity.Entity{
-				Key:       key,
-				CreatedBy: createdBy,
+	tests := []struct {
+		name             string
+		ent              entity.Entity
+		errIsExpected    bool
+		statusIsExpected bool
+	}{
+		{
+			"request with key and entity creator",
+			entity.Entity{
+				Group:     "playlist.grafana.app",
+				Resource:  "playlists",
+				Namespace: "default",
+				Name:      "set-minimum-uid",
+				Key:       "/playlist.grafana.app/playlists/default/set-minimum-uid",
+				CreatedBy: "set-minimum-creator",
 			},
-		}
-		resp, err := s.Create(context.Background(), &req)
-		require.NoError(t, err)
-		require.NotNil(t, resp)
-		require.Equal(t, entity.CreateEntityResponse_CREATED, resp.Status)
-		require.Nil(t, resp.Error)
-
-		exp := entity.Entity{
-			Group:     "playlist.grafana.app",
-			Resource:  "playlists",
-			Namespace: "default",
-			Name:      "set-minimum-uid",
-			Key:       key,
-			CreatedBy: createdBy,
-		}
-
-		read, err := s.Read(context.Background(), &entity.ReadEntityRequest{
-			Key: key,
-		})
-		require.NoError(t, err)
-		require.NotNil(t, read)
-
-		require.Greater(t, len(read.Guid), 0)
-		// is there a predictable way to compare the new version with the previous one?
-		// I seem to remember that they don't increment by 1.
-		require.Greater(t, read.ResourceVersion, int64(0))
-		require.Greater(t, len(read.ETag), 0)
-
-		require.Equal(t, exp.Origin, read.Origin)
-		require.Equal(t, exp.Group, read.Group)
-		require.Equal(t, exp.Resource, read.Resource)
-		require.Equal(t, exp.Namespace, read.Namespace)
-		require.Equal(t, exp.Name, read.Name)
-		require.Equal(t, exp.Subresource, read.Subresource)
-		require.Equal(t, exp.GroupVersion, read.GroupVersion)
-		require.Equal(t, exp.Key, read.Key)
-		require.Equal(t, exp.Folder, read.Folder)
-		require.Equal(t, exp.Meta, read.Meta)
-		require.Equal(t, exp.Body, read.Body)
-		require.Equal(t, exp.Status, read.Status)
-		require.Equal(t, exp.Title, read.Title)
-		require.Equal(t, exp.Size, read.Size)
-		require.Equal(t, exp.CreatedAt, read.CreatedAt)
-		require.Equal(t, exp.CreatedBy, read.CreatedBy)
-		require.Equal(t, exp.UpdatedAt, read.UpdatedAt)
-		require.Equal(t, exp.UpdatedBy, read.UpdatedBy)
-		require.Equal(t, exp.Description, read.Description)
-		require.Equal(t, exp.Slug, read.Slug)
-		require.Equal(t, exp.Message, read.Message)
-		require.Equal(t, exp.Labels, read.Labels)
-		require.Equal(t, exp.Fields, read.Fields)
-		require.Equal(t, exp.Errors, read.Errors)
-	})
-
-	t.Run("entity with no CreatedBy", func(t *testing.T) {
-		key := "/playlist.grafana.app/playlists/default/set-minimum"
-
-		req := entity.CreateEntityRequest{
-			Entity: &entity.Entity{
-				Key: key,
+			false,
+			true,
+		},
+		{
+			"request with no entity creator",
+			entity.Entity{
+				Key: "/playlist.grafana.app/playlists/default/set-only-key",
 			},
-		}
-
-		resp, err := s.Create(context.Background(), &req)
-		require.Error(t, err)
-		require.Nil(t, resp)
-		// entity.CreateEntityResponse_ERROR doesn't get set for the status in this case
-	})
-
-	t.Run("entity with no Key", func(t *testing.T) {
-		req := entity.CreateEntityRequest{
-			Entity: &entity.Entity{
+			true,
+			false,
+		},
+		{
+			"request with no key",
+			entity.Entity{
 				CreatedBy: "entity-creator",
 			},
-		}
+			true,
+			true,
+		},
+	}
 
-		resp, err := s.Create(context.Background(), &req)
-		require.Error(t, err)
-		require.Equal(t, entity.CreateEntityResponse_ERROR, resp.Status)
-		// Entity.Errors docs say "When errors exist" but it looks unused
-	})
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req := entity.CreateEntityRequest{
+				Entity: &entity.Entity{
+					Key:       tc.ent.Key,
+					CreatedBy: tc.ent.CreatedBy,
+				},
+			}
+			resp, err := s.Create(context.Background(), &req)
+
+			if tc.errIsExpected {
+				require.Error(t, err)
+
+				if tc.statusIsExpected {
+					require.Equal(t, entity.CreateEntityResponse_ERROR, resp.Status)
+				}
+
+				return
+			}
+
+			require.Nil(t, err)
+			require.Equal(t, entity.CreateEntityResponse_CREATED, resp.Status)
+			require.NotNil(t, resp)
+			require.Nil(t, resp.Error)
+
+			read, err := s.Read(context.Background(), &entity.ReadEntityRequest{
+				Key: tc.ent.Key,
+			})
+			require.NoError(t, err)
+			require.NotNil(t, read)
+
+			require.Greater(t, len(read.Guid), 0)
+			require.Greater(t, read.ResourceVersion, int64(0))
+
+			expectedETag := createContentsHash(tc.ent.Body, tc.ent.Meta, tc.ent.Status)
+			require.Equal(t, expectedETag, read.ETag)
+			require.Equal(t, tc.ent.Origin, read.Origin)
+			require.Equal(t, tc.ent.Group, read.Group)
+			require.Equal(t, tc.ent.Resource, read.Resource)
+			require.Equal(t, tc.ent.Namespace, read.Namespace)
+			require.Equal(t, tc.ent.Name, read.Name)
+			require.Equal(t, tc.ent.Subresource, read.Subresource)
+			require.Equal(t, tc.ent.GroupVersion, read.GroupVersion)
+			require.Equal(t, tc.ent.Key, read.Key)
+			require.Equal(t, tc.ent.Folder, read.Folder)
+			require.Equal(t, tc.ent.Meta, read.Meta)
+			require.Equal(t, tc.ent.Body, read.Body)
+			require.Equal(t, tc.ent.Status, read.Status)
+			require.Equal(t, tc.ent.Title, read.Title)
+			require.Equal(t, tc.ent.Size, read.Size)
+			require.Equal(t, tc.ent.CreatedAt, read.CreatedAt)
+			require.Equal(t, tc.ent.CreatedBy, read.CreatedBy)
+			require.Equal(t, tc.ent.UpdatedAt, read.UpdatedAt)
+			require.Equal(t, tc.ent.UpdatedBy, read.UpdatedBy)
+			require.Equal(t, tc.ent.Description, read.Description)
+			require.Equal(t, tc.ent.Slug, read.Slug)
+			require.Equal(t, tc.ent.Message, read.Message)
+			require.Equal(t, tc.ent.Labels, read.Labels)
+			require.Equal(t, tc.ent.Fields, read.Fields)
+			require.Equal(t, tc.ent.Errors, read.Errors)
+		})
+	}
 }
 
 func setUpTestServer(t *testing.T) entity.EntityStoreServer {
