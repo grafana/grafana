966a26bc3b5a6b9b0b263820f03ee8f66a5ad40d
eledobleefe <laura.fernandez@grafana.com>
2024-01-26 16:29:39 +0100

    Format returnToPrevious

--- packages/grafana-runtime/src/utils/returnToPrevious.ts
+++ packages/grafana-runtime/src/utils/returnToPrevious.ts
@@ -14,9 +14,9 @@ export const setReturnToPreviousHook = (hook: ReturnToPreviousHook) => {
 export const useReturnToPrevious: ReturnToPreviousHook = () => {
   if (!rtpHook) {
     if (process.env.NODE_ENV !== 'production') {
-      throw new Error("useReturnToPrevious hook not found in @grafana/runtime")
+      throw new Error('useReturnToPrevious hook not found in @grafana/runtime');
     }
-    return () => console.error("ReturnToPrevious hook not found")
+    return () => console.error('ReturnToPrevious hook not found');
   }
 
   return rtpHook();
