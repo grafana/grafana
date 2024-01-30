d5abd3eb1129e0756e8ad60fa17637d853270f76
Gilles De Mey <gilles.de.mey@gmail.com>
2024-01-30 22:52:43 +0100

    fix support check for export with modifications

--- public/app/features/alerting/unified/hooks/useAbilities.ts
+++ public/app/features/alerting/unified/hooks/useAbilities.ts
@@ -146,6 +146,7 @@ export function useAllAlertRuleAbilities(rule: CombinedRule): Abilities<AlertRul
 
   const isProvisioned = isGrafanaRulerRule(rule.rulerRule) && Boolean(rule.rulerRule.grafana_alert.provenance);
   const isFederated = isFederatedRuleGroup(rule.group);
+  const isGrafanaManagedAlertRule = isGrafanaRulerRule(rule.rulerRule);
 
   // if a rule is either provisioned or a federated rule, we don't allow it to be removed or edited
   const immutableRule = isProvisioned || isFederated;
@@ -173,7 +174,7 @@ export function useAllAlertRuleAbilities(rule: CombinedRule): Abilities<AlertRul
     [AlertRuleAction.Delete]: [MaybeSupportedUnlessImmutable, isRemovable ?? false],
     [AlertRuleAction.Explore]: toAbility(AlwaysSupported, AccessControlAction.DataSourcesExplore),
     [AlertRuleAction.Silence]: canSilence,
-    [AlertRuleAction.ModifyExport]: [MaybeSupported, exportAllowed],
+    [AlertRuleAction.ModifyExport]: [isGrafanaManagedAlertRule, exportAllowed],
   };
 
   return abilities;
