5dac64bb9c45e9f1f878effad8f6748c9323891c
bohandley <brendan.ohandley@gmail.com>
2024-01-29 21:51:53 -0600

    fix tests with modeule

--- packages/grafana-prometheus/src/module.ts
+++ packages/grafana-prometheus/src/module.ts
@@ -1,13 +1,13 @@
 // DONT NEED THIS BUT MAYBE EXPORT THIS TO CORE PROM
 
-// import { DataSourcePlugin } from '@grafana/data';
+import { DataSourcePlugin } from '@grafana/data';
 
-// import PromCheatSheet from './components/PromCheatSheet';
-// import PromQueryEditorByApp from './components/PromQueryEditorByApp';
-// import { ConfigEditor } from './configuration/ConfigEditor';
-// import { PrometheusDatasource } from './datasource';
+import PromCheatSheet from './components/PromCheatSheet';
+import PromQueryEditorByApp from './components/PromQueryEditorByApp';
+import { ConfigEditor } from './configuration/ConfigEditor';
+import { PrometheusDatasource } from './datasource';
 
-// export const plugin = new DataSourcePlugin(PrometheusDatasource)
-//   .setQueryEditor(PromQueryEditorByApp)
-//   .setConfigEditor(ConfigEditor)
-//   .setQueryEditorHelp(PromCheatSheet);
+export const plugin = new DataSourcePlugin(PrometheusDatasource)
+  .setQueryEditor(PromQueryEditorByApp)
+  .setConfigEditor(ConfigEditor)
+  .setQueryEditorHelp(PromCheatSheet);
