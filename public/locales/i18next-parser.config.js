b27eb2086d04a0547f49b4849f0e98c0bb2a9e58
Laura Benz <laura.benz@grafana.com>
2024-01-31 14:08:15 +0100

    refactor: add changes after code review

--- public/locales/i18next-parser.config.js
+++ public/locales/i18next-parser.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // Default namespace used in your i18next config
   defaultNamespace: 'grafana',
-  // Adds changes only to en-US and pseudo-LOCALE when extracting keys, every other language is provided by Crowdin
-  locales: ['en-US', 'pseudo-LOCALE'],
+  // Adds changes only to en-US when extracting keys, every other language is provided by Crowdin
+  locales: ['en-US'],
 
   output: './public/locales/$LOCALE/$NAMESPACE.json',
 
