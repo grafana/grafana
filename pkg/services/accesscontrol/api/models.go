df0ccefa1553a520a976f53b24ffadebafb0b3a1
Gabriel MABILLE <gabriel.mabille@grafana.com>
2024-01-31 16:34:54 +0100

    How it should work

--- pkg/services/accesscontrol/api/models.go
+++ pkg/services/accesscontrol/api/models.go
@@ -6,44 +6,36 @@ package api
 // swagger:parameters searchPermissions
 type SearchPermissionsParams struct {
 	// in:query
+	// example: 12
 	// required:false
 	UserId int64 `json:"userId"`
 
 	// in:query
+	// example: "viewer1"
 	// required:false
 	UserLogin string `json:"userLogin"`
 
 	// in:query
+	// example: "teams:"
 	// required:false
 	ActionPrefix string `json:"actionPrefix"`
 
 	// in:query
+	// example: "teams:read"
 	// required:false
 	Action string `json:"action"`
 
 	// in:query
+	// example: "teams:id:1"
 	// required:false
 	Scope string `json:"scope"`
 }
 
-// PermissionsByAction are permissions with scopes grouped by action
-// example: { "teams.read": [ "teams:id:1", "teams:id:2" ] }
-// swagger:model
-type PermissionsByAction map[string][]string
-
-// UserID
-// example: 12
-// pattern: \d+
-// swagger:model
-type UserID string
-
-// UsersPermissions are permissions grouped by userID
-// example: { "1": { "teams.read": [ "teams:id:1", "teams:id:2" ] }, "3": { "teams.read": [ "teams:id:3" ] } }
-// swagger:model
-type UsersPermissions map[UserID]PermissionsByAction
-
 // swagger:response searchPermissionsResponse
 type SearchPermissionsResponse struct {
+
+	// User permissions grouped by user ID, with scopes grouped by action.
+	// example: { "1": { "teams.read": [ "teams:id:1", "teams:id:2" ] }, "3": { "teams.read": [ "teams:id:3" ] } }
 	// in: body
-	Body UsersPermissions `json:"body"`
+	Body map[string]map[string][]string `json:"body"`
 }
