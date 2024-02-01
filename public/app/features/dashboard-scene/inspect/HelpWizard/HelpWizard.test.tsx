4faebf055fb7a3c29289c820884add0667a86958
harisrozajac <haris.rozajac12@gmail.com>
2024-02-01 11:54:35 -0700

    Fix HelpWizard and SupportSnapshotService tests

--- public/app/features/dashboard-scene/inspect/HelpWizard/HelpWizard.test.tsx
+++ public/app/features/dashboard-scene/inspect/HelpWizard/HelpWizard.test.tsx
@@ -1,36 +1,105 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { of } from 'rxjs';
 
 import { FieldType, getDefaultTimeRange, LoadingState, toDataFrame } from '@grafana/data';
 import { getPanelPlugin } from '@grafana/data/test/__mocks__/pluginMocks';
+import {
+  sceneGraph,
+  SceneGridItem,
+  SceneGridLayout,
+  SceneQueryRunner,
+  SceneTimeRange,
+  VizPanel,
+  VizPanelMenu,
+} from '@grafana/scenes';
 
-import { PanelModel } from '../../state/PanelModel';
+import { DashboardScene } from '../../scene/DashboardScene';
+import { VizPanelLinks, VizPanelLinksMenu } from '../../scene/PanelLinks';
+import { panelMenuBehavior } from '../../scene/PanelMenuBehavior';
 
 import { HelpWizard } from './HelpWizard';
 
-function setup() {
-  const panel = new PanelModel({});
-  panel.plugin = getPanelPlugin({});
-  panel.getQueryRunner().setLastResult({
-    timeRange: getDefaultTimeRange(),
-    state: LoadingState.Done,
-    series: [
-      toDataFrame({
-        name: 'http_requests_total',
-        fields: [
-          { name: 'Time', type: FieldType.time, values: [1, 2, 3] },
-          { name: 'Value', type: FieldType.number, values: [11, 22, 33] },
-        ],
-      }),
-    ],
-  });
-  panel.getQueryRunner().resendLastResult();
+async function setup() {
+  const { panel } = await buildTestScene();
+  panel.getPlugin = () => getPanelPlugin({ skipDataQuery: false });
+  const queryRunner = sceneGraph.getData(panel);
+  queryRunner.getResultsStream = jest.fn().mockImplementation(() =>
+    of({
+      data: {
+        state: 'Done',
+        series: [],
+      },
+    })
+  );
 
-  return render(<HelpWizard panel={panel} onClose={() => {}} plugin={panel.plugin} />);
+  return render(<HelpWizard panel={panel} onClose={() => {}} />);
 }
 describe('SupportSnapshot', () => {
   it('Can render', async () => {
     setup();
-    expect(await screen.findByRole('button', { name: 'Dashboard (2.97 KiB)' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Dashboard (2.94 KiB)' })).toBeInTheDocument();
   });
 });
+
+async function buildTestScene() {
+  const menu = new VizPanelMenu({
+    $behaviors: [panelMenuBehavior],
+  });
+
+  const panel = new VizPanel({
+    title: 'Panel A',
+    pluginId: 'timeseries',
+    key: 'panel-12',
+    menu,
+    titleItems: [new VizPanelLinks({ menu: new VizPanelLinksMenu({}) })],
+    $data: new SceneQueryRunner({
+      data: {
+        state: LoadingState.Done,
+        series: [
+          toDataFrame({
+            name: 'http_requests_total',
+            fields: [
+              { name: 'Time', type: FieldType.time, values: [1, 2, 3] },
+              { name: 'Value', type: FieldType.number, values: [11, 22, 33] },
+            ],
+          }),
+        ],
+        timeRange: getDefaultTimeRange(),
+      },
+      datasource: { uid: 'my-uid' },
+      queries: [{ query: 'QueryA', refId: 'A' }],
+    }),
+  });
+
+  const scene = new DashboardScene({
+    title: 'My dashboard',
+    uid: 'dash-1',
+    tags: ['database', 'panel'],
+    $timeRange: new SceneTimeRange({
+      from: 'now-5m',
+      to: 'now',
+      timeZone: 'Africa/Abidjan',
+    }),
+    meta: {
+      canEdit: true,
+      isEmbedded: false,
+    },
+    body: new SceneGridLayout({
+      children: [
+        new SceneGridItem({
+          key: 'griditem-1',
+          x: 0,
+          y: 0,
+          width: 10,
+          height: 12,
+          body: panel,
+        }),
+      ],
+    }),
+  });
+
+  await new Promise((r) => setTimeout(r, 1));
+
+  return { scene, panel, menu };
+}
--- public/app/features/dashboard-scene/inspect/HelpWizard/SupportSnapshotService.test.ts
+++ public/app/features/dashboard-scene/inspect/HelpWizard/SupportSnapshotService.test.ts
@@ -1,33 +1,54 @@
-import { FieldType, getDefaultTimeRange, LoadingState, toDataFrame } from '@grafana/data';
+import { of } from 'rxjs';
 
-import { PanelModel } from '../../state/PanelModel';
+import { FieldType, getDefaultTimeRange, LoadingState, toDataFrame } from '@grafana/data';
+import {
+  sceneGraph,
+  SceneGridItem,
+  SceneGridLayout,
+  SceneQueryRunner,
+  SceneTimeRange,
+  VizPanel,
+  VizPanelMenu,
+} from '@grafana/scenes';
+
+import { DashboardScene } from '../../scene/DashboardScene';
+import { VizPanelLinks, VizPanelLinksMenu } from '../../scene/PanelLinks';
+import { panelMenuBehavior } from '../../scene/PanelMenuBehavior';
 
 import { SnapshotTab, SupportSnapshotService } from './SupportSnapshotService';
 
-describe('SupportSnapshotService', () => {
-  const panel = new PanelModel({});
-  panel.getQueryRunner().setLastResult({
-    timeRange: getDefaultTimeRange(),
-    state: LoadingState.Done,
-    series: [
-      toDataFrame({
-        name: 'http_requests_total',
-        fields: [
-          { name: 'Time', type: FieldType.time, values: [1, 2, 3] },
-          { name: 'Value', type: FieldType.number, values: [11, 22, 33] },
-        ],
-      }),
-    ],
-  });
-  panel.getQueryRunner().resendLastResult();
+async function setup() {
+  const { panel } = await buildTestScene();
 
-  it('Can create it with default state', () => {
-    const service = new SupportSnapshotService(panel);
+  const queryRunner = sceneGraph.getData(panel);
+  queryRunner.getResultsStream = jest.fn().mockImplementation(() =>
+    of({
+      data: {
+        state: 'Done',
+        series: [
+          toDataFrame({
+            name: 'http_requests_total',
+            fields: [
+              { name: 'Time', type: FieldType.time, values: [1, 2, 3] },
+              { name: 'Value', type: FieldType.number, values: [11, 22, 33] },
+            ],
+          }),
+        ],
+      },
+    })
+  );
+
+  return new SupportSnapshotService(panel);
+}
+
+describe('SupportSnapshotService', () => {
+  it('Can create it with default state', async () => {
+    const service = await setup();
     expect(service.state.currentTab).toBe(SnapshotTab.Support);
   });
 
   it('Can can build support snapshot dashboard', async () => {
-    const service = new SupportSnapshotService(panel);
+    const service = await setup();
     await service.buildDebugDashboard();
     expect(service.state.snapshot.panels[0].targets[0]).toMatchInlineSnapshot(`
       {
@@ -76,3 +97,65 @@ describe('SupportSnapshotService', () => {
     `);
   });
 });
+
+async function buildTestScene() {
+  const menu = new VizPanelMenu({
+    $behaviors: [panelMenuBehavior],
+  });
+
+  const panel = new VizPanel({
+    title: 'Panel A',
+    pluginId: 'timeseries',
+    key: 'panel-12',
+    menu,
+    titleItems: [new VizPanelLinks({ menu: new VizPanelLinksMenu({}) })],
+    $data: new SceneQueryRunner({
+      data: {
+        state: LoadingState.Done,
+        series: [
+          toDataFrame({
+            name: 'http_requests_total',
+            fields: [
+              { name: 'Time', type: FieldType.time, values: [1, 2, 3] },
+              { name: 'Value', type: FieldType.number, values: [11, 22, 33] },
+            ],
+          }),
+        ],
+        timeRange: getDefaultTimeRange(),
+      },
+      datasource: { uid: 'my-uid' },
+      queries: [{ query: 'QueryA', refId: 'A' }],
+    }),
+  });
+
+  const scene = new DashboardScene({
+    title: 'My dashboard',
+    uid: 'dash-1',
+    tags: ['database', 'panel'],
+    $timeRange: new SceneTimeRange({
+      from: 'now-5m',
+      to: 'now',
+      timeZone: 'Africa/Abidjan',
+    }),
+    meta: {
+      canEdit: true,
+      isEmbedded: false,
+    },
+    body: new SceneGridLayout({
+      children: [
+        new SceneGridItem({
+          key: 'griditem-1',
+          x: 0,
+          y: 0,
+          width: 10,
+          height: 12,
+          body: panel,
+        }),
+      ],
+    }),
+  });
+
+  await new Promise((r) => setTimeout(r, 1));
+
+  return { scene, panel, menu };
+}
