c072853dbf47fadead73bdb2569e25bf7b29a690
Laura Benz <48948963+L-M-K-B@users.noreply.github.com>
2024-02-01 12:44:18 +0100

    I18n: Refactor crowdin workflow (#81571)
    * refactor: crowdin config
    
    * feat: add GH actions
    
    * refactor: remove old GH action
    
    * refactor: fix formatting issue
    
    * refactor: adjust docs
    
    * refactor: add changes after code review
    
    * refactor: add changes after code review
    
    * refactor: update CODEOWNERS file

--- public/locales/i18next-parser.config.js
+++ public/locales/i18next-parser.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // Default namespace used in your i18next config
   defaultNamespace: 'grafana',
-
-  locales: ['en-US', 'fr-FR', 'es-ES', "de-DE", "zh-Hans", 'pseudo-LOCALE'],
+  // Adds changes only to en-US when extracting keys, every other language is provided by Crowdin
+  locales: ['en-US'],
 
   output: './public/locales/$LOCALE/$NAMESPACE.json',
 
