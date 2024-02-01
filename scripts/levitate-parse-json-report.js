db005438e17a42675371495d3bda013fa97e1f86
Esteban Beltran <esteban@academo.me>
2024-02-01 15:03:34 +0100

    ClearAnsi again

--- scripts/levitate-parse-json-report.js
+++ scripts/levitate-parse-json-report.js
@@ -2,11 +2,15 @@ const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
+function clearAnsi(str) {
+  return str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry]/g, '');
+}
+
 const printSection = (title, items) => {
   let output = `<h4>${title}</h4>`;
   items.forEach((item) => {
     const language = item.declaration ? 'typescript' : 'diff';
-    const code = item.declaration ? item.declaration : item.diff;
+    const code = item.declaration ? item.declaration : clearAnsi(item.diff);
 
     output += `<b>${item.name}</b><br>\n`;
     output += `<sub>${item.location}</sub><br>\n`;
