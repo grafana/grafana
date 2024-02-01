fdbd351b0dd3f6c5fc4474edca5222bd3c2f7a72
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-02-01 10:49:30 -0500

    add integration tests

---
+++ pkg/tests/api/alerting/api_notifications_time_interval_test.go
@@ -0,0 +1,155 @@
+package alerting
+
+import (
+	"net/http"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/prometheus/alertmanager/config"
+	"github.com/prometheus/alertmanager/timeinterval"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
+	"github.com/grafana/grafana/pkg/services/ngalert/models"
+	"github.com/grafana/grafana/pkg/services/org"
+	"github.com/grafana/grafana/pkg/services/user"
+	"github.com/grafana/grafana/pkg/tests/testinfra"
+	"github.com/grafana/grafana/pkg/util"
+)
+
+func TestTimeInterval(t *testing.T) {
+	dir, path := testinfra.CreateGrafDir(t, testinfra.GrafanaOpts{
+		DisableLegacyAlerting: true,
+		EnableUnifiedAlerting: true,
+		DisableAnonymous:      true,
+		AppModeProduction:     true,
+	})
+
+	grafanaListedAddr, store := testinfra.StartGrafana(t, dir, path)
+
+	createUser(t, store, user.CreateUserCommand{
+		DefaultOrgRole: string(org.RoleAdmin),
+		Password:       "admin",
+		Login:          "admin",
+	})
+
+	apiClient := newAlertingApiClient(grafanaListedAddr, "admin", "admin")
+
+	t.Run("default config should return empty list", func(t *testing.T) {
+		mt, status, body := apiClient.GetAllTimeIntervalsWithStatus(t)
+		requireStatusCode(t, http.StatusOK, status, body)
+		require.Empty(t, mt)
+	})
+
+	emptyTimeInterval := definitions.NamedTimeIntervals{
+		Name:          "Empty Mute Timing",
+		TimeIntervals: []definitions.TimeIntervalItem{},
+	}
+
+	func() {
+		// TODO replace with Time-Interval later
+		emptyMuteTiming := definitions.MuteTimeInterval{
+			MuteTimeInterval: config.MuteTimeInterval{
+				Name:          "Empty Mute Timing",
+				TimeIntervals: []timeinterval.TimeInterval{},
+			},
+		}
+
+		// TODO replace with create interval API
+		// t.Run("should create a new mute timing without any intervals", func(t *testing.T) {
+		mt, status, body := apiClient.CreateMuteTimingWithStatus(t, emptyMuteTiming)
+		requireStatusCode(t, http.StatusCreated, status, body)
+		require.Equal(t, emptyMuteTiming.MuteTimeInterval, mt.MuteTimeInterval)
+		require.EqualValues(t, models.ProvenanceAPI, mt.Provenance)
+		// })
+
+		anotherMuteTiming := definitions.MuteTimeInterval{
+			MuteTimeInterval: config.MuteTimeInterval{
+				Name: "Not Empty Mute Timing",
+				TimeIntervals: []timeinterval.TimeInterval{
+					{
+						Times: []timeinterval.TimeRange{
+							{
+								StartMinute: 10,
+								EndMinute:   45,
+							},
+						},
+						Weekdays: []timeinterval.WeekdayRange{
+							{
+								InclusiveRange: timeinterval.InclusiveRange{
+									Begin: 0,
+									End:   2,
+								},
+							},
+							{
+								InclusiveRange: timeinterval.InclusiveRange{
+									Begin: 4,
+									End:   5,
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		// t.Run("should create a new mute timing with some settings", func(t *testing.T) {
+		mt, status, body = apiClient.CreateMuteTimingWithStatus(t, anotherMuteTiming)
+		requireStatusCode(t, http.StatusCreated, status, body)
+		require.Equal(t, anotherMuteTiming.MuteTimeInterval, mt.MuteTimeInterval)
+		require.EqualValues(t, models.ProvenanceAPI, mt.Provenance)
+		// })
+	}()
+
+	anotherTimeInterval := definitions.NamedTimeIntervals{
+		Name: "Not Empty Mute Timing",
+		TimeIntervals: []definitions.TimeIntervalItem{
+			{
+				Times: []definitions.TimeIntervalTimeRange{
+					{
+						StartMinute: "00:10",
+						EndMinute:   "00:45",
+					},
+				},
+				Weekdays: util.Pointer([]string{
+					"sunday:tuesday",
+					"thursday:friday",
+				}),
+			},
+		},
+	}
+
+	t.Run("should return time interval by name", func(t *testing.T) {
+		ti, status, body := apiClient.GetTimeIntervalByNameWithStatus(t, emptyTimeInterval.Name)
+		requireStatusCode(t, http.StatusOK, status, body)
+		require.Equal(t, emptyTimeInterval, ti.NamedTimeIntervals)
+		require.EqualValues(t, models.ProvenanceAPI, ti.Provenance)
+
+		ti, status, body = apiClient.GetTimeIntervalByNameWithStatus(t, anotherTimeInterval.Name)
+		requireStatusCode(t, http.StatusOK, status, body)
+		require.Equal(t, anotherTimeInterval, ti.NamedTimeIntervals)
+		require.EqualValues(t, models.ProvenanceAPI, ti.Provenance)
+	})
+
+	t.Run("should return NotFound if time interval does not exist", func(t *testing.T) {
+		_, status, body := apiClient.GetTimeIntervalByNameWithStatus(t, "some-missing-timing")
+		requireStatusCode(t, http.StatusNotFound, status, body)
+	})
+
+	t.Run("should return all mute timings", func(t *testing.T) {
+		mt, status, body := apiClient.GetAllTimeIntervalsWithStatus(t)
+		requireStatusCode(t, http.StatusOK, status, body)
+		require.Len(t, mt, 2)
+
+		slices.SortFunc(mt, func(a, b definitions.NamedTimeIntervalsWithProvenance) int {
+			return strings.Compare(a.Name, b.Name)
+		})
+
+		require.Equal(t, emptyTimeInterval, mt[0].NamedTimeIntervals)
+		require.EqualValues(t, models.ProvenanceAPI, mt[0].Provenance)
+
+		require.Equal(t, anotherTimeInterval, mt[1].NamedTimeIntervals)
+		require.EqualValues(t, models.ProvenanceAPI, mt[1].Provenance)
+	})
+}
