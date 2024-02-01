628fd3027aa9a661710de6e765bb310bb2f29fce
ismail simsek <ismailsimsek09@gmail.com>
2024-02-01 15:59:33 +0100

    reorder imports

--- pkg/tsdb/prometheus/client/transport.go
+++ pkg/tsdb/prometheus/client/transport.go
@@ -9,6 +9,7 @@ import (
 	sdkhttpclient "github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/tsdb/prometheus/azureauth"
 	"github.com/grafana/grafana/pkg/tsdb/prometheus/middleware"
