9c954d35ee382efe242e39e2124739e75887ac9c
ismail simsek <ismailsimsek09@gmail.com>
2024-02-01 13:20:29 +0100

    remove grafana/pkg/setting imports

--- pkg/tsdb/prometheus/client/transport_test.go
+++ pkg/tsdb/prometheus/client/transport_test.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"testing"
 
-	"github.com/grafana/grafana-azure-sdk-go/azsettings"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/stretchr/testify/require"
-
-	"github.com/grafana/grafana/pkg/setting"
 )
 
 func TestCreateTransportOptions(t *testing.T) {
@@ -21,7 +18,7 @@ func TestCreateTransportOptions(t *testing.T) {
 				"httpHeaderValue1": "bar",
 			},
 		}
-		opts, err := CreateTransportOptions(context.Background(), settings, &setting.Cfg{}, backend.NewLoggerWith("logger", "test"))
+		opts, err := CreateTransportOptions(context.Background(), settings, backend.NewLoggerWith("logger", "test"))
 		require.NoError(t, err)
 		require.Equal(t, map[string]string{"foo": "bar"}, opts.Headers)
 		require.Equal(t, 2, len(opts.Middlewares))
@@ -38,7 +35,7 @@ func TestCreateTransportOptions(t *testing.T) {
 			}`),
 			DecryptedSecureJSONData: map[string]string{},
 		}
-		opts, err := CreateTransportOptions(context.Background(), settings, &setting.Cfg{AzureAuthEnabled: true, Azure: &azsettings.AzureSettings{}}, backend.NewLoggerWith("logger", "test"))
+		opts, err := CreateTransportOptions(context.Background(), settings /*&setting.Cfg{AzureAuthEnabled: true, Azure: &azsettings.AzureSettings{}},*/, backend.NewLoggerWith("logger", "test"))
 		require.NoError(t, err)
 		require.Equal(t, 3, len(opts.Middlewares))
 	})
