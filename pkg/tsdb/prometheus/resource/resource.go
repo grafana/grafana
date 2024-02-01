065ddf89cc216d9fb004c0ee048cd29986813d4e
ismail simsek <ismailsimsek09@gmail.com>
2024-02-01 18:24:23 +0100

    reorder imports

--- pkg/tsdb/prometheus/resource/resource.go
+++ pkg/tsdb/prometheus/resource/resource.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+
 	"github.com/grafana/grafana/pkg/tsdb/prometheus/client"
 	"github.com/grafana/grafana/pkg/tsdb/prometheus/utils"
 	"github.com/grafana/grafana/pkg/util/maputil"
