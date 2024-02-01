0d9278e03ec5d03d413a67686c52f9f8772afa45
Piotr Jamróz <pm.jamroz@gmail.com>
2024-02-01 13:13:15 +0100

    Update packages/grafana-runtime/src/utils/logging.ts
    Co-authored-by: Ivan Ortega Alba <ivanortegaalba@gmail.com>

--- packages/grafana-runtime/src/utils/logging.ts
+++ packages/grafana-runtime/src/utils/logging.ts
@@ -59,8 +59,18 @@ export function logError(err: Error, contexts?: LogContext) {
 }
 
 /**
- * Wrapper for logX functions.
- * Creates a logger for frontend monitoring which contains provided "source" and context as defaults when calling logX()
+ * Creates a monitoring logger with four levels of logging methods: `logDebug`, `logInfo`, `logWarning`, and `logError`.
+ * These methods use `faro.api.pushX` web SDK methods to report these logs or errors to the Faro collector.
+ *
+ * @param {string} source - Identifier for the source of the log messages.
+ * @param {LogContext} [defaultContext] - Context to be included in every log message.
+ *
+ * @returns {Object} Logger object with four methods:
+ * - `logDebug(message: string, contexts?: LogContext)`: Logs a debug message.
+ * - `logInfo(message: string, contexts?: LogContext)`: Logs an informational message.
+ * - `logWarning(message: string, contexts?: LogContext)`: Logs a warning message.
+ * - `logError(error: Error, contexts?: LogContext)`: Logs an error message.
+ * Each method combines the `defaultContext` (if provided), the `source`, and an optional `LogContext` parameter into a full context that is included with the log message.
  */
 export function createMonitoringLogger(source: string, defaultContext?: LogContext) {
   const createFullContext = (contexts?: LogContext) => ({
