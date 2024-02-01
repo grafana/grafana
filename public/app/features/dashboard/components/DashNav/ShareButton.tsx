2b620f49a99a22065ad0299206aff259218c5104
Torkel Ödegaard <torkel@grafana.com>
2024-02-01 14:42:27 +0100

    Updated fix

--- public/app/features/dashboard/components/DashNav/ShareButton.tsx
+++ public/app/features/dashboard/components/DashNav/ShareButton.tsx
@@ -1,38 +1,13 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 
 import { selectors as e2eSelectors } from '@grafana/e2e-selectors/src';
 import { locationService } from '@grafana/runtime';
-import { ModalsContext, Button } from '@grafana/ui';
-import { useQueryParams } from 'app/core/hooks/useQueryParams';
+import { Button } from '@grafana/ui';
 import { Trans } from 'app/core/internationalization';
 import { DashboardModel } from 'app/features/dashboard/state';
 import { DashboardInteractions } from 'app/features/dashboard-scene/utils/interactions';
 
-import { ShareModal } from '../ShareModal';
-
 export const ShareButton = ({ dashboard }: { dashboard: DashboardModel }) => {
-  const [queryParams] = useQueryParams();
-  const shareView = queryParams.shareView;
-  const { showModal, hideModal } = useContext(ModalsContext);
-
-  useEffect(() => {
-    if (shareView) {
-      showModal(ShareModal, {
-        dashboard,
-        onDismiss: () => {
-          locationService.partial({ shareView: null });
-        },
-        activeTab: String(shareView),
-      });
-    }
-
-    return () => {
-      if (shareView) {
-        hideModal();
-      }
-    };
-  }, [showModal, hideModal, dashboard, shareView]);
-
   return (
     <Button
       data-testid={e2eSelectors.pages.Dashboard.DashNav.shareButton}
