97baa116e59cdd5e5ba48ca3471181854dcb4020
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-02-01 21:54:28 -0600

    update imports for renamed export defaults to import consts

--- packages/grafana-prometheus/src/module.ts
+++ packages/grafana-prometheus/src/module.ts
@@ -1,9 +1,7 @@
-// DONT NEED THIS BUT MAYBE EXPORT THIS TO CORE PROM
-
 import { DataSourcePlugin } from '@grafana/data';
 
-import PromCheatSheet from './components/PromCheatSheet';
-import PromQueryEditorByApp from './components/PromQueryEditorByApp';
+import { PromCheatSheet } from './components/PromCheatSheet';
+import { MemoPromQueryEditorByApp as PromQueryEditorByApp } from './components/PromQueryEditorByApp';
 import { ConfigEditor } from './configuration/ConfigEditor';
 import { PrometheusDatasource } from './datasource';
 
