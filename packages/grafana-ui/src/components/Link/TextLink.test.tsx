33e4506f15940e1c0c0e7a82383b0a0a30f38b59
Joao Silva <joao.silva@grafana.com>
2024-02-02 14:16:14 +0000

    TextLink: Do not strip base from external urls

---
+++ packages/grafana-ui/src/components/Link/TextLink.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import { GrafanaConfig, locationUtil } from '@grafana/data';
+import { locationService } from '@grafana/runtime';
+
+import { TextLink } from './TextLink';
+
+describe('TextLink', () => {
+  const link = 'http://www.grafana.com/grafana/after-sub-url';
+  beforeEach(() => {
+    const { location } = window;
+    // @ts-ignore
+    delete window.location;
+
+    window.location = {
+      ...location,
+      origin: 'http://www.grafana.com',
+    };
+  });
+
+  it('should keep the whole url, including app sub url, if external', () => {
+    locationUtil.initialize({
+      config: { appSubUrl: '/grafana' } as GrafanaConfig,
+      getVariablesUrlParams: jest.fn(),
+      getTimeRangeForUrl: jest.fn(),
+    });
+
+    render(
+      <TextLink href={link} external>
+        Link to Grafana
+      </TextLink>
+    );
+    expect(screen.getByRole('link')).toHaveAttribute('href', link);
+  });
+  it('should turn it into a relative url, if not external', () => {
+    locationUtil.initialize({
+      config: { appSubUrl: '/grafana' } as GrafanaConfig,
+      getVariablesUrlParams: jest.fn(),
+      getTimeRangeForUrl: jest.fn(),
+    });
+
+    render(
+      <Router history={locationService.getHistory()}>
+        <TextLink href={link}>Link to Grafana</TextLink>
+      </Router>
+    );
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/after-sub-url');
+  });
+});
