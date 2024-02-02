0382a308d3a3679ceaa5bbe3d0512c42493a0055
Gilles De Mey <gilles.de.mey@gmail.com>
2024-02-02 16:43:46 +0100

    remove unnecessary url

--- public/app/features/alerting/unified/components/WithReturnButton.tsx
+++ public/app/features/alerting/unified/components/WithReturnButton.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 
-import { locationService, useReturnToPrevious } from '@grafana/runtime';
+import { useReturnToPrevious } from '@grafana/runtime';
 
 interface WithReturnButtonProps {
   component: JSX.Element;
@@ -10,11 +10,10 @@ interface WithReturnButtonProps {
 // @TODO translations?
 export const WithReturnButton = ({ component, title = 'previous page' }: WithReturnButtonProps) => {
   const returnToPrevious = useReturnToPrevious();
-  const returnHref = locationService.getLocation().pathname + '?' + locationService.getSearch().toString();
 
   const returnToThisURL = useCallback(() => {
-    returnToPrevious({ title, href: returnHref });
-  }, [returnHref, returnToPrevious, title]);
+    returnToPrevious(title);
+  }, [returnToPrevious, title]);
 
   return React.cloneElement(component, { onClick: returnToThisURL });
 };
