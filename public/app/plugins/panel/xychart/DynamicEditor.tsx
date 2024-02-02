b5fc8f34b5b30073a78de8b9860f5ec363f32c0e
drew08t <drew08@gmail.com>
2024-02-02 09:09:53 -0800

    Remove unused functions and imports

--- public/app/plugins/panel/xychart/DynamicEditor.tsx
+++ public/app/plugins/panel/xychart/DynamicEditor.tsx
@@ -1,15 +1,6 @@
-import { css, cx } from '@emotion/css';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 
-import {
-  GrafanaTheme2,
-  StandardEditorProps,
-  FieldNamePickerBaseNameMode,
-  StandardEditorsRegistryItem,
-  getFrameDisplayName,
-} from '@grafana/data';
-import { Button, IconButton, useStyles2 } from '@grafana/ui';
-import { LayerName } from 'app/core/components/Layers/LayerName';
+import { StandardEditorProps, FieldNamePickerBaseNameMode, StandardEditorsRegistryItem } from '@grafana/data';
 
 import { ScatterSeriesEditor } from './ScatterSeriesEditor';
 import { Options, ScatterSeriesConfig, defaultFieldConfig } from './panelcfg.gen';
@@ -19,29 +10,7 @@ export const DynamicEditor = ({
   onChange,
   context,
 }: StandardEditorProps<ScatterSeriesConfig[], unknown, Options>) => {
-  const frameNames = useMemo(() => {
-    if (context?.data?.length) {
-      return context.data.map((frame, index) => ({
-        value: index,
-        label: `${getFrameDisplayName(frame, index)} (index: ${index}, rows: ${frame.length})`,
-      }));
-    }
-    return [{ value: 0, label: 'First result' }];
-  }, [context.data]);
-
   const selected = 0;
-  const style = useStyles2(getStyles);
-
-  const onFieldChange = (val: unknown | undefined, index: number, field: string) => {
-    onChange(
-      value.map((obj, i) => {
-        if (i === index) {
-          return { ...obj, [field]: val };
-        }
-        return obj;
-      })
-    );
-  };
 
   // TODO set name of series based on common label value if applicable
   // TODO on data changes, compare and recompute if needed
@@ -66,16 +35,6 @@ export const DynamicEditor = ({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const onSeriesDelete = (index: number) => {
-    onChange(value.filter((_, i) => i !== index));
-  };
-
-  // const { options } = context;
-
-  const getRowStyle = (index: number) => {
-    return index === selected ? `${style.row} ${style.sel}` : style.row;
-  };
 
   useEffect(() => {
     console.log(context.data);
@@ -116,37 +75,3 @@ export const DynamicEditor = ({
     )
   );
 };
-
-const getStyles = (theme: GrafanaTheme2) => ({
-  marginBot: css({
-    marginBottom: '20px',
-  }),
-  row: css({
-    padding: `${theme.spacing(0.5, 1)}`,
-    borderRadius: `${theme.shape.radius.default}`,
-    background: `${theme.colors.background.secondary}`,
-    minHeight: `${theme.spacing(4)}`,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: '3px',
-    cursor: 'pointer',
-
-    border: `1px solid ${theme.components.input.borderColor}`,
-    '&:hover': {
-      border: `1px solid ${theme.components.input.borderHover}`,
-    },
-  }),
-  sel: css({
-    border: `1px solid ${theme.colors.primary.border}`,
-    '&:hover': {
-      border: `1px solid ${theme.colors.primary.border}`,
-    },
-  }),
-  actionIcon: css({
-    color: `${theme.colors.text.secondary}`,
-    '&:hover': {
-      color: `${theme.colors.text}`,
-    },
-  }),
-});
