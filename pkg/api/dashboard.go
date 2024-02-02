9271625619e42708b9cf9733cf25b76c0b1c1c8a
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2024-02-02 15:11:23 +0200

    Swagger: Remove redundant annotation (#81780)

--- pkg/api/dashboard.go
+++ pkg/api/dashboard.go
@@ -1021,12 +1021,6 @@ func (hs *HTTPServer) GetDashboardUIDs(c *contextmodel.ReqContext) {
 	c.JSON(http.StatusOK, uids)
 }
 
-// swagger:parameters renderReportPDF
-type RenderReportPDFParams struct {
-	// in:path
-	DashboardID int64
-}
-
 // swagger:parameters restoreDashboardVersionByID
 type RestoreDashboardVersionByIDParams struct {
 	// in:body
