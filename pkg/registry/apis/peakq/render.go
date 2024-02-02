43ee64bbbe348a95f2d55170f6dfbb8d9eb1be07
Ryan McKinley <ryantxu@gmail.com>
2024-02-02 07:31:29 -0800

    Peakq: Split variable definition from replacement strategy (#81757)
    * peakq structure changes
    
    * Update render to get tests passing
    
    ---------
    
    Co-authored-by: Kyle Brandt <kyle@grafana.com>

--- pkg/registry/apis/peakq/render.go
+++ pkg/registry/apis/peakq/render.go
@@ -7,14 +7,15 @@ import (
 	"net/http"
 	"net/url"
 	"sort"
+	"strings"
 
-	"github.com/spyzhov/ajson"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/rest"
 
 	"github.com/grafana/grafana/pkg/apis/common/v0alpha1"
 	peakq "github.com/grafana/grafana/pkg/apis/peakq/v0alpha1"
+	"github.com/spyzhov/ajson"
 )
 
 type renderREST struct {
@@ -90,49 +91,66 @@ func renderPOSTHandler(w http.ResponseWriter, req *http.Request) {
 	_ = json.NewEncoder(w).Encode(results)
 }
 
-func makeVarMapFromParams(v url.Values) (map[string]string, error) {
-	var input map[string]string
-	keys := v["varName"]
-	values := v["varValue"]
-	if len(keys) > 0 || len(values) > 0 {
-		if len(keys) != len(values) {
-			return nil, fmt.Errorf("mismatched parameter lengths for varName and varValue")
+// Replicate the grafana dashboard URL syntax
+// &var-abc=1&var=abc=2&var-xyz=3...
+func makeVarMapFromParams(v url.Values) (map[string][]string, error) {
+	input := make(map[string][]string, len(v))
+	for key, vals := range v {
+		if !strings.HasPrefix(key, "var-") {
+			continue
 		}
-		input = make(map[string]string, len(keys))
-		for i, key := range keys {
-			// TODO check for dup keys
-			input[key] = values[i]
-		}
-	}
-
-	for i, k := range keys {
-		input[k] = values[i] // ignore second values?
+		input[key[4:]] = vals
 	}
 	return input, nil
 }
 
 type replacement struct {
 	*peakq.Position
-	*peakq.QueryVariable
+	*peakq.TemplateVariable
 }
 
 func getReplacementMap(qt peakq.QueryTemplateSpec) map[int]map[string][]replacement {
+	//return make(map[int]map[string][]replacement)
+
 	// int = targetIdx, string = Path
 	byTargetPath := make(map[int]map[string][]replacement)
-	for _, qVar := range qt.Variables {
-		for _, pos := range qVar.Positions {
-			if byTargetPath[pos.TargetIdx] == nil {
-				byTargetPath[pos.TargetIdx] = make(map[string][]replacement)
+
+	varMap := make(map[string]*peakq.TemplateVariable, len(qt.Variables))
+	for i, v := range qt.Variables {
+		varMap[v.Key] = &qt.Variables[i]
+	}
+
+	for i, target := range qt.Targets {
+		if byTargetPath[i] == nil {
+			byTargetPath[i] = make(map[string][]replacement)
+		}
+		for k, vReps := range target.Variables {
+			for rI, rep := range vReps {
+				byTargetPath[i][rep.Path] = append(byTargetPath[i][rep.Path],
+					replacement{
+						Position:         vReps[rI].Position,
+						TemplateVariable: varMap[k],
+					},
+				)
 			}
-			qVar, pos := qVar, pos
-			byTargetPath[pos.TargetIdx][pos.Path] = append(byTargetPath[pos.TargetIdx][pos.Path],
-				replacement{
-					Position:      &pos,
-					QueryVariable: &qVar,
-				},
-			)
 		}
 	}
+	/*
+		for _, rep := range target.Replacement {
+				if byTargetPath[pos.TargetIdx] == nil {
+					byTargetPath[pos.TargetIdx] = make(map[string][]replacement)
+				}
+				qVar, pos := qVar, pos
+				byTargetPath[pos.TargetIdx][pos.Path] = append(byTargetPath[pos.TargetIdx][pos.Path],
+					replacement{
+						Position:         &pos,
+						TemplateVariable: &qVar,
+					},
+				)
+			}
+		}
+	*/
+
 	for idx, byTargetIdx := range byTargetPath {
 		for path := range byTargetIdx {
 			sort.Slice(byTargetPath[idx][path], func(i, j int) bool {
@@ -140,12 +158,11 @@ func getReplacementMap(qt peakq.QueryTemplateSpec) map[int]map[string][]replacem
 			})
 		}
 	}
+
 	return byTargetPath
 }
 
-func Render(qt peakq.QueryTemplateSpec, selectedValues map[string]string) (*peakq.RenderedQuery, error) {
-	// Note: The following is super stupid, will only work with one var, no sanity checking etc
-	// selectedValues is for GET
+func Render(qt peakq.QueryTemplateSpec, selectedValues map[string][]string) (*peakq.RenderedQuery, error) {
 	targets := qt.DeepCopy().Targets
 
 	rawTargetObjects := make([]*ajson.Node, len(qt.Targets))
@@ -182,7 +199,13 @@ func Render(qt peakq.QueryTemplateSpec, selectedValues map[string]string) (*peak
 				// I think breaks with utf...something...?
 				// TODO: Probably simpler to store the non-template parts and insert the values into that, then don't have to track
 				// offsets
-				value := selectedValues[r.Key]
+				if r.Position == nil {
+					return nil, fmt.Errorf("nil position not support yet, will be full replacement")
+				}
+				if len(selectedValues[r.Key]) != 1 {
+					return nil, fmt.Errorf("selected value missing, or more then one provided")
+				}
+				value := selectedValues[r.Key][0]
 				s = s[:r.Start+offSet] + value + s[r.End+offSet:]
 				offSet = int64(len(value)+int(offSet)) - (r.End - r.Start)
 			}
@@ -206,4 +229,74 @@ func Render(qt peakq.QueryTemplateSpec, selectedValues map[string]string) (*peak
 	return &peakq.RenderedQuery{
 		Targets: targets,
 	}, nil
+
+	/*
+		fmt.Printf("TODO, replace: %v\n", selectedValues)
+		return &peakq.RenderedQuery{
+			Targets: qt.Targets,
+		}, nil
+
+
+			// Note: The following is super stupid, will only work with one var, no sanity checking etc
+			// selectedValues is for GET
+			targets := qt.DeepCopy().Targets
+
+			rawTargetObjects := make([]*ajson.Node, len(qt.Targets))
+			for i, t := range qt.Targets {
+				b, err := t.Properties.MarshalJSON()
+				if err != nil {
+					return nil, err
+				}
+				rawTargetObjects[i], err = ajson.Unmarshal(b)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			rm := getReplacementMap(qt)
+			for targetIdx, byTargetIdx := range rm {
+				for path, reps := range byTargetIdx {
+					o := rawTargetObjects[targetIdx]
+					nodes, err := o.JSONPath(string(path))
+					if err != nil {
+						return nil, err
+					}
+					if len(nodes) != 1 {
+						return nil, fmt.Errorf("expected one lead node at path %v but got %v", path, len(nodes))
+					}
+					n := nodes[0]
+					if !n.IsString() {
+						return nil, fmt.Errorf("only string type leaf notes supported currently, %v is not a string", path)
+					}
+					s := n.String()
+					s = s[1 : len(s)-1]
+					var offSet int64
+					for _, r := range reps {
+						// I think breaks with utf...something...?
+						// TODO: Probably simpler to store the non-template parts and insert the values into that, then don't have to track
+						// offsets
+						value := selectedValues[r.Key]
+						s = s[:r.Start+offSet] + value + s[r.End+offSet:]
+						offSet = int64(len(value)+int(offSet)) - (r.End - r.Start)
+					}
+					n.SetString(s)
+				}
+			}
+
+			for i, aT := range rawTargetObjects {
+				raw, err := ajson.Marshal(aT)
+				if err != nil {
+					return nil, err
+				}
+				u := v0alpha1.Unstructured{}
+				err = u.UnmarshalJSON(raw)
+				if err != nil {
+					return nil, err
+				}
+				targets[i].Properties = u
+			}
+
+			return &peakq.RenderedQuery{
+				Targets: targets,
+			}, nil */
 }
--- pkg/registry/apis/peakq/render_test.go
+++ pkg/registry/apis/peakq/render_test.go
@@ -4,29 +4,35 @@ import (
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/stretchr/testify/require"
+
 	common "github.com/grafana/grafana/pkg/apis/common/v0alpha1"
 	peakq "github.com/grafana/grafana/pkg/apis/peakq/v0alpha1"
-	"github.com/stretchr/testify/require"
 )
 
 var nestedFieldRender = peakq.QueryTemplateSpec{
 	Title: "Test",
-	Variables: []peakq.QueryVariable{
+	Variables: []peakq.TemplateVariable{
 		{
 			Key: "metricName",
-			Positions: []peakq.Position{
-				{
-					Path:  "$.nestedObject.anArray[0]",
-					Start: 0,
-					End:   3,
-				},
-			},
 		},
 	},
 	Targets: []peakq.Target{
 		{
 			DataType: data.FrameTypeUnknown,
 			//DataTypeVersion: data.FrameTypeVersion{0, 0},
+
+			Variables: map[string][]peakq.VariableReplacement{
+				"metricName": {
+					{
+						Path: "$.nestedObject.anArray[0]",
+						Position: &peakq.Position{
+							Start: 0,
+							End:   3,
+						},
+					},
+				},
+			},
 			Properties: common.Unstructured{
 				Object: map[string]any{
 					"nestedObject": map[string]any{
@@ -41,6 +47,17 @@ var nestedFieldRender = peakq.QueryTemplateSpec{
 var nestedFieldRenderedTargets = []peakq.Target{
 	{
 		DataType: data.FrameTypeUnknown,
+		Variables: map[string][]peakq.VariableReplacement{
+			"metricName": {
+				{
+					Path: "$.nestedObject.anArray[0]",
+					Position: &peakq.Position{
+						Start: 0,
+						End:   3,
+					},
+				},
+			},
+		},
 		//DataTypeVersion: data.FrameTypeVersion{0, 0},
 		Properties: common.Unstructured{
 			Object: map[string]any{
@@ -53,7 +70,7 @@ var nestedFieldRenderedTargets = []peakq.Target{
 }
 
 func TestNestedFieldRender(t *testing.T) {
-	rT, err := Render(nestedFieldRender, map[string]string{"metricName": "up"})
+	rT, err := Render(nestedFieldRender, map[string][]string{"metricName": {"up"}})
 	require.NoError(t, err)
 	require.Equal(t,
 		nestedFieldRenderedTargets,
@@ -63,37 +80,47 @@ func TestNestedFieldRender(t *testing.T) {
 
 var multiVarTemplate = peakq.QueryTemplateSpec{
 	Title: "Test",
-	Variables: []peakq.QueryVariable{
+	Variables: []peakq.TemplateVariable{
 		{
 			Key: "metricName",
-			Positions: []peakq.Position{
-				{
-					Path:  "$.expr",
-					Start: 4,
-					End:   14,
-				},
-				{
-					Path:  "$.expr",
-					Start: 37,
-					End:   47,
-				},
-			},
 		},
 		{
 			Key: "anotherMetric",
-			Positions: []peakq.Position{
-				{
-					Path:  "$.expr",
-					Start: 21,
-					End:   34,
-				},
-			},
 		},
 	},
 	Targets: []peakq.Target{
 		{
 			DataType: data.FrameTypeUnknown,
 			//DataTypeVersion: data.FrameTypeVersion{0, 0},
+
+			Variables: map[string][]peakq.VariableReplacement{
+				"metricName": {
+					{
+						Path: "$.expr",
+						Position: &peakq.Position{
+							Start: 4,
+							End:   14,
+						},
+					},
+					{
+						Path: "$.expr",
+						Position: &peakq.Position{
+							Start: 37,
+							End:   47,
+						},
+					},
+				},
+				"anotherMetric": {
+					{
+						Path: "$.expr",
+						Position: &peakq.Position{
+							Start: 21,
+							End:   34,
+						},
+					},
+				},
+			},
+
 			Properties: common.Unstructured{
 				Object: map[string]any{
 					"expr": "1 + metricName + 1 + anotherMetric + metricName",
@@ -106,6 +133,33 @@ var multiVarTemplate = peakq.QueryTemplateSpec{
 var multiVarRenderedTargets = []peakq.Target{
 	{
 		DataType: data.FrameTypeUnknown,
+		Variables: map[string][]peakq.VariableReplacement{
+			"metricName": {
+				{
+					Path: "$.expr",
+					Position: &peakq.Position{
+						Start: 4,
+						End:   14,
+					},
+				},
+				{
+					Path: "$.expr",
+					Position: &peakq.Position{
+						Start: 37,
+						End:   47,
+					},
+				},
+			},
+			"anotherMetric": {
+				{
+					Path: "$.expr",
+					Position: &peakq.Position{
+						Start: 21,
+						End:   34,
+					},
+				},
+			},
+		},
 		//DataTypeVersion: data.FrameTypeVersion{0, 0},
 		Properties: common.Unstructured{
 			Object: map[string]any{
@@ -116,7 +170,10 @@ var multiVarRenderedTargets = []peakq.Target{
 }
 
 func TestMultiVarTemplate(t *testing.T) {
-	rT, err := Render(multiVarTemplate, map[string]string{"metricName": "up", "anotherMetric": "sloths_do_like_a_good_nap"})
+	rT, err := Render(multiVarTemplate, map[string][]string{
+		"metricName":    {"up"},
+		"anotherMetric": {"sloths_do_like_a_good_nap"},
+	})
 	require.NoError(t, err)
 	require.Equal(t,
 		multiVarRenderedTargets,
