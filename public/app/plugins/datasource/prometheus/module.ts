5db5579eb8c941006c9ee4bd4bf03dfbb500ef23
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-02-01 22:00:16 -0600

    use imports for core Prometheus

--- public/app/plugins/datasource/prometheus/module.ts
+++ public/app/plugins/datasource/prometheus/module.ts
@@ -1,11 +1,10 @@
 import { DataSourcePlugin } from '@grafana/data';
+import { ConfigEditor, PromCheatSheet, MemoPromQueryEditorByApp as PromQueryEditorByApp } from '@grafana/prometheus';
 
-import PromCheatSheet from './components/PromCheatSheet';
-import PromQueryEditorByApp from './components/PromQueryEditorByApp';
-import { ConfigEditor } from './configuration/ConfigEditor';
 import { PrometheusDatasource } from './datasource';
 
 export const plugin = new DataSourcePlugin(PrometheusDatasource)
+  // @ts-ignore
   .setQueryEditor(PromQueryEditorByApp)
   .setConfigEditor(ConfigEditor)
   .setQueryEditorHelp(PromCheatSheet);
