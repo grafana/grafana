73e60818bc4f6921b3da529da629472ba750d76b
Ryan McKinley <ryantxu@gmail.com>
2024-02-04 21:04:48 -0800

    adding count and access control endpoints

---
+++ pkg/registry/apis/folders/sub_move.go
@@ -0,0 +1,61 @@
+package folders
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/rest"
+
+	"github.com/grafana/grafana/pkg/apis/folder/v0alpha1"
+	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
+	"github.com/grafana/grafana/pkg/services/folder"
+)
+
+type subMoveREST struct {
+	service    folder.Service
+	namespacer request.NamespaceMapper
+}
+
+var _ = rest.Connecter(&subMoveREST{})
+
+func (r *subMoveREST) New() runtime.Object {
+	return &v0alpha1.Folder{}
+}
+
+func (r *subMoveREST) Destroy() {
+}
+
+func (r *subMoveREST) ConnectMethods() []string {
+	return []string{"POST"}
+}
+
+func (r *subMoveREST) NewConnectOptions() (runtime.Object, bool, string) {
+	return nil, false, "" // true means you can use the trailing path as a variable
+}
+
+func (r *subMoveREST) Connect(ctx context.Context, name string, opts runtime.Object, responder rest.Responder) (http.Handler, error) {
+	ns, err := request.NamespaceInfoFrom(ctx, true)
+	if err != nil {
+		return nil, err
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		target := req.FormValue("parent")
+		if target == "" {
+			responder.Error(fmt.Errorf("missing parent parameter"))
+			return
+		}
+
+		f, err := r.service.Move(ctx, &folder.MoveFolderCommand{
+			UID:          name,
+			OrgID:        ns.OrgID,
+			NewParentUID: target,
+		})
+		if err != nil {
+			responder.Error(err)
+		} else {
+			responder.Object(http.StatusOK, convertToK8sResource(f, r.namespacer))
+		}
+	}), nil
+}
