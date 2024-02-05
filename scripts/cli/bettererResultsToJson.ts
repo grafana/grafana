9175611a19e81b2a12c438847fc31537f3f12ad9
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-02-05 13:48:15 +0100

    Add aggregation

--- scripts/cli/bettererResultsToJson.ts
+++ scripts/cli/bettererResultsToJson.ts
@@ -1,12 +1,39 @@
 import { betterer } from '@betterer/betterer';
 import { writeFile } from 'fs/promises';
 
+interface Issue {
+  message: string;
+  count: string;
+}
+
+type ResultMap = Record<string, Record<string, Issue[]>>;
+
+/**
+ *  Produces a JSON file for consumption directly in Grafana
+ */
 async function main() {
   const results = await betterer.results();
-  const resultMap = {};
+  const resultMap: ResultMap = {};
 
-  for (const [suite, issueObj] of Object.entries(results)) {
-    resultMap[suite] = JSON.parse(issueObj.value);
+  for (const suite of results.resultSummaries) {
+    resultMap[suite.name] = {};
+
+    // Aggregate issues for each file in the suite
+    for (const [file, details] of Object.entries(suite.details)) {
+      const fileIssues: Issue[] = [];
+      for (const issue of details) {
+        const issueExists = fileIssues.find((i) => i.message === issue.message)!!;
+        if (issueExists) {
+          continue;
+        }
+        fileIssues.push({
+          message: issue.message,
+          count: details.filter((i) => i.message === issue.message).length,
+        });
+      }
+
+      resultMap[suite.name][file] = fileIssues;
+    }
   }
 
   await writeFile('.betterer.results.json', JSON.stringify(resultMap, undefined, 2));
