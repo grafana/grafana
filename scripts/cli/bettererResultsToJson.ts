e332885cabaaafb0c6395213da7930f3e723c89f
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-02-05 13:04:29 +0100

    Make prettier and add command

--- scripts/cli/bettererResultsToJson.ts
+++ scripts/cli/bettererResultsToJson.ts
@@ -1,41 +1,15 @@
-import fs from 'fs';
+import { betterer } from '@betterer/betterer';
+import { writeFile } from 'fs/promises';
 
-//@ts-ignore It doesn't seem to import correctly, probably because it doesn end in js or ts
-import resultFile from './../../.betterer.results';
+async function main() {
+  const results = await betterer.results();
+  const resultMap = {};
 
-// Format from betterer.results()
-interface BetteterFileIssue {
-  line: number;
-  column: number;
-  length: number;
-  message: string;
-  hash: string;
-}
-
-// JSON output format
-interface BettererFileResult {
-  path: string;
-  issueMessages: string[];
-  issueCount: number[];
-}
-
-type ImportedResult = Record<
-  string,
-  {
-    value: string;
+  for (const [suite, issueObj] of Object.entries(results)) {
+    resultMap[suite] = JSON.parse(issueObj.value);
   }
->;
 
-const resultMap = {};
-const typedResult = resultFile as ImportedResult;
-for (const [suite, issueObj] of Object.entries(typedResult)) {
-  resultMap[suite] = JSON.parse(issueObj.value);
+  await writeFile('.betterer.results.json', JSON.stringify(resultMap, undefined, 2));
 }
 
-fs.writeFile('.betterer.results.json', JSON.stringify(resultMap, undefined, 2), (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('.betterer.results.json written');
-  }
-});
+main().catch(console.error);
