06aa8fe07a57f445192864c71e1fb7b872a50982
Matt Jacobson <matthew.jacobson@grafana.com>
2024-02-07 01:08:38 -0500

    Fix integration tests and public error

--- pkg/services/ngalert/models/errors.go
+++ pkg/services/ngalert/models/errors.go
@@ -5,11 +5,11 @@ import (
 )
 
 var (
-	errAlertRuleConflictMsg  = "conflicting alert rule found [rule_uid: '{{ .Public.RuleUID }}', title: '{{ .Public.Title }}', namespace_uid: '{{ .Public.NamespaceUID }}']: {{ .Error }}"
+	errAlertRuleConflictMsg  = "conflicting alert rule found [rule_uid: '{{ .Public.RuleUID }}', title: '{{ .Public.Title }}', namespace_uid: '{{ .Public.NamespaceUID }}']: {{ .Public.Error }}"
 	ErrAlertRuleConflictBase = errutil.Conflict("alerting.alert-rule.conflict").
 					MustTemplate(errAlertRuleConflictMsg, errutil.WithPublic(errAlertRuleConflictMsg))
 )
 
 func ErrAlertRuleConflict(rule AlertRule, underlying error) error {
-	return ErrAlertRuleConflictBase.Build(errutil.TemplateData{Public: map[string]any{"RuleUID": rule.UID, "Title": rule.Title, "NamespaceUID": rule.NamespaceUID}, Error: underlying})
+	return ErrAlertRuleConflictBase.Build(errutil.TemplateData{Public: map[string]any{"RuleUID": rule.UID, "Title": rule.Title, "NamespaceUID": rule.NamespaceUID, "Error": underlying.Error()}, Error: underlying})
 }
