adc7249660dbf1caf1a1aa83944efdf3f8afbd1b
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-02-07 12:04:36 -0600

    fix package.json for rollup, update licence, keep private

---
+++ packages/grafana-prometheus/rollup.config.ts
@@ -0,0 +1,54 @@
+import image from '@rollup/plugin-image';
+import resolve from '@rollup/plugin-node-resolve';
+import path from 'path';
+import copy from 'rollup-plugin-copy';
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import { externals } from 'rollup-plugin-node-externals';
+
+const icons = require('../../public/app/core/icons/cached.json');
+
+const pkg = require('./package.json');
+
+const iconSrcPaths = icons.map((iconSubPath) => {
+  return `../../public/img/icons/${iconSubPath}.svg`;
+});
+
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [
+      externals({ deps: true, packagePath: './package.json' }),
+      resolve(),
+      copy({
+        targets: [{ src: iconSrcPaths, dest: './dist/public/' }],
+        flatten: false,
+      }),
+      esbuild(),
+      image(),
+    ],
+    output: [
+      {
+        format: 'cjs',
+        sourcemap: true,
+        dir: path.dirname(pkg.publishConfig.main),
+      },
+      {
+        format: 'esm',
+        sourcemap: true,
+        dir: path.dirname(pkg.publishConfig.module),
+        preserveModules: true,
+        // @ts-expect-error (TS cannot assure that `process.env.PROJECT_CWD` is a string)
+        preserveModulesRoot: path.join(process.env.PROJECT_CWD, `packages/grafana-prometheus/src`),
+      },
+    ],
+  },
+  {
+    input: './compiled/index.d.ts',
+    plugins: [dts()],
+    output: {
+      file: pkg.publishConfig.types,
+      format: 'es',
+    },
+  },
+];
