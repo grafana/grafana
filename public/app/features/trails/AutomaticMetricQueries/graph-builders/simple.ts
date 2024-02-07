2fc05e3d06d3b4f4dd792fe0c652a5aa52f2e569
Darren Janeczek <darren.janeczek@grafana.com>
2024-02-07 16:45:02 -0500

    fix: remove unused field

--- public/app/features/trails/AutomaticMetricQueries/graph-builders/simple.ts
+++ public/app/features/trails/AutomaticMetricQueries/graph-builders/simple.ts
@@ -2,7 +2,7 @@ import { PanelBuilders } from '@grafana/scenes';
 
 import { CommonVizParams } from './types';
 
-export function simpleGraphBuilder({ title, unit, axisLabel }: CommonVizParams) {
+export function simpleGraphBuilder({ title, unit }: CommonVizParams) {
   return PanelBuilders.timeseries() //
     .setTitle(title)
     .setUnit(unit)
