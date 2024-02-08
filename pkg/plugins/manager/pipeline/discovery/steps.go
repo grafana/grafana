670716868c04985bcb89b393eba2276bbf04d28a
Will Browne <will.browne@grafana.com>
2024-02-08 15:35:24 +0000

    remove unused step

--- pkg/plugins/manager/pipeline/discovery/steps.go
+++ pkg/plugins/manager/pipeline/discovery/steps.go
@@ -6,54 +6,13 @@ import (
 
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/plugins/config"
-	"github.com/grafana/grafana/pkg/plugins/log"
 	"github.com/grafana/grafana/pkg/plugins/manager/loader/finder"
-	"github.com/grafana/grafana/pkg/plugins/manager/registry"
 )
 
 // DefaultFindFunc is the default function used for the Find step of the Discovery stage. It will scan the local
 // filesystem for plugins.
 func DefaultFindFunc(cfg *config.Cfg) FindFunc {
 	return finder.NewLocalFinder(cfg.DevMode, cfg.Features).Find
-}
-
-// DuplicatePluginVersionValidation is a filter step that will filter out any plugins that are already registered with the
-// registry. This includes both the primary plugin and any child plugins, which are matched using the plugin ID and
-// version fields.
-type DuplicatePluginVersionValidation struct {
-	registry registry.Service
-	log      log.Logger
-}
-
-// NewDuplicatePluginVersionFilterStep returns a new DuplicatePluginVersionValidation.
-func NewDuplicatePluginVersionFilterStep(registry registry.Service) *DuplicatePluginVersionValidation {
-	return &DuplicatePluginVersionValidation{
-		registry: registry,
-		log:      log.New("plugins.dedupe"),
-	}
-}
-
-// Filter will filter out any plugins that are already registered with the registry.
-func (d *DuplicatePluginVersionValidation) Filter(ctx context.Context, bundles []*plugins.FoundBundle) ([]*plugins.FoundBundle, error) {
-	res := make([]*plugins.FoundBundle, 0, len(bundles))
-	for _, b := range bundles {
-		_, exists := d.registry.Plugin(ctx, b.Primary.JSONData.ID, b.Primary.JSONData.Info.Version)
-		if exists {
-			d.log.Warn("Skipping loading of plugin as it's a duplicate", "pluginId", b.Primary.JSONData.ID)
-			continue
-		}
-
-		for _, child := range b.Children {
-			_, exists = d.registry.Plugin(ctx, child.JSONData.ID, b.Primary.JSONData.Info.Version)
-			if exists {
-				d.log.Warn("Skipping loading of child plugin as it's a duplicate", "pluginId", child.JSONData.ID)
-				continue
-			}
-		}
-		res = append(res, b)
-	}
-
-	return res, nil
 }
 
 // PermittedPluginTypesFilter is a filter step that will filter out any plugins that are not of a permitted type.
