c35a403f7428473923d37421234f4868c1fbacf0
jguer <me@jguer.space>
2024-02-08 15:54:48 +0100

    improve test coverage

--- pkg/util/json_test.go
+++ pkg/util/json_test.go
@@ -11,40 +11,57 @@ import (
 func TestSearchJSONForGroups(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
-		Name                 string
-		UserInfoJSONResponse []byte
-		GroupsAttributePath  string
-		ExpectedResult       []string
-		ExpectedError        error
+		Name                string
+		searchObject        any
+		GroupsAttributePath string
+		ExpectedResult      []string
+		ExpectedError       error
 	}{
 		{
-			Name:                 "Given an invalid user info JSON response",
-			UserInfoJSONResponse: []byte("{"),
-			GroupsAttributePath:  "attributes.groups",
-			ExpectedResult:       []string{},
-			ExpectedError:        util.ErrFailedToUnmarshalJSON,
+			Name:                "Given an invalid user info JSON response",
+			searchObject:        []byte("{"),
+			GroupsAttributePath: "attributes.groups",
+			ExpectedResult:      []string{},
+			ExpectedError:       util.ErrFailedToUnmarshalJSON,
 		},
 		{
-			Name:                 "Given an empty user info JSON response and empty JMES path",
-			UserInfoJSONResponse: []byte{},
-			GroupsAttributePath:  "",
-			ExpectedResult:       []string{},
-			ExpectedError:        util.ErrNoAttributePathSpecified,
+			Name:                "Given an empty user info JSON response and empty JMES path",
+			searchObject:        []byte{},
+			GroupsAttributePath: "",
+			ExpectedResult:      []string{},
+			ExpectedError:       util.ErrNoAttributePathSpecified,
 		},
 		{
-			Name:                 "Given an empty user info JSON response and valid JMES path",
-			UserInfoJSONResponse: []byte{},
-			GroupsAttributePath:  "attributes.groups",
-			ExpectedResult:       []string{},
-			ExpectedError:        util.ErrEmptyJSON,
+			Name:                "Given an empty user info JSON response and valid JMES path",
+			searchObject:        []byte{},
+			GroupsAttributePath: "attributes.groups",
+			ExpectedResult:      []string{},
+			ExpectedError:       util.ErrEmptyJSON,
+		},
+		{
+			Name:                "Given a nil JSON and valid JMES path",
+			searchObject:        []byte{},
+			GroupsAttributePath: "attributes.groups",
+			ExpectedResult:      []string{},
+			ExpectedError:       util.ErrEmptyJSON,
 		},
 		{
 			Name: "Given a simple user info JSON response and valid JMES path",
-			UserInfoJSONResponse: []byte(`{
+			searchObject: []byte(`{
 		"attributes": {
 			"groups": ["foo", "bar"]
 		}
 }`),
+			GroupsAttributePath: "attributes.groups[]",
+			ExpectedResult:      []string{"foo", "bar"},
+		},
+		{
+			Name: "Given a simple object and valid JMES path",
+			searchObject: map[string]any{
+				"attributes": map[string]any{
+					"groups": []string{"foo", "bar"},
+				},
+			},
 			GroupsAttributePath: "attributes.groups[]",
 			ExpectedResult:      []string{"foo", "bar"},
 		},
@@ -55,64 +72,7 @@ func TestSearchJSONForGroups(t *testing.T) {
 		t.Run(test.Name, func(t *testing.T) {
 			t.Parallel()
 			actualResult, err := util.SearchJSONForStringSliceAttr(
-				test.GroupsAttributePath, test.UserInfoJSONResponse)
-			if test.ExpectedError == nil {
-				require.NoError(t, err)
-			} else {
-				require.ErrorIs(t, err, test.ExpectedError)
-			}
-			require.Equal(t, test.ExpectedResult, actualResult)
-		})
-	}
-}
-
-func TestSearchJSONForRole(t *testing.T) {
-	t.Parallel()
-	tests := []struct {
-		Name                 string
-		UserInfoJSONResponse []byte
-		RoleAttributePath    string
-		ExpectedResult       string
-		ExpectedError        error
-	}{
-		{
-			Name:                 "Given an invalid user info JSON response",
-			UserInfoJSONResponse: []byte("{"),
-			RoleAttributePath:    "attributes.role",
-			ExpectedResult:       "",
-			ExpectedError:        util.ErrFailedToUnmarshalJSON,
-		},
-		{
-			Name:                 "Given an empty user info JSON response and empty JMES path",
-			UserInfoJSONResponse: []byte{},
-			RoleAttributePath:    "",
-			ExpectedResult:       "",
-			ExpectedError:        util.ErrNoAttributePathSpecified,
-		},
-		{
-			Name:                 "Given an empty user info JSON response and valid JMES path",
-			UserInfoJSONResponse: []byte{},
-			RoleAttributePath:    "attributes.role",
-			ExpectedResult:       "",
-			ExpectedError:        util.ErrEmptyJSON,
-		},
-		{
-			Name: "Given a simple user info JSON response and valid JMES path",
-			UserInfoJSONResponse: []byte(`{
-	"attributes": {
-		"role": "admin"
-	}
-}`),
-			RoleAttributePath: "attributes.role",
-			ExpectedResult:    "admin",
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.Name, func(t *testing.T) {
-			t.Parallel()
-			actualResult, err := util.SearchJSONForStringAttr(test.RoleAttributePath, test.UserInfoJSONResponse)
+				test.GroupsAttributePath, test.searchObject)
 			if test.ExpectedError == nil {
 				require.NoError(t, err)
 			} else {
@@ -127,7 +87,7 @@ func TestSearchJSONForEmail(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
 		Name                 string
-		UserInfoJSONResponse []byte
+		UserInfoJSONResponse any
 		EmailAttributePath   string
 		ExpectedResult       string
 		ExpectedError        error
@@ -139,6 +99,16 @@ func TestSearchJSONForEmail(t *testing.T) {
 		"email": "grafana@localhost"
 	}
 }`),
+			EmailAttributePath: "attributes.email",
+			ExpectedResult:     "grafana@localhost",
+		},
+		{
+			Name: "Given a simple object and valid JMES path",
+			UserInfoJSONResponse: map[string]any{
+				"attributes": map[string]any{
+					"email": "grafana@localhost",
+				},
+			},
 			EmailAttributePath: "attributes.email",
 			ExpectedResult:     "grafana@localhost",
 		},
