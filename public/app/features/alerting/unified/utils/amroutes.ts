be79a513839c5dbab40490bfd5801d9811cad37f
Sonia Aguilar <33540275+soniaAguilarPeiron@users.noreply.github.com>
2024-02-12 12:45:29 +0100

    Alerting: Fix folder and rule name in groupBy for simplified routing (#82148)
    * Fix folder and rule name in groupBy for simplified routing
    
    * Review suggestions
    
    * Use proper type for MultiValueRemove props
    
    * hoist getting groupBy count
    
    * Use watch instead of getValues
    
    * Bring back validation for required fields

--- public/app/features/alerting/unified/utils/amroutes.ts
+++ public/app/features/alerting/unified/utils/amroutes.ts
@@ -44,8 +44,8 @@ export const defaultGroupBy = ['grafana_folder', 'alertname'];
 
 // Common route group_by options for multiselect drop-down
 export const commonGroupByOptions = [
-  { label: 'grafana_folder', value: 'grafana_folder' },
-  { label: 'alertname', value: 'alertname' },
+  { label: 'grafana_folder', value: 'grafana_folder', isFixed: true },
+  { label: 'alertname', value: 'alertname', isFixed: true },
   { label: 'Disable (...)', value: '...' },
 ];
 
