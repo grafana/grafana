5420fbe5edf8afa69029ce23c05af60876f29b0e
Sergey Kostrukov <sekost@microsoft.com>
2024-02-13 00:33:08 -0800

    Cleanup

--- public/app/plugins/datasource/prometheus/configuration/AzureCredentials.ts
+++ public/app/plugins/datasource/prometheus/configuration/AzureCredentials.ts
@@ -1,4 +1,3 @@
-import { AzureCredentials } from '@grafana/azure-sdk';
 import { SelectableValue } from '@grafana/data';
 
 export enum AzureCloud {
@@ -13,15 +12,3 @@ export const KnownAzureClouds: Array<SelectableValue<AzureCloud>> = [
   { value: AzureCloud.China, label: 'Azure China' },
   { value: AzureCloud.USGovernment, label: 'Azure US Government' },
 ];
-
-export function isCredentialsComplete(credentials: AzureCredentials): boolean {
-  switch (credentials.authType) {
-    case 'currentuser':
-    case 'msi':
-    case 'workloadidentity':
-      return true;
-    case 'clientsecret':
-    case 'clientsecret-obo':
-      return !!(credentials.azureCloud && credentials.tenantId && credentials.clientId && credentials.clientSecret);
-  }
-}
