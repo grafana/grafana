64d00d0680016589e441dc1a93bc1851768b619b
Ihor Yeromin <yeryomin.igor@gmail.com>
2024-02-13 14:21:36 +0200

    Table: Update page index on data update (#81574)
    * fix(table): page index reset on data update

--- packages/grafana-ui/src/components/Table/Table.tsx
+++ packages/grafana-ui/src/components/Table/Table.tsx
@@ -237,6 +237,17 @@ export const Table = memo((props: Props) => {
     setPageSize(pageSize);
   }, [pageSize, setPageSize]);
 
+  useEffect(() => {
+    // Reset page index when data changes
+    // This is needed because react-table does not do this automatically
+    // autoResetPage is set to false because setting it to true causes the issue described in
+    // https://github.com/grafana/grafana/pull/67477
+    if (data.length / pageSize < state.pageIndex) {
+      gotoPage(0);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
   useResetVariableListSizeCache(extendedState, listRef, data, hasUniqueId);
   useFixScrollbarContainer(variableSizeListScrollbarRef, tableDivRef);
 
