7bf12521c8ccd207bc8b1dd90082deadbee97d74
nmarrs <nathanielmarrs@gmail.com>
2024-02-13 10:18:54 -0800

    add handling for manual mode migration of x / y fields

--- public/app/plugins/panel/xychart/migrations.ts
+++ public/app/plugins/panel/xychart/migrations.ts
@@ -1,4 +1,4 @@
-import { PanelModel } from '@grafana/data';
+import { FieldMatcherID, PanelModel } from '@grafana/data';
 
 import { Options } from './panelcfg.gen';
 
@@ -9,6 +9,36 @@ export const xyChartMigrationHandler = (panel: PanelModel): Partial<Options> => 
   // Initial plugin version is empty string for first migration
   if (pluginVersion === '') {
     console.log('Migration: xyChartMigrationHandler', panel);
+
+    for (const series of panel.options.series) {
+      // Update x / y to new format using field matchers when in manual mode
+      if (panel.options.seriesMapping === 'manual') {
+        const xField = series.x;
+        const yField = series.y;
+
+        series.x = {
+          field: {
+            matcher: {
+              id: FieldMatcherID.byName,
+              options: xField,
+            },
+          },
+        };
+
+        series.y = {
+          field: {
+            matcher: {
+              id: FieldMatcherID.byName,
+              options: yField,
+            },
+          },
+        };
+      }
+    }
+
+    // Add case for color / size
+
+    // TODO: case for auto mode (table / dynamic rolled into one)
   }
 
   return panel.options;
