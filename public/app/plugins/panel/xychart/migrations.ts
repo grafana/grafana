a54fe54284c36a61abf0c42479d612e615cba272
Leon Sorokin <leeoniya@gmail.com>
2024-02-14 01:41:51 -0600

    poke migrations

--- public/app/plugins/panel/xychart/migrations.ts
+++ public/app/plugins/panel/xychart/migrations.ts
@@ -1,97 +1,131 @@
-import { FieldMatcherID, FrameMatcherID, PanelModel } from '@grafana/data';
+import { FieldMatcherID, FrameMatcherID, MatcherConfig, PanelModel } from '@grafana/data';
 
-import { Options } from './panelcfg.gen';
-import { SeriesMapping2 } from './types2';
+import { Options, SeriesMapping } from './panelcfg.gen';
+import { XYSeriesConfig, Options as Options2, SeriesMapping2 } from './types2';
 
-export const xyChartMigrationHandler = (panel: PanelModel): Partial<Options> => {
+export const xyChartMigrationHandler = (panel: PanelModel<Options>): Options | Options2 => {
   const pluginVersion = panel?.pluginVersion ?? '';
 
   // Update to new format for GA 10.4 release
   // Initial plugin version is empty string for first migration
   if (pluginVersion === '') {
-    console.log('Migration: xyChartMigrationHandler', { ...panel.options });
-    for (const series of panel.options.series) {
-      // Update x / y to new format using field matchers when in manual mode
-      const xField = series.x;
-      const yField = series.y;
-      const excludeYFields = panel.options?.dims?.exclude;
-      const seriesSizeFixed = series?.size?.fixed;
-      const seriesSizeField = series?.size?.field;
-      const seriesColorFixed = series?.color?.fixed;
-      const seriesColorField = series?.color?.field;
+    const { dims, seriesMapping, series: oldSeries, ...cleanedOpts } = panel.options;
+    const { exclude, frame, x: xShared } = dims ?? {};
 
-      series.x = {
-        field: {
-          matcher: {
-            id: FieldMatcherID.byName,
-            options: xField,
-          },
+    let oldSeries2 = oldSeries;
+
+    if (seriesMapping === SeriesMapping.Auto) {
+      oldSeries2 = [
+        {
+          x: undefined,
+          y: undefined,
         },
-      };
-
-      series.y = {
-        field: {
-          matcher: {
-            id: FieldMatcherID.byName,
-            options: yField,
-          },
-          ...(excludeYFields && {
-            exclude: {
-              id: FieldMatcherID.byNames,
-              options: excludeYFields,
-            },
-          }),
-        },
-      };
-
-      if (series.size) {
-        series.size = {
-          ...(seriesSizeFixed && {
-            fixed: seriesSizeFixed,
-          }),
-          ...(seriesSizeField && {
-            field: {
-              matcher: {
-                id: FieldMatcherID.byName,
-                options: seriesSizeField,
-              },
-            },
-          }),
-        };
-      }
-
-      if (series.color) {
-        series.color = {
-          ...(seriesColorFixed && {
-            fixed: seriesColorFixed,
-          }),
-          ...(seriesColorField && {
-            field: {
-              matcher: {
-                id: FieldMatcherID.byName,
-                options: seriesColorField,
-              },
-            },
-          }),
-        };
-      }
-
-      // Frame is required for manual mode
-      if (panel.options.seriesMapping === SeriesMapping2.Manual) {
-        series.frame = {
-          id: FrameMatcherID.byIndex,
-          options: 0,
-        };
-      }
+      ];
     }
 
-    // Handle mapping option cleanup
-    panel.options.mapping = `${panel.options.seriesMapping}`;
-    delete panel.options.seriesMapping;
-  }
+    const newSeries: XYSeriesConfig[] = oldSeries2.map(({ x, y, pointColor, pointSize }) => {
+      const { fixed: colorFixed, field: colorField } = pointColor ?? {};
+      const { fixed: sizeFixed, field: sizeField, min: sizeMin, max: sizeMax } = pointSize ?? {};
 
-  // TODO: Add migration tests
-  console.log('Migration: xyChartMigrationHandler RESULT', panel.options);
+      let xMatcherConfig: MatcherConfig;
+      let yMatcherConfig: MatcherConfig;
+
+      // old auto mode did not require x field defined
+      if (x == null && xShared == null) {
+        xMatcherConfig = {
+          id: FieldMatcherID.byType,
+          options: 'number',
+        };
+      } else {
+        xMatcherConfig = {
+          id: FieldMatcherID.byName,
+          options: x ?? xShared,
+        };
+      }
+
+      if (y == null) {
+        yMatcherConfig = {
+          id: FieldMatcherID.byType,
+          options: 'number',
+        };
+      } else {
+        yMatcherConfig = {
+          id: FieldMatcherID.byName,
+          options: y,
+        };
+      }
+
+      return {
+        frame: {
+          id: FrameMatcherID.byIndex,
+          options: seriesMapping === SeriesMapping.Manual ? 0 : frame ?? 0,
+        },
+        x: {
+          field: {
+            matcher: xMatcherConfig,
+          },
+        },
+        y: {
+          field: {
+            matcher: yMatcherConfig,
+            ...(exclude &&
+              exclude.length && {
+                exclude: {
+                  id: FieldMatcherID.byNames,
+                  options: exclude,
+                },
+              }),
+          },
+        },
+        ...((colorFixed || colorField) && {
+          color: {
+            ...(colorFixed && {
+              fixed: {
+                value: colorFixed,
+              },
+            }),
+            ...(colorField && {
+              field: {
+                matcher: {
+                  id: FieldMatcherID.byName,
+                  options: colorField,
+                },
+              },
+            }),
+          },
+        }),
+        ...((sizeFixed || sizeField) && {
+          size: {
+            ...(sizeFixed && {
+              fixed: {
+                value: sizeFixed,
+              },
+            }),
+            ...(sizeField && {
+              field: {
+                matcher: {
+                  id: FieldMatcherID.byName,
+                  options: sizeField,
+                },
+                min: sizeMin,
+                max: sizeMax,
+              },
+            }),
+          },
+        }),
+      };
+    });
+
+    const newOptions: Options2 = {
+      ...cleanedOpts,
+      mapping: seriesMapping === SeriesMapping.Auto ? SeriesMapping2.Auto : SeriesMapping2.Manual,
+      series: newSeries,
+    };
+
+    // console.log('xyChartMigrationHandler', panel.options, newOptions);
+
+    return newOptions;
+  }
 
   return panel.options;
 };
