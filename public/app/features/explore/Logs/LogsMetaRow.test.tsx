1645ae7f68aa8a1462993ed67ac0f2de18fc335b
Galen <galen.kistler@grafana.com>
2024-02-15 09:03:23 -0600

    optimize imports

--- public/app/features/explore/Logs/LogsMetaRow.test.tsx
+++ public/app/features/explore/Logs/LogsMetaRow.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import saveAs from 'file-saver';
 import React, { ComponentProps } from 'react';
