78b07e74ba9ae05fa0d14e21c2efc45331a0a40c
Clarity-89 <homes89@ukr.net>
2024-02-15 13:55:43 +0200

    Card: Generate docs from the story

--- packages/grafana-ui/.storybook/preview.ts
+++ packages/grafana-ui/.storybook/preview.ts
@@ -50,6 +50,7 @@ const preview: Preview = {
       // We should be able to use the builtin alphabetical sort, but is broken in SB 7.0
       // https://github.com/storybookjs/storybook/issues/22470
       storySort: (a, b) => {
+        console.log('sortt', a, b);
         if (a.title.startsWith('Docs Overview')) {
           if (b.title.startsWith('Docs Overview')) {
             return 0;
@@ -58,7 +59,7 @@ const preview: Preview = {
         } else if (b.title.startsWith('Docs Overview')) {
           return 1;
         }
-        return a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true });
+        return 1;
       },
     },
   },
