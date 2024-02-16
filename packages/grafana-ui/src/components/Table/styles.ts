96c0bf7f01083fa06c9e3b6415145ed7009908af
Alvaro Huarte <ahuarte47@yahoo.es>
2024-02-16 02:16:44 +0100

    Fix text-wrapping of cells in Tables

--- packages/grafana-ui/src/components/Table/styles.ts
+++ packages/grafana-ui/src/components/Table/styles.ts
@@ -48,7 +48,10 @@ export function useTableStyles(theme: GrafanaTheme2, cellHeightOption: TableCell
 
       '&:hover': {
         overflow: overflowOnHover ? 'visible' : undefined,
-        width: overflowOnHover ? 'auto !important' : undefined,
+        width: overflowOnHover ? 'auto' : undefined,
+        height: overflowOnHover ? 'auto' : `${rowHeight - 1}px`,
+        minHeight: `${rowHeight - 1}px`,
+        whiteSpace: overflowOnHover ? 'normal' : 'nowrap',
         boxShadow: overflowOnHover ? `0 0 2px ${theme.colors.primary.main}` : undefined,
         background: overflowOnHover ? background ?? theme.components.table.rowHoverBackground : undefined,
         zIndex: overflowOnHover ? 1 : undefined,
