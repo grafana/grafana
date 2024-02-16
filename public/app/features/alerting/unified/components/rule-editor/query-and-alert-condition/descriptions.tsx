66fd979790cc123f3f47b47bec5c85dc1c71b2ac
Gilles De Mey <gilles.de.mey@gmail.com>
2024-02-16 17:13:43 +0100

    Alerting: Updates to recording rules (#82329)

---
+++ public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/descriptions.tsx
@@ -0,0 +1,32 @@
+import { RuleFormType } from '../../../types/rule-form';
+
+type FormDescriptions = {
+  sectionTitle: string;
+  helpLabel: string;
+  helpContent: string;
+  helpLink: string;
+};
+
+export const DESCRIPTIONS: Record<RuleFormType, FormDescriptions> = {
+  [RuleFormType.cloudRecording]: {
+    sectionTitle: 'Define recording rule',
+    helpLabel: 'Define your recording rule',
+    helpContent:
+      'Pre-compute frequently needed or computationally expensive expressions and save their result as a new set of time series.',
+    helpLink: '',
+  },
+  [RuleFormType.grafana]: {
+    sectionTitle: 'Define query and alert condition',
+    helpLabel: 'Define query and alert condition',
+    helpContent:
+      'An alert rule consists of one or more queries and expressions that select the data you want to measure. Define queries and/or expressions and then choose one of them as the alert rule condition. This is the threshold that an alert rule must meet or exceed in order to fire. For more information on queries and expressions, see Query and transform data.',
+    helpLink: 'https://grafana.com/docs/grafana/latest/panels-visualizations/query-transform-data/',
+  },
+  [RuleFormType.cloudAlerting]: {
+    sectionTitle: 'Define query and alert condition',
+    helpLabel: 'Define query and alert condition',
+    helpContent:
+      'An alert rule consists of one or more queries and expressions that select the data you want to measure. Define queries and/or expressions and then choose one of them as the alert rule condition. This is the threshold that an alert rule must meet or exceed in order to fire. For more information on queries and expressions, see Query and transform data.',
+    helpLink: 'https://grafana.com/docs/grafana/latest/panels-visualizations/query-transform-data/',
+  },
+};
