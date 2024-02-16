4b20e5fd399af68ad8cffa5d2f48f6de9cfe59f2
Leon Sorokin <leeoniya@gmail.com>
2024-02-15 23:25:38 -0600

    migrations ftw

--- public/app/plugins/panel/xychart/migrations.ts
+++ public/app/plugins/panel/xychart/migrations.ts
@@ -1,6 +1,6 @@
 import { FieldMatcherID, FrameMatcherID, MatcherConfig, PanelModel } from '@grafana/data';
 
-import { ScatterSeriesConfig, SeriesMapping } from './panelcfg.gen';
+import { ScatterSeriesConfig, SeriesMapping, XYDimensionConfig } from './panelcfg.gen';
 import { XYSeriesConfig, Options2 as Options2 } from './types2';
 
 export const xyChartMigrationHandler = (panel: PanelModel): Options2 => {
@@ -10,7 +10,7 @@ export const xyChartMigrationHandler = (panel: PanelModel): Options2 => {
   // Initial plugin version is empty string for first migration
   if (pluginVersion === '') {
     const { dims, seriesMapping, series: oldSeries, ...cleanedOpts } = panel.options;
-    const { exclude = [], frame, x: xShared } = dims ?? {};
+    const { exclude = [], frame: frameShared, x: xShared }: XYDimensionConfig = dims ?? {};
 
     let oldSeries2 = oldSeries;
 
@@ -23,129 +23,201 @@ export const xyChartMigrationHandler = (panel: PanelModel): Options2 => {
       ];
     }
 
-    const newSeries: XYSeriesConfig[] = oldSeries2.map(({ x, y, pointColor, pointSize }: ScatterSeriesConfig) => {
-      const { fixed: colorFixed, field: colorField } = pointColor ?? {};
-      const { fixed: sizeFixed, field: sizeField, min: sizeMin, max: sizeMax } = pointSize ?? {};
-
-      let xMatcherConfig: MatcherConfig;
-      let yMatcherConfig: MatcherConfig;
-
-      // old auto mode did not require x field defined
-      if (x == null && xShared == null) {
-        // TODO: this should just be the internal default. no need to store on save model
-        xMatcherConfig = {
-          id: FieldMatcherID.byType,
-          options: 'number',
-        };
-      } else {
-        xMatcherConfig = {
-          id: FieldMatcherID.byName,
-          options: x ?? xShared,
-        };
+    /*
+    // old manual mode example
+    "series": [
+      {
+        "pointColor": {
+          "fixed": "purple"       // this becomes override for y field.config.custom.pointColor.fixed, (or config.color?)
+          "field": "BMI Male",    // ...unless another field is mapped, then ignore
+        },
+        "pointSize": {
+          "field": "Weight Male",
+          "max": 40,              // this becomes override for y field.config.custom.pointSize.max
+          "min": 1,               // ...and .min
+          "fixed": 50.5
+        },
+        "frame": 0,               // byIndex frame matcher
+        "x": "Height Male",       // byName field matcher, falls back to byType/number field matcher
+        "y": "Weight Male"        // byName field matcher, falls back to byType/number field matcher
       }
+    ],
+*/
+    let i = 0;
 
-      if (y == null) {
-        // TODO: this should just be the internal default. no need to store on save model
-        yMatcherConfig = {
-          id: FieldMatcherID.byType,
-          options: 'number',
-        };
-      } else {
-        yMatcherConfig = {
-          id: FieldMatcherID.byName,
-          options: y,
-        };
-      }
+    const newSeries: XYSeriesConfig[] = oldSeries2.map(
+      ({ x, y, pointColor, pointSize, frame }: ScatterSeriesConfig) => {
+        const { fixed: colorFixed, field: colorField } = pointColor ?? {};
+        const { fixed: sizeFixed, field: sizeField, min: sizeMin, max: sizeMax } = pointSize ?? {};
 
-      if (colorField == null && colorFixed) {
-        // if same as defaults, skip
-        if (panel.fieldConfig.defaults.custom.pointColor === colorFixed) {
+        let xMatcherConfig: MatcherConfig;
+        let yMatcherConfig: MatcherConfig;
+
+        // old auto mode did not require x field defined
+        if (x == null && xShared == null) {
+          // TODO: this should just be the internal default. no need to store on save model
+          xMatcherConfig = {
+            id: FieldMatcherID.byType,
+            options: 'number',
+          };
+        } else {
+          xMatcherConfig = {
+            id: FieldMatcherID.byName,
+            options: x ?? xShared,
+          };
         }
 
-        let hasOverride = panel.fieldConfig.overrides.some(
-          (o) =>
-            o.matcher.id === yMatcherConfig.id &&
-            o.matcher.options === yMatcherConfig.options &&
-            o.properties.some((p) => p.id === 'custom.pointColor')
-        );
-
-        if (!hasOverride) {
-          panel.fieldConfig.overrides.push({
-            matcher: yMatcherConfig,
-            properties: [
-              {
-                id: 'custom.pointColor',
-                value: colorFixed,
-              },
-            ],
-          });
-        }
-      }
-
-      // add field overrides for custom size
-      if (sizeField == null && sizeFixed) {
-        // if same as defaults, skip
-        if (panel.fieldConfig.defaults.custom.pointSize === sizeFixed) {
+        if (y == null) {
+          // TODO: this should just be the internal default. no need to store on save model
+          yMatcherConfig = {
+            id: FieldMatcherID.byType,
+            options: 'number',
+          };
+        } else {
+          yMatcherConfig = {
+            id: FieldMatcherID.byName,
+            options: y,
+          };
         }
 
-        let hasOverride = panel.fieldConfig.overrides.some(
-          (o) =>
-            o.matcher.id === yMatcherConfig.id &&
-            o.matcher.options === yMatcherConfig.options &&
-            o.properties.some((p) => p.id === 'custom.pointSize')
-        );
+        let custDefaults = panel.fieldConfig.defaults.custom;
 
-        // if override already exists, ignore
-        if (!hasOverride) {
-          panel.fieldConfig.overrides.push({
-            matcher: yMatcherConfig,
-            properties: [
-              {
-                id: 'custom.pointSize',
-                value: sizeFixed,
-              },
-            ],
-          });
+        if (colorField == null && colorFixed && custDefaults.pointColor?.fixed !== colorFixed) {
+          // NOTE: intentionally not using custom.pointColor.fixed
+          let hasOverride = panel.fieldConfig.overrides.some(
+            (o) =>
+              o.matcher.id === yMatcherConfig.id &&
+              o.matcher.options === yMatcherConfig.options &&
+              o.properties.some((p) => p.id === 'color')
+          );
+
+          if (!hasOverride) {
+            panel.fieldConfig.overrides.push({
+              matcher: yMatcherConfig,
+              properties: [
+                {
+                  id: 'color',
+                  value: {
+                    mode: 'fixed',
+                    fixedColor: colorFixed,
+                  },
+                },
+              ],
+            });
+          }
         }
-      }
 
-      return {
-        frame: {
-          matcher: {
-            id: FrameMatcherID.byIndex,
-            options: seriesMapping === SeriesMapping.Manual ? 0 : frame ?? 0,
+        // add field overrides for custom pointSize.fixed
+        if (sizeField == null && sizeFixed && custDefaults.pointSize?.fixed !== sizeFixed) {
+          let hasOverride = panel.fieldConfig.overrides.some(
+            (o) =>
+              o.matcher.id === yMatcherConfig.id &&
+              o.matcher.options === yMatcherConfig.options &&
+              o.properties.some((p) => p.id === 'custom.pointSize.fixed')
+          );
+
+          if (!hasOverride) {
+            panel.fieldConfig.overrides.push({
+              matcher: yMatcherConfig,
+              properties: [
+                {
+                  id: 'custom.pointSize.fixed',
+                  value: sizeFixed,
+                },
+              ],
+            });
+          }
+        }
+
+        if (sizeField != null) {
+          // add field overrides for custom pointSize.min
+          if (sizeMin && custDefaults.pointSize?.min !== sizeMin) {
+            let hasOverride = panel.fieldConfig.overrides.some(
+              (o) =>
+                o.matcher.id === yMatcherConfig.id &&
+                o.matcher.options === yMatcherConfig.options &&
+                o.properties.some((p) => p.id === 'custom.pointSize.min')
+            );
+
+            if (!hasOverride) {
+              panel.fieldConfig.overrides.push({
+                matcher: {
+                  id: FieldMatcherID.byName,
+                  options: sizeField,
+                },
+                properties: [
+                  {
+                    id: 'custom.pointSize.min',
+                    value: sizeMin,
+                  },
+                ],
+              });
+            }
+          }
+          // add field overrides for custom pointSize.min
+          if (sizeMax && custDefaults.pointSize?.max !== sizeMax) {
+            let hasOverride = panel.fieldConfig.overrides.some(
+              (o) =>
+                o.matcher.id === yMatcherConfig.id &&
+                o.matcher.options === yMatcherConfig.options &&
+                o.properties.some((p) => p.id === 'custom.pointSize.max')
+            );
+
+            if (!hasOverride) {
+              panel.fieldConfig.overrides.push({
+                matcher: {
+                  id: FieldMatcherID.byName,
+                  options: sizeField,
+                },
+                properties: [
+                  {
+                    id: 'custom.pointSize.max',
+                    value: sizeMax,
+                  },
+                ],
+              });
+            }
+          }
+        }
+
+        return {
+          frame: {
+            matcher: {
+              id: FrameMatcherID.byIndex,
+              options: seriesMapping === SeriesMapping.Manual ? i++ : frame ?? frameShared ?? 0,
+            },
           },
-        },
-        x: {
-          matcher: xMatcherConfig,
-        },
-        y: {
-          matcher: yMatcherConfig,
-          ...(exclude.length && {
-            exclude: {
-              id: FieldMatcherID.byNames,
-              options: exclude,
+          x: {
+            matcher: xMatcherConfig,
+          },
+          y: {
+            matcher: yMatcherConfig,
+            ...(exclude.length && {
+              exclude: {
+                id: FieldMatcherID.byNames,
+                options: exclude,
+              },
+            }),
+          },
+          ...(colorField && {
+            color: {
+              matcher: {
+                id: FieldMatcherID.byName,
+                options: colorField,
+              },
             },
           }),
-        },
-        ...(colorField && {
-          color: {
-            matcher: {
-              id: FieldMatcherID.byName,
-              options: colorField,
+          ...(sizeField && {
+            size: {
+              matcher: {
+                id: FieldMatcherID.byName,
+                options: sizeField,
+              },
             },
-          },
-        }),
-        ...(sizeField && {
-          size: {
-            matcher: {
-              id: FieldMatcherID.byName,
-              options: sizeField,
-            },
-          },
-        }),
-      };
-    });
+          }),
+        };
+      }
+    );
 
     const newOptions: Options2 = {
       ...cleanedOpts,
