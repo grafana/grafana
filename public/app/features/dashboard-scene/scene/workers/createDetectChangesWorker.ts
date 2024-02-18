38ab5bf4aabd8d359b7cdf84276b5a505259f3f1
Ivan Ortega <ivanortegaalba@gmail.com>
2024-02-18 21:50:40 +0100

    Use commonJS to not break when getting file url

--- public/app/features/dashboard-scene/scene/workers/createDetectChangesWorker.ts
+++ public/app/features/dashboard-scene/scene/workers/createDetectChangesWorker.ts
@@ -1,1 +1,2 @@
-export const createWorker = () => new Worker(new URL('./DetectChangesWorker.ts', import.meta.url));
+export const createWorker = () =>
+  new Worker(new URL('./DetectChangesWorker.ts', require('url').pathToFileURL(__filename).toString()));
