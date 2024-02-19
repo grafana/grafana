0dda320457d19a6928b088b766bcfd5809ca617f
Erik Sundell <erik.sundell87@gmail.com>
2024-02-19 07:51:32 +0100

    fix path to readme

--- e2e/plugin-e2e/plugin-e2e-api-tests/errors.ts
+++ e2e/plugin-e2e/plugin-e2e-api-tests/errors.ts
@@ -1,4 +1,4 @@
 export const formatExpectError = (message: string) => {
   return `Error while verifying @grafana/plugin-e2e scenarios: ${message}. 
-    See https://github.com/grafana/grafana/blob/main/plugin-e2e/verify-scenario/README.md for more information.`;
+    See https://github.com/grafana/grafana/blob/main/plugin-e2e/plugin-e2e-api-tests/README.md for more information.`;
 };
