209962678705e9f6f78c94d87c52a0463ad0ff99
kay delaney <kay@grafana.com>
2024-02-20 19:09:30 +0000

    Refactor for improved readability

--- public/app/features/dashboard-scene/panel-edit/PanelEditor.tsx
+++ public/app/features/dashboard-scene/panel-edit/PanelEditor.tsx
@@ -117,61 +117,101 @@ export class PanelEditor extends SceneObjectBase<PanelEditorState> {
     const panelManager = this.state.vizManager;
 
     const sourcePanelParent = sourcePanel!.parent;
-    const sourcePanelGrandparent = sourcePanelParent!.parent!;
-    if (!(sourcePanelGrandparent instanceof SceneGridLayout)) {
-      console.error('Expected grandparent to be SceneGridLayout!');
-      return;
-    }
 
     const normalToRepeat = !this._initialRepeatOptions.repeat && panelManager.state.repeat;
     const repeatToNormal = this._initialRepeatOptions.repeat && !panelManager.state.repeat;
 
     if (sourcePanelParent instanceof SceneGridItem) {
       if (normalToRepeat) {
-        const repeatDirection = panelManager.state.repeatDirection ?? 'h';
-        const repeater = new PanelRepeaterGridItem({
-          key: sourcePanelParent.state.key,
-          x: sourcePanelParent.state.x,
-          y: sourcePanelParent.state.y,
-          width: repeatDirection === 'h' ? 24 : sourcePanelParent.state.width,
-          height: sourcePanelParent.state.height,
-          itemHeight: sourcePanelParent.state.height,
-          source: panelManager.state.panel.clone(),
-          variableName: panelManager.state.repeat!,
-          repeatedPanels: [],
-          repeatDirection: panelManager.state.repeatDirection,
-          maxPerRow: panelManager.state.maxPerRow,
-        });
-        sourcePanelGrandparent.setState({
-          children: sourcePanelGrandparent.state.children.map((child) =>
-            child.state.key === sourcePanelParent.state.key ? repeater : child
-          ),
-        });
+        this.replaceSceneGridItemWithPanelRepeater(sourcePanelParent);
       } else {
         sourcePanelParent.setState({ body: panelManager.state.panel.clone() });
       }
     } else if (sourcePanelParent instanceof PanelRepeaterGridItem) {
       if (repeatToNormal) {
-        const panelClone = panelManager.state.panel.clone();
-        const gridItem = new SceneGridItem({
-          key: sourcePanelParent.state.key,
-          x: sourcePanelParent.state.x,
-          y: sourcePanelParent.state.y,
-          width: this._initialRepeatOptions.repeatDirection === 'h' ? 8 : sourcePanelParent.state.width,
-          height: this._initialRepeatOptions.repeatDirection === 'v' ? 8 : sourcePanelParent.state.height,
-          body: panelClone,
-        });
-        sourcePanelGrandparent.setState({
-          children: sourcePanelGrandparent.state.children.map((child) =>
-            child.state.key === sourcePanelParent.state.key ? gridItem : child
-          ),
-        });
+        this.replacePanelRepeaterWithGridItem(sourcePanelParent);
       } else {
-        sourcePanelParent.setState({ source: panelManager.state.panel.clone() });
+        this.handleRepeatOptionChanges(sourcePanelParent);
       }
     } else {
       console.error('Unsupported scene object type');
     }
+  }
+
+  private replaceSceneGridItemWithPanelRepeater(gridItem: SceneGridItem) {
+    const gridLayout = gridItem.parent;
+    if (!(gridLayout instanceof SceneGridLayout)) {
+      console.error('Expected grandparent to be SceneGridLayout!');
+      return;
+    }
+
+    const panelManager = this.state.vizManager;
+    const repeatDirection = panelManager.state.repeatDirection ?? 'h';
+    const repeater = new PanelRepeaterGridItem({
+      key: gridItem.state.key,
+      x: gridItem.state.x,
+      y: gridItem.state.y,
+      width: repeatDirection === 'h' ? 24 : gridItem.state.width,
+      height: gridItem.state.height,
+      itemHeight: gridItem.state.height,
+      source: panelManager.state.panel.clone(),
+      variableName: panelManager.state.repeat!,
+      repeatedPanels: [],
+      repeatDirection: panelManager.state.repeatDirection,
+      maxPerRow: panelManager.state.maxPerRow,
+    });
+    gridLayout.setState({
+      children: gridLayout.state.children.map((child) => (child.state.key === gridItem.state.key ? repeater : child)),
+    });
+  }
+
+  private replacePanelRepeaterWithGridItem(panelRepeater: PanelRepeaterGridItem) {
+    const gridLayout = panelRepeater.parent;
+    if (!(gridLayout instanceof SceneGridLayout)) {
+      console.error('Expected grandparent to be SceneGridLayout!');
+      return;
+    }
+
+    const panelManager = this.state.vizManager;
+    const panelClone = panelManager.state.panel.clone();
+    const gridItem = new SceneGridItem({
+      key: panelRepeater.state.key,
+      x: panelRepeater.state.x,
+      y: panelRepeater.state.y,
+      width: this._initialRepeatOptions.repeatDirection === 'h' ? 8 : panelRepeater.state.width,
+      height: this._initialRepeatOptions.repeatDirection === 'v' ? 8 : panelRepeater.state.height,
+      body: panelClone,
+    });
+    gridLayout.setState({
+      children: gridLayout.state.children.map((child) =>
+        child.state.key === panelRepeater.state.key ? gridItem : child
+      ),
+    });
+  }
+
+  private handleRepeatOptionChanges(panelRepeater: PanelRepeaterGridItem) {
+    let width = panelRepeater.state.width ?? 1;
+    let height = panelRepeater.state.height;
+
+    const panelManager = this.state.vizManager;
+    const horizontalToVertical =
+      this._initialRepeatOptions.repeatDirection === 'h' && panelManager.state.repeatDirection === 'v';
+    const verticalToHorizontal =
+      this._initialRepeatOptions.repeatDirection === 'v' && panelManager.state.repeatDirection === 'h';
+    if (horizontalToVertical) {
+      width = Math.floor(width / (panelRepeater.state.maxPerRow ?? 1));
+    } else if (verticalToHorizontal) {
+      width = 24;
+    }
+
+    panelRepeater.setState({
+      source: panelManager.state.panel.clone(),
+      repeatDirection: panelManager.state.repeatDirection,
+      variableName: panelManager.state.repeat,
+      maxPerRow: panelManager.state.maxPerRow,
+      width,
+      height,
+    });
   }
 
   public toggleOptionsPane() {
