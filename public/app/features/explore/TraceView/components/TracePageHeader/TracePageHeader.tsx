bc490e6273f51e818001a427e1051c85e001fcd8
Joey <90795735+joey-grafana@users.noreply.github.com>
2024-02-21 13:49:41 +0000

    Traces: Add traces panel suggestion (#83089)
    * Add traces panel suggestion
    
    * Render suggestion
    
    * Update styling
    
    * Update styling

--- public/app/features/explore/TraceView/components/TracePageHeader/TracePageHeader.tsx
+++ public/app/features/explore/TraceView/components/TracePageHeader/TracePageHeader.tsx
@@ -202,30 +202,34 @@ const getNewStyles = (theme: GrafanaTheme2) => {
         color: unset;
       }
     `,
-    header: css`
-      label: TracePageHeader;
-      background-color: ${theme.colors.background.primary};
-      padding: 0.5em 0 0 0;
-      position: sticky;
-      top: 0;
-      z-index: 5;
-    `,
-    titleRow: css`
-      align-items: flex-start;
-      display: flex;
-      padding: 0 8px;
-    `,
-    title: css`
-      color: inherit;
-      flex: 1;
-      font-size: 1.7em;
-      line-height: 1em;
-    `,
-    subtitle: css`
-      flex: 1;
-      line-height: 1em;
-      margin: -0.5em 0.5em 0.75em 0.5em;
-    `,
+    header: css({
+      label: 'TracePageHeader',
+      backgroundColor: theme.colors.background.primary,
+      padding: '0.5em 0 0 0',
+      position: 'sticky',
+      top: 0,
+      zIndex: 5,
+      textAlign: 'left',
+    }),
+    titleRow: css({
+      alignItems: 'flex-start',
+      display: 'flex',
+      padding: '0 8px',
+      flexWrap: 'wrap',
+    }),
+    title: css({
+      color: 'inherit',
+      flex: 1,
+      fontSize: '1.7em',
+      lineHeight: '1em',
+      marginBottom: 0,
+      minWidth: '200px',
+    }),
+    subtitle: css({
+      flex: 1,
+      lineHeight: '1em',
+      margin: '-0.5em 0.5em 0.75em 0.5em',
+    }),
     tag: css`
       margin: 0 0.5em 0 0;
     `,
