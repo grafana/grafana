81a6c623db02b803ee8927fe1b4615c3326e828a
Eric Leijonmarck <eric.leijonmarck@gmail.com>
2024-02-21 09:26:09 +0000

    Team LBAC: Add epilogue to permissions (#82523)
    * add epilogue to permissions
    
    * gs
    linting fix
    
    * update docs
    
    * Revert "update docs"
    
    This reverts commit 0902ce2d8a2dc3f402baa63ac0e9515c603231d0.

--- public/app/core/actions/index.ts
+++ public/app/core/actions/index.ts
@@ -1,3 +1,4 @@
 import { hideAppNotification, notifyApp } from '../reducers/appNotification';
 import { updateNavIndex, updateConfigurationSubtitle } from '../reducers/navModel';
+
 export { updateNavIndex, updateConfigurationSubtitle, notifyApp, hideAppNotification };
