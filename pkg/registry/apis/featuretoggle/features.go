79c116df19ec1fc29a0c394dc3641584893a33e3
Todd Treece <todd.treece@grafana.com>
2024-02-21 13:23:30 -0500

    add go.work and update references to new locations

--- pkg/registry/apis/featuretoggle/features.go
+++ pkg/registry/apis/featuretoggle/features.go
@@ -10,7 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/rest"
 
-	common "github.com/grafana/grafana/pkg/apis/common/v0alpha1"
+	common "github.com/grafana/grafana/pkg/apimachinery/apis/common/v0alpha1"
 	"github.com/grafana/grafana/pkg/apis/featuretoggle/v0alpha1"
 	"github.com/grafana/grafana/pkg/services/apiserver/utils"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
