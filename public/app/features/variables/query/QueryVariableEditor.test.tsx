bd8853f22eca66aabd979f7b8eb70f8b7e5ffdbf
Ivan Ortega <ivanortegaalba@gmail.com>
2024-02-21 15:20:51 +0100

    linter again :)

--- public/app/features/variables/query/QueryVariableEditor.test.tsx
+++ public/app/features/variables/query/QueryVariableEditor.test.tsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { MockDataSourceApi } from 'test/mocks/datasource_srv';
 
-import { CustomVariableSupport, DataSourceApi, VariableSupportType } from '@grafana/data';
+import { VariableSupportType } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { mockDataSource } from 'app/features/alerting/unified/mocks';
 import { DataSourceType } from 'app/features/alerting/unified/utils/datasource';
