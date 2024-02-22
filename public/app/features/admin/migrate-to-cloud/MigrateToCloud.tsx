2a39ccf53e41cdbefc77071e478d5c2a17f3e104
Josh Hunt <joshhunt@users.noreply.github.com>
2024-02-22 11:12:00 +0000

    MigrateToCloud: Add API interface for frontend (#83215)
    Add RTK Query mocks

--- public/app/features/admin/migrate-to-cloud/MigrateToCloud.tsx
+++ public/app/features/admin/migrate-to-cloud/MigrateToCloud.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { Stack, Text } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 
+import { useGetStatusQuery } from './api';
+
 export default function MigrateToCloud() {
-  return <Page navId="migrate-to-cloud">TODO</Page>;
+  const { data } = useGetStatusQuery();
+
+  return (
+    <Page navId="migrate-to-cloud">
+      <Stack>
+        <Text>TODO</Text>
+        <pre>{JSON.stringify(data)}</pre>
+      </Stack>
+    </Page>
+  );
 }
