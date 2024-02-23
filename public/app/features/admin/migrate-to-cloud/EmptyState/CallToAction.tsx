b25667223c77f8acdb6225d9454525f4416b1b72
Ashley Harrison <ashley.harrison@grafana.com>
2024-02-23 11:38:36 +0000

    Box: add `direction` prop to `Box` (#83296)
    add "direction" prop to Box

--- public/app/features/admin/migrate-to-cloud/EmptyState/CallToAction.tsx
+++ public/app/features/admin/migrate-to-cloud/EmptyState/CallToAction.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Button, Stack, Text } from '@grafana/ui';
+import { Box, Button, Text } from '@grafana/ui';
 import { Trans } from 'app/core/internationalization';
 
 export const CallToAction = () => {
@@ -9,15 +9,13 @@ export const CallToAction = () => {
   };
 
   return (
-    <Box padding={5} backgroundColor="secondary">
-      <Stack gap={2} direction="column" alignItems="center">
-        <Text variant="h3" textAlignment="center">
-          <Trans i18nKey="migrate-to-cloud.cta.header">Let us manage your Grafana stack</Trans>
-        </Text>
-        <Button onClick={onClickMigrate}>
-          <Trans i18nKey="migrate-to-cloud.cta.button">Migrate this instance to Cloud</Trans>
-        </Button>
-      </Stack>
+    <Box display="flex" padding={5} gap={2} direction="column" alignItems="center" backgroundColor="secondary">
+      <Text variant="h3" textAlignment="center">
+        <Trans i18nKey="migrate-to-cloud.cta.header">Let us manage your Grafana stack</Trans>
+      </Text>
+      <Button onClick={onClickMigrate}>
+        <Trans i18nKey="migrate-to-cloud.cta.button">Migrate this instance to Cloud</Trans>
+      </Button>
     </Box>
   );
 };
