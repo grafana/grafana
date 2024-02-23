fbbeadf5e7e23e255f05658ff6d5202a56caa282
Erik Sundell <erik.sundell87@gmail.com>
2024-02-23 10:02:16 +0100

    remove unused fixture
    Co-authored-by: Marcus Andersson <marcus.andersson@grafana.com>

--- e2e/plugin-e2e/plugin-e2e-api-tests/as-admin-user/explorePage.spec.ts
+++ e2e/plugin-e2e/plugin-e2e-api-tests/as-admin-user/explorePage.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@grafana/plugin-e2e';
 
 import { formatExpectError } from '../errors';
 
-test('query data response should be OK when query is valid', async ({ explorePage, page }) => {
+test('query data response should be OK when query is valid', async ({ explorePage }) => {
   await explorePage.datasource.set('gdev-testdata');
   await expect(explorePage.runQuery(), formatExpectError('Expected Explore query to execute successfully')).toBeOK();
 });
