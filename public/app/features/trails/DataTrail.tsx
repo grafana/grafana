8ae866aa9bf038d41b05ab2d12c654a08e7f164f
Torkel Ödegaard <torkel@grafana.com>
2024-02-23 16:22:34 +0100

    DataTrails: Sticky controls (#83286)
    * DataTrails: Sticky controls
    
    * Update

--- public/app/features/trails/DataTrail.tsx
+++ public/app/features/trails/DataTrail.tsx
@@ -235,13 +235,17 @@ function getStyles(theme: GrafanaTheme2) {
       flexGrow: 1,
       display: 'flex',
       flexDirection: 'column',
-      gap: theme.spacing(1),
     }),
     controls: css({
       display: 'flex',
       gap: theme.spacing(1),
+      padding: theme.spacing(1, 0),
       alignItems: 'flex-end',
       flexWrap: 'wrap',
+      position: 'sticky',
+      background: theme.isDark ? theme.colors.background.canvas : theme.colors.background.primary,
+      zIndex: theme.zIndex.activePanel + 1,
+      top: 0,
     }),
   };
 }
