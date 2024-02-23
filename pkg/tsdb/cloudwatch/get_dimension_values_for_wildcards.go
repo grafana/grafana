d59f57edca45aa827dbaf68a89fc3ff2f62f5a42
Isabella Siu <isabella.siu@grafana.com>
2024-02-23 13:57:56 -0500

    Revert refactoring getDimensionValuesForWildcards

--- pkg/tsdb/cloudwatch/get_dimension_values_for_wildcards.go
+++ pkg/tsdb/cloudwatch/get_dimension_values_for_wildcards.go
@@ -14,8 +14,13 @@ import (
 
 // getDimensionValues gets the actual dimension values for dimensions with a wildcard
 func (e *cloudWatchExecutor) getDimensionValuesForWildcards(ctx context.Context, pluginCtx backend.PluginContext, region string,
-	client models.CloudWatchMetricsAPIProvider, origQueries []*models.CloudWatchQuery, tagValueCache *cache.Cache, listMetricsPageLimit int) ([]*models.CloudWatchQuery, error) {
-	metricsClient := clients.NewMetricsClient(client, listMetricsPageLimit)
+	client models.CloudWatchMetricsAPIProvider, origQueries []*models.CloudWatchQuery, tagValueCache *cache.Cache) ([]*models.CloudWatchQuery, error) {
+	instance, err := e.getInstance(ctx, pluginCtx)
+	if err != nil {
+		return nil, err
+	}
+
+	metricsClient := clients.NewMetricsClient(client, instance.Settings.GrafanaSettings.ListMetricsPageLimit)
 	service := services.NewListMetricsService(metricsClient)
 	// create copies of the original query. All the fields besides Dimensions are primitives
 	queries := copyQueries(origQueries)
