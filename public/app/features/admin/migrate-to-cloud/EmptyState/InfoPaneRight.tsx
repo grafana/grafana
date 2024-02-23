f225425fe817e35dfb10c0f0645580c07fb0584d
Ashley Harrison <ashley.harrison@grafana.com>
2024-02-23 11:18:09 +0000

    E2C: Add initial empty state (#83232)
    * update subtitle
    
    * add empty state
    
    * rename to InfoPaneLeft/Right
    
    * use &apos;
    
    * add "direction" prop to Box
    
    * update subtitle
    
    * Revert "add "direction" prop to Box"
    
    This reverts commit 99f82a27c732541fe9ca0f1dc87e6db0f6eb72fc.

---
+++ public/app/features/admin/migrate-to-cloud/EmptyState/InfoPaneRight.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Box, Stack } from '@grafana/ui';
+import { t, Trans } from 'app/core/internationalization';
+
+import { InfoItem } from './InfoItem';
+
+export const InfoPaneRight = () => {
+  return (
+    <Box padding={2} backgroundColor="secondary">
+      <Stack gap={2} direction="column">
+        <InfoItem
+          title={t('migrate-to-cloud.pdc.title', 'Not all my data sources are on the public internet')}
+          linkTitle={t('migrate-to-cloud.pdc.link-title', 'Learn about PDC')}
+          linkHref="https://grafana.com/docs/grafana-cloud/connect-externally-hosted/private-data-source-connect"
+        >
+          <Trans i18nKey="migrate-to-cloud.pdc.body">
+            Exposing your data sources to the internet can raise security concerns. Private data source connect (PDC)
+            allows Grafana Cloud to access your existing data sources over a secure network tunnel.
+          </Trans>
+        </InfoItem>
+        <InfoItem
+          title={t('migrate-to-cloud.pricing.title', 'How much does it cost?')}
+          linkTitle={t('migrate-to-cloud.pricing.link-title', 'Grafana Cloud pricing')}
+          linkHref="https://grafana.com/pricing"
+        >
+          <Trans i18nKey="migrate-to-cloud.pricing.body">
+            Grafana Cloud has a generous free plan and a 14 day unlimited usage trial. After your trial expires,
+            you&apos;ll be billed based on usage over the free plan limits.
+          </Trans>
+        </InfoItem>
+        <InfoItem
+          title={t('migrate-to-cloud.can-i-move.title', 'Can I move this installation to Grafana Cloud?')}
+          linkTitle={t('migrate-to-cloud.can-i-move.link-title', 'Learn about migrating other settings')}
+          linkHref="https://grafana.com/docs/grafana-cloud/account-management/migration-guide"
+        >
+          <Trans i18nKey="migrate-to-cloud.can-i-move.body">
+            Once you connect this installation to a cloud stack, you&apos;ll be able to upload data sources and
+            dashboards.
+          </Trans>
+        </InfoItem>
+      </Stack>
+    </Box>
+  );
+};
--- public/app/features/admin/migrate-to-cloud/MigrateToCloud.tsx
+++ public/app/features/admin/migrate-to-cloud/MigrateToCloud.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import { Stack, Text } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 
-import { useGetStatusQuery } from './api';
+import { EmptyState } from './EmptyState/EmptyState';
 
 export default function MigrateToCloud() {
-  const { data } = useGetStatusQuery();
-
   return (
     <Page navId="migrate-to-cloud">
-      <Stack>
-        <Text>TODO</Text>
-        <pre>{JSON.stringify(data)}</pre>
-      </Stack>
+      <EmptyState />
     </Page>
   );
 }
