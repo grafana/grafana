ee93c520a89d2bdec1fcd03e21adc62fc4ea7282
Andreas Christou <andreas.christou@grafana.com>
2024-02-26 19:20:24 +0000

    Remove unused import

--- public/app/plugins/datasource/azuremonitor/components/ConfigEditor/CurrentUserFallbackCredentials.test.tsx
+++ public/app/plugins/datasource/azuremonitor/components/ConfigEditor/CurrentUserFallbackCredentials.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import { config } from '@grafana/runtime';
