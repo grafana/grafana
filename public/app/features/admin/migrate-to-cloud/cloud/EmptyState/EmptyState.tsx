878357e3a5752547167256f6b6f4f8d832eaaaeb
Ashley Harrison <ashley.harrison@grafana.com>
2024-02-26 10:10:35 +0000

    E2C: Add cloud landing page (#83316)
    * restructure and create cloud empty state
    
    * use new Grid prop and run i18n:extract

---
+++ public/app/features/admin/migrate-to-cloud/cloud/EmptyState/EmptyState.tsx
@@ -0,0 +1,34 @@
+import { css } from '@emotion/css';
+import React from 'react';
+
+import { GrafanaTheme2 } from '@grafana/data';
+import { Grid, useStyles2 } from '@grafana/ui';
+
+import { InfoPane } from './InfoPane';
+import { MigrationTokenPane } from './MigrationTokenPane';
+
+export const EmptyState = () => {
+  const styles = useStyles2(getStyles);
+
+  return (
+    <div className={styles.container}>
+      <Grid
+        alignItems="flex-start"
+        gap={1}
+        columns={{
+          xs: 1,
+          lg: 2,
+        }}
+      >
+        <InfoPane />
+        <MigrationTokenPane />
+      </Grid>
+    </div>
+  );
+};
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  container: css({
+    maxWidth: theme.breakpoints.values.xl,
+  }),
+});
