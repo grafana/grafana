0dd34aefbaa3f738cc5918c630a7de30c11a9334
Isabella Siu <Isabella.siu@grafana.com>
2024-02-26 09:35:59 -0500

    CloudWatch: Remove unused handleGetRegions and regionCache (#83333)
    * remove unused handleGetRegions and regionCache
    
    * cleanup

--- pkg/tsdb/cloudwatch/metric_find_query_test.go
+++ pkg/tsdb/cloudwatch/metric_find_query_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"net/url"
-	"sort"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,124 +11,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi"
 	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi/resourcegroupstaggingapiiface"
-	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	"github.com/grafana/grafana-plugin-sdk-go/data"
-	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/constants"
-	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/mocks"
 	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/models"
-	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/utils"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func TestQuery_Regions(t *testing.T) {
-	origNewEC2Client := NewEC2Client
-	t.Cleanup(func() {
-		NewEC2Client = origNewEC2Client
-	})
-
-	ec2Mock := &mocks.EC2Mock{}
-	NewEC2Client = func(provider client.ConfigProvider) models.EC2APIProvider {
-		return ec2Mock
-	}
-	t.Run("An extra region", func(t *testing.T) {
-		const regionName = "xtra-region"
-		ec2Mock.On("DescribeRegionsWithContext", mock.Anything, mock.Anything).Return(&ec2.DescribeRegionsOutput{
-			Regions: []*ec2.Region{
-				{
-					RegionName: utils.Pointer(regionName),
-				},
-			},
-		}, nil)
-
-		im := datasource.NewInstanceManager(func(ctx context.Context, s backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-			return DataSource{Settings: models.CloudWatchSettings{
-				AWSDatasourceSettings: awsds.AWSDatasourceSettings{Region: "us-east-2"},
-				GrafanaSettings:       awsds.AuthSettings{ListMetricsPageLimit: 1000},
-			}}, nil
-		})
-
-		executor := newExecutor(im, &fakeSessionCache{}, log.NewNullLogger())
-		resp, err := executor.handleGetRegions(
-			context.Background(),
-			backend.PluginContext{
-				DataSourceInstanceSettings: &backend.DataSourceInstanceSettings{},
-			}, url.Values{
-				"region":    []string{"us-east-1"},
-				"namespace": []string{"custom"},
-			},
-		)
-		require.NoError(t, err)
-
-		expRegions := buildSortedSliceOfDefaultAndExtraRegions(t, regionName)
-		expFrame := data.NewFrame(
-			"",
-			data.NewField("text", nil, expRegions),
-			data.NewField("value", nil, expRegions),
-		)
-		expFrame.Meta = &data.FrameMeta{
-			Custom: map[string]any{
-				"rowCount": len(constants.Regions()) + 1,
-			},
-		}
-
-		expResponse := []suggestData{}
-		for _, region := range expRegions {
-			expResponse = append(expResponse, suggestData{Text: region, Value: region, Label: region})
-		}
-		assert.Equal(t, expResponse, resp)
-	})
-}
-
-func buildSortedSliceOfDefaultAndExtraRegions(t *testing.T, regionName string) []string {
-	t.Helper()
-	regions := constants.Regions()
-	regions[regionName] = struct{}{}
-	var expRegions []string
-	for region := range regions {
-		expRegions = append(expRegions, region)
-	}
-	sort.Strings(expRegions)
-	return expRegions
-}
-
-func Test_handleGetRegions_regionCache(t *testing.T) {
-	origNewEC2Client := NewEC2Client
-	t.Cleanup(func() {
-		NewEC2Client = origNewEC2Client
-	})
-	cli := mockEC2Client{}
-	NewEC2Client = func(client.ConfigProvider) models.EC2APIProvider {
-		return &cli
-	}
-	im := datasource.NewInstanceManager(func(ctx context.Context, s backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-		return DataSource{Settings: models.CloudWatchSettings{
-			AWSDatasourceSettings: awsds.AWSDatasourceSettings{Region: "us-east-2"},
-			GrafanaSettings:       awsds.AuthSettings{ListMetricsPageLimit: 1000},
-		}}, nil
-	})
-
-	t.Run("AWS only called once for multiple calls to handleGetRegions", func(t *testing.T) {
-		cli.On("DescribeRegionsWithContext", mock.Anything, mock.Anything).Return(&ec2.DescribeRegionsOutput{}, nil)
-		executor := newExecutor(im, &fakeSessionCache{}, log.NewNullLogger())
-		_, err := executor.handleGetRegions(
-			context.Background(),
-			backend.PluginContext{DataSourceInstanceSettings: &backend.DataSourceInstanceSettings{}}, nil)
-		require.NoError(t, err)
-
-		_, err = executor.handleGetRegions(
-			context.Background(),
-			backend.PluginContext{DataSourceInstanceSettings: &backend.DataSourceInstanceSettings{}}, nil)
-		require.NoError(t, err)
-
-		cli.AssertNumberOfCalls(t, "DescribeRegionsWithContext", 1)
-	})
-}
 func TestQuery_InstanceAttributes(t *testing.T) {
 	origNewEC2Client := NewEC2Client
 	t.Cleanup(func() {
