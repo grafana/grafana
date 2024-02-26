ad1ab25585b90eaa909ac04e336bbdbe151fedeb
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-02-26 14:47:36 +0000

    Check for correct fields

--- public/app/plugins/panel/nodeGraph/suggestions.ts
+++ public/app/plugins/panel/nodeGraph/suggestions.ts
@@ -1,4 +1,4 @@
-import { VisualizationSuggestionsBuilder, VisualizationSuggestionScore } from '@grafana/data';
+import { DataFrame, FieldType, VisualizationSuggestionsBuilder, VisualizationSuggestionScore } from '@grafana/data';
 import { SuggestionName } from 'app/types/suggestions';
 
 export class NodeGraphSuggestionsSupplier {
@@ -9,20 +9,55 @@ export class NodeGraphSuggestionsSupplier {
     });
   }
 
+  hasCorrectFields(frames: DataFrame[]): boolean {
+    let hasNodesFrame = false;
+    let hasEdgesFrame = false;
+
+    const nodeFields: Array<[string, FieldType[]]> = [
+      ['id', [FieldType.string]],
+      ['title', [FieldType.string]],
+      ['mainstat', [FieldType.number]],
+    ];
+    const edgeFields: Array<[string, FieldType[]]> = [
+      ['id', [FieldType.string]],
+      ['source', [FieldType.string]],
+      ['target', [FieldType.string]],
+    ];
+
+    for (const frame of frames) {
+      if (this.checkFields(nodeFields, frame)) {
+        hasNodesFrame = true;
+      }
+      if (this.checkFields(edgeFields, frame)) {
+        hasEdgesFrame = true;
+      }
+    }
+
+    return hasNodesFrame && hasEdgesFrame;
+  }
+
+  checkFields(fields: Array<[string, FieldType[]]>, frame: DataFrame): boolean {
+    let hasCorrectFields = true;
+
+    for (const field of fields) {
+      const [name, types] = field;
+      const frameField = frame.fields.find((f) => f.name === name);
+      if (!frameField || !types.includes(frameField.type)) {
+        hasCorrectFields = false;
+        break;
+      }
+    }
+
+    return hasCorrectFields ? true : false;
+  }
+
   getSuggestionsForData(builder: VisualizationSuggestionsBuilder) {
     if (!builder.data) {
       return;
     }
 
-    const dataFrames = builder.data.series.filter(
-      (df) => df.meta && df.meta.preferredVisualisationType === 'nodeGraph'
-    );
-    // one frame for nodes, one frame for edges
-    if (dataFrames.length !== 2) {
-      return;
-    }
-
-    if (builder.dataSummary.preferredVisualisationType === 'nodeGraph') {
+    const hasCorrectFields = this.hasCorrectFields(builder.data.series);
+    if (hasCorrectFields) {
       this.getListWithDefaults(builder).append({
         name: SuggestionName.NodeGraph,
         score: VisualizationSuggestionScore.Best,
