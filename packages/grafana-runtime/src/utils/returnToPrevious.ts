0514c33bfa0afe00493d3f872a68a5f30952ed22
eledobleefe <laura.fernandez@grafana.com>
2024-02-26 09:38:34 +0100

    Delete link till documentation is on dev portal

--- packages/grafana-runtime/src/utils/returnToPrevious.ts
+++ packages/grafana-runtime/src/utils/returnToPrevious.ts
@@ -10,8 +10,6 @@ export const setReturnToPreviousHook = (hook: ReturnToPreviousHook) => {
  * Guidelines:
  * - Do only use the ‘Return to previous’ functionality when the user is sent to another context, such as from Alerting to a dashboard.
  * - Do specify a button title that identifies the page to return to in the most understandable way. Do not use text such as ‘Back to the previous page’. Be specific.
- *
- * {@link https://docs.google.com/document/d/1bSh5W_v5okEwjVPvP4bOmILwfKwmYLkf6Y752Ay_02I ReturnToPrevious: Documentation for devs}
  */
 export const useReturnToPrevious: ReturnToPreviousHook = () => {
   if (!rtpHook) {
