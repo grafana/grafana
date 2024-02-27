c322a3f127f07550028a7f5d13c736eae134ac7b
scottlepp <scott.lepper@gmail.com>
2024-02-27 15:33:12 -0500

    make the parser a bit more lenient

--- pkg/expr/sql/parser.go
+++ pkg/expr/sql/parser.go
@@ -2,6 +2,7 @@ package sql
 
 import (
 	"errors"
+	"strings"
 
 	parser "github.com/krasun/gosqlparser"
 	"github.com/xwb1989/sqlparser"
@@ -11,7 +12,11 @@ import (
 func TablesList(rawSQL string) ([]string, error) {
 	stmt, err := sqlparser.Parse(rawSQL)
 	if err != nil {
-		return parse(rawSQL)
+		tables, err := parse(rawSQL)
+		if err != nil {
+			return parseTables(rawSQL)
+		}
+		return tables, nil
 	}
 
 	tables := []string{}
@@ -20,7 +25,10 @@ func TablesList(rawSQL string) ([]string, error) {
 		for _, t := range kind.From {
 			buf := sqlparser.NewTrackedBuffer(nil)
 			t.Format(buf)
-			tables = append(tables, buf.String())
+			table := buf.String()
+			if table != "dual" {
+				tables = append(tables, buf.String())
+			}
 		}
 	default:
 		return nil, errors.New("not a select statement")
@@ -41,4 +49,51 @@ func parse(rawSQL string) ([]string, error) {
 		}
 	}
 	return nil, err
+}
+
+func parseTables(rawSQL string) ([]string, error) {
+	checkSql := strings.ToUpper(rawSQL)
+	if strings.HasPrefix(checkSql, "SELECT") || strings.HasPrefix(rawSQL, "WITH") {
+		tables := []string{}
+		tokens := strings.Split(rawSQL, " ")
+		checkNext := false
+		takeNext := false
+		for _, t := range tokens {
+			t = strings.ToUpper(t)
+			t = strings.TrimSpace(t)
+
+			if takeNext {
+				tables = append(tables, t)
+				checkNext = false
+				takeNext = false
+				continue
+			}
+			if checkNext {
+				if strings.Contains(t, "(") {
+					checkNext = false
+					continue
+				}
+				if strings.Contains(t, ",") {
+					values := strings.Split(t, ",")
+					for _, v := range values {
+						v := strings.TrimSpace(v)
+						if v != "" {
+							tables = append(tables, v)
+						} else {
+							takeNext = true
+							break
+						}
+					}
+					continue
+				}
+				tables = append(tables, t)
+				checkNext = false
+			}
+			if t == "FROM" {
+				checkNext = true
+			}
+		}
+		return tables, nil
+	}
+	return nil, errors.New("Not a select statement")
 }
