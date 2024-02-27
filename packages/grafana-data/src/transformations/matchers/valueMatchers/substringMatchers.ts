51ead8bdd900f66c8594ad18776b48dc20639077
nmarrs <nathanielmarrs@gmail.com>
2024-02-27 23:27:58 +0100

    fix lint

--- packages/grafana-data/src/transformations/matchers/valueMatchers/substringMatchers.ts
+++ packages/grafana-data/src/transformations/matchers/valueMatchers/substringMatchers.ts
@@ -12,7 +12,7 @@ const isLikeValueMatcher: ValueMatcherInfo<BasicValueMatcherOptions> = {
     return (valueIndex: number, field: Field) => {
       const value = field.values[valueIndex];
       // eslint-disable-next-line eqeqeq
-    return value.includes(options.value);
+      return value.includes(options.value);
     };
   },
   getOptionsDisplayText: () => {
@@ -23,23 +23,23 @@ const isLikeValueMatcher: ValueMatcherInfo<BasicValueMatcherOptions> = {
 };
 
 const isNotLikeValueMatcher: ValueMatcherInfo<BasicValueMatcherOptions> = {
-    id: ValueMatcherID.notSubstring,
-    name: 'Is not substring',
-    description: 'Match where value for given field is not a substring to options value.',
-    get: (options) => {
-        return (valueIndex: number, field: Field) => {
-        const value = field.values[valueIndex];
-        // eslint-disable-next-line eqeqeq
+  id: ValueMatcherID.notSubstring,
+  name: 'Is not substring',
+  description: 'Match where value for given field is not a substring to options value.',
+  get: (options) => {
+    return (valueIndex: number, field: Field) => {
+      const value = field.values[valueIndex];
+      // eslint-disable-next-line eqeqeq
 
-        // compare value to options.value, if
-        return !value.includes(options.value);
-        };
-    },
-    getOptionsDisplayText: () => {
-        return `Matches all rows where field is not similar to the value.`;
-    },
-    isApplicable: (field) => field.type === FieldType.string,
-    getDefaultOptions: () => ({ value: '' }),
+      // compare value to options.value, if
+      return !value.includes(options.value);
     };
+  },
+  getOptionsDisplayText: () => {
+    return `Matches all rows where field is not similar to the value.`;
+  },
+  isApplicable: (field) => field.type === FieldType.string,
+  getDefaultOptions: () => ({ value: '' }),
+};
 
 export const getSubstringValueMatchers = (): ValueMatcherInfo[] => [isLikeValueMatcher, isNotLikeValueMatcher];
