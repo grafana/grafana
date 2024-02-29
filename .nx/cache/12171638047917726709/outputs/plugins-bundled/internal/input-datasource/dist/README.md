8fb632ddf485d70103908a3f4f5b1b8d1f9fa3f8
Clarity-89 <homes89@ukr.net>
2024-02-29 16:48:31 +0200

    Switch: Remove transparent prop

---
+++ .nx/cache/12171638047917726709/outputs/plugins-bundled/internal/input-datasource/dist/README.md
@@ -0,0 +1,3 @@
+# Direct Input Data Source - Bundled Plugin
+
+This data source lets you define results directly in CSV. The values are stored either in a shared data source, or directly in panels.
---
+++ packages/grafana-icons/src/index.ts
@@ -0,0 +1,5 @@
+export { type IconProps } from './IconBase';
+export { default as AngleRight } from './icons-gen/AngleRight';
+export { default as Apps } from './icons-gen/Apps';
+export { default as Book } from './icons-gen/Book';
+export { default as Cancel } from './icons-gen/Cancel';
