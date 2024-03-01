e8f2ff1b85b675fe77082788bbcc1f76a04fc7a1
eledobleefe <laura.fernandez@grafana.com>
2024-03-01 15:47:34 +0100

    Fix test checking specific reportInteraction and time triggered

--- public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.test.tsx
+++ public/app/core/components/AppChrome/ReturnToPrevious/ReturnToPrevious.test.tsx
@@ -29,11 +29,14 @@ const setup = () => {
 };
 
 describe('ReturnToPrevious', () => {
-  /* We enabled the feature toggle */
-  config.featureToggles.returnToPrevious = true;
+  beforeEach(() => {
+    /* We enabled the feature toggle */
+    config.featureToggles.returnToPrevious = true;
+  });
   afterEach(() => {
     window.sessionStorage.clear();
     jest.resetAllMocks();
+    config.featureToggles.returnToPrevious = false;
   });
   it('should render component', async () => {
     setup();
@@ -45,22 +48,21 @@ describe('ReturnToPrevious', () => {
     const returnButton = await screen.findByTitle('Back to Dashboards Page');
     expect(returnButton).toBeInTheDocument();
     await userEvent.click(returnButton);
-    expect(reportInteraction).toHaveBeenCalledWith('grafana_return_to_previous_button_dismissed', {
-      action: 'clicked',
-      page: '/dashboards',
-    });
-    expect(reportInteraction).toHaveBeenCalledTimes(1);
+    const mockCalls = (reportInteraction as jest.Mock).mock.calls;
+    /* The report is called 'grafana_return_to_previous_button_dismissed' but the action is 'clicked' */
+    const mockReturn = mockCalls.filter((call) => call[0] === 'grafana_return_to_previous_button_dismissed');
+    expect(mockReturn).toHaveLength(1);
   });
 
   it('should trigger event once when clicking on the Close button', async () => {
     setup();
+
     const closeBtn = await screen.findByRole('button', { name: 'Close' });
     expect(closeBtn).toBeInTheDocument();
     await userEvent.click(closeBtn);
-    expect(reportInteraction).toHaveBeenCalledWith('grafana_return_to_previous_button_dismissed', {
-      action: 'dismissed',
-      page: '/dashboards',
-    });
-    expect(reportInteraction).toHaveBeenCalledTimes(1);
+    const mockCalls = (reportInteraction as jest.Mock).mock.calls;
+    /* The report is called 'grafana_return_to_previous_button_dismissed' but the action is 'dismissed' */
+    const mockDismissed = mockCalls.filter((call) => call[0] === 'grafana_return_to_previous_button_dismissed');
+    expect(mockDismissed).toHaveLength(1);
   });
 });
