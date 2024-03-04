c5bb242dc1a2b3dfa537101943f3d6328f609369
Kuba Siemiatkowski <jakub.siemiatkowski@grafana.com>
2024-03-04 23:04:09 +0100

    request our python server

--- pkg/expr/sql/translator.go
+++ pkg/expr/sql/translator.go
@@ -1,10 +1,60 @@
 package sql
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const SERVER_URL = "http://localhost"
+const SERVER_PORT = 8000
+
+type Body struct {
+	Question string `json:"question"`
+	Schema   string `json:"schema"`
+}
 
 func Translate(text string, schema string) (string, error) {
-	// TODO - kuba call our python api running in a container
 	fmt.Println(text)
 	fmt.Println(schema)
-	return "", nil
+	resp, err := doRequest(text, schema)
+
+	return resp, err
+}
+
+func doRequest(question string, schema string) (string, error) {
+	b := Body{
+		question,
+		schema
+	}
+	body, err := json.Marshal(&b)
+	if err != nil {
+		fmt.Printf("error parsing json: %s\n", err)
+		return "", err
+	}
+	requestURL := fmt.Sprintf("%s:%d", SERVER_URL, SERVER_PORT)
+
+	req, err := http.NewRequest(http.MethodPost, requestURL, strings.NewReader(string(body)))
+	if err != nil {
+		fmt.Printf("error creating request: %s\n", err)
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("error making request: %s\n", err)
+		return "", err
+	}
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("error reading response: %s\n", err)
+		
+	}
+	return resBody
 }
