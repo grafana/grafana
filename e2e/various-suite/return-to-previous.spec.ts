c6202b9fb076b0f2a86b3e05e5eb81ce4ec4af5a
Laura Benz <laura.benz@grafana.com>
2024-03-04 16:16:34 +0100

    refactor: remove assertions for alert rule details view

--- e2e/various-suite/return-to-previous.spec.ts
+++ e2e/various-suite/return-to-previous.spec.ts
@@ -27,64 +27,62 @@ describe('ReturnToPrevious button', () => {
 
     // check whether the RTP button leads back to alert rule
     cy.url().should('eq', url);
-    cy.get('span').contains('e2e-ReturnToPrevious-test').should('be.visible');
-    e2e.components.AlertRules.toDashboard().should('be.visible');
   });
 
-  it('should disappear and clear session storage when clicking "Dismiss"', () => {
-    cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('exist');
-    e2e.components.ReturnToPrevious.dismissButton().should('be.visible').click();
-    e2e.components.ReturnToPrevious.buttonGroup().should('not.exist');
-    cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('not.exist');
-  });
+  // it('should disappear and clear session storage when clicking "Dismiss"', () => {
+  //   cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('exist');
+  //   e2e.components.ReturnToPrevious.dismissButton().should('be.visible').click();
+  //   e2e.components.ReturnToPrevious.buttonGroup().should('not.exist');
+  //   cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('not.exist');
+  // });
 
-  it('should persist when going back to alert rules page via nav and disappear when in the alert rule details view', () => {
-    e2e.components.ReturnToPrevious.buttonGroup().should('be.visible');
-    cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('exist');
+  // it('should persist when going back to alert rules page via nav and disappear when in the alert rule details view', () => {
+  //   e2e.components.ReturnToPrevious.buttonGroup().should('be.visible');
+  //   cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('exist');
+  //
+  //   // make sure the dashboard finished loading
+  //   cy.get('button[aria-label*="BarChart - Label Rotation & Skipping"]').should('be.visible');
+  //
+  //   cy.get('a[href="/alerting/list"]').click();
+  //   e2e.components.ReturnToPrevious.buttonGroup().should('exist');
+  //   cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('exist');
+  //
+  //   e2e.components.AlertRules.groupToggle().first().click();
+  //   cy.get('a[title="View"]').click();
+  //   e2e.components.ReturnToPrevious.buttonGroup().should('not.exist');
+  //   cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('not.exist');
+  // });
 
-    // make sure the dashboard finished loading
-    cy.get('button[aria-label*="BarChart - Label Rotation & Skipping"]').should('be.visible');
-
-    cy.get('a[href="/alerting/list"]').click();
-    e2e.components.ReturnToPrevious.buttonGroup().should('exist');
-    cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('exist');
-
-    e2e.components.AlertRules.groupToggle().first().click();
-    cy.get('a[title="View"]').click();
-    e2e.components.ReturnToPrevious.buttonGroup().should('not.exist');
-    cy.window().its('sessionStorage').invoke('getItem', 'returnToPrevious').should('not.exist');
-  });
-
-  it('should override the information in session storage when user changes alert rules', () => {
-    const alertRule1 =
-      '{"title":"e2e-ReturnToPrevious-test","href":"/alerting/grafana/bddn0v6f1kgzkc/view?returnTo=%2Falerting%2Flist"}';
-    const alertRule2 =
-      '{"title":"e2e-ReturnToPrevious-test-2","href":"/alerting/grafana/dddyksihq7h1ca/view?returnTo=%2Falerting%2Flist"}';
-
-    cy.window()
-      .its('sessionStorage')
-      .invoke('getItem', 'returnToPrevious')
-      .then((result) => {
-        expect(result).to.eql(alertRule1);
-      });
-
-    // make sure the dashboard finished loading
-    cy.get('button[aria-label*="BarChart - Label Rotation & Skipping"]').should('be.visible');
-
-    cy.get('button').contains('Search or jump to...').click();
-    cy.get('[role="dialog"]').find('input').type('alert');
-    cy.get('[role="dialog"]').find('a[href="/alerting/list"]').click();
-
-    e2e.components.AlertRules.groupToggle().last().click();
-    cy.get('a[title="View"]').click();
-    e2e.components.AlertRules.toDashboard().click();
-
-    cy.window()
-      .its('sessionStorage')
-      .invoke('getItem', 'returnToPrevious')
-      .then((result) => {
-        expect(result).to.not.eql(alertRule1);
-        expect(result).to.eql(alertRule2);
-      });
-  });
+  // it('should override the information in session storage when user changes alert rules', () => {
+  //   const alertRule1 =
+  //     '{"title":"e2e-ReturnToPrevious-test","href":"/alerting/grafana/bddn0v6f1kgzkc/view?returnTo=%2Falerting%2Flist"}';
+  //   const alertRule2 =
+  //     '{"title":"e2e-ReturnToPrevious-test-2","href":"/alerting/grafana/dddyksihq7h1ca/view?returnTo=%2Falerting%2Flist"}';
+  //
+  //   cy.window()
+  //     .its('sessionStorage')
+  //     .invoke('getItem', 'returnToPrevious')
+  //     .then((result) => {
+  //       expect(result).to.eql(alertRule1);
+  //     });
+  //
+  //   // make sure the dashboard finished loading
+  //   cy.get('button[aria-label*="BarChart - Label Rotation & Skipping"]').should('be.visible');
+  //
+  //   cy.get('button').contains('Search or jump to...').click();
+  //   cy.get('[role="dialog"]').find('input').type('alert');
+  //   cy.get('[role="dialog"]').find('a[href="/alerting/list"]').click();
+  //
+  //   e2e.components.AlertRules.groupToggle().last().click();
+  //   cy.get('a[title="View"]').click();
+  //   e2e.components.AlertRules.toDashboard().click();
+  //
+  //   cy.window()
+  //     .its('sessionStorage')
+  //     .invoke('getItem', 'returnToPrevious')
+  //     .then((result) => {
+  //       expect(result).to.not.eql(alertRule1);
+  //       expect(result).to.eql(alertRule2);
+  //     });
+  // });
 });
