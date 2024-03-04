b3d7e466f9318ef41f1395181f43f2a7da8cd3b8
scottlepp <scott.lepper@gmail.com>
2024-03-04 17:18:40 -0500

    text to sql translator

--- pkg/expr/sql/translator.go
+++ pkg/expr/sql/translator.go
@@ -1,10 +1,62 @@
 package sql
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func Translate(text string, schema string) (string, error) {
-	// TODO - kuba call our python api running in a container
-	fmt.Println(text)
-	fmt.Println(schema)
-	return "", nil
+	cleanSchema, err := json.Marshal(schema)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cleanQuestion, err := json.Marshal(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	request := fmt.Sprintf(`{"question":%s, "schema":%s}`, string(cleanQuestion), string(cleanSchema))
+
+	fmt.Println(request)
+
+	posturl := "http://localhost:8000"
+	body := []byte(request)
+
+	r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(body))
+	if err != nil {
+		return "", err
+	}
+
+	r.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	body, error := io.ReadAll(res.Body)
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	fmt.Println(string(body))
+
+	resp := &Resp{}
+	err = json.Unmarshal(body, resp)
+	if err != nil {
+		return "", err
+	}
+
+	return resp.SQL, nil
+}
+
+type Resp struct {
+	SQL string `json:"sql"`
 }
