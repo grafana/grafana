af45180a63664436f65ac34c5b2d53b71deecf41
spinillos <selenepinillos@gmail.com>
2024-03-05 15:50:27 +0100

    Remove jenny_ts_resources and use jenny_ts_types for all cases

--- pkg/codegen/jenny_eachmajor.go
+++ pkg/codegen/jenny_eachmajor.go
@@ -43,48 +43,22 @@ func (j *lmox) Generate(kind kindsys.Kind) (codejen.Files, error) {
 	sfg := SchemaForGen{
 		Name:    comm.Name,
 		IsGroup: comm.LineageIsGroup,
+		Schema:  kind.Lineage().Latest(),
 	}
 
 	if j.forceGroup {
 		sfg.IsGroup = true
 	}
 
-	do := func(sfg SchemaForGen, infix string) (codejen.Files, error) {
-		f, err := j.inner.Generate(sfg)
-		if err != nil {
-			return nil, fmt.Errorf("%s jenny failed on %s schema for %s: %w", j.inner.JennyName(), sfg.Schema.Version(), kind.Props().Common().Name, err)
-		}
-		if f == nil || !f.Exists() {
-			return nil, nil
-		}
-
-		f.RelativePath = filepath.Join(j.parentdir, comm.MachineName, infix, f.RelativePath)
-		f.From = append(f.From, j)
-		return codejen.Files{*f}, nil
+	f, err := j.inner.Generate(sfg)
+	if err != nil {
+		return nil, fmt.Errorf("%s jenny failed on %s schema for %s: %w", j.inner.JennyName(), sfg.Schema.Version(), kind.Props().Common().Name, err)
+	}
+	if f == nil || !f.Exists() {
+		return nil, nil
 	}
 
-	if comm.Maturity.Less(kindsys.MaturityStable) {
-		sfg.Schema = kind.Lineage().Latest()
-		return do(sfg, "x")
-	}
-
-	var fl codejen.Files
-	major := -1
-	for sch := kind.Lineage().First(); sch != nil; sch = sch.Successor() {
-		if int(sch.Version()[0]) == major {
-			continue
-		}
-		major = int(sch.Version()[0])
-
-		sfg.Schema = sch.LatestInMajor()
-		files, err := do(sfg, fmt.Sprintf("v%v", sch.Version()[0]))
-		if err != nil {
-			return nil, err
-		}
-		fl = append(fl, files...)
-	}
-	if fl.Validate() != nil {
-		return nil, fl.Validate()
-	}
-	return fl, nil
+	f.RelativePath = filepath.Join(j.parentdir, comm.MachineName, "x", f.RelativePath)
+	f.From = append(f.From, j)
+	return codejen.Files{*f}, nil
 }
