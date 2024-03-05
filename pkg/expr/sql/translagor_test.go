e24612af1047581592f129b9635db25a4dbc1079
scottlepp <scott.lepper@gmail.com>
2024-03-05 08:02:42 -0500

    tests

--- pkg/expr/sql/translagor_test.go
+++ pkg/expr/sql/translagor_test.go
@@ -14,22 +14,30 @@ func TestTranslate(t *testing.T) {
 	frame.RefID = "foo"
 
 	fieldString2 := data.NewField("test", nil, []string{"foo", "bardddd"})
-	fieldNumber2 := data.NewField("val", nil, []float64{353343123.12, 333.34})
+	fieldNumber2 := data.NewField("val", nil, []float64{100.12, 400.34})
 	frame2 := data.NewFrame("bar", fieldString2, fieldNumber2)
 	frame2.RefID = "bar"
 
 	frames := data.Frames{frame, frame2}
 	s := getSchema(frames, 2)
 
-	sql, err := Translate("what is the count of foos", s)
-	if err != nil {
-		fmt.Println(err.Error())
-		t.Fail()
-		return
-	}
-	fmt.Println(sql)
+	// sql, err := Translate("what is the count of foos", s)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	t.Fail()
+	// 	return
+	// }
+	// fmt.Println(sql)
 
-	sql, err = Translate("how many foos", s)
+	// sql, err = Translate("how many foos", s)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	t.Fail()
+	// 	return
+	// }
+	// fmt.Println(sql)
+
+	sql, err := Translate("what is the largest val in foos", s)
 	if err != nil {
 		fmt.Println(err.Error())
 		t.Fail()
