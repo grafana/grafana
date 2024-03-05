cce20e50e9cb9a712283fc7ddda60243740b5362
Dan Cech <dcech@grafana.com>
2024-03-05 10:14:38 -0500

    Storage: Watch support (#82282)
    * initial naive implementation
    
    * Update pkg/services/store/entity/sqlstash/sql_storage_server.go
    
    Co-authored-by: Igor Suleymanov <radiohead@users.noreply.github.com>
    
    * tidy up
    
    * add action column, batch watch events
    
    * initial implementation of broadcast-based watcher
    
    * fix up watch init
    
    * remove batching, it just adds needless complexity
    
    * use StreamWatcher
    
    * make broadcaster generic
    
    * add circular buffer to replay recent events to new watchers
    
    * loop within poll until all events are read
    
    * add index on entity_history.resource_version to support poller
    
    * increment r.Since when we send events to consumer
    
    * switch broadcaster and cache to use channels instead of mutexes
    
    * cleanup
    
    ---------
    
    Co-authored-by: Igor Suleymanov <radiohead@users.noreply.github.com>

--- pkg/services/store/entity/db/migrations/entity_store_mig.go
+++ pkg/services/store/entity/db/migrations/entity_store_mig.go
@@ -7,7 +7,7 @@ import (
 )
 
 func initEntityTables(mg *migrator.Migrator) string {
-	marker := "Initialize entity tables (v13)" // changing this key wipe+rewrite everything
+	marker := "Initialize entity tables (v15)" // changing this key wipe+rewrite everything
 	mg.AddMigration(marker, &migrator.RawSQLMigration{})
 
 	tables := []migrator.Table{}
@@ -59,6 +59,8 @@ func initEntityTables(mg *migrator.Migrator) string {
 			{Name: "labels", Type: migrator.DB_Text, Nullable: true},   // JSON object
 			{Name: "fields", Type: migrator.DB_Text, Nullable: true},   // JSON object
 			{Name: "errors", Type: migrator.DB_Text, Nullable: true},   // JSON object
+
+			{Name: "action", Type: migrator.DB_Int, Nullable: false}, // 1: create, 2: update, 3: delete
 		},
 		Indices: []*migrator.Index{
 			// The keys are ordered for efficiency in mysql queries, not URL consistency
@@ -117,6 +119,8 @@ func initEntityTables(mg *migrator.Migrator) string {
 			{Name: "labels", Type: migrator.DB_Text, Nullable: true},   // JSON object
 			{Name: "fields", Type: migrator.DB_Text, Nullable: true},   // JSON object
 			{Name: "errors", Type: migrator.DB_Text, Nullable: true},   // JSON object
+
+			{Name: "action", Type: migrator.DB_Int, Nullable: false}, // 1: create, 2: update, 3: delete
 		},
 		Indices: []*migrator.Index{
 			{Cols: []string{"guid", "resource_version"}, Type: migrator.UniqueIndex},
@@ -125,6 +129,8 @@ func initEntityTables(mg *migrator.Migrator) string {
 				Type: migrator.UniqueIndex,
 				Name: "UQE_entity_history_namespace_group_name_version",
 			},
+			// index to support watch poller
+			{Cols: []string{"resource_version"}, Type: migrator.IndexType},
 		},
 	})
 
