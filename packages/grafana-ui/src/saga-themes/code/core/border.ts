b4d4146aa1d24e92c0df1d3ca4a5eb7688ab82e5
Joao Silva <joao.silva@grafana.com>
2024-03-05 12:09:04 +0000

    Change files to be .ts

---
+++ packages/grafana-ui/src/saga-themes/code/core/border.ts
@@ -0,0 +1,17 @@
+export default {
+  border: {
+    borderRadius: {
+      default: '2px',
+      circle: '100%',
+      pill: '9999px',
+    },
+    borderWidth: {
+      '1': '1px',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '5': '5px',
+      none: '0',
+    },
+  },
+};
---
+++ packages/grafana-ui/src/saga-themes/code/themes/dark.ts
@@ -0,0 +1,27 @@
+export default {
+  color: {
+    background: {
+      canvas: '#111217',
+      primary: '#181b1f',
+      secondary: '#22252b',
+    },
+    text: {
+      primary: '#ccccdc',
+      secondary: 'rgba(204,204,220,0.65)',
+      disabled: 'rgba(204,204,220,0.6)',
+      link: '#6e9fff',
+      maxContrast: '#ffffff',
+      maxContrastInverted: '#24292e',
+    },
+    border: {
+      weak: 'rgba(204,204,220,0.12)',
+      medium: 'rgba(204,204,220,0.2)',
+      strong: 'rgba(204,204,220,0.3)',
+    },
+  },
+  shadow: {
+    z1: '8px *0 8px *0.125 8px *0.25 8px *0 #010409bf',
+    z2: '0 4px 8px 0 #010409bf',
+    z3: '0 8px 24px 0 #010409',
+  },
+};
