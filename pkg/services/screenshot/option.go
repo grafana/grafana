b0c1ea98aa9ed8991d76a62e3b3c373003509f34
Juan Cabanas <juan.cabanas@grafana.com>
2024-03-06 11:39:22 -0300

    option restored

--- pkg/services/screenshot/option.go
+++ pkg/services/screenshot/option.go
@@ -1,7 +1,6 @@
 package screenshot
 
 import (
-	"github.com/grafana/grafana/pkg/services/rendering"
 	"hash/fnv"
 	"strconv"
 	"time"
@@ -18,16 +17,14 @@ var (
 
 // ScreenshotOptions are the options for taking a screenshot.
 type ScreenshotOptions struct {
-	AuthOptions rendering.AuthOpts
-	// OrgID and DashboardUID are required.
+	// OrgID, DashboardUID and PanelID are required.
 	OrgID        int64
 	DashboardUID string
+	PanelID      int64
 
 	// These are optional. From and To must both be set to take effect.
 	// Width, Height, Theme and Timeout inherit their defaults from
 	// DefaultWidth, DefaultHeight, DefaultTheme and DefaultTimeout.
-	// PanelID must be 0 if the screenshot is for the whole dashboard
-	PanelID int64
 	From    string
 	To      string
 	Width   int
