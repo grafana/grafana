80dea90548450411ea2c7db06117cea5bc276311
jev forsberg <jev.forsberg@grafana.com>
2024-03-06 09:03:57 -0700

    leeoniya/histogram-bukkits/ run lint

--- public/app/plugins/panel/histogram/Histogram.tsx
+++ public/app/plugins/panel/histogram/Histogram.tsx
@@ -50,12 +50,16 @@ export interface HistogramProps extends Themeable2 {
 
 export function getBucketSize(frame: DataFrame) {
   // assumes BucketMin is fields[0] and BucktMax is fields[1]
-  return frame.fields[0].type === FieldType.string ? 1 : roundDecimals(frame.fields[1].values[0] - frame.fields[0].values[0], 9);
+  return frame.fields[0].type === FieldType.string
+    ? 1
+    : roundDecimals(frame.fields[1].values[0] - frame.fields[0].values[0], 9);
 }
 
 export function getBucketSize1(frame: DataFrame) {
   // assumes BucketMin is fields[0] and BucktMax is fields[1]
-  return frame.fields[0].type === FieldType.string ? 1 : roundDecimals(frame.fields[1].values[1] - frame.fields[0].values[1], 9);
+  return frame.fields[0].type === FieldType.string
+    ? 1
+    : roundDecimals(frame.fields[1].values[1] - frame.fields[0].values[1], 9);
 }
 
 const prepConfig = (frame: DataFrame, theme: GrafanaTheme2) => {
