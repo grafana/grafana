5acb40f84d45a3137547a36e6923225a713efefd
Luke Yeager <lyeager@nvidia.com>
2024-03-08 15:16:27 -0600

    nullToValueField: clear field.state.calcs on change
    Ensures that reductions on a timeseries properly account for "No value".

--- packages/grafana-data/src/transformations/transformers/nulls/nullToValue.ts
+++ packages/grafana-data/src/transformations/transformers/nulls/nullToValue.ts
@@ -17,15 +17,22 @@ export function nullToValue(frame: DataFrame) {
 
 export function nullToValueField(field: Field, noValue: number) {
   const transformedVals = field.values.slice();
+  let changed = false;
 
   for (let i = 0; i < transformedVals.length; i++) {
     if (transformedVals[i] === null) {
       transformedVals[i] = noValue;
+      changed = true;
     }
   }
 
   return {
     ...field,
+    // clear field.state.calcs if anything changed
+    state: {
+      ...field.state,
+      calcs: changed ? undefined : field.state?.calcs,
+    },
     values: transformedVals,
   };
 }
