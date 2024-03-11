d0943ebb714e2ccd73c833a3efbd9203fea77722
Owen Smallwood <owen.smallwood@grafana.com>
2024-03-11 17:23:07 -0600

    wait for 10 seconds

--- pkg/services/store/entity/tests/server_integration_test.go
+++ pkg/services/store/entity/tests/server_integration_test.go
@@ -134,7 +134,7 @@ func TestIntegrationWatch(t *testing.T) {
 
 		// create watch client and timeout after 5 seconds of waiting for an event
 		ctx := testCtx.ctx
-		ctx, _ = context.WithTimeout(ctx, time.Second*5)
+		ctx, _ = context.WithTimeout(ctx, time.Second*10)
 		watchClient := newWatchClient(t, ctx, testCtx.client, otherKey)
 
 		// create entity
