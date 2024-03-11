47da28b56f862185e8bd5ace83e106dcc54fc640
Giordano Ricci <me@giordanoricci.com>
2024-03-11 12:09:43 +0000

    Chore: Remove deprecated exploreId from QueryEditorProps (#83971)

--- packages/grafana-data/src/types/datasource.ts
+++ packages/grafana-data/src/types/datasource.ts
@@ -429,10 +429,6 @@ export interface QueryEditorProps<
    */
   data?: PanelData;
   range?: TimeRange;
-  /**
-   * @deprecated This is not used anymore and will be removed in a future release.
-   */
-  exploreId?: string;
   history?: Array<HistoryItem<TQuery>>;
   queries?: DataQuery[];
   app?: CoreApp;
