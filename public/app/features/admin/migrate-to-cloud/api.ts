4287e7bd6f4d5c65c87908ef68566a502e45426d
joshhunt <josh@trtr.co>
2024-03-12 17:21:21 +0000

    finish up resources table

--- public/app/features/admin/migrate-to-cloud/api.ts
+++ public/app/features/admin/migrate-to-cloud/api.ts
@@ -1,5 +1,4 @@
 import { BaseQueryFn, createApi } from '@reduxjs/toolkit/query/react';
-import { shuffle } from 'lodash';
 import { lastValueFrom } from 'rxjs';
 
 import { BackendSrvRequest, getBackendSrv } from '@grafana/runtime';
@@ -46,24 +45,51 @@ export interface MigrationResourceDTO {
   uid: string;
   status: 'not-migrated' | 'migrated' | 'migrating' | 'failed';
   statusMessage?: string;
-  type: 'datasource';
+  type: 'datasource' | 'dashboard'; // TODO: in future this would be a discriminated union with the resource details
   resource: {
     uid: string;
     name: string;
     type: string;
+    icon?: string;
   };
 }
 
-const applications = shuffle(['auth-service', 'web server', 'backend']);
-const environments = shuffle(['DEV', 'PROD']);
-const roles = shuffle(['db', 'load-balancer', 'server', 'logs']);
-const dataSources = shuffle(['Prometheus', 'Loki', 'AWS Athena', 'AWS Cloudwatch', 'InfluxDB', 'Elasticsearch']);
+const mockApplications = ['auth-service', 'web server', 'backend'];
+const mockEnvs = ['DEV', 'PROD'];
+const mockRoles = ['db', 'load-balancer', 'server', 'logs'];
+const mockDataSources = ['Prometheus', 'Loki', 'AWS Athena', 'AWS Cloudwatch', 'InfluxDB', 'Elasticsearch'];
 
-const migrationResources: MigrationResourceDTO[] = Array.from({ length: 500 }).map((_, index) => {
-  const dataSource = dataSources[index % dataSources.length];
-  const environment = environments[index % environments.length];
-  const application = applications[index % applications.length];
-  const role = roles[index % roles.length];
+const mockDataSourceMetadata: Record<string, { image: string }> = {
+  Prometheus: {
+    image: 'https://grafana.com/api/plugins/prometheus/versions/5.0.0/logos/small',
+  },
+
+  Loki: {
+    image: 'https://grafana.com/api/plugins/loki/versions/5.0.0/logos/small',
+  },
+
+  'AWS Athena': {
+    image: 'https://grafana.com/api/plugins/grafana-athena-datasource/versions/2.13.5/logos/small',
+  },
+
+  'AWS Cloudwatch': {
+    image: 'https://grafana.com/api/plugins/computest-cloudwatchalarm-datasource/versions/2.0.0/logos/small',
+  },
+
+  InfluxDB: {
+    image: 'https://grafana.com/api/plugins/influxdb/versions/5.0.0/logos/small',
+  },
+
+  Elasticsearch: {
+    image: 'https://grafana.com/api/plugins/elasticsearch/versions/5.0.0/logos/small',
+  },
+};
+
+const mockMigrationResources: MigrationResourceDTO[] = Array.from({ length: 500 }).map((_, index) => {
+  const dataSource = mockDataSources[index % mockDataSources.length];
+  const environment = mockEnvs[index % mockEnvs.length];
+  const application = mockApplications[index % mockApplications.length];
+  const role = mockRoles[index % mockRoles.length];
 
   return {
     status: 'not-migrated',
@@ -72,17 +98,29 @@ const migrationResources: MigrationResourceDTO[] = Array.from({ length: 500 }).m
     resource: {
       uid: `${application}-${environment}-${role}-${index}`,
       name: `${application} ${environment} ${role}`,
+      icon: mockDataSourceMetadata[dataSource]?.image,
       type: dataSource,
     },
   };
 });
 
+mockMigrationResources[0].status = 'migrated';
+mockMigrationResources[1].status = 'failed';
+mockMigrationResources[1].statusMessage = `Source map error: Error: request failed with status 404
+Resource URL: http://localhost:3000/public/build/app.f4d0c6a0daa6a5b14892.js
+Source Map URL: app.f4d0c6a0daa6a5b14892.js.map`;
+mockMigrationResources[2].status = 'migrated';
+mockMigrationResources[3].status = 'migrated';
+mockMigrationResources[4].status = 'migrating';
+mockMigrationResources[5].status = 'migrating';
+
 // TODO remove these mock properties/functions
+const queryParams = new URLSearchParams(window.location.search);
 const MOCK_DELAY_MS = 1000;
 const MOCK_TOKEN = 'TODO_thisWillBeABigLongToken';
 let HAS_MIGRATION_TOKEN = false;
-let HAS_STACK_DETAILS = false;
-let STACK_URL: string | undefined;
+let HAS_STACK_DETAILS = !!queryParams.get('mockStackURL');
+let STACK_URL: string | undefined = queryParams.get('mockStackURL') || undefined;
 
 function dataWithMockDelay<T>(data: T): Promise<{ data: T }> {
   return new Promise((resolve) => {
@@ -108,6 +146,7 @@ export const migrateToCloudAPI = createApi({
         return dataWithMockDelay(responseData);
       },
     }),
+
     connectStack: builder.mutation<void, ConnectStackDTO>({
       invalidatesTags: ['stackDetails'],
       queryFn: async ({ stackURL }) => {
@@ -116,6 +155,7 @@ export const migrateToCloudAPI = createApi({
         return dataWithMockDelay(undefined);
       },
     }),
+
     disconnectStack: builder.mutation<void, void>({
       invalidatesTags: ['stackDetails'],
       queryFn: async () => {
@@ -150,7 +190,7 @@ export const migrateToCloudAPI = createApi({
     listResources: builder.query<MigrationResourceDTO[], void>({
       providesTags: ['resource'],
       queryFn: async () => {
-        return dataWithMockDelay(migrationResources);
+        return dataWithMockDelay(mockMigrationResources);
       },
     }),
   }),
--- public/app/features/admin/migrate-to-cloud/onprem/ResourcesTable.tsx
+++ public/app/features/admin/migrate-to-cloud/onprem/ResourcesTable.tsx
@@ -1,6 +1,8 @@
+import { css } from '@emotion/css';
 import React from 'react';
 
-import { InteractiveTable, CellProps, Stack, Text, Icon } from '@grafana/ui';
+import { InteractiveTable, CellProps, Stack, Text, Icon, useStyles2, Button } from '@grafana/ui';
+import { getSvgSize } from '@grafana/ui/src/components/Icon/utils';
 import { t } from 'app/core/internationalization';
 
 import { MigrationResourceDTO } from '../api';
@@ -22,11 +24,13 @@ export function ResourcesTable({ resources }: ResourcesTableProps) {
 function NameCell(props: CellProps<MigrationResourceDTO>) {
   const data = props.row.original;
   return (
-    <Stack direction="column" gap={0}>
-      <span>
-        {iconForType(data.type)} {data.resource.name}
-      </span>
-      <Text color="secondary">{data.resource.type}</Text>
+    <Stack direction="row" gap={2}>
+      <ResourceIcon resource={data} />
+
+      <Stack direction="column" gap={0}>
+        <span>{data.resource.name}</span>
+        <Text color="secondary">{data.resource.type}</Text>
+      </Stack>
     </Stack>
   );
 }
@@ -37,9 +41,30 @@ function TypeCell(props: CellProps<MigrationResourceDTO>) {
 }
 
 function StatusCell(props: CellProps<MigrationResourceDTO>) {
-  const data = props.row.original;
+  const { status, statusMessage } = props.row.original;
 
-  return <span>{data.status}</span>;
+  if (status === 'not-migrated') {
+    return <Text color="secondary">{t('migrate-to-cloud.resource-status.not-migrated', 'Not yet uploaded')}</Text>;
+  } else if (status === 'migrating') {
+    return <Text color="info">{t('migrate-to-cloud.resource-status.migrating', 'Uploading...')}</Text>;
+  } else if (status === 'migrated') {
+    return <Text color="success">{t('migrate-to-cloud.resource-status.migrated', 'Uploaded to cloud')}</Text>;
+  } else if (status === 'failed') {
+    return (
+      <Stack alignItems="center">
+        <Text color="error">{t('migrate-to-cloud.resource-status.failed', 'Error')}</Text>
+
+        {statusMessage && (
+          // TODO: trigger a proper modal, probably from the parent, on click
+          <Button size="sm" variant="secondary" onClick={() => window.alert(statusMessage)}>
+            {t('migrate-to-cloud.resource-status.error-details-button', 'Details')}
+          </Button>
+        )}
+      </Stack>
+    );
+  }
+
+  return <Text color="secondary">{t('migrate-to-cloud.resource-status.unknown', 'Unknown')}</Text>;
 }
 
 function getTypeName(type: string) {
@@ -50,10 +75,28 @@ function getTypeName(type: string) {
   return t('migrate-to-cloud.table.type-unknown', 'Unknown');
 }
 
-function iconForType(type: string) {
-  if (type === 'datasource') {
-    return <Icon name="database" />;
+function ResourceIcon({ resource }: { resource: MigrationResourceDTO }) {
+  const styles = useStyles2(getIconStyles);
+
+  if (resource.type === 'dashboard') {
+    return <Icon size="xl" name="dashboard" />;
+  }
+
+  if (resource.type === 'datasource' && resource.resource.icon) {
+    return <img className={styles.icon} src={resource.resource.icon} alt="" />;
+  } else if (resource.type === 'datasource') {
+    return <Icon size="xl" name="database" />;
   }
 
   return undefined;
+}
+
+function getIconStyles() {
+  return {
+    icon: css({
+      display: 'block',
+      width: getSvgSize('xl'),
+      height: getSvgSize('xl'),
+    }),
+  };
 }
