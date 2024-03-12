2c2d3ac9db25cb467ed21eb8ca2ccd558760f5fd
Andres Martinez <andres.martinez@grafana.com>
2024-03-12 17:39:18 +0100

    Simplify middleware

--- pkg/registry/apis/datasource/middleware.go
+++ pkg/registry/apis/datasource/middleware.go
@@ -2,20 +2,14 @@ package datasource
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 )
 
-const forwardPluginRequestHTTPHeaders = "contextual-middlewares"
-
 func contextualMiddlewares(ctx context.Context) context.Context {
 	cfg := backend.GrafanaConfigFromContext(ctx)
 	m := httpclient.ResponseLimitMiddleware(cfg.ResponseLimit())
-	mw := httpclient.NamedMiddlewareFunc(forwardPluginRequestHTTPHeaders, func(opts httpclient.Options, next http.RoundTripper) http.RoundTripper {
-		return m.CreateMiddleware(opts, next)
-	})
 
-	return httpclient.WithContextualMiddleware(ctx, mw)
+	return httpclient.WithContextualMiddleware(ctx, m)
 }
