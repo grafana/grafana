1268a27a0c7be3c52a7b4f46ea6f9456e41fead4
Matias Chomicki <matyax@gmail.com>
2024-03-13 10:38:08 +0100

    Prettier

--- public/app/features/logs/components/InfiniteScroll.test.tsx
+++ public/app/features/logs/components/InfiniteScroll.test.tsx
@@ -266,15 +266,14 @@ describe('InfiniteScroll', () => {
       });
 
       describe('Scrolling to the top', () => {
-        test(
-          'Waits for the user to reach the edge before triggering a new query', async () => {
-            const loadMoreMock = jest.fn();
-            const { wheel } = setup(loadMoreMock, 10, rows, order);
-  
-            wheel(-10);
-            expect(loadMoreMock).not.toHaveBeenCalled();
-            wheel(-5);
-            expect(loadMoreMock).toHaveBeenCalled();
+        test('Waits for the user to reach the edge before triggering a new query', async () => {
+          const loadMoreMock = jest.fn();
+          const { wheel } = setup(loadMoreMock, 10, rows, order);
+
+          wheel(-10);
+          expect(loadMoreMock).not.toHaveBeenCalled();
+          wheel(-5);
+          expect(loadMoreMock).toHaveBeenCalled();
         });
       });
     }
