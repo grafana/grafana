236e556ebb3e97daac7c6c53a8dfda2228ae9baa
Matias Chomicki <matyax@gmail.com>
2024-03-13 17:06:06 +0100

    Prettier

--- public/app/features/logs/components/InfiniteScroll.tsx
+++ public/app/features/logs/components/InfiniteScroll.tsx
@@ -29,7 +29,7 @@ export const InfiniteScroll = ({
   scrollElement,
   sortOrder,
   timeZone,
-  topScrollEnabled = false
+  topScrollEnabled = false,
 }: Props) => {
   const [upperOutOfRange, setUpperOutOfRange] = useState(false);
   const [lowerOutOfRange, setLowerOutOfRange] = useState(false);
