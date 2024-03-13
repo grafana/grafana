20d43bc9856423dfd340b6eddb758137a5105392
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-03-12 23:22:26 -0500

    use config editor with package and remove configOverhaul feature toggle

--- public/app/plugins/datasource/prometheus/module.ts
+++ public/app/plugins/datasource/prometheus/module.ts
@@ -9,16 +9,19 @@ import { config } from '@grafana/runtime';
 import PromCheatSheet from './components/PromCheatSheet';
 import PromQueryEditorByApp from './components/PromQueryEditorByApp';
 import { ConfigEditor } from './configuration/ConfigEditor';
+import { ConfigEditor as ConfigEditorPackage } from './configuration/ConfigEditorPackage';
 import { PrometheusDatasource } from './datasource';
 
 const usePackage = config.featureToggles.usePrometheusFrontendPackage;
-const DataSource = usePackage ? PrometheusDatasource : PrometheusDatasourcePackage;
-const QueryEditorByApp = usePackage ? PromQueryEditorByApp : PromQueryEditorByAppPackage;
-const CheatSheet = usePackage ? PromCheatSheet : PromCheatSheetPackage;
+
+const PrometheusDataSourceUsed = usePackage ? PrometheusDatasource : PrometheusDatasourcePackage;
+const PromQueryEditorByAppUsed = usePackage ? PromQueryEditorByApp : PromQueryEditorByAppPackage;
+const ConfigEditorUsed = usePackage ? ConfigEditor : ConfigEditorPackage;
+const PromCheatSheetUsed = usePackage ? PromCheatSheet : PromCheatSheetPackage;
 
 // @ts-ignore
-export const plugin = new DataSourcePlugin(DataSource)
+export const plugin = new DataSourcePlugin(PrometheusDataSourceUsed)
   // @ts-ignore
-  .setQueryEditor(QueryEditorByApp)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditorHelp(CheatSheet);
+  .setQueryEditor(PromQueryEditorByAppUsed)
+  .setConfigEditor(ConfigEditorUsed)
+  .setQueryEditorHelp(PromCheatSheetUsed);
