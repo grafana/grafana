daf7a5deba473732155e9f4fce42c63f9cfb93b6
Kyle Cunningham <kyle@codeincarnate.com>
2024-03-12 22:51:33 -0500

    Another color fix

--- packages/grafana-ui/src/components/Table/styles.ts
+++ packages/grafana-ui/src/components/Table/styles.ts
@@ -56,7 +56,7 @@ export function useTableStyles(theme: GrafanaTheme2, cellHeightOption: TableCell
         wordBreak: textShouldWrap ? 'break-word' : undefined,
         whiteSpace: textShouldWrap && overflowOnHover ? 'normal' : 'nowrap',
         boxShadow: overflowOnHover ? `0 0 2px ${theme.colors.primary.main}` : undefined,
-        background: background ?? undefined,
+        background: 'inherit',
         zIndex: 1,
         '.cellActions': {
           visibility: 'visible',
