5f6d474352efe52fc631a0bf5af06e66c6f996d9
Clarity-89 <homes89@ukr.net>
2024-03-13 08:00:09 +0200

    Use the Form rom core

--- public/app/features/org/UserInviteForm.tsx
+++ public/app/features/org/UserInviteForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 
 import { locationUtil, SelectableValue } from '@grafana/data';
 import { locationService } from '@grafana/runtime';
@@ -20,6 +20,7 @@ import {
 import { getConfig } from 'app/core/config';
 import { OrgRole, useDispatch } from 'app/types';
 
+import { Form } from '../../core/components/Form/Form';
 import { addInvitee } from '../invites/state/actions';
 
 const tooltipMessage = (
@@ -59,12 +60,6 @@ const defaultValues: FormModel = {
 
 export const UserInviteForm = () => {
   const dispatch = useDispatch();
-  const {
-    handleSubmit,
-    register,
-    control,
-    formState: { errors },
-  } = useForm<FormModel>({ defaultValues });
 
   const onSubmit = async (formData: FormModel) => {
     await dispatch(addInvitee(formData)).unwrap();
@@ -72,50 +67,56 @@ export const UserInviteForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} style={{ maxWidth: 600 }}>
-      <FieldSet>
-        <Field
-          invalid={!!errors.loginOrEmail}
-          error={!!errors.loginOrEmail ? 'Email or username is required' : undefined}
-          label="Email or username"
-        >
-          <Input {...register('loginOrEmail', { required: true })} placeholder="email@example.com" />
-        </Field>
-        <Field invalid={!!errors.name} label="Name">
-          <Input {...register('name')} placeholder="(optional)" />
-        </Field>
-        <Field
-          invalid={!!errors.role}
-          label={
-            <Label>
-              <Stack gap={0.5}>
-                <span>Role</span>
-                {tooltipMessage && (
-                  <Tooltip placement="right-end" interactive={true} content={tooltipMessage}>
-                    <Icon name="info-circle" size="xs" />
-                  </Tooltip>
-                )}
-              </Stack>
-            </Label>
-          }
-        >
-          <Controller
-            render={({ field: { ref, ...field } }) => <RadioButtonGroup {...field} options={roles} />}
-            control={control}
-            name="role"
-          />
-        </Field>
-        <Field label="Send invite email">
-          <Switch id="send-email-switch" {...register('sendEmail')} />
-        </Field>
-      </FieldSet>
-      <Stack>
-        <Button type="submit">Submit</Button>
-        <LinkButton href={locationUtil.assureBaseUrl(getConfig().appSubUrl + '/admin/users')} variant="secondary">
-          Back
-        </LinkButton>
-      </Stack>
-    </form>
+    <Form defaultValues={defaultValues} onSubmit={onSubmit}>
+      {({ register, control, errors }) => {
+        return (
+          <>
+            <FieldSet>
+              <Field
+                invalid={!!errors.loginOrEmail}
+                error={!!errors.loginOrEmail ? 'Email or username is required' : undefined}
+                label="Email or username"
+              >
+                <Input {...register('loginOrEmail', { required: true })} placeholder="email@example.com" />
+              </Field>
+              <Field invalid={!!errors.name} label="Name">
+                <Input {...register('name')} placeholder="(optional)" />
+              </Field>
+              <Field
+                invalid={!!errors.role}
+                label={
+                  <Label>
+                    <Stack gap={0.5}>
+                      <span>Role</span>
+                      {tooltipMessage && (
+                        <Tooltip placement="right-end" interactive={true} content={tooltipMessage}>
+                          <Icon name="info-circle" size="xs" />
+                        </Tooltip>
+                      )}
+                    </Stack>
+                  </Label>
+                }
+              >
+                <Controller
+                  render={({ field: { ref, ...field } }) => <RadioButtonGroup {...field} options={roles} />}
+                  control={control}
+                  name="role"
+                />
+              </Field>
+              <Field label="Send invite email">
+                <Switch id="send-email-switch" {...register('sendEmail')} />
+              </Field>
+            </FieldSet>
+            <Stack>
+              <Button type="submit">Submit</Button>
+              <LinkButton href={locationUtil.assureBaseUrl(getConfig().appSubUrl + '/admin/users')} variant="secondary">
+                Back
+              </LinkButton>
+            </Stack>
+          </>
+        );
+      }}
+    </Form>
   );
 };
 
