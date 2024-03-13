9d5636984c4fc912f4613bc6cbb4b8d4c2f34b77
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-03-12 22:29:45 -0500

    add feature toggle logic to Prometheus module

--- public/app/plugins/datasource/prometheus/module.ts
+++ public/app/plugins/datasource/prometheus/module.ts
@@ -1,11 +1,24 @@
 import { DataSourcePlugin } from '@grafana/data';
+import {
+  PrometheusDatasource as PrometheusDatasourcePackage,
+  PromQueryEditorByApp as PromQueryEditorByAppPackage,
+  PromCheatSheet as PromCheatSheetPackage,
+} from '@grafana/prometheus';
+import { config } from '@grafana/runtime';
 
 import PromCheatSheet from './components/PromCheatSheet';
 import PromQueryEditorByApp from './components/PromQueryEditorByApp';
 import { ConfigEditor } from './configuration/ConfigEditor';
 import { PrometheusDatasource } from './datasource';
 
-export const plugin = new DataSourcePlugin(PrometheusDatasource)
-  .setQueryEditor(PromQueryEditorByApp)
+const usePackage = config.featureToggles.usePrometheusFrontendPackage;
+const DataSource = usePackage ? PrometheusDatasource : PrometheusDatasourcePackage;
+const QueryEditorByApp = usePackage ? PromQueryEditorByApp : PromQueryEditorByAppPackage;
+const CheatSheet = usePackage ? PromCheatSheet : PromCheatSheetPackage;
+
+// @ts-ignore
+export const plugin = new DataSourcePlugin(DataSource)
+  // @ts-ignore
+  .setQueryEditor(QueryEditorByApp)
   .setConfigEditor(ConfigEditor)
-  .setQueryEditorHelp(PromCheatSheet);
+  .setQueryEditorHelp(CheatSheet);
