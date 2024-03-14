f10398d0091e33ee201f7d6207ee975e3e291740
Zoltán Bedi <zoltan.bedi@gmail.com>
2024-03-14 11:19:00 +0100

    Scope locators when locating text

--- e2e/plugin-e2e/mysql/mysql.spec.ts
+++ e2e/plugin-e2e/mysql/mysql.spec.ts
@@ -50,7 +50,8 @@ test('visual query builder should handle time filter macro', async ({ explorePag
 
   // Open column selector
   await explorePage.getByTestIdOrAriaLabel(selectors.components.SQLQueryEditor.selectColumn).click();
-  await page.getByText('createdAt').click();
+  const select = page.getByLabel('Select options menu');
+  await select.locator(page.getByText('createdAt')).click();
 
   // Toggle where row
   await page.getByLabel('Filter').click();
@@ -61,15 +62,15 @@ test('visual query builder should handle time filter macro', async ({ explorePag
 
   // Open field selector
   await explorePage.getByTestIdOrAriaLabel(selectors.components.SQLQueryEditor.filterField).click();
-  await page.getByLabel('Select options menu').getByText('createdAt').click();
+  await select.locator(page.getByText('createdAt')).click();
 
   // Open operator selector
   await explorePage.getByTestIdOrAriaLabel(selectors.components.SQLQueryEditor.filterOperator).click();
-  await page.getByText('Macros').click();
+  await select.locator(page.getByText('Macros')).click();
 
   // Open macros value selector
   await explorePage.getByTestIdOrAriaLabel('Macros value selector').click();
-  await page.getByText('timeFilter', { exact: true }).click();
+  await select.locator(page.getByText('timeFilter', { exact: true })).click();
 
   // Validate that the timeFilter macro was added
   await expect(
@@ -77,9 +78,8 @@ test('visual query builder should handle time filter macro', async ({ explorePag
   ).toHaveValue(`SELECT\n  createdAt\nFROM\n  DataMaker.normalTable\nWHERE\n  $__timeFilter(createdAt)\nLIMIT\n  50`);
 
   // Validate that the timeFilter macro was removed when changed to equals operator
-
   await explorePage.getByTestIdOrAriaLabel(selectors.components.SQLQueryEditor.filterOperator).click();
-  await page.getByLabel('Select options menu').getByText('==').click();
+  await select.locator(page.getByText('==')).click();
 
   await explorePage.getByTestIdOrAriaLabel(selectors.components.DateTimePicker.input).click();
   await explorePage.getByTestIdOrAriaLabel(selectors.components.DateTimePicker.input).blur();
