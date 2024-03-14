4351ac61ed206ff67870a6340ba28270278cc26e
Andreas Christou <andreas.christou@grafana.com>
2024-03-14 10:45:11 +0000

    Bump further to 1 day

--- pkg/services/updatechecker/grafana.go
+++ pkg/services/updatechecker/grafana.go
@@ -60,7 +60,7 @@ func (s *GrafanaService) IsDisabled() bool {
 func (s *GrafanaService) Run(ctx context.Context) error {
 	s.instrumentedCheckForUpdates(ctx)
 
-	ticker := time.NewTicker(time.Hour * 2)
+	ticker := time.NewTicker(time.Hour * 24)
 	run := true
 
 	for run {
