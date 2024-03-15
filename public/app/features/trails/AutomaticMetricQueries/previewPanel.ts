417b822dddd9b60c8d5ea1adfffc868d9505cfd7
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2024-03-15 15:27:31 -0400

    fix: data trails ignore usage syntax (#84610)

--- public/app/features/trails/AutomaticMetricQueries/previewPanel.ts
+++ public/app/features/trails/AutomaticMetricQueries/previewPanel.ts
@@ -37,7 +37,7 @@ export function getPreviewPanelFor(metric: string, index: number, currentFilterC
 
 function convertPreviewQueriesToIgnoreUsage(query: PromQuery, currentFilterCount: number) {
   // If there are filters, we append to the list. Otherwise, we replace the empty list.
-  const replacement = currentFilterCount > 0 ? "${filters},__ignore_usage__=''" : "__ignore_usage__=''";
+  const replacement = currentFilterCount > 0 ? '${filters},__ignore_usage__=""' : '__ignore_usage__=""';
 
   const expr = query.expr?.replace('${filters}', replacement);
 
