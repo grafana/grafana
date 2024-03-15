b62e2b9e201d9712c94dba70b46f85a84c1e0b55
spinillos <selenepinillos@gmail.com>
2024-03-15 10:06:33 +0100

    Fix package name

--- pkg/codegen/jenny_go_spec.go
+++ pkg/codegen/jenny_go_spec.go
@@ -21,6 +21,7 @@ func (jenny *GoSpecJenny) JennyName() string {
 func (jenny *GoSpecJenny) Generate(sfg ...SchemaForGen) (codejen.Files, error) {
 	files := make(codejen.Files, len(sfg))
 	for i, v := range sfg {
+		packageName := strings.ToLower(v.Name)
 		b, err := generators.GenerateTypesGo(v.CueFile,
 			&generators.GoConfig{
 				Config: &generators.OpenApiConfig{
@@ -28,7 +29,7 @@ func (jenny *GoSpecJenny) Generate(sfg ...SchemaForGen) (codejen.Files, error) {
 					RootName: "Spec",
 					SubPath:  cue.MakePath(cue.Str("spec")),
 				},
-				PackageName: strings.ToLower(v.Name),
+				PackageName: packageName,
 				ApplyFuncs:  append(jenny.ApplyFuncs, PrefixDropper(v.Name)),
 			},
 		)
@@ -37,7 +38,7 @@ func (jenny *GoSpecJenny) Generate(sfg ...SchemaForGen) (codejen.Files, error) {
 			return nil, err
 		}
 
-		files[i] = *codejen.NewFile(fmt.Sprintf("pkg/kinds/%s/%s_spec_gen.go", v.Name, v.Name), b, jenny)
+		files[i] = *codejen.NewFile(fmt.Sprintf("pkg/kinds/%s/%s_spec_gen.go", packageName, packageName), b, jenny)
 	}
 
 	return files, nil
