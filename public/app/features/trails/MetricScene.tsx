45a39dc61755732d854fa194576f125328c86bcd
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2024-03-15 11:01:35 -0400

    Update public/app/features/trails/MetricScene.tsx

--- public/app/features/trails/MetricScene.tsx
+++ public/app/features/trails/MetricScene.tsx
@@ -113,7 +113,7 @@ const actionViewsDefinitions: ActionViewDefinition[] = [
     displayName: 'Related metrics',
     value: 'related',
     getScene: buildRelatedMetricsScene,
-    description: 'Related metrics are correlated by similarity of their names',
+    description: 'Related metrics are correlated by similarity in name',
   },
 ];
 
