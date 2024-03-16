7c8294cfe3606f68b63757afaab420ad21455cdc
Fabrizio <fabrizio.casati@grafana.com>
2024-03-16 05:48:40 +0100

    Remove useless line

--- public/app/features/plugins/built_in_plugins.ts
+++ public/app/features/plugins/built_in_plugins.ts
@@ -14,7 +14,6 @@ const influxdbPlugin = async () =>
   await import(/* webpackChunkName: "influxdbPlugin" */ 'app/plugins/datasource/influxdb/module');
 const lokiPlugin = async () => await import(/* webpackChunkName: "lokiPlugin" */ 'app/plugins/datasource/loki/module');
 const jaegerPlugin = async () => await import(/* webpackChunkName: "jaegerPlugin" */ '@grafana-plugins/jaeger/module');
-const zipkinPlugin = async () => await import(/* webpackChunkName: "zipkinPlugin" */ '@grafana-plugins/zipkin/module');
 const mixedPlugin = async () =>
   await import(/* webpackChunkName: "mixedPlugin" */ 'app/plugins/datasource/mixed/module');
 const mysqlPlugin = async () =>
