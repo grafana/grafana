7e3b61bbb5d7a8b432fbea987e0f1f81b1d3edb6
Ashley Harrison <ashley.harrison@grafana.com>
2024-03-18 16:38:28 +0000

    fix dropdown keyboard a11y

--- packages/grafana-ui/src/components/Menu/Menu.tsx
+++ packages/grafana-ui/src/components/Menu/Menu.tsx
@@ -27,12 +27,11 @@ const MenuComp = React.forwardRef<HTMLDivElement, MenuProps>(
     const localRef = useRef<HTMLDivElement>(null);
     useImperativeHandle(forwardedRef, () => localRef.current!);
 
-    const [handleKeys] = useMenuFocus({ localRef, onOpen, onClose, onKeyDown });
+    const [handleKeys] = useMenuFocus({ isMenuOpen: true, localRef, onOpen, onClose, onKeyDown });
 
     return (
       <div
         {...otherProps}
-        tabIndex={-1}
         ref={localRef}
         className={styles.wrapper}
         role="menu"
@@ -66,17 +65,21 @@ export const Menu = Object.assign(MenuComp, {
 const getStyles = (theme: GrafanaTheme2) => {
   return {
     header: css({
-      padding: `${theme.spacing(0.5, 1, 1, 1)}`,
+      padding: theme.spacing(0.5, 1, 1, 1),
     }),
     headerBorder: css({
       borderBottom: `1px solid ${theme.colors.border.weak}`,
     }),
     wrapper: css({
-      background: `${theme.colors.background.primary}`,
-      boxShadow: `${theme.shadows.z3}`,
-      display: `inline-block`,
-      borderRadius: `${theme.shape.radius.default}`,
-      padding: `${theme.spacing(0.5, 0)}`,
+      background: theme.colors.background.primary,
+      boxShadow: theme.shadows.z3,
+      display: 'inline-block',
+      borderRadius: theme.shape.radius.default,
+      padding: theme.spacing(0.5, 0),
+
+      '&:focus': {
+        outline: 'none',
+      },
     }),
   };
 };
