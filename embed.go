a963d89e7c11caaf34bf028954af49ff6181e965
spinillos <selenepinillos@gmail.com>
2024-03-19 15:43:33 +0100

    restore embed.go file

---
+++ embed.go
@@ -0,0 +1,10 @@
+package grafana
+
+import (
+	"embed"
+)
+
+// CueSchemaFS embeds all schema-related CUE files in the Grafana project.
+//
+//go:embed cue.mod/module.cue kinds/*.cue kinds/*/*.cue packages/grafana-schema/src/common/*.cue public/app/plugins/*/*/*.cue public/app/plugins/*/*/plugin.json
+var CueSchemaFS embed.FS
