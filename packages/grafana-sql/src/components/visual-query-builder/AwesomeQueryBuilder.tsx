e35aff888e799c81eac57a914806c47f558099e7
Zoltán Bedi <zoltan.bedi@gmail.com>
2024-03-19 23:07:48 +0100

    E2E: Rewrite mysql tests to playwright (#83424)
    * E2E: Rewrite mysql tests to playwright
    
    * Fix lint
    
    * Add more selectors and address comments
    
    * Scope locators when locating text
    
    * Don't run it 20 times
    
    * Update new-datasource-variable to assert mysql

--- packages/grafana-sql/src/components/visual-query-builder/AwesomeQueryBuilder.tsx
+++ packages/grafana-sql/src/components/visual-query-builder/AwesomeQueryBuilder.tsx
@@ -16,6 +16,7 @@ import { isString } from 'lodash';
 import React from 'react';
 
 import { dateTime, toOption } from '@grafana/data';
+import { selectors } from '@grafana/e2e-selectors';
 import { Button, DateTimePicker, Input, Select } from '@grafana/ui';
 
 const buttonLabels = {
@@ -124,6 +125,7 @@ export const settings: Settings = {
       <Select
         id={conjProps?.id}
         aria-label="Conjunction"
+        data-testid={selectors.components.SQLQueryEditor.filterConjunction}
         menuShouldPortal
         options={conjProps?.conjunctionOptions ? Object.keys(conjProps?.conjunctionOptions).map(toOption) : undefined}
         value={conjProps?.selectedConjunction}
@@ -139,6 +141,7 @@ export const settings: Settings = {
         id={fieldProps?.id}
         width={25}
         aria-label="Field"
+        data-testid={selectors.components.SQLQueryEditor.filterField}
         menuShouldPortal
         options={fieldProps?.items.map((f) => {
           // @ts-ignore
@@ -175,6 +178,7 @@ export const settings: Settings = {
       <Select
         options={operatorProps?.items.map((op) => ({ label: op.label, value: op.key }))}
         aria-label="Operator"
+        data-testid={selectors.components.SQLQueryEditor.filterOperator}
         menuShouldPortal
         value={operatorProps?.selectedKey}
         onChange={(val) => {
