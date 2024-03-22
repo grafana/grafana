31a8184387f22e9b09234a5d1ad198955f644f4d
André Pereira <adrapereira@gmail.com>
2024-03-22 13:19:13 +0000

    Secondary solid button

--- public/app/features/trails/TrailStore/utils.tsx
+++ public/app/features/trails/TrailStore/utils.tsx
@@ -6,14 +6,14 @@ import { createSuccessNotification } from '../../../core/copy/appNotification';
 import { HOME_ROUTE } from '../shared';
 
 export function createBookmarkSavedNotification() {
-  const notification = createSuccessNotification('Exploration successfully saved');
+  const notification = createSuccessNotification('Bookmark created');
 
   notification.component = (
     <Stack gap={2} direction={'row'}>
       <div>
         You can view bookmarks under <i>Explore &gt; Metrics</i>
       </div>
-      <LinkButton fill={'text'} variant={'primary'} href={HOME_ROUTE}>
+      <LinkButton fill={'solid'} variant={'secondary'} href={HOME_ROUTE}>
         View bookmarks
       </LinkButton>
     </Stack>
