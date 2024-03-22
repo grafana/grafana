7f11d552eb03877e5e7dcdac33d9230517bbee9e
Leon Sorokin <leeoniya@gmail.com>
2024-03-21 22:46:58 -0500

    use old

--- public/app/plugins/panel/xychart/module.tsx
+++ public/app/plugins/panel/xychart/module.tsx
@@ -1,45 +1,45 @@
-// import { PanelPlugin } from '@grafana/data';
-// import { commonOptionsBuilder } from '@grafana/ui';
+import { PanelPlugin } from '@grafana/data';
+import { commonOptionsBuilder } from '@grafana/ui';
 
-// import { AutoEditor } from './AutoEditor';
-// import { ManualEditor } from './ManualEditor';
-// import { XYChartPanel } from './XYChartPanel';
-// import { getScatterFieldConfig } from './config';
-// import { Options, FieldConfig, defaultFieldConfig } from './panelcfg.gen';
+import { AutoEditor } from './AutoEditor';
+import { ManualEditor } from './ManualEditor';
+import { XYChartPanel } from './XYChartPanel';
+import { getScatterFieldConfig } from './config';
+import { Options, FieldConfig, defaultFieldConfig } from './panelcfg.gen';
 
-// export const plugin = new PanelPlugin<Options, FieldConfig>(XYChartPanel)
-//   .useFieldConfig(getScatterFieldConfig(defaultFieldConfig))
-//   .setPanelOptions((builder) => {
-//     builder
-//       .addRadio({
-//         path: 'seriesMapping',
-//         name: 'Series mapping',
-//         defaultValue: 'auto',
-//         settings: {
-//           options: [
-//             { value: 'auto', label: 'Table', description: 'Plot values within a single table result' },
-//             { value: 'manual', label: 'Manual', description: 'Construct values from any result' },
-//           ],
-//         },
-//       })
-//       .addCustomEditor({
-//         id: 'xyPlotConfig',
-//         path: 'dims',
-//         name: '',
-//         editor: AutoEditor,
-//         showIf: (cfg) => cfg.seriesMapping === 'auto',
-//       })
-//       .addCustomEditor({
-//         id: 'series',
-//         path: 'series',
-//         name: '',
-//         defaultValue: [],
-//         editor: ManualEditor,
-//         showIf: (cfg) => cfg.seriesMapping === 'manual',
-//       });
+export const plugin = new PanelPlugin<Options, FieldConfig>(XYChartPanel)
+  .useFieldConfig(getScatterFieldConfig(defaultFieldConfig))
+  .setPanelOptions((builder) => {
+    builder
+      .addRadio({
+        path: 'seriesMapping',
+        name: 'Series mapping',
+        defaultValue: 'auto',
+        settings: {
+          options: [
+            { value: 'auto', label: 'Table', description: 'Plot values within a single table result' },
+            { value: 'manual', label: 'Manual', description: 'Construct values from any result' },
+          ],
+        },
+      })
+      .addCustomEditor({
+        id: 'xyPlotConfig',
+        path: 'dims',
+        name: '',
+        editor: AutoEditor,
+        showIf: (cfg) => cfg.seriesMapping === 'auto',
+      })
+      .addCustomEditor({
+        id: 'series',
+        path: 'series',
+        name: '',
+        defaultValue: [],
+        editor: ManualEditor,
+        showIf: (cfg) => cfg.seriesMapping === 'manual',
+      });
 
-//     commonOptionsBuilder.addTooltipOptions(builder, true);
-//     commonOptionsBuilder.addLegendOptions(builder);
-//   });
+    commonOptionsBuilder.addTooltipOptions(builder, true);
+    commonOptionsBuilder.addLegendOptions(builder);
+  });
 
-  export { plugin } from './v2/module';
+  // export { plugin } from './v2/module';
