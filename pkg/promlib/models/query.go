02aa1dc68493bf673f6a0fb2dbb910e4332d70ab
ismail simsek <ismailsimsek09@gmail.com>
2024-03-25 15:54:20 +0100

    fix linting

--- pkg/promlib/models/query.go
+++ pkg/promlib/models/query.go
@@ -11,7 +11,6 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/gtime"
 	"github.com/prometheus/prometheus/model/labels"
 
-	"github.com/grafana/grafana/pkg/apis/scope/v0alpha1"
 	"github.com/grafana/grafana/pkg/promlib/intervalv2"
 )
 
@@ -154,7 +153,7 @@ type Query struct {
 	RangeQuery    bool
 	ExemplarQuery bool
 	UtcOffsetSec  int64
-	Scope         *v0alpha1.ScopeSpec
+	Scope         *ScopeSpec
 }
 
 type Scope struct {
