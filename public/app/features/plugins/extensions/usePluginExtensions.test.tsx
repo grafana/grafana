06bfe1f8a2279659a72c997cb74597560c6423a2
Levente Balogh <balogh.levente.hu@gmail.com>
2024-03-25 11:58:01 +0100

    tests: add more tests for the usePluginExtensions() hook

--- public/app/features/plugins/extensions/usePluginExtensions.test.tsx
+++ public/app/features/plugins/extensions/usePluginExtensions.test.tsx
@@ -60,10 +60,10 @@ describe('usePluginExtensions()', () => {
     const extensionPointId = 'plugins/foo/bar';
     const pluginId = 'my-app-plugin';
     const usePluginExtensions = createPluginExtensionsHook(reactiveRegistry);
-    let hookRender = renderHook(() => usePluginExtensions({ extensionPointId }));
+    let { result, rerender } = renderHook(() => usePluginExtensions({ extensionPointId }));
 
     // No extensions yet
-    expect(hookRender.result.current.extensions.length).toBe(0);
+    expect(result.current.extensions.length).toBe(0);
 
     // Add extensions to the registry
     act(() => {
@@ -89,9 +89,137 @@ describe('usePluginExtensions()', () => {
     });
 
     // Check if the hook returns the new extensions
-    hookRender = renderHook(() => usePluginExtensions({ extensionPointId }));
-    expect(hookRender.result.current.extensions.length).toBe(2);
-    expect(hookRender.result.current.extensions[0].title).toBe('1');
-    expect(hookRender.result.current.extensions[1].title).toBe('2');
+    rerender();
+
+    expect(result.current.extensions.length).toBe(2);
+    expect(result.current.extensions[0].title).toBe('1');
+    expect(result.current.extensions[1].title).toBe('2');
+  });
+
+  it('should only render the hook once', () => {
+    const spy = jest.spyOn(reactiveRegistry, 'asObservable');
+    const extensionPointId = 'plugins/foo/bar';
+    const usePluginExtensions = createPluginExtensionsHook(reactiveRegistry);
+
+    renderHook(() => usePluginExtensions({ extensionPointId }));
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return the same extensions object if the context object is the same', () => {
+    const extensionPointId = 'plugins/foo/bar';
+    const pluginId = 'my-app-plugin';
+    const usePluginExtensions = createPluginExtensionsHook(reactiveRegistry);
+
+    // Add extensions to the registry
+    act(() => {
+      reactiveRegistry.register({
+        pluginId,
+        extensionConfigs: [
+          {
+            type: PluginExtensionTypes.link,
+            extensionPointId,
+            title: '1',
+            description: '1',
+            path: `/a/${pluginId}/2`,
+          },
+          {
+            type: PluginExtensionTypes.link,
+            extensionPointId,
+            title: '2',
+            description: '2',
+            path: `/a/${pluginId}/2`,
+          },
+        ],
+      });
+    });
+
+    // Check if it returns the same extensions object in case nothing changes
+    const context = {};
+    const firstResults = renderHook(() => usePluginExtensions({ extensionPointId, context }));
+    const secondResults = renderHook(() => usePluginExtensions({ extensionPointId, context }));
+    expect(firstResults.result.current.extensions === secondResults.result.current.extensions).toBe(true);
+  });
+
+  it('should return a new extensions object if the context object is different', () => {
+    const extensionPointId = 'plugins/foo/bar';
+    const pluginId = 'my-app-plugin';
+    const usePluginExtensions = createPluginExtensionsHook(reactiveRegistry);
+
+    // Add extensions to the registry
+    act(() => {
+      reactiveRegistry.register({
+        pluginId,
+        extensionConfigs: [
+          {
+            type: PluginExtensionTypes.link,
+            extensionPointId,
+            title: '1',
+            description: '1',
+            path: `/a/${pluginId}/2`,
+          },
+          {
+            type: PluginExtensionTypes.link,
+            extensionPointId,
+            title: '2',
+            description: '2',
+            path: `/a/${pluginId}/2`,
+          },
+        ],
+      });
+    });
+
+    // Check if it returns a different extensions object in case the context object changes
+    const firstResults = renderHook(() => usePluginExtensions({ extensionPointId, context: {} }));
+    const secondResults = renderHook(() => usePluginExtensions({ extensionPointId, context: {} }));
+    expect(firstResults.result.current.extensions === secondResults.result.current.extensions).toBe(false);
+  });
+
+  it('should return a new extensions object if the registry changes but the context object is the same', () => {
+    const extensionPointId = 'plugins/foo/bar';
+    const pluginId = 'my-app-plugin';
+    const context = {};
+    const usePluginExtensions = createPluginExtensionsHook(reactiveRegistry);
+
+    // Add the first extension
+    act(() => {
+      reactiveRegistry.register({
+        pluginId,
+        extensionConfigs: [
+          {
+            type: PluginExtensionTypes.link,
+            extensionPointId,
+            title: '1',
+            description: '1',
+            path: `/a/${pluginId}/2`,
+          },
+        ],
+      });
+    });
+
+    const { result, rerender } = renderHook(() => usePluginExtensions({ extensionPointId, context }));
+    const firstExtensions = result.current.extensions;
+
+    // Add the second extension
+    act(() => {
+      reactiveRegistry.register({
+        pluginId,
+        extensionConfigs: [
+          {
+            type: PluginExtensionTypes.link,
+            extensionPointId,
+            // extensionPointId: 'plugins/foo/bar/zed', // A different extension point (to be sure that it's also returning a new object when the actual extension point doesn't change)
+            title: '2',
+            description: '2',
+            path: `/a/${pluginId}/2`,
+          },
+        ],
+      });
+    });
+
+    rerender();
+
+    const secondExtensions = result.current.extensions;
+
+    expect(firstExtensions === secondExtensions).toBe(false);
   });
 });
