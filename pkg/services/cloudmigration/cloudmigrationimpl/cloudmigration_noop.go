b589a203d9e5b5f6c7676540b9fe2c574b836407
idafurjes <36131195+idafurjes@users.noreply.github.com>
2024-03-25 13:30:47 +0100

    Grafana: define the api for the grafana cloudmigration api (not the csm api) (#84430)
    * Add cloud migration endpoints
    
    * Built auth into creating a migration.
    
    * Added more detail to the migration result model
    
    * goimports
    
    * Update pkg/services/cloudmigration/api/api.go
    
    Co-authored-by: lean.dev <34773040+leandro-deveikis@users.noreply.github.com>
    
    * Update pkg/services/cloudmigration/api/api.go
    
    Co-authored-by: lean.dev <34773040+leandro-deveikis@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leonard Gram <leo@xlson.com>
    Co-authored-by: lean.dev <34773040+leandro-deveikis@users.noreply.github.com>

--- pkg/services/cloudmigration/cloudmigrationimpl/cloudmigration_noop.go
+++ pkg/services/cloudmigration/cloudmigrationimpl/cloudmigration_noop.go
@@ -14,3 +14,47 @@ var _ cloudmigration.Service = (*NoopServiceImpl)(nil)
 func (s *NoopServiceImpl) MigrateDatasources(ctx context.Context, request *cloudmigration.MigrateDatasourcesRequest) (*cloudmigration.MigrateDatasourcesResponse, error) {
 	return nil, cloudmigration.ErrFeatureDisabledError
 }
+
+func (s *NoopServiceImpl) CreateToken(ctx context.Context) error {
+	return cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) ValidateToken(ctx context.Context, token string) error {
+	return cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) SaveEncryptedToken(ctx context.Context, token string) error {
+	return cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) GetMigration(ctx context.Context, id int64) (*cloudmigration.CloudMigrationResponse, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) GetMigrationList(ctx context.Context) ([]cloudmigration.CloudMigrationResponse, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) CreateMigration(ctx context.Context, cm cloudmigration.CloudMigrationRequest) (*cloudmigration.CloudMigrationResponse, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) UpdateMigration(ctx context.Context, id int64, cm cloudmigration.CloudMigrationRequest) (*cloudmigration.CloudMigrationResponse, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) RunMigration(ctx context.Context, uid string) (*cloudmigration.CloudMigrationRun, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) GetMigrationStatus(ctx context.Context, id string, runID string) (*cloudmigration.CloudMigrationRun, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) GetMigrationStatusList(ctx context.Context, id string) ([]cloudmigration.CloudMigrationRun, error) {
+	return nil, cloudmigration.ErrFeatureDisabledError
+}
+
+func (s *NoopServiceImpl) DeleteMigration(ctx context.Context, id string) error {
+	return cloudmigration.ErrFeatureDisabledError
+}
