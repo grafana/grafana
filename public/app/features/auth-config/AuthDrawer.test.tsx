75ff1ccd1efbff6ef99f1c4f25ef155653b68030
linoman <2051016+linoman@users.noreply.github.com>
2024-03-26 10:25:50 +0100

    Replace the test render for redux-rtl

--- public/app/features/auth-config/AuthDrawer.test.tsx
+++ public/app/features/auth-config/AuthDrawer.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
+import { render } from 'test/redux-rtl';
 
 import { AuthDrawerUnconnected, Props } from './AuthDrawer';
 
