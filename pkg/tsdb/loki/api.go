0b8f0611d73a2c212116cafc3b0a80c451dc163b
Sven Grossmann <svennergr@gmail.com>
2024-03-26 20:48:14 +0100

    comment

--- pkg/tsdb/loki/api.go
+++ pkg/tsdb/loki/api.go
@@ -22,7 +22,6 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	// "github.com/grafana/grafana/pkg/infra/tracing"
 
 	"github.com/grafana/grafana/pkg/promlib/converter"
 	"github.com/grafana/grafana/pkg/tsdb/loki/instrumentation"
