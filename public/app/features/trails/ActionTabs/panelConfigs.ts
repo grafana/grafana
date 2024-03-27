b9b8f8716aebdb2a06eb97f1cb5ee096b0d72fc4
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2024-03-27 12:50:02 -0400

    datatrails: avoid double scrollbar with new viz tooltips (#85197)
    fix: avoid double scrollbar

--- public/app/features/trails/ActionTabs/panelConfigs.ts
+++ public/app/features/trails/ActionTabs/panelConfigs.ts
@@ -3,6 +3,6 @@ import { SortOrder } from '@grafana/schema/dist/esm/index';
 import { TooltipDisplayMode } from '@grafana/ui';
 
 export const breakdownPanelOptions = PanelOptionsBuilders.timeseries()
-  .setOption('tooltip', { mode: TooltipDisplayMode.Multi, sort: SortOrder.Descending, maxHeight: 250 })
+  .setOption('tooltip', { mode: TooltipDisplayMode.Multi, sort: SortOrder.Descending })
   .setOption('legend', { showLegend: false })
   .build();
