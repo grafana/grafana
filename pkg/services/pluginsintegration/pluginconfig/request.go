82fda39f0f6547d3569d14e98d360876d6b604fe
Will Browne <wbrowne@users.noreply.github.com>
2024-03-27 18:19:11 +0100

    Plugins: Send PDC file paths and contents for backwards compatibility (#85287)
    * send paths and contents
    
    * go work sync

--- pkg/services/pluginsintegration/pluginconfig/request.go
+++ pkg/services/pluginsintegration/pluginconfig/request.go
@@ -74,8 +74,11 @@ func (s *RequestConfigProvider) PluginRequestConfig(ctx context.Context, pluginI
 	if s.cfg.ProxySettings.Enabled {
 		m[proxy.PluginSecureSocksProxyEnabled] = "true"
 		m[proxy.PluginSecureSocksProxyClientCert] = s.cfg.ProxySettings.ClientCertFilePath
+		m[proxy.PluginSecureSocksProxyClientCertContents] = s.cfg.ProxySettings.ClientCert
 		m[proxy.PluginSecureSocksProxyClientKey] = s.cfg.ProxySettings.ClientKeyFilePath
+		m[proxy.PluginSecureSocksProxyClientKeyContents] = s.cfg.ProxySettings.ClientKey
 		m[proxy.PluginSecureSocksProxyRootCAs] = strings.Join(s.cfg.ProxySettings.RootCAFilePaths, " ")
+		m[proxy.PluginSecureSocksProxyRootCAsContents] = strings.Join(s.cfg.ProxySettings.RootCAs, ",")
 		m[proxy.PluginSecureSocksProxyProxyAddress] = s.cfg.ProxySettings.ProxyAddress
 		m[proxy.PluginSecureSocksProxyServerName] = s.cfg.ProxySettings.ServerName
 		m[proxy.PluginSecureSocksProxyAllowInsecure] = strconv.FormatBool(s.cfg.ProxySettings.AllowInsecure)
