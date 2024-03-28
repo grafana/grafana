9fc9ff107c778526988ec1a4a0e43c2dae40ad36
Leandro Deveikis <leandro.deveikis@gmail.com>
2024-03-28 12:19:38 -0300

    adding new column to store items

--- pkg/services/cloudmigration/cloudmigrationimpl/xorm_store.go
+++ pkg/services/cloudmigration/cloudmigrationimpl/xorm_store.go
@@ -2,6 +2,8 @@ package cloudmigrationimpl
 
 import (
 	"context"
+	"errors"
+	"strconv"
 
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/services/cloudmigration"
@@ -22,4 +24,32 @@ func (ss *sqlStore) GetAllCloudMigrations(ctx context.Context) ([]*cloudmigratio
 		return nil, err
 	}
 	return migrations, nil
+}
+
+func (ss *sqlStore) GetAllCloudMigrationRuns(ctx context.Context) ([]*cloudmigration.CloudMigrationRun, error) {
+	var runs = make([]*cloudmigration.CloudMigrationRun, 0)
+	err := ss.db.WithDbSession(ctx, func(sess *db.Session) error { return sess.Find(&runs) })
+	if err != nil {
+		return nil, err
+	}
+	return runs, nil
+}
+
+// TODO REview this, for now is for testing
+func (ss *sqlStore) SaveCloudMigrationRun(ctx context.Context, run *cloudmigration.CloudMigrationRun) (*cloudmigration.CloudMigrationRun, error) {
+	err := ss.db.WithDbSession(ctx, func(sess *db.Session) error {
+		inserted, err := sess.Insert(run)
+		if err != nil {
+			return err
+		}
+		if inserted != 1 {
+			return errors.New("Error inserting CloudMigrationRun, expected 1 row affected but got: " + strconv.FormatInt(inserted, 10))
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return run, nil
 }
