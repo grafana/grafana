e468a846374e5f9934e1d7330416f2ab9ab37f8a
Michael Mandrus <michael.mandrus@grafana.com>
2024-03-29 10:22:35 -0400

    fix interface

--- pkg/services/cloudmigration/cloudmigrationimpl/store.go
+++ pkg/services/cloudmigration/cloudmigrationimpl/store.go
@@ -9,7 +9,6 @@ import (
 type store interface {
 	GetMigration(context.Context, int64) (*cloudmigration.CloudMigration, error)
 	SaveMigrationRun(context.Context, *cloudmigration.CloudMigrationRun) error
-	MigrateDatasources(context.Context, *cloudmigration.MigrateDatasourcesRequest) (*cloudmigration.MigrateDatasourcesResponse, error)
 	CreateMigration(ctx context.Context, token cloudmigration.CloudMigration) error
 	GetAllCloudMigrations(ctx context.Context) ([]*cloudmigration.CloudMigration, error)
 }
