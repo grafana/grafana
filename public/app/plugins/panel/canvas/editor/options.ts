6bcf10d342433a953500dbcbddcc05b0fdf5b58e
nmarrs <nathanielmarrs@gmail.com>
2024-03-29 23:45:18 -0700

    hide background image size editor for svg based elements as a "good enough" measure and to ensure that we dont have editor options that dont work on release

--- public/app/plugins/panel/canvas/editor/options.ts
+++ public/app/plugins/panel/canvas/editor/options.ts
@@ -2,6 +2,7 @@ import { capitalize } from 'lodash';
 
 import { PanelOptionsSupplier } from '@grafana/data/src/panel/PanelPlugin';
 import { CanvasConnection, CanvasElementOptions, ConnectionDirection } from 'app/features/canvas';
+import { SVGElements } from 'app/features/canvas/runtime/element';
 import { ColorDimensionEditor, ResourceDimensionEditor, ScaleDimensionEditor } from 'app/features/dimensions/editors';
 import { BackgroundSizeEditor } from 'app/features/dimensions/editors/BackgroundSizeEditor';
 
@@ -60,6 +61,14 @@ export const optionBuilder: OptionSuppliers = {
         editor: BackgroundSizeEditor,
         settings: {
           resourceType: 'image',
+        },
+        showIf: () => {
+          // Do not show image size editor for SVG based elements
+          if (context.options?.type) {
+            return !SVGElements.has(context.options.type);
+          }
+
+          return true;
         },
       });
   },
