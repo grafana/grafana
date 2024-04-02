7251237f1963acf2c306d0ea880487a0a2eaf1bc
Kyle Cunningham <kyle@codeincarnate.com>
2024-04-02 16:22:20 -0600

    Add applicability function to group to nested table

--- packages/grafana-data/src/transformations/transformers/utils.ts
+++ packages/grafana-data/src/transformations/transformers/utils.ts
@@ -1,3 +1,22 @@
+import { DataFrame } from "../../types";
+
 export const transformationsVariableSupport = () => {
   return (window as any)?.grafanaBootData?.settings?.featureToggles?.transformationsVariableSupport;
 };
+
+/**
+ * Retrieve the maximum number of fields in a series of a dataframe.
+ */
+export function findMaxFields(data: DataFrame[]) {
+  let maxFields = 0;
+
+  // Group to nested table needs at least two fields
+  // a field to group on and to show in the nested table
+  for (const frame of data) {
+    if (frame.fields.length > maxFields) {
+      maxFields = frame.fields.length;
+    }
+  }
+
+  return maxFields;
+}
