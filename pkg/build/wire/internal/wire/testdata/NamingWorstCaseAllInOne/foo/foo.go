cd6b274e530be93cb4715f8cb78fb80ddd8dde5c
gamab <gabriel.mabille@grafana.com>
2024-04-02 15:09:15 +0200

    Force revert on test data

--- pkg/build/wire/internal/wire/testdata/NamingWorstCaseAllInOne/foo/foo.go
+++ pkg/build/wire/internal/wire/testdata/NamingWorstCaseAllInOne/foo/foo.go
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build wireinject
-// +build wireinject
+//+build wireinject
 
 // This file is specifically designed to cause issues with copying the
 // AST, particularly with the identifier "context".
