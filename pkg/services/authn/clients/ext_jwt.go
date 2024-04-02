b7c9dec642cb9e9491ed736e63300a7ddf6d3822
jguer <me@jguer.space>
2024-04-02 09:15:21 +0200

    fix parameter names

--- pkg/services/authn/clients/ext_jwt.go
+++ pkg/services/authn/clients/ext_jwt.go
@@ -94,8 +94,8 @@ func (s *ExtendedJWT) Authenticate(ctx context.Context, r *authn.Request) (*auth
 func (s *ExtendedJWT) authenticateAsUser(idTokenClaims,
 	accessTokenClaims *ExtendedJWTClaims) (*authn.Identity, error) {
 	// Only allow access policies to impersonate
-	if !strings.HasPrefix(claims.Subject, fmt.Sprintf("%s:", authn.NamespaceAccessPolicy)) {
-		s.log.Error("Invalid subject", "subject", claims.Subject)
+	if !strings.HasPrefix(accessTokenClaims.Subject, fmt.Sprintf("%s:", authn.NamespaceAccessPolicy)) {
+		s.log.Error("Invalid subject", "subject", accessTokenClaims.Subject)
 		return nil, errJWTInvalid.Errorf("Failed to parse sub: %s", "invalid subject format")
 	}
 	// Allow only user impersonation
