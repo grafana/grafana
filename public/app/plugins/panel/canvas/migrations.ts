378db275c0d61e774d593a10101e1a5483954028
Ihor Yeromin <yeryomin.igor@gmail.com>
2024-04-02 17:38:47 +0200

    fix(canvas): ellips migration

--- public/app/plugins/panel/canvas/migrations.ts
+++ public/app/plugins/panel/canvas/migrations.ts
@@ -25,9 +25,15 @@ export const canvasMigrationHandler = (panel: PanelModel): Partial<Options> => {
       for (const element of root.elements) {
         if (element.type === 'ellipse') {
           // Take existing ellipse specific background and border config and apply it to the element's general background and border config
-          element.background = element.config.backgroundColor;
-          element.border.color = element.config.borderColor;
-          element.border.width = element.config.width;
+          if (element.config.backgroundColor) {
+            element.background = element.config.backgroundColor;
+          }
+          if (element.config.borderColor) {
+            element.border.color = element.config.borderColor;
+          }
+          if (element.config.width) {
+            element.border.width = element.config.width;
+          }
 
           // Remove the ellipse specific background and border config
           delete element.config.backgroundColor;
