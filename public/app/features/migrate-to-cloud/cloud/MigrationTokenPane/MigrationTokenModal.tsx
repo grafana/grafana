19daae97e24a707791dc68d3fd176dbdd02cf126
Josh Hunt <joshhunt@users.noreply.github.com>
2024-04-03 09:55:38 +0100

    E2C: Hook Cloud Receiver-side up to real CreateCloudMigrationToken API (#85490)
    E2C: Hook Cloud Reciever-side up to real CreateCloudMigrationToken API

--- public/app/features/migrate-to-cloud/cloud/MigrationTokenPane/MigrationTokenModal.tsx
+++ public/app/features/migrate-to-cloud/cloud/MigrationTokenPane/MigrationTokenModal.tsx
@@ -3,43 +3,54 @@ import React, { useId } from 'react';
 import { Modal, Button, Input, Stack, ClipboardButton, Field } from '@grafana/ui';
 import { Trans, t } from 'app/core/internationalization';
 
+import { TokenErrorAlert } from '../TokenErrorAlert';
+
 interface Props {
   hideModal: () => void;
-  migrationToken: string;
+  migrationToken?: string;
 }
 
 export const MigrationTokenModal = ({ hideModal, migrationToken }: Props) => {
-  const inputId = useId();
-
   return (
     <Modal
       isOpen
       title={t('migrate-to-cloud.migration-token.modal-title', 'Migration token created')}
       onDismiss={hideModal}
     >
-      <Field
-        description={t(
-          'migrate-to-cloud.migration-token.modal-field-description',
-          'Copy the token now as you will not be able to see it again. Losing a token requires creating a new one.'
-        )}
-        htmlFor={inputId}
-        label={t('migrate-to-cloud.migration-token.modal-field-label', 'Token')}
-      >
-        <Stack>
-          <Input id={inputId} value={migrationToken} readOnly />
-          <ClipboardButton icon="clipboard-alt" getText={() => migrationToken}>
-            <Trans i18nKey="migrate-to-cloud.migration-token.modal-copy-button">Copy to clipboard</Trans>
-          </ClipboardButton>
-        </Stack>
-      </Field>
+      {migrationToken ? <TokenSuccessContent migrationToken={migrationToken} /> : <TokenErrorAlert />}
+
       <Modal.ButtonRow>
         <Button variant="secondary" onClick={hideModal}>
           <Trans i18nKey="migrate-to-cloud.migration-token.modal-close">Close</Trans>
         </Button>
-        <ClipboardButton variant="primary" getText={() => migrationToken} onClipboardCopy={hideModal}>
-          <Trans i18nKey="migrate-to-cloud.migration-token.modal-copy-and-close">Copy to clipboard and close</Trans>
-        </ClipboardButton>
+
+        {migrationToken && (
+          <ClipboardButton variant="primary" getText={() => migrationToken} onClipboardCopy={hideModal}>
+            <Trans i18nKey="migrate-to-cloud.migration-token.modal-copy-and-close">Copy to clipboard and close</Trans>
+          </ClipboardButton>
+        )}
       </Modal.ButtonRow>
     </Modal>
   );
 };
+
+function TokenSuccessContent({ migrationToken }: { migrationToken: string }) {
+  const inputId = useId();
+  return (
+    <Field
+      description={t(
+        'migrate-to-cloud.migration-token.modal-field-description',
+        'Copy the token now as you will not be able to see it again. Losing a token requires creating a new one.'
+      )}
+      htmlFor={inputId}
+      label={t('migrate-to-cloud.migration-token.modal-field-label', 'Token')}
+    >
+      <Stack>
+        <Input id={inputId} value={migrationToken} readOnly />
+        <ClipboardButton icon="clipboard-alt" getText={() => migrationToken}>
+          <Trans i18nKey="migrate-to-cloud.migration-token.modal-copy-button">Copy to clipboard</Trans>
+        </ClipboardButton>
+      </Stack>
+    </Field>
+  );
+}
