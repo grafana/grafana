a4a7ab7aa4c38e3d08b6442ce79cd8f596b825da
suntala <arati.rana@grafana.com>
2024-04-03 14:01:20 +0200

    Add Create functionality to each of the mode-specific DualWriters

--- pkg/apiserver/rest/dualwriter_mode1.go
+++ pkg/apiserver/rest/dualwriter_mode1.go
@@ -1,11 +1,30 @@
 package rest
 
+import (
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/rest"
+)
+
 type DualWriterMode1 struct {
 	DualWriter
 }
 
 // NewDualWriterMode1 returns a new DualWriter in mode 1.
-// Mode 1 represents writing to legacy storage and reading from legacy storage.
+// Mode 1 represents writing to and reading from LegacyStorage.
 func NewDualWriterMode1(legacy LegacyStorage, storage Storage) *DualWriterMode1 {
 	return &DualWriterMode1{*newDualWriter(legacy, storage)}
+}
+
+// Create overrides the default behavior of the DualWriter and writes only to LegacyStorage.
+func (d *DualWriterMode1) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
+	legacy, ok := d.legacy.(rest.Creater)
+	if !ok {
+		return nil, fmt.Errorf("legacy storage rest.Creater is missing")
+	}
+
+	return legacy.Create(ctx, obj, createValidation, options)
 }
