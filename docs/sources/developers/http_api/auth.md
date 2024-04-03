b10a58326a2139ca9c8d0eb490bbd1b5289bdd78
Eric Leijonmarck <eric.leijonmarck@gmail.com>
2024-04-03 17:57:07 +0200

    API keys: Disabling of API keys endpoints

--- docs/sources/developers/http_api/auth.md
+++ docs/sources/developers/http_api/auth.md
@@ -29,6 +29,8 @@ If you use Grafana v9.1 or newer, use service accounts instead of API keys. For 
 
 ## List API keys
 
+### DEPRECATED
+
 `GET /api/auth/keys`
 
 **Required permissions**
@@ -55,72 +57,23 @@ Query Parameters:
 **Example Response**:
 
 ```http
-HTTP/1.1 200
+HTTP/1.1 301
 Content-Type: application/json
 
-[
-  {
-    "id": 3,
-    "name": "API",
-    "role": "Admin"
-  },
-  {
-    "id": 1,
-    "name": "TestAdmin",
-    "role": "Admin",
-    "expiration": "2019-06-26T10:52:03+03:00"
-  }
-]
+""
 ```
 
 ## Create API Key
 
+### DEPRECATED
+
 `POST /api/auth/keys`
 
-**Required permissions**
-
-See note in the [introduction]({{< ref "#authentication-api" >}}) for an explanation.
-
-| Action           | Scope |
-| ---------------- | ----- |
-| `apikeys:create` | n/a   |
-
-**Example Request**:
-
-```http
-POST /api/auth/keys HTTP/1.1
-Accept: application/json
-Content-Type: application/json
-Authorization: Bearer eyJrIjoiT0tTcG1pUlY2RnVKZTFVaDFsNFZXdE9ZWmNrMkZYbk
-
-{
-  "name": "mykey",
-  "role": "Admin",
-  "secondsToLive": 86400
-}
-```
-
-JSON Body schema:
-
-- **name** – The key name
-- **role** – Sets the access level/Grafana Role for the key. Can be one of the following values: `Viewer`, `Editor` or `Admin`.
-- **secondsToLive** – Sets the key expiration in seconds. It is optional. If it is a positive number an expiration date for the key is set. If it is null, zero or is omitted completely (unless `api_key_max_seconds_to_live` configuration option is set) the key will never expire.
-
-Error statuses:
-
-- **400** – `api_key_max_seconds_to_live` is set but no `secondsToLive` is specified or `secondsToLive` is greater than this value.
-- **500** – The key was unable to be stored in the database.
-
-**Example Response**:
-
-```http
-HTTP/1.1 200
-Content-Type: application/json
-
-{"name":"mykey","key":"eyJrIjoiWHZiSWd3NzdCYUZnNUtibE9obUpESmE3bzJYNDRIc0UiLCJuIjoibXlrZXkiLCJpZCI6MX1=","id":1}
-```
+- **301** – `api_key_max_seconds_to_live` is set but no `secondsToLive` is specified or `secondsToLive` is greater than this value.
 
 ## Delete API Key
+
+### DEPRECATED
 
 `DELETE /api/auth/keys/:id`
 
@@ -144,8 +97,8 @@ Authorization: Bearer eyJrIjoiT0tTcG1pUlY2RnVKZTFVaDFsNFZXdE9ZWmNrMkZYbk
 **Example Response**:
 
 ```http
-HTTP/1.1 200
+HTTP/1.1 301
 Content-Type: application/json
 
-{"message":"API key deleted"}
+""
 ```
--- pkg/api/apikey.go
+++ pkg/api/apikey.go
@@ -1,20 +1,14 @@
 package api
 
 import (
-	"errors"
 	"net/http"
-	"strconv"
-	"time"
 
-	"github.com/grafana/grafana/pkg/api/dtos"
 	"github.com/grafana/grafana/pkg/api/response"
-	"github.com/grafana/grafana/pkg/components/apikeygen"
 	"github.com/grafana/grafana/pkg/services/apikey"
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
-	"github.com/grafana/grafana/pkg/web"
 )
 
-// swagger:route GET /auth/keys api_keys getAPIkeys
+// swagger:route GET /auth/keys api_keys
 //
 // Get auth keys.
 //
@@ -26,48 +20,17 @@ import (
 // see https://grafana.com/docs/grafana/next/administration/api-keys/#migrate-api-keys-to-grafana-service-accounts-using-the-api.
 //
 // Responses:
-// 200: getAPIkeyResponse
-// 401: unauthorisedError
-// 403: forbiddenError
-// 404: notFoundError
-// 500: internalServerError
+// 301: StatusMovedPermanently
 func (hs *HTTPServer) GetAPIKeys(c *contextmodel.ReqContext) response.Response {
-	query := apikey.GetApiKeysQuery{OrgID: c.SignedInUser.GetOrgID(), User: c.SignedInUser, IncludeExpired: c.QueryBool("includeExpired")}
+	// Set the Location header to the new URL
+	c.Context.Resp.Header().Set("Location", "/api/serviceaccounts/tokens")
 
-	keys, err := hs.apiKeyService.GetAPIKeys(c.Req.Context(), &query)
-	if err != nil {
-		return response.Error(http.StatusInternalServerError, "Failed to list api keys", err)
-	}
-
-	ids := map[string]bool{}
-	result := make([]*dtos.ApiKeyDTO, len(keys))
-	for i, t := range keys {
-		ids[strconv.FormatInt(t.ID, 10)] = true
-		var expiration *time.Time = nil
-		if t.Expires != nil {
-			v := time.Unix(*t.Expires, 0)
-			expiration = &v
-		}
-		result[i] = &dtos.ApiKeyDTO{
-			ID:         t.ID,
-			Name:       t.Name,
-			Role:       t.Role,
-			Expiration: expiration,
-			LastUsedAt: t.LastUsedAt,
-		}
-	}
-
-	metadata := hs.getMultiAccessControlMetadata(c, "apikeys:id", ids)
-	if len(metadata) > 0 {
-		for _, key := range result {
-			key.AccessControl = metadata[strconv.FormatInt(key.ID, 10)]
-		}
-	}
-
-	return response.JSON(http.StatusOK, result)
+	// Respond with a 301 Moved Permanently status code
+	// the Location header is enough for clients to know where to go next.
+	return response.JSON(http.StatusMovedPermanently, nil)
 }
 
-// swagger:route DELETE /auth/keys/{id} api_keys deleteAPIkey
+// swagger:route DELETE /auth/keys/{id} api_keys
 //
 // Delete API key.
 //
@@ -76,33 +39,17 @@ func (hs *HTTPServer) GetAPIKeys(c *contextmodel.ReqContext) response.Response {
 //
 // Deprecated: true
 // Responses:
-// 200: okResponse
-// 401: unauthorisedError
-// 403: forbiddenError
-// 404: notFoundError
-// 500: internalServerError
+// 301: StatusMovedPermanently
 func (hs *HTTPServer) DeleteAPIKey(c *contextmodel.ReqContext) response.Response {
-	id, err := strconv.ParseInt(web.Params(c.Req)[":id"], 10, 64)
-	if err != nil {
-		return response.Error(http.StatusBadRequest, "id is invalid", err)
-	}
+	// Set the Location header to the new URL
+	c.Context.Resp.Header().Set("Location", "/api/serviceaccounts/tokens")
 
-	cmd := &apikey.DeleteCommand{ID: id, OrgID: c.SignedInUser.GetOrgID()}
-	err = hs.apiKeyService.DeleteApiKey(c.Req.Context(), cmd)
-	if err != nil {
-		var status int
-		if errors.Is(err, apikey.ErrNotFound) {
-			status = http.StatusNotFound
-		} else {
-			status = http.StatusInternalServerError
-		}
-		return response.Error(status, "Failed to delete API key", err)
-	}
-
-	return response.Success("API key deleted")
+	// Respond with a 301 Moved Permanently status code
+	// the Location header is enough for clients to know where to go next.
+	return response.JSON(http.StatusMovedPermanently, nil)
 }
 
-// swagger:route POST /auth/keys api_keys addAPIkey
+// swagger:route POST /auth/keys api_keys
 //
 // Creates an API key.
 //
@@ -114,59 +61,14 @@ func (hs *HTTPServer) DeleteAPIKey(c *contextmodel.ReqContext) response.Response
 // see: https://grafana.com/docs/grafana/next/administration/api-keys/#migrate-api-keys-to-grafana-service-accounts-using-the-api.
 //
 // Responses:
-// 200: postAPIkeyResponse
-// 400: badRequestError
-// 401: unauthorisedError
-// 403: forbiddenError
-// 409: conflictError
-// 500: internalServerError
+// 301: StatusMovedPermanently
 func (hs *HTTPServer) AddAPIKey(c *contextmodel.ReqContext) response.Response {
-	cmd := apikey.AddCommand{}
-	if err := web.Bind(c.Req, &cmd); err != nil {
-		return response.Error(http.StatusBadRequest, "bad request data", err)
-	}
-	if !cmd.Role.IsValid() {
-		return response.Error(http.StatusBadRequest, "Invalid role specified", nil)
-	}
-	if !c.SignedInUser.GetOrgRole().Includes(cmd.Role) {
-		return response.Error(http.StatusForbidden, "Cannot assign a role higher than user's role", nil)
-	}
+	// Set the Location header to the new URL
+	c.Context.Resp.Header().Set("Location", "/api/serviceaccounts/tokens")
 
-	if hs.Cfg.ApiKeyMaxSecondsToLive != -1 {
-		if cmd.SecondsToLive == 0 {
-			return response.Error(http.StatusBadRequest, "Number of seconds before expiration should be set", nil)
-		}
-		if cmd.SecondsToLive > hs.Cfg.ApiKeyMaxSecondsToLive {
-			return response.Error(http.StatusBadRequest, "Number of seconds before expiration is greater than the global limit", nil)
-		}
-	}
-
-	cmd.OrgID = c.SignedInUser.GetOrgID()
-
-	newKeyInfo, err := apikeygen.New(cmd.OrgID, cmd.Name)
-	if err != nil {
-		return response.Error(http.StatusInternalServerError, "Generating API key failed", err)
-	}
-
-	cmd.Key = newKeyInfo.HashedKey
-	key, err := hs.apiKeyService.AddAPIKey(c.Req.Context(), &cmd)
-	if err != nil {
-		if errors.Is(err, apikey.ErrInvalidExpiration) {
-			return response.Error(http.StatusBadRequest, err.Error(), nil)
-		}
-		if errors.Is(err, apikey.ErrDuplicate) {
-			return response.Error(http.StatusConflict, err.Error(), nil)
-		}
-		return response.Error(http.StatusInternalServerError, "Failed to add API Key", err)
-	}
-
-	result := &dtos.NewApiKeyResult{
-		ID:   key.ID,
-		Name: key.Name,
-		Key:  newKeyInfo.ClientSecret,
-	}
-
-	return response.JSON(http.StatusOK, result)
+	// Respond with a 301 Moved Permanently status code
+	// the Location header is enough for clients to know where to go next.
+	return response.JSON(http.StatusMovedPermanently, nil)
 }
 
 // swagger:parameters getAPIkeys
@@ -183,25 +85,4 @@ type AddAPIkeyParams struct {
 	// in:body
 	// required:true
 	Body apikey.AddCommand
-}
-
-// swagger:parameters deleteAPIkey
-type DeleteAPIkeyParams struct {
-	// in:path
-	// required:true
-	ID int64 `json:"id"`
-}
-
-// swagger:response getAPIkeyResponse
-type GetAPIkeyResponse struct {
-	// The response message
-	// in: body
-	Body []*dtos.ApiKeyDTO `json:"body"`
-}
-
-// swagger:response postAPIkeyResponse
-type PostAPIkeyResponse struct {
-	// The response message
-	// in: body
-	Body dtos.NewApiKeyResult `json:"body"`
 }
