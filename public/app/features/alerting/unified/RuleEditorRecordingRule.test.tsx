2674043cefd574f9a7e300b49d5ccd43a43e40c2
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-04-04 11:50:01 +0100

    Add whatwg-fetch and fixes to alerting tests that break when polyfill is added

--- public/app/features/alerting/unified/RuleEditorRecordingRule.test.tsx
+++ public/app/features/alerting/unified/RuleEditorRecordingRule.test.tsx
@@ -1,5 +1,7 @@
+import 'whatwg-fetch';
 import { screen, waitFor, waitForElementToBeRemoved, within } from '@testing-library/react';
 import userEvent, { PointerEventsCheckLevel } from '@testing-library/user-event';
+import { setupServer } from 'msw/node';
 import React from 'react';
 import { renderRuleEditor, ui } from 'test/helpers/alertingRuleEditor';
 import { clickSelectOption } from 'test/helpers/selectOptionInTest';
@@ -7,6 +9,7 @@ import { byText } from 'testing-library-selector';
 
 import { setDataSourceSrv } from '@grafana/runtime';
 import { contextSrv } from 'app/core/services/context_srv';
+import { mockApi } from 'app/features/alerting/unified/mockApi';
 import { AccessControlAction } from 'app/types';
 import { PromApplication } from 'app/types/unified-alerting-dto';
 
@@ -93,8 +96,12 @@ const mocks = {
 
 const getLabelInput = (selector: HTMLElement) => within(selector).getByRole('combobox');
 
+const server = setupServer();
+
 describe('RuleEditor recording rules', () => {
   beforeEach(() => {
+    mockApi(server).eval({ results: {} });
+    server.listen();
     jest.clearAllMocks();
     contextSrv.isEditor = true;
     contextSrv.hasEditPermissionInFolders = true;
@@ -111,6 +118,14 @@ describe('RuleEditor recording rules', () => {
       AccessControlAction.AlertingRuleExternalRead,
       AccessControlAction.AlertingRuleExternalWrite,
     ]);
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => {
+    server.close();
   });
 
   it('can create a new cloud recording rule', async () => {
