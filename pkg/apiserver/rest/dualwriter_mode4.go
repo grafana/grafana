19ad2c362b1e97ac338f32c9e6a6a41f4b04a5cd
suntala <arati.rana@grafana.com>
2024-04-04 12:55:36 +0200

    Add Create to mode 4

--- pkg/apiserver/rest/dualwriter_mode4.go
+++ pkg/apiserver/rest/dualwriter_mode4.go
@@ -1,5 +1,13 @@
 package rest
 
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/rest"
+)
+
 type DualWriterMode4 struct {
 	DualWriter
 }
@@ -8,4 +16,10 @@ type DualWriterMode4 struct {
 // Mode 4 represents writing and reading from Storage.
 func NewDualWriterMode4(legacy LegacyStorage, storage Storage) *DualWriterMode4 {
 	return &DualWriterMode4{*newDualWriter(legacy, storage)}
+}
+
+// Create overrides the default behavior of the DualWriter and writes only to Storage.
+// #TODO remove this once we remove the default DualWriter implementation
+func (d *DualWriterMode4) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
+	return d.Storage.Create(ctx, obj, createValidation, options)
 }
