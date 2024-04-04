97113f0504c54df3b8b2ecad51875ffc8dd298b9
Arati R <33031346+suntala@users.noreply.github.com>
2024-04-04 14:02:51 +0200

    Storage: Add mode-specific dual writers (#85551)
    * Set up skeleton dual writers for each mode
    * Add Create functionality to each of the mode-specific DualWriters
    * Add switch for selecting DualWriter

---
+++ pkg/apiserver/rest/dualwriter_mode3.go
@@ -0,0 +1,39 @@
+package rest
+
+import (
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/rest"
+	"k8s.io/klog"
+)
+
+type DualWriterMode3 struct {
+	DualWriter
+}
+
+// NewDualWriterMode3 returns a new DualWriter in mode 3.
+// Mode 3 represents writing to LegacyStorage and Storage and reading from Storage.
+func NewDualWriterMode3(legacy LegacyStorage, storage Storage) *DualWriterMode3 {
+	return &DualWriterMode3{*NewDualWriter(legacy, storage)}
+}
+
+// Create overrides the default behavior of the DualWriter and writes to LegacyStorage and Storage.
+func (d *DualWriterMode3) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
+	legacy, ok := d.legacy.(rest.Creater)
+	if !ok {
+		return nil, fmt.Errorf("legacy storage rest.Creater is missing")
+	}
+
+	created, err := d.Storage.Create(ctx, obj, createValidation, options)
+	if err != nil {
+		return created, err
+	}
+
+	if _, err := legacy.Create(ctx, obj, createValidation, options); err != nil {
+		klog.Error("unable to create object in legacy storage", "error", err)
+	}
+	return created, nil
+}
