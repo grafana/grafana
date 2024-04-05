3ce0c9ca7bebf3b8d33418e397d7b10635f72526
Adela Almasan <88068998+adela-almasan@users.noreply.github.com>
2024-04-05 16:39:10 -0600

    Annotations: Fix annotation title (#85614)

--- public/app/plugins/panel/timeseries/plugins/annotations2/AnnotationTooltip2.tsx
+++ public/app/plugins/panel/timeseries/plugins/annotations2/AnnotationTooltip2.tsx
@@ -54,9 +54,9 @@ export const AnnotationTooltip2 = ({ annoVals, annoIdx, timeZone, onEdit }: Prop
       </div>
     );
 
-    // alertText = alertDef.getAlertAnnotationInfo(annotation); // @TODO ??
+    alertText = annoVals.data?.[annoIdx] ? alertDef.getAlertAnnotationText(annoVals.data[annoIdx]) : '';
   } else if (annoVals.title?.[annoIdx]) {
-    text = annoVals.title[annoIdx] + text ? `<br />${text}` : '';
+    text = annoVals.title[annoIdx] + (text ? `<br />${text}` : '');
   }
 
   return (
