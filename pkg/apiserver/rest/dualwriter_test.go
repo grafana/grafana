116bcb8395bec2c3db78a3c112606ffd3e3d14c2
leonorfmartins <leonorfmartins@gmail.com>
2024-04-05 10:21:51 +0200

    no need for t.Run()

--- pkg/apiserver/rest/dualwriter_test.go
+++ pkg/apiserver/rest/dualwriter_test.go
@@ -11,21 +11,19 @@ import (
 const kind = "dummy"
 
 func Test_Mode1(t *testing.T) {
-	t.Run("mode 1", func(t *testing.T) {
-		var ls = (LegacyStorage)(nil)
-		var s = (Storage)(nil)
-		lsSpy := NewLegacyStorageSpyClient(ls)
-		sSpy := NewStorageSpyClient(s)
+	var ls = (LegacyStorage)(nil)
+	var s = (Storage)(nil)
+	lsSpy := NewLegacyStorageSpyClient(ls)
+	sSpy := NewStorageSpyClient(s)
 
-		dw := NewDualWriterMode1(lsSpy, sSpy)
+	dw := NewDualWriterMode1(lsSpy, sSpy)
 
-		_, err := dw.Get(context.Background(), kind, &metav1.GetOptions{})
-		assert.NoError(t, err)
+	_, err := dw.Get(context.Background(), kind, &metav1.GetOptions{})
+	assert.NoError(t, err)
 
-		// it should use the Legacy Get implementation
-		assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.Get"))
-		assert.Equal(t, 0, sSpy.Counts("Storage.Get"))
-	})
+	// it should use the Legacy Get implementation
+	assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.Get"))
+	assert.Equal(t, 0, sSpy.Counts("Storage.Get"))
 }
 
 func Test_Mode2(t *testing.T) {
