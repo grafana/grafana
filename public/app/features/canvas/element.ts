7a6feb1dd9d574b57dd22a60aee0b85ff277bc56
Adela Almasan <88068998+adela-almasan@users.noreply.github.com>
2024-04-05 08:53:40 -0600

    Canvas: Add support for line animation (#85556)

--- public/app/features/canvas/element.ts
+++ public/app/features/canvas/element.ts
@@ -5,6 +5,7 @@ import { PanelOptionsSupplier } from '@grafana/data/src/panel/PanelPlugin';
 import { ColorDimensionConfig, ScaleDimensionConfig } from '@grafana/schema';
 import { config } from 'app/core/config';
 
+import { LineStyleConfig } from '../../plugins/panel/canvas/editor/LineStyleEditor';
 import { DimensionContext } from '../dimensions';
 
 import { BackgroundConfig, Constraint, LineConfig, Placement, StandardEditorConfig } from './types';
@@ -56,7 +57,7 @@ export interface CanvasConnection {
   path: ConnectionPath;
   color?: ColorDimensionConfig;
   size?: ScaleDimensionConfig;
-  lineStyle?: string;
+  lineStyle?: LineStyleConfig;
   vertices?: ConnectionCoordinates[];
   radius?: ScaleDimensionConfig;
   direction?: ConnectionDirection;
