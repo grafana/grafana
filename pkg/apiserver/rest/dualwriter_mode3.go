2c1e959e10e4d28b5b8daf3b7d8cf186ac986767
Leonor Oliveira <9090754+leonorfmartins@users.noreply.github.com>
2024-04-08 15:59:42 +0200

    Update pkg/apiserver/rest/dualwriter_mode3.go
    Co-authored-by: Dan Cech <dcech@grafana.com>

--- pkg/apiserver/rest/dualwriter_mode3.go
+++ pkg/apiserver/rest/dualwriter_mode3.go
@@ -56,7 +56,5 @@ func (d *DualWriterMode3) Get(ctx context.Context, name string, options *metav1.
 		return nil, err
 	}
 
-	fmt.Println("Resource not found in Unified Storage. Getting it from Legacy Storage.")
-
 	return legacy.Get(ctx, name, &metav1.GetOptions{})
 }
