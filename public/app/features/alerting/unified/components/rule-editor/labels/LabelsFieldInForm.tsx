e5f4fc37562e42d79050b37ebd12bff4d5a24504
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2024-04-08 09:27:04 +0200

    Use button instead of icon for editing labels

--- public/app/features/alerting/unified/components/rule-editor/labels/LabelsFieldInForm.tsx
+++ public/app/features/alerting/unified/components/rule-editor/labels/LabelsFieldInForm.tsx
@@ -1,9 +1,7 @@
-import { css } from '@emotion/css';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { Badge, Button, Icon, Stack, Text, useStyles2 } from '@grafana/ui';
+import { Badge, Button, Stack, Text } from '@grafana/ui';
 
 import { RuleFormValues } from '../../../types/rule-form';
 import { NeedHelpInfo } from '../NeedHelpInfo';
@@ -14,13 +12,12 @@ interface LabelsFieldInFormProps {
   onEditClick: () => void;
 }
 export function LabelsFieldInForm({ onEditClick }: LabelsFieldInFormProps) {
-  const styles = useStyles2(getStyles);
   const { watch } = useFormContext<RuleFormValues>();
   const labels = watch('labels');
   const hasLabels = Object.keys(labels).length > 0 && labels.some((label) => label.key || label.value);
 
   return (
-    <div>
+    <Stack direction="column" gap={2}>
       <Stack direction="column" gap={1}>
         <Text element="h5">Labels</Text>
         <Stack direction={'row'} gap={1}>
@@ -37,7 +34,10 @@ export function LabelsFieldInForm({ onEditClick }: LabelsFieldInFormProps) {
       <Stack direction="row" gap={1} alignItems="center">
         <LabelsInRule />
         {hasLabels ? (
-          <Icon name={'pen'} onClick={onEditClick} className={styles.editIcon} />
+          <Button variant="secondary" type="button">
+            {' '}
+            Edit labels{' '}
+          </Button>
         ) : (
           <Stack direction="row" gap={1}>
             <Badge color="orange" text=" No labels selected " />
@@ -47,17 +47,6 @@ export function LabelsFieldInForm({ onEditClick }: LabelsFieldInFormProps) {
           </Stack>
         )}
       </Stack>
-    </div>
+    </Stack>
   );
 }
-
-const getStyles = (theme: GrafanaTheme2) => {
-  return {
-    labelsContainer: css({
-      marginBottom: theme.spacing(3),
-    }),
-    editIcon: css({
-      cursor: 'pointer',
-    }),
-  };
-};
