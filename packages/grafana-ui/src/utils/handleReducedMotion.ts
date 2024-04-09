d4d93642578936cd11956c64b2afab2d77fd76ac
eledobleefe <laura.fernandez@grafana.com>
2024-04-09 12:27:16 +0200

    Fix info

--- packages/grafana-ui/src/utils/handleReducedMotion.ts
+++ packages/grafana-ui/src/utils/handleReducedMotion.ts
@@ -3,7 +3,7 @@ import { CSSInterpolation } from '@emotion/css';
 /**
  * @param styles - Regular styles.
  * @param reducedMotionStyles - Styles to apply when `prefers-reduced-motion` is enabled.
- * If not @param reducedMotionStyles is not provided, there won't be any animation or transition shown.
+ * If @param reducedMotionStyles is not provided, there won't be any animation or transition shown.
  */
 export const handleReducedMotion = (styles: CSSInterpolation, reducedMotionStyles?: CSSInterpolation) => {
   const result: Record<string, CSSInterpolation> = {
