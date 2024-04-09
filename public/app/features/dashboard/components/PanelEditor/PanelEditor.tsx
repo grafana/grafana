3ecfceec401a8b3aeecd5d5ff9932d0d6d69806b
Ieva <ieva.vasiljeva@grafana.com>
2024-04-09 17:09:41 +0100

    RBAC: hide save button for users who can't save dashboard (#85758)
    hide save button for users who can't save dashboard

--- public/app/features/dashboard/components/PanelEditor/PanelEditor.tsx
+++ public/app/features/dashboard/components/PanelEditor/PanelEditor.tsx
@@ -333,27 +333,28 @@ export class PanelEditorUnconnected extends PureComponent<Props> {
       >
         Discard
       </Button>,
-      this.props.panel.libraryPanel ? (
-        <Button
-          onClick={this.onSaveLibraryPanel}
-          variant="primary"
-          size={size}
-          title="Apply changes and save library panel"
-          key="save-panel"
-        >
-          Save library panel
-        </Button>
-      ) : (
-        <Button
-          onClick={this.onSaveDashboard}
-          title="Apply changes and save dashboard"
-          key="save"
-          size={size}
-          variant="secondary"
-        >
-          Save
-        </Button>
-      ),
+      this.props.dashboard.meta.canSave &&
+        (this.props.panel.libraryPanel ? (
+          <Button
+            onClick={this.onSaveLibraryPanel}
+            variant="primary"
+            size={size}
+            title="Apply changes and save library panel"
+            key="save-panel"
+          >
+            Save library panel
+          </Button>
+        ) : (
+          <Button
+            onClick={this.onSaveDashboard}
+            title="Apply changes and save dashboard"
+            key="save"
+            size={size}
+            variant="secondary"
+          >
+            Save
+          </Button>
+        )),
       <Button
         onClick={this.onBack}
         variant="primary"
