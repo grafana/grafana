ac0b48a752fa844bce78751125300eac1040851a
Torkel Ödegaard <torkel@grafana.com>
2024-04-09 17:10:39 +0200

    EmbeddedDashboard: Fix padding issue

--- public/app/features/dashboard-scene/embedding/EmbeddedDashboardTestPage.tsx
+++ public/app/features/dashboard-scene/embedding/EmbeddedDashboardTestPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import { PageLayoutType } from '@grafana/data';
-import { Box } from '@grafana/ui';
+import { Box, Drawer } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 
 import { EmbeddedDashboard } from './EmbeddedDashboard';
@@ -15,8 +15,12 @@ export function EmbeddedDashboardTestPage() {
       pageNav={{ text: 'Embedding dashboard', subTitle: 'Showing dashboard: Panel Tests - Pie chart' }}
       layout={PageLayoutType.Canvas}
     >
-      <Box paddingY={2}>Internal url state: {state}</Box>
-      <EmbeddedDashboard uid="lVE-2YFMz" initialState={state} onStateChange={setState} />
+      <Drawer title="test" onClose={() => {}}>
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <Box paddingY={2}>Internal url state: {state}</Box>
+          <EmbeddedDashboard uid="O6GmNPvWk" initialState={state} onStateChange={setState} />
+        </div>
+      </Drawer>
     </Page>
   );
 }
