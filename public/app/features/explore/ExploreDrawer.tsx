5305316f5abfee0e3f5990c3ee39ec4fed9e6b6b
Kristina <kristina.durivage@grafana.com>
2024-04-09 07:36:46 -0500

    Explore: Move Query History to be screen wide (#84321)
    * WIP
    
    * Use splitpanewrapper for drawer
    
    * Get rich history pulling from multiple datasources
    
    * highlight pane
    
    * Fix datasource data handling
    
    * create ds/explore map, move around ds lookup
    
    * Handle no filters
    
    * Fix tests and some errors
    
    * Fix context menu issue
    
    * (Poorly) enable scrolling, fix onClose to function
    
    * Remove highlighting, use legacy key, fix casing
    
    * fix filtering to handle non-simple data
    
    * Fix linter, add translations
    
    * Fixing tests~~
    
    * Move to explore drawer and fix some more tests
    
    * Kinda fix drawer stuff?
    
    * Fix remaining card tests
    
    * Fix test
    
    * Fix tests
    
    * Partially fix starred tab tests
    
    * Fix integration tests
    
    * Fix remaining tests 🤞
    
    * Add a test and a clarifying comment behind a couple hooks
    
    * Remove unused code
    
    * Fix button styling and fix animation (but break width)
    
    * Make Drawer using parent width (100%)
    
    * Fix tests and some small catches
    
    * Add tests for selectExploreDSMaps selector
    
    ---------
    
    Co-authored-by: Piotr Jamroz <pm.jamroz@gmail.com>

--- public/app/features/explore/ExploreDrawer.tsx
+++ public/app/features/explore/ExploreDrawer.tsx
@@ -8,22 +8,20 @@ import { GrafanaTheme2 } from '@grafana/data';
 import { getDragStyles, useStyles2, useTheme2 } from '@grafana/ui';
 
 export interface Props {
-  width: number;
   children: React.ReactNode;
   onResize?: ResizeCallback;
 }
 
 export function ExploreDrawer(props: Props) {
-  const { width, children, onResize } = props;
+  const { children, onResize } = props;
   const theme = useTheme2();
   const styles = useStyles2(getStyles);
   const dragStyles = getDragStyles(theme);
-  const drawerWidth = `${width + 31.5}px`;
 
   return (
     <Resizable
       className={cx(styles.fixed, styles.container, styles.drawerActive)}
-      defaultSize={{ width: drawerWidth, height: `${theme.components.horizontalDrawer.defaultHeight}px` }}
+      defaultSize={{ width: '100%', height: `${theme.components.horizontalDrawer.defaultHeight}px` }}
       handleClasses={{ top: dragStyles.dragHandleHorizontal }}
       enable={{
         top: true,
@@ -36,8 +34,6 @@ export function ExploreDrawer(props: Props) {
         topLeft: false,
       }}
       maxHeight="100vh"
-      maxWidth={drawerWidth}
-      minWidth={drawerWidth}
       onResize={onResize}
     >
       {children}
@@ -58,13 +54,12 @@ const drawerSlide = (theme: GrafanaTheme2) => keyframes`
 const getStyles = (theme: GrafanaTheme2) => ({
   // @ts-expect-error csstype doesn't allow !important. see https://github.com/frenic/csstype/issues/114
   fixed: css({
-    position: 'fixed !important',
+    position: 'absolute !important',
   }),
   container: css({
     bottom: 0,
     background: theme.colors.background.primary,
     borderTop: `1px solid ${theme.colors.border.weak}`,
-    margin: theme.spacing(0, -2, 0, -2),
     boxShadow: theme.shadows.z3,
     zIndex: theme.zIndex.navbarFixed,
   }),
