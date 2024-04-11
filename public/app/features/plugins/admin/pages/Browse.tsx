1c8a402ad83c7f3d09318a42e05b0b93b18cd51d
Levente Balogh <balogh.levente.hu@gmail.com>
2024-04-11 13:54:05 +0200

    Wip

--- public/app/features/plugins/admin/pages/Browse.tsx
+++ public/app/features/plugins/admin/pages/Browse.tsx
@@ -69,7 +69,7 @@ export default function Browse({ route }: GrafanaRouteComponentProps): ReactElem
 
   const subTitle = (
     <div>
-      Extend the Grafana experience with panel plugins and apps. To find more data sources go to{' '}
+      (Update) Extend the Grafana experience with panel plugins and apps. To find more data sources go to{' '}
       <a className="external-link" href={`${CONNECTIONS_ROUTES.AddNewConnection}?cat=data-source`}>
         Connections
       </a>
