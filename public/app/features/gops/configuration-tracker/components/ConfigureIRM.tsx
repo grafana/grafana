d9777d6ef821b3ff53572d369ef6aca0bf34a3cf
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2024-04-11 12:21:56 +0200

    Do not hide any card in irm page, and refactor

--- public/app/features/gops/configuration-tracker/components/ConfigureIRM.tsx
+++ public/app/features/gops/configuration-tracker/components/ConfigureIRM.tsx
@@ -2,23 +2,163 @@ import { css } from '@emotion/css';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { getBackendSrv } from '@grafana/runtime';
 import { Button, Card, Icon, IconName, useStyles2 } from '@grafana/ui';
+import { alertRuleApi } from 'app/features/alerting/unified/api/alertRuleApi';
+import { alertmanagerApi } from 'app/features/alerting/unified/api/alertmanagerApi';
+import { OnCallIntegrationDTO, onCallApi } from 'app/features/alerting/unified/api/onCallApi';
+import { usePluginBridge } from 'app/features/alerting/unified/hooks/usePluginBridge';
 import { useRulesSourcesWithRuler } from 'app/features/alerting/unified/hooks/useRuleSourcesWithRuler';
 import { AlertmanagerProvider } from 'app/features/alerting/unified/state/AlertmanagerContext';
 import { fetchAllPromBuildInfoAction } from 'app/features/alerting/unified/state/actions';
+import { SupportedPlugin } from 'app/features/alerting/unified/types/pluginBridges';
 import { GRAFANA_RULES_SOURCE_NAME } from 'app/features/alerting/unified/utils/datasource';
 import { DATASOURCES_ROUTES } from 'app/features/datasources/constants';
+import { useGetSingle } from 'app/features/plugins/admin/state/hooks';
+import { Receiver } from 'app/plugins/datasource/alertmanager/types';
 import { useDispatch } from 'app/types';
 
-import { Essentials } from './Essentials';
+import { Essentials, SectionsDto } from './Essentials';
 interface DataConfiguration {
   id: number;
   title: string;
   description: string;
   text: string;
   actionButtonTitle: string;
-  isVisible: boolean;
+  isDone?: boolean;
+  stepsDone?: number;
   titleIcon?: IconName;
+}
+
+function useGetEssentialsConfiguration() {
+  const contactPoints = useGetContactPoints();
+  const incidentPluginConfig = useGetIncidentPluginConfig();
+  const onCallIntegrations = useGetOnCallIntegrations();
+  const onCallOptions = onCallIntegrations.map((integration) => ({
+    label: integration.display_name,
+    value: integration.value,
+  }));
+  const ESSENTIAL_CONTENT: SectionsDto = {
+    sections: [
+      {
+        title: 'Detect',
+        description: 'Configure alerting',
+        steps: [
+          {
+            title: 'Contact point ready',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/alerting/notifications',
+              label: 'Update',
+              done: isContactPointReady(contactPoints),
+            },
+          },
+          {
+            title: 'Create alert rule',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/alerting/new',
+              label: 'Create',
+              done: isCreateAlertRuleDone(),
+            },
+          },
+          {
+            title: 'Create OnCall contact point',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/alerting/notifications',
+              label: 'View',
+              done: isOnCallContactPointReady(contactPoints),
+            },
+          },
+        ],
+      },
+      {
+        title: 'Respond',
+        description: 'Configure OnCall and Incident',
+        steps: [
+          {
+            title: 'Initialize Incident plugin',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/a/grafana-incident-app/walkthrough/generate-key',
+              label: 'Initialize',
+              done: incidentPluginConfig?.isInstalled,
+            },
+          },
+          {
+            title: 'Create OnCall integration to receive Alerts',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/a/grafana-oncall-app/integrations?tab=monitoring-systems&p=1',
+              label: 'View',
+              done: isOnCallIntegrationReady(onCallIntegrations),
+            },
+          },
+          {
+            title: 'Create your ChatOps workspace to OnCall',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/alerting/notifications',
+              label: 'Connect',
+            },
+          },
+          {
+            title: 'Create your ChatOps workspace to Incident',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/a/grafana-incident-app/integrations/grate.slack',
+              label: 'Connect',
+              done: incidentPluginConfig?.isChatOpsInstalled,
+            },
+          },
+          {
+            title: 'Add ChatOps to your integration',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/alerting/notifications',
+              label: 'Connect',
+            },
+          },
+        ],
+      },
+      {
+        title: 'Test your config',
+        description: '',
+        steps: [
+          {
+            title: 'Send OnCall demo alert',
+            description: 'tbd',
+            button: {
+              type: 'dropDown',
+              url: '/a/grafana-oncall-app/integrations/',
+              label: 'Select integration',
+              options: onCallOptions,
+            },
+          },
+          {
+            title: 'Create Incident drill',
+            description: 'tbd',
+            button: {
+              type: 'openLink',
+              url: '/a/grafana-incident-app?declare=new&drill=1',
+              label: 'Start drill',
+              done: incidentPluginConfig?.isDrillCreated,
+            },
+          },
+        ],
+      },
+    ],
+  };
+  return ESSENTIAL_CONTENT;
 }
 
 export function ConfigureIRM() {
@@ -31,6 +171,7 @@ export function ConfigureIRM() {
   const rulesSourcesWithRuler = useRulesSourcesWithRuler();
 
   const [essentialsOpen, setEssentialsOpen] = useState(false);
+  const essentialsConfig = useGetEssentialsConfiguration();
   const configuration: DataConfiguration[] = useMemo(() => {
     return [
       {
@@ -39,7 +180,7 @@ export function ConfigureIRM() {
         description: 'Before your start configuration you need to connect at least one datasource.',
         text: 'Configure IRM',
         actionButtonTitle: 'Connect',
-        isVisible: rulesSourcesWithRuler.length === 0,
+        isDone: rulesSourcesWithRuler.length > 0,
       },
       {
         id: 2,
@@ -48,7 +189,6 @@ export function ConfigureIRM() {
         description: 'Complete the basic configuration to start using the apps',
         text: 'Configure IRM',
         actionButtonTitle: 'View tasks',
-        isVisible: true,
       },
     ];
   }, [rulesSourcesWithRuler]);
@@ -70,26 +210,29 @@ export function ConfigureIRM() {
     <section className={styles.container}>
       {configuration.map((config) => {
         return (
-          config.isVisible && (
-            <Card key={config.id}>
-              <Card.Heading className={styles.title}>
-                {config.title}
-                {config.titleIcon && <Icon name={config.titleIcon} />}
-              </Card.Heading>
+          <Card key={config.id}>
+            <Card.Heading className={styles.title}>
+              {config.title}
+              {config.titleIcon && <Icon name={config.titleIcon} />}
+              {config.isDone && <Icon name="check-circle" color="green" size="lg" />}
+            </Card.Heading>
+            {!config.isDone && (
+              <>
+                <Card.Description className={styles.description}>{config.description}</Card.Description>
 
-              <Card.Description className={styles.description}>{config.description}</Card.Description>
-              <Card.Actions>
-                <Button variant="secondary" className={styles.actions} onClick={() => handleActionClick(config.id)}>
-                  {config.actionButtonTitle}
-                </Button>
-              </Card.Actions>
-            </Card>
-          )
+                <Card.Actions>
+                  <Button variant="secondary" className={styles.actions} onClick={() => handleActionClick(config.id)}>
+                    {config.actionButtonTitle}
+                  </Button>
+                </Card.Actions>
+              </>
+            )}
+          </Card>
         );
       })}
       {essentialsOpen && (
         <AlertmanagerProvider accessType={'notification'} alertmanagerSourceName={GRAFANA_RULES_SOURCE_NAME}>
-          <Essentials onClose={() => setEssentialsOpen(false)} />
+          <Essentials onClose={() => setEssentialsOpen(false)} essentialsConfig={essentialsConfig} />
         </AlertmanagerProvider>
       )}
     </section>
@@ -117,3 +260,126 @@ const getStyles = () => ({
     marginTop: '24px',
   }),
 });
+
+function isCreateAlertRuleDone() {
+  const { data: namespaces = [] } = alertRuleApi.endpoints.prometheusRuleNamespaces.useQuery(
+    {
+      ruleSourceName: GRAFANA_RULES_SOURCE_NAME,
+    },
+    {
+      refetchOnFocus: true,
+      refetchOnReconnect: true,
+      refetchOnMountOrArgChange: true,
+    }
+  );
+  return namespaces.length > 0;
+}
+
+function isContactPointReady(contactPoints: Receiver[]) {
+  // We consider the contact point ready if the default contact has the address filled or if there is at least one contact point created by the user
+
+  const defaultEmailUpdated = contactPoints.some(
+    (contactPoint: Receiver) =>
+      contactPoint.name === 'grafana-default-email' &&
+      contactPoint.grafana_managed_receiver_configs?.some(
+        (receiver) => receiver.name === 'grafana-default-email' && receiver.settings?.address !== '<example@email.com>'
+      )
+  );
+  const hasAnotherContactPoint = contactPoints.some((contactPoint: Receiver) =>
+    contactPoint.grafana_managed_receiver_configs?.some((receiver) => receiver.name !== 'grafana-default-email')
+  );
+  return defaultEmailUpdated || hasAnotherContactPoint;
+}
+
+function isOnCallContactPointReady(contactPoints: Receiver[]) {
+  return contactPoints.some((contactPoint: Receiver) =>
+    contactPoint.grafana_managed_receiver_configs?.some((receiver) => receiver.type === 'oncall')
+  );
+}
+
+function isOnCallIntegrationReady(onCallIntegrations: OnCallIntegrationDTO[]) {
+  return onCallIntegrations.length > 0;
+}
+
+interface IncidentsPluginConfig {
+  isInstalled: boolean;
+  isChatOpsInstalled: boolean;
+  isDrillCreated: boolean;
+}
+
+function useGetContactPoints() {
+  const alertmanagerConfiguration = alertmanagerApi.endpoints.getAlertmanagerConfiguration.useQuery(
+    GRAFANA_RULES_SOURCE_NAME,
+    {
+      refetchOnFocus: true,
+      refetchOnReconnect: true,
+      refetchOnMountOrArgChange: true,
+    }
+  );
+
+  const contactPoints = alertmanagerConfiguration.data?.alertmanager_config?.receivers ?? [];
+  return contactPoints;
+}
+
+function useGetIncidentPluginConfig() {
+  const incidentsPluginConfig = useGetSingle('grafana-incident-app');
+  const isIncidentPluginInstalled = incidentsPluginConfig?.isInstalled ?? false;
+  const [incidentPluginConfig, setIncidentPluginConfig] = React.useState<IncidentsPluginConfig | null>(null);
+
+  useEffect(() => {
+    if (!isIncidentPluginInstalled) {
+      setIncidentPluginConfig({
+        isInstalled: false,
+        isChatOpsInstalled: false,
+        isDrillCreated: false,
+      });
+    }
+    const getIncidentChatoOpsnstalled = async () => {
+      if (!isIncidentPluginInstalled) {
+        return false;
+      }
+      const availableIntegrations = await getBackendSrv().get(
+        '/api/plugins/grafana-incident-app/resources/api/IntegrationService.GetAvailableIntegrations'
+      );
+
+      const isSackInstalled = availableIntegrations?.find(
+        (integration: { id: string }) => integration.id === 'grate.slack'
+      );
+      const isMSTeamsInstalled = availableIntegrations?.find(
+        (integration: { id: string }) => integration.id === 'grate.msTeams'
+      );
+      return isSackInstalled || isMSTeamsInstalled;
+    };
+
+    const checkIfIncidentsCreated = async () => {
+      const isDrillCreated = await getBackendSrv()
+        .get('/api/plugins/grafana-incident-app/resources/api/IncidentsService.QueryIncidents')
+        .then((response) => response.incidents.length > 0);
+      return isDrillCreated;
+    };
+    if (isIncidentPluginInstalled) {
+      Promise.all([getIncidentChatoOpsnstalled(), checkIfIncidentsCreated()]).then(
+        ([isChatOpsInstalled, isDrillCreated]) =>
+          setIncidentPluginConfig({
+            isInstalled: true,
+            isChatOpsInstalled,
+            isDrillCreated,
+          })
+      );
+    }
+  }, [isIncidentPluginInstalled]);
+  return incidentPluginConfig;
+}
+
+function useGetOnCallIntegrations() {
+  const { installed: onCallPluginInstalled } = usePluginBridge(SupportedPlugin.OnCall);
+
+  const { data: onCallIntegrations } = onCallApi.endpoints.grafanaOnCallIntegrations.useQuery(undefined, {
+    skip: !onCallPluginInstalled,
+    refetchOnFocus: true,
+    refetchOnReconnect: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  return onCallIntegrations ?? [];
+}
--- public/app/features/gops/configuration-tracker/components/Essentials.tsx
+++ public/app/features/gops/configuration-tracker/components/Essentials.tsx
@@ -1,256 +1,37 @@
 import { css } from '@emotion/css';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { getBackendSrv, locationService } from '@grafana/runtime';
+import { locationService } from '@grafana/runtime';
 import { Button, Dropdown, Icon, LinkButton, Menu, Stack, Text, useStyles2 } from '@grafana/ui';
-import { alertRuleApi } from 'app/features/alerting/unified/api/alertRuleApi';
-import { alertmanagerApi } from 'app/features/alerting/unified/api/alertmanagerApi';
-import { OnCallIntegrationDTO, onCallApi } from 'app/features/alerting/unified/api/onCallApi';
-import { usePluginBridge } from 'app/features/alerting/unified/hooks/usePluginBridge';
-import { SupportedPlugin } from 'app/features/alerting/unified/types/pluginBridges';
-import { GRAFANA_RULES_SOURCE_NAME } from 'app/features/alerting/unified/utils/datasource';
 import { createUrl } from 'app/features/alerting/unified/utils/url';
-import { useGetSingle } from 'app/features/plugins/admin/state/hooks';
-import { Receiver } from 'app/plugins/datasource/alertmanager/types';
 
 import { ConfigurationTrackerDrawer } from './ConfigurationTrackerDrawer';
 
 export interface EssentialsProps {
   onClose: () => void;
+  essentialsConfig: SectionsDto;
 }
 
-interface IncidentsPluginConfig {
-  isInstalled: boolean;
-  isChatOpsInstalled: boolean;
-  isDrillCreated: boolean;
-}
-
-function useGetContactPoints() {
-  const alertmanagerConfiguration = alertmanagerApi.endpoints.getAlertmanagerConfiguration.useQuery(
-    GRAFANA_RULES_SOURCE_NAME,
-    {
-      refetchOnFocus: true,
-      refetchOnReconnect: true,
-      refetchOnMountOrArgChange: true,
-    }
-  );
-
-  const contactPoints = alertmanagerConfiguration.data?.alertmanager_config?.receivers ?? [];
-  return contactPoints;
-}
-
-function useGetIncidentPluginConfig() {
-  const incidentsPluginConfig = useGetSingle('grafana-incident-app');
-  const isIncidentPluginInstalled = incidentsPluginConfig?.isInstalled ?? false;
-  const [incidentPluginConfig, setIncidentPluginConfig] = React.useState<IncidentsPluginConfig | null>(null);
-
-  useEffect(() => {
-    if (!isIncidentPluginInstalled) {
-      setIncidentPluginConfig({
-        isInstalled: false,
-        isChatOpsInstalled: false,
-        isDrillCreated: false,
-      });
-    }
-    const getIncidentChatoOpsnstalled = async () => {
-      if (!isIncidentPluginInstalled) {
-        return false;
-      }
-      const availableIntegrations = await getBackendSrv().get(
-        '/api/plugins/grafana-incident-app/resources/api/IntegrationService.GetAvailableIntegrations'
-      );
-
-      const isSackInstalled = availableIntegrations?.find(
-        (integration: { id: string }) => integration.id === 'grate.slack'
-      );
-      const isMSTeamsInstalled = availableIntegrations?.find(
-        (integration: { id: string }) => integration.id === 'grate.msTeams'
-      );
-      return isSackInstalled || isMSTeamsInstalled;
-    };
-
-    const checkIfIncidentsCreated = async () => {
-      const isDrillCreated = await getBackendSrv()
-        .get('/api/plugins/grafana-incident-app/resources/api/IncidentsService.QueryIncidents')
-        .then((response) => response.incidents.length > 0);
-      return isDrillCreated;
-    };
-    if (isIncidentPluginInstalled) {
-      Promise.all([getIncidentChatoOpsnstalled(), checkIfIncidentsCreated()]).then(
-        ([isChatOpsInstalled, isDrillCreated]) =>
-          setIncidentPluginConfig({
-            isInstalled: true,
-            isChatOpsInstalled,
-            isDrillCreated,
-          })
-      );
-    }
-  }, [isIncidentPluginInstalled]);
-  return incidentPluginConfig;
-}
-
-function useGetOnCallIntegrations() {
-  const { installed: onCallPluginInstalled } = usePluginBridge(SupportedPlugin.OnCall);
-
-  const { data: onCallIntegrations } = onCallApi.endpoints.grafanaOnCallIntegrations.useQuery(undefined, {
-    skip: !onCallPluginInstalled,
-    refetchOnFocus: true,
-    refetchOnReconnect: true,
-    refetchOnMountOrArgChange: true,
-  });
-
-  return onCallIntegrations ?? [];
-}
-
-export function Essentials({ onClose }: EssentialsProps) {
-  const contactPoints = useGetContactPoints();
-  const incidentPluginConfig = useGetIncidentPluginConfig();
-  const onCallIntegrations = useGetOnCallIntegrations();
-  const onCallOptions = onCallIntegrations.map((integration) => ({
-    label: integration.display_name,
-    value: integration.value,
-  }));
-
-  const ESSENTIAL_CONTENT: SectionsDto = {
-    sections: [
-      {
-        title: 'Detect',
-        description: 'Configure alerting',
-        steps: [
-          {
-            title: 'Contact point ready',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/alerting/notifications',
-              label: 'Update',
-              done: isContactPointReady(contactPoints),
-            },
-          },
-          {
-            title: 'Create alert rule',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/alerting/new',
-              label: 'Create',
-              done: isCreateAlertRuleDone(),
-            },
-          },
-          {
-            title: 'Create OnCall contact point',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/alerting/notifications',
-              label: 'View',
-              done: isOnCallContactPointReady(contactPoints),
-            },
-          },
-        ],
-      },
-      {
-        title: 'Respond',
-        description: 'Configure OnCall and Incident',
-        steps: [
-          {
-            title: 'Initialize Incident plugin',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/a/grafana-incident-app/walkthrough/generate-key',
-              label: 'Initialize',
-              done: incidentPluginConfig?.isInstalled,
-            },
-          },
-          {
-            title: 'Create OnCall integration to receive Alerts',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/a/grafana-oncall-app/integrations?tab=monitoring-systems&p=1',
-              label: 'View',
-              done: isOnCallIntegrationReady(onCallIntegrations),
-            },
-          },
-          {
-            title: 'Create your ChatOps workspace to OnCall',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/alerting/notifications',
-              label: 'Connect',
-            },
-          },
-          {
-            title: 'Create your ChatOps workspace to Incident',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/a/grafana-incident-app/integrations/grate.slack',
-              label: 'Connect',
-              done: incidentPluginConfig?.isChatOpsInstalled,
-            },
-          },
-          {
-            title: 'Add ChatOps to your integration',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/alerting/notifications',
-              label: 'Connect',
-            },
-          },
-        ],
-      },
-      {
-        title: 'Test your config',
-        description: '',
-        steps: [
-          {
-            title: 'Send OnCall demo alert',
-            description: 'tbd',
-            button: {
-              type: 'dropDown',
-              url: '/a/grafana-oncall-app/integrations/',
-              label: 'Select integration',
-              options: onCallOptions,
-            },
-          },
-          {
-            title: 'Create Incident drill',
-            description: 'tbd',
-            button: {
-              type: 'openLink',
-              url: '/a/grafana-incident-app?declare=new&drill=1',
-              label: 'Start drill',
-              done: incidentPluginConfig?.isDrillCreated,
-            },
-          },
-        ],
-      },
-    ],
-  };
-
+export function Essentials({ onClose, essentialsConfig }: EssentialsProps) {
   return (
     <ConfigurationTrackerDrawer
       title="Essentials"
       subtitle="Complete basic recommended configuration to start using apps basic features"
       onClose={onClose}
     >
-      <EssentialContent essentialContent={ESSENTIAL_CONTENT} />
+      <EssentialContent essentialContent={essentialsConfig} />
     </ConfigurationTrackerDrawer>
   );
 }
-interface StepButtonDto {
+export interface StepButtonDto {
   type: 'openLink' | 'dropDown';
   url: string;
   label: string;
   options?: Array<{ label: string; value: string }>;
   done?: boolean;
 }
-interface SectionDto {
+export interface SectionDto {
   title: string;
   description: string;
   steps: Array<{
@@ -259,7 +40,7 @@ interface SectionDto {
     button: StepButtonDto;
   }>;
 }
-interface SectionsDto {
+export interface SectionsDto {
   sections: SectionDto[];
 }
 
@@ -351,43 +132,3 @@ const getStyles = (theme: GrafanaTheme2) => {
     }),
   };
 };
-
-function isCreateAlertRuleDone() {
-  const { data: namespaces = [] } = alertRuleApi.endpoints.prometheusRuleNamespaces.useQuery(
-    {
-      ruleSourceName: GRAFANA_RULES_SOURCE_NAME,
-    },
-    {
-      refetchOnFocus: true,
-      refetchOnReconnect: true,
-      refetchOnMountOrArgChange: true,
-    }
-  );
-  return namespaces.length > 0;
-}
-
-function isContactPointReady(contactPoints: Receiver[]) {
-  // We consider the contact point ready if the default contact has the address filled or if there is at least one contact point created by the user
-
-  const defaultEmailUpdated = contactPoints.some(
-    (contactPoint: Receiver) =>
-      contactPoint.name === 'grafana-default-email' &&
-      contactPoint.grafana_managed_receiver_configs?.some(
-        (receiver) => receiver.name === 'grafana-default-email' && receiver.settings?.address !== '<example@email.com>'
-      )
-  );
-  const hasAnotherContactPoint = contactPoints.some((contactPoint: Receiver) =>
-    contactPoint.grafana_managed_receiver_configs?.some((receiver) => receiver.name !== 'grafana-default-email')
-  );
-  return defaultEmailUpdated || hasAnotherContactPoint;
-}
-
-function isOnCallContactPointReady(contactPoints: Receiver[]) {
-  return contactPoints.some((contactPoint: Receiver) =>
-    contactPoint.grafana_managed_receiver_configs?.some((receiver) => receiver.type === 'oncall')
-  );
-}
-
-function isOnCallIntegrationReady(onCallIntegrations: OnCallIntegrationDTO[]) {
-  return onCallIntegrations.length > 0;
-}
