fd12b22a0ed823e265dda8267db47bd9949ce4e8
Bogdan Matei <bogdan.matei@grafana.com>
2024-04-11 12:31:47 +0300

    Dashboard: Allow `auto` refresh option when saving a dashboard (#85581)
    Fix saving auto refresh option
    
    (cherry picked from commit 62c512662c717ace7d40a3c1142ea8e93fa65d5e)

--- pkg/services/dashboards/service/dashboard_service.go
+++ pkg/services/dashboards/service/dashboard_service.go
@@ -288,7 +288,7 @@ func validateDashboardRefreshInterval(minRefreshInterval string, dash *dashboard
 	}
 
 	refresh := dash.Data.Get("refresh").MustString("")
-	if refresh == "" {
+	if refresh == "" || refresh == "auto" {
 		// since no refresh is set it is a valid refresh rate
 		return nil
 	}
