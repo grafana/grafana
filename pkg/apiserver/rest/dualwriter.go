262d40b6207cb9a9931a880b7e8e1768f43c6ac1
Leonor Oliveira <9090754+leonorfmartins@users.noreply.github.com>
2024-04-12 15:15:56 +0200

    Update pkg/apiserver/rest/dualwriter.go
    Co-authored-by: Arati R. <33031346+suntala@users.noreply.github.com>

--- pkg/apiserver/rest/dualwriter.go
+++ pkg/apiserver/rest/dualwriter.go
@@ -68,7 +68,7 @@ type DualWriter struct {
 
 var errDualWriterCreaterMissing = errors.New("legacy storage rest.Creater is missing")
 var errDualWriterListerMissing = errors.New("legacy storage rest.Lister is missing")
-var errDualWriterDeleterMissing = errors.New("legacy storage rest.Deleter is missing")
+var errDualWriterDeleterMissing = errors.New("legacy storage rest.GracefulDeleter is missing")
 
 type DualWriterMode int
 
