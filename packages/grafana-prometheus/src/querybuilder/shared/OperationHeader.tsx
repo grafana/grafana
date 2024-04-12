8e90c1579b4d73e29c7b15f371d73885e314ae72
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-04-12 16:08:10 -0500

    add history links for querybuilder/shared folder

--- packages/grafana-prometheus/src/querybuilder/shared/OperationHeader.tsx
+++ packages/grafana-prometheus/src/querybuilder/shared/OperationHeader.tsx
@@ -1,3 +1,4 @@
+// Core Grafana history https://github.com/grafana/grafana/blob/v11.0.0-preview/public/app/plugins/datasource/prometheus/querybuilder/shared/OperationHeader.tsx
 import { css } from '@emotion/css';
 import React, { useState } from 'react';
 import { DraggableProvided } from 'react-beautiful-dnd';
