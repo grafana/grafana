f85470d6523a6d086176686cd47ef0a2177ff9e7
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-04-15 16:45:23 -0500

    Prometheus: Use the frontend package in Prometheus and remove feature toggle (#86080)
    * add history links for monaco completion provider folder
    
    * add history links for monaco query field folder
    
    * add history links for components folder
    
    * add history links for configuration folder
    
    * add history links for dashboard json folder
    
    * add history links for gcopypaste folder
    
    * add history link for variableMigration
    
    * add history link for querybuilder/components/metrics-modal folder
    
    * add history link for querybuilder/components/promqail folder
    
    * add history links for querybuilder/components folder
    
    * add history links for querybuilder/hooks folder
    
    * add history links for querybuilder/shared folder
    
    * add history links for querybuilder folder
    
    * add history links for querycache folder
    
    * add history links for src folder
    
    * use frontend package and custom auth in module.ts
    
    * remove files and fix import issues
    
    * remove usePrometheusFrontendPackage
    
    * remove extra files
    
    * update betterer
    
    * remove extra files after rebase
    
    * fix betterer for rebase
    
    * fix e2e flakiness

--- packages/grafana-prometheus/src/dataquery.ts
+++ packages/grafana-prometheus/src/dataquery.ts
@@ -1,3 +1,4 @@
+// Core Grafana history https://github.com/grafana/grafana/blob/v11.0.0-preview/public/app/plugins/datasource/prometheus/dataquery.ts
 import { ScopeSpec } from '@grafana/data';
 import * as common from '@grafana/schema';
 
