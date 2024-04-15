9baedb7486d4bca61996c3507b7a141b55d37d6b
leonorfmartins <leonorfmartins@gmail.com>
2024-04-15 17:36:18 +0200

    WIP: add mocks for dual writer

--- pkg/apiserver/rest/dualwriter_mode1.go
+++ pkg/apiserver/rest/dualwriter_mode1.go
@@ -7,7 +7,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/rest"
-	"k8s.io/klog/v2"
 )
 
 type DualWriterMode1 struct {
@@ -22,13 +21,7 @@ func NewDualWriterMode1(legacy LegacyStorage, storage Storage) *DualWriterMode1 
 
 // Create overrides the behavior of the generic DualWriter and writes only to LegacyStorage.
 func (d *DualWriterMode1) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
-	legacy, ok := d.Legacy.(rest.Creater)
-	if !ok {
-		klog.FromContext(ctx).Error(errDualWriterCreaterMissing, "legacy storage rest.Creater is missing")
-		return nil, errDualWriterCreaterMissing
-	}
-
-	return legacy.Create(ctx, obj, createValidation, options)
+	return d.Legacy.Create(ctx, obj, createValidation, options)
 }
 
 // Get overrides the behavior of the generic DualWriter and reads only from LegacyStorage.
--- pkg/apiserver/rest/dualwriter_mode2.go
+++ pkg/apiserver/rest/dualwriter_mode2.go
@@ -24,12 +24,7 @@ func NewDualWriterMode2(legacy LegacyStorage, storage Storage) *DualWriterMode2 
 
 // Create overrides the behavior of the generic DualWriter and writes to LegacyStorage and Storage.
 func (d *DualWriterMode2) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
-	legacy, ok := d.Legacy.(rest.Creater)
-	if !ok {
-		return nil, errDualWriterCreaterMissing
-	}
-
-	created, err := legacy.Create(ctx, obj, createValidation, options)
+	created, err := d.Legacy.Create(ctx, obj, createValidation, options)
 	if err != nil {
 		klog.FromContext(ctx).Error(err, "unable to create object in legacy storage", "mode", 2)
 		return created, err
