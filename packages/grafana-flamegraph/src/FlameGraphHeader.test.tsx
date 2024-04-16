633286dd51cfed619ab3116f44cf9577a0de986b
Ashley Harrison <ashley.harrison@grafana.com>
2024-04-16 10:25:41 +0100

    include setupTests in child package tsconfig

--- packages/grafana-flamegraph/src/FlameGraphHeader.test.tsx
+++ packages/grafana-flamegraph/src/FlameGraphHeader.test.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
--- public/app/plugins/datasource/cloudwatch/components/AnnotationQueryEditor/AnnotationQueryEditor.test.tsx
+++ public/app/plugins/datasource/cloudwatch/components/AnnotationQueryEditor/AnnotationQueryEditor.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
-import '@testing-library/jest-dom';
 
 import { QueryEditorProps } from '@grafana/data';
 
