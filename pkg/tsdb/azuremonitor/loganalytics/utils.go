1b724b1315182f3559179582714c1b37c2cac28a
Jocelyn <jcolladokuri@microsoft.com>
2024-04-16 03:20:52 +0000

    add backend safeguards for basiclogsqueries

--- pkg/tsdb/azuremonitor/loganalytics/utils.go
+++ pkg/tsdb/azuremonitor/loganalytics/utils.go
@@ -1,6 +1,9 @@
 package loganalytics
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 )
 
@@ -30,4 +33,22 @@ func AddConfigLinks(frame data.Frame, dl string, title *string) data.Frame {
 	frame = AddCustomDataLink(frame, deepLink)
 
 	return frame
+}
+
+// Check whether a query should be handled as basic logs query
+// 1. basic logs flag is true
+// 2. resource selected is a workspace
+// 3.  query is not an alerts query
+// 4. number of selected resources is exactly one
+func MeetsBasicLogsCriteria(isBasicLogsQuery bool, resources []string) (bool, error) {
+	if len(resources) != 1 {
+		return false, fmt.Errorf("basic logs queries cannot be run against multiple resources")
+	}
+
+	if !strings.Contains(strings.ToLower(resources[0]), "microsoft.operationalinsights/workspaces") {
+		return false, fmt.Errorf("basic Logs queries may only be rung against LA workspaces")
+	}
+
+	// meetsCriteria := isBasicLogsQuery && len(resources) == 1 && strings.Contains(resources[0], "microsoft.operationalinsights/workspaces")
+	return isBasicLogsQuery, nil
 }
