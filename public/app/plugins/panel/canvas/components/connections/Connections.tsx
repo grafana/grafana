bc825588a9469fda87c8390131bc33391ebb6543
Ihor Yeromin <yeryomin.igor@gmail.com>
2024-04-16 13:25:35 +0200

    fix(canvas): types

--- public/app/plugins/panel/canvas/components/connections/Connections.tsx
+++ public/app/plugins/panel/canvas/components/connections/Connections.tsx
@@ -143,19 +143,21 @@ export class Connections {
     }
 
     const customElementAnchors = element?.item.customConnectionAnchors || ANCHORS;
-    let i = 0;
-    for (let img of this.anchorsDiv?.children!) {
-      if (i >= customElementAnchors.length) {
-        img.setAttribute('style', `top: calc(0px); left: calc(0px); pointer-events: auto !important; display: none;`);
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const anchors = Array.from(this.anchorsDiv?.children as HTMLCollectionOf<HTMLElement>);
+    const anchorsAmount = customElementAnchors.length;
+    // re-calculate the position of the existing anchors on hover
+    // and hide the rest of the anchors if there are more than the custom ones
+    anchors.forEach((anchor, index) => {
+      if (index >= anchorsAmount) {
+        anchor.style.display = 'none';
       } else {
-        const { x, y } = customElementAnchors![i];
-        img.setAttribute(
-          'style',
-          `top: calc(${-y * 50 + 50}% - ${HALF_SIZE}px - ${ANCHOR_PADDING}px); left: calc(${x * 50 + 50}% - ${HALF_SIZE}px - ${ANCHOR_PADDING}px); pointer-events: auto !important; display: block;`
-        );
+        const { x, y } = customElementAnchors![index];
+        anchor.style.top = `calc(${-y * 50 + 50}% - ${HALF_SIZE}px - ${ANCHOR_PADDING}px)`;
+        anchor.style.left = `calc(${x * 50 + 50}% - ${HALF_SIZE}px - ${ANCHOR_PADDING}px)`;
+        anchor.style.display = 'block';
       }
-      i++;
-    }
+    });
 
     const elementBoundingRect = element.div!.getBoundingClientRect();
     const transformScale = this.scene.scale;
