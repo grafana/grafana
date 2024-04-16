98caef15f5ad7005cd129085f39e0ac8fa9d4f66
Matias Chomicki <matyax@gmail.com>
2024-04-16 12:36:59 +0200

    Logs: sort displayed fields when assigning to state

--- public/app/features/explore/Logs/Logs.tsx
+++ public/app/features/explore/Logs/Logs.tsx
@@ -382,7 +382,7 @@ class UnthemedLogs extends PureComponent<Props, State> {
     if (index === -1) {
       this.setState((state) => {
         return {
-          displayedFields: state.displayedFields.concat(key),
+          displayedFields: state.displayedFields.concat(key).sort(),
         };
       });
     }
