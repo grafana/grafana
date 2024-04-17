ae08f8aea109389df4c209252472a2affb19903d
leonorfmartins <leonorfmartins@gmail.com>
2024-04-17 11:50:12 +0200

    Lint

--- pkg/apiserver/rest/dualwriter_mode1_test.go
+++ pkg/apiserver/rest/dualwriter_mode1_test.go
@@ -11,7 +11,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/apis/example"
-	"k8s.io/apiserver/pkg/registry/rest"
 )
 
 const kind = "dummy"
