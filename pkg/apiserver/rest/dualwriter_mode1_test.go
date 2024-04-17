dcfaf7e4343af69c30b751cd20ae879b46c7781b
Leonor Oliveira <9090754+leonorfmartins@users.noreply.github.com>
2024-04-17 10:25:19 +0100

    Storage: Add dualwriter test mocks (#86243)
    * WIP: add mocks for dual writer
    
    * Test dualwriter mode1
    
    * Re-add non implementation errors
    
    * Use testify assert
    
    * Write tests for mode2.
    
    * Lint
    
    * Remove comment
    
    * Update pkg/apiserver/rest/dualwriter_mode2_test.go
    
    Co-authored-by: Arati R. <33031346+suntala@users.noreply.github.com>
    
    * [REVIEW improve readability and call fn legacy fn
    
    * [REVIEW] rename mocks and setupFns
    
    * [REVIEW add missing test case
    
    * [REVIEW] add test case with getting object from storage and not legacy
    
    * Add deleteCollection
    
    * Test deleteCollection
    
    * Rename file
    
    ---------
    
    Co-authored-by: Arati R. <33031346+suntala@users.noreply.github.com>

--- pkg/apiserver/rest/dualwriter_mode1_test.go
+++ pkg/apiserver/rest/dualwriter_mode1_test.go
@@ -2,57 +2,301 @@ package rest
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	metainternalversion "k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/apis/example"
 )
 
 const kind = "dummy"
 
-func TestMode1(t *testing.T) {
-	var ls = (LegacyStorage)(nil)
-	var s = (Storage)(nil)
-	lsSpy := NewLegacyStorageSpyClient(ls)
-	sSpy := NewStorageSpyClient(s)
+var exampleObj = &example.Pod{TypeMeta: metav1.TypeMeta{}, ObjectMeta: metav1.ObjectMeta{Name: "foo", ResourceVersion: "1"}, Spec: example.PodSpec{}, Status: example.PodStatus{}}
+var anotherObj = &example.Pod{TypeMeta: metav1.TypeMeta{}, ObjectMeta: metav1.ObjectMeta{Name: "bar", ResourceVersion: "2"}, Spec: example.PodSpec{}, Status: example.PodStatus{}}
+var failingObj = &example.Pod{TypeMeta: metav1.TypeMeta{}, ObjectMeta: metav1.ObjectMeta{Name: "object-fail", ResourceVersion: "2"}, Spec: example.PodSpec{}, Status: example.PodStatus{}}
 
-	dw := NewDualWriterMode1(lsSpy, sSpy)
+func TestMode1_Create(t *testing.T) {
+	type testCase struct {
+		name           string
+		input          runtime.Object
+		setupLegacyFn  func(m *mock.Mock, input runtime.Object)
+		setupStorageFn func(m *mock.Mock, input runtime.Object)
+		wantErr        bool
+	}
+	tests :=
+		[]testCase{
+			{
+				name:  "creating an object only in the legacy store",
+				input: exampleObj,
+				setupLegacyFn: func(m *mock.Mock, input runtime.Object) {
+					m.On("Create", context.Background(), input, mock.Anything, mock.Anything).Return(exampleObj, nil)
+				},
+				setupStorageFn: func(m *mock.Mock, input runtime.Object) {
+					m.On("Create", context.Background(), anotherObj, mock.Anything, mock.Anything).Return(anotherObj, nil)
+				},
+			},
+			{
+				name:  "error when creating object in the legacy store fails",
+				input: failingObj,
+				setupLegacyFn: func(m *mock.Mock, input runtime.Object) {
+					m.On("Create", context.Background(), failingObj, mock.Anything, mock.Anything).Return(nil, errors.New("error"))
+				},
+				wantErr: true,
+			},
+		}
 
-	// Create: it should use the Legacy Create implementation
-	_, err := dw.Create(context.Background(), &dummyObject{}, func(context.Context, runtime.Object) error { return nil }, &metav1.CreateOptions{})
-	assert.NoError(t, err)
-	assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.Create"))
-	assert.Equal(t, 0, sSpy.Counts("Storage.Create"))
+	for _, tt := range tests {
+		l := (LegacyStorage)(nil)
+		s := (Storage)(nil)
+		m := &mock.Mock{}
 
-	// Get: it should use the Legacy Get implementation
-	_, err = dw.Get(context.Background(), kind, &metav1.GetOptions{})
-	assert.NoError(t, err)
-	assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.Get"))
-	assert.Equal(t, 0, sSpy.Counts("Storage.Get"))
+		ls := legacyStoreMock{m, l}
+		us := storageMock{m, s}
 
-	// List: it should use the Legacy List implementation
-	_, err = dw.List(context.Background(), &metainternalversion.ListOptions{})
-	assert.NoError(t, err)
-	assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.List"))
-	assert.Equal(t, 0, sSpy.Counts("Storage.List"))
+		if tt.setupLegacyFn != nil {
+			tt.setupLegacyFn(m, tt.input)
+		}
+		if tt.setupStorageFn != nil {
+			tt.setupStorageFn(m, tt.input)
+		}
 
-	// Delete: it should use the Legacy Delete implementation
-	var deleteValidation = func(ctx context.Context, obj runtime.Object) error { return nil }
-	_, _, err = dw.Delete(context.Background(), kind, deleteValidation, &metav1.DeleteOptions{})
-	assert.NoError(t, err)
-	assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.Delete"))
-	assert.Equal(t, 0, sSpy.Counts("Storage.Delete"))
+		dw := SelectDualWriter(Mode1, ls, us)
 
-	// DeleteCollection: it should use the Legacy DeleteCollection implementation
-	_, err = dw.DeleteCollection(
-		context.Background(),
-		func(context.Context, runtime.Object) error { return nil },
-		&metav1.DeleteOptions{},
-		&metainternalversion.ListOptions{},
-	)
-	assert.NoError(t, err)
-	assert.Equal(t, 1, lsSpy.Counts("LegacyStorage.DeleteCollection"))
-	assert.Equal(t, 0, sSpy.Counts("Storage.DeleteCollection"))
+		obj, err := dw.Create(context.Background(), tt.input, func(context.Context, runtime.Object) error { return nil }, &metav1.CreateOptions{})
+
+		if tt.wantErr {
+			assert.Error(t, err)
+			continue
+		}
+
+		us.AssertNotCalled(t, "Create", context.Background(), tt.input, func(context.Context, runtime.Object) error { return nil }, &metav1.CreateOptions{})
+
+		assert.Equal(t, obj, exampleObj)
+		assert.NotEqual(t, obj, anotherObj)
+	}
+}
+
+func TestMode1_Get(t *testing.T) {
+	type testCase struct {
+		name           string
+		input          string
+		setupLegacyFn  func(m *mock.Mock, name string)
+		setupStorageFn func(m *mock.Mock, name string)
+		wantErr        bool
+	}
+	tests :=
+		[]testCase{
+			{
+				name:  "get an object only in the legacy store",
+				input: "foo",
+				setupLegacyFn: func(m *mock.Mock, name string) {
+					m.On("Get", context.Background(), name, mock.Anything).Return(exampleObj, nil)
+				},
+				setupStorageFn: func(m *mock.Mock, name string) {
+					m.On("Get", context.Background(), name, mock.Anything).Return(anotherObj, nil)
+				},
+			},
+			{
+				name:  "error when getting an object in the legacy store fails",
+				input: "object-fail",
+				setupLegacyFn: func(m *mock.Mock, name string) {
+					m.On("Get", context.Background(), name, mock.Anything).Return(nil, errors.New("error"))
+				},
+				wantErr: true,
+			},
+		}
+
+	for _, tt := range tests {
+		l := (LegacyStorage)(nil)
+		s := (Storage)(nil)
+		m := &mock.Mock{}
+
+		ls := legacyStoreMock{m, l}
+		us := storageMock{m, s}
+
+		if tt.setupLegacyFn != nil {
+			tt.setupLegacyFn(m, tt.input)
+		}
+		if tt.setupStorageFn != nil {
+			tt.setupStorageFn(m, tt.input)
+		}
+
+		dw := SelectDualWriter(Mode1, ls, us)
+
+		obj, err := dw.Get(context.Background(), tt.input, &metav1.GetOptions{})
+
+		if tt.wantErr {
+			assert.Error(t, err)
+			continue
+		}
+
+		us.AssertNotCalled(t, "Get", context.Background(), tt.name, &metav1.GetOptions{})
+
+		assert.Equal(t, obj, exampleObj)
+		assert.NotEqual(t, obj, anotherObj)
+	}
+}
+
+func TestMode1_List(t *testing.T) {
+	type testCase struct {
+		name           string
+		setupLegacyFn  func(m *mock.Mock)
+		setupStorageFn func(m *mock.Mock)
+		wantErr        bool
+	}
+	tests :=
+		[]testCase{
+			{
+				name: "error when listing an object in the legacy store is not implemented",
+				setupLegacyFn: func(m *mock.Mock) {
+					m.On("List", context.Background(), mock.Anything).Return(nil, errors.New("error"))
+				},
+			},
+			// TODO: legacy list is missing
+		}
+
+	for _, tt := range tests {
+		l := (LegacyStorage)(nil)
+		s := (Storage)(nil)
+		m := &mock.Mock{}
+
+		ls := legacyStoreMock{m, l}
+		us := storageMock{m, s}
+
+		if tt.setupLegacyFn != nil {
+			tt.setupLegacyFn(m)
+		}
+		if tt.setupStorageFn != nil {
+			tt.setupStorageFn(m)
+		}
+
+		dw := SelectDualWriter(Mode1, ls, us)
+
+		_, err := dw.List(context.Background(), &metainternalversion.ListOptions{})
+
+		if tt.wantErr {
+			assert.Error(t, err)
+			continue
+		}
+
+		us.AssertNotCalled(t, "List", context.Background(), &metainternalversion.ListOptions{})
+	}
+}
+
+func TestMode1_Delete(t *testing.T) {
+	type testCase struct {
+		name           string
+		input          string
+		setupLegacyFn  func(m *mock.Mock, name string)
+		setupStorageFn func(m *mock.Mock, name string)
+		wantErr        bool
+	}
+	tests :=
+		[]testCase{
+			{
+				name:  "deleting an object in the legacy store",
+				input: "foo",
+				setupLegacyFn: func(m *mock.Mock, name string) {
+					m.On("Delete", context.Background(), name, mock.Anything, mock.Anything).Return(exampleObj, false, nil)
+				},
+			},
+			{
+				name:  "error when deleting an object in the legacy store",
+				input: "object-fail",
+				setupLegacyFn: func(m *mock.Mock, name string) {
+					m.On("Delete", context.Background(), name, mock.Anything, mock.Anything).Return(nil, false, errors.New("error"))
+				},
+				wantErr: true,
+			},
+		}
+
+	for _, tt := range tests {
+		l := (LegacyStorage)(nil)
+		s := (Storage)(nil)
+		m := &mock.Mock{}
+
+		ls := legacyStoreMock{m, l}
+		us := storageMock{m, s}
+
+		if tt.setupLegacyFn != nil {
+			tt.setupLegacyFn(m, tt.input)
+		}
+		if tt.setupStorageFn != nil {
+			tt.setupStorageFn(m, tt.input)
+		}
+
+		dw := SelectDualWriter(Mode1, ls, us)
+
+		obj, _, err := dw.Delete(context.Background(), tt.input, func(ctx context.Context, obj runtime.Object) error { return nil }, &metav1.DeleteOptions{})
+
+		if tt.wantErr {
+			assert.Error(t, err)
+			continue
+		}
+
+		us.AssertNotCalled(t, "Delete", context.Background(), tt.input, func(ctx context.Context, obj runtime.Object) error { return nil }, &metav1.DeleteOptions{})
+		assert.Equal(t, obj, exampleObj)
+		assert.NotEqual(t, obj, anotherObj)
+	}
+}
+
+func TestMode1_DeleteCollection(t *testing.T) {
+	type testCase struct {
+		name           string
+		input          *metav1.DeleteOptions
+		setupLegacyFn  func(m *mock.Mock, input *metav1.DeleteOptions)
+		setupStorageFn func(m *mock.Mock, input *metav1.DeleteOptions)
+		wantErr        bool
+	}
+	tests :=
+		[]testCase{
+			{
+				name:  "deleting a collection in the legacy store",
+				input: &metav1.DeleteOptions{TypeMeta: metav1.TypeMeta{Kind: "foo"}},
+				setupLegacyFn: func(m *mock.Mock, input *metav1.DeleteOptions) {
+					m.On("DeleteCollection", context.Background(), mock.Anything, input, mock.Anything).Return(exampleObj, nil)
+				},
+			},
+			{
+				name:  "error deleting a collection in the legacy store",
+				input: &metav1.DeleteOptions{TypeMeta: metav1.TypeMeta{Kind: "fail"}},
+				setupLegacyFn: func(m *mock.Mock, input *metav1.DeleteOptions) {
+					m.On("DeleteCollection", context.Background(), mock.Anything, input, mock.Anything).Return(nil, errors.New("error"))
+				},
+				wantErr: true,
+			},
+		}
+
+	for _, tt := range tests {
+		l := (LegacyStorage)(nil)
+		s := (Storage)(nil)
+		m := &mock.Mock{}
+
+		ls := legacyStoreMock{m, l}
+		us := storageMock{m, s}
+
+		if tt.setupLegacyFn != nil {
+			tt.setupLegacyFn(m, tt.input)
+		}
+		if tt.setupStorageFn != nil {
+			tt.setupStorageFn(m, tt.input)
+		}
+
+		dw := SelectDualWriter(Mode1, ls, us)
+
+		obj, err := dw.DeleteCollection(context.Background(), func(ctx context.Context, obj runtime.Object) error { return nil }, tt.input, &metainternalversion.ListOptions{})
+
+		if tt.wantErr {
+			assert.Error(t, err)
+			continue
+		}
+
+		us.AssertNotCalled(t, "DeleteCollection", context.Background(), tt.input, func(ctx context.Context, obj runtime.Object) error { return nil }, &metav1.DeleteOptions{})
+		assert.Equal(t, obj, exampleObj)
+		assert.NotEqual(t, obj, anotherObj)
+	}
 }
