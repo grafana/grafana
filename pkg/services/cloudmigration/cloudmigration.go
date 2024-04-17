73a59576921aad48e1d47582bd9dfaed51950680
Michael Mandrus <michael.mandrus@grafana.com>
2024-04-17 11:54:05 -0400

    remove useless comments

--- pkg/services/cloudmigration/cloudmigration.go
+++ pkg/services/cloudmigration/cloudmigration.go
@@ -5,16 +5,15 @@ import (
 )
 
 type Service interface {
-	// tokens
 	CreateToken(context.Context) (CreateAccessTokenResponse, error)
 	ValidateToken(context.Context, CloudMigration) error
-	// migration management
+
 	CreateMigration(context.Context, CloudMigrationRequest) (*CloudMigrationResponse, error)
 	GetMigration(context.Context, int64) (*CloudMigration, error)
 	DeleteMigration(context.Context, int64) (*CloudMigration, error)
 	UpdateMigration(context.Context, int64, CloudMigrationRequest) (*CloudMigrationResponse, error)
 	GetMigrationList(context.Context) (*CloudMigrationListResponse, error)
-	// migration run management
+
 	RunMigration(context.Context, int64) (*MigrateDataResponseDTO, error)
 	SaveMigrationRun(context.Context, *CloudMigrationRun) (int64, error)
 	GetMigrationStatus(context.Context, string, string) (*CloudMigrationRun, error)
