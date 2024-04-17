64310f7594e2b197176935fa578a7c17642080e1
Matias Chomicki <matyax@gmail.com>
2024-04-17 10:32:07 +0200

    Prom: disable cmd f within the editor

--- packages/grafana-prometheus/src/components/monaco-query-field/MonacoQueryField.tsx
+++ packages/grafana-prometheus/src/components/monaco-query-field/MonacoQueryField.tsx
@@ -222,6 +222,10 @@ const MonacoQueryField = (props: Props) => {
             'isEditorFocused' + id
           );
 
+          // Fixes Monaco capturing the search key binding and displaying a useless search box within the Editor.
+          // See https://github.com/grafana/grafana/issues/85850
+          editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyF, () => {});
+
           /* Something in this configuration of monaco doesn't bubble up [mod]+K, which the
                     command palette uses. Pass the event out of monaco manually
                     */
