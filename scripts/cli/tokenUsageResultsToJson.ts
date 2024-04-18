4aed7ac6fe4e7d726ee0da32ace1e5af741eb758
eledobleefe <laura.fernandez@grafana.com>
2024-04-18 13:24:09 +0200

    Finish script

--- scripts/cli/tokenUsageResultsToJson.ts
+++ scripts/cli/tokenUsageResultsToJson.ts
@@ -1,8 +1,9 @@
-const { BettererResultSummary } = require('@betterer/betterer');
 const { exec } = require('child_process');
 const { writeFile } = require('fs/promises');
 
 exec('yarn themes:usage', (error, stdout, stderr) => {
+  //We don't handle error as eslint throws a non-zero error code when it completes with lint errors
+  //And therefore it shows 'error: Command failed: yarn themes:usage' in the console
   if (stderr) {
     console.log(`stderr: ${stderr}`);
     return;
@@ -10,42 +11,46 @@ exec('yarn themes:usage', (error, stdout, stderr) => {
   themeTokenResultsToJson(stdout);
 });
 
-// interface ThemeTokenResult {
-//     name: string;
-//     files: [
-//       {
-//         path: string;
-//         count: number;
-//       }
-//     ];
-//   }
-const themeTokenResultsToJson = async (ruleResults) => {
-  const results = {};
+interface ThemeTokenResult {
+  [token: string]: {
+    [path: string]: number;
+  };
+}
+const themeTokenResultsToJson = async (ruleResults: string) => {
+  const results: ThemeTokenResult = {};
+  //Split the response into blocks using the filepath as the delimiter
+  const perFile: string[] = ruleResults.split('\n/');
 
-  const perFile = ruleResults.split('\n/');
-  let resultsPerLine = [];
-
+  //Regex to match the theme token and the rule name
   const tokenRegex = new RegExp('theme.*');
   const ruleRegex = new RegExp('@grafana.*');
 
-  perFile.forEach((file) => {
+  perFile.forEach((file: string) => {
+    //Split the block data using new line as the delimiter
     const perLine = file.split('\n');
+    //Get the filepath and remove it from the array
     const filePath = perLine.shift();
     perLine.forEach((line) => {
+      //Split each line of data using space as the delimiter
       const lineData = line.split(' ');
       lineData.forEach((data) => {
         if (data === '') {
           return;
         }
+        //Check if the data matches a theme token name and not the rule name
         if (data.match(tokenRegex) && !data.match(ruleRegex)) {
           const token = data;
-          if (results.hasOwnProperty(token)) {
+          //Check if the token already exists in the results
+          if (filePath && results.hasOwnProperty(token)) {
+            //Check if the filepath exists, and if so, increment the count
             if (results[token].hasOwnProperty(filePath)) {
               results[token][filePath] += 1;
             } else {
+              //If the filepath does not exist, create a new entry
               results[token][filePath] = 1;
             }
           } else {
+            //If the token does not exist, add it to the results as long as the filepath and its counter
             filePath && (results[token] = { [filePath]: 1 });
           }
         }
