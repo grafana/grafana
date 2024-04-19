d9608bdf0633219c8e29a192ecc733a97597f1a2
Steve Simpson <steve.simpson@grafana.com>
2024-04-19 10:13:27 +0200

    Alerting: Optimize rule status gathering APIs when a limit is applied.
    The frontend very commonly calls the `/rules` API with `limit_alerts=16`. When
    there are a very large number of alert instances present, this API is quite
    slow to respond, and profiling suggests that a big part of the problem is
    sorting the alerts by importance, in order to select the first 16.
    
    This changes the application of the limit to use a more efficient heap-based
    top-k algorithm. This maintains a slice of only the highest ranked items whilst
    iterating the full set of alert instances, which substantially reduces the
    number of comparisons needed. This is particularly effective, as the
    `AlertsByImportance` comparison is quite complex.
    
    I've included a benchmark to compare the new TopK function to the existing
    Sort/limit strategy. It shows that for small limits, the new approach is
    much faster, especially at high numbers of alerts, e.g.
    
    100K alerts / limit 16: 1.91s vs 0.02s (-99%)
    
    For situations where there is no effective limit, sorting is marginally faster,
    therefore in the API implementation, if there is either a) no limit or b) no
    effective limit, then we just sort the alerts as before. There is also a space
    overhead using a heap which would matter for large limits.

--- pkg/services/ngalert/api/tooling/definitions/prom_test.go
+++ pkg/services/ngalert/api/tooling/definitions/prom_test.go
@@ -64,3 +64,93 @@ func TestSortAlertsByImportance(t *testing.T) {
 		})
 	}
 }
+
+func TestTopKAlertsByImportance(t *testing.T) {
+	//	tm1, tm2 := time.Now(), time.Now().Add(time.Second)
+	tc := []struct {
+		name     string
+		k        int
+		input    []Alert
+		expected []Alert
+	}{{
+		name:     "alerts are ordered in expected importance (k=1)",
+		k:        1,
+		input:    []Alert{{State: "normal"}, {State: "nodata"}, {State: "error"}, {State: "pending"}, {State: "alerting"}},
+		expected: []Alert{{State: "alerting"}},
+	}, {
+		name:     "alerts are ordered in expected importance (k=2)",
+		k:        2,
+		input:    []Alert{{State: "normal"}, {State: "nodata"}, {State: "error"}, {State: "pending"}, {State: "alerting"}},
+		expected: []Alert{{State: "alerting"}, {State: "pending"}},
+	}, {
+		name:     "alerts are ordered in expected importance (k=3)",
+		k:        3,
+		input:    []Alert{{State: "normal"}, {State: "nodata"}, {State: "error"}, {State: "pending"}, {State: "alerting"}},
+		expected: []Alert{{State: "alerting"}, {State: "pending"}, {State: "error"}},
+	}, {
+		name:     "alerts are ordered in expected importance (k=4)",
+		k:        4,
+		input:    []Alert{{State: "normal"}, {State: "nodata"}, {State: "error"}, {State: "pending"}, {State: "alerting"}},
+		expected: []Alert{{State: "alerting"}, {State: "pending"}, {State: "error"}, {State: "nodata"}},
+	}, {
+		name:     "alerts are ordered in expected importance (k=5)",
+		k:        5,
+		input:    []Alert{{State: "normal"}, {State: "nodata"}, {State: "error"}, {State: "pending"}, {State: "alerting"}},
+		expected: []Alert{{State: "alerting"}, {State: "pending"}, {State: "error"}, {State: "nodata"}, {State: "normal"}},
+	}, {
+		name:     "alerts are ordered in expected importance (k=6)",
+		k:        6,
+		input:    []Alert{{State: "normal"}, {State: "nodata"}, {State: "error"}, {State: "pending"}, {State: "alerting"}},
+		expected: []Alert{{State: "alerting"}, {State: "pending"}, {State: "error"}, {State: "nodata"}, {State: "normal"}},
+	},
+
+	/*
+			   {
+			name:     "alerts with same importance are ordered active first",
+			input:    []Alert{{State: "normal"}, {State: "normal", ActiveAt: &tm1}},
+			expected: []Alert{{State: "normal", ActiveAt: &tm1}, {State: "normal"}},
+		}, {
+			name:     "active alerts with same importance are ordered newest first",
+			input:    []Alert{{State: "alerting", ActiveAt: &tm2}, {State: "alerting", ActiveAt: &tm1}},
+			expected: []Alert{{State: "alerting", ActiveAt: &tm1}, {State: "alerting", ActiveAt: &tm2}},
+		}, {
+			name: "inactive alerts with same importance are ordered by labels",
+			input: []Alert{
+				{State: "normal", Labels: map[string]string{"c": "d"}},
+				{State: "normal", Labels: map[string]string{"a": "b"}},
+			},
+			expected: []Alert{
+				{State: "normal", Labels: map[string]string{"a": "b"}},
+				{State: "normal", Labels: map[string]string{"c": "d"}},
+			},
+		}, {
+			name: "active alerts with same importance and active time are ordered fewest labels first",
+			input: []Alert{
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"a": "b", "c": "d"}},
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"e": "f"}},
+			},
+			expected: []Alert{
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"e": "f"}},
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"a": "b", "c": "d"}},
+			},
+		}, {
+			name: "active alerts with same importance and active time are ordered by labels",
+			input: []Alert{
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"c": "d"}},
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"a": "b"}},
+			},
+			expected: []Alert{
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"a": "b"}},
+				{State: "alerting", ActiveAt: &tm1, Labels: map[string]string{"c": "d"}},
+			},
+		}
+	*/
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			result := AlertsBy(AlertsByImportance).TopK(tt.input, tt.k)
+			assert.EqualValues(t, tt.expected, result)
+		})
+	}
+}
