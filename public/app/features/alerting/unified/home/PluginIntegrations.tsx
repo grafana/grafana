cf89bc6c87aec616fc89ded2423dc54ed3989a83
Konrad Lalik <konrad.lalik@grafana.com>
2024-04-22 13:42:11 +0200

    Alerting: Add plugins extension point to alerting home page (#85725)
    * Add basic extension point to alerting home page
    
    * Remove home page scenes app. Improve plugins styles
    
    * Remove unused code
    
    * Fix home page rendering when no plugins registered
    
    * Add row-based integrations component
    
    * Add missing margins
    
    * Rollback the Box component changes
    
    * Remove unused import

---
+++ public/app/features/alerting/unified/home/PluginIntegrations.tsx
@@ -0,0 +1,45 @@
+import { css } from '@emotion/css';
+import React from 'react';
+
+import { PluginExtensionPoints } from '@grafana/data';
+import { GrafanaTheme2 } from '@grafana/data/';
+import { getPluginComponentExtensions } from '@grafana/runtime';
+import { Stack, Text } from '@grafana/ui';
+import { useStyles2 } from '@grafana/ui/';
+
+export function PluginIntegrations() {
+  const styles = useStyles2(getStyles);
+
+  const { extensions } = getPluginComponentExtensions({
+    extensionPointId: PluginExtensionPoints.AlertingHomePage,
+    limitPerPlugin: 1,
+  });
+
+  if (extensions.length === 0) {
+    return null;
+  }
+
+  return (
+    <Stack direction="column" gap={2}>
+      <Text element="h3" variant="h4">
+        Speed up your alerts creation now by using one of our tailored apps
+      </Text>
+      <Stack gap={2} wrap="wrap" direction="row">
+        {extensions.map((extension) => (
+          <div key={extension.id} className={styles.box}>
+            <extension.component />
+          </div>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  box: css({
+    padding: theme.spacing(2),
+    flex: 1,
+    backgroundColor: theme.colors.background.secondary,
+    maxWidth: '460px',
+  }),
+});
