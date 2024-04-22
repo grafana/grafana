93bc8740d923fea0f24fd16408ef9759fee32599
Sven Grossmann <Svennergr@gmail.com>
2024-04-22 17:23:15 +0100

    Revert "Loki/Prometheus Query Editor: Disabled cmd/ctrl+f keybinding within t…"
    This reverts commit ec43edd8a1442020f4a5704fc84ebc87a17c7246.

--- public/app/plugins/datasource/loki/components/monaco-query-field/MonacoQueryField.tsx
+++ public/app/plugins/datasource/loki/components/monaco-query-field/MonacoQueryField.tsx
@@ -264,13 +264,6 @@ const MonacoQueryField = ({
             'isEditorFocused' + id
           );
 
-          // Fixes Monaco capturing the search key binding and displaying a useless search box within the Editor.
-          // See https://github.com/grafana/grafana/issues/85850
-          monaco.editor.addKeybindingRule({
-            keybinding: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyF,
-            command: null,
-          });
-
           editor.onDidFocusEditorText(() => {
             isEditorFocused.set(true);
             if (editor.getValue().trim() === '') {
