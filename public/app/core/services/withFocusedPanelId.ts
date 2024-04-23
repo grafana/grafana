5ba82985c910eb2a9f4fb44482f2f3aa48721b8c
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-04-23 14:53:52 +0200

    Panel: Keyboard shortcuts prio

--- public/app/core/services/withFocusedPanelId.ts
+++ public/app/core/services/withFocusedPanelId.ts
@@ -1,6 +1,15 @@
 export function withFocusedPanel(fn: (panelId: number) => void) {
   return () => {
-    const elements = document.querySelectorAll(':hover');
+    const elements = Array.from(document.querySelectorAll(':hover'));
+
+    // Handle keyboard focus first
+    const focusedElement = document.activeElement;
+    const focusedGridElement = focusedElement?.closest('[data-panelid]');
+
+    if (focusedGridElement instanceof HTMLElement && focusedGridElement.dataset?.panelid) {
+      fn(parseInt(focusedGridElement.dataset?.panelid, 10));
+      return;
+    }
 
     for (let i = elements.length - 1; i > 0; i--) {
       const element = elements[i];
