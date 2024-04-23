1da6eb0bd100e3d290ccc17e0e010bd32a86bf49
Konrad Lalik <konrad.lalik@grafana.com>
2024-04-23 09:39:21 +0200

    Add plugin badge tooltip

--- public/app/features/alerting/unified/plugins/PluginOriginBadge.tsx
+++ public/app/features/alerting/unified/plugins/PluginOriginBadge.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAsync } from 'react-use';
 
-import { Badge } from '@grafana/ui';
+import { Badge, Tooltip } from '@grafana/ui';
 
 import { getPluginSettings } from '../../../plugins/pluginSettings';
 
@@ -13,9 +13,16 @@ export function PluginOriginBadge({ pluginId }: PluginOriginBadgeProps) {
   const { value: pluginMeta } = useAsync(() => getPluginSettings(pluginId));
 
   const logo = pluginMeta?.info.logos?.small;
-  if (!logo) {
-    return <Badge text={pluginId} color="orange" />;
-  }
 
-  return <img src={logo} alt={pluginMeta?.name} style={{ width: '20px', height: '20px', borderRadius: '50%' }} />;
+  const badgeIcon = logo ? (
+    <img src={logo} alt={pluginMeta?.name} style={{ width: '20px', height: '20px', borderRadius: '50%' }} />
+  ) : (
+    <Badge text={pluginId} color="orange" />
+  );
+
+  const tooltipContent = pluginMeta
+    ? `This rule is managed by the ${pluginMeta?.name} plugin`
+    : `This rule is managed by a plugin`;
+
+  return <Tooltip content={tooltipContent}>{badgeIcon}</Tooltip>;
 }
