0a05c9cf658404ed663aca606d91b3336045fa2c
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2024-04-24 12:05:48 -0400

    datatrails: interpolate adhoc variables and datasource variables when opening "explore metrics" from dashboard panels (#86252)
    * fix: interpolate "explore metrics" from panels
    
    ---------
    
    Co-authored-by: Darren Janeczek <darren.janeczek@grafana.com>
    
    * fix: remove support for legacy dashboard
    
    - simplify code
    - take advantage of scenes dashboard async and datasource api object
    
    ---------
    
    Co-authored-by: Torkel Ödegaard <torkel@grafana.com>

--- public/app/features/trails/Integrations/utils.ts
+++ public/app/features/trails/Integrations/utils.ts
@@ -1,9 +1,5 @@
-import { PanelModel } from '@grafana/data';
 import { QueryBuilderLabelFilter } from '@grafana/prometheus/src/querybuilder/shared/types';
-import { SceneQueryRunner, SceneTimeRange, VizPanel } from '@grafana/scenes';
-import { DashboardModel } from 'app/features/dashboard/state';
 import { DashboardScene } from 'app/features/dashboard-scene/scene/DashboardScene';
-import { getQueryRunnerFor } from 'app/features/dashboard-scene/utils/utils';
 
 import { QueryMetric } from './getQueryMetrics';
 
@@ -12,22 +8,8 @@ export function isEquals(labelFilter: QueryBuilderLabelFilter) {
   return labelFilter.op === '=';
 }
 
-export function getQueryRunner(panel: VizPanel | PanelModel) {
-  if (panel instanceof VizPanel) {
-    return getQueryRunnerFor(panel);
-  }
-
-  return new SceneQueryRunner({ datasource: panel.datasource || undefined, queries: panel.targets || [] });
-}
-
-export function getTimeRangeFromDashboard(dashboard: DashboardScene | DashboardModel) {
-  if (dashboard instanceof DashboardScene) {
-    return dashboard.state.$timeRange!.clone();
-  }
-  if (dashboard instanceof DashboardModel) {
-    return new SceneTimeRange({ ...dashboard.time });
-  }
-  return new SceneTimeRange();
+export function getTimeRangeFromDashboard(dashboard: DashboardScene) {
+  return dashboard.state.$timeRange!.clone();
 }
 
 export function getQueryMetricLabel({ metric, labelFilters }: QueryMetric) {
@@ -42,11 +24,4 @@ export function getQueryMetricLabel({ metric, labelFilters }: QueryMetric) {
 
 export function createAdHocFilters(labels: QueryBuilderLabelFilter[]) {
   return labels?.map((label) => ({ key: label.label, value: label.value, operator: label.op }));
-}
-
-export function getPanelType(panel: VizPanel | PanelModel) {
-  if (panel instanceof VizPanel) {
-    return panel.state.pluginId;
-  }
-  return panel.type;
 }
