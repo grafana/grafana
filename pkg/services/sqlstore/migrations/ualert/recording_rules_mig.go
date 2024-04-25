206f961a38eee6fd6f8db0904d22e9da139d260b
Alex Weaver <weaver.alex.d@gmail.com>
2024-04-25 16:02:07 -0500

    Add migration

---
+++ pkg/services/sqlstore/migrations/ualert/recording_rules_mig.go
@@ -0,0 +1,50 @@
+package ualert
+
+import "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
+
+// AddRecordingRuleColumns adds columns to alert_rule to represent recording rules.
+func AddRecordingRuleColumns(mg *migrator.Migrator) {
+	mg.AddMigration("add record column to alert_rule table", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_rule"}, &migrator.Column{
+		Name:     "record",
+		Type:     migrator.DB_NVarchar,
+		Length:   DefaultFieldMaxLength,
+		Default:  "''",
+		Nullable: false,
+	}))
+
+	mg.AddMigration("add record column to alert_rule_version table", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_rule_version"}, &migrator.Column{
+		Name:     "record",
+		Type:     migrator.DB_NVarchar,
+		Length:   DefaultFieldMaxLength,
+		Default:  "''",
+		Nullable: false,
+	}))
+
+	mg.AddMigration("add record_from column to alert_rule table", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_rule"}, &migrator.Column{
+		Name:     "record_from",
+		Type:     migrator.DB_NVarchar,
+		Length:   DefaultFieldMaxLength,
+		Default:  "''",
+		Nullable: false,
+	}))
+
+	mg.AddMigration("add record_from column to alert_rule_version table", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_rule_version"}, &migrator.Column{
+		Name:     "record_from",
+		Type:     migrator.DB_NVarchar,
+		Length:   DefaultFieldMaxLength,
+		Default:  "''",
+		Nullable: false,
+	}))
+
+	mg.AddMigration("add record_to column to alert_rule table", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_rule"}, &migrator.Column{
+		Name:     "record_to",
+		Type:     migrator.DB_Text, // Text, to allow for future growth, as this contains a JSON-ified struct.
+		Nullable: true,
+	}))
+
+	mg.AddMigration("add record_to column to alert_rule_version table", migrator.NewAddColumnMigration(migrator.Table{Name: "alert_rule_version"}, &migrator.Column{
+		Name:     "record_to",
+		Type:     migrator.DB_Text, // Text, to allow for future growth, as this contains a JSON-ified struct.
+		Nullable: true,
+	}))
+}
