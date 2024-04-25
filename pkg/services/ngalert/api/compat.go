1e55c6c8ab6c005a7f8a2e2452a9e531dae8d1ec
Alex Weaver <weaver.alex.d@gmail.com>
2024-04-25 16:44:50 -0500

    Explicitly set defaults in provisioning API

--- pkg/services/ngalert/api/compat.go
+++ pkg/services/ngalert/api/compat.go
@@ -31,6 +31,11 @@ func AlertRuleFromProvisionedAlertRule(a definitions.ProvisionedAlertRule) (mode
 		Labels:               a.Labels,
 		IsPaused:             a.IsPaused,
 		NotificationSettings: NotificationSettingsFromAlertRuleNotificationSettings(a.NotificationSettings),
+		// Recording Rule fields will be implemented in the future.
+		// For now, no rules can be recording rules. So, we force these to be empty.
+		Record:     "",
+		RecordFrom: "",
+		RecordTo:   nil,
 	}, nil
 }
 
