eb8e6b3eb43b8209fee433221e200053c8de871d
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2024-04-25 09:31:36 +0200

    Update pkg/plugins/manager/signature/manifest.go
    Co-authored-by: Will Browne <wbrowne@users.noreply.github.com>

--- pkg/plugins/manager/signature/manifest.go
+++ pkg/plugins/manager/signature/manifest.go
@@ -169,7 +169,7 @@ func (s *Signature) Calculate(ctx context.Context, src plugins.PluginSource, plu
 
 	// Make sure the versions all match
 	if manifest.Plugin != plugin.JSONData.ID || manifest.Version != plugin.JSONData.Info.Version {
-		s.log.Debug("Plugin signature invalid because ID or Version mismatch", "id", plugin.JSONData.ID, "manifest", manifest.Plugin, "version", manifest.Version, "plugin", plugin.JSONData.ID, "version", plugin.JSONData.Info.Version)
+		s.log.Debug("Plugin signature invalid because ID or Version mismatch", "pluginId", plugin.JSONData.ID, "manifestPluginId", manifest.Plugin, "pluginVersion", plugin.JSONData.Info.Version, "manifestPluginVersion", manifest.Version)
 		return plugins.Signature{
 			Status: plugins.SignatureStatusModified,
 		}, nil
