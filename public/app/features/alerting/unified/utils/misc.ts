d0ac46f8f7819717ca5ea369532009d0509ad57a
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-04-29 13:48:54 +0100

    Add more robust error handling for stringifying API responses

--- public/app/features/alerting/unified/utils/misc.ts
+++ public/app/features/alerting/unified/utils/misc.ts
@@ -232,7 +232,17 @@ export function isErrorLike(error: unknown): error is Error {
 export function stringifyErrorLike(error: unknown): string {
   const fetchError = isFetchError(error);
   if (fetchError) {
-    return error.data.message;
+    if (error.message) {
+      return error.message;
+    }
+    if ('message' in error.data && typeof error.data.message === 'string') {
+      return error.data.message;
+    }
+    if (error.statusText) {
+      return error.statusText;
+    }
+
+    return String(error.status) || 'Unknown error';
   }
 
   return isErrorLike(error) ? error.message : String(error);
