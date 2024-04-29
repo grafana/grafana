aadfc020947f28734bc86ce4e8f4bd4463bf32cf
Darren Janeczek <darren.janeczek@grafana.com>
2024-04-29 12:27:21 -0400

    fix:unused  import

--- public/app/features/trails/helpers/MetricDatasourceHelper.ts
+++ public/app/features/trails/helpers/MetricDatasourceHelper.ts
@@ -1,7 +1,7 @@
 import { DataSourceApi } from '@grafana/data';
 import { PromMetricsMetadata, PromMetricsMetadataItem } from '@grafana/prometheus';
 import PromQlLanguageProvider from '@grafana/prometheus/src/language_provider';
-import { getBackendSrv, getDataSourceSrv } from '@grafana/runtime';
+import { getDataSourceSrv } from '@grafana/runtime';
 
 import { DataTrail } from '../DataTrail';
 import { VAR_DATASOURCE_EXPR } from '../shared';
