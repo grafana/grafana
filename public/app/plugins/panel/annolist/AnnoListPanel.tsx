2b53eea01675bc77ab54c9976cc6af95b3a33cd9
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-04-29 14:56:53 +0200

    Handle case for no associated panel or dashboard

--- public/app/plugins/panel/annolist/AnnoListPanel.tsx
+++ public/app/plugins/panel/annolist/AnnoListPanel.tsx
@@ -146,10 +146,10 @@ export class AnnoListPanel extends PureComponent<Props, State> {
     const params = {
       from: this._timeOffset(anno.time, options.navigateBefore, true),
       to: this._timeOffset(anno.timeEnd ?? anno.time, options.navigateAfter, false),
-      viewPanel: options.navigateToPanel ? anno.panelId : undefined,
+      viewPanel: options.navigateToPanel && anno.panelId ? anno.panelId : undefined,
     };
 
-    if (current?.uid === anno.dashboardUID) {
+    if (!anno.dashboardUID || current?.uid === anno.dashboardUID) {
       locationService.partial(params);
       return;
     }
