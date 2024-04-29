c467780970d7f3f1db423a2b69b66714e9ced729
Piotr Jamroz <pm.jamroz@gmail.com>
2024-04-29 12:55:40 +0200

    Update import

--- public/app/features/explore/QueryLibrary/utils/useDatasource.tsx
+++ public/app/features/explore/QueryLibrary/utils/useDatasource.tsx
@@ -1,7 +1,7 @@
 import { useAsync } from 'react-use';
 
 import { getDataSourceSrv } from '@grafana/runtime';
-import { DataSourceRef } from '@grafana/schema/dist/esm/index';
+import { DataSourceRef } from '@grafana/schema';
 
 export function useDatasource(dataSourceRef?: DataSourceRef | null) {
   const { value } = useAsync(async () => await getDataSourceSrv().get(dataSourceRef), [dataSourceRef]);
