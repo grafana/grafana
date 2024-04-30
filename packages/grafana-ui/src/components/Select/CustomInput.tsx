8cbed2f1eb71698f9da171eb8a91df679d8c3665
Clarity-89 <homes89@ukr.net>
2024-04-30 10:12:00 +0300

    Forward data-testid

--- packages/grafana-ui/src/components/Select/CustomInput.tsx
+++ packages/grafana-ui/src/components/Select/CustomInput.tsx
@@ -5,5 +5,11 @@ import { components, InputProps } from 'react-select';
  * Custom input component for react-select to add data-testid attribute
  */
 export const CustomInput = (props: InputProps) => {
-  return <components.Input {...props} data-testid={props.id ? `data-testid ${props.id}` : ''} />;
+  let testId = '';
+
+  if ('data-testid' in props.selectProps) {
+    testId = props.selectProps['data-testid'] + '-input';
+  }
+
+  return <components.Input {...props} data-testid={testId} />;
 };
