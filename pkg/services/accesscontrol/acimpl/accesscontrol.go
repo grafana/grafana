4ebdce5442be17da0cdc492334a7acf42d5bddc3
Karl Persson <kalle.persson@grafana.com>
2024-04-30 19:14:59 +0200

    break up into two functions

--- pkg/services/accesscontrol/acimpl/accesscontrol.go
+++ pkg/services/accesscontrol/acimpl/accesscontrol.go
@@ -33,8 +33,8 @@ func ProvideAccessControl(cfg *setting.Cfg, embed *embedserver.Service) *AccessC
 }
 
 type evalResult struct {
-	descision bool
 	runner    string
+	descision bool
 	err       error
 	duration  time.Duration
 }
@@ -63,82 +63,14 @@ func (a *AccessControl) Evaluate(ctx context.Context, user identity.Requester, e
 	res := make(chan evalResult, 2)
 	go func() {
 		start := time.Now()
-
-		// If the user is in no organization, then the evaluation must happen based on the user's global permissions
-		permissions := user.GetPermissions()
-		if user.GetOrgID() == accesscontrol.NoOrgID {
-			permissions = user.GetGlobalPermissions()
-		}
-		if len(permissions) == 0 {
-			a.debug(ctx, user, "No permissions set", evaluator)
-			res <- evalResult{false, "grafana", nil, time.Since(start)}
-			return
-		}
-
-		a.debug(ctx, user, "Evaluating permissions", evaluator)
-		// Test evaluation without scope resolver first, this will prevent 403 for wildcard scopes when resource does not exist
-		if evaluator.Evaluate(permissions) {
-			res <- evalResult{true, "grafana", nil, time.Since(start)}
-			return
-		}
-
-		resolvedEvaluator, err := evaluator.MutateScopes(ctx, a.resolvers.GetScopeAttributeMutator(user.GetOrgID()))
-		if err != nil {
-			if errors.Is(err, accesscontrol.ErrResolverNotFound) {
-				res <- evalResult{false, "grafana", nil, 0}
-				return
-			}
-			res <- evalResult{false, "grafana", err, time.Since(start)}
-			return
-		}
-
-		a.debug(ctx, user, "Evaluating resolved permissions", resolvedEvaluator)
-		res <- evalResult{resolvedEvaluator.Evaluate(permissions), "grafana", nil, time.Since(start)}
+		hasAccess, err := a.evalGrafana(ctx, user, evaluator)
+		res <- evalResult{"grafana", hasAccess, err, time.Since(start)}
 	}()
 
 	go func() {
 		start := time.Now()
-
-		eval, err := evaluator.MutateScopes(ctx, a.resolvers.GetScopeAttributeMutator(user.GetOrgID()))
-		if err != nil {
-			if !errors.Is(err, accesscontrol.ErrResolverNotFound) {
-				res <- evalResult{false, "grafana", err, 0}
-				return
-			}
-			eval = evaluator
-		}
-
-		for _, pair := range eval.Pairs() {
-			a.log.Info("evaluator", eval.String())
-
-			if pair.Action == "" {
-				a.log.Error("empty evaluator")
-				continue
-			}
-
-			relation, object := zclient.ConvertToRelationObject(pair.Action, pair.Scope, strconv.FormatInt(user.GetOrgID(), 10), "org")
-			result, err := a.zclient.Check(ctx, &openfgav1.CheckRequest{
-				StoreId: a.zclient.MustStoreID(ctx),
-				TupleKey: &openfgav1.CheckRequestTupleKey{
-					User:     user.GetID(),
-					Relation: relation,
-					Object:   object,
-				},
-				AuthorizationModelId: a.zclient.AuthorizationModelID,
-			})
-
-			if err != nil {
-				res <- evalResult{false, "zanzana", err, time.Since(start)}
-				return
-			}
-
-			if !result.Allowed {
-				res <- evalResult{false, "zanzana", nil, time.Since(start)}
-				return
-			}
-		}
-
-		res <- evalResult{true, "zanzana", nil, time.Since(start)}
+		hasAccess, err := a.evalZanzana(ctx, user, evaluator)
+		res <- evalResult{"zanzana", hasAccess, err, time.Since(start)}
 	}()
 
 	first := <-res
@@ -157,6 +89,76 @@ func (a *AccessControl) Evaluate(ctx context.Context, user identity.Requester, e
 	}
 
 	return grafanaResult.descision, grafanaResult.err
+}
+
+func (a *AccessControl) evalGrafana(ctx context.Context, user identity.Requester, evaluator accesscontrol.Evaluator) (bool, error) {
+	// If the user is in no organization, then the evaluation must happen based on the user's global permissions
+	permissions := user.GetPermissions()
+	if user.GetOrgID() == accesscontrol.NoOrgID {
+		permissions = user.GetGlobalPermissions()
+	}
+	if len(permissions) == 0 {
+		a.debug(ctx, user, "No permissions set", evaluator)
+		return false, nil
+	}
+
+	a.debug(ctx, user, "Evaluating permissions", evaluator)
+	// Test evaluation without scope resolver first, this will prevent 403 for wildcard scopes when resource does not exist
+	if evaluator.Evaluate(permissions) {
+		return true, nil
+	}
+
+	resolvedEvaluator, err := evaluator.MutateScopes(ctx, a.resolvers.GetScopeAttributeMutator(user.GetOrgID()))
+	if err != nil {
+		if errors.Is(err, accesscontrol.ErrResolverNotFound) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	a.debug(ctx, user, "Evaluating resolved permissions", resolvedEvaluator)
+	return resolvedEvaluator.Evaluate(permissions), nil
+}
+
+func (a *AccessControl) evalZanzana(ctx context.Context, user identity.Requester, evaluator accesscontrol.Evaluator) (bool, error) {
+	// TODO: let evaluator issue request, then we don't have to rebuild a tree
+	eval, err := evaluator.MutateScopes(ctx, a.resolvers.GetScopeAttributeMutator(user.GetOrgID()))
+	if err != nil {
+		if !errors.Is(err, accesscontrol.ErrResolverNotFound) {
+			return false, err
+		}
+		eval = evaluator
+	}
+
+	for _, pair := range eval.Pairs() {
+		a.log.Info("evaluator", eval.String())
+
+		if pair.Action == "" {
+			a.log.Error("empty evaluator")
+			continue
+		}
+
+		relation, object := zclient.ConvertToRelationObject(pair.Action, pair.Scope, strconv.FormatInt(user.GetOrgID(), 10), "org")
+		result, err := a.zclient.Check(ctx, &openfgav1.CheckRequest{
+			StoreId: a.zclient.MustStoreID(ctx),
+			TupleKey: &openfgav1.CheckRequestTupleKey{
+				User:     user.GetID(),
+				Relation: relation,
+				Object:   object,
+			},
+			AuthorizationModelId: a.zclient.AuthorizationModelID,
+		})
+
+		if err != nil {
+			return false, err
+		}
+
+		if !result.Allowed {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
 
 func (a *AccessControl) RegisterScopeAttributeResolver(prefix string, resolver accesscontrol.ScopeAttributeResolver) {
