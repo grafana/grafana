299100e2f9ca1d1e08f01541da947d918006d8ce
suntala <arati.rana@grafana.com>
2024-05-01 17:46:23 +0200

    Fix mode 2 List test

--- pkg/apiserver/rest/dualwriter_mode2_test.go
+++ pkg/apiserver/rest/dualwriter_mode2_test.go
@@ -5,23 +5,23 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/grafana/grafana/pkg/services/apiserver/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metainternalversion "k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apiserver/pkg/apis/example"
 )
 
 var createFn = func(context.Context, runtime.Object) error { return nil }
-var exampleOption = &metainternalversion.ListOptions{
-	TypeMeta: metav1.TypeMeta{
-		APIVersion: "v1",
-		Kind:       "foo",
-	},
-}
+
+var exampleOption = &metainternalversion.ListOptions{}
 
 func TestMode2_Create(t *testing.T) {
 	type testCase struct {
@@ -157,9 +157,36 @@ func TestMode2_Get(t *testing.T) {
 }
 
 func TestMode2_List(t *testing.T) {
+	legacyItem := example.Pod{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "foo",
+			ResourceVersion: "1",
+			Annotations: map[string]string{
+				"grafana.app/originKey": "1",
+			},
+		},
+		Spec:   example.PodSpec{},
+		Status: example.PodStatus{},
+	}
+	storageItem := legacyItem.DeepCopy()
+	storageItem.Labels = map[string]string{"exampleLabel": "value"}
+
+	legacyList := example.PodList{Items: []example.Pod{legacyItem}}
+	storageList := example.PodList{Items: []example.Pod{*storageItem}}
+	expectedList := storageList.DeepCopy()
+
+	r, err := labels.NewRequirement(utils.AnnoKeyOriginKey, selection.In, []string{"1"})
+	assert.NoError(t, err)
+	storageOptions := &metainternalversion.ListOptions{
+		LabelSelector: labels.NewSelector().Add(*r),
+		TypeMeta:      metav1.TypeMeta{},
+	}
+
 	type testCase struct {
 		name           string
-		input          *metainternalversion.ListOptions
+		inputLegacy    *metainternalversion.ListOptions
+		inputStorage   *metainternalversion.ListOptions
 		setupLegacyFn  func(m *mock.Mock, input *metainternalversion.ListOptions)
 		setupStorageFn func(m *mock.Mock, input *metainternalversion.ListOptions)
 		wantErr        bool
@@ -167,15 +194,15 @@ func TestMode2_List(t *testing.T) {
 	tests :=
 		[]testCase{
 			{
-				name:  "error when legacy list is not implmented",
-				input: exampleOption,
+				name:         "object present in both Storage and LegacyStorage",
+				inputLegacy:  exampleOption,
+				inputStorage: storageOptions,
 				setupLegacyFn: func(m *mock.Mock, input *metainternalversion.ListOptions) {
-					m.On("List", context.Background(), input).Return(exampleObj, nil)
+					m.On("List", context.Background(), input).Return(&legacyList, nil)
 				},
 				setupStorageFn: func(m *mock.Mock, input *metainternalversion.ListOptions) {
-					m.On("List", context.Background(), input).Return(exampleObj, nil)
+					m.On("List", context.Background(), input).Return(&storageList, nil)
 				},
-				wantErr: true,
 			},
 		}
 
@@ -188,10 +215,10 @@ func TestMode2_List(t *testing.T) {
 		us := storageMock{m, s}
 
 		if tt.setupLegacyFn != nil {
-			tt.setupLegacyFn(m, tt.input)
+			tt.setupLegacyFn(m, tt.inputLegacy)
 		}
 		if tt.setupStorageFn != nil {
-			tt.setupStorageFn(m, tt.input)
+			tt.setupStorageFn(m, tt.inputStorage)
 		}
 
 		dw := SelectDualWriter(Mode2, ls, us)
@@ -203,7 +230,7 @@ func TestMode2_List(t *testing.T) {
 			continue
 		}
 
-		assert.Equal(t, obj, exampleObj)
+		assert.Equal(t, expectedList, obj)
 	}
 }
 
