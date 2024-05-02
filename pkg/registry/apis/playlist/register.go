3dce0d227c35796a3f470c4812fd66b88d255cae
Arati R <33031346+suntala@users.noreply.github.com>
2024-05-02 16:52:29 +0200

    Storage: Enable mode 2 dual writing for playlists (#87245)
    Enable mode 2 dual writing for playlists

--- pkg/registry/apis/playlist/register.go
+++ pkg/registry/apis/playlist/register.go
@@ -118,7 +118,7 @@ func (b *PlaylistAPIBuilder) GetAPIGroupInfo(
 		if err != nil {
 			return nil, err
 		}
-		storage[resource.StoragePath()] = grafanarest.NewDualWriter(legacyStore, store)
+		storage[resource.StoragePath()] = grafanarest.NewDualWriterMode2(legacyStore, store)
 	}
 
 	apiGroupInfo.VersionedResourcesStorageMap[playlist.VERSION] = storage
