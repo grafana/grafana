532d37626eb5a9e6626d1be3bc8bdc3473c25aae
Drew Slobodnjak <60050885+drew08t@users.noreply.github.com>
2024-05-02 11:29:43 -0700

    Canvas: Fix datalinks using field variable (#87265)

--- public/app/plugins/panel/canvas/utils.ts
+++ public/app/plugins/panel/canvas/utils.ts
@@ -119,7 +119,9 @@ const addDataLinkForField = (
 ): void => {
   if (field?.getLinks) {
     const disp = field.display ? field.display(data) : { text: `${data}`, numeric: +data! };
-    field.getLinks({ calculatedValue: disp }).forEach((link) => {
+    // TODO add more control over which row index each element uses
+    const valueRowIndex = field.values.length - 1;
+    field.getLinks({ calculatedValue: disp, valueRowIndex }).forEach((link) => {
       const key = `${link.title}/${link.href}`;
       if (!linkLookup.has(key)) {
         links.push(link);
