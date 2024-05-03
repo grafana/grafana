55795cc6d8ad74da88654b4b27a5c3b48913c95b
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2024-05-03 12:24:18 -0400

    datatrails: only fetch metrics metadata when using the language provider, avoid redundant fetch of metric names (#87067)
    * fix: only fetch metrics metadata, not metric names

--- public/app/features/trails/helpers/MetricDatasourceHelper.ts
+++ public/app/features/trails/helpers/MetricDatasourceHelper.ts
@@ -36,7 +36,7 @@ export class MetricDatasourceHelper {
 
     if (ds.languageProvider instanceof PromQlLanguageProvider) {
       if (!ds.languageProvider.metricsMetadata) {
-        await ds.languageProvider.start();
+        await ds.languageProvider.loadMetricsMetadata();
       }
 
       return ds.languageProvider.metricsMetadata!;
