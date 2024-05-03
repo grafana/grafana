8fd76ecc06a41a7d9d9347aeba958abb06041360
kay delaney <kay@grafana.com>
2024-05-03 01:11:30 +0100

    Chore: Use getNextRefId instead of deprecated getNextRefIdChar

--- public/app/core/utils/query.test.ts
+++ public/app/core/utils/query.test.ts
@@ -1,38 +1,10 @@
 import { DataQuery } from '@grafana/data';
 
-import { getNextRefIdChar, queryIsEmpty } from './query';
+import { queryIsEmpty } from './query';
 
 export interface TestQuery extends DataQuery {
   name?: string;
 }
-
-function dataQueryHelper(ids: string[]): DataQuery[] {
-  return ids.map((letter) => {
-    return { refId: letter };
-  });
-}
-
-const singleDataQuery: DataQuery[] = dataQueryHelper('ABCDE'.split(''));
-const outOfOrderDataQuery: DataQuery[] = dataQueryHelper('ABD'.split(''));
-const singleExtendedDataQuery: DataQuery[] = dataQueryHelper('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''));
-
-describe('Get next refId char', () => {
-  it('should return next char', () => {
-    expect(getNextRefIdChar(singleDataQuery)).toEqual('F');
-  });
-
-  it('should get first char', () => {
-    expect(getNextRefIdChar([])).toEqual('A');
-  });
-
-  it('should get the first available character if a query has been deleted out of order', () => {
-    expect(getNextRefIdChar(outOfOrderDataQuery)).toEqual('C');
-  });
-
-  it('should append a new char and start from AA when Z is reached', () => {
-    expect(getNextRefIdChar(singleExtendedDataQuery)).toEqual('AA');
-  });
-});
 
 describe('queryIsEmpty', () => {
   it('should return true if query only includes props that are defined in the DataQuery interface', () => {
