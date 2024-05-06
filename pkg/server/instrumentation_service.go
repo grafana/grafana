f24e9cb9e51d71e1d852d8b04672406bf9d2bb43
Owen Smallwood <owen.smallwood@grafana.com>
2024-05-06 11:48:52 -0600

    adds profiling to instrumentation_service.go

--- pkg/server/instrumentation_service.go
+++ pkg/server/instrumentation_service.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"time"
 
 	"github.com/grafana/dskit/services"
 	"github.com/grafana/grafana/pkg/infra/log"
+	godeltaprof "github.com/grafana/pyroscope-go/godeltaprof/http/pprof"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -55,6 +57,14 @@ func (s *instrumentationService) stop(failureReason error) error {
 func (s *instrumentationService) newInstrumentationServer(ctx context.Context) *http.Server {
 	router := http.NewServeMux()
 	router.Handle("/metrics", promhttp.Handler())
+	router.HandleFunc("/debug/pprof/", pprof.Index)
+	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	router.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	router.HandleFunc("/debug/pprof/delta_heap", godeltaprof.Heap)
+	router.HandleFunc("/debug/pprof/delta_block", godeltaprof.Block)
+	router.HandleFunc("/debug/pprof/delta_mutex", godeltaprof.Mutex)
 
 	srv := &http.Server{
 		// 5s timeout for header reads to avoid Slowloris attacks (https://thetooth.io/blog/slowloris-attack/)
