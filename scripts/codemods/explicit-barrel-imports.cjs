a4e3777ffb36f327db6be72902813175a16b3c8c
Jack Westbrook <jack.westbrook@gmail.com>
2024-05-06 14:05:31 +0200

    fix(codemods): use require.resolve to correctly locate import paths

--- scripts/codemods/explicit-barrel-imports.cjs
+++ scripts/codemods/explicit-barrel-imports.cjs
@@ -1,39 +1,47 @@
 const fs = require('fs');
 const path = require('path');
 
+const isBareSpecifier = (importPath) => {
+  return !importPath.startsWith('app/') && /^[^./]/.test(importPath);
+};
+
+const resolvePath = (fileDir, importPath) => {
+  if (importPath.startsWith('app/')) {
+    const resolvedPath = require.resolve(path.join(process.cwd(), 'public', importPath));
+    return resolvedPath;
+  }
+
+  return require.resolve(path.resolve(fileDir, importPath));
+};
+
 module.exports = function (fileInfo, api) {
   const j = api.jscodeshift;
   const root = j.withParser('tsx')(fileInfo.source);
   const fileDir = path.dirname(fileInfo.path);
-
-  // Function to check if the path potentially points to a barrel file
-  const mightBeBarrelFileImport = (importPath) => {
-    const fullPath = path.join(fileDir, importPath);
-    if (fs.existsSync(fullPath) && fs.lstatSync(fullPath).isDirectory()) {
-      if (fs.existsSync(path.join(fullPath, 'index.ts')) || fs.existsSync(path.join(fullPath, 'index.js'))) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  // Udpate import declarations that import from barrel files
+  // Update import declarations that import from barrel files
   root
     .find(j.ImportDeclaration)
-    .filter((path) => mightBeBarrelFileImport(path.node.source.value))
+    .filter((path) => !isBareSpecifier(path.node.source.value))
     .forEach((path) => {
-      // Create a comment node
-      const comment = j.commentLine(' @todo: replace barrel import path');
+      const resolvedPath = resolvePath(fileDir, path.node.source.value);
+      if (
+        resolvedPath.endsWith('index.ts') ||
+        resolvedPath.endsWith('index.tsx') ||
+        resolvedPath.endsWith('index.js')
+      ) {
+        // Create a comment node
+        const comment = j.commentLine(' @todo: replace barrel import path');
 
-      // Attach the comment as a leading comment to the import declaration
-      if (!path.node.comments) {
-        path.node.comments = [];
+        // Attach the comment as a leading comment to the import declaration
+        if (!path.node.comments) {
+          path.node.comments = [];
+        }
+        path.node.comments.push(comment);
+
+        // Update the import path appending '/index'
+        path.node.source.value = path.node.source.value + '/index';
       }
-      path.node.comments.push(comment);
-
-      // Update the import path appending '/index'
-      path.node.source.value = path.node.source.value + '/index';
     });
 
-  return root.toSource();
+  return root.toSource({});
 };
