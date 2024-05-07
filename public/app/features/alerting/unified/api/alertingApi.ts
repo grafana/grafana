053c18e763e12bd15ce599e1f49247a22e14c86b
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-05-07 13:23:27 +0100

    Suppress API error/success messages if a custom one is provided

--- public/app/features/alerting/unified/api/alertingApi.ts
+++ public/app/features/alerting/unified/api/alertingApi.ts
@@ -1,4 +1,5 @@
 import { BaseQueryFn, createApi } from '@reduxjs/toolkit/query/react';
+import { produce } from 'immer';
 import { lastValueFrom } from 'rxjs';
 
 import { AppEvents } from '@grafana/data';
@@ -8,9 +9,19 @@ import appEvents from 'app/core/app_events';
 import { logMeasurement } from '../Analytics';
 
 type ExtendedBackendSrvRequest = BackendSrvRequest & {
-  /** Custom success message to show after completion of the request */
+  /**
+   * Custom success message to show after completion of the request.
+   *
+   * If a custom message is provided, any success message provided from the API response
+   * will not be shown
+   */
   successMessage?: string;
-  /** Custom error message to show if there's an error completing the request via backendSrv */
+  /**
+   * Custom error message to show if there's an error completing the request via backendSrv.
+   *
+   * If a custom message is provided, any error message from the API response
+   * will not be shown
+   */
   errorMessage?: string;
 };
 
@@ -18,9 +29,18 @@ export const backendSrvBaseQuery =
   (): BaseQueryFn<ExtendedBackendSrvRequest> =>
   async ({ successMessage, errorMessage, ...requestOptions }) => {
     try {
+      const modifiedRequestOptions = produce(requestOptions, (draft) => {
+        if (successMessage) {
+          draft.showSuccessAlert = false;
+        }
+        if (errorMessage) {
+          draft.showErrorAlert = false;
+        }
+      });
+
       const requestStartTs = performance.now();
 
-      const { data, ...meta } = await lastValueFrom(getBackendSrv().fetch(requestOptions));
+      const { data, ...meta } = await lastValueFrom(getBackendSrv().fetch(modifiedRequestOptions));
 
       logMeasurement(
         'backendSrvBaseQuery',
