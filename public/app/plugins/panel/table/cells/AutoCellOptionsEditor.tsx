cc866413f38007f68ec159e2af2d9944cb86a3e8
Kyle Cunningham <kyle@codeincarnate.com>
2024-05-07 21:34:51 -0500

    Add cell option editing

---
+++ public/app/plugins/panel/table/cells/AutoCellOptionsEditor.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { TableAutoCellOptions } from '@grafana/schema';
+import { Field, Switch } from '@grafana/ui';
+
+import { TableCellEditorProps } from '../TableCellOptionEditor';
+
+export const AutoCellOptionsEditor = ({
+    cellOptions,
+    onChange,
+}: TableCellEditorProps<TableAutoCellOptions>) => {
+
+
+    // Handle row coloring changes
+    const onWrapTextChange = () => {
+        cellOptions.wrapText = !cellOptions.wrapText;
+        onChange(cellOptions);
+    };
+
+    return (
+        <>
+            <Field
+                label="Wrap text"
+                description="If selected text will be wrapped to the width of text in the configured column."
+            >
+                <Switch value={cellOptions.wrapText} onChange={onWrapTextChange} />
+            </Field>
+        </>
+    );
+};
