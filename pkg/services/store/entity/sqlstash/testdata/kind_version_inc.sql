150545ced56921379d9955add5952075a29e45c0
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-05-09 16:30:29 -0300

    remove golden tests, they may cause the system to be too rigid to changes

--- pkg/services/store/entity/sqlstash/testdata/kind_version_inc.sql
+++
@@ -1,8 +0,0 @@
-UPDATE "kind_version"
-  SET "resource_version" = ? + 1
-  WHERE 1 = 1
-    AND "group"            = ?
-    AND "group_version"    = ?
-    AND "resource"         = ?
-    AND "resource_version" = ?
-;
--- pkg/services/store/entity/sqlstash/testdata/kind_version_lock_sqlite.sql
+++
@@ -1,7 +0,0 @@
-SELECT "resource_version"
-  FROM "kind_version"
-  WHERE 1 = 1
-    AND "group"         = ?
-    AND "group_version" = ?
-    AND "resource"      = ?
-;
