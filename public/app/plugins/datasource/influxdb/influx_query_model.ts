b94976b4551ab1cabc40b05063d20437a96b9d39
Ashley Harrison <ashley.harrison@grafana.com>
2024-05-09 16:34:58 +0100

    couple of influx improvements

--- public/app/plugins/datasource/influxdb/influx_query_model.ts
+++ public/app/plugins/datasource/influxdb/influx_query_model.ts
@@ -2,6 +2,7 @@ import { filter, find, indexOf, map } from 'lodash';
 
 import { AdHocVariableFilter, escapeRegex, ScopedVars } from '@grafana/data';
 import { TemplateSrv } from '@grafana/runtime';
+import { QueryPart } from 'app/features/alerting/state/query_part';
 
 import { removeRegexWrapper } from './queryUtils';
 import queryPart from './query_part';
@@ -9,11 +10,10 @@ import { DEFAULT_POLICY, InfluxQuery, InfluxQueryTag } from './types';
 
 export default class InfluxQueryModel {
   target: InfluxQuery;
-  selectModels: any[] = [];
-  queryBuilder: any;
-  groupByParts: any;
+  selectModels: QueryPart[][] = [];
+  groupByParts: QueryPart[] = [];
   templateSrv: any;
-  scopedVars: any;
+  scopedVars: ScopedVars | undefined;
   refId?: string;
 
   constructor(target: InfluxQuery, templateSrv?: TemplateSrv, scopedVars?: ScopedVars) {
@@ -244,7 +244,7 @@ export default class InfluxQueryModel {
     return policy + measurement;
   }
 
-  interpolateQueryStr(value: any[], variable: { multi: any; includeAll: any }, defaultFormatFn: any) {
+  interpolateQueryStr(value: string | string[], variable: { multi: boolean; includeAll: boolean }) {
     // if no multi or include all do not regexEscape
     if (!variable.multi && !variable.includeAll) {
       return value;
