db87dfc3c3a63e2974041192e8e27bcc159650b5
Leon Sorokin <leeoniya@gmail.com>
2024-05-10 18:06:35 -0500

    fix

--- public/app/plugins/panel/xychart/v2/scatter.ts
+++ public/app/plugins/panel/xychart/v2/scatter.ts
@@ -1,6 +1,6 @@
 import uPlot from 'uplot';
 
-import { FieldType, formattedValueToString, GrafanaTheme2, NumericRange, reduceField, ReducerID } from '@grafana/data';
+import { formattedValueToString, GrafanaTheme2 } from '@grafana/data';
 import { alpha } from '@grafana/data/src/themes/colorManipulator';
 import { AxisPlacement, ScaleDirection, ScaleOrientation, VisibilityMode } from '@grafana/schema';
 import { UPlotConfigBuilder } from '@grafana/ui';
@@ -434,7 +434,7 @@ export function prepData(xySeries: XYSeries[]): FacetedData {
   //   return [null];
   // }
 
-  const sizeGlobalRange = getGlobalRangeByField(xySeries, 'size');
+  const { size: sizeRange } = getGlobalRanges(xySeries);
 
   return [
     null,
@@ -445,7 +445,6 @@ export function prepData(xySeries: XYSeries[]): FacetedData {
 
       if (s.size.field != null) {
         let { min, max } = s.size;
-        let { min: minVal, delta: valRange } = sizeGlobalRange;
 
         // todo: this scaling should be in renderer from raw values (not by passing css pixel diams via data)
         let minPx = min! ** 2;
@@ -457,13 +456,16 @@ export function prepData(xySeries: XYSeries[]): FacetedData {
         // todo: better min/max with ignoring non-finite values
         // todo: allow this to come from fieldConfig min/max ? or field.state.min/max (shared)
         let vals = s.size.field.values;
+        let minVal = sizeRange.min;
+        let maxVal = sizeRange.max;
+        let valRange = maxVal - minVal;
 
         diams = Array(len);
 
         for (let i = 0; i < vals.length; i++) {
           let val = vals[i];
 
-          let valPct = (val - (minVal ?? 0)) / valRange;
+          let valPct = (val - minVal) / valRange;
           let pxArea = minPx + valPct * pxRange;
           diams[i] = pxArea ** 0.5;
         }
@@ -481,33 +483,41 @@ export function prepData(xySeries: XYSeries[]): FacetedData {
   ];
 }
 
-const getGlobalRangeByField = (xySeries: XYSeries[], fieldName: string): NumericRange => {
-  let min: number | null = null;
-  let max: number | null = null;
+const getGlobalRanges = (xySeries: XYSeries[]) => {
+  const ranges = {
+    size: {
+      min: Infinity,
+      max: -Infinity,
+    },
+    color: {
+      min: Infinity,
+      max: -Infinity,
+    },
+  };
 
-  const reducers = [ReducerID.min, ReducerID.max];
+  xySeries.forEach((series) => {
+    [series.size, series.color].forEach((facet, fi) => {
+      if (facet.field != null) {
+        let range = fi === 0 ? ranges.size : ranges.color;
 
-  xySeries.map((series) => {
-    const fieldByName = series[fieldName as keyof typeof series];
-    if (!fieldByName) {
-      return;
-    }
+        const vals = facet.field.values;
 
-    const field = fieldByName.field; // @TODO fix this
-    if (field.type === FieldType.number) {
-      const calcs = reduceField({ field, reducers });
-      const calcsMin = calcs[ReducerID.min];
-      const calcsMax = calcs[ReducerID.max];
+        for (let i = 0; i < vals.length; i++) {
+          const v = vals[i];
 
-      if (min === null || calcsMin < min) {
-        min = calcsMin;
+          if (v != null) {
+            if (v < range.min) {
+              range.min = v;
+            }
+
+            if (v > range.max) {
+              range.max = v;
+            }
+          }
+        }
       }
-
-      if (max === null || calcsMax > max) {
-        max = calcsMax;
-      }
-    }
+    });
   });
 
-  return { min, max, delta: (max ?? 0) - (min ?? 0) };
+  return ranges;
 };
