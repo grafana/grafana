c96add6583b2834a22531efbc5aa320d93a2bb1c
Ivana Huckova <ivana.huckova@gmail.com>
2024-05-10 11:23:09 +0200

    Move to separate folder

--- packages/grafana-ui/src/components/InfoBox/FeatureInfoBox.tsx
+++ packages/grafana-ui/src/components/InfoBox/FeatureInfoBox.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { FeatureState, GrafanaTheme2 } from '@grafana/data';
 
 import { useStyles2 } from '../../themes';
-import { Badge, BadgeProps } from '../Badge/Badge';
+import { FeatureBadge } from '../FeatureBadge/FeatureBadge';
 
 import { InfoBox, InfoBoxProps } from './InfoBox';
 
@@ -41,45 +41,3 @@ const getFeatureInfoBoxStyles = (theme: GrafanaTheme2) => {
     }),
   };
 };
-
-interface FeatureBadgeProps {
-  featureState: FeatureState;
-  tooltip?: string;
-}
-
-export const FeatureBadge = ({ featureState, tooltip }: FeatureBadgeProps) => {
-  const display = getPanelStateBadgeDisplayModel(featureState);
-  return <Badge text={display.text} color={display.color} icon={display.icon} tooltip={tooltip} />;
-};
-
-function getPanelStateBadgeDisplayModel(featureState: FeatureState): BadgeProps {
-  switch (featureState) {
-    case FeatureState.alpha:
-      return {
-        text: 'Alpha',
-        icon: 'exclamation-triangle',
-        color: 'orange',
-      };
-
-    case FeatureState.beta:
-      return {
-        text: 'Beta',
-        icon: 'rocket',
-        color: 'blue',
-      };
-
-    case FeatureState.experimental:
-      return {
-        text: 'Experimental',
-        icon: 'exclamation-triangle',
-        color: 'orange',
-      };
-
-    case FeatureState.preview:
-      return {
-        text: 'Preview',
-        icon: 'rocket',
-        color: 'blue',
-      };
-  }
-}
