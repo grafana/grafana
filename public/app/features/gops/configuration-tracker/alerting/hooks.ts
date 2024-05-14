e80845aa2e3f97bcd2acc00bc6efec13e56aab20
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2024-05-14 16:17:55 +0200

    Address some nits in the review

--- public/app/features/gops/configuration-tracker/alerting/hooks.ts
+++ public/app/features/gops/configuration-tracker/alerting/hooks.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 
 import { alertRuleApi } from 'app/features/alerting/unified/api/alertRuleApi';
 import { alertmanagerApi } from 'app/features/alerting/unified/api/alertmanagerApi';
+import { ReceiverTypes } from 'app/features/alerting/unified/components/receivers/grafanaAppReceivers/onCall/onCall';
 import { useDataSourceFeatures } from 'app/features/alerting/unified/hooks/useCombinedRule';
 import { GRAFANA_RULES_SOURCE_NAME } from 'app/features/alerting/unified/utils/datasource';
 import { Receiver } from 'app/plugins/datasource/alertmanager/types';
@@ -30,7 +31,7 @@ export function useIsCreateAlertRuleDone() {
 
 export function isOnCallContactPointReady(contactPoints: Receiver[]) {
   return contactPoints.some((contactPoint: Receiver) =>
-    contactPoint.grafana_managed_receiver_configs?.some((receiver) => receiver.type === 'oncall')
+    contactPoint.grafana_managed_receiver_configs?.some((receiver) => receiver.type === ReceiverTypes.OnCall)
   );
 }
 
--- public/app/features/gops/configuration-tracker/alerting/utils.ts
+++ public/app/features/gops/configuration-tracker/alerting/utils.ts
@@ -1,4 +1,5 @@
 import { Receiver } from 'app/plugins/datasource/alertmanager/types';
+const DEFAULT_EMAIL = '<example@email.com>';
 
 export function isContactPointReady(defaultContactPoint: string, contactPoints: Receiver[]) {
   // We consider the contact point ready if the default contact has the address filled
@@ -7,7 +8,7 @@ export function isContactPointReady(defaultContactPoint: string, contactPoints: 
     (contactPoint: Receiver) =>
       contactPoint.name === defaultContactPoint &&
       contactPoint.grafana_managed_receiver_configs?.some(
-        (receiver) => receiver.name === defaultContactPoint && receiver.settings?.address !== '<example@email.com>'
+        (receiver) => receiver.name === defaultContactPoint && receiver.settings?.address !== DEFAULT_EMAIL
       )
   );
   return defaultEmailUpdated;
