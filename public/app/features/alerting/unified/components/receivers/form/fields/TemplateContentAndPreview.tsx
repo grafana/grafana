c8b52645ae3c58589b3cb7e6284aaadce8826401
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2024-05-14 09:59:47 +0200

    Create new usePreviewTemplate to be reused from TemplatePreview and TemplateContentAndPreview

--- public/app/features/alerting/unified/components/receivers/form/fields/TemplateContentAndPreview.tsx
+++ public/app/features/alerting/unified/components/receivers/form/fields/TemplateContentAndPreview.tsx
@@ -1,14 +1,14 @@
 import { css, cx } from '@emotion/css';
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { Box, useStyles2 } from '@grafana/ui';
-import { AlertField, usePreviewTemplateMutation } from 'app/features/alerting/unified/api/templateApi';
 import { useAlertmanager } from 'app/features/alerting/unified/state/AlertmanagerContext';
 import { GRAFANA_RULES_SOURCE_NAME } from 'app/features/alerting/unified/utils/datasource';
 
 import { EditorColumnHeader } from '../../../contact-points/templates/EditorColumnHeader';
 import { getPreviewResults } from '../../TemplatePreview';
+import { usePreviewTemplate } from '../../usePreviewTemplate';
 
 export function TemplateContentAndPreview({
   payload,
@@ -27,24 +27,11 @@ export function TemplateContentAndPreview({
 }) {
   const styles = useStyles2(getStyles);
 
-  const [trigger, { data }] = usePreviewTemplateMutation();
   const { selectedAlertmanager } = useAlertmanager();
   const isGrafanaAlertManager = selectedAlertmanager === GRAFANA_RULES_SOURCE_NAME;
 
+  const { data } = usePreviewTemplate(templateContent, templateName, payload, setPayloadFormatError);
   const previewToRender = getPreviewResults(undefined, payloadFormatError, data);
-
-  const onPreview = useCallback(() => {
-    try {
-      const alertList: AlertField[] = JSON.parse(payload);
-      JSON.stringify([...alertList]); // check if it's iterable, in order to be able to add more data
-      trigger({ template: templateContent, alerts: alertList, name: templateName });
-      setPayloadFormatError(null);
-    } catch (e) {
-      setPayloadFormatError(e instanceof Error ? e.message : 'Invalid JSON.');
-    }
-  }, [templateContent, templateName, payload, setPayloadFormatError, trigger]);
-
-  useEffect(() => onPreview(), [onPreview]);
 
   return (
     <div className={cx(styles.container, className)}>
