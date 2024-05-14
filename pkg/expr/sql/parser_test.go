493cb9d5d5676357e2cc569943ce3bfad4f36e3c
Scott Lepper <scott.lepper@gmail.com>
2024-05-14 15:58:07 -0400

    [sql expressions] fix: use ast to read tables

--- pkg/expr/sql/parser_test.go
+++ pkg/expr/sql/parser_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestParse(t *testing.T) {
 	sql := "select * from foo"
-	tables, err := parseTables((sql))
+	tables, err := TablesList((sql))
 	assert.Nil(t, err)
 
 	assert.Equal(t, "foo", tables[0])
@@ -16,21 +16,21 @@ func TestParse(t *testing.T) {
 
 func TestParseWithComma(t *testing.T) {
 	sql := "select * from foo,bar"
-	tables, err := parseTables((sql))
+	tables, err := TablesList((sql))
 	assert.Nil(t, err)
 
-	assert.Equal(t, "foo", tables[0])
-	assert.Equal(t, "bar", tables[1])
+	assert.Equal(t, "bar", tables[0])
+	assert.Equal(t, "foo", tables[1])
 }
 
 func TestParseWithCommas(t *testing.T) {
 	sql := "select * from foo,bar,baz"
-	tables, err := parseTables((sql))
+	tables, err := TablesList((sql))
 	assert.Nil(t, err)
 
-	assert.Equal(t, "foo", tables[0])
-	assert.Equal(t, "bar", tables[1])
-	assert.Equal(t, "baz", tables[2])
+	assert.Equal(t, "bar", tables[0])
+	assert.Equal(t, "baz", tables[1])
+	assert.Equal(t, "foo", tables[2])
 }
 
 func TestArray(t *testing.T) {
@@ -111,6 +111,12 @@ func TestAlias(t *testing.T) {
 	assert.Equal(t, "A", tables[0])
 }
 
+func TestError(t *testing.T) {
+	sql := `select * from zzz aaa zzz`
+	_, err := TablesList((sql))
+	assert.NotNil(t, err)
+}
+
 func TestParens(t *testing.T) {
 	sql := `SELECT  t1.Col1,
 	t2.Col1,
@@ -127,4 +133,52 @@ func TestParens(t *testing.T) {
 	assert.Equal(t, "table1", tables[0])
 	assert.Equal(t, "table2", tables[1])
 	assert.Equal(t, "table3", tables[2])
+}
+
+func TestWith(t *testing.T) {
+	sql := `WITH
+
+	current_month AS (
+	  select 
+		distinct "Month(ISO)" as mth
+	  from A
+	  ORDER BY mth DESC
+	  LIMIT 1
+	), 
+	
+	last_month_bill AS (
+	  select
+		CAST (
+		  sum(
+			CAST(BillableSeries AS INTEGER)
+		  ) AS INTEGER
+		) AS BillableSeries,
+		"Month(ISO)",
+		label_namespace
+		-- , B.activeseries_count
+	  from A
+	  JOIN current_month
+		ON current_month.mth = A."Month(ISO)"
+	  	JOIN B
+	  	ON B.namespace = A.label_namespace
+	  GROUP BY
+		label_namespace,
+		"Month(ISO)"
+	  ORDER BY BillableSeries DESC
+	)
+	
+	SELECT
+	  last_month_bill.*,
+	  BEE.activeseries_count
+	FROM last_month_bill
+	JOIN BEE
+	  ON BEE.namespace = last_month_bill.label_namespace`
+
+	tables, err := TablesList((sql))
+	assert.Nil(t, err)
+
+	assert.Equal(t, 5, len(tables))
+	assert.Equal(t, "A", tables[0])
+	assert.Equal(t, "B", tables[1])
+	assert.Equal(t, "BEE", tables[2])
 }
