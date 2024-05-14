445bfe3bcccdbb325f991ecca3cccc1116a668c4
Ashley Harrison <ashley.harrison@grafana.com>
2024-05-14 13:47:41 +0100

    Storybook upgrade: Fix icon typing (#87793)
    fix iconOptions typing

--- packages/grafana-ui/src/utils/storybook/icons.ts
+++ packages/grafana-ui/src/utils/storybook/icons.ts
@@ -1,12 +1,11 @@
 import { getAvailableIcons } from '../../types';
 
-// @todo: figure out how best to type this.
-export const iconOptions: any = {
+export const iconOptions: Record<string, string | undefined> = {
   None: undefined,
   ...getAvailableIcons().reduce<Record<string, string>>((prev, c) => {
     return {
       ...prev,
-      [`Icon: ${c}`]: `${c}`,
+      [`${c}`]: `Icon: ${c}`,
     };
   }, {}),
 };
