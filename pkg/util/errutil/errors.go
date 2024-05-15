c4adcb5977d2fed4ee808fb17fa0e1040fcb1f78
Ryan McKinley <ryantxu@gmail.com>
2024-05-15 11:26:17 +0300

    return k8s status

--- pkg/util/errutil/errors.go
+++ pkg/util/errutil/errors.go
@@ -3,6 +3,9 @@ package errutil
 import (
 	"errors"
 	"fmt"
+
+	errorsK8s "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Base represents the static information about a specific error.
@@ -294,6 +297,9 @@ func (b Base) Is(err error) bool {
 	}
 }
 
+// Allow errorutil errors to be returned as informative k8s errors
+var _ = errorsK8s.APIStatus(&Error{})
+
 // Error is the error type for errors within Grafana, extending
 // the Go error type with Grafana specific metadata to reduce
 // boilerplate error handling for status codes and internationalization
@@ -363,6 +369,16 @@ func (e Error) MarshalJSON() ([]byte, error) {
 // Error implements the error interface.
 func (e Error) Error() string {
 	return fmt.Sprintf("[%s] %s", e.MessageID, e.LogMessage)
+}
+
+// When the error is rendered by an apiserver, this format is used
+func (e Error) Status() metav1.Status {
+	return metav1.Status{
+		Status:  metav1.StatusFailure,
+		Code:    e.Reason.Status().HTTPStatus(),
+		Reason:  metav1.StatusReason(e.Reason.Status()), // almost true
+		Message: e.PublicMessage,
+	}
 }
 
 // Unwrap is used by errors.As to iterate over the sequence of
