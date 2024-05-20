2fc533deb97be1e925fe703592e35c290df84f4f
ZuperZee <ole.kristian.kaaseth@gmail.com>
2024-05-20 22:33:42 +0200

    Fix loading bar shifts text

--- packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
+++ packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
@@ -16,17 +16,13 @@ const MILLISECONDS_PER_PIXEL = 2.4;
 const MIN_DURATION_MS = 500;
 const MAX_DURATION_MS = 4000;
 const DEFAULT_ANIMATION_DELAY = 300;
-const MAX_TRANSLATE_X = (100 / BAR_WIDTH) * 100;
 
 export function LoadingBar({ width, delay = DEFAULT_ANIMATION_DELAY, ariaLabel = 'Loading bar' }: LoadingBarProps) {
   const durationMs = Math.min(Math.max(Math.round(width * MILLISECONDS_PER_PIXEL), MIN_DURATION_MS), MAX_DURATION_MS);
   const styles = useStyles2(getStyles, delay, durationMs);
-  const containerStyles: CSSProperties = {
-    overflow: 'hidden',
-  };
 
   return (
-    <div style={containerStyles}>
+    <div className={styles.container}>
       <div aria-label={ariaLabel} className={styles.bar} />
     </div>
   );
@@ -35,21 +31,26 @@ export function LoadingBar({ width, delay = DEFAULT_ANIMATION_DELAY, ariaLabel =
 const getStyles = (theme: GrafanaTheme2, delay: number, duration: number) => {
   const animation = keyframes({
     '0%': {
-      transform: 'translateX(-100%)',
+      left: `-${BAR_WIDTH}%`,
     },
     // this gives us a delay between iterations
     '85%, 100%': {
-      transform: `translateX(${MAX_TRANSLATE_X}%)`,
+      left: '100%',
     },
   });
 
   return {
+    container: css({
+      overflow: 'hidden',
+      position: 'relative',
+      height: 1,
+    }),
     bar: css({
       width: BAR_WIDTH + '%',
       height: 1,
       background: 'linear-gradient(90deg, rgba(110, 159, 255, 0) 0%, #6E9FFF 80.75%, rgba(110, 159, 255, 0) 100%)',
-      transform: 'translateX(-100%)',
-      willChange: 'transform',
+      left: `-${BAR_WIDTH}%`,
+      position: 'absolute',
       [theme.transitions.handleMotion('no-preference')]: {
         animationName: animation,
         // an initial delay to prevent the loader from showing if the response is faster than the delay
