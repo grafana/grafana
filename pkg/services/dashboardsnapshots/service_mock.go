5773c43dfa760f742dbaf6840419ee7b09b54f5d
Lucy Chen <lucy.chen@grafana.com>
2024-05-21 17:30:14 -0400

    update mock

--- pkg/services/dashboardsnapshots/service_mock.go
+++ pkg/services/dashboardsnapshots/service_mock.go
@@ -120,27 +120,17 @@ func (_m *MockService) SearchDashboardSnapshots(_a0 context.Context, _a1 *GetDas
 }
 
 // ValidateDashboardExists provides a mock function with given fields: _a0, _a1, _a2
-func (_m *MockService) ValidateDashboardExists(_a0 context.Context, _a1 int64, _a2 string) (bool, error) {
+func (_m *MockService) ValidateDashboardExists(_a0 context.Context, _a1 int64, _a2 string) error {
 	ret := _m.Called(_a0, _a1, _a2)
 
-	var r0 bool
-	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, int64, string) (bool, error)); ok {
-		return rf(_a0, _a1, _a2)
-	}
-	if rf, ok := ret.Get(0).(func(context.Context, int64, string) bool); ok {
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, int64, string) error); ok {
 		r0 = rf(_a0, _a1, _a2)
 	} else {
-		r0 = ret.Get(0).(bool)
+		r0 = ret.Error(0)
 	}
 
-	if rf, ok := ret.Get(1).(func(context.Context, int64, string) error); ok {
-		r1 = rf(_a0, _a1, _a2)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
+	return r0
 }
 
 // NewMockService creates a new instance of MockService. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
