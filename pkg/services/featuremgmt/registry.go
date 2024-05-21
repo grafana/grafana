3466e67aaf56eafdf95d02b40c44c04a52095ab3
Jack Baldry <jack.baldry@grafana.com>
2024-05-21 19:03:01 +0100

    [v10.1.x] [v10.3.x] Update `nestedFolder` feature toggle description (#88123)
    [v10.3.x] Update `nestedFolder` feature toggle description (#87181)
    
    * Updated nestedFolder description
    
    * Update generated files
    
    
    
    * Empty commit
    
    ---------
    
    
    
    (cherry picked from commit c4c0c937e58836443737bff80e91a25de0e60082)
    
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>
    Co-authored-by: Isabel Matwawana <76437239+imatwawana@users.noreply.github.com>

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -237,7 +237,7 @@ var (
 		},
 		{
 			Name:        "nestedFolders",
-			Description: "Enable folder nesting",
+			Description: "Enable folder nesting for dashboards and alerts. Note that managed alert rules aren't supported for nested folders. This feature is only supported for test and development environments.",
 			Stage:       FeatureStagePublicPreview,
 			Owner:       grafanaBackendPlatformSquad,
 		},
