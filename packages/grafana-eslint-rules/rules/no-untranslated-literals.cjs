71b6b63ba9dd5432eb294c03dd84b05fa0c6c269
eledobleefe <laura.fernandez@grafana.com>
2024-05-23 17:00:10 +0200

    Change Literal to JSXText

--- packages/grafana-eslint-rules/rules/no-untranslated-literals.cjs
+++ packages/grafana-eslint-rules/rules/no-untranslated-literals.cjs
@@ -8,10 +8,7 @@ const createRule = ESLintUtils.RuleCreator(
 const noUntranslatedLiterals = createRule({
   create(context) {
     return {
-      Literal(node) {
-        if (node.value && isEmpty(node.value)) {
-          return;
-        }
+      JSXText(node) {
         const ancestors = context.getAncestors();
         const hasTransAncestor = ancestors.some((ancestor) => {
           return (
@@ -21,7 +18,7 @@ const noUntranslatedLiterals = createRule({
             ancestor.openingElement.name.name === 'Trans'
           );
         });
-        if (getValidation(node) && hasJSXElementParentOrGrandParent(node) && !hasTransAncestor) {
+        if (!isEmpty(node.value) && !hasTransAncestor) {
           context.report({
             node,
             messageId: 'noUntranslatedStrings',
@@ -44,63 +41,9 @@ const noUntranslatedLiterals = createRule({
   defaultOptions: [],
 });
 
-module.exports = noUntranslatedLiterals;
-
-// @ts-expect-error
-const getValidation = (node) => {
-  const parent = getParentIgnoringBinaryExpressions(node);
-
-  function isParentNodeStandard() {
-    if (!/^[\s]+$/.test(node.value) && typeof node.value === 'string' && parent.type.includes('JSX')) {
-      return parent.type !== AST_NODE_TYPES.JSXAttribute;
-    }
-
-    return false;
-  }
-
-  const standard = isParentNodeStandard();
-
-  return standard && parent.type !== AST_NODE_TYPES.JSXExpressionContainer;
-};
-// @ts-expect-error
-const getParentAndGrandParentType = (node) => {
-  const parent = getParentIgnoringBinaryExpressions(node);
-  const parentType = parent.type;
-  const grandParentType = parent.parent.type;
-
-  return {
-    parent,
-    parentType,
-    grandParentType,
-    grandParent: parent.parent,
-  };
-};
-
-// @ts-expect-error
-const hasJSXElementParentOrGrandParent = (node) => {
-  const parents = getParentAndGrandParentType(node);
-  const parentType = parents.parentType;
-  const grandParentType = parents.grandParentType;
-
-  return parentType === AST_NODE_TYPES.JSXFragment || parentType === AST_NODE_TYPES.JSXElement || grandParentType === AST_NODE_TYPES.JSXElement;
-};
-
-
-// @ts-expect-error
-const getParentIgnoringBinaryExpressions = (node) => {
-  let current = node;
-  while (current.parent.type === AST_NODE_TYPES.BinaryExpression) {
-    current = current.parent;
-  }
-  return current.parent;
-};
-
 // @ts-expect-error
 const isEmpty = (val) => {
-  let result = false;
-  if (typeof val === 'string') {
-    const cleaned = val.replaceAll(/\s/g, '').replaceAll(/[\n]/g, '');
-    result = cleaned.length === 0;
-  }
-  return result;
+  return !Boolean(val.trim());
 };
+
+module.exports = noUntranslatedLiterals;
