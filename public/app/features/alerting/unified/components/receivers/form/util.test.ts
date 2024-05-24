d045cd9b9114964878716b0db926243b0fabc218
Sonia Aguilar <33540275+soniaAguilarPeiron@users.noreply.github.com>
2024-05-24 13:35:48 +0200

    Alerting: Template selector in contact points form (#87689)
    * WIP
    
    * Refactor and update how we display these fields in the form
    
    * Add test for getTemplateOptions and udpate parseTemplates to handle minus simbol
    
    * fix betterer
    
    * Fix wrapper
    
    * Create new usePreviewTemplate to be reused from TemplatePreview and TemplateContentAndPreview
    
    * remove unnecessary check
    
    * track interactions
    
    * Include the whole content of the template in the preview
    
    * Update parseTemplates function to return default templates
    
    * handle nested templates in parseTemplates function
    
    * Missing border fixed, whitespaces preserved and no empty space at the bottom
    
    * remove unused styles and add a comment in test
    
    * Add missing error in getPreviewResults
    
    * fix styles for template selector containers
    
    * Alerting: PR feedback to move default templates into RTKQ (#88172)
    
    Move default templates to RTKQ API + constant
    
    * move parseTemplates to a utils file and refactor last part of this function
    
    * Keep selected options when loading exising input and when switching between tabs
    
    * Update descritpion in tabs
    
    * Fix not previewing when loading existing values
    
    * Update text addressing Brenda's feedback
    
    * Add test for matchesOnlyOneTemplate function
    
    * Add minheight to viewer container and fix getContentFromOptions function
    
    ---------
    
    Co-authored-by: Tom Ratcliffe <tom.ratcliffe@grafana.com>

--- public/app/features/alerting/unified/components/receivers/form/util.test.ts
+++ public/app/features/alerting/unified/components/receivers/form/util.test.ts
@@ -1,5 +1,6 @@
 import { ChannelValues, ReceiverFormValues } from '../../../types/receiver-form';
 
+import { matchesOnlyOneTemplate } from './fields/utils';
 import { DeprecatedAuthHTTPConfig, HTTPAuthConfig, normalizeFormValues } from './util';
 
 describe('normalizeFormValues', () => {
@@ -61,3 +62,25 @@ function createContactPoint(httpConfig: DeprecatedAuthHTTPConfig | HTTPAuthConfi
 
   return config;
 }
+
+describe('matchesOnlyOneTemplate', () => {
+  it('should return true when there is only one template and no other text', () => {
+    const fieldValue = '{{ template "nested" . }}';
+    expect(matchesOnlyOneTemplate(fieldValue)).toBe(true);
+  });
+
+  it('should return false when there is more than one template', () => {
+    const fieldValue = '{{ template "nested" . }}{{ template "nested2" . }}';
+    expect(matchesOnlyOneTemplate(fieldValue)).toBe(false);
+  });
+
+  it('should return false when there is other text outside the template', () => {
+    const fieldValue = '{{ template "nested" . }} some other text';
+    expect(matchesOnlyOneTemplate(fieldValue)).toBe(false);
+  });
+
+  it('should return false when there is no template', () => {
+    const fieldValue = 'some other text';
+    expect(matchesOnlyOneTemplate(fieldValue)).toBe(false);
+  });
+});
