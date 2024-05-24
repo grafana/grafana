c44b96e834a7f31cb0b7e735598e52dacbb918d3
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-05-24 15:12:27 +0100

    Update mocks to include RBAC access control logic

--- public/app/features/alerting/unified/mocks/server/handlers/silences.ts
+++ public/app/features/alerting/unified/mocks/server/handlers/silences.ts
@@ -4,22 +4,36 @@ import { mockSilences } from 'app/features/alerting/unified/mocks';
 import { MOCK_DATASOURCE_UID_BROKEN_ALERTMANAGER } from 'app/features/alerting/unified/mocks/server/handlers/datasources';
 
 const silencesListHandler = (silences = mockSilences) =>
-  http.get<{ datasourceUid: string }>('/api/alertmanager/:datasourceUid/api/v2/silences', ({ params }) => {
+  http.get<{ datasourceUid: string }>('/api/alertmanager/:datasourceUid/api/v2/silences', ({ params, request }) => {
     if (params.datasourceUid === MOCK_DATASOURCE_UID_BROKEN_ALERTMANAGER) {
       return HttpResponse.json({ traceId: '' }, { status: 502 });
+    }
+
+    // Server only responds with ACL if query param is sent
+    const accessControlQueryParam = new URL(request.url).searchParams.get('accessControl');
+    if (!accessControlQueryParam) {
+      const withoutAccessControl = silences.map(({ accessControl, ...silence }) => silence);
+      return HttpResponse.json(withoutAccessControl);
     }
     return HttpResponse.json(silences);
   });
 
 const silenceGetHandler = () =>
-  http.get<{ uuid: string }>('/api/alertmanager/:datasourceUid/api/v2/silence/:uuid', ({ params }) => {
+  http.get<{ uuid: string }>('/api/alertmanager/:datasourceUid/api/v2/silence/:uuid', ({ params, request }) => {
     const { uuid } = params;
     const matchingMockSilence = mockSilences.find((silence) => silence.id === uuid);
-    if (matchingMockSilence) {
-      return HttpResponse.json(matchingMockSilence);
+    if (!matchingMockSilence) {
+      return HttpResponse.json({ message: 'silence not found' }, { status: 404 });
     }
 
-    return HttpResponse.json({ message: 'silence not found' }, { status: 404 });
+    // Server only responds with ACL if query param is sent
+    const accessControlQueryParam = new URL(request.url).searchParams.get('accessControl');
+    if (!accessControlQueryParam) {
+      const { accessControl, ...silence } = matchingMockSilence;
+      return HttpResponse.json(silence);
+    }
+
+    return HttpResponse.json(matchingMockSilence);
   });
 
 export const silenceCreateHandler = () =>
