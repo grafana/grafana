c888cbd3f315cba15ea30f32e7a471779c30412a
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-05-24 00:03:19 -0300

    remove unnecessary message

--- pkg/services/store/entity/sqlstash/sql_storage_server_test.go
+++ pkg/services/store/entity/sqlstash/sql_storage_server_test.go
@@ -18,25 +18,6 @@ func TestMain(m *testing.M) {
 	testsuite.Run(m)
 }
 
-// TODO:
-// The testing strategy for this package is devided in layers of abstraction in
-// order to provide correctness, coverage and maintainability of tests. The
-// layers are the following, from most concrete to more abstract:
-//	1. Simple utility functions should be tested using the author's best
-//	   criteria.
-//	2. Utility functions that deal with low-level database operations (like
-//	   Query, QueryContext, Exec, etc.) should be tested with sqlmock and they
-//	   do not need to assert SQL code. Instead, focus on testing that it
-//	   correctly handles all the lower level code flows.
-//	3. SQL generated from templates should be tested using golden tests, using
-//	   at least the sqltemplate.MySQL dialect, and exercising every template
-//	   code branch at least once. For each loop (with the template `range`
-//	   keyword), the template should be exercised at least once with zero, one
-//	   and two elements. Use the function TestGenerateSQLExample in
-//	   queries_test.go to generate your golden files.
-//	2. Methods from the Entity Server implementation can omit testing the
-//	   executed
-
 func TestIsHealthy(t *testing.T) {
 	s := setUpTestServer(t)
 
