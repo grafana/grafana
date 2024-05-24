8cf7306546a52eda72f8d34e639091d554130515
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-05-24 16:46:54 +0100

    Add PoC for reading conf files and passing into webpack

---
+++ scripts/webpack/env-util.js
@@ -0,0 +1,29 @@
+const { parse } = require('ini');
+const { readFileSync, existsSync } = require('node:fs');
+
+const getEnvConfig = () => {
+  const defaultSettings = readFileSync(`./conf/defaults.ini`, {
+    encoding: 'utf-8',
+  });
+
+  const customSettings = existsSync(`./conf/custom.ini`)
+    ? readFileSync(`./conf/custom.ini`, {
+        encoding: 'utf-8',
+      })
+    : {};
+
+  const defaults = parse(defaultSettings);
+  const custom = parse(defaultSettings);
+
+  const merged = { ...defaults.frontend_dev, ...custom.frontend_dev };
+  // Take all frontend keys from the ini file and prefix with `frontend_dev_`,
+  // so they can be added to `process.env` elsewhere
+  return Object.entries(merged).reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [`frontend_dev_${key}`]: value,
+    };
+  }, {});
+};
+
+module.exports = getEnvConfig;
