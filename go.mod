1babcabdc9cd02a336aa53287a1643d398091f17
Matthias Loibl <mail@matthiasloibl.com>
2024-05-28 17:36:13 +0200

    pkg/tsdb/parca: Upgrade to using the flamegraph arrow

--- go.mod
+++ go.mod
@@ -11,8 +11,8 @@ replace cuelang.org/go => github.com/grafana/cue v0.0.0-20230926092038-971951014
 replace github.com/prometheus/prometheus => github.com/prometheus/prometheus v0.52.0
 
 require (
-	buf.build/gen/go/parca-dev/parca/bufbuild/connect-go v1.4.1-20221222094228-8b1d3d0f62e6.1 // @grafana/observability-traces-and-profiling
-	buf.build/gen/go/parca-dev/parca/protocolbuffers/go v1.33.0-20240414232344-9ca06271cb73.1 // @grafana/observability-traces-and-profiling
+	buf.build/gen/go/parca-dev/parca/bufbuild/connect-go v1.10.0-20240523185345-933eab74d046.1 // @grafana/observability-traces-and-profiling
+	buf.build/gen/go/parca-dev/parca/protocolbuffers/go v1.34.1-20240523185345-933eab74d046.1 // @grafana/observability-traces-and-profiling
 	cloud.google.com/go/kms v1.15.7 // @grafana/grafana-backend-group
 	cloud.google.com/go/storage v1.38.0 // @grafana/grafana-backend-group
 	cuelang.org/go v0.6.0-0.dev // @grafana/grafana-as-code
--- pkg/tsdb/parca/query.go
+++ pkg/tsdb/parca/query.go
@@ -1,23 +1,29 @@
 package parca
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
 	v1alpha1 "buf.build/gen/go/parca-dev/parca/protocolbuffers/go/parca/query/v1alpha1"
+	"github.com/apache/arrow/go/v15/arrow"
+	"github.com/apache/arrow/go/v15/arrow/array"
+	"github.com/apache/arrow/go/v15/arrow/ipc"
 	"github.com/bufbuild/connect-go"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/tracing"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
-	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/utils"
-	"github.com/grafana/grafana/pkg/tsdb/parca/kinds/dataquery"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/grafana/grafana/pkg/tsdb/cloudwatch/utils"
+	"github.com/grafana/grafana/pkg/tsdb/parca/kinds/dataquery"
 )
 
 type queryModel struct {
@@ -93,9 +99,8 @@ func makeProfileRequest(qm queryModel, query backend.DataQuery) *connect.Request
 					},
 				},
 			},
-			// We should change this to QueryRequest_REPORT_TYPE_FLAMEGRAPH_TABLE later on
 			// nolint:staticcheck
-			ReportType: v1alpha1.QueryRequest_REPORT_TYPE_FLAMEGRAPH_UNSPECIFIED,
+			ReportType: v1alpha1.QueryRequest_REPORT_TYPE_FLAMEGRAPH_ARROW,
 		},
 	}
 }
@@ -124,11 +129,219 @@ type CustomMeta struct {
 // tree back.
 func responseToDataFrames(resp *connect.Response[v1alpha1.QueryResponse]) *data.Frame {
 	if flameResponse, ok := resp.Msg.Report.(*v1alpha1.QueryResponse_Flamegraph); ok {
+		// TODO: Remove this old response type and all of its functions
 		frame := treeToNestedSetDataFrame(flameResponse.Flamegraph)
 		frame.Meta = &data.FrameMeta{PreferredVisualization: "flamegraph"}
 		return frame
+	} else if flameResponse, ok := resp.Msg.Report.(*v1alpha1.QueryResponse_FlamegraphArrow); ok {
+		frame := arrowToNestedSetDataFrame(flameResponse.FlamegraphArrow)
+		frame.Meta = &data.FrameMeta{PreferredVisualization: "flamegraph"}
+		return frame
 	} else {
-		panic("unknown report type returned from query")
+		// TODO: Can we be nicer about signaling users to update to have the latest APIs?
+		panic("unknown report type returned from query. update parca?")
+	}
+}
+
+func arrowToNestedSetDataFrame(flamegraph *v1alpha1.FlamegraphArrow) *data.Frame {
+	frame := data.NewFrame("response")
+
+	levelField := data.NewField("level", nil, []int64{})
+	valueField := data.NewField("value", nil, []int64{})
+	valueField.Config = &data.FieldConfig{Unit: normalizeUnit(flamegraph.Unit)}
+	selfField := data.NewField("self", nil, []int64{})
+	selfField.Config = &data.FieldConfig{Unit: normalizeUnit(flamegraph.Unit)}
+	labelField := data.NewField("label", nil, []string{})
+	frame.Fields = data.Fields{levelField, valueField, selfField, labelField}
+
+	arrowReader, err := ipc.NewReader(bytes.NewBuffer(flamegraph.GetRecord()))
+	if err != nil {
+		// TODO: Handle properly?
+		return nil
+	}
+	defer arrowReader.Release()
+
+	arrowReader.Next()
+	rec := arrowReader.Record()
+
+	arrowTable := array.NewTableFromRecords(arrowReader.Schema(), []arrow.Record{rec})
+	defer arrowTable.Release()
+
+	// TODO: Should we use a different chunkSize?
+	reader := array.NewTableReader(arrowTable, 0)
+	defer reader.Release()
+
+	fi, err := newFlamegraphIterator(reader)
+	if err != nil {
+		// TODO: Handle properly?
+		return nil
+	}
+
+	fi.iterate(func(name string, level, value, self int64) {
+		labelField.Append(name)
+		levelField.Append(level)
+		valueField.Append(value)
+		selfField.Append(self)
+	})
+
+	return frame
+}
+
+const (
+	FlamegraphFieldMappingFile     = "mapping_file"
+	FlamegraphFieldLocationAddress = "location_address"
+	FlamegraphFieldFunctionName    = "function_name"
+	FlamegraphFieldChildren        = "children"
+	FlamegraphFieldCumulative      = "cumulative"
+	FlamegraphFieldFlat            = "flat"
+)
+
+type flamegraphIterator struct {
+	columnChildren         *array.List
+	columnChildrenValues   *array.Uint32
+	columnCumulative       func(i int) int64
+	columnMappingFile      *array.Dictionary
+	columnMappingFileDict  *array.Binary
+	columnFunctionName     *array.Dictionary
+	columnFunctionNameDict *array.Binary
+	columnLocationAddress  *array.Uint64
+
+	nameBuilder    *bytes.Buffer
+	addressBuilder *bytes.Buffer
+}
+
+func newFlamegraphIterator(tr *array.TableReader) (*flamegraphIterator, error) {
+	if !tr.Next() {
+		return nil, fmt.Errorf("table reader has no record")
+	}
+	rec := tr.Record()
+	schema := rec.Schema()
+
+	columnChildren := rec.Column(schema.FieldIndices(FlamegraphFieldChildren)[0]).(*array.List)
+	columnChildrenValues := columnChildren.ListValues().(*array.Uint32)
+	columnCumulative := uintValue(rec.Column(schema.FieldIndices(FlamegraphFieldCumulative)[0]))
+
+	columnMappingFile := rec.Column(schema.FieldIndices(FlamegraphFieldMappingFile)[0]).(*array.Dictionary)
+	columnMappingFileDict := columnMappingFile.Dictionary().(*array.Binary)
+	columnFunctionName := rec.Column(schema.FieldIndices(FlamegraphFieldFunctionName)[0]).(*array.Dictionary)
+	columnFunctionNameDict := columnFunctionName.Dictionary().(*array.Binary)
+	columnLocationAddress := rec.Column(schema.FieldIndices(FlamegraphFieldLocationAddress)[0]).(*array.Uint64)
+
+	return &flamegraphIterator{
+		columnChildren:         columnChildren,
+		columnChildrenValues:   columnChildrenValues,
+		columnCumulative:       columnCumulative,
+		columnMappingFile:      columnMappingFile,
+		columnMappingFileDict:  columnMappingFileDict,
+		columnFunctionName:     columnFunctionName,
+		columnFunctionNameDict: columnFunctionNameDict,
+		columnLocationAddress:  columnLocationAddress,
+
+		nameBuilder:    &bytes.Buffer{},
+		addressBuilder: &bytes.Buffer{},
+	}, nil
+}
+
+func (fi *flamegraphIterator) iterate(fn func(name string, level, value, self int64)) {
+	type rowNode struct {
+		row   int
+		level int64
+	}
+	childrenStart, childrenEnd := fi.columnChildren.ValueOffsets(0)
+	stack := make([]rowNode, 0, childrenEnd-childrenStart)
+	var childrenValue int64 = 0
+
+	for i := int(childrenStart); i < int(childrenEnd); i++ {
+		child := int(fi.columnChildrenValues.Value(i))
+		childrenValue += fi.columnCumulative(child)
+		stack = append(stack, rowNode{row: child, level: 1})
+	}
+
+	cumulative := fi.columnCumulative(0)
+	fn("total", 0, cumulative, cumulative-childrenValue)
+
+	for {
+		if len(stack) == 0 {
+			break
+		}
+
+		// shift stack
+		node := stack[0]
+		stack = stack[1:]
+		childrenValue = 0
+
+		// Get the children for this node and add them to the stack if they exist.
+		start, end := fi.columnChildren.ValueOffsets(node.row)
+		children := make([]rowNode, 0, end-start)
+		for i := start; i < end; i++ {
+			child := fi.columnChildrenValues.Value(int(i))
+			if fi.columnChildrenValues.IsValid(int(child)) {
+				childrenValue += fi.columnCumulative(int(child))
+				children = append(children, rowNode{row: int(child), level: node.level + 1})
+			}
+		}
+		// prepend the new children to the top of the stack
+		stack = append(children, stack...)
+
+		cumulative := fi.columnCumulative(node.row)
+		name := fi.nodeName(node.row)
+		fn(name, node.level, cumulative, cumulative-childrenValue)
+	}
+}
+
+func (fi *flamegraphIterator) nodeName(row int) string {
+	fi.nameBuilder.Reset()
+	fi.addressBuilder.Reset()
+
+	if fi.columnMappingFile.IsValid(row) {
+		m := fi.columnMappingFileDict.ValueString(fi.columnMappingFile.GetValueIndex(row))
+		fi.nameBuilder.WriteString("[")
+		fi.nameBuilder.WriteString(getLastItem(m))
+		fi.nameBuilder.WriteString("]")
+		fi.nameBuilder.WriteString(" ")
+	}
+	if fi.columnFunctionName.IsValid(row) {
+		if f := fi.columnFunctionNameDict.ValueString(fi.columnFunctionName.GetValueIndex(row)); f != "" {
+			fi.nameBuilder.WriteString(f)
+			return fi.nameBuilder.String()
+		}
+	}
+
+	if fi.columnLocationAddress.IsValid(row) {
+		a := fi.columnLocationAddress.Value(row)
+		fi.addressBuilder.WriteString("0x")
+		fi.addressBuilder.WriteString(strconv.FormatUint(a, 16))
+	}
+
+	if fi.nameBuilder.Len() == 0 && fi.addressBuilder.Len() == 0 {
+		return "<unknown>"
+	} else {
+		return fi.nameBuilder.String() + fi.addressBuilder.String()
+	}
+}
+
+// uintValue is a wrapper to read different uint sizes.
+// Parca returns values encoded depending on the max value in an array.
+func uintValue(arr arrow.Array) func(i int) int64 {
+	switch b := arr.(type) {
+	case *array.Uint64:
+		return func(i int) int64 {
+			return int64(b.Value(i))
+		}
+	case *array.Uint32:
+		return func(i int) int64 {
+			return int64(b.Value(i))
+		}
+	case *array.Uint16:
+		return func(i int) int64 {
+			return int64(b.Value(i))
+		}
+	case *array.Uint8:
+		return func(i int) int64 {
+			return int64(b.Value(i))
+		}
+	default:
+		panic(fmt.Errorf("unsupported type %T", b))
 	}
 }
 
@@ -147,7 +360,7 @@ func treeToNestedSetDataFrame(tree *v1alpha1.Flamegraph) *data.Frame {
 	labelField := data.NewField("label", nil, []string{})
 	frame.Fields = data.Fields{levelField, valueField, selfField, labelField}
 
-	walkTree(tree.Root, func(level int64, value int64, name string, self int64) {
+	walkTree(tree.Root, func(level, value int64, name string, self int64) {
 		levelField.Append(level)
 		valueField.Append(value)
 		labelField.Append(name)
@@ -161,7 +374,7 @@ type Node struct {
 	Level int64
 }
 
-func walkTree(tree *v1alpha1.FlamegraphRootNode, fn func(level int64, value int64, name string, self int64)) {
+func walkTree(tree *v1alpha1.FlamegraphRootNode, fn func(level, value int64, name string, self int64)) {
 	stack := make([]*Node, 0, len(tree.Children))
 	var childrenValue int64 = 0
 
