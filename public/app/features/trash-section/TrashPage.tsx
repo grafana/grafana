4d040544a45ef40b0d4f2f0837ce9019aca6b5c9
Laura Benz <laura.benz@grafana.com>
2024-05-30 08:50:04 +0200

    feat: update search view and add to trash page

--- public/app/features/trash-section/TrashPage.tsx
+++ public/app/features/trash-section/TrashPage.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { memo } from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
-import { GrafanaTheme2 } from '@grafana/data';
-import { FilterInput, useStyles2 } from '@grafana/ui';
+import { FilterInput } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 import { t } from 'app/core/internationalization';
+import { GrafanaRouteComponentProps } from 'app/core/navigation/types';
 import { ActionRow } from 'app/features/search/page/components/ActionRow';
 import { getGrafanaSearcher } from 'app/features/search/service';
 
-import { useTrashStateManager } from './useTrashStateManager';
+import { BrowseView } from '../browse-dashboards/components/BrowseView';
+import { SearchView } from '../browse-dashboards/components/SearchView';
+import { getFolderPermissions } from '../browse-dashboards/permissions';
 
-const TrashPage = () => {
+import { useTrashStateManager } from './useTrashStateManager';
+// import {useDispatch} from "../../types";
+
+export interface Props extends GrafanaRouteComponentProps {}
+
+const TrashPage = memo(({ match }: Props) => {
+  const { uid: folderUID } = match.params;
+  // const dispatch = useDispatch(); TODO: will be used in useEffect()
+
   const [searchState, stateManager] = useTrashStateManager();
+  const isSearching = stateManager.hasSearchFilters();
+
+  const { canEditFolders, canEditDashboards } = getFolderPermissions(); // TODO: as the dashboards are separated from their folders does this still work?
+  const canSelect = canEditFolders || canEditDashboards;
 
   return (
     <Page navId="dashboards/trash">
@@ -35,9 +50,24 @@ const TrashPage = () => {
           onPanelTypeChange={stateManager.onPanelTypeChange}
           onSetIncludePanels={stateManager.onSetIncludePanels}
         />
+        <AutoSizer>
+          {({ width, height }) =>
+            isSearching ? (
+              <SearchView
+                canSelect={canSelect}
+                width={width}
+                height={height}
+                searchStateManager={stateManager}
+                searchState={searchState}
+              />
+            ) : (
+              <BrowseView canSelect={canSelect} width={width} height={height} folderUID={folderUID} />
+            )
+          }
+        </AutoSizer>
       </Page.Contents>
     </Page>
   );
-};
+});
 
 export default TrashPage;
