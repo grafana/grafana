c8febf31dc088dc1bc4b231207c90ba11390cc72
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-05-30 11:55:21 +0100

    Test that error is shown if attempting to update AM config after another user has done so

--- public/app/features/alerting/unified/mocks/server/handlers/alertmanagers.ts
+++ public/app/features/alerting/unified/mocks/server/handlers/alertmanagers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw';
 
+import alertmanagerConfigMock from 'app/features/alerting/unified/components/contact-points/__mocks__/alertmanager.config.mock.json';
 import { MOCK_SILENCE_ID_EXISTING, mockAlertmanagerAlert } from 'app/features/alerting/unified/mocks';
 import { defaultGrafanaAlertingConfigurationStatusResponse } from 'app/features/alerting/unified/mocks/alertmanagerApi';
 import { MOCK_DATASOURCE_UID_BROKEN_ALERTMANAGER } from 'app/features/alerting/unified/mocks/server/handlers/datasources';
@@ -26,5 +27,25 @@ export const alertmanagerAlertsListHandler = () =>
     ]);
   });
 
-const handlers = [alertmanagerAlertsListHandler(), grafanaAlertingConfigurationStatusHandler()];
+export const getGrafanaAlertmanagerConfigHandler = (config = alertmanagerConfigMock) =>
+  http.get('/api/alertmanager/grafana/config/api/v1/alerts', () => HttpResponse.json(config));
+
+const updateGrafanaAlertmanagerConfigHandler = () =>
+  http.post('/api/alertmanager/grafana/config/api/v1/alerts', () =>
+    HttpResponse.json({ message: 'configuration created' })
+  );
+
+const getGrafanaAlertmanagerTemplatePreview = () =>
+  http.post('/api/alertmanager/grafana/config/api/v1/templates/test', () =>
+    // TODO: Scaffold out template preview response as needed by tests
+    HttpResponse.json({})
+  );
+
+const handlers = [
+  alertmanagerAlertsListHandler(),
+  grafanaAlertingConfigurationStatusHandler(),
+  getGrafanaAlertmanagerConfigHandler(),
+  updateGrafanaAlertmanagerConfigHandler(),
+  getGrafanaAlertmanagerTemplatePreview(),
+];
 export default handlers;
