14f671e1e9e176906d8b4e9ba5208cc1193bb75d
Mihai Doarna <mihai.doarna@grafana.com>
2024-05-31 11:08:52 +0300

    SSO: Add oldSettings param to the Validate function from SSO settings (#88245)
    * add oldSettings param to the Validate function from SSO settings
    
    * update unit tests adding the missing param to Validate

--- pkg/login/social/connectors/okta_oauth_test.go
+++ pkg/login/social/connectors/okta_oauth_test.go
@@ -287,7 +287,7 @@ func TestSocialOkta_Validate(t *testing.T) {
 			if tc.requester == nil {
 				tc.requester = &user.SignedInUser{IsGrafanaAdmin: false}
 			}
-			err := s.Validate(context.Background(), tc.settings, tc.requester)
+			err := s.Validate(context.Background(), tc.settings, ssoModels.SSOSettings{}, tc.requester)
 			if tc.wantErr != nil {
 				require.ErrorIs(t, err, tc.wantErr)
 				return
