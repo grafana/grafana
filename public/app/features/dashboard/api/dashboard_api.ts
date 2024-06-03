c64ae7dc6225e1f69d75350e87d9ebdae670ae40
Ryan McKinley <ryantxu@gmail.com>
2024-06-03 16:25:52 +0300

    fix query for a specific uid

---
+++ public/app/features/dashboard/api/dashboard_api.ts
@@ -0,0 +1,98 @@
+import { config, getBackendSrv } from '@grafana/runtime';
+import { ScopedResourceClient } from 'app/features/apiserver/client';
+import { ResourceClient } from 'app/features/apiserver/types';
+import { SaveDashboardCommand } from 'app/features/dashboard/components/SaveDashboard/types';
+import { dashboardWatcher } from 'app/features/live/dashboard/dashboardWatcher';
+import { DeleteDashboardResponse } from 'app/features/manage-dashboards/types';
+import { DashboardDTO, DashboardDataDTO } from 'app/types';
+
+export interface DashboardAPI {
+  dashboardExists(uid: string): Promise<boolean>;
+  getDashboardDTO(uid: string): Promise<DashboardDTO>;
+  deleteDashboard(uid: string, showSuccessAlert: boolean): Promise<DeleteDashboardResponse>;
+  saveDashboard(options: SaveDashboardCommand): Promise<unknown>;
+}
+
+// Implemented using /api/dashboards/*
+class LegacyDashboardAPI implements DashboardAPI {
+  constructor() {}
+
+  saveDashboard(options: SaveDashboardCommand): Promise<unknown> {
+    dashboardWatcher.ignoreNextSave();
+
+    return getBackendSrv().post('/api/dashboards/db/', {
+      dashboard: options.dashboard,
+      message: options.message ?? '',
+      overwrite: options.overwrite ?? false,
+      folderUid: options.folderUid,
+    });
+  }
+
+  dashboardExists(uid: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteDashboard(uid: string, showSuccessAlert: boolean): Promise<DeleteDashboardResponse> {
+    return getBackendSrv().delete<DeleteDashboardResponse>(
+      `/api/dashboards/uid/${uid}`, { showSuccessAlert });
+  }
+
+  getDashboardDTO(uid: string): Promise<DashboardDTO> {
+    return getBackendSrv().get<DashboardDTO>(`/api/dashboards/uid/${uid}`);
+  }
+}
+
+// Implemented using /apis/dashboards.grafana.app/*
+class K8sDashboardAPI implements DashboardAPI {
+  private client: ResourceClient<DashboardDataDTO>;
+  constructor(private legacy: DashboardAPI) {
+    this.client = new ScopedResourceClient<DashboardDataDTO>({
+      group: 'dashboard.grafana.app',
+      version: 'v0alpha1',
+      resource: 'dashboards',
+    });
+  }
+
+  saveDashboard(options: SaveDashboardCommand): Promise<unknown> {
+    return this.legacy.saveDashboard(options);
+  }
+
+  async dashboardExists(uid: string): Promise<boolean> {
+    try {
+      const r = await this.client.get(uid);
+      if (r.metadata.name) {
+        return Promise.resolve(true);
+      }
+    } catch {}
+    return Promise.resolve(false);
+  }
+
+  deleteDashboard(uid: string, showSuccessAlert: boolean): Promise<DeleteDashboardResponse> {
+    return this.legacy.deleteDashboard(uid, showSuccessAlert);
+  }
+
+  async getDashboardDTO(uid: string): Promise<DashboardDTO> {
+    const d = await this.client.get(uid);
+    const m = await this.client.subresource<object>(uid, 'access');
+    return {
+      meta: {
+        ...m,
+        isNew: false,
+        isFolder: false,
+        uid: d.metadata.name,
+      },
+      dashboard: d.spec,
+    };
+  }
+}
+
+let instance: DashboardAPI | undefined = undefined;
+
+export function getDashboardAPI() {
+  if (!instance) {
+    const legacy = new LegacyDashboardAPI();
+    instance = config.featureToggles.grafanaAPIServerWithExperimentalAPIs 
+      ? new K8sDashboardAPI(legacy) : legacy;
+  }
+  return instance;
+}
