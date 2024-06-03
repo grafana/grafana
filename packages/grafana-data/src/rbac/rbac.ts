4c465432503cb232edcb590cd43e372025cdec5a
Jack Westbrook <jack.westbrook@gmail.com>
2024-06-03 12:18:32 +0200

    feat(grafana-data): create rbac functions for checking permissions

---
+++ packages/grafana-data/src/rbac/rbac.ts
@@ -0,0 +1,30 @@
+import { CurrentUserDTO } from '../types';
+
+export interface CurrentUser extends Omit<CurrentUserDTO, 'lightTheme'> {}
+
+export function userHasPermission(action: string, user: CurrentUser): boolean {
+  return !!user.permissions?.[action];
+}
+
+export function userHasPermissionInMetadata(
+  action: string,
+  object: {
+    accessControl?: Record<string, boolean>;
+  }
+): boolean {
+  return !!object.accessControl?.[action];
+}
+
+export function userHasAllPermissions(actions: string[], user: CurrentUser) {
+  if (actions.every((action) => userHasPermission(action, user))) {
+    return [];
+  }
+  return ['Reject'];
+}
+
+export function userHasAnyPermission(actions: string[], user: CurrentUser) {
+  if (actions.some((action) => userHasPermission(action, user))) {
+    return [];
+  }
+  return ['Reject'];
+}
