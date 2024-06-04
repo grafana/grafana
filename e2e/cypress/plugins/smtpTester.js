b44f92619423e3e790ec2148093ad9d9a6dbc9b5
Agnès Toulet <35176601+AgnesToulet@users.noreply.github.com>
2024-06-04 10:30:36 +0200

    apply suggestions + cleanup logs

--- e2e/cypress/plugins/smtpTester.js
+++ e2e/cypress/plugins/smtpTester.js
@@ -1,28 +1,28 @@
 const ms = require('smtp-tester');
 
+const PORT = 7777;
+
 const initialize = (on, config) => {
   // starts the SMTP server at localhost:7777
-  const port = 7777;
-  const mailServer = ms.init(port);
-  console.log('mail server at port %d', port);
+  const mailServer = ms.init(PORT);
+  console.log('mail server at port %d', PORT);
 
   let lastEmail = {};
 
   // process all emails
   mailServer.bind((addr, id, email) => {
-    console.log('email: ', email);
     lastEmail[email.headers.to] = email;
   });
 
   on('task', {
-    resetEmails(email) {
-      console.log('reset all emails');
-      if (email) {
-        delete lastEmail[email];
+    resetEmails(recipient) {
+      if (recipient) {
+        console.log('reset all emails for recipient %s', recipient);
+        delete lastEmail[recipient];
       } else {
+        console.log('reset all emails');
         lastEmail = {};
       }
-      return null;
     },
 
     getLastEmail(email) {
