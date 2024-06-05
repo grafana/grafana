bf57cea68d4b5be856fc02c3cec67aa11a5a9ba1
Gilles De Mey <gilles.de.mey@gmail.com>
2024-06-05 13:03:56 +0200

    adds test for removing from group

--- public/app/features/alerting/unified/reducers/ruler/ruleGroups.test.ts
+++ public/app/features/alerting/unified/reducers/ruler/ruleGroups.test.ts
@@ -1,12 +1,12 @@
-import { RulerRuleGroupDTO } from 'app/types/unified-alerting-dto';
+import { PostableRulerRuleGroupDTO } from 'app/types/unified-alerting-dto';
 
-import { mockRulerGrafanaRule } from '../../mocks';
+import { mockRulerAlertingRule, mockRulerGrafanaRule, mockRulerRecordingRule } from '../../mocks';
 
-import { pauseRuleAction, ruleGroupReducer } from './ruleGroups';
+import { deleteRuleAction, pauseRuleAction, ruleGroupReducer } from './ruleGroups';
 
-describe('test pausing rules', () => {
+describe('pausing rules', () => {
   it('should pause a single rule in a group', () => {
-    const initialGroup: RulerRuleGroupDTO = {
+    const initialGroup: PostableRulerRuleGroupDTO = {
       name: 'group-1',
       interval: '5m',
       rules: [
@@ -29,6 +29,57 @@ describe('test pausing rules', () => {
     expect(output).toHaveProperty('rules.1.grafana_alert.is_paused', true);
 
     // use this to assert no regressions and rule group order / data structure
+    expect(output).toMatchSnapshot();
+  });
+});
+
+describe('removing a rule', () => {
+  it('should remove a Grafana managed ruler rule without touching other rules', () => {
+    const ruleToDelete = mockRulerGrafanaRule({}, { uid: '2' });
+
+    const initialGroup: PostableRulerRuleGroupDTO = {
+      name: 'group-1',
+      interval: '5m',
+      rules: [mockRulerGrafanaRule({}, { uid: '1' }), ruleToDelete, mockRulerGrafanaRule({}, { uid: '3' })],
+    };
+
+    const action = deleteRuleAction(ruleToDelete);
+    const output = ruleGroupReducer(initialGroup, action);
+
+    expect(output).toHaveProperty('rules');
+    expect(output.rules).toHaveLength(2);
+    expect(output.rules[0]).toStrictEqual(initialGroup.rules[0]);
+    expect(output.rules[1]).toStrictEqual(initialGroup.rules[2]);
+
+    expect(output).toMatchSnapshot();
+  });
+
+  it('should remove a Data source managed ruler rule without touching other rules', () => {
+    const ruleToDelete = mockRulerAlertingRule({
+      alert: 'delete me',
+    });
+
+    const initialGroup: PostableRulerRuleGroupDTO = {
+      name: 'group-1',
+      interval: '5m',
+      rules: [
+        mockRulerAlertingRule({ alert: 'do not delete me' }),
+        ruleToDelete,
+        mockRulerRecordingRule({
+          record: 'do not delete me',
+        }),
+      ],
+    };
+
+    const action = deleteRuleAction(ruleToDelete);
+    const output = ruleGroupReducer(initialGroup, action);
+
+    expect(output).toHaveProperty('rules');
+
+    expect(output.rules).toHaveLength(2);
+    expect(output.rules[0]).toStrictEqual(initialGroup.rules[0]);
+    expect(output.rules[1]).toStrictEqual(initialGroup.rules[2]);
+
     expect(output).toMatchSnapshot();
   });
 });
