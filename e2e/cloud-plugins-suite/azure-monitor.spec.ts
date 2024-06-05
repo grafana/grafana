696462632f28d91a89dc9368461478dc64e1b78c
Andreas Christou <andreas.christou@grafana.com>
2024-06-05 12:02:46 +0100

    Azure: Update resource group (#88690)
    Update azure e2e tests

--- e2e/cloud-plugins-suite/azure-monitor.spec.ts
+++ e2e/cloud-plugins-suite/azure-monitor.spec.ts
@@ -221,7 +221,7 @@ describe('Azure monitor datasource', () => {
           .click();
         e2eSelectors.queryEditor.argsQueryEditor.subscriptions.input().find('input').type('datasources{enter}');
         e2e.components.CodeEditor.container().type(
-          "Resources | where resourceGroup == 'cloud-plugins-e2e-test' | project name, resourceGroup"
+          "Resources | where resourceGroup == 'cloud-plugins-e2e-test-azmon' | project name, resourceGroup"
         );
         e2e.components.PanelEditor.toggleTableView().click({ force: true });
       },
@@ -280,7 +280,7 @@ describe('Azure monitor datasource', () => {
     e2e.pages.Dashboard.SubMenu.submenuItemLabels('resourceGroups')
       .parent()
       .find('input')
-      .type('cloud-plugins-e2e-test{downArrow}{enter}');
+      .type('cloud-plugins-e2e-test-azmon{downArrow}{enter}');
     e2e.pages.Dashboard.SubMenu.submenuItemLabels('namespaces').parent().find('button').click();
     e2e.pages.Dashboard.SubMenu.submenuItemLabels('namespaces')
       .parent()
