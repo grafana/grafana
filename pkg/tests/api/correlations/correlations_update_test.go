f3adc445640331926de853d6df5e0f1116a9c5b9
Piotr Jamroz <pm.jamroz@gmail.com>
2024-06-06 12:54:27 +0200

    Make the flakiness error more verbose

--- pkg/tests/api/correlations/correlations_update_test.go
+++ pkg/tests/api/correlations/correlations_update_test.go
@@ -48,8 +48,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 			url:  fmt.Sprintf("/api/datasources/uid/%s/correlations/%s", "some-ds-uid", "some-correlation-uid"),
 			body: ``,
 		})
-		require.Equal(t, http.StatusUnauthorized, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -58,6 +56,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Unauthorized", response.Message)
+		require.Equal(t, http.StatusUnauthorized, res.StatusCode)
 
 		require.NoError(t, res.Body.Close())
 	})
@@ -68,8 +67,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 			body: `{}`,
 			user: editorUser,
 		})
-		require.Equal(t, http.StatusForbidden, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -78,6 +75,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Contains(t, response.Message, "Permissions needed: datasources:write")
+		require.Equal(t, http.StatusForbidden, res.StatusCode)
 
 		require.NoError(t, res.Body.Close())
 	})
@@ -90,8 +88,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 			}`,
 			user: adminUser,
 		})
-		require.Equal(t, http.StatusNotFound, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -100,6 +96,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Data source not found", response.Message)
+		require.Equal(t, http.StatusNotFound, res.StatusCode)
 
 		require.NoError(t, res.Body.Close())
 	})
@@ -112,8 +109,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				"label": ""
 			}`,
 		})
-		require.Equal(t, http.StatusNotFound, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -122,6 +117,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation not found", response.Message)
+		require.Equal(t, http.StatusNotFound, res.StatusCode)
 
 		require.NoError(t, res.Body.Close())
 	})
@@ -141,8 +137,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				"label": "some-label"
 			}`,
 		})
-		require.Equal(t, http.StatusForbidden, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -151,6 +145,8 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation can only be edited via provisioning", response.Message)
+		require.Equal(t, http.StatusForbidden, res.StatusCode)
+
 
 		require.NoError(t, res.Body.Close())
 	})
@@ -168,8 +164,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 			user: adminUser,
 			body: `{}`,
 		})
-		require.Equal(t, http.StatusBadRequest, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -178,6 +172,8 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "At least one of label, description or config is required", response.Message)
+		require.Equal(t, http.StatusBadRequest, res.StatusCode)
+
 		require.NoError(t, res.Body.Close())
 
 		// empty body
@@ -186,8 +182,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 			user: adminUser,
 			body: ``,
 		})
-		require.Equal(t, http.StatusBadRequest, res.StatusCode)
-
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -195,6 +189,8 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "At least one of label, description or config is required", response.Message)
+		require.Equal(t, http.StatusBadRequest, res.StatusCode)
+
 		require.NoError(t, res.Body.Close())
 
 		// all set to null
@@ -207,8 +203,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 						"config": null
 					}`,
 		})
-		require.Equal(t, http.StatusBadRequest, res.StatusCode)
-
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -216,6 +210,8 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "At least one of label, description or config is required", response.Message)
+		require.Equal(t, http.StatusBadRequest, res.StatusCode)
+
 		require.NoError(t, res.Body.Close())
 	})
 
@@ -234,8 +230,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				"label": "updated label"
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
-
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -244,6 +238,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "updated label", response.Result.Label)
 		require.NoError(t, res.Body.Close())
 	})
@@ -277,7 +272,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				}
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
 
 		responseBody, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
@@ -287,6 +281,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "1", response.Result.Label)
 		require.Equal(t, "1", response.Result.Description)
 		require.Equal(t, "field", response.Result.Config.Field)
@@ -302,7 +297,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				"label": "2"
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
 
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
@@ -311,6 +305,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "2", response.Result.Label)
 		require.Equal(t, "1", response.Result.Description)
 		require.Equal(t, "field", response.Result.Config.Field)
@@ -325,7 +320,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				"description": "2"
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
 
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
@@ -334,6 +328,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "2", response.Result.Label)
 		require.Equal(t, "2", response.Result.Description)
 		require.Equal(t, "field", response.Result.Config.Field)
@@ -352,7 +347,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				}
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
 
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
@@ -361,6 +355,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "2", response.Result.Label)
 		require.Equal(t, "2", response.Result.Description)
 		require.Equal(t, "name", response.Result.Config.Field)
@@ -377,7 +372,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				}
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
 
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
@@ -386,6 +380,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "2", response.Result.Label)
 		require.Equal(t, "2", response.Result.Description)
 		require.Equal(t, "newName", response.Result.Config.Field)
@@ -402,7 +397,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				}
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
 
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
@@ -411,6 +405,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "2", response.Result.Label)
 		require.Equal(t, "2", response.Result.Description)
 		require.Equal(t, "newName", response.Result.Config.Field)
@@ -429,8 +424,6 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 				}
 			}`,
 		})
-		require.Equal(t, http.StatusOK, res.StatusCode)
-
 		responseBody, err = io.ReadAll(res.Body)
 		require.NoError(t, err)
 
@@ -438,6 +431,7 @@ func TestIntegrationUpdateCorrelation(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, "Correlation updated", response.Message)
+		require.Equal(t, http.StatusOK, res.StatusCode)
 		require.Equal(t, "", response.Result.Label)
 		require.Equal(t, "", response.Result.Description)
 		require.Equal(t, "", response.Result.Config.Field)
