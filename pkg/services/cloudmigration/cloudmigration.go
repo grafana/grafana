c79c7258d5faeae553fa979d3f9d4e74d0ebbcd2
Leandro Deveikis <leandro.deveikis@gmail.com>
2024-06-06 14:05:33 -0300

    add names to interface parameters

--- pkg/services/cloudmigration/cloudmigration.go
+++ pkg/services/cloudmigration/cloudmigration.go
@@ -8,20 +8,20 @@ import (
 
 type Service interface {
 	// GetToken Returns the cloud migration token if it exists.
-	GetToken(context.Context) (gcom.TokenView, error)
+	GetToken(ctx context.Context) (gcom.TokenView, error)
 	// CreateToken Creates a cloud migration token.
-	CreateToken(context.Context) (CreateAccessTokenResponse, error)
+	CreateToken(ctx context.Context) (CreateAccessTokenResponse, error)
 	// ValidateToken Sends a request to CMS to test the token.
-	ValidateToken(context.Context, CloudMigration) error
+	ValidateToken(ctx context.Context, mig CloudMigration) error
 	DeleteToken(ctx context.Context, uid string) error
 
-	CreateMigration(context.Context, CloudMigrationRequest) (*CloudMigrationResponse, error)
-	GetMigration(ctx context.Context, uid string) (*CloudMigration, error)
-	DeleteMigration(ctx context.Context, uid string) (*CloudMigration, error)
-	UpdateMigration(ctx context.Context, uid string, request CloudMigrationRequest) (*CloudMigrationResponse, error)
+	CreateMigration(ctx context.Context, req CloudMigrationRequest) (*CloudMigrationResponse, error)
+	GetMigration(ctx context.Context, migUID string) (*CloudMigration, error)
+	DeleteMigration(ctx context.Context, migUID string) (*CloudMigration, error)
+	UpdateMigration(ctx context.Context, migUID string, request CloudMigrationRequest) (*CloudMigrationResponse, error)
 	GetMigrationList(context.Context) (*CloudMigrationListResponse, error)
 
-	RunMigration(ctx context.Context, uid string) (*MigrateDataResponseDTO, error)
+	RunMigration(ctx context.Context, migUID string) (*MigrateDataResponseDTO, error)
 	GetMigrationStatus(ctx context.Context, runUID string) (*CloudMigrationRun, error)
-	GetMigrationRunList(context.Context, string) (*CloudMigrationRunList, error)
+	GetMigrationRunList(ctx context.Context, migUID string) (*CloudMigrationRunList, error)
 }
