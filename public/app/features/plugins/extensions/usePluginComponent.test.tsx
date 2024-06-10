a34e08de82e7b5951028db0e7b542ddb90e1dad1
Ashley Harrison <ashley.harrison@grafana.com>
2024-06-10 10:24:10 +0100

    fix 2 more violations

--- public/app/features/plugins/extensions/usePluginComponent.test.tsx
+++ public/app/features/plugins/extensions/usePluginComponent.test.tsx
@@ -1,6 +1,5 @@
 import { act, render, screen } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
-import React from 'react';
 
 import { PluginExtensionTypes } from '@grafana/data';
 
