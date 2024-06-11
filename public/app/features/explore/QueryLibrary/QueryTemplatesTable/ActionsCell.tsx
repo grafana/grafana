6847ae5c6154b7742a12320941ab126e21ebb229
Kristina Durivage <kristina.durivage@grafana.com>
2024-06-11 12:54:46 -0500

    WIP

--- public/app/features/explore/QueryLibrary/QueryTemplatesTable/ActionsCell.tsx
+++ public/app/features/explore/QueryLibrary/QueryTemplatesTable/ActionsCell.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
+import { reportInteraction, getAppEvents } from '@grafana/runtime';
 import { DataQuery } from '@grafana/schema';
+import { IconButton } from '@grafana/ui';
+import { notifyApp } from 'app/core/actions';
+import { createSuccessNotification } from 'app/core/copy/appNotification';
+import { t } from 'app/core/internationalization';
+import { dispatch } from 'app/store/store';
+import { ShowConfirmModalEvent } from 'app/types/events';
 
 import ExploreRunQueryButton from '../../ExploreRunQueryButton';
 
@@ -9,8 +16,39 @@ interface ActionsCellProps {
   rootDatasourceUid?: string;
 }
 
+const onDeleteQuery = () => {
+  const performDelete = (queryId: string) => {
+    //deleteHistoryItem(queryId);
+    dispatch(notifyApp(createSuccessNotification(t('explore.query-library.query-deleted', 'Query deleted'))));
+    reportInteraction('grafana_explore_query_library_deleted');
+  };
+
+  getAppEvents().publish(
+    new ShowConfirmModalEvent({
+      title: t('explore.rich-history-card.delete-query-confirmation-title', 'Delete'),
+      text: t(
+        'explore.rich-history-card.delete-starred-query-confirmation-text',
+        'Are you sure you want to permanently delete your starred query?'
+      ),
+      yesText: t('explore.rich-history-card.confirm-delete', 'Delete'),
+      icon: 'trash-alt',
+      onConfirm: () => performDelete(query),
+    })
+  );
+};
+
 function ActionsCell({ query, rootDatasourceUid }: ActionsCellProps) {
-  return <ExploreRunQueryButton queries={query ? [query] : []} rootDatasourceUid={rootDatasourceUid} />;
+  return (
+    <div>
+      <IconButton
+        name="trash-alt"
+        title={t('explore.query-library.delete-query', 'Delete query')}
+        tooltip={t('explore.query-library.delete-query', 'Delete query')}
+        onClick={onDeleteQuery}
+      />
+      <ExploreRunQueryButton queries={query ? [query] : []} rootDatasourceUid={rootDatasourceUid} />;
+    </div>
+  );
 }
 
 export default ActionsCell;
