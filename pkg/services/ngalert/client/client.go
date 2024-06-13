f76b5f6d95593e8aaa3445367e5fa8a6b9261ab2
Alex Weaver <weaver.alex.d@gmail.com>
2024-06-13 11:38:10 -0500

    Add TracedClient

--- pkg/services/ngalert/client/client.go
+++ pkg/services/ngalert/client/client.go
@@ -7,6 +7,9 @@ import (
 	"strconv"
 
 	"github.com/grafana/dskit/instrument"
+	"github.com/grafana/grafana/pkg/infra/tracing"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // Requester executes an HTTP request.
@@ -14,7 +17,7 @@ type Requester interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-// TimedClient instruments a request. It implements Requester.
+// TimedClient instruments a request with metrics. It implements Requester.
 type TimedClient struct {
 	client    Requester
 	collector instrument.Collector
@@ -69,4 +72,42 @@ func TimeRequest(ctx context.Context, operation string, coll instrument.Collecto
 	err := instrument.CollectedRequest(ctx, fmt.Sprintf("%s %s", request.Method, operation),
 		coll, toStatusCode, doRequest)
 	return response, err
+}
+
+// TracedClient instruments a request with tracing. It implements Requester.
+type TracedClient struct {
+	client Requester
+	tracer tracing.Tracer
+}
+
+func NewTracedClient(client Requester, tracer tracing.Tracer) *TracedClient {
+	return &TracedClient{
+		client: client,
+		tracer: tracer,
+	}
+}
+
+// Do executes the request.
+func (c TracedClient) Do(r *http.Request) (*http.Response, error) {
+	url := r.URL.Path
+	method := r.Method
+	name := fmt.Sprintf("HTTP %s %s", method, url)
+	ctx, span := c.tracer.Start(r.Context(), name, trace.WithAttributes(
+		attribute.String("http.method", method),
+		attribute.String("http.url", url),
+	))
+	defer span.End()
+
+	c.tracer.Inject(ctx, r.Header, span)
+
+	r = r.WithContext(ctx)
+	resp, err := c.client.Do(r)
+	span.SetAttributes(attribute.Int("http.status_code", resp.StatusCode))
+
+	return resp, err
+}
+
+// RoundTrip implements the RoundTripper interface.
+func (c TracedClient) RoundTrip(r *http.Request) (*http.Response, error) {
+	return c.Do(r)
 }
