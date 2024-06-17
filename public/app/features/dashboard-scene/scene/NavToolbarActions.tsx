4760d68a5720bdfdd7d637184644686115e0b20c
Sergej-Vlasov <37613182+Sergej-Vlasov@users.noreply.github.com>
2024-06-17 12:31:52 +0200

    DashboardScene: Adjust a11y tests errors (#89275)
    adjust a11y tests errors

--- public/app/features/dashboard-scene/scene/NavToolbarActions.tsx
+++ public/app/features/dashboard-scene/scene/NavToolbarActions.tsx
@@ -566,7 +566,12 @@ export function ToolbarActions({ dashboard }: Props) {
             Save dashboard
           </Button>
           <Dropdown overlay={menu}>
-            <Button icon="angle-down" variant={isDirty ? 'primary' : 'secondary'} size="sm" />
+            <Button
+              aria-label="More save options"
+              icon="angle-down"
+              variant={isDirty ? 'primary' : 'secondary'}
+              size="sm"
+            />
           </Dropdown>
         </ButtonGroup>
       );
