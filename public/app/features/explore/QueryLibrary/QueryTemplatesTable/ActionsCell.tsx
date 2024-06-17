7c6191ea2716140dfdf9920c4333c7bdcb2ffa70
Kristina Durivage <kristina.durivage@grafana.com>
2024-06-17 10:05:19 -0500

    WIP

--- public/app/features/explore/QueryLibrary/QueryTemplatesTable/ActionsCell.tsx
+++ public/app/features/explore/QueryLibrary/QueryTemplatesTable/ActionsCell.tsx
@@ -6,47 +6,55 @@ import { IconButton } from '@grafana/ui';
 import { notifyApp } from 'app/core/actions';
 import { createSuccessNotification } from 'app/core/copy/appNotification';
 import { t } from 'app/core/internationalization';
+import { useDeleteQueryTemplateMutation } from 'app/features/query-library';
 import { dispatch } from 'app/store/store';
 import { ShowConfirmModalEvent } from 'app/types/events';
 
 import ExploreRunQueryButton from '../../ExploreRunQueryButton';
 
 interface ActionsCellProps {
+  queryUid?: string;
   query?: DataQuery;
   rootDatasourceUid?: string;
 }
 
-const onDeleteQuery = () => {
-  const performDelete = (queryId: string) => {
-    //deleteHistoryItem(queryId);
-    dispatch(notifyApp(createSuccessNotification(t('explore.query-library.query-deleted', 'Query deleted'))));
-    reportInteraction('grafana_explore_query_library_deleted');
+function ActionsCell({ query, rootDatasourceUid, queryUid }: ActionsCellProps) {
+  const [deleteQueryTemplate] = useDeleteQueryTemplateMutation();
+
+  const onDeleteQuery = (queryUid: string) => {
+    const performDelete = (queryUid: string) => {
+      deleteQueryTemplate({ uid: queryUid });
+      dispatch(notifyApp(createSuccessNotification(t('explore.query-library.query-deleted', 'Query deleted'))));
+      reportInteraction('grafana_explore_query_library_deleted');
+    };
+
+    getAppEvents().publish(
+      new ShowConfirmModalEvent({
+        title: t('explore.query-library.delete-query-title', 'Delete'),
+        text: t(
+          'explore.query-library.delete-query-text',
+          'Are you sure you want to permanently delete your query from the query library?'
+        ),
+        yesText: t('query-library.delete-query-button', 'Delete'),
+        icon: 'trash-alt',
+        onConfirm: () => performDelete(queryUid),
+      })
+    );
   };
 
-  getAppEvents().publish(
-    new ShowConfirmModalEvent({
-      title: t('explore.rich-history-card.delete-query-confirmation-title', 'Delete'),
-      text: t(
-        'explore.rich-history-card.delete-starred-query-confirmation-text',
-        'Are you sure you want to permanently delete your starred query?'
-      ),
-      yesText: t('explore.rich-history-card.confirm-delete', 'Delete'),
-      icon: 'trash-alt',
-      onConfirm: () => performDelete(query),
-    })
-  );
-};
-
-function ActionsCell({ query, rootDatasourceUid }: ActionsCellProps) {
   return (
     <div>
       <IconButton
         name="trash-alt"
         title={t('explore.query-library.delete-query', 'Delete query')}
         tooltip={t('explore.query-library.delete-query', 'Delete query')}
-        onClick={onDeleteQuery}
+        onClick={() => {
+          if (queryUid) {
+            onDeleteQuery(queryUid);
+          }
+        }}
       />
-      <ExploreRunQueryButton queries={query ? [query] : []} rootDatasourceUid={rootDatasourceUid} />;
+      <ExploreRunQueryButton queries={query ? [query] : []} rootDatasourceUid={rootDatasourceUid} />
     </div>
   );
 }
