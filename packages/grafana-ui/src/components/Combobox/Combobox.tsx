c9967d472b97d62ad95250670558f8121f68823a
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-06-17 14:16:21 +0200

    use react-virtual

--- packages/grafana-ui/src/components/Combobox/Combobox.tsx
+++ packages/grafana-ui/src/components/Combobox/Combobox.tsx
@@ -1,5 +1,6 @@
 import { useCombobox } from 'downshift';
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
+import { useVirtual } from 'react-virtual';
 
 import { Icon } from '../Icon/Icon';
 import { Input, Props as InputProps } from '../Input/Input';
@@ -33,31 +34,64 @@ function itemFilter(inputValue: string) {
   };
 }
 
+function estimateSize() {
+  return 60;
+}
+
 export const Combobox = ({ options, onChange, value, ...restProps }: ComboboxProps) => {
   const [items, setItems] = useState(options);
   const selectedItem = useMemo(() => options.find((option) => option.value === value) || null, [options, value]);
+  const listRef = useRef(null);
+
+  const rowVirtualizer = useVirtual({
+    size: items.length,
+    parentRef: listRef,
+    estimateSize,
+    overscan: 2,
+  });
 
   const { getInputProps, getMenuProps, getItemProps, isOpen } = useCombobox({
     items,
     itemToString,
     selectedItem,
+    scrollIntoView: () => {},
     onInputValueChange: ({ inputValue }) => {
       setItems(options.filter(itemFilter(inputValue)));
     },
     onSelectedItemChange: ({ selectedItem }) => onChange(selectedItem),
+    onHighlightedIndexChange: ({ highlightedIndex, type }) => {
+      if (type !== useCombobox.stateChangeTypes.MenuMouseLeave) {
+        rowVirtualizer.scrollToIndex(highlightedIndex);
+      }
+    },
   });
   return (
     <div>
       <Input suffix={<Icon name={isOpen ? 'search' : 'angle-down'} />} {...restProps} {...getInputProps()} />
-      <ul {...getMenuProps()}>
-        {isOpen &&
-          items.map((item, index) => {
-            return (
-              <li key={item.value} {...getItemProps({ item, index })}>
-                {item.label}
-              </li>
-            );
-          })}
+      <ul {...getMenuProps({ ref: listRef })}>
+        {isOpen && (
+          <>
+            <li key="total-size" style={{ height: rowVirtualizer.totalSize }} />
+            {rowVirtualizer.virtualItems.map((virtualRow) => {
+              return (
+                <li
+                  key={items[virtualRow.index].value}
+                  {...getItemProps({ item: items[virtualRow.index], index: virtualRow.index })}
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: virtualRow.size,
+                    transform: `translateY(${virtualRow.start}px)`,
+                  }}
+                >
+                  {items[virtualRow.index].label}
+                </li>
+              );
+            })}
+          </>
+        )}
       </ul>
     </div>
   );
