8ad4a421951ce721059322d35519d33084fbf136
Travis Patterson <travis.patterson@grafana.com>
2024-06-17 15:46:37 -0600

    Add info tooltips to logs that have been sampled by adaptive logs

--- public/app/features/logs/components/getLogRowStyles.ts
+++ public/app/features/logs/components/getLogRowStyles.ts
@@ -120,6 +120,9 @@ export const getLogRowStyles = memoizeOne((theme: GrafanaTheme2) => {
     logIconError: css`
       color: ${theme.colors.warning.main};
     `,
+    logIconInfo: css`
+      color: ${theme.colors.info.main};
+    `,
     logsRowToggleDetails: css`
       label: logs-row-toggle-details__level;
       font-size: 9px;
