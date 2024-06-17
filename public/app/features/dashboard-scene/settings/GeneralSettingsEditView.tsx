d424b8f1de1d9a14bb7c33f9b7191341bd9557f1
Dominik Prokop <dominik.prokop@grafana.com>
2024-06-17 12:46:25 +0200

    Dashboard: Allow opting out from dashboard panels lazy loading

--- public/app/features/dashboard-scene/settings/GeneralSettingsEditView.tsx
+++ public/app/features/dashboard-scene/settings/GeneralSettingsEditView.tsx
@@ -12,6 +12,7 @@ import {
   Label,
   RadioButtonGroup,
   Stack,
+  Switch,
   TagsInput,
   TextArea,
 } from '@grafana/ui';
@@ -161,6 +162,10 @@ export class GeneralSettingsEditView
     this.getCursorSync()?.setState({ sync: value });
   };
 
+  public onPreloadChange = (preload: boolean) => {
+    this._dashboard.setState({ preload });
+  };
+
   public onDeleteDashboard = () => {};
 
   static Component = ({ model }: SceneComponentProps<GeneralSettingsEditView>) => {
@@ -270,6 +275,24 @@ export class GeneralSettingsEditView
               )}
             >
               <RadioButtonGroup onChange={model.onTooltipChange} options={GRAPH_TOOLTIP_OPTIONS} value={graphTooltip} />
+            </Field>
+
+            <Field
+              label="Preload panels"
+              description="When enabled all panels will start loading as soon as the dashboard has been loaded"
+
+              // TODO
+              // label={t('dashboard-settings.general.panel-options-graph-tooltip-label', 'Graph tooltip')}
+              // description={t(
+              //   'dashboard-settings.general.panel-options-graph-tooltip-description',
+              //   'Controls tooltip and hover highlight behavior across different panels. Reload the dashboard for changes to take effect'
+              // )}
+            >
+              <Switch
+                id="preload-panels-dashboards-toggle"
+                value={dashboard.state.preload}
+                onChange={(e) => model.onPreloadChange(e.currentTarget.checked)}
+              />
             </Field>
           </CollapsableSection>
 
