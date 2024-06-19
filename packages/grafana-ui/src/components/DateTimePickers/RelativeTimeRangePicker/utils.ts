dd743038287d847cd54a1dff9f4967b596a42528
Gilles De Mey <gilles.de.mey@gmail.com>
2024-06-19 12:57:28 +0200

    initial implementation

--- packages/grafana-ui/src/components/DateTimePickers/RelativeTimeRangePicker/utils.ts
+++ packages/grafana-ui/src/components/DateTimePickers/RelativeTimeRangePicker/utils.ts
@@ -1,6 +1,6 @@
 import { RelativeTimeRange, TimeOption } from '@grafana/data';
 
-const regex = /^now$|^now\-(\d{1,10})([wdhms])$/;
+const regex = /^now$|^now(\-|\+)(\d{1,10})([wdhms])$/;
 
 export const mapOptionToRelativeTimeRange = (option: TimeOption): RelativeTimeRange | undefined => {
   return {
@@ -48,18 +48,19 @@ export const isRelativeFormat = (format: string): boolean => {
 const relativeToSeconds = (relative: string): number => {
   const match = regex.exec(relative);
 
-  if (!match || match.length !== 3) {
+  if (!match || match.length !== 4) {
     return 0;
   }
 
-  const [, value, unit] = match;
+  const [, sign, value, unit] = match;
   const parsed = parseInt(value, 10);
 
   if (isNaN(parsed)) {
     return 0;
   }
 
-  return parsed * units[unit];
+  const seconds = parsed * units[unit];
+  return sign === '+' ? seconds * -1 : seconds;
 };
 
 const units: Record<string, number> = {
@@ -71,25 +72,48 @@ const units: Record<string, number> = {
 };
 
 const secondsToRelativeFormat = (seconds: number): string => {
-  if (seconds <= 0) {
+  if (seconds === 0) {
     return 'now';
   }
 
-  if (seconds >= units.w && seconds % units.w === 0) {
-    return `now-${seconds / units.w}w`;
+  if (seconds < 0) {
+    return `now+${formatPrometheusDuration(Math.abs(seconds))}`;
   }
 
-  if (seconds >= units.d && seconds % units.d === 0) {
-    return `now-${seconds / units.d}d`;
-  }
-
-  if (seconds >= units.h && seconds % units.h === 0) {
-    return `now-${seconds / units.h}h`;
-  }
-
-  if (seconds >= units.m && seconds % units.m === 0) {
-    return `now-${seconds / units.m}m`;
-  }
-
-  return `now-${seconds}s`;
+  return `now-${formatPrometheusDuration(Math.abs(seconds))}`;
 };
+
+/**
+ * Formats the given duration in milliseconds into a human-readable string representation.
+ *
+ * @param milliseconds - The duration in milliseconds.
+ * @returns The formatted duration string.
+ */
+export function formatPrometheusDuration(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+
+  // we'll make an exception here for 0, 0ms seems a bit weird
+  if (seconds === 0) {
+    return '0s';
+  }
+
+  const timeUnits: Array<[number, string]> = [
+    [weeks % 52, 'w'],
+    [(days % 365) - 7 * (weeks % 52), 'd'],
+    [hours % 24, 'h'],
+    [minutes % 60, 'm'],
+    [seconds % 60, 's'],
+  ];
+
+  return (
+    timeUnits
+      // remove all 0 values
+      .filter(([time]) => time > 0)
+      // join time and unit
+      .map(([time, unit]) => time + unit)
+      .join('')
+  );
+}
