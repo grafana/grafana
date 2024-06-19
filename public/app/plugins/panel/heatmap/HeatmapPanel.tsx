911f56dc12a2857d949b54f4ab3754cd1e64b43f
Leon Sorokin <leeoniya@gmail.com>
2024-06-18 21:51:20 -0500

    Heatmap: Use dashboard timeRange for auto-sizing x buckets (#89382)

--- public/app/plugins/panel/heatmap/HeatmapPanel.tsx
+++ public/app/plugins/panel/heatmap/HeatmapPanel.tsx
@@ -59,11 +59,19 @@ export const HeatmapPanel = ({
 
   const info = useMemo(() => {
     try {
-      return prepareHeatmapData(data.series, data.annotations, options, palette, theme, replaceVariables);
+      return prepareHeatmapData({
+        frames: data.series,
+        annotations: data.annotations,
+        options,
+        palette,
+        theme,
+        replaceVariables,
+        timeRange,
+      });
     } catch (ex) {
       return { warning: `${ex}` };
     }
-  }, [data.series, data.annotations, options, palette, theme, replaceVariables]);
+  }, [data.series, data.annotations, options, palette, theme, replaceVariables, timeRange]);
 
   const facets = useMemo(() => {
     let exemplarsXFacet: number[] | undefined = []; // "Time" field
