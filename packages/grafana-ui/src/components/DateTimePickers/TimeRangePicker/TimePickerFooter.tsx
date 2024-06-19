b2d1c5b32c9abcff04d7fe9219a7093d862c628e
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-06-19 15:19:17 +0200

    Aria-expanded and tabs

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimePickerFooter.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimePickerFooter.tsx
@@ -1,5 +1,6 @@
 import { css, cx } from '@emotion/css';
 import { isString } from 'lodash';
+import Icon from 'ol/style/Icon';
 import React, { useCallback, useState } from 'react';
 
 import { getTimeZoneInfo, GrafanaTheme2, TimeZone } from '@grafana/data';
@@ -9,8 +10,8 @@ import { useStyles2 } from '../../../themes';
 import { t, Trans } from '../../../utils/i18n';
 import { Button } from '../../Button';
 import { Field } from '../../Forms/Field';
-import { RadioButtonGroup } from '../../Forms/RadioButtonGroup/RadioButtonGroup';
 import { Select } from '../../Select/Select';
+import { Tab, TabContent, TabsBar } from '../../Tabs';
 import { TimeZonePicker } from '../TimeZonePicker';
 import { TimeZoneDescription } from '../TimeZonePicker/TimeZoneDescription';
 import { TimeZoneOffset } from '../TimeZonePicker/TimeZoneOffset';
@@ -78,62 +79,83 @@ export const TimePickerFooter = (props: Props) => {
           variant="secondary"
           onClick={onToggleChangeTimeSettings}
           size="sm"
+          aria-expanded={isEditing}
+          aria-controls="time-settings-section"
+          icon={isEditing ? 'angle-up' : 'angle-down'}
         >
           <Trans i18nKey="time-picker.footer.change-settings-button">Change time settings</Trans>
         </Button>
       </section>
       {isEditing ? (
-        <div className={style.editContainer}>
+        <div className={style.editContainer} id="time-settings-section">
           <div>
-            <RadioButtonGroup
-              value={editMode}
-              options={[
-                { label: t('time-picker.footer.time-zone-option', 'Time zone'), value: 'tz' },
-                { label: t('time-picker.footer.fiscal-year-option', 'Fiscal year'), value: 'fy' },
-              ]}
-              onChange={setEditMode}
-            ></RadioButtonGroup>
-          </div>
-          {editMode === 'tz' ? (
-            <section
-              data-testid={selectors.components.TimeZonePicker.containerV2}
-              className={cx(style.timeZoneContainer, style.timeSettingContainer)}
-            >
-              <TimeZonePicker
-                includeInternal={true}
-                onChange={(timeZone) => {
-                  onToggleChangeTimeSettings();
-
-                  if (isString(timeZone)) {
-                    onChangeTimeZone(timeZone);
-                  }
+            <TabsBar>
+              <Tab
+                key="tz"
+                label={t('time-picker.footer.time-zone-option', 'Time zone')}
+                active={editMode === 'tz'}
+                onChangeTab={() => {
+                  setEditMode('tz');
                 }}
-                onBlur={onToggleChangeTimeSettings}
-                menuShouldPortal={false}
+                aria-controls="timezone-settings-panel"
               />
-            </section>
-          ) : (
-            <section
-              data-testid={selectors.components.TimeZonePicker.containerV2}
-              className={cx(style.timeZoneContainer, style.timeSettingContainer)}
-            >
-              <Field
-                className={style.fiscalYearField}
-                label={t('time-picker.footer.fiscal-year-start', 'Fiscal year start month')}
+              <Tab
+                key="fy"
+                label={t('time-picker.footer.fiscal-year-option', 'Fiscal year')}
+                active={editMode === 'fy'}
+                onChangeTab={() => {
+                  setEditMode('fy');
+                }}
+                aria-controls="fiscalyear-settings-panel"
+              />
+            </TabsBar>
+          </div>
+          <TabContent>
+            {editMode === 'tz' ? (
+              <section
+                role="tabpanel"
+                data-testid={selectors.components.TimeZonePicker.containerV2}
+                id="timezone-settings-panel"
+                className={cx(style.timeZoneContainer, style.timeSettingContainer)}
               >
-                <Select
-                  value={fiscalYearStartMonth}
-                  menuShouldPortal={false}
-                  options={monthOptions}
-                  onChange={(value) => {
-                    if (onChangeFiscalYearStartMonth) {
-                      onChangeFiscalYearStartMonth(value.value ?? 0);
+                <TimeZonePicker
+                  includeInternal={true}
+                  onChange={(timeZone) => {
+                    onToggleChangeTimeSettings();
+
+                    if (isString(timeZone)) {
+                      onChangeTimeZone(timeZone);
                     }
                   }}
+                  onBlur={onToggleChangeTimeSettings}
+                  menuShouldPortal={false}
                 />
-              </Field>
-            </section>
-          )}
+              </section>
+            ) : (
+              <section
+                role="tabpanel"
+                data-testid={selectors.components.TimeZonePicker.containerV2}
+                id="fiscalyear-settings-panel"
+                className={cx(style.timeZoneContainer, style.timeSettingContainer)}
+              >
+                <Field
+                  className={style.fiscalYearField}
+                  label={t('time-picker.footer.fiscal-year-start', 'Fiscal year start month')}
+                >
+                  <Select
+                    value={fiscalYearStartMonth}
+                    menuShouldPortal={false}
+                    options={monthOptions}
+                    onChange={(value) => {
+                      if (onChangeFiscalYearStartMonth) {
+                        onChangeFiscalYearStartMonth(value.value ?? 0);
+                      }
+                    }}
+                  />
+                </Field>
+              </section>
+            )}
+          </TabContent>
         </div>
       ) : null}
     </div>
