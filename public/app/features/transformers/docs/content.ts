1ad5a065067d9ec8a8069cb97b0e55854d147132
Isabel Matwawana <isabel.matwawana@grafana.com>
2024-06-19 13:25:09 -0400

    Fixed spacing

--- public/app/features/transformers/docs/content.ts
+++ public/app/features/transformers/docs/content.ts
@@ -192,18 +192,18 @@ export const transformationDocsContent: TransformationDocsContentType = {
   This transformation has the following options:
 
   - **Field** - Select from available fields
-- **as** - Select the FieldType to convert to
-  - **Numeric** - attempts to make the values numbers
-  - **String** - will make the values strings
-  - **Time** - attempts to parse the values as time
-    - The input will be parsed according to the [Moment.js parsing format](https://momentjs.com/docs/#/parsing/)
-    - It will parse the numeric input as a Unix epoch timestamp in milliseconds.
-      You must multiply your input by 1000 if it's in seconds.
-    - Will show an option to specify a DateFormat as input by a string like yyyy-mm-dd or DD MM YYYY hh:mm:ss
-  - **Boolean** - will make the values booleans
-  - **Enum** - will make the values enums
-    - Will show a table to manage the enums
-  - **Other** - attempts to parse the values as JSON
+  - **as** - Select the FieldType to convert to
+    - **Numeric** - attempts to make the values numbers
+    - **String** - will make the values strings
+    - **Time** - attempts to parse the values as time
+      - The input will be parsed according to the [Moment.js parsing format](https://momentjs.com/docs/#/parsing/)
+      - It will parse the numeric input as a Unix epoch timestamp in milliseconds.
+        You must multiply your input by 1000 if it's in seconds.
+      - Will show an option to specify a DateFormat as input by a string like yyyy-mm-dd or DD MM YYYY hh:mm:ss
+    - **Boolean** - will make the values booleans
+    - **Enum** - will make the values enums
+      - Will show a table to manage the enums
+    - **Other** - attempts to parse the values as JSON
 
   For example, consider the following query that could be modified by selecting the time field as Time and specifying Date Format as YYYY.
 
