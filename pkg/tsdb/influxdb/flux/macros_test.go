9ee51529682ade02cd9c864db24a82d91fe11a23
ismail simsek <ismailsimsek09@gmail.com>
2024-06-19 08:22:16 +0200

    InfluxDB: Don't interpolate bucket keyword in flux language if it is part of a join query (#89298)
    don't interpolate bucket keyword if it is part of a join query

--- pkg/tsdb/influxdb/flux/macros_test.go
+++ pkg/tsdb/influxdb/flux/macros_test.go
@@ -31,6 +31,11 @@ func TestInterpolate(t *testing.T) {
 			before: `v.timeRangeStart, something.timeRangeStop, XYZ.bucket, uuUUu.defaultBucket, aBcDefG.organization, window.windowPeriod, a91{}.bucket, $__interval, $__interval_ms`,
 			after:  `2021-09-22T10:12:51.310985041Z, 2021-09-22T11:12:51.310985042Z, "grafana2", "grafana3", "grafana1", 1m1.258s, a91{}.bucket, 1m, 61258`,
 		},
+		{
+			name:   "don't interpolate bucket variable in join query",
+			before: `range(start: v.timeRangeStart, stop: v.timeRangeStop) join.left(left: left |> group(), right: right,on:((l,r) => l.bucket == r.id), as: ((l, r) => ({l with name: r.name})))`,
+			after:  `range(start: 2021-09-22T10:12:51.310985041Z, stop: 2021-09-22T11:12:51.310985042Z) join.left(left: left |> group(), right: right,on:((l,r) => l.bucket == r.id), as: ((l, r) => ({l with name: r.name})))`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
