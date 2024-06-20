c1b33aaf5226856ecd428eb822b1050a7034a9df
Cedric Ziel <cedric@cedric-ziel.com>
2024-06-20 18:15:01 +0200

    fix: remove spy

--- public/app/features/explore/TraceView/components/common/AttributeActionExtensionPointTest.tsx
+++ public/app/features/explore/TraceView/components/common/AttributeActionExtensionPointTest.tsx
@@ -7,16 +7,6 @@ describe('<AttributeActionExtensionPoint />', () => {
   const attribute = ({ key: 'key', value: 'value' });
   const attributes = [{ key: 'key', value: 'value' }];
 
-  let copySpy: jest.SpyInstance;
-
-  beforeAll(() => {
-    copySpy = jest.spyOn(navigator.clipboard, 'writeText');
-  });
-
-  beforeEach(() => {
-    copySpy.mockReset();
-  });
-
   it('renders as expected', () => {
     expect(() => render(<AttributeActionExtensionPoint attribute={attribute} attributes={attributes} />)).not.toThrow();
   });
