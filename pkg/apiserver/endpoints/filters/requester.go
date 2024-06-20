5b31bbcb8a1b1136215413a54a9ed4f09e6d8d36
Ryan McKinley <ryantxu@gmail.com>
2024-06-20 09:27:27 +0300

    requester is set

--- pkg/apiserver/endpoints/filters/requester.go
+++ pkg/apiserver/endpoints/filters/requester.go
@@ -2,6 +2,7 @@ package filters
 
 import (
 	"net/http"
+	"slices"
 
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/endpoints/request"
@@ -16,46 +17,43 @@ func WithRequester(handler http.Handler) http.Handler {
 		requester, err := identity.GetRequester(ctx)
 		if err != nil {
 			// Find the kubernetes user info
-			k8sUserInfo, ok := request.UserFrom(ctx)
+			info, ok := request.UserFrom(ctx)
 			if ok {
-				orgId := int64(1)
-				for _, group := range k8sUserInfo.GetGroups() {
-					switch group {
-					case user.Anonymous:
-						requester = &identity.StaticRequester{
-							Namespace: identity.NamespaceAnonymous,
-							OrgID:     orgId,
-							Name:      k8sUserInfo.GetName(),
-							Login:     k8sUserInfo.GetName(),
-							Permissions: map[int64]map[string][]string{
-								orgId: {},
-							},
-						}
-
-					case user.APIServerUser, user.SystemPrivilegedGroup:
-						requester = &identity.StaticRequester{
-							UserID:         1,
-							OrgID:          orgId,
-							Name:           k8sUserInfo.GetName(),
-							Login:          k8sUserInfo.GetName(),
-							OrgRole:        identity.RoleAdmin,
-							IsGrafanaAdmin: true,
-							Permissions: map[int64]map[string][]string{
-								orgId: {
-									"*": {"*"}, // all resources, all scopes
-
-									// Dashboards do not support wildcard action
-									// dashboards.ActionDashboardsRead:   {"*"},
-									// dashboards.ActionDashboardsCreate: {"*"},
-									// dashboards.ActionDashboardsWrite:  {"*"},
-									// dashboards.ActionDashboardsDelete: {"*"},
-									// dashboards.ActionFoldersCreate:    {"*"},
-									// dashboards.ActionFoldersRead:      {dashboards.ScopeFoldersAll}, // access to read all folders
-								},
-							},
-						}
+				if info.GetName() == user.Anonymous {
+					requester = &identity.StaticRequester{
+						Namespace:   identity.NamespaceAnonymous,
+						Name:        info.GetName(),
+						Login:       info.GetName(),
+						Permissions: map[int64]map[string][]string{},
 					}
 				}
+
+				if info.GetName() == user.APIServerUser ||
+					slices.Contains(info.GetGroups(), user.SystemPrivilegedGroup) {
+					orgId := int64(1)
+					requester = &identity.StaticRequester{
+						UserID:         1,
+						OrgID:          orgId,
+						Name:           info.GetName(),
+						Login:          info.GetName(),
+						OrgRole:        identity.RoleAdmin,
+						IsGrafanaAdmin: true,
+						Permissions: map[int64]map[string][]string{
+							orgId: {
+								"*": {"*"}, // all resources, all scopes
+
+								// Dashboards do not support wildcard action
+								// dashboards.ActionDashboardsRead:   {"*"},
+								// dashboards.ActionDashboardsCreate: {"*"},
+								// dashboards.ActionDashboardsWrite:  {"*"},
+								// dashboards.ActionDashboardsDelete: {"*"},
+								// dashboards.ActionFoldersCreate:    {"*"},
+								// dashboards.ActionFoldersRead:      {dashboards.ScopeFoldersAll}, // access to read all folders
+							},
+						},
+					}
+				}
+
 				if requester != nil {
 					req = req.WithContext(identity.WithRequester(ctx, requester))
 				}
