890641ff753513290bae883b7d5a87286d47ab31
Ashley Harrison <ashley.harrison@grafana.com>
2024-06-21 15:40:29 +0100

    migrate font-awesome styles to emotion globals

--- packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
+++ packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
@@ -9,6 +9,7 @@ import { getCardStyles } from './card';
 import { getCodeStyles } from './code';
 import { getElementStyles } from './elements';
 import { getExtraStyles } from './extra';
+import { getFontAwesomeStyles } from './fontAwesome';
 import { getFontStyles } from './fonts';
 import { getFormElementStyles } from './forms';
 import { getJsonFormatterStyles } from './jsonFormatter';
@@ -31,6 +32,7 @@ export function GlobalStyles() {
         getCodeStyles(theme),
         getElementStyles(theme),
         getExtraStyles(theme),
+        getFontAwesomeStyles(theme),
         getFontStyles(theme),
         getFormElementStyles(theme),
         getJsonFormatterStyles(theme),
