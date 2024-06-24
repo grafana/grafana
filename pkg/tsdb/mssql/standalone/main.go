2af2f4934d566706d4dd7e002d91b171db8a739e
Andrew Hackmann <ajhack93@gmail.com>
2024-06-24 00:18:47 -0500

    make health check work and azure config

--- pkg/tsdb/mssql/standalone/main.go
+++ pkg/tsdb/mssql/standalone/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+
+	//"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/tsdb/mssql"
 )
