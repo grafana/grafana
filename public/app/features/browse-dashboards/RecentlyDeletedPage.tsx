ea443b116ef2706e9608eeccc2d30881f3b3d6df
Laura Benz <laura.benz@grafana.com>
2024-06-24 17:17:52 +0200

    feat: add empty state

--- public/app/features/browse-dashboards/RecentlyDeletedPage.tsx
+++ public/app/features/browse-dashboards/RecentlyDeletedPage.tsx
@@ -1,7 +1,9 @@
 import React, { memo, useEffect } from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 
+import { EmptyState } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
+import { t } from 'app/core/internationalization';
 import { ActionRow } from 'app/features/search/page/components/ActionRow';
 import { getGrafanaSearcher } from 'app/features/search/service';
 
@@ -32,6 +34,19 @@ const RecentlyDeletedPage = memo(() => {
       })
     );
   }, [dispatch, stateManager]);
+
+  if (searchState.loading === false && searchState.result && searchState.result.totalRows === 0) {
+    return (
+      <Page navId="dashboards/recently-deleted">
+        <Page.Contents>
+          <EmptyState
+            variant="call-to-action"
+            message={t('recently-deleted.page.empty-state', "You haven't deleted any dashboards recently.")}
+          />
+        </Page.Contents>
+      </Page>
+    );
+  }
 
   return (
     <Page navId="dashboards/recently-deleted">
