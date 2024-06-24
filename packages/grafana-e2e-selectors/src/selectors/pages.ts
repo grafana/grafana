f5df3c8090a8788fd7652282eb3dd870de22da4c
Ashley Harrison <ashley.harrison@grafana.com>
2024-06-24 10:22:29 +0100

    fix e2e selectors

--- packages/grafana-e2e-selectors/src/selectors/pages.ts
+++ packages/grafana-e2e-selectors/src/selectors/pages.ts
@@ -103,7 +103,7 @@ export const Pages = {
          * @deprecated use components.TimeZonePicker.containerV2 from Grafana 8.3 instead
          */
         timezone: 'Time zone picker select container',
-        title: 'Tab General',
+        title: 'General',
       },
       Annotations: {
         List: {
@@ -245,7 +245,7 @@ export const Pages = {
   },
   ShareDashboardModal: {
     PublicDashboard: {
-      Tab: 'Tab Public dashboard',
+      Tab: 'Public dashboard',
       WillBePublicCheckbox: 'data-testid public dashboard will be public checkbox',
       LimitedDSCheckbox: 'data-testid public dashboard limited datasources checkbox',
       CostIncreaseCheckbox: 'data-testid public dashboard cost may increase checkbox',
@@ -271,11 +271,11 @@ export const Pages = {
       },
     },
     PublicDashboardScene: {
-      Tab: 'Tab Public Dashboard',
+      Tab: 'Public Dashboard',
     },
     SnapshotScene: {
       url: (key: string) => `/dashboard/snapshot/${key}`,
-      Tab: 'Tab Snapshot',
+      Tab: 'Snapshot',
       PublishSnapshot: 'data-testid publish snapshot button',
       CopyUrlButton: 'data-testid snapshot copy url button',
       CopyUrlInput: 'data-testid snapshot copy url input',
