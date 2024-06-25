389109fb3de9a86dcce2464ee1234bfc14be41cd
Clarity-89 <homes89@ukr.net>
2024-06-25 09:16:43 +0300

    Set week start from config

--- public/app/core/components/TimePicker/TimePickerWithHistory.tsx
+++ public/app/core/components/TimePicker/TimePickerWithHistory.tsx
@@ -2,6 +2,7 @@ import { uniqBy } from 'lodash';
 import React from 'react';
 
 import { AppEvents, TimeRange, isDateTime, rangeUtil } from '@grafana/data';
+import { config } from '@grafana/runtime';
 import { TimeRangePickerProps, TimeRangePicker } from '@grafana/ui';
 import { t } from '@grafana/ui/src/utils/i18n';
 import appEvents from 'app/core/app_events';
@@ -32,6 +33,7 @@ export const TimePickerWithHistory = (props: Props) => {
           <TimeRangePicker
             {...props}
             history={history}
+            weekStart={config.bootData.user.weekStart}
             onChange={(value) => {
               onAppendToHistory(value, values, onSaveToStore);
               props.onChange(value);
