69480d981f2155d5d5906580d29c025cc988ccc4
Matt Jacobson <matthew.jacobson@grafana.com>
2024-06-25 11:35:10 -0400

    Replace openapi_types.File with []byte

--- pkg/apis/alerting_notifications/v0alpha1/receiver_spec.go
+++ pkg/apis/alerting_notifications/v0alpha1/receiver_spec.go
@@ -1,16 +1,12 @@
 package v0alpha1
 
-import (
-	openapi_types "github.com/deepmap/oapi-codegen/pkg/types"
-)
-
 // Integration defines model for Integration.
 // +k8s:openapi-gen=true
 type Integration struct {
-	DisableResolveMessage *bool              `json:"disableResolveMessage,omitempty"`
-	Settings              openapi_types.File `json:"settings"`
-	Type                  string             `json:"type"`
-	Uid                   *string            `json:"uid,omitempty"`
+	DisableResolveMessage *bool   `json:"disableResolveMessage,omitempty"`
+	Settings              []byte  `json:"settings"`
+	Type                  string  `json:"type"`
+	Uid                   *string `json:"uid,omitempty"`
 }
 
 // Spec defines model for Spec.
