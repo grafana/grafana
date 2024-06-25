57caa217af9902a2f01024559504d88ee2c3b832
Matt Jacobson <matthew.jacobson@grafana.com>
2024-06-25 12:01:18 -0400

    Regenerate types using grafana-app-sdk
    add mapType=atomic

--- pkg/apis/alerting_notifications/v0alpha1/receiver_spec.go
+++ pkg/apis/alerting_notifications/v0alpha1/receiver_spec.go
@@ -4,7 +4,8 @@ package v0alpha1
 // +k8s:openapi-gen=true
 type Integration struct {
 	DisableResolveMessage *bool `json:"disableResolveMessage,omitempty"`
-
+	// +mapType=atomic
+	SecureFields map[string]bool `json:"SecureFields,omitempty"`
 	// +listType=atomic
 	Settings []byte  `json:"settings"`
 	Type     string  `json:"type"`
