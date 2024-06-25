c9fb7bcee134744dc9783e1c47498b01de7107b6
alexandra vargas <alexa1866@gmail.com>
2024-06-25 18:27:54 +0200

    Fix jsx react

--- public/app/features/dashboard-scene/serialization/custom-variables/SnapshotFilterRenderer.tsx
+++ public/app/features/dashboard-scene/serialization/custom-variables/SnapshotFilterRenderer.tsx
@@ -1,5 +1,4 @@
 import { css, cx } from '@emotion/css';
-import React from 'react';
 
 import { AdHocVariableFilter, DataSourceRef, GrafanaTheme2 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
