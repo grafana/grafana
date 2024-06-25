e61d6ebbdb90db944234df4545e59e93977c5b8a
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2024-06-25 14:03:24 +0200

    Show Insights page only on cloud (when required ds's are available)

--- public/app/features/alerting/unified/home/Insights.tsx
+++ public/app/features/alerting/unified/home/Insights.tsx
@@ -98,12 +98,22 @@ const namespace = config.bootData.settings.namespace;
 
 export const INSTANCE_ID = namespace.includes('stack-') ? namespace.replace('stack-', '') : undefined;
 
-export function getInsightsScenes() {
+export const getInsightsDataSources = () => {
   const dataSourceSrv = getDataSourceSrv();
 
   [ashDs, cloudUsageDs, grafanaCloudPromDs].forEach((ds) => {
     ds.settings = dataSourceSrv.getInstanceSettings(ds.uid);
   });
+  return [ashDs, cloudUsageDs, grafanaCloudPromDs];
+};
+
+export const insigthsIsAvailable = () => {
+  const [_, cloudUsageDs, __] = getInsightsDataSources();
+  return cloudUsageDs.settings;
+};
+
+export function getInsightsScenes() {
+  const [ashDs, cloudUsageDs, grafanaCloudPromDs] = getInsightsDataSources();
 
   const categories = [];
 
