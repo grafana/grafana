2354a57aaa76acb5c176eb6f26934603c1fdba51
Ashley Harrison <ashley.harrison@grafana.com>
2024-06-25 12:18:45 +0100

    Chore: Attempt to make `keybinds` e2e test more stable (#89663)
    * attempt to make test more stable
    
    * split everything instead

--- e2e/scenes/various-suite/keybinds.spec.ts
+++ e2e/scenes/various-suite/keybinds.spec.ts
@@ -52,16 +52,20 @@ describe.skip('Keyboard shortcuts', () => {
       zone: 'Browser',
     });
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
+    let expectedRange = `Time range selected: 2024-06-05 10:05:00 to 2024-06-05 10:06:00`;
+    e2e.components.TimePicker.openButton().should('have.attr', 'aria-label', expectedRange);
 
     cy.log('Trying one shift-left');
     cy.get('body').type('t{leftarrow}');
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
-    let expectedRange = `Time range selected: 2024-06-05 10:04:00 to 2024-06-05 10:05:00`; // 1 min back
+    expectedRange = `Time range selected: 2024-06-05 10:04:00 to 2024-06-05 10:05:00`; // 1 min back
     e2e.components.TimePicker.openButton().should('have.attr', 'aria-label', expectedRange);
 
     cy.log('Trying two shift-lefts');
     cy.get('body').type('t{leftarrow}');
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
+    expectedRange = `Time range selected: 2024-06-05 10:03:00 to 2024-06-05 10:04:00`; // 1 min back
+    e2e.components.TimePicker.openButton().should('have.attr', 'aria-label', expectedRange);
     cy.get('body').type('t{leftarrow}');
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
     expectedRange = `Time range selected: 2024-06-05 10:02:00 to 2024-06-05 10:03:00`; // 2 mins back
@@ -70,11 +74,15 @@ describe.skip('Keyboard shortcuts', () => {
     cy.log('Trying two shift-lefts and a shift-right');
     cy.get('body').type('t{leftarrow}');
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
+    expectedRange = `Time range selected: 2024-06-05 10:01:00 to 2024-06-05 10:02:00`; // 1 min back
+    e2e.components.TimePicker.openButton().should('have.attr', 'aria-label', expectedRange);
     cy.get('body').type('t{leftarrow}');
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
+    expectedRange = `Time range selected: 2024-06-05 10:00:00 to 2024-06-05 10:01:00`; // 2 mins back
+    e2e.components.TimePicker.openButton().should('have.attr', 'aria-label', expectedRange);
     cy.get('body').type('t{rightarrow}');
     e2e.components.RefreshPicker.runButtonV2().should('have.text', 'Run query');
-    expectedRange = `Time range selected: 2024-06-05 10:01:00 to 2024-06-05 10:02:00`; // 2 mins back, 1 min forward (1 min back total)
+    expectedRange = `Time range selected: 2024-06-05 10:01:00 to 2024-06-05 10:02:00`; // 1 min forward (1 min back total)
     e2e.components.TimePicker.openButton().should('have.attr', 'aria-label', expectedRange);
   });
 });
