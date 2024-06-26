28808d614f6af8f0841ca58d7efd2d2587b11091
Bogdan Matei <bogdan.matei@grafana.com>
2024-06-26 13:35:55 +0300

    Update

--- public/app/features/dashboard-scene/scene/Scopes/ScopesTreeSearch.tsx
+++ public/app/features/dashboard-scene/scene/Scopes/ScopesTreeSearch.tsx
@@ -1,9 +1,9 @@
 import { css } from '@emotion/css';
 import { debounce } from 'lodash';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { Icon, Input, useStyles2 } from '@grafana/ui';
+import { FilterInput, useStyles2 } from '@grafana/ui';
 import { t } from 'app/core/internationalization';
 
 export interface ScopesTreeSearchProps {
@@ -16,6 +16,12 @@ export interface ScopesTreeSearchProps {
 export function ScopesTreeSearch({ anyChildExpanded, nodePath, query, onNodeUpdate }: ScopesTreeSearchProps) {
   const styles = useStyles2(getStyles);
 
+  const [queryValue, setQueryValue] = useState(query);
+
+  useEffect(() => {
+    setQueryValue(query);
+  }, [query]);
+
   const onQueryUpdate = useMemo(() => debounce(onNodeUpdate, 500), [onNodeUpdate]);
 
   if (anyChildExpanded) {
@@ -23,13 +29,15 @@ export function ScopesTreeSearch({ anyChildExpanded, nodePath, query, onNodeUpda
   }
 
   return (
-    <Input
-      prefix={<Icon name="filter" />}
-      className={styles.input}
+    <FilterInput
       placeholder={t('scopes.tree.search', 'Search')}
-      defaultValue={query}
+      value={queryValue}
+      className={styles.input}
       data-testid="scopes-tree-search"
-      onInput={(evt) => onQueryUpdate(nodePath, true, evt.currentTarget.value)}
+      onChange={(value) => {
+        setQueryValue(value);
+        onQueryUpdate(nodePath, true, value);
+      }}
     />
   );
 }
