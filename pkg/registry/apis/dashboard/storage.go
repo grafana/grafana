1aa3f9350054a6b783da68a603a5fd47d2b079f4
Ryan McKinley <ryantxu@gmail.com>
2024-06-26 13:07:33 +0300

    remove dashboard summary abstraction

--- pkg/registry/apis/dashboard/storage.go
+++ pkg/registry/apis/dashboard/storage.go
@@ -54,15 +54,13 @@ func newStorage(scheme *runtime.Scheme) (*storage, error) {
 					}, nil
 				}
 			}
-			summary, ok := obj.(*v0alpha1.DashboardSummary)
-			if ok {
-				return []interface{}{
-					dash.Name,
-					summary.Spec.Title,
-					dash.CreationTimestamp.UTC().Format(time.RFC3339),
-				}, nil
-			}
 			return nil, fmt.Errorf("expected dashboard or summary")
 		})
 	return &storage{Store: store}, nil
+}
+
+// Compare asserts on the equality of objects returned from both stores	(object storage and legacy storage)
+func (s *storage) Compare(storageObj, legacyObj runtime.Object) bool {
+	//TODO: define the comparison logic between a dashboard returned by the storage and a dashboard returned by the legacy storage
+	return false
 }
