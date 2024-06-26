83488cf5f2648a4f976b8b200b4a781c41193ec5
ArtistBanda <chirag.codes@gmail.com>
2024-06-26 17:51:35 +0530

    Added Query hints for Prometheus query code editor

--- packages/grafana-prometheus/src/querybuilder/components/PromQueryCodeEditor.tsx
+++ packages/grafana-prometheus/src/querybuilder/components/PromQueryCodeEditor.tsx
@@ -9,6 +9,7 @@ import { PromQueryField } from '../../components/PromQueryField';
 import { PromQueryEditorProps } from '../../components/types';
 
 import { PromQueryBuilderExplained } from './PromQueryBuilderExplained';
+import { QueryEditorHints } from '../shared/QueryEditorHints';
 
 type PromQueryCodeEditorProps = PromQueryEditorProps & {
   showExplain: boolean;
@@ -33,8 +34,8 @@ export function PromQueryCodeEditor(props: PromQueryCodeEditorProps) {
         data={data}
         app={app}
       />
-
       {showExplain && <PromQueryBuilderExplained query={query.expr} />}
+      <QueryEditorHints query={query} datasource={datasource} data={data} onChange={onChange} onRunQuery={onRunQuery} />
     </div>
   );
 }
---
+++ packages/grafana-prometheus/src/querybuilder/shared/QueryEditorHints.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { PromQueryEditorProps } from '../../components/types';
+import { QueryHint } from '@grafana/data';
+import { Button, Tooltip } from '@grafana/ui';
+import { reportInteraction } from '@grafana/runtime';
+
+export function QueryEditorHints(props: PromQueryEditorProps) {
+  const [hints, setHints] = useState<QueryHint[]>([]);
+
+  useEffect(() => {
+    const query = { expr: props.query.expr, refId: props.query.refId };
+    const hints = props.datasource.getQueryHints(query, props.data?.series || []).filter((hint) => hint.fix?.action);
+    setHints(hints);
+  }, [props.datasource, props.data, props.query]);
+
+  return (
+    <>
+      {hints.length > 0 && (
+        <div>
+          {hints.map((hint) => {
+            return (
+              <Tooltip content={`${hint.label} ${hint.fix?.label}`} key={hint.type}>
+                <Button
+                  onClick={() => {
+                    reportInteraction('grafana_query_builder_hints_clicked', {
+                      hint: hint.type,
+                      datasourceType: props.datasource.type,
+                    });
+
+                    if (hint.fix?.action) {
+                      const newQuery = props.datasource.modifyQuery(props.query, hint.fix.action);
+                      return props.onChange(newQuery);
+                    }
+                  }}
+                  fill="outline"
+                  size="sm"
+                >
+                  hint: {hint.fix?.title || hint.fix?.action?.type.toLowerCase().replace('_', ' ')}
+                </Button>
+              </Tooltip>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+}
+
+QueryEditorHints.displayName = 'QueryEditorHints';
