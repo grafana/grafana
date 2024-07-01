7ba305d7127371216ea0eb04539a6f1df25cd097
Karl Persson <kalle.persson@grafana.com>
2024-07-01 15:54:19 +0200

    Zanzana: Add utilities for translating RBAC to openFGA tuple keys

---
+++ pkg/services/authz/zanzana/zanzana.go
@@ -0,0 +1,59 @@
+package zanzana
+
+import (
+	"fmt"
+	"strconv"
+
+	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+)
+
+const (
+	TypeUser string = "user"
+)
+
+func NewObject(typ, id string) string {
+	return fmt.Sprintf("%s:%s", typ, id)
+}
+
+func NewScopedObject(typ, id, scope string) string {
+	return NewObject(typ, fmt.Sprintf("%s-%s", scope, id))
+}
+
+// rbac action to relation translation
+var actionTranslations = map[string]string{}
+
+type kindTranslation struct {
+	typ       string
+	orgScoped bool
+}
+
+// all kinds that we can translate into a openFGA object
+var kindTranslations = map[string]kindTranslation{}
+
+func TranslateToTuple(user string, action, kind, identifier string, orgID int64) (*openfgav1.TupleKey, bool) {
+	relation, ok := actionTranslations[action]
+	if !ok {
+		return nil, false
+	}
+
+	t, ok := kindTranslations[kind]
+	if !ok {
+		return nil, false
+	}
+
+	tuple := &openfgav1.TupleKey{
+		Relation: relation,
+	}
+
+	tuple.User = user
+	tuple.Relation = relation
+
+	// UID in grafana are not guarantee to be unique across orgs so we need to scope them.
+	if t.orgScoped {
+		tuple.Object = NewScopedObject(t.typ, identifier, strconv.FormatInt(orgID, 10))
+	} else {
+		tuple.Object = NewObject(t.typ, identifier)
+	}
+
+	return tuple, true
+}
