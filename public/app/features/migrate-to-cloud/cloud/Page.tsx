f13570a951df60f7b3091d2cc286de990dd2f6c6
eledobleefe <laura.fernandez@grafana.com>
2024-07-02 10:36:02 +0200

    Change width

--- public/app/features/migrate-to-cloud/cloud/Page.tsx
+++ public/app/features/migrate-to-cloud/cloud/Page.tsx
@@ -5,7 +5,7 @@ import { EmptyState } from './EmptyState/EmptyState';
 export const Page = () => {
   return (
     <Box backgroundColor="secondary" display="flex" alignItems="center" direction="column">
-      <Box maxWidth={180} paddingY={6} paddingX={2}>
+      <Box maxWidth={90} paddingY={6} paddingX={2}>
         <EmptyState />
       </Box>
     </Box>
