100c1182b8baad12f55a12c445efe996db3271d4
Clarity-89 <homes89@ukr.net>
2024-07-03 15:11:30 +0300

    Update export

--- public/app/features/preferences/api/index.ts
+++ public/app/features/preferences/api/index.ts
@@ -1,6 +1,7 @@
-export * from './user/endpoints.gen';
-
 import { generatedAPI } from './user/endpoints.gen';
+
+export const { useGetUserPreferencesQuery, usePatchUserPreferencesMutation, useUpdateUserPreferencesMutation } =
+  generatedAPI;
 
 export const userPreferencesAPI = generatedAPI.enhanceEndpoints({
   addTagTypes: ['UserPreferences'],
