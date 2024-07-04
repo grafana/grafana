c26755080cf986809b711b9ea33638b0d55e6ef3
Gilles De Mey <gilles.de.mey@gmail.com>
2024-07-04 16:46:51 +0200

    Alerting: Skip fetching alerts for unsaved dashboards (#90061)
    skip fetching alerts for dashboards with null uid
    
    (cherry picked from commit dd4e254900d836b8ea6c4aa17a7e4f844227e877)

--- public/app/features/alerting/unified/PanelAlertTabContent.test.tsx
+++ public/app/features/alerting/unified/PanelAlertTabContent.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { TestProvider } from 'test/helpers/TestProvider';
-import { byTestId } from 'testing-library-selector';
+import { byTestId, byText } from 'testing-library-selector';
 
 import { DataSourceApi } from '@grafana/data';
 import { PromOptions, PrometheusDatasource } from '@grafana/prometheus';
@@ -26,6 +26,7 @@ import {
   mockRulerAlertingRule,
   mockRulerRuleGroup,
 } from './mocks';
+import { captureRequests } from './mocks/server/events';
 import { RuleFormValues } from './types/rule-form';
 import * as config from './utils/config';
 import { Annotation } from './utils/constants';
@@ -184,6 +185,7 @@ const panel = new PanelModel({
 const ui = {
   row: byTestId('row'),
   createButton: byTestId<HTMLAnchorElement>('create-alert-rule-button'),
+  notSavedYet: byText('Dashboard not saved'),
 };
 
 const server = setupMswServer();
@@ -280,6 +282,29 @@ describe('PanelAlertTabContent', () => {
       intervalMs: 300000,
       maxDataPoints: 100,
     });
+  });
+
+  it('should not make requests for unsaved dashboard', async () => {
+    const capture = captureRequests();
+
+    const unsavedDashboard = {
+      ...dashboard,
+      uid: null,
+    } as DashboardModel;
+
+    renderAlertTabContent(
+      unsavedDashboard,
+      new PanelModel({
+        ...panel,
+        datasource: undefined,
+        maxDataPoints: 100,
+        interval: '10s',
+      })
+    );
+
+    expect(await ui.notSavedYet.find()).toBeInTheDocument();
+    const requests = await capture;
+    expect(requests.length).toBe(0);
   });
 
   it('Will take into account datasource minInterval', async () => {
