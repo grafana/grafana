9b1b02a7a17b7ef883423218ea56d2d5c746781b
kay delaney <kay@grafana.com>
2024-07-04 12:00:19 +0100

    Frontend: Use safe stringifier in parseBody
    Closes #88064

--- packages/grafana-data/src/utils/object.ts
+++ packages/grafana-data/src/utils/object.ts
@@ -10,3 +10,28 @@
 export const isEmptyObject = (value: unknown): value is Record<string, never> => {
   return typeof value === 'object' && value !== null && Object.keys(value).length === 0;
 };
+
+export function safeStringifyValue(value: unknown) {
+  // Avoid circular references ignoring those references
+  const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (_: string, value: object | null) => {
+      if (typeof value === 'object' && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+
+      return value;
+    };
+  };
+
+  try {
+    return JSON.stringify(value, getCircularReplacer());
+  } catch (error) {
+    console.error(error);
+  }
+
+  return '';
+}
