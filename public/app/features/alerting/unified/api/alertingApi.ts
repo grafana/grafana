fe44292e3376a259326fece17891010e8bb823a6
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-07-05 13:51:48 +0100

    Make alerting API support passing in `body` as well as `data`
    As the codegen APIs use `body` instead of the expected `data`, we need to map the properties so payloads are sent for mutations

--- public/app/features/alerting/unified/api/alertingApi.ts
+++ public/app/features/alerting/unified/api/alertingApi.ts
@@ -5,9 +5,26 @@ import { BackendSrvRequest, getBackendSrv } from '@grafana/runtime';
 
 import { logMeasurement } from '../Analytics';
 
-export const backendSrvBaseQuery = (): BaseQueryFn<BackendSrvRequest> => async (requestOptions) => {
+type RequestOptions = Omit<BackendSrvRequest, 'data'> & {
+  /**
+   * Data to send with a request. Maps to the `data` property on a `BackendSrvRequest`
+   *
+   * This is done to allow us to more easily consume code-gen APIs that expect/send a `body` property
+   * to endpoints.
+   */
+  body?: BackendSrvRequest['data'];
+};
+
+export const backendSrvBaseQuery = (): BaseQueryFn<RequestOptions> => async (originalOptions) => {
   try {
     const requestStartTs = performance.now();
+
+    const { body, ...options } = originalOptions;
+
+    const requestOptions: BackendSrvRequest = {
+      ...options,
+      ...(body && { data: body }),
+    };
 
     const { data, ...meta } = await lastValueFrom(getBackendSrv().fetch(requestOptions));
 
