1d21dc350671978a4f9975e43216ff62e0d7764f
Ashley Harrison <ashley.harrison@grafana.com>
2024-07-05 14:33:43 +0100

    Chore: More SCSS cleanup (#90132)
    * convert TagBadge to a functional component
    
    * migrate label and label-tag styles to TagBadge
    
    * migrate filter-table to emotion globals
    
    * migrate query-part styles to emotion globals
    
    * move tag color to be an inline style

--- packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
+++ packages/grafana-ui/src/themes/GlobalStyles/GlobalStyles.tsx
@@ -8,12 +8,14 @@ import { getCardStyles } from './card';
 import { getCodeStyles } from './code';
 import { getElementStyles } from './elements';
 import { getExtraStyles } from './extra';
+import { getFilterTableStyles } from './filterTable';
 import { getFontStyles } from './fonts';
 import { getFormElementStyles } from './forms';
 import { getJsonFormatterStyles } from './jsonFormatter';
 import { getLegacySelectStyles } from './legacySelect';
 import { getMarkdownStyles } from './markdownStyles';
 import { getPageStyles } from './page';
+import { getQueryPartStyles } from './queryPart';
 import { getRcTimePickerStyles } from './rcTimePicker';
 import { getSkeletonStyles } from './skeletonStyles';
 import { getSlateStyles } from './slate';
@@ -27,19 +29,21 @@ export function GlobalStyles() {
     <Global
       styles={[
         getAccessibilityStyles(theme),
+        getAgularPanelStyles(theme),
         getCodeStyles(theme),
         getElementStyles(theme),
         getExtraStyles(theme),
+        getFilterTableStyles(theme),
         getFontStyles(theme),
         getFormElementStyles(theme),
         getJsonFormatterStyles(theme),
-        getPageStyles(theme),
         getCardStyles(theme),
-        getAgularPanelStyles(theme),
         getMarkdownStyles(theme),
+        getPageStyles(theme),
+        getQueryPartStyles(theme),
+        getRcTimePickerStyles(theme),
         getSkeletonStyles(theme),
         getSlateStyles(theme),
-        getRcTimePickerStyles(theme),
         getUplotStyles(theme),
         getLegacySelectStyles(theme),
       ]}
