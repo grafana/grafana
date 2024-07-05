d73c35d5b3f78228bbf065a244c9e06fb9d8ae50
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-07-05 14:06:50 +0100

    Use mock provisioning and time intervals handlers

--- public/app/features/alerting/unified/mocks/server/all-handlers.ts
+++ public/app/features/alerting/unified/mocks/server/all-handlers.ts
@@ -8,7 +8,9 @@ import datasourcesHandlers from 'app/features/alerting/unified/mocks/server/hand
 import evalHandlers from 'app/features/alerting/unified/mocks/server/handlers/eval';
 import folderHandlers from 'app/features/alerting/unified/mocks/server/handlers/folders';
 import pluginsHandlers from 'app/features/alerting/unified/mocks/server/handlers/plugins';
+import provisioningHandlers from 'app/features/alerting/unified/mocks/server/handlers/provisioning';
 import silenceHandlers from 'app/features/alerting/unified/mocks/server/handlers/silences';
+import timeIntervalK8sHandlers from 'app/features/alerting/unified/mocks/server/handlers/timeIntervals.k8s';
 
 /**
  * Array of all mock handlers that are required across Alerting tests
@@ -20,7 +22,11 @@ const allHandlers = [
   ...evalHandlers,
   ...folderHandlers,
   ...pluginsHandlers,
+  ...provisioningHandlers,
   ...silenceHandlers,
+
+  // Kubernetes-style handlers
+  ...timeIntervalK8sHandlers,
 ];
 
 export default allHandlers;
