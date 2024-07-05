b347b345c1d3ea0fb8298721ff208062e04737f2
Ashley Harrison <ashley.harrison@grafana.com>
2024-07-05 12:53:55 +0100

    migrate label and label-tag styles to TagBadge

--- public/app/core/components/TagFilter/TagBadge.tsx
+++ public/app/core/components/TagFilter/TagBadge.tsx
@@ -1,6 +1,8 @@
+import { css } from '@emotion/css';
 import * as React from 'react';
 
-import { getTagColorsFromName, Icon } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data';
+import { getTagColorsFromName, Icon, useStyles2 } from '@grafana/ui';
 
 export interface Props {
   label: string;
@@ -11,6 +13,7 @@ export interface Props {
 
 export const TagBadge = ({ count, label, onClick, removeIcon }: Props) => {
   const { color } = getTagColorsFromName(label);
+  const styles = useStyles2(getStyles);
 
   const tagStyle = {
     backgroundColor: color,
@@ -19,9 +22,27 @@ export const TagBadge = ({ count, label, onClick, removeIcon }: Props) => {
   const countLabel = count !== 0 && <span style={{ marginLeft: '3px' }}>{`(${count})`}</span>;
 
   return (
-    <span className={`label label-tag`} style={tagStyle}>
+    <span className={styles.badge} style={tagStyle}>
       {removeIcon && <Icon onClick={onClick} name="times" />}
       {label} {countLabel}
     </span>
   );
 };
+
+export const getStyles = (theme: GrafanaTheme2) => ({
+  badge: css({
+    ...theme.typography.bodySmall,
+    backgroundColor: theme.v1.palette.gray1,
+    borderRadius: theme.shape.radius.default,
+    color: theme.v1.palette.white,
+    display: 'inline-block',
+    height: '20px',
+    lineHeight: '20px',
+    padding: theme.spacing(0, 0.75),
+    verticalAlign: 'baseline',
+    whiteSpace: 'nowrap',
+    '&:hover': {
+      opacity: 0.85,
+    },
+  }),
+});
