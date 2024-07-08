d771256b0389ee358dbda8010745e295f409c057
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-07-08 15:45:30 -0400

    Alerting: Support field selectors in time interval API (#90022)
    * fix kind of TimeInterval
    * register custom fields for selectors
    * support field selectors in legacy storage
    * support selectors in storage
    
    ===== Misc
    * refactor conversions to build in one place
    * hide implementation of provenance status behind accessors to use the key in selectors
    * fix provenance error

---
+++ pkg/apis/alerting_notifications/v0alpha1/types_ext.go
@@ -0,0 +1,46 @@
+package v0alpha1
+
+const ProvenanceStatusAnnotationKey = "grafana.com/provenance"
+const ProvenanceStatusNone = "none"
+
+func (o *TimeInterval) GetProvenanceStatus() string {
+	if o == nil || o.Annotations == nil {
+		return ""
+	}
+	s, ok := o.Annotations[ProvenanceStatusAnnotationKey]
+	if !ok || s == "" {
+		return ProvenanceStatusNone
+	}
+	return s
+}
+
+func (o *TimeInterval) SetProvenanceStatus(status string) {
+	if o.Annotations == nil {
+		o.Annotations = make(map[string]string, 1)
+	}
+	if status == "" {
+		status = ProvenanceStatusNone
+	}
+	o.Annotations[ProvenanceStatusAnnotationKey] = status
+}
+
+func (o *Receiver) GetProvenanceStatus() string {
+	if o == nil || o.Annotations == nil {
+		return ""
+	}
+	s, ok := o.Annotations[ProvenanceStatusAnnotationKey]
+	if !ok || s == "" {
+		return ProvenanceStatusNone
+	}
+	return s
+}
+
+func (o *Receiver) SetProvenanceStatus(status string) {
+	if o.Annotations == nil {
+		o.Annotations = make(map[string]string, 1)
+	}
+	if status == "" {
+		status = ProvenanceStatusNone
+	}
+	o.Annotations[ProvenanceStatusAnnotationKey] = status
+}
--- pkg/services/ngalert/provisioning/errors.go
+++ pkg/services/ngalert/provisioning/errors.go
@@ -17,8 +17,8 @@ var (
 	ErrBadAlertmanagerConfiguration = errutil.Internal("alerting.notification.configCorrupted").MustTemplate("Failed to unmarshal the Alertmanager configuration", errutil.WithPublic("Current Alertmanager configuration in the storage is corrupted. Reset the configuration or rollback to a recent valid one."))
 
 	ErrProvenanceChangeNotAllowed = errutil.Forbidden("alerting.notifications.invalidProvenance").MustTemplate(
-		"Resource with provenance status '{{ .Public.CurrentProvenance }}' cannot be managed via API that handles resources with provenance status '{{ .Public.TargetProvenance }}'",
-		errutil.WithPublic("Resource with provenance status '{{ .Public.CurrentProvenance }}' cannot be managed via API that handles resources with provenance status '{{ .Public.TargetProvenance }}'. You must use appropriate API to manage this resource"),
+		"Resource with provenance status '{{ .Public.SourceProvenance }}' cannot be managed via API that handles resources with provenance status '{{ .Public.TargetProvenance }}'",
+		errutil.WithPublic("Resource with provenance status '{{ .Public.SourceProvenance }}' cannot be managed via API that handles resources with provenance status '{{ .Public.TargetProvenance }}'. You must use appropriate API to manage this resource"),
 	)
 
 	ErrVersionConflict = errutil.Conflict("alerting.notifications.conflict")
