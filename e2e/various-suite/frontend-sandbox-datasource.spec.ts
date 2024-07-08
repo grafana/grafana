63eff5cddca7500cc7650f38671e819c4d3c24b0
Esteban Beltran <esteban@academo.me>
2024-07-08 15:13:35 +0200

    Use selector

--- e2e/various-suite/frontend-sandbox-datasource.spec.ts
+++ e2e/various-suite/frontend-sandbox-datasource.spec.ts
@@ -1,5 +1,7 @@
 import { random } from 'lodash';
 
+import { selectors } from '@grafana/e2e-selectors';
+
 import { e2e } from '../utils';
 
 const DATASOURCE_ID = 'sandbox-test-datasource';
@@ -86,7 +88,9 @@ describe('Datasource sandbox', () => {
         e2e.components.DataSourcePicker.container().should('be.visible').click();
         cy.contains(DATASOURCE_TYPED_NAME).scrollIntoView().should('be.visible').click();
         // make sure the datasource was correctly selected and rendered
-        cy.get('span[data-testid="data-testid SandboxDatasourceInstance breadcrumb"]').should('exist');
+        cy.get(`span[data-testid="${selectors.components.Breadcrumbs.breadcrumb(DATASOURCE_TYPED_NAME)}"]`).should(
+          'be.visible'
+        );
 
         cy.wait(300); // wait to prevent false positives because cypress checks too fast
         cy.get(`div[data-plugin-sandbox="${DATASOURCE_ID}"]`).should('not.exist');
@@ -98,7 +102,9 @@ describe('Datasource sandbox', () => {
         cy.contains(DATASOURCE_TYPED_NAME).scrollIntoView().should('be.visible').click();
 
         // make sure the datasource was correctly selected and rendered
-        cy.get('span[data-testid="data-testid SandboxDatasourceInstance breadcrumb"]').should('exist');
+        cy.get(`span[data-testid="${selectors.components.Breadcrumbs.breadcrumb(DATASOURCE_TYPED_NAME)}"]`).should(
+          'be.visible'
+        );
 
         const valueToType = 'test' + random(100);
 
@@ -120,7 +126,9 @@ describe('Datasource sandbox', () => {
         e2e.components.DataSourcePicker.container().should('be.visible').click();
         cy.contains(DATASOURCE_TYPED_NAME).scrollIntoView().should('be.visible').click();
         // make sure the datasource was correctly selected and rendered
-        cy.get('span[data-testid="data-testid SandboxDatasourceInstance breadcrumb"]').should('exist');
+        cy.get(`span[data-testid="${selectors.components.Breadcrumbs.breadcrumb(DATASOURCE_TYPED_NAME)}"]`).should(
+          'be.visible'
+        );
 
         cy.get(`div[data-plugin-sandbox="${DATASOURCE_ID}"]`).should('exist');
       });
@@ -131,7 +139,9 @@ describe('Datasource sandbox', () => {
         cy.contains(DATASOURCE_TYPED_NAME).scrollIntoView().should('be.visible').click();
 
         // make sure the datasource was correctly selected and rendered
-        cy.get('span[data-testid="data-testid SandboxDatasourceInstance breadcrumb"]').should('exist');
+        cy.get(`span[data-testid="${selectors.components.Breadcrumbs.breadcrumb(DATASOURCE_TYPED_NAME)}"]`).should(
+          'be.visible'
+        );
 
         const valueToType = 'test' + random(100);
 
