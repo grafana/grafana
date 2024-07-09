afda98013f164a4dd990ba4b5d37819f3c31611a
André Pereira <adrapereira@gmail.com>
2024-07-09 17:26:32 +0100

    Require streaming to be specifically disabled

--- public/app/plugins/datasource/tempo/datasource.ts
+++ public/app/plugins/datasource/tempo/datasource.ts
@@ -280,7 +280,7 @@ export class TempoDatasource extends DataSourceWithBackend<TempoQuery, TempoJson
       config.featureToggles.traceQLStreaming &&
       this.isFeatureAvailable(FeatureName.streaming) &&
       config.liveEnabled &&
-      this.streamingEnabled?.search
+      this.streamingEnabled?.search !== false
     );
   }
 
