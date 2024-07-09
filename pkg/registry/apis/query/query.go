91121ecedbee2c0098c24613d8b7b19c55690c66
Jean-Philippe Quémémer <jeanphilippe.quemener@grafana.com>
2024-07-09 14:09:40 +0200

    add user to logs

--- pkg/registry/apis/query/query.go
+++ pkg/registry/apis/query/query.go
@@ -350,6 +350,7 @@ func (b *QueryAPIBuilder) handleExpressions(ctx context.Context, req parsedReque
 					}
 					vars[refId] = res
 				} else {
+
 					// This should error in the parsing phase
 					err := fmt.Errorf("missing variable %s for %s", refId, expression.RefID)
 					qdr.Responses[refId] = backend.DataResponse{
