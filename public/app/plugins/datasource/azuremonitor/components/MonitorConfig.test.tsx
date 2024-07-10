c8f3c69ab98efe81e132a1500e61410f1989ccb6
Andreas Christou <andreas.christou@grafana.com>
2024-07-10 18:42:21 +0700

    [v10.3.x] Azure: Fix default subscription setting (#90266)
    * Azure: Fix default subscription setting (#90185)
    
    * Ensure default subscription is set
    
    * Include some info for when the default subscription is required
    
    * Rename settings mock
    
    - Add mockDatasourceSettings function
    
    * Add MonitorConfig test
    
    * Revert "Rename settings mock"
    
    - Avoid issues when backporting
    
    * Remove description
    
    * Remove required field
    
    * Remove unused import
    
    (cherry picked from commit bba26f8e13a0288044ad771deba49dd908471eaa)
    
    # Conflicts:
    #	public/app/plugins/datasource/azuremonitor/components/MonitorConfig.test.tsx
    
    * Add react and fix import

---
+++ public/app/plugins/datasource/azuremonitor/components/MonitorConfig.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import { createMockDatasourceSettings } from '../__mocks__/datasourceSettings';
+
+import { MonitorConfig, Props } from './MonitorConfig';
+
+const mockDatasourceSettings = createMockDatasourceSettings();
+
+const defaultProps: Props = {
+  options: mockDatasourceSettings,
+  updateOptions: jest.fn(),
+  getSubscriptions: jest.fn().mockResolvedValue([]),
+};
+
+describe('MonitorConfig', () => {
+  it('should render component', () => {
+    render(<MonitorConfig {...defaultProps} />);
+
+    expect(screen.getByText('Azure Cloud')).toBeInTheDocument();
+  });
+
+  it('should render component and set the default auth type if unset', () => {
+    const mockDsSettingsWithoutAuth = createMockDatasourceSettings({
+      jsonData: { azureAuthType: undefined, clientId: undefined, tenantId: undefined },
+    });
+
+    render(<MonitorConfig {...defaultProps} options={mockDsSettingsWithoutAuth} />);
+
+    expect(defaultProps.updateOptions).toHaveBeenCalled();
+    expect(screen.getByText('Azure Cloud')).toBeInTheDocument();
+  });
+  expect(defaultProps.options.jsonData.azureAuthType).toBe('clientsecret');
+
+  it('should render component and set the default subscription if specified', async () => {
+    const mockDsSettingsWithAuth = createMockDatasourceSettings(undefined, { clientSecret: true });
+    const getSubscriptions = jest.fn().mockResolvedValue([{ label: 'Test Sub', value: 'ghi-789' }]);
+    render(<MonitorConfig {...defaultProps} options={mockDsSettingsWithAuth} getSubscriptions={getSubscriptions} />);
+
+    expect(screen.getByText('Azure Cloud')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText('Test Sub')).toBeInTheDocument());
+  });
+});
