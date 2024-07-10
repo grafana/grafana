291d2162934ed4c5c3e9ee455792e74ba8ae3798
nmarrs <nathanielmarrs@gmail.com>
2024-07-10 10:16:40 -0700

    implement agreed upon approach for when to show tooltips

--- public/app/features/canvas/runtime/scene.tsx
+++ public/app/features/canvas/runtime/scene.tsx
@@ -281,7 +281,8 @@ export class Scene {
   };
 
   render() {
-    const canShowElementTooltip = !this.isEditingEnabled;
+    const isTooltipValid = (this.tooltip?.element?.data?.links?.length ?? 0) > 0 || this.tooltip?.element?.data?.field;
+    const canShowElementTooltip = !this.isEditingEnabled && isTooltipValid;
 
     const sceneDiv = (
       <div key={this.revId} className={this.styles.wrap} style={this.style} ref={this.setRef}>
