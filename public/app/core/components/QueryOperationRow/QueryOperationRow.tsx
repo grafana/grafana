017786bc1d8fd2e4c04b489df56adf09cc98d1dc
Ashley Harrison <ashley.harrison@grafana.com>
2024-07-11 11:44:42 +0100

    migrate from react-beautiful-dnd to @hello-pangea/dnd

--- public/app/core/components/QueryOperationRow/QueryOperationRow.tsx
+++ public/app/core/components/QueryOperationRow/QueryOperationRow.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/css';
+import { Draggable } from '@hello-pangea/dnd';
 import { useCallback, useEffect, useState } from 'react';
 import * as React from 'react';
-import { Draggable } from 'react-beautiful-dnd';
 import { useUpdateEffect } from 'react-use';
 
 import { GrafanaTheme2 } from '@grafana/data';
@@ -64,8 +64,8 @@ export function QueryOperationRow({
   }, [isOpen]);
 
   const reportDragMousePosition = useCallback((e: React.MouseEvent) => {
-    // When drag detected react-beautiful-dnd will preventDefault the event
-    // Ref: https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/guides/how-we-use-dom-events.md#a-mouse-drag-has-started-and-the-user-is-now-dragging
+    // When drag detected @hello-pangea/dnd will preventDefault the event
+    // Ref: https://github.com/atlassian/@hello-pangea/dnd/blob/master/docs/guides/how-we-use-dom-events.md#a-mouse-drag-has-started-and-the-user-is-now-dragging
     if (e.defaultPrevented) {
       const rect = e.currentTarget.getBoundingClientRect();
       const x = e.clientX - rect.left;
