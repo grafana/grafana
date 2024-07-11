c21ba5f1d7db279e034ba0070c4514afce608a5c
Hugo Kiyodi Oshiro <hugo.oshiro@grafana.com>
2024-07-11 11:38:43 +0200

    Plugins: Improve levitate / breaking changes report in grafana/grafana (#89822)
    * Plugins: Add gcloud auth and usage to levitate workflow
    
    ---------
    
    Co-authored-by: Levente Balogh <balogh.levente.hu@gmail.com>

--- scripts/levitate-parse-json-report.js
+++ scripts/levitate-parse-json-report.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const printAffectedPluginsSection = require('./levitate-show-affected-plugins');
+
 const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 function stripAnsi(str) {
@@ -27,5 +29,7 @@ if (data.removals.length > 0) {
 if (data.changes.length > 0) {
   markdown += printSection('Changes', data.changes);
 }
+
+markdown += printAffectedPluginsSection(data);
 
 console.log(markdown);
