d7c844b5b6c47ee527d5f78f6e4b7756b03673a2
Pepe Cano <825430+ppcano@users.noreply.github.com>
2024-07-11 13:33:41 +0200

    Alerting UX: copy changes in Groups view (#90173)
    * UI and help copy changes in Alerting Groups view
    
    * Alerting Groups view: change subtitle
    
    * Rename Alerting page title: `Groups` to `Active notifications`
    
    * Update pseudo-local
    
    * update betterer
    
    * Extract translation through `make i18n-extract`

--- public/app/core/utils/navBarItem-translations.ts
+++ public/app/core/utils/navBarItem-translations.ts
@@ -75,7 +75,7 @@ export function getNavTitle(navId: string | undefined) {
     case 'silences':
       return t('nav.alerting-silences.title', 'Silences');
     case 'groups':
-      return t('nav.alerting-groups.title', 'Groups');
+      return t('nav.alerting-groups.title', 'Active notifications');
     case 'alerting-admin':
       return t('nav.alerting-admin.title', 'Settings');
     case 'cfg':
@@ -234,7 +234,7 @@ export function getNavSubTitle(navId: string | undefined) {
     case 'silences':
       return t('nav.alerting-silences.subtitle', 'Stop notifications from one or more alerting rules');
     case 'groups':
-      return t('nav.alerting-groups.subtitle', 'See grouped alerts from an Alertmanager instance');
+      return t('nav.alerting-groups.subtitle', 'See grouped alerts with active notifications');
     case 'datasources':
       return t('nav.datasources.subtitle', 'Add and configure data sources');
     case 'correlations':
