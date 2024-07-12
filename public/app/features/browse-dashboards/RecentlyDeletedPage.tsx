0d043bb7740ff02b0d75a3997bdd81b63393d7bb
joshhunt <josh@trtr.co>
2024-07-12 12:33:34 +0100

    RecentlyDeleted: Only show actions when items selected

--- public/app/features/browse-dashboards/RecentlyDeletedPage.tsx
+++ public/app/features/browse-dashboards/RecentlyDeletedPage.tsx
@@ -16,13 +16,14 @@ import { RecentlyDeletedActions } from './components/RecentlyDeletedActions';
 import { RecentlyDeletedEmptyState } from './components/RecentlyDeletedEmptyState';
 import { SearchView } from './components/SearchView';
 import { getFolderPermissions } from './permissions';
-import { setAllSelection } from './state';
+import { setAllSelection, useHasSelection } from './state';
 
 const RecentlyDeletedPage = memo(() => {
   const dispatch = useDispatch();
   const styles = useStyles2(getStyles);
 
   const [searchState, stateManager] = useRecentlyDeletedStateManager();
+  const hasSelection = useHasSelection();
 
   const { canEditFolders, canEditDashboards } = getFolderPermissions();
   const canSelect = canEditFolders || canEditDashboards;
@@ -48,20 +49,23 @@ const RecentlyDeletedPage = memo(() => {
           escapeRegex={false}
           onChange={stateManager.onQueryChange}
         />
-        <ActionRow
-          state={searchState}
-          getTagOptions={stateManager.getTagOptions}
-          getSortOptions={getGrafanaSearcher().getSortOptions}
-          sortPlaceholder={getGrafanaSearcher().sortPlaceholder}
-          onLayoutChange={stateManager.onLayoutChange}
-          onSortChange={stateManager.onSortChange}
-          onTagFilterChange={stateManager.onTagFilterChange}
-          onDatasourceChange={stateManager.onDatasourceChange}
-          onPanelTypeChange={stateManager.onPanelTypeChange}
-          onSetIncludePanels={stateManager.onSetIncludePanels}
-        />
 
-        <RecentlyDeletedActions />
+        {hasSelection ? (
+          <RecentlyDeletedActions />
+        ) : (
+          <ActionRow
+            state={searchState}
+            getTagOptions={stateManager.getTagOptions}
+            getSortOptions={getGrafanaSearcher().getSortOptions}
+            sortPlaceholder={getGrafanaSearcher().sortPlaceholder}
+            onLayoutChange={stateManager.onLayoutChange}
+            onSortChange={stateManager.onSortChange}
+            onTagFilterChange={stateManager.onTagFilterChange}
+            onDatasourceChange={stateManager.onDatasourceChange}
+            onPanelTypeChange={stateManager.onPanelTypeChange}
+            onSetIncludePanels={stateManager.onSetIncludePanels}
+          />
+        )}
 
         <div className={styles.subView}>
           <AutoSizer>
@@ -85,7 +89,7 @@ const RecentlyDeletedPage = memo(() => {
 const getStyles = (theme: GrafanaTheme2) => ({
   pageContents: css({
     display: 'grid',
-    gridTemplateRows: 'auto auto auto 1fr',
+    gridTemplateRows: 'auto auto 1fr',
     height: '100%',
     rowGap: theme.spacing(1),
   }),
