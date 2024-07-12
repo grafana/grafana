bb93bfbac81c337d01899be99d3c3f5dcb62bde1
Adela Almasan <adela.almasan@grafana.com>
2024-07-12 10:32:30 -0500

    update @hello-pangea/dnd in test

--- packages/grafana-ui/src/components/DataLinks/DataLinksInlineEditor/DataLinksListItem.test.tsx
+++ packages/grafana-ui/src/components/DataLinks/DataLinksInlineEditor/DataLinksListItem.test.tsx
@@ -1,5 +1,5 @@
+import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 import { render, screen } from '@testing-library/react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 import { DataLinksListItem, DataLinksListItemProps } from './DataLinksListItem';
 
