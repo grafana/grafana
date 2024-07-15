0547a9ed8853d6e01cb68141e475d2e01c6eb030
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-07-15 09:09:24 +0100

    Alerting: Remove unneeded uses of setBackendSrv in alerting tests (#90380)

--- public/app/features/alerting/unified/components/contact-points/__mocks__/vanillaAlertmanagerServer.ts
+++ public/app/features/alerting/unified/components/contact-points/__mocks__/vanillaAlertmanagerServer.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from 'msw';
-import { SetupServer } from 'msw/lib/node';
+import { SetupServer } from 'msw/node';
 
 import { AlertmanagerStatus } from 'app/plugins/datasource/alertmanager/types';
 
