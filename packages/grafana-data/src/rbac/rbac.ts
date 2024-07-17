009a5e510e4ce599eedb4d45dbd1a37f736c3212
jguer <me@jguer.space>
2024-07-17 18:04:43 +0200

    always use backend permissions

--- packages/grafana-data/src/rbac/rbac.ts
+++ packages/grafana-data/src/rbac/rbac.ts
@@ -1,10 +1,21 @@
+import { getBackendSrv } from '../../../grafana-runtime/src/services/backendSrv'; // hacky import for specific ask. Clean if kept.
 import { WithAccessControlMetadata } from '../types/accesscontrol';
 import { CurrentUserDTO } from '../types/config';
 
-export interface CurrentUser extends Omit<CurrentUserDTO, 'lightTheme'> {}
+
+export interface CurrentUser extends Omit<CurrentUserDTO, 'lightTheme'> { }
 
 export function userHasPermission(action: string, user: CurrentUser): boolean {
-  return !!user.permissions?.[action];
+  let permissions: Record<string, boolean> | undefined;
+  try {
+    getBackendSrv().get('/api/access-control/user/actions', {
+      reloadcache: false,
+    }).then((res: Record<string, boolean>) => { permissions = res; });
+  } catch (e) {
+    console.error(e);
+    permissions = user.permissions;
+  }
+  return !!permissions?.[action];
 }
 
 export function userHasPermissionInMetadata(action: string, object: WithAccessControlMetadata): boolean {
