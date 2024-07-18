f5c1596bb4373a93d2ebd499689d857a20098a8c
Piotr Jamróz <pm.jamroz@gmail.com>
2024-07-18 13:47:40 +0200

    Memoize visible range passed to LogsVolumePanel

--- public/app/features/explore/Logs/LogsVolumePanelList.tsx
+++ public/app/features/explore/Logs/LogsVolumePanelList.tsx
@@ -13,7 +13,6 @@ import {
   GrafanaTheme2,
   LoadingState,
   SplitOpen,
-  TimeRange,
   TimeZone,
 } from '@grafana/data';
 import { Button, InlineField, Alert, useStyles2, SeriesVisibilityChangeMode } from '@grafana/ui';
@@ -88,9 +87,11 @@ export const LogsVolumePanelList = ({
 
   const timeoutError = isTimeoutErrorResponse(logsVolumeData);
 
-  const from = dateTime(Math.max(absoluteRange.from, allLogsVolumeMaximumRange.from));
-  const to = dateTime(Math.min(absoluteRange.to, allLogsVolumeMaximumRange.to));
-  const visibleRange: TimeRange = { from, to, raw: { from, to } };
+  const visibleRange = useMemo(() => {
+    const from = dateTime(Math.max(absoluteRange.from, allLogsVolumeMaximumRange.from));
+    const to = dateTime(Math.min(absoluteRange.to, allLogsVolumeMaximumRange.to));
+    return { from, to, raw: { from, to } };
+  }, [absoluteRange.from, allLogsVolumeMaximumRange.from, absoluteRange.to, allLogsVolumeMaximumRange.to]);
 
   if (logsVolumeData?.state === LoadingState.Loading) {
     return <span>Loading...</span>;
