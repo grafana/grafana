6feb84727f93053fde0e66d8d92b71b6b89d90ce
Ihor Yeromin <yeryomin.igor@gmail.com>
2024-07-18 15:11:46 +0200

    feat(legend): add missed props

--- packages/grafana-ui/src/components/VizLegend/VizLegend2.tsx
+++ packages/grafana-ui/src/components/VizLegend/VizLegend2.tsx
@@ -18,18 +18,22 @@ export const VizLegend2 = ({ data, placement, calcs, displayMode, ...vizLayoutLe
     const field = frame.fields[1];
     const fieldIndex = field.state?.origin;
 
+    const disabled = (field.config.custom?.hideFrom?.viz || field.state?.hideFrom?.viz) ?? false;
     const label = getFieldDisplayName(field, frame, data);
     const color = getFieldSeriesColor(field, theme).color;
     const yAxis = field.config.custom?.axisPlacement === AxisPlacement.Right ? 2 : 1;
+    const lineStyle = field.config.custom?.lineStyle;
 
     return {
-      fieldIndex, // IHOR: { frameIndex, fieldIndex } - didn't find a usage of fieldIndex; it's only used in getItemKey
+      disabled,
+      fieldIndex, // IHOR: { frameIndex, fieldIndex } - it's only used in getItemKey
       label,
       color,
       yAxis,
       getDisplayValues: () => getDisplayValuesForCalcs(calcs, field, theme),
       // IHOR: getItemKey used in List component to assign a `key` to the item
       getItemKey: () => `${label}-${fieldIndex?.frameIndex}-${fieldIndex?.fieldIndex}`,
+      lineStyle,
     };
   });
 
