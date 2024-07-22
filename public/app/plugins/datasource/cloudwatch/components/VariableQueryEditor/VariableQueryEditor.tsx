b10ae6f369f5dee57a215162abc412831b97b7cf
Ida Štambuk <ida.stambuk@grafana.com>
2024-07-22 10:30:28 +0200

    Update public/app/plugins/datasource/cloudwatch/components/VariableQueryEditor/VariableQueryEditor.tsx
    Co-authored-by: Kevin Yu <kevinwcyu@users.noreply.github.com>

--- public/app/plugins/datasource/cloudwatch/components/VariableQueryEditor/VariableQueryEditor.tsx
+++ public/app/plugins/datasource/cloudwatch/components/VariableQueryEditor/VariableQueryEditor.tsx
@@ -228,7 +228,7 @@ export const VariableQueryEditor = ({ query, datasource, onChange }: Props) => {
                 </a>
               </>
             }
-          />{' '}
+          />
           <EditorField
             label="Filters"
             tooltipInteractive
