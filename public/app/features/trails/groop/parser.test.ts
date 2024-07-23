796c08888326e64981cb0c4624d00b2b9f292d2f
ismail simsek <ismailsimsek09@gmail.com>
2024-07-23 17:25:32 +0200

    update unit tests

--- public/app/features/trails/groop/parser.test.ts
+++ public/app/features/trails/groop/parser.test.ts
@@ -68,7 +68,7 @@ describe('Parser', () => {
     parser.config.maxDepth = 1;
     parser.config.miscGroupKey = 'misc';
     const grouping = parser.parse(metrics);
-    expect(grouping.root.descendants).toBe(1604);
+    expect(grouping.root.descendants).toBe(263);
     expect(grouping.root.values.length).toBe(0);
 
     const miscGroup = lookupNode(grouping.root, 'misc');
@@ -86,24 +86,5 @@ describe('prefixDelimited', () => {
     expect(prefixDelimited('agent_metrics_ha_configs_created', 3)).toBe('agent_metrics_ha_configs');
     expect(prefixDelimited('agent_metrics_ha_configs_created', 4)).toBe('agent_metrics_ha_configs_created');
     expect(prefixDelimited('agent_metrics_ha_configs_created', 5)).toBe('agent_metrics_ha_configs_created');
-  });
-});
-
-describe('lots of strings', () => {
-  const metrics = readFileSync(join(__dirname, 'testdata', 'metrics20k.txt'), 'utf8').split('\n');
-  it('should parse 20k strings', () => {
-    const now = performance.now();
-    const parser = new Parser();
-    parser.config.idealMaxGroupSize = 50;
-    parser.config.maxDepth = 1;
-    const grouping = parser.parse(metrics);
-    const elapsed = performance.now() - now;
-    expect(grouping.root.descendants).toBe(19546);
-    console.log(`${grouping.root.descendants} strings parsed in ${elapsed}ms`);
-    // print all top level groups
-    const topLevelGroups = Array.from(grouping.root.groups.keys());
-    expect(topLevelGroups.join(', ')).toBe(
-      `DCGM, access, adaptive, addsstable, adhoc, admin, admission, agent, aggregator, alertmanager, alloy, analytics, apiserver, apisix, app, argo, asserts, atlantis, auth, authentication, authorization, aws, backstage, base, billing, blackbox, block, bootstrapper, buffer, build, bytes, cache, capacity, celery, certmanager, changefeed, chatops, checked, citadel, cle, closers, cloud, cloudcost, cloudsql, cluster, code, commitlog, config, conntrack, container, context, controller, coordinator, coredns, cortex, counter, cronjob, crossplane, csi, database, datadog, db, dbindex, dbshard, descheduler, disk, distsender, dns, drone, ecs, encoder, endpoint, envoy, erlang, etcd, exec, executor, exporter, externalsecret, faro, fetch, field, flagger, flower, freetier, fs, galley, gatekeeper, gateway, gcom, gcommail, gcp, github, go, gorm, gossip, gotk, grafana, grafanacloud, grafanacom, graphite, grpc, hekate, hgapi, hgcontroller, hggateway, hikaricp, hosted, http, identifier, incident, index, influxdb, informer, instance, integrations, intentresolver, intents, istio, iterator, jaeger, java, jdbc, jmx, job, jobs, jvm, k6, k6cloud, k6stats, kafka, karpenter, keda, kepler, kine, kminion, konnectivity, kopia, kowalski, kube, kubecost, kubedns, kubelet, kubeproxy, kubernetes, kv, leases, legacy, liveness, llm, llmgateway, log, logql, logs, loki, m3dbclient, m3ninx, machine, memberlist, memcached, metadata, method, metrics, mimir, miner, ml, mlapi, mlops, mlscheduler, mlworker, modelapi, mt, mtgr, mtgrdns, multi, mysql, namespace, net, netfilter, network, nginx, node, nodejs, nodepool, objstore, oncall, orchestrator, otelcol, otlp, outbox, pdc, peers, pg, pgbouncer, pilot, pod, postgres, postings, postingslist, preempter, private, probe, process, processing, processor, profiles, prometheus, promhttp, promitor, promrun, promtail, proxy, pushgateway, pyroscope, pyroscopedb, python, querier, query, queue, rabbitmq, raft, range, ranges, rebalancing, redis, registry, replicas, replicaset, replication, request, requests, response, rest, retrieve, ring, rocket, rocksdb, rollout, root, round, rpc, rt, runtime, scc, scheduler, schedules, scrape, secret, security, segment, series, server, service, serviceaccount, sidecar, sift, sigma, skydns, slo, sm, smtprelay, spanconfig, spring, sql, sqlliveness, stack, stackdriver, storage, sys, system, tag, tags, tanka, telemetry, tempo, tempodb, tenant, test, thanos, timer, timeseries, tomcat, traces, traefik, ttl, txn, txnrecovery, txnwaitqueue, type, uninitialized, unused, update, usage, vault, vectorapi, velero, vendor, version, vllm, volume, vpa, warpstream, watch, watcher, webhook, wired, workload, workqueue, write, yace`
-    );
   });
 });
