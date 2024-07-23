02029c2bd05b4665c81329a474c78c1b8093e69c
Leon Sorokin <leeoniya@gmail.com>
2024-07-22 22:50:35 -0500

    LCP

--- public/app/app.ts
+++ public/app/app.ts
@@ -106,6 +106,14 @@ import { createSystemVariableAdapter } from './features/variables/system/adapter
 import { createTextBoxVariableAdapter } from './features/variables/textbox/adapter';
 import { configureStore } from './store/configureStore';
 
+const observer = new PerformanceObserver((list) => {
+  const entries = list.getEntries();
+  const lastEntry = entries[entries.length - 1]; // Use the latest LCP candidate
+  console.log("LCP:", lastEntry.startTime);
+  console.log(lastEntry);
+});
+observer.observe({ type: "largest-contentful-paint", buffered: true });
+
 // add move to lodash for backward compatabilty with plugins
 // @ts-ignore
 _.move = arrayMove;
