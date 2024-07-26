b8b3a2cdd7ea2a563d20b097f5c4a3ba935235dd
lingyufei <1187112693@qq.com>
2024-07-26 16:59:09 +0800

    fix: fix searching will return all folders when folder uid is general

--- pkg/services/sqlstore/searchstore/filters.go
+++ pkg/services/sqlstore/searchstore/filters.go
@@ -103,10 +103,15 @@ func (f FolderUIDFilter) Where() (string, []any) {
 	if includeGeneral {
 		if q == "" {
 			q = "dashboard.folder_id = ? "
+			if f.NestedFoldersEnabled {
+				q = "dashboard.folder_uid IS NULL "
+			}
 		} else {
 			q = "(" + q + " OR dashboard.folder_id = ?)"
+			if f.NestedFoldersEnabled {
+				q = "(" + q + " OR dashboard.folder_uid IS NULL)"
+			}
 		}
-		params = append(params, 0)
 	}
 
 	return q, params
