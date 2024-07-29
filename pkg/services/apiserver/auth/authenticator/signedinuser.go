b98ba44009c13fcc0b80f0d0bfec25b4044bb33e
Ryan McKinley <ryantxu@gmail.com>
2024-07-29 08:57:29 +0300

    extend user.Info

--- pkg/services/apiserver/auth/authenticator/signedinuser.go
+++ pkg/services/apiserver/auth/authenticator/signedinuser.go
@@ -21,28 +21,7 @@ func signedInUserAuthenticator(req *http.Request) (*authenticator.Response, bool
 		return nil, false, nil
 	}
 
-	userInfo := &k8suser.DefaultInfo{
-		Name:   signedInUser.GetLogin(),
-		UID:    signedInUser.GetRawIdentifier(),
-		Groups: []string{},
-		// In order to faithfully round-trip through an impersonation flow, Extra keys MUST be lowercase.
-		// see: https://pkg.go.dev/k8s.io/apiserver@v0.27.1/pkg/authentication/user#Info
-		Extra: map[string][]string{},
-	}
-
-	for _, v := range signedInUser.GetTeams() {
-		userInfo.Groups = append(userInfo.Groups, strconv.FormatInt(v, 10))
-	}
-
-	//
-	if signedInUser.GetIDToken() != "" {
-		userInfo.Extra["id-token"] = []string{signedInUser.GetIDToken()}
-	}
-	if signedInUser.GetOrgRole().IsValid() {
-		userInfo.Extra["user-instance-role"] = []string{string(signedInUser.GetOrgRole())}
-	}
-
 	return &authenticator.Response{
-		User: userInfo,
+		User: signedInUser,
 	}, true, nil
 }
