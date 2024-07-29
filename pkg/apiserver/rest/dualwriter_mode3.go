8345af928cca93e25a050bc145f3ab2cffba04b1
Leonor Oliveira <9090754+leonorfmartins@users.noreply.github.com>
2024-07-29 12:57:44 +0100

    [Dual writer] Read kind value passed as dualwriter's argument (#91113)
    * Include kind on the dualwriter to be used on metrics
    
    * Use kind as argument
    
    * Skip dual writer in mode4

--- pkg/apiserver/rest/dualwriter_mode3.go
+++ pkg/apiserver/rest/dualwriter_mode3.go
@@ -16,13 +16,14 @@ type DualWriterMode3 struct {
 	Legacy  LegacyStorage
 	Storage Storage
 	*dualWriterMetrics
-	Log klog.Logger
+	kind string
+	Log  klog.Logger
 }
 
 // newDualWriterMode3 returns a new DualWriter in mode 3.
 // Mode 3 represents writing to LegacyStorage and Storage and reading from Storage.
-func newDualWriterMode3(legacy LegacyStorage, storage Storage, dwm *dualWriterMetrics) *DualWriterMode3 {
-	return &DualWriterMode3{Legacy: legacy, Storage: storage, Log: klog.NewKlogr().WithName("DualWriterMode3").WithValues("mode", mode3Str), dualWriterMetrics: dwm}
+func newDualWriterMode3(legacy LegacyStorage, storage Storage, dwm *dualWriterMetrics, kind string) *DualWriterMode3 {
+	return &DualWriterMode3{Legacy: legacy, Storage: storage, Log: klog.NewKlogr().WithName("DualWriterMode3").WithValues("mode", mode3Str, "kind", kind), dualWriterMetrics: dwm}
 }
 
 // Mode returns the mode of the dual writer.
@@ -35,18 +36,17 @@ const mode3Str = "3"
 // Create overrides the behavior of the generic DualWriter and writes to LegacyStorage and Storage.
 func (d *DualWriterMode3) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
 	var method = "create"
-	var kind = obj.GetObjectKind().GroupVersionKind().Kind
-	log := d.Log.WithValues("kind", kind, "method", method)
+	log := d.Log.WithValues("method", method)
 	ctx = klog.NewContext(ctx, log)
 
 	startStorage := time.Now()
 	created, err := d.Storage.Create(ctx, obj, createValidation, options)
 	if err != nil {
 		log.Error(err, "unable to create object in storage")
-		d.recordLegacyDuration(true, mode3Str, kind, method, startStorage)
+		d.recordLegacyDuration(true, mode3Str, d.kind, method, startStorage)
 		return created, err
 	}
-	d.recordStorageDuration(false, mode3Str, kind, method, startStorage)
+	d.recordStorageDuration(false, mode3Str, d.kind, method, startStorage)
 
 	go func() {
 		ctx, cancel := context.WithTimeoutCause(ctx, time.Second*10, errors.New("legacy create timeout"))
@@ -54,7 +54,7 @@ func (d *DualWriterMode3) Create(ctx context.Context, obj runtime.Object, create
 
 		startLegacy := time.Now()
 		_, errObjectSt := d.Legacy.Create(ctx, obj, createValidation, options)
-		d.recordLegacyDuration(errObjectSt != nil, mode3Str, kind, method, startLegacy)
+		d.recordLegacyDuration(errObjectSt != nil, mode3Str, d.kind, method, startLegacy)
 	}()
 
 	return created, err
@@ -71,12 +71,7 @@ func (d *DualWriterMode3) Get(ctx context.Context, name string, options *metav1.
 	if err != nil {
 		log.Error(err, "unable to get object in storage")
 	}
-
-	var kind string
-	if res != nil {
-		kind = res.GetObjectKind().GroupVersionKind().Kind
-	}
-	d.recordStorageDuration(err != nil, mode3Str, kind, method, startStorage)
+	d.recordStorageDuration(err != nil, mode3Str, d.kind, method, startStorage)
 
 	return res, err
 }
@@ -92,11 +87,7 @@ func (d *DualWriterMode3) List(ctx context.Context, options *metainternalversion
 	if err != nil {
 		log.Error(err, "unable to list object in storage")
 	}
-	var kind string
-	if res != nil {
-		kind = res.GetObjectKind().GroupVersionKind().Kind
-	}
-	d.recordStorageDuration(err != nil, mode3Str, kind, method, startStorage)
+	d.recordStorageDuration(err != nil, mode3Str, d.kind, method, startStorage)
 
 	return res, err
 }
@@ -106,15 +97,11 @@ func (d *DualWriterMode3) Delete(ctx context.Context, name string, deleteValidat
 	log := d.Log.WithValues("name", name, "method", method)
 	ctx = klog.NewContext(ctx, d.Log)
 
-	var kind string
 	startStorage := time.Now()
 	res, async, err := d.Storage.Delete(ctx, name, deleteValidation, options)
-	if res != nil {
-		kind = res.GetObjectKind().GroupVersionKind().Kind
-	}
 	if err != nil {
 		log.Error(err, "unable to delete object in storage")
-		d.recordStorageDuration(true, mode3Str, kind, method, startStorage)
+		d.recordStorageDuration(true, mode3Str, d.kind, method, startStorage)
 		return res, async, err
 	}
 	d.recordStorageDuration(false, mode3Str, name, method, startStorage)
@@ -124,7 +111,7 @@ func (d *DualWriterMode3) Delete(ctx context.Context, name string, deleteValidat
 		ctx, cancel := context.WithTimeoutCause(ctx, time.Second*10, errors.New("legacy delete timeout"))
 		defer cancel()
 		_, _, err := d.Legacy.Delete(ctx, name, deleteValidation, options)
-		d.recordLegacyDuration(err != nil, mode3Str, kind, method, startLegacy)
+		d.recordLegacyDuration(err != nil, mode3Str, d.kind, method, startLegacy)
 	}()
 
 	return res, async, err
@@ -135,19 +122,15 @@ func (d *DualWriterMode3) Update(ctx context.Context, name string, objInfo rest.
 	var method = "update"
 	log := d.Log.WithValues("name", name, "method", method)
 	ctx = klog.NewContext(ctx, log)
-	var kind string
 
 	startStorage := time.Now()
 	res, async, err := d.Storage.Update(ctx, name, objInfo, createValidation, updateValidation, forceAllowCreate, options)
-	if res != nil {
-		kind = res.GetObjectKind().GroupVersionKind().Kind
-	}
 	if err != nil {
 		log.Error(err, "unable to update in storage")
-		d.recordLegacyDuration(true, mode3Str, kind, method, startStorage)
+		d.recordLegacyDuration(true, mode3Str, d.kind, method, startStorage)
 		return res, async, err
 	}
-	d.recordStorageDuration(false, mode3Str, kind, method, startStorage)
+	d.recordStorageDuration(false, mode3Str, d.kind, method, startStorage)
 
 	go func() {
 		ctx, cancel := context.WithTimeoutCause(ctx, time.Second*10, errors.New("legacy update timeout"))
@@ -155,7 +138,7 @@ func (d *DualWriterMode3) Update(ctx context.Context, name string, objInfo rest.
 		startLegacy := time.Now()
 		defer cancel()
 		_, _, errObjectSt := d.Legacy.Update(ctx, name, objInfo, createValidation, updateValidation, forceAllowCreate, options)
-		d.recordLegacyDuration(errObjectSt != nil, mode3Str, kind, method, startLegacy)
+		d.recordLegacyDuration(errObjectSt != nil, mode3Str, d.kind, method, startLegacy)
 	}()
 
 	return res, async, err
@@ -166,26 +149,22 @@ func (d *DualWriterMode3) DeleteCollection(ctx context.Context, deleteValidation
 	var method = "delete-collection"
 	log := d.Log.WithValues("resourceVersion", listOptions.ResourceVersion, "method", method)
 	ctx = klog.NewContext(ctx, log)
-	var kind string
 
 	startStorage := time.Now()
 	res, err := d.Storage.DeleteCollection(ctx, deleteValidation, options, listOptions)
-	if res != nil {
-		kind = res.GetObjectKind().GroupVersionKind().Kind
-	}
 	if err != nil {
 		log.Error(err, "unable to delete collection in storage")
-		d.recordStorageDuration(true, mode3Str, kind, method, startStorage)
+		d.recordStorageDuration(true, mode3Str, d.kind, method, startStorage)
 		return res, err
 	}
-	d.recordStorageDuration(false, mode3Str, kind, method, startStorage)
+	d.recordStorageDuration(false, mode3Str, d.kind, method, startStorage)
 
 	go func() {
 		startLegacy := time.Now()
 		ctx, cancel := context.WithTimeoutCause(ctx, time.Second*10, errors.New("legacy deletecollection timeout"))
 		defer cancel()
 		_, err := d.Legacy.DeleteCollection(ctx, deleteValidation, options, listOptions)
-		d.recordStorageDuration(err != nil, mode3Str, kind, method, startLegacy)
+		d.recordStorageDuration(err != nil, mode3Str, d.kind, method, startLegacy)
 	}()
 
 	return res, err
@@ -194,7 +173,7 @@ func (d *DualWriterMode3) DeleteCollection(ctx context.Context, deleteValidation
 //TODO: uncomment when storage watch is implemented
 // func (d *DualWriterMode3) Watch(ctx context.Context, options *metainternalversion.ListOptions) (watch.Interface, error) {
 // 	var method = "watch"
-// 	d.Log.WithValues("kind", options.Kind, "method", method, "mode", mode3Str).Info("starting to watch")
+// 	d.Log.WithValues("kind", d.kind, "method", method, "mode", mode3Str).Info("starting to watch")
 // 	return d.Storage.Watch(ctx, options)
 // }
 
