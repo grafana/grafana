6d2651033cf114e8fb0b2871cbe174333362cf3f
kay delaney <kay@grafana.com>
2024-07-29 16:46:10 +0100

    Add tests

--- public/app/features/dashboard-scene/serialization/transformSceneToSaveModel.test.ts
+++ public/app/features/dashboard-scene/serialization/transformSceneToSaveModel.test.ts
@@ -15,7 +15,14 @@ import {
 } from '@grafana/data';
 import { getPanelPlugin } from '@grafana/data/test/__mocks__/pluginMocks';
 import { getPluginLinkExtensions, setPluginImportUtils } from '@grafana/runtime';
-import { MultiValueVariable, SceneGridLayout, SceneGridRow, SceneTimeRange, VizPanel } from '@grafana/scenes';
+import {
+  MultiValueVariable,
+  sceneGraph,
+  SceneGridLayout,
+  SceneGridRow,
+  SceneTimeRange,
+  VizPanel,
+} from '@grafana/scenes';
 import { Dashboard, LoadingState, Panel, RowPanel, VariableRefresh } from '@grafana/schema';
 import { PanelModel } from 'app/features/dashboard/state';
 import { getTimeRange } from 'app/features/dashboard/utils/timeRange';
@@ -34,6 +41,7 @@ import { getVizPanelKeyForPanelId } from '../utils/utils';
 
 import { GRAFANA_DATASOURCE_REF } from './const';
 import dashboard_to_load1 from './testfiles/dashboard_to_load1.json';
+import panelRepeatsDashboard from './testfiles/panel_repeats.json';
 import repeatingRowsAndPanelsDashboardJson from './testfiles/repeating_rows_and_panels.json';
 import snapshotableDashboardJson from './testfiles/snapshotable_dashboard.json';
 import snapshotableWithRowsDashboardJson from './testfiles/snapshotable_with_rows.json';
@@ -1092,6 +1100,36 @@ describe('transformSceneToSaveModel', () => {
 
       const saveModel = transformSceneToSaveModel(scene);
       expect((saveModel.panels![1] as any).options.content).toBe('new content');
+    });
+  });
+
+  describe('Given a scene with repeated panels and non-repeated panels', () => {
+    it('should save repeated panels itemHeight as height', () => {
+      const scene = transformSaveModelToScene({ dashboard: panelRepeatsDashboard as any, meta: {} });
+      const gridItem = sceneGraph.findByKey(scene, 'grid-item-1') as DashboardGridItem;
+      expect(gridItem).toBeInstanceOf(DashboardGridItem);
+      expect(gridItem.state.height).toBe(12);
+      expect(gridItem.state.itemHeight).toBe(12);
+      expect(gridItem.state.variableName).toBe('repeater');
+      gridItem.setState({ itemHeight: 24 });
+      const saveModel = transformSceneToSaveModel(scene);
+      expect(saveModel.panels?.[0].gridPos?.h).toBe(24);
+    });
+
+    it('should not save non-repeated panels itemHeight as height', () => {
+      const scene = transformSaveModelToScene({ dashboard: panelRepeatsDashboard as any, meta: {} });
+      const gridItem = sceneGraph.findByKey(scene, 'grid-item-2') as DashboardGridItem;
+      expect(gridItem).toBeInstanceOf(DashboardGridItem);
+      expect(gridItem.state.height).toBe(9);
+      expect(gridItem.state.itemHeight).toBe(9);
+      expect(gridItem.state.variableName).toBeUndefined();
+      gridItem.setState({ itemHeight: 24 });
+      let saveModel = transformSceneToSaveModel(scene);
+      expect(saveModel.panels?.[1].gridPos?.h).toBe(9);
+
+      gridItem.setState({ height: 34 });
+      saveModel = transformSceneToSaveModel(scene);
+      expect(saveModel.panels?.[1].gridPos?.h).toBe(34);
     });
   });
 });
