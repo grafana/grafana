1d39c1ddf5a1701fa6677862506e288f8dc34055
David Buckley <isreal-github.com@bucko.me.uk>
2024-07-30 12:39:08 +0100

    Explore: new option boilerplate for SegmentAsync

--- packages/grafana-ui/src/components/Segment/types.ts
+++ packages/grafana-ui/src/components/Segment/types.ts
@@ -4,6 +4,8 @@ export interface SegmentProps {
   Component?: ReactElement;
   className?: string;
   allowCustomValue?: boolean;
+  allowCreateWhileLoading?: boolean;
+  createOptionPosition?: 'first' | 'last';
   placeholder?: string;
   disabled?: boolean;
   onExpandedChange?: (expanded: boolean) => void;
