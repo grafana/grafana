5b970f8ca4fbf691ed82ba9450a9c5c687e822b7
Michael Mandrus <michael.mandrus@grafana.com>
2024-07-29 21:45:53 -0400

    move status updates before goroutine to ensure frontend polls

--- pkg/services/cloudmigration/cloudmigrationimpl/cloudmigration.go
+++ pkg/services/cloudmigration/cloudmigrationimpl/cloudmigration.go
@@ -512,6 +512,9 @@ func (s *Service) CreateSnapshot(ctx context.Context, signedInUser *user.SignedI
 	}
 	snapshot.UID = uid
 
+	// ensure the client polls for status
+	s.report(ctx, session, gmsclient.EventStartBuildingSnapshot, 0, nil)
+
 	// start building the snapshot asynchronously while we return a success response to the client
 	go func() {
 		s.cancelMutex.Lock()
@@ -522,8 +525,6 @@ func (s *Service) CreateSnapshot(ctx context.Context, signedInUser *user.SignedI
 
 		ctx, cancelFunc := context.WithCancel(context.Background())
 		s.cancelFunc = cancelFunc
-
-		s.report(ctx, session, gmsclient.EventStartBuildingSnapshot, 0, nil)
 
 		start := time.Now()
 		err := s.buildSnapshot(ctx, signedInUser, initResp.MaxItemsPerPartition, initResp.Metadata, snapshot)
@@ -644,6 +645,9 @@ func (s *Service) UploadSnapshot(ctx context.Context, sessionUid string, snapsho
 
 	s.log.Info("Uploading snapshot in local directory", "gmsSnapshotUID", snapshot.GMSSnapshotUID, "localDir", snapshot.LocalDir, "uploadURL", uploadUrl)
 
+	// ensure the client polls for status
+	s.report(ctx, session, gmsclient.EventStartUploadingSnapshot, 0, nil)
+
 	// start uploading the snapshot asynchronously while we return a success response to the client
 	go func() {
 		s.cancelMutex.Lock()
@@ -654,8 +658,6 @@ func (s *Service) UploadSnapshot(ctx context.Context, sessionUid string, snapsho
 
 		ctx, cancelFunc := context.WithCancel(context.Background())
 		s.cancelFunc = cancelFunc
-
-		s.report(ctx, session, gmsclient.EventStartUploadingSnapshot, 0, nil)
 
 		start := time.Now()
 		err := s.uploadSnapshot(ctx, session, snapshot, uploadUrl)
