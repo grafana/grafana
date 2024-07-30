08909cf4a36f99559e1eb6521b51c4b8b6690212
Esteban Beltran <esteban@academo.me>
2024-07-30 13:44:18 +0200

    use Space component

--- public/app/features/plugins/admin/components/RoadmapLinks.tsx
+++ public/app/features/plugins/admin/components/RoadmapLinks.tsx
@@ -1,21 +1,11 @@
-import { css } from '@emotion/css';
-
-import { GrafanaTheme2 } from '@grafana/data';
 import { reportInteraction } from '@grafana/runtime';
-import { TextLink, useStyles2 } from '@grafana/ui';
+import { Space, TextLink } from '@grafana/ui';
 import { Trans } from 'app/core/internationalization';
 
-const getStyles = (theme: GrafanaTheme2) => ({
-  spacer: css({
-    height: theme.spacing(2),
-  }),
-});
-
 export const RoadmapLinks = () => {
-  const styles = useStyles2(getStyles);
   return (
     <div>
-      <div className={styles.spacer} />
+      <Space v={2} />
       <TextLink
         href="https://github.com/grafana/grafana/issues/new?assignees=&labels=area%2Fdatasource%2Ctype%2Fnew-plugin-request&projects=&template=3-data_source_request.yaml&title=%5BNew+Data+Source%5D%3A+%3Cname-of-service%3E"
         onClick={() => reportInteraction('connections_data_source_request_clicked')}
