b1953786dc0dba6796228257330e59d064c8ac9e
Leandro Deveikis <leandro.deveikis@gmail.com>
2024-08-01 17:01:01 -0300

    fix imports

--- pkg/api/http_server_test.go
+++ pkg/api/http_server_test.go
@@ -1,13 +1,12 @@
 package api
 
 import (
-	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/grafana/grafana/pkg/setting"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHTTPServer_MetricsBasicAuth(t *testing.T) {
