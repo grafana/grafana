cee12799bd5c1432bca3ce01ed537ee189adc3ef
William Wernert <william.wernert@grafana.com>
2024-08-01 16:48:04 -0400

    Add comments

--- pkg/services/ngalert/schedule/metrics.go
+++ pkg/services/ngalert/schedule/metrics.go
@@ -39,11 +39,15 @@ type ruleKey struct {
 }
 
 func (sch *schedule) updateRulesMetrics(alertRules []*models.AlertRule) {
+	// main rule_group_rules metric labels
 	buckets := make(map[ruleKey]int64)
+	// gauge for rules with notification settings per org
 	orgsNfSettings := make(map[int64]int64)
+	// gauge for groups per org
 	groupsPerOrg := make(map[int64]map[string]struct{})
 
 	for _, rule := range alertRules {
+		// Count rules by org, type and state
 		state := metrics.AlertRuleActiveLabelValue
 		if rule.IsPaused {
 			state = metrics.AlertRulePausedLabelValue
@@ -55,9 +59,12 @@ func (sch *schedule) updateRulesMetrics(alertRules []*models.AlertRule) {
 		}
 		buckets[key]++
 
+		// Count rules with notification settings per org
 		if len(rule.NotificationSettings) > 0 {
 			orgsNfSettings[rule.OrgID]++
 		}
+
+		// Count groups per org
 		orgGroups, ok := groupsPerOrg[rule.OrgID]
 		if !ok {
 			orgGroups = make(map[string]struct{})
@@ -66,18 +73,16 @@ func (sch *schedule) updateRulesMetrics(alertRules []*models.AlertRule) {
 		orgGroups[rule.RuleGroup] = struct{}{}
 	}
 
+	// Set metrics
 	for key, count := range buckets {
 		sch.metrics.GroupRules.WithLabelValues(fmt.Sprint(key.orgID), key.ruleType.String(), key.state).Set(float64(count))
 	}
-
 	for orgID, numRulesNfSettings := range orgsNfSettings {
 		sch.metrics.SimpleNotificationRules.WithLabelValues(fmt.Sprint(orgID)).Set(float64(numRulesNfSettings))
 	}
-
 	for orgID, groups := range groupsPerOrg {
 		sch.metrics.Groups.WithLabelValues(fmt.Sprint(orgID)).Set(float64(len(groups)))
 	}
-
 	// While these are the rules that we iterate over, at the moment there's no 100% guarantee that they'll be
 	// scheduled as rules could be removed before we get a chance to evaluate them.
 	sch.metrics.SchedulableAlertRules.Set(float64(len(alertRules)))
