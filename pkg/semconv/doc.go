f99ce9f58dff7cf829ddf2991e5ec3d90ca8b662
Todd Treece <todd.treece@grafana.com>
2024-08-01 11:44:09 -0400

    O11y: Add semconv sub module

---
+++ pkg/semconv/doc.go
@@ -0,0 +1,5 @@
+// Package semconv contains OpenTelemetry semantic conventions for Grafana.
+//
+// The conventions are used to define the structure of the telemetry data that is
+// produced by Grafana.
+package semconv // import "github.com/grafana/grafana/pkg/semconv"
