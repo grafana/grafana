3b89e869d77aad52da13d35744070479949d5582
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-08-02 23:47:02 -0500

    update otel api to check for standardization and return labels

--- public/app/features/trails/otel/api.ts
+++ public/app/features/trails/otel/api.ts
@@ -2,10 +2,10 @@ import { RawTimeRange } from '@grafana/data';
 import { getPrometheusTime } from '@grafana/prometheus/src/language_utils';
 import { getBackendSrv } from '@grafana/runtime';
 
-import { OtelTargetType, OtelResponse } from './types';
+import { OtelResponse, LabelResponse } from './types';
 
 /** This ensures we can join on a single series target*/
-const OTEL_TARGET_INFO_QUERY = 'count(target_info{}) by (job, instance)'; // == 1';
+const OTEL_TARGET_INFO_QUERY = 'count(target_info{}) by (job, instance)';
 
 /**
  * Query the DS for target_info matching job and instance.
@@ -14,21 +14,63 @@ const OTEL_TARGET_INFO_QUERY = 'count(target_info{}) by (job, instance)'; // == 
  * @param timeRange
  * @returns OtelResourcesType[], labels for the query result requesting matching job and instance on target_info metric
  */
-export async function getOtelTargets(
+export async function getOtelResources(
   dataSourceUid: string,
   timeRange: RawTimeRange,
   expr?: string
-): Promise<OtelTargetType[]> {
-  const url = `/api/datasources/uid/${dataSourceUid}/resources/api/v1/query`;
+): Promise<string[]> {
+  const start = getPrometheusTime(timeRange.from, false);
+  const end = getPrometheusTime(timeRange.to, true);
+
+  const url = `/api/datasources/uid/${dataSourceUid}/resources/api/v1/labels`;
   const params: Record<string, string | number> = {
-    start: getPrometheusTime(timeRange.from, false),
-    end: getPrometheusTime(timeRange.to, true),
-    query: expr ?? OTEL_TARGET_INFO_QUERY,
+    start,
+    end,
+    'match[]': '{__name__=~".*target_info.*"}',
   };
 
-  const response = await getBackendSrv().get<OtelResponse>(url, params, 'explore-metrics-otel-targets');
+  const response = await getBackendSrv().get<LabelResponse>(url, params, 'explore-metrics-otel-resources');
 
-  const resources = response.data?.result.map((el) => el.metric);
+  // don't include __name__ or deployment_environment
+  const resources = response.data
+    ?.filter((resource) => resource !== '__name__' && resource !== 'deployment_environment')
+    .map((el: string) => el);
 
   return resources;
+}
+
+export async function isOtelStandardization(
+  dataSourceUid: string,
+  timeRange: RawTimeRange,
+  expr?: string
+): Promise<boolean> {
+  const start = getPrometheusTime(timeRange.from, false);
+  const end = getPrometheusTime(timeRange.to, true);
+
+  const url = `/api/datasources/uid/${dataSourceUid}/resources/api/v1/query`;
+  const paramsTotalTargets: Record<string, string | number> = {
+    start,
+    end,
+    query: OTEL_TARGET_INFO_QUERY,
+  };
+
+  const responseTotal = await getBackendSrv().get<OtelResponse>(
+    url,
+    paramsTotalTargets,
+    'explore-metrics-otel-check-total'
+  );
+
+  const paramsStandardTargets: Record<string, string | number> = {
+    start,
+    end,
+    query: `${OTEL_TARGET_INFO_QUERY} == 1`,
+  };
+
+  const responseStandard = await getBackendSrv().get<OtelResponse>(
+    url,
+    paramsStandardTargets,
+    'explore-metrics-otel-check-standard'
+  );
+
+  return responseTotal.data.result.length === responseStandard.data.result.length;
 }
