c82f603825b18181c970aa0b5139c418e18bdf87
ArtistBanda <chirag.codes@gmail.com>
2024-08-03 21:42:45 +0530

    added css style to hints

--- packages/grafana-prometheus/src/querybuilder/shared/QueryEditorHints.tsx
+++ packages/grafana-prometheus/src/querybuilder/shared/QueryEditorHints.tsx
@@ -1,31 +1,31 @@
+import { css } from '@emotion/css';
 import { useEffect, useState } from 'react';
 
-import { QueryHint } from '@grafana/data';
+import { GrafanaTheme2, QueryHint } from '@grafana/data';
 import { reportInteraction } from '@grafana/runtime';
-import { Button, Tooltip } from '@grafana/ui';
+import { Button, Tooltip, useStyles2 } from '@grafana/ui';
 
 import { PromQueryEditorProps } from '../../components/types';
 
 export function QueryEditorHints(props: PromQueryEditorProps) {
   const [hints, setHints] = useState<QueryHint[]>([]);
   const { query, data, datasource } = props;
+  const styles = useStyles2(getStyles);
 
   useEffect(() => {
-    const promQuery = { expr: props.query.expr, refId: props.query.refId };
-    const hints = props.datasource
-      .getQueryHints(promQuery, props.data?.series || [])
-      .filter((hint) => hint.fix?.action);
+    const promQuery = { expr: query.expr, refId: query.refId };
+    const hints = datasource.getQueryHints(promQuery, data?.series || []).filter((hint) => hint.fix?.action);
     setHints(hints);
   }, [datasource, data, query]);
 
   return (
     <>
       {hints.length > 0 && (
-        <div>
+        <div className={styles.container}>
           {hints.map((hint) => {
             return (
               <Tooltip content={`${hint.label} ${hint.fix?.label}`} key={hint.type}>
-                <Button onClick={() => onHintButtonClick(hint, props)} fill="outline" size="sm">
+                <Button onClick={() => onHintButtonClick(hint, props)} fill="outline" size="sm" className={styles.hint}>
                   hint: {hint.fix?.title || hint.fix?.action?.type.toLowerCase().replace('_', ' ')}
                 </Button>
               </Tooltip>
@@ -48,3 +48,16 @@ function onHintButtonClick(hint: QueryHint, props: PromQueryEditorProps) {
     return props.onChange(newQuery);
   }
 }
+
+const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    container: css({
+      display: 'flex',
+      alignItems: 'start',
+    }),
+    hint: css({
+      marginRight: theme.spacing(1),
+      padEnd: theme.spacing(2),
+    }),
+  };
+};
