40516b9307fad1cfe46be6153ec58d32068c4d44
Matias Chomicki <matyax@gmail.com>
2024-08-05 18:23:03 +0200

    Hoverbot: provide image to GPT to interpret

--- public/app/core/components/Hoverbot/Hoverbot.tsx
+++ public/app/core/components/Hoverbot/Hoverbot.tsx
@@ -1,4 +1,5 @@
 
+import html2canvas from 'html2canvas';
 import { useCallback, useEffect, useState } from 'react';
 
 import { openai } from '@grafana/llm';
@@ -13,29 +14,56 @@ export const Hoverbot = () => {
     openai.enabled().then(setEnabled);
   }, []);
 
-  const helpMe = useCallback(() => {
-    if (!enabled) {
-      console.error('LLM Disabled');
-      return false;
-    }
-
-    setLoading(true);
-
+  const ask = useCallback((image: string) => {
     // Stream the completions. Each element is the next stream chunk.
     const stream = openai
       .streamChatCompletions({
         model: openai.Model.LARGE,
         messages: [
           { role: 'system', content: 'You are helping an observability user understand the data they are seeing.' },
-          { role: 'user', content: 'test' },
-        ],
+          // @ts-expect-error
+          { role: 'user', content: [
+            {
+              "type": "text",
+              "text": "Help me understand the following observability data from logs:"
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": image
+              }
+            }
+          ]
+        }],
       })
       .pipe(
         openai.accumulateContent(),
       );
     // Subscribe to the stream and update the state for each returned value.
-    return stream.subscribe(setReply);
-  }, [enabled]);
+    stream.subscribe({
+      next: setReply,
+      complete: () => {
+        setLoading(false);
+      },
+      error: (e) => {
+        console.error(e);
+        setLoading(false);
+      }
+    });
+  }, [])
+
+  const helpMe = useCallback(() => {
+    if (!enabled) {
+      console.error('LLM Disabled');
+      return;
+    }
+
+    setLoading(true);
+
+    html2canvas(document.querySelector('section')!).then((canvas) => {
+      ask(canvas.toDataURL());
+    });
+  }, [ask, enabled]);
 
   return (
     <div>
