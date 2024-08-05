741585fbcb6814983ba5dcac8d0f00eac7d2e1b4
Georges Chaudy <chaudyg@gmail.com>
2024-08-05 15:35:08 +0200

    unistore: re-enable integration tests

--- pkg/storage/unified/sql/test/integration_test.go
+++ pkg/storage/unified/sql/test/integration_test.go
@@ -60,7 +60,6 @@ func newServer(t *testing.T) (sql.Backend, resource.ResourceServer) {
 }
 
 func TestIntegrationBackendHappyPath(t *testing.T) {
-	t.Skip("TODO: test blocking, skipping to unblock Enterprise until we fix this")
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
@@ -168,7 +167,6 @@ func TestIntegrationBackendHappyPath(t *testing.T) {
 }
 
 func TestIntegrationBackendWatchWriteEventsFromLastest(t *testing.T) {
-	t.Skip("TODO: test blocking, skipping to unblock Enterprise until we fix this")
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
@@ -191,7 +189,6 @@ func TestIntegrationBackendWatchWriteEventsFromLastest(t *testing.T) {
 }
 
 func TestIntegrationBackendList(t *testing.T) {
-	t.Skip("TODO: test blocking, skipping to unblock Enterprise until we fix this")
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
@@ -325,7 +322,6 @@ func TestIntegrationBackendList(t *testing.T) {
 	})
 }
 func TestClientServer(t *testing.T) {
-	t.Skip("TODO: test blocking, skipping to unblock Enterprise until we fix this")
 	ctx := testutil.NewTestContext(t, time.Now().Add(5*time.Second))
 	dbstore := infraDB.InitTestDB(t)
 
