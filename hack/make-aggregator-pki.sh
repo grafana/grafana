ae7724f60646504b9be867ec6fd0d11ef1238733
Charandas Batra <charandas.batra@grafana.com>
2024-08-05 16:31:06 -0700

    add 10 year certs

--- hack/make-aggregator-pki.sh
+++ hack/make-aggregator-pki.sh
@@ -9,11 +9,11 @@ rm -rf data/grafana-aggregator
 mkdir -p data/grafana-aggregator
 
 openssl req -nodes -new -x509 -keyout data/grafana-aggregator/ca.key -out data/grafana-aggregator/ca.crt \
-  -subj "/C=US/ST=New Sweden/L=Stockholm /O=Grafana/OU=R&D/CN=test-ca/emailAddress=test@grafana.app"
+  -subj "/C=US/ST=New Sweden/L=Stockholm /O=Grafana/OU=R&D/CN=test-ca/emailAddress=test@grafana.app" -days 3650
 openssl req -out data/grafana-aggregator/client.csr -new -newkey rsa:4096 -nodes -keyout data/grafana-aggregator/client.key \
   -subj "/CN=development/O=system:masters" \
   -addext "extendedKeyUsage = clientAuth"
-openssl x509 -req -days 365 -in data/grafana-aggregator/client.csr -CA data/grafana-aggregator/ca.crt -CAkey data/grafana-aggregator/ca.key \
+openssl x509 -req -days 3650 -in data/grafana-aggregator/client.csr -CA data/grafana-aggregator/ca.crt -CAkey data/grafana-aggregator/ca.key \
   -set_serial 01 \
   -sha256 -out data/grafana-aggregator/client.crt \
   -copy_extensions=copyall
@@ -22,7 +22,7 @@ openssl req -out data/grafana-aggregator/server.csr -new -newkey rsa:4096 -nodes
   -subj "/CN=localhost/O=aggregated" \
   -addext "subjectAltName = DNS:v0alpha1.example.grafana.app.default.svc,DNS:localhost" \
   -addext "extendedKeyUsage = serverAuth, clientAuth"
-openssl x509 -req -days 365 -in data/grafana-aggregator/server.csr -CA data/grafana-aggregator/ca.crt -CAkey data/grafana-aggregator/ca.key \
+openssl x509 -req -days 3650 -in data/grafana-aggregator/server.csr -CA data/grafana-aggregator/ca.crt -CAkey data/grafana-aggregator/ca.key \
   -set_serial 02 \
   -sha256 -out data/grafana-aggregator/server.crt \
   -copy_extensions=copyall
