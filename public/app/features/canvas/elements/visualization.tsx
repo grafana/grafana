903bc425fff2e797d1b09ff7cb827b6c4c68cd91
Drew Slobodnjak <60050885+drew08t@users.noreply.github.com>
2024-08-05 18:05:56 -0700

    Vizards: Viz Element Styling (#91552)
    * Vizards: Viz Element Styling
    
    * Disable pointer events when inline editing
    
    * Make sure default panel title shows up
    
    * Allow for background color
    
    * Set default panel title and allow empty

--- public/app/features/canvas/elements/visualization.tsx
+++ public/app/features/canvas/elements/visualization.tsx
@@ -1,38 +1,39 @@
 import { css } from '@emotion/css';
 
-import { GrafanaTheme2, SelectableValue } from '@grafana/data';
-import { DataFrame, Field } from '@grafana/data/';
+import { Field, GrafanaTheme2, SelectableValue } from '@grafana/data';
+import { DataFrame } from '@grafana/data/';
 import { EmbeddedScene, PanelBuilders, SceneDataNode, SceneFlexItem, SceneFlexLayout } from '@grafana/scenes';
-import { stylesFactory } from '@grafana/ui';
+import { TextDimensionMode } from '@grafana/schema/dist/esm/index';
+import { stylesFactory, usePanelContext } from '@grafana/ui';
 import { config } from 'app/core/config';
 import { DimensionContext } from 'app/features/dimensions/context';
-import { ColorDimensionEditor } from 'app/features/dimensions/editors/ColorDimensionEditor';
 import { TextDimensionEditor } from 'app/features/dimensions/editors/TextDimensionEditor';
 
-import {
-  CanvasElementItem,
-  CanvasElementOptions,
-  CanvasElementProps,
-  defaultBgColor,
-  defaultTextColor,
-} from '../element';
+import { CanvasElementItem, CanvasElementOptions, CanvasElementProps, defaultTextColor } from '../element';
 import { Align, VAlign, VizElementConfig, VizElementData } from '../types';
 
 const panelTypes: Array<SelectableValue<string>> = Object.keys(PanelBuilders).map((type) => {
   return { label: type, value: type };
 });
 
-const VisualizationDisplay = (props: CanvasElementProps<VizElementConfig, VizElementData>) => {
-  const { data } = props;
-  const styles = getStyles(config.theme2, data);
+const defaultPanelTitle = 'New Visualization';
 
-  let panelToEmbed = PanelBuilders.timeseries().setTitle('Embedded Panel');
+const VisualizationDisplay = (props: CanvasElementProps<VizElementConfig, VizElementData>) => {
+  const context = usePanelContext();
+  const scene = context.instanceState?.scene;
+
+  const { data } = props;
+  const styles = getStyles(config.theme2, data, scene);
+
+  const panelTitle = data?.text ?? '';
+  let panelToEmbed = PanelBuilders.timeseries().setTitle(panelTitle);
   if (data?.vizType) {
     // TODO make this better
-    panelToEmbed = PanelBuilders[data.vizType as keyof typeof PanelBuilders]().setTitle('Embedded Panel');
+    panelToEmbed = PanelBuilders[data.vizType as keyof typeof PanelBuilders]().setTitle(panelTitle);
   }
 
   panelToEmbed.setData(new SceneDataNode({ data: data!.data }));
+  panelToEmbed.setDisplayMode('transparent');
   const panel = panelToEmbed.build();
 
   const embeddedPanel = new EmbeddedScene({
@@ -49,26 +50,18 @@ const VisualizationDisplay = (props: CanvasElementProps<VizElementConfig, VizEle
 
   return (
     <div className={styles.container}>
-      <span className={styles.span}>
-        <embeddedPanel.Component model={embeddedPanel} />
-      </span>
+      <embeddedPanel.Component model={embeddedPanel} />
     </div>
   );
 };
 
-const getStyles = stylesFactory((theme: GrafanaTheme2, data) => ({
+const getStyles = stylesFactory((theme: GrafanaTheme2, data, scene) => ({
   container: css({
     position: 'absolute',
     height: '100%',
     width: '100%',
     display: 'table',
-  }),
-  span: css({
-    display: 'table-cell',
-    verticalAlign: data?.valign,
-    textAlign: data?.align,
-    fontSize: `${data?.size}px`,
-    color: data?.color,
+    pointerEvents: scene?.isEditingEnabled ? 'none' : 'auto',
   }),
 }));
 
@@ -94,10 +87,11 @@ export const visualizationItem: CanvasElementItem<VizElementConfig, VizElementDa
       },
       vizType: 'timeseries',
       fields: options?.fields ?? [],
+      text: { mode: TextDimensionMode.Fixed, fixed: defaultPanelTitle },
     },
     background: {
       color: {
-        fixed: defaultBgColor,
+        fixed: config.theme2.colors.background.canvas,
       },
     },
     links: options?.links ?? [],
@@ -169,51 +163,8 @@ export const visualizationItem: CanvasElementItem<VizElementConfig, VizElementDa
         category,
         id: 'textSelector',
         path: 'config.text',
-        name: 'Text',
+        name: 'Panel Title',
         editor: TextDimensionEditor,
-      })
-      .addCustomEditor({
-        category,
-        id: 'config.color',
-        path: 'config.color',
-        name: 'Text color',
-        editor: ColorDimensionEditor,
-        settings: {},
-        defaultValue: {},
-      })
-      .addRadio({
-        category,
-        path: 'config.align',
-        name: 'Align text',
-        settings: {
-          options: [
-            { value: Align.Left, label: 'Left' },
-            { value: Align.Center, label: 'Center' },
-            { value: Align.Right, label: 'Right' },
-          ],
-        },
-        defaultValue: Align.Left,
-      })
-      .addRadio({
-        category,
-        path: 'config.valign',
-        name: 'Vertical align',
-        settings: {
-          options: [
-            { value: VAlign.Top, label: 'Top' },
-            { value: VAlign.Middle, label: 'Middle' },
-            { value: VAlign.Bottom, label: 'Bottom' },
-          ],
-        },
-        defaultValue: VAlign.Middle,
-      })
-      .addNumberInput({
-        category,
-        path: 'config.size',
-        name: 'Text size',
-        settings: {
-          placeholder: 'Auto',
-        },
       });
   },
 };
