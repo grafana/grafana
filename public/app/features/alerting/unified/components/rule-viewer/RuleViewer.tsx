f9f13e577a30602bb71e5daa58c516930aef70d0
Konrad Lalik <konrad.lalik@grafana.com>
2024-08-07 13:28:31 +0200

    Add useReturnTo hook to safely handle returnTo parameter

--- public/app/features/alerting/unified/components/rule-viewer/RuleViewer.tsx
+++ public/app/features/alerting/unified/components/rule-viewer/RuleViewer.tsx
@@ -12,6 +12,7 @@ import { CombinedRule, RuleHealth, RuleIdentifier } from 'app/types/unified-aler
 import { PromAlertingRuleState, PromRuleType } from 'app/types/unified-alerting-dto';
 
 import { defaultPageNav } from '../../RuleViewer';
+import { useReturnTo } from '../../hooks/useReturnTo';
 import { PluginOriginBadge } from '../../plugins/PluginOriginBadge';
 import { Annotation } from '../../utils/constants';
 import { makeDashboardLink, makePanelLink } from '../../utils/misc';
@@ -225,9 +226,9 @@ interface TitleProps {
 
 export const Title = ({ name, paused = false, state, health, ruleType, ruleOrigin }: TitleProps) => {
   const styles = useStyles2(getStyles);
-  const [queryParams] = useQueryParams();
   const isRecordingRule = ruleType === PromRuleType.Recording;
-  const returnTo = queryParams.returnTo ? String(queryParams.returnTo) : '/alerting/list';
+
+  const { returnTo } = useReturnTo('/alerting/list');
 
   return (
     <div className={styles.title}>
