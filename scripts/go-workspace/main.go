96e4fa681d04c56a49c86233f20397689e1f0b50
Todd Treece <todd.treece@grafana.com>
2024-08-08 15:10:33 -0400

    add skip:golangci-lint comment tag

--- scripts/go-workspace/main.go
+++ scripts/go-workspace/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/mod/modfile"
 )
@@ -37,29 +38,49 @@ func listSubmodules() error {
 	fs := flag.NewFlagSet("list-submodules", flag.ExitOnError)
 	workPath := fs.String("path", "go.work", "Path to go.work")
 	delimiter := fs.String("delimiter", "\n", "Delimiter to use when printing paths")
+	skip := fs.String("skip", "", "Skip submodules with this comment tag")
 	help := fs.Bool("help", false, "Print help message")
 	fs.Parse(os.Args[2:])
+
 	if *help {
 		fs.Usage()
 		return nil
 	}
+
 	workfile, err := parseGoWork(*workPath)
 	if err != nil {
 		return err
 	}
-	paths := getSubmodulePaths(workfile)
+
+	paths := getSubmodulePaths(workfile, *skip)
 	for _, p := range paths {
 		fmt.Printf("%s%s", p, *delimiter)
 	}
+
 	return nil
 }
 
-func getSubmodulePaths(wf *modfile.WorkFile) []string {
+func getSubmodulePaths(wf *modfile.WorkFile, skip string) []string {
 	var paths []string
 	for _, d := range wf.Use {
+		if hasSkipTag(d, skip) {
+			continue
+		}
 		paths = append(paths, d.Path)
 	}
 	return paths
+}
+
+func hasSkipTag(d *modfile.Use, tag string) bool {
+	if tag == "" {
+		return false
+	}
+	for _, c := range d.Syntax.Comments.Suffix {
+		if strings.Contains(c.Token, fmt.Sprintf("skip:%s", tag)) {
+			return true
+		}
+	}
+	return false
 }
 
 func parseGoWork(p string) (*modfile.WorkFile, error) {
