305fc06f18a4aa5f6e6c91a626903736ca916295
multimo <tim.mulqueen@gmail.com>
2024-08-08 12:13:55 +0200

    add netlify to plugin list

--- public/app/features/datasources/state/buildCategories.test.ts
+++ public/app/features/datasources/state/buildCategories.test.ts
@@ -53,7 +53,7 @@ describe('buildCategories', () => {
   it('should add enterprise phantom plugins', () => {
     const enterprisePluginsCategory = categories[3];
     expect(enterprisePluginsCategory.title).toBe('Enterprise plugins');
-    expect(enterprisePluginsCategory.plugins.length).toBe(24);
+    expect(enterprisePluginsCategory.plugins.length).toBe(25);
     expect(enterprisePluginsCategory.plugins[0].name).toBe('Adobe Analytics');
     expect(enterprisePluginsCategory.plugins[enterprisePluginsCategory.plugins.length - 1].name).toBe('Wavefront');
   });
