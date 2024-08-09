ffb75d0ae58d0416b45be6ff35bab2b293c4d24a
Karl Persson <kalle.persson@grafana.com>
2024-08-09 17:20:24 +0200

    Identity: remove GetTypedID (#91745)
    (cherry picked from commit bcfb66b416ee0f06d616bad614041f1c0139324a)

--- pkg/apimachinery/identity/requester.go
+++ pkg/apimachinery/identity/requester.go
@@ -17,15 +17,9 @@ type Requester interface {
 	GetRawIdentifier() string
 	// Deprecated: use GetUID instead
 	GetInternalID() (int64, error)
-
 	// GetID returns namespaced internalID for the entity
 	// Deprecated: use GetUID instead
 	GetID() TypedID
-	// GetTypedID returns the namespace and ID of the active entity.
-	// The namespace is one of the constants defined in pkg/apimachinery/identity.
-	// Deprecated: use GetID instead
-	GetTypedID() (kind IdentityType, identifier string)
-
 	// GetDisplayName returns the display name of the active entity.
 	// The display name is the name if it is set, otherwise the login or email.
 	GetDisplayName() string
@@ -82,14 +76,14 @@ type Requester interface {
 	GetIDClaims() *authnlib.Claims[authnlib.IDTokenClaims]
 }
 
-// IntIdentifier converts a string identifier to an int64.
+// IntIdentifier converts a typeID to an int64.
 // Applicable for users, service accounts, api keys and renderer service.
-// Errors if the identifier is not initialized or if namespace is not recognized.
-func IntIdentifier(kind IdentityType, identifier string) (int64, error) {
-	if IsIdentityType(kind, TypeUser, TypeAPIKey, TypeServiceAccount, TypeRenderService) {
-		id, err := strconv.ParseInt(identifier, 10, 64)
+// Errors if the identifier is not initialized or if type is not recognized.
+func IntIdentifier(typedID TypedID) (int64, error) {
+	if IsIdentityType(typedID, TypeUser, TypeAPIKey, TypeServiceAccount, TypeRenderService) {
+		id, err := strconv.ParseInt(typedID.ID(), 10, 64)
 		if err != nil {
-			return 0, fmt.Errorf("unrecognized format for valid type %s: %w", kind, err)
+			return 0, fmt.Errorf("unrecognized format for valid type %s: %w", typedID.Type(), err)
 		}
 
 		if id < 1 {
@@ -102,19 +96,18 @@ func IntIdentifier(kind IdentityType, identifier string) (int64, error) {
 	return 0, ErrNotIntIdentifier
 }
 
-// UserIdentifier converts a string identifier to an int64.
+// UserIdentifier converts a typeID to an int64.
 // Errors if the identifier is not initialized or if namespace is not recognized.
-// Returns 0 if the namespace is not user or service account
-func UserIdentifier(kind IdentityType, identifier string) (int64, error) {
-	userID, err := IntIdentifier(kind, identifier)
+// Returns 0 if the type is not user or service account
+func UserIdentifier(typedID TypedID) (int64, error) {
+	userID, err := IntIdentifier(typedID)
 	if err != nil {
-		// FIXME: return this error once entity namespaces are handled by stores
-		return 0, nil
+		return 0, err
 	}
 
-	if IsIdentityType(kind, TypeUser, TypeServiceAccount) {
+	if IsIdentityType(typedID, TypeUser, TypeServiceAccount) {
 		return userID, nil
 	}
 
-	return 0, nil
+	return 0, ErrInvalidIDType
 }
