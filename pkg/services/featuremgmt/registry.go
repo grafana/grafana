4fe4ae471e26c0cdf92fd0786da93113cd0ec8f7
Andres Martinez Gotor <andres.martinez@grafana.com>
2024-08-09 14:26:36 +0200

    Chore: Add skeleton for background plugin installer

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1373,6 +1373,13 @@ var (
 			Expression:  "true", // Enabled by default for now
 		},
 		{
+			Name:            "backgroundPluginInstaller",
+			Description:     "Enable background plugin installer",
+			Stage:           FeatureStageExperimental,
+			Owner:           grafanaPluginsPlatformSquad,
+			RequiresRestart: true,
+		},
+		{
 			Name:            "dataplaneAggregator",
 			Description:     "Enable grafana dataplane aggregator",
 			Stage:           FeatureStageExperimental,
