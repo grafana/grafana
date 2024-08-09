10773794d9159bd1a1127f589eb7d7ca40c3da1a
Erik Sundell <erik.sundell87@gmail.com>
2024-08-09 09:00:47 +0200

    change type name

--- packages/grafana-data/src/index.ts
+++ packages/grafana-data/src/index.ts
@@ -554,7 +554,7 @@ export {
   type PluginExtensionDataSourceConfigContext,
   type PluginExtensionCommandPaletteContext,
   type PluginExtensionOpenModalOptions,
-  type PluginExposedComponent,
+  type PluginExportedComponent,
 } from './types/pluginExtensions';
 export {
   type ScopeDashboardBindingSpec,
