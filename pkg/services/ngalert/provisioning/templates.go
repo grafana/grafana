43f0b79d64432a1c104caef4a07a304e466f2102
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-08-13 11:40:35 -0400

    add GetTemplate to template service

--- pkg/services/ngalert/provisioning/templates.go
+++ pkg/services/ngalert/provisioning/templates.go
@@ -56,6 +56,32 @@ func (t *TemplateService) GetTemplates(ctx context.Context, orgID int64) ([]defi
 	return templates, nil
 }
 
+func (t *TemplateService) GetTemplate(ctx context.Context, orgID int64, name string) (definitions.NotificationTemplate, error) {
+	revision, err := t.configStore.Get(ctx, orgID)
+	if err != nil {
+		return definitions.NotificationTemplate{}, err
+	}
+
+	for tmplName, tmpl := range revision.Config.TemplateFiles {
+		if tmplName != name {
+			continue
+		}
+		tmpl := definitions.NotificationTemplate{
+			Name:            name,
+			Template:        tmpl,
+			ResourceVersion: calculateTemplateFingerprint(tmpl),
+		}
+
+		provenance, err := t.provenanceStore.GetProvenance(ctx, &tmpl, orgID)
+		if err != nil {
+			return definitions.NotificationTemplate{}, err
+		}
+		tmpl.Provenance = definitions.Provenance(provenance)
+		return tmpl, nil
+	}
+	return definitions.NotificationTemplate{}, ErrTemplateNotFound.Errorf("")
+}
+
 func (t *TemplateService) SetTemplate(ctx context.Context, orgID int64, tmpl definitions.NotificationTemplate) (definitions.NotificationTemplate, error) {
 	err := tmpl.Validate()
 	if err != nil {
