89b56c622c600e2080a44d55518025bb30363d57
Ryan McKinley <ryantxu@gmail.com>
2024-08-14 15:55:02 +0300

    now with display

--- pkg/registry/apis/identity/legacy/query_display.sql
+++ pkg/registry/apis/identity/legacy/query_display.sql
@@ -3,10 +3,10 @@ SELECT org_user.org_id, u.id, u.uid, u.login, u.email, u.name,
   FROM {{ .Ident "user" }} as u JOIN org_user ON u.id = org_user.user_id
  WHERE org_user.org_id = {{ .Arg .Query.OrgID }} AND ( 1=2
 {{ if .Query.UIDs }}
-   OR uid IN {{ .Arg .Query.UIDs }}
+   OR uid IN ({{ .ArgList .Query.UIDs }})
 {{ end }}
 {{ if .Query.IDs }}
-   OR id > {{ .Arg .Query.IDs }}
+   OR u.id IN ({{ .ArgList .Query.IDs }})
 {{ end }}
  )
  ORDER BY u.id asc
