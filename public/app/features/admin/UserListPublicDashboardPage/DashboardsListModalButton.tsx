6d988aeaa511fab67a3c89a2628b48fa9a36a145
Lucy Chen <140550297+lucychen-grafana@users.noreply.github.com>
2024-08-15 13:09:01 -0400

    Change Public Dashboard text in Enterprise Users section (#91816)
    * update text
    
    * update tooltip
    
    * add conditionals
    
    * add conditions
    
    * add conditionals
    
    * fix conditionals

--- public/app/features/admin/UserListPublicDashboardPage/DashboardsListModalButton.tsx
+++ public/app/features/admin/UserListPublicDashboardPage/DashboardsListModalButton.tsx
@@ -2,6 +2,7 @@ import { css, cx } from '@emotion/css';
 
 import { GrafanaTheme2 } from '@grafana/data/src';
 import { selectors as e2eSelectors } from '@grafana/e2e-selectors/src';
+import { config } from '@grafana/runtime';
 import { Button, LoadingPlaceholder, Modal, ModalsController, useStyles2 } from '@grafana/ui/src';
 import { Trans, t } from 'app/core/internationalization';
 import {
@@ -21,7 +22,11 @@ export const DashboardsListModal = ({ email, onDismiss }: { email: string; onDis
     <Modal
       className={styles.modal}
       isOpen
-      title={t('public-dashboard-users-access-list.modal.dashboard-modal-title', 'Public dashboards')}
+      title={
+        config.featureToggles.newDashboardSharingComponent
+          ? t('public-dashboard-users-access-list.modal.shared-dashboard-modal-title', 'Shared dashboards')
+          : t('public-dashboard-users-access-list.modal.dashboard-modal-title', 'Public dashboards')
+      }
       onDismiss={onDismiss}
     >
       {isLoading ? (
@@ -42,9 +47,15 @@ export const DashboardsListModal = ({ email, onDismiss }: { email: string; onDis
                 href={generatePublicDashboardUrl(dash.publicDashboardAccessToken)}
                 onClick={onDismiss}
               >
-                <Trans i18nKey="public-dashboard-users-access-list.dashboard-modal.public-dashboard-link">
-                  Public dashboard URL
-                </Trans>
+                {config.featureToggles.newDashboardSharingComponent ? (
+                  <Trans i18nKey="public-dashboard-users-access-list.dashboard-modal.external-link">
+                    External link
+                  </Trans>
+                ) : (
+                  <Trans i18nKey="public-dashboard-users-access-list.dashboard-modal.public-dashboard-link">
+                    Public dashboard URL
+                  </Trans>
+                )}
               </a>
               <span className={styles.urlsDivider}>•</span>
               <a
@@ -52,9 +63,15 @@ export const DashboardsListModal = ({ email, onDismiss }: { email: string; onDis
                 href={generatePublicDashboardConfigUrl(dash.dashboardUid, dash.slug)}
                 onClick={onDismiss}
               >
-                <Trans i18nKey="public-dashboard-users-access-list.dashboard-modal.public-dashboard-setting">
-                  Public dashboard settings
-                </Trans>
+                {config.featureToggles.newDashboardSharingComponent ? (
+                  <Trans i18nKey="public-dashboard-users-access-list.dashboard-modal.sharing-setting">
+                    Sharing settings
+                  </Trans>
+                ) : (
+                  <Trans i18nKey="public-dashboard-users-access-list.dashboard-modal.public-dashboard-setting">
+                    Public dashboard settings
+                  </Trans>
+                )}
               </a>
             </div>
             <hr className={styles.divider} />
