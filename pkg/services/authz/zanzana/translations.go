aaf33c792325535733cd7526b068889a1ceb4cd9
Alexander Zobnin <alexanderzobnin@gmail.com>
2024-08-15 16:13:27 +0200

    Zanzana: Migrate basic, fixed and custom roles (#91814)
    * Zanzana: Migrate basic roles permissions
    
    * add basic roles assignments
    
    * refactor
    
    * Sync basic roles permissions in all orgs
    
    * migrate fixed roles
    
    * map root folders to orgs
    
    * fix basic role assignments in orgs
    
    * migrate other roles
    
    * migrate team roles assignments
    
    * add notes about authorization schema
    
    * don't migrate fixed roles

--- pkg/services/authz/zanzana/translations.go
+++ pkg/services/authz/zanzana/translations.go
@@ -46,16 +46,59 @@ var dashboardActions = map[string]string{
 	"dashboards.permissions:write": "permissions_write",
 }
 
+var orgActions = map[string]string{
+	"folders:create":            "folder_create",
+	"folders:read":              "folder_read",
+	"folders:write":             "folder_write",
+	"folders:delete":            "folder_delete",
+	"folders.permissions:read":  "folder_permissions_read",
+	"folders.permissions:write": "folder_permissions_write",
+
+	"dashboards:create":            "dashboard_create",
+	"dashboards:read":              "dashboard_read",
+	"dashboards:write":             "dashboard_write",
+	"dashboards:delete":            "dashboard_delete",
+	"dashboards.permissions:read":  "dashboard_permissions_read",
+	"dashboards.permissions:write": "dashboard_permissions_write",
+
+	"library.panels:create": "library_panel_create",
+	"library.panels:read":   "library_panel_read",
+	"library.panels:write":  "library_panel_write",
+	"library.panels:delete": "library_panel_delete",
+
+	"alert.rules:create": "alert_rule_create",
+	"alert.rules:read":   "alert_rule_read",
+	"alert.rules:write":  "alert_rule_write",
+	"alert.rules:delete": "alert_rule_delete",
+
+	"alert.silences:create": "alert_silence_create",
+	"alert.silences:read":   "alert_silence_read",
+	"alert.silences:write":  "alert_silence_write",
+}
+
 // RBAC to OpenFGA translations grouped by kind
 var actionKindTranslations = map[string]actionKindTranslation{
-	"folders": {
-		objectType:   "folder",
+	KindOrg: {
+		objectType:   TypeOrg,
+		orgScoped:    false,
+		translations: orgActions,
+	},
+	KindFolders: {
+		objectType:   TypeFolder,
 		orgScoped:    true,
 		translations: folderActions,
 	},
-	"dashboards": {
-		objectType:   "dashboard",
+	KindDashboards: {
+		objectType:   TypeDashboard,
 		orgScoped:    true,
 		translations: dashboardActions,
 	},
+}
+
+var basicRolesTranslations = map[string]string{
+	RoleGrafanaAdmin: "basic_grafana_admin",
+	RoleAdmin:        "basic_admin",
+	RoleEditor:       "basic_editor",
+	RoleViewer:       "basic_viewer",
+	RoleNone:         "basic_none",
 }
--- pkg/services/authz/zanzana/zanzana.go
+++ pkg/services/authz/zanzana/zanzana.go
@@ -10,14 +10,37 @@ import (
 const (
 	TypeUser      string = "user"
 	TypeTeam      string = "team"
+	TypeRole      string = "role"
 	TypeFolder    string = "folder"
 	TypeDashboard string = "dashboard"
+	TypeOrg       string = "org"
 )
 
 const (
 	RelationTeamMember string = "member"
 	RelationTeamAdmin  string = "admin"
 	RelationParent     string = "parent"
+	RelationAssignee   string = "assignee"
+	RelationOrg        string = "org"
+)
+
+const (
+	KindOrg        string = "org"
+	KindDashboards string = "dashboards"
+	KindFolders    string = "folders"
+)
+
+const (
+	RoleGrafanaAdmin = "Grafana Admin"
+	RoleAdmin        = "Admin"
+	RoleEditor       = "Editor"
+	RoleViewer       = "Viewer"
+	RoleNone         = "None"
+
+	BasicRolePrefix    = "basic:"
+	BasicRoleUIDPrefix = "basic_"
+
+	GlobalOrgID = 0
 )
 
 // NewTupleEntry constructs new openfga entry type:id[#relation].
@@ -34,7 +57,7 @@ func NewTupleEntry(objectType, id, relation string) string {
 // NewScopedTupleEntry constructs new openfga entry type:id[#relation]
 // with id prefixed by scope (usually org id)
 func NewScopedTupleEntry(objectType, id, relation, scope string) string {
-	return NewTupleEntry(objectType, fmt.Sprintf("%s-%s", scope, id), "")
+	return NewTupleEntry(objectType, fmt.Sprintf("%s-%s", scope, id), relation)
 }
 
 func TranslateToTuple(user string, action, kind, identifier string, orgID int64) (*openfgav1.TupleKey, bool) {
@@ -63,4 +86,28 @@ func TranslateToTuple(user string, action, kind, identifier string, orgID int64)
 	}
 
 	return tuple, true
+}
+
+func TranslateToOrgTuple(user string, action string, orgID int64) (*openfgav1.TupleKey, bool) {
+	typeTranslation, ok := actionKindTranslations[KindOrg]
+	if !ok {
+		return nil, false
+	}
+
+	relation, ok := typeTranslation.translations[action]
+	if !ok {
+		return nil, false
+	}
+
+	tuple := &openfgav1.TupleKey{
+		Relation: relation,
+		User:     user,
+		Object:   NewTupleEntry(typeTranslation.objectType, strconv.FormatInt(orgID, 10), ""),
+	}
+
+	return tuple, true
+}
+
+func TranslateBasicRole(role string) string {
+	return basicRolesTranslations[role]
 }
