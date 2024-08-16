ba568f2b7816f696200420fd9b648ba478a61f28
Jeff Levin <jeff@levinology.com>
2024-08-16 13:39:18 -0800

    fix tests

--- pkg/api/ds_query_test.go
+++ pkg/api/ds_query_test.go
@@ -6,15 +6,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/trace/noop"
 
 	"github.com/grafana/grafana/pkg/apimachinery/errutil"
 	"github.com/grafana/grafana/pkg/infra/db/dbtest"
@@ -213,56 +211,86 @@ var reqDatasourceByIdNotFound = `{
 }`
 
 func TestDataSourceQueryError(t *testing.T) {
+
+	type body struct {
+		Message    string `json:"message"`
+		MessageId  string `json:"messageId"`
+		StatusCode int    `json:"statusCode"`
+	}
+
 	tcs := []struct {
 		request        string
 		clientErr      error
 		expectedStatus int
-		expectedBody   string
+		expectedBody   body
 	}{
 		{
 			request:        reqValid,
 			clientErr:      plugins.ErrPluginUnavailable,
 			expectedStatus: http.StatusInternalServerError,
-			expectedBody:   `{"message":"Plugin unavailable","messageId":"plugin.unavailable","statusCode":500,"traceID":""}`,
+			expectedBody: body{
+				Message:    "Plugin unavailable",
+				MessageId:  "plugin.unavailable",
+				StatusCode: 500,
+			},
 		},
 		{
 			request:        reqValid,
 			clientErr:      plugins.ErrMethodNotImplemented,
 			expectedStatus: http.StatusNotFound,
-			expectedBody:   `{"message":"Method not implemented","messageId":"plugin.notImplemented","statusCode":404,"traceID":""}`,
+			expectedBody: body{
+				Message:    "Method not implemented",
+				MessageId:  "plugin.notImplemented",
+				StatusCode: 404,
+			},
 		},
 		{
 			request:        reqValid,
 			clientErr:      errors.New("surprise surprise"),
 			expectedStatus: errutil.StatusInternal.HTTPStatus(),
-			expectedBody:   `{"message":"An error occurred within the plugin","messageId":"plugin.downstreamError","statusCode":500,"traceID":""}`,
+			expectedBody: body{
+				Message:    "An error occurred within the plugin",
+				MessageId:  "plugin.downstreamError",
+				StatusCode: 500,
+			},
 		},
 		{
 			request:        reqNoQueries,
 			expectedStatus: http.StatusBadRequest,
-			expectedBody:   `{"message":"No queries found","messageId":"query.noQueries","statusCode":400,"traceID":""}`,
+			expectedBody: body{
+				Message:    "No queries found",
+				MessageId:  "query.noQueries",
+				StatusCode: 400,
+			},
 		},
 		{
 			request:        reqQueryWithInvalidDatasourceID,
 			expectedStatus: http.StatusBadRequest,
-			expectedBody:   `{"message":"Query does not contain a valid data source identifier","messageId":"query.invalidDatasourceId","statusCode":400,"traceID":""}`,
+			expectedBody: body{
+				Message:    "Query does not contain a valid data source identifier",
+				MessageId:  "query.invalidDatasourceId",
+				StatusCode: 400,
+			},
 		},
 		{
 			request:        reqDatasourceByUidNotFound,
 			expectedStatus: http.StatusNotFound,
-			expectedBody:   `{"message":"Data source not found","traceID":""}`,
+			expectedBody: body{
+				Message: "Data source not found",
+			},
 		},
 		{
 			request:        reqDatasourceByIdNotFound,
 			expectedStatus: http.StatusNotFound,
-			expectedBody:   `{"message":"Data source not found","traceID":""}`,
+			expectedBody: body{
+				Message: "Data source not found",
+			},
 		},
 	}
 
 	for _, tc := range tcs {
 
 		t.Run(fmt.Sprintf("Plugin client error %q should propagate to API", tc.clientErr), func(t *testing.T) {
-			otel.SetTracerProvider(noop.NewTracerProvider())
 
 			p := &plugins.Plugin{
 				JSONData: plugins.JSONData{
@@ -302,9 +330,18 @@ func TestDataSourceQueryError(t *testing.T) {
 			require.NoError(t, err)
 
 			require.Equal(t, tc.expectedStatus, resp.StatusCode)
-			body, err := io.ReadAll(resp.Body)
+
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
 			require.NoError(t, err)
-			require.Equal(t, tc.expectedBody, string(body))
+
+			var responseBody body
+			err = json.Unmarshal(bodyBytes, &responseBody)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.expectedBody.Message, responseBody.Message)
+			require.Equal(t, tc.expectedBody.MessageId, responseBody.MessageId)
+			require.Equal(t, tc.expectedBody.StatusCode, responseBody.StatusCode)
+
 			require.NoError(t, resp.Body.Close())
 		})
 	}
