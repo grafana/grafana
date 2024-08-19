9e714cc21fc83eb0b933e7f6af55a128efb15c58
Erik Sundell <erik.sundell87@gmail.com>
2024-08-19 13:12:00 +0200

    Update e2e/plugin-e2e/plugin-e2e-api-tests/as-admin-user/extensions/usePluginComponents.spec.ts
    Co-authored-by: Jack Westbrook <jack.westbrook@gmail.com>

--- e2e/plugin-e2e/plugin-e2e-api-tests/as-admin-user/extensions/usePluginComponents.spec.ts
+++ e2e/plugin-e2e/plugin-e2e-api-tests/as-admin-user/extensions/usePluginComponents.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@grafana/plugin-e2e';
 const pluginId = 'grafana-extensionstest-app';
 const exposedComponentTestId = 'exposed-component';
 
-test('should be render addedComponent with usePluginComponents hook', async ({ page }) => {
+test('should render component with usePluginComponents hook', async ({ page }) => {
   await page.goto(`/a/${pluginId}/added-components`);
   await expect(
     page.getByTestId('data-testid pg-added-components-container').getByTestId('b-app-add-component')
