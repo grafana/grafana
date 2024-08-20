3827c5e47ed91c8e80261515a7abaeade1adcc79
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2024-08-20 14:01:01 -0400

    test fixture whackamole

--- pkg/services/publicdashboards/service/service_test.go
+++ pkg/services/publicdashboards/service/service_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/trace/noop"
 
 	"github.com/grafana/grafana/pkg/api/dtos"
 	"github.com/grafana/grafana/pkg/apimachinery/errutil"
@@ -1547,7 +1548,7 @@ func TestPublicDashboardServiceImpl_NewPublicDashboardUid(t *testing.T) {
 			store.On("Find", mock.Anything, mock.Anything).
 				Return(tt.mockStore.PublicDashboard, tt.mockStore.Err)
 
-			pd := &PublicDashboardServiceImpl{store: store}
+			pd := &PublicDashboardServiceImpl{store: store, tracer: noop.NewTracerProvider().Tracer("test")}
 
 			got, err := pd.NewPublicDashboardUid(tt.args.ctx)
 			if !tt.wantErr(t, err, fmt.Sprintf("NewPublicDashboardUid(%v)", tt.args.ctx)) {
