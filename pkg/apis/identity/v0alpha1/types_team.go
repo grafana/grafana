9728b2c0a0936efe62ce5336b76cfa85f800ec39
Karl Persson <kalle.persson@grafana.com>
2024-08-21 11:41:30 +0200

    Add TeamBinding resource

--- pkg/apis/identity/v0alpha1/types_team.go
+++ pkg/apis/identity/v0alpha1/types_team.go
@@ -1,6 +1,8 @@
 package v0alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type Team struct {
@@ -9,6 +11,7 @@ type Team struct {
 
 	Spec TeamSpec `json:"spec,omitempty"`
 }
+
 type TeamSpec struct {
 	Title string `json:"name,omitempty"`
 	Email string `json:"email,omitempty"`
@@ -20,4 +23,38 @@ type TeamList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 
 	Items []Team `json:"items,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type TeamBinding struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec TeamBindingSpec `json:"spec,omitempty"`
+}
+
+type TeamBindingSpec struct {
+	Subjects []TeamSubject `json:"subjects,omitempty"`
+	TeamRef  TeamRef       `json:"teamRef,omitempty"`
+}
+
+type TeamSubject struct {
+	// Kind is the kind of the subject, only supports "User".
+	Kind string `json:"kind,omitempty"`
+
+	// Name is the unique identifier for subject.
+	Name string `json:"name,omitempty"`
+}
+
+type TeamRef struct {
+	// Name is the unique identifier for a team.
+	Name string `json:"name,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type TeamBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []TeamBinding `json:"items,omitempty"`
 }
