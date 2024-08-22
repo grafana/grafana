d53d3511e38de155cbe3232cfb2b84b6bd1f67fd
Kyle Brandt <kyle@grafana.com>
2024-08-22 09:00:42 -0400

    remove ts config import

--- packages/grafana-prometheus/src/result_transformer.ts
+++ packages/grafana-prometheus/src/result_transformer.ts
@@ -17,7 +17,7 @@ import {
   TIME_SERIES_TIME_FIELD_NAME,
   TIME_SERIES_VALUE_FIELD_NAME,
 } from '@grafana/data';
-import { config, getDataSourceSrv } from '@grafana/runtime';
+import { getDataSourceSrv } from '@grafana/runtime';
 
 import { ExemplarTraceIdDestination, PromMetric, PromQuery, PromValue } from './types';
 
