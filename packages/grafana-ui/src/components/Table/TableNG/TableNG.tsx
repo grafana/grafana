3997734a58ce7e742c20228a936165d26c9302e3
Kyle Cunningham <kyle@codeincarnate.com>
2024-08-22 16:44:52 -0500

    Latest changes

--- packages/grafana-ui/src/components/Table/TableNG/TableNG.tsx
+++ packages/grafana-ui/src/components/Table/TableNG/TableNG.tsx
@@ -1,21 +1,46 @@
 import 'react-data-grid/lib/styles.css';
 
 import { Component } from 'react';
-import DataGrid from 'react-data-grid';
+import DataGrid, { Column } from 'react-data-grid';
+import { Cell } from 'react-table';
 
 import { DataFrame } from '@grafana/data';
 
+import { TableCell } from '../Cells/TableCell';
 import { TableNGProps } from '../types';
+
+
+interface TableColumn {
+  key: string;
+  name: string;
+  renderCell: Function;
+}
+
+interface TableRow {
+  id: number;
+  title: string;
+  cell: Cell;
+}
 
 export class TableNG extends Component<TableNGProps> {
 
   mapFrameToDataGrid(main: DataFrame) {
-    const columns: Array<{ key: string; name: string }> = [];
+    const columns: Array<Column<TableRow>> = [];
     const rows: Array<{ [key: string]: string }> = [];
 
     main.fields.map((field) => {
       const key = field.name;
-      columns.push({ key, name: key }); // TODO add display function output
+
+      columns.push({
+        key, name: key, renderCell: (props: any) => {
+          console.log(props);
+          return <TableCell {...props} />
+        }
+      });
+
+      console.log(field);
+
+
       field.values.map((value, index) => {
         const currentValue = { [key]: String(value) };
         if (rows.length > index) {
