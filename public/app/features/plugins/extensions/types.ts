8a37c8f56615b972430ad461ec4c27e5ddbdffd5
Erik Sundell <erik.sundell87@gmail.com>
2024-08-22 09:25:25 +0200

    use new addedComponent registry in legacy endpoints

--- public/app/features/plugins/extensions/types.ts
+++ public/app/features/plugins/extensions/types.ts
@@ -1,5 +1,8 @@
 import type { PluginExtensionConfig } from '@grafana/data';
 
+import { AddedComponentRegistryItem } from './registry/AddedComponentsRegistry';
+import { RegistryType } from './registry/Registry';
+
 // The information that is stored in the registry
 export type PluginExtensionRegistryItem = {
   // Any additional meta information that we would like to store about the extension in the registry
@@ -13,3 +16,5 @@ export type PluginExtensionRegistry = {
   id: string;
   extensions: Record<string, PluginExtensionRegistryItem[]>;
 };
+
+export type AddedComponentsRegistryState = RegistryType<Array<AddedComponentRegistryItem<{}>>>;
