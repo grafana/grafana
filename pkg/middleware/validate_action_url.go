283f99848eb61a0c77639f8112be321244bf4dc3
Brian Gann <bkgann@gmail.com>
2024-08-22 17:42:55 -0400

    cleanup

--- pkg/middleware/validate_action_url.go
+++ pkg/middleware/validate_action_url.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/grafana/grafana/pkg/cmd/grafana-cli/logger"
 	"github.com/grafana/grafana/pkg/infra/log"
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
 	"github.com/grafana/grafana/pkg/setting"
@@ -21,12 +20,6 @@ func ValidateActionUrl(cfg *setting.Cfg, logger log.Logger) web.Handler {
 			return
 		}
 
-		// only process requests targeting local instance
-		if !isLocalPath(c) {
-			// call next on url
-			return
-		}
-
 		// only process POST and PUT
 		if c.Req.Method != http.MethodPost && c.Req.Method != http.MethodPut {
 			return
@@ -37,7 +30,7 @@ func ValidateActionUrl(cfg *setting.Cfg, logger log.Logger) web.Handler {
 			action := c.Req.Header.Get("X-Grafana-Action")
 
 			if action == "" {
-				// header not found, just return
+				// header not found, this is not an action request
 				return
 			}
 
@@ -48,48 +41,15 @@ func ValidateActionUrl(cfg *setting.Cfg, logger log.Logger) web.Handler {
 			for _, i := range pathsToCheck {
 				logger.Info("Checking match", "actions", i)
 				matched := glob.Glob(i, urlToCheck.Path)
-				/*
-					matched, err := path.Match(i, urlToCheck.Path)
-					if err != nil {
-						// match error, ignore
-						logger.Warn("Error matching configured paths", "err", err)
-						c.JsonApiErr(http.StatusForbidden, fmt.Sprintf("Error matching configured paths: %s", err.Error()), nil)
-						return
-					}
-				*/
-				if matched {
+				if matched && action != "" {
 					// allowed
-					logger.Info("POST/PUT call matches allow configuration settings", "actions_allow_post_url", i)
+					logger.Info("POST/PUT call matches allow configuration settings", "ValidateActionUrl", i)
 					return
 				}
 			}
-			logger.Warn("POST/PUT to path not allowed", "warn", urlToCheck)
+			logger.Warn("POST/PUT to path not allowed", "ValidateActionUrl", urlToCheck)
 			c.JsonApiErr(http.StatusForbidden, fmt.Sprintf("POST/PUT to path not allowed: %s", urlToCheck), nil)
 			return
 		}
 	}
-}
-
-// isLocalPath
-// Actions are processed by internal api paths, this checks the URL to ensure the request is to the local instance
-func isLocalPath(c *contextmodel.ReqContext) bool {
-	netAddr, err := util.SplitHostPortDefault(c.Req.Host, "", "0") // we ignore the port
-	if err != nil {
-		c.JsonApiErr(http.StatusBadRequest, fmt.Sprintf("Error parsing request for action: %s", err.Error()), nil)
-		return false
-	}
-
-	urlAddr, err := util.SplitHostPortDefault(c.Req.URL.Host, "", "0") // we ignore the port
-	if err != nil {
-		// match error, ignore
-		logger.Warn("Error getting url address", "err", err)
-		return false
-	}
-	pathIsLocal := urlAddr.Host == netAddr.Host
-	if netAddr.Host != "" || pathIsLocal {
-		// request is local
-		return true
-	}
-
-	return false
 }
