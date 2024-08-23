ab58c4d7576ea88441095e2a11229c213c5dc65d
Arati R. <33031346+suntala@users.noreply.github.com>
2024-08-23 11:48:42 +0200

    Provisioning: Allow startup when there are dashboard provisioning failures (#92201)
    * Stop returning an error if dashboard provisioning fails
    * Test that Run() does not error when dashboard provisioning fails
    
    (cherry picked from commit 927ce6c700bee7338aee929f895ee6c836afd392)

--- pkg/services/provisioning/provisioning.go
+++ pkg/services/provisioning/provisioning.go
@@ -120,6 +120,7 @@ func newProvisioningServiceImpl(
 	newDashboardProvisioner dashboards.DashboardProvisionerFactory,
 	provisionDatasources func(context.Context, string, datasources.BaseDataSourceService, datasources.CorrelationsStore, org.Service) error,
 	provisionPlugins func(context.Context, string, pluginstore.Store, pluginsettings.Service, org.Service) error,
+	searchService searchV2.SearchService,
 ) *ProvisioningServiceImpl {
 	return &ProvisioningServiceImpl{
 		log:                     log.New("provisioning"),
@@ -127,6 +128,7 @@ func newProvisioningServiceImpl(
 		provisionDatasources:    provisionDatasources,
 		provisionPlugins:        provisionPlugins,
 		Cfg:                     setting.NewCfg(),
+		searchService:           searchService,
 	}
 }
 
@@ -183,7 +185,6 @@ func (ps *ProvisioningServiceImpl) Run(ctx context.Context) error {
 	err := ps.ProvisionDashboards(ctx)
 	if err != nil {
 		ps.log.Error("Failed to provision dashboard", "error", err)
-		return err
 	}
 	if ps.dashboardProvisioner.HasDashboardSources() {
 		ps.searchService.TriggerReIndex()
