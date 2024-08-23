1350526c051ce8de3bc9bdb18853a59ec69015ee
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-08-23 16:50:37 +0100

    Update component usage and tests

--- public/app/features/alerting/unified/mocks/server/handlers/notifications.ts
+++ public/app/features/alerting/unified/mocks/server/handlers/notifications.ts
@@ -1,13 +1,18 @@
 import { HttpResponse, http } from 'msw';
 
 import alertmanagerConfig from 'app/features/alerting/unified/components/contact-points/__mocks__/alertmanager.config.mock.json';
-import { GrafanaManagedContactPoint } from 'app/plugins/datasource/alertmanager/types';
+import { GrafanaManagedContactPoint, MuteTimeInterval } from 'app/plugins/datasource/alertmanager/types';
 
-const defaultReceiversResponse: GrafanaManagedContactPoint[] = JSON.parse(JSON.stringify(alertmanagerConfig))
-  .alertmanager_config.receivers;
+const defaultReceiversResponse: GrafanaManagedContactPoint[] = alertmanagerConfig.alertmanager_config.receivers;
+
+const defaultTimeIntervalsResponse: MuteTimeInterval[] = alertmanagerConfig.alertmanager_config.time_intervals;
 
 const getNotificationReceiversHandler = (response = defaultReceiversResponse) =>
   http.get('/api/v1/notifications/receivers', () => HttpResponse.json(response));
 
-const handlers = [getNotificationReceiversHandler()];
+const getTimeIntervalsHandler = (response = defaultTimeIntervalsResponse) =>
+  http.get('/api/v1/notifications/time-intervals', () => HttpResponse.json(response));
+
+const handlers = [getNotificationReceiversHandler(), getTimeIntervalsHandler()];
+
 export default handlers;
