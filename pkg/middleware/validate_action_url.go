d8faa00aa55c0dab4e259546bc6141791de95b01
Brian Gann <bkgann@gmail.com>
2024-08-26 14:44:05 -0400

    refactoring

--- pkg/middleware/validate_action_url.go
+++ pkg/middleware/validate_action_url.go
@@ -1,10 +1,8 @@
 package middleware
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
-	"reflect"
 
 	"github.com/gobwas/glob"
 	"github.com/grafana/grafana/pkg/cmd/grafana-cli/logger"
@@ -35,37 +33,34 @@ func (e errorWithStatus) Unwrap() error {
 }
 
 func ValidateActionUrl(cfg *setting.Cfg, logger log.Logger) func(http.Handler) http.Handler {
+	// get the urls allowed from server config
+	allGlobs, globErr := cacheGlobs(cfg.ActionsAllowPostURL)
+	if globErr != nil {
+		logger.Error("invalid glob settings in config section [security] actions_allow_post_url", cfg.ActionsAllowPostURL)
+	}
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			e := &errorWithStatus{}
 			ctx := contexthandler.FromContext(r.Context())
-			// get the urls allowed from server config
-			allGlobs, err := cacheGlobs(cfg.ActionsAllowPostURL)
-			if err != nil {
-				if !errors.As(err, &e) {
-					http.Error(w, fmt.Sprintf("internal server error: expected error type errorWithStatus, got %s. Error: %v", reflect.TypeOf(err), err), http.StatusInternalServerError)
-				}
-				http.Error(w, err.Error(), e.HTTPStatus)
+			// if no header
+			// return nil
+			// check if action header exists
+			action := ctx.Req.Header.Get("X-Grafana-Action")
+			if action == "" {
+				// header not found, this is not an action request
+				next.ServeHTTP(w, r)
 				return
 			}
-			matched, allowed, matchErr := check(ctx, allGlobs, logger)
+			if globErr != nil {
+				http.Error(w, "check server logs for glob configuration failure", http.StatusInternalServerError)
+				return
+			}
+			matchErr := check(ctx, allGlobs, logger)
 			if matchErr != nil {
-				if !errors.As(matchErr, &e) {
-					http.Error(w, fmt.Sprintf("internal server error: expected error type errorWithStatus, got %s. Error: %v", reflect.TypeOf(err), err), http.StatusInternalServerError)
-				}
-				http.Error(w, matchErr.Error(), e.HTTPStatus)
+				http.Error(w, matchErr.Error(), http.StatusMethodNotAllowed)
 				return
 			}
-			if matched {
-				if !allowed {
-					logger.Error("POST/PUT to path not allowed", "validateActionUrl", ctx.Req.URL)
-					return
-				} else {
-					next.ServeHTTP(w, r)
-					return
-				}
-			}
-			// allowed and no matches fall through
+			// no errors fall through
 			next.ServeHTTP(w, r)
 		})
 	}
@@ -74,45 +69,36 @@ func ValidateActionUrl(cfg *setting.Cfg, logger log.Logger) func(http.Handler) h
 // check
 // Detects header for action urls and compares to globbed pattern list
 // returns true if allowed
-func check(ctx *contextmodel.ReqContext, allGlobs *[]glob.Glob, logger log.Logger) (bool, bool, *errorWithStatus) {
-	// ignore local render calls
-	if ctx.IsRenderCall {
-		return false, false, nil
-	}
+func check(ctx *contextmodel.ReqContext, allGlobs *[]glob.Glob, logger log.Logger) *errorWithStatus {
 	// only process POST and PUT
 	if ctx.Req.Method != http.MethodPost && ctx.Req.Method != http.MethodPut {
-		return false, false, nil
-	}
-	// if no header
-	// return nil
-	// check if action header exists
-	action := ctx.Req.Header.Get("X-Grafana-Action")
-	if action == "" {
-		// header not found, this is not an action request
-		return false, false, nil
+		return &errorWithStatus{
+			Underlying: fmt.Errorf("method not allowed for path %s", ctx.Req.URL),
+			HTTPStatus: http.StatusMethodNotAllowed,
+		}
 	}
 	// for each split config
 	// if matches glob
 	// return nil
 	urlToCheck := ctx.Req.URL
 	if matchesAllowedPath(allGlobs, urlToCheck.Path) {
-		return true, true, nil
+		return nil
 	}
-	logger.Warn("POST/PUT to path not allowed", "validateActionUrl", urlToCheck)
+	logger.Warn("POST/PUT to path not allowed", urlToCheck)
 	// return some error
-	return false, false, &errorWithStatus{
-		Underlying: fmt.Errorf("POST/PUT not allowed for path %s", urlToCheck),
+	return &errorWithStatus{
+		Underlying: fmt.Errorf("method POST/PUT not allowed for path %s", urlToCheck),
 		HTTPStatus: http.StatusMethodNotAllowed,
 	}
 }
 
 func matchesAllowedPath(allGlobs *[]glob.Glob, pathToCheck string) bool {
-	logger.Info("Checking url", "actions", pathToCheck)
-	for _, i := range *allGlobs {
-		logger.Info("Checking match", "actions", i)
-		if i.Match(pathToCheck) {
+	logger.Debug("Checking url", "actions", pathToCheck)
+	for _, rule := range *allGlobs {
+		logger.Debug("Checking match", "actions", rule)
+		if rule.Match(pathToCheck) {
 			// allowed
-			logger.Info("POST/PUT call matches allow configuration settings", "ValidateActionUrl", i)
+			logger.Debug("POST/PUT call matches allow configuration settings")
 			return true
 		}
 	}
