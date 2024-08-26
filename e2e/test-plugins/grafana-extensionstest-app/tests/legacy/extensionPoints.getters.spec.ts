fa9fb4996e41885fa0474e1d7921d359eb9be89c
Erik Sundell <erik.sundell87@gmail.com>
2024-08-26 12:54:10 +0200

    use correct file names

--- e2e/test-plugins/grafana-extensionstest-app/tests/legacy/extensionPoints.getters.spec.ts
+++ e2e/test-plugins/grafana-extensionstest-app/tests/legacy/extensionPoints.getters.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-import { ensureExtensionRegistryIsPopulated } from '../utils';
 import { testIds } from '../../testIds';
 import pluginJson from '../../plugin.json';
 
-test.describe('usePluginExtensions + configureExtensionLink', () => {
+test.describe('getPluginExtensions + configureExtensionLink', () => {
   test('should extend the actions menu with a link to a-app plugin', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
-    await ensureExtensionRegistryIsPopulated(page);
-    const section = await page.getByTestId(testIds.legacyGettersPage.section1);
+    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
+    const section = await page.getByTestId(testIds.legacyHooksPage.section1);
     await section.getByTestId(testIds.actions.button).click();
     await page.getByTestId(testIds.container).getByText('Go to A').click();
     await page.getByTestId(testIds.modal.open).click();
@@ -16,22 +14,20 @@ test.describe('usePluginExtensions + configureExtensionLink', () => {
   });
 });
 
-test.describe('usePluginExtensions + configureExtensionComponent', () => {
+test.describe('getPluginExtensions + configureExtensionComponent', () => {
   test('should extend main app with component extension from app B', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
-    await ensureExtensionRegistryIsPopulated(page);
-    const section = await page.getByTestId(testIds.legacyGettersPage.section1);
+    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
+    const section = await page.getByTestId(testIds.legacyHooksPage.section1);
     await section.getByTestId(testIds.actions.button).click();
     await page.getByTestId(testIds.container).getByText('Open from B').click();
     await expect(page.getByTestId(testIds.appB.modal)).toBeVisible();
   });
 });
 
-test.describe('usePluginLinkExtensions + configureExtensionLink', () => {
+test.describe('getPluginLinkExtensions + configureExtensionLink', () => {
   test('should extend the actions menu with a link to a-app plugin', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
-    await ensureExtensionRegistryIsPopulated(page);
-    const section = await page.getByTestId(testIds.legacyGettersPage.section2);
+    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
+    const section = await page.getByTestId(testIds.legacyHooksPage.section2);
     await section.getByTestId(testIds.actions.button).click();
     await page.getByTestId(testIds.container).getByText('Go to A').click();
     await page.getByTestId(testIds.modal.open).click();
@@ -39,14 +35,11 @@ test.describe('usePluginLinkExtensions + configureExtensionLink', () => {
   });
 });
 
-test.describe('usePluginComponentExtensions + configureExtensionComponent', () => {
+test.describe('getPluginComponentExtensions + configureExtensionComponent', () => {
   test('should extend the actions menu with a command triggered from b-app plugin', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
-    await ensureExtensionRegistryIsPopulated(page);
+    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
     await expect(
-      page
-        .getByTestId('configure-extension-component-get-plugin-component-extensions')
-        .getByTestId(testIds.appB.reusableComponent)
+      page.getByTestId(testIds.legacyHooksPage.section3).getByTestId(testIds.appB.reusableComponent)
     ).toHaveText('Hello World!');
   });
 });
--- e2e/test-plugins/grafana-extensionstest-app/tests/legacy/extensionPoints.hooks.spec.ts
+++ e2e/test-plugins/grafana-extensionstest-app/tests/legacy/extensionPoints.hooks.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
+import { ensureExtensionRegistryIsPopulated } from '../utils';
 import { testIds } from '../../testIds';
 import pluginJson from '../../plugin.json';
 
-test.describe('getPluginExtensions + configureExtensionLink', () => {
+test.describe('usePluginExtensions + configureExtensionLink', () => {
   test('should extend the actions menu with a link to a-app plugin', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
-    const section = await page.getByTestId(testIds.legacyHooksPage.section1);
+    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
+    await ensureExtensionRegistryIsPopulated(page);
+    const section = await page.getByTestId(testIds.legacyGettersPage.section1);
     await section.getByTestId(testIds.actions.button).click();
     await page.getByTestId(testIds.container).getByText('Go to A').click();
     await page.getByTestId(testIds.modal.open).click();
@@ -14,20 +16,22 @@ test.describe('getPluginExtensions + configureExtensionLink', () => {
   });
 });
 
-test.describe('getPluginExtensions + configureExtensionComponent', () => {
+test.describe('usePluginExtensions + configureExtensionComponent', () => {
   test('should extend main app with component extension from app B', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
-    const section = await page.getByTestId(testIds.legacyHooksPage.section1);
+    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
+    await ensureExtensionRegistryIsPopulated(page);
+    const section = await page.getByTestId(testIds.legacyGettersPage.section1);
     await section.getByTestId(testIds.actions.button).click();
     await page.getByTestId(testIds.container).getByText('Open from B').click();
     await expect(page.getByTestId(testIds.appB.modal)).toBeVisible();
   });
 });
 
-test.describe('getPluginLinkExtensions + configureExtensionLink', () => {
+test.describe('usePluginLinkExtensions + configureExtensionLink', () => {
   test('should extend the actions menu with a link to a-app plugin', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
-    const section = await page.getByTestId(testIds.legacyHooksPage.section2);
+    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
+    await ensureExtensionRegistryIsPopulated(page);
+    const section = await page.getByTestId(testIds.legacyGettersPage.section2);
     await section.getByTestId(testIds.actions.button).click();
     await page.getByTestId(testIds.container).getByText('Go to A').click();
     await page.getByTestId(testIds.modal.open).click();
@@ -35,11 +39,14 @@ test.describe('getPluginLinkExtensions + configureExtensionLink', () => {
   });
 });
 
-test.describe('getPluginComponentExtensions + configureExtensionComponent', () => {
+test.describe('usePluginComponentExtensions + configureExtensionComponent', () => {
   test('should extend the actions menu with a command triggered from b-app plugin', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/legacy-hooks`);
+    await page.goto(`/a/${pluginJson.id}/legacy-getters`);
+    await ensureExtensionRegistryIsPopulated(page);
     await expect(
-      page.getByTestId(testIds.legacyHooksPage.section3).getByTestId(testIds.appB.reusableComponent)
+      page
+        .getByTestId('configure-extension-component-get-plugin-component-extensions')
+        .getByTestId(testIds.appB.reusableComponent)
     ).toHaveText('Hello World!');
   });
 });
