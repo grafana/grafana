d86de47820dfe4cbebf5b724cf8cfd92f2234c3b
leonorfmartins <leonorfmartins@gmail.com>
2024-08-26 14:10:26 +0200

    Fix test

--- pkg/setting/setting_unified_storage_test.go
+++ pkg/setting/setting_unified_storage_test.go
@@ -1,29 +1,28 @@
 package setting
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/grafana/grafana/pkg/apiserver/rest"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestCfg_setUnifiedStorageConfig(t *testing.T) {
-// 	t.Run("read unified_storage configs", func(t *testing.T) {
-// 		cfg := NewCfg()
-// 		err := cfg.Load(CommandLineArgs{HomePath: "../../", Config: "../../conf/defaults.ini"})
-// 		assert.NoError(t, err)
+func TestCfg_setUnifiedStorageConfig(t *testing.T) {
+	t.Run("read unified_storage configs", func(t *testing.T) {
+		cfg := NewCfg()
+		err := cfg.Load(CommandLineArgs{HomePath: "../../", Config: "../../conf/defaults.ini"})
+		assert.NoError(t, err)
 
-// 		s, err := cfg.Raw.NewSection("unified_storage")
-// 		assert.NoError(t, err)
+		s, err := cfg.Raw.NewSection("unified_storage.playlist.grafana.app/playlists")
+		assert.NoError(t, err)
 
-// 		_, err = s.NewKey("playlist.grafana.app/playlists", "2")
-// 		assert.NoError(t, err)
+		_, err = s.NewKey("dualWriterMode", "2")
+		assert.NoError(t, err)
 
-// 		cfg.setUnifiedStorageConfig()
+		cfg.setUnifiedStorageConfig()
 
-// 		value, exists := cfg.UnifiedStorage["playlist.grafana.app/playlists"]
+		value, exists := cfg.UnifiedStorage["playlist.grafana.app/playlists"]
 
-// 		assert.Equal(t, exists, true)
-// 		assert.Equal(t, value, rest.DualWriterMode(2))
-// 	})
-// }
+		assert.Equal(t, exists, true)
+		assert.Equal(t, value, UnifiedStorageConfig{DualWriterMode: 2})
+	})
+}
