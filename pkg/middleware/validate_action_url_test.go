7e948b02a5bc6d53f5e047c5584c92d9f54f0874
Brian Gann <bkgann@gmail.com>
2024-08-27 16:57:17 -0400

    add more tests

--- pkg/middleware/validate_action_url_test.go
+++ pkg/middleware/validate_action_url_test.go
@@ -1,10 +1,77 @@
 package middleware
 
 import (
+	"net/http"
 	"testing"
 
+	"github.com/grafana/grafana/pkg/setting"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestMiddlewareValidateActionUrl(t *testing.T) {
+	tests := []struct {
+		name                string
+		method              string
+		path                string
+		actionsAllowPostURL string
+		addHeader           bool
+		code                int
+	}{
+		{
+			name:                "POST without action header",
+			method:              "POST",
+			path:                "/api/plugins/org-generic-app",
+			actionsAllowPostURL: "",
+			addHeader:           false,
+			code:                http.StatusOK,
+		},
+		{
+			name:                "POST with action header, no paths defined",
+			method:              "POST",
+			path:                "/api/plugins/org-generic-app",
+			actionsAllowPostURL: "",
+			addHeader:           true,
+			code:                http.StatusMethodNotAllowed,
+		},
+		{
+			name:                "POST action with allowed path",
+			method:              "POST",
+			path:                "/api/plugins/org-generic-app",
+			actionsAllowPostURL: "/api/plugins/*",
+			addHeader:           true,
+			code:                http.StatusOK,
+		},
+		{
+			name:                "POST action without invalid path",
+			method:              "POST",
+			path:                "/api/notplugins/org-generic-app",
+			actionsAllowPostURL: "/api/plugins/*",
+			addHeader:           true,
+			code:                http.StatusMethodNotAllowed,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			middlewareScenario(t, tt.name, func(t *testing.T, sc *scenarioContext) {
+				sc.m.Post(tt.path, sc.defaultHandler)
+				sc.fakeReq(tt.method, tt.path)
+				if tt.addHeader {
+					sc.req.Header.Add("X-Grafana-Action", "1")
+				}
+				sc.exec()
+				resp := sc.resp.Result()
+				t.Cleanup(func() {
+					err := resp.Body.Close()
+					assert.NoError(t, err)
+				})
+				// nolint:bodyclose
+				assert.Equal(t, tt.code, sc.resp.Result().StatusCode)
+			}, func(cfg *setting.Cfg) {
+				cfg.ActionsAllowPostURL = tt.actionsAllowPostURL
+			})
+		})
+	}
+}
 
 func TestMatchesAllowedPath(t *testing.T) {
 	tests := []struct {
