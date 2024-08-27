769ce9c21d80d177ae41ded8d298d1839428c300
Erik Sundell <erik.sundell87@gmail.com>
2024-08-27 14:00:28 +0200

    add hook tests

--- public/app/features/plugins/extensions/usePluginLinks.test.tsx
+++ public/app/features/plugins/extensions/usePluginLinks.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen } from '@testing-library/react';
+import { act } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 
 import { AddedLinksRegistry } from './registry/AddedLinksRegistry';
@@ -22,7 +22,7 @@ describe('usePluginLinks()', () => {
     registry = new AddedLinksRegistry();
   });
 
-  it('should return an empty array if there are no extensions registered for the extension point', () => {
+  it('should return an empty array if there are no link extensions registered for the extension point', () => {
     const usePluginComponents = createUsePluginLinks(registry);
     const { result } = renderHook(() =>
       usePluginComponents({
@@ -33,8 +33,8 @@ describe('usePluginLinks()', () => {
     expect(result.current.links).toEqual([]);
   });
 
-  it('should only return the plugin extension components for the given extension point ids', async () => {
-    const extensionPointId = 'plugins/foo/bar/v1';
+  it('should only return the link extensions for the given extension point ids', async () => {
+    const extensionPointId = 'plugins/foo/bar';
     const pluginId = 'my-app-plugin';
 
     registry.register({
@@ -44,41 +44,36 @@ describe('usePluginLinks()', () => {
           targets: extensionPointId,
           title: '1',
           description: '1',
-          component: () => <div>Hello World1</div>,
+          path: `/a/${pluginId}/2`,
         },
         {
           targets: extensionPointId,
           title: '2',
           description: '2',
-          component: () => <div>Hello World2</div>,
+          path: `/a/${pluginId}/2`,
         },
         {
-          targets: 'plugins/another-extension/v1',
+          targets: 'plugins/another-extension',
           title: '3',
           description: '3',
-          component: () => <div>Hello World3</div>,
+          path: `/a/${pluginId}/3`,
         },
       ],
     });
 
-    const usePluginComponents = createUsePluginLinks(registry);
-    const { result } = renderHook(() => usePluginComponents({ extensionPointId }));
+    const usePluginExtensions = createUsePluginLinks(registry);
+    const { result } = renderHook(() => usePluginExtensions({ extensionPointId }));
 
     expect(result.current.links.length).toBe(2);
-
-    act(() => {
-      render(result.current.links.map((Component, index) => <Component key={index} />));
-    });
-    expect(await screen.findByText('Hello World1')).toBeVisible();
-    expect(await screen.findByText('Hello World2')).toBeVisible();
-    expect(await screen.queryByText('Hello World3')).toBeNull();
+    expect(result.current.links[0].title).toBe('1');
+    expect(result.current.links[1].title).toBe('2');
   });
 
   it('should dynamically update the extensions registered for a certain extension point', () => {
-    const extensionPointId = 'plugins/foo/bar/v1';
+    const extensionPointId = 'plugins/foo/bar';
     const pluginId = 'my-app-plugin';
-    const usePluginComponents = createUsePluginLinks(registry);
-    let { result, rerender } = renderHook(() => usePluginComponents({ extensionPointId }));
+    const usePluginExtensions = createUsePluginLinks(registry);
+    let { result, rerender } = renderHook(() => usePluginExtensions({ extensionPointId }));
 
     // No extensions yet
     expect(result.current.links.length).toBe(0);
@@ -92,19 +87,13 @@ describe('usePluginLinks()', () => {
             targets: extensionPointId,
             title: '1',
             description: '1',
-            component: () => <div>Hello World1</div>,
+            path: `/a/${pluginId}/2`,
           },
           {
             targets: extensionPointId,
             title: '2',
             description: '2',
-            component: () => <div>Hello World2</div>,
-          },
-          {
-            targets: 'plugins/another-extension/v1',
-            title: '3',
-            description: '3',
-            component: () => <div>Hello World3</div>,
+            path: `/a/${pluginId}/2`,
           },
         ],
       });
@@ -114,58 +103,16 @@ describe('usePluginLinks()', () => {
     rerender();
 
     expect(result.current.links.length).toBe(2);
+    expect(result.current.links[0].title).toBe('1');
+    expect(result.current.links[1].title).toBe('2');
   });
 
   it('should only render the hook once', () => {
-    const spy = jest.spyOn(registry, 'asObservable');
+    const addedLinksRegistrySpy = jest.spyOn(registry, 'asObservable');
     const extensionPointId = 'plugins/foo/bar';
-    const usePluginComponents = createUsePluginLinks(registry);
+    const usePluginLinks = createUsePluginLinks(registry);
 
-    renderHook(() => usePluginComponents({ extensionPointId }));
-    expect(spy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should honour the limitPerPlugin arg if its set', () => {
-    const extensionPointId = 'plugins/foo/bar/v1';
-    const plugins = ['my-app-plugin1', 'my-app-plugin2', 'my-app-plugin3'];
-    const usePluginComponents = createUsePluginLinks(registry);
-    let { result, rerender } = renderHook(() => usePluginComponents({ extensionPointId, limitPerPlugin: 2 }));
-
-    // No extensions yet
-    expect(result.current.links.length).toBe(0);
-
-    // Add extensions to the registry
-    act(() => {
-      for (let pluginId of plugins) {
-        registry.register({
-          pluginId,
-          configs: [
-            {
-              targets: extensionPointId,
-              title: '1',
-              description: '1',
-              component: () => <div>Hello World1</div>,
-            },
-            {
-              targets: extensionPointId,
-              title: '2',
-              description: '2',
-              component: () => <div>Hello World2</div>,
-            },
-            {
-              targets: extensionPointId,
-              title: '3',
-              description: '3',
-              component: () => <div>Hello World3</div>,
-            },
-          ],
-        });
-      }
-    });
-
-    // Check if the hook returns the new extensions
-    rerender();
-
-    expect(result.current.links.length).toBe(6);
+    renderHook(() => usePluginLinks({ extensionPointId }));
+    expect(addedLinksRegistrySpy).toHaveBeenCalledTimes(1);
   });
 });
