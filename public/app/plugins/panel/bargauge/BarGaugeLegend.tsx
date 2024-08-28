0fd8194a74e363a6e61a79370726ebd90535943f
Adela Almasan <adela.almasan@grafana.com>
2024-08-28 13:47:54 -0500

    loop

--- public/app/plugins/panel/bargauge/BarGaugeLegend.tsx
+++ public/app/plugins/panel/bargauge/BarGaugeLegend.tsx
@@ -2,8 +2,8 @@ import { memo } from 'react';
 
 import { DataFrame, getFieldSeriesColor, outerJoinDataFrames } from '@grafana/data';
 import { Field } from '@grafana/data/';
-import { VizLegendOptions, AxisPlacement } from '@grafana/schema';
-import { VizLayout, VizLayoutLegendProps, VizLegend, VizLegendItem, useTheme2 } from '@grafana/ui';
+import { AxisPlacement, VizLegendOptions } from '@grafana/schema';
+import { useTheme2, VizLayout, VizLayoutLegendProps, VizLegend, VizLegendItem } from '@grafana/ui';
 import { getDisplayValuesForCalcs } from '@grafana/ui/src/components/uPlot/utils';
 
 interface BarGaugeLegendProps extends VizLegendOptions, Omit<VizLayoutLegendProps, 'children'> {
@@ -14,37 +14,31 @@ interface BarGaugeLegendProps extends VizLegendOptions, Omit<VizLayoutLegendProp
 export const BarGaugeLegend = memo(
   ({ data, placement, calcs, displayMode, ...vizLayoutLegendProps }: BarGaugeLegendProps) => {
     const theme = useTheme2();
-
-    const alignedDataFrames = outerJoinDataFrames({ frames: data });
-
     let legendItems: VizLegendItem[] = [];
 
-    if (alignedDataFrames && alignedDataFrames.fields.length > 0) {
-      legendItems = alignedDataFrames.fields
-        .map((field, i) => {
-          const frameIndex = 0;
-          const fieldIndex = i + 1;
+    data.map((series, frameIndex) => {
+      series.fields.map((field, i) => {
+        const fieldIndex = i + 1;
 
-          if (!field || field.type === 'time' || field.config.custom?.hideFrom?.legend) {
-            return undefined;
-          }
+        if (!field || field.type === 'time' || field.config.custom?.hideFrom?.legend) {
+          return undefined;
+        }
 
-          const label = field.state?.displayName ?? field.name;
-          const color = getFieldSeriesColor(field, theme).color;
+        const label = field.state?.displayName ?? field.name;
+        const color = getFieldSeriesColor(field, theme).color;
 
-          const item: VizLegendItem = {
-            label: label.toString(),
-            color,
-            yAxis: field.config.custom?.axisPlacement === AxisPlacement.Right ? 2 : 1,
-            disabled: field.state?.hideFrom?.viz,
-            getDisplayValues: () => getDisplayValuesForCalcs(calcs, field, theme),
-            getItemKey: () => `${label}-${frameIndex}-${fieldIndex}`,
-          };
+        const item: VizLegendItem = {
+          label: label.toString(),
+          color,
+          yAxis: field.config.custom?.axisPlacement === AxisPlacement.Right ? 2 : 1,
+          disabled: field.state?.hideFrom?.viz,
+          getDisplayValues: () => getDisplayValuesForCalcs(calcs, field, theme),
+          getItemKey: () => `${label}-${frameIndex}-${fieldIndex}`,
+        };
 
-          return item;
-        })
-        .filter((i): i is VizLegendItem => i !== undefined);
-    }
+        legendItems.push(item);
+      });
+    });
 
     return (
       <VizLayout.Legend placement={placement} {...vizLayoutLegendProps}>
