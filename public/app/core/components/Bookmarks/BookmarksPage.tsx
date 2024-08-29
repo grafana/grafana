94f3a0b9a77559f0db9ed62d0ebe1efd2424eef0
Joao Silva <joao.silva@grafana.com>
2024-08-29 16:20:38 +0100

    Move part of the message to a description

--- public/app/core/components/Bookmarks/BookmarksPage.tsx
+++ public/app/core/components/Bookmarks/BookmarksPage.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/css';
 import { GrafanaTheme2, NavModelItem } from '@grafana/data';
 import { EmptyState, useStyles2 } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
-import { t } from 'app/core/internationalization';
+import { t, Trans } from 'app/core/internationalization';
 import { useSelector } from 'app/types';
 
 import { usePinnedItems } from '../AppChrome/MegaMenu/hooks';
@@ -29,11 +29,12 @@ export function BookmarksPage() {
         {validItems.length === 0 ? (
           <EmptyState
             variant="call-to-action"
-            message={t(
-              'bookmarks.empty-state.message',
-              'It looks like you haven’t created any bookmarks yet. Hover over any item in the nav menu and click on the bookmark icon to add it here.'
-            )}
-          />
+            message={t('bookmarks-page.empty.message', 'It looks like you haven’t created any bookmarks yet')}
+          >
+            <Trans i18nKey="bookmarks-page.empty.tip">
+              Hover over any item in the nav menu and click on the bookmark icon to add it here.
+            </Trans>
+          </EmptyState>
         ) : (
           <section className={styles.grid}>
             {validItems.map((item) => {
