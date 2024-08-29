9a90785917ff126619575a836d70ffe81db51810
Adela Almasan <adela.almasan@grafana.com>
2024-08-29 18:57:09 -0500

    json stringify

--- packages/grafana-ui/src/components/Table/TableCellInspector.tsx
+++ packages/grafana-ui/src/components/Table/TableCellInspector.tsx
@@ -28,6 +28,7 @@ export function TableCellInspector({ value, onDismiss, mode }: TableCellInspecto
     if (trimmedValue[0] === '{' || trimmedValue[0] === '[' || mode === 'code') {
       try {
         value = JSON.parse(value);
+        displayValue = JSON.stringify(value, null, ' ');
       } catch {}
     }
   } else {
