50651ab605ce9bd46d5312d36aa98f964b07f4a6
Erik Sundell <erik.sundell87@gmail.com>
2024-08-29 09:34:49 +0200

    fix broken hook

--- public/app/features/plugins/extensions/usePluginLinks.tsx
+++ public/app/features/plugins/extensions/usePluginLinks.tsx
@@ -3,7 +3,6 @@ import { useObservable } from 'react-use';
 import { isString } from 'util';
 
 import { PluginExtensionLink, PluginExtensionTypes } from '@grafana/data';
-import { logError } from '@grafana/runtime';
 import {
   UsePluginLinksOptions,
   UsePluginLinksResult,
@@ -28,79 +27,72 @@ export function createUsePluginLinks(registry: AddedLinksRegistry) {
     extensionPointId,
     context,
   }: UsePluginLinksOptions): UsePluginLinksResult {
-    try {
-      const registry = useObservable(observableRegistry);
+    const registry = useObservable(observableRegistry);
 
-      return useMemo(() => {
-        if (!registry || !registry[extensionPointId]) {
-          return {
-            isLoading: false,
-            links: [],
-          };
-        }
-        const frozenContext = context ? getReadOnlyProxy(context) : {};
-        // We don't return the extensions separated by type, because in that case it would be much harder to define a sort-order for them.
-        const extensions: PluginExtensionLink[] = [];
-        const extensionsByPlugin: Record<string, number> = {};
-
-        for (const addedLink of registry[extensionPointId] ?? []) {
-          try {
-            const { pluginId } = addedLink;
-            // Only limit if the `limitPerPlugin` is set
-            if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
-              continue;
-            }
-
-            if (extensionsByPlugin[pluginId] === undefined) {
-              extensionsByPlugin[pluginId] = 0;
-            }
-
-            // Run the configure() function with the current context, and apply the ovverides
-            const overrides = getLinkExtensionOverrides(pluginId, addedLink, frozenContext);
-
-            // configure() returned an `undefined` -> hide the extension
-            if (addedLink.configure && overrides === undefined) {
-              continue;
-            }
-
-            const path = overrides?.path || addedLink.path;
-            const extension: PluginExtensionLink = {
-              id: generateExtensionId(pluginId, {
-                ...addedLink,
-                extensionPointId,
-                type: PluginExtensionTypes.link,
-              }),
-              type: PluginExtensionTypes.link,
-              pluginId: pluginId,
-              onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, frozenContext),
-
-              // Configurable properties
-              icon: overrides?.icon || addedLink.icon,
-              title: overrides?.title || addedLink.title,
-              description: overrides?.description || addedLink.description,
-              path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
-              category: overrides?.category || addedLink.category,
-            };
-
-            extensions.push(extension);
-            extensionsByPlugin[pluginId] += 1;
-          } catch (error) {
-            if (error instanceof Error) {
-              logWarning(error.message);
-            }
-          }
-        }
-
+    return useMemo(() => {
+      if (!registry || !registry[extensionPointId]) {
         return {
           isLoading: false,
-          links: extensions,
+          links: [],
         };
-      }, [context, extensionPointId, limitPerPlugin, registry]);
-    } catch (e) {
-      if (e instanceof Error) {
-        logError(e);
       }
-      console.error(e);
-    }
+      const frozenContext = context ? getReadOnlyProxy(context) : {};
+      // We don't return the extensions separated by type, because in that case it would be much harder to define a sort-order for them.
+      const extensions: PluginExtensionLink[] = [];
+      const extensionsByPlugin: Record<string, number> = {};
+
+      for (const addedLink of registry[extensionPointId] ?? []) {
+        try {
+          const { pluginId } = addedLink;
+          // Only limit if the `limitPerPlugin` is set
+          if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
+            continue;
+          }
+
+          if (extensionsByPlugin[pluginId] === undefined) {
+            extensionsByPlugin[pluginId] = 0;
+          }
+
+          // Run the configure() function with the current context, and apply the ovverides
+          const overrides = getLinkExtensionOverrides(pluginId, addedLink, frozenContext);
+
+          // configure() returned an `undefined` -> hide the extension
+          if (addedLink.configure && overrides === undefined) {
+            continue;
+          }
+
+          const path = overrides?.path || addedLink.path;
+          const extension: PluginExtensionLink = {
+            id: generateExtensionId(pluginId, {
+              ...addedLink,
+              extensionPointId,
+              type: PluginExtensionTypes.link,
+            }),
+            type: PluginExtensionTypes.link,
+            pluginId: pluginId,
+            onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, frozenContext),
+
+            // Configurable properties
+            icon: overrides?.icon || addedLink.icon,
+            title: overrides?.title || addedLink.title,
+            description: overrides?.description || addedLink.description,
+            path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
+            category: overrides?.category || addedLink.category,
+          };
+
+          extensions.push(extension);
+          extensionsByPlugin[pluginId] += 1;
+        } catch (error) {
+          if (error instanceof Error) {
+            logWarning(error.message);
+          }
+        }
+      }
+
+      return {
+        isLoading: false,
+        links: extensions,
+      };
+    }, [context, extensionPointId, limitPerPlugin, registry]);
   };
 }
