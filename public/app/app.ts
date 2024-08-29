8edfdef560452df685aac4539c24ba667cf1f363
Erik Sundell <erik.sundell87@gmail.com>
2024-08-29 16:51:44 +0200

    use the right import ofr isString

--- public/app/app.ts
+++ public/app/app.ts
@@ -41,7 +41,7 @@ import {
   setPluginComponentsHook,
   setCurrentUser,
   setChromeHeaderHeightHook,
-  // setPluginLinksHook,
+  setPluginLinksHook,
 } from '@grafana/runtime';
 import { setPanelDataErrorView } from '@grafana/runtime/src/components/PanelDataErrorView';
 import { setPanelRenderer } from '@grafana/runtime/src/components/PanelRenderer';
@@ -89,7 +89,7 @@ import { setupPluginExtensionRegistries } from './features/plugins/extensions/re
 import { createUsePluginComponent } from './features/plugins/extensions/usePluginComponent';
 import { createUsePluginComponents } from './features/plugins/extensions/usePluginComponents';
 import { createUsePluginExtensions } from './features/plugins/extensions/usePluginExtensions';
-// import { createUsePluginLinks } from './features/plugins/extensions/usePluginLinks';
+import { createUsePluginLinks } from './features/plugins/extensions/usePluginLinks';
 import { importPanelPlugin, syncGetPanelPlugin } from './features/plugins/importPanelPlugin';
 import { preloadPlugins } from './features/plugins/pluginPreloader';
 import { QueryRunner } from './features/query/state/QueryRunner';
@@ -225,7 +225,7 @@ export class GrafanaApp {
         await preloadPlugins(awaitedAppPlugins, pluginExtensionsRegistries, 'frontend_awaited_plugins_preload');
       }
 
-      // setPluginLinksHook(createUsePluginLinks(pluginExtensionsRegistries.addedLinksRegistry));
+      setPluginLinksHook(createUsePluginLinks(pluginExtensionsRegistries.addedLinksRegistry));
       setPluginExtensionGetter(createPluginExtensionsGetter(pluginExtensionsRegistries));
       setPluginExtensionsHook(createUsePluginExtensions(pluginExtensionsRegistries));
       setPluginComponentHook(createUsePluginComponent(pluginExtensionsRegistries.exposedComponentsRegistry));
--- public/app/features/plugins/extensions/usePluginLinks.tsx
+++ public/app/features/plugins/extensions/usePluginLinks.tsx
@@ -1,6 +1,6 @@
+import { isString } from 'lodash';
 import { useMemo } from 'react';
 import { useObservable } from 'react-use';
-import { isString } from 'util';
 
 import { PluginExtensionLink, PluginExtensionTypes } from '@grafana/data';
 import {
@@ -42,51 +42,45 @@ export function createUsePluginLinks(registry: AddedLinksRegistry) {
       const extensionsByPlugin: Record<string, number> = {};
 
       for (const addedLink of registry[extensionPointId] ?? []) {
-        try {
-          const { pluginId } = addedLink;
-          // Only limit if the `limitPerPlugin` is set
-          if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
-            continue;
-          }
-
-          if (extensionsByPlugin[pluginId] === undefined) {
-            extensionsByPlugin[pluginId] = 0;
-          }
-
-          // Run the configure() function with the current context, and apply the ovverides
-          const overrides = getLinkExtensionOverrides(pluginId, addedLink, frozenContext);
-
-          // configure() returned an `undefined` -> hide the extension
-          if (addedLink.configure && overrides === undefined) {
-            continue;
-          }
-
-          const path = overrides?.path || addedLink.path;
-          const extension: PluginExtensionLink = {
-            id: generateExtensionId(pluginId, {
-              ...addedLink,
-              extensionPointId,
-              type: PluginExtensionTypes.link,
-            }),
-            type: PluginExtensionTypes.link,
-            pluginId: pluginId,
-            onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, frozenContext),
-
-            // Configurable properties
-            icon: overrides?.icon || addedLink.icon,
-            title: overrides?.title || addedLink.title,
-            description: overrides?.description || addedLink.description,
-            path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
-            category: overrides?.category || addedLink.category,
-          };
-
-          extensions.push(extension);
-          extensionsByPlugin[pluginId] += 1;
-        } catch (error) {
-          if (error instanceof Error) {
-            logWarning(error.message);
-          }
+        const { pluginId } = addedLink;
+        // Only limit if the `limitPerPlugin` is set
+        if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
+          continue;
         }
+
+        if (extensionsByPlugin[pluginId] === undefined) {
+          extensionsByPlugin[pluginId] = 0;
+        }
+
+        // Run the configure() function with the current context, and apply the ovverides
+        const overrides = getLinkExtensionOverrides(pluginId, addedLink, frozenContext);
+
+        // configure() returned an `undefined` -> hide the extension
+        if (addedLink.configure && overrides === undefined) {
+          continue;
+        }
+
+        const path = overrides?.path || addedLink.path;
+        const extension: PluginExtensionLink = {
+          id: generateExtensionId(pluginId, {
+            ...addedLink,
+            extensionPointId,
+            type: PluginExtensionTypes.link,
+          }),
+          type: PluginExtensionTypes.link,
+          pluginId: pluginId,
+          onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, frozenContext),
+
+          // Configurable properties
+          icon: overrides?.icon || addedLink.icon,
+          title: overrides?.title || addedLink.title,
+          description: overrides?.description || addedLink.description,
+          path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
+          category: overrides?.category || addedLink.category,
+        };
+
+        extensions.push(extension);
+        extensionsByPlugin[pluginId] += 1;
       }
 
       return {
