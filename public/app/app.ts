42defb2e6c0dff13af7450645209f76a57ca6969
Erik Sundell <erik.sundell87@gmail.com>
2024-08-29 09:12:08 +0200

    catch init error

--- public/app/app.ts
+++ public/app/app.ts
@@ -41,7 +41,7 @@ import {
   setPluginComponentsHook,
   setCurrentUser,
   setChromeHeaderHeightHook,
-  // setPluginLinksHook,
+  setPluginLinksHook,
 } from '@grafana/runtime';
 import { setPanelDataErrorView } from '@grafana/runtime/src/components/PanelDataErrorView';
 import { setPanelRenderer } from '@grafana/runtime/src/components/PanelRenderer';
@@ -89,7 +89,7 @@ import { setupPluginExtensionRegistries } from './features/plugins/extensions/re
 import { createUsePluginComponent } from './features/plugins/extensions/usePluginComponent';
 import { createUsePluginComponents } from './features/plugins/extensions/usePluginComponents';
 import { createUsePluginExtensions } from './features/plugins/extensions/usePluginExtensions';
-// import { createUsePluginLinks } from './features/plugins/extensions/usePluginLinks';
+import { createUsePluginLinks } from './features/plugins/extensions/usePluginLinks';
 import { importPanelPlugin, syncGetPanelPlugin } from './features/plugins/importPanelPlugin';
 import { preloadPlugins } from './features/plugins/pluginPreloader';
 import { QueryRunner } from './features/query/state/QueryRunner';
@@ -225,11 +225,15 @@ export class GrafanaApp {
         await preloadPlugins(awaitedAppPlugins, pluginExtensionsRegistries, 'frontend_awaited_plugins_preload');
       }
 
+      try {
+        setPluginLinksHook(createUsePluginLinks(pluginExtensionsRegistries.addedLinksRegistry));
+      } catch (error) {
+        console.error('Failed to set plugin links hook', error);
+      }
       setPluginExtensionGetter(createPluginExtensionsGetter(pluginExtensionsRegistries));
       setPluginExtensionsHook(createUsePluginExtensions(pluginExtensionsRegistries));
       setPluginComponentHook(createUsePluginComponent(pluginExtensionsRegistries.exposedComponentsRegistry));
       setPluginComponentsHook(createUsePluginComponents(pluginExtensionsRegistries.addedComponentsRegistry));
-      // setPluginLinksHook(createUsePluginLinks(pluginExtensionsRegistries.addedLinksRegistry));
 
       // initialize chrome service
       const queryParams = locationService.getSearchObject();
