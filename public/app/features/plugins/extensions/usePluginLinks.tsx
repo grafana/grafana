d5a078916d4cab65003d3828c21d8ec85c858fbb
Erik Sundell <erik.sundell87@gmail.com>
2024-08-29 14:20:50 +0200

    test

--- public/app/features/plugins/extensions/usePluginLinks.tsx
+++ public/app/features/plugins/extensions/usePluginLinks.tsx
@@ -19,61 +19,72 @@ export function createUsePluginLinks(registry: AddedLinksRegistry) {
     extensionPointId,
     context,
   }: UsePluginLinksOptions): UsePluginLinksResult {
-    const registry = useObservable(observableRegistry);
+    try {
+      const registry = useObservable(observableRegistry);
 
-    if (!registry || !registry[extensionPointId]) {
+      if (!registry || !registry[extensionPointId]) {
+        return {
+          isLoading: false,
+          links: [],
+        };
+      }
+      // We don't return the extensions separated by type, because in that case it would be much harder to define a sort-order for them.
+      const extensions: PluginExtensionLink[] = [];
+      const extensionsByPlugin: Record<string, number> = {};
+
+      for (const addedLink of registry[extensionPointId] ?? []) {
+        try {
+          const { pluginId } = addedLink;
+          // Only limit if the `limitPerPlugin` is set
+          if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
+            continue;
+          }
+
+          if (extensionsByPlugin[pluginId] === undefined) {
+            extensionsByPlugin[pluginId] = 0;
+          }
+
+          const path = addedLink.path;
+          const extension: PluginExtensionLink = {
+            id: generateExtensionId(pluginId, {
+              ...addedLink,
+              extensionPointId,
+              type: PluginExtensionTypes.link,
+            }),
+            type: PluginExtensionTypes.link,
+            pluginId: pluginId,
+            onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, context),
+
+            // Configurable properties
+            icon: addedLink.icon,
+            title: addedLink.title,
+            description: addedLink.description,
+            path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
+            category: addedLink.category,
+          };
+
+          extensions.push(extension);
+          extensionsByPlugin[pluginId] += 1;
+        } catch (error) {
+          if (error instanceof Error) {
+            logWarning(error.message);
+          }
+        }
+      }
+
+      return {
+        isLoading: false,
+        links: extensions,
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        logWarning(error.message);
+      }
+      console.log('error in usePluginLinks', error);
       return {
         isLoading: false,
         links: [],
       };
     }
-    // We don't return the extensions separated by type, because in that case it would be much harder to define a sort-order for them.
-    const extensions: PluginExtensionLink[] = [];
-    const extensionsByPlugin: Record<string, number> = {};
-
-    for (const addedLink of registry[extensionPointId] ?? []) {
-      try {
-        const { pluginId } = addedLink;
-        // Only limit if the `limitPerPlugin` is set
-        if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
-          continue;
-        }
-
-        if (extensionsByPlugin[pluginId] === undefined) {
-          extensionsByPlugin[pluginId] = 0;
-        }
-
-        const path = addedLink.path;
-        const extension: PluginExtensionLink = {
-          id: generateExtensionId(pluginId, {
-            ...addedLink,
-            extensionPointId,
-            type: PluginExtensionTypes.link,
-          }),
-          type: PluginExtensionTypes.link,
-          pluginId: pluginId,
-          onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, context),
-
-          // Configurable properties
-          icon: addedLink.icon,
-          title: addedLink.title,
-          description: addedLink.description,
-          path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
-          category: addedLink.category,
-        };
-
-        extensions.push(extension);
-        extensionsByPlugin[pluginId] += 1;
-      } catch (error) {
-        if (error instanceof Error) {
-          logWarning(error.message);
-        }
-      }
-    }
-
-    return {
-      isLoading: false,
-      links: extensions,
-    };
   };
 }
