9dc1a35419355d55be59a0f7d87dbe249dfcb940
lingyufei <1187112693@qq.com>
2024-09-02 16:00:16 +0800

    test: add unit test for FolderUIDFilter when NestedFoldersEnabled

--- pkg/services/sqlstore/searchstore/filters_test.go
+++ pkg/services/sqlstore/searchstore/filters_test.go
@@ -57,3 +57,55 @@ func TestFolderUIDFilter(t *testing.T) {
 		})
 	}
 }
+
+func TestFolderUIDFilterWithNestedFoldersEnabled(t *testing.T) {
+	testCases := []struct {
+		description    string
+		uids           []string
+		expectedSql    string
+		expectedParams []any
+	}{
+		{
+			description:    "searching general folder",
+			uids:           []string{"general"},
+			expectedSql:    "dashboard.folder_uid IS NULL ",
+			expectedParams: []any{},
+		},
+		{
+			description:    "searching a specific folder",
+			uids:           []string{"abc-123"},
+			expectedSql:    "dashboard.org_id = ? AND dashboard.folder_uid = ?",
+			expectedParams: []any{int64(1), "abc-123"},
+		},
+		{
+			description:    "searching a specific folders",
+			uids:           []string{"abc-123", "def-456"},
+			expectedSql:    "dashboard.org_id = ? AND dashboard.folder_uid IN (?,?)",
+			expectedParams: []any{int64(1), "abc-123", "def-456"},
+		},
+		{
+			description:    "searching a specific folders or general",
+			uids:           []string{"general", "abc-123", "def-456"},
+			expectedSql:    "(dashboard.org_id = ? AND dashboard.folder_uid IN (?,?) OR dashboard.folder_uid IS NULL)",
+			expectedParams: []any{int64(1), "abc-123", "def-456"},
+		},
+	}
+
+	store := setupTestEnvironment(t)
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			f := searchstore.FolderUIDFilter{
+				Dialect:              store.GetDialect(),
+				OrgID:                1,
+				UIDs:                 tc.uids,
+				NestedFoldersEnabled: true,
+			}
+
+			sql, params := f.Where()
+
+			assert.Equal(t, tc.expectedSql, sql)
+			assert.Equal(t, tc.expectedParams, params)
+		})
+	}
+}
