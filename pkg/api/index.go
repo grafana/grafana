52282b2975a65ce237ca1fc17677029b2db63d47
UpBlueio <115873593+UpBlueio@users.noreply.github.com>
2024-09-02 15:23:24 +0200

    Update Readme

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -136,7 +136,7 @@ func (hs *HTTPServer) setIndexViewData(c *contextmodel.ReqContext) (*dtos.IndexV
 		AppTitle:                            "Grafana",
 		NavTree:                             navTree,
 		Nonce:                               c.RequestNonce,
-		LoadingLogo:                         "public/img/grafana_icon.svg",
+		LoadingLogo:                         "public/img/img/upblue_icon.svg",
 		IsDevelopmentEnv:                    hs.Cfg.Env == setting.Dev,
 		Assets:                              assets,
 	}
