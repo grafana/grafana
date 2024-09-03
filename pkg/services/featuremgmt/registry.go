d382ea773a3eeef18ac41650073ae083e7faa381
Kyle Brandt <kyle@grafana.com>
2024-09-03 09:56:25 -0400

    Prometheus: Enable the promQLScope (Scopes, Adhoc filters and groupby) by default (#92080)

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1039,8 +1039,9 @@ var (
 		{
 			Name:        "promQLScope",
 			Description: "In-development feature that will allow injection of labels into prometheus queries.",
-			Stage:       FeatureStageExperimental,
+			Stage:       FeatureStageGeneralAvailability,
 			Owner:       grafanaObservabilityMetricsSquad,
+			Expression:  "true",
 		},
 		{
 			Name:         "sqlExpressions",
