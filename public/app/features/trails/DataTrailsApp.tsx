618c97fd09ec89779ae684ce5b59ddd50291cef1
ismail simsek <ismailsimsek09@gmail.com>
2024-09-04 18:34:54 +0200

    clean up

--- public/app/features/trails/DataTrailsApp.tsx
+++ public/app/features/trails/DataTrailsApp.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { PageLayoutType } from '@grafana/data';
