5dbb947289e11501e6b6050929d77d974757f27e
Laura Benz <48948963+L-M-K-B@users.noreply.github.com>
2024-09-06 14:19:39 +0200

    RestoreDashboards: Show root location as 'Dashboards' in folder picker (#92936)
    * refactor: replace label if its value is Root
    
    * refactor: replace label in the backend

--- pkg/services/folder/model.go
+++ pkg/services/folder/model.go
@@ -54,7 +54,7 @@ type Folder struct {
 }
 
 var GeneralFolder = Folder{ID: 0, Title: "General"}
-var RootFolder = &Folder{ID: 0, Title: "Root", UID: GeneralFolderUID, ParentUID: ""}
+var RootFolder = &Folder{ID: 0, Title: "Dashbboards", UID: GeneralFolderUID, ParentUID: ""}
 var SharedWithMeFolder = Folder{
 	Title:       "Shared with me",
 	Description: "Dashboards and folders shared with me",
