bfd419effb81f09e0f9bb6f5b085fdd49c00cbaa
Polidoro-root <54002172+Polidoro-root@users.noreply.github.com>
2024-09-09 09:49:47 -0300

    I18n: Add constants value tests to avoid typos (#93070)

--- public/app/core/internationalization/constants.test.ts
+++ public/app/core/internationalization/constants.test.ts
@@ -1,8 +1,30 @@
 import { uniqBy } from 'lodash';
 
-import { LANGUAGES, VALID_LANGUAGES } from './constants';
+import {
+  BRAZILIAN_PORTUGUESE,
+  CHINESE_SIMPLIFIED,
+  DEFAULT_LANGUAGE,
+  ENGLISH_US,
+  FRENCH_FRANCE,
+  GERMAN_GERMANY,
+  LANGUAGES,
+  PSEUDO_LOCALE,
+  SPANISH_SPAIN,
+  VALID_LANGUAGES,
+} from './constants';
 
 describe('internationalization constants', () => {
+  it('should have set the constants correctly', () => {
+    expect(ENGLISH_US).toBe('en-US');
+    expect(FRENCH_FRANCE).toBe('fr-FR');
+    expect(SPANISH_SPAIN).toBe('es-ES');
+    expect(GERMAN_GERMANY).toBe('de-DE');
+    expect(BRAZILIAN_PORTUGUESE).toBe('pt-BR');
+    expect(CHINESE_SIMPLIFIED).toBe('zh-Hans');
+    expect(PSEUDO_LOCALE).toBe('pseudo-LOCALE');
+    expect(DEFAULT_LANGUAGE).toBe(ENGLISH_US);
+  });
+
   it('should not have duplicate languages codes', () => {
     const uniqLocales = uniqBy(LANGUAGES, (v) => v.code);
     expect(LANGUAGES).toHaveLength(uniqLocales.length);
