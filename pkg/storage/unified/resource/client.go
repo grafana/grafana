a4e5aa7334738ac05fb7d1597f4aa92a130fa56d
Claudiu Dragalina-Paraipan <claudiu.dragalina@grafana.com>
2024-09-09 18:13:33 +0300

    Add authlib gRPC authenticators for in-proc mode

--- pkg/storage/unified/resource/client.go
+++ pkg/storage/unified/resource/client.go
@@ -1,11 +1,17 @@
 package resource
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/fullstorydev/grpchan"
 	"github.com/fullstorydev/grpchan/inprocgrpc"
+	authnlib "github.com/grafana/authlib/authn"
+	"github.com/grafana/authlib/claims"
 	grpcAuth "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/auth"
 	"google.golang.org/grpc"
 
+	"github.com/grafana/grafana/pkg/services/authn/grpcutils"
 	grpcUtils "github.com/grafana/grafana/pkg/storage/unified/resource/grpc"
 )
 
@@ -31,24 +37,38 @@ func NewResourceClient(channel *grpc.ClientConn) ResourceClient {
 	}
 }
 
-func NewLocalResourceClient(server ResourceServer) ResourceClient {
+func NewLocalResourceClient(server ResourceStoreServer) ResourceStoreClient {
+	// scenario: local in-proc
 	channel := &inprocgrpc.Channel{}
 
-	auth := &grpcUtils.Authenticator{}
-
+	grpcAuthInt := grpcutils.NewInProcGrpcAuthenticator()
 	channel.RegisterService(
 		grpchan.InterceptServer(
 			&ResourceStore_ServiceDesc,
-			grpcAuth.UnaryServerInterceptor(auth.Authenticate),
-			grpcAuth.StreamServerInterceptor(auth.Authenticate),
+			grpcAuth.UnaryServerInterceptor(grpcAuthInt.Authenticate),
+			grpcAuth.StreamServerInterceptor(grpcAuthInt.Authenticate),
 		),
 		server, // Implements all the things
 	)
 
-	cc := grpchan.InterceptClientConn(channel, grpcUtils.UnaryClientInterceptor, grpcUtils.StreamClientInterceptor)
-	return &resourceClient{
-		ResourceStoreClient: NewResourceStoreClient(cc),
-		ResourceIndexClient: NewResourceIndexClient(cc),
-		DiagnosticsClient:   NewDiagnosticsClient(cc),
+	clientInt, _ := authnlib.NewGrpcClientInterceptor(
+		&authnlib.GrpcClientConfig{},
+		authnlib.WithDisableAccessTokenOption(),
+		authnlib.WithIDTokenExtractorOption(idTokenExtractor),
+	)
+	return NewResourceStoreClient(grpchan.InterceptClientConn(channel, clientInt.UnaryClientInterceptor, clientInt.StreamClientInterceptor))
+}
+
+func idTokenExtractor(ctx context.Context) (string, error) {
+	authInfo, ok := claims.From(ctx)
+	if !ok {
+		return "", fmt.Errorf("no claims found")
 	}
+
+	extra := authInfo.GetExtra()
+	if token, exists := extra["id-token"]; exists && len(token) != 0 && token[0] != "" {
+		return token[0], nil
+	}
+
+	return "", fmt.Errorf("id-token not found")
 }
