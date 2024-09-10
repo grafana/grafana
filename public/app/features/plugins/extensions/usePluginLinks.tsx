82154f894b2c862ab41bd27823b89a502b206181
Levente Balogh <balogh.levente.hu@gmail.com>
2024-09-10 10:42:07 +0200

    UI Extensions: Share the registries using a React context (#92014)
    * feat: add a context for the extension registries
    
    * feat: add a provider for registries to `AppWrapper`
    
    * feat(extensions): add a read-only registry version
    
    * feat: share the registry for exposed components using the context
    
    * fix: tests
    
    * feat: share the registry for added components using the context
    
    * feat: share the addedLinks registry using react context
    
    * use read-only registry versions

--- public/app/features/plugins/extensions/usePluginLinks.tsx
+++ public/app/features/plugins/extensions/usePluginLinks.tsx
@@ -8,7 +8,7 @@ import {
   UsePluginLinksResult,
 } from '@grafana/runtime/src/services/pluginExtensions/getPluginExtensions';
 
-import { AddedLinksRegistry } from './registry/AddedLinksRegistry';
+import { useAddedLinksRegistry } from './ExtensionRegistriesContext';
 import {
   generateExtensionId,
   getLinkExtensionOnClick,
@@ -18,69 +18,67 @@ import {
 } from './utils';
 
 // Returns an array of component extensions for the given extension point
-export function createUsePluginLinks(registry: AddedLinksRegistry) {
-  const observableRegistry = registry.asObservable();
+export function usePluginLinks({
+  limitPerPlugin,
+  extensionPointId,
+  context,
+}: UsePluginLinksOptions): UsePluginLinksResult {
+  const registry = useAddedLinksRegistry();
+  const registryState = useObservable(registry.asObservable());
 
-  return function usePluginLinks({
-    limitPerPlugin,
-    extensionPointId,
-    context,
-  }: UsePluginLinksOptions): UsePluginLinksResult {
-    const registry = useObservable(observableRegistry);
-
-    return useMemo(() => {
-      if (!registry || !registry[extensionPointId]) {
-        return {
-          isLoading: false,
-          links: [],
-        };
-      }
-      const frozenContext = context ? getReadOnlyProxy(context) : {};
-      const extensions: PluginExtensionLink[] = [];
-      const extensionsByPlugin: Record<string, number> = {};
-
-      for (const addedLink of registry[extensionPointId] ?? []) {
-        const { pluginId } = addedLink;
-        // Only limit if the `limitPerPlugin` is set
-        if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
-          continue;
-        }
-
-        if (extensionsByPlugin[pluginId] === undefined) {
-          extensionsByPlugin[pluginId] = 0;
-        }
-
-        // Run the configure() function with the current context, and apply the ovverides
-        const overrides = getLinkExtensionOverrides(pluginId, addedLink, frozenContext);
-
-        // configure() returned an `undefined` -> hide the extension
-        if (addedLink.configure && overrides === undefined) {
-          continue;
-        }
-
-        const path = overrides?.path || addedLink.path;
-        const extension: PluginExtensionLink = {
-          id: generateExtensionId(pluginId, extensionPointId, addedLink.title),
-          type: PluginExtensionTypes.link,
-          pluginId: pluginId,
-          onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, frozenContext),
-
-          // Configurable properties
-          icon: overrides?.icon || addedLink.icon,
-          title: overrides?.title || addedLink.title,
-          description: overrides?.description || addedLink.description,
-          path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
-          category: overrides?.category || addedLink.category,
-        };
-
-        extensions.push(extension);
-        extensionsByPlugin[pluginId] += 1;
-      }
-
+  return useMemo(() => {
+    if (!registryState || !registryState[extensionPointId]) {
       return {
         isLoading: false,
-        links: extensions,
+        links: [],
       };
-    }, [context, extensionPointId, limitPerPlugin, registry]);
-  };
+    }
+
+    const frozenContext = context ? getReadOnlyProxy(context) : {};
+    const extensions: PluginExtensionLink[] = [];
+    const extensionsByPlugin: Record<string, number> = {};
+
+    for (const addedLink of registryState[extensionPointId] ?? []) {
+      const { pluginId } = addedLink;
+      // Only limit if the `limitPerPlugin` is set
+      if (limitPerPlugin && extensionsByPlugin[pluginId] >= limitPerPlugin) {
+        continue;
+      }
+
+      if (extensionsByPlugin[pluginId] === undefined) {
+        extensionsByPlugin[pluginId] = 0;
+      }
+
+      // Run the configure() function with the current context, and apply the ovverides
+      const overrides = getLinkExtensionOverrides(pluginId, addedLink, frozenContext);
+
+      // configure() returned an `undefined` -> hide the extension
+      if (addedLink.configure && overrides === undefined) {
+        continue;
+      }
+
+      const path = overrides?.path || addedLink.path;
+      const extension: PluginExtensionLink = {
+        id: generateExtensionId(pluginId, extensionPointId, addedLink.title),
+        type: PluginExtensionTypes.link,
+        pluginId: pluginId,
+        onClick: getLinkExtensionOnClick(pluginId, extensionPointId, addedLink, frozenContext),
+
+        // Configurable properties
+        icon: overrides?.icon || addedLink.icon,
+        title: overrides?.title || addedLink.title,
+        description: overrides?.description || addedLink.description,
+        path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
+        category: overrides?.category || addedLink.category,
+      };
+
+      extensions.push(extension);
+      extensionsByPlugin[pluginId] += 1;
+    }
+
+    return {
+      isLoading: false,
+      links: extensions,
+    };
+  }, [context, extensionPointId, limitPerPlugin, registryState]);
 }
