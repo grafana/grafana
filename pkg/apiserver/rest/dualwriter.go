22c63ea3c58ad8af1373b74be2799c1f43102677
Ryan McKinley <ryantxu@gmail.com>
2024-09-10 16:01:14 +0300

    DualWriter: remove mode4 wrapper (#93172)

--- pkg/apiserver/rest/dualwriter.go
+++ pkg/apiserver/rest/dualwriter.go
@@ -111,12 +111,12 @@ func NewDualWriter(
 	storage Storage,
 	reg prometheus.Registerer,
 	resource string,
-) DualWriter {
+) Storage {
 	metrics := &dualWriterMetrics{}
 	metrics.init(reg)
 	switch mode {
-	// It is not possible to initialize a mode 0 dual writer. Mode 0 represents
-	// writing to legacy storage without Unified Storage enabled.
+	case Mode0:
+		return legacy
 	case Mode1:
 		// read and write only from legacy storage
 		return newDualWriterMode1(legacy, storage, metrics, resource)
@@ -127,8 +127,7 @@ func NewDualWriter(
 		// write to both, read from storage only
 		return newDualWriterMode3(legacy, storage, metrics, resource)
 	case Mode4:
-		// read and write only from storage
-		return newDualWriterMode4(legacy, storage, metrics, resource)
+		return storage
 	default:
 		return newDualWriterMode1(legacy, storage, metrics, resource)
 	}
