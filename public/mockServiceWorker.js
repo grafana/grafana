b370c2bf6773b77b59e9da231c632008c7d722bb
Ashley Harrison <ashley.harrison@grafana.com>
2024-09-11 14:06:04 +0100

    Chore: Update mockServiceWorker version (#93223)
    update version

--- public/mockServiceWorker.js
+++ public/mockServiceWorker.js
@@ -8,7 +8,7 @@
  * - Please do NOT serve this file on production.
  */
 
-const PACKAGE_VERSION = '2.3.5'
+const PACKAGE_VERSION = '2.4.5'
 const INTEGRITY_CHECKSUM = '26357c79639bfa20d64c0efca2a87423'
 const IS_MOCKED_RESPONSE = Symbol('isMockedResponse')
 const activeClientIds = new Set()
