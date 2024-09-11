23d23764c654c57234a3019817b18316dee27930
Gilles De Mey <gilles.de.mey@gmail.com>
2024-09-11 13:12:18 +0200

    remove stack

--- public/app/features/alerting/unified/components/rule-list/RuleList.v1.tsx
+++ public/app/features/alerting/unified/components/rule-list/RuleList.v1.tsx
@@ -103,24 +103,22 @@ const RuleList = withErrorBoundary(
       // We don't want to show the Loading... indicator for the whole page.
       // We show separate indicators for Grafana-managed and Cloud rules
       <AlertingPageWrapper navId="alert-list" isLoading={false} actions={hasAlertRulesCreated && <CreateAlertButton />}>
-        <Stack direction="column">
-          <RuleListErrors />
-          <RulesFilter onClear={onFilterCleared} />
-          {hasAlertRulesCreated && (
-            <Stack direction="row" alignItems="center">
-              {view === 'groups' && hasActiveFilters && (
-                <Button
-                  icon={expandAll ? 'angle-double-up' : 'angle-double-down'}
-                  variant="secondary"
-                  onClick={() => setExpandAll(!expandAll)}
-                >
-                  {expandAll ? 'Collapse all' : 'Expand all'}
-                </Button>
-              )}
-              <RuleStats namespaces={filteredNamespaces} />
-            </Stack>
-          )}
-        </Stack>
+        <RuleListErrors />
+        <RulesFilter onClear={onFilterCleared} />
+        {hasAlertRulesCreated && (
+          <Stack direction="row" alignItems="center">
+            {view === 'groups' && hasActiveFilters && (
+              <Button
+                icon={expandAll ? 'angle-double-up' : 'angle-double-down'}
+                variant="secondary"
+                onClick={() => setExpandAll(!expandAll)}
+              >
+                {expandAll ? 'Collapse all' : 'Expand all'}
+              </Button>
+            )}
+            <RuleStats namespaces={filteredNamespaces} />
+          </Stack>
+        )}
         {hasNoAlertRulesCreatedYet && <NoRulesSplash />}
         {hasAlertRulesCreated && <ViewComponent expandAll={expandAll} namespaces={filteredNamespaces} />}
       </AlertingPageWrapper>
