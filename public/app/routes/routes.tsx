416b0d5e02c7789656a43cfc6f35eccbc95b9943
Jack Westbrook <jack.westbrook@gmail.com>
2024-09-12 14:01:08 +0200

    Performance: Code split login page (#92704)
    feat(frontend): lazy load login page

--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -1,7 +1,6 @@
 import { Redirect, RouteComponentProps } from 'react-router-dom';
 
 import { isTruthy } from '@grafana/data';
-import { LoginPage } from 'app/core/components/Login/LoginPage';
 import { NavLandingPage } from 'app/core/components/NavLandingPage/NavLandingPage';
 import { PageNotFound } from 'app/core/components/PageNotFound/PageNotFound';
 import config from 'app/core/config';
@@ -382,7 +381,9 @@ export function getAppRoutes(): RouteDescriptor[] {
     // LOGIN / SIGNUP
     {
       path: '/login',
-      component: LoginPage,
+      component: SafeDynamicImport(
+        () => import(/* webpackChunkName: "LoginPage" */ 'app/core/components/Login/LoginPage')
+      ),
       pageClass: 'login-page',
       chromeless: true,
     },
