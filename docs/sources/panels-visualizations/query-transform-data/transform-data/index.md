6f024960e8502d7d163b0f8b1b1922458aaed90b
Drew Slobodnjak <60050885+drew08t@users.noreply.github.com>
2024-09-12 08:50:23 -0700

    Transformations: Binary operation on all numbers (#92172)
    * Transformations: Binary operation on all numbers
    
    * Handle replaceFields option
    
    * Change left clear out to string
    
    * Handle time field
    
    * Fix filtering
    
    * Update new field names to remove double space
    
    * Add tests
    
    * Add BinaryValue interface and update editor
    
    * Fix initial behavior
    
    * Rollback rendering standards
    
    * Add ctx interpolate
    
    * Fix fixed value variable
    
    * Add function to convert old binary value type
    
    * Update tests for new structures
    
    * Add bullet for all number field option
    
    * baldm0mma/run content build script
    
    * Disable alias control for type matching

--- docs/sources/panels-visualizations/query-transform-data/transform-data/index.md
+++ docs/sources/panels-visualizations/query-transform-data/transform-data/index.md
@@ -188,6 +188,7 @@ Use this transformation to add a new field calculated from two other fields. Eac
 - **Field name** - Select the names of fields you want to use in the calculation for the new field.
 - **Calculation** - If you select **Reduce row** mode, then the **Calculation** field appears. Click in the field to see a list of calculation choices you can use to create the new field. For information about available calculations, refer to [Calculation types][].
 - **Operation** - If you select **Binary operation** or **Unary operation** mode, then the **Operation** fields appear. These fields allow you to apply basic math operations on values in a single row from selected fields. You can also use numerical values for binary operations.
+  - **All number fields** - Set the left side of a **Binary operation** to apply the calculation to all number fields.
 - **As percentile** - If you select **Row index** mode, then the **As percentile** switch appears. This switch allows you to transform the row index as a percentage of the total number of rows.
 - **Alias** - (Optional) Enter the name of your new field. If you leave this blank, then the field will be named to match the calculation.
 - **Replace all fields** - (Optional) Select this option if you want to hide all other fields and display only your calculated field in the visualization.
--- packages/grafana-data/src/transformations/transformers/calculateField.test.ts
+++ packages/grafana-data/src/transformations/transformers/calculateField.test.ts
@@ -7,6 +7,7 @@ import { BinaryOperationID } from '../../utils/binaryOperators';
 import { mockTransformationsRegistry } from '../../utils/tests/mockTransformationsRegistry';
 import { UnaryOperationID } from '../../utils/unaryOperators';
 import { ReducerID } from '../fieldReducer';
+import { FieldMatcherID } from '../matchers/ids';
 import { transformDataFrame } from '../transformDataFrame';
 
 import {
@@ -191,6 +192,72 @@ describe('calculateField transformer w/ timeseries', () => {
         },
         {
           'B + 2': 202,
+          TheTime: 2000,
+        },
+      ]);
+    });
+  });
+
+  it('all numbers + static number', async () => {
+    const cfg = {
+      id: DataTransformerID.calculateField,
+      options: {
+        mode: CalculateFieldMode.BinaryOperation,
+        binary: {
+          left: { matcher: { id: FieldMatcherID.byType, options: FieldType.number } },
+          operator: BinaryOperationID.Add,
+          right: '2',
+        },
+        replaceFields: true,
+      },
+    };
+
+    await expect(transformDataFrame([cfg], [seriesBC])).toEmitValuesWith((received) => {
+      const data = received[0];
+      const filtered = data[0];
+      const rows = new DataFrameView(filtered).toArray();
+      expect(rows).toEqual([
+        {
+          'B + 2': 4,
+          'C + 2': 5,
+          TheTime: 1000,
+        },
+        {
+          'B + 2': 202,
+          'C + 2': 302,
+          TheTime: 2000,
+        },
+      ]);
+    });
+  });
+
+  it('all numbers + field number', async () => {
+    const cfg = {
+      id: DataTransformerID.calculateField,
+      options: {
+        mode: CalculateFieldMode.BinaryOperation,
+        binary: {
+          left: { matcher: { id: FieldMatcherID.byType, options: FieldType.number } },
+          operator: BinaryOperationID.Add,
+          right: 'C',
+        },
+        replaceFields: true,
+      },
+    };
+
+    await expect(transformDataFrame([cfg], [seriesBC])).toEmitValuesWith((received) => {
+      const data = received[0];
+      const filtered = data[0];
+      const rows = new DataFrameView(filtered).toArray();
+      expect(rows).toEqual([
+        {
+          'B + C': 5,
+          'C + C': 6,
+          TheTime: 1000,
+        },
+        {
+          'B + C': 500,
+          'C + C': 600,
           TheTime: 2000,
         },
       ]);
