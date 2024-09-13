6fd4bdff701fa8324252dd13aa5664f87cca6aa5
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-09-13 18:15:29 +0100

    Update worker start signature

--- public/app/index.ts
+++ public/app/index.ts
@@ -23,12 +23,12 @@ import app from './app';
 const prepareInit = async () => {
   if (process.env.frontend_dev_mock_api) {
     return import('test/mock-api/worker').then((workerModule) => {
-      workerModule.default.start({ onUnhandledRequest: 'bypass' });
+      return workerModule.default().start({ onUnhandledRequest: 'bypass' });
     });
   }
   return Promise.resolve();
 };
 
 prepareInit().then(() => {
-  app.init();
+  return app.init();
 });
