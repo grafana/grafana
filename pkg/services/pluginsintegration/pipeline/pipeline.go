2c07280cf9a778ea33eba9adbb7ecc1944ab23d3
Giuseppe Guerra <giuseppe@guerra.in>
2024-09-13 11:19:09 +0200

    Removed unused argument from bootstrap.DefaultConstructFunc

--- pkg/services/pluginsintegration/pipeline/pipeline.go
+++ pkg/services/pluginsintegration/pipeline/pipeline.go
@@ -44,7 +44,7 @@ func ProvideDiscoveryStage(cfg *config.PluginManagementCfg, pf finder.Finder, pr
 
 func ProvideBootstrapStage(cfg *config.PluginManagementCfg, sc plugins.SignatureCalculator, a *assetpath.Service) *bootstrap.Bootstrap {
 	return bootstrap.New(cfg, bootstrap.Opts{
-		ConstructFunc: bootstrap.DefaultConstructFunc(sc, a, cfg.Features),
+		ConstructFunc: bootstrap.DefaultConstructFunc(sc, a),
 		DecorateFuncs: bootstrap.DefaultDecorateFuncs(cfg),
 	})
 }
