4b998ba9ee1362a4118452750d7e8634eac8a493
ismail simsek <ismailsimsek09@gmail.com>
2024-09-16 12:01:45 +0200

    remove wasm support from webpack

--- scripts/webpack/webpack.common.js
+++ scripts/webpack/webpack.common.js
@@ -9,10 +9,6 @@ module.exports = {
     app: './public/app/index.ts',
     swagger: './public/swagger/index.tsx',
   },
-  experiments: {
-    // Required to load WASM modules.
-    asyncWebAssembly: true,
-  },
   output: {
     clean: true,
     path: path.resolve(__dirname, '../../public/build'),
