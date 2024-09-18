773ff3733d4e4146affab857dc2f93550403c107
Aaron Godin <aaron.godin@grafana.com>
2024-09-17 20:01:43 -0500

    Add access check on permissions to show External group sync in nav

--- pkg/services/navtree/navtreeimpl/admin.go
+++ pkg/services/navtree/navtreeimpl/admin.go
@@ -154,7 +154,7 @@ func (s *ServiceImpl) getAdminNode(c *contextmodel.ReqContext) (*navtree.NavLink
 		})
 	}
 
-	if s.license.FeatureEnabled("groupsync") && s.features.IsEnabled(ctx, featuremgmt.FlagGroupAttributeSync) {
+	if s.license.FeatureEnabled("groupsync") && s.features.IsEnabled(ctx, featuremgmt.FlagGroupAttributeSync) && hasAccess(ac.EvalPermission("groupsync.mappings:read", "groupsync.mappings:write")) {
 		accessNodeLinks = append(accessNodeLinks, &navtree.NavLink{
 			Text:     "External group sync",
 			Id:       "groupsync",
