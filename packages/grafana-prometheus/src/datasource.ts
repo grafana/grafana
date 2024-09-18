0d9720041e8f1ecf826ce5c459650aab52840d00
Brad Cowie <brad@faucet.nz>
2024-09-18 14:08:01 +1200

    Prometheus: Fix escaping of double quotation marks for regex variables

--- packages/grafana-prometheus/src/datasource.ts
+++ packages/grafana-prometheus/src/datasource.ts
@@ -1051,5 +1051,10 @@ export function prometheusRegularEscape<T>(value: T) {
 }
 
 export function prometheusSpecialRegexEscape<T>(value: T) {
-  return typeof value === 'string' ? value.replace(/\\/g, '\\\\\\\\').replace(/[$^*{}\[\]\'+?.()|]/g, '\\\\$&') : value;
+  return typeof value === 'string'
+    ? value
+        .replace(/\\/g, '\\\\\\\\')
+        .replace(/"/g, '\\\\\\"')
+        .replace(/[$^*{}\[\]\'+?.()|]/g, '\\\\$&')
+    : value;
 }
