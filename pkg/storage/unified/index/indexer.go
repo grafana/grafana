c9b8fc783ce835a06d658b005ec760827bff7e97
leonorfmartins <leonorfmartins@gmail.com>
2024-09-19 15:46:16 +0200

    List everything, then index

--- pkg/storage/unified/index/indexer.go
+++ pkg/storage/unified/index/indexer.go
@@ -7,6 +7,7 @@ import (
 	"github.com/blevesearch/bleve/v2"
 	"github.com/blevesearch/bleve/v2/mapping"
 	"github.com/grafana/grafana/pkg/storage/unified/resource"
+	"google.golang.org/grpc"
 )
 
 type IndexConfig struct {
@@ -21,14 +22,9 @@ func (i IndexConfig) IndexFromBeginning(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("could not create new bleve index: %w", err)
 	}
-	// list all resources
-	l, err := i.ResourceClient.List(ctx, &resource.ListRequest{})
-	if err != nil {
-		return fmt.Errorf("could not list all resources: %w", err)
-	}
-	if l == nil {
-		return fmt.Errorf("empty list, not indexing")
-	}
+	// for now listing and indexing syncronously, we can improve this later
+	var allResources *resource.ListResponse
+	l, err := i.listAllResources(ctx, allResources)
 	for _, r := range l.Items {
 		err := index.Index(string(r.ResourceVersion), r)
 		if err != nil {
@@ -36,6 +32,21 @@ func (i IndexConfig) IndexFromBeginning(ctx context.Context) error {
 		}
 	}
 	return nil
+}
+
+func (i *IndexConfig) listAllResources(ctx context.Context, allResources *resource.ListResponse) (*resource.ListResponse, error) {
+	l, err := i.ResourceClient.List(ctx, &resource.ListRequest{}, grpc.EmptyCallOption{})
+	if err != nil {
+		return nil, fmt.Errorf("could not list all resources: %w", err)
+	}
+	if l == nil {
+		return nil, fmt.Errorf("empty list")
+	}
+	allResources.Items = append(allResources.Items, l.Items...)
+	if l.NextPageToken != "" {
+		return i.listAllResources(ctx, &resource.ListResponse{NextPageToken: l.NextPageToken, Items: allResources.Items})
+	}
+	return l, nil
 }
 
 func createIndexMapping() *mapping.IndexMappingImpl {
