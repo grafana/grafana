b88252f1b73a4422ec9d9fe0f2960d719238b3ca
Matt Jacobson <matthew.jacobson@grafana.com>
2024-09-23 17:04:16 -0400

    Add some debug logs to ReceiverPermissionsService

--- pkg/services/accesscontrol/ossaccesscontrol/receivers.go
+++ pkg/services/accesscontrol/ossaccesscontrol/receivers.go
@@ -75,6 +75,7 @@ type ReceiverPermissionsService struct {
 
 // SetDefaultPermissions sets the default permissions for a newly created receiver.
 func (r ReceiverPermissionsService) SetDefaultPermissions(ctx context.Context, orgID int64, user identity.Requester, uid string) {
+	r.log.Debug("Setting default permissions for receiver", "receiver_uid", uid)
 	permissions := defaultPermissions()
 	clearCache := false
 	if user != nil && user.IsIdentityType(claims.TypeUser) {
@@ -118,6 +119,7 @@ func copyPermissionUser(orgID int64) identity.Requester {
 // method to be used during receiver renaming that is necessitated by receiver uids being generated from the receiver
 // name.
 func (r ReceiverPermissionsService) CopyPermissions(ctx context.Context, orgID int64, user identity.Requester, oldUID, newUID string) (int, error) {
+	r.log.Debug("Copying permissions from receiver", "old_uid", oldUID, "new_uid", newUID)
 	currentPermissions, err := r.GetPermissions(ctx, copyPermissionUser(orgID), oldUID)
 	if err != nil {
 		return 0, err
