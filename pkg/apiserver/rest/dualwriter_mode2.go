1a758ec1758f8843ab027a9c9487988d150340dd
leonorfmartins <leonorfmartins@gmail.com>
2024-09-24 17:31:27 +0200

    Populate UID in legacy

--- pkg/apiserver/rest/dualwriter_mode2.go
+++ pkg/apiserver/rest/dualwriter_mode2.go
@@ -73,14 +73,14 @@ func (d *DualWriterMode2) Create(ctx context.Context, in runtime.Object, createV
 		return nil, fmt.Errorf("there is an UID and it should not: %v", accIn.GetUID())
 	}
 
-	accLegacy, err := meta.Accessor(createdFromLegacy)
-	if err != nil {
-		return createdFromLegacy, err
-	}
+	// accLegacy, err := meta.Accessor(createdFromLegacy)
+	// if err != nil {
+	// 	return createdFromLegacy, err
+	// }
 
-	if accIn.GetName() == "" {
-		accIn.SetName(accLegacy.GetName())
-	}
+	// if accIn.GetName() == "" {
+	// 	accIn.SetName(accLegacy.GetName())
+	// }
 
 	startStorage := time.Now()
 	createdFromStorage, err := d.Storage.Create(ctx, in, createValidation, options)
