41646057f9b8734131c78c0543302dbcd82a4e02
Levente Balogh <balogh.levente.hu@gmail.com>
2024-09-24 07:34:05 +0200

    feat(usePluginComponents): add meta-info validation

--- public/app/features/plugins/extensions/usePluginComponents.tsx
+++ public/app/features/plugins/extensions/usePluginComponents.tsx
@@ -1,12 +1,14 @@
 import { useMemo } from 'react';
 import { useObservable } from 'react-use';
 
+import { usePluginContext } from '@grafana/data';
 import {
   UsePluginComponentOptions,
   UsePluginComponentsResult,
 } from '@grafana/runtime/src/services/pluginExtensions/getPluginExtensions';
 
 import { useAddedComponentsRegistry } from './ExtensionRegistriesContext';
+import { isExtensionPointIdInvalid, isExtensionPointMetaInfoMissing, isGrafanaDevMode } from './utils';
 
 // Returns an array of component extensions for the given extension point
 export function usePluginComponents<Props extends object = {}>({
@@ -15,10 +17,33 @@ export function usePluginComponents<Props extends object = {}>({
 }: UsePluginComponentOptions): UsePluginComponentsResult<Props> {
   const registry = useAddedComponentsRegistry();
   const registryState = useObservable(registry.asObservable());
+  const pluginContext = usePluginContext();
 
   return useMemo(() => {
+    // For backwards compatibility we don't enable restrictions in production or when the hook is used in core Grafana.
+    const enableRestrictions = isGrafanaDevMode && pluginContext;
     const components: Array<React.ComponentType<Props>> = [];
     const extensionsByPlugin: Record<string, number> = {};
+
+    if (enableRestrictions && isExtensionPointIdInvalid(extensionPointId, pluginContext)) {
+      console.error(
+        `usePluginComponents("${extensionPointId}") - The extension point ID "${extensionPointId}" is invalid.`
+      );
+      return {
+        isLoading: false,
+        components: [],
+      };
+    }
+
+    if (enableRestrictions && isExtensionPointMetaInfoMissing(extensionPointId, pluginContext)) {
+      console.error(
+        `usePluginComponents("${extensionPointId}") - The extension point is missing from the "plugin.json" file.`
+      );
+      return {
+        isLoading: false,
+        components: [],
+      };
+    }
 
     for (const registryItem of registryState?.[extensionPointId] ?? []) {
       const { pluginId } = registryItem;
@@ -40,5 +65,5 @@ export function usePluginComponents<Props extends object = {}>({
       isLoading: false,
       components,
     };
-  }, [extensionPointId, limitPerPlugin, registryState]);
+  }, [extensionPointId, limitPerPlugin, pluginContext, registryState]);
 }
