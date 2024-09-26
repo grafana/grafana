3d856f06074c0c5da8d00138d100859a7f9a0909
suntala <arati.rana@grafana.com>
2024-09-26 07:58:46 +0200

    Fix testutil folder service set up

--- pkg/services/ngalert/testutil/testutil.go
+++ pkg/services/ngalert/testutil/testutil.go
@@ -27,7 +27,9 @@ import (
 
 func SetupFolderService(tb testing.TB, cfg *setting.Cfg, db db.DB, dashboardStore dashboards.Store, folderStore *folderimpl.DashboardFolderStoreImpl, bus *bus.InProcBus, features featuremgmt.FeatureToggles, ac accesscontrol.AccessControl) folder.Service {
 	tb.Helper()
-	return folderimpl.ProvideService(ac, bus, dashboardStore, folderStore, db, features, supportbundlestest.NewFakeBundleService(), nil, tracing.InitializeTracerForTest())
+	folderPermissions := acmock.NewMockedPermissionsService()
+	return folderimpl.ProvideService(ac, bus, dashboardStore, folderStore, db,
+		features, folderPermissions, supportbundlestest.NewFakeBundleService(), nil, tracing.InitializeTracerForTest())
 }
 
 func SetupDashboardService(tb testing.TB, sqlStore db.DB, fs *folderimpl.DashboardFolderStoreImpl, cfg *setting.Cfg) (*dashboardservice.DashboardServiceImpl, dashboards.Store) {
