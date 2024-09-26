f1f085121e94e8e347a19d05f66bc38e62f70b0a
Owen Smallwood <owen.smallwood@grafana.com>
2024-09-25 19:33:53 -0600

    commenting out for now

--- pkg/registry/apis/search/register.go
+++ pkg/registry/apis/search/register.go
@@ -10,7 +10,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	"k8s.io/apiserver/pkg/registry/generic"
-	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	common "k8s.io/kube-openapi/pkg/common"
 	"k8s.io/kube-openapi/pkg/spec3"
@@ -112,13 +111,14 @@ func (b *SearchAPIBuilder) PostProcessOpenAPI(oas *spec3.OpenAPI) (*spec3.OpenAP
 }
 
 func (b *SearchAPIBuilder) UpdateAPIGroupInfo(apiGroupInfo *genericapiserver.APIGroupInfo, scheme *runtime.Scheme, optsGetter generic.RESTOptionsGetter, dualWriteBuilder grafanarest.DualWriteBuilder) error {
-	storage := map[string]rest.Storage{}
-	store, err := newStorage(scheme, optsGetter)
-	if err != nil {
-		return err
-	}
-	storage["search"] = store
-	apiGroupInfo.VersionedResourcesStorageMap["v0alpha1"] = storage
-	apiGroupInfo.PrioritizedVersions = []schema.GroupVersion{b.GetGroupVersion()}
+	//storage := map[string]rest.Storage{}
+	//store, err := newStorage(scheme, optsGetter)
+	//if err != nil {
+	//	return err
+	//}
+	//storage["search"] = store
+	//apiGroupInfo.VersionedResourcesStorageMap["v0alpha1"] = storage
+	//apiGroupInfo.PrioritizedVersions = []schema.GroupVersion{b.GetGroupVersion()}
+
 	return nil
 }
