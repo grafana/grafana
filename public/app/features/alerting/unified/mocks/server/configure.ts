4f172226516f98f2c1c059f114ed608d4f0673be
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-09-26 14:28:57 +0100

    Add method for specifying folder response and update UID for happy path

--- public/app/features/alerting/unified/mocks/server/configure.ts
+++ public/app/features/alerting/unified/mocks/server/configure.ts
@@ -53,6 +53,17 @@ export const setFolderAccessControl = (accessControl: FolderDTO['accessControl']
 };
 
 /**
+ * Makes the mock server respond with different folder response, for just the folder in question
+ */
+export const setFolderResponse = (response: Partial<FolderDTO>) => {
+  const handler = http.get<{ folderUid: string }>(`/api/folders/${response.uid}`, () => {
+    debugger;
+    return HttpResponse.json(response);
+  });
+  server.use(handler);
+};
+
+/**
  * Makes the mock server respond with different Grafana Alertmanager config
  */
 export const setGrafanaAlertmanagerConfig = (config: AlertManagerCortexConfig) => {
