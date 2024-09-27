1a5591f26001e2a3a84b4db1dda3bf93b83a33ec
suntala <arati.rana@grafana.com>
2024-09-27 20:11:32 +0200

    Include folder store in NewFolderNameScopeResolver

--- pkg/services/folder/folderimpl/folder.go
+++ pkg/services/folder/folderimpl/folder.go
@@ -86,7 +86,7 @@ func ProvideService(
 
 	supportBundles.RegisterSupportItemCollector(srv.supportBundleCollector())
 
-	ac.RegisterScopeAttributeResolver(dashboards.NewFolderNameScopeResolver(folderStore, srv))
+	ac.RegisterScopeAttributeResolver(dashboards.NewFolderNameScopeResolver(folderStore, srv, store))
 	ac.RegisterScopeAttributeResolver(dashboards.NewFolderIDScopeResolver(folderStore, srv))
 	ac.RegisterScopeAttributeResolver(dashboards.NewFolderUIDScopeResolver(srv))
 	return srv
