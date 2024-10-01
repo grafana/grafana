0ace5ecd02f451c9cd255c69146745dc75d799a4
Sam Kh. <syerikjan.khusayan@grafana.com>
2024-10-01 13:27:53 -0400

    ref: wrap tracer with tracer provider to satisfy WithTracerProvider

--- pkg/plugins/backendplugin/grpcplugin/client.go
+++ pkg/plugins/backendplugin/grpcplugin/client.go
@@ -6,6 +6,8 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/grpcplugin"
 	goplugin "github.com/hashicorp/go-plugin"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	trace "go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/embedded"
 	"google.golang.org/grpc"
 
 	"github.com/grafana/grafana/pkg/infra/tracing"
@@ -41,6 +43,19 @@ var pluginSet = map[int]goplugin.PluginSet{
 	},
 }
 
+type clientTracerProvider struct {
+	tracer tracing.Tracer
+	embedded.TracerProvider
+}
+
+func (ctp *clientTracerProvider) Tracer(instrumentationName string, opts ...trace.TracerOption) trace.Tracer {
+	return ctp.tracer
+}
+
+func newClientTracerProvider(tracer tracing.Tracer) trace.TracerProvider {
+	return &clientTracerProvider{tracer: tracer}
+}
+
 func newClientConfig(executablePath string, args []string, env []string, skipHostEnvVars bool, logger log.Logger, tracer tracing.Tracer,
 	versionedPlugins map[int]goplugin.PluginSet) *goplugin.ClientConfig {
 	// We can ignore gosec G201 here, since the dynamic part of executablePath comes from the plugin definition
@@ -56,7 +71,7 @@ func newClientConfig(executablePath string, args []string, env []string, skipHos
 		Logger:           logWrapper{Logger: logger},
 		AllowedProtocols: []goplugin.Protocol{goplugin.ProtocolGRPC},
 		GRPCDialOptions: []grpc.DialOption{
-			grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+			grpc.WithStatsHandler(otelgrpc.NewClientHandler(otelgrpc.WithTracerProvider(newClientTracerProvider(tracer)))),
 		},
 	}
 }
