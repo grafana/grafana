30886ad499c0c8bca0ceb75013c25352861ef629
Tim Levett <tim.levett@grafana.com>
2024-10-01 05:31:31 -0500

    Change from Apps to "More Apps" (#93454)
    * Change the label used for additional app links from apps to more apps so it doesn't conflict with applications, which is application observability, not the other bucket
    
    * update to more apps
    
    * more in german is mehr
    
    * fix case, update translations correctly
    
    * revert changes to de
    
    * fix be tests
    
    ---------
    
    Co-authored-by: joshhunt <josh@trtr.co>
    Co-authored-by: Ashley Harrison <ashley.harrison@grafana.com>

--- public/app/core/utils/navBarItem-translations.ts
+++ public/app/core/utils/navBarItem-translations.ts
@@ -140,7 +140,7 @@ export function getNavTitle(navId: string | undefined) {
     case 'frontend':
       return t('nav.frontend.title', 'Frontend');
     case 'apps':
-      return t('nav.apps.title', 'Apps');
+      return t('nav.apps.title', 'More apps');
     case 'alerts-and-incidents':
       return t('nav.alerts-and-incidents.title', 'Alerts & IRM');
     case 'testing-and-synthetics':
