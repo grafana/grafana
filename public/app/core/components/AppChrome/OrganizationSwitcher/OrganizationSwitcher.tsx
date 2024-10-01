9135bce384ef56b8c07e01646305ff25bb3f96c1
Ashley Harrison <ashley.harrison@grafana.com>
2024-10-01 10:55:48 +0100

    SingleTopNav: Move org switcher into Megamenu header (#94053)
    move org switcher into megamenu header

--- public/app/core/components/AppChrome/OrganizationSwitcher/OrganizationSwitcher.tsx
+++ public/app/core/components/AppChrome/OrganizationSwitcher/OrganizationSwitcher.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 
 import { SelectableValue } from '@grafana/data';
-import { locationService } from '@grafana/runtime';
-import { useTheme2 } from '@grafana/ui';
+import { config, locationService } from '@grafana/runtime';
+import { Text, useTheme2 } from '@grafana/ui';
 import { useMediaQueryChange } from 'app/core/hooks/useMediaQueryChange';
 import { contextSrv } from 'app/core/services/context_srv';
 import { getUserOrganizations, setUserOrganization } from 'app/features/org/state/actions';
 import { useDispatch, useSelector, UserOrg } from 'app/types';
+
+import { Branding } from '../../Branding/Branding';
 
 import { OrganizationPicker } from './OrganizationPicker';
 import { OrganizationSelect } from './OrganizationSelect';
@@ -31,17 +33,23 @@ export function OrganizationSwitcher() {
 
   const breakpoint = theme.breakpoints.values.sm;
 
-  const [isSmallScreen, setIsSmallScreen] = useState(!window.matchMedia(`(min-width: ${breakpoint}px)`).matches);
+  const [isSmallScreen, setIsSmallScreen] = useState(
+    !config.featureToggles.singleTopNav && !window.matchMedia(`(min-width: ${breakpoint}px)`).matches
+  );
 
   useMediaQueryChange({
     breakpoint,
     onChange: (e) => {
-      setIsSmallScreen(!e.matches);
+      setIsSmallScreen(!config.featureToggles.singleTopNav && !e.matches);
     },
   });
 
   if (orgs?.length <= 1) {
-    return null;
+    if (config.featureToggles.singleTopNav) {
+      return <Text truncate>{Branding.AppTitle}</Text>;
+    } else {
+      return null;
+    }
   }
 
   const Switcher = isSmallScreen ? OrganizationPicker : OrganizationSelect;
