0a7b94b2ffb47e833732d3f4a0127cf82906a7f2
Ashley Harrison <ashley.harrison@grafana.com>
2024-10-02 14:20:12 +0100

    tweaks for mobile responsiveness

--- public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
+++ public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
@@ -3,7 +3,7 @@ import { cloneDeep } from 'lodash';
 import { memo } from 'react';
 
 import { GrafanaTheme2, NavModelItem } from '@grafana/data';
-import { Dropdown, Icon, ToolbarButton, useStyles2 } from '@grafana/ui';
+import { Dropdown, Icon, Stack, ToolbarButton, useStyles2 } from '@grafana/ui';
 import { config } from 'app/core/config';
 import { useGrafana } from 'app/core/context/GrafanaContext';
 import { contextSrv } from 'app/core/core';
@@ -22,7 +22,6 @@ import { TOP_BAR_LEVEL_HEIGHT } from '../types';
 import { SignInLink } from './SignInLink';
 import { TopNavBarMenu } from './TopNavBarMenu';
 import { TopSearchBarCommandPaletteTrigger } from './TopSearchBarCommandPaletteTrigger';
-import { TopSearchBarSection } from './TopSearchBarSection';
 
 interface Props {
   sectionNav: NavModelItem;
@@ -51,7 +50,7 @@ export const SingleTopBar = memo(function SingleTopBar({
 
   return (
     <div className={styles.layout}>
-      <TopSearchBarSection>
+      <Stack minWidth={0} gap={0.5} alignItems="center">
         {!menuDockedAndOpen && (
           <ToolbarButton narrow onClick={onToggleMegaMenu}>
             <Branding.MenuLogo className={styles.img} />
@@ -59,9 +58,9 @@ export const SingleTopBar = memo(function SingleTopBar({
         )}
         <Breadcrumbs breadcrumbs={breadcrumbs} className={styles.breadcrumbsWrapper} />
         <ScopesSelector />
-      </TopSearchBarSection>
+      </Stack>
 
-      <TopSearchBarSection align="right">
+      <Stack gap={0.5} alignItems="center">
         <TopSearchBarCommandPaletteTrigger />
         <QuickAdd />
         {enrichedHelpNode && (
@@ -84,7 +83,7 @@ export const SingleTopBar = memo(function SingleTopBar({
         <ToolbarButton className={styles.kioskToggle} onClick={onToggleKioskMode} narrow aria-label="Enable kiosk mode">
           <Icon name="angle-up" size="xl" />
         </ToolbarButton>
-      </TopSearchBarSection>
+      </Stack>
     </div>
   );
 });
@@ -93,15 +92,15 @@ const getStyles = (theme: GrafanaTheme2, menuDockedAndOpen: boolean) => ({
   layout: css({
     height: TOP_BAR_LEVEL_HEIGHT,
     display: 'flex',
-    gap: theme.spacing(1),
+    gap: theme.spacing(2),
     alignItems: 'center',
     padding: theme.spacing(0, 1),
     paddingLeft: menuDockedAndOpen ? theme.spacing(3.5) : theme.spacing(0.75),
     borderBottom: `1px solid ${theme.colors.border.weak}`,
     justifyContent: 'space-between',
 
-    [theme.breakpoints.up('sm')]: {
-      gridTemplateColumns: '2fr minmax(240px, 1fr)', // TODO probably change these values
+    [theme.breakpoints.up('lg')]: {
+      gridTemplateColumns: '2fr minmax(440px, 1fr)',
       display: 'grid',
 
       justifyContent: 'flex-start',
@@ -111,7 +110,7 @@ const getStyles = (theme: GrafanaTheme2, menuDockedAndOpen: boolean) => ({
     display: 'flex',
     overflow: 'hidden',
     [theme.breakpoints.down('sm')]: {
-      minWidth: '50%',
+      minWidth: '40%',
     },
   }),
   img: css({
