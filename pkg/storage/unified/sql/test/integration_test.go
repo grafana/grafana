87805dc4d87e59a4ea5cd7e6642f28be56f427ac
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-10-03 12:22:47 -0300

    Reuse MySQL and Postgres Grafana config instead of the object
    - Only reuse the Grafana DB object for SQLite. Support for SQLite will be added in a different PR
    - Fail when reusing the Grafana DB object if it is using DB instrumentation
    - In the case that we have to reuse a Grafana DB with its instrumentation, fail with an error that describes a workaround
    - Add regression tests to reproduce incident 2144

--- pkg/storage/unified/sql/test/integration_test.go
+++ pkg/storage/unified/sql/test/integration_test.go
@@ -33,7 +33,7 @@ func newServer(t *testing.T) (sql.Backend, resource.ResourceServer) {
 	cfg := setting.NewCfg()
 	features := featuremgmt.WithFeatures()
 
-	eDB, err := dbimpl.ProvideResourceDB(dbstore, cfg, features, nil)
+	eDB, err := dbimpl.ProvideResourceDB(dbstore, cfg, nil)
 	require.NoError(t, err)
 	require.NotNil(t, eDB)
 
