712ed9ac068c60f1f0fe4f9f9bff7f2d236002e1
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2024-10-04 15:41:23 +0100

    remove an empty line, try to make linting pass

--- pkg/services/sqlstore/migrations/migrations.go
+++ pkg/services/sqlstore/migrations/migrations.go
@@ -129,7 +129,6 @@ func (oss *OSSMigrations) AddMigration(mg *Migrator) {
 	enableTraceQLStreaming(mg, oss.features != nil && oss.features.IsEnabledGlobally(featuremgmt.FlagTraceQLStreaming))
 
 	ualert.AddReceiverActionScopesMigration(mg)
-
 	accesscontrol.AddActionSetPermissionsMigrator(mg)
 }
 
