ce8d8e6e5424523ffe6bc1b46a35e7c0de6ea235
dana-axinte <dana.axinte@grafana.com>
2024-10-07 18:42:28 +0100

    rephrased with suggestions from Daniel

--- pkg/services/cloudmigration/api/errors.go
+++ pkg/services/cloudmigration/api/errors.go
@@ -10,10 +10,10 @@ var (
 // Human-friendly error messages that are returned by cloud migration API backend and are displayed in the on-prem instance.
 // FIXME: Ideally we should be dealing with error codes here as well, and let the frontend generate the copies.
 var (
-	ErrorMessageInvalidToken           = "Token is not valid. Regenerate the token and try again."
-	ErrorMessageTokenValidationRequest = "An error occured while validating the token. Please ensure the token is valid."
-	ErrorMessageInstanceNotFound       = "The stack cannot be found. Make sure the stack is running and try again."
-	ErrorMessageInstanceUnreachable    = "The stack cannot be reached. Make sure the stack is running and try again."
-	ErrorMessageInstanceNotReached     = "An error occurred while attempting to verify the stack's connectivity. Please check the network settings or stack status."
-	ErrorMessageCreateSessionFailed    = "There was an error creating the migration." // TODO: what action?
+	ErrorMessageInvalidToken           = "Token is not valid. Generate a new token on your cloud instance and try again."
+	ErrorMessageTokenValidationRequest = "An error occurred while validating the token. Please ensure the token matches the migration token on your cloud instance."
+	ErrorMessageInstanceNotFound       = "The cloud instance cannot be found. Please ensure the cloud instance exists and is active."
+	ErrorMessageInstanceUnreachable    = "The cloud instance cannot be reached. Make sure the instance is running and try again."
+	ErrorMessageInstanceNotReached     = "An error occurred while attempting to verify the cloud instance's connectivity. Please check the network settings or cloud instance status."
+	ErrorMessageCreateSessionFailed    = "There was an error creating the migration. Please try again."
 )
