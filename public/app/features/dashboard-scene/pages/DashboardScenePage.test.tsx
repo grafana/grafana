2a3466d46d7ead04297a07d15494f5338105a56f
Ashley Harrison <ashley.harrison@grafana.com>
2024-10-07 11:46:24 +0100

    fix unit tests

--- public/app/features/dashboard-scene/pages/DashboardScenePage.test.tsx
+++ public/app/features/dashboard-scene/pages/DashboardScenePage.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { cloneDeep } from 'lodash';
 import { TestProvider } from 'test/helpers/TestProvider';
@@ -196,7 +196,7 @@ describe('DashboardScenePage', () => {
 
     const inspectMenuItem = await screen.findAllByText('Inspect');
 
-    act(() => fireEvent.click(inspectMenuItem[0]));
+    await userEvent.click(inspectMenuItem[0]);
 
     expect(await screen.findByText('Inspect: Panel B')).toBeInTheDocument();
 
