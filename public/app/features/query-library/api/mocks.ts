c913415ab960859a9d7ef4772a18c8e1a7113dbc
Haris Rozajac <58232930+harisrozajac@users.noreply.github.com>
2024-10-08 07:12:33 -0600

    Query Library: Enhance user data (#94231)
    * Enhance user data
    
    * Remove irrrelevant logic outside of try block
    
    * Encode user uid for url

--- public/app/features/query-library/api/mocks.ts
+++ public/app/features/query-library/api/mocks.ts
@@ -1,9 +1,13 @@
 import { BASE_URL } from './query';
+import { getIdentityDisplayList } from './testdata/identityDisplayList';
 import { getTestQueryList } from './testdata/testQueryList';
 
 export const mockData = {
   all: {
     url: BASE_URL,
     response: getTestQueryList(),
+  },
+  identityDisplay: {
+    response: getIdentityDisplayList(),
   },
 };
