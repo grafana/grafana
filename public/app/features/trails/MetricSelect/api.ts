c232417e648e58d6584bafd2bfa394955af8287d
Bogdan Matei <bogdan.matei@grafana.com>
2024-10-08 16:43:14 +0300

    Scopes: Use scopes in metrics explore

--- public/app/features/trails/MetricSelect/api.ts
+++ public/app/features/trails/MetricSelect/api.ts
@@ -1,30 +1,24 @@
-import { RawTimeRange } from '@grafana/data';
-import { getPrometheusTime } from '@grafana/prometheus/src/language_utils';
-import { getBackendSrv } from '@grafana/runtime';
+import { AdHocVariableFilter, RawTimeRange, Scope } from '@grafana/data';
+import { PromResponse } from 'app/types/unified-alerting-dto';
 
-type MetricValuesResponse = {
-  data: string[];
-  status: 'success' | 'error';
-  error?: 'string';
-  warnings?: string[];
-};
+import { callSuggestionsApi } from '../utils';
 
-const LIMIT_REACHED = 'results truncated due to limit';
+export async function getMetricNames(
+  dataSourceUid: string,
+  timeRange: RawTimeRange,
+  scopes: Scope[],
+  filters: AdHocVariableFilter[],
+  limit?: number
+): Promise<PromResponse<string[]>> {
+  const response = await callSuggestionsApi(
+    dataSourceUid,
+    timeRange,
+    scopes,
+    filters,
+    '__name__',
+    limit,
+    'explore-metrics-names'
+  );
 
-export async function getMetricNames(dataSourceUid: string, timeRange: RawTimeRange, filters: string, limit?: number) {
-  const url = `/api/datasources/uid/${dataSourceUid}/resources/api/v1/label/__name__/values`;
-  const params: Record<string, string | number> = {
-    start: getPrometheusTime(timeRange.from, false),
-    end: getPrometheusTime(timeRange.to, true),
-    ...(filters && filters !== '{}' ? { 'match[]': filters } : {}),
-    ...(limit ? { limit } : {}),
-  };
-
-  const response = await getBackendSrv().get<MetricValuesResponse>(url, params, 'explore-metrics-names');
-
-  if (limit && response.warnings?.includes(LIMIT_REACHED)) {
-    return { ...response, limitReached: true };
-  }
-
-  return { ...response, limitReached: false };
+  return response.data;
 }
