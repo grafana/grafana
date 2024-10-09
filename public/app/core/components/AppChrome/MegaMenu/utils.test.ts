74b7db9b9322476b876f58ef904b9693dc91d270
Joao Silva <joao.silva@grafana.com>
2024-10-09 14:41:14 +0100

    Navigation: Fix wrong active item shown

--- public/app/core/components/AppChrome/MegaMenu/utils.test.ts
+++ public/app/core/components/AppChrome/MegaMenu/utils.test.ts
@@ -6,6 +6,18 @@ import { enrichHelpItem, getActiveItem, findByUrl } from './utils';
 const starredDashboardUid = 'foo';
 const mockNavTree: NavModelItem[] = [
   {
+    text: 'Bookmarks',
+    url: '/bookmarks',
+    id: 'bookmarks',
+    children: [
+      {
+        text: 'Item with children',
+        url: '/itemWithChildren',
+        id: 'item-with-children',
+      },
+    ],
+  },
+  {
     text: 'Item',
     url: '/item',
     id: 'item',
@@ -112,6 +124,10 @@ describe('getActiveItem', () => {
     const mockPage: NavModelItem = {
       text: 'Some child page',
       id: 'child',
+      parentItem: {
+        text: 'Item with children',
+        id: 'item-with-children',
+      },
     };
     expect(getActiveItem(mockNavTree, mockPage)?.id).toEqual('child');
   });
