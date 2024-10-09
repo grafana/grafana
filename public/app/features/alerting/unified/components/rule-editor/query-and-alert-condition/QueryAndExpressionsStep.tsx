cd033497160c4bdc3b180bac1d25bb59c6716888
Ilya Nuevo <ilya.novojilov@logz.io>
2024-10-09 10:17:40 +0300

    show not the type switch (#49)

--- public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/QueryAndExpressionsStep.tsx
+++ public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/QueryAndExpressionsStep.tsx
@@ -39,7 +39,8 @@ import { RuleEditorSection } from '../RuleEditorSection';
 import { errorFromCurrentCondition, errorFromPreviewData, findRenamedDataQueryReferences, refIdExists } from '../util';
 
 import { CloudDataSourceSelector } from './CloudDataSourceSelector';
-import { SmartAlertTypeDetector } from './SmartAlertTypeDetector';
+// LOGZ.IO CHANGE :: DEV-46521 remove rule type switch
+// import { SmartAlertTypeDetector } from './SmartAlertTypeDetector';
 import { DESCRIPTIONS } from './descriptions';
 import {
   addExpressions,
@@ -431,12 +432,14 @@ export const QueryAndExpressionsStep = ({ editingExistingRule, onDataChange }: P
               }}
             />
           </Field>
+          {/* 
+          // LOGZ.IO CHANGE :: DEV-46521 remove rule type switch
           <SmartAlertTypeDetector
             editingExistingRule={editingExistingRule}
             queries={queries}
             rulesSourcesWithRuler={rulesSourcesWithRuler}
             onClickSwitch={onClickSwitch}
-          />
+          /> */}
         </Stack>
       )}
 
@@ -468,12 +471,14 @@ export const QueryAndExpressionsStep = ({ editingExistingRule, onDataChange }: P
               Add query
             </Button>
           </Tooltip>
+          {/* 
+          // LOGZ.IO CHANGE :: DEV-46521 remove rule type switch
           <SmartAlertTypeDetector
             editingExistingRule={editingExistingRule}
             rulesSourcesWithRuler={rulesSourcesWithRuler}
             queries={queries}
             onClickSwitch={onClickSwitch}
-          />
+          /> */}
           {/* Expression Queries */}
           <Stack direction="column" gap={0}>
             <Text element="h5">Expressions</Text>
