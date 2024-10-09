a676c341f324f1f6691de771cb5d115073243809
Scott Lepper <scott.lepper@gmail.com>
2024-10-09 12:02:52 -0400

    [unified search] fix: remove unified searcher (#94492)

--- public/app/features/search/service/searcher.ts
+++ public/app/features/search/service/searcher.ts
@@ -4,7 +4,6 @@ import { BlugeSearcher } from './bluge';
 import { FrontendSearcher } from './frontend';
 import { SQLSearcher } from './sql';
 import { GrafanaSearcher } from './types';
-import { UnifiedSearcher } from './unified';
 
 let searcher: GrafanaSearcher | undefined = undefined;
 
@@ -13,11 +12,6 @@ export function getGrafanaSearcher(): GrafanaSearcher {
     const sqlSearcher = new SQLSearcher();
     const useBluge = config.featureToggles.panelTitleSearch;
     searcher = useBluge ? new BlugeSearcher(sqlSearcher) : sqlSearcher;
-
-    const useUnified = config.featureToggles.unifiedStorageSearch;
-    if (useUnified) {
-      searcher = new UnifiedSearcher(sqlSearcher);
-    }
 
     if (useBluge && location.search.includes('do-frontend-query')) {
       searcher = new FrontendSearcher(searcher);
