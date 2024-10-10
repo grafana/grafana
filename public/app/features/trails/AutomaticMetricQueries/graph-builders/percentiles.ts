cb39d4f829678c3c9695967fd2ce50b4226fadd2
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-10-10 11:49:24 -0500

    Explore metrics: set options directly because of scenes error when options not set (#94284)
    * scenes error when options not set
    
    * add all of the options that are updated in onOptionsChange
    
    * add options to all the other places we are building timeseries panels
    
    * Update public/app/features/trails/AutomaticMetricQueries/graph-builders/percentiles.ts
    
    Co-authored-by: Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
    
    * remove activation handlers and unused code
    
    * Update public/app/features/trails/AutomaticMetricQueries/graph-builders/simple.ts
    
    Co-authored-by: Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
    
    * Update public/app/features/trails/AutomaticMetricQueries/graph-builders/percentiles.ts
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>
    
    ---------
    
    Co-authored-by: Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>

--- public/app/features/trails/AutomaticMetricQueries/graph-builders/percentiles.ts
+++ public/app/features/trails/AutomaticMetricQueries/graph-builders/percentiles.ts
@@ -1,10 +1,14 @@
 import { PanelBuilders } from '@grafana/scenes';
+import { SortOrder } from '@grafana/schema';
+import { TooltipDisplayMode } from '@grafana/ui';
 
 import { CommonVizParams } from './types';
 
 export function percentilesGraphBuilder({ title, unit }: CommonVizParams) {
-  return PanelBuilders.timeseries() //
+  return PanelBuilders.timeseries()
     .setTitle(title)
     .setUnit(unit)
-    .setCustomFieldConfig('fillOpacity', 9);
+    .setCustomFieldConfig('fillOpacity', 9)
+    .setOption('tooltip', { mode: TooltipDisplayMode.Multi, sort: SortOrder.Descending })
+    .setOption('legend', { showLegend: false });
 }
