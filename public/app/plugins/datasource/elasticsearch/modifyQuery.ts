cf09efda874124b2ed5a652b2df82ca706d6bded
Isabella Siu <isabella.siu@grafana.com>
2024-10-10 16:37:59 -0400

    Elasticsearch: Fix adhoc filters with forward slash

--- public/app/plugins/datasource/elasticsearch/modifyQuery.ts
+++ public/app/plugins/datasource/elasticsearch/modifyQuery.ts
@@ -188,6 +188,7 @@ export function escapeFilter(value: string) {
  */
 export function escapeFilterValue(value: string) {
   value = value.replace(/\\/g, '\\\\');
+  value = value.replace(/\//g, '\\/');
   return lucene.phrase.escape(value);
 }
 
