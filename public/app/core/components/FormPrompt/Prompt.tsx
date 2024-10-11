062a533db33d5ff759f69115ed976dad1383a2db
Clarity-89 <homes89@ukr.net>
2024-10-11 08:48:45 +0300

    Simplify component

--- public/app/core/components/FormPrompt/Prompt.tsx
+++ public/app/core/components/FormPrompt/Prompt.tsx
@@ -4,11 +4,11 @@ import { useEffect } from 'react';
 import { locationService } from '@grafana/runtime';
 
 interface PromptProps {
-  when: boolean;
+  when?: boolean;
   message: string | ((location: H.Location) => string | boolean);
 }
 
-export const Prompt = ({ when, message }: PromptProps) => {
+export const Prompt = ({ message, when = true }: PromptProps) => {
   const currentLocation = locationService.getLocation();
   const history = locationService.getHistory();
 
@@ -17,13 +17,7 @@ export const Prompt = ({ when, message }: PromptProps) => {
       return undefined;
     }
     //@ts-expect-error TODO Update the history package to fix types
-    const unblock = history.block((nextLocation) => {
-      if (nextLocation.pathname !== currentLocation.pathname) {
-        return typeof message === 'function' ? message(nextLocation) : message;
-      } else {
-        return false;
-      }
-    });
+    const unblock = history.block(message);
 
     return () => {
       unblock();
