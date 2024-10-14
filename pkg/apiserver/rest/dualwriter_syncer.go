8421c7e873a806018452d63f061480e941dd9527
maicon <maiconscosta@gmail.com>
2024-10-14 16:02:35 -0300

    Update pkg/apiserver/rest/dualwriter_syncer.go
    Co-authored-by: Diego Augusto Molina <diegoaugustomolina@gmail.com>

--- pkg/apiserver/rest/dualwriter_syncer.go
+++ pkg/apiserver/rest/dualwriter_syncer.go
@@ -175,7 +175,7 @@ func legacyToUnifiedStorageDataSyncer(ctx context.Context, mode DualWriterMode, 
 			// - existing in both legacy and storage, but objects are different, or
 			// - if it's missing from storage
 			if item.objLegacy != nil &&
-				((item.objStorage != nil && !Compare(item.objLegacy, item.objStorage)) || (item.objStorage == nil)) {
+				(item.objStorage == nil || !Compare(item.objLegacy, item.objStorage)) {
 				outOfSync++
 
 				accessor, err := utils.MetaAccessor(item.objLegacy)
