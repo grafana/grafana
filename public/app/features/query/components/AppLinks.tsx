d022f857ecdc7283818fc8965e530d8dadbefa63
Piotr Jamróz <pm.jamroz@gmail.com>
2024-10-14 12:31:34 +0200

    Change App Links styles
    - show logo
    - change copy
    - change style to be more subtle

--- public/app/features/query/components/AppLinks.tsx
+++ public/app/features/query/components/AppLinks.tsx
@@ -1,18 +1,33 @@
 import { css } from '@emotion/css';
 import { first } from 'lodash';
+import { useEffect, useState } from 'react';
 
-import { DataQuery, PluginExtensionPoints } from '@grafana/data';
+import { DataQuery, PluginExtensionPoints, PluginMeta } from '@grafana/data';
+import { GrafanaTheme2 } from '@grafana/data/';
 import { QueryToAppPluginContext } from '@grafana/data/src/types/pluginExtensions';
 import { usePluginLinks } from '@grafana/runtime';
-import { Badge } from '@grafana/ui';
+import { Link } from '@grafana/ui';
+import { useStyles2, Tooltip } from '@grafana/ui/';
+
+import { getPluginMeta } from '../../plugins/admin/api';
 
 type Props = {
   query: DataQuery;
 };
 
-const moreButtonStyle = css({
-  cursor: 'pointer',
-});
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    goQueryLessLink: css({
+      marginRight: '3px',
+      fontSize: theme.typography.bodySmall.fontSize,
+    }),
+    goQueryLessIcon: css({
+      marginLeft: '5px',
+      marginTop: '-5px',
+      width: '11px',
+    }),
+  };
+}
 
 export const AppLinks = ({ query }: Props) => {
   const context: QueryToAppPluginContext = {
@@ -20,22 +35,31 @@ export const AppLinks = ({ query }: Props) => {
     datasource: query.datasource,
   };
 
+  const [plugin, setPlugin] = useState<PluginMeta | undefined>(undefined);
+
   const links = usePluginLinks({
     extensionPointId: PluginExtensionPoints.QueryToAppPlugin,
     context,
   });
 
-  if (links.links.length === 0) {
-    return undefined;
-  }
+  const styles = useStyles2(getStyles);
 
   const firstLink = first(links.links)!;
 
+  useEffect(() => {
+    firstLink?.pluginId && getPluginMeta(firstLink.pluginId).then(setPlugin);
+  }, [firstLink?.pluginId]);
+
+  if (links.links.length === 0 || !plugin) {
+    return undefined;
+  }
+
   return (
-    <>
-      <a href={firstLink.path} target="_blank" rel="noreferrer">
-        <Badge className={moreButtonStyle} text={firstLink.title} color="orange" icon="rocket" />
-      </a>
-    </>
+    <Tooltip content={firstLink.description}>
+      <Link className={styles.goQueryLessLink} href={firstLink.path} target="_blank">
+        Go Queryless
+        <img className={styles.goQueryLessIcon} alt={firstLink.title} src={plugin.info.logos.small} />
+      </Link>
+    </Tooltip>
   );
 };
