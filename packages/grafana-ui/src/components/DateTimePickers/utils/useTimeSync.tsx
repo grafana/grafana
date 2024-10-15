0ab58381a81900e03d440cfd8b4bef67a027ff44
Andrej Ocenas <mr.ocenas@gmail.com>
2024-10-15 12:57:51 +0200

    Change import syntax

--- packages/grafana-ui/src/components/DateTimePickers/utils/useTimeSync.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/utils/useTimeSync.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect } from 'react';
-import usePrevious from 'react-use/lib/usePrevious';
+import { usePrevious } from 'react-use';
 
 import { TimeRange } from '@grafana/data';
 
