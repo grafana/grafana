47115c714adfcf07c1898021581d5f4a817f9c45
Matheus Macabu <macabu@users.noreply.github.com>
2024-10-15 16:27:28 +0200

    CloudMigrations: create snapshot for Contact Points (#94719)
    * CloudMigrations: create snapshot for Contact Points
    
    * CloudMigrations: add contact point copies and components in frontend
    
    * CloudMigrations: temporarily use bell for all alerts resources

--- public/app/features/migrate-to-cloud/onprem/NameCell.tsx
+++ public/app/features/migrate-to-cloud/onprem/NameCell.tsx
@@ -220,9 +220,11 @@ function ResourceIcon({ resource }: { resource: ResourceTableItem }) {
     case 'LIBRARY_ELEMENT':
       return <Icon size="xl" name="library-panel" />;
     case 'MUTE_TIMING':
-      return <Icon size="xl" name="bell-slash" />;
+      return <Icon size="xl" name="bell" />;
     case 'NOTIFICATION_TEMPLATE':
-      return <Icon size="xl" name="gf-layout-simple" />;
+      return <Icon size="xl" name="bell" />;
+    case 'CONTACT_POINT':
+      return <Icon size="xl" name="bell" />;
     default:
       return undefined;
   }
