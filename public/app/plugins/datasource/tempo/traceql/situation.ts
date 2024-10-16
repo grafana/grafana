667f3f2220dc16138bd160548c07f9725f0fc15f
Piotr Jamróz <pm.jamroz@gmail.com>
2024-10-16 16:59:56 +0200

    Tempo: Update error detection shift in error lookup
    Since lezer 1.3.8 the position seems to be shifted + 1

--- public/app/plugins/datasource/tempo/traceql/situation.ts
+++ public/app/plugins/datasource/tempo/traceql/situation.ts
@@ -168,8 +168,8 @@ export function getSituation(text: string, offset: number): Situation | null {
   // so first we check if there is an error node at the cursor position
   let maybeErrorNode = getErrorNode(tree, shiftedOffset);
   if (!maybeErrorNode) {
-    // try again with the previous character
-    maybeErrorNode = getErrorNode(tree, shiftedOffset - 1);
+    // try again with the next character
+    maybeErrorNode = getErrorNode(tree, shiftedOffset + 1);
   }
 
   const cur = maybeErrorNode != null ? maybeErrorNode.cursor() : tree.cursorAt(shiftedOffset);
