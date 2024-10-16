956cb93a66241ac10e0c0706bfd7b8ef5058a46d
suntala <arati.rana@grafana.com>
2024-10-16 09:17:38 +0200

    Use user UID as identifier instead of ID

--- pkg/api/folder.go
+++ pkg/api/folder.go
@@ -807,16 +807,13 @@ func (fk8s *folderK8sHandler) newToFolderDto(c *contextmodel.ReqContext, item un
 		return dtos.Folder{}, err
 	}
 
-	toID := func(rawIdentifier string) (int64, error) {
+	// #TODO Is there a preexisting function we can use instead, something along the lines of UserIdentifier?
+	toUID := func(rawIdentifier string) (string, error) {
 		parts := strings.Split(rawIdentifier, ":")
 		if len(parts) < 2 {
-			return 0, fmt.Errorf("invalid user identifier")
+			return "", fmt.Errorf("invalid user identifier")
 		}
-		userID, err := strconv.ParseInt(parts[1], 10, 64)
-		if err != nil {
-			return 0, fmt.Errorf("faild to parse user identifier")
-		}
-		return userID, nil
+		return parts[1], nil
 	}
 
 	toDTO := func(fold *folder.Folder, checkCanView bool) (dtos.Folder, error) {
@@ -835,18 +832,18 @@ func (fk8s *folderK8sHandler) newToFolderDto(c *contextmodel.ReqContext, item un
 		// #TODO refactor the various conversions of the folder so that we either set created by in folder.Folder or
 		// we convert from unstructured to folder DTO without an intermediate conversion to folder.Folder
 		if len(fDTO.CreatedBy) > 0 {
-			id, err := toID(fDTO.CreatedBy)
+			userUID, err := toUID(fDTO.CreatedBy)
 			if err != nil {
 				return dtos.Folder{}, err
 			}
-			creator = fk8s.getUserLogin(ctx, id)
+			creator = fk8s.getUserLogin(ctx, userUID, orgID)
 		}
 		if len(fDTO.UpdatedBy) > 0 {
-			id, err := toID(fDTO.UpdatedBy)
+			userUID, err := toUID(fDTO.UpdatedBy)
 			if err != nil {
 				return dtos.Folder{}, err
 			}
-			updater = fk8s.getUserLogin(ctx, id)
+			updater = fk8s.getUserLogin(ctx, userUID, orgID)
 		}
 
 		acMetadata, _ := fk8s.getFolderACMetadata(c, fold)
@@ -917,12 +914,15 @@ func (fk8s *folderK8sHandler) newToFolderDto(c *contextmodel.ReqContext, item un
 	return folderDTO, nil
 }
 
-func (fk8s *folderK8sHandler) getUserLogin(ctx context.Context, userID int64) string {
+func (fk8s *folderK8sHandler) getUserLogin(ctx context.Context, userUID string, orgID int64) string {
 	ctx, span := tracer.Start(ctx, "api.getUserLogin")
 	defer span.End()
 
-	query := user.GetUserByIDQuery{ID: userID}
-	user, err := fk8s.userService.GetByID(ctx, &query)
+	query := user.GetUserByUIDQuery{
+		UID:   userUID,
+		OrgID: orgID,
+	}
+	user, err := fk8s.userService.GetByUID(ctx, &query)
 	if err != nil {
 		return anonString
 	}
