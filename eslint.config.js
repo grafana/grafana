b2275c96de6714b771ad3ecdaee682d2fecb9912
Jack Westbrook <jack.westbrook@gmail.com>
2024-10-16 14:10:26 +0200

    chore(eslint): introduce flat config

---
+++ eslint.config.js
@@ -0,0 +1,201 @@
+// @ts-check
+const emotionPlugin = require('@emotion/eslint-plugin');
+const importPlugin = require('eslint-plugin-import');
+const jestPlugin = require('eslint-plugin-jest');
+const jsxA11yPlugin = require('eslint-plugin-jsx-a11y');
+const lodashPlugin = require('eslint-plugin-lodash');
+const barrelPlugin = require('eslint-plugin-no-barrel-files');
+const reactPlugin = require('eslint-plugin-react');
+const testingLibraryPlugin = require('eslint-plugin-testing-library');
+
+const grafanaConfig = require('@grafana/eslint-config/flat');
+const grafanaPlugin = require('@grafana/eslint-plugin');
+
+/**
+ * @type {Array<import('eslint').Linter.Config>}
+ */
+module.exports = [
+  {
+    name: 'grafana/ignores',
+    ignores: [
+      '**/.*', // dotfiles aren't ignored by default in FlatConfig
+      '.github',
+      '.yarn',
+      '**/build/',
+      '**/compiled/',
+      'data/',
+      'deployment_tools_config.json',
+      'devenv',
+      '**/dist/',
+      'e2e/tmp',
+      'pkg',
+      'scripts/grafana-server/tmp',
+      'public/vendor/',
+      'e2e/test-plugins',
+      'playwright-report',
+      '**/*.gen.ts',
+      'public/locales/_build',
+      'public/locales/**/*.js',
+      'packages/grafana-ui/src/components/Icon/iconBundle.ts',
+    ],
+  },
+  grafanaConfig,
+  reactPlugin.configs.flat['jsx-runtime'],
+  {
+    name: 'grafana/defaults',
+    linterOptions: {
+      // This reports unused disable directives that we can clean up but
+      // it also conflicts with the betterer eslint rules so disabled
+      reportUnusedDisableDirectives: false,
+    },
+    files: ['**/*.{ts,tsx,js}'],
+    plugins: {
+      '@emotion': emotionPlugin,
+      lodash: lodashPlugin,
+      jest: jestPlugin,
+      import: importPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      'no-barrel-files': barrelPlugin,
+      'testing-library': testingLibraryPlugin,
+      '@grafana': grafanaPlugin,
+    },
+
+    settings: {
+      'import/internal-regex': '^(app/)|(@grafana)',
+      'import/external-module-folders': ['node_modules', '.yarn'],
+    },
+
+    rules: {
+      '@grafana/no-border-radius-literal': 'error',
+      '@grafana/no-unreduced-motion': 'error',
+      'react/prop-types': 'off',
+      // need to ignore emotion's `css` prop, see https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md#rule-options
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      '@emotion/jsx-import': 'error',
+      'lodash/import-scope': [2, 'member'],
+      'jest/no-focused-tests': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc' },
+        },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              name: 'react-redux',
+              importNames: ['useDispatch', 'useSelector'],
+              message: 'Please import from app/types instead.',
+            },
+            {
+              name: 'react-i18next',
+              importNames: ['Trans', 't'],
+              message: 'Please import from app/core/internationalization instead',
+            },
+          ],
+        },
+      ],
+
+      // Use typescript's no-redeclare for compatibility with overrides
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': ['error'],
+    },
+  },
+  {
+    name: 'grafana/uplot-overrides',
+    files: ['packages/grafana-ui/src/components/uPlot/**/*.{ts,tsx}'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
+  {
+    name: 'grafana/theme-demo-overrides',
+    files: ['packages/grafana-ui/src/components/ThemeDemos/**/*.{ts,tsx}'],
+    rules: {
+      '@emotion/jsx-import': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    name: 'grafana/public-dashboards-overrides',
+    files: ['public/dashboards/scripted*.js'],
+    rules: {
+      'no-redeclare': 'error',
+      '@typescript-eslint/no-redeclare': 'off',
+    },
+  },
+  {
+    name: 'grafana/tests-overrides',
+    files: ['**/*.tsx'],
+    ignores: ['**/*.{spec,test}.tsx'],
+    rules: {
+      // rules marked "off" are those left in the recommended preset we need to fix
+      // we should remove the corresponding line and fix them one by one
+      // any marked "error" contain specific overrides we'll need to keep
+      'jsx-a11y/no-autofocus': [
+        'error',
+        {
+          ignoreNonDOM: true,
+        },
+      ],
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          controlComponents: ['NumberInput'],
+          depth: 2,
+        },
+      ],
+    },
+  },
+  {
+    name: 'grafana/decoupled-plugins-overrides',
+    files: [
+      'public/app/plugins/datasource/azuremonitor/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/cloud-monitoring/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/cloudwatch/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/elasticsearch/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/elasticsearch/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/grafana-postgresql-datasource/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/grafana-pyroscope-datasource/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/grafana-testdata-datasource/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/jaeger/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/loki/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/loki/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/mysql/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/parca/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/tempo/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/zipkin/**/*.{ts,tsx}',
+    ],
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx'],
+        },
+      },
+    },
+    rules: {
+      'import/no-restricted-paths': [
+        'error',
+        {
+          zones: [
+            {
+              target: './public/app/plugins',
+              from: './public',
+              except: ['./app/plugins'],
+              message: 'Core plugins are not allowed to depend on Grafana core packages',
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
