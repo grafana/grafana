9016b418885212c90c315a51067ad993127b6c97
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-10-17 17:08:03 +0100

    Fix display of contact points tab as well

--- public/app/features/alerting/unified/hooks/useAbilities.ts
+++ public/app/features/alerting/unified/hooks/useAbilities.ts
@@ -231,9 +231,21 @@ export function useAllAlertmanagerAbilities(): Abilities<AlertmanagerAction> {
       // TODO: Move this into the permissions config and generalise that code to allow for an array of permissions
       isGrafanaFlavoredAlertmanager ? AccessControlAction.AlertingReceiversCreate : null
     ),
-    [AlertmanagerAction.ViewContactPoint]: toAbility(AlwaysSupported, notificationsPermissions.read),
-    [AlertmanagerAction.UpdateContactPoint]: toAbility(hasConfigurationAPI, notificationsPermissions.update),
-    [AlertmanagerAction.DeleteContactPoint]: toAbility(hasConfigurationAPI, notificationsPermissions.delete),
+    [AlertmanagerAction.ViewContactPoint]: toAbility(
+      AlwaysSupported,
+      notificationsPermissions.read,
+      isGrafanaFlavoredAlertmanager ? AccessControlAction.AlertingReceiversRead : null
+    ),
+    [AlertmanagerAction.UpdateContactPoint]: toAbility(
+      hasConfigurationAPI,
+      notificationsPermissions.update,
+      isGrafanaFlavoredAlertmanager ? AccessControlAction.AlertingReceiversWrite : null
+    ),
+    [AlertmanagerAction.DeleteContactPoint]: toAbility(
+      hasConfigurationAPI,
+      notificationsPermissions.delete,
+      isGrafanaFlavoredAlertmanager ? AccessControlAction.AlertingReceiversWrite : null
+    ),
     // At the time of writing, only Grafana flavored alertmanager supports exporting,
     // and if a user can view the contact point, then they can also export it
     // So the only check we make is if the alertmanager is Grafana flavored
