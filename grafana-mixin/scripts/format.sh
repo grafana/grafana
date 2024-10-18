7157502967085d392e07db294ca32875cd95074e
mnv <manavrj.07@gmail.com>
2024-10-18 15:53:51 +0530

    Update format.sh
    JSONNET_FMT: The script now checks if JSONNET_FMT is set and exits with an error message if not and also added the --no-run-if-empty flag to xargs to avoid errors if no files are found.

--- grafana-mixin/scripts/format.sh
+++ grafana-mixin/scripts/format.sh
@@ -4,6 +4,10 @@ set -eo pipefail
 cd "$(dirname "$0")"/..
 
 . scripts/common.sh
+if [ -z "$JSONNET_FMT" ]; then
+  echo "Error: JSONNET_FMT is not set. Please define the path to the jsonnet formatter."
+  exit 1
+fi
 
 find . -name 'vendor' -prune -o -name '*.libsonnet' -print -o -name '*.jsonnet' -print | \
-  xargs -n 1 -- ${JSONNET_FMT} -i
+  xargs -n 1 --no-run-if-empty -- ${JSONNET_FMT} -i
