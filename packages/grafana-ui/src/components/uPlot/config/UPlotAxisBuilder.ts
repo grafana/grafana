594e9b1b73f4196f6b4c19633a6d260b9fc98044
Leon Sorokin <leeoniya@gmail.com>
2024-10-21 14:29:51 -0500

    BarChart: Skip formatting null-valued ticks (#94935)
    Co-authored-by: Ihor Yeromin <yeryomin.igor@gmail.com>

--- packages/grafana-ui/src/components/uPlot/config/UPlotAxisBuilder.ts
+++ packages/grafana-ui/src/components/uPlot/config/UPlotAxisBuilder.ts
@@ -168,7 +168,13 @@ export class UPlotAxisBuilder extends PlotConfigBuilder<AxisProps, Axis> {
     } else if (formatValue) {
       config.values = (u: uPlot, splits, axisIdx, tickSpace, tickIncr) => {
         let decimals = guessDecimals(roundDecimals(tickIncr, 6));
-        return splits.map((v) => formatValue!(v, decimals > 0 ? decimals : undefined));
+        return splits.map((v) => {
+          if (v == null) {
+            return null;
+          } else {
+            return formatValue!(v, decimals > 0 ? decimals : undefined);
+          }
+        });
       };
     }
 
