11c8a48a9234bec6d4883fe74af5d82adaffc0e2
Piotr Jamróz <pm.jamroz@gmail.com>
2024-10-21 14:51:55 +0200

    Pass stringified context and make sure it's flat

--- public/app/core/crash/crash.reporter.ts
+++ public/app/core/crash/crash.reporter.ts
@@ -8,6 +8,36 @@ import initCrashDetection from '../utils/crashdetection/client-controller';
 
 const logger = createMonitoringLogger('core.crash-detection');
 
+/**
+ * Ensures the context is a flat object with strings
+ */
+function prepareContext(context: Object): Record<string, string> {
+  const preparedContext = {};
+  function prepare(value, propertyName) {
+    if (typeof value === 'object' && value !== null) {
+      if (Array.isArray(value)) {
+        throw new Error('Array values are not supported.');
+      } else {
+        for (const key in value) {
+          if (value.hasOwnProperty(key)) {
+            prepare(value[key], propertyName ? `${propertyName}_${key}` : key);
+          }
+        }
+      }
+    } else if (typeof value === 'string') {
+      preparedContext[propertyName] = value;
+    } else if (typeof value === 'number') {
+      if (Number.isInteger(value)) {
+        preparedContext[propertyName] = value.toString();
+      } else {
+        preparedContext[propertyName] = value.toFixed(4);
+      }
+    }
+  }
+  prepare(context, 'context');
+  return preparedContext;
+}
+
 export function startReportingCrashes() {
   initCrashDetection({
     id: nanoid(5),
@@ -21,32 +51,33 @@ export function startReportingCrashes() {
     },
 
     reportCrash: async (tab) => {
-      tab.tabLastActive = String(tab.tabLastActive);
-      tab.workerLastActive = String(tab.workerLastActive);
-      tab.tabFirstActive = String(tab.tabFirstActive);
-      logger.logInfo('browser crash detected', tab);
+      const preparedContext = prepareContext(tab);
+      logger.logInfo('browser crash detected', preparedContext);
+      console.log('browser crash detected, context', preparedContext);
       console.log('browser crash detected', tab);
       return true;
     },
 
     updateInfo: (info) => {
-      info.url = window.location.href;
-      info.version = config.buildInfo.version;
-      info.userEmail = contextSrv.user.email;
-      info.userLogin = contextSrv.user.login;
-      info.userName = contextSrv.user.name;
+      info.app = {
+        version: config.buildInfo.version,
+        url: window.location.href,
+      };
+
+      info.user = {
+        email: contextSrv.user.email,
+        login: contextSrv.user.login,
+        name: contextSrv.user.name,
+      };
 
       if (performance?.memory?.usedJSHeapSize) {
-        info.memoryHeapUtilization = (
-          performance?.memory?.usedJSHeapSize / performance?.memory?.totalJSHeapSize
-        ).toFixed(4);
-        info.memoryLimitUtilization = (
-          performance?.memory?.totalJSHeapSize / performance?.memory?.jsHeapSizeLimit
-        ).toFixed(4);
-
-        info.memoryUsedJSHeapSize = String(performance?.memory?.usedJSHeapSize);
-        info.memoryTotalJSHeapSize = String(performance?.memory?.totalJSHeapSize);
-        info.memoryJsHeapSizeLimit = String(performance?.memory?.jsHeapSizeLimit);
+        info.memory = {
+          heapUtilization: performance?.memory?.usedJSHeapSize / performance?.memory?.totalJSHeapSize,
+          limitUtilization: performance?.memory?.totalJSHeapSize / performance?.memory?.jsHeapSizeLimit,
+          usedJSHeapSize: performance?.memory?.usedJSHeapSize,
+          totalJSHeapSize: performance?.memory?.totalJSHeapSize,
+          jsHeapSizeLimit: performance?.memory?.jsHeapSizeLimit,
+        };
       }
     },
   });
