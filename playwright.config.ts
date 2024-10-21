87732f8da7be1181bd5eee620d512908dac8070d
Jack Westbrook <jack.westbrook@gmail.com>
2024-10-21 11:36:18 +0200

    MSSQL: Add playwright smoke test (#94977)
    test(mssql-datasource): add smoke test to assert decoupled plugin assets are built and load

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,6 +71,15 @@ export default defineConfig<PluginOptions>({
       dependencies: ['authenticate'],
     },
     {
+      name: 'mssql',
+      testDir: path.join(testDirRoot, '/mssql'),
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'playwright/.auth/admin.json',
+      },
+      dependencies: ['authenticate'],
+    },
+    {
       name: 'extensions-test-app',
       testDir: 'e2e/test-plugins/grafana-extensionstest-app',
       use: {
