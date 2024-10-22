f8b563609d2bcc3d30c2056e0324259d01968c03
linoman <2051016+linoman@users.noreply.github.com>
2024-10-22 16:21:10 +0200

    RolePickerDrawer: Replace RolePicker modal for drawer (#94801)
    * Add RolePickerDrawer
    
    * Add RoiePickerBadges
    
    * Add RolePickerSelect
    
    * Replace RolePicker in OrgUsersTable
    
    * Replace RolePicker in ServiceAccountCreatePage
    
    * Add RolePickerDrawer test
    
    * Add tests
    
    * Add i18n texts
    
    * Update RolePickerBadges

---
+++ public/app/core/components/RolePickerDrawer/RolePickerBadges.tsx
@@ -0,0 +1,63 @@
+import { css } from '@emotion/css';
+import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { GrafanaTheme2 } from '@grafana/data';
+import { useStyles2, Badge, Stack } from '@grafana/ui';
+import { OrgUser } from 'app/types';
+
+import { RolePickerDrawer } from './RolePickerDrawer';
+
+export interface Props {
+  disabled?: boolean;
+  user: OrgUser;
+}
+
+export const RolePickerBadges = ({ disabled, user }: Props) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const { badge, badgeDisabled } = useStyles2(getStyles);
+  const badgeStyle = disabled ? badgeDisabled : badge;
+
+  const methods = useForm({
+    defaultValues: {
+      name: user.name,
+      role: user.role,
+      roles: user.roles,
+    },
+  });
+  const { watch } = methods;
+
+  const drawerControl = () => {
+    if (!disabled) {
+      setIsDrawerOpen(true);
+    }
+  };
+
+  return (
+    <>
+      <Stack gap={1}>
+        <Badge className={badgeStyle} color="blue" onClick={drawerControl} text={watch('role')} />
+        {user.roles && user.roles.length > 0 && (
+          <Badge className={badgeStyle} color="blue" onClick={drawerControl} text={`+${user.roles.length}`} />
+        )}
+      </Stack>
+      {isDrawerOpen && (
+        <FormProvider {...methods}>
+          <RolePickerDrawer onClose={() => setIsDrawerOpen(false)} />
+        </FormProvider>
+      )}
+    </>
+  );
+};
+
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    badge: css({
+      cursor: 'pointer',
+    }),
+    badgeDisabled: css({
+      cursor: 'not-allowed',
+    }),
+  };
+}
--- public/app/features/serviceaccounts/ServiceAccountCreatePage.tsx
+++ public/app/features/serviceaccounts/ServiceAccountCreatePage.tsx
@@ -1,11 +1,14 @@
 import { useCallback, useEffect, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 
-import { getBackendSrv, locationService } from '@grafana/runtime';
+import { config, getBackendSrv, locationService } from '@grafana/runtime';
 import { Button, Input, Field, FieldSet } from '@grafana/ui';
+import { t, Trans } from '@grafana/ui/src/utils/i18n';
 import { Form } from 'app/core/components/Form/Form';
 import { Page } from 'app/core/components/Page/Page';
 import { UserRolePicker } from 'app/core/components/RolePicker/UserRolePicker';
 import { fetchRoleOptions, updateUserRoles } from 'app/core/components/RolePicker/api';
+import { RolePickerSelect } from 'app/core/components/RolePickerDrawer/RolePickerSelect';
 import { contextSrv } from 'app/core/core';
 import { AccessControlAction, OrgRole, Role, ServiceAccountCreateApiResponse, ServiceAccountDTO } from 'app/types';
 
@@ -22,22 +25,37 @@ const createServiceAccount = async (sa: ServiceAccountDTO) => {
 const updateServiceAccount = async (id: number, sa: ServiceAccountDTO) =>
   getBackendSrv().patch(`/api/serviceaccounts/${id}`, sa);
 
+const defaultServiceAccount = {
+  id: 0,
+  orgId: contextSrv.user.orgId,
+  role: contextSrv.licensedAccessControlEnabled() ? OrgRole.None : OrgRole.Viewer,
+  tokens: 0,
+  name: '',
+  login: '',
+  isDisabled: false,
+  createdAt: '',
+  teams: [],
+};
+
 export const ServiceAccountCreatePage = ({}: Props): JSX.Element => {
   const [roleOptions, setRoleOptions] = useState<Role[]>([]);
   const [pendingRoles, setPendingRoles] = useState<Role[]>([]);
 
-  const currentOrgId = contextSrv.user.orgId;
-  const [serviceAccount, setServiceAccount] = useState<ServiceAccountDTO>({
-    id: 0,
-    orgId: contextSrv.user.orgId,
-    role: contextSrv.licensedAccessControlEnabled() ? OrgRole.None : OrgRole.Viewer,
-    tokens: 0,
-    name: '',
-    login: '',
-    isDisabled: false,
-    createdAt: '',
-    teams: [],
+  const methods = useForm({
+    defaultValues: {
+      name: '',
+      role: defaultServiceAccount.role,
+      roleCollection: [defaultServiceAccount.role],
+      roles: [],
+    },
   });
+  const {
+    formState: { errors },
+    register,
+  } = methods;
+
+  const currentOrgId = contextSrv.user.orgId;
+  const [serviceAccount, setServiceAccount] = useState<ServiceAccountDTO>(defaultServiceAccount);
 
   useEffect(() => {
     async function fetchOptions() {
@@ -47,7 +65,7 @@ export const ServiceAccountCreatePage = ({}: Props): JSX.Element => {
           setRoleOptions(options);
         }
       } catch (e) {
-        console.error('Error loading options', e);
+        console.error('Error loading options', e); // TODO: handle error
       }
     }
     if (contextSrv.licensedAccessControlEnabled()) {
@@ -79,7 +97,7 @@ export const ServiceAccountCreatePage = ({}: Props): JSX.Element => {
           await updateUserRoles(pendingRoles, newAccount.id, newAccount.orgId);
         }
       } catch (e) {
-        console.error(e);
+        console.error(e); // TODO: handle error
       }
       locationService.push(`/org/serviceaccounts/${response.id}`);
     },
@@ -99,44 +117,85 @@ export const ServiceAccountCreatePage = ({}: Props): JSX.Element => {
   };
 
   return (
-    <Page navId="serviceaccounts" pageNav={{ text: 'Create service account' }}>
+    <Page
+      navId="serviceaccounts"
+      pageNav={{ text: t('service-account-create-page.page-nav.label', 'Create service account') }}
+    >
       <Page.Contents>
-        <Form onSubmit={onSubmit} validateOn="onSubmit">
-          {({ register, errors }) => {
-            return (
-              <>
-                <FieldSet>
-                  <Field
-                    label="Display name"
-                    required
-                    invalid={!!errors.name}
-                    error={errors.name ? 'Display name is required' : undefined}
-                  >
-                    <Input id="display-name-input" {...register('name', { required: true })} autoFocus />
-                  </Field>
-                  <Field label="Role">
-                    {contextSrv.licensedAccessControlEnabled() ? (
-                      <UserRolePicker
-                        apply
-                        userId={serviceAccount.id || 0}
-                        orgId={serviceAccount.orgId}
-                        basicRole={serviceAccount.role}
-                        onBasicRoleChange={onRoleChange}
-                        roleOptions={roleOptions}
-                        onApplyRoles={onPendingRolesUpdate}
-                        pendingRoles={pendingRoles}
-                        maxWidth="100%"
-                      />
-                    ) : (
-                      <OrgRolePicker aria-label="Role" value={serviceAccount.role} onChange={onRoleChange} />
-                    )}
-                  </Field>
-                </FieldSet>
-                <Button type="submit">Create</Button>
-              </>
-            );
-          }}
-        </Form>
+        {config.featureToggles.rolePickerDrawer && (
+          <FormProvider {...methods}>
+            <form>
+              <FieldSet>
+                <Field
+                  label={t('service-account-create-page.name.label', 'Display name')}
+                  required
+                  invalid={!!errors.name}
+                  error={
+                    errors.name
+                      ? t('service-account-create-page.name.required-error', 'Display name is required')
+                      : undefined
+                  }
+                >
+                  <Input id="name" {...register('name', { required: true })} autoFocus />
+                </Field>
+                <Field label={t('service-account-create-page.role.label', 'Role')}>
+                  <RolePickerSelect />
+                </Field>
+              </FieldSet>
+              <Button type="submit">
+                <Trans i18nKey="service-account-create-page.create.button">Create</Trans>
+              </Button>
+            </form>
+          </FormProvider>
+        )}
+        {!config.featureToggles.rolePickerDrawer && (
+          <Form onSubmit={onSubmit} validateOn="onSubmit">
+            {({ register, errors }) => {
+              return (
+                <>
+                  <FieldSet>
+                    <Field
+                      label={t('service-account-create-page.name.label', 'Display name')}
+                      required
+                      invalid={!!errors.name}
+                      error={
+                        errors.name
+                          ? t('service-account-create-page.name.required-error', 'Display name is required')
+                          : undefined
+                      }
+                    >
+                      <Input id="display-name-input" {...register('name', { required: true })} autoFocus />
+                    </Field>
+                    <Field label={t('service-account-create-page.role.label', 'Role')}>
+                      {contextSrv.licensedAccessControlEnabled() ? (
+                        <UserRolePicker
+                          apply
+                          userId={serviceAccount.id || 0}
+                          orgId={serviceAccount.orgId}
+                          basicRole={serviceAccount.role}
+                          onBasicRoleChange={onRoleChange}
+                          roleOptions={roleOptions}
+                          onApplyRoles={onPendingRolesUpdate}
+                          pendingRoles={pendingRoles}
+                          maxWidth="100%"
+                        />
+                      ) : (
+                        <OrgRolePicker
+                          aria-label={t('service-account-create-page.role.label', 'Role')}
+                          value={serviceAccount.role}
+                          onChange={onRoleChange}
+                        />
+                      )}
+                    </Field>
+                  </FieldSet>
+                  <Button type="submit">
+                    <Trans i18nKey="service-account-create-page.create.button">Create</Trans>
+                  </Button>
+                </>
+              );
+            }}
+          </Form>
+        )}
       </Page.Contents>
     </Page>
   );
