b57c36ec01796dcbea8c2782727b47fd3bd2f174
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2024-10-22 13:48:04 +0200

    remove gcom request at local changelog method

--- public/app/features/plugins/admin/api.ts
+++ public/app/features/plugins/admin/api.ts
@@ -36,7 +36,7 @@ export async function getPluginDetails(id: string): Promise<CatalogPluginDetails
     versions,
     statusContext: remote?.statusContext ?? '',
     iam: remote?.json?.iam,
-    changelog: localChangelog || remote?.changelog,
+    changelog: remote?.changelog || localChangelog,
   };
 }
 
@@ -120,9 +120,8 @@ async function getLocalPluginReadme(id: string): Promise<string> {
 
 async function getLocalPluginChangelog(id: string): Promise<string> {
   try {
-    const pluginFromGcom = await getBackendSrv().get(`api/gnet/plugins/${id}?version=latest`);
-    const markdownAsHtml = pluginFromGcom ? renderMarkdown(pluginFromGcom.changelog) : '';
-
+    const markdown: string = await getBackendSrv().get(`${API_ROOT}/${id}/markdown/CHANGELOG`);
+    const markdownAsHtml = markdown ? renderMarkdown(markdown) : '';
     return markdownAsHtml;
   } catch (error) {
     if (isFetchError(error)) {
