62359f7600c39815269f8e9b5419f5822f321052
Gabriel MABILLE <gamab@users.noreply.github.com>
2024-10-22 09:36:08 +0200

    Update pkg/services/authz/config.go

--- pkg/services/authz/config.go
+++ pkg/services/authz/config.go
@@ -47,8 +47,10 @@ func ReadCfg(cfg *setting.Cfg) (*Cfg, error) {
 	tokenNamespace := grpcClientAuthSection.Key("token_namespace").MustString("stacks-" + cfg.StackID)
 
 	// When running in cloud mode, the token and tokenExchangeURL are required.
-	if mode == ModeGRPC && cfg.StackID != "" && token == "" && tokenExchangeURL == "" {
-		return nil, fmt.Errorf("authorization: missing token or tokenExchangeUrl")
+	if mode == ModeGRPC && cfg.StackID != "" {
+		if token == "" || tokenExchangeURL == "" {
+			return nil, fmt.Errorf("authorization:  missing token or tokenExchangeUrl")
+		}
 	}
 
 	return &Cfg{
