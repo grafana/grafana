9ef95c93aad7154b64f219ae5032f3c89de78e09
leonorfmartins <leonorfmartins@gmail.com>
2024-10-23 18:39:17 +0200

    Setup internal test fn

--- pkg/registry/apis/folders/register_test.go
+++ pkg/registry/apis/folders/register_test.go
@@ -1,21 +1,18 @@
 package folders
 
 import (
+	"context"
 	"testing"
 
-	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
-	"github.com/grafana/grafana/pkg/services/authz/zanzana"
-	"github.com/grafana/grafana/pkg/services/featuremgmt"
-	"github.com/grafana/grafana/pkg/services/folder/foldertest"
-	"github.com/stretchr/testify/require"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 )
 
-func TestFolderAPIBuilder_GetAuthorizer(t *testing.T) {
+func TestFolderAPIBuilder_getAuthorizerFunc(t *testing.T) {
 	tests := []struct {
-		name  string
-		input authorizer.Authorizer
-		want  authorizer.Authorizer
+		name         string
+		inputContext context.Context
+		inputAttr    authorizer.Attributes
+		want         authorizer.Authorizer
 	}{
 		{
 			name: "When creating folder should not return access denied error",
@@ -24,15 +21,9 @@ func TestFolderAPIBuilder_GetAuthorizer(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := &FolderAPIBuilder{
-				gv:            resourceInfo.GroupVersion(),
-				features:      nil,
-				namespacer:    func(_ int64) string { return "123" },
-				folderSvc:     foldertest.NewFakeService(),
-				accessControl: acimpl.ProvideAccessControl(featuremgmt.WithFeatures("nestedFolders"), zanzana.NewNoopClient()),
-			}
-			got := b.GetAuthorizer()
-			require.NotNil(t, got)
+
+			_, user, eval, decision, requester := authorizerFunc(tt.inputContext, tt.inputAttr)
+			// require.NotNil(t, )
 		})
 	}
 }
