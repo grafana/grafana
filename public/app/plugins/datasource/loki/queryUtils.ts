83f88664ae75b8212f2adcced96797718c746495
Matias Chomicki <matyax@gmail.com>
2024-10-23 11:06:34 +0200

    Use log volume refId constant

--- public/app/plugins/datasource/loki/queryUtils.ts
+++ public/app/plugins/datasource/loki/queryUtils.ts
@@ -28,6 +28,7 @@ import { DataQuery } from '@grafana/schema';
 import { addLabelToQuery, getStreamSelectorPositions, NodePosition } from './modifyQuery';
 import { ErrorId } from './querybuilder/parsingUtils';
 import { LabelType, LokiQuery, LokiQueryDirection, LokiQueryType } from './types';
+import { REF_ID_STARTER_LOG_VOLUME } from './datasource';
 
 /**
  * Returns search terms from a LogQL query.
@@ -318,7 +319,7 @@ export function requestSupportsSharding(allQueries: LokiQuery[]) {
     .filter((query) => !query.hide)
     .filter((query) => !query.refId.includes('do-not-shard'))
     .filter((query) => query.expr)
-    .filter((query) => query.direction === LokiQueryDirection.Scan || query.refId?.startsWith('log-volume-'));
+    .filter((query) => query.direction === LokiQueryDirection.Scan || query.refId?.startsWith(REF_ID_STARTER_LOG_VOLUME));
 
   return queries.length > 0;
 }
