bad1f0f5677362b0156f946465a1dab4a32b8250
Gábor Farkas <gabor.farkas@gmail.com>
2024-10-23 09:13:34 +0200

    merged tests

--- pkg/tests/api/loki/loki_test.go
+++ pkg/tests/api/loki/loki_test.go
@@ -75,57 +75,6 @@ func TestIntegrationLoki(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	t.Run("When calling /api/ds/query forward alerting-related headers", func(t *testing.T) {
-		query := simplejson.NewFromAny(map[string]interface{}{
-			"datasource": map[string]interface{}{
-				"uid": uid,
-			},
-			"expr": "{job=\"grafana\"}",
-		})
-		buf1 := &bytes.Buffer{}
-		err = json.NewEncoder(buf1).Encode(dtos.MetricRequest{
-			From:    "now-1h",
-			To:      "now",
-			Queries: []*simplejson.Json{query},
-		})
-		require.NoError(t, err)
-		u := fmt.Sprintf("http://admin:admin@%s/api/ds/query", grafanaListeningAddr)
-		// nolint:gosec
-		req, err := http.NewRequest("POST", u, buf1)
-		if err != nil {
-			require.NoError(t, err)
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("X-Unspported-Header", "uh")
-		req.Header.Set("Fromalert", "true")
-		req.Header.Set("X-Rule-Name", "n1")
-		req.Header.Set("X-Rule-Folder", "f1")
-		req.Header.Set("X-Rule-Source", "s1")
-		req.Header.Set("X-Rule-Type", "t1")
-		req.Header.Set("X-Rule-Version", "v1")
-
-		resp, err := http.DefaultClient.Do(req)
-		require.NoError(t, err)
-
-		require.NoError(t, err)
-		require.Equal(t, http.StatusBadRequest, resp.StatusCode)
-		t.Cleanup(func() {
-			err := resp.Body.Close()
-			require.NoError(t, err)
-		})
-		_, err = io.ReadAll(resp.Body)
-		require.NoError(t, err)
-
-		require.NotNil(t, outgoingRequest)
-		require.Equal(t, "", outgoingRequest.Header.Get("X-Unspported-Header"))
-		require.Equal(t, "true", outgoingRequest.Header.Get("Fromalert"))
-		require.Equal(t, "n1", outgoingRequest.Header.Get("X-Rule-Name"))
-		require.Equal(t, "f1", outgoingRequest.Header.Get("X-Rule-Folder"))
-		require.Equal(t, "s1", outgoingRequest.Header.Get("X-Rule-Source"))
-		require.Equal(t, "t1", outgoingRequest.Header.Get("X-Rule-Type"))
-		require.Equal(t, "v1", outgoingRequest.Header.Get("X-Rule-Version"))
-	})
-
 	t.Run("When calling /api/ds/query should set expected headers on outgoing HTTP request", func(t *testing.T) {
 		query := simplejson.NewFromAny(map[string]interface{}{
 			"datasource": map[string]interface{}{
@@ -142,7 +91,17 @@ func TestIntegrationLoki(t *testing.T) {
 		require.NoError(t, err)
 		u := fmt.Sprintf("http://admin:admin@%s/api/ds/query", grafanaListeningAddr)
 		// nolint:gosec
-		resp, err := http.Post(u, "application/json", buf1)
+		req, err := http.NewRequest("POST", u, buf1)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("X-Unspported-Header", "uh")
+		req.Header.Set("Fromalert", "true")
+		req.Header.Set("X-Rule-Name", "n1")
+		req.Header.Set("X-Rule-Folder", "f1")
+		req.Header.Set("X-Rule-Source", "s1")
+		req.Header.Set("X-Rule-Type", "t1")
+		req.Header.Set("X-Rule-Version", "v1")
+		resp, err := http.DefaultClient.Do(req)
 		require.NoError(t, err)
 		require.Equal(t, http.StatusBadRequest, resp.StatusCode)
 		t.Cleanup(func() {
@@ -158,6 +117,13 @@ func TestIntegrationLoki(t *testing.T) {
 		require.True(t, ok)
 		require.Equal(t, "basicAuthUser", username)
 		require.Equal(t, "basicAuthPassword", pwd)
+		require.Equal(t, "", outgoingRequest.Header.Get("X-Unspported-Header"))
+		require.Equal(t, "true", outgoingRequest.Header.Get("Fromalert"))
+		require.Equal(t, "n1", outgoingRequest.Header.Get("X-Rule-Name"))
+		require.Equal(t, "f1", outgoingRequest.Header.Get("X-Rule-Folder"))
+		require.Equal(t, "s1", outgoingRequest.Header.Get("X-Rule-Source"))
+		require.Equal(t, "t1", outgoingRequest.Header.Get("X-Rule-Type"))
+		require.Equal(t, "v1", outgoingRequest.Header.Get("X-Rule-Version"))
 	})
 
 	t.Run("should forward `X-Dashboard-Title` header but no `X-Panel-Title`", func(t *testing.T) {
