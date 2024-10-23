0849408ef4df77677c316fe0a87cc186b91ef760
leonorfmartins <leonorfmartins@gmail.com>
2024-10-23 19:15:46 +0200

    Better define test inputs

--- pkg/registry/apis/folders/register_test.go
+++ pkg/registry/apis/folders/register_test.go
@@ -4,25 +4,34 @@ import (
 	"context"
 	"testing"
 
+	"github.com/grafana/grafana/pkg/apimachinery/identity"
+	"github.com/grafana/grafana/pkg/services/user"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 )
 
 func TestFolderAPIBuilder_getAuthorizerFunc(t *testing.T) {
+	type expected struct {
+		user      identity.Requester
+		eval      authorizer.Attributes
+		decision  authorizer.Decision
+		requester string
+	}
+	var ctx = context.Background()
 	tests := []struct {
-		name         string
-		inputContext context.Context
-		inputAttr    authorizer.Attributes
-		want         authorizer.Authorizer
+		name  string
+		input context.Context
+		want  expected
 	}{
 		{
-			name: "When creating folder should not return access denied error",
+			name:  "When creating folder should not return access denied error",
+			input: identity.WithRequester(ctx, &user.SignedInUser{}),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			_, user, eval, decision, requester := authorizerFunc(tt.inputContext, tt.inputAttr)
+			_, user, eval, decision, requester := authorizerFunc(tt.input, authorizer.AttributesRecord{})
 			// require.NotNil(t, )
 		})
 	}
