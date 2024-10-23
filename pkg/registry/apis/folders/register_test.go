44dfa9368229911edca6cc0d41cc2f612eede1ce
leonorfmartins <leonorfmartins@gmail.com>
2024-10-23 13:01:32 +0200

    WIP: setup to test folders GetAuthorizer

---
+++ pkg/registry/apis/folders/register_test.go
@@ -0,0 +1,37 @@
+package folders
+
+import (
+	"testing"
+
+	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
+	"github.com/grafana/grafana/pkg/services/authz/zanzana"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
+	"github.com/grafana/grafana/pkg/services/folder/foldertest"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apiserver/pkg/authorization/authorizer"
+)
+
+func TestFolderAPIBuilder_GetAuthorizer(t *testing.T) {
+	tests := []struct {
+		name  string
+		input authorizer.AuthorizerFunc
+		want  authorizer.Authorizer
+	}{
+		// name: ""
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &FolderAPIBuilder{
+				gv:            resourceInfo.GroupVersion(),
+				features:      nil,
+				namespacer:    func(_ int64) string { return "123" },
+				folderSvc:     foldertest.NewFakeService(),
+				accessControl: acimpl.ProvideAccessControl(featuremgmt.WithFeatures("nestedFolders"), zanzana.NewNoopClient()),
+			}
+			got := b.GetAuthorizer()
+
+			require.NotNil(t, got)
+		})
+	}
+}
