86293aff5b08efc320d0b24822b48b9a19e8ee90
Laura Benz <laura.benz@grafana.com>
2024-10-24 12:29:35 +0200

    feat: add interface for modal props

--- public/app/core/components/NestedFolderPicker/FolderCreationModal.tsx
+++ public/app/core/components/NestedFolderPicker/FolderCreationModal.tsx
@@ -4,7 +4,13 @@ import { Trans, t } from 'app/core/internationalization';
 import { useNewFolderMutation } from '../../../features/browse-dashboards/api/browseDashboardsAPI';
 import { NewFolderForm } from '../../../features/browse-dashboards/components/NewFolderForm';
 
-export const FolderCreationModal = ({ onDismissModal, ...props }) => {
+export interface FolderCreationModalProps {
+  isOpen: boolean;
+  onConfirm: () => void; //TODO: is this true?
+  onDismissModal: () => void;
+}
+
+export const FolderCreationModal = ({ onDismissModal, ...props }: FolderCreationModalProps) => {
   const [newFolder] = useNewFolderMutation();
   const onCreateNewFolder = async (folderName: string) => {
     try {
@@ -13,15 +19,12 @@ export const FolderCreationModal = ({ onDismissModal, ...props }) => {
       });
     } finally {
     }
-    // todo: catch error or close modal in finally?
+    // TODO: catch error or close modal in finally?
   };
 
+  //TODO: adjust styling of input in NewFolderForm
   return (
-    <Modal
-      isOpen={true}
-      title={t('browse-dashboards.folder-picker.folder-creation-modal.title', 'New Folder')}
-      {...props}
-    >
+    <Modal title={t('browse-dashboards.folder-picker.folder-creation-modal.title', 'New Folder')} {...props}>
       <Text>
         <Trans i18nKey="browse-dashboards.folder-picker.folder-creation-modal.text">
           Create a new folder to store your restored dashboard
