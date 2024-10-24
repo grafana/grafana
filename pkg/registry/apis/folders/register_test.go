af1898ba148223dbe35be9844360d68abd73d94d
leonorfmartins <leonorfmartins@gmail.com>
2024-10-24 12:21:48 +0200

    Add FolderAPI builder to the test

--- pkg/registry/apis/folders/register_test.go
+++ pkg/registry/apis/folders/register_test.go
@@ -5,34 +5,48 @@ import (
 	"testing"
 
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
+	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
+	"github.com/grafana/grafana/pkg/services/authz/zanzana"
+	"github.com/grafana/grafana/pkg/services/dashboards"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
+	"github.com/grafana/grafana/pkg/services/folder/foldertest"
 	"github.com/grafana/grafana/pkg/services/user"
+	"github.com/stretchr/testify/require"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 )
 
 func TestFolderAPIBuilder_getAuthorizerFunc(t *testing.T) {
-	type expected struct {
-		user      identity.Requester
-		eval      authorizer.Attributes
-		decision  authorizer.Decision
-		requester string
-	}
+
 	var ctx = context.Background()
+	var orgID int64 = 1
 	tests := []struct {
-		name  string
-		input context.Context
-		want  expected
+		name     string
+		input    context.Context
+		allow bool
 	}{
 		{
-			name:  "When creating folder should not return access denied error",
-			input: identity.WithRequester(ctx, &user.SignedInUser{}),
+			name: "When creating folder should not return access denied error",
+			input: identity.WithRequester(ctx, &user.SignedInUser{UserID: 1, Permissions: map[int64]map[string][]string{
+				orgID: {dashboards.ActionFoldersCreate: {}, dashboards.ActionFoldersWrite: {dashboards.ScopeFoldersAll}},
+			}}),
+			allow: true,
 		},
+	}
+
+	b := &FolderAPIBuilder{
+		gv:            resourceInfo.GroupVersion(),
+		features:      nil,
+		namespacer:    func(_ int64) string { return "123" },
+		folderSvc:     foldertest.NewFakeService(),
+		accessControl: acimpl.ProvideAccessControl(featuremgmt.WithFeatures("nestedFolders"), zanzana.NewNoopClient()),
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
-			_, user, eval, decision, requester := authorizerFunc(tt.input, authorizer.AttributesRecord{})
-			// require.NotNil(t, )
+			ctx, user, eval, _, _ := authorizerFunc(tt.input, authorizer.AttributesRecord{})
+			ok, err := b.accessControl.Evaluate(ctx, user, eval)
+			require.NoError(t, err)
+			require.Equal(t, tt.allow, ok)
 		})
 	}
 }
