c2e34836c49808b15a1d32c73d75aa7eecbafca7
Ihor Yeromin <yeryomin.igor@gmail.com>
2024-10-24 21:32:43 +0200

    TableNG: Sorting with custom table header (#95351)

--- packages/grafana-ui/src/components/Table/TableNG/Cells/TableCellNG.tsx
+++ packages/grafana-ui/src/components/Table/TableNG/Cells/TableCellNG.tsx
@@ -1,49 +1,38 @@
+import { ReactNode } from 'react';
 
-import { ReactNode } from "react";
+import { TableCellDisplayMode } from '@grafana/schema';
 
-import { TableCellDisplayMode } from "@grafana/schema";
-
-import AutoCell from "./AutoCell";
-import { BarGaugeCell } from "./BarGaugeCell";
-import { SparklineCell } from "./SparklineCell";
-
-
+import AutoCell from './AutoCell';
+import { BarGaugeCell } from './BarGaugeCell';
+import { SparklineCell } from './SparklineCell';
 
 // interface TableCellNGProps {
 //     fieldConfig: FieldConfig;
 //     fieldDisplay: any?
 // }
 
-
 export function TableCellNG(props: any) {
-    const { field: shallowField, value, theme, timeRange, height } = props;
-    const { config: fieldConfig } = shallowField;
-    const { type: cellType } = fieldConfig.custom.cellOptions;
-    console.log(cellType);
+  const { field: shallowField, value, theme, timeRange, height } = props;
+  const { config: fieldConfig } = shallowField;
+  const { type: cellType } = fieldConfig.custom.cellOptions;
 
-    // Get the correct cell type
-    let cell: ReactNode = null;
-    switch (cellType) {
-        // case TableCellDisplayMode.
-        case TableCellDisplayMode.Sparkline:
-            cell = <SparklineCell value={value} field={shallowField} theme={theme} timeRange={timeRange} height={height} />
-            break;
-        case TableCellDisplayMode.Gauge:
-        case TableCellDisplayMode.BasicGauge:
-        case TableCellDisplayMode.GradientGauge:
-        case TableCellDisplayMode.LcdGauge:
-            cell = <BarGaugeCell value={value} field={shallowField} theme={theme} timeRange={timeRange} height={height} />
-            break;
-        case TableCellDisplayMode.Auto:
-        default:
-            cell = <AutoCell value={value} field={shallowField} theme={theme} />
-    }
+  // Get the correct cell type
+  let cell: ReactNode = null;
+  switch (cellType) {
+    // case TableCellDisplayMode.
+    case TableCellDisplayMode.Sparkline:
+      cell = <SparklineCell value={value} field={shallowField} theme={theme} timeRange={timeRange} height={height} />;
+      break;
+    case TableCellDisplayMode.Gauge:
+    case TableCellDisplayMode.BasicGauge:
+    case TableCellDisplayMode.GradientGauge:
+    case TableCellDisplayMode.LcdGauge:
+      cell = <BarGaugeCell value={value} field={shallowField} theme={theme} timeRange={timeRange} height={height} />;
+      break;
+    case TableCellDisplayMode.Auto:
+    default:
+      cell = <AutoCell value={value} field={shallowField} theme={theme} />;
+  }
 
-    console.log(cell);
-
-    return cell;
+  return cell;
 }
-
-
-
-
