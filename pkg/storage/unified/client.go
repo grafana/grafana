ad4df4b3f63bdf3e16423ac8c3fdb1a7fae5582e
gamab <gabriel.mabille@grafana.com>
2024-10-24 10:24:04 +0200

    nit

--- pkg/storage/unified/client.go
+++ pkg/storage/unified/client.go
@@ -87,7 +87,7 @@ func ProvideUnifiedStorageClient(
 		}
 
 		// Create a client instance
-		client, err := newResourceClient(tracer, conn, cfg, features)
+		client, err := newResourceClient(conn, cfg, features, tracer)
 		if err != nil {
 			return nil, err
 		}
@@ -116,7 +116,7 @@ func clientCfgMapping(clientCfg *grpcutils.GrpcClientConfig) authnlib.GrpcClient
 	}
 }
 
-func newResourceClient(tracer tracing.Tracer, conn *grpc.ClientConn, cfg *setting.Cfg, features featuremgmt.FeatureToggles) (resource.ResourceClient, error) {
+func newResourceClient(conn *grpc.ClientConn, cfg *setting.Cfg, features featuremgmt.FeatureToggles, tracer tracing.Tracer) (resource.ResourceClient, error) {
 	if !features.IsEnabledGlobally(featuremgmt.FlagAppPlatformGrpcClientAuth) {
 		return resource.NewLegacyResourceClient(conn), nil
 	}
