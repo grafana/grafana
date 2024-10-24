6ef24ebf149d39c8f242557c822e52b5795dfeaa
Matias Chomicki <matyax@gmail.com>
2024-10-24 12:15:52 +0200

    Logs Volume: update timeout message (#95277)
    * Logs Volume: update timeout message
    
    * SupplementaryResultError: add list styles
    
    * Address lint issues
    
    * Update public/app/features/explore/Logs/LogsVolumePanelList.tsx
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>
    
    ---------
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>

--- public/app/features/explore/SupplementaryResultError.tsx
+++ public/app/features/explore/SupplementaryResultError.tsx
@@ -1,53 +1,59 @@
 import { css } from '@emotion/css';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 
 import { DataQueryError, GrafanaTheme2 } from '@grafana/data';
 import { Alert, AlertVariant, Button, useTheme2 } from '@grafana/ui';
 
 type Props = {
   error?: DataQueryError;
+  message?: ReactNode;
   title: string;
   severity?: AlertVariant;
   suggestedAction?: string;
   onSuggestedAction?(): void;
   onRemove?(): void;
 };
+const SHORT_ERROR_MESSAGE_LIMIT = 100;
 export function SupplementaryResultError(props: Props) {
   const [isOpen, setIsOpen] = useState(false);
-  const SHORT_ERROR_MESSAGE_LIMIT = 100;
+
   const { error, title, suggestedAction, onSuggestedAction, onRemove, severity = 'warning' } = props;
   // generic get-error-message-logic, taken from
   // /public/app/features/explore/ErrorContainer.tsx
-  const message = error?.message || error?.data?.message || '';
-  const showButton = !isOpen && message.length > SHORT_ERROR_MESSAGE_LIMIT;
+  const message = props.message ?? error?.message ?? error?.data?.message ?? '';
+  const showButton = typeof message === 'string' && message.length > SHORT_ERROR_MESSAGE_LIMIT;
   const theme = useTheme2();
   const styles = getStyles(theme);
 
   return (
     <div className={styles.supplementaryErrorContainer}>
       <Alert title={title} severity={severity} onRemove={onRemove}>
-        <div className={styles.suggestedActionWrapper}>
-          {showButton ? (
-            <Button
-              variant="secondary"
-              size="xs"
-              onClick={() => {
-                setIsOpen(true);
-              }}
-            >
-              Show details
-            </Button>
-          ) : (
-            message
-          )}
-          {suggestedAction && onSuggestedAction && (
-            <div className={styles.suggestedActionWrapper}>
+        {showButton ? (
+          <div className={styles.suggestedActionWrapper}>
+            {!isOpen ? (
+              <Button
+                variant="secondary"
+                size="xs"
+                onClick={() => {
+                  setIsOpen(true);
+                }}
+              >
+                Show details
+              </Button>
+            ) : (
+              message
+            )}
+          </div>
+        ) : (
+          <div className={styles.suggestedActionWrapper}>
+            {message}
+            {suggestedAction && onSuggestedAction && (
               <Button variant="primary" size="xs" onClick={onSuggestedAction}>
                 {suggestedAction}
               </Button>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </Alert>
     </div>
   );
@@ -59,13 +65,22 @@ const getStyles = (theme: GrafanaTheme2) => {
       width: '50%',
       minWidth: `${theme.breakpoints.values.sm}px`,
       margin: '0 auto',
+      [theme.breakpoints.down('lg')]: {
+        width: '70%',
+      },
+      [theme.breakpoints.down('md')]: {
+        width: '100%',
+      },
     }),
     suggestedActionWrapper: css({
-      height: theme.spacing(6),
+      minHeight: theme.spacing(3),
       ['button']: {
         position: 'absolute',
         right: theme.spacing(2),
-        top: theme.spacing(7),
+        bottom: theme.spacing(2),
+      },
+      ['ul']: {
+        paddingLeft: theme.spacing(2),
       },
     }),
   };
