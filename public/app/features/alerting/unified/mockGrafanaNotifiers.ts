2209a42f6dc6cacc4ba44a068acfa03cb62a64b9
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-10-25 16:41:25 +0100

    Alerting: Fix template editing issues for contact points (#95387)

--- public/app/features/alerting/unified/mockGrafanaNotifiers.ts
+++ public/app/features/alerting/unified/mockGrafanaNotifiers.ts
@@ -289,7 +289,7 @@ export const grafanaAlertNotifiersMock: NotifierDTO[] = [
         label: 'Message',
         description:
           'Optional message. You can use templates to customize this field. Using a custom message will replace the default message',
-        placeholder: '',
+        placeholder: '{{ template "default.message" . }}',
         propertyName: 'message',
         selectOptions: null,
         showWhen: {
