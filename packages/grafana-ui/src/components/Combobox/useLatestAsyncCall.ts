b19976db8febb14784439d3d5631bb8ea323e2b8
Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
2024-10-25 10:10:02 +0200

    Combobox: Invalidate async calls using counter instead of value (#95049)
    * Combobox async: Invalidate using timestamp instead of value
    
    * Simulate keyboard delay in test
    
    * Use a counter instead of timestamps
    
    * Fix feedback
    
    * Expand tests to include case discussed on PR
    
    ---------
    
    Co-authored-by: Joao Silva <joao.silva@grafana.com>

--- packages/grafana-ui/src/components/Combobox/useLatestAsyncCall.ts
+++ packages/grafana-ui/src/components/Combobox/useLatestAsyncCall.ts
@@ -7,16 +7,17 @@ type AsyncFn<T, V> = (value: T) => Promise<V>;
  * Used to prevent a faster call being overwritten by an earlier slower call.
  */
 export function useLatestAsyncCall<T, V>(fn: AsyncFn<T, V>): AsyncFn<T, V> {
-  const latestValue = useRef<T>();
+  const latestValueCount = useRef<number>(0);
 
   const wrappedFn = useCallback(
     (value: T) => {
-      latestValue.current = value;
+      latestValueCount.current++;
+      const requestCount = latestValueCount.current;
 
       return new Promise<V>((resolve, reject) => {
         fn(value).then((result) => {
           // Only resolve if the value is still the latest
-          if (latestValue.current === value) {
+          if (requestCount === latestValueCount.current) {
             resolve(result);
           } else {
             reject(new StaleResultError());
