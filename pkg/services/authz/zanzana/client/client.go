be90f4317cee84ec56ae3b9b57e54d833f70e7a7
Karl Persson <kalle.persson@grafana.com>
2024-10-25 09:31:27 +0200

    Zanzana: Remove model and store initiation from client (#95328)
    * Remove model and store initiation from client

--- pkg/services/authz/zanzana/client/client.go
+++ pkg/services/authz/zanzana/client/client.go
@@ -6,14 +6,12 @@ import (
 	"fmt"
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
-	"github.com/openfga/language/pkg/go/transformer"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/grafana/pkg/services/authz/zanzana/schema"
 )
 
 var tracer = otel.Tracer("github.com/grafana/grafana/pkg/services/authz/zanzana/client")
@@ -32,16 +30,9 @@ func WithLogger(logger log.Logger) ClientOption {
 	}
 }
 
-func WithSchema(modules []transformer.ModuleFile) ClientOption {
-	return func(c *Client) {
-		c.modules = modules
-	}
-}
-
 type Client struct {
 	logger   log.Logger
 	client   openfgav1.OpenFGAServiceClient
-	modules  []transformer.ModuleFile
 	tenantID string
 	storeID  string
 	modelID  string
@@ -61,21 +52,17 @@ func New(ctx context.Context, cc grpc.ClientConnInterface, opts ...ClientOption)
 	}
 
 	if c.tenantID == "" {
-		c.tenantID = "stack-default"
+		c.tenantID = "stacks-default"
 	}
 
-	if len(c.modules) == 0 {
-		c.modules = schema.SchemaModules
-	}
-
-	store, err := c.getOrCreateStore(ctx, c.tenantID)
+	store, err := c.getStore(ctx, c.tenantID)
 	if err != nil {
 		return nil, err
 	}
 
 	c.storeID = store.GetId()
 
-	modelID, err := c.loadModel(ctx, c.storeID, c.modules)
+	modelID, err := c.loadModel(ctx, c.storeID)
 	if err != nil {
 		return nil, err
 	}
@@ -119,24 +106,6 @@ func (c *Client) Write(ctx context.Context, in *openfgav1.WriteRequest) error {
 	return err
 }
 
-func (c *Client) getOrCreateStore(ctx context.Context, name string) (*openfgav1.Store, error) {
-	store, err := c.getStore(ctx, name)
-
-	if errors.Is(err, errStoreNotFound) {
-		var res *openfgav1.CreateStoreResponse
-		res, err = c.client.CreateStore(ctx, &openfgav1.CreateStoreRequest{Name: name})
-		if res != nil {
-			store = &openfgav1.Store{
-				Id:        res.GetId(),
-				Name:      res.GetName(),
-				CreatedAt: res.GetCreatedAt(),
-			}
-		}
-	}
-
-	return store, err
-}
-
 var errStoreNotFound = errors.New("store not found")
 
 func (c *Client) getStore(ctx context.Context, name string) (*openfgav1.Store, error) {
@@ -170,52 +139,21 @@ func (c *Client) getStore(ctx context.Context, name string) (*openfgav1.Store, e
 	}
 }
 
-func (c *Client) loadModel(ctx context.Context, storeID string, modules []transformer.ModuleFile) (string, error) {
-	var continuationToken string
-
-	model, err := schema.TransformModulesToModel(modules)
-	if err != nil {
-		return "", err
-	}
-
-	for {
-		// ReadAuthorizationModels returns authorization models for a store sorted in descending order of creation.
-		// So with a pageSize of 1 we will get the latest model.
-		res, err := c.client.ReadAuthorizationModels(ctx, &openfgav1.ReadAuthorizationModelsRequest{
-			StoreId:           storeID,
-			PageSize:          &wrapperspb.Int32Value{Value: 20},
-			ContinuationToken: continuationToken,
-		})
-
-		if err != nil {
-			return "", fmt.Errorf("failed to load authorization model: %w", err)
-		}
-
-		for _, m := range res.GetAuthorizationModels() {
-			// If provided dsl is equal to a stored dsl we use that as the authorization id
-			if schema.EqualModels(m, model) {
-				return m.GetId(), nil
-			}
-		}
-
-		// If we have not found any matching authorization model we break the loop and create a new one
-		if res.GetContinuationToken() == "" {
-			break
-		}
-
-		continuationToken = res.GetContinuationToken()
-	}
-
-	writeRes, err := c.client.WriteAuthorizationModel(ctx, &openfgav1.WriteAuthorizationModelRequest{
-		StoreId:         c.storeID,
-		TypeDefinitions: model.GetTypeDefinitions(),
-		SchemaVersion:   model.GetSchemaVersion(),
-		Conditions:      model.GetConditions(),
+func (c *Client) loadModel(ctx context.Context, storeID string) (string, error) {
+	// ReadAuthorizationModels returns authorization models for a store sorted in descending order of creation.
+	// So with a pageSize of 1 we will get the latest model.
+	res, err := c.client.ReadAuthorizationModels(ctx, &openfgav1.ReadAuthorizationModelsRequest{
+		StoreId:  storeID,
+		PageSize: &wrapperspb.Int32Value{Value: 1},
 	})
 
 	if err != nil {
-		return "", fmt.Errorf("failed to load authorization model: %w", err)
+		return "", fmt.Errorf("failed to load latest authorization model: %w", err)
 	}
 
-	return writeRes.GetAuthorizationModelId(), nil
+	if len(res.AuthorizationModels) != 1 {
+		return "", fmt.Errorf("failed to load latest authorization model")
+	}
+
+	return res.AuthorizationModels[0].GetId(), nil
 }
