ee25b05fdf6d7396e5dc213caf0ee172558005d6
eledobleefe <laura.fernandez@grafana.com>
2024-10-25 14:05:05 +0200

    Fix search bar tracking

--- public/app/core/components/AppChrome/AppChromeService.tsx
+++ public/app/core/components/AppChrome/AppChromeService.tsx
@@ -198,7 +198,7 @@ export class AppChromeService {
 
     this.update({ searchBarHidden: newSearchBarHidden, kioskMode: null });
     reportInteraction('grafana_search_bar', {
-      show: newSearchBarHidden,
+      visible: !newSearchBarHidden,
     });
   };
 
