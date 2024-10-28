c4e960314e4955d8da86ac269dec7e26423747c9
Nick Richmond <nick.richmond@grafana.com>
2024-10-28 10:39:30 -0400

    fix: add missing space

--- public/app/features/trails/RelatedLogs/NoRelatedLogsFoundScene.tsx
+++ public/app/features/trails/RelatedLogs/NoRelatedLogsFoundScene.tsx
@@ -7,7 +7,7 @@ export class NoRelatedLogsScene extends SceneObjectBase<SceneObjectState> {
       <Stack direction="column" gap={1}>
         <Text color="warning">Related logs is an experimental feature.</Text>
         <Text>
-          Related logs are not available for this metric. Try selecting a metric created by a
+          Related logs are not available for this metric. Try selecting a metric created by a{' '}
           <TextLink external href="https://grafana.com/docs/loki/latest/alert/#recording-rules">
             Loki Recording Rule
           </TextLink>
