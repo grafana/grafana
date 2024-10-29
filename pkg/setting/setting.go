189802d3c3e0aa876d148e1f707b44afd5e3a563
Josh Hunt <joshhunt@users.noreply.github.com>
2024-10-29 18:20:54 +0000

    EchoSrv: Add BrowserConsoleBackend to log analytics events (#95554)
    * EchoSrv: Add BrowserConsoleBackend to log analytics events
    
    * rename config name
    
    * warn on non-scalar property values

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -382,6 +382,7 @@ type Cfg struct {
 	RudderstackConfigURL                string
 	RudderstackIntegrationsURL          string
 	IntercomSecret                      string
+	FrontendAnalyticsConsoleReporting   bool
 
 	// LDAP
 	LDAPAuthEnabled       bool
@@ -1158,6 +1159,7 @@ func (cfg *Cfg) parseINIFile(iniFile *ini.File) error {
 	cfg.RudderstackConfigURL = analytics.Key("rudderstack_config_url").String()
 	cfg.RudderstackIntegrationsURL = analytics.Key("rudderstack_integrations_url").String()
 	cfg.IntercomSecret = analytics.Key("intercom_secret").String()
+	cfg.FrontendAnalyticsConsoleReporting = analytics.Key("browser_console_reporter").MustBool(false)
 
 	cfg.ReportingEnabled = analytics.Key("reporting_enabled").MustBool(true)
 	cfg.ReportingDistributor = analytics.Key("reporting_distributor").MustString("grafana-labs")
