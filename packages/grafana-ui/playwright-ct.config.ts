8bde99f856eab3b8404aa2fa62aa462e3547f947
Tobias Skarhed <tobias.skarhed@gmail.com>
2024-10-29 10:13:42 +0100

    Configure running performance test

--- packages/grafana-ui/playwright-ct.config.ts
+++ packages/grafana-ui/playwright-ct.config.ts
@@ -29,19 +29,11 @@ export default defineConfig({
     ctPort: 3100,
   },
 
-  /* Configure projects for major browsers */
+  /* Configure projects for major browsers. Only use chromium, as we need access to CDP */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
   ],
 });
