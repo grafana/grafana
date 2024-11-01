adae4b9d78418964c66bc5cfdfee394de76f21c8
Dana Axinte <53751979+dana-axinte@users.noreply.github.com>
2024-11-01 05:09:50 -0400

    CloudMigrations: increase size of resource_uid column (#95684)
    * len 255
    
    * comment
    
    * Update pkg/services/sqlstore/migrations/cloud_migrations.go
    
    * comment on sqlite
    
    * postgres varchar
    
    ---------
    
    Co-authored-by: Michael Mandrus <41969079+mmandrus@users.noreply.github.com>

--- pkg/services/sqlstore/migrations/cloud_migrations.go
+++ pkg/services/sqlstore/migrations/cloud_migrations.go
@@ -176,4 +176,10 @@ func addCloudMigrationsMigrations(mg *Migrator) {
 		Type:     DB_Text,
 		Nullable: true,
 	}))
+
+	// -- increase the length of resource_uid column
+	// -- not needed in sqlite as type is TEXT with length defined by SQLITE_MAX_LENGTH preprocessor macro
+	mg.AddMigration("increase resource_uid column length", NewRawSQLMigration("").
+		Mysql("ALTER TABLE cloud_migration_resource MODIFY resource_uid NVARCHAR(255);").
+		Postgres("ALTER TABLE cloud_migration_resource ALTER COLUMN resource_uid TYPE VARCHAR(255);"))
 }
