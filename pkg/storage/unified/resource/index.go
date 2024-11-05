33d569455d342a814f1dec52fe859d39980b1e58
Scott Lepper <scott.lepper@gmail.com>
2024-11-05 17:02:13 -0500

    search: add OR filter for kinds (#95915)
    search: add OR filter for kinds

--- pkg/storage/unified/resource/index.go
+++ pkg/storage/unified/resource/index.go
@@ -269,7 +269,19 @@ func (i *Index) Search(ctx context.Context, request *SearchRequest) (*IndexResul
 		request.Limit = 10
 	}
 
-	query := bleve.NewQueryStringQuery(request.Query)
+	textQuery := bleve.NewQueryStringQuery(request.Query)
+	query := bleve.NewConjunctionQuery(textQuery)
+
+	if len(request.Kind) > 0 {
+		// apply OR condition filter for each kind ( dashboard, folder, etc )
+		orQuery := bleve.NewDisjunctionQuery()
+		for _, term := range request.Kind {
+			termQuery := bleve.NewTermQuery(term)
+			orQuery.AddQuery(termQuery)
+		}
+		query.AddQuery(orQuery)
+	}
+
 	req := bleve.NewSearchRequest(query)
 
 	for _, group := range request.GroupBy {
@@ -387,6 +399,12 @@ func fetchResourceTypes() []*ListOptions {
 			Group:    "folder.grafana.app",
 			Resource: "folders",
 		},
-	})
+	},
+		&ListOptions{
+			Key: &ResourceKey{
+				Group:    "dashboard.grafana.app",
+				Resource: "dashboards",
+			},
+		})
 	return items
 }
