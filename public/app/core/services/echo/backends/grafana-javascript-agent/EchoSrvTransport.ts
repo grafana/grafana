ed046c3224bfc4564eaa7aa6f809d5b9e491b3de
Elliot Kirk <elliot.kirk@grafana.com>
2024-11-05 10:30:46 -0800

    cleaning up

--- public/app/core/services/echo/backends/grafana-javascript-agent/EchoSrvTransport.ts
+++ public/app/core/services/echo/backends/grafana-javascript-agent/EchoSrvTransport.ts
@@ -1,11 +1,16 @@
 import { BaseTransport, TransportItem } from '@grafana/faro-core';
 import { getEchoSrv, EchoEventType, config } from '@grafana/runtime';
+
+interface EchoSrcTransportOptions {
+  ignoreUrls: RegExp[];
+}
+
 export class EchoSrvTransport extends BaseTransport {
   readonly name: string = 'EchoSrvTransport';
   readonly version: string = config.buildInfo.version;
   private ignoreUrls: RegExp[] = [];
 
-  constructor(private options?: { ignoreUrls: RegExp[] }) {
+  constructor(options?: EchoSrcTransportOptions) {
     super();
 
     this.ignoreUrls = options?.ignoreUrls ?? [];
