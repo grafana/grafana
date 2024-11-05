1225340463744e35e61a07c5d28d136052f2673c
Todd Treece <360020+toddtreece@users.noreply.github.com>
2024-11-05 17:45:09 -0500

    Playlist: Add mutation and validation (#95708)

--- apps/playlist/pkg/app/app.go
+++ apps/playlist/pkg/app/app.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 
 	"github.com/grafana/grafana-app-sdk/app"
-	"github.com/grafana/grafana-app-sdk/logging"
 	"github.com/grafana/grafana-app-sdk/operator"
 	"github.com/grafana/grafana-app-sdk/resource"
 	"github.com/grafana/grafana-app-sdk/simple"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/klog/v2"
 
 	playlistv0alpha1 "github.com/grafana/grafana/apps/playlist/pkg/apis/playlist/v0alpha1"
 	"github.com/grafana/grafana/apps/playlist/pkg/watchers"
@@ -38,13 +38,27 @@ func New(cfg app.Config) (app.App, error) {
 		KubeConfig: cfg.KubeConfig,
 		InformerConfig: simple.AppInformerConfig{
 			ErrorHandler: func(ctx context.Context, err error) {
-				logging.DefaultLogger.With("error", err).Error("Informer processing error")
+				klog.ErrorS(err, "Informer processing error")
 			},
 		},
 		ManagedKinds: []simple.AppManagedKind{
 			{
 				Kind:    playlistv0alpha1.PlaylistKind(),
 				Watcher: playlistWatcher,
+				Mutator: &simple.Mutator{
+					MutateFunc: func(ctx context.Context, req *app.AdmissionRequest) (*app.MutatingResponse, error) {
+						// modify req.Object if needed
+						return &app.MutatingResponse{
+							UpdatedObject: req.Object,
+						}, nil
+					},
+				},
+				Validator: &simple.Validator{
+					ValidateFunc: func(ctx context.Context, req *app.AdmissionRequest) error {
+						// do something here if needed
+						return nil
+					},
+				},
 			},
 		},
 	}
