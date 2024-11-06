e41b7f5bfb39921f4377c37ca3b8c443520e4f57
bergquist <carl.bergquist@gmail.com>
2024-11-06 14:09:33 +0100

    tests with exisiting labels
    Signed-off-by: bergquist <carl.bergquist@gmail.com>

--- pkg/tsdb/loki/scopes.go
+++ pkg/tsdb/loki/scopes.go
@@ -7,19 +7,23 @@ import (
 	"github.com/grafana/loki/v3/pkg/logql/syntax"
 )
 
+// ApplyScopes applies the given scope filters to the given raw expression.
 func ApplyScopes(rawExpr string, scopeFilters []models.ScopeFilter) (string, error) {
 	if len(scopeFilters) == 0 {
 		return rawExpr, nil
 	}
+
 	scopeMatchers, err := models.FiltersToMatchers(scopeFilters, nil)
 	if err != nil {
 		return "", fmt.Errorf("failed to convert filters to matchers: %w", err)
 	}
+
 	// Need WithoutValidation to allow empty `{}` expressions
 	syntaxTree, err := syntax.ParseExprWithoutValidation(rawExpr)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse raw expression: %w", err)
 	}
+
 	syntaxTree.Walk(func(e syntax.Expr) {
 		switch e := e.(type) {
 		case *syntax.MatchersExpr:
--- pkg/tsdb/loki/scopes_test.go
+++ pkg/tsdb/loki/scopes_test.go
@@ -74,6 +74,16 @@ func TestInjectScopesIntoLokiQuery(t *testing.T) {
 			expected:  `(count_over_time({cluster="us-central-1", namespace="default"} |= "error"[1m]) / count_over_time({cluster="us-central-1", namespace="default"}[1m]))`,
 			expectErr: false,
 		},
+		{
+			name:  "multi range metric query operation with existing label matchers",
+			query: `count_over_time({a="bar"} |= "error" [1m])/count_over_time({a="bar"} [1m])`,
+			scopeFilters: []models.ScopeFilter{
+				{Key: "cluster", Value: "us-central-1", Operator: models.FilterOperatorEquals},
+				{Key: "namespace", Value: "default", Operator: models.FilterOperatorEquals},
+			},
+			expected:  `(count_over_time({a="bar", cluster="us-central-1", namespace="default"} |= "error"[1m]) / count_over_time({a="bar", cluster="us-central-1", namespace="default"}[1m]))`,
+			expectErr: false,
+		},
 	}
 
 	for _, tt := range tests {
@@ -84,7 +94,7 @@ func TestInjectScopesIntoLokiQuery(t *testing.T) {
 				require.Error(t, err)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, tt.expected, expr)
+				require.Equal(t, tt.expected, expr, tt.name)
 			}
 		})
 	}
