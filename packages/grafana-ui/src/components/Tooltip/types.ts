83ce25590d43a761947beae1ec0b4d85fada6993
Kristina <kristina.durivage@grafana.com>
2024-11-07 11:37:49 -0600

    Canvas: Fix references for ResourcePicker (#95580)
    * useRef instead of create ref for functional components, pass function to close popper if needed
    
    * Cleanup, hide on save as well
    
    * add comment about when hidePopper is available to content

--- packages/grafana-ui/src/components/Tooltip/types.ts
+++ packages/grafana-ui/src/components/Tooltip/types.ts
@@ -7,8 +7,11 @@ export interface PopoverContentProps {
    * It will be removed in a future release.
    */
   updatePopperPosition?: () => void;
+
+  hidePopper?: () => void;
 }
 
+// hidePopper is only available to popover content when it is passed as a function
 export type PopoverContent = string | React.ReactElement | ((props: PopoverContentProps) => JSX.Element);
 
 export type TooltipPlacement = Placement | 'auto' | 'auto-start' | 'auto-end';
