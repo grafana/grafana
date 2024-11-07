37dc85539a280b31d13c94db7866611118509530
ismail simsek <ismailsimsek09@gmail.com>
2024-11-07 16:38:05 +0100

    Revert "Prometheus: Fix escaping of quotation marks for variables" (#96038)
    Revert "Prometheus: Fix escaping of quotation marks for variables (#93415)"
    
    This reverts commit 2bfdee73e367e92f63b526711994077e7a7757c2.

--- packages/grafana-prometheus/src/datasource.ts
+++ packages/grafana-prometheus/src/datasource.ts
@@ -640,7 +640,7 @@ export class PrometheusDatasource
 
     const labelFilters: QueryBuilderLabelFilter[] = options.filters.map((f) => ({
       label: f.key,
-      value: prometheusRegularEscape(f.value),
+      value: f.value,
       op: f.operator,
     }));
     const expr = promQueryModeller.renderLabels(labelFilters);
@@ -672,7 +672,7 @@ export class PrometheusDatasource
 
     const labelFilters: QueryBuilderLabelFilter[] = options.filters.map((f) => ({
       label: f.key,
-      value: prometheusRegularEscape(f.value),
+      value: f.value,
       op: f.operator,
     }));
 
@@ -1045,14 +1045,9 @@ export function extractRuleMappingFromGroups(groups: RawRecordingRules[]): RuleQ
 // in language_utils.ts, but they are not exactly the same algorithm, and we found
 // no way to reuse one in the another or vice versa.
 export function prometheusRegularEscape<T>(value: T) {
-  return typeof value === 'string' ? value.replace(/\\/g, '\\\\').replace(/"/g, '\\"') : value;
+  return typeof value === 'string' ? value.replace(/\\/g, '\\\\').replace(/'/g, "\\\\'") : value;
 }
 
 export function prometheusSpecialRegexEscape<T>(value: T) {
-  return typeof value === 'string'
-    ? value
-        .replace(/\\/g, '\\\\\\\\')
-        .replace(/"/g, '\\\\\\"')
-        .replace(/[$^*{}\[\]\'+?.()|]/g, '\\\\$&')
-    : value;
+  return typeof value === 'string' ? value.replace(/\\/g, '\\\\\\\\').replace(/[$^*{}\[\]\'+?.()|]/g, '\\\\$&') : value;
 }
