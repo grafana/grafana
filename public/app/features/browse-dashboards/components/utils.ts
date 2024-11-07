fd16711732b3d3e75aac34db69a1a8b5b3481466
Clarity-89 <homes89@ukr.net>
2024-11-07 15:35:03 +0200

    BrowseDashboards: Append orgId to folder URL

--- public/app/features/browse-dashboards/components/utils.ts
+++ public/app/features/browse-dashboards/components/utils.ts
@@ -1,4 +1,5 @@
 import { config } from '@grafana/runtime';
+import { contextSrv } from 'app/core/core';
 
 import { DashboardViewItemWithUIItems } from '../types';
 
@@ -8,4 +9,13 @@ export function makeRowID(baseId: string, item: DashboardViewItemWithUIItems) {
 
 export function isSharedWithMe(uid: string) {
   return uid === config.sharedWithMeFolderUID;
+}
+
+// Append orgId to the folder URL
+export function getFolderURL(url: string) {
+  const { orgId } = contextSrv.user;
+  if (orgId) {
+    return `${url}?orgId=${orgId}`;
+  }
+  return url;
 }
