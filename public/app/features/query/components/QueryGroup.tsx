ce3c1097bed5a9d3a5057d81a409912383d7426d
Clarity-89 <homes89@ukr.net>
2024-11-07 13:27:42 +0200

    Add memo

--- public/app/features/query/components/QueryGroup.tsx
+++ public/app/features/query/components/QueryGroup.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 
 import {
   CoreApp,
@@ -45,241 +45,245 @@ export interface Props {
 
 const dataSourceSrv = getDataSourceSrv();
 
-export const QueryGroup = ({ queryRunner, options, onOpenQueryInspector, onRunQueries, onOptionsChange }: Props) => {
-  const [dataSource, setDataSource] = useState<DataSourceApi>();
-  const [dsSettings, setDsSettings] = useState<DataSourceInstanceSettings>();
-  const [queries, setQueries] = useState<DataQuery[]>([]);
-  const [isHelpOpen, setIsHelpOpen] = useState(false);
-  const [defaultDataSource, setDefaultDataSource] = useState<DataSourceApi>();
-  const [data, setData] = useState<PanelData>({
-    state: LoadingState.NotStarted,
-    series: [],
-    timeRange: getDefaultTimeRange(),
-  });
-  const scrollElementRef = useRef<HTMLDivElement | null>(null);
-  const styles = useStyles(getStyles);
-
-  const setNewQueriesAndDatasource = useCallback(async (options: QueryGroupOptions) => {
-    try {
-      const ds = await dataSourceSrv.get(options.dataSource);
-      const dsSettings = dataSourceSrv.getInstanceSettings(options.dataSource);
-      const defaultDs = await dataSourceSrv.get();
-      const datasource = ds.getRef();
-      const newQueries = options.queries.map((q) => ({
-        ...(queryIsEmpty(q) && ds?.getDefaultQuery?.(CoreApp.PanelEditor)),
-        datasource,
-        ...q,
-      }));
-
-      setQueries(newQueries);
-      setDataSource(ds);
-      setDsSettings(dsSettings);
-      setDefaultDataSource(defaultDs);
-    } catch (error) {
-      console.error('failed to load data source', error);
-    }
-  }, []);
-
-  useEffect(() => {
-    const subscription = queryRunner.getData({ withTransforms: false, withFieldConfig: false }).subscribe({
-      next: (data: PanelData) => setData(data),
+export const QueryGroup = memo(
+  ({ queryRunner, options, onOpenQueryInspector, onRunQueries, onOptionsChange }: Props) => {
+    const [dataSource, setDataSource] = useState<DataSourceApi>();
+    const [dsSettings, setDsSettings] = useState<DataSourceInstanceSettings>();
+    const [queries, setQueries] = useState<DataQuery[]>([]);
+    const [isHelpOpen, setIsHelpOpen] = useState(false);
+    const [defaultDataSource, setDefaultDataSource] = useState<DataSourceApi>();
+    const [data, setData] = useState<PanelData>({
+      state: LoadingState.NotStarted,
+      series: [],
+      timeRange: getDefaultTimeRange(),
     });
+    const scrollElementRef = useRef<HTMLDivElement | null>(null);
+    const styles = useStyles(getStyles);
 
-    setNewQueriesAndDatasource(options);
+    const setNewQueriesAndDatasource = useCallback(async (options: QueryGroupOptions) => {
+      try {
+        const ds = await dataSourceSrv.get(options.dataSource);
+        const dsSettings = dataSourceSrv.getInstanceSettings(options.dataSource);
+        const defaultDs = await dataSourceSrv.get();
+        const datasource = ds.getRef();
+        const newQueries = options.queries.map((q) => ({
+          ...(queryIsEmpty(q) && ds?.getDefaultQuery?.(CoreApp.PanelEditor)),
+          datasource,
+          ...q,
+        }));
 
-    return () => subscription.unsubscribe();
-  }, [queryRunner, options, setNewQueriesAndDatasource]);
-
-  useEffect(() => {
-    const checkDataSource = async () => {
-      const currentDS = await getDataSourceSrv().get(options.dataSource);
-      if (dataSource && currentDS.uid !== dataSource.uid) {
-        setNewQueriesAndDatasource(options);
+        setQueries(newQueries);
+        setDataSource(ds);
+        setDsSettings(dsSettings);
+        setDefaultDataSource(defaultDs);
+      } catch (error) {
+        console.error('failed to load data source', error);
       }
-    };
-    checkDataSource();
-  }, [options, dataSource, setNewQueriesAndDatasource]);
+    }, []);
 
-  const onChange = useCallback(
-    (changedProps: Partial<QueryGroupOptions>) => {
-      onOptionsChange({
-        ...options,
-        ...changedProps,
+    useEffect(() => {
+      const subscription = queryRunner.getData({ withTransforms: false, withFieldConfig: false }).subscribe({
+        next: (data: PanelData) => setData(data),
       });
-    },
-    [options, onOptionsChange]
-  );
 
-  const onChangeDataSource = async (
-    newSettings: DataSourceInstanceSettings,
-    defaultQueries?: DataQuery[] | GrafanaQuery[]
-  ) => {
-    const currentDS = dsSettings ? await getDataSourceSrv().get(dsSettings.uid) : undefined;
-    const nextDS = await getDataSourceSrv().get(newSettings.uid);
-    const newQueries = defaultQueries || (await updateQueries(nextDS, newSettings.uid, queries, currentDS));
-    const newDataSource = await dataSourceSrv.get(newSettings.name);
+      setNewQueriesAndDatasource(options);
 
-    onChange({
-      queries: newQueries,
-      dataSource: {
-        name: newSettings.name,
-        uid: newSettings.uid,
-        ...getDataSourceRef(newSettings),
+      return () => subscription.unsubscribe();
+    }, [queryRunner, options, setNewQueriesAndDatasource]);
+
+    useEffect(() => {
+      const checkDataSource = async () => {
+        const currentDS = await getDataSourceSrv().get(options.dataSource);
+        if (dataSource && currentDS.uid !== dataSource.uid) {
+          setNewQueriesAndDatasource(options);
+        }
+      };
+      checkDataSource();
+    }, [options, dataSource, setNewQueriesAndDatasource]);
+
+    const onChange = useCallback(
+      (changedProps: Partial<QueryGroupOptions>) => {
+        onOptionsChange({
+          ...options,
+          ...changedProps,
+        });
       },
-    });
-
-    setQueries(newQueries);
-    setDataSource(newDataSource);
-    setDsSettings(newSettings);
-
-    if (defaultQueries) {
-      onRunQueries();
-    }
-  };
-
-  const newQuery = useCallback(() => {
-    const ds =
-      dsSettings && !dsSettings.meta.mixed
-        ? getDataSourceRef(dsSettings)
-        : defaultDataSource
-          ? defaultDataSource.getRef()
-          : { type: undefined, uid: undefined };
-
-    return {
-      ...dataSource?.getDefaultQuery?.(CoreApp.PanelEditor),
-      datasource: ds,
-    };
-  }, [dsSettings, defaultDataSource, dataSource]);
-
-  const onQueriesChange = useCallback(
-    (newQueries: DataQuery[] | GrafanaQuery[]) => {
-      onChange({ queries: newQueries });
-      setQueries(newQueries);
-    },
-    [onChange]
-  );
-
-  const onScrollBottom = useCallback(() => {
-    setTimeout(() => {
-      if (scrollElementRef.current) {
-        scrollElementRef.current.scrollTo({ top: 10000 });
-      }
-    }, 20);
-  }, []);
-
-  const onAddQuery = useCallback(
-    (query: Partial<DataQuery>) => {
-      const dsRef = dsSettings ? getDataSourceRef(dsSettings) : { type: undefined, uid: undefined };
-      onQueriesChange(addQuery(queries, query, dsRef));
-      onScrollBottom();
-    },
-    [dsSettings, queries, onQueriesChange, onScrollBottom]
-  );
-
-  const onAddQueryClick = useCallback(() => {
-    onQueriesChange(addQuery(queries, newQuery()));
-    onScrollBottom();
-  }, [queries, newQuery, onQueriesChange, onScrollBottom]);
-
-  const onAddExpressionClick = useCallback(() => {
-    onQueriesChange(addQuery(queries, expressionDatasource.newQuery()));
-    onScrollBottom();
-  }, [queries, onQueriesChange, onScrollBottom]);
-
-  const onUpdateAndRun = useCallback(
-    (options: QueryGroupOptions) => {
-      onOptionsChange(options);
-      onRunQueries();
-    },
-    [onOptionsChange, onRunQueries]
-  );
-
-  const isExpressionsSupported = (dsSettings: DataSourceInstanceSettings): boolean => {
-    return (dsSettings.meta.alerting || dsSettings.meta.mixed) === true;
-  };
-
-  const renderTopSection = () => {
-    if (!dsSettings || !dataSource) {
-      return null;
-    }
-    return (
-      <QueryGroupTopSection
-        data={data}
-        dataSource={dataSource}
-        options={options}
-        dsSettings={dsSettings}
-        onOptionsChange={onUpdateAndRun}
-        onDataSourceChange={onChangeDataSource}
-        onOpenQueryInspector={onOpenQueryInspector}
-      />
+      [options, onOptionsChange]
     );
-  };
 
-  const renderExtraActions = () => {
-    return GroupActionComponents.getAllExtraRenderAction()
-      .map((action, index) =>
-        action({
-          onAddQuery,
-          onChangeDataSource,
-          key: index,
-        })
-      )
-      .filter(Boolean);
-  };
+    const onChangeDataSource = async (
+      newSettings: DataSourceInstanceSettings,
+      defaultQueries?: DataQuery[] | GrafanaQuery[]
+    ) => {
+      const currentDS = dsSettings ? await getDataSourceSrv().get(dsSettings.uid) : undefined;
+      const nextDS = await getDataSourceSrv().get(newSettings.uid);
+      const newQueries = defaultQueries || (await updateQueries(nextDS, newSettings.uid, queries, currentDS));
+      const newDataSource = await dataSourceSrv.get(newSettings.name);
 
-  return (
-    <CustomScrollbar autoHeightMin="100%" scrollRefCallback={(el) => (scrollElementRef.current = el)}>
-      <div className={styles.innerWrapper}>
-        {renderTopSection()}
-        {dsSettings && (
-          <>
-            <div className={styles.queriesWrapper}>
-              <div aria-label={selectors.components.QueryTab.content}>
-                <QueryEditorRows
-                  queries={queries}
-                  dsSettings={dsSettings}
-                  onQueriesChange={onQueriesChange}
-                  onAddQuery={onAddQuery}
-                  onRunQueries={onRunQueries}
-                  data={data}
-                />
+      onChange({
+        queries: newQueries,
+        dataSource: {
+          name: newSettings.name,
+          uid: newSettings.uid,
+          ...getDataSourceRef(newSettings),
+        },
+      });
+
+      setQueries(newQueries);
+      setDataSource(newDataSource);
+      setDsSettings(newSettings);
+
+      if (defaultQueries) {
+        onRunQueries();
+      }
+    };
+
+    const newQuery = useCallback(() => {
+      const ds =
+        dsSettings && !dsSettings.meta.mixed
+          ? getDataSourceRef(dsSettings)
+          : defaultDataSource
+            ? defaultDataSource.getRef()
+            : { type: undefined, uid: undefined };
+
+      return {
+        ...dataSource?.getDefaultQuery?.(CoreApp.PanelEditor),
+        datasource: ds,
+      };
+    }, [dsSettings, defaultDataSource, dataSource]);
+
+    const onQueriesChange = useCallback(
+      (newQueries: DataQuery[] | GrafanaQuery[]) => {
+        onChange({ queries: newQueries });
+        setQueries(newQueries);
+      },
+      [onChange]
+    );
+
+    const onScrollBottom = useCallback(() => {
+      setTimeout(() => {
+        if (scrollElementRef.current) {
+          scrollElementRef.current.scrollTo({ top: 10000 });
+        }
+      }, 20);
+    }, []);
+
+    const onAddQuery = useCallback(
+      (query: Partial<DataQuery>) => {
+        const dsRef = dsSettings ? getDataSourceRef(dsSettings) : { type: undefined, uid: undefined };
+        onQueriesChange(addQuery(queries, query, dsRef));
+        onScrollBottom();
+      },
+      [dsSettings, queries, onQueriesChange, onScrollBottom]
+    );
+
+    const onAddQueryClick = useCallback(() => {
+      onQueriesChange(addQuery(queries, newQuery()));
+      onScrollBottom();
+    }, [queries, newQuery, onQueriesChange, onScrollBottom]);
+
+    const onAddExpressionClick = useCallback(() => {
+      onQueriesChange(addQuery(queries, expressionDatasource.newQuery()));
+      onScrollBottom();
+    }, [queries, onQueriesChange, onScrollBottom]);
+
+    const onUpdateAndRun = useCallback(
+      (options: QueryGroupOptions) => {
+        onOptionsChange(options);
+        onRunQueries();
+      },
+      [onOptionsChange, onRunQueries]
+    );
+
+    const isExpressionsSupported = (dsSettings: DataSourceInstanceSettings): boolean => {
+      return (dsSettings.meta.alerting || dsSettings.meta.mixed) === true;
+    };
+
+    const renderTopSection = () => {
+      if (!dsSettings || !dataSource) {
+        return null;
+      }
+      return (
+        <QueryGroupTopSection
+          data={data}
+          dataSource={dataSource}
+          options={options}
+          dsSettings={dsSettings}
+          onOptionsChange={onUpdateAndRun}
+          onDataSourceChange={onChangeDataSource}
+          onOpenQueryInspector={onOpenQueryInspector}
+        />
+      );
+    };
+
+    const renderExtraActions = () => {
+      return GroupActionComponents.getAllExtraRenderAction()
+        .map((action, index) =>
+          action({
+            onAddQuery,
+            onChangeDataSource,
+            key: index,
+          })
+        )
+        .filter(Boolean);
+    };
+
+    return (
+      <CustomScrollbar autoHeightMin="100%" scrollRefCallback={(el) => (scrollElementRef.current = el)}>
+        <div className={styles.innerWrapper}>
+          {renderTopSection()}
+          {dsSettings && (
+            <>
+              <div className={styles.queriesWrapper}>
+                <div aria-label={selectors.components.QueryTab.content}>
+                  <QueryEditorRows
+                    queries={queries}
+                    dsSettings={dsSettings}
+                    onQueriesChange={onQueriesChange}
+                    onAddQuery={onAddQuery}
+                    onRunQueries={onRunQueries}
+                    data={data}
+                  />
+                </div>
               </div>
-            </div>
-            <Stack gap={2} alignItems="flex-start">
-              {!isSharedDashboardQuery(dsSettings.name) && (
-                <Button
-                  icon="plus"
-                  onClick={onAddQueryClick}
-                  variant="secondary"
-                  data-testid={selectors.components.QueryTab.addQuery}
-                >
-                  Add query
-                </Button>
+              <Stack gap={2} alignItems="flex-start">
+                {!isSharedDashboardQuery(dsSettings.name) && (
+                  <Button
+                    icon="plus"
+                    onClick={onAddQueryClick}
+                    variant="secondary"
+                    data-testid={selectors.components.QueryTab.addQuery}
+                  >
+                    Add query
+                  </Button>
+                )}
+                {config.expressionsEnabled && isExpressionsSupported(dsSettings) && (
+                  <Button
+                    icon="plus"
+                    onClick={onAddExpressionClick}
+                    variant="secondary"
+                    className={styles.expressionButton}
+                    data-testid="query-tab-add-expression"
+                  >
+                    <span>Expression&nbsp;</span>
+                  </Button>
+                )}
+                {renderExtraActions()}
+              </Stack>
+              {isHelpOpen && (
+                <Modal title="Data source help" isOpen={true} onDismiss={() => setIsHelpOpen(false)}>
+                  <PluginHelp pluginId={dsSettings.meta.id} />
+                </Modal>
               )}
-              {config.expressionsEnabled && isExpressionsSupported(dsSettings) && (
-                <Button
-                  icon="plus"
-                  onClick={onAddExpressionClick}
-                  variant="secondary"
-                  className={styles.expressionButton}
-                  data-testid="query-tab-add-expression"
-                >
-                  <span>Expression&nbsp;</span>
-                </Button>
-              )}
-              {renderExtraActions()}
-            </Stack>
-            {isHelpOpen && (
-              <Modal title="Data source help" isOpen={true} onDismiss={() => setIsHelpOpen(false)}>
-                <PluginHelp pluginId={dsSettings.meta.id} />
-              </Modal>
-            )}
-          </>
-        )}
-      </div>
-    </CustomScrollbar>
-  );
-};
+            </>
+          )}
+        </div>
+      </CustomScrollbar>
+    );
+  }
+);
+
+QueryGroup.displayName = 'QueryGroup';
 
 const getStyles = (theme: GrafanaTheme) => {
   return {
