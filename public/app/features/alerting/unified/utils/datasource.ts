774bceb99afedca1778faa91df54b69d932fd135
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-11-08 17:55:46 +0000

    Add additional checks for whether to show grafana AM

--- public/app/features/alerting/unified/utils/datasource.ts
+++ public/app/features/alerting/unified/utils/datasource.ts
@@ -1,6 +1,8 @@
 import { DataSourceInstanceSettings, DataSourceJsonData, DataSourceSettings } from '@grafana/data';
 import { getDataSourceSrv } from '@grafana/runtime';
 import { contextSrv } from 'app/core/services/context_srv';
+import { PERMISSIONS_TIME_INTERVALS } from 'app/features/alerting/unified/components/mute-timings/permissions';
+import { PERMISSIONS_NOTIFICATION_POLICIES } from 'app/features/alerting/unified/components/notification-policies/permissions';
 import {
   AlertManagerDataSourceJsonData,
   AlertManagerImplementation,
@@ -151,6 +153,8 @@ export function getAlertManagerDataSourcesByPermission(permission: 'instance' | 
   const builtinAlertmanagerPermissions = [
     ...Object.values(permissions).flatMap((permissions) => permissions.grafana),
     ...PERMISSIONS_CONTACT_POINTS,
+    ...PERMISSIONS_TIME_INTERVALS,
+    ...PERMISSIONS_NOTIFICATION_POLICIES,
   ];
 
   const hasPermissionsForInternalAlertmanager = builtinAlertmanagerPermissions.some((permission) =>
