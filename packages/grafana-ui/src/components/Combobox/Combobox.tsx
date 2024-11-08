7f16bbc33e567e2f0c1c7e3bb33f90d04c1c32a9
eledobleefe <laura.fernandez@grafana.com>
2024-11-08 12:49:33 +0100

    Comment the description for now in custom values

--- packages/grafana-ui/src/components/Combobox/Combobox.tsx
+++ packages/grafana-ui/src/components/Combobox/Combobox.tsx
@@ -189,8 +189,10 @@ export const Combobox = <T extends string | number>({
         items.findIndex((opt) => opt.label === inputValue || opt.value === inputValue) === -1
           ? {
               // Type casting needed to make this work when T is a number
-              value: inputValue as unknown as T,
+              value: (t('combobox.custom-value.label', 'Custom value: ') + inputValue) as unknown as T,
+              /* TODO: Add this back when we do support descriptions and have need for it
               description: t('combobox.custom-value.create', 'Create custom value'),
+              */
             }
           : null;
 
