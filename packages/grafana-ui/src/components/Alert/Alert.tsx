a2d09f5c669329adde5ba3be468e395a5fbffb9b
Ashley Harrison <ashley.harrison@grafana.com>
2024-11-12 10:54:06 +0000

    remove flexGrow on Alert

--- packages/grafana-ui/src/components/Alert/Alert.tsx
+++ packages/grafana-ui/src/components/Alert/Alert.tsx
@@ -133,7 +133,6 @@ const getStyles = (
 
   return {
     wrapper: css({
-      flexGrow: 1,
       marginBottom: theme.spacing(bottomSpacing ?? 2),
       marginTop: theme.spacing(topSpacing ?? 0),
       position: 'relative',
