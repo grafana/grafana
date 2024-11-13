3d8758222eb771efb1d8701a13fee6f9cd5d2d73
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2024-11-13 21:47:25 +0000

    fix config summary keys

--- pkg/tsdb/grafana-postgresql-datasource/sqleng/handler_checkhealth.go
+++ pkg/tsdb/grafana-postgresql-datasource/sqleng/handler_checkhealth.go
@@ -26,37 +26,37 @@ func (e *DataSourceHandler) CheckHealth(ctx context.Context, req *backend.CheckH
 
 func logCheckHealthError(dsInfo DataSourceInfo, err error, logger log.Logger) {
 	configSummary := map[string]any{
-		"config_url_length":                    len(dsInfo.URL),
-		"config_user_length":                   len(dsInfo.User),
-		"config_database_length":               len(dsInfo.Database),
-		"config_max_open_conns":                dsInfo.JsonData.MaxOpenConns,
-		"config_max_idle_conns":                dsInfo.JsonData.MaxIdleConns,
-		"config_conn_max_life_time":            dsInfo.JsonData.ConnMaxLifetime,
-		"config_conn_timeout":                  dsInfo.JsonData.ConnectionTimeout,
-		"config_timescaledb":                   dsInfo.JsonData.Timescaledb,
-		"config_ssl_mode":                      dsInfo.JsonData.Mode,
-		"config_tls_configuration_method":      dsInfo.JsonData.ConfigurationMethod,
-		"config_tls_skip_verify":               dsInfo.JsonData.TlsSkipVerify,
-		"config_ssl_root_cert_file_length":     len(dsInfo.JsonData.RootCertFile),
-		"config_ssl_cert_file_length":          len(dsInfo.JsonData.CertFile),
-		"config_ssl_key_file_length":           len(dsInfo.JsonData.CertKeyFile),
-		"config_timezone":                      dsInfo.JsonData.Timezone,
-		"config_encrypt":                       len(dsInfo.JsonData.Encrypt),
-		"config_server_name":                   len(dsInfo.JsonData.Servername),
-		"config_time_interval":                 dsInfo.JsonData.TimeInterval,
-		"config_database":                      len(dsInfo.JsonData.Database),
-		"config_enable_secure_proxy":           dsInfo.JsonData.SecureDSProxy,
-		"config_allow_clear_text_passwords":    dsInfo.JsonData.AllowCleartextPasswords,
-		"config_authentication_type":           dsInfo.JsonData.AuthenticationType,
-		"secure_config_password_length":        len(dsInfo.DecryptedSecureJSONData["password"]),
-		"secure_config_tls_ca_cert_length":     len(dsInfo.DecryptedSecureJSONData["tlsCACert"]),
-		"secure_config_tls_client_cert_length": len(dsInfo.DecryptedSecureJSONData["tlsClientCert"]),
-		"secure_config_tls_client_key_length":  len(dsInfo.DecryptedSecureJSONData["tlsClientKey"]),
+		"config_url_length":                 len(dsInfo.URL),
+		"config_user_length":                len(dsInfo.User),
+		"config_database_length":            len(dsInfo.Database),
+		"config_json_data_database_length":  len(dsInfo.JsonData.Database),
+		"config_max_open_conns":             dsInfo.JsonData.MaxOpenConns,
+		"config_max_idle_conns":             dsInfo.JsonData.MaxIdleConns,
+		"config_conn_max_life_time":         dsInfo.JsonData.ConnMaxLifetime,
+		"config_conn_timeout":               dsInfo.JsonData.ConnectionTimeout,
+		"config_timescaledb":                dsInfo.JsonData.Timescaledb,
+		"config_ssl_mode":                   dsInfo.JsonData.Mode,
+		"config_tls_configuration_method":   dsInfo.JsonData.ConfigurationMethod,
+		"config_tls_skip_verify":            dsInfo.JsonData.TlsSkipVerify,
+		"config_timezone":                   dsInfo.JsonData.Timezone,
+		"config_time_interval":              dsInfo.JsonData.TimeInterval,
+		"config_enable_secure_proxy":        dsInfo.JsonData.SecureDSProxy,
+		"config_allow_clear_text_passwords": dsInfo.JsonData.AllowCleartextPasswords,
+		"config_authentication_type":        dsInfo.JsonData.AuthenticationType,
+		"config_ssl_root_cert_file_length":  len(dsInfo.JsonData.RootCertFile),
+		"config_ssl_cert_file_length":       len(dsInfo.JsonData.CertFile),
+		"config_ssl_key_file_length":        len(dsInfo.JsonData.CertKeyFile),
+		"config_encrypt_length":             len(dsInfo.JsonData.Encrypt),
+		"config_server_name_length":         len(dsInfo.JsonData.Servername),
+		"config_password_length":            len(dsInfo.DecryptedSecureJSONData["password"]),
+		"config_tls_ca_cert_length":         len(dsInfo.DecryptedSecureJSONData["tlsCACert"]),
+		"config_tls_client_cert_length":     len(dsInfo.DecryptedSecureJSONData["tlsClientCert"]),
+		"config_tls_client_key_length":      len(dsInfo.DecryptedSecureJSONData["tlsClientKey"]),
 	}
 	configSummaryJson, marshalError := json.Marshal(configSummary)
 	if marshalError != nil {
 		logger.Error("Check health failed", "error", err, "message_type", "ds_config_health_check_error", "plugin_id", "grafana-postgresql-datasource")
 		return
 	}
-	logger.Error("Check health failed", "error", err, "message_type", "ds_config_health_check_detailed_error", "plugin_id", "grafana-postgresql-datasource", "details", string(configSummaryJson))
+	logger.Error("Check health failed", "error", err, "message_type", "ds_config_health_check_error_detailed", "plugin_id", "grafana-postgresql-datasource", "details", string(configSummaryJson))
 }
