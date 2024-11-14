192423956b744feb8ae120e9bd5083b312642b96
Ashley Harrison <ashley.harrison@grafana.com>
2024-11-14 12:06:21 +0000

    GrafanaUI: Mark `ScrollContainer` as stable and deprecate `CustomScrollbar` (#96331)
    * deprecate CustomScrollbar, promote ScrollContainer
    
    * add documentation start
    
    * use the properly exported ScrollContainer
    
    * Update packages/grafana-ui/src/components/ScrollContainer/ScrollContainer.mdx
    
    Co-authored-by: Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>

--- public/app/features/dashboard/components/SubMenu/DashboardLinksDashboard.tsx
+++ public/app/features/dashboard/components/SubMenu/DashboardLinksDashboard.tsx
@@ -6,9 +6,8 @@ import { GrafanaTheme2, ScopedVars } from '@grafana/data';
 import { sanitize, sanitizeUrl } from '@grafana/data/src/text/sanitize';
 import { selectors } from '@grafana/e2e-selectors';
 import { DashboardLink } from '@grafana/schema';
-import { Dropdown, Icon, Button, Menu, useStyles2 } from '@grafana/ui';
+import { Dropdown, Icon, Button, Menu, ScrollContainer, useStyles2 } from '@grafana/ui';
 import { ButtonLinkProps, LinkButton } from '@grafana/ui/src/components/Button';
-import { ScrollContainer } from '@grafana/ui/src/unstable';
 import { getBackendSrv } from 'app/core/services/backend_srv';
 import { DashboardSearchItem } from 'app/features/search/types';
 
