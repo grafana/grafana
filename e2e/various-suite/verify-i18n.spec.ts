46e6bd73f1ad6940271a3a4377372eaaff8a358a
Ashley Harrison <ashley.harrison@grafana.com>
2024-11-15 14:33:34 +0000

    fix pseudo locale and add simple e2e test

---
+++ e2e/various-suite/verify-i18n.spec.ts
@@ -0,0 +1,35 @@
+import { e2e } from '../utils';
+
+describe('Verify i18n', () => {
+  beforeEach(() => {
+    e2e.flows.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'));
+  });
+
+  const languageMap = {
+    Deutsch: 'Sprache',
+    English: 'Language',
+    Español: 'Idioma',
+    Français: 'Langue',
+    'Português Brasileiro': 'Idioma',
+    '中文（简体）': '语言',
+    'Pseudo-locale': 'Ŀäŉģūäģę',
+  };
+
+  it('loads all the languages correctly', () => {
+    cy.intercept('GET', '/api/user/preferences').as('preferences');
+    cy.visit('/profile');
+
+    cy.wrap(Object.entries(languageMap)).each(([language, label]: [string, string]) => {
+      cy.wait('@preferences');
+      cy.wait('@preferences');
+      cy.wait('@preferences');
+      cy.wait('@preferences');
+      cy.wait('@preferences');
+      cy.wait('@preferences');
+      cy.get('[id="locale-select"]').click();
+      cy.get('[id="locale-select"]').type(language).type('{downArrow}{enter}');
+      e2e.components.UserProfile.preferencesSaveButton().click();
+      cy.contains('label', label).should('be.visible');
+    });
+  });
+});
