14efac9a2e2d23ee3d77a0d86e62a37009c5d5ea
Dominik Prokop <dominik.prokop@grafana.com>
2024-11-15 15:36:48 +0100

    structure apic

---
+++ public/app/features/dashboard/api/v1.ts
@@ -0,0 +1,95 @@
+import { ScopedResourceClient } from 'app/features/apiserver/client';
+import {
+  ResourceClient,
+  ResourceForCreate,
+  AnnoKeyMessage,
+  AnnoKeyFolder,
+  Resource,
+} from 'app/features/apiserver/types';
+import { DeleteDashboardResponse } from 'app/features/manage-dashboards/types';
+import { DashboardDataDTO, DashboardDTO, SaveDashboardResponseDTO } from 'app/types';
+
+import { SaveDashboardCommand } from '../components/SaveDashboard/types';
+
+import { DashboardAPI, DashboardWithAccessInfo } from './types';
+
+export class K8sDashboardAPI implements DashboardAPI<DashboardDataDTO> {
+  private client: ResourceClient<DashboardDataDTO>;
+
+  constructor() {
+    this.client = new ScopedResourceClient<DashboardDataDTO>({
+      group: 'dashboard.grafana.app',
+      version: 'v0alpha1',
+      resource: 'dashboards',
+    });
+  }
+
+  saveDashboard(options: SaveDashboardCommand): Promise<SaveDashboardResponseDTO> {
+    const dashboard = options.dashboard as DashboardDataDTO; // type for the uid property
+    const obj: ResourceForCreate<DashboardDataDTO> = {
+      metadata: {
+        ...options?.k8s,
+      },
+      spec: {
+        ...dashboard,
+      },
+    };
+
+    if (options.message) {
+      obj.metadata.annotations = {
+        ...obj.metadata.annotations,
+        [AnnoKeyMessage]: options.message,
+      };
+    } else if (obj.metadata.annotations) {
+      delete obj.metadata.annotations[AnnoKeyMessage];
+    }
+
+    if (options.folderUid) {
+      obj.metadata.annotations = {
+        ...obj.metadata.annotations,
+        [AnnoKeyFolder]: options.folderUid,
+      };
+    }
+
+    if (dashboard.uid) {
+      obj.metadata.name = dashboard.uid;
+      return this.client.update(obj).then((v) => this.asSaveDashboardResponseDTO(v));
+    }
+    return this.client.create(obj).then((v) => this.asSaveDashboardResponseDTO(v));
+  }
+
+  asSaveDashboardResponseDTO(v: Resource<DashboardDataDTO>): SaveDashboardResponseDTO {
+    return {
+      uid: v.metadata.name,
+      version: v.spec.version ?? 0,
+      id: v.spec.id ?? 0,
+      status: 'success',
+      slug: '',
+      url: '',
+    };
+  }
+
+  deleteDashboard(uid: string, showSuccessAlert: boolean): Promise<DeleteDashboardResponse> {
+    return this.client.delete(uid).then((v) => ({
+      id: 0,
+      message: v.message,
+      title: 'deleted',
+    }));
+  }
+
+  async getDashboardDTO(uid: string) {
+    const dash = await this.client.subresource<DashboardWithAccessInfo<DashboardDataDTO>>(uid, 'dto');
+    const result: DashboardDTO<DashboardDataDTO> = {
+      meta: {
+        ...dash.access,
+        isNew: false,
+        isFolder: false,
+        uid: dash.metadata.name,
+        k8s: dash.metadata,
+      },
+      dashboard: dash,
+    };
+
+    return result;
+  }
+}
---
+++ public/app/features/dashboard/api/v2.ts
@@ -0,0 +1,81 @@
+import { UrlQueryMap } from '@grafana/data';
+import { DashboardCursorSync, DashboardSpec } from '@grafana/schema/dist/esm/schema/dashboard/v2alpha0/dashboard.gen';
+import { ScopedResourceClient } from 'app/features/apiserver/client';
+import { ResourceClient } from 'app/features/apiserver/types';
+import { DeleteDashboardResponse } from 'app/features/manage-dashboards/types';
+import { DashboardDTO, SaveDashboardResponseDTO } from 'app/types';
+
+import { SaveDashboardCommand } from '../components/SaveDashboard/types';
+
+import { DashboardAPI, DashboardWithAccessInfo } from './types';
+
+export class K8sDashboardV2APIStub implements DashboardAPI<DashboardSpec> {
+  // @ts-ignore
+  private client: ResourceClient<DashboardSpec>;
+
+  constructor() {
+    this.client = new ScopedResourceClient<DashboardSpec>({
+      group: 'dashboard.grafana.app',
+      version: 'v2alpha1',
+      resource: 'dashboards',
+    });
+  }
+
+  getDashboardDTO(uid: string, params?: UrlQueryMap) {
+    const spec: DashboardSpec = {
+      annotations: [],
+      cursorSync: DashboardCursorSync.Off,
+      title: 'Random dash',
+      description: '',
+      elements: {},
+      layout: {
+        kind: 'GridLayout',
+        spec: {
+          items: [],
+        },
+      },
+      links: [],
+      variables: [],
+      preload: false,
+      // @ts-ignore
+      timeSettings: {
+        from: 'now-6h',
+        to: 'now',
+      },
+    };
+
+    const resultResource: DashboardWithAccessInfo<DashboardSpec> = {
+      kind: 'DashboardWithAccessInfo',
+      apiVersion: 'dashboard.grafana.app/v2alpha1',
+      spec,
+      metadata: {
+        name: uid,
+        namespace: 'default',
+        resourceVersion: '24600000004',
+        creationTimestamp: '2024-03-20T16:41:16Z',
+      },
+      // will be provided by the subresource call
+      access: {},
+    };
+
+    const result: DashboardDTO<DashboardSpec> = {
+      // redirectUri: '',
+      meta: {
+        isNew: false,
+        isFolder: false,
+        uid: uid,
+      },
+      dashboard: resultResource,
+    };
+
+    return Promise.resolve(result);
+  }
+
+  deleteDashboard(uid: string, showSuccessAlert: boolean): Promise<DeleteDashboardResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  saveDashboard(options: SaveDashboardCommand): Promise<SaveDashboardResponseDTO> {
+    throw new Error('Method not implemented.');
+  }
+}
