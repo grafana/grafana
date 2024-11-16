9564eff238976aac13d91d7db824bb485210caf3
Ryan McKinley <ryantxu@gmail.com>
2024-11-16 15:50:28 +0300

    use json encoding

--- pkg/storage/unified/resource/table.go
+++ pkg/storage/unified/resource/table.go
@@ -1,16 +1,22 @@
 package resource
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	reflect "reflect"
 	"strconv"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/grafana/grafana-plugin-sdk-go/data/utils/jsoniter"
 )
 
-// Convert the the protobuf model to k8s table format
-func (x *ResourceTable) ToK8s(parseCells bool) (metav1.Table, error) {
+// Convert the the protobuf model into k8s (will decode each value)
+func (x *ResourceTable) ToK8s() (metav1.Table, error) {
 	table := metav1.Table{
 		ListMeta: metav1.ListMeta{
 			Continue: x.NextPageToken,
@@ -23,19 +29,21 @@ func (x *ResourceTable) ToK8s(parseCells bool) (metav1.Table, error) {
 		table.ResourceVersion = strconv.FormatInt(x.ResourceVersion, 10)
 	}
 
-	var err error
 	columnCount := len(x.Columns)
-	converter := make([]func(v []byte) (any, error), columnCount)
+	columns := make([]ResourceTableColumn, columnCount)
 	table.ColumnDefinitions = make([]metav1.TableColumnDefinition, columnCount)
-	for i, column := range x.Columns {
-		table.ColumnDefinitions[i], converter[i] = column.ToK8s()
-		if !parseCells && column.Type != ResourceTableColumnDefinition_STRING {
-			converter[i] = func(v []byte) (any, error) {
-				return json.RawMessage(v), nil
-			} // keep it as JSON
+	for i, c := range x.Columns {
+		columns[i] = *NewResourceTableColumn(c, i)
+		table.ColumnDefinitions[i] = metav1.TableColumnDefinition{
+			Name:        c.Name,
+			Description: c.Description,
+			Priority:    c.Priority,
+			Type:        columns[i].OpenAPIType,
+			Format:      columns[i].OpenAPIFormat,
 		}
 	}
 
+	var err error
 	table.Rows = make([]metav1.TableRow, len(x.Rows))
 	for i, r := range x.Rows {
 		row := metav1.TableRow{
@@ -46,9 +54,9 @@ func (x *ResourceTable) ToK8s(parseCells bool) (metav1.Table, error) {
 		}
 
 		for j, v := range r.Cells {
-			row.Cells[j], err = converter[j](v)
+			row.Cells[j], err = columns[j].Decode(v)
 			if err != nil {
-				return table, fmt.Errorf("error converting (row=%d, column=%d) %w", i, j, err)
+				return table, fmt.Errorf("error decoding (row=%d, column=%d) %w", i, j, err)
 			}
 		}
 
@@ -78,158 +86,302 @@ func (x *ResourceTable) ToK8s(parseCells bool) (metav1.Table, error) {
 	return table, err
 }
 
-func (x *ResourceTable) AddRow(key *ResourceKey, rv int64, values []any, obj runtime.Object) error {
+type TableBuilder struct {
+	ResourceTable
+
+	lookup map[string]*ResourceTableColumn
+
+	// Just keep track of it
+	hasDuplicateNames bool
+
+	// When add row gets a colum
+	ignoreUnknownColumns bool
+}
+
+func NewTableBuilder(cols []*ResourceTableColumnDefinition) *TableBuilder {
+	table := &TableBuilder{
+		ResourceTable: ResourceTable{
+			Columns: cols,
+		},
+
+		lookup: make(map[string]*ResourceTableColumn, len(cols)),
+
+		// defaults
+		ignoreUnknownColumns: false,
+	}
+	for i, v := range cols {
+		if table.lookup[v.Name] != nil {
+			table.hasDuplicateNames = true
+			continue
+		}
+		table.lookup[v.Name] = NewResourceTableColumn(v, i)
+	}
+	return table
+}
+
+func (x *TableBuilder) AddRow(key *ResourceKey, rv int64, vals map[string]any) error {
 	row := &ResourceTableRow{
 		Key:             key,
 		ResourceVersion: rv,
-	}
-	if x.Columns != nil && len(x.Columns) != len(values) {
-		return fmt.Errorf("wrong length")
-	}
-	err := row.SetCells(values)
-	if err != nil {
-		return err
+		Cells:           make([][]byte, len(x.Columns)),
 	}
 
-	if obj != nil {
-		row.Object, err = json.Marshal(obj)
+	for k, v := range vals {
+		column, ok := x.lookup[k]
+		if !ok {
+			if x.ignoreUnknownColumns {
+				continue
+			}
+			return fmt.Errorf("unknown column: " + k)
+		}
+		b, err := column.Encode(v)
 		if err != nil {
 			return err
 		}
+		row.Cells[column.index] = b
 	}
 
-	// finally add the row
 	x.Rows = append(x.Rows, row)
-	return err
+	return nil
 }
 
-func (x *ResourceTableColumnDefinition) ToK8s() (metav1.TableColumnDefinition, func([]byte) (any, error)) {
-	def := metav1.TableColumnDefinition{
-		Name:        x.Name,
-		Description: x.Description,
-		Priority:    x.Priority,
-	}
-	converter := func(v []byte) (any, error) { return json.RawMessage(v), nil } // noop
+type ResourceTableColumn struct {
+	def   *ResourceTableColumnDefinition
+	index int
 
-	switch x.Type {
-	case ResourceTableColumnDefinition_UNKNOWN_TYPE:
+	reader func(iter *jsoniter.Iterator) (any, error)
+	writer func(v any, stream *jsoniter.Stream) error
+
+	OpenAPIType   string
+	OpenAPIFormat string
+}
+
+func NewResourceTableColumn(def *ResourceTableColumnDefinition, index int) *ResourceTableColumn {
+	col := &ResourceTableColumn{def: def, index: index}
+
+	// Initially ignore the array property, we wil wrap that at the end
+	switch def.Type {
+	case ResourceTableColumnDefinition_UNKNOWN_TYPE: // left as null
 	case ResourceTableColumnDefinition_STRING:
-		def.Type = "string"
-		converter = func(v []byte) (any, error) {
-			return string(v), nil
+		col.OpenAPIType = "string"
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.ReadString()
 		}
 
 	case ResourceTableColumnDefinition_BOOLEAN:
-		def.Type = "boolean"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
-			}
-			b := false
-			err := json.Unmarshal(v, &b)
-			return b, err
+		col.OpenAPIType = "boolean"
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.ReadBool()
 		}
 
 	case ResourceTableColumnDefinition_INT32:
-		def.Type = "number"
-		def.Format = "int32"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
-			}
-			b := int32(0)
-			err := json.Unmarshal(v, &b)
-			return b, err
+		col.OpenAPIType = "number"
+		col.OpenAPIFormat = "int32"
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.ReadInt32()
 		}
 
 	case ResourceTableColumnDefinition_INT64:
-		def.Type = "number"
-		def.Format = "int64"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
-			}
-			b := int64(0)
-			err := json.Unmarshal(v, &b)
-			return b, err
+		col.OpenAPIType = "number"
+		col.OpenAPIFormat = "int64"
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.ReadInt64()
 		}
 
 	case ResourceTableColumnDefinition_DOUBLE:
-		def.Type = "number"
-		def.Format = "double"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
-			}
-			b := float64(0)
-			err := json.Unmarshal(v, &b)
-			return b, err
+		col.OpenAPIType = "number"
+		col.OpenAPIFormat = "double"
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.ReadFloat64()
 		}
 
 	case ResourceTableColumnDefinition_FLOAT:
-		def.Type = "number"
-		def.Format = "float"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
-			}
-			b := float32(0)
-			err := json.Unmarshal(v, &b)
-			return b, err
+		col.OpenAPIType = "number"
+		col.OpenAPIFormat = "float"
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.ReadFloat32()
 		}
 
-	case ResourceTableColumnDefinition_DATE:
-		def.Type = "string"
-		def.Format = "date"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
-			}
-			b := metav1.Time{}
-			err := json.Unmarshal(v, &b)
-			return b, err
+	// Encode everything we can -- the lower conversion can happen later?
+	case ResourceTableColumnDefinition_DATE, ResourceTableColumnDefinition_DATE_TIME:
+		col.OpenAPIType = "string"
+		col.OpenAPIFormat = "date"
+		if def.Type == ResourceTableColumnDefinition_DATE_TIME {
+			col.OpenAPIFormat = "date_time"
 		}
+		col.writer = func(v any, stream *jsoniter.Stream) error {
+			var t time.Time
 
-	case ResourceTableColumnDefinition_DATE_TIME:
-		def.Type = "string"
-		def.Format = "date_time"
-		converter = func(v []byte) (any, error) {
-			if len(v) == 0 {
-				return nil, nil
+			switch typed := v.(type) {
+			case time.Time:
+				t = typed
+			case *time.Time:
+				t = *typed
+			case int64:
+				t = time.UnixMilli(typed)
+			default:
+				return fmt.Errorf("unsupported date conversion (%t)", v)
 			}
-			b := metav1.Time{}
-			err := json.Unmarshal(v, &b)
-			return b, err
+
+			// encode as millis has fastest parsing
+			stream.WriteInt64(t.UnixMilli())
+			return stream.Error
+		}
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			nxt, err := iter.WhatIsNext()
+			if err != nil {
+				return nil, err
+			}
+			switch nxt {
+			case jsoniter.NumberValue:
+				ts, err := iter.ReadInt64()
+				if err != nil {
+					return nil, err
+				}
+				return time.UnixMilli(ts).UTC(), nil
+				// TODOcase jsoniter.StringValue:
+			}
+			return nil, fmt.Errorf("unexpected: %+v", nxt)
 		}
 
 	case ResourceTableColumnDefinition_JSON:
-		def.Type = "string"
-		def.Format = "json"
-		// default converter
+		col.OpenAPIType = "string"
+		col.OpenAPIFormat = "json"
+
+		col.reader = func(iter *jsoniter.Iterator) (any, error) {
+			return iter.Read()
+		}
 	}
 
-	return def, converter
+	return col
 }
 
-func (x *ResourceTableRow) SetCells(values []any) (err error) {
-	x.Cells = make([][]byte, len(values))
-	for i, v := range values {
-		if v == nil {
-			continue
-		}
+func (x *ResourceTableColumn) Encode(v any) ([]byte, error) {
+	if v == nil {
+		return nil, nil // no types to write
+	}
 
-		switch t := v.(type) {
-		case string:
-			x.Cells[i] = []byte(t)
-		case json.RawMessage:
-			x.Cells[i] = []byte(t)
-
-		default:
-			x.Cells[i], err = json.Marshal(v)
-			if err != nil {
-				return err
+	// Arrays always need JSON wrappers
+	if !x.def.IsArray {
+		// But simple string can be raw bytes
+		if x.def.Type == ResourceTableColumnDefinition_STRING {
+			s, ok := v.(string)
+			if !ok {
+				return nil, fmt.Errorf("expecting a string field")
 			}
+			return []byte(s), nil
+		}
+		if x.def.Type == ResourceTableColumnDefinition_BINARY {
+			s, ok := v.([]byte)
+			if !ok {
+				return nil, fmt.Errorf("expecting a byte array")
+			}
+			return s, nil
 		}
 	}
-	return err
+
+	buff := bytes.NewBuffer(make([]byte, 0, 128))
+	cfg := jsoniter.ConfigCompatibleWithStandardLibrary
+	stream := cfg.BorrowStream(buff)
+	defer cfg.ReturnStream(stream)
+	var err error
+
+	writer := func(v any) error {
+		if v == nil {
+			stream.WriteNil() // only happens in an array
+		} else if x.writer != nil {
+			return x.writer(v, stream)
+		} else {
+			stream.WriteVal(v)
+		}
+		return stream.Error
+	}
+
+	if x.def.IsArray {
+		stream.WriteArrayStart()
+
+		switch reflect.TypeOf(v).Kind() {
+		case reflect.Slice, reflect.Array:
+			s := reflect.ValueOf(v)
+			for i := 0; i < s.Len(); i++ {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				sub := s.Index(i).Interface()
+				err = writer(sub)
+				if err != nil {
+					return nil, err
+				}
+			}
+		default:
+			// single value? just write it and we will see?
+			err = writer(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		stream.WriteArrayEnd()
+	} else {
+		err = writer(v)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if stream.Error != nil {
+		return nil, stream.Error
+	}
+
+	err = stream.Flush()
+	if err != nil {
+		return nil, err
+	}
+	return json.RawMessage(buff.Bytes()), nil
+}
+
+func (x *ResourceTableColumn) Decode(buff []byte) (any, error) {
+	if len(buff) == 0 {
+		return nil, nil
+	}
+
+	if !x.def.IsArray {
+		if x.def.Type == ResourceTableColumnDefinition_STRING {
+			return string(buff), nil
+		}
+		if x.def.Type == ResourceTableColumnDefinition_BINARY {
+			return buff, nil
+		}
+	}
+
+	iter, err := jsoniter.ParseBytes(jsoniter.ConfigDefault, buff)
+	if err != nil {
+		return nil, err
+	}
+
+	if x.def.IsArray {
+		vals := []any{} // it may have nulls
+
+		for more, err := iter.ReadArray(); more; more, err = iter.ReadArray() {
+			if err != nil {
+				return nil, err
+			}
+			v, err := x.reader(iter)
+			if err == io.EOF {
+				err = nil
+			} else if err != nil {
+				return nil, err
+			}
+			vals = append(vals, v)
+		}
+
+		return vals, iter.ReadError()
+	}
+
+	v, err := x.reader(iter)
+	if err == io.EOF {
+		err = nil
+	} else if err != nil {
+		return nil, err
+	}
+	return v, err
 }
--- pkg/storage/unified/resource/table_test.go
+++ pkg/storage/unified/resource/table_test.go
@@ -2,67 +2,68 @@ package resource
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestTableFormat(t *testing.T) {
-	table := ResourceTable{
-		ResourceVersion:    1234,
-		NextPageToken:      "next",
-		RemainingItemCount: 7,
-		Columns: []*ResourceTableColumnDefinition{
-			{
-				Name: "title",
-				Type: ResourceTableColumnDefinition_STRING,
-			},
-			{
-				Name: "stats.count",
-				Type: ResourceTableColumnDefinition_INT64,
-			},
-			{
-				Name:        "number",
-				Description: "float64 value",
-				Type:        ResourceTableColumnDefinition_DOUBLE,
-			},
+	columns := []*ResourceTableColumnDefinition{
+		{
+			Name: "title",
+			Type: ResourceTableColumnDefinition_STRING,
+		},
+		{
+			Name: "stats.count",
+			Type: ResourceTableColumnDefinition_INT64,
+		},
+		{
+			Name: "number",
+			Type: ResourceTableColumnDefinition_DOUBLE,
+
+			Description: "float64 value",
+		},
+		{
+			Name:    "tags",
+			Type:    ResourceTableColumnDefinition_STRING,
+			IsArray: true,
 		},
 	}
 
-	err := table.AddRow(&ResourceKey{
+	var err error
+	builder := NewTableBuilder(columns)
+
+	err = builder.AddRow(&ResourceKey{
 		Namespace: "default",
 		Group:     "ggg",
 		Resource:  "xyz", // does not have a home in table!
 		Name:      "aaa",
-	}, 10, []any{"aaa", 123, 1.23}, nil)
+	}, 10, map[string]any{
+		"title":  "AAA",
+		"number": 12345,
+		"tags":   "one", // becomes an array
+	})
 	require.NoError(t, err)
 
-	err = table.AddRow(&ResourceKey{
+	err = builder.AddRow(&ResourceKey{
 		Namespace: "default",
 		Group:     "ggg",
-		Resource:  "xyz",
+		Resource:  "xyz", // does not have a home in table!
 		Name:      "bbb",
-	}, 11, []any{"bbb", 456, 78}, nil) // note that 78 is an int, not double (OK?)
+	}, 10, map[string]any{
+		"title":       "BBB",
+		"stats.count": 12345,
+		"tags":        []string{"one", "two"}, // becomes an array
+	})
 	require.NoError(t, err)
 
-	err = table.AddRow(&ResourceKey{
-		Namespace: "default",
-		Group:     "ggg",
-		Resource:  "xyz",
-		Name:      "ccc",
-	}, 12, []any{"ccc", nil, 99}, nil) // note that 78 is an int, not double (OK?)
-	require.NoError(t, err)
-
-	after, err := table.ToK8s(false)
+	after, err := builder.ToK8s()
 	require.NoError(t, err)
 	jsraw, _ := json.MarshalIndent(after, "", "  ")
-	// fmt.Printf("%s\n", string(jsraw))
+	fmt.Printf("%s\n", string(jsraw))
 	require.JSONEq(t, `{
-		"metadata": {
-			"resourceVersion": "1234",
-			"continue": "next",
-			"remainingItemCount": 7
-		},
+		"metadata": {},
 		"columnDefinitions": [
 			{
 				"name": "title",
@@ -84,14 +85,24 @@ func TestTableFormat(t *testing.T) {
 				"format": "double",
 				"description": "float64 value",
 				"priority": 0
+			},
+			{
+				"name": "tags",
+				"type": "string",
+				"format": "",
+				"description": "",
+				"priority": 0
 			}
 		],
 		"rows": [
 			{
 				"cells": [
-					"aaa",
-					123,
-					1.23
+					"AAA",
+					null,
+					12345,
+					[
+						"one"
+					]
 				],
 				"object": {
 					"metadata": {
@@ -104,39 +115,98 @@ func TestTableFormat(t *testing.T) {
 			},
 			{
 				"cells": [
-					"bbb",
-					456,
-					78
+					"BBB",
+					12345,
+					null,
+					[
+						"one",
+						"two"
+					]
 				],
 				"object": {
 					"metadata": {
 						"name": "bbb",
 						"namespace": "default",
-						"resourceVersion": "11",
-						"creationTimestamp": null
-					}
-				}
-			},
-			{
-				"cells": [
-					"ccc",
-					null,
-					99
-				],
-				"object": {
-					"metadata": {
-						"name": "ccc",
-						"namespace": "default",
-						"resourceVersion": "12",
+						"resourceVersion": "10",
 						"creationTimestamp": null
 					}
 				}
 			}
 		]
 	}`, string(jsraw))
+}
 
-	parsed, err := table.ToK8s(true)
-	require.NoError(t, err)
-	parsedjs, _ := json.MarshalIndent(parsed, "", "  ")
-	require.JSONEq(t, string(jsraw), string(parsedjs))
+func TestColumnEncoding(t *testing.T) {
+	type check struct {
+		// The table definition
+		def *ResourceTableColumnDefinition
+
+		// Passed to the encode function
+		input any
+
+		// Expected error from input
+		input_err error
+
+		// Skip the encode step
+		raw []byte
+
+		// Expected output from decode
+		output any
+
+		// Expected error from decode
+		output_err error
+	}
+
+	checks := []check{
+		{
+			def: &ResourceTableColumnDefinition{
+				Type:    ResourceTableColumnDefinition_STRING,
+				IsArray: true,
+			},
+			input:  "aaa",
+			output: []any{"aaa"},
+		},
+		{
+			def: &ResourceTableColumnDefinition{
+				Type: ResourceTableColumnDefinition_INT64,
+			},
+			input:  12345,
+			output: int64(12345),
+		},
+		{
+			def: &ResourceTableColumnDefinition{
+				Type: ResourceTableColumnDefinition_DOUBLE,
+			},
+			input:  12345,
+			output: float64(12345),
+		},
+	}
+
+	var err error
+	for _, tst := range checks {
+		col := NewResourceTableColumn(tst.def, 0)
+
+		buff := tst.raw
+		if buff == nil {
+			buff, err = col.Encode(tst.input)
+			if tst.input_err != nil {
+				require.Equal(t, tst.input_err, err)
+			} else {
+				require.NoError(t, err)
+			}
+		}
+
+		out, err := col.Decode(buff)
+		if tst.output_err != nil {
+			require.Equal(t, tst.output_err, err)
+		} else {
+			require.NoError(t, err)
+		}
+
+		if tst.output != nil {
+			require.Equal(t, tst.output, out)
+		} else {
+			require.Equal(t, tst.input, out)
+		}
+	}
 }
