078a45742f7cc15f3b23305c282382e062e3cf48
Gilles De Mey <gilles.de.mey@gmail.com>
2024-11-19 18:05:13 +0100

    separate hook for useFilteredRulesIteratorProvider

--- public/app/features/alerting/unified/rule-list/FilterView.tsx
+++ public/app/features/alerting/unified/rule-list/FilterView.tsx
@@ -1,30 +1,20 @@
-import { merge } from 'ix/asynciterable/merge';
-import { filter, flatMap, map, take, tap, withAbort } from 'ix/asynciterable/operators';
-import { compact, isEqual } from 'lodash';
-import memoize from 'micro-memoize';
+import { take, tap, withAbort } from 'ix/asynciterable/operators';
+import { isEqual } from 'lodash';
 import { useCallback, useEffect, useRef, useState, useTransition } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
 import { Card, EmptyState, Stack } from '@grafana/ui';
 import { Trans } from 'app/core/internationalization';
-import { Matcher } from 'app/plugins/datasource/alertmanager/types';
-import { DataSourceRuleGroupIdentifier } from 'app/types/unified-alerting';
-import { PromRuleDTO, PromRuleGroupDTO } from 'app/types/unified-alerting-dto';
 
-import { prometheusApi } from '../api/prometheusApi';
 import { isLoading, useAsync } from '../hooks/useAsync';
 import { RulesFilter } from '../search/rulesSearchParser';
-import { labelsMatchMatchers } from '../utils/alertmanager';
-import { getAllRulesSourceNames, getDatasourceAPIUid } from '../utils/datasource';
-import { parseMatcher } from '../utils/matchers';
-import { hashRule } from '../utils/rule-id';
-import { isAlertingRule } from '../utils/rules';
 
 import LoadMoreHelper from './LoadMoreHelper';
 import { AlertRuleLoader } from './RuleList.v2';
 import { ListItem } from './components/ListItem';
 import { ActionsLoader } from './components/RuleActionsButtons.V2';
 import { RuleListIcon } from './components/RuleListIcon';
+import { RuleWithOrigin, useFilteredRulesIteratorProvider } from './hooks/useFilteredRulesIterator';
 
 interface FilterViewProps {
   filterState: RulesFilter;
@@ -53,7 +43,7 @@ export function FilterView({ filterState }: FilterViewProps) {
     () =>
       getFilteredRulesIterator().pipe(
         withAbort(controller.current.signal),
-        onFinished(() => setNoMoreResults(true))
+        onFinished(() => setDoneSearching(true))
       ),
     [getFilteredRulesIterator]
   );
@@ -62,7 +52,7 @@ export function FilterView({ filterState }: FilterViewProps) {
   const rulesIterator = useRef(initIterator());
 
   const [rules, setRules] = useState<RuleWithOrigin[]>([]);
-  const [noMoreResults, setNoMoreResults] = useState(false);
+  const [doneSearching, setDoneSearching] = useState(false);
 
   /* This function will fetch a page of results from the iterable */
   const [{ execute: loadResultPage }, state] = useAsync(async () => {
@@ -88,7 +78,7 @@ export function FilterView({ filterState }: FilterViewProps) {
 
     // reset view state
     setRules([]);
-    setNoMoreResults(false);
+    setDoneSearching(false);
     setPrevFilterState(filterState);
 
     loadResultPage();
@@ -107,9 +97,10 @@ export function FilterView({ filterState }: FilterViewProps) {
   }, [controller]);
 
   const loading = isLoading(state) || transitionPending;
+  const noRules = rules.length === 0;
 
   /* If we don't have any rules and have exhausted all sources, show a EmptyState */
-  if (rules.length === 0 && noMoreResults) {
+  if (noRules && doneSearching) {
     return (
       <EmptyState variant="not-found" message="No matching rules found">
         No alert- or recording rules matched your current set of filters.
@@ -127,7 +118,7 @@ export function FilterView({ filterState }: FilterViewProps) {
           <AlertRuleListItemLoader />
           <AlertRuleListItemLoader />
         </>
-      ) : noMoreResults ? (
+      ) : doneSearching ? (
         <Card>
           <Trans i18nKey="alerting.rule-list.filter-view.no-more-results">No more results</Trans>
         </Card>
@@ -146,158 +137,6 @@ const AlertRuleListItemLoader = () => (
     actions={<ActionsLoader />}
   />
 );
-
-const { useLazyGroupsQuery } = prometheusApi;
-
-interface RuleWithOrigin {
-  /**
-   * Artificial frontend-only identifier for the rule.
-   * It's used as a key for the rule in the rule list to prevent key duplication
-   */
-  ruleKey: string;
-  rule: PromRuleDTO;
-  groupIdentifier: DataSourceRuleGroupIdentifier;
-}
-
-function useFilteredRulesIteratorProvider(filterState: RulesFilter, groupLimit: number) {
-  const [fetchGroups] = useLazyGroupsQuery();
-  const allRuleSourceNames = getAllRulesSourceNames();
-
-  /**
-   * This async generator will continue to yield rule groups and will keep fetching backend pages as long as the consumer
-   * is iterating.
-   */
-  const fetchRuleSourceGroups = useCallback(
-    async function* (ruleSourceName: string, maxGroups: number) {
-      const ruleSourceUid = getDatasourceAPIUid(ruleSourceName);
-
-      const response = await fetchGroups({
-        ruleSource: { uid: ruleSourceUid },
-        groupLimit: maxGroups,
-      });
-
-      if (response.data?.data) {
-        yield* response.data.data.groups.map((group) => [ruleSourceName, group] as const);
-      }
-
-      let lastToken: string | undefined = undefined;
-      if (response.data?.data?.groupNextToken) {
-        lastToken = response.data.data.groupNextToken;
-      }
-
-      while (lastToken) {
-        const response = await fetchGroups({
-          ruleSource: { uid: ruleSourceUid },
-          groupNextToken: lastToken,
-          groupLimit: maxGroups,
-        });
-
-        if (response.data?.data) {
-          yield* response.data.data.groups.map((group) => [ruleSourceName, group] as const);
-        }
-
-        lastToken = response.data?.data?.groupNextToken;
-      }
-    },
-    [fetchGroups]
-  );
-
-  const getFilteredRulesIterator = () => {
-    const ruleSourcesToFetchFrom = filterState.dataSourceNames.length
-      ? filterState.dataSourceNames
-      : allRuleSourceNames;
-    const [source, ...iterables] = ruleSourcesToFetchFrom.map((ds) => fetchRuleSourceGroups(ds, groupLimit));
-
-    return merge(source, ...iterables).pipe(
-      filter(([rulesSource, group]) => groupFilter(rulesSource, group, filterState)),
-      flatMap(([rulesSource, group]) => group.rules.map((rule) => [rulesSource, group, rule] as const)),
-      filter(([_, __, rule]) => ruleFilter(rule, filterState)),
-      map(([rulesSource, group, rule]) => mapRuleToRuleWithOrigin(rulesSource, group, rule))
-    );
-  };
-
-  return { getFilteredRulesIterator };
-}
-
-// TODO maybe we want infinitely large?
-const getRulesSourceUidMemoized = memoize(getDatasourceAPIUid, { maxSize: 10 }); // 10 is totally arbitrary value
-
-function mapRuleToRuleWithOrigin(rulesSourceName: string, group: PromRuleGroupDTO, rule: PromRuleDTO): RuleWithOrigin {
-  const ruleKey = `${rulesSourceName}-${group.file}-${group.name}-${rule.name}-${rule.type}-${hashRule(rule)}`;
-
-  return {
-    ruleKey,
-    rule,
-    groupIdentifier: {
-      rulesSource: { name: rulesSourceName, uid: getRulesSourceUidMemoized(rulesSourceName) },
-      namespace: { name: group.file },
-      groupName: group.name,
-      groupOrigin: 'datasource',
-    },
-  };
-}
-
-/**
- * Returns a new group with only the rules that match the filter.
- * @returns A new group with filtered rules, or undefined if the group does not match the filter or all rules are filtered out.
- */
-function groupFilter(rulesSourceName: string, group: PromRuleGroupDTO, filterState: RulesFilter): boolean {
-  const { name, file } = group;
-
-  // TODO Add fuzzy filtering or not
-  if (filterState.namespace && !file.includes(filterState.namespace)) {
-    return false;
-  }
-
-  if (filterState.groupName && !name.includes(filterState.groupName)) {
-    return false;
-  }
-
-  return true;
-}
-
-function ruleFilter(rule: PromRuleDTO, filterState: RulesFilter) {
-  const { name, labels = {}, health, type } = rule;
-
-  if (filterState.freeFormWords.length > 0 && !filterState.freeFormWords.some((word) => name.includes(word))) {
-    return false;
-  }
-  if (filterState.ruleName && !name.includes(filterState.ruleName)) {
-    return false;
-  }
-  if (filterState.labels.length > 0) {
-    const matchers = compact(filterState.labels.map(looseParseMatcher));
-    const doRuleLabelsMatchQuery = matchers.length > 0 && labelsMatchMatchers(labels, matchers);
-    if (!doRuleLabelsMatchQuery) {
-      return false;
-    }
-  }
-  if (filterState.ruleType && type !== filterState.ruleType) {
-    return false;
-  }
-  if (filterState.ruleState) {
-    if (!isAlertingRule(rule)) {
-      return false;
-    }
-    if (rule.state !== filterState.ruleState) {
-      return false;
-    }
-  }
-  if (filterState.ruleHealth && health !== filterState.ruleHealth) {
-    return false;
-  }
-
-  return true;
-}
-
-function looseParseMatcher(matcherQuery: string): Matcher | undefined {
-  try {
-    return parseMatcher(matcherQuery);
-  } catch {
-    // Try to createa a matcher than matches all values for a given key
-    return { name: matcherQuery, value: '', isRegex: true, isEqual: true };
-  }
-}
 
 // simple helper function to detect the end of the source async iterable
 function onFinished<T>(fn: () => void) {
---
+++ public/app/features/alerting/unified/rule-list/hooks/useFilteredRulesIterator.ts
@@ -0,0 +1,169 @@
+import { merge } from 'ix/asynciterable/merge';
+import { filter, flatMap, map } from 'ix/asynciterable/operators';
+import { compact } from 'lodash';
+import memoize from 'micro-memoize';
+import { useCallback } from 'react';
+
+import { Matcher } from 'app/plugins/datasource/alertmanager/types';
+import { DataSourceRuleGroupIdentifier } from 'app/types/unified-alerting';
+import { PromRuleDTO, PromRuleGroupDTO } from 'app/types/unified-alerting-dto';
+
+import { prometheusApi } from '../../api/prometheusApi';
+import { RulesFilter } from '../../search/rulesSearchParser';
+import { labelsMatchMatchers } from '../../utils/alertmanager';
+import { getAllRulesSourceNames, getDatasourceAPIUid } from '../../utils/datasource';
+import { parseMatcher } from '../../utils/matchers';
+import { hashRule } from '../../utils/rule-id';
+import { isAlertingRule } from '../../utils/rules';
+
+export interface RuleWithOrigin {
+  /**
+   * Artificial frontend-only identifier for the rule.
+   * It's used as a key for the rule in the rule list to prevent key duplication
+   */
+  ruleKey: string;
+  rule: PromRuleDTO;
+  groupIdentifier: DataSourceRuleGroupIdentifier;
+}
+
+const { useLazyGroupsQuery } = prometheusApi;
+
+export function useFilteredRulesIteratorProvider(filterState: RulesFilter, groupLimit: number) {
+  const [fetchGroups] = useLazyGroupsQuery();
+  const allRuleSourceNames = getAllRulesSourceNames();
+
+  /**
+   * This async generator will continue to yield rule groups and will keep fetching backend pages as long as the consumer
+   * is iterating.
+   */
+  const fetchRuleSourceGroups = useCallback(
+    async function* (ruleSourceName: string, maxGroups: number) {
+      const ruleSourceUid = getDatasourceAPIUid(ruleSourceName);
+
+      const response = await fetchGroups({
+        ruleSource: { uid: ruleSourceUid },
+        groupLimit: maxGroups,
+      });
+
+      if (response.data?.data) {
+        yield* response.data.data.groups.map((group) => [ruleSourceName, group] as const);
+      }
+
+      let lastToken: string | undefined = undefined;
+      if (response.data?.data?.groupNextToken) {
+        lastToken = response.data.data.groupNextToken;
+      }
+
+      while (lastToken) {
+        const response = await fetchGroups({
+          ruleSource: { uid: ruleSourceUid },
+          groupNextToken: lastToken,
+          groupLimit: maxGroups,
+        });
+
+        if (response.data?.data) {
+          yield* response.data.data.groups.map((group) => [ruleSourceName, group] as const);
+        }
+
+        lastToken = response.data?.data?.groupNextToken;
+      }
+    },
+    [fetchGroups]
+  );
+
+  const getFilteredRulesIterator = () => {
+    const ruleSourcesToFetchFrom = filterState.dataSourceNames.length
+      ? filterState.dataSourceNames
+      : allRuleSourceNames;
+    const [source, ...iterables] = ruleSourcesToFetchFrom.map((ds) => fetchRuleSourceGroups(ds, groupLimit));
+
+    return merge(source, ...iterables).pipe(
+      filter(([rulesSource, group]) => groupFilter(rulesSource, group, filterState)),
+      flatMap(([rulesSource, group]) => group.rules.map((rule) => [rulesSource, group, rule] as const)),
+      filter(([_, __, rule]) => ruleFilter(rule, filterState)),
+      map(([rulesSource, group, rule]) => mapRuleToRuleWithOrigin(rulesSource, group, rule))
+    );
+  };
+
+  return { getFilteredRulesIterator };
+}
+
+// TODO maybe we want infinitely large?
+const getRulesSourceUidMemoized = memoize(getDatasourceAPIUid);
+
+function mapRuleToRuleWithOrigin(rulesSourceName: string, group: PromRuleGroupDTO, rule: PromRuleDTO): RuleWithOrigin {
+  const ruleKey = `${rulesSourceName}-${group.file}-${group.name}-${rule.name}-${rule.type}-${hashRule(rule)}`;
+
+  return {
+    ruleKey,
+    rule,
+    groupIdentifier: {
+      rulesSource: { name: rulesSourceName, uid: getRulesSourceUidMemoized(rulesSourceName) },
+      namespace: { name: group.file },
+      groupName: group.name,
+      groupOrigin: 'datasource',
+    },
+  };
+}
+
+/**
+ * Returns a new group with only the rules that match the filter.
+ * @returns A new group with filtered rules, or undefined if the group does not match the filter or all rules are filtered out.
+ */
+function groupFilter(rulesSourceName: string, group: PromRuleGroupDTO, filterState: RulesFilter): boolean {
+  const { name, file } = group;
+
+  // TODO Add fuzzy filtering or not
+  if (filterState.namespace && !file.includes(filterState.namespace)) {
+    return false;
+  }
+
+  if (filterState.groupName && !name.includes(filterState.groupName)) {
+    return false;
+  }
+
+  return true;
+}
+
+function ruleFilter(rule: PromRuleDTO, filterState: RulesFilter) {
+  const { name, labels = {}, health, type } = rule;
+
+  if (filterState.freeFormWords.length > 0 && !filterState.freeFormWords.some((word) => name.includes(word))) {
+    return false;
+  }
+  if (filterState.ruleName && !name.includes(filterState.ruleName)) {
+    return false;
+  }
+  if (filterState.labels.length > 0) {
+    const matchers = compact(filterState.labels.map(looseParseMatcher));
+    const doRuleLabelsMatchQuery = matchers.length > 0 && labelsMatchMatchers(labels, matchers);
+    if (!doRuleLabelsMatchQuery) {
+      return false;
+    }
+  }
+  if (filterState.ruleType && type !== filterState.ruleType) {
+    return false;
+  }
+  if (filterState.ruleState) {
+    if (!isAlertingRule(rule)) {
+      return false;
+    }
+    if (rule.state !== filterState.ruleState) {
+      return false;
+    }
+  }
+  if (filterState.ruleHealth && health !== filterState.ruleHealth) {
+    return false;
+  }
+
+  return true;
+}
+
+function looseParseMatcher(matcherQuery: string): Matcher | undefined {
+  try {
+    return parseMatcher(matcherQuery);
+  } catch {
+    // Try to createa a matcher than matches all values for a given key
+    return { name: matcherQuery, value: '', isRegex: true, isEqual: true };
+  }
+}
