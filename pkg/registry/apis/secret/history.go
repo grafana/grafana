7f5aaca4eb46611034d35592ed9aef84406239c5
Matheus Macabu <macabu.matheus@gmail.com>
2024-11-19 11:08:09 +0100

    pkg/registry/apis/secret: comments and todos on history storage

--- pkg/registry/apis/secret/history.go
+++ pkg/registry/apis/secret/history.go
@@ -12,44 +12,62 @@ import (
 	secretstore "github.com/grafana/grafana/pkg/storage/secret"
 )
 
+// secretHistory implements the methods for the "history" subresource. This is exposed via HTTP, not gRPC.
 type secretHistory struct {
+	// TODO: we can use composition and only expose the `History` method this uses.
 	store secretstore.SecureValueStore
 }
 
+// TODO: what really do we need to implement in this case? rest.Scoper?
 var (
+	_ rest.Storage         = (*secretHistory)(nil)
 	_ rest.Connecter       = (*secretHistory)(nil)
 	_ rest.StorageMetadata = (*secretHistory)(nil)
 )
 
+// New returns an empty `*SecureValueActivityList` that is required to be implemented by any storage.
 func (r *secretHistory) New() runtime.Object {
 	return &secret.SecureValueActivityList{}
 }
 
-func (r *secretHistory) Destroy() {
+// Destroy is a no-op.
+func (r *secretHistory) Destroy() {}
+
+func (s *secretHistory) NamespaceScoped() bool {
+	panic("should this be called?")
 }
 
+// ConnectMethods returns the list of HTTP methods we accept for this subresource.
 func (r *secretHistory) ConnectMethods() []string {
-	return []string{"GET"}
+	return []string{http.MethodGet}
 }
 
+// NewConnectOptions returns some custom options that is passed in the `opts` field used by `Connect`.
 func (r *secretHistory) NewConnectOptions() (runtime.Object, bool, string) {
 	return nil, false, ""
 }
 
+// ProducesMIMETypes returns the `Content-Type` used by `Connect`.
 func (r *secretHistory) ProducesMIMETypes(verb string) []string {
 	return []string{"application/json"}
 }
 
+// ProducesObject returns the concrete type (marshable with `json` tags) used by `Connect`.
 func (r *secretHistory) ProducesObject(verb string) interface{} {
 	return &secret.SecureValueActivityList{}
 }
 
+// Connect returns an http.Handler that will handle the request/response for a given API invocation.
+// See other methods implemented for supporting/optional functionality.
 func (r *secretHistory) Connect(ctx context.Context, name string, opts runtime.Object, responder rest.Responder) (http.Handler, error) {
 	ns := request.NamespaceValue(ctx)
+
+	// TODO: should this be inside the HTTP handler?
 	val, err := r.store.History(ctx, ns, name, "")
 	if err != nil {
 		return nil, err
 	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		responder.Object(http.StatusOK, val)
 	}), nil
