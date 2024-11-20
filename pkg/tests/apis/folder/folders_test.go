8c5638ca4f64b2dcdf6cb466b8fe1ce1efe8e15f
suntala <arati.rana@grafana.com>
2024-11-20 10:40:27 +0100

    Add folder get test

--- pkg/tests/apis/folder/folders_test.go
+++ pkg/tests/apis/folder/folders_test.go
@@ -784,6 +784,20 @@ func TestIntegrationFolderGetPermissions(t *testing.T) {
 	}
 	tcs := []testCase{
 		{
+			description:          "get folder by UID should return parent folders if nested folder are enabled",
+			expectedCode:         http.StatusOK,
+			expectedParentUIDs:   []string{"parentuid"},
+			expectedParentTitles: []string{"testparent"},
+			permissions: []resourcepermissions.SetResourcePermissionCommand{
+				{
+					Actions:           []string{dashboards.ActionFoldersRead},
+					Resource:          "folders",
+					ResourceAttribute: "uid",
+					ResourceID:        "*",
+				},
+			},
+		},
+		{
 			description:          "get folder by UID should return parent folders redacted if nested folder are enabled and user does not have read access to parent folders",
 			expectedCode:         http.StatusOK,
 			expectedParentUIDs:   []string{api.REDACTED},
