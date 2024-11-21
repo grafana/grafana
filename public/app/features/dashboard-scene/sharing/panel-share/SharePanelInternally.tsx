620e48ccee4f3252c2a8413ebe717c2bdcbbca1e
Juan Cabanas <juan.cabanas@grafana.com>
2024-11-21 15:58:14 -0300

    inputs controlled

--- public/app/features/dashboard-scene/sharing/panel-share/SharePanelInternally.tsx
+++ public/app/features/dashboard-scene/sharing/panel-share/SharePanelInternally.tsx
@@ -1,14 +1,27 @@
 import { css } from '@emotion/css';
+import { Controller, useForm } from 'react-hook-form';
 import { useAsyncFn } from 'react-use';
 import { lastValueFrom } from 'rxjs';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { config, getBackendSrv } from '@grafana/runtime';
 import { SceneComponentProps } from '@grafana/scenes';
-import { Alert, Button, ClipboardButton, Divider, Field, LoadingBar, Stack, Text, useStyles2 } from '@grafana/ui';
+import {
+  Alert,
+  Button,
+  ClipboardButton,
+  Divider,
+  Field,
+  LoadingBar,
+  Select,
+  Stack,
+  Text,
+  useStyles2,
+} from '@grafana/ui';
 import { Input } from '@grafana/ui/src/components/Input/Input';
 import { t, Trans } from 'app/core/internationalization';
 
+import { defaultZoom, getZoomOptions } from '../../../../extensions/reports/constants';
 import { getDashboardSceneFor } from '../../utils/utils';
 import ShareInternallyConfiguration from '../ShareInternallyConfiguration';
 import { ShareLinkTab, ShareLinkTabState } from '../ShareLinkTab';
@@ -25,34 +38,40 @@ export class SharePanelInternally extends ShareLinkTab {
   }
 }
 
-// type ImageSettingsForm = {
-//   width: number;
-//   height: number;
-//   zoom: number;
-// };
+type ImageSettingsForm = {
+  width: number;
+  height: number;
+  scaleFactor: number;
+};
 
 function SharePanelInternallyRenderer({ model }: SceneComponentProps<SharePanelInternally>) {
   const styles = useStyles2(getStyles);
   const { useLockedTime, useShortUrl, selectedTheme, isBuildUrlLoading, imageUrl, panelRef, onDismiss } =
     model.useState();
 
-  // const [renderImage, { data: image, isLoading, isFetching }] = useLazyRenderImageQuery();
+  const { handleSubmit, register, control } = useForm<ImageSettingsForm>({
+    mode: 'onBlur',
+    defaultValues: {
+      width: 100,
+      height: 200,
+      scaleFactor: defaultZoom,
+    },
+  });
 
   const [{ loading, value }, renderImage] = useAsyncFn(async () => {
     const response = await lastValueFrom(getBackendSrv().fetch<BlobPart>({ url: imageUrl, responseType: 'blob' }));
     const blob = new Blob([response.data], { type: 'image/png' });
 
     return URL.createObjectURL(blob);
-  }, [imageUrl]);
+  }, [model.state]);
 
   const panelTitle = panelRef?.resolve().state.title;
-  // const { handleSubmit, reset, ...formMethods } = useForm({ mode: 'onBlur', defaultValues: settings });
 
   const dashboard = getDashboardSceneFor(model);
   const isDashboardSaved = Boolean(dashboard.state.uid);
 
-  const onRenderImageClick = async () => {
-    await model.buildUrl();
+  const onRenderImageClick = async ({ width, height, scaleFactor }: ImageSettingsForm) => {
+    await model.buildUrl({ width, height });
     await renderImage();
   };
 
@@ -114,66 +133,84 @@ function SharePanelInternallyRenderer({ model }: SceneComponentProps<SharePanelI
             </Trans>
           </Alert>
         )}
-        <Stack gap={2} direction="column">
-          <Text element="h4">
-            <Trans i18nKey="link.share-panel.render-image-title">Panel preview</Trans>
-          </Text>
-          <Text element="h5">
-            <Trans i18nKey="link.share-panel.render-image-subtitle">Image settings</Trans>
-          </Text>
-          <div>
-            <Stack gap={1} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }}>
-              <Field label="Width" className={styles.imageConfigurationField}>
-                <Input
-                  type="number"
-                  suffix="px"
-                  // onChange={(event) => onChange(event.currentTarget.value, to.value)}
-                  // addonAfter={icon}
-                  // onKeyDown={submitOnEnter}
-                  // data-testid={selectors.components.TimePicker.fromField}
-                  // value={from.value}
-                />
-              </Field>
-              <Field label="Height" className={styles.imageConfigurationField}>
-                <Input
-                  type="number"
-                  suffix="px"
-                  // onChange={(event) => onChange(event.currentTarget.value, to.value)}
-                  // addonAfter={icon}
-                  // onKeyDown={submitOnEnter}
-                  // data-testid={selectors.components.TimePicker.fromField}
-                  // value={from.value}
-                />
-              </Field>
-              <Field label="Zoom" className={styles.imageConfigurationField}>
-                <Input name="importantInput" required />
-              </Field>
-            </Stack>
-            <Stack gap={1}>
-              <Button
-                icon="gf-layout-simple"
-                variant="secondary"
-                fill="solid"
-                disabled={!config.rendererAvailable || !isDashboardSaved}
-                onClick={onRenderImageClick}
-              >
-                <Trans i18nKey="link.share-panel.render-image">Render image</Trans>
-              </Button>
-              <Button variant="secondary" fill="outline" onClick={onDismiss}>
-                <Trans i18nKey="share-modal.export.cancel-button">Cancel</Trans>
-              </Button>
-            </Stack>
-          </div>
-          {loading && (
+        <form onSubmit={handleSubmit(onRenderImageClick)}>
+          <Stack gap={2} direction="column">
+            <Text element="h4">
+              <Trans i18nKey="link.share-panel.render-image-title">Panel preview</Trans>
+            </Text>
+            <Text element="h5">
+              <Trans i18nKey="link.share-panel.render-image-subtitle">Image settings</Trans>
+            </Text>
             <div>
-              <LoadingBar width={128} />
-              <div className={styles.imageLoadingContainer}>
-                <Text variant="body">{panelTitle || ''}</Text>
-              </div>
+              <Stack gap={1} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }}>
+                <Field label="Width" className={styles.imageConfigurationField}>
+                  <Input
+                    {...register('width', {
+                      required: t('link.share-panel.render-image-width-required', 'Width is required'),
+                    })}
+                    type="number"
+                    suffix="px"
+                  />
+                </Field>
+                <Field label="Height" className={styles.imageConfigurationField}>
+                  <Input
+                    {...register('height', {
+                      required: t('link.share-panel.render-image-height-required', 'Height is required'),
+                    })}
+                    type="number"
+                    suffix="px"
+                  />
+                </Field>
+                <Field label={'Zoom'} className={styles.imageConfigurationField}>
+                  <Controller
+                    name={'scaleFactor'}
+                    control={control}
+                    // defaultValue={defaultZoom}
+                    render={({ field: { ref, value, onChange, ...field } }) => (
+                      <Select<number>
+                        {...field}
+                        defaultValue={value}
+                        onChange={(scale) => onChange(scale.value!)}
+                        options={getZoomOptions().filter((opt) => !opt.isDisabled)}
+                        // value={
+                        //   getZoomOptions().find((option) => option.value === value && !option.isDisabled)
+                        //     ? value
+                        //     : defaultZoom
+                        // }
+                        // width={30}
+                        placeholder={'xx%'}
+                        aria-label={'Zoom'}
+                      />
+                    )}
+                  />
+                </Field>
+              </Stack>
+              <Stack gap={1}>
+                <Button
+                  icon="gf-layout-simple"
+                  variant="secondary"
+                  fill="solid"
+                  type="submit"
+                  disabled={!config.rendererAvailable || !isDashboardSaved}
+                >
+                  <Trans i18nKey="link.share-panel.render-image">Render image</Trans>
+                </Button>
+                <Button variant="secondary" fill="outline" onClick={onDismiss}>
+                  <Trans i18nKey="share-modal.export.cancel-button">Cancel</Trans>
+                </Button>
+              </Stack>
             </div>
-          )}
-          {value && !loading && <img src={value} alt="panel-img" className={styles.image} />}
-        </Stack>
+            {loading && (
+              <div>
+                <LoadingBar width={128} />
+                <div className={styles.imageLoadingContainer}>
+                  <Text variant="body">{panelTitle || ''}</Text>
+                </div>
+              </div>
+            )}
+            {value && !loading && <img src={value} alt="panel-img" className={styles.image} />}
+          </Stack>
+        </form>
       </Stack>
     </div>
   );
