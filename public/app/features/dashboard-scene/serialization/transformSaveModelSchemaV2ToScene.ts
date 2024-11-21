177f35943def92a1e2c5ba9507f7276f765d2451
Haris Rozajac <haris.rozajac12@gmail.com>
2024-11-21 15:35:18 -0700

    variables wip

--- public/app/features/dashboard-scene/serialization/transformSaveModelSchemaV2ToScene.ts
+++ public/app/features/dashboard-scene/serialization/transformSaveModelSchemaV2ToScene.ts
@@ -1,6 +1,6 @@
 import { uniqueId } from 'lodash';
 
-import { config } from '@grafana/runtime';
+import { config, getDataSourceSrv } from '@grafana/runtime';
 import {
   VizPanel,
   SceneTimePicker,
@@ -18,17 +18,35 @@ import {
   SceneDataProvider,
   SceneQueryRunner,
   SceneDataTransformer,
+  SceneVariable,
+  SceneVariableSet,
+  AdHocFiltersVariable,
+  CustomVariable,
+  QueryVariable,
+  DataSourceVariable,
+  IntervalVariable,
+  ConstantVariable,
+  TextBoxVariable,
+  GroupByVariable,
 } from '@grafana/scenes';
 import {
   DashboardCursorSync as DashboardCursorSyncV1,
   defaultDashboardCursorSync,
 } from '@grafana/schema/dist/esm/index.gen';
-
 import {
+  AdhocVariableKind,
+  ConstantVariableKind,
+  CustomVariableKind,
   DashboardCursorSync,
   DashboardV2Spec,
+  DatasourceVariableKind,
+  GroupByVariableKind,
+  IntervalVariableKind,
   PanelKind,
-} from '../../../../../packages/grafana-schema/src/schema/dashboard/v2alpha0/dashboard.gen';
+  QueryVariableKind,
+  TextVariableKind,
+} from '@grafana/schema/src/schema/dashboard/v2alpha0/dashboard.gen';
+
 import { addPanelsOnLoadBehavior } from '../addToDashboard/addPanelsOnLoadBehavior';
 import { DashboardAnnotationsDataLayer } from '../scene/DashboardAnnotationsDataLayer';
 import { DashboardControls } from '../scene/DashboardControls';
@@ -47,23 +65,24 @@ import { DefaultGridLayoutManager } from '../scene/layout-default/DefaultGridLay
 import { setDashboardPanelContext } from '../scene/setDashboardPanelContext';
 import { preserveDashboardSceneStateInLocalStorage } from '../utils/dashboardSessionState';
 import { DashboardInteractions } from '../utils/interactions';
-import { getDashboardSceneFor } from '../utils/utils';
+import { getDashboardSceneFor, getIntervalsFromQueryString } from '../utils/utils';
 
 import { getAngularPanelMigrationHandler } from './angularMigration';
+import { SnapshotVariable } from './custom-variables/SnapshotVariable';
+
+const DEFAULT_DATASOURCE = 'default';
+
+type TypedVariableModelv2 =
+  | QueryVariableKind
+  | TextVariableKind
+  | ConstantVariableKind
+  | DatasourceVariableKind
+  | IntervalVariableKind
+  | CustomVariableKind
+  | GroupByVariableKind
+  | AdhocVariableKind;
 
 export function transformSaveModelSchemaV2ToScene(dashboard: DashboardV2Spec): DashboardScene {
-  // FIXME: Variables
-  // const variables = new SceneVariableSet({
-  //   variables: spec.variables.map((variable) => {
-  //     // Map variables to SceneVariableSet
-  //     return {
-  //       name: variable.name,
-  //       type: variable.kind,
-  //       value: variable.value,
-  //     };
-  //   }),
-  // });
-
   const annotationLayers = dashboard.annotations.map((annotation) => {
     return new DashboardAnnotationsDataLayer({
       key: uniqueId('annotations-'),
@@ -101,8 +120,7 @@ export function transformSaveModelSchemaV2ToScene(dashboard: DashboardV2Spec): D
       weekStart: dashboard.timeSettings.weekStart,
       UNSAFE_nowDelay: dashboard.timeSettings.nowDelay,
     }),
-    // FIXME: Variables pending to implement
-    // $variables: variables,
+    $variables: getVariables(dashboard),
     $behaviors: [
       new behaviors.CursorSync({
         sync: transformCursorSyncV2ToV1(dashboard.cursorSync),
@@ -316,4 +334,275 @@ function transformCursorSyncV2ToV1(cursorSync: DashboardCursorSync): DashboardCu
     default:
       return defaultDashboardCursorSync;
   }
+}
+
+function getVariables(dashboard: DashboardV2Spec): SceneVariableSet | undefined {
+  let variables: SceneVariableSet | undefined;
+
+  if (dashboard.variables.length) {
+    if (false) {
+      // FIXME: isSnapshot is not added to the schema yet
+      // in the old model we use .meta.isSnapshot but meta is not persisted
+      variables = createVariablesForSnapshot(dashboard);
+    } else {
+      variables = createVariablesForDashboard(dashboard);
+    }
+  } else {
+    // Create empty variable set
+    variables = new SceneVariableSet({
+      variables: [],
+    });
+  }
+
+  return variables;
+}
+
+function createVariablesForDashboard(dashboard: DashboardV2Spec) {
+  const variableObjects = dashboard.variables
+    .map((v) => {
+      try {
+        return createSceneVariableFromVariableModel(v);
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+    })
+    // TODO: Remove filter
+    // Added temporarily to allow skipping non-compatible variables
+    .filter((v): v is SceneVariable => Boolean(v));
+
+  return new SceneVariableSet({
+    variables: variableObjects,
+  });
+}
+
+function createSceneVariableFromVariableModel(variable: TypedVariableModelv2): SceneVariable {
+  const commonProperties = {
+    name: variable.spec.name,
+    label: variable.spec.label,
+    description: variable.spec.description,
+  };
+  if (variable.kind === 'AdhocVariable') {
+    return new AdHocFiltersVariable({
+      ...commonProperties,
+      description: variable.spec.description,
+      skipUrlSync: variable.spec.skipUrlSync,
+      // FIXME: need to fix this in cue schema defintion by updating VariableHide type definition to use default numeric values
+      // the same applies to sort and referesh fields which require updaring VariableSort and VariableRefresh type definitions
+      hide: variable.spec.hide,
+      datasource: variable.spec.datasource,
+      applyMode: 'auto',
+      filters: variable.spec.filters ?? [],
+      baseFilters: variable.spec.baseFilters ?? [],
+      defaultKeys: variable.spec.defaultKeys,
+      useQueriesAsFilterForOptions: true,
+      layout: config.featureToggles.newFiltersUI ? 'combobox' : undefined,
+      supportsMultiValueOperators: Boolean(
+        getDataSourceSrv().getInstanceSettings(variable.spec.datasource)?.meta.multiValueFilterOperators
+      ),
+    });
+  }
+  if (variable.kind === 'CustomVariable') {
+    return new CustomVariable({
+      ...commonProperties,
+      value: variable.spec.current?.value ?? '',
+      text: variable.spec.current?.text ?? '',
+
+      query: variable.spec.query,
+      isMulti: variable.spec.multi,
+      allValue: variable.spec.allValue || undefined,
+      includeAll: variable.spec.includeAll,
+      defaultToAll: Boolean(variable.spec.includeAll),
+      skipUrlSync: variable.spec.skipUrlSync,
+      hide: variable.spec.hide,
+    });
+  } else if (variable.kind === 'QueryVariable') {
+    return new QueryVariable({
+      ...commonProperties,
+      value: variable.spec.current?.value ?? '',
+      text: variable.spec.current?.text ?? '',
+      query: variable.spec.query,
+      datasource: variable.spec.datasource,
+      sort: variable.spec.sort, //FIXME: need to fix this in cue schema defintion by updating VariableSort to use default numeric values
+      refresh: variable.spec.refresh, //FIXME: need to fix this in cue schema defintion by updating VariableRefresh to use default numeric values
+      regex: variable.spec.regex,
+      allValue: variable.spec.allValue || undefined,
+      includeAll: variable.spec.includeAll,
+      defaultToAll: Boolean(variable.spec.includeAll),
+      isMulti: variable.spec.multi,
+      skipUrlSync: variable.spec.skipUrlSync,
+      hide: variable.spec.hide,
+      definition: variable.spec.definition,
+    });
+  } else if (variable.kind === 'DatasourceVariable') {
+    return new DataSourceVariable({
+      ...commonProperties,
+      value: variable.spec.current?.value ?? '',
+      text: variable.spec.current?.text ?? '',
+      regex: variable.spec.regex,
+      pluginId: variable.spec.pluginId,
+      allValue: variable.spec.allValue || undefined,
+      includeAll: variable.spec.includeAll,
+      defaultToAll: Boolean(variable.spec.includeAll),
+      skipUrlSync: variable.spec.skipUrlSync,
+      isMulti: variable.spec.multi,
+      hide: variable.spec.hide,
+      defaultOptionEnabled:
+        variable.spec.current?.value === DEFAULT_DATASOURCE && variable.spec.current?.text === 'default',
+    });
+  } else if (variable.kind === 'IntervalVariable') {
+    const intervals = getIntervalsFromQueryString(variable.spec.query);
+    const currentInterval = getCurrentValueForOldIntervalModel(variable, intervals);
+    return new IntervalVariable({
+      ...commonProperties,
+      value: currentInterval,
+      intervals: intervals,
+      autoEnabled: variable.spec.auto,
+      autoStepCount: variable.spec.auto_count,
+      autoMinInterval: variable.spec.auto_min,
+      refresh: variable.spec.refresh,
+      skipUrlSync: variable.spec.skipUrlSync,
+      hide: variable.spec.hide,
+    });
+  } else if (variable.kind === 'ConstantVariable') {
+    return new ConstantVariable({
+      ...commonProperties,
+      value: variable.spec.query,
+      skipUrlSync: variable.spec.skipUrlSync,
+      hide: variable.spec.hide,
+    });
+  } else if (variable.kind === 'TextVariable') {
+    let val;
+    if (!variable?.spec.current?.value) {
+      val = variable.spec.query;
+    } else {
+      if (typeof variable.spec.current.value === 'string') {
+        val = variable.spec.current.value;
+      } else {
+        val = variable.spec.current.value[0];
+      }
+    }
+
+    return new TextBoxVariable({
+      ...commonProperties,
+      value: val,
+      skipUrlSync: variable.spec.skipUrlSync,
+      hide: variable.spec.hide,
+    });
+  } else if (config.featureToggles.groupByVariable && variable.kind === 'GroupByVariable') {
+    return new GroupByVariable({
+      ...commonProperties,
+      datasource: variable.spec.datasource,
+      value: variable.spec.current?.value || [],
+      text: variable.spec.current?.text || [],
+      skipUrlSync: variable.spec.skipUrlSync,
+      hide: variable.spec.hide,
+      // @ts-expect-error
+      defaultOptions: variable.options,
+    });
+  } else {
+    throw new Error(`Scenes: Unsupported variable type ${variable.kind}`);
+  }
+}
+
+export function getCurrentValueForOldIntervalModel(variable: IntervalVariableKind, intervals: string[]): string {
+  const selectedInterval = Array.isArray(variable.spec.current.value)
+    ? variable.spec.current.value[0]
+    : variable.spec.current.value;
+
+  // If the interval is the old auto format, return the new auto interval from scenes.
+  if (selectedInterval.startsWith('$__auto_interval_')) {
+    return '$__auto';
+  }
+
+  // Check if the selected interval is valid.
+  if (intervals.includes(selectedInterval)) {
+    return selectedInterval;
+  }
+
+  // If the selected interval is not valid, return the first valid interval.
+  return intervals[0];
+}
+
+export function createVariablesForSnapshot(dashboard: DashboardV2Spec): SceneVariableSet {
+  const variableObjects = dashboard.variables
+    .map((v) => {
+      try {
+        // for adhoc we are using the AdHocFiltersVariable from scenes becuase of its complexity
+        if (v.kind === 'AdhocVariable') {
+          return new AdHocFiltersVariable({
+            name: v.spec.name,
+            label: v.spec.label,
+            readOnly: true,
+            description: v.spec.description,
+            skipUrlSync: v.spec.skipUrlSync,
+            hide: v.spec.hide,
+            datasource: v.spec.datasource,
+            applyMode: 'auto',
+            filters: v.spec.filters ?? [],
+            baseFilters: v.spec.baseFilters ?? [],
+            defaultKeys: v.spec.defaultKeys,
+            useQueriesAsFilterForOptions: true,
+            layout: config.featureToggles.newFiltersUI ? 'combobox' : undefined,
+            supportsMultiValueOperators: Boolean(
+              getDataSourceSrv().getInstanceSettings(v.spec.datasource)?.meta.multiValueFilterOperators
+            ),
+          });
+        }
+        // for other variable types we are using the SnapshotVariable
+        return createSnapshotVariable(v);
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+    })
+    // TODO: Remove filter
+    // Added temporarily to allow skipping non-compatible variables
+    .filter((v): v is SceneVariable => Boolean(v));
+
+  return new SceneVariableSet({
+    variables: variableObjects,
+  });
+}
+
+/** Snapshots variables are read-only and should not be updated */
+export function createSnapshotVariable(variable: TypedVariableModelv2): SceneVariable {
+  let snapshotVariable: SnapshotVariable;
+  let current: { value: string | string[]; text: string | string[] };
+  if (variable.kind === 'IntervalVariable') {
+    const intervals = getIntervalsFromQueryString(variable.spec.query);
+    const currentInterval = getCurrentValueForOldIntervalModel(variable, intervals);
+    snapshotVariable = new SnapshotVariable({
+      name: variable.spec.name,
+      label: variable.spec.label,
+      description: variable.spec.description,
+      value: currentInterval,
+      text: currentInterval,
+      hide: variable.spec.hide,
+    });
+    return snapshotVariable;
+  }
+
+  // FIXME: don't have a system variable that schema v2 would return
+  if (variable.kind === 'system' || variable.kind === 'ConstantVariable' || variable.kind === 'AdhocVariable') {
+    current = {
+      value: '',
+      text: '',
+    };
+  } else {
+    current = {
+      value: variable.current?.value ?? '',
+      text: variable.current?.text ?? '',
+    };
+  }
+
+  snapshotVariable = new SnapshotVariable({
+    name: variable.spec.name,
+    label: variable.spec.label,
+    description: variable.spec.description,
+    value: current?.value ?? '',
+    text: current?.text ?? '',
+    hide: variable.spec.hide,
+  });
+  return snapshotVariable;
 }
