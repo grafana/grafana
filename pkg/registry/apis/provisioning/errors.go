5d4de8d8f3240fd9f993dc529c1b35ce9243a43f
Ryan McKinley <ryantxu@gmail.com>
2024-11-22 16:48:49 +0300

    POC/Provisioning: Support write (after dryRun) (#96919)
    now writing

--- pkg/registry/apis/provisioning/errors.go
+++ pkg/registry/apis/provisioning/errors.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
-	ErrFileNotFound      = fs.ErrNotExist
-	ErrNamespaceMismatch = errors.New("the file namespace does not match target namespace")
+	ErrFileNotFound              = fs.ErrNotExist
+	ErrNamespaceMismatch         = errors.New("the file namespace does not match target namespace")
+	ErrUnableToReadResourceBytes = errors.New("unable to read bytes as a resource")
 )
