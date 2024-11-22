b34136a7628b19903f90f1737f7916649f2b50ba
Clarity-89 <homes89@ukr.net>
2024-11-22 16:50:20 +0200

    Cleanup

--- public/app/features/provisioning/dashboard.ts
+++ public/app/features/provisioning/dashboard.ts
@@ -1,7 +1,7 @@
-import { config, getBackendSrv } from '@grafana/runtime';
-import { DashboardDataDTO, DashboardDTO } from 'app/types';
+import { getBackendSrv } from '@grafana/runtime';
+import { DashboardDTO } from 'app/types';
 
-import { Resource } from '../apiserver/types';
+import { BASE_URL } from './api/baseAPI';
 
 /**
  * Load a dashboard from repository
@@ -10,18 +10,18 @@ export async function loadDashboardFromProvisioning(repo: string, path: string):
   const params = new URLSearchParams(window.location.search);
   const ref = params.get('ref') ?? undefined; // commit hash or branch
 
-  const url = `apis/provisioning.grafana.app/v0alpha1/namespaces/${config.namespace}/repositories/${repo}/files/${path}`;
+  const url = `${BASE_URL}/repositories/${repo}/files/${path}`;
   return getBackendSrv()
     .get(url, ref ? { ref } : undefined)
     .then((v) => {
       // Load the results from dryRun
-      const dryRun = v.resource.dryRun as Resource<DashboardDataDTO, 'Dashboard'>;
+      const dryRun = v.resource.dryRun;
       if (!dryRun) {
-        return Promise.reject("failed to read provisioned dashboard")
+        return Promise.reject('failed to read provisioned dashboard');
       }
 
-      if (!dryRun.apiVersion.startsWith("dashboard.grafana.app")) {
-        return Promise.reject("unexpected resource type: "+dryRun.apiVersion)
+      if (!dryRun.apiVersion.startsWith('dashboard.grafana.app')) {
+        return Promise.reject('unexpected resource type: ' + dryRun.apiVersion);
       }
 
       return {
@@ -43,9 +43,9 @@ export async function loadDashboardFromProvisioning(repo: string, path: string):
             file: url,
             ref: ref,
             repo: repo,
-          }
+          },
         },
-        dashboard: dryRun.spec
-      }
+        dashboard: dryRun.spec,
+      };
     });
 }
