f6a40c1bcc11d5224f79e04695866225b40e8edb
Isabel Matwawana <76437239+imatwawana@users.noreply.github.com>
2024-11-25 15:34:07 -0500

    Docs: Add example image to RegExp extract fields option (#97007)

--- public/app/features/transformers/docs/content.ts
+++ public/app/features/transformers/docs/content.ts
@@ -235,7 +235,7 @@ This transformation allows you to flexibly adapt your data types, ensuring compa
   },
   extractFields: {
     name: 'Extract fields',
-    getHelperDocs: function () {
+    getHelperDocs: function (imageRenderType: ImageRenderType = ImageRenderType.ShortcodeFigure) {
       return `
 Use this transformation to select a source of data and extract content from it in different formats. This transformation has the following fields:
 
@@ -244,6 +244,11 @@ Use this transformation to select a source of data and extract content from it i
   - **JSON** - Parse JSON content from the source.
   - **Key+value pairs** - Parse content in the format 'a=b' or 'c:d' from the source.
   - **RegExp** - Parse content using a regular expression with [named capturing group(s)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Named_capturing_group) like \`/(?<New_Field>.*)/\`.
+  ${buildImageContent(
+    '/media/docs/grafana/panels-visualizations/screenshot-regexp-detail-v11.3-2.png',
+    imageRenderType,
+    'Example of a regular expression'
+  )}
   - **Auto** - Discover fields automatically.
 - **Replace All Fields** - (Optional) Select this option to hide all other fields and display only your calculated field in the visualization.
 - **Keep Time** - (Optional) Available only if **Replace All Fields** is true. Keeps the time field in the output.
