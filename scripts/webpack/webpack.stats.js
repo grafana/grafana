fa978622c6cb37d5105cd2940e6800d5e5d43bec
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-26 16:13:14 +0100

    feat(webpack): use bundle analyser by default, env vars for additional plugins

--- scripts/webpack/webpack.stats.js
+++ scripts/webpack/webpack.stats.js
@@ -1,19 +1,35 @@
-'use strict';
 const { RsdoctorWebpackPlugin } = require('@rsdoctor/webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require('webpack-merge');
 
 const prodConfig = require('./webpack.prod.js');
 
 module.exports = (env = {}) => {
-  return merge(prodConfig(env), {
-    // disable hashing in output filenames to make them easier to identify
-    output: {
+  const config = { plugins: [new BundleAnalyzerPlugin()] };
+
+  // yarn build:stats --env doctor
+  if (env.doctor) {
+    config.plugins.push(
+      new RsdoctorWebpackPlugin({
+        supports: {
+          // disable rsdoctor bundle-analyser
+          generateTileGraph: false,
+        },
+      })
+    );
+  }
+
+  // disable hashing in output filenames to make them easier to identify
+  // yarn build:stats --env doctor --env namedChunks
+  if (env.namedChunks) {
+    config.optimization = {
+      chunkIds: 'named',
+    };
+    config.output = {
       filename: '[name].js',
       chunkFilename: '[name].js',
-    },
-    optimization: {
-      chunkIds: 'named',
-    },
-    plugins: [new RsdoctorWebpackPlugin()],
-  });
+    };
+  }
+
+  return merge(prodConfig(env), config);
 };
