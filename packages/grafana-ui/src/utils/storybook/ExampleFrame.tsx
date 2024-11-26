ba61f4e9fc13fa3afcfcd913accc29903e652b7a
joshhunt <josh@trtr.co>
2024-11-26 16:39:41 +0000

    Add ExampleFrame component to render grafana-ui examples

---
+++ packages/grafana-ui/src/utils/storybook/ExampleFrame.tsx
@@ -0,0 +1,34 @@
+import { css } from '@emotion/css';
+import { ReactNode } from 'react';
+
+import { GrafanaTheme2 } from '@grafana/data';
+
+import { useStyles2 } from '../../themes';
+
+interface ExampleFrameProps {
+  children: ReactNode;
+}
+
+/**
+ * Wraps children with a border for nicer presentation in Storybook docs.
+ *
+ * This is intended to be used temporarily to move away from our <Preview> patch until
+ * we have something more long term.
+ */
+export function ExampleFrame(props: ExampleFrameProps) {
+  const { children } = props;
+  const styles = useStyles2(getStyles);
+
+  return <div className={styles.container}>{children}</div>;
+}
+
+const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    container: css({
+      border: `1px solid ${theme.colors.border.medium}`,
+      borderRadius: theme.shape.radius.default,
+      padding: theme.spacing(2),
+      marginBottom: theme.spacing(4),
+    }),
+  };
+};
