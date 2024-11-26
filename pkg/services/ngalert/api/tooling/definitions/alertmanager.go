7ee6bb68e97746d752b7612a1dc7c04eed0de356
Alexander Akhmetov <me@alx.cx>
2024-11-26 13:46:02 +0100

    Alerting: Fix swagger spec generation

--- pkg/services/ngalert/api/tooling/definitions/alertmanager.go
+++ pkg/services/ngalert/api/tooling/definitions/alertmanager.go
@@ -481,6 +481,8 @@ type PostSilencesOKBody struct { // vendored from "github.com/prometheus/alertma
 	SilenceID string `json:"silenceID,omitempty"`
 }
 
+// GettableSilences gettable silences
+//
 // swagger:model gettableSilences
 type GettableSilences = amv2.GettableSilences
 
@@ -535,11 +537,15 @@ func (s GettableGrafanaSilence) MarshalJSON() ([]byte, error) {
 // swagger:model gettableGrafanaSilences
 type GettableGrafanaSilences []*GettableGrafanaSilence
 
+// GettableAlerts gettable alerts
+//
 // swagger:model gettableAlerts
 type GettableAlerts = amv2.GettableAlerts
 
 type GettableAlert = amv2.GettableAlert
 
+// AlertGroups alert groups
+//
 // swagger:model alertGroups
 type AlertGroups = amv2.AlertGroups
 
