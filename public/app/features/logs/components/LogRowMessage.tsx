a3131ccdb120138debfdb0c6610a35cd2bc82cc4
Matias Chomicki <matyax@gmail.com>
2024-11-26 10:40:24 +0100

    Revert test change

--- public/app/features/logs/components/LogRowMessage.tsx
+++ public/app/features/logs/components/LogRowMessage.tsx
@@ -10,7 +10,7 @@ import { LogMessageAnsi } from './LogMessageAnsi';
 import { LogRowMenuCell } from './LogRowMenuCell';
 import { LogRowStyles } from './getLogRowStyles';
 
-export const MAX_CHARACTERS = 10;
+export const MAX_CHARACTERS = 100000;
 
 interface Props {
   row: LogRowModel;
