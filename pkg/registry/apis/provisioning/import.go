adeb104ed929cd9c95f21c09c6466e77cefc9cfd
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2024-11-27 20:29:08 +0100

    Use replicator in import

--- pkg/registry/apis/provisioning/import.go
+++ pkg/registry/apis/provisioning/import.go
@@ -2,20 +2,13 @@ package provisioning
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
-	"path"
-	"strings"
 
-	apiutils "github.com/grafana/grafana/pkg/apimachinery/utils"
 	provisioning "github.com/grafana/grafana/pkg/apis/provisioning/v0alpha1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apiserver/pkg/registry/rest"
 )
 
@@ -71,26 +64,14 @@ func (c *importConnector) Connect(
 
 	// TODO: We need some way to filter what we import.
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		client, err := c.client.Client(ns)
-		if err != nil {
-			responder.Error(apierrors.NewInternalError(fmt.Errorf("failed to create a dynamic client: %w", err)))
-			return
-		}
-		kinds := newKindsLookup(client)
-		fileParser := newFileParser(ns, client, kinds)
+	replicatorFactory := newReplicatorFactory(c.client, ns)
 
-		folderGVR, ok := kinds.Resource(schema.GroupVersionKind{
-			Group:   "folder.grafana.app",
-			Version: "v0alpha1",
-			Kind:    "Folder",
-		})
-		if !ok {
-			logger.DebugContext(ctx, "no folder GVR was returned")
-			responder.Error(apierrors.NewInternalError(fmt.Errorf("failed to find folder GVR")))
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		replicator, err := replicatorFactory.New()
+		if err != nil {
+			responder.Error(apierrors.NewInternalError(fmt.Errorf("failed to create replicator: %w", err)))
 			return
 		}
-		folderIface := client.Resource(folderGVR).Namespace(ns)
 
 		ref := r.URL.Query().Get("ref")
 		logger := logger.With("ref", ref)
@@ -100,18 +81,13 @@ func (c *importConnector) Connect(
 			responder.Error(err) // TODO: do we want to do anything more for this?
 			return
 		}
+
 		for _, entry := range tree {
 			logger := logger.With("file", entry.Path)
 			if !entry.Blob {
 				logger.DebugContext(ctx, "ignoring non-blob entry")
 				continue
 			}
-
-			name := path.Base(entry.Path)
-			if strings.ContainsRune(name, '.') {
-				name = name[:strings.LastIndex(name, ".")]
-			}
-			logger = logger.With("dashboard_name", name)
 
 			info, err := repo.Read(r.Context(), logger, entry.Path, ref)
 			if err != nil {
@@ -120,101 +96,11 @@ func (c *importConnector) Connect(
 				return
 			}
 
-			// NOTE: We're validating here to make sure we want the folders to be created.
-			//  If the file isn't valid, its folders aren't relevant, either.
-			file, err := fileParser.parse(r.Context(), logger, info, true)
-			if err != nil {
-				logger.DebugContext(ctx, "error on parsing the entry's data", "error", err)
-				if errors.Is(err, ErrUnableToReadResourceBytes) {
-					// Non-resource data is not relevant to us.
-					logger.DebugContext(ctx, "ignoring file due to being a non-resource", "error", err)
-					continue
-				}
-				responder.Error(apierrors.NewInternalError(fmt.Errorf("failed to read %s: %w", entry.Path, err)))
+			if err := replicator.Replicate(r.Context(), info); err != nil {
+				logger.DebugContext(ctx, "error on replicating the entry", "error", err)
+				responder.Error(apierrors.NewInternalError(fmt.Errorf("failed to replicate %s: %w", entry.Path, err)))
 				return
 			}
-
-			// TODO: Don't create folders for resources that aren't folder-able. Maybe we can check the annotations for this?
-			dir := path.Dir(entry.Path)
-			if dir != "." && dir != "/" {
-				logger := logger.With("dir", dir)
-				logger.DebugContext(ctx, "creating directories")
-				for _, folder := range strings.Split(dir, "/") {
-					logger := logger.With("folder", folder)
-					obj, err := folderIface.Get(r.Context(), folder, metav1.GetOptions{})
-					// FIXME: Check for IsNotFound properly
-					if obj != nil || err == nil {
-						logger.DebugContext(ctx, "folder already existed")
-						continue
-					}
-
-					_, err = folderIface.Create(r.Context(), &unstructured.Unstructured{
-						Object: map[string]interface{}{
-							"metadata": map[string]any{
-								"name":      folder,
-								"namespace": ns,
-							},
-							"spec": map[string]any{
-								"title":       folder, // TODO: how do we want to get this?
-								"description": "Repository-managed folder.",
-							},
-						},
-					}, metav1.CreateOptions{})
-					if err != nil {
-						logger.DebugContext(ctx, "failed to create folder", "error", err)
-						responder.Error(apierrors.NewInternalError(fmt.Errorf("got error when creating folder %s: %w", folder, err)))
-						return
-					}
-					logger.DebugContext(ctx, "folder created")
-
-					// TODO: folder parents
-					// TODO: the top-most folder's parent must be the repo folder.
-				}
-			}
-
-			wrapper := file.AsResourceWrapper()
-			resource := wrapper.Resource.File
-			resource.SetNestedField(name, "metadata", "name")
-			resource.SetNestedField(ns, "metadata", "namespace")
-			if folder := path.Base(dir); folder != "." && folder != "/" {
-				resource.SetNestedField(path.Base(dir), "metadata", "annotations", apiutils.AnnoKeyFolder)
-			}
-
-			gv, err := schema.ParseGroupVersion(resource.GetNestedString("apiVersion"))
-			if err != nil {
-				logger.DebugContext(ctx, "invalid GroupVersion was given as the apiVersion of the parsed object?", "error", err)
-				responder.Error(apierrors.NewInternalError(fmt.Errorf("got an invalid groupversion from a parsed file %s: %w", entry.Path, err)))
-				return
-			}
-			gvk := gv.WithKind(resource.GetNestedString("kind"))
-			logger = logger.With("gvk", gvk)
-			gvr, ok := kinds.Resource(gvk)
-			if !ok {
-				logger.DebugContext(ctx, "got GVK of a resource we don't know how to control; ignoring")
-				continue
-			}
-			iface := client.Resource(gvr).Namespace(ns)
-			_, err = iface.Get(r.Context(), name, metav1.GetOptions{})
-			// FIXME: Remove the 'false &&' when .Get returns 404 on 404 instead of 500. Until then, this is a really ugly workaround.
-			if false && err != nil && !apierrors.IsNotFound(err) {
-				logger.DebugContext(ctx, "failed to check if the object already exists", "error", err)
-				responder.Error(apierrors.NewInternalError(fmt.Errorf("failed to check if object already exists: %w", err)))
-				return
-			}
-			kubeObj := resource.ToKubernetesObject()
-			logger.DebugContext(ctx, "upserting kube object", "name", kubeObj.GetName())
-			if err != nil { // IsNotFound
-				_, err = iface.Create(r.Context(), kubeObj, metav1.CreateOptions{})
-			} else { // already exists
-				_, err = iface.Update(r.Context(), kubeObj, metav1.UpdateOptions{})
-			}
-			if err != nil {
-				logger.DebugContext(ctx, "failed to upsert object", "error", err)
-				responder.Error(err) // FIXME
-				return
-			}
-
-			logger.DebugContext(ctx, "successfully upserted the object")
 		}
 
 		responder.Object(http.StatusOK, &provisioning.ResourceWrapper{})
--- pkg/registry/apis/provisioning/replicator.go
+++ pkg/registry/apis/provisioning/replicator.go
@@ -5,12 +5,16 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+	"path"
 	"path/filepath"
 	"strings"
 
+	apiutils "github.com/grafana/grafana/pkg/apimachinery/utils"
 	"github.com/grafana/grafana/pkg/registry/apis/provisioning/repository"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 )
 
@@ -35,11 +39,23 @@ func (f *replicatorFactory) New() (repository.FileReplicator, error) {
 	kinds := newKindsLookup(dynamicClient)
 	parser := newFileParser(f.namespace, dynamicClient, kinds)
 
+	folderGVR, ok := kinds.Resource(schema.GroupVersionKind{
+		Group:   "folder.grafana.app",
+		Version: "v0alpha1",
+		Kind:    "Folder",
+	})
+	if !ok {
+		return nil, errors.New("missing folder resource")
+	}
+
+	folders := dynamicClient.Resource(folderGVR).Namespace(f.namespace)
+
 	return &replicator{
 		namespace: f.namespace,
 		logger:    slog.Default().With("logger", "replicator", "namespace", f.namespace),
 		parser:    parser,
 		client:    dynamicClient,
+		folders:   folders,
 	}, nil
 }
 
@@ -48,6 +64,7 @@ type replicator struct {
 	logger    *slog.Logger
 	client    *dynamic.DynamicClient
 	parser    *fileParser
+	folders   dynamic.ResourceInterface
 }
 
 // Replicate creates a new resource in the cluster.
@@ -61,11 +78,24 @@ func (r *replicator) Replicate(ctx context.Context, fileInfo *repository.FileInf
 
 	name := resourceName(fileInfo.Path)
 
+	parent, err := r.createFolderPath(ctx, fileInfo.Path)
+	if err != nil {
+		return fmt.Errorf("failed to create folder path: %w", err)
+	}
+
 	iface := r.client.Resource(*file.gvr).Namespace(r.namespace)
 	_, err = iface.Get(ctx, name, metav1.GetOptions{})
 	// FIXME: Remove the 'false &&' when .Get returns 404 on 404 instead of 500. Until then, this is a really ugly workaround.
 	if false && err != nil && !apierrors.IsNotFound(err) {
 		return fmt.Errorf("failed to check if object already exists: %w", err)
+	}
+
+	resource := file.AsResourceWrapper().Resource.File
+	resource.SetNestedField(name, "metadata", "name")
+	resource.SetNestedField(r.namespace, "metadata", "namespace")
+
+	if parent != "" {
+		resource.SetNestedField(parent, "metadata", "annotations", apiutils.AnnoKeyFolder)
 	}
 
 	obj := file.AsResourceWrapper().Resource.File.ToKubernetesObject()
@@ -82,6 +112,53 @@ func (r *replicator) Replicate(ctx context.Context, fileInfo *repository.FileInf
 	r.logger.InfoContext(ctx, "Replicated file", "name", name, "path", fileInfo.Path)
 
 	return nil
+}
+
+func (r *replicator) createFolderPath(ctx context.Context, filePath string) (string, error) {
+	dir := filepath.Dir(filePath)
+	if dir == "." || dir == "/" {
+		return "", nil
+	}
+
+	logger := r.logger.With("file", filePath)
+
+	for _, folder := range strings.Split(dir, "/") {
+		logger := logger.With("folder", folder)
+		obj, err := r.folders.Get(ctx, folder, metav1.GetOptions{})
+		// FIXME: Check for IsNotFound properly
+		if obj != nil || err == nil {
+			logger.DebugContext(ctx, "folder already existed")
+			continue
+		}
+
+		_, err = r.folders.Create(ctx, &unstructured.Unstructured{
+			Object: map[string]interface{}{
+				"metadata": map[string]any{
+					"name":      folder,
+					"namespace": r.namespace,
+				},
+				"spec": map[string]any{
+					"title":       folder, // TODO: how do we want to get this?
+					"description": "Repository-managed folder.",
+				},
+			},
+		}, metav1.CreateOptions{})
+		if err != nil {
+			return "", fmt.Errorf("failed to create folder %s: %w", folder, err)
+		}
+
+		logger.DebugContext(ctx, "folder created")
+
+		// TODO: folder parents
+		// TODO: the top-most folder's parent must be the repo folder.
+	}
+
+	parent := path.Base(filePath)
+	if parent == "." || parent == "/" {
+		return "", nil
+	}
+
+	return parent, nil
 }
 
 func (r *replicator) Delete(ctx context.Context, fileInfo *repository.FileInfo) error {
@@ -107,15 +184,17 @@ func (r *replicator) Delete(ctx context.Context, fileInfo *repository.FileInfo) 
 		return fmt.Errorf("failed to delete object: %w", err)
 	}
 
+	// TOOD: prune empty folders
+
 	r.logger.InfoContext(ctx, "Deleted file", "name", name, "path", fileInfo.Path)
 
 	return nil
 }
 
 func (r *replicator) parseResource(ctx context.Context, fileInfo *repository.FileInfo) (*parsedFile, error) {
-	// NOTE: We don't need validation because we're about to apply the data for real.
-	// If the data is invalid, we'll know then!
-	file, err := r.parser.parse(ctx, r.logger, fileInfo, false)
+	// NOTE: We're validating here to make sure we want the folders to be created.
+	//  If the file isn't valid, its folders aren't relevant, either.
+	file, err := r.parser.parse(ctx, r.logger, fileInfo, true)
 	if err != nil {
 		if errors.Is(err, ErrUnableToReadResourceBytes) {
 			return nil, ErrUnableToReadResourceBytes
