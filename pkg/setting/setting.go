c346a4545cd7645c606ed2d13ddde4b16fdcb95e
Hugo Oshiro <hugo.oshiro@grafana.com>
2024-11-27 12:25:51 +0100

    Plugins: Use grafana-com sso_api_token

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -195,7 +195,6 @@ type Cfg struct {
 	PluginSkipPublicKeyDownload      bool
 	DisablePlugins                   []string
 	HideAngularDeprecation           []string
-	PluginInstallToken               string
 	ForwardHostEnvVars               []string
 	PreinstallPlugins                []InstallPlugin
 	PreinstallPluginsAsync           bool
