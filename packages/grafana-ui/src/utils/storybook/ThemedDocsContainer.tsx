ba8f8280dd9e8dd688ce39a5e694e4c461224704
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-28 14:58:36 +0100

    Chore: Bump Storybook to 8.4.x (#96128)
    * feat(storybook): upgrade to version 8.4
    
    * chore(grafana-ui): replace all usage of preview with canvas
    
    * chore(grafana-ui): add fs-extra as dev dependency
    
    * feat(storybook): copy required assets to temp static directory due to 8.4 not supporting file paths
    
    * chore(yarn): fix up lock file so swc-loader doesnt break for decoupled plugins
    
    * Add ExampleFrame component to render grafana-ui examples
    
    * Prevent Storybook from styling in ExampleFrame
    
    * Use global styles in Storybook docs
    
    * Update mdx docs to use ExampleFrame or correct Canvas usage
    
    * update AutoSizeInput
    
    * Update Index mdx
    
    * remove the gfm mdx package
    
    * silence sass warnings
    
    * fix(storybook): add missing imports to fix failed rendering of stories/docs
    
    * remove empty docs
    
    ---------
    
    Co-authored-by: joshhunt <josh@trtr.co>

--- packages/grafana-ui/src/utils/storybook/ThemedDocsContainer.tsx
+++ packages/grafana-ui/src/utils/storybook/ThemedDocsContainer.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
 
 import { GrafanaLight, GrafanaDark } from '../../../.storybook/storybookTheme';
+import { GlobalStyles } from '../../themes';
 
 type Props = {
   context: DocsContextProps;
@@ -15,6 +16,7 @@ export const ThemedDocsContainer = ({ children, context }: Props) => {
 
   return (
     <DocsContainer theme={dark ? GrafanaDark : GrafanaLight} context={context}>
+      <GlobalStyles />
       {children}
     </DocsContainer>
   );
