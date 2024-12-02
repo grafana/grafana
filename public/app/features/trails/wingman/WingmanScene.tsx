2cc70e2bd4b533468823b91bf4b97a6d4decba4a
ismail simsek <ismailsimsek09@gmail.com>
2024-12-02 15:34:01 +0100

    type safety

--- public/app/features/trails/wingman/WingmanScene.tsx
+++ public/app/features/trails/wingman/WingmanScene.tsx
@@ -1,16 +1,38 @@
 import { SceneComponentProps, SceneObjectBase, SceneObjectState } from '@grafana/scenes';
+import { InlineField, InlineSwitch } from '@grafana/ui';
 
-interface WingmanSceneState extends SceneObjectState {}
+import { AllowedWingmanOptions, getWingmanOptionCollection, WingmanOptionCollection } from './wingman';
+
+interface WingmanSceneState extends SceneObjectState {
+  wingmanOptions: WingmanOptionCollection;
+}
 
 export class WingmanScene extends SceneObjectBase<WingmanSceneState> {
   constructor(props: Partial<WingmanSceneState>) {
-    super({});
+    super({
+      wingmanOptions: getWingmanOptionCollection(),
+    });
   }
 
+  onWingmanOptionChanged = (optId: AllowedWingmanOptions, newValue: boolean) => {
+    const updatedWingmanOptions = { ...this.state.wingmanOptions };
+    updatedWingmanOptions[optId].enabled = newValue;
+    this.setState({ wingmanOptions: updatedWingmanOptions });
+  };
+
   public static Component = ({ model }: SceneComponentProps<WingmanScene>) => {
+    const { wingmanOptions } = model.useState();
+    const { onWingmanOptionChanged } = model;
     return (
       <div>
-        <div>wingman items will be here</div>
+        {Object.entries(wingmanOptions).map(([key, opt]) => (
+          <InlineField labelWidth={26} key={key} label={opt.label} tooltip={opt.description}>
+            <InlineSwitch
+              value={opt.enabled}
+              onChange={() => onWingmanOptionChanged(key as AllowedWingmanOptions, !opt.enabled)}
+            />
+          </InlineField>
+        ))}
       </div>
     );
   };
--- public/app/features/trails/wingman/wingman.ts
+++ public/app/features/trails/wingman/wingman.ts
@@ -5,14 +5,28 @@ import { MetricSelectScene } from '../MetricSelect/MetricSelectScene';
 
 import { WingmanScene } from './WingmanScene';
 
+export type AllowedWingmanOptions = 'opt_1' | 'opt_2' | 'opt_3';
+
+export type WingmanOption = {
+  label: string;
+  description: string;
+  enabled: boolean;
+};
+
+export type WingmanOptionCollection = Record<AllowedWingmanOptions, WingmanOption>;
+
+export const getWingmanOptionCollection = (): WingmanOptionCollection => ({
+  opt_1: { label: 'Option 1', description: 'Option 1', enabled: true },
+  opt_2: { label: 'Option 2', description: 'Option 2', enabled: true },
+  opt_3: { label: 'Option 3', description: 'Option 3', enabled: false },
+});
+
 export function withWingman(topScene: MetricScene | MetricSelectScene) {
   return new EmbeddedScene({
     body: new SceneFlexLayout({
       direction: 'row',
       children: [
         new SceneFlexItem({
-          width: '20%',
-          height: '100%',
           body: new WingmanScene({}),
         }),
         topScene,
