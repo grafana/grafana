6e02cb72203f09dd0ec4ac2dbad3aab3bec215e6
Kat Yang <69819079+yangkb09@users.noreply.github.com>
2024-12-02 10:08:54 -0500

    Style: Update homepage styles (#96860)
    * style: increase size of the ‘let’s start’ section; decrease number of filter lines in the trail cards; remove DS line; change select metric to all metrics
    
    * chore: delete unused get datasource test mock
    
    * fix: adjust test to account for select metric to all metrics change
    
    * chore: update i18n
    
    * text: update truncate long labels test to truncate after 2 lines
    
    * style: shrink card height

--- public/app/features/trails/DataTrailCard.tsx
+++ public/app/features/trails/DataTrailCard.tsx
@@ -9,7 +9,7 @@ import { Trans } from 'app/core/internationalization';
 import { DataTrail } from './DataTrail';
 import { getTrailStore, DataTrailBookmark } from './TrailStore/TrailStore';
 import { VAR_FILTERS } from './shared';
-import { getDataSource, getDataSourceName, getMetricName } from './utils';
+import { getMetricName } from './utils';
 
 export type Props = {
   trail?: DataTrail;
@@ -46,7 +46,6 @@ export function DataTrailCard(props: Props) {
     const createdAt = bookmark?.createdAt || trail.state.createdAt;
 
     return {
-      dsValue: getDataSource(trail),
       filters: filtersVariable.state.filters,
       metric: trail.state.metric,
       createdAt,
@@ -57,16 +56,13 @@ export function DataTrailCard(props: Props) {
     return null;
   }
 
-  const { dsValue, filters, metric, createdAt } = values;
+  const { filters, metric, createdAt } = values;
 
   return (
     <div>
       <Card onClick={onSelect} className={styles.card}>
         <Card.Heading>
-          <div className={styles.metricLabel}>
-            <Trans i18nKey="trails.card.metric">Metric:</Trans>
-          </div>
-          <div className={styles.metricValue}>{getMetricName(metric)}</div>
+          <div className={styles.metricValue}>{truncateValue('', getMetricName(metric), 39)}</div>
         </Card.Heading>
         <Card.Meta className={styles.meta}>
           {filters.map((f) => (
@@ -76,12 +72,6 @@ export function DataTrailCard(props: Props) {
             </span>
           ))}
         </Card.Meta>
-        <div className={styles.datasource}>
-          <div className={styles.secondaryFont}>
-            <Trans i18nKey="trails.card.data-source">Data source: </Trans>
-          </div>
-          <div className={styles.primaryFont}>{dsValue && getDataSourceName(dsValue)}</div>
-        </div>
         <div className={styles.deleteButton}>
           {onDelete && (
             <Card.SecondaryActions>
@@ -124,7 +114,6 @@ export function getStyles(theme: GrafanaTheme2) {
       fontSize: '14px',
       fontStyle: 'normal',
       fontWeight: 500,
-      marginLeft: '8px', // Add space between the label and the value
       wordBreak: 'break-all',
     }),
     tag: css({
@@ -136,7 +125,7 @@ export function getStyles(theme: GrafanaTheme2) {
       position: 'relative',
       width: '318px',
       padding: `12px ${theme.spacing(2)} ${theme.spacing(1)} ${theme.spacing(2)}`,
-      height: '152px',
+      height: '110px',
       alignItems: 'start',
       marginBottom: 0,
       borderTop: `1px solid ${theme.colors.border.weak}`,
@@ -150,9 +139,6 @@ export function getStyles(theme: GrafanaTheme2) {
       color: theme.colors.text.secondary,
       fontSize: '12px',
     }),
-    datasource: css({
-      gridArea: 'Description',
-    }),
     date: css({
       border: `1px solid ${theme.colors.border.weak}`,
       // eslint-disable-next-line @grafana/no-border-radius-literal
@@ -164,8 +150,7 @@ export function getStyles(theme: GrafanaTheme2) {
       flexWrap: 'wrap',
       overflow: 'hidden',
       textOverflow: 'ellipsis',
-      maxHeight: '54px',
-      width: '100%',
+      maxHeight: '36px', // 2 lines * 18px line-height
       margin: 0,
       gridArea: 'Meta',
       color: theme.colors.text.secondary,
