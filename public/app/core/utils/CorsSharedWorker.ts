3670b195777faea965817ffab3df0d6229f34a3e
Piotr Jamróz <pm.jamroz@gmail.com>
2024-12-02 15:12:36 +0100

    Frontend o11y: Load SharedWorkers for crash detection (#96673)
    * Load SharedWorkers from blob
    
    * Fix typo
    
    * Update docs
    
    * Add more docs
    
    * Simplify extending CorsSharedWorker
    
    * Revert "Simplify extending CorsSharedWorker"
    
    This reverts commit 1603e5f02f995f325011713e1bc64fb3267f150b.
    
    * Simplify extending CorsSharedWorker
    
    * Remove ts-ignore
    
    * Update betterer and add docs
    
    * Update public/app/core/crash/index.ts
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>
    
    * Update public/app/core/utils/CorsSharedWorker.ts
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>
    
    * Update public/app/core/crash/index.ts
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>
    
    * Update public/app/core/utils/CorsSharedWorker.ts
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>
    
    * Simplify getting scriptsBasePathUrl
    
    * Disable linting for SharedWorker type assertion
    
    ---------
    
    Co-authored-by: Sven Grossmann <sven.grossmann@grafana.com>

--- public/app/core/utils/CorsSharedWorker.ts
+++ public/app/core/utils/CorsSharedWorker.ts
@@ -1,6 +1,17 @@
-// works with webpack plugin: scripts/webpack/plugins/CorsWorkerPlugin.js
-export class CorsWorker extends window.Worker {
+// Almost identical to CorsWorker.ts. Main difference being it allows loading a SharedWorker if browser supports it
+
+export function sharedWorkersSupported() {
+  return typeof window.SharedWorker !== 'undefined';
+}
+
+/**
+ * Creating CorsSharedWorker should be called only if sharedWorkersSupported() is truthy
+ */
+export class CorsSharedWorker {
   constructor(url: URL, options?: WorkerOptions) {
+    if (!sharedWorkersSupported()) {
+      throw new Error('SharedWorker is not supported');
+    }
     // by default, worker inherits HTML document's location and pathname which leads to wrong public path value
     // the CorsWorkerPlugin will override it with the value based on the initial worker chunk, ie.
     //    initial worker chunk: http://host.com/cdn/scripts/worker-123.js
@@ -15,7 +26,8 @@ export class CorsWorker extends window.Worker {
         type: 'application/javascript',
       })
     );
-    super(objectURL, options);
+    const worker = new SharedWorker(objectURL, options);
     URL.revokeObjectURL(objectURL);
+    return worker;
   }
 }
