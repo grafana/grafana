119d5f7ca16f14e4a6344c182a368610c595e315
owensmallwood <owen.smallwood@grafana.com>
2024-12-03 15:56:13 -0600

    Unified Storage: Use tls preferred when grafana db using ssl (#97378)
    When unified storage is using the grafana db (default case for on-prem), if the db is using ssl then set tls=preferred. This fixes an on-prem bug in 11.3.1.
    
    (cherry picked from commit 9642eedce6e29e840f94cbbf782fcfb10372e837)

--- pkg/storage/unified/sql/db/dbimpl/dbEngine.go
+++ pkg/storage/unified/sql/db/dbimpl/dbEngine.go
@@ -26,6 +26,10 @@ func getEngineMySQL(getter confGetter, tracer tracing.Tracer) (*xorm.Engine, err
 		// See: https://dev.mysql.com/doc/refman/en/sql-mode.html
 		"@@SESSION.sql_mode": "ANSI",
 	}
+	sslMode := getter.String("ssl_mode")
+	if sslMode == "true" || sslMode == "skip-verify" {
+		config.Params["tls"] = "preferred"
+	}
 	tls := getter.String("tls")
 	if tls != "" {
 		config.Params["tls"] = tls
