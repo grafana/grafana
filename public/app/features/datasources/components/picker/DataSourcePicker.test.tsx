c29873a50e0a7cd8a9d0e53fa9cb7b59283c20ca
Jack Westbrook <jack.westbrook@gmail.com>
2024-12-03 07:22:49 +0100

    Frontend: Set moduleResolution to bundler in TSconfig (#96450)
    * build(typescript): switch module resolution to bundler for exports imports goodness
    
    * chore: bump react-calendar and temp patch prometheus-io package
    
    * Wip
    
    * feat(decoupled-plugin): fix types errors in tests by including testing-library/jest-dom
    
    * chore(betterer): pass custom tsconfig so betterer continues to run

--- public/app/features/datasources/components/picker/DataSourcePicker.test.tsx
+++ public/app/features/datasources/components/picker/DataSourcePicker.test.tsx
@@ -1,6 +1,5 @@
 import { findByText, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
+import userEvent, { UserEvent } from '@testing-library/user-event';
 
 import {
   DataSourceInstanceSettings,
