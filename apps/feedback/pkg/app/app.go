a4b9827eb33f4bcb1a250879ac5d5d1fe13f66b5
Matheus Macabu <macabu.matheus@gmail.com>
2024-12-03 16:04:36 +0100

    Bootstrap operator

---
+++ apps/feedback/pkg/app/app.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/grafana/grafana-app-sdk/app"
+	"github.com/grafana/grafana-app-sdk/logging"
+	"github.com/grafana/grafana-app-sdk/simple"
+
+	apis "github.com/grafana/grafana/apps/feedback/pkg/apis"
+	feedbackv0alpha1 "github.com/grafana/grafana/apps/feedback/pkg/apis/feedback/v0alpha1"
+	"github.com/grafana/grafana/apps/feedback/pkg/watchers"
+)
+
+func Provider(appCfg app.SpecificConfig) app.Provider {
+	return simple.NewAppProvider(apis.LocalManifest(), appCfg, New)
+}
+
+func New(cfg app.Config) (app.App, error) {
+	feedbackWatcher, err := watchers.NewFeedbackWatcher()
+	if err != nil {
+		return nil, fmt.Errorf("unable to create FeedbackWatcher: %w", err)
+	}
+
+	config := simple.AppConfig{
+		Name:       "feedback",
+		KubeConfig: cfg.KubeConfig,
+		InformerConfig: simple.AppInformerConfig{
+			ErrorHandler: func(ctx context.Context, err error) {
+				// FIXME: add your own error handling here
+				logging.FromContext(ctx).With("error", err).Error("Informer processing error")
+			},
+		},
+		ManagedKinds: []simple.AppManagedKind{
+			{
+				Kind:    feedbackv0alpha1.FeedbackKind(),
+				Watcher: feedbackWatcher,
+			},
+		},
+	}
+
+	// Create the App
+	a, err := simple.NewApp(config)
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate the capabilities against the provided manifest to make sure there isn't a mismatch
+	err = a.ValidateManifest(cfg.ManifestData)
+	return a, err
+}
---
+++ apps/feedback/pkg/watchers/watcher_feedback.go
@@ -0,0 +1,88 @@
+package watchers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/grafana/grafana-app-sdk/logging"
+	"github.com/grafana/grafana-app-sdk/operator"
+	"github.com/grafana/grafana-app-sdk/resource"
+	"go.opentelemetry.io/otel"
+
+	feedback "github.com/grafana/grafana/apps/feedback/pkg/apis/feedback/v0alpha1"
+)
+
+var _ operator.ResourceWatcher = &FeedbackWatcher{}
+
+type FeedbackWatcher struct{}
+
+func NewFeedbackWatcher() (*FeedbackWatcher, error) {
+	return &FeedbackWatcher{}, nil
+}
+
+// Add handles add events for feedback.Feedback resources.
+func (s *FeedbackWatcher) Add(ctx context.Context, rObj resource.Object) error {
+	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-add")
+	defer span.End()
+	object, ok := rObj.(*feedback.Feedback)
+	if !ok {
+		return fmt.Errorf("provided object is not of type *feedback.Feedback (name=%s, namespace=%s, kind=%s)",
+			rObj.GetStaticMetadata().Name, rObj.GetStaticMetadata().Namespace, rObj.GetStaticMetadata().Kind)
+	}
+
+	// TODO
+	logging.FromContext(ctx).Debug("Added resource", "name", object.GetStaticMetadata().Identifier().Name)
+	return nil
+}
+
+// Update handles update events for feedback.Feedback resources.
+func (s *FeedbackWatcher) Update(ctx context.Context, rOld resource.Object, rNew resource.Object) error {
+	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-update")
+	defer span.End()
+	oldObject, ok := rOld.(*feedback.Feedback)
+	if !ok {
+		return fmt.Errorf("provided object is not of type *feedback.Feedback (name=%s, namespace=%s, kind=%s)",
+			rOld.GetStaticMetadata().Name, rOld.GetStaticMetadata().Namespace, rOld.GetStaticMetadata().Kind)
+	}
+
+	_, ok = rNew.(*feedback.Feedback)
+	if !ok {
+		return fmt.Errorf("provided object is not of type *feedback.Feedback (name=%s, namespace=%s, kind=%s)",
+			rNew.GetStaticMetadata().Name, rNew.GetStaticMetadata().Namespace, rNew.GetStaticMetadata().Kind)
+	}
+
+	// TODO
+	logging.FromContext(ctx).Debug("Updated resource", "name", oldObject.GetStaticMetadata().Identifier().Name)
+	return nil
+}
+
+// Delete handles delete events for feedback.Feedback resources.
+func (s *FeedbackWatcher) Delete(ctx context.Context, rObj resource.Object) error {
+	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-delete")
+	defer span.End()
+	object, ok := rObj.(*feedback.Feedback)
+	if !ok {
+		return fmt.Errorf("provided object is not of type *feedback.Feedback (name=%s, namespace=%s, kind=%s)",
+			rObj.GetStaticMetadata().Name, rObj.GetStaticMetadata().Namespace, rObj.GetStaticMetadata().Kind)
+	}
+
+	// TODO
+	logging.FromContext(ctx).Debug("Deleted resource", "name", object.GetStaticMetadata().Identifier().Name)
+	return nil
+}
+
+// Sync is not a standard resource.Watcher function, but is used when wrapping this watcher in an operator.OpinionatedWatcher.
+// It handles resources which MAY have been updated during an outage period where the watcher was not able to consume events.
+func (s *FeedbackWatcher) Sync(ctx context.Context, rObj resource.Object) error {
+	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-sync")
+	defer span.End()
+	object, ok := rObj.(*feedback.Feedback)
+	if !ok {
+		return fmt.Errorf("provided object is not of type *feedback.Feedback (name=%s, namespace=%s, kind=%s)",
+			rObj.GetStaticMetadata().Name, rObj.GetStaticMetadata().Namespace, rObj.GetStaticMetadata().Kind)
+	}
+
+	// TODO
+	logging.FromContext(ctx).Debug("Possible resource update", "name", object.GetStaticMetadata().Identifier().Name)
+	return nil
+}
