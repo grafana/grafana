ae553b25b700813be196020d2947384fb5f31a0f
Michael Mandrus <41969079+mmandrus@users.noreply.github.com>
2024-12-04 04:45:27 -0500

    wip: support screenshots for report button (#97383)
    * support for images
    
    * make it work
    
    * fix double import and typo
    
    * less convinced this is hideous now
    
    * remove print statement
    
    * double import

--- apps/feedback/pkg/apis/feedback/v0alpha1/feedback_status_gen.go
+++ apps/feedback/pkg/apis/feedback/v0alpha1/feedback_status_gen.go
@@ -1,70 +1,44 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
 package v0alpha1
 
-// Defines values for FeedbackOperatorStateState.
-const (
-	FeedbackOperatorStateStateFailed     FeedbackOperatorStateState = "failed"
-	FeedbackOperatorStateStateInProgress FeedbackOperatorStateState = "in_progress"
-	FeedbackOperatorStateStateSuccess    FeedbackOperatorStateState = "success"
-)
-
-// Defines values for FeedbackstatusOperatorStateState.
-const (
-	FeedbackstatusOperatorStateStateFailed     FeedbackstatusOperatorStateState = "failed"
-	FeedbackstatusOperatorStateStateInProgress FeedbackstatusOperatorStateState = "in_progress"
-	FeedbackstatusOperatorStateStateSuccess    FeedbackstatusOperatorStateState = "success"
-)
-
-// FeedbackOperatorState defines model for FeedbackOperatorState.
 // +k8s:openapi-gen=true
-type FeedbackOperatorState struct {
-	// descriptiveState is an optional more descriptive state field which has no requirements on format
-	DescriptiveState *string `json:"descriptiveState,omitempty"`
-
-	// details contains any extra information that is operator-specific
-	Details map[string]interface{} `json:"details,omitempty"`
-
+type FeedbackstatusOperatorState struct {
 	// lastEvaluation is the ResourceVersion last evaluated
 	LastEvaluation string `json:"lastEvaluation"`
-
 	// state describes the state of the lastEvaluation.
 	// It is limited to three possible states for machine evaluation.
-	State FeedbackOperatorStateState `json:"state"`
+	State FeedbackStatusOperatorStateState `json:"state"`
+	// descriptiveState is an optional more descriptive state field which has no requirements on format
+	DescriptiveState *string `json:"descriptiveState,omitempty"`
+	// details contains any extra information that is operator-specific
+	Details map[string]any `json:"details,omitempty"`
 }
 
-// FeedbackOperatorStateState state describes the state of the lastEvaluation.
-// It is limited to three possible states for machine evaluation.
-// +k8s:openapi-gen=true
-type FeedbackOperatorStateState string
+// NewFeedbackstatusOperatorState creates a new FeedbackstatusOperatorState object.
+func NewFeedbackstatusOperatorState() *FeedbackstatusOperatorState {
+	return &FeedbackstatusOperatorState{}
+}
 
-// FeedbackStatus defines model for FeedbackStatus.
 // +k8s:openapi-gen=true
 type FeedbackStatus struct {
-	// additionalFields is reserved for future use
-	AdditionalFields map[string]interface{} `json:"additionalFields,omitempty"`
-
 	// operatorStates is a map of operator ID to operator state evaluations.
 	// Any operator which consumes this kind SHOULD add its state evaluation information to this field.
 	OperatorStates map[string]FeedbackstatusOperatorState `json:"operatorStates,omitempty"`
+	// additionalFields is reserved for future use
+	AdditionalFields map[string]any `json:"additionalFields,omitempty"`
 }
 
-// FeedbackstatusOperatorState defines model for Feedbackstatus.#OperatorState.
-// +k8s:openapi-gen=true
-type FeedbackstatusOperatorState struct {
-	// descriptiveState is an optional more descriptive state field which has no requirements on format
-	DescriptiveState *string `json:"descriptiveState,omitempty"`
-
-	// details contains any extra information that is operator-specific
-	Details map[string]interface{} `json:"details,omitempty"`
-
-	// lastEvaluation is the ResourceVersion last evaluated
-	LastEvaluation string `json:"lastEvaluation"`
-
-	// state describes the state of the lastEvaluation.
-	// It is limited to three possible states for machine evaluation.
-	State FeedbackstatusOperatorStateState `json:"state"`
+// NewFeedbackStatus creates a new FeedbackStatus object.
+func NewFeedbackStatus() *FeedbackStatus {
+	return &FeedbackStatus{}
 }
 
-// FeedbackstatusOperatorStateState state describes the state of the lastEvaluation.
-// It is limited to three possible states for machine evaluation.
 // +k8s:openapi-gen=true
-type FeedbackstatusOperatorStateState string
+type FeedbackStatusOperatorStateState string
+
+const (
+	StatusOperatorStateStateSuccess    FeedbackStatusOperatorStateState = "success"
+	StatusOperatorStateStateInProgress FeedbackStatusOperatorStateState = "in_progress"
+	StatusOperatorStateStateFailed     FeedbackStatusOperatorStateState = "failed"
+)
