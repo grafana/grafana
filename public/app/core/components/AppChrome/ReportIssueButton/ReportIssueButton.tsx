85e0a22d2519ad663c1ed2cc3d429a3da367ed0a
Sarah Zinger <sarah.zinger@grafana.com>
2024-12-04 11:21:23 -0500

    Use actual message

--- public/app/core/components/AppChrome/ReportIssueButton/ReportIssueButton.tsx
+++ public/app/core/components/AppChrome/ReportIssueButton/ReportIssueButton.tsx
@@ -9,51 +9,66 @@ import { canvasToBase64String, extractImageTypeAndData } from '../../../../featu
 
 export interface Props {}
 
-export const ReportIssueButton = ({}: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+const MenuActions = () => {
+  const [formData, setFormData] = useState({ message: '' });
+  const onClick = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    let screenshot = null;
 
-  const MenuActions = () => {
-    const onClick = async (e: { preventDefault: () => void }) => {
-      e.preventDefault();
-      console.log('hi', e);
+    const element = document.body; // TODO: choose a different selector?
+    if (element) {
+      const canvas = await html2canvas(element, { backgroundColor: null });
 
-      let screenshot = null;
-
-      const element = document.body; // TODO: choose a different selector?
-      if (element) {
-        const canvas = await html2canvas(element, { backgroundColor: null });
-
-        const encoded = await canvasToBase64String(canvas);
-        if (encoded && typeof encoded === 'string') {
-          screenshot = extractImageTypeAndData(encoded);
-        }
+      const encoded = await canvasToBase64String(canvas);
+      if (encoded && typeof encoded === 'string') {
+        screenshot = extractImageTypeAndData(encoded);
       }
+    }
 
-      const feedback: Spec = {
-        message: 'test sarah test',
-        ...(screenshot && { screenshot: screenshot.data, imageType: screenshot.type }),
-      };
-
-      const feedbackApi = getFeedbackAPI();
-      await feedbackApi.createFeedback(feedback);
+    const feedback: Spec = {
+      message: formData.message,
+      ...(screenshot && { screenshot: screenshot.data, imageType: screenshot.type }),
     };
 
-    return (
-      <Menu>
-          <b>Send feedback to Grafana</b>
-          <Stack gap={2} direction={'column'}>
-            <input placeholder="so what happened?"></input>
-            <Button type="submit" onClick={onClick}>Submit feedback</Button>
-          </Stack>
-      </Menu>
-    );
+    const feedbackApi = getFeedbackAPI();
+    await feedbackApi.createFeedback(feedback);
   };
 
   return (
+    <Menu>
+      <b>Send feedback to Grafana</b>
+
+      <Stack gap={2} direction={'column'}>
+        <input
+          value={formData.message}
+          onChange={(e) => setFormData({ message: e.target.value })}
+          placeholder="so what happened?"
+        ></input>
+        <Button type="submit" onClick={onClick}>
+          Submit feedback
+        </Button>
+      </Stack>
+    </Menu>
+  );
+};
+
+export const ReportIssueButton = ({}: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
     <>
-      <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={setIsOpen}>
+      <Dropdown overlay={<MenuActions />} placement="bottom-end" onVisibleChange={setIsOpen}>
         <ToolbarButton iconOnly icon={'bug'} isOpen={isOpen} aria-label="New" />
       </Dropdown>
     </>
   );
 };
+
+/* 
+  TODO:
+  - get the actual message from the form
+  - fix the styling of the top bar
+  - put this behind a feature flag
+  - screenshot should be behind a button
+  - see if we can get annotations to work
+*/
