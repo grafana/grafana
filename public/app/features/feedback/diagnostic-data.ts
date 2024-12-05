31b1b06d7f4366ef07245ff9086ab91b18636c2b
Matheus Macabu <macabu.matheus@gmail.com>
2024-12-05 12:55:40 +0100

    feedback: find even more data

--- public/app/features/feedback/diagnostic-data.ts
+++ public/app/features/feedback/diagnostic-data.ts
@@ -1,10 +1,13 @@
-import { config, getBackendSrv } from '@grafana/runtime';
+import { config, getBackendSrv, GrafanaBootConfig } from '@grafana/runtime';
+
+type GrafanaBootConfigExtra = GrafanaBootConfig & { licensing: { slug: string }};
 
 export async function getDiagnosticData() {
   const externallyInstalledPlugins = await getBackendSrv().get('/api/plugins', { embedded: 0, core: 0, enabled: 1 });
 
   return {
     instance: {
+      ...((config as GrafanaBootConfigExtra)?.licensing?.slug && { slug: (config as GrafanaBootConfigExtra)?.licensing?.slug }),
       version: config?.buildInfo?.versionString,
       edition: config?.licenseInfo?.edition,
       apps: Object.keys(config?.apps),
@@ -19,7 +22,20 @@ export async function getDiagnosticData() {
       featureToggles: Object.keys(config?.featureToggles),
       rbacEnabled: config.rbacEnabled,
       samlEnabled: config.samlEnabled,
+      ldapEnabled: config.ldapEnabled,
+      hasAngularsupport: config.angularSupportEnabled,
+      authProxyEnabled: config.authProxyEnabled,
+      expressionsEnabled: config.expressionsEnabled,
+      publicDashboardsEnabled: config.publicDashboardsEnabled,
+      queryHistoryEnabled: config.queryHistoryEnabled,
+      recordedQueriesEnabled: config.recordedQueries.enabled,
+      reportingEnabled: config.reporting.enabled,
+      secureSocksDSProxyEnabled: config.secureSocksDSProxyEnabled,
       imageRendererAvailable: config.rendererAvailable,
+      unifiedAlerting: {
+        enabled: config.unifiedAlertingEnabled,
+        ...(config.unifiedAlertingEnabled && { minInterval: config.unifiedAlerting.minInterval }),
+      },
       datasources: Object.values(config?.datasources).map(settings => ({
         name: settings.meta.name,
         type: settings.type,
