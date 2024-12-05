666996192bd8822d7d4871347273ede5a37a2488
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2024-12-05 12:25:46 +0100

    Move createGraphFrames to `grafana/o11y-ds-frontend` package (#97394)
    * Move createGraphFrames to o11y-ds-frontend package
    
    * Remove duplicated mock response
    
    * Rename files and function

--- packages/grafana-o11y-ds-frontend/src/createNodeGraphFrames.ts
+++ packages/grafana-o11y-ds-frontend/src/createNodeGraphFrames.ts
@@ -1,8 +1,80 @@
+import {
+  FieldType,
+  NodeGraphDataFrameFieldNames as Fields,
+  DataFrameView,
+  DataFrame,
+  MutableDataFrame,
+} from '@grafana/data';
+
+export function createNodeGraphFrames(data: DataFrame): DataFrame[] {
+  const { nodes, edges } = convertTraceToGraph(data);
+  const [nodesFrame, edgesFrame] = makeFrames();
+
+  for (const node of nodes) {
+    nodesFrame.add(node);
+  }
+  for (const edge of edges) {
+    edgesFrame.add(edge);
+  }
+
+  return [nodesFrame, edgesFrame];
+}
+
+function convertTraceToGraph(data: DataFrame): { nodes: Node[]; edges: Edge[] } {
+  const nodes: Node[] = [];
+  const edges: Edge[] = [];
+
+  const view = new DataFrameView<TraceRow>(data);
+
+  const traceDuration = findTraceDuration(view);
+  const spanMap = makeSpanMap((index) => {
+    if (index >= data.length) {
+      return undefined;
+    }
+    const span = view.get(index);
+    return {
+      span: { ...span },
+      id: span.spanID,
+      parentIds: span.parentSpanID ? [span.parentSpanID] : [],
+    };
+  });
+
+  for (let i = 0; i < view.length; i++) {
+    const row = view.get(i);
+
+    const ranges: Array<[number, number]> = spanMap[row.spanID].children.map((c) => {
+      const span = spanMap[c].span;
+      return [span.startTime, span.startTime + span.duration];
+    });
+    const childrenDuration = getNonOverlappingDuration(ranges);
+    const selfDuration = row.duration - childrenDuration;
+    const stats = getStats(row.duration, traceDuration, selfDuration);
+
+    nodes.push({
+      [Fields.id]: row.spanID,
+      [Fields.title]: row.serviceName ?? '',
+      [Fields.subTitle]: row.operationName,
+      [Fields.mainStat]: stats.main,
+      [Fields.secondaryStat]: stats.secondary,
+      [Fields.color]: selfDuration / traceDuration,
+    });
+
+    // Sometimes some span can be missing. Don't add edges for those.
+    if (row.parentSpanID && spanMap[row.parentSpanID].span) {
+      edges.push({
+        [Fields.id]: row.parentSpanID + '--' + row.spanID,
+        [Fields.target]: row.spanID,
+        [Fields.source]: row.parentSpanID,
+      });
+    }
+  }
+
+  return { nodes, edges };
+}
+
 /**
  * Get non overlapping duration of the ranges as they can overlap or have gaps.
  */
-import { FieldType, MutableDataFrame, NodeGraphDataFrameFieldNames as Fields } from '@grafana/data';
-
 export function getNonOverlappingDuration(ranges: Array<[number, number]>): number {
   ranges.sort((a, b) => a[0] - b[0]);
   const mergedRanges = ranges.reduce<Array<[number, number]>>((acc, range) => {
@@ -116,4 +188,55 @@ export function makeFrames() {
   });
 
   return [nodesFrame, edgesFrame];
+}
+
+/**
+ * Get the duration of the whole trace as it isn't a part of the response data.
+ * Note: Seems like this should be the same as just longest span, but this is probably safer.
+ */
+function findTraceDuration(view: DataFrameView<TraceRow>): number {
+  let traceEndTime = 0;
+  let traceStartTime = Infinity;
+
+  for (let i = 0; i < view.length; i++) {
+    const row = view.get(i);
+
+    if (row.startTime < traceStartTime) {
+      traceStartTime = row.startTime;
+    }
+
+    if (row.startTime + row.duration > traceEndTime) {
+      traceEndTime = row.startTime + row.duration;
+    }
+  }
+
+  return traceEndTime - traceStartTime;
+}
+
+interface Node {
+  [Fields.id]: string;
+  [Fields.title]: string;
+  [Fields.subTitle]: string;
+  [Fields.mainStat]: string;
+  [Fields.secondaryStat]: string;
+  [Fields.color]: number;
+}
+
+interface Edge {
+  [Fields.id]: string;
+  [Fields.target]: string;
+  [Fields.source]: string;
+}
+
+interface TraceRow {
+  traceID: string;
+  spanID: string;
+  parentSpanID: string;
+  operationName: string;
+  serviceName: string;
+  serviceTags: string;
+  startTime: number;
+  duration: number;
+  logs: string;
+  tags: string;
 }
