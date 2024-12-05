2f1dbdf66782104237db01b241b8d006c979d387
Kat Yang <69819079+yangkb09@users.noreply.github.com>
2024-12-05 10:36:04 -0500

    Kat/hackathon UI (#97413)
    * style: update sidebar styles; wip: label font size
    
    * wip: add 16px margin to left of vertical line
    
    * style: update sidebar border and divider lines

--- public/app/features/trails/wingman/WingmanScene.tsx
+++ public/app/features/trails/wingman/WingmanScene.tsx
@@ -1,3 +1,6 @@
+import { css } from '@emotion/css';
+
+import { GrafanaTheme2 } from '@grafana/data';
 import {
   SceneComponentProps,
   SceneObjectBase,
@@ -6,7 +9,7 @@ import {
   SceneObjectUrlValues,
   SceneObjectWithUrlSync,
 } from '@grafana/scenes';
-import { RadioButtonList } from '@grafana/ui';
+import { RadioButtonList, useStyles2 } from '@grafana/ui';
 
 import { isWingmanGroupKey, useWingmanOptionGroup, WingmanGroupKeyType } from './wingman';
 
@@ -51,25 +54,60 @@ export class WingmanScene extends SceneObjectBase<WingmanSceneState> implements 
   };
 
   public static Component = ({ model }: SceneComponentProps<WingmanScene>) => {
+    const styles = useStyles2(getStyles);
     const state = model.useState();
     const { onWingmanOptionChanged } = model;
     const initialData = useWingmanOptionGroup();
     return (
-      <div>
-        <div>11241 Metrics</div>
+      <div className={styles.verticalLine}>
+        <div className={styles.title}>11241 Metrics</div>
         {initialData.map((group, groupIdx) => (
           <div key={group.title}>
-            <h2>{group.title}</h2>
-            <RadioButtonList<string>
-              name={group.title + '---name'}
-              value={state[group.id]}
-              disabledOptions={group.options.filter((opt) => !opt.available).map((op) => op.id)}
-              options={group.options.map((opt) => ({ label: opt.label, value: opt.id }))}
-              onChange={(val) => onWingmanOptionChanged(group.id, val)}
-            />
+            <div className={styles.horizontalLine} />
+            <h2 className={styles.title}>{group.title}</h2>
+            <div className={styles.label}>
+              <RadioButtonList<string>
+                name={group.title + '---name'}
+                value={state[group.id]}
+                disabledOptions={group.options.filter((opt) => !opt.available).map((op) => op.id)}
+                options={group.options.map((opt) => ({ label: opt.label, value: opt.id }))}
+                onChange={(val) => onWingmanOptionChanged(group.id, val)}
+              />
+            </div>
           </div>
         ))}
       </div>
     );
+  };
+}
+
+export function getStyles(theme: GrafanaTheme2) {
+  return {
+    verticalLine: css({
+      borderRight: `1px solid var(--border-Weak, rgba(204, 204, 220, 0.12))`,
+      height: '540px',
+      paddingRight: '16px',
+    }),
+    title: css({
+      overflow: 'hidden',
+      color: '#FFF',
+      textOverflow: 'ellipsis',
+      fontSize: '14px',
+      fontWeight: 500,
+      lineHeight: '18px',
+      letterSpacing: '0.018px',
+    }),
+    label: css({
+      color: 'theme.colors.text.primary',
+      fontWeight: 400,
+      letterSpacing: '0.018px',
+    }),
+    horizontalLine: css({
+      width: '200px',
+      height: '1px',
+      background: theme.colors.border.weak,
+      marginTop: '8px',
+      marginBottom: '8px',
+    }),
   };
 }
