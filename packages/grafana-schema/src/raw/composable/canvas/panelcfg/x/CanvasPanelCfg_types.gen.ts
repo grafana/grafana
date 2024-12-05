51d977a3891386c6b247e4b67c355b9f59451b15
Jev Forsberg <46619047+baldm0mma@users.noreply.github.com>
2024-12-05 13:01:59 -0700

    Chore: Update dep version (#97507)
    * update dep versions
    
    * run gen-cue

--- packages/grafana-schema/src/raw/composable/canvas/panelcfg/x/CanvasPanelCfg_types.gen.ts
+++ packages/grafana-schema/src/raw/composable/canvas/panelcfg/x/CanvasPanelCfg_types.gen.ts
@@ -10,7 +10,7 @@
 
 import * as ui from '@grafana/schema';
 
-export const pluginVersion = "11.4.0-pre";
+export const pluginVersion = "11.5.0-pre";
 
 export enum HorizontalConstraint {
   Center = 'center',
