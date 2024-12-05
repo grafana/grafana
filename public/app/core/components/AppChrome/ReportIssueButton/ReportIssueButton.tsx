984e798d47e23f6905e22a423aa86b1b3cc8b68b
Sarah Zinger <sarah.zinger@grafana.com>
2024-12-05 03:38:06 -0500

    Hackathon: some frontend fixes (#97449)
    * Use actual message
    
    * Fix top bar styling
    
    * Put screenshot function behind a button

--- public/app/core/components/AppChrome/ReportIssueButton/ReportIssueButton.tsx
+++ public/app/core/components/AppChrome/ReportIssueButton/ReportIssueButton.tsx
@@ -1,59 +1,91 @@
 import html2canvas from 'html2canvas';
-import { useState } from 'react';
+import { ChangeEvent, MouseEvent, useState } from 'react';
 
-import { Menu, Dropdown, ToolbarButton, Button, Stack } from '@grafana/ui';
+import { Dropdown, ToolbarButton, Button, Stack, Menu } from '@grafana/ui';
 
 import { Spec } from '../../../../../../apps/feedback/plugin/src/feedback/v0alpha1/types.spec.gen';
 import { getFeedbackAPI } from '../../../../features/feedback/api';
 import { canvasToBase64String, extractImageTypeAndData } from '../../../../features/feedback/screenshot-encode';
 
-export interface Props {}
+export interface Props { }
 
-export const ReportIssueButton = ({}: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+const MenuActions = () => {
+  const [formData, setFormData] = useState({ message: '', screenshot: '', imageType: '' });
 
-  const MenuActions = () => {
-    const onClick = async (e: { preventDefault: () => void }) => {
-      e.preventDefault();
-      console.log('hi', e);
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setFormData({ ...formData, message: e.target.value });
+  };
 
-      let screenshot = null;
+  const onTakeScreenshot = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-      const element = document.body; // TODO: choose a different selector?
-      if (element) {
-        const canvas = await html2canvas(element, { backgroundColor: null });
+    const element = document.body; // TODO: choose a different selector?
+    if (element) {
+      const canvas = await html2canvas(element, { backgroundColor: null });
 
-        const encoded = await canvasToBase64String(canvas);
-        if (encoded && typeof encoded === 'string') {
-          screenshot = extractImageTypeAndData(encoded);
+      const encoded = await canvasToBase64String(canvas);
+      if (encoded && typeof encoded === 'string') {
+        const screenshot = extractImageTypeAndData(encoded);
+        if (screenshot) {
+          setFormData({ ...formData, screenshot: screenshot.data, imageType: screenshot.type });
         }
       }
+    }
+  };
 
-      const feedback: Spec = {
-        message: 'test sarah test',
-        ...(screenshot && { screenshot: screenshot.data, imageType: screenshot.type }),
-      };
+  const onSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
 
-      const feedbackApi = getFeedbackAPI();
-      await feedbackApi.createFeedback(feedback);
+    const feedback: Spec = {
+      message: formData.message,
+      screenshot: formData.screenshot,
+      imageType: formData.imageType,
     };
 
-    return (
-      <Menu>
-          <b>Send feedback to Grafana</b>
-          <Stack gap={2} direction={'column'}>
-            <input placeholder="so what happened?"></input>
-            <Button type="submit" onClick={onClick}>Submit feedback</Button>
-          </Stack>
-      </Menu>
-    );
+    const feedbackApi = getFeedbackAPI();
+    await feedbackApi.createFeedback(feedback);
   };
+
+  const stopAutoClose = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  return (
+    <Menu>
+      <div onClick={stopAutoClose}>
+        <b>Send feedback to Grafana</b>
+
+        <Stack gap={2} direction={'column'}>
+          <input onChange={onInputChange} placeholder="so what happened?"></input>
+          <Button onClick={onTakeScreenshot}>Take Screenshot</Button>
+          <Button type="submit" onClick={onSubmit}>
+            Submit feedback
+          </Button>
+        </Stack>
+      </div>
+    </Menu>
+
+  );
+};
+
+export const ReportIssueButton = ({ }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={setIsOpen}>
+      <Dropdown overlay={<MenuActions />} placement="bottom-end" onVisibleChange={setIsOpen}>
         <ToolbarButton iconOnly icon={'bug'} isOpen={isOpen} aria-label="New" />
       </Dropdown>
     </>
   );
 };
+
+/* 
+  TODO:
+  - see if we can get annotations to work
+  - make it stylistically pretty
+*/
