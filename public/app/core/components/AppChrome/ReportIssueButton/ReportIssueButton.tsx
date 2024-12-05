2770e1697762b268e97e1e654e9bff2233ff1804
Sarah Zinger <sarah.zinger@grafana.com>
2024-12-05 13:07:00 -0500

    Very hacky hiding implemented

--- public/app/core/components/AppChrome/ReportIssueButton/ReportIssueButton.tsx
+++ public/app/core/components/AppChrome/ReportIssueButton/ReportIssueButton.tsx
@@ -25,6 +25,12 @@ const ScreenShotEditModal = ({
   feedbackPlus: any;
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasContainerRef = useRef<HTMLDivElement>(null);
+  const hideToolRef = useRef<HTMLDivElement>(null);
+  const [isInEditMode, setIsInEditMode] = useState(false);
+  const [isInTheMiddleOfHiding, setIsInTheMiddleOfHiding] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const drawImage = (
@@ -46,26 +52,73 @@ const ScreenShotEditModal = ({
     }
   };
 
-  // TODO: edit is creates another modal underneath and below this one
-  // this feels maybe like it's just a real css nightmare but maybe one that can be solved
-  // I think there are html ids that we can reference: https://github.com/puffinsoft/feedbackplus/blob/master/docs/demos/script.js
-  const edit = () => {
-    feedbackPlus.showEditDialog(
-      bitmap,
-      function (canvas: any) {
-        // user completed edit
-        FeedbackPlus.canvasToBitmap(canvas).then(
-          (result: { bitmap: HTMLImageElement; width: number; height: number }) => {
-            drawImage(result, canvasRef);
-            feedbackPlus.closeEditDialog();
-          }
-        );
-      },
-      function () {
-        // user cancelled edit
-        feedbackPlus.closeEditDialog();
+  // ideally we could just use "showEditDialog from feedbackPlus, but it doesn't seem to work"
+  // implementing a similar thing just for hide
+  const hide = () => {
+    setIsInEditMode(true);
+    // add mouseDown, mousemove,  listener to canvas
+    // on done, edit the clone and return that
+    // then remove old and draw new image?
+  };
+
+  // when mousedown create a new element to hide things and set starting top and left and append it to the canvas.
+  const onMouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
+    if (isInEditMode && e && canvasContainerRef.current && hideToolRef.current) {
+      setIsInTheMiddleOfHiding(true);
+      const canvasContainer = canvasContainerRef.current;
+      const isHighlight = false; // at somepoint could explore highlight
+      const highlightElem = hideToolRef.current;
+      const x = e.pageX;
+      const y = e.pageY;
+      const offset = canvasContainerRef.current.getBoundingClientRect();
+      const [realX, realY] = [
+        x - offset.left + canvasContainer.scrollLeft - (isHighlight ? 5 : 0),
+        y - offset.top + canvasContainer.scrollTop - (isHighlight ? 5 : 0),
+      ];
+      setStartX(realX);
+      setStartY(realY);
+      highlightElem.style.top = realY + 'px';
+      highlightElem.style.left = realX + 'px';
+      canvasContainer.appendChild(highlightElem);
+    }
+  };
+  // when mouse move, set width and height (based on boundingClientRect) of the element to hide things
+  const onMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
+    if (isInEditMode && isInTheMiddleOfHiding) {
+      const highlightElem = hideToolRef.current;
+      const canvasContainer = canvasContainerRef.current;
+      const isHighlight = false; // at somepoint could explore highlight
+
+      if (highlightElem && canvasContainer) {
+        e.preventDefault();
+
+        const offset = canvasContainer.getBoundingClientRect();
+        const x = e.pageX - offset.left + canvasContainer.scrollLeft - (isHighlight ? 5 : 0);
+        const y = e.pageY - offset.top + canvasContainer.scrollTop - (isHighlight ? 5 : 0);
+        const Xdiff = x - startX;
+        const Ydiff = y - startY;
+        if (Xdiff < 0) {
+          highlightElem.style.left = startX + Xdiff + 'px';
+          highlightElem.style.width = startX - x + 'px';
+        } else {
+          highlightElem.style.left = startX + 'px';
+          highlightElem.style.width = Xdiff + 'px';
+        }
+        if (Ydiff < 0) {
+          highlightElem.style.top = startY + Ydiff + 'px';
+          highlightElem.style.height = startY - y + 'px';
+        } else {
+          highlightElem.style.top = startY + 'px';
+          highlightElem.style.height = Ydiff + 'px';
+        }
       }
-    );
+    }
+  };
+
+  const onMouseUp = (e: MouseEvent<HTMLCanvasElement>) => {
+    setIsInTheMiddleOfHiding(false);
+    setStartX(0);
+    setStartY(0);
   };
 
   useEffect(() => {
@@ -74,9 +127,19 @@ const ScreenShotEditModal = ({
 
   return (
     <Modal title="title" isOpen={isOpen}>
-      <canvas ref={canvasRef} />
+      <div ref={canvasContainerRef}>
+        <canvas ref={canvasRef} onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp} />
+      </div>
+      <div
+        ref={hideToolRef}
+        style={{
+          position: 'absolute',
+          backgroundColor: 'red',
+        }}
+      ></div>
+
       <Modal.ButtonRow>
-        <Button onClick={edit}>Edit</Button>
+        <Button onClick={hide}>Hide</Button>
         <Button>Save</Button>
       </Modal.ButtonRow>
     </Modal>
@@ -201,5 +264,11 @@ function isCanvas(obj: HTMLCanvasElement | HTMLElement): obj is HTMLCanvasElemen
 /* 
   TODO:
   - see if we can get annotations to work
-  - make it stylistically pretty
+    - save the edited screenshot
+    - fix the offset issue when hiding
+    - add a done button for hiding
+    - fix the scroll issue inside the widget
+    - add a title to the modal
+    - close drodown on screenshot, and reopen after done
+  - make dropdown cooler looking
 */
