08495b590a780c6f456bcdbd5b754b99b59000eb
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-12-06 11:21:08 -0500

    lint

--- public/app/features/alerting/unified/components/receivers/grafanaAppReceivers/onCall/useOnCallIntegration.ts
+++ public/app/features/alerting/unified/components/receivers/grafanaAppReceivers/onCall/useOnCallIntegration.ts
@@ -359,7 +359,7 @@ export function useOnCallEscalationChain() {
 
       return notifier;
     },
-    [grafanaOnCallEscalationChains]
+    [grafanaOnCallEscalationChains, slackChannels, teams]
   );
 
   return {
