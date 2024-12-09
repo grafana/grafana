17f05caff2a416a11b6af316af8476f67f56e325
Haris Rozajac <58232930+harisrozajac@users.noreply.github.com>
2024-12-09 08:23:23 -0700

    Dashboard Schema v2: Panel links (#97441)
    * panel links
    
    * Update transformer scene -> v2; update snapshot
    
    * clean up
    
    * data links test
    
    * Remove type assertion

--- public/app/features/dashboard-scene/serialization/transformSceneToSaveModelSchemaV2.test.ts
+++ public/app/features/dashboard-scene/serialization/transformSceneToSaveModelSchemaV2.test.ts
@@ -28,6 +28,7 @@ import { DashboardAnnotationsDataLayer } from '../scene/DashboardAnnotationsData
 import { DashboardControls } from '../scene/DashboardControls';
 import { DashboardDataLayerSet } from '../scene/DashboardDataLayerSet';
 import { DashboardScene, DashboardSceneState } from '../scene/DashboardScene';
+import { VizPanelLinks, VizPanelLinksMenu } from '../scene/PanelLinks';
 import { DashboardGridItem } from '../scene/layout-default/DashboardGridItem';
 import { DefaultGridLayoutManager } from '../scene/layout-default/DefaultGridLayoutManager';
 
@@ -142,6 +143,15 @@ describe('transformSceneToSaveModelSchemaV2', () => {
                 key: 'test-panel-uid',
                 pluginId: 'timeseries',
                 title: 'Test Panel',
+                titleItems: [
+                  new VizPanelLinks({
+                    rawLinks: [
+                      { title: 'Test Link 1', url: 'http://test1.com', targetBlank: true },
+                      { title: 'Test Link 2', url: 'http://test2.com' },
+                    ],
+                    menu: new VizPanelLinksMenu({}),
+                  }),
+                ],
                 description: 'Test Description',
                 hoverHeader: true,
                 hoverHeaderOffset: 10,
