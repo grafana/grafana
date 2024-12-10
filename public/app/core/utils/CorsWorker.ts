c325471a57fe67a4de4eea0e8657dae8c58ec128
Jack Westbrook <jack.westbrook@gmail.com>
2024-12-10 10:57:55 +0100

    chore(frontend): add comment related to using workers

--- public/app/core/utils/CorsWorker.ts
+++ public/app/core/utils/CorsWorker.ts
@@ -1,4 +1,10 @@
 // This function is used to create a worker that can load across domains.
+// To use it, you need to import the workerUrl and pass it as the first argument.
+//
+// import clientWorkerUrl from './client.worker?worker&url';
+//
+// corsWorker(clientWorkerUrl, { name: 'myWorker' });
+//
 export function corsWorker(workerUrl: string, options: WorkerOptions) {
   const js = `import ${JSON.stringify(new URL(workerUrl, import.meta.url))}`;
   const blob = new Blob([js], { type: 'application/javascript' });
