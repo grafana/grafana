4f7ffafe9898ce0ae553b1c6be35d41b70d6f773
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2024-12-10 17:42:33 +0000

    Alerting: Consistently order member imports in alerting code (#97688)
    * Lint import member orders within alerting
    
    * Consistently order member imports in alerting code

--- public/app/features/alerting/unified/mocks/server/handlers/datasources.ts
+++ public/app/features/alerting/unified/mocks/server/handlers/datasources.ts
@@ -1,4 +1,4 @@
-import { http, HttpResponse } from 'msw';
+import { HttpResponse, http } from 'msw';
 
 import { buildInfoResponse } from 'app/features/alerting/unified/testSetup/featureDiscovery';
 
