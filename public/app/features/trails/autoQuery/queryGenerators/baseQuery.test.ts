2b7786bf54e1ec240c8e4d7659560debb5f3b3a3
ismail simsek <ismailsimsek09@gmail.com>
2024-12-11 20:23:08 +0100

    summary tests

--- public/app/features/trails/autoQuery/queryGenerators/baseQuery.test.ts
+++ public/app/features/trails/autoQuery/queryGenerators/baseQuery.test.ts
@@ -23,21 +23,21 @@ describe('generateBaseQuery', () => {
 
   it('should return UTF-8 base query without rate and groupings', () => {
     const result = generateBaseQuery({ isUtf8Metric: true });
-    expect(result).toBe('{${metric}, ${filters}} ${otel_join_query}');
+    expect(result).toBe('{"${metric}", ${filters}} ${otel_join_query}');
   });
 
   it('should return UTF-8 rate base query without groupings', () => {
     const result = generateBaseQuery({ isRateQuery: true, isUtf8Metric: true });
-    expect(result).toBe('rate({${metric}, ${filters}}[$__rate_interval]) ${otel_join_query}');
+    expect(result).toBe('rate({"${metric}", ${filters}}[$__rate_interval]) ${otel_join_query}');
   });
 
   it('should return UTF-8 base query with groupings', () => {
     const result = generateBaseQuery({ isUtf8Metric: true, groupings: ['job', 'instance'] });
-    expect(result).toBe('sum by(job, instance) ({${metric}, ${filters}} ${otel_join_query})');
+    expect(result).toBe('sum by(job, instance) ({"${metric}", ${filters}} ${otel_join_query})');
   });
 
   it('should return UTF-8 rate base query with groupings', () => {
     const result = generateBaseQuery({ isRateQuery: true, isUtf8Metric: true, groupings: ['job', 'instance'] });
-    expect(result).toBe('sum by(job, instance) (rate({${metric}, ${filters}}[$__rate_interval]) ${otel_join_query})');
+    expect(result).toBe('sum by(job, instance) (rate({"${metric}", ${filters}}[$__rate_interval]) ${otel_join_query})');
   });
 });
