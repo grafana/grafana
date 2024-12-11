5b0eebb60a336e1b0d9998a34f5c38c21994cc6c
Laura Fernández <laura.fernandez@grafana.com>
2024-12-11 15:45:20 +0100

    MetricCombobox: Fix overflowing parents (#97049)

--- packages/grafana-ui/src/components/Forms/InlineFieldRow.tsx
+++ packages/grafana-ui/src/components/Forms/InlineFieldRow.tsx
@@ -27,6 +27,7 @@ const getStyles = (theme: GrafanaTheme2) => {
       flexWrap: 'wrap',
       alignContent: 'flex-start',
       rowGap: theme.spacing(0.5),
+      maxWidth: '100%',
     }),
   };
 };
