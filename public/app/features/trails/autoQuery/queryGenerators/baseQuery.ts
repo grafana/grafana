29964c026d1a5634e8b5fa84f9ddf696555b662d
ismail simsek <ismailsimsek09@gmail.com>
2024-12-11 16:14:37 +0100

    add tests for baseQuery

--- public/app/features/trails/autoQuery/queryGenerators/baseQuery.ts
+++ public/app/features/trails/autoQuery/queryGenerators/baseQuery.ts
@@ -1,7 +1,7 @@
 import { VAR_METRIC_EXPR, VAR_FILTERS_WITH_CURLY_EXPR, VAR_OTEL_JOIN_QUERY_EXPR } from '../../shared';
 
-const GENERAL_BASE_QUERY = `${VAR_METRIC_EXPR}${VAR_FILTERS_WITH_CURLY_EXPR}`;
-const GENERAL_RATE_BASE_QUERY = `rate(${GENERAL_BASE_QUERY}[$__rate_interval])`;
+export const GENERAL_BASE_QUERY = `${VAR_METRIC_EXPR}${VAR_FILTERS_WITH_CURLY_EXPR}`;
+export const GENERAL_RATE_BASE_QUERY = `rate(${GENERAL_BASE_QUERY}[$__rate_interval])`;
 
 export function getGeneralBaseQuery(rate: boolean) {
   return rate
