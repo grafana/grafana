3345f860dc001eda96f51c9d70c61e6050bc0401
ismail simsek <ismailsimsek09@gmail.com>
2024-12-12 18:40:11 +0100

    Explore Metrics: Refactor auto query generators (#97815)
    * move graph builders in one file
    
    * rename the folder as auto-query
    
    * have a components folder
    
    * more refactoring
    
    * rename
    
    * remove getQueryGeneratorFor
    
    * why not renaming again
    
    * add tests for baseQuery
    
    * remove the need of default query generator
    
    * refactor getAutoQueriesForMetric
    
    * split the logic
    
    * better structure
    
    * better structure 2
    
    * clean up
    
    * summary tests
    
    * unite tests for default.ts
    
    * update unit tests
    
    * update unit tests
    
    * cleaning
    
    * one variable for filters

--- public/app/features/trails/shared.ts
+++ public/app/features/trails/shared.ts
@@ -15,7 +15,7 @@ export const TRAILS_ROUTE = '/explore/metrics/trail';
 export const HOME_ROUTE = '/explore/metrics';
 
 export const VAR_FILTERS = 'filters';
-export const VAR_FILTERS_EXPR = '{${filters}}';
+export const VAR_FILTERS_EXPR = '${filters}';
 export const VAR_METRIC = 'metric';
 export const VAR_METRIC_EXPR = '${metric}';
 export const VAR_GROUP_BY = 'groupby';
