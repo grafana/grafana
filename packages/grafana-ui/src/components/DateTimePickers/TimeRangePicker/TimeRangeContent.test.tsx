4ab86569ad0e96b5c31b2fcf1cc47d8488e150c8
Torkel Ödegaard <torkel@grafana.com>
2024-12-13 09:58:00 -0800

    Fix lint

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimeRangeContent.test.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/TimeRangeContent.test.tsx
@@ -1,6 +1,5 @@
 import { fireEvent, render, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { before } from 'lodash';
 
 import { dateTimeParse, systemDateFormats, TimeRange } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
