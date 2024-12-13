1c26fe46c0d8c7298b4f8e4d53c597c147b686f4
Ryan McKinley <ryantxu@gmail.com>
2024-12-13 16:17:25 +0300

    parents

--- pkg/registry/apis/folders/sub_parents.go
+++ pkg/registry/apis/folders/sub_parents.go
@@ -2,18 +2,20 @@ package folders
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/apiserver/pkg/registry/rest"
 
+	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	"github.com/grafana/grafana/pkg/apis/folder/v0alpha1"
-	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
-	"github.com/grafana/grafana/pkg/services/folder"
 )
 
 type subParentsREST struct {
-	service folder.Service
+	getter rest.Getter
 }
 
 var _ = rest.Connecter(&subParentsREST{})
@@ -43,11 +45,51 @@ func (r *subParentsREST) NewConnectOptions() (runtime.Object, bool, string) {
 }
 
 func (r *subParentsREST) Connect(ctx context.Context, name string, opts runtime.Object, responder rest.Responder) (http.Handler, error) {
+	ns, ok := request.NamespaceFrom(ctx)
+	if !ok {
+		return nil, fmt.Errorf("expecting namespace")
+	}
+
+	// the current folder
+	obj, err := r.getter.Get(ctx, name, &v1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	folder, ok := obj.(*v0alpha1.Folder)
+	if !ok {
+		return nil, fmt.Errorf("expecting folder, found: %T", folder)
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		ns, err := request.NamespaceInfoFrom(ctx, true)
-		if err != nil {
-			responder.Error(err)
-			return
+		info := &v0alpha1.FolderInfoList{
+			Items: []v0alpha1.FolderInfo{},
+		}
+
+		// Walk up the tree
+		for folder != nil {
+			parent := ""
+			meta, _ := utils.MetaAccessor(folder)
+			folder = nil
+			if meta != nil {
+				parent = meta.GetFolder()
+			}
+			info.Items = append(info.Items, v0alpha1.FolderInfo{
+				Name:        folder.Name,
+				Title:       folder.Spec.Title,
+				Description: folder.Spec.Description,
+				Parent:      parent,
+			})
+			folder = nil
+			if parent != "" {
+				obj, err = r.getter.Get(ctx, parent, &v1.GetOptions{})
+				if err != nil {
+
+				}
+				folder, ok := obj.(*v0alpha1.Folder)
+				if !ok {
+					return nil, fmt.Errorf("expecting folder, found: %T", folder)
+				}
+			}
 		}
 
 		parents, err := r.service.GetParents(ctx, folder.GetParentsQuery{
@@ -59,9 +101,6 @@ func (r *subParentsREST) Connect(ctx context.Context, name string, opts runtime.
 			return
 		}
 
-		info := &v0alpha1.FolderInfoList{
-			Items: make([]v0alpha1.FolderInfo, 0),
-		}
 		for _, parent := range parents {
 			info.Items = append(info.Items, v0alpha1.FolderInfo{
 				UID:    parent.UID,
