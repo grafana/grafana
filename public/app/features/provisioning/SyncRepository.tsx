9e7c7cd87f27aa22cb1c0327a2632073deade625
Ryan McKinley <ryantxu@gmail.com>
2024-12-13 15:31:05 +0300

    update UI

---
+++ public/app/features/provisioning/SyncRepository.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom-v5-compat';
+import { useAsync } from 'react-use';
+
+import { AppEvents } from '@grafana/data';
+import { config, getAppEvents, getBackendSrv } from '@grafana/runtime';
+import { Button, ConfirmModal } from '@grafana/ui';
+
+import { Resource } from '../apiserver/types';
+import { Loader } from '../plugins/admin/components/Loader';
+
+import { useCreateRepositorySyncMutation, useListRepositoryQuery } from './api';
+import { RepositorySpec } from './api/types';
+
+interface Props {
+  repository: Resource<RepositorySpec>;
+}
+
+export function SyncRepository({ repository }: Props) {
+  const query = useListRepositoryQuery();
+  const [syncResource, syncQuery] = useCreateRepositorySyncMutation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+  const name = repository.metadata?.name;
+  const folder = repository.spec?.folder;
+
+  // TODO generate endpoints for this
+  const { value } = useAsync(async () => {
+    return getBackendSrv().get(`/apis/folder.grafana.app/v0alpha1/namespaces/${config.namespace}/folders/${folder}`);
+  });
+
+  useEffect(() => {
+    // TODO... not true anymore --
+    // clicking sync just means we queued the job; can watch it until finished
+    const appEvents = getAppEvents();
+    if (syncQuery.isSuccess) {
+      appEvents.publish({
+        type: AppEvents.alertSuccess.name,
+        payload: ['Resources imported'],
+      });
+      // TODO the URL will be different based on the type of resource imported
+      navigate(`/dashboards${folder ? `/f/${folder}` : ''}`);
+    } else if (syncQuery.isError) {
+      appEvents.publish({
+        type: AppEvents.alertError.name,
+        payload: ['Error importing resources', syncQuery.error],
+      });
+    }
+  }, [folder, syncQuery.error, syncQuery.isError, syncQuery.isSuccess, navigate]);
+
+  const onClick = () => {
+    if (!name) {
+      return;
+    }
+
+    syncResource({ name });
+  };
+
+  if (query.isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <Button variant={'secondary'} onClick={() => setIsModalOpen(true)} disabled={syncQuery.isLoading || !name}>
+        Sync
+      </Button>
+      <ConfirmModal
+        isOpen={isModalOpen}
+        title={'Synchronize resources from repository'}
+        body={`This will pull all resources from the repository into your instance into the "${value?.spec?.title}" folder. Existing dashboards with the same UID will be overwritten. Proceed?`}
+        confirmText={syncQuery.isLoading ? 'Importing...' : 'Import'}
+        onConfirm={onClick}
+        onDismiss={() => setIsModalOpen(false)}
+      />
+    </>
+  );
+}
