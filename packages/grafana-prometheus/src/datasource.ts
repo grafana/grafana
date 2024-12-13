132ad455ac8701d60e28ad787321772a8b3c0725
Konstantin Kornienko <50887992+konstantin-kornienko@users.noreply.github.com>
2024-12-13 15:07:51 +0300

    Update packages/grafana-prometheus/src/datasource.ts
    Co-authored-by: Brendan O'Handley <brendan.ohandley@grafana.com>

--- packages/grafana-prometheus/src/datasource.ts
+++ packages/grafana-prometheus/src/datasource.ts
@@ -537,15 +537,11 @@ export class PrometheusDatasource
     const annotation = options.annotation;
     const { tagKeys = '', titleFormat = '', textFormat = '' } = annotation;
 
-    const scopedVars = {
-      ...this.getIntervalVars(),
-      ...this.getRangeScopedVars(options?.range ?? getDefaultTimeRange()),
-    };
-
-    const step = rangeUtil.intervalToSeconds(
-      this.interpolateString(annotation.step || ANNOTATION_QUERY_STEP_DEFAULT, scopedVars)
-    ) * 1000;
-
+    const input = frames[0].meta?.executedQueryString || '';
+    const regex = /Step:\s*([\d\w]+)/;
+    const match = input.match(regex);
+    const stepValue = match ? match[1] : null;
+    const step = rangeUtil.intervalToSeconds(stepValue || ANNOTATION_QUERY_STEP_DEFAULT) * 1000;
     const tagKeysArray = tagKeys.split(',');
 
     const eventList: AnnotationEvent[] = [];
