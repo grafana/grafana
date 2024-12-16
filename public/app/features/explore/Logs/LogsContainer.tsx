9a69ba19cac74370505306094af446e6019265e7
Matias Chomicki <matyax@gmail.com>
2024-12-16 13:13:25 +0100

    Revert change

--- public/app/features/explore/Logs/LogsContainer.tsx
+++ public/app/features/explore/Logs/LogsContainer.tsx
@@ -293,7 +293,7 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
       onPinLineCallback,
     } = this.props;
 
-    if (!logRows || Object.keys(this.state.dsInstances).length === 0) {
+    if (!logRows) {
       return null;
     }
 
