eb45cf7df2842864dd3e673a44dc4fdbe4fdef53
Pepe Cano <825430+ppcano@users.noreply.github.com>
2024-12-16 13:47:36 +0100

    Alerting UI: add tooltip for the Preview silences UI

--- public/app/features/alerting/unified/components/silences/SilencedInstancesPreview.tsx
+++ public/app/features/alerting/unified/components/silences/SilencedInstancesPreview.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useDebounce, useDeepCompareEffect } from 'react-use';
 
 import { GrafanaTheme2, dateTime } from '@grafana/data';
-import { Alert, Badge, LoadingPlaceholder, useStyles2 } from '@grafana/ui';
+import { Alert, Badge, Icon, LoadingPlaceholder, Tooltip, useStyles2 } from '@grafana/ui';
 import { MatcherFieldValue } from 'app/features/alerting/unified/types/silence-form';
 import { matcherFieldToMatcher } from 'app/features/alerting/unified/utils/alertmanager';
 import { MATCHER_ALERT_RULE_UID } from 'app/features/alerting/unified/utils/constants';
@@ -79,7 +79,18 @@ export const SilencedInstancesPreview = ({ amSourceName, matchers: inputMatchers
   return (
     <div>
       <h4 className={styles.title}>
-        Affected alert rule instances
+        Affected alert rule instances&nbsp;
+        <Tooltip
+          content={
+            <div>
+              Preview the alert instances affected by this silence.
+              <br />
+              Only alert instances in the firing state are displayed.
+            </div>
+          }
+        >
+          <Icon name="info-circle" size="sm" />
+        </Tooltip>
         {tableItemAlerts.length > 0 ? (
           <Badge className={styles.badge} color="blue" text={tableItemAlerts.length} />
         ) : null}
@@ -97,7 +108,7 @@ export const SilencedInstancesPreview = ({ amSourceName, matchers: inputMatchers
               pagination={{ itemsPerPage: 10 }}
             />
           ) : (
-            <span>No firing alert instances found</span>
+            <span>No firing alert instances Afound</span>
           )}
         </div>
       )}
