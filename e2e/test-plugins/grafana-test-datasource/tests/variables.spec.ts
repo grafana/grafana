93e6c989b95063a09659494f87a67ef4b2de87b4
Erik Sundell <erik.sundell87@gmail.com>
2024-12-17 15:29:10 +0100

    remove unused fixture

--- e2e/test-plugins/grafana-test-datasource/tests/variables.spec.ts
+++ e2e/test-plugins/grafana-test-datasource/tests/variables.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('should render variable editor', async ({ variableEditPage, page, readProvisionedDataSource }) => {
+test('should render variable editor', async ({ variableEditPage, page }) => {
   await variableEditPage.datasource.set('gdev-e2etestdatasource');
   await expect(page.getByRole('textbox', { name: 'Query Text' })).toBeVisible();
 });
 
-test('create new, successful variable query', async ({ variableEditPage, readProvisionedDataSource, page }) => {
+test('create new, successful variable query', async ({ variableEditPage, page }) => {
   await variableEditPage.datasource.set('gdev-e2etestdatasource');
   await page.getByRole('textbox', { name: 'Query Text' }).fill('variableQuery');
   await variableEditPage.runQuery();
