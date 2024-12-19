1a54d78bbecf94a8479d632de678ee1f17bb01cb
Joao Silva <100691367+JoaoSilvaGrafana@users.noreply.github.com>
2024-12-19 11:25:44 +0000

    MultiCombobox: Add truncation behavior (#97691)

--- packages/grafana-ui/src/components/Combobox/getMultiComboboxStyles.ts
+++ packages/grafana-ui/src/components/Combobox/getMultiComboboxStyles.ts
@@ -5,7 +5,7 @@ import { GrafanaTheme2 } from '@grafana/data';
 import { getFocusStyles } from '../../themes/mixins';
 import { getInputStyles } from '../Input/Input';
 
-export const getMultiComboboxStyles = (theme: GrafanaTheme2) => {
+export const getMultiComboboxStyles = (theme: GrafanaTheme2, isOpen: boolean) => {
   const inputStyles = getInputStyles({ theme });
   const focusStyles = getFocusStyles(theme);
 
@@ -34,10 +34,30 @@ export const getMultiComboboxStyles = (theme: GrafanaTheme2) => {
         outline: 'none',
       },
     }),
+    inputClosed: css({
+      width: 0,
+      flexGrow: 0,
+      paddingLeft: 0,
+      paddingRight: 0,
+    }),
     pillWrapper: css({
       display: 'inline-flex',
-      flexWrap: 'wrap',
+      flexWrap: isOpen ? 'wrap' : 'nowrap',
+      flexGrow: 1,
+      minWidth: '50px',
       gap: theme.spacing(0.5),
+    }),
+    restNumber: css({
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: theme.spacing(0, 1),
+      borderRadius: theme.shape.radius.default,
+      backgroundColor: theme.colors.background.secondary,
+      cursor: 'pointer',
+      '&:hover': {
+        backgroundColor: theme.colors.action.hover,
+      },
     }),
   };
 };
