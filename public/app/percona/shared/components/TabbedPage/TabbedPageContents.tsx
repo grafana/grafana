266da08650b1466c0770d6c191442f456ec42440
Dora <103416234+doracretu3pillar@users.noreply.github.com>
2024-12-19 14:34:25 +0200

    PMM-13451: Tabbed table content size and browser tab title (#790)
    * Fixed tabbed content size and browser tab title
    
    * Fixed title and added comment

--- public/app/percona/shared/components/TabbedPage/TabbedPageContents.tsx
+++ public/app/percona/shared/components/TabbedPage/TabbedPageContents.tsx
@@ -6,7 +6,7 @@ import { Page } from 'app/core/components/Page/Page';
 import { styles } from './TabbedPageContents.styles';
 
 export const TabbedPageContents: FC<ComponentProps<typeof Page.Contents>> = ({ children, className, ...props }) => (
-  <Page.Contents {...props} className={cx('page-container', 'page-body', styles.Contents, className)}>
+  <Page.Contents {...props} className={cx('page-body', styles.Contents, className)}>
     {children}
   </Page.Contents>
 );
