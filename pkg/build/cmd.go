d8e55ebb2522cd817071156093e8c36361cb3d67
yincong <yincong@baidu.com>
2024-12-26 10:44:34 +0800

    optimize

--- pkg/build/cmd.go
+++ pkg/build/cmd.go
@@ -243,6 +243,7 @@ func ldflags(opts BuildOpts) (string, error) {
 		buildBranch = v
 	}
 	var b bytes.Buffer
+	b.WriteString("-w")
 	b.WriteString(fmt.Sprintf(" -X main.version=%s", opts.version))
 	b.WriteString(fmt.Sprintf(" -X main.commit=%s", commitSha))
 	if enterpriseCommitSha != "" {
