1908149d6bd03ec102a8985d07b7bc351cdaeadd
ismail simsek <ismailsimsek09@gmail.com>
2025-01-02 21:45:18 +0100

    remove toggle

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -88,7 +88,6 @@ export interface FeatureToggles {
   featureToggleAdminPage?: boolean;
   awsAsyncQueryCaching?: boolean;
   permissionsFilterRemoveSubquery?: boolean;
-  prometheusConfigOverhaulAuth?: boolean;
   configurableSchedulerTick?: boolean;
   alertingNoDataErrorExecution?: boolean;
   angularDeprecationUI?: boolean;
