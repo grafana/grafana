2e5eaef9cfeff24139b17ff9d01e5d631035cb7c
Joao Calisto <joao.santana.calisto@gmail.com>
2025-01-03 15:09:18 +0000

    test lint

--- eslint.config.js
+++ eslint.config.js
@@ -45,7 +45,7 @@ module.exports = [
       'public/lib/monaco/', // this path is no longer required but local dev environments may still have it
       'public/locales/_build',
       'public/locales/**/*.js',
-      'vendor',
+      '/public/vendor',
       'scripts/grafana-server/tmp',
     ],
   },
