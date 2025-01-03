b4ab55ae38ca8c66269e3d77ee68bbe1f8e76023
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-01-03 08:28:45 -0700

    K8s: Dashboard spec should not have id (#98336)

--- pkg/registry/apis/dashboard/v1alpha1/register.go
+++ pkg/registry/apis/dashboard/v1alpha1/register.go
@@ -1,15 +1,20 @@
 package v1alpha1
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	"k8s.io/kube-openapi/pkg/common"
 	"k8s.io/kube-openapi/pkg/spec3"
 
+	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	dashboardinternal "github.com/grafana/grafana/pkg/apis/dashboard"
 	dashboardv1alpha1 "github.com/grafana/grafana/pkg/apis/dashboard/v1alpha1"
 	grafanaregistry "github.com/grafana/grafana/pkg/apiserver/registry/generic"
@@ -210,4 +215,28 @@ func (b *DashboardsAPIBuilder) PostProcessOpenAPI(oas *spec3.OpenAPI) (*spec3.Op
 		sub.Get.Tags = []string{"API Discovery"} // sorts first in the list
 	}
 	return oas, nil
+}
+
+// Mutate removes any internal ID set in the spec & adds it as a label
+func (b *DashboardsAPIBuilder) Mutate(ctx context.Context, a admission.Attributes, o admission.ObjectInterfaces) (err error) {
+	op := a.GetOperation()
+	if op == admission.Create || op == admission.Update {
+		obj := a.GetObject()
+		dash, ok := obj.(*dashboardv1alpha1.Dashboard)
+		if !ok {
+			return fmt.Errorf("expected v1alpha1 dashboard")
+		}
+
+		if id, ok := dash.Spec.Object["id"].(float64); ok {
+			delete(dash.Spec.Object, "id")
+			if id != 0 {
+				meta, err := utils.MetaAccessor(obj)
+				if err != nil {
+					return err
+				}
+				meta.SetDeprecatedInternalID(int64(id)) // nolint:staticcheck
+			}
+		}
+	}
+	return nil
 }
