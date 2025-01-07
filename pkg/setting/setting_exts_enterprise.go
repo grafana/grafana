7019de739223271e505951d6e9fd0bcc922e8a85
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-01-07 17:26:56 +0100

    Update comment

--- pkg/setting/setting_exts_enterprise.go
+++ pkg/setting/setting_exts_enterprise.go
@@ -5,7 +5,7 @@ package setting
 
 // HACK: this is a hack to keep the go modules in sync with the enterprise dependencies as it cannot be used as a module
 // itself.
-// This file is present in this package as it's where you enable / disable the enterprise features.
+// This file is present in this package as it's where you enable enterprise features.
 import (
 	_ "cloud.google.com/go/kms/apiv1"
 	_ "cloud.google.com/go/kms/apiv1/kmspb"
