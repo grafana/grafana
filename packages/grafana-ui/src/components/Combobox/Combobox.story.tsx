abc8c82bce7a0de738d5f196ce4ec12d00182cb5
Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
2025-01-07 14:59:58 +0100

    Combobox: Add description support (#98593)
    * Support multiple sizes
    
    * Add support for multi
    
    * add a longer description to test clipping
    
    ---------
    
    Co-authored-by: joshhunt <josh@trtr.co>

--- packages/grafana-ui/src/components/Combobox/Combobox.story.tsx
+++ packages/grafana-ui/src/components/Combobox/Combobox.story.tsx
@@ -30,8 +30,17 @@ const meta: Meta<PropsAndCustomArgs> = {
     isClearable: false,
     placeholder: 'Select an option...',
     options: [
-      { label: 'Apple', value: 'apple' },
-      { label: 'Banana', value: 'banana' },
+      {
+        label: 'Apple',
+        value: 'apple',
+        description: 'Apples are a great source of fiber and vitamin C.',
+      },
+      {
+        label: 'Banana',
+        value: 'banana',
+        description:
+          'Bananas are a great source of potassium, fiber, and vitamin C. They are also a great snack for on the go.',
+      },
       { label: 'Carrot', value: 'carrot' },
       // Long label to test overflow
       {
@@ -44,7 +53,12 @@ const meta: Meta<PropsAndCustomArgs> = {
       { label: 'Fennel', value: 'fennel' },
       { label: 'Grape', value: 'grape' },
       { label: 'Honeydew', value: 'honeydew' },
-      { label: 'Iceberg Lettuce', value: 'iceberg-lettuce' },
+      {
+        label: 'Iceberg Lettuce',
+        value: 'iceberg-lettuce',
+        description:
+          'this is a very long description that should be longer than the longest option label which should make it clip to only one line. It is a bit tough to estimate the width of the descriptions because the font size is smaller, but this should be enough.',
+      },
       { label: 'Jackfruit', value: 'jackfruit' },
     ],
     value: 'banana',
