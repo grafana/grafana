9c81afd546933af8fb64ecef2ba0e4430b90453e
Ryan McKinley <ryantxu@gmail.com>
2025-01-07 11:42:46 +0300

    include version in date

--- pkg/services/apiserver/builder/helper.go
+++ pkg/services/apiserver/builder/helper.go
@@ -139,16 +139,19 @@ func SetupConfig(
 	}
 
 	v := utilversion.DefaultKubeEffectiveVersion()
+	patchver := 0 // required for semver
 
 	info := v.BinaryVersion().Info()
-	info.BuildDate = "xxx"
-	info.GitVersion = "xxxx.yyyy"
-	info.GitTreeState = buildVersion
+	info.BuildDate = time.Unix(buildTimestamp, 0).UTC().Format(time.RFC3339)
+	info.GitVersion = fmt.Sprintf("%s.%s.%d+grafana-v%s", info.Major, info.Minor, patchver, buildVersion)
+	info.GitCommit = fmt.Sprintf("%s@%s", buildBranch, buildCommit)
+	info.GitTreeState = fmt.Sprintf("grafana v%s", buildVersion)
 
-	info = v.EmulationVersion().Info()
-	info.BuildDate = "xxx"
-	info.GitVersion = "xxxx.yyyy"
-	info.GitTreeState = buildVersion
+	info2 := v.EmulationVersion().Info()
+	info2.BuildDate = info.BuildDate
+	info2.GitVersion = fmt.Sprintf("%s.%s.%d+grafana-v%s", info2.Major, info2.Minor, patchver, buildVersion)
+	info2.GitCommit = info.GitCommit
+	info2.GitTreeState = info.GitTreeState
 
 	serverConfig.EffectiveVersion = v
 
