30185179a84eba0e3d09083b387709e37eaa8488
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2025-01-08 16:08:01 +0100

    Plugins: Fix Managed By Grafana version display for grafana cloud (#98577)
    * Change usePluginInfo to keep installed and latest version
    
    * fix tests, delete console.log
    
    * fix isManaged plugin which s not installed
    
    * refactor add versions into info

--- public/app/features/plugins/admin/components/PluginDetailsPanel.tsx
+++ public/app/features/plugins/admin/components/PluginDetailsPanel.tsx
@@ -2,11 +2,10 @@ import { css } from '@emotion/css';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { PageInfoItem } from '@grafana/runtime/src/components/PluginPage';
-import { Stack, Text, LinkButton, Box, TextLink, useStyles2 } from '@grafana/ui';
+import { Stack, Text, LinkButton, Box, TextLink } from '@grafana/ui';
 import { Trans } from 'app/core/internationalization';
 import { formatDate } from 'app/core/internationalization/dates';
 
-import { getLatestCompatibleVersion } from '../helpers';
 import { CatalogPlugin } from '../types';
 
 type Props = {
@@ -18,31 +17,11 @@ type Props = {
 export function PluginDetailsPanel(props: Props): React.ReactElement | null {
   const { pluginExtentionsInfo, plugin, width = '250px' } = props;
 
-  const styles = useStyles2(getStyles);
   return (
     <Stack direction="column" gap={3} shrink={0} grow={0} maxWidth={width} data-testid="plugin-details-panel">
       <Box padding={2} borderColor="medium" borderStyle="solid">
         <Stack direction="column" gap={2}>
-          {plugin.isInstalled && plugin.installedVersion && (
-            <Stack wrap direction="column" gap={0.5}>
-              <Text color="secondary">
-                <Trans i18nKey="plugins.details.labels.installedVersion">Installed version: </Trans>
-              </Text>
-              <div className={styles.pluginVersionDetails}>{plugin.installedVersion}</div>
-            </Stack>
-          )}
-          <Stack wrap direction="column" gap={0.5}>
-            <Text color="secondary">
-              <Trans i18nKey="plugins.details.labels.latestVersion">Latest version: </Trans>
-            </Text>
-            <div className={styles.pluginVersionDetails}>
-              {plugin.latestVersion || getLatestCompatibleVersion(plugin.details?.versions)?.version}
-            </div>
-          </Stack>
           {pluginExtentionsInfo.map((infoItem, index) => {
-            if (infoItem.label === 'Version') {
-              return null;
-            }
             return (
               <Stack key={index} wrap direction="column" gap={0.5}>
                 <Text color="secondary">{infoItem.label + ':'}</Text>
