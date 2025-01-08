1f8f0b3815121841a19aa0094470e47636c72032
Marcus Andersson <marcus.andersson@grafana.com>
2025-01-08 12:00:08 +0100

    fixed import order.

--- packages/grafana-ui/src/components/Portal/Portal.tsx
+++ packages/grafana-ui/src/components/Portal/Portal.tsx
@@ -4,9 +4,9 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 
 import { GrafanaTheme2 } from '@grafana/data';
+import { selectors } from '@grafana/e2e-selectors';
 
 import { useStyles2, useTheme2 } from '../../themes';
-import { selectors } from '@grafana/e2e-selectors';
 
 interface Props {
   className?: string;
