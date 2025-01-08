78a1c2bdadd644f66525debd4122791633b97872
Ryan McKinley <ryantxu@gmail.com>
2025-01-08 20:35:09 +0300

    fix test

--- public/app/features/dashboard/api/v0.test.ts
+++ public/app/features/dashboard/api/v0.test.ts
@@ -158,7 +158,7 @@ describe('v0 dashboard API', () => {
 
         expect(result.uid).toBe('adh59cn');
         expect(result.version).toBe(0);
-        expect(result.url).toBe('/d/adh59cn');
+        expect(result.url).toBe('/d/adh59cn/new-dashboard-saved');
       });
       it('should provide dashboard URL with app sub url configured', async () => {
         const api = new K8sDashboardAPI();
@@ -184,7 +184,7 @@ describe('v0 dashboard API', () => {
 
         expect(result.uid).toBe('adh59cn');
         expect(result.version).toBe(0);
-        expect(result.url).toBe('/grafana/d/adh59cn');
+        expect(result.url).toBe('/grafana/d/adh59cn/new-dashboard-saved');
       });
     });
     describe('saving a new dashboard', () => {
@@ -228,7 +228,7 @@ describe('v0 dashboard API', () => {
 
         expect(result.uid).toBe('adh59cn');
         expect(result.version).toBe(0);
-        expect(result.url).toBe('/grafana/d/adh59cn');
+        expect(result.url).toBe('/grafana/d/adh59cn/new-dashboard-saved');
       });
     });
   });
