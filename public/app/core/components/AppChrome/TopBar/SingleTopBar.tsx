a93e388a8feebc0ae27172fb092ed3ad335e5eea
eledobleefe <laura.fernandez@grafana.com>
2025-01-08 18:46:06 +0100

    Remove unnecesary styles

--- public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
+++ public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
@@ -118,17 +118,7 @@ const getStyles = (theme: GrafanaTheme2, menuDockedAndOpen: boolean) => ({
       gridTemplateColumns: '2fr minmax(550px, 1fr)',
       display: 'grid',
       justifyContent: 'flex-start',
-      ...(menuDockedAndOpen && {
-        gridTemplateColumns: '2fr minmax(55%, 1fr)',
-      }),
     },
-    [theme.breakpoints.up('xl')]: {
-      gridTemplateColumns: '2fr minmax(40%, 1fr)',
-      ...(menuDockedAndOpen && {
-        gridTemplateColumns: '2fr minmax(55%, 1fr)',
-      }),
-    },
-    [theme.breakpoints.up('xxl')]: menuDockedAndOpen && { gridTemplateColumns: '2fr minmax(40%, 1fr)' },
   }),
   breadcrumbsWrapper: css({
     display: 'flex',
