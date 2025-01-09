05bfb5a93721b117a7ff9212c47378bd3a923e3e
Mihaly Gyongyosi <mgyongyosi@users.noreply.github.com>
2025-01-09 17:38:35 +0100

    Add separate feature toggle to roll out SAML-related external session improvements

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1489,7 +1489,7 @@ var (
 		},
 		{
 			Name:        "improvedExternalSessionHandling",
-			Description: "Enable improved support for OAuth and SAML external sessions in Grafana",
+			Description: "Enables improved support for OAuth external sessions. After enabling this feature, users might need to re-authenticate themselves.",
 			Stage:       FeatureStagePublicPreview,
 			Owner:       identityAccessTeam,
 		},
@@ -1721,6 +1721,12 @@ var (
 			Stage:       FeatureStageExperimental,
 			Owner:       grafanaSearchAndStorageSquad,
 			Expression:  "false",
+		},
+		{
+			Name:        "improvedExternalSessionHandlingSAML",
+			Description: "Enables improved support for SAML external sessions. Ensure the NameID format is correctly configured in Grafana for SAML Single Logout to function properly.",
+			Stage:       FeatureStagePublicPreview,
+			Owner:       identityAccessTeam,
 		},
 	}
 )
