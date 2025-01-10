b9ee95e62427bbd615c7b8f22c5ece1139a735a7
Santiago <santiagohernandez.1997@gmail.com>
2025-01-10 15:31:03 +0100

    Alerting: Use AlertRuleKey for comparison before rule evaluation (#98808)
    (WIP) Alerting: Use AlertRuleKey for comparison before rule evaluation

--- pkg/services/ngalert/schedule/alert_rule.go
+++ pkg/services/ngalert/schedule/alert_rule.go
@@ -194,7 +194,7 @@ func (a *alertRule) Status() ngmodels.RuleStatus {
 //
 // the second element contains a dropped message that was sent by a concurrent sender.
 func (a *alertRule) Eval(eval *Evaluation) (bool, *Evaluation) {
-	if a.key != eval.rule.GetKeyWithGroup() {
+	if a.key.AlertRuleKey != eval.rule.GetKey() {
 		// Make sure that rule has the same key. This should not happen
 		a.logger.Error("Invalid rule sent for evaluating. Skipping", "ruleKeyToEvaluate", eval.rule.GetKey().String())
 		return false, eval
