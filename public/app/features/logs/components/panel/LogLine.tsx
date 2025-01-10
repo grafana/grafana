d97b1de3bb88402bc230de8ff806e5ce5ad28aa3
Matias Chomicki <matyax@gmail.com>
2025-01-10 17:22:40 +0100

    Use measurement to render virtualized log lines

--- public/app/features/logs/components/panel/LogLine.tsx
+++ public/app/features/logs/components/panel/LogLine.tsx
@@ -1,6 +1,10 @@
+import { css } from "@emotion/css";
 import { CSSProperties } from "react";
 
-import { LogRowModel } from "@grafana/data";
+import { GrafanaTheme2, LogRowModel } from "@grafana/data";
+import { useTheme2 } from "@grafana/ui";
+
+import { measureText } from "./virtualization";
 
 interface Props {
   log: LogRowModel;
@@ -8,9 +12,30 @@ interface Props {
 }
 
 export const LogLine = ({ log, style }: Props) => {
+  const theme = useTheme2();
+  const styles = getStyles(theme);
   return (
-    <div style={style}>
+    <div style={style} className={styles.logLine}>
       {log.entry}
     </div>
   )
+}
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  logLine: css({
+    fontFamily: theme.typography.fontFamilyMonospace,
+    fontSize: theme.typography.body.fontSize,
+    wordBreak: 'break-all',
+    '&:hover': {
+      opacity: 0.5,
+    }
+  }),
+});
+
+export function getLogLineSize(logs: LogRowModel[], container: HTMLDivElement | null, theme: GrafanaTheme2, index: number) {
+  if (!container) {
+    return 0;
+  }
+  const { height } = measureText(logs[index].entry, container.getBoundingClientRect().width, 22);
+  return height;
 }
--- public/app/features/logs/components/panel/LogList.tsx
+++ public/app/features/logs/components/panel/LogList.tsx
@@ -1,27 +1,47 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ListChildComponentProps, VariableSizeList } from "react-window";
 
 import { CoreApp, LogRowModel } from "@grafana/data";
+import { useTheme2 } from "@grafana/ui";
 
-import { LogLine } from "./LogLine";
+import { getLogLineSize, LogLine } from "./LogLine";
+import { init as initVirtualization } from "./virtualization";
 
 interface Props {
   app?: CoreApp;
   logs: LogRowModel[];
+  containerElement: HTMLDivElement | null;
 }
 
-export const LogList = ({ logs }: Props) => {
+export const LogList = ({ containerElement, logs }: Props) => {
+  const [listKey, setListKey] = useState(`${Math.random()}`);
+  const theme = useTheme2();
+
+  useEffect(() => {
+    initVirtualization(theme.typography.fontFamilyMonospace, theme.typography.body.fontSize);
+  }, [theme.typography.body.fontSize, theme.typography.fontFamilyMonospace]);
+
+  useEffect(() => {
+    setListKey(`${Math.random()}`);
+  }, [logs]);
+  
   const Renderer = useCallback(({ index, style }: ListChildComponentProps) => {
     return <LogLine log={logs[index]} style={style} />
   }, [logs]);
 
   const height = window.innerHeight * 0.75;
 
+  if (!containerElement) {
+    // Wait for container to be rendered
+    return null;
+  }
+
   return (
     <VariableSizeList
+      key={listKey}
       height={height}
       itemCount={logs.length}
-      itemSize={getLogLineSize}
+      itemSize={getLogLineSize.bind(null, logs, containerElement, theme)}
       itemKey={(index: number) => index}
       width={'100%'}
       layout="vertical"
@@ -29,8 +49,4 @@ export const LogList = ({ logs }: Props) => {
       {Renderer}
     </VariableSizeList>
   );
-}
-
-function getLogLineSize() {
-  return 20;
 }
