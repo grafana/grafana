edf022e95d1887a8ae88a31c3290b101d059c2de
xavi <114113189+volcanonoodle@users.noreply.github.com>
2025-01-10 17:37:37 +0100

    Auth: Fix SAML user IsExternallySynced not being set correctly (#98487)

--- pkg/login/social/social.go
+++ pkg/login/social/social.go
@@ -121,6 +121,14 @@ func (o *OAuthInfo) IsAutoLoginEnabled() bool {
 	return o.AutoLogin
 }
 
+func (o *OAuthInfo) IsSkipOrgRoleSyncEnabled() bool {
+	return o.SkipOrgRoleSync
+}
+
+func (o *OAuthInfo) IsAllowAssignGrafanaAdminEnabled() bool {
+	return o.AllowAssignGrafanaAdmin
+}
+
 type BasicUserInfo struct {
 	Id             string
 	Name           string
