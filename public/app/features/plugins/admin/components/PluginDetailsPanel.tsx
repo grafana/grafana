8b69a222cd9d36c0059ec613a797007244e04a3c
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2025-01-10 14:16:43 +0100

    change UI with a new design

--- public/app/features/plugins/admin/components/PluginDetailsPanel.tsx
+++ public/app/features/plugins/admin/components/PluginDetailsPanel.tsx
@@ -3,16 +3,11 @@ import { useEffect, useState } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { PageInfoItem } from '@grafana/runtime/src/components/PluginPage';
-import { Stack, Text, LinkButton, Box, TextLink } from '@grafana/ui';
+import { Stack, Text, LinkButton, Box, TextLink, CollapsableSection, Tooltip, Icon, Modal, Button } from '@grafana/ui';
 import { Trans } from 'app/core/internationalization';
 import { formatDate } from 'app/core/internationalization/dates';
 
-import {
-  getLatestCompatibleVersion,
-  getPluginRepositoryUrl,
-  fetchDefaultBranchFromRepo,
-  getCustomLink,
-} from '../helpers';
+import { getPluginRepositoryUrl, fetchDefaultBranchFromRepo, getCustomLink } from '../helpers';
 import { CatalogPlugin } from '../types';
 
 type Props = {
@@ -24,6 +19,7 @@ type Props = {
 export function PluginDetailsPanel(props: Props): React.ReactElement | null {
   const { pluginExtentionsInfo, plugin, width = '250px' } = props;
   const [defaultBranch, setDefaultBranch] = useState<string | null>(null);
+  const [reportAbuseModalOpen, setReportAbuseModalOpen] = useState(false);
 
   const trailURLSlash = (url: string | null) => (url?.endsWith('/') ? url?.slice(0, -1) : url);
 
@@ -55,91 +51,161 @@ export function PluginDetailsPanel(props: Props): React.ReactElement | null {
   const shouldRenderLinks = Boolean(plugin.details?.links?.length || repositoryLink);
 
   return (
-    <Stack direction="column" gap={3} shrink={0} grow={0} maxWidth={width} data-testid="plugin-details-panel">
-      <Box padding={2} borderColor="medium" borderStyle="solid">
-        <Stack direction="column" gap={2}>
-          {pluginExtentionsInfo.map((infoItem, index) => {
-            return (
-              <Stack key={index} wrap direction="column" gap={0.5}>
-                <Text color="secondary">{infoItem.label + ':'}</Text>
-                <div>{infoItem.value}</div>
-              </Stack>
-            );
-          })}
-          {plugin.updatedAt && (
-            <Stack direction="column" gap={0.5}>
-              <Text color="secondary">
-                <Trans i18nKey="plugins.details.labels.updatedAt">Last updated:</Trans>
-              </Text>{' '}
-              <Text>{formatDate(new Date(plugin.updatedAt), { day: 'numeric', month: 'short', year: 'numeric' })}</Text>
-            </Stack>
-          )}
-          {plugin?.details?.lastCommitDate && (
-            <Stack direction="column" gap={0.5}>
-              <Text color="secondary">
-                <Trans i18nKey="plugins.details.labels.lastCommitDate">Last commit date:</Trans>
-              </Text>{' '}
-              <Text>
-                {formatDate(new Date(plugin.details.lastCommitDate), {
-                  day: 'numeric',
-                  month: 'short',
-                  year: 'numeric',
-                })}
-              </Text>
-            </Stack>
-          )}
-        </Stack>
-      </Box>
-
-      {shouldRenderLinks && (
+    <>
+      <Stack direction="column" gap={3} shrink={0} grow={0} minWidth={width} data-testid="plugin-details-panel">
         <Box padding={2} borderColor="medium" borderStyle="solid">
           <Stack direction="column" gap={2}>
-            <Text color="secondary">
-              <Trans i18nKey="plugins.details.labels.links">Links </Trans>
-            </Text>
-            {repositoryLink && (
-              <LinkButton href={repositoryLink} variant="secondary" fill="solid">
-                <Trans i18nKey="plugins.details.labels.repository">Repository</Trans>
-              </LinkButton>
+            {pluginExtentionsInfo.map((infoItem, index) => {
+              return (
+                <Stack key={index} wrap direction="column" gap={0.5}>
+                  <Text color="secondary">{infoItem.label + ':'}</Text>
+                  <div>{infoItem.value}</div>
+                </Stack>
+              );
+            })}
+            {plugin.updatedAt && (
+              <Stack direction="column" gap={0.5}>
+                <Text color="secondary">
+                  <Trans i18nKey="plugins.details.labels.updatedAt">Last updated:</Trans>
+                </Text>{' '}
+                <Text>
+                  {formatDate(new Date(plugin.updatedAt), { day: 'numeric', month: 'short', year: 'numeric' })}
+                </Text>
+              </Stack>
             )}
+            {plugin?.details?.lastCommitDate && (
+              <Stack direction="column" gap={0.5}>
+                <Text color="secondary">
+                  <Trans i18nKey="plugins.details.labels.lastCommitDate">Last commit date:</Trans>
+                </Text>{' '}
+                <Text>
+                  {formatDate(new Date(plugin.details.lastCommitDate), {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric',
+                  })}
+                </Text>
+              </Stack>
+            )}
+          </Stack>
+        </Box>
 
-            {licenseLink && (
-              <LinkButton href={licenseLink} variant="secondary" fill="solid">
-                <Trans i18nKey="plugins.details.labels.license">License</Trans>
-              </LinkButton>
+        {shouldRenderLinks && (
+          <>
+            <Box padding={2} borderColor="medium" borderStyle="solid">
+              <Stack direction="column" gap={2}>
+                <Text color="secondary">
+                  <Trans i18nKey="plugins.details.labels.links">Links </Trans>
+                </Text>
+                {repositoryLink && (
+                  <LinkButton href={repositoryLink} variant="secondary" fill="solid" icon="github">
+                    <Trans i18nKey="plugins.details.labels.repository">Repository</Trans>
+                  </LinkButton>
+                )}
+                {repositoryLink && (
+                  <LinkButton href={`${repositoryLink}/issues/new`} variant="secondary" fill="solid" icon="github">
+                    <Trans i18nKey="plugins.details.labels.raiseAnIssue">Raise an issue</Trans>
+                  </LinkButton>
+                )}
+                {licenseLink && (
+                  <LinkButton href={licenseLink} variant="secondary" fill="solid" icon={'document-info'}>
+                    <Trans i18nKey="plugins.details.labels.license">License</Trans>
+                  </LinkButton>
+                )}
+                {documentationLink && (
+                  <LinkButton href={documentationLink} variant="secondary" fill="solid" icon={'list-ui-alt'}>
+                    <Trans i18nKey="plugins.details.labels.documentation">Documentation</Trans>
+                  </LinkButton>
+                )}
+              </Stack>
+            </Box>
+
+            {customLinks && customLinks?.length > 0 && (
+              <Box padding={2} borderColor="medium" borderStyle="solid">
+                <CollapsableSection
+                  isOpen={false}
+                  label={
+                    <Stack direction="row" justifyContent="space-between" alignItems="center">
+                      <Text color="secondary" variant="body">
+                        <Trans i18nKey="plugins.details.labels.customLinks">Custom links </Trans>
+                      </Text>
+                      <Tooltip
+                        content={
+                          'These links are provided by the plugin developer to offer additional, developer-specific resources and information'
+                        }
+                        placement="right-end"
+                      >
+                        <Icon name="info-circle" size="xs" />
+                      </Tooltip>
+                    </Stack>
+                  }
+                >
+                  <Stack direction="column" gap={2}>
+                    {customLinks.map((link, index) => (
+                      <TextLink key={index} href={link.url} external>
+                        {link.name}
+                      </TextLink>
+                    ))}
+                  </Stack>
+                </CollapsableSection>
+              </Box>
             )}
-            {documentationLink && (
-              <LinkButton href={documentationLink} variant="secondary" fill="solid">
-                <Trans i18nKey="plugins.details.labels.documentation">Documentation</Trans>
-              </LinkButton>
-            )}
-            {repositoryLink && (
-              <LinkButton href={`${repositoryLink}/issues/new`} variant="secondary" fill="solid">
-                <Trans i18nKey="plugins.details.labels.raiseAnIssue">Raise an issue</Trans>
-              </LinkButton>
-            )}
-            {customLinks &&
-              customLinks.map((link, index) => (
-                <TextLink key={index} href={link.url} external>
-                  {link.name}
-                </TextLink>
-              ))}
-          </Stack>
-        </Box>
-      )}
-      {!plugin?.isCore && (
-        <Box padding={2} borderColor="medium" borderStyle="solid">
-          <Stack direction="column">
-            <Text color="secondary">
-              <Trans i18nKey="plugins.details.labels.reportAbuse">Report a concern </Trans>
+          </>
+        )}
+        {!plugin?.isCore && (
+          <Box padding={2} borderColor="medium" borderStyle="solid">
+            <CollapsableSection
+              isOpen={false}
+              label={
+                <Stack direction="row" justifyContent="space-between" alignItems="center">
+                  <Text color="secondary" variant="body">
+                    <Trans i18nKey="plugins.details.labels.reportAbuse">Report a concern </Trans>
+                  </Text>
+                  <Tooltip
+                    content={'Report issues related to malicious or harmful plugins directly to Grafana Labs.'}
+                    placement="right-end"
+                  >
+                    <Icon name="info-circle" size="xs" />
+                  </Tooltip>
+                </Stack>
+              }
+            >
+              <Stack direction="column">
+                <Button variant="secondary" fill="solid" icon="bell" onClick={() => setReportAbuseModalOpen(true)}>
+                  <Trans i18nKey="plugins.details.labels.contactGrafanaLabs">Contact Grafana Labs</Trans>
+                </Button>
+              </Stack>
+            </CollapsableSection>
+          </Box>
+        )}
+      </Stack>
+      {reportAbuseModalOpen && (
+        <Modal title="Report a plugin concern" isOpen onDismiss={() => setReportAbuseModalOpen(false)}>
+          <Stack direction="column" gap={2}>
+            <Text>
+              <Trans i18nKey="plugins.details.modal.description">
+                This feature is for reporting malicious or harmful behaviour within plugins. For plugin concerns, email
+                us at: <TextLink href="mailto:integrations@grafana.com">integrations@grafana.com</TextLink>.{' '}
+              </Trans>
             </Text>
-            <LinkButton href="mailto:integrations@grafana.com" variant="secondary" fill="solid">
-              <Trans i18nKey="plugins.details.labels.contactGrafanaLabs">Contact Grafana Labs</Trans>
-            </LinkButton>
+            <Text>
+              <Trans i18nKey="plugins.details.modal.node">
+                Note: For general plugin issues like bugs or feature requests, please contact the plugin author using
+                the provided links.{' '}
+              </Trans>
+            </Text>
           </Stack>
-        </Box>
+          <Modal.ButtonRow>
+            <Button variant="secondary" fill="outline" onClick={() => setReportAbuseModalOpen(false)}>
+              Cancel
+            </Button>
+            <Button icon="copy" onClick={() => navigator.clipboard.writeText('integrations@grafana.com')}>
+              Copy email address
+            </Button>
+          </Modal.ButtonRow>
+        </Modal>
       )}
-    </Stack>
+    </>
   );
 }
 
