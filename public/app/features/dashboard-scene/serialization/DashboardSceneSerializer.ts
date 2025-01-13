4a9c4e810c77756f34f6c086ba88b24cc4d6229d
Dominik Prokop <dominik.prokop@grafana.com>
2025-01-13 12:40:14 +0100

    Revert "Schema v2: Reason about new dashboard based on UID (#98800)"
    This reverts commit 72846e34313838d31d27bceb314d4f842e6fe78b.

--- public/app/features/dashboard-scene/serialization/DashboardSceneSerializer.ts
+++ public/app/features/dashboard-scene/serialization/DashboardSceneSerializer.ts
@@ -155,7 +155,7 @@ export class V2DashboardSerializer
     );
 
     const hasFolderChanges = scene.getInitialState()?.meta.folderUid !== scene.state.meta.folderUid;
-    const isNew = scene.getInitialState()?.uid === '';
+    const isNew = scene.getInitialState()?.meta.isNew;
 
     return {
       ...changeInfo,
