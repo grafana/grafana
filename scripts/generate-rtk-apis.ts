57166a9be5de390723f8b1586db9d80ee6cbd4b5
Clarity-89 <homes89@ukr.net>
2025-01-13 16:13:03 +0200

    Add openapi file

--- scripts/generate-rtk-apis.ts
+++ scripts/generate-rtk-apis.ts
@@ -38,7 +38,7 @@ const config: ConfigFile = {
     },
     '../public/app/features/provisioning/api/endpoints.gen.ts': {
       apiFile: '../public/app/features/provisioning/api/baseAPI.ts',
-      schemaFile: '../pkg/tests/apis/provisioning/testdata/openapi.json',
+      schemaFile: '../public/app/features/provisioning/api/openapi.json',
       apiImport: 'baseAPI',
       filterEndpoints,
       argSuffix: 'Arg',
