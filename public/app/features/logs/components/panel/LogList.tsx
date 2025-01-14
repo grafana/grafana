27d6342851ebcbac0eb0fd1e5da6873fa855602a
Matias Chomicki <matyax@gmail.com>
2025-01-14 12:51:23 +0100

    Introduce log line processing

--- public/app/features/logs/components/panel/LogList.tsx
+++ public/app/features/logs/components/panel/LogList.tsx
@@ -1,22 +1,25 @@
 import { debounce } from 'lodash';
-import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react';
 import { ListChildComponentProps, VariableSizeList } from 'react-window';
 
 import { CoreApp, LogRowModel } from '@grafana/data';
 import { useTheme2 } from '@grafana/ui';
 
 import { getLogLineSize, LogLine } from './LogLine';
+import { preProcessLogs } from './processing';
 import { init as initVirtualization } from './virtualization';
 
 interface Props {
   app?: CoreApp;
   logs: LogRowModel[];
   containerElement: HTMLDivElement | null;
+  wrapLogMessage: boolean;
 }
 
-export const LogList = ({ containerElement, logs }: Props) => {
+export const LogList = ({ containerElement, logs, wrapLogMessage }: Props) => {
   const [listKey, setListKey] = useState(`${Math.random()}`);
   const theme = useTheme2();
+  const processedLogs = useMemo(() => preProcessLogs(logs, { wrapLogMessage }), [logs, wrapLogMessage]);
 
   useEffect(() => {
     initVirtualization(theme);
@@ -24,7 +27,7 @@ export const LogList = ({ containerElement, logs }: Props) => {
 
   useEffect(() => {
     setListKey(`${Math.random()}`);
-  }, [logs]);
+  }, [processedLogs]);
 
   useLayoutEffect(() => {
     const handleResize = debounce(() => setListKey(`${Math.random()}`), 500);
@@ -36,9 +39,9 @@ export const LogList = ({ containerElement, logs }: Props) => {
 
   const Renderer = useCallback(
     ({ index, style }: ListChildComponentProps) => {
-      return <LogLine log={logs[index]} style={style} />;
+      return <LogLine log={processedLogs[index]} style={style} wrapLogMessage={wrapLogMessage} />;
     },
-    [logs]
+    [processedLogs, wrapLogMessage]
   );
 
   const height = window.innerHeight * 0.75;
@@ -52,8 +55,8 @@ export const LogList = ({ containerElement, logs }: Props) => {
     <VariableSizeList
       key={listKey}
       height={height}
-      itemCount={logs.length}
-      itemSize={getLogLineSize.bind(null, logs, containerElement, theme)}
+      itemCount={processedLogs.length}
+      itemSize={getLogLineSize.bind(null, processedLogs, containerElement, theme, wrapLogMessage)}
       itemKey={(index: number) => index}
       width={'100%'}
       layout="vertical"
