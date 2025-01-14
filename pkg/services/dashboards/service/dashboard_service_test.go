7bd5a6d7724095298fb2d5279473e26334286eca
Owen Smallwood <owen.smallwood@grafana.com>
2025-01-14 12:22:59 -0600

    adds regression test

--- pkg/services/dashboards/service/dashboard_service_test.go
+++ pkg/services/dashboards/service/dashboard_service_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/grafana/grafana/pkg/storage/unified/search"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -1871,4 +1872,48 @@ func TestToUID(t *testing.T) {
 		result := toUID(rawIdentifier)
 		assert.Equal(t, "", result)
 	})
+}
+
+// regression test - parsing int32 values from search results was causing a panic
+func TestParseResults(t *testing.T) {
+	resSearchResp := &resource.ResourceSearchResponse{
+		Results: &resource.ResourceTable{
+			Columns: []*resource.ResourceTableColumnDefinition{
+				{
+					Name: "title",
+					Type: resource.ResourceTableColumnDefinition_STRING,
+				},
+				{
+					Name: "folder",
+					Type: resource.ResourceTableColumnDefinition_STRING,
+				},
+				{
+					Name: search.DASHBOARD_ERRORS_LAST_1_DAYS,
+					Type: resource.ResourceTableColumnDefinition_INT64,
+				},
+				{
+					Name: search.DASHBOARD_LINK_COUNT,
+					Type: resource.ResourceTableColumnDefinition_INT32,
+				},
+			},
+			Rows: []*resource.ResourceTableRow{
+				{
+					Key: &resource.ResourceKey{
+						Name:     "uid",
+						Resource: "dashboard",
+					},
+					Cells: [][]byte{
+						[]byte("Dashboard 1"),
+						[]byte("folder1"),
+						[]byte("100"),
+						[]byte("25"),
+					},
+				},
+			},
+		},
+		TotalHits: 1,
+	}
+
+	_, err := ParseResults(resSearchResp, 0)
+	require.NoError(t, err)
 }
