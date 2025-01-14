b50ceac862cbe839855e1763b84216d69f2683a4
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-01-14 16:52:45 +0000

    Fix logic for checking default email was changed

--- public/app/features/gops/configuration-tracker/alerting/utils.ts
+++ public/app/features/gops/configuration-tracker/alerting/utils.ts
@@ -3,13 +3,14 @@ const DEFAULT_EMAIL = '<example@email.com>';
 
 export function isContactPointReady(defaultContactPoint: string, contactPoints: Receiver[]) {
   // We consider the contact point ready if the default contact has the address filled
-
   const defaultEmailUpdated = contactPoints.some(
     (contactPoint: Receiver) =>
       contactPoint.name === defaultContactPoint &&
-      contactPoint.grafana_managed_receiver_configs?.some(
-        (receiver) => receiver.name === defaultContactPoint && receiver.settings?.address !== DEFAULT_EMAIL
-      )
+      contactPoint.grafana_managed_receiver_configs?.some((receiver) => {
+        const isEmailReceiver = receiver.type === 'email';
+        const addresses = receiver.settings?.addresses;
+        return isEmailReceiver && addresses && addresses !== DEFAULT_EMAIL;
+      })
   );
   return defaultEmailUpdated;
 }
