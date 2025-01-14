9b34a56d7cb393a2931a030cc52d822267497f08
Ieva <ieva.vasiljeva@grafana.com>
2025-01-14 08:33:42 +0000

    AuthZ service: Take action sets into account when checking folder create permissions (#98751)
    take action sets into account when checking folder create permissions

--- pkg/services/authz/rbac/store/testdata/mysql--permission_query-With_action_sets.sql
+++ pkg/services/authz/rbac/store/testdata/mysql--permission_query-With_action_sets.sql
@@ -1,6 +1,6 @@
 SELECT p.kind, p.attribute, p.identifier, p.scope FROM `grafana`.`permission` as p
 WHERE
-  p.action = 'folders:read'
+  p.action IN ('folders:edit', 'folders:admin', 'folders:create')
 AND p.role_id IN (
   SELECT role_id FROM `grafana`.`builtin_role` as br WHERE (br.role = 'Viewer' AND (br.org_id = 1 OR br.org_id = 0))
     UNION
