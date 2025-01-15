e9826c4fba0a3ac25d05bce4f9aa401f0a79d823
gamab <gabriel.mabille@grafana.com>
2025-01-15 10:49:12 +0100

    Tests

--- pkg/storage/unified/resource/grpc/authenticator_test.go
+++ pkg/storage/unified/resource/grpc/authenticator_test.go
@@ -1,17 +1,12 @@
 package grpc
 
 import (
-	"context"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/authlib/claims"
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
-	"github.com/grafana/grafana/pkg/services/authn"
-	"github.com/grafana/grafana/pkg/services/login"
-	"github.com/grafana/grafana/pkg/services/org"
 )
 
 func TestBasicEncodeDecode(t *testing.T) {
@@ -27,32 +22,7 @@ func TestBasicEncodeDecode(t *testing.T) {
 	auth := &Authenticator{}
 
 	md := encodeIdentityInMetadata(before)
-	after, err := auth.decodeMetadata(context.Background(), md)
-	require.NoError(t, err)
-	require.Equal(t, before.GetID(), after.GetID())
-	require.Equal(t, before.GetUID(), after.GetUID())
-	require.Equal(t, before.GetIdentityType(), after.GetIdentityType())
-	require.Equal(t, before.GetLogin(), after.GetLogin())
-	require.Equal(t, before.GetOrgID(), after.GetOrgID())
-	require.Equal(t, before.GetOrgName(), after.GetOrgName())
-	require.Equal(t, before.GetOrgRole(), after.GetOrgRole())
-}
-
-func TestRenderEncodeDecode(t *testing.T) {
-	before := &authn.Identity{
-		ID:              "0",
-		Type:            claims.TypeRenderService,
-		OrgID:           1,
-		OrgRoles:        map[int64]org.RoleType{1: identity.RoleEditor},
-		ClientParams:    authn.ClientParams{SyncPermissions: true},
-		LastSeenAt:      time.Now(),
-		AuthenticatedBy: login.RenderModule,
-	}
-
-	auth := &Authenticator{}
-
-	md := encodeIdentityInMetadata(before)
-	after, err := auth.decodeMetadata(context.Background(), md)
+	after, err := auth.decodeMetadata(md)
 	require.NoError(t, err)
 	require.Equal(t, before.GetID(), after.GetID())
 	require.Equal(t, before.GetUID(), after.GetUID())
