e31e2e365d32357c30b7ec39a029620fab1232cd
Vijay Karthik <vijay.karthik@oodle.ai>
2025-01-15 15:29:05 -0800

    Add cell type to inspect data links

--- public/app/plugins/panel/table/TableCellOptionEditor.tsx
+++ public/app/plugins/panel/table/TableCellOptionEditor.tsx
@@ -88,6 +88,7 @@ const cellDisplayModeOptions: Array<SelectableValue<TableCellOptions>> = [
   { value: { type: TableCellDisplayMode.ColorBackground }, label: 'Colored background' },
   { value: { type: TableCellDisplayMode.Gauge }, label: 'Gauge' },
   { value: { type: TableCellDisplayMode.DataLinks }, label: 'Data links' },
+  { value: { type: TableCellDisplayMode.InspectableDataLink }, label: 'Inspectable Data Link' },
   { value: { type: TableCellDisplayMode.JSONView }, label: 'JSON View' },
   { value: { type: TableCellDisplayMode.Image }, label: 'Image' },
 ];
