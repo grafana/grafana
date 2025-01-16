5d3066d000ac9e8878b890eb0d32fe156115d290
ismail simsek <ismailsimsek09@gmail.com>
2025-01-16 19:20:06 +0100

    lint

--- packages/grafana-prometheus/src/index.ts
+++ packages/grafana-prometheus/src/index.ts
@@ -88,8 +88,8 @@ export { PrometheusVariableSupport } from './variables';
 
 // For explore metrics
 export * from './language_provider';
-export {getPrometheusTime} from './language_utils';
-export {isValidLegacyName, utf8Support, wrapUtf8Filters} from './utf8_support';
-export {buildVisualQueryFromString} from './querybuilder/parsing';
-export {PromQueryModeller} from './querybuilder/PromQueryModeller';
-export {type QueryBuilderLabelFilter} from './querybuilder/shared/types';
+export { getPrometheusTime } from './language_utils';
+export { isValidLegacyName, utf8Support, wrapUtf8Filters } from './utf8_support';
+export { buildVisualQueryFromString } from './querybuilder/parsing';
+export { PromQueryModeller } from './querybuilder/PromQueryModeller';
+export { type QueryBuilderLabelFilter } from './querybuilder/shared/types';
