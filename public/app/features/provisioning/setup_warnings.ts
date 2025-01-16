f8eef9b4bb3d7652458e224654c8abfe425c463b
Ryan McKinley <ryantxu@gmail.com>
2025-01-16 12:53:36 +0300

    show error in UI

--- public/app/features/provisioning/setup_warnings.ts
+++ public/app/features/provisioning/setup_warnings.ts
@@ -1,15 +1,25 @@
-import { AppEvents } from '@grafana/data';
+import { AppEvents, FeatureToggles } from '@grafana/data';
 import { config, getAppEvents } from '@grafana/runtime';
 
 let sent = 0;
+const requiredFeatureToggles: Array<keyof FeatureToggles> = [
+  'kubernetesFolders',
+  'kubernetesDashboards',
+  'unifiedStorageSearch',
+];
 
 export function checkSetup() {
   const now = Date.now();
-  if (!config.featureToggles.unifiedStorageSearch && now - sent > 10000) {
-    sent = now;
-    getAppEvents().publish({
-      type: AppEvents.alertError.name,
-      payload: ['Provisioning setup errors', 'Missing feature: unifiedStorageSearch'],
+  if (now - sent > 10000) {
+    requiredFeatureToggles.forEach((f) => {
+      if (!config.featureToggles[f]) {
+        console.warn('Provisioning is missing feature toggle: ', f);
+        getAppEvents().publish({
+          type: AppEvents.alertError.name,
+          payload: ['Provisioning setup errors', 'Missing feature: ' + f],
+        });
+        sent = now;
+      }
     });
   }
 }
