4b65ffdbcd9b445836b9845b5f74cb6e30e93a7e
Ryan McKinley <ryantxu@gmail.com>
2025-01-16 16:01:33 +0300

    no memo

--- public/app/features/provisioning/SetupWarnings.tsx
+++ public/app/features/provisioning/SetupWarnings.tsx
@@ -5,22 +5,20 @@ import { config } from '@grafana/runtime';
 import { Alert } from '@grafana/ui';
 
 export function SetupWarnings() {
-  return useMemo(() => {
-    const alerts: ReactNode[] = [];
-    const requiredFeatureToggles: Array<keyof FeatureToggles> = [
-      'kubernetesFolders',
-      'kubernetesDashboards',
-      'unifiedStorageSearch',
-    ];
-    for (let f of requiredFeatureToggles) {
-      if (!config.featureToggles[f]) {
-        alerts.push(
-          <Alert title="Provisioning setup error" severity="error">
-            Missing required feature toggle: {f}
-          </Alert>
-        );
-      }
+  const alerts: ReactNode[] = [];
+  const requiredFeatureToggles: Array<keyof FeatureToggles> = [
+    'kubernetesFolders',
+    'kubernetesDashboards',
+    'unifiedStorageSearch',
+  ];
+  for (let f of requiredFeatureToggles) {
+    if (!config.featureToggles[f]) {
+      alerts.push(
+        <Alert title="Provisioning setup error" severity="error">
+          Missing required feature toggle: {f}
+        </Alert>
+      );
     }
-    return alerts;
-  }, []);
+  }
+  return alerts;
 }
