d713b4f95eb4962da6c4cc738f2fc2d63e4def97
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-01-17 15:35:09 -0500

    ignore UpdatedBy in diff and hash calculation

--- pkg/services/ngalert/store/deltas.go
+++ pkg/services/ngalert/store/deltas.go
@@ -9,7 +9,7 @@ import (
 )
 
 // AlertRuleFieldsToIgnoreInDiff contains fields that are ignored when calculating the RuleDelta.Diff.
-var AlertRuleFieldsToIgnoreInDiff = [...]string{"ID", "Version", "Updated"}
+var AlertRuleFieldsToIgnoreInDiff = [...]string{"ID", "Version", "Updated", "UpdatedBy"}
 
 type RuleDelta struct {
 	Existing *models.AlertRule
