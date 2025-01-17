aa63d1736314ac2e0bf9265ceac2b5aaeb17ee60
Dan Snyder <dan.snyder.3rd@gmail.com>
2025-01-17 11:40:27 -0500

    FlameGraph: Added units standard option (#89815)

--- public/app/plugins/panel/flamegraph/module.tsx
+++ public/app/plugins/panel/flamegraph/module.tsx
@@ -1,6 +1,13 @@
-import { PanelPlugin } from '@grafana/data';
+import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
 
 import { FlameGraphPanel } from './FlameGraphPanel';
 import { FlameGraphSuggestionsSupplier } from './suggestions';
 
-export const plugin = new PanelPlugin(FlameGraphPanel).setSuggestionsSupplier(new FlameGraphSuggestionsSupplier());
+export const plugin = new PanelPlugin(FlameGraphPanel)
+  .setSuggestionsSupplier(new FlameGraphSuggestionsSupplier())
+  .useFieldConfig({
+    disableStandardOptions: Object.values(FieldConfigProperty).filter((v) => v !== FieldConfigProperty.Unit),
+    standardOptions: {
+      [FieldConfigProperty.Unit]: {},
+    },
+  });
