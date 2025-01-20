38de0c77fc0e47801e2b4179a26194390ed64db9
Konrad Lalik <konrad.lalik@grafana.com>
2025-01-20 13:31:38 +0100

    Revert filtered items page size

--- public/app/features/alerting/unified/rule-list/FilterView.tsx
+++ public/app/features/alerting/unified/rule-list/FilterView.tsx
@@ -24,7 +24,7 @@ interface FilterViewProps {
   filterState: RulesFilter;
 }
 
-const FRONTENT_PAGE_SIZE = 40;
+const FRONTENT_PAGE_SIZE = 100;
 const API_PAGE_SIZE = 2000;
 
 export function FilterView({ filterState }: FilterViewProps) {
