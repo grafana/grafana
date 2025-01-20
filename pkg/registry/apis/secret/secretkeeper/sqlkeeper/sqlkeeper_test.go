bca1c9aef1233cf2c8b0fd64e91049c110f5b6ae
Dana Axinte <dana.axinte@grafana.com>
2025-01-20 10:55:56 +0000

    add simple tests

---
+++ pkg/registry/apis/secret/secretkeeper/sqlkeeper/sqlkeeper_test.go
@@ -0,0 +1,14 @@
+package sqlkeeper
+
+import (
+	"testing"
+
+	"github.com/grafana/grafana/pkg/tests/testsuite"
+)
+
+func TestMain(m *testing.M) {
+	testsuite.Run(m)
+}
+func Test_SQLKeeperSetup(t *testing.T) {
+	// TODO: add tests
+}
