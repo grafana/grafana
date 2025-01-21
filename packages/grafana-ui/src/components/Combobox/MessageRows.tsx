05aae98867ec6290639a617d3bf3e59161c63874
Tobias Skarhed <tobias.skarhed@gmail.com>
2025-01-21 14:01:11 +0100

    Fix text color

--- packages/grafana-ui/src/components/Combobox/MessageRows.tsx
+++ packages/grafana-ui/src/components/Combobox/MessageRows.tsx
@@ -1,22 +1,19 @@
 import { ReactNode } from 'react';
 
-import { useStyles2 } from '../../themes';
 import { Trans } from '../../utils/i18n';
 import { Icon } from '../Icon/Icon';
 import { Box } from '../Layout/Box/Box';
 import { Stack } from '../Layout/Stack/Stack';
+import { Text } from '../Text/Text';
 
-import { getComboboxStyles } from './getComboboxStyles';
-
-export const AsyncError = () => {
-  const styles = useStyles2(getComboboxStyles);
-  return (
-    <MessageRow>
-      <Icon name="exclamation-triangle" size="md" className={styles.warningIcon} />
-      <Trans i18nKey="combobox.async.error">An error occurred while loading options.</Trans>
-    </MessageRow>
-  );
-};
+export const AsyncError = () => (
+  <MessageRow>
+    <Text color="warning">
+      <Icon name="exclamation-triangle" size="md" />
+    </Text>
+    <Trans i18nKey="combobox.async.error">An error occurred while loading options.</Trans>
+  </MessageRow>
+);
 
 export const NotFoundError = () => (
   <MessageRow>
@@ -26,8 +23,8 @@ export const NotFoundError = () => (
 
 const MessageRow = ({ children }: { children: ReactNode }) => {
   return (
-    <Box padding={2} color="secondary">
-      <Stack justifyContent="center" alignItems="center">
+    <Box padding={2}>
+      <Stack justifyContent="center" alignItems="center" direction="column">
         {children}
       </Stack>
     </Box>
