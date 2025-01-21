d58f29753dc64cb3f8bd113a22e3c41435e8cedf
Victor Marin <36818606+mdvictor@users.noreply.github.com>
2025-01-21 18:09:23 +0200

    DashboardScenes: Fix issue where timezone was not considered in relative time override (#99316)
    * Fix issue where timezone was not considered in relative time override
    
    * fix

--- public/app/features/dashboard-scene/scene/PanelTimeRange.tsx
+++ public/app/features/dashboard-scene/scene/PanelTimeRange.tsx
@@ -81,10 +81,11 @@ export class PanelTimeRange extends SceneTimeRangeTransformerBase<PanelTimeRange
 
       // Only evaluate if the timeFrom if parent time is relative
       if (rangeUtil.isRelativeTimeRange(parentTimeRange.raw)) {
+        const timeZone = this.getTimeZone();
         newTimeData.timeInfo = timeFromInfo.display;
         newTimeData.timeRange = {
-          from: dateMath.parse(timeFromInfo.from)!,
-          to: dateMath.parse(timeFromInfo.to)!,
+          from: dateMath.parse(timeFromInfo.from, undefined, timeZone)!,
+          to: dateMath.parse(timeFromInfo.to, undefined, timeZone)!,
           raw: { from: timeFromInfo.from, to: timeFromInfo.to },
         };
       }
