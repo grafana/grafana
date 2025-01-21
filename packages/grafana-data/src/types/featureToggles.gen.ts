97d8f68b705f9949493079d1833abfe80e7b48f3
Syerikjan Kh <syerikjan.khusayan@grafana.com>
2025-01-21 12:39:48 -0500

    Feat: Data source connections tab in plugin detail page (#99049)
    * feat: datasource connections tab in plugin detail page
    
    * test: no ds defined test added
    
    * feat: configure feature toggle
    
    * chore: i18n extract
    
    * test: added unit tests for page and list

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -254,4 +254,5 @@ export interface FeatureToggles {
   queryLibraryDashboards?: boolean;
   grafanaAdvisor?: boolean;
   elasticsearchImprovedParsing?: boolean;
+  datasourceConnectionsTab?: boolean;
 }
