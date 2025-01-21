45e2f95a4174e9b4e7f7b6c32bf8fb521e755cad
maicon <maiconscosta@gmail.com>
2025-01-21 17:42:38 -0300

    Unistore: Avoid circular dependency when injecting restcfgprovider into FolderSvc (#99295)
    * Avoid circular dependency when getting a restCfgProvider for Folder Svc
    
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>
    
    ---------
    
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/services/folder/folderimpl/unifiedstore.go
+++ pkg/services/folder/folderimpl/unifiedstore.go
@@ -15,9 +15,7 @@ import (
 	"github.com/grafana/grafana/pkg/apis/folder/v0alpha1"
 	"github.com/grafana/grafana/pkg/infra/log"
 	internalfolders "github.com/grafana/grafana/pkg/registry/apis/folders"
-	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
 	"github.com/grafana/grafana/pkg/services/folder"
-	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/util"
 )
 
@@ -29,13 +27,7 @@ type FolderUnifiedStoreImpl struct {
 // sqlStore implements the store interface.
 var _ folder.Store = (*FolderUnifiedStoreImpl)(nil)
 
-func ProvideUnifiedStore(cfg *setting.Cfg) *FolderUnifiedStoreImpl {
-	k8sHandler := &foldk8sHandler{
-		gvr:        v0alpha1.FolderResourceInfo.GroupVersionResource(),
-		namespacer: request.GetNamespaceMapper(cfg),
-		cfg:        cfg,
-	}
-
+func ProvideUnifiedStore(k8sHandler *foldk8sHandler) *FolderUnifiedStoreImpl {
 	return &FolderUnifiedStoreImpl{
 		k8sclient: k8sHandler,
 		log:       log.New("folder-store"),
