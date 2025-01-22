5bde5c0e5b8a5b2f7ed315f2b932c3b1bca1632b
Bogdan Matei <bogdan.matei@grafana.com>
2025-01-22 15:57:45 +0200

    Dashboards: Prevent rows nesting (#99246)

--- public/app/features/dashboard-scene/scene/layout-rows/RowItem.tsx
+++ public/app/features/dashboard-scene/scene/layout-rows/RowItem.tsx
@@ -95,13 +95,13 @@ export class RowItem extends SceneObjectBase<RowItemState> implements LayoutPare
   };
 
   public static Component = ({ model }: SceneComponentProps<RowItem>) => {
-    const { layout, title, isCollapsed, height = 'expand', isHeaderHidden } = model.useState();
+    const { layout, title, isCollapsed, height = 'expand', isHeaderHidden, key } = model.useState();
     const { isEditing, showHiddenElements } = getDashboardSceneFor(model).useState();
     const styles = useStyles2(getStyles);
     const titleInterpolated = sceneGraph.interpolate(model, title, undefined, 'text');
     const ref = useRef<HTMLDivElement>(null);
     const shouldGrow = !isCollapsed && height === 'expand';
-    const { isSelected, onSelect } = useElementSelection(model.state.key);
+    const { isSelected, onSelect } = useElementSelection(key);
 
     return (
       <div
