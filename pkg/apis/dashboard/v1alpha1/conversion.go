b4c2b72e38bee5074b9929f846a1afac438e6b9d
Todd Treece <360020+toddtreece@users.noreply.github.com>
2025-01-22 14:11:19 -0500

    Dashboards: Schema version migration

--- pkg/apis/dashboard/v1alpha1/conversion.go
+++ pkg/apis/dashboard/v1alpha1/conversion.go
@@ -5,22 +5,24 @@ import (
 	klog "k8s.io/klog/v2"
 
 	common "github.com/grafana/grafana/pkg/apimachinery/apis/common/v0alpha1"
+	"github.com/grafana/grafana/pkg/apis/dashboard/migration"
+	"github.com/grafana/grafana/pkg/apis/dashboard/migration/schemaversion"
 )
 
 func Convert_v0alpha1_Unstructured_To_v1alpha1_DashboardSpec(in *common.Unstructured, out *DashboardSpec, s conversion.Scope) error {
-	out.Unstructured = *in
-
-	t, ok := in.Object["title"]
-	if !ok {
-		return nil // skip setting the title if it's not in the unstructured object
+	err := migration.Migrate(in.Object, schemaversion.LATEST_VERSION)
+	if err != nil {
+		return err
 	}
 
-	title, ok := t.(string)
+	out.Unstructured = *in
+
+	t, ok := in.Object["title"].(string)
 	if !ok {
 		klog.V(5).Infof("unstructured dashboard title field is not a string %v", t)
 		return nil // skip setting the title if it's not a string in the unstructured object
 	}
-	out.Title = title
+	out.Title = t
 
 	return nil
 }
