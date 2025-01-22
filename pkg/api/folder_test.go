82ce3728a153b6a81c5128c75b6c03665e07e6a0
Ryan McKinley <ryantxu@gmail.com>
2025-01-23 00:09:35 +0300

    remove feature toggles

--- pkg/api/folder_test.go
+++ pkg/api/folder_test.go
@@ -625,7 +625,7 @@ func TestGetFolderLegacyAndUnifiedStorage(t *testing.T) {
 
 				featuresArr := []any{featuremgmt.FlagNestedFolders}
 				if tc.unifiedStorageEnabled {
-					featuresArr = append(featuresArr, featuremgmt.FlagKubernetesFolders)
+					featuresArr = append(featuresArr, featuremgmt.FlagKubernetesFoldersServiceV2)
 				}
 
 				server := SetupAPITestServer(t, func(hs *HTTPServer) {
