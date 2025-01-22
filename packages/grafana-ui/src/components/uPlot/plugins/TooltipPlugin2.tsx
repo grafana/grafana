489edd703a2a29043d5c0766d214f93e9059903f
Adela Almasan <adela.almasan@grafana.com>
2025-01-22 11:21:41 -0600

    remove cursor todo

--- packages/grafana-ui/src/components/uPlot/plugins/TooltipPlugin2.tsx
+++ packages/grafana-ui/src/components/uPlot/plugins/TooltipPlugin2.tsx
@@ -626,13 +626,6 @@ export const TooltipPlugin2 = ({
 
         transform = `translateX(${shiftX}px) ${reflectX} translateY(${shiftY}px) ${reflectY}`;
 
-        // TODO: revisit
-        // if (closestSeriesIdx != null && persistentLinks[closestSeriesIdx]?.length > 0) {
-        //   u.over.style.cursor = 'pointer';
-        // } else {
-        //   u.over.style.cursor = 'default';
-        // }
-
         if (domRef.current != null) {
           domRef.current.style.transform = transform;
         } else {
