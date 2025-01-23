10f3b3f97dd49489b5f43178f525f46249728312
Mariell Hoversholm <mariell.hoversholm@grafana.com>
2025-01-23 08:03:54 +0100

    feat: initial folder ID concept

---
+++ pkg/registry/apis/provisioning/resources/folderid_test.go
@@ -0,0 +1,40 @@
+package resources
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseFolderID(t *testing.T) {
+	hash := func(s string) string {
+		hashed := sha256.Sum256([]byte(strings.Trim(path.Clean(s), "/"))) // just to make it easier to do strings.Repeat with slashes
+		return hex.EncodeToString(hashed[:])[:8]
+	}
+
+	cases := []struct {
+		Description string
+		Path        string
+		Title       string
+		KubeName    string
+	}{
+		{"Short, simple path", "hello/world", "world", "helloworld-" + hash("hello/world")},
+		{"Capital letters and punctuation", "Hello, World!", "Hello, World!", "hello-world-" + hash("Hello, World!")},
+		// With fish: echo (string repeat -n 200 -m (math 253-9) "helloworld")-(string repeat -n 200 "/hello/world" | sha256sum | awk '{print substr($1,0,8);}')
+		{"Very long name", strings.Repeat("/hello/world", 200), "world", strings.Repeat("helloworld", 200)[:253-9] + "-" + hash(strings.Repeat("hello/world/", 200))},
+		{"Leading, trailing, and unnecessary slashes", "/hello///world////", "world", "helloworld-" + hash("hello/world")},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Description, func(t *testing.T) {
+			id := ParseFolderID(c.Path)
+			assert.Equal(t, c.Path, id.Path)
+			assert.Equal(t, c.KubeName, id.KubernetesName)
+			assert.Equal(t, c.Title, id.Title)
+		})
+	}
+}
