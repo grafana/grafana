ed8c47405f48285e1ea98ec84c61eb2a65458456
Torkel Ödegaard <torkel@grafana.com>
2025-01-23 12:19:38 +0100

    Update

--- public/app/plugins/datasource/grafana-testdata-datasource/metricTree.ts
+++ public/app/plugins/datasource/grafana-testdata-datasource/metricTree.ts
@@ -69,18 +69,9 @@ function queryTree(children: TreeNode[], query: string[], queryIndex: number): T
   return result;
 }
 
-let i = 0;
-
 export function queryMetricTree(query: string): TreeNode[] {
   if (query.indexOf('value') === 0) {
     return [{ name: query, children: [] }];
-  }
-
-  if (i === 1) {
-    query = 'B.*';
-    i = 0;
-  } else {
-    i = 1;
   }
 
   const children = buildMetricTree('', 0);
