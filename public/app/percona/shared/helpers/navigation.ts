b9728768611d00c1707247cc443d1490786a2dd7
Matej Kubinec <32638572+matejkubinec@users.noreply.github.com>
2025-01-23 11:42:50 +0100

    PMM-13689 Remove menu link variables with value "All" (#801)

--- public/app/percona/shared/helpers/navigation.ts
+++ public/app/percona/shared/helpers/navigation.ts
@@ -2,12 +2,13 @@ import { getLinkSrv } from 'app/features/panel/panellinks/link_srv';
 
 export const useLinkWithVariables = (url?: string) => {
   if (url && isDashboardUrl(url) && isDashboardUrl(window.location.pathname)) {
-    return getLinkSrv().getLinkUrl({
+    const urlWithLinks = getLinkSrv().getLinkUrl({
       url: url,
       keepTime: true,
       // Check if the DB type matches the current one used
       includeVars: checkDbType(url),
     });
+    return cleanupVariables(urlWithLinks);
   } else {
     return url ? url : '#';
   }
@@ -19,5 +20,22 @@ const checkDbType = (url: string): boolean => {
   const currentDB = window.location.pathname?.split('/')[3]?.split('-')[0];
   const urlDB = url?.split('/')[3]?.split('-')[0];
 
-  return currentDB !== undefined && currentDB === urlDB;
+  // enable variable sharing between same db types and db type -> os/node
+  return (currentDB !== undefined && currentDB === urlDB) || urlDB === 'node';
+};
+
+const cleanupVariables = (urlWithLinks: string) => {
+  const [base, params] = urlWithLinks.split('?');
+
+  if (params) {
+    // remove variables which have the All value or the value is empty
+    const variables = params
+      .split('&')
+      .filter((param) => !(param.includes('All') || param.endsWith('=')))
+      .join('&');
+
+    return base + '?' + variables;
+  }
+
+  return base;
 };
