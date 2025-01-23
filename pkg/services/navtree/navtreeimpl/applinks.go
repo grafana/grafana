3bbe2b4a2b363a454400b44d90b0cc334da31805
Nick Richmond <nick.richmond@grafana.com>
2025-01-23 10:10:49 -0500

    chore: put app navigation settings behind feature toggle

--- pkg/services/navtree/navtreeimpl/applinks.go
+++ pkg/services/navtree/navtreeimpl/applinks.go
@@ -286,10 +286,9 @@ func (s *ServiceImpl) readNavigationSettings() {
 		"grafana-k8s-app":                  {SectionID: navtree.NavIDInfrastructure, SortWeight: 1, Text: "Kubernetes"},
 		"grafana-dbo11y-app":               {SectionID: navtree.NavIDInfrastructure, SortWeight: 2, Text: "Databases"},
 		"grafana-app-observability-app":    {SectionID: navtree.NavIDRoot, SortWeight: navtree.WeightApplication, Text: "Application", Icon: "graph-bar"},
-		"grafana-exploremetrics-app":       {SectionID: navtree.NavIDExplore, SortWeight: 1, Text: "Metrics"},
-		"grafana-lokiexplore-app":          {SectionID: navtree.NavIDExplore, SortWeight: 2, Text: "Logs"},
-		"grafana-exploretraces-app":        {SectionID: navtree.NavIDExplore, SortWeight: 3, Text: "Traces"},
-		"grafana-pyroscope-app":            {SectionID: navtree.NavIDExplore, SortWeight: 4, Text: "Profiles"},
+		"grafana-lokiexplore-app":          {SectionID: navtree.NavIDExplore, SortWeight: 1, Text: "Logs"},
+		"grafana-exploretraces-app":        {SectionID: navtree.NavIDExplore, SortWeight: 2, Text: "Traces"},
+		"grafana-pyroscope-app":            {SectionID: navtree.NavIDExplore, SortWeight: 3, Text: "Profiles"},
 		"grafana-kowalski-app":             {SectionID: navtree.NavIDRoot, SortWeight: navtree.WeightFrontend, Text: "Frontend", Icon: "frontend-observability"},
 		"grafana-synthetic-monitoring-app": {SectionID: navtree.NavIDTestingAndSynthetics, SortWeight: 2, Text: "Synthetics"},
 		"grafana-irm-app":                  {SectionID: navtree.NavIDAlertsAndIncidents, SortWeight: 1, Text: "IRM"},
@@ -308,6 +307,13 @@ func (s *ServiceImpl) readNavigationSettings() {
 		"k6-app":                           {SectionID: navtree.NavIDTestingAndSynthetics, SortWeight: 1, Text: "Performance"},
 		"grafana-asserts-app":              {SectionID: navtree.NavIDRoot, SortWeight: navtree.WeightAsserts, Icon: "asserts"},
 		"grafana-csp-app":                  {SectionID: navtree.NavIDRoot, SortWeight: navtree.WeightCloudServiceProviders, Icon: "cloud"},
+	}
+
+	if s.features.IsEnabledGlobally(featuremgmt.FlagExploreMetricsUseExternalAppPlugin) {
+		s.navigationAppConfig["grafana-exploremetrics-app"] = NavigationAppConfig{SectionID: navtree.NavIDExplore, SortWeight: 1, Text: "Metrics"}
+		s.navigationAppConfig["grafana-lokiexplore-app"] = NavigationAppConfig{SectionID: navtree.NavIDExplore, SortWeight: 2, Text: "Logs"}
+		s.navigationAppConfig["grafana-exploretraces-app"] = NavigationAppConfig{SectionID: navtree.NavIDExplore, SortWeight: 3, Text: "Traces"}
+		s.navigationAppConfig["grafana-pyroscope-app"] = NavigationAppConfig{SectionID: navtree.NavIDExplore, SortWeight: 4, Text: "Profiles"}
 	}
 
 	s.navigationAppPathConfig = map[string]NavigationAppConfig{
