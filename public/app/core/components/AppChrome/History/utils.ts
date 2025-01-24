f1a946f208dd74a7da360d9ed0f826bc7e8ef5dc
eledobleefe <laura.fernandez@grafana.com>
2025-01-24 16:29:09 +0100

    Avoid adding entries when they do not have a name

--- public/app/core/components/AppChrome/History/utils.ts
+++ public/app/core/components/AppChrome/History/utils.ts
@@ -8,6 +8,7 @@ export const hackyFixes = (newEntry: HistoryEntry, entries: HistoryEntry[]): His
   const existingEntry = entries[0];
   const existingUrlPath = existingEntry && getEntryPath(existingEntry.url);
   const existingUrlQueryParams = existingEntry && getEntryQueryParams(existingEntry.url);
+  const existingUrlTitle = existingEntry && existingEntry.name;
   const newUrlPath = getEntryPath(newEntry.url);
   const newUrlQueryParams = getEntryQueryParams(newEntry.url);
   const newUrlTitle = newEntry.name;
@@ -15,10 +16,26 @@ export const hackyFixes = (newEntry: HistoryEntry, entries: HistoryEntry[]): His
   // Explore page without query params should return the entries without the new entry
   if (newUrlPath.endsWith('/explore') && !newUrlQueryParams) {
     entries = entries;
+  } else if (newUrlPath.endsWith('/explore') && newUrlQueryParams && newUrlTitle === 'Explore') {
+    // Explore page with query params called 'Explore' shouldn't be added to the history
+    entries = entries;
+  } else if (
+    newUrlPath.endsWith('/explore') &&
+    existingEntry &&
+    existingUrlQueryParams &&
+    newUrlTitle === existingUrlTitle &&
+    newUrlQueryParams !== existingUrlQueryParams
+  ) {
+    // Explore page with query params but different than the previous one while having the same title shouldn't be added to the history
+    entries = entries;
   } else if (newUrlPath.includes('\/d\/') && newUrlTitle === 'Dashboards') {
     // Dashboard page called 'Dashboards' won't be added to the history as it is the Browse Dashboard page
     entries = entries;
   } else if (newUrlPath.includes('\/d\/') && existingEntry && existingUrlPath === newUrlPath) {
+    entries = entries;
+    // If the new entry does not have a name we avoid adding this to the history
+    // This is what happens to Frotend page that gets redirected and creates a 'Home > Frontend >' entry
+  } else if (newUrlTitle === '') {
     entries = entries;
   } else {
     entries = [newEntry, ...entries];
