dc2a04542f89186826d5bf67528e563b3d1c6977
ismail simsek <ismailsimsek09@gmail.com>
2025-01-26 17:51:12 +0100

    add unit tests

---
+++ pkg/promlib/resource/resource_test.go
@@ -0,0 +1,92 @@
+package resource_test
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	// "github.com/grafana/grafana/pkg/promlib/client"
+	"github.com/grafana/grafana/pkg/promlib/resource"
+)
+
+func TestNewResource(t *testing.T) {
+	httpClient := &http.Client{}
+	settings := backend.DataSourceInstanceSettings{
+		ID:       1,
+		URL:      "http://localhost:9090",
+		JSONData: []byte(`{"httpMethod": "GET"}`),
+	}
+	logger := log.DefaultLogger
+
+	res, err := resource.New(httpClient, settings, logger)
+	require.NoError(t, err)
+	assert.NotNil(t, res)
+}
+
+func TestResource_Execute(t *testing.T) {
+	httpClient := &http.Client{}
+	settings := backend.DataSourceInstanceSettings{
+		ID:       1,
+		URL:      "http://localhost:9090",
+		JSONData: []byte(`{"httpMethod": "GET"}`),
+	}
+	logger := log.DefaultLogger
+
+	res, err := resource.New(httpClient, settings, logger)
+	require.NoError(t, err)
+
+	req := &backend.CallResourceRequest{
+		URL: "/test",
+	}
+	ctx := context.Background()
+
+	resp, err := res.Execute(ctx, req)
+	require.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+// func TestGetSelectors(t *testing.T) {
+// 	expr := `http_requests_total{job="api-server"}`
+// 	selectors, err := resource.GetSelectors(expr)
+// 	require.NoError(t, err)
+// 	assert.Contains(t, selectors, "http_requests_total")
+// }
+
+func TestResource_GetSuggestions(t *testing.T) {
+	httpClient := &http.Client{}
+	settings := backend.DataSourceInstanceSettings{
+		ID:       1,
+		URL:      "http://localhost:9090",
+		JSONData: []byte(`{"httpMethod": "GET"}`),
+	}
+	logger := log.DefaultLogger
+
+	res, err := resource.New(httpClient, settings, logger)
+	require.NoError(t, err)
+
+	suggestionReq := resource.SuggestionRequest{
+		LabelName: "instance",
+		Queries:   []string{"up"},
+		Start:     "1609459200",
+		End:       "1609462800",
+		Limit:     10,
+	}
+
+	body, err := json.Marshal(suggestionReq)
+	require.NoError(t, err)
+
+	req := &backend.CallResourceRequest{
+		Body: body,
+	}
+	ctx := context.Background()
+
+	resp, err := res.GetSuggestions(ctx, req)
+	require.NoError(t, err)
+	assert.NotNil(t, resp)
+}
