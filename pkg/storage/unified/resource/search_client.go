a617e8c78b97d9bb4217bd5ff637fe6f777e1ec5
Will Assis <william@williamassis.com>
2025-01-27 12:37:53 -0300

    improve naming

--- pkg/storage/unified/resource/search_client.go
+++ pkg/storage/unified/resource/search_client.go
@@ -2,11 +2,11 @@ package resource
 
 import "github.com/grafana/grafana/pkg/apiserver/rest"
 
-func NewSearchClient(mode rest.DualWriterMode, newClient ResourceIndexClient, legacyClient ResourceIndexClient) ResourceIndexClient {
+func NewSearchClient(mode rest.DualWriterMode, unifiedClient ResourceIndexClient, legacyClient ResourceIndexClient) ResourceIndexClient {
 	switch mode {
 	case rest.Mode0, rest.Mode1, rest.Mode2:
 		return legacyClient
 	default:
-		return newClient
+		return unifiedClient
 	}
 }
