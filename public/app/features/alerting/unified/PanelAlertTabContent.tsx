a909f7703b17949865863177bca38d155b512b39
Dominik Prokop <dominik.prokop@grafana.com>
2025-01-27 11:18:06 +0100

    Schema v2: Reason about new dashboard based on UID (#98879)
    * Schema v2: Reason about new dashboard based on UID
    
    * Fix test
    
    * Alerting: respect isNew dashboard for legacy and new arch
    
    * Translate untranslated strings
    
    * Unify is new checks
    
    * PanelInspectDrawer update
    
    * typo fix
    
    * on close test for panel inspect drawer
    
    * Update public/app/features/alerting/unified/PanelAlertTabContent.tsx

--- public/app/features/alerting/unified/PanelAlertTabContent.tsx
+++ public/app/features/alerting/unified/PanelAlertTabContent.tsx
@@ -3,6 +3,7 @@ import { css } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { Alert, LoadingPlaceholder, ScrollContainer, useStyles2 } from '@grafana/ui';
+import { Trans } from 'app/core/internationalization';
 import { contextSrv } from 'app/core/services/context_srv';
 import { DashboardModel } from 'app/features/dashboard/state/DashboardModel';
 import { PanelModel } from 'app/features/dashboard/state/PanelModel';
@@ -59,18 +60,26 @@ export const PanelAlertTabContent = ({ dashboard, panel }: Props) => {
     );
   }
 
+  const isNew = !Boolean(dashboard.uid);
+
   return (
     <div data-testid={selectors.components.PanelAlertTabContent.content} className={styles.noRulesWrapper}>
       {alert}
-      {!!dashboard.uid && (
+      {!isNew && (
         <>
-          <p>There are no alert rules linked to this panel.</p>
+          <p>
+            <Trans i18nKey="dashboard.panel-edit.alerting-tab.no-rules">
+              There are no alert rules linked to this panel.
+            </Trans>
+          </p>
           {!!dashboard.meta.canSave && canCreateRules && <NewRuleFromPanelButton panel={panel} dashboard={dashboard} />}
         </>
       )}
-      {!dashboard.uid && !!dashboard.meta.canSave && (
+      {isNew && !!dashboard.meta.canSave && (
         <Alert severity="info" title="Dashboard not saved">
-          Dashboard must be saved before alerts can be added.
+          <Trans i18nKey="dashboard.panel-edit.alerting-tab.dashboard-not-saved">
+            Dashboard must be saved before alerts can be added.
+          </Trans>
         </Alert>
       )}
     </div>
