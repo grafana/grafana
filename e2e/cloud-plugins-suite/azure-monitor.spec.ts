bbd79529e87797349351e348baacd27c64fd5705
alyssabull <alyssabull@gmail.com>
2025-01-27 11:29:56 -0700

    Fix selector

--- e2e/cloud-plugins-suite/azure-monitor.spec.ts
+++ e2e/cloud-plugins-suite/azure-monitor.spec.ts
@@ -209,7 +209,7 @@ describe('Azure monitor datasource', () => {
           .type('{enter}');
         cy.contains(logAnalyticsName).click();
         e2eSelectors.queryEditor.resourcePicker.apply.button().click();
-        e2e.components.CodeEditor.container().type('AzureDiagnostics');
+        e2eSelectors.queryEditor.logsQueryEditor.container.input.apply('AzureDiagnostics');
         e2eSelectors.queryEditor.logsQueryEditor.formatSelection.input().type('Time series{enter}');
       },
       timeout: 10000,
