ec59c1620d3b140636e9853576502947db79b9c1
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-01-28 10:22:17 -0700

    K8s: Folders: Fix save command

--- pkg/services/dashboards/service/dashboard_service.go
+++ pkg/services/dashboards/service/dashboard_service.go
@@ -361,12 +361,22 @@ func (dr *DashboardServiceImpl) BuildSaveDashboardCommand(ctx context.Context, d
 
 	// Validate folder
 	if dr.features.IsEnabledGlobally(featuremgmt.FlagKubernetesFoldersServiceV2) {
-		folder, err := dr.folderService.Get(ctx, &folder.GetFolderQuery{
+		query := &folder.GetFolderQuery{
 			OrgID:        dash.OrgID,
-			UID:          &dash.FolderUID,
-			ID:           &dash.FolderID, // nolint:staticcheck
 			SignedInUser: dto.User,
-		})
+		}
+
+		switch {
+		case dash.FolderUID != "":
+			query.UID = &dash.FolderUID
+		case dash.FolderID != 0:
+			query.ID = &dash.FolderID // nolint:staticcheck
+		default:
+			// return the default folder if no folder information is set
+			query.UID = &dash.FolderUID
+		}
+
+		folder, err := dr.folderService.Get(ctx, query)
 		if err != nil {
 			return nil, err
 		}
