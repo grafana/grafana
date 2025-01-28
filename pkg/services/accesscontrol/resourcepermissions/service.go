95364187ae4d0693d6c951dfbee362f186414664
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-01-28 12:22:40 -0700

    Validate with service account

--- pkg/services/accesscontrol/resourcepermissions/service.go
+++ pkg/services/accesscontrol/resourcepermissions/service.go
@@ -293,7 +293,9 @@ func (s *Service) SetPermissions(
 	ctx, span := tracer.Start(ctx, "accesscontrol.resourcepermissions.SetPermissions")
 	defer span.End()
 
-	if err := s.validateResource(ctx, orgID, resourceID); err != nil {
+	// permissions have not yet been set - validate the resource with a service account
+	identityCtx, _ := identity.WithServiceIdentitiy(ctx, orgID)
+	if err := s.validateResource(identityCtx, orgID, resourceID); err != nil {
 		return nil, err
 	}
 
