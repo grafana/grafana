ce42a4349ccaf5a22034193aee2825d1f54eb5fe
Jev Forsberg <46619047+baldm0mma@users.noreply.github.com>
2025-01-28 10:00:40 -0700

    Release: 11.2.6 (#99682)
    * apply security patch: v11.2.x/308-202501232104.patch
    
    commit 50c0dd282b73ed95b7c5a9033935203f556b239d
    Author: Matt Jacobson <matthew.jacobson@grafana.com>
    Date:   Thu Jan 23 15:26:33 2025 -0500
    
        CVE-2024-11741 - victorops url
    
    * baldm0mma/ add changelog content
    
    * baldm0mma/ update version
    
    * baldm0mma/ update go changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- pkg/services/ngalert/notifier/channels_config/available_channels.go
+++ pkg/services/ngalert/notifier/channels_config/available_channels.go
@@ -387,6 +387,7 @@ func GetAvailableNotifiers() []*NotifierPlugin {
 					Placeholder:  "VictorOps url",
 					PropertyName: "url",
 					Required:     true,
+					Secure:       true,
 				},
 				{ // New in 8.0.
 					Label:        "Message Type",
