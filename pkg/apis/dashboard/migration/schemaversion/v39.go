b9e822f98802a2b4cf4a1b86f8c5edddbeb3c15f
Ivan Ortega <ivanortegaalba@gmail.com>
2025-01-29 11:49:21 +0100

    remove nesting using early return if casting doesn't work

--- pkg/apis/dashboard/migration/schemaversion/v39.go
+++ pkg/apis/dashboard/migration/schemaversion/v39.go
@@ -2,38 +2,57 @@ package schemaversion
 
 // V39 updates the configuration of the Timeseries to table transformation
 // to support multiple options per query
-func V39(dash map[string]interface{}) error {
-	dash["schemaVersion"] = int(39)
-	if panels, ok := dash["panels"].([]interface{}); ok {
-		for _, panel := range panels {
-			if p, ok := panel.(map[string]interface{}); ok {
-				if transformations, ok := p["transformations"].([]interface{}); ok {
-					for _, transformation := range transformations {
-						if t, ok := transformation.(map[string]interface{}); ok {
-							// If we run into a timeSeriesTable transformation
-							// and it doesn't have undefined options then we migrate
-							if t["id"] == "timeSeriesTable" {
-								if options, ok := t["options"].(map[string]interface{}); ok {
-									if refIdToStat, ok := options["refIdToStat"].(map[string]interface{}); ok {
-										tableTransformOptions := make(map[string]interface{})
-										// For each {refIdtoStat} record which maps refId to a statistic
-										// we add that to the stat property of the new
-										// RefIdTransformerOptions interface which includes multiple settings
-										for refId, stat := range refIdToStat {
-											newSettings := map[string]interface{}{
-												"stat": stat,
-											}
-											tableTransformOptions[refId] = newSettings
-										}
-										// Update the options
-										t["options"] = tableTransformOptions
-									}
-								}
-							}
-						}
-					}
-				}
+func V39(dashboard map[string]interface{}) error {
+	dashboard["schemaVersion"] = int(39)
+
+	panels, ok := dashboard["panels"].([]interface{})
+	if !ok {
+		return nil
+	}
+
+	for _, panel := range panels {
+		p, ok := panel.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		transformations, ok := p["transformations"].([]interface{})
+		if !ok {
+			continue
+		}
+
+		for _, transformation := range transformations {
+			t, ok := transformation.(map[string]interface{})
+			if !ok {
+				continue
 			}
+
+			// If we run into a timeSeriesTable transformation
+			// and it doesn't have undefined options then we migrate
+			if t["id"] != "timeSeriesTable" {
+				continue
+			}
+
+			options, ok := t["options"].(map[string]interface{})
+			if !ok {
+				continue
+			}
+
+			refIdStats, ok := options["refIdToStat"].(map[string]interface{})
+			if !ok {
+				continue
+			}
+
+			// For each {refIdtoStat} record which maps refId to a statistic
+			// we add that to the stat property of the new
+			// RefIdTransformerOptions interface which includes multiple settings
+			transformationOptions := make(map[string]interface{})
+			for refId, stat := range refIdStats {
+				transformationOptions[refId] = map[string]interface{}{"stat": stat}
+			}
+
+			// Update the options
+			t["options"] = transformationOptions
 		}
 	}
 
