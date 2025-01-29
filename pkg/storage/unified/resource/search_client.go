18a6bc5e6ed0dd04a0d31b76e6bc359dea2494e3
Maicon Costa <maiconscosta@gmail.com>
2025-01-28 23:46:28 -0300

    Add explanation on why we are disabling search fallback
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/storage/unified/resource/search_client.go
+++ pkg/storage/unified/resource/search_client.go
@@ -4,6 +4,9 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 )
 
+// Search Fallback was returning both Folders and Dashboards which resulted
+// in issues with rendering the Folder UI. Also, filters are not implemented
+// yet. For those reasons, we will be disabling Search Fallback for now
 func NewSearchClient(cfg *setting.Cfg, unifiedStorageConfigKey string, unifiedClient ResourceIndexClient, legacyClient ResourceIndexClient) ResourceIndexClient {
 	// config, ok := cfg.UnifiedStorage[unifiedStorageConfigKey]
 	// if !ok {
