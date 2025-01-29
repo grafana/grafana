40b20fc607c486952851da8553399a9ece66a446
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-01-28 19:11:19 -0700

    fix linter

--- pkg/storage/unified/resource/search_client.go
+++ pkg/storage/unified/resource/search_client.go
@@ -1,17 +1,16 @@
 package resource
 
 import (
-	"github.com/grafana/grafana/pkg/apiserver/rest"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 func NewSearchClient(cfg *setting.Cfg, unifiedStorageConfigKey string, unifiedClient ResourceIndexClient, legacyClient ResourceIndexClient) ResourceIndexClient {
-	config, ok := cfg.UnifiedStorage[unifiedStorageConfigKey]
+	/*config, ok := cfg.UnifiedStorage[unifiedStorageConfigKey]
 	if !ok {
 		return legacyClient
 	}
 
-	/*switch config.DualWriterMode {
+	switch config.DualWriterMode {
 	case rest.Mode0, rest.Mode1, rest.Mode2:
 		return legacyClient
 	default:
