661043a90dd3c06792010707ef1102a29c477eca
Scott Lepper <scott.lepper@gmail.com>
2025-01-29 17:31:59 -0500

    [search] title phrase (#99753)
    [search] rename title_sort to title_phrase

--- pkg/services/folder/folderimpl/folder_unifiedstorage.go
+++ pkg/services/folder/folderimpl/folder_unifiedstorage.go
@@ -240,9 +240,9 @@ func (s *Service) getFolderByTitleFromApiServer(ctx context.Context, orgID int64
 			Key: folderkey,
 			Fields: []*resource.Requirement{
 				{
-					Key:      resource.SEARCH_FIELD_TITLE_SORT,
+					Key:      resource.SEARCH_FIELD_TITLE_PHRASE,
 					Operator: string(selection.In),
-					Values:   []string{strings.ToLower(title)},
+					Values:   []string{title},
 				},
 			},
 			Labels: []*resource.Requirement{},
