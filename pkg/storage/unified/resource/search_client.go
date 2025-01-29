aae9b02df1b7db203fc1de6273a777349ecb43f6
Maicon Costa <maiconscosta@gmail.com>
2025-01-28 23:19:14 -0300

    Ignore search fallback for now
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/storage/unified/resource/search_client.go
+++ pkg/storage/unified/resource/search_client.go
@@ -1,20 +1,21 @@
 package resource
 
 import (
-	"github.com/grafana/grafana/pkg/apiserver/rest"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 func NewSearchClient(cfg *setting.Cfg, unifiedStorageConfigKey string, unifiedClient ResourceIndexClient, legacyClient ResourceIndexClient) ResourceIndexClient {
-	config, ok := cfg.UnifiedStorage[unifiedStorageConfigKey]
-	if !ok {
-		return legacyClient
-	}
+	// config, ok := cfg.UnifiedStorage[unifiedStorageConfigKey]
+	// if !ok {
+	// 	return legacyClient
+	// }
 
-	switch config.DualWriterMode {
-	case rest.Mode0, rest.Mode1, rest.Mode2:
-		return legacyClient
-	default:
-		return unifiedClient
-	}
+	// switch config.DualWriterMode {
+	// case rest.Mode0, rest.Mode1, rest.Mode2:
+	// 	return legacyClient
+	// default:
+	// 	return unifiedClient
+	// }
+
+	return unifiedClient
 }
