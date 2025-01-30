328ca2070ac9c179a48fef73ee1d1988f414df4d
Clarity-89 <homes89@ukr.net>
2025-01-30 11:02:29 +0200

    RTK APIs: Extract base query function

--- public/app/features/dashboard/api/publicDashboardApi.ts
+++ public/app/features/dashboard/api/publicDashboardApi.ts
@@ -1,7 +1,6 @@
-import { BaseQueryFn, createApi } from '@reduxjs/toolkit/query/react';
-import { lastValueFrom } from 'rxjs';
+import { createApi } from '@reduxjs/toolkit/query/react';
 
-import { BackendSrvRequest, config, FetchError, getBackendSrv, isFetchError } from '@grafana/runtime/src';
+import { config, FetchError, isFetchError } from '@grafana/runtime/src';
 import { notifyApp } from 'app/core/actions';
 import { createErrorNotification, createSuccessNotification } from 'app/core/copy/appNotification';
 import { t } from 'app/core/internationalization';
@@ -19,31 +18,11 @@ import {
   PublicDashboardListWithPaginationResponse,
 } from 'app/features/manage-dashboards/types';
 
-type ReqOptions = {
-  manageError?: (err: unknown) => { error: unknown };
-  showErrorAlert?: boolean;
-};
+import { createBaseQuery } from '../../../api/createBaseQuery';
 
 function isFetchBaseQueryError(error: unknown): error is { error: FetchError } {
   return typeof error === 'object' && error != null && 'error' in error;
 }
-
-const backendSrvBaseQuery =
-  ({ baseUrl }: { baseUrl: string }): BaseQueryFn<BackendSrvRequest & ReqOptions> =>
-  async (requestOptions) => {
-    try {
-      const { data: responseData, ...meta } = await lastValueFrom(
-        getBackendSrv().fetch({
-          ...requestOptions,
-          url: baseUrl + requestOptions.url,
-          showErrorAlert: requestOptions.showErrorAlert,
-        })
-      );
-      return { data: responseData, meta };
-    } catch (error) {
-      return requestOptions.manageError ? requestOptions.manageError(error) : { error };
-    }
-  };
 
 const getConfigError = (err: unknown) => ({
   error: isFetchError(err) && err.data.messageId !== 'publicdashboards.notFound' ? err : null,
@@ -51,7 +30,7 @@ const getConfigError = (err: unknown) => ({
 
 export const publicDashboardApi = createApi({
   reducerPath: 'publicDashboardApi',
-  baseQuery: backendSrvBaseQuery({ baseUrl: '/api' }),
+  baseQuery: createBaseQuery({ baseURL: '/api' }),
   tagTypes: ['PublicDashboard', 'AuditTablePublicDashboard', 'UsersWithActiveSessions', 'ActiveUserDashboards'],
   refetchOnMountOrArgChange: true,
   endpoints: (builder) => ({
--- public/app/features/query-library/api/query.ts
+++ public/app/features/query-library/api/query.ts
@@ -1,8 +1,3 @@
-import { BaseQueryFn } from '@reduxjs/toolkit/query/react';
-import { lastValueFrom } from 'rxjs';
-
-import { BackendSrvRequest, getBackendSrv, isFetchError } from '@grafana/runtime/src/services/backendSrv';
-
 import { getAPINamespace } from '../../../api/utils';
 
 /**
@@ -23,29 +18,3 @@ export enum QueryTemplateKinds {
  * @alpha
  */
 export const BASE_URL = `/apis/${API_VERSION}/namespaces/${getAPINamespace()}`;
-
-interface QueryLibraryBackendRequest extends BackendSrvRequest {
-  body?: BackendSrvRequest['data'];
-}
-
-export const baseQuery: BaseQueryFn<QueryLibraryBackendRequest, unknown, Error> = async (requestOptions) => {
-  try {
-    const responseObservable = getBackendSrv().fetch({
-      url: `${BASE_URL}/${requestOptions.url ?? ''}`,
-      showErrorAlert: true,
-      method: requestOptions.method || 'GET',
-      data: requestOptions.body,
-      params: requestOptions.params,
-      headers: { ...requestOptions.headers },
-    });
-    return await lastValueFrom(responseObservable);
-  } catch (error) {
-    if (isFetchError(error)) {
-      return { error: new Error(error.data.message) };
-    } else if (error instanceof Error) {
-      return { error };
-    } else {
-      return { error: new Error('Unknown error') };
-    }
-  }
-};
