d94ce2e6598b35f575c7114eeed1ba2d2542994a
Alex Khomenko <Clarity-89@users.noreply.github.com>
2025-01-31 14:25:16 +0200

    RTK APIs: Extract base query logic (#99800)
    * RTK APIs: Extract base query function
    
    * Add error handling
    
    * Add return type
    
    * Use createBaseQuery in browseDashboards
    
    * Support custom manageError
    
    * Export getConfigError
    
    * Remove redundant type
    
    * data -> body

--- public/app/features/query-library/api/factory.ts
+++ public/app/features/query-library/api/factory.ts
@@ -1,12 +1,14 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 
-import { baseQuery } from './query';
+import { createBaseQuery } from '../../../api/createBaseQuery';
+
+import { BASE_URL } from './query';
 
 // Currently, we are loading all query templates
 // Organizations can have maximum of 1000 query templates
 export const QUERY_LIBRARY_GET_LIMIT = 1000;
 
 export const queryLibraryApi = createApi({
-  baseQuery,
+  baseQuery: createBaseQuery({ baseURL: BASE_URL }),
   endpoints: () => ({}),
 });
