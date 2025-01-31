60a384bb10adf0fb4eeabf48e3866a13f0a9e8fd
André Pereira <adrapereira@gmail.com>
2025-01-31 15:12:26 +0000

    Fix tests

--- pkg/tsdb/tempo/traceql/metrics_test.go
+++ pkg/tsdb/tempo/traceql/metrics_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"github.com/grafana/grafana/pkg/tsdb/tempo/kinds/dataquery"
 	"github.com/grafana/tempo/pkg/tempopb"
 	v1 "github.com/grafana/tempo/pkg/tempopb/common/v1"
 	"github.com/stretchr/testify/assert"
@@ -42,9 +43,6 @@ func TestTransformMetricsResponse_SingleSeriesSingleLabel(t *testing.T) {
 }
 
 func TestTransformMetricsResponse_SingleSeriesMultipleLabels(t *testing.T) {
-	// Skipping for now because this test is broken.
-	t.Skip()
-
 	resp := tempopb.QueryRangeResponse{
 		Series: []*tempopb.TimeSeries{
 			{
