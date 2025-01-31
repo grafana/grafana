1983a37f3e5d47af80825055979dd54f9bb6a3a0
Alex Spencer <alex.spencer@grafana.com>
2025-01-31 12:53:52 -0700

    chore: update tests to show more capabilities, remove GHA

--- e2e/visual-regression/visualRegressionTesting.test.ts
+++ e2e/visual-regression/visualRegressionTesting.test.ts
@@ -6,5 +6,24 @@ test('BarGauge', { tag: '@viz_regression' }, async ({ page }) => {
   const barGaugeLocation = 'd/O6f11TZWk/panel-tests-bar-gauge?orgId=1&from=now-6h&to=now&timezone=Europe%2FLondon';
   await page.goto(`${baseURL}/${barGaugeLocation}`);
 
-  await expect(page).toHaveScreenshot('bargauge.png', { fullPage: true, animations: 'disabled' });
+  const ledBarGauge = await page.locator('.react-grid-item').nth(6).boundingBox();
+
+  await expect(page).toHaveScreenshot('bargauge.png', { animations: 'disabled', clip: ledBarGauge, fullPage: true });
+});
+
+test('TimeSeriesStacking', { tag: '@viz_regression' }, async ({ page }) => {
+  const timeSeriesStackingLocation =
+    'd/Ei86FP_Mx/panel-tests-timeseries-stacking?orgId=1&from=now-6h&to=now&timezone=Europe%2FLondon';
+  await page.goto(`${baseURL}/${timeSeriesStackingLocation}`);
+
+  const stackedTimeSeries = await page.locator('.react-grid-item').first().boundingBox();
+  const yAxis = await page.locator('.u-axis').nth(0);
+  const xAxis = await page.locator('.u-axis').nth(1);
+
+  await expect(page).toHaveScreenshot('timeseriesstacking.png', {
+    animations: 'disabled',
+    clip: stackedTimeSeries,
+    fullPage: true,
+    mask: [yAxis, xAxis],
+  });
 });
