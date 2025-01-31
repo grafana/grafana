bb294fe519bc75ddfe63fe6168cb10e124acee53
Ashley Harrison <ashley.harrison@grafana.com>
2025-01-31 12:55:35 +0000

    use new createBaseQuery method

--- public/app/features/iam/api/api.ts
+++ public/app/features/iam/api/api.ts
@@ -1,9 +1,14 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 
-import { baseQuery } from './query';
+import { createBaseQuery } from '../../../api/createBaseQuery';
+import { getAPINamespace } from '../../../api/utils';
+
+export const API_VERSION = 'iam.grafana.app/v0alpha1';
+
+export const BASE_URL = `/apis/${API_VERSION}/namespaces/${getAPINamespace()}`;
 
 export const iamApi = createApi({
-  baseQuery,
+  baseQuery: createBaseQuery({ baseURL: BASE_URL }),
   reducerPath: 'iamAPI',
   endpoints: () => ({}),
 });
