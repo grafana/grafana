fc2b315094c4a147b3466454f9065bf50a37bc49
Dan Snyder <dan.snyder.3rd@gmail.com>
2025-01-31 11:30:36 -0500

    FlameGraph: Added decimals, and cleaned up module plugin configuration

--- public/app/plugins/panel/flamegraph/module.tsx
+++ public/app/plugins/panel/flamegraph/module.tsx
@@ -3,11 +3,10 @@ import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
 import { FlameGraphPanel } from './FlameGraphPanel';
 import { FlameGraphSuggestionsSupplier } from './suggestions';
 
+const flamegraphConfigOptions = [FieldConfigProperty.Unit, FieldConfigProperty.Decimals]
+
 export const plugin = new PanelPlugin(FlameGraphPanel)
   .setSuggestionsSupplier(new FlameGraphSuggestionsSupplier())
   .useFieldConfig({
-    disableStandardOptions: Object.values(FieldConfigProperty).filter((v) => v !== FieldConfigProperty.Unit),
-    standardOptions: {
-      [FieldConfigProperty.Unit]: {},
-    },
+    disableStandardOptions: Object.values(FieldConfigProperty).filter((v) => !flamegraphConfigOptions.includes(v)),
   });
