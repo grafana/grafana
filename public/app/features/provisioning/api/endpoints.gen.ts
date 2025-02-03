2d410b79de228ecd640790c1f5e28428aeb9e0ad
Ryan McKinley <ryantxu@gmail.com>
2025-02-03 17:54:53 +0300

    frontend settings as list

--- public/app/features/provisioning/api/endpoints.gen.ts
+++ public/app/features/provisioning/api/endpoints.gen.ts
@@ -1038,11 +1038,32 @@ export type WebhookResponse = {
   /** Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds */
   kind?: string;
 };
+export type RepositoryView = {
+  /** The k8s name for this repository */
+  name: string;
+  /** Edit options within the repository */
+  readOnly: boolean;
+  /** When syncing, where values are saved
+    
+    Possible enum values:
+     - `"folder"` Resources will be saved into a folder managed by this repository The folder k8s name will be the same as the repository k8s name It will contain a copy of everything from the remote
+     - `"instance"` Resources are saved in the global context Only one repository may specify the `instance` target When this exists, the UI will promote writing to the instance repo rather than the grafana database (where possible) */
+  target: 'folder' | 'instance';
+  /** Repository display */
+  title: string;
+  /** The repository type
+    
+    Possible enum values:
+     - `"github"`
+     - `"local"`
+     - `"s3"` */
+  type: 'github' | 'local' | 's3';
+};
 export type RepositoryViewList = {
   /** APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources */
   apiVersion?: string;
   /** The basic repository settings */
-  items: any[];
+  items: RepositoryView[];
   /** Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds */
   kind?: string;
   metadata?: any;
