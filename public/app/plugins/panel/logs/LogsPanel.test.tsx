79b16d4db544debac9ec2bd4382c5e4e8aefccbb
Matias Chomicki <matyax@gmail.com>
2025-02-03 15:47:26 +0000

    LogsPanel: do not re-render while data is loading (#99882)

--- public/app/plugins/panel/logs/LogsPanel.test.tsx
+++ public/app/plugins/panel/logs/LogsPanel.test.tsx
@@ -518,6 +518,22 @@ describe('LogsPanel', () => {
       expect(await screen.findByRole('row')).toBeInTheDocument();
       expect(jest.mocked(styles.getLogRowStyles).mock.calls.length).toBeGreaterThan(3);
     });
+
+    it('does not re-render when data is loading', async () => {
+      const { rerender, props } = setup({
+        data: {
+          ...defaultProps.data,
+          series,
+        },
+      });
+
+      expect(await screen.findByRole('row')).toBeInTheDocument();
+
+      rerender(<LogsPanel {...props} data={{ ...props.data, state: LoadingState.Loading }} />);
+
+      expect(await screen.findByRole('row')).toBeInTheDocument();
+      expect(styles.getLogRowStyles).toHaveBeenCalledTimes(3);
+    });
   });
 
   describe('Filters', () => {
