c36572747e9f903512a498473429dc9b532cc4a7
Ashley Harrison <ashley.harrison@grafana.com>
2025-02-04 17:49:44 +0000

    default to dark theme

--- packages/grafana-ui/.storybook/manager.ts
+++ packages/grafana-ui/.storybook/manager.ts
@@ -2,7 +2,7 @@ import { addons } from '@storybook/manager-api';
 import { getThemeById } from '@grafana/data';
 import { createStorybookTheme } from './storybookTheme';
 
-const darkTheme = getThemeById('debug');
+const darkTheme = getThemeById('dark');
 addons.setConfig({
   isFullscreen: false,
   panelPosition: 'right',
