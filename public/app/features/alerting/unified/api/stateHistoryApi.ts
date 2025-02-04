ac302f20e8144c01629d4216c6fa5a50ffb22612
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2025-02-04 09:23:40 +0100

    Use BE filter for labels in ash page

--- public/app/features/alerting/unified/api/stateHistoryApi.ts
+++ public/app/features/alerting/unified/api/stateHistoryApi.ts
@@ -2,12 +2,38 @@ import { DataFrameJSON } from '@grafana/data';
 
 import { alertingApi } from './alertingApi';
 
+export const LABELS_PREFIX = 'labels_';
+
+export function labelsDtoFromLabelsAsString(labels?: string): Record<string, string> {
+  // convert "{ke1=val1,key2=val2}" to { labels_key1: val1, labels_key2: val2 }
+  if (!labels) {
+    return {};
+  }
+  const result = labels
+    .replace(/[{()}]/g, '')
+    .split(',')
+    .reduce(
+      (acc, label) => {
+        const [key, value] = label.split('=');
+        acc[`${LABELS_PREFIX}${key.trim()}`] = value;
+        return acc;
+      },
+      // eslint-disable-next-line
+      {} as Record<string, string>
+    );
+
+  return result;
+}
+
 export const stateHistoryApi = alertingApi.injectEndpoints({
   endpoints: (build) => ({
-    getRuleHistory: build.query<DataFrameJSON, { ruleUid?: string; from?: number; to?: number; limit?: number }>({
-      query: ({ ruleUid, from, to, limit = 100 }) => ({
+    getRuleHistory: build.query<
+      DataFrameJSON,
+      { ruleUid?: string; from?: number; to?: number; limit?: number; labels?: string }
+    >({
+      query: ({ ruleUid, from, to, limit = 100, labels }) => ({
         url: '/api/v1/rules/history',
-        params: { ruleUID: ruleUid, from, to, limit },
+        params: { ruleUID: ruleUid, from, to, limit, ...labelsDtoFromLabelsAsString(labels) },
       }),
     }),
   }),
