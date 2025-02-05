298e5cbf42325542d51561fa60d32cf908a51c8c
Kyle Brandt <kyle@grafana.com>
2025-02-05 12:43:00 -0500

    redo parser_test.go existing tests as table

--- pkg/expr/sql/parser_test.go
+++ pkg/expr/sql/parser_test.go
@@ -3,175 +3,132 @@ package sql
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestParse(t *testing.T) {
-	sql := "select * from foo"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
+func TestTablesList(t *testing.T) {
+	tests := []struct {
+		name        string
+		sql         string
+		expected    []string
+		expectError bool
+	}{
+		{
+			name:     "simple select",
+			sql:      "select * from foo",
+			expected: []string{"foo"},
+		},
+		{
+			name:     "select with comma",
+			sql:      "select * from foo,bar",
+			expected: []string{"bar", "foo"},
+		},
+		{
+			name:     "select with multiple commas",
+			sql:      "select * from foo,bar,baz",
+			expected: []string{"bar", "baz", "foo"},
+		},
+		{
+			name:     "no table",
+			sql:      "select 1 as 'n'",
+			expected: []string{},
+		},
+		{
+			name:     "json array",
+			sql:      "SELECT JSON_ARRAY(1, 2, 3) AS array_value",
+			expected: []string{},
+		},
+		{
+			name:     "json extract",
+			sql:      "SELECT JSON_EXTRACT(JSON_ARRAY(1, 2, 3), '$[0]') AS first_element;",
+			expected: []string{},
+		},
+		{
+			name:     "json int array",
+			sql:      "SELECT JSON_ARRAY(3, 2, 1) AS int_array;",
+			expected: []string{},
+		},
+		{
+			name:     "subquery",
+			sql:      "select * from (select * from people limit 1) AS subquery",
+			expected: []string{"people"},
+		},
+		{
+			name: "join",
+			sql: `select * from A
+			JOIN B ON A.name = B.name
+			LIMIT 10`,
+			expected: []string{"A", "B"},
+		},
+		{
+			name: "right join",
+			sql: `select * from A
+			RIGHT JOIN B ON A.name = B.name
+			LIMIT 10`,
+			expected: []string{"A", "B"},
+		},
+		{
+			name: "alias with join",
+			sql: `select * from A as X
+			RIGHT JOIN B ON A.name = X.name
+			LIMIT 10`,
+			expected: []string{"A", "B"},
+		},
+		{
+			name:     "alias",
+			sql:      "select * from A as X LIMIT 10",
+			expected: []string{"A"},
+		},
+		{
+			name:        "error case",
+			sql:         "select * from zzz aaa zzz",
+			expectError: true,
+		},
+		{
+			name: "parens",
+			sql: `SELECT  t1.Col1,
+				t2.Col1,
+				t3.Col1
+			FROM    table1 AS t1
+			LEFT JOIN	(
+				table2 AS t2
+				INNER JOIN table3 AS t3 ON t3.Col1 = t2.Col1
+			) ON t2.Col1 = t1.Col1;`,
+			expected: []string{"table1", "table2", "table3"},
+		},
+		{
+			name: "with clause",
+			sql: `WITH top_products AS (
+				SELECT * FROM products
+				ORDER BY price DESC
+				LIMIT 5
+			)
+			SELECT name, price
+			FROM top_products;`,
+			expected: []string{"products", "top_products"},
+		},
+		{
+			name:     "with quote",
+			sql:      "select *,'junk' from foo",
+			expected: []string{"foo"},
+		},
+		{
+			name:     "with quote 2",
+			sql:      "SELECT json_serialize_sql('SELECT 1')",
+			expected: []string{},
+		},
+	}
 
-	assert.Equal(t, "foo", tables[0])
-}
-
-func TestParseWithComma(t *testing.T) {
-	sql := "select * from foo,bar"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, "bar", tables[0])
-	assert.Equal(t, "foo", tables[1])
-}
-
-func TestParseWithCommas(t *testing.T) {
-	sql := "select * from foo,bar,baz"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, "bar", tables[0])
-	assert.Equal(t, "baz", tables[1])
-	assert.Equal(t, "foo", tables[2])
-}
-
-func TestNoTable(t *testing.T) {
-	sql := "select 1 as 'n'"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 0, len(tables))
-}
-
-func TestArray(t *testing.T) {
-	sql := "SELECT JSON_ARRAY(1, 2, 3) AS array_value"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 0, len(tables))
-}
-
-func TestArray2(t *testing.T) {
-	sql := "SELECT JSON_EXTRACT(JSON_ARRAY(1, 2, 3), '$[0]') AS first_element;"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 0, len(tables))
-}
-
-func TestXxx(t *testing.T) {
-	sql := "SELECT JSON_ARRAY(3, 2, 1) AS int_array;"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 0, len(tables))
-}
-
-func TestParseSubquery(t *testing.T) {
-	sql := "select * from (select * from people limit 1) AS subquery"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 1, len(tables))
-	assert.Equal(t, "people", tables[0])
-}
-
-func TestJoin(t *testing.T) {
-	sql := `select * from A
-	JOIN B ON A.name = B.name
-	LIMIT 10`
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 2, len(tables))
-	assert.Equal(t, "A", tables[0])
-	assert.Equal(t, "B", tables[1])
-}
-
-func TestRightJoin(t *testing.T) {
-	sql := `select * from A
-	RIGHT JOIN B ON A.name = B.name
-	LIMIT 10`
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 2, len(tables))
-	assert.Equal(t, "A", tables[0])
-	assert.Equal(t, "B", tables[1])
-}
-
-func TestAliasWithJoin(t *testing.T) {
-	sql := `select * from A as X
-	RIGHT JOIN B ON A.name = X.name
-	LIMIT 10`
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 2, len(tables))
-	assert.Equal(t, "A", tables[0])
-	assert.Equal(t, "B", tables[1])
-}
-
-func TestAlias(t *testing.T) {
-	sql := `select * from A as X LIMIT 10`
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 1, len(tables))
-	assert.Equal(t, "A", tables[0])
-}
-
-func TestError(t *testing.T) {
-	sql := `select * from zzz aaa zzz`
-	_, err := TablesList((sql))
-	assert.NotNil(t, err)
-}
-
-func TestParens(t *testing.T) {
-	sql := `SELECT  t1.Col1,
-	t2.Col1,
-	t3.Col1
-	FROM    table1 AS t1
-	LEFT JOIN	(
-	table2 AS t2
-	INNER JOIN table3 AS t3 ON t3.Col1 = t2.Col1
-	) ON t2.Col1 = t1.Col1;`
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 3, len(tables))
-	assert.Equal(t, "table1", tables[0])
-	assert.Equal(t, "table2", tables[1])
-	assert.Equal(t, "table3", tables[2])
-}
-
-func TestWith(t *testing.T) {
-	sql := `WITH top_products AS (
-		SELECT * FROM products
-		ORDER BY price DESC
-		LIMIT 5
-	)
-	SELECT name, price
-	FROM top_products;`
-
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 2, len(tables))
-	assert.Equal(t, "products", tables[0])
-	assert.Equal(t, "top_products", tables[1])
-}
-
-func TestWithQuote(t *testing.T) {
-	sql := "select *,'junk' from foo"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, "foo", tables[0])
-}
-
-func TestWithQuote2(t *testing.T) {
-	sql := "SELECT json_serialize_sql('SELECT 1')"
-	tables, err := TablesList((sql))
-	assert.Nil(t, err)
-
-	assert.Equal(t, 0, len(tables))
+	for _, tc := range tests {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			tables, err := TablesList(tc.sql)
+			if tc.expectError {
+				require.NotNil(t, err, "expected error for SQL: %s", tc.sql)
+			} else {
+				require.Nil(t, err, "unexpected error for SQL: %s", tc.sql)
+				require.Equal(t, tc.expected, tables, "mismatched tables for SQL: %s", tc.sql)
+			}
+		})
+	}
 }
