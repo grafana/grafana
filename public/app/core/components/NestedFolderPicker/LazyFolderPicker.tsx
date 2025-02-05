d4ddb478b41128065e6c178703f405e94c2381ce
joshhunt <josh@trtr.co>
2025-02-05 10:57:30 +0000

    comment

--- public/app/core/components/NestedFolderPicker/LazyFolderPicker.tsx
+++ public/app/core/components/NestedFolderPicker/LazyFolderPicker.tsx
@@ -6,6 +6,7 @@ const SuspendingNestedFolderPicker = lazy(() =>
   import('./NestedFolderPicker').then((module) => ({ default: module.NestedFolderPicker }))
 );
 
+// Lazily load folder picker, is what is exposed to plugins through @grafana/runtime
 export const LazyFolderPicker = (props: Parameters<typeof SuspendingNestedFolderPicker>[0]) => {
   return (
     <Suspense fallback={<FolderPickerSkeleton />}>
