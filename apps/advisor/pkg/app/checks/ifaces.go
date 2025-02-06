5fae6e535dd196fcf480c8c1d5a0c0a0d4be01d3
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-02-06 09:55:17 +0100

    Advisor: Refactor check interfaces (#100043)

--- apps/advisor/pkg/app/checks/ifaces.go
+++ apps/advisor/pkg/app/checks/ifaces.go
@@ -6,8 +6,24 @@ import (
 	advisorv0alpha1 "github.com/grafana/grafana/apps/advisor/pkg/apis/advisor/v0alpha1"
 )
 
-// Check defines the methods that a check must implement to be executed.
+// Check returns metadata about the check being executed and the list of Steps
 type Check interface {
-	Run(ctx context.Context, obj *advisorv0alpha1.CheckSpec) (*advisorv0alpha1.CheckV0alpha1StatusReport, error)
-	Type() string
+	// ID returns the unique identifier of the check
+	ID() string
+	// Items returns the list of items that will be checked
+	Items(ctx context.Context) ([]any, error)
+	// Steps returns the list of steps that will be executed
+	Steps() []Step
+}
+
+// Step is a single step in a check, including its metadata
+type Step interface {
+	// ID returns the unique identifier of the step
+	ID() string
+	// Title returns the title of the step
+	Title() string
+	// Description returns the description of the step
+	Description() string
+	// Run executes the step and returns a list of errors
+	Run(ctx context.Context, obj *advisorv0alpha1.CheckSpec, items []any) ([]advisorv0alpha1.CheckReportError, error)
 }
