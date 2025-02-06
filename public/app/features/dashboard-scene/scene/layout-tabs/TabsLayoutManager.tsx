3210093dc3733c5d1789dfb4e5aab7d07abf0f93
Bogdan Matei <bogdan.matei@grafana.com>
2025-02-06 10:55:54 +0200

    Remove repeats

--- public/app/features/dashboard-scene/scene/layout-tabs/TabsLayoutManager.tsx
+++ public/app/features/dashboard-scene/scene/layout-tabs/TabsLayoutManager.tsx
@@ -4,7 +4,6 @@ import { t } from 'app/core/internationalization';
 import { DashboardLayoutManager } from '../types/DashboardLayoutManager';
 
 import { TabItem } from './TabItem';
-import { TabItemRepeaterBehavior } from './TabItemRepeaterBehavior';
 import { TabsLayoutManagerRenderer } from './TabsLayoutManagerRenderer';
 
 interface TabsLayoutManagerState extends SceneObjectState {
