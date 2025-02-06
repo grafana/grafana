704b4dd53522a3c00cfda55033af8105cb22390e
Torkel Ödegaard <torkel@grafana.com>
2025-02-06 19:34:09 +0100

    refactor

--- packages/grafana-data/src/themes/themeDefinitions/gloom.ts
+++ packages/grafana-data/src/themes/themeDefinitions/gloom.ts
@@ -4,61 +4,69 @@ import { NewThemeOptions } from '../createTheme';
  * Torkel's GrafanaCon theme
  * very WIP state
  */
+
+const whiteBase = `210, 210, 220`;
+const brandMain = '#ff934d';
+const brandText = '#f99a5c';
+
+const disabledText = `rgba(${whiteBase}, 0.6)`;
+
 const gloomTheme: NewThemeOptions = {
   name: 'Gloom',
   colors: {
     mode: 'dark',
     border: {
-      weak: 'rgba(200, 200, 180, 0.12)',
-      medium: 'rgba(200, 200, 180, 0.20)',
-      strong: 'rgba(200, 200, 180, 0.30)',
+      weak: `rgba(${whiteBase}, 0.08)`,
+      medium: `rgba(${whiteBase}, 0.12)`,
+      strong: `rgba(${whiteBase}, 0.30)`,
     },
+
     text: {
-      primary: 'rgb(210, 210, 220)',
-      secondary: 'rgba(200, 200, 180, 0.85)',
-      disabled: 'rgba(200, 200, 180, 0.6)',
-      link: '#ff934d',
-      maxContrast: '#FFFFFF',
+      primary: `rgb(${whiteBase})`,
+      secondary: `rgba(${whiteBase}, 0.65)`,
+      disabled: disabledText,
+      link: brandText,
+      maxContrast: '#FFF',
     },
+
     primary: {
-      main: '#ff934d',
-      text: '#ff934d',
-      border: '#ff934d',
+      main: brandMain,
+      text: brandText,
+      border: brandMain,
       name: 'primary',
-      // shade: 'rgb(255, 173, 80)',
-      // transparent: '#FEAC3426',
-      //  contrastText: '#111614',
-      //  borderTransparent: '#FFD78340',
     },
+
     secondary: {
-      main: 'rgba(200, 200, 180, 0.10)',
-      shade: 'rgba(200, 200, 180, 0.14)',
-      transparent: 'rgba(200, 200, 180, 0.08)',
-      text: 'rgb(200, 200, 180)',
-      contrastText: 'rgb(200, 200, 180)',
-      border: 'rgba(200, 200, 180, 0.08)',
-      name: 'secondary',
-      borderTransparent: 'rgba(200, 200, 180, 0.25)',
+      main: `rgba(${whiteBase}, 0.10)`,
+      shade: `rgba(${whiteBase}, 0.14)`,
+      transparent: `rgba(${whiteBase}, 0.08)`,
+      text: `rgba(${whiteBase})`,
+      contrastText: `rgb(${whiteBase})`,
+      border: `rgba(${whiteBase}, 0.08)`,
     },
+
     background: {
       canvas: '#000',
       primary: '#111',
       secondary: '#222',
     },
+
     action: {
-      hover: 'rgba(200, 200, 180, 0.10)',
-      selected: 'rgba(200, 200, 180, 0.12)',
-      selectedBorder: '#ff934d',
-      focus: 'rgba(200, 200, 180, 0.10)',
+      hover: `rgba(${whiteBase}, 0.16)`,
+      selected: `rgba(${whiteBase}, 0.12)`,
+      selectedBorder: brandMain,
+      focus: `rgba(${whiteBase}, 0.16)`,
       hoverOpacity: 0.08,
-      disabledText: 'rgba(200, 200, 180, 0.6)',
-      disabledBackground: 'rgba(200, 200, 180, 0.04)',
+      disabledText: disabledText,
+      disabledBackground: `rgba(${whiteBase}, 0.04)`,
       disabledOpacity: 0.38,
     },
+
     gradients: {
       brandHorizontal: 'linear-gradient(270deg, #ff934d 0%, #FEAC34 100%)',
       brandVertical: 'linear-gradient(0.01deg, #ff934d 0.01%, #FEAC34 99.99%)',
     },
+
     contrastThreshold: 3,
     hoverFactor: 0.03,
     tonalOffset: 0.15,
