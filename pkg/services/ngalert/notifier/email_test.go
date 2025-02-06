12c032804e4b2dfb91edf14632b19cc4d7bf0d4a
Matt Jacobson <matthew.jacobson@grafana.com>
2025-02-06 09:20:54 -0500

    Fix test by keeping SendEmailCommand.EmbeddedContents nil when empty

--- pkg/services/ngalert/notifier/email_test.go
+++ pkg/services/ngalert/notifier/email_test.go
@@ -222,24 +222,26 @@ func (e emailSender) SendWebhook(ctx context.Context, cmd *receivers.SendWebhook
 }
 
 func (e emailSender) SendEmail(ctx context.Context, cmd *receivers.SendEmailSettings) error {
-	embeddedContents := make([]notifications.EmbeddedContent, len(cmd.EmbeddedContents))
-	for i, ec := range cmd.EmbeddedContents {
-		embeddedContents[i] = notifications.EmbeddedContent{
-			Name:    ec.Name,
-			Content: ec.Content,
+	sendEmailCommand := notifications.SendEmailCommand{
+		To:            cmd.To,
+		SingleEmail:   cmd.SingleEmail,
+		Template:      cmd.Template,
+		Subject:       cmd.Subject,
+		Data:          cmd.Data,
+		ReplyTo:       cmd.ReplyTo,
+		EmbeddedFiles: cmd.EmbeddedFiles,
+	}
+	if len(cmd.EmbeddedContents) > 0 {
+		sendEmailCommand.EmbeddedContents = make([]notifications.EmbeddedContent, len(cmd.EmbeddedContents))
+		for i, ec := range cmd.EmbeddedContents {
+			sendEmailCommand.EmbeddedContents[i] = notifications.EmbeddedContent{
+				Name:    ec.Name,
+				Content: ec.Content,
+			}
 		}
 	}
 	return e.ns.SendEmailCommandHandlerSync(ctx, &notifications.SendEmailCommandSync{
-		SendEmailCommand: notifications.SendEmailCommand{
-			To:               cmd.To,
-			SingleEmail:      cmd.SingleEmail,
-			Template:         cmd.Template,
-			Subject:          cmd.Subject,
-			Data:             cmd.Data,
-			ReplyTo:          cmd.ReplyTo,
-			EmbeddedFiles:    cmd.EmbeddedFiles,
-			EmbeddedContents: embeddedContents,
-		},
+		SendEmailCommand: sendEmailCommand,
 	})
 }
 
