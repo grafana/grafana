6d6f4c0debb4d4cae2d6fe7d5525f06c37e374c4
Sam Jewell <sam.jewell@grafana.com>
2025-02-06 10:41:44 +0000

    Add comment for Arm builds linking to Dolt issue

--- pkg/expr/sql/dummy_arm.go
+++ pkg/expr/sql/dummy_arm.go
@@ -11,6 +11,8 @@ import (
 
 type DB struct{}
 
+// Stub out the QueryFrames method for ARM builds
+// See github.com/dolthub/go-mysql-server/issues/2837
 func (db *DB) QueryFrames(_ context.Context, _, _ string, _ []*data.Frame) (*data.Frame, error) {
 	return nil, fmt.Errorf("sql expressions not supported in arm")
 }
