d132335572c9ef0c6e95b1afb953475d766b0653
Bogdan Matei <bogdan.matei@grafana.com>
2025-02-07 16:44:55 +0200

    Dashboard: Fix for overwriting an edited dashboard in the old architecture (#100247)

--- public/app/features/dashboard/components/SaveDashboard/useDashboardSave.tsx
+++ public/app/features/dashboard/components/SaveDashboard/useDashboardSave.tsx
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash';
 import { useAsyncFn } from 'react-use';
 
 import { locationUtil } from '@grafana/data';
@@ -46,6 +47,8 @@ export const useDashboardSave = (isCopy = false) => {
         const result = await saveDashboard(clone, options, dashboard, saveDashboardRtkQuery);
         dashboard.version = result.version;
 
+        // Altering the clone leads to an error due to the clone being immutable
+        clone = cloneDeep(clone);
         clone.version = result.version;
         dashboard.clearUnsavedChanges(clone, options);
 
