a8956e1c893cb9ee1c7047bef52ea042245762c0
Mariell Hoversholm <mariell.hoversholm@grafana.com>
2025-02-07 10:57:20 +0100

    Provisioning: Support incremental syncing

--- pkg/apis/provisioning/v0alpha1/types.go
+++ pkg/apis/provisioning/v0alpha1/types.go
@@ -185,6 +185,9 @@ type SyncStatus struct {
 
 	// The repository hash when the last sync ran
 	Hash string `json:"hash,omitempty"`
+
+	// Incremental synchronization for versioned repositories
+	Incremental bool `json:"incremental,omitempty"`
 }
 
 type WebhookStatus struct {
