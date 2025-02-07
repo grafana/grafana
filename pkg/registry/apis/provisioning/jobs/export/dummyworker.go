3943b8a2c31a78adcd183ded98788c7f5afb7de3
Roberto Jiménez Sánchez <roberto.jimenez@grafana.com>
2025-02-07 13:33:03 +0100

    [Provisioning] Extract export into separate package (#100268)
    * Move export files into separate package
    
    * Use worker terminoligy
    
    * Rename dummy export worker

--- pkg/registry/apis/provisioning/jobs/export/dummyworker.go
+++ pkg/registry/apis/provisioning/jobs/export/dummyworker.go
@@ -1,4 +1,4 @@
-package jobs
+package export
 
 import (
 	"context"
@@ -12,9 +12,13 @@ import (
 	"github.com/grafana/grafana/pkg/util"
 )
 
-type dummyExporter struct{}
+type dummyExportWorker struct{}
 
-func (s *dummyExporter) Export(ctx context.Context,
+func NewDummyExportWorker() ExportWorker {
+	return &dummyExportWorker{}
+}
+
+func (s *dummyExportWorker) Export(ctx context.Context,
 	repo repository.Repository,
 	options provisioning.ExportJobOptions,
 	progress func(provisioning.JobStatus) error,
