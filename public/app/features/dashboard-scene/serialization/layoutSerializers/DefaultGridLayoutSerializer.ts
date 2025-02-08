dbcf3119b48f208a85f7c628e473f962db602aff
oscarkilhed <oscar.kilhed@grafana.com>
2025-02-08 16:05:16 +0100

    move default grid serializer functions outside of class

--- public/app/features/dashboard-scene/serialization/layoutSerializers/DefaultGridLayoutSerializer.ts
+++ public/app/features/dashboard-scene/serialization/layoutSerializers/DefaultGridLayoutSerializer.ts
@@ -43,7 +43,7 @@ export class DefaultGridLayoutManagerSerializer implements LayoutManagerSerializ
     return {
       kind: 'GridLayout',
       spec: {
-        items: this.getGridLayoutItems(layoutManager, isSnapshot),
+        items: getGridLayoutItems(layoutManager, isSnapshot),
       },
     };
   }
@@ -63,176 +63,176 @@ export class DefaultGridLayoutManagerSerializer implements LayoutManagerSerializ
       }),
     });
   }
+}
 
-  getGridLayoutItems(
-    body: DefaultGridLayoutManager,
-    isSnapshot?: boolean
-  ): Array<GridLayoutItemKind | GridLayoutRowKind> {
-    let items: Array<GridLayoutItemKind | GridLayoutRowKind> = [];
-    for (const child of body.state.grid.state.children) {
-      if (child instanceof DashboardGridItem) {
-        // TODO: handle panel repeater scenario
-        if (child.state.variableName) {
-          items = items.concat(this.repeaterToLayoutItems(child, isSnapshot));
-        } else {
-          items.push(this.gridItemToGridLayoutItemKind(child));
-        }
-      } else if (child instanceof SceneGridRow) {
-        if (isClonedKey(child.state.key!) && !isSnapshot) {
-          // Skip repeat rows
-          continue;
-        }
-        items.push(this.gridRowToLayoutRowKind(child, isSnapshot));
+function getGridLayoutItems(
+  body: DefaultGridLayoutManager,
+  isSnapshot?: boolean
+): Array<GridLayoutItemKind | GridLayoutRowKind> {
+  let items: Array<GridLayoutItemKind | GridLayoutRowKind> = [];
+  for (const child of body.state.grid.state.children) {
+    if (child instanceof DashboardGridItem) {
+      // TODO: handle panel repeater scenario
+      if (child.state.variableName) {
+        items = items.concat(repeaterToLayoutItems(child, isSnapshot));
+      } else {
+        items.push(gridItemToGridLayoutItemKind(child));
       }
+    } else if (child instanceof SceneGridRow) {
+      if (isClonedKey(child.state.key!) && !isSnapshot) {
+        // Skip repeat rows
+        continue;
+      }
+      items.push(gridRowToLayoutRowKind(child, isSnapshot));
     }
-
-    return items;
   }
 
-  getRowRepeat(row: SceneGridRow): RepeatOptions | undefined {
-    if (row.state.$behaviors) {
-      for (const behavior of row.state.$behaviors) {
-        if (behavior instanceof RowRepeaterBehavior) {
-          return { value: behavior.state.variableName, mode: 'variable' };
-        }
+  return items;
+}
+
+function getRowRepeat(row: SceneGridRow): RepeatOptions | undefined {
+  if (row.state.$behaviors) {
+    for (const behavior of row.state.$behaviors) {
+      if (behavior instanceof RowRepeaterBehavior) {
+        return { value: behavior.state.variableName, mode: 'variable' };
       }
     }
-    return undefined;
+  }
+  return undefined;
+}
+
+function gridRowToLayoutRowKind(row: SceneGridRow, isSnapshot = false): GridLayoutRowKind {
+  const children = row.state.children.map((child) => {
+    if (!(child instanceof DashboardGridItem)) {
+      throw new Error('Unsupported row child type');
+    }
+    const y = (child.state.y ?? 0) - (row.state.y ?? 0) - GRID_ROW_HEIGHT;
+    return gridItemToGridLayoutItemKind(child, y);
+  });
+
+  return {
+    kind: 'GridLayoutRow',
+    spec: {
+      title: row.state.title,
+      y: row.state.y ?? 0,
+      collapsed: Boolean(row.state.isCollapsed),
+      elements: children,
+      repeat: getRowRepeat(row),
+    },
+  };
+}
+
+function gridItemToGridLayoutItemKind(gridItem: DashboardGridItem, yOverride?: number): GridLayoutItemKind {
+  let elementGridItem: GridLayoutItemKind | undefined;
+  let x = 0,
+    y = 0,
+    width = 0,
+    height = 0;
+
+  let gridItem_ = gridItem;
+
+  if (!(gridItem_.state.body instanceof VizPanel)) {
+    throw new Error('DashboardGridItem body expected to be VizPanel');
   }
 
-  gridRowToLayoutRowKind(row: SceneGridRow, isSnapshot = false): GridLayoutRowKind {
-    const children = row.state.children.map((child) => {
-      if (!(child instanceof DashboardGridItem)) {
-        throw new Error('Unsupported row child type');
-      }
-      const y = (child.state.y ?? 0) - (row.state.y ?? 0) - GRID_ROW_HEIGHT;
-      return this.gridItemToGridLayoutItemKind(child, y);
-    });
+  // Get the grid position and size
+  height = (gridItem_.state.variableName ? gridItem_.state.itemHeight : gridItem_.state.height) ?? 0;
+  x = gridItem_.state.x ?? 0;
+  y = gridItem_.state.y ?? 0;
+  width = gridItem_.state.width ?? 0;
+  const repeatVar = gridItem_.state.variableName;
 
-    return {
-      kind: 'GridLayoutRow',
-      spec: {
-        title: row.state.title,
-        y: row.state.y ?? 0,
-        collapsed: Boolean(row.state.isCollapsed),
-        elements: children,
-        repeat: this.getRowRepeat(row),
+  // FIXME: which name should we use for the element reference, key or something else ?
+  const elementName = gridItem_.state.body.state.key ?? 'DefaultName';
+  elementGridItem = {
+    kind: 'GridLayoutItem',
+    spec: {
+      x,
+      y: yOverride ?? y,
+      width: width,
+      height: height,
+      element: {
+        kind: 'ElementReference',
+        name: elementName,
       },
-    };
-  }
+    },
+  };
 
-  gridItemToGridLayoutItemKind(gridItem: DashboardGridItem, yOverride?: number): GridLayoutItemKind {
-    let elementGridItem: GridLayoutItemKind | undefined;
-    let x = 0,
-      y = 0,
-      width = 0,
-      height = 0;
-
-    let gridItem_ = gridItem;
-
-    if (!(gridItem_.state.body instanceof VizPanel)) {
-      throw new Error('DashboardGridItem body expected to be VizPanel');
-    }
-
-    // Get the grid position and size
-    height = (gridItem_.state.variableName ? gridItem_.state.itemHeight : gridItem_.state.height) ?? 0;
-    x = gridItem_.state.x ?? 0;
-    y = gridItem_.state.y ?? 0;
-    width = gridItem_.state.width ?? 0;
-    const repeatVar = gridItem_.state.variableName;
-
-    // FIXME: which name should we use for the element reference, key or something else ?
-    const elementName = gridItem_.state.body.state.key ?? 'DefaultName';
-    elementGridItem = {
-      kind: 'GridLayoutItem',
-      spec: {
-        x,
-        y: yOverride ?? y,
-        width: width,
-        height: height,
-        element: {
-          kind: 'ElementReference',
-          name: elementName,
-        },
-      },
+  if (repeatVar) {
+    const repeat: RepeatOptions = {
+      mode: 'variable',
+      value: repeatVar,
     };
 
-    if (repeatVar) {
-      const repeat: RepeatOptions = {
-        mode: 'variable',
-        value: repeatVar,
-      };
-
-      if (gridItem_.state.maxPerRow) {
-        repeat.maxPerRow = gridItem_.getMaxPerRow();
-      }
-
-      if (gridItem_.state.repeatDirection) {
-        repeat.direction = gridItem_.getRepeatDirection();
-      }
-
-      elementGridItem.spec.repeat = repeat;
+    if (gridItem_.state.maxPerRow) {
+      repeat.maxPerRow = gridItem_.getMaxPerRow();
     }
 
-    if (!elementGridItem) {
-      throw new Error('Unsupported grid item type');
+    if (gridItem_.state.repeatDirection) {
+      repeat.direction = gridItem_.getRepeatDirection();
     }
 
-    return elementGridItem;
+    elementGridItem.spec.repeat = repeat;
   }
 
-  repeaterToLayoutItems(repeater: DashboardGridItem, isSnapshot = false): GridLayoutItemKind[] {
-    if (!isSnapshot) {
-      return [this.gridItemToGridLayoutItemKind(repeater)];
-    } else {
-      if (repeater.state.body instanceof VizPanel && isLibraryPanel(repeater.state.body)) {
-        // TODO: implement
-        // const { x = 0, y = 0, width: w = 0, height: h = 0 } = repeater.state;
-        // return [vizPanelToPanel(repeater.state.body, { x, y, w, h }, isSnapshot)];
-        return [];
-      }
+  if (!elementGridItem) {
+    throw new Error('Unsupported grid item type');
+  }
 
-      if (repeater.state.repeatedPanels) {
-        const { h, w, columnCount } = calculateGridItemDimensions(repeater);
-        const panels = repeater.state.repeatedPanels!.map((panel, index) => {
-          let x = 0,
-            y = 0;
-          if (repeater.state.repeatDirection === 'v') {
-            x = repeater.state.x!;
-            y = index * h;
-          } else {
-            x = (index % columnCount) * w;
-            y = repeater.state.y! + Math.floor(index / columnCount) * h;
-          }
+  return elementGridItem;
+}
 
-          const gridPos = { x, y, w, h };
-
-          const result: GridLayoutItemKind = {
-            kind: 'GridLayoutItem',
-            spec: {
-              x: gridPos.x,
-              y: gridPos.y,
-              width: gridPos.w,
-              height: gridPos.h,
-              repeat: {
-                mode: 'variable',
-                value: repeater.state.variableName!,
-                maxPerRow: repeater.getMaxPerRow(),
-                direction: repeater.state.repeatDirection,
-              },
-              element: {
-                kind: 'ElementReference',
-                name: panel.state.key!,
-              },
-            },
-          };
-          return result;
-        });
-
-        return panels;
-      }
+function repeaterToLayoutItems(repeater: DashboardGridItem, isSnapshot = false): GridLayoutItemKind[] {
+  if (!isSnapshot) {
+    return [gridItemToGridLayoutItemKind(repeater)];
+  } else {
+    if (repeater.state.body instanceof VizPanel && isLibraryPanel(repeater.state.body)) {
+      // TODO: implement
+      // const { x = 0, y = 0, width: w = 0, height: h = 0 } = repeater.state;
+      // return [vizPanelToPanel(repeater.state.body, { x, y, w, h }, isSnapshot)];
       return [];
     }
+
+    if (repeater.state.repeatedPanels) {
+      const { h, w, columnCount } = calculateGridItemDimensions(repeater);
+      const panels = repeater.state.repeatedPanels!.map((panel, index) => {
+        let x = 0,
+          y = 0;
+        if (repeater.state.repeatDirection === 'v') {
+          x = repeater.state.x!;
+          y = index * h;
+        } else {
+          x = (index % columnCount) * w;
+          y = repeater.state.y! + Math.floor(index / columnCount) * h;
+        }
+
+        const gridPos = { x, y, w, h };
+
+        const result: GridLayoutItemKind = {
+          kind: 'GridLayoutItem',
+          spec: {
+            x: gridPos.x,
+            y: gridPos.y,
+            width: gridPos.w,
+            height: gridPos.h,
+            repeat: {
+              mode: 'variable',
+              value: repeater.state.variableName!,
+              maxPerRow: repeater.getMaxPerRow(),
+              direction: repeater.state.repeatDirection,
+            },
+            element: {
+              kind: 'ElementReference',
+              name: panel.state.key!,
+            },
+          },
+        };
+        return result;
+      });
+
+      return panels;
+    }
+    return [];
   }
 }
 
