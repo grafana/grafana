30abe88b2f63137e93e4e556318caeffa3864509
Alex Spencer <alex.spencer@grafana.com>
2025-02-10 13:04:20 -0700

    fix: use baseURL instead of defining it

--- e2e/visual-regression/visualRegressionTesting.test.ts
+++ e2e/visual-regression/visualRegressionTesting.test.ts
@@ -1,10 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-const baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`;
-
 test('BarGauge', { tag: '@viz_regression' }, async ({ page }) => {
   const barGaugeLocation = 'd/O6f11TZWk/panel-tests-bar-gauge?orgId=1&from=now-6h&to=now&timezone=Europe%2FLondon';
-  await page.goto(`${baseURL}/${barGaugeLocation}`);
+  await page.goto(`/${barGaugeLocation}`);
 
   const ledBarGauge = await page.locator('.react-grid-item').nth(6).boundingBox();
 
@@ -14,7 +12,7 @@ test('BarGauge', { tag: '@viz_regression' }, async ({ page }) => {
 test('TimeSeriesStacking', { tag: '@viz_regression' }, async ({ page }) => {
   const timeSeriesStackingLocation =
     'd/Ei86FP_Mx/panel-tests-timeseries-stacking?orgId=1&from=now-6h&to=now&timezone=Europe%2FLondon';
-  await page.goto(`${baseURL}/${timeSeriesStackingLocation}`);
+  await page.goto(`/${timeSeriesStackingLocation}`);
 
   const stackedTimeSeries = await page.locator('.react-grid-item').first().boundingBox();
   const yAxis = await page.locator('.u-axis').nth(0);
