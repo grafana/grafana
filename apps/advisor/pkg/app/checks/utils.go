7e46186039bacd155e8b34743392689f6815eac6
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-02-10 13:15:41 +0100

    Advisor: Create checks following a schedule (#100282)

--- apps/advisor/pkg/app/checks/utils.go
+++ apps/advisor/pkg/app/checks/utils.go
@@ -4,6 +4,11 @@ import (
 	advisor "github.com/grafana/grafana/apps/advisor/pkg/apis/advisor/v0alpha1"
 )
 
+const (
+	TypeLabel        = "advisor.grafana.app/type"
+	StatusAnnotation = "advisor.grafana.app/status"
+)
+
 func NewCheckReportFailure(
 	severity advisor.CheckReportFailureSeverity,
 	reason string,
