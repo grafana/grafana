39725a87f322e9ceb30903362261390fe11626b6
SchneiderMaster <fynn2746@outlook.de>
2025-02-11 09:30:13 +0100

    Dashboard: sort only based on transformations on sorting option 'none'

--- public/app/plugins/panel/piechart/PieChart.tsx
+++ public/app/plugins/panel/piechart/PieChart.tsx
@@ -66,9 +66,6 @@ export const PieChart = ({
   });
 
   const filteredFieldDisplayValues = fieldDisplayValues.filter(filterDisplayItems);
-  const sortedFieldDisplayValues = filteredFieldDisplayValues.slice().sort((a, b) => {
-    return (a.colIndex ?? Infinity) - (b.colIndex ?? Infinity);
-  });
 
   const getValue = (d: FieldDisplay) => d.display.numeric;
   const getGradientId = (color: string) => `${componentInstanceId}-${tinycolor(color).toHex()}`;
@@ -78,10 +75,12 @@ export const PieChart = ({
 
   const showLabel = displayLabels.length > 0;
   const showTooltip = tooltipOptions.mode !== 'none' && tooltip.tooltipOpen;
-  const total = sortedFieldDisplayValues.reduce(sumDisplayItemsReducer, 0);
+  const total = filteredFieldDisplayValues.reduce(sumDisplayItemsReducer, 0);
   const layout = getPieLayout(width, height, pieType);
   const colors = [
-    ...new Set(sortedFieldDisplayValues.map((fieldDisplayValue) => fieldDisplayValue.display.color ?? FALLBACK_COLOR)),
+    ...new Set(
+      filteredFieldDisplayValues.map((fieldDisplayValue) => fieldDisplayValue.display.color ?? FALLBACK_COLOR)
+    ),
   ];
 
   // The pie chart will display slices going clockwise with the largest value first, we set a constant sort so it will be displayed with the legend order
@@ -107,7 +106,7 @@ export const PieChart = ({
             );
           })}
           <Pie
-            data={sortedFieldDisplayValues}
+            data={filteredFieldDisplayValues}
             pieValue={getValue}
             outerRadius={layout.outerRadius}
             innerRadius={layout.innerRadius}
