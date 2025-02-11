c3dca8717eed27d8a0ca3215ed61e4a5bc9ab971
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-02-11 10:00:05 +0100

    Add padding

--- public/app/features/provisioning/RepositoryOverview.tsx
+++ public/app/features/provisioning/RepositoryOverview.tsx
@@ -1,6 +1,7 @@
-import { Stack, Text, LinkButton, Card, TextLink, InteractiveTable } from '@grafana/ui';
-import { CellProps } from '@grafana/ui';
 import { useMemo } from 'react';
+
+import { Stack, Box, Text, LinkButton, Card, TextLink, InteractiveTable } from '@grafana/ui';
+import { CellProps } from '@grafana/ui';
 
 import { CheckRepository } from './CheckRepository';
 import { RepositoryHealth } from './RepositoryHealth';
@@ -23,103 +24,104 @@ export function RepositoryOverview({ repo }: { repo: Repository }) {
   const webhookURL = getWebhookURL(repo);
   const status = repo.status;
   const name = repo.metadata?.name ?? '';
-
   return (
-    <Stack gap={2}>
-      <Card>
-        <Card.Heading>Links</Card.Heading>
-        <Card.Meta>
-          <Stack>
-            {remoteURL && (
-              <Text>
-                <TextLink external href={remoteURL}>
-                  {remoteURL}
-                </TextLink>
-              </Text>
-            )}
-
-            {webhookURL && (
-              <Text>
-                <TextLink external href={webhookURL}>
-                  Webhook
-                </TextLink>
-              </Text>
-            )}
-          </Stack>
-        </Card.Meta>
-        <Card.Actions>
-          <LinkButton fill="outline" size="md" href={getFolderURL(repo)}>
-            Containing Folder
-          </LinkButton>
-        </Card.Actions>
-      </Card>
-      <Card>
-        <Card.Heading>Resources</Card.Heading>
-        <Card.Description>
-          {repo.status?.stats ? (
-            <InteractiveTable
-              columns={useMemo(
-                () => [
-                  {
-                    id: 'Resource',
-                    header: 'Resource Type',
-                    cell: ({ row: { original } }: StatCell<'resource'>) => {
-                      return <span>{original.resource}</span>;
-                    },
-                    size: 'auto',
-                  },
-                  {
-                    id: 'count',
-                    header: 'Count',
-                    cell: ({ row: { original } }: StatCell<'count'>) => {
-                      return <span>{original.count}</span>;
-                    },
-                    size: 100,
-                  },
-                ],
-                []
+    <Box padding={2}>
+      <Stack gap={2}>
+        <Card>
+          <Card.Heading>Links</Card.Heading>
+          <Card.Meta>
+            <Stack>
+              {remoteURL && (
+                <Text>
+                  <TextLink external href={remoteURL}>
+                    {remoteURL}
+                  </TextLink>
+                </Text>
               )}
-              data={repo.status.stats}
-              getRowId={(r: StatItem) => `${r.group}-${r.resource}`}
-            />
-          ) : null}
-        </Card.Description>
-      </Card>
-      <Card>
-        <Card.Heading>Health</Card.Heading>
-        <Card.Meta>
-          <RepositoryHealth repo={repo} />
-        </Card.Meta>
 
-        <Card.Actions>
-          <CheckRepository repository={repo} />
-        </Card.Actions>
-      </Card>
-      <Card>
-        <Card.Heading>
-          Sync Status{' '}
-          <StatusBadge enabled={Boolean(repo.spec?.sync?.enabled)} state={repo.status?.sync?.state} name={name} />
-        </Card.Heading>
-        <Card.Description>
-          <div style={{ display: 'grid', gridTemplateColumns: '100px 1fr', gap: '8px', alignItems: 'baseline' }}>
-            <Text color="secondary">Job ID:</Text>
-            <Text variant="body">{status?.sync.job ?? 'N/A'}</Text>
+              {webhookURL && (
+                <Text>
+                  <TextLink external href={webhookURL}>
+                    Webhook
+                  </TextLink>
+                </Text>
+              )}
+            </Stack>
+          </Card.Meta>
+          <Card.Actions>
+            <LinkButton fill="outline" size="md" href={getFolderURL(repo)}>
+              Containing Folder
+            </LinkButton>
+          </Card.Actions>
+        </Card>
+        <Card>
+          <Card.Heading>Resources</Card.Heading>
+          <Card.Description>
+            {repo.status?.stats ? (
+              <InteractiveTable
+                columns={useMemo(
+                  () => [
+                    {
+                      id: 'Resource',
+                      header: 'Resource Type',
+                      cell: ({ row: { original } }: StatCell<'resource'>) => {
+                        return <span>{original.resource}</span>;
+                      },
+                      size: 'auto',
+                    },
+                    {
+                      id: 'count',
+                      header: 'Count',
+                      cell: ({ row: { original } }: StatCell<'count'>) => {
+                        return <span>{original.count}</span>;
+                      },
+                      size: 100,
+                    },
+                  ],
+                  []
+                )}
+                data={repo.status.stats}
+                getRowId={(r: StatItem) => `${r.group}-${r.resource}`}
+              />
+            ) : null}
+          </Card.Description>
+        </Card>
+        <Card>
+          <Card.Heading>Health</Card.Heading>
+          <Card.Meta>
+            <RepositoryHealth repo={repo} />
+          </Card.Meta>
 
-            <Text color="secondary">Last Ref:</Text>
-            <Text variant="body">{status?.sync.hash ? status.sync.hash.substring(0, 7) : 'N/A'}</Text>
+          <Card.Actions>
+            <CheckRepository repository={repo} />
+          </Card.Actions>
+        </Card>
+        <Card>
+          <Card.Heading>
+            Sync Status{' '}
+            <StatusBadge enabled={Boolean(repo.spec?.sync?.enabled)} state={repo.status?.sync?.state} name={name} />
+          </Card.Heading>
+          <Card.Description>
+            <div style={{ display: 'grid', gridTemplateColumns: '100px 1fr', gap: '8px', alignItems: 'baseline' }}>
+              <Text color="secondary">Job ID:</Text>
+              <Text variant="body">{status?.sync.job ?? 'N/A'}</Text>
 
-            <Text color="secondary">Started:</Text>
-            <Text variant="body">{formatTimestamp(status?.sync.started)}</Text>
+              <Text color="secondary">Last Ref:</Text>
+              <Text variant="body">{status?.sync.hash ? status.sync.hash.substring(0, 7) : 'N/A'}</Text>
 
-            <Text color="secondary">Finished:</Text>
-            <Text variant="body">{formatTimestamp(status?.sync.finished)}</Text>
-          </div>
-        </Card.Description>
-        <Card.Actions>
-          <SyncRepository repository={repo} />
-        </Card.Actions>
-      </Card>
-    </Stack>
+              <Text color="secondary">Started:</Text>
+              <Text variant="body">{formatTimestamp(status?.sync.started)}</Text>
+
+              <Text color="secondary">Finished:</Text>
+              <Text variant="body">{formatTimestamp(status?.sync.finished)}</Text>
+            </div>
+          </Card.Description>
+          <Card.Actions>
+            <SyncRepository repository={repo} />
+          </Card.Actions>
+        </Card>
+      </Stack>
+    </Box>
   );
 }
 
