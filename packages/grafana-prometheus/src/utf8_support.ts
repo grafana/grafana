8153efea76702966420baf25c78b6c90ed94720c
Ryan McKinley <ryantxu@gmail.com>
2025-02-12 15:32:51 +0300

    change target

--- packages/grafana-prometheus/src/utf8_support.ts
+++ packages/grafana-prometheus/src/utf8_support.ts
@@ -82,19 +82,10 @@ const isValidCodePoint = (codePoint: number): boolean => {
 
 export const wrapUtf8Filters = (filterStr: string): string => {
   const resultArray: string[] = [];
-  const operatorRegex = /(=~|!=|!~|=)/; // NOTE: the order of the operators is important here
   let currentKey = '';
   let currentValue = '';
   let inQuotes = false;
   let temp = '';
-  const addResult = () => {
-    const operatorMatch = temp.match(operatorRegex);
-    if (operatorMatch) {
-      const operator = operatorMatch[0];
-      [currentKey, currentValue] = temp.split(operator);
-      resultArray.push(`${utf8Support(currentKey.trim())}${operator}"${currentValue.slice(1, -1)}"`);
-    }
-  };
 
   for (const char of filterStr) {
     if (char === '"' && temp[temp.length - 1] !== '\\') {
@@ -103,7 +94,8 @@ export const wrapUtf8Filters = (filterStr: string): string => {
       temp += char;
     } else if (char === ',' && !inQuotes) {
       // When outside quotes and encountering ',', finalize the current pair
-      addResult();
+      [currentKey, currentValue] = temp.split('=');
+      resultArray.push(`${utf8Support(currentKey.trim())}="${currentValue.slice(1, -1)}"`);
       temp = ''; // Reset for the next pair
     } else {
       // Collect characters
@@ -113,7 +105,9 @@ export const wrapUtf8Filters = (filterStr: string): string => {
 
   // Handle the last key-value pair
   if (temp) {
-    addResult();
+    [currentKey, currentValue] = temp.split('=');
+    resultArray.push(`${utf8Support(currentKey.trim())}="${currentValue.slice(1, -1)}"`);
   }
+
   return resultArray.join(',');
 };
