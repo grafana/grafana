1ff403f974e082aa0322f3a6b8c79778c6542629
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-02-12 10:38:31 +0100

    Notify always if finished

--- pkg/registry/apis/provisioning/jobs/notifier.go
+++ pkg/registry/apis/provisioning/jobs/notifier.go
@@ -8,11 +8,12 @@ import (
 )
 
 // MaybeNotifyProgress will only notify if a certain amount of time has passed
+// or if the job completed
 func MaybeNotifyProgress(threshold time.Duration, fn ProgressFn) ProgressFn {
 	var last time.Time
 
 	return func(ctx context.Context, status provisioning.JobStatus) error {
-		if last.IsZero() || time.Since(last) > threshold {
+		if status.Finished != 0 || last.IsZero() || time.Since(last) > threshold {
 			last = time.Now()
 			return fn(ctx, status)
 		}
