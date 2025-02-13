8d7039ee4230be3cd556e8b6979ad719a7548c88
Sam Kh. <syerikjan.khusayan@grafana.com>
2025-02-13 18:36:40 -0500

    feat: OSS connections page state filter and update all added

--- public/app/features/connections/pages/AddNewConnectionPage.tsx
+++ public/app/features/connections/pages/AddNewConnectionPage.tsx
@@ -1,12 +1,40 @@
+import { useState } from 'react';
+
+import { PluginType } from '@grafana/data';
+import { Button } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
+import { Trans } from 'app/core/internationalization';
+import UpdateAllModal from 'app/features/plugins/admin/components/UpdateAllModal';
+import { useGetUpdatable } from 'app/features/plugins/admin/state/hooks';
 
 import { AddNewConnection } from '../tabs/ConnectData';
 
 export function AddNewConnectionPage() {
+  const { isLoading: areUpdatesLoading, updatablePlugins } = useGetUpdatable();
+  const updatableDSPlugins = updatablePlugins.filter((plugin) => plugin.type === PluginType.datasource);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const disableUpdateAllButton = updatableDSPlugins.length <= 0 || areUpdatesLoading;
+
+  const onUpdateAll = () => {
+    setShowUpdateModal(true);
+  };
+
+  const updateAll = (
+    <Button disabled={disableUpdateAllButton} onClick={onUpdateAll}>
+      <Trans i18nKey="plugins.catalog.update-all.button">Update all</Trans>
+      {disableUpdateAllButton ? '' : ` (${updatableDSPlugins.length})`}
+    </Button>
+  );
   return (
-    <Page navId={'connections-add-new-connection'}>
+    <Page navId={'connections-add-new-connection'} actions={updateAll}>
       <Page.Contents>
         <AddNewConnection />
+        <UpdateAllModal
+          isOpen={showUpdateModal}
+          isLoading={areUpdatesLoading}
+          onDismiss={() => setShowUpdateModal(false)}
+          plugins={updatableDSPlugins}
+        />
       </Page.Contents>
     </Page>
   );
