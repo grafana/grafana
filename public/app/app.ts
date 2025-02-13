f1b4676a26c37ba62cb06774b8eb904a700f0d00
Misi <mgyongyosi@users.noreply.github.com>
2025-02-13 10:46:19 +0100

    Auth: Add early return if `auth_token` is in the URL for JWT auth (#100539)
    * Add early return
    
    * Update public/app/app.ts
    
    Co-authored-by: Victor Cinaglia <victor@grafana.com>
    
    ---------
    
    Co-authored-by: Victor Cinaglia <victor@grafana.com>
    (cherry picked from commit 5a6d2f2e49b8d7c3cb2032c3fe5e88eeed28d524)

--- public/app/app.ts
+++ public/app/app.ts
@@ -393,6 +393,7 @@ function handleRedirectTo(): void {
   if (queryParams.has('auth_token')) {
     // URL Login should not be redirected
     window.sessionStorage.removeItem(RedirectToUrlKey);
+    return;
   }
 
   if (queryParams.has(redirectToParamKey) && window.location.pathname !== '/') {
