d8e7f18334259d92a4faf1a37a424257ca236b4f
Bogdan Matei <bogdan.matei@grafana.com>
2025-02-13 11:58:28 +0200

    Dashboard: Fix removing row repeats having indexes ending with 0 (#100487)

--- public/app/features/dashboard-scene/utils/clone.test.ts
+++ public/app/features/dashboard-scene/utils/clone.test.ts
@@ -48,6 +48,20 @@ describe('clone', () => {
       expect(isClonedKey('tab-clone-1/row-clone-2/panel')).toBe(false);
       expect(isClonedKey('row-clone-1/panel')).toBe(false);
     });
+
+    it('should properly handle indexes containing 0', () => {
+      expect(isClonedKey('tab-clone-0/row-clone-1/panel-clone-0')).toBe(false);
+      expect(isClonedKey('row-clone-0/panel-clone-0')).toBe(false);
+      expect(isClonedKey('panel-clone-0')).toBe(false);
+
+      expect(isClonedKey('tab-clone-0/row-clone-1/panel-clone-101')).toBe(true);
+      expect(isClonedKey('row-clone-0/panel-clone-101')).toBe(true);
+      expect(isClonedKey('panel-clone-1010')).toBe(true);
+
+      expect(isClonedKey('tab-clone-0/row-clone-1/panel-clone-10')).toBe(true);
+      expect(isClonedKey('row-clone-0/panel-clone-100')).toBe(true);
+      expect(isClonedKey('panel-clone-1000')).toBe(true);
+    });
   });
 
   describe('isClonedKeyOf', () => {
