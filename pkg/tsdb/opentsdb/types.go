8f2dcf1e7bfd4dd12ae67c794f10263a5ae3be50
Andreas Christou <andreas.christou@grafana.com>
2025-02-13 20:23:17 +0000

    Update query logic to parse new and old format
    - Minor naming updates
    - Extract logic for initial frame creation
    - When parsing old api responses, ensure data is in ascending order
    - Update tests

--- pkg/tsdb/opentsdb/types.go
+++ pkg/tsdb/opentsdb/types.go
@@ -6,8 +6,17 @@ type OpenTsdbQuery struct {
 	Queries []map[string]any `json:"queries"`
 }
 
+type OpenTsdbCommon struct {
+	Metric string            `json:"metric"`
+	Tags   map[string]string `json:"tags"`
+}
+
 type OpenTsdbResponse struct {
-	Metric     string            `json:"metric"`
-	Tags       map[string]string `json:"tags"`
-	DataPoints [][]float64       `json:"dps"`
+	OpenTsdbCommon
+	DataPoints map[string]float64 `json:"dps"`
+}
+
+type OpenTsdbResponse24 struct {
+	OpenTsdbCommon
+	DataPoints [][]float64 `json:"dps"`
 }
