6db155649c255fc673a9906476bebbf4813ab48f
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2025-02-13 11:31:57 +0100

    Plugins: Custom links for plugin details page (#97186)
    * Custom links with repository link, licence link, docs link and raise an issue link
    
    * run translation command
    
    * delete console log
    
    * delete console log
    
    * fix frontend tests
    
    * change UI with a new design
    
    * remove license, documentation, repository url calculation logic from grafana
    
    * remove unsused function from helpers
    
    * change repo icons and raise an issue icon
    
    * fix the build
    
    * remove logic for raiseAnIssueUrl
    
    * fix the build
    
    * fix lint
    
    * Delete Links title in the box of links
    
    ---------
    
    Co-authored-by: Timur Olzhabayev <timur.olzhabayev@grafana.com>

--- public/app/features/plugins/admin/api.ts
+++ public/app/features/plugins/admin/api.ts
@@ -37,6 +37,8 @@ export async function getPluginDetails(id: string): Promise<CatalogPluginDetails
     iam: remote?.json?.iam,
     lastCommitDate: remote?.lastCommitDate,
     changelog: remote?.changelog || localChangelog,
+    licenseUrl: remote?.licenseUrl,
+    documentationUrl: remote?.documentationUrl,
     signatureType: local?.signatureType || (remote?.signatureType !== '' ? remote?.signatureType : undefined),
     signature: local?.signature,
   };
