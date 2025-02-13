8769233e502d7111013475c70bd881ea6f4d2d17
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-02-13 19:16:38 +0100

    Use recorder for folders

--- pkg/registry/apis/provisioning/jobs/export/folders.go
+++ pkg/registry/apis/provisioning/jobs/export/folders.go
@@ -24,7 +24,6 @@ var _ resource.BatchResourceWriter = (*folderReader)(nil)
 type folderReader struct {
 	tree           *resources.FolderTree
 	targetRepoName string
-	progress       *jobs.JobProgressRecorder
 }
 
 // Close implements resource.BatchResourceWriter.
@@ -48,6 +47,7 @@ func (f *folderReader) Write(ctx context.Context, key *resource.ResourceKey, val
 	return f.tree.AddUnstructured(item, f.targetRepoName)
 }
 
+// FIXME: revise logging in this method
 func (r *exportJob) loadFolders(ctx context.Context) error {
 	logger := r.logger
 	r.progress.SetMessage("reading folder tree")
@@ -55,6 +55,7 @@ func (r *exportJob) loadFolders(ctx context.Context) error {
 	repoName := r.target.Config().Name
 
 	if r.legacy != nil {
+		r.progress.SetMessage("migrate folder tree from legacy")
 		reader := &folderReader{
 			tree:           r.folderTree,
 			targetRepoName: repoName,
@@ -71,6 +72,8 @@ func (r *exportJob) loadFolders(ctx context.Context) error {
 			return fmt.Errorf("unable to read folders from legacy storage %w", err)
 		}
 	} else {
+		// TODO: should this be logging or message or both?
+		r.progress.SetMessage("read folder tree from unified storage")
 		client := r.client.Resource(schema.GroupVersionResource{
 			Group:    folders.GROUP,
 			Version:  folders.VERSION,
@@ -84,10 +87,16 @@ func (r *exportJob) loadFolders(ctx context.Context) error {
 		if rawList.GetContinue() != "" {
 			return fmt.Errorf("unable to list all folders in one request: %s", rawList.GetContinue())
 		}
+
 		for _, item := range rawList.Items {
 			err = r.folderTree.AddUnstructured(&item, repoName)
 			if err != nil {
-				// summary.Errors = append(summary.Errors, err.Error())
+				r.progress.Record(ctx, jobs.JobResourceResult{
+					Name:     item.GetName(),
+					Resource: folders.RESOURCE,
+					Group:    folders.GROUP,
+					Error:    err,
+				})
 			}
 		}
 	}
@@ -96,29 +105,43 @@ func (r *exportJob) loadFolders(ctx context.Context) error {
 	// NOTE: this is required so that empty folders exist when finished
 	r.progress.SetMessage("writing folders")
 
-	err := tree.Walk(ctx, func(ctx context.Context, folder resources.Folder) error {
+	err := r.folderTree.Walk(ctx, func(ctx context.Context, folder resources.Folder) error {
 		p := folder.Path + "/"
 		if r.prefix != "" {
 			p = r.prefix + "/" + p
 		}
 		logger := logger.With("path", p)
 
+		result := jobs.JobResourceResult{
+			Name:     folder.ID,
+			Resource: folders.RESOURCE,
+			Group:    folders.GROUP,
+			Path:     p,
+		}
+
 		_, err := r.target.Read(ctx, p, r.ref)
 		if err != nil && !(errors.Is(err, repository.ErrFileNotFound) || apierrors.IsNotFound(err)) {
-			logger.Error("failed to check if folder exists before writing", "error", err)
-			return fmt.Errorf("failed to check if folder exists before writing: %w", err)
+			result.Error = fmt.Errorf("failed to check if folder exists before writing: %w", err)
+			// TODO: what should we do here? Should we stop execution?
+			return result.Error
 		} else if err == nil {
 			logger.Info("folder already exists")
-			// summary.Noop++
+			result.Action = repository.FileActionIgnored
+			r.progress.Record(ctx, result)
 			return nil
 		}
 
+		result.Action = repository.FileActionCreated
+		msg := fmt.Sprintf("export folder %s", p)
 		// Create with an empty body will make a folder (or .keep file if unsupported)
-		if err := r.target.Create(ctx, p, r.ref, nil, "export folder `"+p+"`"); err != nil {
-			logger.Error("failed to write a folder in repository", "error", err)
-			return fmt.Errorf("failed to write folder in repo: %w", err)
+		if err := r.target.Create(ctx, p, r.ref, nil, msg); err != nil {
+			result.Error = fmt.Errorf("failed to write folder in repo: %w", err)
+			r.progress.Record(ctx, result)
+			// TODO: should we stop execution here?
+			return result.Error
 		}
-		// summary.Create++
+
+		r.progress.Record(ctx, result)
 		logger.Debug("successfully exported folder")
 		return nil
 	})
