7eaca44a9802f2c8e61f658c41d83c9e23b18d22
Peter Štibraný <pstibrany@gmail.com>
2025-02-14 16:39:48 +0100

    Extract NewSearchOptions from unified sql client setup. (#100719)
    * Extract NewSearchOptions from unified sql client setup.
    
    Co-authored-by: Georges Chaudy <chaudyg@gmail.com>

--- pkg/storage/unified/sql/server.go
+++ pkg/storage/unified/sql/server.go
@@ -1,28 +1,23 @@
 package sql
 
 import (
-	"context"
-	"log/slog"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/grafana/authlib/types"
+
 	infraDB "github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/infra/tracing"
-	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/storage/unified/resource"
-	"github.com/grafana/grafana/pkg/storage/unified/search"
 	"github.com/grafana/grafana/pkg/storage/unified/sql/db/dbimpl"
 )
 
 // Creates a new ResourceServer
-func NewResourceServer(ctx context.Context, db infraDB.DB, cfg *setting.Cfg,
-	features featuremgmt.FeatureToggles, docs resource.DocumentBuilderSupplier,
-	tracer tracing.Tracer, reg prometheus.Registerer, ac types.AccessClient) (resource.ResourceServer, error) {
+func NewResourceServer(db infraDB.DB, cfg *setting.Cfg,
+	tracer tracing.Tracer, reg prometheus.Registerer, ac types.AccessClient, searchOptions resource.SearchOptions) (resource.ResourceServer, error) {
 	apiserverCfg := cfg.SectionWithEnvOverrides("grafana-apiserver")
 	opts := resource.ResourceServerOptions{
 		Tracer: tracer,
@@ -55,44 +50,7 @@ func NewResourceServer(ctx context.Context, db infraDB.DB, cfg *setting.Cfg,
 	opts.Backend = store
 	opts.Diagnostics = store
 	opts.Lifecycle = store
-
-	// Setup the search server
-	if features.IsEnabledGlobally(featuremgmt.FlagUnifiedStorageSearch) {
-		root := cfg.IndexPath
-		if root == "" {
-			root = filepath.Join(cfg.DataPath, "unified-search", "bleve")
-		}
-		err = os.MkdirAll(root, 0750)
-		if err != nil {
-			return nil, err
-		}
-		bleve, err := search.NewBleveBackend(search.BleveOptions{
-			Root:          root,
-			FileThreshold: int64(cfg.IndexFileThreshold), // fewer than X items will use a memory index
-			BatchSize:     cfg.IndexMaxBatchSize,         // This is the batch size for how many objects to add to the index at once
-		}, tracer, features)
-
-		if err != nil {
-			return nil, err
-		}
-
-		opts.Search = resource.SearchOptions{
-			Backend:       bleve,
-			Resources:     docs,
-			WorkerThreads: cfg.IndexWorkers,
-			InitMinCount:  cfg.IndexMinCount,
-		}
-
-		// Register indexer metrics
-		err = reg.Register(resource.NewIndexMetrics(cfg.IndexPath, opts.Search.Backend))
-		if err != nil {
-			slog.Warn("Failed to register indexer metrics", "error", err)
-		}
-		err = reg.Register(resource.NewSprinklesMetrics())
-		if err != nil {
-			slog.Warn("Failed to register sprinkles metrics", "error", err)
-		}
-	}
+	opts.Search = searchOptions
 
 	rs, err := resource.NewResourceServer(opts)
 	if err != nil {
