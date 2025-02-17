bb3d64d1e9a03d219576393b8d26739933a36241
Ashley Harrison <ashley.harrison@grafana.com>
2025-02-17 11:37:03 +0000

    Theme: minor tweaks (#100751)
    * theme tweaks!
    
    * remove commented code

--- packages/grafana-ui/.storybook/preview.ts
+++ packages/grafana-ui/.storybook/preview.ts
@@ -37,6 +37,7 @@ if (process.env.NODE_ENV === 'development') {
   allowedExtraThemes.push('debug');
   allowedExtraThemes.push('desertbloom');
   allowedExtraThemes.push('gildedgrove');
+  allowedExtraThemes.push('gloom');
   allowedExtraThemes.push('sapphiredusk');
   allowedExtraThemes.push('tron');
 }
