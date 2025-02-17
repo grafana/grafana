5ea1a081c5995cdaa2323d1c806e65cf6596d818
Jack Westbrook <jack.westbrook@gmail.com>
2025-02-17 16:24:44 +0100

    chore(runtime): delete angular injector

--- packages/grafana-runtime/src/services/index.ts
+++ packages/grafana-runtime/src/services/index.ts
@@ -4,7 +4,6 @@ export * from './dataSourceSrv';
 export * from './LocationSrv';
 export * from './EchoSrv';
 export * from './templateSrv';
-export * from './legacyAngularInjector';
 export * from './live';
 export * from './LocationService';
 export * from './appEvents';
