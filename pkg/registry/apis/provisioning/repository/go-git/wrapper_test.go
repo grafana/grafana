0faa7211f4cdd2e52215c840a5d7be9fd3eb2d75
Ryan McKinley <ryantxu@gmail.com>
2025-02-18 11:34:48 +0300

    Provisioning: Better clone/push error support (#100854)

--- pkg/registry/apis/provisioning/repository/go-git/wrapper_test.go
+++ pkg/registry/apis/provisioning/repository/go-git/wrapper_test.go
@@ -15,13 +15,29 @@ import (
 	"github.com/grafana/grafana/pkg/registry/apis/provisioning/repository"
 )
 
+type dummySecret struct{}
+
+// Decrypt implements secrets.Service.
+func (d *dummySecret) Decrypt(ctx context.Context, data []byte) ([]byte, error) {
+	token, ok := os.LookupEnv("gitwraptoken")
+	if !ok {
+		return nil, fmt.Errorf("missing token in environment")
+	}
+	return []byte(token), nil
+}
+
+// Encrypt implements secrets.Service.
+func (d *dummySecret) Encrypt(ctx context.Context, data []byte) ([]byte, error) {
+	panic("unimplemented")
+}
+
 // FIXME!! NOTE!!!!!
 // This is really just a sketchpad while trying to get things working
 // the test makes destructive changes to a real git repository :)
 // this should be removed before committing to main (likely sooner)
 // and replaced with integration tests that check the more specific results
 func TestGoGitWrapper(t *testing.T) {
-	token, ok := os.LookupEnv("gitwraptoken")
+	_, ok := os.LookupEnv("gitwraptoken")
 	if !ok {
 		t.Skipf("no token found in environment")
 	}
@@ -35,8 +51,7 @@ func TestGoGitWrapper(t *testing.T) {
 		Spec: v0alpha1.RepositorySpec{
 			GitHub: &v0alpha1.GitHubRepositoryConfig{
 				URL:    "https://github.com/grafana/git-ui-sync-demo",
-				Branch: "ryan-test",
-				Token:  token,
+				Branch: "ryan-test-test-test",
 			},
 		},
 	},
@@ -45,7 +60,7 @@ func TestGoGitWrapper(t *testing.T) {
 			// one commit (not 11)
 			SingleCommitBeforePush: true,
 		},
-		nil, // TODO: add a mock
+		&dummySecret{},
 		os.Stdout)
 	require.NoError(t, err)
 
@@ -57,9 +72,9 @@ func TestGoGitWrapper(t *testing.T) {
 
 	fmt.Printf("TREE:%s\n", string(jj))
 
-	branch := fmt.Sprintf("unit-test-%s", time.Now().Format("20060102-150405")) // the branch name to create
+	branch := "" //fmt.Sprintf("unit-test-%s", time.Now().Format("20060102-150405")) // the branch name to create
 
-	count, err := wrap.NewEmptyBranch(ctx, branch)
+	count, err := wrap.CheckoutEmptyBranch(ctx, branch)
 	require.NoError(t, err)
 	fmt.Printf("REMOVED: %d\n", count)
 
