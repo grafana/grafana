626a719c908f9fe30cfcf33666dc543afaab4aee
Matias Chomicki <matyax@gmail.com>
2025-02-19 17:11:44 +0100

    LogLineMenu: Add placeholder options

--- public/app/features/logs/components/panel/LogLineMenu.tsx
+++ public/app/features/logs/components/panel/LogLineMenu.tsx
@@ -1,7 +1,8 @@
-import { IconButton } from "@grafana/ui";
+import { Dropdown, IconButton, Menu } from '@grafana/ui';
+import { t } from 'app/core/internationalization';
 
-import { LogLineStyles } from "./LogLine";
-import { LogListModel } from "./processing"
+import { LogLineStyles } from './LogLine';
+import { LogListModel } from './processing';
 
 interface Props {
   log: LogListModel;
@@ -9,7 +10,19 @@ interface Props {
 }
 
 export const LogLineMenu = ({ styles }: Props) => {
+  const menu = (
+    <Menu>
+      <Menu.Item label={t('logs.log-line-menu.copy-log', 'Copy log line')} />
+      <Menu.Item label={t('logs.log-line-menu.copy-link', 'Copy link to log line')} />
+      <Menu.Divider />
+      <Menu.Item label={t('logs.log-line-menu.show-context', 'Show context')} />
+      <Menu.Item label={t('logs.log-line-menu.pin-to-outline', 'Pin to content outline')} />
+    </Menu>
+  );
+
   return (
-    <IconButton className={styles.menuIcon} name="ellipsis-v" aria-label="Log menu" />
-  )
-}
+    <Dropdown overlay={menu} placement="bottom-start">
+      <IconButton className={styles.menuIcon} name="ellipsis-v" aria-label="Log menu" />
+    </Dropdown>
+  );
+};
