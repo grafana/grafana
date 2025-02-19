68ee3b8389f4b1ff9c478347e1fda9bbe402abaa
Sarah Zinger <sarah.zinger@grafana.com>
2025-02-18 19:54:08 -0500

    Let's make it an error tho

--- pkg/tsdb/cloudwatch/cloudwatch.go
+++ pkg/tsdb/cloudwatch/cloudwatch.go
@@ -234,7 +234,7 @@ func (e *cloudWatchExecutor) QueryData(ctx context.Context, req *backend.QueryDa
 	}
 
 	if features.IsEnabled(ctx, features.FlagCloudWatchRoundUpEndTime) {
-		e.logger.Debug("!!!!!!!!!! IT BROKE HERE !!!!!!!!!!")
+		e.logger.Error("!!!!!!!!!! IT BROKE HERE !!!!!!!!!!")
 		return nil, fmt.Errorf("test bug")
 	}
 
