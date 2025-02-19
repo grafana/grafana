d3a9e5e20b164797d500af00baba96b49156f97e
Sergej-Vlasov <37613182+Sergej-Vlasov@users.noreply.github.com>
2025-02-19 10:03:37 +0000

    ImportDashboardForm: Add more meaningful error message for Editor user (#100581)
    return error message when getting 403 during dashboard import

--- public/app/features/manage-dashboards/utils/validation.ts
+++ public/app/features/manage-dashboards/utils/validation.ts
@@ -53,6 +53,12 @@ export const validateUid = (value: string) => {
     })
     .catch((error) => {
       error.isHandled = true;
+
+      // when Editor user tries to import admin only dashboard (with same uid) he gets an unhelpful 403 error
+      //  therefore handling this use case to return some indication of whats wrong
+      if (error.status === 403) {
+        return 'Dashboard with the same UID already exists';
+      }
       return true;
     });
 };
