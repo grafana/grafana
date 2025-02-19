5a7916133eac32e5d085d8e88ae9785add4bcb57
Zoltán Bedi <zoltan.bedi@gmail.com>
2025-02-19 15:38:27 +0100

    Chore: Remove `sqlQuerybuilderFunctionParameters` feature toggle (#100809)
    * chore: remove `sqlQuerybuilderFunctionParameters` ff
    
    * Update packages/grafana-sql/src/components/visual-query-builder/SelectRow.tsx
    
    Co-authored-by: ismail simsek <ismailsimsek09@gmail.com>
    
    ---------
    
    Co-authored-by: ismail simsek <ismailsimsek09@gmail.com>

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1234,13 +1234,6 @@ var (
 			Expression:  "false", // disabled by default
 		},
 		{
-			Name:         "sqlQuerybuilderFunctionParameters",
-			Description:  "Enables SQL query builder function parameters",
-			Stage:        FeatureStageExperimental,
-			Owner:        grafanaOSSBigTent,
-			FrontendOnly: true,
-		},
-		{
 			Name:        "azureMonitorPrometheusExemplars",
 			Description: "Allows configuration of Azure Monitor as a data source that can provide Prometheus exemplars",
 			Stage:       FeatureStagePublicPreview,
