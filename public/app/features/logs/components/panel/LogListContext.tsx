b14352dcaffce235d0703aa8c678ae3666f631a7
Matias Chomicki <matyax@gmail.com>
2025-02-20 13:28:05 +0100

    LogListContext: add unit test

--- public/app/features/logs/components/panel/LogListContext.tsx
+++ public/app/features/logs/components/panel/LogListContext.tsx
@@ -1,13 +1,12 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext } from 'react';
 
-import { LogRowModel } from "@grafana/data";
-import { PopoverContent } from "@grafana/ui";
+import { LogRowModel } from '@grafana/data';
+import { PopoverContent } from '@grafana/ui';
 
-import { GetRowContextQueryFn } from "./LogLineMenu";
+import { GetRowContextQueryFn } from './LogLineMenu';
 
 export interface LogListContextData {
   getRowContextQuery?: GetRowContextQueryFn;
-  initialScrollPosition?: 'top' | 'bottom';
   logSupportsContext?: (row: LogRowModel) => boolean;
   onPermalinkClick?: (row: LogRowModel) => Promise<void>;
   onPinLine?: (row: LogRowModel) => void;
@@ -22,13 +21,13 @@ export const LogListContext = createContext<LogListContextData>({});
 export const useLogListContextData = (key: keyof LogListContextData) => {
   const data: LogListContextData = useContext(LogListContext);
   return data[key];
-}
+};
 
 export const useLogListContext = (): LogListContextData => {
   return useContext(LogListContext);
-}
+};
 
 export const useLogIsPinned = (log: LogRowModel) => {
   const { pinnedLogs } = useContext(LogListContext);
   return pinnedLogs?.some((logId) => logId === log.rowId);
-}
+};
