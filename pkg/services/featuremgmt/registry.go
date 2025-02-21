10c6f1878ec5ccda556a10977d9c5da2e3e5675e
Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
2025-02-21 09:31:16 -0500

    Docs: Show `exploreMetricsRelatedLogs` feature toggle in docs (#101106)
    docs: show `exploreMetricsRelatedLogs` feature toggle in docs

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1509,11 +1509,11 @@ var (
 		},
 		{
 			Name:         "exploreMetricsRelatedLogs",
-			Description:  "Display Related Logs in Explore Metrics",
+			Description:  "Display Related Logs in Grafana Metrics Drilldown",
 			Stage:        FeatureStageExperimental,
 			Owner:        grafanaObservabilityMetricsSquad,
 			FrontendOnly: true,
-			HideFromDocs: true,
+			HideFromDocs: false,
 		},
 		{
 			Name:         "prometheusSpecialCharsInLabelValues",
