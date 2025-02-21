d2fe0613f6c923c3e8f2571e6d9ec8410fd457c6
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-02-21 13:35:00 +0100

    Advisor: Debug Log authorizer

--- apps/advisor/pkg/app/authorizer.go
+++ apps/advisor/pkg/app/authorizer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
+	"github.com/grafana/grafana/pkg/infra/log"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 )
 
@@ -11,16 +12,21 @@ func GetAuthorizer() authorizer.Authorizer {
 	return authorizer.AuthorizerFunc(func(
 		ctx context.Context, attr authorizer.Attributes,
 	) (authorized authorizer.Decision, reason string, err error) {
+		l := log.New("advisor.authorizer")
+		l.Info("GetAuthorizer", "attr", attr)
 		if !attr.IsResourceRequest() {
 			return authorizer.DecisionNoOpinion, "", nil
 		}
 
 		// require a user
 		u, err := identity.GetRequester(ctx)
+		l.Info("GetAuthorizer", "user", u, "err", err)
 		if err != nil {
 			return authorizer.DecisionDeny, "valid user is required", err
 		}
 
+		l.Info("IsAdmin", "isAdmin", u.GetIsGrafanaAdmin())
+		l.Info("IsAPIKey", "uid", u.GetUID(), "email", u.GetEmail())
 		// check if is admin
 		if u.GetIsGrafanaAdmin() {
 			return authorizer.DecisionAllow, "", nil
