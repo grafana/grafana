5a2a02030f44d94d1f21a38f96f41a5d6c8e2fff
Ryan McKinley <ryantxu@gmail.com>
2025-02-24 13:47:41 +0300

    avoid migrate

--- public/app/features/provisioning/Wizard/MigrateToProvisioningPage.tsx
+++ public/app/features/provisioning/Wizard/MigrateToProvisioningPage.tsx
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom-v5-compat';
+
 import { Page } from 'app/core/components/Page/Page';
 
 import { SetupWarnings } from '../SetupWarnings';
+import { useGetFrontendSettingsQuery } from '../api';
+import { PROVISIONING_URL } from '../constants';
 
 import { ProvisioningWizard } from './ProvisioningWizard';
 
 export default function MigrateToProvisioningPage() {
+  const settings = useGetFrontendSettingsQuery();
+  const navigate = useNavigate();
+  if (settings.data) {
+    // Do run the migration wizard if you are already using unified storage
+    if (!Boolean(settings.data.legacyStorage)) {
+      navigate(PROVISIONING_URL);
+    }
+    // Do run the migration wizard if something is already targeting the instance
+    if (settings.data.items.find((v) => v.target === 'instance')) {
+      navigate(PROVISIONING_URL);
+    }
+  }
+
   return (
     <Page
       navId="provisioning"
