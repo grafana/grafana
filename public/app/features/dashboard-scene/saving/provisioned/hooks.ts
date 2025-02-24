b7682921f08d8f12cc243dc4ea3eb316caccd2cd
Ryan McKinley <ryantxu@gmail.com>
2025-02-24 09:15:38 +0300

    better branch handling

--- public/app/features/dashboard-scene/saving/provisioned/hooks.ts
+++ public/app/features/dashboard-scene/saving/provisioned/hooks.ts
@@ -1,6 +1,11 @@
+import { time } from 'console';
+
+import { dateTime } from '@grafana/data';
 import { AnnoKeyRepoName, AnnoKeyRepoPath } from 'app/features/apiserver/types';
 import { useGetResourceRepository } from 'app/features/provisioning/hooks';
 import { DashboardMeta } from 'app/types';
+
+import { newLetterRandomizer } from '../../inspect/HelpWizard/randomizer';
 
 import { getDefaultWorkflow } from './defaults';
 
@@ -10,7 +15,7 @@ interface UseDefaultValuesParams {
   defaultDescription?: string;
 }
 
-function generatePath(timestamp: number, pathFromAnnotation?: string, slug?: string) {
+function generatePath(timestamp: string, pathFromAnnotation?: string, slug?: string) {
   if (pathFromAnnotation) {
     const hashIndex = pathFromAnnotation.indexOf('#');
     return hashIndex > 0 ? pathFromAnnotation.substring(0, hashIndex) : pathFromAnnotation;
@@ -27,7 +32,9 @@ export function useDefaultValues({ meta, defaultTitle, defaultDescription }: Use
   // Get config by resource name or folder UID for new resources
   const repositoryConfig = useGetResourceRepository({ name: annoName, folderUid: meta.folderUid });
   const repository = repositoryConfig?.spec;
-  const timestamp = Date.now();
+
+  const randomizer = newLetterRandomizer(); // replace with random
+  const timestamp = dateTime().format('YYYY-MM-DD-') + randomizer('xxxxx');
 
   return {
     values: {
