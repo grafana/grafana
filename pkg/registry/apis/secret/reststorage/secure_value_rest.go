38951d43049b0b5d9a45baa03c22ac2e40d493bf
Matheus Macabu <macabu@users.noreply.github.com>
2025-02-25 12:23:24 +0100

    SecureValues: add temporary allow list for decrypters (#101272)

--- pkg/registry/apis/secret/reststorage/secure_value_rest.go
+++ pkg/registry/apis/secret/reststorage/secure_value_rest.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -190,7 +191,7 @@ func (s *SecureValueRest) Delete(ctx context.Context, name string, deleteValidat
 }
 
 // ValidateSecureValue does basic spec validation of a securevalue.
-func ValidateSecureValue(sv, oldSv *secretv0alpha1.SecureValue, operation admission.Operation) field.ErrorList {
+func ValidateSecureValue(sv, oldSv *secretv0alpha1.SecureValue, operation admission.Operation, decryptersAllowList []string) field.ErrorList {
 	errs := make(field.ErrorList, 0)
 
 	// Operation-specific field validation.
@@ -211,6 +212,16 @@ func ValidateSecureValue(sv, oldSv *secretv0alpha1.SecureValue, operation admiss
 
 	// If populated, `Decrypters` must match "{group}/{name OR *}" and must be unique.
 	for i, decrypter := range sv.Spec.Decrypters {
+		// Allow List: decrypters must match exactly and be in the allowed list to be able to decrypt.
+		if len(decryptersAllowList) > 0 && !slices.Contains(decryptersAllowList, decrypter) {
+			errs = append(
+				errs,
+				field.Invalid(field.NewPath("spec", "decrypters", "["+strconv.Itoa(i)+"]"), decrypter, fmt.Sprintf("allowed values: %v", decryptersAllowList)),
+			)
+
+			return errs
+		}
+
 		group, name, found := strings.Cut(decrypter, "/")
 		if !found {
 			errs = append(
