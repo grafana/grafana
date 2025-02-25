a520d3d53109f357ed0b4648786b420ad0593715
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-02-25 12:08:58 +0100

    Revert changes in test

--- pkg/registry/apis/provisioning/repository/github_test.go
+++ pkg/registry/apis/provisioning/repository/github_test.go
@@ -116,6 +116,8 @@ func TestParseWebhooks(t *testing.T) {
 				GitHub: &provisioning.GitHubRepositoryConfig{
 					URL:    "https://github.com/grafana/git-ui-sync-demo",
 					Branch: "main",
+
+					GenerateDashboardPreviews: true,
 				},
 			},
 		},
