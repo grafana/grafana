e547ee52b59ad6d6a1b90afd63818a10ac6a5902
Will Browne <will.browne@grafana.com>
2025-02-25 12:10:06 +0000

    fix linter + tests

--- public/app/features/plugins/admin/helpers.test.ts
+++ public/app/features/plugins/admin/helpers.test.ts
@@ -209,11 +209,6 @@ describe('Plugins/Helpers', () => {
         isFullyInstalled: false,
         angularDetected: false,
         url: 'https://github.com/alexanderzobnin/grafana-zabbix',
-        details: {
-          dependantPlugins: [],
-          links: [],
-          pluginDependencies: [],
-        },
       });
     });
 
@@ -296,24 +291,6 @@ describe('Plugins/Helpers', () => {
         installedVersion: '4.2.2',
         isFullyInstalled: true,
         angularDetected: false,
-        details: {
-          dependantPlugins: [],
-          links: [
-            {
-              name: 'GitHub',
-              url: 'https://github.com/alexanderzobnin/grafana-zabbix',
-            },
-            {
-              name: 'Docs',
-              url: 'https://alexanderzobnin.github.io/grafana-zabbix',
-            },
-            {
-              name: 'License',
-              url: 'https://github.com/alexanderzobnin/grafana-zabbix/blob/master/LICENSE',
-            },
-          ],
-          pluginDependencies: [],
-        },
       });
     });
 
@@ -370,24 +347,6 @@ describe('Plugins/Helpers', () => {
         isFullyInstalled: true,
         angularDetected: false,
         url: 'https://github.com/alexanderzobnin/grafana-zabbix',
-        details: {
-          dependantPlugins: [],
-          links: [
-            {
-              name: 'GitHub',
-              url: 'https://github.com/alexanderzobnin/grafana-zabbix',
-            },
-            {
-              name: 'Docs',
-              url: 'https://alexanderzobnin.github.io/grafana-zabbix',
-            },
-            {
-              name: 'License',
-              url: 'https://github.com/alexanderzobnin/grafana-zabbix/blob/master/LICENSE',
-            },
-          ],
-          pluginDependencies: [],
-        },
       });
     });
 
