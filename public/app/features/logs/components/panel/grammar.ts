99c22745e22cbdfd23f4006fb4e332744a45e0c0
Matias Chomicki <matyax@gmail.com>
2025-02-25 16:16:44 +0100

    Extend base grammar to support numbers, dates, strings, and fields

--- public/app/features/logs/components/panel/grammar.ts
+++ public/app/features/logs/components/panel/grammar.ts
@@ -1,21 +1,42 @@
 import { Grammar } from 'prismjs';
 
+import { LogListModel } from './processing';
+
 // The Logs grammar is used for highlight in the logs panel
 export const logsGrammar: Grammar = {
-  comment: {
-    pattern: /#.*/,
+  timestamp: /\b\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{1,9})?([+-]\d{2}:?\d{2}|\d{4})?\b/,
+  timestamp_iso: /\b\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,9})?Z\b/,
+  size: {
+    pattern: /\b\d+(\.\d+)?\s*[kKmMGgPpbB]?\s*[bB]?\b/,
+    alias: 'log-token-number',
+    greedy: true,
   },
   quote: {
-    pattern: /"(?:\\.|[^\\"])*"/,
-    alias: 'string',
+    pattern: /(?::\s*|=)"(?:\\.|[^\\"])*"/,
+    alias: 'log-token-string',
     greedy: true,
   },
   backticks: {
     pattern: /`(?:\\.|[^\\`])*`/,
-    alias: 'string',
+    alias: 'log-token-string',
     greedy: true,
   },
-  number: /\b-?\d+((\.\d*)?([eE][+-]?\d+)?)?\b/,
-  operator: /\s?(\|[=~]?|!=?|<(?:=>?|<|>)?|>[>=]?)\s?/i,
-  punctuation: /[{}(),.]/,
+  number: {
+    pattern: /\b\d+(?:\.\d+)?\b/,
+    alias: 'log-token-number',
+  },
+};
+
+export const generateLogGrammar = (log: LogListModel) => {
+  const labels = Object.keys(log.labels).concat(log.fields.map((field) => field.keys[0]));
+  const logGrammar: Grammar = {
+    log_field: {
+      pattern: new RegExp(`\\b(${labels.join('|')})(?:[=:]{1})\\b`, 'g'),
+      alias: 'log-token-label',
+    },
+  };
+  return {
+    ...logGrammar,
+    ...logsGrammar,
+  };
 };
