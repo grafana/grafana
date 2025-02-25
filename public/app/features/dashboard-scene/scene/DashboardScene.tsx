5c792c203e6bcd65c2cfbcf67e3e6065cee16858
Torkel Ödegaard <torkel@grafana.com>
2025-02-25 15:03:29 +0100

    Dashboard: Add new elements logic (#101162)

--- public/app/features/dashboard-scene/scene/DashboardScene.tsx
+++ public/app/features/dashboard-scene/scene/DashboardScene.tsx
@@ -78,7 +78,9 @@ import { isUsingAngularDatasourcePlugin, isUsingAngularPanelPlugin } from './ang
 import { setupKeyboardShortcuts } from './keyboardShortcuts';
 import { DashboardGridItem } from './layout-default/DashboardGridItem';
 import { DefaultGridLayoutManager } from './layout-default/DefaultGridLayoutManager';
+import { addNewRowTo, addNewTabTo } from './layouts-shared/addNew';
 import { DashboardLayoutManager } from './types/DashboardLayoutManager';
+import { LayoutParent } from './types/LayoutParent';
 
 export const PERSISTED_PROPS = ['title', 'description', 'tags', 'editable', 'graphTooltip', 'links', 'meta', 'preload'];
 export const PANEL_SEARCH_VAR = 'systemPanelFilterVar';
@@ -141,7 +143,7 @@ export interface DashboardSceneState extends SceneObjectState {
   editPane: DashboardEditPane;
 }
 
-export class DashboardScene extends SceneObjectBase<DashboardSceneState> {
+export class DashboardScene extends SceneObjectBase<DashboardSceneState> implements LayoutParent {
   static Component = DashboardSceneRenderer;
 
   /**
@@ -593,11 +595,11 @@ export class DashboardScene extends SceneObjectBase<DashboardSceneState> {
   }
 
   public onCreateNewRow() {
-    this.state.body.addNewRow();
+    addNewRowTo(this.state.body);
   }
 
   public onCreateNewTab() {
-    this.state.body.addNewTab();
+    addNewTabTo(this.state.body);
   }
 
   public onCreateNewPanel(): VizPanel {
@@ -611,6 +613,10 @@ export class DashboardScene extends SceneObjectBase<DashboardSceneState> {
   public switchLayout(layout: DashboardLayoutManager) {
     this.setState({ body: layout });
     layout.activateRepeaters?.();
+  }
+
+  public getLayout(): DashboardLayoutManager {
+    return this.state.body;
   }
 
   /**
