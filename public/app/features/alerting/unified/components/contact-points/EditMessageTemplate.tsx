deeb3c51276ee3ab08ae5224df0d76e14eaea22f
Gilles De Mey <gilles.de.mey@gmail.com>
2025-02-26 11:11:49 +0100

    Alerting: Fix notification templates layout (#101232)

--- public/app/features/alerting/unified/components/contact-points/EditMessageTemplate.tsx
+++ public/app/features/alerting/unified/components/contact-points/EditMessageTemplate.tsx
@@ -2,14 +2,17 @@ import { useParams } from 'react-router-dom-v5-compat';
 
 import { Alert, LoadingPlaceholder } from '@grafana/ui';
 import { EntityNotFound } from 'app/core/components/PageNotFound/EntityNotFound';
+import { t } from 'app/core/internationalization';
 
 import { isNotFoundError } from '../../api/util';
 import { useAlertmanager } from '../../state/AlertmanagerContext';
 import { stringifyErrorLike } from '../../utils/misc';
+import { createRelativeUrl } from '../../utils/url';
 import { withPageErrorBoundary } from '../../withPageErrorBoundary';
 import { AlertmanagerPageWrapper } from '../AlertingPageWrapper';
 import { TemplateForm } from '../receivers/TemplateForm';
 
+import { ActiveTab } from './ContactPoints';
 import { useGetNotificationTemplate } from './useNotificationTemplates';
 
 const notFoundComponent = <EntityNotFound entity="Notification template" />;
@@ -19,7 +22,7 @@ const EditMessageTemplateComponent = () => {
   const templateUid = name ? decodeURIComponent(name) : undefined;
 
   const { selectedAlertmanager } = useAlertmanager();
-  const { currentData, isLoading, error } = useGetNotificationTemplate({
+  const { currentData, isLoading, error, isUninitialized } = useGetNotificationTemplate({
     alertmanager: selectedAlertmanager ?? '',
     uid: templateUid ?? '',
   });
@@ -28,7 +31,7 @@ const EditMessageTemplateComponent = () => {
     return <EntityNotFound entity="Notification template" />;
   }
 
-  if (isLoading) {
+  if (isLoading || isUninitialized) {
     return <LoadingPlaceholder text="Loading template..." />;
   }
 
@@ -51,7 +54,21 @@ const EditMessageTemplateComponent = () => {
 
 function EditMessageTemplate() {
   return (
-    <AlertmanagerPageWrapper navId="receivers" accessType="notification">
+    <AlertmanagerPageWrapper
+      navId="receivers"
+      accessType="notification"
+      pageNav={{
+        id: 'templates',
+        text: t('alerting.notification-templates.edit.title', 'Edit notification template group'),
+        subTitle: t('alerting.notification-templates.edit.subTitle', 'Edit a group of notification templates'),
+        parentItem: {
+          text: t('alerting.common.titles.notification-templates', 'Notification Templates'),
+          url: createRelativeUrl('/alerting/notifications', {
+            tab: ActiveTab.NotificationTemplates,
+          }),
+        },
+      }}
+    >
       <EditMessageTemplateComponent />
     </AlertmanagerPageWrapper>
   );
