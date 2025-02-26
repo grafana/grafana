829bf5b8845a8f9e87f9c9691404b63cad03f713
Matias Chomicki <matyax@gmail.com>
2025-02-26 11:42:44 +0100

    Refactor grammar

--- public/app/features/logs/components/panel/grammar.ts
+++ public/app/features/logs/components/panel/grammar.ts
@@ -4,36 +4,18 @@ import { LogListModel } from './processing';
 
 // The Logs grammar is used for highlight in the logs panel
 export const logsGrammar: Grammar = {
-  timestamp: /\b\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{1,9})?([+-]\d{2}:?\d{2}|\d{4})?\b/,
-  timestamp_iso: /\b\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,9})?Z\b/,
-  size: {
-    pattern: /\b\d+(\.\d+)?\s*[kKmMGgPpbB]?\s*[bB]?\b/,
-    alias: 'log-token-number',
-    greedy: true,
-  },
-  quote: {
-    pattern: /(?::\s*|=)"(?:\\.|[^\\"])*"/,
-    alias: 'log-token-string',
-    greedy: true,
-  },
-  backticks: {
-    pattern: /`(?:\\.|[^\\`])*`/,
-    alias: 'log-token-string',
-    greedy: true,
-  },
-  number: {
-    pattern: /\b\d+(?:\.\d+)?\b/,
-    alias: 'log-token-number',
-  },
+  'log-token-timestamp': /\b\d{4}-\d{2}-\d{2}[T|\s]{1}\d{1,2}:\d{2}:\d{2}(?:[\.]{0,1}\d{0,9})?(?:Z|\+\d{2}:\d{2}|\b)\b/,
+  'log-token-json-key': /"(\b|\B)[\w-]+"(?=\s*:)/gi,
+  'log-token-key': /(\b|\B)[\w_]+(?=\s*=)/gi,
+  'log-token-size': /"\d+\.{0,1}\d*\s*[kKmMGgtTPp]*[bB]{1}"/g,
+  'log-token-string': /"(?!:)(\\?[^'"])*?"(?!:)/g,
+  'log-token-number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
 };
 
 export const generateLogGrammar = (log: LogListModel) => {
   const labels = Object.keys(log.labels).concat(log.fields.map((field) => field.keys[0]));
   const logGrammar: Grammar = {
-    log_field: {
-      pattern: new RegExp(`\\b(${labels.join('|')})(?:[=:]{1})\\b`, 'g'),
-      alias: 'log-token-label',
-    },
+    'log-token-label': new RegExp(`\\b(${labels.join('|')})(?:[=:]{1})\\b`, 'g'),
   };
   return {
     ...logGrammar,
