7b3634421b701d9abc8a91478bd2c839110761ca
Darren Janeczek <darren.janeczek@grafana.com>
2025-02-26 11:09:44 -0500

    fix: mock extension hook

--- public/app/features/query/components/QueryEditorRow.test.tsx
+++ public/app/features/query/components/QueryEditorRow.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { PropsWithChildren } from 'react';
 
 import { DataQueryRequest, dateTime, LoadingState, PanelData, toDataFrame } from '@grafana/data';
+import { setPluginComponentsHook } from '@grafana/runtime';
 import { DataQuery } from '@grafana/schema';
 import { mockDataSource } from 'app/features/alerting/unified/mocks';
 
@@ -33,6 +34,8 @@ function makePretendRequest(requestId: string, subRequests?: DataQueryRequest[])
     // subRequests,
   } as DataQueryRequest;
 }
+
+setPluginComponentsHook(() => ({ components: [], isLoading: false }));
 
 describe('filterPanelDataToQuery', () => {
   const data: PanelData = {
