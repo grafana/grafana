c09b97ae8d4bd7eade8e312f6c12578c3493372a
Marcus Andersson <marcus.andersson@grafana.com>
2025-02-26 12:20:14 +0100

    Refactored page a bit.

--- public/app/features/profile/UserProfileEditPage.tsx
+++ public/app/features/profile/UserProfileEditPage.tsx
@@ -1,31 +1,19 @@
-import { Fragment, useMemo, useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { useMount } from 'react-use';
 
-import { PluginExtensionComponent, PluginExtensionPoints } from '@grafana/data';
-import { selectors } from '@grafana/e2e-selectors';
-import { usePluginComponentExtensions, usePluginComponents } from '@grafana/runtime';
-import { Tab, TabsBar, TabContent, Stack } from '@grafana/ui';
+import { PluginExtensionPoints } from '@grafana/data';
+import { usePluginComponents } from '@grafana/runtime';
+import { Stack } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 import SharedPreferences from 'app/core/components/SharedPreferences/SharedPreferences';
-import { useQueryParams } from 'app/core/hooks/useQueryParams';
-import { t } from 'app/core/internationalization';
 import { StoreState } from 'app/types';
 
 import UserOrganizations from './UserOrganizations';
 import UserProfileEditForm from './UserProfileEditForm';
+import { UserProfileEditTabs } from './UserProfileEditTabs';
 import UserSessions from './UserSessions';
 import { UserTeams } from './UserTeams';
 import { changeUserOrg, initUserProfilePage, revokeUserSession, updateUserProfile } from './state/actions';
-import { string } from 'yargs';
-
-const TAB_QUERY_PARAM = 'tab';
-const GENERAL_SETTINGS_TAB = 'general';
-
-type TabInfo = {
-  id: string;
-  title: string;
-};
 
 export interface OwnProps {}
 
@@ -69,132 +57,29 @@ export function UserProfileEditPage({
   changeUserOrg,
   updateUserProfile,
 }: Props) {
-  const [queryParams, updateQueryParams] = useQueryParams();
-  const tabQueryParam = queryParams[TAB_QUERY_PARAM];
-  const [activeTab, setActiveTab] = useState<string>(
-    typeof tabQueryParam === 'string' ? tabQueryParam : GENERAL_SETTINGS_TAB
-  );
-
   useMount(() => initUserProfilePage());
 
-  const { extensions } = usePluginComponentExtensions({ extensionPointId: PluginExtensionPoints.UserProfileTab });
-
-  const groupedExtensionComponents = extensions.reduce<Record<string, PluginExtensionComponent[]>>((acc, extension) => {
-    const { title } = extension;
-    if (acc[title]) {
-      acc[title].push(extension);
-    } else {
-      acc[title] = [extension];
-    }
-    return acc;
-  }, {});
-
-  const convertExtensionComponentTitleToTabId = (title: string) => title.toLowerCase();
-
-  const showTabs = extensions.length > 0;
-  const tabs: TabInfo[] = [
-    {
-      id: GENERAL_SETTINGS_TAB,
-      title: t('user-profile.tabs.general', 'General'),
-    },
-    ...Object.keys(groupedExtensionComponents).map((title) => ({
-      id: convertExtensionComponentTitleToTabId(title),
-      title,
-    })),
-  ];
-
-  const UserProfile = () => (
-    <Stack direction="column" gap={2}>
-      <UserProfileEditForm updateProfile={updateUserProfile} isSavingUser={isUpdating} user={user} />
-      <SharedPreferences resourceUri="user" preferenceType="user" />
-      <Stack direction="column" gap={6}>
-        <UserTeams isLoading={teamsAreLoading} teams={teams} />
-        <UserOrganizations isLoading={orgsAreLoading} setUserOrg={changeUserOrg} orgs={orgs} user={user} />
-        <UserSessions isLoading={sessionsAreLoading} revokeUserSession={revokeUserSession} sessions={sessions} />
-      </Stack>
-    </Stack>
-  );
-
-  const UserProfileWithTabs = () => (
-    <div data-testid={selectors.components.UserProfile.extensionPointTabs}>
-      <Stack direction="column" gap={2}>
-        <TabsBar>
-          {tabs.map(({ id, title }) => {
-            return (
-              <Tab
-                key={id}
-                label={title}
-                active={activeTab === id}
-                onChangeTab={() => {
-                  setActiveTab(id);
-                  updateQueryParams({ [TAB_QUERY_PARAM]: id });
-                }}
-                data-testid={selectors.components.UserProfile.extensionPointTab(id)}
-              />
-            );
-          })}
-        </TabsBar>
-        <TabContent>
-          {activeTab === GENERAL_SETTINGS_TAB && <UserProfile />}
-          {Object.entries(groupedExtensionComponents).map(([title, pluginExtensionComponents]) => {
-            const tabId = convertExtensionComponentTitleToTabId(title);
-
-            if (activeTab === tabId) {
-              return (
-                <Fragment key={tabId}>
-                  {pluginExtensionComponents.map(({ component: Component }, index) => (
-                    <Component key={`${tabId}-${index}`} />
-                  ))}
-                </Fragment>
-              );
-            }
-            return null;
-          })}
-        </TabContent>
-      </Stack>
-    </div>
-  );
+  const { components, isLoading } = usePluginComponents({
+    extensionPointId: PluginExtensionPoints.UserProfileTab,
+  });
 
   return (
     <Page navId="profile/settings">
-      <Page.Contents isLoading={!user}>{showTabs ? <UserProfileWithTabs /> : <UserProfile />}</Page.Contents>
+      <Page.Contents isLoading={!user || isLoading}>
+        <UserProfileEditTabs components={components}>
+          <Stack direction="column" gap={2}>
+            <UserProfileEditForm updateProfile={updateUserProfile} isSavingUser={isUpdating} user={user} />
+            <SharedPreferences resourceUri="user" preferenceType="user" />
+            <Stack direction="column" gap={6}>
+              <UserTeams isLoading={teamsAreLoading} teams={teams} />
+              <UserOrganizations isLoading={orgsAreLoading} setUserOrg={changeUserOrg} orgs={orgs} user={user} />
+              <UserSessions isLoading={sessionsAreLoading} revokeUserSession={revokeUserSession} sessions={sessions} />
+            </Stack>
+          </Stack>
+        </UserProfileEditTabs>
+      </Page.Contents>
     </Page>
   );
 }
 
 export default connector(UserProfileEditPage);
-
-type useProfileTabeExtensionsResult = {
-  components: Record<string, React.ComponentType[]>;
-  tabs: TabInfo[];
-  isLoading: boolean;
-};
-
-function useProfileTabeExtensions(): useProfileTabeExtensionsResult {
-  const { components, isLoading } = usePluginComponents({
-    extensionPointId: PluginExtensionPoints.UserProfileTab,
-  });
-
-  const grouped: Record<string, React.ComponentType[]> = {};
-
-  for (const component of components) {
-    const { title } = component.meta;
-  }
-
-  return useMemo(() => {
-    const grouped = components.reduce<Record<string, React.ComponentType[]>>((acc, component) => {
-      const { title } = component.meta;
-      if (acc[title]) {
-        acc[title].push(component);
-      } else {
-        acc[title] = [component];
-      }
-      return acc;
-    }, {});
-
-    return {
-      components: grouped,
-      isLoading,
-    };
-  }, [components, isLoading]);
-}
