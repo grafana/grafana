4e0e398744645b4bf732af97b52154b1f0d97711
Gareth Dawson <email@garethdawson.xyz>
2025-02-26 21:36:46 +0900

    Jaeger: run metadata requests through the backend (#100337)
    * run metadata requests throught the backend
    
    * fix tests
    
    * add tests to backend
    
    * fix lint

--- pkg/tsdb/jaeger/client.go
+++ pkg/tsdb/jaeger/client.go
@@ -60,3 +60,31 @@ func (j *JaegerClient) Services() ([]string, error) {
 	services = response.Data
 	return services, err
 }
+
+func (j *JaegerClient) Operations(s string) ([]string, error) {
+	var response ServicesResponse
+	operations := []string{}
+
+	u, err := url.JoinPath(j.url, "/api/services/", s, "/operations")
+	if err != nil {
+		return operations, backend.DownstreamError(fmt.Errorf("failed to join url: %w", err))
+	}
+
+	res, err := j.httpClient.Get(u)
+	if err != nil {
+		return operations, err
+	}
+
+	defer func() {
+		if err = res.Body.Close(); err != nil {
+			j.logger.Error("Failed to close response body", "error", err)
+		}
+	}()
+
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return operations, err
+	}
+
+	operations = response.Data
+	return operations, err
+}
