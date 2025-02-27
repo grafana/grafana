57a1574f82c73c17078b4ee477b737c84e639eb3
ismail simsek <ismailsimsek09@gmail.com>
2025-02-27 14:59:57 +0100

    split PrometheusMetricsBrowser into small components

--- packages/grafana-prometheus/src/components/metrics-browser/PrometheusMetricsBrowser.test.tsx
+++ packages/grafana-prometheus/src/components/metrics-browser/PrometheusMetricsBrowser.test.tsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import { createTheme } from '@grafana/data';
 
-import PromQlLanguageProvider from '../language_provider';
+import PromQlLanguageProvider from '../../language_provider';
 
 import {
   BrowserProps,
---
+++ packages/grafana-prometheus/src/components/metrics-browser/types.ts
@@ -0,0 +1,47 @@
+import { GrafanaTheme2, TimeRange } from '@grafana/data';
+
+import PromQlLanguageProvider from '../../language_provider';
+
+export interface FacettableValue {
+  name: string;
+  selected?: boolean;
+  details?: string;
+}
+
+export interface SelectableLabel {
+  name: string;
+  selected?: boolean;
+  loading?: boolean;
+  values?: FacettableValue[];
+  hidden?: boolean;
+  facets?: number;
+}
+
+export interface BrowserProps {
+  languageProvider: PromQlLanguageProvider;
+  onChange: (selector: string) => void;
+  theme: GrafanaTheme2;
+  autoSelect?: number;
+  hide?: () => void;
+  lastUsedLabels: string[];
+  storeLastUsedLabels: (labels: string[]) => void;
+  deleteLastUsedLabels: () => void;
+  timeRange?: TimeRange;
+}
+
+export interface BrowserState {
+  labels: SelectableLabel[];
+  labelSearchTerm: string;
+  metricSearchTerm: string;
+  status: string;
+  error: string;
+  validationStatus: string;
+  valueSearchTerm: string;
+  seriesLimit?: string;
+}
+
+export const DEFAULT_SERIES_LIMIT = '40000';
+export const REMOVE_SERIES_LIMIT = 'none';
+export const EMPTY_SELECTOR = '{}';
+export const METRIC_LABEL = '__name__';
+export const LIST_ITEM_SIZE = 25;
