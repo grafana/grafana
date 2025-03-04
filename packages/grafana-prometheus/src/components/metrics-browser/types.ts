54d8e76167046e0fa2683c91414b467371a2b711
ismail simsek <ismailsimsek09@gmail.com>
2025-03-04 01:17:56 +0100

    remove extra new line

--- packages/grafana-prometheus/src/components/metrics-browser/types.ts
+++ packages/grafana-prometheus/src/components/metrics-browser/types.ts
@@ -43,4 +43,3 @@ export const EMPTY_SELECTOR = '{}';
 export const METRIC_LABEL = '__name__';
 export const LIST_ITEM_SIZE = 25;
 export const LAST_USED_LABELS_KEY = 'grafana.datasources.prometheus.browser.labels';
-
