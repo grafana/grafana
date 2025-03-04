42fa0ac9ae5768c267f7f2d1b95165831023f92a
leonorfmartins <leonorfmartins@gmail.com>
2025-03-04 12:33:54 +0100

    Add Dummy BrowserFoldersPage component. Try to wire it with /folder-app route

--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -32,6 +32,14 @@ export function getAppRoutes(): RouteDescriptor[] {
     // In order to make it possible we need to register them first due to how `<Switch>` is evaluating routes. (This will be unnecessary once/when we upgrade to React Router v6 and start using `<Routes>` instead.)
     ...getAppPluginRoutes(),
     {
+      path: '/folder-app',
+      pageClass: 'page-folders',
+      routeName: DashboardRoutes.Folders,
+      component: SafeDynamicImport(
+        () => import(/* webpackChunkName: "FoldersPage" */ 'app/features/browse-dashboards/BrowseFoldersPage')
+      ),
+    },
+    {
       path: '/',
       pageClass: 'page-dashboard',
       routeName: DashboardRoutes.Home,
