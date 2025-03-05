d42d99650e73cadeab956ffea630ff09c731c314
Clarity-89 <homes89@ukr.net>
2025-03-05 14:24:26 +0100

    Do not show provisioned badge for instances

--- public/app/core/components/NestedFolderPicker/FolderRepo.tsx
+++ public/app/core/components/NestedFolderPicker/FolderRepo.tsx
@@ -6,30 +6,9 @@ export interface Props {
 }
 
 export function FolderRepo({ repo }: Props) {
-  if (!repo) {
+  if (!repo || (typeof repo !== 'string' && repo.target === 'instance')) {
     return null;
   }
 
   return <Badge text={'Provisioned'} color={'darkgrey'} />;
-  // const [repoName, icon] = getRepoDetails(repo);
-  //
-  // return (
-  //   <Text color={'secondary'}>
-  //     {' '}
-  //     | <Icon name={icon} /> {repoName}
-  //   </Text>
-  // );
 }
-
-// function getRepoDetails(spec: RepositoryView): [string, IconName] {
-//   switch (spec.type) {
-//     case 'github':
-//       return [spec.title ?? '', 'github'];
-//     case 'local':
-//       return [spec.title ?? '', 'file-blank'];
-//     case 's3':
-//       return [spec.title ?? '', 'cloud'];
-//     default:
-//       return ['', 'question-circle'];
-//   }
-// }
