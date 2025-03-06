8406525737db863a2abec4a5c4af16f61c437837
Sarah Zinger <sarah.zinger@grafana.com>
2025-03-06 12:55:49 -0500

    Debug nil responses with log line

--- pkg/registry/apis/query/query.go
+++ pkg/registry/apis/query/query.go
@@ -419,7 +419,8 @@ func (b *QueryAPIBuilder) convertQueryWithoutExpression(ctx context.Context, req
 		return nil, errors.New("no queries to convert")
 	}
 	if qdr == nil {
-		return nil, errors.New("queryDataResponse is nil")
+		b.log.Debug("unexpected response of nil from datasource", "datasource.type", req.PluginId, "datasource.uid", req.UID, "request", req.Request)
+		return nil, errors.New("unexpected response of nil from datasource")
 	}
 	refID := req.Request.Queries[0].RefID
 	if _, exist := qdr.Responses[refID]; !exist {
