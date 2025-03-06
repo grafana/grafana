b4d2885e7c80cea98d2fcd2f5bdcd14e92a68094
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-03-06 15:56:49 +0000

    Update logic of how we track and report untranslated strings so we can prevent autofixes

--- packages/grafana-eslint-rules/tests/no-untranslated-strings.test.js
+++ packages/grafana-eslint-rules/tests/no-untranslated-strings.test.js
@@ -24,18 +24,30 @@ const ruleTester = new RuleTester();
 ruleTester.run('eslint no-untranslated-strings', noUntranslatedStrings, {
   valid: [
     {
-      code: `<div><Trans>Translated text</Trans></div>`,
+      name: 'Text in Trans component',
+      code: `const Foo = () => <Bar><Trans>Translated text</Trans></Bar>`,
     },
     {
+      name: 'Text in Trans component with whitespace/JSXText elements',
+      code: `const Foo = () => <Bar>
+      <Trans>
+        Translated text
+    </Trans>   </Bar>`,
+    },
+    {
+      name: 'Empty component',
+      code: `<div>     </div>`,
+    },
+    {
+      name: 'Text using t() function',
       code: `<div>{t('translated.key', 'Translated text')}</div>`,
     },
     {
+      name: 'Prop using t() function',
       code: `<div aria-label={t('aria.label', 'Accessible label')} />`,
     },
     {
-      code: `<Button><Trans>Button text</Trans></Button>`,
-    },
-    {
+      name: 'Variable interpolation',
       code: `<div>{variable}</div>`,
     },
   ],
@@ -47,6 +59,7 @@ ruleTester.run('eslint no-untranslated-strings', noUntranslatedStrings, {
     // untranslated text, in a component, in a file location where we can extract a prefix,
     // and it can be fixed
     {
+      name: 'Basic untranslated text in component',
       code: `
 const Foo = () => <div>Untranslated text</div>`,
       filename,
@@ -56,8 +69,17 @@ import { Trans } from 'app/core/internationalization';
 const Foo = () => <div><Trans i18nKey="some-feature.foo.untranslated-text">Untranslated text</Trans></div>`,
     },
 
-    // Fixes medium length strings
     {
+      name: 'Text inside JSXElement, not in a function',
+      code: `const thing = <div>foo</div>`,
+      filename,
+      errors: [{ message: transError }],
+      output: `import { Trans } from 'app/core/internationalization';
+const thing = <div><Trans i18nKey="some-feature.thing.foo">foo</Trans></div>`,
+    },
+
+    {
+      name: 'Fixes medium length strings',
       code: `
 const Foo = () => <div>This is a longer string that we will translate</div>`,
       filename,
@@ -67,8 +89,8 @@ import { Trans } from 'app/core/internationalization';
 const Foo = () => <div><Trans i18nKey="some-feature.foo.longer-string-translate">This is a longer string that we will translate</Trans></div>`,
     },
 
-    // Fixes short strings
     {
+      name: 'Fixes short strings with many words',
       code: `
 const Foo = () => <div>lots of sho rt word s to be filt ered</div>`,
       filename,
@@ -78,8 +100,19 @@ import { Trans } from 'app/core/internationalization';
 const Foo = () => <div><Trans i18nKey="some-feature.foo.lots-of-sho-rt-word-s">lots of sho rt word s to be filt ered</Trans></div>`,
     },
 
-    // Fixes strings in JSX in props
     {
+      name: 'expression',
+      code: `
+const foo = <>hello</>`,
+      filename,
+      errors: [{ message: transError }],
+      output: `
+import { Trans } from 'app/core/internationalization';
+const foo = <><Trans i18nKey="some-feature.foo.hello">hello</Trans></>`,
+    },
+
+    {
+      name: 'Fixes strings in JSX in props',
       code: `
 const Foo = () => <div><TestingComponent someProp={<>Test</>} /></div>`,
       filename,
@@ -89,8 +122,8 @@ import { Trans } from 'app/core/internationalization';
 const Foo = () => <div><TestingComponent someProp={<><Trans i18nKey="some-feature.foo.test">Test</Trans></>} /></div>`,
     },
 
-    // Fixes and uses ID from attribute if exists
     {
+      name: 'Fixes and uses ID from attribute if exists',
       code: `
 import { t } from 'app/core/internationalization';
 const Foo = () => <div id="someid" title="foo"/>`,
@@ -101,8 +134,8 @@ import { t } from 'app/core/internationalization';
 const Foo = () => <div id="someid" title={t("some-feature.foo.someid-title-foo", "foo")}/>`,
     },
 
-    // Fixes correctly when import already exists (<Trans>)
     {
+      name: 'Fixes correctly when Trans import already exists',
       code: `
 import { Trans } from 'app/core/internationalization';
 const Foo = () => <div>Untranslated text</div>`,
@@ -113,8 +146,8 @@ import { Trans } from 'app/core/internationalization';
 const Foo = () => <div><Trans i18nKey="some-feature.foo.untranslated-text">Untranslated text</Trans></div>`,
     },
 
-    // Fixes correctly when import already exists (t())
     {
+      name: 'Fixes correctly when t() import already exists',
       code: `
 import { t } from 'app/core/internationalization';
 const Foo = () => <div title="foo" />`,
@@ -125,8 +158,8 @@ import { t } from 'app/core/internationalization';
 const Foo = () => <div title={t("some-feature.foo.title-foo", "foo")} />`,
     },
 
-    // Fixes correctly when import already exists but it needs to add t() method
     {
+      name: 'Fixes correctly when import exists but needs to add t()',
       code: `
 import { Trans } from 'app/core/internationalization';
 const Foo = () => <div title="foo" />`,
@@ -137,8 +170,8 @@ import { Trans, t } from 'app/core/internationalization';
 const Foo = () => <div title={t("some-feature.foo.title-foo", "foo")} />`,
     },
 
-    // Fixes correctly with a Class component
     {
+      name: 'Fixes correctly with a Class component',
       code: `
 class Foo extends React.Component {
   render() {
@@ -156,8 +189,8 @@ class Foo extends React.Component {
 }`,
     },
 
-    // Fixes basic prop case
     {
+      name: 'Fixes basic prop case',
       code: `
 const Foo = () => <div title="foo" />`,
       filename,
@@ -167,8 +200,8 @@ import { t } from 'app/core/internationalization';
 const Foo = () => <div title={t("some-feature.foo.title-foo", "foo")} />`,
     },
 
-    // Fixes prop case with double quotes in value
     {
+      name: 'Fixes prop case with double quotes in value',
       code: `
 const Foo = () => <div title='"foo"' />`,
       filename,
@@ -178,8 +211,8 @@ import { t } from 'app/core/internationalization';
 const Foo = () => <div title={t("some-feature.foo.title-foo", '"foo"')} />`,
     },
 
-    // Fixes case with nested functions/components
     {
+      name: 'Fixes case with nested functions/components',
       code: `
 import { Trans } from 'app/core/internationalization';
 const Foo = () => {
@@ -204,69 +237,87 @@ const Foo = () => {
 `,
     },
 
-    // Fixes case with JSX expression next to HTML tag
-    {
-      code: `
-const Foo = () => <div>foo<code>bar</code></div>`,
-      filename,
-      errors: [{ message: transError }, { message: transError }],
-    },
-
     /**
      * UNFIXABLE CASES
      */
 
-    // Can't be fixed because its entirely non-alphanumeric
     {
+      name: 'Multiple untranslated strings in one element',
+      code: `const Foo = () => <div>test {name} example</div>`,
+      filename,
+      errors: [{ message: transError }],
+    },
+
+    {
+      name: 'Cannot fix entirely non-alphanumeric text',
       code: `const Foo = () => <div>-</div>`,
       filename,
       errors: [{ message: transError }],
     },
-
-    // Can't be fixed because it has an expression sibling
     {
-      code: `const Foo = () => <div>Hello {name}</div>`,
+      name: 'Cannot fix text with expression sibling',
+      code: `const Foo = () => <div>{name} Hello</div>`,
       filename,
       errors: [{ message: transError }],
     },
 
-    // Can't be fixed because it contains HTML entities
     {
+      name: 'Cannot fix text with expression sibling in fragment',
+      code: `
+const Foo = () => {
+  const bar = {
+    baz: (<>Hello {name}</>)
+  }
+}`,
+      filename,
+      errors: [{ message: transError }],
+    },
+
+    {
+      name: 'Cannot fix text containing HTML entities',
       code: `const Foo = () => <div>Something&nbsp;</div>`,
       filename,
       errors: [{ message: transError }],
     },
 
-    // Can't be fixed because the text is too long to summarise nicely
     {
+      name: 'Cannot fix text that is too long',
       code: `const Foo = () => <div>This is something with lots of text that we don't want to translate automatically</div>`,
       filename,
       errors: [{ message: transError }],
     },
 
-    // Can't be fixed because it has an HTML sibling
     {
-      code: `const Foo = () => <div>something <code>foo bar</code></div>`,
+      // only: true,
+      name: 'Cannot fix prop text that is too long',
+      code: `const Foo = () => <div title="This is something with lots of text that we don't want to translate automatically" />`,
       filename,
-      errors: [{ message: transError }, { message: transError }],
+      errors: [{ message: propsError }],
     },
 
-    // Can't be fixed because its not in a function/component
     {
-      code: `<div>Untranslated text</div>`,
+      name: 'Cannot fix text with HTML sibling',
+      code: `const Foo = () => <div>something <code>foo bar</code></div>`,
       filename,
       errors: [{ message: transError }],
     },
 
-    // Can't be fixed because its a JSXExpression in an attribute
     {
+      name: 'JSXAttribute not in a function',
+      code: `<div title="foo" />`,
+      filename,
+      errors: [{ message: propsError }],
+    },
+
+    {
+      name: 'Cannot fix JSXExpression in attribute',
       code: `const Foo = () => <div title={"foo"} />`,
       filename,
       errors: [{ message: propsError }],
     },
 
-    // Can't be fixed because it's not in the right directory/location
     {
+      name: 'Cannot fix text outside correct directory location',
       code: `const Foo = () => <div>Untranslated text</div>`,
       filename: 'public/something-else/foo/SomeOtherFile.tsx',
       errors: [{ message: transError }],
