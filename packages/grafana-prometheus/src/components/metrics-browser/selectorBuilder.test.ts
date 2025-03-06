2a5f8528a195b7b6ed06c19e6ee939ffaa781c6f
ismail simsek <ismailsimsek09@gmail.com>
2025-03-06 07:39:25 +0100

    fix the test

--- packages/grafana-prometheus/src/components/metrics-browser/selectorBuilder.test.ts
+++ packages/grafana-prometheus/src/components/metrics-browser/selectorBuilder.test.ts
@@ -26,6 +26,47 @@ describe('selectorBuilder', () => {
       expect(buildSelector('foo', { bar: ['baz'] })).toEqual('foo{bar="baz"}');
     });
 
+    it('skips labels with empty value arrays', () => {
+      expect(buildSelector('metric', { emptyLabel: [], validLabel: ['value'] })).toEqual('metric{validLabel="value"}');
+    });
+
+    it('handles multiple values for a label using regex matcher', () => {
+      expect(buildSelector('', { multi: ['val1', 'val2', 'val3'] })).toEqual('{multi=~"val1|val2|val3"}');
+    });
+
+    it('properly escapes special characters in regex values', () => {
+      expect(buildSelector('', { special: ['val*', 'val.', 'val+'] })).toEqual(
+        '{special=~"val\\\\*|val\\\\.|val\\\\+"}'
+      );
+    });
+
+    it('properly escapes double quotes in exact matcher', () => {
+      expect(buildSelector('', { quoted: ['value"with"quotes'] })).toEqual('{quoted="value\\"with\\"quotes"}');
+    });
+
+    it('properly handles newlines in values', () => {
+      expect(buildSelector('', { newline: ['value\nwith\nnewlines'] })).toEqual('{newline="value\\nwith\\nnewlines"}');
+    });
+
+    it('combines multiple labels properly', () => {
+      expect(
+        buildSelector('', {
+          label1: ['value1'],
+          label2: ['value2'],
+          label3: ['value3'],
+        })
+      ).toEqual('{label1="value1",label2="value2",label3="value3"}');
+    });
+
+    it('combines single and multi-value labels correctly', () => {
+      expect(
+        buildSelector('', {
+          single: ['value'],
+          multi: ['val1', 'val2'],
+        })
+      ).toEqual('{single="value",multi=~"val1|val2"}');
+    });
+
     describe('utf8 support', () => {
       it('metric selector with utf8 metric', () => {
         expect(buildSelector('utf8.metric', {})).toEqual('{"utf8.metric"}');
@@ -55,6 +96,10 @@ describe('selectorBuilder', () => {
             bar: ['baz'],
           })
         ).toEqual('foo{"utf8.label"="uuu",bar="baz"}');
+      });
+
+      it('handles utf8 characters in label values', () => {
+        expect(buildSelector('', { label: ['值', '😀', '你好'] })).toEqual('{label=~"值|😀|你好"}');
       });
     });
   });
