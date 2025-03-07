87decb59f932dac21e9e6782eb28ea9033dbca19
Kyle Brandt <kyle@grafana.com>
2025-03-07 13:26:57 -0500

    SQL Expressions: (WIP/PoC) Work with Alerting

--- pkg/expr/query.go
+++ pkg/expr/query.go
@@ -77,6 +77,7 @@ type ClassicQuery struct {
 // SQLQuery requires the sqlExpression feature flag
 type SQLExpression struct {
 	Expression string `json:"expression" jsonschema:"minLength=1,example=SELECT * FROM A LIMIT 1"`
+	Format     string `json:"format"`
 }
 
 //-------------------------------
