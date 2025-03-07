15b9e704e85d3f8458e860d07c9cb4e75ac13257
Clarity-89 <homes89@ukr.net>
2025-03-07 07:55:39 +0100

    Cleanup

--- public/app/features/provisioning/api/types.ts
+++ public/app/features/provisioning/api/types.ts
@@ -1,18 +1,5 @@
 // TODO: This file should be removed when the swagger docs match the real payloads
 
-export type FileDetails = {
-  path: string;
-  size: string;
-  hash: string;
-};
-
-export type HistoryListResponse = {
-  apiVersion?: string;
-  kind?: string;
-  metadata?: any;
-  items?: HistoryItem[];
-};
-
 export type HistoryItem = {
   ref: string;
   message: string;
