4b3f8069eaa6bf76701f55689f0e4599613a3de3
Drew Slobodnjak <60050885+drew08t@users.noreply.github.com>
2025-03-07 13:16:17 -0800

    Geomap: WebGL for Marker Layer (#95457)
    * Geomap: Implement webgl for marker layer
    
    * Cover rgb color formatting
    
    * Adjust size
    
    * Adjust size and leave todo for adjustment
    
    * Add custom icon example
    
    * Use prepareSVG to handle custom icons
    
    * Apply icon offset
    
    * Add example of text labels for fixed case
    
    * Add text canvas layer and layerGroup
    
    * Use textMarker for text layer style
    
    * Fix geometry calcs
    
    * Only include text layer if needed
    
    * Remove extra line
    
    * Move color functions to utils
    
    * Pass webGL bool to marker maker
    
    * Move webgl symbol style to marker maker
    
    * Get hit detection working for webgl layers
    
    * Improve icon hit detection
    
    * Improve text performance
    
    * Fix types and simplify webgl styling
    
    * Simplify webgl regular shape lookup
    
    * Add comments
    
    * Set fallback color to white
    
    * Fix data fit for initial view
    
    * Update color utils to support colors with alpha
    
    * Add tests for color value function
    
    * Add tests for getWebGLStyle function
    
    * Clean up comments
    
    * Only show text if no symbol is specified
    
    * Remove size multiplier from webgl implementation
    
    * Add size multiplier back for now

--- public/app/plugins/panel/geomap/style/markers.ts
+++ public/app/plugins/panel/geomap/style/markers.ts
@@ -1,4 +1,5 @@
 import { Fill, RegularShape, Stroke, Circle, Style, Icon, Text } from 'ol/style';
+import { LiteralStyle } from 'ol/style/literal';
 import tinycolor from 'tinycolor2';
 
 import { Registry, RegistryItem, textUtil } from '@grafana/data';
@@ -29,6 +30,12 @@ const MarkerShapePath = {
   star: 'img/icons/marker/star.svg',
   cross: 'img/icons/marker/cross.svg',
   x: 'img/icons/marker/x-mark.svg',
+};
+
+const WebGLRegularShapes: Record<string, string> = {
+  circle: 'img/icons/marker/circle.svg',
+  square: 'img/icons/marker/square.svg',
+  triangle: 'img/icons/marker/triangle.svg',
 };
 
 export function getFillColor(cfg: StyleConfigValues) {
@@ -251,7 +258,7 @@ const makers: SymbolMaker[] = [
   },
 ];
 
-async function prepareSVG(url: string, size?: number): Promise<string> {
+async function prepareSVG(url: string, size?: number, backgroundOpacity?: number): Promise<string> {
   return fetch(url, { method: 'GET' })
     .then((res) => {
       return res.text();
@@ -274,6 +281,23 @@ async function prepareSVG(url: string, size?: number): Promise<string> {
       svg.setAttribute('fill', '#fff');
       svg.setAttribute('width', `${width}px`);
       svg.setAttribute('height', `${height}px`);
+
+      // add a mostly transparent circle behind the icon for webGL hit detection
+      // TODO open layers discards fully transparent elements for hit detection
+      if (backgroundOpacity) {
+        const viewBox = svg.getAttribute('viewBox')?.split(' ') ?? [0, 0, width, height];
+        const viewCenterX = Number(viewBox[2]) / 2;
+        const viewCenterY = Number(viewBox[3]) / 2;
+        const circleElement = doc.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circleElement.setAttribute('cx', viewCenterX.toString());
+        circleElement.setAttribute('cy', viewCenterY.toString());
+        circleElement.setAttribute('fill', 'none');
+        circleElement.setAttribute('r', (viewCenterX / 2).toString());
+        circleElement.setAttribute('stroke', `rgba(255,255,255,${backgroundOpacity})`);
+        circleElement.setAttribute('stroke-width', viewCenterX.toString());
+        svg.prepend(circleElement);
+      }
+
       const svgString = new XMLSerializer().serializeToString(svg);
       const svgURI = encodeURIComponent(svgString);
       return `data:image/svg+xml,${svgURI}`;
@@ -293,6 +317,36 @@ export function getMarkerAsPath(shape?: string): string | undefined {
     return marker.aliasIds[0];
   }
   return undefined;
+}
+
+// Returns literal style for WebGL markers
+export async function getWebGLStyle(symbol?: string, opacity?: number): Promise<LiteralStyle> {
+  // style expressions
+  const symbolStyle: LiteralStyle = {
+    symbol: {
+      symbolType: 'circle',
+      size: ['get', 'size', 'number'],
+      color: ['color', ['get', 'red'], ['get', 'green'], ['get', 'blue']],
+      offset: ['array', ['get', 'offsetX'], ['get', 'offsetY']],
+      rotation: ['get', 'rotation', 'number'],
+      opacity: ['get', 'opacity', 'number'],
+    },
+  };
+  // set symbolType and src if a symbol is provided
+  if (symbol && symbolStyle.symbol) {
+    const imageString = 'image';
+    const symbolType = Object.keys(WebGLRegularShapes).find((key) => WebGLRegularShapes[key] === symbol) ?? imageString;
+    symbolStyle.symbol = { ...symbolStyle.symbol, symbolType };
+    if (symbolType === imageString) {
+      const backgroundOpacity = opacity === 0 ? 0 : 0.1 / (opacity ?? 1);
+      symbolStyle.symbol = {
+        ...symbolStyle.symbol,
+        src: await prepareSVG(getPublicOrAbsoluteUrl(symbol), undefined, backgroundOpacity),
+      };
+    }
+  }
+
+  return symbolStyle;
 }
 
 // Will prepare symbols as necessary
