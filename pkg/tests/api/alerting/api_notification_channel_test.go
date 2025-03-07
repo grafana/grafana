b056dba9f5c9b3a8f1cd9bbfe25c7f0f56040122
Matthew Jacobson <matthew.jacobson@grafana.com>
2025-03-07 17:55:33 -0500

    [release-11.0.12] Alerting: Re-encrypt existing contact points before get and patch in legacy config API (#101835)
    * Alerting: Re-encrypt existing contact points before get and patch in legacy config API (#101263)
    
    * Test covering Get+Save interaction for newly secret fields
    
    * Alerting: Re-encrypt existing contact points before get and patch
    
    (cherry picked from commit b73c59547cd77169b077f285c26b19aff97852ca)
    
    * Fix provisioning tests
    
    (cherry picked from commit c4f30ea991372222e4357f007c54b44f1eb8c9c5)
    
    * googlechat url test fix
    
    (cherry picked from commit 0f4af56177d1589540afeac7e6efc9e3a10ea7a6)

--- pkg/tests/api/alerting/api_notification_channel_test.go
+++ pkg/tests/api/alerting/api_notification_channel_test.go
@@ -2168,10 +2168,8 @@ var expAlertmanagerConfigFromAPI = `
             "name": "discord_test",
             "type": "discord",
             "disableResolveMessage": false,
-            "settings": {
-              "url": "http://CHANNEL_ADDR/discord_recv/discord_test"
-            },
-            "secureFields": {}
+            "settings": {},
+            "secureFields": {"url": true}
           }
         ]
       },
@@ -2214,10 +2212,8 @@ var expAlertmanagerConfigFromAPI = `
             "name": "victorops_test",
             "type": "victorops",
             "disableResolveMessage": false,
-            "settings": {
-              "url": "http://CHANNEL_ADDR/victorops_recv/victorops_test"
-            },
-            "secureFields": {}
+            "settings": {},
+            "secureFields": {"url": true}
           }
         ]
       },
