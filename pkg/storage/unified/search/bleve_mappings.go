b275c4de2ad22235b5097660777e216f6910a4f7
owensmallwood <owen.smallwood@grafana.com>
2025-03-07 09:51:15 -0600

    Unified Storage: Remove wildcard queries (#101441)
    * WIP adding custom analyzer so we can do substring search efficiently
    
    * Adding unit tests for title search
    
    * formatting
    
    * adds more title search unit tests
    
    * organize helpers
    
    * fixes issue caused by having two title mappings
    
    * Removes camelcase token filter since it prevents you from searching for a substring of chars and numbers. Adds regression test.
    
    * adds back mapping for title_phrase
    
    * use simple analyzer for input query string so it doesn't filter out english stop words
    
    * ran bleve tests, table snapshots updated
    
    * ignore linter for "unused" test functions. They are very helpful for troubleshooting search. Keeping them.
    
    * only log total hits and query cost if result not nil
    
    * fixes failing test - one more field because there are two title mappings now
    
    * fix test
    
    * fixes test - only take first item when its the title
    
    * Adds separate internal field for title ngram mapping.
    
    When searching with a query, results are sorted by score desc.
    
    When searching without a query, results are sorted by title desc.
    
    Adjusts ngram max to be 10.
    
    Text queries are a disjunction of an exact match, phrase match, and a match. Boosted to have priority in that order.
    
    Adds more unit tests for searching.
    
    * linter
    
    * fix test
    
    * ran tests - generated new test dash json
    
    * sort by title phrase instead of title
    
    * fix test - not relying on /apis/dashboard/search to apply title sorting anymore

--- pkg/storage/unified/search/bleve_mappings.go
+++ pkg/storage/unified/search/bleve_mappings.go
@@ -3,15 +3,22 @@ package search
 import (
 	"github.com/blevesearch/bleve/v2"
 	"github.com/blevesearch/bleve/v2/analysis/analyzer/keyword"
+	"github.com/blevesearch/bleve/v2/analysis/analyzer/standard"
 	"github.com/blevesearch/bleve/v2/mapping"
 
 	"github.com/grafana/grafana/pkg/storage/unified/resource"
 )
 
-func getBleveMappings(fields resource.SearchableDocumentFields) mapping.IndexMapping {
+func getBleveMappings(fields resource.SearchableDocumentFields) (mapping.IndexMapping, error) {
 	mapper := bleve.NewIndexMapping()
+
+	err := RegisterCustomAnalyzers(mapper)
+	if err != nil {
+		return nil, err
+	}
 	mapper.DefaultMapping = getBleveDocMappings(fields)
-	return mapper
+
+	return mapper, nil
 }
 
 func getBleveDocMappings(_ resource.SearchableDocumentFields) *mapping.DocumentMapping {
@@ -24,14 +31,21 @@ func getBleveDocMappings(_ resource.SearchableDocumentFields) *mapping.DocumentM
 	}
 	mapper.AddFieldMappingsAt(resource.SEARCH_FIELD_NAME, nameMapping)
 
+	// for searching by title - uses an edge ngram token filter
+	titleSearchMapping := bleve.NewTextFieldMapping()
+	titleSearchMapping.Analyzer = TITLE_ANALYZER
+	titleSearchMapping.Store = true
+	mapper.AddFieldMappingsAt(resource.SEARCH_FIELD_TITLE_NGRAM, titleSearchMapping)
+
+	// mapping for title to search on words/tokens larger than the ngram size
+	titleWordMapping := bleve.NewTextFieldMapping()
+	titleWordMapping.Analyzer = standard.Name
+	titleWordMapping.Store = true
+	mapper.AddFieldMappingsAt(resource.SEARCH_FIELD_TITLE, titleWordMapping)
+
 	// for filtering/sorting by title full phrase
 	titlePhraseMapping := bleve.NewKeywordFieldMapping()
 	mapper.AddFieldMappingsAt(resource.SEARCH_FIELD_TITLE_PHRASE, titlePhraseMapping)
-
-	// for searching by title
-	// TODO: do we still need this since we have SEARCH_FIELD_TITLE_PHRASE?
-	titleSearchMapping := bleve.NewTextFieldMapping()
-	mapper.AddFieldMappingsAt(resource.SEARCH_FIELD_TITLE, titleSearchMapping)
 
 	descriptionMapping := &mapping.FieldMapping{
 		Name:               resource.SEARCH_FIELD_DESCRIPTION,
