ee7a19725aef3fb7a9be85349b5f424557baff9f
Peter Štibraný <pstibrany@gmail.com>
2025-03-07 15:56:12 +0100

    Add basic test for inserting single object.

--- pkg/util/xorm/xorm_test.go
+++ pkg/util/xorm/xorm_test.go
@@ -9,9 +9,27 @@ import (
 
 func TestNewEngine(t *testing.T) {
 	t.Run("successfully create a new engine", func(t *testing.T) {
-		eng, err := NewEngine("sqlite3", "./test.db")
+		eng, err := NewEngine("sqlite3", ":memory:")
 		require.NoError(t, err)
 		require.NotNil(t, eng)
 		require.Equal(t, "sqlite3", eng.DriverName())
 	})
+
+	t.Run("insert object", func(t *testing.T) {
+		eng, err := NewEngine("sqlite3", ":memory:")
+		require.NoError(t, err)
+		require.NotNil(t, eng)
+
+		_, err = eng.Exec("CREATE TABLE test_struct (id int primary key, comment text)")
+		require.NoError(t, err)
+
+		id, err := eng.Insert(testStruct{Comment: "test comment"})
+		require.NoError(t, err)
+		require.Equal(t, int64(1), id)
+	})
+}
+
+type testStruct struct {
+	ID      int64
+	Comment string
 }
