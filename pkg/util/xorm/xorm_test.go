38225c442bb6ceb29344a459e5c15d3fad5a485a
Peter Štibraný <pstibrany@gmail.com>
2025-03-10 15:59:06 +0100

    Spanner: support for lastInsertId (#101770)
    * Support for returning inserted ID in Spanner.
    * Convert bytes to string when storing them into text column.

--- pkg/util/xorm/xorm_test.go
+++ pkg/util/xorm/xorm_test.go
@@ -5,13 +5,47 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/grafana/pkg/components/simplejson"
 )
 
-func TestNewEngine(t *testing.T) {
-	t.Run("successfully create a new engine", func(t *testing.T) {
-		eng, err := NewEngine("sqlite3", "./test.db")
+func TestBasicOperationsWithSqlite(t *testing.T) {
+	eng, err := NewEngine("sqlite3", ":memory:")
+	require.NoError(t, err)
+	require.NotNil(t, eng)
+	require.Equal(t, "sqlite3", eng.DriverName())
+
+	_, err = eng.Exec("CREATE TABLE test_struct (id int primary key, comment text, json text)")
+	require.NoError(t, err)
+
+	testBasicOperations(t, eng)
+}
+
+func testBasicOperations(t *testing.T, eng *Engine) {
+	t.Run("insert object", func(t *testing.T) {
+		obj := &TestStruct{Comment: "test comment"}
+		_, err := eng.Insert(obj)
 		require.NoError(t, err)
-		require.NotNil(t, eng)
-		require.Equal(t, "sqlite3", eng.DriverName())
+		require.Equal(t, int64(1), obj.Id)
 	})
+
+	t.Run("update object with json field", func(t *testing.T) {
+		sess := eng.NewSession()
+		defer sess.Close()
+
+		obj := &TestStruct{Comment: "new comment"}
+		_, err := sess.Insert(obj)
+		require.NoError(t, err)
+		require.NotZero(t, obj.Id)
+
+		obj.Json = simplejson.MustJson([]byte(`{"test": "test", "key": null}`))
+		_, err = sess.Update(obj)
+		require.NoError(t, err)
+	})
+}
+
+type TestStruct struct {
+	Id      int64
+	Comment string
+	Json    *simplejson.Json
 }
