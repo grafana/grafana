a5fd2825c06e91263537368890d75a38996e460c
joshhunt <josh.hunt@grafana.com>
2025-03-11 09:40:13 +0000

    stricter test regex

--- public/app/core/internationalization/constants.test.ts
+++ public/app/core/internationalization/constants.test.ts
@@ -12,7 +12,7 @@ describe('internationalization constants', () => {
 
   it('should have locale codes including the country code', () => {
     for (const lang of LANGUAGES) {
-      expect(lang.code).toMatch(/^[a-zA-Z]{2}-[a-zA-Z]+$/);
+      expect(lang.code).toMatch(/^[a-z]{2}-[a-zA-Z]+$/);
     }
   });
 
