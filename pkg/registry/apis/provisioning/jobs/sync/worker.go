44caa4ca423cbc73523474aa827ca49853eb59ff
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-03-11 16:16:04 +0100

    Remove commented out lines

--- pkg/registry/apis/provisioning/jobs/sync/worker.go
+++ pkg/registry/apis/provisioning/jobs/sync/worker.go
@@ -146,10 +146,6 @@ func (r *SyncWorker) Process(ctx context.Context, repo repository.Repository, jo
 // start a job and run it
 func (r *SyncWorker) createJob(ctx context.Context, repo repository.ReaderWriter, progress jobs.JobProgressRecorder) (*syncJob, error) {
 	cfg := repo.Config()
-	// if !cfg.Spec.Sync.Enabled {
-	// 	return nil, errors.New("sync is not enabled")
-	// }
-
 	parser, err := r.parsers.GetParser(ctx, repo)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get parser for %s: %w", cfg.Name, err)
