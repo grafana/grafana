324c38be865e081f3dd087bbdff6665716a905bc
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-03-11 13:51:38 +0100

    update go.mod

--- pkg/plugins/repo/client.go
+++ pkg/plugins/repo/client.go
@@ -5,10 +5,12 @@ import (
 	"bufio"
 	"context"
 	"crypto/sha256"
+	"crypto/tls"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -288,4 +290,24 @@ func (c *Client) handleResp(res *http.Response, compatOpts CompatOpts) (io.ReadC
 	}
 
 	return res.Body, nil
+}
+
+func MakeHttpClient(skipTLSVerify bool, timeout time.Duration) http.Client {
+	return http.Client{
+		Timeout: timeout,
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: skipTLSVerify,
+			},
+		},
+	}
 }
