7815945a346b22000135092225a6a20a1346ef18
Ben Sully <ben.sully@grafana.com>
2025-03-12 08:53:20 +0000

    Dashboards: update `@grafana/llm` to v0.13.2 and update usage (#101814)
    This version of the package deprecates the `openai` object in
    favour of the vendor-agnostic `llm` object, so this PR also
    updates the usage of the package to use the new object and
    take advantage of the vendor-agnostic APIs.

--- public/app/features/dashboard/components/GenAI/GenAIDashboardChangesButton.tsx
+++ public/app/features/dashboard/components/GenAI/GenAIDashboardChangesButton.tsx
@@ -1,5 +1,7 @@
 import { useCallback } from 'react';
 
+import { llm } from '@grafana/llm';
+
 import { DashboardModel } from '../../state/DashboardModel';
 
 import { GenAIButton } from './GenAIButton';
@@ -42,7 +44,7 @@ export const GenAIDashboardChangesButton = ({ dashboard, onGenerate, disabled }:
       messages={messages}
       onGenerate={onGenerate}
       temperature={0}
-      model={'gpt-3.5-turbo-16k'}
+      model={llm.Model.BASE}
       eventTrackingSrc={EventTrackingSrc.dashboardChanges}
       toggleTipTitle={'Improve your dashboard changes summary'}
       disabled={disabled}
