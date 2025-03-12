aaf3030fc0c7d52a296fc7b256571b9817d9fa46
Mustafa Sencer Özcan <m.sencerozcan@gmail.com>
2025-03-12 12:16:46 +0100

    test: introduce initial e2e test scenario for unified storage

---
+++ pkg/storage/unified/e2e/unified_test.go
@@ -0,0 +1,57 @@
+package e2e
+
+import (
+	"testing"
+
+	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnifiedStorage(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping e2e test")
+	}
+
+	t.Run("Create folder and dashboard", func(t *testing.T) {
+		s, err := NewUnifiedScenario()
+		require.NoError(t, err)
+
+		postgres := s.NewPostgresService("postgres")
+		require.NoError(t, s.Scenario.StartAndWaitReady(postgres))
+
+		storage := s.NewUnifiedStorageService("storage")
+		require.NoError(t, s.Scenario.StartAndWaitReady(storage))
+
+		grafana := s.NewGrafanaService("grafana", storage.GRPCEndpoint())
+		require.NoError(t, s.Scenario.StartAndWaitReady(grafana))
+
+		const org1 = 1
+		gorg1, err := s.NewGrafanaClient("grafana", org1)
+		require.NoError(t, err)
+
+		folderOrg1, err := gorg1.NewFolder("test-folder-1")
+		require.NoError(t, err)
+
+		const uid = "test-dashboard-1"
+		dashboard := gapi.Dashboard{
+			Model: map[string]interface{}{
+				"uid":   uid,
+				"title": "Test Dashboard 1",
+			},
+			FolderID: folderOrg1.ID,
+		}
+
+		dashboardOrg1, err := gorg1.NewDashboard(dashboard)
+		require.NoError(t, err)
+
+		newDashboard, err := gorg1.DashboardByUID(uid)
+		require.NoError(t, err)
+		require.Equal(t, dashboardOrg1.UID, newDashboard.Model["uid"])
+
+		newFolder, err := gorg1.Folder(folderOrg1.ID)
+		require.NoError(t, err)
+		require.Equal(t, folderOrg1.UID, newFolder.UID)
+
+		require.NoError(t, s.Scenario.Stop(postgres, storage, grafana))
+	})
+}
