8b793bbfcf9821cd8d4ab1e916a117e85c9eb69f
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2025-03-12 14:58:28 +0100

    wip

--- public/app/features/alerting/routes.tsx
+++ public/app/features/alerting/routes.tsx
@@ -228,7 +228,7 @@ export function getAlertingRoutes(cfg = config): RouteDescriptor[] {
       path: '/alerting/import-datasource-managed-rules',
       roles: () => ['Admin'],
       component: importAlertingComponent(
-        () => import(/* webpackChunkName: "AlertingImportFromDSRules"*/ 'app/features/alerting/unified/import-to-gma/ImportFromDSRules')
+        () => import(/* webpackChunkName: "AlertingImportFromDSRules"*/ 'app/features/alerting/unified/components/import-to-gma/ImportFromDSRules')
       ),
     },
     {
