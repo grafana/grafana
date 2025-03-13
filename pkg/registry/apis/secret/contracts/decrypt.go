de66998825155f660e52aeec06d18172d9d76482
Matheus Macabu <macabu.matheus@gmail.com>
2025-03-13 12:21:49 +0100

    Remove decrypt client interface abstraction

--- pkg/registry/apis/secret/contracts/decrypt.go
+++ pkg/registry/apis/secret/contracts/decrypt.go
@@ -5,21 +5,9 @@ import (
 
 	secretv0alpha1 "github.com/grafana/grafana/pkg/apis/secret/v0alpha1"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/xkube"
-	"k8s.io/apiserver/pkg/authorization/authorizer"
 )
 
 // DecryptStorage is the interface for wiring and dependency injection.
 type DecryptStorage interface {
 	Decrypt(ctx context.Context, namespace xkube.Namespace, name string) (secretv0alpha1.ExposedSecureValue, error)
-}
-
-// DecryptClient will have a function-call implementation and a gRPC implementation.
-// The function-call implementation is used by the gRPC server, and the gRPC implementation calls that same gRPC server.
-type DecryptClient interface {
-	Decrypt(ctx context.Context, namespace string, names []string) (map[string]string, error)
-}
-
-// DecryptAuthorizer is implemented as a custom authorizer for the gRPC implementation.
-type DecryptAuthorizer interface {
-	Authorize(ctx context.Context, namespace string, names []string) (authorized authorizer.Decision, reason string, err error)
 }
