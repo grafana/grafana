05fd665eaa3ab105f186ec46f44c789b54c259ad
Levente Balogh <balogh.levente.hu@gmail.com>
2025-03-13 08:11:30 +0100

    feat: pass read-only props to extension components
    This initial commit is used to verify that things are broken in case
    the datasource object is not cloned before passed in as a prop.

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1831,6 +1831,15 @@ var (
 			Owner:        grafanaPluginsPlatformSquad,
 			FrontendOnly: false,
 		},
+		{
+			Name:              "extensionsReadOnlyProxy",
+			Description:       "Use proxy-based read-only objects for plugin extensions instead of deep cloning",
+			Stage:             FeatureStageExperimental,
+			Owner:             grafanaPluginsPlatformSquad,
+			HideFromAdminPage: true,
+			HideFromDocs:      true,
+			FrontendOnly:      true,
+		},
 	}
 )
 
