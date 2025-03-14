adc293db9d7383463dea74b3c6ec01a6306329d4
Igor Suleymanov <igor.suleymanov@grafana.com>
2025-03-14 13:58:42 +0200

    Update generated code to use the `MappingType` properly
    Signed-off-by: Igor Suleymanov <igor.suleymanov@grafana.com>

--- apps/dashboard/pkg/apis/dashboard/v2alpha1/dashboard_spec_gen.go
+++ apps/dashboard/pkg/apis/dashboard/v2alpha1/dashboard_spec_gen.go
@@ -407,10 +407,7 @@ func NewDashboardValueMapping() *DashboardValueMapping {
 // For example, you can configure a value mapping so that all instances of the value 10 appear as Perfection! rather than the number.
 // +k8s:openapi-gen=true
 type DashboardValueMap struct {
-	// TODO (@radiohead): Something broke in cog / app SDK codegen
-	// And this is no longer producing valid TS / Go output
-	// type: MappingType & "value"
-	Type string `json:"type"`
+	Type DashboardMappingType `json:"type"`
 	// Map with <value_to_match>: ValueMappingResult. For example: { "10": { text: "Perfection!", color: "green" } }
 	Options map[string]DashboardValueMappingResult `json:"options"`
 }
@@ -418,9 +415,24 @@ type DashboardValueMap struct {
 // NewDashboardValueMap creates a new DashboardValueMap object.
 func NewDashboardValueMap() *DashboardValueMap {
 	return &DashboardValueMap{
-		Type: "value",
+		Type: DashboardMappingTypeValue,
 	}
 }
+
+// Supported value mapping types
+// `value`: Maps text values to a color or different display text and color. For example, you can configure a value mapping so that all instances of the value 10 appear as Perfection! rather than the number.
+// `range`: Maps numerical ranges to a display text and color. For example, if a value is within a certain range, you can configure a range value mapping to display Low or High rather than the number.
+// `regex`: Maps regular expressions to replacement text and a color. For example, if a value is www.example.com, you can configure a regex value mapping so that Grafana displays www and truncates the domain.
+// `special`: Maps special values like Null, NaN (not a number), and boolean values like true and false to a display text and color. See SpecialValueMatch to see the list of special values. For example, you can configure a special value mapping so that null values appear as N/A.
+// +k8s:openapi-gen=true
+type DashboardMappingType string
+
+const (
+	DashboardMappingTypeValue   DashboardMappingType = "value"
+	DashboardMappingTypeRange   DashboardMappingType = "range"
+	DashboardMappingTypeRegex   DashboardMappingType = "regex"
+	DashboardMappingTypeSpecial DashboardMappingType = "special"
+)
 
 // Result used as replacement with text and color when the value matches
 // +k8s:openapi-gen=true
@@ -444,10 +456,7 @@ func NewDashboardValueMappingResult() *DashboardValueMappingResult {
 // For example, if a value is within a certain range, you can configure a range value mapping to display Low or High rather than the number.
 // +k8s:openapi-gen=true
 type DashboardRangeMap struct {
-	// TODO (@radiohead): Something broke in cog / app SDK codegen
-	// And this is no longer producing valid TS / Go output
-	// type: MappingType & "range"
-	Type string `json:"type"`
+	Type DashboardMappingType `json:"type"`
 	// Range to match against and the result to apply when the value is within the range
 	Options DashboardV2alpha1RangeMapOptions `json:"options"`
 }
@@ -455,7 +464,7 @@ type DashboardRangeMap struct {
 // NewDashboardRangeMap creates a new DashboardRangeMap object.
 func NewDashboardRangeMap() *DashboardRangeMap {
 	return &DashboardRangeMap{
-		Type:    "range",
+		Type:    DashboardMappingTypeRange,
 		Options: *NewDashboardV2alpha1RangeMapOptions(),
 	}
 }
@@ -464,10 +473,7 @@ func NewDashboardRangeMap() *DashboardRangeMap {
 // For example, if a value is www.example.com, you can configure a regex value mapping so that Grafana displays www and truncates the domain.
 // +k8s:openapi-gen=true
 type DashboardRegexMap struct {
-	// TODO (@radiohead): Something broke in cog / app SDK codegen
-	// And this is no longer producing valid TS / Go output
-	// type: MappingType & "regex"
-	Type string `json:"type"`
+	Type DashboardMappingType `json:"type"`
 	// Regular expression to match against and the result to apply when the value matches the regex
 	Options DashboardV2alpha1RegexMapOptions `json:"options"`
 }
@@ -475,7 +481,7 @@ type DashboardRegexMap struct {
 // NewDashboardRegexMap creates a new DashboardRegexMap object.
 func NewDashboardRegexMap() *DashboardRegexMap {
 	return &DashboardRegexMap{
-		Type:    "regex",
+		Type:    DashboardMappingTypeRegex,
 		Options: *NewDashboardV2alpha1RegexMapOptions(),
 	}
 }
@@ -485,17 +491,14 @@ func NewDashboardRegexMap() *DashboardRegexMap {
 // For example, you can configure a special value mapping so that null values appear as N/A.
 // +k8s:openapi-gen=true
 type DashboardSpecialValueMap struct {
-	// TODO (@radiohead): Something broke in cog / app SDK codegen
-	// And this is no longer producing valid TS / Go output
-	// type: MappingType & "special"
-	Type    string                                  `json:"type"`
+	Type    DashboardMappingType                    `json:"type"`
 	Options DashboardV2alpha1SpecialValueMapOptions `json:"options"`
 }
 
 // NewDashboardSpecialValueMap creates a new DashboardSpecialValueMap object.
 func NewDashboardSpecialValueMap() *DashboardSpecialValueMap {
 	return &DashboardSpecialValueMap{
-		Type:    "special",
+		Type:    DashboardMappingTypeSpecial,
 		Options: *NewDashboardV2alpha1SpecialValueMapOptions(),
 	}
 }
