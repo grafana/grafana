f8c28d4b3ed5dce061b20c6e3534bfae42cb30be
Bogdan Matei <bogdan.matei@grafana.com>
2025-03-14 16:20:10 +0200

    Changes

--- public/app/features/dashboard-scene/scene/new-toolbar/actions/ExportDashboardButton.tsx
+++ public/app/features/dashboard-scene/scene/new-toolbar/actions/ExportDashboardButton.tsx
@@ -1,6 +1,63 @@
-import ExportButton from '../../../sharing/ExportButton/ExportButton';
+import { css } from '@emotion/css';
+import { useCallback, useState } from 'react';
+
+import { selectors as e2eSelectors } from '@grafana/e2e-selectors';
+import { locationService } from '@grafana/runtime';
+import { Button, ButtonGroup, Dropdown, useStyles2 } from '@grafana/ui';
+import { t, Trans } from 'app/core/internationalization';
+import { getTrackingSource, shareDashboardType } from 'app/features/dashboard/components/ShareModal/utils';
+
+import ExportMenu from '../../../sharing/ExportButton/ExportMenu';
+import { DashboardInteractions } from '../../../utils/interactions';
 import { ToolbarActionProps } from '../types';
 
-export const ExportDashboardButton = ({ dashboard }: ToolbarActionProps) => (
-  <ExportButton dashboard={dashboard} iconOnly />
-);
+const newExportButtonSelector = e2eSelectors.pages.Dashboard.DashNav.NewExportButton;
+
+export const ExportDashboardButton = ({ dashboard }: ToolbarActionProps) => {
+  const styles = useStyles2(getStyles);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onMenuClick = useCallback((isOpen: boolean) => {
+    setIsOpen(isOpen);
+  }, []);
+
+  const MenuActions = () => <ExportMenu dashboard={dashboard} />;
+
+  return (
+    <ButtonGroup data-testid={newExportButtonSelector.container} className={styles.container}>
+      <Button
+        data-testid={newExportButtonSelector.container}
+        size="sm"
+        tooltip={t('export.menu.export-as-json-tooltip', 'Export')}
+        variant="secondary"
+        onClick={() => {
+          locationService.partial({ shareView: shareDashboardType.export });
+
+          DashboardInteractions.sharingCategoryClicked({
+            item: shareDashboardType.export,
+            shareResource: getTrackingSource(),
+          });
+        }}
+      >
+        <Trans i18nKey="export.menu.export-as-json-label">Export</Trans>
+      </Button>
+      <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={onMenuClick}>
+        <Button
+          aria-label={t('export.menu.export-as-json-tooltip', 'Export')}
+          data-testid={newExportButtonSelector.arrowMenu}
+          size="sm"
+          icon={isOpen ? 'angle-up' : 'angle-down'}
+          variant="secondary"
+        />
+      </Dropdown>
+    </ButtonGroup>
+  );
+};
+
+function getStyles() {
+  return {
+    container: css({
+      gap: 1,
+    }),
+  };
+}
--- public/app/features/dashboard-scene/scene/new-toolbar/actions/MakeDashboardEditableButton.tsx
+++ public/app/features/dashboard-scene/scene/new-toolbar/actions/MakeDashboardEditableButton.tsx
@@ -1,36 +1,22 @@
-import { css } from '@emotion/css';
-
-import { GrafanaTheme2 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import { Button, useStyles2 } from '@grafana/ui';
+import { Button } from '@grafana/ui';
 import { t, Trans } from 'app/core/internationalization';
 import { playlistSrv } from 'app/features/playlist/PlaylistSrv';
 
 import { ToolbarActionProps } from '../types';
 
-export const MakeDashboardEditableButton = ({ dashboard }: ToolbarActionProps) => {
-  const styles = useStyles2(getStyles);
-
-  return (
-    <Button
-      disabled={playlistSrv.state.isPlaying}
-      onClick={() => {
-        dashboard.onEnterEditMode();
-        dashboard.setState({ editable: true, meta: { ...dashboard.state.meta, canEdit: true } });
-      }}
-      tooltip={t('dashboard.toolbar.enter-edit-mode.tooltip', 'This dashboard was marked as read only')}
-      className={styles.button}
-      variant="secondary"
-      size="sm"
-      data-testid={selectors.components.NavToolbar.editDashboard.editButton}
-    >
-      <Trans i18nKey="dashboard.toolbar.enter-edit-mode.label">Make editable</Trans>
-    </Button>
-  );
-};
-
-export const getStyles = (theme: GrafanaTheme2) => ({
-  button: css({
-    margin: theme.spacing(0, 0.5),
-  }),
-});
+export const MakeDashboardEditableButton = ({ dashboard }: ToolbarActionProps) => (
+  <Button
+    disabled={playlistSrv.state.isPlaying}
+    onClick={() => {
+      dashboard.onEnterEditMode();
+      dashboard.setState({ editable: true, meta: { ...dashboard.state.meta, canEdit: true } });
+    }}
+    tooltip={t('dashboard.toolbar.enter-edit-mode.tooltip', 'This dashboard was marked as read only')}
+    variant="secondary"
+    size="sm"
+    data-testid={selectors.components.NavToolbar.editDashboard.editButton}
+  >
+    <Trans i18nKey="dashboard.toolbar.enter-edit-mode.label">Make editable</Trans>
+  </Button>
+);
