f29ae87f73dcc022e0a7471df4f8d057fea9b2de
Jack Westbrook <jack.westbrook@gmail.com>
2025-03-14 11:53:48 +0100

    test(datasourcesrv): fix failing tests due to mocks

--- public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/reducer.test.tsx
+++ public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/reducer.test.tsx
@@ -48,9 +48,29 @@ const thresholdExpression: AlertQuery<ExpressionQuery> = {
   },
 };
 
+const ds1 = {
+  id: 1,
+  uid: 'c8eceabb-0275-4108-8f03-8f74faf4bf6d',
+  type: 'prometheus',
+  name: 'gdev-prometheus',
+  meta: {
+    alerting: true,
+    info: {
+      logos: {
+        small: 'http://example.com/logo.png',
+      },
+    },
+  },
+  jsonData: {},
+  access: 'proxy',
+};
+
 jest.mock('@grafana/runtime', () => ({
   ...jest.requireActual('@grafana/runtime'),
-  getDataSourceSrv: getDataSourceSrv,
+  getDataSourceSrv: () => ({
+    getList: () => [ds1],
+    getInstanceSettings: () => ds1,
+  }),
 }));
 
 const alertQuery: AlertQuery = {
