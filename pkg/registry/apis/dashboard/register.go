e6f23eac0c6014f0d8ef4404e5eb8fb448b409ec
Jean-Philippe Quéméner <jeanphilippe.quemener@grafana.com>
2025-03-16 20:16:14 +0100

    pass nil to legacy storage

--- pkg/registry/apis/dashboard/register.go
+++ pkg/registry/apis/dashboard/register.go
@@ -264,7 +264,7 @@ func (b *DashboardsAPIBuilder) storageForVersion(
 	storage := map[string]rest.Storage{}
 	apiGroupInfo.VersionedResourcesStorageMap[dashboards.GroupVersion().Version] = storage
 
-	legacyStore, err := b.legacy.NewStore(dashboards, opts.Scheme, opts.OptsGetter, b.reg)
+	legacyStore, err := b.legacy.NewStore(dashboards, opts.Scheme, opts.OptsGetter, nil)
 	if err != nil {
 		return err
 	}
