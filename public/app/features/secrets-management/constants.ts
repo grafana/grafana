36f781130dfb84c6acaad334aa95cb357e1d82f5
Matheus Macabu <macabu.matheus@gmail.com>
2025-03-17 10:23:00 +0100

    SecretsManagement: update allowed decrypters constant (and default keeper)

--- public/app/features/secrets-management/constants.ts
+++ public/app/features/secrets-management/constants.ts
@@ -1,10 +1,10 @@
 // @see https://github.com/grafana/grafana-enterprise/blob/secret-service/feature-branch/src/pkg/extensions/secret/decrypt/allow_list.go
-export const ALLOWED_SECRET_DECRYPTERS = [
-  'k6.grafana.app/secret-decrypter-thing',
-  'synthetic-monitoring.grafana.app/secret-decrypter-thing',
-];
+export const ALLOWED_SECRET_DECRYPTERS = {
+  'k6': 'actor_k6',
+  'Synthetic Monitoring': 'actor_synthetic-monitoring',
+};
 
-export const MOCKED_SECRET_KEEPER = 'my-keeper-1';
+export const MOCKED_SECRET_KEEPER = 'kp-default-sql';
 
 export const MOCKED_FILTER_OPTIONS = [
   { label: 'All', value: 'all' },
