4b970c0ddb9ec72665abb7aee8b62567204f53de
Clarity-89 <homes89@ukr.net>
2025-03-17 14:22:10 +0200

    More copy fixes

--- public/app/features/provisioning/Shared/TokenPermissionsInfo.tsx
+++ public/app/features/provisioning/Shared/TokenPermissionsInfo.tsx
@@ -4,30 +4,32 @@ export function TokenPermissionsInfo() {
   return (
     <ControlledCollapse collapsible label="Access Token Permissions">
       <div>
-        To create a new Access Token, navigate to{' '}
+        To create a new access token, navigate to{' '}
         <TextLink external href="https://github.com/settings/personal-access-tokens/new">
-          Personal Access Tokens
+          Personal access tokens
         </TextLink>{' '}
       </div>
 
-      <p>Select the appropriate owner and repository. Then expand Repository permissions, granting</p>
+      <p>Select the appropriate owner and repository. Then expand repository permissions, granting</p>
       <table style={{ fontSize: '2em' }}>
-        <tr>
-          <td>Content</td>
-          <td>Read and write</td>
-        </tr>
-        <tr>
-          <td>Metadata</td>
-          <td>Read only</td>
-        </tr>
-        <tr>
-          <td>Pull requests&nbsp;&nbsp;</td>
-          <td>Read and write</td>
-        </tr>
-        <tr>
-          <td>Webhooks</td>
-          <td>Read and write &nbsp;&nbsp;</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Content</td>
+            <td>Read and write</td>
+          </tr>
+          <tr>
+            <td>Metadata</td>
+            <td>Read only</td>
+          </tr>
+          <tr>
+            <td>Pull requests&nbsp;&nbsp;</td>
+            <td>Read and write</td>
+          </tr>
+          <tr>
+            <td>Webhooks</td>
+            <td>Read and write &nbsp;&nbsp;</td>
+          </tr>
+        </tbody>
       </table>
     </ControlledCollapse>
   );
