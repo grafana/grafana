98030bf4ce37f194c0576b2031e58cfde0505287
Elliot Kirk <elliot.kirk@grafana.com>
2025-03-17 15:21:51 -0700

    addressing feedback

--- packages/grafana-ui/src/components/ShareTimeRangeButton/ShareTimeRangeButton.tsx
+++ packages/grafana-ui/src/components/ShareTimeRangeButton/ShareTimeRangeButton.tsx
@@ -2,13 +2,14 @@ import { css } from '@emotion/css';
 import { useRef, useState } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
+import { AppEvents } from '@grafana/data';
+import { getAppEvents } from '@grafana/runtime';
 
 import { copyText } from '../../utils/clipboard';
 import { absoluteTimeRangeURL } from '../../utils/time';
 import { useStyles2 } from '../../themes';
 import { t, Trans } from '../../utils/i18n';
 import { Button, ButtonGroup, ButtonProps } from '../Button';
-import { ClipboardButton } from '../ClipboardButton/ClipboardButton';
 import { Dropdown } from '../Dropdown/Dropdown';
 import { Menu } from '../Menu/Menu';
 import { MenuItemElement } from '../Menu/MenuItem';
@@ -43,72 +44,71 @@ export function ShareTimeRangeButton({ collapsed, url, fromParam, toParam }: Pro
   const [isOpen, setIsOpen] = useState(false);
   const styles = useStyles2(getStyles);
 
-  const relativeUrlRef = useRef<null | MenuItemElement>(null);
-  const absoluteUrlRef = useRef<null | MenuItemElement>(null);
+  const relativeUrlRef = useRef<MenuItemElement>(null);
+  const absoluteUrlRef = useRef<MenuItemElement>(null);
+
+  const clickHandler = (text: string, ref: React.RefObject<MenuItemElement>) => {
+    copyText(text, ref);
+    getAppEvents().publish({
+      type: AppEvents.alertSuccess.name,
+      payload: t('grafana-ui.toolbar.copy-link-success', 'URL copied to clipboard'),
+    });
+    setIsOpen(false);
+  };
 
   const menu = (
     <Menu>
-      <Menu.Group label={t('explore.toolbar.copy-links-normal-category', 'Normal URL links')}>
-        <Menu.Item
-          key="copy-url-relative"
-          label={t('explore.toolbar.copy-link', 'Copy URL')}
-          icon="link"
-          onClick={() => copyText(window.location.href, relativeUrlRef)}
-          ref={relativeUrlRef}
-        />
-      </Menu.Group>
-      <Menu.Group
-        label={t('explore.toolbar.copy-links-absolute-category', 'Time-sync URL links (share with time range intact)')}
-      >
-        <Menu.Item
-          key="copy-url"
-          label={t('explore.toolbar.copy-link-abs-time', 'Copy absolute URL')}
-          icon="clock-nine"
-          onClick={() => copyText(absoluteTimeRangeURL({ url, fromParam, toParam }), absoluteUrlRef)}
-          ref={absoluteUrlRef}
-        />
-      </Menu.Group>
+      <Menu.Item
+        key="copy-url-relative"
+        label={t('grafana-ui.toolbar.copy-link', 'Copy URL')}
+        icon="link"
+        onClick={() => clickHandler(url ?? window.location.href, relativeUrlRef)}
+        ref={relativeUrlRef}
+      />
+      <Menu.Item
+        key="copy-url"
+        label={t('grafana-ui.toolbar.copy-link-abs-time', 'Copy absolute URL')}
+        icon="clock-nine"
+        onClick={() => clickHandler(absoluteTimeRangeURL({ url, fromParam, toParam }), absoluteUrlRef)}
+        ref={absoluteUrlRef}
+      />
     </Menu>
   );
 
   return (
-    <ButtonGroup>
-      <ClipboardButton
-        className={styles.copy}
-        variant="secondary"
-        size="md"
-        icon="share-alt"
-        tooltip={t('explore.toolbar.copy-link-abs-time', 'Copy absolute URL')}
-        getText={() => absoluteTimeRangeURL()}
-      >
-        <span className={collapsed ? styles.collapsed : styles.shareText}>
-          <Trans i18nKey="explore.toolbar.copy-shortened-link-label">Share</Trans>
-        </span>
-      </ClipboardButton>
-      <Dropdown overlay={menu} placement="bottom-start" onVisibleChange={() => setIsOpen(!isOpen)}>
-        <Button variant="secondary" size="md" icon={isOpen ? 'angle-down' : 'angle-up'} />
-      </Dropdown>
-    </ButtonGroup>
+    <Dropdown overlay={menu} placement="bottom-start" onVisibleChange={() => setIsOpen(!isOpen)}>
+      <ButtonGroup>
+        <Button
+          className={styles.copy}
+          variant="secondary"
+          size="md"
+          icon="share-alt"
+          tooltip={t('grafana-ui.toolbar.copy-link-abs-time', 'Copy absolute URL')}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span className={collapsed ? styles.collapsed : styles.shareText}>
+            <Trans i18nKey="grafana-ui.toolbar.copy-shortened-link-label">Share</Trans>
+          </span>
+        </Button>
+        <Button variant="secondary" size="md" icon={isOpen ? 'angle-up' : 'angle-down'} />
+      </ButtonGroup>
+    </Dropdown>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
   copy: css({
-    marginLeft: 'auto',
-    marginRight: '0',
-    padding: '0 8px',
+    marginRight: `${theme.spacing(0)}`,
+    padding: `${theme.spacing(0, 1)}`,
     svg: css({
-      marginRight: '0',
+      marginRight: `${theme.spacing(0)}`,
     }),
   }),
   collapsed: css({
-    marginLeft: '8px',
+    marginLeft: `${theme.spacing(1)}`,
     display: 'none',
   }),
   shareText: css({
-    marginLeft: '8px',
-    '@media screen and (max-width: 1040px)': css({
-      display: 'none', // won't be displayed on screens below or equal to 1040px width
-    }),
+    marginLeft: `${theme.spacing(1)}`,
   }),
 });
