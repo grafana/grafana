9491fa18957cff5ae7ca986d82d567d66fc402cd
Alexander Akhmetov <me@alx.cx>
2025-03-18 11:15:55 +0100

    Alerting: Add an index to alert_rule_version table on (rule_org_id, rule_uid) (#102347)

--- pkg/services/sqlstore/migrations/migrations.go
+++ pkg/services/sqlstore/migrations/migrations.go
@@ -149,4 +149,6 @@ func (oss *OSSMigrations) AddMigration(mg *Migrator) {
 	ualert.AddAlertRuleGuidMigration(mg)
 
 	ualert.AddAlertRuleMissingSeriesEvalsToResolve(mg)
+
+	ualert.AddAlertRuleVersionUIDIndex(mg)
 }
