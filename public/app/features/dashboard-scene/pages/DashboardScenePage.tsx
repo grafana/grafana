edc3c19cf4ec44b5f782ed6d23de0043d5344cea
Alex Khomenko <Clarity-89@users.noreply.github.com>
2025-03-18 08:55:25 +0200

    Provisioning: Add dashboard preview banner (#102305)
    * Provisioned: Add dashboard preview banner
    
    * Add translations
    
    * Add banner to the dashboard page
    
    * Add translations
    
    * Add feature toggle

--- public/app/features/dashboard-scene/pages/DashboardScenePage.tsx
+++ public/app/features/dashboard-scene/pages/DashboardScenePage.tsx
@@ -14,6 +14,7 @@ import { DashboardPageRouteParams, DashboardPageRouteSearchParams } from 'app/fe
 import { DashboardRoutes } from 'app/types';
 
 import { DashboardPrompt } from '../saving/DashboardPrompt';
+import { DashboardPreviewBanner } from '../saving/provisioned/DashboardPreviewBanner';
 
 import { getDashboardScenePageStateManager } from './DashboardScenePageStateManager';
 
@@ -76,6 +77,7 @@ export function DashboardScenePage({ route, queryParams, location }: Props) {
 
   return (
     <UrlSyncContextProvider scene={dashboard} updateUrlOnInit={true} createBrowserHistorySteps={true}>
+      <DashboardPreviewBanner queryParams={queryParams} route={route.routeName} slug={slug} path={path} />
       <dashboard.Component model={dashboard} key={dashboard.state.key} />
       <DashboardPrompt dashboard={dashboard} />
     </UrlSyncContextProvider>
