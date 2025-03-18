f7597e680e51723c6c607db78469bee89f71ee97
Ivan Ortega <ivanortegaalba@gmail.com>
2025-03-18 15:16:36 +0100

    Fix tests and remove folder dependencies

--- public/app/features/dashboard/api/v2.ts
+++ public/app/features/dashboard/api/v2.ts
@@ -42,7 +42,8 @@ export class K8sDashboardV2API
 
       if (
         dashboard.status?.conversion?.failed &&
-        (dashboard.status.conversion.error === 'v1alpha1' || dashboard.status.conversion.error === 'v0alpha1')
+        (dashboard.status.conversion.storedVersion === 'v1alpha1' ||
+          dashboard.status.conversion.storedVersion === 'v0alpha1')
       ) {
         throw new DashboardVersionError(dashboard.status.conversion.storedVersion, dashboard.status.conversion.error);
       }
