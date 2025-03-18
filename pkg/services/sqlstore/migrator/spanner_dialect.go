b8c1a74e412baba71dbdb0c2929a137568f09797
Georges Chaudy <chaudyg@gmail.com>
2025-03-18 11:38:35 +0100

    use engine.db.query

--- pkg/services/sqlstore/migrator/spanner_dialect.go
+++ pkg/services/sqlstore/migrator/spanner_dialect.go
@@ -15,7 +15,6 @@ import (
 	"github.com/googleapis/gax-go/v2"
 	spannerdriver "github.com/googleapis/go-sql-spanner"
 	"github.com/grafana/dskit/concurrency"
-	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -174,7 +173,7 @@ func (s *SpannerDialect) CleanDB(engine *xorm.Engine) error {
 
 	// Collect all DROP statements.
 	var statements []string
-	changeStreams, err := s.findChangeStreams(context.Background(), engine)
+	changeStreams, err := s.findChangeStreams(engine)
 	if err != nil {
 		return err
 	}
@@ -343,37 +342,20 @@ func (s *SpannerDialect) UnionDistinct() string {
 	return "UNION DISTINCT"
 }
 
-func (s *SpannerDialect) findChangeStreams(ctx context.Context, engine *xorm.Engine) ([]string, error) {
-	dsn := engine.Dialect().DataSourceName()
-	cfg, err := spannerdriver.ExtractConnectorConfig(dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	opts := SpannerConnectorConfigToClientOptions(cfg)
-	client, err := spanner.NewClient(ctx, dsn, opts...)
-	if err != nil {
-		return nil, err
-	}
-	defer client.Close()
-
+func (s *SpannerDialect) findChangeStreams(engine *xorm.Engine) ([]string, error) {
 	var result []string
 	query := `SELECT c.CHANGE_STREAM_NAME
 	FROM INFORMATION_SCHEMA.CHANGE_STREAMS AS C
 	WHERE C.CHANGE_STREAM_CATALOG=''
 	AND C.CHANGE_STREAM_SCHEMA=''`
-
-	iter := client.ReadOnlyTransaction().Query(ctx, spanner.NewStatement(query))
-	for {
-		r, err := iter.Next()
-		if errors.Is(err, iterator.Done) {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-		name := ""
-		if err := r.Columns(&name); err != nil {
+	rows, err := engine.DB().Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
 			return nil, err
 		}
 		result = append(result, name)
