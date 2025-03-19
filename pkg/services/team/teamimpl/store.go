ac690a7c6ef5e5b605aa60b26f0974dddfc96a2a
Mihai Doarna <mihai.doarna@grafana.com>
2025-03-19 10:48:11 +0200

    fix sql select syntax

--- pkg/services/team/teamimpl/store.go
+++ pkg/services/team/teamimpl/store.go
@@ -558,7 +558,7 @@ func (ss *xormStore) getTeamMembers(ctx context.Context, query *team.GetTeamMemb
 			user.email,
 			user.name,
 			user.login,
-			user.uid AS user_uid,
+			user.uid as "user_uid",
 			team_member.external,
 			team_member.permission,
 			user_auth.auth_module,
