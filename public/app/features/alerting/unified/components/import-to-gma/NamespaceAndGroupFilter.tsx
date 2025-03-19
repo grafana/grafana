79073f0ebb91dafa9438855cd56068784322cffb
Gilles De Mey <gilles.de.mey@gmail.com>
2025-03-19 16:28:45 +0100

    Add collapsible section for import rule options

--- public/app/features/alerting/unified/components/import-to-gma/NamespaceAndGroupFilter.tsx
+++ public/app/features/alerting/unified/components/import-to-gma/NamespaceAndGroupFilter.tsx
@@ -3,7 +3,7 @@ import { useEffect, useMemo } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
 import { GrafanaTheme2, SelectableValue } from '@grafana/data';
-import { Field, Stack, Text, VirtualizedSelect, useStyles2 } from '@grafana/ui';
+import { Field, Stack, VirtualizedSelect, useStyles2 } from '@grafana/ui';
 import { t } from 'app/core/internationalization';
 import { RulerRulesConfigDTO } from 'app/types/unified-alerting-dto';
 
@@ -45,71 +45,68 @@ export const NamespaceAndGroupFilter = ({ rulesSourceName }: Props) => {
   );
 
   return (
-    <Stack direction="column" gap={2}>
-      <Text variant="h4">{t('alerting.import-to-gma.optional-filters', 'Optional filters')}</Text>
-      <Stack direction="row" gap={2}>
-        <Field
-          data-testid="namespace-picker"
-          label={t('alerting.import-to-gma.namespace.label', 'Namespace')}
-          // Disable translations as we don't intend to use this dropdown longterm,
-          // so avoiding us adding translations for the sake of it
-          // eslint-disable-next-line @grafana/no-untranslated-strings
-          description={t('alerting.import-to-gma.namespace.description', 'Type to search for an existing namespace')}
-          error={errors.namespace?.message}
-          invalid={!!errors.namespace?.message}
-        >
-          <Controller
-            render={({ field: { onChange, ref, ...field } }) => (
-              <VirtualizedSelect
-                {...field}
-                className={style.input}
-                onChange={(value) => {
-                  setValue('ruleGroup', ''); //reset if namespace changes
-                  onChange(value.value);
-                }}
-                options={namespaceOptions}
-                width={42}
-                isLoading={isLoading}
-                disabled={isLoading}
-              />
-            )}
-            name="namespace"
-            control={control}
-          />
-        </Field>
-        <Field
-          data-testid="group-picker"
-          label={t('alerting.import-to-gma.group.label', 'Group')}
-          // Disable translations as we don't intend to use this dropdown longterm,
-          // so avoiding us adding translations for the sake of it
-          // eslint-disable-next-line @grafana/no-untranslated-strings
-          description={t(
-            'alerting.import-to-gma.group.description',
-            'Type to search for an existing group or create a new one'
+    <Stack direction="row" gap={2}>
+      <Field
+        data-testid="namespace-picker"
+        label={t('alerting.import-to-gma.namespace.label', 'Namespace')}
+        // Disable translations as we don't intend to use this dropdown longterm,
+        // so avoiding us adding translations for the sake of it
+        // eslint-disable-next-line @grafana/no-untranslated-strings
+        description={t('alerting.import-to-gma.namespace.description', 'Type to search for an existing namespace')}
+        error={errors.namespace?.message}
+        invalid={!!errors.namespace?.message}
+      >
+        <Controller
+          render={({ field: { onChange, ref, ...field } }) => (
+            <VirtualizedSelect
+              {...field}
+              className={style.input}
+              onChange={(value) => {
+                setValue('ruleGroup', ''); //reset if namespace changes
+                onChange(value.value);
+              }}
+              options={namespaceOptions}
+              width={42}
+              isLoading={isLoading}
+              disabled={isLoading}
+            />
           )}
-          error={errors.ruleGroup?.message}
-          invalid={!!errors.ruleGroup?.message}
-        >
-          <Controller
-            render={({ field: { ref, ...field } }) => (
-              <VirtualizedSelect
-                {...field}
-                allowCustomValue
-                options={groupOptions}
-                width={42}
-                onChange={(value) => {
-                  setValue('ruleGroup', value.value ?? '');
-                }}
-                className={style.input}
-                isLoading={isLoading}
-                disabled={isLoading}
-              />
-            )}
-            name="ruleGroup"
-            control={control}
-          />
-        </Field>
-      </Stack>
+          name="namespace"
+          control={control}
+        />
+      </Field>
+      <Field
+        data-testid="group-picker"
+        label={t('alerting.import-to-gma.group.label', 'Group')}
+        // Disable translations as we don't intend to use this dropdown longterm,
+        // so avoiding us adding translations for the sake of it
+        // eslint-disable-next-line @grafana/no-untranslated-strings
+        description={t(
+          'alerting.import-to-gma.group.description',
+          'Type to search for an existing group or create a new one'
+        )}
+        error={errors.ruleGroup?.message}
+        invalid={!!errors.ruleGroup?.message}
+      >
+        <Controller
+          render={({ field: { ref, ...field } }) => (
+            <VirtualizedSelect
+              {...field}
+              allowCustomValue
+              options={groupOptions}
+              width={42}
+              onChange={(value) => {
+                setValue('ruleGroup', value.value ?? '');
+              }}
+              className={style.input}
+              isLoading={isLoading}
+              disabled={isLoading}
+            />
+          )}
+          name="ruleGroup"
+          control={control}
+        />
+      </Field>
     </Stack>
   );
 };
