e2ee3addb180a5fb59b288c32e20dee12f6e63bd
Peter Štibraný <pstibrany@gmail.com>
2025-03-19 14:16:20 +0100

    Speed up truncation in mysql, postgres and sqlite in integration tests. (#102439)
    Speed up truncation in sqlite, mysql and postgres integration tests.

--- pkg/services/sqlstore/migrator/sqlite_dialect.go
+++ pkg/services/sqlstore/migrator/sqlite_dialect.go
@@ -103,7 +103,7 @@ func (db *SQLite3) CleanDB(engine *xorm.Engine) error {
 // TruncateDBTables deletes all data from all the tables and resets the sequences.
 // A special case is the dashboard_acl table where we keep the default permissions.
 func (db *SQLite3) TruncateDBTables(engine *xorm.Engine) error {
-	tables, err := engine.DBMetas()
+	tables, err := engine.Dialect().GetTables()
 	if err != nil {
 		return err
 	}
