efa714962af269df8db046677b6e47893f370bb3
Matias Chomicki <matyax@gmail.com>
2025-03-19 16:12:39 +0100

    LogsPanel: separate scroll-keeping situations

--- public/app/plugins/panel/logs/LogsPanel.tsx
+++ public/app/plugins/panel/logs/LogsPanel.tsx
@@ -143,7 +143,7 @@ export const LogsPanel = ({
   const [panelData, setPanelData] = useState(data);
   const dataSourcesMap = useDatasourcesFromTargets(panelData.request?.targets);
   // Prevents the scroll position to change when new data from infinite scrolling is received
-  const keepScrollPositionRef = useRef(false);
+  const keepScrollPositionRef = useRef<null | 'infinite-scroll' | 'user'>(null);
   let closeCallback = useRef<() => void>();
   const { eventBus, onAddAdHocFilter } = usePanelContext();
 
@@ -290,7 +290,8 @@ export const LogsPanel = ({
 
   useLayoutEffect(() => {
     if (!logsContainerRef.current || !scrollElement || keepScrollPositionRef.current) {
-      keepScrollPositionRef.current = false;
+      keepScrollPositionRef.current =
+        keepScrollPositionRef.current === 'infinite-scroll' ? null : keepScrollPositionRef.current;
       return;
     }
     /**
@@ -377,13 +378,13 @@ export const LogsPanel = ({
         return;
       }
       // Signal to keep the user scroll position
-      keepScrollPositionRef.current = true;
+      keepScrollPositionRef.current = 'user';
       const atTheBottom = scrollElement.scrollHeight - scrollElement.scrollTop - scrollElement.clientHeight === 0;
       // Except when the user resets the scroll to the original position depending on the sort direction
       if (scrollElement.scrollTop === 0 && !isAscending) {
-        keepScrollPositionRef.current = false;
+        keepScrollPositionRef.current = null;
       } else if (atTheBottom && isAscending) {
-        keepScrollPositionRef.current = false;
+        keepScrollPositionRef.current = null;
       }
     }
     scrollElement?.addEventListener('scroll', handleScroll);
@@ -415,7 +416,7 @@ export const LogsPanel = ({
         loadingRef.current = false;
       }
 
-      keepScrollPositionRef.current = true;
+      keepScrollPositionRef.current = 'infinite-scroll';
       setPanelData({
         ...panelData,
         series: newSeries,
