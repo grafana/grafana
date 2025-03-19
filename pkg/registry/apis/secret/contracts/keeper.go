38d14c2ea044de017bf98c5f3463997fa10ce36e
Matheus Macabu <macabu.matheus@gmail.com>
2025-03-19 15:21:57 +0100

    SecretsManager: add domain errors to contracts
    Co-authored-by: PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
    Co-authored-by: Dana Axinte <53751979+dana-axinte@users.noreply.github.com>
    Co-authored-by: Michael Mandrus <michael.mandrus@grafana.com>

--- pkg/registry/apis/secret/contracts/keeper.go
+++ pkg/registry/apis/secret/contracts/keeper.go
@@ -2,12 +2,18 @@ package contracts
 
 import (
 	"context"
+	"errors"
 
 	secretv0alpha1 "github.com/grafana/grafana/pkg/apis/secret/v0alpha1"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/xkube"
 	"k8s.io/apimachinery/pkg/apis/meta/internalversion"
 )
 
+var (
+	ErrKeeperNotFound = errors.New("keeper not found")
+)
+
+// KeeperMetadataStorage is the interface for wiring and dependency injection.
 type KeeperMetadataStorage interface {
 	Create(ctx context.Context, keeper *secretv0alpha1.Keeper) (*secretv0alpha1.Keeper, error)
 	Read(ctx context.Context, namespace xkube.Namespace, name string) (*secretv0alpha1.Keeper, error)
