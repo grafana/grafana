905845ff630f1e26f68e361cefa511dfbdec5bf1
ivanahuckova <ivana.huckova@gmail.com>
2025-03-20 18:11:24 +0100

    Fix lint

--- pkg/plugins/manager/client/client_test.go
+++ pkg/plugins/manager/client/client_test.go
@@ -25,38 +25,38 @@ func TestQueryData(t *testing.T) {
 
 	t.Run("Non-empty registry", func(t *testing.T) {
 		tcs := []struct {
-			err           error
-			expectedError error
+			err               error
+			expectedError     error
 			shouldPassThrough bool
 		}{
 			{
-				err:           plugins.ErrPluginUnavailable,
-				expectedError: plugins.ErrPluginUnavailable,
+				err:               plugins.ErrPluginUnavailable,
+				expectedError:     plugins.ErrPluginUnavailable,
 				shouldPassThrough: true,
 			},
 			{
-				err:           plugins.ErrMethodNotImplemented,
-				expectedError: plugins.ErrMethodNotImplemented,
+				err:               plugins.ErrMethodNotImplemented,
+				expectedError:     plugins.ErrMethodNotImplemented,
 				shouldPassThrough: true,
 			},
 			{
-				err:           errors.New("surprise surprise"),
-				expectedError: plugins.ErrPluginRequestFailureErrorBase,
+				err:               errors.New("surprise surprise"),
+				expectedError:     plugins.ErrPluginRequestFailureErrorBase,
 				shouldPassThrough: false,
 			},
 			{
-				err:           context.Canceled,
-				expectedError: plugins.ErrPluginRequestCanceledErrorBase,
+				err:               context.Canceled,
+				expectedError:     plugins.ErrPluginRequestCanceledErrorBase,
 				shouldPassThrough: false,
 			},
 			{
-				err:           plugins.ErrPluginGrpcConnectionUnavailableBase.Errorf("unavailable"),
-				expectedError: plugins.ErrPluginGrpcConnectionUnavailableBase.Errorf("unavailable"),
+				err:               plugins.ErrPluginGrpcConnectionUnavailableBase.Errorf("unavailable"),
+				expectedError:     plugins.ErrPluginGrpcConnectionUnavailableBase.Errorf("unavailable"),
 				shouldPassThrough: true,
 			},
 			{
-				err:           plugins.ErrPluginGrpcResourceExhaustedBase.Errorf("exhausted"),
-				expectedError: plugins.ErrPluginGrpcResourceExhaustedBase.Errorf("exhausted"),
+				err:               plugins.ErrPluginGrpcResourceExhaustedBase.Errorf("exhausted"),
+				expectedError:     plugins.ErrPluginGrpcResourceExhaustedBase.Errorf("exhausted"),
 				shouldPassThrough: true,
 			},
 		}
