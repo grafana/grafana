c36972aedb18b1ed2e9d0fdab9f7e3a4a2087f60
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2025-03-20 09:25:04 +0100

    Plugins: Update some links in plugin jsons due of mistakes in previous PR (#102214)
    * update some links in plugin jsons update backend tests
    
    * fix the test
    
    * fix the test
    
    * Update public/app/plugins/datasource/azuremonitor/plugin.json
    
    Co-authored-by: David Harris <david.harris@grafana.com>
    
    * more docs links added, fix tests
    
    ---------
    
    Co-authored-by: David Harris <david.harris@grafana.com>

--- pkg/services/pluginsintegration/loader/loader_test.go
+++ pkg/services/pluginsintegration/loader/loader_test.go
@@ -89,6 +89,7 @@ func TestLoader_Load(t *testing.T) {
 							},
 							Links: []plugins.InfoLink{
 								{Name: "Raise issue", URL: "https://github.com/grafana/grafana/issues/new"},
+								{Name: "Documentation", URL: "https://grafana.com/docs/grafana/latest/datasources/aws-cloudwatch/"},
 							},
 						},
 						Includes: []*plugins.Includes{
