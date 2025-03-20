bc9d348616e4a7cf2080df07ac46b908840b1408
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-03-20 10:34:39 +0100

    Fix linting issue in test

--- pkg/registry/apis/provisioning/safepath/limit_test.go
+++ pkg/registry/apis/provisioning/safepath/limit_test.go
@@ -1,6 +1,7 @@
 package safepath
 
 import (
+	"errors"
 	"strings"
 	"testing"
 )
@@ -46,7 +47,7 @@ func TestValidatePath(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := ValidatePath(tt.path)
-			if err != tt.wantErr {
+			if !errors.Is(err, tt.wantErr) {
 				t.Errorf("ValidatePath() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
