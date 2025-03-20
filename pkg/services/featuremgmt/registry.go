ab13dd567d81c9d6025796c48e09117e2495db33
Will Assis <william@williamassis.com>
2025-03-20 13:40:04 -0300

    make deprecated to pass tests

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1405,7 +1405,7 @@ var (
 		{
 			Name:              "unifiedStorageSearchPermissionFiltering",
 			Description:       "Enable permission filtering on unified storage search",
-			Stage:             FeatureStageExperimental,
+			Stage:             FeatureStageDeprecated,
 			Owner:             grafanaSearchAndStorageSquad,
 			Expression:        "true",
 			HideFromDocs:      true,
