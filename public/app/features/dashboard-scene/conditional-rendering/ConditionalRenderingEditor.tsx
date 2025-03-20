3af6b72d50ae6c0dfd028a339dbe514dc14c2d76
kay delaney <kay@grafana.com>
2025-03-20 13:15:54 +0000

    ConditionalRenderingEditor: Restructure ConditionalRenderingEditor.tsx

--- public/app/features/dashboard-scene/conditional-rendering/ConditionalRenderingEditor.tsx
+++ public/app/features/dashboard-scene/conditional-rendering/ConditionalRenderingEditor.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/css';
+import { useCallback } from 'react';
 
 import { Button, Dropdown, Menu, Stack, useStyles2 } from '@grafana/ui';
 import { t } from 'app/core/internationalization';
@@ -21,60 +22,7 @@ export function useConditionalRenderingEditor(
   const title = t('dashboard.conditional-rendering.title', 'Display conditions');
 
   return new OptionsPaneCategoryDescriptor({
-    renderTitle: () => {
-      const styles = useStyles2(getStyles);
-
-      return (
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          onClick={(evt) => evt.stopPropagation()}
-        >
-          <h6 className={styles.title}>{title}</h6>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item
-                  label={t('dashboard.conditional-rendering.add.data', 'Data')}
-                  onClick={() => conditionalRendering.addItem(new ConditionalRenderingData({ value: true }))}
-                />
-                <Menu.Item
-                  label={t('dashboard.conditional-rendering.add.interval', 'Interval')}
-                  onClick={() => conditionalRendering.addItem(new ConditionalRenderingInterval({ value: '7d' }))}
-                />
-                <Menu.Item
-                  label={t('dashboard.conditional-rendering.add.variable', 'Variable value')}
-                  onClick={() =>
-                    showConditionalRenderingVariableEditor(conditionalRendering, {
-                      onSave: (name: string, operator: VariableConditionValueOperator, value: string) => {
-                        conditionalRendering.addItem(
-                          new ConditionalRenderingVariable({ value: { name, operator, value } })
-                        );
-                      },
-                    })
-                  }
-                />
-              </Menu>
-            }
-          >
-            <Button
-              aria-label={t('dashboard.conditional-rendering.add.label', 'Add condition')}
-              icon="plus"
-              type="button"
-              fill="text"
-              size="md"
-              variant="secondary"
-              onPointerDown={(evt) => evt.stopPropagation()}
-              onClick={(evt) => {
-                evt.stopPropagation();
-                evt.preventDefault();
-              }}
-            />
-          </Dropdown>
-        </Stack>
-      );
-    },
+    renderTitle: () => <ConditionalRenderingEditor title={title} conditionalRendering={conditionalRendering} />,
     isOpenDefault: true,
     forceOpen: 1,
     title,
@@ -86,6 +34,67 @@ export function useConditionalRenderingEditor(
     })
   );
 }
+
+interface ConditionalRenderingEditorProps {
+  title: string;
+  conditionalRendering: ConditionalRendering;
+}
+
+export const ConditionalRenderingEditor = ({ title, conditionalRendering }: ConditionalRenderingEditorProps) => {
+  const styles = useStyles2(getStyles);
+
+  const addData = useCallback(
+    () => conditionalRendering.addItem(new ConditionalRenderingData({ value: true })),
+    [conditionalRendering]
+  );
+
+  const addInterval = useCallback(
+    () => conditionalRendering.addItem(new ConditionalRenderingInterval({ value: '7d' })),
+    [conditionalRendering]
+  );
+
+  const addVariable = useCallback(
+    () =>
+      showConditionalRenderingVariableEditor(conditionalRendering, {
+        onSave: (name: string, operator: VariableConditionValueOperator, value: string) => {
+          conditionalRendering.addItem(new ConditionalRenderingVariable({ value: { name, operator, value } }));
+        },
+      }),
+    [conditionalRendering]
+  );
+
+  return (
+    <Stack direction="row" justifyContent="space-between" alignItems="center" onClick={(evt) => evt.stopPropagation()}>
+      <h6 className={styles.title}>{title}</h6>
+      <Dropdown
+        overlay={
+          <Menu>
+            <Menu.Item label={t('dashboard.conditional-rendering.add.data', 'Data')} onClick={addData} />
+            <Menu.Item label={t('dashboard.conditional-rendering.add.interval', 'Interval')} onClick={addInterval} />
+            <Menu.Item
+              label={t('dashboard.conditional-rendering.add.variable', 'Variable value')}
+              onClick={addVariable}
+            />
+          </Menu>
+        }
+      >
+        <Button
+          aria-label={t('dashboard.conditional-rendering.add.label', 'Add condition')}
+          icon="plus"
+          type="button"
+          fill="text"
+          size="md"
+          variant="secondary"
+          onPointerDown={(evt) => evt.stopPropagation()}
+          onClick={(evt) => {
+            evt.stopPropagation();
+            evt.preventDefault();
+          }}
+        />
+      </Dropdown>
+    </Stack>
+  );
+};
 
 const getStyles = () => ({
   title: css({
