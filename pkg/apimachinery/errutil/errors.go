07386822b9cc0c3f0fbaabb39deb47a2320ba78a
ivanahuckova <ivana.huckova@gmail.com>
2025-03-24 15:59:53 +0100

    Revert "Improve data source error message when stackID"
    This reverts commit 4bf0a2f7b712e77b9de4655716695a7ee75c183b.

--- pkg/apimachinery/errutil/errors.go
+++ pkg/apimachinery/errutil/errors.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"strings"
 
 	errorsK8s "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -357,9 +356,6 @@ type Error struct {
 	LogLevel LogLevel
 	// Source identifies from where the error originates.
 	Source Source
-	// WithContactSupportErrorMessage indicates whether we should append a message
-	// to the public message advising the user to contact support
-	WithContactSupportErrorMessage bool
 }
 
 // MarshalJSON returns an error, we do not want raw [Error]s being
@@ -462,13 +458,6 @@ func (e Error) Public() PublicError {
 		}
 	}
 
-	if e.WithContactSupportErrorMessage {
-		if !strings.HasSuffix(message, ".") {
-			message += "."
-		}
-		message += " Please contact support if the issue persists."
-	}
-
 	return PublicError{
 		StatusCode: e.Reason.Status().HTTPStatus(),
 		MessageID:  e.MessageID,
@@ -480,10 +469,4 @@ func (e Error) Public() PublicError {
 // Error implements the error interface.
 func (p PublicError) Error() string {
 	return fmt.Sprintf("[%s] %s", p.MessageID, p.Message)
-}
-
-// WithSupportContact creates a new Error with WithContactSupportErrorMessage set to true
-func (e Error) WithContactSupportMessage() Error {
-	e.WithContactSupportErrorMessage = true
-	return e
 }
--- pkg/plugins/backendplugin/grpcplugin/client_v2.go
+++ pkg/plugins/backendplugin/grpcplugin/client_v2.go
@@ -14,7 +14,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/grafana/grafana/pkg/extensions/datasource/dsrunner/pluginmetrics"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/plugins/backendplugin/pluginextensionv2"
 	"github.com/grafana/grafana/pkg/plugins/log"
@@ -174,13 +173,7 @@ func (c *ClientV2) QueryData(ctx context.Context, req *backend.QueryDataRequest)
 		}
 
 		if status.Code(err) == codes.Unavailable {
-			connectionErr := plugins.ErrPluginGrpcConnectionUnavailableBase.Errorf("%v", err)
-			// Check if the plugin is running in a cloud environment
-			_, hasStackId := pluginmetrics.StackIDFromContext(ctx)
-			if hasStackId {
-				return nil,connectionErr.WithContactSupportMessage()
-			}
-			return nil, connectionErr
+			return nil, plugins.ErrPluginGrpcConnectionUnavailableBase.Errorf("%v", err)
 		}
 
 		if status.Code(err) == codes.ResourceExhausted {
