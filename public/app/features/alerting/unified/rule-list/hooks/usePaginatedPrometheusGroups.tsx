2fc67ea6867e72d1102797e568a0a291f33fdea6
Gilles De Mey <gilles.de.mey@gmail.com>
2025-03-25 16:56:47 +0100

    Clean up prometheus alert group imports

--- public/app/features/alerting/unified/rule-list/hooks/usePaginatedPrometheusGroups.tsx
+++ public/app/features/alerting/unified/rule-list/hooks/usePaginatedPrometheusGroups.tsx
@@ -1,9 +1,6 @@
 import { useCallback, useMemo, useState } from 'react';
 
-import type * as GrafanaFlavor from '@grafana/alerting/src/types/grafana/rules/api';
-import type * as VanillaFlavor from '@grafana/alerting/src/types/prometheus/rules/api';
-
-type PrometheusRuleGroup = GrafanaFlavor.PrometheusRuleGroup | VanillaFlavor.PrometheusRuleGroup;
+import { PrometheusRuleGroup } from '@grafana/alerting/src/types';
 
 import { isLoading, useAsync } from '../../hooks/useAsync';
 
