ed003c62d5006d2b57f5cac7492e8b9f86b6b14b
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-03-25 14:58:26 +0100

    Maintain empty folders until next full pulling

--- pkg/registry/apis/provisioning/jobs/sync/worker.go
+++ pkg/registry/apis/provisioning/jobs/sync/worker.go
@@ -334,9 +334,9 @@ func (r *syncJob) applyVersionedChanges(ctx context.Context, repo repository.Ver
 		}
 
 		if err := resources.IsPathSupported(change.Path); err != nil {
-			// Maintain the non-hidden path for empty folders if it's not a deletion or ignore operation
+			// Maintain the non-hidden path if valid until the next full pulling even if folders are empty
 			unhiddenPath := safepath.UnhiddenPath(change.Path)
-			if unhiddenPath != "" && resources.IsPathSupported(unhiddenPath) == nil && change.Action != repository.FileActionDeleted && change.Action != repository.FileActionIgnored {
+			if unhiddenPath != "" && resources.IsPathSupported(unhiddenPath) == nil {
 				if _, err := r.folders.EnsureFolderPathExist(ctx, unhiddenPath); err != nil {
 					return fmt.Errorf("unable to create empty file folder: %w", err)
 				}
