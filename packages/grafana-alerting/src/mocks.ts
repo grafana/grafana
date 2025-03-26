68107af2d61277c08eec64d5b692ad9d46b3e737
Gilles De Mey <gilles.de.mey@gmail.com>
2025-03-26 18:18:15 +0100

    Rename mock namespaces for clarity

--- packages/grafana-alerting/src/mocks.ts
+++ packages/grafana-alerting/src/mocks.ts
@@ -2,9 +2,9 @@ import * as PrometheusAPI from './types/prometheus/rules/api.mocks';
 import * as PrometheusDefinition from './types/prometheus/rules/definitions.mocks';
 
 export default {
-  prometheus: {
+  prometheusMocks: {
     api: PrometheusAPI,
     definitions: PrometheusDefinition,
   },
-  grafana: {},
+  grafanaMocks: {},
 };
