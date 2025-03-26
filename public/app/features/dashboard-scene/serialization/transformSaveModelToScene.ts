b0ae84bf58ce7768e13e86683eebfba17e59e9f2
Oscar Kilhed <oscar.kilhed@grafana.com>
2025-03-26 17:03:10 +0100

    Dashboards: Limit panel title length (#102911)
    Limit panel title length

--- public/app/features/dashboard-scene/serialization/transformSaveModelToScene.ts
+++ public/app/features/dashboard-scene/serialization/transformSaveModelToScene.ts
@@ -323,7 +323,7 @@ export function buildGridItemForPanel(panel: PanelModel): DashboardGridItem {
 
   const vizPanelState: VizPanelState = {
     key: getVizPanelKeyForPanelId(panel.id),
-    title: panel.title,
+    title: panel.title?.substring(0, 5000),
     description: panel.description,
     pluginId: panel.type ?? 'timeseries',
     options: panel.options ?? {},
