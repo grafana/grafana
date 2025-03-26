76d66f81f070cf013518f93611e60d394abec6e7
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-03-25 20:17:56 -0600

    Tests: Fix failing integration test (#102847)

--- pkg/registry/apis/provisioning/resources/client.go
+++ pkg/registry/apis/provisioning/resources/client.go
@@ -8,7 +8,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 
-	dashboard "github.com/grafana/grafana/apps/dashboard/pkg/apis/dashboard/v0alpha1"
+	dashboard "github.com/grafana/grafana/apps/dashboard/pkg/apis/dashboard/v1alpha1"
 	folders "github.com/grafana/grafana/pkg/apis/folder/v0alpha1"
 	iam "github.com/grafana/grafana/pkg/apis/iam/v0alpha1"
 	"github.com/grafana/grafana/pkg/services/apiserver"
