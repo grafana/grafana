6e17197ed62be2962e744caabf3e02749b5aa565
Mariell Hoversholm <mariell.hoversholm@grafana.com>
2025-03-27 08:22:50 +0100

    App Platform(Provisioning): Listen to context cancel ASAP
    We should not write after the context is cancelled. This causes tests to fail as they are trying to clean up. To try to
    accommodate this, we'll need to listen to the context in any loop where we don't immediately get other instructions from
    e.g. trying to do an HTTP request.

--- pkg/registry/apis/provisioning/jobs/sync/worker.go
+++ pkg/registry/apis/provisioning/jobs/sync/worker.go
@@ -266,6 +266,9 @@ func (r *syncJob) applyChanges(ctx context.Context, changes []ResourceFileChange
 	r.progress.SetMessage(ctx, "replicating changes")
 
 	for _, change := range changes {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
 		if err := r.progress.TooManyErrors(); err != nil {
 			return err
 		}
@@ -344,6 +347,9 @@ func (r *syncJob) applyVersionedChanges(ctx context.Context, repo repository.Ver
 	r.progress.SetMessage(ctx, "replicating versioned changes")
 
 	for _, change := range diff {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
 		if err := r.progress.TooManyErrors(); err != nil {
 			return err
 		}
