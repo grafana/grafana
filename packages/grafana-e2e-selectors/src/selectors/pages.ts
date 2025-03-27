33914252854c997345650ee566d47d06a4e7ad77
Scott Lepper <scott.lepper@gmail.com>
2025-03-27 10:38:03 -0400

    Search e2e (#102827)
    * search e2e
    * add github action
    * run test for every ini file in test folder

--- packages/grafana-e2e-selectors/src/selectors/pages.ts
+++ packages/grafana-e2e-selectors/src/selectors/pages.ts
@@ -569,6 +569,9 @@ export const versionedPages = {
     dashboards: {
       '10.2.0': (title: string) => `Dashboard search item ${title}`,
     },
+    toggleView: {
+      [MIN_GRAFANA_VERSION]: 'data-testid radio-button',
+    },
   },
   SaveDashboardAsModal: {
     newName: {
@@ -943,6 +946,11 @@ export const versionedPages = {
       createButton: {
         '10.2.0': 'data-testid new-folder-create-button',
       },
+    },
+  },
+  SearchDashboards: {
+    table: {
+      '10.2.0': 'Search results table',
     },
   },
   Search: {
