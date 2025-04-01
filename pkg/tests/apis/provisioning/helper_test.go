6754781d7b999254790b0575138cc3c5f8b34a32
Ryan McKinley <ryantxu@gmail.com>
2025-04-01 13:43:00 +0300

    Provisioning: Remove unnecessary flags from tests and instructinos (#103165)

--- pkg/tests/apis/provisioning/helper_test.go
+++ pkg/tests/apis/provisioning/helper_test.go
@@ -201,8 +201,6 @@ func runGrafana(t *testing.T, options ...grafanaOption) *provisioningTestHelper 
 		AppModeProduction: false, // required for experimental APIs
 		EnableFeatureToggles: []string{
 			featuremgmt.FlagProvisioning,
-			featuremgmt.FlagGrafanaAPIServerWithExperimentalAPIs,
-			featuremgmt.FlagNestedFolders,
 			featuremgmt.FlagUnifiedStorageSearch,
 			featuremgmt.FlagKubernetesClientDashboardsFolders,
 		},
