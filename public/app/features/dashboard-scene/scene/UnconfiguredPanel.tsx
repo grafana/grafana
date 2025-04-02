dd9cd4823dc2435369b32a9da18bb63b48c25557
Torkel Ödegaard <torkel@grafana.com>
2025-04-02 10:13:07 +0200

    It works now for auto grid as well

--- public/app/features/dashboard-scene/scene/UnconfiguredPanel.tsx
+++ public/app/features/dashboard-scene/scene/UnconfiguredPanel.tsx
@@ -9,23 +9,12 @@ import { Trans, t } from 'app/core/internationalization';
 import { findVizPanelByKey, getVizPanelKeyForPanelId } from '../utils/utils';
 
 import { DashboardScene } from './DashboardScene';
-import { DashboardGridItem } from './layout-default/DashboardGridItem';
 
 export const UNCONFIGURED_PANEL_PLUGIN_ID = '__unconfigured-panel';
 const UnconfiguredPanel = new PanelPlugin(UnconfiguredPanelComp);
 
 function UnconfiguredPanelComp(props: PanelProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const dashboard = window.__grafanaSceneContext;
-
-  if (!(dashboard instanceof DashboardScene)) {
-    throw new Error('DashboardScene not found');
-  }
-
-  const panel = findVizPanelByKey(dashboard, getVizPanelKeyForPanelId(props.id));
-  if (!panel) {
-    throw new Error('Panel not found');
-  }
 
   const onMenuClick = useCallback((isOpen: boolean) => {
     setIsOpen(isOpen);
@@ -36,10 +25,19 @@ function UnconfiguredPanelComp(props: PanelProps) {
   };
 
   const onUseLibraryPanel = () => {
+    const dashboard = window.__grafanaSceneContext;
+
+    if (!(dashboard instanceof DashboardScene)) {
+      throw new Error('DashboardScene not found');
+    }
+
+    const panel = findVizPanelByKey(dashboard, getVizPanelKeyForPanelId(props.id));
+    if (!panel) {
+      throw new Error('Panel not found');
+    }
+
     dashboard.onShowAddLibraryPanelDrawer(panel.getRef());
   };
-
-  const isInsideCustomGrid = panel.parent instanceof DashboardGridItem;
 
   const MenuActions = () => (
     <Menu>
@@ -59,24 +57,14 @@ function UnconfiguredPanelComp(props: PanelProps) {
   return (
     <Stack direction={'row'} alignItems={'center'} height={'100%'} justifyContent={'center'}>
       <Box paddingBottom={2}>
-        {/* Temporary restrict library panel usage to only panels inside custom grid */}
-        {isInsideCustomGrid ? (
-          <ButtonGroup>
-            <Button icon="sliders-v-alt" onClick={onConfigure}>
-              <Trans i18nKey="dashboard.new-panel.configure-button">Configure</Trans>
-            </Button>
-            <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={onMenuClick}>
-              <Button
-                aria-label="dashboard.new-panel.configure-button-menu"
-                icon={isOpen ? 'angle-up' : 'angle-down'}
-              />
-            </Dropdown>
-          </ButtonGroup>
-        ) : (
+        <ButtonGroup>
           <Button icon="sliders-v-alt" onClick={onConfigure}>
             <Trans i18nKey="dashboard.new-panel.configure-button">Configure</Trans>
           </Button>
-        )}
+          <Dropdown overlay={MenuActions} placement="bottom-end" onVisibleChange={onMenuClick}>
+            <Button aria-label="dashboard.new-panel.configure-button-menu" icon={isOpen ? 'angle-up' : 'angle-down'} />
+          </Dropdown>
+        </ButtonGroup>
       </Box>
     </Stack>
   );
