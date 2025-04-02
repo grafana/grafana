17d089d38a75bf3ccebfdf9df81f1d3287a1106b
Dominik Prokop <dominik.prokop@grafana.com>
2025-04-02 16:22:24 +0200

    Schema V2: Use App SDK generated types (#103100)
    * Use App SDK generated types
    
    * Move examples
    
    * Merge fix
    
    * PAth updates

--- public/app/features/dashboard-scene/serialization/layoutSerializers/TabsLayoutSerializer.ts
+++ public/app/features/dashboard-scene/serialization/layoutSerializers/TabsLayoutSerializer.ts
@@ -1,4 +1,7 @@
-import { DashboardV2Spec, TabsLayoutTabKind } from '@grafana/schema/dist/esm/schema/dashboard/v2alpha0';
+import {
+  Spec as DashboardV2Spec,
+  TabsLayoutTabKind,
+} from '@grafana/schema/dist/esm/schema/dashboard/v2alpha1/types.spec.gen';
 
 import { TabItem } from '../../scene/layout-tabs/TabItem';
 import { TabsLayoutManager } from '../../scene/layout-tabs/TabsLayoutManager';
