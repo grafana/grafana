fb02a5a571f541522bd4229dfe87f4815ebfdbb7
Andreas Christou <andreas.christou@grafana.com>
2025-04-02 13:41:26 +0100

    Update comment

--- pkg/tsdb/influxdb/fsql/client.go
+++ pkg/tsdb/influxdb/fsql/client.go
@@ -34,10 +34,8 @@ func newFlightSQLClient(addr string, metadata metadata.MD, secure bool, proxyCli
 		return nil, fmt.Errorf("grpc dial options: %s", err)
 	}
 
-	// If the secure socks proxy is enabled, we use the proxy adddress with
-	// the passthrough scheme. Otherwise, we use the processed address.
-	// This ensures the address is passed directly to the transport
-	// rather than trying to resolve via DNS.
+	// If the secure socks proxy is enabled, we add the passthrough scheme. Otherwise, we use the raw address.
+	// This ensures the address is passed directly to the transport rather than trying to resolve via DNS.
 	if proxyClient.SecureSocksProxyEnabled() {
 		addr = fmt.Sprintf("passthrough:///%s", addr)
 	}
