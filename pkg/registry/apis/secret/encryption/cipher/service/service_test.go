b2c000de63f76b5997788dfee2a7be0be0399d2b
Mariell Hoversholm <mariell.hoversholm@grafana.com>
2025-04-02 10:19:37 +0200

    feat: use GCM by default

--- pkg/registry/apis/secret/encryption/cipher/service/service_test.go
+++ pkg/registry/apis/secret/encryption/cipher/service/service_test.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"context"
 	"testing"
 	"time"
 
@@ -15,8 +14,6 @@ import (
 )
 
 func Test_Service(t *testing.T) {
-	ctx := context.Background()
-
 	usageStats := &usagestats.UsageStatsMock{}
 	settings := &setting.Cfg{
 		SecretsManagement: setting.SecretsManagerSettings{
@@ -34,19 +31,19 @@ func Test_Service(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Run("decrypt empty payload should return error", func(t *testing.T) {
-		_, err := svc.Decrypt(context.Background(), []byte(""), "1234")
+		_, err := svc.Decrypt(t.Context(), []byte(""), "1234")
 		require.Error(t, err)
 
 		assert.Equal(t, "unable to derive encryption algorithm", err.Error())
 	})
 
-	t.Run("encrypt and decrypt with aes-cfb should work", func(t *testing.T) {
-		settings.SecretsManagement.Encryption.Algorithm = cipher.AesCfb
+	t.Run("encrypt and decrypt with aes-gcm should work", func(t *testing.T) {
+		settings.SecretsManagement.Encryption.Algorithm = cipher.AesGcm
 
-		encrypted, err := svc.Encrypt(ctx, []byte("grafana"), "1234")
+		encrypted, err := svc.Encrypt(t.Context(), []byte("grafana"), "1234")
 		require.NoError(t, err)
 
-		decrypted, err := svc.Decrypt(ctx, encrypted, "1234")
+		decrypted, err := svc.Decrypt(t.Context(), encrypted, "1234")
 		require.NoError(t, err)
 
 		assert.Equal(t, []byte("grafana"), decrypted)
@@ -59,7 +56,7 @@ func Test_Service(t *testing.T) {
 		// - 'aes-gcm' as encryption algorithm
 		ciphertext := []byte{42, 89, 87, 86, 122, 76, 87, 100, 106, 98, 81, 42, 48, 99, 55, 50, 51, 48, 83, 66, 20, 99, 47, 238, 61, 44, 129, 125, 14, 37, 162, 230, 47, 31, 104, 70, 144, 223, 26, 51, 180, 17, 76, 52, 36, 93, 17, 203, 99, 158, 219, 102, 74, 173, 74}
 
-		decrypted, err := svc.Decrypt(context.Background(), ciphertext, "1234")
+		decrypted, err := svc.Decrypt(t.Context(), ciphertext, "1234")
 		require.NoError(t, err)
 		assert.Equal(t, []byte("grafana"), decrypted)
 	})
@@ -67,7 +64,7 @@ func Test_Service(t *testing.T) {
 	t.Run("encrypt with aes-gcm should fail", func(t *testing.T) {
 		settings.SecretsManagement.Encryption.Algorithm = cipher.AesGcm
 
-		_, err := svc.Encrypt(ctx, []byte("grafana"), "1234")
+		_, err := svc.Encrypt(t.Context(), []byte("grafana"), "1234")
 		require.Error(t, err)
 	})
 
@@ -78,7 +75,7 @@ func Test_Service(t *testing.T) {
 		// - no encryption algorithm metadata
 		ciphertext := []byte{73, 71, 50, 57, 121, 110, 90, 109, 115, 23, 237, 13, 130, 188, 151, 118, 98, 103, 80, 209, 79, 143, 22, 122, 44, 40, 102, 41, 136, 16, 27}
 
-		decrypted, err := svc.Decrypt(context.Background(), ciphertext, "1234")
+		decrypted, err := svc.Decrypt(t.Context(), ciphertext, "1234")
 		require.NoError(t, err)
 		assert.Equal(t, []byte("grafana"), decrypted)
 	})
