a8b2343eb30e79171fcc2453b1b3dc45fb4248ef
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-04-02 14:41:23 +0200

    PMM-13463 Hide new badge for Drilldown

--- pkg/services/navtree/navtreeimpl/navtree.go
+++ pkg/services/navtree/navtreeimpl/navtree.go
@@ -137,7 +137,7 @@ func (s *ServiceImpl) GetNavTree(c *contextmodel.ReqContext, prefs *pref.Prefere
 			Id:         navtree.NavIDDrilldown,
 			SubTitle:   "Drill down into your data using Grafana's powerful queryless apps",
 			Icon:       "drilldown",
-			IsNew:      true,
+			IsNew:      false,
 			SortWeight: navtree.WeightDrilldown,
 			Url:        s.cfg.AppSubURL + "/drilldown",
 			Children:   drilldownChildNavLinks,
