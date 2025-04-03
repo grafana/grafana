3c91eb73d18afab0e9266afc928b9657772cc0ec
Matias Chomicki <matyax@gmail.com>
2025-04-03 18:55:13 +0200

    ControlledLogRows: create component

---
+++ public/app/features/logs/components/ControlledLogRows.tsx
@@ -0,0 +1,98 @@
+import { css } from '@emotion/css';
+import { useRef } from 'react';
+
+import { AbsoluteTimeRange, CoreApp, EventBus, EventBusSrv, LogsSortOrder, TimeRange } from '@grafana/data';
+
+import { InfiniteScroll } from './InfiniteScroll';
+import { LogRows, Props } from './LogRows';
+import { LogListContextProvider, useLogListContext } from './panel/LogListContext';
+import { LogListControls } from './panel/LogListControls';
+import { config } from '@grafana/runtime';
+
+interface ControlledLogRowsProps extends Omit<Props, 'scrollElement'> {
+  eventBus?: EventBus;
+  loading: boolean;
+  loadMoreLogs?: (range: AbsoluteTimeRange) => void;
+  range: TimeRange;
+}
+
+type LogRowsComponentProps = Omit<
+  ControlledLogRowsProps,
+  'app' | 'dedupStrategy' | 'showTime' | 'logsSortOrder' | 'wrapLogMessage'
+>;
+
+export const ControlledLogRows = ({
+  eventBus = new EventBusSrv(),
+  dedupStrategy,
+  showTime,
+  logsSortOrder,
+  wrapLogMessage,
+  ...rest
+}: ControlledLogRowsProps) => {
+  return (
+    <LogListContextProvider
+      app={rest.app || CoreApp.Unknown}
+      displayedFields={[]}
+      dedupStrategy={dedupStrategy}
+      showControls
+      showTime={showTime}
+      sortOrder={logsSortOrder || LogsSortOrder.Descending}
+      wrapLogMessage={wrapLogMessage}
+    >
+      <LogRowsComponent {...rest} eventBus={eventBus} />
+    </LogListContextProvider>
+  );
+};
+
+const LogRowsComponent = ({ eventBus, loading, loadMoreLogs, range, ...rest }: LogRowsComponentProps) => {
+  const { app, dedupStrategy, showTime, sortOrder, wrapLogMessage } = useLogListContext();
+  const scrollElementRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div className={styles.logRowsContainer}>
+      <div
+        ref={scrollElementRef}
+        className={config.featureToggles.logsInfiniteScrolling ? styles.scrollableLogRows : styles.logRows}
+      >
+        <InfiniteScroll
+          loading={loading}
+          loadMoreLogs={loadMoreLogs}
+          range={range}
+          timeZone={rest.timeZone}
+          rows={rest.logRows ?? []}
+          scrollElement={scrollElementRef.current}
+          sortOrder={sortOrder}
+        >
+          <LogRows
+            {...rest}
+            app={app}
+            dedupStrategy={dedupStrategy}
+            logsSortOrder={sortOrder}
+            scrollElement={scrollElementRef.current}
+            showTime={showTime}
+            wrapLogMessage={wrapLogMessage}
+            renderPreview
+          />
+          );
+        </InfiniteScroll>
+      </div>
+      <LogListControls eventBus={eventBus} />
+    </div>
+  );
+};
+
+const styles = {
+  scrollableLogRows: css({
+    overflowY: 'scroll',
+    width: '100%',
+    maxHeight: '75vh',
+  }),
+  logRows: css({
+    overflowX: 'scroll',
+    overflowY: 'visible',
+    width: '100%',
+  }),
+  logRowsContainer: css({
+    display: 'flex',
+  }),
+};
