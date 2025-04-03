d6cdc465a9d71db5bf1595230845a9438f0f3eb3
Todd Treece <360020+toddtreece@users.noreply.github.com>
2025-04-03 14:16:53 -0400

    Dashboards: Add k8s fallback client

---
+++ pkg/services/dashboards/service/k8s_client.go
@@ -0,0 +1,125 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/otel/attribute"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	dashboardv1alpha1 "github.com/grafana/grafana/apps/dashboard/pkg/apis/dashboard/v1alpha1"
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/infra/tracing"
+	"github.com/grafana/grafana/pkg/services/apiserver"
+	"github.com/grafana/grafana/pkg/services/apiserver/client"
+	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
+	"github.com/grafana/grafana/pkg/services/dashboards"
+	"github.com/grafana/grafana/pkg/services/search/sort"
+	"github.com/grafana/grafana/pkg/services/user"
+	"github.com/grafana/grafana/pkg/setting"
+	"github.com/grafana/grafana/pkg/storage/legacysql/dualwrite"
+	"github.com/grafana/grafana/pkg/storage/unified/resource"
+)
+
+type k8sHandlerFactory func(version string) client.K8sHandler
+
+type k8sHandlerWithFallback struct {
+	client.K8sHandler
+
+	newClientFunc k8sHandlerFactory
+	log           log.Logger
+}
+
+func NewK8sHandlerWithFallback(
+	cfg *setting.Cfg,
+	restConfigProvider apiserver.RestConfigProvider,
+	dashboardStore dashboards.Store,
+	userService user.Service,
+	resourceClient resource.ResourceClient,
+	sorter sort.Service,
+	dual dualwrite.Service,
+	parentLogger log.Logger,
+) client.K8sHandler {
+	newClientFunc := newK8sHandlerFactory(
+		cfg,
+		restConfigProvider,
+		dashboardStore,
+		userService,
+		resourceClient,
+		sorter,
+		dual,
+	)
+	return &k8sHandlerWithFallback{
+		K8sHandler:    newClientFunc(dashboardv1alpha1.VERSION),
+		newClientFunc: newClientFunc,
+	}
+}
+
+func (h *k8sHandlerWithFallback) Get(ctx context.Context, name string, orgID int64, options metav1.GetOptions, subresources ...string) (*unstructured.Unstructured, error) {
+	ctx, span := tracing.Start(ctx, "versionFallbackK8sHandler.Get")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("dashboard.metadata.name", name),
+		attribute.Int64("org.id", orgID),
+		attribute.Bool("fallback", false),
+	)
+
+	span.AddEvent("v1alpha1 Get")
+	result, err := h.K8sHandler.Get(ctx, name, orgID, options, subresources...)
+	if err != nil {
+		return nil, tracing.Error(span, err)
+	}
+
+	failed, storedVersion, conversionErr := getConversionStatus(result)
+	if !failed {
+		// if the conversion did not fail, there is no need to fallback.
+		return result, nil
+	}
+
+	h.log.Debug("falling back to stored version", "name", name, "storedVersion", storedVersion, "conversionErr", conversionErr)
+	span.SetAttributes(
+		attribute.Bool("fallback", true),
+		attribute.String("fallback.stored_version", storedVersion),
+		attribute.String("fallback.conversion_error", conversionErr),
+	)
+
+	span.AddEvent(fmt.Sprintf("%s Get", storedVersion))
+	return h.newClientFunc(storedVersion).Get(ctx, name, orgID, options, subresources...)
+}
+
+func getConversionStatus(obj *unstructured.Unstructured) (failed bool, storedVersion string, conversionErr string) {
+	status, found, _ := unstructured.NestedMap(obj.Object, "status")
+	if !found {
+		return false, "", ""
+	}
+	conversionStatus, found, _ := unstructured.NestedMap(status, "conversion")
+	if !found {
+		return false, "", ""
+	}
+	failed, _, _ = unstructured.NestedBool(conversionStatus, "failed")
+	storedVersion, _, _ = unstructured.NestedString(conversionStatus, "storedVersion")
+	conversionErr, _, _ = unstructured.NestedString(conversionStatus, "error")
+	return failed, storedVersion, conversionErr
+}
+
+func newK8sHandlerFactory(
+	cfg *setting.Cfg,
+	restConfigProvider apiserver.RestConfigProvider,
+	dashboardStore dashboards.Store,
+	userService user.Service,
+	resourceClient resource.ResourceClient,
+	sorter sort.Service,
+	dual dualwrite.Service,
+) k8sHandlerFactory {
+	return func(version string) client.K8sHandler {
+		gvr := schema.GroupVersionResource{
+			Group:    dashboardv1alpha1.GROUP,
+			Version:  version,
+			Resource: "dashboards",
+		}
+		return client.NewK8sHandler(dual, request.GetNamespaceMapper(cfg), gvr, restConfigProvider.GetRestConfig, dashboardStore, userService, resourceClient, sorter)
+	}
+}
