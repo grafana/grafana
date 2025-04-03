c81ca2dd3ce3c5fd602fd54f00388cf1d86a3e77
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2025-04-03 16:44:25 +0200

    fix wrong move in import

--- public/app/features/alerting/unified/components/StateTag.tsx
+++ public/app/features/alerting/unified/components/StateTag.tsx
@@ -1,6 +1,6 @@
 import { css, cx } from '@emotion/css';
-import * as React from 'react';
 import { forwardRef } from 'react';
+import * as React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { useStyles2 } from '@grafana/ui';
