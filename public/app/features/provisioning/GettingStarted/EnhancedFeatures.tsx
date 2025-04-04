8b67456945ce01abb16dae9a9863d639585cc230
Clarity-89 <homes89@ukr.net>
2025-04-04 09:58:56 +0300

    Cleanup

--- public/app/features/provisioning/GettingStarted/EnhancedFeatures.tsx
+++ public/app/features/provisioning/GettingStarted/EnhancedFeatures.tsx
@@ -2,9 +2,8 @@ import { css } from '@emotion/css';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { Box, Stack, Text, LinkButton, useStyles2 } from '@grafana/ui';
-import { Trans, t } from 'app/core/internationalization';
+import { Trans } from 'app/core/internationalization';
 
-import { FeatureCard } from './FeatureCard';
 import { IconCircle } from './IconCircle';
 
 interface EnhancedFeaturesProps {
@@ -17,62 +16,68 @@ export const EnhancedFeatures = ({ hasPublicAccess, hasImageRenderer, onSetupPub
   const style = useStyles2(getStyles);
 
   return (
-    <Box marginTop={2}>
-      <Text variant="h4">
-        <Trans i18nKey="provisioning.enhanced-features.header">Enhance your GitHub experience</Trans>
-      </Text>
-      <Box marginTop={4}>
-        <Stack gap={2}>
-          <FeatureCard
-            title={t(
-              'provisioning.enhanced-features.title-instant-updates-requests-webhooks',
-              'Instant updates and pull requests with webhooks.'
-            )}
-            description={t(
-              'provisioning.enhanced-features.description-instant-updates',
-              'Get instant updates in Grafana as soon as changes are committed. Review and approve changes using pull requests before they go live.'
-            )}
-            icon={
-              <Stack gap={2}>
-                <IconCircle icon="sync" color="blue" />
-                <IconCircle icon="code-branch" color="purple" />
-              </Stack>
-            }
-            action={
-              !hasPublicAccess && (
-                <LinkButton fill="outline" variant="secondary" onClick={onSetupPublicAccess}>
-                  <Trans i18nKey="provisioning.enhanced-features.set-up-public-webhooks">Set up public webhooks</Trans>
-                </LinkButton>
-              )
-            }
-          />
+    <Stack direction="column" gap={2}>
+      <Stack direction="column">
+        <Text variant="h4">
+          <Trans i18nKey="provisioning.enhanced-features.header">Enhance your GitHub experience</Trans>
+        </Text>
+        <Text color="secondary">
+          <Trans i18nKey="provisioning.enhanced-features.description">
+            Get the most out of your GitHub integration with these optional add-ons
+          </Trans>
+        </Text>
+      </Stack>
+      <Stack gap={2} direction="row" height="100%">
+        <Box width="40%" height="100%" padding={2} display="flex" direction="column" gap={2} alignItems="flex-start">
+          <Stack gap={2}>
+            <IconCircle icon="sync" color="blue" />
+            <IconCircle icon="code-branch" color="purple" />
+          </Stack>
+          <Trans i18nKey="provisioning.enhanced-features.title-instant-updates-requests-webhooks">
+            Instant updates and pull requests with webhooks.
+          </Trans>
+          <Box display="flex" flex="1" minHeight="50px">
+            <Text variant="body" color="secondary">
+              <Trans i18nKey="provisioning.enhanced-features.description-instant-updates">
+                Get instant updates in Grafana as soon as changes are committed. Review and approve changes using pull
+                requests before they go live.
+              </Trans>
+            </Text>
+          </Box>
+          {!hasPublicAccess && (
+            <LinkButton fill="outline" variant="secondary" onClick={onSetupPublicAccess}>
+              <Trans i18nKey="provisioning.enhanced-features.set-up-public-webhooks">Set up public webhooks</Trans>
+            </LinkButton>
+          )}
+        </Box>
 
-          <div className={style.separator} />
+        <div className={style.separator} />
 
-          <FeatureCard
-            title={t(
-              'provisioning.enhanced-features.title-visual-previews-in-pull-requests',
-              'Visual previews in pull requests with image rendering'
-            )}
-            description={t(
-              'provisioning.enhanced-features.description-visual-previews-dashboard-updates-directly-requests',
-              'See visual previews of dashboard updates directly in pull requests'
-            )}
-            icon={<IconCircle icon="camera" color="orange" />}
-            action={
-              <LinkButton
-                fill="outline"
-                variant="secondary"
-                href="https://grafana.com/grafana/plugins/grafana-image-renderer/"
-                icon="external-link-alt"
-              >
-                <Trans i18nKey="provisioning.enhanced-features.set-up-image-rendering">Set up image rendering</Trans>
-              </LinkButton>
-            }
-          />
-        </Stack>
-      </Box>
-    </Box>
+        <Box width="40%" height="100%" padding={2} display="flex" direction="column" gap={2} alignItems="flex-start">
+          <IconCircle icon="camera" color="orange" />
+          <Trans i18nKey="provisioning.enhanced-features.title-visual-previews-in-pull-requests">
+            Visual previews in pull requests with image rendering
+          </Trans>
+          <Box display="flex" flex="1" minHeight="50px">
+            <Text variant="body" color="secondary">
+              <Trans i18nKey="provisioning.enhanced-features.description-visual-previews-dashboard-updates-directly-requests">
+                See visual previews of dashboard updates directly in pull requests
+              </Trans>
+            </Text>
+          </Box>
+          {hasImageRenderer && (
+            <LinkButton
+              fill="outline"
+              variant="secondary"
+              href="https://grafana.com/grafana/plugins/grafana-image-renderer/"
+              icon="external-link-alt"
+            >
+              <Trans i18nKey="provisioning.enhanced-features.set-up-image-rendering">Set up image rendering</Trans>
+            </LinkButton>
+          )}
+        </Box>
+      </Stack>
+    </Stack>
   );
 };
 
