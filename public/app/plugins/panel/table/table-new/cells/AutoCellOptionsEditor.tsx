8cd3322ab6ad53d385b419cc8a3f5ce32e38e45d
Leon Sorokin <leeoniya@gmail.com>
2025-04-05 16:29:10 -0500

    revert some stuffs

---
+++ public/app/plugins/panel/table/table-new/cells/AutoCellOptionsEditor.tsx
@@ -0,0 +1,10 @@
+import { TableAutoCellOptions, TableColorTextCellOptions } from '@grafana/schema';
+
+import { TableCellEditorProps } from '../TableCellOptionEditor';
+
+export const AutoCellOptionsEditor = ({
+  cellOptions,
+  onChange,
+}: TableCellEditorProps<TableAutoCellOptions | TableColorTextCellOptions>) => {
+  return <></>;
+};
