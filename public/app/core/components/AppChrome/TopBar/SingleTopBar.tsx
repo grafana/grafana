979d5d3afdf5cb5ac6bdfc11abd6d005a86b50ff
Torkel Ödegaard <torkel@grafana.com>
2025-04-06 13:44:38 +0200

    fix

--- public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
+++ public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
@@ -93,6 +93,7 @@ export const SingleTopBar = memo(function SingleTopBar({
           justifyContent={'flex-end'}
           flex={1}
           data-testid={!showToolbarLevel ? Components.NavToolbar.container : undefined}
+          minWidth={{ xs: 'unset', lg: 0 }}
         >
           <TopSearchBarCommandPaletteTrigger />
           {unifiedHistoryEnabled && !isSmallScreen && <HistoryContainer />}
