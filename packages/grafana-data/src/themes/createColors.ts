f2b229a96e295710f6f1492e34d304c7f4b754b9
Torkel Ödegaard <torkel@grafana.com>
2025-04-07 10:03:48 +0200

    revert color change

--- packages/grafana-data/src/themes/createColors.ts
+++ packages/grafana-data/src/themes/createColors.ts
@@ -107,7 +107,7 @@ class DarkColors implements ThemeColorsBase<Partial<ThemeRichColor>> {
   text = {
     primary: `rgb(${this.whiteBase})`,
     secondary: `rgba(${this.whiteBase}, 0.65)`,
-    disabled: `rgba(${this.whiteBase}, 0.5)`,
+    disabled: `rgba(${this.whiteBase}, 0.6)`,
     link: palette.blueDarkText,
     maxContrast: palette.white,
   };
