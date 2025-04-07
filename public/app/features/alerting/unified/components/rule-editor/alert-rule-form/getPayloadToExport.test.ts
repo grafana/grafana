82558bfa8ddd60f3245907d9e665aa44babda9b0
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-04-07 13:24:33 +0100

    Update tests

--- public/app/features/alerting/unified/components/rule-editor/alert-rule-form/getPayloadToExport.test.ts
+++ public/app/features/alerting/unified/components/rule-editor/alert-rule-form/getPayloadToExport.test.ts
@@ -113,6 +113,7 @@ const expectedModifiedRule2 = (uid: string) => ({
     no_data_state: 'NoData',
     title: 'Rule2 updated',
     uid: uid,
+    missing_series_evals_to_resolve: 0,
   },
   labels: {
     newLabel: 'newLabel',
