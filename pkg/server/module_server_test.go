c252643cccc4b47da9e64afbbd198e9b7949d480
Mustafa Sencer Özcan <mustafasencer.ozcan@grafana.com>
2025-04-08 00:18:21 +0200

    fix: postgres test

--- pkg/server/module_server_test.go
+++ pkg/server/module_server_test.go
@@ -4,15 +4,12 @@ import (
 	"context"
 	"errors"
 	"net/http"
-	"os"
 	"testing"
 	"time"
 
-	"cuelang.org/go/pkg/regexp"
 	"github.com/grafana/grafana/pkg/api"
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/modules"
-	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/tests/testsuite"
 	"github.com/stretchr/testify/require"
 )
@@ -25,23 +22,14 @@ func TestIntegrationWillRunInstrumentationServerWhenTargetHasNoHttpServer(t *tes
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
-	dbType := os.Getenv("GRAFANA_TEST_DB")
-	if dbType == "" {
-		t.Skip("skipping - GRAFANA_TEST_DB not defined")
-	}
-	if dbType == "sqlite3" {
-		t.Skip("skipping - sqlite not supported for storage server target")
-	}
-	// TODO: remove this when drone integration tests are removed
-	if dbType == "postgres" {
-		t.Skip("skipping - enable test when drone integration tests are removed")
+	if db.IsTestDbSQLite() {
+		t.Skip("sqlite is not supported by the storage server target")
 	}
 
 	_, cfg := db.InitTestDBWithCfg(t)
 	cfg.HTTPPort = "3001"
 	cfg.GRPCServer.Network = "tcp"
 	cfg.GRPCServer.Address = "localhost:10000"
-	addStorageServerToConfig(t, cfg, dbType)
 	cfg.Target = []string{modules.StorageServer}
 
 	ms, err := InitializeModuleServer(cfg, Options{}, api.ServerOptions{})
@@ -71,35 +59,10 @@ func TestIntegrationWillRunInstrumentationServerWhenTargetHasNoHttpServer(t *tes
 
 	select {
 	case err := <-errChan:
-		if err != nil && errors.Is(err, context.Canceled) {
+		if err != nil && !errors.Is(err, context.Canceled) {
 			t.Fatalf("unexpected error from module server: %v", err)
 		}
 	case <-time.After(10 * time.Second):
 		t.Fatal("timeout waiting for module server to shut down")
-	}
-}
-
-func addStorageServerToConfig(t *testing.T, cfg *setting.Cfg, dbType string) {
-	s, err := cfg.Raw.NewSection("resource_api")
-	require.NoError(t, err)
-	_, err = s.NewKey("db_type", dbType)
-	require.NoError(t, err)
-
-	if dbType == "postgres" {
-		_, _ = s.NewKey("db_host", "localhost")
-		_, _ = s.NewKey("db_name", "grafanatest")
-		_, _ = s.NewKey("db_user", "grafanatest")
-		_, _ = s.NewKey("db_pass", "grafanatest")
-	} else {
-		// cant use localhost as hostname in drone tests for mysql, so need to parse it from connection string
-		sec, err := cfg.Raw.GetSection("database")
-		require.NoError(t, err)
-		connString := sec.Key("connection_string").String()
-		matches, err := regexp.FindSubmatch("(.+):(.+)@tcp\\((.+):(\\d+)\\)/(.+)\\?", connString)
-		require.NoError(t, err)
-		_, _ = s.NewKey("db_host", matches[3])
-		_, _ = s.NewKey("db_name", matches[5])
-		_, _ = s.NewKey("db_user", matches[1])
-		_, _ = s.NewKey("db_pass", matches[2])
 	}
 }
