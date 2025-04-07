2c24da4a663c2d01f7323d05297cd41ba1cbb1ee
Clarity-89 <homes89@ukr.net>
2025-04-07 13:06:57 +0300

    Update TokenPermissionsInfo.tsx

--- public/app/features/provisioning/Shared/TokenPermissionsInfo.tsx
+++ public/app/features/provisioning/Shared/TokenPermissionsInfo.tsx
@@ -14,55 +14,33 @@ export function TokenPermissionsInfo() {
           Go to{' '}
           <TextLink external href="https://github.com/settings/personal-access-tokens/new">
             GitHub Personal Access Tokens
-          </TextLink>
-          . Make sure to include these permissions under <b>Repository</b>:
+          </TextLink>{' '}
+          and click <strong>"Fine-grained token"</strong>. Make sure to include these permissions:
         </Trans>
       </div>
 
-      <table className={styles.permissionTable}>
-        <tbody>
-          <tr className={styles.headerSeparator}>
-            <th>
-              <Trans i18nKey="provisioning.token-permissions-info.permission">Permission</Trans>
-            </th>
-            <th>
-              <Trans i18nKey="provisioning.token-permissions-info.access">Access</Trans>
-            </th>
-          </tr>
-          <tr>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.contents">Contents</Trans>
-            </td>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.read-and-write">Read and write</Trans>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.metadata">Metadata</Trans>
-            </td>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.readonly">Read-only</Trans>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.pull-requests">Pull requests</Trans>
-            </td>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.read-and-write">Read and write</Trans>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.webhooks">Webhooks</Trans>
-            </td>
-            <td>
-              <Trans i18nKey="provisioning.token-permissions-info.read-and-write">Read and write</Trans>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <ul className={styles.permissionsList}>
+        <li>
+          <Trans i18nKey="provisioning.token-permissions-info.content-permission">
+            Content: <span className={styles.accessLevel}>Read and write</span>
+          </Trans>
+        </li>
+        <li>
+          <Trans i18nKey="provisioning.token-permissions-info.metadata-permission">
+            Metadata: <span className={styles.accessLevel}>Read only</span>
+          </Trans>
+        </li>
+        <li>
+          <Trans i18nKey="provisioning.token-permissions-info.pull-requests-permission">
+            Pull requests: <span className={styles.accessLevel}>Read and write</span>
+          </Trans>
+        </li>
+        <li>
+          <Trans i18nKey="provisioning.token-permissions-info.webhooks-permission">
+            Webhooks: <span className={styles.accessLevel}>Read and write</span>
+          </Trans>
+        </li>
+      </ul>
     </div>
   );
 }
@@ -71,22 +49,27 @@ function getStyles(theme: GrafanaTheme2) {
   return {
     container: css({
       marginBottom: theme.spacing(1),
-      backgroundColor: theme.colors.background.secondary,
-      border: `1px solid ${theme.colors.border.weak}`,
       position: 'relative',
-      borderRadius: theme.shape.radius.default,
       width: '100%',
       display: 'flex',
       flexDirection: 'column',
       flex: '1 1 0',
       padding: theme.spacing(theme.components.panel.padding),
     }),
-    permissionTable: css({
-      tableLayout: 'auto',
-      width: '40%',
+    permissionsList: css({
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(1),
+      paddingLeft: theme.spacing(3),
+
+      li: css({
+        marginBottom: theme.spacing(1),
+      }),
     }),
-    headerSeparator: css({
-      borderBottom: `1px solid ${theme.colors.border.weak}`,
+    accessLevel: css({
+      fontFamily: theme.typography.fontFamilyMonospace,
+      background: '#22262B',
+      borderRadius: theme.shape.radius.default,
+      padding: theme.spacing(0.25, 0.5),
     }),
   };
 }
