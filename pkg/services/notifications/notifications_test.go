f7f75b22f1133e09b48764ca05730a0c64c9bad8
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-04-07 14:27:26 +0200

    PMM-13463 Fix some BE unit tests

--- pkg/services/notifications/notifications_test.go
+++ pkg/services/notifications/notifications_test.go
@@ -227,7 +227,7 @@ func TestSendEmailAsync(t *testing.T) {
 		sentMsg := <-sut.mailQueue
 		assert.Contains(t, sentMsg.Body["text/html"], "body")
 		assert.NotContains(t, sentMsg.Body["text/plain"], "body")
-		assert.Equal(t, "Reset your Grafana password - asd@asd.com", sentMsg.Subject)
+		assert.Equal(t, "Reset your PMM password - asd@asd.com", sentMsg.Subject)
 		assert.NotContains(t, sentMsg.Body["text/html"], "Subject")
 		assert.NotContains(t, sentMsg.Body["text/plain"], "Subject")
 
