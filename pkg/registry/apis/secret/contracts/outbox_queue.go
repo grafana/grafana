bcaf2fdf846d3c41f5ddd30ef3f665585b373fb8
Bruno <brunotj2015@hotmail.com>
2025-04-07 11:24:39 -0300

    Secrets: Add outbox contract and store (#103381)
    * add outbox queue contract / outbox store implementation
    
    * add indices / fix outbox receive sort order
    
    * outbox store: don't construct exposed secure value type for msgs of type delete
    
    * document test iterations decision
    
    * change TableNameSecureValueOutbox.message_type to string
    
    * chnge TableNameSecureValueOutbox.message_type length to 16

---
+++ pkg/registry/apis/secret/contracts/outbox_queue.go
@@ -0,0 +1,43 @@
+package contracts
+
+import (
+	"context"
+
+	secretv0alpha1 "github.com/grafana/grafana/pkg/apis/secret/v0alpha1"
+)
+
+type OutboxMessageType string
+
+const (
+	CreateSecretOutboxMessage OutboxMessageType = "create"
+	UpdateSecretOutboxMessage OutboxMessageType = "update"
+	DeleteSecretOutboxMessage OutboxMessageType = "delete"
+)
+
+type AppendOutboxMessage struct {
+	Type            OutboxMessageType
+	Name            string
+	Namespace       string
+	EncryptedSecret secretv0alpha1.ExposedSecureValue
+	KeeperName      string
+	ExternalID      *string
+}
+
+type OutboxMessage struct {
+	Type            OutboxMessageType
+	MessageID       string
+	Name            string
+	Namespace       string
+	EncryptedSecret secretv0alpha1.ExposedSecureValue
+	KeeperName      string
+	ExternalID      *string
+}
+
+type OutboxQueue interface {
+	// Appends a message to the outbox queue
+	Append(ctx context.Context, message AppendOutboxMessage) (string, error)
+	// Receives at most n messages from the outbox queue
+	ReceiveN(ctx context.Context, n uint) ([]OutboxMessage, error)
+	// Deletes a message from the outbox queue
+	Delete(ctx context.Context, messageID string) error
+}
