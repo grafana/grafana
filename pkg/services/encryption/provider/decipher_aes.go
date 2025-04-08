1c4284a8b44ce164e24d45c21cc6b6bb91aab5a3
Mariell Hoversholm <mariell.hoversholm@grafana.com>
2025-04-08 16:26:54 +0200

    CI: Update golangci-lint

--- pkg/services/encryption/provider/decipher_aes.go
+++ pkg/services/encryption/provider/decipher_aes.go
@@ -59,6 +59,7 @@ func decryptCFB(block cipher.Block, payload []byte) ([]byte, error) {
 	payload = payload[encryption.SaltLength+aes.BlockSize:]
 	payloadDst := make([]byte, len(payload))
 
+	//nolint:staticcheck // SA1019: We're not changing away from CFB in older versions
 	stream := cipher.NewCFBDecrypter(block, iv)
 
 	// XORKeyStream can work in-place if the two arguments are the same.
