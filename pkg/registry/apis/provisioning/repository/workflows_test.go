19991bb592c6411bff7c734d35e3e785dc0bb9fd
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-04-08 09:48:27 +0200

    Add unit tests for IsWriteAllowed

---
+++ pkg/registry/apis/provisioning/repository/workflows_test.go
@@ -0,0 +1,111 @@
+package repository
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
+	provisioning "github.com/grafana/grafana/pkg/apis/provisioning/v0alpha1"
+)
+
+func TestIsWriteAllowed(t *testing.T) {
+	tests := []struct {
+		name        string
+		repository  *provisioning.Repository
+		ref         string
+		wantErr     bool
+		expectedErr string
+		statusCode  int32
+	}{
+		{
+			name: "read only repository",
+			repository: &provisioning.Repository{
+				Spec: provisioning.RepositorySpec{
+					Workflows: []provisioning.Workflow{},
+				},
+			},
+			wantErr:     true,
+			expectedErr: "this repository is read only",
+			statusCode:  http.StatusBadRequest,
+		},
+		{
+			name: "write workflow allowed",
+			repository: &provisioning.Repository{
+				Spec: provisioning.RepositorySpec{
+					Workflows: []provisioning.Workflow{provisioning.WriteWorkflow},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "write workflow not allowed",
+			repository: &provisioning.Repository{
+				Spec: provisioning.RepositorySpec{
+					Workflows: []provisioning.Workflow{provisioning.BranchWorkflow},
+				},
+			},
+			wantErr:     true,
+			expectedErr: "this repository does not support the write workflow",
+			statusCode:  http.StatusBadRequest,
+		},
+		{
+			name: "branch workflow on non-github repository",
+			repository: &provisioning.Repository{
+				Spec: provisioning.RepositorySpec{
+					Type:      provisioning.LocalRepositoryType,
+					Workflows: []provisioning.Workflow{provisioning.BranchWorkflow},
+				},
+			},
+			ref:         "feature-branch",
+			wantErr:     true,
+			expectedErr: "Only github supports writing to a branch",
+			statusCode:  http.StatusPreconditionFailed,
+		},
+		{
+			name: "branch workflow allowed on github repository",
+			repository: &provisioning.Repository{
+				Spec: provisioning.RepositorySpec{
+					Type:      provisioning.GitHubRepositoryType,
+					Workflows: []provisioning.Workflow{provisioning.BranchWorkflow},
+				},
+			},
+			ref:     "feature-branch",
+			wantErr: false,
+		},
+		{
+			name: "branch workflow not allowed on github repository",
+			repository: &provisioning.Repository{
+				Spec: provisioning.RepositorySpec{
+					Type:      provisioning.GitHubRepositoryType,
+					Workflows: []provisioning.Workflow{provisioning.WriteWorkflow},
+				},
+			},
+			ref:         "feature-branch",
+			wantErr:     true,
+			expectedErr: "this repository does not support the branch workflow",
+			statusCode:  http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := IsWriteAllowed(tt.repository, tt.ref)
+			if tt.wantErr {
+				require.Error(t, err)
+				require.Equal(t, tt.expectedErr, err.Error())
+
+				if statusErr, ok := err.(*apierrors.StatusError); ok {
+					require.Equal(t, tt.statusCode, statusErr.Status().Code)
+				} else {
+					statusErr, ok := err.(apierrors.APIStatus)
+					require.True(t, ok, "error should implement APIStatus")
+					require.Equal(t, tt.statusCode, statusErr.Status().Code)
+				}
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
