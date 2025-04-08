f8643281441c49c58219798e9bc9cff2d32c504b
Maicon Costa <maiconscosta@gmail.com>
2025-04-07 22:13:02 -0300

    Fix test
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/cmd/grafana-cli/commands/datamigrations/encrypt_datasource_passwords_test.go
+++ pkg/cmd/grafana-cli/commands/datamigrations/encrypt_datasource_passwords_test.go
@@ -65,7 +65,7 @@ func passwordMigration(t *testing.T, session *db.Session, sqlstore db.DB) {
 	// run migration
 	c, err := commandstest.NewCliContext(map[string]string{})
 	require.Nil(t, err)
-	err = EncryptDatasourcePasswords(c, setting.NewCfg(), sqlstore)
+	err = EncryptDatasourcePasswords(c, setting.NewCfg(), sqlstore, nil)
 	require.NoError(t, err)
 
 	// verify that no datasources still have password or basic_auth
