269e7f713f98526eb07fb15a17386a51de1ee036
Bruno <brunotj2015@hotmail.com>
2025-04-09 16:23:12 -0300

    Secrets: add contracts.Database (#103747)

---
+++ pkg/registry/apis/secret/contracts/database.go
@@ -0,0 +1,18 @@
+package contracts
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Database interface {
+	Transaction(ctx context.Context, f func(context.Context) error) error
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...any) (Rows, error)
+}
+
+type Rows interface {
+	Close() error
+	Next() bool
+	Scan(dest ...any) error
+}
