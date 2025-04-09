72ccd9dac4e27747e4497271ba042de2c2096922
Will Assis <william@williamassis.com>
2025-04-09 17:00:21 -0300

    wip setup ring

--- pkg/storage/unified/search/bleve.go
+++ pkg/storage/unified/search/bleve.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log/slog"
 	"math"
+	"net"
+	"net/http"
 	"os"
 	"path/filepath"
 	"slices"
@@ -24,6 +26,9 @@ import (
 	"go.opentelemetry.io/otel/trace"
 	"k8s.io/apimachinery/pkg/selection"
 
+	"github.com/grafana/dskit/grpcclient"
+	"github.com/grafana/dskit/ring"
+	ringclient "github.com/grafana/dskit/ring/client"
 	"github.com/grafana/grafana/pkg/services/dashboards/dashboardaccess"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 
@@ -62,6 +67,9 @@ type bleveBackend struct {
 
 	features     featuremgmt.FeatureToggles
 	indexMetrics *resource.BleveIndexMetrics
+
+	// sharding things
+	Pool *ringclient.Pool
 }
 
 func NewBleveBackend(opts BleveOptions, tracer trace.Tracer, features featuremgmt.FeatureToggles, indexMetrics *resource.BleveIndexMetrics) (*bleveBackend, error) {
@@ -88,7 +96,66 @@ func NewBleveBackend(opts BleveOptions, tracer trace.Tracer, features featuremgm
 
 	go bleveBackend.updateIndexSizeMetric(opts.Root)
 
+	// TODO wrap in feature flag
+	err = bleveBackend.enableSharding()
+	if err != nil {
+		return nil, fmt.Errorf("error sharding", err)
+	}
+
 	return bleveBackend, nil
+}
+
+func (b *bleveBackend) enableSharding() error {
+	ctx := context.Background()
+	pool := newClientPool(grpcclient.Config{} /*TODO*/, b.log, nil)
+
+	ringsvc, lfcsvc, err := InitRing(log.New("bleve-ring"), nil)
+
+	err = ringsvc.StartAsync(ctx)
+	if err != nil {
+		return err
+	}
+	err = lfcsvc.StartAsync(ctx)
+	if err != nil {
+		return err
+	}
+
+	b.log.Info("waiting until bleve is JOINING in the rinng")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	if err := ring.WaitInstanceState(ctx, ringsvc, lfcsvc.GetInstanceID(), ring.JOINING); err != nil {
+		return err
+	}
+	b.log.Info("bleve is JOINING in the rinng")
+
+	if err := lfcsvc.ChangeState(context.Background(), ring.ACTIVE); err != nil {
+		return fmt.Errorf("switch instance to %s in the ring: %w", ring.ACTIVE, err)
+	}
+
+	b.log.Info("waiting until bleve is ACTIVE in the rinng")
+	if err := ring.WaitInstanceState(context.Background(), ringsvc, lfcsvc.GetInstanceID(), ring.ACTIVE); err != nil {
+		return err
+	}
+	b.log.Info("bleve is ACTIVE in the rinng")
+
+	if err := pool.StartAsync(context.Background()); err != nil {
+		return fmt.Errorf("failed to create pool client: %w", err)
+	}
+
+	b.Pool = pool
+
+	// TODO remove this
+	listener, err := net.Listen("tcp", net.JoinHostPort("127.0.0.1", "8100"))
+	if err != nil {
+		panic(err)
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/ring", lfcsvc)
+	http.Serve(listener, mux)
+	// mux.Handle("/kv", memberlistsvc)
+
+	return nil
+
 }
 
 // This will return nil if the key does not exist
---
+++ pkg/storage/unified/search/ring.go
@@ -0,0 +1,223 @@
+package search
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/grafana/dskit/dns"
+	"github.com/grafana/dskit/flagext"
+	"github.com/grafana/dskit/grpcclient"
+	"github.com/grafana/dskit/kv"
+	"github.com/grafana/dskit/kv/codec"
+	"github.com/grafana/dskit/kv/memberlist"
+	"github.com/grafana/dskit/ring"
+	"github.com/grafana/dskit/ring/client"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"go.etcd.io/etcd/api/v3/etcdserverpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+type RingConfig struct {
+	KVStore          kv.Config     `yaml:"kvstore"`
+	HeartbeatPeriod  time.Duration `yaml:"heartbeat_period" category:"advanced"`
+	HeartbeatTimeout time.Duration `yaml:"heartbeat_timeout" category:"advanced"`
+
+	// Instance details
+	InstanceID             string   `yaml:"instance_id" doc:"default=<hostname>" category:"advanced"`
+	InstanceInterfaceNames []string `yaml:"instance_interface_names" doc:"default=[<private network interfaces>]"`
+	InstancePort           int      `yaml:"instance_port" category:"advanced"`
+	InstanceAddr           string   `yaml:"instance_addr" category:"advanced"`
+	NumTokens              int      `yaml:"num_tokens" category:"advanced"`
+
+	// Injected internally
+	ListenPort int `yaml:"-"`
+
+	// Used for testing
+	SkipUnregister bool `yaml:"-"`
+}
+
+// TODO make this configurable
+var cfg = RingConfig{
+	KVStore: kv.Config{Store: "memberlist"},
+	HeartbeatPeriod: 15*time.Second,
+	HeartbeatTimeout: time.Minute,
+	// InstanceInterfaceNames: , maybe skip this. only needed if instanceaddr is not defined
+	InstanceAddr: "127.0.0.1",
+	InstancePort: 9378,
+	InstanceID: "1",
+	NumTokens: 128,
+}
+
+const ringKey = "ring"
+const ringName = "bleve"
+var metricsPrefix = ringName + "_"
+const ringAutoForgetUnhealthyPeriods = 2
+
+func (cfg *RingConfig) ToLifecyclerConfig(logger log.Logger) (ring.BasicLifecyclerConfig, error) {
+	instanceAddr, err := ring.GetInstanceAddr(cfg.InstanceAddr, cfg.InstanceInterfaceNames, logger, true)
+	if err != nil {
+		return ring.BasicLifecyclerConfig{}, err
+	}
+
+	instancePort := ring.GetInstancePort(cfg.InstancePort, cfg.ListenPort)
+
+	return ring.BasicLifecyclerConfig{
+		ID:                  cfg.InstanceID,
+		Addr:                fmt.Sprintf("%s:%d", instanceAddr, instancePort),
+		HeartbeatPeriod:     cfg.HeartbeatPeriod,
+		HeartbeatTimeout:    cfg.HeartbeatTimeout,
+		TokensObservePeriod: 0,
+		NumTokens:           cfg.NumTokens,
+	}, nil
+}
+
+func (cfg *RingConfig) ToRingConfig() ring.Config {
+	rc := ring.Config{}
+	flagext.DefaultValues(&rc)
+
+	rc.KVStore = cfg.KVStore
+	rc.HeartbeatTimeout = cfg.HeartbeatTimeout
+	rc.SubringCacheDisabled = true
+
+	rc.ReplicationFactor = 1
+
+	return rc
+}
+
+func InitRing(logger log.Logger, registerer prometheus.Registerer) (*ring.Ring, *ring.BasicLifecycler, error) {
+	memberlistKVcfg := &memberlist.KVConfig{}
+	memberlistKVcfg.MetricsNamespace = ringName
+	memberlistKVcfg.Codecs = []codec.Codec{
+		ring.GetCodec(),
+	}
+	//m.Cfg.MemberlistKV.AdvertisePort = m.Cfg.Server.GRPCListenPort
+
+	dnsProviderReg := prometheus.WrapRegistererWithPrefix(
+		metricsPrefix,
+		prometheus.WrapRegistererWith(
+			prometheus.Labels{"component": "memberlist"},
+			registerer,
+		),
+	)
+	dnsProvider := dns.NewProvider(logger, dnsProviderReg, dns.GolangResolverType)
+
+	memberlistKVsvc := memberlist.NewKVInitService(memberlistKVcfg, logger, dnsProvider, registerer)
+	memberlistKVsvc.StartAsync(context.Background())
+	cfg.KVStore.MemberlistKV = memberlistKVsvc.GetMemberlistKV
+
+	ringStore, err := kv.NewClient(
+		cfg.KVStore,
+		ring.GetCodec(),
+		kv.RegistererWithKVName(prometheus.WrapRegistererWithPrefix(metricsPrefix, registerer), "ruler"),
+		logger,
+	)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to create KV store client")
+	}
+
+	lifecyclerCfg, err := cfg.ToLifecyclerConfig(logger)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to initialize ruler's lifecycler config")
+	}
+
+	// Define lifecycler delegates in reverse order (last to be called defined first because they're
+	// chained via "next delegate").
+	delegate := ring.BasicLifecyclerDelegate(ring.NewInstanceRegisterDelegate(ring.JOINING, cfg.NumTokens))
+	delegate = ring.NewLeaveOnStoppingDelegate(delegate, logger)
+	delegate = ring.NewAutoForgetDelegate(cfg.HeartbeatTimeout*ringAutoForgetUnhealthyPeriods, delegate, logger)
+
+	lifecycler, err := ring.NewBasicLifecycler(
+		lifecyclerCfg,
+		ringName,
+		ringKey,
+		ringStore,
+		delegate,
+		logger,
+		prometheus.WrapRegistererWithPrefix(metricsPrefix, registerer),
+	)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to initialize ruler's lifecycler")
+	}
+
+	rulerRing, err := ring.NewWithStoreClientAndStrategy(
+		cfg.ToRingConfig(),
+		ringName,
+		ringKey,
+		ringStore,
+		ring.NewIgnoreUnhealthyInstancesReplicationStrategy(),
+		prometheus.WrapRegistererWithPrefix(metricsPrefix, registerer),
+		logger,
+	)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to initialize ruler's ring")
+	}
+
+	return rulerRing, lifecycler, nil
+}
+
+func newClientPool(clientCfg grpcclient.Config, log *slog.Logger, reg prometheus.Registerer) *client.Pool {
+	poolCfg := client.PoolConfig{
+		CheckInterval:      10 * time.Second,
+		HealthCheckEnabled: true,
+		HealthCheckTimeout: 10 * time.Second,
+	}
+	clientsCount := promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+		Name: "memora_clients",
+		Help: "The current number of memora clients in the pool.",
+	})
+	log.Info("ring client pool",
+		"grcp.MaxSendMsgSize", clientCfg.MaxSendMsgSize,
+		"grpc.MaxRecvMsgSize", clientCfg.MaxRecvMsgSize)
+
+	factoryRequestDuration := promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "memora_client_request_duration_seconds",
+		Help:    "Time spent executing requests to memora.",
+		Buckets: prometheus.ExponentialBuckets(0.008, 4, 7),
+	}, []string{"operation", "status_code"})
+
+	factory := client.PoolInstFunc(func(inst ring.InstanceDesc) (client.PoolClient, error) {
+		opts, err := clientCfg.DialOption(grpcclient.Instrument(factoryRequestDuration))
+		if err != nil {
+			return nil, err
+		}
+
+		conn, err := grpc.NewClient(inst.Addr, opts...)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to dial memora %s %s", inst.Id, inst.Addr)
+		}
+
+		return &bleveRingClient{
+			KVClient:     etcdserverpb.NewKVClient(conn),
+			HealthClient: grpc_health_v1.NewHealthClient(conn),
+			conn:         conn,
+		}, nil
+	})
+
+	return client.NewPool(ringName, poolCfg, nil, factory, clientsCount, nil /* TODO add logger here*/)
+}
+
+type bleveRingClient struct {
+	etcdserverpb.KVClient
+	grpc_health_v1.HealthClient
+	conn *grpc.ClientConn
+}
+
+func (c *bleveRingClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *bleveRingClient) String() string {
+	return c.RemoteAddress()
+}
+
+func (c *bleveRingClient) RemoteAddress() string {
+	return c.conn.Target()
+}
+
+
