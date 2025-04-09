6262367ae885a219c37ff6b4398d6895030e852a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-04-09 10:06:13 -0300

    remove unused db.DB dependency

--- pkg/storage/secret/metadata/decrypt_store.go
+++ pkg/storage/secret/metadata/decrypt_store.go
@@ -8,7 +8,6 @@ import (
 	claims "github.com/grafana/authlib/types"
 
 	secretv0alpha1 "github.com/grafana/grafana/pkg/apis/secret/v0alpha1"
-	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/contracts"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/secretkeeper"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/xkube"
@@ -17,7 +16,6 @@ import (
 
 // TODO: this should be a "decrypt" service rather, so that other services can wire and call it.
 func ProvideDecryptStorage(
-	db db.DB,
 	features featuremgmt.FeatureToggles,
 	keeperService secretkeeper.Service,
 	keeperMetadataStorage contracts.KeeperMetadataStorage,
@@ -34,13 +32,11 @@ func ProvideDecryptStorage(
 		return nil, fmt.Errorf("failed to get keepers: %w", err)
 	}
 
-	return &decryptStorage{db: db, keeperMetadataStorage: keeperMetadataStorage, keepers: keepers, secureValueMetadataStorage: secureValueMetadataStorage, allowList: allowList}, nil
+	return &decryptStorage{keeperMetadataStorage: keeperMetadataStorage, keepers: keepers, secureValueMetadataStorage: secureValueMetadataStorage, allowList: allowList}, nil
 }
 
 // decryptStorage is the actual implementation of the decrypt storage.
 type decryptStorage struct {
-	// TODO: remove this and only use the storages (when making this a service), need to figure out the getKeeperConfig part.
-	db                         db.DB
 	keeperMetadataStorage      contracts.KeeperMetadataStorage
 	keepers                    map[contracts.KeeperType]contracts.Keeper
 	secureValueMetadataStorage contracts.SecureValueMetadataStorage
