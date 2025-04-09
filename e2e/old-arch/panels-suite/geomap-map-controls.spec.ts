6132822fcfa7a18a55e7aaafc40a0b07ed6a70b5
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-04-09 06:27:39 +0200

    e2e: more refactor

--- e2e/old-arch/panels-suite/geomap-map-controls.spec.ts
+++ e2e/old-arch/panels-suite/geomap-map-controls.spec.ts
@@ -1,5 +1,6 @@
 import { e2e } from '../utils';
 const DASHBOARD_ID = 'P2jR04WVk';
+const TIMEOUT = 60000;
 
 describe('Geomap layer controls options', () => {
   beforeEach(() => {
@@ -13,14 +14,12 @@ describe('Geomap layer controls options', () => {
     e2e.components.Select.singleValue().contains('flight_info_by_state.csv').should('be.visible');
     e2e.components.OptionsGroup.group('Map controls').scrollIntoView().should('be.visible');
 
-    // Show zoom fi
+    // Show zoom field
     e2e.components.PanelEditor.showZoomField()
       .should('be.visible')
       .within(() => {
         cy.get('input[type="checkbox"]').check({ force: true }).should('be.checked');
       });
-
-    cy.get('.ol-zoom').should('be.visible');
 
     // Show attribution
     e2e.components.PanelEditor.showAttributionField()
@@ -29,15 +28,12 @@ describe('Geomap layer controls options', () => {
         cy.get('input[type="checkbox"]').check({ force: true }).should('be.checked');
       });
 
-    cy.get('.ol-attribution').should('be.visible');
     // Show scale
     e2e.components.PanelEditor.showScaleField()
       .should('be.visible')
       .within(() => {
         cy.get('input[type="checkbox"]').check({ force: true }).should('be.checked');
       });
-
-    cy.get('.ol-scale-line').should('be.visible');
 
     // Show measure tool
     e2e.components.PanelEditor.showMeasureField()
@@ -46,8 +42,6 @@ describe('Geomap layer controls options', () => {
         cy.get('input[type="checkbox"]').check({ force: true }).should('be.checked');
       });
 
-    e2e.components.PanelEditor.measureButton().should('be.visible');
-
     // Show debug
     e2e.components.PanelEditor.showDebugField()
       .should('be.visible')
@@ -55,6 +49,23 @@ describe('Geomap layer controls options', () => {
         cy.get('input[type="checkbox"]').check({ force: true }).should('be.checked');
       });
 
-    e2e.components.DebugOverlay.wrapper().should('be.visible');
+    e2e.components.Panels.Panel.content({ timeout: TIMEOUT })
+      .should('be.visible')
+      .within(() => {
+        // Verify zoom
+        cy.get('.ol-zoom', { timeout: TIMEOUT }).should('be.visible');
+
+        // Verify attribution
+        cy.get('.ol-attribution', { timeout: TIMEOUT }).should('be.visible');
+
+        // Verify scale
+        cy.get('.ol-scale-line', { timeout: TIMEOUT }).should('be.visible');
+
+        // Verify measure tool
+        e2e.components.PanelEditor.measureButton({ timeout: TIMEOUT }).should('be.visible');
+
+        // Verify debug tool
+        e2e.components.DebugOverlay.wrapper({ timeout: TIMEOUT }).should('be.visible');
+      });
   });
 });
