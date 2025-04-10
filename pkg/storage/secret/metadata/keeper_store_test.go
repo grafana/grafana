77eae0e7ffe2047f3d7fbdb54db827e374389451
Bruno <brunotj2015@hotmail.com>
2025-04-10 15:59:26 -0300

    Secrets: move secureValueMetadataStorage.GetKeeperConfig closer to keeper_store.go (#103823)

---
+++ pkg/storage/secret/metadata/keeper_store_test.go
@@ -0,0 +1,34 @@
+package metadata
+
+import (
+	"context"
+	"testing"
+
+	"github.com/grafana/grafana/pkg/registry/apis/secret/contracts"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_KeeperMetadataStorage_GetKeeperConfig(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	s := setupTestService(t).(*secureValueMetadataStorage)
+
+	features := featuremgmt.WithFeatures(featuremgmt.FlagGrafanaAPIServerWithExperimentalAPIs, featuremgmt.FlagSecretsManagementAppPlatform)
+	storage, err := ProvideKeeperMetadataStorage(s.db, features, s.accessClient)
+	require.NoError(t, err)
+
+	t.Run("get default sql keeper config", func(t *testing.T) {
+		keeperType, keeperConfig, err := storage.GetKeeperConfig(ctx, "default", "kp-default-sql")
+		require.NoError(t, err)
+		require.Equal(t, contracts.SQLKeeperType, keeperType)
+		require.Nil(t, keeperConfig)
+	})
+
+	t.Run("get test keeper config", func(t *testing.T) {
+		keeperType, keeperConfig, err := storage.GetKeeperConfig(ctx, "default", "kp-test")
+		require.NoError(t, err)
+		require.Equal(t, contracts.SQLKeeperType, keeperType)
+		require.NotNil(t, keeperConfig)
+	})
+}
