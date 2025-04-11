f59ecc1a037ecd870ccda240db146732a989f70a
bergquist <carl.bergquist@gmail.com>
2025-04-11 08:45:59 +0200

    scopes: marks scopes ff as experimental
    Signed-off-by: bergquist <carl.bergquist@gmail.com>

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -889,7 +889,7 @@ var (
 		{
 			Name:              "promQLScope",
 			Description:       "In-development feature that will allow injection of labels into prometheus queries.",
-			Stage:             FeatureStageGeneralAvailability,
+			Stage:             FeatureStageExperimental,
 			Owner:             grafanaOSSBigTent,
 			Expression:        "true",
 			HideFromDocs:      true,
