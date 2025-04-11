25531d3367d80b6eb91ac77e0b40caa171cce1bb
Peter Štibraný <pstibrany@gmail.com>
2025-04-11 14:51:07 +0200

    Rename tests to make them integration tests.

--- pkg/services/libraryelements/libraryelements_get_all_test.go
+++ pkg/services/libraryelements/libraryelements_get_all_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/search/sort"
 )
 
-func TestGetAllLibraryElements(t *testing.T) {
+func TestIntegration_GetAllLibraryElements(t *testing.T) {
 	testScenario(t, "When an admin tries to get all library panels and none exists, it should return none",
 		func(t *testing.T, sc scenarioContext) {
 			resp := sc.service.getAllHandler(sc.reqContext)
