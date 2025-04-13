823ec19eecd60ef0786543fc9463a7dadb88c34b
joshhunt <josh.hunt@grafana.com>
2025-04-13 14:37:34 +0100

    add todo list

--- pkg/server/frontend_server.go
+++ pkg/server/frontend_server.go
@@ -78,6 +78,12 @@ func (s *frontendServer) newFrontendServer(ctx context.Context) *http.Server {
 }
 
 func (s *frontendServer) handleRequest(writer http.ResponseWriter, request *http.Request) {
+	// This should:
+	// - call http://slug/api/bootdata
+	// - render index.html with the bootdata
+	// - something-something-something frontend assets
+	// - return it to the user!
+
 	s.log.Info("handling request", "method", request.Method, "url", request.URL.String())
 	htmlContent := `<!DOCTYPE html>
 <html>
