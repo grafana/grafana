eb7d141568216ed95ffe461554b216739212f9d4
Ryan McKinley <ryantxu@gmail.com>
2025-04-15 00:00:42 +0300

    v1beta1

--- apps/dashboard/pkg/apis/dashboard/v1beta1/constants.go
+++ apps/dashboard/pkg/apis/dashboard/v1beta1/constants.go
@@ -6,7 +6,7 @@ const (
 	// Group is the API group used by all kinds in this package
 	Group = "dashboard.grafana.app"
 	// Version is the API version used by all kinds in this package
-	Version = "v1alpha1"
+	Version = "v1beta1"
 )
 
 var (
