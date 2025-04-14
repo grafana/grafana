a2ad7ea386104eda0da2fe539a18a271ee6d1bda
Ryan McKinley <ryantxu@gmail.com>
2025-04-14 22:23:08 +0300

    no export

--- public/app/api/clients/folder/index.ts
+++ public/app/api/clients/folder/index.ts
@@ -3,5 +3,3 @@ import { generatedAPI } from './endpoints.gen';
 export const folderAPI = generatedAPI.enhanceEndpoints({});
 
 export const { useGetFolderQuery } = folderAPI;
-// eslint-disable-next-line no-barrel-files/no-barrel-files
-export { type FolderSpec, type Folder } from './endpoints.gen';
