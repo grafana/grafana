7f08e76d34f6b77967145b3120ff424693433c7b
Alex Khomenko <Clarity-89@users.noreply.github.com>
2025-04-15 10:21:47 +0300

    Folders: Switch to use v1beta1 API version (#104020)

--- public/app/api/clients/folder/baseAPI.ts
+++ public/app/api/clients/folder/baseAPI.ts
@@ -3,7 +3,7 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import { createBaseQuery } from 'app/api/createBaseQuery';
 import { getAPIBaseURL } from 'app/api/utils';
 
-export const BASE_URL = getAPIBaseURL('folder.grafana.app', 'v0alpha1');
+export const BASE_URL = getAPIBaseURL('folder.grafana.app', 'v1beta1');
 
 export const api = createApi({
   reducerPath: 'folderAPI',
