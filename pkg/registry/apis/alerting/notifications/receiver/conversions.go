acab0d47e360d65160e74b54c20e74c5331a274c
Gilles De Mey <gilles.de.mey@gmail.com>
2025-04-15 18:45:57 +0200

    codegen – used v0.36.1 of the app sdk
    rest of the owl

--- pkg/registry/apis/alerting/notifications/receiver/conversions.go
+++ pkg/registry/apis/alerting/notifications/receiver/conversions.go
@@ -8,7 +8,7 @@ import (
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/types"
 
-	model "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/resource/receiver/v0alpha1"
+	model "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/receiver/v0alpha1"
 	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
 	gapiutil "github.com/grafana/grafana/pkg/services/apiserver/utils"
 	ngmodels "github.com/grafana/grafana/pkg/services/ngalert/models"
