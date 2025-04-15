7af39a356dafa6c44925f5c62b72bbe534515476
Matias Chomicki <matyax@gmail.com>
2025-04-15 13:20:30 +0200

    Query builder: parse variables in vector aggregation

--- public/app/plugins/datasource/loki/querybuilder/parsing.ts
+++ public/app/plugins/datasource/loki/querybuilder/parsing.ts
@@ -526,9 +526,12 @@ function handleVectorAggregation(expr: string, node: SyntaxNode, context: Contex
   const params = [];
 
   const numberNode = node.getChild(NumberLezer);
+  const errorNode = node.getChild(ErrorId)?.getChild(Identifier);
 
   if (numberNode) {
     params.push(Number(getString(expr, numberNode)));
+  } else if (errorNode) {
+    params.push(getString(expr, errorNode));
   }
 
   if (grouping) {
