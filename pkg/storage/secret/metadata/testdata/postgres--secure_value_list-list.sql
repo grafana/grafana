88783a908a639ee3aa0f05db24b0192868c87995
Leandro Deveikis <leandro.deveikis@gmail.com>
2025-04-16 12:19:06 -0300

    secure value list sql files

---
+++ pkg/storage/secret/metadata/testdata/postgres--secure_value_list-list.sql
@@ -0,0 +1,24 @@
+}
+SELECT
+  "guid",
+  "name",
+  "namespace",
+  "annotations",
+  "labels",
+  "created",
+  "created_by",
+  "updated",
+  "updated_by",
+  "status_phase",
+  "status_message",
+  "title",
+  "keeper",
+  "decrypters"
+  "ref"
+  "external_id"
+FROM
+  "secret_secure_value"
+WHERE 1 = 1 AND
+  "namespace" = 'ns'
+ORDER BY "updated" DESC
+;
