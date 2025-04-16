2276182d37251c802a14baa3bad634c150d73be3
Hugo Kiyodi Oshiro <hugo.oshiro@grafana.com>
2025-04-16 13:44:39 +0200

    Plugins: Add feature toggle for plugins auto-update (#103993)

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1032,4 +1032,8 @@ export interface FeatureToggles {
   * Enables creating metrics from profiles and storing them as recording rules
   */
   metricsFromProfiles?: boolean;
+  /**
+  * Enables auto-updating of users installed plugins
+  */
+  pluginsAutoUpdate?: boolean;
 }
