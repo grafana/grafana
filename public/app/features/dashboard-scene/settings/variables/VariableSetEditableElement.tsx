eb00915804ef26687c3c5cea511b83e0c8fa0264
Torkel Ödegaard <torkel@grafana.com>
2025-04-16 16:01:55 +0200

    Dashboard: Adding variables and selecting type (#103956)
    * Dashboard: Adding variables and selecting type
    
    * Make i18n-extract
    
    ---------
    
    Co-authored-by: oscarkilhed <oscar.kilhed@grafana.com>

--- public/app/features/dashboard-scene/settings/variables/VariableSetEditableElement.tsx
+++ public/app/features/dashboard-scene/settings/variables/VariableSetEditableElement.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/css';
 import { useMemo } from 'react';
+import { useToggle } from 'react-use';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { SceneVariable, SceneVariableSet } from '@grafana/scenes';
-import { Stack, Button, useStyles2, Text, Box } from '@grafana/ui';
+import { Stack, Button, useStyles2, Text, Box, Card } from '@grafana/ui';
 import { t, Trans } from 'app/core/internationalization';
 import { OptionsPaneCategoryDescriptor } from 'app/features/dashboard/components/PanelEditor/OptionsPaneCategoryDescriptor';
 import { OptionsPaneItemDescriptor } from 'app/features/dashboard/components/PanelEditor/OptionsPaneItemDescriptor';
@@ -12,7 +13,7 @@ import { NewObjectAddedToCanvasEvent } from '../../edit-pane/shared';
 import { EditableDashboardElement, EditableDashboardElementInfo } from '../../scene/types/EditableDashboardElement';
 import { getDashboardSceneFor } from '../../utils/utils';
 
-import { getVariableDefault } from './utils';
+import { EditableVariableType, getNextAvailableId, getVariableScene, getVariableTypeSelectOptions } from './utils';
 
 export class VariableSetEditableElement implements EditableDashboardElement {
   public readonly isEditableDashboardElement = true;
@@ -49,18 +50,25 @@ export class VariableSetEditableElement implements EditableDashboardElement {
 function VariableList({ set }: { set: SceneVariableSet }) {
   const { variables } = set.useState();
   const styles = useStyles2(getStyles);
+  const [isAdding, setIsAdding] = useToggle(false);
 
   const onEditVariable = (variable: SceneVariable) => {
     const { editPane } = getDashboardSceneFor(set).state;
     editPane.selectObject(variable, variable.state.key!);
   };
 
-  const onAddVariable = () => {
-    //add the new variable to the end of the array
-    const newVariable = getVariableDefault(variables);
-    set.setState({ variables: [...variables, newVariable] });
-    set.publishEvent(new NewObjectAddedToCanvasEvent(newVariable), true);
+  const onAddVariable = (type: EditableVariableType) => {
+    const { variables } = set.state;
+    const nextName = getNextAvailableId(type, variables);
+    const newVar = getVariableScene(type, { name: nextName });
+    set.setState({ variables: [...variables, newVar] });
+    set.publishEvent(new NewObjectAddedToCanvasEvent(newVar), true);
+    setIsAdding(false);
   };
+
+  if (isAdding) {
+    return <VariableTypeSelection onAddVariable={onAddVariable} />;
+  }
 
   return (
     <Stack direction="column" gap={0}>
@@ -76,10 +84,38 @@ function VariableList({ set }: { set: SceneVariableSet }) {
         </div>
       ))}
       <Box paddingBottom={1} display={'flex'}>
-        <Button fullWidth icon="plus" size="sm" variant="secondary" onClick={onAddVariable}>
+        <Button fullWidth icon="plus" size="sm" variant="secondary" onClick={setIsAdding}>
           <Trans i18nKey="dashboard.edit-pane.variables.add-variable">Add variable</Trans>
         </Button>
       </Box>
+    </Stack>
+  );
+}
+
+interface VariableTypeSelectionProps {
+  onAddVariable: (type: EditableVariableType) => void;
+}
+
+function VariableTypeSelection({ onAddVariable }: VariableTypeSelectionProps) {
+  const options = getVariableTypeSelectOptions();
+  const styles = useStyles2(getStyles);
+
+  return (
+    <Stack direction={'column'} gap={0}>
+      <Box paddingBottom={1} display={'flex'}>
+        <Trans i18nKey="dashboard.edit-pane.variables.select-type">Choose variable type</Trans>
+      </Box>
+      {options.map((option) => (
+        <Card
+          isCompact
+          onClick={() => onAddVariable(option.value!)}
+          key={option.value}
+          title={t('dashboard.edit-pane.variables.select-type-card-tooltip', 'Click to select type')}
+        >
+          <Card.Heading>{option.label}</Card.Heading>
+          <Card.Description className={styles.cardDescription}>{option.description}</Card.Description>
+        </Card>
+      ))}
     </Stack>
   );
 }
@@ -111,6 +147,10 @@ function getStyles(theme: GrafanaTheme2) {
           visibility: 'visible',
         },
       },
+    }),
+    cardDescription: css({
+      fontSize: theme.typography.bodySmall.fontSize,
+      marginTop: theme.spacing(0),
     }),
   };
 }
