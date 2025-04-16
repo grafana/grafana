9430fecde27f08d3255440cb416069ad62a526ec
Dana Axinte <dana.axinte@grafana.com>
2025-04-16 11:35:13 +0100

    remove transactions after review

--- pkg/storage/secret/encryption/encrypted_value_store.go
+++ pkg/storage/secret/encryption/encrypted_value_store.go
@@ -11,7 +11,6 @@ import (
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/contracts"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
-	"github.com/grafana/grafana/pkg/services/sqlstore/session"
 	"github.com/grafana/grafana/pkg/storage/unified/sql/sqltemplate"
 )
 
@@ -52,17 +51,11 @@ func (s *encryptedValStorage) Create(ctx context.Context, namespace string, encr
 	}
 	query, err := sqltemplate.Execute(sqlEncryptedValueCreate, req)
 	if err != nil {
-		return nil, fmt.Errorf("execute template %q: %w", sqlEncryptedValueCreate.Name(), err)
+		return nil, fmt.Errorf("executing template %q: %w", sqlEncryptedValueCreate.Name(), err)
 	}
 
-	err = s.db.GetSqlxSession().WithTransaction(ctx, func(sess *session.SessionTx) error {
-		if _, err := sess.Exec(ctx, query, req.GetArgs()...); err != nil {
-			return fmt.Errorf("inserting row: %w", err)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("db failure: %w", err)
+	if _, err = s.db.GetSqlxSession().Exec(ctx, query, req.GetArgs()...); err != nil {
+		return nil, fmt.Errorf("inserting row: %w", err)
 	}
 
 	return &contracts.EncryptedValue{
@@ -85,25 +78,18 @@ func (s *encryptedValStorage) Update(ctx context.Context, namespace string, uid 
 
 	query, err := sqltemplate.Execute(sqlEncryptedValueUpdate, req)
 	if err != nil {
-		return fmt.Errorf("execute template %q: %w", sqlEncryptedValueUpdate.Name(), err)
+		return fmt.Errorf("executing template %q: %w", sqlEncryptedValueUpdate.Name(), err)
 	}
 
-	err = s.db.GetSqlxSession().WithTransaction(ctx, func(sess *session.SessionTx) error {
-		res, err := sess.Exec(ctx, query, req.GetArgs()...)
-		if err != nil {
-			return fmt.Errorf("updating row: %w", err)
-		}
-
-		if rowsAffected, err := res.RowsAffected(); err != nil {
-			return fmt.Errorf("getting rows affected: %w", err)
-		} else if rowsAffected == 0 {
-			return ErrEncryptedValueNotFound
-		}
-
-		return nil
-	})
+	res, err := s.db.GetSqlxSession().Exec(ctx, query, req.GetArgs()...)
 	if err != nil {
-		return fmt.Errorf("db failure: %w", err)
+		return fmt.Errorf("updating row: %w", err)
+	}
+
+	if rowsAffected, err := res.RowsAffected(); err != nil {
+		return fmt.Errorf("getting rows affected: %w", err)
+	} else if rowsAffected == 0 {
+		return ErrEncryptedValueNotFound
 	}
 
 	return nil
@@ -117,26 +103,29 @@ func (s *encryptedValStorage) Get(ctx context.Context, namespace string, uid str
 	}
 	query, err := sqltemplate.Execute(sqlEncryptedValueRead, req)
 	if err != nil {
-		return nil, fmt.Errorf("execute template %q: %w", sqlEncryptedValueRead.Name(), err)
+		return nil, fmt.Errorf("executing template %q: %w", sqlEncryptedValueRead.Name(), err)
 	}
 
-	res, err := s.db.GetSqlxSession().Query(ctx, query, req.GetArgs()...)
+	rows, err := s.db.GetSqlxSession().Query(ctx, query, req.GetArgs()...)
+	defer func() {
+		if rows != nil {
+			_ = rows.Close()
+		}
+	}()
 	if err != nil {
 		return nil, fmt.Errorf("getting row: %w", err)
 	}
 
-	defer func() { _ = res.Close() }()
-
-	if !res.Next() {
+	if !rows.Next() {
 		return nil, ErrEncryptedValueNotFound
 	}
 
 	encryptedValue := &EncryptedValue{}
-	err = res.Scan(&encryptedValue.UID, &encryptedValue.Namespace, &encryptedValue.EncryptedData, &encryptedValue.Created, &encryptedValue.Updated)
+	err = rows.Scan(&encryptedValue.UID, &encryptedValue.Namespace, &encryptedValue.EncryptedData, &encryptedValue.Created, &encryptedValue.Updated)
 	if err != nil {
 		return nil, fmt.Errorf("failed to scan encrypted value row: %w", err)
 	}
-	if err := res.Err(); err != nil {
+	if err := rows.Err(); err != nil {
 		return nil, fmt.Errorf("read rows error: %w", err)
 	}
 
@@ -157,18 +146,11 @@ func (s *encryptedValStorage) Delete(ctx context.Context, namespace string, uid 
 	}
 	query, err := sqltemplate.Execute(sqlEncryptedValueDelete, req)
 	if err != nil {
-		return fmt.Errorf("execute template %q: %w", sqlEncryptedValueDelete.Name(), err)
+		return fmt.Errorf("executing template %q: %w", sqlEncryptedValueDelete.Name(), err)
 	}
 
-	err = s.db.GetSqlxSession().WithTransaction(ctx, func(sess *session.SessionTx) error {
-		if _, err := sess.Exec(ctx, query, req.GetArgs()...); err != nil {
-			return fmt.Errorf("deleting row: %w", err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return fmt.Errorf("db failure: %w", err)
+	if _, err = s.db.GetSqlxSession().Exec(ctx, query, req.GetArgs()...); err != nil {
+		return fmt.Errorf("deleting row: %w", err)
 	}
 
 	return nil
