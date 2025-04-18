f93f609e73c3428dfc886907807fe0c7adf4a2b2
Jo Garnier <git@jguer.space>
2025-04-18 12:00:34 +0200

    ignore nullable permission
    ensure listing continues
    
    assume member permission if NULL

--- pkg/registry/apis/iam/legacy/user.go
+++ pkg/registry/apis/iam/legacy/user.go
@@ -259,13 +259,23 @@ func (s *legacySQLStore) ListUserTeams(ctx context.Context, ns claims.NamespaceI
 	var lastID int64
 	for rows.Next() {
 		t := UserTeam{}
-		err := rows.Scan(&t.ID, &t.UID, &t.Name, &t.Permission)
+
+		// regression: team_member.permission has been nulled in some instances
+		var nullablePermission *int64
+		err := rows.Scan(&t.ID, &t.UID, &t.Name, &nullablePermission)
 		if err != nil {
 			return nil, err
 		}
 
-		lastID = t.ID
+		// ignore null permissions
+		if nullablePermission != nil {
+			t.Permission = team.PermissionType(*nullablePermission)
+		} else {
+			t.Permission = team.PermissionType(0)
+		}
+
 		res.Items = append(res.Items, t)
+		lastID = t.ID
 		if len(res.Items) > int(query.Pagination.Limit)-1 {
 			res.Continue = lastID
 			res.Items = res.Items[0 : len(res.Items)-1]
