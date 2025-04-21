35ce1ea2450601521e9aba5b4679cfe8b2705827
William Wernert <william.wernert@grafana.com>
2025-04-21 11:15:09 -0400

    Alerting: Ensure field validators return the proper type (#104050)
    * Ensure field validators return the proper type
    
    This ensures correct error propagation through services up to
    the API layer.
    
    * Move error wrapping up to call site

--- pkg/services/ngalert/models/alert_rule.go
+++ pkg/services/ngalert/models/alert_rule.go
@@ -642,7 +642,7 @@ func (alertRule *AlertRule) ValidateAlertRule(cfg setting.UnifiedAlertingSetting
 		err = validateAlertRuleFields(alertRule)
 	}
 	if err != nil {
-		return err
+		return fmt.Errorf("%w: %s", ErrAlertRuleFailedValidation, err)
 	}
 
 	if alertRule.For < 0 {
@@ -682,7 +682,7 @@ func validateAlertRuleFields(rule *AlertRule) error {
 	}
 
 	if rule.MissingSeriesEvalsToResolve != nil && *rule.MissingSeriesEvalsToResolve <= 0 {
-		return fmt.Errorf("%w: field `missing_series_evals_to_resolve` must be greater than 0", ErrAlertRuleFailedValidation)
+		return errors.New("field `missing_series_evals_to_resolve` must be greater than 0")
 	}
 
 	return nil
@@ -691,10 +691,10 @@ func validateAlertRuleFields(rule *AlertRule) error {
 func validateRecordingRuleFields(rule *AlertRule) error {
 	metricName := prommodels.LabelValue(rule.Record.Metric)
 	if !metricName.IsValid() {
-		return fmt.Errorf("%w: %s", ErrAlertRuleFailedValidation, "metric name for recording rule must be a valid utf8 string")
+		return errors.New("metric name for recording rule must be a valid utf8 string")
 	}
 	if !prommodels.IsValidMetricName(metricName) {
-		return fmt.Errorf("%w: %s", ErrAlertRuleFailedValidation, "metric name for recording rule must be a valid Prometheus metric name")
+		return errors.New("metric name for recording rule must be a valid Prometheus metric name")
 	}
 
 	ClearRecordingRuleIgnoredFields(rule)
