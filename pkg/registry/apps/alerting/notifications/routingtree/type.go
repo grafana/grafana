afe06500a5a9f8f200580831ddf1e4ec85dcafb2
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-04-23 15:04:44 -0400

    remove unused code

--- pkg/registry/apps/alerting/notifications/routingtree/type.go
+++ pkg/registry/apps/alerting/notifications/routingtree/type.go
@@ -3,7 +3,6 @@ package routingtree
 import (
 	"strings"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	model "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/routingtree/v0alpha1"
@@ -11,19 +10,9 @@ import (
 )
 
 var kind = model.Kind()
-var GetOpenAPIDefinitions = model.GetOpenAPIDefinitions
 var ResourceInfo = utils.NewResourceInfo(kind.Group(), kind.Version(),
 	kind.GroupVersionResource().Resource, strings.ToLower(kind.Kind()), kind.Kind(),
 	func() runtime.Object { return kind.ZeroValue() },
 	func() runtime.Object { return kind.ZeroListValue() },
 	utils.TableColumns{},
 )
-
-func AddKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(ResourceInfo.GroupVersion(),
-		&model.RoutingTree{},
-		&model.RoutingTreeList{},
-	)
-	metav1.AddToGroupVersion(scheme, ResourceInfo.GroupVersion())
-	return nil
-}
