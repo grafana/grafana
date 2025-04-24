7b9a5851b6155d4db45af4de048aae903e7e3de8
Gareth Dawson <gwdawson.work@gmail.com>
2025-04-24 12:33:12 +0100

    update

--- pkg/tsdb/jaeger/querydata.go
+++ pkg/tsdb/jaeger/querydata.go
@@ -94,6 +94,7 @@ func queryData(ctx context.Context, dsInfo *datasourceInfo, req *backend.QueryDa
 }
 
 func transformSearchResponse(response []TraceResponse, dsInfo *datasourceInfo) *data.Frame {
+	// Create a frame for the traces
 	frame := data.NewFrame("traces",
 		data.NewField("traceID", nil, []string{}).SetConfig(&data.FieldConfig{
 			DisplayName: "Trace ID",
@@ -123,25 +124,56 @@ func transformSearchResponse(response []TraceResponse, dsInfo *datasourceInfo) *
 		}),
 	)
 
+	// Set the visualization type to table
 	frame.Meta = &data.FrameMeta{
 		PreferredVisualization: "table",
 	}
 
+	// Sort traces by start time in descending order (newest first)
+	sort.Slice(response, func(i, j int) bool {
+		rootSpanI := response[i].Spans[0]
+		rootSpanJ := response[j].Spans[0]
+
+		for _, span := range response[i].Spans {
+			if span.StartTime < rootSpanI.StartTime {
+				rootSpanI = span
+			}
+		}
+
+		for _, span := range response[j].Spans {
+			if span.StartTime < rootSpanJ.StartTime {
+				rootSpanJ = span
+			}
+		}
+
+		return rootSpanI.StartTime > rootSpanJ.StartTime
+	})
+
+	// Process each trace
 	for _, trace := range response {
 		if len(trace.Spans) == 0 {
 			continue
 		}
 
+		// Get the root span
 		rootSpan := trace.Spans[0]
+		for _, span := range trace.Spans {
+			if span.StartTime < rootSpan.StartTime {
+				rootSpan = span
+			}
+		}
 
+		// Get the service name for the trace
 		serviceName := ""
 		if process, ok := trace.Processes[rootSpan.ProcessID]; ok {
 			serviceName = process.ServiceName
 		}
 
+		// Get the trace name and start time
 		traceName := fmt.Sprintf("%s: %s", serviceName, rootSpan.OperationName)
 		startTime := time.Unix(0, rootSpan.StartTime*1000)
 
+		// Append the row to the frame
 		frame.AppendRow(
 			trace.TraceID,
 			traceName,
