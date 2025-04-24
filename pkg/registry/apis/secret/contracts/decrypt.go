225fcdabb9247ba71eaa8a53d492a66100375dca
Dana Axinte <dana.axinte@grafana.com>
2025-04-24 19:07:22 +0300

    move interface to secret service folder

--- pkg/registry/apis/secret/contracts/decrypt.go
+++ pkg/registry/apis/secret/contracts/decrypt.go
@@ -26,32 +26,3 @@ type DecryptAuthorizer interface {
 
 // TEMPORARY: Needed to pass it with wire.
 type DecryptAllowList map[string]struct{}
-
-// DecryptResult is the (union) result of a decryption operation.
-// It contains the decrypted `value` when the decryption succeeds, and the `err` when it fails.
-// It is not possible to construct a `DecryptResult` where both `value` and `err` are set from another package.
-type DecryptResult struct {
-	value *secretv0alpha1.ExposedSecureValue
-	err   error
-}
-
-func (d DecryptResult) Error() error {
-	return d.err
-}
-
-func (d DecryptResult) Value() *secretv0alpha1.ExposedSecureValue {
-	return d.value
-}
-
-func NewDecryptResultErr(err error) DecryptResult {
-	return DecryptResult{err: err}
-}
-
-func NewDecryptResultValue(value *secretv0alpha1.ExposedSecureValue) DecryptResult {
-	return DecryptResult{value: value}
-}
-
-// DecryptService is the inferface for the decrypt service.
-type DecryptService interface {
-	Decrypt(ctx context.Context, namespace string, names ...string) (map[string]DecryptResult, error)
-}
--- pkg/registry/apis/secret/decrypt/service.go
+++ pkg/registry/apis/secret/decrypt/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/grafana/grafana/pkg/registry/apis/secret/contracts"
+	"github.com/grafana/grafana/pkg/registry/apis/secret/service"
 	"github.com/grafana/grafana/pkg/registry/apis/secret/xkube"
 )
 
@@ -11,7 +12,7 @@ type OSSDecryptService struct {
 	decryptStore contracts.DecryptStorage
 }
 
-var _ contracts.DecryptService = &OSSDecryptService{}
+var _ service.DecryptService = &OSSDecryptService{}
 
 func ProvideDecryptService(decryptStore contracts.DecryptStorage) *OSSDecryptService {
 	return &OSSDecryptService{
@@ -19,15 +20,15 @@ func ProvideDecryptService(decryptStore contracts.DecryptStorage) *OSSDecryptSer
 	}
 }
 
-func (d *OSSDecryptService) Decrypt(ctx context.Context, namespace string, names ...string) (map[string]contracts.DecryptResult, error) {
-	results := make(map[string]contracts.DecryptResult, len(names))
+func (d *OSSDecryptService) Decrypt(ctx context.Context, namespace string, names ...string) (map[string]service.DecryptResult, error) {
+	results := make(map[string]service.DecryptResult, len(names))
 
 	for _, name := range names {
 		exposedSecureValue, err := d.decryptStore.Decrypt(ctx, xkube.Namespace(namespace), name)
 		if err != nil {
-			results[name] = contracts.NewDecryptResultErr(err)
+			results[name] = service.NewDecryptResultErr(err)
 		} else {
-			results[name] = contracts.NewDecryptResultValue(&exposedSecureValue)
+			results[name] = service.NewDecryptResultValue(&exposedSecureValue)
 		}
 	}
 
