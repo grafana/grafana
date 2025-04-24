109267ab03f16ebe9849625929ebfe13ecfd9db3
Gilles De Mey <gilles.de.mey@gmail.com>
2025-04-24 17:58:17 +0200

    Alerting: Remove feature toggle for custom recovery threshold (#104455)

--- pkg/services/featuremgmt/toggles_gen.go
+++ pkg/services/featuremgmt/toggles_gen.go
@@ -319,10 +319,6 @@ const (
 	// Runs CloudWatch metrics queries as separate batches
 	FlagCloudWatchBatchQueries = "cloudWatchBatchQueries"
 
-	// FlagRecoveryThreshold
-	// Enables feature recovery threshold (aka hysteresis) for threshold server-side expression
-	FlagRecoveryThreshold = "recoveryThreshold"
-
 	// FlagLokiStructuredMetadata
 	// Enables the loki data source to request structured metadata from the Loki server
 	FlagLokiStructuredMetadata = "lokiStructuredMetadata"
