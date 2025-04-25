cf3485c43cbf4524d302bcb9dbf942e598389fd6
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-04-25 10:54:48 -0300

    Secrets: outbox worker handles updates and deletes

--- pkg/registry/apis/secret/worker/worker.go
+++ pkg/registry/apis/secret/worker/worker.go
@@ -111,6 +111,12 @@ func (w *Worker) processMessage(ctx context.Context, message contracts.OutboxMes
 			return fmt.Errorf("setting secret metadata externalID: externalID=%+v message=%+v %w", externalID, message, err)
 		}
 
+		// Setting the status to Succeeded must be the last action
+		// since it acts as a fence to clients.
+		if err := w.secureValueMetadataStorage.SetStatusSucceeded(ctx, xkube.Namespace(message.Namespace), message.Name); err != nil {
+			return fmt.Errorf("setting secret metadata status to Succeeded: message=%+v", message)
+		}
+
 	case contracts.UpdateSecretOutboxMessage:
 		// TODO: DECRYPT HERE
 		rawSecret := message.EncryptedSecret.DangerouslyExposeAndConsumeValue()
@@ -119,22 +125,25 @@ func (w *Worker) processMessage(ctx context.Context, message contracts.OutboxMes
 			return fmt.Errorf("calling keeper to update secret: %w", err)
 		}
 
+		// Setting the status to Succeeded must be the last action
+		// since it acts as a fence to clients.
+		if err := w.secureValueMetadataStorage.SetStatusSucceeded(ctx, xkube.Namespace(message.Namespace), message.Name); err != nil {
+			return fmt.Errorf("setting secret metadata status to Succeeded: message=%+v", message)
+		}
+
 	case contracts.DeleteSecretOutboxMessage:
 		if err := keeper.Delete(ctx, keeperCfg, message.Namespace, contracts.ExternalID(*message.ExternalID)); err != nil {
 			return fmt.Errorf("calling keeper to delete secret: %w", err)
+		}
+		if err := w.secureValueMetadataStorage.Delete(ctx, xkube.Namespace(message.Namespace), message.Name); err != nil {
+			return fmt.Errorf("deleting secure value metadata: %+w", err)
 		}
 
 	default:
 		return fmt.Errorf("unhandled message type: %s", message.Type)
 	}
 
-	// Setting the status to Succeeded must be the last action
-	// since it acts as a fence to clients.
-	if err := w.secureValueMetadataStorage.SetStatusSucceeded(ctx, xkube.Namespace(message.Namespace), message.Name); err != nil {
-		return fmt.Errorf("setting secret metadata status to Succeeded: message=%+v", message)
-	}
-
-	// Delete the message from the queue after setting the status because
+	// Delete the message from the queue after completing all operations because
 	// if the message is deleted first, the response may be lost,
 	// resulting in an error, but since the message was actually deleted
 	// the worker would never retry.
