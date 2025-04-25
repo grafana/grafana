271f32fedf121421b34454b2bbc4e18674cccfe9
Leandro Deveikis <leandro.deveikis@gmail.com>
2025-04-25 16:13:21 -0300

    fix lint

--- pkg/storage/secret/metadata/secure_value_model.go
+++ pkg/storage/secret/metadata/secure_value_model.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"k8s.io/utils/ptr"
 	"time"
 
 	"github.com/google/uuid"
@@ -15,6 +14,7 @@ import (
 	"github.com/grafana/grafana/pkg/storage/secret/migrator"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 )
 
 type secureValueDB struct {
