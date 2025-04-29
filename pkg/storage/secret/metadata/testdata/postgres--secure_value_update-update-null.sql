77936458d80078cf948b45b03eab312fe0db9025
lean.dev <34773040+leandro-deveikis@users.noreply.github.com>
2025-04-29 18:00:34 +0100

    Migrate secureValueMetadataStorage from xorm to sqlx (#104571)

---
+++ pkg/storage/secret/metadata/testdata/postgres--secure_value_update-update-null.sql
@@ -0,0 +1,19 @@
+UPDATE
+  "secret_secure_value"
+SET
+  "guid" = 'abc',
+  "name" = 'name',
+  "namespace" = 'ns',
+  "annotations" = '{"x":"XXXX"}',
+  "labels" = '{"a":"AAA", "b", "BBBB"}',
+  "created" = 1234,
+  "created_by" = 'user:ryan',
+  "updated" = 5678,
+  "updated_by" = 'user:cameron',
+  "status_phase" = 'creating',
+  "description" = 'description',
+  "external_id" = 'extId'
+WHERE 1 = 1 AND
+  "namespace" = 'ns' AND
+  "name" = 'name'
+;
