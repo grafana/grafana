938805ced35fcc75db056fa4af0791fe3d5cd7e6
Ryan McKinley <ryantxu@gmail.com>
2025-04-30 09:09:12 +0300

    fix lint

--- pkg/services/navtree/navtreeimpl/admin.go
+++ pkg/services/navtree/navtreeimpl/admin.go
@@ -61,7 +61,7 @@ func (s *ServiceImpl) getAdminNode(c *contextmodel.ReqContext) (*navtree.NavLink
 			Url:      s.cfg.AppSubURL + "/admin/migrate-to-cloud",
 		})
 	}
-	if c.SignedInUser.HasRole(identity.RoleAdmin) &&
+	if c.HasRole(identity.RoleAdmin) &&
 		(s.cfg.StackID == "" || // show OnPrem even when provisioning is disabled
 			s.features.IsEnabledGlobally(featuremgmt.FlagProvisioning)) {
 		configNodes = append(configNodes, &navtree.NavLink{
