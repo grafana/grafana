507eae194ef86c7533bf4d82f9d762eee76a99c1
Tito Lins <tito.linsesilva@grafana.com>
2025-05-05 18:20:03 +0200

    fix linter complaints

--- pkg/services/ngalert/models/testing.go
+++ pkg/services/ngalert/models/testing.go
@@ -652,10 +652,10 @@ func (g *AlertRuleGenerator) GenerateQuery() AlertQuery {
 			From: Duration(time.Duration(f) * time.Minute),
 			To:   Duration(time.Duration(t) * time.Minute),
 		},
-		RefID:     util.GenerateShortUID(),
-		QueryType: util.GenerateShortUID(),
+		RefID:          util.GenerateShortUID(),
+		QueryType:      util.GenerateShortUID(),
 		DatasourceType: "prometheus",
-		IsMTQuery: true,
+		IsMTQuery:      true,
 	}
 }
 
