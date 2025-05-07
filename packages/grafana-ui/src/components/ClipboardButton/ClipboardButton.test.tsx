77fd14593b074882aef5a6d84461aaf074574c64
Collin Fingar <collin.fingar@grafana.com>
2025-05-07 15:32:43 -0400

    Grafana UI: Adding Some Unit Tests (#104804)
    * Grafana UI: Adding Some Unit Tests
    
    * updates per feedback
    
    * Updates per feedback

---
+++ packages/grafana-ui/src/components/ClipboardButton/ClipboardButton.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { ClipboardButton } from './ClipboardButton';
+
+const setup = (jsx: JSX.Element) => {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+};
+
+describe('ClipboardButton', () => {
+  const originalWindow = { ...window };
+
+  beforeAll(() => {
+    Object.assign(window, {
+      isSecureContext: true,
+    });
+  });
+
+  afterAll(() => {
+    Object.assign(window, originalWindow);
+  });
+
+  it('should copy text to clipboard when clicked', async () => {
+    const textToCopy = 'Copy me!';
+    const onClipboardCopy = jest.fn();
+
+    const { user } = setup(
+      <ClipboardButton getText={() => textToCopy} onClipboardCopy={onClipboardCopy}>
+        Copy
+      </ClipboardButton>
+    );
+
+    const button = screen.getByRole('button');
+    await user.click(button);
+
+    const clipboardText = await navigator.clipboard.readText();
+
+    expect(clipboardText).toBe(textToCopy);
+    expect(onClipboardCopy).toHaveBeenCalledWith(textToCopy);
+  });
+});
