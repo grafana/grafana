e088223480b2c7ef5a0d848ad896ad8fa7c92384
Fahad Siddiqui <56387313+fahad-git@users.noreply.github.com>
2025-05-07 16:14:10 +0200

    chore: Deprecate Layout components - Replace Horizontal/VerticalGroup with Stack (#86874) (#104227)
    * chore: Deprecate Layout components - Replace Horizontal/VerticalGroup with Stack (#86874)
    
    * chore: Deprecate Layout components - Replace Horizontal/VerticalGroup with Stack (#86874)
    
    * Update public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/datasource/jaeger/components/QueryEditor.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/datasource/tempo/QueryField.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/datasource/tempo/SearchTraceQLEditor/DurationInput.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/panel/nodeGraph/ViewControls.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/datasource/tempo/SearchTraceQLEditor/SearchField.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/datasource/tempo/SearchTraceQLEditor/TraceQLSearch.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/panel/nodeGraph/ViewControls.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Update public/app/plugins/datasource/zipkin/QueryField.tsx
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>
    
    * Fixed linting issue - spacing problem in different import groups.
    
    * Updated betterer results with improved 1 test.
    
    ---------
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>

--- public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
+++ public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
@@ -17,7 +17,7 @@ import React, { useState, useEffect, memo, useCallback } from 'react';
 
 import { GrafanaTheme2, SelectableValue, toOption } from '@grafana/data';
 import { IntervalInput } from '@grafana/o11y-ds-frontend';
-import { Collapse, HorizontalGroup, Icon, InlineField, InlineFieldRow, Select, Tooltip, useStyles2 } from '@grafana/ui';
+import { Collapse, Icon, InlineField, InlineFieldRow, Select, Stack, Tooltip, useStyles2 } from '@grafana/ui';
 import { t, Trans } from 'app/core/internationalization';
 
 import { defaultFilters, SearchProps } from '../../../useSearch';
@@ -144,7 +144,7 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
       <Collapse label={collapseLabel} collapsible={true} isOpen={showSpanFilters} onToggle={setShowSpanFilters}>
         <InlineFieldRow className={styles.flexContainer}>
           <InlineField label={t('explore.span-filters.label-service-name', 'Service name')} labelWidth={16}>
-            <HorizontalGroup spacing={'xs'}>
+            <Stack gap={0.5}>
               <Select
                 aria-label={t(
                   'explore.span-filters.aria-label-select-service-name-operator',
@@ -164,7 +164,7 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
                 value={search.serviceName || null}
                 defaultValue={search.serviceName || null}
               />
-            </HorizontalGroup>
+            </Stack>
           </InlineField>
           <SearchBarInput
             onChange={(v) => {
@@ -175,7 +175,7 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
         </InlineFieldRow>
         <InlineFieldRow>
           <InlineField label={t('explore.span-filters.label-span-name', 'Span name')} labelWidth={16}>
-            <HorizontalGroup spacing={'xs'}>
+            <Stack gap={0.5}>
               <Select
                 aria-label={t('explore.span-filters.aria-label-select-span-name-operator', 'Select span name operator')}
                 onChange={(v) => setSpanFiltersSearch({ ...search, spanNameOperator: v.value! })}
@@ -191,7 +191,7 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
                 placeholder={t('explore.span-filters.placeholder-all-span-names', 'All span names')}
                 value={search.spanName || null}
               />
-            </HorizontalGroup>
+            </Stack>
           </InlineField>
         </InlineFieldRow>
         <InlineFieldRow>
@@ -202,7 +202,7 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
               units: 'ns, us, ms, s, m, h',
             })}
           >
-            <HorizontalGroup spacing="xs" align="flex-start">
+            <Stack alignItems="flex-start" gap={0.5}>
               <Select
                 aria-label={t('explore.span-filters.aria-label-select-min-span-operator', 'Select min span operator')}
                 onChange={(v) => setSpanFiltersSearch({ ...search, fromOperator: v.value! })}
@@ -237,7 +237,7 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
                 value={search.to || ''}
                 validationRegex={durationRegex}
               />
-            </HorizontalGroup>
+            </Stack>
           </InlineField>
         </InlineFieldRow>
         <InlineFieldRow className={styles.tagsRow}>
