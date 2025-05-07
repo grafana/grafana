6e15de586baf34dc78c40213ec532fb5ff1e06e8
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-05-07 08:32:23 +0100

    Alerting: Tidy up some leftover logic from pre-alertingApiServer (#104776)

--- public/app/features/alerting/unified/components/alertmanager-entities/MuteTimingsSelector.tsx
+++ public/app/features/alerting/unified/components/alertmanager-entities/MuteTimingsSelector.tsx
@@ -1,7 +1,7 @@
 import { SelectableValue } from '@grafana/data';
 import { MultiSelect, MultiSelectCommonProps } from '@grafana/ui';
 import { t } from 'app/core/internationalization';
-import { useSelectableMuteTimings } from 'app/features/alerting/unified/components/mute-timings/useMuteTimings';
+import { useMuteTimings } from 'app/features/alerting/unified/components/mute-timings/useMuteTimings';
 import { BaseAlertmanagerArgs } from 'app/features/alerting/unified/types/hooks';
 import { timeIntervalToString } from 'app/features/alerting/unified/utils/alertmanager';
 import { MuteTimeInterval } from 'app/plugins/datasource/alertmanager/types';
@@ -17,7 +17,7 @@ const MuteTimingsSelector = ({
   alertmanager,
   selectProps,
 }: BaseAlertmanagerArgs & { selectProps: MultiSelectCommonProps<string> }) => {
-  const { data } = useSelectableMuteTimings({ alertmanager, skip: selectProps.disabled });
+  const { data } = useMuteTimings({ alertmanager, skip: selectProps.disabled });
 
   const muteTimingOptions = data?.map((value) => mapMuteTiming(value)) || [];
 
