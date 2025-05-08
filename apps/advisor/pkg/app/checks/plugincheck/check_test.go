1254fb9b682731a679d209aaac154fdbb08378f3
Misi <mgyongyosi@users.noreply.github.com>
2025-05-08 10:42:38 +0200

    Advisor: Return array of CheckReportFailures from checks (#104958)
    * Return array from Run
    
    * Change NewCheckReportFailure signature

--- apps/advisor/pkg/app/checks/plugincheck/check_test.go
+++ apps/advisor/pkg/app/checks/plugincheck/check_test.go
@@ -172,8 +172,8 @@ func TestRun(t *testing.T) {
 				for _, item := range items {
 					stepFailures, err := step.Run(context.Background(), logging.DefaultLogger, &advisor.CheckSpec{}, item)
 					assert.NoError(t, err)
-					if stepFailures != nil {
-						failures = append(failures, *stepFailures)
+					if len(stepFailures) > 0 {
+						failures = append(failures, stepFailures...)
 					}
 				}
 			}
