f2a9a156b11b29b81509f303a93e57a664432311
Ashley Harrison <ashley.harrison@grafana.com>
2025-05-08 14:10:06 +0100

    Profile page: Add horizontal scroll for teams table (#105111)
    add horizontal scroll for teams table

--- public/app/features/profile/UserTeams.tsx
+++ public/app/features/profile/UserTeams.tsx
@@ -1,6 +1,6 @@
 import { PureComponent } from 'react';
 
-import { LoadingPlaceholder } from '@grafana/ui';
+import { LoadingPlaceholder, ScrollContainer } from '@grafana/ui';
 import { t, Trans } from 'app/core/internationalization';
 import { Team } from 'app/types';
 
@@ -26,39 +26,41 @@ export class UserTeams extends PureComponent<Props> {
         <h3 className="page-sub-heading">
           <Trans i18nKey="profile.user-teams.teams">Teams</Trans>
         </h3>
-        <table
-          className="filter-table form-inline"
-          aria-label={t('profile.user-teams.aria-label-user-teams-table', 'User teams table')}
-        >
-          <thead>
-            <tr>
-              <th />
-              <th>
-                <Trans i18nKey="profile.user-teams.name">Name</Trans>
-              </th>
-              <th>
-                <Trans i18nKey="profile.user-teams.email">Email</Trans>
-              </th>
-              <th>
-                <Trans i18nKey="profile.user-teams.members">Members</Trans>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {teams.map((team: Team, index) => {
-              return (
-                <tr key={index}>
-                  <td className="width-4 text-center">
-                    <img className="filter-table__avatar" src={team.avatarUrl} alt="" />
-                  </td>
-                  <td>{team.name}</td>
-                  <td>{team.email}</td>
-                  <td>{team.memberCount}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <ScrollContainer overflowY="visible" overflowX="auto" width="100%">
+          <table
+            className="filter-table form-inline"
+            aria-label={t('profile.user-teams.aria-label-user-teams-table', 'User teams table')}
+          >
+            <thead>
+              <tr>
+                <th />
+                <th>
+                  <Trans i18nKey="profile.user-teams.name">Name</Trans>
+                </th>
+                <th>
+                  <Trans i18nKey="profile.user-teams.email">Email</Trans>
+                </th>
+                <th>
+                  <Trans i18nKey="profile.user-teams.members">Members</Trans>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {teams.map((team: Team, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="width-4 text-center">
+                      <img className="filter-table__avatar" src={team.avatarUrl} alt="" />
+                    </td>
+                    <td>{team.name}</td>
+                    <td>{team.email}</td>
+                    <td>{team.memberCount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </ScrollContainer>
       </div>
     );
   }
