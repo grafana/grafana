8266b76c75d6568c042225d0b7a2f49cb1ecf875
Bård Dahlmo-Lerbæk <bard.dahlmo-lerbaek@skatteetaten.no>
2025-05-09 08:06:53 +0200

    Update categories.ts
    Changed dBm from SIPrefix to toFixedUnit.

--- packages/grafana-data/src/valueFormats/categories.ts
+++ packages/grafana-data/src/valueFormats/categories.ts
@@ -246,7 +246,7 @@ export const getCategories = (): ValueFormatCategory[] => [
       { name: 'Volt (V)', id: 'volt', fn: SIPrefix('V') },
       { name: 'Kilovolt (kV)', id: 'kvolt', fn: SIPrefix('V', 1) },
       { name: 'Millivolt (mV)', id: 'mvolt', fn: SIPrefix('V', -1) },
-      { name: 'Decibel-milliwatt (dBm)', id: 'dBm', fn: SIPrefix('dBm') },
+      { name: 'Decibel-milliwatt (dBm)', id: 'dBm', fn: toFixedUnit('dBm') },
       { name: 'Milliohm (mΩ)', id: 'mohm', fn: SIPrefix('Ω', -1) },
       { name: 'Ohm (Ω)', id: 'ohm', fn: SIPrefix('Ω') },
       { name: 'Kiloohm (kΩ)', id: 'kohm', fn: SIPrefix('Ω', 1) },
