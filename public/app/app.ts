84d63177591bcaf9e39ce8a173a6ba4a2cb34f96
joshhunt <josh.hunt@grafana.com>
2025-05-10 00:55:30 +0100

    remove console.log

--- public/app/app.ts
+++ public/app/app.ts
@@ -285,7 +285,6 @@ function addExtensionReducers() {
 
 function initExtensions() {
   if (extensionsExports.length > 0) {
-    console.log('Initializing extension', extensionsExports);
     extensionsExports[0].init();
   }
 }
