92cbc66dd7cee4199836c747a0e79a4ab80d8b22
eledobleefe <laura.fernandez@grafana.com>
2025-05-12 18:30:39 +0200

    Localise mapRangeToTimeOption

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/mapper.ts
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/mapper.ts
@@ -1,4 +1,6 @@
 import { TimeOption, TimeRange, TimeZone, rangeUtil, dateTimeFormat } from '@grafana/data';
+import { formatDate } from '@grafana/i18n';
+import { getFeatureToggle } from 'src/utils/featureToggle';
 
 export const mapOptionToTimeRange = (option: TimeOption, timeZone?: TimeZone): TimeRange => {
   return rangeUtil.convertRawToRange({ from: option.from, to: option.to }, timeZone);
@@ -7,10 +9,23 @@ export const mapOptionToTimeRange = (option: TimeOption, timeZone?: TimeZone): T
 export const mapRangeToTimeOption = (range: TimeRange, timeZone?: TimeZone): TimeOption => {
   const from = dateTimeFormat(range.from, { timeZone });
   const to = dateTimeFormat(range.to, { timeZone });
+  const display = `${from} to ${to}`;
+  const formatOptions: Intl.DateTimeFormatOptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  };
+  const isLocaleToggleEnabled = getFeatureToggle('localeFormatPreference');
+  const displayFrom = formatDate(range.from.toDate(), formatOptions);
+  const displayTo = formatDate(range.to.toDate(), formatOptions);
+  const displayRegionLocale = `${displayFrom} - ${displayTo}`;
 
   return {
     from,
     to,
-    display: `${from} to ${to}`,
+    display: isLocaleToggleEnabled ? displayRegionLocale : display,
   };
 };
