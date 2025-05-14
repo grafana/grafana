4ef7a4ecb8fd9d4c38b8225643e3271e189bb893
Scott Lepper <scott.lepper@gmail.com>
2025-05-14 11:11:16 -0400

    lint

--- public/app/features/dashboard-scene/settings/variables/editors/AdHocFiltersVariableEditor.test.tsx
+++ public/app/features/dashboard-scene/settings/variables/editors/AdHocFiltersVariableEditor.test.tsx
@@ -1,4 +1,4 @@
-import { render, act, waitFor, within, screen } from '@testing-library/react';
+import { render, act, waitFor, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import { of } from 'rxjs';
