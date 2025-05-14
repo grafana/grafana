a8114a32e4e2f562156e7140a9126cf411b33a8b
Gilles De Mey <gilles.de.mey@gmail.com>
2025-05-14 16:57:59 +0200

    fix codegen

--- packages/grafana-alerting/src/grafana/contactPoints/components/ContactPointSelector/ContactPointSelector.test.tsx
+++ packages/grafana-alerting/src/grafana/contactPoints/components/ContactPointSelector/ContactPointSelector.test.tsx
@@ -1,4 +1,4 @@
-import server, { setupMockServer } from '@grafana/test-utils/server';
+import { setupMockServer } from '@grafana/test-utils/server';
 
 import { screen, render, within } from '../../../../../tests/test-utils';
 import { getContactPointDescription } from '../../utils';
@@ -6,7 +6,7 @@ import { getContactPointDescription } from '../../utils';
 import { ContactPointSelector } from './ContactPointSelector';
 import { simpleContactPointsList, simpleContactPointsListScenario } from './ContactPointSelector.test.scenario';
 
-setupMockServer();
+const server = setupMockServer();
 
 beforeEach(() => {
   server.use(...simpleContactPointsListScenario);
