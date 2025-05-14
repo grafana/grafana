080bd6cc75732b589a4b389405fa2943a345d158
Sam Jewell <sam.jewell@grafana.com>
2025-05-14 09:54:20 +0100

    use jest.mocked for a type rich mock
    First in the BasicMatcherEditor.test
    
    Thanks to @fastfrwrd for the PR feedback

--- public/app/features/transformers/FilterByValueTransformer/ValueMatchers/BasicMatcherEditor.test.tsx
+++ public/app/features/transformers/FilterByValueTransformer/ValueMatchers/BasicMatcherEditor.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
-import { FieldType } from '@grafana/data';
+import { FieldType, TypedVariableModel } from '@grafana/data';
 import { getTemplateSrv } from '@grafana/runtime';
 
 import { basicMatcherEditor } from './BasicMatcherEditor';
@@ -12,14 +12,19 @@ jest.mock('@grafana/runtime', () => ({
 describe('BasicMatcherEditor', () => {
   it('shows variable suggestions', async () => {
     // Mock template service variables
-    const mockVariables = [
-      { name: 'var1', label: 'Variable 1', value: '$var1' },
-      { name: 'var2', label: 'Variable 2', value: '$var2' },
+    const mockVariables: TypedVariableModel[] = [
+      { name: 'var1', label: 'Variable 1', type: 'custom' } as TypedVariableModel,
+      { name: 'var2', label: 'Variable 2', type: 'custom' } as TypedVariableModel,
     ];
 
-    (getTemplateSrv as jest.Mock).mockReturnValue({
+    const mockTemplateSrv = {
       getVariables: () => mockVariables,
-    });
+      replace: jest.fn(),
+      containsTemplate: jest.fn(),
+      updateTimeRange: jest.fn(),
+    };
+
+    jest.mocked(getTemplateSrv).mockReturnValue(mockTemplateSrv);
 
     const onChangeMock = jest.fn();
     const options = { value: '' };
@@ -45,7 +50,7 @@ describe('BasicMatcherEditor', () => {
       expect(suggestions).toHaveLength(mockVariables.length);
 
       mockVariables.forEach((variable) => {
-        const suggestion = suggestions.find((s) => s.textContent?.includes(variable.label));
+        const suggestion = suggestions.find((s) => s.textContent?.includes(variable.label as string));
         expect(suggestion).toBeInTheDocument();
       });
     });
