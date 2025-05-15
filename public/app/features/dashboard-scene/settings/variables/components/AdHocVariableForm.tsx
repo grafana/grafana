ffed87cc7eedaa1417c050d764dcb65f5854309e
Scott Lepper <scott.lepper@gmail.com>
2025-05-14 20:03:17 -0400

    make required

--- public/app/features/dashboard-scene/settings/variables/components/AdHocVariableForm.tsx
+++ public/app/features/dashboard-scene/settings/variables/components/AdHocVariableForm.tsx
@@ -20,7 +20,7 @@ export interface AdHocVariableFormProps {
   onDefaultKeysChange?: (keys?: MetricFindValue[]) => void;
   onAllowCustomValueChange?: (event: FormEvent<HTMLInputElement>) => void;
   inline?: boolean;
-  datasourceSupported?: boolean;
+  datasourceSupported: boolean;
 }
 
 export function AdHocVariableForm({
