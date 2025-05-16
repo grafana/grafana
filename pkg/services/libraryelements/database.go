c263f14182b72531e0963817ac8dcef7c20c0db6
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2025-05-16 15:43:40 +0300

    undo unwanted change

--- pkg/services/libraryelements/database.go
+++ pkg/services/libraryelements/database.go
@@ -900,13 +900,11 @@ func (l *LibraryElementService) disconnectElementsFromDashboardID(c context.Cont
 // deleteLibraryElementsInFolderUID deletes all Library Elements in a folder.
 func (l *LibraryElementService) deleteLibraryElementsInFolderUID(c context.Context, signedInUser identity.Requester, folderUID string) error {
 	return l.SQLStore.WithTransactionalDbSession(c, func(session *db.Session) error {
-		evaluator := ac.EvalPermission(dashboards.ActionFoldersWrite, dashboards.ScopeFoldersProvider.GetResourceScopeUID(folderUID))
-		canEdit, err := l.AccessControl.Evaluate(c, signedInUser, evaluator)
-		if err != nil {
+		if err := l.requireEditPermissionsOnFolderUID(c, signedInUser, folderUID); err != nil {
+			if errors.Is(err, dashboards.ErrDashboardNotFound) {
+				return dashboards.ErrFolderNotFound
+			}
 			return err
-		}
-		if !canEdit {
-			return dashboards.ErrFolderAccessDenied
 		}
 		var connectionIDs []struct {
 			ConnectionID int64 `xorm:"connection_id"`
@@ -914,7 +912,7 @@ func (l *LibraryElementService) deleteLibraryElementsInFolderUID(c context.Conte
 		sql := "SELECT lec.connection_id FROM library_element AS le"
 		sql += " INNER JOIN " + model.LibraryElementConnectionTableName + " AS lec on le.id = lec.element_id"
 		sql += " WHERE le.folder_uid=? AND le.org_id=?"
-		err = session.SQL(sql, folderUID, signedInUser.GetOrgID()).Find(&connectionIDs)
+		err := session.SQL(sql, folderUID, signedInUser.GetOrgID()).Find(&connectionIDs)
 		if err != nil {
 			return err
 		}
