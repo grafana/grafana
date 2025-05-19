b6f0105058106e6f2a36e752e5022949eef24e67
Paul Marbach <paul.marbach@grafana.com>
2025-05-19 15:42:20 -0400

    test: more tests for hasMappedNaN and hasMappedNull

--- public/app/core/components/TimelineChart/timeline.test.ts
+++ public/app/core/components/TimelineChart/timeline.test.ts
@@ -6,6 +6,8 @@ import { VisibilityMode } from '@grafana/schema';
 import { getConfig, TimelineCoreOptions } from './timeline';
 import { TimelineMode } from './utils';
 
+jest.mock('uplot');
+
 describe('StateTimeline uPlot integration', () => {
   const buildTestCoreOptions = (opts: Partial<TimelineCoreOptions> = {}): TimelineCoreOptions => ({
     mode: TimelineMode.Changes,
@@ -15,7 +17,7 @@ describe('StateTimeline uPlot integration', () => {
     isDiscrete: jest.fn(() => true),
     hasMappedNull: jest.fn(() => false),
     hasMappedNaN: jest.fn(() => false),
-    getValueColor: jest.fn(() => 'white'),
+    getValueColor: jest.fn(() => '#fff'),
     label: jest.fn(() => 'foo'),
     getTimeRange: jest.fn(() => getDefaultTimeRange()),
     getFieldConfig: jest.fn(() => ({})),
@@ -23,13 +25,68 @@ describe('StateTimeline uPlot integration', () => {
     ...opts,
   });
 
-  beforeAll(() => {
-    jest.mock('uplot', () => {
-      const uPlot = jest.requireActual('uplot');
-      jest.spyOn(uPlot, 'orient');
-      return uPlot;
-    });
-  });
+  const buildMockUplotInstance = (
+    data: Array<Array<null | number>> = [
+      [0, 0, 0],
+      [0, 1, 2],
+    ]
+  ) =>
+    ({
+      ctx: {
+        save: jest.fn(),
+        restore: jest.fn(),
+        rect: jest.fn(),
+        clip: jest.fn(),
+        font: '',
+        fill: jest.fn(),
+        fillStyle: '',
+        fillText: jest.fn(),
+        measureText: jest.fn(() => ({ width: 0 })),
+        beginPath: jest.fn(),
+      },
+      root: document.createElement('div'),
+      bbox: { left: 0, top: 0, width: 100, height: 100 },
+      data,
+      cursor: { left: 0, top: 0 },
+      series: [{}],
+      scales: {},
+      opts: {},
+      pxRatio: 1,
+      posToVal: jest.fn(),
+      valToPos: jest.fn(),
+    }) as unknown as uPlot;
+
+  const callOrientCallback = (mockUplot: uPlot) => {
+    const orientCallback = jest.mocked(uPlot.orient).mock.calls[jest.mocked(uPlot.orient).mock.calls.length - 1][2];
+    const methods = {
+      moveTo: jest.fn(() => {}),
+      lineTo: jest.fn(() => {}),
+      rect: jest.fn(() => {}),
+      arc: jest.fn(() => {}),
+      bezierCurveTo: jest.fn(() => {}),
+    };
+
+    orientCallback(
+      mockUplot.series[0],
+      mockUplot.data[0] as number[],
+      mockUplot.data[1] as number[],
+      mockUplot.scales.x,
+      mockUplot.scales.y,
+      jest.fn(() => 1),
+      jest.fn(() => 1),
+      0,
+      0,
+      100,
+      100,
+      methods.moveTo,
+      methods.lineTo,
+      methods.rect,
+      methods.arc,
+      methods.bezierCurveTo
+    );
+
+    return methods;
+  };
 
   describe('#drawPoints', () => {
     it('returns a `drawPoints` method when a `formatValue` function is provided', () => {
@@ -47,12 +104,108 @@ describe('StateTimeline uPlot integration', () => {
       expect(config.drawPoints).toBe(false);
     });
 
-    // it('returns false for `drawPoints` if the visibility mode is `never`', () => {
-    //   const { drawPoints } = getConfig(buildTestCoreOptions({ formatValue: () => 'foo' }));
-    //   if (!drawPoints) {
-    //     throw new Error('drawPoints is not defined');
-    //   }
-    //   drawPoints(uPlot, 0, );
-    // });
+    it('returns a function for `drawPoints` if the conditions are met', () => {
+      const { drawPoints } = getConfig(buildTestCoreOptions({ formatValue: () => 'foo' }));
+      if (!drawPoints) {
+        throw new Error('drawPoints is not defined');
+      }
+      const mockUplot = buildMockUplotInstance();
+      expect(drawPoints(mockUplot, 1, 2, 3, null)).toBe(false);
+      expect(uPlot.orient).toHaveBeenCalledWith(mockUplot, 1, expect.any(Function));
+    });
+
+    describe('#drawPaths', () => {
+      describe('null and NaN values', () => {
+        // these tests are attempting to determine whether `shouldDrawBox` is returning false
+        // and preventing a draw for a given value. this is being done by checking the number of
+        // calls to `rect` by the `orient` callback created by a `drawPaths` call. Even
+
+        it('should draw boxes for null values when hasMappedNull returns true and isDiscrete returns true', () => {
+          const { drawClear, drawPaths } = getConfig(
+            buildTestCoreOptions({
+              hasMappedNull: jest.fn(() => true),
+              isDiscrete: jest.fn(() => true),
+              formatValue: () => 'foo',
+            })
+          );
+          const mockUplot = buildMockUplotInstance([[0], [null]]);
+
+          drawClear(mockUplot);
+          drawPaths(mockUplot, 1, 0, 1);
+
+          const { rect } = callOrientCallback(mockUplot);
+          expect(rect).toHaveBeenCalledTimes(2);
+        });
+
+        it('should not draw boxes for null values when hasMappedNull returns false and isDiscrete returns true', () => {
+          const { drawClear, drawPaths } = getConfig(
+            buildTestCoreOptions({
+              hasMappedNull: jest.fn(() => false),
+              isDiscrete: jest.fn(() => true),
+              formatValue: () => 'foo',
+            })
+          );
+          const mockUplot = buildMockUplotInstance([[0], [null]]);
+
+          drawClear(mockUplot);
+          drawPaths(mockUplot, 1, 0, 1);
+
+          const { rect } = callOrientCallback(mockUplot);
+          expect(rect).toHaveBeenCalledTimes(1);
+        });
+
+        it('should draw boxes for NaN values when hasMappedNaN returns true and isDiscrete returns true', () => {
+          const { drawClear, drawPaths } = getConfig(
+            buildTestCoreOptions({
+              hasMappedNaN: jest.fn(() => true),
+              isDiscrete: jest.fn(() => true),
+              formatValue: () => 'foo',
+            })
+          );
+          const mockUplot = buildMockUplotInstance([[0], [NaN]]);
+
+          drawClear(mockUplot);
+          drawPaths(mockUplot, 1, 0, 1);
+
+          const { rect } = callOrientCallback(mockUplot);
+          expect(rect).toHaveBeenCalledTimes(2);
+        });
+
+        it('should not draw boxes for NaN values when hasMappedNaN returns false and isDiscrete returns true', () => {
+          const { drawClear, drawPaths } = getConfig(
+            buildTestCoreOptions({
+              hasMappedNaN: jest.fn(() => false),
+              isDiscrete: jest.fn(() => true),
+              formatValue: () => 'foo',
+            })
+          );
+          const mockUplot = buildMockUplotInstance([[0], [NaN]]);
+
+          drawClear(mockUplot);
+          drawPaths(mockUplot, 1, 0, 1);
+
+          const { rect } = callOrientCallback(mockUplot);
+          expect(rect).toHaveBeenCalledTimes(1);
+        });
+
+        it('should not draw boxes for NaN or null values when isDiscrete returns false', () => {
+          const { drawClear, drawPaths } = getConfig(
+            buildTestCoreOptions({
+              hasMappedNaN: jest.fn(() => true),
+              hasMappedNull: jest.fn(() => true),
+              isDiscrete: jest.fn(() => false),
+              formatValue: () => 'foo',
+            })
+          );
+          const mockUplot = buildMockUplotInstance([[0], [NaN, null]]);
+
+          drawClear(mockUplot);
+          drawPaths(mockUplot, 1, 0, 1);
+
+          const { rect } = callOrientCallback(mockUplot);
+          expect(rect).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
   });
 });
