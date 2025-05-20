81dc2f7cfa9291a6819d73d833f006bd1aec4ca8
MattIPv4 <me@mattcowley.co.uk>
2025-05-20 17:09:17 +0100

    Home: remove now unused homeSetupGuide feature toggle

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -671,10 +671,6 @@ export interface FeatureToggles {
   */
   exploreLogsLimitedTimeRange?: boolean;
   /**
-  * Used in Home for users who want to return to the onboarding flow or quickly find popular config pages
-  */
-  homeSetupGuide?: boolean;
-  /**
   * Enables the gRPC client to authenticate with the App Platform by using ID & access tokens
   */
   appPlatformGrpcClientAuth?: boolean;
