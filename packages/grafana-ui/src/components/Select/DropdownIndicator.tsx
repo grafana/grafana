aa257cebdfd0bff01e65b63888598c6b24b2d328
Tobias Skarhed <tobias.skarhed@gmail.com>
2025-05-21 15:57:27 +0200

    Replace the dropdown icon with loading to reduce jankiness

--- packages/grafana-ui/src/components/Select/DropdownIndicator.tsx
+++ packages/grafana-ui/src/components/Select/DropdownIndicator.tsx
@@ -1,10 +1,27 @@
+import { css } from '@emotion/css';
 import { DropdownIndicatorProps } from 'react-select';
 
+import { GrafanaTheme2 } from '@grafana/data';
+
+import { useStyles2 } from '../../themes';
 import { Icon } from '../Icon/Icon';
+import { Spinner } from '../Spinner/Spinner';
 
 export function DropdownIndicator({ selectProps }: DropdownIndicatorProps) {
   const isOpen = selectProps.menuIsOpen;
   const icon = isOpen ? 'search' : 'angle-down';
   const size = 'md';
+  const styles = useStyles2(getStyles);
+
+  if (selectProps.isLoading) {
+    return <Spinner inline className={styles.spinner} />;
+  }
+
   return <Icon name={icon} size={size} />;
 }
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  spinner: css({
+    marginTop: -4, // Because the spinner is misaligned for some reason
+  }),
+});
