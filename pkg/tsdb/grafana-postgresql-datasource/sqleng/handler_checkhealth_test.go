9c641fd176d6c763eb7623e4b8537e05cc46a285
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2025-05-21 14:18:53 +0100

    fix typo

--- pkg/tsdb/grafana-postgresql-datasource/sqleng/handler_checkhealth_test.go
+++ pkg/tsdb/grafana-postgresql-datasource/sqleng/handler_checkhealth_test.go
@@ -54,7 +54,7 @@ func TestErrToHealthCheckResult(t *testing.T) {
 			err:  fmt.Errorf("%w %q: %w", ErrParsingPostgresURL, `"foo.bar.co"`, errors.New(`strconv.Atoi: parsing "foo.bar.co": invalid syntax`)),
 			want: &backend.CheckHealthResult{
 				Status:      backend.HealthStatusError,
-				Message:     "Connection string error: ierror parsing postgres url",
+				Message:     "Connection string error: error parsing postgres url",
 				JSONDetails: []byte(`{"errorDetailsLink":"https://grafana.com/docs/grafana/latest/datasources/postgres","verboseMessage":"error parsing postgres url \"\\\"foo.bar.co\\\"\": strconv.Atoi: parsing \"foo.bar.co\": invalid syntax"}`),
 			},
 		},
