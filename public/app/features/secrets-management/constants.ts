5cd8dd32d8fb358bbf1793e83c92f7b6f5dd9a7a
Matheus Macabu <macabu.matheus@gmail.com>
2025-05-21 16:33:43 +0200

    SecureValues: Update decrypter format to get rid of actor_ prefix

--- public/app/features/secrets-management/constants.ts
+++ public/app/features/secrets-management/constants.ts
@@ -1,12 +1,12 @@
 import { Entries } from './types';
 
 // @see https://github.com/grafana/grafana-enterprise/blob/secret-service/feature-branch/src/pkg/extensions/secret/decrypt/allow_list.go
-export const DECRYPT_ALLOW_LIST = ['actor_k6', 'actor_synthetic-monitoring'] as const;
+export const DECRYPT_ALLOW_LIST = ['k6', 'synthetic-monitoring'] as const;
 export type AllowedDecrypter = (typeof DECRYPT_ALLOW_LIST)[number];
 
 export const DECRYPT_ALLOW_LIST_LABEL_MAP: Record<AllowedDecrypter, string> = {
-  actor_k6: 'k6',
-  'actor_synthetic-monitoring': 'Synthetic Monitoring',
+  k6: 'k6',
+  'synthetic-monitoring': 'Synthetic Monitoring',
 };
 
 export const DECRYPT_ALLOW_LIST_OPTIONS = (
