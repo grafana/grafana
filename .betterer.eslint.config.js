df71cb14522bf6381ebef6fe5820c0df18f2650e
Tobias Skarhed <tobias.skarhed@gmail.com>
2025-05-22 10:38:12 +0200

    Update betterer message

--- .betterer.eslint.config.js
+++ .betterer.eslint.config.js
@@ -122,11 +122,11 @@ module.exports = [
         'error',
         {
           selector: 'Identifier[name=localStorage]',
-          message: 'Direct usage of localStorage is not allowed. import store from app/core/store in core and Store from @grafana/data in packages (grafana-data, grafana-runtime, grafana-prometheus, grafana-ui, etc.)',
+          message: 'Direct usage of localStorage is not allowed. import store from @grafana/data instead',
         },
         {
           selector: 'MemberExpression[object.name=localStorage]',
-          message: 'Direct usage of localStorage is not allowed. import store from app/core/store in core and Store from @grafana/data in packages (grafana-data, grafana-runtime, grafana-prometheus, grafana-ui, etc.)',
+          message: 'Direct usage of localStorage is not allowed. import store from @grafana/data instead',
         },
         {
           selector:
