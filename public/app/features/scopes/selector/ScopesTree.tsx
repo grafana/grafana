89627fd191ac72989dad6a9fbf57875c78b53b5c
Andrej Ocenas <mr.ocenas@gmail.com>
2025-05-22 13:37:30 +0200

    Move ScopesTreeItemList.tsx into separate file

--- public/app/features/scopes/selector/ScopesTree.tsx
+++ public/app/features/scopes/selector/ScopesTree.tsx
@@ -2,13 +2,12 @@ import { css } from '@emotion/css';
 import Skeleton from 'react-loading-skeleton';
 
 import { GrafanaTheme2, Scope } from '@grafana/data';
-import { ScrollContainer, useStyles2 } from '@grafana/ui';
+import { useStyles2 } from '@grafana/ui';
 
 import { RecentScopes } from './RecentScopes';
 import { ScopesTreeHeadline } from './ScopesTreeHeadline';
-import { ScopesTreeItem } from './ScopesTreeItem';
+import { ScopesTreeItemList } from './ScopesTreeItemList';
 import { ScopesTreeSearch } from './ScopesTreeSearch';
-import { isNodeSelectable } from './scopesTreeUtils';
 import { NodesMap, SelectedScope, TreeNode } from './types';
 
 export interface ScopesTreeProps {
@@ -80,7 +79,7 @@ export function ScopesTree({
         <Skeleton count={5} className={styles.loader} />
       ) : (
         <>
-          <TreeItemList
+          <ScopesTreeItemList
             items={selectedNodesToShow}
             anyChildExpanded={anyChildExpanded}
             lastExpandedNode={lastExpandedNode}
@@ -100,7 +99,7 @@ export function ScopesTree({
             scopeNodes={scopeNodes}
           />
 
-          <TreeItemList
+          <ScopesTreeItemList
             items={childrenArray}
             anyChildExpanded={anyChildExpanded}
             lastExpandedNode={lastExpandedNode}
@@ -118,87 +117,8 @@ export function ScopesTree({
   );
 }
 
-type TreeItemListProps = {
-  anyChildExpanded: boolean;
-  lastExpandedNode: boolean;
-  loadingNodeName: string | undefined;
-  items: TreeNode[];
-  maxHeight: string;
-  selectedScopes: SelectedScope[];
-  scopeNodes: NodesMap;
-  onNodeUpdate: (scopeNodeId: string, expanded: boolean, query: string) => void;
-  selectScope: (scopeNodeId: string) => void;
-  deselectScope: (scopeNodeId: string) => void;
-};
-
-function TreeItemList({
-  items,
-  anyChildExpanded,
-  lastExpandedNode,
-  maxHeight,
-  selectedScopes,
-  scopeNodes,
-  loadingNodeName,
-  onNodeUpdate,
-  selectScope,
-  deselectScope,
-}: TreeItemListProps) {
-  const styles = useStyles2(getStyles);
-
-  if (items.length === 0) {
-    return null;
-  }
-
-  const children = (
-    <div role="tree" className={anyChildExpanded ? styles.expandedContainer : undefined}>
-      {items.map((childNode) => {
-        const selected =
-          isNodeSelectable(scopeNodes[childNode.scopeNodeId]) &&
-          selectedScopes.some((s) => {
-            if (s.scopeNodeId) {
-              // If we have scopeNodeId we only match based on that so even if the actual scope is the same we don't
-              // mark different scopeNode as selected.
-              return s.scopeNodeId === childNode.scopeNodeId;
-            } else {
-              return s.scopeId === scopeNodes[childNode.scopeNodeId]?.spec.linkId;
-            }
-          });
-        return (
-          <ScopesTreeItem
-            key={childNode.scopeNodeId}
-            treeNode={childNode}
-            selected={selected}
-            selectedScopes={selectedScopes}
-            scopeNodes={scopeNodes}
-            loadingNodeName={loadingNodeName}
-            anyChildExpanded={anyChildExpanded}
-            onNodeUpdate={onNodeUpdate}
-            selectScope={selectScope}
-            deselectScope={deselectScope}
-          />
-        );
-      })}
-    </div>
-  );
-
-  if (lastExpandedNode) {
-    return (
-      <ScrollContainer minHeight={`${Math.min(5, items.length) * 30}px`} maxHeight={maxHeight}>
-        {children}
-      </ScrollContainer>
-    );
-  }
-
-  return children;
-}
-
 const getStyles = (theme: GrafanaTheme2) => {
   return {
-    expandedContainer: css({
-      display: 'flex',
-      flexDirection: 'column',
-      maxHeight: '100%',
-    }),
     loader: css({
       margin: theme.spacing(0.5, 0),
     }),
