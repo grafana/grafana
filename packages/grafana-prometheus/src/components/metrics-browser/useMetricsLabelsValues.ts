7e64b93b5faf253bbde62e9b9ce330a63d07aebd
ismail simsek <ismailsimsek09@gmail.com>
2025-05-25 21:59:40 +0200

    fix some types

--- packages/grafana-prometheus/src/components/metrics-browser/useMetricsLabelsValues.ts
+++ packages/grafana-prometheus/src/components/metrics-browser/useMetricsLabelsValues.ts
@@ -1,14 +1,14 @@
-import { useCallback, useEffect, useRef, useState, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useDebounce } from 'react-use';
 
 import { TimeRange } from '@grafana/data';
 
-import PromQlLanguageProvider from '../../language_provider';
+import { PrometheusLanguageProviderInterface } from '../../language_provider';
 
 import { buildSelector } from './selectorBuilder';
 import { DEFAULT_SERIES_LIMIT, EMPTY_SELECTOR, LAST_USED_LABELS_KEY, Metric, METRIC_LABEL } from './types';
 
-export const useMetricsLabelsValues = (timeRange: TimeRange, languageProvider: PromQlLanguageProvider) => {
+export const useMetricsLabelsValues = (timeRange: TimeRange, languageProvider: PrometheusLanguageProviderInterface) => {
   const timeRangeRef = useRef<TimeRange>(timeRange);
   const lastSeriesLimitRef = useRef(DEFAULT_SERIES_LIMIT);
   const isInitializedRef = useRef(false);
