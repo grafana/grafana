817974d1da2a53a7fb3d056cd6b7f5dc9e1718c7
Will Assis <william@williamassis.com>
2025-05-26 10:19:21 -0300

    test

--- devenv/docker/blocks/mysql_tests/setup.sql
+++ devenv/docker/blocks/mysql_tests/setup.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE grafana_ds_tests;
 GRANT ALL PRIVILEGES ON grafana_ds_tests.* TO 'grafana';
-CREATE DATABASE grafana_tests;
+CREATE DATABASE IF NOT EXISTS grafana_tests;
 GRANT ALL PRIVILEGES ON grafana_tests.* TO 'grafana';
