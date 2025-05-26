e9f4ae9655633bfe45f5f52ac1d9c3d8486466ca
Dana Axinte <53751979+dana-axinte@users.noreply.github.com>
2025-05-26 18:28:53 +0100

    SecretsManager: Conditionally lock DB before migrations using config setting (#106003)
    Secrets: Conditionally lock DB before migrations using config setting (#105949)
    
    Co-authored-by: Matheus Macabu <macabu@users.noreply.github.com>

--- pkg/registry/apis/secret/register.go
+++ pkg/registry/apis/secret/register.go
@@ -69,7 +69,10 @@ func RegisterAPIService(
 		return nil, nil
 	}
 
-	if err := secretDBMigrator.RunMigrations(); err != nil {
+	// Some DBs that claim to be MySQL/Postgres-compatible might not support table locking.
+	lockDatabase := cfg.Raw.Section("database").Key("migration_locking").MustBool(true)
+
+	if err := secretDBMigrator.RunMigrations(context.Background(), lockDatabase); err != nil {
 		return nil, fmt.Errorf("running secret database migrations: %w", err)
 	}
 
