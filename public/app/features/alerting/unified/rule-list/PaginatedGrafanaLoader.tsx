23bf35c43d72ec010ed919ff9c0d19468c541135
Sonia Aguilar <33540275+soniaAguilarPeiron@users.noreply.github.com>
2025-06-04 15:51:11 +0200

    Alerting: Add loading spinner for loading groups state (#106289)
    Add loading spinner for loading groups state

--- public/app/features/alerting/unified/rule-list/PaginatedGrafanaLoader.tsx
+++ public/app/features/alerting/unified/rule-list/PaginatedGrafanaLoader.tsx
@@ -1,7 +1,8 @@
 import { groupBy, isEmpty } from 'lodash';
 import { useEffect, useMemo, useRef } from 'react';
 
-import { Icon, Stack, Text } from '@grafana/ui';
+import { Trans } from '@grafana/i18n';
+import { Icon, Spinner, Stack, Text } from '@grafana/ui';
 import { GrafanaRuleGroupIdentifier, GrafanaRulesSourceSymbol } from 'app/types/unified-alerting';
 import { GrafanaPromRuleGroupDTO } from 'app/types/unified-alerting-dto';
 
@@ -83,6 +84,12 @@ export function PaginatedGrafanaLoader() {
           <div>
             <LoadMoreButton onClick={fetchMoreGroups} />
           </div>
+        )}
+        {isLoading && (
+          <Stack direction="row" gap={2} alignItems="center" justifyContent="flex-start">
+            <Spinner inline={true} />
+            <Trans i18nKey="alerting.rule-list.loading-more-groups">Loading more groups...</Trans>
+          </Stack>
         )}
       </Stack>
     </DataSourceSection>
