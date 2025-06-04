4eafb1c4cb55e11532fea79adef35ed51b5ab6c7
Gilles De Mey <gilles.de.mey@gmail.com>
2025-06-04 15:52:20 +0200

    add rollup config

---
+++ packages/grafana-alerting/rollup.config.ts
@@ -0,0 +1,33 @@
+import { createRequire } from 'node:module';
+
+import { cjsOutput, entryPoint, esmOutput, plugins, tsDeclarationOutput } from '../rollup.config.parts';
+
+const rq = createRequire(import.meta.url);
+const pkg = rq('./package.json');
+
+export default [
+  {
+    input: entryPoint,
+    plugins,
+    output: [cjsOutput(pkg), esmOutput(pkg, 'grafana-alerting')],
+  },
+  {
+    input: 'src/unstable.ts',
+    plugins,
+    output: [cjsOutput(pkg), esmOutput(pkg, 'grafana-alerting')],
+  },
+  tsDeclarationOutput(pkg),
+  tsDeclarationOutput(pkg, {
+    input: './compiled/unstable.d.ts',
+    output: [
+      {
+        file: './dist/cjs/unstable.d.cts',
+        format: 'cjs',
+      },
+      {
+        file: './dist/esm/unstable.d.mts',
+        format: 'es',
+      },
+    ],
+  }),
+];
