1809f1323dc8c3bc2ec9d8ae758081b32a64df76
shub <shubhamnainwal70@gmail.com>
2025-06-06 02:01:00 +0530

    feat: add selected columns support to GROUP BY dropdown

--- packages/grafana-sql/src/components/visual-query-builder/SQLOrderByRow.tsx
+++ packages/grafana-sql/src/components/visual-query-builder/SQLOrderByRow.tsx
@@ -6,6 +6,8 @@ import { useSqlChange } from '../../utils/useSqlChange';
 
 import { OrderByRow } from './OrderByRow';
 
+import { getColumnsWithIndices } from '../../utils/getColumnsWithIndices';
+
 type SQLOrderByRowProps = {
   fields: SelectableValue[];
   query: QueryWithDefaults;
@@ -15,26 +17,6 @@ type SQLOrderByRowProps = {
 
 export function SQLOrderByRow({ fields, query, onQueryChange, db }: SQLOrderByRowProps) {
   const { onSqlChange } = useSqlChange({ query, onQueryChange, db });
-  let columnsWithIndices: SelectableValue[] = [];
-
-  if (fields) {
-    const options = query.sql?.columns?.map((c, i) => {
-      const value = c.name ? `${c.name}(${c.parameters?.map((p) => p.name)})` : c.parameters?.map((p) => p.name);
-      return {
-        value,
-        label: `${i + 1} - ${value}`,
-      };
-    });
-    columnsWithIndices = [
-      {
-        value: '',
-        label: 'Selected columns',
-        options,
-        expanded: true,
-      },
-      ...fields,
-    ];
-  }
-
+  let columnsWithIndices: SelectableValue[]=getColumnsWithIndices(query, fields);
   return <OrderByRow sql={query.sql!} onSqlChange={onSqlChange} columns={columnsWithIndices} />;
 }
