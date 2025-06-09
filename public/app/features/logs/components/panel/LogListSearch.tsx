c3a56443a5b93004fd666f8a97fc02535845630e
Matias Chomicki <matyax@gmail.com>
2025-06-09 16:32:39 +0200

    LogListSearch: implement go to results

--- public/app/features/logs/components/panel/LogListSearch.tsx
+++ public/app/features/logs/components/panel/LogListSearch.tsx
@@ -1,13 +1,15 @@
-import { css } from "@emotion/css";
-import { VariableSizeList } from "react-window";
-import tinycolor from "tinycolor2";
+import { css } from '@emotion/css';
+import UFuzzy from '@leeoniya/ufuzzy';
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import { VariableSizeList } from 'react-window';
+import tinycolor from 'tinycolor2';
 
-import { GrafanaTheme2 } from "@grafana/data";
-import { useTranslate } from "@grafana/i18n";
-import { IconButton, Input, useStyles2 } from "@grafana/ui";
+import { GrafanaTheme2 } from '@grafana/data';
+import { useTranslate } from '@grafana/i18n';
+import { IconButton, Input, useStyles2 } from '@grafana/ui';
 
-import { useLogListContext } from "./LogListContext";
-import { LogListModel } from "./processing";
+import { useLogListContext } from './LogListContext';
+import { LogListModel } from './processing';
 
 interface Props {
   listRef: VariableSizeList | null;
@@ -17,22 +19,90 @@ interface Props {
 
 export const LogListSearch = ({ listRef, logs, width }: Props) => {
   const { searchVisible, hideSearch } = useLogListContext();
+  const [search, setSearch] = useState('');
+  const [currentResult, setCurrentResult] = useState<number | null>(null);
   const styles = useStyles2(getStyles);
   const { t } = useTranslate();
-  
+
+  const filterer = useMemo(() => new UFuzzy({ intraMode: 1 }), []);
+  const matches = useMemo(
+    () => (search !== '' ? filterer.filter(getHaystack(logs), search) : null),
+    [filterer, logs, search]
+  );
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setSearch(input);
+  }, []);
+
+  const prevResult = useCallback(() => {
+    if (!matches || currentResult === null) {
+      return;
+    }
+    const prev = currentResult > 0 ? currentResult - 1 : matches.length - 1;
+    setCurrentResult(prev);
+    listRef?.scrollToItem(matches[prev], 'center');
+  }, [currentResult, listRef, matches]);
+
+  const nextResult = useCallback(() => {
+    if (!matches || currentResult === null) {
+      return;
+    }
+    const next = currentResult <= matches.length - 1 ? currentResult + 1 : 0;
+    setCurrentResult(next);
+    listRef?.scrollToItem(matches[next], 'center');
+  }, [currentResult, listRef, matches]);
+
+  useEffect(() => {
+    if (!matches || !matches.length) {
+      setCurrentResult(null);
+      return;
+    }
+    if (currentResult === null) {
+      setCurrentResult(0);
+      listRef?.scrollToItem(matches[0], 'center');
+    }
+  }, [currentResult, listRef, matches]);
+
+  useEffect(() => {
+    if (!searchVisible) {
+      setSearch('');
+    }
+  }, [searchVisible]);
+
   if (!searchVisible) {
     return null;
   }
 
+  const suffix = search !== '' ? <>{`${currentResult ? currentResult + 1 : 0}/${matches?.length ?? 0}`}</> : undefined;
+
   return (
     <div className={styles.container} style={{ width }}>
       <div style={{ width: Math.round(width / 2) }}>
-        <Input autoFocus placeholder={t('logs.log-list-search.input-placeholder', 'Search in logs')} />
+        <Input
+          value={search}
+          onChange={handleChange}
+          autoFocus
+          placeholder={t('logs.log-list-search.input-placeholder', 'Search in logs')}
+          suffix={suffix}
+        />
       </div>
+      <IconButton
+        onClick={prevResult}
+        disabled={!matches || !matches.length}
+        name="angle-up"
+        aria-label={t('logs.log-list-search.prev', 'Previous result')}
+      />
+      <IconButton
+        onClick={nextResult}
+        disabled={!matches || !matches.length}
+        name="angle-down"
+        aria-label={t('logs.log-list-search.next', 'Next result')}
+      />
       <IconButton onClick={hideSearch} name="times" aria-label={t('logs.log-list-search.close', 'Close search')} />
     </div>
-  )
-}
+  );
+};
 
 const getStyles = (theme: GrafanaTheme2) => ({
   container: css({
@@ -45,5 +115,8 @@ const getStyles = (theme: GrafanaTheme2) => ({
     left: theme.spacing(1.25),
     zIndex: theme.zIndex.modal,
   }),
-})
+});
 
+function getHaystack(logs: LogListModel[]) {
+  return logs.map((log) => log.entry);
+}
