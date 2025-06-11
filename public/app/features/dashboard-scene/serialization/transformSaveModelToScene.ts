827b29564b34a93effab5b833057fe0f59473258
Dominik Süß <dominik@suess.wtf>
2025-06-11 10:41:57 +0200

    refactor: split building VizPanel and GridItem into separate functions

--- public/app/features/dashboard-scene/serialization/transformSaveModelToScene.ts
+++ public/app/features/dashboard-scene/serialization/transformSaveModelToScene.ts
@@ -294,9 +294,9 @@ export function createDashboardSceneFromDashboardModel(oldModel: DashboardModel,
   const scopeMeta =
     config.featureToggles.scopeFilters && oldModel.scopeMeta
       ? {
-          trait: oldModel.scopeMeta.trait,
-          groups: oldModel.scopeMeta.groups,
-        }
+        trait: oldModel.scopeMeta.trait,
+        groups: oldModel.scopeMeta.groups,
+      }
       : undefined;
 
   // Create profiler once and reuse to avoid duplicate metadata setting
@@ -402,14 +402,7 @@ export function createDashboardSceneFromDashboardModel(oldModel: DashboardModel,
   return dashboardScene;
 }
 
-export function buildGridItemForPanel(panel: PanelModel): DashboardGridItem {
-  const repeatOptions: Partial<{ variableName: string; repeatDirection: RepeatDirection }> = panel.repeat
-    ? {
-        variableName: panel.repeat,
-        repeatDirection: panel.repeatDirection === 'v' ? 'v' : 'h',
-      }
-    : {};
-
+export function buildVizPanelForPanel(panel: PanelModel): VizPanel {
   const titleItems: SceneObject[] = [];
 
   titleItems.push(
@@ -481,8 +474,18 @@ export function buildGridItemForPanel(panel: PanelModel): DashboardGridItem {
     });
   }
 
-  const body = new VizPanel(vizPanelState);
+  return new VizPanel(vizPanelState);
+}
 
+export function buildGridItemForPanel(panel: PanelModel): DashboardGridItem {
+  const repeatOptions: Partial<{ variableName: string; repeatDirection: RepeatDirection }> = panel.repeat
+    ? {
+      variableName: panel.repeat,
+      repeatDirection: panel.repeatDirection === 'v' ? 'v' : 'h',
+    }
+    : {};
+
+  const body = buildVizPanelForPanel(panel);
   return new DashboardGridItem({
     key: `grid-item-${panel.id}`,
     x: panel.gridPos.x,
