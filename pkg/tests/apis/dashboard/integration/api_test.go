556c807d20cd11cd37dd9407a0f633d7fc31c3f8
Mustafa Sencer Özcan <mustafasencer.ozcan@grafana.com>
2025-06-11 08:05:40 +0200

    fix: typo

--- pkg/tests/apis/dashboard/integration/api_test.go
+++ pkg/tests/apis/dashboard/integration/api_test.go
@@ -135,7 +135,7 @@ func TestIntegrationDashboardAPIAuthorization(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
-	// TODO: rest.Mode3 is failint on dashboard creation, so we skip it for now
+	// TODO: rest.Mode3 is failing on dashboard creation, so we skip it for now
 	dualWriterModes := []rest.DualWriterMode{rest.Mode0, rest.Mode1, rest.Mode2, rest.Mode4, rest.Mode5}
 	for _, dualWriterMode := range dualWriterModes {
 		t.Run(fmt.Sprintf("DualWriterMode %d", dualWriterMode), func(t *testing.T) {
@@ -186,7 +186,7 @@ func TestIntegrationDashboardAPI(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
-	// TODO: rest.Mode3 is failint on dashboard creation, so we skip it for now
+	// TODO: rest.Mode3 is failing on dashboard creation, so we skip it for now
 	dualWriterModes := []rest.DualWriterMode{rest.Mode0, rest.Mode1, rest.Mode2, rest.Mode4, rest.Mode5}
 	for _, dualWriterMode := range dualWriterModes {
 		t.Run(fmt.Sprintf("DualWriterMode %d", dualWriterMode), func(t *testing.T) {
