d781b3b7933d2cf7430d24aa73e52abf42975b61
Andreas Christou <andreas.christou@grafana.com>
2025-06-11 13:54:48 +0100

    Fix errors

--- public/app/plugins/datasource/graphite/datasource.ts
+++ public/app/plugins/datasource/graphite/datasource.ts
@@ -979,7 +979,10 @@ export class GraphiteDatasource
       .fetch(options)
       .pipe(
         catchError((err) => {
-          return throwError(reduceError(err));
+          return throwError(() => {
+            const reduced = reduceError(err);
+            return new Error(`${reduced.data.message}`);
+          });
         })
       );
   }
