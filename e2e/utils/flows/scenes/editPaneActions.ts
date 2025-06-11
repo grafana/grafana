b78c17c6e734838aaa2186868ed9499a43a74812
Sergej-Vlasov <37613182+Sergej-Vlasov@users.noreply.github.com>
2025-06-11 13:36:12 +0100

    DashboardsE2E: Add E2E tests for Auto grid (#106171)
    * add auto grid e2e tests
    
    * remove unnecessary clean up in grouping tests
    
    * refaactor repeated code
    
    * Refactor some repeated blocks
    
    ---------
    
    Co-authored-by: kay delaney <kay@grafana.com>

--- e2e/utils/flows/scenes/editPaneActions.ts
+++ e2e/utils/flows/scenes/editPaneActions.ts
@@ -12,6 +12,14 @@ export const selectRowsLayout = () => {
   clickGroupLayoutButton('Rows');
 };
 
+export const selectCustomGridLayout = () => {
+  clickGroupLayoutButton('Custom');
+};
+
+export const selectAutoGridLayout = () => {
+  clickGroupLayoutButton('Auto grid');
+};
+
 const editPaneCopyOrDuplicate = (buttonLabel: string) => {
   e2e.components.EditPaneHeader.copyDropdown().click();
   cy.get('[role="menu"]').within(() => {
