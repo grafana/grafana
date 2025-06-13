6a11d462cb8292cdc78b050a9fff97a00fac4d4b
Alex Khomenko <Clarity-89@users.noreply.github.com>
2025-06-13 12:24:37 +0200

    API: Support versioned frontend clients (#106545)
    * Update the generator to include version
    
    * Add versioned APIs
    
    * Update imports
    
    * Prettier

--- public/app/api/clients/advisor/v0alpha1/baseAPI.ts
+++ public/app/api/clients/advisor/v0alpha1/baseAPI.ts
@@ -1,12 +1,12 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 
-import { config } from '@grafana/runtime';
 import { createBaseQuery } from 'app/api/createBaseQuery';
+import { getAPIBaseURL } from 'app/api/utils';
 
-export const BASE_URL = `apis/provisioning.grafana.app/v0alpha1/namespaces/${config.namespace}`;
+export const BASE_URL = getAPIBaseURL('advisor.grafana.app', 'v0alpha1');
 
 export const api = createApi({
-  reducerPath: 'provisioningAPI',
+  reducerPath: 'advisorAPIv0alpha1',
   baseQuery: createBaseQuery({
     baseURL: BASE_URL,
   }),
