ddad9628477d43a9387c262b1c4124cd93e97e84
Kristina Durivage <kristina.durivage@grafana.com>
2025-06-13 15:16:12 -0500

    Remove all suggestions, just use table/column population but trigger on space

--- public/app/features/expressions/components/sqlCompletionProvider.ts
+++ public/app/features/expressions/components/sqlCompletionProvider.ts
@@ -1,27 +1,15 @@
 import { SelectableValue } from '@grafana/data';
-import {
-  ColumnDefinition,
-  CompletionItemKind,
-  CompletionItemPriority,
-  LanguageCompletionProvider,
-  LinkedToken,
-  PositionContext,
-  SuggestionKind,
-  SuggestionKindProvider,
-  TableDefinition,
-  TableIdentifier,
-  TokenType,
-} from '@grafana/plugin-ui';
+import { ColumnDefinition, LanguageCompletionProvider, TableDefinition, TableIdentifier } from '@grafana/plugin-ui';
 
 interface CompletionProviderGetterArgs {
   getMeta: (t: TableIdentifier) => Promise<ColumnDefinition[]>;
   refIds: Array<SelectableValue<string>>;
 }
 
-// set the language to undefined so autocomplete only shows what we specify
 export const getSqlCompletionProvider: (args: CompletionProviderGetterArgs) => LanguageCompletionProvider =
   (args) => (monaco, language) => ({
     ...language,
+    triggerCharacters: [' '],
     tables: {
       resolve: async () => {
         const refIdsToTableDefs = args.refIds.map((refId) => {
@@ -39,53 +27,4 @@ export const getSqlCompletionProvider: (args: CompletionProviderGetterArgs) => L
         return await args.getMeta({ table: t?.table });
       },
     },
-    //customSuggestionKinds: customSuggestionKinds(args),
   });
-
-export const customSuggestionKinds: (args: CompletionProviderGetterArgs) => SuggestionKindProvider = (args) => () => [
-  {
-    id: SuggestionKind.Tables,
-    overrideDefault: true,
-    suggestionsResolver: async (ctx) => {
-      const refIdsToTableDefs = args.refIds.map((refId) => {
-        const tableDef: TableDefinition = {
-          name: refId.label || refId.value || '',
-          completion: refId.label || refId.value || '',
-        };
-        return tableDef;
-      });
-      return refIdsToTableDefs.map(mapToSuggestion(ctx));
-    },
-  },
-  {
-    id: SuggestionKind.Columns,
-    overrideDefault: true,
-    suggestionsResolver: async (ctx) => {
-      const table = getTableName(ctx.currentToken);
-      const suggestions = await args.getMeta({ table });
-      return suggestions.map(mapToSuggestion(ctx));
-    },
-  },
-];
-
-function mapToSuggestion(ctx: PositionContext) {
-  return function (tableDefinition: TableDefinition) {
-    return {
-      label: tableDefinition.name,
-      insertText: tableDefinition.completion ?? tableDefinition.name,
-      command: { id: 'editor.action.triggerSuggest', title: '' },
-      kind: CompletionItemKind.Field,
-      sortText: CompletionItemPriority.High,
-      range: {
-        ...ctx.range,
-        startColumn: ctx.range.endColumn,
-        endColumn: ctx.range.endColumn,
-      },
-    };
-  };
-}
-
-function getTableName(token: LinkedToken | null | undefined) {
-  const identifier = token?.getNextOfType(TokenType.Identifier);
-  return identifier?.value;
-}
