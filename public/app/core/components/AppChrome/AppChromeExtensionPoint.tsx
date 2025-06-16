d4358dbc38e8ae904d3991ac0b081a27a2448375
Marcus Andersson <marcus.andersson@grafana.com>
2025-06-16 19:52:48 +0200

    Excluding the app chrome extension to be rendered on login/signup pages.

--- public/app/core/components/AppChrome/AppChromeExtensionPoint.tsx
+++ public/app/core/components/AppChrome/AppChromeExtensionPoint.tsx
@@ -1,9 +1,29 @@
 import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { PluginExtensionPoints } from '@grafana/data';
 import { usePluginComponents } from '@grafana/runtime';
 
+const excludedRoutes: Record<string, boolean> = {
+  '/login': true,
+  '/signup': true,
+  '/verify': true,
+  '/user/password/send-reset-email': true,
+  '/user/password/reset': true,
+  '/sandbox/benchmarks': true,
+};
+
 export function AppChromeExtensionPoint(): JSX.Element | null {
+  const location = useLocation();
+
+  if (excludedRoutes[location.pathname]) {
+    return null;
+  }
+
+  return <InternalAppChromeExtensionPoint />;
+}
+
+function InternalAppChromeExtensionPoint(): JSX.Element | null {
   const { components, isLoading } = usePluginComponents({
     extensionPointId: PluginExtensionPoints.AppChrome,
   });
