65db9a94d2af7dca28e37c903578b0d67fa06a9a
Paul Marbach <paul.marbach@grafana.com>
2025-06-16 11:58:20 -0400

    fix lint

--- packages/grafana-ui/src/components/Table/TableNG/TableNG.test.tsx
+++ packages/grafana-ui/src/components/Table/TableNG/TableNG.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { applyFieldOverrides, createTheme, DataFrame, EventBus, FieldType, toDataFrame } from '@grafana/data';
