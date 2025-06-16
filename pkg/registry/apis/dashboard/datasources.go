30b0f77fc27062eda3232de62d3388025c499bc5
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2025-06-16 15:53:41 +0200

    Dashboard V0->V1 Migration: Schema migration v36 (#100757)
    ---------
    
    Co-authored-by: Todd Treece <360020+toddtreece@users.noreply.github.com>
    Co-authored-by: Haris Rozajac <haris.rozajac12@gmail.com>
    Co-authored-by: Stephanie Hingtgen <stephanie.hingtgen@grafana.com>

---
+++ pkg/registry/apis/dashboard/datasources.go
@@ -0,0 +1,36 @@
+package dashboard
+
+import (
+	"context"
+
+	"github.com/grafana/grafana/apps/dashboard/pkg/migration/schemaversion"
+	"github.com/grafana/grafana/pkg/services/datasources"
+)
+
+type datasourceInfoProvider struct {
+	datasourceService datasources.DataSourceService
+}
+
+func (d *datasourceInfoProvider) GetDataSourceInfo() []schemaversion.DataSourceInfo {
+	query := datasources.GetAllDataSourcesQuery{}
+	dataSources, err := d.datasourceService.GetAllDataSources(context.Background(), &query)
+
+	if err != nil {
+		return []schemaversion.DataSourceInfo{}
+	}
+
+	out := make([]schemaversion.DataSourceInfo, 0, len(dataSources))
+
+	for _, ds := range dataSources {
+		out = append(out, schemaversion.DataSourceInfo{
+			Name:       ds.Name,
+			UID:        ds.UID,
+			ID:         ds.ID,
+			Type:       ds.Type,
+			Default:    ds.IsDefault,
+			APIVersion: ds.APIVersion,
+		})
+	}
+
+	return out
+}
