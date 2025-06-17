9522cea64f9de6403b4f874110673654b9ec52a2
Mariell Hoversholm <mariell@mardroemmar.dev>
2025-06-17 13:51:19 +0200

    Actions: Install Chrome

--- pkg/build/a11y/run.go
+++ pkg/build/a11y/run.go
@@ -16,7 +16,9 @@ func RunTest(
 			" --grafana-host grafana --grafana-port 3001 %s", runnerFlags)
 
 	return GrafanaFrontend(d, cache, nodeVersion, src).
-		WithExec([]string{"/bin/sh", "-c", "apt-get update && apt-get install -y git"}).
+		WithExec([]string{"/bin/sh", "-c", "apt-get update && apt-get install -y git curl"}).
+		WithExec([]string{"curl", "-LO", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"}).
+		WithExec([]string{"apt-get", "install", "-y", "./google-chrome-stable_current_amd64.deb"}).
 		WithWorkdir("/src").
 		WithServiceBinding("grafana", svc).
 		WithExec([]string{"/bin/bash", "-c", command}, dagger.ContainerWithExecOpts{Expect: dagger.ReturnTypeAny})
