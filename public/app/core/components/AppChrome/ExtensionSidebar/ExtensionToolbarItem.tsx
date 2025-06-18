a1bb128b2bebec989c413083e0263bed5b1c303b
Jack Baldry <jack.baldry@grafana.com>
2025-06-18 13:42:39 +0100

    Fix undefined variable
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- public/app/core/components/AppChrome/ExtensionSidebar/ExtensionToolbarItem.tsx
+++ public/app/core/components/AppChrome/ExtensionSidebar/ExtensionToolbarItem.tsx
@@ -14,6 +14,7 @@ export function ExtensionToolbarItem() {
     useExtensionSidebarContext();
 
   let dockedComponentTitle = '';
+  let dockedPluginId = '';
   if (dockedComponentId) {
     const dockedComponent = getComponentMetaFromComponentId(dockedComponentId);
     if (dockedComponent) {
