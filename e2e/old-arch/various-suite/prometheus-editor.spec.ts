93f18c176f3afd33f1fc393b7c289f7ae1c9d671
idastambuk <ida.stambuk@grafana.com>
2025-06-20 11:40:33 +0200

    Use e2e to select elements

--- e2e/old-arch/various-suite/prometheus-editor.spec.ts
+++ e2e/old-arch/various-suite/prometheus-editor.spec.ts
@@ -1,5 +1,3 @@
-import { selectors } from '@grafana/e2e-selectors';
-
 import { e2e } from '../utils';
 
 import { getResources } from './helpers/prometheus-helpers';
@@ -67,13 +65,9 @@ describe('Prometheus query editor', () => {
     // check options
     e2e.components.DataSource.Prometheus.queryEditor.legend().scrollIntoView().should('exist');
     e2e.components.DataSource.Prometheus.queryEditor.format().scrollIntoView().should('exist');
-    cy.get(`[data-testid="${selectors.components.DataSource.Prometheus.queryEditor.step}"]`)
-      .scrollIntoView()
-      .should('exist');
+    e2e.components.DataSource.Prometheus.queryEditor.step().scrollIntoView().should('exist');
     e2e.components.DataSource.Prometheus.queryEditor.type().scrollIntoView().should('exist');
-    cy.get(`[data-testid="${selectors.components.DataSource.Prometheus.queryEditor.exemplars}"]`)
-      .scrollIntoView()
-      .should('exist');
+    e2e.components.DataSource.Prometheus.queryEditor.exemplars().scrollIntoView().should('exist');
   });
 
   describe('Code editor', () => {
