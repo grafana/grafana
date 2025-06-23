5305e284730c521acd7f5032804d17125f7ab7d5
Matias Chomicki <matyax@gmail.com>
2025-06-23 18:40:00 +0200

    LogLineDetails: process links and add sample sections

--- public/app/features/logs/components/panel/LogLineDetails.tsx
+++ public/app/features/logs/components/panel/LogLineDetails.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/css';
 import { Resizable } from 're-resizable';
-import { useCallback, useRef } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { t } from '@grafana/i18n';
@@ -11,6 +11,7 @@ import { LogDetails } from './LogDetails';
 import { useLogListContext } from './LogListContext';
 import { LogListModel } from './processing';
 import { LOG_LIST_MIN_WIDTH } from './virtualization';
+import { createLogLineLinks } from '../logParser';
 
 interface Props {
   containerElement: HTMLDivElement;
@@ -89,30 +90,43 @@ export const LogLineDetails = ({ containerElement, getFieldLinks, logs, onResize
               />
             </tbody>
           </table>
-          <LogDetailsComponent styles={styles} />
+          <LogDetailsComponent log={showDetails[0]} styles={styles} />
         </div>
       </div>
     </Resizable>
   );
 };
 
-const LogDetailsComponent = ({ styles }: { styles: LogLineDetailsStyles }) => {
+const LogDetailsComponent = ({ log, styles }: { log: LogListModel; styles: LogLineDetailsStyles }) => {
+  const links = useMemo(() => {
+    const fieldsWithLinks = log.fields.filter((f) => f.links?.length);
+    const displayedFieldsWithLinks = fieldsWithLinks.filter((f) => f.fieldIndex !== log.entryFieldIndex).sort();
+    const hiddenFieldsWithLinks = fieldsWithLinks.filter((f) => f.fieldIndex === log.entryFieldIndex).sort();
+    const fieldsWithLinksFromVariableMap = createLogLineLinks(hiddenFieldsWithLinks);
+    return [...displayedFieldsWithLinks, ...fieldsWithLinksFromVariableMap];
+  }, [log.entryFieldIndex, log.fields]);
+
   return (
     <div className={styles.componentWrapper}>
       <ControlledCollapse label={t('logs.log-line-details.log-line-section', 'Log line')} collapsible>
-        <p>Log line</p>
+        {log.raw}
+      </ControlledCollapse>
+      <ControlledCollapse label={t('logs.log-line-details.links-section', 'Links')} collapsible>
+        {links.map((link, i) => (
+          <div key={i}>Link</div>
+        ))}
       </ControlledCollapse>
       <ControlledCollapse
         label={t('logs.log-line-details.structured-metadata-section', 'Structured metadata')}
         collapsible
       >
-        <p>Log line</p>
+        <p>Metadata</p>
       </ControlledCollapse>
-      <ControlledCollapse label={t('logs.log-line-details.structured-metadata-section', 'Parsed fields')} collapsible>
-        <p>Log line</p>
+      <ControlledCollapse label={t('logs.log-line-details.parsed-fields-section', 'Parsed fields')} collapsible>
+        <p>Fields</p>
       </ControlledCollapse>
-      <ControlledCollapse label={t('logs.log-line-details.structured-metadata-section', 'Indexed labels')} collapsible>
-        <p>Log line</p>
+      <ControlledCollapse label={t('logs.log-line-details.indexed-labels-section', 'Indexed labels')} collapsible>
+        <p>Labels</p>
       </ControlledCollapse>
     </div>
   );
