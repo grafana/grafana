a7bd8f2b06a6c443657c727a6f1f5f10558f26ec
Josh Hunt <joshhunt@users.noreply.github.com>
2025-06-24 14:40:37 +0100

    CI: Fix pa11y check by running in docker-puppeteer image (#107084)
    * Change pa11y dagger to run in docker-puppeteer image
    
    * export json results
    
    * re-enable pa11y ci check
    
    * update gha workflow to new flags
    
    * add no-threshold-fail, use single pa11y config
    
    * fix codeowners
    
    * readme
    
    * fix drone config

--- pkg/build/a11y/run.go
+++ pkg/build/a11y/run.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"dagger.io/dagger"
 )
 
 func RunTest(
+	ctx context.Context,
 	d *dagger.Client,
-	svc *dagger.Service,
-	src *dagger.Directory, cache *dagger.CacheVolume,
-	nodeVersion, runnerFlags string) *dagger.Container {
-	command := fmt.Sprintf(
-		"./e2e-runner a11y --start-grafana=false"+
-			" --grafana-host grafana --grafana-port 3001 %s", runnerFlags)
-
-	return GrafanaFrontend(d, cache, nodeVersion, src).
-		WithExec([]string{"/bin/sh", "-c", "apt-get update && apt-get install -y git curl"}).
-		WithExec([]string{"curl", "-LO", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"}).
-		WithExec([]string{"apt-get", "install", "-y", "./google-chrome-stable_current_amd64.deb"}).
+	grafanaService *dagger.Service,
+	pa11yConfig *dagger.File,
+	noThresholdFail bool,
+	pa11yResultsPath string,
+) (*dagger.Container, error) {
+	// docker-puppeteer container already has Chrome and Pa11y installed in it
+	pa11yContainer := d.Container().From("grafana/docker-puppeteer:1.1.0").
 		WithWorkdir("/src").
-		WithServiceBinding("grafana", svc).
-		WithExec([]string{"/bin/bash", "-c", command}, dagger.ContainerWithExecOpts{Expect: dagger.ReturnTypeAny})
+		WithExec([]string{"mkdir", "-p", "./screenshots"}). // not yet exported
+		WithEnvVariable("HOST", grafanaHost).
+		WithEnvVariable("PORT", fmt.Sprint(grafanaPort))
+
+	if noThresholdFail {
+		// This logic is non-intuitive - --no-threshold-fail will make pa11y fail (by removing thresholds from the config)
+		// so it can write all violations to the results file. This failure is then ignored by the caller in main.go.
+		// Otherwise, pa11y ignores violations if they're within the thresholds and doesn't include them in the results file
+		pa11yContainer = pa11yContainer.
+			WithEnvVariable("NO_THRESHOLDS", "true")
+	}
+
+	pa11yContainer = pa11yContainer.
+		WithServiceBinding(grafanaHost, grafanaService).
+		WithMountedFile("pa11yci-config.js", pa11yConfig).
+		WithExec([]string{"pa11y-ci", "--config", "pa11yci-config.js"}, dagger.ContainerWithExecOpts{
+			Expect: dagger.ReturnTypeAny, // allow this to fail here so we can handle non-zero exit codes at the caller
+		})
+
+	if pa11yResultsPath != "" {
+		_, err := pa11yContainer.File("/src/pa11y-ci-results.json").Export(ctx, pa11yResultsPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get pa11y results: %w", err)
+		}
+	}
+
+	return pa11yContainer, nil
 }
