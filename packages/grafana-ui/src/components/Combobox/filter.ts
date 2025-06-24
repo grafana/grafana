17c4268f31b7e450f48aadd566334d63aee99aef
Clarity-89 <homes89@ukr.net>
2025-06-24 13:33:37 +0300

    Cleanup

--- packages/grafana-ui/src/components/Combobox/filter.ts
+++ packages/grafana-ui/src/components/Combobox/filter.ts
@@ -1,26 +1,12 @@
 import { fuzzySearch } from '@grafana/data';
 
-import { ALL_OPTION_VALUE, ComboboxOption } from './types';
+import { ComboboxOption } from './types';
 
 export function itemToString<T extends string | number>(item?: ComboboxOption<T> | null) {
   if (item == null) {
     return '';
   }
   return item.label ?? item.value.toString();
-}
-
-//TODO: Remove when MutliCombobox async has been merged
-export function itemFilter<T extends string | number>(inputValue: string) {
-  const lowerCasedInputValue = inputValue.toLowerCase();
-
-  return (item: ComboboxOption<T>) => {
-    return (
-      !inputValue ||
-      item.label?.toLowerCase().includes(lowerCasedInputValue) ||
-      item.value?.toString().toLowerCase().includes(lowerCasedInputValue) ||
-      item.value.toString() === ALL_OPTION_VALUE
-    );
-  };
 }
 
 export function fuzzyFind<T extends string | number>(
