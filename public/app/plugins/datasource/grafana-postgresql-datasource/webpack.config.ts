6a8170021a41c95290e697e0530bbf909dc2a304
Jack Westbrook <jack.westbrook@gmail.com>
2025-06-24 11:06:51 +0200

    refactor(plugins): use allowImportingTsExtensions in favour of ts-ignore comments

--- public/app/plugins/datasource/grafana-postgresql-datasource/webpack.config.ts
+++ public/app/plugins/datasource/grafana-postgresql-datasource/webpack.config.ts
@@ -1,4 +1,3 @@
-// @ts-ignore - node needs the extension to strip types successfully
 import config from '@grafana/plugin-configs/webpack.config.ts';
 
 // eslint-disable-next-line no-barrel-files/no-barrel-files
