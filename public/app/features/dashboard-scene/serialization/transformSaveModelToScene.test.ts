ec2bfc45feae29e205934c70f27e42c186e9bd4a
Torkel Ödegaard <torkel@grafana.com>
2025-06-25 10:02:42 +0200

    RowsLayout: Rethinking row repeats (#104312)
    * Something is working
    
    * Update
    
    * Update
    
    * working
    
    * clear repeated rows
    
    * Update
    
    * Update
    
    * Outline via function
    
    * Update
    
    * Update
    
    * Update
    
    * Progress
    
    * Update
    
    * Udpate sum
    
    * Update
    
    * Update public/app/features/dashboard-scene/scene/types/EditableDashboardElement.ts
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    
    * Update public/app/features/dashboard-scene/edit-pane/DashboardOutline.tsx
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    
    * Update public/app/features/dashboard-scene/edit-pane/DashboardOutline.tsx
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    
    * Update public/app/features/dashboard-scene/scene/layout-auto-grid/AutoGridLayoutManager.tsx
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    
    * Update public/app/features/dashboard-scene/scene/layout-default/DefaultGridLayoutManager.tsx
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    
    * Update public/app/features/dashboard-scene/scene/layout-rows/RowsLayoutManagerRenderer.tsx
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>
    
    * Update
    
    * more tests
    
    * Update
    
    * Update
    
    * Removed old behavior
    
    * Update
    
    * Update
    
    * Update
    
    * fix outline for default grid
    
    * Update
    
    ---------
    
    Co-authored-by: kay delaney <45561153+kaydelaney@users.noreply.github.com>

--- public/app/features/dashboard-scene/serialization/transformSaveModelToScene.test.ts
+++ public/app/features/dashboard-scene/serialization/transformSaveModelToScene.test.ts
@@ -33,7 +33,6 @@ import { PanelTimeRange } from '../scene/PanelTimeRange';
 import { DashboardGridItem } from '../scene/layout-default/DashboardGridItem';
 import { DefaultGridLayoutManager } from '../scene/layout-default/DefaultGridLayoutManager';
 import { RowRepeaterBehavior } from '../scene/layout-default/RowRepeaterBehavior';
-import { RowItemRepeaterBehavior } from '../scene/layout-rows/RowItemRepeaterBehavior';
 import { RowsLayoutManager } from '../scene/layout-rows/RowsLayoutManager';
 import { NEW_LINK } from '../settings/links/utils';
 import { getQueryRunnerFor } from '../utils/utils';
@@ -845,10 +844,7 @@ describe('transformSaveModelToScene', () => {
 
       const row2 = layout.state.rows[1];
 
-      expect(row2.state.$behaviors?.[0]).toBeInstanceOf(RowItemRepeaterBehavior);
-
-      const repeatBehavior = row2.state.$behaviors?.[0] as RowItemRepeaterBehavior;
-      expect(repeatBehavior.state.variableName).toBe('server');
+      expect(row2.state.repeatByVariable).toBe('server');
 
       const lastRow = layout.state.rows[layout.state.rows.length - 1];
       expect(lastRow.state.title).toBe('Row at the bottom - not repeated - saved collapsed ');
