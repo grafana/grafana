1eed1824b39976bda6b041071f4775889b1c6c73
Michael Mandrus <41969079+mmandrus@users.noreply.github.com>
2025-06-26 00:34:04 -0400

    Secrets: Add logs and metrics to service for public preview readiness (#106739)
    * start adding some metrics and logging
    
    * add metrics to the outbox
    
    * add a lot more metrics
    
    * mass commits woo
    
    * fix compile errors
    
    * add missing metrics
    
    * add metrics for data key operation timings
    
    * set keepertype in worker
    
    * remove out of date comment
    
    * add config for system keeper so we don't accidentally pass around nil
    
    * fix unit tests
    
    * fix remaining tests
    
    * close rows before executing the next query

--- pkg/registry/apis/secret/secretkeeper/secretkeeper_test.go
+++ pkg/registry/apis/secret/secretkeeper/secretkeeper_test.go
@@ -56,7 +56,7 @@ func setupTestService(t *testing.T, cfg *setting.Cfg) (*OSSKeeperService, error)
 	database := database.ProvideDatabase(testDB, tracer)
 	features := featuremgmt.WithFeatures(featuremgmt.FlagGrafanaAPIServerWithExperimentalAPIs, featuremgmt.FlagSecretsManagementAppPlatform)
 
-	dataKeyStore, err := encryptionstorage.ProvideDataKeyStorage(database, tracer, features)
+	dataKeyStore, err := encryptionstorage.ProvideDataKeyStorage(database, tracer, features, nil)
 	require.NoError(t, err)
 
 	encValueStore, err := encryptionstorage.ProvideEncryptedValueStorage(database, tracer, features)
@@ -66,7 +66,7 @@ func setupTestService(t *testing.T, cfg *setting.Cfg) (*OSSKeeperService, error)
 	require.NoError(t, err)
 
 	// Initialize the keeper service
-	keeperService, err := ProvideService(tracer, encValueStore, encryptionManager)
+	keeperService, err := ProvideService(tracer, encValueStore, encryptionManager, nil)
 
 	return keeperService, err
 }
