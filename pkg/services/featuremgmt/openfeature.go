24fd395f868152fb6d4641188f0de985f29fa8f5
Tania <10127682+undef1nd@users.noreply.github.com>
2025-06-27 18:30:38 +0200

    Implement OFREP compatible feature flag service (#105632)
    * Add ofrep pkg
    
    * api server: Use namespace from request in case user is not authenticated
    
    * Add handlers to ofrep api builder
    
    * Add NewOpenFeatureService to initialize mt apiserver
    
    * allow specifying CA and insecure
    
    * Compare namespace with eval ctx stackID
    
    * Organize ofrep package
    
    * Implement AllowedV0Alpha1Resources
    
    * Revert folderimpl changes
    
    * Handle default namespace
    
    * Fix extracting stack id from eval ctx
    
    * Add more logs
    
    * Update pkg/registry/apis/ofrep/register.go
    
    Co-authored-by: Dave Henderson <dave.henderson@grafana.com>
    
    * Update pkg/registry/apis/ofrep/register.go
    
    Co-authored-by: Dave Henderson <dave.henderson@grafana.com>
    
    * Apply review feedback
    
    * Replace contexthandler with types
    
    * Fix identifying authed request
    
    * Refactor checks in the handlers
    
    * Remove anonymous from isAuthenticatedRequest check
    
    ---------
    
    Co-authored-by: Todd Treece <360020+toddtreece@users.noreply.github.com>
    Co-authored-by: Gabriel Mabille <gabriel.mabille@grafana.com>
    Co-authored-by: Charandas Batra <charandas.batra@grafana.com>
    Co-authored-by: Dave Henderson <dave.henderson@grafana.com>

--- pkg/services/featuremgmt/openfeature.go
+++ pkg/services/featuremgmt/openfeature.go
@@ -1,8 +1,8 @@
 package featuremgmt
 
 import (
-	"context"
 	"fmt"
+	"net/url"
 
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/setting"
@@ -11,102 +11,58 @@ import (
 )
 
 type OpenFeatureService struct {
-	cfg      *setting.Cfg
 	log      log.Logger
 	provider openfeature.FeatureProvider
 	Client   openfeature.IClient
 }
 
+// ProvideOpenFeatureService is used for wiring dependencies in single tenant grafana
 func ProvideOpenFeatureService(cfg *setting.Cfg) (*OpenFeatureService, error) {
-	var provider openfeature.FeatureProvider
-	var err error
-	if cfg.OpenFeature.ProviderType == setting.GOFFProviderType {
-		if cfg.OpenFeature.URL == nil {
-			return nil, fmt.Errorf("feature provider url is required for GOFFProviderType")
-		}
-
-		provider, err = newGOFFProvider(cfg.OpenFeature.URL.String())
-	} else {
-		provider, err = newStaticProvider(cfg)
-	}
-
+	confFlags, err := setting.ReadFeatureTogglesFromInitFile(cfg.Raw.Section("feature_toggles"))
 	if err != nil {
-		return nil, fmt.Errorf("failed to create %s feature provider: %w", cfg.OpenFeature.ProviderType, err)
-	}
-
-	if err := openfeature.SetProviderAndWait(provider); err != nil {
-		return nil, fmt.Errorf("failed to set global %s feature provider: %w", cfg.OpenFeature.ProviderType, err)
+		return nil, fmt.Errorf("failed to read feature toggles from config: %w", err)
 	}
 
 	openfeature.SetEvaluationContext(openfeature.NewEvaluationContext(cfg.OpenFeature.TargetingKey, cfg.OpenFeature.ContextAttrs))
+	return newOpenFeatureService(cfg.OpenFeature.ProviderType, cfg.OpenFeature.URL, confFlags)
+}
+
+// TODO: might need to be public, so other MT services could set up open feature client
+func newOpenFeatureService(pType string, u *url.URL, staticFlags map[string]bool) (*OpenFeatureService, error) {
+	p, err := createProvider(pType, u, staticFlags)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create feature provider: type %s, %w", pType, err)
+	}
+
+	if err := openfeature.SetProviderAndWait(p); err != nil {
+		return nil, fmt.Errorf("failed to set global feature provider: %s, %w", pType, err)
+	}
 
 	client := openfeature.NewClient("grafana-openfeature-client")
 	return &OpenFeatureService{
-		cfg:      cfg,
 		log:      log.New("openfeatureservice"),
-		provider: provider,
+		provider: p,
 		Client:   client,
 	}, nil
 }
 
-func (s *OpenFeatureService) EvalFlagWithStaticProvider(ctx context.Context, flagKey string) (openfeature.BooleanEvaluationDetails, error) {
-	_, ok := s.provider.(*inMemoryBulkProvider)
-	if !ok {
-		return openfeature.BooleanEvaluationDetails{}, fmt.Errorf("not a static provider, request must be sent to open feature service")
+func createProvider(providerType string, u *url.URL, staticFlags map[string]bool) (openfeature.FeatureProvider, error) {
+	if providerType != setting.GOFFProviderType {
+		return newStaticProvider(staticFlags)
 	}
 
-	result, err := s.Client.BooleanValueDetails(ctx, flagKey, false, openfeature.TransactionContext(ctx))
-	if err != nil {
-		return openfeature.BooleanEvaluationDetails{}, fmt.Errorf("failed to evaluate flag %s: %w", flagKey, err)
+	if u.String() == "" {
+		return nil, fmt.Errorf("feature provider url is required for GOFFProviderType")
 	}
 
-	return result, nil
+	return newGOFFProvider(u.String())
 }
 
-func (s *OpenFeatureService) EvalAllFlagsWithStaticProvider(ctx context.Context) (OFREPBulkResponse, error) {
-	p, ok := s.provider.(*inMemoryBulkProvider)
-	if !ok {
-		return OFREPBulkResponse{}, fmt.Errorf("not a static provider, request must be sent to open feature service")
+func createClient(provider openfeature.FeatureProvider) (openfeature.IClient, error) {
+	if err := openfeature.SetProviderAndWait(provider); err != nil {
+		return nil, fmt.Errorf("failed to set global feature provider: %w", err)
 	}
 
-	flags, err := p.ListFlags()
-	if err != nil {
-		return OFREPBulkResponse{}, fmt.Errorf("static provider failed to list all flags: %w", err)
-	}
-
-	allFlags := make([]OFREPFlag, 0, len(flags))
-	for _, flagKey := range flags {
-		result, err := s.Client.BooleanValueDetails(ctx, flagKey, false, openfeature.TransactionContext(ctx))
-		if err != nil {
-			s.log.Error("failed to evaluate flag during bulk evaluation", "flagKey", flagKey, "error", err)
-			continue
-		}
-
-		allFlags = append(allFlags, OFREPFlag{
-			Key:          flagKey,
-			Value:        result.Value,
-			Reason:       "static provider evaluation result",
-			Variant:      result.Variant,
-			ErrorCode:    string(result.ErrorCode),
-			ErrorDetails: result.ErrorMessage,
-		})
-	}
-
-	return OFREPBulkResponse{Flags: allFlags}, nil
-}
-
-// Bulk evaluation response
-type OFREPBulkResponse struct {
-	Flags    []OFREPFlag    `json:"flags"`
-	Metadata map[string]any `json:"metadata,omitempty"`
-}
-
-type OFREPFlag struct {
-	Key          string         `json:"key"`
-	Value        bool           `json:"value"`
-	Reason       string         `json:"reason"`
-	Variant      string         `json:"variant,omitempty"`
-	Metadata     map[string]any `json:"metadata,omitempty"`
-	ErrorCode    string         `json:"errorCode,omitempty"`
-	ErrorDetails string         `json:"errorDetails,omitempty"`
+	client := openfeature.NewClient("grafana-openfeature-client")
+	return client, nil
 }
