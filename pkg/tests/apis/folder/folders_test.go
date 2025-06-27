104535a0c2594ce6510254c48ab58e69da90d48b
Maicon Costa <maiconscosta@gmail.com>
2025-06-27 14:29:04 -0300

    Remove skip instruction from TestIntegrationFolderCreatePermissions
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/tests/apis/folder/folders_test.go
+++ pkg/tests/apis/folder/folders_test.go
@@ -583,7 +583,6 @@ func TestIntegrationFolderCreatePermissions(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
-	t.Skip("not working yet")
 
 	folderWithoutParentInput := "{ \"uid\": \"uid\", \"title\": \"Folder\"}"
 	folderWithParentInput := "{ \"uid\": \"uid\", \"title\": \"Folder\", \"parentUid\": \"parentuid\"}"
