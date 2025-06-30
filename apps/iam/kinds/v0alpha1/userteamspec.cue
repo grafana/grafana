73c31c740c4e3118ad1a3a54404014405bf6f8dd
Mihaly Gyongyosi <mgyongyosi@users.noreply.github.com>
2025-06-30 15:12:18 +0200

    Clean up

--- apps/iam/kinds/v0alpha1/userteamspec.cue
+++ apps/iam/kinds/v0alpha1/userteamspec.cue
@@ -1,5 +1,6 @@
 package v0alpha1
 
+// Not used currently, but will be used in the future.
 UserTeamSpec: {
     title: string
     teamRef: TeamRef
--- pkg/apis/iam/v0alpha1/types_service_account.go
+++ pkg/apis/iam/v0alpha1/types_service_account.go
@@ -2,33 +2,6 @@ package v0alpha1
 
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-// // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// type ServiceAccount struct {
-// 	metav1.TypeMeta   `json:",inline"`
-// 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-// 	Spec ServiceAccountSpec `json:"spec,omitempty"`
-// }
-
-// func (s ServiceAccount) AuthID() string {
-// 	return fmt.Sprintf("%d", s.Spec.InternalID)
-// }
-
-// type ServiceAccountSpec struct {
-// 	Title    string `json:"title,omitempty"`
-// 	Disabled bool   `json:"disabled,omitempty"`
-// 	// This is currently used for authorization checks but we don't want to expose it
-// 	InternalID int64 `json:"-"`
-// }
-
-// // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// type ServiceAccountList struct {
-// 	metav1.TypeMeta `json:",inline"`
-// 	metav1.ListMeta `json:"metadata,omitempty"`
-
-// 	Items []ServiceAccount `json:"items"`
-// }
-
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type ServiceAccountTokenList struct {
 	metav1.TypeMeta `json:",inline"`
