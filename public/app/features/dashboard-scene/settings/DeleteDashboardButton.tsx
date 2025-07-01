939761b1ea72b93d3e893a92c1e3f05e0203ad53
Bogdan Matei <bogdan.matei@grafana.com>
2025-07-01 14:38:02 +0300

    Dashboard: Remove dto and scene on dashboard delete

--- public/app/features/dashboard-scene/settings/DeleteDashboardButton.tsx
+++ public/app/features/dashboard-scene/settings/DeleteDashboardButton.tsx
@@ -6,6 +6,7 @@ import { config, reportInteraction } from '@grafana/runtime';
 import { Button, ConfirmModal, Modal, Space, Text, TextLink } from '@grafana/ui';
 
 import { useDeleteItemsMutation } from '../../browse-dashboards/api/browseDashboardsAPI';
+import { getDashboardScenePageStateManager } from '../pages/DashboardScenePageStateManager';
 import { DashboardScene } from '../scene/DashboardScene';
 
 import { DeleteProvisionedDashboardDrawer } from './DeleteProvisionedDashboardDrawer';
@@ -49,6 +50,11 @@ export function DeleteDashboardButton({ dashboard }: ButtonProps) {
       });
     }
     await dashboard.onDashboardDelete();
+    const pageStateManager = getDashboardScenePageStateManager();
+    pageStateManager.clearDashboardCache();
+    if (dashboard.state.uid) {
+      pageStateManager.removeSceneCache(dashboard.state.uid);
+    }
   }, [dashboard, toggleModal]);
 
   // Git managed dashboard
