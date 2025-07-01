8459c60ebf450ace8e92f1924e7c7c7ddfb37fe5
Juan Cabanas <juan.cabanas@grafana.com>
2025-07-01 17:12:28 -0300

    create pull request with context

--- public/app/features/dashboard-scene/saving/provisioned/DashboardPreviewBanner.tsx
+++ public/app/features/dashboard-scene/saving/provisioned/DashboardPreviewBanner.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 import { textUtil } from '@grafana/data';
 import { Trans, t } from '@grafana/i18n';
 import { config } from '@grafana/runtime';
-import { Alert, Icon, Stack } from '@grafana/ui';
-import { useGetRepositoryFilesWithPathQuery } from 'app/api/clients/provisioning/v0alpha1';
+import { Alert, Button, Field, Icon, Input, Label, Modal, Spinner, Stack, TextArea } from '@grafana/ui';
+import {
+  useCreateRepositoryPrMutation,
+  useGetRepositoryDiffQuery,
+  useGetRepositoryFilesWithPathQuery,
+  useGetRepositoryRefsQuery,
+} from 'app/api/clients/provisioning/v0alpha1';
+import { GenAIButton } from 'app/features/dashboard/components/GenAI/GenAIButton';
+import { EventTrackingSrc } from 'app/features/dashboard/components/GenAI/tracking';
+import { Message, Role } from 'app/features/dashboard/components/GenAI/utils';
 import { DashboardPageRouteSearchParams } from 'app/features/dashboard/containers/types';
 import { usePullRequestParam } from 'app/features/provisioning/hooks/usePullRequestParam';
 import { DashboardRoutes } from 'app/types';
@@ -24,9 +35,242 @@ const commonAlertProps = {
   style: { flex: 0 } as const,
 };
 
+interface GitHubPRForm {
+  title: string;
+  description: string;
+  baseBranch: string;
+}
+
+const CreatePullRequestModal = ({
+  slug,
+  branch,
+  isOpen,
+  onDismiss,
+}: {
+  slug: string;
+  branch: string;
+  isOpen: boolean;
+  onDismiss: () => void;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<GitHubPRForm>();
+
+  const baseBranch = watch('baseBranch') ?? 'master';
+
+  const { data: diffData, isLoading: isDiffLoading } = useGetRepositoryDiffQuery(
+    {
+      name: slug,
+      ref: branch,
+      base: baseBranch,
+    },
+    {
+      skip: !isOpen || !baseBranch,
+    }
+  );
+
+  const { data: branches, isLoading: isBranchesLoading } = useGetRepositoryRefsQuery({ name: slug });
+
+  useEffect(() => {
+    if (branches) {
+      setValue('baseBranch', branches.items[0]?.name);
+    }
+  }, [branches, setValue]);
+
+  const [
+    createPullRequest,
+    { isLoading: isCreatingPullRequest, isSuccess: isCreatePullRequestSuccess, data: createPullRequestData },
+  ] = useCreateRepositoryPrMutation();
+
+  const onSubmit = async (data: GitHubPRForm) => {
+    createPullRequest({
+      name: slug,
+      ref: branch,
+      title: data.title,
+      content: data.description,
+    });
+  };
+
+  const titleMessage: Message[] = [
+    {
+      role: Role.system,
+      content: `You are an expert in creating pull request in Github and titles for them.
+  Your goal is to write a short but descriptive title for a pull request.
+  The title should explain the purpose of the pull request.
+  It should be between 10-30 characters and be helpful for users to understand the pull request's value.
+  Do not include quotes in your response.
+  The response should contain ONLY the proposed title, no other text.
+  `,
+    },
+    {
+      role: Role.user,
+      content: `Create a title for a pull request."
+  These are the changes between the current branch and the master branch: ${JSON.stringify(diffData?.diff?.files)}
+  These are the commits of the current branch:
+  ${JSON.stringify(diffData?.diff?.commits)}
+  `,
+    },
+  ];
+
+  const descriptionMessage: Message[] = [
+    {
+      role: Role.system,
+      content: `You are an expert in creating pull request in Github and descriptions for them.
+  Your goal is to write a descriptive and informative pull request description.
+  The description should explain the purpose of the pull request.
+  The response should contain ONLY the proposed description, no other text.
+  `,
+    },
+    {
+      role: Role.user,
+      content: `Create a description for a pull request"
+These are the changes between the current branch and the master branch: ${JSON.stringify(diffData?.diff?.files)}
+These are the commits of the current branch:
+${JSON.stringify(diffData?.diff?.commits)}
+`,
+    },
+  ];
+
+  return (
+    <Modal
+      isOpen
+      title={t('dashboard-scene.dashboard-preview-banner.create-pull-request', 'Create pull request')}
+      onDismiss={onDismiss}
+    >
+      {isCreatePullRequestSuccess ? (
+        <Alert
+          severity="success"
+          title={t(
+            'dashboard-scene.dashboard-preview-banner.pull-request-created',
+            'Pull request successfully created'
+          )}
+          buttonContent={
+            <Stack alignItems="center">
+              <Trans i18nKey="dashboard-scene.dashboard-preview-banner.view-pull-request-in-git-hub">
+                View pull request in GitHub
+              </Trans>
+              <Icon name="external-link-alt" />
+            </Stack>
+          }
+          onRemove={() => window.open(createPullRequestData?.pullRequest?.url, '_blank')}
+        />
+      ) : (
+        <fieldset disabled={isCreatingPullRequest}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack direction="column" gap={2}>
+              <Field
+                noMargin
+                invalid={!!errors.title}
+                error={errors.title?.message}
+                label={
+                  <Stack direction="row" justifyContent="space-between" alignItems="center" gap={1}>
+                    <Label>{t('dashboard-scene.dashboard-preview-banner.title', 'Title')}</Label>
+                    <Stack direction="row" alignItems="center" gap={0.5}>
+                      {isDiffLoading && <Spinner />}
+                      <GenAIButton
+                        tooltip={t(
+                          'provisioned-resource-form.save-or-delete-resource-shared-fields.ai-fill-path',
+                          'AI autofill path'
+                        )}
+                        messages={titleMessage}
+                        disabled={isDiffLoading}
+                        onGenerate={(response) => setValue('title', response, { shouldDirty: true })}
+                        eventTrackingSrc={EventTrackingSrc.dashboardTitle}
+                      />
+                    </Stack>
+                  </Stack>
+                }
+              >
+                <Input
+                  {...register('title', {
+                    required: t('dashboard-scene.dashboard-preview-banner.title-required', 'Title is required'),
+                  })}
+                />
+              </Field>
+              <Field
+                noMargin
+                invalid={!!errors.description}
+                error={errors.description?.message}
+                label={
+                  <Stack direction="row" justifyContent="space-between" alignItems="center" gap={1}>
+                    <Label>{t('dashboard-scene.dashboard-preview-banner.description', 'Description')}</Label>
+                    <Stack direction="row" alignItems="center" gap={0.5}>
+                      {isDiffLoading && <Spinner />}
+                      <GenAIButton
+                        tooltip={t(
+                          'provisioned-resource-form.save-or-delete-resource-shared-fields.ai-fill-path',
+                          'AI autofill path'
+                        )}
+                        messages={descriptionMessage}
+                        disabled={isDiffLoading}
+                        onGenerate={(response) => setValue('description', response, { shouldDirty: true })}
+                        eventTrackingSrc={EventTrackingSrc.dashboardTitle}
+                      />
+                    </Stack>
+                  </Stack>
+                }
+              >
+                <TextArea
+                  {...register('description', {
+                    required: t(
+                      'dashboard-scene.dashboard-preview-banner.description-required',
+                      'Description is required'
+                    ),
+                  })}
+                  rows={10}
+                />
+              </Field>
+              {/* <Field label={t('dashboard-scene.dashboard-preview-banner.base-branch', 'Base branch')}>
+                <Controller
+                  name={'baseBranch'}
+                  control={control}
+                  render={({ field: { ref, onChange, ...field } }) => {
+                    return (
+                      <Combobox
+                        {...field}
+                        loading={isBranchesLoading}
+                        options={branches?.items?.map((branch) => ({ label: branch.name, value: branch.name })) ?? []}
+                        onChange={(value) => onChange(value?.value)}
+                        placeholder={t(
+                          'dashboard-scene.dashboard-preview-banner.placeholder-select-branch',
+                          'Select branch'
+                        )}
+                      />
+                    );
+                  }}
+                />
+              </Field> */}
+              <Modal.ButtonRow>
+                <Button variant="secondary" fill="outline" onClick={onDismiss}>
+                  {t('dashboard-scene.dashboard-preview-banner.cancel', 'Cancel')}
+                </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  icon={isCreatingPullRequest ? 'spinner' : undefined}
+                  disabled={isBranchesLoading}
+                >
+                  {isCreatingPullRequest
+                    ? t('dashboard-scene.dashboard-preview-banner.creating-pull-request', 'Creating pull request')
+                    : t('dashboard-scene.dashboard-preview-banner.create-pull-request', 'Create pull request')}
+                </Button>
+              </Modal.ButtonRow>
+            </Stack>
+          </form>
+        </fieldset>
+      )}
+    </Modal>
+  );
+};
+
 function DashboardPreviewBannerContent({ queryParams, slug, path }: DashboardPreviewBannerContentProps) {
   const prParam = usePullRequestParam();
   const file = useGetRepositoryFilesWithPathQuery({ name: slug, path, ref: queryParams.ref });
+  const [showPRModal, setShowPRModal] = useState(false);
 
   if (file.data?.errors) {
     return (
@@ -70,28 +314,36 @@ function DashboardPreviewBannerContent({ queryParams, slug, path }: DashboardPre
 
   // Check if this is a GitHub link
   const githubURL = file.data?.urls?.newPullRequestURL ?? file.data?.urls?.compareURL;
+
   if (githubURL) {
     return (
-      <Alert
-        {...commonAlertProps}
-        title={t(
-          'dashboard-scene.dashboard-preview-banner.title-dashboard-loaded-branch-git-hub',
-          'This dashboard is loaded from a branch in GitHub.'
-        )}
-        buttonContent={
-          <Stack alignItems="center">
-            <Trans i18nKey="dashboard-scene.dashboard-preview-banner.open-pull-request-in-git-hub">
-              Open pull request in GitHub
+      <>
+        <Alert
+          {...commonAlertProps}
+          title={t(
+            'dashboard-scene.dashboard-preview-banner.title-dashboard-loaded-branch-git-hub',
+            'This dashboard is loaded from a branch in GitHub.'
+          )}
+          buttonContent={
+            <Trans i18nKey="dashboard-scene.dashboard-preview-banner.create-pull-request-in-git-hub">
+              Create pull request
             </Trans>
-            <Icon name="external-link-alt" />
-          </Stack>
-        }
-        onRemove={() => window.open(textUtil.sanitizeUrl(githubURL), '_blank')}
-      >
-        <Trans i18nKey="dashboard-scene.dashboard-preview-banner.not-saved">
-          The value is not yet saved in the Grafana database
-        </Trans>
-      </Alert>
+          }
+          onRemove={() => setShowPRModal(true)}
+        >
+          <Trans i18nKey="dashboard-scene.dashboard-preview-banner.not-saved">
+            The value is not yet saved in the Grafana database
+          </Trans>
+        </Alert>
+        {showPRModal && (
+          <CreatePullRequestModal
+            slug={slug}
+            branch={queryParams.ref ?? 'master'}
+            isOpen={showPRModal}
+            onDismiss={() => setShowPRModal(false)}
+          />
+        )}
+      </>
     );
   }
 
