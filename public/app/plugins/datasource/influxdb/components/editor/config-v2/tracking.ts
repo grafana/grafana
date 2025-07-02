19a8d0f68db1b035680b913d9e06a11c9fcf5e85
alyssabull <alyssabull@gmail.com>
2025-07-02 15:58:58 -0600

    Keep event names

--- public/app/plugins/datasource/influxdb/components/editor/config-v2/tracking.ts
+++ public/app/plugins/datasource/influxdb/components/editor/config-v2/tracking.ts
@@ -2,89 +2,88 @@ import { reportInteraction } from '@grafana/runtime';
 
 // Config Section
 export const trackInfluxDBConfigV2FeedbackButtonClicked = () => {
-  console.log('hello');
-  reportInteraction('influxdb_config_v2_feedback_button_clicked');
+  reportInteraction('influxdb-config-v2-feedback-button-clicked');
 };
 
 // URL and Auth Section
 export const trackInfluxDBConfigV2URLInputField = () => {
-  reportInteraction('influxdb_config_v2_url_input_field');
+  reportInteraction('influxdb-config-v2-url-input-field');
 };
 
 export const trackInfluxDBConfigV2QueryLanguageSelected = (props: { version: string }) => {
-  reportInteraction('influxdb_config_v2_query_language_dropdown', props);
+  reportInteraction('influxdb-config-v2-query-language-dropdown', props);
 };
 
 export const trackInfluxDBConfigV2ProductSelected = (props: { product: string }) => {
-  reportInteraction('influxdb_config_v2_product_selection_dropdown', props);
+  reportInteraction('influxdb-config-v2-product-selection-dropdown', props);
 };
 
 // Flux Database Details Fields
 export const trackInfluxDBConfigV2FluxDBDetailsOrgInputField = () => {
-  reportInteraction('influxdb_config_v2_flux_dbdetails_org_input_field');
+  reportInteraction('influxdb-config-v2-flux-dbdetails-org-input-field');
 };
 
 export const trackInfluxDBConfigV2FluxDBDetailsDefaultBucketInputField = () => {
-  reportInteraction('influxdb_config_v2_flux_dbdetails_default_bucket_input_field');
+  reportInteraction('influxdb-config-v2-flux-dbdetails-default-bucket-input-field');
 };
 
 export const trackInfluxDBConfigV2FluxDBDetailsTokenInputField = () => {
-  reportInteraction('influxdb_config_v2_flux_dbdetails_token_input_field');
+  reportInteraction('influxdb-config-v2-flux-dbdetails-token-input-field');
 };
 
 // InfluxQL Database Details Fields
 export const trackInfluxDBConfigV2InfluxQLDBDetailsDatabaseInputField = () => {
-  reportInteraction('influxdb_config_v2_influxql_dbdetails_database_input_field');
+  reportInteraction('influxdb-config-v2-influxql-dbdetails-database-input-field');
 };
 
 export const trackInfluxDBConfigV2InfluxQLDBDetailsUserInputField = () => {
-  reportInteraction('influxdb_config_v2_influxql_dbdetails_user_input_field');
+  reportInteraction('influxdb-config-v2-influxql-dbdetails-user-input-field');
 };
 
 export const trackInfluxDBConfigV2InfluxQLDBDetailsPasswordInputField = () => {
-  reportInteraction('influxdb_config_v2_influxql_dbdetails_password_input_field');
+  reportInteraction('influxdb-config-v2-influxql-dbdetails-password-input-field');
 };
 
 // SQL Database Details Fields
 export const trackInfluxDBConfigV2SQLDBDetailsDatabaseInputField = () => {
-  reportInteraction('influxdb_config_v2_sql_dbdetails_database_input_field');
+  reportInteraction('influxdb-config-v2-sql-dbdetails-database-input-field');
 };
 
 export const trackInfluxDBConfigV2SQLDBDetailsTokenInputField = () => {
-  reportInteraction('influxdb_config_v2_sql_dbdetails_token_input_field');
+  reportInteraction('influxdb-config-v2-sql-dbdetails-token-input-field');
 };
 
 // Advanced DB Connection Settings
 export const trackInfluxDBConfigV2AdvancedDbConnectionSettingsToggleClicked = () => {
-  reportInteraction('influxdb_config_v2_advanceddb_settings_toggle_clicked');
+  reportInteraction('influxdb-config-v2-advanceddb-settings-toggle-clicked');
 };
 
 export const trackInfluxDBConfigV2AdvancedDbConnectionSettingsHTTPMethodClicked = () => {
-  reportInteraction('influxdb_config_v2_advanceddb_settings_http_method_clicked');
+  reportInteraction('influxdb-config-v2-advanceddb-settings-http-method-clicked');
 };
 
 export const trackInfluxDBConfigV2AdvancedDbConnectionSettingsInsecureConnectClicked = () => {
-  reportInteraction('influxdb_config_v2_advanceddb_settings_insecure_connection_clicked');
+  reportInteraction('influxdb-config-v2-advanceddb-settings-insecure-connection-clicked');
 };
 
 export const trackInfluxDBConfigV2AdvancedDbConnectionSettingsMinTimeClicked = () => {
-  reportInteraction('influxdb_config_v2_advanceddb_settings_min_time_clicked');
+  reportInteraction('influxdb-config-v2-advanceddb-settings-min-time-clicked');
 };
 
 // Advanced HTTP Settings
 export const trackInfluxDBConfigV2AdvancedHTTPSettingsToggleClicked = () => {
-  reportInteraction('influxdb_config_v2_advanced_http_settings_toggle_clicked');
+  reportInteraction('influxdb-config-v2-advanced-http-settings-toggle-clicked');
 };
 
 export const trackInfluxDBConfigV2AdvancedHTTPSettingsTimeoutField = () => {
-  reportInteraction('influxdb_config_v2_advanced_http_settings_timeout_field');
+  reportInteraction('influxdb-config-v2-advanced-http-settings-timeout-field');
 };
 
 // Auth Settings
 export const trackInfluxDBConfigV2AuthSettingsToggleClicked = () => {
-  reportInteraction('influxdb_config_v2_auth_settings_toggle_clicked');
+  reportInteraction('influxdb-config-v2-auth-settings-toggle-clicked');
 };
 
 export const trackInfluxDBConfigV2AuthSettingsAuthMethodSelected = (props: { authMethod: string }) => {
-  reportInteraction('influxdb_config_v2_advanced_http_settings_timeout_field', props);
+  reportInteraction('influxdb-config-v2-advanced-http-settings-timeout-field', props);
 };
--- public/app/plugins/datasource/influxdb/components/editor/config/trackingv1.ts
+++ public/app/plugins/datasource/influxdb/components/editor/config/trackingv1.ts
@@ -1,40 +1,40 @@
 import { reportInteraction } from '@grafana/runtime';
 
 export const trackInfluxDBConfigV1QueryLanguageSelection = (props: { version: string }) => {
-  reportInteraction('influxdb_configv1_query_language_dropdown', props);
+  reportInteraction('influxdb-configv1-query-language-dropdown', props);
 };
 
 // Flux Database Fields
 export const trackInfluxDBConfigV1FluxOrgInputField = () => {
-  reportInteraction('influxdb_configv1_flux_dbdetails_organization_input_field');
+  reportInteraction('influxdb-configv1-flux-dbdetails-organization-input-field');
 };
 
 export const trackInfluxDBConfigV1FluxTokenInputField = () => {
-  reportInteraction('influxdb_configv1_flux_dbdetails_token_input_field');
+  reportInteraction('influxdb-configv1-flux-dbdetails-token-input-field');
 };
 
 export const trackInfluxDBConfigV1FluxDefaultBucketInputField = () => {
-  reportInteraction('influxdb_configv1_flux_dbdetails_default_bucket_input_field');
+  reportInteraction('influxdb-configv1-flux-dbdetails-default-bucket-input-field');
 };
 
 // InfluxQL Database Fields
 export const trackInfluxDBConfigV1InfluxQLDatabaseInputField = () => {
-  reportInteraction('influxdb_configv1_influxql_dbdetails_database_input_field');
+  reportInteraction('influxdb-configv1-influxql-dbdetails-database-input-field');
 };
 
 export const trackInfluxDBConfigV1InfluxQLUserInputField = () => {
-  reportInteraction('influxdb_configv1_influxql_dbdetails_user_input_field');
+  reportInteraction('influxdb-configv1-influxql-dbdetails-user-input-field');
 };
 
 export const trackInfluxDBConfigV1InfluxQLPasswordInputField = () => {
-  reportInteraction('influxdb_configv1_influxql_dbdetails_password_input_field');
+  reportInteraction('influxdb-configv1-influxql-dbdetails-password-input-field');
 };
 
 // SQL Database Fields
 export const trackInfluxDBConfigV1SQLDatabaseInputField = () => {
-  reportInteraction('influxdb_configv1_sql_dbdetails_database_input_field');
+  reportInteraction('influxdb-configv1-sql-dbdetails-database-input-field');
 };
 
 export const trackInfluxDBConfigV1SQLTokenInputField = () => {
-  reportInteraction('influxdb_configv1_sql_dbdetails_token_input_field');
+  reportInteraction('influxdb-configv1-sql-dbdetails-token-input-field');
 };
