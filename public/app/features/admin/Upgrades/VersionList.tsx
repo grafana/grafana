ec5e8a5417f487c3b8cccfe352927adeb3077281
Michael Mandrus <mmandrus@grafana.com>
2025-07-02 16:58:53 -0400

    only link to minor version for upgrade guide

--- public/app/features/admin/Upgrades/VersionList.tsx
+++ public/app/features/admin/Upgrades/VersionList.tsx
@@ -22,83 +22,89 @@ export function VersionList({ versions, installedVersion }: Props) {
   const styles = useStyles2(getStyles);
   return (
     <Stack direction="column" gap={2}>
-    <br></br>
-    <h1 style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-      <img src={GrafanaLogo} alt="Grafana Logo" style={{ height: 32, width: 32 }} />
-      <span>Grafana Version: {installedVersion}</span>
-      <Badge text="Installed" color="green" icon="check" />
-    </h1>
-    {versions.length === 0 ? (<p>No recommended upgrades found.</p>) : (
-    <>
-    <h2>Recommended Upgrades</h2>
-    <p>The following upgrades are recommended for your Grafana instance to ensure you are running the latest supported version of Grafana.</p>
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>Version</th>
-          <th></th>
-          <th>Release Date</th>
-          <th>Upgrade Type</th>
-          <th>Support Status</th>
-          <th>Changelog</th>
-          <th>Upgrade Guide</th>
-        </tr>
-      </thead>
-      <tbody>
-        {versions.map((v) => {
-          return (
-            <tr key={v.version}>
-              <td>{v.version}</td>
-              <td>
-                  <LinkButton
-                    fill="solid"
-                    variant="primary"
-                    icon="download-alt"
-                    size="sm"
-                    // className={styles.button}
-                    href={`https://grafana.com/grafana/download/${v.version}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    tooltip="Install"
-                  >
-                    Install
-                  </LinkButton>
-              </td>
-              <td>{v.releaseDate}</td>
-              <td>{v.type.toUpperCase()}</td>
-              <td>{v.isOutOfSupport ? 'Out of Support' : 'In Support'}</td>
-              <td>
-                  <LinkButton
-                    type="button"
-                    variant="secondary"
-                    icon="info-circle"
-                    size="sm"
-                    href={`https://github.com/grafana/grafana/releases/tag/v${v.version}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Release Notes
-                  </LinkButton>
-              
-              </td>
-              <td>
-                  <LinkButton
-                    type="button"
-                    variant="secondary"
-                    icon="external-link-alt"
-                    size="sm"
-                    href={`https://grafana.com/docs/grafana/latest/upgrade-guide/upgrade-v${v.version}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >Upgrade Guide</LinkButton>
-            </td>
-            </tr>
-          );
-        })}
-        </tbody>
-      </table>
-    </>
-    )}
+      <br></br>
+      <h1 style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+        <img src={GrafanaLogo} alt="Grafana Logo" style={{ height: 32, width: 32 }} />
+        <span>Grafana Version: {installedVersion}</span>
+        <Badge text="Installed" color="green" icon="check" />
+      </h1>
+      {versions.length === 0 ? (
+        <p>No recommended upgrades found.</p>
+      ) : (
+        <>
+          <h2>Recommended Upgrades</h2>
+          <p>
+            The following upgrades are recommended for your Grafana instance to ensure you are running the latest
+            supported version of Grafana.
+          </p>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Version</th>
+                <th></th>
+                <th>Release Date</th>
+                <th>Upgrade Type</th>
+                <th>Support Status</th>
+                <th>Changelog</th>
+                <th>Upgrade Guide</th>
+              </tr>
+            </thead>
+            <tbody>
+              {versions.map((v) => {
+                return (
+                  <tr key={v.version}>
+                    <td>{v.version}</td>
+                    <td>
+                      <LinkButton
+                        fill="solid"
+                        variant="primary"
+                        icon="download-alt"
+                        size="sm"
+                        // className={styles.button}
+                        href={`https://grafana.com/grafana/download/${v.version}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        tooltip="Install"
+                      >
+                        Install
+                      </LinkButton>
+                    </td>
+                    <td>{v.releaseDate}</td>
+                    <td>{v.type.toUpperCase()}</td>
+                    <td>{v.isOutOfSupport ? 'Out of Support' : 'In Support'}</td>
+                    <td>
+                      <LinkButton
+                        type="button"
+                        variant="secondary"
+                        icon="info-circle"
+                        size="sm"
+                        href={`https://github.com/grafana/grafana/releases/tag/v${v.version}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Release Notes
+                      </LinkButton>
+                    </td>
+                    <td>
+                      <LinkButton
+                        type="button"
+                        variant="secondary"
+                        icon="external-link-alt"
+                        size="sm"
+                        href={`https://grafana.com/docs/grafana/latest/upgrade-guide/upgrade-v${v.version.split('.')?.slice(0, 2)?.join('.')}`} // Upgrade guides only go as deep as the minor version
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Upgrade Guide
+                      </LinkButton>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </Stack>
   );
 }
