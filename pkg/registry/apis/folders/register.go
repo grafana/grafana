1be8e86c8fd9d6643a45eb71d8e41e43e141147e
Andrej Ocenas <mr.ocenas@gmail.com>
2025-07-03 16:15:23 +0200

    NestedFolderPicker: Migrate to app platform API (#106926)
    * Add /children endpoint
    
    * Update folder client
    
    * Add comment
    
    * Add feature toggle
    
    * Add new version of useFoldersQuery
    
    * Error handling
    
    * Format
    
    * Rename feature toggle
    
    * Remove options and move root folder constant
    
    * Fix feature toggle merge
    
    * Add feature toggle again
    
    * Rename useFoldersQuery files
    
    * Update API spec
    
    * Fix test
    
    * Add test
    
    * Better typings
    
    ---------
    
    Co-authored-by: Clarity-89 <homes89@ukr.net>

--- pkg/registry/apis/folders/register.go
+++ pkg/registry/apis/folders/register.go
@@ -192,6 +192,11 @@ func (b *FolderAPIBuilder) UpdateAPIGroupInfo(apiGroupInfo *genericapiserver.API
 	storage[resourceInfo.StoragePath("counts")] = &subCountREST{searcher: b.searcher}
 	storage[resourceInfo.StoragePath("access")] = &subAccessREST{b.folderSvc, b.ac}
 
+	// Adds a path to return children of a given folder
+	storage[resourceInfo.StoragePath("children")] = &subChildrenREST{
+		lister: storage[resourceInfo.StoragePath()].(rest.Lister),
+	}
+
 	apiGroupInfo.VersionedResourcesStorageMap[folders.VERSION] = storage
 	b.storage = storage[resourceInfo.StoragePath()].(grafanarest.Storage)
 	return nil
