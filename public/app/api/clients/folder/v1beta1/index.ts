e7b4f3e9c8f7622d0fc4bc97ce9ba12113453cf3
Andrej Ocenas <mr.ocenas@gmail.com>
2025-07-04 11:52:57 +0200

    Use getFolder facade hook

--- public/app/api/clients/folder/v1beta1/index.ts
+++ public/app/api/clients/folder/v1beta1/index.ts
@@ -1,10 +1,22 @@
+import { skipToken } from '@reduxjs/toolkit/query';
+
 import { config } from '@grafana/runtime';
 
-import { AnnoKeyCreatedBy, AnnoKeyFolder, AnnoKeyUpdatedTimestamp } from '../../../../features/apiserver/types';
+import {
+  AnnoKeyCreatedBy,
+  AnnoKeyFolder,
+  // AnnoKeyFolderUrl,
+  AnnoKeyManagerKind,
+  AnnoKeyUpdatedBy,
+  AnnoKeyUpdatedTimestamp,
+  DeprecatedInternalId,
+  ManagerKind,
+} from '../../../../features/apiserver/types';
 import { useGetFolderQuery as useGetFoldersQueryLegacy } from '../../../../features/browse-dashboards/api/browseDashboardsAPI';
 import { PAGE_SIZE } from '../../../../features/browse-dashboards/api/services';
 import { refetchChildren } from '../../../../features/browse-dashboards/state';
 import { GENERAL_FOLDER_UID } from '../../../../features/search/constants';
+import { FolderDTO } from '../../../../types';
 
 import { generatedAPI } from './endpoints.gen';
 
@@ -26,36 +38,56 @@ export const folderAPIv1beta1 = generatedAPI.enhanceEndpoints({
   },
 });
 
-export const { useGetFolderQuery } = folderAPIv1beta1;
+export const { useGetFolderQuery, useGetFolderParentsQuery } = folderAPIv1beta1;
 
-export function useGetFolderQueryFacade(uid: string) {
+function folderUrl(uid: string, slug: string): string {
+  // mimics https://github.com/grafana/grafana/blob/79fe8a9902335c7a28af30e467b904a4ccfac503/pkg/services/dashboards/models.go#L188
+  return `${config.appSubUrl}/dashboards/f/${uid}/${slug}`;
+}
+
+export function useGetFolderQueryFacade(uid?: string) {
   if (config.featureToggles.foldersAppPlatformAPI) {
-    return useGetFolderQuery({ name: uid });
-  } else {
-    const result = useGetFoldersQueryLegacy(uid);
-    if (result.data) {
-      const newData: Folder = {
-        metadata: {
-          annotations: {
-            [AnnoKeyCreatedBy]: result.data.createdBy,
-            [AnnoKeyUpdatedTimestamp]: result.data.updated,
-            [AnnoKeyFolder]: result.data.parentUid,
-          },
-          creationTimestamp: result.data.created,
-          name: result.data.uid,
-        },
-        spec: {
-          description: result.data.description,
-          title: result.data.title,
-        },
-        status: {},
-      };
-      return {
-        ...result,
-        data: newData,
+    const result = useGetFolderQuery(uid ? { name: uid } : skipToken);
+    const resultParents = useGetFolderParentsQuery(uid ? { name: uid } : skipToken);
+
+    let newData: FolderDTO | undefined = undefined;
+    if (result.data && resultParents.data) {
+      newData = {
+        canAdmin: false,
+        canDelete: false,
+        canEdit: false,
+        canSave: false,
+        created: result.data.metadata.creationTimestamp || '1970-01-01T00:00:00Z',
+        createdBy: result.data.metadata.annotations?.[AnnoKeyCreatedBy] || 'unknown',
+        hasAcl: false,
+        id: parseInt(result.data.metadata.labels?.[DeprecatedInternalId] || '0', 10) || 0,
+        parentUid: result.data.metadata.annotations?.[AnnoKeyFolder],
+        managedBy: result.data.metadata.annotations?.[AnnoKeyManagerKind] as ManagerKind,
+
+        parents: resultParents.data.items
+          .filter((i) => i.name !== result.data!.metadata.name)
+          .map((i) => ({
+            title: i.title,
+            uid: i.name,
+            // No idea how to make slug, on the server: https://github.com/grafana/grafana/blob/aac66e91198004bc044754105e18bfff8fbfd383/pkg/infra/slugify/slugify.go#L56
+            url: folderUrl(i.name, ''),
+          })),
+        title: result.data.spec.title,
+        uid: result.data.metadata.name!,
+        updated: result.data.metadata.annotations?.[AnnoKeyUpdatedTimestamp] || '1970-01-01T00:00:00Z',
+        updatedBy: result.data.metadata.annotations?.[AnnoKeyUpdatedBy] || 'unknown',
+        // Seems like this annotation is not populated
+        // url: result.data.metadata.annotations?.[AnnoKeyFolderUrl] || '',
+        url: folderUrl(result.data.metadata.name!, ''),
+        version: result.data.metadata.generation || 1,
       };
     }
-    return result;
+    return {
+      ...result,
+      data: newData,
+    };
+  } else {
+    return useGetFoldersQueryLegacy(uid || skipToken);
   }
 }
 
