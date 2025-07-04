291558a7ac6a3720d35602c7f4f925a2cc96f5ac
Misi <mgyongyosi@users.noreply.github.com>
2025-07-04 11:07:48 +0200

    IAM: Move resource definitions to apps/iam step 1 (#107389)
    * wip
    
    * Use serviceaccount model from /apps/iam
    
    * revert version update
    
    * Add tembinding, userteam, other improvements
    
    * Change serviceaccounttoken spec
    
    * Revert the change of ServiceAccountToken
    
    * Revert the change of UserTeam
    
    * Clean up
    
    * Remove files that are not needed for now
    
    * Lint
    
    * Update sql query's integration tests
    
    * Fix tests
    
    * update openapi spec
    
    * Move LastSeenAt to the annotations
    
    * Updte openapi_snapshots
    
    * Change lastSeenAt annotation name

--- apps/iam/pkg/apis/iam_manifest.go
+++ apps/iam/pkg/apis/iam_manifest.go
@@ -14,6 +14,8 @@ import (
 	v0alpha1 "github.com/grafana/grafana/apps/iam/pkg/apis/iam/v0alpha1"
 )
 
+var ()
+
 var appManifestData = app.ManifestData{
 	AppName: "iam",
 	Group:   "iam.grafana.app",
@@ -83,6 +85,50 @@ var appManifestData = app.ManifestData{
 				},
 			},
 		},
+
+		{
+			Kind:       "User",
+			Scope:      "Namespaced",
+			Conversion: false,
+			Versions: []app.ManifestKindVersion{
+				{
+					Name: "v0alpha1",
+				},
+			},
+		},
+
+		{
+			Kind:       "Team",
+			Scope:      "Namespaced",
+			Conversion: false,
+			Versions: []app.ManifestKindVersion{
+				{
+					Name: "v0alpha1",
+				},
+			},
+		},
+
+		{
+			Kind:       "TeamBinding",
+			Scope:      "Namespaced",
+			Conversion: false,
+			Versions: []app.ManifestKindVersion{
+				{
+					Name: "v0alpha1",
+				},
+			},
+		},
+
+		{
+			Kind:       "ServiceAccount",
+			Scope:      "Namespaced",
+			Conversion: false,
+			Versions: []app.ManifestKindVersion{
+				{
+					Name: "v0alpha1",
+				},
+			},
+		},
 	},
 }
 
@@ -101,6 +147,10 @@ var kindVersionToGoType = map[string]resource.Kind{
 	"Role/v0alpha1":               v0alpha1.RoleKind(),
 	"RoleBinding/v0alpha1":        v0alpha1.RoleBindingKind(),
 	"ResourcePermission/v0alpha1": v0alpha1.ResourcePermissionKind(),
+	"User/v0alpha1":               v0alpha1.UserKind(),
+	"Team/v0alpha1":               v0alpha1.TeamKind(),
+	"TeamBinding/v0alpha1":        v0alpha1.TeamBindingKind(),
+	"ServiceAccount/v0alpha1":     v0alpha1.ServiceAccountKind(),
 }
 
 // ManifestGoTypeAssociator returns the associated resource.Kind instance for a given Kind and Version, if one exists.
