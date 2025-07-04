d6bd60d5c37776ec09f6cef0349a0b1fd0b93f81
Dominik Prokop <dominik.prokop@grafana.com>
2025-07-04 16:32:46 +0200

    PublicDashboards: Use API to render public dashboard badge

--- public/app/features/dashboard-scene/scene/NavToolbarActions.tsx
+++ public/app/features/dashboard-scene/scene/NavToolbarActions.tsx
@@ -5,17 +5,7 @@ import { GrafanaTheme2, store } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { Trans, t } from '@grafana/i18n';
 import { config, locationService } from '@grafana/runtime';
-import {
-  Badge,
-  Button,
-  ButtonGroup,
-  Dropdown,
-  Icon,
-  Menu,
-  ToolbarButton,
-  ToolbarButtonRow,
-  useStyles2,
-} from '@grafana/ui';
+import { Button, ButtonGroup, Dropdown, Icon, Menu, ToolbarButton, ToolbarButtonRow, useStyles2 } from '@grafana/ui';
 import { AppChromeUpdate } from 'app/core/components/AppChrome/AppChromeUpdate';
 import { NavToolbarSeparator } from 'app/core/components/AppChrome/NavToolbar/NavToolbarSeparator';
 import grafanaConfig from 'app/core/config';
@@ -39,6 +29,7 @@ import { GoToSnapshotOriginButton } from './GoToSnapshotOriginButton';
 import ManagedDashboardNavBarBadge from './ManagedDashboardNavBarBadge';
 import { LeftActions } from './new-toolbar/LeftActions';
 import { RightActions } from './new-toolbar/RightActions';
+import { PublicDashboardBadge } from './new-toolbar/actions/PublicDashboardBadge';
 
 interface Props {
   dashboard: DashboardScene;
@@ -85,6 +76,7 @@ export function ToolbarActions({ dashboard }: Props) {
   const folderRepo = useSelector((state) => selectFolderRepository(state, meta.folderUid));
   const isManaged = Boolean(dashboard.isManagedRepository() || folderRepo);
 
+  console.log('navToolbar');
   // Internal only;
   // allows viewer editing without ability to save
   // used for grafana play
@@ -119,23 +111,13 @@ export function ToolbarActions({ dashboard }: Props) {
     },
   });
 
-  if (meta.publicDashboardEnabled) {
-    toolbarActions.push({
-      group: 'icon-actions',
-      condition: uid && Boolean(meta.canStar) && isShowingDashboard && !isEditing,
-      render: () => {
-        return (
-          <Badge
-            color="blue"
-            text={t('dashboard.toolbar.public-dashboard', 'Public')}
-            key="public-dashboard-button-badge"
-            className={styles.publicBadge}
-            data-testid={selectors.pages.Dashboard.DashNav.publicDashboardTag}
-          />
-        );
-      },
-    });
-  }
+  toolbarActions.push({
+    group: 'icon-actions',
+    condition: uid && Boolean(meta.canStar) && isShowingDashboard && !isEditing,
+    render: () => {
+      return <PublicDashboardBadge dashboard={dashboard} />;
+    },
+  });
 
   if (dashboard.isManaged() && meta.canEdit) {
     toolbarActions.push({
