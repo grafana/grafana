d98604374178756702424548dcdcb8ec66b2ceec
Brendan O'Handley <brendan.ohandley@grafana.com>
2025-07-05 12:07:54 -0500

    remove link button from data source picker

--- public/app/features/query/components/QueryEditorRowHeader.tsx
+++ public/app/features/query/components/QueryEditorRowHeader.tsx
@@ -6,7 +6,6 @@ import { DataQuery, DataSourceInstanceSettings, GrafanaTheme2 } from '@grafana/d
 import { selectors } from '@grafana/e2e-selectors';
 import { Trans, t } from '@grafana/i18n';
 import { FieldValidationMessage, Icon, Input, useStyles2 } from '@grafana/ui';
-import { AlertingRuleQueryExtentionPoint } from 'app/features/alerting/unified/components/rule-editor/alert-rule-form/extensions/AlertingRuleQueryExtentionPoint';
 import { DataSourcePicker } from 'app/features/datasources/components/picker/DataSourcePicker';
 
 export interface Props<TQuery extends DataQuery = DataQuery> {
@@ -151,8 +150,6 @@ const renderDataSource = <TQuery extends DataQuery>(
         current={dataSource.name}
         onChange={onChangeDataSource}
       />
-      {/* Only show the extensions in alerting. This data source picker is also used in dashboard panel mixed data source editor */}
-      {alerting && <AlertingRuleQueryExtentionPoint query={query} extensionsToShow="queryless" />}
     </div>
   );
 };
