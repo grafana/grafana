4581234f5136b6f2a905de5e3c9108cd9149d5b9
Georges Chaudy <chaudyg@gmail.com>
2025-07-07 11:57:08 +0200

    unistore: add custom kv grpc (#107464)
    add StorageTypeUnifiedKVGrpc

--- pkg/services/apiserver/options/storage.go
+++ pkg/services/apiserver/options/storage.go
@@ -22,10 +22,11 @@ import (
 type StorageType string
 
 const (
-	StorageTypeFile        StorageType = "file"
-	StorageTypeEtcd        StorageType = "etcd"
-	StorageTypeUnified     StorageType = "unified"
-	StorageTypeUnifiedGrpc StorageType = "unified-grpc"
+	StorageTypeFile          StorageType = "file"
+	StorageTypeEtcd          StorageType = "etcd"
+	StorageTypeUnified       StorageType = "unified"
+	StorageTypeUnifiedGrpc   StorageType = "unified-grpc"
+	StorageTypeUnifiedKVGrpc StorageType = "unified-kv-grpc"
 
 	// Deprecated: legacy is a shim that is no longer necessary
 	StorageTypeLegacy StorageType = "legacy"
@@ -95,6 +96,8 @@ func (o *StorageOptions) Validate() []error {
 	// nolint:staticcheck
 	case StorageTypeLegacy:
 		// no-op
+	case StorageTypeUnifiedKVGrpc:
+		// no-op (enterprise only)
 	case StorageTypeFile, StorageTypeEtcd, StorageTypeUnified, StorageTypeUnifiedGrpc:
 		// no-op
 	default:
