e8b47ffafc5b14a2e71b34d96ac789d5c1973375
MattIPv4 <me@mattcowley.co.uk>
2025-07-07 14:51:42 +0100

    Plugin Extensions: Cast usePluginContext type on return

--- packages/grafana-data/src/context/plugins/usePluginContext.tsx
+++ packages/grafana-data/src/context/plugins/usePluginContext.tsx
@@ -1,12 +1,11 @@
-import { Context, useContext } from 'react';
+import { useContext } from 'react';
 
 import { KeyValue } from '../../types/data';
 
 import { PluginContext, PluginContextType } from './PluginContext';
 
 export function usePluginContext<T extends KeyValue = KeyValue>(): PluginContextType<T> | null {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  const context = useContext(PluginContext as Context<PluginContextType<T> | undefined>);
+  const context = useContext(PluginContext);
 
   // The extensions hooks (e.g. `usePluginLinks()`) are using this hook to check
   // if they are inside a plugin or not (core Grafana), so we should be able to return an empty state as well (`null`).
@@ -14,5 +13,6 @@ export function usePluginContext<T extends KeyValue = KeyValue>(): PluginContext
     return null;
   }
 
-  return context;
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  return context as PluginContextType<T>;
 }
