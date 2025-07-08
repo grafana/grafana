434988f5cec1922bf6f324d94b9c9e36016b8646
Jack Baldry <jack.baldry@grafana.com>
2025-07-08 17:23:15 +0100

    Mount docs from the current repository
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -1,5 +1,10 @@
 # List of projects to provide to the make-docs script.
-PROJECTS := grafana
+# Format is PROJECT[:[VERSION][:[REPOSITORY][:[DIRECTORY]]]]
+# The following PROJECTS value mounts content into the "grafana" project, at the "latest" version, which is the default if not explicitly set.
+# This results in the content being served at /docs/grafana/latest/.
+# The source of the content is the current repository which is determined by the name of the parent directory of the git root.
+# This overrides the default behavior of assuming the repository directory is the same as the project name.
+PROJECTS := grafana::$(notdir $(basename $(shell git rev-parse --show-toplevel)))
 
 # Use the doc-validator image defined in CI by default.
 export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: "(grafana/doc-validator[^"]+)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/doc-validator.yml")
