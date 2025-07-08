026815089dec2b757ea14cc43826723d04153c77
Tania B. <10127682+undef1nd@users.noreply.github.com>
2025-07-08 12:23:42 +0200

    Apply review feedback

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -137,6 +137,11 @@ func (s *Server) Init() error {
 		return err
 	}
 
+	// Initialize the OpenFeature feature flag system
+	if err := featuremgmt.InitOpenFeatureWithCfg(s.cfg); err != nil {
+		return err
+	}
+
 	return s.provisioningService.RunInitProvisioners(s.context)
 }
 
