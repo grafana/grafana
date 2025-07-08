56d0965dd9cecf3b56c9a84bd38734cb3554fc3b
Santiago Hernández <santiagohernandez.1997@gmail.com>
2025-07-08 17:33:31 +0200

    reorder MimirClient interface

--- pkg/services/ngalert/remote/client/mimir.go
+++ pkg/services/ngalert/remote/client/mimir.go
@@ -24,6 +24,7 @@ import (
 
 // MimirClient contains all the methods to query the migration critical endpoints of Mimir instance, it's an interface to allow multiple implementations.
 type MimirClient interface {
+	GetFullState(ctx context.Context) (*UserState, error)
 	GetGrafanaAlertmanagerState(ctx context.Context) (*UserState, error)
 	CreateGrafanaAlertmanagerState(ctx context.Context, state string) error
 	DeleteGrafanaAlertmanagerState(ctx context.Context) error
@@ -31,8 +32,6 @@ type MimirClient interface {
 	GetGrafanaAlertmanagerConfig(ctx context.Context) (*UserGrafanaConfig, error)
 	CreateGrafanaAlertmanagerConfig(ctx context.Context, configuration *apimodels.PostableUserConfig, hash string, createdAt int64, isDefault bool) error
 	DeleteGrafanaAlertmanagerConfig(ctx context.Context) error
-
-	GetFullState(ctx context.Context) (*UserState, error)
 
 	TestTemplate(ctx context.Context, c alertingNotify.TestTemplatesConfigBodyParams) (*alertingNotify.TestTemplatesResults, error)
 	TestReceivers(ctx context.Context, c alertingNotify.TestReceiversConfigBodyParams) (*alertingNotify.TestReceiversResult, int, error)
