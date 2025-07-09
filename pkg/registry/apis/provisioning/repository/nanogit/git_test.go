62d3c4de125df0fc42ef4146f099523ec39f85c5
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-07-09 13:37:18 +0200

    Fix more unit tests

--- pkg/registry/apis/provisioning/repository/nanogit/git_test.go
+++ pkg/registry/apis/provisioning/repository/nanogit/git_test.go
@@ -1939,7 +1939,7 @@ func TestGitRepository_resolveRefToHash(t *testing.T) {
 
 			if tt.wantError {
 				require.Error(t, err)
-				require.Nil(t, refHash)
+				require.Equal(t, hash.Zero, refHash)
 			} else {
 				require.NoError(t, err)
 				require.NotNil(t, refHash)
@@ -3218,8 +3218,8 @@ func TestGitRepository_CompareFiles_EmptyBase(t *testing.T) {
 	// Verify CompareCommits was called with empty base hash and feature hash
 	require.Equal(t, 1, mockClient.CompareCommitsCallCount())
 	_, baseHash, refHash := mockClient.CompareCommitsArgsForCall(0)
-	require.Nil(t, baseHash) // Empty hash for empty base
-	require.Equal(t, hash.Hash{4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}, refHash)
+	require.Equal(t, hash.Zero, baseHash) // Empty hash for empty base
+	require.Equal(t, hash.MustFromHex("0102030405060708090a0b0c0d0e0f1011121314"), refHash)
 }
 
 func TestGitRepository_EmptyRefHandling(t *testing.T) {
