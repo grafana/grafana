3b13beee151c2be212ab7b462b037311964ae868
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-07-09 12:42:10 -0400

    Alerting: Separate configuration model for remote Alertmanager Mimir client (#107741)
    * replace PostableUserConfig with GrafanaAlertmanagerConfig to decouple from internal Grafana models
    * update alertmanager + tests
    * calculate hash of the GrafanaAlertmanagerConfig

--- pkg/services/ngalert/remote/client/mimir.go
+++ pkg/services/ngalert/remote/client/mimir.go
@@ -29,7 +29,7 @@ type MimirClient interface {
 	DeleteGrafanaAlertmanagerState(ctx context.Context) error
 
 	GetGrafanaAlertmanagerConfig(ctx context.Context) (*UserGrafanaConfig, error)
-	CreateGrafanaAlertmanagerConfig(ctx context.Context, configuration *apimodels.PostableUserConfig, hash string, createdAt int64, isDefault bool) error
+	CreateGrafanaAlertmanagerConfig(ctx context.Context, configuration *GrafanaAlertmanagerConfig, hash string, createdAt int64, isDefault bool) error
 	DeleteGrafanaAlertmanagerConfig(ctx context.Context) error
 
 	TestTemplate(ctx context.Context, c alertingNotify.TestTemplatesConfigBodyParams) (*alertingNotify.TestTemplatesResults, error)
