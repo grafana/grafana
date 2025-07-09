9d2eadcfd27354ef56ce7289a03a890c3f2e9cbe
Peter Štibraný <pstibrany@gmail.com>
2025-07-09 17:32:10 +0200

    PoC: replace using testing.Short in storage/unified package with integration test skip method (#107887)
    * PoC: check if testing.Short is called from integration tests only.
    
    * Rename helper function.
    
    * Fix logic.
    
    * Remove skipping of integration tests from non-integration tests.
    
    * Remove skipping of integration tests from non-integration tests.
    
    * Fix import.

--- pkg/storage/unified/sql/test/integration_test.go
+++ pkg/storage/unified/sql/test/integration_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/grafana/grafana/pkg/storage/unified/sql"
 	"github.com/grafana/grafana/pkg/storage/unified/sql/db/dbimpl"
 	unitest "github.com/grafana/grafana/pkg/storage/unified/testing"
+	"github.com/grafana/grafana/pkg/tests"
 	"github.com/grafana/grafana/pkg/tests/testsuite"
 	"github.com/grafana/grafana/pkg/util/testutil"
 )
@@ -95,9 +96,7 @@ func TestIntegrationSQLStorageBackend(t *testing.T) {
 }
 
 func TestIntegrationSearchAndStorage(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping integration test in short mode")
-	}
+	tests.SkipIntegrationTestInShortMode(t)
 
 	ctx := context.Background()
 
