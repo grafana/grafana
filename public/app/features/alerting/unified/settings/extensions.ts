2f29b0ab7576dabf8413c9a57e27b8dce1971fad
Konrad Lalik <konradlalik@gmail.com>
2025-07-09 09:14:53 +0200

    Update import paths

--- public/app/features/alerting/unified/settings/extensions.ts
+++ public/app/features/alerting/unified/settings/extensions.ts
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom-v5-compat';
 
 import { NavModelItem } from '@grafana/data';
-import { useSelector } from 'app/types';
+import { useSelector } from 'app/types/store';
 
 type SettingsSectionUrl = `/alerting/admin/${string}`;
 type SettingsSectionNav = Pick<NavModelItem, 'id' | 'text' | 'icon'> & {
