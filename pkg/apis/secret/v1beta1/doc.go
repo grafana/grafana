c6bf1dc417c8d95776fa39c78556a81e2900c931
Matheus Macabu <macabu.matheus@gmail.com>
2025-07-10 10:19:56 +0200

    Secrets: Add v1beta1 API version

---
+++ pkg/apis/secret/v1beta1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package
+// +k8s:openapi-gen=true
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=secret.grafana.app
+
+package v1beta1 // import "github.com/grafana/grafana/pkg/apis/secret/v1beta1"
