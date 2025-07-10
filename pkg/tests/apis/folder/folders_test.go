0fbdda717d72d373cdbcda55ff800a1f268a7bb1
Maicon Costa <maiconscosta@gmail.com>
2025-07-09 22:39:55 -0300

    Run TestIntegrationFoldersGetAPIEndpointK8S only for SQLite
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/tests/apis/folder/folders_test.go
+++ pkg/tests/apis/folder/folders_test.go
@@ -18,6 +18,7 @@ import (
 	folders "github.com/grafana/grafana/apps/folder/pkg/apis/folder/v1beta1"
 	"github.com/grafana/grafana/pkg/api/dtos"
 	grafanarest "github.com/grafana/grafana/pkg/apiserver/rest"
+	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/resourcepermissions"
 	"github.com/grafana/grafana/pkg/services/dashboards"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
@@ -1026,7 +1027,10 @@ func TestIntegrationFoldersGetAPIEndpointK8S(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
-	t.Skip("not working yet")
+
+	if !db.IsTestDbSQLite() {
+		t.Skip("test only on sqlite for now")
+	}
 
 	type testCase struct {
 		description         string
