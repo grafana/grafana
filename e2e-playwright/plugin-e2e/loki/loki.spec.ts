5baa97ae3baee397ea834fcb319040a2eb8747f5
Ashley Harrison <ashley.harrison@grafana.com>
2025-07-10 15:23:38 +0100

    move loki plugin-e2e test

--- e2e-playwright/plugin-e2e/loki/loki.spec.ts
+++ e2e-playwright/plugin-e2e/loki/loki.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@grafana/plugin-e2e';
 test('Smoke test: decoupled frontend plugin loads', async ({ createDataSourceConfigPage, page }) => {
   await createDataSourceConfigPage({ type: 'loki' });
 
-  await expect(await page.getByText('Type: Loki', { exact: true })).toBeVisible();
-  await expect(await page.getByRole('heading', { name: 'Connection', exact: true })).toBeVisible();
+  await expect(page.getByText('Type: Loki', { exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Connection', exact: true })).toBeVisible();
 });
