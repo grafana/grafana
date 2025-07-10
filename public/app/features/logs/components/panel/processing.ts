b50c95f4b250a584cbbc04929712efabf0b02515
Matias Chomicki <matyax@gmail.com>
2025-07-10 12:58:58 +0200

    processing: implement newlines truncation

--- public/app/features/logs/components/panel/processing.ts
+++ public/app/features/logs/components/panel/processing.ts
@@ -165,14 +165,24 @@ export class LogListModel implements LogRowModel {
   }
 
   updateCollapsedState(displayedFields: string[], container: HTMLDivElement | null) {
-    const lineLength =
+    const line =
       displayedFields.length > 0
-        ? displayedFields.map((field) => this.getDisplayedFieldValue(field, true)).join('').length
-        : this.entry.length;
-    const collapsed =
-      lineLength >= (this._virtualization?.getTruncationLength(container) ?? TRUNCATION_DEFAULT_LENGTH)
+        ? displayedFields.map((field) => this.getDisplayedFieldValue(field, true)).join('')
+        : this.entry;
+
+    // Length truncation
+    let collapsed =
+      line.length >= (this._virtualization?.getTruncationLength(container) ?? TRUNCATION_DEFAULT_LENGTH)
         ? true
         : undefined;
+
+    // Newlines truncation
+    if (!collapsed && this._virtualization) {
+      const truncationLimit = this._virtualization.getTruncationLineCount();
+      console.log(truncationLimit);
+      collapsed = countNewLines(this.raw, truncationLimit) >= truncationLimit ? true : collapsed;
+    }
+
     if (this.collapsed === undefined || collapsed === undefined) {
       this.collapsed = collapsed;
     }
@@ -236,4 +246,22 @@ function logLevelToDisplayLevel(level = '') {
     default:
       return level;
   }
+}
+
+function countNewLines(log: string, limit = Infinity) {
+  let count = 0;
+  for (let i = 0; i < log.length; ++i) {
+    // No need to iterate further
+    if (count > Infinity) {
+      return count;
+    }
+    if (log[i] === '\n') {
+      count += 1;
+    } else if (log[i] === '\r') {
+      count += 1;
+      // skip LF in CRLF
+      if (log[i] === '\n') i += 1;
+    }
+  }
+  return count;
 }
