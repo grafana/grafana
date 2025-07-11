a0a0cbf0128c7a93f4643019c9eaf47b237979bd
Yury Molodov <yurymolodov@gmail.com>
2025-07-11 14:22:50 +0200

    Tempo: Add Victoria Logs support for "traces to logs" (#105985)
    * feat(trace-to-logs): add VictoriaLogs datasource support
    
    Signed-off-by: Yury Molodov <yurymolodov@gmail.com>
    
    * tempo: fix lint errors in createSpanLink.tsx
    
    Signed-off-by: Yury Molodov <yurymolodov@gmail.com>
    
    ---------
    
    Signed-off-by: Yury Molodov <yurymolodov@gmail.com>

--- public/app/features/explore/TraceView/createSpanLink.test.ts
+++ public/app/features/explore/TraceView/createSpanLink.test.ts
@@ -1515,6 +1515,91 @@ describe('createSpanLinkFactory', () => {
       expect(decodeURIComponent(links![0].href)).toContain('spanName=\\"operation\\"');
     });
   });
+
+  describe('should return victorialogs link', () => {
+    const victoriaLogsUID = 'victoriaLogsUID';
+
+    beforeAll(() => {
+      setDataSourceSrv({
+        getInstanceSettings() {
+          return {
+            uid: victoriaLogsUID,
+            name: 'VictoriaLogs',
+            type: 'victoriametrics-logs-datasource',
+          } as unknown as DataSourceInstanceSettings;
+        },
+      } as unknown as DataSourceSrv);
+
+      setLinkSrv(new LinkSrv());
+      setTemplateSrv(new TemplateSrv());
+    });
+
+    it('with default keys when tags not configured', () => {
+      const createLink = setupSpanLinkFactory({}, victoriaLogsUID);
+      const links = createLink!(createTraceSpan());
+
+      const linkDef = links?.[0];
+      expect(linkDef).toBeDefined();
+      expect(linkDef?.type).toBe(SpanLinkType.Logs);
+      expect(linkDef!.href).toBe(
+        `/explore?left=${encodeURIComponent(
+          '{"range":{"from":"1602637200000","to":"1602637201000"},"datasource":"victoriaLogsUID","queries":[{"expr":"cluster:=\\"cluster1\\" AND hostname:=\\"hostname1\\" AND service_namespace:=\\"namespace1\\"","refId":""}]}'
+        )}`
+      );
+    });
+
+    it('formats query correctly if filterByTraceID and filterBySpanID is true', () => {
+      const createLink = setupSpanLinkFactory(
+        {
+          datasourceUid: victoriaLogsUID,
+          filterByTraceID: true,
+          filterBySpanID: true,
+        },
+        victoriaLogsUID
+      );
+
+      const links = createLink!(createTraceSpan());
+
+      const linkDef = links?.[0];
+      expect(linkDef).toBeDefined();
+      expect(linkDef?.type).toBe(SpanLinkType.Logs);
+      expect(linkDef!.href).toBe(
+        `/explore?left=${encodeURIComponent(
+          '{"range":{"from":"1602637200000","to":"1602637201000"},"datasource":"victoriaLogsUID","queries":[{"expr":"span_id:=\\"6605c7b08e715d6c\\" AND trace_id:=\\"7946b05c2e2e4e5a\\" AND cluster:=\\"cluster1\\" AND hostname:=\\"hostname1\\" AND service_namespace:=\\"namespace1\\"","refId":""}]}'
+        )}`
+      );
+    });
+
+    it('should format multiple tags correctly', () => {
+      const createLink = setupSpanLinkFactory(
+        {
+          tags: [{ key: 'ip' }, { key: 'hostname' }],
+        },
+        victoriaLogsUID
+      );
+
+      const links = createLink!(
+        createTraceSpan({
+          process: {
+            serviceName: 'service',
+            tags: [
+              { key: 'hostname', value: 'hostname1' },
+              { key: 'ip', value: '192.168.0.1' },
+            ],
+          },
+        })
+      );
+
+      const linkDef = links?.[0];
+      expect(linkDef).toBeDefined();
+      expect(linkDef?.type).toBe(SpanLinkType.Logs);
+      expect(linkDef!.href).toBe(
+        `/explore?left=${encodeURIComponent(
+          '{"range":{"from":"1602637200000","to":"1602637201000"},"datasource":"victoriaLogsUID","queries":[{"expr":"hostname:=\\"hostname1\\" AND ip:=\\"192.168.0.1\\"","refId":""}]}'
+        )}`
+      );
+    });
+  });
 });
 
 describe('dataFrame links', () => {
