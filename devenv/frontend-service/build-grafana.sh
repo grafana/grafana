a90a284dca04697362bfdf111ef319ce4994b448
joshhunt <josh.hunt@grafana.com>
2025-07-13 15:35:09 +0100

    rename more things

--- devenv/frontend-service/build-grafana.sh
+++ devenv/frontend-service/build-grafana.sh
@@ -2,23 +2,10 @@ VERSION=$(jq -r .version package.json)
 
 echo "Building Grafana with ${VERSION}"
 
-echo "Go mod cache: $(go env GOMODCACHE)"
-echo "Folders: $(ls -1 $(go env GOMODCACHE) | wc -l)"
-
-echo "---"
-
-echo "Go build cache: $(go env GOCACHE)"
-echo "Folders: $(ls -1 $(go env GOCACHE) | wc -l)"
-
-echo "---"
-
-echo "Build dir: $(pwd)"
-echo "Folders: $(ls -1 ./ | wc -l)"
-
-echo "---"
+echo "Go mod cache: $(go env GOMODCACHE), $(ls -1 $(go env GOMODCACHE) | wc -l) items"
+echo "Go build cache: $(go env GOCACHE), $(ls -1 $(go env GOCACHE) | wc -l) items"
 
 go build \
   -ldflags "-X main.version=${VERSION}" \
   -gcflags "all=-N -l" \
   -o ./bin/grafana ./pkg/cmd/grafana
-
--- devenv/frontend-service/proxy.dockerfile
+++ devenv/frontend-service/proxy.dockerfile
@@ -1,10 +1,6 @@
 # Runtime stage - just a simple nginx to serve static files
 FROM nginx:alpine
 
-# Delete the default nginx content
-RUN rm -rf /usr/share/nginx/html
-RUN mkdir -p /usr/share/nginx/html/public
-
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
