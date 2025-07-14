5abe27b952da0f0063b17dda5c66290681e9881d
Matias Chomicki <matyax@gmail.com>
2025-07-14 18:33:52 +0200

    LogLineDisplayedFields: add icons to organize

--- public/app/features/logs/components/panel/LogLineDetailsDisplayedFields.tsx
+++ public/app/features/logs/components/panel/LogLineDetailsDisplayedFields.tsx
@@ -9,24 +9,32 @@ import { Card, IconButton, useStyles2 } from '@grafana/ui';
 import { LOG_LINE_BODY_FIELD_NAME } from '../LogDetailsBody';
 
 import { useLogListContext } from './LogListContext';
+import { LogLineDetailsMode } from './LogLineDetails';
 
 export const LogLineDetailsDisplayedFields = () => {
   const { displayedFields, setDisplayedFields } = useLogListContext();
+
+  const reorganizeDisplayedFields = useCallback(
+    (srcIndex: number, destIndex: number) => {
+      const newDisplayedFields = [...displayedFields];
+      const element = displayedFields[srcIndex];
+
+      newDisplayedFields.splice(srcIndex, 1);
+      newDisplayedFields.splice(destIndex, 0, element);
+
+      setDisplayedFields?.(newDisplayedFields);
+    },
+    [displayedFields, setDisplayedFields]
+  );
 
   const onDragEnd = useCallback(
     (result: DropResult) => {
       if (result.destination == null) {
         return;
       }
-
-      const newDisplayedFields = [...displayedFields];
-      const element = displayedFields[result.source.index];
-      newDisplayedFields.splice(result.source.index, 1);
-      newDisplayedFields.splice(result.destination.index, 0, element);
-
-      setDisplayedFields?.(newDisplayedFields);
+      reorganizeDisplayedFields(result.source.index, result.source.index);
     },
-    [displayedFields, setDisplayedFields]
+    [reorganizeDisplayedFields]
   );
 
   return (
@@ -38,7 +46,12 @@ export const LogLineDetailsDisplayedFields = () => {
               <>
                 <div ref={provided.innerRef} {...provided.droppableProps}>
                   {displayedFields.map((field, index) => (
-                    <DraggableDisplayedField key={field} field={field} index={index} />
+                    <DraggableDisplayedField
+                      key={field}
+                      field={field}
+                      index={index}
+                      moveField={reorganizeDisplayedFields}
+                    />
                   ))}
                 </div>
                 {provided.placeholder}
@@ -54,26 +67,45 @@ export const LogLineDetailsDisplayedFields = () => {
 interface DraggableDisplayedFieldProps {
   field: string;
   index: number;
+  moveField: (srcIndex: number, destIndex: number) => void;
 }
 
-const DraggableDisplayedField = ({ field, index }: DraggableDisplayedFieldProps) => {
-  const { onClickHideField } = useLogListContext();
-  const styles = useStyles2(getStyles);
+const DraggableDisplayedField = ({ field, index, moveField }: DraggableDisplayedFieldProps) => {
+  const { detailsMode, displayedFields, onClickHideField } = useLogListContext();
+  const styles = useStyles2(getStyles, detailsMode);
+  const nextIndex = index === displayedFields.length - 1 ? 0 : index + 1;
+  const prevIndex = index === 0 ? displayedFields.length - 1 : index - 1;
   return (
     <Draggable draggableId={field} index={index}>
       {(provided) => (
         <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
           <Card noMargin className={styles.fieldCard}>
-            <div>
-              {field === LOG_LINE_BODY_FIELD_NAME ? t('logs.log-line-details.log-line-field', 'Log line') : field}
+            <div className={styles.fieldWrapper}>
+              <div className={styles.field}>
+                {field === LOG_LINE_BODY_FIELD_NAME ? t('logs.log-line-details.log-line-field', 'Log line') : field}
+              </div>
+              {displayedFields.length > 1 && (
+                <>
+                  <IconButton
+                    name="arrow-down"
+                    onClick={() => moveField(index, nextIndex)}
+                    tooltip={t('logs.log-line-details.move-displayed-field-down', 'Move down')}
+                  />
+                  <IconButton
+                    name="arrow-up"
+                    onClick={() => moveField(index, prevIndex)}
+                    tooltip={t('logs.log-line-details.move-displayed-field-up', 'Move up')}
+                  />
+                </>
+              )}
+              {onClickHideField && (
+                <IconButton
+                  name="times"
+                  onClick={() => onClickHideField(field)}
+                  tooltip={t('logs.log-line-details.remove-displayed-field', 'Remove field')}
+                />
+              )}
             </div>
-            {onClickHideField && (
-              <IconButton
-                name="times"
-                onClick={() => onClickHideField(field)}
-                tooltip={t('logs.log-line-details.remove-displayed-field', 'Remove field')}
-              />
-            )}
           </Card>
         </div>
       )}
@@ -81,11 +113,22 @@ const DraggableDisplayedField = ({ field, index }: DraggableDisplayedFieldProps)
   );
 };
 
-const getStyles = (theme: GrafanaTheme2) => ({
+const getStyles = (theme: GrafanaTheme2, detailsMode: LogLineDetailsMode) => ({
   fieldCard: css({
     cursor: 'move',
+    display: 'block',
     padding: theme.spacing(1),
     marginBottom: theme.spacing(1),
+    maxWidth: detailsMode === 'inline' ? 'max-content' : undefined,
+    minWidth: detailsMode === 'inline' ? '20%' : undefined,
     wordBreak: 'break-word',
+  }),
+  fieldWrapper: css({
+    display: 'flex',
+    gap: theme.spacing(0.5),
+    justifyContent: 'space-evenly',
+  }),
+  field: css({
+    flex: 1,
   }),
 });
