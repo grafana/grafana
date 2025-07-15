275f9f43d6c2ce371096a76f4dd479a014efc08f
maicon <maiconscosta@gmail.com>
2025-07-15 11:16:26 -0300

    Unistore: Fix dualwriter mode 3 (#108092)
    * Unistore: Fix dualwriter mode 3
    
    Fallback to legacy if Unified is the main store and we get an error when
    reading from it
    
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>
    
    
    ---------
    
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/tests/apis/dashboard/integration/api_validation_test.go
+++ pkg/tests/apis/dashboard/integration/api_validation_test.go
@@ -58,11 +58,10 @@ type TestContext struct {
 // TestIntegrationValidation tests the dashboard K8s API
 func TestIntegrationValidation(t *testing.T) {
 	if testing.Short() {
-		t.Skip("skipping integration test2")
+		t.Skip("skipping integration test")
 	}
 
-	// TODO: Skip mode3 - borken due to race conditions while setting default permissions across storage backends
-	dualWriterModes := []rest.DualWriterMode{rest.Mode0, rest.Mode1, rest.Mode2}
+	dualWriterModes := []rest.DualWriterMode{rest.Mode0, rest.Mode1, rest.Mode2, rest.Mode3, rest.Mode4, rest.Mode5}
 	for _, dualWriterMode := range dualWriterModes {
 		t.Run(fmt.Sprintf("DualWriterMode %d", dualWriterMode), func(t *testing.T) {
 			// Create a K8sTestHelper which will set up a real API server
