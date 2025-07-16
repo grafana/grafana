974103c6fa20701c5467c67fb24f173aea9113fb
Matias Chomicki <matias.chomicki@grafana.com>
2025-07-16 17:42:14 +0200

    OTel: Add experimental formatting for OTel identified log lines (#108170)
    * OTel: create formats service
    
    * LogListContext: use OTel service to resolve the default displayed fields
    
    * Create otelLogsFormatting feature flag
    
    * LogListContext: conditionally use otel format based on feature flag
    
    * OTel logs: reformat body and improve displayed fields
    
    * formats: remove duplicated scope name
    
    * Update formats.ts
    
    * format: use regex rules to exclude fields
    
    * Add tests
    
    * Formatting
    
    * LogsPanel: expose setDisplayedFields
    
    * LogListContext: refactor otel displayed fields
    
    * Prettier
    
    * format: optimize identifyOTelLanguages

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1765,6 +1765,13 @@ var (
 			FrontendOnly:      true,
 			Expression:        "false",
 		},
+		{
+			Name:         "otelLogsFormatting",
+			Description:  "Applies OTel formatting templates to displayed logs",
+			Stage:        FeatureStageExperimental,
+			FrontendOnly: true,
+			Owner:        grafanaObservabilityLogsSquad,
+		},
 	}
 )
 
