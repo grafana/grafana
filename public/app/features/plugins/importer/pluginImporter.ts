8b28b95d89ba20ed2fbe01c3ef7faa038609ac16
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-07-16 06:42:28 +0200

    Plugins: replaces various plugin imports with pluginImporter (#108002)
    * Plugins: renames plugin_loader
    
    * Wip
    
    * chore: adds pluginImporter
    
    * chore: some small refactors
    
    * chore: better typings
    
    * chore: merge functions
    
    * chore: create a generic plugin cache
    
    * chore: adds comments
    
    * chore: change to const
    
    * chore: remove unused change
    
    * chore: put everything behind feature toggle
    
    * chore: rename test file and props
    
    * chore: adds sync cache as well
    
    * chore: fix the typings
    
    * chore: fix broken unit test
    
    * chore: small rename
    
    * chore: adds tests
    
    * chore: updates after PR feedback
    
    * chore: updates after PR feedback

---
+++ public/app/features/plugins/importer/pluginImporter.ts
@@ -0,0 +1,185 @@
+import {
+  AppPlugin,
+  AppPluginMeta,
+  DataQuery,
+  DataSourceApi,
+  DataSourceJsonData,
+  DataSourcePlugin,
+  DataSourcePluginMeta,
+  PanelPlugin,
+  PanelPluginMeta,
+  PluginLoadingStrategy,
+  PluginMeta,
+  throwIfAngular,
+} from '@grafana/data';
+import { config } from '@grafana/runtime';
+import { GenericDataSourcePlugin } from 'app/features/datasources/types';
+import { getPanelPluginLoadError } from 'app/features/panel/components/PanelPluginError';
+
+import {
+  addedComponentsRegistry,
+  addedFunctionsRegistry,
+  addedLinksRegistry,
+  exposedComponentsRegistry,
+} from '../extensions/registry/setup';
+import { pluginsLogger } from '../utils';
+
+import { importPluginModule } from './importPluginModule';
+import { PluginImporter, PostImportStrategy, PreImportStrategy } from './types';
+
+const defaultPreImport: PreImportStrategy = (plugin) => {
+  throwIfAngular(plugin);
+  const fallbackLoadingStrategy = plugin.loadingStrategy ?? PluginLoadingStrategy.fetch;
+
+  const args = {
+    path: plugin.module,
+    version: plugin.info?.version,
+    loadingStrategy: fallbackLoadingStrategy,
+    pluginId: plugin.id,
+    moduleHash: plugin.moduleHash,
+    translations: plugin.translations,
+  };
+
+  return args;
+};
+
+const panelPluginPostImport: PostImportStrategy<PanelPlugin, PanelPluginMeta> = async (meta, module) => {
+  try {
+    const pluginExports = await module;
+
+    if (pluginExports.plugin) {
+      const plugin: PanelPlugin = pluginExports.plugin;
+      plugin.meta = meta;
+      pluginsCache.set(meta.id, plugin);
+      return plugin;
+    }
+
+    throwIfAngular(pluginExports);
+    throw new Error('missing export: plugin');
+  } catch (error) {
+    // TODO, maybe a different error plugin
+    console.warn('Error loading panel plugin: ' + meta.id, error);
+    return getPanelPluginLoadError(meta, error);
+  }
+};
+
+const datasourcePluginPostImport: PostImportStrategy<GenericDataSourcePlugin, DataSourcePluginMeta> = async (
+  meta,
+  module
+) => {
+  const pluginExports = await module;
+
+  if (pluginExports.plugin) {
+    const dsPlugin: GenericDataSourcePlugin = pluginExports.plugin;
+    dsPlugin.meta = meta;
+    pluginsCache.set(meta.id, dsPlugin);
+    return dsPlugin;
+  }
+
+  if (pluginExports.Datasource) {
+    const dsPlugin = new DataSourcePlugin<DataSourceApi<DataQuery, DataSourceJsonData>, DataQuery, DataSourceJsonData>(
+      pluginExports.Datasource
+    );
+    dsPlugin.setComponentsFromLegacyExports(pluginExports);
+    dsPlugin.meta = meta;
+    pluginsCache.set(meta.id, dsPlugin);
+    return dsPlugin;
+  }
+
+  throw new Error('Plugin module is missing DataSourcePlugin or Datasource constructor export');
+};
+
+const appPluginPostImport: PostImportStrategy<AppPlugin, AppPluginMeta> = async (meta, module) => {
+  const pluginExports = await module;
+
+  const { plugin = new AppPlugin() } = pluginExports;
+  plugin.init(meta);
+  plugin.meta = meta;
+  plugin.setComponentsFromLegacyExports(pluginExports);
+
+  exposedComponentsRegistry.register({ pluginId: meta.id, configs: plugin.exposedComponentConfigs || [] });
+  addedComponentsRegistry.register({ pluginId: meta.id, configs: plugin.addedComponentConfigs || [] });
+  addedLinksRegistry.register({ pluginId: meta.id, configs: plugin.addedLinkConfigs || [] });
+  addedFunctionsRegistry.register({ pluginId: meta.id, configs: plugin.addedFunctionConfigs || [] });
+
+  pluginsCache.set(meta.id, plugin);
+  return plugin;
+};
+
+const promisesCache: Map<string, Promise<PanelPlugin | GenericDataSourcePlugin | AppPlugin>> = new Map();
+
+const getPromiseFromCache = <M extends PluginMeta, P extends PanelPlugin | GenericDataSourcePlugin | AppPlugin>(
+  meta: M
+): Promise<P> => {
+  const cached = promisesCache.get(meta.id);
+  if (cached) {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return cached as Promise<P>;
+  }
+
+  throw new Error(`Trying to get unknown plugin type ${meta.type} from cache for plugin ${meta.id}`);
+};
+
+const pluginsCache: Map<string, PanelPlugin | GenericDataSourcePlugin | AppPlugin> = new Map();
+
+const getPluginFromCache = <P extends PanelPlugin | GenericDataSourcePlugin | AppPlugin>(id: string): P | undefined => {
+  const cached = pluginsCache.get(id);
+  if (cached) {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return cached as P;
+  }
+
+  return undefined;
+};
+
+const importPlugin = <M extends PluginMeta, P extends PanelPlugin | GenericDataSourcePlugin | AppPlugin>(
+  meta: M,
+  postImportStrategy: PostImportStrategy<P, M>,
+  preImportStrategy: PreImportStrategy<M> = defaultPreImport
+): Promise<P> => {
+  const cached = getPluginFromCache<P>(meta.id);
+  if (cached) {
+    pluginsLogger.logDebug(`Retrieving plugin from cache`, {
+      path: meta.module,
+      pluginId: meta.id,
+      pluginVersion: meta.info?.version ?? '',
+      expectedHash: meta.moduleHash ?? '',
+      loadingStrategy: meta.loadingStrategy ?? PluginLoadingStrategy.fetch,
+      sriChecksEnabled: String(Boolean(config.featureToggles.pluginsSriChecks)),
+      newPluginLoadingEnabled: String(Boolean(config.featureToggles.enablePluginImporter)),
+    });
+    return Promise.resolve(cached);
+  }
+
+  if (promisesCache.has(meta.id)) {
+    pluginsLogger.logDebug(`Retrieving plugin from inflight plugin load request`, {
+      path: meta.module,
+      pluginId: meta.id,
+      pluginVersion: meta.info?.version ?? '',
+      expectedHash: meta.moduleHash ?? '',
+      loadingStrategy: meta.loadingStrategy ?? PluginLoadingStrategy.fetch,
+      sriChecksEnabled: String(Boolean(config.featureToggles.pluginsSriChecks)),
+      newPluginLoadingEnabled: String(Boolean(config.featureToggles.enablePluginImporter)),
+    });
+    return getPromiseFromCache(meta);
+  }
+
+  const args = preImportStrategy(meta);
+  const module = importPluginModule(args);
+  const plugin = postImportStrategy(meta, module);
+  promisesCache.set(meta.id, plugin);
+
+  return getPromiseFromCache(meta);
+};
+
+export const pluginImporter: PluginImporter = {
+  importPanel: (meta: PanelPluginMeta) => importPlugin(meta, panelPluginPostImport),
+  importDataSource: (meta: DataSourcePluginMeta) => importPlugin(meta, datasourcePluginPostImport),
+  importApp: (meta: AppPluginMeta) => importPlugin(meta, appPluginPostImport),
+  getPanel: (id: string) => getPluginFromCache<PanelPlugin>(id), // we need this sync because how the panel plugins are loaded in PanelRenderer
+};
+
+export const clearCaches = () => {
+  promisesCache.clear();
+  pluginsCache.clear();
+};
--- public/app/features/plugins/pluginLoader.ts
+++ public/app/features/plugins/pluginLoader.ts
@@ -8,31 +8,27 @@ import {
   PluginMeta,
   throwIfAngular,
 } from '@grafana/data';
-import { DEFAULT_LANGUAGE } from '@grafana/i18n';
-import { addResourceBundle, getResolvedLanguage } from '@grafana/i18n/internal';
 import { config } from '@grafana/runtime';
 import { DataQuery } from '@grafana/schema';
 
 import { GenericDataSourcePlugin } from '../datasources/types';
 
-import builtInPlugins from './built_in_plugins';
 import {
   addedComponentsRegistry,
   addedFunctionsRegistry,
   addedLinksRegistry,
   exposedComponentsRegistry,
 } from './extensions/registry/setup';
-import { getPluginFromCache, registerPluginInCache } from './loader/cache';
+import { importPluginModule } from './importer/importPluginModule';
+import { pluginImporter } from './importer/pluginImporter';
+import { getPluginFromCache } from './loader/cache';
 // SystemJS has to be imported before the sharedDependenciesMap
 import { SystemJS } from './loader/systemjs';
 // eslint-disable-next-line import/order
 import { sharedDependenciesMap } from './loader/sharedDependencies';
 import { decorateSystemJSFetch, decorateSystemJSResolve, decorateSystemJsOnload } from './loader/systemjsHooks';
 import { SystemJSWithLoaderHooks } from './loader/types';
-import { buildImportMap, resolveModulePath } from './loader/utils';
-import { importPluginModuleInSandbox } from './sandbox/sandboxPluginLoader';
-import { shouldLoadPluginInFrontendSandbox } from './sandbox/sandboxPluginLoaderRegistry';
-import { pluginsLogger } from './utils';
+import { buildImportMap } from './loader/utils';
 
 const imports = buildImportMap(sharedDependenciesMap);
 
@@ -78,84 +74,11 @@ systemJSPrototype.resolve = decorateSystemJSResolve.bind(systemJSPrototype, syst
 // Any css files loaded via SystemJS have their styles applied onload.
 systemJSPrototype.onload = decorateSystemJsOnload;
 
-type PluginImportInfo = {
-  path: string;
-  pluginId: string;
-  loadingStrategy: PluginLoadingStrategy;
-  version?: string;
-  moduleHash?: string;
-  translations?: Record<string, string>;
-};
-
-export async function importPluginModule({
-  path,
-  pluginId,
-  loadingStrategy,
-  version,
-  moduleHash,
-  translations,
-}: PluginImportInfo): Promise<System.Module> {
-  if (version) {
-    registerPluginInCache({ path, version, loadingStrategy });
-  }
-
-  // Add locales to i18n for a plugin if the feature toggle is enabled and the plugin has locales
-  if (config.featureToggles.localizationForPlugins && translations) {
-    await addTranslationsToI18n({
-      resolvedLanguage: getResolvedLanguage(),
-      fallbackLanguage: DEFAULT_LANGUAGE,
-      pluginId,
-      translations,
-    });
-  }
-
-  const builtIn = builtInPlugins[path];
-  if (builtIn) {
-    // for handling dynamic imports
-    if (typeof builtIn === 'function') {
-      return await builtIn();
-    } else {
-      return builtIn;
-    }
-  }
-
-  const modulePath = resolveModulePath(path);
-
-  // inject integrity hash into SystemJS import map
-  if (config.featureToggles.pluginsSriChecks) {
-    const resolvedModule = System.resolve(modulePath);
-    const integrityMap = System.getImportMap().integrity;
-
-    if (moduleHash && integrityMap && !integrityMap[resolvedModule]) {
-      SystemJS.addImportMap({
-        integrity: {
-          [resolvedModule]: moduleHash,
-        },
-      });
-    }
-  }
-
-  // the sandboxing environment code cannot work in nodejs and requires a real browser
-  if (await shouldLoadPluginInFrontendSandbox({ pluginId })) {
-    return importPluginModuleInSandbox({ pluginId });
-  }
-
-  return SystemJS.import(modulePath).catch((e) => {
-    let error = new Error('Could not load plugin: ' + e);
-    console.error(error);
-    pluginsLogger.logError(error, {
-      path,
-      pluginId,
-      pluginVersion: version ?? '',
-      expectedHash: moduleHash ?? '',
-      loadingStrategy: loadingStrategy.toString(),
-      sriChecksEnabled: (config.featureToggles.pluginsSriChecks ?? false).toString(),
-    });
-    throw error;
-  });
-}
-
 export function importDataSourcePlugin(meta: DataSourcePluginMeta): Promise<GenericDataSourcePlugin> {
+  if (config.featureToggles.enablePluginImporter) {
+    return pluginImporter.importDataSource(meta);
+  }
+
   throwIfAngular(meta);
 
   const fallbackLoadingStrategy = meta.loadingStrategy ?? PluginLoadingStrategy.fetch;
@@ -191,6 +114,10 @@ export function importDataSourcePlugin(meta: DataSourcePluginMeta): Promise<Gene
 const importPromises: Record<string, Promise<AppPlugin>> = {};
 
 export async function importAppPlugin(meta: PluginMeta): Promise<AppPlugin> {
+  if (config.featureToggles.enablePluginImporter) {
+    return pluginImporter.importApp(meta);
+  }
+
   const pluginId = meta.id;
 
   // We are caching the import promises to prevent duplicate imports
@@ -236,50 +163,4 @@ async function doImportAppPlugin(meta: PluginMeta): Promise<AppPlugin> {
   });
 
   return plugin;
-}
-
-interface AddTranslationsToI18nOptions {
-  resolvedLanguage: string;
-  fallbackLanguage: string;
-  pluginId: string;
-  translations: Record<string, string>;
-}
-
-// exported for testing purposes only
-export async function addTranslationsToI18n({
-  resolvedLanguage,
-  fallbackLanguage,
-  pluginId,
-  translations,
-}: AddTranslationsToI18nOptions): Promise<void> {
-  const resolvedPath = translations[resolvedLanguage];
-  const fallbackPath = translations[fallbackLanguage];
-  const path = resolvedPath ?? fallbackPath;
-
-  if (!path) {
-    console.warn(`Could not find any translation for plugin ${pluginId}`, { resolvedLanguage, fallbackLanguage });
-    return;
-  }
-
-  try {
-    const module = await SystemJS.import(resolveModulePath(path));
-    if (!module.default) {
-      console.warn(`Could not find default export for plugin ${pluginId}`, {
-        resolvedLanguage,
-        fallbackLanguage,
-        path,
-      });
-      return;
-    }
-
-    const language = resolvedPath ? resolvedLanguage : fallbackLanguage;
-    addResourceBundle(language, pluginId, module.default);
-  } catch (error) {
-    console.warn(`Could not load translation for plugin ${pluginId}`, {
-      resolvedLanguage,
-      fallbackLanguage,
-      error,
-      path,
-    });
-  }
 }
