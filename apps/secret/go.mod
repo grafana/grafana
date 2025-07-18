c94975e87ab82cab2fe6d3ad65d2542b85ecc8f2
Matheus Macabu <macabu.matheus@gmail.com>
2025-07-18 14:40:19 +0200

    Secrets: Remove MarshalJSON/MarshalYAML implementation for ExposedSecureValue
    The typed client will try to marshal the ExposedSecureValue type as json,
    and will call our custom implementation that redacts the data.

--- apps/secret/go.mod
+++ apps/secret/go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
-	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/apimachinery v0.33.2
 	k8s.io/kube-openapi v0.0.0-20250318190949-c8a335a9a2ff
 	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738
@@ -57,6 +56,7 @@ require (
 	golang.org/x/time v0.11.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/client-go v0.33.2 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
--- apps/secret/pkg/apis/secret/v1beta1/exposed_secure_value_test.go
+++ apps/secret/pkg/apis/secret/v1beta1/exposed_secure_value_test.go
@@ -6,12 +6,10 @@ package v1beta1
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v3"
 )
 
 func TestExposedSecureValue(t *testing.T) {
@@ -32,16 +30,6 @@ func TestExposedSecureValue(t *testing.T) {
 	_, err := fmt.Fprintf(buf, "%#v", esv)
 	require.NoError(t, err)
 	require.Equal(t, expected, buf.String())
-
-	// MarshalJSON must not return the exposed secure value.
-	bytes, err := json.Marshal(esv)
-	require.NoError(t, err)
-	require.Equal(t, `"`+expected+`"`, string(bytes))
-
-	// MarshalYAML must not return the exposed secure value.
-	bytes, err = yaml.Marshal(esv)
-	require.NoError(t, err)
-	require.Equal(t, "'"+expected+"'\n", string(bytes))
 
 	// DangerouslyExposeAndConsumeValue returns the raw value.
 	require.Equal(t, rawValue, esv.DangerouslyExposeAndConsumeValue())
