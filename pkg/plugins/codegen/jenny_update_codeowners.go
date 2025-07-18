b1061d5955219376ecf17729969bd4f26308e093
spinillos <selenepinillos@gmail.com>
2025-07-18 15:59:30 +0200

    Fix fmt

--- pkg/plugins/codegen/jenny_update_codeowners.go
+++ pkg/plugins/codegen/jenny_update_codeowners.go
@@ -7,7 +7,7 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-	
+
 	"github.com/grafana/codejen"
 	"github.com/grafana/grafana/pkg/plugins/codegen/pfs"
 )
@@ -29,22 +29,22 @@ func (jenny *UpdateCodeOwners) Generate(decl *pfs.PluginDecl) (codejen.Files, er
 	if err != nil {
 		return nil, err
 	}
-	
+
 	lines := strings.Split(string(codeowners), "\n")
-	
+
 	codeOwner := lookForCodeOwner(lines, filepath.Join(jenny.PanelsFolder, decl.PluginPath))
 	if codeOwner == "" {
 		fmt.Printf("we couldn't find a code owner for %s plugin\n", filepath.Join(jenny.PanelsFolder, decl.PluginPath))
 		return nil, nil
 	}
-	
+
 	pathName := strings.ReplaceAll(strings.ToLower(decl.PluginMeta.Name), " ", "")
 	codeownerPath := fmt.Sprintf(generatedSchemas, pathName)
 	genCodeOwner := lookForCodeOwner(lines, codeownerPath)
 	if genCodeOwner != "" {
 		return nil, nil
 	}
-	
+
 	fmt.Println("assigning codeowner for", pathName)
 	return nil, jenny.insertCodeOwner(lines, fmt.Sprintf("/%s %s", codeownerPath, codeOwner))
 }
@@ -57,13 +57,13 @@ func lookForCodeOwner(lines []string, path string) string {
 		if len(parts) != 3 {
 			continue
 		}
-		
+
 		if strings.Trim(parts[1], "/") == path {
 			codeOwner = parts[2]
 			break
 		}
 	}
-	
+
 	return codeOwner
 }
 
@@ -84,22 +84,22 @@ func (jenny *UpdateCodeOwners) insertCodeOwner(lines []string, path string) erro
 			break
 		}
 	}
-	
+
 	if pkgStart == -1 {
 		return nil
 	}
-	
+
 	packageLines := lines[pkgStart+1 : pkgEnd-1]
 	packageLines = append(packageLines, path)
 	sort.Strings(packageLines)
-	
+
 	var output []string
 	output = append(output, lines[:pkgStart+1]...)
 	output = append(output, packageLines...)
 	if pkgEnd < len(lines) && (len(output) == 0 || output[len(output)-1] != "") {
 		output = append(output, "")
 	}
-	
+
 	output = append(output, lines[pkgEnd:]...)
 	return os.WriteFile(filepath.Join(jenny.Root, ".github", "CODEOWNERS"), []byte(strings.Join(output, "\n")), 0644)
 }
