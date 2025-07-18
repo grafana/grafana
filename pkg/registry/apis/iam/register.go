6bd70ac71537e56fdf82629c468dba59819dcec1
Mihaly Gyongyosi <mgyongyosi@users.noreply.github.com>
2025-07-18 10:57:06 +0200

    Comment out DW setup for users

--- pkg/registry/apis/iam/register.go
+++ pkg/registry/apis/iam/register.go
@@ -126,23 +126,22 @@ func (b *IdentityAccessManagementAPIBuilder) UpdateAPIGroupInfo(apiGroupInfo *ge
 	storage[teamBindingResource.StoragePath()] = team.NewLegacyBindingStore(b.store)
 
 	userResource := legacyiamv0.UserResourceInfo
-	store, err := grafanaregistry.NewRegistryStore(opts.Scheme, userResource, opts.OptsGetter)
-	if err != nil {
-		return err
-	}
-
-	opts.StorageOptsRegister(userResource.GroupResource(), apistore.StorageOptions{
-		RequireDeprecatedInternalID: true,
-	})
-
 	legacyStore := user.NewLegacyStore(b.store, b.legacyAccessClient)
 
-	dw, err := opts.DualWriteBuilder(userResource.GroupResource(), legacyStore, store)
-	if err != nil {
-		return err
-	}
+	// TODO: Figure out what's missing for the DualWriter setup in a MT setup
+	// MT app is unable to start if DW is configured
+	// store, err := grafanaregistry.NewRegistryStore(opts.Scheme, userResource, opts.OptsGetter)
+	// if err != nil {
+	// 	return err
+	// }
 
-	storage[userResource.StoragePath()] = dw
+	// dw, err := opts.DualWriteBuilder(userResource.GroupResource(), legacyStore, store)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// storage[userResource.StoragePath()] = dw
+	storage[userResource.StoragePath()] = legacyStore
 	storage[userResource.StoragePath("teams")] = user.NewLegacyTeamMemberREST(b.store)
 
 	serviceAccountResource := legacyiamv0.ServiceAccountResourceInfo
