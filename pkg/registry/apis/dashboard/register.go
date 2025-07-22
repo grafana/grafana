8a9d89575a722fb32c1fd95cc59741252924d37a
Dominik Prokop <dominik.prokop@grafana.com>
2025-07-22 17:35:26 +0200

    Fix merge

--- pkg/registry/apis/dashboard/register.go
+++ pkg/registry/apis/dashboard/register.go
@@ -159,7 +159,6 @@ func (b *DashboardsAPIBuilder) GetGroupVersions() []schema.GroupVersion {
 	if featuremgmt.AnyEnabled(b.features, featuremgmt.FlagDashboardNewLayouts) {
 		// If dashboards v2 is enabled, we want to use v2alpha1 as the default API version.
 		return []schema.GroupVersion{
-			dashv2alpha2.DashboardResourceInfo.GroupVersion(),
 			dashv2alpha1.DashboardResourceInfo.GroupVersion(),
 			dashv0.DashboardResourceInfo.GroupVersion(),
 			dashv1.DashboardResourceInfo.GroupVersion(),
@@ -170,8 +169,8 @@ func (b *DashboardsAPIBuilder) GetGroupVersions() []schema.GroupVersion {
 	return []schema.GroupVersion{
 		dashv1.DashboardResourceInfo.GroupVersion(),
 		dashv0.DashboardResourceInfo.GroupVersion(),
-		dashv2alpha2.DashboardResourceInfo.GroupVersion(),
 		dashv2alpha1.DashboardResourceInfo.GroupVersion(),
+		dashv2alpha2.DashboardResourceInfo.GroupVersion(),
 	}
 }
 
