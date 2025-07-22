15140c21895cdb3a1177cd574e5ce78d46c6dfd8
Clarity-89 <homes89@ukr.net>
2025-07-22 16:25:14 +0300

    Refactor test

--- public/app/features/browse-dashboards/components/DashboardsTree.test.tsx
+++ public/app/features/browse-dashboards/components/DashboardsTree.test.tsx
@@ -23,6 +23,12 @@ function render(...[ui, options]: Parameters<typeof rtlRender>) {
 describe('browse-dashboards DashboardsTree', () => {
   const WIDTH = 800;
   const HEIGHT = 600;
+  const mockPermissions = {
+    canEditFolders: true,
+    canEditDashboards: true,
+    canDeleteFolders: true,
+    canDeleteDashboards: true,
+  };
 
   const folder = wellFormedFolder(1);
   const emptyFolderIndicator = wellFormedEmptyFolder();
@@ -36,15 +42,20 @@ describe('browse-dashboards DashboardsTree', () => {
     config.sharedWithMeFolderUID = 'sharedwithme';
   });
 
+  afterEach(() => {
+    // Reset permissions back to defaults
+    Object.assign(mockPermissions, {
+      canEditFolders: true,
+      canEditDashboards: true,
+      canDeleteFolders: true,
+      canDeleteDashboards: true,
+    });
+  });
+
   it('renders a dashboard item', () => {
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[dashboard]}
         isSelected={isSelected}
         width={WIDTH}
@@ -62,14 +73,14 @@ describe('browse-dashboards DashboardsTree', () => {
   });
 
   it('does not render checkbox when disabled', () => {
+    mockPermissions.canEditFolders = false;
+    mockPermissions.canEditDashboards = false;
+    mockPermissions.canDeleteFolders = false;
+    mockPermissions.canDeleteDashboards = false;
+
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: false,
-          canEditDashboards: false,
-          canDeleteFolders: false,
-          canDeleteDashboards: false,
-        }}
+        permissions={mockPermissions}
         items={[dashboard]}
         isSelected={isSelected}
         width={WIDTH}
@@ -89,12 +100,7 @@ describe('browse-dashboards DashboardsTree', () => {
   it('renders a folder item', () => {
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[folder]}
         isSelected={isSelected}
         width={WIDTH}
@@ -113,12 +119,7 @@ describe('browse-dashboards DashboardsTree', () => {
   it('renders a folder link', () => {
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[folder]}
         isSelected={isSelected}
         width={WIDTH}
@@ -139,12 +140,7 @@ describe('browse-dashboards DashboardsTree', () => {
 
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[sharedWithMe, folder]}
         isSelected={isSelected}
         width={WIDTH}
@@ -165,12 +161,7 @@ describe('browse-dashboards DashboardsTree', () => {
 
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[sharedWithMe, folder]}
         isSelected={isSelected}
         width={WIDTH}
@@ -192,12 +183,7 @@ describe('browse-dashboards DashboardsTree', () => {
     const handler = jest.fn();
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[folder]}
         isSelected={isSelected}
         width={WIDTH}
@@ -218,12 +204,7 @@ describe('browse-dashboards DashboardsTree', () => {
   it('renders empty folder indicators', () => {
     render(
       <DashboardsTree
-        permissions={{
-          canEditFolders: true,
-          canEditDashboards: true,
-          canDeleteFolders: true,
-          canDeleteDashboards: true,
-        }}
+        permissions={mockPermissions}
         items={[emptyFolderIndicator]}
         isSelected={isSelected}
         width={WIDTH}
