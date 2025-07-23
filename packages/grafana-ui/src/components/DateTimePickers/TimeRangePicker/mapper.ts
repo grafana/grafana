a580ab5ac3d9250581b4d549636a9307b9d4104e
samsch <git@samsch.org>
2025-07-23 15:37:09 -0400

    replace new mapper additional code with new rangeUtil.describeTimeRange

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/mapper.ts
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/mapper.ts
@@ -1,21 +1,10 @@
 import { TimeOption, TimeRange, TimeZone, rangeUtil, dateTimeFormat } from '@grafana/data';
-import { formatDateRange } from '@grafana/i18n';
 
 import { getFeatureToggle } from '../../../utils/featureToggle';
 import { commonFormat } from '../commonFormat';
+
 export const mapOptionToTimeRange = (option: TimeOption, timeZone?: TimeZone): TimeRange => {
   return rangeUtil.convertRawToRange({ from: option.from, to: option.to }, timeZone, undefined, commonFormat);
-};
-
-// TODO: Should we keep these format presets somewhere common?
-const rangeFormatShort: Intl.DateTimeFormatOptions = {
-  dateStyle: 'short',
-  timeStyle: 'short',
-};
-
-const rangeFormatFull: Intl.DateTimeFormatOptions = {
-  dateStyle: 'short',
-  timeStyle: 'medium',
 };
 
 export const mapRangeToTimeOption = (range: TimeRange, timeZone?: TimeZone): TimeOption => {
@@ -25,13 +14,7 @@ export const mapRangeToTimeOption = (range: TimeRange, timeZone?: TimeZone): Tim
   let display = `${from} to ${to}`;
 
   if (getFeatureToggle('localeFormatPreference')) {
-    const fromDate = range.from.toDate();
-    const toDate = range.to.toDate();
-
-    // The short time format doesn't include seconds, so change the format
-    // if the range includes seconds
-    const hasSeconds = fromDate.getSeconds() !== 0 || toDate.getSeconds() !== 0;
-    display = formatDateRange(fromDate, toDate, hasSeconds ? rangeFormatFull : rangeFormatShort);
+    display = rangeUtil.describeTimeRange(range, timeZone);
   }
 
   return {
