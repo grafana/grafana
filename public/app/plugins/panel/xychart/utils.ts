1a54fcfc84cb2372a129ba8b5f0227c17fc39b6b
Leon Sorokin <leeoniya@gmail.com>
2025-07-23 09:55:58 -0500

    XYChart: Lift restrictions around re-using fields for color and size (#106690)

--- public/app/plugins/panel/xychart/utils.ts
+++ public/app/plugins/panel/xychart/utils.ts
@@ -99,14 +99,8 @@ export function prepSeries(
       // only grabbing number fields (exclude time, string, enum, other)
       let onlyNumFields = onlyNumTimeFields.filter((field) => field.type === FieldType.number);
 
-      let color =
-        colorMatcher != null
-          ? onlyNumFields.find((field) => field !== x && colorMatcher!(field, frame, frames))
-          : undefined;
-      let size =
-        sizeMatcher != null
-          ? onlyNumFields.find((field) => field !== x && field !== color && sizeMatcher!(field, frame, frames))
-          : undefined;
+      let color = colorMatcher != null ? onlyNumFields.find((field) => colorMatcher(field, frame, frames)) : undefined;
+      let size = sizeMatcher != null ? onlyNumFields.find((field) => sizeMatcher(field, frame, frames)) : undefined;
 
       // x field is required
       if (x != null) {
