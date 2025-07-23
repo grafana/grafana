41adca4edda5ffa40828dacb47a838ce9dda97be
Mihai Turdean <6640685+mihai-turdean@users.noreply.github.com>
2025-07-23 16:15:55 +0000

    Change to watch for CoreRole

--- apps/iam/pkg/watchers/watcher_corerole.go
+++ apps/iam/pkg/watchers/watcher_corerole.go
@@ -7,25 +7,26 @@ import (
 	"github.com/grafana/grafana-app-sdk/logging"
 	"github.com/grafana/grafana-app-sdk/operator"
 	"github.com/grafana/grafana-app-sdk/resource"
-	"github.com/grafana/grafana/apps/iam/pkg/apis/iam/v0alpha1"
 	"go.opentelemetry.io/otel"
+
+	"github.com/grafana/grafana/apps/iam/pkg/apis/iam/v0alpha1"
 )
 
-var _ operator.ResourceWatcher = &ResourcePermissionWatcher{}
+var _ operator.ResourceWatcher = &CoreRoleWatcher{}
 
-type ResourcePermissionWatcher struct{}
+type CoreRoleWatcher struct{}
 
-func NewResourcePermissionWatcher() (*ResourcePermissionWatcher, error) {
-	return &ResourcePermissionWatcher{}, nil
+func NewCoreRoleWatcher() (*CoreRoleWatcher, error) {
+	return &CoreRoleWatcher{}, nil
 }
 
-// Add handles add events for v0alpha1.ResourcePermission resources.
-func (s *ResourcePermissionWatcher) Add(ctx context.Context, rObj resource.Object) error {
+// Add handles add events for v0alpha1.CoreRole resources.
+func (s *CoreRoleWatcher) Add(ctx context.Context, rObj resource.Object) error {
 	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-add")
 	defer span.End()
-	object, ok := rObj.(*v0alpha1.ResourcePermission)
+	object, ok := rObj.(*v0alpha1.CoreRole)
 	if !ok {
-		return fmt.Errorf("provided object is not of type *v0alpha1.ResourcePermission (name=%s, namespace=%s, kind=%s)",
+		return fmt.Errorf("provided object is not of type *v0alpha1.CoreRole (name=%s, namespace=%s, kind=%s)",
 			rObj.GetStaticMetadata().Name, rObj.GetStaticMetadata().Namespace, rObj.GetStaticMetadata().Kind)
 	}
 
@@ -34,19 +35,19 @@ func (s *ResourcePermissionWatcher) Add(ctx context.Context, rObj resource.Objec
 	return nil
 }
 
-// Update handles update events for v0alpha1.ResourcePermission resources.
-func (s *ResourcePermissionWatcher) Update(ctx context.Context, rOld resource.Object, rNew resource.Object) error {
+// Update handles update events for v0alpha1.CoreRole resources.
+func (s *CoreRoleWatcher) Update(ctx context.Context, rOld resource.Object, rNew resource.Object) error {
 	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-update")
 	defer span.End()
-	oldObject, ok := rOld.(*v0alpha1.ResourcePermission)
+	oldObject, ok := rOld.(*v0alpha1.CoreRole)
 	if !ok {
-		return fmt.Errorf("provided object is not of type *v0alpha1.ResourcePermission (name=%s, namespace=%s, kind=%s)",
+		return fmt.Errorf("provided object is not of type *v0alpha1.CoreRole (name=%s, namespace=%s, kind=%s)",
 			rOld.GetStaticMetadata().Name, rOld.GetStaticMetadata().Namespace, rOld.GetStaticMetadata().Kind)
 	}
 
-	_, ok = rNew.(*v0alpha1.ResourcePermission)
+	_, ok = rNew.(*v0alpha1.CoreRole)
 	if !ok {
-		return fmt.Errorf("provided object is not of type *v0alpha1.ResourcePermission (name=%s, namespace=%s, kind=%s)",
+		return fmt.Errorf("provided object is not of type *v0alpha1.CoreRole (name=%s, namespace=%s, kind=%s)",
 			rNew.GetStaticMetadata().Name, rNew.GetStaticMetadata().Namespace, rNew.GetStaticMetadata().Kind)
 	}
 
@@ -55,13 +56,13 @@ func (s *ResourcePermissionWatcher) Update(ctx context.Context, rOld resource.Ob
 	return nil
 }
 
-// Delete handles delete events for v0alpha1.ResourcePermission resources.
-func (s *ResourcePermissionWatcher) Delete(ctx context.Context, rObj resource.Object) error {
+// Delete handles delete events for v0alpha1.CoreRole resources.
+func (s *CoreRoleWatcher) Delete(ctx context.Context, rObj resource.Object) error {
 	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-delete")
 	defer span.End()
-	object, ok := rObj.(*v0alpha1.ResourcePermission)
+	object, ok := rObj.(*v0alpha1.CoreRole)
 	if !ok {
-		return fmt.Errorf("provided object is not of type *v0alpha1.ResourcePermission (name=%s, namespace=%s, kind=%s)",
+		return fmt.Errorf("provided object is not of type *v0alpha1.CoreRole (name=%s, namespace=%s, kind=%s)",
 			rObj.GetStaticMetadata().Name, rObj.GetStaticMetadata().Namespace, rObj.GetStaticMetadata().Kind)
 	}
 
@@ -72,12 +73,12 @@ func (s *ResourcePermissionWatcher) Delete(ctx context.Context, rObj resource.Ob
 
 // Sync is not a standard resource.Watcher function, but is used when wrapping this watcher in an operator.OpinionatedWatcher.
 // It handles resources which MAY have been updated during an outage period where the watcher was not able to consume events.
-func (s *ResourcePermissionWatcher) Sync(ctx context.Context, rObj resource.Object) error {
+func (s *CoreRoleWatcher) Sync(ctx context.Context, rObj resource.Object) error {
 	ctx, span := otel.GetTracerProvider().Tracer("watcher").Start(ctx, "watcher-sync")
 	defer span.End()
-	object, ok := rObj.(*v0alpha1.ResourcePermission)
+	object, ok := rObj.(*v0alpha1.CoreRole)
 	if !ok {
-		return fmt.Errorf("provided object is not of type *v0alpha1.ResourcePermission (name=%s, namespace=%s, kind=%s)",
+		return fmt.Errorf("provided object is not of type *v0alpha1.CoreRole (name=%s, namespace=%s, kind=%s)",
 			rObj.GetStaticMetadata().Name, rObj.GetStaticMetadata().Namespace, rObj.GetStaticMetadata().Kind)
 	}
 
