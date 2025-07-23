cdf877e848f1c10d484b779890d0d76c3717d178
samsch <git@samsch.org>
2025-07-23 16:23:25 -0400

    add some function descriptions

--- packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/mapper.ts
+++ packages/grafana-ui/src/components/DateTimePickers/TimeRangePicker/mapper.ts
@@ -3,10 +3,16 @@ import { TimeOption, TimeRange, TimeZone, rangeUtil, dateTimeFormat } from '@gra
 import { getFeatureToggle } from '../../../utils/featureToggle';
 import { commonFormat } from '../commonFormat';
 
+/**
+ * Takes a printable TimeOption and builds a TimeRange with DateTime properties from it
+ */
 export const mapOptionToTimeRange = (option: TimeOption, timeZone?: TimeZone): TimeRange => {
   return rangeUtil.convertRawToRange({ from: option.from, to: option.to }, timeZone, undefined, commonFormat);
 };
 
+/**
+ * Takes a TimeRange and makes a printable TimeOption with formatted date strings correct for the timezone from it
+ */
 export const mapRangeToTimeOption = (range: TimeRange, timeZone?: TimeZone): TimeOption => {
   const from = dateTimeFormat(range.from, { timeZone, format: commonFormat });
   const to = dateTimeFormat(range.to, { timeZone, format: commonFormat });
