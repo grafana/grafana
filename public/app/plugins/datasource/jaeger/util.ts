0839b0bc7877d1f55cfb605d9371aa7a5d255e8d
Gareth Dawson <gwdawson.work@gmail.com>
2025-07-24 12:37:54 +0100

    remove convertTagsLogfmt

--- public/app/plugins/datasource/jaeger/util.ts
+++ public/app/plugins/datasource/jaeger/util.ts
@@ -1,19 +1,5 @@
 import logfmt from 'logfmt';
 
-export function convertTagsLogfmt(tags: string | undefined) {
-  if (!tags) {
-    return '';
-  }
-  const data = logfmt.parse(tags);
-  Object.keys(data).forEach((key) => {
-    const value = data[key];
-    if (typeof value !== 'string') {
-      data[key] = String(value);
-    }
-  });
-  return JSON.stringify(data);
-}
-
 export function transformToLogfmt(tags: string | undefined) {
   if (!tags) {
     return '';
