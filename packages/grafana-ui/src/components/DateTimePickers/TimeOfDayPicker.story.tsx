2be4e44a029dbbe8906252f6f4b5b4e60a9c874c
Ashley Harrison <ashley.harrison@grafana.com>
2025-07-24 16:32:02 +0100

    Accessibility: Add storybook a11y tests that run in CI (#108543)
    * add test runner and start experimenting with storybook a11y tests
    
    * skip currently failing a11y tests
    
    * first attempt at github action
    
    * fix CODEOWNERS, fix copy assets
    
    * no caching?
    
    * make wait-on better
    
    * increase timeout and add verbose logging
    
    * kick CI
    
    * remove verbose logging, try different runner
    
    * remove commented out code in copyAssets
    
    * try maxWorkers 2
    
    * turn on verbose logging, run as part of same step
    
    * test storybook verify
    
    * try setting LANG
    
    * tidy up
    
    * add betterer test

--- packages/grafana-ui/src/components/DateTimePickers/TimeOfDayPicker.story.tsx
+++ packages/grafana-ui/src/components/DateTimePickers/TimeOfDayPicker.story.tsx
@@ -13,6 +13,8 @@ const meta: Meta<typeof TimeOfDayPicker> = {
     controls: {
       exclude: ['onChange'],
     },
+    // TODO fix a11y issue in story and remove this
+    a11y: { test: 'off' },
   },
   args: {
     value: dateTime(Date.now()),
