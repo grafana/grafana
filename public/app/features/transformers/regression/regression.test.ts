4d9705cb041df67c4c1c467b9b522019ed863641
Kristina <kristina.durivage@grafana.com>
2025-07-28 14:36:33 -0500

    [release-12.1.1] Transformations: Rename Regression Analysis to Trendline' (#108815)
    Transformations: Rename Regression Analysis to Trendline (#108631)
    
    * Rename regression analysis transformation
    
    * fix a couple translations
    
    * remove extra word
    
    * Fix tests
    
    * Change frame name to use regression
    
    (cherry picked from commit ffb8f4ea0c0eed3cb483edc0973388732d2fc2aa)

--- public/app/features/transformers/regression/regression.test.ts
+++ public/app/features/transformers/regression/regression.test.ts
@@ -10,7 +10,7 @@ import {
 
 import { ModelType, RegressionTransformer, RegressionTransformerOptions } from './regression';
 
-describe('Regression transformation', () => {
+describe('Trendline transformation', () => {
   it('it should predict a linear regression to exactly fit the data when the data is f(x) = x', () => {
     const source = [
       toDataFrame({
@@ -45,7 +45,7 @@ describe('Regression transformation', () => {
           name: 'Linear regression',
           fields: [
             { name: 'time', type: FieldType.time, values: [0, 1, 2, 3, 4, 5], config: {} },
-            { name: 'value predicted', type: FieldType.number, values: [0, 1, 2, 3, 4, 5], config: {} },
+            { name: 'value', type: FieldType.number, values: [0, 1, 2, 3, 4, 5], config: {} },
           ],
           length: 6,
         }),
@@ -86,7 +86,7 @@ describe('Regression transformation', () => {
           name: 'Linear regression',
           fields: [
             { name: 'time', type: FieldType.time, values: [0, 1, 2, 3, 4, 5], config: {} },
-            { name: 'value predicted', type: FieldType.number, values: [1, 1, 1, 1, 1, 1], config: {} },
+            { name: 'value', type: FieldType.number, values: [1, 1, 1, 1, 1, 1], config: {} },
           ],
           length: 6,
         }),
