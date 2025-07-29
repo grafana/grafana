a869e7d90f2a5c8303de654ba5c62b47b45fbd08
Levente Balogh <balogh.levente.hu@gmail.com>
2025-07-29 09:17:11 +0200

    feat(PluginContext): extend PluginContext with parent meta info

--- packages/grafana-data/src/context/plugins/PluginContext.tsx
+++ packages/grafana-data/src/context/plugins/PluginContext.tsx
@@ -5,6 +5,10 @@ import { DataSourceInstanceSettings } from '../../types/datasource';
 import { PluginMeta } from '../../types/plugin';
 
 export interface PluginContextType<T extends KeyValue = KeyValue> {
+  // In certain cases it's possible that plugins are nested into each other.
+  // Example: an app plugin using scenes which is rendering panel plugins for visualisations.
+  // In those cases the `parent` property would hold the plugin meta of the original plugin.
+  parent: PluginContextType | null;
   meta: PluginMeta<T>;
 }
 
