2b060c168b637f2ada5ee1248f2ef36c89ee2f41
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2025-07-29 17:47:33 -0300

    update generated code

--- apps/shorturl/pkg/apis/shorturl/v1alpha1/shorturl_spec_gen.go
+++ apps/shorturl/pkg/apis/shorturl/v1alpha1/shorturl_spec_gen.go
@@ -4,10 +4,14 @@ package v1alpha1
 
 // +k8s:openapi-gen=true
 type ShortURLSpec struct {
-	Path       string `json:"path"`
-	Uid        string `json:"uid"`
-	LastSeenAt int64  `json:"lastSeenAt"`
-	ShortURL   string `json:"shortURL"`
+	// The original path to where the short url is linking too e.g. https://localhost:3000/eer8i1kictngga/new-dashboard-with-lib-panel
+	Path string `json:"path"`
+	// The random string that is used as part of the generated short URL. e.g. l356YhwHg
+	Uid string `json:"uid"`
+	// The last time the short URL was used, 0 is the initial value
+	LastSeenAt int64 `json:"lastSeenAt"`
+	// The actual short URL that is generated e.g. https://localhost:3000/goto/l356YhwHg?orgId=1
+	ShortURL string `json:"shortURL"`
 }
 
 // NewShortURLSpec creates a new ShortURLSpec object.
