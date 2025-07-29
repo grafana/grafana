54b7b4f32dd6888c3c7511d8e0bbf7efddf03398
Kevin Minehart <5140827+kminehart@users.noreply.github.com>
2025-07-29 08:36:07 -0500

    Use playwright in daggerbuild

--- pkg/build/daggerbuild/e2e/validate_package.go
+++ pkg/build/daggerbuild/e2e/validate_package.go
@@ -1,32 +1,45 @@
 package e2e
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	"dagger.io/dagger"
 	"github.com/grafana/grafana/pkg/build/daggerbuild/frontend"
 )
 
-func CypressImage(version string) string {
-	return "cypress/included:13.1.0"
+// Playwright Container returns a docker container with everything set up that is needed to build or run e2e tests.
+// It reads the package.json from `src` to determine the playwright version, which forces a dag evaluation.
+func PlaywrightContainer(ctx context.Context, d *dagger.Client, src *dagger.Directory) (*dagger.Container, error) {
+	playwrightVersion, err := d.Container().From("alpine").
+		WithMountedFile("/src/package.json", src.File("package.json")).
+		WithEntrypoint([]string{}).
+		WithExec([]string{"apk", "add", "jq"}).
+		WithExec([]string{"/bin/sh", "-c", `jq -r '.devDependencies["@playwright/test"]' /src/package.json`}).
+		Stdout(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	image := fmt.Sprintf("mcr.microsoft.com/playwright:v%s", strings.TrimSpace(playwrightVersion))
+	container := d.Container().From(image).WithEntrypoint([]string{})
+	return container, nil
 }
 
-// CypressContainer returns a docker container with everything set up that is needed to build or run e2e tests.
-func CypressContainer(d *dagger.Client, base string) *dagger.Container {
-	container := d.Container().From(base).WithEntrypoint([]string{})
-
-	return container
-}
-
-func ValidatePackage(d *dagger.Client, service *dagger.Service, src *dagger.Directory, yarnCacheVolume *dagger.CacheVolume, nodeVersion string) *dagger.Container {
+func ValidatePackage(ctx context.Context, d *dagger.Client, service *dagger.Service, src *dagger.Directory, yarnCacheVolume *dagger.CacheVolume, nodeVersion string) (*dagger.Container, error) {
 	// The cypress container should never be cached
-	c := CypressContainer(d, CypressImage(nodeVersion))
-
+	c, err := PlaywrightContainer(ctx, d, src)
+	if err != nil {
+		return nil, err
+	}
 	c = frontend.WithYarnCache(c, yarnCacheVolume)
 
 	return c.WithDirectory("/src", src).
 		WithWorkdir("/src").
-		WithServiceBinding("grafana", service).
-		WithEnvVariable("HOST", "grafana").
-		WithEnvVariable("PORT", "3000").
 		WithExec([]string{"yarn", "install", "--immutable"}).
-		WithExec([]string{"/bin/sh", "-c", "/src/e2e/verify-release"})
+		WithServiceBinding("grafana", service).
+		WithEnvVariable("GRAFANA_URL", "http://grafana:3000").
+		WithExec([]string{"yarn", "e2e:acceptance"}), nil
 }
