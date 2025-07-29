7a1f972a9c380b54b18526fa5d50a59af73d1bf6
Levente Balogh <balogh.levente.hu@gmail.com>
2025-07-29 10:00:38 +0200

    feat(plugins): add a way to expose core apis only to certain plugins

--- packages/grafana-runtime/src/config.ts
+++ packages/grafana-runtime/src/config.ts
@@ -206,6 +206,8 @@ export class GrafanaBootConfig implements GrafanaConfig {
   exploreDefaultTimeOffset = '1h';
   exploreHideLogsDownload: boolean | undefined;
   quickRanges?: TimeOption[];
+  pluginRestrictedAPIsWhitelist?: Record<string, string[]>;
+  pluginRestrictedAPIsBlacklist?: Record<string, string[]>;
 
   /**
    * Language used in Grafana's UI. This is after the user's preference (or deteceted locale) is resolved to one of
--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1866,6 +1866,15 @@ var (
 			Owner:             grafanaAlertingSquad,
 			HideFromAdminPage: true,
 			HideFromDocs:      true,
+		},
+		{
+			Name:              "restrictedPluginApis",
+			Description:       "Enables sharing a list of APIs with a list of plugins",
+			Stage:             FeatureStageExperimental,
+			Owner:             grafanaAppPlatformSquad,
+			HideFromAdminPage: true,
+			HideFromDocs:      true,
+			FrontendOnly:      true,
 			Expression:        "false",
 		},
 	}
