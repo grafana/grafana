66765adfdc3f855c89646ccb979c87a55dc5acfd
joshhunt <josh.hunt@grafana.com>
2025-07-30 15:31:14 +0100

    more tidy

--- devenv/frontend-service/build-grafana.sh
+++ devenv/frontend-service/build-grafana.sh
@@ -2,21 +2,6 @@
 
 cd ../../
 
-if [[ -n "$USE_DOCKER_BUILDER" ]]; then
-  echo "Using Docker builder for Grafana build"
-
-  docker run --rm --name grafana-go-builder \
-  -v $(pwd):/src \
-  -e HOST_UID=$(id -u) \
-  -e HOST_GID=$(id -g) \
-  -v go-cache:/go-cache \
-  -v go-mod-cache:/go-mod-cache \
-  --entrypoint /bin/sh \
-  grafana-fs-builder \
-  -c "cd /src/devenv/frontend-service; ./build-grafana.sh"
-  exit $?
-fi
-
 echo "Go mod cache: $(go env GOMODCACHE), $(ls -1 $(go env GOMODCACHE) | wc -l) items"
 echo "Go build cache: $(go env GOCACHE), $(ls -1 $(go env GOCACHE) | wc -l) items"
 
@@ -42,8 +27,3 @@ go build -v \
   -ldflags "-X main.version=${VERSION}" \
   -gcflags "all=-N -l" \
   -o ./devenv/frontend-service/build/grafana ./pkg/cmd/grafana
-
-if [[ -n "$HOST_UID" && -n "$HOST_GID" ]]; then
-  echo "Setting ownership of grafana binary to $HOST_UID:$HOST_GID"
-  chown "$HOST_UID:$HOST_GID" ./devenv/frontend-service/build/grafana
-fi
