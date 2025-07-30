1708e8d5c18da9e33a610af3cb867ed7d5c3c008
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-07-30 17:42:41 -0600

    removing unused config

--- public/app/features/plugins/extensions/getCoreExtensionConfigurations.ts
+++ public/app/features/plugins/extensions/getCoreExtensionConfigurations.ts
@@ -1,7 +1,6 @@
 import { PluginExtensionAddedLinkConfig } from '@grafana/data';
-import { getDataSourceExtensionConfigs } from 'app/features/datasources/extensions/getDataSourceExtensionConfigs';
 import { getExploreExtensionConfigs } from 'app/features/explore/extensions/getExploreExtensionConfigs';
 
 export function getCoreExtensionConfigurations(): PluginExtensionAddedLinkConfig[] {
-  return [...getExploreExtensionConfigs(), ...getDataSourceExtensionConfigs()];
+  return [...getExploreExtensionConfigs()];
 }
