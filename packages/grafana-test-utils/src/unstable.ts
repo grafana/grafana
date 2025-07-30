0da3d5b1ffa5bab5f6f5944953b6e0d32af424a0
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-07-30 11:38:01 +0100

    Update tests and fixtures

--- packages/grafana-test-utils/src/unstable.ts
+++ packages/grafana-test-utils/src/unstable.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-export { MOCK_TEAMS } from './fixtures/teams';
-=======
 import { wellFormedTree } from './fixtures/folders';
 
 export const getFolderFixtures = wellFormedTree;
->>>>>>> d1491598e5b (Update test utils package with mock folder + search endpoints)
+export { MOCK_TEAMS } from './fixtures/teams';
