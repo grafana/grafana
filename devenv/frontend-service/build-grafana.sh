c7ba34522091f20254ed98001071c02510a16acc
joshhunt <josh.hunt@grafana.com>
2025-07-30 11:55:08 +0100

    tidy up

--- devenv/frontend-service/build-grafana.sh
+++ devenv/frontend-service/build-grafana.sh
@@ -20,17 +20,12 @@ fi
 echo "Go mod cache: $(go env GOMODCACHE), $(ls -1 $(go env GOMODCACHE) | wc -l) items"
 echo "Go build cache: $(go env GOCACHE), $(ls -1 $(go env GOCACHE) | wc -l) items"
 
-# Need to build version into the binary so plugin compatibility works correctly
-VERSION=$(jq -r .version package.json)
-
 # Set cross-compilation env vars only on macOS (Darwin)
 if [[ "$(uname)" == "Darwin" ]]; then
   echo "Setting up cross-compilation environment for macOS"
   export CGO_ENABLED=0
   export GOOS=linux
   export GOARCH=arm64
-  # export CC="zig cc -target aarch64-linux"
-  # export CXX="zig c++ -target aarch64-linux"
 fi
 
 if [[ -n "$USE_ZIG" ]]; then
@@ -39,6 +34,9 @@ if [[ -n "$USE_ZIG" ]]; then
   export CC="zig cc -target aarch64-linux"
   export CXX="zig c++ -target aarch64-linux"
 fi
+
+# Need to build version into the binary so plugin compatibility works correctly
+VERSION=$(jq -r .version package.json)
 
 go build -v \
   -ldflags "-X main.version=${VERSION}" \
