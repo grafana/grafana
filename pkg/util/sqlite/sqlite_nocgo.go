c36d70df13f1a0f43f8c03354f05df4f5feeb496
Serge Zaitsev <serge.zaitsev@grafana.com>
2025-07-31 10:53:23 +0200

    add a comment

--- pkg/util/sqlite/sqlite_nocgo.go
+++ pkg/util/sqlite/sqlite_nocgo.go
@@ -4,6 +4,13 @@ package sqlite
 
 import "modernc.org/sqlite"
 
+//
+// FIXME (@zserge)
+//
+// This non-CGo "implementation" is merely a stub to make Grafana compile without CGo.
+// Any attempts to actually use this driver are likely to fail at runtime in the most brutal ways.
+//
+
 type Driver = sqlite.Driver
 
 func IsBusyOrLocked(err error) bool {
