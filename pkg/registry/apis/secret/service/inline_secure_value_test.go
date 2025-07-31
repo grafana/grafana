9a0f28e743b5a1acb983b9aac1ed5600d9fb14e7
Matheus Macabu <macabu@users.noreply.github.com>
2025-07-31 16:42:40 +0200

    Secrets: Add inline secure value can reference method (#108965)

---
+++ pkg/registry/apis/secret/service/inline_secure_value_test.go
@@ -0,0 +1,294 @@
+package service_test
+
+import (
+	"testing"
+
+	common "github.com/grafana/grafana/pkg/apimachinery/apis/common/v0alpha1"
+	"github.com/grafana/grafana/pkg/apimachinery/identity"
+	"github.com/grafana/grafana/pkg/registry/apis/secret/service"
+	"github.com/grafana/grafana/pkg/registry/apis/secret/testutils"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/trace/noop"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestIntegration_InlineSecureValue_CanReference(t *testing.T) {
+	t.Parallel()
+
+	tracer := noop.NewTracerProvider().Tracer("test")
+
+	defaultNs := "org-1234"
+	owner := common.ObjectReference{
+		APIGroup:   "prometheus.datasource.grafana.app",
+		APIVersion: "v1alpha1",
+		Kind:       "DataSourceConfig",
+		Name:       "test-datasource",
+		Namespace:  defaultNs,
+	}
+
+	t.Run("happy path with owned and shared secure values", func(t *testing.T) {
+		t.Parallel()
+
+		tu := testutils.Setup(t)
+
+		sv1 := "test-secure-value-1"
+		createdSv1, err := tu.CreateSv(t.Context(), func(cfg *testutils.CreateSvConfig) {
+			cfg.Sv.Name = sv1
+			cfg.Sv.Namespace = defaultNs
+			cfg.Sv.OwnerReferences = []metav1.OwnerReference{owner.ToOwnerReference()}
+		})
+		require.NoError(t, err)
+		require.NotNil(t, createdSv1)
+
+		sv2 := "test-secure-value-2"
+		createdSv2, err := tu.CreateSv(t.Context(), func(cfg *testutils.CreateSvConfig) {
+			cfg.Sv.Name = sv2
+			cfg.Sv.Namespace = defaultNs
+		})
+		require.NoError(t, err)
+		require.NotNil(t, createdSv2)
+
+		values := common.InlineSecureValues{
+			"fieldA": {Name: sv1},
+			"fieldB": {Name: sv2},
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{
+			"securevalues:read": {"securevalues:uid:" + sv2},
+		})
+
+		svc := service.ProvideInlineSecureValueService(tracer, tu.SecureValueService, tu.AccessClient)
+
+		err = svc.CanReference(ctx, owner, values)
+		require.NoError(t, err)
+	})
+
+	t.Run("when the auth info is missing it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+		err := svc.CanReference(t.Context(), common.ObjectReference{}, common.InlineSecureValues{})
+		require.Error(t, err)
+	})
+
+	t.Run("when the owner namespace does not match auth info namespace it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		reqNs := "org-2345"
+		ctx := testutils.CreateUserAuthContext(t.Context(), reqNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, common.InlineSecureValues{})
+		require.Error(t, err)
+	})
+
+	t.Run("when the owner namespace is empty it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, common.ObjectReference{}, common.InlineSecureValues{})
+		require.Error(t, err)
+	})
+
+	t.Run("when the owner reference has empty fields it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		owner := common.ObjectReference{
+			Namespace: defaultNs,
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, common.InlineSecureValues{})
+		require.Error(t, err)
+
+		owner.APIGroup = "prometheus.datasource.grafana.app"
+		require.Error(t, svc.CanReference(ctx, owner, common.InlineSecureValues{}))
+		owner.APIGroup = ""
+
+		owner.APIVersion = "v1alpha1"
+		require.Error(t, svc.CanReference(ctx, owner, common.InlineSecureValues{}))
+		owner.APIVersion = ""
+
+		owner.Kind = "DataSourceConfig"
+		require.Error(t, svc.CanReference(ctx, owner, common.InlineSecureValues{}))
+		owner.Kind = ""
+
+		owner.Name = "test-datasource"
+		require.Error(t, svc.CanReference(ctx, owner, common.InlineSecureValues{}))
+		owner.Name = ""
+	})
+
+	t.Run("when no secure values are provided it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, common.InlineSecureValues{})
+		require.Error(t, err)
+	})
+
+	t.Run("when one of the secure values does not have a `name`, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		values := common.InlineSecureValues{
+			"fieldA": {Name: ""},
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+
+	t.Run("when one of the secure values has `create` field set, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		values := common.InlineSecureValues{
+			"fieldA": {
+				Name:   "test-sv",
+				Create: common.NewSecretValue("test"),
+			},
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+
+	t.Run("when one of the secure values has `remove` field set, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		svc := service.ProvideInlineSecureValueService(tracer, nil, nil)
+
+		values := common.InlineSecureValues{
+			"fieldA": {
+				Name:   "test-sv",
+				Remove: true,
+			},
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+
+	t.Run("when the secure value does not exist, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		tu := testutils.Setup(t)
+		svc := service.ProvideInlineSecureValueService(tracer, tu.SecureValueService, nil)
+
+		values := common.InlineSecureValues{
+			"fieldA": {Name: "non-existent-sv"},
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		err := svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+
+	t.Run("when the secure value is owned by a different resource, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		tu := testutils.Setup(t)
+
+		differentOwner := common.ObjectReference{
+			APIGroup:   "prometheus.datasource.grafana.app",
+			APIVersion: "v1alpha1",
+			Kind:       "DataSourceConfig",
+			Name:       "different-datasource",
+			Namespace:  defaultNs,
+		}
+
+		sv1 := "test-secure-value-1"
+		createdSv1, err := tu.CreateSv(t.Context(), func(cfg *testutils.CreateSvConfig) {
+			cfg.Sv.Name = sv1
+			cfg.Sv.Namespace = defaultNs
+			cfg.Sv.OwnerReferences = []metav1.OwnerReference{differentOwner.ToOwnerReference()}
+		})
+		require.NoError(t, err)
+		require.NotNil(t, createdSv1)
+
+		values := common.InlineSecureValues{
+			"fieldA": {Name: sv1},
+		}
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{})
+
+		svc := service.ProvideInlineSecureValueService(tracer, tu.SecureValueService, nil)
+
+		err = svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+
+	t.Run("when the request identity is not a user nor a service account, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		tu := testutils.Setup(t)
+
+		sv1 := "test-secure-value-1"
+		_, err := tu.CreateSv(t.Context(), func(cfg *testutils.CreateSvConfig) {
+			cfg.Sv.Name = sv1
+			cfg.Sv.Namespace = defaultNs
+		})
+		require.NoError(t, err)
+
+		values := common.InlineSecureValues{
+			"fieldA": {Name: sv1},
+		}
+
+		ctx := identity.WithServiceIdentityContext(t.Context(), 1234)
+
+		svc := service.ProvideInlineSecureValueService(tracer, tu.SecureValueService, nil)
+
+		err = svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+
+	t.Run("when the identity does not have permissions to read the secure value, it returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		tu := testutils.Setup(t)
+
+		sv1 := "test-secure-value-1"
+		_, err := tu.CreateSv(t.Context(), func(cfg *testutils.CreateSvConfig) {
+			cfg.Sv.Name = sv1
+			cfg.Sv.Namespace = defaultNs
+		})
+		require.NoError(t, err)
+
+		values := common.InlineSecureValues{
+			"fieldA": {Name: sv1},
+		}
+
+		svc := service.ProvideInlineSecureValueService(tracer, tu.SecureValueService, tu.AccessClient)
+
+		ctx := testutils.CreateUserAuthContext(t.Context(), defaultNs, map[string][]string{
+			"securevalues:read": {"securevalues:uid:another-sv"}, // can read, but another resource!
+		})
+
+		err = svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+
+		ctx = testutils.CreateUserAuthContext(t.Context(), defaultNs, nil)
+
+		err = svc.CanReference(ctx, owner, values)
+		require.Error(t, err)
+	})
+}
