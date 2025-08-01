ed1859de38cdb4f1c49137e8b368161624c37d6d
Gareth Dawson <gwdawson.work@gmail.com>
2025-08-01 12:46:00 +0100

    update error response

--- pkg/tsdb/jaeger/client.go
+++ pkg/tsdb/jaeger/client.go
@@ -117,7 +117,7 @@ func (j *JaegerClient) Operations(s string) ([]string, error) {
 func (j *JaegerClient) Search(query *JaegerQuery, start, end int64) ([]TraceResponse, error) {
 	u, err := url.JoinPath(j.url, "/api/traces")
 	if err != nil {
-		return []TraceResponse{}, fmt.Errorf("failed to parse Jaeger URL: %w", err)
+		return []TraceResponse{}, fmt.Errorf("failed to join url path: %w", err)
 	}
 
 	jaegerURL, err := url.Parse(u)
