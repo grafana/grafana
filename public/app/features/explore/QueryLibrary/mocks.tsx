0198581787e6de5d0f5dd85d20f8a24b63462c62
Juan Cabanas <juan.cabanas@grafana.com>
2025-08-01 17:52:58 -0300

    openDrawer modified

--- public/app/features/explore/QueryLibrary/mocks.tsx
+++ public/app/features/explore/QueryLibrary/mocks.tsx
@@ -13,11 +13,10 @@ export function QueryLibraryContextProviderMock(props: PropsWithChildren<Props>)
         openDrawer: jest.fn(),
         closeDrawer: jest.fn(),
         isDrawerOpen: false,
-        openAddQueryModal: jest.fn(),
-        closeAddQueryModal: jest.fn(),
         renderSaveQueryButton: jest.fn(),
         queryLibraryEnabled: Boolean(props.queryLibraryEnabled),
         context: 'explore',
+        setActiveQuery: jest.fn(),
       }}
     >
       {props.children}
