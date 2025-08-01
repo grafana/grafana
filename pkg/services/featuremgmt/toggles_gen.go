0bf1e17588a5eb5e275ad6837ce197eac7329366
ismail simsek <ismailsimsek09@gmail.com>
2025-08-01 09:55:37 +0200

    remove feature toggle prometheusCodeModeMetricNamesSearch

--- pkg/services/featuremgmt/toggles_gen.go
+++ pkg/services/featuremgmt/toggles_gen.go
@@ -299,10 +299,6 @@ const (
 	// If enabled, the caching backend gradually serializes query responses for the cache, comparing against the configured `[caching]max_value_mb` value as it goes. This can can help prevent Grafana from running out of memory while attempting to cache very large query responses.
 	FlagCachingOptimizeSerializationMemoryUsage = "cachingOptimizeSerializationMemoryUsage"
 
-	// FlagPrometheusCodeModeMetricNamesSearch
-	// Enables search for metric names in Code Mode, to improve performance when working with an enormous number of metric names
-	FlagPrometheusCodeModeMetricNamesSearch = "prometheusCodeModeMetricNamesSearch"
-
 	// FlagAddFieldFromCalculationStatFunctions
 	// Add cumulative and window functions to the add field from calculation transformation
 	FlagAddFieldFromCalculationStatFunctions = "addFieldFromCalculationStatFunctions"
