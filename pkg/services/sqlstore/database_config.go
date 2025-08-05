8494d107b275a48d71d7a7cb45cde5d16824e683
Serge Zaitsev <serge.zaitsev@grafana.com>
2025-08-05 13:54:17 +0200

    host might contain port already

--- pkg/services/sqlstore/database_config.go
+++ pkg/services/sqlstore/database_config.go
@@ -93,7 +93,10 @@ func (dbCfg *DatabaseConfig) readConfig(cfg *setting.Cfg) error {
 		dbCfg.UrlQueryParams = dbURL.Query()
 	} else {
 		dbCfg.Type = sec.Key("type").String()
-		dbCfg.Host = sec.Key("host").String() + ":" + sec.Key("port").String()
+		dbCfg.Host = sec.Key("host").String()
+		if port := sec.Key("port").String(); port != "" {
+			dbCfg.Host = dbCfg.Host + ":" + port
+		}
 		dbCfg.Name = sec.Key("name").String()
 		dbCfg.User = sec.Key("user").String()
 		dbCfg.ConnectionString = sec.Key("connection_string").String()
