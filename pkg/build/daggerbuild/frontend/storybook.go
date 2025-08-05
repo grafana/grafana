cc515827072139e01201d50f5c56c352a18e5427
jev forsberg <jev.forsberg@grafana.com>
2025-08-05 14:10:48 -0600

    Add test workflow for core dagger build and clean up NX variables
    - Add test-dagger-build.yml workflow to test core release build command
    - Remove all NX environment variables from frontend build functions
    - This isolates whether core dagger build works independently of E2E issues

--- pkg/build/daggerbuild/frontend/storybook.go
+++ pkg/build/daggerbuild/frontend/storybook.go
@@ -5,9 +5,6 @@ import "dagger.io/dagger"
 // Storybook returns a dagger.Directory which contains the built storybook server.
 func Storybook(builder *dagger.Container, src *dagger.Directory, version string) *dagger.Directory {
 	return builder.
-		WithEnvVariable("CI", "true").
-		WithEnvVariable("NX_DAEMON", "false").
-		WithEnvVariable("NX_CACHE_PROJECT_GRAPH", "false").
 		WithExec([]string{"yarn", "run", "storybook:build"}).
 		Directory("./packages/grafana-ui/dist/storybook")
 }
