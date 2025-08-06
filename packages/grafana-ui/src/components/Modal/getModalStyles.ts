fb9a0e355dbeeafba07f8e35b6ad2ab35725fcd5
Ashley Harrison <ashley.harrison@grafana.com>
2025-08-06 16:20:33 +0100

    Modal: Center modals at smaller screen heights (#109256)
    * centre and fill modals at smaller heights
    
    * fix datasourcemodal styling
    
    * ensure modal height remains similar to before

--- packages/grafana-ui/src/components/Modal/getModalStyles.ts
+++ packages/grafana-ui/src/components/Modal/getModalStyles.ts
@@ -25,6 +25,13 @@ export const getModalStyles = (theme: GrafanaTheme2) => {
       maxHeight: '80%',
       display: 'flex',
       flexDirection: 'column',
+      // Centre the modal vertically on smaller height screens
+      // this allows us to fill the full height for maximum usability
+      ['@media (max-height: 750px)']: {
+        maxHeight: '100%',
+        top: '50%',
+        transform: 'translateY(-50%)',
+      },
     }),
     modalBackdrop: css({
       position: 'fixed',
