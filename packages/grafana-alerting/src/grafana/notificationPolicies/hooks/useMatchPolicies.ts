ace3fea8c8ed0f75a49b59a13239e51e7e007ca4
Gilles De Mey <gilles.de.mey@gmail.com>
2025-08-06 14:14:00 +0200

    update matching hook

--- packages/grafana-alerting/src/grafana/notificationPolicies/hooks/useMatchPolicies.ts
+++ packages/grafana-alerting/src/grafana/notificationPolicies/hooks/useMatchPolicies.ts
@@ -1,16 +1,15 @@
-import { groupBy } from 'lodash';
 import { useCallback } from 'react';
 
-import { alertingAPI } from '../../api/v0alpha1/api.gen';
+import { RoutingTree, alertingAPI } from '../../api/v0alpha1/api.gen';
 import { Label } from '../../matchers/types';
 import { Route } from '../types';
-import { RouteMatchResult, RouteWithID, addUniqueIdentifier, computeInheritedTree, findMatchingRoutes } from '../utils';
+import { TreeMatch, matchAlertInstancesToPolicyTree } from '../utils';
 
-type TreeMatch = {
-  expandedTree: RouteWithID;
-  matchedPolicies: Map<string, Array<RouteMatchResult<RouteWithID>>>; // the route that matched the labels
-};
-type MatchResult = Map<string, TreeMatch>;
+export type MatchResult = Array<
+  {
+    treeMetadata: Pick<RoutingTree['metadata'], 'name'>;
+  } & TreeMatch
+>;
 
 /**
  * React hook that finds notification policy routes in all routing trees that match the provided set of alert instances.
@@ -36,51 +35,34 @@ export function useMatchAlertInstancesToNotificationPolicies() {
   const matchInstancesToPolicies = useCallback(
     (instances: Label[][]): MatchResult => {
       if (!data) {
-        return new Map();
+        return [];
       }
 
-      // the routing trees are returned as an array of items
+      // the routing trees are returned as an array of items because there can be several
       const trees = data.items;
 
-      // prepare the intial results, by default there won't be any matches for each tree
       const matchResult = trees.reduce<MatchResult>((acc, tree) => {
         const treeName = tree.metadata.name ?? 'unknown';
 
-        // compute the entire expanded tree for matching routes and diagnostics
-        // this will include inherited properties from parent nodes
-        // @TODO I don't like the type cast here :(
-        const expandedTree = addUniqueIdentifier(
-          computeInheritedTree({
-            ...(tree.spec.defaults as Route),
-            routes: tree.spec.routes as Route[],
-          })
-        );
+        // construct a pseudo-route from the route tree we get from the API
+        // @TODO make type stricter here and remove type assertions
+        const rootPolicy: Route = {
+          ...(tree.spec.defaults as Route),
+          routes: tree.spec.routes as Route[],
+        };
 
-        // initially empty map of matches policies
-        const matchedPolicies = new Map();
+        const { expandedTree, matchedPolicies } = matchAlertInstancesToPolicyTree(instances, rootPolicy);
 
-        acc.set(treeName, {
+        acc.push({
+          treeMetadata: {
+            name: treeName,
+          },
           expandedTree,
           matchedPolicies,
         });
 
         return acc;
-      }, new Map());
-
-      // for each tree, find matching routes for the provided instances
-      matchResult.forEach(({ expandedTree, matchedPolicies }) => {
-        // let's first find all matching routes for the provided instances
-        const matchesArray = instances.flatMap((labels) => findMatchingRoutes(expandedTree, labels));
-
-        // now group the matches by route ID
-        // this will give us a map of route IDs to their matching instances
-        // we use the route ID as the key to ensure uniqueness
-        // and to allow for easy lookup later
-        const groupedByRoute = groupBy(matchesArray, (match) => match.route.id);
-        Object.entries(groupedByRoute).forEach(([key, match]) => {
-          matchedPolicies.set(key, match);
-        });
-      });
+      }, []);
 
       return matchResult;
     },
