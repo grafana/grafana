9c6c07a2c97c4ae384f99bb6dd9b86692ec7053f
Alex Spencer <52186778+alexjonspencer1@users.noreply.github.com>
2025-08-06 14:06:35 -0700

    init: feature work

---
+++ public/app/features/expressions/components/QueryToolbox.tsx
@@ -0,0 +1,99 @@
+import { css } from '@emotion/css';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import { GrafanaTheme2 } from '@grafana/data';
+import { t } from '@grafana/i18n';
+import { IconButton, useStyles2, Stack, InlineToast } from '@grafana/ui';
+
+import { SqlExpressionQuery } from '../types';
+
+interface QueryToolboxProps {
+  onFormatCode?: () => void;
+  onExpand?: (isExpanded: boolean) => void;
+  isExpanded?: boolean;
+  query: SqlExpressionQuery;
+}
+
+const SHOW_SUCCESS_DURATION = 2 * 1000;
+
+export const QueryToolbox = ({ onFormatCode, onExpand, isExpanded, query }: QueryToolboxProps): JSX.Element => {
+  const styles = useStyles2(getStyles);
+
+  const [showCopySuccess, setShowCopySuccess] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (!showCopySuccess) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      setShowCopySuccess(false);
+    }, SHOW_SUCCESS_DURATION);
+
+    return () => clearTimeout(timeoutId);
+  }, [showCopySuccess]);
+
+  const copyTextCallback = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(query.expression ?? '');
+      setShowCopySuccess(true);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [query.expression]);
+
+  const copiedText = t('clipboard-button.inline-toast.success', 'Copied');
+
+  return (
+    <div className={styles.container}>
+      <Stack alignItems="center" direction="row" gap={1}>
+        {onFormatCode && (
+          <IconButton
+            onClick={onFormatCode}
+            name="brackets-curly"
+            size="xs"
+            tooltip={t('expressions.query-toolbox.tooltip-format-query', 'Format query')}
+          />
+        )}
+        {onExpand && (
+          <IconButton
+            onClick={() => onExpand(!isExpanded)}
+            name={isExpanded ? 'angle-double-up' : 'angle-double-down'}
+            size="xs"
+            tooltip={
+              isExpanded
+                ? t('expressions.query-toolbox.tooltip-collapse-editor', 'Collapse editor')
+                : t('expressions.query-toolbox.tooltip-expand-editor', 'Expand editor')
+            }
+          />
+        )}
+        {showCopySuccess && (
+          <InlineToast placement="top" referenceElement={buttonRef.current}>
+            {copiedText}
+          </InlineToast>
+        )}
+        <IconButton
+          name={showCopySuccess ? 'check' : 'copy'}
+          onClick={copyTextCallback}
+          ref={buttonRef}
+          size="xs"
+          tooltip={t('expressions.query-toolbox.tooltip-copy-query', 'Copy query')}
+          variant={showCopySuccess ? 'primary' : 'secondary'}
+        />
+      </Stack>
+    </div>
+  );
+};
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  container: css({
+    border: `1px solid ${theme.colors.border.medium}`,
+    borderTop: 'none',
+    padding: theme.spacing(1),
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'end',
+    fontSize: theme.typography.bodySmall.fontSize,
+  }),
+});
--- public/app/features/expressions/components/SqlExpr.tsx
+++ public/app/features/expressions/components/SqlExpr.tsx
@@ -1,14 +1,19 @@
 import { css } from '@emotion/css';
 import { useMemo, useRef, useEffect, useState } from 'react';
+import { useMeasure } from 'react-use';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 import { SelectableValue } from '@grafana/data';
+import { t } from '@grafana/i18n';
 import { SQLEditor, CompletionItemKind, LanguageDefinition, TableIdentifier } from '@grafana/plugin-ui';
 import { DataQuery } from '@grafana/schema/dist/esm/index';
-import { useStyles2 } from '@grafana/ui';
+import { formatSQL } from '@grafana/sql';
+import { Modal, useStyles2 } from '@grafana/ui';
 
 import { SqlExpressionQuery } from '../types';
 import { fetchSQLFields } from '../utils/metaSqlExpr';
 
+import { QueryToolbox } from './QueryToolbox';
 import { getSqlCompletionProvider } from './sqlCompletionProvider';
 
 // Account for Monaco editor's border to prevent clipping
@@ -38,6 +43,7 @@ export const SqlExpr = ({ onChange, refIds, query, alerting = false, queries }: 
   const EDITOR_LANGUAGE_DEFINITION: LanguageDefinition = {
     id: 'mysql',
     completionProvider,
+    formatter: formatSQL,
   };
 
   const initialQuery = `SELECT *
@@ -47,6 +53,8 @@ export const SqlExpr = ({ onChange, refIds, query, alerting = false, queries }: 
   const styles = useStyles2(getStyles);
   const containerRef = useRef<HTMLDivElement>(null);
   const [dimensions, setDimensions] = useState({ height: 0 });
+  const [toolboxRef, toolboxMeasure] = useMeasure<HTMLDivElement>();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const onEditorChange = (expression: string) => {
     onChange({
@@ -80,24 +88,67 @@ export const SqlExpr = ({ onChange, refIds, query, alerting = false, queries }: 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const renderToolbox = (formatQuery: () => void) => (
+    <div ref={toolboxRef}>
+      <QueryToolbox query={query} onFormatCode={formatQuery} onExpand={setIsExpanded} isExpanded={isExpanded} />
+    </div>
+  );
+
+  const renderSQLEditor = (width?: number, height?: number) => (
+    <SQLEditor
+      query={query.expression || initialQuery}
+      onChange={onEditorChange}
+      width={width}
+      height={height ?? dimensions.height - EDITOR_BORDER_ADJUSTMENT - toolboxMeasure.height}
+      language={EDITOR_LANGUAGE_DEFINITION}
+    >
+      {({ formatQuery }) => renderToolbox(formatQuery)}
+    </SQLEditor>
+  );
+
+  const renderStandaloneEditor = () => (
+    <AutoSizer>
+      {({ width, height }) =>
+        renderSQLEditor(width, height ? height - EDITOR_BORDER_ADJUSTMENT - toolboxMeasure.height : undefined)
+      }
+    </AutoSizer>
+  );
+
   return (
     <div ref={containerRef} className={styles.editorContainer}>
-      <SQLEditor
-        query={query.expression || initialQuery}
-        onChange={onEditorChange}
-        height={dimensions.height - EDITOR_BORDER_ADJUSTMENT}
-        language={EDITOR_LANGUAGE_DEFINITION}
-      />
+      {renderSQLEditor()}
+      {isExpanded && (
+        <Modal
+          title={t('expressions.sql-expr.modal-title', 'SQL Editor')}
+          closeOnBackdropClick={false}
+          closeOnEscape={false}
+          className={styles.modal}
+          contentClassName={styles.modalContent}
+          isOpen={isExpanded}
+          onDismiss={() => setIsExpanded(false)}
+        >
+          {renderStandaloneEditor()}
+        </Modal>
+      )}
     </div>
   );
 };
 
 const getStyles = () => ({
   editorContainer: css({
-    height: '240px',
+    height: '180px',
+    width: '100%',
     resize: 'vertical',
     overflow: 'auto',
     minHeight: '100px',
+  }),
+  modal: css({
+    width: '95vw',
+    height: '95vh',
+  }),
+  modalContent: css({
+    height: '100%',
+    paddingTop: 0,
   }),
 });
 
