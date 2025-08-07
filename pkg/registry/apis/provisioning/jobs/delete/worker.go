c77363ff4ce50ce960cda87b090b84fd4348b01a
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-08-07 07:44:54 +0200

    Fix missing ref in staging options

--- pkg/registry/apis/provisioning/jobs/delete/worker.go
+++ pkg/registry/apis/provisioning/jobs/delete/worker.go
@@ -70,6 +70,7 @@ func (w *Worker) Process(ctx context.Context, repo repository.Repository, job pr
 		CommitOnlyOnceMessage: msg,
 		PushOnWrites:          false,
 		Timeout:               10 * time.Minute,
+		Ref:                   opts.Ref,
 	}
 
 	err := w.wrapFn(ctx, repo, stageOptions, fn)
