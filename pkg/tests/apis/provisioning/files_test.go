25577751e5aaedc4ff983a8cdd73f9150c245cca
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-08-07 11:57:30 +0200

    Fix missing import

--- pkg/tests/apis/provisioning/files_test.go
+++ pkg/tests/apis/provisioning/files_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
