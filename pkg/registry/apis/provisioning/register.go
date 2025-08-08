d432ce15f082972faa72463ee5168e616a2d06cf
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-08-08 15:04:52 +0200

    Add HistoryJob back to Spec

--- pkg/registry/apis/provisioning/register.go
+++ pkg/registry/apis/provisioning/register.go
@@ -351,7 +351,8 @@ func (b *APIBuilder) GetAuthorizer() authorizer.Authorizer {
 				}
 				return authorizer.DecisionDeny, "viewer role is required", nil
 
-			case provisioning.JobResourceInfo.GetName():
+			case provisioning.JobResourceInfo.GetName(),
+				provisioning.HistoricJobResourceInfo.GetName():
 				// Jobs are shown on the configuration page.
 				if id.GetOrgRole().Includes(identity.RoleAdmin) {
 					return authorizer.DecisionAllow, "", nil
