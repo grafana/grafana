aa1a47ada0ec606098b17958898aec2d3ab38c92
Steven White <steven.white@weavegrid.com>
2025-08-08 07:24:52 -0700

    allow editing file-provisioned alerts

--- pkg/services/ngalert/store/provisioning_store.go
+++ pkg/services/ngalert/store/provisioning_store.go
@@ -23,43 +23,13 @@ func (pr provenanceRecord) TableName() string {
 
 // GetProvenance gets the provenance status for a provisionable object.
 func (st DBstore) GetProvenance(ctx context.Context, o models.Provisionable, org int64) (models.Provenance, error) {
-	recordType := o.ResourceType()
-	recordKey := o.ResourceID()
-
-	provenance := models.ProvenanceNone
-	err := st.SQLStore.WithDbSession(ctx, func(sess *db.Session) error {
-		filter := "record_key = ? AND record_type = ? AND org_id = ?"
-		var result models.Provenance
-		has, err := sess.Table(provenanceRecord{}).Where(filter, recordKey, recordType, org).Desc("id").Cols("provenance").Get(&result)
-		if err != nil {
-			return fmt.Errorf("failed to query for existing provenance status: %w", err)
-		}
-		if has {
-			provenance = result
-		}
-		return nil
-	})
-	if err != nil {
-		return models.ProvenanceNone, err
-	}
-	return provenance, nil
+	return models.ProvenanceNone, nil
 }
 
 // GetProvenance gets the provenance status for a provisionable object.
 func (st DBstore) GetProvenances(ctx context.Context, org int64, resourceType string) (map[string]models.Provenance, error) {
 	resultMap := make(map[string]models.Provenance)
-	err := st.SQLStore.WithDbSession(ctx, func(sess *db.Session) error {
-		filter := "record_type = ? AND org_id = ?"
-		rawData, err := sess.Table(provenanceRecord{}).Where(filter, resourceType, org).Desc("id").Cols("record_key", "provenance").QueryString()
-		if err != nil {
-			return fmt.Errorf("failed to query for existing provenance status: %w", err)
-		}
-		for _, data := range rawData {
-			resultMap[data["record_key"]] = models.Provenance(data["provenance"])
-		}
-		return nil
-	})
-	return resultMap, err
+	return resultMap, nil
 }
 
 // SetProvenance changes the provenance status for a provisionable object.
