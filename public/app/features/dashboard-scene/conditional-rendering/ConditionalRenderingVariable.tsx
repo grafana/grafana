80e8a6e2118020274e2379ac37ef381903725089
Bogdan Matei <bogdan.matei@grafana.com>
2025-08-12 16:23:19 +0300

    Dashboard: Improvements to conditional rendering (#108108)

--- public/app/features/dashboard-scene/conditional-rendering/ConditionalRenderingVariable.tsx
+++ public/app/features/dashboard-scene/conditional-rendering/ConditionalRenderingVariable.tsx
@@ -1,9 +1,12 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { t } from '@grafana/i18n';
 import { SceneComponentProps, sceneGraph, VariableDependencyConfig } from '@grafana/scenes';
-import { ConditionalRenderingVariableKind } from '@grafana/schema/dist/esm/schema/dashboard/v2';
-import { Box, Combobox, ComboboxOption, Input, Stack } from '@grafana/ui';
+import {
+  ConditionalRenderingVariableKind,
+  ConditionalRenderingVariableSpec,
+} from '@grafana/schema/dist/esm/schema/dashboard/v2';
+import { Box, Combobox, ComboboxOption, Field, Input, Stack } from '@grafana/ui';
 
 import { dashboardEditActions } from '../edit-pane/shared';
 
@@ -54,16 +57,29 @@ export class ConditionalRenderingVariable extends ConditionalRenderingBase<Condi
     const variable = sceneGraph.getVariables(this).getByName(this.state.value.name);
 
     if (!variable) {
-      return false;
+      return true;
     }
 
     const variableValue = variable.getValue() ?? '';
 
-    const hit = Array.isArray(variableValue)
-      ? variableValue.includes(this.state.value.value)
-      : variableValue === this.state.value.value;
+    let hit: boolean;
 
-    return this.state.value.operator === '!=' ? !hit : hit;
+    if (this.state.value.operator === '=' || this.state.value.operator === '!=') {
+      hit = Array.isArray(variableValue)
+        ? variableValue.includes(this.state.value.value.toString())
+        : variableValue === this.state.value.value.toString();
+    } else {
+      try {
+        const regex = new RegExp(this.state.value.value);
+        hit = Array.isArray(variableValue)
+          ? variableValue.some((currentVariableValue) => regex.test(currentVariableValue.toString()))
+          : regex.test(variableValue.toString());
+      } catch (err) {
+        return true;
+      }
+    }
+
+    return this.state.value.operator === '!=' || this.state.value.operator === '!~' ? !hit : hit;
   }
 
   public serialize(): ConditionalRenderingVariableKind {
@@ -71,7 +87,7 @@ export class ConditionalRenderingVariable extends ConditionalRenderingBase<Condi
       kind: 'ConditionalRenderingVariable',
       spec: {
         variable: this.state.value.name,
-        operator: this.state.value.operator === '=' ? 'equals' : 'notEquals',
+        operator: this._getLongOperator(this.state.value.operator),
         value: this.state.value.value,
       },
     };
@@ -81,7 +97,7 @@ export class ConditionalRenderingVariable extends ConditionalRenderingBase<Condi
     return new ConditionalRenderingVariable({
       value: {
         name: model.spec.variable,
-        operator: model.spec.operator === 'equals' ? '=' : '!=',
+        operator: ConditionalRenderingVariable._getShortOperator(model.spec.operator),
         value: model.spec.value,
       },
     });
@@ -90,14 +106,54 @@ export class ConditionalRenderingVariable extends ConditionalRenderingBase<Condi
   public static createEmpty(name: string): ConditionalRenderingVariable {
     return new ConditionalRenderingVariable({ value: { name, operator: '=', value: '' } });
   }
+
+  private _getLongOperator(operator: VariableConditionValueOperator): ConditionalRenderingVariableSpec['operator'] {
+    switch (operator) {
+      case '=':
+        return 'equals';
+
+      case '!=':
+        return 'notEquals';
+
+      case '=~':
+        return 'matches';
+
+      case '!~':
+        return 'notMatches';
+    }
+  }
+
+  private static _getShortOperator(
+    operator: ConditionalRenderingVariableSpec['operator']
+  ): VariableConditionValueOperator {
+    switch (operator) {
+      case 'equals':
+        return '=';
+
+      case 'notEquals':
+        return '!=';
+
+      case 'matches':
+        return '=~';
+
+      case 'notMatches':
+        return '!~';
+    }
+  }
 }
 
 function ConditionalRenderingVariableRenderer({ model }: SceneComponentProps<ConditionalRenderingVariable>) {
   const { value } = model.useState();
 
+  const [actualValue, setActualValue] = useState(value.value);
+
+  useEffect(() => {
+    setActualValue(value.value);
+  }, [value.value]);
+
   const variables = useMemo(() => sceneGraph.getVariables(model), [model]);
 
-  const variableNames: Array<ComboboxOption<string>> = useMemo(
+  const variableNames: ComboboxOption[] = useMemo(
     () => variables.state.variables.map((v) => ({ value: v.state.name, label: v.state.label ?? v.state.name })),
     [variables.state.variables]
   );
@@ -109,9 +165,30 @@ function ConditionalRenderingVariableRenderer({ model }: SceneComponentProps<Con
         value: '!=',
         description: t('dashboard.conditional-rendering.conditions.variable.operator.not-equals', 'Not equals'),
       },
+      {
+        value: '=~',
+        description: t('dashboard.conditional-rendering.conditions.variable.operator.matches', 'Matches'),
+      },
+      {
+        value: '!~',
+        description: t('dashboard.conditional-rendering.conditions.variable.operator.not-matches', 'Not matches'),
+      },
     ],
     []
   );
+
+  const valueError = useMemo(() => {
+    if (value.operator === '=~' || value.operator === '!~') {
+      try {
+        new RegExp(actualValue);
+        return '';
+      } catch (err) {
+        return t('dashboard.conditional-rendering.conditions.variable.error.invalid-regex', 'Invalid regex');
+      }
+    }
+
+    return '';
+  }, [actualValue, value.operator]);
 
   const undoText = t('dashboard.edit-actions.edit-template-variable-rule', 'Change template variable rule');
 
@@ -124,12 +201,14 @@ function ConditionalRenderingVariableRenderer({ model }: SceneComponentProps<Con
             options={variableNames}
             value={value.name}
             onChange={(option) => {
-              dashboardEditActions.edit({
-                description: undoText,
-                source: model,
-                perform: () => model.setStateAndNotify({ value: { ...value, name: option.value } }),
-                undo: () => model.setStateAndNotify({ value: { ...value, name: value.name } }),
-              });
+              if (option.value !== value.name) {
+                dashboardEditActions.edit({
+                  description: undoText,
+                  source: model,
+                  perform: () => model.setStateAndNotify({ value: { ...value, name: option.value } }),
+                  undo: () => model.setStateAndNotify({ value: { ...value, name: value.name } }),
+                });
+              }
             }}
           />
         </Box>
@@ -140,28 +219,38 @@ function ConditionalRenderingVariableRenderer({ model }: SceneComponentProps<Con
           options={operatorOptions}
           value={value.operator}
           onChange={(option) => {
-            dashboardEditActions.edit({
-              description: undoText,
-              source: model,
-              perform: () => model.setStateAndNotify({ value: { ...value, operator: option.value } }),
-              undo: () => model.setStateAndNotify({ value: { ...value, operator: value.operator } }),
-            });
+            if (option.value !== value.operator) {
+              dashboardEditActions.edit({
+                description: undoText,
+                source: model,
+                perform: () => model.setStateAndNotify({ value: { ...value, operator: option.value } }),
+                undo: () => model.setStateAndNotify({ value: { ...value, operator: value.operator } }),
+              });
+            }
           }}
         />
       </Stack>
-      <Input
-        placeholder={t('dashboard.conditional-rendering.conditions.variable.value', 'Value')}
-        value={value.value}
-        onChange={(e) => {
-          const eventValue = e.currentTarget.value;
-          dashboardEditActions.edit({
-            description: undoText,
-            source: model,
-            perform: () => model.setStateAndNotify({ value: { ...value, value: eventValue } }),
-            undo: () => model.setStateAndNotify({ value: { ...value, value: value.value } }),
-          });
-        }}
-      />
+      <Field error={valueError} invalid={!!valueError} noMargin>
+        <Input
+          placeholder={t('dashboard.conditional-rendering.conditions.variable.value', 'Value')}
+          value={actualValue}
+          onChange={(evt) => {
+            if (evt.currentTarget.value !== value.value) {
+              setActualValue(evt.currentTarget.value);
+            }
+          }}
+          onBlur={() => {
+            if (actualValue !== value.value) {
+              dashboardEditActions.edit({
+                description: undoText,
+                source: model,
+                perform: () => model.setStateAndNotify({ value: { ...value, value: actualValue } }),
+                undo: () => model.setStateAndNotify({ value: { ...value, value: value.value } }),
+              });
+            }
+          }}
+        />
+      </Field>
     </Stack>
   );
 }
