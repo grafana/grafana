c1f996def332e5e72bede2da1905cd63f409b004
Alex Spencer <52186778+alexjonspencer1@users.noreply.github.com>
2025-08-12 12:34:13 -0700

    SQL Expressions: Add SQL banner (#106865)
    * prototype: sql expressions discovery work
    
    * chore: remove compact view work
    
    * chore: cleanup
    
    * chore: moar clean
    
    * chore: use grid instead
    
    * chore: remove no margin + i18n fix
    
    * chore: betterer
    
    * cleanupz
    
    * chore: oh it needs noMargin
    
    * feat: convert to alert
    
    * chore: i18n extract
    
    * i18n
    
    * chore: polish
    
    * fix: change localstorage provider import
    
    * chore: update local storage key name
    
    ---------
    
    Co-authored-by: Develer <yeryomin.igor@gmail.com>

---
+++ public/app/features/dashboard/components/TransformationsEditor/SqlExpressions/SqlExpressionsBanner.tsx
@@ -0,0 +1,35 @@
+import { LocalStorageValueProvider } from '@grafana/data';
+import { Trans, t } from '@grafana/i18n';
+import { Alert, Stack, TextLink } from '@grafana/ui';
+
+const LOCAL_STORAGE_KEY = 'grafana.dashboard.sql-expressions-banner.dismissed';
+
+export const SqlExpressionsBanner = () => (
+  <LocalStorageValueProvider<boolean> storageKey={LOCAL_STORAGE_KEY} defaultValue={false}>
+    {(isDismissed, onDismiss) => {
+      if (isDismissed) {
+        return null;
+      }
+      return (
+        <Alert
+          bottomSpacing={1}
+          title={t('dashboard.transformation-picker-ng.sql-expressions-title', 'SQL Expressions')}
+          severity="info"
+          onRemove={() => onDismiss(true)}
+        >
+          <Stack direction="column" gap={1}>
+            <Trans i18nKey="dashboard.transformation-picker-ng.sql-expressions-message-description">
+              A new way to manipulate and transform the results of data source queries using MySQL-like syntax.
+            </Trans>
+            <TextLink
+              href="https://grafana.com/docs/grafana/latest/panels-visualizations/query-transform-data/sql-expressions/"
+              external
+            >
+              <Trans i18nKey="dashboard.transformation-picker-ng.sql-expressions-message-link">Learn more</Trans>
+            </TextLink>
+          </Stack>
+        </Alert>
+      );
+    }}
+  </LocalStorageValueProvider>
+);
