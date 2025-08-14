8a28b617c014de927d7308d3dc3a54677d1fee38
Ryan McKinley <ryantxu@gmail.com>
2025-08-14 11:51:16 +0300

    rename tests

--- pkg/apimachinery/apis/common/v0alpha1/secure_values_test.go
+++ pkg/apimachinery/apis/common/v0alpha1/secure_values_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 func TestSecureValues(t *testing.T) {
-	t.Run("redacted", func(t *testing.T) {
+	t.Run("redaction", func(t *testing.T) {
 		expected := "[REDACTED]"
 
 		rawValue := "a-password"
@@ -53,67 +53,69 @@ func TestSecureValues(t *testing.T) {
 		require.Panics(t, func() { esv.DangerouslyExposeAndConsumeValue() })
 	})
 
-	t.Run("IsZero", func(t *testing.T) {
-		require.True(t, common.InlineSecureValue{}.IsZero())
-		require.True(t, common.NewSecretValue("").IsZero())
-		require.True(t, common.InlineSecureValue{Remove: false}.IsZero())
+	t.Run("Inline", func(t *testing.T) {
+		t.Run("IsZero", func(t *testing.T) {
+			require.True(t, common.InlineSecureValue{}.IsZero())
+			require.True(t, common.NewSecretValue("").IsZero())
+			require.True(t, common.InlineSecureValue{Remove: false}.IsZero())
 
-		require.False(t, common.NewSecretValue("X").IsZero())
-		require.False(t, common.InlineSecureValue{Name: "X"}.IsZero())
-		require.False(t, common.InlineSecureValue{Remove: true}.IsZero())
-	})
+			require.False(t, common.NewSecretValue("X").IsZero())
+			require.False(t, common.InlineSecureValue{Name: "X"}.IsZero())
+			require.False(t, common.InlineSecureValue{Remove: true}.IsZero())
+		})
 
-	t.Run("Validate OneOf", func(t *testing.T) {
-		def := common.InlineSecureValue{}.OpenAPIDefinition()
-		// jj, _ := json.MarshalIndent(def.Schema, "", "  ")
-		// fmt.Printf("%s", string(jj))
+		t.Run("Validate OneOf", func(t *testing.T) {
+			def := common.InlineSecureValue{}.OpenAPIDefinition()
+			// jj, _ := json.MarshalIndent(def.Schema, "", "  ")
+			// fmt.Printf("%s", string(jj))
 
-		validator := validate.NewSchemaValidator(&def.Schema, nil, "", strfmt.Default)
+			validator := validate.NewSchemaValidator(&def.Schema, nil, "", strfmt.Default)
 
-		tests := []struct {
-			name  string
-			input map[string]any
-			valid bool
-			err   string
-		}{
-			{
-				name:  "with name",
-				input: map[string]any{"name": "x"},
-				valid: true,
-			},
-			{
-				name:  "with create",
-				input: map[string]any{"create": "x"},
-				valid: true,
-			},
-			{
-				name:  "with remove",
-				input: map[string]any{"remove": true},
-				valid: true,
-			},
-			{
-				name:  "empty",
-				input: map[string]any{},
-				valid: false,
-				err:   "must validate one and only one",
-			},
-			{
-				name:  "with unknown property",
-				input: map[string]any{"unknown": "property"},
-				valid: false,
-				err:   "unknown",
-			},
-		}
-		for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-				result := validator.Validate(tt.input)
-				require.Equal(t, tt.valid, result.IsValid())
-				if tt.err == "" {
-					require.NoError(t, result.AsError())
-				} else {
-					require.ErrorContains(t, result.AsError(), tt.err)
-				}
-			})
-		}
+			tests := []struct {
+				name  string
+				input map[string]any
+				valid bool
+				err   string
+			}{
+				{
+					name:  "with name",
+					input: map[string]any{"name": "x"},
+					valid: true,
+				},
+				{
+					name:  "with create",
+					input: map[string]any{"create": "x"},
+					valid: true,
+				},
+				{
+					name:  "with remove",
+					input: map[string]any{"remove": true},
+					valid: true,
+				},
+				{
+					name:  "empty",
+					input: map[string]any{},
+					valid: false,
+					err:   "must validate one and only one",
+				},
+				{
+					name:  "with unknown property",
+					input: map[string]any{"unknown": "property"},
+					valid: false,
+					err:   "unknown",
+				},
+			}
+			for _, tt := range tests {
+				t.Run(tt.name, func(t *testing.T) {
+					result := validator.Validate(tt.input)
+					require.Equal(t, tt.valid, result.IsValid())
+					if tt.err == "" {
+						require.NoError(t, result.AsError())
+					} else {
+						require.ErrorContains(t, result.AsError(), tt.err)
+					}
+				})
+			}
+		})
 	})
 }
