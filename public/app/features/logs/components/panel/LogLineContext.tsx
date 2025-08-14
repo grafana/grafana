03d8806401abe6c3a751a3842f8df64ba8ac0dc6
Matias Chomicki <matyax@gmail.com>
2025-08-14 16:30:36 +0200

    LogLineContext: fix overflow and scroll

--- public/app/features/logs/components/panel/LogLineContext.tsx
+++ public/app/features/logs/components/panel/LogLineContext.tsx
@@ -433,8 +433,8 @@ const getStyles = (theme: GrafanaTheme2) => {
     wrapper: css({
       border: `1px solid ${theme.colors.border.weak}`,
       padding: theme.spacing(0, 1, 1, 0),
-      flex: 1,
-      height: '100%',
+      flex: '1 1 auto',
+      minHeight: 0,
     }),
     logsContainer: css({
       height: '100%',
