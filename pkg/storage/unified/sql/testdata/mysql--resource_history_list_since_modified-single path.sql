c58193ec7be8351e689b59540e36d78ddaf8789b
Owen Smallwood <owen.smallwood@grafana.com>
2025-08-15 16:28:41 -0600

    adds test data for new query

---
+++ pkg/storage/unified/sql/testdata/mysql--resource_history_list_since_modified-single path.sql
@@ -0,0 +1,17 @@
+SELECT
+    `guid`,
+    `resource_version`,
+    `namespace`,
+    `group`,
+    `resource`,
+    `name`,
+    `folder`,
+    `value`,
+    `action`
+FROM resource_history
+WHERE `namespace` = 'ns'
+  AND `group` = 'group'
+  AND `resource` = 'res'
+  AND `resource_version` > 10000 -- needs to be exclusive of the sinceRv
+ORDER BY `resource_version`, `group`, `resource`, `name`
+  DESC
