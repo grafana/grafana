ffa34cce3225a202ee7ea43809df3bdad6457750
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-08-15 13:01:15 -0400

    lint

--- pkg/services/ngalert/store/models_test.go
+++ pkg/services/ngalert/store/models_test.go
@@ -215,7 +215,6 @@ func TestAlertRuleVersion_EqualSpec(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			if got := tt.a.EqualSpec(tt.b); got != tt.expect {
 				t.Errorf("EqualSpec() = %v, expect %v", got, tt.expect)
 			}
