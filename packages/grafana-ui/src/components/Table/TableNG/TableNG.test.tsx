ddd9a9a8379f12532250b76759787a9887e256a4
Paul Marbach <paul.marbach@grafana.com>
2025-08-18 15:48:33 -0400

    fix lint issue

--- packages/grafana-ui/src/components/Table/TableNG/TableNG.test.tsx
+++ packages/grafana-ui/src/components/Table/TableNG/TableNG.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import {
