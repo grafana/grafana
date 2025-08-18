a3fe6d0f435959ef42cfef74062c40d52754c997
Mariell Hoversholm <mariell@mardroemmar.dev>
2025-08-18 14:45:37 +0200

    Rendering: Remove SVG sanitization
    We don't need to sanitize SVGs: reports are inherently trusted, as they come from the same organisation, and the
    frontend itself should already sanitise any and all SVGs as-is.
    
    In the store service, this feature was never used, either, and as such, it is not useful there.

--- pkg/services/rendering/interface.go
+++ pkg/services/rendering/interface.go
@@ -132,5 +132,4 @@ type Service interface {
 	HasCapability(ctx context.Context, capability CapabilityName) (CapabilitySupportRequestResult, error)
 	IsCapabilitySupported(ctx context.Context, capability CapabilityName) error
 	CreateRenderingSession(ctx context.Context, authOpts AuthOpts, sessionOpts SessionOpts) (Session, error)
-	SanitizeSVG(ctx context.Context, req *SanitizeSVGRequest) (*SanitizeSVGResponse, error)
 }
