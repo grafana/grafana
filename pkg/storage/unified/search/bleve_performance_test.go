0035da1d7358e776a92d38fa73030236e69daff3
Peter Štibraný <pstibrany@gmail.com>
2025-08-18 15:37:30 +0200

    Close indexes created in temp dir.

--- pkg/storage/unified/search/bleve_performance_test.go
+++ pkg/storage/unified/search/bleve_performance_test.go
@@ -14,14 +14,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func setupIndex() (resource.ResourceIndex, string) {
+func setupIndex(b testing.TB) (resource.ResourceIndex, string) {
 	// size := 1000000  // TODO: 200k documents standard size?
 	size := 200000
 	// batchSize := 1000 slower 8s (for 200k documents) - 34s (for 1M documents)
 	// batchSize := 10000 // faster 5s  (for 200k documents) - 27s (for 1M documents)
 	batchSize := 100000 // fasterer 3.5s  (for 200k documents) - 27s  (for 1M documents)
 	writer := newTestWriter(size, batchSize)
-	return newTestDashboardsIndex(nil, 1, int64(size), int64(batchSize), writer)
+	return newTestDashboardsIndex(b, 1, int64(size), int64(batchSize), writer)
 }
 
 const maxAllowedTime = 20 * time.Millisecond // Reasonable (can vary per env) performance threshold per query (e.g., 20ms)
@@ -37,7 +37,7 @@ func BenchmarkBleveQuery(b *testing.B) {
 	var memStatsAfterIndex runtime.MemStats
 	runtime.ReadMemStats(&memStatsStart)
 
-	testIndex, testIndexDir := setupIndex()
+	testIndex, testIndexDir := setupIndex(b)
 	defer func() {
 		err := os.RemoveAll(testIndexDir)
 		if err != nil {
