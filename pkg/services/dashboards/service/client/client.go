6c89fbfae2adfbd5ac7f705e9f324d35140d052a
Igor Suleymanov <igor.suleymanov@grafana.com>
2025-08-19 15:47:31 +0300

    Fix listing and getting dashboard versions across different API versions
    What
    
    This commit updates dashboard version service to use API version aware
    API client. The service now also supports parsing different API version
    representation of dashboards.
    
    The API version aware client is also updated to support listing across
    versions.
    
    Why
    
    Currently listing or getting specific versions is broken for all v2
    versions of the dashboard API, especially if the dashboard being checked
    is still saved using v1 APIs.
    
    Signed-off-by: Igor Suleymanov <igor.suleymanov@grafana.com>

--- pkg/services/dashboards/service/client/client.go
+++ pkg/services/dashboards/service/client/client.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 
 	"go.opentelemetry.io/otel/attribute"
+	"golang.org/x/sync/errgroup"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -26,8 +27,22 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	// maxConcurrentGetRequests is the maximum number of concurrent Get requests to the K8s API.
+	// It is used to determine max amount of concurrent version fallback requests when handling a List request.
+	// TODO: do we need to make this configurable via config.ini?
+	maxConcurrentGetRequests = 10
+)
+
+// K8sHandlerWithFallback is a wrapper around the K8sHandler that provides a fallback to the stored version.
+type K8sHandlerWithFallback interface {
+	client.K8sHandler
+}
+
+// K8sClientFactory creates a K8sHandler for a given version.
 type K8sClientFactory func(ctx context.Context, version string) client.K8sHandler
 
+// K8sClientWithFallback is a wrapper around the K8sHandler that provides a fallback to the stored version.
 type K8sClientWithFallback struct {
 	client.K8sHandler
 
@@ -36,6 +51,24 @@ type K8sClientWithFallback struct {
 	log           log.Logger
 }
 
+// ProvideK8sClientWithFallback provides a K8sHandlerWithFallback.
+func ProvideK8sClientWithFallback(
+	cfg *setting.Cfg,
+	restConfigProvider apiserver.RestConfigProvider,
+	dashboardStore dashboards.Store,
+	userService user.Service,
+	resourceClient resource.ResourceClient,
+	featureToggles featuremgmt.FeatureToggles,
+	dualWriter dualwrite.Service,
+	sorter sort.Service,
+	reg prometheus.Registerer,
+) K8sHandlerWithFallback {
+	return NewK8sClientWithFallback(
+		cfg, restConfigProvider, dashboardStore, userService, resourceClient, sorter, dualWriter, reg, featureToggles,
+	)
+}
+
+// NewK8sClientWithFallback creates a new K8sClientWithFallback.
 func NewK8sClientWithFallback(
 	cfg *setting.Cfg,
 	restConfigProvider apiserver.RestConfigProvider,
@@ -56,8 +89,12 @@ func NewK8sClientWithFallback(
 	}
 }
 
-func (h *K8sClientWithFallback) Get(ctx context.Context, name string, orgID int64, options metav1.GetOptions, subresources ...string) (*unstructured.Unstructured, error) {
-	spanCtx, span := tracing.Start(ctx, "versionFallbackK8sHandler.Get")
+// Get gets a resource from the K8s API.
+// If the resource indicates that it was stored using a different version, the client will re-fetch it using the stored version.
+func (h *K8sClientWithFallback) Get(
+	ctx context.Context, name string, orgID int64, options metav1.GetOptions, subresources ...string,
+) (*unstructured.Unstructured, error) {
+	spanCtx, span := tracing.Start(ctx, "K8sClientWithFallback.Get")
 	defer span.End()
 
 	span.SetAttributes(
@@ -89,6 +126,144 @@ func (h *K8sClientWithFallback) Get(ctx context.Context, name string, orgID int6
 
 	span.AddEvent(fmt.Sprintf("%s Get", storedVersion))
 	return h.newClientFunc(spanCtx, storedVersion).Get(spanCtx, name, orgID, options, subresources...)
+}
+
+type nameAndResourceVersion struct {
+	Name            string
+	ResourceVersion string
+}
+
+// List lists resources from the K8s API.
+// It will check individual resources and re-fetch them if necessary.
+func (h *K8sClientWithFallback) List(
+	ctx context.Context, orgID int64, options metav1.ListOptions,
+) (*unstructured.UnstructuredList, error) {
+	ctx, span := tracing.Start(ctx, "K8sClientWithFallback.List")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.Int64("org.id", orgID),
+		attribute.Bool("fallback", false),
+	)
+
+	span.AddEvent("List")
+	initial, err := h.K8sHandler.List(ctx, orgID, options)
+	if err != nil {
+		h.log.Error("failed to fetch initial list", "error", err)
+		return nil, tracing.Error(span, err)
+	}
+
+	res := initial.DeepCopy()
+	res.Items = res.Items[:0]
+
+	// Map of version -> list of names to fetch.
+	toFetch := make(map[string][]nameAndResourceVersion)
+
+	for _, item := range initial.Items {
+		failed, storedVersion, conversionErr := getConversionStatus(&item)
+		if !failed {
+			res.Items = append(res.Items, item)
+			continue
+		}
+
+		h.log.Debug(
+			"will fetch object with the stored version",
+			"name", item.GetName(),
+			"storedVersion", storedVersion,
+			"conversionErr", conversionErr,
+		)
+		h.metrics.fallbackCounter.WithLabelValues(storedVersion).Inc()
+
+		names, ok := toFetch[storedVersion]
+		if !ok {
+			names = make([]nameAndResourceVersion, 0)
+		}
+
+		names = append(names, nameAndResourceVersion{
+			Name:            item.GetName(),
+			ResourceVersion: item.GetResourceVersion(),
+		})
+		toFetch[storedVersion] = names
+	}
+
+	for version, names := range toFetch {
+		h.log.Info(
+			"fetching multiple objects with the stored version",
+			"version", version,
+		)
+
+		// Log names at debug level, because there could be a lot of them.
+		h.log.Debug(
+			"will fetch objects with names",
+			"names", names,
+		)
+
+		items, err := h.fetchWithVersion(ctx, orgID, version, names...)
+		if err != nil {
+			return nil, tracing.Error(span, err)
+		}
+
+		res.Items = append(res.Items, items...)
+	}
+
+	return res, nil
+}
+
+// fetchWithVersion fetches multiple resources from the K8s API.
+// It uses concurrent Get requests, one for each name.
+//
+// TODO: maybe consider using List with a field selector and / or search?
+//
+//	items, err := client.List(spanCtx, orgID, metav1.ListOptions{
+//		FieldSelector: fmt.Sprintf("metadata.name in (%s)", strings.Join(names, ",")),
+//	})
+func (h *K8sClientWithFallback) fetchWithVersion(
+	ctx context.Context, orgID int64, version string, items ...nameAndResourceVersion,
+) ([]unstructured.Unstructured, error) {
+	ctx, span := tracing.Start(ctx, "K8sClientWithFallback.fetchWithVersion")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("version", version),
+		attribute.Int("count", len(items)),
+	)
+
+	client := h.newClientFunc(ctx, version)
+
+	g, ctx := errgroup.WithContext(ctx)
+	g.SetLimit(maxConcurrentGetRequests)
+	res := make([]unstructured.Unstructured, len(items))
+
+	for i, it := range items {
+		g.Go(func() error {
+			ctx, span := tracing.Start(ctx, "K8sClientWithFallback.fetchWithVersion.ConcurrentGet")
+			defer span.End()
+
+			span.SetAttributes(
+				attribute.String("name", it.Name),
+				attribute.String("resourceVersion", it.ResourceVersion),
+			)
+
+			item, err := client.Get(ctx, it.Name, orgID, metav1.GetOptions{
+				ResourceVersion: it.ResourceVersion,
+			})
+			if err != nil {
+				return tracing.Error(span, err)
+			}
+
+			// NB: it's important to set via the index,
+			// because `append`ing would create a race condition.
+			res[i] = *item
+
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return nil, tracing.Error(span, err)
+	}
+
+	return res, nil
 }
 
 func getConversionStatus(obj *unstructured.Unstructured) (failed bool, storedVersion string, conversionErr string) {
