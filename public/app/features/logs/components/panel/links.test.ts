1adc6c6f82f15d0d2c9ed10e5e709288a35cd8b7
Matias Chomicki <matyax@gmail.com>
2025-08-21 14:00:26 +0200

    Prettier

--- public/app/features/logs/components/panel/links.test.ts
+++ public/app/features/logs/components/panel/links.test.ts
@@ -1,8 +1,8 @@
-import { FieldType } from "@grafana/data";
+import { FieldType } from '@grafana/data';
 
-import { FieldDef } from "../logParser";
+import { FieldDef } from '../logParser';
 
-import { getTraceFromLinks } from "./links";
+import { getTraceFromLinks } from './links';
 
 describe('getTraceFromLinks', () => {
   let fields: FieldDef[];
@@ -11,7 +11,7 @@ describe('getTraceFromLinks', () => {
       {
         keys: ['test'],
         values: ['not a trace'],
-        fieldIndex: 0
+        fieldIndex: 0,
       },
       {
         keys: ['traceID'],
@@ -19,24 +19,24 @@ describe('getTraceFromLinks', () => {
         fieldIndex: 1,
         links: [
           {
-            href: "/explore?left=%7B%22range%22%3A%7B%22from%22%3A%22now-15m%22%2C%22to%22%3A%22now%22%7D%2C%22datasource%22%3A%22fetpfiwe8asqoe%22%2C%22queries%22%3A%5B%7B%22query%22%3A%22abcd1234%22%2C%22queryType%22%3A%22traceql%22%7D%5D%7D",
-            title: "tempo",
-            target: "_self",
+            href: '/explore?left=%7B%22range%22%3A%7B%22from%22%3A%22now-15m%22%2C%22to%22%3A%22now%22%7D%2C%22datasource%22%3A%22fetpfiwe8asqoe%22%2C%22queries%22%3A%5B%7B%22query%22%3A%22abcd1234%22%2C%22queryType%22%3A%22traceql%22%7D%5D%7D',
+            title: 'tempo',
+            target: '_self',
             origin: {
-              name: "traceID",
+              name: 'traceID',
               type: FieldType.string,
               config: {},
-              values: []
-            }
-          }
-        ]
-      }
-    ]
+              values: [],
+            },
+          },
+        ],
+      },
+    ];
   });
 
   test('Gets the trace information from a link', () => {
     expect(getTraceFromLinks(fields)).toEqual({
-      dsUID: "fetpfiwe8asqoe",
+      dsUID: 'fetpfiwe8asqoe',
       query: 'abcd1234',
       queryType: 'traceql',
     });
