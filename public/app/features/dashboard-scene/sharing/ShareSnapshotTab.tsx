4d5e7e61fb5d29793c4157754809378c3f4815a3
Haris Rozajac <haris.rozajac12@gmail.com>
2025-08-25 17:27:57 -0600

    fix test

--- public/app/features/dashboard-scene/sharing/ShareSnapshotTab.tsx
+++ public/app/features/dashboard-scene/sharing/ShareSnapshotTab.tsx
@@ -119,7 +119,9 @@ export class ShareSnapshotTab extends SceneObjectBase<ShareSnapshotTabState> imp
 
     let saveModel: Dashboard | DashboardV2SpecWithUid;
 
-    const isV2Dashboard = dashboardRef.resolve().serializer.apiVersion === 'v2beta1' || 'v2alpha1';
+    const isV2Dashboard =
+      dashboardRef.resolve().serializer.apiVersion === 'v2beta1' ||
+      dashboardRef.resolve().serializer.apiVersion === 'v2alpha1';
 
     if (isV2Dashboard) {
       saveModel = transformSceneToSaveModelSchemaV2(dashboardRef.resolve(), true);
