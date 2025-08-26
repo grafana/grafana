620e381573a7f92767b170d351a81f5a3d36a57c
Matias Chomicki <matyax@gmail.com>
2025-08-26 19:21:25 +0200

    Rename interactions key

--- public/app/features/logs/components/panel/analytics.ts
+++ public/app/features/logs/components/panel/analytics.ts
@@ -1,7 +1,7 @@
 import { reportInteraction } from '@grafana/runtime';
 
 export const reportInteractionOnce = (interactionName: string, properties?: Record<string, unknown>) => {
-  const key = `logs.log-list-context.events.${interactionName}`;
+  const key = `logs.interactions.${interactionName}`;
   if (sessionStorage.getItem(key)) {
     return;
   }
