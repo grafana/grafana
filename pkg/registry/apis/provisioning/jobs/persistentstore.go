d7e0a91c49c62882f96c4c36535bd1c41a0d4af9
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-08-27 15:49:24 +0200

    Move apifmt

--- pkg/registry/apis/provisioning/jobs/persistentstore.go
+++ pkg/registry/apis/provisioning/jobs/persistentstore.go
@@ -14,10 +14,10 @@ import (
 	"k8s.io/apimachinery/pkg/selection"
 
 	"github.com/grafana/grafana-app-sdk/logging"
+	"github.com/grafana/grafana/apps/provisioning/pkg/apifmt"
 	provisioning "github.com/grafana/grafana/apps/provisioning/pkg/apis/provisioning/v0alpha1"
 	client "github.com/grafana/grafana/apps/provisioning/pkg/generated/clientset/versioned/typed/provisioning/v0alpha1"
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
-	"github.com/grafana/grafana/pkg/registry/apis/provisioning/apifmt"
 )
 
 const (
