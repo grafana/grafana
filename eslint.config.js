d2d5f15bd6f4776af9f0c742656256b2ffbe3eed
Andreas Christou <andreas.christou@grafana.com>
2025-08-28 15:02:53 +0100

    Add lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -409,6 +409,7 @@ module.exports = [
       'public/app/plugins/datasource/grafana-postgresql-datasource/**/*.{ts,tsx}',
       'public/app/plugins/datasource/grafana-pyroscope-datasource/**/*.{ts,tsx}',
       'public/app/plugins/datasource/grafana-testdata-datasource/**/*.{ts,tsx}',
+      'public/app/plugins/datasource/graphite/**/*.{ts,tsx}',
       'public/app/plugins/datasource/jaeger/**/*.{ts,tsx}',
       'public/app/plugins/datasource/loki/**/*.{ts,tsx}',
       'public/app/plugins/datasource/loki/**/*.{ts,tsx}',
