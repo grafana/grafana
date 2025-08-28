d411f744220f261cc236204ff12e2fd94cfaa3ad
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-08-28 11:30:35 -0300

    fix typo

--- pkg/setting/setting_secrets_manager.go
+++ pkg/setting/setting_secrets_manager.go
@@ -50,7 +50,7 @@ func (cfg *Cfg) readSecretsManagerSettings() {
 	cfg.SecretsManagement.GCWorkerEnabled = secretsMgmt.Key("gc_worker_enabled").MustBool(true)
 	cfg.SecretsManagement.GCWorkerMaxBatchSize = uint16(secretsMgmt.Key("gc_worker_batch_size").MustUint(16))
 	cfg.SecretsManagement.GCWorkerMaxConcurrentCleanups = uint16(secretsMgmt.Key("gc_worker_max_concurrency").MustUint(16))
-	cfg.SecretsManagement.GCWorkerPollInterval = secretsMgmt.Key("gc_worker_batch_size").MustDuration(1 * time.Minute)
+	cfg.SecretsManagement.GCWorkerPollInterval = secretsMgmt.Key("gc_worker_poll_interval").MustDuration(1 * time.Minute)
 	cfg.SecretsManagement.GCWorkerPerSecureValueCleanupTimeout = secretsMgmt.Key("gc_worker_per_request_timeout").MustDuration(5 * time.Second)
 
 	// Extract available KMS providers from configuration sections
