7f6db769c5a98a3099b7aa836814bc1e7ed638c7
Matias Chomicki <matyax@gmail.com>
2025-08-29 15:50:56 +0200

    LogLineDetailsLog: check if the log contains ansi

--- public/app/features/logs/components/panel/LogLineDetailsLog.tsx
+++ public/app/features/logs/components/panel/LogLineDetailsLog.tsx
@@ -3,6 +3,8 @@ import { memo, useMemo } from 'react';
 
 import { useStyles2 } from '@grafana/ui';
 
+import { LogMessageAnsi } from '../LogMessageAnsi';
+
 import { HighlightedLogRenderer } from './HighlightedLogRenderer';
 import { getStyles } from './LogLine';
 import { LogListModel } from './processing';
@@ -21,15 +23,22 @@ export const LogLineDetailsLog = memo(({ log: originalLog, syntaxHighlighting }:
 
   return (
     <div className={styles.logLineWrapper}>
-      {!syntaxHighlighting ? (
-        <div className="field no-highlighting">{log.body}</div>
-      ) : (
-        <div className={logStyles.logLine}>
-          <div className={logStyles.wrappedLogLine}>
-            <div className="field log-syntax-highlight">{<HighlightedLogRenderer log={log} />}</div>
-          </div>
+      <div className={logStyles.logLine}>
+        <div className={logStyles.wrappedLogLine}>
+          {log.hasAnsi ? (
+            <span className="field no-highlighting">
+              <LogMessageAnsi value={log.body} />
+            </span>
+          ) : (
+            <>
+              {!syntaxHighlighting && <div className="field no-highlighting">{log.body}</div>}
+              {syntaxHighlighting && (
+                <div className="field log-syntax-highlight">{<HighlightedLogRenderer log={log} />}</div>
+              )}
+            </>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 });
