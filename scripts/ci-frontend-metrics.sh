3646e3c75134ba9f1c9f75dfb56cc2ebde655100
Ashley Harrison <ashley.harrison@grafana.com>
2025-08-29 09:24:22 +0100

    Chore: Publish frontend metrics from github actions (#110271)
    * remove build size from ci scripts, test adding a github action step
    
    * generate pa11y results file
    
    * setup node
    
    * don't need grabpl
    
    * get key from vault
    
    * doublequote env var
    
    * write node script for publishing
    
    * update CODEOWNERS
    
    * add some logging
    
    * yarn install...
    
    * tidy up
    
    * only on main branch

--- scripts/ci-frontend-metrics.sh
+++ scripts/ci-frontend-metrics.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-BUILD_FOLDER=$1
-if [ -z "$BUILD_FOLDER" ]; then
-  BUILD_FOLDER="./public/build"
-fi
-
 ERROR_COUNT="0"
 ACCESSIBILITY_ERRORS="$(grep -oP '\"errors\":(\d+),' pa11y-ci-results.json | grep -oP '\d+')"
 DIRECTIVES="$(grep -r -o  directive public/app/ | wc -l)"
@@ -16,15 +11,7 @@ CLASSNAME_PROP="$(grep -r -o -E --include="*.ts*" "\.*.className=\W.*\W.*" publi
 EMOTION_IMPORTS="$(grep -r -o -E --include="*.ts*" --exclude="*.test*" "\{.*css.*\} from '@emotion/css'" public/app | wc -l)"
 TS_FILES="$(find public/app -type f -name "*.ts*" -not -name "*.test*" | wc -l)"
 SCSS_FILES="$(find public packages -name '*.scss' | wc -l)"
-
-TOTAL_BUNDLE="$(du -sk "$BUILD_FOLDER" | cut -f1)"
 OUTDATED_DEPENDENCIES="$(yarn outdated --all | grep -oP '[[:digit:]]+ *(?= dependencies are out of date)')"
-## Disabled due to yarn PnP update breaking npm audit
-#VULNERABILITY_AUDIT="$(yarn npm audit --all --recursive --json)"
-#LOW_VULNERABILITIES="$(echo "${VULNERABILITY_AUDIT}" | grep -o -i '"severity":"low"' | wc -l)"
-#MED_VULNERABILITIES="$(echo "${VULNERABILITY_AUDIT}" | grep -o -i '"severity":"moderate"' | wc -l)"
-#HIGH_VULNERABILITIES="$(echo "${VULNERABILITY_AUDIT}" | grep -o -i '"severity":"high"' | wc -l)"
-#CRITICAL_VULNERABILITIES="$(echo "${VULNERABILITY_AUDIT}" | grep -o -i '"severity":"critical"' | wc -l)"
 
 echo -e "Typescript errors: $ERROR_COUNT"
 echo -e "Accessibility errors: $ACCESSIBILITY_ERRORS"
@@ -32,12 +19,7 @@ echo -e "Directives: $DIRECTIVES"
 echo -e "Controllers: $CONTROLLERS"
 echo -e "Legacy forms: $LEGACY_FORMS"
 echo -e "Barrel imports: $BARREL_IMPORTS"
-echo -e "Total bundle folder size: $TOTAL_BUNDLE"
 echo -e "Total outdated dependencies: $OUTDATED_DEPENDENCIES"
-echo -e "Low vulnerabilities: $LOW_VULNERABILITIES"
-echo -e "Med vulnerabilities: $MED_VULNERABILITIES"
-echo -e "High vulnerabilities: $HIGH_VULNERABILITIES"
-echo -e "Critical vulnerabilities: $CRITICAL_VULNERABILITIES"
 echo -e "ClassName in props: $CLASSNAME_PROP"
 echo -e "@emotion/css imports: $EMOTION_IMPORTS"
 echo -e "Total TS files: $TS_FILES"
@@ -73,7 +55,6 @@ echo "Metrics: {
   \"grafana.ci-code.directives\": \"${DIRECTIVES}\",
   \"grafana.ci-code.controllers\": \"${CONTROLLERS}\",
   \"grafana.ci-code.legacyForms\": \"${LEGACY_FORMS}\",
-  \"grafana.ci-code.bundleFolderSize\": \"${TOTAL_BUNDLE}\",
   \"grafana.ci-code.dependencies.outdated\": \"${OUTDATED_DEPENDENCIES}\",
   \"grafana.ci-code.props.className\": \"${CLASSNAME_PROP}\",
   \"grafana.ci-code.imports.emotion\": \"${EMOTION_IMPORTS}\",
