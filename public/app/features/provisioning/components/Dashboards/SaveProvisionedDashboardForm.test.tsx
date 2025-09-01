b9434e215440b04a170a6e39bb68e69d66599106
Clarity-89 <homes89@ukr.net>
2025-09-01 09:52:29 +0300

    tests

--- public/app/features/provisioning/components/Dashboards/SaveProvisionedDashboardForm.test.tsx
+++ public/app/features/provisioning/components/Dashboards/SaveProvisionedDashboardForm.test.tsx
@@ -240,7 +240,7 @@ describe('SaveProvisionedDashboardForm', () => {
 
     await waitFor(() => {
       expect(mockAction).toHaveBeenCalledWith({
-        ref: undefined,
+        ref: 'dashboard/2023-01-01-abcde',
         name: 'test-repo',
         path: 'test-dashboard.json',
         message: 'Initial commit',
@@ -351,7 +351,7 @@ describe('SaveProvisionedDashboardForm', () => {
 
     await waitFor(() => {
       expect(mockAction).toHaveBeenCalledWith({
-        ref: undefined,
+        ref: 'dashboard/2023-01-01-abcde',
         name: 'test-repo',
         path: 'error-dashboard.json',
         message: 'Error commit',
