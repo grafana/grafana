a062d547a5109c8bbd642acef4c4415abe9be52d
Clarity-89 <homes89@ukr.net>
2025-09-02 14:31:56 +0300

    Update NavLandingPageCard.tsx

--- public/app/core/components/NavLandingPage/NavLandingPageCard.tsx
+++ public/app/core/components/NavLandingPage/NavLandingPageCard.tsx
@@ -21,7 +21,6 @@ export function NavLandingPageCard({ description, text, url }: Props) {
 
 const getStyles = (theme: GrafanaTheme2) => ({
   card: css({
-    marginBottom: 0,
     gridTemplateRows: '1fr 0 2fr',
   }),
   // Limit descriptions to 3 lines max before ellipsing
