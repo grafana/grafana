a60c73ae68da41d8f8ba09db273748984f62511c
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-09-02 13:42:10 +0100

    Remove betterer undocumented stories check

--- .betterer.ts
+++ .betterer.ts
@@ -17,7 +17,6 @@ export default {
     countEslintErrors()
       .include('**/*.{ts,tsx}')
       .exclude(new RegExp(eslintPathsToIgnore.join('|'))),
-  'no undocumented stories': () => countUndocumentedStories().include('**/*.story.tsx'),
   'no skipping a11y tests in stories': () => countSkippedA11yTestStories().include('**/*.story.tsx'),
   'no gf-form usage': () =>
     regexp(/gf-form/gm, 'gf-form usage has been deprecated. Use a component from @grafana/ui or custom CSS instead.')
@@ -40,31 +39,6 @@ function countSkippedA11yTestStories() {
           const file = fileTestResult.addFile(filePath, '');
           // Add the issue to the first character of the file:
           file.addIssue(0, 0, 'No skipping of a11y tests in stories. Please fix the component or story instead.');
-        }
-      })
-    );
-  });
-}
-
-function countUndocumentedStories() {
-  return new BettererFileTest(async (filePaths, fileTestResult) => {
-    await Promise.all(
-      filePaths.map(async (filePath) => {
-        // look for .mdx import in the story file
-        const mdxImportRegex = new RegExp("^import.*\\.mdx';$", 'gm');
-        // Looks for the "autodocs" string in the file
-        const autodocsStringRegex = /autodocs/;
-
-        const fileText = await fs.readFile(filePath, 'utf8');
-
-        const hasMdxImport = mdxImportRegex.test(fileText);
-        const hasAutodocsString = autodocsStringRegex.test(fileText);
-        // If both .mdx import and autodocs string are missing, add an issue
-        if (!hasMdxImport && !hasAutodocsString) {
-          // In this case the file contents don't matter:
-          const file = fileTestResult.addFile(filePath, '');
-          // Add the issue to the first character of the file:
-          file.addIssue(0, 0, 'No undocumented stories are allowed, please add an .mdx file with some documentation');
         }
       })
     );
