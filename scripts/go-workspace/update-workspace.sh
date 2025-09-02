5745515b90451a4774701d5b75ac67db234f85ec
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-09-02 14:29:16 +0200

    Revert update workspace

--- scripts/go-workspace/update-workspace.sh
+++ scripts/go-workspace/update-workspace.sh
@@ -11,15 +11,14 @@ echo "running go work sync"
 go work sync
 popd
 
-for mod in $(GOWORK=off go run scripts/go-workspace/main.go list-submodules --path "${REPO_ROOT}/go.work"); do
-  pushd "${mod}"
-  echo "Running go mod tidy in ${mod}"
-  go mod tidy || true
-  popd
+for mod in $(go run scripts/go-workspace/main.go list-submodules --path "${REPO_ROOT}/go.work"); do
+    pushd "${mod}"
+    echo "Running go mod tidy in ${mod}"
+    go mod tidy || true
+    popd
 done
 
 pushd "${REPO_ROOT}"
 echo "running go mod download"
 go mod download
 popd
-
