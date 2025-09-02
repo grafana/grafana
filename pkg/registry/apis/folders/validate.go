d4246ece54538e6886b5416394dc95433e3a8017
Ryan McKinley <ryantxu@gmail.com>
2025-09-02 18:31:09 +0300

    name validation

--- pkg/registry/apis/folders/validate.go
+++ pkg/registry/apis/folders/validate.go
@@ -3,9 +3,11 @@ package folders
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	folders "github.com/grafana/grafana/apps/folder/pkg/apis/folder/v1beta1"
-	"github.com/grafana/grafana/pkg/apimachinery/utils" // TODO, will be moved into utils
+	"github.com/grafana/grafana/pkg/apimachinery/utils"
+	utils2 "github.com/grafana/grafana/pkg/registry/apis/preferences/utils" // TODO, will be moved into utils
 	"github.com/grafana/grafana/pkg/services/dashboards"
 	"github.com/grafana/grafana/pkg/services/folder"
 	"github.com/grafana/grafana/pkg/util"
@@ -26,6 +28,39 @@ func validateOnCreate(ctx context.Context, f *folders.Folder, getter parentsGett
 	meta, err := utils.MetaAccessor(f)
 	if err != nil {
 		return fmt.Errorf("unable to read metadata from object: %w", err)
+	}
+
+	name, ok := utils2.ParseOwnerFromName(id)
+	if ok {
+		if name.Owner == utils2.NamespaceResourceOwner {
+			return fmt.Errorf("folder may not be a namespace")
+		}
+		if meta.GetFolder() != "" {
+			return fmt.Errorf("%s folder must be a root", name.Owner)
+		}
+
+		// Make sure a team/root
+		refs := meta.GetOwnerReferences()
+		switch len(refs) {
+		case 0:
+			return fmt.Errorf("folder is missing owner reference (%s)", id)
+		case 1: // OK
+		default:
+			return fmt.Errorf("folder has multiple owner references (%s)", id)
+		}
+		ref := refs[0]
+		if ref.Name != name.Name {
+			return fmt.Errorf("owner reference must match the same name")
+		}
+		if strings.ToLower(ref.Kind) != string(name.Owner) {
+			return fmt.Errorf("owner reference kind must match the name")
+		}
+
+		// The title will be based on the team/user so we should not save a value in the folder
+		if f.Spec.Title != "" {
+			return fmt.Errorf("folder title must be empty when creating a %s folder", name.Owner)
+		}
+		return nil
 	}
 
 	if !util.IsValidShortUID(id) {
