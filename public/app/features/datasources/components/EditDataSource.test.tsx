6e21ac1945710f76b4ed4405554d493e873f3203
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-09-02 09:43:27 +0200

    lint fix

--- public/app/features/datasources/components/EditDataSource.test.tsx
+++ public/app/features/datasources/components/EditDataSource.test.tsx
@@ -2,12 +2,7 @@ import { screen, render } from '@testing-library/react';
 import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
-import {
-  DataSourceJsonData,
-  DataSourceSettings,
-  PluginExtensionDataSourceConfigContext,
-  PluginState,
-} from '@grafana/data';
+import { DataSourceJsonData, PluginExtensionDataSourceConfigContext, PluginState } from '@grafana/data';
 import { setPluginComponentsHook, setPluginLinksHook } from '@grafana/runtime';
 import { createComponentWithMeta } from 'app/features/plugins/extensions/usePluginComponents';
 import { configureStore } from 'app/store/configureStore';
