f67cb2734f6279317751b6c201b3b7087d475a85
Lauren Armstrong <lauren.armstrong@grafana.com>
2025-09-03 13:14:25 +0100

    move rule view enrichment tab to enterprise

---
+++ public/app/features/alerting/unified/enterprise-components/rule-view-page/extensions.ts
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { FeatureState, NavModelItem } from '@grafana/data';
+import { t } from '@grafana/i18n';
+import { FeatureBadge } from '@grafana/ui';
+
+type SetActiveTab = (tab: string) => void;
+
+type RuleViewTabBuilderArgs = {
+  activeTab: string;
+  setActiveTab: SetActiveTab;
+};
+
+type RuleViewTabBuilder = (args: RuleViewTabBuilderArgs) => NavModelItem;
+
+const ruleViewTabBuilders: RuleViewTabBuilder[] = [];
+
+export function registerRuleViewTab(builder: RuleViewTabBuilder) {
+  ruleViewTabBuilders.push(builder);
+}
+
+export function getRuleViewExtensionTabs(args: RuleViewTabBuilderArgs): NavModelItem[] {
+  return ruleViewTabBuilders.map((builder) => builder(args));
+}
+
+export function addEnrichmentSection() {
+  registerRuleViewTab(({ activeTab, setActiveTab }) => {
+    const tabId = 'enrichment';
+    return {
+      text: t('alerting.use-page-nav.page-nav.text.enrichment', 'Alert enrichment'),
+      active: activeTab === tabId,
+      onClick: () => setActiveTab(tabId),
+      tabSuffix: () =>
+        React.createElement(
+          'span',
+          { style: { marginLeft: 8 } },
+          React.createElement(FeatureBadge, { featureState: FeatureState.new })
+        ),
+    };
+  });
+}
