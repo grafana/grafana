30751faa9bec5bb8880bfa532450bb0ce37c8a69
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-09-03 08:30:48 +0100

    Make sort options a smaller list

--- packages/grafana-test-utils/src/handlers/api/search/constants.ts
+++ packages/grafana-test-utils/src/handlers/api/search/constants.ts
@@ -13,53 +13,5 @@ export const SORT_OPTIONS = {
       meta: '',
       name: 'alpha-desc',
     },
-    {
-      description: 'Sort results based on recent errors in descending order',
-      displayName: 'Errors 30 days (most)',
-      meta: 'errors',
-      name: 'errors-recently-desc',
-    },
-    {
-      description: 'Sort results based on recent errors in ascending order',
-      displayName: 'Errors 30 days (least)',
-      meta: 'errors',
-      name: 'errors-recently-asc',
-    },
-    {
-      description: 'Sort results based on errors in descending order',
-      displayName: 'Errors total (most)',
-      meta: 'errors',
-      name: 'errors-desc',
-    },
-    {
-      description: 'Sort results based on errors in ascending order',
-      displayName: 'Errors total (least)',
-      meta: 'errors',
-      name: 'errors-asc',
-    },
-    {
-      description: 'Sort results based on recent views in descending order',
-      displayName: 'Views 30 days (most)',
-      meta: 'views',
-      name: 'viewed-recently-desc',
-    },
-    {
-      description: 'Sort results based on recent views in ascending order',
-      displayName: 'Views 30 days (least)',
-      meta: 'views',
-      name: 'viewed-recently-asc',
-    },
-    {
-      description: 'Sort results based on views in descending order',
-      displayName: 'Views total (most)',
-      meta: 'views',
-      name: 'viewed-desc',
-    },
-    {
-      description: 'Sort results based on views in ascending order',
-      displayName: 'Views total (least)',
-      meta: 'views',
-      name: 'viewed-asc',
-    },
   ],
 };
