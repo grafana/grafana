ffffe9429250b5bdc761eb67917e06c298203629
Ryan McKinley <ryantxu@gmail.com>
2025-09-03 10:38:09 +0300

    remove deprecated endpoints

--- pkg/services/star/api/api.go
+++ pkg/services/star/api/api.go
@@ -3,7 +3,6 @@ package api
 import (
 	"context"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/grafana/grafana/pkg/api/response"
@@ -69,46 +68,6 @@ func (api *API) GetStars(c *contextmodel.ReqContext) response.Response {
 	return response.JSON(http.StatusOK, uids)
 }
 
-// swagger:route POST /user/stars/dashboard/{dashboard_id} signed_in_user starDashboard
-//
-// Star a dashboard.
-//
-// Stars the given Dashboard for the actual user.
-//
-// Deprecated: true
-//
-// Responses:
-// 200: okResponse
-// 400: badRequestError
-// 401: unauthorisedError
-// 403: forbiddenError
-// 500: internalServerError
-func (api *API) StarDashboard(c *contextmodel.ReqContext) response.Response {
-	userID, err := identity.UserIdentifier(c.GetID())
-	if err != nil {
-		return response.Error(http.StatusBadRequest, "Only users and service accounts can star dashboards", nil)
-	}
-
-	id, err := strconv.ParseInt(web.Params(c.Req)[":id"], 10, 64)
-	if err != nil {
-		return response.Error(http.StatusBadRequest, "Invalid dashboard ID", nil)
-	}
-
-	api.logger.Warn("POST /user/stars/dashboard/{dashboard_id} is deprecated, please use POST /user/stars/dashboard/uid/{dashboard_uid} instead")
-
-	cmd := star.StarDashboardCommand{UserID: userID, DashboardID: id, Updated: time.Now()}
-	// nolint:staticcheck
-	if cmd.DashboardID <= 0 {
-		return response.Error(http.StatusBadRequest, "Missing dashboard id", nil)
-	}
-
-	if err := api.starService.Add(c.Req.Context(), &cmd); err != nil {
-		return response.Error(http.StatusInternalServerError, "Failed to star dashboard", err)
-	}
-
-	return response.Success("Dashboard starred!")
-}
-
 // swagger:route POST /user/stars/dashboard/uid/{dashboard_uid} signed_in_user starDashboardByUID
 //
 // Star a dashboard.
@@ -132,60 +91,18 @@ func (api *API) StarDashboardByUID(c *contextmodel.ReqContext) response.Response
 		return response.Error(http.StatusBadRequest, "Only users and service accounts can star dashboards", nil)
 	}
 
-	dash, rsp := api.getDashboardHelper(c.Req.Context(), c.GetOrgID(), 0, uid)
+	_, rsp := api.getDashboardHelper(c.Req.Context(), c.GetOrgID(), 0, uid)
 	if rsp != nil {
 		return rsp
 	}
 
-	cmd := star.StarDashboardCommand{UserID: userID, DashboardID: dash.ID, DashboardUID: uid, OrgID: c.GetOrgID(), Updated: time.Now()}
+	cmd := star.StarDashboardCommand{UserID: userID, DashboardUID: uid, OrgID: c.GetOrgID(), Updated: time.Now()}
 
 	if err := api.starService.Add(c.Req.Context(), &cmd); err != nil {
 		return response.Error(http.StatusInternalServerError, "Failed to star dashboard", err)
 	}
 
 	return response.Success("Dashboard starred!")
-}
-
-// swagger:route DELETE /user/stars/dashboard/{dashboard_id} signed_in_user unstarDashboard
-//
-// Unstar a dashboard.
-//
-// Deletes the starring of the given Dashboard for the actual user.
-//
-// Deprecated: true
-//
-// Please refer to the [new](#/signed_in_user/unstarDashboardByUID) API instead
-//
-// Responses:
-// 200: okResponse
-// 400: badRequestError
-// 401: unauthorisedError
-// 403: forbiddenError
-// 500: internalServerError
-func (api *API) UnstarDashboard(c *contextmodel.ReqContext) response.Response {
-	id, err := strconv.ParseInt(web.Params(c.Req)[":id"], 10, 64)
-	if err != nil {
-		return response.Error(http.StatusBadRequest, "Invalid dashboard ID", nil)
-	}
-
-	userID, err := identity.UserIdentifier(c.GetID())
-	if err != nil {
-		return response.Error(http.StatusBadRequest, "Only users and service accounts can star dashboards", nil)
-	}
-
-	api.logger.Warn("DELETE /user/stars/dashboard/{dashboard_id} is deprecated, please use DELETE /user/stars/dashboard/uid/{dashboard_uid} instead")
-
-	cmd := star.UnstarDashboardCommand{UserID: userID, DashboardID: id}
-	// nolint:staticcheck
-	if cmd.DashboardID <= 0 {
-		return response.Error(http.StatusBadRequest, "Missing dashboard id", nil)
-	}
-
-	if err := api.starService.Delete(c.Req.Context(), &cmd); err != nil {
-		return response.Error(http.StatusInternalServerError, "Failed to unstar dashboard", err)
-	}
-
-	return response.Success("Dashboard unstarred")
 }
 
 // swagger:route DELETE /user/stars/dashboard/uid/{dashboard_uid} signed_in_user unstarDashboardByUID
