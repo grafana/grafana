b6fb42eb38c3f0f7204ec560154139321cec97ab
jev forsberg <jev.forsberg@grafana.com>
2025-09-04 16:52:33 -0600

    Fix flot path resolution using process.cwd() instead of __dirname
    The issue was that in the CI environment, using __dirname resulted in
    incorrect path resolution for the flot files. Using process.cwd() ensures
    the paths resolve correctly relative to the working directory in both
    local and CI environments.

--- scripts/webpack/webpack.common.js
+++ scripts/webpack/webpack.common.js
@@ -34,16 +34,16 @@ module.exports = {
       '@locker/near-membrane-dom/custom-devtools-formatter': require.resolve(
         '@locker/near-membrane-dom/custom-devtools-formatter.js'
       ),
-      // Fix flot package resolution issues
-      'vendor/flot/jquery.flot': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.js'),
-      'vendor/flot/jquery.flot.selection': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.selection.js'),
-      'vendor/flot/jquery.flot.time': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.time.js'),
-      'vendor/flot/jquery.flot.stack': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.stack.js'),
-      'vendor/flot/jquery.flot.stackpercent': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.stackpercent.js'),
-      'vendor/flot/jquery.flot.fillbelow': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.fillbelow.js'),
-      'vendor/flot/jquery.flot.crosshair': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.crosshair.js'),
-      'vendor/flot/jquery.flot.dashes': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.dashes.js'),
-      'vendor/flot/jquery.flot.gauge': path.resolve(__dirname, '../../public/vendor/flot/jquery.flot.gauge.js'),
+      // Fix flot package resolution issues - use absolute paths from working directory
+      'vendor/flot/jquery.flot': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.js'),
+      'vendor/flot/jquery.flot.selection': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.selection.js'),
+      'vendor/flot/jquery.flot.time': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.time.js'),
+      'vendor/flot/jquery.flot.stack': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.stack.js'),
+      'vendor/flot/jquery.flot.stackpercent': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.stackpercent.js'),
+      'vendor/flot/jquery.flot.fillbelow': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.fillbelow.js'),
+      'vendor/flot/jquery.flot.crosshair': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.crosshair.js'),
+      'vendor/flot/jquery.flot.dashes': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.dashes.js'),
+      'vendor/flot/jquery.flot.gauge': path.resolve(process.cwd(), 'public/vendor/flot/jquery.flot.gauge.js'),
     },
     modules: [
       // default value
