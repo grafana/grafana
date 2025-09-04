9cc8527418c3ca1d5614270b9dc74896968d642a
Andreas Christou <andreas.christou@grafana.com>
2025-09-04 12:23:20 +0100

    Add util function for decoding responses

--- pkg/tsdb/graphite/utils.go
+++ pkg/tsdb/graphite/utils.go
@@ -1,12 +1,16 @@
 package graphite
 
 import (
+	"compress/flate"
+	"compress/gzip"
 	"errors"
 	"fmt"
+	"io"
 	"regexp"
 	"strings"
 	"time"
 
+	"github.com/andybalholm/brotli"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 )
 
@@ -43,4 +47,40 @@ func parseDataTimePoint(dataTimePoint DataTimePoint) (time.Time, *float64, error
 	} else {
 		return timestamp, nil, nil
 	}
+}
+
+func decode(encoding string, original io.ReadCloser) ([]byte, error) {
+	var reader io.Reader
+	var err error
+	switch encoding {
+	case "gzip":
+		reader, err = gzip.NewReader(original)
+		if err != nil {
+			return nil, err
+		}
+		defer func() {
+			if err := reader.(io.ReadCloser).Close(); err != nil {
+				backend.Logger.Warn("Failed to close reader body", "err", err)
+			}
+		}()
+	case "deflate":
+		reader = flate.NewReader(original)
+		defer func() {
+			if err := reader.(io.ReadCloser).Close(); err != nil {
+				backend.Logger.Warn("Failed to close reader body", "err", err)
+			}
+		}()
+	case "br":
+		reader = brotli.NewReader(original)
+	case "":
+		reader = original
+	default:
+		return nil, fmt.Errorf("unexpected encoding type %v", err)
+	}
+
+	body, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
