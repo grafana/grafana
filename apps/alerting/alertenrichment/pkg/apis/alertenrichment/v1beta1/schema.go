77d65fad54bb6c2546d4b3ce386bbaa2e60f20ac
Alexander Akhmetov <me@alx.cx>
2025-09-04 16:18:03 +0200

    Alerting: Fix alert enrichment API plural schema name (#110593)

--- apps/alerting/alertenrichment/pkg/apis/alertenrichment/v1beta1/schema.go
+++ apps/alerting/alertenrichment/pkg/apis/alertenrichment/v1beta1/schema.go
@@ -7,7 +7,7 @@ import (
 // schema is unexported to prevent accidental overwrites
 var (
 	schemaAlertEnrichment = resource.NewSimpleSchema(APIGroup, APIVersion, &AlertEnrichment{}, &AlertEnrichmentList{}, resource.WithKind("AlertEnrichment"),
-		resource.WithPlural("alertenrichments"), resource.WithScope(resource.NamespacedScope))
+		resource.WithPlural("alert-enrichments"), resource.WithScope(resource.NamespacedScope))
 	kindAlertEnrichment = resource.Kind{
 		Schema: schemaAlertEnrichment,
 		Codecs: map[resource.KindEncoding]resource.Codec{
