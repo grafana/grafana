c655793f7cd733dace98f5f84ed095fc75a43e17
Bruno <brunotj2015@hotmail.com>
2025-09-04 10:01:05 -0300

    use standard sql in secure_value_lease_inactive.sql (#110532)
    * use standard sql in secure_value_lease_inactive.sql
    
    * ci

--- pkg/storage/secret/metadata/testdata/mysql--secure_value_lease_inactive-lease inactive.sql
+++ pkg/storage/secret/metadata/testdata/mysql--secure_value_lease_inactive-lease inactive.sql
@@ -1,14 +1,20 @@
+WITH to_update AS (
+  SELECT guid FROM (
+    SELECT 
+      guid,
+      ROW_NUMBER() OVER (ORDER BY created ASC) AS rn
+    FROM `secret_secure_value` 
+    WHERE 
+      `active` = FALSE AND
+      10 - `created` > 300 AND
+      10 - `lease_created` > 30
+  ) AS sub
+  WHERE rn <= 10
+)
 UPDATE
   `secret_secure_value`
 SET
   `lease_token` = 'token',
   `lease_created` = 10
-WHERE 
-  `guid` IN (SELECT `guid` 
-                          FROM `secret_secure_value` 
-                          WHERE 
-                            `active` = FALSE AND
-                            10 - `created` > 300 AND
-                            10 - `lease_created` > 30
-                          LIMIT 10)
+WHERE guid IN (SELECT guid FROM to_update)
 ;
