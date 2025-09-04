027a07899d4d40d9c10f4bdd12cc5abc1bff62cc
Ryan McKinley <ryantxu@gmail.com>
2025-09-04 21:42:01 +0300

    did not help

--- pkg/registry/apis/folders/authorizer.go
+++ pkg/registry/apis/folders/authorizer.go
@@ -5,31 +5,24 @@ import (
 
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 
-	"github.com/grafana/authlib/types"
-	"github.com/grafana/grafana/pkg/apimachinery/identity"
+	authlib "github.com/grafana/authlib/types"
 )
 
 // newMultiTenantAuthorizer creates an authorizer suitable to multi-tenant setup.
 // For now it only allow authorization of access tokens.
-func newAuthorizer(ac types.AccessClient) authorizer.Authorizer {
+func newAuthorizer(ac authlib.AccessClient) authorizer.Authorizer {
 	return authorizer.AuthorizerFunc(func(ctx context.Context, a authorizer.Attributes) (authorizer.Decision, string, error) {
-		info, ok := types.AuthInfoFrom(ctx)
+		info, ok := authlib.AuthInfoFrom(ctx)
 		if !ok {
 			return authorizer.DecisionDeny, "missing auth info", nil
 		}
 
-		// Check grafana admin
-		user, _ := identity.GetRequester(ctx)
-		if user != nil && user.GetIsGrafanaAdmin() {
-			return authorizer.DecisionAllow, "", nil
-		}
-
 		// For now we only allow access policy to authorize with multi-tenant setup
-		if !types.IsIdentityType(info.GetIdentityType(), types.TypeAccessPolicy) {
+		if !authlib.IsIdentityType(info.GetIdentityType(), authlib.TypeAccessPolicy) {
 			return authorizer.DecisionDeny, "permission denied", nil
 		}
 
-		res, err := ac.Check(ctx, info, types.CheckRequest{
+		res, err := ac.Check(ctx, info, authlib.CheckRequest{
 			Verb:        a.GetVerb(),
 			Group:       a.GetAPIGroup(),
 			Resource:    a.GetResource(),
