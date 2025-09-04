145a50be4d0fa74dd9fb74d1bd3469d0ba0e9689
Ryan McKinley <ryantxu@gmail.com>
2025-09-04 13:50:31 +0300

    Folders: Improve folder parent testing (#110439)

--- pkg/registry/apis/folders/sub_parents.go
+++ pkg/registry/apis/folders/sub_parents.go
@@ -4,20 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"slices"
-
-	"github.com/grafana/grafana/pkg/services/folder"
-	"k8s.io/apiserver/pkg/storage"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/rest"
+	"k8s.io/apiserver/pkg/storage"
 
 	folders "github.com/grafana/grafana/apps/folder/pkg/apis/folder/v1beta1"
+	"github.com/grafana/grafana/pkg/services/folder"
 )
 
 type subParentsREST struct {
-	getter rest.Getter
+	getter  rest.Getter
+	parents parentsGetter
 }
 
 var _ = rest.Connecter(&subParentsREST{})
@@ -71,53 +70,11 @@ func (r *subParentsREST) Connect(ctx context.Context, name string, opts runtime.
 			return
 		}
 
-		info := r.parents(ctx, folderObj)
-		// Start from the root
-		slices.Reverse(info.Items)
+		info, err := r.parents(ctx, folderObj)
+		if err != nil {
+			responder.Error(err)
+			return
+		}
 		responder.Object(http.StatusOK, info)
 	}), nil
-}
-
-func (r *subParentsREST) parents(ctx context.Context, folder *folders.Folder) *folders.FolderInfoList {
-	info := &folders.FolderInfoList{
-		Items: []folders.FolderInfo{},
-	}
-	for folder != nil {
-		parent := getParent(folder)
-		descr := ""
-		if folder.Spec.Description != nil {
-			descr = *folder.Spec.Description
-		}
-		info.Items = append(info.Items, folders.FolderInfo{
-			Name:        folder.Name,
-			Title:       folder.Spec.Title,
-			Description: descr,
-			Parent:      parent,
-		})
-		if parent == "" {
-			break
-		}
-
-		obj, err := r.getter.Get(ctx, parent, &metav1.GetOptions{})
-		if err != nil {
-			info.Items = append(info.Items, folders.FolderInfo{
-				Name:        parent,
-				Detached:    true,
-				Description: err.Error(),
-			})
-			break
-		}
-
-		parentFolder, ok := obj.(*folders.Folder)
-		if !ok {
-			info.Items = append(info.Items, folders.FolderInfo{
-				Name:        parent,
-				Detached:    true,
-				Description: fmt.Sprintf("expected folder, found: %T", obj),
-			})
-			break
-		}
-		folder = parentFolder
-	}
-	return info
 }
