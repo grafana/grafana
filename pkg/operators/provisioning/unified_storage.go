be8883ca834e3e2714beb0c3d7acc86f48d63110
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-09-05 09:13:37 +0200

    Clean up unified storage setup logic

--- pkg/operators/provisioning/unified_storage.go
+++ pkg/operators/provisioning/unified_storage.go
@@ -1,7 +1,6 @@
 package provisioning
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/grafana/grafana/pkg/infra/tracing"
@@ -9,92 +8,56 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/storage/unified"
 	"github.com/grafana/grafana/pkg/storage/unified/resource"
-	"github.com/grafana/grafana/pkg/storage/unified/resourcepb"
 	"github.com/prometheus/client_golang/prometheus"
-	"google.golang.org/grpc"
 )
 
-// unifiedStorageFactory implements resources.ResourceStore
-// and provides a new unified storage client for each request
 // HACK: This logic directly connects to unified storage. We are doing this for now as there is no global
 // search endpoint. But controllers, in general, should not connect directly to unified storage and instead
 // go through the api server. Once there is a global search endpoint, we will switch to that here as well.
-type unifiedStorageFactory struct {
-	cfg       resource.RemoteResourceClientConfig
-	tracer    tracing.Tracer
-	conn      grpc.ClientConnInterface
-	indexConn grpc.ClientConnInterface
-}
+// expects:
+// [grpc_client_authentication]
+// token =
+// token_exchange_url =
+// token_namespace =
+// audiences =
+// [unified_storage]
+// grpc_address =
+// grpc_index_address =
+// allow_insecure =
+func setupUnifiedStorageClient(cfg *setting.Cfg, tracer tracing.Tracer) (resources.ResourceStore, error) {
+	unifiedStorageSec := cfg.SectionWithEnvOverrides("unified_storage")
 
-func NewUnifiedStorageClientFactory(cfg resource.RemoteResourceClientConfig, address string, indexAddress string, tracer tracing.Tracer) (resources.ResourceStore, error) {
+	// Connect to Server
+	address := unifiedStorageSec.Key("grpc_address").String()
+	if address == "" {
+		return nil, fmt.Errorf("grpc_address is required in [unified_storage] section")
+	}
 	registry := prometheus.NewPedanticRegistry()
 	conn, err := unified.GrpcConn(address, registry)
 	if err != nil {
 		return nil, fmt.Errorf("create unified storage gRPC connection: %w", err)
 	}
 
+	// Connect to Index
+	indexAddress := unifiedStorageSec.Key("grpc_index_address").String()
 	if indexAddress == "" {
 		indexAddress = address
 	}
-
 	indexConn, err := unified.GrpcConn(indexAddress, registry)
 	if err != nil {
 		return nil, fmt.Errorf("create unified storage index gRPC connection: %w", err)
 	}
 
-	return &unifiedStorageFactory{
-		tracer:    tracer,
-		conn:      conn,
-		indexConn: indexConn,
-	}, nil
-}
-
-func (s *unifiedStorageFactory) getClient(ctx context.Context) (resource.ResourceClient, error) {
-	return resource.NewRemoteResourceClient(s.tracer, s.conn, s.indexConn, s.cfg)
-}
-
-func (s *unifiedStorageFactory) CountManagedObjects(ctx context.Context, in *resourcepb.CountManagedObjectsRequest, opts ...grpc.CallOption) (*resourcepb.CountManagedObjectsResponse, error) {
-	client, err := s.getClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("get unified storage client: %w", err)
-	}
-	return client.CountManagedObjects(ctx, in, opts...)
-}
-
-func (s *unifiedStorageFactory) ListManagedObjects(ctx context.Context, in *resourcepb.ListManagedObjectsRequest, opts ...grpc.CallOption) (*resourcepb.ListManagedObjectsResponse, error) {
-	client, err := s.getClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("get unified storage client: %w", err)
-	}
-
-	return client.ListManagedObjects(ctx, in, opts...)
-}
-
-func (s *unifiedStorageFactory) Search(ctx context.Context, in *resourcepb.ResourceSearchRequest, opts ...grpc.CallOption) (*resourcepb.ResourceSearchResponse, error) {
-	client, err := s.getClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("get unified storage client: %w", err)
-	}
-
-	return client.Search(ctx, in, opts...)
-}
-
-func (s *unifiedStorageFactory) GetStats(ctx context.Context, in *resourcepb.ResourceStatsRequest, opts ...grpc.CallOption) (*resourcepb.ResourceStatsResponse, error) {
-	client, err := s.getClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("get unified storage client: %w", err)
-	}
-
-	return client.GetStats(ctx, in, opts...)
-}
-
-func setupUnifiedStorageClient(cfg *setting.Cfg, tracer tracing.Tracer) (resources.ResourceStore, error) {
-	// TODO: This is duplicate
+	// Create client
 	gRPCAuth := cfg.SectionWithEnvOverrides("grpc_client_authentication")
+	if gRPCAuth == nil {
+		return nil, fmt.Errorf("missing [grpc_client_authentication] section in config")
+	}
 	token := gRPCAuth.Key("token").String()
 	if token == "" {
 		return nil, fmt.Errorf("token is required in [grpc_client_authentication] section")
 	}
+
 	tokenExchangeURL := gRPCAuth.Key("token_exchange_url").String()
 	if tokenExchangeURL == "" {
 		return nil, fmt.Errorf("token_exchange_url is required in [grpc_client_authentication] section")
@@ -102,33 +65,19 @@ func setupUnifiedStorageClient(cfg *setting.Cfg, tracer tracing.Tracer) (resourc
 
 	tokenNamespace := gRPCAuth.Key("token_namespace").String()
 	allowInsecure := gRPCAuth.Key("allow_insecure").MustBool(false)
-
-	unifiedStorageSec := cfg.SectionWithEnvOverrides("unified_storage")
-	address := unifiedStorageSec.Key("grpc_address").String()
-	if address == "" {
-		return nil, fmt.Errorf("grpc_address is required in [unified_storage] section")
-	}
-
-	// Optional separate index address
-	indexAddress := unifiedStorageSec.Key("grpc_index_address").String()
+	audiences := gRPCAuth.Key("audiences").Strings("|")
 	unifiedCfg := resource.RemoteResourceClientConfig{
 		Token:            token,
 		TokenExchangeURL: tokenExchangeURL,
-		// TODO: why do we get this?
-		// Audiences:     unifiedStorageSec.Key("grpc_client_authentication_audiences").Strings(","),
-		Namespace:     tokenNamespace,
-		AllowInsecure: allowInsecure,
+		Audiences:        audiences,
+		Namespace:        tokenNamespace,
+		AllowInsecure:    allowInsecure,
 	}
 
-	unified, err := NewUnifiedStorageClientFactory(
-		unifiedCfg,
-		address,
-		indexAddress,
-		tracer,
-	)
+	client, err := resource.NewRemoteResourceClient(tracer, conn, indexConn, unifiedCfg)
 	if err != nil {
 		return nil, fmt.Errorf("create unified storage client: %w", err)
 	}
 
-	return unified, nil
+	return client, nil
 }
