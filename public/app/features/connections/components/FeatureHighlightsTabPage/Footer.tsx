09550ffc820a770b893c28c4a038dc28876c42e0
Hugo Oshiro <hugo.oshiro@grafana.com>
2025-09-09 16:09:06 -0300

    Update style and i18n

--- public/app/features/connections/components/FeatureHighlightsTabPage/Footer.tsx
+++ public/app/features/connections/components/FeatureHighlightsTabPage/Footer.tsx
@@ -1,19 +1,19 @@
-import { Trans } from "@grafana/i18n";
-import { Icon, TextLink } from "@grafana/ui";
+import { Trans } from '@grafana/i18n';
+import { Icon, TextLink } from '@grafana/ui';
 
 export function Footer() {
-   return (
-        <div>
-          <Trans i18nKey="connections.feature-highlight-page.footer">
-            Create a Grafana Cloud Free account to start using data source permissions. This feature is also available
-            with a Grafana Enterprise license.
-          </Trans>
-          <div>
-            <TextLink href="https://grafana.com/products/enterprise/grafana/">
-              <Icon name="external-link-alt" />
-              <Trans i18nKey="connections.feature-highlight-page.footer-link">Learn about Enterprise</Trans>
-            </TextLink>
-          </div>
-        </div>
-   );
+  return (
+    <div>
+      <Trans i18nKey="connections.feature-highlight-page.footer">
+        Create a Grafana Cloud Free account to start using data source permissions. This feature is also available with
+        a Grafana Enterprise license.
+      </Trans>
+      <div>
+        <TextLink href="https://grafana.com/products/enterprise/grafana/">
+          <Icon name="external-link-alt" />
+          <Trans i18nKey="connections.feature-highlight-page.footer-link">Learn about Enterprise</Trans>
+        </TextLink>
+      </div>
+    </div>
+  );
 }
--- public/app/features/connections/components/FeatureHighlightsTabPage/Footnote.tsx
+++ public/app/features/connections/components/FeatureHighlightsTabPage/Footnote.tsx
@@ -1,22 +1,22 @@
 import { css } from '@emotion/css';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { Trans } from "@grafana/i18n";
-import { TextLink, useStyles2 } from "@grafana/ui";
+import { Trans } from '@grafana/i18n';
+import { TextLink, useStyles2 } from '@grafana/ui';
 
 export function Footnote() {
   const styles = useStyles2(getStyles);
-    return (
-        <div className={styles.footNote}>
-          <Trans i18nKey="connections.feature-highlight-page.foot-note">
-            After creating an account, you can have Grafana{' '}
-            <TextLink href="https://grafana.com/products/bring-your-own-cloud-byoc/">
-              migrate this instance to Grafana Cloud
-            </TextLink>{' '}
-            with minimal effort.
-          </Trans>
-        </div>
-    );
+  return (
+    <div className={styles.footNote}>
+      <Trans i18nKey="connections.feature-highlight-page.foot-note">
+        After creating an account, you can have Grafana{' '}
+        <TextLink href="https://grafana.com/products/bring-your-own-cloud-byoc/">
+          migrate this instance to Grafana Cloud
+        </TextLink>{' '}
+        with minimal effort.
+      </Trans>
+    </div>
+  );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
