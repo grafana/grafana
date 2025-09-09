58d90905412305b8e775bd5ae50ca0a1a5ff0397
Javier Ruiz Calle <javier.ruizcalle@grafana.com>
2025-09-09 12:11:48 +0200

    Update subtitle for Observability section

--- pkg/services/navtree/navtreeimpl/applinks.go
+++ pkg/services/navtree/navtreeimpl/applinks.go
@@ -236,7 +236,7 @@ func (s *ServiceImpl) addPluginToSection(c *contextmodel.ReqContext, treeRoot *n
 			treeRoot.AddSection(&navtree.NavLink{
 				Text:       "Observability",
 				Id:         navtree.NavIDObservability,
-				SubTitle:   "Opinionated observability across applications, services, and infrastructure",
+				SubTitle:   "Monitor infrastructure and applications in real time with Grafana Cloud&apos;s fully managed observability suite",
 				Icon:       "heart-rate",
 				SortWeight: navtree.WeightObservability,
 				Children:   []*navtree.NavLink{appLink},
