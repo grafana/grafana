2aae58628aad04990b284ecb5f99b27381d7e997
Piotr Jamróz <pm.jamroz@gmail.com>
2025-09-10 23:43:23 +0200

    Fix tests

--- public/app/features/logs/components/panel/LogLineDetails.test.tsx
+++ public/app/features/logs/components/panel/LogLineDetails.test.tsx
@@ -760,7 +760,17 @@ describe('LogLineDetails', () => {
           if (field.config && field.config.links) {
             return field.config.links.map((link) => {
               return {
-                href: '/explore?left=%7B%22range%22%3A%7B%22from%22%3A%22now-15m%22%2C%22to%22%3A%22now%22%7D%2C%22datasource%22%3A%22fetpfiwe8asqoe%22%2C%22queries%22%3A%5B%7B%22query%22%3A%22abcd1234%22%2C%22queryType%22%3A%22traceql%22%7D%5D%7D',
+                href: '/explore',
+                meta: {
+                  internalLink: {
+                    interpolated: {
+                      query: {
+                        query: 'abcd1234',
+                        queryType: 'traceql'
+                      }
+                    }
+                  }
+                },
                 title: 'tempo',
                 target: '_blank',
                 origin: field,
