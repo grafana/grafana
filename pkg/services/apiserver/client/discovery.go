5fec706906155b4bc6c12f57398591a3a27191e6
Daniele Ferru <daniele.ferru@grafana.com>
2025-09-10 18:29:59 +0200

    adding some logs to better understand what might be happening

--- pkg/services/apiserver/client/discovery.go
+++ pkg/services/apiserver/client/discovery.go
@@ -13,7 +13,7 @@ type DiscoveryClient interface {
 	discovery.DiscoveryInterface
 	GetResourceForKind(gvk schema.GroupVersionKind) (schema.GroupVersionResource, error)
 	GetKindForResource(gvr schema.GroupVersionResource) (schema.GroupVersionKind, error)
-	GetPreferredVesion(gr schema.GroupResource) (schema.GroupVersionResource, schema.GroupVersionKind, error)
+	GetPreferredVersion(gr schema.GroupResource) (schema.GroupVersionResource, schema.GroupVersionKind, error)
 	GetPreferredVersionForKind(gk schema.GroupKind) (schema.GroupVersionResource, schema.GroupVersionKind, error)
 }
 
@@ -67,10 +67,10 @@ func (d *DiscoveryClientImpl) GetKindForResource(gvr schema.GroupVersionResource
 	return schema.GroupVersionKind{}, fmt.Errorf("kind not found for %s", gvr.String())
 }
 
-func (d *DiscoveryClientImpl) GetPreferredVesion(gr schema.GroupResource) (schema.GroupVersionResource, schema.GroupVersionKind, error) {
+func (d *DiscoveryClientImpl) GetPreferredVersion(gr schema.GroupResource) (schema.GroupVersionResource, schema.GroupVersionKind, error) {
 	apiList, err := d.ServerPreferredResources()
 	if err != nil {
-		return schema.GroupVersionResource{}, schema.GroupVersionKind{}, err
+		return schema.GroupVersionResource{}, schema.GroupVersionKind{}, fmt.Errorf("error getting server's preferred resources: %w", err)
 	}
 	for _, apis := range apiList {
 		if !strings.HasPrefix(apis.GroupVersion, gr.Group) {
