9d6c6f30de32b268cf87d2ac710e95fb6f3ec047
Darren Janeczek <darren.janeczek@grafana.com>
2025-09-10 08:00:43 -0400

    fix: avoid iterating over and checking enabled plugins twice

--- pkg/services/navtree/navtreeimpl/applinks.go
+++ pkg/services/navtree/navtreeimpl/applinks.go
@@ -54,7 +54,7 @@ func (s *ServiceImpl) addAppLinks(treeRoot *navtree.NavTreeRoot, c *contextmodel
 		}
 	}
 
-	if adaptiveTelemetryPlugin, adaptiveTelemetrySection := enabledAccessibleAppPluginMap["grafana-adaptivetelemetry-app"], treeRoot.FindById(navtree.NavIDAdaptiveTelemetry); adaptiveTelemetryPlugin != nil && adaptiveTelemetryNode != nil {
+	if adaptiveTelemetryPlugin, adaptiveTelemetrySection := enabledAccessibleAppPluginMap["grafana-adaptivetelemetry-app"], treeRoot.FindById(navtree.NavIDAdaptiveTelemetry); adaptiveTelemetryPlugin != nil && adaptiveTelemetrySection != nil {
 		// If the adaptivetelemetry app is enabled, and the adaptiveTelemetrySection exists, then update the section to point to the plugin
 		adaptiveTelemetrySection.Url = s.cfg.AppSubURL + "/a/" + adaptiveTelemetryPlugin.ID
 		adaptiveTelemetrySection.PluginID = adaptiveTelemetryPlugin.ID
