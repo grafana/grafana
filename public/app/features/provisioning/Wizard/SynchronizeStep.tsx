fabbdec6fb9174bdb0019557518a65dbd7719c16
Clarity-89 <homes89@ukr.net>
2025-09-10 18:35:04 +0300

    Remove info and add cancel button

--- public/app/features/provisioning/Wizard/SynchronizeStep.tsx
+++ public/app/features/provisioning/Wizard/SynchronizeStep.tsx
@@ -16,9 +16,11 @@ import { WizardFormData } from './types';
 
 export interface SynchronizeStepProps {
   isLegacyStorage?: boolean;
+  onCancel?: (repoName: string) => void;
+  isCancelling?: boolean;
 }
 
-export function SynchronizeStep({ isLegacyStorage }: SynchronizeStepProps) {
+export function SynchronizeStep({ isLegacyStorage, onCancel, isCancelling }: SynchronizeStepProps) {
   const { getValues, register, watch } = useFormContext<WizardFormData>();
   const { setStepStatusInfo } = useStepStatus();
   const [repoName = '', repoType] = watch(['repositoryName', 'repository.type']);
@@ -71,41 +73,43 @@ export function SynchronizeStep({ isLegacyStorage }: SynchronizeStepProps) {
           }}
         />
       )}
-      <Alert
-        title={t(
-          'provisioning.wizard.alert-title',
-          'Important: No data or configuration will be lost, but dashboards will be temporarily unavailable for a few minutes.'
-        )}
-        severity={'info'}
-      >
-        <ul style={{ marginLeft: '16px' }}>
-          <li>
-            <Trans i18nKey="provisioning.wizard.alert-point-1">
-              Resources won&#39;t be able to be created, edited, or deleted during this process. In the last step, they
-              will disappear.
-            </Trans>
-          </li>
-          <li>
-            <Trans i18nKey="provisioning.wizard.alert-point-2">
-              Once provisioning is complete, resources will reappear and be managed through external storage.
-            </Trans>
-          </li>
-          <li>
-            <Trans i18nKey="provisioning.wizard.alert-point-3">
-              The duration of this process depends on the number of resources involved.
-            </Trans>
-          </li>
-          <li>
-            <Trans i18nKey="provisioning.wizard.alert-point-4">
-              Enterprise instance administrators can display an announcement banner to users. See{' '}
-              <TextLink external href="https://grafana.com/docs/grafana/latest/administration/announcement-banner/">
-                this guide
-              </TextLink>{' '}
-              for step-by-step instructions.
-            </Trans>
-          </li>
-        </ul>
-      </Alert>
+      {isRepositoryHealthy && (
+        <Alert
+          title={t(
+            'provisioning.wizard.alert-title',
+            'Important: No data or configuration will be lost, but dashboards will be temporarily unavailable for a few minutes.'
+          )}
+          severity={'info'}
+        >
+          <ul style={{ marginLeft: '16px' }}>
+            <li>
+              <Trans i18nKey="provisioning.wizard.alert-point-1">
+                Resources won&#39;t be able to be created, edited, or deleted during this process. In the last step,
+                they will disappear.
+              </Trans>
+            </li>
+            <li>
+              <Trans i18nKey="provisioning.wizard.alert-point-2">
+                Once provisioning is complete, resources will reappear and be managed through external storage.
+              </Trans>
+            </li>
+            <li>
+              <Trans i18nKey="provisioning.wizard.alert-point-3">
+                The duration of this process depends on the number of resources involved.
+              </Trans>
+            </li>
+            <li>
+              <Trans i18nKey="provisioning.wizard.alert-point-4">
+                Enterprise instance administrators can display an announcement banner to users. See{' '}
+                <TextLink external href="https://grafana.com/docs/grafana/latest/administration/announcement-banner/">
+                  this guide
+                </TextLink>{' '}
+                for step-by-step instructions.
+              </Trans>
+            </li>
+          </ul>
+        </Alert>
+      )}
       {supportsHistory && (
         <>
           <Text element="h3">
@@ -127,9 +131,19 @@ export function SynchronizeStep({ isLegacyStorage }: SynchronizeStepProps) {
       )}
 
       <Field noMargin>
-        <Button variant="primary" onClick={startSynchronization} disabled={isButtonDisabled}>
-          <Trans i18nKey="provisioning.wizard.button-start">Begin synchronization</Trans>
-        </Button>
+        {!isRepositoryHealthy ? (
+          <Button variant="destructive" onClick={() => onCancel?.(repoName)} disabled={isCancelling}>
+            {isCancelling ? (
+              <Trans i18nKey="provisioning.wizard.button-cancelling">Cancelling...</Trans>
+            ) : (
+              <Trans i18nKey="provisioning.wizard.button-cancel">Cancel</Trans>
+            )}
+          </Button>
+        ) : (
+          <Button variant="primary" onClick={startSynchronization} disabled={isButtonDisabled}>
+            <Trans i18nKey="provisioning.wizard.button-start">Begin synchronization</Trans>
+          </Button>
+        )}
       </Field>
     </Stack>
   );
