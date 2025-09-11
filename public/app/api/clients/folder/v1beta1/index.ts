d89ca7ff21e57636ae04be6265198e01c7e83b95
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-09-11 13:49:24 +0100

    Add getAffectedItems custom endpoint to folders API

--- public/app/api/clients/folder/v1beta1/index.ts
+++ public/app/api/clients/folder/v1beta1/index.ts
@@ -1,43 +1,84 @@
-import { generatedAPI } from './endpoints.gen';
+import { DescendantCount } from 'app/types/folders';
 
-export const folderAPIv1beta1 = generatedAPI.enhanceEndpoints({
-  endpoints: {
-    getFolder: {
-      providesTags: (result, error, arg) => (result ? [{ type: 'Folder', id: arg.name }] : []),
+import { generatedAPI } from './endpoints.gen';
+import { getParsedCounts } from './utils';
+
+export const folderAPIv1beta1 = generatedAPI
+  .enhanceEndpoints({
+    endpoints: {
+      getFolder: {
+        providesTags: (result, error, arg) => (result ? [{ type: 'Folder', id: arg.name }] : []),
+      },
+      listFolder: {
+        providesTags: (result) =>
+          result
+            ? [
+                { type: 'Folder', id: 'LIST' },
+                ...result.items
+                  .map((folder) => ({ type: 'Folder' as const, id: folder.metadata?.name }))
+                  .filter(Boolean),
+              ]
+            : [{ type: 'Folder', id: 'LIST' }],
+      },
+      deleteFolder: {
+        // We don't want delete to invalidate getFolder tags, as that would lead to unnecessary 404s
+        invalidatesTags: (result, error) => (error ? [] : [{ type: 'Folder', id: 'LIST' }]),
+      },
+      updateFolder: {
+        query: (queryArg) => ({
+          url: `/folders/${queryArg.name}`,
+          method: 'PATCH',
+          // We need to stringify the body and set the correct header for the call to work with k8s api.
+          body: JSON.stringify(queryArg.patch),
+          headers: {
+            'Content-Type': 'application/strategic-merge-patch+json',
+          },
+          params: {
+            pretty: queryArg.pretty,
+            dryRun: queryArg.dryRun,
+            fieldManager: queryArg.fieldManager,
+            fieldValidation: queryArg.fieldValidation,
+            force: queryArg.force,
+          },
+        }),
+      },
     },
-    listFolder: {
-      providesTags: (result) =>
-        result
-          ? [
-              { type: 'Folder', id: 'LIST' },
-              ...result.items.map((folder) => ({ type: 'Folder' as const, id: folder.metadata?.name })).filter(Boolean),
-            ]
-          : [{ type: 'Folder', id: 'LIST' }],
-    },
-    deleteFolder: {
-      // We don't want delete to invalidate getFolder tags, as that would lead to unnecessary 404s
-      invalidatesTags: (result, error) => (error ? [] : [{ type: 'Folder', id: 'LIST' }]),
-    },
-    updateFolder: {
-      query: (queryArg) => ({
-        url: `/folders/${queryArg.name}`,
-        method: 'PATCH',
-        // We need to stringify the body and set the correct header for the call to work with k8s api.
-        body: JSON.stringify(queryArg.patch),
-        headers: {
-          'Content-Type': 'application/strategic-merge-patch+json',
-        },
-        params: {
-          pretty: queryArg.pretty,
-          dryRun: queryArg.dryRun,
-          fieldManager: queryArg.fieldManager,
-          fieldValidation: queryArg.fieldValidation,
-          force: queryArg.force,
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      getAffectedItems: builder.query<Record<string, number>, { folderUIDs: string[]; dashboardUIDs: string[] }>({
+        // Similar to legacy API, don't cache this data as we don't have full knowledge of the descendant entities
+        // and when they're created/deleted, so we can't easily know when this data is stale
+        keepUnusedDataFor: 0,
+        queryFn: async ({ folderUIDs, dashboardUIDs }, queryApi) => {
+          const initialCounts: DescendantCount = {
+            folders: folderUIDs.length,
+            dashboards: dashboardUIDs.length,
+            library_elements: 0,
+            alertrules: 0,
+          };
+
+          const promises = folderUIDs.map(async (folderUID) =>
+            queryApi.dispatch(generatedAPI.endpoints.getFolderCounts.initiate({ name: folderUID }))
+          );
+          const results = await Promise.all(promises);
+
+          const mapped = results.reduce((acc, result) => {
+            const { data } = result;
+
+            const counts = getParsedCounts(data?.counts ?? []);
+            acc.folders += counts.folders;
+            acc.dashboards += counts.dashboards;
+            acc.alertrules += counts.alertrules;
+            acc.library_elements += counts.library_elements;
+            return acc;
+          }, initialCounts);
+
+          return { data: mapped };
         },
       }),
-    },
-  },
-});
+    }),
+  });
 
 export const {
   useGetFolderQuery,
@@ -46,6 +87,7 @@ export const {
   useCreateFolderMutation,
   useUpdateFolderMutation,
   useReplaceFolderMutation,
+  useGetAffectedItemsQuery,
 } = folderAPIv1beta1;
 
 // eslint-disable-next-line no-barrel-files/no-barrel-files
--- public/app/api/clients/folder/v1beta1/utils.ts
+++ public/app/api/clients/folder/v1beta1/utils.ts
@@ -6,6 +6,8 @@ import appEvents from '../../../../core/app_events';
 import { isProvisionedFolder } from '../../../../features/browse-dashboards/api/isProvisioned';
 import { useDispatch } from '../../../../types/store';
 
+import { ResourceStats } from './endpoints.gen';
+
 import { folderAPIv1beta1 as folderAPI } from './index';
 
 export async function isProvisionedFolderCheck(
@@ -34,3 +36,34 @@ export async function isProvisionedFolderCheck(
     return false;
   }
 }
+
+const initialCounts: Record<string, number> = {
+  folder: 0,
+  dashboard: 0,
+  libraryPanel: 0,
+  alertRule: 0,
+};
+
+/**
+ * Parses descendant counts into legacy-friendly format
+ *
+ * Takes the first count information as the source of truth, e.g. if
+ * the array has a
+ *
+ * `"group": "dashboard.grafana.app"`
+ *
+ * entry first, and a
+ *
+ * `"group": "sql-fallback"`
+ *
+ * entry later, the `dashboard.grafana.app` count will be used
+ */
+export const getParsedCounts = (counts: ResourceStats[]) => {
+  return counts.reduce((acc, { resource, count }) => {
+    // If there's no value already, then use that count, so a fallback count is not used
+    if (!acc[resource]) {
+      acc[resource] = count;
+    }
+    return acc;
+  }, initialCounts);
+};
