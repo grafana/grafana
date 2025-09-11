009dc37135b7c3f8e91c174675c67d5e8503b96d
Torkel Ödegaard <torkel@grafana.com>
2025-09-11 12:26:33 +0100

    Variables: Style tweaks to new variables menu

--- public/app/features/dashboard-scene/scene/DropdownVariableControls.tsx
+++ public/app/features/dashboard-scene/scene/DropdownVariableControls.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 import { t } from '@grafana/i18n';
 import { sceneGraph } from '@grafana/scenes';
-import { Dropdown, Menu, ToolbarButton, useStyles2 } from '@grafana/ui';
+import { Box, Dropdown, ToolbarButton, useStyles2 } from '@grafana/ui';
 
 import { DashboardScene } from './DashboardScene';
 import { VariableValueSelectWrapper } from './VariableControls';
@@ -24,18 +24,29 @@ export function DropdownVariableControls({ dashboard }: { dashboard: DashboardSc
 
   return (
     <Dropdown
+      placement="bottom-end"
       overlay={
-        <Menu
+        <Box
+          minWidth={32}
+          borderColor={'weak'}
+          borderStyle={'solid'}
+          boxShadow={'z3'}
+          display={'flex'}
+          direction={'column'}
+          borderRadius={'default'}
+          backgroundColor={'primary'}
+          padding={1}
+          gap={0.5}
           onClick={(e) => {
             e.stopPropagation();
           }}
         >
           {variables.map((variable) => (
             <div className={styles.menuItem} key={variable.state.key}>
-              <VariableValueSelectWrapper variable={variable} />
+              <VariableValueSelectWrapper variable={variable} inMenu />
             </div>
           ))}
-        </Menu>
+        </Box>
       }
     >
       <ToolbarButton
@@ -44,6 +55,7 @@ export function DropdownVariableControls({ dashboard }: { dashboard: DashboardSc
         icon="ellipsis-v"
         iconSize="md"
         narrow
+        variant="canvas"
       />
     </Dropdown>
   );
