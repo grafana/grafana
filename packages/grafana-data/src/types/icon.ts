08203a060e94de08220bdbc9edb1fff6abb75b92
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2025-09-11 13:13:17 -0400

    feat: NavTree menu entry for adaptive telemetry grafana cloud apps (#109561)
    * feat: NavTree menu entry for adaptive telemetry grafana cloud apps
    
    * feat: 'adaptive-telemetry' icon added

--- packages/grafana-data/src/types/icon.ts
+++ packages/grafana-data/src/types/icon.ts
@@ -98,6 +98,7 @@ export const availableIconsIndex = {
   'download-alt': true,
   draggabledots: true,
   drilldown: true,
+  'adaptive-telemetry': true,
   edit: true,
   'ellipsis-v': true,
   enter: true,
