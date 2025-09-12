63615617adff1d9fe741c2a81f9e519a46e264f9
Paul Marbach <paul.marbach@grafana.com>
2025-09-12 17:33:06 -0400

    Table: Restore previous footer behavior of reducers applying to filtered data (#111041)
    * Table: Restore previous footer behavior of reducers applying to filtered data
    
    * update e2e to match new behavior
    
    (cherry picked from commit f258d8a41726e7bb482ae1cf8549a2686bfdb38c)

--- packages/grafana-ui/src/components/Table/TableNG/TableNG.tsx
+++ packages/grafana-ui/src/components/Table/TableNG/TableNG.tsx
@@ -677,7 +677,7 @@ export function TableNG(props: TableNGProps) {
           ),
           renderSummaryCell: () => (
             <SummaryCell
-              rows={rows}
+              rows={sortedRows}
               footers={footers}
               field={field}
               colIdx={i}
@@ -707,10 +707,11 @@ export function TableNG(props: TableNGProps) {
       maxRowHeight,
       numFrozenColsFullyInView,
       onCellFilterAdded,
+      rows,
       rowHeight,
       rowHeightFn,
-      rows,
       setFilter,
+      sortedRows,
       showTypeIcons,
       theme,
       timeRange,
