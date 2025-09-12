dc882c3316947262097732147eb986eb42467ff0
Andreas Christou <andreas.christou@grafana.com>
2025-09-12 23:35:10 +0200

    Graphite: Backend metrics expand endpoint (#110678)
    * Add lint rules
    
    * Backend decoupling
    
    - Add standalone files
    - Add graphite query type
    - Add logger to Service
    - Create logger in the ProvideService method
    - Use a pointer for the HTTP client provider
    - Update logger usage everywhere
    - Update tracer type
    - Replace simplejson with json
    - Add dummy CallResource and CheckHealth methods
    - Update tests
    
    * Update ConfigEditor imports
    
    * Update types imports
    
    * Update datasource
    
    - Switch to using semver package
    - Update imports
    
    * Update store imports
    
    * Update helper imports and notification creation
    
    * Update context import
    
    * Update version numbers and logic
    
    * Copy array_move from core
    
    * Test updates
    
    * Add required files and update plugin.json
    
    * Update core references and packages
    
    * Remove commented code
    
    * Update wire
    
    * Lint
    
    * Fix import
    
    * Copy null type
    
    * More lint
    
    * Update snapshot
    
    * Refactor backend
    
    - Split query logic into separate file
    - Move utils to separate file
    
    * Add health-check logic
    
    - Support backend healthcheck if the FF is enabled
    
    * Remove query import support as unneeded
    
    * Add test
    
    * Add util function for decoding responses
    
    * Add events types
    
    * Add resource handler
    
    * Add events handler and generic resource req handler
    
    * Tests
    
    * Update frontend
    
    - Add types
    - Update events function to support backend requests
    
    * Lint and typing
    
    * Lint
    
    * Add metrics find endpoint
    
    - Add types
    - Add generic response parser
    - Add endpoint
    - Tests
    
    * Update FE functoin to use backend endpoint
    
    * Lint
    
    * Simplify request
    
    * Update test
    
    * Metrics expand type
    
    * Extract shared logic and add metric expand endpoint
    
    * Update tests
    
    * Call metric expand from backend
    
    * Add tests
    
    * Review
    
    * Review
    
    * Fix packages
    
    * Format
    
    * Fix merge issues
    
    * Review
    
    * Fix undefined values
    
    * Extract request creation
    
    - Add method for create requests generically with tests
    - Replace usage in query method
    - Update usages in resource handlers
    - Update tests
    - Update types

--- public/app/plugins/datasource/graphite/datasource.ts
+++ public/app/plugins/datasource/graphite/datasource.ts
@@ -704,8 +704,8 @@ export class GraphiteDatasource
 
     if (config.featureToggles.graphiteBackendMode) {
       return await this.postResource<MetricFindValue[]>('metrics/find', {
-        from: typeof params.from === 'string' ? params.from : `${params.from}`,
-        until: typeof params.until === 'string' ? params.until : `${params.until}`,
+        from: params.from ? (typeof params.from === 'string' ? params.from : `${params.from}`) : undefined,
+        until: params.until ? (typeof params.until === 'string' ? params.until : `${params.until}`) : undefined,
         query,
       });
     }
@@ -741,7 +741,7 @@ export class GraphiteDatasource
    * The result will contain all metrics (with full name) matching provided query.
    * It's a more flexible version of /metrics/find endpoint (@see requestMetricFind)
    */
-  private requestMetricExpand(
+  private async requestMetricExpand(
     query: string,
     requestId: string,
     range?: { from: string | number; until: string | number }
@@ -750,6 +750,18 @@ export class GraphiteDatasource
     if (range) {
       params.from = range.from;
       params.until = range.until;
+    }
+
+    if (config.featureToggles.graphiteBackendMode) {
+      const metrics = await this.postResource<MetricFindValue[]>('metrics/expand', {
+        from: params.from ? (typeof params.from === 'string' ? params.from : `${params.from}`) : undefined,
+        until: params.until ? (typeof params.until === 'string' ? params.until : `${params.until}`) : undefined,
+        query,
+      });
+      return metrics.map((metric) => ({
+        text: metric.text,
+        expandable: false,
+      }));
     }
 
     const httpOptions: BackendSrvRequest = {
