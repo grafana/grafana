164b7224fbb6c7491bee87b62ef25f3e84254596
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2025-09-15 19:37:04 -0300

    fix tests playwright

--- e2e-playwright/dashboards-suite/dashboard-public-templating.spec.ts
+++ e2e-playwright/dashboards-suite/dashboard-public-templating.spec.ts
@@ -23,10 +23,10 @@ test.describe(
       });
 
       // Open sharing modal
-      await dashboardPage.getByGrafanaSelector(selectors.components.NavToolbar.shareDashboard).click();
+      await dashboardPage.getByGrafanaSelector(selectors.pages.Dashboard.DashNav.shareButton).click();
 
       // Select public dashboards tab
-      await dashboardPage.getByGrafanaSelector(selectors.components.Tab.title('Public Dashboard')).click();
+      await dashboardPage.getByGrafanaSelector(selectors.components.Tab.title('Public dashboard')).click();
 
       // Warning Alert dashboard cannot be made public because it has template variables
       await expect(
