abdb8e556f847ad84a1de3a6202c83dcba3f35e2
John Troy <jtroy@users.noreply.github.com>
2025-09-15 09:22:26 -0400

    LDAP: Restore test user mapping functionality (#110841)
    * Migrate LdapPage from connect() to React-Redux hooks
    
    * Convert LDAP debug page into a drawer and hook it into settings
    
    * prettier
    
    * Use the Text component and make the input and button look like they do on the main settings page.
    
    * Bring back isLoading and put in a LoadingPlaceholder
    
    * i18n-extract
    
    * rejigger
    
    * linter fix
    
    (cherry picked from commit 585b53bc7dba1925926a95e86aa8f1d45a2e74d6)

--- public/app/features/admin/ldap/LdapSettingsPage.tsx
+++ public/app/features/admin/ldap/LdapSettingsPage.tsx
@@ -30,6 +30,7 @@ import { Loader } from 'app/features/plugins/admin/components/Loader';
 import { LdapPayload, MapKeyCertConfigured, StoreState } from 'app/types';
 
 import { LdapDrawerComponent } from './LdapDrawer';
+import { LdapTestDrawer } from './LdapTestDrawer';
 
 const appEvents = getAppEvents();
 
@@ -98,6 +99,8 @@ const emptySettings: LdapPayload = {
 export const LdapSettingsPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isTestDrawerOpen, setIsTestDrawerOpen] = useState(false);
+  const [usernameParam, setUsernameParam] = useState<string | null>(null);
 
   const [isBindPasswordConfigured, setBindPasswordConfigured] = useState(false);
   const [mapKeyCertConfigured, setMapKeyCertConfigured] = useState<MapKeyCertConfigured>({
@@ -121,6 +124,10 @@ export const LdapSettingsPage = () => {
 
   useEffect(() => {
     async function init() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const username = urlParams.get('username');
+      setUsernameParam(username);
+
       const payload = await getSettings();
       let serverConfig = emptySettings.settings.config.servers[0];
       if (payload.settings.config.servers?.length > 0) {
@@ -134,6 +141,10 @@ export const LdapSettingsPage = () => {
 
       reset(payload);
       setIsLoading(false);
+
+      if (username) {
+        setIsTestDrawerOpen(true);
+      }
     }
     init();
   }, [reset]);
@@ -415,6 +426,9 @@ export const LdapSettingsPage = () => {
                     <Button variant="secondary" onClick={handleSubmit(saveForm)}>
                       <Trans i18nKey="ldap-settings-page.buttons-section.save-button">Save</Trans>
                     </Button>
+                    <Button variant="secondary" onClick={() => setIsTestDrawerOpen(true)}>
+                      <Trans i18nKey="ldap-settings-page.buttons-section.test-button">Test</Trans>
+                    </Button>
                     <LinkButton href="/admin/authentication" variant="secondary">
                       <Trans i18nKey="ldap-settings-page.buttons-section.discard-button">Discard</Trans>
                     </LinkButton>
@@ -454,6 +468,9 @@ export const LdapSettingsPage = () => {
               />
             )}
           </form>
+          {isTestDrawerOpen && (
+            <LdapTestDrawer onClose={() => setIsTestDrawerOpen(false)} username={usernameParam || undefined} />
+          )}
         </FormProvider>
       </Page.Contents>
     </Page>
