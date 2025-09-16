0a79b3bdc56dcda7bb8cf591d38c0a871667aef1
Ryan McKinley <ryantxu@gmail.com>
2025-09-16 13:35:20 +0300

    Chore: Upgrade k8s.io/api v0.34.1 and grafana-app-sdk v0.43.1 (#111009)

--- pkg/generated/applyconfiguration/service/v0alpha1/externalname.go
+++ pkg/generated/applyconfiguration/service/v0alpha1/externalname.go
@@ -28,6 +28,7 @@ func ExternalName(name, namespace string) *ExternalNameApplyConfiguration {
 	b.WithAPIVersion("service.grafana.app/v0alpha1")
 	return b
 }
+func (b ExternalNameApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -195,8 +196,24 @@ func (b *ExternalNameApplyConfiguration) WithSpec(value *ExternalNameSpecApplyCo
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *ExternalNameApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *ExternalNameApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *ExternalNameApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *ExternalNameApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
