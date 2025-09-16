b5e9220ba827994bc86f26e2df72efe243c12549
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-09-16 16:23:10 -0400

    refactor: move check for origin to go after reading

--- pkg/services/ngalert/notifier/receiver_svc.go
+++ pkg/services/ngalert/notifier/receiver_svc.go
@@ -403,6 +403,10 @@ func (rs *ReceiverService) UpdateReceiver(ctx context.Context, r *models.Receive
 		return nil, err
 	}
 
+	if existing.Origin != models.ResourceOriginGrafana {
+		return nil, makeErrReceiverOrigin(existing, "update")
+	}
+
 	// We re-encrypt the existing receiver to ensure any unencrypted secure fields that are correctly encrypted, note this should NOT re-encrypt secure fields that are already encrypted.
 	// This is rare, but can happen if a receiver is created with unencrypted secure fields and then the secure option is added later.
 	// Preferably, this would be handled by receiver config versions and migrations but for now this is a good safety net.
@@ -423,10 +427,6 @@ func (rs *ReceiverService) UpdateReceiver(ctx context.Context, r *models.Receive
 	err = rs.checkOptimisticConcurrency(existing, r.Version)
 	if err != nil {
 		return nil, err
-	}
-
-	if existing.Origin != models.ResourceOriginGrafana {
-		return nil, makeErrReceiverOrigin(existing, "update")
 	}
 
 	if err := rs.provenanceValidator(existing.Provenance, r.Provenance); err != nil {
