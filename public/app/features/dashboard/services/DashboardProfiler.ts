57c87866bc7015a20cb40d0d3078f23296cebe95
Victor Marin <victor.marin@grafana.com>
2025-09-18 12:59:21 +0300

    refactor

--- public/app/features/dashboard/services/DashboardProfiler.ts
+++ public/app/features/dashboard/services/DashboardProfiler.ts
@@ -35,17 +35,23 @@ export function getDashboardInteractionCallback(uid: string, title: string) {
     const payload = {
       duration: e.duration,
       networkDuration: e.networkDuration,
+      processingTime: e.duration - e.networkDuration,
       startTs: e.startTs,
       endTs: e.endTs,
+      totalJSHeapSize: e.totalJSHeapSize,
+      usedJSHeapSize: e.usedJSHeapSize,
+      jsHeapSizeLimit: e.jsHeapSizeLimit,
+      longFramesCount: e.longFramesCount,
+      longFramesTotalTime: e.longFramesTotalTime,
       timeSinceBoot: performance.measure('time_since_boot', 'frontend_boot_js_done_time_seconds').duration,
     };
 
-    reportInteraction('dashboard_interaction', {
+    reportInteraction('dashboard_render', {
       interactionType: e.origin,
       uid,
       ...payload,
     });
 
-    logMeasurement(`dashboard_interaction`, payload, { interactionType: e.origin, dashboard: uid, title: title });
+    logMeasurement(`dashboard_render`, payload, { interactionType: e.origin, dashboard: uid, title: title });
   };
 }
