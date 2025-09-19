84beadc3c07532c5e1dc9eb75e0801f46638169f
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-09-19 03:40:14 -0600

    Provisioning: Allow disabling of image rendering instance wide (#111359)

--- pkg/registry/apis/provisioning/register.go
+++ pkg/registry/apis/provisioning/register.go
@@ -90,7 +90,8 @@ type APIBuilder struct {
 	// TODO: Set this up in the standalone API server
 	onlyApiServer bool
 
-	allowedTargets []provisioning.SyncTargetType
+	allowedTargets      []provisioning.SyncTargetType
+	allowImageRendering bool
 
 	features   featuremgmt.FeatureToggles
 	usageStats usagestats.Service
@@ -141,6 +142,7 @@ func NewAPIBuilder(
 	extraWorkers []jobs.Worker,
 	jobHistoryConfig *JobHistoryConfig,
 	allowedTargets []provisioning.SyncTargetType,
+	allowImageRendering bool,
 	newStandaloneClientFactoryFunc func(loopbackConfigProvider apiserver.RestConfigProvider) resources.ClientFactory, // optional, only used for standalone apiserver
 ) *APIBuilder {
 	var clients resources.ClientFactory
@@ -169,6 +171,7 @@ func NewAPIBuilder(
 		jobHistoryConfig:    jobHistoryConfig,
 		extraWorkers:        extraWorkers,
 		allowedTargets:      allowedTargets,
+		allowImageRendering: allowImageRendering,
 	}
 
 	for _, builder := range extraBuilders {
@@ -252,6 +255,7 @@ func RegisterAPIService(
 		extraWorkers,
 		createJobHistoryConfigFromSettings(cfg),
 		allowedTargets,
+		cfg.ProvisioningAllowImageRendering,
 		nil,
 	)
 	apiregistration.RegisterAPI(builder)
@@ -570,7 +574,18 @@ func (b *APIBuilder) Validate(ctx context.Context, a admission.Attributes, o adm
 	cfg := repo.Config()
 
 	if !slices.Contains(b.allowedTargets, cfg.Spec.Sync.Target) {
-		return fmt.Errorf("sync target %s is not supported", cfg.Spec.Sync.Target)
+		list = append(list,
+			field.Invalid(
+				field.NewPath("spec", "target"),
+				cfg.Spec.Sync.Target,
+				"sync target is not supported"))
+	}
+
+	if !b.allowImageRendering && cfg.Spec.GitHub != nil && cfg.Spec.GitHub.GenerateDashboardPreviews {
+		list = append(list,
+			field.Invalid(field.NewPath("spec", "generateDashboardPreviews"),
+				cfg.Spec.GitHub.GenerateDashboardPreviews,
+				"image rendering is not enabled"))
 	}
 
 	if a.GetOperation() == admission.Update {
