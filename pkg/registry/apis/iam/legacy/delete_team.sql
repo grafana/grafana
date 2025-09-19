23e04136934e6f998593c651048057f7eb1a7863
Mihai Doarna <mihai.doarna@grafana.com>
2025-09-19 18:21:50 +0300

    remove comment

--- pkg/registry/apis/iam/legacy/delete_team.sql
+++ pkg/registry/apis/iam/legacy/delete_team.sql
@@ -1,4 +1,3 @@
--- Delete from user table (org_user will be handled separately to avoid locking)
 DELETE FROM {{ .Ident .TeamTable }} 
 WHERE uid = {{ .Arg .Command.UID }}
 AND org_id = {{ .Arg .Command.OrgID }}
--- pkg/registry/apis/iam/legacy/testdata/mysql--delete_team-delete_team_basic.sql
+++ pkg/registry/apis/iam/legacy/testdata/mysql--delete_team-delete_team_basic.sql
@@ -1,4 +1,3 @@
--- Delete from user table (org_user will be handled separately to avoid locking)
 DELETE FROM `grafana`.`team` 
 WHERE uid = 'team-1'
 AND org_id = 1
