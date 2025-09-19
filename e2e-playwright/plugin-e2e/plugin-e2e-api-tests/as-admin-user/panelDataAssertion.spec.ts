c078a45ed90a45f86472b8cc4fc24ebf85a2b73f
Erik Sundell <erik.sundell87@gmail.com>
2025-09-19 09:00:56 +0200

    add dashboard constant

--- e2e-playwright/plugin-e2e/plugin-e2e-api-tests/as-admin-user/panelDataAssertion.spec.ts
+++ e2e-playwright/plugin-e2e/plugin-e2e-api-tests/as-admin-user/panelDataAssertion.spec.ts
@@ -4,6 +4,7 @@ import { formatExpectError } from '../errors';
 import { successfulDataQuery } from '../mocks/queries';
 
 const REACT_TABLE_DASHBOARD = { uid: 'U_bZIMRMk' };
+const TABLE_TEST_PANEL_DASHBOARD = { uid: 'pttable' };
 
 test.describe(
   'plugin-e2e-api-tests admin',
