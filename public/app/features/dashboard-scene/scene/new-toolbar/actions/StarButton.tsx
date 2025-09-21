331bfbef8eb258f7c81bc67cb3312997fa350b0a
Ryan McKinley <ryantxu@gmail.com>
2025-09-21 17:24:33 +0300

    prettier

--- public/app/features/dashboard-scene/scene/new-toolbar/actions/StarButton.tsx
+++ public/app/features/dashboard-scene/scene/new-toolbar/actions/StarButton.tsx
@@ -3,11 +3,5 @@ import { StarToolbarButton } from 'app/features/stars/StarToolbarButton';
 import { ToolbarActionProps } from '../types';
 
 export const StarButton = ({ dashboard }: ToolbarActionProps) => {
-  return (
-    <StarToolbarButton
-      group="dashboard.grafana.app"
-      kind="Dashboard"
-      dashboard={dashboard}
-    />
-  );
+  return <StarToolbarButton group="dashboard.grafana.app" kind="Dashboard" dashboard={dashboard} />;
 };
