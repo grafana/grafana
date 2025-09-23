93677553a7ff5e464eb59e283bf188d00bccad22
Mihaly Gyongyosi <mgyongyosi@users.noreply.github.com>
2025-09-23 14:56:16 +0200

    Use the new authorizer for the User resource

--- pkg/services/authz/rbac/mapper.go
+++ pkg/services/authz/rbac/mapper.go
@@ -110,6 +110,7 @@ func NewMapperRegistry() MapperRegistry {
 			"folders": newResourceTranslation("folders", "uid", true, false),
 		},
 		"iam.grafana.app": {
+			"users":           newResourceTranslation("users", "uid", false, true),
 			"serviceaccounts": newResourceTranslation("serviceaccounts", "uid", false, true),
 			// Teams is a special case. We translate user permissions from id to uid based.
 			"teams": newResourceTranslation("teams", "uid", false, true),
