85ae18e6ea30a4afa3b98b1912d3dbe1d5058837
Yunwen Zheng <yunwen.zheng@grafana.com>
2025-09-24 16:50:24 -0400

    FileStatusPage: Disable save button if repo is read only (#111561)

--- public/app/features/provisioning/File/FileStatusPage.tsx
+++ public/app/features/provisioning/File/FileStatusPage.tsx
@@ -28,6 +28,7 @@ export default function FileStatusPage() {
   const path = params['*'] ?? '';
   const file = useGetRepositoryFilesWithPathQuery({ name, path, ref });
   const { isReadOnlyRepo } = useGetResourceRepositoryView({ name });
+  console.log('isReadOnlyRepo', isReadOnlyRepo);
 
   return (
     <Page
