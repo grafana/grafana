21848e6fdde6334771ed4c16a666802e82409aae
Gabriel Mabille <gabriel.mabille@grafana.com>
2025-09-24 14:00:48 +0200

    Missing the most important piece :smile:

---
+++ pkg/services/apiserver/auth/authorizer/service.go
@@ -0,0 +1,35 @@
+package authorizer
+
+import (
+	"context"
+	"errors"
+
+	authzlib "github.com/grafana/authlib/authz"
+	"github.com/grafana/authlib/types"
+
+	"k8s.io/apiserver/pkg/authorization/authorizer"
+)
+
+// Use this only when user authorization is not needed as it's performed downstream (e.g. in the UniStore).
+// This authorizer makes a final decision based on the service permissions only, it does not consider the user permissions.
+func NewServiceAuthorizer() authorizer.Authorizer {
+	return authorizer.AuthorizerFunc(
+		func(ctx context.Context, attr authorizer.Attributes) (authorized authorizer.Decision, reason string, err error) {
+			if !attr.IsResourceRequest() {
+				return authorizer.DecisionNoOpinion, "", nil
+			}
+
+			ident, ok := types.AuthInfoFrom(ctx)
+			if !ok {
+				return authorizer.DecisionDeny, "", errors.New("no identity found for request")
+			}
+
+			res := authzlib.CheckServicePermissions(ident, attr.GetAPIGroup(), attr.GetResource(), attr.GetVerb())
+			if !res.Allowed {
+				return authorizer.DecisionDeny, "unauthorized request", nil
+			}
+
+			return authorizer.DecisionAllow, "", nil
+		},
+	)
+}
