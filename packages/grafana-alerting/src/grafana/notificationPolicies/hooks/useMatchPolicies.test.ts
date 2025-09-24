18caa8035830ee68549697f3cb6c58056b221b39
Gilles De Mey <gilles.de.mey@gmail.com>
2025-09-24 18:33:29 +0200

    typecheck ✅

--- packages/grafana-alerting/src/grafana/notificationPolicies/hooks/useMatchPolicies.test.ts
+++ packages/grafana-alerting/src/grafana/notificationPolicies/hooks/useMatchPolicies.test.ts
@@ -14,6 +14,7 @@ describe('matchInstancesToRouteTrees', () => {
     const treeName = 'test-tree';
     const trees: RoutingTree[] = [
       {
+        kind: 'RoutingTree',
         metadata: { name: treeName },
         spec: {
           defaults: {
@@ -47,6 +48,7 @@ describe('matchInstancesToRouteTrees', () => {
     const treeName = 'test-tree';
     const trees: RoutingTree[] = [
       {
+        kind: 'RoutingTree',
         metadata: { name: treeName },
         spec: {
           defaults: {
