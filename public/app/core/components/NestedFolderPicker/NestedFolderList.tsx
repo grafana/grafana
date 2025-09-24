124da42ad42679007de3ef848ee91e5b3270f58a
Andrej Ocenas <mr.ocenas@gmail.com>
2025-09-24 14:58:37 +0200

    Fix selecting folder on expand

--- public/app/core/components/NestedFolderPicker/NestedFolderList.tsx
+++ public/app/core/components/NestedFolderPicker/NestedFolderList.tsx
@@ -203,6 +203,11 @@ function Row({ index, style: virtualStyles, data }: RowProps) {
             // by using onMouseDown here instead of onClick we can stop focus moving
             // to the button when the user clicks it (via preventDefault + stopPropagation)
             onMouseDown={handleExpand}
+            // Additionally, prevent the click event from bubbling to the row, which would select the folder
+            onClick={(ev) => {
+              ev.preventDefault();
+              ev.stopPropagation();
+            }}
             // tabIndex not needed here because we handle keyboard navigation at the input level
             tabIndex={-1}
             aria-label={isOpen ? `Collapse folder ${item.title}` : `Expand folder ${item.title}`}
