110fcba5329a4a28b134196e4fefd9410b587778
Ryan McKinley <ryantxu@gmail.com>
2025-09-25 14:08:47 +0300

    Dashboards/search: improve openapi spec (#111575)
    Co-authored-by: Clarity-89 <homes89@ukr.net>

--- pkg/registry/apis/dashboard/search.go
+++ pkg/registry/apis/dashboard/search.go
@@ -302,22 +302,20 @@ func (s *SearchHandler) DoSearch(w http.ResponseWriter, r *http.Request) {
 	}
 	searchRequest.Fields = fields
 
-	// A search request can include multiple types, we need to acces the slice directly.
-	types := queryParams["type"]
-	hasDash := len(types) == 0 || slices.Contains(types, "dashboard")
-	hasFolder := len(types) == 0 || slices.Contains(types, "folder")
-	// If both types are present, we need to search both dashboards and folders, by default is nothing is set we also search both.
-	if (hasDash && hasFolder) || (!hasDash && !hasFolder) {
+	// Search dashboards or folders (or both)
+	switch queryParams.Get("type") {
+	case "folder":
+		searchRequest.Options.Key, err = asResourceKey(user.GetNamespace(), folders.RESOURCE)
+	case "dashboard":
+		searchRequest.Options.Key, err = asResourceKey(user.GetNamespace(), dashboardv0alpha1.DASHBOARD_RESOURCE)
+	default:
 		searchRequest.Options.Key, err = asResourceKey(user.GetNamespace(), dashboardv0alpha1.DASHBOARD_RESOURCE)
 		if err == nil {
-			if federate, _ := asResourceKey(user.GetNamespace(), folders.RESOURCE); federate != nil {
+			federate, _ := asResourceKey(user.GetNamespace(), folders.RESOURCE)
+			if federate != nil {
 				searchRequest.Federated = []*resourcepb.ResourceKey{federate}
 			}
 		}
-	} else if hasFolder {
-		searchRequest.Options.Key, err = asResourceKey(user.GetNamespace(), folders.RESOURCE)
-	} else if hasDash {
-		searchRequest.Options.Key, err = asResourceKey(user.GetNamespace(), dashboardv0alpha1.DASHBOARD_RESOURCE)
 	}
 	if err != nil {
 		errhttp.Write(ctx, err, w)
