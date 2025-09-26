94095fccf1e2fd669836a8b579fd770821aadade
Clarity-89 <homes89@ukr.net>
2025-09-26 12:39:09 +0300

    update paths and move scripts

--- packages/grafana-api-clients/src/scripts/generate-rtk-apis.ts
+++ packages/grafana-api-clients/src/scripts/generate-rtk-apis.ts
@@ -7,9 +7,9 @@ const config: ConfigFile = {
   exportName: 'generatedAPI',
 
   outputFiles: {
-    '../public/app/features/migrate-to-cloud/api/endpoints.gen.ts': {
-      schemaFile: '../public/openapi3.json',
-      apiFile: '../public/app/features/migrate-to-cloud/api/baseAPI.ts',
+    '../clients/migrate-to-cloud/endpoints.gen.ts': {
+      schemaFile: '../../../public/openapi3.json',
+      apiFile: '../clients/migrate-to-cloud/baseAPI.ts',
       apiImport: 'baseAPI',
       hooks: true,
       filterEndpoints: [
@@ -34,34 +34,34 @@ const config: ConfigFile = {
         'getResourceDependencies',
       ],
     },
-    '../public/app/features/preferences/api/user/endpoints.gen.ts': {
-      schemaFile: '../public/openapi3.json',
+    '../clients/preferences/user/endpoints.gen.ts': {
+      schemaFile: '../../../public/openapi3.json',
       hooks: true,
-      apiFile: '../public/app/features/preferences/api/user/baseAPI.ts',
+      apiFile: '../clients/preferences/user/baseAPI.ts',
       apiImport: 'baseAPI',
       filterEndpoints: ['getUserPreferences', 'updateUserPreferences', 'patchUserPreferences'],
     },
-    '../public/app/api/clients/iam/v0alpha1/endpoints.gen.ts': {
-      schemaFile: '../data/openapi/iam.grafana.app-v0alpha1.json',
-      apiFile: '../public/app/api/clients/iam/v0alpha1/baseAPI.ts',
+    '../clients/iam/v0alpha1/endpoints.gen.ts': {
+      schemaFile: '../../../data/openapi/iam.grafana.app-v0alpha1.json',
+      apiFile: '../clients/iam/v0alpha1/baseAPI.ts',
       filterEndpoints: ['getDisplayMapping'],
       tag: true,
     },
-    '../public/app/api/clients/provisioning/v0alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/provisioning/v0alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/provisioning.grafana.app-v0alpha1.json',
+    '../clients/provisioning/v0alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/provisioning/v0alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/provisioning.grafana.app-v0alpha1.json',
       filterEndpoints,
       tag: true,
       hooks: true,
     },
-    '../public/app/api/clients/folder/v1beta1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/folder/v1beta1/baseAPI.ts',
-      schemaFile: '../data/openapi/folder.grafana.app-v1beta1.json',
+    '../clients/folder/v1beta1/endpoints.gen.ts': {
+      apiFile: '../clients/folder/v1beta1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/folder.grafana.app-v1beta1.json',
       tag: true,
     },
-    '../public/app/api/clients/advisor/v0alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/advisor/v0alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/advisor.grafana.app-v0alpha1.json',
+    '../clients/advisor/v0alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/advisor/v0alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/advisor.grafana.app-v0alpha1.json',
       filterEndpoints: [
         'createCheck',
         'getCheck',
@@ -73,15 +73,15 @@ const config: ConfigFile = {
       ],
       tag: true,
     },
-    '../public/app/api/clients/playlist/v0alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/playlist/v0alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/playlist.grafana.app-v0alpha1.json',
+    '../clients/playlist/v0alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/playlist/v0alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/playlist.grafana.app-v0alpha1.json',
       filterEndpoints: ['listPlaylist', 'getPlaylist', 'createPlaylist', 'deletePlaylist', 'replacePlaylist'],
       tag: true,
     },
-    '../public/app/api/clients/dashboard/v0alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/dashboard/v0alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/dashboard.grafana.app-v0alpha1.json',
+    '../clients/dashboard/v0alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/dashboard/v0alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/dashboard.grafana.app-v0alpha1.json',
       filterEndpoints: [
         // Do not use any other endpoints from this version
         // If other endpoints are required, they must be used from a newer version of the dashboard API
@@ -90,19 +90,19 @@ const config: ConfigFile = {
       tag: true,
     },
 
-    '../public/app/api/clients/shorturl/v1alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/shorturl/v1alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/shorturl.grafana.app-v1alpha1.json',
+    '../clients/shorturl/v1alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/shorturl/v1alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/shorturl.grafana.app-v1alpha1.json',
       tag: true,
     },
-    '../public/app/api/clients/rules/v0alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/rules/v0alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/rules.alerting.grafana.app-v0alpha1.json',
+    '../clients/rules/v0alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/rules/v0alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/rules.alerting.grafana.app-v0alpha1.json',
       tag: true,
     },
-    '../public/app/api/clients/preferences/v1alpha1/endpoints.gen.ts': {
-      apiFile: '../public/app/api/clients/preferences/v1alpha1/baseAPI.ts',
-      schemaFile: '../data/openapi/preferences.grafana.app-v1alpha1.json',
+    '../clients/preferences/v1alpha1/endpoints.gen.ts': {
+      apiFile: '../clients/preferences/v1alpha1/baseAPI.ts',
+      schemaFile: '../../../data/openapi/preferences.grafana.app-v1alpha1.json',
       tag: true,
       hooks: true,
     },
