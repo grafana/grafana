514eb44245d9dd668b2ea026fb8ddd02d07aff7b
Leon Sorokin <leeoniya@gmail.com>
2025-09-29 16:44:24 -0500

    by-name override matcher UI for annos, and apply anno overrides in PQR

--- packages/grafana-ui/src/components/MatchersUI/utils.ts
+++ packages/grafana-ui/src/components/MatchersUI/utils.ts
@@ -67,6 +67,19 @@ export function useFieldDisplayNames(data: DataFrame[], filter?: (field: Field) 
   }, [data, filter]);
 }
 
+export function useAllFieldDisplayNames(series: DataFrame[], annotations: DataFrame[]) {
+  const seriesNames = useFieldDisplayNames(series);
+  const annoNames = useFieldDisplayNames(annotations);
+
+  return useMemo<FrameFieldsDisplayNames>(() => {
+    return {
+      display: new Set([...seriesNames.display, ...annoNames.display]),
+      raw: new Set([...seriesNames.raw, ...annoNames.raw]),
+      fields: new Map([...seriesNames.fields.entries(), ...annoNames.fields.entries()]),
+    };
+  }, [seriesNames, annoNames]);
+}
+
 /**
  * @internal
  */
