542ec8afc4e399787682b12661c7e9f976510751
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-09-30 12:19:49 +0100

    Export handle request error

--- packages/grafana-api-clients/src/index.ts
+++ packages/grafana-api-clients/src/index.ts
@@ -1,2 +1,3 @@
 export { createBaseQuery, type RequestOptions } from './utils/createBaseQuery';
 export { getAPINamespace } from './utils/utils';
+export { handleRequestError } from './utils/createBaseQuery';
