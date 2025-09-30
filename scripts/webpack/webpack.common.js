5091a8501c58794ee53a5a73244f4c4ada4f7d3b
Jack Westbrook <jack.westbrook@gmail.com>
2025-09-30 14:46:09 +0200

    build(frontend): fix failing build

--- scripts/webpack/webpack.common.js
+++ scripts/webpack/webpack.common.js
@@ -64,7 +64,7 @@ module.exports = {
   ],
   plugins: [
     new webpack.NormalModuleReplacementPlugin(/^@grafana\/schema\/dist\/esm\/(.*)$/, (resource) => {
-      resource.request = resource.request.replace('@grafana/schema/dist/esm', '@grafana/schema/src');
+      resource.request = resource.request.replace('@grafana/schema/dist/esm', '@grafana/schema');
     }),
     new CorsWorkerPlugin(),
     new webpack.ProvidePlugin({
