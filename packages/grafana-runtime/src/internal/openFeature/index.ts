359a61412438e1e71cb190f63a0f7b7fea13cb5f
joshhunt <josh.hunt@grafana.com>
2025-10-02 12:53:38 +0100

    Comments

--- packages/grafana-runtime/src/internal/openFeature/index.ts
+++ packages/grafana-runtime/src/internal/openFeature/index.ts
@@ -8,6 +8,14 @@ import { config } from '../../config';
 export type FeatureFlagName = keyof FeatureToggles;
 
 export async function initOpenFeature() {
+  /**
+   * Note: Currently we don't have a way to override OpenFeature flags for tests or localStorage.
+   * A few improvements we could make:
+   * - When running in tests (unit or e2e?), we could use InMemoryProvider instead
+   * - Use Multi-Provider to combine InMemoryProvider (for localStorage) with OFREPWebProvider
+   *   to allow for overrides https://github.com/open-feature/js-sdk-contrib/tree/main/libs/providers/multi-provider
+   */
+
   const ofProvider = new OFREPWebProvider({
     baseUrl: '/apis/features.grafana.app/v0alpha1/namespaces/' + config.namespace,
     pollInterval: -1, // disable polling
