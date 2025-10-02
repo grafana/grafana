fc9632df61687ef8bf662e4381384cff4c50ca9f
Ryan McKinley <ryantxu@gmail.com>
2025-10-02 18:25:37 +0300

    starts a stub server

--- pkg/registry/apps/history/register.go
+++ pkg/registry/apps/history/register.go
@@ -6,8 +6,8 @@ import (
 	"github.com/grafana/grafana-app-sdk/app"
 	appsdkapiserver "github.com/grafana/grafana-app-sdk/k8s/apiserver"
 	"github.com/grafana/grafana-app-sdk/simple"
-	"github.com/grafana/grafana/apps/correlations/pkg/apis"
-	correlationsapp "github.com/grafana/grafana/apps/correlations/pkg/app"
+	"github.com/grafana/grafana/apps/history/pkg/apis"
+	historyapp "github.com/grafana/grafana/apps/history/pkg/app"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/setting"
 )
@@ -28,7 +28,7 @@ func RegisterAppInstaller(
 	installer := &AppInstaller{
 		cfg: cfg,
 	}
-	provider := simple.NewAppProvider(apis.LocalManifest(), nil, correlationsapp.New)
+	provider := simple.NewAppProvider(apis.LocalManifest(), nil, historyapp.New)
 
 	appConfig := app.Config{
 		KubeConfig:   restclient.Config{}, // this will be overridden by the installer's InitializeApp method
