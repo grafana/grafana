192b05902f38d3d8e49be24a7ce1fe8983df6f8c
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-10-03 14:52:35 +0100

    Remove header overrides for advisor

--- public/app/api/clients/advisor/v0alpha1/index.ts
+++ public/app/api/clients/advisor/v0alpha1/index.ts
@@ -1,34 +1,6 @@
 import { generatedAPI } from './endpoints.gen';
 
-export const advisorAPIv0alpha1 = generatedAPI.enhanceEndpoints({
-  endpoints: {
-    // Need to mutate the generated query to set the Content-Type header correctly
-    updateCheck: (endpointDefinition) => {
-      const originalQuery = endpointDefinition.query;
-      if (originalQuery) {
-        endpointDefinition.query = (requestOptions) => ({
-          ...originalQuery(requestOptions),
-          headers: {
-            'Content-Type': 'application/json-patch+json',
-          },
-          body: JSON.stringify(requestOptions.patch),
-        });
-      }
-    },
-    updateCheckType: (endpointDefinition) => {
-      const originalQuery = endpointDefinition.query;
-      if (originalQuery) {
-        endpointDefinition.query = (requestOptions) => ({
-          ...originalQuery(requestOptions),
-          headers: {
-            'Content-Type': 'application/json-patch+json',
-          },
-          body: JSON.stringify(requestOptions.patch),
-        });
-      }
-    },
-  },
-});
+export const advisorAPIv0alpha1 = generatedAPI.enhanceEndpoints({});
 export const {
   useGetCheckQuery,
   useListCheckQuery,
