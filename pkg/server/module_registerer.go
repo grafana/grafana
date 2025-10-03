c5663c2ada7e10c08a554df5dc70d277b90ac82f
Gabriel Mabille <gabriel.mabille@grafana.com>
2025-10-03 15:02:39 +0200

    wip authz service

--- pkg/server/module_registerer.go
+++ pkg/server/module_registerer.go
@@ -4,15 +4,15 @@ import (
 	"github.com/grafana/grafana/pkg/modules"
 )
 
+// ModuleRegisterer is used to inject enterprise dskit modules into
+// the target module manager.
 type ModuleRegisterer interface {
 	RegisterModules(manager modules.Registry)
 }
 
 type noopModuleRegisterer struct{}
 
-func (noopModuleRegisterer) RegisterModules(manager modules.Registry) {
-	// No-op
-}
+func (noopModuleRegisterer) RegisterModules(manager modules.Registry) {}
 
 func ProvideNoopModuleRegisterer() ModuleRegisterer {
 	return &noopModuleRegisterer{}
