7937606b159c0f4c2cc96711e3ba120c7d76834e
Peter Štibraný <pstibrany@gmail.com>
2025-10-03 12:34:06 +0200

    Make test less flaky by allowing for higher diff variance.

--- pkg/storage/unified/search/bleve_test.go
+++ pkg/storage/unified/search/bleve_test.go
@@ -1493,8 +1493,8 @@ func TestConcurrentIndexUpdateAndSearchWithIndexMinUpdateInterval(t *testing.T) 
 					if rvDiff == 0 {
 						// OK
 					} else {
-						// Allow returned RV to be within 5% of minInterval.
-						require.InDelta(t, minInterval.Milliseconds(), rvDiff, float64(minInterval.Milliseconds())*0.05)
+						// Allow returned RV to be within 10% of minInterval.
+						require.InDelta(t, minInterval.Milliseconds(), rvDiff, float64(minInterval.Milliseconds())*0.10)
 					}
 				}
 
