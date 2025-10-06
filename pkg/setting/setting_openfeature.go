a595f4d489fd0ab0e8a5ec0864d2d463a456446c
Tania B. <10127682+undef1nd@users.noreply.github.com>
2025-10-06 15:27:18 +0200

    Update targeting key to namespace

--- pkg/setting/setting_openfeature.go
+++ pkg/setting/setting_openfeature.go
@@ -24,7 +24,7 @@ func (cfg *Cfg) readOpenFeatureSettings() error {
 	config := cfg.Raw.Section("feature_toggles.openfeature")
 	cfg.OpenFeature.APIEnabled = config.Key("enable_api").MustBool(true)
 	cfg.OpenFeature.ProviderType = config.Key("provider").MustString(StaticProviderType)
-	cfg.OpenFeature.TargetingKey = config.Key("targetingKey").MustString(cfg.AppURL)
+	cfg.OpenFeature.TargetingKey = config.Key("targetingKey").MustString(fmt.Sprintf("stacks-%s", cfg.StackID))
 
 	strURL := config.Key("url").MustString("")
 
