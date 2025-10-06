13cc38ea83fd8619663c31b3bbccf59f7d2e089e
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-10-06 14:27:25 +0100

    Simplify tags

--- public/app/api/clients/dashboard/v0alpha1/index.ts
+++ public/app/api/clients/dashboard/v0alpha1/index.ts
@@ -1,7 +1,6 @@
-import { generatedAPI, addTagTypes } from '@grafana/api-clients/rtkq/dashboard/v0alpha1';
+import { generatedAPI } from '@grafana/api-clients/rtkq/dashboard/v0alpha1';
 
 export const dashboardAPIv0alpha1 = generatedAPI.enhanceEndpoints({
-  addTagTypes: [...addTagTypes, 'Folder', 'Dashboard'],
   endpoints: {
     getSearch: (endpointDefinition) => {
       endpointDefinition.providesTags = ['Search', 'Folder', 'Dashboard'];
