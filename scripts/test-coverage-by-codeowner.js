2fddd741d68814a038c50137a1e494c814893081
Jesse David Peterson <jesdavpet@users.noreply.github.com>
2025-10-07 09:17:45 -0400

    refactor(script): simplify with fs promises API for consistency

--- scripts/test-coverage-by-codeowner.js
+++ scripts/test-coverage-by-codeowner.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cp = require('node:child_process');
-const fs = require('node:fs');
+const { readFile } = require('node:fs/promises');
 
 const { CODEOWNERS_JSON_PATH: CODEOWNERS_MANIFEST_CODEOWNERS_PATH } = require('./codeowners-manifest/constants.js');
 
@@ -13,8 +13,8 @@ const JEST_CONFIG_PATH = 'jest.config.codeowner.js';
  * @param {string} codeownersPath - Path to the teams.json file
  * @param {string} jestConfigPath - Path to the Jest config file
  */
-function runTestCoverageByCodeowner(codeownerName, codeownersPath, jestConfigPath) {
-  const codeownersJson = fs.readFileSync(codeownersPath);
+async function runTestCoverageByCodeowner(codeownerName, codeownersPath, jestConfigPath) {
+  const codeownersJson = await readFile(codeownersPath, 'utf8');
   const codeowners = JSON.parse(codeownersJson);
 
   if (!codeowners.includes(codeownerName)) {
@@ -27,25 +27,27 @@ function runTestCoverageByCodeowner(codeownerName, codeownersPath, jestConfigPat
 }
 
 if (require.main === module) {
-  const codeownerName = process.argv[2];
+  (async () => {
+    try {
+      const codeownerName = process.argv[2];
 
-  if (!codeownerName) {
-    console.error('Codeowner argument is required ...');
-    console.error('Usage: yarn test:coverage:by-codeowner @grafana/team-name');
-    process.exit(1);
-  }
+      if (!codeownerName) {
+        console.error('Codeowner argument is required ...');
+        console.error('Usage: yarn test:coverage:by-codeowner @grafana/team-name');
+        process.exit(1);
+      }
 
-  try {
-    console.log(`🧪 Running test coverage for codeowner: ${codeownerName}`);
-    runTestCoverageByCodeowner(codeownerName, CODEOWNERS_MANIFEST_CODEOWNERS_PATH, JEST_CONFIG_PATH);
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      console.error(`Could not read ${CODEOWNERS_MANIFEST_CODEOWNERS_PATH} ...`);
-    } else {
-      console.error(e.message);
+      console.log(`🧪 Running test coverage for codeowner: ${codeownerName}`);
+      await runTestCoverageByCodeowner(codeownerName, CODEOWNERS_MANIFEST_CODEOWNERS_PATH, JEST_CONFIG_PATH);
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        console.error(`Could not read ${CODEOWNERS_MANIFEST_CODEOWNERS_PATH} ...`);
+      } else {
+        console.error(e.message);
+      }
+      process.exit(1);
     }
-    process.exit(1);
-  }
+  })();
 }
 
 module.exports = { runTestCoverageByCodeowner };
