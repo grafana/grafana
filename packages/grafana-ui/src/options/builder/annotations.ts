4293bdfe67986d5f9c12fd9a91c44f8352fc3ebb
Galen <galen.kistler@grafana.com>
2025-10-08 15:25:27 -0500

    feat: add multi-lane annotation options to supported panels

---
+++ packages/grafana-ui/src/options/builder/annotations.ts
@@ -0,0 +1,23 @@
+import { PanelOptionsEditorBuilder } from '@grafana/data';
+import { t } from '@grafana/i18n';
+
+/**
+ * Adds common text control options to a visualization options
+ * @param builder
+ * @param defaultValue
+ * @public
+ */
+export function addAnnotationOptions<T>(builder: PanelOptionsEditorBuilder<T>, defaultValue = false) {
+  const category = [t('grafana-ui.builder.annotations', 'Annotations')];
+
+  builder.addBooleanSwitch({
+    path: 'annotations.multiLane',
+    category,
+    name: t('grafana-ui.builder.annotations.multi-lane-name', 'Enable multi-lane annotations'),
+    description: t(
+      'grafana-ui.builder.annotations.multi-lane-desc',
+      'Breaks each annotation frame into a separate row in the visualization'
+    ),
+    defaultValue,
+  });
+}
