634ea278898ca9a7a3268052d76aa55b17060864
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-10-08 05:25:57 -0600

    Provisioning: Do full sync on resync period when needed (#112144)

--- apps/provisioning/pkg/repository/workflows.go
+++ apps/provisioning/pkg/repository/workflows.go
@@ -1,9 +1,12 @@
 package repository
 
 import (
+	"strings"
+
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	provisioning "github.com/grafana/grafana/apps/provisioning/pkg/apis/provisioning/v0alpha1"
+	"github.com/grafana/grafana/apps/provisioning/pkg/safepath"
 )
 
 func IsWriteAllowed(repo *provisioning.Repository, ref string) error {
@@ -39,4 +42,33 @@ func IsWriteAllowed(repo *provisioning.Repository, ref string) error {
 	default:
 		return nil
 	}
+}
+
+// CanUseIncrementalSync checks if an incremental sync can be performed or if a full sync is needed,
+// given a list of deleted file paths. It will return true if a .keep file is deleted without
+// other files being deleted in the same directory. This is because the folder will not be a part of the
+// deleted files, and the .keep file is not a resource in grafana, so we can't get the folder uid.
+// A full sync will clean that up.
+func CanUseIncrementalSync(deletedPaths []string) bool {
+	dirsWithKeepDeletes := make(map[string]struct{})
+	dirsWithOtherDeletes := make(map[string]struct{})
+
+	for _, path := range deletedPaths {
+		dir := safepath.Dir(path)
+		if strings.HasSuffix(path, ".keep") {
+			dirsWithKeepDeletes[dir] = struct{}{}
+		} else {
+			dirsWithOtherDeletes[dir] = struct{}{}
+		}
+	}
+
+	// if there are any .keep files deleted that don't have other files deleted in the same folder,
+	// we need to do a full sync
+	for dir := range dirsWithKeepDeletes {
+		if _, exists := dirsWithOtherDeletes[dir]; !exists {
+			return false
+		}
+	}
+
+	return true
 }
