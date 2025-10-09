7ed641ccb2a98f0121a1b5c70c8648b9cdeb2c46
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-10-09 11:06:18 +0100

    Add helper method for resetting grafana searcher

--- public/app/features/search/service/searcher.ts
+++ public/app/features/search/service/searcher.ts
@@ -23,3 +23,14 @@ export function getGrafanaSearcher(): GrafanaSearcher {
   }
   return searcher!;
 }
+
+/**
+ * Testing only - otherwise tests will use the same searcher instance, making it hard to test unified search vs legacy
+ * @deprecated Don't use this other than in tests!
+ */
+export function resetGrafanaSearcher() {
+  if (process.env.NODE_ENV !== 'test') {
+    throw new Error('resetGrafanaSearcher can only be used in tests');
+  }
+  searcher = undefined;
+}
