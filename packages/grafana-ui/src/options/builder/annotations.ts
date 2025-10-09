731f755e5c10dfdf1ec3ed6430609d3564c12de9
Galen <galen.kistler@grafana.com>
2025-10-09 07:53:20 -0500

    chore: remove unused prop

--- packages/grafana-ui/src/options/builder/annotations.ts
+++ packages/grafana-ui/src/options/builder/annotations.ts
@@ -4,10 +4,9 @@ import { t } from '@grafana/i18n';
 /**
  * Adds common text control options to a visualization options
  * @param builder
- * @param defaultValue
  * @public
  */
-export function addAnnotationOptions<T>(builder: PanelOptionsEditorBuilder<T>, defaultValue = false) {
+export function addAnnotationOptions<T>(builder: PanelOptionsEditorBuilder<T>) {
   const category = [t('grafana-ui.builder.annotations', 'Annotations')];
 
   builder.addBooleanSwitch({
@@ -18,6 +17,6 @@ export function addAnnotationOptions<T>(builder: PanelOptionsEditorBuilder<T>, d
       'grafana-ui.builder.annotations.multi-lane-desc',
       'Breaks each annotation frame into a separate row in the visualization'
     ),
-    defaultValue,
+    defaultValue: false,
   });
 }
