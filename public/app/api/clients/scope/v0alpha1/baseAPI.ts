5f40d6c1bcb2fc20176537c5b82e64a0bc3830b1
Matheus Macabu <macabu.matheus@gmail.com>
2025-10-09 11:48:13 +0200

    Scopes: Add TS API client

---
+++ public/app/api/clients/scope/v0alpha1/baseAPI.ts
@@ -0,0 +1,14 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+
+import { createBaseQuery } from 'app/api/createBaseQuery';
+import { getAPIBaseURL } from 'app/api/utils';
+
+export const BASE_URL = getAPIBaseURL('scope.grafana.app', 'v0alpha1');
+
+export const api = createApi({
+  reducerPath: 'scopeAPIv0alpha1',
+  baseQuery: createBaseQuery({
+    baseURL: BASE_URL,
+  }),
+  endpoints: () => ({}),
+});
