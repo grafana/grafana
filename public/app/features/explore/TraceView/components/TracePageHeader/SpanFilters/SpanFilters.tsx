ed8f3821846ad225387db31d5dfe6f2e84cf3482
Andre Pereira <adrapereira@gmail.com>
2025-10-10 14:24:17 +0100

    Fix types

--- public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
+++ public/app/features/explore/TraceView/components/TracePageHeader/SpanFilters/SpanFilters.tsx
@@ -89,13 +89,6 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
     [search, setSearch]
   );
 
-  const setShowCriticalPathSpansOnly = useCallback(
-    (showCriticalPathSpansOnly: boolean) => {
-      setSearch({ ...search, criticalPathOnly: showCriticalPathSpansOnly });
-    },
-    [search, setSearch]
-  );
-
   if (!trace) {
     return null;
   }
@@ -276,12 +269,10 @@ export const SpanFilters = memo((props: SpanFilterProps) => {
           search={search}
           spanFilterMatches={spanFilterMatches}
           setShowSpanFilterMatchesOnly={setShowSpanFilterMatchesOnly}
-          setShowCriticalPathSpansOnly={setShowCriticalPathSpansOnly}
           setFocusedSpanIdForSearch={setFocusedSpanIdForSearch}
           focusedSpanIndexForSearch={focusedSpanIndexForSearch}
           setFocusedSpanIndexForSearch={setFocusedSpanIndexForSearch}
           datasourceType={datasourceType}
-          clear={clear}
           showSpanFilters={showSpanFilters}
         />
       </Collapse>
