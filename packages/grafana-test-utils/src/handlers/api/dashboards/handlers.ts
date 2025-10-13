1e399811cb2ac1d6bfdc28f172d0c8fbb1f13293
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-10-13 15:52:32 +0100

    Add mock handlers for dashboard logic

---
+++ packages/grafana-test-utils/src/handlers/api/dashboards/handlers.ts
@@ -0,0 +1,34 @@
+import { HttpResponse, http } from 'msw';
+
+import { wellFormedTree } from '../../../fixtures/folders';
+
+const [mockTree] = wellFormedTree();
+
+const getDashboardHandler = () =>
+  http.get<{ uid: string }>('/api/dashboards/uid/:uid', ({ params, request }) => {
+    const { uid } = params;
+    const dashboard = mockTree.find((v) => v.item.uid === uid);
+
+    if (!dashboard || dashboard.item.kind !== 'dashboard') {
+      return HttpResponse.json({ message: 'Dashboard not found' }, { status: 404 });
+    }
+
+    const { item } = dashboard;
+
+    const parentFolder = mockTree.find((v) => v.item.kind === 'folder' && v.item.uid === item.parentUID);
+
+    return HttpResponse.json({
+      meta: {
+        folderTitle: parentFolder?.item.title,
+        folderUid: parentFolder?.item.uid,
+      },
+      dashboard: {
+        title: item.title,
+        uid: item.uid,
+      },
+    });
+  });
+
+const handlers = [getDashboardHandler()];
+
+export default handlers;
