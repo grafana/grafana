2a18e8ce0ad665c376a4b02ff035e310f94f1a0c
Matheus Macabu <macabu@users.noreply.github.com>
2025-10-14 09:21:54 +0200

    E2E: Use standalone Image Renderer for tests instead of plugin (#112266)
    * E2E: Use standalone Image Renderer for tests instead of plugin
    
    * E2E: Allow configuring image renderer version if needed

--- e2e/internal/cmd/cypress/cmd.go
+++ e2e/internal/cmd/cypress/cmd.go
@@ -109,10 +109,15 @@ func NewCmd() *cli.Command {
 			},
 			&cli.BoolFlag{
 				Name:     "image-renderer",
-				Usage:    "Install the image renderer plugin (requires --start-grafana)",
+				Usage:    "Start the image renderer Docker container (requires --start-grafana)",
 				Category: "Grafana Server",
 			},
-
+			&cli.StringFlag{
+				Name:     "image-renderer-version",
+				Usage:    "When enabling the image renderer, which version to use",
+				Value:    "latest",
+				Category: "Grafana Server",
+			},
 			&cli.StringFlag{
 				Name:      "suite",
 				Usage:     "Path to the suite to run (e.g. './e2e/dashboards-suite')",
@@ -194,7 +199,10 @@ func runAction(ctx context.Context, c *cli.Command) error {
 			cmd.Env = os.Environ()
 			cmd.Env = append(cmd.Env, fmt.Sprintf("TZ=%s", c.String("timezone")))
 			if c.Bool("image-renderer") {
-				cmd.Env = append(cmd.Env, "INSTALL_IMAGE_RENDERER=true")
+				cmd.Env = append(cmd.Env, "START_IMAGE_RENDERER=true")
+			}
+			if c.String("image-renderer-version") != "" {
+				cmd.Env = append(cmd.Env, fmt.Sprintf("IMAGE_RENDERER_VERSION=%s", c.String("image-renderer-version")))
 			}
 			cmd.Stdout = prefixGrafana(os.Stdout)
 			cmd.Stderr = prefixGrafana(os.Stderr)
