d8fd006ca6b1baf5beb0c4b7373c2aceaf8b70bd
Torkel Ödegaard <torkel@grafana.com>
2025-10-17 09:17:47 +0200

    Stat/BarGauge: Border radius tweak

--- packages/grafana-ui/src/components/BarGauge/BarGauge.tsx
+++ packages/grafana-ui/src/components/BarGauge/BarGauge.tsx
@@ -195,7 +195,7 @@ export class BarGauge extends PureComponent<Props> {
       const currentValue = minValue + (valueRange / cellCount) * i;
       const cellColor = getCellColor(currentValue, value, display);
       const cellStyles: CSSProperties = {
-        borderRadius: theme.shape.radius.default,
+        borderRadius: '2px',
       };
 
       if (cellColor.isLit) {
@@ -525,7 +525,7 @@ export function getBasicAndGradientStyles(props: Props): BasicAndGradientStyles 
   };
 
   const barStyles: CSSProperties = {
-    borderRadius: theme.shape.radius.default,
+    borderRadius: theme.shape.radius.sm,
     position: 'relative',
   };
 
@@ -533,7 +533,7 @@ export function getBasicAndGradientStyles(props: Props): BasicAndGradientStyles 
     background: theme.colors.background.secondary,
     flexGrow: 1,
     display: 'flex',
-    borderRadius: theme.shape.radius.default,
+    borderRadius: theme.shape.radius.sm,
     position: 'relative',
   };
 
