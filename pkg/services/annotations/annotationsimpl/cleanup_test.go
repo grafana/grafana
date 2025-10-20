49493124db207b35eda78a151e2e1b0e4aea10ae
Serge Zaitsev <serge.zaitsev@grafana.com>
2025-10-20 16:44:53 +0200

    pass registerer into cleanup test

--- pkg/services/annotations/annotationsimpl/cleanup_test.go
+++ pkg/services/annotations/annotationsimpl/cleanup_test.go
@@ -129,7 +129,7 @@ func TestIntegrationAnnotationCleanUp(t *testing.T) {
 
 			cfg := setting.NewCfg()
 			cfg.AnnotationCleanupJobBatchSize = int64(test.annotationCleanupJobBatchSize)
-			cleaner := ProvideCleanupService(fakeSQL, cfg)
+			cleaner := ProvideCleanupService(fakeSQL, cfg, prometheus.NewRegistry())
 			affectedAnnotations, affectedAnnotationTags, err := cleaner.Run(context.Background(), test.cfg)
 			require.NoError(t, err)
 
