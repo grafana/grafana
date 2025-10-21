312f4ebe4545e96e9d5a01368308cbcfc43d414c
Ashley Harrison <ashley.harrison@grafana.com>
2025-10-21 17:33:38 +0100

    Chore: convert some class components to functional (#112661)
    * convert BigValue to functional component
    
    * convert AbstractList
    
    * Revert "convert AbstractList"
    
    This reverts commit 673c22a81db6b59935f83bb27b8afbb0b6805fe3.
    
    * ignore and deprecate remaining list components
    
    * convert NotFoundDisplay
    
    * convert rectangle
    
    * fix some test components
    
    * convert thresholds.tsx
    
    * convert SpanGraph to functional component
    
    * convert InviteeRow to functional component
    
    * converted InviteesTable to functional component
    
    * convert PanelPluginError to functional components
    
    * convert UserOrganizations
    
    * convert UserSessions to a functional component
    
    * convert UserTeams to a functional component
    
    * convert CloudMonitoringCheatSheet to a functional component
    
    * convert ConfigEditor a react functional component
    
    * convert TestInfoTab to a functional component
    
    * convert DebugPanel to a functional component
    
    * fix types + BigValue story

--- packages/grafana-ui/src/components/List/List.tsx
+++ packages/grafana-ui/src/components/List/List.tsx
@@ -3,6 +3,8 @@ import { PureComponent } from 'react';
 import { ListProps, AbstractList } from './AbstractList';
 
 /** @deprecated Use ul/li/arr.map directly instead */
+// no point converting, this is deprecated
+// eslint-disable-next-line react-prefer-function-component/react-prefer-function-component
 export class List<T> extends PureComponent<ListProps<T>> {
   render() {
     return <AbstractList {...this.props} />;
