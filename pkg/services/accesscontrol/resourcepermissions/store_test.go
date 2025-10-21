a0c053a0cd2763076c9a0fc8de72db022a81e8d0
Matheus Macabu <macabu.matheus@gmail.com>
2025-10-21 17:23:18 +0200

    Also remove println

--- pkg/services/accesscontrol/resourcepermissions/store_test.go
+++ pkg/services/accesscontrol/resourcepermissions/store_test.go
@@ -907,7 +907,6 @@ func TestIntegrationStore_setResourcePermission(t *testing.T) {
 
 			// Get permissions directly from DB to verify
 			permissions := retrievePermissionsHelper(store, t)
-			fmt.Println(permissions)
 
 			require.ElementsMatch(t, test.expectedPermissions, permissions)
 		})
