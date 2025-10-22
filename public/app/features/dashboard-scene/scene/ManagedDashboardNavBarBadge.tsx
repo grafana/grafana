fdc8fcebe4fa3fe45829e127c90d16a8635f8b21
Clarity-89 <homes89@ukr.net>
2025-10-22 15:18:41 +0300

    Provisioning: Display manager kind

--- public/app/features/dashboard-scene/scene/ManagedDashboardNavBarBadge.tsx
+++ public/app/features/dashboard-scene/scene/ManagedDashboardNavBarBadge.tsx
@@ -1,26 +1,35 @@
+import { t } from '@grafana/i18n';
 import { Badge } from '@grafana/ui';
-import { AnnoKeyManagerIdentity, AnnoKeyManagerKind, ManagerKind } from 'app/features/apiserver/types';
-import { DashboardMeta } from 'app/types/dashboard';
+import { ManagerKind } from 'app/features/apiserver/types';
 
-export default function ManagedDashboardNavBarBadge({ meta }: { meta: DashboardMeta }) {
-  const obj = meta.k8s;
-  if (!obj?.annotations) {
-    return;
+import { DashboardScene } from './DashboardScene';
+
+export const ManagedDashboardNavBarBadge = ({ dashboard }: { dashboard: DashboardScene }) => {
+  if (!dashboard.state.meta.k8s?.annotations) {
+    return null;
   }
 
-  let text = 'Provisioned';
-  const kind = obj.annotations?.[AnnoKeyManagerKind];
-  const id = obj.annotations?.[AnnoKeyManagerIdentity];
+  let text;
+  const kind = dashboard.getManagerKind();
+  const id = dashboard.getManagerIdentity();
+
   switch (kind) {
     case ManagerKind.Terraform:
-      text = 'Terraform';
+      text = t('dashboard-scene.managed-badge.terraform', 'Managed by: Terraform');
       break;
     case ManagerKind.Kubectl:
-      text = 'Kubectl';
+      text = t('dashboard-scene.managed-badge.kubectl', 'Managed by: Kubectl');
       break;
     case ManagerKind.Plugin:
-      text = `Plugin: ${id}`;
+      text = t('dashboard-scene.managed-badge.plugin', 'Managed by: Plugin: {{id}}', { id });
       break;
+    case ManagerKind.Repo:
+      text = t('dashboard-scene.managed-badge.repository', 'Managed by: Repository');
+      break;
+    default:
+      console.error('Unknown kind ' + kind);
+      return null;
   }
+
   return <Badge color="purple" icon="exchange-alt" tooltip={text} key="provisioned-dashboard-button-badge" />;
-}
+};
