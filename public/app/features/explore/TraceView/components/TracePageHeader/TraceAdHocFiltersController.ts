5c645d822f62049ebffc33bde87f6f3263d4111a
Andre Pereira <adrapereira@gmail.com>
2025-10-22 14:17:39 +0100

    Remove copyright

--- public/app/features/explore/TraceView/components/TracePageHeader/TraceAdHocFiltersController.ts
+++ public/app/features/explore/TraceView/components/TracePageHeader/TraceAdHocFiltersController.ts
@@ -1,18 +1,3 @@
-// Copyright (c) 2025 Grafana Labs
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import { isEqual } from 'lodash';
 
 import { SelectableValue, toOption, TraceSearchProps } from '@grafana/data';
