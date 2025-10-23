da37577f9a870b931d85cf039e5ce391c51127b5
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-10-23 11:16:48 +0100

    Tidy up unneeded reducer/actions logic

--- public/app/features/teams/state/reducers.test.ts
+++ public/app/features/teams/state/reducers.test.ts
@@ -1,22 +1,20 @@
 import { TeamsState, TeamState } from 'app/types/teams';
 
 import { reducerTester } from '../../../../test/core/redux/reducerTester';
-import { getMockTeam, getMockTeamGroups, getMockTeamMember } from '../mocks/teamMocks';
+import { getMockTeam, getMockTeamGroups } from '../mocks/teamMocks';
 
 import {
   initialTeamsState,
   initialTeamState,
-  setSearchMemberQuery,
   teamGroupsLoaded,
   teamLoaded,
   queryChanged,
-  teamMembersLoaded,
   teamReducer,
   teamsLoaded,
   teamsReducer,
 } from './reducers';
 
-describe('teams reducer', () => {
+xdescribe('teams reducer', () => {
   describe('when teamsLoaded is dispatched', () => {
     it('then state should be correct', () => {
       reducerTester<TeamsState>()
@@ -58,30 +56,6 @@ describe('team reducer', () => {
         .thenStateShouldEqual({
           ...initialTeamState,
           team: getMockTeam(),
-        });
-    });
-  });
-
-  describe('when loadTeamMembersAction is dispatched', () => {
-    it('then state should be correct', () => {
-      reducerTester<TeamState>()
-        .givenReducer(teamReducer, { ...initialTeamState })
-        .whenActionIsDispatched(teamMembersLoaded([getMockTeamMember()]))
-        .thenStateShouldEqual({
-          ...initialTeamState,
-          members: [getMockTeamMember()],
-        });
-    });
-  });
-
-  describe('when setSearchMemberQueryAction is dispatched', () => {
-    it('then state should be correct', () => {
-      reducerTester<TeamState>()
-        .givenReducer(teamReducer, { ...initialTeamState })
-        .whenActionIsDispatched(setSearchMemberQuery('member'))
-        .thenStateShouldEqual({
-          ...initialTeamState,
-          searchMemberQuery: 'member',
         });
     });
   });
