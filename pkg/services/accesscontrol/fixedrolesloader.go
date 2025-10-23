f489a49d3cfb1ac265a3a9b57d0a8a958e431a85
grambbledook <denis.vodopianov@grafana.com>
2025-10-23 18:14:07 +0200

    add more nolints

--- pkg/services/accesscontrol/fixedrolesloader.go
+++ pkg/services/accesscontrol/fixedrolesloader.go
@@ -48,6 +48,7 @@ func (l *FixedRolesLoader) running(ctx context.Context) error {
 }
 
 func (l *FixedRolesLoader) IsDisabled() bool {
+	//nolint:staticcheck // using deprecated FFS service for backward compatibility
 	return !l.features.IsEnabledGlobally(featuremgmt.FlagPluginStoreServiceLoading)
 }
 
