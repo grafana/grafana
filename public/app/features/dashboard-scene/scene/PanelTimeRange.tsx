61683d5401758e55983c5bd2731e6ef686bad759
Torkel Ödegaard <torkel@grafana.com>
2025-10-24 09:31:58 +0200

    Something is working

--- public/app/features/dashboard-scene/scene/PanelTimeRange.tsx
+++ public/app/features/dashboard-scene/scene/PanelTimeRange.tsx
@@ -1,22 +1,57 @@
+/* eslint @grafana/i18n/no-untranslated-strings: 0 */
 import { css } from '@emotion/css';
+import { capitalize } from 'lodash';
+import { useState } from 'react';
+import { of } from 'rxjs';
 
-import { dateMath, getDefaultTimeRange, GrafanaTheme2, rangeUtil, TimeRange } from '@grafana/data';
 import {
+  DataQuery,
+  DataQueryRequest,
+  dateMath,
+  dateTime,
+  DateTime,
+  getDefaultTimeRange,
+  GrafanaTheme2,
+  rangeUtil,
+  TimeRange,
+} from '@grafana/data';
+import {
+  ExtraQueryDataProcessor,
+  ExtraQueryDescriptor,
   SceneComponentProps,
+  SceneDataQuery,
   sceneGraph,
   SceneTimeRangeLike,
   SceneTimeRangeState,
   SceneTimeRangeTransformerBase,
   VariableDependencyConfig,
 } from '@grafana/scenes';
-import { Icon, PanelChrome, TimePickerTooltip, Tooltip, useStyles2 } from '@grafana/ui';
+import {
+  Badge,
+  Box,
+  Button,
+  Drawer,
+  Icon,
+  Input,
+  Label,
+  PanelChrome,
+  Stack,
+  TimePickerTooltip,
+  Tooltip,
+  Field,
+  useStyles2,
+  Combobox,
+} from '@grafana/ui';
 import { TimeOverrideResult } from 'app/features/dashboard/utils/panel';
 
 export interface PanelTimeRangeState extends SceneTimeRangeState {
+  enabled?: boolean;
   timeFrom?: string;
   timeShift?: string;
   hideTimeOverride?: boolean;
   timeInfo?: string;
+  compareWith?: string;
+  isSettingsOpen?: boolean;
 }
 
 export class PanelTimeRange extends SceneTimeRangeTransformerBase<PanelTimeRangeState> implements SceneTimeRangeLike {
@@ -61,13 +96,44 @@ export class PanelTimeRange extends SceneTimeRangeTransformerBase<PanelTimeRange
       to: timeRange.raw.to.toString(),
     });
   }
+
   protected ancestorTimeRangeChanged(timeRange: SceneTimeRangeState): void {
     const overrideResult = this.getTimeOverride(timeRange.value);
     this.setState({ value: overrideResult.timeRange, timeInfo: overrideResult.timeInfo });
   }
 
+  // Get a time shifted request to compare with the primary request.
+  public getExtraQueries(request: DataQueryRequest): ExtraQueryDescriptor[] {
+    const extraQueries: ExtraQueryDescriptor[] = [];
+    const compareRange = getCompareTimeRange(request.range, this.state.compareWith);
+    if (!compareRange) {
+      return extraQueries;
+    }
+
+    const targets = request.targets.filter((query: SceneDataQuery) => query.timeRangeCompare !== false);
+    if (targets.length) {
+      extraQueries.push({
+        req: {
+          ...request,
+          targets,
+          range: compareRange,
+        },
+        processor: timeShiftAlignmentProcessor,
+      });
+    }
+    return extraQueries;
+  }
+
+  // The query runner should rerun the comparison query if the compareWith value has changed and there are queries that haven't opted out of TWC
+  public shouldRerun(prev: PanelTimeRangeState, next: PanelTimeRangeState, queries: SceneDataQuery[]): boolean {
+    return (
+      prev.compareWith !== next.compareWith && queries.find((query) => query.timeRangeCompare !== false) !== undefined
+    );
+  }
+
   private getTimeOverride(parentTimeRange: TimeRange): TimeOverrideResult {
-    const { timeFrom, timeShift } = this.state;
+    const { timeFrom, timeShift, compareWith } = this.state;
+    const infoBlocks = [];
     const newTimeData = { timeInfo: '', timeRange: parentTimeRange };
 
     if (timeFrom) {
@@ -82,12 +148,12 @@ export class PanelTimeRange extends SceneTimeRangeTransformerBase<PanelTimeRange
       // Only evaluate if the timeFrom if parent time is relative
       if (rangeUtil.isRelativeTimeRange(parentTimeRange.raw)) {
         const timeZone = this.getTimeZone();
-        newTimeData.timeInfo = timeFromInfo.display;
         newTimeData.timeRange = {
           from: dateMath.parse(timeFromInfo.from, undefined, timeZone)!,
           to: dateMath.parse(timeFromInfo.to, undefined, timeZone)!,
           raw: { from: timeFromInfo.from, to: timeFromInfo.to },
         };
+        infoBlocks.push(timeFromInfo.display);
       }
     }
 
@@ -101,7 +167,8 @@ export class PanelTimeRange extends SceneTimeRangeTransformerBase<PanelTimeRange
       }
 
       const timeShift = '-' + timeShiftInterpolated;
-      newTimeData.timeInfo += ' timeshift ' + timeShift;
+      infoBlocks.push('timeshift ' + timeShift);
+
       const from = dateMath.parseDateMath(timeShift, newTimeData.timeRange.from, false)!;
       const to = dateMath.parseDateMath(timeShift, newTimeData.timeRange.to, true)!;
 
@@ -113,12 +180,20 @@ export class PanelTimeRange extends SceneTimeRangeTransformerBase<PanelTimeRange
       newTimeData.timeRange = { from, to, raw: { from, to } };
     }
 
+    if (compareWith) {
+      const option = DEFAULT_COMPARE_OPTIONS.find((x) => x.value === compareWith);
+      const text = option ? `compared to ${option.label.toLowerCase()}` : '';
+      const final = infoBlocks.length === 0 ? capitalize(text) : text;
+      infoBlocks.push(final);
+    }
+
+    newTimeData.timeInfo = infoBlocks.join(' + ');
     return newTimeData;
   }
 }
 
 function PanelTimeRangeRenderer({ model }: SceneComponentProps<PanelTimeRange>) {
-  const { timeInfo, hideTimeOverride } = model.useState();
+  const { timeInfo, hideTimeOverride, isSettingsOpen } = model.useState();
   const styles = useStyles2(getStyles);
 
   if (!timeInfo || hideTimeOverride) {
@@ -126,11 +201,87 @@ function PanelTimeRangeRenderer({ model }: SceneComponentProps<PanelTimeRange>) 
   }
 
   return (
-    <Tooltip content={<TimePickerTooltip timeRange={model.state.value} timeZone={model.getTimeZone()} />}>
-      <PanelChrome.TitleItem className={styles.timeshift}>
-        <Icon name="clock-nine" size="sm" /> {timeInfo}
-      </PanelChrome.TitleItem>
-    </Tooltip>
+    <>
+      {isSettingsOpen && (
+        <Drawer title="Panel time range settings" onClose={() => model.setState({ isSettingsOpen: false })} size="sm">
+          <PanelTimeRangeSettings model={model} />
+        </Drawer>
+      )}
+      <Tooltip content={'Click to open panel time range settings'}>
+        <PanelChrome.TitleItem className={styles.timeshift} onClick={() => model.setState({ isSettingsOpen: true })}>
+          <Stack gap={1} alignItems={'center'}>
+            <Icon name="clock-nine" size="sm" />
+            <div>{timeInfo}</div>
+          </Stack>
+        </PanelChrome.TitleItem>
+      </Tooltip>
+    </>
+  );
+}
+
+const DEFAULT_COMPARE_OPTIONS = [
+  { label: 'Disabled', value: '' },
+  { label: 'Day before', value: '24h' },
+  { label: 'Week before', value: '1w' },
+  { label: 'Month before', value: '1M' },
+];
+
+function PanelTimeRangeSettings({ model }: SceneComponentProps<PanelTimeRange>) {
+  const { timeFrom, timeShift, hideTimeOverride, compareWith } = model.useState();
+  const [localTimeFrom, setLocalTimeFrom] = useState(timeFrom);
+  const [localTimeShift, setLocalTimeShift] = useState(timeShift);
+  const [localHideTimeOverride, setLocalHideTimeOverride] = useState(hideTimeOverride);
+  const [timeCompare, setTimeCompare] = useState(compareWith ?? '');
+
+  return (
+    <Stack direction="column" gap={2}>
+      <Field label="Relative time override" noMargin>
+        <Input
+          value={localTimeFrom}
+          onChange={(e) => setLocalTimeFrom(e.currentTarget.value)}
+          placeholder="e.g. now-1h"
+        />
+      </Field>
+      <Field label="Time shift" noMargin>
+        <Input
+          value={localTimeShift}
+          onChange={(e) => setLocalTimeShift(e.currentTarget.value)}
+          placeholder="e.g. 1h"
+        />
+      </Field>
+      <Field
+        noMargin
+        label={
+          <Stack>
+            <Label>Time window comparison</Label> <Badge color="blue" text="New" />
+          </Stack>
+        }
+      >
+        <Combobox options={DEFAULT_COMPARE_OPTIONS} value={timeCompare} onChange={(x) => setTimeCompare(x.value)} />
+      </Field>
+
+      <Box paddingTop={3}>
+        <Stack>
+          <Button variant="secondary" onClick={() => model.setState({ isSettingsOpen: false })}>
+            Cancel
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              model.setState({
+                timeFrom: localTimeFrom,
+                timeShift: localTimeShift,
+                hideTimeOverride: localHideTimeOverride,
+                isSettingsOpen: false,
+                compareWith: timeCompare,
+              });
+            }}
+          >
+            Apply
+          </Button>
+        </Stack>
+      </Box>
+    </Stack>
   );
 }
 
@@ -143,3 +294,54 @@ const getStyles = (theme: GrafanaTheme2) => {
     }),
   };
 };
+
+const PREVIOUS_PERIOD_VALUE = '__previousPeriod';
+const NO_PERIOD_VALUE = '__noPeriod';
+
+function getCompareTimeRange(timeRange: TimeRange, compareWith: string | undefined): TimeRange | undefined {
+  let compareFrom: DateTime;
+  let compareTo: DateTime;
+
+  if (compareWith) {
+    if (compareWith === PREVIOUS_PERIOD_VALUE) {
+      const diffMs = timeRange.to.diff(timeRange.from);
+      compareFrom = dateTime(timeRange.from!).subtract(diffMs);
+      compareTo = dateTime(timeRange.to!).subtract(diffMs);
+    } else {
+      compareFrom = dateTime(timeRange.from!).subtract(rangeUtil.intervalToMs(compareWith));
+      compareTo = dateTime(timeRange.to!).subtract(rangeUtil.intervalToMs(compareWith));
+    }
+    return {
+      from: compareFrom,
+      to: compareTo,
+      raw: {
+        from: compareFrom,
+        to: compareTo,
+      },
+    };
+  }
+
+  return undefined;
+}
+
+// Processor function for use with time shifted comparison series.
+// This aligns the secondary series with the primary and adds custom
+// metadata and config to the secondary series' fields so that it is
+// rendered appropriately.
+const timeShiftAlignmentProcessor: ExtraQueryDataProcessor = (primary, secondary) => {
+  const diff = secondary.timeRange.from.diff(primary.timeRange.from);
+  secondary.series.forEach((series) => {
+    series.refId = getCompareSeriesRefId(series.refId || '');
+    series.meta = {
+      ...series.meta,
+      // @ts-ignore Remove when https://github.com/grafana/grafana/pull/71129 is released
+      timeCompare: {
+        diffMs: diff,
+        isTimeShiftQuery: true,
+      },
+    };
+  });
+  return of(secondary);
+};
+
+export const getCompareSeriesRefId = (refId: string) => `${refId}-compare`;
