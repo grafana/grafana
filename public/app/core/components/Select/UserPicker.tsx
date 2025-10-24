09fbf98f7f8b4e83fccfd2eadcae9d46f3c40c50
Ashley Harrison <ashley.harrison@grafana.com>
2025-10-24 13:54:50 +0100

    convert UserPicker to a functional component

--- public/app/core/components/Select/UserPicker.tsx
+++ public/app/core/components/Select/UserPicker.tsx
@@ -1,6 +1,6 @@
 import debounce from 'debounce-promise';
 import { isNil } from 'lodash';
-import { Component } from 'react';
+import { useMemo, useState } from 'react';
 
 import { SelectableValue } from '@grafana/data';
 import { t } from '@grafana/i18n';
@@ -14,63 +14,56 @@ export interface Props {
   inputId?: string;
 }
 
-export interface State {
-  isLoading: boolean;
-}
+export const UserPicker = ({ className, onSelected, inputId }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
 
-export class UserPicker extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { isLoading: false };
-  }
+  const search = useMemo(
+    () =>
+      debounce(
+        async (query?: string) => {
+          setIsLoading(true);
 
-  search = debounce(
-    async (query?: string) => {
-      this.setState({ isLoading: true });
+          if (isNil(query)) {
+            query = '';
+          }
 
-      if (isNil(query)) {
-        query = '';
-      }
-
-      return getBackendSrv()
-        .get(`/api/org/users/lookup?query=${query}&limit=100`)
-        .then((result: OrgUser[]) => {
-          return result.map((user) => ({
-            id: user.userId,
-            uid: user.uid,
-            value: user,
-            label: user.login,
-            imgUrl: user.avatarUrl,
-            login: user.login,
-          }));
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    },
-    300,
-    { leading: true }
+          return getBackendSrv()
+            .get(`/api/org/users/lookup?query=${query}&limit=100`)
+            .then((result: OrgUser[]) => {
+              return result.map((user) => ({
+                id: user.userId,
+                uid: user.uid,
+                value: user,
+                label: user.login,
+                imgUrl: user.avatarUrl,
+                login: user.login,
+              }));
+            })
+            .finally(() => {
+              setIsLoading(false);
+            });
+        },
+        300,
+        { leading: true }
+      ),
+    []
   );
 
-  render() {
-    const { className, onSelected, inputId } = this.props;
-    const { isLoading } = this.state;
-
-    return (
-      <div className="user-picker" data-testid="userPicker">
-        <AsyncSelect
-          isClearable
-          className={className}
-          inputId={inputId}
-          isLoading={isLoading}
-          defaultOptions={true}
-          loadOptions={this.search}
-          onChange={onSelected}
-          placeholder={t('user-picker.select-placeholder', 'Start typing to search for user')}
-          noOptionsMessage={t('user-picker.noOptionsMessage-no-users-found', 'No users found')}
-          aria-label={t('user-picker.select-aria-label', 'User picker')}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="user-picker" data-testid="userPicker">
+      <AsyncSelect
+        isClearable
+        className={className}
+        inputId={inputId}
+        isLoading={isLoading}
+        defaultOptions={true}
+        loadOptions={search}
+        onChange={onSelected}
+        placeholder={t('user-picker.select-placeholder', 'Start typing to search for user')}
+        noOptionsMessage={t('user-picker.noOptionsMessage-no-users-found', 'No users found')}
+        aria-label={t('user-picker.select-aria-label', 'User picker')}
+      />
+    </div>
+  );
+};
+UserPicker.displayName = 'UserPicker';
