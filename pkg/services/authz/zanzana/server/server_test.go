ec9513a3d81198a3e3e9660959ec79fb734fe8a5
Alexander Zobnin <alexanderzobnin@gmail.com>
2025-10-27 16:27:49 +0100

    fix linter

--- pkg/services/authz/zanzana/server/server_test.go
+++ pkg/services/authz/zanzana/server/server_test.go
@@ -173,7 +173,7 @@ func setupOpenFGADatabase(t *testing.T, srv *Server, tuples []*openfgav1.TupleKe
 	}
 
 	// Try to delete existing tuples (ignore errors if they don't exist)
-	_, err = srv.openfga.Write(context.Background(), &openfgav1.WriteRequest{
+	srv.openfga.Write(context.Background(), &openfgav1.WriteRequest{
 		StoreId:              storeInf.ID,
 		AuthorizationModelId: storeInf.ModelID,
 		Deletes: &openfgav1.WriteRequestDeletes{
