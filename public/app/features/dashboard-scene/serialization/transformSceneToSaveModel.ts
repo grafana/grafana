c5bcc73a29e539ff45b0312b8db8b03afe5727ea
Torkel Ödegaard <torkel@grafana.com>
2025-10-27 12:19:09 +0100

    Fix

--- public/app/features/dashboard-scene/serialization/transformSceneToSaveModel.ts
+++ public/app/features/dashboard-scene/serialization/transformSceneToSaveModel.ts
@@ -32,10 +32,10 @@ import { GrafanaQueryType } from 'app/plugins/datasource/grafana/types';
 
 import { DashboardDataLayerSet } from '../scene/DashboardDataLayerSet';
 import { DashboardScene } from '../scene/DashboardScene';
-import { PanelTimeRange } from '../scene/panel-timerange/PanelTimeRange';
 import { DashboardGridItem } from '../scene/layout-default/DashboardGridItem';
 import { DefaultGridLayoutManager } from '../scene/layout-default/DefaultGridLayoutManager';
 import { RowRepeaterBehavior } from '../scene/layout-default/RowRepeaterBehavior';
+import { PanelTimeRange } from '../scene/panel-timerange/PanelTimeRange';
 import { dashboardSceneGraph } from '../utils/dashboardSceneGraph';
 import { djb2Hash } from '../utils/djb2Hash';
 import {
