1dee2d445ad8d4475783bc16121b7a9118f874a9
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-10-27 16:22:43 +0000

    Migrate FolderFilter to combobox

---
+++ public/app/core/components/FolderFilter/FolderFilter.test.tsx
@@ -0,0 +1,48 @@
+import { comboboxTestSetup } from 'test/helpers/comboboxTestSetup';
+import { render, screen, testWithFeatureToggles } from 'test/test-utils';
+
+import { setBackendSrv } from '@grafana/runtime';
+import { setupMockServer } from '@grafana/test-utils/server';
+import { getFolderFixtures } from '@grafana/test-utils/unstable';
+import { backendSrv } from 'app/core/services/backend_srv';
+import { resetGrafanaSearcher } from 'app/features/search/service/searcher';
+
+import { FolderFilter } from './FolderFilter';
+const [_, { folderA, folderB }] = getFolderFixtures();
+
+setBackendSrv(backendSrv);
+setupMockServer();
+comboboxTestSetup();
+
+const fixtures: Array<
+  [
+    // Test title
+    string,
+    // Feature toggle setup
+    Parameters<typeof testWithFeatureToggles>[0],
+  ]
+> = [
+  ['app platform APIs enabled', { enable: ['unifiedStorageSearchUI'] }],
+  ['app platform APIs disabled', {}],
+];
+
+describe('FolderFilter', () => {
+  describe.each(fixtures)('%s', (_title, featureToggleSetup) => {
+    beforeEach(() => {
+      resetGrafanaSearcher();
+    });
+
+    testWithFeatureToggles(featureToggleSetup);
+
+    it('allows selecting folders', async () => {
+      const onChange = jest.fn();
+      const { user } = render(<FolderFilter onChange={onChange} />);
+
+      await user.click(screen.getByPlaceholderText('Filter by folder'));
+
+      await user.click(await screen.findByText(folderA.item.title));
+      await user.click(await screen.findByText(folderB.item.title));
+      expect(onChange).toHaveBeenCalledWith([folderA.item.uid, folderB.item.uid]);
+    });
+  });
+});
