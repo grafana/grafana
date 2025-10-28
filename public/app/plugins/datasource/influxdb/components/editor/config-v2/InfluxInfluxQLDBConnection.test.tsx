dfc759185f5ad29debc7fc2cdcc94803c4f24bb9
Alyssa Joyner <58453566+alyssajoyner@users.noreply.github.com>
2025-10-28 08:09:43 -0600

    [InfluxDB]: Update product selection and UI  (#112074)

--- public/app/plugins/datasource/influxdb/components/editor/config-v2/InfluxInfluxQLDBConnection.test.tsx
+++ public/app/plugins/datasource/influxdb/components/editor/config-v2/InfluxInfluxQLDBConnection.test.tsx
@@ -28,14 +28,17 @@ describe('InfluxInfluxQLDBConnection', () => {
 
   it('renders dbName, user and password fields', () => {
     render(<InfluxInfluxQLDBConnection {...defaultProps} />);
-    expect(screen.getByLabelText(/Database/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/User/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
+
+    expect(screen.getByLabelText(/^Database\b/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^User\b/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^Password\b/i)).toBeInTheDocument();
   });
 
   it('calls onOptionsChange on input changes', () => {
     render(<InfluxInfluxQLDBConnection {...defaultProps} />);
+
     fireEvent.change(screen.getByLabelText(/User/i), { target: { value: 'newuser' } });
+
     expect(onOptionsChangeMock).toHaveBeenCalled();
   });
 });
