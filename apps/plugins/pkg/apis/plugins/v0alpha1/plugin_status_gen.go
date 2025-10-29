de88abafdd955d71d931b70c24dfe0c4b5a25b50
Todd Treece <360020+toddtreece@users.noreply.github.com>
2025-10-29 13:32:31 -0400

    Plugins API: Merge meta and installs (#112962)

--- apps/plugins/pkg/apis/plugins/v0alpha1/plugin_status_gen.go
+++ apps/plugins/pkg/apis/plugins/v0alpha1/plugin_status_gen.go
@@ -3,42 +3,42 @@
 package v0alpha1
 
 // +k8s:openapi-gen=true
-type PluginMetastatusOperatorState struct {
+type PluginstatusOperatorState struct {
 	// lastEvaluation is the ResourceVersion last evaluated
 	LastEvaluation string `json:"lastEvaluation"`
 	// state describes the state of the lastEvaluation.
 	// It is limited to three possible states for machine evaluation.
-	State PluginMetaStatusOperatorStateState `json:"state"`
+	State PluginStatusOperatorStateState `json:"state"`
 	// descriptiveState is an optional more descriptive state field which has no requirements on format
 	DescriptiveState *string `json:"descriptiveState,omitempty"`
 	// details contains any extra information that is operator-specific
 	Details map[string]interface{} `json:"details,omitempty"`
 }
 
-// NewPluginMetastatusOperatorState creates a new PluginMetastatusOperatorState object.
-func NewPluginMetastatusOperatorState() *PluginMetastatusOperatorState {
-	return &PluginMetastatusOperatorState{}
+// NewPluginstatusOperatorState creates a new PluginstatusOperatorState object.
+func NewPluginstatusOperatorState() *PluginstatusOperatorState {
+	return &PluginstatusOperatorState{}
 }
 
 // +k8s:openapi-gen=true
-type PluginMetaStatus struct {
+type PluginStatus struct {
 	// operatorStates is a map of operator ID to operator state evaluations.
 	// Any operator which consumes this kind SHOULD add its state evaluation information to this field.
-	OperatorStates map[string]PluginMetastatusOperatorState `json:"operatorStates,omitempty"`
+	OperatorStates map[string]PluginstatusOperatorState `json:"operatorStates,omitempty"`
 	// additionalFields is reserved for future use
 	AdditionalFields map[string]interface{} `json:"additionalFields,omitempty"`
 }
 
-// NewPluginMetaStatus creates a new PluginMetaStatus object.
-func NewPluginMetaStatus() *PluginMetaStatus {
-	return &PluginMetaStatus{}
+// NewPluginStatus creates a new PluginStatus object.
+func NewPluginStatus() *PluginStatus {
+	return &PluginStatus{}
 }
 
 // +k8s:openapi-gen=true
-type PluginMetaStatusOperatorStateState string
+type PluginStatusOperatorStateState string
 
 const (
-	PluginMetaStatusOperatorStateStateSuccess    PluginMetaStatusOperatorStateState = "success"
-	PluginMetaStatusOperatorStateStateInProgress PluginMetaStatusOperatorStateState = "in_progress"
-	PluginMetaStatusOperatorStateStateFailed     PluginMetaStatusOperatorStateState = "failed"
+	PluginStatusOperatorStateStateSuccess    PluginStatusOperatorStateState = "success"
+	PluginStatusOperatorStateStateInProgress PluginStatusOperatorStateState = "in_progress"
+	PluginStatusOperatorStateStateFailed     PluginStatusOperatorStateState = "failed"
 )
