d89b14138f4a9ad39a66e66b0077bcd2330af68d
Jayclifford345 <jay.clifford@grafana.com>
2025-10-30 12:40:21 +0000

    fixed missing tooltips for help button

--- public/app/core/components/AppChrome/TopBar/HelpTopBarButton.tsx
+++ public/app/core/components/AppChrome/TopBar/HelpTopBarButton.tsx
@@ -34,7 +34,12 @@ export const HelpTopBarButton = memo(function HelpTopBarButton({ isSmallScreen }
   if (isSmallScreen || !enrichedHelpNode.hideFromTabs || interactiveLearningPluginId === undefined) {
     return (
       <Dropdown overlay={() => <TopNavBarMenu node={enrichedHelpNode} />} placement="bottom-end">
-        <ToolbarButton iconOnly icon="question-circle" aria-label={t('navigation.help.aria-label', 'Help')} />
+        <ToolbarButton 
+          iconOnly 
+          icon="question-circle" 
+          aria-label={t('navigation.help.aria-label', 'Help')}
+          tooltip={t('navigation.help.tooltip', 'Help and documentation')}
+        />
       </Dropdown>
     );
   }
@@ -48,6 +53,11 @@ export const HelpTopBarButton = memo(function HelpTopBarButton({ isSmallScreen }
       icon="question-circle"
       aria-label={t('navigation.help.aria-label', 'Help')}
       className={isOpen ? styles.helpButtonActive : undefined}
+      tooltip={
+        isOpen
+          ? t('navigation.help.interactive-learning.close-tooltip', 'Close interactive learning')
+          : t('navigation.help.interactive-learning.open-tooltip', 'Interactive learning, help and documentation')
+      }
       onClick={() => {
         if (isOpen) {
           setDockedComponentId(undefined);
