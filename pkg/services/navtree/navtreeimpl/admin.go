6b1d393018f1454690c05bbbdf8e31d366d5bce5
Alex Khomenko <Clarity-89@users.noreply.github.com>
2025-10-31 10:06:41 +0200

    Provisioning: Move nav item to General (#113266)

--- pkg/services/navtree/navtreeimpl/admin.go
+++ pkg/services/navtree/navtreeimpl/admin.go
@@ -56,7 +56,7 @@ func (s *ServiceImpl) getAdminNode(c *contextmodel.ReqContext) (*navtree.NavLink
 	if c.HasRole(identity.RoleAdmin) &&
 		(s.cfg.StackID == "" || // show OnPrem even when provisioning is disabled
 			s.features.IsEnabledGlobally(featuremgmt.FlagProvisioning)) {
-		configNodes = append(configNodes, &navtree.NavLink{
+		generalNodeLinks = append(generalNodeLinks, &navtree.NavLink{
 			Text:     "Provisioning",
 			Id:       "provisioning",
 			SubTitle: "View and manage your provisioning connections",
