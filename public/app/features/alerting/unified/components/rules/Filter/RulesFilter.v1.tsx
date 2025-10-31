97e24454190f28fe61d0703397eb03fe73fef860
Pepe Cano <825430+ppcano@users.noreply.github.com>
2025-10-31 10:56:56 +0100

    ui(alerting): show correct tooltip when label search includes spaces (#113038)

--- public/app/features/alerting/unified/components/rules/Filter/RulesFilter.v1.tsx
+++ public/app/features/alerting/unified/components/rules/Filter/RulesFilter.v1.tsx
@@ -368,7 +368,10 @@ function SearchQueryHelp() {
         />
         <HelpRow title={t('alerting.search-query-help.title-group', 'Group')} expr="group:cpu-usage" />
         <HelpRow title={t('alerting.search-query-help.title-rule', 'Rule')} expr='rule:"cpu 80%"' />
-        <HelpRow title={t('alerting.search-query-help.title-labels', 'Labels')} expr="label:team=A label:cluster=a1" />
+        <HelpRow
+          title={t('alerting.search-query-help.title-labels', 'Labels')}
+          expr='label:team=A label:"cluster=new york"'
+        />
         <HelpRow title={t('alerting.search-query-help.title-state', 'State')} expr="state:firing|normal|pending" />
         <HelpRow title={t('alerting.search-query-help.title-type', 'Type')} expr="type:alerting|recording" />
         <HelpRow title={t('alerting.search-query-help.title-health', 'Health')} expr="health:ok|nodata|error" />
