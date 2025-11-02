a65d494811d9797c9502a8de1d8670a426dc8878
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2025-11-02 21:39:15 +0100

    Dashboard Provisioning: Add duplicate cleanup for modes 0-2 (#113336)

--- pkg/services/dashboards/models.go
+++ pkg/services/dashboards/models.go
@@ -241,11 +241,15 @@ type DeleteOrphanedProvisionedDashboardsCommand struct {
 }
 
 type DashboardProvisioningSearchResults struct {
-	Dashboard       Dashboard `xorm:"extends"`
-	Provisioner     string    `xorm:"name"`
-	ExternalID      string    `xorm:"external_id"`
-	CheckSum        string    `xorm:"check_sum"`
-	ProvisionUpdate int64     `xorm:"provisioning_updated"`
+	ID              int64  `xorm:"id"`
+	UID             string `xorm:"uid"`
+	Title           string `xorm:"title"`
+	FolderUID       string `xorm:"folder_uid"`
+	OrgID           int64  `xorm:"org_id"`
+	Provisioner     string `xorm:"name"`
+	ExternalID      string `xorm:"external_id"`
+	CheckSum        string `xorm:"check_sum"`
+	ProvisionUpdate int64  `xorm:"provisioning_updated"`
 }
 
 //
