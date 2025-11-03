d85fc11f11a52a01de82925da977f9750d3ee451
grambbledook <denis.vodopianov@grafana.com>
2025-11-03 12:27:06 +0100

    experiment with openfeature api

---
+++ pkg/services/featuremgmt/testing/client.go
@@ -0,0 +1,38 @@
+package testing
+
+import (
+	"github.com/open-feature/go-sdk/openfeature"
+	"github.com/open-feature/go-sdk/openfeature/testing"
+	"golang.org/x/net/context"
+	"sync"
+)
+
+var (
+	mutex    sync.Mutex
+	provider *testing.TestProvider
+)
+
+type TestClient struct {
+	delegate *openfeature.Client
+	test     *testing.TestProvider
+}
+
+func NewTestClient() *TestClient {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	err := openfeature.SetProviderAndWait(provider)
+	if err != nil {
+		panic("unable to set test provider: " + err.Error())
+	}
+	return &TestClient{
+		delegate: openfeature.NewDefaultClient(),
+		test:     provider,
+	}
+}
+
+func (c *TestClient) IsEnabled(ctx context.Context, flag string) bool {
+	return c.delegate.Boolean(ctx, flag, false, openfeature.TransactionContext(ctx))
+}
+
+func (c *TestClient) SetEnabled(ctx context.Context, flag string, enabled bool) {}
