d5fb7e1145a63e69f08d7306448f9c3c8406c24c
Will Assis <william@williamassis.com>
2025-11-06 18:15:45 -0300

    fmt

--- pkg/storage/unified/resource/storage_backend.go
+++ pkg/storage/unified/resource/storage_backend.go
@@ -490,7 +490,7 @@ func (k *kvStorageBackend) ListIterator(ctx context.Context, req *resourcepb.Lis
 			Namespace: namespace,
 			Name:      req.Options.Key.Name,
 		}
-		listOptions.EndKeyPrefix= PrefixRangeEnd(listOptions.StartKey.Prefix())
+		listOptions.EndKeyPrefix = PrefixRangeEnd(listOptions.StartKey.Prefix())
 	}
 
 	// We set the listRV to the last event resource version.
@@ -524,9 +524,9 @@ func (k *kvStorageBackend) ListIterator(ctx context.Context, req *resourcepb.Lis
 	defer stop()
 
 	iter := kvListIterator{
-		listRV:         listRV,
-		endKey:         listOptions.EndKeyPrefix,
-		next:           next,
+		listRV: listRV,
+		endKey: listOptions.EndKeyPrefix,
+		next:   next,
 	}
 	err := cb(&iter)
 	if err != nil {
