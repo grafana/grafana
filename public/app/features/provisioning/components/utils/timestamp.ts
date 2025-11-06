1b8316590d24977d02c2d8b1d6c65c24c7eb42a1
Leon Sorokin <leeoniya@gmail.com>
2025-11-06 05:08:31 -0600

    Chore: Remove Chance.js dependency from runtime code (#113457)

--- public/app/features/provisioning/components/utils/timestamp.ts
+++ public/app/features/provisioning/components/utils/timestamp.ts
@@ -1,11 +1,9 @@
-import { Chance } from 'chance';
-
 import { dateTime } from '@grafana/data';
 
 /**
  * Generates a timestamp string in the format YYYY-MM-DD-xxxxx where xxxxx is a random string
  */
 export function generateTimestamp(): string {
-  const random = new Chance();
-  return `${dateTime().format('YYYY-MM-DD')}-${random.string({ length: 5, alpha: true })}`;
+  const randStr = Math.random().toString(36).substring(2, 7);
+  return `${dateTime().format('YYYY-MM-DD')}-${randStr}`;
 }
