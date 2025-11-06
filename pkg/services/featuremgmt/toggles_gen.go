8da345e9048396fa605182dfaabcf76b6f6a81f0
Adela Almasan <adela.almasan@grafana.com>
2025-11-06 14:09:26 -0600

    newVizSuggestions feature toggle

--- pkg/services/featuremgmt/toggles_gen.go
+++ pkg/services/featuremgmt/toggles_gen.go
@@ -1094,6 +1094,10 @@ const (
 	// Enable new gauge visualization
 	FlagNewGauge = "newGauge"
 
+	// FlagNewVizSuggestions
+	// Enable new visualization suggestions
+	FlagNewVizSuggestions = "newVizSuggestions"
+
 	// FlagPreventPanelChromeOverflow
 	// Restrict PanelChrome contents with overflow: hidden;
 	FlagPreventPanelChromeOverflow = "preventPanelChromeOverflow"
