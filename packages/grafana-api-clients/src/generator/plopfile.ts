11c7e17290e02afd2772507b5f1e91ceb65eabfc
Clarity-89 <homes89@ukr.net>
2025-11-07 13:35:28 +0200

    API clients: Use open_snapshots for possibleOpenAPISpecs

--- packages/grafana-api-clients/src/generator/plopfile.ts
+++ packages/grafana-api-clients/src/generator/plopfile.ts
@@ -107,15 +107,17 @@ export default function plopGenerator(plop: NodePlopAPI) {
     return actions;
   };
 
-  // Read files from data/openapi directory and use as an array of API clients that the user can select
+  const getOpenAPISpecs = (isEnterprise: boolean): string[] => {
+    const openapiDir = isEnterprise
+      ? path.join(basePath, 'pkg/extensions/apiserver/tests/openapi_snapshots')
+      : path.join(basePath, 'pkg/tests/apis/openapi_snapshots');
 
-  const openapiDir = path.join(basePath, 'data/openapi');
-  let possibleOpenAPISpecs: string[] = [];
-  try {
-    possibleOpenAPISpecs = fs.readdirSync(openapiDir).filter((file: string) => file.endsWith('.json'));
-  } catch (e) {
-    possibleOpenAPISpecs = [];
-  }
+    try {
+      return fs.readdirSync(openapiDir).filter((file: string) => file.endsWith('.json'));
+    } catch (e) {
+      return [];
+    }
+  };
 
   const generator: PlopGeneratorConfig = {
     description: 'Generate RTK Query API client for a Grafana API group',
@@ -129,7 +131,9 @@ export default function plopGenerator(plop: NodePlopAPI) {
       {
         type: 'list',
         loop: false,
-        choices: possibleOpenAPISpecs,
+        choices: (answers: { isEnterprise?: boolean }) => {
+          return getOpenAPISpecs(answers.isEnterprise ?? false);
+        },
         pageSize: 50,
         name: 'apiInfo',
         message: 'OpenAPI spec:',
