f4ddea1f87faa7e6fac099abab13a7290836ece8
Mike <mmelvin0@gmail.com>
2025-11-10 16:12:03 -0800

    Secrets: fix MariaDB syntax error due to unsupported CTE syntax (#111610)

--- pkg/storage/secret/metadata/data/secure_value_lease_inactive.sql
+++ pkg/storage/secret/metadata/data/secure_value_lease_inactive.sql
@@ -1,20 +1,19 @@
-WITH to_update AS (
+UPDATE
+  {{ .Ident "secret_secure_value" }}
+SET
+  {{ .Ident "lease_token" }} = {{ .Arg .LeaseToken }},
+  {{ .Ident "lease_created" }} = {{ .Arg .Now }}
+WHERE guid IN (
   SELECT guid FROM (
-    SELECT 
+    SELECT
       guid,
       ROW_NUMBER() OVER (ORDER BY created ASC) AS rn
-    FROM {{ .Ident "secret_secure_value" }} 
-    WHERE 
+    FROM {{ .Ident "secret_secure_value" }}
+    WHERE
       {{ .Ident "active" }} = FALSE AND
       {{ .Arg .Now }} - {{ .Ident "created" }} > {{ .Arg .MinAge }} AND
       {{ .Arg .Now }} - {{ .Ident "lease_created" }} > {{ .Arg .LeaseTTL }}
   ) AS sub
   WHERE rn <= {{ .Arg .MaxBatchSize }}
 )
-UPDATE
-  {{ .Ident "secret_secure_value" }}
-SET
-  {{ .Ident "lease_token" }} = {{ .Arg .LeaseToken }},
-  {{ .Ident "lease_created" }} = {{ .Arg .Now }}
-WHERE guid IN (SELECT guid FROM to_update)
 ;
