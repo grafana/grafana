b166797bc09970381e67ee0fb458d6ab367e7bfc
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2025-11-11 17:21:18 +0000

    Remove unneeded code

--- public/app/features/teams/state/actions.ts
+++ public/app/features/teams/state/actions.ts
@@ -3,29 +3,22 @@ import { ThunkResult } from 'app/types/store';
 
 import { teamGroupsLoaded } from './reducers';
 
-export function updateTeam(name: string, email: string): ThunkResult<void> {
-  return async (dispatch, getStore) => {
-    const team = getStore().team.team;
-    await getBackendSrv().put(`/api/teams/${team.uid}`, { name, email });
-  };
-}
-
 export function loadTeamGroups(teamUid: string): ThunkResult<void> {
-  return async (dispatch, getStore) => {
+  return async (dispatch) => {
     const response = await getBackendSrv().get(`/api/teams/${teamUid}/groups`);
     dispatch(teamGroupsLoaded(response));
   };
 }
 
 export function addTeamGroup(teamUid: string, groupId: string): ThunkResult<void> {
-  return async (dispatch, getStore) => {
+  return async (dispatch) => {
     await getBackendSrv().post(`/api/teams/${teamUid}/groups`, { groupId: groupId });
     dispatch(loadTeamGroups(teamUid));
   };
 }
 
 export function removeTeamGroup(teamUid: string, groupId: string): ThunkResult<void> {
-  return async (dispatch, getStore) => {
+  return async (dispatch) => {
     // need to use query parameter due to escaped characters in the request
     await getBackendSrv().delete(`/api/teams/${teamUid}/groups?groupId=${encodeURIComponent(groupId)}`);
     dispatch(loadTeamGroups(teamUid));
--- public/app/features/teams/state/reducers.ts
+++ public/app/features/teams/state/reducers.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { TeamsState, Team, TeamState, TeamGroup } from 'app/types/teams';
+import { TeamsState, TeamState, TeamGroup } from 'app/types/teams';
 
 export const initialTeamsState: TeamsState = {
   teams: [],
@@ -20,16 +20,13 @@ const teamSlice = createSlice({
   name: 'team',
   initialState: initialTeamState,
   reducers: {
-    teamLoaded: (state, action: PayloadAction<Team>): TeamState => {
-      return { ...state, team: action.payload };
-    },
     teamGroupsLoaded: (state, action: PayloadAction<TeamGroup[]>): TeamState => {
       return { ...state, groups: action.payload };
     },
   },
 });
 
-export const { teamLoaded, teamGroupsLoaded } = teamSlice.actions;
+export const { teamGroupsLoaded } = teamSlice.actions;
 
 export const teamReducer = teamSlice.reducer;
 
