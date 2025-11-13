b6be0fe5372387359faefedcbbb985184406986d
laurenashleigh <lauren.armstrong@grafana.com>
2025-11-13 17:15:41 +0000

    add info on hover of preview routing when disabled

--- public/app/features/alerting/unified/components/rule-editor/notificaton-preview/NotificationPreview.tsx
+++ public/app/features/alerting/unified/components/rule-editor/notificaton-preview/NotificationPreview.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/css';
 import { Fragment, Suspense, lazy } from 'react';
+import { useEffectOnce } from 'react-use';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { Trans, t } from '@grafana/i18n';
-import { Button, LoadingPlaceholder, Stack, Text, useEffectOnce, useStyles2 } from '@grafana/ui';
+import { Button, LoadingPlaceholder, Stack, Text, Tooltip, useStyles2 } from '@grafana/ui';
 import { alertRuleApi } from 'app/features/alerting/unified/api/alertRuleApi';
 import { AlertQuery, Labels } from 'app/types/unified-alerting-dto';
 
@@ -103,9 +104,21 @@ export const NotificationPreview = ({
             </Text>
           )}
         </Stack>
-        <Button icon="sync" variant="secondary" type="button" onClick={onPreview} disabled={disabled}>
-          <Trans i18nKey="alerting.notification-preview.preview-routing">Preview routing</Trans>
-        </Button>
+        <Tooltip
+          content={
+            disabled ? (
+              <Trans i18nKey="alerting.notification-preview.disabled-tooltip">
+                You don&apos;t have sufficient permissions to preview
+              </Trans>
+            ) : (
+              ''
+            )
+          }
+        >
+          <Button icon="sync" variant="secondary" type="button" onClick={onPreview} disabled={disabled}>
+            <Trans i18nKey="alerting.notification-preview.preview-routing">Preview routing</Trans>
+          </Button>
+        </Tooltip>
       </Stack>
       {potentialInstances.length > 0 && (
         <Suspense
