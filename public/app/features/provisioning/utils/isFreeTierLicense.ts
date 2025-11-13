b84a6a48b4c14e2f9c560b7c7bcaf614e4073dbd
Clarity-89 <homes89@ukr.net>
2025-11-13 15:40:23 +0200

    Add trial edition

--- public/app/features/provisioning/utils/isFreeTierLicense.ts
+++ public/app/features/provisioning/utils/isFreeTierLicense.ts
@@ -1,16 +1,9 @@
+import { GrafanaEdition } from '@grafana/data/internal';
 import { config } from '@grafana/runtime';
-
-import { isOnPrem } from './isOnPrem';
 
 /**
  * Checks if the current Grafana instance is on a free tier license
- *
- * Free tier is defined as:
- * - Cloud instance (not on-prem) AND
- * - Not licensed (unlicensed or trial)
- *
- * @returns true if the instance is a free tier cloud instance
  */
 export function isFreeTierLicense() {
-  return !isOnPrem() && config.licenseInfo.stateInfo !== 'Licensed';
+  return config.licenseInfo.edition === GrafanaEdition.Trial;
 }
