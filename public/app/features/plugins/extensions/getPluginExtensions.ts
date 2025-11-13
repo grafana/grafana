6353e4eabd9e2d1da1bce2c9cd681ae66565b20a
Brendan O'Handley <brendan.ohandley@grafana.com>
2025-11-13 16:11:51 -0600

    Add buildPathAsync function to support dynamic async path generation in plugins, enabling lazy loading of heavy modules, like promql parser

--- public/app/features/plugins/extensions/getPluginExtensions.ts
+++ public/app/features/plugins/extensions/getPluginExtensions.ts
@@ -113,6 +113,7 @@ export const getPluginExtensions: GetExtensions = ({
         title: addedLink.title,
         description: addedLink.description ?? '',
         onClick: typeof addedLink.onClick,
+        buildPathAsync: typeof addedLink.buildPathAsync,
       });
       // Run the configure() function with the current context, and apply the ovverides
       const overrides = getLinkExtensionOverrides(pluginId, addedLink, linkLog, frozenContext);
@@ -135,6 +136,7 @@ export const getPluginExtensions: GetExtensions = ({
         description: overrides?.description || addedLink.description || '',
         path: isString(path) ? getLinkExtensionPathWithTracking(pluginId, path, extensionPointId) : undefined,
         category: overrides?.category || addedLink.category,
+        buildPathAsync: overrides?.buildPathAsync || addedLink.buildPathAsync,
       };
 
       extensions.push(extension);
