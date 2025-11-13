b7011702baeaf5ee7646d59db6306442660ff568
Dafydd <dafydd.thomas@grafana.com>
2025-11-13 15:18:27 +0000

    add nolint check for global feature

--- pkg/registry/apis/datasource/register.go
+++ pkg/registry/apis/datasource/register.go
@@ -115,6 +115,7 @@ func RegisterAPIService(
 			datasources.GetDatasourceProvider(pluginJSON),
 			contextProvider,
 			accessControl,
+			//nolint:staticcheck // not yet migrated to OpenFeature
 			features.IsEnabledGlobally(featuremgmt.FlagDatasourceQueryTypes),
 			false,
 			true, // useShorterAPIGroupName
