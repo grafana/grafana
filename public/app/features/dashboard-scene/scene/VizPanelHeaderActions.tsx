8757fcb056c320b76dc7da83e2e2ed55afde77ca
Victor Marin <victor.marin@grafana.com>
2025-11-15 00:51:29 +0200

    refactor

--- public/app/features/dashboard-scene/scene/VizPanelHeaderActions.tsx
+++ public/app/features/dashboard-scene/scene/VizPanelHeaderActions.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import {
   GroupByVariable,
   SceneComponentProps,
@@ -6,6 +8,7 @@ import {
   SceneObjectState,
   SceneQueryRunner,
   VizPanel,
+  SceneDataQuery,
 } from '@grafana/scenes';
 
 import { PanelGroupByAction } from './PanelGroupByAction';
@@ -23,50 +26,95 @@ export class VizPanelHeaderActions extends SceneObjectBase<VizPanelHeaderActions
       ...state,
     });
   }
+
+  public getQueries(): SceneDataQuery[] {
+    const panel = this.parent;
+
+    if (!(panel instanceof VizPanel)) {
+      return [];
+    }
+
+    const dataObject = sceneGraph.getData(panel);
+
+    if (!dataObject) {
+      return [];
+    }
+
+    const queryRunner = dataObject.state.$data;
+
+    if (!queryRunner || !(queryRunner instanceof SceneQueryRunner)) {
+      return [];
+    }
+
+    return queryRunner.state.queries;
+  }
+
+  public checkGroupByActionRender(): boolean {
+    const panel = this.parent;
+
+    if (!(panel instanceof VizPanel)) {
+      return false;
+    }
+
+    const groupByVariable = sceneGraph
+      .getVariables(panel)
+      .state.variables.find((variable) => variable instanceof GroupByVariable);
+
+    if (!groupByVariable) {
+      return false;
+    }
+
+    const dataObject = sceneGraph.getData(panel);
+
+    if (!dataObject) {
+      return false;
+    }
+
+    const queryRunner = dataObject.state.$data;
+
+    if (!queryRunner || !(queryRunner instanceof SceneQueryRunner)) {
+      return false;
+    }
+
+    const { datasource } = queryRunner.useState();
+
+    const dsUid = sceneGraph.interpolate(queryRunner, datasource?.uid);
+    const groupByAppliedOnPanel = Boolean(
+      dsUid === sceneGraph.interpolate(groupByVariable, groupByVariable.state.datasource?.uid) &&
+        groupByVariable.isApplicabilityEnabled()
+    );
+
+    return !this.state.hideGroupByAction && groupByAppliedOnPanel;
+  }
 }
 
 export function VizPanelHeaderActionsRenderer({ model }: SceneComponentProps<VizPanelHeaderActions>) {
-  const hideGroupByAction = model.state.hideGroupByAction;
   const panel = model.parent;
 
-  if (!panel || !(panel instanceof VizPanel)) {
-    return null;
-  }
+  const queries = model.getQueries();
 
   const groupByVariable = sceneGraph
-    .getVariables(panel)
+    .getVariables(model)
     .state.variables.find((variable) => variable instanceof GroupByVariable);
 
-  if (!groupByVariable) {
-    return null;
-  }
+  const groupByActionRender = model.checkGroupByActionRender();
 
-  const dataObject = panel ? sceneGraph.getData(panel) : undefined;
+  useEffect(() => {
+    if (!(panel instanceof VizPanel)) {
+      return;
+    }
 
-  if (!dataObject) {
-    return null;
-  }
+    const showMenuAlways = groupByActionRender;
+    if (panel.state.showMenuAlways !== showMenuAlways) {
+      panel.setState({ showMenuAlways });
+    }
 
-  const queryRunner = dataObject.state.$data;
+    return () => {
+      if (panel.state.showMenuAlways) {
+        panel.setState({ showMenuAlways: false });
+      }
+    };
+  }, [groupByActionRender, panel]);
 
-  if (!queryRunner || !(queryRunner instanceof SceneQueryRunner)) {
-    return null;
-  }
-
-  const { queries, datasource } = queryRunner.useState();
-
-  const dsUid = sceneGraph.interpolate(queryRunner, datasource?.uid);
-
-  const groupByAppliedOnPanel = Boolean(
-    dsUid === sceneGraph.interpolate(groupByVariable, groupByVariable.state.datasource?.uid) &&
-      groupByVariable.isApplicabilityEnabled()
-  );
-
-  return (
-    <>
-      {!hideGroupByAction && groupByAppliedOnPanel && (
-        <PanelGroupByAction groupByVariable={groupByVariable} queries={queries} />
-      )}
-    </>
-  );
+  return <>{groupByActionRender && <PanelGroupByAction groupByVariable={groupByVariable!} queries={queries} />}</>;
 }
