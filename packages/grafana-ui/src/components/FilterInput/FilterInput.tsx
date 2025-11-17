2efe09a919bcb97655aa74794ccbef7c195046b1
Paul Marbach <paul.marbach@grafana.com>
2025-11-17 15:50:34 -0500

    Table: Improve filter selection ux text

--- packages/grafana-ui/src/components/FilterInput/FilterInput.tsx
+++ packages/grafana-ui/src/components/FilterInput/FilterInput.tsx
@@ -6,49 +6,57 @@ import { Trans } from '@grafana/i18n';
 import { useCombinedRefs } from '../../utils/useCombinedRefs';
 import { Button } from '../Button/Button';
 import { Icon } from '../Icon/Icon';
-import { Input } from '../Input/Input';
+import { Input, Props as InputProps } from '../Input/Input';
 
 export interface Props extends Omit<HTMLProps<HTMLInputElement>, 'onChange'> {
   value: string | undefined;
   width?: number;
   onChange: (value: string) => void;
   escapeRegex?: boolean;
+  suffix?: InputProps['suffix'];
 }
 
 export const FilterInput = forwardRef<HTMLInputElement, Props>(
-  ({ value, width, onChange, escapeRegex = true, ...restProps }, ref) => {
+  ({ value, width, onChange, escapeRegex = true, suffix: _suffix, ...restProps }, ref) => {
+    const containerRef = useRef<HTMLDivElement>(null);
     const innerRef = useRef<HTMLInputElement | null>(null);
     const combinedRef = useCombinedRefs<HTMLInputElement>(ref, innerRef);
 
-    const suffix =
-      value !== '' ? (
-        <Button
-          icon="times"
-          fill="text"
-          size="sm"
-          onClick={(e) => {
-            innerRef.current?.focus();
-            onChange('');
-            e.stopPropagation();
-          }}
-        >
-          <Trans i18nKey="grafana-ui.filter-input.clear">Clear</Trans>
-        </Button>
-      ) : null;
+    const suffix = (
+      <>
+        {value !== '' && (
+          <Button
+            icon="times"
+            fill="text"
+            size="sm"
+            onClick={(e) => {
+              innerRef.current?.focus();
+              onChange('');
+              e.stopPropagation();
+            }}
+          >
+            <Trans i18nKey="grafana-ui.filter-input.clear">Clear</Trans>
+          </Button>
+        )}
+        {_suffix}
+      </>
+    );
 
     return (
-      <Input
-        prefix={<Icon name="search" />}
-        suffix={suffix}
-        width={width}
-        type="text"
-        value={escapeRegex ? unEscapeStringFromRegex(value ?? '') : value}
-        onChange={(event) =>
-          onChange(escapeRegex ? escapeStringForRegex(event.currentTarget.value) : event.currentTarget.value)
-        }
-        {...restProps}
-        ref={combinedRef}
-      />
+      <div ref={containerRef}>
+        <Input
+          prefix={<Icon name="search" />}
+          suffix={suffix}
+          width={width}
+          type="text"
+          value={escapeRegex ? unEscapeStringFromRegex(value ?? '') : value}
+          onChange={(event) =>
+            onChange(escapeRegex ? escapeStringForRegex(event.currentTarget.value) : event.currentTarget.value)
+          }
+          {...restProps}
+          ref={combinedRef}
+        />
+      </div>
     );
   }
 );
