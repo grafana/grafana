e49c0abeee978161900e3e32d9b5c3d47933c64e
Ashley Harrison <ashley.harrison@grafana.com>
2025-11-18 13:06:29 +0000

    split into separate stories

--- packages/grafana-ui/src/components/PageLayout/PageToolbar.story.tsx
+++ packages/grafana-ui/src/components/PageLayout/PageToolbar.story.tsx
@@ -1,9 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import { Meta } from '@storybook/react';
 
-import { StoryExample } from '../../utils/storybook/StoryExample';
 import { IconButton } from '../IconButton/IconButton';
-import { Stack } from '../Layout/Stack/Stack';
 import { ToolbarButton } from '../ToolbarButton/ToolbarButton';
 
 import { PageToolbar } from './PageToolbar';
@@ -13,46 +11,48 @@ const meta: Meta<typeof PageToolbar> = {
   component: PageToolbar,
   parameters: {
     // TODO fix a11y issue in story and remove this
-    a11y: { test: 'off' },
+    // a11y: { test: 'off' },
   },
 };
 
-export const Examples = () => {
+export const WithNonClickableTitle = () => {
   return (
-    <Stack direction="column">
-      <StoryExample name="With non clickable title">
-        <PageToolbar pageIcon="bell" title="Dashboard">
-          <ToolbarButton icon="panel-add" />
-          <ToolbarButton icon="sync">Sync</ToolbarButton>
-        </PageToolbar>
-      </StoryExample>
-      <StoryExample name="With clickable title and parent">
-        <PageToolbar
-          pageIcon="apps"
-          title="A very long dashboard name"
-          parent="A long folder name"
-          titleHref=""
-          parentHref=""
-          leftItems={[
-            <IconButton name="share-alt" size="lg" key="share" tooltip="Share" />,
-            <IconButton name="favorite" iconType="mono" size="lg" key="favorite" tooltip="Add to favourites" />,
-          ]}
-        >
-          <ToolbarButton icon="panel-add" />
-          <ToolbarButton icon="share-alt" />
-          <ToolbarButton icon="sync">Sync</ToolbarButton>
-          <ToolbarButton icon="cog">Settings </ToolbarButton>
-        </PageToolbar>
-      </StoryExample>
-      <StoryExample name="Go back version">
-        <PageToolbar title="Service overview / Edit panel" onGoBack={() => action('Go back')}>
-          <ToolbarButton icon="cog" />
-          <ToolbarButton icon="save" />
-          <ToolbarButton>Discard</ToolbarButton>
-          <ToolbarButton>Apply</ToolbarButton>
-        </PageToolbar>
-      </StoryExample>
-    </Stack>
+    <PageToolbar pageIcon="bell" title="Dashboard">
+      <ToolbarButton icon="panel-add" />
+      <ToolbarButton icon="sync">Sync</ToolbarButton>
+    </PageToolbar>
+  );
+};
+
+export const WithClickableTitleAndParent = () => {
+  return (
+    <PageToolbar
+      pageIcon="apps"
+      title="A very long dashboard name"
+      parent="A long folder name"
+      titleHref=""
+      parentHref=""
+      leftItems={[
+        <IconButton name="share-alt" size="lg" key="share" tooltip="Share" />,
+        <IconButton name="favorite" iconType="mono" size="lg" key="favorite" tooltip="Add to favourites" />,
+      ]}
+    >
+      <ToolbarButton icon="panel-add" />
+      <ToolbarButton icon="share-alt" />
+      <ToolbarButton icon="sync">Sync</ToolbarButton>
+      <ToolbarButton icon="cog">Settings </ToolbarButton>
+    </PageToolbar>
+  );
+};
+
+export const GoBackVersion = () => {
+  return (
+    <PageToolbar title="Service overview / Edit panel" onGoBack={() => action('Go back')}>
+      <ToolbarButton icon="cog" />
+      <ToolbarButton icon="save" />
+      <ToolbarButton>Discard</ToolbarButton>
+      <ToolbarButton>Apply</ToolbarButton>
+    </PageToolbar>
   );
 };
 
