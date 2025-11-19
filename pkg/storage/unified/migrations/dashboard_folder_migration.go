27770ccb21725a302d9ee9995d27ce9c7d3214df
Mustafa Sencer Özcan <mustafasencer.ozcan@grafana.com>
2025-11-19 09:11:20 +0100

    fix: minor issues

--- pkg/storage/unified/migrations/dashboard_folder_migration.go
+++ pkg/storage/unified/migrations/dashboard_folder_migration.go
@@ -83,7 +83,7 @@ type orgInfo struct {
 
 func (sp *dashboardAndFolderMigration) getAllOrgs(sess *xorm.Session) ([]orgInfo, error) {
 	var orgs []orgInfo
-	err := sess.Table("org").Where("id > 0").Cols("id", "name").Find(&orgs)
+	err := sess.Table("org").Cols("id", "name").Find(&orgs)
 	if err != nil {
 		return nil, err
 	}
