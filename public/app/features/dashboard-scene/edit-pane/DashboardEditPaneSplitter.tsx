2d08ceb9ee667bc43f0eaada17c74f77cdf45feb
Torkel Ödegaard <torkel@grafana.com>
2025-11-20 16:56:21 +0100

    Adding starbutton and custom precence

--- public/app/features/dashboard-scene/edit-pane/DashboardEditPaneSplitter.tsx
+++ public/app/features/dashboard-scene/edit-pane/DashboardEditPaneSplitter.tsx
@@ -6,10 +6,14 @@ import { selectors } from '@grafana/e2e-selectors';
 import { config, useChromeHeaderHeight } from '@grafana/runtime';
 import { useSceneObjectState } from '@grafana/scenes';
 import { ElementSelectionContext, useSidebar, useStyles2, Sidebar } from '@grafana/ui';
+import { AppChromeUpdate } from 'app/core/components/AppChrome/AppChromeUpdate';
 import NativeScrollbar, { DivScrollElement } from 'app/core/components/NativeScrollbar';
+import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
 
 import { DashboardScene } from '../scene/DashboardScene';
 import { NavToolbarActions } from '../scene/NavToolbarActions';
+import { StarButton } from '../scene/new-toolbar/actions/StarButton';
+import { dynamicDashNavActions } from '../utils/registerDynamicDashNavAction';
 
 import { DashboardEditPaneRenderer } from './DashboardEditPaneRenderer';
 
@@ -24,6 +28,9 @@ export function DashboardEditPaneSplitter({ dashboard, isEditing, body, controls
   const headerHeight = useChromeHeaderHeight();
   const { editPane } = dashboard.state;
   const styles = useStyles2(getStyles, headerHeight ?? 0);
+  const hasUid = Boolean(dashboard.state.uid);
+  const canStar = Boolean(dashboard.state.meta.canStar);
+
   //const [isCollapsed, setIsCollapsed] = useEditPaneCollapsed();
 
   if (!config.featureToggles.dashboardNewLayouts) {
@@ -77,6 +84,14 @@ export function DashboardEditPaneSplitter({ dashboard, isEditing, body, controls
   return (
     <div className={styles.container}>
       <ElementSelectionContext.Provider value={selectionContext}>
+        <AppChromeUpdate
+          breadcrumbActions={
+            <>
+              {hasUid && canStar && <StarButton dashboard={dashboard} />}
+              {renderDynamicNavActions()}
+            </>
+          }
+        />
         <div className={cx(styles.controlsWrapperSticky)} onPointerDown={onClearSelection}>
           {controls}
         </div>
@@ -96,6 +111,19 @@ export function DashboardEditPaneSplitter({ dashboard, isEditing, body, controls
       </ElementSelectionContext.Provider>
     </div>
   );
+}
+
+function renderDynamicNavActions() {
+  const dashboard = getDashboardSrv().getCurrent()!;
+  const showProps = { dashboard };
+
+  return dynamicDashNavActions.left.map((action, index) => {
+    if (action.show(showProps)) {
+      const ActionComponent = action.component;
+      return <ActionComponent key={index} dashboard={dashboard} />;
+    }
+    return null;
+  });
 }
 
 function getStyles(theme: GrafanaTheme2, headerHeight: number) {
