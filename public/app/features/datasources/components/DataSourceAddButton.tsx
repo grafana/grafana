d39f75d8cc073e2b0428ff6cdec9364058ed68c8
Jay Clifford <45856600+Jayclifford345@users.noreply.github.com>
2025-11-20 11:27:04 +0000

    chore(testid): added missing testid to add datasource button (#114169)
    * added missing testid
    
    * added to pages.ts
    
    * added e2e to datasource button
    
    * fixed lint

--- public/app/features/datasources/components/DataSourceAddButton.tsx
+++ public/app/features/datasources/components/DataSourceAddButton.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 
+import { Pages } from '@grafana/e2e-selectors';
 import { Trans } from '@grafana/i18n';
 import { config } from '@grafana/runtime';
 import { LinkButton } from '@grafana/ui';
@@ -16,7 +17,12 @@ export function DataSourceAddButton(): JSX.Element | null {
   }, []);
 
   return canCreateDataSource ? (
-    <LinkButton icon="plus" href={config.appSubUrl + ROUTES.DataSourcesNew} onClick={handleClick}>
+    <LinkButton
+      icon="plus"
+      href={config.appSubUrl + ROUTES.DataSourcesNew}
+      onClick={handleClick}
+      data-testid={Pages.DataSources.dataSourceAddButton}
+    >
       <Trans i18nKey="data-sources.datasource-add-button.label">Add new data source</Trans>
     </LinkButton>
   ) : null;
