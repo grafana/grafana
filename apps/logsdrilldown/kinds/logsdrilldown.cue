23dc6552fd09f80f846f0ea809996ffe6d12c39a
Galen <galen.kistler@grafana.com>
2025-11-21 10:10:52 -0600

    chore: wip - add spec

--- apps/logsdrilldown/kinds/logsdrilldown.cue
+++ apps/logsdrilldown/kinds/logsdrilldown.cue
@@ -1,12 +1,27 @@
 package kinds
 
+#LogsDefaultColumns: {
+		datasource: [...{
+			dsUID: string
+			records: [...{
+				columns: [...string],
+				labels: [...{
+					key: string,
+					value: string
+				}]
+			}]
+		}]
+	}
+
 LogsDrilldownSpecv0alpha1: {
 	defaultFields: [...string] | *[]
 	prettifyJSON: bool
 	wrapLogMessage: bool
 	interceptDismissed: bool
+	defaultColumns: #LogsDefaultColumns
 }
 
+// Users
 logsdrilldownv0alpha1: {
 	kind:       "LogsDrilldown"  // note: must be uppercase
 	schema: {
@@ -14,6 +29,7 @@ logsdrilldownv0alpha1: {
 	}
 }
 
+// Admins
 logsdrilldownDefaultsv0alpha1: {
 	kind:       "LogsDrilldownDefaults"  // note: must be uppercase
 	pluralName: "LogsDrilldownDefaults"
--- apps/logsdrilldown/plugin/src/generated/logsdrilldown/v1alpha1/types.spec.gen.ts
+++ apps/logsdrilldown/plugin/src/generated/logsdrilldown/v1alpha1/types.spec.gen.ts
@@ -1,10 +1,28 @@
 // Code generated - EDITING IS FUTILE. DO NOT EDIT.
 
+export interface LogsDefaultColumns {
+	datasource: {
+		dsUID: string;
+		records: {
+			columns: string[];
+			labels: {
+				key: string;
+				value: string;
+			}[];
+		}[];
+	}[];
+}
+
+export const defaultLogsDefaultColumns = (): LogsDefaultColumns => ({
+	datasource: [],
+});
+
 export interface Spec {
 	defaultFields: string[];
 	prettifyJSON: boolean;
 	wrapLogMessage: boolean;
 	interceptDismissed: boolean;
+	defaultColumns: LogsDefaultColumns;
 }
 
 export const defaultSpec = (): Spec => ({
@@ -12,5 +30,6 @@ export const defaultSpec = (): Spec => ({
 	prettifyJSON: false,
 	wrapLogMessage: false,
 	interceptDismissed: false,
+	defaultColumns: defaultLogsDefaultColumns(),
 });
 
