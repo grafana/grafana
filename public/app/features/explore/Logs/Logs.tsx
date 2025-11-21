39fd5f892557b3b87fde69f6920dd65ccb87fd6c
Galen Kistler <109082771+gtk-grafana@users.noreply.github.com>
2025-11-21 08:05:19 -0600

    Explore: fix logs viz toggle alignment (#114302)
    chore: fix alignment

--- public/app/features/explore/Logs/Logs.tsx
+++ public/app/features/explore/Logs/Logs.tsx
@@ -1235,7 +1235,7 @@ const getStyles = (theme: GrafanaTheme2, wrapLogMessage: boolean, tableHeight: n
     visualisationType: css({
       display: 'flex',
       flex: '1',
-      justifyContent: 'space-between',
+      justifyContent: 'flex-end',
     }),
     visualisationTypeRadio: css({
       margin: `0 0 0 ${theme.spacing(1)}`,
