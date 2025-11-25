089366b2269e46bfef2bfb2e326fadf4e4ff03ac
Paul Marbach <paul.marbach@grafana.com>
2025-11-25 10:16:43 -0800

    Improve FlameGgraph suggestions

--- public/app/plugins/panel/flamegraph/module.tsx
+++ public/app/plugins/panel/flamegraph/module.tsx
@@ -1,19 +1,29 @@
 import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
 import { checkFields } from '@grafana/flamegraph';
-import { showDefaultSuggestion } from 'app/features/panel/suggestions/utils';
 
 import { FlameGraphPanel } from './FlameGraphPanel';
+import { Options } from './types';
 
 const flamegraphConfigOptions = [FieldConfigProperty.Unit, FieldConfigProperty.Decimals];
 
-export const plugin = new PanelPlugin(FlameGraphPanel)
+export const plugin = new PanelPlugin<Options>(FlameGraphPanel)
   // check that the first frame of the data has the required fields for a flamegraph
-  .setSuggestionsSupplier(
-    showDefaultSuggestion((ds) => {
-      const firstFrame = ds.rawFrames?.[0];
-      return Boolean(firstFrame && !checkFields(firstFrame));
-    })
-  )
+  .setSuggestionsSupplier((ds) => {
+    if (!ds.rawFrames?.some((frame) => checkFields(frame) === undefined)) {
+      return;
+    }
+
+    return [
+      {
+        cardOptions: {
+          previewModifier: (s) => {
+            s.options = s.options || {};
+            s.options.showFlameGraphOnly = true;
+          },
+        },
+      },
+    ];
+  })
   .useFieldConfig({
     disableStandardOptions: Object.values(FieldConfigProperty).filter((v) => !flamegraphConfigOptions.includes(v)),
   });
