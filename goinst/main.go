4b463a54537a2ed0f8ff93228e67e77e2f360b48
grambbledook <denis.vodopianov@grafana.com>
2025-11-25 22:00:07 +0100

    cat toolexec.log

--- goinst/main.go
+++ goinst/main.go
@@ -22,7 +22,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	f, err := os.OpenFile("toolexec.logger", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("toolexec.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
