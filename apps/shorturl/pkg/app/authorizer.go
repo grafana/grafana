4fe3e37f89ff84b547d5e0666923be4c93144afd
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2025-11-25 14:53:54 -0300

    simplifiying authorizer since authentication is handled beforehand

--- apps/shorturl/pkg/app/authorizer.go
+++ apps/shorturl/pkg/app/authorizer.go
@@ -3,7 +3,6 @@ package app
 import (
 	"context"
 
-	"github.com/grafana/grafana/pkg/apimachinery/identity"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 )
 
@@ -15,23 +14,7 @@ func GetAuthorizer() authorizer.Authorizer {
 			return authorizer.DecisionNoOpinion, "", nil
 		}
 
-		// Check for service identity
-		if identity.IsServiceIdentity(ctx) {
-			return authorizer.DecisionAllow, "", nil
-		}
-
-		// Check if a user is logged in
-		u, err := identity.GetRequester(ctx)
-		if err != nil {
-			return authorizer.DecisionDeny, "valid user is required", err
-		}
-
-		// This shouldn't be necessary since above function returns err or a user
-		// however, I prefer to be explicit when returning allow and leave the last return as Deny
-		if u != nil {
-			return authorizer.DecisionAllow, "", nil
-		}
-
-		return authorizer.DecisionDeny, "forbidden", nil
+		// Any authenticated user can access the API
+		return authorizer.DecisionAllow, "", nil
 	})
 }
