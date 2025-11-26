1fd8ddc3a4825322a4452f9012582f06b7ba48e2
Jack Westbrook <jack.westbrook@gmail.com>
2025-11-26 10:23:08 +0100

    Wip

--- pkg/build/e2e/service.go
+++ pkg/build/e2e/service.go
@@ -68,10 +68,9 @@ func GrafanaService(ctx context.Context, d *dagger.Client, opts GrafanaServiceOp
 	}
 	log.Println("done getting node version")
 
-	src :=
-		WithGrafanaFrontend(d.Container().From(NodeImage(nodeVersion)), opts.GrafanaDir).
-			WithExec([]string{"yarn", "install", "--immutable"}).
-			WithExec([]string{"yarn", "e2e:plugin:build"})
+	src := WithGrafanaFrontend(d.Container().From(NodeImage(nodeVersion)), opts.GrafanaDir).
+		WithExec([]string{"yarn", "install", "--immutable"}).
+		WithExec([]string{"yarn", "e2e:plugin:build"})
 
 	// _/ubuntu:latest sticks to latest LTS.
 	// We need ubuntu to support the image renderer plugin, which assumes glibc.
