837dfc54a95d90657e796df24666ebc366661909
Jack Westbrook <jack.westbrook@gmail.com>
2025-11-26 10:07:21 +0100

    Wip

--- pkg/build/e2e/run.go
+++ pkg/build/e2e/run.go
@@ -11,7 +11,7 @@ func RunSuite(d *dagger.Client, svc *dagger.Service, src *dagger.Directory, cach
 		"./e2e-runner cypress --start-grafana=false --cypress-video"+
 			" --grafana-base-url http://grafana:3001 --suite %s %s", suite, runnerFlags)
 
-	return WithYarnCache(WithGrafanaFrontend(d.Container().From("cypress/included:13.1.0"), src), cache).
+	return WithGrafanaFrontend(d.Container().From("cypress/included:13.1.0"), src).
 		WithWorkdir("/src").
 		WithServiceBinding("grafana", svc).
 		WithExec([]string{"yarn", "install", "--immutable"}).
