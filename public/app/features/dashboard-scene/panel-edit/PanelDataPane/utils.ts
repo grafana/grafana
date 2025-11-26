51203f2399732077316d6b24627968769924de29
Develer <yeryomin.igor@gmail.com>
2025-11-26 18:31:08 +0100

    remove unused function

--- public/app/features/dashboard-scene/panel-edit/PanelDataPane/utils.ts
+++ public/app/features/dashboard-scene/panel-edit/PanelDataPane/utils.ts
@@ -1,12 +1,3 @@
-import { DataQuery } from '@grafana/schema';
-import { ExpressionQueryType } from 'app/features/expressions/types';
-
-export function findSqlExpression(queries: DataQuery[]) {
-  return queries.find((query) => {
-    return typeof query === 'object' && query !== null && 'type' in query && query.type === ExpressionQueryType.sql;
-  });
-}
-
 export function scrollToQueryRow(refId: string) {
   // Query rows use uniqueId(refId + '_') for their internal id
   // The aria-controls attribute will be like "A_1" for refId "A"
