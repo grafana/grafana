94bd1e1c86eb832f6f275454ca67446d60595c5b
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-11-26 13:41:06 +0100

    App Plugins: Allow to define experimental pages (#114232)

--- pkg/services/navtree/navtreeimpl/applinks.go
+++ pkg/services/navtree/navtreeimpl/applinks.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/grafana/grafana/pkg/middleware"
 	"github.com/grafana/grafana/pkg/plugins"
 	ac "github.com/grafana/grafana/pkg/services/accesscontrol"
 	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
@@ -128,6 +129,10 @@ func (s *ServiceImpl) processAppPlugin(plugin pluginstore.Plugin, c *contextmode
 		}
 
 		if include.Type == "page" {
+			if !middleware.PageIsFeatureToggleEnabled(c.Req.Context(), include.Path) {
+				s.log.Debug("Skipping page", "plugin", plugin.ID, "path", include.Path)
+				continue
+			}
 			link := &navtree.NavLink{
 				Text:     include.Name,
 				Icon:     include.Icon,
