40dc372cfef18798d4f62d5f62d1b9aed767d1db
Jack Westbrook <jack.westbrook@gmail.com>
2025-11-26 09:13:41 +0100

    chore(e2e-plugins): do workspace protocols resolve in ci

--- e2e-playwright/test-plugins/grafana-test-datasource/module.ts
+++ e2e-playwright/test-plugins/grafana-test-datasource/module.ts
@@ -3,6 +3,12 @@ import { DataSource } from './datasource';
 import { ConfigEditor } from './components/ConfigEditor';
 import { QueryEditor } from './components/QueryEditor';
 import { MyQuery, MyDataSourceOptions } from './types';
+import pluginJson from './plugin.json';
+import { initPluginTranslations, t } from '@grafana/i18n';
+
+await initPluginTranslations(pluginJson.id);
+
+console.warn(t('grafana-test-datasource.config', 'my message'));
 
 export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
