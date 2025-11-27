796c7ce6f29b05e1cdc3783222487c7f8e499204
Alexander Zobnin <alexanderzobnin@gmail.com>
2025-11-27 11:29:24 +0100

    Zanzana: Role write APIs

---
+++ pkg/services/authz/zanzana/server/server_mutate_roles.go
@@ -0,0 +1,120 @@
+package server
+
+import (
+	"context"
+	"strings"
+
+	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+
+	iamv0 "github.com/grafana/grafana/apps/iam/pkg/apis/iam/v0alpha1"
+	authzextv1 "github.com/grafana/grafana/pkg/services/authz/proto/v1"
+	"github.com/grafana/grafana/pkg/services/authz/zanzana"
+	"github.com/grafana/grafana/pkg/services/authz/zanzana/common"
+)
+
+func (s *Server) mutateRoles(ctx context.Context, store *storeInfo, operations []*authzextv1.MutateOperation) error {
+	ctx, span := s.tracer.Start(ctx, "server.mutateRoles")
+	defer span.End()
+
+	writeTuples := make([]*openfgav1.TupleKey, 0)
+	deleteTuples := make([]*openfgav1.TupleKeyWithoutCondition, 0)
+
+	for _, operation := range operations {
+		switch op := operation.Operation.(type) {
+		case *authzextv1.MutateOperation_CreateRole:
+			tuples, err := getRoleTuples(op.CreateRole.RoleName, op.CreateRole.Permissions)
+			if err != nil {
+				return err
+			}
+			writeTuples = append(writeTuples, tuples...)
+		case *authzextv1.MutateOperation_DeleteRole:
+			tuples, err := getRoleTuples(op.DeleteRole.RoleName, op.DeleteRole.Permissions)
+			if err != nil {
+				return err
+			}
+			deletes := make([]*openfgav1.TupleKeyWithoutCondition, 0, len(tuples))
+			for _, tuple := range tuples {
+				deletes = append(deletes, common.ToOpenFGADeleteTupleKey(tuple))
+			}
+			deleteTuples = append(deleteTuples, deletes...)
+		default:
+			s.logger.Debug("unsupported mutate operation", "operation", op)
+		}
+	}
+
+	writeReq := &openfgav1.WriteRequest{
+		StoreId:              store.ID,
+		AuthorizationModelId: store.ModelID,
+	}
+	if len(writeTuples) > 0 {
+		writeReq.Writes = &openfgav1.WriteRequestWrites{
+			TupleKeys:   writeTuples,
+			OnDuplicate: "ignore",
+		}
+	}
+	if len(deleteTuples) > 0 {
+		writeReq.Deletes = &openfgav1.WriteRequestDeletes{
+			TupleKeys: deleteTuples,
+			OnMissing: "ignore",
+		}
+	}
+
+	_, err := s.openfga.Write(ctx, writeReq)
+	if err != nil {
+		s.logger.Error("failed to write resource role binding tuples", "error", err)
+		return err
+	}
+
+	return nil
+}
+
+func getRoleTuples(roleName string, rolePermissions []*authzextv1.RolePermission) ([]*openfgav1.TupleKey, error) {
+	permissions := make([]iamv0.RolespecPermission, len(rolePermissions))
+	for i, p := range rolePermissions {
+		permissions[i] = iamv0.RolespecPermission{
+			Action: p.Action,
+			Scope:  p.Scope,
+		}
+	}
+	return convertRolePermissionsToTuples(roleName, permissions)
+}
+
+// convertRolePermissionsToTuples converts role permissions (action/scope) to v1 TupleKey format
+// using the shared zanzana.ConvertRolePermissionsToTuples utility and common.ToAuthzExtTupleKeys
+func convertRolePermissionsToTuples(roleUID string, permissions []iamv0.RolespecPermission) ([]*openfgav1.TupleKey, error) {
+	// Convert IAM permissions to zanzana.RolePermission format
+	rolePerms := make([]zanzana.RolePermission, 0, len(permissions))
+	for _, perm := range permissions {
+		// Split the scope to get kind, attribute, identifier
+		kind, _, identifier := splitScope(perm.Scope)
+		rolePerms = append(rolePerms, zanzana.RolePermission{
+			Action:     perm.Action,
+			Kind:       kind,
+			Identifier: identifier,
+		})
+	}
+
+	// Translate to Zanzana tuples
+	tuples, err := zanzana.ConvertRolePermissionsToTuples(roleUID, rolePerms)
+	if err != nil {
+		return nil, err
+	}
+
+	return tuples, nil
+}
+
+func splitScope(scope string) (string, string, string) {
+	if scope == "" {
+		return "", "", ""
+	}
+
+	fragments := strings.Split(scope, ":")
+	switch l := len(fragments); l {
+	case 1: // Splitting a wildcard scope "*" -> kind: "*"; attribute: "*"; identifier: "*"
+		return fragments[0], fragments[0], fragments[0]
+	case 2: // Splitting a wildcard scope with specified kind "dashboards:*" -> kind: "dashboards"; attribute: "*"; identifier: "*"
+		return fragments[0], fragments[1], fragments[1]
+	default: // Splitting a scope with all fields specified "dashboards:uid:my_dash" -> kind: "dashboards"; attribute: "uid"; identifier: "my_dash"
+		return fragments[0], fragments[1], strings.Join(fragments[2:], ":")
+	}
+}
