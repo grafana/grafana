558dbdf9bd0b265d0c183b8386c30eb551416ed8
Mihai Doarna <mihai.doarna@grafana.com>
2025-11-27 15:55:21 +0200

    make gofmt

--- apps/iam/pkg/apis/iam/v0alpha1/getsearchteams_request_params_object_gen.go
+++ apps/iam/pkg/apis/iam/v0alpha1/getsearchteams_request_params_object_gen.go
@@ -3,31 +3,31 @@
 package v0alpha1
 
 import (
-    "github.com/grafana/grafana-app-sdk/resource"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/grafana/grafana-app-sdk/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 type GetSearchTeamsRequestParamsObject struct {
-    metav1.TypeMeta `json:",inline"`
-    GetSearchTeamsRequestParams `json:",inline"`
+	metav1.TypeMeta             `json:",inline"`
+	GetSearchTeamsRequestParams `json:",inline"`
 }
 
 func NewGetSearchTeamsRequestParamsObject() *GetSearchTeamsRequestParamsObject {
-    return &GetSearchTeamsRequestParamsObject{}
+	return &GetSearchTeamsRequestParamsObject{}
 }
 
 func (o *GetSearchTeamsRequestParamsObject) DeepCopyObject() runtime.Object {
-    dst := NewGetSearchTeamsRequestParamsObject()
-    o.DeepCopyInto(dst)
-    return dst
+	dst := NewGetSearchTeamsRequestParamsObject()
+	o.DeepCopyInto(dst)
+	return dst
 }
 
 func (o *GetSearchTeamsRequestParamsObject) DeepCopyInto(dst *GetSearchTeamsRequestParamsObject) {
-    dst.TypeMeta.APIVersion = o.TypeMeta.APIVersion
-    dst.TypeMeta.Kind = o.TypeMeta.Kind
-    dstGetSearchTeamsRequestParams := GetSearchTeamsRequestParams{}
-    _ = resource.CopyObjectInto(&dstGetSearchTeamsRequestParams, &o.GetSearchTeamsRequestParams)
+	dst.TypeMeta.APIVersion = o.TypeMeta.APIVersion
+	dst.TypeMeta.Kind = o.TypeMeta.Kind
+	dstGetSearchTeamsRequestParams := GetSearchTeamsRequestParams{}
+	_ = resource.CopyObjectInto(&dstGetSearchTeamsRequestParams, &o.GetSearchTeamsRequestParams)
 }
 
 var _ runtime.Object = NewGetSearchTeamsRequestParamsObject()
