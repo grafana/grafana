f935a6579f9e30c05f4f5b3d8087297d5ff41670
Kristina Durivage <kristina.durivage@grafana.com>
2025-11-30 10:32:34 -0600

    properly split logic

--- public/app/features/correlations/CorrelationsPageWrapper.tsx
+++ public/app/features/correlations/CorrelationsPageWrapper.tsx
@@ -6,46 +6,52 @@ import { GetCorrelationsParams } from './types';
 import { useCorrelations } from './useCorrelations';
 import { toEnrichedCorrelationDataK8s } from './useCorrelationsK8s';
 
-export default function CorrelationsPageWrapper() {
-  // I cannot use these conditionally, is this okay?
+function CorrelationsPageLegacy() {
   const { remove, get } = useCorrelations();
+  return (
+    <CorrelationsPage
+      fetchCorrelations={get.execute}
+      correlations={get.value}
+      isLoading={get.loading}
+      error={get.error}
+      removeFn={remove.execute}
+    />
+  );
+}
+
+function CorrelationsPageAppPlatform() {
+  // todo remove fake limit
   const { currentData, isLoading, error } = useListCorrelationQuery({ limit: 10 });
+  const enrichedCorrelations = (correlations?: CorrelationList) => {
+    return correlations !== undefined
+      ? correlations.items.map((item) => toEnrichedCorrelationDataK8s(item)).filter((i) => i !== undefined)
+      : [];
+  };
+
+  // we cant do a straight refetch, we have to pass in new pages if necessary
+  const enhRefetch = (params: GetCorrelationsParams): Promise<CorrelationsData> => {
+    return new Promise(() => enrichedCorrelations(currentData));
+  };
+
+  return (
+    <CorrelationsPage
+      fetchCorrelations={enhRefetch}
+      correlations={{
+        correlations: enrichedCorrelations(currentData),
+        page: 0,
+        limit: 1000,
+        totalCount: enrichedCorrelations.length,
+      }}
+      isLoading={isLoading}
+      error={error as Error}
+    />
+  );
+}
+
+export default function CorrelationsPageWrapper() {
   if (config.featureToggles.kubernetesCorrelations) {
-    const enrichedCorrelations = (correlations?: CorrelationList) => {
-      return correlations !== undefined
-        ? correlations.items.map((item) => toEnrichedCorrelationDataK8s(item)).filter((i) => i !== undefined)
-        : [];
-    };
-
-    console.log(currentData?.metadata.remainingItemCount);
-
-    // we cant do a straight refetch, we have to pass in new pages if necessary
-    const enhRefetch = (params: GetCorrelationsParams): Promise<CorrelationsData> => {
-      return new Promise(() => enrichedCorrelations(currentData));
-    };
-
-    return (
-      <CorrelationsPage
-        fetchCorrelations={enhRefetch}
-        correlations={{
-          correlations: enrichedCorrelations(currentData),
-          page: 0,
-          limit: 1000,
-          totalCount: enrichedCorrelations.length,
-        }}
-        isLoading={isLoading}
-        error={error as Error}
-      />
-    );
-  } else {
-    return (
-      <CorrelationsPage
-        fetchCorrelations={get.execute}
-        correlations={get.value}
-        isLoading={get.loading}
-        error={get.error}
-        removeFn={remove.execute}
-      />
-    );
+    return <CorrelationsPageAppPlatform />;
   }
+
+  return <CorrelationsPageLegacy />;
 }
