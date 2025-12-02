f8ac92de64e16b288e835a529d2a261bedcd0b0c
Torkel Ödegaard <torkel@grafana.com>
2025-12-02 12:23:17 +0100

    Active state

--- public/app/features/explore/SecondaryActions.tsx
+++ public/app/features/explore/SecondaryActions.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/css';
 import { CoreApp, GrafanaTheme2 } from '@grafana/data';
 import { Components, selectors } from '@grafana/e2e-selectors';
 import { Trans, t } from '@grafana/i18n';
-import { Button, ToolbarButton, useTheme2 } from '@grafana/ui';
+import { ToolbarButton, useTheme2 } from '@grafana/ui';
 
 import { useQueriesDrawerContext } from './QueriesDrawer/QueriesDrawerContext';
 import { useQueryLibraryContext } from './QueryLibrary/QueryLibraryContext';
@@ -76,16 +76,16 @@ export function SecondaryActions({
           )}
         </>
       )}
-      <Button
+      <ToolbarButton
         key="query-history"
-        variant={'secondary'}
+        variant={drawerOpened ? 'active' : 'canvas'}
         aria-label={t('explore.secondary-actions.query-history-button-aria-label', 'Query history')}
         onClick={() => setDrawerOpened(!drawerOpened)}
         data-testid={Components.QueryTab.queryHistoryButton}
         icon="history"
       >
         <Trans i18nKey="explore.secondary-actions.query-history-button">Query history</Trans>
-      </Button>
+      </ToolbarButton>
       <ToolbarButton
         variant={queryInspectorButtonActive ? 'active' : 'canvas'}
         aria-label={t('explore.secondary-actions.query-inspector-button-aria-label', 'Query inspector')}
