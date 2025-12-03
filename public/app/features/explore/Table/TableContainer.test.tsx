b114f33c7fdc8f36e963fc0c86e6fccbe59b8536
Piotr Jamróz <pm.jamroz@gmail.com>
2025-12-03 12:20:28 +0100

    Update tests

--- public/app/features/explore/Table/TableContainer.test.tsx
+++ public/app/features/explore/Table/TableContainer.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { DataFrame, FieldType, getDefaultTimeRange, InternalTimeZones, toDataFrame } from '@grafana/data';
 
