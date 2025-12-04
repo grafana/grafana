dcb4a9d31f7cecd481e1e4f619cd7dcd2a5fe95e
Owen Smallwood <owen.smallwood@grafana.com>
2025-12-03 21:48:30 -0600

    formatting

--- apps/quotas/pkg/apis/quotas/v0alpha1/getusage_request_params_object_gen.go
+++ apps/quotas/pkg/apis/quotas/v0alpha1/getusage_request_params_object_gen.go
@@ -3,31 +3,31 @@
 package v0alpha1
 
 import (
-    "github.com/grafana/grafana-app-sdk/resource"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/grafana/grafana-app-sdk/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 type GetUsageRequestParamsObject struct {
-    metav1.TypeMeta `json:",inline"`
-    GetUsageRequestParams `json:",inline"`
+	metav1.TypeMeta       `json:",inline"`
+	GetUsageRequestParams `json:",inline"`
 }
 
 func NewGetUsageRequestParamsObject() *GetUsageRequestParamsObject {
-    return &GetUsageRequestParamsObject{}
+	return &GetUsageRequestParamsObject{}
 }
 
 func (o *GetUsageRequestParamsObject) DeepCopyObject() runtime.Object {
-    dst := NewGetUsageRequestParamsObject()
-    o.DeepCopyInto(dst)
-    return dst
+	dst := NewGetUsageRequestParamsObject()
+	o.DeepCopyInto(dst)
+	return dst
 }
 
 func (o *GetUsageRequestParamsObject) DeepCopyInto(dst *GetUsageRequestParamsObject) {
-    dst.TypeMeta.APIVersion = o.TypeMeta.APIVersion
-    dst.TypeMeta.Kind = o.TypeMeta.Kind
-    dstGetUsageRequestParams := GetUsageRequestParams{}
-    _ = resource.CopyObjectInto(&dstGetUsageRequestParams, &o.GetUsageRequestParams)
+	dst.TypeMeta.APIVersion = o.TypeMeta.APIVersion
+	dst.TypeMeta.Kind = o.TypeMeta.Kind
+	dstGetUsageRequestParams := GetUsageRequestParams{}
+	_ = resource.CopyObjectInto(&dstGetUsageRequestParams, &o.GetUsageRequestParams)
 }
 
 var _ runtime.Object = NewGetUsageRequestParamsObject()
