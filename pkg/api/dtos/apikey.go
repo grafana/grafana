6c02b5e9073520c250eeed800ccbb749541549d3
Mariell Hoversholm <mariell@mardroemmar.dev>
2025-12-04 14:27:25 +0100

    fix: fix checksum of token

--- pkg/api/dtos/apikey.go
+++ pkg/api/dtos/apikey.go
@@ -5,6 +5,6 @@ type NewApiKeyResult struct {
 	ID int64 `json:"id"`
 	// example: grafana
 	Name string `json:"name"`
-	// example: glsa_iNValIdinValiDinvalidinvalidinva_lidinVal
+	// example: glsa_iNValIdinValiDinvalidinvalidinva_5b582697
 	Key string `json:"key"`
 }
