47a79b8fc5ea9e6c92520c251000a7e1f0250f0b
Will Assis <william@williamassis.com>
2025-12-05 16:27:29 -0300

    update test snapshots

--- pkg/storage/unified/sql/testdata/mysql--resource_history_update_rv-single path.sql
+++ pkg/storage/unified/sql/testdata/mysql--resource_history_update_rv-single path.sql
@@ -6,34 +6,6 @@ SET `resource_version` = (
     END
 ), `key_path` = (
     CASE
-    WHEN `guid` = 'guid1' THEN CONCAT(
-      'unified', CHAR(47), 'data', CHAR(47),
-      `group`, CHAR(47),
-      `resource`, CHAR(47),
-      `namespace`, CHAR(47),
-      `name`, CHAR(47),
-      CAST(((((CAST(123 AS SIGNED) / 1000) - 1288834974657) << 22) + (CAST(123 AS SIGNED) % 1000 )) AS SIGNED),
-      CHAR(126),
-      CASE `action`
-        WHEN 1 THEN 'created'
-        WHEN 2 THEN 'updated'
-        WHEN 3 THEN 'deleted'
-      END, CHAR(126),
-      COALESCE(`folder`, ''))
-    WHEN `guid` = 'guid2' THEN CONCAT(
-      'unified', CHAR(47), 'data', CHAR(47),
-      `group`, CHAR(47),
-      `resource`, CHAR(47),
-      `namespace`, CHAR(47),
-      `name`, CHAR(47),
-      CAST(((((CAST(456 AS SIGNED) / 1000) - 1288834974657) << 22) + (CAST(456 AS SIGNED) % 1000 )) AS SIGNED),
-      CHAR(126),
-      CASE `action`
-        WHEN 1 THEN 'created'
-        WHEN 2 THEN 'updated'
-        WHEN 3 THEN 'deleted'
-      END, CHAR(126),
-      COALESCE(`folder`, ''))
     END
 )
 WHERE `guid` IN (
--- pkg/storage/unified/sql/testdata/postgres--resource_history_update_rv-single path.sql
+++ pkg/storage/unified/sql/testdata/postgres--resource_history_update_rv-single path.sql
@@ -6,34 +6,6 @@ SET "resource_version" = (
     END
 ), "key_path" = (
     CASE
-    WHEN "guid" = 'guid1' THEN CONCAT(
-      'unified', CHR(47), 'data', CHR(47),
-      "group", CHR(47),
-      "resource", CHR(47),
-      "namespace", CHR(47),
-      "name", CHR(47),
-      CAST(((((CAST(123 AS BIGINT) / 1000) - 1288834974657) << 22) + (CAST(123 AS BIGINT) % 1000 )) AS BIGINT),
-      CHR(126),
-      CASE "action"
-        WHEN 1 THEN 'created'
-        WHEN 2 THEN 'updated'
-        WHEN 3 THEN 'deleted'
-      END, CHR(126),
-      COALESCE("folder", ''))
-    WHEN "guid" = 'guid2' THEN CONCAT(
-      'unified', CHR(47), 'data', CHR(47),
-      "group", CHR(47),
-      "resource", CHR(47),
-      "namespace", CHR(47),
-      "name", CHR(47),
-      CAST(((((CAST(456 AS BIGINT) / 1000) - 1288834974657) << 22) + (CAST(456 AS BIGINT) % 1000 )) AS BIGINT),
-      CHR(126),
-      CASE "action"
-        WHEN 1 THEN 'created'
-        WHEN 2 THEN 'updated'
-        WHEN 3 THEN 'deleted'
-      END, CHR(126),
-      COALESCE("folder", ''))
     END
 )
 WHERE "guid" IN (
