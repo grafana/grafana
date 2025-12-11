ccbb562685c8b069c7e4f5846fb903f63291f59b
Jack Westbrook <jack.westbrook@gmail.com>
2025-12-11 13:11:50 +0100

    build(packages): prevent empty exports added to package.json during pack

--- scripts/prepare-npm-package.js
+++ scripts/prepare-npm-package.js
@@ -5,27 +5,30 @@ const cwd = process.cwd();
 
 try {
   const pkgJson = await PackageJson.load(cwd);
-  const pkgJsonExports = pkgJson.content.exports ?? {};
+  const pkgJsonExports = pkgJson.content.exports;
 
-  // Remove all exports that only contain a single key '@grafana-app/source'
-  // as these will not resolve when validating packages with attw because the
-  // source code is not available in the tarball.
-  for (const [key, val] of Object.entries(pkgJsonExports)) {
-    if (
-      val !== null &&
-      typeof val === 'object' &&
-      Object.keys(val).length === 1 &&
-      Object.keys(val)[0] === '@grafana-app/source'
-    ) {
-      delete pkgJsonExports[key];
+  // skip packages without exports otherwise consumers cannot import anything from the package
+  if (pkgJsonExports && typeof pkgJsonExports === 'object') {
+    // Remove all exports that only contain a single key '@grafana-app/source'
+    // as these will not resolve when validating packages with attw because the
+    // source code is not available in the tarball.
+    for (const [key, val] of Object.entries(pkgJsonExports)) {
+      if (
+        val !== null &&
+        typeof val === 'object' &&
+        Object.keys(val).length === 1 &&
+        Object.keys(val)[0] === '@grafana-app/source'
+      ) {
+        delete pkgJsonExports[key];
+      }
     }
+
+    pkgJson.update({
+      exports: pkgJsonExports,
+    });
+
+    await pkgJson.save();
   }
-
-  pkgJson.update({
-    exports: pkgJsonExports,
-  });
-
-  await pkgJson.save();
 } catch (e) {
   console.error(e);
   process.exit(1);
