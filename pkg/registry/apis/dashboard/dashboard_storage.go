bcc67124ccf31930aa8c6298f90ca7ea16eb7810
Ryan McKinley <ryantxu@gmail.com>
2025-12-11 09:26:35 +0300

    Live: Move dashboard events from the raw http server to the apiserver (#115066)

--- pkg/registry/apis/dashboard/dashboard_storage.go
+++ pkg/registry/apis/dashboard/dashboard_storage.go
@@ -7,20 +7,41 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/rest"
 
+	"github.com/grafana/grafana-app-sdk/logging"
 	grafanarest "github.com/grafana/grafana/pkg/apiserver/rest"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
 	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
+	"github.com/grafana/grafana/pkg/services/live"
 )
 
-// dashboardStoragePermissionWrapper is a wrapper around the grafanarest.Storage that adds dashboard permissions handling
-// when dual writing is enabled.
-type dashboardStoragePermissionWrapper struct {
-	dashboardPermissionsSvc accesscontrol.DashboardPermissionsService
+// dashboardStorageWrapper is a wrapper around the grafanarest.Storage so it will:
+// 1. support adds dashboard permissions handling
+// 2. broadcast changes to grafana live
+// when running in single tenant mode
+type dashboardStorageWrapper struct {
 	grafanarest.Storage
+
+	dashboardPermissionsSvc accesscontrol.DashboardPermissionsService
+	live                    live.DashboardActivityChannel
 }
 
-func (d dashboardStoragePermissionWrapper) Delete(ctx context.Context, name string, deleteValidation rest.ValidateObjectFunc, options *metav1.DeleteOptions) (runtime.Object, bool, error) {
-	info, err := request.NamespaceInfoFrom(ctx, true)
+func (d dashboardStorageWrapper) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc, forceAllowCreate bool, options *metav1.UpdateOptions) (runtime.Object, bool, error) {
+	ns, err := request.NamespaceInfoFrom(ctx, true)
+	if err != nil {
+		return nil, false, err
+	}
+
+	obj, created, err := d.Storage.Update(ctx, name, objInfo, createValidation, updateValidation, forceAllowCreate, options)
+	if err == nil && ns.OrgID > 0 && d.live != nil {
+		if err := d.live.DashboardSaved(ns.OrgID, name); err != nil {
+			logging.FromContext(ctx).Info("live dashboard update failed", "err", err)
+		}
+	}
+	return obj, created, err
+}
+
+func (d dashboardStorageWrapper) Delete(ctx context.Context, name string, deleteValidation rest.ValidateObjectFunc, options *metav1.DeleteOptions) (runtime.Object, bool, error) {
+	ns, err := request.NamespaceInfoFrom(ctx, true)
 	if err != nil {
 		return nil, false, err
 	}
@@ -28,7 +49,12 @@ func (d dashboardStoragePermissionWrapper) Delete(ctx context.Context, name stri
 	if err != nil {
 		return obj, async, err
 	}
-	if accessErr := d.dashboardPermissionsSvc.DeleteResourcePermissions(ctx, info.OrgID, name); accessErr != nil {
+	if ns.OrgID > 0 && d.live != nil {
+		if err := d.live.DashboardDeleted(ns.OrgID, name); err != nil {
+			logging.FromContext(ctx).Info("live dashboard update failed", "err", err)
+		}
+	}
+	if accessErr := d.dashboardPermissionsSvc.DeleteResourcePermissions(ctx, ns.OrgID, name); accessErr != nil {
 		return obj, async, accessErr
 	}
 	return obj, async, nil
