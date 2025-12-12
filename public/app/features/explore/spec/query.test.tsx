4a0e9204b3c820394b80a6a492e12c04aee458fa
Ashley Harrison <ashley.harrison@grafana.com>
2025-12-12 15:55:19 +0000

    fix explore query tests

--- public/app/features/explore/spec/query.test.tsx
+++ public/app/features/explore/spec/query.test.tsx
@@ -50,7 +50,7 @@ describe('Explore: handle running/not running query', () => {
     jest.mocked(datasources.loki.query).mockReturnValueOnce(makeLogsQueryResponse());
 
     // Make sure we render the logs panel
-    await screen.findByText(/^Logs$/);
+    await screen.findByRole('heading', { name: /^Logs$/ });
 
     // Make sure we render the log line
     await screen.findByText(/custom log line/i);
