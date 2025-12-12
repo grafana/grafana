b81336fb8d4e1c2808e7e3d780fccad4e93a6c70
Daniele Ferru <daniele.ferru@grafana.com>
2025-12-12 19:26:56 +0100

    adding some more integration tests

--- pkg/tests/apis/provisioning/connection_test.go
+++ pkg/tests/apis/provisioning/connection_test.go
@@ -2,11 +2,13 @@ package provisioning
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/grafana/grafana/pkg/util/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -58,5 +60,53 @@ func TestIntegrationProvisioning_CreatingAndGettingConnections(t *testing.T) {
 
 		require.Contains(t, output.Object, "secure", "object should contain secure")
 		assert.Contains(t, output.Object["secure"], "privateKey", "secure should contain PrivateKey")
+	})
+
+	t.Run("viewer can't create or get connection", func(t *testing.T) {
+		connection := &unstructured.Unstructured{Object: map[string]any{
+			"apiVersion": "provisioning.grafana.app/v0alpha1",
+			"kind":       "Connection",
+			"metadata": map[string]any{
+				"name":      "connection",
+				"namespace": "default",
+			},
+			"spec": map[string]any{
+				"type": "github",
+				"github": map[string]any{
+					"appID":          "123456",
+					"installationID": "454545",
+				},
+			},
+			"secure": map[string]any{
+				"privateKey": map[string]any{
+					"create": "someSecret",
+				},
+			},
+		}}
+
+		result := helper.ViewerREST.Post().
+			Namespace("default").
+			Resource("connections").
+			Body(connection).
+			Do(t.Context())
+
+		require.NotNil(t, result.Error())
+		err := &k8serrors.StatusError{}
+		require.True(t, errors.As(result.Error(), &err))
+		assert.Equal(t, metav1.StatusReasonForbidden, err.Status().Reason)
+		assert.Contains(t, err.Status().Message, "User \"viewer\" cannot create resource \"connections\"")
+		assert.Contains(t, err.Status().Message, "admin role is required")
+
+		result = helper.ViewerREST.Get().
+			Namespace("default").
+			Resource("connections").
+			Name("connection").
+			Do(t.Context())
+		require.NotNil(t, result.Error())
+		err = &k8serrors.StatusError{}
+		require.True(t, errors.As(result.Error(), &err))
+		assert.Equal(t, metav1.StatusReasonForbidden, err.Status().Reason)
+		assert.Contains(t, err.Status().Message, "User \"viewer\" cannot get resource \"connections\"")
+		assert.Contains(t, err.Status().Message, "admin role is required")
 	})
 }
