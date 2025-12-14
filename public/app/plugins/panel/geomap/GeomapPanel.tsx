1c968b7b5dcf8ead93ea9dded3ed287f5a81bec8
dashie <dashie.dev@gmail.com>
2025-12-14 12:44:20 +0100

    Geomap: log to console if "options.controls.showDebug" is true

--- public/app/plugins/panel/geomap/GeomapPanel.tsx
+++ public/app/plugins/panel/geomap/GeomapPanel.tsx
@@ -378,37 +378,7 @@ export class GeomapPanel extends Component<Props, State> {
     return view;
   };
 
-  /**
-   * Updates the dashboard variable `mapViewData` with the view extent value.
-   * Use a debounce strategy to wait the user stop dragging or zooming the map.
-   */
-  private timeoutId: NodeJS.Timeout | null = null; // for debounce
-
-  updateGeoVariables = (view: View) => {
-    const bounds = view.calculateExtent();
-    const bounds4326 = transformExtent(bounds, 'EPSG:3857', 'EPSG:4326');
-    console.log('GeomapPanel.updateGeoVariables', bounds4326);
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-    this.timeoutId = setTimeout(() => {
-      locationService.partial({ 'var-mapViewData': `${bounds4326}` }, true);
-    }, 500);
-  };
-
-  private viewListernerKey: EventsKey | null = null;
-
   initViewExtent(view: View, config: MapViewConfig) {
-    if (config.dashboardVariable) {
-      if (this.viewListernerKey != null) {
-        view.un('change', this.viewListernerKey.listener);
-      }
-      this.viewListernerKey = view.on('change', () => {
-        this.updateGeoVariables(view);
-      });
-      this.updateGeoVariables(view);
-    }
-
     const v = centerPointRegistry.getIfExists(config.id);
     if (v) {
       let coord: Coordinate | undefined = undefined;
