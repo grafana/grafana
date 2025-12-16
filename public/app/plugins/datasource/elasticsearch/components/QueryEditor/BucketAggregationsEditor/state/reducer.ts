813e7b173c6b641083ed46c9dba0c14ecb462e13
Andrew Hackmann <andrew.hackmann@grafana.com>
2025-12-16 16:17:28 -0600

    The builder query no longer runs if code mode query is empty. Remove checks for query being empty to run raw query.

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/BucketAggregationsEditor/state/reducer.ts
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/BucketAggregationsEditor/state/reducer.ts
@@ -6,7 +6,7 @@ import { defaultBucketAgg } from '../../../../queryDef';
 import { removeEmpty } from '../../../../utils';
 import { changeMetricType } from '../../MetricAggregationsEditor/state/actions';
 import { metricAggregationConfig } from '../../MetricAggregationsEditor/utils';
-import { initQuery } from '../../state';
+import { changeEditorTypeAndResetQuery, initQuery } from '../../state';
 import { bucketAggregationConfig } from '../utils';
 
 import {
@@ -85,6 +85,10 @@ export const createReducer =
         return [{ ...defaultBucketAgg('2'), field: defaultTimeField }];
       }
       return state;
+    }
+
+    if (changeEditorTypeAndResetQuery.match(action)) {
+        return [{ ...defaultBucketAgg('2'), field: defaultTimeField }];
     }
 
     if (changeBucketAggregationSetting.match(action)) {
