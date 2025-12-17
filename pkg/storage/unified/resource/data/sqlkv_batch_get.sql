c1de5bb87b6b56cfd10f400fcb215ba266eb677f
Renato Costa <renato.costa@grafana.com>
2025-12-17 16:34:16 -0500

    unified-storage: add `BatchGet` support to the sqlkv implementation

---
+++ pkg/storage/unified/resource/data/sqlkv_batch_get.sql
@@ -0,0 +1,12 @@
+SELECT r.{{ .Ident "key_path" }}, r.{{ .Ident "value" }}
+FROM (
+{{ range $id, $key_path := .KeyPaths }}
+    {{ if eq $id 0 }}
+        SELECT {{ $.Arg $id }} AS idx, {{ $.Arg $key_path }} AS key_path
+    {{ else }}
+        UNION ALL SELECT {{ $.Arg $id }}, {{ $.Arg $key_path }}
+    {{ end }}
+{{ end }}
+) AS requested_keys
+JOIN {{ .TableName }} r ON r.{{ .Ident "key_path" }} = requested_keys.{{ .Ident "key_path" }}
+ORDER BY requested_keys.{{ .Ident "idx" }};
