3672d9c41d33b3c6daf8793d94f6b7165f1e5002
Yunwen Zheng <yunwen.zheng@grafana.com>
2025-12-17 09:40:04 -0500

    DashListItem: Added DashListItem shared component (#115384)
    * DashListItem: Add DashListItem shared component and shared with DashList and RecentlyViewedDashboards

--- public/app/features/browse-dashboards/components/RecentlyViewedDashboards.tsx
+++ public/app/features/browse-dashboards/components/RecentlyViewedDashboards.tsx
@@ -4,7 +4,9 @@ import { useAsync } from 'react-use';
 import { GrafanaTheme2 } from '@grafana/data';
 import { t, Trans } from '@grafana/i18n';
 import { evaluateBooleanFlag } from '@grafana/runtime/internal';
-import { CollapsableSection, Link, Spinner, Text, useStyles2 } from '@grafana/ui';
+import { CollapsableSection, Grid, Spinner, Text, useStyles2 } from '@grafana/ui';
+import { useDashboardLocationInfo } from 'app/features/search/hooks/useDashboardLocationInfo';
+import { DashListItem } from 'app/plugins/panel/dashlist/DashListItem';
 
 import { getRecentlyViewedDashboards } from './utils';
 
@@ -19,6 +21,7 @@ export function RecentlyViewedDashboards() {
     }
     return getRecentlyViewedDashboards(MAX_RECENT);
   }, []);
+  const { foldersByUid } = useDashboardLocationInfo(recentDashboards.length > 0);
 
   if (!evaluateBooleanFlag('recentlyViewedDashboards', false)) {
     return null;
@@ -43,35 +46,53 @@ export function RecentlyViewedDashboards() {
         <Text>{t('browse-dashboards.recently-viewed.empty', 'Nothing viewed yet')}</Text>
       )}
 
-      {/* TODO: implement actual card content */}
       {!loading && recentDashboards.length > 0 && (
-        <>
-          {recentDashboards.map((dash) => (
-            <div key={dash.uid}>
-              <Link href={dash.url}>{dash.name}</Link>
-            </div>
-          ))}
-        </>
+        <ul className={styles.list}>
+          <Grid columns={{ xs: 1, sm: 2, md: 3, lg: 5 }} gap={2}>
+            {recentDashboards.map((dash) => (
+              <li key={dash.uid} className={styles.listItem}>
+                <DashListItem
+                  key={dash.uid}
+                  dashboard={dash}
+                  url={dash.url}
+                  showFolderNames={true}
+                  locationInfo={foldersByUid[dash.location]}
+                  layoutMode="card"
+                />
+              </li>
+            ))}
+          </Grid>
+        </ul>
       )}
     </CollapsableSection>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => {
-  const accent = theme.visualization.getColorByName('purple'); // or your own hex
-
   return {
     title: css({
-      background: `linear-gradient(90deg, ${accent} 0%, #e478eaff 100%)`,
-      WebkitTextFillColor: 'transparent',
-      backgroundClip: 'text',
-      color: 'transparent',
       '& button svg': {
-        color: accent,
+        color: theme.colors.primary.text,
+      },
+      h3: {
+        background: `linear-gradient(90deg, ${theme.colors.primary.text} 0%, ${theme.colors.secondary.text} 100%)`,
+        WebkitTextFillColor: 'transparent',
+        backgroundClip: 'text',
+        color: 'transparent',
       },
     }),
     content: css({
       paddingTop: theme.spacing(0),
+    }),
+    list: css({
+      listStyle: 'none',
+      margin: 0,
+      padding: 0,
+      display: 'grid',
+      gap: theme.spacing(2),
+    }),
+    listItem: css({
+      margin: 0,
     }),
   };
 };
