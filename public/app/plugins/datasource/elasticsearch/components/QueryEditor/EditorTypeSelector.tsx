c0df6b7fabc1db486f135d989b1525f27d6325ce
Andrew Hackmann <andrew.hackmann@grafana.com>
2025-12-16 21:25:25 -0600

    add warning when switching builders

--- public/app/plugins/datasource/elasticsearch/components/QueryEditor/EditorTypeSelector.tsx
+++ public/app/plugins/datasource/elasticsearch/components/QueryEditor/EditorTypeSelector.tsx
@@ -1,29 +1,22 @@
 import { SelectableValue } from '@grafana/data';
 import { RadioButtonGroup } from '@grafana/ui';
 
-import { useDispatch } from '../../hooks/useStatelessReducer';
 import { EditorType } from '../../types';
-
-import { useQuery } from './ElasticsearchQueryContext';
-import { changeEditorTypeAndResetQuery } from './state';
 
 const BASE_OPTIONS: Array<SelectableValue<EditorType>> = [
   { value: 'builder', label: 'Builder' },
   { value: 'code', label: 'Code' },
 ];
 
-export const EditorTypeSelector = () => {
-  const query = useQuery();
-  const dispatch = useDispatch();
+interface Props {
+  value: EditorType;
+  onChange: (editorType: EditorType) => void;
+}
 
-  // Default to 'builder' if editorType is empty
-  const editorType: EditorType = query.editorType === 'code' ? 'code' : 'builder';
-
-  const onChange = (newEditorType: EditorType) => {
-    dispatch(changeEditorTypeAndResetQuery(newEditorType));
-  };
-
+export const EditorTypeSelector = ({ value, onChange }: Props) => {
   return (
-      <RadioButtonGroup<EditorType> size="sm" options={BASE_OPTIONS} value={editorType} onChange={onChange} />
+    <div data-testid="ElasticsearchEditorTypeToggle">
+      <RadioButtonGroup<EditorType> size="sm" options={BASE_OPTIONS} value={value} onChange={onChange} />
+    </div>
   );
 };
