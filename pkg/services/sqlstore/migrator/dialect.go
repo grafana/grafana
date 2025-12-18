b7c6a26a9edde762b34b3d901396e86975ef78e8
Peter Štibraný <pstibrany@gmail.com>
2025-12-18 16:31:46 +0100

    Backport DB snapshot for MySQL into release-11.6.10 (#115558)
    Backport DB snapshot for MySQL into release-11.6.10 (#115402)

--- pkg/services/sqlstore/migrator/dialect.go
+++ pkg/services/sqlstore/migrator/dialect.go
@@ -8,6 +8,8 @@ import (
 
 	"golang.org/x/exp/slices"
 
+	"github.com/grafana/grafana/pkg/infra/log"
+
 	"github.com/grafana/grafana/pkg/services/sqlstore/session"
 	"xorm.io/xorm"
 )
@@ -72,7 +74,7 @@ type Dialect interface {
 	// CreateDatabaseFromSnapshot is called when migration log table is not found.
 	// Dialect can recreate all tables from existing snapshot. After successful (nil error) return,
 	// migrator will list migrations from the log, and apply all missing migrations.
-	CreateDatabaseFromSnapshot(ctx context.Context, engine *xorm.Engine, migrationLogTableName string) error
+	CreateDatabaseFromSnapshot(ctx context.Context, engine *xorm.Engine, migrationLogTableName string, logger log.Logger) error
 
 	IsUniqueConstraintViolation(err error) bool
 	ErrorMessage(err error) string
@@ -343,7 +345,7 @@ func (b *BaseDialect) CleanDB(engine *xorm.Engine) error {
 	return nil
 }
 
-func (b *BaseDialect) CreateDatabaseFromSnapshot(ctx context.Context, engine *xorm.Engine, tableName string) error {
+func (b *BaseDialect) CreateDatabaseFromSnapshot(ctx context.Context, engine *xorm.Engine, migrationLogTableName string, logger log.Logger) error {
 	return nil
 }
 
