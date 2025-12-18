113d9bdce62252faad57ae6b43a0bcde88f34003
Peter Štibraný <pstibrany@gmail.com>
2025-12-18 11:57:19 +0100

    Move dependency registration to apis.go

--- pkg/storage/unified/client.go
+++ pkg/storage/unified/client.go
@@ -22,7 +22,6 @@ import (
 
 	infraDB "github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/infra/tracing"
-	"github.com/grafana/grafana/pkg/registry/apis/secret"
 	"github.com/grafana/grafana/pkg/services/apiserver/options"
 	"github.com/grafana/grafana/pkg/services/authn/grpcutils"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
@@ -44,9 +43,6 @@ type Options struct {
 	Reg      prometheus.Registerer
 	Authzc   types.AccessClient
 	Docs     resource.DocumentBuilderSupplier
-
-	// Only injected to make sure that secret DB is initialized on startup.
-	SecretDependencies *secret.DependencyRegisterer
 }
 
 type clientMetrics struct {
