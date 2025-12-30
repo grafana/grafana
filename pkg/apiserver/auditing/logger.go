285ef5cb962448ad2ee50c50033c9c1e381b767b
Matheus Macabu <macabu.matheus@gmail.com>
2025-12-30 12:24:08 +0100

    Auditing: Add global default logger implementation

--- pkg/apiserver/auditing/logger.go
+++ pkg/apiserver/auditing/logger.go
@@ -1,6 +1,7 @@
 package auditing
 
 import (
+	"context"
 	"encoding/json"
 	"time"
 )
@@ -17,4 +18,38 @@ type Logger interface {
 	Log(entry Sinkable) error
 	Close() error
 	Type() string
+}
+
+// Implementation inspired by https://github.com/grafana/grafana-app-sdk/blob/main/logging/logger.go
+type loggerContextKey struct{}
+
+var (
+	// DefaultLogger is the default Logger if one hasn't been provided in the context.
+	// You may use this to add arbitrary audit logging outside of an API request lifecycle.
+	DefaultLogger Logger = &NoopLogger{}
+
+	contextKey = loggerContextKey{}
+)
+
+// FromContext returns the Logger set in the context with Context(), or the DefaultLogger if no Logger is set in the context.
+// If DefaultLogger is nil, it returns a *NoopLogger so that the return is always valid to call methods on without nil-checking.
+// You may use this to add arbitrary audit logging outside of an API request lifecycle.
+func FromContext(ctx context.Context) Logger {
+	if l := ctx.Value(contextKey); l != nil {
+		if logger, ok := l.(Logger); ok {
+			return logger
+		}
+	}
+
+	if DefaultLogger != nil {
+		return DefaultLogger
+	}
+
+	return &NoopLogger{}
+}
+
+// Context returns a new context built from the provided context with the provided logger in it.
+// The Logger added with Context() can be retrieved with FromContext()
+func Context(ctx context.Context, logger Logger) context.Context {
+	return context.WithValue(ctx, contextKey, logger)
 }
