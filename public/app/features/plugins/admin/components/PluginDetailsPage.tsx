5f0f0cb033943be0ddc2e5b9f764b0608fc517bb
Will Browne <wbrowne@tcd.ie>
2026-01-06 16:45:34 +0000

    test without page loading check

--- public/app/features/plugins/admin/components/PluginDetailsPage.tsx
+++ public/app/features/plugins/admin/components/PluginDetailsPage.tsx
@@ -51,11 +51,7 @@ export function PluginDetailsPage({
   useGetPluginInsights(pluginId, plugin?.isInstalled ? plugin?.installedVersion : plugin?.latestVersion);
 
   const isNarrowScreen = useMedia('(max-width: 600px)');
-  const {
-    navModel,
-    activePageId,
-    loading: isPluginConfigLoading,
-  } = usePluginDetailsTabs(plugin, queryParams.get('page'), isNarrowScreen);
+  const { navModel, activePageId } = usePluginDetailsTabs(plugin, queryParams.get('page'), isNarrowScreen);
   const { actions, info, subtitle } = usePluginPageExtensions(plugin);
   const { isLoading: isFetchLoading } = useFetchStatus();
   const { isLoading: isFetchDetailsLoading } = useFetchDetailsStatus();
@@ -86,7 +82,7 @@ export function PluginDetailsPage({
   return (
     <Page navId={navId} pageNav={navModel} actions={actions} subTitle={subtitle}>
       <Stack gap={4} justifyContent="space-between" direction={{ xs: 'column-reverse', sm: 'row' }}>
-        <Page.Contents isLoading={isPluginConfigLoading}>
+        <Page.Contents>
           <TabContent className={styles.tabContent}>
             <PluginDetailsSignature plugin={plugin} className={styles.alert} />
             <PluginDetailsDisabledError plugin={plugin} className={styles.alert} />
