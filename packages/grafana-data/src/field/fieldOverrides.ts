2ed582c5ea63ca2e6aa2d2fadcbd41a87520e5f7
Kristina Demeshchik <kristina.demeshchik@gmail.com>
2026-01-08 11:55:18 -0500

    multiple value mappingg overrides

--- packages/grafana-data/src/field/fieldOverrides.ts
+++ packages/grafana-data/src/field/fieldOverrides.ts
@@ -341,7 +341,7 @@ export function setDynamicConfigValue(config: FieldConfig, value: DynamicConfigV
     return;
   }
 
-  const val = item.process(value.value, context, item.settings);
+  let val = item.process(value.value, context, item.settings);
 
   const remove = val === undefined || val === null;
 
@@ -352,6 +352,14 @@ export function setDynamicConfigValue(config: FieldConfig, value: DynamicConfigV
       unset(config, item.path);
     }
   } else {
+    if (Array.isArray(val)) {
+      const existingValue = item.isCustom ? get(config.custom, item.path) : get(config, item.path);
+
+      if (Array.isArray(existingValue) && existingValue.length > 0) {
+        val = [...existingValue, ...val];
+      }
+    }
+
     if (item.isCustom) {
       if (!config.custom) {
         config.custom = {};
