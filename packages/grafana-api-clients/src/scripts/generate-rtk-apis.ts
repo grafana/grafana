528b885bf2007fa1561806b90105751ab85bd40d
Galen <galen.kistler@grafana.com>
2026-01-08 16:44:16 -0600

    chore: remove duplicates

--- packages/grafana-api-clients/src/scripts/generate-rtk-apis.ts
+++ packages/grafana-api-clients/src/scripts/generate-rtk-apis.ts
@@ -113,8 +113,6 @@ const config: ConfigFile = {
     ...createAPIConfig('historian.alerting', 'v0alpha1'),
     ...createAPIConfig('logsdrilldown', 'v1beta1'),
     ...createAPIConfig('logsdrilldown', 'v1alpha1'),
-    ...createAPIConfig('logsdrilldown', 'v1beta1'),
-    ...createAPIConfig('logsdrilldown', 'v1alpha1'),
     // PLOP_INJECT_API_CLIENT - Used by the API client generator
   },
 };
