055b9f164ba7b77968d3f9e692dfa6fe28af6829
Gareth Dawson <gwdawson.work@gmail.com>
2026-01-09 19:37:20 +0900

    remove feature toggle definition

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1124,11 +1124,6 @@ export interface FeatureToggles {
   */
   pluginContainers?: boolean;
   /**
-  * Run search queries through the tempo backend
-  * @default false
-  */
-  tempoSearchBackendMigration?: boolean;
-  /**
   * Prioritize loading plugins from the CDN before other sources
   * @default false
   */
