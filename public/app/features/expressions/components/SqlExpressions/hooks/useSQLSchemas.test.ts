d6d34ddf844a8f5cf33b8cf493aadee7943f4d3b
Develer <yeryomin.igor@gmail.com>
2026-01-12 15:45:53 +0100

    fix(sql expression): sql schema frontend datasources filtering

---
+++ public/app/features/expressions/components/SqlExpressions/hooks/useSQLSchemas.test.ts
@@ -0,0 +1,18 @@
+import { DataQuery } from '@grafana/schema';
+import { SHARED_DASHBOARD_QUERY } from 'app/plugins/datasource/dashboard/constants';
+
+import { isDashboardDatasource } from './useSQLSchemas';
+
+describe('isDashboardDatasource', () => {
+  it('identifies Dashboard datasource queries in a mixed set', () => {
+    const queries: DataQuery[] = [
+      { refId: 'A', datasource: { uid: 'prometheus-uid', type: 'prometheus' } },
+      { refId: 'B', datasource: { uid: SHARED_DASHBOARD_QUERY, type: 'datasource' } },
+      { refId: 'C', datasource: { uid: 'mysql-uid', type: 'mysql' } },
+    ];
+
+    const backendQueries = queries.filter((q) => !isDashboardDatasource(q));
+
+    expect(backendQueries.map((q) => q.refId)).toEqual(['A', 'C']);
+  });
+});
