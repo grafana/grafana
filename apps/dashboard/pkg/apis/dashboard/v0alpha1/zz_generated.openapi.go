0ce2e32ea55a5ca8ccc5f47ab3debb8b7146b892
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2026-01-12 17:32:01 -0300

    update api docs

--- apps/dashboard/pkg/apis/dashboard/v0alpha1/zz_generated.openapi.go
+++ apps/dashboard/pkg/apis/dashboard/v0alpha1/zz_generated.openapi.go
@@ -1496,6 +1496,12 @@ func schema_pkg_apis_dashboard_v0alpha1_SnapshotSharingOptions(ref common.Refere
 							Format: "",
 						},
 					},
+					"publicMode": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"boolean"},
+							Format: "",
+						},
+					},
 				},
 				Required: []string{"snapshotEnabled"},
 			},
