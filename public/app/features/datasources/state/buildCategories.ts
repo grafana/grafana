36bd838c6d365810d193804c8742f9ccb55b785b
Fabrizio <fabrizio.casati@grafana.com>
2026-01-13 11:51:47 +0100

    Fix ID

--- public/app/features/datasources/state/buildCategories.ts
+++ public/app/features/datasources/state/buildCategories.ts
@@ -420,7 +420,7 @@ function getEnterprisePhantomPlugins(): DataSourcePluginMeta[] {
       imgUrl: solarWindsSvg,
     }),
     getPhantomPlugin({
-      id: 'grafana-db2-datasource',
+      id: 'grafana-db2-jdbc-datasource',
       description: t(
         'datasources.get-enterprise-phantom-plugins.description.db2-datasource',
         'IBM Db2 data source'
