a666e74ca48b1f4c9793659573c159a1e7fd79d7
rodrigopk <rodrigo.barros@grafana.com>
2026-01-13 11:38:49 -0500

    Fix JSDoc

--- public/app/features/alerting/unified/types/knownProvenance.ts
+++ public/app/features/alerting/unified/types/knownProvenance.ts
@@ -1,5 +1,5 @@
 export enum KnownProvenance {
-  None = 'none' /** Value of {@link PROVENANCE_ANNOTATION} given for entities that were not provisioned */,
+  None = 'none' /** Provenance value given for entities that were not provisioned */,
   API = 'api',
   File = 'file',
   ConvertedPrometheus = 'converted_prometheus',
