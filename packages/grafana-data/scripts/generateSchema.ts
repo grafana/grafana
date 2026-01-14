4808f1264df3b37c796b3ea2d7b77f45077081f8
Jack Westbrook <jack.westbrook@gmail.com>
2026-01-14 08:57:46 +0100

    refactor(grafana-data): move node scripts out of source code

--- packages/grafana-data/scripts/generateSchema.ts
+++ packages/grafana-data/scripts/generateSchema.ts
@@ -2,13 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { NewThemeOptionsSchema } from '../createTheme';
+import { NewThemeOptionsSchema } from '../src/themes/createTheme';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 fs.writeFileSync(
-  path.join(__dirname, '../schema.generated.json'),
+  path.join(__dirname, '..', 'src', 'themes', 'schema.generated.json'),
   JSON.stringify(
     NewThemeOptionsSchema.toJSONSchema({
       target: 'draft-07',
@@ -17,3 +17,5 @@ fs.writeFileSync(
     2
   )
 );
+
+console.log('Successfully generated theme schema');
