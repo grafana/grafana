2c05944e5e1c2221245b057e2b17e365864ae79c
Paul Marbach <paul.marbach@grafana.com>
2026-01-15 10:28:25 -0500

    use temp setVisualization method which forces the picker to be on the All visualizations tab

---
+++ e2e-playwright/utils/panel-helpers.ts
@@ -0,0 +1,20 @@
+import { expect } from '@playwright/test';
+
+import { PanelEditPage } from '@grafana/plugin-e2e';
+
+export async function setVisualization(page: PanelEditPage, visualization: string) {
+  const showPanelEditElement = page.getByGrafanaSelector('Show options pane');
+  const showPanelEditElementCount = await showPanelEditElement.count();
+  if (showPanelEditElementCount > 0) {
+    await showPanelEditElement.click();
+  }
+  await page.getByGrafanaSelector(page.ctx.selectors.components.PanelEditor.toggleVizPicker).click();
+  await page.getByGrafanaSelector(page.ctx.selectors.components.Tab.item('All Visualizations')).click();
+  await page.getByGrafanaSelector(page.ctx.selectors.components.PluginVisualization.item(visualization)).click();
+
+  const vizSelector = page.ctx.selectors.components.PanelEditor.toggleVizPicker;
+  await expect(
+    page.getByGrafanaSelector(vizSelector),
+    `Could not set visualization to ${visualization}. Ensure the panel is installed.`
+  ).toHaveText(visualization);
+}
