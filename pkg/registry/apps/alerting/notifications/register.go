b022a1e378718cea5dae42eef14c7a7ee58d2107
Tito Lins <tito.linsesilva@grafana.com>
2026-01-16 14:44:43 +0100

    enable service with include imported on feature toggle

--- pkg/registry/apps/alerting/notifications/register.go
+++ pkg/registry/apps/alerting/notifications/register.go
@@ -97,7 +97,12 @@ func (a AppInstaller) GetLegacyStorage(gvr schema.GroupVersionResource) grafanar
 	if gvr == receiver.ResourceInfo.GroupVersionResource() {
 		return receiver.NewStorage(api.ReceiverService, namespacer, api.ReceiverService)
 	} else if gvr == timeinterval.ResourceInfo.GroupVersionResource() {
-		return timeinterval.NewStorage(api.MuteTimings, namespacer)
+		srv := api.MuteTimings
+		//nolint:staticcheck // not yet migrated to OpenFeature
+		if a.ng.FeatureToggles.IsEnabledGlobally(featuremgmt.FlagAlertingImportAlertmanagerAPI) {
+			srv = srv.WithIncludeImported()
+		}
+		return timeinterval.NewStorage(srv, namespacer)
 	} else if gvr == templategroup.ResourceInfo.GroupVersionResource() {
 		srv := api.Templates
 		//nolint:staticcheck // not yet migrated to OpenFeature
