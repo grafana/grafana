f5e3bfbb4d353f374197535c7066038bb344c01b
Ryan McKinley <ryantxu@gmail.com>
2026-01-17 17:58:53 +0300

    fewer changes

--- packages/grafana-sql/src/datasource/SqlDatasource.ts
+++ packages/grafana-sql/src/datasource/SqlDatasource.ts
@@ -36,7 +36,8 @@ import { DB, SQLQuery, SQLOptions, SqlQueryModel, QueryFormat } from '../types';
 import migrateAnnotation from '../utils/migration';
 
 export abstract class SqlDatasource extends DataSourceWithBackend<SQLQuery, SQLOptions> {
-  id: number;
+  id?: number;
+  uid: string;
   responseParser: ResponseParser;
   name: string;
   interval: string;
@@ -51,6 +52,7 @@ export abstract class SqlDatasource extends DataSourceWithBackend<SQLQuery, SQLO
     this.name = instanceSettings.name;
     this.responseParser = new ResponseParser();
     this.id = instanceSettings.id;
+    this.uid = instanceSettings.uid;
     const settingsData = instanceSettings.jsonData || {};
     this.interval = settingsData.timeInterval || '1m';
     this.db = this.getDB();
--- public/app/plugins/datasource/influxdb/fsql/datasource.flightsql.ts
+++ public/app/plugins/datasource/influxdb/fsql/datasource.flightsql.ts
@@ -144,7 +144,7 @@ export class FlightSQLDatasource extends SqlDatasource {
       fields: (query: SQLQuery) => this.fetchFields(query),
       validateQuery: (query: SQLQuery, range?: TimeRange) =>
         Promise.resolve({ query, error: '', isError: false, isValid: true }),
-      dsUID: () => this.uid,
+      dsID: () => this.id,
       toRawSql,
       functions: () => this.getFunctions(),
       getEditorLanguageDefinition: () => this.getSqlLanguageDefinition(),
