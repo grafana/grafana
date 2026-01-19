8bd4089c33491b9d692da8c2db15b63585e6b317
Marcus Andersson <marcus.andersson@grafana.com>
2026-01-19 11:38:07 +0100

    removed comment since this is extension point specific.

--- packages/grafana-data/src/types/pluginExtensions.ts
+++ packages/grafana-data/src/types/pluginExtensions.ts
@@ -138,10 +138,7 @@ export type PluginExtensionAddedLinkConfig<Context extends object = object> = Pl
   // (Optional) A icon that can be displayed in the ui for the extension option.
   icon?: IconName;
 
-  /**
-   * (Optional) A category to be used when grouping the options in the ui.
-   * If set to "root", the link will be rendered at the root level of the menu.
-   */
+  // (Optional) A category to be used when grouping the options in the ui.
   category?: string;
 
   // (Optional) If true, opens the link in a new tab (renders with target="_blank")
