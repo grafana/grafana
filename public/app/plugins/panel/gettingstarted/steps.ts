dc1e95aa1c032fae495a64146e15a137c3aa86ed
Ryan McKinley <ryantxu@gmail.com>
2026-01-19 20:09:10 +0300

    cleanup

--- public/app/plugins/panel/gettingstarted/steps.ts
+++ public/app/plugins/panel/gettingstarted/steps.ts
@@ -41,7 +41,7 @@ export const getSteps = (): SetupStep[] => [
           return new Promise((resolve) => {
             resolve(
               getDatasourceSrv()
-                .getAll()
+                .getList({ metrics: true })
                 .filter((item) => {
                   return item.meta.builtIn !== true;
                 }).length > 0
