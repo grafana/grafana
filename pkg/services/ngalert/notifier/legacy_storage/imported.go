9f4733d1f461dfa70921644d5dfdff676761b9bb
Tito Lins <tito.linsesilva@grafana.com>
2026-01-19 23:04:32 +0100

    add imported revision helper

--- pkg/services/ngalert/notifier/legacy_storage/imported.go
+++ pkg/services/ngalert/notifier/legacy_storage/imported.go
@@ -5,6 +5,7 @@ import (
 	"slices"
 
 	"github.com/grafana/alerting/definition"
+	"github.com/prometheus/alertmanager/config"
 
 	"github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 	"github.com/grafana/grafana/pkg/services/ngalert/models"
@@ -66,6 +67,53 @@ func (e ImportedConfigRevision) GetReceivers(uids []string) ([]*models.Receiver,
 		}
 		result = append(result, recv)
 	}
+	return result, nil
+}
+
+func (e ImportedConfigRevision) GetMuteTimeIntervals() ([]config.MuteTimeInterval, error) {
+	if e.importedConfig == nil {
+		return nil, nil
+	}
+
+	// Get original imported intervals (before deduplication)
+	importedMute := e.importedConfig.GetMuteTimeIntervals()
+	importedTime := e.importedConfig.GetTimeIntervals()
+
+	if len(importedMute) == 0 && len(importedTime) == 0 {
+		return nil, nil
+	}
+
+	// Get Grafana time intervals for merge
+	grafanaMute := e.rev.Config.AlertmanagerConfig.MuteTimeIntervals
+	grafanaTime := e.rev.Config.AlertmanagerConfig.TimeIntervals
+
+	// Merge to get the renames map (only renamed if name collision occurs)
+	_, renames := definition.MergeTimeIntervals(
+		grafanaMute,
+		grafanaTime,
+		importedMute,
+		importedTime,
+		e.opts.DedupSuffix,
+	)
+
+	// Apply renames to imported intervals
+	result := make([]config.MuteTimeInterval, 0, len(importedTime)+len(importedMute))
+
+	push := func(mt config.MuteTimeInterval) {
+		if newName, renamed := renames[mt.Name]; renamed {
+			mt.Name = newName
+		}
+		result = append(result, mt)
+	}
+
+	for _, ti := range importedTime {
+		push(config.MuteTimeInterval(ti))
+	}
+
+	for _, mti := range importedMute {
+		push(mti)
+	}
+
 	return result, nil
 }
 
