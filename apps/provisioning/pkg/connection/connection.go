022fc72dd9b1f6807473f1b3dde42b194c88729e
Daniele Ferru <daniele.ferru@grafana.com>
2026-01-21 23:27:45 +0100

    wip: update TokenGenerator godoc

--- apps/provisioning/pkg/connection/connection.go
+++ apps/provisioning/pkg/connection/connection.go
@@ -34,8 +34,7 @@ type Connection interface {
 }
 
 // TokenGenerator is an optional interface that connections can implement if they need
-// to generate connection-level tokens (e.g., GitHub App JWT tokens).
-// Connections that don't need connection-level tokens (e.g., GitLab, Bitbucket) don't need to implement this.
+// to generate connection-level tokens.
 type TokenGenerator interface {
 	// GenerateConnectionToken generates a connection-level token.
 	// Returns the generated token value.
