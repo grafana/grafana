bd868693c80b456adda068ef79b3850997b01ddb
Daniele Ferru <daniele.ferru@grafana.com>
2026-01-21 23:31:15 +0100

    wip: adding a bit more todos

--- apps/provisioning/pkg/connection/github/token.go
+++ apps/provisioning/pkg/connection/github/token.go
@@ -11,12 +11,15 @@ import (
 
 const (
 	// JWTExpirationMinutes is the token expiration time for GitHub App JWT tokens
+	// TODO(ferruvich): this could be in settings
 	JWTExpirationMinutes = 10
 )
 
 // GenerateJWTToken creates a GitHub App JWT token from appID and base64-encoded private key.
 // The private key should be base64-encoded PEM format.
 // Returns the signed JWT token string.
+// TODO(ferruvich): this can be unexported - we're using it only in this package, and it's only
+// related to how Github wants their token to be built.
 func GenerateJWTToken(appID string, privateKey common.RawSecureValue) (common.RawSecureValue, error) {
 	// Decode base64-encoded private key
 	privateKeyPEM, err := base64.StdEncoding.DecodeString(string(privateKey))
