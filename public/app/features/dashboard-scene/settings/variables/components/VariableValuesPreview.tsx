eb9604fb5ca5a3c0f5156303d7b17eba599fb36e
grafakus <marc.mignonsin@grafana.com>
2026-01-21 15:45:12 +0100

    Fix values preview

--- public/app/features/dashboard-scene/settings/variables/components/VariableValuesPreview.tsx
+++ public/app/features/dashboard-scene/settings/variables/components/VariableValuesPreview.tsx
@@ -12,14 +12,16 @@ export interface Props {
   options: VariableValueOption[];
 }
 
+// the option at index 0 can be "All" so we try to grab the propertiesfrom the 2nd option
+export const getPropertiesFromOptions = (options: VariableValueOption[]) => Object.keys(options[1]?.properties ?? options[0]?.properties ?? {})
+
 export const VariableValuesPreview = ({ options }: Props) => {
   const styles = useStyles2(getStyles);
   const hasOptions = options.length > 0;
   const displayMultiPropsPreview =
     config.featureToggles.multiPropsVariables &&
     hasOptions &&
-    // the option at index 0 can be "All" so we try to grab the propertiesfrom the 2nd option
-    Object.keys(options[1]?.properties ?? options[0]?.properties ?? {}).length > 0;
+    getPropertiesFromOptions(options).length > 0;
 
   return (
     <div className={styles.previewContainer} style={{ gap: '8px' }}>
@@ -38,11 +40,7 @@ function VariableValuesWithPropsPreview({ options }: { options: VariableValueOpt
   const styles = useStyles2(getStyles);
 
   const { data, columns } = useMemo(() => {
-    const data = options.map(({ label, value, properties }) => ({
-      label: String(label),
-      value: String(value),
-      ...flattenProperties(properties),
-    }));
+    const data = options.map(({ properties }) => flattenProperties(properties));
 
     return {
       data,
