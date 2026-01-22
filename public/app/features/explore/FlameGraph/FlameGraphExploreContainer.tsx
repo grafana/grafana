3e13e95463cc50f5a57b339b546fa68476919237
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2026-01-22 16:30:10 -0400

    Wire up feature flag

--- public/app/features/explore/FlameGraph/FlameGraphExploreContainer.tsx
+++ public/app/features/explore/FlameGraph/FlameGraphExploreContainer.tsx
@@ -30,6 +30,7 @@ export const FlameGraphExploreContainer = (props: Props) => {
         onViewSelected={(view: string) => interaction('view_selected', { view })}
         onTextAlignSelected={(align: string) => interaction('text_align_selected', { align })}
         onTableSort={(sort: string) => interaction('table_sort_selected', { sort })}
+        enableNewUI={config.featureToggles.flameGraphWithCallTree}
       />
     </div>
   );
