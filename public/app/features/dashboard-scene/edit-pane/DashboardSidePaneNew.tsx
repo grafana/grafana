5b1451fac1f2c0b49b9b689f2b91e406238a2c1d
grafakus <marc.mignonsin@grafana.com>
2026-01-28 18:44:08 +0100

    DynamicDashboards: Add tabs and rows from the side bar

--- public/app/features/dashboard-scene/edit-pane/DashboardSidePaneNew.tsx
+++ public/app/features/dashboard-scene/edit-pane/DashboardSidePaneNew.tsx
@@ -1,11 +1,11 @@
 import { css, cx } from '@emotion/css';
 import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 
-import { GrafanaTheme2 } from '@grafana/data';
+import { GrafanaTheme2, IconName } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import { t } from '@grafana/i18n';
+import { t, Trans } from '@grafana/i18n';
 import { SceneObject } from '@grafana/scenes';
-import { Sidebar, Text, useStyles2 } from '@grafana/ui';
+import { Button, Icon, Sidebar, Stack, Text, useStyles2 } from '@grafana/ui';
 import { getLayoutType } from 'app/features/dashboard/utils/tracking';
 import addPanelImg from 'img/dashboards/add-panel.png';
 
@@ -20,73 +20,148 @@ interface Props {
 
 export function DashboardSidePaneNew({ onAddPanel, dashboard, selectedElement }: Props) {
   const styles = useStyles2(getStyles);
-  const orchestrator = getDashboardSceneFor(dashboard).state.layoutOrchestrator;
+  const dashboardScene = getDashboardSceneFor(dashboard);
+  const orchestrator = dashboardScene.state.layoutOrchestrator;
 
   const onAddPanelClick = () => {
     onAddPanel();
     DashboardInteractions.trackAddPanelClick('sidebar', getLayoutType(selectedElement));
   };
 
+  const onAddTabsClick = () => {};
+  const onAddRowsClick = () => {};
+
   return (
-    <DragDropContext onDragStart={() => orchestrator?.startDraggingNewPanel()} onDragEnd={() => {}}>
-      <Droppable droppableId="side-drop-id" isDropDisabled>
-        {(dropProvided) => (
-          <div className={styles.sidePanel} ref={dropProvided.innerRef} {...dropProvided.droppableProps}>
-            <Sidebar.PaneHeader title={t('dashboard.add.pane-header', 'Add')} />
-            <div className={styles.sidePanelContainer}>
-              <Text weight="medium">{t('dashboard.add.new-panel.title', 'Panel')}</Text>
-              <Text element="p" variant="bodySmall">
-                {t('dashboard.add.new-panel.description', 'Drag or click to add a panel')}
-              </Text>
-              <div className={styles.dragContainer}>
-                <Draggable draggableId="new-panel-drag" index={0}>
-                  {(dragProvided, dragSnapshot) => {
-                    return (
-                      <div
-                        role="button"
-                        data-testid={selectors.components.Sidebar.newPanelButton}
-                        tabIndex={0}
-                        ref={dragProvided.innerRef}
-                        {...dragProvided.draggableProps}
-                        {...dragProvided.dragHandleProps}
-                        className={cx(styles.imageContainer, dragSnapshot.isDragging && styles.dragging)}
-                        onClick={onAddPanelClick}
-                        onKeyDown={(e) => {
-                          if (e.key === 'Enter' || e.key === ' ') {
-                            e.preventDefault();
-                            onAddPanelClick();
-                          }
-                        }}
-                        aria-label={t('dashboard.add.new-panel.title', 'Panel')}
-                      >
-                        <img
-                          alt={t('dashboard.add.new-panel.button', 'Add new panel button')}
-                          src={addPanelImg}
-                          draggable={false}
-                        />
-                      </div>
-                    );
-                  }}
-                </Draggable>
+    <div>
+      <Sidebar.PaneHeader title={t('dashboard.add.pane-header', 'Add')} />
+
+      <div className={styles.section}>
+        <div className={styles.sectionHeader}>
+          <Text weight="medium">{t('dashboard.add.new-panel.title', 'Panel')}</Text>
+          <Text element="p" variant="bodySmall" color="secondary">
+            {t('dashboard.add.new-panel.description', 'Drag or click to add a panel')}
+          </Text>
+        </div>
+        <DragDropContext onDragStart={() => orchestrator?.startDraggingNewPanel()} onDragEnd={() => {}}>
+          <Droppable droppableId="side-drop-id" isDropDisabled>
+            {(dropProvided) => (
+              <div ref={dropProvided.innerRef} {...dropProvided.droppableProps}>
+                <div className={styles.dragContainer}>
+                  <Draggable draggableId="new-panel-drag" index={0}>
+                    {(dragProvided, dragSnapshot) => {
+                      return (
+                        <div
+                          role="button"
+                          data-testid={selectors.components.Sidebar.newPanelButton}
+                          tabIndex={0}
+                          ref={dragProvided.innerRef}
+                          {...dragProvided.draggableProps}
+                          {...dragProvided.dragHandleProps}
+                          className={cx(styles.imageContainer, dragSnapshot.isDragging && styles.dragging)}
+                          onClick={onAddPanelClick}
+                          onKeyDown={(e) => {
+                            if (e.key === 'Enter' || e.key === ' ') {
+                              e.preventDefault();
+                              onAddPanelClick();
+                            }
+                          }}
+                          aria-label={t('dashboard.add.new-panel.title', 'Panel')}
+                        >
+                          <img
+                            alt={t('dashboard.add.new-panel.button', 'Add new panel button')}
+                            src={addPanelImg}
+                            draggable={false}
+                          />
+                        </div>
+                      );
+                    }}
+                  </Draggable>
+                </div>
               </div>
-            </div>
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles.sectionHeader}>
+          <Text weight="medium">
+            <Trans i18nKey="dashboard-scene.dashboard-side-pane-new.panel-groups">Panel groups</Trans>
+          </Text>
+          <Text element="p" variant="bodySmall" color="secondary">
+            <Trans i18nKey="dashboard-scene.dashboard-side-pane-new.existing-panels-first-group">
+              Existing panels go to the first group
+            </Trans>
+          </Text>
+        </div>
+
+        <Stack direction="column" gap={2}>
+          <AddItemButton
+            icon="layers"
+            label={t('dashboard-scene.dashboard-side-pane-new.label-tabs', 'Tabs')}
+            description={t('dashboard-scene.dashboard-side-pane-new.description-multiple-views', 'Multiple views')}
+            tooltip={t(
+              'dashboard-scene.dashboard-side-pane-new.tooltip-group-panels-into-tabs',
+              'Group panels into tabs'
+            )}
+            onClick={onAddTabsClick}
+          />
+          <AddItemButton
+            icon="brackets-curly"
+            label={t('dashboard-scene.dashboard-side-pane-new.label-rows', 'Rows')}
+            description={t(
+              'dashboard-scene.dashboard-side-pane-new.description-expandable-sections',
+              'Expandable sections'
+            )}
+            tooltip={t(
+              'dashboard-scene.dashboard-side-pane-new.tooltip-group-panels-into-rows',
+              'Group panels into rows'
+            )}
+            onClick={onAddRowsClick}
+          />
+        </Stack>
+      </div>
+    </div>
+  );
+}
+
+type AddItemButtonProps = {
+  icon: IconName;
+  label: string;
+  description: string;
+  tooltip: string;
+  onClick: () => void;
+};
+
+function AddItemButton({ icon, label, description, tooltip, onClick }: AddItemButtonProps) {
+  const styles = useStyles2(getStyles);
+
+  return (
+    <div className={styles.control}>
+      <Button variant="secondary" fill="outline" onClick={onClick} tooltip={tooltip} className={styles.iconButton}>
+        <Icon name={icon} size="xl" />
+      </Button>
+      <div className={styles.labelContainer}>
+        <Text weight="medium">{label}</Text>
+        <Text element="p" variant="bodySmall" color="secondary">
+          {description}
+        </Text>
+      </div>
+    </div>
   );
 }
 
 function getStyles(theme: GrafanaTheme2) {
   return {
-    sidePanel: css({
+    section: css({
       borderBottom: `1px solid ${theme.colors.border.weak}`,
+      padding: theme.spacing(2),
+    }),
+    sectionHeader: css({
+      margin: theme.spacing(0, 0, 3, 0),
     }),
     dragging: css({
       cursor: 'move',
-    }),
-    sidePanelContainer: css({
-      padding: theme.spacing(2),
     }),
     dragContainer: css({
       height: 150,
@@ -101,6 +176,29 @@ function getStyles(theme: GrafanaTheme2) {
       '&:hover': {
         opacity: 1,
       },
+    }),
+    control: css({
+      display: 'flex',
+      flexDirection: 'row',
+      gap: theme.spacing(2),
+      alignItems: 'stretch',
+      width: '100%',
+    }),
+    iconButton: css({
+      height: '46px',
+      lineHeight: '46px',
+      padding: theme.spacing(0, 1),
+      '& svg': {
+        opacity: 0.6,
+      },
+      '&:hover svg': {
+        opacity: 1,
+      },
+    }),
+    labelContainer: css({
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-evenly',
     }),
   };
 }
