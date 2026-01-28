1e08ebab2aaf2feb6e351ff2e17abc78b0d3d22d
grafakus <marc.mignonsin@grafana.com>
2026-01-28 18:59:53 +0100

    DashboardSidePaneNew: better markup

--- public/app/features/dashboard-scene/edit-pane/DashboardSidePaneNew.tsx
+++ public/app/features/dashboard-scene/edit-pane/DashboardSidePaneNew.tsx
@@ -4,10 +4,10 @@ import { useCallback } from 'react';
 
 import { AnnotationQuery, getDataSourceRef, GrafanaTheme2, IconName } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import { t, Trans } from '@grafana/i18n';
+import { t } from '@grafana/i18n';
 import { getDataSourceSrv } from '@grafana/runtime';
 import { SceneObject } from '@grafana/scenes';
-import { Button, Icon, Sidebar, Text, useStyles2 } from '@grafana/ui';
+import { Button, Icon, Sidebar, Stack, Text, useStyles2 } from '@grafana/ui';
 import { getLayoutType } from 'app/features/dashboard/utils/tracking';
 import addPanelImg from 'img/dashboards/add-panel.png';
 
@@ -18,33 +18,29 @@ import { dashboardSceneGraph } from '../utils/dashboardSceneGraph';
 import { DashboardInteractions } from '../utils/interactions';
 import { getDashboardSceneFor } from '../utils/utils';
 
-interface Props {
+interface DashboardSidePaneNewProps {
   dashboard: SceneObject;
   selectedElement: SceneObject | undefined;
   onAddPanel: () => void;
 }
 
-export function DashboardSidePaneNew({ onAddPanel, dashboard, selectedElement }: Props) {
+export function DashboardSidePaneNew({ onAddPanel, dashboard, selectedElement }: DashboardSidePaneNewProps) {
   const styles = useStyles2(getStyles);
   const dashboardScene = getDashboardSceneFor(dashboard);
   const orchestrator = dashboardScene.state.layoutOrchestrator;
 
-  const onAddPanelClick = () => {
+  const onAddPanelClick = useCallback(() => {
     onAddPanel();
     DashboardInteractions.trackAddPanelClick('sidebar', getLayoutType(selectedElement));
-  };
+  }, [onAddPanel, selectedElement]);
 
   return (
     <div>
       <Sidebar.PaneHeader title={t('dashboard.add.pane-header', 'Add')} />
-
-      <div className={styles.section}>
-        <div className={styles.sectionHeader}>
-          <Text weight="medium">{t('dashboard.add.new-panel.title', 'Panel')}</Text>
-          <Text element="p" variant="bodySmall" color="secondary">
-            {t('dashboard.add.new-panel.description', 'Drag or click to add a panel')}
-          </Text>
-        </div>
+      <SidePaneSection
+        title={t('dashboard.add.new-panel.title', 'Panel')}
+        description={t('dashboard.add.new-panel.description', 'Drag or click to add a panel')}
+      >
         <DragDropContext onDragStart={() => orchestrator?.startDraggingNewPanel()} onDragEnd={() => {}}>
           <Droppable droppableId="side-drop-id" isDropDisabled>
             {(dropProvided) => (
@@ -84,22 +80,39 @@ export function DashboardSidePaneNew({ onAddPanel, dashboard, selectedElement }:
             )}
           </Droppable>
         </DragDropContext>
-      </div>
-
-      <div className={styles.section}>
-        <div className={styles.sectionHeader}>
-          <Text weight="medium">
-            <Trans i18nKey="dashboard-scene.dashboard-side-pane-new.dashboard-controls">Dashboard controls</Trans>
-          </Text>
-        </div>
-        <AnnotationControl dashboardScene={dashboardScene} />
-      </div>
+      </SidePaneSection>
+      <SidePaneSection title={t('dashboard-scene.dashboard-side-pane-new.dashboard-controls', 'Dashboard controls')}>
+        <AddAnnotation dashboardScene={dashboardScene} />
+      </SidePaneSection>
     </div>
   );
 }
 
-function AnnotationControl({ dashboardScene }: { dashboardScene: DashboardScene }) {
-  const onAddAnnotation = useCallback(() => {
+type SidePaneSectionProps = {
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+function SidePaneSection({ title, description, children }: SidePaneSectionProps) {
+  const styles = useStyles2(getStyles);
+  return (
+    <div className={styles.SidePanesection}>
+      <div className={styles.SidePanesectionHeader}>
+        <Text weight="medium">{title}</Text>
+        <Text element="p" variant="bodySmall" color="secondary">
+          {description || ''}
+        </Text>
+      </div>
+      <Stack direction="column" gap={2}>
+        {children}
+      </Stack>
+    </div>
+  );
+}
+
+function AddAnnotation({ dashboardScene }: { dashboardScene: DashboardScene }) {
+  const onAddAnnotationClick = useCallback(() => {
     const defaultDatasource = getDataSourceSrv().getInstanceSettings(null);
     const datasourceRef = defaultDatasource?.meta.annotations ? getDataSourceRef(defaultDatasource) : undefined;
 
@@ -120,13 +133,11 @@ function AnnotationControl({ dashboardScene }: { dashboardScene: DashboardScene 
     const dataLayers = dashboardSceneGraph.getDataLayers(dashboardScene);
     dataLayers.addAnnotationLayer(newAnnotation);
 
-    // Select the newly added annotation
-    const { editPane } = dashboardScene.state;
-    editPane.selectObject(newAnnotation, newAnnotation.state.key!);
+    dashboardScene.state.editPane.selectObject(newAnnotation, newAnnotation.state.key!);
   }, [dashboardScene]);
 
   return (
-    <DashboardControl
+    <AddItem
       icon="comment-alt"
       label={t('dashboard-scene.annotation-control.label-annotation-query', 'Annotation query')}
       description={t(
@@ -134,12 +145,12 @@ function AnnotationControl({ dashboardScene }: { dashboardScene: DashboardScene 
         'Add event data to graphs'
       )}
       tooltip={t('dashboard-scene.annotation-control.tooltip-add-new-annotation-query', 'Add new annotation query')}
-      onClick={onAddAnnotation}
+      onClick={onAddAnnotationClick}
     />
   );
 }
 
-type DashboardControlProps = {
+type AddItemProps = {
   icon: IconName;
   label: string;
   description: string;
@@ -147,11 +158,10 @@ type DashboardControlProps = {
   onClick: () => void;
 };
 
-function DashboardControl({ icon, label, description, tooltip, onClick }: DashboardControlProps) {
+function AddItem({ icon, label, description, tooltip, onClick }: AddItemProps) {
   const styles = useStyles2(getStyles);
-
   return (
-    <div className={styles.control}>
+    <div className={styles.addItemContainer}>
       <Button variant="secondary" fill="outline" onClick={onClick} tooltip={tooltip} className={styles.iconButton}>
         <Icon name={icon} size="xl" />
       </Button>
@@ -167,31 +177,14 @@ function DashboardControl({ icon, label, description, tooltip, onClick }: Dashbo
 
 function getStyles(theme: GrafanaTheme2) {
   return {
-    section: css({
+    SidePanesection: css({
       borderBottom: `1px solid ${theme.colors.border.weak}`,
       padding: theme.spacing(2),
     }),
-    sectionHeader: css({
-      margin: theme.spacing(0, 0, 1, 0),
+    SidePanesectionHeader: css({
+      margin: theme.spacing(0, 0, 3, 0),
     }),
-    dragging: css({
-      cursor: 'move',
-    }),
-    dragContainer: css({
-      height: 150,
-    }),
-    imageContainer: css({
-      cursor: 'pointer',
-      opacity: 0.8,
-      overflow: 'hidden',
-      padding: theme.spacing(2, 0),
-      borderRadius: theme.shape.radius.sm,
-      width: '100%',
-      '&:hover': {
-        opacity: 1,
-      },
-    }),
-    control: css({
+    addItemContainer: css({
       display: 'flex',
       flexDirection: 'row',
       gap: theme.spacing(2),
@@ -213,6 +206,23 @@ function getStyles(theme: GrafanaTheme2) {
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-evenly',
+    }),
+    dragging: css({
+      cursor: 'move',
+    }),
+    dragContainer: css({
+      height: 150,
+    }),
+    imageContainer: css({
+      cursor: 'pointer',
+      opacity: 0.8,
+      overflow: 'hidden',
+      padding: theme.spacing(2, 0),
+      borderRadius: theme.shape.radius.sm,
+      width: '100%',
+      '&:hover': {
+        opacity: 1,
+      },
     }),
   };
 }
