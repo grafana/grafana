e3cb7a81998ffb2baac1f65c2d317d9fdb9c6897
Bogdan Matei <bogdan.matei@grafana.com>
2026-01-29 16:02:33 +0200

    Apply suggestion from @bfmatei

--- packages/grafana-schema/src/schema/dashboard/v2beta1/types.spec.gen.ts
+++ packages/grafana-schema/src/schema/dashboard/v2beta1/types.spec.gen.ts
@@ -307,7 +307,7 @@ export interface FieldConfig {
 	description?: string;
 	// An explicit path to the field in the datasource.  When the frame meta includes a path,
 	// This will default to `${frame.meta.path}/${field.name}
-	//
+	// 
 	// When defined, this value can be used as an identifier within the datasource scope, and
 	// may be used to update the results
 	path?: string;
