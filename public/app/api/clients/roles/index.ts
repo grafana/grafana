eb7f01eb416570f3153abc5222762a5a96bd307d
yudintsev <yudintsevegor@gmail.com>
2026-01-29 12:38:17 -0800

    chore: use enhance endpoints where it's possible

--- public/app/api/clients/roles/index.ts
+++ public/app/api/clients/roles/index.ts
@@ -19,26 +19,24 @@ const transformRolesError = (error: FetchBaseQueryError) => {
   return error;
 };
 
-// rolesAPI is needed to be overriden to add transformResponse and transformErrorResponse for some endpoints.
-export const rolesAPI = generatedAPI.injectEndpoints({
-  overrideExisting: true,
-  endpoints: (build) => ({
-    listUserRoles: build.query<
-      Role[],
-      { userId: number; includeHidden?: boolean; includeMapped?: boolean; targetOrgId?: number }
-    >({
-      query: (queryArg) => ({
-        url: `/access-control/users/${queryArg.userId}/roles`,
-        params: {
-          includeHidden: queryArg.includeHidden,
-          includeMapped: queryArg.includeMapped,
-          targetOrgId: queryArg.targetOrgId,
-        },
-      }),
-      providesTags: ['access_control', 'enterprise'],
+const enhancedAPI = generatedAPI.enhanceEndpoints({
+  endpoints: {
+    listUserRoles: {
       transformResponse: transformRolesResponse,
       transformErrorResponse: transformRolesError,
-    }),
+    },
+    listRoles: {
+      transformResponse: transformRolesResponse,
+    },
+  },
+});
+
+// Then inject listTeamRoles endpoint that has type compatibility issues with the generated API.
+// On generated API the `SuccessResponseBody` is returned. However, the current implementation returns a `RoleDto[]`.
+// This incompatibility will be resolved later.
+export const rolesAPI = enhancedAPI.injectEndpoints({
+  overrideExisting: true,
+  endpoints: (build) => ({
     listTeamRoles: build.query<Role[], { teamId: number; includeHidden?: boolean; targetOrgId?: number }>({
       query: (queryArg) => ({
         url: `/access-control/teams/${queryArg.teamId}/roles`,
@@ -50,18 +48,6 @@ export const rolesAPI = generatedAPI.injectEndpoints({
       providesTags: ['access_control', 'enterprise'],
       transformResponse: transformRolesResponse,
       transformErrorResponse: transformRolesError,
-    }),
-    listRoles: build.query<Role[], { delegatable?: boolean; includeHidden?: boolean; targetOrgId?: number }>({
-      query: (queryArg) => ({
-        url: '/access-control/roles',
-        params: {
-          delegatable: queryArg.delegatable,
-          includeHidden: queryArg.includeHidden,
-          targetOrgId: queryArg.targetOrgId,
-        },
-      }),
-      providesTags: ['access_control', 'enterprise'],
-      transformResponse: transformRolesResponse,
     }),
   }),
 });
