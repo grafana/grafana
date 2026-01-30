778e90b46d7acc3b552b3c4f570fcca6dc9ca13b
Paul Marbach <paul.marbach@grafana.com>
2026-01-30 15:25:28 -0500

    Table: Use Stack and noMargin for field spacing for options editor

--- public/app/plugins/panel/table/cells/BarGaugeCellOptionsEditor.tsx
+++ public/app/plugins/panel/table/cells/BarGaugeCellOptionsEditor.tsx
@@ -1,44 +1,36 @@
 import { SelectableValue } from '@grafana/data';
 import { t } from '@grafana/i18n';
 import { BarGaugeDisplayMode, BarGaugeValueMode, TableBarGaugeCellOptions } from '@grafana/schema';
-import { Field, RadioButtonGroup, Stack } from '@grafana/ui';
+import { Field, RadioButtonGroup } from '@grafana/ui';
 
 import { TableCellEditorProps } from '../TableCellOptionEditor';
 
 type Props = TableCellEditorProps<TableBarGaugeCellOptions>;
 
-export function BarGaugeCellOptionsEditor({ cellOptions, onChange }: Props) {
-  // Set the display mode on change
-
-  const onCellOptionsChange = (v: BarGaugeDisplayMode) => {
-    cellOptions.mode = v;
-    onChange(cellOptions);
-  };
-
-  const onValueModeChange = (v: BarGaugeValueMode) => {
-    cellOptions.valueDisplayMode = v;
-    onChange(cellOptions);
-  };
-
-  return (
-    <Stack direction="column" gap={0}>
-      <Field label={t('table.bar-gauge-cell-options-editor.label-gauge-display-mode', 'Gauge display mode')}>
-        <RadioButtonGroup
-          value={cellOptions?.mode ?? BarGaugeDisplayMode.Gradient}
-          onChange={onCellOptionsChange}
-          options={barGaugeOpts}
-        />
-      </Field>
-      <Field label={t('table.bar-gauge-cell-options-editor.label-value-display', 'Value display')}>
-        <RadioButtonGroup
-          value={cellOptions?.valueDisplayMode ?? BarGaugeValueMode.Text}
-          onChange={onValueModeChange}
-          options={valueModes}
-        />
-      </Field>
-    </Stack>
-  );
-}
+export const BarGaugeCellOptionsEditor = ({ cellOptions, onChange }: Props) => (
+  <>
+    <Field noMargin label={t('table.bar-gauge-cell-options-editor.label-gauge-display-mode', 'Gauge display mode')}>
+      <RadioButtonGroup<BarGaugeDisplayMode>
+        value={cellOptions?.mode ?? BarGaugeDisplayMode.Gradient}
+        onChange={(v) => {
+          cellOptions.mode = v;
+          onChange(cellOptions);
+        }}
+        options={barGaugeOpts}
+      />
+    </Field>
+    <Field noMargin label={t('table.bar-gauge-cell-options-editor.label-value-display', 'Value display')}>
+      <RadioButtonGroup<BarGaugeValueMode>
+        value={cellOptions?.valueDisplayMode ?? BarGaugeValueMode.Text}
+        onChange={(v) => {
+          cellOptions.valueDisplayMode = v;
+          onChange(cellOptions);
+        }}
+        options={valueModes}
+      />
+    </Field>
+  </>
+);
 
 const barGaugeOpts: SelectableValue[] = [
   { value: BarGaugeDisplayMode.Basic, label: 'Basic' },
--- public/app/plugins/panel/table/cells/SparklineCellOptionsEditor.tsx
+++ public/app/plugins/panel/table/cells/SparklineCellOptionsEditor.tsx
@@ -3,7 +3,7 @@ import { useId, useMemo } from 'react';
 
 import { createFieldConfigRegistry, SetFieldConfigOptionsArgs } from '@grafana/data';
 import { GraphFieldConfig, TableSparklineCellOptions } from '@grafana/schema';
-import { Field, useStyles2, Stack } from '@grafana/ui';
+import { Field, useStyles2 } from '@grafana/ui';
 import { defaultSparklineCellConfig } from '@grafana/ui/internal';
 
 import { getGraphFieldConfig } from '../../timeseries/config';
@@ -54,7 +54,7 @@ export const SparklineCellOptionsEditor = (props: TableCellEditorProps<TableSpar
   const htmlIdBase = useId();
 
   return (
-    <Stack direction="column">
+    <>
       {registry.list(optionIds.map((id) => `custom.${id}`)).map((item) => {
         if (item.showIf && !item.showIf(values)) {
           return null;
@@ -63,7 +63,7 @@ export const SparklineCellOptionsEditor = (props: TableCellEditorProps<TableSpar
         const path = item.path;
 
         return (
-          <Field label={item.name} key={item.id} className={style.field}>
+          <Field key={item.id} noMargin label={item.name} className={style.field}>
             <Editor
               onChange={(val) => onChange({ ...cellOptions, [path]: val })}
               value={(isOptionKey(path, values) ? values[path] : undefined) ?? item.defaultValue}
@@ -74,7 +74,7 @@ export const SparklineCellOptionsEditor = (props: TableCellEditorProps<TableSpar
           </Field>
         );
       })}
-    </Stack>
+    </>
   );
 };
 
