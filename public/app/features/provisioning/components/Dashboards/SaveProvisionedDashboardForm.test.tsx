ff2caeeeb125470e50b03b7aec3648110ef86174
Yunwen Zheng <yunwen.zheng@grafana.com>
2026-02-02 16:43:40 -0500

    more unit tests fix

--- public/app/features/provisioning/components/Dashboards/SaveProvisionedDashboardForm.test.tsx
+++ public/app/features/provisioning/components/Dashboards/SaveProvisionedDashboardForm.test.tsx
@@ -188,7 +188,7 @@ describe('SaveProvisionedDashboardForm', () => {
     expect(screen.getByTestId('folder-picker')).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: /path/i })).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: /comment/i })).toBeInTheDocument();
-    expect(screen.getByRole('radiogroup')).toBeInTheDocument();
+    expect(screen.getByRole('combobox', { name: /branch/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /cancel/i })).toBeInTheDocument();
   });
@@ -199,7 +199,7 @@ describe('SaveProvisionedDashboardForm', () => {
     expect(screen.getByTestId('common-options')).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: /path/i })).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: /comment/i })).toBeInTheDocument();
-    expect(screen.getByRole('radiogroup')).toBeInTheDocument();
+    expect(screen.getByRole('combobox', { name: /branch/i })).toBeInTheDocument();
     expect(screen.queryByRole('textbox', { name: /title/i })).not.toBeInTheDocument();
     expect(screen.queryByRole('textbox', { name: /description/i })).not.toBeInTheDocument();
   });
@@ -408,10 +408,10 @@ describe('SaveProvisionedDashboardForm', () => {
     expect(screen.queryByTestId('common-options')).not.toBeInTheDocument();
 
     // Workflow options are not shown
-    expect(screen.queryByRole('radiogroup')).not.toBeInTheDocument();
+    expect(screen.queryByRole('combobox', { name: /branch/i })).not.toBeInTheDocument();
 
     // Branch field is not shown
-    expect(screen.queryByRole('textbox', { name: /branch/i })).not.toBeInTheDocument();
+    expect(screen.queryByRole('combobox', { name: /branch/i })).not.toBeInTheDocument();
   });
 
   it('enables save button when only the comment changes', async () => {
