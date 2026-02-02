6c5eeafc5cada67b9367b44733e31a20023cf317
Gilles De Mey <gilles.de.mey@gmail.com>
2026-02-02 17:31:11 +0100

    update test assertions

--- public/app/features/alerting/unified/components/contact-points/ContactPoints.test.tsx
+++ public/app/features/alerting/unified/components/contact-points/ContactPoints.test.tsx
@@ -520,7 +520,7 @@ describe('contact points', () => {
       const { user } = renderWithProvider(<ContactPoint contactPoint={contactPointWithEverything} />);
 
       expect(screen.getByText(/used by 3 alert rules/i)).toBeInTheDocument();
-      expect(screen.getByText(/used by 1 notification policy/i)).toBeInTheDocument();
+      expect(screen.getByText(/used by 1 notification policies/i)).toBeInTheDocument();
 
       await clickMoreActionsButton(contactPointWithEverything.name);
       const deleteButton = screen.getByRole('menuitem', { name: /delete/i });
