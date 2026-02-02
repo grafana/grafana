79e809deaa9a358ecef1d0085e0e78c64c7b543c
Matt Jacobson <matthew.jacobson@grafana.com>
2026-02-02 13:57:47 -0500

    Include imported extra config in routes API as managed route

--- pkg/services/ngalert/notifier/legacy_storage/imported.go
+++ pkg/services/ngalert/notifier/legacy_storage/imported.go
@@ -133,3 +133,35 @@ func (e ImportedConfigRevision) ReceiverUseByName() map[string]int {
 	}
 	return m
 }
+
+func (e ImportedConfigRevision) GetManagedRoute() (*ManagedRoute, error) {
+	if e.importedConfig == nil {
+		return nil, nil
+	}
+
+	// We use a stub config so that calling definition.Merge doesn't modify the original Route.
+	// We leave out parts that are irrelevant to constructing the correctly imported route with renamed receivers and
+	// time intervals.
+	mergeResult, err := definition.Merge(definitions.PostableApiAlertingConfig{
+		Config: definitions.Config{
+			Route:             &definitions.Route{},
+			MuteTimeIntervals: e.rev.Config.AlertmanagerConfig.MuteTimeIntervals,
+			TimeIntervals:     e.rev.Config.AlertmanagerConfig.TimeIntervals,
+		},
+		Receivers: e.rev.Config.AlertmanagerConfig.Receivers,
+	}, *e.importedConfig, e.opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to merge imported config: %w", err)
+	}
+	// The imported route should have been injected into the stub and be the only subroute.
+	if len(mergeResult.Config.Route.Routes) != 1 {
+		// This really shouldn't happen, but just in case.
+		return nil, fmt.Errorf("failed to merge imported config: missing imported route")
+	}
+	importedRoute := mergeResult.Config.Route.Routes[0]
+
+	mr := NewManagedRoute(e.opts.DedupSuffix, importedRoute)
+	mr.Provenance = models.ProvenanceConvertedPrometheus
+	mr.Origin = models.ResourceOriginImported
+	return mr, nil
+}
---
+++ pkg/tests/apis/alerting/notifications/routingtree/imported_test.go
@@ -0,0 +1,144 @@
+package routingtree
+
+import (
+	"context"
+	"embed"
+	"path"
+	"testing"
+
+	"github.com/grafana/grafana-app-sdk/resource"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/alertingnotifications/v0alpha1"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
+	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
+	"github.com/grafana/grafana/pkg/services/ngalert/models"
+	"github.com/grafana/grafana/pkg/tests/api/alerting"
+	"github.com/grafana/grafana/pkg/tests/apis"
+	"github.com/grafana/grafana/pkg/tests/testinfra"
+	"github.com/grafana/grafana/pkg/util"
+	"github.com/grafana/grafana/pkg/util/testutil"
+)
+
+//go:embed test-data/*.*
+var testData embed.FS
+
+func TestIntegrationReadImported_Snapshot(t *testing.T) {
+	testutil.SkipIntegrationTestInShortMode(t)
+	ctx := context.Background()
+
+	helper := apis.NewK8sTestHelper(t, testinfra.GrafanaOpts{
+		EnableFeatureToggles: []string{
+			featuremgmt.FlagAlertingImportAlertmanagerAPI,
+			featuremgmt.FlagAlertingMultiplePolicies,
+		},
+	})
+
+	client, err := v0alpha1.NewRoutingTreeClientFromGenerator(helper.Org1.Admin.GetClientRegistry())
+	require.NoError(t, err)
+
+	cliCfg := helper.Org1.Admin.NewRestConfig()
+	alertingApi := alerting.NewAlertingLegacyAPIClient(helper.GetEnv().Server.HTTPServer.Listener.Addr().String(), cliCfg.Username, cliCfg.Password)
+
+	configYaml, err := testData.ReadFile(path.Join("test-data", "imported.yaml"))
+	require.NoError(t, err)
+
+	identifier := "test-create-get-config"
+	mergeMatchers := "_imported=true"
+
+	headers := map[string]string{
+		"Content-Type":                         "application/yaml",
+		"X-Grafana-Alerting-Config-Identifier": identifier,
+		"X-Grafana-Alerting-Merge-Matchers":    mergeMatchers,
+	}
+
+	amConfig := apimodels.AlertmanagerUserConfig{
+		AlertmanagerConfig: string(configYaml),
+	}
+
+	response := alertingApi.ConvertPrometheusPostAlertmanagerConfig(t, amConfig, headers)
+	require.Equal(t, "success", response.Status)
+
+	routes, err := client.List(ctx, apis.DefaultNamespace, resource.ListOptions{})
+	require.NoError(t, err)
+	require.Len(t, routes.Items, 2)
+	var importedRoute *v0alpha1.RoutingTree
+	for _, r := range routes.Items {
+		if r.Name == identifier {
+			importedRoute = &r
+		}
+	}
+	require.NotNil(t, importedRoute)
+	expected := &v0alpha1.RoutingTree{
+		ObjectMeta: v1.ObjectMeta{
+			Name:            identifier,
+			Namespace:       apis.DefaultNamespace,
+			ResourceVersion: "60514828ecf72916",
+		},
+		Spec: v0alpha1.RoutingTreeSpec{
+			Defaults: v0alpha1.RoutingTreeRouteDefaults{
+				GroupBy:        []string{"alertname", "cluster"},
+				GroupWait:      util.Pointer("1s"),
+				GroupInterval:  util.Pointer("5s"),
+				RepeatInterval: util.Pointer("4h"),
+				Receiver:       "noop",
+			},
+			Routes: []v0alpha1.RoutingTreeRoute{
+				{
+					Receiver:          util.Pointer("noop-warn"),
+					Matchers:          []v0alpha1.RoutingTreeMatcher{{Label: "severity", Type: v0alpha1.RoutingTreeMatcherTypeEqual, Value: "warn"}},
+					MuteTimeIntervals: []string{"mute-interval-1"},
+				},
+				{
+					Receiver:            util.Pointer("noop-critical"),
+					Matchers:            []v0alpha1.RoutingTreeMatcher{{Label: "severity", Type: v0alpha1.RoutingTreeMatcherTypeEqual, Value: "critical"}},
+					ActiveTimeIntervals: []string{"time-interval-1"},
+				},
+			},
+		},
+	}
+	expected.ObjectMeta.UID = importedRoute.ObjectMeta.UID // UID is generated.
+	expected.SetProvenanceStatus(string(models.ProvenanceConvertedPrometheus))
+	require.Equal(t, expected, importedRoute)
+
+	t.Run("should not be able to update", func(t *testing.T) {
+		toUpdate := importedRoute.Copy().(*v0alpha1.RoutingTree)
+		toUpdate.Spec.Defaults.GroupWait = util.Pointer("10s")
+
+		_, err = client.Update(ctx, toUpdate, resource.UpdateOptions{})
+		require.Truef(t, errors.IsBadRequest(err), "Expected BadRequest but got %s", err)
+		require.ErrorContains(t, err, "imported configuration")
+	})
+
+	t.Run("should not be able to delete", func(t *testing.T) {
+		toDelete := importedRoute.Copy().(*v0alpha1.RoutingTree)
+
+		err = client.Delete(ctx, toDelete.GetStaticMetadata().Identifier(), resource.DeleteOptions{})
+		require.Truef(t, errors.IsBadRequest(err), "Expected BadRequest but got %s", err)
+		require.ErrorContains(t, err, "imported configuration")
+	})
+
+	t.Run("should not return if flag is disabled", func(t *testing.T) {
+		helper := apis.NewK8sTestHelper(t, testinfra.GrafanaOpts{
+			EnableFeatureToggles: []string{
+				featuremgmt.FlagAlertingMultiplePolicies,
+			},
+		})
+
+		client, err := v0alpha1.NewRoutingTreeClientFromGenerator(helper.Org1.Admin.GetClientRegistry())
+		require.NoError(t, err)
+
+		routes, err := client.List(ctx, apis.DefaultNamespace, resource.ListOptions{})
+		require.NoError(t, err)
+		require.Len(t, routes.Items, 1)
+		var importedRoute *v0alpha1.RoutingTree
+		for _, r := range routes.Items {
+			if r.Name == identifier {
+				importedRoute = &r
+			}
+		}
+		require.Nil(t, importedRoute)
+	})
+}
