a6d5ee72780fb4a20f34ba2b51214e48935459d2
eledobleefe <laura.fernandez@grafana.com>
2026-02-03 17:13:42 +0100

    SetInterval to functional component draft

--- packages/grafana-ui/src/components/SetInterval/SetInterval.tsx
+++ packages/grafana-ui/src/components/SetInterval/SetInterval.tsx
@@ -1,5 +1,5 @@
 import { isEqual } from 'lodash';
-import { PureComponent } from 'react';
+import { useEffect, useRef } from 'react';
 import { interval, Subscription, Subject, of, NEVER } from 'rxjs';
 import { tap, switchMap } from 'rxjs/operators';
 
@@ -20,20 +20,14 @@ interface Props {
   interval: string;
 }
 
-export class SetInterval extends PureComponent<Props> {
-  private propsSubject: Subject<Props>;
-  private subscription: Subscription | null;
+export const SetInterval = ({ func, loading, interval: intervalStr }: Props) => {
+  const propsSubjectRef = useRef<Subject<Props> | null>(null);
+  const subscriptionRef = useRef<Subscription | null>(null);
+  const prevPropsRef = useRef<Props>({ func, loading, interval: intervalStr });
 
-  constructor(props: Props) {
-    super(props);
-    this.propsSubject = new Subject<Props>();
-    this.subscription = null;
-  }
-
-  componentDidMount() {
-    // Creating a subscription to propsSubject. This subject pushes values every time
-    // SetInterval's props change
-    this.subscription = this.propsSubject
+  useEffect(() => {
+    propsSubjectRef.current = new Subject<Props>();
+    subscriptionRef.current = propsSubjectRef.current
       .pipe(
         // switchMap creates a new observables based on the input stream,
         // which becomes part of the propsSubject stream
@@ -50,37 +44,40 @@ export class SetInterval extends PureComponent<Props> {
           // which makes tap function below execute on that interval basis.
           return props.loading ? NEVER : interval(stringToMs(props.interval));
         }),
-        // tap will execute function passed via func prop
+        // tap will execute function passed by props
         // * on value from `of` stream merged if query is live
         // * on specified interval (triggered by values emitted by interval)
-        tap(() => this.props.func())
+        tap(() => {
+          propsSubjectRef.current && func();
+        })
       )
       .subscribe();
 
-    // When component has mounted, propsSubject emits its first value
-    this.propsSubject.next(this.props);
-  }
+    propsSubjectRef.current.next({ func, loading, interval: intervalStr });
 
-  componentDidUpdate(prevProps: Props) {
-    if (
-      (RefreshPicker.isLive(prevProps.interval) && RefreshPicker.isLive(this.props.interval)) ||
-      isEqual(prevProps, this.props)
-    ) {
+    return () => {
+      if (subscriptionRef.current) {
+        subscriptionRef.current.unsubscribe();
+        subscriptionRef.current = null;
+      }
+      if (propsSubjectRef.current) {
+        propsSubjectRef.current.unsubscribe();
+        propsSubjectRef.current = null;
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const prev = prevPropsRef.current;
+    const current: Props = { func, loading, interval: intervalStr };
+
+    if ((RefreshPicker.isLive(prev.interval) && RefreshPicker.isLive(current.interval)) || isEqual(prev, current)) {
       return;
     }
-    // if props changed, a new value is emitted from propsSubject
-    this.propsSubject.next(this.props);
-  }
 
-  componentWillUnmount() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-
-    this.propsSubject.unsubscribe();
-  }
-
-  render(): null {
-    return null;
-  }
-}
+    propsSubjectRef.current && propsSubjectRef.current.next(current);
+    prevPropsRef.current = current;
+  }, [func, loading, intervalStr]);
+  return null;
+};
