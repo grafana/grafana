7f9ffacd01da85bfbf6343c135d03d6e83c0c5fe
Tito Lins <tito.linsesilva@grafana.com>
2026-02-03 10:16:11 +0100

    add cue definitions / gen

---
+++ apps/alerting/notifications/pkg/apis/alertingnotifications/v0alpha1/inhibitionrule_spec_gen.go
@@ -0,0 +1,41 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+package v0alpha1
+
+// +k8s:openapi-gen=true
+type InhibitionRuleMatcher struct {
+	Type  InhibitionRuleMatcherType `json:"type"`
+	Label string                    `json:"label"`
+	Value string                    `json:"value"`
+}
+
+// NewInhibitionRuleMatcher creates a new InhibitionRuleMatcher object.
+func NewInhibitionRuleMatcher() *InhibitionRuleMatcher {
+	return &InhibitionRuleMatcher{}
+}
+
+// +k8s:openapi-gen=true
+type InhibitionRuleSpec struct {
+	// source_matchers define the alerts that act as inhibitors (silencing other alerts)
+	SourceMatchers []InhibitionRuleMatcher `json:"source_matchers,omitempty"`
+	// target_matchers define the alerts that can be inhibited (silenced)
+	TargetMatchers []InhibitionRuleMatcher `json:"target_matchers,omitempty"`
+	// equal specifies which labels must have equal values between source and target alerts
+	// for the inhibition to take effect
+	Equal []string `json:"equal,omitempty"`
+}
+
+// NewInhibitionRuleSpec creates a new InhibitionRuleSpec object.
+func NewInhibitionRuleSpec() *InhibitionRuleSpec {
+	return &InhibitionRuleSpec{}
+}
+
+// +k8s:openapi-gen=true
+type InhibitionRuleMatcherType string
+
+const (
+	InhibitionRuleMatcherTypeEqual         InhibitionRuleMatcherType = "="
+	InhibitionRuleMatcherTypeNotEqual      InhibitionRuleMatcherType = "!="
+	InhibitionRuleMatcherTypeEqualRegex    InhibitionRuleMatcherType = "=~"
+	InhibitionRuleMatcherTypeNotEqualRegex InhibitionRuleMatcherType = "!~"
+)
