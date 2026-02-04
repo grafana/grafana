67862cd3c8235d141eb6835ab21052730a25cd6f
Kristina Demeshchik <kristina.demeshchik@gmail.com>
2026-02-04 14:41:33 -0500

    log skip variable

--- apps/dashboard/pkg/migration/conversion/v1beta1_to_v2alpha1.go
+++ apps/dashboard/pkg/migration/conversion/v1beta1_to_v2alpha1.go
@@ -1002,6 +1002,8 @@ func transformVariables(ctx context.Context, dashboard map[string]interface{}, d
 
 	variables := make([]dashv2alpha1.DashboardVariableKind, 0, len(list))
 
+	dashboardUID := schemaversion.GetStringValue(dashboard, "uid")
+
 	for _, v := range list {
 		varMap, ok := v.(map[string]interface{})
 		if !ok {
@@ -1050,7 +1052,12 @@ func transformVariables(ctx context.Context, dashboard map[string]interface{}, d
 				variables = append(variables, groupByVar)
 			}
 		default:
-			// Skip unknown variable types
+			// Log and skip unsupported variable type
+			getLogger().Warn("Variable skipped during conversion: unsupported variable type",
+				"dashboardUID", dashboardUID,
+				"variableName", commonProps.Name,
+				"variableType", varType,
+			)
 			continue
 		}
 	}
