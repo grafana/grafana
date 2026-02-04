c00508ddc0255625cf1e4f3d18acf92bb2261c52
Ashley Harrison <ashley.harrison@grafana.com>
2026-02-04 13:01:56 +0000

    import jsx

--- public/app/features/alerting/unified/components/notification-policies/PoliciesList.tsx
+++ public/app/features/alerting/unified/components/notification-policies/PoliciesList.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import { Fragment } from 'react';
+import { Fragment, type JSX } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { Trans, t } from '@grafana/i18n';
