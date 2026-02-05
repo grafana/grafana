6058c529eedd122efc931c5dcee5aa9af0e2269c
Andrej Ocenas <mr.ocenas@gmail.com>
2026-02-05 14:58:46 +0100

    Call search for folders with multiple params instead of one by one

--- public/app/core/components/NestedFolderPicker/useTeamOwnedFolder.ts
+++ public/app/core/components/NestedFolderPicker/useTeamOwnedFolder.ts
@@ -1,10 +1,9 @@
-import { zip } from 'lodash';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 import type { DashboardHit } from '@grafana/api-clients/rtkq/dashboard/v0alpha1';
 import { TeamDto, useGetSignedInUserTeamListQuery } from '@grafana/api-clients/rtkq/legacy';
 import { isFetchError } from '@grafana/runtime';
-import { useLazySearchDashboardsAndFoldersQuery } from 'app/api/clients/dashboard/v0alpha1';
+import { useSearchDashboardsAndFoldersQuery } from 'app/api/clients/dashboard/v0alpha1';
 
 export const TEAM_FOLDERS_UID = 'teamfolders';
 
@@ -23,106 +22,91 @@ type UseGetTeamFoldersResult = {
  * Returns folders owned by teams the current user belongs to.
  */
 export function useGetTeamFolders(options?: { skip: boolean }): UseGetTeamFoldersResult {
-  const { teams, error: teamError } = useTeams(options);
+  const { data: teams, error: teamError } = useTeams(options);
 
-  // We need the lazy version as we are going to call it for every team in a loop.
-  const [triggerSearch] = useLazySearchDashboardsAndFoldersQuery();
+  const ownerReferences = teams?.map(teamOwnerRef);
+  const shouldSkipSearch = !ownerReferences?.length;
 
-  const [foldersByTeam, setFoldersByTeam] = useState<FoldersByTeam[]>([]);
-  const [foldersLoading, setFoldersLoading] = useState(false);
-  const [foldersError, setFoldersError] = useState<Error | undefined>(undefined);
+  const {
+    data: foldersResult,
+    error: foldersError,
+    isLoading: foldersLoading,
+  } = useSearchDashboardsAndFoldersQuery(
+    {
+      // API supports multiple owner references but the client type expects a string because the typing isn't right
+      // in the openAPI spec.
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      ownerReference: ownerReferences as unknown as string,
+      type: 'folder',
+    },
+    { skip: shouldSkipSearch }
+  );
 
-  useEffect(() => {
-    if (!teams || teams.length === 0 || teamError) {
-      return;
-    }
-
-    setFoldersLoading(true);
-    setFoldersError(undefined);
-
-    // Map each team to a request to get the folders they own.
-    const requests = teams.map((team) => {
-      const request = triggerSearch({ ownerReference: `iam.grafana.app/Team/${team.uid}`, type: 'folder' }, true);
-      return { team, request };
-    });
-
-    Promise.allSettled(requests.map((r) => r.request.unwrap()))
-      .then((results) => {
-        // Process all the responses and map them to teams
-
-        const teams = requests.map((r) => r.team);
-        const foldersByTeam = zip(teams, results).reduce<FoldersByTeam[]>((acc, [team, foldersResult]) => {
-          if (foldersResult!.status === 'rejected') {
-            console.warn(`Failed getting folders for ${team}`, foldersResult!.reason);
-            return acc;
-          }
-
-          if (foldersResult!.value.hits && foldersResult!.value.hits.length) {
-            acc.push({ team: team!, folders: foldersResult!.value.hits });
-          }
-          return acc;
-        }, []);
-
-        setFoldersByTeam(foldersByTeam);
-      })
-      .catch((err) => {
-        if (!isAbortError(err)) {
-          setFoldersError(err);
-        }
-      })
-      .finally(() => {
-        setFoldersLoading(false);
-      });
-
-    return () => {
-      for (const r of requests) {
-        r.request.abort();
-      }
-    };
-  }, [teams, teamError, triggerSearch]);
+  const foldersByTeam = useMemo(() => mapTeamsToFolders(teams, foldersResult?.hits), [foldersResult?.hits, teams]);
 
   return {
     foldersByTeam,
     isLoading: (teams === null && !teamError) || foldersLoading,
-    error: teamError ?? foldersError,
+    error: teamError ?? coercedError(foldersError, 'Failed to load team folders'),
   };
+}
+
+function mapTeamsToFolders(teams: TeamDto[] | undefined, folders: DashboardHit[] | undefined): FoldersByTeam[] {
+  if (!teams || !folders?.length) {
+    return [];
+  }
+
+  // Map folders to their owner ownerRef strings
+  const foldersMap = folders.reduce<Record<string, DashboardHit[]>>((acc, folder) => {
+    // Folder can be owned by multiple teams technically
+    for (const ref of folder.ownerReferences!) {
+      if (!acc[ref]) {
+        acc[ref] = [];
+      }
+      acc[ref].push(folder);
+    }
+    return acc;
+  }, {});
+
+  return teams
+    .map((team) => {
+      return {
+        team,
+        // Each team can own multiple folders
+        folders: foldersMap[teamOwnerRef(team)],
+      };
+    })
+    .filter((group) => group.folders?.length > 0);
 }
 
 /**
  * Returns all the teams user is part of.
  */
 function useTeams(options?: { skip: boolean }) {
-  const { data, error } = useGetSignedInUserTeamListQuery(undefined, { skip: options?.skip });
-  const teamError = useCoercedError(error);
+  const { data, error, isLoading } = useGetSignedInUserTeamListQuery(undefined, { skip: options?.skip });
   return {
-    teams: data,
-    error: teamError,
+    data,
+    isLoading,
+    error: coercedError(error),
   };
 }
 
-function useCoercedError(error: unknown) {
-  return useMemo(() => {
-    if (!error) {
-      return undefined;
-    }
-
-    if (error instanceof Error) {
-      return error;
-    }
-
-    if (isFetchError(error)) {
-      return new Error(error.data?.message ?? error.statusText ?? 'Failed to load teams');
-    }
-
-    return new Error('Failed to load teams');
-  }, [error]);
-}
-
-// TODO maybe there should be a common utility like this
-function isAbortError(err: unknown) {
-  if (err instanceof DOMException && err.name === 'AbortError') {
-    return true;
+function coercedError(error: unknown, fallbackMessage = 'Failed to load teams') {
+  if (!error) {
+    return undefined;
   }
 
-  return isFetchError(err) && (err.cancelled || err.statusText === 'Request was aborted');
+  if (error instanceof Error) {
+    return error;
+  }
+
+  if (isFetchError(error)) {
+    return new Error(error.data?.message ?? error.statusText ?? fallbackMessage);
+  }
+
+  return new Error(fallbackMessage);
+}
+
+function teamOwnerRef(team: TeamDto) {
+  return `iam.grafana.app/Team/${team.uid}`;
 }
