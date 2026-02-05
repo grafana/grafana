4f72562ad0770130b5cd52cbbf9b000ab2de7345
Renato Costa <renato.costa@grafana.com>
2026-02-05 15:44:06 -0800

    add test

--- pkg/storage/unified/testing/storage_backend_test.go
+++ pkg/storage/unified/testing/storage_backend_test.go
@@ -3,13 +3,17 @@ package test
 import (
 	"context"
 	"fmt"
+	"strings"
+	"sync"
 	"testing"
 
 	badger "github.com/dgraph-io/badger/v4"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/storage/unified/resource"
+	"github.com/grafana/grafana/pkg/storage/unified/resourcepb"
 	"github.com/grafana/grafana/pkg/util/testutil"
 )
 
@@ -50,6 +54,118 @@ func TestIntegrationBenchmarkSQLKVStorageBackend(t *testing.T) {
 			RunStorageBackendBenchmark(t, backend, opts)
 		})
 	}
+}
+
+func TestConcurrentUpdates(t *testing.T) {
+	const concurrency = 30
+	sqlkv, dbConn := NewTestSqlKvBackend(t, t.Context(), true)
+	dbConn.SqlDB().SetMaxOpenConns(concurrency)
+
+	// Create contended resource
+	const namespace = "namespace"
+	const group = "group"
+	const resourceType = "dashboard"
+	const name = "r1"
+
+	runAttempt := func(attempt int, previousRV int64) int64 {
+		g, groupCtx := errgroup.WithContext(t.Context())
+		newRV := previousRV
+		var mu sync.Mutex
+
+		for tid := range concurrency {
+			g.Go(func() error {
+				updateRV, err := WriteEvent(groupCtx, sqlkv, name, resourcepb.WatchEvent_MODIFIED,
+					WithNamespaceAndRV(namespace, previousRV),
+					WithGroup(group),
+					WithResource(resourceType),
+					WithValue(fmt.Sprintf("123-%d", concurrency*attempt+tid)),
+				)
+
+				// optmistic locking errors are expected
+				if err != nil {
+					if strings.Contains(err.Error(), "optimistic locking failed") {
+					}
+				}
+
+				mu.Lock()
+				newRV = max(newRV, updateRV)
+				mu.Unlock()
+
+				return nil
+			})
+		}
+
+		// Wait for all workers to complete
+		require.NoError(t, g.Wait())
+		return newRV
+	}
+
+	checkConsistency := func(t *testing.T) (string, int64, string, int) {
+		const resourceQuery = `SELECT guid, resource_version, value FROM resource WHERE namespace = ? AND ` + "`group`" + ` = ? AND resource = ? AND name = ?`
+
+		var resourceGUID string
+		var resourceVersion int64
+		var resourceValue string
+		err := dbConn.QueryRowContext(
+			t.Context(), resourceQuery, namespace, group, resourceType, name,
+		).Scan(&resourceGUID, &resourceVersion, &resourceValue)
+		require.NoError(t, err)
+
+		const historyQuery = `SELECT guid, resource_version, value FROM resource_history WHERE namespace = ? AND ` + "`group`" + ` = ? AND resource = ? AND name = ? ORDER BY resource_version ASC`
+
+		type historyRow struct {
+			GUID    string
+			Version int64
+			Value   string
+		}
+
+		var historyRows []historyRow
+		rows, err := dbConn.QueryContext(
+			t.Context(), historyQuery, namespace, group, resourceType, name,
+		)
+		require.NoError(t, err)
+
+		for rows.Next() {
+			var record historyRow
+			require.NoError(t, rows.Scan(&record.GUID, &record.Version, &record.Value))
+			historyRows = append(historyRows, record)
+		}
+		require.NoError(t, rows.Err())
+
+		// Ensure what's in the resource table is the same as the latest entry in resource_history
+		last := historyRows[len(historyRows)-1]
+
+		if last.GUID != resourceGUID || last.Version != resourceVersion || last.Value != resourceValue {
+			msg := fmt.Sprintf("MISMATCH!\n\nresource:\n{%q, %d, %q}\n\n", resourceGUID, resourceVersion, resourceValue)
+
+			msg += "resource_history:\n"
+			for _, h := range historyRows {
+				msg += fmt.Sprintf("{%q, %d, %q}\n", h.GUID, h.Version, h.Value)
+			}
+
+			require.FailNow(t, msg)
+		}
+
+		return resourceGUID, resourceVersion, resourceValue, len(historyRows)
+	}
+
+	resourceRV, err := WriteEvent(t.Context(), sqlkv, name, resourcepb.WatchEvent_ADDED,
+		WithNamespace(namespace),
+		WithGroup(group),
+		WithResource(resourceType),
+		WithValue("abc"))
+	require.NoError(t, err)
+
+	const maxAttempts = 100
+
+	for j := range maxAttempts {
+		t.Logf("Attempt %d", j+1)
+		resourceRV = runAttempt(j, resourceRV)
+		checkConsistency(t)
+	}
+
+	guid, version, value, history := checkConsistency(t)
+	t.Logf("GUID=%q | Version=%d | Value=%q | History=%d", guid, version, value, history)
 }
 
 func TestIntegrationSQLKVStorageBackend(t *testing.T) {
