e675205de8f305b0e04215279c54f356fb037720
joshhunt <josh.hunt@grafana.com>
2026-02-06 09:39:13 +0000

    Update useMTPlugins usage with vanilla OF client

--- packages/grafana-runtime/src/services/pluginMeta/apps.test.ts
+++ packages/grafana-runtime/src/services/pluginMeta/apps.test.ts
@@ -1,4 +1,4 @@
-import { evaluateBooleanFlag } from '../../internal/openFeature';
+import { setTestFlags } from '@grafana/test-utils/unstable';
 
 import {
   getAppPluginMeta,
@@ -11,20 +11,22 @@ import { initPluginMetas } from './plugins';
 import { app } from './test-fixtures/config.apps';
 
 jest.mock('./plugins', () => ({ ...jest.requireActual('./plugins'), initPluginMetas: jest.fn() }));
-jest.mock('../../internal/openFeature', () => ({
-  ...jest.requireActual('../../internal/openFeature'),
-  evaluateBooleanFlag: jest.fn(),
-}));
 
 const initPluginMetasMock = jest.mocked(initPluginMetas);
-const evaluateBooleanFlagMock = jest.mocked(evaluateBooleanFlag);
 
 describe('when useMTPlugins flag is enabled and apps is not initialized', () => {
+  beforeAll(() => {
+    setTestFlags({ useMTPlugins: true });
+  });
+
+  afterAll(() => {
+    setTestFlags({});
+  });
+
   beforeEach(() => {
     setAppPluginMetas({});
     jest.resetAllMocks();
     initPluginMetasMock.mockResolvedValue({ items: [] });
-    evaluateBooleanFlagMock.mockReturnValue(true);
   });
 
   it('getAppPluginMetas should call initPluginMetas and return correct result', async () => {
@@ -57,10 +59,17 @@ describe('when useMTPlugins flag is enabled and apps is not initialized', () => 
 });
 
 describe('when useMTPlugins flag is enabled and apps is initialized', () => {
+  beforeAll(() => {
+    setTestFlags({ useMTPlugins: true });
+  });
+
+  afterAll(() => {
+    setTestFlags({});
+  });
+
   beforeEach(() => {
     setAppPluginMetas({ 'myorg-someplugin-app': app });
     jest.resetAllMocks();
-    evaluateBooleanFlagMock.mockReturnValue(true);
   });
 
   it('getAppPluginMetas should not call initPluginMetas and return correct result', async () => {
@@ -111,10 +120,17 @@ describe('when useMTPlugins flag is enabled and apps is initialized', () => {
 });
 
 describe('when useMTPlugins flag is disabled and apps is not initialized', () => {
+  beforeAll(() => {
+    setTestFlags({ useMTPlugins: false });
+  });
+
+  afterAll(() => {
+    setTestFlags({});
+  });
+
   beforeEach(() => {
     setAppPluginMetas({});
     jest.resetAllMocks();
-    evaluateBooleanFlagMock.mockReturnValue(false);
   });
 
   it('getAppPluginMetas should not call initPluginMetas and return correct result', async () => {
@@ -147,10 +163,17 @@ describe('when useMTPlugins flag is disabled and apps is not initialized', () =>
 });
 
 describe('when useMTPlugins flag is disabled and apps is initialized', () => {
+  beforeAll(() => {
+    setTestFlags({ useMTPlugins: false });
+  });
+
+  afterAll(() => {
+    setTestFlags({});
+  });
+
   beforeEach(() => {
     setAppPluginMetas({ 'myorg-someplugin-app': app });
     jest.resetAllMocks();
-    evaluateBooleanFlagMock.mockReturnValue(false);
   });
 
   it('getAppPluginMetas should not call initPluginMetas and return correct result', async () => {
@@ -201,10 +224,17 @@ describe('when useMTPlugins flag is disabled and apps is initialized', () => {
 });
 
 describe('immutability', () => {
+  beforeAll(() => {
+    setTestFlags({ useMTPlugins: false });
+  });
+
+  afterAll(() => {
+    setTestFlags({});
+  });
+
   beforeEach(() => {
     setAppPluginMetas({ 'myorg-someplugin-app': app });
     jest.resetAllMocks();
-    evaluateBooleanFlagMock.mockReturnValue(false);
   });
 
   it('getAppPluginMetas should return a deep clone', async () => {
