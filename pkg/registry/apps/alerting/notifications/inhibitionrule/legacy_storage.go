19bfed7922b9faf02a7a7e02fb75e7c64b41dc49
Tito Lins <tito.linsesilva@grafana.com>
2026-02-06 14:14:01 +0100

    setup app platform kind

---
+++ pkg/registry/apps/alerting/notifications/inhibitionrule/legacy_storage.go
@@ -0,0 +1,203 @@
+package inhibitionrule
+
+import (
+	"context"
+	stderrors "errors"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/internalversion"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/rest"
+
+	model "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/alertingnotifications/v0alpha1"
+	grafanarest "github.com/grafana/grafana/pkg/apiserver/rest"
+	"github.com/grafana/grafana/pkg/services/apiserver/endpoints/request"
+	ngmodels "github.com/grafana/grafana/pkg/services/ngalert/models"
+	"github.com/grafana/grafana/pkg/services/ngalert/provisioning"
+)
+
+var (
+	_ grafanarest.Storage = (*legacyStorage)(nil)
+)
+
+// InhibitionRuleService defines the interface for inhibition rule operations
+type InhibitionRuleService interface {
+	GetInhibitionRules(ctx context.Context, orgID int64) ([]ngmodels.InhibitionRule, error)
+	GetInhibitionRule(ctx context.Context, uid string, orgID int64) (ngmodels.InhibitionRule, error)
+	CreateInhibitionRule(ctx context.Context, rule ngmodels.InhibitionRule, orgID int64) (ngmodels.InhibitionRule, error)
+	UpdateInhibitionRule(ctx context.Context, rule ngmodels.InhibitionRule, orgID int64) (ngmodels.InhibitionRule, error)
+	DeleteInhibitionRule(ctx context.Context, uid string, orgID int64, provenance ngmodels.Provenance, version string) error
+}
+
+type legacyStorage struct {
+	service        InhibitionRuleService
+	namespacer     request.NamespaceMapper
+	tableConverter rest.TableConvertor
+}
+
+func (s *legacyStorage) New() runtime.Object {
+	return ResourceInfo.NewFunc()
+}
+
+func (s *legacyStorage) Destroy() {}
+
+func (s *legacyStorage) NamespaceScoped() bool {
+	return true // namespace == org
+}
+
+func (s *legacyStorage) GetSingularName() string {
+	return ResourceInfo.GetSingularName()
+}
+
+func (s *legacyStorage) NewList() runtime.Object {
+	return ResourceInfo.NewListFunc()
+}
+
+func (s *legacyStorage) ConvertToTable(ctx context.Context, object runtime.Object, tableOptions runtime.Object) (*metav1.Table, error) {
+	return s.tableConverter.ConvertToTable(ctx, object, tableOptions)
+}
+
+func (s *legacyStorage) List(ctx context.Context, opts *internalversion.ListOptions) (runtime.Object, error) {
+	orgId, err := request.OrgIDForList(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := s.service.GetInhibitionRules(ctx, orgId)
+	if err != nil {
+		return nil, err
+	}
+
+	return ConvertToK8sResources(orgId, res, s.namespacer, opts.FieldSelector)
+}
+
+func (s *legacyStorage) Get(ctx context.Context, uid string, _ *metav1.GetOptions) (runtime.Object, error) {
+	info, err := request.NamespaceInfoFrom(ctx, true)
+	if err != nil {
+		return nil, err
+	}
+
+	rule, err := s.service.GetInhibitionRule(ctx, uid, info.OrgID)
+	if err != nil {
+		if stderrors.Is(err, provisioning.ErrInhibitionRuleNotFound) {
+			return nil, errors.NewNotFound(ResourceInfo.GroupResource(), uid)
+		}
+		return nil, err
+	}
+
+	return ConvertToK8sResource(info.OrgID, rule, s.namespacer)
+}
+
+func (s *legacyStorage) Create(ctx context.Context,
+	obj runtime.Object,
+	createValidation rest.ValidateObjectFunc,
+	_ *metav1.CreateOptions,
+) (runtime.Object, error) {
+	info, err := request.NamespaceInfoFrom(ctx, true)
+	if err != nil {
+		return nil, err
+	}
+	if createValidation != nil {
+		if err := createValidation(ctx, obj.DeepCopyObject()); err != nil {
+			return nil, err
+		}
+	}
+	p, ok := obj.(*model.InhibitionRule)
+	if !ok {
+		return nil, fmt.Errorf("expected inhibition-rule but got %s", obj.GetObjectKind().GroupVersionKind())
+	}
+	if p.Name != "" { // TODO remove when metadata.name can be defined by user
+		return nil, errors.NewBadRequest("object's metadata.name should be empty")
+	}
+	domainModel, err := convertToDomainModel(p)
+	if err != nil {
+		return nil, err
+	}
+	out, err := s.service.CreateInhibitionRule(ctx, domainModel, info.OrgID)
+	if err != nil {
+		return nil, err
+	}
+	return ConvertToK8sResource(info.OrgID, out, s.namespacer)
+}
+
+func (s *legacyStorage) Update(ctx context.Context,
+	uid string,
+	objInfo rest.UpdatedObjectInfo,
+	createValidation rest.ValidateObjectFunc,
+	updateValidation rest.ValidateObjectUpdateFunc,
+	_ bool,
+	_ *metav1.UpdateOptions,
+) (runtime.Object, bool, error) {
+	info, err := request.NamespaceInfoFrom(ctx, true)
+	if err != nil {
+		return nil, false, err
+	}
+
+	old, err := s.Get(ctx, uid, nil)
+	if err != nil {
+		return old, false, err
+	}
+	obj, err := objInfo.UpdatedObject(ctx, old)
+	if err != nil {
+		return old, false, err
+	}
+	if updateValidation != nil {
+		if err := updateValidation(ctx, obj, old); err != nil {
+			return nil, false, err
+		}
+	}
+	p, ok := obj.(*model.InhibitionRule)
+	if !ok {
+		return nil, false, fmt.Errorf("expected inhibition-rule but got %s", obj.GetObjectKind().GroupVersionKind())
+	}
+	rule, err := convertToDomainModel(p)
+	if err != nil {
+		return old, false, err
+	}
+
+	if p.Name != rule.UID {
+		return nil, false, errors.NewBadRequest("name cannot be changed. Consider creating a new resource.")
+	}
+
+	updated, err := s.service.UpdateInhibitionRule(ctx, rule, info.OrgID)
+	if err != nil {
+		return nil, false, err
+	}
+
+	r, err := ConvertToK8sResource(info.OrgID, updated, s.namespacer)
+	return r, false, err
+}
+
+// GracefulDeleter
+func (s *legacyStorage) Delete(ctx context.Context, uid string, deleteValidation rest.ValidateObjectFunc, options *metav1.DeleteOptions) (runtime.Object, bool, error) {
+	info, err := request.NamespaceInfoFrom(ctx, true)
+	if err != nil {
+		return nil, false, err
+	}
+	old, err := s.Get(ctx, uid, nil)
+	if err != nil {
+		return old, false, err
+	}
+	if deleteValidation != nil {
+		if err = deleteValidation(ctx, old); err != nil {
+			return nil, false, err
+		}
+	}
+	version := ""
+	if options.Preconditions != nil && options.Preconditions.ResourceVersion != nil {
+		version = *options.Preconditions.ResourceVersion
+	}
+	p, ok := old.(*model.InhibitionRule)
+	if !ok {
+		return nil, false, fmt.Errorf("expected inhibition-rule but got %s", old.GetObjectKind().GroupVersionKind())
+	}
+
+	err = s.service.DeleteInhibitionRule(ctx, p.Name, info.OrgID, ngmodels.ProvenanceNone, version)
+	return old, false, err // false - will be deleted async
+}
+
+func (s *legacyStorage) DeleteCollection(context.Context, rest.ValidateObjectFunc, *metav1.DeleteOptions, *internalversion.ListOptions) (runtime.Object, error) {
+	return nil, errors.NewMethodNotSupported(ResourceInfo.GroupResource(), "deleteCollection")
+}
