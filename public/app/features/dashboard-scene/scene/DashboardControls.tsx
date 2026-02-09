81587a5d7ea6e36291ec0dd6e54cbcc314a4dc1e
Victor Marin <victor.marin@grafana.com>
2026-02-09 18:04:41 +0200

    fix

--- public/app/features/dashboard-scene/scene/DashboardControls.tsx
+++ public/app/features/dashboard-scene/scene/DashboardControls.tsx
@@ -32,7 +32,7 @@ import { DrilldownControls } from './DrilldownControls';
 import { VariableControls } from './VariableControls';
 import { DashboardControlsButton } from './dashboard-controls-menu/DashboardControlsMenuButton';
 import { hasDashboardControls, useHasDashboardControls } from './dashboard-controls-menu/utils';
-// import { DashboardFiltersOverviewPaneToggle } from './dashboard-filters-overview/DashboardFiltersOverviewPaneToggle';
+import { DashboardFiltersOverviewPaneToggle } from './dashboard-filters-overview/DashboardFiltersOverviewPaneToggle';
 import { EditDashboardSwitch } from './new-toolbar/actions/EditDashboardSwitch';
 import { MakeDashboardEditableButton } from './new-toolbar/actions/MakeDashboardEditableButton';
 import { SaveDashboard } from './new-toolbar/actions/SaveDashboard';
@@ -197,7 +197,7 @@ function DashboardControlsRenderer({ model }: SceneComponentProps<DashboardContr
             )}
             {config.featureToggles.dashboardFiltersOverview && !config.featureToggles.dashboardNewLayouts && (
               <div className={styles.fixedControls}>
-                {/*<DashboardFiltersOverviewPaneToggle dashboard={dashboard} />*/}
+                <DashboardFiltersOverviewPaneToggle dashboard={dashboard} />
               </div>
             )}
           </div>
@@ -235,7 +235,9 @@ function DashboardControlsRenderer({ model }: SceneComponentProps<DashboardContr
           </div>
         )}
         {config.featureToggles.dashboardFiltersOverview && (
-          <div className={styles.fixedControls}>{/*<DashboardFiltersOverviewPaneToggle dashboard={dashboard} />*/}</div>
+          <div className={styles.fixedControls}>
+            <DashboardFiltersOverviewPaneToggle dashboard={dashboard} />
+          </div>
         )}
       </div>
       {config.featureToggles.scopeFilters && !editPanel && (
