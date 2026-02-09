5b4f579b071b1e4c2e82cdf6f55a31f42373351e
Galen <galen.kistler@grafana.com>
2026-02-09 10:28:45 -0600

    chore: add init e2e dash - wip

--- e2e-playwright/panels-suite/logstable.spec.ts
+++ e2e-playwright/panels-suite/logstable.spec.ts
@@ -7,10 +7,17 @@ const DASHBOARD_UID = 'adhjhtt';
 test.use({ viewport: { width: 2000, height: 1080 } });
 test.describe('Panels test: LogsTable - Kitchen Sink', { tag: ['@panels', '@logstable'] }, () => {
   test.only('should render logs table panel', async ({ page, gotoDashboardPage, selectors }) => {
+    await page.pause();
+
     const dashboardPage = await gotoDashboardPage({
       uid: DASHBOARD_UID,
-      queryParams: new URLSearchParams({ editPanel: '1' }),
     });
+
+    await page.pause();
+
+    page.goto('localhost:3001/d/adhjhtt/');
+
+    //http://localhost:3001/d/adhjhtt/
 
     await page.pause();
 
