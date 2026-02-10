3399b4c17c87159f089a4dbec2ecf13030aece8a
Galen <galen.kistler@grafana.com>
2026-02-10 15:42:57 -0600

    chore: fix

--- public/app/features/logs/components/panel/LogTableControls.test.tsx
+++ public/app/features/logs/components/panel/LogTableControls.test.tsx
@@ -9,6 +9,7 @@ describe('LogTableControls', () => {
   it.each([LogsSortOrder.Descending, LogsSortOrder.Ascending])('should render descending', (sortOrder) => {
     render(
       <LogTableControls
+        logOptionsStorageKey={''}
         controlsExpanded={false}
         setControlsExpanded={jest.fn()}
         sortOrder={sortOrder}
@@ -29,6 +30,7 @@ describe('LogTableControls', () => {
 
     render(
       <LogTableControls
+        logOptionsStorageKey={''}
         controlsExpanded={expanded}
         setControlsExpanded={setControlsExpanded}
         sortOrder={LogsSortOrder.Ascending}
@@ -49,6 +51,7 @@ describe('LogTableControls', () => {
       const sortOrderText = `Sorted by ${sortOrder === LogsSortOrder.Ascending ? 'oldest' : 'newest'}`;
       render(
         <LogTableControls
+          logOptionsStorageKey={''}
           controlsExpanded={false}
           setControlsExpanded={jest.fn()}
           sortOrder={sortOrder}
