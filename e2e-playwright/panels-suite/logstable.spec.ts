da6ca543f6d12554ebd3a7c5a1da3d5229420a51
Galen <galen.kistler@grafana.com>
2026-02-10 08:02:57 -0600

    test(e2e): polish tests, update codeowner

--- e2e-playwright/panels-suite/logstable.spec.ts
+++ e2e-playwright/panels-suite/logstable.spec.ts
@@ -4,102 +4,99 @@ const DASHBOARD_UID = 'adhjhtt';
 
 test.use({ viewport: { width: 2000, height: 1080 } });
 test.describe('Panels test: LogsTable', { tag: ['@panels', '@logstable'] }, () => {
-  test('should render logs table panel', async ({ page, gotoDashboardPage, selectors }) => {
-    // this test can absolutely take longer than the default 30s timeout
-    test.setTimeout(120_000);
+  test.describe('Defaults', () => {
+    test('Should render logs table panel', async ({ page, gotoDashboardPage, selectors }) => {
+      const dashboardPage = await gotoDashboardPage({
+        uid: DASHBOARD_UID,
+        queryParams: new URLSearchParams({ editPanel: '2' }),
+      });
 
-    const dashboardPage = await gotoDashboardPage({
-      uid: DASHBOARD_UID,
-      queryParams: new URLSearchParams({ editPanel: '2' }),
+      await expect(
+        dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Default Logs Table Panel'))
+      ).toBeVisible();
+
+      // View log line button should be defined by default
+      await expect(page.getByLabel('View log line').first()).toBeVisible();
+
+      // timestamp and log body headers should be visible
+      await expect(page.getByRole('columnheader', { name: 'timestamp' })).toBeVisible();
+      await expect(page.getByRole('columnheader', { name: 'body' })).toBeVisible();
+      await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
+
+      // timestamp and body columns are selected
+      await expect(page.getByRole('checkbox', { name: 'timestamp' })).toBeChecked();
+      await expect(page.getByRole('checkbox', { name: 'body' })).toBeChecked();
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).toBeVisible();
+
+      // bytes field is not selected
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked({ timeout: 400 });
+
+      // Select bytes field
+      await page.getByText('bytes', { exact: true }).click();
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).toBeChecked();
+      await expect(page.getByRole('columnheader', { name: 'bytes' })).toBeVisible();
+
+      // Reset
+      await page.getByRole('button', { name: 'Reset' }).click();
+      await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked({ timeout: 400 });
+
+      // Search input is visible
+      await expect(page.getByRole('textbox', { name: 'Search fields by name' })).toBeVisible();
+      await page.getByRole('textbox', { name: 'Search fields by name' }).fill('btyes'); // Fuzzy search matches "bytes" against "btyes" (Levenshtein distance < 2)
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked({ timeout: 400 });
+
+      await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
+      await page.getByText('bytes', { exact: true }).click();
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).toBeChecked();
+      await expect(page.getByRole('columnheader', { name: 'bytes' })).toBeVisible();
+
+      // Clear search input
+      await page.getByRole('button', { name: 'Clear' }).click();
+
+      // Reset
+      await page.getByRole('button', { name: 'Reset' }).click();
+      await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
+      await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked({ timeout: 400 });
+
+      // Selected fields collapse
+      await expect(page.getByText('Selected fields')).toBeVisible();
+      await page.getByRole('button', { name: 'Collapse sidebar' }).click();
+      await expect(page.getByText('Selected fields')).not.toBeVisible();
     });
+    test('Show inspect button', async ({ page, gotoDashboardPage, selectors }) => {
+      const dashboardPage = await gotoDashboardPage({
+        uid: DASHBOARD_UID,
+        queryParams: new URLSearchParams({ editPanel: '2' }),
+      });
 
-    await expect(
-      dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Default Logs Table Panel'))
-    ).toBeVisible();
+      await expect(
+        dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Default Logs Table Panel'))
+      ).toBeVisible();
 
-    // View log line button should be defined by default
-    await expect(page.getByLabel('View log line').first()).toBeVisible();
+      await expect(page.getByRole('columnheader', { name: 'timestamp' })).toBeVisible();
+      await page.getByRole('columnheader', { name: 'timestamp' }).click();
 
-    // timestamp and log body headers should be visible
-    await expect(page.getByRole('columnheader', { name: 'timestamp' })).toBeVisible();
-    await expect(page.getByRole('columnheader', { name: 'body' })).toBeVisible();
-    await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
+      // Hide body
+      // @todo locator.click does not work to deselect checkboxes, just causes infinite click loop without updating state, when clicking on the label or the checkbox. Is there a less hacky way to deselect?
+      await expect.poll(() => page.getByRole('checkbox', { name: 'body' }).isChecked()).toEqual(true);
+      page.getByLabel(/body/).first().dispatchEvent('click');
+      await expect.poll(() => page.getByRole('checkbox', { name: 'body' }).isChecked()).toEqual(false);
 
-    // timestamp and body columns are selected
-    await expect(page.getByRole('checkbox', { name: 'timestamp' })).toBeChecked();
-    await expect(page.getByRole('checkbox', { name: 'body' })).toBeChecked();
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).toBeVisible();
+      // Click inspect on 9th row
+      await page.getByRole('gridcell').getByLabel('View log line').nth(9).click();
 
-    // bytes field is not selected
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked();
+      // Assert drawer header is visible
+      await expect(page.getByRole('heading', { name: 'Inspect value' })).toBeVisible();
 
-    // Select bytes field
-    await page.getByText('bytes', { exact: true }).click();
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).toBeChecked();
-    await expect(page.getByRole('columnheader', { name: 'bytes' })).toBeVisible();
-
-    // Reset
-    await page.getByRole('button', { name: 'Reset' }).click();
-    await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked();
-
-    // Search input is visible
-    await expect(page.getByRole('textbox', { name: 'Search fields by name' })).toBeVisible();
-    await page.getByRole('textbox', { name: 'Search fields by name' }).fill('btyes'); // Fuzzy search matches "bytes" against "btyes" (Levenshtein distance < 2)
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked();
-
-    await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
-    await page.getByText('bytes', { exact: true }).click();
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).toBeChecked();
-    await expect(page.getByRole('columnheader', { name: 'bytes' })).toBeVisible();
-
-    // Clear search input
-    await page.getByRole('button', { name: 'Clear' }).click();
-
-    // Reset
-    await page.getByRole('button', { name: 'Reset' }).click();
-    await expect(page.getByRole('columnheader', { name: 'bytes' })).toHaveCount(0);
-    await expect(page.getByRole('checkbox', { name: 'bytes' })).not.toBeChecked();
-
-    // Selected fields collapse
-    await expect(page.getByText('Selected fields')).toBeVisible();
-    await page.getByRole('button', { name: 'Collapse sidebar' }).click();
-    await expect(page.getByText('Selected fields')).not.toBeVisible();
-  });
-
-  test('Show inspect button', async ({ page, gotoDashboardPage, selectors }) => {
-    const dashboardPage = await gotoDashboardPage({
-      uid: DASHBOARD_UID,
-      queryParams: new URLSearchParams({ editPanel: '2' }),
+      // Assert the inspect drawer shows the correct log line body
+      await expect(page.getByLabel('Drawer title Inspect value').locator('.view-lines')).toContainText(
+        `level=info ts=2026-02-06T18:42:42.083508023Z caller=flush.go:253 msg="completing block" userid=29 blockID=73zco`
+      );
     });
-
-    await expect(
-      dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Default Logs Table Panel'))
-    ).toBeVisible();
-
-    await expect(page.getByRole('columnheader', { name: 'timestamp' })).toBeVisible();
-    await page.getByRole('columnheader', { name: 'timestamp' }).click();
-
-    // Hide body
-    // @todo locator.click does not work to deselect checkboxes, just causes infinite click loop without updating state, when clicking on the label or the checkbox. Is there a less hacky way to deselect?
-    await expect.poll(() => page.getByRole('checkbox', { name: 'body' }).isChecked()).toEqual(true);
-    page.getByLabel(/body/).first().dispatchEvent('click');
-    await expect.poll(() => page.getByRole('checkbox', { name: 'body' }).isChecked()).toEqual(false);
-
-    // Click inspect on 9th row
-    await page.getByRole('gridcell').getByLabel('View log line').nth(9).click();
-
-    // Assert drawer header is visible
-    await expect(page.getByRole('heading', { name: 'Inspect value' })).toBeVisible();
-
-    // Assert the inspect drawer shows the correct log line body
-    await expect(page.locator('.view-lines')).toContainText(
-      `level=info ts=2026-02-06T18:42:42.083508023Z caller=flush.go:253 msg="completing block" userid=29 blockID=73zco`
-    );
   });
-
   test.describe('Options', () => {
-    test('Options: Inspect button', async ({ page, gotoDashboardPage, selectors }) => {
+    test('Inspect button', async ({ page, gotoDashboardPage, selectors }) => {
       const dashboardPage = await gotoDashboardPage({
         uid: DASHBOARD_UID,
         queryParams: new URLSearchParams({ editPanel: '2' }),
@@ -116,33 +113,75 @@ test.describe('Panels test: LogsTable', { tag: ['@panels', '@logstable'] }, () =
       await expect(option).toBeChecked();
       await expect(inspectLogLineButton.nth(0)).toBeVisible();
       await optionWrapper.click();
-      await expect(option).not.toBeChecked({ timeout: 1000 });
+      await expect(option).not.toBeChecked({ timeout: 400 });
       await expect(inspectLogLineButton).toHaveCount(0);
     });
-
-    test.skip('Options: Copy log line button', async ({ page, gotoDashboardPage, selectors }) => {
+    test('Copy log line button', async ({ page, gotoDashboardPage, selectors }) => {
       const dashboardPage = await gotoDashboardPage({
         uid: DASHBOARD_UID,
+        queryParams: new URLSearchParams({ editPanel: '2' }),
       });
 
       await expect(
         dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Default Logs Table Panel'))
       ).toBeVisible();
+
+      const optionWrapper = page.getByLabel('Logs Table Show copy log link button field property editor');
+      const option = optionWrapper.getByLabel(/Show copy log link button/);
+      const copyLogLineButton = page.getByLabel('Copy link to log line');
+
+      await expect(option).toHaveCount(1);
+      await expect(option).not.toBeChecked({ timeout: 400 });
+      await expect(copyLogLineButton).toHaveCount(0);
+      await optionWrapper.click();
+      await expect(option).toBeChecked();
+      await expect(copyLogLineButton.nth(0)).toBeVisible();
     });
-
-    test.skip('Options: Show controls', async ({ page, gotoDashboardPage, selectors }) => {
+    test('Show controls', async ({ page, gotoDashboardPage, selectors }) => {
       const dashboardPage = await gotoDashboardPage({
         uid: DASHBOARD_UID,
+        queryParams: new URLSearchParams({ editPanel: '2' }),
       });
 
       await expect(
         dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Default Logs Table Panel'))
       ).toBeVisible();
+
+      const optionWrapper = page.getByLabel('Logs Table Show controls field property editor');
+      const optionLabel = optionWrapper.getByText(/Show controls/);
+      const option = optionWrapper.getByLabel(/Show controls/);
+      const controlsExpandButtonDefault = page.getByLabel('Collapse', { exact: true });
+      const controlsExpandButtonChecked = page.getByLabel('Expand', { exact: true });
+      const controlsSortByButtonNewest = page.getByLabel('Sorted by newest logs first - Click to show oldest first', {
+        exact: true,
+      });
+      const controlsSortByButtonOldest = page.getByLabel('Sorted by oldest logs first - Click to show newest first', {
+        exact: true,
+      });
+
+      // Assert default option state
+      await expect(option).toHaveCount(1);
+      await expect(option).not.toBeChecked({ timeout: 400 });
+      await expect(controlsExpandButtonDefault).toHaveCount(0);
+      await expect(controlsSortByButtonNewest).toHaveCount(0);
+
+      // Toggle option on
+      await optionLabel.click();
+      await expect(option).toBeChecked();
+      await expect(controlsExpandButtonDefault).toHaveCount(1);
+      await expect(controlsSortByButtonNewest).toHaveCount(1);
+
+      // Sort by should update state (but won't change logs in test data source)
+      await controlsSortByButtonNewest.click();
+      await expect(controlsSortByButtonOldest).toHaveCount(1);
+
+      // Collapse expanded options sidebar
+      await controlsExpandButtonDefault.click();
+      await expect(controlsExpandButtonChecked).toHaveCount(1);
     });
   });
-
   test.describe('No data', () => {
-    test('Not logs frame', async ({ page, gotoDashboardPage, selectors }) => {
+    test('Invalid logs frame', async ({ page, gotoDashboardPage, selectors }) => {
       const dashboardPage = await gotoDashboardPage({
         uid: DASHBOARD_UID,
         queryParams: new URLSearchParams({ editPanel: '3' }),
@@ -157,7 +196,6 @@ test.describe('Panels test: LogsTable', { tag: ['@panels', '@logstable'] }, () =
         'Data is missing a string field'
       );
     });
-
     test('Empty logs frame', async ({ page, gotoDashboardPage, selectors }) => {
       const dashboardPage = await gotoDashboardPage({
         uid: DASHBOARD_UID,
