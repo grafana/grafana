b2bb428322b6cc235295a7936235b160732a2723
Ryan McKinley <ryantxu@gmail.com>
2026-02-10 08:56:32 +0300

    historian

--- pkg/tests/apis/openapi_test.go
+++ pkg/tests/apis/openapi_test.go
@@ -121,7 +121,13 @@ func TestIntegrationOpenAPIs(t *testing.T) {
 	}, {
 		Group:   "rules.alerting.grafana.app",
 		Version: "v0alpha1",
-		// }, { Temporarily disabled while we fix OpenAPI generation issues
+		// }, {
+		// In app-sdk v0.40+, the exported names changed
+		// The request/response payload is the same, so the existing generated clients
+		// based on the old OpenAPI will still work.
+		//
+		// This should be updated and replaced soon!
+		//
 		// 	Group:   "historian.alerting.grafana.app",
 		// 	Version: "v0alpha1",
 	}, {
