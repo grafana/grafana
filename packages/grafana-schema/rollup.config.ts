b5474ed004360e95b5f0162fd05f98a375b7de67
Marcus Andersson <marcus.andersson@grafana.com>
2026-02-11 18:09:02 +0100

    fixed so we are backwards compatible with the node moduleresolution.

--- packages/grafana-schema/rollup.config.ts
+++ packages/grafana-schema/rollup.config.ts
@@ -8,6 +8,7 @@ import { cjsOutput, entryPoint, esmOutput, plugins } from '../rollup.config.part
 
 const rq = createRequire(import.meta.url);
 const pkg = rq('./package.json');
+const typePaths = backwardsCompatibilityTypeNames();
 
 export default [
   {
@@ -33,7 +34,10 @@ export default [
           {
             src: 'dist/types/raw/composable/**/*.d.ts',
             dest: 'dist/esm/raw/composable',
-            rename: (_name, _extension, fullpath) => fullpath.split(path.sep).slice(4).join('/'),
+            rename: (_name, _extension, fullpath) => {
+              const typePath = typePaths[fullpath] || fullpath;
+              return typePath.split(path.sep).slice(4).join('/');
+            },
           },
         ],
         hook: 'writeBundle',
@@ -54,3 +58,14 @@ export default [
     treeshake: false,
   },
 ];
+
+function backwardsCompatibilityTypeNames(): Record<string, string> {
+  return Object.keys(pkg.exports).reduce<Record<string, string>>((map, key) => {
+    const typesPath = pkg.exports[key].types;
+    if (!typesPath) {
+      return map;
+    }
+    map[path.normalize(typesPath)] = `${path.normalize(key)}.d.ts`;
+    return map;
+  }, {});
+}
