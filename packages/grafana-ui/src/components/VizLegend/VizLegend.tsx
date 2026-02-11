760468841b071f7b8cf4167d362b550262d2bf05
Leon Sorokin <leeoniya@gmail.com>
2026-02-11 15:10:29 -0600

    VizLegend: Add limit for number of series items

--- packages/grafana-ui/src/components/VizLegend/VizLegend.tsx
+++ packages/grafana-ui/src/components/VizLegend/VizLegend.tsx
@@ -1,15 +1,46 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import * as React from 'react';
 
 import { DataHoverClearEvent, DataHoverEvent } from '@grafana/data';
+import { Trans, t } from '@grafana/i18n';
 import { LegendDisplayMode } from '@grafana/schema';
 
 import { SeriesVisibilityChangeMode, usePanelContext } from '../PanelChrome';
 
+import { LimitedDataDisclaimer } from './LimitedDataDisclaimer';
 import { VizLegendList } from './VizLegendList';
 import { VizLegendTable } from './VizLegendTable';
 import { LegendProps, SeriesVisibilityChangeBehavior, VizLegendItem } from './types';
 import { mapMouseEventToMode } from './utils';
+
+const DEFAULT_SERIES_COUNT = 30;
+
+function wrapWithLimit(
+  children: React.ReactNode,
+  shown: number,
+  total: number,
+  setItemLimit: React.Dispatch<React.SetStateAction<number>>
+) {
+  if (shown < total) {
+    return (
+      <div style={{ position: 'relative' }}>
+        {children}
+        <LimitedDataDisclaimer
+          toggleShowAllSeries={() => setItemLimit(-1)}
+          info={
+            <Trans i18nKey={'legend.container.show-only-series'}>
+              Showing {{ shown }} / {{ total }} series
+            </Trans>
+          }
+          tooltip={t('legend.container.content', 'Showing too many series will impact browser performance')}
+          buttonLabel={<Trans i18nKey={'legend.container.show-all-series'}>Show all series</Trans>}
+        />
+      </div>
+    );
+  }
+
+  return children;
+}
 
 /**
  * @public
@@ -33,6 +64,12 @@ export function VizLegend<T>({
   isSortable,
 }: LegendProps<T>) {
   const { eventBus, onToggleSeriesVisibility, onToggleLegendSort } = usePanelContext();
+
+  const [itemLimit, setItemLimit] = useState(DEFAULT_SERIES_COUNT);
+
+  const limitedItems = useMemo(() => {
+    return itemLimit > 0 ? items.slice(0, itemLimit) : items;
+  }, [items, itemLimit]);
 
   const onMouseOver = useCallback(
     (
@@ -107,10 +144,10 @@ export function VizLegend<T>({
 
   switch (displayMode) {
     case LegendDisplayMode.Table:
-      return (
+      return wrapWithLimit(
         <VizLegendTable<T>
           className={className}
-          items={items}
+          items={limitedItems}
           placement={placement}
           sortBy={sortKey}
           sortDesc={sortDesc}
@@ -121,22 +158,30 @@ export function VizLegend<T>({
           itemRenderer={itemRenderer}
           readonly={readonly}
           isSortable={isSortable}
-        />
+        />,
+        limitedItems.length,
+        items.length,
+        setItemLimit
       );
     case LegendDisplayMode.List:
       const isThresholdsEnabled = thresholdItems && thresholdItems.length > 1;
       const isValueMappingEnabled = mappingItems && mappingItems.length > 0;
-      return (
+      return wrapWithLimit(
         <>
           {/* render items when single series and there is no thresholds and no value mappings
            * render items when multi series and there is no thresholds
            */}
-          {!isThresholdsEnabled && (!isValueMappingEnabled || items.length > 1) && makeVizLegendList(items)}
+          {!isThresholdsEnabled &&
+            (!isValueMappingEnabled || limitedItems.length > 1) &&
+            makeVizLegendList(limitedItems)}
           {/* render threshold colors if From thresholds scheme selected */}
           {isThresholdsEnabled && makeVizLegendList(thresholdItems)}
           {/* render value mapping colors */}
           {isValueMappingEnabled && makeVizLegendList(mappingItems)}
-        </>
+        </>,
+        limitedItems.length,
+        items.length,
+        setItemLimit
       );
     default:
       return null;
