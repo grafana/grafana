ffcbda485c23927209429eb9bc1e224a6a1029bc
Hugo Häggmark <hugo.haggmark@gmail.com>
2026-02-11 07:18:57 +0100

    chore: transfer Levitate alerts

--- packages/grafana-runtime/src/index.ts
+++ packages/grafana-runtime/src/index.ts
@@ -85,4 +85,4 @@ export {
   usePanelPluginInstalled,
   usePanelPluginVersion,
 } from './services/pluginMeta/hooks';
-export { getListedPanelPluginIds, getPanelPluginVersion, isPanelPluginInstalled } from './services/pluginMeta/panels';
+export { getPanelPluginVersion, isPanelPluginInstalled } from './services/pluginMeta/panels';
