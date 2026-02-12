53966292d29c73efbedec9f0554b65847623b857
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2026-02-12 16:54:56 +0000

    Add clarifying comment for admin check logic

--- public/app/core/components/OwnerReferences/OwnerReference.tsx
+++ public/app/core/components/OwnerReferences/OwnerReference.tsx
@@ -7,6 +7,9 @@ import { contextSrv } from 'app/core/services/context_srv';
  * Display a team owner reference as a link to team details page
  */
 export const OwnerReference = ({ team }: { team: Team }) => {
+  // Check if the user is admin, because this is the quickest way (for now)
+  // to be sure that they'll definitely be able to view the team details page in question
+  // In the future, we'll check the access control permissions for the specific team in question
   const isAdmin = contextSrv.hasRole('Admin') || contextSrv.isGrafanaAdmin;
   if (!isAdmin) {
     return <Text>{team.spec.title}</Text>;
