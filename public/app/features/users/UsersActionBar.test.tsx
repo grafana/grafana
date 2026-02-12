5229a1bf46714e8b5c9c4fa2d75936e4023efc0e
MattIPv4 <me@mattcowley.co.uk>
2026-02-12 19:33:26 +0000

    Update tests to provide correct redux state

--- public/app/features/users/UsersActionBar.test.tsx
+++ public/app/features/users/UsersActionBar.test.tsx
@@ -1,39 +1,67 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { mockToolkitActionCreator } from 'test/core/redux/mocks';
 
 import { config } from '@grafana/runtime';
+import { configureStore } from 'app/store/configureStore';
 
 import { Props, UsersActionBarUnconnected } from './UsersActionBar';
-import { searchQueryChanged } from './state/reducers';
+import { initialState, searchQueryChanged } from './state/reducers';
 
 jest.mock('app/core/services/context_srv', () => ({
   contextSrv: {
+    user: {},
     hasPermission: () => true,
   },
 }));
 
 const setup = (propOverrides?: object) => {
+  const store = configureStore({
+    users: {
+      ...initialState,
+      externalUserMngInfo: config.externalUserMngInfo,
+      externalUserMngLinkName: config.externalUserMngLinkName,
+      externalUserMngLinkUrl: config.externalUserMngLinkUrl,
+    },
+  });
+
   const props: Props = {
     searchQuery: '',
     changeSearchQuery: mockToolkitActionCreator(searchQueryChanged),
     onShowInvites: jest.fn(),
     pendingInvitesCount: 0,
-    externalUserMngLinkUrl: '',
-    externalUserMngLinkName: '',
     showInvites: false,
   };
 
   Object.assign(props, propOverrides);
 
-  config.externalUserMngLinkUrl = props.externalUserMngLinkUrl;
-  config.externalUserMngLinkName = props.externalUserMngLinkName;
-
-  const { rerender } = render(<UsersActionBarUnconnected {...props} />);
+  const { rerender } = render(
+    <Provider store={store}>
+      <UsersActionBarUnconnected {...props} />
+    </Provider>
+  );
 
   return { rerender, props };
 };
 
 describe('Render', () => {
+  const originalExternalUserMngInfo = config.externalUserMngInfo;
+  const originalExternalUserMngLinkUrl = config.externalUserMngLinkUrl;
+  const originalExternalUserMngLinkName = config.externalUserMngLinkName;
+  const originalExternalUserMngAnalytics = config.externalUserMngAnalytics;
+  const originalExternalUserMngAnalyticsParams = config.externalUserMngAnalyticsParams;
+  const originalDisableLoginForm = config.disableLoginForm;
+
+  afterEach(() => {
+    // Restore original config after each test
+    config.externalUserMngInfo = originalExternalUserMngInfo;
+    config.externalUserMngLinkUrl = originalExternalUserMngLinkUrl;
+    config.externalUserMngLinkName = originalExternalUserMngLinkName;
+    config.externalUserMngAnalytics = originalExternalUserMngAnalytics;
+    config.externalUserMngAnalyticsParams = originalExternalUserMngAnalyticsParams;
+    config.disableLoginForm = originalDisableLoginForm;
+  });
+
   it('should render component', () => {
     setup();
 
@@ -55,10 +83,10 @@ describe('Render', () => {
   });
 
   it('should show external user management button', () => {
-    setup({
-      externalUserMngLinkUrl: 'http://some/url',
-      externalUserMngLinkName: 'someUrl',
-    });
+    config.externalUserMngLinkUrl = 'http://some/url';
+    config.externalUserMngLinkName = 'someUrl';
+
+    setup();
 
     expect(screen.getByRole('link', { name: 'someUrl' })).toHaveAttribute('href', 'http://some/url');
   });
@@ -66,11 +94,10 @@ describe('Render', () => {
   it('should show external user management button with analytics values when configured', () => {
     config.externalUserMngAnalytics = true;
     config.externalUserMngAnalyticsParams = 'src=grafananet&other=value1';
+    config.externalUserMngLinkUrl = 'http://some/url';
+    config.externalUserMngLinkName = 'someUrl';
 
-    setup({
-      externalUserMngLinkUrl: 'http://some/url',
-      externalUserMngLinkName: 'someUrl',
-    });
+    setup();
 
     expect(screen.getByRole('link', { name: 'someUrl' })).toHaveAttribute(
       'href',
@@ -81,25 +108,21 @@ describe('Render', () => {
   it('should show external user management button without analytics values when disabled', () => {
     config.externalUserMngAnalytics = false;
     config.externalUserMngAnalyticsParams = 'src=grafananet&other=value1';
+    config.externalUserMngLinkUrl = 'http://some/url';
+    config.externalUserMngLinkName = 'someUrl';
 
-    setup({
-      externalUserMngLinkUrl: 'http://some/url',
-      externalUserMngLinkName: 'someUrl',
-    });
+    setup();
 
     expect(screen.getByRole('link', { name: 'someUrl' })).toHaveAttribute('href', 'http://some/url');
   });
 
   it('should not show invite button when externalUserMngInfo is set and disableLoginForm is true', () => {
-    const originalExternalUserMngInfo = config.externalUserMngInfo;
     config.externalUserMngInfo = 'truthy';
     config.disableLoginForm = true;
 
     setup();
 
     expect(screen.queryByRole('link', { name: 'Invite' })).not.toBeInTheDocument();
-    // Reset the disableLoginForm mock to its original value
-    config.externalUserMngInfo = originalExternalUserMngInfo;
   });
 
   it('should show invite button when externalUserMngInfo is not set and disableLoginForm is true', () => {
@@ -109,18 +132,13 @@ describe('Render', () => {
     setup();
 
     expect(screen.getByRole('link', { name: 'Invite' })).toHaveAttribute('href', 'org/users/invite');
-    // Reset the disableLoginForm mock to its original value
-    config.disableLoginForm = false;
   });
 
   it('should show invite button when externalUserMngInfo is set and disableLoginForm is false', () => {
-    const originalExternalUserMngInfo = config.externalUserMngInfo;
     config.externalUserMngInfo = 'truthy';
 
     setup();
 
     expect(screen.getByRole('link', { name: 'Invite' })).toHaveAttribute('href', 'org/users/invite');
-    // Reset the disableLoginForm mock to its original value
-    config.externalUserMngInfo = originalExternalUserMngInfo;
   });
 });
