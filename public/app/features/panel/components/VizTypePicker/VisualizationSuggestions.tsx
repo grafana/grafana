c30e13df52a6dfd36ea4e2268150db2bea350bcc
Adela Almasan <adela.almasan@grafana.com>
2026-02-12 11:56:51 -0600

    useStructureRev

--- public/app/features/panel/components/VizTypePicker/VisualizationSuggestions.tsx
+++ public/app/features/panel/components/VizTypePicker/VisualizationSuggestions.tsx
@@ -16,6 +16,7 @@ import { VizPanel } from '@grafana/scenes';
 import { Alert, Button, Icon, Spinner, Text, useStyles2 } from '@grafana/ui';
 import { UNCONFIGURED_PANEL_PLUGIN_ID } from 'app/features/dashboard-scene/scene/UnconfiguredPanel';
 
+import { useStructureRev } from '../../../explore/Graph/useStructureRev';
 import { getAllPanelPluginMeta } from '../../state/util';
 import { MIN_MULTI_COLUMN_SIZE } from '../../suggestions/constants';
 import { getAllSuggestions } from '../../suggestions/getAllSuggestions';
@@ -35,7 +36,7 @@ export interface Props {
 
 const useSuggestions = (data: PanelData | undefined, searchQuery: string | undefined) => {
   const [hasFetched, setHasFetched] = useState(false);
-  const structureRev = data?.structureRev ?? data?.series?.length ?? 0;
+  const structureRev = useStructureRev(data?.series ?? []);
 
   const { value, loading, error, retry } = useAsyncRetry(async () => {
     await new Promise((resolve) => setTimeout(resolve, hasFetched ? 75 : 0));
