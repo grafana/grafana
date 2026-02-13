b0de080f78d23020110e5f4071b905e017326e82
Sean Griffin <sean@sgriffin.dev>
2026-02-13 18:16:39 -0500

    Query Editor: Update sidebar card styling (#118034)

--- public/app/features/dashboard-scene/panel-edit/PanelEditNext/constants.ts
+++ public/app/features/dashboard-scene/panel-edit/PanelEditNext/constants.ts
@@ -21,7 +21,6 @@ export enum QueryEditorType {
   Expression = 'expression',
   Transformation = 'transformation',
 }
-
 export enum SidebarSize {
   Mini = 'mini',
   Full = 'full',
@@ -32,6 +31,11 @@ export const QUERY_EDITOR_COLORS = {
   query: '#FF8904',
   expression: '#C27AFF',
   transformation: '#00D492',
+  card: {
+    activeBg: '#314158',
+    hoverBg: '#1D293D',
+    headerBg: '#20262F',
+  },
 };
 
 export interface QueryEditorTypeConfig {
