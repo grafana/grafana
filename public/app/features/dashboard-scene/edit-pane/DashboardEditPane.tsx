64c8775a2f33a069d356d2be26e2062901592583
Dominik Prokop <dominik.prokop@grafana.com>
2026-02-13 13:11:14 +0100

    Fix code pane not closing on edit mode exit

--- public/app/features/dashboard-scene/edit-pane/DashboardEditPane.tsx
+++ public/app/features/dashboard-scene/edit-pane/DashboardEditPane.tsx
@@ -208,7 +208,7 @@ export class DashboardEditPane extends SceneObjectBase<DashboardEditPaneState> {
     this.setState({
       selectionContext: { ...this.state.selectionContext, selected: [], enabled: false },
       selection: undefined,
-      openPane: this.state.openPane === 'element' ? undefined : this.state.openPane,
+      openPane: undefined,
     });
   }
 
