c61af73d7bba3f6ebd5f8234ffaeff660ae01b26
Ashley Harrison <ashley.harrison@grafana.com>
2026-02-16 10:29:51 +0000

    Internationalisation: Remove specific i18n sorting function (#118178)
    remove specific i18n sorting function

--- public/app/plugins/datasource/mssql/i18next.config.ts
+++ public/app/plugins/datasource/mssql/i18next.config.ts
@@ -8,6 +8,5 @@ export default defineConfig({
     defaultNS: 'mssql',
     functions: ['t', '*.t'],
     transComponents: ['Trans'],
-    sort: (a, b) => (a.key > b.key ? 1 : a.key < b.key ? -1 : 0),
   },
 });
