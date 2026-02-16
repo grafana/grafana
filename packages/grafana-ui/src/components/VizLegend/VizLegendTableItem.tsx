959d95316b16e7adda4da666ecd3977f0c184880
Ashley Harrison <ashley.harrison@grafana.com>
2026-02-16 11:56:54 +0000

    Remove noMargin prop on SeriesIcon and make it default behaviour

--- packages/grafana-ui/src/components/VizLegend/VizLegendTableItem.tsx
+++ packages/grafana-ui/src/components/VizLegend/VizLegendTableItem.tsx
@@ -148,6 +148,7 @@ const getStyles = (theme: GrafanaTheme2) => {
       display: 'flex',
       whiteSpace: 'nowrap',
       alignItems: 'center',
+      gap: theme.spacing(1),
     }),
     value: css({
       textAlign: 'right',
