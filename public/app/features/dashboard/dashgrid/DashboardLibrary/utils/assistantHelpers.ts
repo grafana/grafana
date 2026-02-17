208b7879e867c1bba23b14a29c0123d1762111ee
Juan Cabanas <juan.cabanas@grafana.com>
2026-02-17 12:30:00 -0300

    sourceEntryPoint unused removed

--- public/app/features/dashboard/dashgrid/DashboardLibrary/utils/assistantHelpers.ts
+++ public/app/features/dashboard/dashgrid/DashboardLibrary/utils/assistantHelpers.ts
@@ -1,6 +1,5 @@
 import { PluginDashboard } from 'app/types/plugins';
 
-import { SourceEntryPoint, TemplateDashboardSourceEntryPoint } from '../interactions';
 import { GnetDashboard } from '../types';
 
 import { isGnetDashboard } from './dashboardLibraryHelpers';
@@ -16,7 +15,6 @@ export interface TemplateContextData {
   panelTypes?: string[];
   author?: string;
   instructions?: string;
-  sourceEntryPoint?: SourceEntryPoint;
 }
 
 /**
@@ -81,7 +79,6 @@ export function buildTemplateContextData(
     panelTypes: isGnet ? dashboard.panelTypeSlugs : undefined,
     author: isGnet ? dashboard.orgName || dashboard.userName : undefined,
     instructions: `Use the following template details to create the dashboard: ${buildInstructions()}.`,
-    sourceEntryPoint: TemplateDashboardSourceEntryPoint.ASSISTANT_BUTTON,
   };
 }
 
