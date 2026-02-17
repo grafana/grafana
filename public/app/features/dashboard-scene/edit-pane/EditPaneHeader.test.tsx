17de8f12d217a0c719770f0da54afaf13a840898
idastambuk <ida.stambuk@grafana.com>
2026-02-17 14:42:26 +0100

    Refactor tests

--- public/app/features/dashboard-scene/edit-pane/EditPaneHeader.test.tsx
+++ public/app/features/dashboard-scene/edit-pane/EditPaneHeader.test.tsx
@@ -4,16 +4,15 @@ import userEvent from '@testing-library/user-event';
 import { getPanelPlugin } from '@grafana/data/test';
 import { selectors } from '@grafana/e2e-selectors';
 import { setPluginImportUtils } from '@grafana/runtime';
-import { SceneGridItem, SceneGridLayout, SceneTimeRange, VizPanel } from '@grafana/scenes';
+import { SceneTimeRange } from '@grafana/scenes';
 import { Sidebar, useSidebar } from '@grafana/ui';
 
 import { DashboardScene } from '../scene/DashboardScene';
-import { DashboardGridItem } from '../scene/layout-default/DashboardGridItem';
-import { DefaultGridLayoutManager } from '../scene/layout-default/DefaultGridLayoutManager';
 import { RowItem } from '../scene/layout-rows/RowItem';
 import { RowsLayoutManager } from '../scene/layout-rows/RowsLayoutManager';
 import { TabItem } from '../scene/layout-tabs/TabItem';
 import { TabsLayoutManager } from '../scene/layout-tabs/TabsLayoutManager';
+import { EditableDashboardElement } from '../scene/types/EditableDashboardElement';
 import { DashboardInteractions } from '../utils/interactions';
 import { activateFullSceneTree } from '../utils/test-utils';
 
@@ -33,49 +32,79 @@ jest.mock('../utils/interactions', () => ({
   },
 }));
 
-const sceneWithTab = new DashboardScene({
-  $timeRange: new SceneTimeRange({ from: 'now-6h', to: 'now' }),
-  isEditing: true,
-  body: new TabsLayoutManager({
-    tabs: [
-      new TabItem({
-        title: 'test tab',
-      }),
-    ],
-  }),
+describe('EditPaneHeader', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  describe('tracking item deletion', () => {
+    it('should call DashboardInteractions.trackDeleteDashboardElement when deleting a row', async () => {
+      const { row, mockEditPane } = setup('row');
+      const elementSelection = new ElementSelection([['row-test', row!.getRef()]]);
+      const editableElement = elementSelection.createSelectionElement()!;
+      renderEditPaneHeader(editableElement, mockEditPane);
+
+      const user = userEvent.setup();
+      await user.click(screen.getByTestId(selectors.components.EditPaneHeader.deleteButton));
+
+      expect(DashboardInteractions.trackDeleteDashboardElement).toHaveBeenCalledWith('Row');
+    });
+
+    it('should call DashboardInteractions.trackDeleteDashboardElement when deleting a tab', async () => {
+      const { tab, mockEditPane } = setup('tab');
+      const elementSelection = new ElementSelection([['tab-test', tab!.getRef()]]);
+      const editableElement = elementSelection.createSelectionElement()!;
+      renderEditPaneHeader(editableElement, mockEditPane);
+
+      const user = userEvent.setup();
+      await user.click(screen.getByTestId(selectors.components.EditPaneHeader.deleteButton));
+
+      expect(DashboardInteractions.trackDeleteDashboardElement).toHaveBeenCalledWith('Tab');
+    });
+  });
 });
 
-const sceneWithRow = new DashboardScene({
-  $timeRange: new SceneTimeRange({ from: 'now-6h', to: 'now' }),
-  isEditing: true,
-  body: new RowsLayoutManager({
-    rows: [
-      new RowItem({
-        title: 'test row',
-      }),
-    ],
-  }),
-});
+const renderEditPaneHeader = (editableElement: EditableDashboardElement, mockEditPane: DashboardEditPane) => {
+  render(
+    <WrapSidebar>
+      <EditPaneHeader element={editableElement} editPane={mockEditPane} />
+    </WrapSidebar>
+  );
+};
 
-const panel = new VizPanel({
-  key: 'panel-test',
-  pluginId: 'text',
-  title: 'panel-test',
-});
-const gridItem = new DashboardGridItem({ body: panel });
-const sceneWithPanel = new DashboardScene({
-  $timeRange: new SceneTimeRange({ from: 'now-6h', to: 'now' }),
-  isEditing: true,
-  body: new DefaultGridLayoutManager({
-    grid: new SceneGridLayout({
-      children: [gridItem],
+const setup = (layout: 'row' | 'tab'): { row?: RowItem; tab?: TabItem; mockEditPane: DashboardEditPane } => {
+  const mockEditPane = {
+    state: { selection: null },
+    clearSelection: jest.fn(),
+  } as unknown as DashboardEditPane;
+
+  if (layout === 'row') {
+    const sceneWithRow = new DashboardScene({
+      $timeRange: new SceneTimeRange({ from: 'now-6h', to: 'now' }),
+      isEditing: true,
+      body: new RowsLayoutManager({
+        rows: [
+          new RowItem({
+            title: 'test row',
+          }),
+        ],
+      }),
+    });
+    activateFullSceneTree(sceneWithRow);
+    return { row: (sceneWithRow.state.body as RowsLayoutManager).state.rows[0], mockEditPane };
+  }
+  const sceneWithTab = new DashboardScene({
+    $timeRange: new SceneTimeRange({ from: 'now-6h', to: 'now' }),
+    isEditing: true,
+    body: new TabsLayoutManager({
+      tabs: [
+        new TabItem({
+          title: 'test tab',
+        }),
+      ],
     }),
-  }),
-});
-
-const buildTestScene = (scene: DashboardScene) => {
-  activateFullSceneTree(scene);
-  return scene;
+  });
+  activateFullSceneTree(sceneWithTab);
+  return { tab: (sceneWithTab.state.body as TabsLayoutManager).state.tabs[0], mockEditPane };
 };
 
 function WrapSidebar({ children }: { children: React.ReactElement }) {
@@ -83,68 +112,3 @@ function WrapSidebar({ children }: { children: React.ReactElement }) {
 
   return <Sidebar contextValue={sidebarContext}>{children}</Sidebar>;
 }
-
-describe('EditPaneHeader', () => {
-  const mockEditPane = {
-    state: { selection: null },
-    clearSelection: jest.fn(),
-  } as unknown as DashboardEditPane;
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe('tracking item deletion', () => {
-    it('should call DashboardInteractions.trackDeleteDashboardElement when deleting a row', async () => {
-      const user = userEvent.setup();
-      const scene = buildTestScene(sceneWithRow);
-      const row = (scene.state.body as RowsLayoutManager).state.rows[0];
-      const elementSelection = new ElementSelection([['row-test', row.getRef()]]);
-      const editableElement = elementSelection.createSelectionElement()!;
-
-      render(
-        <WrapSidebar>
-          <EditPaneHeader element={editableElement} editPane={mockEditPane} />
-        </WrapSidebar>
-      );
-
-      await user.click(screen.getByTestId(selectors.components.EditPaneHeader.deleteButton));
-      expect(DashboardInteractions.trackDeleteDashboardElement).toHaveBeenCalledWith('row');
-    });
-
-    it('should call DashboardInteractions.trackDeleteDashboardElement when deleting a tab', async () => {
-      const user = userEvent.setup();
-      const scene = buildTestScene(sceneWithTab);
-      const tab = (scene.state.body as TabsLayoutManager).state.tabs[0];
-      const elementSelection = new ElementSelection([['tab-test', tab.getRef()]]);
-      const editableElement = elementSelection.createSelectionElement()!;
-
-      render(
-        <WrapSidebar>
-          <EditPaneHeader element={editableElement} editPane={mockEditPane} />
-        </WrapSidebar>
-      );
-
-      await user.click(screen.getByTestId(selectors.components.EditPaneHeader.deleteButton));
-      expect(DashboardInteractions.trackDeleteDashboardElement).toHaveBeenCalledWith('tab');
-    });
-
-    it('should call DashboardInteractions.trackDeleteDashboardElement when deleting a panel', async () => {
-      const user = userEvent.setup();
-      const scene = buildTestScene(sceneWithPanel);
-      const panel = ((scene.state.body as DefaultGridLayoutManager).state.grid.state.children[0] as SceneGridItem).state
-        .body as VizPanel;
-      const elementSelection = new ElementSelection([['panel-test', panel.getRef()]]);
-      const editableElement = elementSelection.createSelectionElement()!;
-
-      render(
-        <WrapSidebar>
-          <EditPaneHeader element={editableElement} editPane={mockEditPane} />
-        </WrapSidebar>
-      );
-
-      await user.click(screen.getByTestId(selectors.components.EditPaneHeader.deleteButton));
-      expect(DashboardInteractions.trackDeleteDashboardElement).toHaveBeenCalledWith('panel');
-    });
-  });
-});
