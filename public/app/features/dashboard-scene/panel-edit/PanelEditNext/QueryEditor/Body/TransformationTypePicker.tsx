636aca6a63bfc2d2589fa5bee92c193fb70dc994
Nick Richmond <nick.richmond@grafana.com>
2026-02-17 15:21:39 -0500

    refactor: leverage existing `TransformationCard`

--- public/app/features/dashboard-scene/panel-edit/PanelEditNext/QueryEditor/Body/TransformationTypePicker.tsx
+++ public/app/features/dashboard-scene/panel-edit/PanelEditNext/QueryEditor/Body/TransformationTypePicker.tsx
@@ -2,15 +2,15 @@ import { css } from '@emotion/css';
 import { useMemo } from 'react';
 
 import { GrafanaTheme2, standardTransformersRegistry } from '@grafana/data';
-import { Card, Stack, Text, useStyles2, useTheme2 } from '@grafana/ui';
-import { PluginStateInfo } from 'app/features/plugins/components/PluginStateInfo';
+import { useStyles2 } from '@grafana/ui';
+import { TransformationCard } from 'app/features/dashboard/components/TransformationsEditor/TransformationCard';
 
-import { useQueryEditorUIContext } from '../QueryEditorContext';
+import { useQueryEditorUIContext, useQueryRunnerContext } from '../QueryEditorContext';
 
 export function TransformationTypePicker() {
   const styles = useStyles2(getStyles);
-  const theme = useTheme2();
   const { finalizePendingTransformation } = useQueryEditorUIContext();
+  const { data } = useQueryRunnerContext();
 
   const allTransformations = useMemo(() => {
     const collator = new Intl.Collator();
@@ -19,24 +19,16 @@ export function TransformationTypePicker() {
 
   return (
     <div className={styles.grid}>
-      {allTransformations.map((item) => {
-        const imageUrl = theme.isDark ? item.imageDark : item.imageLight;
-
-        return (
-          <Card key={item.id} onClick={() => finalizePendingTransformation(item.id)} noMargin>
-            <Card.Heading>
-              <Stack alignItems="center" justifyContent="space-between">
-                {item.name}
-                <PluginStateInfo state={item.state} />
-              </Stack>
-            </Card.Heading>
-            <Card.Description>
-              <Text variant="bodySmall">{item.description ?? ''}</Text>
-              {imageUrl && <img className={styles.image} src={imageUrl} alt={item.name} />}
-            </Card.Description>
-          </Card>
-        );
-      })}
+      {allTransformations.map((item) => (
+        <TransformationCard
+          key={item.id}
+          transform={item}
+          data={data?.series}
+          onClick={finalizePendingTransformation}
+          showIllustrations
+          fullWidth
+        />
+      ))}
     </div>
   );
 }
@@ -46,10 +38,5 @@ const getStyles = (theme: GrafanaTheme2) => ({
     display: 'grid',
     gap: theme.spacing(1),
     gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-  }),
-  image: css({
-    display: 'block',
-    maxWidth: '100%',
-    marginTop: theme.spacing(2),
   }),
 });
