ee69a5d99e9c68422ffb3e17b1914a509813a764
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2026-02-18 12:10:37 +0000

    Add tests for expand/collapse/filtering

--- public/app/core/components/AppChrome/MegaMenu/MegaMenu.test.tsx
+++ public/app/core/components/AppChrome/MegaMenu/MegaMenu.test.tsx
@@ -1,6 +1,4 @@
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { render } from 'test/test-utils';
+import { render, screen } from 'test/test-utils';
 
 import { NavModelItem } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
@@ -30,7 +28,6 @@ const setup = () => {
       url: 'profile',
     },
   ];
-
   const store = configureStore({ navBarTree });
   return render(<MegaMenu onClose={() => {}} />, { store });
 };
@@ -47,17 +44,17 @@ describe('MegaMenu', () => {
   });
 
   it('should render children', async () => {
-    setup();
-    await userEvent.click(await screen.findByRole('button', { name: 'Expand section: Section name' }));
+    const { user } = setup();
+    await user.click(await screen.findByRole('button', { name: 'Expand section: Section name' }));
     expect(await screen.findByRole('link', { name: 'Child1' })).toBeInTheDocument();
     expect(await screen.findByRole('link', { name: 'Child2' })).toBeInTheDocument();
   });
 
   it('should render grandchildren', async () => {
-    setup();
-    await userEvent.click(await screen.findByRole('button', { name: 'Expand section: Section name' }));
+    const { user } = setup();
+    await user.click(await screen.findByRole('button', { name: 'Expand section: Section name' }));
     expect(await screen.findByRole('link', { name: 'Child1' })).toBeInTheDocument();
-    await userEvent.click(await screen.findByRole('button', { name: 'Expand section: Child1' }));
+    await user.click(await screen.findByRole('button', { name: 'Expand section: Child1' }));
     expect(await screen.findByRole('link', { name: 'Grandchild1' })).toBeInTheDocument();
     expect(await screen.findByRole('link', { name: 'Child2' })).toBeInTheDocument();
   });
@@ -66,5 +63,49 @@ describe('MegaMenu', () => {
     setup();
 
     expect(screen.queryByLabelText('Profile')).not.toBeInTheDocument();
+  });
+
+  fdescribe('mega menu controls', () => {
+    describe('expand/collapse all sections', () => {
+      it('should expand and collapse all sections', async () => {
+        const allExpectedChildItems = ['Child1', 'Grandchild1', 'Child2'];
+
+        const { user } = setup();
+        allExpectedChildItems.forEach((item) => {
+          expect(screen.queryByText(item)).not.toBeInTheDocument();
+        });
+
+        await user.click(await screen.findByRole('button', { name: 'Expand all sections' }));
+
+        allExpectedChildItems.forEach((item) => {
+          expect(screen.getByText(item)).toBeInTheDocument();
+        });
+
+        await user.click(await screen.findByRole('button', { name: 'Collapse all sections' }));
+
+        allExpectedChildItems.forEach((item) => {
+          expect(screen.queryByText(item)).not.toBeInTheDocument();
+        });
+      });
+    });
+
+    describe('filter menu items', () => {
+      it('filters menu items', async () => {
+        const { user } = setup();
+        await user.type(await screen.findByPlaceholderText('Search menu'), 'Child1');
+        expect(await screen.findByText('Child1')).toBeInTheDocument();
+      });
+
+      it('clears filter and shows all menu items', async () => {
+        const { user } = setup();
+        await user.type(await screen.findByPlaceholderText('Search menu'), 'Child1');
+
+        await screen.findByText('Child1');
+
+        await user.click(await screen.findByRole('button', { name: 'Clear filter' }));
+
+        expect(await screen.findByText('Grandchild1')).toBeInTheDocument();
+      });
+    });
   });
 });
