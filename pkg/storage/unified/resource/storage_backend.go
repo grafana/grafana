08c52019325d0eb11beaccfa369d8e2faf2dfcd2
Maicon Costa <maiconscosta@gmail.com>
2026-02-18 11:42:07 -0300

    run gofmt
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/storage/unified/resource/storage_backend.go
+++ pkg/storage/unified/resource/storage_backend.go
@@ -500,7 +500,7 @@ func (b *kvStorageBackend) garbageCollectBatch(ctx context.Context, group, resou
 			}
 
 			// get the keys for the same resource
-			for currDK, _ := range b.dataStore.Keys(ctx, ListRequestKey{
+			for currDK := range b.dataStore.Keys(ctx, ListRequestKey{
 				Group:     dk.Group,
 				Resource:  dk.Resource,
 				Namespace: dk.Namespace,
