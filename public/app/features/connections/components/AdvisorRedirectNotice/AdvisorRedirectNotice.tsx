fd37d97edccbbd2a7bb58a79e3478ee0fe011b6f
Andres Martinez Gotor <andres.martinez@grafana.com>
2026-02-18 10:18:56 +0100

    skip a11y error

--- public/app/features/connections/components/AdvisorRedirectNotice/AdvisorRedirectNotice.tsx
+++ public/app/features/connections/components/AdvisorRedirectNotice/AdvisorRedirectNotice.tsx
@@ -1,14 +1,30 @@
+import { css } from '@emotion/css';
 import { useEffect, useState } from 'react';
 
+import { GrafanaTheme2 } from '@grafana/data';
 import { Trans, t } from '@grafana/i18n';
 import { config, useAppPluginInstalled } from '@grafana/runtime';
 import { UserStorage } from '@grafana/runtime/internal';
-import { Alert, LinkButton } from '@grafana/ui';
+import { Alert, LinkButton, useStyles2 } from '@grafana/ui';
 import { contextSrv } from 'app/core/services/context_srv';
 
+const getStyles = (theme: GrafanaTheme2) => ({
+  alertContent: css({
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 0,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }),
+  alertParagraph: css({
+    margin: theme.spacing(0, 1, 0, 0),
+    lineHeight: theme.spacing(theme.components.height.sm),
+  }),
+});
 const userStorage = new UserStorage('advisor-redirect-notice');
 
 export function AdvisorRedirectNotice() {
+  const styles = useStyles2(getStyles);
   const hasAdminRights = contextSrv.hasRole('Admin') || contextSrv.isGrafanaAdmin;
   const [showNotice, setShowNotice] = useState(false);
   const { value: isAdvisorInstalled } = useAppPluginInstalled('grafana-advisor-app');
@@ -34,23 +50,27 @@ export function AdvisorRedirectNotice() {
   return (
     <Alert
       severity="info"
-      title={t(
-        'connections.advisor-redirect-notice.title',
-        'Try the new Advisor to uncover potential issues with your data sources and plugins'
-      )}
+      title=""
       onRemove={() => {
         userStorage.setItem('showNotice', 'false');
         setShowNotice(false);
       }}
     >
-      <LinkButton
-        aria-label={t('connections.advisor-redirect-notice.aria-label-link-to-advisor', 'Link to Advisor')}
-        icon="arrow-right"
-        href="/a/grafana-advisor-app"
-        variant="primary"
-      >
-        <Trans i18nKey="connections.advisor-redirect-notice.go-to-advisor">Go to Advisor</Trans>
-      </LinkButton>
+      <div className={styles.alertContent}>
+        <p className={styles.alertParagraph}>
+          <Trans i18nKey="connections.advisor-redirect-notice.body">
+            Try the new Advisor to uncover potential issues with your data sources and plugins.
+          </Trans>
+        </p>
+        <LinkButton
+          aria-label={t('connections.advisor-redirect-notice.aria-label-link-to-advisor', 'Link to Advisor')}
+          icon="arrow-right"
+          href="/a/grafana-advisor-app"
+          fill="text"
+        >
+          <Trans i18nKey="connections.advisor-redirect-notice.go-to-advisor">Go to Advisor</Trans>
+        </LinkButton>
+      </div>
     </Alert>
   );
 }
