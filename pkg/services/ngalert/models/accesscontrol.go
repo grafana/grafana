f06191260ee1f7b43a5548c4dfd32905da421f8c
Tito Lins <tito.linsesilva@grafana.com>
2026-02-18 11:56:49 +0100

    use base scope provider
    since we don't need to shorten uids, we don't need a custom provider
    here

--- pkg/services/ngalert/models/accesscontrol.go
+++ pkg/services/ngalert/models/accesscontrol.go
@@ -22,7 +22,7 @@ const (
 var (
 	ScopeReceiversProvider       = ReceiverScopeProvider{accesscontrol.NewScopeProvider(ScopeReceiversRoot)}
 	ScopeReceiversAll            = ScopeReceiversProvider.GetResourceAllScope()
-	ScopeInhibitionRulesProvider = InhibitionRuleScopeProvider{accesscontrol.NewScopeProvider(ScopeInhibitionRulesRoot)}
+	ScopeInhibitionRulesProvider = accesscontrol.NewScopeProvider(ScopeInhibitionRulesRoot)
 	ScopeInhibitionRulesAll      = ScopeInhibitionRulesProvider.GetResourceAllScope()
 )
 
@@ -37,26 +37,6 @@ func (p ReceiverScopeProvider) GetResourceScopeUID(uid string) string {
 // GetResourceIDFromUID converts a receiver uid to a resource id. This is necessary as resource ids are limited to 40 characters.
 // If the uid is already less than or equal to 40 characters, it is returned as is.
 func (p ReceiverScopeProvider) GetResourceIDFromUID(uid string) string {
-	if len(uid) <= util.MaxUIDLength {
-		return uid
-	}
-	// #nosec G505 Used only for shortening the uid, not for security purposes.
-	h := sha1.New()
-	h.Write([]byte(uid))
-	return hex.EncodeToString(h.Sum(nil))
-}
-
-type InhibitionRuleScopeProvider struct {
-	accesscontrol.ScopeProvider
-}
-
-func (p InhibitionRuleScopeProvider) GetResourceScopeUID(uid string) string {
-	return ScopeInhibitionRulesProvider.ScopeProvider.GetResourceScopeUID(p.GetResourceIDFromUID(uid))
-}
-
-// GetResourceIDFromUID converts an inhibition rule uid to a resource id. This is necessary as resource ids are limited to 40 characters.
-// If the uid is already less than or equal to 40 characters, it is returned as is.
-func (p InhibitionRuleScopeProvider) GetResourceIDFromUID(uid string) string {
 	if len(uid) <= util.MaxUIDLength {
 		return uid
 	}
