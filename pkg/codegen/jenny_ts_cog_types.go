dfe98ea8473914aac7d1016b98e87df1f28628fe
spinillos <selenepinillos@gmail.com>
2026-02-19 13:43:02 +0100

    Create a new ts cog jenny and migrate core kinds

---
+++ pkg/codegen/jenny_ts_cog_types.go
@@ -0,0 +1,41 @@
+package codegen
+
+import (
+	"context"
+	"strings"
+
+	"cuelang.org/go/cue"
+	"github.com/grafana/codejen"
+	"github.com/grafana/cog"
+)
+
+// TSCogTypesJenny is a [OneToOne] that produces TypeScript types and defaults using cog.
+type TSCogTypesJenny struct {
+	constantToEnum map[string][]string
+}
+
+var _ codejen.OneToOne[SchemaForGen] = &TSCogTypesJenny{}
+
+func (j TSCogTypesJenny) JennyName() string {
+	return "TSCogTypesJenny"
+}
+
+func (j TSCogTypesJenny) Generate(sfg SchemaForGen) (*codejen.File, error) {
+	packageName := strings.ToLower(sfg.Name)
+	cueValue := sfg.CueFile.LookupPath(cue.ParsePath("lineage.schemas[0].schema.spec"))
+
+	types := cog.TypesFromSchema().
+		CUEValue(packageName, cueValue, cog.ForceEnvelope(sfg.Name)).
+		Typescript(cog.TypescriptConfig{})
+	
+	if j.constantToEnum != nil {
+		types.SchemaTransformations(cog.ConstantToEnum(j.constantToEnum))
+	}
+
+	b, err := types.Run(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return codejen.NewFile("types.gen.ts", b[0].Data, j), nil
+}
