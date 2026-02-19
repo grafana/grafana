d96a9dc2cce8a3413165039058a4c83bf2f07c3c
Tim Mulqueen <Multimo@users.noreply.github.com>
2026-02-19 10:52:43 +0100

    add feature flag to enable frontend to /apis/ for datasource resources (#118450)
    add feature flag to enable front to /apis/

--- pkg/services/featuremgmt/toggles_gen.go
+++ pkg/services/featuremgmt/toggles_gen.go
@@ -207,6 +207,10 @@ const (
 	// Handle datasource resource requests to the legacy API routes by querying the new datasource api group endpoints behind the scenes.
 	FlagDatasourcesApiServerEnableResourceEndpoint = "datasourcesApiServerEnableResourceEndpoint"
 
+	// FlagDatasourcesApiServerEnableResourceEndpointFrontend
+	// Send Datsource resource requests to K8s /apis/ API routes instead of the legacy /api/datasources/uid/{uid}/resources/{path} routes.
+	FlagDatasourcesApiServerEnableResourceEndpointFrontend = "datasourcesApiServerEnableResourceEndpointFrontend"
+
 	// FlagCloudWatchBatchQueries
 	// Runs CloudWatch metrics queries as separate batches
 	FlagCloudWatchBatchQueries = "cloudWatchBatchQueries"
