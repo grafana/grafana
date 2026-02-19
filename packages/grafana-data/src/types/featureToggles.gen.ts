48914febd95497e2c57a0ee32dfa6d7b657730e0
Misi <mgyongyosi@users.noreply.github.com>
2026-02-19 15:05:10 +0100

    IAM: Add feature toggle for teamgroupsync redirect, chores (#118229)
    * Chores
    
    * Add uid to the ctx in MiddlewareTeamUIDResolver
    
    * Add kubernetesTeamGroupSyncsHandlerRedirect feature toggle
    
    * Fix team list error, cleanup feature toggles for external group mapping

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1153,11 +1153,6 @@ export interface FeatureToggles {
   */
   kubernetesAuthnMutation?: boolean;
   /**
-  * Routes external group mapping requests from /api to the /apis endpoint
-  * @default false
-  */
-  kubernetesExternalGroupMapping?: boolean;
-  /**
   * Enables restore deleted dashboards feature
   * @default false
   */
@@ -1498,10 +1493,20 @@ export interface FeatureToggles {
   */
   kubernetesTeamBindings?: boolean;
   /**
-  * Redirects the request to teams related endpoints to the app platform API
+  * Redirects the request of the team endpoints to the app platform APIs
   * @default false
   */
   kubernetesTeamsHandlerRedirect?: boolean;
+  /**
+  * Enables external group mapping APIs in the app platform
+  * @default false
+  */
+  kubernetesExternalGroupMappingsApi?: boolean;
+  /**
+  * Redirects the request of the external group mapping endpoints to the app platform APIs
+  * @default false
+  */
+  kubernetesExternalGroupMappingsRedirect?: boolean;
   /**
   * Use the new APIs for syncing users to teams
   * @default false
