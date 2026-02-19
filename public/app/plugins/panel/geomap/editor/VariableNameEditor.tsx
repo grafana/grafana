f2e0a74f9ae0a07bf229783341a1b5955be7352f
Drew Slobodnjak <60050885+drew08t@users.noreply.github.com>
2026-02-19 13:01:40 -0800

    Fix bug with variable editor

--- public/app/plugins/panel/geomap/editor/VariableNameEditor.tsx
+++ public/app/plugins/panel/geomap/editor/VariableNameEditor.tsx
@@ -1,12 +1,19 @@
-import { StandardEditorProps } from '@grafana/data';
+import { StandardEditorProps, VariableOrigin } from '@grafana/data';
 import { t } from '@grafana/i18n';
+import { getTemplateSrv } from '@grafana/runtime';
 import { SuggestionsInput } from 'app/features/transformers/suggestionsInput/SuggestionsInput';
 import { getVariableName } from 'app/features/variables/inspect/utils';
 
 interface Settings {}
 
 export const VariableNameEditor = ({ value, onChange, context }: StandardEditorProps<string, Settings>) => {
-  const suggestions = context.getSuggestions?.() ?? [];
+  const suggestions = getTemplateSrv()
+    .getVariables()
+    .map((v) => ({
+      value: v.name,
+      label: v.name,
+      origin: VariableOrigin.Template,
+    }));
 
   const handleChange = (newValue: string) => {
     const cleanValue = getVariableName(newValue) ?? newValue;
