63773aa91bd9cc396a31b8a3997e16980a1183f1
Jocelyn Collado-Kuri <jcolladokuri@icloud.com>
2026-02-20 12:34:22 -0800

    add logger and rough draft of initial changes for fetching team http headers

--- pkg/tsdb/tempo/stream_handler.go
+++ pkg/tsdb/tempo/stream_handler.go
@@ -43,7 +43,7 @@ func (s *Service) RunStream(ctx context.Context, request *backend.RunStreamReque
 	tempoDatasource, dsInfoErr := s.getDSInfo(ctx, request.PluginContext)
 
 	// get incoming and team http headers and append to stream request.
-	headers, err := stream_utils.SetHeadersFromIncomingContext(ctx)
+	headers, err := stream_utils.SetHeadersFromIncomingContext(ctx, s.logger)
 	if err != nil {
 		return err
 	}
--- pkg/tsdb/tempo/utils/stream_utils.go
+++ pkg/tsdb/tempo/utils/stream_utils.go
@@ -2,13 +2,18 @@ package stream_utils
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
 
+	claims "github.com/grafana/authlib/types"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
-	"github.com/grafana/grafana/pkg/components/simplejson"
 	"github.com/grafana/grafana/pkg/services/datasources"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
+	"github.com/grafana/grafana/pkg/services/team"
+	"github.com/grafana/grafana/pkg/services/team/teamimpl"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -27,18 +32,18 @@ func AppendHeadersToOutgoingContext(ctx context.Context, req *backend.RunStreamR
 
 // When we receive a new query request we should make sure that all incoming HTTP headers are being forwarding to the grpc stream request
 // this is to make sure that no headers are lost when we make the actual call to Tempo later on.
-func SetHeadersFromIncomingContext(ctx context.Context) (map[string]string, error) {
+func SetHeadersFromIncomingContext(ctx context.Context, logger log.Logger) (map[string]string, error) {
 	// get the plugin from context
 	plugin := backend.PluginConfigFromContext(ctx)
 
 	// get the HTTP headers for the current user's teams only (LBAC)
-	teamHeaders, err := getTeamHTTPHeaders(ctx, plugin)
+	teamHeaders, err := getTeamHTTPHeaders(ctx, plugin, logger)
 	if err != nil {
 		return nil, err
 	}
 
 	// get the rest of the incoming headers
-	headers, err := getClientOptionsHeaders(ctx, plugin)
+	headers, err := getClientOptionsHeaders(ctx, plugin, logger)
 	if err != nil {
 		return nil, err
 	}
@@ -52,11 +57,38 @@ func SetHeadersFromIncomingContext(ctx context.Context) (map[string]string, erro
 // getTeamHTTPHeaders returns HTTP headers for LBAC: only the rules for teams the current user belongs to.
 // This matches the grafana-enterprise teamhttpheaders middleware behavior so that each user only gets
 // headers for their own teams' rules.
-func getTeamHTTPHeaders(ctx context.Context, plugin backend.PluginContext) (map[string]string, error) {
+func getTeamHTTPHeaders(ctx context.Context, plugin backend.PluginContext, logger log.Logger) (map[string]string, error) {
 	headers := map[string]string{}
 
+	if plugin.DataSourceInstanceSettings == nil || plugin.DataSourceInstanceSettings.JSONData == nil {
+		return headers, nil
+	}
+
+	// team headers are only available when basic auth is enabled
+	if !plugin.DataSourceInstanceSettings.BasicAuthEnabled {
+		return headers, nil
+	}
+
+	jsonData := plugin.DataSourceInstanceSettings.JSONData
+	var data map[string]interface{}
+	err := json.Unmarshal(jsonData, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	// no team headers present in the datasource settings
+	if _, exists := data["teamHttpHeaders"]; !exists {
+		return headers, nil
+	}
+
 	requester, err := identity.GetRequester(ctx)
-	if err != nil || requester == nil || requester.IsNil() {
+	if err != nil {
+		return headers, nil
+	}
+
+	// Only support team HTTP headers for users
+	if !requester.IsIdentityType(claims.TypeUser) {
+		logger.Debug("teamHttpHeaders is not supported for identity type %s", "identityType", requester.GetIdentityType())
 		return headers, nil
 	}
 
@@ -68,15 +100,6 @@ func getTeamHTTPHeaders(ctx context.Context, plugin backend.PluginContext) (map[
 	userTeamIDSet := make(map[string]struct{}, len(userTeamIDs))
 	for _, id := range userTeamIDs {
 		userTeamIDSet[strconv.FormatInt(id, 10)] = struct{}{}
-	}
-
-	jsonData := plugin.DataSourceInstanceSettings.JSONData
-	if len(jsonData) == 0 {
-		return headers, nil
-	}
-	sj, err := simplejson.NewJson(jsonData)
-	if err != nil {
-		return nil, fmt.Errorf("parse datasource jsonData: %w", err)
 	}
 
 	teamHTTPHeaders, err := datasources.GetTeamHTTPHeaders(sj)
@@ -105,7 +128,7 @@ func getTeamHTTPHeaders(ctx context.Context, plugin backend.PluginContext) (map[
 	return headers, nil
 }
 
-func getClientOptionsHeaders(ctx context.Context, plugin backend.PluginContext) (map[string]string, error) {
+func getClientOptionsHeaders(ctx context.Context, plugin backend.PluginContext, logger log.Logger) (map[string]string, error) {
 	headers := map[string]string{}
 	opts, err := plugin.DataSourceInstanceSettings.HTTPClientOptions(ctx)
 	if err != nil {
