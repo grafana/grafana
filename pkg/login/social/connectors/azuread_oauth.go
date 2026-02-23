eafe235740846ceb511ae30ec85a8010a7ce7a1d
Richard Hagen <richard.hagen@gmail.com>
2026-02-23 10:32:15 +0100

    fix: move oauth2 import

--- pkg/login/social/connectors/azuread_oauth.go
+++ pkg/login/social/connectors/azuread_oauth.go
@@ -17,6 +17,7 @@ import (
 	jose "github.com/go-jose/go-jose/v4"
 	"github.com/go-jose/go-jose/v4/jwt"
 	"github.com/google/uuid"
+	"golang.org/x/oauth2"
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/infra/remotecache"
@@ -28,7 +29,6 @@ import (
 	"github.com/grafana/grafana/pkg/services/ssosettings/validation"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/util"
-	"golang.org/x/oauth2"
 )
 
 const (
