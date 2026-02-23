a81b749c55452d7448bf9bcc863ba723ca474723
Sean Griffin <sean@sgriffin.dev>
2026-02-23 14:51:37 -0500

    add error styling to title

--- public/app/features/dashboard-scene/panel-edit/PanelEditNext/QueryEditor/Sidebar/CardTitle.tsx
+++ public/app/features/dashboard-scene/panel-edit/PanelEditNext/QueryEditor/Sidebar/CardTitle.tsx
@@ -4,11 +4,11 @@ import { GrafanaTheme2 } from '@grafana/data';
 import { Text, useStyles2 } from '@grafana/ui';
 
 // Text component doesn't let us use strikethrough so we use a span with the correct style instead
-export const CardTitle = ({ title, isHidden }: { title: string; isHidden: boolean }) => {
+export const CardTitle = ({ title, isHidden, isError }: { title: string; isHidden: boolean; isError?: boolean }) => {
   const styles = useStyles2(getStyles, { isHidden });
   return (
     <span className={styles.title}>
-      <Text weight="light" variant="code" color="primary" truncate>
+      <Text weight="light" variant="code" color={isError ? 'error' : 'primary'} truncate>
         {title}
       </Text>
     </span>
