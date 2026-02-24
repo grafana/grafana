c968f8a82e3a27ed08c800d21f7cf5da195c2133
ywzheng1 <yunwen.zheng@grafana.com>
2026-02-24 14:38:16 -0600

    Feature Toggle: createdByMeSearchFilter

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1183,6 +1183,11 @@ export interface FeatureToggles {
   */
   experimentRecentlyViewedDashboards?: boolean;
   /**
+  * Enables the created by me search filter on the browse dashboards page
+  * @default false
+  */
+  createdByMeSearchFilter?: boolean;
+  /**
   * Enable configuration of alert enrichments in Grafana Cloud.
   * @default false
   */
