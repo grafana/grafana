22c023f510d7b82e8a46b94b3eed4291046d26de
Galen <galen.kistler@grafana.com>
2026-02-24 08:03:39 -0600

    chore: clean up story book

--- packages/grafana-ui/src/components/Combobox/Combobox.story.tsx
+++ packages/grafana-ui/src/components/Combobox/Combobox.story.tsx
@@ -106,7 +106,7 @@ export const OptionIcons: Story = {
     value: 'one',
     options: [
       { label: 'One', value: 'one', group: 'Group 1', icon: 'text-fields' },
-      { label: 'Two a longer thing that lorem ipsum et deloret', value: 'two', group: 'Group 1', icon: 'text-fields' },
+      { label: 'Two', value: 'two', group: 'Group 1', icon: 'text-fields' },
       { label: 'Three', value: 'three', group: 'Group 2', icon: 'keyboard' },
       { label: 'Four', value: 'four', group: 'Group 2', icon: 'keyboard' },
     ],
