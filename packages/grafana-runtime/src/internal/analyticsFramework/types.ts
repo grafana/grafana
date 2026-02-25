567b4acb3d50e84bfad8a12df238ed847f9ce4e1
Laura Fernández <laura.fernandez@grafana.com>
2026-02-25 12:12:18 +0100

    Update packages/grafana-runtime/src/internal/analyticsFramework/types.ts
    Co-authored-by: Hugo Häggmark <hugo.haggmark@gmail.com>

--- packages/grafana-runtime/src/internal/analyticsFramework/types.ts
+++ packages/grafana-runtime/src/internal/analyticsFramework/types.ts
@@ -6,7 +6,7 @@ type DataType = string | number | boolean | null | undefined;
 
 export type EventProperty = {
   [key: string]: DataType | DataType[];
-};
+export type EventProperty = Record<string, DataType | DataType[]>;
 export interface Event {
   repo?: string;
   feature: string;
