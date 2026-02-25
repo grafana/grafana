278c1bca6af4cde05d7c460fe478fcd9a3949629
eledobleefe <laura.fernandez@grafana.com>
2026-02-25 16:46:33 +0100

    Fix imports

--- public/app/core/components/SharedPreferences/SharedPreferences.tsx
+++ public/app/core/components/SharedPreferences/SharedPreferences.tsx
@@ -1,10 +1,12 @@
-import { useBooleanFlagValue } from "@openfeature/react-sdk";
+import { useBooleanFlagValue } from '@openfeature/react-sdk';
 
-import { SharedPreferencesFunctional } from "./SharedPreferencesFunctional";
-import SharedPreferencesOld from "./SharedPreferencesOld";
-import { Props } from "./utils";
+import { SharedPreferencesFunctional } from './SharedPreferencesFunctional';
+import SharedPreferencesOld from './SharedPreferencesOld';
+import { Props } from './utils';
 
 export const SharedPreferences = (props: Props) => {
-    const newPrefsEnabled = useBooleanFlagValue('grafana.functional.functionalSharedPreferences', false);
-    return newPrefsEnabled ? <SharedPreferencesFunctional {...props} /> : <SharedPreferencesOld {...props} />;
+  const newPrefsEnabled = useBooleanFlagValue('grafana.functional.functionalSharedPreferences', false);
+  return newPrefsEnabled ? <SharedPreferencesFunctional {...props} /> : <SharedPreferencesOld {...props} />;
 };
+
+SharedPreferences.displayName = 'SharedPreferences';
