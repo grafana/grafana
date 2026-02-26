c3846571506fbe9e3b569dd0dee6b4276a787a72
eledobleefe <laura.fernandez@grafana.com>
2026-02-26 14:30:46 +0100

    Fix weird commit from GitHub

--- packages/grafana-runtime/src/internal/analyticsFramework/types.ts
+++ packages/grafana-runtime/src/internal/analyticsFramework/types.ts
@@ -4,13 +4,11 @@
 
 type DataType = string | number | boolean | null | undefined;
 
-export type EventProperty = {
-  [key: string]: DataType | DataType[];
 export type EventProperty = Record<string, DataType | DataType[]>;
 export interface Event {
   repo?: string;
   feature: string;
   eventName: string;
   description?: string;
-  properties?: EventProperty[];
+  properties?: EventProperty;
 }
