43e0307c2acada86e1a91a77be0e53207f5cbbe9
Galen <galen.kistler@grafana.com>
2026-03-04 11:45:03 -0600

    chore: whoopsie

--- public/app/plugins/panel/status-history/utils.ts
+++ public/app/plugins/panel/status-history/utils.ts
@@ -5,7 +5,6 @@ export const getDataLinks = (field: Field, rowIdx: number) => {
   const links: Array<LinkModel<Field>> = [];
 
   if ((field.config.links?.length ?? 0) > 0 && field.getLinks != null) {
-    // console.log('getLinks');
     const v = field.values[rowIdx];
     const disp = field.display ? field.display(v) : { text: `${v}`, numeric: +v };
 
