3dc163a78b1cc3d84d7460f4ec94a2783c8af3f2
Ryan McKinley <ryantxu@gmail.com>
2026-03-04 08:40:40 +0300

    openapi fix

--- packages/grafana-api-clients/src/clients/rtkq/iam/v0alpha1/endpoints.gen.ts
+++ packages/grafana-api-clients/src/clients/rtkq/iam/v0alpha1/endpoints.gen.ts
@@ -941,7 +941,17 @@ export type SearchExternalGroupMappingsApiArg = {
     externalGroups?: string[];
   };
 };
-export type GetSearchTeamsApiResponse = /** status 200 undefined */ any;
+export type GetSearchTeamsApiResponse = /** status 200 undefined */ {
+  /** APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources */
+  apiVersion?: string;
+  hits: any[];
+  /** Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds */
+  kind?: string;
+  maxScore: number;
+  offset: number;
+  queryCost: number;
+  totalHits: number;
+};
 export type GetSearchTeamsApiArg = {
   /** team name query string */
   query?: string;
