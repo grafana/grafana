ada475cff4d479a57123e0e53d1b9eca3e632f12
Gareth Dawson <gwdawson.work@gmail.com>
2026-03-05 10:54:08 +0000

    remove duplicated test case

--- pkg/tests/apis/datasource/testdata_test.go
+++ pkg/tests/apis/datasource/testdata_test.go
@@ -13,7 +13,6 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	datasourceV0alpha1 "github.com/grafana/grafana/pkg/apis/datasource/v0alpha1"
 	grafanarest "github.com/grafana/grafana/pkg/apiserver/rest"
@@ -162,21 +161,6 @@ func TestIntegrationTestDatasource(t *testing.T) {
 	})
 
 	t.Run("health", func(t *testing.T) {
-		t.Run("GET health returns OK and plugin message", func(t *testing.T) {
-			var healthResult datasourceV0alpha1.HealthCheckResult
-			raw := apis.DoRequest(helper, apis.RequestParams{
-				User:   helper.Org1.Admin,
-				Method: "GET",
-				Path:   "/apis/grafana-testdata-datasource.datasource.grafana.app/v0alpha1/namespaces/default/datasources/test/health",
-			}, &healthResult)
-			require.Equal(t, http.StatusOK, raw.Response.StatusCode, "health check should succeed: %s", string(raw.Body))
-			require.Equal(t, "OK", healthResult.Status)
-			require.Equal(t, int(backend.HealthStatusOk), healthResult.Code)
-			require.Equal(t, "Data source is working", healthResult.Message)
-			require.Equal(t, "HealthCheckResult", healthResult.Kind)
-			require.Equal(t, "grafana-testdata-datasource.datasource.grafana.app/v0alpha1", healthResult.APIVersion)
-			require.Nil(t, healthResult.Details, "testdata plugin does not return JSON details")
-		})
 		t.Run("health endpoint returns 404 for non-existent datasource", func(t *testing.T) {
 			raw := apis.DoRequest[any](helper, apis.RequestParams{
 				User:   helper.Org1.Admin,
