b810dd5c6ecb989df984e38f348c7ed8de95ab02
Paul Marbach <paul.marbach@grafana.com>
2026-03-05 17:27:41 -0500

    some updates

--- e2e-playwright/panels-suite/table-nested.spec.ts
+++ e2e-playwright/panels-suite/table-nested.spec.ts
@@ -7,31 +7,24 @@ const DASHBOARD_UID = 'dcb9f5e9-8066-4397-889e-864b99555dbb';
 test.use({ viewport: { width: 2000, height: 4000 } });
 
 test.describe('Panels test: Table - Nested', { tag: ['@panels', '@table'] }, () => {
-  test('a11y', { tag: ['@a11y'] }, async ({ gotoDashboardPage, scanForA11yViolations, selectors, page }) => {
-    const dashboardPage = await gotoDashboardPage({
-      uid: DASHBOARD_UID,
-      queryParams: new URLSearchParams({ viewPanel: '4' }),
-    });
-
-    await expect(
-      dashboardPage.getByGrafanaSelector(selectors.components.Panels.Panel.title('Nested tables'))
-    ).toBeVisible();
-
+  test('a11y', { tag: ['@a11y'] }, async ({ scanForA11yViolations, selectors, page }) => {
+    await page.goto(selectors.pages.SoloPanel.url(`${DASHBOARD_UID}/panel-tests-table-kitchen-sink?orgId=1&panelId=4`));
+    await expect(page.getByTestId(selectors.components.Panels.Panel.title('Nested tables'))).toBeVisible();
     await waitForTableLoad(page);
 
     await expect(page.locator('[role="row"]').filter({ visible: true })).toHaveCount(3); // header + 2 rows
 
-    const firstRowExpander = dashboardPage
-      .getByGrafanaSelector(selectors.components.Panels.Visualization.TableNG.RowExpander)
-      .first();
+    const firstRowExpander = page.getByTestId(selectors.components.Panels.Visualization.TableNG.RowExpander).first();
 
     await firstRowExpander.click();
     await expect(page.locator('[role="row"]').filter({ visible: true })).not.toHaveCount(3);
 
     const report = await scanForA11yViolations({
-      runOnly: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice'],
+      options: {
+        runOnly: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice'],
+      },
     });
-    expect(report).toHaveNoA11yViolations({ ignoredRules: ['page-has-heading-one', 'region'] });
+    expect(report).toHaveNoA11yViolations({ ignoredRules: ['page-has-heading-one', 'region', 'color-contrast'] });
   });
 
   test('expand and collapse', async ({ gotoDashboardPage, selectors, page }) => {
