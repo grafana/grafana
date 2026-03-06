bb50cb86d799ed4777f704f99638fdbfcd02fc4b
Paul Marbach <paul.marbach@grafana.com>
2026-03-06 18:05:37 -0500

    Table: Add feature toggle to control nested field overrides

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1403,6 +1403,11 @@ export interface FeatureToggles {
   */
   vizLegendSeriesLimit?: boolean;
   /**
+  * Enable field overrides for FieldType.nestedFrames fields (like in nested tables)
+  * @default false
+  */
+  nestedFramesFieldOverrides?: boolean;
+  /**
   * Enable Y-axis scale configuration options for pre-bucketed heatmap data (heatmap-rows)
   * @default false
   */
