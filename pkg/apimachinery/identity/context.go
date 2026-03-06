abbd7cca30a55f3f3ce008b2dfce4e1bfd7aea65
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2026-03-06 08:59:46 -0600

    remove for now

--- pkg/apimachinery/identity/context.go
+++ pkg/apimachinery/identity/context.go
@@ -160,7 +160,6 @@ var serviceIdentityTokenPermissions = []string{
 	"query.grafana.app:*",
 	"datasource.grafana.app:*",
 	"iam.grafana.app:*",
-	"provisioning.grafana.app:*",
 	"preferences.grafana.app:*", // user, team, and org preferences
 	"collections.grafana.app:*", // user stars
 	"plugins.grafana.app:*",
