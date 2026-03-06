1593bcb3bde9abde7d4134ed4c9e435e2f6e44cc
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2026-03-06 17:01:25 +0000

    API Clients: Remove generic type from customFetch

--- packages/grafana-api-clients/src/clients/fetch/dashboard/v0alpha1/baseApi.ts
+++ packages/grafana-api-clients/src/clients/fetch/dashboard/v0alpha1/baseApi.ts
@@ -4,9 +4,8 @@ const API_GROUP = 'dashboard.grafana.app';
 const API_VERSION = 'v0alpha1';
 const BASE_URL = getAPIBaseURL(API_GROUP, API_VERSION);
 
-export const customFetch = async <T>(url: string, options: RequestInit): Promise<T> => {
+export const customFetch = async (url: string, options: RequestInit) => {
   const response = await fetch(`${BASE_URL}${url}`, options);
   const data = await response.json();
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  return { status: response.status, data, headers: response.headers } as T;
+  return { status: response.status, data, headers: response.headers };
 };
