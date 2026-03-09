a06b76a85367113e71edd0fdd0146a56f6deb528
Hugo Häggmark <hugo.haggmark@gmail.com>
2026-03-09 11:11:20 +0100

    chore: fix i18n

--- packages/grafana-i18n/vitest.config.ts
+++ packages/grafana-i18n/vitest.config.ts
@@ -5,24 +5,9 @@ export default defineConfig({
   test: {
     globals: true,
     reporters: ['verbose'],
-    css: false, // disables CSS processing entirely in tests
-    environment: 'jsdom',
     include: ['src/**/*.test.{ts,tsx}'],
     exclude: ['node_modules/**'],
-    setupFiles: [
-      // 'public/test/setup-polyfills.ts', // MUST be first
-      // 'vitest-canvas-mock',
-      // 'public/test/vitest-setup.ts',
-      // 'public/test/setupVitestTests.ts',
-    ],
-    testTimeout: 30_000,
-    env: {
-      TZ: 'Pacific/Easter',
-    },
-  },
-  resolve: {
-    conditions: ['@grafana-app/source', 'import', 'module', 'default'],
-    alias: [],
+    setupFiles: ['vitest.setup.ts'],
   },
   plugins: [tsconfigPaths()],
 });
