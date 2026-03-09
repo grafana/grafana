f7f92f8b5746fd9b1acd97588438f40475bb4378
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2026-03-09 11:57:08 +0000

    split out schemas

--- packages/grafana-api-clients/src/clients/fetch/quotas/v0alpha1/client.gen.ts
+++ packages/grafana-api-clients/src/clients/fetch/quotas/v0alpha1/client.gen.ts
@@ -4,23 +4,9 @@
  * Grafana API Server
  * OpenAPI spec version: 1.35
  */
-import { customFetch } from './baseApi';
-export interface GetUsageResponse {
-  /** APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources */
-  apiVersion: string;
-  group: string;
-  /** Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds */
-  kind: string;
-  limit: number;
-  namespace: string;
-  resource: string;
-  usage: number;
-}
+import type { GetUsageParams, GetUsageResponse } from './client.schemas';
 
-export type GetUsageParams = {
-  group?: string;
-  resource?: string;
-};
+import { customFetch } from './baseApi';
 
 export type getUsageResponse200 = {
   data: GetUsageResponse;
