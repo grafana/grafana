35a41b9615f52835a97c8f97782ac5a7d7a28042
Tom Ratcliffe <tom.ratcliffe@grafana.com>
2026-03-09 13:19:36 +0000

    Add megaMenuControls feature flag
    Register experimental frontend-only feature flag to gate the new mega
    menu search/filter and expand/collapse controls.

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1216,6 +1216,11 @@ export interface FeatureToggles {
   */
   recentlyViewedDashboards?: boolean;
   /**
+  * Enables search filtering and expand/collapse controls in the mega menu
+  * @default false
+  */
+  megaMenuControls?: boolean;
+  /**
   * A/A test for recently viewed dashboards feature
   * @default false
   */
