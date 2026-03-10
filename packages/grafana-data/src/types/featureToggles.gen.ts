06acd96b3ed2e1e2ddc1f99850f270e0dc5c88fb
Kristina Demeshchik <kristina.demeshchik@gmail.com>
2026-03-10 12:26:57 -0400

    trailing space

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -995,7 +995,7 @@ export interface FeatureToggles {
   */
   alertingJiraIntegration?: boolean;
   /**
-  *
+  * 
   * @default true
   */
   alertingUseNewSimplifiedRoutingHashAlgorithm?: boolean;
