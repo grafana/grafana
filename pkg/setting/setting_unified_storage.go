499318ada7a332058b9e733966f5ccf93b4fc493
beejeebus <justin.randell@grafana.com>
2026-03-10 15:34:01 +0000

    This adds support for prometheus and grafana-testdata-datasource types.
    Migration works, but those types are hardcoded, left TODOs about listing
    datasource resources dynamically.
    
    Added writing secrets, but we don't yet clean up already migrated
    secrets if we rerun a migration.
    
    Added the following to my local `conf/custom.ini`:
    
        [unified_storage]
        disable_data_migrations = false
        [unified_storage.grafana-testdata-datasource.datasource.grafana.app]
        dualWriterMode = 0
        enableMigration = true
        [unified_storage.prometheus.datasource.grafana.app]
        dualWriterMode = 0
        enableMigration = true

--- pkg/setting/setting_unified_storage.go
+++ pkg/setting/setting_unified_storage.go
@@ -19,18 +19,22 @@ var knownUnifiedStorageKeys = map[string]string{
 }
 
 const (
-	PlaylistResource  = "playlists.playlist.grafana.app"
-	FolderResource    = "folders.folder.grafana.app"
-	DashboardResource = "dashboards.dashboard.grafana.app"
-	ShortURLResource  = "shorturls.shorturl.grafana.app"
+	PlaylistResource   = "playlists.playlist.grafana.app"
+	FolderResource     = "folders.folder.grafana.app"
+	DashboardResource  = "dashboards.dashboard.grafana.app"
+	ShortURLResource   = "shorturls.shorturl.grafana.app"
+	PrometheusResource = "prometheus.datasource.grafana.app"
+	GTDResource        = "grafana-testdata-datasource.datasource.grafana.app"
 )
 
 // MigratedUnifiedResources maps resources to a boolean indicating if migration is enabled by default
 var MigratedUnifiedResources = map[string]bool{
-	PlaylistResource:  true,  // Only Mode5!
-	FolderResource:    true,  // enabled by default
-	DashboardResource: true,  // enabled by default
-	ShortURLResource:  false, // Requires kubernetesShortURLs to be enabled by default
+	PlaylistResource:   true,  // Only Mode5!
+	FolderResource:     true,  // enabled by default
+	DashboardResource:  true,  // enabled by default
+	ShortURLResource:   false, // Requires kubernetesShortURLs to be enabled by default
+	PrometheusResource: false,
+	GTDResource:        false,
 }
 
 // applyUnifiedStorageEnvOverrides scans environment variables matching
@@ -95,6 +99,12 @@ func (cfg *Cfg) applyUnifiedStorageEnvOverrides() {
 	}
 }
 
+// read storage configs from ini file. They look like:
+// [unified_storage.<group>.<resource>]
+// <field> = <value>
+// e.g.
+// [unified_storage.playlists.playlist.grafana.app]
+// dualWriterMode = 2
 func (cfg *Cfg) setUnifiedStorageConfig() {
 	// Pre-create sections from GF_UNIFIED_STORAGE_* env vars so that
 	// resource sections can be configured purely via environment variables.
