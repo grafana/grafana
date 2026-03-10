365954afca630ba76a69df934c287ffbb2643f15
Dafydd <dafydd.thomas@grafana.com>
2026-03-10 17:18:13 +0000

    use openfeature to evaluate datasourcesRerouteLegacyCRUDAPIs

--- pkg/api/datasources_k8s.go
+++ pkg/api/datasources_k8s.go
@@ -22,6 +22,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/datasources"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/web"
+	"github.com/open-feature/go-sdk/openfeature"
 )
 
 // getK8sDataSourceByUIDHandler returns a handler that redirects GET /api/datasources/uid/:uid
@@ -29,23 +30,14 @@ import (
 //
 // Optional access control metadata is still fetched from the legacy accesscontrol service for now.
 func (hs *HTTPServer) getK8sDataSourceByUIDHandler() web.Handler {
-	//nolint:staticcheck // not yet migrated to OpenFeature
-	if !hs.Features.IsEnabledGlobally(featuremgmt.FlagDatasourcesRerouteLegacyCRUDAPIs) {
-		return routing.Wrap(hs.GetDataSourceByUID)
-	}
-
-	// datasourcesRerouteLegacyCRUDAPIs requires these flags to be enabled
-	//nolint:staticcheck // not yet migrated to OpenFeature
-	if !hs.Features.IsEnabledGlobally(featuremgmt.FlagQueryService) ||
-		!hs.Features.IsEnabledGlobally(featuremgmt.FlagQueryServiceWithConnections) {
-		return routing.Wrap(func(c *contextmodel.ReqContext) response.Response {
-			return response.Error(http.StatusInternalServerError,
-				"datasourcesRerouteLegacyCRUDAPIs requires queryService and queryServiceWithConnections feature flags",
-				nil)
-		})
-	}
-
 	return routing.Wrap(func(c *contextmodel.ReqContext) response.Response {
+
+		// use the legacy handler if the feature toggle is disabled
+		client := openfeature.NewDefaultClient()
+		if !client.Boolean(c.Req.Context(), featuremgmt.FlagDatasourcesRerouteLegacyCRUDAPIs, false, openfeature.TransactionContext(c.Req.Context())) {
+			return hs.GetDataSourceByUID(c)
+		}
+
 		start := time.Now()
 		defer func() {
 			metricutil.ObserveWithExemplar(c.Req.Context(), hs.dsConfigHandlerRequestsDuration.WithLabelValues("getK8sDataSourceByUIDHandler"), time.Since(start).Seconds())
