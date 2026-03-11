3b34e5ca918ec20007c08c69619180aad09ded29
Matias Chomicki <matyax@gmail.com>
2026-03-11 11:34:05 +0100

    Regenerate feature flag

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -2618,6 +2618,13 @@ var (
 			FrontendOnly: true,
 			Expression:   "false",
 		},
+		{
+			Name:        "inlineLogDetailsNoScrolls",
+			Description: "Enables an inline version of Log Details that creates no new scrolls",
+			Stage:       FeatureStageExperimental,
+			Owner:       grafanaObservabilityLogsSquad,
+			Expression:  "false",
+		},
 	}
 )
 
