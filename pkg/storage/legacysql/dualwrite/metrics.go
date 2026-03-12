3f9cd8f0b7574443a2b64b4e012a03f75ebc87b7
Mustafa Sencer Özcan <mustafasencer.ozcan@grafana.com>
2026-03-12 10:41:26 +0100

    fix: registration

--- pkg/storage/legacysql/dualwrite/metrics.go
+++ pkg/storage/legacysql/dualwrite/metrics.go
@@ -1,8 +1,18 @@
 package dualwrite
 
 import (
+	"sync"
+
+	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	registerMetricsOnce    sync.Once
+	backgroundErrorsMetric = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "dualwriter_background_errors_total",
+		Help: "Total number of failed background operations in unified storage",
+	}, []string{"resource", "method"})
 )
 
 type dualWriterMetrics struct {
@@ -10,10 +20,14 @@ type dualWriterMetrics struct {
 }
 
 func provideDualWriterMetrics(reg prometheus.Registerer) *dualWriterMetrics {
+	registerMetricsOnce.Do(func() {
+		if reg != nil {
+			if err := reg.Register(backgroundErrorsMetric); err != nil {
+				log.New("dualwrite").Warn("failed to register dualwriter metrics", "error", err)
+			}
+		}
+	})
 	return &dualWriterMetrics{
-		backgroundErrors: promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
-			Name: "dualwriter_background_errors_total",
-			Help: "Total number of failed background operations in unified storage",
-		}, []string{"resource", "method"}),
+		backgroundErrors: backgroundErrorsMetric,
 	}
 }
