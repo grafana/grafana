b477b563f2a7226e091fbe66e37fd1dc3b5ceaf3
Matias Chomicki <matyax@gmail.com>
2026-03-12 13:14:58 +0100

    Logs Panel: expose gammar option

--- packages/grafana-schema/src/raw/composable/logs/panelcfg/x/types.gen.ts
+++ packages/grafana-schema/src/raw/composable/logs/panelcfg/x/types.gen.ts
@@ -22,6 +22,7 @@ export interface Options {
   enableInfiniteScrolling?: boolean;
   enableLogDetails: boolean;
   fontSize?: ('default' | 'small');
+  gammar?: unknown;
   isFilterLabelActive?: unknown;
   logLineMenuCustomItems?: unknown;
   logRowMenuIconsAfter?: unknown;
