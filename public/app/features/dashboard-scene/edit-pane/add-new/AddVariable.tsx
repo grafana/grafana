c195d8781464bd4b9fca917638b71573ae9ffb9f
Haris Rozajac <haris.rozajac12@gmail.com>
2026-03-12 16:11:27 -0600

    move findSectionOwner to dashboardSceneGraph

--- public/app/features/dashboard-scene/edit-pane/add-new/AddVariable.tsx
+++ public/app/features/dashboard-scene/edit-pane/add-new/AddVariable.tsx
@@ -5,9 +5,8 @@ import { t } from '@grafana/i18n';
 import { SceneObject } from '@grafana/scenes';
 
 import { DashboardScene } from '../../scene/DashboardScene';
-import { RowItem } from '../../scene/layout-rows/RowItem';
-import { TabItem } from '../../scene/layout-tabs/TabItem';
 import { openAddSectionVariablePane, openAddVariablePane } from '../../settings/variables/VariableAddEditableElement';
+import { dashboardSceneGraph } from '../../utils/dashboardSceneGraph';
 import { DashboardInteractions } from '../../utils/interactions';
 
 import { AddButton } from './AddButton';
@@ -22,7 +21,7 @@ export function AddVariable({
   const sectionVariablesEnabled = useBooleanFlagValue('dashboardSectionVariables', false);
 
   const onAddVariableClick = useCallback(() => {
-    const sectionOwner = sectionVariablesEnabled ? findSectionOwner(selectedElement) : undefined;
+    const sectionOwner = sectionVariablesEnabled ? dashboardSceneGraph.findSectionOwner(selectedElement) : undefined;
     if (sectionOwner) {
       openAddSectionVariablePane(dashboardScene, sectionOwner);
     } else {
@@ -38,15 +37,4 @@ export function AddVariable({
       onClick={onAddVariableClick}
     />
   );
-}
-
-function findSectionOwner(element: SceneObject | undefined): RowItem | TabItem | undefined {
-  let current = element;
-  while (current) {
-    if (current instanceof RowItem || current instanceof TabItem) {
-      return current;
-    }
-    current = current.parent;
-  }
-  return undefined;
 }
