5a0a8aeccd12afea7d31f7a02c3f0d7af0de11ce
Ryan McKinley <ryantxu@gmail.com>
2026-03-13 14:39:52 +0300

    explicitly parse the namespace

--- pkg/registry/apis/query/query.go
+++ pkg/registry/apis/query/query.go
@@ -14,7 +14,10 @@ import (
 	"go.opentelemetry.io/otel/codes"
 	errorsK8s "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/apiserver/pkg/registry/rest"
+
+	"github.com/gorilla/mux"
 
 	"github.com/grafana/authlib/authn"
 	claims "github.com/grafana/authlib/types"
@@ -68,6 +71,14 @@ func (b *QueryAPIBuilder) QueryDatasources(w http.ResponseWriter, httpreq *http.
 	defer span.End()
 	traceId := span.SpanContext().TraceID()
 	connectLogger := b.log.New("traceId", traceId.String(), "rule_uid", httpreq.Header.Get("X-Rule-Uid"))
+
+	// Read the namespace from the path
+	ns := mux.Vars(httpreq)["namespace"]
+	if ns == "" {
+		errhttp.Write(ctx, fmt.Errorf("missing namespace"), w)
+		return
+	}
+	ctx = request.WithNamespace(ctx, ns)
 
 	responder := newResponderWrapper(ctx, w,
 		func(statusCode *int, obj runtime.Object) {
