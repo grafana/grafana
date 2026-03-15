4aff05d7558c59265d0e41d80aa9385ae268016d
Ryan McKinley <ryantxu@gmail.com>
2026-03-15 15:47:31 +0300

    us full datasource service

--- pkg/storage/unified/migrations/testcases/datasources.go
+++ pkg/storage/unified/migrations/testcases/datasources.go
@@ -3,26 +3,11 @@ package testcases
 import (
 	"testing"
 
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	authlib "github.com/grafana/authlib/types"
-	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/grafana/pkg/plugins/manager/pluginfakes"
-	"github.com/grafana/grafana/pkg/services/accesscontrol"
-	"github.com/grafana/grafana/pkg/services/accesscontrol/actest"
-	acmock "github.com/grafana/grafana/pkg/services/accesscontrol/mock"
 	"github.com/grafana/grafana/pkg/services/datasources"
-	dsservice "github.com/grafana/grafana/pkg/services/datasources/service"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/pluginconfig"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/plugincontext"
-	"github.com/grafana/grafana/pkg/services/pluginsintegration/pluginstore"
-	"github.com/grafana/grafana/pkg/services/quota/quotatest"
-	"github.com/grafana/grafana/pkg/services/secrets/fakes"
-	secretskvs "github.com/grafana/grafana/pkg/services/secrets/kvstore"
-	secretsmng "github.com/grafana/grafana/pkg/services/secrets/manager"
 	"github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 	"github.com/grafana/grafana/pkg/tests/apis"
 )
@@ -68,34 +53,29 @@ func (tc *datasourcesTestCase) AddLegacySQLMigrations(mg *migrator.Migrator) {}
 func (tc *datasourcesTestCase) Setup(t *testing.T, helper *apis.K8sTestHelper) bool {
 	t.Helper()
 
-	env := helper.GetEnv()
-
-	sqlStore := env.SQLStore
-	features := featuremgmt.WithFeatures()
-	secretsService := secretsmng.SetupTestService(t, fakes.NewFakeSecretsStore()) // TODO -- I don't think this can be fake!
-	secretsStore := secretskvs.NewSQLSecretsKVStore(sqlStore, secretsService, log.New("test.logger"))
-	mockPermission := acmock.NewMockedPermissionsService()
-	mockPermission.On("SetPermissions", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return([]accesscontrol.ResourcePermission{}, nil)
-	quotaService := quotatest.New(false, nil)
-	dsRetriever := dsservice.ProvideDataSourceRetriever(sqlStore, features)
-	dsService, err := dsservice.ProvideService(sqlStore, secretsService, secretsStore, env.Cfg, features,
-		actest.FakeAccessControl{}, mockPermission,
-		quotaService, &pluginstore.FakePluginStore{}, &pluginfakes.FakePluginClient{},
-		plugincontext.ProvideBaseService(env.Cfg, pluginconfig.NewFakePluginRequestConfigProvider()), dsRetriever)
-	require.NoError(t, err)
-
-	ds, err := dsService.AddDataSource(t.Context(), &datasources.AddDataSourceCommand{
-		Name: "test",
-		Type: "grafana-testdata-datasource",
-		URL:  "http://localhost",
+	ds := helper.CreateDS(&datasources.AddDataSourceCommand{
+		OrgID: helper.Org1.OrgID,
+		Name:  "test1",
+		Type:  "grafana-testdata-datasource",
+		URL:   "http://localhost",
 		SecureJsonData: map[string]string{
-			"secret": "xyz",
+			"test1": "abc",
 		},
 	})
-	require.NoError(t, err)
 	tc.testdata = append(tc.testdata, ds.UID)
 
-	return false // TODO, should be true! should be supported by mode0 (legacy API)
+	ds = helper.CreateDS(&datasources.AddDataSourceCommand{
+		OrgID: helper.Org1.OrgID,
+		Name:  "test2",
+		Type:  "grafana-testdata-datasource",
+		URL:   "http://localhost",
+		SecureJsonData: map[string]string{
+			"test2": "xyz",
+		},
+	})
+	tc.testdata = append(tc.testdata, ds.UID)
+
+	return false // TODO, should be true should be supported by mode0 (legacy API)
 }
 
 func (tc *datasourcesTestCase) Verify(t *testing.T, helper *apis.K8sTestHelper, shouldExist bool) {
