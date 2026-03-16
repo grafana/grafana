52e89dafc7be4cc01c36e9b1170cd0ba1c08f698
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2026-03-16 16:53:23 -0300

    remove duplicated call

--- public/app/features/dashboard-scene/sharing/ShareLinkTab.tsx
+++ public/app/features/dashboard-scene/sharing/ShareLinkTab.tsx
@@ -66,7 +66,6 @@ export class ShareLinkTab extends SceneObjectBase<ShareLinkTabState> implements 
     let shareUrl = createDashboardShareUrl(dashboard, opts, panel);
 
     if (useShortUrl) {
-      shareUrl = await createShortLink(shareUrl);
       try {
         shareUrl = await createShortLink(shareUrl);
       } catch {
