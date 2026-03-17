8738579e063d7cd303a48d54b0247aec8f048baa
Maicon Costa <maiconscosta@gmail.com>
2026-03-17 17:25:34 -0300

    initialize legacy storage convertor
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/registry/apis/folders/register.go
+++ pkg/registry/apis/folders/register.go
@@ -197,6 +197,7 @@ func (b *FolderAPIBuilder) storageForVersion(
 			service:        b.folderSvc,
 			namespacer:     b.namespacer,
 			tableConverter: folders.TableConverter(),
+			convertor:      opts.Scheme,
 		}
 		dw, err := opts.DualWriteBuilder(folders.GroupResource(), legacyStore, unified)
 		if err != nil {
