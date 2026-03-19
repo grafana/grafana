51684901e39498e264fc5ad99c0193b6962c275a
Leon Sorokin <leeoniya@gmail.com>
2026-03-19 17:30:30 -0500

    extract plugin to file, add yarn run build:statsFilt, add minDominance param

--- scripts/webpack/webpack.stats.js
+++ scripts/webpack/webpack.stats.js
@@ -1,52 +1,33 @@
 const { RsdoctorWebpackPlugin } = require('@rsdoctor/webpack-plugin');
-const fs = require('fs');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require('webpack-merge');
 
-function pathContains(node, regexp) {
-  if (regexp.test(node.label)) { return true; }
-
-  if (node.groups != null) {
-    for (let i = 0; i < node.groups.length; i++) {
-      if (pathContains(node.groups[i], regexp)) { return true; }
-    }
-  }
-
-  return false;
-}
-
+const { FilterStatsPlugin } = require('./plugins/FilterStatsPlugin');
 const prodConfig = require('./webpack.prod.js');
 
-const excludeRegExp = /@kusto|monaco-editor|public\/locales/;
-
-class FilterStats {
-  apply(compiler) {
-    compiler.hooks.done.tap('FilterStats', (stats) => {
-
-      let statsHTML = fs.readFileSync('public/build/bundle-stats.html', 'utf8');
-      let filteredStatsHTML = statsHTML.replace(/(window.chartData = )(\[.*?\])(;)/, (m, head, data, tail) => {
-        let nodes = JSON.parse(data);
-        let filtered = nodes.filter(node => !pathContains(node, excludeRegExp));
-        return head + JSON.stringify(filtered) + tail;
-      });
-
-      fs.writeFileSync('public/build/bundle-stats-filtered.html', filteredStatsHTML);
-    });
-  }
-}
-
 module.exports = (env = {}) => {
+  let opts = env.filtered ? {
+    analyzerMode: 'static',
+    reportFilename: 'bundle-stats.html',
+    openAnalyzer: false,
+    generateStatsFile: false,
+  } : {};
+
   const config = {
     plugins: [
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: 'bundle-stats.html',
-        openAnalyzer: false,
-        generateStatsFile: false,
-      }),
-      new FilterStats(),
+      new BundleAnalyzerPlugin(opts),
     ]
   };
+
+  // yarn build:stats --env filtered
+  if (env.filtered) {
+    config.plugins.push(
+      new FilterStatsPlugin({
+        exclude: /@kusto|monaco-editor|public\/locales/,
+        minDominance: 0.75,
+      })
+    );
+  }
 
   // yarn build:stats --env doctor
   if (env.doctor) {
