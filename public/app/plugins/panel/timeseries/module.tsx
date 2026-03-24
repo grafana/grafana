fad4f9dc4cc9448f74b23e6ba79da56d710895ad
Leon Sorokin <leeoniya@gmail.com>
2026-03-24 16:10:21 -0500

    VizLegend: Enable vizLegendSeriesLimit by default

--- public/app/plugins/panel/timeseries/module.tsx
+++ public/app/plugins/panel/timeseries/module.tsx
@@ -1,6 +1,5 @@
 import { PanelPlugin } from '@grafana/data';
 import { t } from '@grafana/i18n';
-import { config } from '@grafana/runtime';
 import { commonOptionsBuilder } from '@grafana/ui';
 import { optsWithHideZeros } from '@grafana/ui/internal';
 import { addAnnotationOptions } from 'app/features/panel/options/builder/annotations';
@@ -18,7 +17,7 @@ export const plugin = new PanelPlugin<Options, FieldConfig>(TimeSeriesPanel)
   .useFieldConfig(getGraphFieldConfig(defaultGraphConfig))
   .setPanelOptions((builder) => {
     commonOptionsBuilder.addTooltipOptions(builder, false, true, optsWithHideZeros);
-    commonOptionsBuilder.addLegendOptions(builder, true, true, config.featureToggles.vizLegendSeriesLimit);
+    commonOptionsBuilder.addLegendOptions(builder, true, true, true);
 
     const legendCategory = [t('timeseries.legend.category', 'Legend')];
 
