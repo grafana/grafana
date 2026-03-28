2e56b9859b095a8f5ff07533c2c97578b412bd58
Taras Malinovskii <bar.norilsk@gmail.com>
2026-03-28 09:33:37 +0700

    Zipkin: Fix incorrect JSON field casing in TraceLog struct
    The TraceLog struct was missing json tags, causing fields to serialize
    as "Timestamp" and "Fields" (uppercase) instead of "timestamp" and
    "fields" (lowercase) expected by the frontend TraceLog type from
    @grafana/data. This caused NaN/NaH display in trace view span events.
    
    Fixes #118073

--- pkg/tsdb/zipkin/handler_querydata.go
+++ pkg/tsdb/zipkin/handler_querydata.go
@@ -86,8 +86,8 @@ type TraceKeyValuePair struct {
 }
 
 type TraceLog struct {
-	Timestamp int64
-	Fields    []TraceKeyValuePair
+	Timestamp int64               `json:"timestamp"`
+	Fields    []TraceKeyValuePair `json:"fields"`
 }
 
 func transformResponse(zipkinSpans []model.SpanModel, refId string) *data.Frame {
