7f33c05e7f74eb58ea2c9bf94b2bd7422a89c766
Paul Marbach <paul.marbach@grafana.com>
2026-03-30 16:06:04 -0500

    Legend: Deleting vizLegendSeriesLimit toggle (#121483)

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1396,11 +1396,6 @@ export interface FeatureToggles {
   */
   externalVizSuggestions?: boolean;
   /**
-  * Limit the number of legend items by default, with an option to show all
-  * @default false
-  */
-  vizLegendSeriesLimit?: boolean;
-  /**
   * Enable field overrides for FieldType.nestedFrames fields (like in nested tables)
   * @default false
   */
