d70226638ef2f5eda0f46cfec439a66c7f181d8a
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2026-04-02 14:40:48 -0400

    do not emit and do not accept "none" as provenance status
    instead, on get, skip annotation altogether

--- pkg/services/ngalert/models/provisioning.go
+++ pkg/services/ngalert/models/provisioning.go
@@ -15,10 +15,6 @@ const (
 	ProvenanceFile Provenance = "file"
 	// ProvenanceConvertedPrometheus is used for objects converted from Prometheus definitions.
 	ProvenanceConvertedPrometheus Provenance = "converted_prometheus"
-
-	// k8sProvenanceNone is the textual representation of ProvenanceNone used in Kubernetes annotations.
-	// https://github.com/grafana/grafana/blob/main/apps/alerting/notifications/pkg/apis/alertingnotifications/v1beta1/ext.go#L5-L5
-	k8sProvenanceNone = "none"
 )
 
 var KnownProvenances = []Provenance{ProvenanceNone, ProvenanceAPI, ProvenanceFile, ProvenanceConvertedPrometheus}
@@ -31,14 +27,9 @@ type Provisionable interface {
 
 // ProvenanceFromString converts a string to a Provenance type, validating that it is one of the known provenances.
 func ProvenanceFromString(s string) (Provenance, error) {
-	if s == k8sProvenanceNone {
-		return ProvenanceNone, nil
-	}
-
 	p := Provenance(s)
 	if !slices.Contains(KnownProvenances, p) {
-		return Provenance(""), fmt.Errorf("invalid provenance status: %s", s)
+		return "", fmt.Errorf("invalid provenance status: %s", s)
 	}
-
 	return p, nil
 }
