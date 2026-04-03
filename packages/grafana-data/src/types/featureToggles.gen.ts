760745bf03e905e953187ee601ad79b1fc3103fa
Andrew Hackmann <ajhack93@gmail.com>
2026-04-03 10:00:04 -0500

    deprecate prometheus type mig feature flag

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1352,7 +1352,8 @@ export interface FeatureToggles {
   azureResourcePickerUpdates?: boolean;
   /**
   * Checks for deprecated Prometheus authentication methods (SigV4 and Azure), installs the relevant data source, and migrates the Prometheus data sources
-  * @default false
+  * @deprecated
+  * @default true
   */
   prometheusTypeMigration?: boolean;
   /**
