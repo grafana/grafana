7acc438ae3515a260727501d526ad3839200e7bb
Craig O'Donnell <craig.odonnell@grafana.com>
2026-04-08 15:07:22 -0400

    annotations: set createdBy in k8sRestAdapter

--- pkg/registry/apps/annotation/k8s_adapter.go
+++ pkg/registry/apps/annotation/k8s_adapter.go
@@ -16,6 +16,7 @@ import (
 
 	authtypes "github.com/grafana/authlib/types"
 	annotationV0 "github.com/grafana/grafana/apps/annotation/pkg/apis/annotation/v0alpha1"
+	"github.com/grafana/grafana/pkg/apimachinery/identity"
 	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	"github.com/grafana/grafana/pkg/util"
 )
@@ -209,6 +210,10 @@ func (s *k8sRESTAdapter) Create(ctx context.Context,
 
 	if annotation.Name == "" && annotation.GenerateName != "" {
 		annotation.Name = annotation.GenerateName + util.GenerateShortUID()
+	}
+
+	if user, err := identity.GetRequester(ctx); err == nil {
+		annotation.SetCreatedBy(user.GetUID())
 	}
 
 	return s.store.Create(ctx, annotation)
