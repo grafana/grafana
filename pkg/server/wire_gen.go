c58082d366e94ff5b5f4271f119d268ae94be6a9
Collin Fingar <collin.fingar@grafana.com>
2026-04-08 11:09:02 -0400

    Playlists: Added RBAC backend support - DO NOT MERGE UNTIL FRONTEND MERGES (#119975)
    * Playlists: Added RBAC backend support
    
    * Playlists: Fix integration test by using a fresh user to avoid stale permission cache
    
    * Playlists: Fix integration test by using managed role prefix for permission grant
    
    * Updates per feedback. Moved hotfix to behind toggle
    
    * Fix test file
    
    * Fix  linting
    
    * Updates per feedback

--- pkg/server/wire_gen.go
+++ pkg/server/wire_gen.go
@@ -803,7 +803,7 @@ func Initialize(ctx context.Context, cfg *setting.Cfg, opts Options, apiOpts api
 	scopedPluginDatasourceProvider := datasource.ProvideDefaultPluginConfigs(service14, cacheServiceImpl, plugincontextProvider, cfg)
 	v := builder.ProvideDefaultBuildHandlerChainFuncFromBuilders()
 	aggregatorRunner := aggregatorrunner.ProvideNoopAggregatorConfigurator()
-	appInstaller, err := playlist2.RegisterAppInstaller(featureToggles)
+	appInstaller, err := playlist2.RegisterAppInstaller(featureToggles, acimplService, accessControl)
 	if err != nil {
 		return nil, err
 	}
@@ -1502,7 +1502,7 @@ func InitializeForTest(ctx context.Context, t sqlutil.ITestDB, testingT interfac
 	scopedPluginDatasourceProvider := datasource.ProvideDefaultPluginConfigs(service14, cacheServiceImpl, plugincontextProvider, cfg)
 	v := builder.ProvideDefaultBuildHandlerChainFuncFromBuilders()
 	aggregatorRunner := aggregatorrunner.ProvideNoopAggregatorConfigurator()
-	appInstaller, err := playlist2.RegisterAppInstaller(featureToggles)
+	appInstaller, err := playlist2.RegisterAppInstaller(featureToggles, acimplService, accessControl)
 	if err != nil {
 		return nil, err
 	}
