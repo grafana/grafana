836fd7977d585c62ad41549b3f0b965dfeddfcbe
Georges Chaudy <chaudyg@gmail.com>
2026-04-10 20:05:06 +0200

    wire gen

--- pkg/server/wire_gen.go
+++ pkg/server/wire_gen.go
@@ -1796,7 +1796,16 @@ func InitializeForCLI(ctx context.Context, cfg *setting.Cfg) (Runner, error) {
 	actionSetService := resourcepermissions.NewActionSetService()
 	permissionRegistry := permreg.ProvidePermissionRegistry()
 	serverLockService := serverlock.ProvideService(sqlStore, tracingService)
-	acimplService, err := acimpl.ProvideService(cfg, sqlStore, routeRegisterImpl, cacheService, accessControl, userimplService, actionSetService, featureToggles, tracingService, permissionRegistry, serverLockService)
+	storeProvider := store2.ProvideDefaultStoreProvider()
+	server, err := authz.ProvideEmbeddedZanzanaServer(cfg, sqlStore, tracingService, featureToggles, registerer, eventualRestConfigProvider, storeProvider)
+	if err != nil {
+		return Runner{}, err
+	}
+	zanzanaClient, err := authz.ProvideZanzanaClient(cfg, sqlStore, server, featureToggles, registerer)
+	if err != nil {
+		return Runner{}, err
+	}
+	acimplService, err := acimpl.ProvideService(cfg, sqlStore, routeRegisterImpl, cacheService, accessControl, userimplService, actionSetService, featureToggles, tracingService, permissionRegistry, serverLockService, zanzanaClient)
 	if err != nil {
 		return Runner{}, err
 	}
