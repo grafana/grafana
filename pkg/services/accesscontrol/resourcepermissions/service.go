e1e80164811df6a0e5b713a63286e1204dff8012
Stephanie Hingtgen <stephanie.hingtgen@grafana.com>
2026-04-10 11:54:09 -0600

    [release-13.0.1] Roles: Fix setting ds scopes on global roles

--- pkg/services/accesscontrol/resourcepermissions/service.go
+++ pkg/services/accesscontrol/resourcepermissions/service.go
@@ -226,7 +226,7 @@ func (s *Service) SetUserPermission(ctx context.Context, orgID int64, user acces
 	}
 
 	var datasourceType string
-	if s.options.DatasourceTypeResolver != nil {
+	if s.options.DatasourceTypeResolver != nil && resourceID != "*" {
 		if t, err := s.options.DatasourceTypeResolver(ctx, orgID, resourceID); err == nil {
 			datasourceType = t
 		}
@@ -260,7 +260,7 @@ func (s *Service) SetTeamPermission(ctx context.Context, orgID, teamID int64, re
 	}
 
 	var datasourceType string
-	if s.options.DatasourceTypeResolver != nil {
+	if s.options.DatasourceTypeResolver != nil && resourceID != "*" {
 		if t, err := s.options.DatasourceTypeResolver(ctx, orgID, resourceID); err == nil {
 			datasourceType = t
 		}
@@ -294,7 +294,7 @@ func (s *Service) SetBuiltInRolePermission(ctx context.Context, orgID int64, bui
 	}
 
 	var datasourceType string
-	if s.options.DatasourceTypeResolver != nil {
+	if s.options.DatasourceTypeResolver != nil && resourceID != "*" {
 		if t, err := s.options.DatasourceTypeResolver(ctx, orgID, resourceID); err == nil {
 			datasourceType = t
 		}
@@ -322,7 +322,7 @@ func (s *Service) SetPermissions(
 	}
 
 	var datasourceType string
-	if s.options.DatasourceTypeResolver != nil {
+	if s.options.DatasourceTypeResolver != nil && resourceID != "*" {
 		if t, err := s.options.DatasourceTypeResolver(ctx, orgID, resourceID); err == nil {
 			datasourceType = t
 		}
