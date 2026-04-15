54a0c924316aa18b844e670072888180e17c64b0
multimo <tim.mulqueen@gmail.com>
2026-04-15 10:14:34 +0200

    swapping to use goff instead of legacy feature flag

--- packages/grafana-runtime/src/utils/DataSourceWithBackend.ts
+++ packages/grafana-runtime/src/utils/DataSourceWithBackend.ts
@@ -401,7 +401,10 @@ class DataSourceWithBackend<
    * Run the datasource healthcheck
    */
   async callHealthCheck(): Promise<HealthCheckResult> {
-    const useNewApi = config.featureToggles.datasourcesApiServerEnableHealthEndpointFrontend;
+     const useNewApi = getFeatureFlagClient().getBooleanValue(
+      config.featureToggles.datasourcesApiServerEnableHealthEndpointFrontend,
+      false
+    );
     const healthCheckURL = useNewApi
       ? `/apis/${this.type}.datasource.grafana.app/v0alpha1/namespaces/${config.namespace}/datasources/${this.uid}/health`
       : `/api/datasources/uid/${this.uid}/health`;
