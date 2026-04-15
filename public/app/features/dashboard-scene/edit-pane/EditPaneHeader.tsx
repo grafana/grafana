8b7fa60ac94389dd7dee24facb29a4b266c485a2
gulshank0 <gulshanbahadur002@gmail.com>
2026-04-16 00:27:42 +0530

    Dashboards: Fixed and resolved all the conflicts
    Signed-off-by: gulshank0 <gulshanbahadur002@gmail.com>

--- public/app/features/dashboard-scene/edit-pane/EditPaneHeader.tsx
+++ public/app/features/dashboard-scene/edit-pane/EditPaneHeader.tsx
@@ -20,7 +20,7 @@ export function EditPaneHeader({ element, editPane }: EditPaneHeaderProps) {
   const { hasCopiedPanel, hasCopiedRow, hasCopiedTab } = useClipboardState();
 
   // TODO this type check here is hacky and should be replaced with a more generic solid solution
-  const canPaste = element instanceof RowItem || element instanceof TabItem ? element : undefined;
+  const pasteTarget = element instanceof RowItem || element instanceof TabItem ? element : undefined;
   const onCopy = element.onCopy?.bind(element);
   const onDuplicate = element.onDuplicate?.bind(element);
   const onDelete = element.onDelete?.bind(element);
@@ -53,7 +53,7 @@ export function EditPaneHeader({ element, editPane }: EditPaneHeaderProps) {
                     onClick={onDuplicate}
                   />
                 ) : null}
-                {canPaste && hasCopiedPanel ? (
+                {pasteTarget && hasCopiedPanel ? (
                   <Menu.Item
                     icon="clipboard-alt"
                     label={t('dashboard.layout.common.paste', 'Paste')}
