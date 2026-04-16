5285d540b4a16d9dc4689e158647cb79360c95e9
Kristina Durivage <kristina.durivage@grafana.com>
2026-04-16 09:19:34 -0500

    fix test

--- pkg/services/correlations/correlationstest/fake.go
+++ pkg/services/correlations/correlationstest/fake.go
@@ -1,10 +1,13 @@
 package correlationstest
 
 import (
+	"context"
+
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/infra/db"
 	"github.com/grafana/grafana/pkg/services/accesscontrol/acimpl"
+	"github.com/grafana/grafana/pkg/services/apiserver"
 	"github.com/grafana/grafana/pkg/services/correlations"
 	"github.com/grafana/grafana/pkg/services/datasources"
 	fakeDatasources "github.com/grafana/grafana/pkg/services/datasources/fakes"
@@ -13,13 +16,15 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 )
 
-func New(db db.DB, cfg *setting.Cfg, bus bus.Bus) *correlations.CorrelationsService {
+func New(ctx context.Context, db db.DB, cfg *setting.Cfg, bus bus.Bus) *correlations.CorrelationsService {
 	ds := &fakeDatasources.FakeDataSourceService{
 		DataSources: []*datasources.DataSource{
 			{ID: 1, UID: "graphite", Type: datasources.DS_GRAPHITE},
 		},
 	}
 
-	correlationsSvc, _ := correlations.ProvideService(db, routing.NewRouteRegister(), ds, acimpl.ProvideAccessControl(featuremgmt.WithFeatures()), bus, quotatest.New(false, nil), cfg)
-	return correlationsSvc
+	clientGenerator := apiserver.ProvideClientGenerator(apiserver.ProvideEventualRestConfigProvider())
+
+	correlationsSvc, _ := correlations.ProvideService(ctx, db, routing.NewRouteRegister(), ds, acimpl.ProvideAccessControl(featuremgmt.WithFeatures()), bus, quotatest.New(false, nil), cfg, clientGenerator)
+	return correlationsSvc.(*correlations.CorrelationsService)
 }
