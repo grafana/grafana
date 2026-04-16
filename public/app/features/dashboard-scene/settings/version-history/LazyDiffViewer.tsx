64c46d5b4f76f23fd2d7951fde15490a92fd8e2b
Leon Sorokin <leeoniya@gmail.com>
2026-04-16 14:29:53 -0500

    used

--- public/app/features/dashboard-scene/settings/version-history/LazyDiffViewer.tsx
+++ public/app/features/dashboard-scene/settings/version-history/LazyDiffViewer.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { type ReactDiffViewerProps } from 'react-diff-viewer-continued';
 
+import { t } from '@grafana/i18n';
 import { LoadingPlaceholder } from '@grafana/ui';
 
 const LazyDiffViewerInternal = lazy(() => import('./DiffViewer').then((module) => ({ default: module.DiffViewer })));
