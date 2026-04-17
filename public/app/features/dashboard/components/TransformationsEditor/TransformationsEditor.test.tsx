be9958a62040d1bb7d55c437c3a1cb75e5ef664a
Paul Marbach <paul.marbach@grafana.com>
2026-04-17 16:15:46 -0400

    remove unused import

--- public/app/features/dashboard/components/TransformationsEditor/TransformationsEditor.test.tsx
+++ public/app/features/dashboard/components/TransformationsEditor/TransformationsEditor.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { type DataTransformerConfig, standardTransformersRegistry } from '@grafana/data';
