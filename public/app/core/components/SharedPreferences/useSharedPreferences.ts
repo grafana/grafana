6c04aa06385b75938ae4a26e9992f081b972309e
joshhunt <josh.hunt@grafana.com>
2026-04-17 13:01:43 +0100

    remove our todo comment

--- public/app/core/components/SharedPreferences/useSharedPreferences.ts
+++ public/app/core/components/SharedPreferences/useSharedPreferences.ts
@@ -9,37 +9,6 @@ import {
 import { type Props } from './utils';
 
 export const useSharedPreferences = (preferencesName: Props['resourceUri']) => {
-  /**
-   * TODO: change to use listPreferences with fieldSelector=metadata.name={name} instead of getPreferences
-   * and unwrap the array to return just the one preference object
-   *
-   * todo list:
-   * - [x] get user preferences
-   * - [x] get team preferences
-   * - [x] get org preferences
-   *
-   * - [x] update user preferences
-   * - [x] update team preferences
-   * - [x] update org preferences
-   *
-   * ~~~ context ~~~
-   *
-   * getPreferences: /apis/preferences.grafana.app/v1alpha1/namespaces/{namespace}/preferences/{name}
-   *   - gets the preferences for {name}
-   *   - returns 404 if doesn't exist (empty preferences)
-   *
-   *  ~~~ or ~~~
-   *
-   * listPreferences: /apis/preferences.grafana.app/v1alpha1/preferences?fieldSelector=metadata.name={name}
-   *  - returns an array of preferences that match the filter (metadata.name={name}) // {name} = user-123, team-456, namespace
-   *  - if preferences don't exist, the array is just empty
-   *
-   * listPreferences: /apis/preferences.grafana.app/v1alpha1/preferences
-   *  - returns an array of ALL preferences for the user - user, team, org
-   *  - if preferences don't exist, the array is just empty
-   *
-   */
-
   const { data, isLoading, isError } = useListPreferencesQuery({ fieldSelector: `metadata.name=${preferencesName}` });
   const [updatePreferences, { data: updateData, isLoading: isUpdating, isError: isUpdateError }] =
     useUpdatePreferencesMutation();
