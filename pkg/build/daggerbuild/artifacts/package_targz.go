e744406d8a6ed8d81f08be775a9803eaac7b4425
Mariell Hoversholm <mariell.hoversholm@grafana.com>
2026-04-17 13:37:43 +0200

    Docker: Bump Alpine-based images to 3.23.4 (#122930)

--- pkg/build/daggerbuild/artifacts/package_targz.go
+++ pkg/build/daggerbuild/artifacts/package_targz.go
@@ -220,7 +220,7 @@ func NewTarball(
 
 func (t *Tarball) Builder(ctx context.Context, opts *pipeline.ArtifactContainerOpts) (*dagger.Container, error) {
 	container := opts.Client.Container().
-		From("alpine:3.23.3").
+		From("alpine:3.23.4").
 		WithExec([]string{"apk", "add", "--update", "tar"})
 
 	return container, nil
