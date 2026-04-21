649d83e014c5ae67cfa650459dc95a72509404fa
Andrej Ocenas <mr.ocenas@gmail.com>
2026-04-21 11:23:31 +0200

    Update api comment

--- packages/grafana-api-clients/src/clients/rtkq/dashboard/v0alpha1/endpoints.gen.ts
+++ packages/grafana-api-clients/src/clients/rtkq/dashboard/v0alpha1/endpoints.gen.ts
@@ -1206,7 +1206,7 @@ export type DashboardHit = {
   name: string;
   /** Owner references set on the resource metadata in the format {Group}/{Kind}/{Name} */
   ownerReferences?: string[];
-  /** Dashboard or folder */
+  /** Dashboards or folders */
   resource: string;
   /** When using "real" search, this is the score */
   score?: number;
