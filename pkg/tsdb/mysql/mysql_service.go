286d915e7101f5db8444f6b069737e9e9d5f0af4
Andreas Christou <andreas.christou@grafana.com>
2026-04-21 10:26:56 +0100

    MySQL

--- pkg/tsdb/mysql/mysql_service.go
+++ pkg/tsdb/mysql/mysql_service.go
@@ -6,22 +6,36 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
-
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	schemas "github.com/grafana/schemads"
+
 	"github.com/grafana/grafana/pkg/tsdb/mysql/sqleng"
 )
 
 type Service struct {
-	im     instancemgmt.InstanceManager
-	logger log.Logger
+	im              instancemgmt.InstanceManager
+	logger          log.Logger
+	resourceHandler backend.CallResourceHandler
 }
 
 func ProvideService() *Service {
 	logger := backend.NewLoggerWith("logger", "tsdb.mysql")
-	return &Service{
+	s := &Service{
 		im:     datasource.NewInstanceManager(NewInstanceSettings(logger)),
 		logger: logger,
 	}
+
+	schemaHandler := newMySQLSchema(s, logger)
+	s.resourceHandler = schemas.NewSchemaDatasource(
+		schemaHandler,
+		schemaHandler,
+		schemaHandler,
+		schemaHandler,
+		schemaHandler,
+		nil,
+	)
+
+	return s
 }
 
 func (s *Service) getDataSourceHandler(ctx context.Context, pluginCtx backend.PluginContext) (*sqleng.DataSourceHandler, error) {
@@ -43,11 +57,15 @@ func (s *Service) CheckHealth(ctx context.Context, req *backend.CheckHealthReque
 	return dsHandler.CheckHealth(ctx, req)
 }
 
-// NOTE: do not put any business logic into this method. it's whole job is to forward the call "inside"
 func (s *Service) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	req = preprocessGrafanaSQLQueries(req)
 	dsHandler, err := s.getDataSourceHandler(ctx, req.PluginContext)
 	if err != nil {
 		return nil, err
 	}
 	return dsHandler.QueryData(ctx, req)
+}
+
+func (s *Service) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	return s.resourceHandler.CallResource(ctx, req, sender)
 }
