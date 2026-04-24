7fa699ecf69c67945c346d425c451c170ccfdb28
Kristina Durivage <kristina.durivage@grafana.com>
2026-04-23 20:53:23 -0500

    Clean up resource conversion signature, add backend correlations migration flag for future use

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -3054,6 +3054,14 @@ var (
 			Expression:   "false",
 			Generate:     Generate{LegacyGo: true, LegacyFrontend: true},
 		},
+		{
+			Name:        "grafana.correlationsSkipLegacy",
+			Description: "Route any calls to legacy correlations endpoints to call through to app platform",
+			Stage:       FeatureStageExperimental,
+			Generate:    Generate{React: false, Go: true, LegacyGo: true, LegacyFrontend: false},
+			Owner:       grafanaDataProSquad,
+			Expression:  "false",
+		},
 		// tl;dr: name your new flag `component.featureName`, specify Go and/or React generation targets, and use with OpenFeature!
 		//
 		// Adding a new feature flag? Be sure to check out the updated docs at /contribute/feature-toggles.md#Steps-to-adding-a-feature-toggle
