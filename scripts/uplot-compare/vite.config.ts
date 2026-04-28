32b466bac0e14f5284e5258402592e566fc1967d
Galen Kistler <109082771+gtk-grafana@users.noreply.github.com>
2026-04-28 06:44:14 -0500

    Dataviz: Canvas snapshot debug tool (#123077)
    * test(Candlestick): init visual canvas regression tests
    * feat: init uplot-compare canvas debugger tool

---
+++ scripts/uplot-compare/vite.config.ts
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+});
