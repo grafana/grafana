471201dcebbcc827c669b62b8590d767062918a5
Kristian Bremberg <114284895+KristianGrafana@users.noreply.github.com>
2026-04-28 09:50:37 +0200

    Updated plugin_proxy_test.go with new test cases

--- pkg/api/plugin_proxy_test.go
+++ pkg/api/plugin_proxy_test.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"net/http"
+	"net/url"
 	"testing"
 
+	contextmodel "github.com/grafana/grafana/pkg/services/contexthandler/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,6 +38,42 @@ func TestExtractPluginProxyPath(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run("Given raw path, should extract expected proxy path", func(t *testing.T) {
 			assert.Equal(t, tc.exp, extractProxyPath(tc.originalRawPath))
+		})
+	}
+}
+
+func TestGetProxyPath(t *testing.T) {
+	testCases := []struct {
+		rawPath string
+		exp     string
+	}{
+		{
+			"/api/plugin-proxy/test",
+			"/",
+		},
+		{
+			"/api/plugin-proxy/test/some/thing",
+			"/some/thing",
+		},
+		{
+			"/api/plugin-proxy/test2/api/services/afsd%2Fafsd/operations",
+			"/api/services/afsd%2Fafsd/operations",
+		},
+		{
+			"/api/plugin-proxy/cloudflare-app/with-token/api/v4/accounts",
+			"/with-token/api/v4/accounts",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run("Given raw path, getProxyPath should normalize with leading slash", func(t *testing.T) {
+			c := &contextmodel.ReqContext{
+				Context: &contextmodel.Context{
+					Req: &http.Request{
+						URL: &url.URL{RawPath: tc.rawPath},
+					},
+				},
+			}
+			assert.Equal(t, tc.exp, getProxyPath(c))
 		})
 	}
 }
