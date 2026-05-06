6053b28f9b1b07b415e3f5e29ba8e338a9c46e39
antonio-mello-ai <ajgcvm@gmail.com>
2026-05-05 20:29:43 -0600

    CloudWatch: Add pagination support for log groups selection #121391

--- pkg/tsdb/cloudwatch/models/resources/types.go
+++ pkg/tsdb/cloudwatch/models/resources/types.go
@@ -44,3 +44,8 @@ type LogGroupField struct {
 	Percent int64  `json:"percent"`
 	Name    string `json:"name"`
 }
+
+type LogGroupsResponse struct {
+	Results   []ResourceResponse[LogGroup] `json:"results"`
+	NextToken *string                      `json:"nextToken,omitempty"`
+}
