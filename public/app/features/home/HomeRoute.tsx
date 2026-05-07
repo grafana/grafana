9fa896268bc3093e68127d2dec6300c16dbff1be
Clarity-89 <homes89@ukr.net>
2026-05-07 18:33:43 +0300

    Home: Stub JSX-based homepage behind grafana.unifiedHomepage flag

---
+++ public/app/features/home/HomeRoute.tsx
@@ -0,0 +1,20 @@
+import { lazy, Suspense } from 'react';
+
+import { useFlagGrafanaUnifiedHomepage } from '@grafana/runtime/internal';
+import { LoadingPlaceholder } from '@grafana/ui';
+
+import { type DashboardPageProxyProps } from '../dashboard/containers/DashboardPageProxy';
+
+const DashboardPageProxy = lazy(
+  () => import(/* webpackChunkName: "DashboardPageProxy" */ '../dashboard/containers/DashboardPageProxy')
+);
+const HomePage = lazy(() => import(/* webpackChunkName: "HomePage" */ './HomePage'));
+
+export default function HomeRoute(props: DashboardPageProxyProps) {
+  const unifiedHomepageEnabled = useFlagGrafanaUnifiedHomepage();
+  return (
+    <Suspense fallback={<LoadingPlaceholder text="" />}>
+      {unifiedHomepageEnabled ? <HomePage /> : <DashboardPageProxy {...props} />}
+    </Suspense>
+  );
+}
