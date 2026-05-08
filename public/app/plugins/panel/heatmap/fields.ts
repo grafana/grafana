70bf43da958a3f74c6104d56751d39f754789ff9
Jesse David Peterson <jesdavpet@users.noreply.github.com>
2026-05-07 22:20:58 -0300

    fix(heatmap): fix bug when bucket size can't be inferred x not ascending

--- public/app/plugins/panel/heatmap/fields.ts
+++ public/app/plugins/panel/heatmap/fields.ts
@@ -338,6 +338,14 @@ const getDenseHeatmapData = (
   let xBinQty = dlen / yBinQty;
   let yBinIncr = ys[1] - ys[0];
   let xBinIncr = xs[yBinQty] - xs[0];
+  const hasValidXBucketSize = Number.isFinite(xBinIncr) && xBinIncr > 0;
+
+  if (!hasValidXBucketSize) {
+    return {
+      warning: 'Could not infer heatmap bucket size; x field is not sorted ascending',
+      heatmap: frame,
+    };
+  }
 
   let [minValue, maxValue] = boundedMinMax(
     valueField.values,
