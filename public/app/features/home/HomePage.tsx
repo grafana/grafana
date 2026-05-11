daada6325575caf3f0a18a6b4a89b274ee7f0aff
MattIPv4 <me@mattcowley.co.uk>
2026-05-11 17:35:48 +0100

    Add time-of-day greeting to homepage

--- public/app/features/home/HomePage.tsx
+++ public/app/features/home/HomePage.tsx
@@ -2,11 +2,19 @@ import { PageLayoutType } from '@grafana/data';
 import { t } from '@grafana/i18n';
 import { Page } from 'app/core/components/Page/Page';
 
+import useHomeGreeting from './useHomeGreeting';
+
 export default function HomePage() {
+  const greeting = useHomeGreeting();
+
   return (
     <Page
       navId="home"
-      pageNav={{ text: t('home.home-page.placeholder', 'Welcome to Grafana'), hideFromBreadcrumbs: true }}
+      pageNav={{
+        text: greeting,
+        subTitle: t('home.home-page.placeholder', 'Welcome to Grafana.'),
+        hideFromBreadcrumbs: true,
+      }}
       layout={PageLayoutType.Home}
     >
       <Page.Contents>
