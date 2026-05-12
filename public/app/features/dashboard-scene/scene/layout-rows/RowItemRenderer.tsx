23da9b927e2b035f7d08de92ed74b8549527d36e
Bogdan Matei <bogdan.matei@grafana.com>
2026-05-12 16:05:20 +0300

    Dashboards: Fix auto height in the auto grid layout placed in a row

--- public/app/features/dashboard-scene/scene/layout-rows/RowItemRenderer.tsx
+++ public/app/features/dashboard-scene/scene/layout-rows/RowItemRenderer.tsx
@@ -168,7 +168,7 @@ export function RowItemRenderer({ model }: SceneComponentProps<RowItem>) {
             </div>
           )}
           {!isCollapsed && (
-            <div>
+            <div className={styles.rowLayoutWrapper}>
               {sectionVariablesEnabled && rowVariablesSet && <SectionVariableControls variableSet={rowVariablesSet} />}
               <layout.Component model={layout} />
             </div>
@@ -303,6 +303,9 @@ function getStyles(theme: GrafanaTheme2) {
       display: 'flex',
       alignItems: 'center',
       paddingLeft: theme.spacing(1),
+    }),
+    rowLayoutWrapper: css({
+      height: '100%',
     }),
   };
 }
