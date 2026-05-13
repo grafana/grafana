b7f6eebf88eabec3533d63848a36a11d3b56b54f
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2026-05-13 12:01:55 -0300

    Pyroscope: Bump the Call Tree feature flag to public preview

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -2807,7 +2807,7 @@ var (
 		{
 			Name:        "flameGraphWithCallTree",
 			Description: "Enables the new Flame Graph UI containing the Call Tree view",
-			Stage:       FeatureStageExperimental,
+			Stage:       FeatureStagePublicPreview,
 			Owner:       grafanaObservabilityTracesAndProfilingSquad,
 			Generate:    Generate{LegacyFrontend: true, React: true}, // legacy frontend for old naming convention
 			Expression:  "false",
