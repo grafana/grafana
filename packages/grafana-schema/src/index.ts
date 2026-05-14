64fc16467a9536d24a99c6daff0348e3433be91c
Paul Marbach <paul.marbach@grafana.com>
2026-05-14 16:05:27 -0400

    full enum replace

--- packages/grafana-schema/src/index.ts
+++ packages/grafana-schema/src/index.ts
@@ -8,12 +8,15 @@ export * from './veneer/librarypanel.types';
 export * from './index.gen';
 
 /***** OVERRIDES *****/
+// TODO: Delete this section along with Hidden --------------------------------------
 // override LegendDisplayMode enum to deprecated `hidden`, since
 // it is not possible to deprecate individual enum values in Cue.
 import { LegendDisplayMode as BaseLegendDisplayMode } from './veneer/common.types';
 
-export const LegendDisplayMode = {
-  ...BaseLegendDisplayMode,
+export enum LegendDisplayMode {
+  List = BaseLegendDisplayMode.List,
+  Table = BaseLegendDisplayMode.Table,
   /** @deprecated use showLegend: false and omit displayMode */
-  Hidden: 'hidden',
-} as const;
+  Hidden = BaseLegendDisplayMode.Hidden,
+}
+// ENDTODO --------------------------------------------------------------------------
