972f2a533831e01220d3b8bb9d4c1bf17b739519
Hugo Häggmark <hugo.haggmark@gmail.com>
2026-05-18 08:11:19 +0200

    chore: remove some more

--- packages/grafana-data/src/transformations/matchers/predicates.ts
+++ packages/grafana-data/src/transformations/matchers/predicates.ts
@@ -1,89 +1,9 @@
-import { type Field, type DataFrame, FieldType } from '../../types/dataFrame';
-import { type FieldMatcherInfo, type FrameMatcherInfo } from '../../types/transformations';
-
-import { MatcherID } from './ids';
+import { type Field, FieldType } from '../../types/dataFrame';
 
 export const alwaysFieldMatcher = (field: Field) => {
   return true;
 };
 
-const alwaysFrameMatcher = (frame: DataFrame) => {
-  return true;
-};
-
-const neverFieldMatcher = (field: Field) => {
-  return false;
-};
-
 export const notTimeFieldMatcher = (field: Field) => {
   return field.type !== FieldType.time;
 };
-
-const neverFrameMatcher = (frame: DataFrame) => {
-  return false;
-};
-
-const alwaysFieldMatcherInfo: FieldMatcherInfo = {
-  id: MatcherID.alwaysMatch,
-  name: 'All Fields',
-  description: 'Always Match',
-
-  get: (_option) => {
-    return alwaysFieldMatcher;
-  },
-
-  getOptionsDisplayText: (_options) => {
-    return 'Always';
-  },
-};
-
-const alwaysFrameMatcherInfo: FrameMatcherInfo = {
-  id: MatcherID.alwaysMatch,
-  name: 'All Frames',
-  description: 'Always Match',
-
-  get: (_option) => {
-    return alwaysFrameMatcher;
-  },
-
-  getOptionsDisplayText: (_options) => {
-    return 'Always';
-  },
-};
-
-const neverFieldMatcherInfo: FieldMatcherInfo = {
-  id: MatcherID.neverMatch,
-  name: 'No Fields',
-  description: 'Never Match',
-  excludeFromPicker: true,
-
-  get: (_option) => {
-    return neverFieldMatcher;
-  },
-
-  getOptionsDisplayText: (_options) => {
-    return 'Never';
-  },
-};
-
-const neverFrameMatcherInfo: FrameMatcherInfo = {
-  id: MatcherID.neverMatch,
-  name: 'No Frames',
-  description: 'Never Match',
-
-  get: (_option) => {
-    return neverFrameMatcher;
-  },
-
-  getOptionsDisplayText: (_options) => {
-    return 'Never';
-  },
-};
-
-export function getFieldPredicateMatchers(): FieldMatcherInfo[] {
-  return [alwaysFieldMatcherInfo, neverFieldMatcherInfo];
-}
-
-export function getFramePredicateMatchers(): FrameMatcherInfo[] {
-  return [alwaysFrameMatcherInfo, neverFrameMatcherInfo];
-}
