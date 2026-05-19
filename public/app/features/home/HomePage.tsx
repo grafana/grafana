c86803dddb69a6a65569d8d7eae032cbd3bc46e8
MattIPv4 <me@mattcowley.co.uk>
2026-05-19 19:15:29 +0100

    Add extension points for extra homepage content

--- public/app/features/home/HomePage.tsx
+++ public/app/features/home/HomePage.tsx
@@ -1,8 +1,8 @@
-import { PageLayoutType } from '@grafana/data';
+import { PageLayoutType, PluginExtensionPoints } from '@grafana/data';
 import { GrafanaEdition } from '@grafana/data/internal';
 import { t } from '@grafana/i18n';
-import { config } from '@grafana/runtime';
-import { Box } from '@grafana/ui';
+import { config, renderLimitedComponents, usePluginComponents } from '@grafana/runtime';
+import { Box, Stack } from '@grafana/ui';
 import { Page } from 'app/core/components/Page/Page';
 import { isOnPrem } from 'app/core/utils/isOnPrem';
 
@@ -24,6 +24,14 @@ const getEdition = () => {
 export default function HomePage() {
   const greeting = useHomeGreeting();
 
+  const { components: preComponents } = usePluginComponents({
+    extensionPointId: PluginExtensionPoints.HomepagePre,
+  });
+
+  const { components: extraComponents } = usePluginComponents({
+    extensionPointId: PluginExtensionPoints.HomepageExtra,
+  });
+
   return (
     <Page
       navId="home"
@@ -35,9 +43,28 @@ export default function HomePage() {
       layout={PageLayoutType.Home}
     >
       <Page.Contents>
-        <Box backgroundColor="canvas" borderRadius="default" padding={4}>
-          <DashboardTabs />
-        </Box>
+        <Stack direction="column" gap={2}>
+          <Box backgroundColor="canvas" borderRadius="default" padding={4} direction="column" display="flex" gap={2}>
+            {renderLimitedComponents({
+              props: {},
+              components: preComponents,
+              pluginId: 'grafana-setupguide-app',
+            })}
+            <DashboardTabs />
+          </Box>
+
+          {renderLimitedComponents({
+            props: {},
+            components: extraComponents,
+            pluginId: 'grafana-setupguide-app',
+            wrapper: ({ children }) =>
+              children && (
+                <Box backgroundColor="canvas" borderRadius="default" padding={4}>
+                  {children}
+                </Box>
+              ),
+          })}
+        </Stack>
       </Page.Contents>
     </Page>
   );
