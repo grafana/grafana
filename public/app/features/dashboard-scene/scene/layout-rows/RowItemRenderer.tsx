8ac2a972ea0bc13f0c215dc930afbaf754393330
Bogdan Matei <bogdan.matei@grafana.com>
2026-05-19 12:03:31 +0300

    Fix

--- public/app/features/dashboard-scene/scene/layout-rows/RowItemRenderer.tsx
+++ public/app/features/dashboard-scene/scene/layout-rows/RowItemRenderer.tsx
@@ -305,7 +305,10 @@ function getStyles(theme: GrafanaTheme2) {
       paddingLeft: theme.spacing(1),
     }),
     rowLayoutWrapper: css({
-      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      flex: 1,
+      minHeight: 0,
     }),
   };
 }
