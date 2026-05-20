f338ce096dfe61d16b9345138635bf90b0d6f799
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2026-05-20 12:34:07 -0400

    Alerting: remove amSimple and dead Cortex/Loki YAML compat from PostableUserConfig and GettableUserConfig (#124953)
    * alerting: remove amSimple from PostableUserConfig and GettableUserConfig
    
    * alerting: remove dead Cortex/Loki YAML compat code from PostableUserConfig and GettableUserConfig

--- pkg/services/ngalert/api/tooling/definitions/alertmanager.go
+++ pkg/services/ngalert/api/tooling/definitions/alertmanager.go
@@ -712,7 +712,6 @@ type PostableUserConfig struct {
 	ExtraConfigs           []ExtraConfiguration      `yaml:"extra_config,omitempty" json:"extra_config,omitempty"`
 	ManagedRoutes          ManagedRoutes             `yaml:"managed_routes,omitempty" json:"managed_routes,omitempty"`                     // TODO: Move to ConfigRevision?
 	ManagedInhibitionRules ManagedInhibitionRules    `yaml:"managed_inhibition_rules,omitempty" json:"managed_inhibition_rules,omitempty"` // TODO: Move to ConfigRevision?
-	amSimple               map[string]interface{}    `yaml:"-" json:"-"`
 }
 
 // GetMergedTemplateDefinitions converts the given PostableUserConfig's TemplateFiles to a slice of Templates.
@@ -739,25 +738,11 @@ func (c *PostableUserConfig) UnmarshalJSON(b []byte) error {
 		return errors.New("only one extra config is supported")
 	}
 
-	type intermediate struct {
-		AlertmanagerConfig map[string]interface{} `yaml:"alertmanager_config" json:"alertmanager_config"`
-	}
-
-	var tmp intermediate
-	if err := json.Unmarshal(b, &tmp); err != nil {
-		return err
-	}
-	// store the map[string]interface{} variant for re-encoding later without redaction
-	c.amSimple = tmp.AlertmanagerConfig
-
 	return nil
 }
 
 func (c *PostableUserConfig) validate() error {
 	// Taken from https://github.com/prometheus/alertmanager/blob/master/config/config.go#L170-L191
-	// Check if we have a root route. We cannot check for it in the
-	// UnmarshalYAML method because it won't be called if the input is empty
-	// (e.g. the config file is empty or only contains whitespace).
 	if c.AlertmanagerConfig.Route == nil {
 		return fmt.Errorf("no route provided in config")
 	}
@@ -781,94 +766,12 @@ func (c *PostableUserConfig) GetGrafanaReceiverMap() map[string]*PostableGrafana
 	return UIDs
 }
 
-// MarshalYAML implements yaml.Marshaller.
-func (c *PostableUserConfig) MarshalYAML() (interface{}, error) {
-	yml, err := yaml.Marshal(c.amSimple)
-	if err != nil {
-		return nil, err
-	}
-	// cortex/loki actually pass the AM config as a string.
-	cortexPostableUserConfig := struct {
-		TemplateFiles      map[string]string `yaml:"template_files" json:"template_files"`
-		AlertmanagerConfig string            `yaml:"alertmanager_config" json:"alertmanager_config"`
-	}{
-		TemplateFiles:      c.TemplateFiles,
-		AlertmanagerConfig: string(yml),
-	}
-	return cortexPostableUserConfig, nil
-}
-
-func (c *PostableUserConfig) UnmarshalYAML(value *yaml.Node) error {
-	// cortex/loki actually pass the AM config as a string.
-	type cortexPostableUserConfig struct {
-		TemplateFiles      map[string]string `yaml:"template_files" json:"template_files"`
-		AlertmanagerConfig string            `yaml:"alertmanager_config" json:"alertmanager_config"`
-	}
-
-	var tmp cortexPostableUserConfig
-
-	if err := value.Decode(&tmp); err != nil {
-		return err
-	}
-
-	if err := yaml.Unmarshal([]byte(tmp.AlertmanagerConfig), &c.AlertmanagerConfig); err != nil {
-		return err
-	}
-
-	c.TemplateFiles = tmp.TemplateFiles
-	return nil
-}
-
 // swagger:model
 type GettableUserConfig struct {
 	TemplateFiles           map[string]string         `yaml:"template_files" json:"template_files"`
 	TemplateFileProvenances map[string]Provenance     `yaml:"template_file_provenances,omitempty" json:"template_file_provenances,omitempty"`
 	AlertmanagerConfig      GettableApiAlertingConfig `yaml:"alertmanager_config" json:"alertmanager_config"`
 	ExtraConfigs            []ExtraConfiguration      `yaml:"extra_config,omitempty" json:"extra_config,omitempty"`
-
-	// amSimple stores a map[string]interface of the decoded alertmanager config.
-	// This enables circumventing the underlying alertmanager secret type
-	// which redacts itself during encoding.
-	amSimple map[string]interface{} `yaml:"-" json:"-"`
-}
-
-func (c *GettableUserConfig) UnmarshalYAML(value *yaml.Node) error {
-	// cortex/loki actually pass the AM config as a string.
-	type cortexGettableUserConfig struct {
-		TemplateFiles      map[string]string `yaml:"template_files" json:"template_files"`
-		AlertmanagerConfig string            `yaml:"alertmanager_config" json:"alertmanager_config"`
-	}
-
-	var tmp cortexGettableUserConfig
-
-	if err := value.Decode(&tmp); err != nil {
-		return err
-	}
-
-	if err := yaml.Unmarshal([]byte(tmp.AlertmanagerConfig), &c.AlertmanagerConfig); err != nil {
-		return err
-	}
-
-	if err := yaml.Unmarshal([]byte(tmp.AlertmanagerConfig), &c.amSimple); err != nil {
-		return err
-	}
-
-	c.TemplateFiles = tmp.TemplateFiles
-	return nil
-}
-
-func (c *GettableUserConfig) MarshalJSON() ([]byte, error) {
-	type plain struct {
-		TemplateFiles      map[string]string      `yaml:"template_files" json:"template_files"`
-		AlertmanagerConfig map[string]interface{} `yaml:"alertmanager_config" json:"alertmanager_config"`
-	}
-
-	tmp := plain{
-		TemplateFiles:      c.TemplateFiles,
-		AlertmanagerConfig: c.amSimple,
-	}
-
-	return json.Marshal(tmp)
 }
 
 // GetGrafanaReceiverMap returns a map that associates UUIDs to grafana receivers
