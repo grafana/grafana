f86536c6ce6eb189e0b8881d8d576d3e0182282f
eledobleefe <laura.fernandez@grafana.com>
2026-05-21 14:03:56 +0200

    Add ReduceBootdataAPI toggle condition

--- public/app/index.ts
+++ public/app/index.ts
@@ -2,7 +2,7 @@
 // Since much of Grafana depends on it in includes side effects at import time,
 // we delay loading the rest of the app using import() until the boot data is ready.
 
-import type { PreferencesSpec } from '@grafana/api-clients/rtkq/preferences/v1alpha1';
+import { initPreferences } from './initPreferences';
 
 // Check if we are hosting files on cdn and set webpack public path
 if (window.public_cdn_path) {
@@ -20,49 +20,14 @@ if (window.nonce) {
 // This is an indication to the window.onLoad failure check that the app bundle has loaded.
 window.__grafana_app_bundle_loaded = true;
 
-async function fetchMergedPreferences(): Promise<{ spec: PreferencesSpec } | undefined> {
-  const namespace = window.grafanaBootData?.settings?.namespace;
-  const isSignedIn = window.grafanaBootData?.user?.isSignedIn;
-  const publicDashboardAccessToken = window.__grafanaPublicDashboardAccessToken;
-
-  if (!isSignedIn || publicDashboardAccessToken || !namespace) {
-    return undefined;
-  }
-  try {
-    const url = `/apis/preferences.grafana.app/v1alpha1/namespaces/${namespace}/preferences/merged`;
-    const resp = await fetch(url, { headers: { Accept: 'application/json' } });
-    if (!resp.ok) {
-      return undefined;
-    }
-    return await resp.json();
-  } catch (err) {
-    console.warn('Failed to fetch merged preferences', err);
-    return undefined;
-  }
-}
-
 async function bootstrapWindowData() {
-  const mergedPreferences = fetchMergedPreferences();
+  if (window.__grafanaReduceBootdataAPI) {
+    await initPreferences();
+  }
+
   // Wait for window.grafanaBootData is ready. The new index.html loads it from
   // an API call, but the old one just sets an immediately resolving promise.
   await window.__grafana_boot_data_promise;
-
-  const mergedPromise = await mergedPreferences;
-  if (mergedPromise) {
-    const { theme, language, weekStart, timezone } = mergedPromise.spec;
-    if (theme !== undefined) {
-      window.grafanaBootData.user.theme = theme;
-    }
-    if (language !== undefined) {
-      window.grafanaBootData.user.language = language;
-    }
-    if (weekStart !== undefined) {
-      window.grafanaBootData.user.weekStart = weekStart;
-    }
-    if (timezone !== undefined) {
-      window.grafanaBootData.user.timezone = timezone;
-    }
-  }
 
   // Use eager to ensure the app is included in the initial chunk and does not
   // require additional network requests to load.
