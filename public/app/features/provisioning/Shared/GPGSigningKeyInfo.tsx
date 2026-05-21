7f7e76d82758256dae32d522d026f887606f0ecd
Alejandro Malavet <alejandro.malavet@grafana.com>
2026-05-21 16:26:45 -0400

    add gitlab

--- public/app/features/provisioning/Shared/GPGSigningKeyInfo.tsx
+++ public/app/features/provisioning/Shared/GPGSigningKeyInfo.tsx
@@ -6,28 +6,37 @@ import { Stack, TextLink, useStyles2 } from '@grafana/ui';
 
 import { type InstructionAvailability } from '../Wizard/types';
 
-const GPG_GENERATE_KEY_URL =
+const GITHUB_GPG_GENERATE_URL =
   'https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key';
-const GPG_ADD_KEY_URL =
+const GITHUB_GPG_ADD_URL =
   'https://docs.github.com/en/authentication/managing-commit-signature-verification/adding-a-gpg-key-to-your-github-account';
+const GITLAB_GPG_GENERATE_URL = 'https://docs.gitlab.com/user/project/repository/signed_commits/gpg/';
+const GITLAB_GPG_ADD_URL =
+  'https://docs.gitlab.com/user/project/repository/signed_commits/gpg/#add-a-gpg-key-to-your-account';
 
 export function GPGSigningKeyInfo({ type }: { type: InstructionAvailability }) {
   const styles = useStyles2(getStyles);
-  if (type !== 'github') {
+  if (type !== 'github' && type !== 'gitlab') {
     return null;
   }
+  const generateUrl = type === 'github' ? GITHUB_GPG_GENERATE_URL : GITLAB_GPG_GENERATE_URL;
+  const addUrl = type === 'github' ? GITHUB_GPG_ADD_URL : GITLAB_GPG_ADD_URL;
   return (
     <div className={styles.container}>
       <Stack gap={0.5} wrap={'wrap'}>
         <Trans i18nKey="provisioning.gpg-signing-key-info.required">
           If you require verified commits,
         </Trans>
-        <TextLink external href={GPG_GENERATE_KEY_URL}>
+        <TextLink external href={generateUrl}>
           <Trans i18nKey="provisioning.gpg-signing-key-info.generate-link">generate a GPG key</Trans>
         </TextLink>
         <Trans i18nKey="provisioning.gpg-signing-key-info.then">then</Trans>
-        <TextLink external href={GPG_ADD_KEY_URL}>
-          <Trans i18nKey="provisioning.gpg-signing-key-info.add-link">add it to a GitHub account</Trans>
+        <TextLink external href={addUrl}>
+          {type === 'github' ? (
+            <Trans i18nKey="provisioning.gpg-signing-key-info.add-link-github">add it to a GitHub account</Trans>
+          ) : (
+            <Trans i18nKey="provisioning.gpg-signing-key-info.add-link-gitlab">add it to a GitLab account</Trans>
+          )}
         </TextLink>
         <Trans i18nKey="provisioning.gpg-signing-key-info.period">.</Trans>
       </Stack>
@@ -35,7 +44,7 @@ export function GPGSigningKeyInfo({ type }: { type: InstructionAvailability }) {
   );
 }
 
-function getStyles(theme: GrafanaTheme2) {
+function getStyles() {
   return {
     container: css({
       display: 'flex',
