f6495792073c17bf911186750b06d8032c358a81
Ashley Harrison <ashley.harrison@grafana.com>
2026-05-26 17:18:51 +0100

    run knip for grafana-alerting

--- knip.config.ts
+++ knip.config.ts
@@ -1,5 +1,14 @@
 import type { KnipConfig } from 'knip';
 
+const packageIgnoreDeps = [
+  // These are used by the base rollup config located outside of the packages
+  '@rollup/plugin-node-resolve',
+  'rollup-plugin-esbuild',
+  'rollup-plugin-node-externals',
+];
+
+const packageEntries = ['i18next.config.ts'];
+
 const config: KnipConfig = {
   exclude: [
     // we don't often use enums, but when we do we usually include members we'll utilise in the future
@@ -19,13 +28,15 @@ const config: KnipConfig = {
       webpack: false,
     },
     'packages/**': {
-      entry: ['i18next.config.ts'],
-      ignoreDependencies: [
-        // These are used by the base rollup config located outside of the packages
-        '@rollup/plugin-node-resolve',
-        'rollup-plugin-esbuild',
-        'rollup-plugin-node-externals',
-      ],
+      entry: packageEntries,
+      ignoreDependencies: packageIgnoreDeps,
+    },
+    // `grafana-alerting` has stories that are included in `grafana-ui`'s storybook
+    // this is a bad idea
+    // `grafana-alerting` should have its own storybook (like `grafana-flamegraph`), then we could remove this special block
+    'packages/grafana-alerting': {
+      entry: [...packageEntries, '**/*.story.tsx'],
+      ignoreDependencies: [...packageIgnoreDeps, '@storybook/react-webpack5', '@storybook/react'],
     },
   },
 };
