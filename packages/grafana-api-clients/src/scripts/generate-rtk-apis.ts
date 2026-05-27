99a953d0b338f82efaf80f66fca8c4bd4a0d53d4
Ashley Harrison <ashley.harrison@grafana.com>
2026-05-27 10:08:11 +0100

    manual fix

--- packages/grafana-api-clients/src/scripts/generate-rtk-apis.ts
+++ packages/grafana-api-clients/src/scripts/generate-rtk-apis.ts
@@ -131,6 +131,7 @@ const config: ConfigFile = {
     ...createAPIConfig('logsdrilldown', 'v1alpha1'),
     ...createAPIConfig('quotas', 'v0alpha1'),
     ...createAPIConfig('dashboard', 'v1beta1'),
+    ...createAPIConfig('dashboard', 'v2'),
     ...createAPIConfig('dashboard', 'v2alpha1'),
     ...createAPIConfig('dashboard', 'v2beta1'),
     // PLOP_INJECT_API_CLIENT - Used by the API client generator
