2cba4bab83fa7802848572e64a420a4aab21ad13
Hugo Häggmark <hugo.haggmark@gmail.com>
2026-05-27 11:06:01 +0200

    chore: testing levitate changes

--- packages/grafana-data/src/index.ts
+++ packages/grafana-data/src/index.ts
@@ -717,6 +717,7 @@ export {
   DataSourceApi,
 } from './types/datasource';
 export { CoreApp, type AppRootProps, type AppPluginMeta, AppPlugin, FeatureState } from './types/app';
+export { patchArrayVectorProrotypeMethods } from './types/vector';
 export {
   type DynamicConfigValue,
   type ConfigOverrideRule,
