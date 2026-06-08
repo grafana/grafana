c1cd2c390f8fa617d19097839ad73cf337d16fcb
Jenna <jennaleigh311@gmail.com>
2026-06-08 10:43:37 -0700

    remove whiespace

--- public/app/plugins/datasource/graphite/graphite_query.ts
+++ public/app/plugins/datasource/graphite/graphite_query.ts
@@ -86,7 +86,6 @@ export default class GraphiteQuery {
         const sanitizeQuery = (o: string): string => o.replace(/\s|'|"|,/g, '');
         const oldSanitized = sanitizeQuery(oldQuery);
         const newSanitized = sanitizeQuery(newQuery);
-
         if (oldSanitized && newSanitized && oldSanitized !== newSanitized && !parser.hasPipe) {
           throw new Error(
             `Failed to make a visual query builder query that is equivalent to the query.\nOriginal query: ${oldQuery}\nQuery builder query: ${newQuery}`
