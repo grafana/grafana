5326d8cf071ca631b272d640b6b7f2b7b1655d91
eledobleefe <laura.fernandez@grafana.com>
2026-06-09 17:33:56 +0200

    Move all themes to the same array

--- public/app/core/components/ThemeSelector/getSelectableThemes.ts
+++ public/app/core/components/ThemeSelector/getSelectableThemes.ts
@@ -1,13 +1,17 @@
 import { getBuiltInThemes } from '@grafana/data';
 
 export function getSelectableThemes() {
-  const allowedExtraThemes = ['deut_prot_dark', 'deut_prot_light', 'tritanopia_dark', 'tritanopia_light'];
-
-  allowedExtraThemes.push('desertbloom');
-  allowedExtraThemes.push('gildedgrove');
-  allowedExtraThemes.push('sapphiredusk');
-  allowedExtraThemes.push('tron');
-  allowedExtraThemes.push('gloom');
+  const allowedExtraThemes = [
+    'deut_prot_dark',
+    'deut_prot_light',
+    'tritanopia_dark',
+    'tritanopia_light',
+    'desertbloom',
+    'gildedgrove',
+    'sapphiredusk',
+    'tron',
+    'gloom',
+  ];
 
   return getBuiltInThemes(allowedExtraThemes);
 }
