d86b2d0996e8ad7ed4a339fcb5ded4fa547db279
Alejandro <61026152+amalavet@users.noreply.github.com>
2026-06-17 12:58:07 -0500

    Provisioning: move webhookDisabled into generic webhook config (#126619)
    * Provisioning: move webhookDisabled into generic webhook config
    
    * codegen
    
    * fix test and connection
    
    * copilot nits

--- apps/provisioning/pkg/apis/provisioning/v0alpha1/connections.go
+++ apps/provisioning/pkg/apis/provisioning/v0alpha1/connections.go
@@ -53,11 +53,6 @@ type GitHubConnectionConfig struct {
 
 	// GitHub App installation ID
 	InstallationID string `json:"installationID"`
-
-	// WebhookDisabled disables webhook integration for this connection. When true, the GitHub
-	// App does not require webhooks:write permission and Grafana will not register or receive
-	// webhook events. Use this when Grafana is not reachable from the public internet.
-	WebhookDisabled bool `json:"webhookDisabled,omitempty"`
 }
 
 func (GitHubConnectionConfig) OpenAPIModelName() string {
@@ -99,6 +94,17 @@ func (GitlabConnectionConfig) OpenAPIModelName() string {
 	return OpenAPIPrefix + "GitlabConnectionConfig"
 }
 
+type ConnectionWebhookConfig struct {
+	// Disabled disables webhook integration for this connection. When true, the GitHub
+	// App does not require webhooks:write permission and Grafana will not register or receive
+	// webhook events. Use this when Grafana is not reachable from the public internet.
+	Disabled bool `json:"disabled,omitempty"`
+}
+
+func (ConnectionWebhookConfig) OpenAPIModelName() string {
+	return OpenAPIPrefix + "ConnectionWebhookConfig"
+}
+
 // ConnectionType defines the types of Connection providers
 // +enum
 type ConnectionType string
@@ -137,6 +143,9 @@ type ConnectionSpec struct {
 	// Gitlab connection configuration
 	// Only applicable when provider is "gitlab"
 	Gitlab *GitlabConnectionConfig `json:"gitlab,omitempty"`
+
+	// Webhook configuration for this connection
+	Webhook *ConnectionWebhookConfig `json:"webhook,omitempty"`
 }
 
 func (ConnectionSpec) OpenAPIModelName() string {
