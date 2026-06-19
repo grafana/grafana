71e3c51c9e8c9f59cb2fbc8d7449e1238cc6d02d
Babak Vladislav <jayone.makevlad@gmail.com>
2026-06-19 14:45:01 +0500

    PanelChrome: Make panel error message copyable on click

--- packages/grafana-ui/src/components/PanelChrome/PanelStatus.tsx
+++ packages/grafana-ui/src/components/PanelChrome/PanelStatus.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { selectors } from '@grafana/e2e-selectors';
 
-import { Button } from '../Button/Button';
+import { ClipboardButton } from '../ClipboardButton/ClipboardButton';
 
 export interface Props {
   message?: string;
@@ -12,11 +12,12 @@ export interface Props {
 
 export function PanelStatus({ message, onClick, ariaLabel = 'status' }: Props) {
   return (
-    <Button
-      onClick={onClick}
+    <ClipboardButton
       variant={'destructive'}
       icon="exclamation-triangle"
       size="sm"
+      getText={() => message || ''}
+      onClick={onClick}
       tooltip={message || ''}
       aria-label={ariaLabel}
       data-testid={selectors.components.Panels.Panel.status('error')}
