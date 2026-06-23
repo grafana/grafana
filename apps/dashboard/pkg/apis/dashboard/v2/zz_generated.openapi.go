240171d2ff69da95fe480e748c2ec5dfa947e32a
Galen <galen.kistler@grafana.com>
2026-06-23 14:27:30 -0500

    chore: run scripts

--- apps/dashboard/pkg/apis/dashboard/v2/zz_generated.openapi.go
+++ apps/dashboard/pkg/apis/dashboard/v2/zz_generated.openapi.go
@@ -2173,6 +2173,13 @@ func schema_pkg_apis_dashboard_v2_DashboardFieldColor(ref common.ReferenceCallba
 							Format:      "",
 						},
 					},
+					"gradientColorTo": {
+						SchemaProps: spec.SchemaProps{
+							Description: "The end color for the gradient color mode (smallest value). Only used when mode is gradient.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
 					"seriesBy": {
 						SchemaProps: spec.SchemaProps{
 							Description: "Some visualizations need to know how to assign a series color from by value color schemes.",
