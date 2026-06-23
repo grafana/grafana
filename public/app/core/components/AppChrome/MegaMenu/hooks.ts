5e3f98a6d29ab93f42dc4f3fc61814bd4b3339db
Ashley Harrison <ashley.harrison@grafana.com>
2026-06-23 17:50:35 +0100

    use k8s preferences for bookmarks behind newPreferencesPage flag

--- public/app/core/components/AppChrome/MegaMenu/hooks.ts
+++ public/app/core/components/AppChrome/MegaMenu/hooks.ts
@@ -1,12 +1,24 @@
 import { useMemo } from 'react';
 
 import { useGetUserPreferencesQuery } from '@grafana/api-clients/internal/rtkq/legacy/preferences/user';
+import { useListPreferencesQuery } from '@grafana/api-clients/rtkq/preferences/v1alpha1';
+import { useFlagGrafanaNewPreferencesPage } from '@grafana/runtime/internal';
 
 import { contextSrv } from '../../../services/context_srv';
 
 export const usePinnedItems = () => {
-  const preferences = useGetUserPreferencesQuery(undefined, { skip: !contextSrv.user.isSignedIn });
-  const pinnedItems = useMemo(() => preferences.data?.navbar?.bookmarkUrls || [], [preferences]);
+  const newPrefsEnabled = useFlagGrafanaNewPreferencesPage();
+  const { data: preferencesK8s } = useListPreferencesQuery(
+    { fieldSelector: `metadata.name=user-${contextSrv.user.uid}` },
+    { skip: !contextSrv.user.isSignedIn || !newPrefsEnabled }
+  );
+  // TODO remove this when newPrefsEnabled is fully rolled out
+  const { data: preferencesLegacy } = useGetUserPreferencesQuery(undefined, {
+    skip: !contextSrv.user.isSignedIn || newPrefsEnabled,
+  });
+
+  const preferences = newPrefsEnabled ? preferencesK8s?.items[0].spec : preferencesLegacy;
+  const pinnedItems = useMemo(() => preferences?.navbar?.bookmarkUrls || [], [preferences]);
 
   return pinnedItems;
 };
