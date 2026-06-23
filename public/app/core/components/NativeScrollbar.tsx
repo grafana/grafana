4cfa0c57776250bff5e65945a4d5afca3d1c9b81
Bogdan Matei <bogdan.matei@grafana.com>
2026-06-23 16:41:52 +0300

    Fix knip

--- public/app/core/components/NativeScrollbar.tsx
+++ public/app/core/components/NativeScrollbar.tsx
@@ -23,7 +23,7 @@ export default function NativeScrollbar({ children, onSetScrollRef, divId }: Pro
   return children;
 }
 
-export class DivScrollElement implements ScrollRefElement {
+class DivScrollElement implements ScrollRefElement {
   public constructor(private element: HTMLElement) {}
   public get scrollTop() {
     return this.element.scrollTop;
