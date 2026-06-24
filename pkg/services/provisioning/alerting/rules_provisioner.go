ab5725ef7880fe8f277705bf47ba607002559bc6
Santiago Hernández <santiagohernandez.1997@gmail.com>
2026-06-24 12:33:59 +0200

    provisioning stuff

--- pkg/services/provisioning/alerting/rules_provisioner.go
+++ pkg/services/provisioning/alerting/rules_provisioner.go
@@ -44,6 +44,12 @@ type defaultAlertRuleProvisioner struct {
 func (prov *defaultAlertRuleProvisioner) Provision(ctx context.Context,
 	files []*AlertingFile) error {
 	for _, file := range files {
+		// When the file opts out of provenance, rules are created without it so they
+		// remain editable in the UI (they're still reset to file state on reprovision).
+		provenance := alert_models.ProvenanceFile
+		if file.DisableProvenance {
+			provenance = alert_models.ProvenanceNone
+		}
 		for _, group := range file.Groups {
 			ctx, u := identity.WithServiceIdentity(ctx, group.OrgID)
 
@@ -60,7 +66,7 @@ func (prov *defaultAlertRuleProvisioner) Provision(ctx context.Context,
 			for _, rule := range group.Rules {
 				rule.NamespaceUID = folderUID
 				rule.RuleGroup = group.Title
-				err = prov.provisionRule(ctx, u, rule)
+				err = prov.provisionRule(ctx, u, rule, provenance)
 				if err != nil {
 					return err
 				}
@@ -71,7 +77,7 @@ func (prov *defaultAlertRuleProvisioner) Provision(ctx context.Context,
 			}
 		}
 		for _, deleteRule := range file.DeleteRules {
-			err := prov.ruleService.DeleteAlertRule(ctx, provisionerUser(deleteRule.OrgID), deleteRule.UID, alert_models.ProvenanceFile)
+			err := prov.ruleService.DeleteAlertRule(ctx, provisionerUser(deleteRule.OrgID), deleteRule.UID, provenance)
 			if err != nil {
 				return err
 			}
@@ -83,7 +89,8 @@ func (prov *defaultAlertRuleProvisioner) Provision(ctx context.Context,
 func (prov *defaultAlertRuleProvisioner) provisionRule(
 	ctx context.Context,
 	user identity.Requester,
-	rule alert_models.AlertRule) error {
+	rule alert_models.AlertRule,
+	provenance alert_models.Provenance) error {
 	prov.logger.Debug("provisioning alert rule", "uid", rule.UID, "org", rule.OrgID)
 	_, _, err := prov.ruleService.GetAlertRule(ctx, user, rule.UID)
 	if err != nil && !errors.Is(err, alert_models.ErrAlertRuleNotFound) {
@@ -92,10 +99,10 @@ func (prov *defaultAlertRuleProvisioner) provisionRule(
 		prov.logger.Debug("creating rule", "uid", rule.UID, "org", rule.OrgID)
 		// a nil user is passed in as then the quota logic will only check for
 		// the organization quota since we don't have any user scope here.
-		_, err = prov.ruleService.CreateAlertRule(ctx, user, rule, alert_models.ProvenanceFile)
+		_, err = prov.ruleService.CreateAlertRule(ctx, user, rule, provenance)
 	} else {
 		prov.logger.Debug("updating rule", "uid", rule.UID, "org", rule.OrgID)
-		_, err = prov.ruleService.UpdateAlertRule(ctx, user, rule, alert_models.ProvenanceFile)
+		_, err = prov.ruleService.UpdateAlertRule(ctx, user, rule, provenance)
 	}
 	return err
 }
