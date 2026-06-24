e85aece9b8ffdda7be715eb91c4faaa83dc61f38
Paul Marbach <paul.marbach@grafana.com>
2026-06-24 19:00:13 -0400

    Update public/app/features/transformers/docs/content.ts
    Co-authored-by: Isabel Matwawana <76437239+imatwawana@users.noreply.github.com>

--- public/app/features/transformers/docs/content.ts
+++ public/app/features/transformers/docs/content.ts
@@ -711,7 +711,7 @@ Use this transformation to construct a matrix by specifying fields from your que
 
   #### Display options
 
-  > **Note:** Nested field overrides, which may be useful in conjunction with this transformation, are still in public preview. To try them out, enable \`nestedFramesFieldOverrides\`.
+> **Note:** Nested field overrides, which may be useful in conjunction with this transformation, are still in public preview. To try them out, enable \`nestedFramesFieldOverrides\`.
 
   | Option                            | Description                                                                                                                |
   | --------------------------------- | -------------------------------------------------------------------------------------------------------------------------- |
