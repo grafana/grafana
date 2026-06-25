7a3ce9583096e098cbd0656df5bee5a7738e1b04
Galen <galen.kistler@grafana.com>
2026-06-25 12:42:52 -0500

    chore: add flag back in?

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -628,6 +628,14 @@ var (
 			Generate:        Generate{LegacyGo: true, LegacyFrontend: true},
 		},
 		{
+			Name:        "annotationsClustering",
+			Description: "Enables annotation clustering and switches to refactored annotation code",
+			Stage:       FeatureStageGeneralAvailability,
+			Generate:    Generate{LegacyFrontend: true},
+			Owner:       grafanaDatavizSquad,
+			Expression:  "true",
+		},
+		{
 			Name:        "dashboardNewLayouts",
 			Description: "Enables new dashboard layouts",
 			Stage:       FeatureStageGeneralAvailability,
