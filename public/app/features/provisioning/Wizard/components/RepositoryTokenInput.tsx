9c9a55335030b0b30581b5266757a3639e55af66
Alejandro Malavet <alejandro.malavet@grafana.com>
2026-06-25 00:28:56 -0500

    fix UI

--- public/app/features/provisioning/Wizard/components/RepositoryTokenInput.tsx
+++ public/app/features/provisioning/Wizard/components/RepositoryTokenInput.tsx
@@ -75,6 +75,22 @@ export function RepositoryTokenInput() {
           />
         </Field>
       )}
+      {gitFields.emailConfig && (
+        <Field
+          noMargin
+          label={gitFields.emailConfig.label}
+          required={gitFields.emailConfig.required}
+          description={gitFields.emailConfig.description}
+          error={errors?.repository?.email?.message}
+          invalid={!!errors?.repository?.email?.message}
+        >
+          <Input
+            {...register('repository.email', gitFields.emailConfig.validation)}
+            id="email"
+            placeholder={gitFields.emailConfig.placeholder}
+          />
+        </Field>
+      )}
     </>
   );
 }
