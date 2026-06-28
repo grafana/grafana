d924bfcb6b9150c3c1c74af64e706104c72209b1
Shreya <shreya.joshi@neeyamoworks.com>
2026-06-28 15:18:38 +0200

    Deletion issue fixed

--- pkg/services/dashboards/service/dashboard_service.go
+++ pkg/services/dashboards/service/dashboard_service.go
@@ -1871,43 +1871,6 @@ func (dr *DashboardServiceImpl) saveDashboardThroughK8s(ctx context.Context, cmd
 	return dr.UnstructuredToLegacyDashboard(ctx, out, orgID)
 }
 
-func (dr *DashboardServiceImpl) deleteAllDashboardThroughK8s(ctx context.Context, orgID int64) error {
-	err := dr.k8sclient.DeleteCollection(ctx, orgID, v1.ListOptions{})
-	if err == nil {
-		return nil
-	}
-
-	// Unified storage does not support the deletecollection verb and returns 405.
-	// Fall back to listing all dashboards and deleting them one by one.
-	if !apierrors.IsMethodNotSupported(err) {
-		return err
-	}
-
-	dr.log.Debug("DeleteCollection not supported, falling back to individual deletes", "orgID", orgID)
-
-	for continueToken := ""; ; {
-		list, listErr := dr.k8sclient.List(ctx, orgID, v1.ListOptions{
-			Limit:    listAllDashboardsLimit,
-			Continue: continueToken,
-		})
-		if listErr != nil {
-			return listErr
-		}
-
-		for _, item := range list.Items {
-			if delErr := dr.k8sclient.Delete(ctx, item.GetName(), orgID, v1.DeleteOptions{}); delErr != nil && !apierrors.IsNotFound(delErr) {
-				return delErr
-			}
-		}
-
-		continueToken = list.GetContinue()
-		if continueToken == "" {
-			break
-		}
-	}
-
-	return nil
-}
 
 func (dr *DashboardServiceImpl) deleteDashboardThroughK8s(ctx context.Context, cmd *dashboards.DeleteDashboardCommand, validateProvisionedDashboard bool) error {
 	// get uid if not passed in
@@ -2461,6 +2424,44 @@ func (dr *DashboardServiceImpl) cleanupAfterDelete(ctx context.Context, orgID in
 
 		return dr.deleteResourcePermissions(sess, orgID, accesscontrol.GetResourceScopeUID("dashboards", uid))
 	})
+}
+
+func (dr *DashboardServiceImpl) deleteAllDashboardThroughK8s(ctx context.Context, orgID int64) error {
+	err := dr.k8sclient.DeleteCollection(ctx, orgID, v1.ListOptions{})
+	if err == nil {
+		return nil
+	}
+
+	// Unified storage does not support the deletecollection verb and returns 405.
+	// Fall back to listing all dashboards and deleting them one by one.
+	if !apierrors.IsMethodNotSupported(err) {
+		return err
+	}
+
+	dr.log.Debug("DeleteCollection not supported, falling back to individual deletes", "orgID", orgID)
+
+	for continueToken := ""; ; {
+		list, listErr := dr.k8sclient.List(ctx, orgID, v1.ListOptions{
+			Limit:    listAllDashboardsLimit,
+			Continue: continueToken,
+		})
+		if listErr != nil {
+			return listErr
+		}
+
+		for _, item := range list.Items {
+			if delErr := dr.k8sclient.Delete(ctx, item.GetName(), orgID, v1.DeleteOptions{}); delErr != nil && !apierrors.IsNotFound(delErr) {
+				return delErr
+			}
+		}
+
+		continueToken = list.GetContinue()
+		if continueToken == "" {
+			break
+		}
+	}
+
+	return nil
 }
 
 // FIXME: Remove me and handle nested deletions in the service with the DashboardPermissionsService
