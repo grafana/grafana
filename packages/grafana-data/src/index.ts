0a74c8c81a66ca61bd8ed553879bdc3c8a317b6c
Bogdan Matei <bogdan.matei@grafana.com>
2026-06-29 15:21:22 +0300

    Move to a new approach

--- packages/grafana-data/src/index.ts
+++ packages/grafana-data/src/index.ts
@@ -682,10 +682,6 @@ export {
   type PanelMenuItem,
   type AngularPanelMenuItem,
   type PanelPluginDataSupport,
-  type PanelNaturalHeightContext,
-  type PanelNaturalHeightSupplier,
-  PANEL_CHROME_HEIGHT_OVERHEAD,
-  clampPanelNaturalHeight,
   VizOrientation,
 } from './types/panel';
 export {
