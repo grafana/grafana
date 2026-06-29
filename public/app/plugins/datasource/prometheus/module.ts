7b3098da1ff5ffc39135b8a9c527d08461f6fb3f
Victor Marin <victor.marin@grafana.com>
2026-06-29 15:35:00 +0300

    remove duplicated error and notices inspectors, use standard

--- public/app/plugins/datasource/prometheus/module.ts
+++ public/app/plugins/datasource/prometheus/module.ts
@@ -1,11 +1,9 @@
 import { DataSourcePlugin } from '@grafana/data';
 import { PromCheatSheet, PrometheusDatasource, PromQueryEditorByApp } from '@grafana/prometheus';
 
-import { ErrorsAndNoticesInspector } from './ErrorsAndNoticesInspector';
 import { ConfigEditor } from './configuration/ConfigEditor';
 
 export const plugin = new DataSourcePlugin(PrometheusDatasource)
   .setQueryEditor(PromQueryEditorByApp)
   .setConfigEditor(ConfigEditor)
-  .setErrorsAndNoticesInspector(ErrorsAndNoticesInspector)
   .setQueryEditorHelp(PromCheatSheet);
