c5c553a319621367cd4a61d8884d9c8dd4394a31
Jessica Liu <jessica.liu@grafana.com>
2026-06-30 11:58:47 -0400

    fix test

--- pkg/tests/apis/annotations/annotations_test.go
+++ pkg/tests/apis/annotations/annotations_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -263,11 +264,12 @@ func TestIntegrationAnnotationGraphite(t *testing.T) {
 	path := fmt.Sprintf("/apis/annotation.grafana.app/v0alpha1/namespaces/%s/graphite", namespace)
 
 	// Post a Graphite-format event with array tags and a data field; the response is the created Annotation resource.
+	when := time.Now().Unix()
 	rsp := apis.DoRequest(helper, apis.RequestParams{
 		User:   helper.Org1.Admin,
 		Method: http.MethodPost,
 		Path:   path,
-		Body:   []byte(`{"what":"deployment","when":1700000000,"data":"v1.2.3","tags":["release","prod"]}`),
+		Body:   []byte(fmt.Sprintf(`{"what":"deployment","when":%d,"data":"v1.2.3","tags":["release","prod"]}`, when)),
 	}, &annotationV0.Annotation{})
 	require.Equal(t, http.StatusOK, rsp.Response.StatusCode)
 	require.NotEmpty(t, rsp.Result.GetName())
