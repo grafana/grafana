979a1e5236d2ec702fefe396f65ef482e0c0f483
MattIPv4 <me@mattcowley.co.uk>
2026-07-02 14:42:54 +0100

    Refactor useStoredBoolean to rely on store

--- public/app/core/hooks/useStoredBoolean.ts
+++ public/app/core/hooks/useStoredBoolean.ts
@@ -1,11 +1,13 @@
-import { useCallback } from 'react';
-import { useLocalStorage } from 'react-use';
+import { useCallback, useSyncExternalStore } from 'react';
+
+import { store } from '@grafana/data';
 
 export const useStoredBoolean = (key: string, initialValue: boolean): [boolean, (value: boolean) => void] => {
-  const [value, setValue] = useLocalStorage<boolean>(key, initialValue, {
-    raw: false,
-    serializer: (value: boolean) => value.toString(),
-    deserializer: (value: string) => value === 'true',
-  });
-  return [value ?? false, useCallback((nextValue: boolean) => setValue(nextValue), [setValue])];
+  const subscribe = useCallback((onStoreChange: () => void) => store.subscribe(key, onStoreChange), [key]);
+  const getSnapshot = useCallback(() => store.getBool(key, initialValue), [key, initialValue]);
+
+  const value = useSyncExternalStore(subscribe, getSnapshot, () => initialValue);
+  const setValue = useCallback((nextValue: boolean) => store.set(key, nextValue), [key]);
+
+  return [value, setValue];
 };
