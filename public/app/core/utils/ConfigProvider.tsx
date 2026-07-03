1a8d84e0b452c9521f37b0284e2325eb6b41e464
Ashley Harrison <ashley.harrison@grafana.com>
2026-07-03 16:36:54 +0100

    remap dark/light themes when visualDesignRefresh flag is enabled

--- public/app/core/utils/ConfigProvider.tsx
+++ public/app/core/utils/ConfigProvider.tsx
@@ -1,18 +1,34 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import * as React from 'react';
 import { SkeletonTheme } from 'react-loading-skeleton';
 
-import { type GrafanaTheme2, ThemeContext } from '@grafana/data';
+import { getThemeById, type GrafanaTheme2, ThemeContext } from '@grafana/data';
 import { ThemeChangedEvent, config } from '@grafana/runtime';
 import { useFlagGrafanaVisualDesignRefresh } from '@grafana/runtime/internal';
 
 import { appEvents } from '../app_events';
-
 import 'react-loading-skeleton/dist/skeleton.css';
 
+// temporarily remap dark/light to the visual refresh themes if the flag is enabled
+// when delivering the visual refresh, remove this remapping and use the updated dark/light themes directly
+function remapThemeForVisualDesignRefresh(theme: GrafanaTheme2): GrafanaTheme2 {
+  if (theme.name === 'Dark') {
+    return getThemeById('visual_refresh_dark');
+  } else if (theme.name === 'Light') {
+    return getThemeById('visual_refresh_light');
+  }
+  return theme;
+}
+
 export const ThemeProvider = ({ children, value }: { children: React.ReactNode; value: GrafanaTheme2 }) => {
-  const [theme, setTheme] = useState(value);
   const visualRefreshEnabled = useFlagGrafanaVisualDesignRefresh();
+
+  const maybeRemapTheme = useCallback(
+    (theme: GrafanaTheme2) => (visualRefreshEnabled ? remapThemeForVisualDesignRefresh(theme) : theme),
+    [visualRefreshEnabled]
+  );
+
+  const [theme, setTheme] = useState(() => maybeRemapTheme(value));
 
   const themeWithFlags = useMemo(
     () => ({
@@ -27,16 +43,17 @@ export const ThemeProvider = ({ children, value }: { children: React.ReactNode; 
 
   useEffect(() => {
     const sub = appEvents.subscribe(ThemeChangedEvent, (event) => {
-      config.theme2 = event.payload;
-      setTheme(event.payload);
+      const newTheme = maybeRemapTheme(event.payload);
+      config.theme2 = newTheme;
+      setTheme(newTheme);
     });
 
     return () => sub.unsubscribe();
-  }, []);
+  }, [maybeRemapTheme]);
 
   useEffect(() => {
-    setTheme(value);
-  }, [value]);
+    setTheme(maybeRemapTheme(value));
+  }, [value, maybeRemapTheme]);
 
   return (
     <ThemeContext.Provider value={themeWithFlags}>
