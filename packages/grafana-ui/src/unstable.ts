436d7a6d8e8b728444caf72bd16d0635b6305730
Nic Westvold <nic.westvold@grafana.com>
2026-07-07 16:35:16 -0400

    feat(grafana-ui): add getQueryFieldConfig CodeMirror helper

--- packages/grafana-ui/src/unstable.ts
+++ packages/grafana-ui/src/unstable.ts
@@ -29,5 +29,7 @@ export type {
   SignatureInformation,
   SignatureParameter,
 } from './components/CodeMirror/types';
+export { getQueryFieldConfig } from './components/CodeMirror/queryField';
+export type { QueryFieldConfig, QueryFieldConfigOptions } from './components/CodeMirror/queryField';
 export type { SignatureHelpOptions } from './components/CodeMirror/signatureHelp';
 export { TableNG } from './components/Table/TableNG/TableNG';
