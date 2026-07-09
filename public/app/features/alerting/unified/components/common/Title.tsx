1787a25243f3cdfe2736148fd9ee16c2635c0742
eledobleefe <laura.fernandez@grafana.com>
2026-07-09 13:41:56 +0200

    Modify Title

--- public/app/features/alerting/unified/components/common/Title.tsx
+++ public/app/features/alerting/unified/components/common/Title.tsx
@@ -1,10 +1,12 @@
 import { t } from '@grafana/i18n';
+import { useFlagGrafanaVisualDesignRefresh } from '@grafana/runtime/internal';
 import { LinkButton, Stack, Text } from '@grafana/ui';
 
 import { useReturnTo } from '../../hooks/useReturnTo';
 
 export const Title = ({ name, returnToFallback }: { name: string; returnToFallback: string }) => {
   const { returnTo } = useReturnTo(returnToFallback);
+  const visualRefreshEnabled = useFlagGrafanaVisualDesignRefresh();
 
   return (
     <Stack direction="row" gap={1} minWidth={0} alignItems="center">
@@ -14,7 +16,12 @@ export const Title = ({ name, returnToFallback }: { name: string; returnToFallba
         icon="angle-left"
         href={returnTo}
       />
-      <Text element="h1" truncate>
+      <Text
+        element="h1"
+        variant={visualRefreshEnabled ? 'h4' : 'h1'}
+        weight={visualRefreshEnabled ? 'bold' : 'regular'}
+        truncate
+      >
         {name}
       </Text>
     </Stack>
