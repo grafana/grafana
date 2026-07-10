ffa7b86b5b1ca6e4267829ce0185478014f6cb45
rodrigopk <rodrigo.barros@grafana.com>
2026-07-10 11:16:54 -0400

    Address review comments
    - Removed redundant tests
    - Use helper function instead of constants when checking for user defined tree name

--- public/app/features/alerting/unified/utils/k8s/constants.test.ts
+++ public/app/features/alerting/unified/utils/k8s/constants.test.ts
@@ -1,10 +1,7 @@
-import { USER_DEFINED_TREE_NAME } from '@grafana/alerting';
-
 import { ROOT_ROUTE_NAME } from './constants';
 
 describe('ROOT_ROUTE_NAME', () => {
-  it('is the single-sourced user-defined send name', () => {
+  it('is the user-defined send name', () => {
     expect(ROOT_ROUTE_NAME).toBe('user-defined');
-    expect(ROOT_ROUTE_NAME).toBe(USER_DEFINED_TREE_NAME);
   });
 });
