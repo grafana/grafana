4451f95024417227ecbc24690a34cedd60db3cbc
Ashley Harrison <ashley.harrison@grafana.com>
2026-07-10 17:44:31 +0100

    test updated plugin-e2e

--- e2e-playwright/test-plugins/grafana-test-datasource/tests/translations/french.spec.ts
+++ e2e-playwright/test-plugins/grafana-test-datasource/tests/translations/french.spec.ts
@@ -2,18 +2,7 @@ import { FRENCH_FRANCE } from '@grafana/i18n';
 import { expect, test } from '@grafana/plugin-e2e';
 import pluginJson from '../../plugin.json';
 
-// TODO remove this once newPreferencesPage flag is removed
 test.use({ userPreferences: { language: FRENCH_FRANCE } });
-
-// mock out the k8s preferences (needed when newPreferencesPage is enabled)
-test.beforeEach(async ({ page }) => {
-  await page.route('**/apis/preferences.grafana.app/*/namespaces/*/preferences/merged', async (route) => {
-    const response = await route.fetch();
-    const json = await response.json();
-    json.spec = { ...json.spec, language: FRENCH_FRANCE };
-    await route.fulfill({ response, json });
-  });
-});
 
 test('should display default translation (en-US)', async ({ createDataSourceConfigPage }) => {
   const configPage = await createDataSourceConfigPage({ type: pluginJson.id });
