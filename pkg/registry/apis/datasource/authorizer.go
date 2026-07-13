eed29e251526f9ee5ae9ab7d674a8d123da3776b
multimo <tim.mulqueen@gmail.com>
2026-07-13 14:57:08 +0200

    add metrics for  authz in datasource apiservers

--- pkg/registry/apis/datasource/authorizer.go
+++ pkg/registry/apis/datasource/authorizer.go
@@ -5,6 +5,7 @@ import (
 
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 
+	authn "github.com/grafana/authlib/authn"
 	authlib "github.com/grafana/authlib/types"
 	"github.com/grafana/grafana/pkg/apimachinery/identity"
 	"github.com/grafana/grafana/pkg/apimachinery/utils"
@@ -17,8 +18,23 @@ func (b *DataSourceAPIBuilder) GetAuthorizer() authorizer.Authorizer {
 			if !attr.IsResourceRequest() {
 				return authorizer.DecisionNoOpinion, "", nil
 			}
+
+			sub := attr.GetSubresource()
+
+			var svcIdentity []string
+			if authInfo, ok := authlib.AuthInfoFrom(ctx); ok {
+				svcIdentity = authInfo.GetExtra()[authn.ServiceIdentityKey]
+			}
+
+			// Observe svc_identity state for future enforcement.
+			caller := "empty"
+			if len(svcIdentity) > 0 {
+				caller = svcIdentity[0]
+			}
+
 			user, err := identity.GetRequester(ctx)
 			if err != nil {
+				recordAuthzDecision(sub, caller, "deny", "no_user")
 				return authorizer.DecisionDeny, "valid user is required", err
 			}
 
@@ -30,22 +46,26 @@ func (b *DataSourceAPIBuilder) GetAuthorizer() authorizer.Authorizer {
 				Verb:      attr.GetVerb(),
 			}
 
-			// Must have query permission to access any subresource
-			if attr.GetSubresource() != "" {
+			if sub != "" {
 				req.Verb = utils.VerbCreate
 				req.Subresource = "query"
 			}
 
 			rsp, err := b.accessClient.Check(ctx, user, req, "")
 			if err != nil {
+				recordAuthzDecision(sub, caller, "deny", "error")
 				return authorizer.DecisionDeny, "failed to check permissions", err
 			}
 			if rsp.Allowed {
+				recordAuthzDecision(sub, caller, "allow", "")
 				return authorizer.DecisionAllow, "", nil
 			}
 			if req.Subresource != "" {
+				recordAuthzDecision(sub, caller, "deny", "missing_permissions")
 				return authorizer.DecisionDeny, "missing `query` subresource permission", nil
 			}
+
+			recordAuthzDecision(sub, caller, "deny", "access_denied")
 			return authorizer.DecisionDeny, "access denied", nil
 		})
 }
