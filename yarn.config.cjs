bd0582c4394ec92929dacb264083206544e75a88
Jack Westbrook <jack.westbrook@gmail.com>
2026-07-15 08:23:54 +0200

    chore(codeowners): add yarn.config.cjs

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   async constraints({ Yarn }) {
     const root = Yarn.workspace({ cwd: '.' });
     for (const workspace of Yarn.workspaces()) {
+      // Ensure all workspaces are using the same package manager version otherwise builds can fail.
       if (workspace.manifest.packageManager) {
         workspace.set('packageManager', root.manifest.packageManager);
       }
