60ffa40e5c4789bcbc14429d42416eb7ee1005e0
idastambuk <ida.stambuk@grafana.com>
2026-07-20 22:15:51 +0200

    Remove import

--- public/app/core/components/QueryOperationRow/QueryOperationRowHeader.tsx
+++ public/app/core/components/QueryOperationRow/QueryOperationRowHeader.tsx
@@ -1,6 +1,5 @@
 import { css, cx } from '@emotion/css';
 import { type DraggableProvided } from '@hello-pangea/dnd';
-import { type MouseEventHandler } from 'react';
 import * as React from 'react';
 
 import { type GrafanaTheme2 } from '@grafana/data';
