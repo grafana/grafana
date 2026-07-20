2b7fe07d9c59be39378a017bc418fe88e2734c24
AkiraTamai <akira.tamai.main@gmail.com>
2026-07-20 21:05:17 +0900

    fix(utils): guard getFactors against non-positive and non-integer input

--- public/app/core/utils/factors.ts
+++ public/app/core/utils/factors.ts
@@ -1,5 +1,9 @@
 // Returns the factors of a number
 // Example getFactors(12) -> [1, 2, 3, 4, 6, 12]
 export default function getFactors(num: number): number[] {
+  if (!Number.isInteger(num) || num < 1) {
+    return [];
+  }
+
   return Array.from(new Array(num + 1), (_, i) => i).filter((i) => num % i === 0);
 }
