5493beff2c6ce8a2678cef660180030d1a7c9b2e
Tanmaykaturi <159781384+Tanmaykaturi@users.noreply.github.com>
2026-07-21 10:33:08 +0530

    fix: math-random-used-5 (#70)
    Co-authored-by: appuser <appuser@ai-guardian-remediation-9f4b65bdf-pkspv>

--- apps/alerting/notifications/pkg/apis/alertingnotifications/v1beta1/fakes/timeinterval_gen.go
+++ apps/alerting/notifications/pkg/apis/alertingnotifications/v1beta1/fakes/timeinterval_gen.go
@@ -1,14 +1,46 @@
 package fakes
 
 import (
+	"crypto/rand"
+	"encoding/binary"
 	"fmt"
-	"math/rand"
 	"slices"
 	"strings"
 	"time"
 
 	"github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/alertingnotifications/v1beta1"
 )
+
+// randInt returns a cryptographically random non-negative int.
+func randInt() int {
+	var b [8]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic(err)
+	}
+	return int(binary.BigEndian.Uint64(b[:]) & 0x7fffffffffffffff)
+}
+
+// randIntn returns a cryptographically random non-negative int in [0, n).
+func randIntn(n int) int {
+	if n <= 0 {
+		panic("randIntn: n must be positive")
+	}
+	return randInt() % n
+}
+
+// randInt63n returns a cryptographically random non-negative int64 in [0, n).
+func randInt63n(n int64) int64 {
+	if n <= 0 {
+		panic("randInt63n: n must be positive")
+	}
+	var b [8]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic(err)
+	}
+	return int64(binary.BigEndian.Uint64(b[:])&0x7fffffffffffffff) % n
+}
 
 var UTCLocation = "UTC"
 
@@ -27,18 +59,18 @@ func (t IntervalGenerator) With(mutators ...IntervalMutator) IntervalGenerator {
 }
 
 func (t IntervalGenerator) generateDaysOfMonth() string {
-	isRange := rand.Int()%2 == 0
+	isRange := randInt()%2 == 0
 	if !isRange {
-		return fmt.Sprintf("%d", rand.Intn(30)+1)
+		return fmt.Sprintf("%d", randIntn(30)+1)
 	}
-	from := rand.Intn(15) + 1
-	to := rand.Intn(31-from) + from + 1
+	from := randIntn(15) + 1
+	to := randIntn(31-from) + from + 1
 	return fmt.Sprintf("%d:%d", from, to)
 }
 
 func (t IntervalGenerator) generateTimeRange() v1beta1.TimeIntervalTimeRange {
-	from := rand.Int63n(1440 / 2)        // [0, 719]
-	to := from + rand.Int63n(1440/2) + 1 // from < ([0,719] + [1,720]) < 1440
+	from := randInt63n(1440 / 2)        // [0, 719]
+	to := from + randInt63n(1440/2) + 1 // from < ([0,719] + [1,720]) < 1440
 	return v1beta1.TimeIntervalTimeRange{
 		StartTime: time.Unix(from*60, 0).UTC().Format("15:04"),
 		EndTime:   time.Unix(to*60, 0).UTC().Format("15:04"),
@@ -46,28 +78,28 @@ func (t IntervalGenerator) generateTimeRange() v1beta1.TimeIntervalTimeRange {
 }
 
 func (t IntervalGenerator) generateWeekday() string {
-	day := rand.Intn(7)
+	day := randIntn(7)
 	return strings.ToLower(time.Weekday(day).String())
 }
 
 func (t IntervalGenerator) generateYear() string {
-	from := 1970 + rand.Intn(100)
-	if rand.Int()%3 == 0 {
-		to := 1970 + from + rand.Intn(10) + 1
+	from := 1970 + randIntn(100)
+	if randInt()%3 == 0 {
+		to := 1970 + from + randIntn(10) + 1
 		return fmt.Sprintf("%d:%d", from, to)
 	}
 	return fmt.Sprintf("%d", from)
 }
 
 func (t IntervalGenerator) generateLocation() *string {
-	if rand.Int()%3 == 0 {
+	if randInt()%3 == 0 {
 		return nil
 	}
 	return &UTCLocation
 }
 
 func (t IntervalGenerator) generateMonth() string {
-	return fmt.Sprintf("%d", rand.Intn(12)+1)
+	return fmt.Sprintf("%d", randIntn(12)+1)
 }
 
 func (t IntervalGenerator) GenerateMany(count int) []v1beta1.TimeIntervalInterval {
@@ -80,12 +112,12 @@ func (t IntervalGenerator) GenerateMany(count int) []v1beta1.TimeIntervalInterva
 
 func (t IntervalGenerator) Generate() v1beta1.TimeIntervalInterval {
 	i := v1beta1.TimeIntervalInterval{
-		DaysOfMonth: generateMany(rand.Intn(6), true, t.generateDaysOfMonth),
+		DaysOfMonth: generateMany(randIntn(6), true, t.generateDaysOfMonth),
 		Location:    t.generateLocation(),
-		Months:      generateMany(rand.Intn(3), true, t.generateMonth),
-		Times:       generateMany(rand.Intn(6), true, t.generateTimeRange),
-		Weekdays:    generateMany(rand.Intn(3), true, t.generateWeekday),
-		Years:       generateMany(rand.Intn(3), true, t.generateYear),
+		Months:      generateMany(randIntn(3), true, t.generateMonth),
+		Times:       generateMany(randIntn(6), true, t.generateTimeRange),
+		Weekdays:    generateMany(randIntn(3), true, t.generateWeekday),
+		Years:       generateMany(randIntn(3), true, t.generateYear),
 	}
 	for _, mutator := range t.mutators {
 		mutator(&i)
