bed2bfaa8911c22de5735ee0709ef97c237d0116
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2026-07-21 15:30:12 -0300

    fix test

--- pkg/tests/api/shorturl/short_url_test.go
+++ pkg/tests/api/shorturl/short_url_test.go
@@ -73,7 +73,7 @@ func TestShortURL(t *testing.T) {
 	defer func() {
 		_ = res.Body.Close()
 	}()
-	assert.Equal(t, "/explore", res.Header.Get("Location"))
+	assert.Equal(t, "http://localhost:3000/explore", res.Header.Get("Location"))
 	assert.Equal(t, http.StatusFound, res.StatusCode)
 
 	// If the go-to does not exist, it should redirect to the home page and return 308.
