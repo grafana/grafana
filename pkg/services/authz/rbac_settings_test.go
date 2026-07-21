6dbfba221b431f354c6b1ee71314bbabce1c42cd
Maicon Costa <maiconscosta@gmail.com>
2026-07-21 04:32:56 -0300

    Update comment and test about local_folder_cache_ttl
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/services/authz/rbac_settings_test.go
+++ pkg/services/authz/rbac_settings_test.go
@@ -10,13 +10,13 @@ import (
 )
 
 func TestReadAuthzClientSettings_LocalFolderCacheTTL(t *testing.T) {
-	t.Run("defaults to 30s when not configured", func(t *testing.T) {
+	t.Run("defaults to 0 (disabled) when not configured", func(t *testing.T) {
 		cfg, err := setting.NewCfgFromBytes([]byte(``))
 		require.NoError(t, err)
 
 		s, err := readAuthzClientSettings(cfg)
 		require.NoError(t, err)
-		require.Equal(t, 30*time.Second, s.localFolderCacheTTL)
+		require.Equal(t, time.Duration(0), s.localFolderCacheTTL)
 	})
 
 	t.Run("reads override from the authorization section", func(t *testing.T) {
@@ -29,17 +29,5 @@ local_folder_cache_ttl = 5s
 		s, err := readAuthzClientSettings(cfg)
 		require.NoError(t, err)
 		require.Equal(t, 5*time.Second, s.localFolderCacheTTL)
-	})
-
-	t.Run("can be disabled with 0", func(t *testing.T) {
-		cfg, err := setting.NewCfgFromBytes([]byte(`
-[authorization]
-local_folder_cache_ttl = 0
-`))
-		require.NoError(t, err)
-
-		s, err := readAuthzClientSettings(cfg)
-		require.NoError(t, err)
-		require.Equal(t, time.Duration(0), s.localFolderCacheTTL)
 	})
 }
