7fd64ae897677ca213c212b28a4cd7c3547d4897
Matias Chomicki <matyax@gmail.com>
2026-07-22 12:03:20 +0200

    Update test

--- public/app/features/explore/TraceView/components/TraceTimelineViewer/SpanDetail/SpanDetailLinkButtons.test.tsx
+++ public/app/features/explore/TraceView/components/TraceTimelineViewer/SpanDetail/SpanDetailLinkButtons.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from 'test/test-utils';
 
 import { CoreApp, type LinkModel, type TimeRange } from '@grafana/data';
 import { usePluginLinks } from '@grafana/runtime';
