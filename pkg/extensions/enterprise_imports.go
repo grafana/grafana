2e9a35bad1afd33b31b1b3126c5c725ea83d4bd0
Ryan McKinley <ryantxu@gmail.com>
2026-07-23 10:59:58 -0700

    test as admin and anonymous

--- pkg/extensions/enterprise_imports.go
+++ pkg/extensions/enterprise_imports.go
@@ -254,7 +254,6 @@ import (
 	_ "github.com/grafana/grafana/pkg/registry/apis/iam/noopstorage"
 	_ "github.com/grafana/grafana/pkg/registry/apis/iam/resourcepermission"
 	_ "github.com/grafana/grafana/pkg/registry/apis/iam/roleeffective"
-	_ "github.com/grafana/grafana/pkg/registry/apis/ofrep"
 	_ "github.com/grafana/grafana/pkg/registry/apis/preferences"
 	_ "github.com/grafana/grafana/pkg/registry/apis/preferences/legacy"
 	_ "github.com/grafana/grafana/pkg/registry/apis/provisioning"
