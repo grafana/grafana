0e1acf51604cb67c885b1435625363ccd28994d0
Haris Rozajac <haris.rozajac12@gmail.com>
2026-07-28 07:23:19 -0600

    fix

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1191,11 +1191,6 @@ export interface FeatureToggles {
   */
   kubernetesAlertingHistorian?: boolean;
   /**
-  * Enables global and folder-scoped dashboard variables via dashboard.grafana.app
-  * @default false
-  */
-  globalDashboardVariables?: boolean;
-  /**
   * Enables the ASAP smoothing transformation for time series data
   * @default false
   */
