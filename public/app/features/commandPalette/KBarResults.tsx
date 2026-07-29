589b5e25895501e2d6bfefcdc5bcdec0d397d59c
Ashley Harrison <ashley.harrison@grafana.com>
2026-07-29 11:35:13 +0100

    add comment

--- public/app/features/commandPalette/KBarResults.tsx
+++ public/app/features/commandPalette/KBarResults.tsx
@@ -81,6 +81,7 @@ export const KBarResults = (props: KBarResultsProps) => {
         groupSize++;
       }
     }
+    // flush the last group
     flushGroup();
     return results;
   }, [props.items]);
