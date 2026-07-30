a9f618f1b146581c3d4a0d31998e94331f604a78
Paul Marbach <paul.marbach@grafana.com>
2026-07-30 16:39:27 -0400

    updating the types a bit more

--- public/app/plugins/panel/geomap/layers/basemaps/generic.ts
+++ public/app/plugins/panel/geomap/layers/basemaps/generic.ts
@@ -1,12 +1,9 @@
-import type OpenLayersMap from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 
 import {
   type MapLayerRegistryItem,
   type MapLayerOptions,
-  type GrafanaTheme2,
-  type EventBus,
   type PanelOptionsEditorBuilder,
   textUtil,
 } from '@grafana/data';
