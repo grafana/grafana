643a7a70b41c365a488723c29e6353a5c8702679
Marc M. <146180665+grafakus@users.noreply.github.com>
2026-08-03 18:59:19 +0200

    test: Update E2E Panel page object for consistency (#129944)

--- e2e-playwright/dashboard-new-layouts/page-objects/sidebar/PanelOptions.ts
+++ e2e-playwright/dashboard-new-layouts/page-objects/sidebar/PanelOptions.ts
@@ -39,7 +39,10 @@ export class PanelOptions extends PageObject {
 
   async toggleTransparentBackground() {
     await test.step('Toggle transparent background', async () => {
-      await this.page.getByRole('switch', { name: 'Transparent background' }).click({ force: true });
+      await this.dashboardPage
+        .getByGrafanaSelector(this.selectors.components.Sidebar.container)
+        .getByRole('switch', { name: 'Transparent background' })
+        .click({ force: true });
     });
   }
 }
