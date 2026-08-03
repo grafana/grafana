a25e63de9c6d0de9aa3da06b7e813636b01ec68c
MattIPv4 <me@mattcowley.co.uk>
2026-08-03 19:25:44 +0100

    Make theme flags type generic record

--- packages/grafana-data/src/themes/types.ts
+++ packages/grafana-data/src/themes/types.ts
@@ -34,12 +34,7 @@ export interface GrafanaTheme2 {
   /** @deprecated Will be removed in a future version */
   v1: GrafanaTheme;
   /** feature flags that might impact component looks */
-  flags: {
-    /**
-     * @internal
-     */
-    visualDesignRefresh?: boolean;
-  };
+  flags: Record<string, boolean>;
 }
 
 export const ThemeRichColorInputSchema = z.object({
