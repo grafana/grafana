0db6798ab96570b7d0b1652bd41c562b5c927df9
nmarrs <nathanielmarrs@gmail.com>
2026-08-04 15:21:55 -0700

    Notebooks: Add translated navigation entry

--- public/app/core/utils/navBarItem-translations.ts
+++ public/app/core/utils/navBarItem-translations.ts
@@ -63,6 +63,8 @@ export function getNavTitle(navId: string | undefined) {
       return t('nav.explore.title', 'Explore');
     case 'drilldown':
       return t('nav.drilldown.title', 'Drilldown');
+    case 'notebooks':
+      return t('nav.notebooks.title', 'Notebooks');
     case 'alerting':
       return t('nav.alerting.title', 'Alerting');
     case 'plugin-page-grafana-oncall-app':
@@ -214,6 +216,8 @@ export function getNavSubTitle(navId: string | undefined) {
       return t('nav.dashboards.subtitle', 'Create and manage dashboards to visualize your data');
     case 'dashboards/browse':
       return t('nav.dashboards.subtitle', 'Create and manage dashboards to visualize your data');
+    case 'notebooks':
+      return t('nav.notebooks.subtitle', 'Capture investigations with narrative text and live visualizations');
     case 'manage-folder':
       return t('nav.manage-folder.subtitle', 'Manage folder dashboards and permissions');
     case 'dashboards/playlists':
