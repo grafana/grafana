168a0faf03aa2bdd6549c5dc25f29516d655cd04
Owen Smallwood <owen.smallwood@grafana.com>
2026-08-04 15:50:36 -0600

    Unified Storage: gate vector store registration behind unifiedStorage.vectorStore feature flag

--- pkg/storage/unified/sql/service.go
+++ pkg/storage/unified/sql/service.go
@@ -575,7 +575,8 @@ func (s *service) createAndRegisterServer(provider grpcserver.Provider, opts Ser
 	s.uninitializedSearchServer = server
 
 	var vs *resource.VectorStoreServer
-	if opts.VectorBackend != nil && opts.Embedder != nil {
+	if opts.VectorBackend != nil && opts.Embedder != nil &&
+		opts.Features != nil && opts.Features.IsEnabledGlobally(featuremgmt.FlagUnifiedStorageVectorStore) {
 		vs = resource.NewVectorStoreServer(opts.VectorBackend, opts.Embedder, opts.Cfg.VectorAllowedExternalCollections, opts.Cfg.VectorAllowedWriteServices, opts.VectorMetrics)
 	}
 	s.registerUnifiedResourceServer(provider, server, vs)
