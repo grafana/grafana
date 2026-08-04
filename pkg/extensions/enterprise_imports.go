9b382c763c5d4b7174a5fc22435f4a236b2fbc5c
Ryan Morris <ryan.morris@grafana.com>
2026-08-04 11:37:09 -0500

    InfluxDB: Remove from core plugins (#129602)

--- pkg/extensions/enterprise_imports.go
+++ pkg/extensions/enterprise_imports.go
@@ -549,7 +549,6 @@ import (
 	_ "github.com/grafana/grafana/pkg/tsdb/grafana-testdata-datasource"
 	_ "github.com/grafana/grafana/pkg/tsdb/grafanads"
 	_ "github.com/grafana/grafana/pkg/tsdb/graphite"
-	_ "github.com/grafana/grafana/pkg/tsdb/influxdb"
 	_ "github.com/grafana/grafana/pkg/util"
 	_ "github.com/grafana/grafana/pkg/util/errhttp"
 	_ "github.com/grafana/grafana/pkg/util/retryer"
