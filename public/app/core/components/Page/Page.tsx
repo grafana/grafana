bdd73348ead92ab7e31014dd7a76b50dfc43a46a
MattIPv4 <me@mattcowley.co.uk>
2026-08-05 23:23:10 +0100

    Update home background for visual refresh

--- public/app/core/components/Page/Page.tsx
+++ public/app/core/components/Page/Page.tsx
@@ -129,7 +129,7 @@ const getStyles = (theme: GrafanaTheme2, visualRefreshEnabled: boolean) => {
     }),
     wrapperGradient: css({
       label: 'page-wrapper-gradient',
-      background: `url('data:image/svg+xml;utf8,${encodeURIComponent(getGradientBackgroundForTheme(theme))}') center center / cover no-repeat`,
+      background: `url('data:image/svg+xml;utf8,${encodeURIComponent(getGradientBackgroundForTheme(theme, visualRefreshEnabled))}') center center / cover no-repeat`,
     }),
     pageContent: css({
       label: 'page-content',
@@ -177,9 +177,40 @@ function getDefaultBackgroundForLayout(layout: PageLayoutType, visualRefreshEnab
   return visualRefreshEnabled ? 'primary' : 'canvas';
 }
 
-function getGradientBackgroundForTheme(theme: GrafanaTheme2) {
+function getGradientBackgroundForTheme(theme: GrafanaTheme2, visualRefreshEnabled: boolean) {
   // Use an inline SVG as a background to avoid flashing of the background when loading a page
   // Use an inline SVG rather than a CSS gradient due to the complexity of the gradients being used
+
+  if (visualRefreshEnabled) {
+    return `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 3840 2160">
+  <rect width="3840" height="2160" fill="${theme.colors.background.page}" />
+  <rect width="3840" height="2160" fill="url(#full)" fill-opacity="0.5"/>
+  <rect width="3840" height="2160" fill="url(#highlight)" fill-opacity="0.25"/>
+  <rect width="3840" height="2160" fill="url(#right)" fill-opacity="0.20"/>
+  <rect width="3840" height="2160" fill="url(#left)" fill-opacity="0.25"/>
+  <defs>
+    <linearGradient id="full" x1="1920" x2="1920" y1="0" y2="2160" gradientUnits="userSpaceOnUse">
+      <stop stop-color="${theme.colors.tertiary.background}"/>
+      <stop offset="1" stop-color="${theme.colors.tertiary.background}" stop-opacity="0.5"/>
+    </linearGradient>
+    <radialGradient id="highlight" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(1920 2160) rotate(90) scale(684.5 3891.29)">
+      <stop stop-color="${theme.colors.tertiary.backgroundEmphasis}"/>
+      <stop offset="1" stop-color="${theme.colors.tertiary.backgroundEmphasis}" stop-opacity="0"/>
+    </radialGradient>
+    <linearGradient id="right" x1="3728.97" y1="41.797" x2="2775.64" y2="1368.31" gradientUnits="userSpaceOnUse">
+      <stop stop-color="${theme.colors.accent.backgroundEmphasis}"/>
+      <stop offset="1" stop-color="${theme.colors.accent.backgroundEmphasis}" stop-opacity="0"/>
+    </linearGradient>
+    <linearGradient id="left" x1="-18.9569" y1="14.831" x2="884.72" y2="850.544" gradientUnits="userSpaceOnUse">
+      <stop stop-color="${theme.colors.info.backgroundEmphasis}"/>
+      <stop offset="1" stop-color="${theme.colors.info.backgroundEmphasis}" stop-opacity="0"/>
+    </linearGradient>
+  </defs>
+</svg>
+`;
+  }
+
   return theme.isDark
     ? `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 2322 1181"><path fill="url(#a)" d="M0 0h2322v1181H0z"/><path fill="url(#b)" fill-opacity=".25" d="M0 0h2322v1181H0z"/><path fill="url(#c)" fill-opacity=".2" d="M0 0h2322v1181H0z"/><path fill="#1b1b2c" fill-opacity=".4" d="M0 0h2322v1181H0z"/><defs><linearGradient id="a" x1="472.424" x2="335.969" y1="1181" y2="-57.275" gradientUnits="userSpaceOnUse"><stop stop-color="#1a1626"/><stop offset="1" stop-color="#3a364c"/></linearGradient><linearGradient id="b" x1="2254.86" x2="1752.33" y1="22.853" y2="796.185" gradientUnits="userSpaceOnUse"><stop stop-color="#722323"/><stop offset="1" stop-color="#722323" stop-opacity="0"/></linearGradient><linearGradient id="c" x1="-11.463" x2="484.016" y1="8.109" y2="514.871" gradientUnits="userSpaceOnUse"><stop stop-color="#1b6d68"/><stop offset="1" stop-color="#1b416d" stop-opacity="0"/></linearGradient></defs></svg>`
     : `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 2322 1181"><path fill="url(#a)" d="M0 0h2322v1181H0z"/><path fill="url(#b)" d="M0 0h2322v1181H0z"/><path fill="url(#c)" fill-opacity=".18" d="M0 0h2322v1181H0z"/><path fill="url(#d)" fill-opacity=".18" d="M0 0h2322v1181H0z"/><defs><linearGradient id="a" x1="472.424" x2="335.969" y1="1181" y2="-57.275" gradientUnits="userSpaceOnUse"><stop stop-color="#cbcee9"/><stop offset=".745" stop-color="#f4f2fb"/></linearGradient><linearGradient id="b" x1="862" x2="762.5" y1="1209.5" y2="12" gradientUnits="userSpaceOnUse"><stop stop-color="#e3e3ec"/><stop offset="1" stop-color="#dedfee"/></linearGradient><linearGradient id="c" x1="2268.5" x2="1749.44" y1="53.5" y2="797.834" gradientUnits="userSpaceOnUse"><stop stop-color="#ff9a9a"/><stop offset="1" stop-color="#ddc8eb" stop-opacity="0"/></linearGradient><linearGradient id="d" x1="-11.463" x2="655.435" y1="8.109" y2="772.366" gradientUnits="userSpaceOnUse"><stop stop-color="#a6e3df"/><stop offset="1" stop-color="#b6e1ee" stop-opacity="0"/></linearGradient></defs></svg>`;
