8c0c5f296822e671d714962542676772fac58628
Leon Sorokin <leeoniya@gmail.com>
2026-08-06 19:51:56 -0500

    eslint-disable

--- packages/grafana-data/src/datetime/moment_implementation.ts
+++ packages/grafana-data/src/datetime/moment_implementation.ts
@@ -5,4 +5,6 @@ import luxonMoment from './luxon_moment_compat/moment';
 
 const useLuxon = typeof window !== 'undefined' && '__grafanaUseLuxon' in window && window.__grafanaUseLuxon === true;
 
+// Both implementations expose the same runtime API but use different concrete return types.
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default useLuxon ? luxonMoment : (legacyMoment as unknown as typeof luxonMoment);
