38585a5807374e8dc044e43fc7f62827df6f0abd
Leon Sorokin <leeoniya@gmail.com>
2026-08-10 18:41:44 -0500

    make knip happy

--- packages/grafana-data/src/datetime/moment_implementation.ts
+++ packages/grafana-data/src/datetime/moment_implementation.ts
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-restricted-imports
 import legacyMoment from 'moment-timezone';
 
-import luxonMoment from './luxon_moment_compat/moment';
+import luxonMoment, { type MomentFactory } from './luxon_moment_compat/moment';
 
 const useLuxon = typeof window !== 'undefined' && '__grafanaUseLuxon' in window && window.__grafanaUseLuxon === true;
 
 // Both implementations expose the same runtime API but use different concrete return types.
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-export default useLuxon ? luxonMoment : (legacyMoment as unknown as typeof luxonMoment);
+const moment: MomentFactory = useLuxon ? luxonMoment : (legacyMoment as unknown as MomentFactory);
+
+export default moment;
