800126fae5548d24e84cb3a89be647ea7580397f
Adela Almasan <88068998+adela-almasan@users.noreply.github.com>
2026-08-12 07:33:51 -0500

    Text: Add flag for the new features (#130577)

--- packages/grafana-runtime/src/internal/openFeature/openfeature-types.gen.d.ts
+++ packages/grafana-runtime/src/internal/openFeature/openfeature-types.gen.d.ts
@@ -43,6 +43,7 @@ declare module "@openfeature/core" {
     | "otelLogsFormatting"
     | "grafana.starredFolders"
     | "grafana.newTextPanel"
+    | "text.newFeatures"
     | "plugins.useMTPlugins"
     | "globalDashboardVariables"
     | "grafana.dashboardGlobalVariables"
