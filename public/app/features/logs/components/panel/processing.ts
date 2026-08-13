48d9adb77ee89cb87243433030b3e32a7f9a1b80
Matias Chomicki <matyax@gmail.com>
2026-08-13 14:44:26 +0200

    LogLineDetailsLog: add support to conditionally prettify

--- public/app/features/logs/components/panel/processing.ts
+++ public/app/features/logs/components/panel/processing.ts
@@ -130,9 +130,12 @@ export class LogListModel implements LogRowModel {
     }
   }
 
-  clone() {
+  clone({ prettifyJSON }: { prettifyJSON?: boolean }) {
     const clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
     // Unless this function is required outside of <LogLineDetailsLog />, we create a wrapped clone, so new lines are not stripped.
+    if (prettifyJSON !== undefined) {
+      clone._prettifyJSON = prettifyJSON;
+    }
     clone._wrapLogMessage = true;
     clone._body = undefined;
     clone._highlightTokens = undefined;
