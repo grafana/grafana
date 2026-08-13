223c8831c7a824cf61d5e19dd59bb15f5209297b
Khalil Haji <khalil.haji@grafana.com>
2026-08-13 17:01:17 -0400

    Alerting: fetch ManagerProperties for all rules instead of a subset

--- pkg/services/ngalert/notifier/alertmanager_config.go
+++ pkg/services/ngalert/notifier/alertmanager_config.go
@@ -522,7 +522,7 @@ type provisioningStore interface {
 	SetProvenance(ctx context.Context, o models.Provisionable, org int64, p models.Provenance) error
 	DeleteProvenance(ctx context.Context, o models.Provisionable, org int64) error
 	GetManagerProperties(ctx context.Context, o models.Provisionable, org int64) (utils.ManagerProperties, error)
-	GetManagerPropertiesByUIDs(ctx context.Context, org int64, resourceType string, uids []string) (map[string]utils.ManagerProperties, error)
+	GetAllManagerProperties(ctx context.Context, org int64, resourceType string) (map[string]utils.ManagerProperties, error)
 	SetManagerProperties(ctx context.Context, o models.Provisionable, org int64, m utils.ManagerProperties) error
 }
 
