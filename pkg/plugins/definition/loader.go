f1e566c73650b84c656b539cddab38bc4e762866
Ryan McKinley <ryantxu@gmail.com>
2026-08-19 07:54:55 +0900

    Update pkg/plugins/definition/loader.go
    Co-authored-by: Steve Simpson <steve@grafana.com>

--- pkg/plugins/definition/loader.go
+++ pkg/plugins/definition/loader.go
@@ -156,7 +156,7 @@ func loadManifest(rootfs fs.FS) (*app.ManifestData, error) {
 	case "apps.grafana.app/v1alpha2":
 		var cr appmanifestV1alpha2.AppManifest
 		if err := json.Unmarshal(raw, &cr); err != nil {
-			return nil, fmt.Errorf("decoding AppManifest CR: %w", err)
+			return nil, fmt.Errorf("decoding AppManifest CR (%s): %w", meta.APIVersion, err)
 		}
 		manifest, err = cr.Spec.ToManifestData()
 	case "apps.grafana.app/v1alpha1":
