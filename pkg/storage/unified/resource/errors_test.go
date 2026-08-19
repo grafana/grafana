a7fc26cfce28761aa07a88ce4cf492c1d31360f1
gngrchrl <charlie.tiehm@grafana.com>
2026-08-19 13:34:13 +0200

    remove unusued pieces of first PR

--- pkg/storage/unified/resource/errors_test.go
+++ pkg/storage/unified/resource/errors_test.go
@@ -1,12 +1,10 @@
 package resource
 
 import (
-	"net/http"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/grafana/grafana/pkg/storage/unified/resourcepb"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -19,50 +17,6 @@ func TestErrResourceAlreadyExistsIsRecognisable(t *testing.T) {
 	t.Parallel()
 
 	require.True(t, apierrors.IsAlreadyExists(ErrResourceAlreadyExists), "ErrResourceAlreadyExists should be recognised as an AlreadyExists error")
-}
-
-func TestGRPCCodeFromHTTPStatus(t *testing.T) {
-	tests := []struct {
-		httpCode int32
-		expected codes.Code
-	}{
-		{http.StatusOK, codes.OK},
-		{http.StatusBadRequest, codes.InvalidArgument},
-		{http.StatusUnauthorized, codes.Unauthenticated},
-		{http.StatusForbidden, codes.PermissionDenied},
-		{http.StatusNotFound, codes.NotFound},
-		{http.StatusRequestTimeout, codes.DeadlineExceeded},
-		{http.StatusConflict, codes.AlreadyExists},
-		{http.StatusPreconditionFailed, codes.FailedPrecondition},
-		{http.StatusRequestedRangeNotSatisfiable, codes.OutOfRange},
-		{http.StatusUnprocessableEntity, codes.InvalidArgument},
-		{http.StatusTooManyRequests, codes.ResourceExhausted},
-		{http.StatusInternalServerError, codes.Internal},
-		{http.StatusNotImplemented, codes.Unimplemented},
-		{http.StatusServiceUnavailable, codes.Unavailable},
-		{http.StatusGatewayTimeout, codes.DeadlineExceeded},
-		// Unmapped statuses fall back by class.
-		{http.StatusTeapot, codes.InvalidArgument},
-		{http.StatusInsufficientStorage, codes.Internal},
-		{http.StatusMovedPermanently, codes.Unknown},
-		{0, codes.Unknown},
-	}
-
-	for _, tt := range tests {
-		require.Equal(t, tt.expected, grpcCodeFromHTTPStatus(tt.httpCode),
-			"http %d", tt.httpCode)
-	}
-}
-
-// The HTTP status a gRPC code maps out to must map back to a code with the same
-// HTTP status, otherwise a request crossing the boundary twice changes meaning.
-func TestHTTPStatusRoundTrip(t *testing.T) {
-	for code := codes.OK; code <= codes.Unauthenticated; code++ {
-		httpCode := runtime.HTTPStatusFromCode(code)
-		back := grpcCodeFromHTTPStatus(int32(httpCode))
-		require.Equal(t, httpCode, runtime.HTTPStatusFromCode(back),
-			"%s -> %d -> %s", code, httpCode, back)
-	}
 }
 
 func TestAsErrorResult_UnpackCorrectErrorDetails(t *testing.T) {
