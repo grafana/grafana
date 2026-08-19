61449ad72b4be2aa70a4d31d5e5f50ca7d4503e7
gngrchrl <charlie.tiehm@grafana.com>
2026-08-19 15:04:44 +0200

    removal of errors.go changes, part of another PR

--- pkg/storage/unified/resource/errors_test.go
+++ pkg/storage/unified/resource/errors_test.go
@@ -3,48 +3,12 @@ package resource
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/grafana/grafana/pkg/storage/unified/resourcepb"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/testing/protocmp"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 func TestErrResourceAlreadyExistsIsRecognisable(t *testing.T) {
 	t.Parallel()
 
 	require.True(t, apierrors.IsAlreadyExists(ErrResourceAlreadyExists), "ErrResourceAlreadyExists should be recognised as an AlreadyExists error")
-}
-
-func TestAsErrorResult_UnpackCorrectErrorDetails(t *testing.T) {
-	st := status.New(codes.Aborted, "concurrent create")
-	errDetails := resourcepb.ErrorResult{
-		Message: "message",
-		Reason:  "reason",
-		Details: &resourcepb.ErrorDetails{
-			Name:  "name",
-			Group: "group",
-			Kind:  "kind",
-			Uid:   "uid",
-			Causes: []*resourcepb.ErrorCause{
-				{
-					Reason: string(field.ErrorTypeForbidden),
-					Field:  "field",
-				},
-			},
-			RetryAfterSeconds: 12,
-		},
-		Code: 12,
-	}
-	st, err := st.WithDetails(&errDetails)
-	require.NoError(t, err)
-
-	got := AsErrorResult(st.Err())
-
-	// diff used as require.Equal has it's issues with Details.Causes
-	diff := cmp.Diff(&errDetails, got, protocmp.Transform())
-	require.Empty(t, diff)
 }
