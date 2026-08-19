a69e9b0b9ff459673ba0009ceaeefa37d512b420
gngrchrl <charlie.tiehm@grafana.com>
2026-08-19 14:59:55 +0200

    removal of client grpc error handling changes

--- pkg/storage/unified/resource/server.go
+++ pkg/storage/unified/resource/server.go
@@ -2488,11 +2488,6 @@ func (s *server) checkQuota(ctx context.Context, nsr NamespacedResource) error {
 		Kinds:     []string{nsr.GroupResource()},
 	})
 	if err != nil {
-		// this is the equivalent of checking statsRsp.Error below
-		if errorResultFromGRPCDetails(err) != nil {
-			s.degraded(ctx, "check_quota", "stats_error", nsr, err)
-			return nil
-		}
 		s.degraded(ctx, "check_quota", "get_stats_failed", nsr, err)
 		return nil
 	}
