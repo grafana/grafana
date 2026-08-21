71b25e4d6c3b053a8b73440711f89e5a6430dba8
Torkel Ödegaard <torkel@grafana.com>
2026-08-21 11:17:03 +0200

    fix primary body

--- public/app/features/alerting/unified/components/contact-points/ContactPoint.tsx
+++ public/app/features/alerting/unified/components/contact-points/ContactPoint.tsx
@@ -279,7 +279,7 @@ const getStyles = (theme: GrafanaTheme2) => ({
   }),
   integrationWrapper: css({
     position: 'relative',
-    background: theme.flags.visualDesignRefresh ? theme.colors.background.page : theme.colors.background.secondary,
+    background: theme.flags.visualDesignRefresh ? theme.colors.background.page : theme.colors.background.primary,
     padding: `${theme.spacing(1)} ${theme.spacing(1.5)}`,
   }),
   metadataRow: css({
