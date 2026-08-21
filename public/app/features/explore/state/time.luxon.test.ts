cae56d878501ee56925cc66c843edadf5343d1f8
Leon Sorokin <leeoniya@gmail.com>
2026-08-21 12:13:42 -0500

    move to beforeEach / afterEach

--- public/app/features/explore/state/time.luxon.test.ts
+++ public/app/features/explore/state/time.luxon.test.ts
@@ -15,42 +15,46 @@ jest.mock('@grafana/runtime', () => ({
 }));
 
 describe('Explore time range with Luxon', () => {
-  it('normalizes fractional millisecond timestamps', async () => {
-    const flagName = '__grafanaUseLuxon';
-    const originalDescriptor = Object.getOwnPropertyDescriptor(window, flagName);
+  const flagName = '__grafanaUseLuxon';
+  let originalDescriptor: PropertyDescriptor | undefined;
+
+  beforeEach(() => {
+    originalDescriptor = Object.getOwnPropertyDescriptor(window, flagName);
     Object.defineProperty(window, flagName, { configurable: true, value: true });
+  });
 
-    try {
-      await jest.isolateModulesAsync(async () => {
-        const [{ configureStore }, { createDefaultInitialState }, { updateTime }] = await Promise.all([
-          import('app/store/configureStore'),
-          import('./testHelpers'),
-          import('./time'),
-        ]);
-        const state = createDefaultInitialState().defaultInitialState as any;
-        const store = configureStore(state);
-
-        store.dispatch(
-          updateTime({
-            exploreId: 'left',
-            absoluteRange: {
-              from: 1787318699146.3264,
-              to: 1787318699250.8,
-            },
-          })
-        );
-
-        expect(store.getState().explore.panes.left!.absoluteRange).toEqual({
-          from: 1787318699146,
-          to: 1787318699250,
-        });
-      });
-    } finally {
-      if (originalDescriptor) {
-        Object.defineProperty(window, flagName, originalDescriptor);
-      } else {
-        Reflect.deleteProperty(window, flagName);
-      }
+  afterEach(() => {
+    if (originalDescriptor) {
+      Object.defineProperty(window, flagName, originalDescriptor);
+    } else {
+      Reflect.deleteProperty(window, flagName);
     }
+  });
+
+  it('normalizes fractional millisecond timestamps', async () => {
+    await jest.isolateModulesAsync(async () => {
+      const [{ configureStore }, { createDefaultInitialState }, { updateTime }] = await Promise.all([
+        import('app/store/configureStore'),
+        import('./testHelpers'),
+        import('./time'),
+      ]);
+      const state = createDefaultInitialState().defaultInitialState as any;
+      const store = configureStore(state);
+
+      store.dispatch(
+        updateTime({
+          exploreId: 'left',
+          absoluteRange: {
+            from: 1787318699146.3264,
+            to: 1787318699250.8,
+          },
+        })
+      );
+
+      expect(store.getState().explore.panes.left!.absoluteRange).toEqual({
+        from: 1787318699146,
+        to: 1787318699250,
+      });
+    });
   });
 });
