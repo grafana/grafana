a8781a99d5c94680ee091ed20efa5546a9bcb0b5
Ravi Dasari <raviunivers@gmail.com>
2026-08-24 21:14:12 -0400

    fix(alerting): use shared fuzzySearch for contact point filtering

--- public/app/features/alerting/unified/components/contact-points/useContactPointsSearch.tsx
+++ public/app/features/alerting/unified/components/contact-points/useContactPointsSearch.tsx
@@ -1,19 +1,9 @@
-import uFuzzy from '@leeoniya/ufuzzy';
+import { fuzzySearch } from '@grafana/data';
 import { uniq } from 'lodash';
 import { useMemo } from 'react';
 
 import { RECEIVER_META_KEY } from 'app/features/alerting/unified/components/contact-points/constants';
 import { type ContactPointWithMetadata } from 'app/features/alerting/unified/components/contact-points/utils';
-
-const LONG_QUERY_LENGTH_THRESHOLD = 40;
-
-const fuzzyFinder = new uFuzzy({
-  intraMode: 1,
-  intraIns: 1,
-  intraSub: 1,
-  intraDel: 1,
-  intraTrn: 1,
-});
 
 // let's search in two different haystacks, the name of the contact point and the type of the receiver(s)
 export const useContactPointsSearch = (
@@ -37,27 +27,8 @@ export const useContactPointsSearch = (
     return contactPoints;
   }
 
-  // Long, separator-heavy queries can trigger expensive regex backtracking in fuzzy matching.
-  // For these cases, use a deterministic contains check to keep typing responsive.
-  if (normalizedSearch.length >= LONG_QUERY_LENGTH_THRESHOLD) {
-    const lowerSearch = normalizedSearch.toLocaleLowerCase();
-
-    const hits = contactPoints
-      .map((contactPoint, index) => ({ contactPoint, index }))
-      .filter(({ contactPoint }) => {
-        const nameMatch = contactPoint.name.toLocaleLowerCase().includes(lowerSearch);
-        const typeMatch = contactPoint.grafana_managed_receiver_configs
-          .some((receiver) => receiver[RECEIVER_META_KEY].name.toLocaleLowerCase().includes(lowerSearch));
-
-        return nameMatch || typeMatch;
-      })
-      .map(({ index }) => index);
-
-    return hits.map((id) => contactPoints[id]);
-  }
-
-  const nameHits = fuzzyFinder.filter(nameHaystack, normalizedSearch) ?? [];
-  const typeHits = fuzzyFinder.filter(typeHaystack, normalizedSearch) ?? [];
+  const nameHits = fuzzySearch(nameHaystack, normalizedSearch);
+  const typeHits = fuzzySearch(typeHaystack, normalizedSearch);
 
   const hits = [...nameHits, ...typeHits];
 
