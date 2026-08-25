c03e5d9d68df48b82ed6fcc290167aabe7625f46
Ryan McKinley <ryantxu@gmail.com>
2026-08-25 19:58:22 +0900

    plumbing works

--- pkg/plugins/backendplugin/v3/types.go
+++ pkg/plugins/backendplugin/v3/types.go
@@ -1,6 +1,11 @@
 package v3
 
-import "context"
+import (
+	"context"
+
+	pluginv3 "github.com/grafana/grafana-app-sdk/plugin/genproto/grafana/plugin/v3"
+	appgrpcplugin "github.com/grafana/grafana-app-sdk/plugin/grpcplugin"
+)
 
 // ClientV3 implements all required functions in the V3 interface.
 // This is currently a stub, and will evolve as the V3 interface get exercised with real usage
@@ -10,8 +15,22 @@ import "context"
 // 2. No requests contain pluginContext (the root plugin configuration)
 // 3. The v3 client does not include the expansive middleware that exists for v2
 type ClientV3 interface {
-	// Return an error when requests to this implementation will fail for setup reasons
-	IsHealthy(ctx context.Context) error
+	pluginv3.AdmissionServiceClient
+	pluginv3.ConversionServiceClient
+	pluginv3.RouteServiceClient
+}
+
+type sdkClient struct {
+	*appgrpcplugin.ClientV3
+}
+
+// NewClientFromSDK adapts the app SDK's negotiated clients to Grafana's V3 client interface.
+func NewClientFromSDK(client *appgrpcplugin.ClientV3) ClientV3 {
+	return &sdkClient{ClientV3: client}
+}
+
+func (*sdkClient) IsHealthy(context.Context) error {
+	return nil
 }
 
 // ClientV3Loader looks up the ClientV3 for a plugin by ID, waiting for the plugin
