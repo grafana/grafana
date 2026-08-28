e88542c998d5d3b257ed25c6a06c9eb33841d589
Renato Costa <renato.costa@grafana.com>
2026-08-28 13:33:53 -0400

    unified-storage: remove KV leases related config options
    This commit makes the KV lease manager always enabled when the KV storage
    backend is used, removing the `enable_kv_leases` config option. The
    'auto-renew' option is also removed; however, the storage backend can still
    configure a lease duration.
    
    Part of: https://github.com/grafana/search-and-storage-team/issues/972

--- pkg/storage/unified/resource/server_test.go
+++ pkg/storage/unified/resource/server_test.go
@@ -2623,7 +2623,7 @@ func TestStatsAccessChecks(t *testing.T) {
 		t.Cleanup(func() { _ = db.Close() })
 
 		kv := NewBadgerKV(db)
-		store, err := NewKVStorageBackend(KVBackendOptions{KvStore: kv, EnableKVLeases: true, Holder: "test"})
+		store, err := NewKVStorageBackend(KVBackendOptions{KvStore: kv, Holder: "test"})
 		require.NoError(t, err)
 
 		srv, err := NewResourceServer(ResourceServerOptions{
@@ -2735,7 +2735,7 @@ func TestGetResourceDailyStats(t *testing.T) {
 		t.Cleanup(func() { _ = db.Close() })
 
 		kv := NewBadgerKV(db)
-		store, err := NewKVStorageBackend(KVBackendOptions{KvStore: kv, EnableKVLeases: true, Holder: "test"})
+		store, err := NewKVStorageBackend(KVBackendOptions{KvStore: kv, Holder: "test"})
 		require.NoError(t, err)
 
 		srv, err := NewResourceServer(ResourceServerOptions{
