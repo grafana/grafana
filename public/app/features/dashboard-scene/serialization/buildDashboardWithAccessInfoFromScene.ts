245c731695e6d6c29018c5f8e2db278762e7d4e2
Piotr Jamróz <pm.jamroz@gmail.com>
2026-09-09 16:50:53 +0200

    Update builder to use group correctly

--- public/app/features/dashboard-scene/serialization/buildDashboardWithAccessInfoFromScene.ts
+++ public/app/features/dashboard-scene/serialization/buildDashboardWithAccessInfoFromScene.ts
@@ -1,9 +1,6 @@
 import { type Spec as DashboardV2Spec } from '@grafana/schema/apis/dashboard.grafana.app/v2';
-import {
-  DASHBOARD_API_GROUP,
-  dashboardAPIVersionResolver,
-} from 'app/features/dashboard/api/DashboardAPIVersionResolver';
 import { type DashboardWithAccessInfo } from 'app/features/dashboard/api/types';
+import { getK8sV2DashboardApiConfig } from 'app/features/dashboard/api/v2';
 
 import { type DashboardScene } from '../scene/DashboardScene';
 
@@ -42,11 +39,12 @@ export function buildDashboardWithAccessInfoFromScene(
   scene: DashboardScene,
   spec: DashboardV2Spec
 ): DashboardWithAccessInfo<DashboardV2Spec> {
+  const { group, version } = getK8sV2DashboardApiConfig();
   return {
     kind: 'DashboardWithAccessInfo',
     metadata: resolveMetadata(scene),
     access: resolveAccess(scene),
-    apiVersion: `${DASHBOARD_API_GROUP}/${dashboardAPIVersionResolver.getV2()}`,
+    apiVersion: `${group}/${version}`,
     spec,
   };
 }
