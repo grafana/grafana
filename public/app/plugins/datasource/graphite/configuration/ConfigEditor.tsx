dd03b142c375c5bf723cc8a456f70cd8b2837f6d
Ashley Harrison <ashley.harrison@grafana.com>
2026-09-09 13:28:31 +0100

    convert ConfigEditor to a function component

--- public/app/plugins/datasource/graphite/configuration/ConfigEditor.tsx
+++ public/app/plugins/datasource/graphite/configuration/ConfigEditor.tsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   type DataSourcePluginOptionsEditorProps,
@@ -27,114 +27,104 @@ const graphiteTypes = Object.entries(GraphiteType).map(([label, value]) => ({
 
 export type Props = DataSourcePluginOptionsEditorProps<GraphiteOptions>;
 
-type State = {
-  showMappingsHelp: boolean;
-};
+export function ConfigEditor(props: Props) {
+  const { options, onOptionsChange } = props;
+  const [showMappingsHelp, setShowMappingsHelp] = useState(() => store.getObject(SHOW_MAPPINGS_HELP_KEY, true));
 
-export class ConfigEditor extends PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      showMappingsHelp: store.getObject(SHOW_MAPPINGS_HELP_KEY, true),
-    };
-  }
+  const currentGraphiteVersion = options.jsonData.graphiteVersion || DEFAULT_GRAPHITE_VERSION;
 
-  componentDidMount() {
-    updateDatasourcePluginJsonDataOption(this.props, 'graphiteVersion', this.currentGraphiteVersion);
-  }
+  // Persist the default so the rest of the app can rely on a version being set. Only written when
+  // absent, so the resulting options change does not retrigger this.
+  useEffect(() => {
+    if (!options.jsonData.graphiteVersion) {
+      updateDatasourcePluginJsonDataOption({ options, onOptionsChange }, 'graphiteVersion', DEFAULT_GRAPHITE_VERSION);
+    }
+  }, [options, onOptionsChange]);
 
-  render() {
-    const { options, onOptionsChange } = this.props;
+  const currentVersion = graphiteVersions.find((item) => item.value === currentGraphiteVersion);
 
-    const currentVersion = graphiteVersions.find((item) => item.value === this.currentGraphiteVersion);
+  return (
+    <>
+      {options.access === 'direct' && (
+        <Alert title="Deprecation Notice" severity="warning">
+          This data source uses browser access mode. This mode is deprecated and will be removed in the future. Please
+          use server access mode instead.
+        </Alert>
+      )}
+      <DataSourceHttpSettings
+        defaultUrl="http://localhost:8080"
+        dataSourceConfig={options}
+        onChange={onOptionsChange}
+        secureSocksDSProxyEnabled={config.secureSocksDSProxyEnabled}
+      />
+      <FieldSet>
+        <legend className="page-heading">Graphite details</legend>
+        <Field
+          label="Version"
+          description="This option controls what functions are available in the Graphite query editor."
+        >
+          <Select
+            id="graphite-version"
+            aria-label="Graphite version"
+            value={currentVersion}
+            options={graphiteVersions}
+            width={16}
+            onChange={onUpdateDatasourceJsonDataOptionSelect(props, 'graphiteVersion')}
+          />
+        </Field>
 
-    return (
-      <>
-        {options.access === 'direct' && (
-          <Alert title="Deprecation Notice" severity="warning">
-            This data source uses browser access mode. This mode is deprecated and will be removed in the future. Please
-            use server access mode instead.
-          </Alert>
-        )}
-        <DataSourceHttpSettings
-          defaultUrl="http://localhost:8080"
-          dataSourceConfig={options}
-          onChange={onOptionsChange}
-          secureSocksDSProxyEnabled={config.secureSocksDSProxyEnabled}
-        />
-        <FieldSet>
-          <legend className="page-heading">Graphite details</legend>
-          <Field
-            label="Version"
-            description="This option controls what functions are available in the Graphite query editor."
-          >
-            <Select
-              id="graphite-version"
-              aria-label="Graphite version"
-              value={currentVersion}
-              options={graphiteVersions}
-              width={16}
-              onChange={onUpdateDatasourceJsonDataOptionSelect(this.props, 'graphiteVersion')}
-            />
-          </Field>
-
-          <Field
-            label="Graphite backend type"
-            description="There are different types of Graphite compatible backends. Here you can specify the type you are using. For Metrictank, this will enable specific features, like query processing meta data. Metrictank
+        <Field
+          label="Graphite backend type"
+          description="There are different types of Graphite compatible backends. Here you can specify the type you are using. For Metrictank, this will enable specific features, like query processing meta data. Metrictank
         is a multi-tenant timeseries engine for Graphite and friends."
+        >
+          <Select
+            id="backend-type"
+            options={graphiteTypes}
+            value={graphiteTypes.find((type) => type.value === options.jsonData.graphiteType)}
+            width={16}
+            onChange={onUpdateDatasourceJsonDataOptionSelect(props, 'graphiteType')}
+          />
+        </Field>
+        {options.jsonData.graphiteType === GraphiteType.Metrictank && (
+          <Field
+            label="Rollup indicator"
+            description="Shows up as an info icon in panel headers when data is aggregated."
           >
-            <Select
-              id="backend-type"
-              options={graphiteTypes}
-              value={graphiteTypes.find((type) => type.value === options.jsonData.graphiteType)}
-              width={16}
-              onChange={onUpdateDatasourceJsonDataOptionSelect(this.props, 'graphiteType')}
+            <Switch
+              id="rollup-indicator"
+              value={!!options.jsonData.rollupIndicatorEnabled}
+              onChange={onUpdateDatasourceJsonDataOptionChecked(props, 'rollupIndicatorEnabled')}
             />
           </Field>
-          {options.jsonData.graphiteType === GraphiteType.Metrictank && (
-            <Field
-              label="Rollup indicator"
-              description="Shows up as an info icon in panel headers when data is aggregated."
-            >
-              <Switch
-                id="rollup-indicator"
-                value={!!options.jsonData.rollupIndicatorEnabled}
-                onChange={onUpdateDatasourceJsonDataOptionChecked(this.props, 'rollupIndicatorEnabled')}
-              />
-            </Field>
-          )}
-        </FieldSet>
-        <MappingsConfiguration
-          mappings={(options.jsonData.importConfiguration?.loki?.mappings || []).map(toString)}
-          showHelp={this.state.showMappingsHelp}
-          onDismiss={() => {
-            this.setState({ showMappingsHelp: false });
-            store.setObject(SHOW_MAPPINGS_HELP_KEY, false);
-          }}
-          onRestoreHelp={() => {
-            this.setState({ showMappingsHelp: true });
-            store.setObject(SHOW_MAPPINGS_HELP_KEY, true);
-          }}
-          onChange={(mappings) => {
-            onOptionsChange({
-              ...options,
-              jsonData: {
-                ...options.jsonData,
-                importConfiguration: {
-                  ...options.jsonData.importConfiguration,
-                  loki: {
-                    mappings: mappings.map(fromString),
-                  },
+        )}
+      </FieldSet>
+      <MappingsConfiguration
+        mappings={(options.jsonData.importConfiguration?.loki?.mappings || []).map(toString)}
+        showHelp={showMappingsHelp}
+        onDismiss={() => {
+          setShowMappingsHelp(false);
+          store.setObject(SHOW_MAPPINGS_HELP_KEY, false);
+        }}
+        onRestoreHelp={() => {
+          setShowMappingsHelp(true);
+          store.setObject(SHOW_MAPPINGS_HELP_KEY, true);
+        }}
+        onChange={(mappings) => {
+          onOptionsChange({
+            ...options,
+            jsonData: {
+              ...options.jsonData,
+              importConfiguration: {
+                ...options.jsonData.importConfiguration,
+                loki: {
+                  mappings: mappings.map(fromString),
                 },
               },
-            });
-          }}
-        />
-      </>
-    );
-  }
-
-  private get currentGraphiteVersion() {
-    return this.props.options.jsonData.graphiteVersion || DEFAULT_GRAPHITE_VERSION;
-  }
+            },
+          });
+        }}
+      />
+    </>
+  );
 }
