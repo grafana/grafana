43d1b57b326592fda0cae1f0453d0716e4032549
eledobleefe <laura.fernandez@grafana.com>
2026-09-09 16:56:05 +0200

    Fix side effect and missalignment

--- public/app/core/components/TagFilter/TagBadge.tsx
+++ public/app/core/components/TagFilter/TagBadge.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/css';
 import * as React from 'react';
 
 import { type GrafanaTheme2 } from '@grafana/data';
-import { getTagColorsFromName, Icon, useTheme2 } from '@grafana/ui';
+import { getTagColorsFromName, Icon, useStyles2, useTheme2 } from '@grafana/ui';
 
 export interface Props {
   label: string;
@@ -13,26 +13,33 @@ export interface Props {
 
 export const TagBadge = ({ count, label, onClick, removeIcon }: Props) => {
   const theme = useTheme2();
-  const styles = getStyles(theme, label);
+  const visualRefreshEnabled = theme.flags.visualDesignRefresh;
+  const styles = useStyles2(getStyles);
+  const { background, text } = getTagColorsFromName(label, theme);
 
   const countLabel = count !== 0 && <span style={{ marginLeft: '3px' }}>{`(${count})`}</span>;
 
   return (
-    <span className={styles.badge}>
+    <span
+      className={styles.badge}
+      style={{
+        backgroundColor: background,
+        color: visualRefreshEnabled ? text : undefined,
+      }}
+    >
       {removeIcon && <Icon onClick={onClick} name="times" />}
       {label} {countLabel}
     </span>
   );
 };
 
-export const getStyles = (theme: GrafanaTheme2, label?: string) => {
+export const getStyles = (theme: GrafanaTheme2, isVisualRefreshEnabled?: boolean) => {
   const visualRefreshEnabled = theme.flags.visualDesignRefresh;
-  const { background, text } = getTagColorsFromName(label, theme);
   return {
     badge: css(
       {
         ...theme.typography.bodySmall,
-        backgroundColor: background,
+        backgroundColor: theme.v1.palette.gray1,
         borderRadius: theme.shape.radius.sm,
         color: theme.v1.palette.white,
         display: 'inline-block',
@@ -46,14 +53,15 @@ export const getStyles = (theme: GrafanaTheme2, label?: string) => {
         },
       },
       visualRefreshEnabled && {
-        color: text,
-        gap: '3px',
+        gap: theme.spacing(0.5),
         borderRadius: theme.shape.radius.pill,
         padding: `${theme.spacing.x0} ${theme.spacing.x1}`,
         border: 'none',
+        height: '20px',
         // needed for the icon/text gap below to take effect
         display: 'inline-flex',
         alignItems: 'center',
+        verticalAlign: 'center',
         fontSize: theme.typography.size.xs,
         fontWeight: theme.typography.fontWeightRegular,
       }
