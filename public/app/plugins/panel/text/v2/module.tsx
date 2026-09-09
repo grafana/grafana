ccf348eca30fec6c224365484b0f1b75a70dc5db
Adela Almasan <88068998+adela-almasan@users.noreply.github.com>
2026-09-09 14:28:21 -0500

    Text: Remove row limit option (#132207)

--- public/app/plugins/panel/text/v2/module.tsx
+++ public/app/plugins/panel/text/v2/module.tsx
@@ -5,7 +5,7 @@ import { getFeatureFlagClient } from '@grafana/runtime/internal';
 import { defaultCodeOptions, defaultOptions, type Options, RenderMode } from '../panelcfg.gen';
 
 import { TextNGPanel } from './TextNGPanel';
-import { hasRenderableData, MAX_RENDERED_ROWS } from './renderContent';
+import { hasRenderableData } from './renderContent';
 import { textPanelMigrationHandler } from './textPanelMigrationHandler';
 
 function newFeaturesEnabled(): boolean {
@@ -53,18 +53,6 @@ export const textNGPanelOptions: PanelOptionsSupplier<Options> = (builder) => {
         },
       ],
     },
-    showIf: showForData,
-  });
-
-  builder.addNumberInput({
-    path: 'maxRows',
-    name: t('textng.options.max-rows', 'Row limit'),
-    description: t(
-      'textng.options.max-rows-description',
-      'Rows of query data to render. High values can slow the panel, especially with complex HTML.'
-    ),
-    category: dataCategory,
-    settings: { min: 1, max: MAX_RENDERED_ROWS, integer: true, placeholder: String(MAX_RENDERED_ROWS) },
     showIf: showForData,
   });
 };
