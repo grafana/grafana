a0459da59d08f4bbbfe919226f845c1807e1eb92
Piotr Jamróz <pm.jamroz@gmail.com>
2026-09-10 09:51:43 +0200

    Revert group/version resolution (it introduces import  cycles)

--- public/app/features/dashboard-scene/serialization/buildDashboardWithAccessInfoFromScene.ts
+++ public/app/features/dashboard-scene/serialization/buildDashboardWithAccessInfoFromScene.ts
@@ -1,6 +1,9 @@
 import { type Spec as DashboardV2Spec } from '@grafana/schema/apis/dashboard.grafana.app/v2';
+import {
+  DASHBOARD_API_GROUP,
+  dashboardAPIVersionResolver,
+} from 'app/features/dashboard/api/DashboardAPIVersionResolver';
 import { type DashboardWithAccessInfo } from 'app/features/dashboard/api/types';
-import { getK8sV2DashboardApiConfig } from 'app/features/dashboard/api/v2';
 
 import { type DashboardScene } from '../scene/DashboardScene';
 
@@ -40,12 +43,11 @@ export function buildDashboardWithAccessInfoFromScene(
   scene: DashboardScene,
   spec: DashboardV2Spec
 ): DashboardWithAccessInfo<DashboardV2Spec> {
-  const { group, version } = getK8sV2DashboardApiConfig();
   return {
     kind: 'DashboardWithAccessInfo',
     metadata: resolveMetadata(scene),
     access: resolveAccess(scene),
-    apiVersion: `${group}/${version}`,
+    apiVersion: `${DASHBOARD_API_GROUP}/${dashboardAPIVersionResolver.getV2()}`,
     spec,
   };
 }
