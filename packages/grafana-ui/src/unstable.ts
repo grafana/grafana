f3d083b0c021ee68ccf65250728972b37e8b8230
Leon Sorokin <leeoniya@gmail.com>
2026-09-11 08:40:06 -0500

    move to internal package

--- packages/grafana-ui/src/unstable.ts
+++ packages/grafana-ui/src/unstable.ts
@@ -34,8 +34,6 @@ export type {
   SignatureInformation,
   SignatureParameter,
 } from './components/CodeMirror/types';
-export { useDragAndDrop } from './components/DragAndDrop/useDragAndDrop';
-export type { DragAndDropComponents, DragAndDropModule } from './components/DragAndDrop/useDragAndDrop';
 export { getQueryFieldConfig } from './components/QueryFieldConfig/queryFieldConfig';
 export type { QueryFieldConfig, QueryFieldConfigOptions } from './components/QueryFieldConfig/queryFieldConfig';
 export { TableNG } from './components/Table/TableNG/TableNG';
