96579c762c0550289232594e0dee2c8ed3bced6b
Leon Sorokin <leeoniya@gmail.com>
2026-09-12 09:43:46 -0500

    lazy table

--- packages/grafana-ui/src/components/Table/Table.tsx
+++ packages/grafana-ui/src/components/Table/Table.tsx
@@ -1,1 +1,23 @@
-export { Table } from './TableRT/Table';
+import { lazy, Suspense } from 'react';
+
+import { t } from '@grafana/i18n';
+
+import { LoadingPlaceholder } from '../LoadingPlaceholder/LoadingPlaceholder';
+
+import { type TableRTProps } from './types';
+
+const LazyTable = lazy(() => import('./TableRT/Table').then((module) => ({ default: module.Table })));
+
+export function Table(props: TableRTProps) {
+  return (
+    <Suspense
+      fallback={
+        <div style={{ width: props.width, height: props.height }}>
+          <LoadingPlaceholder text={t('grafana-ui.table.loading', 'Loading table')} />
+        </div>
+      }
+    >
+      <LazyTable {...props} />
+    </Suspense>
+  );
+}
