9a8c3bb5ec342321765b540d63a78aa9f4e0648f
septy <septycreator@gmail.com>
2026-09-13 16:29:11 +0000

    Graphite: Fix query editor state race

--- public/app/plugins/datasource/graphite/state/store.ts
+++ public/app/plugins/datasource/graphite/state/store.ts
@@ -189,10 +189,17 @@ const reducer = async (action: Action, state: GraphiteQueryEditorState): Promise
 
 export const createStore = (onChange: (state: GraphiteQueryEditorState) => void): Dispatch<AnyAction> => {
   let state = {} as GraphiteQueryEditorState;
+  let pending = Promise.resolve();
 
-  const dispatch = async (action: AnyAction) => {
-    state = await reducer(action, state);
-    onChange(state);
+  const dispatch = (action: AnyAction) => {
+    const next = pending.then(async () => {
+      state = await reducer(action, state);
+      onChange(state);
+    });
+
+    pending = next.catch(() => {});
+
+    return next;
   };
 
   return dispatch as Dispatch<AnyAction>;
