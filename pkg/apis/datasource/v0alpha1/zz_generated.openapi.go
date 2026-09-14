a1b4a931205a2dd75cd29266dccefac2b8ebc0a7
Ryan McKinley <ryantxu@gmail.com>
2026-09-14 17:12:41 +0900

    labels

--- pkg/apis/datasource/v0alpha1/zz_generated.openapi.go
+++ pkg/apis/datasource/v0alpha1/zz_generated.openapi.go
@@ -304,6 +304,21 @@ func schema_pkg_apis_datasource_v0alpha1_DataSourceConnection(ref common.Referen
 							Format: "",
 						},
 					},
+					"labels": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"object"},
+							AdditionalProperties: &spec.SchemaOrBool{
+								Allows: true,
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
 				},
 				Required: []string{"title", "name", "group", "version"},
 			},
@@ -530,8 +545,9 @@ func schema_pkg_apis_datasource_v0alpha1_GenericDataSourceSpec(ref common.Refere
 					},
 					"isDefault": {
 						SchemaProps: spec.SchemaProps{
-							Type:   []string{"boolean"},
-							Format: "",
+							Description: "Deprecated this will be replaced with metadata.labels[\"default\"]",
+							Type:        []string{"boolean"},
+							Format:      "",
 						},
 					},
 					"url": {
--- pkg/registry/apis/datasource/converter/converter.go
+++ pkg/registry/apis/datasource/converter/converter.go
@@ -14,6 +14,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/grafana/authlib/types"
+
 	common "github.com/grafana/grafana/pkg/apimachinery/apis/common/v0alpha1"
 	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	datasourceV0 "github.com/grafana/grafana/pkg/apis/datasource/v0alpha1"
@@ -101,6 +102,14 @@ func (r *Converter) AsDataSource(ds *datasources.DataSource) (*datasourceV0.Data
 			utils.LabelKeyDeprecatedInternalID: strconv.FormatInt(ds.ID, 10),
 		}
 	}
+
+	if ds.IsDefault {
+		if obj.Labels == nil {
+			obj.Labels = map[string]string{}
+		}
+		obj.Labels["default"] = "true"
+	}
+
 	return obj, nil
 }
 
@@ -136,6 +145,12 @@ func (r *Converter) ToAddCommand(ds *datasourceV0.DataSource) (*datasources.AddD
 		return nil, err
 	}
 
+	// Can configure in the body or in the labels
+	isDefault := ds.Spec.IsDefault()
+	if ds.Labels != nil && !isDefault {
+		isDefault = ds.Labels["default"] == "true"
+	}
+
 	cmd := &datasources.AddDataSourceCommand{
 		Name:  ds.Spec.Title(),
 		UID:   ds.Name,
@@ -149,8 +164,8 @@ func (r *Converter) ToAddCommand(ds *datasourceV0.DataSource) (*datasources.AddD
 		BasicAuth:       ds.Spec.BasicAuth(),
 		BasicAuthUser:   ds.Spec.BasicAuthUser(),
 		WithCredentials: ds.Spec.WithCredentials(),
-		IsDefault:       ds.Spec.IsDefault(),
 		ReadOnly:        ds.Spec.ReadOnly(),
+		IsDefault:       isDefault,
 	}
 
 	jsonData := ds.Spec.JSONData()
