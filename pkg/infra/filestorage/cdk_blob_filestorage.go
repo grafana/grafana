37453394639954f9a9f873b29c24f21f547a3053
Ryan McKinley <ryantxu@gmail.com>
2026-09-14 16:43:06 +0900

    moderniz

--- pkg/infra/filestorage/cdk_blob_filestorage.go
+++ pkg/infra/filestorage/cdk_blob_filestorage.go
@@ -182,7 +182,6 @@ func (c cdkBlobStorage) CreateFolder(ctx context.Context, path string) error {
 	foundFolderIndex := -1
 
 	for i, currentFolder := range slices.Backward(precedingFolders) {
-
 		att, err := c.bucket.Attributes(ctx, strings.ToLower(currentFolder+Delimiter+directoryMarker))
 		if err != nil {
 			if gcerrors.Code(err) != gcerrors.NotFound {
