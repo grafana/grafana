fc3d02c6a6974b2860b43c4c504ecb07a95f9c68
Ashley Harrison <ashley.harrison@grafana.com>
2026-09-15 17:18:05 +0100

    remove unused import

--- public/app/app.ts
+++ public/app/app.ts
@@ -113,7 +113,6 @@ import {
   getObservablePluginComponents,
   getObservablePluginLinks,
 } from './features/plugins/extensions/getPluginExtensions';
-import { getPluginExtensionRegistries } from './features/plugins/extensions/registry/setup';
 import { usePluginComponent } from './features/plugins/extensions/usePluginComponent';
 import { usePluginComponents } from './features/plugins/extensions/usePluginComponents';
 import { usePluginFunctions } from './features/plugins/extensions/usePluginFunctions';
