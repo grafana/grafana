7ff94c6cc75ae4204d3f4825d854ab34f8cc0d36
eledobleefe <laura.fernandez@grafana.com>
2026-09-15 11:44:14 +0200

    Generate files

--- packages/grafana-runtime/src/internal/openFeature/openfeature.gen.ts
+++ packages/grafana-runtime/src/internal/openFeature/openfeature.gen.ts
@@ -91,6 +91,8 @@ export const FlagKeys = {
   GrafanaKubernetesAnnotationsClient: "grafana.kubernetesAnnotationsClient",
   /** Enables log level inference from log line contents when level is not defined as a field or a label */
   GrafanaLogLevelInference: "grafana.logLevelInference",
+  /** Enables multi-tenant fallback behavior */
+  GrafanaMtFallback: "grafana.mtFallback",
   /** Builds the navigation tree client-side instead of reading it from /bootdata */
   GrafanaMultiTenantNavTree: "grafana.multiTenantNavTree",
   /** Read the current user's permissions from the IAM app platform API instead of /api/access-control/user/actions */
@@ -638,6 +640,17 @@ export const useFlagGrafanaKubernetesAnnotationsClient = (options?: ReactFlagEva
  */
 export const useFlagGrafanaLogLevelInference = (options?: ReactFlagEvaluationOptions): boolean => {
   return useFlag("grafana.logLevelInference", false, options).value;
+};
+
+/**
+ * Enables multi-tenant fallback behavior
+ *
+ * **Details:**
+ * - flag key: `grafana.mtFallback`
+ * - default value: `false`
+ */
+export const useFlagGrafanaMtFallback = (options?: ReactFlagEvaluationOptions): boolean => {
+  return useFlag("grafana.mtFallback", false, options).value;
 };
 
 /**
