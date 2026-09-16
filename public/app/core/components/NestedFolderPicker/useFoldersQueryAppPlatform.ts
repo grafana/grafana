c82744d09249f9172e2f0b87951361c685217aea
Alex Khomenko <Clarity-89@users.noreply.github.com>
2026-09-16 19:41:25 +0300

    Dashboards: Show Git Sync badges in folder search (#132790)

--- public/app/core/components/NestedFolderPicker/useFoldersQueryAppPlatform.ts
+++ public/app/core/components/NestedFolderPicker/useFoldersQueryAppPlatform.ts
@@ -6,6 +6,7 @@ import { t } from '@grafana/i18n';
 import { dashboardAPIv0alpha1 } from 'app/api/clients/dashboard/v0alpha1';
 import { getMessageFromError } from 'app/core/utils/errors';
 import { type DashboardViewItemWithUIItems, type DashboardsTreeItem } from 'app/features/browse-dashboards/types';
+import { isRootFolderUID } from 'app/features/search/constants';
 import { useDispatch, useSelector } from 'app/types/store';
 
 import { type ManagerKind } from '../../../features/apiserver/types';
@@ -164,7 +165,7 @@ export function useFoldersQueryAppPlatform({
             // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
             managedBy: item.managedBy?.kind as ManagerKind | undefined,
             managerId: item.managedBy?.id,
-            parentUID: item.folder,
+            parentUID: isRootFolderUID(item.folder) ? undefined : item.folder,
           },
         };
 
