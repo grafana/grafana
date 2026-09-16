b3c87ecf6f1418512c489874802316e4a9ecc5a9
Mihaly Gyongyosi <mgyongyosi@users.noreply.github.com>
2026-09-16 15:50:49 +0200

    Build: Refresh enterprise query import manifests

--- pkg/extensions/enterprise_imports_test.go
+++ pkg/extensions/enterprise_imports_test.go
@@ -81,6 +81,8 @@ import (
 	_ "k8s.io/apiserver/pkg/util/compatibility"
 	_ "k8s.io/client-go/dynamic/fake"
 	_ "k8s.io/component-base/featuregate/testing"
+	_ "k8s.io/kube-openapi/pkg/validation/strfmt"
+	_ "k8s.io/kube-openapi/pkg/validation/validate"
 	_ "pgregory.net/rapid"
 	_ "sigs.k8s.io/randfill"
 	_ "sigs.k8s.io/structured-merge-diff/v6/fieldpath"
