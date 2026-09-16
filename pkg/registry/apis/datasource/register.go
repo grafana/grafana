c24e06b7be24c800e675f4a05108f2ae05037e22
Ryan McKinley <ryantxu@gmail.com>
2026-09-16 22:52:29 +0900

    remove scheme

--- pkg/registry/apis/datasource/register.go
+++ pkg/registry/apis/datasource/register.go
@@ -270,21 +270,6 @@ func (b *DataSourceAPIBuilder) AllowedV0Alpha1Resources() []string {
 }
 
 func (b *DataSourceAPIBuilder) UpdateAPIGroupInfo(apiGroupInfo *genericapiserver.APIGroupInfo, opts builder.APIGroupOptions) error {
-	if opts.StorageOptsRegister != nil {
-		opts.StorageOptsRegister(b.datasourceResourceInfo.GroupResource(), apistore.StorageOptions{
-			Index: nil, // TODO, required to check that they are unique
-
-			// Keep them for now, but we should get rid of them when possible
-			DeprecatedInternalID: apistore.DeprecatedID_Required,
-
-			// Setting the schema explicitly will force the apistore to explicitly marshal with a matching Group+version
-			// This is required because we map the same go type (DataSourceConfig) across multiple api groups
-			// and the default k8s codec will pick the first one registered, regardless which group is set
-			// See: https://github.com/kubernetes/kubernetes/blob/v1.34.3/staging/src/k8s.io/apimachinery/pkg/runtime/serializer/versioning/versioning.go#L267
-			Scheme: opts.Scheme,
-		})
-	}
-
 	storage := map[string]rest.Storage{}
 
 	// Register the raw datasource connection
@@ -306,8 +291,19 @@ func (b *DataSourceAPIBuilder) UpdateAPIGroupInfo(apiGroupInfo *genericapiserver
 			resourceInfo:                    &ds,
 			dsConfigHandlerRequestsDuration: b.dataSourceCRUDMetric,
 		}
+		// The declared GVK forces apistore to marshal with a matching group+version.
+		// This is required because we map the same go type (DataSourceConfig) across
+		// multiple api groups, and the default k8s codec picks the first one registered
+		// regardless of which group is set.
+		// See: https://github.com/kubernetes/kubernetes/blob/v1.34.3/staging/src/k8s.io/apimachinery/pkg/runtime/serializer/versioning/versioning.go#L267
+		optsGetter := opts.StorageOptsGetterFor(ds, apistore.StorageOptions{
+			Index: nil, // TODO, required to check that they are unique
+
+			// Keep them for now, but we should get rid of them when possible
+			DeprecatedInternalID: apistore.DeprecatedID_Required,
+		})
 		// NOTE! there is currently NO PATH that is using unified storage to read!
-		unified, err := grafanaregistry.NewRegistryStore(opts.Scheme, ds, opts.OptsGetter)
+		unified, err := grafanaregistry.NewRegistryStore(opts.Scheme, ds, optsGetter)
 		if err != nil {
 			return err
 		}
