1c3e6c94ade378c8c7a344195e2080fd7a9422b0
Elaine Vuong <elaine.yt.vuong@gmail.com>
2026-09-16 10:45:45 -0400

    fix bug for when the last eval is 1 interval old

--- pkg/services/ngalert/state/manager_test.go
+++ pkg/services/ngalert/state/manager_test.go
@@ -2288,6 +2288,46 @@ func TestNoDataKeepLastDoesNotBlockMissingSeriesEviction(t *testing.T) {
 		len(currentStates), iterations)
 }
 
+func TestNoDataKeepLastWithMissingSeriesEvalsToResolveOne(t *testing.T) {
+	ctx := context.Background()
+	clk := clock.NewMock()
+
+	cfg := state.ManagerCfg{
+		Metrics:       metrics.NewNGAlert(prometheus.NewPedanticRegistry()).GetStateMetrics(),
+		ExternalURL:   nil,
+		InstanceStore: &state.FakeInstanceStore{},
+		Images:        &state.NoopImageService{},
+		Clock:         clk,
+		Historian:     &state.FakeHistorian{},
+		Tracer:        tracing.InitializeTracerForTest(),
+		Log:           log.New("ngalert.state.manager"),
+	}
+	st := state.NewManager(cfg, state.NewNoopPersister())
+
+	gen := models.RuleGen
+	rule := gen.With(
+		gen.WithFor(0),
+		gen.WithMissingSeriesEvalsToResolve(1),
+		gen.WithNoDataExecAs(models.KeepLast),
+	).GenerateRef()
+
+	dataResult := eval.ResultGen(eval.WithState(eval.Normal), eval.WithEvaluatedAt(clk.Now()))()
+	_, err := st.ProcessEvalResults(ctx, clk.Now(), rule, eval.Results{dataResult}, nil, nil)
+	require.NoError(t, err)
+	clk.Add(time.Duration(rule.IntervalSeconds) * time.Second)
+
+	for i := 0; i < 3; i++ {
+		_, err = st.ProcessEvalResults(ctx, clk.Now(), rule, eval.Results{}, nil, nil)
+		require.NoError(t, err)
+		clk.Add(time.Duration(rule.IntervalSeconds) * time.Second)
+
+		currentStates := st.GetStatesForRuleUID(ctx, rule.OrgID, rule.UID)
+		require.Lenf(t, currentStates, 1,
+			"MissingSeriesEvalsToResolve=1 should not stop KeepLast from holding a continuously-evaluated series (NoData tick %d)", i+1)
+		require.Equal(t, eval.Normal, currentStates[0].State)
+	}
+}
+
 func TestIntegrationDeleteStateByRuleUID(t *testing.T) {
 	tutil.SkipIntegrationTestInShortMode(t)
 
