305be372ada18363272a27efcf06609ad9b9f85c
eledobleefe <laura.fernandez@grafana.com>
2026-09-17 13:32:09 +0200

    Move custom hook to a new file

--- public/app/core/navigation/GrafanaRoute.tsx
+++ public/app/core/navigation/GrafanaRoute.tsx
@@ -1,10 +1,7 @@
-import { type JsonValue } from '@openfeature/react-sdk';
-import { type Location } from 'history';
-import { Suspense, useEffect, useLayoutEffect, useState } from 'react';
-import { matchPath, Navigate, useLocation, useParams } from 'react-router-dom-v5-compat';
+import { Suspense, useEffect, useLayoutEffect } from 'react';
+import { Navigate, useLocation, useParams } from 'react-router-dom-v5-compat';
 
 import { config, locationSearchToObject, navigationLogger, reportPageview } from '@grafana/runtime';
-import { useFlagGrafanaMtFallback } from '@grafana/runtime/internal';
 import { ErrorBoundary, PageLoader } from '@grafana/ui';
 import { updateMeticulousRecording } from 'app/core/services/meticulous';
 import { isFrontendService } from 'app/core/utils/isFrontendService';
@@ -14,43 +11,10 @@ import { useGrafana } from '../context/GrafanaContext';
 import { contextSrv } from '../services/context_srv';
 
 import { GrafanaRouteError } from './GrafanaRouteError';
+import { useMTFallback } from './mtFallback';
 import { type GrafanaRouteComponentProps, type RouteDescriptor } from './types';
 
 export interface Props extends Pick<GrafanaRouteComponentProps, 'route' | 'location'> {}
-
-const useMTFallback = (location: Location) => {
-  const flagValue = useFlagGrafanaMtFallback();
-  const urlList = getAllowedList(flagValue);
-  const isUrlAllowed: boolean = urlList?.length
-    ? urlList.some((pattern) => matchPath(pattern, location.pathname) !== null)
-    : true;
-  const [isWaiting, setIsWaiting] = useState(!isUrlAllowed);
-
-  useEffect(() => {
-    if (isUrlAllowed) {
-      setIsWaiting(false);
-      return;
-    }
-
-    setIsWaiting(true);
-    const timeout = setTimeout(() => setIsWaiting(false), 60_000);
-    return () => clearTimeout(timeout);
-  }, [isUrlAllowed, location.pathname]);
-
-  return isWaiting;
-};
-
-const getAllowedList = (value: JsonValue): string[] | undefined => {
-  if (typeof value !== 'object' || !value) {
-    return;
-  }
-  const allowList = 'allowList' in value ? value.allowList : undefined;
-  if (Array.isArray(allowList)) {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return allowList as string[];
-  }
-  return;
-};
 
 export function GrafanaRoute(props: Props) {
   const { chrome, keybindings } = useGrafana();
