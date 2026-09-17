c5b3d0cb7c96b90f9841792396ae787cb3f08e2f
Torkel Ödegaard <torkel@grafana.com>
2026-09-17 16:06:46 +0200

    AA fixes

--- packages/grafana-ui/src/components/Button/Button.tsx
+++ packages/grafana-ui/src/components/Button/Button.tsx
@@ -317,7 +317,7 @@ export const getButtonStyles = (props: StyleProps) => {
 export function getActiveButtonStyles(color: ThemeRichColor, fill: ButtonFill, visualRefreshEnabled?: boolean) {
   let backgroundColor = 'transparent';
   if (fill === 'solid') {
-    backgroundColor = visualRefreshEnabled ? color.main : color.background;
+    backgroundColor = visualRefreshEnabled ? color.background : color.main;
   }
   return {
     background: backgroundColor,
