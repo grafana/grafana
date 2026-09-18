bb9919a99d89c53e15a7873920cf2a003ef529fa
Peter Štibraný <pstibrany@gmail.com>
2026-09-18 16:14:23 +0200

    Search: Fix direct client CI check

--- pkg/storage/unified/semgrep/resource-search-field-values.go
+++ pkg/storage/unified/semgrep/resource-search-field-values.go
@@ -5,42 +5,42 @@ package semgreptest
 import resourcepb "github.com/grafana/grafana/pkg/storage/unified/resourcepb"
 
 func missing() *resourcepb.ResourceSearchRequest {
-	// ruleid: resource-search-requires-field-values
+	// ruleid: direct-go-resource-search-requires-field-values
 	return &resourcepb.ResourceSearchRequest{Limit: 10}
 }
 
 func unspecified() *resourcepb.ResourceSearchRequest {
-	// ruleid: resource-search-requires-field-values
+	// ruleid: direct-go-resource-search-requires-field-values
 	return &resourcepb.ResourceSearchRequest{
 		ResultFormat: resourcepb.ResourceSearchRequest_UNSPECIFIED,
 	}
 }
 
 func table() *resourcepb.ResourceSearchRequest {
-	// ruleid: resource-search-requires-field-values
+	// ruleid: direct-go-resource-search-requires-field-values
 	return &resourcepb.ResourceSearchRequest{
 		ResultFormat: resourcepb.ResourceSearchRequest_RESOURCE_TABLE,
 	}
 }
 
 func dynamic(format resourcepb.ResourceSearchRequest_ResultFormat) *resourcepb.ResourceSearchRequest {
-	// ruleid: resource-search-requires-field-values
+	// ruleid: direct-go-resource-search-requires-field-values
 	return &resourcepb.ResourceSearchRequest{ResultFormat: format}
 }
 
 func allocated() *resourcepb.ResourceSearchRequest {
-	// ruleid: resource-search-requires-field-values
+	// ruleid: direct-go-resource-search-requires-field-values
 	return new(resourcepb.ResourceSearchRequest)
 }
 
 func declared() resourcepb.ResourceSearchRequest {
-	// ruleid: resource-search-requires-field-values
+	// ruleid: direct-go-resource-search-requires-field-values
 	var request resourcepb.ResourceSearchRequest
 	return request
 }
 
 func fieldValues() *resourcepb.ResourceSearchRequest {
-	// ok: resource-search-requires-field-values
+	// ok: direct-go-resource-search-requires-field-values
 	return &resourcepb.ResourceSearchRequest{
 		ResultFormat: resourcepb.ResourceSearchRequest_FIELD_VALUES,
 	}
