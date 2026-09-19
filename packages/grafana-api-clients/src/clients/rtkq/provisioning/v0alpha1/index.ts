de4c44c2164bb5c8586ca56746a8e7b1bc68d1ac
Rafael Paulovic <rafael.paulovic@grafana.com>
2026-09-19 12:57:19 +0200

    refactor: use qualified provisioning resource ref

--- packages/grafana-api-clients/src/clients/rtkq/provisioning/v0alpha1/index.ts
+++ packages/grafana-api-clients/src/clients/rtkq/provisioning/v0alpha1/index.ts
@@ -2,5 +2,4 @@ export { BASE_URL, API_GROUP, API_VERSION } from './baseAPI';
 import { generatedAPI as rawAPI } from './endpoints.gen';
 
 export * from './endpoints.gen';
-export type { ProvisioningResourceRef as ResourceRef } from './endpoints.gen';
 export const generatedAPI = rawAPI.enhanceEndpoints({});
