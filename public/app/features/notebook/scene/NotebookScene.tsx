43f20e7b169802c950a655ed4bf9d59e005045f6
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2026-09-21 14:53:19 -0300

    fix lint issues

--- public/app/features/notebook/scene/NotebookScene.tsx
+++ public/app/features/notebook/scene/NotebookScene.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/css';
-import { isEqual } from 'lodash';
 import { Global } from '@emotion/react';
+import { isEqual } from 'lodash';
 
 import { CoreApp, type DataQueryRequest, type GrafanaTheme2 } from '@grafana/data';
 import { t } from '@grafana/i18n';
