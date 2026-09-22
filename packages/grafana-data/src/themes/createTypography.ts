643b5e27cbb8990d4e181ac0a2f7a454913ea304
Ashley Harrison <ashley.harrison@grafana.com>
2026-09-22 16:12:47 +0100

    add correct letter-spacing

--- packages/grafana-data/src/themes/createTypography.ts
+++ packages/grafana-data/src/themes/createTypography.ts
@@ -112,12 +112,12 @@ export function createTypography(colors: ThemeColors, typographyInput: ThemeTypo
 
   // All our fonts/line heights should be integer multiples of 2 to prevent issues with alignment
   const variants = {
-    '2xl': buildVariant(fontFamily, fontWeightBold, 24, 34),
-    xl: buildVariant(fontFamily, fontWeightBold, 22, 30),
+    '2xl': buildVariant(fontFamily, fontWeightBold, 24, 34, -0.3),
+    xl: buildVariant(fontFamily, fontWeightBold, 22, 30, -0.2),
     lg: buildVariant(fontFamily, fontWeightMedium, 20, 26),
     md: buildVariant(fontFamily, fontWeightMedium, 16, 22),
     base: buildVariant(fontFamily, fontWeightRegular, fontSize, 22),
-    sm: buildVariant(fontFamily, fontWeightMedium, 12, 18),
+    sm: buildVariant(fontFamily, fontWeightMedium, 12, 18, 0.2),
     code: buildVariant(fontFamilyMonospace, fontWeightRegular, 14, 16),
 
     // Deprecated variants
