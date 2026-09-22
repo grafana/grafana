12971c7a3f78afd345b5e0e9472293af82fd97b2
grafakus <marc.mignonsin@grafana.com>
2026-09-22 20:11:55 +0200

    Dashboards: Add edit actions popover for tabs

--- public/app/features/dashboard-scene/scene/edit-actions-popover/PanelEditActions.tsx
+++ public/app/features/dashboard-scene/scene/edit-actions-popover/PanelEditActions.tsx
@@ -53,8 +53,28 @@ export function PanelEditActions({
         {t('dashboard-scene.panel-edit-actions.edit-visualization', 'Edit visualization')}
       </Button>
       <div className={styles.actionsDivider} />
-      <CopyActionButton onClick={onClickCopy} isRepeated={isRepeated} />
-      <DuplicateActionButton onClick={onClickDuplicate} isRepeated={isRepeated} />
+      <CopyActionButton
+        onClick={onClickCopy}
+        disabledReason={
+          isRepeated
+            ? t(
+                'dashboard-scene.control-edit-actions.copied-tooltip-disabled',
+                "Repeated panels can't be copied individually"
+              )
+            : undefined
+        }
+      />
+      <DuplicateActionButton
+        onClick={onClickDuplicate}
+        disabledReason={
+          isRepeated
+            ? t(
+                'dashboard-scene.control-edit-actions.duplicate-tooltip-disabled',
+                "Repeated panels can't be duplicated individually"
+              )
+            : undefined
+        }
+      />
       <DeleteActionButton
         title={t('dashboard.sidebar.viz-panel.delete-panel-title', 'Delete panel?')}
         text={t(
@@ -63,7 +83,14 @@ export function PanelEditActions({
         )}
         yesText={t('dashboard.sidebar.viz-panel.delete-panel-yes', 'Delete')}
         onConfirm={onClickDelete}
-        isRepeated={isRepeated}
+        disabledReason={
+          isRepeated
+            ? t(
+                'dashboard-scene.control-edit-actions.delete-tooltip-disabled',
+                "Repeated panels can't be deleted individually"
+              )
+            : undefined
+        }
       />
     </>
   );
