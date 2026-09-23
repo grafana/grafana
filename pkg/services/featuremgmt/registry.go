e67585e63cd1bc5dc3069074a3914665ba05cb0e
Ashley Harrison <ashley.harrison@grafana.com>
2026-09-23 13:02:29 +0100

    promote visual refresh flag to public preview

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -2968,7 +2968,7 @@ var (
 		{
 			Name:         "grafana.visualDesignRefresh",
 			Description:  "Enables the new visual design refresh for the Grafana UI",
-			Stage:        FeatureStageExperimental,
+			Stage:        FeatureStagePublicPreview,
 			Owner:        grafanaFrontendPlatformSquad,
 			HideFromDocs: true,
 			Expression:   "false",
