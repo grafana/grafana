1967612fe22aa61aaef6261ec0a127c986192866
Ashley Harrison <ashley.harrison@grafana.com>
2026-09-23 16:56:18 +0100

    fix unintentional letter-spacing change

--- packages/grafana-data/src/themes/createTypography.ts
+++ packages/grafana-data/src/themes/createTypography.ts
@@ -91,21 +91,19 @@ export function createTypography(colors: ThemeColors, typographyInput: ThemeTypo
     fontWeight: number,
     size: number,
     lineHeight: number,
-    letterSpacing?: number,
+    letterSpacing: number,
     casing?: object
   ): ThemeTypographyVariant => {
     if (lineHeight % 2 !== 0 || size % 2 !== 0) {
       throw new Error('Font size and line height should be integer multiples of 2 to prevent issues with alignment');
     }
 
-    const roundedLetterSpacing = letterSpacing !== undefined ? `${round(letterSpacing / size)}em` : undefined;
-
     return {
       fontFamily,
       fontWeight,
       fontSize: pxToRem(size),
       lineHeight: lineHeight / size,
-      letterSpacing: roundedLetterSpacing,
+      ...(fontFamily === defaultFontFamily ? { letterSpacing: `${round(letterSpacing / size)}em` } : {}),
       ...casing,
     };
   };
@@ -114,18 +112,18 @@ export function createTypography(colors: ThemeColors, typographyInput: ThemeTypo
   const variants = {
     xxl: buildVariant(fontFamily, fontWeightBold, 24, 34, -0.3),
     xl: buildVariant(fontFamily, fontWeightBold, 22, 30, -0.2),
-    lg: buildVariant(fontFamily, fontWeightMedium, 20, 26),
-    md: buildVariant(fontFamily, fontWeightMedium, 16, 22),
-    base: buildVariant(fontFamily, fontWeightRegular, fontSize, 22),
+    lg: buildVariant(fontFamily, fontWeightMedium, 20, 26, 0),
+    md: buildVariant(fontFamily, fontWeightMedium, 16, 22, 0),
+    base: buildVariant(fontFamily, fontWeightRegular, fontSize, 22, 0),
     sm: buildVariant(fontFamily, fontWeightMedium, 12, 18, 0.2),
-    code: buildVariant(fontFamilyMonospace, fontWeightRegular, 14, 16),
+    code: buildVariant(fontFamilyMonospace, fontWeightRegular, 14, 16, 0.15),
 
     // Deprecated variants
     h1: buildVariant(fontFamily, fontWeightRegular, 28, 32, -0.25),
-    h2: buildVariant(fontFamily, fontWeightRegular, 24, 28),
-    h3: buildVariant(fontFamily, fontWeightRegular, 22, 24),
+    h2: buildVariant(fontFamily, fontWeightRegular, 24, 28, 0),
+    h3: buildVariant(fontFamily, fontWeightRegular, 22, 24, 0),
     h4: buildVariant(fontFamily, fontWeightRegular, 18, 22, 0.25),
-    h5: buildVariant(fontFamily, fontWeightRegular, 16, 22),
+    h5: buildVariant(fontFamily, fontWeightRegular, 16, 22, 0),
     h6: buildVariant(fontFamily, fontWeightMedium, 14, 22, 0.15),
     body: buildVariant(fontFamily, fontWeightRegular, fontSize, 22, 0.15),
     bodySmall: buildVariant(fontFamily, fontWeightRegular, 12, 18, 0.15),
