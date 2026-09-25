1491d0ee4b65c0b02e8c2498196f432550feebbe
Craig O'Donnell <craig.odonnell@grafana.com>
2026-09-25 10:17:45 -0400

    fix: no-op down migration

--- pkg/registry/apps/annotation/migrations/00009_backfill_time_end.sql
+++ pkg/registry/apps/annotation/migrations/00009_backfill_time_end.sql
@@ -2,4 +2,5 @@
 UPDATE annotations SET time_end = time WHERE time_end IS NULL;
 
 -- +goose Down
-UPDATE annotations SET time_end = NULL WHERE time_end = time;
+-- This migration is not safely reversible as it is not possible to know which rows had NULL time_end before the update.
+-- Leaving time_end = time is acceptable since the annotations will still be interpreted as point annotations.
