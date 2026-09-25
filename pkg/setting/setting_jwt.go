61e3f32ca39d7ae1a53dd535d2b8e2cd6c048862
Charandas <542168+charandas@users.noreply.github.com>
2026-09-25 11:42:18 -0700

    ext_jwt: allow configuring org_id for authn (#133607)

--- pkg/setting/setting_jwt.go
+++ pkg/setting/setting_jwt.go
@@ -41,10 +41,11 @@ type AuthJWTSettings struct {
 }
 
 type ExtJWTSettings struct {
-	Enabled      bool
-	ExpectIssuer string
-	JWKSUrl      string
-	Audiences    []string
+	Enabled       bool
+	ExpectIssuer  string
+	JWKSUrl       string
+	Audiences     []string
+	WildcardOrgID int64
 }
 
 func (cfg *Cfg) readAuthExtJWTSettings() {
@@ -52,6 +53,7 @@ func (cfg *Cfg) readAuthExtJWTSettings() {
 	jwtSettings := ExtJWTSettings{}
 	jwtSettings.Enabled = authExtendedJWT.Key("enabled").MustBool(false)
 	jwtSettings.JWKSUrl = authExtendedJWT.Key("jwks_url").MustString("")
+	jwtSettings.WildcardOrgID = authExtendedJWT.Key("wildcard_org_id").MustInt64(0)
 	// for Grafana, this defaults to hard coded value, but we leave it as a configurable param for other use-cases
 	audiences := extJWTAccessTokenExpectAudience
 	if authExtendedJWT.HasKey("audiences") {
